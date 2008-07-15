a2f5f0226f035d5920e0600b9e5eae8509824d02
donncha <donncha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-07-15 10:11:27 +0000

    Use comment_author instead of email in case email isn't mandatory. props rhapsodyv @ http://wordpress.org/support/topic/188775
    git-svn-id: http://plugins.svn.wordpress.org/wp-super-cache/trunk@54851 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-cache-phase1.php
+++ wp-cache-phase1.php
@@ -89,7 +89,7 @@ function wp_cache_postload() {
 function wp_cache_get_cookies_values() {
 	$string = '';
 	while ($key = key($_COOKIE)) {
-		if (preg_match("/^wp-postpass|^wordpress|^comment_author_email_/", $key)) {
+		if (preg_match("/^wp-postpass|^wordpress|^comment_author_/", $key)) {
 			$string .= $_COOKIE[$key] . ",";
 		}
 		next($_COOKIE);
