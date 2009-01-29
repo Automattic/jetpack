b817b845df96942db2edc7997b2eb663a80480c7
donncha <donncha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-29 16:28:55 +0000

    Guess the install root properly
    git-svn-id: http://plugins.svn.wordpress.org/wp-super-cache/trunk@90227 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-cache.php
+++ wp-cache.php
@@ -375,8 +375,10 @@ function wsc_mod_rewrite() {
 	$home_path = get_home_path();
 	$home_root = parse_url(get_bloginfo('url'));
 	$home_root = trailingslashit($home_root['path']);
-	$inst_root = parse_url(get_bloginfo('wpurl'));
-	$inst_root = trailingslashit($inst_root['path']);
+	//$inst_root = parse_url(get_bloginfo('wpurl'));
+	//$inst_root = trailingslashit($inst_root['path']);
+	//$inst_root = trailingslashit( str_replace( get_option('siteurl'), '', WP_CONTENT_URL ) );
+	$inst_root = '/' . trailingslashit( str_replace( $_SERVER[ 'DOCUMENT_ROOT' ], '', WP_CONTENT_DIR ) );
 	$wprules = implode( "\n", extract_from_markers( $home_path.'.htaccess', 'WordPress' ) );
 	$wprules = str_replace( "RewriteEngine On\n", '', $wprules );
 	$wprules = str_replace( "RewriteBase $home_root\n", '', $wprules );
