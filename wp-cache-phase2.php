414df9c91a55fe46ac415d95b538c9578747e416
donncha <donncha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-05 20:53:06 +0000

    No need to call this callback twice!
    git-svn-id: http://plugins.svn.wordpress.org/wp-super-cache/trunk@92230 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-cache-phase2.php
+++ wp-cache-phase2.php
@@ -173,7 +173,6 @@ function get_current_url_supercache_dir() {
 
 function wp_cache_ob_callback( $buffer ) {
 	$buffer = wp_cache_get_ob( $buffer );
-	wp_cache_shutdown_callback();
 	return $buffer;
 }
 
