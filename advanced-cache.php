631fd72f59ec74f8b844670f1eacd400cb04bb01
donncha <donncha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-05 22:15:25 +0000

    Don't show broken path message in wp-admin. Check if file is missing before showing an error
    git-svn-id: http://plugins.svn.wordpress.org/wp-super-cache/trunk@107964 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- advanced-cache.php
+++ advanced-cache.php
@@ -1,8 +1,12 @@
 <?php
 # WP SUPER CACHE 0.8.9.1
 function wpcache_broken_message() {
-	        echo "<!-- WP Super Cache is installed but broken. The path to wp-cache-phase1.php in wp-content/advanced-cache.php must be fixed! -->";
+	if ( false == strpos( $_SERVER[ 'REQUEST_URI' ], 'wp-admin' ) )
+		echo "<!-- WP Super Cache is installed but broken. The path to wp-cache-phase1.php in wp-content/advanced-cache.php must be fixed! -->";
 }
-if( !include_once( 'CACHEHOME' . 'wp-cache-phase1.php' ) ) 
-	register_shutdown_function( 'wpcache_broken_message' );
+
+if ( !include_once( 'CACHEHOME' . 'wp-cache-phase1.php' ) ) {
+	if ( !@is_file( 'CACHEHOME' . 'wp-cache-phase1.php' ) )
+		register_shutdown_function( 'wpcache_broken_message' );
+}
 ?>
