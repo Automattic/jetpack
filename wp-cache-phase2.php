e9be13a545dc41dc9bea70e3df094b4382cd8a09
donncha <donncha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-18 12:34:06 +0000

    Typos in closing and unlink commands
    git-svn-id: http://plugins.svn.wordpress.org/wp-super-cache/trunk@127014 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-cache-phase2.php
+++ wp-cache-phase2.php
@@ -280,17 +280,17 @@ function wp_cache_get_ob(&$buffer) {
 				if (!$fr2) {
 					$buffer .= "<!-- File not cached! Super Cache Couldn't write to: " . str_replace( ABSPATH, '', $tmp_cache_filename ) . " -->\n";
 					@fclose( $fr );
-					@nlink( $tmp_wpcache_filename );
+					@unlink( $tmp_wpcache_filename );
 					return $buffer;
 				}
 				if( $cache_compression ) {
 					$gz = @fopen( $tmp_cache_filename . ".gz", 'w');
 					if (!$gz) {
 						$buffer .= "<!-- File not cached! Super Cache Couldn't write to: " . str_replace( ABSPATH, '', $tmp_cache_filename ) . ".gz -->\n";
-						@close( $fr );
-						@nlink( $tmp_wpcache_filename );
-						@close( $fr2 );
-						@nlink( $tmp_cache_filename );
+						@fclose( $fr );
+						@unlink( $tmp_wpcache_filename );
+						@fclose( $fr2 );
+						@unlink( $tmp_cache_filename );
 						return $buffer;
 					}
 				}
