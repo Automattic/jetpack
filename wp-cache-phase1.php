fb970baf92a4867787ec3a6855e462ac290b8636
donncha <donncha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-09 08:05:14 +0000

    Don't do slash checks on /
    git-svn-id: http://plugins.svn.wordpress.org/wp-super-cache/trunk@238848 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-cache-phase1.php
+++ wp-cache-phase1.php
@@ -148,7 +148,7 @@ function wp_cache_serve_cache_file() {
 	} else {
 		// last chance, check if a supercache file exists. Just in case .htaccess rules don't work on this host
 		$file = get_current_url_supercache_dir() . "index.html";
-		if ( ( $wp_cache_slash_check && substr( $wp_cache_request_uri, -1 ) == '/' ) || ( $wp_cache_slash_check == 0 && substr( $wp_cache_request_uri, -1 ) != '/' ) && ( wp_cache_get_cookies_values() == '' && empty( $_GET ) && file_exists( $file ) ) ) {
+		if ( ( $wp_cache_request_uri == '/' || ( $wp_cache_slash_check && substr( $wp_cache_request_uri, -1 ) == '/' ) || ( $wp_cache_slash_check == 0 && substr( $wp_cache_request_uri, -1 ) != '/' ) ) && ( wp_cache_get_cookies_values() == '' && empty( $_GET ) && file_exists( $file ) ) ) {
 			header( "Content-type: text/html; charset=UTF-8" ); // UTF-8 hard coded is bad but we don't know what it is this early in the process
 			header( "Vary: Accept-Encoding, Cookie" );
 			header( "Cache-Control: max-age=300, must-revalidate" );
