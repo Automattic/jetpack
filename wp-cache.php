6f9f062ec5d30b77b750a57b73dc3c72f964b44a
donncha <donncha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-29 09:05:24 +0000

    Pause 2 seconds instead of 5 between preload urls
    git-svn-id: http://plugins.svn.wordpress.org/wp-super-cache/trunk@294844 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-cache.php
+++ wp-cache.php
@@ -2402,7 +2402,7 @@ function wp_cron_preload_cache() {
 			$url = get_permalink( $post_id );
 			$msg .= "$url\n";
 			wp_remote_get( $url, array('timeout' => 60, 'blocking' => true ) );
-			sleep( 5 );
+			sleep( 2 );
 			$count++;
 		}
 		if ( $wp_cache_preload_email_me && $wp_cache_preload_email_volume != 'less' )
