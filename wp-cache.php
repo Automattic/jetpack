e81925bb88c6d674cde6cb96cf21f6e2677db9da
donncha <donncha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-29 16:06:20 +0000

    Added "need help?" section to yellow box
    git-svn-id: http://plugins.svn.wordpress.org/wp-super-cache/trunk@294935 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-cache.php
+++ wp-cache.php
@@ -893,6 +893,12 @@ jQuery(document).ready(function(){
 	<p><?php printf( __( '%1$s is maintained and developed by %2$s with contributions from many others.', 'wp-super-cache' ), '<a href="http://ocaoimh.ie/wp-super-cache/?r=supercache">WP Super Cache</a>', '<a href="http://ocaoimh.ie/?r=supercache">Donncha O Caoimh</a>' ); ?></p>
 	<p><?php printf( __( 'He blogs at %1$s and posts photos at %2$s.', 'wp-super-cache' ), '<a href="http://ocaoimh.ie/?r=supercache">Holy Shmoly</a>', '<a href="http://inphotos.org/?r=supercache">In Photos.org</a>' ); ?></p>
 	<p><?php printf( __( 'Please say hi to him on %s too!', 'wp-super-cache' ), '<a href="http://twitter.com/donncha/">Twitter</a>' ); ?></p>
+	<h3 align='center'><?php _e( 'Need Help?', 'wp-super-cache' ); ?></h3>
+	<ol>
+	<li><?php printf( __( '<a href="%1$s">Installation Help</a>', 'wp-super-cache' ), 'http://wordpress.org/extend/plugins/wp-super-cache/installation/' ); ?></li>
+	<li><?php printf( __( '<a href="%1$s">Frequently Asked Questions</a>', 'wp-super-cache' ), 'http://wordpress.org/extend/plugins/wp-super-cache/faq/' ); ?></li>
+	<li><?php printf( __( '<a href="%1$s">Support Forum</a>', 'wp-super-cache' ), 'http://wordpress.org/tags/wp-super-cache' ); ?></li>
+	</ol>
 	<?php 
 	if ( isset( $wp_supercache_cache_list ) && $wp_supercache_cache_list ) { 
 		$start_date = get_option( 'wpsupercache_start' );
