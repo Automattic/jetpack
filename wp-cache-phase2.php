b794bb4efda5bbeae87c83dafdbf90631bd5eeb8
donncha <donncha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-21 13:45:57 +0000

    First paranoid check - check if the front page is being cached correctly
    git-svn-id: http://plugins.svn.wordpress.org/wp-super-cache/trunk@302647 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-cache-phase2.php
+++ wp-cache-phase2.php
@@ -304,7 +304,7 @@ function wp_cache_get_ob(&$buffer) {
 	global $cache_enabled, $cache_path, $cache_filename, $meta_file, $wp_start_time, $supercachedir;
 	global $new_cache, $wp_cache_meta, $file_expired, $blog_id, $cache_compression;
 	global $wp_cache_gzip_encoding, $super_cache_enabled, $cached_direct_pages;
-	global $wp_cache_404, $gzsize, $supercacheonly;
+	global $wp_cache_404, $gzsize, $supercacheonly, $wp_cache_paranoid_checks;
 	global $blog_cache_dir, $wp_cache_request_uri, $wp_supercache_cache_list;
 	global $wp_cache_not_logged_in, $wp_cache_object_cache, $cache_max_time;
 
@@ -532,9 +532,9 @@ function wp_cache_get_ob(&$buffer) {
 		}
 		if( $fr2 ) {
 			fclose($fr2);
-			if ( $cache_fname == $supercachedir . $home_url[ 'path' ] . 'index.html' && ( is_single() || is_category() || is_tag() ) ) {
+			if ( $wp_cache_paranoid_checks && $cache_fname == $supercachedir . $home_url[ 'path' ] . 'index.html' && ( is_single() || is_category() || is_tag() ) ) {
 				wp_cache_writers_exit();
-				if ( isset( $GLOBALS[ 'wp_super_cache_debug' ] ) && $GLOBALS[ 'wp_super_cache_debug' ] ) wp_cache_debug( "Warning! Not writing another page to front page cache.", 5 );
+				if ( isset( $GLOBALS[ 'wp_super_cache_debug' ] ) && $GLOBALS[ 'wp_super_cache_debug' ] ) wp_cache_debug( "Warning! Not writing another page to front page cache.", 1 );
 				return $buffer;
 			} elseif ( filesize( $tmp_cache_filename ) == 0 ) {
 				if ( isset( $GLOBALS[ 'wp_super_cache_debug' ] ) && $GLOBALS[ 'wp_super_cache_debug' ] ) wp_cache_debug( "Warning! The file $tmp_cache_filename was empty. Did not rename to {$cache_fname}", 5 );
