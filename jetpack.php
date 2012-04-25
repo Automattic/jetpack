f727bef571f4dfa5524b7d1a6cae9b0eb9dcd7d6
mdawaffe <mike@automattic.com>
2012-04-25 05:18:05 +0000

    Jetpack requires WP 3.2 as stated in the readme.

--- jetpack.php
+++ jetpack.php
@@ -14,7 +14,7 @@
 
 defined( 'JETPACK__API_BASE' ) or define( 'JETPACK__API_BASE', 'https://jetpack.wordpress.com/jetpack.' );
 define( 'JETPACK__API_VERSION', 1 );
-define( 'JETPACK__MINIMUM_WP_VERSION', '3.1' );
+define( 'JETPACK__MINIMUM_WP_VERSION', '3.2' );
 defined( 'JETPACK_CLIENT__AUTH_LOCATION' ) or define( 'JETPACK_CLIENT__AUTH_LOCATION', 'header' );
 defined( 'JETPACK_CLIENT__HTTPS' ) or define( 'JETPACK_CLIENT__HTTPS', 'AUTO' );
 define( 'JETPACK__VERSION', '1.3.1-alpha' );
