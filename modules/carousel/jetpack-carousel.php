8233e4b97f189c90e76086ea2a3b44fd50977601
stephdau <stephane@automattic.com>
2012-05-21 21:00:53 +0000

    Carousel:  * registering comment get ajax endpoints  * registering ajaxurl (using home_url() instead of admin_url() to be sure to stay on same domain) and nonce in localized script  * switching existing ajax calls (likes, reblog posts) from harcoded paths to admin-ajax from / because we're not gauaranteed to be running from hostname root in self-hosted context  * adding extra meta data to image for later use in gallery display (exif, geo, etc).

--- modules/carousel/jetpack-carousel.php
+++ modules/carousel/jetpack-carousel.php
@@ -29,10 +29,15 @@ class Jetpack_Carousel {
 			return;
 
 		if ( ! is_admin() ) {
+			// If on front-end, do the Carousel thang.
 			$this->prebuilt_widths = apply_filters( 'jp_carousel_widths', $this->prebuilt_widths );
 			add_filter( 'post_gallery', array( $this, 'enqueue_assets' ), 1000, 2 ); // load later than other callbacks hooked it
 			add_filter( 'gallery_style', array( $this, 'add_data_to_container' ) );
 			add_filter( 'wp_get_attachment_link', array( $this, 'add_data_to_images' ), 10, 2 );
+		} else {
+			// If in admin, register the ajax endpoints.
+			add_action( 'wp_ajax_get_attachment_comments', array( $this, 'get_attachment_comments' ) );
+			add_action( 'wp_ajax_nopriv_get_attachment_comments', array( $this, 'get_attachment_comments' ) );
 		}
 	}
 
@@ -57,9 +62,13 @@ class Jetpack_Carousel {
 		if ( $this->first_run ) {
 			wp_enqueue_script( 'jetpack-carousel', plugins_url( 'jetpack-carousel.js', __FILE__ ), array( 'jquery' ), $this->asset_version( '20120517' ), true );
 
+			// Note: using  home_url() instead of admin_url() for ajaxurl to be sure  to get same domain on wpcom when using mapped domains (also works on self-hosted)
+			// Also: not hardcoding path since there is no guarantee site is running on site root in self-hosted context.
 			$localize_strings = array(
 				'widths'       => $this->prebuilt_widths,
 				'is_logged_in' => is_user_logged_in(),
+				'ajaxurl'      => home_url( '/wp-admin/admin-ajax.php', is_ssl() ? 'https' : 'http' ),
+				'nonce'        => wp_create_nonce( 'carousel_nonce' ),
 			);
 			$localize_strings = apply_filters( 'jp_carousel_localize_strings', $localize_strings );
 			wp_localize_script( 'jetpack-carousel', 'jetpackCarouselStrings', $localize_strings );
@@ -78,10 +87,11 @@ class Jetpack_Carousel {
 			return $html;
 
 		$attachment_id = intval( $attachment_id );
-		$meta = wp_get_attachment_metadata( $attachment_id );
-		$size = isset( $meta['width'] ) ? "{$meta['width']},{$meta['height']}" : '';
+		$meta          = wp_get_attachment_metadata( $attachment_id );
+		$size          = isset( $meta['width'] ) ? "{$meta['width']},{$meta['height']}" : '';
+		$img_meta      = esc_attr( json_encode( $meta['image_meta'] ) );
 
-		$html = str_replace( '<img ', "<img data-attachment-id='$attachment_id' data-orig-size='$size' ", $html );
+		$html = str_replace( '<img ', "<img data-attachment-id='$attachment_id' data-orig-size='$size' data-image-meta='$img_meta' ", $html );
 
 		$html = apply_filters( 'jp_carousel_add_data_to_images', $html, $attachment_id );
 
@@ -102,6 +112,38 @@ class Jetpack_Carousel {
 		}
 
 		return $html;
+	}
+	
+	function get_attachment_comments() {
+		header('Content-type: text/javascript');
+		$attachment_id = ( isset( $_REQUEST['id'] ) ) ? (int) $_REQUEST['id'] : 0;
+		$offset        = ( isset( $_REQUEST['offset'] ) ) ? (int) $_REQUEST['offset'] : 0;
+		if ( ! $attachment_id ) {
+			echo json_encode( 'Whoops, missing attachment ID.' );
+			die();
+		}
+		if ( $offset < 1 )
+			$offset = 0;
+		$comments = get_comments( array(
+			'status'  => 'approve',
+			'number'  => 10,
+			'post_id' => $attachment_id,
+			'offset'  => $offset,
+		) );
+		$out      = array();
+		// Can't just send the results, they contain the commenter's email address.
+		foreach ( $comments as $comment ) {
+			$author_markup   = '<a href="' . esc_url( $comment->comment_author_url ) . '">' . esc_html( $comment->comment_author ) . '</a>';
+			$out[] = array(
+				'id'              => $comment->comment_ID,
+				'parent_id'       => $comment->comment_parent,
+				'author_markup'   => $author_markup,
+				'gravatar_markup' => get_avatar( $comment->comment_author_email, 64 ),
+				'date_gmt'        => $comment->comment_date_gmt,
+				'content'         => $comment->comment_content,
+			);
+		}
+		die( json_encode( $out ) );
 	}
 }
 
