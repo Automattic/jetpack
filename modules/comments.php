5af1cd3da1dfc286211fd04eca7c07f8b28869f0
mdawaffe <mike@automattic.com>
2012-06-08 19:45:39 +0000

    Comments: Add configuration link

--- modules/comments.php
+++ modules/comments.php
@@ -12,3 +12,15 @@ require dirname( __FILE__ ) . '/comments/comments.php';
 if ( is_admin() ) {
 	require dirname( __FILE__ ) . '/comments/admin.php';
 }
+
+function jetpack_comments_load() {
+	Jetpack::enable_module_configurable( __FILE__ );
+	Jetpack::module_configuration_load( __FILE__, 'jetpack_comments_configuration_load' );
+}
+
+function jetpack_comments_configuration_load() {
+	wp_safe_redirect( admin_url( 'options-discussion.php#jetpack-comments-settings' ) );
+	exit;
+}
+
+add_action( 'jetpack_modules_loaded', 'jetpack_comments_load' );
