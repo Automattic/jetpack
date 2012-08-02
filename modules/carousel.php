1bcd0af4330786b1b438d70bd54d30898317f538
beaulebens <beau@automattic.com>
2012-08-02 13:05:21 +0000

    Fix typo in module description.

--- modules/carousel.php
+++ modules/carousel.php
@@ -2,7 +2,7 @@
 
 /**
  * Module Name: Carousel
- * Module Description: Transform your standard image galleries into an immersize full-screen experience.
+ * Module Description: Transform your standard image galleries into an immersive full-screen experience.
  * Sort Order: 4
  * First Introduced: 1.5
  */
