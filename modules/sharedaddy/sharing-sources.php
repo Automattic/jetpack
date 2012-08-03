deeab543992601d5eafd9d7bf31e2ab1357fddf1
mdawaffe <mike@automattic.com>
2012-08-03 02:42:55 +0000

    Fatal error: fix require path

--- modules/sharedaddy/sharing-sources.php
+++ modules/sharedaddy/sharing-sources.php
@@ -637,14 +637,11 @@ class Share_Facebook extends Sharing_Source {
 		}
 
 		if ( !class_exists( 'GP_Locales' ) ) {
-			require ABSPATH . 'glotpress.dir/gp/locales/locales.php';
+			require JETPACK__PLUGIN_DIR . 'locales.php';
 		}
 
-		// WP.com: get_locale() returns 'it'
-		$locale = GP_Locales::by_slug( $lang );
-
 		// Jetpack: get_locale() returns 'it_IT';
-		// $locale = GP_Locales::by_field( 'wp_locale', $lang );
+		$locale = GP_Locales::by_field( 'wp_locale', $lang );
 
 		if ( !$locale || empty( $locale->facebook_locale ) ) {
 			return false;
