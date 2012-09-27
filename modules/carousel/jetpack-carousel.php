dd44326fef47231f9e8fa5fde667f1d6bdef05fc
mdawaffe <mike@automattic.com>
2012-09-27 23:16:01 +0000

    Carousel: Bump JS version.

--- modules/carousel/jetpack-carousel.php
+++ modules/carousel/jetpack-carousel.php
@@ -93,7 +93,7 @@ class Jetpack_Carousel {
 				wp_register_script( 'jquery.spin', plugins_url( 'jquery.spin.js', __FILE__ ) , array( 'jquery', 'spin' ) );
 			}
 
-			wp_enqueue_script( 'jetpack-carousel', plugins_url( 'jetpack-carousel.js', __FILE__ ), array( 'jquery.spin' ), $this->asset_version( '20120629' ), true );
+			wp_enqueue_script( 'jetpack-carousel', plugins_url( 'jetpack-carousel.js', __FILE__ ), array( 'jquery.spin' ), $this->asset_version( '20120926' ), true );
 
 			// Note: using  home_url() instead of admin_url() for ajaxurl to be sure  to get same domain on wpcom when using mapped domains (also works on self-hosted)
 			// Also: not hardcoding path since there is no guarantee site is running on site root in self-hosted context.
