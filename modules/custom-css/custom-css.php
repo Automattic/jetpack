1f818c453d210aeac7f091bc746108605643ce62
cfinke <finke@automattic.com>
2012-09-28 03:31:30 +0000

    Custom CSS: Moving formatting and comment wrapper of the default Custom CSS instructions out of the translators duties.

--- modules/custom-css/custom-css.php
+++ modules/custom-css/custom-css.php
@@ -402,12 +402,19 @@ function safecss() {
 	$css = str_replace( array( '\\\00BB \\\0020', '\0BB \020', '0BB 020' ), '\00BB \0020', $css );
 
 	if ( empty( $css ) ) {
-		$css = _e( apply_filters( 'safecss_default_css', '/* Welcome to Custom CSS!
-
-CSS (Cascading Style Sheets) is a kind of code that tells the browser how to render a web page. You may delete these comments and get started with your customizations.
-
-By default, your stylesheet will be loaded after the theme stylesheets, which means that your rules can take precedence and override the theme CSS rules. Just write here what you want to change, you don\'t need to copy all your theme\'s stylesheet content.
-*/' ), 'jetpack' );
+		$css = "/*\n"
+			. wordwrap(
+				__(
+					apply_filters(
+						'safecss_default_css',
+						"Welcome to Custom CSS!" .
+							"\n\nCSS (Cascading Style Sheets) is a kind of code that tells the browser how to render a web page. You may delete these comments and get started with your customizations." .
+							"\n\nBy default, your stylesheet will be loaded after the theme stylesheets, which means that your rules can take precedence and override the theme CSS rules. Just write here what you want to change, you don't need to copy all your theme's stylesheet content."
+					),
+					'jetpack'
+				)
+			)
+			. "\n*/";
 	}
 
 	$css = apply_filters( 'safecss_css', $css );
