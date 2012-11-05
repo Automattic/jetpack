819ce2c0d549052953e85483e19992a810725da6
ethitter <erick@automattic.com>
2012-11-05 20:08:07 +0000

    Correct logic error in r621369 that would show the "supported theme" Infinite Scroll module text to users with unsupported themes.

--- modules/module-info.php
+++ modules/module-info.php
@@ -629,7 +629,7 @@ function jetpack_infinite_scroll_more_info() {
 
 	<h4><?php esc_html_e( 'Infinite Scroll', 'jetpack' ); ?></h4>
 
-	<?php if ( ! Jetpack::is_active() || current_theme_supports( 'infinite-scroll' ) ) : ?>
+	<?php if ( ! Jetpack::is_active() || ( Jetpack::is_active() && current_theme_supports( 'infinite-scroll' ) ) ) : ?>
 		<p><?php esc_html_e( 'When you write great content, all you really want is people to find it, right?', 'jetpack' ); ?></p>
 
 		<p><?php esc_html_e( "With the Infinite Scroll module and a supported theme, that's exactly what happens. Instead of the old way of navigating down a page by scrolling and then clicking a link to get to the next page, waiting for a page refresh&mdash;the document model of the web&mdash;infinite scrolling pulls the next set of posts automatically into view when the reader approaches the bottom of the page, more like an application.", 'jetpack' ); ?></p>
