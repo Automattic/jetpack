82b55837088d04f6236ce87d9b765ac4e88db2a1
cfinke <finke@automattic.com>
2012-12-27 23:28:50 +0000

    Tiled Galleries: Compatibility with Infinite Scroll.
    After the post-load event, re-run the JavaScript that makes tiled galleries flexible.

--- modules/tiled-gallery/tiled-gallery/tiled-gallery.js
+++ modules/tiled-gallery/tiled-gallery/tiled-gallery.js
@@ -3,11 +3,8 @@
 var TiledGallery = function() {
 	this.resizeTimeout = null;
 
-	this.gallery = $( '.tiled-gallery' );
-	this.item    = this.gallery.find( '.tiled-gallery-item' );
-	this.caption = this.gallery.find( '.tiled-gallery-caption' );
+	this.populate();
 
-	this.Captions();
 	var self = this;
 
 	$( window ).on( 'resize', function () {
@@ -16,9 +13,29 @@ var TiledGallery = function() {
 		self.resizeTimeout = setTimeout( function () { self.resize(); }, 150 );
 	} );
 
+    // Make any new galleries loaded by Infinite Scroll flexible
+    $( 'body' ).on( 'post-load', function () {
+		self.populate();
+
+		// After each image load, run resize in case all images in the gallery are loaded.
+		self.gallery.find( 'img' ).off( 'load.tiled-gallery' ).on( 'load.tiled-gallery', function () {
+			self.resize();
+		} );
+
+		// Run resize now in case all images loaded from cache.
+		self.resize();
+    } );
+
 	this.resize();
 };
 
+TiledGallery.prototype.populate = function() {
+	this.gallery = $( '.tiled-gallery' );
+	this.item    = this.gallery.find( '.tiled-gallery-item' );
+	this.caption = this.gallery.find( '.tiled-gallery-caption' );
+
+	this.Captions();
+};
 
 /**
  * Story
