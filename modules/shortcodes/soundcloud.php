33697bed0555e527f115e27c725aa736ad83155a
stephdau <stephane@automattic.com>
2013-02-11 18:00:42 +0000

    Jetpack: Shorcodes: SoundCloud: allow for Flash based player in user-defined options.

--- modules/shortcodes/soundcloud.php
+++ modules/shortcodes/soundcloud.php
@@ -95,16 +95,12 @@ function soundcloud_shortcode($atts, $content = null) {
   }
   if (isset($options['height']) && !preg_match('/^\d+$/', $options['height'])) { unset($options['height']); }
 
-  // A8C: default to iframe
-  $iframe = true;
-/*
   // The "iframe" option must be true to load the iframe widget
   $iframe = soundcloud_booleanize($options['iframe'])
     // Default to flash widget for permalink urls (e.g. http://soundcloud.com/{username})
     // because HTML5 widget doesn’t support those yet
     ? preg_match('/api.soundcloud.com/i', $options['url'])
     : false;
- */
 
   // Return html embed code
   if ($iframe) {
