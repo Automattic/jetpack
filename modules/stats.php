fa6883be1750f23e93b3837fc4b387ba5a590f0a
skeltoac <andy@automattic.com>
2013-02-12 21:50:47 +0000

    Jetpack Stats: sync stats_options to make roles available to server

--- modules/stats.php
+++ modules/stats.php
@@ -10,13 +10,14 @@ if ( defined( 'STATS_VERSION' ) ) {
 	return;
 }
 
-define( 'STATS_VERSION', '7' );
+define( 'STATS_VERSION', '8' );
 defined( 'STATS_DASHBOARD_SERVER' ) or define( 'STATS_DASHBOARD_SERVER', 'dashboard.wordpress.com' );
 
 add_action( 'jetpack_modules_loaded', 'stats_load' );
 
 // Tell HQ about changed settings
 Jetpack_Sync::sync_options( __FILE__,
+	'stats_options',
 	'home',
 	'siteurl',
 	'blogname',
