a939cac4864ec320320fc0234339aae87d56cb34
justinshreve <justin@automattic.com>
2013-02-22 21:13:45 +0000

    Jetpack Mobile Theme Use a filter to modify the output of wp_title().

--- modules/minileven/theme/pub/minileven/inc/tweaks.php
+++ modules/minileven/theme/pub/minileven/inc/tweaks.php
@@ -65,3 +65,30 @@ function minileven_body_classes( $classes ) {
 	return $classes;
 }
 add_filter( 'body_class', 'minileven_body_classes' );
+
+/**
+ * Filters wp_title to print a neat <title> tag based on what is being viewed.
+ *
+ * @since Minileven 2.0
+ */
+function minileven_wp_title( $title, $sep ) {
+	global $page, $paged;
+
+	if ( is_feed() )
+		return $title;
+
+	// Add the blog name
+	$title .= get_bloginfo( 'name' );
+
+	// Add the blog description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		$title .= " $sep $site_description";
+
+	// Add a page number if necessary:
+	if ( $paged >= 2 || $page >= 2 )
+		$title .= " $sep " . sprintf( __( 'Page %s', 'jetpack' ), max( $paged, $page ) );
+
+	return $title;
+}
+add_filter( 'wp_title', 'minileven_wp_title', 10, 2 );
