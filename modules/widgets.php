743403fae3657567990d61a3bfc4454cfbfa4d99
tmoorewp <tim@automattic.com>
2013-03-14 17:14:30 +0000

    Filter Jetpack's widgets so that a plugin can control which widgets get loaded.

--- modules/widgets.php
+++ modules/widgets.php
@@ -8,8 +8,16 @@
  */
 
 function jetpack_load_widgets() {
+	$widgets_include = array();
+	
 	foreach ( Jetpack::glob_php( dirname( __FILE__ ) . '/widgets' ) as $file ) {
-		include $file;
+		$widgets_include[] = $file;
+	}
+	
+	$widgets_include = apply_filters( 'jetpack_widgets_to_include', $widgets_include );
+	
+	foreach( $widgets_include as $include ) {
+		include $include;
 	}
 }
 
