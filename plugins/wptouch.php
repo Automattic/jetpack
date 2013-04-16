4d8e7926f29de2b070915a8c1b0a79562f3b7196
donncha <donncha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-16 11:48:42 +0000

    Minor change to text output
    git-svn-id: http://plugins.svn.wordpress.org/wp-super-cache/trunk@698483 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugins/wptouch.php
+++ plugins/wptouch.php
@@ -21,7 +21,7 @@ function wp_super_cache_wptouch_admin() {
 		<form name="wp_manager" action="" method="post">
 		<label><input type="radio" name="cache_wptouch" value="1" <?php if( $cache_wptouch ) { echo 'checked="checked" '; } ?>/> <?php _e( 'Enabled', 'wp-super-cache' ); ?></label>
 		<label><input type="radio" name="cache_wptouch" value="0" <?php if( !$cache_wptouch ) { echo 'checked="checked" '; } ?>/> <?php _e( 'Disabled', 'wp-super-cache' ); ?></label>
-		<p><?php _e( '', 'wp-super-cache' ); ?></p><?php
+		<?php
 		echo '<p>' . __( 'Provides support for <a href="http://wordpress.org/extend/plugins/wptouch/">WPTouch</a> mobile theme and plugin.', 'wp-super-cache' ) . '</p>';
 		if ( isset( $changed ) && $changed ) {
 			if ( $cache_wptouch )
