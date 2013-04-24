48c1674e1c47c0da3a558b6fbdbfd99eac1e813f
lezama <miguel@automattic.com>
2013-04-24 14:00:18 +0000

    Jetpack: Contact Form: prepends a 'g' to every field id in order to generate W3C compliant ids

--- modules/contact-form/grunion-contact-form.php
+++ modules/contact-form/grunion-contact-form.php
@@ -1206,12 +1206,12 @@ class Grunion_Contact_Form_Field extends Crunion_Contact_Form_Shortcode {
 			$unescaped_label = preg_replace( '/[^a-zA-Z0-9.-_:]/', '', $unescaped_label );
 
 			if ( empty( $id ) ) {
-				$id = sanitize_title_with_dashes( 'g' + $form_id . '-' . $unescaped_label );
+				$id = sanitize_title_with_dashes( 'g' . $form_id . '-' . $unescaped_label );
 				$i = 0;
 				$max_tries = 24;
 				while ( isset( $form->fields[$id] ) ) {
 					$i++;
-					$id = sanitize_title_with_dashes( 'g' + $form_id . '-' . $unescaped_label . '-' . $i );
+					$id = sanitize_title_with_dashes( 'g' . $form_id . '-' . $unescaped_label . '-' . $i );
 
 					if ( $i > $max_tries ) {
 						break;
