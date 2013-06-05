051e8644a6121cb23c4c6070662618ef55592312
lezama <miguel@automattic.com>
2013-06-05 18:19:42 +0000

    Jetpack: Minileven: Revert add textdomain. Already textdomained to minileven.

--- modules/minileven/theme/pub/minileven/functions.php
+++ modules/minileven/theme/pub/minileven/functions.php
@@ -88,13 +88,13 @@ function minileven_fonts() {
 	/*	translators: If there are characters in your language that are not supported
 		by Open Sans, translate this to 'off'. Do not translate into your own language. */
 
-	if ( 'off' !== _x( 'on', 'Open Sans font: on or off', 'minileven' , 'jetpack') ) {
+	if ( 'off' !== _x( 'on', 'Open Sans font: on or off', 'minileven' ) ) {
 
 		$opensans_subsets = 'latin,latin-ext';
 
 		/* translators: To add an additional Open Sans character subset specific to your language, translate
 		this to 'greek', 'cyrillic' or 'vietnamese'. Do not translate into your own language. */
-		$opensans_subset = _x( 'no-subset', 'Open Sans font: add new subset (greek, cyrillic, vietnamese)', 'minileven' , 'jetpack');
+		$opensans_subset = _x( 'no-subset', 'Open Sans font: add new subset (greek, cyrillic, vietnamese)', 'minileven' );
 
 		if ( 'cyrillic' == $opensans_subset )
 			$opensans_subsets .= ',cyrillic,cyrillic-ext';
