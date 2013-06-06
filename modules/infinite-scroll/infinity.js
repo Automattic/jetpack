df46bff67c1d79cbeac30cfbde143b444bedf7b4
georgestephanis <georgestephanis@automattic.com>
2013-06-06 16:34:51 +0000

    Removing the reference to a wpcom ticket from the wporg plugin.

--- modules/infinite-scroll/infinity.js
+++ modules/infinite-scroll/infinity.js
@@ -84,7 +84,6 @@ Scroller.prototype.check = function() {
 	var bottom = this.window.scrollTop() + this.window.height(),
 		threshold = this.element.offset().top + this.element.outerHeight(false) - this.window.height();
 
-	// Trigger IS sooner to reduce the likelihood that viewers see the loading spinner. See #5403-wpcom.
 	threshold = Math.round( threshold * 0.75 );
 
 	return bottom > threshold;
