40568912446030981520294454a285711aa6cb9e
georgestephanis <georgestephanis@automattic.com>
2013-06-20 00:32:25 +0000

    Jetpack: Jetpack Options: Update the class to call self:: rather than Jetpack::

--- class.jetpack-options.php
+++ class.jetpack-options.php
@@ -26,7 +26,7 @@ class Jetpack_Options {
 			'version',                      // (string) Used during upgrade procedure to auto-activate new modules. version:time
 			'old_version',                  // (string) Used to determine which modules are the most recently added. previous_version:time
 			'fallback_no_verify_ssl_certs', // (int)    Flag for determining if this host must skip SSL Certificate verification due to misconfigured SSL.
-			'time_diff',                    // (int)    Offset between Jetpack server's clocks and this server's clocks. Jetpack Server Time = time() + (int) Jetpack::get_option( 'time_diff' )
+			'time_diff',                    // (int)    Offset between Jetpack server's clocks and this server's clocks. Jetpack Server Time = time() + (int) Jetpack_Options::get_option( 'time_diff' )
 			'public',                       // (int|bool) If we think this site is public or not (1, 0), false if we haven't yet tried to figure it out.
 			'is_network_site',              // (int|bool) If we think this site is a network or a single blog (1, 0), false if we haven't yet tried to figue it out.
 			'social_links',                 // (array)    The specified links for each social networking site.
@@ -40,9 +40,9 @@ class Jetpack_Options {
 	 * @param mixed  $default (optional)
 	 */
 	public static function get_option( $name, $default = false ) {
-		if ( in_array( $name, Jetpack::get_option_names( 'non_compact' ) ) ) {
+		if ( in_array( $name, self::get_option_names( 'non_compact' ) ) ) {
 			return get_option( "jetpack_$name" );
-		} else if ( !in_array( $name, Jetpack::get_option_names() ) ) {
+		} else if ( !in_array( $name, self::get_option_names() ) ) {
 			trigger_error( sprintf( 'Invalid Jetpack option name: %s', $name ), E_USER_WARNING );
 			return false;
 		}
@@ -62,9 +62,9 @@ class Jetpack_Options {
 	 * @param mixed  $value Option value
 	 */
 	public static function update_option( $name, $value ) {
-		if ( in_array( $name, Jetpack::get_option_names( 'non_compact' ) ) ) {
+		if ( in_array( $name, self::get_option_names( 'non_compact' ) ) ) {
 			return update_option( "jetpack_$name", $value );
-		} else if ( !in_array( $name, Jetpack::get_option_names() ) ) {
+		} else if ( !in_array( $name, self::get_option_names() ) ) {
 			trigger_error( sprintf( 'Invalid Jetpack option name: %s', $name ), E_USER_WARNING );
 			return false;
 		}
@@ -87,12 +87,12 @@ class Jetpack_Options {
 	public static function update_options( $array ) {
 		$names = array_keys( $array );
 
-		foreach ( array_diff( $names, Jetpack::get_option_names(), Jetpack::get_option_names( 'non_compact' ) ) as $unknown_name ) {
+		foreach ( array_diff( $names, self::get_option_names(), self::get_option_names( 'non_compact' ) ) as $unknown_name ) {
 			trigger_error( sprintf( 'Invalid Jetpack option name: %s', $unknown_name ), E_USER_WARNING );
 			unset( $array[$unknown_name] );
 		}
 
-		foreach ( array_intersect( $names, Jetpack::get_option_names( 'non_compact' ) ) as $name ) {
+		foreach ( array_intersect( $names, self::get_option_names( 'non_compact' ) ) as $name ) {
 			update_option( "jetpack_$name", $array[$name] );
 			unset( $array[$name] );
 		}
@@ -114,11 +114,11 @@ class Jetpack_Options {
 	public static function delete_option( $names ) {
 		$names = (array) $names;
 
-		foreach ( array_diff( $names, Jetpack::get_option_names(), Jetpack::get_option_names( 'non_compact' ) ) as $unknown_name ) {
+		foreach ( array_diff( $names, self::get_option_names(), self::get_option_names( 'non_compact' ) ) as $unknown_name ) {
 			trigger_error( sprintf( 'Invalid Jetpack option name: %s', $unknown_name ), E_USER_WARNING );
 		}
 
-		foreach ( array_intersect( $names, Jetpack::get_option_names( 'non_compact' ) ) as $name ) {
+		foreach ( array_intersect( $names, self::get_option_names( 'non_compact' ) ) as $name ) {
 			delete_option( "jetpack_$name" );
 		}
 
@@ -127,7 +127,7 @@ class Jetpack_Options {
 			$options = array();
 		}
 
-		$to_delete = array_intersect( $names, Jetpack::get_option_names(), array_keys( $options ) );
+		$to_delete = array_intersect( $names, self::get_option_names(), array_keys( $options ) );
 		if ( $to_delete ) {
 			foreach ( $to_delete as $name ) {
 				unset( $options[$name] );
@@ -140,3 +140,4 @@ class Jetpack_Options {
 	}
 
 }
+
