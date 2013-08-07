ea90c876d99d027a292cce234ed64b93d9dc3170
tmoorewp <tim@automattic.com>
2013-08-07 14:51:00 +0000

    Add the Twitter Timeline shortcode.
    Fixes #1819

---
+++ modules/shortcodes/twitter-timeline.php
@@ -0,0 +1,31 @@
+<?php
+add_shortcode( 'twitter-timeline', 'twitter_timeline_shortcode' );
+
+function twitter_timeline_shortcode( $attr ) {
+
+	$default_atts = array(
+		'username'         => '',
+		'id'               => '',
+		'height'           => 282,
+		'width'            => 450,
+
+	);
+
+	$attr = shortcode_atts( $default_atts, $attr );
+
+	if ( $attr['username'] != preg_replace( '/[^A-Za-z0-9_]+/', '', $attr['username'] ) )
+		return '<!--' . __( 'Invalid username' ) . '-->';
+
+	if ( ! is_numeric( $attr['id'] ) )
+		return '<!--' . __( 'Invalid id' ) . '-->';
+
+	$tweets_by = sprintf( __( 'Tweets by @%s' ), $attr['username'] );
+	$output = '<a class="twitter-timeline" width="' . (int)$attr['width'] . '" height="' . (int)$attr['width'] . '" href="' . esc_url( 'https://twitter.com/'. $attr['username'] ) . '" data-widget-id="' . (int)$attr['id'] . '">' . esc_html( $tweets_by ) . '</a>';
+	add_action( 'wp_footer', 'twitter_timeline_js' );
+
+	return $output;
+}
+
+function twitter_timeline_js() {
+	echo '<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>';
+}
