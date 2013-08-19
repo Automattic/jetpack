3ca0bd8caf8bba11ee22932a483403a4759c52f6
georgestephanis <georgestephanis@automattic.com>
2013-08-19 14:45:36 +0000

    Jetpack: When calling Jetpack::get_content_width() if the global content_width isn't set, pass a zero to the filter instead.  Themes really should be setting this on their own, however.  Fixes #1878

--- class.jetpack.php
+++ class.jetpack.php
@@ -3537,7 +3537,8 @@ p {
 	 * Get $content_width, but with a <s>twist</s> filter.
 	 */
 	public static function get_content_width() {
-		return apply_filters( 'jetpack_content_width', $GLOBALS['content_width'] );
+		$content_width = isset( $GLOBALS['content_width'] ) ? $GLOBALS['content_width'] : 0;
+		return apply_filters( 'jetpack_content_width', $content_width );
 	}
 
 	/**
