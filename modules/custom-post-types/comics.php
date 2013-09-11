14afc1ee4eb7b0f11f7ee336da394160fce53000
blobaugh <blobaugh@automattic.com>
2013-09-11 20:15:39 +0000

    Jetpack: Custom Post Types: Added check from queue_publish_post function. Props tmoorewp

--- modules/custom-post-types/comics.php
+++ modules/custom-post-types/comics.php
@@ -45,7 +45,12 @@ class Jetpack_Comic {
 
 		add_filter( 'post_updated_messages', array( $this, 'updated_messages' ) );
 
-		add_action( 'publish_jetpack-comic', 'queue_publish_post', 10, 2 );
+		if ( function_exists( 'queue_publish_post' ) ) {
+			add_action( 'publish_jetpack-comic', 'queue_publish_post', 10, 2 );
+		} else {
+			add_action( 'publish_jetpack-comic', 'publish_post', 10, 2 );
+		}
+
 		add_action( 'pre_get_posts', array( $this, 'include_in_feeds' ) );
 
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
