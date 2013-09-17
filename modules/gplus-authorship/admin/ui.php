b5751091c0ca1f01a4232e1503e8a2b849df5c75
blobaugh <blobaugh@automattic.com>
2013-09-17 22:10:26 +0000

    Jetpack: GooglePlus Authorship : Added check for disconnected property existing before getting value to avoid a php notice in jetpack on disconnecting a google authorship connection

--- modules/gplus-authorship/admin/ui.php
+++ modules/gplus-authorship/admin/ui.php
@@ -134,7 +134,7 @@ class GPlus_Authorship_Admin {
 	private function disconnected_message() {
 		global $publicize;
 		if ( !empty( $_GET['disconnect'] ) && 'gplus' == $_GET['disconnect'] ) {
-			if ( $publicize->disconnected_from_authorship ) { ?>
+			if ( isset( $publicize->disconnected_from_authorship ) && $publicize->disconnected_from_authorship ) { ?>
 				<div class="updated"><p><?php _e( sprintf( "Your Google+ profile and WordPress.com accounts have been disconnected, including your Publicize connections. If you no longer wish to be associated with this blog on Google we recommend that you also remove the blog URL from your <a href='%s' target='_blank'>Google+ profile</a>.", 'http://plus.google.com/me/about/edit/co' ) ); ?></p></div><?php
 			} else { ?>
 				<div class="updated"><p><?php _e( sprintf( "Your Google+ profile and WordPress.com accounts have been disconnected. If you no longer wish to be associated with this blog on Google we recommend that you also remove the blog URL from your <a href='%s' target='_blank'>Google+ profile</a>.", 'http://plus.google.com/me/about/edit/co' ) ); ?></p></div><?php
