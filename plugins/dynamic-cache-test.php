990fbcf7ffe2fb4876a7cb50a631f6fae3744008
donncha <donncha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-21 14:13:46 +0000

    Added and output buffer example plugin
    git-svn-id: http://plugins.svn.wordpress.org/wp-super-cache/trunk@791220 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugins/dynamic-cache-test.php
+++ plugins/dynamic-cache-test.php
@@ -47,13 +47,54 @@
  * tag can be whatever you like.
  */
 
-
 /*
-
-define( 'DYNAMIC_CACHE_TEST_TAG', '' );
+define( 'DYNAMIC_CACHE_TEST_TAG', '' ); // CHANGE THIS!
+define( 'DYNAMIC_OUTPUT_BUFFER_TAG', '' ); // CHANGE THIS!
 
 if ( DYNAMIC_CACHE_TEST_TAG == '' )
 	return false;
+
+// To use an output buffer when generating the dynamic section of your web page
+// you must generate the HTML/JS/CSS and store it before the page is completed.
+// That stored text is used by the wpsc_cachedata filter later when the same
+// function is called again.
+// You must also create a safety function. This function checks that your newly
+// cached page is ready to be processed by the wpsc_cachedata filter. It's not
+// required for already cached pages.
+// See dynamic_output_buffer_test_safety() for an example. You must add this 
+// to avoid the following error:
+// "PHP Fatal error:  ob_start(): Cannot use output buffering in output buffering display handlers in..."
+//
+// Steps to run example plugin:
+// 1. Add the DYNAMIC_OUTPUT_BUFFER_TAG text (as defined above) to your theme where the dynamic content should be.
+// 2. Call dynamic_output_buffer_test() from your theme or an action like wp_footer
+// 3. Clear all cached files.
+
+function dynamic_output_buffer_test( &$cachedata = 0 ) {
+	if ( defined( 'DYNAMIC_OB_TEXT' ) )
+		return str_replace( DYNAMIC_OUTPUT_BUFFER_TAG, DYNAMIC_OB_TEXT, $cachedata );
+
+	ob_start();
+	// call the sidebar function, do something dynamic
+	echo "<p>This is a test. The current time on the server is: " . date( 'H:i:s' ) . "</p>";
+	$text = ob_get_contents();
+	ob_end_clean();
+
+	if ( $cachedata === 0 ) // called directly from the theme so store the output
+		define( 'DYNAMIC_OB_TEXT', $text );
+	else // called via the wpsc_cachedata filter. We only get here in cached pages in wp-cache-phase1.php
+		return str_replace( DYNAMIC_OUTPUT_BUFFER_TAG, $text, $cachedata );
+
+}
+add_cacheaction( 'wpsc_cachedata', 'dynamic_output_buffer_test' );
+
+function dynamic_output_buffer_test_safety( $safety ) {
+	if ( defined( 'DYNAMIC_OB_TEXT' ) )
+		return 1; // ready to replace tag with dynamic content.
+	else
+		return 0; // tag cannot be replaced.
+}
+add_cacheaction( 'wpsc_cachedata_safety', 'dynamic_output_buffer_test_safety' );
 
 function dynamic_cache_test_filter( &$cachedata) {
 	return str_replace( DYNAMIC_CACHE_TEST_TAG, "<!-- Hello world at " . date( 'H:i:s' ) . " -->", $cachedata );
@@ -68,7 +109,5 @@ function dynamic_cache_test_init() {
 	add_action( 'wp_footer', 'dynamic_cache_test_template_tag' );
 }
 add_cacheaction( 'add_cacheaction', 'dynamic_cache_test_init' );
-
 */
-
 ?>
