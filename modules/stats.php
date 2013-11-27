f7328305a3f40a5c1cf417c1fc29819346e18af3
georgestephanis <georgestephanis@automattic.com>
2013-11-27 17:44:33 +0000

    Jetpack: Stats: Correct a url being mangled `if_ssl()` due to awkwardness between ternaries and appending strings.  Fixes #2061

--- modules/stats.php
+++ modules/stats.php
@@ -351,7 +351,7 @@ function stats_reports_page() {
 }
 </style>
 <div id="stats-loading-wrap" class="wrap">
-<p class="hide-if-no-js"><img class="wpcom-loading-64" alt="<?php esc_attr_e( 'Loading&hellip;', 'jetpack' ); ?>" src="<?php echo esc_url( apply_filters( 'jetpack_static_url', is_ssl() ? 'https' : 'http' . '://en.wordpress.com/i/loading/loading-64.gif' ) ); ?>" /></p>
+<p class="hide-if-no-js"><img class="wpcom-loading-64" alt="<?php esc_attr_e( 'Loading&hellip;', 'jetpack' ); ?>" src="<?php echo esc_url( apply_filters( 'jetpack_static_url', "{$http}://en.wordpress.com/i/loading/loading-64.gif" ) ); ?>" /></p>
 <p class="hide-if-js"><?php esc_html_e( 'Your Site Stats work better with Javascript enabled.', 'jetpack' ); ?><br />
 <a href="<?php echo esc_url( $nojs_url ); ?>"><?php esc_html_e( 'View Site Stats without Javascript', 'jetpack' ); ?></a>.</p>
 </div>
