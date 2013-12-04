9b9b2eb3a21ecf13e1e1b956003384e9afbefc6a
georgestephanis <georgestephanis@automattic.com>
2013-12-04 20:48:04 +0000

    Jetpack: Shortcodes: Facebook Embeds: Register alternate permalink.php URL for posts. props @iandunn

--- modules/shortcodes/facebook.php
+++ modules/shortcodes/facebook.php
@@ -5,10 +5,15 @@
  */
 
 define( 'JETPACK_FACEBOOK_EMBED_REGEX', '#^https?://(www.)?facebook\.com/([^/]+)/posts/([^/]+)?#' );
+define( 'JETPACK_FACEBOOK_ALTERNATE_EMBED_REGEX', '#^https?://(www.)?facebook\.com/permalink.php\?([^\s]+)#' );
 define( 'JETPACK_FACEBOOK_PHOTO_EMBED_REGEX', '#^https?://(www.)?facebook\.com/photo.php\?([^\s]+)#' );
 
 // Example URL: https://www.facebook.com/VenusWilliams/posts/10151647007373076 
 wp_embed_register_handler( 'facebook', JETPACK_FACEBOOK_EMBED_REGEX, 'jetpack_facebook_embed_handler' );
+
+// Example URL: https://www.facebook.com/permalink.php?id=222622504529111&story_fbid=559431180743788
+wp_embed_register_handler( 'facebook-alternate', JETPACK_FACEBOOK_ALTERNATE_EMBED_REGEX, 'jetpack_facebook_embed_handler' );
+
 // Photos are handled on a different endpoint; e.g. https://www.facebook.com/photo.php?fbid=10151609960150073&set=a.398410140072.163165.106666030072&type=1 
 wp_embed_register_handler( 'facebook-photo', JETPACK_FACEBOOK_PHOTO_EMBED_REGEX, 'jetpack_facebook_embed_handler' );
 
