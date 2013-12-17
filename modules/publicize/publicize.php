8ed18b1eef0c2b9dfdbaab0d8efdfec05ce20b1c
George Stephanis <george@stephanis.info>
2013-12-17 11:49:57 -0500

    Tidy up new filter for disabling publicize on a post.
    Follow-up to 4b741e2e702a2fb8ba14afa66a0f121e78ef3391
    
    Whitespace fixes, renaming the filter to something that makes it a bit
    more understandable, and pass the post’s data across with it.
    
    Also converting to Core’s new recommended brace syntax.

--- modules/publicize/publicize.php
+++ modules/publicize/publicize.php
@@ -276,10 +276,11 @@ abstract class Publicize_Base {
 		// don't publish password protected posts
 		if ( '' !== $post->post_password )
 			$submit_post = false;
-			
+
 		// allow plugins to avoid Publicize
-		if ( ! apply_filters( 'jetpack_no_publicize_post', true ) )
+		if ( ! apply_filters( 'jetpack_should_publicize_post', true, $post_id, $post ) ) {
 			$submit_post = false;
+		}
 
 		// Did this request happen via wp-admin?
 		$from_web = 'post' == strtolower( $_SERVER['REQUEST_METHOD'] ) && isset( $_POST[$this->ADMIN_PAGE] );
