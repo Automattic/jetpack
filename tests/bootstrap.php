424376d326a53ea3cdc5d18f40783bfe20f43eac
Ben Lobaugh <ben@lobaugh.net>
2013-12-31 16:47:43 -0800

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,7 +9,7 @@
 
 // Activates this plugin in WordPress so it can be tested.
 $GLOBALS['wp_tests_options'] = array(
-	'active_plugins' => array( 'jetpack/jetpack.php' ),
+	'active_plugins' => array( 'Jetpack/jetpack.php' ),
 );
 
 // If the develop repo location is defined (as WP_DEVELOP_DIR), use that
