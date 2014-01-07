f01c15e8b5b79c464f8b1f5c1f3936cabea24070
blobaugh <ben@lobaugh.net>
2014-01-06 23:08:01 -0800

    Jetpack: Multisite: Adding documentation

--- tests/test_class.jetpack-network.php
+++ tests/test_class.jetpack-network.php
@@ -35,4 +35,35 @@ class WP_Test_Jetpack_Network extends WP_UnitTestCase {
 
 		$this->assertInternalType( 'array', $jpms->list_modules() );
     }
+
+    /**
+     * @since 2.8
+     **/
+    public function test_set_auto_activated_modules_returns_array() {
+		$jpms = Jetpack_Network::init();
+
+		$this->assertInternalType( 'array', $jpms->set_auto_activated_modules( array() );
+    }
+
+    /**
+     * @since 2.8
+     **/
+    public function test_add_jetpack_sites_column_adds_column() {
+		$jpms = Jetpack_Network::init();
+
+		$columns = $jpms->add_jetpack_sites_column( array() );
+
+		$this->assertTrue( isset( $columns['jetpack_connection'] ) );
+    }
+
+    /**
+     * @since 2.8
+     **/
+    public function test_body_class_contains_network_admin() {
+		$jpms = Jetpack_Network::init();
+
+		$classes = $jpms->body_class( array() );
+
+		$this->assertTrue( in_array( 'network-admin', $classes ) );
+    }
 } // end class
