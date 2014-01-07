31dac40b43ed32c62d98a5d948bd6ea585caefbc
blobaugh <ben@lobaugh.net>
2014-01-06 17:56:06 -0800

    Jetpack: Multisite: Updating tests

--- tests/test_class.jetpack-network.php
+++ tests/test_class.jetpack-network.php
@@ -1,23 +1,21 @@
 <?php
 
-require_once( '../jetpack.php' );
-
 class WP_Test_Jetpack_Network extends WP_UnitTestCase {
 
     /**
      * @since 2.5
      */
     public function test_jetpack_network_init_returns_jetpack_network() {
-	$this->assertInstanceOf( 'Jetpack_Network', Jetpack_Network::init() );
+		$this->assertInstanceOf( 'Jetpack_Network', Jetpack_Network::init() );
     }
 
     /**
      * @since 2.5
      */
     public function test_get_url_returns_string_on_valid_input() {
-	$jpms = new Jetpack_Network::init();
+		$jpms = new Jetpack_Network::init();
 
-	$this->assertInternalType( 'string', $jpms->get_url( 'network_admin_page' ) );
+		$this->assertInternalType( 'string', $jpms->get_url( 'network_admin_page' ) );
     }
 
     /**
@@ -33,8 +31,8 @@ class WP_Test_Jetpack_Network extends WP_UnitTestCase {
      * @since 2.5
      */
     public function test_list_modules_returns_array() {
-	$jpms = Jetpack_Network::init();
+		$jpms = Jetpack_Network::init();
 
-	$this->assertInternalType( 'array', $jpms->list_modules() );
+		$this->assertInternalType( 'array', $jpms->list_modules() );
     }
 } // end class
