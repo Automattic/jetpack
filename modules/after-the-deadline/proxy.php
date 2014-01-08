3f6acc3e6d3fcdd505dadedb8119efba689ad2a1
Jeremy Herve <jeremy@tagada.hu>
2014-01-08 10:03:46 +0000

    After the Deadline: Add support for TinyMCE 4.0 Props @azaozz
    See #38

--- modules/after-the-deadline/proxy.php
+++ modules/after-the-deadline/proxy.php
@@ -39,12 +39,12 @@ function AtD_http_post( $request, $host, $path, $port = 80 ) {
  *  This function is called as an action handler to admin-ajax.php
  */
 function AtD_redirect_call() {
-        if ( $_SERVER['REQUEST_METHOD'] === 'POST' )
-                $postText = trim(  file_get_contents( 'php://input' )  );
+	if ( $_SERVER['REQUEST_METHOD'] === 'POST' )
+		$postText = trim(  file_get_contents( 'php://input' )  );
 
-        $url = $_GET['url'];
-
+	$url = $_GET['url'];
 	$service = apply_filters( 'atd_service_domain', 'service.afterthedeadline.com' );
+
 	if ( defined('WPLANG') ) {
 		if ( strpos(WPLANG, 'pt') !== false )
 			$service = 'pt.service.afterthedeadline.com';
@@ -55,15 +55,16 @@ function AtD_redirect_call() {
 		else if ( strpos(WPLANG, 'fr') !== false )
 			$service = 'fr.service.afterthedeadline.com';
 	}
+
 	$user = wp_get_current_user();
 	$guess = strcmp( AtD_get_setting( $user->ID, 'AtD_guess_lang' ), "true" ) == 0 ? "true" : "false";
 
-        $data = AtD_http_post( $postText . "&guess=$guess", defined('ATD_HOST') ? ATD_HOST : $service, $url, defined('ATD_PORT') ? ATD_PORT : 80 );
+    $data = AtD_http_post( $postText . "&guess=$guess", defined('ATD_HOST') ? ATD_HOST : $service, $url, defined('ATD_PORT') ? ATD_PORT : 80 );
 
-        header( 'Content-Type: text/xml' );
+    header( 'Content-Type: text/xml' );
 
-		if ( !empty($data[1]) )
-			echo $data[1];
+	if ( ! empty( $data[1] ) )
+		echo $data[1];
 
-		die();
+	die();
 }
