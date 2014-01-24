dd4c39bff803307792f05d5c7a41f7e74dcad931
Jeremy Herve <jeremy@tagada.hu>
2014-01-24 12:07:52 +0000

    Lib: move funcitons.compat into the lib/ folder

--- jetpack.php
+++ jetpack.php
@@ -50,7 +50,6 @@ require_once( JETPACK__PLUGIN_DIR . 'class.jetpack-debugger.php'      );
 require_once( JETPACK__PLUGIN_DIR . 'class.jetpack-heartbeat.php'     );
 require_once( JETPACK__PLUGIN_DIR . 'class.photon.php'                );
 require_once( JETPACK__PLUGIN_DIR . 'functions.photon.php'            );
-require_once( JETPACK__PLUGIN_DIR . 'functions.compat.php'            );
 require_once( JETPACK__PLUGIN_DIR . 'functions.gallery.php'           );
 require_once( JETPACK__PLUGIN_DIR . 'require-lib.php'                 );
 
