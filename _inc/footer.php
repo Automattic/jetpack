1d578ca1ca656c0230e92610d61af1870619b770
George Stephanis <george@stephanis.info>
2014-01-25 13:21:52 -0800

    Remove Google Analytics tracking

--- _inc/footer.php
+++ _inc/footer.php
@@ -399,14 +399,3 @@
 		<td class="med"><a href="{{ url }}" data-name="{{ name }}"><span class="genericon genericon-help" title="Learn more"></span></a>{{#hasConfig}}<a href="{{ url }}" data-name="{{ name }}"><span class="genericon genericon-cog" title="Configure"></span></a>{{/hasConfig}}</td>
 	</tr>
 </script>
-<script type="text/javascript">
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-52447-43']);
-	_gaq.push(['_trackPageview']);
-	
-	(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
-</script>
