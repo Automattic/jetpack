d37b0e30427c0f219adc543dd674b446e8618619
George Stephanis <george@stephanis.info>
2014-01-27 13:33:42 -0500

    Add file smusher and .min.css files.

--- tools/csstidy-internal.php
+++ tools/csstidy-internal.php
@@ -26,13 +26,36 @@ function tidy_css( $css ) {
 	return $csstidy->print->plain();
 }
 
+function smush_css( $css ) {
+	$csstidy = new csstidy();
+
+	$csstidy->set_cfg( 'remove_bslash',              false );
+	$csstidy->set_cfg( 'compress_colors',            true );
+	$csstidy->set_cfg( 'compress_font-weight',       true );
+	$csstidy->set_cfg( 'remove_last_;',              true );
+	$csstidy->set_cfg( 'case_properties',            true );
+	$csstidy->set_cfg( 'discard_invalid_properties', true );
+	$csstidy->set_cfg( 'css_level',                  'CSS3.0' );
+	$csstidy->set_cfg( 'template',                   'highest');
+
+	$csstidy->parse( $css );
+
+	return $csstidy->print->plain();
+}
+
 foreach ( $files as $this_file ) {
 	$path = dirname( __FILE__ ) . '/../_inc/' . $this_file;
 	if ( is_readable( $path ) ) {
 		$css = file_get_contents( $path );
+
 		$tidied_css = tidy_css( $css );
 		file_put_contents( $path, $tidied_css );
-		echo "Success: {$this_file} tidied.\r\n";
+
+		$min_path = str_replace( '.css', '.min.css', $path );
+		$smushed_css = smush_css( $css );
+		file_put_contents( $min_path, $smushed_css );
+
+		echo "Success: {$this_file} tidied and smushed.\r\n";
 	} else {
 		echo "Error: {$this_file} not found.\r\n";
 	}
