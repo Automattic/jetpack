7a53086b0b1974ad89c726f0e583df0f12de8b03
George Stephanis <george@stephanis.info>
2014-01-27 13:24:37 -0500

    Build a csstidy fixer for Sass output
    This will tidy up what Sass kicks out, normalizing to tabs, etc.

---
+++ tools/csstidy-internal.php
@@ -0,0 +1,39 @@
+<?php
+
+$files = array(
+	'jetpack.css',
+	'jetpack-banners.css',
+);
+
+require_once( dirname( __FILE__ ) . '/../modules/custom-css/csstidy/class.csstidy.php' );
+
+function tidy_css( $css ) {
+	$csstidy = new csstidy();
+
+	$csstidy->set_cfg( 'remove_bslash',              false );
+	$csstidy->set_cfg( 'compress_colors',            false );
+	$csstidy->set_cfg( 'compress_font-weight',       false );
+	$csstidy->set_cfg( 'optimise_shorthands',        0 );
+	$csstidy->set_cfg( 'remove_last_;',              false );
+	$csstidy->set_cfg( 'case_properties',            false );
+	$csstidy->set_cfg( 'discard_invalid_properties', true );
+	$csstidy->set_cfg( 'css_level',                  'CSS3.0' );
+	$csstidy->set_cfg( 'preserve_css',               true );
+	$csstidy->set_cfg( 'template',                   dirname( __FILE__ ) . '/../modules/custom-css/csstidy/wordpress-standard.tpl' );
+
+	$csstidy->parse( $css );
+
+	return $csstidy->print->plain();
+}
+
+foreach ( $files as $this_file ) {
+	$path = dirname( __FILE__ ) . '/../_inc/' . $this_file;
+	if ( is_readable( $path ) ) {
+		$css = file_get_contents( $path );
+		$tidied_css = tidy_css( $css );
+		file_put_contents( $path, $tidied_css );
+		echo "Success: {$this_file} tidied.\r\n";
+	} else {
+		echo "Error: {$this_file} not found.\r\n";
+	}
+}
