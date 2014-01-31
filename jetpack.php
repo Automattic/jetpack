e46f9899b2af40fd553b0fd722db03b8bfa593aa
George Stephanis <george@stephanis.info>
2014-01-31 11:01:12 -0500

    Update the minimum and tested WP versions
    Minimum goes to 3.7
    Tested goes to 3.8.1

--- jetpack.php
+++ jetpack.php
@@ -14,7 +14,7 @@
 
 defined( 'JETPACK__API_BASE' ) or define( 'JETPACK__API_BASE', 'https://jetpack.wordpress.com/jetpack.' );
 define( 'JETPACK__API_VERSION', 1 );
-define( 'JETPACK__MINIMUM_WP_VERSION', '3.5' );
+define( 'JETPACK__MINIMUM_WP_VERSION', '3.7' );
 defined( 'JETPACK_CLIENT__AUTH_LOCATION' ) or define( 'JETPACK_CLIENT__AUTH_LOCATION', 'header' );
 defined( 'JETPACK_CLIENT__HTTPS' ) or define( 'JETPACK_CLIENT__HTTPS', 'AUTO' );
 define( 'JETPACK__VERSION', '2.8-beta1' );
