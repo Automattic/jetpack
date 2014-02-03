71f59032db31007813e18f741c5c819a84b766bc
Konstantin Obenland <obenland@automattic.com>
2014-02-03 22:59:58 +0000

    Featured Content: Hide term based on term name rather than term id.
    Since only the term name gets updated by the Customizer, the term_id will not
    always be correct during preview. This might lead to confusion when the
    previewed term is still visible, even though it is supposed to be hidden.
    
    See #themes2208 for original report.
    
    Merges r90228-wpcom.

--- modules/theme-tools/featured-content.php
+++ modules/theme-tools/featured-content.php
@@ -328,9 +328,13 @@ class Featured_Content {
 			return $terms;
 		}
 
-		foreach( $terms as $order => $term ) {
-			if ( is_object( $term ) && self::get_setting( 'tag-id' ) == $term->term_id && 'post_tag' == $term->taxonomy ) {
-				unset( $terms[ $order ] );
+		$tag = get_term_by( 'name', self::get_setting( 'tag-name' ), 'post_tag' );
+
+		if ( false !== $tag ) {
+			foreach ( $terms as $order => $term ) {
+				if ( is_object( $term ) && $tag->term_id == $term->term_id && 'post_tag' == $term->taxonomy ) {
+					unset( $terms[ $order ] );
+				}
 			}
 		}
 
@@ -367,9 +371,13 @@ class Featured_Content {
 			return $terms;
 		}
 
-		foreach( $terms as $order => $term ) {
-			if ( self::get_setting( 'tag-id' ) == $term->term_id ) {
-				unset( $terms[ $term->term_id ] );
+		$tag = get_term_by( 'name', self::get_setting( 'tag-name' ), 'post_tag' );
+
+		if ( false !== $tag ) {
+			foreach ( $terms as $term ) {
+				if ( $tag->term_id == $term->term_id ) {
+					unset( $terms[ $term->term_id ] );
+				}
 			}
 		}
 
