505a137b9159a5d516ddc5a22a9372d9610ed140
Simon Wheatley <simonw@codeforthepeople.com>
2014-02-03 18:52:58 +0000

    Add filters/actions to enable the addition of type rules.

--- modules/widget-visibility/widget-conditions.php
+++ modules/widget-visibility/widget-conditions.php
@@ -179,6 +179,7 @@ class Jetpack_Widget_Conditions {
 				</div><!-- .condition-top -->
 
 				<div class="conditions">
+					<?php do_action( 'widget_conditions_before_admin_form', $instance, $return, $widget ); ?>
 					<?php
 
 					foreach ( $conditions['rules'] as $rule ) {
@@ -207,6 +208,8 @@ class Jetpack_Widget_Conditions {
 						</div><!-- .condition -->
 						<?php
 					}
+					
+					do_action( 'widget_conditions_after_admin_form', $instance, $return, $widget );
 
 					?>
 				</div><!-- .conditions -->
@@ -227,6 +230,7 @@ class Jetpack_Widget_Conditions {
 		$conditions['action'] = $_POST['conditions']['action'];
 		$conditions['rules'] = array();
 
+		$conditions[ 'active' ] = false;
 		foreach ( $_POST['conditions']['rules_major'] as $index => $major_rule ) {
 			if ( ! $major_rule )
 				continue;
@@ -235,9 +239,13 @@ class Jetpack_Widget_Conditions {
 				'major' => $major_rule,
 				'minor' => isset( $_POST['conditions']['rules_minor'][$index] ) ? $_POST['conditions']['rules_minor'][$index] : ''
 			);
+
+			$conditions[ 'active' ] = true;
 		}
 
-		if ( ! empty( $conditions['rules'] ) )
+		$conditions = apply_filters( 'widget_conditions_update', $conditions, $instance, $new_instance, $old_instance );
+
+		if ( $conditions[ 'active' ] )
 			$instance['conditions'] = $conditions;
 		else
 			unset( $instance['conditions'] );
@@ -424,6 +432,8 @@ class Jetpack_Widget_Conditions {
 			if ( $condition_result )
 				break;
 		}
+
+		apply_filters( 'widget_conditions_condition_result', $condition_result, $instance );
 
 		if ( ( 'show' == $instance['conditions']['action'] && ! $condition_result ) || ( 'hide' == $instance['conditions']['action'] && $condition_result ) )
 			return false;
