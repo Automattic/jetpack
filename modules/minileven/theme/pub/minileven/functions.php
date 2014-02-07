9745808429a462c1ea4cb8db18371aa9581d0080
Caroline Moore <caroline@automattic.com>
2014-02-07 15:00:15 +0000

    Minileven: Replace TEMPLATEPATH with get_template_directory(). See #2135
    Merges r16535-wpcom.

--- modules/minileven/theme/pub/minileven/functions.php
+++ modules/minileven/theme/pub/minileven/functions.php
@@ -50,7 +50,7 @@ function minileven_setup() {
 	 * If you're building a theme based on Minileven, use a find and replace
 	 * to change 'minileven' to the name of your theme in all the template files.
 	 */
-	load_theme_textdomain( 'minileven', TEMPLATEPATH . '/languages' );
+	load_theme_textdomain( 'minileven', get_template_directory() . '/languages' );
 
 	// Add default posts and comments RSS feed links to <head>.
 	add_theme_support( 'automatic-feed-links' );
