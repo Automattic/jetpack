cd551de86477438191a06b8604e77357fd155945
blobaugh <ben@lobaugh.net>
2014-02-08 13:39:51 -0800

    Jetpack: Push Notifications: Remove module card from Jetpack modules page
    Push Notifications are now handled by modules/notes.php. This module card is no longer needed.
    
    Resolves #175

--- modules/mobile-push.php
+++ modules/mobile-push.php
@@ -1,14 +1,6 @@
 <?php
 /**
- * Module Name: Mobile Push Notifications
- * Module Description: Receive notifications on your mobile device.
- * Sort Order: 100
- * First Introduced: 1.9
- * Requires Connection: Yes
- * Auto Activate: Yes
- * Module Tags: Mobile
- */
-
-Jetpack_Sync::sync_comments( __FILE__, array(
-	'comment_stati' => array( 'approved', 'unapproved' ),
-) );
+ * Deprecated. See notes.php for the new module
+ *
+ * @package Jetpack
+ **/
