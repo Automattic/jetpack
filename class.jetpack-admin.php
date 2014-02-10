60fa37828a80231ca6ab4be13ee70363e3fd2332
George Stephanis <georgestephanis@automattic.com>
2014-02-10 16:42:11 -0500

    Add .wp-list-table to the classes

--- class.jetpack-admin.php
+++ class.jetpack-admin.php
@@ -375,7 +375,7 @@ class Jetpack_Admin {
 						</div>
 					</div>
 					<div class="manage-left">
-						<table class="table table-bordered <?php echo implode( ' ', $list_table->get_table_classes() ); ?>">
+						<table class="table table-bordered wp-list-table <?php echo implode( ' ', $list_table->get_table_classes() ); ?>">
 							<tbody id="the-list">
 								<?php $list_table->display_rows_or_placeholder(); ?>
 							</tbody>
