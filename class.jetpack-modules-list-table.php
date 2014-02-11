6de925e0a10095640fc97da45c4ad4e30a6b4ccd
George Stephanis <georgestephanis@automattic.com>
2014-02-11 13:57:01 -0500

    If an object is passed, clear out any random cruft.

--- class.jetpack-modules-list-table.php
+++ class.jetpack-modules-list-table.php
@@ -52,6 +52,7 @@ class Jetpack_Modules_List_Table extends WP_List_Table {
 			<% var i = 0;
 			if ( items.length ) {
 			_.each( items, function( item, key, list ) {
+				if ( item === undefined ) return;
 				%>
 				<tr class="jetpack-module <% if ( ++i % 2 ) { %> alternate<% } %><% if ( item.activated ) { %> active<% } %><% if ( ! item.available ) { %> unavailable<% } %>" id="<%= item.module %>">
 					<th scope="row" class="check-column">
