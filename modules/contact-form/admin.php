Automattic/jetpack

commit 9cc9561fdf596e7e2856f2e0c568c12b1dd1a5e5

Shortcodes: use has_shortcode to check if shortcode exists
has_shortcode was introduced in 3.6. It's now safe to use in Jetpack

modules/contact-form/admin.php
@@ -483,6 +483,7 @@ function grunion_ajax_shortcode_to_json() {
 
 	// doesn't look like a post with a [contact-form] already.
 	if ( false === strpos( $content, '[contact-form' ) ) {
+	if ( false === has_shortcode( $content, 'contact-form' ) ) {
 		die( '' );
 	}
 

class.jetpack-post-images.php
@@ -21,8 +21,9 @@ class Jetpack_PostImages {
 		if ( !empty( $post->post_password ) )
 			return $images;
 
-		if ( false === strpos( $post->post_content, '[slideshow' ) )
+		if ( false === has_shortcode( $post->post_content, 'slideshow' ) ) {
 			return false; // no slideshow - bail
+		}
 
 		$permalink = get_permalink( $post->ID );
 
@@ -87,8 +88,9 @@ class Jetpack_PostImages {
 		if ( !empty( $post->post_password ) )
 			return $images;
 
-		if ( false === strpos( $post->post_content, '[gallery' ) )
+		if ( false === has_shortcode( $post->post_content, 'gallery' ) ) {
 			return false; // no gallery - bail
+		}
 
 		$permalink = get_permalink( $post->ID );
 

modules/shortcodes/presentations.php
@@ -109,7 +109,7 @@ class Presentations {
 		}
 
 		foreach ( $GLOBALS['posts'] as $p ) {
-			if ( false !== strpos( $p->post_content, '[presentation' ) ) {
+			if ( has_shortcode( $p->post_content, 'presentation' ) ) {
 				$this->scripts_and_style_included = true;
 				break;
 			}

commit f30f7658231f7fe9d0643c70a4b7718a5a1dc0f4

Contact Form: remove old conditional, oops
Error introduced in  9cc9561fdf596e7e2856f2e0c568c12b1dd1a5e5

modules/contact-form/admin.php
@@ -482,7 +482,6 @@ function grunion_ajax_shortcode_to_json() {
 	$content = stripslashes( $_POST['content'] );
 
 	// doesn't look like a post with a [contact-form] already.
-	if ( false === strpos( $content, '[contact-form' ) ) {
 	if ( false === has_shortcode( $content, 'contact-form' ) ) {
 		die( '' );
 	}