9f114ea23e6766c6ea3b0b4a82b33a6d417feab8
Aaron Jorbin <aaron@jorb.in>
2014-03-02 14:19:46 -0500

    first pass at grunt

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		clean: {
+      css : ['_inc/**/*.min.css'],
+      js  : ['_inc/**/*.min.js']
+		},
+		cssmin: {
+			admin: {
+				expand: true,
+				ext: '.min.css',
+				src: [
+					'_inc/**/*.css',
+          '!_inc/**/*.min.css'
+				]
+			}
+		},
+		uglify: {
+			admin: {
+				expand: true,
+				ext: '.min.js',
+				src: [
+					'_inc/**/*.js',
+          '!_inc/**/*.min.js'
+				]
+			},
+		},
+		watch: {
+			js: {
+				files: [
+            '_inc/**/*.js',
+            '!_inc/**/*.min.js'
+				],
+				tasks: ['clean:js', 'uglify'],
+				options: {
+					dot      : false,
+					spawn    : false,
+					interval : 2000
+				}
+			},
+      css: {
+        files: [
+          '_inc/**/*.css',
+          '!_inc/**/*.min.css'
+        ],
+				tasks: ['clean:css', 'cssmin'],
+				options: {
+					dot      : false,
+					spawn    : false,
+					interval : 2000
+				}
+      }
+		}
+	});
+
+  // Load tasks
+  require('matchdep').filterDev('grunt-*').forEach( grunt.loadNpmTasks );
+
+	// Register tasks.
+	grunt.registerTask('build', ['clean:admin', 'cssmin:admin','uglify:admin']);
+
+	// Default task.
+	grunt.registerTask('default', ['build']);
+
+};
