06254f192c5903cc31c9bd0c0b0aadc40493cec7
George Stephanis <georgestephanis@automattic.com>
2014-03-03 15:04:56 -0500

    Pull the ifcheck wrapper around everything.
    Fix some whitespace and indentation.

--- 3rd-party/wpml.php
+++ 3rd-party/wpml.php
@@ -1,17 +1,21 @@
 <?php
 
+// Only load these if WPML is active.
+if ( defined( 'ICL_SITEPRESS_VERSION' ) ) :
+
 add_action( 'jetpack_widget_get_top_posts', 'wpml_jetpack_widget_get_top_posts', 10, 3 );
 function wpml_jetpack_widget_get_top_posts( $posts, $post_ids, $count ) {
-	if ( defined('ICL_SITEPRESS_VERSION') ) {
-		global $sitepress;
-		foreach ( $posts as $k => $post ) {
-			$lang_information = wpml_get_language_information( $post['post_id'] );
-			$post_language = substr( $lang_information['locale'], 0, 2 );
+	global $sitepress;
 
-			if ( $post_language !== $sitepress->get_current_language() ) {
-				unset( $posts[ $k ] );
-			}
+	foreach ( $posts as $k => $post ) {
+		$lang_information = wpml_get_language_information( $post['post_id'] );
+		$post_language    = substr( $lang_information['locale'], 0, 2 );
+		if ( $post_language !== $sitepress->get_current_language() ) {
+			unset( $posts[ $k ] );
 		}
 	}
+
 	return $posts;
 }
+
+endif;
