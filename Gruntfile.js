51f505dc069056dea73f82aa70f62a7c1797275a
George Stephanis <georgestephanis@automattic.com>
2014-03-11 13:43:23 -0400

    Initial attempt at adding a phplint grunt task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,31 @@
 
 module.exports = function(grunt) {
 
-	grunt.initConfig({
+	var cfg = {
 		pkg: grunt.file.readJSON('package.json'),
 		shell: {
 			buildSass: {
 				command: 'php tools/pre-commit-build-scss.php'
 			}
+		},
+		phplint: {
+			files: [
+				'*.php',
+				'*/*.php',
+				'*/*/*.php',
+				'*/*/*/*.php',
+				'*/*/*/*/*.php',
+				'*/*/*/*/*/*.php',
+			]
 		}
-	});
+	};
+
+	grunt.initConfig( cfg );
 
 	grunt.loadNpmTasks('grunt-shell');
+	grunt.loadNpmTasks("grunt-phplint");
 
 	grunt.registerTask('default', ['shell']);
+	grunt.registerTask('default', ['phplint']);
 
 };
