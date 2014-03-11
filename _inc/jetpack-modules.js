d6d92a7edda685a81533b5092991fc95dfa86506
George Stephanis <georgestephanis@automattic.com>
2014-03-11 15:00:32 -0400

    Whitespace tidying.

--- _inc/jetpack-modules.js
+++ _inc/jetpack-modules.js
@@ -4,10 +4,10 @@
 
 	var modules, list_table, handle_module_tag_click, $the_table, $the_filters, $the_search, $jp_frame, show_modal, hide_modal, set_modal_tab;
 
-	$the_table   = $('.wp-list-table.jetpack-modules');
-	$the_filters = $('.navbar-form');
-	$the_search  = $('#srch-term-search-input');
-	$jp_frame    = $('.jp-frame');
+	$the_table   = $( '.wp-list-table.jetpack-modules' );
+	$the_filters = $( '.navbar-form' );
+	$the_search  = $( '#srch-term-search-input' );
+	$jp_frame    = $( '.jp-frame' );
 
 	modules = new models.Modules( {
 		items : items
@@ -24,42 +24,42 @@
 	// Handle the filtering of modules.
 	handle_module_tag_click = function( event ) {
 		// Switch the item in the subsubsub list that's flagged as current.
-		$('.subsubsub').find('a[data-title="' + $(this).data('title') + '"]').addClass('current')
-			.closest('li').siblings().find('a.current').removeClass('current');
+		$( '.subsubsub' ).find( 'a[data-title="' + $(this).data('title') + '"]' ).addClass( 'current' )
+			.closest( 'li' ).siblings().find( 'a.current' ).removeClass( 'current' );
 
 		event.preventDefault();
 		modules.trigger( 'change' );
 	};
 
-	$('.subsubsub a').on( 'click', { modules : modules }, handle_module_tag_click );
 
 	/**
 	 * Temporarily comment out, as our table no longer has a tags column.
 	 */
 	// $the_table.on( 'click', '.module_tags a', { modules : modules }, handle_module_tag_click );
+	$( '.subsubsub a' ).on( 'click', { modules : modules }, handle_module_tag_click );
 
 	show_modal = function( module, tab ) {
-		$jp_frame.children('.modal, .shade').show();
+		$jp_frame.children( '.modal, .shade' ).show();
 		set_modal_tab( tab );
 	};
 
 	hide_modal = function() {
-		$jp_frame.children('.modal, .shade').hide();
+		$jp_frame.children( '.modal, .shade' ).hide();
 		set_modal_tab( null );
 	};
 
 	$jp_frame.on( 'click', '.modal header .close, .shade', hide_modal );
 
 	set_modal_tab = function( tab ) {
-		$jp_frame.find('.modal .active').removeClass('active');
+		$jp_frame.find( '.modal .active' ).removeClass( 'active' );
 		switch ( tab ) {
 			case 'learn-more':
-				$jp_frame.find('.modal .learn-more a').addClass('active');
-				$jp_frame.children('.modal' ).trigger( 'learn-more' );
+				$jp_frame.find( '.modal .learn-more a' ).addClass( 'active' );
+				$jp_frame.children( '.modal' ).trigger( 'learn-more' );
 				break;
 			case 'config':
-				$jp_frame.find('.modal .config a').addClass('active');
-				$jp_frame.children('.modal' ).trigger( 'config' );
+				$jp_frame.find( '.modal .config a' ).addClass( 'active' );
+				$jp_frame.children( '.modal' ).trigger( 'config' );
 				break;
 			default:
 				break;
