433676994ec2e38d2a4eb9d5f8dc3eeace481e54
Jeremy Herve <jeremy@tagada.hu>
2014-03-18 15:57:12 +0000

    Admin footer: update the localization of the Automattic tagline
    Also fix apostrophes in strings

--- _inc/footer.php
+++ _inc/footer.php
@@ -8,8 +8,8 @@
 				<nav class="primary nav-horizontal">
 					<div class="a8c-attribution">
 						<span>
-							<?php sprintf( __( 'An <a href="%s" class="a8c-logo">Automattic</a> Airline', 'jetpack' ),
-							'http://automattic.com/'
+							<?php echo sprintf( __( 'An %s Airline', 'jetpack' ),
+							'<a href="http://automattic.com/" class="a8c-logo">Automattic</a>'
 							); ?>
 						</span>
 					</div>
@@ -20,9 +20,9 @@
 						<a href="http://jetpack.me">Jetpack <?php echo JETPACK__VERSION; ?></a>
 						<a href="http://wordpress.com/tos/"><?php _e( 'Terms', 'jetpack' ); ?></a>
 						<a href="http://automattic.com/privacy/"><?php _e( 'Privacy', 'jetpack' ); ?></a>
-						<a href="admin.php?page=jetpack-debugger" title="<?php esc_attr_e( 'Test your site's compatibility with Jetpack.', 'jetpack' ); ?>">Debug</a>
+						<a href="admin.php?page=jetpack-debugger" title="<?php esc_attr_e( 'Test your site&#8217;s compatibility with Jetpack.', 'jetpack' ); ?>"><?php _e( 'Debug', 'jetpack' ); ?></a>
 						<a href="/support/" title="<?php esc_attr_e( 'Contact the Jetpack Happiness Squad.', 'jetpack' ); ?>"><?php _e( 'Support', 'jetpack' ); ?></a>
-						<a href="http://jetpack.me/survey/?rel=<?php echo JETPACK__VERSION; ?>" title="<?php esc_attr_e( 'Take a survey.  Tell us how we're doing.', 'jetpack' ); ?>"><?php _e( 'Give Us Feedback', 'jetpack' ); ?></a>
+						<a href="http://jetpack.me/survey/?rel=<?php echo JETPACK__VERSION; ?>" title="<?php esc_attr_e( 'Take a survey.  Tell us how we&#8217;re doing.', 'jetpack' ); ?>"><?php _e( 'Give Us Feedback', 'jetpack' ); ?></a>
 					</div>
 				</nav><!-- .secondary -->
 			</div><!-- .footer -->
