ab72aabdcb2d68e693f6068a4407c806891423b9
Chase Livingston <chaselivingston@gmail.com>
2014-03-20 11:21:06 -0400

    Add currently connected wpcom user to debugger fixes #220

--- class.jetpack-debugger.php
+++ class.jetpack-debugger.php
@@ -151,6 +151,8 @@ class Jetpack_Debugger {
 				<?php if ( self::is_jetpack_support_open() ): ?>
 				<p class="jetpack-show-contact-form"><?php _e( 'If none of these help you find a solution, <a href="#">click here to contact Jetpack support</a>. Tell us as much as you can about the issue and what steps you\'ve tried to resolve it, and one of our Happiness Engineers will be in touch to help.', 'jetpack' ); ?>
 				</p>
+				<p class="jetpack-show-contact-form"><?php echo sprintf( __( 'Current connected user is ' . Jetpack::get_connected_user_email(), 'jetpack' ) ); ?>
+				</p>
 				<?php endif; ?>
 			</div>
 			<div id="contact-message" style="display:none">
