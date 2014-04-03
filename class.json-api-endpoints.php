3440e25d532b8830f014df6f8bc97beb5db87b5e
Justin Shreve <justin@automattic.com>
2014-04-03 21:50:06 +0000

    REST API
    Endpoints for getting all of a site's tags or categories.
    See http://[private link]
    
    Merges r95266-wpcom.

--- class.json-api-endpoints.php
+++ class.json-api-endpoints.php
@@ -2293,6 +2293,47 @@ class WPCOM_JSON_API_Get_Taxonomy_Endpoint extends WPCOM_JSON_API_Taxonomy_Endpo
 	}
 }
 
+class WPCOM_JSON_API_Get_Taxonomies_Endpoint extends WPCOM_JSON_API_Endpoint {
+	// /sites/%s/tags       -> $blog_id
+	// /sites/%s/categories -> $blog_id
+	function callback( $path = '', $blog_id = 0 ) {
+		$blog_id = $this->api->switch_to_blog_and_validate_user( $this->api->get_blog_id( $blog_id ) );
+		if ( is_wp_error( $blog_id ) ) {
+			return $blog_id;
+		}
+		if ( preg_match( '#/tags#i', $path ) ) {
+			return $this->tags();
+		} else {
+			return $this->categories();
+		}
+	}
+
+	function categories() {
+		$cats = get_categories( array( 'get' => 'all' ) );
+		$found = count( $cats );
+		$cats_obj = array();
+		foreach ( $cats as $cat ) {
+			$cats_obj[] = $this->get_taxonomy( $cat->slug, 'category', 'display' );
+		}
+		return array(
+			'found'       => $found,
+			'categories'  => $cats_obj
+		);
+	}
+
+	function tags() {
+		$tags = (array) get_tags( array( 'get' => 'all' ) );
+		$found = count( $tags );
+		$tags_obj = array();
+		foreach ( $tags as $tag ) {
+			$tags_obj[] = $this->get_taxonomy( $tag->slug, 'post_tag', 'display' );
+		}
+		return array(
+			'found' => $found,
+			'tags'  => $tags_obj
+		);
+	}
+}
 
 class WPCOM_JSON_API_Update_Taxonomy_Endpoint extends WPCOM_JSON_API_Taxonomy_Endpoint {
 	// /sites/%s/tags|categories/new    -> $blog_id
@@ -4096,6 +4137,36 @@ new WPCOM_JSON_API_Get_Taxonomy_Endpoint( array(
 	),
 
 	'example_request'  => 'https://public-api.wordpress.com/rest/v1/sites/en.blog.wordpress.com/categories/slug:community?pretty=1'
+) );
+
+new WPCOM_JSON_API_Get_Taxonomies_Endpoint( array(
+	'description' => "Returns a list of a site's categories",
+	'group'       => 'taxonomy',
+	'stat'        => 'categories',
+	'method'      => 'GET',
+	'path'        => '/sites/%s/categories',
+	'path_labels' => array(
+		'$site'     => '(int|string) The site ID, The site domain'
+	),
+	'response_format' => array(
+		'found'      => '(int) The number of categories returned.',
+		'categories' => '(array) Array of category objects.',
+	)
+) );
+
+new WPCOM_JSON_API_Get_Taxonomies_Endpoint( array(
+	'description' => "Returns a list of a site's tags",
+	'group'       => 'taxonomy',
+	'stat'        => 'tags',
+	'method'      => 'GET',
+	'path'        => '/sites/%s/tags',
+	'path_labels' => array(
+		'$site'     => '(int|string) The site ID, The site domain'
+	),
+	'response_format' => array(
+		'found'    => '(int) The number of tags returned.',
+		'tags'     => '(array) Array of tag objects.',
+	)
 ) );
 
 new WPCOM_JSON_API_Get_Taxonomy_Endpoint( array(
