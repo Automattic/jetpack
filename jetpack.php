5b086fae72f6245dc582627514652af3a7aa0643
Cathy J. Fitzpatrick <cathy@automattic.com>
2014-04-10 20:04:12 +0000

    Bump version to 2.9.3 in a couple more places

--- jetpack.php
+++ jetpack.php
@@ -5,7 +5,7 @@
  * Plugin URI: http://wordpress.org/extend/plugins/jetpack/
  * Description: Bring the power of the WordPress.com cloud to your self-hosted WordPress. Jetpack enables you to connect your blog to a WordPress.com account to use the powerful features normally only available to WordPress.com users.
  * Author: Automattic
- * Version: 2.9.2
+ * Version: 2.9.3
  * Author URI: http://jetpack.me
  * License: GPL2+
  * Text Domain: jetpack
@@ -13,7 +13,7 @@
  */
 
 define( 'JETPACK__MINIMUM_WP_VERSION', '3.7' );
-define( 'JETPACK__VERSION',            '2.9.2' );
+define( 'JETPACK__VERSION',            '2.9.3' );
 define( 'JETPACK_MASTER_USER',         true );
 define( 'JETPACK__API_VERSION',        1 );
 define( 'JETPACK__PLUGIN_DIR',         plugin_dir_path( __FILE__ ) );
