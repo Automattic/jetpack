e8db499ba937c80b95c2ac6300bc4e807dc1ca55
Erick Hitter <erick@automattic.com>
2014-05-15 15:05:43 +0000

    Tiled Galleries: even `json_encode()`'d data needs to be escaped for attributes
    Merges r97425-wpcom.

--- modules/tiled-gallery/tiled-gallery.php
+++ modules/tiled-gallery/tiled-gallery.php
@@ -305,7 +305,7 @@ class Jetpack_Tiled_Gallery {
 				esc_url( wp_get_attachment_url( $attachment_id ) ),
 				esc_attr( $size ),
 				esc_attr( $comments_opened ),
-				json_encode( array_map( 'esc_attr', $img_meta ) ),
+				esc_attr( json_encode( array_map( 'esc_attr', $img_meta ) ) ),
 				esc_attr( $attachment_title ),
 				esc_attr( $attachment_desc ),
 				esc_url( $medium_file ),
