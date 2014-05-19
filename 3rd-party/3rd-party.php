f4608f4a10aa65d87091fb4e59ea3034c6023a05
blobaugh <ben@lobaugh.net>
2014-05-19 14:45:41 -0700

    3rd Party: Iterating on the 3rd party plugin tweaks loader
    Adds some "intelligence" into the 3rd party loader. Instead of loading all 3rd party
    tweaks all the time it now checks to see if the plugin the tweak is for and loads
    the tweak file based on plugin slug.
    
    Initial attempt make to take multisite into account. Needs further testing.

--- 3rd-party/3rd-party.php
+++ 3rd-party/3rd-party.php
@@ -1,10 +1,33 @@
 <?php
-
 /*
- * Placeholder to load 3rd party plugin tweaks until a legit system
- * is architected
+ * Including tweaks for 3rd party plugins is as simple as creating
+ * a php file in the 3rd-party directory with the plugin slug as 
+ * the filename. When the plugin is active in WordPress the 3rd
+ * party tweaks file with the corresponding slug will automagically
+ * be included and all tweaks loaded.
  */
 
-require_once( 'buddypress.php' );
-require_once( 'wpml.php' );
-require_once( 'bbpress.php' );
+$plugins = get_option( 'active_plugins' );
+jetpack_load_3rd_party_tweaks( $plugins );
+
+if( is_multisite() ) {
+	$network_plugins = get_site_option( 'active_sitewide_plugins' );
+	jetpack_load_3rd_party_tweaks( $network_plugins );
+}
+
+// Slight cleanup for resources
+unset( $plugins );
+unset( $network_plugins );
+
+function jetpack_load_3rd_party_tweaks( $slugs ) {
+	foreach( $slugs AS $p ) {
+		$slug = explode( '/', $p );
+		$slug = $slug[0];
+
+		$file = dirname( __FILE__ ) . "/$slug.php";
+
+		if( file_exists( $file ) ) {
+			require_once( $file );
+		}
+	}
+}
