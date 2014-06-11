2e7752654ca964323f79bb6c132d53016dfb29f1
Cathy J. Fitzpatrick <cathy@cathyjf.com>
2014-06-11 01:46:06 -0600

    Revert "o2: Infinite Scroll: Test for the response object to contain html before attempting to use it - avoids an invalid error on o2 - see http://[private link]"
    This reverts commit 40b0cf67fcb0bf51557f0afc72d5ae4c9c6f9807.
    
    This merges r98204-wpcom.

--- modules/infinite-scroll/infinity.js
+++ modules/infinite-scroll/infinity.js
@@ -377,11 +377,6 @@ Scroller.prototype.maybeLoadMejs = function() {
  * Initialize the MediaElement.js player for any posts not previously initialized
  */
 Scroller.prototype.initializeMejs = function( ev, response ) {
-	// Test for response before using it
-	if ( ( 'undefined' === typeof response ) || ( 'undefined' === typeof response.html ) ) {
-		return;
-	}
-
 	// Are there media players in the incoming set of posts?
 	if ( -1 === response.html.indexOf( 'wp-audio-shortcode' ) && -1 === response.html.indexOf( 'wp-video-shortcode' ) ) {
 		return;
