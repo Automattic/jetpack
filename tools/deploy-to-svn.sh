5400065e151ec0f6d337df8c7fb489cb7da84609
George Stephanis <georgestephanis@automattic.com>
2014-06-12 17:54:03 -0400

    deploy.sh: Run the svn commands in quiet mode.

--- tools/deploy-to-svn.sh
+++ tools/deploy-to-svn.sh
@@ -29,10 +29,10 @@ git checkout $TAG
 
 # Prep a home to drop our new files in. Just make it in /tmp so we can start fresh each time.
 rm -rf $JETPACK_SVN_DIR
-svn checkout http://plugins.svn.wordpress.org/jetpack/ --depth=empty $JETPACK_SVN_DIR
+svn -q checkout http://plugins.svn.wordpress.org/jetpack/ --depth=empty $JETPACK_SVN_DIR
 cd $JETPACK_SVN_DIR
-svn up trunk
-svn up tags --depth=empty
+svn -q up trunk
+svn -q up tags --depth=empty
 
 # delete everything except .svn dirs
 for file in $(find $JETPACK_SVN_DIR/trunk/* -not -path "*.svn*"); do
