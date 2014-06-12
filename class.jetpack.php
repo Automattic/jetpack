855d1a5c409bf75fe8d4215efb38218ec95c80b1
enej <enej.bajgoric@gmail.com>
2014-06-12 15:06:35 -0700

    Fixes issue #591
    Make sure that we only pass a string to the set cookie function.

--- class.jetpack.php
+++ class.jetpack.php
@@ -3962,6 +3962,9 @@ p {
 
 		// Set a state variable
 		if ( isset ( $key ) && isset( $value ) ) {
+			if( is_array( $value ) && isset( $value[0] ) ) {
+				$value = $value[0];
+			}
 			$state[ $key ] = $value;
 			setcookie( "jetpackState[$key]", $value, 0, $path, $domain );
 		}
