94dd77ba90d3fbc7e52c342c2c8f9d442b9dff8a
enej <enej.bajgoric@gmail.com>
2014-06-17 09:46:40 -0700

    Also version bump the JETPACK_VERSION

--- jetpack.php
+++ jetpack.php
@@ -13,7 +13,7 @@
  */
 
 define( 'JETPACK__MINIMUM_WP_VERSION', '3.8' );
-define( 'JETPACK__VERSION',            '3.0.2b2' );
+define( 'JETPACK__VERSION',            '3.0.2' );
 define( 'JETPACK_MASTER_USER',         true );
 define( 'JETPACK__API_VERSION',        1 );
 define( 'JETPACK__PLUGIN_DIR',         plugin_dir_path( __FILE__ ) );
