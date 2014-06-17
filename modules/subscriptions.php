d8c135369a8514cb2932a214664655e19b01e111
Jeremy Herve <jeremy@tagada.hu>
2014-06-17 15:10:18 +0000

    Subscription form: do not display the logged in user's email
    Can be overwritten with the jetpack_auto_fill_logged_in_user filter
    Fixes #694

--- modules/subscriptions.php
+++ modules/subscriptions.php
@@ -563,7 +563,20 @@ class Jetpack_Subscriptions_Widget extends WP_Widget {
 	}
 
 	function widget( $args, $instance ) {
-		global $current_user;
+		if (
+			( !defined( 'IS_WPCOM' ) || !IS_WPCOM )
+		&&
+			false === apply_filters( 'jetpack_auto_fill_logged_in_user', false )
+		) {
+			$subscribe_email = esc_html__( 'Email Address', 'jetpack' );
+		} else {
+			global $current_user;
+			if ( !empty( $current_user->user_email ) ) {
+				$subscribe_email = esc_attr( $current_user->user_email );
+			} else {
+				$subscribe_email = esc_html__( 'Email Address', 'jetpack' );
+			}
+		}
 
 
 
@@ -635,7 +648,7 @@ class Jetpack_Subscriptions_Widget extends WP_Widget {
 				<label id="jetpack-subscribe-label" for="email">
 					<?php echo !empty( $subscribe_placeholder ) ? esc_html( $subscribe_placeholder ) : esc_html__( 'Email Address:', 'jetpack' ); ?>
 				</label>
-				<input type="email" name="email" value="<?php echo !empty( $current_user->user_email ) ? esc_attr( $current_user->user_email ) : ''; ?>" id="<?php echo esc_attr( $subscribe_field_id ) ?>" placeholder="<?php echo esc_attr( $subscribe_placeholder ); ?>" />
+				<input type="email" name="email" value="<?php echo esc_attr( $subscribe_email ); ?>" id="<?php echo esc_attr( $subscribe_field_id ) ?>" placeholder="<?php echo esc_attr( $subscribe_placeholder ); ?>" />
 			</p>
 
 			<p id="subscribe-submit">
