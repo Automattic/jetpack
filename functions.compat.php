c85dc1342fc60424fb2caf3325eace6f285fa2be
Jeremy Herve <jeremy@tagada.hu>
2014-06-18 14:42:16 +0200

    Remove 3.4 backward compatibility function
    Jetpack requires WP 3.8 now

--- functions.compat.php
+++ functions.compat.php
@@ -1,19 +1,5 @@
 <?php
 
-if ( !function_exists( 'rawurlencode_deep' ) ) :
-/**
- * Navigates through an array and raw encodes the values to be used in a URL.
- *
- * @since WordPress 3.4.0
- *
- * @param array|string $value The array or string to be encoded.
- * @return array|string $value The encoded array (or string from the callback).
- */
-function rawurlencode_deep( $value ) {
-	return is_array( $value ) ? array_map( 'rawurlencode_deep', $value ) : rawurlencode( $value );
-}
-endif;
-
 /**
 * @param $url Can be just the $url or the whole $atts array
 * @return bool|mixed The Youtube video ID
