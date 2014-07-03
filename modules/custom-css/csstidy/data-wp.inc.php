3969c8ed1333dd4796dd215596bae3be58f23bcc
enej <enej.bajgoric@gmail.com>
2014-07-03 10:17:07 -0700

    Custom CSS: Fixes #402, Adding the css font smoothing for moz-osx property

--- modules/custom-css/csstidy/data-wp.inc.php
+++ modules/custom-css/csstidy/data-wp.inc.php
@@ -73,6 +73,7 @@ $GLOBALS['csstidy']['all_properties']['-webkit-transform-origin-x'] = 'CSS3.0';
 $GLOBALS['csstidy']['all_properties']['-webkit-transform-origin-y'] = 'CSS3.0';
 $GLOBALS['csstidy']['all_properties']['-webkit-transform-origin-z'] = 'CSS3.0';
 $GLOBALS['csstidy']['all_properties']['-webkit-font-smoothing'] = 'CSS3.0';
+$GLOBALS['csstidy']['all_properties']['-moz-osx-font-smoothing'] = 'CSS3.0';
 $GLOBALS['csstidy']['all_properties']['-font-smooth'] = 'CSS3.0';
 $GLOBALS['csstidy']['all_properties']['-o-object-fit'] = 'CSS3.0';
 $GLOBALS['csstidy']['all_properties']['object-fit'] = 'CSS3.0';
