3c72246ec7b3f8ea9df7d3eef76dd1ef4cfd9399
Jeremy Herve <jeremy@tagada.hu>
2014-07-04 07:31:01 +0000

    Subscriptions: Fix spacing
    See https://github.com/jeherve/jetpack/commit/686ed49ceda02d4f49d66f1028a4523e9f882437#commitcomment-6888035

--- modules/subscriptions.php
+++ modules/subscriptions.php
@@ -563,15 +563,13 @@ class Jetpack_Subscriptions_Widget extends WP_Widget {
 	}
 
 	function widget( $args, $instance ) {
-		if (
-			( !defined( 'IS_WPCOM' ) || !IS_WPCOM )
-		&&
-			false === apply_filters( 'jetpack_auto_fill_logged_in_user', false )
+		if ( ( ! defined( 'IS_WPCOM' ) || !IS_WPCOM )
+		    && false === apply_filters( 'jetpack_auto_fill_logged_in_user', false )
 		) {
 			$subscribe_email = esc_html__( 'Email Address', 'jetpack' );
 		} else {
 			global $current_user;
-			if ( !empty( $current_user->user_email ) ) {
+			if ( ! empty( $current_user->user_email ) ) {
 				$subscribe_email = esc_attr( $current_user->user_email );
 			} else {
 				$subscribe_email = esc_html__( 'Email Address', 'jetpack' );
