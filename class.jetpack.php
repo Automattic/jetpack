b81b39e185ea26c7e0ba5a741b4c3e85d627a2a8
richardmtl <richard@richardarchambault.ca>
2014-07-11 10:51:20 -0400

    Alphabetical sort of conflicting OG plugins

--- class.jetpack.php
+++ class.jetpack.php
@@ -133,49 +133,49 @@ class Jetpack {
 	 * Plugins for which we turn off our Facebook OG Tags implementation.
 	 */
 	private $open_graph_conflicting_plugins = array(
-		'facebook/facebook.php',                                 // Official Facebook plugin
-		'wordpress-seo/wp-seo.php',                              // WordPress SEO by Yoast
+		'2-click-socialmedia-buttons/2-click-socialmedia-buttons.php',
+		                                                         // 2 Click Social Media Buttons
 		'add-link-to-facebook/add-link-to-facebook.php',         // Add Link to Facebook
+		'add-meta-tags/add-meta-tags.php',                       // Add Meta Tags
+		'easy-facebook-share-thumbnails/esft.php',               // Easy Facebook Share Thumbnail
+		'facebook/facebook.php',                                 // Facebook (official plugin)
 		'facebook-awd/AWD_facebook.php',                         // Facebook AWD All in one
+		'facebook-featured-image-and-open-graph-meta-tags/fb-featured-image.php',
+		                                                         // Facebook Featured Image & OG Meta Tags
+		'facebook-meta-tags/facebook-metatags.php',              // Facebook Meta Tags
+		'facebook-revised-open-graph-meta-tag/index.php',        // Facebook Revised Open Graph Meta Tag
+		'facebook-thumb-fixer/_facebook-thumb-fixer.php',        // Facebook Thumb Fixer
+		'facebook-and-digg-thumbnail-generator/facebook-and-digg-thumbnail-generator.php',
+		                                                         // Fedmich's Facebook Open Graph Meta
 		'header-footer/plugin.php',                              // Header and Footer
+		'network-publisher/networkpub.php',                      // Network Publisher
 		'nextgen-facebook/nextgen-facebook.php',                 // NextGEN Facebook OG
+		'social-networks-auto-poster-facebook-twitter-g/NextScripts_SNAP.php',
+		                                                         // NextScripts SNAP
+		'opengraph/opengraph.php',                               // Open Graph
+		'open-graph-protocol-framework/open-graph-protocol-framework.php',
+		                                                         // Open Graph Protocol Framework
 		'seo-facebook-comments/seofacebook.php',                 // SEO Facebook Comments
 		'seo-ultimate/seo-ultimate.php',                         // SEO Ultimate
 		'sexybookmarks/sexy-bookmarks.php',                      // Shareaholic
 		'shareaholic/sexy-bookmarks.php',                        // Shareaholic
-		'social-discussions/social-discussions.php',             // Social Discussions
-		'social-networks-auto-poster-facebook-twitter-g/NextScripts_SNAP.php',
-		                                                         // NextScripts SNAP
-		'wordbooker/wordbooker.php',                             // Wordbooker
-		'socialize/socialize.php',                               // Socialize
-		'simple-facebook-connect/sfc.php',                       // Simple Facebook Connect
-		'social-sharing-toolkit/social_sharing_toolkit.php',     // Social Sharing Toolkit
-		'wp-facebook-open-graph-protocol/wp-facebook-ogp.php',   // WP Facebook Open Graph protocol
-		'opengraph/opengraph.php',                               // Open Graph
 		'sharepress/sharepress.php',                             // SharePress
-		'wp-facebook-like-send-open-graph-meta/wp-facebook-like-send-open-graph-meta.php',
-		                                                         // WP Facebook Like Send & Open Graph Meta
-		'network-publisher/networkpub.php',                      // Network Publisher
-		'wp-ogp/wp-ogp.php',                                     // WP-OGP
-		'open-graph-protocol-framework/open-graph-protocol-framework.php',
-		                                                         // Open Graph Protocol Framework
-		'facebook-featured-image-and-open-graph-meta-tags/fb-featured-image.php',
-		                                                         // Facebook Featured Image & OG Meta Tags
-		'add-meta-tags/add-meta-tags.php',                       // Add Meta Tags
+		'simple-facebook-connect/sfc.php',                       // Simple Facebook Connect
+		'social-discussions/social-discussions.php',             // Social Discussions
+		'social-sharing-toolkit/social_sharing_toolkit.php',     // Social Sharing Toolkit
+		'socialize/socialize.php',                               // Socialize
 		'only-tweet-like-share-and-google-1/tweet-like-plusone.php',
 		                                                         // Tweet, Like, Google +1 and Share
-		'easy-facebook-share-thumbnails/esft.php',               // Easy Facebook Share Thumbnail
-		'2-click-socialmedia-buttons/2-click-socialmedia-buttons.php',
-		                                                         // 2 Click Social Media Buttons
-		'facebook-thumb-fixer/_facebook-thumb-fixer.php',        // Facebook Thumb Fixer
-		'zoltonorg-social-plugin/zosp.php',                      // Zolton.org Social Plugin
-		'wp-caregiver/wp-caregiver.php',                         // WP Caregiver
-		'facebook-revised-open-graph-meta-tag/index.php',        // Facebook Revised Open Graph Meta Tag
-		'facebook-and-digg-thumbnail-generator/facebook-and-digg-thumbnail-generator.php',
-		                                                         // Fedmich's Facebook Open Graph Meta
-		'facebook-meta-tags/facebook-metatags.php',              // Facebook Meta Tags
-		'wordpress-seo-premium/wp-seo-premium.php',              // WordPress SEO Premium by Yoast
+		'wordbooker/wordbooker.php',                             // Wordbooker
 		'wpsso/wpsso.php',                                       // WordPress Social Sharing Optimization
+		'wordpress-seo/wp-seo.php',                              // WordPress SEO by Yoast
+		'wordpress-seo-premium/wp-seo-premium.php',              // WordPress SEO Premium by Yoast
+		'wp-caregiver/wp-caregiver.php',                         // WP Caregiver
+		'wp-facebook-like-send-open-graph-meta/wp-facebook-like-send-open-graph-meta.php',
+		                                                         // WP Facebook Like Send & Open Graph Meta
+		'wp-facebook-open-graph-protocol/wp-facebook-ogp.php',   // WP Facebook Open Graph protocol
+		'wp-ogp/wp-ogp.php',                                     // WP-OGP
+		'zoltonorg-social-plugin/zosp.php',                      // Zolton.org Social Plugin
 	);
 
 	/**
