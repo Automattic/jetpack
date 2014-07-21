eb5a80f1f337e86452dc60a433888da679481fab
LA Watts <luke@thisis.la>
2014-07-21 11:04:06 +0100

    Top Posts Widget: Add filters for adding content before and after posts

--- modules/widgets/top-posts.php
+++ modules/widgets/top-posts.php
@@ -190,7 +190,9 @@ class Jetpack_Top_Posts_Widget extends WP_Widget {
 				foreach ( $posts as $post ) :
 				?>
 					<div class="widget-grid-view-image">
+					        <?php echo apply_filters( 'jetpack_widget_top_posts_before_post', '', $post['post_id'] ); ?> 
 						<a href="<?php echo esc_url( $post['permalink'] ); ?>" title="<?php echo esc_attr( wp_kses( $post['title'], array() ) ); ?>" class="bump-view" data-bump-view="tp"><img src="<?php echo esc_url( $post['image'] ); ?>" alt="<?php echo esc_attr( wp_kses( $post['title'], array() ) ); ?>" /></a>
+						<?php echo apply_filters( 'jetpack_widget_top_posts_after_post', '', $post['post_id'] ); ?> 
 					</div>
 
 				<?php
@@ -202,7 +204,11 @@ class Jetpack_Top_Posts_Widget extends WP_Widget {
 				?>
 					<li>
 						<img src="<?php echo esc_url( $post['image'] ); ?>" class='widgets-list-layout-blavatar' alt="<?php echo esc_attr( wp_kses( $post['title'], array() ) ); ?>" />
-						<div class="widgets-list-layout-links"><a href="<?php echo esc_url( $post['permalink'] ); ?>" class="bump-view" data-bump-view="tp"><?php echo esc_html( wp_kses( $post['title'], array() ) ); ?></a></div>
+						<div class="widgets-list-layout-links">
+						<?php echo apply_filters( 'jetpack_widget_top_posts_before_post', '', $post['post_id'] ); ?> 
+						<a href="<?php echo esc_url( $post['permalink'] ); ?>" class="bump-view" data-bump-view="tp"><?php echo esc_html( wp_kses( $post['title'], array() ) ); ?></a>
+						<?php echo apply_filters( 'jetpack_widget_top_posts_after_post', '', $post['post_id'] ); ?> 
+						</div>
 					</li>
 				<?php
 				endforeach;
@@ -211,9 +217,16 @@ class Jetpack_Top_Posts_Widget extends WP_Widget {
 			break;
 		default :
 			echo '<ul>';
-			foreach ( $posts as $post ) {
-				echo '<li><a href="' . esc_url( $post['permalink'] ) . '" class="bump-view" data-bump-view="tp">' . esc_html( wp_kses( $post['title'], array() ) ) . "</a></li>\n";
-			}
+			foreach ( $posts as $post ) :
+			?>
+				<li>
+					<?php echo apply_filters( 'jetpack_widget_top_posts_before_post', '', $post['post_id'] ) ?>
+					<a href="<?php echo esc_url( $post['permalink'] ); ?>" class="bump-view" data-bump-view="tp"><?php echo esc_html( wp_kses( $post['title'], array() ) ); ?></a>
+					<?php echo apply_filters( 'jetpack_widget_top_posts_after_post', '', $post['post_id'] ) ?>
+				</li>
+
+			<?php
+			endforeach;
 			echo '</ul>';
 		}
 
