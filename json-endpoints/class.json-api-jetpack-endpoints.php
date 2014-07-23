b629eee28dc82f70718dc1317f87d745b03de62e
Miguel Lezama <miguel@automattic.com>
2014-07-23 00:56:55 -0300

    Add check capability endpoint.
    + Move the capability check inside `validate_call` to the
    `check_capability` method.

--- json-endpoints/class.json-api-jetpack-endpoints.php
+++ json-endpoints/class.json-api-jetpack-endpoints.php
@@ -11,40 +11,54 @@ abstract class Jetpack_JSON_API_Endpoint extends WPCOM_JSON_API_Endpoint {
 	 */
 	protected function validate_call( $_blog_id, $capability, $check_full_management = true ) {
 		$blog_id = $this->api->switch_to_blog_and_validate_user( $this->api->get_blog_id( $_blog_id ) );
-		if ( is_wp_error( $blog_id ) )
+		if ( is_wp_error( $blog_id ) ) {
 			return $blog_id;
+		}
 
-		if( is_array ( $capability ) ) {
+		if ( is_wp_error( $error = $this->check_capability( $capability ) ) ) {
+			return $error;
+		}
+
+		if ( $check_full_management && ! Jetpack_Options::get_option( 'json_api_full_management' ) ) {
+			return new WP_Error( 'unauthorized_full_access', sprintf( __( 'Full management mode is off for this site.' , 'jetpack' ), $capability ), 403 );
+		}
+		return true;
+	}
+
+	/**
+	 * @param $capability
+	 *
+	 * @return bool|WP_Error
+	 */
+	protected function check_capability( $capability ) {
+		if ( is_array( $capability ) ) {
 			// the idea is that the we can pass in an array of capabilitie that the user needs to have before we allowing them to do something
 			$capabilities = ( isset( $capability['capabilities'] ) ? $capability['capabilities'] : $capability );
-			
-			// We can pass in the number of conditions we must pass by default it is all. 
+
+			// We can pass in the number of conditions we must pass by default it is all.
 			$must_pass = ( isset( $capability['must_pass'] ) && is_int( $capability['must_pass'] ) ? $capability['must_pass'] : count( $capabilities ) );
-			
+
 			$failed = array(); // store the failed capabilities
-			$passed = 0; // 
-			
-			foreach( $capabilities as $cap ) {
-				if( current_user_can( $cap ) ) {
-					$passed++;
+			$passed = 0; //
+
+			foreach ( $capabilities as $cap ) {
+				if ( current_user_can( $cap ) ) {
+					$passed ++;
 				} else {
 					$failed[] = $cap;
 				}
 			}
-			// Check that must have conditions is less then 
-			if( $passed < $must_pass ) {
-				return new WP_Error( 'unauthorized', sprintf( __( 'This user is not authorized to %s on this blog.' , 'jetpack' ), implode( ', ', $failed ), 403 ) );
+			// Check that must have conditions is less then
+			if ( $passed < $must_pass ) {
+				return new WP_Error( 'unauthorized', sprintf( __( 'This user is not authorized to %s on this blog.', 'jetpack' ), implode( ', ', $failed ), 403 ) );
 			}
 
 		} else {
-			if ( ! current_user_can( $capability ) ) {
-				return new WP_Error( 'unauthorized', sprintf( __( 'This user is not authorized to %s on this blog.' , 'jetpack' ), $capability ), 403 );
+			if ( !current_user_can( $capability ) ) {
+				return new WP_Error( 'unauthorized', sprintf( __( 'This user is not authorized to %s on this blog.', 'jetpack' ), $capability ), 403 );
 			}
 		}
-		
-		if ( $check_full_management && ! Jetpack_Options::get_option( 'json_api_full_management' ) ) {
-			return new WP_Error( 'unauthorized_full_access', sprintf( __( 'Full management mode is off for this site.' , 'jetpack' ), $capability ), 403 );
-		}
+
 		return true;
 	}
 
@@ -829,4 +843,54 @@ new Jetpack_JSON_API_GET_Update_Data( array(
 		),
 	),
 	'example_request' => 'https://public-api.wordpress.com/rest/v1/sites/example.wordpress.org/updates'
+) );
+
+
+// Jetpack Extras
+
+
+class Jetpack_Check_Capabilities_Endpoint extends Jetpack_JSON_API_Jetpack_Modules_Endpoint {
+	// GET /sites/%s/me/capability
+	public function callback( $path = '', $_blog_id = 0 ) {
+		// Check minimum capability and blog membership first
+		if ( is_wp_error( $error = $this->validate_call( $_blog_id, 'read', false ) ) ) {
+			return $error;
+		}
+
+		$args = $this->input();
+
+		if ( ! isset( $args['capability'] ) || empty( $args['capability'] ) ) {
+			return new WP_Error( 'missing_capability', __( 'You are required to specify a capability to check.', 'jetpack' ), 400 );
+		}
+
+		if ( is_wp_error( $this->check_capability( $args['capability'] ) ) ){
+			return false;
+		}
+
+		return true;
+	}
+}
+
+new Jetpack_Check_Capabilities_Endpoint( array(
+	'description'     => 'Check if the current user has a certain capability over a Jetpack site',
+	'group'           => 'jetpack',
+	'stat'            => 'jetpack:me:capability',
+	'method'          => 'GET',
+	'path'            => '/sites/%s/me/capability',
+	'path_labels' => array(
+		'$site' => '(int|string) The site ID, The site domain'
+	),
+	'response_format' => '(bool) True if the user has the queried capability.',
+	'example_request_data' => array(
+		'headers' => array(
+			'authorization' => 'Bearer YOUR_API_TOKEN'
+		),
+		'body' => array(
+			'capability' => array(
+				'capabilities' => array( 'manage_options', 'read' ),
+				'must_pass' => 'The number of capabilities needed to return True'
+			)
+		)
+	),
+	'example_request' => 'https://public-api.wordpress.com/rest/v1/sites/example.wordpress.org/me/capability'
 ) );
