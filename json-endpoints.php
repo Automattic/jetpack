8561146551174942d7b42dbf10eaef1091b9679c
Joey Kudish <jkudish@automattic.com>
2014-07-23 23:20:08 +0000

    REST API: add post thumbnail image object as part of the post response
    see https://[private link]
    
    Merges r101978-wpcom.

--- json-endpoints.php
+++ json-endpoints.php
@@ -489,6 +489,7 @@ new WPCOM_JSON_API_Update_Post_Endpoint( array(
 	"is_reblogged": false,
 	"is_following": false,
 	"featured_image": "",
+	"post_thumbnail": "",
 	"format": "standard",
 	"geo": false,
 	"publicize_URLs": [
@@ -599,6 +600,7 @@ new WPCOM_JSON_API_Update_Post_Endpoint( array(
 	"is_reblogged": false,
 	"is_following": false,
 	"featured_image": "",
+	"post_thumbnail": "",
 	"format": "standard",
 	"geo": false,
 	"publicize_URLs": [
