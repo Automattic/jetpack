69573c150c41affca4983bdc9688a9e8968df74d
George Stephanis <georgestephanis@automattic.com>
2014-07-31 10:36:29 -0400

    Minor tidying of whitespace, if syntax, & comments

--- modules/widgets/readmill.php
+++ modules/widgets/readmill.php
@@ -7,8 +7,8 @@ class Jetpack_Readmill_Widget extends WP_Widget {
 	 */
 	function __construct() {
 		parent::__construct(
-	 		'jetpack_readmill_widget', // Base ID
-	 		apply_filters( 'jetpack_widget_name', esc_html__( 'Retired: Send To Readmill', 'jetpack' ) ),
+			'jetpack_readmill_widget', // Base ID
+			apply_filters( 'jetpack_widget_name', esc_html__( 'Retired: Send To Readmill', 'jetpack' ) ),
 			array(
 				'description' => esc_html__( 'Readmill has closed its doors. http://readmill.com/', 'jetpack' ),
 			)
@@ -22,18 +22,19 @@ class Jetpack_Readmill_Widget extends WP_Widget {
 	 *
 	 * @param array $instance Previously saved values from database.
 	 */
- 	function form( $instance ) {
- 		?>
- 		<p><?php printf( str_replace( 'taget', 'target', __( "<strong>The Readmill reading service has shut down</strong><br /> <a taget=\"_blank\" href=\"%s\">Learn More</a>", 'jetpack' ) ), 'http://readmill.com' ); ?>. </p>
- 		<p style="color:#A00; ">
- 			<?php if( !defined( 'IS_WPCOM' ) || false == IS_WPCOM ) { ?>
+	function form( $instance ) {
+		// Temporary Fix for 3.1 so we don't break a translation string before release.
+		?>
+		<p><?php printf( str_replace( ' taget="', ' target="', __( "<strong>The Readmill reading service has shut down</strong><br /> <a taget=\"_blank\" href=\"%s\">Learn More</a>", 'jetpack' ) ), 'http://readmill.com' ); ?>. </p>
+		<p style="color:#A00; ">
+			<?php if ( ! defined( 'IS_WPCOM' ) || false == IS_WPCOM ) : ?>
 				<p><?php esc_html_e( 'The Send to Readmill widget is no longer working and will be removed completely from Jetpack.', 'jetpack' ); ?></p>
-	 			<?php } else { ?>
+			<?php else : ?>
 				<p><?php esc_html_e( 'The Send to Readmill widget is no longer working and will be removed completely.', 'jetpack' );  ?></p>
-	 			<?php } ?>
-	 		<em><?php esc_html_e( 'You can remove it yourself now.', 'jetpack' ); ?></em> 
- 			<?php esc_html_e( 'No content is displayed to users who can\'t manage widgets' , 'jetpack' ); ?>
- 		</p><?php
+			<?php endif; ?>
+			<em><?php esc_html_e( 'You can remove it yourself now.', 'jetpack' ); ?></em> 
+			<?php esc_html_e( 'No content is displayed to users who can\'t manage widgets' , 'jetpack' ); ?>
+		</p><?php
 	}
 
 	/**
@@ -62,16 +63,17 @@ class Jetpack_Readmill_Widget extends WP_Widget {
 	function widget( $args, $instance ) {
 		if( current_user_can( 'edit_theme_options' ) ) { ?>
 
-	 		<div style="border:1px solid #A00; padding:10px; margin:10px 0;color:#A00; background:#FFF; ">
-	 			<p><?php printf( str_replace( 'taget', 'target', __( "Notice to Administrators:<br /> <strong>The Readmill reading service has shut down</strong> <a taget=\"_blank\" href=\"%s\">learn more</a>.", 'jetpack' ) ), 'http://readmill.com/' );  ?></p>
-	 			<?php if( !defined( 'IS_WPCOM' ) || false == IS_WPCOM ) { ?>
-				<p><?php esc_html_e( 'The Send to Readmill widget is no longer working and will be removed completely from Jetpack.', 'jetpack' ); ?></p>
-	 			<?php } else { ?>
-				<p><?php esc_html_e( 'The Send to Readmill widget is no longer working and will be removed completely.', 'jetpack' ); ?></p>
-	 			<?php } ?>
-	 			<p><?php printf( __( "You can visit <a href=\"%s\" title=\"Appearance > Widgets\">Appearance > Widgets </a> to remove it from this sidebar.", 'jetpack' ) , esc_url( admin_url( 'widgets.php' ) ) ) ; ?></p>
-	 			<p><?php esc_html_e( 'This notice is only visible to logged in users that can manage widgets', 'jetpack' ); ?>.</p>
-	 		</div>
+			<div style="border:1px solid #A00; padding:10px; margin:10px 0;color:#A00; background:#FFF; ">
+				<?php /* Temporary Fix for 3.1 so we don't break a translation string before release. */ ?>
+				<p><?php printf( str_replace( ' taget="', ' target="', __( "Notice to Administrators:<br /> <strong>The Readmill reading service has shut down</strong> <a taget=\"_blank\" href=\"%s\">learn more</a>.", 'jetpack' ) ), 'http://readmill.com/' );  ?></p>
+				<?php if ( ! defined( 'IS_WPCOM' ) || false == IS_WPCOM ) : ?>
+					<p><?php esc_html_e( 'The Send to Readmill widget is no longer working and will be removed completely from Jetpack.', 'jetpack' ); ?></p>
+				<?php else : ?>
+					<p><?php esc_html_e( 'The Send to Readmill widget is no longer working and will be removed completely.', 'jetpack' ); ?></p>
+				<?php endif; ?>
+				<p><?php printf( __( "You can visit <a href=\"%s\" title=\"Appearance > Widgets\">Appearance > Widgets </a> to remove it from this sidebar.", 'jetpack' ) , esc_url( admin_url( 'widgets.php' ) ) ) ; ?></p>
+				<p><?php esc_html_e( 'This notice is only visible to logged in users that can manage widgets', 'jetpack' ); ?>.</p>
+			</div>
 		<?php
 		}
 	}
