a2785dd5608e8b5af32bd71fb8e73bdf8ef8711e
Michael Adams <mike@automattic.com>
2014-08-06 16:52:24 +0000

    JSON API: Make sure datetimes are always well formed.
    WordPress stores `date` and `date_gmt` fields for posts (same for `modified` and `modified_gmt`).
    
    The API's date formatting code uses both of these values to generate a datetimestring including the local time and a timezone offset for that post.
    
    For drafts, though, date_gmt is set to "0000-00-00 00:00:00", which is messing up the API's date formatting code.
    
    Dates were being output like: "2014-02-14T09:21:51+17656209:21".
    
    When the gmt field is missing like that, use the blog's timezone/offset option to calculate the correct offsite time.
    
    http://[private link]
    
    Merges r102805-wpcom.

--- class.json-api-endpoints.php
+++ class.json-api-endpoints.php
@@ -1113,12 +1113,38 @@ EOPHP;
 	 */
 	function format_date( $date_gmt, $date = null ) {
 		$timestamp_gmt = strtotime( "$date_gmt+0000" );
+
 		if ( null === $date ) {
 			$timestamp = $timestamp_gmt;
 			$hours     = $minutes = $west = 0;
 		} else {
-			$timestamp = strtotime( "$date+0000" );
-			$offset    = $timestamp - $timestamp_gmt;
+			$date_time = date_create( "$date+0000" );
+			if ( $date_time ) {
+				$timestamp = $date_time->getTimestamp();
+			} else {
+				$timestamp = 0;
+			}
+
+			// "0000-00-00 00:00:00" == -62169984000
+			if ( -62169984000 == $timestamp_gmt ) {
+				// WordPress sets post_date=now, post_date_gmt="0000-00-00 00:00:00" for all drafts
+				// WordPress sets post_modified=now, post_modified_gmt="0000-00-00 00:00:00" for new drafts
+
+				// Try to guess the correct offset from the blog's options.
+				$timezone_string = get_option( 'timezone_string' );
+
+				if ( $timezone_string && $date_time ) {
+					$timezone = timezone_open( $timezone_string );
+					if ( $timezone ) {
+						$offset = $timezone->getOffset( $date_time );
+					}
+				} else {
+					$offset = 3600 * get_option( 'gmt_offset' );
+				}
+			} else {
+				$offset = $timestamp - $timestamp_gmt;
+			}
+
 			$west      = $offset < 0;
 			$offset    = abs( $offset );
 			$hours     = (int) floor( $offset / 3600 );
