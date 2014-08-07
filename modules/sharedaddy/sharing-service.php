b5a538121d8d54fdcff9acc30f17b4869a2ceb03
Jeremy Herve <jeremy@tagada.hu>
2014-08-07 18:01:41 +0100

    Sharedaddy: align statements

--- modules/sharedaddy/sharing-service.php
+++ modules/sharedaddy/sharing-service.php
@@ -400,10 +400,10 @@ function sharing_register_post_for_share_counts( $post_id ) {
 }
 
 function sharing_maybe_enqueue_scripts() {
-	$sharer = new Sharing_Service();
-	$global  = $sharer->get_global_options();
+	$sharer      = new Sharing_Service();
+	$global      = $sharer->get_global_options();
 
-	$enqueue = false;
+	$enqueue     = false;
 	if ( is_singular() && in_array( get_post_type(), $global['show'] ) ) {
 		$enqueue = true;
 	} elseif ( in_array( 'index', $global['show'] ) && ( is_home() || is_archive() || is_search() || in_array( get_post_type(), $global['show'] ) ) ) {
