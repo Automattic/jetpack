5319095095028e4b1e307d123d4bbd7af19065d3
iannuttall <me@iannuttall.co.uk>
2014-08-07 11:57:15 +0100

    Added additional random parameters Fixes #867
    Made changes as per @enejb's comments.

--- modules/theme-tools/random-redirect.php
+++ modules/theme-tools/random-redirect.php
@@ -17,7 +17,19 @@ function jetpack_matt_random_redirect() {
 	}
 
 	// Set default post type.
-	$post_type = 'post';
+		$post_type = get_post_type();
+
+	// Set default category type
+		if ( is_category() ) {
+			$category = get_the_category();
+			$random_cat_id = $category[0]->term_id;
+		}
+
+	// Set author name if we're on an author archive.
+		if ( is_author() ) {
+			$random_author_name = get_the_author_meta( 'user_login' );
+			$random_author_query = 'AND user_login = "' . $random_author_name . '"';
+		}
 
 	// Acceptable URL formats: /[...]/?random=[post type], /?random, /&random, /&random=1
 	if ( ! isset( $_GET['random'] ) && ! in_array( strtolower( $_SERVER['REQUEST_URI'] ), array( '/&random', '/&random=1' ) ) )
@@ -36,20 +48,15 @@ function jetpack_matt_random_redirect() {
 		$random_cat_id = (int) $_GET['random_cat_id'];
 
 	// Change the post type if the parameter is set.
-	if ( isset( $_GET['random_post_type'] ) ) 
-		$post_type = preg_replace( '|[^a-z]|i', '', $_GET['random_post_type'] );
-
-	// If it's not a static home page or a specified post type, use the post type of the current page as the context for the random lookup.
-	if ( ! is_front_page() && ! isset( $_GET['random_post_type'] ) ) {
-		$post_type = get_post_type();
-	}
+	if ( isset( $_GET['random_post_type'] ) && post_type_exists( $_GET['random_post_type'] ) ) 
+		$post_type = $_GET['random_post_type'];
 
 	global $wpdb;
 
-	if ( isset( $_GET['random_cat_id'] ) ) {
-		$random_id = $wpdb->get_var( $wpdb->prepare( "SELECT DISTINCT ID FROM $wpdb->posts AS p INNER JOIN $wpdb->term_relationships AS tr ON (p.ID = tr.object_id AND tr.term_taxonomy_id = %s) INNER JOIN  $wpdb->term_taxonomy AS tt ON(tr.term_taxonomy_id = tt.term_taxonomy_id AND taxonomy = 'category') WHERE post_type = %s AND post_password = '' AND post_status = 'publish' ORDER BY RAND() LIMIT 1", $random_cat_id, $post_type ) );
+	if ( isset( $random_cat_id ) ) {
+		$random_id = $wpdb->get_var( $wpdb->prepare( "SELECT DISTINCT ID FROM $wpdb->posts AS p INNER JOIN $wpdb->term_relationships AS tr ON (p.ID = tr.object_id AND tr.term_taxonomy_id = %s) INNER JOIN  $wpdb->term_taxonomy AS tt ON(tr.term_taxonomy_id = tt.term_taxonomy_id AND taxonomy = 'category') WHERE p.post_type = %s AND post_password = '' AND post_status = 'publish' %s ORDER BY RAND() LIMIT 1", $random_cat_id, $post_type, $random_author_query ) );
 	} else {
-		$random_id = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_type = %s AND post_password = '' AND post_status = 'publish' ORDER BY RAND() LIMIT 1", $post_type ) );
+		$random_id = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_type = %s AND post_password = '' AND post_status = 'publish' %s ORDER BY RAND() LIMIT 1", $post_type, $random_author_query ) );
 	}
 	
 	$permalink = get_permalink( $random_id );
