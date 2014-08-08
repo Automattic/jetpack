c375fb2bcf6b88ca6840673c5975ead7f8ef8f07
MichaelArestad <marestad@gmail.com>
2014-08-08 12:46:29 +0100

    Fixed font links and `jetpack-icons.css` paths

--- class.jetpack.php
+++ class.jetpack.php
@@ -560,9 +560,8 @@ class Jetpack {
 		require_once( JETPACK__PLUGIN_DIR . '_inc/genericons.php' );
 		jetpack_register_genericons();
 
-		if ( ! wp_style_is( 'jetpack-icons', 'registered' ) ) {
-			wp_register_style( 'jetpack-icons', plugins_url( '_inc/jetpack-icons.min.css', JETPACK__PLUGIN_FILE ), false, JETPACK__VERSION );
-		}
+		if ( ! wp_style_is( 'jetpack-icons', 'registered' ) )
+			wp_register_style( 'jetpack-icons', plugins_url( 'css/jetpack-icons.min.css', JETPACK__PLUGIN_FILE ), false, JETPACK__VERSION );
 	}
 
 	/**
