e5277c64254d5335732443b83aa5ba7c55db2948
MichaelArestad <marestad@gmail.com>
2014-08-08 12:42:13 +0100

    Combined `img/` with `images/' and moved `images/` to root

--- class.jetpack.php
+++ class.jetpack.php
@@ -3106,7 +3106,7 @@ p {
 			</div>
 
 			<div id="jetpack-configuration" style="display:none;">
-				<p><img width="16" src="<?php echo esc_url( plugins_url( '_inc/images/wpspin_light-2x.gif', __FILE__ ) ); ?>" alt="Loading ..." /></p>
+				<p><img width="16" src="<?php echo esc_url( plugins_url( 'images/wpspin_light-2x.gif', __FILE__ ) ); ?>" alt="Loading ..." /></p>
 			</div>
 		</div>
 	<?php
