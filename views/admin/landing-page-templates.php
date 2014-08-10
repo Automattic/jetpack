9249d4c39194ad36eb6bb098a90b75ad650adfd0
Jeremy Herve <jeremy@tagada.hu>
2014-08-10 14:53:15 +0000

    Module description: add context to "Paid" to avoid confusion
    This should help translators find out how to best translate "Paid" in this context.

--- views/admin/landing-page-templates.php
+++ views/admin/landing-page-templates.php
@@ -13,7 +13,7 @@
 </script>
 <script id="tmpl-mod" type="text/html">
 	<div href="{{ data.url }}" data-index="{{ data.index }}" data-name="{{ data.name }}" class="module{{ ( data.new ) ? ' new' : '' }}">
-		<h3 class="icon {{ data.module }}">{{{ data.name }}}<# if ( ! data.free ) { #><span class="paid"><?php esc_html_e( 'Paid', 'jetpack' ); ?></span><# } #></h3>
+		<h3 class="icon {{ data.module }}">{{{ data.name }}}<# if ( ! data.free ) { #><span class="paid"><?php echo esc_html_x( 'Paid', 'As in Premium. Premium module description', 'jetpack' ); ?></span><# } #></h3>
 		<p>{{{ data.short_description }}}</p>
 	</div>
 </script>
