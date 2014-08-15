94771280221420a0870eb51072f01ccd0eb6f225
Tony Kovanen <tonykovanen@hotmail.com>
2014-08-15 16:00:53 +0300

    Tiled Galleries: Fix indentation.
    Indent div further.

--- modules/tiled-gallery/tiled-gallery/templates/square-layout.php
+++ modules/tiled-gallery/tiled-gallery/templates/square-layout.php
@@ -1,8 +1,8 @@
 <?php foreach ( $rows as $row ): ?>
-<div class="tiled-gallery-row"
-	data-original-width="<?php echo esc_attr( $row->width ); ?>"
-	data-original-height="<?php echo esc_attr( $row->height ); ?>"
->
+	<div class="tiled-gallery-row"
+		data-original-width="<?php echo esc_attr( $row->width ); ?>"
+		data-original-height="<?php echo esc_attr( $row->height ); ?>"
+	>
 		<?php $add_link = 'none' !== $link; ?>
 		<?php foreach ( $row->images as $item ): ?>
 			<div class="tiled-gallery-group"
