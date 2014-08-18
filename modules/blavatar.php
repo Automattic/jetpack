45a50fea36c87f6fa6a65772c01b91a8667edece
enej <enej.bajgoric@gmail.com>
2014-08-18 16:20:32 -0700

    Blavatar: Adding the module so that it shows up in the admin

---
+++ modules/blavatar.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Module Name: Blavatar
+ * Module Description: Add an avatar to your site.
+ * Sort Order: 22
+ * First Introduced: 3.2
+ * Requires Connection: No
+ * Auto Activate: No
+ * Module Tags: Other
+ */
+
+include dirname( __FILE__ ) . '/blavatar/jetpack-blavatar.php';
