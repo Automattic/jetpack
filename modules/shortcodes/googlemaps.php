619e921a3a438dfcbe61dd16abdcd3d5e845d970
George Stephanis <george@stephanis.info>
2014-08-27 14:08:03 -0400

    Use absint() rather than (int)
    This is a bit more explicit that it’s only applying to the first bit
    before the string concatenation `.` — also better handles incoming
    negative numbers (which shouldn’t ever happen, but paranoia)

--- modules/shortcodes/googlemaps.php
+++ modules/shortcodes/googlemaps.php
@@ -28,14 +28,14 @@ function jetpack_googlemaps_embed_to_short_code_callback( $match ) {
 
 	if ( preg_match( '/\bwidth=[\'"](\d+)(%)?/', $match[0], $width ) ) {
 		$percent = ! empty( $width[2] ) ? '%' : '';
-		$width = (int)$width[1] . $percent;
+		$width = absint( $width[1] ) . $percent;
 	} else {
 		$width = 425;
 	}
 
 	if ( preg_match( '/\bheight=[\'"](\d+)(%)?/', $match[0], $height ) ) {
 		$percent = ! empty( $height[2] ) ? '%' : '';
-		$height = (int)$height[1] . $percent;
+		$height = absint( $height[1] ) . $percent;
 	} else {
 		$height = 350;
 	}
