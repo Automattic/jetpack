8d73da42835fb732523a3b90324c100fd80e067a
George Stephanis <george@stephanis.info>
2014-08-28 14:33:36 -0400

    Create Protect prototype module.

---
+++ modules/protect.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Module Name: Protect
+ * Module Description: Protect your site from brute-force attacks that want to have their way with your data.
+ * Sort Order: 28
+ * First Introduced: 3.9
+ * Requires Connection: Yes
+ * Auto Activate: Yes
+ * Module Tags: Security
+ */
+
