3da6e4df8aa5f10b3c7c8e8c05e4a7a4dc451950
Andrew Duthie <andrew.duthie@automattic.com>
2014-09-05 20:42:24 +0000

    REST API: Bypass looped call to get term details to avoid incurring additional queries
    The detail provided by get_term_by is the same as what would be returned by get_taxonomy and wp_get_post_categories, so there's no need to make these calls. This should have a sign$
    
    Related to r104721
    
    Merges r104722-wpcom.

--- json-endpoints/class.wpcom-json-api-post-endpoint.php
+++ json-endpoints/class.wpcom-json-api-post-endpoint.php
@@ -393,18 +393,17 @@ abstract class WPCOM_JSON_API_Post_Endpoint extends WPCOM_JSON_API_Endpoint {
 				$terms = wp_get_post_tags( $post->ID );
 				foreach ( $terms as $term ) {
 					if ( !empty( $term->name ) ) {
-						$response[$key][$term->name] = $this->get_taxonomy( $term->slug, 'post_tag', $context );
+						$response[$key][$term->name] = $this->format_taxonomy( $term, 'post_tag', $context );
 					}
 				}
 				$response[$key] = (object) $response[$key];
 				break;
 			case 'categories':
 				$response[$key] = array();
-				$terms = wp_get_post_categories( $post->ID );
+				$terms = wp_get_object_terms( $post->ID, 'category', array( 'fields' => 'all' ) );
 				foreach ( $terms as $term ) {
-					$category = $taxonomy = get_term_by( 'id', $term, 'category' );
-					if ( !empty( $category->name ) ) {
-						$response[$key][$category->name] = $this->format_taxonomy( $category, 'category', $context );
+					if ( !empty( $term->name ) ) {
+						$response[$key][$term->name] = $this->format_taxonomy( $term, 'category', $context );
 					}
 				}
 				$response[$key] = (object) $response[$key];
