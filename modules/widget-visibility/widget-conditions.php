19885bf9cc7b95feab55fc4d47a030d027c859d6
enej <enej.bajgoric@gmail.com>
2014-09-25 09:42:14 -0700

    Widget Visibility: Removes some PHP Notice Errors.

--- modules/widget-visibility/widget-conditions.php
+++ modules/widget-visibility/widget-conditions.php
@@ -457,9 +457,10 @@ class Jetpack_Widget_Conditions {
 				case 'taxonomy':
 					$term = explode( '_tax_', $rule['minor'] ); // $term[0] = taxonomy name; $term[1] = term id
 					$terms = get_the_terms( $post->ID, $rule['minor'] ); // Does post have terms in taxonomy?
-					if ( is_tax( $term[0], $term[1] ) )
+					
+					if ( isset( $term[1] ) && is_tax( $term[0], $term[1] ) )
 						$condition_result = true;
-					else if ( is_singular() && $term[1] && has_term( $term[1], $term[0] ) )
+					else if ( isset( $term[1] ) && is_singular() && $term[1] && has_term( $term[1], $term[0] ) )
 						$condition_result = true;
 					else if ( is_singular() && $terms & !is_wp_error( $terms ) )
 						$condition_result = true;
