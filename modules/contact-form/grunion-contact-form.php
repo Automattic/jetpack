92f779724c8e5f748fce7a32a6192397e45137d5
Tony Kovanen <tonykovanen@hotmail.com>
2014-09-25 16:58:27 +0300

    Contact Form: Register field shortcode permanently
    The activeness of the parsing is toggled via a variable that is set to true whenever the shortcode is needed and false when it is not (and is parsed to empty string). Requires some reorganization to decouple the field parsing from the form instance.

--- modules/contact-form/grunion-contact-form.php
+++ modules/contact-form/grunion-contact-form.php
@@ -20,10 +20,13 @@ if ( is_admin() )
  * Sets up various actions, filters, post types, post statuses, shortcodes.
  */
 class Grunion_Contact_Form_Plugin {
+
 	/**
 	 * @var string The Widget ID of the widget currently being processed.  Used to build the unique contact-form ID for forms embedded in widgets.
 	 */
 	var $current_widget_id;
+
+	static $using_contact_form_field = false;
 
 	static function init() {
 		static $instance = false;
@@ -228,11 +231,11 @@ class Grunion_Contact_Form_Plugin {
 	}
 	/*
 	 * Adds our contact-form shortcode
-	 * The "child" contact-field shortcode is added as needed by the contact-form shortcode handler
+	 * The "child" contact-field shortcode is enabled as needed by the contact-form shortcode handler
 	 */
 	function add_shortcode() {
 		add_shortcode( 'contact-form',         array( 'Grunion_Contact_Form', 'parse' ) );
-		add_filter( 'no_texturize_shortcodes', array( 'Grunion_Contact_Form', 'no_texturize' ) );
+		add_shortcode( 'contact-field',        array( 'Grunion_Contact_Form', 'parse_contact_field' ) );
 	}
 
 	static function tokenize_label( $label ) {
@@ -303,10 +306,12 @@ class Grunion_Contact_Form_Plugin {
 
 		$old = $GLOBALS['shortcode_tags'];
 		remove_all_shortcodes();
+		Grunion_Contact_Form_Plugin::$using_contact_form_field = true;
 		$this->add_shortcode();
 
 		$text = do_shortcode( $text );
 
+		Grunion_Contact_Form_Plugin::$using_contact_form_field = false;
 		$GLOBALS['shortcode_tags'] = $old;
 
 		return $text;
@@ -827,6 +832,11 @@ class Grunion_Contact_Form extends Crunion_Contact_Form_Shortcode {
 	static $last;
 
 	/**
+	 * @var Whatever form we are currently looking at. If processed, will become $last
+	 */
+	static $current_form;
+
+	/**
 	 * @var bool Whether to print the grunion.css style when processing the contact-form shortcode
 	 */
 	static $style = false;
@@ -849,6 +859,9 @@ class Grunion_Contact_Form extends Crunion_Contact_Form_Shortcode {
 			$default_to = $post_author->user_email;
 		}
 
+		// Keep reference to $this for parsing form fields
+		self::$current_form = $this;
+
 		$this->defaults = array(
 			'to'                 => $default_to,
 			'subject'            => $default_subject,
@@ -860,8 +873,8 @@ class Grunion_Contact_Form extends Crunion_Contact_Form_Shortcode {
 
 		$attributes = shortcode_atts( $this->defaults, $attributes, 'contact-form' );
 
-		// We only add the contact-field shortcode temporarily while processing the contact-form shortcode
-		add_shortcode( 'contact-field', array( $this, 'parse_contact_field' ) );
+		// We only enable the contact-field shortcode temporarily while processing the contact-form shortcode
+		Grunion_Contact_Form_Plugin::$using_contact_form_field = true;
 
 		parent::__construct( $attributes, $content );
 
@@ -885,7 +898,7 @@ class Grunion_Contact_Form extends Crunion_Contact_Form_Shortcode {
 		}
 
 		// $this->body and $this->fields have been setup.  We no longer need the contact-field shortcode.
-		remove_shortcode( 'contact-field' );
+		Grunion_Contact_Form_Plugin::$using_contact_form_field = false;
 	}
 
 	/**
@@ -907,17 +920,6 @@ class Grunion_Contact_Form extends Crunion_Contact_Form_Shortcode {
 	 */
 	static function _style_on() {
 		return self::style( true );
-	}
-
-	/**
-	 * Do not run the Contact form shortcode through wptexturize()
-	 *
-	 * @param array $shortcodes Key => An array of shortcodes to exempt from texturizations
-	 * @return array Contact Form shortcode
-	 */
-	static function no_texturize( $shortcodes ) {
-		$shortcodes[] = 'contact-form';
-		return $shortcodes;
 	}
 
 	/**
@@ -1101,20 +1103,27 @@ class Grunion_Contact_Form extends Crunion_Contact_Form_Shortcode {
 	 * @param string|null $content The shortcode's inner content: [contact-field]$content[/contact-field]
 	 * @return HTML for the contact form field
 	 */
-	function parse_contact_field( $attributes, $content ) {
-		$field = new Grunion_Contact_Form_Field( $attributes, $content, $this );
+	static function parse_contact_field( $attributes, $content ) {
+		// Don't try to parse contact form fields if not inside a contact form
+		if ( ! Grunion_Contact_Form_Plugin::$using_contact_form_field ) {
+			return '';
+		}
+
+		$form = Grunion_Contact_Form::$current_form;
+
+		$field = new Grunion_Contact_Form_Field( $attributes, $content, $form );
 
 		$field_id = $field->get_attribute( 'id' );
 		if ( $field_id ) {
-			$this->fields[$field_id] = $field;
+			$form->fields[$field_id] = $field;
 		} else {
-			$this->fields[] = $field;
+			$form->fields[] = $field;
 		}
 
 		if (
 			isset( $_POST['action'] ) && 'grunion-contact-form' === $_POST['action']
 		&&
-			isset( $_POST['contact-form-id'] ) && $this->get_attribute( 'id' ) == $_POST['contact-form-id']
+			isset( $_POST['contact-form-id'] ) && $form->get_attribute( 'id' ) == $_POST['contact-form-id']
 		) {
 			// If we're processing a POST submission for this contact form, validate the field value so we can show errors as necessary.
 			$field->validate();
