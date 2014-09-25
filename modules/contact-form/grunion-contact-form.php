60a7c509e834c6bc53b36b3587b1f9f45fb57761
Tony Kovanen <tonykovanen@hotmail.com>
2014-09-26 00:19:14 +0300

    Contact Form: enhance unparsed contact-fields.
    - Make sure everything is escaped with an `esc_html` call on the return value.
    - Leave the value part out of shortcode atts that do not have one
    - Leave the "trailing" closing tag when there is no content

--- modules/contact-form/grunion-contact-form.php
+++ modules/contact-form/grunion-contact-form.php
@@ -1108,9 +1108,22 @@ class Grunion_Contact_Form extends Crunion_Contact_Form_Shortcode {
 		if ( ! Grunion_Contact_Form_Plugin::$using_contact_form_field ) {
 			$att_strs = array();
 			foreach ( $attributes as $att => $val ) {
-				$att_strs[] = $att . '=\'' . $val . '\'';
+				if ( is_numeric( $att ) ) { // Is a valueless attribute
+					$att_strs[] = $val;
+				} else if ( isset( $val ) ) { // A regular attr - value pair
+					$att_strs[] = $att . '=\'' . $val . '\'';
+				}
 			}
-			return '[contact-field ' . implode( ' ', $att_strs ) . ']' . $content . '[/contact-field]';
+
+			$html = '[contact-field ' . implode( ' ', $att_strs );
+
+			if ( isset( $content ) && ! empty( $content ) ) { // If there is content, let's add a closing tag
+				$html .=  ']' . $content . '[/contact-field]';
+			} else { // Otherwise let's add a closing slash in the first tag
+				$html .= '/]';
+			}
+
+			return esc_html( $html );
 		}
 
 		$form = Grunion_Contact_Form::$current_form;
