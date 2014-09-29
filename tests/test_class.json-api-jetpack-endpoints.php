8880bad2cb7d2c67d82ed2e4a8bb086ed0e02913
Miguel Lezama <miguel@automattic.com>
2014-09-29 11:32:06 -0300

    Add Update Core Test
    Leaving it commented as it could change the state of the site.

--- tests/test_class.json-api-jetpack-endpoints.php
+++ tests/test_class.json-api-jetpack-endpoints.php
@@ -96,6 +96,70 @@ class WP_Test_Jetpack_Json_Api_endpoints extends WP_UnitTestCase {
 
 	}
 
+//	/**
+//	 * @author lezama
+//	 * @covers Jetpack_JSON_API_Core_Update_Endpoint
+//   * WARNING: this test downgrades and upgrades core, things could go wrong :)
+//	 */
+//	public function test_Jetpack_JSON_API_Core_Update_Endpoint() {
+//
+//		$endpoint = new Jetpack_JSON_API_Core_Update_Endpoint( array(
+//			'description'     => 'Update WordPress installation on a Jetpack blog',
+//			'method'          => 'POST',
+//			'path'            => '/sites/%s/core/update',
+//			'stat'            => 'core:update',
+//			'path_labels' => array(
+//				'$site' => '(int|string) The site ID, The site domain'
+//			),
+//			'request_format' => array(
+//				'version'   => '(string) The core version to update',
+//			),
+//			'response_format' => array(
+//				'version' => '(string) The core version after the upgrade has run.',
+//				'log'     => '(array:safehtml) An array of log strings.',
+//			),
+//			'example_request_data' => array(
+//				'headers' => array(
+//					'authorization' => 'Bearer YOUR_API_TOKEN'
+//				),
+//			),
+//			'example_request' => 'https://public-api.wordpress.com/rest/v1/sites/example.wordpress.org/core/update'
+//		) );
+//
+//		/**
+//		 * Changes the Accessibility of the protected update_core method.
+//		 */
+//		$class = new ReflectionClass('Jetpack_JSON_API_Core_Update_Endpoint');
+//		$update_core_method = $class->getMethod( 'update_core' );
+//		$update_core_method->setAccessible( true );
+//
+//		$version_file = ABSPATH . WPINC . '/version.php';
+//
+//		include( ABSPATH . WPINC . '/version.php' ); // $wp_version; // x.y.z
+//
+//		$current_version = floatval( implode( '.', array_slice( preg_split( '/[.-]/', $wp_version  ), 0, 2 ) ) );
+//		$previous_version = number_format( $current_version - 0.1, 1 );
+//
+//		$lines = file( $version_file );
+//		$file_contents = '';
+//		foreach( $lines as $line ) {
+//			// Downgrade the $wp_version number.
+//			if( strpos( $line, '$wp_version' ) === 0 )
+//				$line = '$wp_version = ' . $previous_version . ';' . "\n";
+//			$file_contents .= $line;
+//		}
+//		file_put_contents( $version_file, $file_contents );
+//
+//		$locale = get_locale();
+//
+//		// Invoke the update_core method.
+//
+//		$result = $update_core_method->invokeArgs( $endpoint, array( false, $locale ) );
+//
+//		$this->assertEquals( number_format( round( floatval( $result ), 1 ), 1 ),  number_format( $current_version, 1 ) );
+//
+//	}
+
 	function filesystem_method_direct( $method ) {
 
 		return 'direct';
