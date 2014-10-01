384f8eedc7f7a87776d2523b672622732649bebe
William Sullivan <william@enkrates.com>
2014-10-01 16:16:08 -0500

    Changing priority in filter to allow overrides
    prioritize_creator_over_default_site was previously set at a priority of -99, which would disallow other code to take its place.
    In fact, it should be allowed to be overridden very liberally and the priority has been changed to 99.

--- class.jetpack-twitter-cards.php
+++ class.jetpack-twitter-cards.php
@@ -208,7 +208,7 @@ class Jetpack_Twitter_Cards {
 		add_filter( 'jetpack_open_graph_tags',        array( __CLASS__, 'twitter_cards_tags' ) );
 		add_filter( 'jetpack_open_graph_output',      array( __CLASS__, 'twitter_cards_output' ) );
 		add_filter( 'jetpack_twitter_cards_site_tag', array( __CLASS__, 'site_tag' ), -99 );
-		add_filter( 'jetpack_twitter_cards_site_tag', array( __CLASS__, 'prioritize_creator_over_default_site' ), -99 );
+		add_filter( 'jetpack_twitter_cards_site_tag', array( __CLASS__, 'prioritize_creator_over_default_site' ), 99 );
 		add_action( 'admin_init',                     array( __CLASS__, 'settings_init' ) );
 		add_action( 'sharing_global_options',         array( __CLASS__, 'sharing_global_options' ) );
 		add_action( 'sharing_admin_update',           array( __CLASS__, 'settings_validate' ) );
