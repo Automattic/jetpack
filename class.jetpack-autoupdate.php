e88eff785fad58570376889517f6b39f45b1c0cb
Rocco Tripaldi <tripaldir@gmail.com>
2014-10-08 14:38:59 -0400

    testing the plugin update filter and it's working!

--- class.jetpack-autoupdate.php
+++ class.jetpack-autoupdate.php
@@ -16,18 +16,18 @@ class Jetpack_Autoupdate {
 
 	private function __construct() {
 		$this->updates_allowed = Jetpack_Options::get_option( 'json_api_full_management', false );
+
 		if( $this->updates_allowed ) {
-			add_filter( 'auto_update_plugin',  array( $this, 'autoupdate_plugin' ), 10, 2 );
-			add_filter( 'auto_update_theme',   array( $this, 'autoupdate_theme' ), 10, 2 );
-			add_filter( 'auto_update_core',    array( $this, 'autoupdate_core' ), 10, 2 );
+			add_filter( 'auto_update_plugin',  array( &$this, 'autoupdate_plugin' ), 10, 2 );
+			add_filter( 'auto_update_theme',   array( &$this, 'autoupdate_theme' ), 10, 2 );
+			add_filter( 'auto_update_core',    array( &$this, 'autoupdate_core' ), 10, 2 );
 		}
 	}
 
 	function autoupdate_plugin( $update, $item ) {
 		$autoupdate_plugin_list = Jetpack_Options::get_option( 'autoupdate_plugins', array() );
-
 		if( in_array( $item->plugin, $autoupdate_plugin_list ) ) {
-			return $true;
+			return true;
 		}
 
 		return $update;
