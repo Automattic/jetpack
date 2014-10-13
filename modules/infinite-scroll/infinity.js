c467e7464c9640172860c624123e2fef5a7aa0bc
Enej Bajgoric <enej.bajgoric@automattic.com>
2014-10-13 23:34:03 +0000

    infinity.js : Resolve Divergence from Jetpack
    This patch teaches the infinite scroll system how to pull DOM nodes out of
    nodes outside the current viewport and stash them in a cache. It does this
    by hooking into determineURL and walking all of the pages that were determined
    to be hidden. It pulls all of the child nodes out of the page and stashes them
    in a DocumentFragment. It also updates the min-height of the page wrapper to
    match the old height when all the children were present.
    
    When a user scrolls a page back into view, the children are reinserted into
    the wrapper and the min-height and class marker are removed.
    
    See https://github.com/Automattic/jetpack/pull/300
    
    But also fixes : https://[private link]
    
    The cache that was stored did not work with lazy load or any other plugin that fires on the 'post-load' event.
    Since the post load wasn't fired every time the content was added or re-added to the dom.
    Now we store the html also right before we take it away because it might have been altered by the post-load event.
    
    Another  optimization that was done to fire the change url  in the same timer as the the refresh() since this will only create one times instead of multiple once.
    
    Merges r106807-wpcom.

--- modules/infinite-scroll/infinity.js
+++ modules/infinite-scroll/infinity.js
@@ -1,13 +1,17 @@
 (function($){ // Open closure
-
 // Local vars
-var Scroller, ajaxurl, stats, type, text, totop, timer;
+var Scroller, ajaxurl, stats, type, text, totop;
 
 // IE requires special handling
 var isIE = ( -1 != navigator.userAgent.search( 'MSIE' ) );
 if ( isIE ) {
 	var IEVersion = navigator.userAgent.match(/MSIE\s?(\d+)\.?\d*;/);
 	var IEVersion = parseInt( IEVersion[1] );
+}
+
+// HTTP ajaxurl when site is HTTPS causes Access-Control-Allow-Origin failure in Desktop and iOS Safari
+if ( "https:" == document.location.protocol ) {
+	infiniteScroll.settings.ajaxurl = infiniteScroll.settings.ajaxurl.replace( "http://", "https://" );
 }
 
 /**
@@ -34,6 +38,7 @@ Scroller = function( settings ) {
 	this.google_analytics = settings.google_analytics;
 	this.history          = settings.history;
 	this.origURL          = window.location.href;
+	this.pageCache        = {};
 
 	// Footer settings
 	this.footer           = $( '#infinite-footer' );
@@ -65,8 +70,9 @@ Scroller = function( settings ) {
 				self.thefooter();
 				// Fire the refresh
 				self.refresh();
+                self.determineURL(); // determine the url 
 			}
-		}, 300 );
+		}, 250 );
 
 		// Ensure that enough posts are loaded to fill the initial viewport, to compensate for short posts and large displays.
 		self.ensureFilledViewport();
@@ -116,7 +122,6 @@ Scroller.prototype.render = function( response ) {
 
 	// Check if we can wrap the html
 	this.element.append( response.html );
-
 	this.body.trigger( 'post-load', response );
 	this.ready = true;
 };
@@ -309,6 +314,9 @@ Scroller.prototype.refresh = function() {
 					} );
 				}
 
+				// stash the response in the page cache
+                self.pageCache[self.page] = response;
+
 				// Increment the page number
 				self.page++;
 
@@ -424,7 +432,7 @@ Scroller.prototype.initializeMejs = function( ev, response ) {
 Scroller.prototype.ensureFilledViewport = function() {
 	var	self = this,
 	   	windowHeight = self.window.height(),
-	   	postsHeight = self.element.height()
+	   	postsHeight = self.element.height(),
 	   	aveSetHeight = 0,
 	   	wrapperQty = 0;
 
@@ -473,11 +481,12 @@ Scroller.prototype.checkViewportOnLoad = function( ev ) {
  * Identify archive page that corresponds to majority of posts shown in the current browser window.
  */
 Scroller.prototype.determineURL = function () {
-	var self         = window.infiniteScroll.scroller,
+	var self         = this,
 		windowTop    = $( window ).scrollTop(),
 		windowBottom = windowTop + $( window ).height(),
 		windowSize   = windowBottom - windowTop,
 		setsInView   = [],
+		setsHidden   = [],
 		pageNum      = false;
 
 	// Find out which sets are in view
@@ -489,7 +498,7 @@ Scroller.prototype.determineURL = function () {
 			setPageNum = $( this ).data( 'page-num' );
 
 		// Account for containers that have no height because their children are floated elements.
-		if ( 0 == setHeight ) {
+		if ( 0 === setHeight ) {
 			$( '> *', this ).each( function() {
 				setHeight += $( this ).outerHeight( false );
 			} );
@@ -507,8 +516,37 @@ Scroller.prototype.determineURL = function () {
 		}
 		else if( setBottom > windowTop && setBottom < windowBottom ) { // bottom of set is between top (gt) and bottom (lt)
 			setsInView.push({'id': id, 'top': setTop, 'bottom': setBottom, 'pageNum': setPageNum });
+		} else {
+			setsHidden.push({'id': id, 'top': setTop, 'bottom': setBottom, 'pageNum': setPageNum });
 		}
 	} );
+
+    $.each(setsHidden, function() {
+		var $set = $('#' + this.id);
+		if( $set.hasClass( 'is--replaced' ) ) {
+			return;
+		}
+
+        self.pageCache[ this.pageNum].html = $set.html();
+
+		$set.css('min-height', ( this.bottom - this.top ) + 'px' )
+		    .addClass('is--replaced')
+		    .empty();
+	});
+
+	$.each(setsInView, function() {
+		var $set = $('#' + this.id);
+
+		if( $set.hasClass('is--replaced') ) {
+			$set.css('min-height', '').removeClass('is--replaced');
+			if( this.pageNum in self.pageCache ) {
+				$set.html( self.pageCache[this.pageNum].html );
+                self.body.trigger( 'post-load', self.pageCache[this.pageNum] );
+
+			}
+		}
+
+	});
 
 	// Parse number of sets found in view in an attempt to update the URL to match the set that comprises the majority of the window.
 	if ( 0 == setsInView.length ) {
@@ -568,6 +606,10 @@ Scroller.prototype.determineURL = function () {
  * Checks if URL is different to prevent pollution of browser history.
  */
 Scroller.prototype.updateURL = function( page ) {
+	// IE only supports pushState() in v10 and above, so don't bother if those conditions aren't met.
+	if ( ! window.history.pushState ) {
+		return;
+	}
 	var self = this,
 		offset = self.offset > 0 ? self.offset - 1 : 0,
 		pageSlug = -1 == page ? self.origURL : window.location.protocol + '//' + self.history.host + self.history.path.replace( /%d/, page + offset ) + self.history.parameters;
@@ -601,14 +643,18 @@ $( document ).ready( function() {
 
 	/**
 	 * Monitor user scroll activity to update URL to correspond to archive page for current set of IS posts
-	 * IE only supports pushState() in v10 and above, so don't bother if those conditions aren't met.
-	 */
-	if ( ! isIE || ( isIE && IEVersion >= 10 ) ) {
-		$( window ).bind( 'scroll', function() {
-			clearTimeout( timer );
-			timer = setTimeout( infiniteScroll.scroller.determineURL , 100 );
-		});
-	}
+     */
+    if( type == 'click' ) {
+        var timer = null;
+        $( window ).bind( 'scroll', function() {
+            // run the real scroll handler once every 250 ms.
+            if ( timer ) { return; }
+            timer = setTimeout( function() {
+                infiniteScroll.scroller.determineURL();
+                timer = null;
+            } , 250 );
+        });
+    }
 });
 
 
