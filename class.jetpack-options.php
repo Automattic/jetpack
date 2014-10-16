0f26188719b19facdbf811c82e458f157aed4e0b
Miguel Lezama <miguel@automattic.com>
2014-10-16 01:22:12 -0200

    Save autoupdate options as non-compact only.
    + Clean whitespaces.

--- class.jetpack-options.php
+++ class.jetpack-options.php
@@ -19,8 +19,8 @@ class Jetpack_Options {
 				'wpcc_options',
 				'relatedposts',
 				'file_data',
-                'autoupdate_plugins',
-				'autoupdate_themes',
+				'autoupdate_plugins',          // (array)  An array of plugin ids ( eg. jetpack/jetpack ) that should be autoupdated
+				'autoupdate_themes',           // (array)  An array of theme ids ( eg. twentyfourteen ) that should be autoupdated
 			);
 		}
 
@@ -44,8 +44,6 @@ class Jetpack_Options {
 			'last_heartbeat',               // (int)    The timestamp of the last heartbeat that fired.
 			'sync_bulk_reindexing',         // (bool)   If a bulk reindex is currently underway.
 			'json_api_full_management',     // (bool)   Allow full management (eg. Activate, Upgrade plugins) of the site via the JSON API.
-            'autoupdate_plugins',           // (array)  An array of plugin ids ( eg. jetpack/jetpack ) that should be autoupdated
-			'autoupdate_themes',            // (array)  An array of theme ids ( eg. twentyfourteen ) that should be autoupdated
 		);
 	}
 
