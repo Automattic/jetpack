15dc68b4ee1d3fd7132d0477b15b3acb40e77dd3
Rocco Tripaldi <tripaldir@gmail.com>
2014-10-16 14:54:45 -0400

    Core Endpoint: adding a new core endpoint
    There is currently an endpoint to update the core version. Adding an endpoint that
    will GET core info, and toggle autoupdates.  Moving find_latest_core_version function
    into new class so that it is available to both core related classes

--- json-endpoints/jetpack/class.jetpack-json-api-core-update-endpoint.php
+++ json-endpoints/jetpack/class.jetpack-json-api-core-update-endpoint.php
@@ -1,6 +1,6 @@
 <?php
 
-class Jetpack_JSON_API_Core_Update_Endpoint extends Jetpack_JSON_API_Endpoint {
+class Jetpack_JSON_API_Core_Update_Endpoint extends Jetpack_JSON_API_Core_Endpoint {
 
 	// POST /sites/%s/core/update
 	public function callback( $path = '', $blog_id = 0 ) {
@@ -48,21 +48,6 @@ class Jetpack_JSON_API_Core_Update_Endpoint extends Jetpack_JSON_API_Endpoint {
 		$this->log = $upgrader->skin->get_upgrade_messages();
 
 		return $result;
-	}
-
-	private function find_latest_core_version() {
-		// Select the latest update.
-		// Remove filters to bypass automattic updates.
-		add_filter( 'request_filesystem_credentials',      '__return_true'  );
-		add_filter( 'automatic_updates_is_vcs_checkout',   '__return_false' );
-		add_filter( 'allow_major_auto_core_updates',       '__return_true'  );
-		add_filter( 'send_core_update_notification_email', '__return_false' );
-		$update = find_core_auto_update();
-		remove_filter( 'request_filesystem_credentials',      '__return_true'  );
-		remove_filter( 'automatic_updates_is_vcs_checkout',   '__return_false' );
-		remove_filter( 'allow_major_auto_core_updates',       '__return_true'  );
-		remove_filter( 'send_core_update_notification_email', '__return_false' );
-		return $update;
 	}
 
 }
