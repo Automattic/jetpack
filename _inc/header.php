a60a5b9ce3c1ba08b979c9e53355d11a3e494304
Jeremy Herve <jeremy@tagada.hu>
2014-10-16 11:10:33 +0000

    Menu: don't show setings links to users who can't edit settings

--- _inc/header.php
+++ _inc/header.php
@@ -6,7 +6,9 @@
 
 				<ul class="main-nav">
 					<li class="jetpack-logo"><a href="<?php echo Jetpack::admin_url(); ?>" title="<?php esc_attr_e( 'Home', 'jetpack' ); ?>" <?php if ( 'jetpack' == $current ) { echo 'class="current"'; } ?>><span><?php esc_html_e( 'Jetpack', 'jetpack' ); ?></span></a></li>
-					<?php if ( Jetpack::is_active() || Jetpack::is_development_mode() ) : ?>
+					<?php if ( ( Jetpack::is_active() || Jetpack::is_development_mode() )
+						&& current_user_can( 'jetpack_manage_modules' )
+					 ) : ?>
 					<li class="jetpack-modules">
 						<a href="<?php echo Jetpack::admin_url( 'page=jetpack_modules' ); ?>" class="jp-button--settings <?php if ( 'jetpack_modules' == $current ) { echo 'current'; } ?>"><?php esc_html_e( 'Settings', 'jetpack' ); ?></a>
 					</li>
