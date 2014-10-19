936de5684963613d6cc6cec6684fd341d1a337c7
Miguel Lezama <miguel@automattic.com>
2014-10-19 12:12:05 -0200

    Add bulk install capacity.
    Needs some testing, and tests :)

--- json-endpoints/jetpack/class.jetpack-json-api-plugins-install-endpoint.php
+++ json-endpoints/jetpack/class.jetpack-json-api-plugins-install-endpoint.php
@@ -4,58 +4,63 @@ class Jetpack_JSON_API_Plugins_Install_Endpoint extends Jetpack_JSON_API_Plugins
 
 	// POST  /sites/%s/plugins/new
 	protected $needed_capabilities = 'install_plugins';
-
-	public function callback( $path = '', $blog_id = 0, $plugin = null ) {
-
-		if ( is_wp_error( $error = $this->validate_call( $blog_id, $this->needed_capabilities, true ) ) ) {
-			return $error;
-		}
-
-		$result = $this->install();
-		return $result;
-	}
+	protected $action              = 'install';
+	protected $download_links      = array();
 
 	protected function install() {
-		$args = $this->input();
-		if ( empty( $args['plugin'] ) ) {
-			new WP_Error( 'missing_plugin', __( 'You are required to specify a plugin.', 'jetpack' ), 400 );
-		}
-		$plugin = $args['plugin'];
 
 		include_once ABSPATH . 'wp-admin/includes/plugin-install.php';
 		include_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
 		include_once ABSPATH . 'wp-admin/includes/file.php';
 
-		$api = plugins_api('plugin_information', array('slug' => $plugin, 'fields' => array('sections' => false) ) );
+		foreach ( $this->plugins as $plugin ) {
 
-		if ( is_wp_error($api) ) {
-			return $api;
+			$skin      = new Automatic_Upgrader_Skin();
+			$upgrader  = new Plugin_Upgrader( $skin );
+
+			$result = $upgrader->install( $this->download_links['plugin'] );
+
+			if ( ! $this->bulk && is_wp_error( $result ) ) {
+				return $result;
+			}
+
+			if ( ! $this->bulk && ! $result ) {
+				$error = $this->log[ $plugin ]['error'] = __( 'An unknown error occurred during installation', 'jetpack' );
+			}
+
+			$installed_plugin = $this->find_installed_plugin( $plugin );
+
+			if ( is_wp_error( $installed_plugin ) ) {
+				$error = $this->log[ $plugin ]['error'] = __( 'There was an error deactivating your plugin', 'jetpack' );
+			}
+
+			$this->log[ $plugin ][] = $upgrader->skin->get_upgrade_messages();
 		}
 
-		$skin      = new Automatic_Upgrader_Skin();
-		$upgrader  = new Plugin_Upgrader( $skin );
-
-		$result = $upgrader->install( $api->download_link );
-
-		if ( is_wp_error( $result ) ) {
-			return $result;
+		if ( ! $this->bulk && isset( $error ) ) {
+			return  new WP_Error( 'install_error', $this->log[ $plugin ]['error'], 400 );
 		}
 
-		if ( ! $result ) {
-			return new WP_Error( 'install_error', __( 'An unknown error occurred during installation', 'jetpack' ), 400 );
+	}
+
+	protected function validate_input( $plugin ) {
+		if ( empty( $this->plugins ) || ! is_array( $this->plugins ) ) {
+			return new WP_Error( 'missing_plugins', __( 'No plugins found.', 'jetpack' ));
 		}
+		foreach( $this->plugins as $index => $plugin ) {
+			if ( ! preg_match( "/\.php$/", $plugin ) ) {
+				$plugin =  $plugin . '.php';
+				$this->plugins[ $index ] = $plugin;
+			}
 
-		$log = $upgrader->skin->get_upgrade_messages();
+			$plugin_data = plugins_api( 'plugin_information', array( 'slug' => $plugin, 'fields' => array( 'sections' => false ) ) );
+			if ( is_wp_error( $plugin_data ) ) {
+				return $plugin_data;
+			}
+			$this->download_links[ $plugin ] = $plugin_data->download_link;
 
-		$installed_plugin = $this->find_installed_plugin( $plugin );
-
-		if ( is_wp_error( $installed_plugin ) ) {
-			return $installed_plugin;
 		}
-
-		$installed_plugin['log'] = $log;
-		return $installed_plugin;
-
+		return true;
 	}
 
 	protected function find_installed_plugin( $slug ) {
