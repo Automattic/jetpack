88703d0f1dbba881eecfd2a7a1d9dba5ccbce48c
Miguel Lezama <miguel@automattic.com>
2014-10-20 15:07:21 -0200

    Add modules bulk activation/deactivation logic.

--- json-endpoints/jetpack/class.jetpack-json-api-modules-modify-endpoint.php
+++ json-endpoints/jetpack/class.jetpack-json-api-modules-modify-endpoint.php
@@ -9,7 +9,7 @@ class Jetpack_JSON_API_Modules_Modify_Endpoint extends Jetpack_JSON_API_Modules_
 	protected $action              = 'default_action';
 
 	public function default_action() {
-
+		$args = $this->input();
 		if ( isset( $args['active'] ) && is_bool( $args['active'] ) ) {
 			if ( $args['active'] ) {
 				return $this->activate_module();
@@ -22,31 +22,38 @@ class Jetpack_JSON_API_Modules_Modify_Endpoint extends Jetpack_JSON_API_Modules_
 	}
 
 	protected function activate_module() {
-
-		if ( Jetpack::is_module_active( $this->module_slug ) ) {
-			return new WP_Error( 'jetpack_module_already_active', __( 'The Module is already active.', 'jetpack' ), 400 );
+		foreach ( $this->modules as $module ) {
+			if ( Jetpack::is_module_active( $module ) ) {
+				$error = $this->log[ $module ][] = __( 'The Jetpack Module is already activated.', 'jetpack' );
+				continue;
+			}
+			$result = Jetpack::activate_module( $module, false, false );
+			if ( false === $result || ! Jetpack::is_module_active( $module ) ) {
+				$error = $this->log[ $module ][] = __( 'There was an error while activating the module.', 'jetpack' );
+			}
 		}
 
-		$result = Jetpack::activate_module( $this->module_slug, false, false );
-
-		// TODO return WP_Error instead of bool in order to forward the error message.
-		if ( false === $result || ! Jetpack::is_module_active( $this->module_slug ) ) {
-			return new WP_Error( 'activation_error', sprintf( __( 'There was an error while activating the module `%s`.', 'jetpack' ), $this->module_slug ), 500 );
+		if ( ! $this->bulk && isset( $error ) ) {
+			return new WP_Error( 'activation_error', $error, 400 );
 		}
 
 		return true;
 	}
 
 	protected function deactivate_module() {
-
-		if ( ! Jetpack::is_module_active( $this->module_slug ) ) {
-			return new WP_Error( 'jetpack_module_already_deactivated', __( 'The Jetpack Module is already deactivated.', 'jetpack' ), 400 );
+		foreach ( $this->modules as $module ) {
+			if ( ! Jetpack::is_module_active( $module ) ) {
+				$error = $this->log[ $module ][] = __( 'The Jetpack Module is already deactivated.', 'jetpack' );
+				continue;
+			}
+			$result = Jetpack::deactivate_module( $module );
+			if ( false === $result || Jetpack::is_module_active( $module ) ) {
+				$error = $this->log[ $module ][] = __( 'There was an error while deactivating the module.', 'jetpack' );
+			}
 		}
 
-		$result = Jetpack::deactivate_module( $this->module_slug );
-
-		if ( false === $result || Jetpack::is_module_active( $this->module_slug ) ) {
-			return new WP_Error( 'deactivation_error', sprintf( __( 'There was an error while deactivating the module `%s`.', 'jetpack' ), $this->module_slug ), 500 );
+		if ( ! $this->bulk && isset( $error ) ) {
+			return new WP_Error( 'deactivation_error', $error, 400 );
 		}
 
 		return true;
