ea7859cffd044bfbd5552fefa9075dd8016261bc
Daisuke Takahashi <daisuke@extendwings.com>
2014-10-22 04:42:19 +0900

    Use `set_url_scheme()` instead of `str_replace()`

--- modules/contact-form/grunion-contact-form.php
+++ modules/contact-form/grunion-contact-form.php
@@ -985,7 +985,7 @@ class Grunion_Contact_Form extends Crunion_Contact_Form_Shortcode {
 			}
 
 			// For SSL/TLS page. See RFC 3986 Section 4.2
-			$url = str_replace( parse_url( $url, PHP_URL_SCHEME ) . ':', '', $url );
+			$url = set_url_scheme( $url );
 
 			// May eventually want to send this to admin-post.php...
 			$url = apply_filters( 'grunion_contact_form_form_action', "{$url}#contact-form-{$id}", $GLOBALS['post'], $id );
