c5bb09eac87436e8418f224414799f23e5644fb7
Miguel Lezama <miguel@automattic.com>
2014-10-21 12:39:54 -0200

    Fix Logic
    Remove `find_installed_plugin` in favour of `is_installed_plugin`

--- json-endpoints/jetpack/class.jetpack-json-api-plugins-install-endpoint.php
+++ json-endpoints/jetpack/class.jetpack-json-api-plugins-install-endpoint.php
@@ -26,10 +26,8 @@ class Jetpack_JSON_API_Plugins_Install_Endpoint extends Jetpack_JSON_API_Plugins
 				$error = $this->log[ $plugin ]['error'] = __( 'An unknown error occurred during installation', 'jetpack' );
 			}
 
-			$installed_plugin = $this->find_installed_plugin( $plugin );
-
-			if ( is_wp_error( $installed_plugin ) ) {
-				$error = $this->log[ $plugin ]['error'] = __( 'There was an error deactivating your plugin', 'jetpack' );
+			if ( ! $this::is_installed_plugin( $plugin ) ) {
+				$error = $this->log[ $plugin ]['error'] = __( 'There was an error installing your plugin', 'jetpack' );
 			}
 
 			$this->log[ $plugin ][] = $upgrader->skin->get_upgrade_messages();
@@ -42,16 +40,18 @@ class Jetpack_JSON_API_Plugins_Install_Endpoint extends Jetpack_JSON_API_Plugins
 		return true;
 	}
 
-	protected function validate_input( $plugin ) {
+	protected function validate_plugins() {
 		if ( empty( $this->plugins ) || ! is_array( $this->plugins ) ) {
-			return new WP_Error( 'missing_plugins', __( 'No plugins found.', 'jetpack' ));
+			return new WP_Error( 'missing_plugins', __( 'No plugins found.', 'jetpack' ) );
 		}
 		foreach( $this->plugins as $index => $plugin ) {
 			if ( ! preg_match( "/\.php$/", $plugin ) ) {
 				$plugin =  $plugin . '.php';
 				$this->plugins[ $index ] = $plugin;
 			}
-
+			if ( $this::is_installed_plugin( $plugin ) ) {
+				return new WP_Error( 'plugin_already_installed', __( 'The plugin is already installed', 'jetpack' ) );
+			}
 			$response = wp_remote_get( "http://api.wordpress.org/plugins/info/1.0/$plugin?fields=-sections" );
 			$plugin_data = unserialize( $response['body'] );
 			if ( is_wp_error( $plugin_data ) ) {
@@ -63,17 +63,8 @@ class Jetpack_JSON_API_Plugins_Install_Endpoint extends Jetpack_JSON_API_Plugins
 		return true;
 	}
 
-	protected function find_installed_plugin( $slug ) {
-		$all_plugins = get_plugins();
-
-		foreach( $all_plugins as $id => $plugin ) {
-			if ( strpos( $id, $slug . '/' ) !== false ) {
-				$installed_plugin = $this->format_plugin( $id, $plugin );
-				return $installed_plugin;
-			}
-		}
-
-		return new WP_Error( 'install_error', __( 'An unknown error occurred during installation', 'jetpack' ), 400 );
-
+	protected static function is_installed_plugin( $plugin ) {
+		return in_array( $plugin, array_keys( get_plugins() ) );
 	}
+
 }
