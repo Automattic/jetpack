aa4ec65a423ad10d09020e61e6b547ed242a62c6
Miguel Lezama <miguel@automattic.com>
2014-10-22 16:56:04 -0200

    Add delete plugin endpoint

---
+++ json-endpoints/jetpack/class.jetpack-json-api-plugins-delete-endpoint.php
@@ -0,0 +1,17 @@
+<?php
+
+class Jetpack_JSON_API_Plugins_Delete_Endpoint extends Jetpack_JSON_API_Plugins_Endpoint {
+
+	// POST  /sites/%s/plugins/%s/delete
+	protected $needed_capabilities = 'delete_plugins';
+	protected $action              = 'delete';
+	protected $download_links      = array();
+
+	protected function delete() {
+		if ( $error = delete_plugins( $this->plugins ) ) {
+			return $error;
+		};
+		return true;
+	}
+
+}
