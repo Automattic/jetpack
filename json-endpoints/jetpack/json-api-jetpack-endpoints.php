75a41c70b407648bb20ffe2efa2c068d7850f133
Rocco Tripaldi <tripaldir@gmail.com>
2014-10-22 08:56:32 -0400

    Plugin Modify Endpoint: Install method
    We can now install plugins remotely!
    
    Use /sites/%s/plugins/%s/install or /sites/%s/plugins/%s with action set to install
    Use /sites/%s/plugins/ for bulk install

--- json-endpoints/jetpack/json-api-jetpack-endpoints.php
+++ json-endpoints/jetpack/json-api-jetpack-endpoints.php
@@ -208,7 +208,7 @@ new Jetpack_JSON_API_Plugins_Modify_Endpoint( array(
 		'$plugin'   => '(string) The plugin ID',
 	),
 	'request_format' => array(
-		'action'       => '(string) Only possible value is \'update\'. More to follow',
+		'action'       => '(string) Possible values are \'update\', \'install\'',
 		'autoupdate'   => '(bool) Whether or not to automatically update the plugin',
 		'active'       => '(bool) Activate or deactivate the plugin',
 		'network_wide' => '(bool) Do action network wide (default value: false)',
@@ -234,7 +234,7 @@ new Jetpack_JSON_API_Plugins_Modify_Endpoint( array(
 		'$site'   => '(int|string) The site ID, The site domain',
 	),
 	'request_format' => array(
-		'action'       => '(string) Only possible value is \'update\'. More to follow',
+		'action'       => '(string) Possible values are \'update\', \'install\'',
 		'autoupdate'   => '(bool) Whether or not to automatically update the plugin',
 		'active'       => '(bool) Activate or deactivate the plugin',
 		'network_wide' => '(bool) Do action network wide (default value: false)',
@@ -261,30 +261,24 @@ new Jetpack_JSON_API_Plugins_Modify_Endpoint( array(
 	'example_request' => 'https://public-api.wordpress.com/rest/v1/sites/example.wordpress.org/plugins'
 ) );
 
-require_once( $json_jetpack_endpoints_dir . 'class.jetpack-json-api-plugins-install-endpoint.php' );
-// POST /sites/%s/plugins/new
-new Jetpack_JSON_API_Plugins_Install_Endpoint( array(
+// POST /sites/%s/plugins/%s/install
+new Jetpack_JSON_API_Plugins_Modify_Endpoint( array(
 	'description'     => 'Install a plugin to your jetpack blog',
 	'group'           => '__do_not_document',
 	'stat'            => 'plugins:1:new',
 	'method'          => 'POST',
-	'path'            => '/sites/%s/plugins/%s/new',
+	'path'            => '/sites/%s/plugins/%s/install',
 	'path_labels' => array(
 		'$site'   => '(int|string) The site ID, The site domain',
-	),
-	'request_format' => array(
-		'plugin'       => '(string) The plugin slug.'
+		'$plugin' => '(int|string) The plugin slug to install',
 	),
 	'response_format' => Jetpack_JSON_API_Plugins_Endpoint::$_response_format,
 	'example_request_data' => array(
 		'headers' => array(
 			'authorization' => 'Bearer YOUR_API_TOKEN'
 		),
-		'body' => array(
-			'plugin' => 'buddypress'
-		)
 	),
-	'example_request' => 'https://public-api.wordpress.com/rest/v1/sites/example.wordpress.org/plugins/new'
+	'example_request' => 'https://public-api.wordpress.com/rest/v1/sites/example.wordpress.org/plugins/akismet%2Fakismet/new'
 ) );
 
 new Jetpack_JSON_API_Plugins_Modify_Endpoint( array(
