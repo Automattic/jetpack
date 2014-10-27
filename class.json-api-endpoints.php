c9c0ff99d34c9bbf9a5998eb088d3f60e35fa2d7
Miguel Lezama <miguel@automattic.com>
2014-10-27 13:39:38 +0000

    REST API: Add 'slug' to plugins format.
    + Add some missing safehtml escapes.
    See: https://github.com/Automattic/jetpack/commit/2b4575b882b2d7ad7218f911994bdde215e29672

--- class.json-api-endpoints.php
+++ class.json-api-endpoints.php
@@ -517,18 +517,19 @@ abstract class WPCOM_JSON_API_Endpoint {
 			break;
 		case 'plugin' :
 			$docs = array(
-				'id'          => '(string)   The plugin\'s ID',
+				'id'          => '(safehtml) The plugin\'s ID',
+				'slug'        => '(safehtml) The plugin\'s Slug',
 				'active'      => '(boolean)  The plugin status.',
 				'update'      => '(object)   The plugin update info.',
-				'name'        => '(string)   The name of the plugin.',
+				'name'        => '(safehtml) The name of the plugin.',
 				'plugin_url'  => '(url)      Link to the plugin\'s web site.',
-				'version'     => '(string)   The plugin version number.',
+				'version'     => '(safehtml) The plugin version number.',
 				'description' => '(safehtml) Description of what the plugin does and/or notes from the author',
-				'author'      => '(string)   The plugin author\'s name',
+				'author'      => '(safehtml) The plugin author\'s name',
 				'author_url'  => '(url)      The plugin author web site address',
 				'network'     => '(boolean)  Whether the plugin can only be activated network wide.',
-				'autoupdate'  => '(boolean)  Whether the plugin is automatically updated',
-				'log'         => '(array)    An array of log strings telling how the plugin was modified',
+				'autoupdate'  => '(boolean)  Whether the plugin is auto updated',
+				'log'         => '(array:safehtml) An array of update log strings.',
 			);
 			$return[$key] = (object) $this->cast_and_filter( $value, apply_filters( 'wpcom_json_api_plugin_cast_and_filter', $docs ), false, $for_output );
 			break;
