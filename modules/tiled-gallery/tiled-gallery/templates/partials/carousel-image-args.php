e04fcbe61b2ab572949c8d8d736a1e43caaf4603
Jeremy Herve <jeremy@tagada.hu>
2014-10-28 00:49:11 +0000

    Tiled Galleries: fix typo in gallery attributes

--- modules/tiled-gallery/tiled-gallery/templates/partials/carousel-image-args.php
+++ modules/tiled-gallery/tiled-gallery/templates/partials/carousel-image-args.php
@@ -1,11 +1,11 @@
-<?php 
-if ( defined( 'JSON_HEX_AMP' ) ) { 
-	// see shortcodes/slideshow.php 
-	// This is nice to have, but not strictly necessary since we use _wp_specialchars() below 
-	$fuzzy_image_meta = json_encode( array_map( 'strval', $item->fuzzy_image_meta() ), JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT ); 
-} else { 
-	$fuzzy_image_meta = json_encode( array_map( 'strval', $item->fuzzy_image_meta() ) ); 
-} 
+<?php
+if ( defined( 'JSON_HEX_AMP' ) ) {
+	// see shortcodes/slideshow.php
+	// This is nice to have, but not strictly necessary since we use _wp_specialchars() below
+	$fuzzy_image_meta = json_encode( array_map( 'strval', $item->fuzzy_image_meta() ), JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT );
+} else {
+	$fuzzy_image_meta = json_encode( array_map( 'strval', $item->fuzzy_image_meta() ) );
+}
 ?>
 data-attachment-id="<?php echo esc_attr( $item->image->ID ); ?>"
 data-orig-file="<?php echo esc_url( wp_get_attachment_url( $item->image->ID ) ); ?>"
@@ -15,4 +15,4 @@ data-image-meta="<?php echo _wp_specialchars( wp_check_invalid_utf8( $fuzzy_imag
 data-image-title="<?php echo esc_attr( wptexturize( $item->image->post_title ) ); ?>"
 data-image-description="<?php echo esc_attr( wpautop( wptexturize( $item->image->post_content ) ) ); ?>"
 data-medium-file="<?php echo esc_url( $item->medium_file() ); ?>"
-data-lage-file="<?php echo esc_url( $item->large_file() ); ?>"
+data-large-file="<?php echo esc_url( $item->large_file() ); ?>"
