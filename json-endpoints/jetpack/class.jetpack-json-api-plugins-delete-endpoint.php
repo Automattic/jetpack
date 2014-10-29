902e6eea715de85ba7d3c5d601dec45d50c0518e
Miguel <miguel@automattic.com>
2014-10-29 03:07:52 +0000

    REST API: Plugin Delete Endpoint:  do not delete an active plugin.
    The WordPress dashboard does not allow one to delete an active plugin,
    neither then should the API.
    
    + the plugin slug could have been empty if the plugin was a single file,
    and not stored in a directory
    
    + Instead of using the plugin id ( i.e. wordpress-seo/wp-seo ) in the request
    we will use the slug ( i.e. wordpress-seo ). In browsing plugins on the .org
    directory, we found that only the slug is available. I created a new function
    get_plugin_id_by_slug, so that, after installation, we can convert the slug
    into a plugin id, so that our newly installed plugin will pass validation
    
    + Removing some unneeded properties
    
    props @roccotripaldi

--- json-endpoints/jetpack/class.jetpack-json-api-plugins-delete-endpoint.php
+++ json-endpoints/jetpack/class.jetpack-json-api-plugins-delete-endpoint.php
@@ -5,13 +5,27 @@ class Jetpack_JSON_API_Plugins_Delete_Endpoint extends Jetpack_JSON_API_Plugins_
 	// POST  /sites/%s/plugins/%s/delete
 	protected $needed_capabilities = 'delete_plugins';
 	protected $action              = 'delete';
-	protected $download_links      = array();
 
 	protected function delete() {
 
-		if ( is_wp_error( $error = delete_plugins( $this->plugins ) ) ) {
-			return $error;
-		};
+		foreach( $this->plugins as $plugin ) {
+
+			if ( Jetpack::is_plugin_active( $plugin ) ) {
+				$error = $this->log[ $plugin ][] ='You cannot delete a plugin while it is active on the main site.';
+				continue;
+			}
+
+			$result = delete_plugins ( array( $plugin ) );
+			if ( is_wp_error( $result ) ) {
+				$error = $this->log[ $plugin ][] = $result->get_error_message();
+			} else {
+				$this->log[ $plugin ][] = 'Plugin deleted';
+			}
+		}
+
+		if( ! $this->bulk && isset( $error ) ) {
+			return  new WP_Error( 'delete_plugin_error', $error, 400 );
+		}
 
 		return true;
 	}
