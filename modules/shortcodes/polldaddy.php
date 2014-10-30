42d4cbaf763a730b31fc03ebd5738d69173daabc
John Godley <johng@automattic.com>
2014-10-30 11:29:43 +0000

    Sanitisation was a bit too strict - allow a full alphabet, not hex only
    Merges r107765-wpcom.

--- modules/shortcodes/polldaddy.php
+++ modules/shortcodes/polldaddy.php
@@ -354,7 +354,7 @@ CONTAINER;
 					}
 					elseif ( !empty( $domain ) && !empty( $id ) ) {
 
-						$domain = preg_replace( '/[^a-f0-9]/i', '', $domain );
+						$domain = preg_replace( '/[^a-z0-9\-]/i', '', $domain );
 						$id = preg_replace( '/[\/\?&\{\}]/', '', $id );
 
 						$auto_src = esc_url( "http://{$domain}.polldaddy.com/s/{$id}" );
