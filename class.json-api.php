2626b22d8de9a8ee5d918818efc0ae9d86615f86
Joey Kudish <jkudish@automattic.com>
2014-10-30 09:19:50 +0000

    revert [107759] as it didn't solve the problem with CORS and firefox
    Merges r107760-wpcom.

--- class.json-api.php
+++ class.json-api.php
@@ -136,11 +136,6 @@ class WPCOM_JSON_API {
 		add_filter( 'comment_edit_pre', array( $this, 'comment_edit_pre' ) );
 
 		$initialization = $this->initialize();
-		if ( 'OPTIONS' == $this->method ) {
-			do_action( 'wpcom_json_api_options' );
-			exit;
-		}
-
 		if ( is_wp_error( $initialization ) ) {
 			$this->output_error( $initialization );
 			return;
@@ -157,6 +152,11 @@ class WPCOM_JSON_API {
 
 		$is_help = preg_match( '#/help/?$#i', $this->path );
 		$matching_endpoints = array();
+
+		if ( 'OPTIONS' == $this->method ) {
+			do_action( 'wpcom_json_api_options' );
+			exit;
+		}
 
 		if ( $is_help ) {
 			$origin = get_http_origin();
