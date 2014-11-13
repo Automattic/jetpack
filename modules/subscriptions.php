d95011be01c9bef85fc7cf4f8cc9695b5ea77cfb
enej <enej.bajgoric@gmail.com>
2014-11-13 10:48:07 -0800

    Fixes the #1269: Remove the label for the subscription notification.

--- modules/subscriptions.php
+++ modules/subscriptions.php
@@ -654,9 +654,8 @@ class Jetpack_Subscriptions_Widget extends WP_Widget {
 
 		<script>
 			( function( d ) {
-				var i = d.createElement('input');
-				if ( 'placeholder' in i && d.getElementById( 'jetpack-subscribe-label' ).length > 0 ) {
-					d.getElementById( 'jetpack-subscribe-label' ).style.visibility = 'hidden';
+				if ( ( 'placeholder' in d.createElement( 'input' ) ) ) {
+ 					d.getElementById( 'jetpack-subscribe-label' ).style.visibility = 'hidden';
 				}
 			} ) ( document );
 		</script>
