65f18d98c0ceff021ed6639bbc3a4a8d279e6d7c
Ben Lowery <ben.lowery@automattic.com>
2014-12-08 20:47:07 +0000

    REST: switch paging handles over to just the value, instead of the full URL.
    The full URL was too difficult to built reliably, as we include both parameters
    that affect the query and parameters that affect how we output the response, like
    callback and http_envelope. Additionally, every client I've written for the page
    handles really has to extract the value from the URL and use it as a value anyway,
    as clients for our API want a base URL and params, not a fully-formed URL.
    
    See https://[private link]
    
    Merges r109976-wpcom.

--- json-endpoints/class.wpcom-json-api-list-posts-v1-1-endpoint.php
+++ json-endpoints/class.wpcom-json-api-list-posts-v1-1-endpoint.php
@@ -113,12 +113,14 @@ class WPCOM_JSON_API_List_Posts_v1_1_Endpoint extends WPCOM_JSON_API_Post_v1_1_E
 		}
 
 		if ( isset( $args['page'] ) ) {
-			$is_eligible_for_page_handle = false;
 			if ( $args['page'] < 1 ) {
 				$args['page'] = 1;
 			}
 
 			$query['paged'] = $args['page'];
+			if ( $query['paged'] !== 1 ) {
+				$is_eligible_for_page_handle = false;
+			}
 		} else {
 			if ( $args['offset'] < 0 ) {
 				$args['offset'] = 0;
@@ -231,7 +233,7 @@ class WPCOM_JSON_API_List_Posts_v1_1_Endpoint extends WPCOM_JSON_API_Post_v1_1_E
 
 			if ( ( $return['found'] > count( $return['posts'] ) ) && $last_post ) {
 				$return['meta'] = array();
-				$return['meta']['next_page'] = $this->build_next_page_link( $last_post, $query );
+				$return['meta']['next_page'] = $this->build_page_handle( $last_post, $query );
 			}
 		}
 
@@ -240,18 +242,12 @@ class WPCOM_JSON_API_List_Posts_v1_1_Endpoint extends WPCOM_JSON_API_Post_v1_1_E
 		return $return;
 	}
 
-	function build_next_page_link( $post, $query ) {
+	function build_page_handle( $post, $query ) {
 		$column = $query['orderby'];
 		if ( ! $column ) {
 			$column = 'date';
 		}
-
-		// yes, we really have to double encode this. The page handle is itself url-encoded and it's stuck into
-		// a URL. So once for the page handle values, once overall for the URL.
-		// If we used a different way of encoding the map of values for the page handle, this might seem saner,
-		// but then we get to invent our own escaping system.
-		$handle = build_query( array( 'value' => urlencode($post[$column]), 'id' => $post['ID'] ) );
-		return add_query_arg( array( 'page_handle' => urlencode( $handle ) ), $this->api->url );
+		return build_query( array( 'value' => urlencode($post[$column]), 'id' => $post['ID'] ) );
 	}
 
 	function _build_date_range_query( $column, $range, $where ) {
