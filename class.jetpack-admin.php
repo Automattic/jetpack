b18e9554a9f1be1ed08a287cf5d44641b9954260
enej <enej.bajgoric@gmail.com>
2014-12-10 10:41:59 -0800

    Bug fix: Don't indicate what modules activated
    If the user doesn’t have a connected jetpack site or is in developer
    mode. don’t show if the module is active. Since it shouldn’t do
    anything but can cause confusion in the UI.
    Since some boxes will be highlighted as activeed and others will not.
    The user is not able to click the deactivate links.

--- class.jetpack-admin.php
+++ class.jetpack-admin.php
@@ -69,7 +69,7 @@ class Jetpack_Admin {
 				$short_desc_trunc = ( strlen( $short_desc ) > 143 ) ? substr( $short_desc, 0, 140 ) . '...' : $short_desc;
 
 				$module_array['module']            = $module;
-				$module_array['activated']         = in_array( $module, $active_modules );
+				$module_array['activated']         = ( Jetpack::is_active() || Jetpack::is_development_mode() ? in_array( $module, $active_modules ) : false );
 				$module_array['deactivate_nonce']  = wp_create_nonce( 'jetpack_deactivate-' . $module );
 				$module_array['activate_nonce']    = wp_create_nonce( 'jetpack_activate-' . $module );
 				$module_array['available']         = self::is_module_available( $module_array );
