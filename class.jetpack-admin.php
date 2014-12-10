297c2f502a8d4102d83192f364f853d038148e9c
enej <enej.bajgoric@gmail.com>
2014-12-10 10:43:25 -0800

    Bug fix: Move the request for is_active and development mode out of the loop.

--- class.jetpack-admin.php
+++ class.jetpack-admin.php
@@ -62,14 +62,14 @@ class Jetpack_Admin {
 		$available_modules = $this->jetpack->get_available_modules();
 		$active_modules    = $this->jetpack->get_active_modules();
 		$modules           = array();
-
+		$jetpack_active = Jetpack::is_active() || Jetpack::is_development_mode();
 		foreach ( $available_modules as $module ) {
 			if ( $module_array = $this->jetpack->get_module( $module ) ) {
 				$short_desc = apply_filters( 'jetpack_short_module_description', $module_array['description'], $module );
 				$short_desc_trunc = ( strlen( $short_desc ) > 143 ) ? substr( $short_desc, 0, 140 ) . '...' : $short_desc;
 
 				$module_array['module']            = $module;
-				$module_array['activated']         = ( Jetpack::is_active() || Jetpack::is_development_mode() ? in_array( $module, $active_modules ) : false );
+				$module_array['activated']         = ( $jetpack_active ? in_array( $module, $active_modules ) : false );
 				$module_array['deactivate_nonce']  = wp_create_nonce( 'jetpack_deactivate-' . $module );
 				$module_array['activate_nonce']    = wp_create_nonce( 'jetpack_activate-' . $module );
 				$module_array['available']         = self::is_module_available( $module_array );
