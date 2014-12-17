5a32f571895bbe5c0066b44b047f58ba683aa0a1
Mrau Hu <mrauhu@yandex.ru>
2014-12-17 17:52:49 +0300

    Fix: correct multibyte strings truncate

--- class.jetpack-admin.php
+++ class.jetpack-admin.php
@@ -66,7 +66,10 @@ class Jetpack_Admin {
 		foreach ( $available_modules as $module ) {
 			if ( $module_array = $this->jetpack->get_module( $module ) ) {
 				$short_desc = apply_filters( 'jetpack_short_module_description', $module_array['description'], $module );
-				$short_desc_trunc = ( strlen( $short_desc ) > 143 ) ? substr( $short_desc, 0, 140 ) . '...' : $short_desc;
+				// Fix: correct multibyte strings truncate
+				$short_desc_trunc = ( mb_strlen( $short_desc ) > 143 )
+							? mb_substr( $short_desc, 0, 140 ) . '...'
+							: $short_desc;
 
 				$module_array['module']            = $module;
 				$module_array['activated']         = ( $jetpack_active ? in_array( $module, $active_modules ) : false );
