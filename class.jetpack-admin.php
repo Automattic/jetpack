0e79313494e349e57772a33b05be7a5552146b50
Mrau Hu <mrauhu@yandex.ru>
2014-12-17 19:01:42 +0300

    Add: check for mbstring extension

--- class.jetpack-admin.php
+++ class.jetpack-admin.php
@@ -66,10 +66,14 @@ class Jetpack_Admin {
 		foreach ( $available_modules as $module ) {
 			if ( $module_array = $this->jetpack->get_module( $module ) ) {
 				$short_desc = apply_filters( 'jetpack_short_module_description', $module_array['description'], $module );
-				// Fix: correct multibyte strings truncate
-				$short_desc_trunc = ( mb_strlen( $short_desc ) > 143 )
-							? mb_substr( $short_desc, 0, 140 ) . '...'
-							: $short_desc;
+				// Fix: correct multibyte strings truncate with checking for mbstring extension
+				$short_desc_trunc = ( function_exists( 'mb_strlen' ) )
+							? ( ( mb_strlen( $short_desc ) > 143 )
+								? mb_substr( $short_desc, 0, 140 ) . '...'
+								: $short_desc )
+							: ( ( strlen( $short_desc ) > 143 )
+								? substr( $short_desc, 0, 140 ) . '...'
+								: $short_desc );
 
 				$module_array['module']            = $module;
 				$module_array['activated']         = ( $jetpack_active ? in_array( $module, $active_modules ) : false );
