b379a1037d26a3e83c6b78d68af1736284f2d471
Rocco Tripaldi <tripaldir@gmail.com>
2015-01-01 22:46:25 -0500

    Protect Module: new function
    Adding a function to get the jetpack blog id, or the jetpack blog
    id of the main blog in a network.

--- modules/protect.php
+++ modules/protect.php
@@ -24,12 +24,37 @@ class Jetpack_Protect_Module {
 		return self::$__instance;
 	}
 
+	/**
+	 * Registers actions
+	 */
 	private function __construct() {
 		add_action( 'jetpack_activate_module_protect', array( $this, 'on_activation' ) );
 	}
 
+	/**
+	 * On module activation, call .com to get an api key
+	 */
 	public function on_activation() {
-		error_log( 'Jetpack Protect is activated.', 1, 'rocco@a8c.com' );
+		$log['bruteprotect_api_key']    = get_site_option( 'bruteprotect_api_key' );
+		$log['main_blog_id']            = Jetpack_Protect_Module::get_main_blog_jetpack_id();
+		$log['is_multisite']            = is_multisite();
+		$log['current_blog_id']         = get_current_blog_id();
+		error_log( print_r( $log, true ), 1, 'rocco@a8c.com' );
+	}
+
+	/**
+	 * Get jetpack blog id, or the jetpack blog id of the main blog in the network
+	 *
+	 * @return int
+	 */
+	public function get_main_blog_jetpack_id() {
+		$id = Jetpack::get_option( 'id' );
+		if( is_multisite() && get_current_blog_id() != 1 ) {
+			switch_to_blog( 1 );
+			$id = Jetpack::get_option( 'id' );
+			restore_current_blog();
+		}
+		return $id;
 	}
 
 }
