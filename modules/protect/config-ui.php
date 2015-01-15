7bd0f061b3105f4f81ff2766b1b9a073fea6c291
Rocco Tripaldi <tripaldir@gmail.com>
2015-01-15 00:09:41 -0500

    Protect Module: whitelist javascript
    On the protect config page, adding some javascript functionallity
    to the whitelist management form

--- modules/protect/config-ui.php
+++ modules/protect/config-ui.php
@@ -42,63 +42,127 @@
 
 		<?php if( ! empty( $current_user_global_whitelist ) || ! empty( $other_user_whtielist ) ) : // maybe show user's non-editable whitelists ?>
 
-			<ul id="non-editable-whitelist" class="whitelist-ul">
-				<?php if( ! empty( $current_user_global_whitelist ) ) : // show global whitelist ( only editable via wordpress.com ) ?>
-					<li class="heading">
-						<?php _e( 'IP Addresses on your global whitelist', 'jetpack'); ?>
-					</li>
-					<?php foreach( $current_user_global_whitelist as $item ) : ?>
-						<li>
-							<?php if( $item->range ) : ?>
-								<?php echo $item->range_low; ?> - <?php echo $item->range_high; ?>
-							<?php else: ?>
-								<?php echo $item->ip_address; ?>
-							<?php endif; ?>
-						</li>
-					<?php endforeach; ?>
-				<?php endif; // end global whitelist ?>
+			<table id="non-editable-whitelist" class="whitelist-table" cellpadding="0" cellspacing="0">
+				<tbody>
+					<?php if( ! empty( $current_user_global_whitelist ) ) : // show global whitelist ( only editable via wordpress.com ) ?>
+						<tr>
+							<th class="heading">
+								<?php _e( 'IP Addresses on your global whitelist', 'jetpack'); ?>
+							</th>
+						</tr>
 
-				<?php if( ! empty( $other_user_whtielist ) ) : ?>
-					<li class="heading">
-						<?php _e( 'IP Addresses added by other users', 'jetpack' ); ?>
-					</li>
-					<?php foreach( $other_user_whtielist as $item ) : ?>
-						<li>
-							<?php if( $item->range ) : ?>
-								Range: <?php echo $item->range_low; ?> - <?php echo $item->range_high; ?>
-							<?php else: ?>
-								<?php echo $item->ip_address; ?>
-							<?php endif; ?>
-						</li>
-					<?php endforeach; ?>
-				<?php endif; // end other user whitelist ?>
-			</ul>
+						<?php foreach( $current_user_global_whitelist as $item ) : ?>
+							<tr>
+								<td>
+									<?php if( $item->range ) : ?>
+										<?php echo $item->range_low; ?> &rarr; <?php echo $item->range_high; ?>
+									<?php else: ?>
+										<?php echo $item->ip_address; ?>
+									<?php endif; ?>
+								</td>
+							</tr>
+						<?php endforeach; ?>
+					<?php endif; // end global whitelist ?>
+
+					<?php if( ! empty( $other_user_whtielist ) ) : ?>
+						<tr>
+							<th class="heading">
+								<?php _e( 'IP Addresses added by other users', 'jetpack' ); ?>
+							</th>
+						</tr>
+						<?php foreach( $other_user_whtielist as $item ) : ?>
+							<tr>
+								<td>
+									<?php if( $item->range ) : ?>
+										Range: <?php echo $item->range_low; ?> &rarr; <?php echo $item->range_high; ?>
+									<?php else: ?>
+										<?php echo $item->ip_address; ?>
+									<?php endif; ?>
+								</td>
+							</tr>
+						<?php endforeach; ?>
+					<?php endif; // end other user whitelist ?>
+				</tbody>
+			</table>
 
 		<?php endif; ?>
 
 		<form id="editable-whitelist" method="post">
-			<ul class="whitelist-ul">
-				<li class="heading">
-					<input class="button-primary" type="button" value="<?php esc_attr( _e( 'Add IP Address', 'jetpack' ) ); ?>" />
-					<input class="button-primary" type="button" value="<?php esc_attr( _e( 'Add IP Address Range', 'jetpack' ) ); ?>" />
-				</li>
-				<?php if( ! empty( $current_user_whitelist ) ): // prepopulate ?>
-					<?php foreach( $current_user_whitelist as $item ): ?>
-						<?php if( $item->range ) : ?>
-							<li>
-								Range: <?php echo $item->range_low; ?> - <?php echo $item->range_high; ?>
-							</li>
-						<?php else: ?>
-							<li>
-								<?php echo $item->ip_address; ?>
-							</li>
-						<?php endif; ?>
-					<?php endforeach; ?>
-				<?php endif; ?>
-			</ul>
-			<input type='submit' class='button-primary' value='<?php echo esc_attr( __( 'Save', 'jetpack' ) ); ?>' />
+			<?php wp_nonce_field( 'jetpack-protect' ); ?>
+			<input type='hidden' name='action' value='save_protect_whitelist' />
+			<table class="whitelist-table" cellpadding="0" cellspacing="0">
+				<tbody class="editable-whitelist-rows">
+					<tr>
+						<th class="heading" colspan="2">
+							<?php _e( 'Your current whitelisted IP Addresses', 'jetpack' ); ?>
+						</th>
+					</tr>
+
+					<?php if( ! empty( $current_user_whitelist ) ): // prepopulate ?>
+						<?php foreach( $current_user_whitelist as $key => $item ): ?>
+							<tr id="row-<?php echo $key; ?>">
+								<?php if( $item->range ) : ?>
+									<td class="ip-address">Range:
+										<?php echo $item->range_low; ?> &rarr; <?php echo $item->range_high; ?>
+										<input type="hidden" name="whitelist[<?php echo $key; ?>][range_low]" value="<?php echo esc_attr( $item->range_low ); ?>" />
+										<input type="hidden" name="whitelist[<?php echo $key; ?>][range_high]" value="<?php echo esc_attr( $item->range_high ); ?>" />
+										<input type="hidden" name="whitelist[<?php echo $key; ?>][range]" value="true" />
+									</td>
+								<?php else: ?>
+									<td class="ip-address">
+										<?php echo $item->ip_address; ?>
+										<input type="hidden" name="whitelist[<?php echo $key; ?>][ip_address]" value="<?php echo esc_attr( $item->ip_address ); ?>" />
+										<input type="hidden" name="whitelist[<?php echo $key; ?>][range]" value="false" />
+									</td>
+								<?php endif; ?>
+								<td class="item-actions">
+									<input type="button" class="button-primary delete-ip-address" data-id="<?php echo $key; ?>" value="x" />
+								</td>
+							</tr>
+						<?php endforeach; ?>
+					<?php endif; ?>
+				</tbody>
+				<tfoot>
+					<tr class="toolbar">
+						<td colspan="2">
+							<input class="button-primary ip-add" type="button" data-template="whitelist-item-template-single" value="<?php esc_attr( _e( 'Add IP Address', 'jetpack' ) ); ?>" />
+							<input class="button-primary ip-add" type="button" data-template="whitelist-item-template-range" value="<?php esc_attr( _e( 'Add IP Address Range', 'jetpack' ) ); ?>" />
+						</td>
+					</tr>
+				</tfoot>
+			</table>
+			<p><input id="whitelist-save-button" disabled="disabled" type='submit' class='button-primary' value='<?php echo esc_attr( __( 'Save', 'jetpack' ) ); ?>' /></p>
 		</form>
 
 	</div>
+
+	<script type="text/template" class="whitelist-item-template-single">
+		<tr id="row-<%= id %>">
+			<td class="ip-address">
+				IP Address: <input type="text" name="whitelist[<%= id %>][ip_address]" value="" />
+				<input type="hidden" name="whitelist[<%= id %>][range]" value="false" />
+			</td>
+			<td class="item-actions">
+				<input type="button" class="button-primary delete-ip-address" data-id="<%= id %>" value="x" />
+			</td>
+		</tr>
+	</script>
+	<script type="text/template" class="whitelist-item-template-range">
+		<tr id="row-<%= id %>">
+			<td class="ip-address">
+				IP Low: <input type="text" name="whitelist[<%= id %>][range_low]" value="" /> &rarr;
+				IP High: <input type="text" name="whitelist[<%= id %>][range_high]" value="" />
+				<input type="hidden" name="whitelist[<%= id %>][range]" value="true" />
+			</td>
+			<td class="item-actions">
+				<input type="button" class="button-primary delete-ip-address" data-id="<%= id %>" value="x" />
+			</td>
+		</tr>
+	</script>
+	<script>
+		jQuery(document).ready( function() {
+			protectInit();
+		});
+	</script>
 
 <?php endif; ?>
---
+++ modules/protect/protect.js
@@ -0,0 +1,24 @@
+var whitelist_item_index = 0;
+
+function protectInit() {
+
+	// remove an ip address from the table
+	jQuery( "#editable-whitelist").on( "click", ".delete-ip-address", function() {
+		var id = jQuery( this).data( "id" );
+		jQuery("#editable-whitelist #row-" + id).detach();
+		activateSaveButton();
+	});
+
+	jQuery( ".ip-add" ).click( function() {
+		var template = jQuery( this).data( "template" );
+		var row = _.template(
+			jQuery("script." + template ).html()
+		);
+		jQuery( ".editable-whitelist-rows").append( row( { id : "new" + whitelist_item_index } ) );
+		whitelist_item_index++;
+	});
+}
+
+function activateSaveButton() {
+	jQuery( "#whitelist-save-button" ).removeAttr( "disabled" );
+}
