45c7fed4b17413a6d2a462ce0ae3f215fb6eb932
Sam Hotchkiss <sam@hotchkissconsulting.net>
2015-01-16 01:43:48 -0700

    add function_exists

--- class.jetpack.php
+++ class.jetpack.php
@@ -5273,14 +5273,9 @@ function jetpack_report_security( $type, $plugin_slug, $args ) {
 }
 
 //Example:
-jetpack_report_security( 'backup', 'backupbuddy', array( 'plugin' => 'BackupBuddy', 'last' => '2015-01-14 03:00 UTC', 'next' => '2015-01-16 03:00 UTC' ) );
-
-
-
-
-
-
-
+if( function_exists( 'jetpack_report_security' ) ) {
+	jetpack_report_security( 'backup', 'backupbuddy', array( 'plugin' => 'BackupBuddy', 'last' => '2015-01-14 03:00 UTC', 'next' => '2015-01-16 03:00 UTC' ) );
+}
 
 
 
