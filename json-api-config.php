bfe5f57e273cfe483d8eba3d94d519487c1d97eb
Derek Smart <smart@automattic.com>
2015-01-29 16:57:55 +0000

    add required file

---
+++ json-api-config.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * WARNING: This file is distributed verbatim in Jetpack.
+ * There should be nothing WordPress.com specific in this file.
+ *
+ * @hide-in-jetpack
+ */
+
+
+define( 'WPCOM_JSON_API__CURRENT_VERSION', '1.1' );
+
+$wpcom_json_api_production_versions = array(
+	'1',
+	'1.1',
+);
+
+$wpcom_json_api_dev_versions = array(
+	'1.2',
+);
