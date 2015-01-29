18d54bf4118cfaf680363182739908aea480d64e
Derek Smart <smart@automattic.com>
2015-01-29 17:06:36 +0000

    remove wpcom-specific comments

--- json-api-config.php
+++ json-api-config.php
@@ -1,10 +1,4 @@
 <?php
-/*
- * WARNING: This file is distributed verbatim in Jetpack.
- * There should be nothing WordPress.com specific in this file.
- *
- * @hide-in-jetpack
- */
 
 
 define( 'WPCOM_JSON_API__CURRENT_VERSION', '1.1' );
