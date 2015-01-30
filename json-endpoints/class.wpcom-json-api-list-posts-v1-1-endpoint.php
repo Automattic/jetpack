880f8789d8f6c31f68273722b76570e267d620c5
Kat Hagan <kat@automattic.com>
2015-01-30 18:44:21 +0000

    REST API: Revert r112051-wpcom.
    Can't do this until we move exclude_tree logic into the query.
    
    https://[private link]
    
    Merges r112080-wpcom.
    
    Conflicts:
    	json-endpoints/class.wpcom-json-api-list-posts-v1-1-endpoint.php

--- json-endpoints/class.wpcom-json-api-list-posts-v1-1-endpoint.php
+++ json-endpoints/class.wpcom-json-api-list-posts-v1-1-endpoint.php
@@ -76,7 +76,6 @@ class WPCOM_JSON_API_List_Posts_v1_1_Endpoint extends WPCOM_JSON_API_Post_v1_1_E
 			'post_parent'    => isset( $args['parent_id'] ) ? $args['parent_id'] : null,
 			'author'         => isset( $args['author'] ) && 0 < $args['author'] ? $args['author'] : null,
 			's'              => isset( $args['search'] ) ? $args['search'] : null,
-			'fields'         => 'ids',
 		);
 
 		if ( isset( $args['meta_key'] ) ) {
@@ -228,9 +227,10 @@ class WPCOM_JSON_API_List_Posts_v1_1_Endpoint extends WPCOM_JSON_API_Post_v1_1_E
 				break;
 			case 'posts' :
 				$posts = array();
-				foreach ( $wp_query->posts as $post_ID ) {
-					$the_post = $this->get_post_by( 'ID', $post_ID, $args['context'] );
-					if ( $the_post && ! is_wp_error( $the_post ) ) {
+				foreach ( $wp_query->posts as $post ) {
+					$the_post = $this->get_post_by( 'ID', $post->ID, $args['context'] );
+					$is_excluded_from_tree = isset( $exclude_tree ) && in_array( $post->ID, $exclude_tree );
+					if ( $the_post && ! is_wp_error( $the_post ) && ! $is_excluded_from_tree ) {
 						$posts[] = $the_post;
 					} else {
 						$excluded_count++;
