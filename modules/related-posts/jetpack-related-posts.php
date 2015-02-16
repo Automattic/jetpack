68ed44a5f155ae36845ddab3337aa55f6e0dd4df
Brady Vercher <brady@blazersix.com>
2015-02-16 10:59:47 -0800

    Prevent a debug notice in Jetpack_RelatedPosts::_get_related_post_ids() when trying to access an array index that doesn't exist.

--- modules/related-posts/jetpack-related-posts.php
+++ modules/related-posts/jetpack-related-posts.php
@@ -860,8 +860,8 @@ EOT;
 		// Build cache key
 		$cache_key = md5( serialize( $body ) );
 
-		// Cache is valid! Return cacheed value.
-		if ( is_array( $cache[ $cache_key ] ) && $cache[ $cache_key ][ 'expires' ] > $now_ts ) {
+		// Cache is valid! Return cached value.
+		if ( isset( $cache[ $cache_key ] ) && is_array( $cache[ $cache_key ] ) && $cache[ $cache_key ][ 'expires' ] > $now_ts ) {
 			return $cache[ $cache_key ][ 'payload' ];
 		}
 
