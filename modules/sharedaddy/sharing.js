fb4e0bc337306be8fd895b62cbb0dfb72df3d4bd
George Stephanis <george@stephanis.info>
2015-03-01 21:42:57 -0500

    Remove two unused function arguments
    I checked both the wpcom and jp versions, and these variables aren’t
    actually used anywhere.  So I’m tidying them out because jshint
    standards.

--- modules/sharedaddy/sharing.js
+++ modules/sharedaddy/sharing.js
@@ -141,7 +141,7 @@ if ( sharing_js_options && sharing_js_options.counts ) {
 	var $body, $sharing_email;
 
 	$.fn.extend( {
-		share_is_email: function( value ) {
+		share_is_email: function() {
 			return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i.test( this.val() );
 		}
 	} );
@@ -304,7 +304,7 @@ if ( sharing_js_options && sharing_js_options.counts ) {
 		});
 
 		// Add click functionality
-		$( '.sharedaddy ul' ).each( function( item ) {
+		$( '.sharedaddy ul' ).each( function() {
 
 			if ( 'yep' === $( this ).data( 'has-click-events' ) ) {
 				return;
