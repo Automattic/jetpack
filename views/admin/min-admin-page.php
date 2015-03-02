d99a2d0481cec31cb722d36a10a384a61bb46b77
dereksmart <smart@automattic.com>
2015-03-02 16:41:18 -0500

    Bring the clouds back - some other minor css tweaks

--- views/admin/min-admin-page.php
+++ views/admin/min-admin-page.php
@@ -1,29 +1,30 @@
-<div class="masthead <?php if ( ! $data['is_connected'] || ! $data['is_user_connected'] ) echo 'hasbutton'; ?>">
+<div class="clouds-sm"></div>
+<div class="page-content landing">
+<div class="masthead-new <?php if ( ! $data['is_connected'] || ! $data['is_user_connected'] ) echo 'hasbutton'; ?>">
 	<!-- needs to get rendered as SCSS -->
 	<style>
 
-		.jp-content .masthead { text-align:left!important; }
-		.jp-content .masthead h1 { margin:0!important; padding: 0 0 .5em .5em!important; font-size: 2.2em!important; min-width: 100%!important; }
+		.landing { max-width: 992px !important; margin: 0 auto; min-height: 400px; }
+		.masthead-new h1 { font: 300 2.57143em/1.4em "proxima-nova","Open Sans",Helvetica,Arial,sans-serif !important;  position: relative;  text-shadow: 0 1px 1px rgba(0, 0, 0, 0.12);  z-index: 3; text-align: center;}
+		/*.jp-content .masthead { text-align:left!important; }*/
+		/*.jp-content .masthead h1 { margin:0!important; padding: 0 0 .5em .5em!important; font-size: 2.2em!important; min-width: 100%!important; }*/
 		.jp-content .subhead { padding: .8em 1.5em; margin: 0!important; }
-		.jp-content .subhead:after { background: none!important; }
-		.jp-content .subhead p { font-size: 1.2em; }
-		.jp-content .subhead a.download-jetpack { margin: 1em 0!important; background: #1E8CBE!important; box-shadow: 0 6px 0 #0074A2,0 6px 3px rgba(0,0,0,0.4)!important; }
-		.jp-content .footer { padding-top: 0!important; background-image: none!important; }
-		.jp-content .footer:before { height: inherit!important; }
-		.jp-content .module { cursor: default!important; }
-		.jp-content .module .press { cursor: pointer!important; }
-
+		.footer { padding-top: 4em; padding-bottom: 14em; }
+		.jp-content .subhead:after, .footer:before { background: none!important; }
+		/*.jp-content .subhead p { font-size: 1.2em; }*/
+		/*.jp-content .subhead a.download-jetpack { margin: 1em 0!important; background: #1E8CBE!important; box-shadow: 0 6px 0 #0074A2,0 6px 3px rgba(0,0,0,0.4)!important; }*/
+		/*.jp-content .footer { padding-top: 0!important; background-image: none!important; }*/
+		/*.jp-content .footer:before { height: inherit!important; }*/
 	</style>
-
 	<!-- /needs to get rendered as SCSS -->
 	<?php Jetpack::init()->load_view( 'admin/network-activated-notice.php' ); ?>
 
 	<?php do_action( 'jetpack_notices' ) ?>
 
 	<?php if ( $data['is_connected'] ) : ?>
-	<h1><?php esc_html_e( 'Get the most out of Jetpack with...', 'jetpack' ); ?></h1>
+		<h1><?php esc_html_e( 'Get the most out of Jetpack with...', 'jetpack' ); ?></h1>
 	<?php else : ?>
-	<h1><?php esc_html_e( 'Boost traffic, enhance security, and improve performance.', 'jetpack' ); ?></h1>
+		<h1><?php esc_html_e( 'Boost traffic, enhance security, and improve performance.', 'jetpack' ); ?></h1>
 	<?php endif; ?>
 
 	<div class="subhead">
@@ -48,3 +49,4 @@
 		<?php endif; ?>
 	</div><!-- .subhead -->
 </div><!-- .masthead -->
+</div>
