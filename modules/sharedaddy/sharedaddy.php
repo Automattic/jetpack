7ad9875c757d099467b0f7f764b1649441fac13a
Chase Livingston <chaselivingston@gmail.com>
2015-03-08 17:00:02 -1000

    Add global

--- modules/sharedaddy/sharedaddy.php
+++ modules/sharedaddy/sharedaddy.php
@@ -22,6 +22,7 @@ function sharing_email_send_post( $data ) {
 }
 
 function sharing_add_meta_box() {
+	global $post;
 	$post_types = get_post_types( array( 'public' => true ) );
 	$title = apply_filters( 'sharing_meta_box_title', __( 'Sharing', 'jetpack' ) );
 	if ( $post->ID == get_option( 'page_for_posts' ) && empty( $post->post_content ) ) {
