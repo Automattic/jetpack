42b3c2b4eabb103ecbda02e3586d1e6ae9fca623
jessefriedman <jesserfriedman@gmail.com>
2015-03-10 13:05:16 -0400

    changed the url in the Manage description to link to wordpress.com/plugins

--- modules/manage.php
+++ modules/manage.php
@@ -2,7 +2,7 @@
 /**
  * Module Name: Manage
  * Module Description: Manage all your sites from a centralized place, https://wordpress.com/sites.
- * Jumpstart Description: adds API endpoints that let you manage plugins, turn on automated updates, and more from https://wordpress.com/plugins/THISURL.
+ * Jumpstart Description: adds API endpoints that let you manage plugins, turn on automated updates, and more from <a href="https://wordpress.com/plugins/" target="_blank">wordpress.com/plugins</a>.
  * Sort Order: 1
  * Recommendation Order: 3
  * First Introduced: 3.4
