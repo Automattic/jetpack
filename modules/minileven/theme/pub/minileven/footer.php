5b793a76590b8c7a0e7c56836f6c9186235f8ec9
RCowles <contact@ryanscowles.com>
2015-03-10 15:14:03 -1000

    Minileven: Document footer hooks
    Add inline documentation for wp_mobile_theme_footer and minileven_credits

--- modules/minileven/theme/pub/minileven/footer.php
+++ modules/minileven/theme/pub/minileven/footer.php
@@ -22,8 +22,21 @@
 	$current_url =  trailingslashit( home_url( add_query_arg( array(), $wp->request ) ) );
 ?>
 		<a href="<?php echo $current_url . '?ak_action=reject_mobile'; ?>"><?php _e( 'View Full Site', 'jetpack' ); ?></a><br />
-		<?php do_action( 'wp_mobile_theme_footer' ); ?>
-		<?php do_action( 'minileven_credits' ); ?>
+		<?php
+			/**
+			 * Fires after View Full Site link.
+			 *
+			 * @since 1.8.0
+			 */
+			do_action( 'wp_mobile_theme_footer' );
+
+			/**
+			 * Fires before footer credits.
+			 *
+			 * @since 1.8.0
+			 */
+			do_action( 'minileven_credits' );
+		?>
 		<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'jetpack' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'jetpack' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'jetpack' ), 'WordPress' ); ?></a>
 	</div>
 </footer><!-- #colophon -->
