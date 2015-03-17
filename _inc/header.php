ebfdf429a3d5d15cf503990bedf27929ef10071d
jeffgolenski <jeffgolenski@gmail.com>
2015-03-17 12:46:03 -0400

    Repaired vertically compressed JP logo in ie 9+10
    Repaired vertically compressed JP logo in ie 9+10

--- _inc/header.php
+++ _inc/header.php
@@ -5,7 +5,7 @@
 			<nav role="navigation" class="header-nav drawer-nav nav-horizontal">
 
 				<ul class="main-nav">
-					<li class="jetpack-logo"><a href="<?php echo Jetpack::admin_url(); ?>" title="<?php esc_attr_e( 'Home', 'jetpack' ); ?>" <?php if ( 'jetpack' == $current ) { echo 'class="current"'; } ?>><span><?php esc_html_e( 'Jetpack', 'jetpack' ); ?></span></a></li>
+					<li class="jetpack-logo"><a href="<?php echo Jetpack::admin_url(); ?>" title="<?php esc_attr_e( 'Jetpack', 'jetpack' ); ?>" <?php if ( 'jetpack' == $current ) { echo 'class="current"'; } ?>><span><?php esc_html_e( 'Jetpack', 'jetpack' ); ?></span></a></li>
 					<?php if ( ( Jetpack::is_active() || Jetpack::is_development_mode() )
 						&& current_user_can( 'jetpack_manage_modules' )
 					 ) : ?>
