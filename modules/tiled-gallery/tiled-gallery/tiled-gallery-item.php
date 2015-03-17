dbc32d5a083e70f55ea5d628a18354cdf900b1b9
Veselin Nikolov <veselin@automattic.com>
2015-03-17 15:05:04 +0200

    Tiled Gallery: space and closing php tag

--- modules/tiled-gallery/tiled-gallery/tiled-gallery-item.php
+++ modules/tiled-gallery/tiled-gallery/tiled-gallery-item.php
@@ -13,7 +13,7 @@ abstract class Jetpack_Tiled_Gallery_Item {
 		$this->link = $needs_attachment_link ? get_attachment_link( $this->image->ID ) : $this->orig_file;
 
 		$this->img_src = add_query_arg( array( 'w' => $this->image->width, 'h' => $this->image->height, 'crop' => true ), $this->orig_file );
-      
+
    	}
 
 	public function fuzzy_image_meta() {
@@ -67,10 +67,9 @@ class Jetpack_Tiled_Gallery_Rectangular_Item extends Jetpack_Tiled_Gallery_Item 
 class Jetpack_Tiled_Gallery_Square_Item extends Jetpack_Tiled_Gallery_Item {
 	public function __construct( $attachment_image, $needs_attachment_link, $grayscale ) {
 		parent::__construct( $attachment_image, $needs_attachment_link, $grayscale );
-		$this->img_src_grayscale = jetpack_photon_url( $this->img_src, array( 'filter' => 'grayscale', 'resize' => array( $this->image->width, $this->image->height ) ) ); 
+		$this->img_src_grayscale = jetpack_photon_url( $this->img_src, array( 'filter' => 'grayscale', 'resize' => array( $this->image->width, $this->image->height ) ) );
 	}
 }
 
 class Jetpack_Tiled_Gallery_Circle_Item extends Jetpack_Tiled_Gallery_Square_Item {
 }
-?>
