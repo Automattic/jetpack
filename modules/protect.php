7563b2507552d94087282d31a3b664c3b327dc44
Rocco Tripaldi <tripaldir@gmail.com>
2015-03-18 16:33:08 -0400

    Protect Module: re-working activation hooks
    Instead of getting api key using the module activation hook,
    use `init` instead.

--- modules/protect.php
+++ modules/protect.php
@@ -27,6 +27,7 @@ class Jetpack_Protect_Module {
 	public  $last_request;
 	public  $last_response_raw;
 	public  $last_response;
+	public  $is_activating = false;
 
 	/**
 	 * Singleton implementation
@@ -45,6 +46,7 @@ class Jetpack_Protect_Module {
 	 */
 	private function __construct() {
 		add_action( 'jetpack_activate_module_protect', array( $this, 'on_activation' ) );
+		add_action( 'init',                            array( $this, 'maybe_get_protect_key' ) );
 		add_action( 'jetpack_modules_loaded',          array( $this, 'modules_loaded' ) );
 		add_action( 'login_head',                      array( $this, 'check_use_math' ) );
 		add_filter( 'authenticate',                    array( $this, 'check_preauth' ), 10, 3 );
@@ -63,7 +65,14 @@ class Jetpack_Protect_Module {
 	 * On module activation, try to get an api key
 	 */
 	public function on_activation() {
-		$this->get_protect_key();
+		update_site_option('jetpack_protect_activating', 'activating');
+	}
+
+	public function maybe_get_protect_key() {
+		if ( get_site_option('jetpack_protect_activating', false ) && ! get_site_option('jetpack_protect_key', false ) ) {
+			$this->get_protect_key();
+			delete_site_option( 'jetpack_protect_activating' );
+		}
 	}
 
 	/**
@@ -261,7 +270,7 @@ class Jetpack_Protect_Module {
 	 */
 	function ip_is_whitelisted( $ip ) {
 		// If we found an exact match in wp-config
-		if ( defined( 'JETPACK_IP_ADDRESS_OK' ) && JETPACK_IP_ADDRESS_OK == $ip ) {
+				if ( defined( 'JETPACK_IP_ADDRESS_OK' ) && JETPACK_IP_ADDRESS_OK == $ip ) {
 			return true;
 		}
 
