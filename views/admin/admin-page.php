a0514f704ee7cf57d3841b66d5f4989062f4ea82
jessefriedman <jesserfriedman@gmail.com>
2015-03-18 11:14:44 +0000

    Fixed a typo on the link you account cat

--- views/admin/admin-page.php
+++ views/admin/admin-page.php
@@ -71,7 +71,7 @@
 					<?php if ( ! $data['is_connected'] && current_user_can( 'jetpack_connect' ) ) : ?>
 						<a href="<?php echo Jetpack::init()->build_connect_url() ?>" class="download-jetpack"><?php esc_html_e( 'Connect to WordPress.com', 'jetpack' ); ?></a>
 					<?php elseif ( $data['is_connected'] && ! $data['is_user_connected'] && current_user_can( 'jetpack_connect_user' ) ) : ?>
-						<a href="<?php echo Jetpack::init()->build_connect_url() ?>" class="download-jetpack"><?php esc_html_e( 'Link to your account to WordPress.com', 'jetpack' ); ?></a>
+						<a href="<?php echo Jetpack::init()->build_connect_url() ?>" class="download-jetpack"><?php esc_html_e( 'Link your account to WordPress.com', 'jetpack' ); ?></a>
 					<?php endif; ?>
 				</div>
 			</div>
