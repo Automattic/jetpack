1d12821b6142d990172dce2cb0182523963f91cc
jeffgolenski <jeffgolenski@gmail.com>
2015-03-31 13:06:40 -0400

    slight style changes to messages
    slight style changes to messages

--- class.jetpack.php
+++ class.jetpack.php
@@ -5458,7 +5458,7 @@ p {
 ?>
 <script>
 (function( $ ) {
-	var SECOND_IN_MS = 1000;
+	var SECOND_IN_MS = 500;
 
 	function contactSupport( e ) {
 		e.preventDefault();
@@ -5546,7 +5546,7 @@ p {
 			.jp-identity-crisis strong {
 					color: #518d2a;
 				}
-			.jp-id-crisis-success.dismiss {
+			.jp-identity-crisis.dismiss {
 				display: none;
 			}
 		</style>
