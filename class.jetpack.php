31e95ecdc00655e89195fec3540413ad06078d99
George Stephanis <george@stephanis.info>
2015-04-07 10:18:09 -0400

    Delay the Jetpack Init, and call it in the right context.
    The sole purpose of this function is to let us schedule something to
    fire, but not yet initialize Jetpack.
    
    It's an odd workaround. If it doesn't make sense, ask George. :)

--- class.jetpack.php
+++ class.jetpack.php
@@ -1531,7 +1531,7 @@ class Jetpack {
 		}
 
 		if ( version_compare( $jetpack_version, '1.9.2', '<' ) && version_compare( '1.9-something', JETPACK__VERSION, '<' ) ) {
-			add_action( 'jetpack_activate_default_modules', array( 'Jetpack_Sync', 'sync_all_registered_options' ), 1000 );
+			add_action( 'jetpack_activate_default_modules', array( __CLASS__, 'delayed_init_sync_all_registered_options' ), 1000 );
 		}
 
 		$new_version = JETPACK__VERSION . ':' . time();
@@ -1555,6 +1555,16 @@ class Jetpack {
 			wp_safe_redirect( Jetpack::admin_url( 'page=' . $page ) );
 			exit;
 		}
+	}
+
+	/**
+	 * The sole purpose of this function is to let us schedule
+	 * something to fire, but not yet initialize Jetpack.
+	 *
+	 * It's an odd workaround. If it doesn't make sense, ask George. :)
+	 */
+	public static function delayed_init_sync_all_registered_options() {
+		Jetpack::init()->sync->sync_all_registered_options();
 	}
 
 	/**
