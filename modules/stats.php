Automattic/jetpack

commit ab2ae39c0ece0ec90e3447a725dea12ad43cebbf

Relocate the call to stats_dashboard_head

modules/stats.php
@@ -63,6 +63,7 @@ function stats_load() {
 		add_action( 'wp_dashboard_setup', 'stats_register_dashboard_widget' );
 	} else {
 		// New way.
+		add_action( 'admin_head', 'stats_dashboard_head' );
 		add_action( 'jetpack_dashboard_widget', 'stats_jetpack_dashboard_widget' );
 	}
 
@@ -855,7 +856,6 @@ function stats_jetpack_dashboard_widget() {
 		}
 	</style>
 	<?php
-	stats_dashboard_head();
 }
 
 // Javascript and CSS for dashboard widget

commit 71e2987afd5797fb00f85fa1a66237455495bb76

Only print the dashboard head JS on the dashboard
fixes JS error introduced in ab2ae39c0ece0e

modules/stats.php
@@ -68,7 +68,7 @@ function stats_load() {
 		add_action( 'wp_dashboard_setup', 'stats_register_dashboard_widget' );
 	} elseif ( current_user_can( 'view_stats' ) ) {
 		// New way.
-		add_action( 'admin_head', 'stats_dashboard_head' );
+		add_action( 'load-index.php', 'stats_enqueue_dashboard_head' );
 		add_action( 'wp_dashboard_setup', 'stats_register_widget_control_callback' ); // hacky but works
 		add_action( 'jetpack_dashboard_widget', 'stats_jetpack_dashboard_widget' );
 	}
@@ -77,6 +77,10 @@ function stats_load() {
 
 
 	add_filter( 'pre_option_db_version', 'stats_ignore_db_version' );
+}
+
+function stats_enqueue_dashboard_head() {
+	add_action( 'admin_head', 'stats_dashboard_head' );
 }
 
 /**