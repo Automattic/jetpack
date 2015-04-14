285e6141b1d910f0bf7efb698bdb059020367141
Dennis Snell <dennis.snell@automattic.com>
2015-04-14 15:59:05 +0000

    Comments API: Grab post status through `get_post_status()`
    Previously there were certain requests to create new comments or
    comment replies that failed with the error,
    
    `Comments on drafts are not allowed`
    
    But the parent post or comment wasn't a draft at all. It turns out
    that in the API endpoint the code was grabbing the parent post's
    status by using `$post->post_status`, which seems reasonable, but
    returns the wrong information for pages such as media attachment
    pages.
    
    The proper behavior in these cases should be to get the post status of
    the parent post from which the media attachment page hangs. The correct
    behavior was already present in `wp-comments-post.php` but not in the
    API endpoint.
    
    This commit replaces the simpler status call with the helper function
    to get the proper value when posting to things like media attachment
    pages.
    
    Merges r116196-wpcom.

--- json-endpoints/class.wpcom-json-api-update-comment-endpoint.php
+++ json-endpoints/class.wpcom-json-api-update-comment-endpoint.php
@@ -63,7 +63,7 @@ class WPCOM_JSON_API_Update_Comment_Endpoint extends WPCOM_JSON_API_Comment_Endp
 			return $can_view;
 		}
 
-		$post_status = get_post_status_object( $post->post_status );
+		$post_status = get_post_status_object( get_post_status( $post ) );
 		if ( !$post_status->public && !$post_status->private ) {
 			return new WP_Error( 'unauthorized', 'Comments on drafts are not allowed', 403 );
 		}
