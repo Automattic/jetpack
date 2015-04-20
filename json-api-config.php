7deb922025bac28a1622fe6b3a8d846ec419c0ee
Joey Kudish <jkudish@automattic.com>
2015-04-20 18:04:41 +0000

    REST API: declare the production/development version variables as global. Otherwise they are not actually accessible inside a function scope.
    Merges r116542-wpcom.

--- json-api-config.php
+++ json-api-config.php
@@ -2,6 +2,7 @@
 
 
 define( 'WPCOM_JSON_API__CURRENT_VERSION', '1.1' );
+global $wpcom_json_api_production_versions, $wpcom_json_api_dev_versions;
 
 $wpcom_json_api_production_versions = array(
 	'1',
