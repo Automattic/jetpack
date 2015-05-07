54dd042be80d874326355bcba1470522d577bb53
Dagobert Renouf <dagobert.king@gmail.com>
2015-05-07 18:12:16 +0200

    Display form again when there has been an error

--- modules/subscriptions.php
+++ modules/subscriptions.php
@@ -635,7 +635,7 @@ class Jetpack_Subscriptions_Widget extends WP_Widget {
 		} else { ?>
 			<form action="#" method="post" accept-charset="utf-8" id="subscribe-blog-<?php echo $widget_id; ?>">
 				<?php
-				if ( ! isset ( $_GET['subscribe'] ) ) {
+				if ( ! isset ( $_GET['subscribe'] ) OR $_GET['subscribe'] != 'success' ) {
 					?><div id="subscribe-text"><?php echo wpautop( str_replace( '[total-subscribers]', number_format_i18n( $subscribers_total['value'] ), $subscribe_text ) ); ?></div><?php
 				}
 
@@ -643,7 +643,7 @@ class Jetpack_Subscriptions_Widget extends WP_Widget {
 					echo wpautop( sprintf( _n( 'Join %s other subscriber', 'Join %s other subscribers', $subscribers_total['value'], 'jetpack' ), number_format_i18n( $subscribers_total['value'] ) ) );
 				}
 
-				if ( ! isset ( $_GET['subscribe'] ) ) { ?>
+				if ( ! isset ( $_GET['subscribe'] ) OR $_GET['subscribe'] != 'success' ) { ?>
 					<p id="subscribe-email">
 						<label id="jetpack-subscribe-label" for="<?php echo esc_attr( $subscribe_field_id ); ?>">
 							<?php echo !empty( $subscribe_placeholder ) ? esc_html( $subscribe_placeholder ) : esc_html__( 'Email Address:', 'jetpack' ); ?>
