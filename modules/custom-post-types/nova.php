d4e1c52e9a232760521d4bcfc96f6e2718162ddd
Christopher Finke <finke@automattic.com>
2015-05-15 02:13:10 +0000

    Nova: Proper usage of the _n() function.
    We shouldn't concatenate a number and the result of _n; the number should be a placeholder in the _n() function so that languages that don't use the same number placement as English will still make sense.
    
    Merges r118063-wpcom.

--- modules/custom-post-types/nova.php
+++ modules/custom-post-types/nova.php
@@ -283,16 +283,14 @@ class Nova_Restaurant {
 		$number_menu_items = wp_count_posts( self::MENU_ITEM_POST_TYPE );
 
 		if ( current_user_can( 'administrator' ) ) {
-			$number_menu_items_published = sprintf( '<a href="%1$s">%2$s %3$s</a>',
+			$number_menu_items_published = sprintf( '<a href="%1$s">%2$s</a>',
 				esc_url( get_admin_url( get_current_blog_id(), 'edit.php?post_type=' . self::MENU_ITEM_POST_TYPE ) ),
-				number_format_i18n( $number_menu_items->publish ),
-				_n( 'Food Menu Item', 'Food Menu Items', intval( $number_menu_items->publish ), 'jetpack' )
+				sprintf( _n( '1 Food Menu Item', '%1$d Food Menu Items', intval( $number_menu_items->publish ), 'jetpack' ), number_format_i18n( $number_menu_items->publish ) )
 			);
 		}
 		else {
-			$number_menu_items_published = sprintf( '<span>%1$s %2$s</span>',
-				number_format_i18n( $number_menu_items->publish ),
-				_n( 'Food Menu Item', 'Food Menu Items', intval( $number_menu_items->publish ), 'jetpack' )
+			$number_menu_items_published = sprintf( '<span>%1$s</span>',
+				sprintf( _n( '1 Food Menu Item', '%1$d Food Menu Items', intval( $number_menu_items->publish ), 'jetpack' ), number_format_i18n( $number_menu_items->publish ) )
 			);
 		}
 
