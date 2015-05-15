abaee68ee1291b7d2d70fff505ebb9191c562006
George Stephanis <george@stephanis.info>
2015-05-15 15:35:28 -0400

    Add `stats_get_from_restapi` and caching stuff.
    Use the same 5 minute cache used by the old `stats_get_csv()`
    functions, but in a new option.
    
    The structure of the return data may change, this is just in place for
    playing around at the moment.  It will eventually be used for all in
    dashboard stats, as it also pulls down unique visitors and much richer
    data, not merely views.

--- class.jetpack-options.php
+++ class.jetpack-options.php
@@ -33,7 +33,9 @@ class Jetpack_Options {
 				'site_icon_id',                // (int)    Attachment id of the site icon file
 				'dismissed_manage_banner',     // (bool) Dismiss Jetpack manage banner allows the user to dismiss the banner permanently
 				'updates',                     // (array) information about available updates to plugins, theme, WordPress core, and if site is under version control
+				'restapi_stats_cache',         // (array) Stats Cache data.
 			);
+
 		case 'private' :
 			return array(
 				'register',
