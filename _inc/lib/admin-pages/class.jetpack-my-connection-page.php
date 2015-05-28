7b942f039fce9c39515db6b9508ef383e84325b5
dereksmart <smart@automattic.com>
2015-05-28 11:48:14 -0400

    My Connection Page: Add initial functionality
    adds the Jetapck connection information about the logged in user and the master user to My Connection page
    - Admins can disconnect the Jetpack site, or their own link to wpcom
    - Will display who is the Master user for a site
    - Admins who are not master user will be able to set themselves as master
    - Other users can link/unlink their accounts
    
    New file: _inc/jp-connection.js
    New file: class.jetpack-my-jetpack-page.php
    New file: my-jetpack-page.php

---
+++ _inc/lib/admin-pages/class.jetpack-my-connection-page.php
@@ -0,0 +1,137 @@
+<?php
+include_once( 'class.jetpack-admin-page.php' );
+include_once( JETPACK__PLUGIN_DIR . 'class.jetpack-modules-list-table.php' );
+
+// Builds the My Connection page
+class Jetpack_My_Connection_Page extends Jetpack_Admin_Page {
+	// Show the settings page only when Jetpack is connected or in dev mode
+	protected $dont_show_if_not_active = true;
+	function add_page_actions( $hook ) {} // There are no page specific actions to attach to the menu
+
+	// Adds the My Connection page, but hides it from the submenu
+	function get_page_hook() {
+		return add_submenu_page( null, __( 'My Connection', 'jetpack' ), __( 'My Connection', 'jetpack' ), 'jetpack_connect_user', 'my_connection', array( $this, 'render' ) );
+	}
+
+	// Renders the view file
+	function page_render() {
+		Jetpack::init()->load_view( 'admin/my-connection-page.php' );
+	}
+
+	/*
+	 * Handle the change in master user
+	 */
+	function jetpack_my_connection_change_user() {
+		if ( ! isset( $_POST['_my_connect_nonce'] ) || ! wp_verify_nonce( $_POST['_my_connect_nonce'], 'jetpack_change_primary_user' ) ) {
+			wp_die( __( 'Failed permissions, please try again.', 'jetpack' ) );
+			exit;
+		}
+
+		if ( isset( $_POST['jetpack-new-master'] ) ) {
+			$new_master_user   = $_POST['jetpack-new-master'];
+			$user_token        = Jetpack_Data::get_access_token( $new_master_user   );
+			$is_user_connected = $user_token && ! is_wp_error( $user_token );
+			if ( is_admin() && $is_user_connected ) {
+				Jetpack_Options::update_option( 'master_user', $new_master_user );
+			}
+		}
+	}
+
+	/*
+	 * Determine if we should show the primary user info
+	 * on the My Connection page
+	 *
+	 * @return (bool) True = show | False = Don't show
+	 */
+	function jetpack_show_primary_user_row() {
+		$all_users = count_users();
+		if ( ! current_user_can( 'jetpack_manage_modules' ) ) {
+			return false;
+		}
+
+		// If only one admin
+		if ( 2 > $all_users['avail_roles']['administrator'] ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/*
+	 * Checks to see if there are any other users available to become primary
+	 * Users must both:
+	 * - Be linked to wpcom
+	 * - Be an admin
+	 *
+	 * @return bool
+	 */
+	function jetpack_potential_primary_users_available() {
+		// If not admin, or only one admin
+		if ( false === $this->jetpack_show_primary_user_row() ) {
+			return false;
+		}
+
+		$users = get_users();
+		$available = array();
+		// If no one else is linked to dotcom
+		foreach ( $users as $user ) {
+			if ( isset( $user->caps['administrator'] ) && Jetpack::is_user_connected( $user->ID ) ) {
+				$available[] = $user->ID;
+			}
+		}
+
+		if ( 2 > count( $available ) ) {
+			return false;
+		}
+
+		return 'true';
+	}
+
+	/*
+	 * Info about the user's connection relationship with the site.
+	 *
+	 * @return array
+	 */
+	function jetpack_my_connection_logic() {
+		global $current_user;
+		$is_user_connected    = Jetpack::is_user_connected( $current_user->ID );
+		$is_master_user       = $current_user->ID == Jetpack_Options::get_option( 'master_user' );
+		$master_user_id       = Jetpack_Options::get_option( 'master_user' );
+		$master_user_data_org = get_userdata( $master_user_id );
+		$master_user_data_com = Jetpack::get_connected_user_data( $master_user_id );
+		if ( $master_user_data_org ) {
+			$edit_master_user_link = sprintf( __( '<a href="%s">%s</a>', 'jetpack' ), get_edit_user_link( $master_user_id ), $master_user_data_org->user_login );
+		} else {
+			$edit_master_user_link = __( 'No master user set!', 'jetpack' );
+		}
+		$connection_info = array(
+			'isMasterUser'    => $is_master_user,
+			'masterUserLink'  => $edit_master_user_link,
+			'isUserConnected' => $is_user_connected,
+			'master_data_com' => $master_user_data_com,
+			'adminUsername'   => $current_user->user_login
+		);
+		return $connection_info;
+	}
+
+	// Load up admin scripts
+	function page_admin_scripts() {
+		wp_enqueue_script( 'jp-connection-js', plugins_url( '_inc/jp-connection.js', JETPACK__PLUGIN_FILE ), array( 'jquery', 'wp-util' ), JETPACK__VERSION . 'yep' );
+
+		$master_user_com_data = $this->jetpack_my_connection_logic();
+		$jetpack_user_data    = Jetpack::get_connected_user_data();
+		$current_user         = wp_get_current_user();
+		wp_localize_script( 'jp-connection-js', 'jpConnection',
+			array(
+				'connectionLogic'    => $this->jetpack_my_connection_logic(),
+				'jetpackIsActive'    => Jetpack::is_active(),
+				'showPrimaryUserRow' => $this->jetpack_show_primary_user_row(),
+				'masterComData'      => $master_user_com_data['master_data_com'],
+				'userComData'        => $jetpack_user_data,
+				'userGrav'           => get_avatar( $current_user->ID, 40 ),
+				'masterUserGrav'     => get_avatar( Jetpack_Options::get_option( 'master_user' ), 40 ),
+				'potentialPrimaries' => $this->jetpack_potential_primary_users_available(),
+			)
+		);
+	}
+}
---
+++ views/admin/my-connection-page.php
@@ -0,0 +1,175 @@
+<div class="clouds-sm"></div>
+
+<div class="page-content landing">
+	<?php Jetpack::init()->load_view( 'admin/network-activated-notice.php' ); ?>
+
+	<?php do_action( 'jetpack_notices' ) ?>
+
+	<?php if ( Jetpack::is_active() ) : ?>
+		<div id="my-connection-page-template"></div>
+
+		<script id="tmpl-connection-page" type="text/html">
+			<div class="content-container <# if ( data.available ) { #>modal-footer<# } #>">
+				<div id="my-connection-content" class="content">
+					<h2><?php _e( 'Jetpack Connection Status' ); ?></h2>
+
+						<?php
+						/*
+						 * Special row if there is only one admin on the site
+						 */
+						?>
+						<# if ( ! data.showPrimaryUserRow && data.connectionLogic.isMasterUser ) { #>
+							<div class="connection-details local-user j-row">
+								<?php // left col ?>
+								<div class="j-col j-lrg-6 j-md-6 j-sm-12 jp-user">
+									<h3 title="<?php _e( 'Username', 'jetpack' ); ?>"><?php _e( 'Site Username', 'jetpack' ); ?></h3>
+									<div class="user-01">
+										{{{ data.userGrav }}} {{{ data.connectionLogic.adminUsername }}}
+									</div>
+								</div>
+
+								<?php // right col ?>
+								<div class="j-col j-lrg-6 j-md-6 j-sm-12 wp-user">
+									<h3 title="<?php _e( 'WordPress.com Username', 'jetpack' ); ?>"><?php _e( 'WordPress.com Username', 'jetpack' ); ?></h3>
+									<div class="wpuser-02">
+										{{{ data.userComData.login }}}
+									</div>
+								</div>
+							</div>
+						<# } #>
+
+						<?php
+						/*
+						 * Local user row: Shown to all users,
+						 * unless the current user is the master
+						 */
+						?>
+						<# if ( ! data.connectionLogic.isMasterUser ) { #>
+							<div class="connection-details local-user j-row">
+								<?php // left col ?>
+								<div class="j-col j-lrg-4 j-md-6 j-sm-12 jp-user">
+									<h3 title="<?php _e( 'Username', 'jetpack' ); ?>"><?php _e( 'Site Username', 'jetpack' ); ?></h3>
+									<div class="user-01">
+										{{{ data.userGrav }}} {{{ data.connectionLogic.adminUsername }}}
+									</div>
+								</div>
+
+								<!-- middle col -->
+								<div class="j-col j-lrg-4 j-md-4 j-sm-4 wp-user">
+									<h3 title="<?php _e( 'WordPress.com Username', 'jetpack' ); ?>"><?php _e( 'WordPress.com Username', 'jetpack' ); ?></h3>
+									<div class="wpuser-02">
+										<# if ( data.connectionLogic.isUserConnected ) { #>
+											{{{ data.userComData.login }}}
+										<# } else { #>
+											<span><em>Not connected</em></span>
+										<# } #>
+									</div> 
+								</div>
+
+								<?php // right col ( Link/Unlink my account ) ?>
+								<div class="j-col j-lrg-4 j-md-12 j-sm-12 wp-action">
+									<h3 title="<?php _e( 'Account Actions', 'jetpack' ); ?>"><?php _e( 'Account Actions', 'jetpack' ); ?></h3>
+									<div class="action-btns">
+										<# if ( data.connectionLogic.isUserConnected ) { #>
+											<a class="button" title="Disconnect your WordPress.com account from Jetpack" href="<?php echo wp_nonce_url( Jetpack::admin_url( 'action=unlink' ), 'jetpack-unlink' ); ?>"><?php esc_html_e( 'Unlink my account ', 'jetpack' ); ?></a>
+										<# } else { #>
+											<a class="button button-primary" href="<?php echo Jetpack::init()->build_connect_url() ?>" ><?php esc_html_e( 'Link your account', 'jetpack' ); ?></a>
+										<# } #>
+									</div>
+								</div>
+							</div>
+						<# } #><?php // End if not master user ?>
+						</div><?php // connection details ?>
+
+						<?php
+						/*
+						 * Master user row & Disconnect button
+						 * Only shown to admins.
+						 * Only shown if there are other as well, because if there aren't it's obvious who is primary.
+						 */
+						?>
+						<# if ( data.showPrimaryUserRow ) { #>
+							<div class="connection-details master-user j-row">
+								<?php // Master User Row, Left col ?>
+								<div class="j-col j-lrg-4 j-md-6 j-sm-12 jp-user">
+									<h3 title="<?php _e( 'Primary User', 'jetpack' ); ?>"><?php _e( 'Primary User', 'jetpack' ); ?></h3>
+									<div class="user-01">
+										{{{ data.masterUserGrav }}} {{{ data.connectionLogic.masterUserLink }}}
+									</div>
+								</div>
+
+								<?php // middle col ?>
+								<div class="j-col j-lrg-4 j-md-6 j-sm-12 wp-user">
+									<h3 title="<?php _e( 'WordPress.com Username', 'jetpack' ); ?>"><?php _e( 'WordPress.com Username', 'jetpack' ); ?></h3>
+									<div class="wpuser-02">
+										<span>{{{ data.masterComData.login }}}</span>
+									</div> 
+								</div>
+
+								<?php // right col ( Change primary user ) ?>
+								<div class="j-col j-lrg-4 j-md-12 j-sm-12 wp-action">
+									<h3 title="<?php _e( 'Account Actions', 'jetpack' ); ?>"><?php _e( 'Change Primary User', 'jetpack' ); ?></h3>
+									<div class="action-btns">
+										<a class="button" title="Change the primary account holder" id="change-primary-btn"><?php esc_html_e( 'Change Primary', 'jetpack' ); ?></a>
+
+										<form action="" method="post">
+											<select name="jetpack-new-master" id="user-list">
+												<?php
+												$all_users = get_users();
+												foreach ( $all_users as $user ) {
+													if ( $user->ID != Jetpack_Options::get_option( 'master_user' ) && Jetpack::is_user_connected( $user->ID ) && $user->caps['administrator'] ) {
+														echo "<option value='{$user->ID}'>$user->display_name</option>";
+													}
+												}
+												?>
+											</select>
+											<?php wp_nonce_field( 'jetpack_change_primary_user', '_my_connect_nonce' ); ?>
+											<input type="submit" name="jetpack-set-master-user" id="save-primary-btn" class="button button-primary" value="Save" title="Set the primary account holder"/>
+										</form>
+									</div>
+								</div>
+							</div>
+						<# } #><?php // End if show primary ?>
+
+					<?php if ( current_user_can( 'jetpack_configure_modules' ) ) : ?>
+						<?php // Disconnect Site Button ?>
+						<div class="j-row disconnect">
+							<div class="j-col j-lrg-12 j-md-12 j-sm-12">
+								<a class="button" href="<?php echo wp_nonce_url( Jetpack::admin_url( 'action=disconnect' ), 'jetpack-disconnect' ); ?>" onclick="return confirm('<?php echo htmlspecialchars( __( 'Are you sure you want to disconnect from WordPress.com?', 'jetpack' ), ENT_QUOTES ); ?>');"><?php esc_html_e( 'Disconnect site from WordPress.com', 'jetpack' ); ?></a>
+							</div>
+						</div>
+
+						<div id="jetpack-disconnect-content">
+							<h2>Disconnecting Jetpack</h2>
+							<p>Before you completely disconnect Jetpack is there anything we can do to help?</p>
+							<a class="button" title="Disconnect Jetpack" href="<?php echo wp_nonce_url( Jetpack::admin_url( 'action=disconnect' ), 'jetpack-disconnect' ); ?>">Confirm Disconnect</a>
+							<a class="button primary" target="_blank" title="Jetpack Support" href="http://jetpack.me/contact-support/">I Need Support</a>
+					</div>
+					<?php endif;?>
+				</div>
+			</script>
+
+	<?php else : ?>
+		<div class="wpcom-connect">
+			<h1 title="Boost traffic, enhance security, and improve performance."><?php esc_html_e( 'Boost traffic, enhance security, and improve performance.', 'jetpack' ); ?></h1>
+			<div class="j-row">
+				<div class="j-col j-sm-12 j-md-8 j-lrg-7 connect-desc">
+					<p><?php _e('Jetpack connects your site to WordPress.com to give you traffic and customization tools, enhanced security, speed boosts, and more.', 'jetpack' ); ?></p>
+					<p><?php _e('To start using Jetpack, connect to your WordPress.com account by clicking the button (if you don’t have an account you can create one quickly and for free).', 'jetpack' ); ?></p>
+				</div>
+				<div class="j-col j-sm-12 j-md-4 j-lrg-5 connect-btn">
+					<?php if ( ! $data['is_connected'] && current_user_can( 'jetpack_connect' ) ) : ?>
+						<a href="<?php echo Jetpack::init()->build_connect_url() ?>" class="download-jetpack"><?php esc_html_e( 'Connect to WordPress.com', 'jetpack' ); ?></a>
+					<?php elseif ( $data['is_connected'] && ! $data['is_user_connected'] && current_user_can( 'jetpack_connect_user' ) ) : ?>
+						<a href="<?php echo Jetpack::init()->build_connect_url() ?>" class="download-jetpack"><?php esc_html_e( 'Link your account to WordPress.com', 'jetpack' ); ?></a>
+					<?php endif; ?>
+				</div>
+			</div>
+		</div>
+	<?php endif; ?>
+
+	<style>
+		#user-list, #save-primary-btn, .sweet {
+			display: none;
+		}
+	</style>
