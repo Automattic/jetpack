75291b9c6b5a39133f591fa1a4ff3c4c13cd8351
dereksmart <smart@automattic.com>
2015-05-28 11:48:14 -0400

    My Connection Page: Add initial functionality
    adds the Jetapck connection information about the logged in user and the master user to My Connection page
    - Admins can disconnect the Jetpack site, or their own link to wpcom
    - Will display who is the Master user for a site
    - Admins who are not master user will be able to set themselves as master
    - Other users can link/unlink their accounts
    
    New file: _inc/jp-connection.js
    New file: class.jetpack-my-jetpack-page.php
    New file: my-jetpack-page.php

--- _inc/header.php
+++ _inc/header.php
@@ -6,16 +6,21 @@
 
 				<ul class="main-nav">
 					<li class="jetpack-logo"><a href="<?php echo Jetpack::admin_url(); ?>" title="<?php esc_attr_e( 'Jetpack', 'jetpack' ); ?>" <?php if ( 'jetpack' == $current ) { echo 'class="current"'; } ?>><span><?php esc_html_e( 'Jetpack', 'jetpack' ); ?></span></a></li>
-					<?php if ( ( Jetpack::is_active() || Jetpack::is_development_mode() )
-						&& current_user_can( 'jetpack_manage_modules' )
-					 ) : ?>
+					<?php
+					if ( Jetpack::is_active() || Jetpack::is_development_mode() ) :
+						if ( current_user_can( 'jetpack_manage_modules' ) ) :
+					 ?>
 					<li class="jetpack-modules">
 						<a href="<?php echo Jetpack::admin_url( 'page=jetpack_modules' ); ?>" class="jp-button--settings <?php if ( 'jetpack_modules' == $current ) { echo 'current'; } ?>"><?php esc_html_e( 'Settings', 'jetpack' ); ?></a>
 					</li>
 					<li class="jetpack-modules">
 						<a href="http://jetpack.me/survey/?rel=<?php echo JETPACK__VERSION; ?>" class="jp-button--settings"><?php esc_html_e( 'Feedback', 'jetpack' ); ?></a>
 					</li>
-					<?php endif; ?>
+						<?php endif; // End if admin ?>
+					<li class="jetpack-modules">
+						<a title="View your Jetpack connection details" href="<?php echo Jetpack::admin_url( 'page=my_connection' ); ?>" id="my-connection" class="jp-button--settings"><?php esc_html_e( 'My Connection', 'jetpack' ); ?></a>
+					</li>
+					<?php endif; // End if connected or dev mode ?>
 				</ul>
 
 			</nav>
