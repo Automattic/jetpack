a68bdebba9ad94f8af589336be755e5ad91ebb02
jessefriedman <jesserfriedman@gmail.com>
2015-06-02 11:13:34 -0400

    fixed merge conflicts in the JITM messages

--- modules/related-posts.php
+++ modules/related-posts.php
@@ -10,10 +10,7 @@
  * Auto Activate: No
  * Module Tags: Recommended
  * Feature: Recommended, Jumpstart
-<<<<<<< HEAD
-=======
  * JITM Tags: related posts, related content, related
->>>>>>> 74fbe7e... improved and added additional JITM tags to modules
  */
 class Jetpack_RelatedPosts_Module {
 	/**
