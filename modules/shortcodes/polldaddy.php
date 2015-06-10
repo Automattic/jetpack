f51b97cc12b3c1cf6f45dca32ed7f5364fe2ecd5
Igor Zinovyev <igor.zinovyev@automattic.com>
2015-06-10 21:29:29 +0000

    Shortcodes: Adding a common ancestor for wpcom merges.

--- modules/shortcodes/polldaddy.php
+++ modules/shortcodes/polldaddy.php
@@ -1,7 +1,5 @@
 <?php
-
-if ( !class_exists( 'PolldaddyShortcode' ) ) {
-	/**
+/**
 * Class wrapper for polldaddy shortcodes
 */
 class PolldaddyShortcode {
@@ -84,7 +82,7 @@ class PolldaddyShortcode {
 				$item_id = is_page() ? '_page_'.$post->ID : '_post_'.$post->ID;
 	
 			if ( empty( $title ) )
-				$title = apply_filters( 'wp_title', $post->post_title, '', '' );
+				$title = apply_filters( 'wp_title', $post->post_title );
 	
 			if ( empty( $permalink ) )
 				$permalink = get_permalink( $post->ID );
@@ -139,7 +137,7 @@ CONTAINER;
 		
 			$poll      = intval( $poll );
 			$poll_url  = sprintf( 'http://polldaddy.com/poll/%d', $poll );
-			$poll_js   = sprintf( '%s.polldaddy.com/p/%d.js', ( is_ssl() ? 'https://secure' : 'http://static' ), $poll );
+			$poll_js   = sprintf( 'http://static.polldaddy.com/p/%d.js', $poll );
 			$poll_link = sprintf( '<a href="%s">Take Our Poll</a>', $poll_url );
 	
 			if ( $no_script )
@@ -383,16 +381,12 @@ SCRIPT;
 // kick it all off
 new PolldaddyShortcode();
 
-if ( !function_exists( 'polldaddy_link' ) ) {
-	// http://polldaddy.com/poll/1562975/?view=results&msg=voted
-	function polldaddy_link( $content ) {
-		return preg_replace( '!(?:\n|\A)http://polldaddy.com/poll/([0-9]+?)/(.+)?(?:\n|\Z)!i', "\n<script type='text/javascript' language='javascript' charset='utf-8' src='http://static.polldaddy.com/p/$1.js'></script><noscript> <a href='http://polldaddy.com/poll/$1/'>View Poll</a></noscript>\n", $content );
-	}
-	
-	// higher priority because we need it before auto-link and autop get to it
-	add_filter( 'the_content', 'polldaddy_link', 1 );
-	add_filter( 'the_content_rss', 'polldaddy_link', 1 );
-	add_filter( 'comment_text', 'polldaddy_link', 1 );
+// http://polldaddy.com/poll/1562975/?view=results&msg=voted
+function polldaddy_link( $content ) {
+	return preg_replace( '!(?:\n|\A)http://polldaddy.com/poll/([0-9]+?)/(.+)?(?:\n|\Z)!i', "\n<script type='text/javascript' language='javascript' charset='utf-8' src='http://static.polldaddy.com/p/$1.js'></script><noscript> <a href='http://polldaddy.com/poll/$1/'>View Poll</a></noscript>\n", $content );
 }
 
-}
+// higher priority because we need it before auto-link and autop get to it
+add_filter( 'the_content', 'polldaddy_link', 1 );
+add_filter( 'the_content_rss', 'polldaddy_link', 1 );
+add_filter( 'comment_text', 'polldaddy_link', 1 );
