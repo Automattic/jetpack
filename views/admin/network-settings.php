5df397a4912a31d9e0080cf85c9f90e876f04973
Rocco Tripaldi <tripaldir@gmail.com>
2015-06-09 21:17:59 -0400

    Network Admin: adding user IP to the network settings page
    Mimicking the Protect settings page for a single site,
    the network admin will also show the current user's
    IP address.

--- views/admin/network-settings.php
+++ views/admin/network-settings.php
@@ -35,6 +35,7 @@
 			<tr valign="top">
 				<th scope="row"><label for="sub-site-override"><?php _e( 'Protect whitelist', 'jetpack' ); ?></label></th>
 				<td>
+					<p><strong><?php printf( __( 'Your current IP: %s', 'jetpack' ), jetpack_protect_get_ip() ); ?></strong></p>
 					<textarea name="global-whitelist" style="width: 100%;" rows="8"><?php echo implode( PHP_EOL, $jetpack_protect_whitelist['global'] ); ?></textarea> <br />
 					<label for="global-whitelist"><?php _e('IPv4 and IPv6 are acceptable. <br />To specify a range, enter the low value and high value separated by a dash. Example: 12.12.12.1-12.12.12.100', 'jetpack' ); ?></label>
 				</td>
