ed9219a4ae7a0fc22650160015524ae4af1f9fc7
jessefriedman <jesserfriedman@gmail.com>
2015-06-11 08:41:17 -0400

    Removed the disconnect and unlink links from footer
    We want to funnel user traffic to disconnect their site to the “My
    Connection” page so we can provide them with additional options and
    support before they disconnect

--- _inc/footer.php
+++ _inc/footer.php
@@ -1,12 +1,4 @@
-<?php
-global $current_user;
-$is_active         = Jetpack::is_active();
-$user_token        = Jetpack_Data::get_access_token( $current_user->ID );
-$is_user_connected = $user_token && ! is_wp_error( $user_token );
-$is_master_user    = $current_user->ID == Jetpack_Options::get_option( 'master_user' );
-?>
-
-		</div><!-- .wrapper -->
+	</div><!-- .wrapper -->
 		<div class="footer">
 
 			<nav class="primary nav-horizontal">
@@ -27,14 +19,6 @@ $is_master_user    = $current_user->ID == Jetpack_Options::get_option( 'master_u
 					<a href="<?php echo esc_url( Jetpack::admin_url( 'page=jetpack-debugger' ) ); ?>" title="<?php esc_attr_e( 'Test your site&#8217;s compatibility with Jetpack.', 'jetpack' ); ?>"><?php _e( 'Debug', 'jetpack' ); ?></a>
 					<a href="http://jetpack.me/contact-support/" title="<?php esc_attr_e( 'Contact the Jetpack Happiness Squad.', 'jetpack' ); ?>"><?php _e( 'Support', 'jetpack' ); ?></a>
 					<a href="http://jetpack.me/survey/?rel=<?php echo JETPACK__VERSION; ?>" title="<?php esc_attr_e( 'Take a survey.  Tell us how we&#8217;re doing.', 'jetpack' ); ?>"><?php _e( 'Give Us Feedback', 'jetpack' ); ?></a>
-
-					<?php if ( $is_active && current_user_can( 'jetpack_disconnect' ) ) : ?>
-						<a href="<?php echo wp_nonce_url( Jetpack::admin_url( 'action=disconnect' ), 'jetpack-disconnect' ); ?>" onclick="return confirm('<?php echo htmlspecialchars( __('Are you sure you want to disconnect from WordPress.com?', 'jetpack'), ENT_QUOTES ); ?>');"><?php esc_html_e( 'Disconnect from WordPress.com', 'jetpack' ); ?></a>
-					<?php endif; ?>
-					<?php if ( $is_active && $is_user_connected && ! $is_master_user ) : ?>
-						<a href="<?php echo wp_nonce_url( Jetpack::admin_url( 'action=unlink' ), 'jetpack-unlink' ); ?>"><?php esc_html_e( 'Unlink your user account', 'jetpack' ); ?></a>
-					<?php endif; ?>
-
 				</div>
 			</nav><!-- .secondary -->
 		</div><!-- .footer -->
