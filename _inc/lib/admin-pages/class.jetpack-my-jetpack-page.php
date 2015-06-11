e64d08d54b1817a8bbc73a279d2e45b752af33e1
dereksmart <smart@automattic.com>
2015-06-11 10:04:02 -0400

    My Connection is now My Jetpack
    file/element refactor to now call the page "My Jetpack" instead of My Connection

--- _inc/lib/admin-pages/class.jetpack-my-jetpack-page.php
+++ _inc/lib/admin-pages/class.jetpack-my-jetpack-page.php
@@ -2,27 +2,27 @@
 include_once( 'class.jetpack-admin-page.php' );
 include_once( JETPACK__PLUGIN_DIR . 'class.jetpack-modules-list-table.php' );
 
-// Builds the My Connection page
-class Jetpack_My_Connection_Page extends Jetpack_Admin_Page {
+// Builds the My Jetpack page
+class Jetpack_My_Jetpack_Page extends Jetpack_Admin_Page {
 	// Show the settings page only when Jetpack is connected or in dev mode
 	protected $dont_show_if_not_active = true;
 	function add_page_actions( $hook ) {} // There are no page specific actions to attach to the menu
 
-	// Adds the My Connection page, but hides it from the submenu
+	// Adds the My Jetpack page, but hides it from the submenu
 	function get_page_hook() {
 		return add_submenu_page( null, __( 'My Jetpack', 'jetpack' ), __( 'My Jetpack', 'jetpack' ), 'jetpack_connect_user', 'my_jetpack', array( $this, 'render' ) );
 	}
 
 	// Renders the view file
 	function page_render() {
-		Jetpack::init()->load_view( 'admin/my-connection-page.php' );
+		Jetpack::init()->load_view( 'admin/my-jetpack-page.php' );
 	}
 
 	/*
 	 * Handle the change in master user
 	 */
-	function jetpack_my_connection_change_user() {
-		if ( ! isset( $_POST['_my_connect_nonce'] ) || ! wp_verify_nonce( $_POST['_my_connect_nonce'], 'jetpack_change_primary_user' ) ) {
+	function jetpack_my_jetpack_change_user() {
+		if ( ! isset( $_POST['_my_jetpack_nonce'] ) || ! wp_verify_nonce( $_POST['_my_jetpack_nonce'], 'jetpack_change_primary_user' ) ) {
 			wp_die( __( 'Failed permissions, please try again.', 'jetpack' ) );
 			exit;
 		}
@@ -39,7 +39,7 @@ class Jetpack_My_Connection_Page extends Jetpack_Admin_Page {
 
 	/*
 	 * Determine if we should show the primary user info
-	 * on the My Connection page
+	 * on the My Jetpack page
 	 *
 	 * @return (bool) True = show | False = Don't show
 	 */
@@ -92,7 +92,7 @@ class Jetpack_My_Connection_Page extends Jetpack_Admin_Page {
 	 *
 	 * @return array
 	 */
-	function jetpack_my_connection_logic() {
+	function jetpack_my_jetpack_logic() {
 		global $current_user;
 		$is_user_connected    = Jetpack::is_user_connected( $current_user->ID );
 		$is_master_user       = $current_user->ID == Jetpack_Options::get_option( 'master_user' );
@@ -118,12 +118,12 @@ class Jetpack_My_Connection_Page extends Jetpack_Admin_Page {
 	function page_admin_scripts() {
 		wp_enqueue_script( 'jp-connection-js', plugins_url( '_inc/jp-connection.js', JETPACK__PLUGIN_FILE ), array( 'jquery', 'wp-util' ), JETPACK__VERSION . 'yep' );
 
-		$master_user_com_data = $this->jetpack_my_connection_logic();
+		$master_user_com_data = $this->jetpack_my_jetpack_logic();
 		$jetpack_user_data    = Jetpack::get_connected_user_data();
 		$current_user         = wp_get_current_user();
 		wp_localize_script( 'jp-connection-js', 'jpConnection',
 			array(
-				'connectionLogic'    => $this->jetpack_my_connection_logic(),
+				'connectionLogic'    => $this->jetpack_my_jetpack_logic(),
 				'jetpackIsActive'    => Jetpack::is_active(),
 				'showPrimaryUserRow' => $this->jetpack_show_primary_user_row(),
 				'masterComData'      => $master_user_com_data['master_data_com'],
