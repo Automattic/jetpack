bf269798c136da3bb6a4d4148f219ebf07f2c666
jeffgolenski <jeffgolenski@gmail.com>
2015-06-12 11:32:04 -0400

    added 2nd feedback link, changed link
    per Mikey Arce’s request: added 2nd feedback link at the top, updated
    link location.

--- admin/jpbeta-admin-main.php
+++ admin/jpbeta-admin-main.php
@@ -8,6 +8,14 @@ $testing_checklist = jpbeta_get_testing_list();
 ?>
 <style type="text/css">
 
+	.j-beta-wrap {
+		max-width: 574px;
+	}
+
+	.j-beta-wrap h2 {
+		line-height: 125%;
+	}
+
 	.jetpack_page_jetpack-beta .card h3 {
 		margin-bottom: 20px;
 	}
@@ -30,6 +38,16 @@ $testing_checklist = jpbeta_get_testing_list();
 		display: inline-block;
 	}
 
+	.jetpack_page_jetpack-beta .feedback.beta-top {
+		float: right;
+	}
+
+	@media (max-width: 574px) {
+		.jetpack_page_jetpack-beta .feedback.beta-top {
+			padding-right: 10px;
+		}
+	}
+
 	.jetpack_page_jetpack-beta .card .autoupdate {
 		border-top: 1px #eee solid;
 		margin: 20px 0;
@@ -37,26 +55,29 @@ $testing_checklist = jpbeta_get_testing_list();
 
 </style>
 
-<h2 title="<?php _e('Jetpack Beta Settings', 'jpbeta'); ?>"><?php _e('Jetpack Beta Settings', 'jpbeta'); ?></h2>
+<div class="j-beta-wrap">
+	<a title="Submit Your Feedback" class="feedback beta-top" href="http://jetpack.me/contact-support/beta-group/"><?php _e('Submit Your Feedback', 'jpbeta'); ?></a>
+	<h2 title="<?php _e('Jetpack Beta Settings', 'jpbeta'); ?>"><?php _e('Jetpack Beta Settings', 'jpbeta'); ?></h2>
 
-<div class="card">
-	<h3 title="<?php _e('Use Jetpack Version', 'jpbeta'); ?>"><?php _e('Use Jetpack Version', 'jpbeta'); ?>:</h3>
+	<div class="card">
+		<h3 title="<?php _e('Use Jetpack Version', 'jpbeta'); ?>"><?php _e('Use Jetpack Version', 'jpbeta'); ?>:</h3>
 
-	<form method="post" id="jp_beta_choose_type">
-		<ul>
-			<li><p><input type="radio" name="version_type" value="latest" <?php echo ( $jp_beta_type == 'rc_only' ? '' : 'checked="checked"' ); ?>> <strong><?php _e('Latest Beta', 'jpbeta'); ?></strong></p><p><?php _e('This might be updated anywhere from once a week to multiple times a day.', 'jpbeta'); ?></p></li>
-			<li><p><input type="radio" name="version_type" value="rc_only" <?php echo ( $jp_beta_type == 'rc_only' ? 'checked="checked"' : '' ); ?>> <strong><?php _e('Release Candidates Only', 'jpbeta'); ?></strong></p><p><?php _e('These are our tagged pre-releases, and there are generally 2-3 per Jetpack version.', 'jpbeta'); ?></p></li>
-		</ul>
-		<ul class="autoupdate">
-			<li><p><input type="checkbox" name="auto_update" value="1" <?php echo ( $jp_beta_autoupdate == 'no' ? '' : 'checked="checked"' ); ?>> <strong><?php _e('Auto-Update Jetpack when new betas are available', 'jpbeta'); ?></strong></p><p><?php _e('This only runs every 12 hours, so you might want to manually update sooner.', 'jpbeta'); ?></p></li>
-		</ul>
-		<input class="button-primary" type="submit" value="<?php _e('Save my choice', 'jpbeta'); ?>" name="submit">
-		<?php wp_nonce_field( 'jp_beta_recent_save' , 'jp_beta_recent_save_nonce' ); ?>
-	</form>
-</div>
+		<form method="post" id="jp_beta_choose_type">
+			<ul>
+				<li><p><input type="radio" name="version_type" value="latest" <?php echo ( $jp_beta_type == 'rc_only' ? '' : 'checked="checked"' ); ?>> <strong><?php _e('Latest Beta', 'jpbeta'); ?></strong></p><p><?php _e('This might be updated anywhere from once a week to multiple times a day.', 'jpbeta'); ?></p></li>
+				<li><p><input type="radio" name="version_type" value="rc_only" <?php echo ( $jp_beta_type == 'rc_only' ? 'checked="checked"' : '' ); ?>> <strong><?php _e('Release Candidates Only', 'jpbeta'); ?></strong></p><p><?php _e('These are our tagged pre-releases, and there are generally 2-3 per Jetpack version.', 'jpbeta'); ?></p></li>
+			</ul>
+			<ul class="autoupdate">
+				<li><p><input type="checkbox" name="auto_update" value="1" <?php echo ( $jp_beta_autoupdate == 'no' ? '' : 'checked="checked"' ); ?>> <strong><?php _e('Auto-Update Jetpack when new betas are available', 'jpbeta'); ?></strong></p><p><?php _e('This only runs every 12 hours, so you might want to manually update sooner.', 'jpbeta'); ?></p></li>
+			</ul>
+			<input class="button-primary" type="submit" value="<?php _e('Save my choice', 'jpbeta'); ?>" name="submit">
+			<?php wp_nonce_field( 'jp_beta_recent_save' , 'jp_beta_recent_save_nonce' ); ?>
+		</form>
+	</div>
 
-<div class="card">
-<?php echo $testing_checklist; ?>
+	<div class="card">
+	<?php echo $testing_checklist; ?>
 
-<a title="Submit Your Feedback" class="feedback" href="http://jetpack.me/contact-support/"><?php _e('Submit Your Feedback', 'jpbeta'); ?></a>
-</div>
+	<a title="Submit Your Feedback" class="feedback" href="http://jetpack.me/contact-support/beta-group/"><?php _e('Submit Your Feedback', 'jpbeta'); ?></a>
+	</div>
+</div><?php // j-beta-wrap ?>
