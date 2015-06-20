b9ad1d306582e61718787ff04890b1c1d80a779d
Jeremy Herve <jeremy@tagada.hu>
2015-06-20 13:32:28 +0000

    Site Icon: add dimensions to use in Open Graph Image tags

--- functions.opengraph.php
+++ functions.opengraph.php
@@ -248,7 +248,9 @@ function jetpack_og_get_image( $width = 200, $height = 200, $max_images = 4 ) { 
 
 	// Third fall back, Site Icon
 	if ( empty( $image ) && ( function_exists( 'jetpack_has_site_icon' ) && jetpack_has_site_icon() ) ) {
-		$image['src'] = jetpack_site_icon_url( null, '512' );
+		$image['src']     = jetpack_site_icon_url( null, '512' );
+		$image['width']   = '512';
+		$image['height']  = '512';
 	}
 
 	// Fourth fall back, blank image
