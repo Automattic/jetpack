a6b7d18437c85f9ab46408aa87b43d5a22c1d5db
Andrew Duthie <andrew.duthie@automattic.com>
2015-06-29 14:10:00 +0000

    Sharing Buttons: Facebook: Trigger Facebook SDK re-parse after infinite scroll load
    See: https://github.com/Automattic/io/issues/4
    See: https://[private link]
    See: https://[private link]
    
    Merges r120337-wpcom.

--- modules/sharedaddy/sharing-sources.php
+++ modules/sharedaddy/sharing-sources.php
@@ -699,7 +699,17 @@ class Share_Facebook extends Sharing_Source {
 			if ( ! $locale ) {
 				$locale = 'en_US';
 			}
-			?><div id="fb-root"></div><script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = '//connect.facebook.net/<?php echo $locale; ?>/sdk.js#xfbml=1&appId=249643311490&version=v2.3'; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));</script><?php
+			?>
+			<div id="fb-root"></div>
+			<script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = '//connect.facebook.net/<?php echo $locale; ?>/sdk.js#xfbml=1&appId=249643311490&version=v2.3'; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));</script>
+			<script>
+			jQuery( document.body ).on( 'post-load', function() {
+				if ( 'undefined' !== typeof FB ) {
+					FB.XFBML.parse();
+				}
+			} );
+			</script>
+			<?php
 		}
 	}
 }
