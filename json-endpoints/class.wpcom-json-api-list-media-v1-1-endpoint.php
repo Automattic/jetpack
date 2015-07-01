fdf5657d739ede3d235f36de84ff4854f5ce1abd
Michael Adams <mike@automattic.com>
2015-07-01 16:55:32 +0000

    JSON API: Filtering list of media items by post ID of parent post has never worked in v1.1.
    Fix copy/paste typo from v1
    
    Merges r120519-wpcom.

--- json-endpoints/class.wpcom-json-api-list-media-v1-1-endpoint.php
+++ json-endpoints/class.wpcom-json-api-list-media-v1-1-endpoint.php
@@ -32,12 +32,10 @@ class WPCOM_JSON_API_List_Media_v1_1_Endpoint extends WPCOM_JSON_API_Endpoint {
 			$this->date_range['after'] = $args['after'];
 		}
 
-
-
-        $query = array(
+		$query = array(
 			'post_type'      => 'attachment',
 			'post_status'    => 'inherit',
-			'post_parent'    => isset( $args['parent_id'] ) ? $args['parent_id'] : null,
+			'post_parent'    => isset( $args['post_ID'] ) ? $args['post_ID'] : null,
 			'offset'         => isset( $args['offset'] ) ? $args['offset'] : null,
 			'posts_per_page' => $args['number'],
 			'post_mime_type' => isset( $args['mime_type'] ) ? $args['mime_type'] : null,
