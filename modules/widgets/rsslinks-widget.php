764a4ea78fa54a953ece55a5746609566b632839
Jeremy Herve <jeremy@tagada.hu>
2015-07-09 17:29:32 +0200

    RSS Links Widget: add docblocks

--- modules/widgets/rsslinks-widget.php
+++ modules/widgets/rsslinks-widget.php
@@ -18,6 +18,7 @@ class Jetpack_RSS_Links_Widget extends WP_Widget {
 
 		extract( $args );
 
+		/** This filter is documented in core/src/wp-includes/default-widgets.php */
 		$title = apply_filters( 'widget_title', $instance['title'] );
 		echo $before_widget;
 
@@ -158,6 +159,13 @@ class Jetpack_RSS_Links_Widget extends WP_Widget {
 		$link_item = '';
 		$format = $args['format'];
 
+		/**
+		 * Filters the target link attribute for the RSS link in the RSS widget.
+		 *
+		 * @since 3.4.0
+		 *
+		 * @param bool false Control whether the link should open in a new tab. Default to false.
+		 */
 		if ( apply_filters( 'jetpack_rsslinks_widget_target_blank', false ) ) {
 			$link_target = '_blank';
 		} else {
