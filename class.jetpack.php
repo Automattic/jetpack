7952727ac0fb2e3e67f1e39ca46f7e65c127a229
dereksmart <smart@automattic.com>
2015-07-17 10:51:51 -0400

    Improve string format for translators

--- class.jetpack.php
+++ class.jetpack.php
@@ -969,7 +969,7 @@ class Jetpack {
 
 		// Throw up a notice if using a development version and as for feedback.
 		if ( Jetpack::is_development_version() ) {
-			$notice = sprintf( __( 'You are currently running a development version of Jetpack.  %1sSubmit your feedback%2s', 'jetpack' ), '<a href="https://jetpack.me/contact-support/beta-group/" target="_blank">', '</a>' );
+			$notice = sprintf( _x( 'You are currently running a development version of Jetpack.  %1s Submit your feedback. %2s', '%1s & %2s are HTML tags', 'jetpack' ), '<a href="https://jetpack.me/contact-support/beta-group/" target="_blank">', '</a>' );
 
 			echo '<div class="error"><p>' . $notice . '</p></div>';
 		}
