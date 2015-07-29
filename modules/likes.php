f259eb8a56868ca9ce31ee75517efd7a2ac21e8a
Jeremy Herve <jeremy@tagada.hu>
2015-07-29 14:53:22 +0000

    Likes: Delete the Email notif. option when Likes get deactivated.
    Fixes #2456

--- modules/likes.php
+++ modules/likes.php
@@ -40,6 +40,7 @@ class Jetpack_Likes {
 			add_action( 'jetpack_activate_module_likes',   array( $this, 'maybe_sync_content' ) );
 			add_action( 'jetpack_activate_module_likes',   array( $this, 'module_toggle' ) );
 			add_action( 'jetpack_deactivate_module_likes', array( $this, 'module_toggle' ) );
+			add_action( 'jetpack_deactivate_module_likes', array( $this, 'delete_social_notifications_like' ) );
 
 			Jetpack::enable_module_configurable( __FILE__ );
 			Jetpack::module_configuration_load( __FILE__, array( $this, 'configuration_redirect' ) );
@@ -98,6 +99,17 @@ class Jetpack_Likes {
 	function module_toggle() {
 		$jetpack = Jetpack::init();
 		$jetpack->sync->register( 'noop' );
+	}
+
+	/**
+	 * Delete the social_notifications_like option that was set to `on` on module activation.
+	 *
+	 * @since 3.7.0
+	 *
+	 * @return null
+	 */
+	function delete_social_notifications_like() {
+		delete_option( 'social_notifications_like' );
 	}
 
 	/**
