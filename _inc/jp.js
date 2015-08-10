c6e56f2db9012eab95b8ab008ea67290e3d41ea9
richardmuscat <richard.muscat@gmail.com>
2015-08-10 12:23:14 +0200

    Jump Start: Also hide "Skip" link
    When the blue Jump Start button is clicked also hide the “Skip” link.

--- _inc/jp.js
+++ _inc/jp.js
@@ -253,6 +253,7 @@
 
 			$( '.jumpstart-spinner' ).show().css( 'display', 'block' );
 			$( '#jump-start' ).hide();
+			$( '.dismiss-jumpstart' ).hide();
 
 			data.jumpStartActivate = 'jump-start-activate';
 			data.action = 'jetpack_jumpstart_ajax';
