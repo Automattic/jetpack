34423a3efb6d7bcdf1b0cfcbf068a50d65c4b154
dereksmart <smart@automattic.com>
2015-08-12 09:27:46 -0400

    NUX: Toggle manage CTA button correctly if Jump-started

--- _inc/jp.js
+++ _inc/jp.js
@@ -301,8 +301,8 @@
 					$( checkBox ).prop( 'checked', true );
 				});
 
-				$( '.jumpstart-spinner, .jstart, #jumpstart-cta' ).hide();
-				$( '.jumpstart-message, .miguel' ).toggle();
+				$( '.jumpstart-spinner, .jstart, #jumpstart-cta, .manage-cta-inactive' ).hide();
+				$( '.jumpstart-message, .miguel, .manage-cta-active' ).toggle();
 				$( '#jump-start-area' ).delay( 5000 ).hide( 600 );
 
 				// Log Jump Start event in MC Stats
