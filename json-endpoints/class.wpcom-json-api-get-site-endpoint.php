d8e0bafbd22e12381e534ab37b76d2abc8e46d3f
Rocco Tripaldi <tripaldir@gmail.com>
2015-08-13 11:48:35 -0400

    Get Site Endpoint
    Remove modifications because they will be
    made instead by the build script.

--- json-endpoints/class.wpcom-json-api-get-site-endpoint.php
+++ json-endpoints/class.wpcom-json-api-get-site-endpoint.php
@@ -329,9 +329,7 @@ class WPCOM_JSON_API_GET_Site_Endpoint extends WPCOM_JSON_API_Endpoint {
 					// If the sync happends right then we should be able to see that we are not dealing with a network site
 					$response['options']['is_multi_network'] = (bool) get_option( 'jetpack_is_main_network', true  );
 					$response['options']['is_multi_site'] = (bool) get_option( 'jetpack_is_multi_site', true );
-
-					// Show available updates.
-					$response['options']['updates'] = Jetpack_Options::get_option( 'updates' );
+					
 				}
 
 				if ( ! current_user_can( 'edit_posts' ) )
