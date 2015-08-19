6fa5bf989e9dd64156a32b8c122ff8bad4cc924d
dereksmart <smart@automattic.com>
2015-08-19 09:33:47 -0400

    Correct translation function

--- views/admin/my-jetpack-page.php
+++ views/admin/my-jetpack-page.php
@@ -143,7 +143,7 @@
 						<div class="j-col j-lrg-12 j-md-12 j-sm-12">
 							<h2><?php _e( 'Disconnecting Jetpack', 'jetpack' ); ?></h2>
 							<p><?php _e( 'Before you completely disconnect Jetpack is there anything we can do to help?', 'jetpack' ); ?></p>
-							<a class="button" id="confirm-disconnect" title="<?php esc_attr_e( 'Disconnect Jetpack', 'jetpack' ); ?>" href="<?php echo wp_nonce_url( Jetpack::admin_url( 'action=disconnect' ), 'jetpack-disconnect' ); ?>"><?php __( 'Confirm Disconnect', 'jetpack' ); ?></a>
+							<a class="button" id="confirm-disconnect" title="<?php esc_attr_e( 'Disconnect Jetpack', 'jetpack' ); ?>" href="<?php echo wp_nonce_url( Jetpack::admin_url( 'action=disconnect' ), 'jetpack-disconnect' ); ?>"><?php _e( 'Confirm Disconnect', 'jetpack' ); ?></a>
 							<a class="button primary" id="support-no-disconnect" target="_blank" title="<?php esc_attr_e( 'Jetpack Support', 'jetpack' ); ?>" href="http://jetpack.me/contact-support/"><?php esc_html_e( 'I Need Support', 'jetpack' ); ?></a>
 							<a class="cancel-disconnect" id="cancel-disconnect" target="_blank" title="<?php esc_attr_e( 'cancel', 'jetpack' ); ?>" href="#"><?php esc_html_e( 'cancel', 'jetpack' ); ?></a>
 
