f4e89ce6d36ba687117c36c360ed5bc3e888503a
Igor Zinovyev <igor.zinovyev@automattic.com>
2015-09-08 21:23:35 +0000

    Related Posts: resolve divergence from dotcom code
    Merges r123794-wpcom.

--- modules/related-posts/jetpack-related-posts.php
+++ modules/related-posts/jetpack-related-posts.php
@@ -772,8 +772,10 @@ EOT;
 			'title' => $this->_to_utf8( $this->_get_title( $post->post_title, $post->post_content ) ),
 			'date' => get_the_date( '', $post->ID ),
 			'format' => get_post_format( $post->ID ),
+			'excerpt' => html_entity_decode( $this->_to_utf8( $this->_get_excerpt( $post->post_excerpt, $post->post_content ) ), ENT_QUOTES, 'UTF-8' ),
 			/**
 			 * Filters the rel attribute for the Related Posts' links.
+			 *
 			 *
 			 * @module related-posts
 			 *
@@ -783,9 +785,9 @@ EOT;
 			 * @param int $post->ID Post ID.
 			 */
 			'rel' => apply_filters( 'jetpack_relatedposts_filter_post_link_rel', 'nofollow', $post->ID ),
-			'excerpt' => html_entity_decode( $this->_to_utf8( $this->_get_excerpt( $post->post_excerpt, $post->post_content ) ), ENT_QUOTES, 'UTF-8' ),
 			/**
 			 * Filter the context displayed below each Related Post.
+			 *
 			 *
 			 * @module related-posts
 			 *
