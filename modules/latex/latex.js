9bde70317e3298296a3f3e903a673943574a1c01
Ryan Martinsen <rmartinsen@about.com>
2015-09-22 10:51:26 -0400

    Only run MathJax Typeset in the newest infinite-wrap element.

--- modules/latex/latex.js
+++ modules/latex/latex.js
@@ -1,8 +1,12 @@
 ( function( $ ) {
-	$( document.body ).on( 'post-load', function() {
+	$( document.body ).on( 'post-load', function( response ) {
 		// New posts have been added to the page. Re-typeset MathJax if necessary.
 		if ( typeof MathJax !== 'undefined' ) {
-			MathJax.Hub.Queue( [ 'Typeset', MathJax.Hub ] );
+			MathJax.Hub.Queue( [
+				'Typeset',
+				MathJax.Hub,
+				jQuery('.infinite-wrap').get().pop() // Only typeset in the newest infinite-wrap element.
+			] );
 		}
 	} );
 } )( jQuery );
