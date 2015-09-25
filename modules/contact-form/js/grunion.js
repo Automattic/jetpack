af2eebf47a05cd16708b3b1aff41569e971b9d71
dereksmart <smart@automattic.com>
2015-09-25 16:11:35 -0400

    Contact Form: Fix multiple emails not parsing commas
    We need to do a global replace for instances where there are more than one comma

--- modules/contact-form/js/grunion.js
+++ modules/contact-form/js/grunion.js
@@ -395,7 +395,7 @@ FB.ContactForm = (function() {
 				// Remove new lines that cause BR tags to show up
 				response = response.replace(/\n/g,' ');
 				// Convert characters to comma
-				response = response.replace( '%26#x002c;' , ',' );
+				response = response.replace( /%26#x002c;/g , ',' );
 
 				// Add new shortcode
 				if (currentCode.match(regexp)) {
