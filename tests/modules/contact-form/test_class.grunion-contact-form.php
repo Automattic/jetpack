43ec3c81bc81edf005be6006a4a54cd141da2fa9
Brandon Kraft <public@brandonkraft.com>
2015-09-30 09:24:31 -0500

    Contact Form: Update unit test to play nicely with 5.2

--- tests/modules/contact-form/test_class.grunion-contact-form.php
+++ tests/modules/contact-form/test_class.grunion-contact-form.php
@@ -299,7 +299,9 @@ class WP_Test_Grunion_Contact_Form extends WP_UnitTestCase {
 		$expected .= 'Radio: Second option' . PHP_EOL;
 		$expected .= 'Text: Texty text';
 
-		$email_body = explode( PHP_EOL . PHP_EOL, $email['message'] )[0];
+		$email_body = explode( PHP_EOL . PHP_EOL, $email['message'] );
+
+		$email_body = $email_body[0];
 
 		$this->assertEquals( $expected, $email_body );
 	}
@@ -317,20 +319,7 @@ class WP_Test_Grunion_Contact_Form extends WP_UnitTestCase {
 			'text'     => 'Texty text'
 		) );
 
-		add_filter( 'wp_mail', function( $args ) {
-			$this->assertContains( 'mellow@hello.com', $args['to'] );
-			$this->assertEquals( 'Hello there!', $args['subject'] );
-
-			$expected = 'Name: John Doe' . PHP_EOL;
-			$expected .= 'Dropdown: First option' . PHP_EOL;
-			$expected .= 'Radio: Second option' . PHP_EOL;
-			$expected .= 'Text: Texty text';
-
-			// Divides email by the first empty line
-			$email_body = explode( PHP_EOL . PHP_EOL, $args['message'] )[0];
-
-			$this->assertEquals( $expected, $email_body );
-		} );
+		add_filter( 'wp_mail', array( $this, 'pre_test_process_submission_sends_correct_email' ) );
 
 		// Initialize a form with name, dropdown and radiobutton (first, second
 		// and third option), text field
@@ -338,14 +327,29 @@ class WP_Test_Grunion_Contact_Form extends WP_UnitTestCase {
 		$form->process_submission();
 	}
 
+	public function pre_test_process_submission_sends_correct_email( $args ){
+		$this->assertContains( 'mellow@hello.com', $args['to'] );
+		$this->assertEquals( 'Hello there!', $args['subject'] );
+
+		$expected = 'Name: John Doe' . PHP_EOL;
+		$expected .= 'Dropdown: First option' . PHP_EOL;
+		$expected .= 'Radio: Second option' . PHP_EOL;
+		$expected .= 'Text: Texty text';
+
+		// Divides email by the first empty line
+		$email_body = explode( PHP_EOL . PHP_EOL, $args['message'] );
+
+		$email_body = $email_body[0];
+
+		$this->assertEquals( $expected, $email_body );
+	}
+
 	/**
 	 * @author tonykova
 	 * @covers Grunion_Contact_Form::process_submission
 	 */
 	public function test_process_submission_fails_if_spam_marked_with_WP_Error() {
-		add_filter( 'jetpack_contact_form_is_spam', function() {
-			return new WP_Error( 'spam', 'Message is spam' );
-		}, 11 ); // Run after akismet filter
+		add_filter( 'jetpack_contact_form_is_spam', array( $this, 'pre_test_process_submission_fails_if_spam_marked_with_WP_Error' ), 11 ); // Run after akismet filter
 
 		$form = new Grunion_Contact_Form( array() );
 		$result = $form->process_submission();
@@ -354,21 +358,25 @@ class WP_Test_Grunion_Contact_Form extends WP_UnitTestCase {
 		$this->assertEquals( 'Message is spam', $result->get_error_message() );
 	}
 
+	public function pre_test_process_submission_fails_if_spam_marked_with_WP_Error(){
+		return new WP_Error( 'spam', 'Message is spam' );
+	}
+
 	/**
 	 * @author tonykova
 	 * @covers Grunion_Contact_Form::process_submission
 	 */
 	public function test_process_submission_wont_send_spam_if_marked_as_spam_with_true() {
-		add_filter( 'jetpack_contact_form_is_spam', function() {
-			return true;
-		}, 11 ); // Run after akismet filter
+		add_filter( 'jetpack_contact_form_is_spam', '__return_true', 11 ); // Run after akismet filter
 
-		add_filter( 'wp_mail', function( $args ) {
-			$this->assertTrue( false ); // Fail if trying to send
-		} );
+		add_filter( 'wp_mail', array( $this, 'pre_test_process_submission_wont_send_spam_if_marked_as_spam_with_true' ) );
 
 		$form = new Grunion_Contact_Form( array( 'to' => 'mellow@hello.com' ) );
 		$result = $form->process_submission();
+	}
+
+	public function pre_test_process_submission_wont_send_spam_if_marked_as_spam_with_true(){
+		$this->assertTrue( false ); // Fail if trying to send
 	}
 
 	/**
@@ -376,20 +384,18 @@ class WP_Test_Grunion_Contact_Form extends WP_UnitTestCase {
 	 * @covers Grunion_Contact_Form::process_submission
 	 */
 	public function test_process_submission_labels_message_as_spam_in_subject_if_marked_as_spam_with_true_and_sending_spam() {
-		add_filter( 'jetpack_contact_form_is_spam', function() {
-			return true;
-		}, 11 ); // Run after akismet filter
+		add_filter( 'jetpack_contact_form_is_spam', '__return_true' , 11 ); // Run after akismet filter
 
-		add_filter( 'grunion_still_email_spam', function() {
-			return true;
-		} );
+		add_filter( 'grunion_still_email_spam', '__return_true' );
 
-		add_filter( 'wp_mail', function( $args ) {
-			$this->assertContains( '***SPAM***', $args['subject'] );
-		} );
+		add_filter( 'wp_mail', array( $this, 'pre_test_process_submission_labels_message_as_spam_in_subject_if_marked_as_spam_with_true_and_sending_spam') );
 
 		$form = new Grunion_Contact_Form( array( 'to' => 'mellow@hello.com' ) );
 		$result = $form->process_submission();
+	}
+
+	public function pre_test_process_submission_labels_message_as_spam_in_subject_if_marked_as_spam_with_true_and_sending_spam( $args ){
+		$this->assertContains( '***SPAM***', $args['subject'] );
 	}
 
 
