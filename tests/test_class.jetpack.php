c34b1252736bf50dddbb7cf319a983177c6dd397
Brandon Kraft <public@brandonkraft.com>
2015-09-29 23:06:26 -0500

    Update force_ssl_login to force_ssl_admin.
    WordPress 4.4 will deprecate `force_ssl_login` in favor of more usage of `force_ssl_admin`. Updating the one usage in JP proper and one unit test.

--- tests/test_class.jetpack.php
+++ tests/test_class.jetpack.php
@@ -219,7 +219,7 @@ EXPECTED;
 	 */
 	public function test_check_identity_crisis_will_not_report_crisis_if_a_siteurl_mismatch_when_forcing_ssl() {
 		// Kick in with force ssl and store master user data
-		force_ssl_login( true );
+		force_ssl_admin( true );
 		Jetpack_Options::update_option( 'master_user', 'test' );
 		Jetpack_Options::update_option( 'user_tokens', array( 'test' => 'herp.derp.test' ) );
 		add_filter( 'jetpack_development_mode', '__return_false', 1, 1 );
