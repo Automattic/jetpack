5730ae12fab3a2b68788d8938b1a1a7cde1085eb
Jesse Friedman <jesserfriedman@gmail.com>
2015-10-01 06:49:51 -0400

    Improved white spacing and JITM message

--- _inc/jetpack-jitm.js
+++ _inc/jetpack-jitm.js
@@ -30,46 +30,46 @@
 		var module_slug, success_msg, fail_msg, hide_msg;
 
 		// On dismiss of JITM admin notice
-		$('.jp-jitm .dismiss').click(function() {
+		$( '.jp-jitm .dismiss' ).click( function() {
 			// hide the notice
-			$('.jp-jitm').hide();
+			$( '.jp-jitm' ).hide();
 
 			// ajax request to save dismiss and never show again
 			data.jitmActionToTake = 'dismiss';
-			module_slug = $(this).data('module');
+			module_slug = $(this).data( 'module' );
 			data.jitmModule = module_slug;
 
-			$.post( jitmL10n.ajaxurl, data, function (response) {
+			$.post( jitmL10n.ajaxurl, data, function ( response ) {
 				if ( true === response.success ) {
 					//console.log('successfully dismissed for ever')
 				}
 			});
 		});
 
-		$('.jp-jitm .activate').click(function() {
+		$( '.jp-jitm .activate' ).click(function() {
 
 			data.jitmActionToTake = 'activate';
 
 			// get the module we're working with using the data-module attribute
-			module_slug = $(this).data('module');
+			module_slug = $( this ).data( 'module' );
 			success_msg = data[module_slug].success;
 			fail_msg = data[module_slug].fail;
 
 			data.jitmModule = module_slug;
 
 			// since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
-			$.post( jitmL10n.ajaxurl, data, function (response) {
+			$.post( jitmL10n.ajaxurl, data, function ( response ) {
 				// If there's no response, something bad happened
 				if ( true === response.success ) {
-					$('.msg').html(success_msg);
-					$('.button').toggleClass('hide');
+					$( '.msg' ).html( success_msg );
+					$( '.button' ).toggleClass( 'hide' );
 					if ( 'manage' !== data.jitmModule ){
-						hide_msg = setTimeout(function () {
-							$('.jp-jitm').hide('slow');
-						}, 5000);
+						hide_msg = setTimeout( function () {
+							$( '.jp-jitm' ).hide( 'slow' );
+						}, 5000 );
 					}
 				} else {
-					$('.jp-jitm').html('<p><span class="icon"></span>'+fail_msg+'</p>');
+					$( '.jp-jitm' ).html( '<p><span class="icon"></span>'+fail_msg+'</p>' );
 				}
 			});
 
