076ef0f8396ac224ad1eed520581328089f68c88
Daisuke Takahashi <daisuke@extendwings.com>
2015-10-08 14:28:05 +0900

    Get rid of extract()

--- modules/widgets/googleplus-badge.php
+++ modules/widgets/googleplus-badge.php
@@ -47,16 +47,13 @@ class WPCOM_Widget_GooglePlus_Badge extends WP_Widget {
 	}
 
 	function widget( $args, $instance ) {
+		$instance = $this->normalize_googleplus_args( $instance );
 
-		extract( $args );
-
-		$like_args = $this->normalize_googleplus_args( $instance['like_args'] );
-
-		if ( empty( $like_args['href'] ) || ! $this->is_valid_googleplus_url( $like_args['href'] ) ) {
+		if ( empty( $instance['href'] ) || ! $this->is_valid_googleplus_url( $instance['href'] ) ) {
 			if ( current_user_can('edit_theme_options') ) {
-				echo $before_widget;
+				echo $args['before_widget'];
 				echo '<p>' . sprintf( __( 'It looks like your Google+ URL is incorrectly configured. Please check it in your <a href="%s">widget settings</a>.', 'jetpack' ), admin_url( 'widgets.php' ) ) . '</p>';
-				echo $after_widget;
+				echo $args['after_widget'];
 			}
 			echo '<!-- Invalid Google+ URL -->';
 			return;
@@ -66,16 +63,16 @@ class WPCOM_Widget_GooglePlus_Badge extends WP_Widget {
 		/** This filter is documented in core/src/wp-includes/default-widgets.php */
 		$title = apply_filters( 'widget_title', $instance['title'] );
 
-		$like_args['show_photo']   = (bool) $like_args['show_photo']   ? 'true' : 'false';
-		$like_args['show_owners']  = (bool) $like_args['show_owners']  ? 'true' : 'false';
-		$like_args['show_tagline'] = (bool) $like_args['show_tagline'] ? 'true' : 'false';
+		$instance['show_photo']   = (bool) $instance['show_photo']   ? 'true' : 'false';
+		$instance['show_owners']  = (bool) $instance['show_owners']  ? 'true' : 'false';
+		$instance['show_tagline'] = (bool) $instance['show_tagline'] ? 'true' : 'false';
 
-		echo $before_widget;
+		echo $args['before_widget'];
 
 		if ( ! empty( $title ) ):
-			echo $before_title;
+			echo $args['before_title'];
 
-			$badge_widget_title = '<a href="' . esc_url( $like_args['href'] ) . '">' . esc_html( $title ) . '</a>';
+			$badge_widget_title = '<a href="' . esc_url( $instance['href'] ) . '">' . esc_html( $title ) . '</a>';
 
 			/**
 			 * Modify the title of Google+ Badge widget.
@@ -84,29 +81,25 @@ class WPCOM_Widget_GooglePlus_Badge extends WP_Widget {
 			 * @param string $text_title The title of badge widget
 			 * @param string $href       The URL of Google+
 			 */
-			echo apply_filters( 'jetpack_googleplus_community_badge_title', $badge_widget_title, $title, $like_args['href'] );
+			echo apply_filters( 'jetpack_googleplus_badge_title', $badge_widget_title, $title, $instance['href'] );
 
-			echo $after_title;
+			echo $args['after_title'];
 		endif;
 
 		?><script src="https://apis.google.com/js/platform.js" async defer></script>
-		<g:community href="<?php echo esc_url( $like_args['href'] ); ?>" layout="<?php echo esc_attr( $like_args['layout'] ); ?>" theme="<?php echo esc_attr( $like_args['theme'] ); ?>" showphoto="<?php echo esc_attr( $like_args['show_photo'] ); ?>" showowners="<?php echo esc_attr( $like_args['show_owners'] ); ?>" showtagline="<?php echo esc_attr( $like_args['show_tagline'] ); ?>" width="<?php echo esc_attr( $like_args['width'] ); ?>"></g:community><?php
+		<g:community href="<?php echo esc_url( $instance['href'] ); ?>" layout="<?php echo esc_attr( $instance['layout'] ); ?>" theme="<?php echo esc_attr( $instance['theme'] ); ?>" showphoto="<?php echo esc_attr( $instance['show_photo'] ); ?>" showowners="<?php echo esc_attr( $instance['show_owners'] ); ?>" showtagline="<?php echo esc_attr( $instance['show_tagline'] ); ?>" width="<?php echo esc_attr( $instance['width'] ); ?>"></g:community><?php
 
-		echo $after_widget;
+		echo $args['after_widget'];
 
 		do_action( 'jetpack_stats_extra', 'widget', 'googleplus-badge' );
 	}
 
 	function update( $new_instance, $old_instance ) {
-		$instance = array(
-			'title' => '',
-			'like_args' => $this->get_default_args(),
-		);
-
-		$instance['title'] = trim( strip_tags( stripslashes( $new_instance['title'] ) ) );
+		$instance = $this->get_default_args();
 
 		// Set up widget values
-		$instance['like_args'] = array(
+		$instance = array(
+			'title'        => trim( strip_tags( stripslashes( $new_instance['title'] ) ) ),
 			'href'         => trim( strip_tags( stripslashes( $new_instance['href'] ) ) ),
 			'width'        => (int) $new_instance['width'],
 			'layout'       => $new_instance['layout'],
@@ -116,17 +109,13 @@ class WPCOM_Widget_GooglePlus_Badge extends WP_Widget {
 			'show_tagline' => (bool) $new_instance['show_tagline'],
 		);
 
-		$instance['like_args'] = $this->normalize_googleplus_args( $instance['like_args'] );
+		$instance = $this->normalize_googleplus_args( $instance );
 
 		return $instance;
 	}
 
 	function form( $instance ) {
-		$instance = wp_parse_args( (array) $instance, array(
-			'title'     => '',
-			'like_args' => $this->get_default_args()
-		) );
-		$like_args = $this->normalize_googleplus_args( $instance['like_args'] );
+		$instance = $this->normalize_googleplus_args( (array) $instance );
 		?>
 
 		<p>
@@ -139,14 +128,14 @@ class WPCOM_Widget_GooglePlus_Badge extends WP_Widget {
 		<p>
 			<label for="<?php echo $this->get_field_id( 'href' ); ?>">
 				<?php _e( 'Google+ URL', 'jetpack' ); ?>
-				<input type="text" name="<?php echo $this->get_field_name( 'href' ); ?>" id="<?php echo $this->get_field_id( 'href' ); ?>" value="<?php echo esc_url( $like_args['href'] ); ?>" class="widefat" />
+				<input type="text" name="<?php echo $this->get_field_name( 'href' ); ?>" id="<?php echo $this->get_field_id( 'href' ); ?>" value="<?php echo esc_url( $instance['href'] ); ?>" class="widefat" />
 			</label>
 		</p>
 
 		<p>
 			<label for="<?php echo $this->get_field_id( 'width' ); ?>">
 				<?php _e( 'Width', 'jetpack' ); ?>
-				<input type="number" class="smalltext" min="<?php echo esc_attr( $this->min_width ); ?>" max="<?php echo esc_attr( $this->max_width ); ?>" maxlength="3" name="<?php echo $this->get_field_name( 'width' ); ?>" id="<?php echo $this->get_field_id( 'width' ); ?>" value="<?php echo esc_attr( $like_args['width'] ); ?>" style="text-align: center;" />px
+				<input type="number" class="smalltext" min="<?php echo esc_attr( $this->min_width ); ?>" max="<?php echo esc_attr( $this->max_width ); ?>" maxlength="3" name="<?php echo $this->get_field_name( 'width' ); ?>" id="<?php echo $this->get_field_id( 'width' ); ?>" value="<?php echo esc_attr( $instance['width'] ); ?>" style="text-align: center;" />px
 			</label>
 		</p>
 
@@ -154,8 +143,8 @@ class WPCOM_Widget_GooglePlus_Badge extends WP_Widget {
 			<label for="<?php echo $this->get_field_id( 'layout' ); ?>">
 				<?php _e( 'Layout', 'jetpack' ); ?>
 				<select name="<?php echo $this->get_field_name( 'layout' ); ?>" id="<?php echo $this->get_field_id( 'layout' ); ?>">
-					<option value="landscape" <?php selected( $like_args['layout'], 'landscape' ); ?>><?php _e( 'Landscape', 'jetpack' ); ?></option>
-					<option value="portrait" <?php selected( $like_args['layout'], 'portrait' ); ?>><?php _e( 'Portrait', 'jetpack' ); ?></option>
+					<option value="landscape" <?php selected( $instance['layout'], 'landscape' ); ?>><?php _e( 'Landscape', 'jetpack' ); ?></option>
+					<option value="portrait" <?php selected( $instance['layout'], 'portrait' ); ?>><?php _e( 'Portrait', 'jetpack' ); ?></option>
 				</select>
 			</label>
 		</p>
@@ -164,29 +153,29 @@ class WPCOM_Widget_GooglePlus_Badge extends WP_Widget {
 			<label for="<?php echo $this->get_field_id( 'theme' ); ?>">
 				<?php _e( 'Theme', 'jetpack' ); ?>
 				<select name="<?php echo $this->get_field_name( 'theme' ); ?>" id="<?php echo $this->get_field_id( 'theme' ); ?>">
-					<option value="light" <?php selected( $like_args['theme'], 'light' ); ?>><?php _e( 'Light', 'jetpack' ); ?></option>
-					<option value="dark" <?php selected( $like_args['theme'], 'dark' ); ?>><?php _e( 'Dark', 'jetpack' ); ?></option>
+					<option value="light" <?php selected( $instance['theme'], 'light' ); ?>><?php _e( 'Light', 'jetpack' ); ?></option>
+					<option value="dark" <?php selected( $instance['theme'], 'dark' ); ?>><?php _e( 'Dark', 'jetpack' ); ?></option>
 				</select>
 			</label>
 		</p>
 
 		<p>
 			<label for="<?php echo $this->get_field_id( 'show_photo' ); ?>">
-				<input type="checkbox" name="<?php echo $this->get_field_name( 'show_photo' ); ?>" id="<?php echo $this->get_field_id( 'show_photo' ); ?>" <?php checked( $like_args['show_photo'] ); ?> />
+				<input type="checkbox" name="<?php echo $this->get_field_name( 'show_photo' ); ?>" id="<?php echo $this->get_field_id( 'show_photo' ); ?>" <?php checked( $instance['show_photo'] ); ?> />
 				<?php _e( 'Show Cover Photo', 'jetpack' ); ?>
 			</label>
 		</p>
 
 		<p>
 			<label for="<?php echo $this->get_field_id( 'show_owners' ); ?>">
-				<input type="checkbox" name="<?php echo $this->get_field_name( 'show_owners' ); ?>" id="<?php echo $this->get_field_id( 'show_owners' ); ?>" <?php checked( $like_args['show_owners'] ); ?> />
+				<input type="checkbox" name="<?php echo $this->get_field_name( 'show_owners' ); ?>" id="<?php echo $this->get_field_id( 'show_owners' ); ?>" <?php checked( $instance['show_owners'] ); ?> />
 				<?php _e( 'Show Owners', 'jetpack' ); ?>
 			</label>
 		</p>
 
 		<p>
 			<label for="<?php echo $this->get_field_id( 'show_tagline' ); ?>">
-				<input type="checkbox" name="<?php echo $this->get_field_name( 'show_tagline' ); ?>" id="<?php echo $this->get_field_id( 'show_tagline' ); ?>" <?php checked( $like_args['show_tagline'] ); ?> />
+				<input type="checkbox" name="<?php echo $this->get_field_name( 'show_tagline' ); ?>" id="<?php echo $this->get_field_id( 'show_tagline' ); ?>" <?php checked( $instance['show_tagline'] ); ?> />
 				<?php _e( 'Show Tag Line', 'jetpack' ); ?>
 			</label>
 		</p>
@@ -196,6 +185,7 @@ class WPCOM_Widget_GooglePlus_Badge extends WP_Widget {
 
 	function get_default_args() {
 		$defaults = array(
+			'title'        => '',
 			'href'         => '',
 			'width'        => $this->default_width,
 			'layout'       => $this->default_layout,
@@ -211,13 +201,14 @@ class WPCOM_Widget_GooglePlus_Badge extends WP_Widget {
 		 * @param array $args {
 		 *     Default arguments.
 		 *
+		 *     @var string $title        The title
 		 *     @var string $href         The URL of Google+
 		 *     @var int    $width        The pixel width of the badge to render.
 		 *     @var string $layout       Sets the orientation of the badge.
 		 *     @var string $theme        The color theme of the badge.
-		 *     @var bool   $show_photo   Whether display the community profile photo.
-		 *     @var bool   $show_owners  Whether display a list of community owners.
-		 *     @var bool   $show_tagline Whether display the community's tag line.
+		 *     @var bool   $show_photo   Whether display the profile photo.
+		 *     @var bool   $show_owners  Whether display a list of owners.
+		 *     @var bool   $show_tagline Whether display the tag line.
 		 * }
 		 */
 		return apply_filters( 'jetpack_googleplus_badge_defaults', $defaults );
