d2e3600064e5813edb1cc6d18383f3cc6086d52c
Jeremy Herve <jeremy@tagada.hu>
2015-10-22 16:17:30 +0200

    Shortcodes: add inline docs to new twitchtv shortcode.
    @see https://github.com/Automattic/jetpack/pull/2843

--- modules/shortcodes/twitchtv.php
+++ modules/shortcodes/twitchtv.php
@@ -29,41 +29,42 @@ Video:
 </object>
  */
 function wpcom_twitchtv_shortcode( $attr, $content = NULL ) {
-        $attr = extract( shortcode_atts( array(
-            'height'     => 378,
-            'width'      => 620,
-            'url'        => '',
-            'autoplay'   => false
-        ), $attr ) );
+	$attr = extract( shortcode_atts( array(
+		'height'	 => 378,
+		'width'	  => 620,
+		'url'		=> '',
+		'autoplay'   => false
+	), $attr ) );
 
-        if ( empty( $url ) )
-                return;
+	if ( empty( $url ) )
+		return;
 
-        preg_match( '|^http://www.twitch.tv/([^/?]+)(/v/(\d+))?|i', $url, $match );
+	preg_match( '|^http://www.twitch.tv/([^/?]+)(/v/(\d+))?|i', $url, $match );
 
-        $width = (int) $width;
-        $height = (int) $height;
-        $autoplay = var_export( filter_var( $autoplay, FILTER_VALIDATE_BOOLEAN ), true );
+	$width = (int) $width;
+	$height = (int) $height;
+	$autoplay = var_export( filter_var( $autoplay, FILTER_VALIDATE_BOOLEAN ), true );
 
-        $user_id = esc_attr( $match[1] );
-        $video_id = 0;
-        if ( !empty( $match[3] ) )
-                $video_id = (int) $match[3];
+	$user_id = esc_attr( $match[1] );
+	$video_id = 0;
+	if ( !empty( $match[3] ) )
+		$video_id = (int) $match[3];
 
-        do_action( 'jetpack_bump_stats_extras', 'twitchtv', 'shortcode' );
+	/** This action is documented in modules/widgets/social-media-icons.php */
+	do_action( 'jetpack_bump_stats_extras', 'twitchtv', 'shortcode' );
 
-        if ( $video_id > 0 ) {
-                // Archive video
-                return "<object bgcolor='#000000' data='//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf' height='$height' width='$width' id='clip_embed_player_flash' type='application/x-shockwave-flash'>
+	if ( $video_id > 0 ) {
+		// Archive video
+		return "<object bgcolor='#000000' data='//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf' height='$height' width='$width' id='clip_embed_player_flash' type='application/x-shockwave-flash'>
 <param name='movie' value='//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf'>
 <param name='allowScriptAccess' value='always'>
 <param name='allowNetworking' value='all'>
 <param name='allowFullScreen' value='true'>
 <param name='flashvars' value='videoId=v$video_id&hostname=www.twitch.tv&channel=$user_id&auto_play=$autoplay'>
 </object>";
-        }
+	}
 
-        $html = "<object type='application/x-shockwave-flash' height='$height' width='$width' id='live_embed_player_flash' data='//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf?channel=$user_id' bgcolor='#000000'>
+	$html = "<object type='application/x-shockwave-flash' height='$height' width='$width' id='live_embed_player_flash' data='//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf?channel=$user_id' bgcolor='#000000'>
 <param name='allowFullScreen' value='true' />
 <param name='allowScriptAccess' value='always' />
 <param name='allowNetworking' value='all' />
@@ -71,7 +72,7 @@ function wpcom_twitchtv_shortcode( $attr, $content = NULL ) {
 <param name='flashvars' value='hostname=www.twitch.tv&channel=$user_id&auto_play=$autoplay&start_volume=25' />
 </object>";
 
-        return $html;
+	return $html;
 }
 
 add_shortcode( 'twitch', 'wpcom_twitchtv_shortcode' );
