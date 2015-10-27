7dfc20fe73ff4954fe01382d95ddca89b7cb13cc
Brandon Kraft <public@brandonkraft.com>
2015-10-27 10:17:46 -0500

    CPTs: Correct spacing

--- modules/custom-post-types/nova.php
+++ modules/custom-post-types/nova.php
@@ -159,7 +159,7 @@ class Nova_Restaurant {
 				'update_item'           => __( 'Update Menu Section',           'jetpack' ),
 				'add_new_item'          => __( 'Add New Menu Section',          'jetpack' ),
 				'new_item_name'         => __( 'New Menu Sections Name',        'jetpack' ),
-				'items_list_navigation' => __( 'Menu section list navigation', 'jetpack' ),
+				'items_list_navigation' => __( 'Menu section list navigation',  'jetpack' ),
 				'items_list'            => __( 'Menu section list',             'jetpack' ),
 			),
 			'rewrite' => array(
@@ -192,7 +192,7 @@ class Nova_Restaurant {
 				'not_found'             => __( 'No Menu Items found',          'jetpack' ),
 				'not_found_in_trash'    => __( 'No Menu Items found in Trash', 'jetpack' ),
 				'filter_items_list'     => __( 'Filter menu items list',       'jetpack' ),
-				'items_list_navigation' => __( 'Menu item list navigation',   'jetpack' ),
+				'items_list_navigation' => __( 'Menu item list navigation',    'jetpack' ),
 				'items_list'            => __( 'Menu items list',              'jetpack' ),
 			),
 			'supports' => array(
--- modules/custom-post-types/testimonial.php
+++ modules/custom-post-types/testimonial.php
@@ -301,7 +301,7 @@ class Jetpack_Testimonial {
 				'not_found'             => esc_html__( 'No Testimonials found',          'jetpack' ),
 				'not_found_in_trash'    => esc_html__( 'No Testimonials found in Trash', 'jetpack' ),
 				'filter_items_list'     => esc_html__( 'Filter testiominals list',       'jetpack' ),
-				'items_list_navigation' => esc_html__( 'Testiominal list navigation',   'jetpack' ),
+				'items_list_navigation' => esc_html__( 'Testiominal list navigation',    'jetpack' ),
 				'items_list'            => esc_html__( 'Testiominals list',              'jetpack' ),
 			),
 			'supports' => array(
