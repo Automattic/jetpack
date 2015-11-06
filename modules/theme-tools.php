9f53ebe9c3769bb341b5e710838707fe79f9dee6
Takashi Irie <takashi@automattic.com>
2015-11-06 01:59:58 +0000

    Theme Compat Files for Twenty Sixteen

--- modules/theme-tools.php
+++ modules/theme-tools.php
@@ -37,6 +37,7 @@ function jetpack_load_theme_compat() {
 	$compat_files = apply_filters( 'jetpack_theme_compat_files', array(
 		'twentyfourteen' => JETPACK__PLUGIN_DIR . 'modules/theme-tools/compat/twentyfourteen.php',
 		'twentyfifteen'  => JETPACK__PLUGIN_DIR . 'modules/theme-tools/compat/twentyfifteen.php',
+		'twentysixteen'  => JETPACK__PLUGIN_DIR . 'modules/theme-tools/compat/twentysixteen.php',
 	) );
 
 	_jetpack_require_compat_file( get_stylesheet(), $compat_files );
