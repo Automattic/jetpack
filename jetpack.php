36bf4a1e2244cc956e5d36f0c58334f6f68d4399
dereksmart <smart@automattic.com>
2015-11-09 16:54:23 -0500

    Fix alpha version numbers 3.8.1-alpha

--- jetpack.php
+++ jetpack.php
@@ -14,7 +14,7 @@
 
 define( 'JETPACK__MINIMUM_WP_VERSION', '4.2' );
 
-define( 'JETPACK__VERSION',            '3.8.0-alpha' );
+define( 'JETPACK__VERSION',            '3.8.1-alpha' );
 define( 'JETPACK_MASTER_USER',         true );
 define( 'JETPACK__API_VERSION',        1 );
 define( 'JETPACK__PLUGIN_DIR',         plugin_dir_path( __FILE__ ) );
