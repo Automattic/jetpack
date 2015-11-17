ae5dc999d95b120905498d091e578a1bf925ff9c
Miguel Lezama <miguel@automattic.com>
2015-11-17 11:44:25 -0300

    fix `get_update_results` call

--- json-endpoints/jetpack/class.jetpack-json-api-maybe-auto-update-endpoint.php
+++ json-endpoints/jetpack/class.jetpack-json-api-maybe-auto-update-endpoint.php
@@ -7,7 +7,7 @@ class Jetpack_JSON_API_Maybe_Auto_Update_Endpoint extends Jetpack_JSON_API_Endpo
 	protected $update_results = array();
 
 	protected function result() {
-		add_action( 'automatic_updates_complete', array( $this, 'get_update_results', 100, 1 ) );
+		add_action( 'automatic_updates_complete', array( $this, 'get_update_results' ), 100, 1 );
 
 		include_once( ABSPATH . '/wp-admin/includes/admin.php' );
 		include_once( ABSPATH . '/wp-admin/includes/class-wp-upgrader.php' );
@@ -19,7 +19,7 @@ class Jetpack_JSON_API_Maybe_Auto_Update_Endpoint extends Jetpack_JSON_API_Endpo
 		return $result;
 	}
 
-	protected function get_update_results( $results ) {
+	public function get_update_results( $results ) {
 		$this->update_results = $results;
 	}
 
