d44cc73e1fa5247633fbcf66ab927144c9e03749
Miguel Lezama <miguel@automattic.com>
2015-12-14 16:59:04 -0300

    REST API: Options endpoint: Throw WP_Errors when invalid input

--- json-endpoints/jetpack/class.wpcom-json-api-get-option-endpoint.php
+++ json-endpoints/jetpack/class.wpcom-json-api-get-option-endpoint.php
@@ -18,17 +18,20 @@ class WPCOM_JSON_API_Get_Option_Endpoint extends Jetpack_JSON_API_Endpoint {
 		return array( 'option_value' => get_option( $this->option_name ) );
 	}
 
-
 	function validate_input( $object ) {
 		$query_args = $this->query_args();
 		$this->option_name = isset( $query_args['option_name'] ) ? $query_args['option_name'] : false;
-		$this->site_option = isset( $query_args['site_option'] ) ? $query_args['site_option'] : false;
 		if ( ! $this->option_name ) {
 			return new WP_Error( 'option_name_not_set', __( 'You must specify an option_name', 'jetpack' ) );
 		}
+		$this->site_option = isset( $query_args['site_option'] ) ? $query_args['site_option'] : false;
 		if ( $this->site_option ) {
-			return in_array( $this->option_name, apply_filters( 'jetpack_site_option_whitelist', $this->site_option_whitelist ) );
+			if ( ! in_array( $this->option_name, apply_filters( 'jetpack_site_option_whitelist', $this->site_option_whitelist ) ) ) {
+				return new WP_Error( 'option_name_not_in_whitelist', __( 'You must specify a whitelisted option_name', 'jetpack' ) );
+			}
+		} elseif ( ! in_array( $this->option_name, apply_filters( 'jetpack_option_whitelist', $this->option_whitelist ) ) ) {
+			return new WP_Error( 'option_name_not_in_whitelist', __( 'You must specify a whitelisted option_name', 'jetpack' ) );
 		}
-		return in_array( $this->option_name, apply_filters( 'jetpack_option_whitelist', $this->option_whitelist ) );
+		return true;
 	}
 }
