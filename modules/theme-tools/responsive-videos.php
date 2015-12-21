1a6ff87db555b8c52054d692ca7e8cd430255e46
Lance Willett <lance@automattic.com>
2015-12-21 23:27:27 +0000

    # This is a combination of 5 commits. # The first commit's message is: Random Post: if a static page is set as home, show a random post instead of a random page when accessing `?random` from the home page.
    Fixes #6949.
    
    Merges r128609-wpcom.
    
    # This is the 2nd commit message:
    
    VideoPress: For a12s and sites with videopress-next sticker, skip the extra jetpack responsive video script which causes issues with the new scripts.
    
    Merges r120135-wpcom.
    
    # This is the 3rd commit message:
    
    VideoPress: use videopress_show_2015_player()
    
    Merges r120400-wpcom.
    
    # This is the 4th commit message:
    
    Videos: Do not disable the jetpack responsive videos filters everywhere that uses the new player. Just do not apply the vidoe_embed_html filter on the new player, which causes all kinds of issues since the player is already responsive.
    
    Merges r120609-wpcom.
    
    # This is the 5th commit message:
    
    Responsive Videos: Avoid applying jetpack-video-wrapper to non-video content by checking get_media_embedded_in_content() before applying the oEmbed filter. This was causing Twitter embeds to be treated as videos.
    
    Merges r121634-wpcom.

--- modules/theme-tools/responsive-videos.php
+++ modules/theme-tools/responsive-videos.php
@@ -12,6 +12,11 @@ function jetpack_responsive_videos_init() {
 	/* If the theme does support 'jetpack-responsive-videos', wrap the videos */
 	add_filter( 'wp_video_shortcode', 'jetpack_responsive_videos_embed_html' );
 	add_filter( 'video_embed_html',   'jetpack_responsive_videos_embed_html' );
+	
+	/* Check to make sure the content is a video before applying to oEmbeds */
+	if ( ! empty( get_media_embedded_in_content( 'video' ) ) ) {
+		add_filter( 'embed_oembed_html',  'jetpack_responsive_videos_embed_html' );
+	}
 
 	/* Only wrap oEmbeds if video */
 	add_filter( 'embed_oembed_html',  'jetpack_responsive_videos_maybe_wrap_oembed', 10, 2 );
