1868073526b156d254044ce7174fcba263ec3e3c
Sam Hotchkiss <s@swh.me>
2015-12-28 13:01:29 -0700

    Update version number

--- tests/php/test_class.jetpack-heartbeat.php
+++ tests/php/test_class.jetpack-heartbeat.php
@@ -4,7 +4,7 @@ class WP_Test_Jetpack_Heartbeat extends WP_UnitTestCase {
 
 	/**
 	 * @covers Jetpack_Heartbeat::init
-	 * @since 3.8.0
+	 * @since 3.9.0
 	 */
 	public function test_init() {
 		$this->assertInstanceOf( 'Jetpack_Heartbeat', Jetpack_Heartbeat::init() );
@@ -16,7 +16,7 @@ class WP_Test_Jetpack_Heartbeat extends WP_UnitTestCase {
 
 	/**
 	 * @covers Jetpack_Heartbeat::cron_exec
-	 * @since 3.8.0
+	 * @since 3.9.0
 	 */
 	public function test_cron_exec() {
 		$this->heartbeat_action = false;
@@ -29,7 +29,7 @@ class WP_Test_Jetpack_Heartbeat extends WP_UnitTestCase {
 
 	/**
 	 * @covers Jetpack_Heartbeat::generate_stats_array
-	 * @since 3.8.0
+	 * @since 3.9.0
 	 */
 	public function test_generate_stats_array() {
 		$prefix = 'test';
@@ -42,7 +42,7 @@ class WP_Test_Jetpack_Heartbeat extends WP_UnitTestCase {
 
 	/**
 	 * @covers Jetpack_Heartbeat::jetpack_xmlrpc_methods
-	 * @since 3.8.0
+	 * @since 3.9.0
 	 */
 	public function test_jetpack_xmlrpc_methods() {
 		$this->assertNotEmpty( Jetpack_Heartbeat::jetpack_xmlrpc_methods( array() ) );
