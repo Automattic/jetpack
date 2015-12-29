f3a8dbcb8398ae653131f6aa3001c916dcd4b529
Lance Willett <lance@automattic.com>
2015-12-29 16:23:01 +0000

    Shortcodes: code style updates for Bandcamp shortcode.
    Merges r128816-wpcom.

--- modules/shortcodes/bandcamp.php
+++ modules/shortcodes/bandcamp.php
@@ -10,43 +10,43 @@ function shortcode_handler_bandcamp( $atts ) {
 	// there are no default values, but specify here anyway
 	// to explicitly list supported atts
 	$attributes = shortcode_atts( array(
-		'album'			=> null,		// integer album id
-		'track'			=> null,		// integer track id
-		'video'			=> null,		// integer track id for video player
-		'size'			=> 'venti',		// one of the supported sizes
-		'bgcol'			=> 'FFFFFF',	// hex, no '#' prefix
-		'linkcol'		=> null,		// hex, no '#' prefix
-		'layout'		=> null,		// encoded layout url
-		'width'			=> null,		// integer with optional "%"
-		'height'		=> null,		// integer with optional "%"
-		'notracklist'	=> null,		// may be string "true" (defaults false)
-		'tracklist'		=> null,		// may be string "false" (defaults true)
-		'artwork'		=> null,		// may be string "false" (alternately: "none") or "small" (default is large)
-		'minimal'		=> null,		// may be string "true" (defaults false)
-		'theme'			=> null,		// may be theme identifier string ("light"|"dark" so far)
-		'package'		=> null,		// integer package id
-		't'				=> null,		// integer track number
-		'tracks'		=> null,		// comma separated list of allowed tracks
-		'esig'			=> null			// hex, no '#' prefix
+		'album'       => null,        // integer album id
+		'track'       => null,        // integer track id
+		'video'       => null,        // integer track id for video player
+		'size'        => 'venti',        // one of the supported sizes
+		'bgcol'       => 'FFFFFF',    // hex, no '#' prefix
+		'linkcol'     => null,        // hex, no '#' prefix
+		'layout'      => null,        // encoded layout url
+		'width'       => null,        // integer with optional "%"
+		'height'      => null,        // integer with optional "%"
+		'notracklist' => null,        // may be string "true" (defaults false)
+		'tracklist'   => null,        // may be string "false" (defaults true)
+		'artwork'     => null,        // may be string "false" (alternately: "none") or "small" (default is large)
+		'minimal'     => null,        // may be string "true" (defaults false)
+		'theme'       => null,        // may be theme identifier string ("light"|"dark" so far)
+		'package'     => null,        // integer package id
+		't'           => null,        // integer track number
+		'tracks'      => null,        // comma separated list of allowed tracks
+		'esig'        => null            // hex, no '#' prefix
 	), $atts, 'bandcamp' );
 
 	$sizes = array(
-		'venti'			=> array( 'width' => 400, 'height' => 100 ),
-		'grande'		=> array( 'width' => 300, 'height' => 100 ),
-		'grande2'		=> array( 'width' => 300, 'height' => 355 ),
-		'grande3'		=> array( 'width' => 300, 'height' => 415 ),
-		'tall_album'	=> array( 'width' => 150, 'height' => 295 ),
-		'tall_track'	=> array( 'width' => 150, 'height' => 270 ),
-		'tall2'			=> array( 'width' => 150, 'height' => 450 ),
-		'short'			=> array( 'width' => 46, 'height' => 23 ),
-		'large'			=> array( 'width' => 350, 'height' => 470 ),
-		'medium'		=> array( 'width' => 450, 'height' => 120 ),
-		'small'			=> array( 'width' => 350, 'height' => 42 )
+		'venti'      => array( 'width' => 400, 'height' => 100 ),
+		'grande'     => array( 'width' => 300, 'height' => 100 ),
+		'grande2'    => array( 'width' => 300, 'height' => 355 ),
+		'grande3'    => array( 'width' => 300, 'height' => 415 ),
+		'tall_album' => array( 'width' => 150, 'height' => 295 ),
+		'tall_track' => array( 'width' => 150, 'height' => 270 ),
+		'tall2'      => array( 'width' => 150, 'height' => 450 ),
+		'short'      => array( 'width' => 46, 'height' => 23 ),
+		'large'      => array( 'width' => 350, 'height' => 470 ),
+		'medium'     => array( 'width' => 450, 'height' => 120 ),
+		'small'      => array( 'width' => 350, 'height' => 42 )
 	);
 
 	$sizekey = $attributes['size'];
-	$height = null;
-	$width = null;
+	$height  = null;
+	$width   = null;
 
 	$isVideo = false;
 
@@ -58,7 +58,7 @@ function shortcode_handler_bandcamp( $atts ) {
 	// a video player or an audio player
 	$argparts = array();
 
-	if ( !isset( $attributes['album'] ) && !isset( $attributes['track'] ) && !isset( $attributes['video'] ) ) {
+	if ( ! isset( $attributes['album'] ) && ! isset( $attributes['track'] ) && ! isset( $attributes['video'] ) ) {
 		return "[bandcamp: shortcode must include 'track', 'album', or 'video' param]";
 	}
 
@@ -66,7 +66,7 @@ function shortcode_handler_bandcamp( $atts ) {
 		$track = (int) $attributes['track'];
 		array_push( $argparts, "track={$track}" );
 	} elseif ( isset( $attributes['video'] ) ) {
-		$track = (int) $attributes['video']; // videos are referenced by track id
+		$track   = (int) $attributes['video']; // videos are referenced by track id
 		$urlbase = "//bandcamp.com/EmbeddedPlayer/v=2";
 		$isVideo = true;
 		array_push( $argparts, "track={$track}" );
@@ -85,8 +85,8 @@ function shortcode_handler_bandcamp( $atts ) {
 	}
 
 	// if size specified that we don't recognize, fall back on venti
-	if ( empty( $sizes[$sizekey] ) ) {
-		$sizekey = 'venti';
+	if ( empty( $sizes[ $sizekey ] ) ) {
+		$sizekey            = 'venti';
 		$attributes['size'] = 'venti';
 	}
 
@@ -106,13 +106,13 @@ function shortcode_handler_bandcamp( $atts ) {
 		}
 	}
 
-	if ( !$height ) {
-		$height = $sizes[$sizekey]['height'];
+	if ( ! $height ) {
+		$height    = $sizes[ $sizekey ]['height'];
 		$cssheight = $height . "px";
 	}
 
-	if ( !$width ) {
-		$width = $sizes[$sizekey]['width'];
+	if ( ! $width ) {
+		$width    = $sizes[ $sizekey ]['width'];
 		$csswidth = $width . "px";
 	}
 
@@ -176,10 +176,10 @@ function shortcode_handler_bandcamp( $atts ) {
 	}
 
 	if ( $isVideo ) {
-		$url = "//bandcamp.com/VideoEmbed?" . join( '&', $argparts );
+		$url        = "//bandcamp.com/VideoEmbed?" . join( '&', $argparts );
 		$extraAttrs = " mozallowfullscreen='1' webkitallowfullscreen='1' allowfullscreen='1'";
 	} else {
-		$url = "//bandcamp.com/EmbeddedPlayer/v=2/" . join( '/', $argparts ) . '/';
+		$url        = "//bandcamp.com/EmbeddedPlayer/v=2/" . join( '/', $argparts ) . '/';
 		$extraAttrs = '';
 	}
 
