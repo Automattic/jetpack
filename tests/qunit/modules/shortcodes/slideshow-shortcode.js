696d77381c5bb126feabf2ad2a027fa54d3e2387
Elio Rivero <eliorivero@gmail.com>
2015-12-29 09:52:23 -0300

    Rewrote QUnit test using the nested hooks approach

--- tests/qunit/modules/shortcodes/slideshow-shortcode.js
+++ tests/qunit/modules/shortcodes/slideshow-shortcode.js
@@ -8,33 +8,34 @@ var jetpackSlideshowSettings = {
 };
 
 jQuery( function( $ ) {
-	module( 'slideshow-shortcode', {
-		beforeEach: function() {
+	module( 'slideshow-shortcode', function( hooks ) {
+
+		var container, slideshow;
+
+		hooks.beforeEach ( function() {
 			container = $( '<div class="jetpack-slideshow" data-trans="fade" data-autostart="true">' );
 			$( 'body' ).append( container );
 			slideshow = new JetpackSlideshow( container, container.data( 'trans' ), container.data( 'autostart' ) );
-		}
+		} );
+
+		test( 'Jetpack slideshow is a jQuery object', function( assert ) {
+
+			assert.expect( 1 );
+
+			assert.ok( slideshow.element instanceof jQuery, 'is jQuery object' );
+		});
+
+		test( 'Loading image is added and removed', function( assert ) {
+
+			assert.expect( 2 );
+
+			slideshow.showLoadingImage( true );
+			assert.ok( slideshow.element.find( 'img' ).length > 0, 'image added' );
+
+			slideshow.showLoadingImage( false );
+			assert.ok( slideshow.loadingImage_ === null, 'image removed' );
+
+		} );
+
 	});
-
-	var container, slideshow;
-
-	test( 'Jetpack slideshow is a jQuery object', function( assert ) {
-
-		assert.expect( 1 );
-
-		assert.ok( slideshow.element instanceof jQuery, 'is jQuery object' );
-	});
-
-	test( 'Loading image is added and removed', function( assert ) {
-
-		assert.expect( 2 );
-
-		slideshow.showLoadingImage( true );
-		assert.ok( slideshow.element.find( 'img' ).length > 0, 'image added' );
-
-		slideshow.showLoadingImage( false );
-		assert.ok( slideshow.loadingImage_ === null, 'image removed' );
-
-	} );
-
 });
