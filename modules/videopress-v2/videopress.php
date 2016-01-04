b1522c061fe9432e38eea75a36f098d7ab210c83
George Stephanis <george@stephanis.info>
2016-01-04 12:49:08 -0500

    Add core JSON api endpoint to test stuff with.

--- modules/videopress-v2/videopress.php
+++ modules/videopress-v2/videopress.php
@@ -19,3 +19,19 @@ function videopress_get_video_details( $guid ) {
 
 	return apply_filters( 'videopress_get_video_details', $data );
 }
+
+/**
+ * Add some dummy REST API routes for testing.  Delete before merging to master.
+ */
+add_action( 'rest_api_init', 'videopress_core_json_api_init' );
+function videopress_core_json_api_init() {
+	if ( Jetpack::is_active() ) {
+		register_rest_route( 'jetpack/v4', '/video/(?P<guid>[a-zA-Z\d]+)', array(
+			'methods' => 'GET',
+			'callback' => 'videopress_core_json_api_get_video_details',
+		) );
+	}
+}
+function videopress_core_json_api_get_video_details( $data ) {
+	return videopress_get_video_details( $data['guid'] );
+}
