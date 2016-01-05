e3f3a3ebea5ab3f764c3a423837b4aa85cd1de1c
Lance Willett <lance@automattic.com>
2016-01-05 18:48:04 +0000

    Shortcodes: update code style for `slideshare` before other fixes.
    Merges r129099-wpcom.

--- modules/shortcodes/slideshare.php
+++ modules/shortcodes/slideshare.php
@@ -27,23 +27,26 @@ function slideshare_shortcode( $atts ) {
 	$params = shortcode_new_to_old_params( $atts );
 	parse_str( $params, $arguments );
 
-	if ( empty( $arguments ) )
+	if ( empty( $arguments ) ) {
 		return '<!-- SlideShare error: no arguments -->';
+	}
 
 	extract( $arguments );
 
 	// check that the Slideshare ID contains letters, numbers and query strings
 	$pattern = '/[^-_a-zA-Z0-9?=&]/';
-	if ( empty( $id ) || preg_match( $pattern, $id ) )
+	if ( empty( $id ) || preg_match( $pattern, $id ) ) {
 		return '<!-- SlideShare error: id is missing or has illegal characters -->';
+	}
 
 	// check the width/height
-	if ( empty( $w ) && ! empty( $content_width ) )
+	if ( empty( $w ) && ! empty( $content_width ) ) {
 		$w = intval( $content_width );
-	elseif ( ! ( $w = intval( $w ) ) || $w < 300 || $w > 1600 )
+	} elseif ( ! ( $w = intval( $w ) ) || $w < 300 || $w > 1600 ) {
 		$w = 425;
-	else
+	} else {
 		$w = intval( $w );
+	}
 
 	$h = ceil( $w * 348 / 425 );
 
@@ -53,34 +56,41 @@ function slideshare_shortcode( $atts ) {
 		$source = "https://www.slideshare.net/slideshow/embed_code/$id";
 	}
 
-	if ( isset( $rel ) )
+	if ( isset( $rel ) ) {
 		$source = add_query_arg( 'rel', intval( $rel ), $source );
+	}
 
-	if ( isset( $startSlide ) )
+	if ( isset( $startSlide ) ) {
 		$source = add_query_arg( 'startSlide', intval( $startSlide ), $source );
+	}
 
 	$player = sprintf( "<iframe src='%s' width='%d' height='%d'", esc_url( $source ), $w, $h );
 
 	// check the frameborder
-	if ( isset( $fb ) )
+	if ( isset( $fb ) ) {
 		$player .= " frameborder='" . intval( $fb ) . "'";
+	}
 
 	// check the margin width; if not empty, cast as int
-	if ( isset( $mw ) )
+	if ( isset( $mw ) ) {
 		$player .= " marginwidth='" . intval( $mw ) . "'";
+	}
 
 	// check the margin height, if not empty, cast as int
-	if ( isset( $mh ) )
+	if ( isset( $mh ) ) {
 		$player .= " marginheight='" . intval( $mh ) . "'";
+	}
 
-	if ( ! empty( $style ) )
+	if ( ! empty( $style ) ) {
 		$player .= " style='" . $style . "'";
+	}
 
 	// check the scrollbar; cast as a lowercase string for comparison
 	$sc = isset( $sc ) ? strtolower( $sc ) : '';
 
-	if ( in_array( $sc, array( 'yes', 'no' ) ) )
+	if ( in_array( $sc, array( 'yes', 'no' ) ) ) {
 		$player .= " scrolling='" . $sc . "'";
+	}
 
 	$player .= ' allowfullscreen webkitallowfullscreen mozallowfullscreen></iframe>';
 
