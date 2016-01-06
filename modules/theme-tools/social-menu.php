c02d3fccdc2185e4be2a0f6141eac54d80b247c4
Thomas Guillot <thomas@automattic.com>
2016-01-06 11:08:10 +0000

    Social Menu: Rename function to enqueue CSS and extend docblock

--- modules/theme-tools/social-menu.php
+++ modules/theme-tools/social-menu.php
@@ -26,6 +26,10 @@ function jetpack_social_menu_init() {
 	 * Rename the social menu description.
 	 *
 	 * @module theme-tools
+	 *
+	 * @since 3.9.0
+	 *
+	 * @param string $social_menu_description Social Menu description
 	 */
 	$social_menu_description = apply_filters( 'jetpack_social_menu_description', __( 'Social Menu', 'jetpack' ) );
 
@@ -35,12 +39,12 @@ function jetpack_social_menu_init() {
 	) );
 
 	// Enqueue CSS
-	add_action( 'wp_enqueue_scripts', 'jetpack_social_menu_script' );
+	add_action( 'wp_enqueue_scripts', 'jetpack_social_menu_style' );
 }
 add_action( 'after_setup_theme', 'jetpack_social_menu_init', 99 );
 
 /* Function to enqueue CSS */
-function jetpack_social_menu_script() {
+function jetpack_social_menu_style() {
 	if ( has_nav_menu( 'jetpack-social-menu' ) ) {
 		wp_enqueue_style( 'jetpack-social-menu', plugins_url( 'social-menu/social-menu.css', __FILE__ ), array( 'genericons' ), '1.0' );
 	}
