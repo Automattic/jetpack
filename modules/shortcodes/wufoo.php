788d8e45148c5f42fe06c9956804e1e8df82655b
Lance Willett <lance@automattic.com>
2016-01-07 18:44:26 +0000

    Shortcodes: update `wufoo` to remove `extract`; adds unit tests. See #7620.
    Merges r129269-wpcom.

--- modules/shortcodes/wufoo.php
+++ modules/shortcodes/wufoo.php
@@ -10,21 +10,19 @@ http://wufoo.com/docs/code-manager/wordpress-shortcode-plugin/
 
 
 function wufoo_shortcode( $atts ) {
-	extract(
-		shortcode_atts(
-			array(
-				'username'   => '',
-				'formhash'   => '',
-				'autoresize' => true,
-				'height'     => '500',
-				'header'     => 'show',
-				'ssl'        => '',
-			), $atts
-		)
+	$attr = shortcode_atts(
+		array(
+			'username'   => '',
+			'formhash'   => '',
+			'autoresize' => true,
+			'height'     => '500',
+			'header'     => 'show',
+			'ssl'        => '',
+		), $atts
 	);
 
-	//Check username and formhash to ensure they only have alphanumeric characters or underscores, and aren't empty
-	if ( ! preg_match( '/^[a-zA-Z0-9_]+$/', $username ) || ! preg_match( '/^[a-zA-Z0-9_]+$/', $formhash ) ) {
+	// Check username and formhash to ensure they only have alphanumeric characters or underscores, and aren't empty.
+	if ( ! preg_match( '/^[a-zA-Z0-9_]+$/', $attr['username'] ) || ! preg_match( '/^[a-zA-Z0-9_]+$/', $attr['formhash'] ) ) {
 
 		/**
 		 * Return an error to the users with instructions if one of these params is invalid
@@ -40,44 +38,43 @@ function wufoo_shortcode( $atts ) {
 	}
 
 	/**
-	 * Required parameters are present
-	 * An error will be returned inside the form if they are invalid
+	 * Required parameters are present.
+	 * An error will be returned inside the form if they are invalid.
 	 */
-
 	$js_embed = '<script type="text/javascript">var host = (("https:" == document.location.protocol) ? "https://secure." : "http://");document.write(unescape("%3Cscript src=\'" + host + "wufoo.com/scripts/embed/form.js\'  type=\'text/javascript\'%3E%3C/script%3E"));</script>';
 	$js_embed .= "<script type='text/javascript'>";
-	$js_embed .= "var wufoo_$formhash = new WufooForm();";
-	$js_embed .= "wufoo_$formhash.initialize({";
-	$js_embed .= "'userName':'$username', ";
-	$js_embed .= "'formHash':'$formhash', ";
-	$js_embed .= "'autoResize':" . (bool) ( $autoresize ) . ',';
-	$js_embed .= "'height':'" . (int) $height . "',";
-	$js_embed .= "'header':'" . esc_js( $header ) . "' ";
+	$js_embed .= 'var wufoo_' . $attr['formhash'] . ' = new WufooForm();';
+	$js_embed .= 'wufoo_' . $attr['formhash'] . ' .initialize({';
+	$js_embed .= "'userName':'" . $attr['username'] . "', ";
+	$js_embed .= "'formHash':'" . $attr['formhash'] . "', ";
+	$js_embed .= "'autoResize':" . (bool) ( $attr['autoresize'] ) . ',';
+	$js_embed .= "'height':'" . (int) $attr['height'] . "',";
+	$js_embed .= "'header':'" . esc_js( $attr['header'] ) . "' ";
 
 	/**
-	 * Only output SSL value if passes as param
-	 * Lower tier plans don't show this param (don't offer SSL)
+	 * Only output SSL value if passes as param.
+	 * Lower tier plans don't show this param (don't offer SSL).
 	 */
-	$js_embed .= ( $ssl ) ? ",'ssl':" . (bool) $ssl : '';
+	$js_embed .= ( $attr['ssl'] ) ? ",'ssl':" . (bool) $attr['ssl'] : '';
 	$js_embed .= '});';
-	$js_embed .= "wufoo_$formhash.display();";
+	$js_embed .= 'wufoo_' . $attr['formhash'] . '.display();';
 	$js_embed .= '</script>';
 
 	/**
-	 * iframe embed, loaded inside <noscript> tags
+	 * iframe embed, loaded inside <noscript> tags.
 	 */
 	$iframe_embed = '<iframe ';
-	$iframe_embed .= 'height="' . (int) $height . '" ';
+	$iframe_embed .= 'height="' . (int) $attr['height'] . '" ';
 	$iframe_embed .= 'allowTransparency="true" frameborder="0" scrolling="no" style="width:100%;border:none;"';
-	$iframe_embed .= 'src="https://' . $username . '.wufoo.com/embed/' . $formhash . '/">';
-	$iframe_embed .= '<a href="https://' . $username . '.wufoo.com/forms/' . $formhash . '/" ';
+	$iframe_embed .= 'src="https://' . $attr['username'] . '.wufoo.com/embed/' . $attr['formhash'] . '/">';
+	$iframe_embed .= '<a href="https://' . $attr['username'] . '.wufoo.com/forms/' . $attr['formhash'] . '/" ';
 	$iframe_embed .= 'rel="nofollow">Fill out my Wufoo form!</a></iframe>';
 
 	/** This action is already documented in modules/widgets/gravatar-profile.php */
 	do_action( 'jetpack_stats_extra', 'embeds', 'wufoo' );
 
 	/**
-	 * Return embed in JS and iframe
+	 * Return embed in JS and iframe.
 	 */
 	return "$js_embed <noscript> $iframe_embed </noscript>";
 }
