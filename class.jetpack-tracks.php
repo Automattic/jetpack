3a9b0f6aa54be8d33c9911b599634d25ce58a8ea
dereksmart <smart@automattic.com>
2016-01-07 12:04:11 -0500

    Tracks: only send tracking events if site is connected

--- class.jetpack-tracks.php
+++ class.jetpack-tracks.php
@@ -9,6 +9,10 @@ class JetpackTracking {
 	static $product_name = 'jetpack';
 
 	static function track_jetpack_usage() {
+		if ( ! Jetpack::is_active() ) {
+			return;
+		}
+
 		add_action( 'jetpack_pre_activate_module',   array( __CLASS__, 'track_activate_module'), 1, 1 );
 		add_action( 'jetpack_pre_deactivate_module', array( __CLASS__, 'track_deactivate_module'), 1, 1 );
 		add_action( 'jetpack_user_authorized',       array( __CLASS__, 'track_user_linked' ) );
