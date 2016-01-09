58491fd9cedaef6967e29922b6e613b2b432f2cc
Lance Willett <lance@automattic.com>
2016-01-09 20:54:05 +0000

    Shortcodes: update code style for `soundcloud` and add more unit tests. See #7620.
    Merges r129393-wpcom.

--- tests/php/modules/shortcodes/test_class.soundcloud.php
+++ tests/php/modules/shortcodes/test_class.soundcloud.php
@@ -2,20 +2,10 @@
 
 class WP_Test_Jetpack_Shortcodes_Soundcloud extends WP_UnitTestCase {
 
-	/**
-	 * @author scotchfield
-	 * @covers ::soundcloud_shortcode
-	 * @since 3.2
-	 */
 	public function test_shortcodes_soundcloud_exists() {
 		$this->assertEquals( shortcode_exists( 'soundcloud' ), true );
 	}
 
-	/**
-	 * @author scotchfield
-	 * @covers ::soundcloud_shortcode
-	 * @since 3.2
-	 */
 	public function test_shortcodes_soundcloud() {
 		$content = '[soundcloud]';
 
@@ -24,4 +14,12 @@ class WP_Test_Jetpack_Shortcodes_Soundcloud extends WP_UnitTestCase {
 		$this->assertNotEquals( $content, $shortcode_content );
 	}
 
+	public function test_shortcodes_soundcloud_html() {
+		$content = '[soundcloud url="https://api.soundcloud.com/tracks/156661852" params="auto_play=false&amp;hide_related=false&amp;visual=true" width="100%" height="450" iframe="true" /]';
+
+		$shortcode_content = do_shortcode( $content );
+
+		$this->assertContains( '<iframe width="100%" height="450"', $shortcode_content );
+		$this->assertContains( 'w.soundcloud.com/player/?url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F156661852&visual=true&auto_play=false&hide_related=false', $shortcode_content );
+	}
 }
