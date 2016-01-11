ee2503a734ce255ebf048f89ec387b3cf3e2b356
George Stephanis <george@stephanis.info>
2016-01-11 11:40:26 -0500

    New function `videopress_get_attachment_id_by_url`
    This will help us get meta off of a local video url, when we aren’t
    given its attachment id.

--- modules/videopress-v2/videopress.php
+++ modules/videopress-v2/videopress.php
@@ -49,3 +49,54 @@ function videopress_core_json_api_init() {
 function videopress_core_json_api_get_video_details( $data ) {
 	return videopress_get_video_details( $data['guid'] );
 }
+
+/**
+ * Get an attachment ID given a URL.
+ *
+ * Modified from http://wpscholar.com/blog/get-attachment-id-from-wp-image-url/
+ *
+ * @todo: Add some caching in here.
+ *
+ * @param string $url
+ *
+ * @return int|bool Attachment ID on success, false on failure
+ */
+function videopress_get_attachment_id_by_url( $url ) {
+	// Strip out protocols, so it doesn't fail because searching for http: in https: dir.
+	$dir = set_url_scheme( trailingslashit( wp_upload_dir()['baseurl'] ), 'relative' );
+
+	// Is URL in uploads directory?
+	if ( false !== strpos( $url, $dir ) ) {
+
+		$file = basename( $url );
+
+		$query_args = array(
+			'post_type'   => 'attachment',
+			'post_status' => 'inherit',
+			'fields'      => 'ids',
+			'meta_query'  => array(
+				array(
+					'key'     => '_wp_attachment_metadata',
+					'compare' => 'LIKE',
+					'value'   => $file,
+				),
+			)
+		);
+
+		$query = new WP_Query( $query_args );
+
+		if ( $query->have_posts() ) {
+			foreach ( $query->posts as $attachment_id ) {
+				$meta          = wp_get_attachment_metadata( $attachment_id );
+				$original_file = basename( $meta['file'] );
+				$cropped_files = wp_list_pluck( $meta['sizes'], 'file' );
+
+				if ( $original_file === $file || in_array( $file, $cropped_files ) ) {
+					return (int) $attachment_id;
+				}
+			}
+		}
+
+	}
+	return false;
+}
