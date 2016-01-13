492190a760a926aba31453b289fade37aa692b6b
Rocco Tripaldi <tripaldir@gmail.com>
2016-01-13 16:44:46 -0500

    Plugins install endpoint
    Maintain backwards compatibility: versions prior to 3.9 would return no_package instead of download_failed.

--- json-endpoints/jetpack/class.jetpack-json-api-plugins-install-endpoint.php
+++ json-endpoints/jetpack/class.jetpack-json-api-plugins-install-endpoint.php
@@ -38,6 +38,12 @@ class Jetpack_JSON_API_Plugins_Install_Endpoint extends Jetpack_JSON_API_Plugins
 		}
 
 		if ( ! $this->bulk && isset( $error ) ) {
+
+			if ( $error_code === 'download_failed' && $error === 'Download failed.' ) {
+				// For backwards compatibility: versions prior to 3.9 would return no_package instead of download_failed.
+				$error_code = 'no_package';
+			}
+
 			return new WP_Error( $error_code, $this->log[ $slug ]['error'], 400 );
 		}
 
