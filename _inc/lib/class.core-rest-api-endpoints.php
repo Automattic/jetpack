ed043076d95bd46ddf1dc587c8ca69216e44338e
dereksmart <smart@automattic.com>
2016-02-09 12:51:53 -0500

    Core REST Endpoints: add endpoints for activating, deactivating, and new method for checking if it's a registered module (keepin it DRY)

--- _inc/lib/class.core-rest-api-endpoints.php
+++ _inc/lib/class.core-rest-api-endpoints.php
@@ -9,9 +9,23 @@ function jetpack_core_json_api_init() {
 		'permission_callback' => 'Jetpack_Core_Json_Api_Endpoints::manage_modules_permission_check',
 	) );
 
-	register_rest_route( 'jetpack/v4', '/modules/(?P<slug>[a-z\-]+)', array(
+	register_rest_route( 'jetpack/v4', '/module/(?P<slug>[a-z\-]+)', array(
 		'methods' => 'GET',
 		'callback' => 'Jetpack_Core_Json_Api_Endpoints::get_module',
+		'permission_callback' => 'Jetpack_Core_Json_Api_Endpoints::manage_modules_permission_check',
+	) );
+
+	// Activate a Module
+	register_rest_route( 'jetpack/v4', '/module/(?P<slug>[-[:alpha:]]+)/activate', array(
+		'methods' => 'GET',
+		'callback' => 'Jetpack_Core_Json_Api_Endpoints::activate_module',
+		'permission_callback' => 'Jetpack_Core_Json_Api_Endpoints::manage_modules_permission_check',
+	) );
+
+	// Deactivate a Module
+	register_rest_route( 'jetpack/v4', '/module/(?P<slug>[-[:alpha:]]+)/deactivate', array(
+		'methods' => 'GET',
+		'callback' => 'Jetpack_Core_Json_Api_Endpoints::deactivate_module',
 		'permission_callback' => 'Jetpack_Core_Json_Api_Endpoints::manage_modules_permission_check',
 	) );
 
@@ -19,22 +33,46 @@ function jetpack_core_json_api_init() {
 
 class Jetpack_Core_Json_Api_Endpoints {
 
+	public static function manage_modules_permission_check() {
+		return current_user_can( 'jetpack_manage_modules' );
+	}
+
+	public static function is_jetpack_module( $slug ) {
+		$modules = Jetpack::get_available_modules();
+		if ( ! in_array( $slug, $modules, true ) ) {
+			return false;
+		}
+
+		return true;
+	}
+
 	public static function get_modules() {
 		require_once( JETPACK__PLUGIN_DIR . 'class.jetpack-admin.php' );
 		return Jetpack_Admin::init()->get_modules();
 	}
 
 	public static function get_module( $data ) {
-		$modules = Jetpack::get_available_modules();
-		if ( in_array( $data['slug'], $modules ) ) {
+		if ( self::is_jetpack_module( $data['slug'] ) ) {
 			return Jetpack::get_module( $data['slug'] );
 		}
 
 		return new WP_Error( 'not-found', __( 'The requested Jetpack module was not found.' ), array( 'status' => 404 ) );
 	}
 
-	public static function manage_modules_permission_check() {
-		return current_user_can( 'jetpack_manage_modules' );
+	public static function activate_module( $data ) {
+		if ( self::is_jetpack_module( $data['slug'] ) ) {
+			return Jetpack::activate_module( $data['slug'], true, false );
+		}
+
+		return new WP_Error( 'not-found', __( 'The requested Jetpack module was not found.' ), array( 'status' => 404 ) );
+	}
+
+	public static function deactivate_module( $data ) {
+		if ( self::is_jetpack_module( $data['slug'] ) ) {
+			return Jetpack::deactivate_module( $data['slug'] );
+		}
+
+		return new WP_Error( 'not-found', __( 'The requested Jetpack module was not found.' ), array( 'status' => 404 ) );
 	}
 
 }
