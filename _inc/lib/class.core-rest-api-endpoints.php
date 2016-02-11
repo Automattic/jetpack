ea19e725dc788724ee0d7664ca3e750bd91374db
dereksmart <smart@automattic.com>
2016-02-11 13:54:11 -0500

    Core REST endpoints: add endpoints for getting Protect and Akismet counts

--- _inc/lib/class.core-rest-api-endpoints.php
+++ _inc/lib/class.core-rest-api-endpoints.php
@@ -1,4 +1,5 @@
 <?php
+require_once ABSPATH . '/wp-includes/class-wp-error.php';
 
 add_action( 'rest_api_init', 'jetpack_core_json_api_init' );
 function jetpack_core_json_api_init() {
@@ -29,14 +30,40 @@ function jetpack_core_json_api_init() {
 		'permission_callback' => 'Jetpack_Core_Json_Api_Endpoints::manage_modules_permission_check',
 	) );
 
+
+	// Protect: Get blocked count
+	register_rest_route( 'jetpack/v4', '/module/protect/count/get', array(
+		'methods' => 'GET',
+		'callback' => 'Jetpack_Core_Json_Api_Endpoints::protect_get_blocked_count',
+		'permission_callback' => 'Jetpack_Core_Json_Api_Endpoints::manage_modules_permission_check',
+	) );
+
+	// Akismet: Get spam count
+	register_rest_route( 'jetpack/v4', '/akismet/count/get', array(
+		'methods'  => 'GET',
+		'callback' => 'Jetpack_Core_Json_Api_Endpoints::akismet_get_spam_count',
+		'args'     => array(
+			'date' => array(
+				'default' => 'all',
+				'required' => true,
+				'sanitize_callback' => 'absint'
+			),
+		),
+		'permission_callback' => 'Jetpack_Core_Json_Api_Endpoints::manage_modules_permission_check',
+	) );
 }
 
 class Jetpack_Core_Json_Api_Endpoints {
-
 	public static function manage_modules_permission_check() {
 		return current_user_can( 'jetpack_manage_modules' );
 	}
 
+	/*
+	 * Check if given slug is actually a Jetpack module
+	 *
+	 * @param  string $slug
+	 * @return bool   true|false
+	 */
 	public static function is_jetpack_module( $slug ) {
 		$modules = Jetpack::get_available_modules();
 		if ( ! in_array( $slug, $modules, true ) ) {
@@ -45,6 +72,39 @@ class Jetpack_Core_Json_Api_Endpoints {
 
 		return true;
 	}
+
+	/*
+	 * Checks if a plugin is active, and includes the files necessary to do so.
+	 *
+	 * @param  string $plugin the path of the main plugin file.
+	 * @return bool true|false
+	 */
+	public static function is_plugin_activated( $plugin ) {
+		require_once ABSPATH . '/wp-admin/includes/plugin.php';
+		return is_plugin_active( $plugin );
+	}
+
+	/*
+	 * Is Akismet registered and active?
+	 *
+	 * @return true|WP_Error on failure.
+	 */
+	public static function akismet_is_active_and_registered() {
+		if ( ! self::is_plugin_activated( 'akismet/akismet.php' ) ) {
+			return new WP_Error( 'not-active', __( 'Please activate akismet.' ), array( 'status' => 404 ) );
+		}
+
+		require_once WP_PLUGIN_DIR . '/akismet/class.akismet.php';
+		require_once WP_PLUGIN_DIR . '/akismet/class.akismet-admin.php';
+		$akismet_key = Akismet::verify_key( Akismet::get_api_key() );
+
+		if ( ! $akismet_key || 'invalid' === $akismet_key || 'failed' === $akismet_key ) {
+			return new WP_Error( 'akismet-no-key', __( 'No valid API key for Akismet' ), array( 'status' => 404 ) );
+		}
+
+		return true;
+	}
+
 
 	public static function get_modules() {
 		require_once( JETPACK__PLUGIN_DIR . 'class.jetpack-admin.php' );
@@ -73,6 +133,34 @@ class Jetpack_Core_Json_Api_Endpoints {
 		}
 
 		return new WP_Error( 'not-found', __( 'The requested Jetpack module was not found.' ), array( 'status' => 404 ) );
+	}
+
+
+	/*
+	 * Admin stats stuff
+	 */
+	public static function protect_get_blocked_count() {
+		if ( Jetpack::is_module_active( 'protect' ) ) {
+			return get_site_option( 'jetpack_protect_blocked_attempts' );
+		}
+
+		return new WP_Error( 'not-active', __( 'The requested Jetpack module is not active.' ), array( 'status' => 404 ) );
+	}
+
+	public static function akismet_get_spam_count( WP_REST_Request $data ) {
+		if ( ! is_wp_error( $status = self::akismet_is_active_and_registered() ) ) {
+			$count_data = Akismet_Admin::get_stats( Akismet::get_api_key() );
+		} else {
+			return $status->get_error_messages();
+		}
+
+		if ( 'all' === $data['date'] ) {
+			return $count_data['all']->spam;
+		}
+
+		// Organize the requested date time to YYYY-MM
+		$data['date'] = DateTime::createFromFormat( 'Ym', $data['date'] );
+		return $count_data['6-months']->breakdown->{ $data['date']->format( 'Y-m' ) }->spam;
 	}
 
 }
