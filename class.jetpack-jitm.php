a435d547058a7e43c70b612ecac0971829863252
dereksmart <smart@automattic.com>
2016-02-21 15:41:13 -0500

    General: move get_jp_emblem() to a more central place
    we'll need this later, and it can't be in a private Class.

--- class.jetpack-jitm.php
+++ class.jetpack-jitm.php
@@ -84,7 +84,7 @@ class Jetpack_JITM {
 			<a href="#" data-module="manage" class="dismiss"><span class="genericon genericon-close"></span></a>
 
 			<div class="jp-emblem">
-				<?php echo self::get_jp_emblem(); ?>
+				<?php echo Jetpack::get_jp_emblem(); ?>
 			</div>
 			<p class="msg">
 				<?php esc_html_e( 'Reduce security risks with automated plugin updates.', 'jetpack' ); ?>
@@ -115,7 +115,7 @@ class Jetpack_JITM {
 			<a href="#" data-module="photon" class="dismiss"><span class="genericon genericon-close"></span></a>
 
 			<div class="jp-emblem">
-				<?php echo self::get_jp_emblem(); ?>
+				<?php echo Jetpack::get_jp_emblem(); ?>
 			</div>
 			<p class="msg">
 				<?php esc_html_e( 'Speed up your photos and save bandwidth costs by using a free content delivery network.', 'jetpack' ); ?>
@@ -144,7 +144,7 @@ class Jetpack_JITM {
 				<a href="#" data-module="photon" class="dismiss"><span class="genericon genericon-close"></span></a>
 
 				<div class="jp-emblem">
-					<?php echo self::get_jp_emblem(); ?>
+					<?php echo Jetpack::get_jp_emblem(); ?>
 				</div>
 				<p class="msg">
 					<?php esc_html_e( 'Let Jetpack deliver your images optimized and faster than ever.', 'jetpack' ); ?>
@@ -212,7 +212,7 @@ class Jetpack_JITM {
 				<a href="#" data-module="manage-pi" class="dismiss"><span class="genericon genericon-close"></span></a>
 
 				<div class="jp-emblem">
-					<?php echo self::get_jp_emblem(); ?>
+					<?php echo Jetpack::get_jp_emblem(); ?>
 				</div>
 				<?php if ( ! $manage_active ) : ?>
 					<p class="msg">
@@ -254,7 +254,7 @@ class Jetpack_JITM {
 			<div class="jp-jitm">
 				<a href="#"  data-module="editor" class="dismiss"><span class="genericon genericon-close"></span></a>
 				<div class="jp-emblem">
-					<?php echo self::get_jp_emblem(); ?>
+					<?php echo Jetpack::get_jp_emblem(); ?>
 				</div>
 				<p class="msg">
 					<?php esc_html_e( 'Try the brand new editor.', 'jetpack' ); ?>
@@ -285,7 +285,7 @@ class Jetpack_JITM {
 			<a href="#" data-module="stats" class="dismiss"><span class="genericon genericon-close"></span></a>
 
 			<div class="jp-emblem">
-				<?php echo self::get_jp_emblem(); ?>
+				<?php echo Jetpack::get_jp_emblem(); ?>
 			</div>
 			<p class="msg">
 				<?php esc_html_e( 'Track detailed stats on this post and the rest of your site.', 'jetpack' ); ?>
@@ -452,17 +452,6 @@ class Jetpack_JITM {
 
 		// so if it's not an array, it means no JITM was dismissed
 		return is_array( self::$jetpack_hide_jitm );
-	}
-
-	/**
-	 * Return string containing the Jetpack logo.
-	 *
-	 * @since 3.9.0
-	 *
-	 * @return string
-	 */
-	function get_jp_emblem() {
-		return '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0" y="0" viewBox="0 0 172.9 172.9" enable-background="new 0 0 172.9 172.9" xml:space="preserve">	<path d="M86.4 0C38.7 0 0 38.7 0 86.4c0 47.7 38.7 86.4 86.4 86.4s86.4-38.7 86.4-86.4C172.9 38.7 134.2 0 86.4 0zM83.1 106.6l-27.1-6.9C49 98 45.7 90.1 49.3 84l33.8-58.5V106.6zM124.9 88.9l-33.8 58.5V66.3l27.1 6.9C125.1 74.9 128.4 82.8 124.9 88.9z" /></svg>';
 	}
 }
 /**
