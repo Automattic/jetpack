165470a3275b06cd3da403103debfcaaef4df574
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-02-24 11:48:18 -0800

    Fixed failing test, added addiotional tests, added listenings for changes to the post meta

--- class.jetpack-rest-sync.php
+++ class.jetpack-rest-sync.php
@@ -2,14 +2,49 @@
 
 class Jetpack_Rest_Sync {
 
-	static $posts = array();
+	static $posts = array(
+		'new' => array(),
+		'update' => array(),
+		'delete' => array(),
+		);
 
 	static function init() {
 		add_action( 'transition_post_status', array( __CLASS__, 'transition_post_status' ), 10, 3 );
+		add_action( 'delete_post', array( __CLASS__, 'delete_post_action' ) );
+
+		// set mark the post as needs updating when post meta changes.
+		add_action( 'added_post_meta', array( __CLASS__, 'update_post_meta') , 10, 4 );
+		add_action( 'updated_postmeta', array( __CLASS__, 'update_post_meta') , 10, 4 );
+		add_action( 'deleted_post_meta', array( __CLASS__, 'update_post_meta') , 10, 4 );
 	}
 
 	static function transition_post_status( $new_status, $old_status, $post ) {
-		self::$posts = array_unique( array_push( self::$posts, $post->ID ) );
+		if ( 'new' === $old_status && ! in_array( $post->ID, self::$posts['new'] ) ) {
+			self::$posts['new'][] = $post->ID;
+			return;
+		}
+
+		if ( 'trash' === $new_status && ! in_array( $post->ID, self::$posts['delete'] ) ) {
+			self::$posts['delete'][] = $post->ID;
+			return;
+		}
+
+		if ( ! in_array( $post->ID, self::$posts['update'] ) ) {
+			self::$posts['update'][] = $post->ID;
+		}
+	}
+
+	static function delete_post_action( $post_id ) {
+		if ( ! in_array( $post_id, self::$posts['delete'] ) ) {
+			self::$posts['delete'][] = $post_id;
+			return;
+		}
+	}
+
+	static function update_post_meta( $meta_id, $post_id, $meta_key, $_meta_value ) {
+		if ( ! in_array( $post_id, self::$posts['update'] ) ) {
+			self::$posts['update'][] = $post_id;
+		}
 	}
 }
 
--- tests/php/test_class.jetpack-sync.php
+++ tests/php/test_class.jetpack-sync.php
@@ -1,5 +1,5 @@
 <?php
-
+// phpunit --filter test_sync_
 class WP_Test_Jetpack_Sync extends WP_UnitTestCase {
 
 	protected $_globals;
@@ -7,24 +7,145 @@ class WP_Test_Jetpack_Sync extends WP_UnitTestCase {
 	public function setUp() {
 		require_once dirname( __FILE__ ) . '/../../class.jetpack-rest-sync.php';
 		parent::setUp();
+		Jetpack_Rest_Sync::$posts['new'] = array();
+		Jetpack_Rest_Sync::$posts['update'] = array();
+		Jetpack_Rest_Sync::$posts['delete'] = array();
+
 	}
 
 	public function tearDown() {
 		parent::tearDown();
 	}
 
-	public function test_Jetpack_instance() {
-		$my_post = array(
+	public function test_sync_new_post() {
+		$new_post = array(
+			'post_title'    => 'this is the title',
+			'post_content'  => 'this is the content',
+			'post_status'   => 'draft',
+			'post_author'   => 1,
+		);
+		$post_id = wp_insert_post( $new_post );
+		$this->assertContains( $post_id, Jetpack_Rest_Sync::$posts['new'] );
+	}
+
+	public function test_sync_update_post() {
+		$new_post = array(
+			'post_title'    => 'this is the title',
+			'post_content'  => 'this is the content',
+			'post_status'   => 'draft',
+			'post_author'   => 1,
+		);
+		$post_id = wp_insert_post( $new_post );
+
+		wp_update_post( array(
+			'ID' => $post_id,
+			'post_title'    => 'this is the updated title',
+			'post_content'  => 'this is the content',
+			'post_status'   => 'draft',
+			'post_author'   => 1,
+		) );
+
+		$this->assertContains( $post_id, Jetpack_Rest_Sync::$posts['update'] );
+	}
+
+	public function test_sync_status_change() {
+		$new_post = array(
 			'post_title'    => 'this is the title',
 			'post_content'  => 'this is the content',
 			'post_status'   => 'draft',
 			'post_author'   => 1,
 		);
 
-		$post_id = wp_insert_post( $my_post );
+		$post_id = wp_insert_post( $new_post );
 
-		$this->assertContains( $post_id, Jetpack_Rest_Sync::posts );
+		wp_update_post( array(
+			'ID' => $post_id,
+			'post_status'   => 'publish',
+		) );
 
+		$this->assertContains( $post_id, Jetpack_Rest_Sync::$posts['update'] );
 	}
 
+	public function test_sync_add_post_meta() {
+		$new_post = array(
+			'post_title'    => 'this is the title',
+			'post_content'  => 'this is the content',
+			'post_status'   => 'draft',
+			'post_author'   => 1,
+		);
+		$post_id = wp_insert_post( $new_post );
+
+		add_post_meta( $post_id, '_color', 'red', true );
+
+		$this->assertContains( $post_id, Jetpack_Rest_Sync::$posts['update'] );
+	}
+
+	public function test_sync_update_post_meta() {
+		$new_post = array(
+			'post_title'    => 'this is the title',
+			'post_content'  => 'this is the content',
+			'post_status'   => 'draft',
+			'post_author'   => 1,
+		);
+
+		$post_id = wp_insert_post( $new_post );
+
+		add_post_meta( $post_id, '_color', 'red' );
+		// Reset the array since if the add post meta test passes so should the test.
+		Jetpack_Rest_Sync::$posts['update'] = array();
+
+		update_post_meta( $post_id, '_color', 'blue' );
+
+		$this->assertContains( $post_id, Jetpack_Rest_Sync::$posts['update'] );
+	}
+
+
+	public function test_sync_delete_post_meta() {
+		$new_post = array(
+			'post_title'    => 'this is the title',
+			'post_content'  => 'this is the content',
+			'post_status'   => 'draft',
+			'post_author'   => 1,
+		);
+
+		$post_id = wp_insert_post( $new_post );
+
+		add_post_meta( $post_id, '_color', 'blue' );
+		// Reset the array since if the add post meta test passes so should the test.
+		Jetpack_Rest_Sync::$posts['update'] = array();
+
+		delete_post_meta( $post_id, '_color', 'blue' );
+
+		$this->assertContains( $post_id, Jetpack_Rest_Sync::$posts['update'] );
+	}
+
+	public function test_sync_delete_post() {
+		$new_post = array(
+			'post_title'    => 'this is the title',
+			'post_content'  => 'this is the content',
+			'post_status'   => 'draft',
+			'post_author'   => 1,
+		);
+
+		$post_id = wp_insert_post( $new_post );
+
+		wp_delete_post( $post_id );
+
+		$this->assertContains( $post_id, Jetpack_Rest_Sync::$posts['delete'] );
+	}
+
+	public function test_sync_force_delete_post() {
+		$new_post = array(
+			'post_title'    => 'this is the title',
+			'post_content'  => 'this is the content',
+			'post_status'   => 'draft',
+			'post_author'   => 1,
+		);
+
+		$post_id = wp_insert_post( $new_post );
+
+		wp_delete_post( $post_id, true );
+
+		$this->assertContains( $post_id, Jetpack_Rest_Sync::$posts['delete'] );
+	}
 }
