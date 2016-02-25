375f6d153491c5c854b124c45e980c9c17b44abe
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-02-24 16:58:12 -0800

    Add Test: That makes sure that a we sync the post when a new post gets added.

--- tests/php/test_class.jetpack-sync.php
+++ tests/php/test_class.jetpack-sync.php
@@ -221,6 +221,31 @@ class WP_Test_Jetpack_Sync extends WP_UnitTestCase {
 		$this->assertContains( $parent_post_id, Jetpack_Post_Sync::$posts['sync'] );
 	}
 
+	public function test_sync_post_data_when_new_comment_gets_added() {
+
+		$time = current_time('mysql');
+		$post_id = wp_insert_post( self::get_new_post_array() );
+
+		$data = array(
+			'comment_post_ID' => $post_id,
+			'comment_author' => 'admin',
+			'comment_author_email' => 'admin@admin.com',
+			'comment_author_url' => 'http://',
+			'comment_content' => 'content here',
+			'comment_type' => '',
+			'comment_parent' => 0,
+			'user_id' => 1,
+			'comment_author_IP' => '127.0.0.1',
+			'comment_agent' => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.10) Gecko/2009042316 Firefox/3.0.10 (.NET CLR 3.5.30729)',
+			'comment_date' => $time,
+			'comment_approved' => 1,
+		);
+
+		wp_insert_comment( $data );
+
+		$this->assertContains( $post_id, Jetpack_Post_Sync::$posts['sync'] );
+	}
+
 	public function test_sync_delete_post() {
 		$new_post = self::get_new_post_array();
 
