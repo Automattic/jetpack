2b7fa9ffb40413f2fb6d182346a9900ce68a61dd
dereksmart <smart@automattic.com>
2016-02-28 20:46:53 -0500

    Grunt-to-gulp: Update package.json and add styling grunt tasks

---
+++ gulpfile.js
@@ -0,0 +1,159 @@
+process.env.UV_THREADPOOL_SIZE = 100; // fix a bug in libsass
+
+var autoprefixer = require( 'gulp-autoprefixer' );
+var banner = require( 'gulp-banner' );
+var cleanCSS = require( 'gulp-clean-css' );
+var concat = require( 'gulp-concat' );
+var gulp = require( 'gulp' );
+var path = require( 'path' );
+var rename = require( 'gulp-rename' );
+var rtlcss = require( 'gulp-rtlcss' );
+var sass = require( 'gulp-sass' );
+var shell = require( 'gulp-shell' );
+var sourcemaps = require( 'gulp-sourcemaps' );
+
+/* Admin CSS to be minified, autoprefixed, rtl */
+var admincss = [
+	'modules/after-the-deadline/atd.css',
+	'modules/after-the-deadline/tinymce/css/content.css',
+	'modules/contact-form/css/menu-alter.css',
+	'modules/custom-css/csstidy/cssparse.css',
+	'modules/custom-css/csstidy/cssparsed.css',
+	'modules/custom-css/custom-css/css/codemirror.css',
+	'modules/custom-css/custom-css/css/css-editor.css',
+	'modules/custom-css/custom-css/css/use-codemirror.css',
+	'modules/omnisearch/omnisearch.css',
+	'modules/omnisearch/omnisearch-jetpack.css',
+	'modules/post-by-email/post-by-email.css',
+	'modules/publicize/assets/publicize.css',
+	'modules/protect/protect-dashboard-widget.css',
+	'modules/sharedaddy/admin-sharing.css',
+	'modules/videopress/videopress-admin.css',
+	'modules/widget-visibility/widget-conditions/widget-conditions.css',
+	'modules/widgets/gallery/css/admin.css'
+];
+
+/* Front-end CSS to be concatenated */
+var frontendcss = [
+	'modules/carousel/jetpack-carousel.css',
+	'modules/contact-form/css/grunion.css',
+	'modules/infinite-scroll/infinity.css',
+	'modules/likes/style.css',
+	'modules/related-posts/related-posts.css',
+	'modules/sharedaddy/sharing.css',
+	'modules/shortcodes/css/slideshow-shortcode.css',
+	'modules/shortcodes/css/style.css', // TODO: Should be renamed to shortcode-presentations
+	'modules/subscriptions/subscriptions.css',
+	'modules/theme-tools/responsive-videos/responsive-videos.css',
+	'modules/theme-tools/social-menu/social-menu.css',
+	'modules/tiled-gallery/tiled-gallery/tiled-gallery.css',
+	'modules/widgets/wordpress-post-widget/style.css',
+	'modules/widgets/gravatar-profile.css',
+	'modules/widgets/goodreads/css/goodreads.css',
+	'modules/widgets/social-media-icons/style.css',
+	'modules/widgets/top-posts/style.css',
+	'modules/widgets/widgets.css' // TODO Moved to image-widget/style.css
+];
+
+// Minimizes admin css for modules.  Outputs to same folder as min.css
+gulp.task( 'admincss', function() {
+	return gulp.src( admincss, { base: "./" } )
+		.pipe( autoprefixer( 'last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'Firefox 14', 'opera 12.1', 'ios 6', 'android 4' ) )
+		.pipe( cleanCSS( { compatibility: 'ie8' } ) )
+		.pipe( rename( { suffix: '.min' } ) )
+		.pipe( banner( '\n/* Do not modify this file directly.  It is concatenated from individual module CSS files. */\n\n') )
+		.pipe( gulp.dest( '.' ) )
+		.on( 'end', function() {
+			console.log( 'admin CSS finished.' );
+		} );
+});
+
+// Admin RTL CSS for modules.  Auto-prefix, RTL, Minify, RTL the minimized version.
+gulp.task( 'admincss:rtl', function() {
+	return gulp.src( admincss, { base: './' } )
+		.pipe( autoprefixer( 'last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'Firefox 14', 'opera 12.1', 'ios 6', 'android 4' ) )
+		.pipe( rtlcss() )
+		.pipe( rename( { suffix: '-rtl' } ) )
+		.pipe( banner( '\n/* Do not modify this file directly.  It is concatenated from individual module CSS files. */\n\n') )
+		.pipe( gulp.dest( '.' ) )
+		.pipe( cleanCSS( { compatibility: 'ie8' } ) )
+		.pipe( rename( { suffix: '.min' } ) )
+		.pipe( gulp.dest( '.' ) )
+		.on( 'end', function() {
+			console.log( 'admin RTL CSS finished.' );
+		} );
+} );
+
+// Frontend CSS.  Auto-prefix and minimize.
+gulp.task( 'frontendcss', function() {
+	return gulp.src( frontendcss )
+		.pipe( autoprefixer( 'last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'Firefox 14', 'opera 12.1', 'ios 6', 'android 4' ) )
+		.pipe( cleanCSS( { compatibility: 'ie8' } ) )
+		.pipe( concat( 'jetpack.css' ) )
+		.pipe( banner( '\n/* Do not modify this file directly.  It is concatenated from individual module CSS files. */\n\n') )
+		.pipe( gulp.dest( 'css' ) )
+		.on( 'end', function() {
+			console.log( 'front end CSS finished.' );
+		} );
+});
+
+/*
+	Sass!
+ */
+gulp.task( 'sass', function() {
+		return gulp.src( 'scss/*.scss' )
+			.pipe( sass().on( 'Hold up! error!', sass.logError ) )
+			.pipe( autoprefixer() )
+			// Build *.css & sourcemaps
+			.pipe( sourcemaps.init() )
+			.pipe( sourcemaps.write( './' ) )
+			.pipe( rename( { dirname: 'cssssss' } ) )
+			.pipe( gulp.dest( './' ) )
+			// Build *.min.css & sourcemaps
+			.pipe( cleanCSS( { compatibility: 'ie8' } ) )
+			.pipe( rename( { suffix: '.min' } ) )
+			.pipe( gulp.dest( './' ) )
+			.pipe( sourcemaps.write( '.' ) )
+			.on( 'end', function() {
+				console.log( 'CSS finished.' );
+			} );
+} );
+
+/*
+    Sass! (RTL)
+ */
+gulp.task( 'sass:rtl', function() {
+	return gulp.src( 'scss/*.scss' )
+		.pipe( sass().on( 'Hold up! error!', sass.logError ) )
+		.pipe( autoprefixer() )
+		// Build *-rtl.css & sourcemaps
+		.pipe( rtlcss() )
+		.pipe( rename( { suffix: '-rtl' } ) )
+		.pipe( sourcemaps.init() )
+		.pipe( sourcemaps.write( './' ) )
+		.pipe( rename( { dirname: 'cssssss' } ) )
+		.pipe( gulp.dest( './' ) )
+		// Build *-rtl.min.css
+		.pipe( cleanCSS( { compatibility: 'ie8' } ) )
+		.pipe( rename( { suffix: '.min' } ) )
+		.pipe( gulp.dest( './' ) )
+		// Finished
+		.on( 'end', function() {
+			console.log( 'CSS finished.' );
+		} );
+} );
+
+gulp.task( 'sass:watch', function () {
+	gulp.watch( 'scss/*.scss', ['sass'] );
+});
+
+/*
+	Shell commands
+ */
+gulp.task( 'shell', function() {
+
+} );
+
+gulp.task( 'watch',   ['sass:watch'] );
+gulp.task( 'styles',  ['frontendcss', 'admincss', 'admincss:rtl', 'sass', 'sass:rtl'] );
+gulp.task( 'default', ['styles'] );
