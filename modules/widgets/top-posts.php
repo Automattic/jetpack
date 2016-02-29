59619d3a99055675f0500290610d560a9fbb77a7
Ryan Hellyer <ryanhellyer@gmail.com>
2016-02-29 18:06:59 +0100

    Allowing for passing the widget arguments to filters. This allows external plugins to work out which widget ID is being used at loading time (this allows them to find settings via get_option).

--- modules/widgets/top-posts.php
+++ modules/widgets/top-posts.php
@@ -241,7 +241,7 @@ class Jetpack_Top_Posts_Widget extends WP_Widget {
 		if ( function_exists( 'wpl_get_blogs_most_liked_posts' ) && 'likes' == $ordering ) {
 			$posts = $this->get_by_likes( $count );
 		} else {
-			$posts = $this->get_by_views( $count );
+			$posts = $this->get_by_views( $count, $args );
 		}
 
 		// Filter the returned posts. Remove all posts that do not match the chosen Post Types.
@@ -402,19 +402,7 @@ class Jetpack_Top_Posts_Widget extends WP_Widget {
 		return $this->get_posts( array_keys( $post_likes ), $count );
 	}
 
-	function get_by_views( $count ) {
-		if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
-			global $wpdb;
-
-			$post_views = wp_cache_get( "get_top_posts_$count", 'stats' );
-			if ( false === $post_views ) {
-				$post_views = array_shift( stats_get_daily_history( false, get_current_blog_id(), 'postviews', 'post_id', false, 2, '', $count * 2 + 10, true ) );
-				unset( $post_views[0] );
-				wp_cache_add( "get_top_posts_$count", $post_views, 'stats', 1200);
-			}
-
-			return $this->get_posts( array_keys( $post_views ), $count );
-		}
+	function get_by_views( $count, $args ) {
 
 		/**
 		 * Filter the number of days used to calculate Top Posts for the Top Posts widget.
@@ -425,7 +413,7 @@ class Jetpack_Top_Posts_Widget extends WP_Widget {
 		 *
 		 * @param int 2 Number of days. Default is 2.
 		 */
-		$days = (int) apply_filters( 'jetpack_top_posts_days', 2 );
+		$days = (int) apply_filters( 'jetpack_top_posts_days', 2, $args );
 
 		if ( $days < 1 ) {
 			$days = 2;
