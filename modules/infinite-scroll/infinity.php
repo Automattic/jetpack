04cc129ef8f057c161fac633efeb904508109e28
Jeremy Herve <jeremy@tagada.hu>
2016-03-02 18:46:43 +0100

    Infinite Scroll: document filter

--- modules/infinite-scroll/infinity.php
+++ modules/infinite-scroll/infinity.php
@@ -247,6 +247,7 @@ class The_Neverending_Home_Page {
 			self::$settings = apply_filters( 'infinite_scroll_settings', $settings );
 		}
 
+		/** This filter is documented in modules/infinite-scroll/inifinity.php */
 		return (object) apply_filters( 'infinite_scroll_settings', self::$settings );
 	}
 
