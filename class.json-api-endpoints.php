2a16c68d0f1159ce5573b32269ee82e50d0d0ed4
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-03-09 11:27:10 -0800

    Improvements to REST API so that it is easy to call internally
    - Defined new constant JETPACK__WPCOM_JSON_API_BASE,
    - Prevent notice error if $_SERVER['HTTP_USER_AGENT'] is not set.
    - Allow the WPCOM JSON API to each time.
    - Allow serve to be called and return data internally.

--- class.json-api-endpoints.php
+++ class.json-api-endpoints.php
@@ -1689,7 +1689,7 @@ abstract class WPCOM_JSON_API_Endpoint {
 	function get_link() {
 		$args   = func_get_args();
 		$format = array_shift( $args );
-		$base = WPCOM_JSON_API__BASE;
+		$base = JETPACK__WPCOM_JSON_API_BASE;
 
 		$path = array_pop( $args );
 
@@ -1715,7 +1715,7 @@ abstract class WPCOM_JSON_API_Endpoint {
 
 		// escape any % in the relative path before running it through sprintf again
 		$relative_path = str_replace( '%', '%%', $relative_path );
-		// http, WPCOM_JSON_API__BASE, ...    , path
+		// http, JETPACK__WPCOM_JSON_API_BASE, ...    , path
 		// %s  , %s                  , $format, %s
 		return esc_url_raw( sprintf( "%s://%s$relative_path", $this->api->public_api_scheme, $base ) );
 	}
