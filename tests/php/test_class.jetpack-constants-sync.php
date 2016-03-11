8643ccb11170f83aacbeb7b76faaa6e1a4c4090b
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-03-11 11:48:34 -0800

    Make it pretty

--- tests/php/test_class.jetpack-constants-sync.php
+++ tests/php/test_class.jetpack-constants-sync.php
@@ -16,27 +16,30 @@ class WP_Test_Jetpack_Constants_Sync extends WP_UnitTestCase {
 	}
 
 	public function test_sync_update_option() {
-		if( defined( 'EMPTY_TRASH_DAYS' ) ) {
+		if ( defined( 'EMPTY_TRASH_DAYS' ) ) {
 			$empty_trash_days = EMPTY_TRASH_DAYS;
 		} else {
 			$empty_trash_days = 30;
 			define( 'EMPTY_TRASH_DAYS', $empty_trash_days );
 		}
-		$query_string = Jetpack_Constants_Sync::getQueryString();
-		$this->assertContains( 'EMPTY_TRASH_DAYS=' . $empty_trash_days,  $query_string );
+		$values       = Jetpack_Constants_Sync::sync_all();
+		$query_string = Jetpack_Constants_Sync::getQueryString( $values );
+		$this->assertContains( 'EMPTY_TRASH_DAYS=' . $empty_trash_days, $query_string );
 	}
 
 
-	public function test_sync_post_revisiions_constant() {
-		if( defined( 'WP_POST_REVISIONS' ) ) {
+	public function test_sync_post_revisions_constant() {
+		if ( defined( 'WP_POST_REVISIONS' ) ) {
 			$post_revisions = WP_POST_REVISIONS;
 		} else {
 			$post_revisions = rand( 0, 1000 );
 			define( 'WP_POST_REVISIONS', $post_revisions );
 		}
 
-		$query_string = Jetpack_Constants_Sync::sync();
-		$this->assertContains( 'WP_POST_REVISIONS=' . $post_revisions,  $query_string );
+		$values = Jetpack_Constants_Sync::sync();
+
+		$query_string = Jetpack_Constants_Sync::getQueryString( $values );
+		$this->assertContains( 'WP_POST_REVISIONS=' . $post_revisions, $query_string );
 
 		$dont_sync = Jetpack_Constants_Sync::sync();
 		$this->assertNull( $dont_sync );
