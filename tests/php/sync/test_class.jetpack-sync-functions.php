760f8ca2f961f055e3bfbdfb68d3e36ab3c5a3bb
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-03-14 12:09:20 -0700

    Fix all the tests.

--- tests/php/sync/test_class.jetpack-sync-functions.php
+++ tests/php/sync/test_class.jetpack-sync-functions.php
@@ -15,18 +15,13 @@ class WP_Test_Jetpack_Sync_Functions extends WP_UnitTestCase {
 		parent::tearDown();
 	}
 
-	public function test_sync_functions() {
-		$values = Jetpack_Sync_Functions::get_all();
-
-		$this->assertEquals( Jetpack::featured_images_enabled(), $values['featured_images_enabled'] );
-	}
-
 	public function test_sync_all_functions() {
 		global $wp_version;
 
 		$values = Jetpack_Sync_Functions::get_all();
 
 		$this->assertEquals( $wp_version, $values['wp_version'] );
+		$this->assertEquals( Jetpack::featured_images_enabled(), $values['featured_images_enabled'] );
 		$this->assertEquals( wp_max_upload_size(), $values['wp_max_upload_size'] );
 	}
 
--- tests/php/sync/test_class.jetpack-sync-options.php
+++ tests/php/sync/test_class.jetpack-sync-options.php
@@ -26,7 +26,7 @@ class WP_Test_Jetpack_Sync_Options extends WP_UnitTestCase {
 
 		add_option( $option, 1 );
 
-		$this->assertContains( $option, Jetpack_Sync_Options::get_to_sync() );
+		$this->assertContains( $option, array_keys( Jetpack_Sync_Options::get_to_sync() ) );
 	}
 
 	public function test_sync_update_option() {
@@ -37,7 +37,7 @@ class WP_Test_Jetpack_Sync_Options extends WP_UnitTestCase {
 		self::reset_sync();
 		update_option( $option, 2 );
 
-		$this->assertContains( $option, Jetpack_Sync_Options::get_to_sync() );
+		$this->assertContains( $option, array_keys( Jetpack_Sync_Options::get_to_sync() ) );
 	}
 
 	public function test_sync_delete_option() {
@@ -56,30 +56,10 @@ class WP_Test_Jetpack_Sync_Options extends WP_UnitTestCase {
 		$new_blogname = 'updated first option';
 
 		update_option( $first_option, $new_blogname );
-		$data_to_sync = Jetpack_Sync_Options::get_all();
+		$data_to_sync = Jetpack_Sync_Options::get_to_sync();
 
 		$this->assertContains( $new_blogname, $data_to_sync );
 	}
-
-	public function test_sync_first_option_sometimes() {
-		$first_option = Jetpack_Sync_Options::$options[0];
-		$new_blogname = get_option( $first_option );
-
-		$data_to_sync = Jetpack_Sync_Options::get_all();
-		$this->assertContains( $new_blogname, $data_to_sync );
-
-		$new_blogname = 'Another update to the first option';
-		update_option( $first_option, $new_blogname );
-
-		$data_to_sync = Jetpack_Sync_Options::get_all();
-		self::reset_sync();
-		$this->assertContains( $new_blogname, $data_to_sync );
-
-		// returns a null since we know nothing has changed
-		$is_null = Jetpack_Sync_Options::get_all();
-		$this->assertNull( $is_null );
-	}
-
 
 	public function test_sync_first_option_all() {
 		$first_option = Jetpack_Sync_Options::$options[0];
@@ -88,7 +68,6 @@ class WP_Test_Jetpack_Sync_Options extends WP_UnitTestCase {
 		$data_to_sync = Jetpack_Sync_Options::get_all();
 		$this->assertContains( $new_blogname, $data_to_sync );
 	}
-
 
 	private function reset_sync() {
 		Jetpack_Sync_Options::$sync   = array();
