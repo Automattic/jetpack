ba4af49ab73e429b0e8ec2448ea964ed61df1bd5
George Stephanis <georgestephanis@automattic.com>
2016-03-16 10:27:45 -0400

    Tidy up the shortcode.php
    Extract it from a class, as it wasn't really needing to be a class.
    Also pretty up some formatting and such.

--- modules/videopress.php
+++ modules/videopress.php
@@ -14,6 +14,11 @@ Jetpack::dns_prefetch( array(
 	'//v0.wordpress.com',
 ) );
 
+/**
+ * We won't have any videos less than sixty pixels wide. That would be silly.
+ */
+define( 'VIDEOPRESS_MIN_WIDTH', 60 );
+
 include_once dirname( __FILE__ ) . '/videopress/utility-functions.php';
 include_once dirname( __FILE__ ) . '/videopress/shortcode.php';
 include_once dirname( __FILE__ ) . '/videopress/videopress.php';
