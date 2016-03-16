42b05074facf71d03deccd9ee98c620ba0b8851a
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-03-16 10:52:42 -0700

    Add should sync function.

--- sync/class.jetpack-sync-functions.php
+++ sync/class.jetpack-sync-functions.php
@@ -5,19 +5,14 @@ class Jetpack_Sync_Functions {
 	static $check_sum_id = 'function_check_sum';
 
 	static function get_all() {
-		$data['wp_version']                   = Jetpack::get_wp_version();
 		$data['wp_max_upload_size']           = wp_max_upload_size();
-		$data['featured_images_enabled']      = Jetpack::featured_images_enabled();
-		$data['updates']                      = Jetpack::get_updates();
-		$data['update_details']               = Jetpack::get_update_details();
 		$data['is_main_network']              = Jetpack::is_multi_network();
 		$data['is_multi_site']                = is_multisite();
 		$data['main_network_site']            = network_site_url();
 		$data['single_user_site']             = Jetpack::is_single_user_site();
-		$data['has_file_system_write_access'] = Jetpack::file_system_write_access();
-		$data['is_version_controlled']        = Jetpack::is_version_controlled();
-		$data['content_width']                = Jetpack::get_content_width();
-
+		$data['has_file_system_write_access'] = self::file_system_write_access();
+		$data['is_version_controlled']        = self::is_version_controlled();
+		$data['modules']                      = self::get_modules();
 		if ( is_multisite() ) {
 			$data['network_name']                        = Jetpack::network_name();
 			$data['network_allow_new_registrations']     = Jetpack::network_allow_new_registrations();
@@ -26,5 +21,60 @@ class Jetpack_Sync_Functions {
 			$data['network_upload_file_types']           = Jetpack::network_upload_file_types();
 			$data['network_enable_administration_menus'] = Jetpack::network_enable_administration_menus();
 		}
+		return $data;
 	}
+
+	static protected function get_modules() {
+		$modules = array();
+		$active_modules = Jetpack::get_active_modules();
+		foreach ( Jetpack::get_available_modules() as $available_module ) {
+			$modules[ $available_module ] = in_array( $available_module, $active_modules );
+		}
+		$modules['vaultpress'] = class_exists( 'VaultPress' ) || function_exists( 'vaultpress_contact_service' );
+		return $modules;
+	}
+
+	/**
+	 * Finds out if a site is using a version control system.
+	 * @return string ( '1' | '0' )
+	 **/
+	public static function is_version_controlled() {
+
+		if ( !class_exists( 'WP_Automatic_Updater' ) ) {
+			require_once( ABSPATH . 'wp-admin/includes/class-wp-upgrader.php' );
+		}
+		$updater = new WP_Automatic_Updater();
+		$is_version_controlled = strval( $updater->is_vcs_checkout( $context = ABSPATH ) );
+		// transients should not be empty
+		if ( empty( $is_version_controlled ) ) {
+			$is_version_controlled = '0';
+		}
+		return $is_version_controlled;
+	}
+
+	/**
+	 * Returns true if the site has file write access false otherwise.
+	 * @return string ( '1' | '0' )
+	 **/
+	public static function file_system_write_access() {
+		if ( ! function_exists( 'get_filesystem_method' ) ) {
+			require_once( ABSPATH . 'wp-admin/includes/file.php' );
+		}
+
+		require_once( ABSPATH . 'wp-admin/includes/template.php' );
+
+		$filesystem_method = get_filesystem_method();
+		if ( $filesystem_method === 'direct' ) {
+			return 1;
+		}
+
+		ob_start();
+		$filesystem_credentials_are_stored = request_filesystem_credentials( self_admin_url() );
+		ob_end_clean();
+		if ( $filesystem_credentials_are_stored ) {
+			return 1;
+		}
+		return 0;
+	}
+
 }
