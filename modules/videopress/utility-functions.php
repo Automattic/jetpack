652528f9bc304273e4b6bbf4fecffadee2bc95bb
George Stephanis <georgestephanis@automattic.com>
2016-03-18 15:13:47 -0400

    Add inline docs for `videopress_get_video_details` filter.
    Props @eliorivero @jeherve for the catch!
    
    Oh, and also pass in `$guid` as a second parameter.

--- modules/videopress/utility-functions.php
+++ modules/videopress/utility-functions.php
@@ -30,7 +30,19 @@ function videopress_get_video_details( $guid ) {
 	$response = wp_remote_get( sprintf( 'https://public-api.wordpress.com/rest/v%1$s%2$s', $version, $endpoint ) );
 	$data     = json_decode( wp_remote_retrieve_body( $response ) );
 
-	return apply_filters( 'videopress_get_video_details', $data );
+	/**
+	 * Allow functions to modify fetched video details.
+	 *
+	 * This filter allows third-party code to modify the return data
+	 * about a given video.  It may involve swapping some data out or
+	 * adding new parameters.
+	 *
+	 * @since 3.10
+	 *
+	 * @param object $data The data returned by the WPCOM API. See: https://developer.wordpress.com/docs/api/1.1/get/videos/%24guid/
+	 * @param string $guid The GUID of the VideoPress video in question.
+	 */
+	return apply_filters( 'videopress_get_video_details', $data, $guid );
 }
 
 
