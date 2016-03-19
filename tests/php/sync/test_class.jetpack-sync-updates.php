f7097fbed75e3ee34ada1b46aa71771b7028e895
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-03-18 17:14:50 -0700

    Skip test if we can't connect to wp.org

--- tests/php/sync/test_class.jetpack-sync-updates.php
+++ tests/php/sync/test_class.jetpack-sync-updates.php
@@ -20,41 +20,52 @@ class WP_Test_Jetpack_Sync_Updates extends WP_UnitTestCase {
 	}
 
 	public function ttest_sync_updated_get_all() {
-		$updates_keys = array( 'plugins', 'themes', 'wordpress', 'translations', 'total' );
+		$updates_keys         = array( 'plugins', 'themes', 'wordpress', 'translations', 'total' );
 		$updates_details_keys = array( 'plugins', 'themes', 'wordpress' );
-		$updated_data = Jetpack_Sync_Updates::get_all();
-		foreach( $updates_keys as $key ) {
+		$updated_data         = Jetpack_Sync_Updates::get_all();
+		foreach ( $updates_keys as $key ) {
 			$this->assertArrayHasKey( $key, $updated_data['updates'] );
 		}
 
-		foreach( $updates_details_keys  as $key ) {
+		foreach ( $updates_details_keys as $key ) {
 			$this->assertArrayHasKey( $key, $updated_data['update_details'] );
 		}
 	}
 
 	public function test_sync_update_plugin() {
-		wp_update_plugins();
+		try {
+			wp_update_plugins();
 
-		$updated_data = Jetpack_Sync_Updates::get_to_sync();
-		$this->assertArrayHasKey( 'plugins', $updated_data['update_details'] );
+			$updated_data = Jetpack_Sync_Updates::get_to_sync();
+			$this->assertArrayHasKey( 'plugins', $updated_data['update_details'] );
+		} catch ( Exception $e ) {
+			$this->markTestSkipped( "Can't connect to wordpress.org" );
+		}
 	}
 
 	public function test_sync_update_themes() {
-		wp_update_themes();
+		try {
+			wp_update_themes();
 
-		$updated_data = Jetpack_Sync_Updates::get_to_sync();
-		$this->assertArrayHasKey( 'themes', $updated_data['update_details'] );
+			$updated_data = Jetpack_Sync_Updates::get_to_sync();
+			$this->assertArrayHasKey( 'themes', $updated_data['update_details'] );
+		} catch ( Exception $e ) {
+			$this->markTestSkipped( "Can't connect to wordpress.org" );
+		}
 	}
 
 	public function utest_sync_maybe_update_core() {
-		_maybe_update_core();
-
-		$updated_data = Jetpack_Sync_Updates::get_to_sync();
-		$this->assertArrayHasKey( 'wordpress', $updated_data['update_details'] );
+		try {
+			_maybe_update_core();
+			$updated_data = Jetpack_Sync_Updates::get_to_sync();
+			$this->assertArrayHasKey( 'wordpress', $updated_data['update_details'] );
+		} catch ( Exception $e ) {
+			$this->markTestSkipped( "Can't connect to wordpress.org" );
+		}
 
 	}
 
 	private function reset_sync() {
-		Jetpack_Sync_Updates::$sync   = array();
+		Jetpack_Sync_Updates::$sync = array();
 	}
 }
