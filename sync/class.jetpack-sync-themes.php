69e674477246d8a7761a87588bdac11bc93b4c8d
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-03-21 11:56:31 -0700

    Sync theme data.

--- sync/class.jetpack-sync-themes.php
+++ sync/class.jetpack-sync-themes.php
@@ -3,28 +3,49 @@
 class Jetpack_Sync_Themes {
 
 	static $check_sum_id = 'function_check_sum';
+	static $sync = false;
 
 	static function init() {
-		add_action( 'init', array( __CLASS__, 'sync_theme_data' ) );
+		add_action( 'switch_theme', array( __CLASS__, 'refresh_theme_data' ) );
 	}
 
 	/**
 	 * Triggers a sync of information specific to the current theme.
 	 */
 	static function sync_theme_data() {
-		add_action( 'switch_theme', array( 'Jetpack', 'refresh_theme_data' ) );
-//		$this->sync->mock_option( 'featured_images_enabled', array( 'Jetpack', 'featured_images_enabled' ) );
+
 	}
 
 	static function refresh_theme_data() {
-		/**
-		 * Fires whenever a theme change is made.
-		 *
-		 * @since 3.8.1
-		 *
-		 * @param string featured_images_enabled
-		 * @param boolean Whether featured images are enabled or not
-		 */
-		do_action( 'add_option_jetpack_featured_images_enabled', 'jetpack_featured_images_enabled', Jetpack::featured_images_enabled() );
+		self::$sync = true;
+	}
+
+	static function get_sync() {
+		if ( self::$sync ) {
+			return self::get_all();
+		}
+		return array();
+	}
+
+	static function get_all() {
+		return array(
+			'featured_images_enabled'                          => Jetpack::featured_images_enabled(),
+			'content_width'                                    => Jetpack::get_content_width(),
+			'current_theme_supports_post-thumbnails'           => current_theme_supports( 'post-thumbnails' ),
+			'current_theme_supports_post-formats'              => current_theme_supports( 'post-formats' ),
+			'current_theme_supports_custom-header'             => current_theme_supports( 'custom-header' ),
+			'current_theme_supports_custom-background'         => current_theme_supports( 'custom-background' ),
+			'current_theme_supports_custom-logo'               => current_theme_supports( 'custom-logo' ),
+			'current_theme_supports_menus'                     => current_theme_supports( 'menus' ),
+			'current_theme_supports_automatic-feed-links'      => current_theme_supports( 'automatic-feed-links' ),
+			'current_theme_supports_editor-style'              => current_theme_supports( 'editor-style' ),
+			'current_theme_supports_widgets'                   => current_theme_supports( 'widgets' ),
+			'current_theme_supports_html5'                     => current_theme_supports( 'html5' ),
+			'current_theme_supports_title-tag'                 => current_theme_supports( 'title-tag' ),
+			'current_theme_supports_jetpack-social-menu'       => current_theme_supports( 'jetpack-social-menu' ),
+			'current_theme_supports_jetpack-responsive-videos' => current_theme_supports( 'jetpack-responsive-videos' ),
+			'current_theme_supports_infinite-scroll'           => current_theme_supports( 'infinite-scroll' ),
+			'current_theme_supports_site-logo'                 => current_theme_supports( 'site-logo' ),
+		);
 	}
 }
---
+++ tests/php/sync/test_class.jetpack-sync-themes.php
@@ -0,0 +1,27 @@
+<?php
+require_once dirname( __FILE__ ) . '/../../../sync/class.jetpack-sync-themes.php';
+
+// phpunit --testsuite sync
+class WP_Test_Jetpack_Sync_Themes extends WP_UnitTestCase {
+
+
+
+	public function setUp() {
+		parent::setUp();
+		Jetpack_Sync_Themes::init();
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+
+	}
+
+	public function test_sync_theme_data_after_theme_switch() {
+		$this->assertFalse( Jetpack_Sync_Themes::$sync );
+		$this->assertEmpty(Jetpack_Sync_Themes::get_sync() );
+		switch_theme( 'twentyfourteen' );
+		$this->assertTrue( Jetpack_Sync_Themes::$sync );
+		$this->assertNotEmpty(Jetpack_Sync_Themes::get_sync() );
+
+	}
+}
