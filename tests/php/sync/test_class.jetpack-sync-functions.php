33d232953f609ceeab16191dfd32bb9e26dbf30f
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-03-21 11:58:22 -0700

    Remove data from functions that is already being synced by themes.

--- tests/php/sync/test_class.jetpack-sync-functions.php
+++ tests/php/sync/test_class.jetpack-sync-functions.php
@@ -8,7 +8,7 @@ class WP_Test_Jetpack_Sync_Functions extends WP_UnitTestCase {
 	public function test_sync_all_functions() {
 		$values = Jetpack_Sync_Functions::get_all();
 
-		$this->assertEquals( Jetpack::featured_images_enabled(), $values['featured_images_enabled'] );
+		$this->assertEquals( network_site_url(), $values['main_network_site'] );
 		$this->assertEquals( wp_max_upload_size(), $values['wp_max_upload_size'] );
 	}
 
