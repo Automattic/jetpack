bb07b0037393f8e7ea0b5bbcbc0c8dab343ee6f7
dereksmart <smart@automattic.com>
2016-03-30 17:44:01 -0400

    Update version number as to not conflict with the real Jetpack plugin if both are installed

--- jetpack.php
+++ jetpack.php
@@ -14,7 +14,7 @@
 
 define( 'JETPACK__MINIMUM_WP_VERSION', '4.4' );
 
-define( 'JETPACK__VERSION',            '4.1.0-alpha' );
+define( 'JETPACK__VERSION',            '4.0.0-alpha' );
 define( 'JETPACK_MASTER_USER',         true );
 define( 'JETPACK__API_VERSION',        1 );
 define( 'JETPACK__PLUGIN_DIR',         plugin_dir_path( __FILE__ ) );
