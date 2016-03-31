6e690ee18224212b85d2065b51eca60288861a77
Jeff Golenski <jeffgolenski@gmail.com>
2016-03-31 17:10:08 -0400

    building out masthead component and styling
    building out masthead component and styling

--- _inc/client/components/masthead/index.jsx
+++ _inc/client/components/masthead/index.jsx
@@ -4,7 +4,13 @@ const Masthead = React.createClass( {
 	render: function() {
 		return (
 			<div className='jp-masthead'>
-					<p>testing masthead</p>
+
+				<img src="../../wp-content/plugins/jetpack-react/images/jp-4/jetpack-logo.svg" alt="Jetpack by WordPress.com" />
+
+				<ul>
+					<li><a href="#">Need Help?</a></li>
+					<li><a href="#" id="contextual-help-link">Send us Feedback</a></li>
+				</ul>
 			</div>
 		)
 	}
