22315f1614b5398312283754727181c965adc6df
Jeff Golenski <jeffgolenski@gmail.com>
2016-04-01 16:18:39 -0400

    Fixing some minor mobile styles and cleaning up the code
    Fixing some minor mobile styles and cleaning up the code

--- _inc/client/components/masthead/index.jsx
+++ _inc/client/components/masthead/index.jsx
@@ -4,16 +4,17 @@ var State = require('state/sample-state-tree');
 const Masthead = React.createClass( {
 	render: function() {
 		return (
-			<div className='jp-masthead'>
+			<div className="jp-masthead">
 
-				<div className='jp-logo'>
-					<img src={State.imagePath+"/jetpack-logo.svg"} alt="Jetpack by WordPress.com" width="185" />
+				<div className="jp-logo">
+					<img src={State.imagePath+"/jetpack-logo.svg"} alt="Jetpack by WordPress.com" />
 				</div>
 
 				<ul>
-					<li><a href="#">Need Help?</a></li>
-					<li><a href="#" id="contextual-help-link">Send us Feedback</a></li>
+					<li><a href="http://jetpack.com/support/" target="_blank"><span className="dashicons dashicons-editor-help" title="Need Help?"></span><span>Need Help?</span></a></li>
+					<li><a href="http://surveys.jetpack.me/research-plugin?rel=3.9.4" target="_blank" id="contextual-help-link"><span className="dashicons dashicons-admin-comments" title="Send us Feedback"></span><span>Send us Feedback</span></a></li>
 				</ul>
+
 			</div>
 		)
 	}
