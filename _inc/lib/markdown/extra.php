f999546b0ee3fa7ec67bac50ceeb6c665b06488b
Brandon Hubbard <bkhubbard@gmail.com>
2016-04-09 18:46:13 -0700

    Update DaringFireball links to HTTPS

--- _inc/lib/markdown/extra.php
+++ _inc/lib/markdown/extra.php
@@ -8,7 +8,7 @@
 #
 # Original Markdown
 # Copyright (c) 2004-2006 John Gruber
-# <http://daringfireball.net/projects/markdown/>
+# <https://daringfireball.net/projects/markdown/>
 #
 # Tweaked to remove WordPress interface
 
@@ -3130,7 +3130,7 @@ HTML tags (like <div> and <table> as well).
 
 For more information about Markdown's syntax, see:
 
-<http://daringfireball.net/projects/markdown/>
+<https://daringfireball.net/projects/markdown/>
 
 
 Bugs
@@ -3160,7 +3160,7 @@ All rights reserved.
 
 Based on Markdown
 Copyright (c) 2003-2006 John Gruber
-<http://daringfireball.net/>
+<https://daringfireball.net/>
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
