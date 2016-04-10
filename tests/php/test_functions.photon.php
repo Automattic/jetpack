d05ed4dd170de632046e410c12c72a9859e426a3
Brandon Hubbard <bkhubbard@gmail.com>
2016-04-09 20:48:03 -0700

    Fixed broken php unit test

--- tests/php/test_functions.photon.php
+++ tests/php/test_functions.photon.php
@@ -19,7 +19,7 @@ class WP_Test_Jetpack_Photon_Functions extends WP_UnitTestCase {
 	 * @since  3.9.2
 	 */
 	public function test_photonizing_http_image_no_ssl_query_arg() {
-		$url = jetpack_photon_url( 'https://example.com/images/photon.jpg' );
+		$url = jetpack_photon_url( 'http://example.com/images/photon.jpg' );
 		parse_str( parse_url( $url, PHP_URL_QUERY ), $args );
 		$this->assertArrayNotHasKey( 'ssl', $args, 'HTTP image source should not have an ssl query string.' );
 	}
