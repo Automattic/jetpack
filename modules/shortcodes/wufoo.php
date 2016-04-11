431601250bf7e5327a227fea54563446fdee0715
Brandon Hubbard <bkhubbard@gmail.com>
2016-04-11 10:29:36 -0700

    Updated WUFOO link

--- modules/shortcodes/wufoo.php
+++ modules/shortcodes/wufoo.php
@@ -4,8 +4,7 @@ Plugin Name: Wufoo Shortcode Plugin
 Description: Enables shortcode to embed Wufoo forms. Usage: [wufoo username="chriscoyier" formhash="x7w3w3" autoresize="true" height="458" header="show" ssl="true"]
 Author: Chris Coyier / Wufoo, evansolomon
 
-Based on https://wordpress.org/extend/plugins/wufoo-shortcode/
-http://wufoo.com/docs/code-manager/wordpress-shortcode-plugin/
+Based on https://wordpress.org/plugins/wufoo-shortcode/
 */
 
 
