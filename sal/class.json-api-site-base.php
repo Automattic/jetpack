d8099581fc179ee599275f540a533238e94ab76b
Andrew Duthie <andrew.duthie@automattic.com>
2016-04-15 15:29:09 +0000

    REST API: Posts: Search any whitelisted post type for detail request by slug
    By not specifying `post_type`, we were limiting slug search to posts. Page lookup by slug may have worked previously by consequence of our page path lookup, but not if trying to find a page by its slug when the page is nested as a child (where path would include parent's slug too).
    
    This behavior includes any whitelisted post type as a candidate for being included in the results of `get_posts`.
    
    See: https://[private link]
    
    Merges r134338-wpcom.

--- sal/class.json-api-site-base.php
+++ sal/class.json-api-site-base.php
@@ -253,7 +253,11 @@ abstract class SAL_Site {
 			return new WP_Error( 'invalid_post', 'Invalid post', 400 );
 		}
 
-		$posts = get_posts( array( 'name' => $name, 'numberposts' => 1 ) );
+		$posts = get_posts( array(
+			'name' => $name,
+			'numberposts' => 1,
+			'post_type' => $this->_get_whitelisted_post_types(),
+		) );
 
 		if ( ! $posts || ! isset( $posts[0]->ID ) || ! $posts[0]->ID ) {
 			$page = get_page_by_path( $name );
