23fccba62bf2f4b220c304630ad770cb4b2b3b1c
Jeff Golenski <jeffgolenski@gmail.com>
2016-04-19 19:27:13 -0400

    Setting up flex box for proper footer div alignment
    Setting up flex box for proper footer div alignment

--- _inc/client/components/support-card/index.jsx
+++ _inc/client/components/support-card/index.jsx
@@ -8,6 +8,7 @@ import classNames from 'classnames';
  * Internal dependencies
  */
 import Card from 'components/card';
+import imagePath from 'constants';
 
 export default React.createClass( {
 	displayName: 'SupportCard',
@@ -21,9 +22,14 @@ export default React.createClass( {
 		return (
 			<div className={ classes }>
 				<Card className="jp-support-card__happiness">
-					<h4 className="jp-support-card__header">Need help? The Jetpack team is here for you.</h4>
-					<p className="jp-support-card__desc">We offer free, full support to all of our Jetpack users. Our support team is always around to help you. </p>
-					<p className="jp-support-card__desc"><a href="http://jetpack.com/support/" title="Go to Jetpack.com/support" target="_blank">View our support page</a><span>,</span> <a href="https://wordpress.org/support/plugin/jetpack" title="Go to the WordPress.org support forums" target="_blank">check the forums for answers</a><span>, or</span> <a href="https://jetpack.com/contact-support/" title="Contact Jetpack support staff directly" target="_blank">contact us directly</a><span>.</span></p>
+					<div className="jp-support-card__happiness-photo">
+						<img src={ imagePath + 'support-engineer-01.jpg' } alt="Jetpack Happiness Engineer" className="happiness-engineer" />
+					</div>
+					<div className="jp-support-card__happiness-contact">
+						<h4 className="jp-support-card__header">Need help? The Jetpack team is here for you.</h4>
+						<p className="jp-support-card__desc">We offer free, full support to all of our Jetpack users. Our support team is always around to help you. </p>
+						<p className="jp-support-card__desc"><a href="http://jetpack.com/support/" title="Go to Jetpack.com/support" target="_blank">View our support page</a><span>,</span> <a href="https://wordpress.org/support/plugin/jetpack" title="Go to the WordPress.org support forums" target="_blank">check the forums for answers</a><span>, or</span> <a href="https://jetpack.com/contact-support/" title="Contact Jetpack support staff directly" target="_blank">contact us directly</a><span>.</span></p>
+					</div>
 				</Card>
 				<Card className="jp-support-card__social">
 					<p className="jp-support-card__desc">Enjoying Jetpack or have feedback? <a href="https://wordpress.org/support/view/plugin-reviews/jetpack" title="Leave a Jetpack review" target="_blank">Leave us a review</a>, <a href="http://twitter.com/jetpack" title="Follow Jetpack on Twitter" target="_blank">follow us on twitter</a>, or <a href="https://www.facebook.com/jetpackme" title="Like us on facebook" target="_blank">like us on facebook.</a> </p>
