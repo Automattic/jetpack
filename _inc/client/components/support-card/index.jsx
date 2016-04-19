af6283a0e564436143d53420b2f45c8843ab17a3
Jeff Golenski <jeffgolenski@gmail.com>
2016-04-19 13:48:27 -0400

    adding content, structure, and beginning to style
    adding content, structure, and beginning to style

--- _inc/client/components/support-card/index.jsx
+++ _inc/client/components/support-card/index.jsx
@@ -20,12 +20,13 @@ export default React.createClass( {
 
 		return (
 			<div className={ classes }>
-				<div className="jp-support-card__happiness">
-				testing happiness
-				</div>
-				<div className="jp-support-card__social">
-				testing social
-				</div>
+			<Card className="jp-support-card__happiness">
+				<h4>Need help? The Jetpack team is here for you.</h4>
+				<p>We offer free, full support to all of our Jetpack users. Our support team is always around to help you. View our support page, check the forums for answers, or contact us directly.</p>
+			</Card>
+			<Card className="jp-support-card__social">
+				<p>Enjoying Jetpack or have feedback?  Leave us a review, follow us on twitter, or like us on facebook.</p>
+			</Card>
 			</div>
 		);
 	}
