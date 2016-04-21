b2bb1afd82bcc8d07a1f165a8c24326d0913b658
Igor Zinovyev <zinigor@gmail.com>
2016-04-21 16:09:05 +0200

    Jetpack: move jetpack_require_lib_dir outside of the Jetpack class
    Fixes #3697

--- jetpack.php
+++ jetpack.php
@@ -68,6 +68,17 @@ add_action( 'plugins_loaded', array( 'Jetpack', 'load_modules' ), 100 );
 add_filter( 'jetpack_static_url', array( 'Jetpack', 'staticize_subdomain' ) );
 add_filter( 'is_jetpack_site', '__return_true' );
 
+/*
+ * Returns the location of Jetpack's lib directory. This filter is applied
+ * in require_lib().
+ *
+ * @filter require_lib_dir
+ */
+function jetpack_require_lib_dir() {
+	return JETPACK__PLUGIN_DIR . '_inc/lib';
+}
+add_filter( 'jetpack_require_lib_dir', 'jetpack_require_lib_dir' );
+
 /**
  * Add an easy way to photon-ize a URL that is safe to call even if Jetpack isn't active.
  *
