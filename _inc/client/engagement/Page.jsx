d4bdd0385e61ad98ed7b9dde8ea8209632d70c3b
oskosk <oskosk@gmail.com>
2016-04-22 11:02:01 -0300

    Update engagement tab Cards rendering to be based on a loop instead of laying out every single Card on the markup

--- _inc/client/engagement/Page.jsx
+++ _inc/client/engagement/Page.jsx
@@ -14,155 +14,80 @@ import {
 	activateModule,
 	deactivateModule,
 	isActivatingModule,
-	isDeactivatingModule
+	isDeactivatingModule,
+	getModule as _getModule
 } from 'state/modules';
 
-export const Page = ( { toggleModule, isModuleActivated, isTogglingModule } ) => (
-	<div>
+export const Page = ( { toggleModule, isModuleActivated, isTogglingModule, getModule } ) => {
+	/**
+	 * Array of modules that directly map to a card for rendering
+	 * @type {Array}
+	 */
+	let cards = [
+		[ 'stats', getModule( 'stats' ).name, getModule( 'stats' ).description ],
+		[ 'sharedaddy', getModule( 'sharedaddy' ).name, getModule( 'sharedaddy' ).description ],
+		[ 'likes', getModule( 'likes' ).name, getModule( 'likes' ).description ],
+		[ 'enhanced-distribution', getModule( 'enhanced-distribution' ).name, getModule( 'enhanced-distribution' ).description ],
+		[ 'related-posts', getModule( 'related-posts' ).name, getModule( 'related-posts' ).description ],
+		[ 'publicize', getModule( 'publicize' ).name, getModule( 'publicize' ).description ],
+		[ 'verification-tools', getModule( 'verification-tools' ).name, getModule( 'verification-tools' ).description ],
+		[ 'subscriptions', getModule( 'subscriptions' ).name, getModule( 'subscriptions' ).description ],
+		[ 'comments', getModule( 'comments' ).name, getModule( 'comments' ).description ],
+		[ 'notes', getModule( 'notes' ).name, getModule( 'notes' ).description ]
+	].map( ( element ) => (
 		<FoldableCard
-			header="Site Stats"
-			subheader="Manage how your statistics are displayed"
+			header={ element[1] }
+			subheader={ element[2] }
 			summary={
-				<FormToggle checked={ isModuleActivated( 'stats' ) }
-					toggling={ isTogglingModule( 'stats' ) }
-					onChange={ toggleModule.bind( null, 'stats', isModuleActivated( 'stats' ) ) } />
+				<FormToggle checked={ isModuleActivated( element[0] ) }
+					toggling={ isTogglingModule( element[0] ) }
+					onChange={ toggleModule.bind( null, element[0], isModuleActivated( element[0] ) ) } />
 			}
 			expandedSummary={
-				<FormToggle checked={ isModuleActivated( 'stats' ) }
-					toggling={ isTogglingModule( 'stats' ) }
-					onChange={ toggleModule.bind( null, 'stats', isModuleActivated( 'stats' ) ) } />
+				<FormToggle checked={ isModuleActivated( element[0] ) }
+					toggling={ isTogglingModule( element[0] ) }
+					onChange={ toggleModule.bind( null, element[0], isModuleActivated( element[0] ) ) } />
 			} >
-				settings
+				{ isModuleActivated( element[0] ) ? renderSettings( getModule( element[0] ) ) :
+					// Render the long_description if module is deactivated
+					<div dangerouslySetInnerHTML={ renderLongDescription( getModule( element[0] ) ) } />}
 		</FoldableCard>
-		<FoldableCard
-			header="Sharing & Likes"
-			subheader="Display social sharing & a like button on your site."
-			summary={
-				<FormToggle checked={ isModuleActivated( 'sharedaddy' ) }
-					toggling={ isTogglingModule( 'sharedaddy' ) }
-					onChange={ toggleModule.bind( null, 'sharedaddy', isModuleActivated( 'sharedaddy' ) ) } />
-			}
-			expandedSummary={
-				<FormToggle checked={ isModuleActivated( 'sharedaddy' ) }
-					toggling={ isTogglingModule( 'sharedaddy' ) }
-					onChange={ toggleModule.bind( null, 'sharedaddy', isModuleActivated( 'sharedaddy' ) ) } />
-			} >
-				settings
-		</FoldableCard>
-		<FoldableCard
-			header="Enhanced Distribution"
-			subheader="Instantly share your content with search engines and more."
-			summary={
-				<FormToggle checked={ isModuleActivated( 'enhanced-distribution' ) }
-					toggling={ isTogglingModule( 'enhanced-distribution' ) }
-					onChange={ toggleModule.bind( null, 'enhanced-distribution', isModuleActivated( 'enhanced-distribution' ) ) } />
-			}
-			expandedSummary={
-				<FormToggle checked={ isModuleActivated( 'enhanced-distribution' ) }
-					toggling={ isTogglingModule( 'enhanced-distribution' ) }
-					onChange={ toggleModule.bind( null, 'enhanced-distribution', isModuleActivated( 'enhanced-distribution' ) ) } />
-			} >
-				settings
-		</FoldableCard>
-		<FoldableCard
-			header="Related Posts"
-			subheader="Display your related posts underneath each post."
-			summary={
-				<FormToggle checked={ isModuleActivated( 'related-posts' ) }
-					toggling={ isTogglingModule( 'related-posts' ) }
-					onChange={ toggleModule.bind( null, 'related-posts', isModuleActivated( 'related-posts' ) ) } />
-			}
-			expandedSummary={
-				<FormToggle checked={ isModuleActivated( 'related-posts' ) }
-					toggling={ isTogglingModule( 'related-posts' ) }
-					onChange={ toggleModule.bind( null, 'related-posts', isModuleActivated( 'related-posts' ) ) } />
-			} >
-				settings
-		</FoldableCard>
-		<FoldableCard
-			header="Publicize"
-			subheader="Automatically share content on your social media accounts."
-			summary={
-				<FormToggle checked={ isModuleActivated( 'publicize' ) }
-					toggling={ isTogglingModule( 'publicize' ) }
-					onChange={ toggleModule.bind( null, 'publicize', isModuleActivated( 'publicize' ) ) } />
-			}
-			expandedSummary={
-				<FormToggle checked={ isModuleActivated( 'publicize' ) }
-					toggling={ isTogglingModule( 'publicize' ) }
-					onChange={ toggleModule.bind( null, 'publicize', isModuleActivated( 'publicize' ) ) } />
-			} >
-				settings
-		</FoldableCard>
-		<FoldableCard
-			header="Site Verification"
-			subheader="Verify your site with Google, Bing, and more."
-			summary={
-				<FormToggle checked={ isModuleActivated( 'verification-tools' ) }
-					toggling={ isTogglingModule( 'verification-tools' ) }
-					onChange={ toggleModule.bind( null, 'verification-tools', isModuleActivated( 'verification-tools' ) ) } />
-			}
-			expandedSummary={
-				<FormToggle checked={ isModuleActivated( 'verification-tools' ) }
-					toggling={ isTogglingModule( 'verification-tools' ) }
-					onChange={ toggleModule.bind( null, 'verification-tools', isModuleActivated( 'verification-tools' ) ) } />
-			} >
-				settings
-		</FoldableCard>
-		<FoldableCard
-			header="Subscriptions"
-			subheader="Allow users to subscribe to your content."
-			summary={
-				<FormToggle checked={ isModuleActivated( 'subscriptions' ) }
-					toggling={ isTogglingModule( 'subscriptions' ) }
-					onChange={ toggleModule.bind( null, 'subscriptions', isModuleActivated( 'subscriptions' ) ) } />
-			}
-			expandedSummary={
-				<FormToggle checked={ isModuleActivated( 'subscriptions' ) }
-					toggling={ isTogglingModule( 'subscriptions' ) }
-					onChange={ toggleModule.bind( null, 'subscriptions', isModuleActivated( 'subscriptions' ) ) } />
-			} >
-				settings
-		</FoldableCard>
-		<FoldableCard
-			header="Comments"
-			subheader="Allow users to comment with Facebook, Twitter, or WordPress.com."
-			summary={
-				<FormToggle checked={ isModuleActivated( 'comments' ) }
-					toggling={ isTogglingModule( 'comments' ) }
-					onChange={ toggleModule.bind( null, 'comments', isModuleActivated( 'comments' ) ) } />
-			}
-			expandedSummary={
-				<FormToggle checked={ isModuleActivated( 'comments' ) }
-					toggling={ isTogglingModule( 'comments' ) }
-					onChange={ toggleModule.bind( null, 'comments', isModuleActivated( 'comments' ) ) } />
-			} >
-				settings
-		</FoldableCard>
-		<FoldableCard
-			header="Notifications"
-			subheader="Receive notifications of activity on your site on your mobile device."
-			summary={
-				<FormToggle checked={ isModuleActivated( 'minileven' ) }
-					toggling={ isTogglingModule( 'minileven' ) }
-					onChange={ toggleModule.bind( null, 'minileven', isModuleActivated( 'minileven' ) ) } />
-			}
-			expandedSummary={
-				<FormToggle checked={ isModuleActivated( 'minileven' ) }
-					toggling={ isTogglingModule( 'minileven' ) }
-					onChange={ toggleModule.bind( null, 'minileven', isModuleActivated( 'minileven' ) ) } />
-			} >
-				settings
-		</FoldableCard>
-	</div>
-)
+	) );
+	return (
+		<div>
+			{ cards }
+		</div>
+	);
+}
+
+function renderLongDescription( module ) {
+	// Rationale behind returning an object and not just the string
+	// https://facebook.github.io/react/tips/dangerously-set-inner-html.html
+	return { __html: module.long_description };
+}
+
+function renderSettings( module ) {
+	switch ( module.name ) {
+		case 'stats':
+			return renderStatsSettings( module )
+		default:
+			return ( <div>Settings</div> );
+	}
+}
+
+function renderStatsSettings( module ) {
+	return (
+		<div>Stats Settings</div>
+);
+}
 
 export default connect(
 	( state ) => {
 		return {
 			isModuleActivated: ( module_name ) => _isModuleActivated( state, module_name ),
 			isTogglingModule: ( module_name ) =>
-				isActivatingModule( state, module_name ) || isDeactivatingModule( state, module_name )
+				isActivatingModule( state, module_name ) || isDeactivatingModule( state, module_name ),
+			getModule: ( module_name ) => _getModule( state, module_name )
 		};
 	},
 	( dispatch ) => {
