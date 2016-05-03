a41ccab1f008d66ff4315b26154f3b9d43ff2dcb
Jeff Golenski <jeffgolenski@gmail.com>
2016-05-03 17:19:08 -0400

    Connection Landing: Creating connect CTA
    Connection Landing: Creating & styling connect CTA

--- _inc/client/components/jetpack-connect/index.jsx
+++ _inc/client/components/jetpack-connect/index.jsx
@@ -3,6 +3,8 @@
  */
 import React from 'react';
 import { connect } from 'react-redux';
+import Button from 'components/button';
+import Card from 'components/card';
 
 /**
  * Internal dependencies
@@ -11,7 +13,19 @@ import { getConnectUrl } from 'state/initial-state';
 
 const JetpackConnect = React.createClass( {
 	render: function() {
-		return <a href={ getConnectUrl( this.props ) }>click to connect</a>;
+		return (
+			<div className="jp-connection__container">
+
+				<h2 className="jp-connection__container-title">Please Connect Jetpack</h2>
+
+				<Card className="jp-connection__cta">
+					<p className="jp-connection__description">Please connect to or create a WordPress.com account to enable Jetpack, including powerful security, traffic, and customization services.</p>
+					<Button className="is-primary jp-connection__button" onClick={ getConnectUrl( this.props ) } >Connect Jetpack</Button>
+					<p><a href="#" className="jp-connection__link">No WordPress.com account? Create one for free.</a></p>
+				</Card>
+
+			</div>
+		);
 	}
 } );
 
