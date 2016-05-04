3b28e2666c10d52af909095bd9633185743f7f66
Jeff Golenski <jeffgolenski@gmail.com>
2016-05-04 14:48:16 -0400

    Connection Landing: revising srcSet, adding revised content, and sectionHeader
    Connection Landing: revising srcSet, adding revised content, and
    sectionHeader

--- _inc/client/components/jetpack-connect/index.jsx
+++ _inc/client/components/jetpack-connect/index.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Button from 'components/button';
 import Card from 'components/card';
+import SectionHeader from 'components/section-header';
 
 /**
  * Internal dependencies
@@ -28,6 +29,8 @@ const JetpackConnect = React.createClass( {
 
 					<header className="jp-connection__header">
 						<h2 className="jp-connection__container-subtitle" title="Drive more traffic to your site with Jetpack">Drive more traffic to your site</h2>
+						<p className="jp-connection__feature-description">Jetpack has many traffic and engagement tools to help you get more viewers 
+to your site and keep them there.</p>
 
 						<div className="jp-connection__header-img-container">
 							<img src={ imagePath + "long-clouds.svg" } width="1160" height="63" alt="Decoration: Jetpack clouds" className="jp-connection__header-img" /> {/* defining width and height for IE here */}
@@ -38,30 +41,37 @@ const JetpackConnect = React.createClass( {
 					<div className="jp-connection__interior-container">
 						<div className="jp-connection__feature-list">
 							<div className="jp-connection__feature-list-column">
-								<h3 title="Automated social marketing">Automated social marketing</h3>
-								<p>Use Publicize to automatically share your posts with friends, followers, and the world.</p>
+								<h3 title="Jetpack's Publicize feature" className="dops-section-header__label">Publicize</h3>
+								<div className="jp-connection__feature-content">
+									<h4 className="jp-connection__feature-content-title" title="Automated social marketing">Automated social marketing.</h4>
+									<p>Use Publicize to automatically share your posts with friends, followers, and the world.</p>
+								</div>
 							</div>
 							<div className="jp-connection__feature-list-column">
-								<h3 title="Build a community">Build a community</h3>
-								<p>Give visitors the tools to share and subscribe to your content.</p>
+								<h3 title="Jetpack's Sharing and Like features" className="dops-section-header__label">Sharing &amp; Like Buttons</h3>
+								<div className="jp-connection__feature-content">
+									<h4 className="jp-connection__feature-content-title" title="Build a community">Build a community</h4>
+									<p>Give visitors the tools to share and subscribe to your content.</p>
+								</div>
 							</div>
 							<div className="jp-connection__feature-list-column">
-								<h3 title="Increase page views">Increase page views</h3>
-								<p>Keep visitors engaged by giving them more to share and read with Related Posts.</p>
+								<h3 title="Jetpack's Related Posts feature" className="dops-section-header__label">Related Posts</h3>
+								<div className="jp-connection__feature-content">
+									<h4 className="jp-connection__feature-content-title" title="Increase page views">Increase page views</h4>
+									<p>Keep visitors engaged by giving them more to share and read with Related Posts.</p>
+								</div>
 							</div>
 						</div>
 
 						<h2 className="jp-connection__container-subtitle" title="Track your growth">Track your growth</h2>
+						<p className="jp-connection__feature-description">Jetpack harnesses the power of WordPress.com to show you detailed insights about your visitors, what they’re reading, and where they’re coming from.</p>
 
 						<img src={ imagePath + "stats-example-med.png" } 
-							srcSet={ imagePath + "stats-example-sm.png 500w, " + imagePath + "stats-example-med.png 600w, " + imagePath + "stats-example-lrg.png 900w" }
+							srcSet={ `${imagePath}stats-example-sm.png 500w, ${imagePath}stats-example-med.png 600w, ${imagePath}stats-example-lrg.png 900w` }
 							className="jp-connection__feature-image"  alt="Jetpack statistics and traffic insights graph" />
 
 					</div>
-
-
 				</Card>
-
 				<Card className="jp-connection__feature jp-connection__security">
 
 					<header className="jp-connection__header">
