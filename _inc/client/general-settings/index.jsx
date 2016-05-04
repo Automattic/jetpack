4659fefb53ce1add59de6e36051700a38b7ba3cd
dereksmart <smart@automattic.com>
2016-05-03 21:57:22 -0400

    General Settings: remove toggles from foldable cards

--- _inc/client/general-settings/index.jsx
+++ _inc/client/general-settings/index.jsx
@@ -20,43 +20,37 @@ const GeneralSettings = React.createClass( {
 				<FoldableCard
 					header="Jetpack Add-ons"
 					subheader="Manage your Jetpack account and premium add-ons."
-					summary={ <FormToggle /> }
-					expandedSummary={ <FormToggle /> } >
+				>
 					settings
 				</FoldableCard>
 				<FoldableCard
 					header="Jetpack Connection Settings"
 					subheader="Manage your connected user accounts or disconnect."
-					summary={ <FormToggle /> }
-					expandedSummary={ <FormToggle /> } >
+				>
 					<Button onClick={ this.props.disconnectSite } >Disconnect Site</Button>
 				</FoldableCard>
 				<FoldableCard
 					header="Miscellaneous Settings"
 					subheader="Manage Snow and other fun things for your site."
-					summary={ <FormToggle /> }
-					expandedSummary={ <FormToggle /> } >
+				>
 					settings
 				</FoldableCard>
 				<FoldableCard
 					header="Summary Report Settings"
 					subheader="Manage how Jetpack informs you about your site."
-					summary={ <FormToggle /> }
-					expandedSummary={ <FormToggle /> } >
+				>
 					settings
 				</FoldableCard>
 				<FoldableCard
 					header="Import Jetpack Feature Configuration"
 					subheader="Import your Jetpack setup from another intsallation."
-					summary={ <FormToggle /> }
-					expandedSummary={ <FormToggle /> } >
+				>
 					settings
 				</FoldableCard>
 				<FoldableCard
 					header="Widget Settings"
 					subheader="Configure your WordPress admin dashboard widget."
-					summary={ <FormToggle /> }
-					expandedSummary={ <FormToggle /> } >
+				>
 					settings
 				</FoldableCard>
 
