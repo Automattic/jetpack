8802f6980e0b90336de024c2f8f0268a92899849
Jeff Golenski <jeffgolenski@gmail.com>
2016-05-06 17:55:53 -0400

    Jumpstart: Initial styling of jumpstart
    Jumpstart: Initial styling of jumpstart

--- _inc/client/components/jumpstart/index.jsx
+++ _inc/client/components/jumpstart/index.jsx
@@ -4,6 +4,7 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import Card from 'components/card';
 import Button from 'components/button';
 
 /**
@@ -18,11 +19,14 @@ import {
 const JumpStart = React.createClass( {
 	render: function() {
 		return (
-			<div>
-				<h2>Jump Start?</h2>
-				<Button primary={ true } onClick={ this.props.jumpStartActivate }>Enable Default Features</Button>
-				<br />
-				<Button onClick={ this.props.jumpStartSkip } >Skip</Button>
+			<div className="jp-jumpstart">
+				<h2>Jump Start your site</h2>
+				<Card className="jp-jumpstart__cta">
+				<p className="jp-jumpstart__description">Quickly enhance your site by activating Jetpack's default features.</p>
+					<Button primary={ true } onClick={ this.props.jumpStartActivate }>Enable Default Features</Button>
+					<br />
+					<a onClick={ this.props.jumpStartSkip } >Skip</a>
+				</Card>
 			</div>
 		);
 	}
