129f34dc102e76a36ee104ff096031c3c9d64c19
Dan Walmsley <dan.walmsley@automattic.com>
2016-05-06 20:38:24 +0000

    working version of cached-data-check api

--- json-endpoints/jetpack/class.jetpack-json-api-sync-endpoint.php
+++ json-endpoints/jetpack/class.jetpack-json-api-sync-endpoint.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../../sync/class.jetpack-sync-wp-replicastore.php';
+require_once dirname(__FILE__).'/../../sync/class.jetpack-sync-wp-replicastore.php';
 
 class Jetpack_JSON_API_Sync_Endpoint extends Jetpack_JSON_API_Endpoint {
 	// POST /sites/%s/sync
@@ -17,12 +17,9 @@ class Jetpack_JSON_API_Sync_Endpoint extends Jetpack_JSON_API_Endpoint {
 
 class Jetpack_JSON_API_Sync_Check_Endpoint extends Jetpack_JSON_API_Endpoint {
 	// POST /sites/%s/cached-data-check
+	protected $needed_capabilities = array();
 
-	public function callback( $path = '', $_blog_id = 0 ) {
-		$blog_id = $this->api->switch_to_blog_and_validate_user( $this->api->get_blog_id( $_blog_id ) );
-		if ( is_wp_error( $blog_id ) ) {
-			return $blog_id;
-		}
+	protected function result() {
 
 		Jetpack::init();
 		/** This action is documented in class.jetpack.php */
@@ -43,5 +40,6 @@ class Jetpack_JSON_API_Sync_Check_Endpoint extends Jetpack_JSON_API_Endpoint {
 		$store = new Jetpack_Sync_WP_Replicastore();
 
 		return $store->checksum_all();
+
 	}
 }
