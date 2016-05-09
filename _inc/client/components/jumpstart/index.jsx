a9beee2e7d5ea3b6df84c6f87162ffc67eee782a
Jeff Golenski <jeffgolenski@gmail.com>
2016-05-09 16:21:06 -0400

    JumpStart: Verbiage changes & small screen style changes
    JumpStart: Verbiage changes & small screen style changes

--- _inc/client/components/jumpstart/index.jsx
+++ _inc/client/components/jumpstart/index.jsx
@@ -17,31 +17,35 @@ import {
 } from 'state/jumpstart';
 
 const JumpStart = React.createClass( {
+
+	displayName: 'JumpStart',
+
 	render: function() {
 		return (
 			<div className="jp-jumpstart">
-				<h2 className="jp-jumpstart__title">Jump Start your site</h2>
+				<h2 className="jp-jumpstart__title">Jump Start your Website</h2>
 				<Card className="jp-jumpstart__cta-container">
 					<Card className="jp-jumpstart__cta">
 						<p className="jp-jumpstart__description">Quickly enhance your site by activating Jetpack's recommended features.</p>
 							<Button primary={ true } onClick={ this.props.jumpStartActivate }>Activate Recommended Features</Button>
 					</Card>
-						
 					<Card className="jp-jumpstart__features">
-						<p className="jp-jumpstart__description">What are Jetpack's recommended features?</p>
+						<p className="jp-jumpstart__description">Jetpack's recommended features include:</p>
 
 						<ul className="jp-jumpstart__feature-list">
-							<li>Detailed Site Statistics</li>
-							<li>Automatic Site Security</li>
-							<li>Single Sign On</li>
+							<li>Social Sharing Tools</li>
 							<li>Image Performance (Photon)</li>
+							<li>Single Sign On</li>
+							<li>Contact Form</li>
+							<li>Related Posts</li>
 						</ul>
 						<ul className="jp-jumpstart__feature-list">
 							<li>Automatic Updates (Site Manangement)</li>
-							<li>Anti-spam (Akismet)</li>
-							<li>Downtime Monitoring</li>
-							<li>Related Posts</li>
+							<li>Image Carousel</li>
+							<li>Gravatar Hovercards</li>
+							<li>Visitor Subscriptions</li>
 						</ul>
+						<p className="jp-jumpstart__note">Features can be activated or deactivated at any time.</p>
 					</Card>
 				</Card>
 				<a onClick={ this.props.jumpStartSkip } className="jp-jumpstart__skip-step" title="Skip the Jetpack Jumpstart process">Skip this step</a>
