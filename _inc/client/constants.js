4fa85d61f457e2ecdd71267793d92f5d5a464723
Jeff Golenski <jeffgolenski@gmail.com>
2016-05-12 13:24:17 -0400

    imagePath: Removing duplicate comments

--- _inc/client/constants.js
+++ _inc/client/constants.js
@@ -1,3 +1,1 @@
-export const _imagePath = '/wp-content/plugins/jetpack-react/images/jp-4/'; // temporarily storing jp 4 images within jp-4 subdirectory, change before going live
-export const imagePath = window.Initial_State.pluginBaseUrl + '/images/jp-4/'; // temporarily storing jp 4 images within jp-4 subdirectory, change before going live
-console.log(_imagePath, imagePath);
+export const imagePath = window.Initial_State.pluginBaseUrl + '/images/jp-4/';
