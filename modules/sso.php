62262b6392379648a48bf0cd34638694d206f4c1
Eric Binnion <ericbinnion@gmail.com>
2016-05-17 09:32:28 -0500

    SSO: Store WPOM user gravatar and display name on logout

--- modules/sso.php
+++ modules/sso.php
@@ -21,15 +21,18 @@ class Jetpack_SSO {
 
 		self::$instance = $this;
 
-		add_action( 'admin_init',  array( $this, 'maybe_authorize_user_after_sso' ), 1 );
-		add_action( 'admin_init',  array( $this, 'admin_init' ) );
-		add_action( 'admin_init',  array( $this, 'register_settings' ) );
-		add_action( 'login_init',  array( $this, 'login_init' ) );
-		add_action( 'delete_user', array( $this, 'delete_connection_for_user' ) );
+		add_action( 'admin_init',             array( $this, 'maybe_authorize_user_after_sso' ), 1 );
+		add_action( 'admin_init',             array( $this, 'admin_init' ) );
+		add_action( 'admin_init',             array( $this, 'register_settings' ) );
+		add_action( 'login_init',             array( $this, 'login_init' ) );
+		add_action( 'delete_user',            array( $this, 'delete_connection_for_user' ) );
 		add_filter( 'jetpack_xmlrpc_methods', array( $this, 'xmlrpc_methods' ) );
-		add_action( 'init', array( $this, 'maybe_logout_user' ), 5 );
+		add_action( 'init',                   array( $this, 'maybe_logout_user' ), 5 );
 		add_action( 'jetpack_modules_loaded', array( $this, 'module_configure_button' ) );
-		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
+		add_action( 'admin_enqueue_scripts',  array( $this, 'admin_enqueue_scripts' ) );
+		add_action( 'login_form_logout',      array( $this, 'store_wpcom_profile_cookies_on_logout' ) );
+		add_action( 'wp_login',               array( 'Jetpack_SSO', 'clear_wpcom_profile_cookies' ) );
+
 
 		// Adding this action so that on login_init, the action won't be sanitized out of the $action global.
 		add_action( 'login_form_jetpack-sso', '__return_true' );
@@ -770,26 +773,6 @@ class Jetpack_SSO {
 			// Cache the user's details, so we can present it back to them on their user screen
 			update_user_meta( $user->ID, 'wpcom_user_data', $user_data );
 
-			// Cache user's display name and Gravatar so it can be displayed on the login screen
-			setcookie(
-				'jetpack_sso_wpcom_name' . COOKIEHASH,
-				$user_data->display_name,
-				time() + YEAR_IN_SECONDS,
-				COOKIEPATH,
-				COOKIE_DOMAIN
-			);
-
-			setcookie(
-				'jetpack_sso_wpcom_gravatar' . COOKIEHASH,
-				get_avatar_url(
-					$user_data->email,
-					array( 'size' => 72, 'default' => 'mystery' )
-				),
-				time() + YEAR_IN_SECONDS,
-				COOKIEPATH,
-				COOKIE_DOMAIN
-			);
-
 			$remember = false;
 			if ( ! empty( $_COOKIE['jetpack_sso_remember_me'] ) ) {
 				$remember = true;
@@ -1159,6 +1142,40 @@ class Jetpack_SSO {
 		add_filter( 'allowed_redirect_hosts', array( $this, 'allowed_redirect_hosts' ) );
 		wp_safe_redirect( $connect_url );
 		exit;
+	}
+
+	/**
+	 * Cache user's display name and Gravatar so it can be displayed on the login screen. These cookies are
+	 * stored when the user logs out, and then deleted when the user logs in.
+	 */
+	function store_wpcom_profile_cookies_on_logout() {
+		if ( ! Jetpack::is_user_connected( get_current_user_id() ) ) {
+			return;
+		}
+
+		$user_data = $this->get_user_data( get_current_user_id() );
+		if ( ! $user_data ) {
+			return;
+		}
+
+		setcookie(
+			'jetpack_sso_wpcom_name' . COOKIEHASH,
+			$user_data->display_name,
+			time() + WEEK_IN_SECONDS,
+			COOKIEPATH,
+			COOKIE_DOMAIN
+		);
+
+		setcookie(
+			'jetpack_sso_wpcom_gravatar' . COOKIEHASH,
+			get_avatar_url(
+				$user_data->email,
+				array( 'size' => 72, 'default' => 'mystery' )
+			),
+			time() + WEEK_IN_SECONDS,
+			COOKIEPATH,
+			COOKIE_DOMAIN
+		);
 	}
 
 	/**
