c198677658f49eee76887b0bd97603902fd6ce27
Daniel Walmsley <goldsounds@gmail.com>
2016-05-20 10:36:55 -0700

    Add filter which can prevent publicizing
    Fixes #7

--- tests/php/modules/publicize/test_class.publicize.php
+++ tests/php/modules/publicize/test_class.publicize.php
@@ -5,6 +5,7 @@ class WP_Test_Publicize extends WP_UnitTestCase {
 
 	private $fired_publicized_post = false;
 	private $publicized_post = null;
+	private $post;
 
 	public function setUp() {
 		parent::setUp();
@@ -12,38 +13,56 @@ class WP_Test_Publicize extends WP_UnitTestCase {
 		$this->publicize = publicize_init();
 		$this->fired_publicized_post = false;
 		$this->publicized_post = null;
+
+		$post_id = $this->factory->post->create( array( 'post_status' => 'draft' ) );
+		$this->post = get_post( $post_id );
+
+		add_action( 'jetpack_publicize_post', array( $this, 'publicized_post' ), 10, 1 );
 	}
 
 	public function test_fires_jetpack_publicize_post_on_save_as_published() {
-		$post_id = $this->factory->post->create( array( 'post_status' => 'draft' ) );
-		$post = get_post( $post_id );
+		$this->post->post_status = 'publish';
 
-		add_action( 'jetpack_publicize_post', array( $this, 'publicized_post' ), 10, 1 );
+		wp_insert_post( $this->post->to_array() );
 
-		$post->post_status = 'publish';
-
-		wp_insert_post( $post->to_array() );
-
-		$this->assertTrue( $this->fired_publicized_post );
-		$this->assertEquals( $post->ID, $this->publicized_post->ID );
+		$this->assertPublicized( true, $this->post );
 	}
 
 	public function test_does_not_fire_jetpack_publicize_post_on_other_status_transitions() {
-		$post_id = $this->factory->post->create( array( 'post_status' => 'publish' ) );
-		$post = get_post( $post_id );
+		$this->post->post_status = 'pending';
+
+		wp_insert_post( $this->post->to_array() );
+
+		$this->assertPublicized( false, $this->post );
+	}
+
+	public function test_filter_can_prevent_publicize() {
+		add_filter( 'publicize_should_publicize_published_post', array( $this, 'prevent_publicize_post' ), 10, 2 );
+
+		$this->post->post_status = 'publish';
+
+		wp_insert_post( $this->post->to_array() );
+
+		$this->assertPublicized( false, $this->post );
+	}
+
+	function assertPublicized( $should_have_publicized, $post ) {
+		if ( $should_have_publicized ) {
+			$this->assertTrue( $this->fired_publicized_post );
+			$this->assertEquals( $post->ID, $this->publicized_post->ID );	
+		} else {
+			$this->assertFalse( $this->fired_publicized_post );
+			$this->assertNull( $this->publicized_post );
+		}
 		
-		add_action( 'jetpack_publicize_post', array( $this, 'publicized_post' ), 10, 1 );
-
-		$post->post_status = 'draft';
-
-		wp_insert_post( $post->to_array() );
-
-		$this->assertFalse( $this->fired_publicized_post );
-		$this->assertNull( $this->publicized_post );
 	}
 
 	function publicized_post( $post ) {
 		$this->fired_publicized_post = true;
 		$this->publicized_post = $post;
+	}
+
+	function prevent_publicize_post( $should_publicize, $post ) {
+		return false;
 	}
 }
