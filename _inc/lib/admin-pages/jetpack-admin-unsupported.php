2a1b3e8bfc5ea774f000848af413f1034ff056eb
Igor Zinovyev <zinigor@gmail.com>
2016-05-20 23:12:36 +0300

    General: minor indentation fix and added translation.

--- _inc/lib/admin-pages/jetpack-admin-unsupported.php
+++ _inc/lib/admin-pages/jetpack-admin-unsupported.php
@@ -5,22 +5,23 @@
  * Renders the module list table where you can use bulk action or row
  * actions to activate/deactivate and configure modules
  */
-?>
-<?php /** This action is already documented in views/admin/admin-page.php */
-	do_action( 'jetpack_notices' );
-	include_once( JETPACK__PLUGIN_DIR . 'class.jetpack-modules-list-table.php' );
-	$list_table = new Jetpack_Modules_List_Table;
 
-	if ( wp_version_too_old() ) {
-		echo 'Update WordPress to unlock Jetpack\'s full potential!';
-	}
+/** This action is already documented in views/admin/admin-page.php */
+do_action( 'jetpack_notices' );
 
-	if ( maybe_load_old_jetpack_config_page() ) {
-		return;
-	}
+include_once( JETPACK__PLUGIN_DIR . 'class.jetpack-modules-list-table.php' );
+$list_table = new Jetpack_Modules_List_Table;
+
+if ( wp_version_too_old() ) {
+	echo 'Update WordPress to unlock Jetpack\'s full potential!';
+}
+
+if ( maybe_load_old_jetpack_config_page() ) {
+	return;
+}
 ?>
 	<noscript>
-		<p>Turn on Javascript to unlock Jetpack's full potential!</p>
+		<p><?php esc_html_e( "Turn on JavaScript to unlock Jetpack's full potential!" ) ?></p>
 	</noscript>
 	<div class="page-content configure">
 		<div class="frame bottom">
