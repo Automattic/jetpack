7c369f70b7c69686b3417fc7a02e789f4a08c7b7
Eric Binnion <ericbinnion@gmail.com>
2016-05-22 15:44:37 -0500

    Admin Page: Prepare connect button for internationalization

--- _inc/client/components/connect-button/index.jsx
+++ _inc/client/components/connect-button/index.jsx
@@ -4,6 +4,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Button from 'components/button';
+import { translate as __ } from 'lib/mixins/i18n';
 
 /**
  * Internal dependencies
@@ -20,6 +21,7 @@ import {
 } from 'state/connection';
 import QueryConnectionStatus from 'components/data/query-connection-status';
 import QueryConnectUrl from 'components/data/query-connect-url';
+
 
 const ConnectButton = React.createClass( {
 	displayName: 'ConnectButton',
@@ -44,8 +46,9 @@ const ConnectButton = React.createClass( {
 				<div>
 					<Button
 						onClick={ this.props.unlinkUser }
-						disabled={ isUnlinking }
-					>Unlink me from WordPress.com</Button>
+						disabled={ isUnlinking } >
+						{ __( 'Unlink me from WordPress.com' ) }
+					</Button>
 				</div>
 			);
 		}
@@ -54,9 +57,8 @@ const ConnectButton = React.createClass( {
 			<Button
 				className="is-primary jp-jetpack-connect__button"
 				href={ this.props.connectUrl( this.props ) }
-				disabled={ fetchingUrl }
-			>
-				Link to WordPress.com
+				disabled={ fetchingUrl } >
+				{ __( 'Link to WordPress.com' ) }
 			</Button>
 		);
 	},
@@ -73,9 +75,8 @@ const ConnectButton = React.createClass( {
 			return(
 				<Button
 					onClick={ this.props.disconnectSite }
-					disabled={ disconnecting }
-				>
-					Disconnect site from WordPress.com
+					disabled={ disconnecting }>
+					{ __( 'Disconnect site from WordPress.com ') }
 				</Button>
 			);
 		}
@@ -84,9 +85,8 @@ const ConnectButton = React.createClass( {
 			<Button
 				className="is-primary jp-jetpack-connect__button"
 				href={ this.props.connectUrl( this.props ) }
-				disabled={ fetchingUrl }
-			>
-				Connect to WordPress.com
+				disabled={ fetchingUrl }>
+				{ __( 'Connect to WordPress.com' ) }
 			</Button>
 		);
 	},
