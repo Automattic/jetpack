b5f2812322797be61dbeb226d689beb9f220972f
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-05-23 14:03:20 -0700

    Add tests that and implement jetpack_sync::sync_options() method for backwards compatibility with ValutPress
    Updated the tests

--- tests/php/sync/test_class.jetpack-sync-options.php
+++ tests/php/sync/test_class.jetpack-sync-options.php
@@ -65,6 +65,19 @@ class WP_Test_Jetpack_New_Sync_Options extends WP_Test_Jetpack_New_Sync_Base {
 		}
 	}
 
+
+	public function test_backwards_compatibility_sync_options() {
+		Jetpack_Sync::sync_options( __FILE__, 'foo_option', 'bar_option' );
+
+		update_option( 'foo_option', '123' );
+		update_option( 'bar_option', '456' );
+		$this->client->do_sync();
+
+		$this->assertEquals( '123', $this->server_replica_storage->get_option( 'foo_option' ) );
+		$this->assertEquals( '456', $this->server_replica_storage->get_option( 'bar_option' ) );
+
+	}
+
 	function _get_site_icon( $url, $size, $blog_id ) {
 		return 'http://foo.com/icon.gif';
 	}
