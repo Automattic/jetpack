e51b194f70455454e52458cc2f7db69605a1552b
dereksmart <smart@automattic.com>
2016-05-23 00:56:48 -0400

    Notices: Refactor to make each notice it's own reusable component.

--- _inc/client/components/jetpack-notices/index.jsx
+++ _inc/client/components/jetpack-notices/index.jsx
@@ -18,12 +18,12 @@ import {
 	dismissJetpackNotice
 } from 'state/jetpack-notices';
 
-const JetpackNotices = React.createClass( {
-	displayName: 'JetpackNotices',
+export const WelcomeNotice = React.createClass( {
+	displayName: 'WelcomeNotice',
 
-	maybeShowWelcomeNotice: function() {
+	render() {
 		if ( this.props.isDismissed( 'welcome' ) ) {
-			return;
+			return false;
 		}
 		return (
 			<div>
@@ -35,8 +35,14 @@ const JetpackNotices = React.createClass( {
 				</SimpleNotice>
 			</div>
 		);
-	},
-	maybeShowDevVersion: function() {
+	}
+
+} );
+
+export const DevVersionNotice = React.createClass( {
+	displayName: 'DevVersionNotice',
+
+	render() {
 		if ( window.Initial_State.isDevVersion ) {
 			const text = __( 'You are currently running a development version of Jetpack. {{a}} Submit your feedback {{/a}}',
 				{
@@ -52,33 +58,16 @@ const JetpackNotices = React.createClass( {
 				</SimpleNotice>
 			);
 		}
-	},
-	maybeShowDevMode: function() {
-		const devMode = window.Initial_State.connectionStatus.devMode;
-		if ( ! devMode.isActive ) {
-			return;
-		}
 
-		let devModeType = '';
-		if ( devMode.filter ) { devModeType += __( 'the jetpack_development_mode filter. ' ); }
-		if ( devMode.constant ) { devModeType += __( 'the JETPACK_DEV_DEBUG constant. ' ); }
-		if ( devMode.url ) { devModeType += __( 'your site URL lacking a dot (e.g. http://localhost).' ); }
+		return false;
+	}
 
-		const text = __( 'Currently in {{a}}Development Mode{{/a}} VIA ' + devModeType,
-			{
-				components: {
-					a: <a href="https://jetpack.com/support/development-mode/" target="_blank" />
-				}
-			}
-		);
+} );
 
-		return (
-			<SimpleNotice showDismiss={ false }>
-				{ text }
-			</SimpleNotice>
-		);
-	},
-	maybeShowStagingSite: function() {
+export const StagingSiteNotice = React.createClass( {
+	displayName: 'StagingSiteNotice',
+
+	render() {
 		if ( window.Initial_State.connectionStatus.isStaging ) {
 			const text = __( 'You are running Jetpack on a {{a}}staging server{{/a}}.',
 				{
@@ -94,9 +83,64 @@ const JetpackNotices = React.createClass( {
 				</SimpleNotice>
 			);
 		}
-	},
 
-	actionNotices: function() {
+		return false;
+	}
+
+} );
+
+export const DevModeNotice = React.createClass( {
+	displayName: 'DevModeNotice',
+
+	render() {
+		const devMode = window.Initial_State.connectionStatus.devMode;
+		if ( devMode.isActive ) {
+			let devModeType = '';
+			if (devMode.filter) {
+				devModeType += __('the jetpack_development_mode filter. ');
+			}
+			if (devMode.constant) {
+				devModeType += __('the JETPACK_DEV_DEBUG constant. ');
+			}
+			if (devMode.url) {
+				devModeType += __('your site URL lacking a dot (e.g. http://localhost).');
+			}
+
+			const text = __('Currently in {{a}}Development Mode{{/a}} VIA ' + devModeType,
+				{
+					components: {
+						a: <a href="https://jetpack.com/support/development-mode/" target="_blank"/>
+					}
+				}
+			);
+
+			return (
+				<SimpleNotice showDismiss={ false }>
+					{ text }
+				</SimpleNotice>
+			);
+		}
+
+		return false;
+	}
+
+} );
+
+
+/**
+ * These notices are triggered by actions in the app, like:
+ * - Disconnecting
+ * - Connecting
+ * - Unlink
+ * - Activate Module, etc...
+ *
+ * It will probably break off into it's own thing when it gets bigger.
+ * It listens to the notice actions via this.props.jetpackNotices( this.props );
+ */
+export const ActionNotices = React.createClass( {
+	displayName: 'ActionNotices',
+
+	render() {
 		const notices = this.props.jetpackNotices( this.props );
 
 		if ( 'disconnected' === notices ) {
@@ -118,16 +162,24 @@ const JetpackNotices = React.createClass( {
 				</div>
 			);
 		}
-	},
+
+		return false;
+	}
+
+} );
+
+
+const JetpackNotices = React.createClass( {
+	displayName: 'JetpackNotices',
 
 	render() {
 		return (
 			<div>
-				{ this.maybeShowWelcomeNotice() }
-				{ this.maybeShowDevVersion() }
-				{ this.maybeShowDevMode() }
-				{ this.maybeShowStagingSite() }
-				{ this.actionNotices() }
+				<WelcomeNotice { ...this.props } />
+				<DevVersionNotice { ...this.props } />
+				<DevModeNotice { ...this.props } />
+				<StagingSiteNotice { ...this.props } />
+				<ActionNotices { ...this.props } />
 			</div>
 		);
 	}
