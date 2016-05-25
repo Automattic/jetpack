8d5c7f2445e99216ef1449fa7a6e0054343a2bd3
Brandon Kraft <public@brandonkraft.com>
2016-05-25 17:12:37 -0500

    Unit Tests: Remove mention of Travis' phpunit test. No longer in use.

--- gulpfile.js
+++ gulpfile.js
@@ -367,5 +367,4 @@ gulp.task( 'old-styles',   ['frontendcss', 'admincss', 'admincss:rtl', 'old-sass
 gulp.task( 'languages',    ['languages:get', 'languages:build', 'languages:cleanup'] );
 
 // travis CI tasks.
-gulp.task( 'travis:phpunit', ['php:unit'] );
 gulp.task( 'travis:js', ['js:hint', 'js:qunit'] );
