b09d136dfa82bf438af1e68f5c9a222fd89ef3c8
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-05-27 10:20:46 -0700

    Remove jetpack-sync options

--- tests/php/sync/test_class.jetpack_sync_backward_compatibility.php
+++ tests/php/sync/test_class.jetpack_sync_backward_compatibility.php
@@ -13,14 +13,5 @@ class WP_Test_Jetpack_Sync_Backward_Compatibility extends WP_Test_Jetpack_New_Sy
 		require_once( JETPACK__PLUGIN_DIR . '3rd-party/polldaddy.php' );
 		$this->setExpectedDeprecated( 'Jetpack_Sync::sync_options' );
 		Jetpack_Sync::sync_options( __FILE__, 'foo_option', 'bar_option' );
-
-		update_option( 'foo_option', '123' );
-		update_option( 'bar_option', '456' );
-
-		$this->client->do_sync();
-
-		$this->assertEquals( '123', $this->server_replica_storage->get_option( 'foo_option' ) );
-		$this->assertEquals( '456', $this->server_replica_storage->get_option( 'bar_option' ) );
 	}
-
 }
