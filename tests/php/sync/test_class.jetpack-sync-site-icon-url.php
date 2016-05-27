615d4ea42a551b1622101a35aa68683ddfc6a93b
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-05-27 12:21:28 -0700

    Remove the restriction on delete_option_site_icon action
    Since the site_icon gets set to 0 instead of deleteing it. We want to
    remove the site icon_url when ever we have a empty site_icon_url.

--- tests/php/sync/test_class.jetpack-sync-site-icon-url.php
+++ tests/php/sync/test_class.jetpack-sync-site-icon-url.php
@@ -47,6 +47,24 @@ class WP_Test_Jetpack_Sync_Site_Icon_Url extends WP_Test_Jetpack_New_Sync_Base {
 		$this->assertEquals( Jetpack_Options::get_option( 'site_icon_url' ), $this->server_replica_storage->get_option( 'jetpack_site_icon_url' ) );
 	}
 
+	public function test_site_icon_update_to_null_is_synced_using_jetpack_function() {
+		global $wp_version;
+
+		// verify that we started with an icon.
+		$this->assertEquals( 'http://foo.com/icon.gif', $this->server_replica_storage->get_option( 'jetpack_site_icon_url' ) );
+
+		if ( version_compare( $wp_version, '4.4', '>=' ) ) {
+			remove_filter( 'get_site_icon_url', array( $this, '_get_site_icon' ), 99, 3 );
+			update_option( 'site_icon', 0 );
+		} else {
+			// wp 4.3 or less
+			Jetpack_Options::delete_option( 'site_icon_url' );
+		}
+		$this->client->do_sync();
+		$this->assertEmpty( $this->server_replica_storage->get_option( 'jetpack_site_icon_url' ) );
+		$this->assertEquals( Jetpack_Options::get_option( 'site_icon_url' ), $this->server_replica_storage->get_option( 'jetpack_site_icon_url' ) );
+	}
+
 	function _get_site_icon( $url, $size, $blog_id ) {
 		return 'http://foo.com/icon.gif';
 	}
