3fad9cf48ead2187eaab7dd7ea25bf30797b877c
Jeremy Herve <jeremy@tagada.hu>
2016-05-27 13:29:28 +0200

    Publicize: add missing parameter in filter documentation.
    @see https://github.com/Automattic/jetpack/commit/b2d3b7878a0020cfb12d9fe4fb44b4901e038b0b#commitcomment-17578042

--- modules/publicize/publicize-jetpack.php
+++ modules/publicize/publicize-jetpack.php
@@ -362,18 +362,20 @@ class Publicize extends Publicize_Base {
 		if ( 'publish' == $new_status && 'publish' != $old_status ) {
 			/**
 			 * Determines whether a post being published gets publicized.
+			 *
 			 * Side-note: Possibly our most alliterative filter name.
 			 *
 			 * @module publicize
 			 *
 			 * @since 4.1.0
 			 *
-			 * @param bool should_publicize
+			 * @param bool $should_publicize Should the post be publicized? Default to true.
+			 * @param WP_POST $post Current Post object.
 			 */
 			$should_publicize = apply_filters( 'publicize_should_publicize_published_post', true, $post );
 
 			if ( $should_publicize ) {
-				update_post_meta( $post->ID, $this->PENDING, true );	
+				update_post_meta( $post->ID, $this->PENDING, true );
 			}
 		}
 	}
