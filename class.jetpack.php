2d70d7936ef6703792dfb1253d7a1840040db3e6
roccotripaldi <tripaldir@gmail.com>
2016-05-29 10:18:01 -0400

    Jetpack Connect URL adds home_url, site_url, and site_icon for use in Calypso connection UI.

--- class.jetpack.php
+++ class.jetpack.php
@@ -4343,6 +4343,10 @@ p {
 
 			$secrets = Jetpack::init()->generate_secrets( 'authorize' );
 			@list( $secret ) = explode( ':', $secrets );
+			
+			$site_icon = ( function_exists( 'has_site_icon') && has_site_icon() )
+				? get_site_icon_url()
+				: false;
 
 			$args = urlencode_deep(
 				array(
@@ -4366,6 +4370,9 @@ p {
 					'secret'        => $secret,
 					'locale'        => isset( $gp_locale->slug ) ? $gp_locale->slug : '',
 					'blogname'      => get_option( 'blogname' ),
+					'site_url'      => site_url(),
+					'home_url'      => home_url(),
+					'site_icon'     => $site_icon,
 				)
 			);
 
