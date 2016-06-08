2e5ce221b884f544fb96f2016ba6decd0ccfd83d
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-06-08 14:16:54 -0700

    Add Social Logos to Jetpack in order to update the Sharebuttons

---
+++ _inc/social-logos.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Globally registers the 'social-logos' style and font.
+ *
+ * This ensures any theme or plugin using it is on the latest version of Social Logos, and helps to avoid conflicts.
+ */
+add_action( 'init', 'jetpack_register_social_logos', 1 );
+function jetpack_register_social_logos() {
+	if ( ! wp_style_is( 'social-logos', 'registered' ) ) {
+		wp_register_style( 'social-logos', plugins_url( 'social-logos/social-logos.css', __FILE__ ), false, '1' );
+	}
+}
