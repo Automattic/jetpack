5bea90dac16d9c9045085337003a6cbec455bf37
James Nylen <jnylen@gmail.com>
2016-06-16 17:03:08 +0000

    Whitespace fixes

--- json-endpoints/class.wpcom-json-api-update-post-v1-2-endpoint.php
+++ json-endpoints/class.wpcom-json-api-update-post-v1-2-endpoint.php
@@ -13,7 +13,7 @@ class WPCOM_JSON_API_Update_Post_v1_2_Endpoint extends WPCOM_JSON_API_Update_Pos
 			add_action( 'rest_api_inserted_post', array( $GLOBALS['publicize_ui']->publicize, 'async_publicize_post' ) );
 
 			if ( $this->should_load_theme_functions( $post_id ) ) {
-				$this->load_theme_functions();				
+				$this->load_theme_functions();
 			}
 		}
 
