3ad0b6b9e6fad98b14e154f1d800a505fce16130
Dennis Snell <dennis.snell@automattic.com>
2016-06-21 18:56:31 +0000

    Advanced SEO: Add per-post custom meta description
    Part of T473
    Closes D2049
    
    Adds the ability to save a new metadata field to post objects.
    
    Previously, all descriptions and titles given to search engines were generated
    automatically inside of WordPress. Now, this and a new title metadata field
    will be available to certain users to customize how that information will be
    generated.
    
    Modifying this infomration currently requires a business plan subscription.
    
    Props to @vindl for doing all the work on this patch.
    
    Merges r137590-wpcom.
    
    Conflicts:
    	json-endpoints/class.wpcom-json-api-update-post-endpoint.php

--- json-endpoints/class.wpcom-json-api-update-post-endpoint.php
+++ json-endpoints/class.wpcom-json-api-update-post-endpoint.php
@@ -521,6 +521,11 @@ class WPCOM_JSON_API_Update_Post_Endpoint extends WPCOM_JSON_API_Post_Endpoint {
 				if ( Jetpack_SEO_Posts::DESCRIPTION_META_KEY == $meta->key && ! Jetpack_SEO_Utils::is_enabled_jetpack_seo() ) {
 					return new WP_Error( 'unauthorized', __( 'SEO tools are not enabled for this site.', 'jetpack' ), 403 );
 				}
+				if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
+					if ( 'advanced_seo_description' == $meta->key and ! WPCOM_Store::has_business_plan() ) {
+						return new WP_Error( 'unauthorized', __( 'Advanced SEO can only be set on sites that have a business plan.' ), 403 );
+					}
+				}
 
 				$existing_meta_item = new stdClass;
 
