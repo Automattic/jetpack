91ddcc8df53ebfa59e61b07db90c4442a336becb
Igor Zinovyev <zinigor@gmail.com>
2016-06-22 19:35:01 +0200

    Added whatwg fetch to replace isomorphic fetch variant.
    That resolves a bug that doesn't allow isomorphic fetch to work with server side React rendering.

--- _inc/client/rest-api/index.js
+++ _inc/client/rest-api/index.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 require( 'es6-promise' ).polyfill();
-import fetch from 'isomorphic-fetch';
+import fetch from 'whatwg-fetch';
 
 const restApi = {
 	fetchSiteConnectionStatus: () => fetch( `${ window.Initial_State.WP_API_root }jetpack/v4/connection-status`, {
