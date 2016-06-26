7c77b74633fa0caaed99ccdbb99f5ccceffcbc7e
Jeremy Herve <jeremy@tagada.hu>
2016-06-26 13:15:11 +0000

    VideoPress: do not set constant if it is already set.
    Fixes #4204

--- modules/videopress.php
+++ modules/videopress.php
@@ -13,7 +13,7 @@
 /**
  * We won't have any videos less than sixty pixels wide. That would be silly.
  */
-define( 'VIDEOPRESS_MIN_WIDTH', 60 );
+defined( 'VIDEOPRESS_MIN_WIDTH' ) or define( 'VIDEOPRESS_MIN_WIDTH', 60 );
 
 include_once dirname( __FILE__ ) . '/videopress/utility-functions.php';
 include_once dirname( __FILE__ ) . '/videopress/shortcode.php';
