a1ed3e6587bec8aeab4a075ed734bbdcfbad81d4
Nick Daugherty <ndaugherty987@gmail.com>
2016-07-01 12:13:02 -0600

    Add pre and post filters for jetpack_is_mobile
    Allows the calculated value to be filtered, for custom rules and setups
    
    Fixes #4275

--- class.jetpack-user-agent.php
+++ class.jetpack-user-agent.php
@@ -18,6 +18,23 @@ function jetpack_is_mobile( $kind = 'any', $return_matched_agent = false ) {
 	static $first_run     = true;
 	static $matched_agent = '';
 
+	/**
+	 * Filter the value of jetpack_is_mobile before it is calculated.
+	 *
+	 * Passing a truthy value to the filter will short-circuit determining the
+	 * mobile type, returning the passed value instead.
+	 *
+	 * @param bool|string $matches Boolean if current UA matches $kind or not. If
+	 *                             $return_matched_agent is true, should return the UA string
+	 * @param string $kind Category of mobile device being checked
+	 * @param bool $return_matched_agent Boolean indicating if the UA should be returned
+	 */
+	$pre = apply_filters( 'pre_jetpack_is_mobile', null, $kind, $return_matched_agent );
+
+	if ( null !== $pre ) {
+		return $pre;
+	}
+
 	$ua_info = new Jetpack_User_Agent_Info();
 
 	if ( empty( $_SERVER['HTTP_USER_AGENT'] ) || strpos( strtolower( $_SERVER['HTTP_USER_AGENT'] ), 'ipad' ) ) {
@@ -75,12 +92,24 @@ function jetpack_is_mobile( $kind = 'any', $return_matched_agent = false ) {
 			$kinds['any'] = true;
 		}
 	}
+
+	$value = $kinds[ $kind ];
+
+	if ( $return_matched_agent ) {
+		$value = $matched_agent;
 	}
 
-	if ( $return_matched_agent )
-		return $matched_agent;
+	/**
+	 * Filter the value of jetpack_is_mobile
+	 *
+	 * @param bool|string $matches Boolean if current UA matches $kind or not. If
+	 *                             $return_matched_agent is true, should return the UA string
+	 * @param string $kind Category of mobile device being checked
+	 * @param bool $return_matched_agent Boolean indicating if the UA should be returned
+	 */
+	$value = apply_filters( 'jetpack_is_mobile', $value, $kind, $return_matched_agent );
 
-	return $kinds[$kind];
+	return $value;
 }
 
 class Jetpack_User_Agent_Info {
