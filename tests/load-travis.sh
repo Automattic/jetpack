0a07eadb2d103dbf933f1f8e2e2b72a8f96685cd
Igor Zinovyev <zinigor@gmail.com>
2016-07-08 15:47:38 +0300

    Using nvmrc in order to get around the binaries problem.

--- tests/load-travis.sh
+++ tests/load-travis.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-if [ "$WP_TRAVISCI" == "npm run test-client" ]; then
+if [ "$WP_TRAVISCI" != "phpunit" ]; then
 	gem install sass
 	gem install compass
-	source ~/.nvm/nvm.sh
-	nvm install 5
-	nvm alias default 5
 	npm install -g npm
 	npm install -g gulp-cli
 	npm install
-fi
-
-if [ "$WP_TRAVISCI" == "gulp travis:js" ]; then
-	npm install -g gulp-cli
 fi
