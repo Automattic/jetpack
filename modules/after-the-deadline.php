5ceeb2a9bdf03d8334150ad5f29316b14596861d
Josiah Sprague <josiah.sprague@gmail.com>
2016-07-15 09:35:45 +0200

    Use add_query_arg

--- modules/after-the-deadline.php
+++ modules/after-the-deadline.php
@@ -130,7 +130,7 @@ function register_AtD_button( $buttons ) {
 function add_AtD_tinymce_plugin( $plugin_array ) {
 	$plugin = ATD_TINYMCE_4 ? 'plugin' : 'editor_plugin';
 
-	$plugin_array['AtD'] = plugins_url( 'after-the-deadline/tinymce/' . $plugin . '.js', __FILE__ ) . '?v=' . ATD_VERSION;
+	$plugin_array['AtD'] = add_query_arg('v', ATD_VERSION, plugins_url( 'after-the-deadline/tinymce/' . $plugin . '.js', __FILE__));
 	return $plugin_array;
 }
 
