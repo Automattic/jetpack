b71884ecb69a3937433cf3ec9a3c8b42fe88489a
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-07-20 10:41:40 -0700

    Fix: Make sure that _jetpack_dont_email_post_to_subs is always set correctly/
    Send the data as true if we don't have the module enabled.

--- sync/class.jetpack-sync-module-posts.php
+++ sync/class.jetpack-sync-module-posts.php
@@ -81,7 +81,9 @@ class Jetpack_Sync_Module_Posts extends Jetpack_Sync_Module {
 		$post->post_excerpt_filtered   = apply_filters( 'the_content', $post->post_excerpt );
 		$post->permalink               = get_permalink( $post->ID );
 		$post->shortlink               = wp_get_shortlink( $post->ID );
-		$post->dont_email_post_to_subs = get_post_meta( $post->ID, '_jetpack_dont_email_post_to_subs', true );
+		$post->dont_email_post_to_subs = Jetpack::is_module_active( 'subscriptions' ) ?
+				get_post_meta( $post->ID, '_jetpack_dont_email_post_to_subs', true ) :
+				true; // Don't email subscription if the subscription module is not active.
 
 		return $post;
 	}
