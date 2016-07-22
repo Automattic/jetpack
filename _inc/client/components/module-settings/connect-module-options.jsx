bde4b6de29b0b0c76e2034260170efb3fec1e852
oskosk <oskosk@gmail.com>
2016-07-22 12:40:13 -0300

    Remove deprecated currentValue property from Admin Page's connect-module-options component.

--- _inc/client/components/module-settings/connect-module-options.jsx
+++ _inc/client/components/module-settings/connect-module-options.jsx
@@ -28,7 +28,6 @@ export function connectModuleOptions( Component ) {
 		( state, ownProps ) => {
 			return {
 				validValues: ( option_name ) => getModuleOptionValidValues( state, ownProps.module.module, option_name ),
-				currentValue: getModuleOption( state, ownProps.module.module, ownProps.option_name ),
 				getOptionCurrentValue: ( module_name, option_name ) => getModuleOption( state, module_name, option_name ),
 				enabled: getModuleOption( state, ownProps.module.module, ownProps.option_name ),
 				getModuleOption: ( module_slug ) => getModuleOption( state, module_slug, module_name ),
