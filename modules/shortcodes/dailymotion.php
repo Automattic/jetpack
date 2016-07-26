967f2957ece9cdddd8d061b0384e93f2ad390d94
mathilde SAHUGUET <mathilde.sahuguet@dailymotion.com>
2016-07-26 16:09:45 +0200

    dailymotion widget shortcode: fix embed reversal

--- modules/shortcodes/dailymotion.php
+++ modules/shortcodes/dailymotion.php
@@ -365,20 +365,19 @@ function jetpack_dailymotion_widget_reversal( $content ) {
 		<div class="dailymotion-widget" data-placement="XXXXXXXXX"></div><script>(function(w,d,s,u,n,e,c){w.PXLObject = n; w[n] = w[n] || function(){(w[n].q = w[n].q || []).push(arguments);};w[n].l = 1 * new Date();e = d.createElement(s); e.async = 1; e.src = u;c = d.getElementsByTagName(s)[0]; c.parentNode.insertBefore(e,c);})(window, document, "script", "//api.dmcdn.net/pxl/client.js", "pxl");</script>
 	*/
 
-	$regexes = array();
-	$ifr_regexp = '<div class="dailymotion-widget" data-placement="([^"]+)"><\/div><script>.+?<\/script>';
+	$ifr_regexp = '#<div class="dailymotion-widget" data-placement="([A-Za-z0-9]+)"><\/div><script>.+?<\/script>#';
 	$ifr_regexp_ent = str_replace( '&amp;#0*58;', '&amp;#0*58;|&#0*58;', htmlspecialchars( $ifr_regexp, ENT_NOQUOTES ) );
 
-	foreach ( array( 'regexp', 'regexp_ent' ) as $regex ) {
-		if ( ! preg_match_all( $regex, $content, $matches, PREG_SET_ORDER ) ) {
+	foreach ( array( 'ifr_regexp', 'ifr_regexp_ent' ) as $regex ) {
+		if ( ! preg_match_all( $$regex, $content, $matches, PREG_SET_ORDER ) ) {
 			continue;
 		}
 
 		foreach ( $matches as $match ) {
 			$id = $match[1];
-
 			$shortcode = '[dailymotion-widget id=' . $id . ']';
 			$content = str_replace( $match[0], $shortcode, $content );
+
 		}
 	}
 
