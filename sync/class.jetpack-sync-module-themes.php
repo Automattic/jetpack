e08d7bc55ec59b2b894042c036ace735c44cd8ca
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-07-27 14:14:55 -0700

    Sync: Update full sync to expand theme data before sending it on full sync.

--- sync/class.jetpack-sync-module-themes.php
+++ sync/class.jetpack-sync-module-themes.php
@@ -10,9 +10,8 @@ class Jetpack_Sync_Module_Themes extends Jetpack_Sync_Module {
 		add_action( 'jetpack_sync_current_theme_support', $callable );
 	}
 
-	// TODO: distinct action for full sync
 	public function init_full_sync_listeners( $callable ) {
-		add_action( 'jetpack_full_sync_themes', $callable );
+		add_action( 'jetpack_full_sync_theme_data', $callable );
 	}
 
 	public function sync_theme_support() {
@@ -20,10 +19,31 @@ class Jetpack_Sync_Module_Themes extends Jetpack_Sync_Module {
 	}
 
 	public function enqueue_full_sync_actions() {
-		return $this->enqueue_theme_support_as_action( 'jetpack_full_sync_themes' );
+		/**
+		 * Tells the client to sync all theme data to the server
+		 *
+		 * @since 4.2.0
+		 *
+		 * @param boolean Whether to expand theme data (should always be true)
+		 */
+		do_action( 'jetpack_full_sync_theme_data', true );
+		return 1; // The number of actions enqueued
+	}
+	public function init_before_send() {
+		// full sync
+		add_filter( 'jetpack_sync_before_send_jetpack_full_sync_themes_data', array( $this, 'expand_theme_data' ) );
 	}
 
-	function enqueue_theme_support_as_action( $action_name ) {
+	function get_full_sync_actions() {
+		return array( 'jetpack_full_sync_theme_data' );
+	}
+
+	function expand_theme_data() {
+		return $this->enqueue_theme_support_as_action();
+	}
+
+
+	function enqueue_theme_support_as_action( $action_name = null ) {
 		global $_wp_theme_features;
 
 		$theme_support = array();
@@ -35,20 +55,20 @@ class Jetpack_Sync_Module_Themes extends Jetpack_Sync_Module {
 			}
 		}
 
-		/**
-		 * Fires when the client needs to sync theme support info
-		 * Only sends theme support attributes whitelisted in Jetpack_Sync_Defaults::$default_theme_support_whitelist
-		 *
-		 * @since 4.2.0
-		 *
-		 * @param object the theme support hash
-		 */
-		do_action( $action_name, $theme_support );
+		if ( $action_name )  {
+			/**
+			 * Fires when the client needs to sync theme support info
+			 * Only sends theme support attributes whitelisted in Jetpack_Sync_Defaults::$default_theme_support_whitelist
+			 *
+			 * @since 4.2.0
+			 *
+			 * @param object the theme support hash
+			 */
+			do_action( $action_name, $theme_support );
+		}
+
 
 		return 1; // The number of actions enqueued
 	}
 
-	function get_full_sync_actions() {
-		return array( 'jetpack_full_sync_themes' );
-	}
 }
--- tests/php/sync/test_class.jetpack-sync-full.php
+++ tests/php/sync/test_class.jetpack-sync-full.php
@@ -686,7 +686,7 @@ class WP_Test_Jetpack_Sync_Full extends WP_Test_Jetpack_Sync_Base {
 
 	function test_full_sync_status_with_a_small_queue() {
 
-		$this->sender->set_dequeue_max_bytes( 1500 ); // process 0.0015MB of items at a time\
+		$this->sender->set_dequeue_max_bytes( 700 ); // process 0.0015MB of items at a time\
 
 		$this->create_dummy_data_and_empty_the_queue();
 
