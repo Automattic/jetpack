321f08297c93016ddad0c73a1671a428e5c4b5bc
Marin Atanasov <tyxla@abv.bg>
2016-08-05 10:02:03 +0300

    Manage: Handle case when coming from plans setup page

--- modules/manage/confirm-admin.php
+++ modules/manage/confirm-admin.php
@@ -22,6 +22,11 @@ switch( $section ) {
 		$link_title = __( 'Manage Your Plugins', 'jetpack' );
 		break;
 
+	case 'plugins-setup':
+		$link = 'https://wordpress.com/plugins/setup/' . $normalized_site_url;
+		$link_title = __( 'Back to Plan Setup', 'jetpack' );
+		break;
+
 	case 'themes':
 		$link = 'https://wordpress.com/design/' . $normalized_site_url;
 		$link_title = __( 'Manage Your Themes', 'jetpack' );
@@ -41,11 +46,6 @@ switch( $section ) {
 		$link = 'https://wordpress.com/stats/day/' . $normalized_site_url;
 		$link_title = __( 'Manage Your Site', 'jetpack' );
 		break;
-}
-
-if ( isset( $_GET['return_url'] ) && isset( $_GET['return_title'] ) ) {
-	$link = $_GET['return_url'];
-	$link_title = $_GET['return_title'];
 }
 ?>
 <div class="page-content landing manage-page">
