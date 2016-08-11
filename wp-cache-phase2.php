93633214e1d2c2512a7df5ef25e7c5050108b96b
zytzagoo <zytzagoo@users.noreply.github.com>
2016-08-11 15:32:49 +0200

    Remove `flush()` call in wp_cache_get_response_headers()
    For more details see: https://github.com/Automattic/wp-super-cache/issues/126
    
    Fixes #126

--- wp-cache-phase2.php
+++ wp-cache-phase2.php
@@ -121,7 +121,6 @@ if ( !function_exists( 'wp_cache_user_agent_is_rejected' ) ) {
 
 function wp_cache_get_response_headers() {
 	if(function_exists('apache_response_headers')) {
-		flush();
 		$headers = apache_response_headers();
 	} else if(function_exists('headers_list')) {
 		$headers = array();
