35d2822fff219ea0212f156c171ed3f6a726ac47
Daniel Walmsley <goldsounds@gmail.com>
2016-08-15 12:59:53 -0700

    Merge pull request #4804 from Automattic/fix/remote-disable-sync
    Add setting to remotely (and entirely) disable sync

--- sync/class.jetpack-sync-defaults.php
+++ sync/class.jetpack-sync-defaults.php
@@ -259,6 +259,7 @@ class Jetpack_Sync_Defaults {
 	static $default_queue_max_writes_sec = 100; // 100 rows a second
 	static $default_post_types_blacklist = array();
 	static $default_meta_blacklist = array();
+	static $default_disable = 0; // completely disable sending data to wpcom
 	static $default_sync_callables_wait_time = MINUTE_IN_SECONDS; // seconds before sending callables again
 	static $default_sync_constants_wait_time = HOUR_IN_SECONDS; // seconds before sending constants again
 }
--- tests/php/sync/test_class.jetpack-sync-settings.php
+++ tests/php/sync/test_class.jetpack-sync-settings.php
@@ -11,7 +11,8 @@ class WP_Test_Jetpack_Sync_Settings extends WP_Test_Jetpack_Sync_Base {
 				'upload_max_bytes',
 				'upload_max_rows',
 				'max_queue_size',
-				'max_queue_lag'
+				'max_queue_lag',
+				'disable',
 			) as $key
 		) {
 			$this->assertTrue( isset( $settings[ $key ] ) );
@@ -23,5 +24,26 @@ class WP_Test_Jetpack_Sync_Settings extends WP_Test_Jetpack_Sync_Base {
 		$updated_settings = Jetpack_Sync_Settings::get_settings();
 
 		$this->assertSame( 50, $updated_settings['dequeue_max_bytes'] );
+	}
+
+	function test_settings_disable_enqueue_and_clears_queue() {
+		$event = $this->server_event_storage->reset();
+
+		// create a post - this will end up in the queue before data is sent
+		$post_id = $this->factory->post->create();
+		$this->assertTrue( $this->listener->get_sync_queue()->size() > 0 );
+
+		Jetpack_Sync_Settings::update_settings( array( 'disable' => 1 ) );
+
+		// generating posts should no longer affect queue size
+		$this->assertEquals( 0, $this->listener->get_sync_queue()->size() );
+		$post_id = $this->factory->post->create();
+		$this->assertEquals( 0, $this->listener->get_sync_queue()->size() );
+
+		// syncing sends no data
+		$this->sender->do_sync();
+		$this->assertFalse( $this->server_event_storage->get_most_recent_event( 'wp_insert_post' ) );
+
+		Jetpack_Sync_Settings::update_settings( array( 'disable' => 0 ) );
 	}
 }
