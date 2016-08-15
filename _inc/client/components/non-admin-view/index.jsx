b31ead52b86bfc19a467aed4c0908f2321c4826a
dereksmart <smart@automattic.com>
2016-08-15 12:52:31 -0400

    Non-admin: Don't require non-admin to be linked to see settings/dashboard
    We have a notice to prompt them to link.
    Linking should not be a requirement for configuring settings

--- _inc/client/components/non-admin-view/index.jsx
+++ _inc/client/components/non-admin-view/index.jsx
@@ -3,51 +3,85 @@
  */
 import React from 'react';
 import { connect } from 'react-redux';
-import Card from 'components/card';
-import { translate as __ } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
-import ConnectButton from 'components/connect-button';
-import { isCurrentUserLinked as _isCurrentUserLinked } from 'state/connection';
-import QueryUserConnectionData from 'components/data/query-user-connection';
-import NonAdminViewConnected from './connected';
-import NonAdminViewNotConnected from './not-connected';
+import { userCanViewStats as _userCanViewStats } from 'state/initial-state';
+import { isModuleActivated as _isModuleActivated } from 'state/modules';
+import Navigation from 'components/navigation';
+import NavigationSettings from 'components/navigation-settings';
+import AtAGlance from 'at-a-glance/index.jsx';
+import Engagement from 'engagement/index.jsx';
+import GeneralSettings from 'general-settings/index.jsx';
+import Writing from 'writing/index.jsx';
+import Apps from 'apps/index.jsx';
 
 const NonAdminView = React.createClass( {
-	displayName: 'JetpackConnect',
+	componentWillMount: function() {
+		this.props.setInitialState();
+	},
 
-	renderContent: function() {
-		if ( this.props.isLinked( this.props ) ) {
-			return (
-				<div>
-					<NonAdminViewConnected { ...this.props } />
-				</div>
-			);
+	shouldComponentUpdate: function( nextProps ) {
+		return nextProps.jetpack.connection.status !== this.props.jetpack.connection.status || nextProps.route.path !== this.props.route.path;
+	},
+
+	renderMainContent: function( route ) {
+		let pageComponent,
+			navComponent = <Navigation { ...this.props } />;
+		switch ( route ) {
+			case '/dashboard':
+				if ( this.props.userCanViewStats || this.props.isModuleActivated( 'protect' ) ) {
+					pageComponent = <AtAGlance { ...this.props } />;
+				}
+				break;
+			case '/apps':
+				pageComponent = <Apps { ...this.props } />;
+				break;
+			case '/settings':
+				navComponent = <NavigationSettings { ...this.props } />;
+				pageComponent = <GeneralSettings { ...this.props } />;
+				break;
+			case '/general':
+				navComponent = <NavigationSettings { ...this.props } />;
+				pageComponent = <GeneralSettings { ...this.props } />;
+				break;
+			case '/engagement':
+				navComponent = <NavigationSettings { ...this.props } />;
+				pageComponent = <Engagement { ...this.props } />;
+				break;
+			case '/writing':
+				navComponent = <NavigationSettings { ...this.props } />;
+				pageComponent = <Writing { ...this.props } />;
+				break;
+
+			default:
+				pageComponent = <AtAGlance { ...this.props } />;
 		}
+
+		window.wpNavMenuClassChange();
 
 		return (
 			<div>
-				<NonAdminViewNotConnected />
+				{ navComponent }
+				{ pageComponent }
 			</div>
 		);
 	},
 
 	render: function() {
 		return (
-			<div>
-				<QueryUserConnectionData />
-				{ this.renderContent() }
-			</div>
+			this.renderMainContent( this.props.route.path )
 		);
 	}
+
 } );
 
 export default connect(
 	( state ) => {
 		return {
-			isLinked: () => _isCurrentUserLinked( state )
-		}
+			userCanViewStats: _userCanViewStats( state ),
+			isModuleActivated: ( module_name ) => _isModuleActivated( state, module_name )
+		};
 	}
 )( NonAdminView );
