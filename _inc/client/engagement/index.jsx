ff3827b0ecaf88b58acf887d58034a1c985d9490
Elio Rivero <eliorivero@gmail.com>
2016-08-16 11:30:15 -0300

    Admin: spell accessible correctly

--- _inc/client/engagement/index.jsx
+++ _inc/client/engagement/index.jsx
@@ -40,7 +40,7 @@ export const Page = ( props ) => {
 	if ( ! props.isSitePublic() ) {
 		let makePublic = (
 			<p className="howto small">
-				{ __( 'Your site must be publicly accesible for this feature to work properly. You can make your site public in {{a}}Reading Settings{{/a}}.', {
+				{ __( 'Your site must be publicly accessible for this feature to work properly. You can make your site public in {{a}}Reading Settings{{/a}}.', {
 					components: {
 						a: <a href={ props.getSiteAdminUrl() + 'options-reading.php#blog_public' } className="jetpack-js-stop-propagation" />
 					}
