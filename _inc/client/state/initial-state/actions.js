9d0e9d62aac78dbf99148a09f0b6b0ede9a348e9
oskosk <oskosk@gmail.com>
2016-08-18 05:21:19 -0300

    Move some Admin Page's initial-state selectors from actions file to reducer file

--- _inc/client/state/initial-state/actions.js
+++ _inc/client/state/initial-state/actions.js
@@ -23,34 +23,3 @@ export function getConnectUrl( state ) {
 	return state.jetpack.initialState.connectUrl;
 }
 
-/**
- * Returns a string of the current Jetpack version defined
- * by JETPACK__VERSION
- *
- * @param  {Object}  state  Global state tree
- * @return {string}         Version number
- */
-export function getCurrentVersion( state ) {
-	return state.jetpack.initialState.currentVersion;
-}
-
-/**
- * Returns an array of HE gravatar ID's
- *
- * @param  {Object}  state  Global state tree
- * @return {array}          array of IDs
- */
-export function getHappinessGravatarIds( state ) {
-	return state.jetpack.initialState.happinessGravIds;
-}
-
-/**
- * Returns bool if current version is Dev version
- * Which means -alpha, -beta, etc...
- *
- * @param  {Object}  state  Global state tree
- * @return {bool} true if dev version
- */
-export function isDevVersion( state ) {
-	return state.jetpack.initialState.isDevVersion;
-}
