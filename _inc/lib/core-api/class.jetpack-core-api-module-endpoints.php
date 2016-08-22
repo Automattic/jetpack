ad8f7e4400854a05777ddfca2bf4e59c6a3620cf
Elio Rivero <eliorivero@gmail.com>
2016-08-22 15:55:44 -0300

    Endpoints: add inline documentation

--- _inc/lib/core-api/class.jetpack-core-api-module-endpoints.php
+++ _inc/lib/core-api/class.jetpack-core-api-module-endpoints.php
@@ -6,12 +6,29 @@
 class Jetpack_Core_API_Module_Activate_Endpoint
 	extends Jetpack_Core_API_XMLRPC_Consumer_Endpoint {
 
+	/**
+	 * List of modules that require WPCOM public access.
+	 *
+	 * @since 4.3
+	 *
+	 * @var array
+	 */
 	private $modules_requiring_public = array(
 		'photon',
 		'enhanced-distribution',
 		'json-api',
 	);
 
+	/**
+	 * Check if the module requires the site to be publicly accessible from WPCOM.
+	 * If the site meets this requirement, the module is activated. Otherwise an error is returned.
+	 *
+	 * @since 4.3
+	 *
+	 * @param array $data
+	 *
+	 * @return bool|WP_Error
+	 */
 	public function process( $data ) {
 		if (
 			! in_array( $data['slug'], $this->modules_requiring_public )
@@ -25,6 +42,13 @@ class Jetpack_Core_API_Module_Activate_Endpoint
 			array( 'status' => 404 ) );
 	}
 
+	/**
+	 * Check that the current user has permissions to manage Jetpack modules.
+	 *
+	 * @since 4.3
+	 *
+	 * @return bool
+	 */
 	public function can_write() {
 		return current_user_can( 'jetpack_manage_modules' );
 	}
