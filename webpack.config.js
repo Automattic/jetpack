9fb1470ecaf58f79be90814fd51efe5e738a0688
oskosk <oskosk@gmail.com>
2016-08-29 10:39:07 -0300

    Indent and avoid eslint warnings in webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,21 @@
 require( 'es6-promise' ).polyfill();
 
-var path = require( 'path' );
-var webpack = require( 'webpack' );
-var fs = require('fs');
-var NODE_ENV = process.env.NODE_ENV || 'development';
-var ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+var path = require( 'path' ),
+	webpack = require( 'webpack' ),
+	fs = require( 'fs' ),
+	NODE_ENV = process.env.NODE_ENV || 'development',
+	ExtractTextPlugin = require( 'extract-text-webpack-plugin' ),
+	IS_HOT_UPDATE = ( process.env.NODE_ENV !== 'production' ),
+	jsLoader = IS_HOT_UPDATE
+		? [ require.resolve( 'react-hot-loader' ), require.resolve( 'babel-loader' ), require.resolve( 'eslint-loader' ) ]
+		: [ require.resolve( 'babel-loader' ), require.resolve( 'eslint-loader' ) ],
+	cssLoader = IS_HOT_UPDATE
+		? 'style!css?sourceMap!autoprefixer!'
+		: ExtractTextPlugin.extract( 'css?sourceMap!autoprefixer!' ),
+	webpackConfig;
 
-var IS_HOT_UPDATE = ( process.env.NODE_ENV !== 'production' );
-
-var jsLoader = IS_HOT_UPDATE ?
-	[ require.resolve( 'react-hot-loader' ), require.resolve( 'babel-loader' ), require.resolve( 'eslint-loader' ) ] :
-	[ require.resolve( 'babel-loader' ), require.resolve( "eslint-loader" ) ];
-
-var cssLoader = IS_HOT_UPDATE ?
-	'style!css?sourceMap!autoprefixer!' :
-	ExtractTextPlugin.extract( 'css?sourceMap!autoprefixer!' );
-
-// This file is written in ES5 because it is run via Node.js and is not transpiled by babel. We want to support various versions of node, so it is best to not use any ES6 features even if newer versions support ES6 features out of the box.
-var webpackConfig = {
+		// This file is written in ES5 because it is run via Node.js and is not transpiled by babel. We want to support various versions of node, so it is best to not use any ES6 features even if newer versions support ES6 features out of the box.
+webpackConfig = {
 
 	// Entry points point to the javascript module that is used to generate the script file.
 	// The key is used as the name of the script.
@@ -27,7 +25,7 @@ var webpackConfig = {
 	},
 	output: {
 		path: path.join( __dirname, '_inc/build' ),
-		filename: "[name].js"
+		filename: '[name].js'
 	},
 	devtool: '#source-map',
 	module: {
@@ -67,43 +65,42 @@ var webpackConfig = {
 	resolve: {
 		extensions: [ '', '.js', '.jsx' ],
 		alias: {
-			"react": path.join(__dirname, "/node_modules/react")
+			react: path.join( __dirname, '/node_modules/react' )
 		},
 		root: [
 			path.resolve( __dirname, '_inc/client' ),
-			fs.realpathSync( path.join(__dirname, 'node_modules/@automattic/dops-components/client') )
+			fs.realpathSync( path.join( __dirname, 'node_modules/@automattic/dops-components/client' ) )
 		]
 	},
 	resolveLoader: {
 		root: path.join( __dirname, 'node_modules' )
 	},
 	node: {
-		fs: "empty",
+		fs: 'empty',
 		process: true
 	},
 	eslint: {
-		configFile: path.join(__dirname, '.eslintrc'),
+		configFile: path.join( __dirname, '.eslintrc' ),
 		quiet: true
 	},
 	plugins: [
-		new webpack.DefinePlugin({
+		new webpack.DefinePlugin( {
 
 			// NODE_ENV is used inside React to enable/disable features that should only
 			// be used in development
 			'process.env': {
 				NODE_ENV: JSON.stringify( NODE_ENV )
 			}
-		}),
+		} ),
 		new ExtractTextPlugin( '[name].dops-style.css' )
 	]
 };
 
 if ( NODE_ENV === 'production' ) {
-
 	webpack.DefinePlugin( {
-		"process.env": {
+		'process.env': {
 			// This has effect on the react lib size
-			"NODE_ENV": JSON.stringify(process.env.NODE_ENV) // TODO switch depending on actual environment
+			NODE_ENV: JSON.stringify( process.env.NODE_ENV ) // TODO switch depending on actual environment
 		}
 	} );
 }
