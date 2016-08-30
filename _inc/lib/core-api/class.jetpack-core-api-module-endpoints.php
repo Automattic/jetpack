edcda2220212b60ea5aee6fff5270be2ed0b166e
Igor Zinovyev <zinigor@gmail.com>
2016-08-30 21:33:33 +0400

    Added properly translated module names and descriptions. (#5027)
    * Added properly translated module names and descriptions.
    
    Previously the endpoint was returning untranslated module names and descriptions.
    
    * Fixed the single module endpoint to return properly translated text as well.

--- _inc/lib/core-api/class.jetpack-core-api-module-endpoints.php
+++ _inc/lib/core-api/class.jetpack-core-api-module-endpoints.php
@@ -177,6 +177,8 @@ class Jetpack_Core_API_Module_List_Endpoint {
 			}
 		}
 
+		$modules = Jetpack::get_translated_modules( $modules );
+
 		return Jetpack_Core_Json_Api_Endpoints::prepare_modules_for_response( $modules );
 	}
 
@@ -308,6 +310,15 @@ class Jetpack_Core_API_Module_Endpoint
 				&& Jetpack::is_development_mode()
 			) {
 				$module['activated'] = false;
+			}
+
+			$i18n = jetpack_get_module_i18n( $data['slug'] );
+			if ( isset( $module['name'] ) ) {
+				$module['name'] = $i18n['name'];
+			}
+			if ( isset( $module['description'] ) ) {
+				$module['description'] = $i18n['description'];
+				$module['short_description'] = $i18n['description'];
 			}
 
 			return Jetpack_Core_Json_Api_Endpoints::prepare_modules_for_response( $module );
