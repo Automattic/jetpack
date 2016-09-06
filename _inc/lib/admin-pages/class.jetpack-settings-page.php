42384fd7df4f574ba2c8b9931dcd7a3fa3a93914
Elio Rivero <eliorivero@gmail.com>
2016-09-06 15:12:18 -0300

    Admin - Styles: fix url to admin.dops-style*.css

--- _inc/lib/admin-pages/class.jetpack-settings-page.php
+++ _inc/lib/admin-pages/class.jetpack-settings-page.php
@@ -156,7 +156,7 @@ class Jetpack_Settings_Page extends Jetpack_Admin_Page {
 	 */
 	function additional_styles() {
 		$rtl = is_rtl() ? '.rtl' : '';
-		wp_enqueue_style( 'dops-css', plugins_url( "_inc/build/static.dops-style$rtl.css", JETPACK__PLUGIN_FILE ), array(), JETPACK__VERSION );
+		wp_enqueue_style( 'dops-css', plugins_url( "_inc/build/admin.dops-style$rtl.css", JETPACK__PLUGIN_FILE ), array(), JETPACK__VERSION );
 		wp_enqueue_style( 'components-css', plugins_url( "_inc/build/style.min$rtl.css", JETPACK__PLUGIN_FILE ), array(), JETPACK__VERSION );
 	}
 
