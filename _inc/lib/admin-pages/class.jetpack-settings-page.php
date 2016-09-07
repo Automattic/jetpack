289c480ade28bde543371168a9f6900e0bdc4727
Derek Smart <smart@automattic.com>
2016-09-07 14:03:16 -0400

    Tracks: fix event name to prefix instead of the prop (#5116)
    Before we were prefixing the propname rather than the eventname.  This is backwards.

--- _inc/lib/admin-pages/class.jetpack-settings-page.php
+++ _inc/lib/admin-pages/class.jetpack-settings-page.php
@@ -146,7 +146,7 @@ class Jetpack_Settings_Page extends Jetpack_Admin_Page {
 			$static_html
 		);
 
-		JetpackTracking::record_user_event( 'page_view', array( 'path' => 'wpa_old_settings' ) );
+		JetpackTracking::record_user_event( 'wpa_page_view', array( 'path' => 'old_settings' ) );
 	}
 
 	/**
