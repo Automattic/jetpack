36bec682a7d0682d01de525f794bce7fa2fcb2d8
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-09-23 11:42:22 -0700

    Remove unused code that is not needed any more

--- json-endpoints/jetpack/class.jetpack-json-api-sync-endpoint.php
+++ json-endpoints/jetpack/class.jetpack-json-api-sync-endpoint.php
@@ -48,7 +48,6 @@ class Jetpack_JSON_API_Sync_Status_Endpoint extends Jetpack_JSON_API_Sync_Endpoi
 		$sender      = Jetpack_Sync_Sender::get_instance();
 		$queue       = $sender->get_sync_queue();
 		$full_queue  = $sender->get_full_sync_queue();
-		$next_sync_time = $sender->get_next_sync_time() - microtime( true );
 
 		return array_merge(
 			$sync_module->get_status(),
