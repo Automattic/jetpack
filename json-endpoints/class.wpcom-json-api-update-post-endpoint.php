5e4ab0fceb35d691f2e74ebff2b64b89268b1cab
Marko Andrijasevic <marko.andrijasevic@automattic.com>
2016-09-27 22:07:36 +0000

    Advanced SEO: refactor feature checks
    Refactor all instances of has_business_plan checks, and replace them
    with new is_enabled_advanced_seo helper function. This way we will
    make our checks for Advanced SEO feature consistent acrose the entire
    codebase.
    
    Differential revision: D2615
    
    Merges r142825-wpcom.

--- json-endpoints/class.wpcom-json-api-update-post-endpoint.php
+++ json-endpoints/class.wpcom-json-api-update-post-endpoint.php
@@ -531,8 +531,8 @@ class WPCOM_JSON_API_Update_Post_Endpoint extends WPCOM_JSON_API_Post_Endpoint {
 					return new WP_Error( 'unauthorized', __( 'SEO tools are not enabled for this site.', 'jetpack' ), 403 );
 				}
 				if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
-					if ( 'advanced_seo_description' == $meta->key and ! WPCOM_Store::has_business_plan() ) {
-						return new WP_Error( 'unauthorized', __( 'Advanced SEO can only be set on sites that have a business plan.' ), 403 );
+					if ( 'advanced_seo_description' == $meta->key && ! A8C\SEO\Helpers\is_enabled_advanced_seo() ) {
+						return new WP_Error( 'unauthorized', __( 'Advanced SEO is not enabled for this site.' ), 403 );
 					}
 				}
 
