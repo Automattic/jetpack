f4bd108f48c99d6e4dd28b39c70893c924d7be51
Ryan Kanner <rkanner@medianewsgroup.com>
2016-09-29 08:45:13 -0600

    5221 refreshed from feedback

--- modules/widget-visibility/widget-conditions.php
+++ modules/widget-visibility/widget-conditions.php
@@ -144,10 +144,17 @@ class Jetpack_Widget_Conditions {
 				<?php
 				/**
 				 * Filters args passed to get_taxonomies.
+				 *
 				 * @see https://developer.wordpress.org/reference/functions/get_taxonomies/
+				 * @since 4.4.0
+				 * @module widget-visibility
+				 *
 				 * @param array jetpack_widget_visibility_tax_args
 				 */
-				$taxonomies = get_taxonomies( apply_filters( 'jetpack_widget_visibility_tax_args', array( '_builtin' => false ) ), 'objects' );
+				$taxonomies = get_taxonomies(
+					apply_filters( 'jetpack_widget_visibility_tax_args', array( '_builtin' => false )
+					), 'objects'
+				);
 				usort( $taxonomies, array( __CLASS__, 'strcasecmp_name' ) );
 
 				$parts = explode( '_tax_', $minor );
