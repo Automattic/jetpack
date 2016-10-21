b1792cb8c0c4744b54d8fbfaa5e5af3a36a38c1b
artpi <artpi@post.pl>
2016-10-21 14:22:16 +0200

    Add comment

--- modules/footer-credit.php
+++ modules/footer-credit.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Module Name: Custom footer credit
+ * Module Description: Customize your footer credit
+ * Sort Order: 4
+ * First Introduced: 1.2
+ * Requires Connection: No
+ * Auto Activate: Yes
+ * Module Tags: Social, Appearance
+ * Feature: Appearance
+ */
+
 require_once dirname( __FILE__ ) . '/footer-credit/theme-optimizations.php';
 require_once dirname( __FILE__ ) . '/footer-credit/footer-credit/footer-credit.php';
 ?>
