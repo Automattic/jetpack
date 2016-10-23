8df2cc2dd481e34251de42718f739e78d4f2a3db
Nathan Bloomfield <nbloomf@gmail.com>
2016-10-23 15:04:16 -0500

    Add build_image_sitemap_tree

--- modules/sitemaps/sitemaps.php
+++ modules/sitemaps/sitemaps.php
@@ -241,6 +241,14 @@ class Jetpack_Sitemap_Manager {
 				);
 			}
 
+			// Catch image sitemap xsl
+			if ( preg_match( $regex['image-style'], $_SERVER['REQUEST_URI']) ) {
+				serve_raw_and_die(
+					'text/xml',
+					$this->image_sitemap_xsl()
+				);
+			}
+
 			// URL did not match any sitemap patterns.
 			return;
 		});
@@ -409,7 +417,7 @@ class Jetpack_Sitemap_Manager {
 
 
 	/**
-	 * Build and store a single sitemap index.
+	 * Build and store a single page sitemap index.
 	 *
 	 * Side effect: Create/update a jp_sitemap_index post.
 	 *
@@ -605,7 +613,7 @@ class Jetpack_Sitemap_Manager {
 
 			/* open tag */
 			"<?xml version='1.0' encoding='UTF-8'?>\n" .
-			"<?xml-stylesheet type='text/xsl' href='" . site_url() . "/sitemap.xsl" . "'?>\n" .
+			"<?xml-stylesheet type='text/xsl' href='" . site_url() . "/image-sitemap.xsl" . "'?>\n" .
 			"<urlset\n" .
 			"  xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'\n" .
 			"  xmlns:image='http://www.google.com/schemas/sitemap-image/1.1'>\n",
@@ -706,6 +714,178 @@ class Jetpack_Sitemap_Manager {
 
 
 
+	/**
+	 * Build and store a single image sitemap index.
+	 *
+	 * Side effect: Create/update a jp_img_sitemap_index post.
+	 *
+	 * @since 4.5.0
+	 *
+	 * @param int $number The number of the current image sitemap index.
+	 * @param int $from_ID The greatest lower bound of the IDs of the sitemaps to be included.
+	 * @param string $timestamp Timestamp of previous sitemap in 'YYYY-MM-DD hh:mm:ss' format.
+	 */
+	private function build_one_image_sitemap_index ( $number, $from_ID, $timestamp ) {
+		$last_sitemap_ID = $from_ID;
+		$any_sitemaps_left = true;
+
+		$buffer = new Jetpack_Sitemap_Buffer(
+			self::SITEMAP_MAX_ITEMS,
+			self::SITEMAP_MAX_BYTES,
+
+			/* open tag */
+			"<?xml version='1.0' encoding='UTF-8'?>\n" .
+			"<!-- generator='jetpack-" . JETPACK__VERSION . "' -->\n" .
+			"<?xml-stylesheet type='text/xsl' href='" . site_url() . "/sitemap-index.xsl" . "'?>\n" .
+			"<sitemapindex xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>\n",
+
+			/* close tag */
+			"</sitemapindex>\n",
+
+			/* initial last_modified value */
+			$timestamp
+		);
+
+		// Add pointer to the previous sitemap index (unless we're at the first one)
+		if ( 1 != $number ) {
+			$i = $number - 1;
+			$buffer->try_to_add_item(
+				"<sitemap>\n" .
+ 				" <loc>" . site_url() . "/image-sitemap-index-$i.xml</loc>\n" .
+				" <lastmod>" . str_replace( ' ', 'T', $timestamp) . 'Z' . "</lastmod>\n" .
+				"</sitemap>\n"
+			);
+		}
+
+		// Until the buffer is too large,
+		while ( false == $buffer->is_full() ) {
+			// Retrieve a batch of posts (in order)
+			$posts = $this->get_image_sitemap_posts_after_ID($last_sitemap_ID, 1000);
+
+			// If there were no posts to get, make a note.
+			if (null == $posts) {
+				$any_sitemaps_left = false;
+				break;
+			}
+
+			// Otherwise, for each post in the batch,
+			foreach ($posts as $post) {
+				// Generate the sitemap XML for the post.
+				$current_item = $this->sitemap_to_index_item($post);
+
+				// If adding this item to the buffer doesn't make it too large,
+				if ( true == $buffer->try_to_add_item($current_item['xml']) ) {
+					// Add it and update the last sitemap ID.
+					$last_sitemap_ID = $post->ID;
+					$buffer->view_time($current_item['last_modified']);
+				} else {
+					// Otherwise stop looping through posts.
+					break;
+				}
+			}
+		}
+
+		// Store the buffer as the content of a jp_sitemap_index post.
+		$this->set_contents_of_post(
+			'image-sitemap-index-' . $number,
+			'jp_img_sitemap_index',
+			$buffer->contents(),
+			$buffer->last_modified()
+		);
+
+		/*
+		 * Now report back with the ID of the last sitemap post ID to
+		 * be successfully added, whether there are any sitemap posts
+		 * left, and the most recent modification time seen.
+		 */
+		return array(
+			'last_sitemap_ID'   => $last_sitemap_ID,
+			'any_sitemaps_left' => $any_sitemaps_left,
+		  'last_modified'     => $buffer->last_modified()
+		);
+	}
+
+
+
+	/**
+	 * Build and store all image sitemap indices.
+	 *
+	 * Side effect: Create/update jp_img_sitemap_index posts
+	 * image-sitemap-index-1, image-sitemap-index-2, etc.
+	 *
+	 * @since 4.5.0
+	 *
+	 * @return The number of image sitemap indices generated.
+	 */
+	private function build_all_image_sitemap_indices () {
+		$sitemap_ID = 0;
+		$sitemap_index_number = 1;
+		$last_modified = strtotime('1970-01-01 00:00:00'); // Epoch
+		$any_sitemaps_left = True;
+
+		// Generate sitemap indices until no sitemaps remain.
+		while ( true == $any_sitemaps_left ) {
+			$result = $this->build_one_image_sitemap_index(
+				$sitemap_index_number,
+				$sitemap_ID,
+				$last_modified
+			);
+
+			if ( true == $result['any_sitemaps_left'] ) {
+				$sitemap_ID = $result['last_sitemap_ID'];
+				$sitemap_index_number += 1;
+				$last_modified = $result['last_modified'];
+			} else {
+				$any_sitemaps_left = False;
+			}
+		}
+
+		// Clean up old sitemap indices.
+		$this->delete_numbered_posts_after(
+			'image-sitemap-index-',
+			$sitemap_index_number,
+			'jp_img_sitemap_index'
+		);
+
+		return array(
+			'filename'      => '/image-sitemap-index-' . $sitemap_index_number . '.xml',
+		);
+	}
+
+
+
+	/**
+	 * Build the image sitemap tree structure.
+	 *
+	 * @since 4.5.0
+	 *
+	 * @return array $args {
+	 *     @type string filename The filename of the root image sitemap.
+	 *     @type string last_modified The timestamp of the root image sitemap.
+	 * }
+	 */
+	private function build_image_sitemap_tree () {
+		$num_sitemaps = $this->build_all_image_sitemaps();
+
+		// If there's only one sitemap, make that the root.
+		if ( 1 == $num_sitemaps ) {
+			$this->delete_numbered_posts_after(
+				'image-sitemap-index-',
+				0,
+				'jp_img_sitemap_index'
+			);
+
+			return array(
+				'filename'      => '/image-sitemap-1.xml',
+			);
+		}
+
+		// Otherwise, we have to generate sitemap indices.
+		return $this->build_all_image_sitemap_indices();
+	}
+
+
+
 
 
 
@@ -717,9 +897,8 @@ class Jetpack_Sitemap_Manager {
 	private function generate_all_sitemaps () {
 		$log = new Jetpack_Sitemap_Logger('begin generation');
 
-		$this->build_all_image_sitemaps();
-
 		$page = $this->build_page_sitemap_tree();
+		$image = $this->build_image_sitemap_tree();
 
 		$master = 
 			"<?xml version='1.0' encoding='UTF-8'?>\n" .
@@ -731,7 +910,7 @@ class Jetpack_Sitemap_Manager {
 			"  <lastmod>" . $page['last_modified'] . "</lastmod>\n" .
 			" </sitemap>\n" .
 			" <sitemap>\n" .
-			"  <loc>" . site_url() . "/image-sitemap-blah" . "</loc>\n" .
+			"  <loc>" . site_url() . $image['filename'] . "</loc>\n" .
 			" </sitemap>\n" .
 			"</sitemapindex>\n";
 
@@ -1120,6 +1299,133 @@ XSL;
 
 
 	/**
+	 * Returns the xsl of an image sitemap xml file as a string.
+	 *
+	 * @since 4.5.0
+	 *
+	 * @return string The contents of the xsl file.
+	 */
+	private function image_sitemap_xsl() {
+		$title = esc_html( ent2ncr( __( 'XML Image Sitemap', 'jetpack' ) ) );
+
+		$description = wp_kses(
+			ent2ncr(
+				sprintf(
+					__(
+						'This is an XML Sitemap generated by <a href="%s" target="_blank">Jetpack</a>, meant to be consumed by search engines like <a href="%s" target="_blank">Google</a> or <a href="%s" target="_blank">Bing</a>.',
+						'jetpack'
+					),
+					'http://jetpack.com/',
+					'https://www.google.com/',
+					'https://www.bing.com/'
+				)
+			),
+			array( 'a' => array( 'href' => true, 'title' => true )
+			)
+		);
+
+		$more_info = wp_kses(
+			ent2ncr(
+				sprintf(
+					__(
+						'You can find more information on XML sitemaps at <a href="%s" target="_blank">sitemaps.org</a>',
+						'jetpack'
+					),
+					'http://sitemaps.org'
+				)
+			),
+			array(
+				'a' => array( 'href' => true, 'title' => true )
+			)
+		);
+
+		$header_url = esc_html( ent2ncr( __( 'URL', 'jetpack' ) ) );
+		$header_lastmod = esc_html( ent2ncr( __( 'Last Modified', 'jetpack' ) ) );
+
+		$generated_by = wp_kses(
+			sprintf(
+				ent2ncr(
+					__(
+						'<em>Generated</em> by <a href="%s" target="_blank">Jetpack for WordPress</a>',
+						'jetpack'
+					)
+				),
+				'https://jetpack.com'
+			),
+			array( 'a' => array( 'href' => true, 'title' => true ) )
+		);
+
+		$css = $this->sitemap_xsl_css();
+
+		return <<<XSL
+<?xml version='1.0' encoding='UTF-8'?>
+<xsl:stylesheet version='2.0'
+  xmlns:html='http://www.w3.org/TR/REC-html40'
+  xmlns:sitemap='http://www.sitemaps.org/schemas/sitemap/0.9'
+  xmlns:image='http://www.google.com/schemas/sitemap-image/1.1'
+  xmlns:xsl='http://www.w3.org/1999/XSL/Transform'>
+<xsl:output method='html' version='1.0' encoding='UTF-8' indent='yes'/>
+<xsl:template match="/">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title>$title</title>
+  <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+  <style type='text/css'>
+$css
+  </style>
+</head>
+<body>
+  <div id='description'>
+    <h1>$title</h1>
+    <p>$description</p>
+    <p>$more_info</p>
+  </div>
+  <div id='content'>
+    <!-- <xsl:value-of select="count(sitemap:urlset/sitemap:url)"/> -->
+    <table>
+      <tr>
+        <th>#</th>
+        <th>$header_url</th>
+        <th>$header_lastmod</th>
+      </tr>
+      <xsl:for-each select="sitemap:urlset/sitemap:url">
+        <tr>
+          <xsl:choose>
+            <xsl:when test='position() mod 2 != 1'>
+              <xsl:attribute name="class">odd</xsl:attribute>
+            </xsl:when>
+          </xsl:choose>
+          <td>
+            <xsl:value-of select = "position()" />
+          </td>
+          <td>
+            <xsl:variable name='itemURL'>
+              <xsl:value-of select='sitemap:loc'/>
+            </xsl:variable>
+            <a href='{\$itemURL}'>
+              <xsl:value-of select='sitemap:loc'/>
+            </a>
+          </td>
+          <td>
+            <xsl:value-of select='sitemap:lastmod'/>
+          </td>
+        </tr>
+      </xsl:for-each>
+    </table>
+  </div>
+  <div id='footer'>
+    <p>$generated_by</p>
+  </div>
+</body>
+</html>
+</xsl:template>
+</xsl:stylesheet>\n
+XSL;
+	}
+
+
+
+	/**
 	 * The CSS to be included in sitemap xsl stylesheets;
 	 * factored out for uniformity.
 	 *
@@ -1286,6 +1592,35 @@ CSS;
 			SELECT *
 				FROM $wpdb->posts
 				WHERE post_type='jp_sitemap' AND ID>$from_ID
+				ORDER BY ID ASC
+				LIMIT $num_posts;
+		";
+
+		return $wpdb->get_results( $query_string );
+	}
+
+
+
+	/**
+	 * Retrieve an array of image sitemap posts sorted by ID.
+	 *
+	 * Returns the smallest $num_posts image sitemap posts (measured by ID)
+	 * which are larger than $from_ID.
+	 *
+	 * @since 4.5.0
+	 *
+	 * @param int $from_ID Greatest lower bound of retrieved image sitemap post IDs.
+	 * @param int $num_posts Largest number of sitemap posts to retrieve.
+	 *
+	 * @return array The sitemap posts.
+	 */
+	private function get_image_sitemap_posts_after_ID ( $from_ID, $num_posts ) {
+		global $wpdb;
+
+		$query_string = "
+			SELECT *
+				FROM $wpdb->posts
+				WHERE post_type='jp_img_sitemap' AND ID>$from_ID
 				ORDER BY ID ASC
 				LIMIT $num_posts;
 		";
