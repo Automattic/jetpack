7e0368e98bfabdb37ee9480e05c17072f5510532
artpi <artpi@post.pl>
2016-10-24 11:33:19 -0500

    Fix more code styling issues

--- modules/widgets/google-translate.php
+++ modules/widgets/google-translate.php
@@ -8,6 +8,7 @@
  * Author URI: http://automattic.com
  * Text Domain: jetpack
  */
+
 class Google_Translate_Widget extends WP_Widget {
 	static $instance = null;
 
@@ -71,8 +72,8 @@ class Google_Translate_Widget extends WP_Widget {
 		}
 		?>
 <p>
-	<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php esc_html_e( 'Title:', 'jetpack' ); ?></label>
-	<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+	<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_html_e( 'Title:', 'jetpack' ); ?></label>
+	<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
 </p>
 		<?php
 	}
