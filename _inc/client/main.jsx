9c62f2337195e17850bad236afba5fa13db5ab4e
Igor Zinovyev <zinigor@gmail.com>
2016-10-27 17:29:13 +0300

    Merged two identical switch cases.

--- _inc/client/main.jsx
+++ _inc/client/main.jsx
@@ -167,9 +167,6 @@ const Main = React.createClass( {
 				pageComponent = <Plans siteRawUrl={ this.props.siteRawUrl } siteAdminUrl={ this.props.siteAdminUrl } />;
 				break;
 			case '/settings':
-				navComponent = <NavigationSettings route={ this.props.route } />;
-				pageComponent = <Writing route={ this.props.route } siteAdminUrl={ this.props.siteAdminUrl } />;
-				break;
 			case '/writing':
 				navComponent = <NavigationSettings route={ this.props.route } />;
 				pageComponent = <Writing route={ this.props.route } siteAdminUrl={ this.props.siteAdminUrl } />;
