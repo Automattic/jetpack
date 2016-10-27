24bc85014c563f6ed1d9fb23aacebb49723ed8ed
Elio Rivero <eliorivero@gmail.com>
2016-10-27 12:30:05 -0300

    Tools: consolidate all Yarn scripts in a single one that accepts parameters to call each task.

--- tools/yarn-scripts/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This file is meant to be run as a yarn script
-# Run `yarn run build-client` from the root Jetpack folder to execute it
-
-rm -rf _inc/build/*.js _inc/build/*.css _inc/build/*.map _inc/build/*.html
