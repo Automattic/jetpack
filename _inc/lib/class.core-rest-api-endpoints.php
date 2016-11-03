47ccbd8a9ca8b63b7c280b975e81db10bb952626
Rich Collier <rich.collier@automattic.com>
2016-11-03 12:25:07 -0400

    Remove some unneeded VideoPress options, get options saving properly, lay groundwork for detecting plans

--- _inc/lib/class.core-rest-api-endpoints.php
+++ _inc/lib/class.core-rest-api-endpoints.php
@@ -943,9 +943,7 @@ class Jetpack_Core_Json_Api_Endpoints {
 	 * @return array
 	 */
 	public static function get_updateable_data_list( $selector = '' ) {
-
 		$options = array(
-
 			// Carousel
 			'carousel_background_color' => array(
 				'description'       => esc_html__( 'Background color.', 'jetpack' ),
