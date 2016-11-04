0e970c3440000547a1534efd6d83cd57fedaef40
George Stephanis <georgestephanis@automattic.com>
2016-11-04 15:48:03 -0400

    JSHint: Use strict type comparison.

--- modules/widgets/milestone/milestone.js
+++ modules/widgets/milestone/milestone.js
@@ -14,26 +14,26 @@ var Milestone = ( function( $ ) {
 
 			if ( 2 * MilestoneConfig.YEAR_IN_SECONDS <= this.diff ) { // more than 2 years - show in years, one decimal point
 				var num = ( this.diff / MilestoneConfig.YEAR_IN_SECONDS ).toFixed( 1 );
-				if ( 0 == num.charAt( num.length - 1 ) ) {
+				if ( '0' === num.charAt( num.length - 1 ) ) {
 					num = Math.floor( num );
 				}
 				this.number = num;
 				this.label = labels.years;
 			} else if ( 3 * MilestoneConfig.MONTH_IN_SECONDS <= this.diff ) { // fewer than 2 years - show in months
 				this.number = Math.floor( this.diff / MilestoneConfig.MONTH_IN_SECONDS );
-				this.label = ( 1 == this.number ) ? labels.month : labels.months;
+				this.label = ( 1 === this.number ) ? labels.month : labels.months;
 			} else if ( MilestoneConfig.DAY_IN_SECONDS <= this.diff ) { // fewer than 3 months - show in days
 				this.number = Math.floor( this.diff / MilestoneConfig.DAY_IN_SECONDS ) + 1;
-				this.label = ( 1 == this.number ) ? labels.day : labels.days;
+				this.label = ( 1 === this.number ) ? labels.day : labels.days;
 			} else if ( MilestoneConfig.HOUR_IN_SECONDS <= this.diff ) { // less than 1 day - show in hours
 				this.number = Math.floor( this.diff / MilestoneConfig.HOUR_IN_SECONDS );
-				this.label = ( 1 == this.number ) ? labels.hour : labels.hours;
+				this.label = ( 1 === this.number ) ? labels.hour : labels.hours;
 			} else if ( MilestoneConfig.MINUTE_IN_SECONDS <= this.diff ) { // less than 1 hour - show in minutes
 				this.number = Math.floor( this.diff / MilestoneConfig.MINUTE_IN_SECONDS ) + 1;
-				this.label = ( 1 == this.number ) ? labels.minute : labels.minutes;
+				this.label = ( 1 === this.number ) ? labels.minute : labels.minutes;
 			} else { // less than 1 minute - show in seconds
 				this.number = this.diff;
-				this.label = ( 1 == this.number ) ? labels.second : labels.seconds;
+				this.label = ( 1 === this.number ) ? labels.second : labels.seconds;
 			}
 
 			this.widget.find( '.difference' ).text( this.number );
