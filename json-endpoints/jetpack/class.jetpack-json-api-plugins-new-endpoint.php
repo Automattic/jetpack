f1e38428ed955ddb88426f2f882209f52dd280ae
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-11-07 10:05:34 -0800

    Format code.

--- json-endpoints/jetpack/class.jetpack-json-api-plugins-new-endpoint.php
+++ json-endpoints/jetpack/class.jetpack-json-api-plugins-new-endpoint.php
@@ -7,40 +7,42 @@ class Jetpack_JSON_API_Plugins_New_Endpoint extends Jetpack_JSON_API_Plugins_End
 
 	// POST /sites/%s/plugins/new
 	protected $needed_capabilities = 'install_plugins';
-	protected $action              = 'install';
+	protected $action = 'install';
 
 	protected function validate_call( $_blog_id, $capability, $check_manage_active = true ) {
 		$validate = parent::validate_call( $_blog_id, $capability, $check_manage_active );
 		if ( is_wp_error( $validate ) ) {
-			
+
 			// Lets delete the attachment... if the user doesn't have the right permissions to do things.
 			$args = $this->input();
 			if ( isset( $args['zip'][0]['id'] ) ) {
 				wp_delete_attachment( $args['zip'][0]['id'], true );
 			}
 		}
+
 		return $validate;
 	}
+
 	// no need to try to validate the plugin since we didn't pass one in.
 	protected function validate_input( $plugin ) {
-		$this->bulk = false;
+		$this->bulk    = false;
 		$this->plugins = array();
 	}
 
 	function install() {
 		$args = $this->input();
 
-		if ( isset(  $args['zip'][0]['id'] ) ) {
+		if ( isset( $args['zip'][0]['id'] ) ) {
 			$plugin_attachment_id = $args['zip'][0]['id'];
-			$local_file = get_attached_file( $plugin_attachment_id );
+			$local_file           = get_attached_file( $plugin_attachment_id );
 			if ( ! $local_file ) {
 				return new WP_Error( 'local-file-does-not-exist' );
 			}
-			$skin      = new Jetpack_Automatic_Plugin_Install_Skin();
-			$upgrader  = new Plugin_Upgrader( $skin );
+			$skin     = new Jetpack_Automatic_Plugin_Install_Skin();
+			$upgrader = new Plugin_Upgrader( $skin );
 
 			$pre_install_plugin_list = get_plugins();
-			$result = $upgrader->install( $local_file );
+			$result                  = $upgrader->install( $local_file );
 
 			// clean up.
 			wp_delete_attachment( $plugin_attachment_id, true );
@@ -50,13 +52,13 @@ class Jetpack_JSON_API_Plugins_New_Endpoint extends Jetpack_JSON_API_Plugins_End
 			}
 
 			$after_install_plugin_list = get_plugins();
-			$plugin = array_values( array_diff( array_keys( $after_install_plugin_list ), array_keys( $pre_install_plugin_list ) ) );
+			$plugin                    = array_values( array_diff( array_keys( $after_install_plugin_list ), array_keys( $pre_install_plugin_list ) ) );
 
 			if ( ! $result ) {
 				$error_code = $upgrader->skin->get_main_error_code();
-				$message = $upgrader->skin->get_main_error_message() ;
+				$message    = $upgrader->skin->get_main_error_message();
 				if ( empty( $message ) ) {
-					$message = __( 'An unknown error occurred during installation' , 'jetpack' );
+					$message = __( 'An unknown error occurred during installation', 'jetpack' );
 				}
 
 				if ( 'download_failed' === $error_code ) {
@@ -70,11 +72,12 @@ class Jetpack_JSON_API_Plugins_New_Endpoint extends Jetpack_JSON_API_Plugins_End
 				return new WP_Error( 'plugin_already_installed' );
 			}
 
-			$this->plugins = $plugin;
+			$this->plugins           = $plugin;
 			$this->log[ $plugin[0] ] = $upgrader->skin->get_upgrade_messages();
+
 			return true;
 		}
 
-		return new WP_Error( 'No Plugin installed' );
+		return new WP_Error( 'no_plugin_installed' );
 	}
 }
