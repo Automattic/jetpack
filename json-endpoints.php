b7978c80e30e412ba39105895f649376f0f34623
Andrew Duthie <andrew.duthie@automattic.com>
2016-11-07 14:54:34 +0000

    REST API: Media: Add meta object to response format
    Required to be included in Jetpack response, used in pagination of Calypso editor media library
    
    See: https://github.com/Automattic/wp-calypso/issues/1191
    See: https://github.com/Automattic/wp-calypso/issues/9165
    
    Merges r145158-wpcom.

--- json-endpoints.php
+++ json-endpoints.php
@@ -1233,7 +1233,8 @@ new WPCOM_JSON_API_List_Media_v1_1_Endpoint( array(
 
 	'response_format' => array(
 		'media' => '(array) Array of media objects',
-		'found' => '(int) The number of total results found'
+		'found' => '(int) The number of total results found',
+		'meta'  => '(object) Meta data',
 	),
 
 	'example_request'      => 'https://public-api.wordpress.com/rest/v1.1/sites/82974409/media',
@@ -1279,7 +1280,8 @@ new WPCOM_JSON_API_List_Media_v1_2_Endpoint( array(
 
 	'response_format' => array(
 		'media' => '(array) Array of media objects',
-		'found' => '(int) The number of total results found'
+		'found' => '(int) The number of total results found',
+		'meta'  => '(object) Meta data',
 	),
 
 	'example_request'      => 'https://public-api.wordpress.com/rest/v1.2/sites/82974409/media',
