09d4f30700e4e08044211ba981007e3d08c13160
dereksmart <smart@automattic.com>
2016-11-08 08:23:58 -0500

    Revert: Load in initial state via localized data
    This (temporarily) fixes the initial state in the Main component by loading in the initial state immediately.  It was too slow for Main.jsx and threw errors.

--- _inc/client/state/initial-state/reducer.js
+++ _inc/client/state/initial-state/reducer.js
@@ -9,7 +9,7 @@ import get from 'lodash/get';
  */
 import { JETPACK_SET_INITIAL_STATE } from 'state/action-types';
 
-export const initialState = ( state = {}, action ) => {
+export const initialState = ( state = window.Initial_State, action ) => {
 	switch ( action.type ) {
 		case JETPACK_SET_INITIAL_STATE:
 			return assign( {}, state, action.initialState );
