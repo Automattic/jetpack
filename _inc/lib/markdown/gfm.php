d4688dcfbbc74b73f0d4e9bd910b9706b3d4c487
Sam Hotchkiss <samhotchkiss@users.noreply.github.com>
2016-11-09 16:22:05 -0700

    Markdown: allow filtering $preserve_shortcodes (#5573)
    * Markdown: allow filtering $preserve_shortcodes, fix #367
    
    * Change shortcode prefix

--- _inc/lib/markdown/gfm.php
+++ _inc/lib/markdown/gfm.php
@@ -60,7 +60,16 @@ class WPCom_GHF_Markdown_Parser extends MarkdownExtra_Parser {
 	 */
 	public function __construct() {
 		$this->use_code_shortcode  = class_exists( 'SyntaxHighlighter' );
-		$this->preserve_shortcodes = function_exists( 'get_shortcode_regex' );
+		/**
+		 * Allow processing shortcode contents.
+		 *
+		 * @module markdown
+		 *
+		 * @since 4.4.0
+		 *
+		 * @param boolean $preserve_shortcodes Defaults to $this->preserve_shortcodes.
+		 */
+		$this->preserve_shortcodes = apply_filters( 'jetpack_markdown_preserve_shortcodes', $this->preserve_shortcodes ) && function_exists( 'get_shortcode_regex' );
 		$this->preserve_latex      = function_exists( 'latex_markup' );
 		$this->strip_paras         = function_exists( 'wpautop' );
 
