a19d4b75c76f546862f75d99d96fddc3054ec0a3
Daniel Walmsley <goldsounds@gmail.com>
2016-11-10 07:39:19 -0800

    Fix test

--- tests/php/sync/test_class.jetpack-sync-posts.php
+++ tests/php/sync/test_class.jetpack-sync-posts.php
@@ -386,7 +386,6 @@ class WP_Test_Jetpack_Sync_Post extends WP_Test_Jetpack_Sync_Base {
 	}
 
 	function test_sync_post_includes_dont_email_post_to_subs_when_subscription_is_not_active() {
-		$active_modules = Jetpack::get_active_modules();
 		Jetpack_Options::update_option( 'active_modules', array() );
 		// Subscription is not an active module
 		$this->assertTrue( ! in_array( 'subscriptions', Jetpack::get_active_modules() ) );
@@ -394,11 +393,7 @@ class WP_Test_Jetpack_Sync_Post extends WP_Test_Jetpack_Sync_Base {
 
 		$this->sender->do_sync();
 
-		$post_on_server = $this->server_event_storage->get_most_recent_event( 'wp_insert_post' )->args[1];
-
-		$this->assertEquals( true, $post_on_server->dont_email_post_to_subs );
-
-		Jetpack_Options::update_option( 'active_modules', $active_modules );
+		$this->assertEmpty( $this->server_event_storage->get_all_events( 'jetpack_email_post_to_subs' ) );
 	}
 
 	function test_sync_post_includes_feature_image_meta_when_featured_image_set() {
