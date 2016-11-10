8260e099644f586f2c9de6ec17971a5d2daee3d0
Daniel Walmsley <goldsounds@gmail.com>
2016-11-10 11:13:37 -0800

    Remove redundant code that undoes shutdown hooks in cron

--- sync/class.jetpack-sync-actions.php
+++ sync/class.jetpack-sync-actions.php
@@ -213,11 +213,6 @@ class Jetpack_Sync_Actions {
 
 		self::initialize_sender();
 
-		// remove shutdown hook - no need to sync twice
-		if ( has_action( 'shutdown', array( self::$sender, 'do_sync' ) ) ) {
-			remove_action( 'shutdown', array( self::$sender, 'do_sync' ) );
-		}
-
 		do {
 			$next_sync_time = self::$sender->get_next_sync_time( 'sync' );
 
@@ -240,11 +235,6 @@ class Jetpack_Sync_Actions {
 		}
 
 		self::initialize_sender();
-
-		// remove shutdown hook - no need to sync twice
-		if ( has_action( 'shutdown', array( self::$sender, 'do_sync' ) ) ) {
-			remove_action( 'shutdown', array( self::$sender, 'do_sync' ) );
-		}
 
 		do {
 			$next_sync_time = self::$sender->get_next_sync_time( 'full_sync' );
