914f6766d065a4032488aea512c09b34c9ce8c07
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-11-18 15:55:48 -0800

    Remove empty space.

--- 3rd-party/3rd-party.php
+++ 3rd-party/3rd-party.php
@@ -13,4 +13,3 @@ require_once( JETPACK__PLUGIN_DIR . '3rd-party/woocommerce.php' );
 
 // We can't load this conditionally since polldaddy add the call in class constuctor.
 require_once( JETPACK__PLUGIN_DIR . '3rd-party/polldaddy.php' );
-
