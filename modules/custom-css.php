ecc50be3d3eb02ea6511774a8cf73681f15b2f0c
George Stephanis <georgestephanis@automattic.com>
2016-11-21 14:58:22 -0500

    Add a Jetpack Option for controlling whether old CSS has been migrated to the new CPT.

--- modules/custom-css.php
+++ modules/custom-css.php
@@ -16,7 +16,9 @@ function jetpack_load_custom_css() {
 	// If WordPress has the core version of Custom CSS, load our new version.
 	// @see https://core.trac.wordpress.org/changeset/38829
 	if ( function_exists( 'wp_get_custom_css' ) ) {
-		include dirname( __FILE__ ) . '/custom-css/migrate-to-core.php';
+		if ( ! Jetpack_Options::get_option( 'custom_css_4.7_migration' ) ) {
+			include dirname(__FILE__) . '/custom-css/migrate-to-core.php';
+		}
 		include dirname( __FILE__ ) . '/custom-css/custom-css/preprocessors.php';
 		include dirname( __FILE__ ) . '/custom-css/custom-css-4.7.php';
 		return;
