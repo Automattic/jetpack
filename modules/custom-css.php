1ae1e0ca8c5512c571eb51f7f829aad848676ec4
George Stephanis <georgestephanis@automattic.com>
2016-11-21 15:19:41 -0500

    Add WP-CLI command to undo migration for testing.

--- modules/custom-css.php
+++ modules/custom-css.php
@@ -18,7 +18,15 @@ function jetpack_load_custom_css() {
 	if ( function_exists( 'wp_get_custom_css' ) ) {
 		if ( ! Jetpack_Options::get_option( 'custom_css_4.7_migration' ) ) {
 			include dirname(__FILE__) . '/custom-css/migrate-to-core.php';
-		}
+		} else {
+            if ( defined( 'WP_CLI' ) && WP_CLI ) {
+                function jetpack_custom_css_undo_data_migration_cli() {
+                    Jetpack_Options::delete_option( 'custom_css_4.7_migration' );
+                    WP_CLI::success( __( 'Option deleted, re-migrate via `wp jetpack custom-css migrate`.', 'jetpack' ) );
+                }
+                WP_CLI::add_command( 'jetpack custom-css undo-migrate', 'jetpack_custom_css_undo_data_migration_cli' );
+            }
+        }
 		include dirname( __FILE__ ) . '/custom-css/custom-css/preprocessors.php';
 		include dirname( __FILE__ ) . '/custom-css/custom-css-4.7.php';
 		return;
