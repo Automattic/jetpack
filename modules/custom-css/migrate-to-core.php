13651d5c8011a4ac2ce9003c2df2ef3bfb3d43b7
George Stephanis <georgestephanis@automattic.com>
2016-11-21 22:47:35 -0500

    Change how we pull the options and the latest revision to yank from.

--- modules/custom-css/migrate-to-core.php
+++ modules/custom-css/migrate-to-core.php
@@ -76,6 +76,9 @@ class Jetpack_Custom_CSS_Data_Migration {
 			) );
 		}
 
+		$options = self::get_options( $jetpack_css_post->ID );
+		set_theme_mod( 'jetpack_custom_css', $options );
+
 		// If we've migrated some CSS for the current theme and there was already something there in the Core dataset ...
 		if ( $core_css_post && $jetpack_css_post ) {
 			$preprocessor = $options['preprocessor'];
