cad05dd5caedf365566b4ceb8b7d9fbd9b78455a
George Stephanis <georgestephanis@automattic.com>
2016-11-23 14:19:03 -0500

    Move where the options are migrated.

--- modules/custom-css/migrate-to-core.php
+++ modules/custom-css/migrate-to-core.php
@@ -76,10 +76,6 @@ class Jetpack_Custom_CSS_Data_Migration {
 			) );
 		}
 
-		// Migrate the settings from revision meta to theme mod.
-		$options = self::get_options( $jetpack_css_revision->ID );
-		set_theme_mod( 'jetpack_custom_css', $options );
-
 		// If we've migrated some CSS for the current theme and there was already something there in the Core dataset ...
 		if ( $core_css_post && $jetpack_css_post ) {
 			$preprocessor = $options['preprocessor'];
