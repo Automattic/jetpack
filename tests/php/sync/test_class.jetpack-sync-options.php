dddf61d1c6409942e8caedd4e24f5dd72284e7eb
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-11-22 17:44:43 -0800

    Add: Test that shows we also can add the filter at 89 init

--- tests/php/sync/test_class.jetpack-sync-options.php
+++ tests/php/sync/test_class.jetpack-sync-options.php
@@ -194,17 +194,27 @@ class WP_Test_Jetpack_Sync_Options extends WP_Test_Jetpack_Sync_Base {
 		$this->assertTrue( empty( $whitelist_and_option_keys_difference ), 'Some whitelisted options don\'t have a test: ' . print_r( $whitelist_and_option_keys_difference, 1 ) );
 	}
 
+	public function test_add_whitelisted_option_on_init_89() {
+		add_action( 'init', array( $this, 'add_option_on_89' ), 89 );
+		do_action( 'init' );
+
+		$whitelist = $this->options_module->get_options_whitelist();
+		
+		$this->assertTrue( in_array( 'foo_option_bar', $whitelist ) );
+	}
+
 	function assertOptionIsSynced( $option_name, $value ) {
 		$this->assertEqualsObject( $value, $this->server_replica_storage->get_option( $option_name ), 'Option ' . $option_name . ' did\'t have the extected value of ' . json_encode( $value ) );
 	}
 
-	function add_fiter_on_init() {
-		add_filter( 'jetpack_options_whitelist', array( $this, 'add_jetpack_options_whitelist_filter' ) );
-	}
-
 	public function add_jetpack_options_whitelist_filter( $options ) {
 		$options[] = 'foo_option_bar';
-
 		return $options;
+	}
+
+
+	
+	function add_option_on_89() {
+		add_filter( 'jetpack_options_whitelist', array( $this, 'add_jetpack_options_whitelist_filter' ) );
 	}
 }
