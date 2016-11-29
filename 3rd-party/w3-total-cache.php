0f2c87e296eedf5259cf223f48c11dea74e990c8
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-11-29 14:47:21 -0800

    Clean up the debugging.

--- 3rd-party/w3-total-cache.php
+++ 3rd-party/w3-total-cache.php
@@ -5,8 +5,6 @@
 add_filter( 'w3tc_config_default_values', 'jetpack_bypass_w3_total_cache' );
 
 function jetpack_bypass_w3_total_cache( $default_values ) {
-
-	error_log( print_r( 'This is loaded...',1 ) );
 	$default_values['dbcache.reject.words']['default'][] = '\bjpsq_\w';
 	return $default_values;
 }
