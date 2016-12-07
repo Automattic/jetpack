8af3d86e392b7fb9c464276c6ed6aebf8655a320
Elio Rivero <eliorivero@gmail.com>
2016-12-07 16:47:40 -0300

    Shortcodes - Sitemap: only output if there are pages to show. Add CSS class if user ever wants to stylize this differently. Remove unused parameters and update inline doc.

--- modules/shortcodes/sitemap.php
+++ modules/shortcodes/sitemap.php
@@ -8,19 +8,19 @@
 add_shortcode( 'sitemap', 'jetpack_sitemap_shortcode' );
 
 /**
- * Parse shortcode arguments and render its output.
+ * Renders a tree of pages.
  *
  * @since 4.5.0
  *
- * @param array  $atts    Shortcode parameters.
- * @param string $content Content enclosed by shortcode tags.
- *
  * @return string
  */
-function jetpack_sitemap_shortcode( $atts, $content ) {
-	return '<ul>' .	wp_list_pages( array(
+function jetpack_sitemap_shortcode() {
+	$tree = wp_list_pages( array(
 		'title_li' => '<b><a href="/">' . esc_html( get_bloginfo( 'name' ) ) . '</a></b>',
 		'exclude'  => get_option( 'page_on_front' ),
 		'echo'     => false,
-	) ) . '</ul>';
+	) );
+	return empty( $tree )
+		? ''
+		: '<ul class="jetpack-sitemap-shortcode">' . $tree . '</ul>';
 }
