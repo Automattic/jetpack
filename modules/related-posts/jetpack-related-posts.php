a90c5fe392da9e5bec7950bda3bb485159a052ce
Eric Binnion <ericbinnion@gmail.com>
2016-12-07 15:46:18 -0600

    New build

--- modules/related-posts/jetpack-related-posts.php
+++ modules/related-posts/jetpack-related-posts.php
@@ -187,7 +187,8 @@ class Jetpack_RelatedPosts {
 
 		if ( $options['show_headline'] ) {
 			$headline = sprintf(
-				'<h3 class="jp-relatedposts-headline"><em>%s</em></h3>',
+				/** This filter is already documented in modules/sharedaddy/sharing-service.php */
+				apply_filters( 'jetpack_sharing_headline_html', '<h3 class="jp-relatedposts-headline"><em>%s</em></h3>', esc_html__( 'Related', 'jetpack' ), 'related-posts' ),
 				esc_html__( 'Related', 'jetpack' )
 			);
 		} else {
