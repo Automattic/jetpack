Automattic/jetpack

commit f4ef1f8469580e2c3fff4a020f4dadb4e8f36257

Fix Carousel not starting for single image after its link was edited
Before the fix Carousel for single images was dependent on attribute rel
of 'a' tag wrapping the 'img' tag. This attribute is set differently when
image is added to post and when its link is edited in Media Settings modal.

After the fix this attribute is not used - additional data-permalink
attribute was introduced to img tag that allows easy diffrentiation
whether image links to 'Media File' or 'Attachment Page'

Fixes: #5431

modules/carousel/jetpack-carousel.js
@@ -1419,17 +1419,30 @@ jQuery(document).ready(function($) {
 	// handle lightbox (single image gallery) for images linking to 'Attachment Page'
 	if ( 1 === Number( jetpackCarouselStrings.single_image_gallery ) ) {
 		// process links that have rel=attachment and img tag inside
-		$( 'a[rel*="attachment"] img' ).each(function() {
+		$( 'a img[data-attachment-id]' ).each(function() {
 			var container = $( this ).parent();
-			// if link points to 'Media File' and flag is not set skip it
+			var valid = false;
+
+			// if link points to 'Media File' and flag is set allow it
 			if ( $( container ).attr( 'href' ) === $( this ).attr( 'data-orig-file' ) &&
-				0 === Number( jetpackCarouselStrings.single_image_gallery_media_file )
+				1 === Number( jetpackCarouselStrings.single_image_gallery_media_file )
 			) {
+				valid = true;
+			}
+
+			// if link points to 'Attachment Page' allow it
+			if( $( container ).attr( 'href' ) === $( this ).attr( 'data-permalink' ) ) {
+				valid = true;
+			}
+
+			// links to 'Custom URL' or 'Media File' when flag not set are not valid
+			if( ! valid ) {
 				return;
 			}
+
 			// make this node a gallery recognizable by event listener above
 			$( container ).addClass( 'single-image-gallery') ;
-			// method 'open' needs this attribute to distinguish from defailt WP gallery
+			// method 'open' needs this attribute to distinguish from default WP gallery
 			container.data( 'carousel-extra', { single_image: true } );
 		});
 	}

modules/carousel/jetpack-carousel.php
@@ -421,6 +421,7 @@ class Jetpack_Carousel {
 		$img_meta = json_encode( array_map( 'strval', $img_meta ) );
 
 		$attr['data-attachment-id']     = $attachment_id;
+		$attr['data-permalink']         = esc_attr( get_permalink( $attachment->ID ) );
 		$attr['data-orig-file']         = esc_attr( $orig_file );
 		$attr['data-orig-size']         = $size;
 		$attr['data-comments-opened']   = $comments_opened;

commit 2c6568ba642ec741eabae2c73a7f728976b2efa9

Add/carousel lightbox single image (#5469)
* Add Carousel for single image

Implementation of https://github.com/Automattic/jetpack/issues/375
Initial version without configuration flag - feature is always on.
Replace single image that links to itself (Media File) with a
one-image gallery, so that the site look&feel is preserved when
image is clicked.

* Add carousel lightbox to single images

Revert gallery shortcode base solution with the one that doesn't affect post
layout. When feature is on (toggle added in Settings/Media) this happens:
- PHP adds data-* attributes to img tags
- JS detects images linking to 'Attachment Page'
   and makes them open in Lightbox

Known limitations:
- Works only on pages where some other gallery is present
- Configuration is redundant and should be replace with a filter

Implements #5397

* Carousel single image: remove config, fix assets queue

Fix bug where carousel assets were added only on pages with gallery shortcode.

Replace configuration checkbox for single image carousel
with a filter 'maybe_disable_jp_carousel_single_images'.
Feature is by default turned on if Carousel is on.

Implements #5397

* Renamed add_data_to_single_images to sth more meaningful

This change implement request from a PR comment:
Even though this function is similarly named as add_data_to_images
they do very different things – this one parses the HTML and adds
HTML attributes, while the other one works only on structured data.

Fixes: #5431

* Replace WP_Post::get_instance with get_post

Change suggested in code review.

Fixes: #5431

* Fix buggy single-image gellery navigation with keyboard

Description: If I press left/right with the carousel open I get the same image, but dimmed.
Ideally, nothing will happen when I press the arrows.

Fixed by not triggering navigation logic
when there's only one slide in the gallery.

Part of: #5431

* Fix for single image Lightbox and 3rd party galleries

Before the fix there was a risk that assets from 3rd party plugin for galleries
will conflict with Carousel assets. After the fix Carousel assets are
not loaded if 3rd party plugin for gallery shortcode was already loaded.

Fixes: #5431

* Added @since for filter jp_carousel_maybe_disable_single_images

Fixes: #5431

* Fix JS comparison operator to make JSHint happy

Fixes: #5431

* Introduce filter to load Carousel for images linking to Media File

When filter jp_carousel_load_for_images_linked_to_file returns true
Carousel gallery will be loaded also for images linking to Media File
(raw graphics file in full resolution).

Previously existing filter jp_carousel_maybe_disable_single_images
acts as a master switch: if it returs true (disabled) the newly added
filter has no effect.

Fixes: #5431

* Fix Carousel prev/next arrows flashing

For single image gallery arrows were being displayed by default
and hidden after few miliseconds as unnecessary. With this fix
they are hidden by default and are shown only for galleries with
more than one image.

Fixes: #5431

* Fix Carousel not starting for single image after its link was edited

Before the fix Carousel for single images was dependent on attribute rel
of 'a' tag wrapping the 'img' tag. This attribute is set differently when
image is added to post and when its link is edited in Media Settings modal.

After the fix this attribute is not used - additional data-permalink
attribute was introduced to img tag that allows easy diffrentiation
whether image links to 'Media File' or 'Attachment Page'

Fixes: #5431

* Fix buggy single-image gallery navigation with left/right wipe

Complementary to fix from commit 66ac502da8b889de3bb2c4011e44c136b44ed49c

Additionally fixed some spacing and typos.

Fixes #5431

* Fix bug where images belonging to gallery were processed twice

Bug was introduced in f4ef1f8469580e2c3fff4a020f4dadb4e8f36257

modules/carousel/jetpack-carousel.js
@@ -37,18 +37,15 @@ jQuery(document).ready(function($) {
 				break;
 			case 39: // right
 				e.preventDefault();
-				gallery.jp_carousel('clearCommentTextAreaValue');
 				gallery.jp_carousel('next');
 				break;
 			case 37: // left
 			case 8: // backspace
 				e.preventDefault();
-				gallery.jp_carousel('clearCommentTextAreaValue');
 				gallery.jp_carousel('previous');
 				break;
 			case 27: // escape
 				e.preventDefault();
-				gallery.jp_carousel('clearCommentTextAreaValue');
 				container.jp_carousel('close');
 				break;
 			default:
@@ -193,13 +190,15 @@ jQuery(document).ready(function($) {
 				.addClass('jp-carousel-next-button')
 				.css({
 					'right'    : '15px'
-				});
+				})
+				.hide();
 
 			previousButton = $('<div><span></span></div>')
 				.addClass('jp-carousel-previous-button')
 				.css({
 					'left'     : 0
-				});
+				})
+				.hide();
 
 			nextButton.add( previousButton ).css( {
 				'position' : 'fixed',
@@ -387,6 +386,8 @@ jQuery(document).ready(function($) {
 					$(window).unbind('keydown', keyListener);
 					$(window).unbind('resize', resizeListener);
 					$(window).scrollTop(scroll);
+					$( '.jp-carousel-previous-button' ).hide();
+					$( '.jp-carousel-next-button' ).hide();
 				})
 				.bind('jp_carousel.afterClose', function(){
 					if ( window.location.hash && history.back ) {
@@ -460,7 +461,7 @@ jQuery(document).ready(function($) {
 
 		open: function(options) {
 			var settings = {
-				'items_selector' : '.gallery-item [data-attachment-id], .tiled-gallery-item [data-attachment-id]',
+				'items_selector' : '.gallery-item [data-attachment-id], .tiled-gallery-item [data-attachment-id], img[data-attachment-id]',
 				'start_index': 0
 			},
 			data = $(this).data('carousel-extra');
@@ -523,6 +524,7 @@ jQuery(document).ready(function($) {
 			// make sure to let the page scroll again
 			$('body').css('overflow', originalOverflow);
 			$('html').css('overflow', originalHOverflow);
+			this.jp_carousel( 'clearCommentTextAreaValue' );
 			return container
 				.trigger('jp_carousel.beforeClose')
 				.fadeOut('fast', function(){
@@ -532,21 +534,25 @@ jQuery(document).ready(function($) {
 
 		},
 
-		next : function(){
-			var slide = gallery.jp_carousel( 'nextSlide' );
-			container.animate({scrollTop:0}, 'fast');
-
-			if ( slide ) {
-				this.jp_carousel('selectSlide', slide);
-			}
+		next : function() {
+			this.jp_carousel( 'previousOrNext', 'nextSlide' );
 		},
 
-		previous : function(){
-			var slide = gallery.jp_carousel( 'prevSlide' );
-			container.animate({scrollTop:0}, 'fast');
+		previous : function() {
+			this.jp_carousel( 'previousOrNext', 'prevSlide' );
+		},
+
+		previousOrNext : function ( slideSelectionMethodName ) {
+			if ( ! this.jp_carousel( 'hasMultipleImages' ) ) {
+				return false;
+			}
+
+			var slide = gallery.jp_carousel( slideSelectionMethodName );
 
 			if ( slide ) {
-				this.jp_carousel('selectSlide', slide);
+				container.animate( { scrollTop: 0 }, 'fast' );
+				this.jp_carousel( 'clearCommentTextAreaValue' );
+				this.jp_carousel( 'selectSlide', slide );
 			}
 		},
 
@@ -1374,6 +1380,10 @@ jQuery(document).ready(function($) {
 
 				image.data( 'loaded', 1 );
 			}
+		},
+
+		hasMultipleImages : function () {
+			return gallery.jp_carousel('slides').length > 1;
 		}
 	};
 
@@ -1391,7 +1401,7 @@ jQuery(document).ready(function($) {
 	};
 
 	// register the event listener for starting the gallery
-	$( document.body ).on( 'click.jp-carousel', 'div.gallery,div.tiled-gallery', function(e) {
+	$( document.body ).on( 'click.jp-carousel', 'div.gallery,div.tiled-gallery, a.single-image-gallery', function(e) {
 		if ( ! $(this).jp_carousel( 'testForData', e.currentTarget ) ) {
 			return;
 		}
@@ -1405,6 +1415,43 @@ jQuery(document).ready(function($) {
 		e.stopPropagation();
 		$(this).jp_carousel('open', {start_index: $(this).find('.gallery-item, .tiled-gallery-item').index($(e.target).parents('.gallery-item, .tiled-gallery-item'))});
 	});
+
+	// handle lightbox (single image gallery) for images linking to 'Attachment Page'
+	if ( 1 === Number( jetpackCarouselStrings.single_image_gallery ) ) {
+		// process links that contain img tag with attribute data-attachment-id
+		$( 'a img[data-attachment-id]' ).each(function() {
+			var container = $( this ).parent();
+
+			// skip if image was already added to gallery by shortcode
+			if( container.parent( '.gallery-icon' ).length ) {
+				return;
+			}
+
+			var valid = false;
+
+			// if link points to 'Media File' and flag is set allow it
+			if ( $( container ).attr( 'href' ) === $( this ).attr( 'data-orig-file' ) &&
+				1 === Number( jetpackCarouselStrings.single_image_gallery_media_file )
+			) {
+				valid = true;
+			}
+
+			// if link points to 'Attachment Page' allow it
+			if( $( container ).attr( 'href' ) === $( this ).attr( 'data-permalink' ) ) {
+				valid = true;
+			}
+
+			// links to 'Custom URL' or 'Media File' when flag not set are not valid
+			if( ! valid ) {
+				return;
+			}
+
+			// make this node a gallery recognizable by event listener above
+			$( container ).addClass( 'single-image-gallery' ) ;
+			// blog_id is needed to allow posting comments to correct blog
+			$( container ).data( 'carousel-extra', { blog_id: Number( jetpackCarouselStrings.blog_id ) } );
+		});
+	}
 
 	// Makes carousel work on page load and when back button leads to same URL with carousel hash (ie: no actual document.ready trigger)
 	$( window ).on( 'hashchange.jp-carousel', function () {
@@ -1432,7 +1479,7 @@ jQuery(document).ready(function($) {
 		last_known_location_hash = window.location.hash;
 		matches = window.location.hash.match( hashRegExp );
 		attachmentId = parseInt( matches[1], 10 );
-		galleries = $( 'div.gallery, div.tiled-gallery' );
+		galleries = $( 'div.gallery, div.tiled-gallery, a.single-image-gallery' );
 
 		// Find the first thumbnail that matches the attachment ID in the location
 		// hash, then open the gallery that contains it.
@@ -1447,6 +1494,7 @@ jQuery(document).ready(function($) {
 			if ( selectedThumbnail ) {
 				$( selectedThumbnail.gallery )
 					.jp_carousel( 'openOrSelectSlide', selectedThumbnail.index );
+				return false;
 			}
 		});
 	});

modules/carousel/jetpack-carousel.php
@@ -24,6 +24,10 @@ class Jetpack_Carousel {
 
 	public $in_jetpack = true;
 
+	public $single_image_gallery_enabled = false;
+
+	public $single_image_gallery_enabled_media_file = false;
+
 	function __construct() {
 		add_action( 'init', array( $this, 'init' ) );
 	}
@@ -33,6 +37,9 @@ class Jetpack_Carousel {
 			return;
 
 		$this->in_jetpack = ( class_exists( 'Jetpack' ) && method_exists( 'Jetpack', 'enable_module_configurable' ) ) ? true : false;
+
+		$this->single_image_gallery_enabled = !$this->maybe_disable_jp_carousel_single_images();
+		$this->single_image_gallery_enabled_media_file = $this->maybe_enable_jp_carousel_single_images_media_file();
 
 		if ( is_admin() ) {
 			// Register the Carousel-related related settings
@@ -62,10 +69,14 @@ class Jetpack_Carousel {
 			 * @param array $this->prebuilt_widths Array of default widths.
 			 */
 			$this->prebuilt_widths = apply_filters( 'jp_carousel_widths', $this->prebuilt_widths );
-			add_filter( 'post_gallery', array( $this, 'enqueue_assets' ), 1000, 2 ); // load later than other callbacks hooked it
+			// below: load later than other callbacks hooked it (e.g. 3rd party plugins handling gallery shortcode)
+			add_filter( 'post_gallery', array( $this, 'check_if_shortcode_processed_and_enqueue_assets' ), 1000, 2 );
 			add_filter( 'post_gallery', array( $this, 'set_in_gallery' ), -1000 );
 			add_filter( 'gallery_style', array( $this, 'add_data_to_container' ) );
 			add_filter( 'wp_get_attachment_image_attributes', array( $this, 'add_data_to_images' ), 10, 2 );
+			if ( $this->single_image_gallery_enabled ) {
+				add_filter( 'the_content', array( $this, 'add_data_img_tags_and_enqueue_assets' ) );
+			}
 		}
 
 		if ( $this->in_jetpack && method_exists( 'Jetpack', 'module_configuration_load' ) ) {
@@ -85,6 +96,33 @@ class Jetpack_Carousel {
 		 * @param bool false Should Carousel be disabled? Default to false.
 		 */
 		return apply_filters( 'jp_carousel_maybe_disable', false );
+	}
+
+	function maybe_disable_jp_carousel_single_images() {
+		/**
+		 * Allow third-party plugins or themes to disable Carousel for single images.
+		 *
+		 * @module carousel
+		 *
+		 * @since 4.5.0
+		 *
+		 * @param bool false Should Carousel be disabled for single images? Default to false.
+		 */
+		return apply_filters( 'jp_carousel_maybe_disable_single_images', false );
+	}
+
+	function maybe_enable_jp_carousel_single_images_media_file() {
+		/**
+		 * Allow third-party plugins or themes to enable Carousel
+		 * for single images linking to 'Media File' (full size image).
+		 *
+		 * @module carousel
+		 *
+		 * @since 4.5.0
+		 *
+		 * @param bool false Should Carousel be enabled for single images linking to 'Media File'? Default to false.
+		 */
+		return apply_filters( 'jp_carousel_load_for_images_linked_to_file', false );
 	}
 
 	function jetpack_configuration_load() {
@@ -115,7 +153,7 @@ class Jetpack_Carousel {
 		return $output;
 	}
 
-	function enqueue_assets( $output ) {
+	function check_if_shortcode_processed_and_enqueue_assets( $output ) {
 		if (
 			! empty( $output ) &&
 			/**
@@ -132,6 +170,7 @@ class Jetpack_Carousel {
 			// Bail because someone is overriding the [gallery] shortcode.
 			remove_filter( 'gallery_style', array( $this, 'add_data_to_container' ) );
 			remove_filter( 'wp_get_attachment_image_attributes', array( $this, 'add_data_to_images' ) );
+			remove_filter( 'the_content', array( $this, 'add_data_img_tags_and_enqueue_assets' ) );
 			// Display message that carousel has bailed, if user is super_admin, and if we're not on WordPress.com.
 			if (
 				is_super_admin() &&
@@ -151,6 +190,12 @@ class Jetpack_Carousel {
 		 **/
 		do_action( 'jp_carousel_thumbnails_shown' );
 
+		$this->enqueue_assets();
+
+		return $output;
+	}
+
+	function enqueue_assets() {
 		if ( $this->first_run ) {
 			wp_enqueue_script( 'jetpack-carousel', plugins_url( 'jetpack-carousel.js', __FILE__ ), array( 'jquery.spin' ), $this->asset_version( '20160325' ), true );
 
@@ -168,6 +213,8 @@ class Jetpack_Carousel {
 				'nonce'                => wp_create_nonce( 'carousel_nonce' ),
 				'display_exif'         => $this->test_1or0_option( Jetpack_Options::get_option_and_ensure_autoload( 'carousel_display_exif', true ) ),
 				'display_geo'          => $this->test_1or0_option( Jetpack_Options::get_option_and_ensure_autoload( 'carousel_display_geo', true ) ),
+				'single_image_gallery' => $this->single_image_gallery_enabled,
+				'single_image_gallery_media_file' => $this->single_image_gallery_enabled_media_file,
 				'background_color'     => $this->carousel_background_color_sanitize( Jetpack_Options::get_option_and_ensure_autoload( 'carousel_background_color', '' ) ),
 				'comment'              => __( 'Comment', 'jetpack' ),
 				'post_comment'         => __( 'Post Comment', 'jetpack' ),
@@ -188,6 +235,7 @@ class Jetpack_Carousel {
 				'require_name_email'   => $require_name_email,
 				/** This action is documented in core/src/wp-includes/link-template.php */
 				'login_url'            => wp_login_url( apply_filters( 'the_permalink', get_permalink() ) ),
+				'blog_id'              => (int) get_current_blog_id(),
 			);
 
 			if ( ! isset( $localize_strings['jetpack_comments_iframe_src'] ) || empty( $localize_strings['jetpack_comments_iframe_src'] ) ) {
@@ -275,8 +323,6 @@ class Jetpack_Carousel {
 
 			$this->first_run = false;
 		}
-
-		return $output;
 	}
 
 	function set_in_gallery( $output ) {
@@ -284,13 +330,49 @@ class Jetpack_Carousel {
 		return $output;
 	}
 
-	function add_data_to_images( $attr, $attachment = null ) {
+	/**
+	 * Adds data-* attributes required by carousel to img tags in post HTML
+	 * content. To be used by 'the_content' filter.
+	 *
+	 * @see add_data_to_images()
+	 * @see wp_make_content_images_responsive() in wp-includes/media.php
+	 *
+	 * @param string $content HTML content of the post
+	 * @return string Modified HTML content of the post
+	 */
+	function add_data_img_tags_and_enqueue_assets( $content ) {
+		if ( ! preg_match_all( '/<img [^>]+>/', $content, $matches ) ) {
+			return $content;
+		}
+		$selected_images = array();
 
-		// not in a gallery?
-		if ( ! $this->in_gallery ) {
-			return $attr;
+		foreach( $matches[0] as $image_html ) {
+			if ( preg_match( '/wp-image-([0-9]+)/i', $image_html, $class_id ) &&
+				( $attachment_id = absint( $class_id[1] ) ) ) {
+
+				/*
+				 * If exactly the same image tag is used more than once, overwrite it.
+				 * All identical tags will be replaced later with 'str_replace()'.
+				 */
+				$selected_images[ $attachment_id  ] = $image_html;
+			}
 		}
 
+		foreach ( $selected_images as $attachment_id => $image_html ) {
+			$attachment = get_post( $attachment_id );
+			$attributes = $this->add_data_to_images( array(), $attachment );
+			$attributes_html = '';
+			foreach( $attributes as $k => $v ) {
+				$attributes_html .= esc_attr( $k ) . '="' . esc_attr( $v ) . '"';
+			}
+			$image_html_with_data = str_replace( '<img ', "<img $attributes_html", $image_html );
+			$content = str_replace( $image_html, $image_html_with_data, $content );
+		}
+		$this->enqueue_assets();
+		return $content;
+	}
+
+	function add_data_to_images( $attr, $attachment = null ) {
 		$attachment_id   = intval( $attachment->ID );
 		$orig_file       = wp_get_attachment_image_src( $attachment_id, 'full' );
 		$orig_file       = isset( $orig_file[0] ) ? $orig_file[0] : wp_get_attachment_url( $attachment_id );
@@ -340,6 +422,7 @@ class Jetpack_Carousel {
 		$img_meta = json_encode( array_map( 'strval', $img_meta ) );
 
 		$attr['data-attachment-id']     = $attachment_id;
+		$attr['data-permalink']         = esc_attr( get_permalink( $attachment->ID ) );
 		$attr['data-orig-file']         = esc_attr( $orig_file );
 		$attr['data-orig-size']         = $size;
 		$attr['data-comments-opened']   = $comments_opened;