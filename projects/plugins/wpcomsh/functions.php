efe52b47fd35628329383b93f609b05adac7629a
Konstantin Obenland <obenland@gmx.de>
2016-12-08 11:40:54 +0000

    Simplifies functions and adds docs.

--- projects/plugins/wpcomsh/functions.php
+++ projects/plugins/wpcomsh/functions.php
@@ -15,11 +15,23 @@ function jetpress_is_maybe_wpcom_theme( $theme_slug ) {
 	return substr( $theme_slug, -6 ) === '-wpcom';
 }
 
+/**
+ * Whether the theme is a wpcom theme.
+ *
+ * @param string $theme_slug Theme slug.
+ * @return bool
+ */
 function jetpress_is_wpcom_theme( $theme_slug ) {
 	return jetpress_is_wpcom_premium_theme( $theme_slug ) ||
 		   jetpress_is_wpcom_pub_theme( $theme_slug );
 }
 
+/**
+ * Whether the theme is a premium wpcom theme.
+ *
+ * @param string $theme_slug Theme slug.
+ * @return bool
+ */
 function jetpress_is_wpcom_premium_theme( $theme_slug ) {
 	if (
 		! defined( 'JETPRESS_WPCOM_PREMIUM_THEMES_PATH' ) ||
@@ -33,11 +45,15 @@ function jetpress_is_wpcom_premium_theme( $theme_slug ) {
 		return false;
 	}
 
-	$theme_dir_path = JETPRESS_WPCOM_PREMIUM_THEMES_PATH . "/${theme_slug}";
-
-	return file_exists( $theme_dir_path );
+	return file_exists( JETPRESS_WPCOM_PREMIUM_THEMES_PATH . "/${theme_slug}" );
 }
 
+/**
+ * Whether the theme is a free wpcom theme.
+ *
+ * @param string $theme_slug Theme slug.
+ * @return bool
+ */
 function jetpress_is_wpcom_pub_theme( $theme_slug ) {
 	if (
 		! defined( 'JETPRESS_WPCOM_PUB_THEMES_PATH' ) ||
@@ -51,22 +67,16 @@ function jetpress_is_wpcom_pub_theme( $theme_slug ) {
 		return false;
 	}
 
-	$theme_dir_path = JETPRESS_WPCOM_PUB_THEMES_PATH . "/${theme_slug}";
-
-	return file_exists( $theme_dir_path );
+	return file_exists( JETPRESS_WPCOM_PUB_THEMES_PATH . "/${theme_slug}" );
 }
 
-function jetpress_get_parent_theme_slug( $theme_slug ) {
-	$theme_obj = wp_get_theme( $theme_slug );
-
-	if ( is_wp_error( $theme_obj ) ) {
-		return $theme_obj;
-	}
-
-	return $theme_obj->parent();
-
-}
-
+/**
+ * Symlinks a wpcom theme.
+ *
+ * @param string $theme_slug
+ * @param string $theme_type
+ * @return bool|WP_Error
+ */
 function jetpress_symlink_theme( $theme_slug, $theme_type ) {
 	$themes_source_path = '';
 
@@ -116,33 +126,28 @@ function jetpress_delete_theme_cache( $theme_slug = null ) {
 /**
  * Checks whether a theme (by theme slug) is symlinked in the themes' directory.
  *
- * @param string $theme_slug the slug of a theme
- *
- * @return bool whether a theme is symlinked in the themes' directory
+ * @param string $theme_slug The slug of a theme.
+ * @return bool Whether a theme is symlinked in the themes' directory.
  */
 function jetpress_is_theme_symlinked( $theme_slug ) {
-	$site_themes_dir_path = get_theme_root();
-	$symlinked_theme_dir_path = $site_themes_dir_path . "/{$theme_slug}";
+	$theme_root  = get_theme_root();
+	$theme_dir   = "$theme_root/$theme_slug";
+	$site_themes = scandir( $theme_root );
 
-	$site_themes = scandir( $site_themes_dir_path );
-
-	if (
-		! in_array( $theme_slug, $site_themes ) ||
-		! is_link( $symlinked_theme_dir_path )
-	) {
-		return false;
-	}
-
-	return true;
+	return in_array( $theme_slug, $site_themes ) && is_link( $theme_dir );
 }
 
+/**
+ * Deletes a symlinked theme.
+ *
+ * @param string $theme_slug The slug of a theme.
+ * @return bool|WP_Error True on success, WP_Error on error.
+ */
 function jetpress_delete_symlinked_theme( $theme_slug ) {
-	$site_themes_dir_path = get_theme_root();
+	$theme_dir = get_theme_root() . "/$theme_slug";
 
-	$symlinked_theme_path = $site_themes_dir_path . '/' . $theme_slug;
-
-	if ( file_exists( $symlinked_theme_path ) && is_link( $symlinked_theme_path ) ) {
-		unlink( $symlinked_theme_path );
+	if ( file_exists( $theme_dir ) && is_link( $theme_dir ) ) {
+		unlink( $theme_dir );
 
 		return true;
 	}
@@ -157,6 +162,12 @@ function jetpress_delete_symlinked_theme( $theme_slug ) {
 	);
 }
 
+/**
+ * Returns a theme type.
+ *
+ * @param string $theme_slug The slug of a theme.
+ * @return false|string Theme type or false if not a wpcom theme.
+ */
 function jetpress_get_wpcom_theme_type( $theme_slug ) {
 	if ( jetpress_is_wpcom_premium_theme( $theme_slug ) ) {
 		return JETPRESS_WPCOM_PREMIUM_THEME_TYPE;
@@ -179,19 +190,23 @@ function jetpress_is_wpcom_child_theme( $theme_slug = null ) {
 	return $theme->get_stylesheet() !== $theme->get_template();
 }
 
-function jetpress_symlink_parent_theme( $child_theme_slug ) {
-	$child_theme_obj = wp_get_theme( $child_theme_slug );
-	$parent_theme_slug = $child_theme_obj->get( 'Template' );
+/**
+ * Symlinks the theme's parent if it's a child theme.
+ *
+ * @param string $stylesheet Theme slug.
+ * @return bool|WP_Error
+ */
+function jetpress_symlink_parent_theme( $stylesheet ) {
+	$theme    = wp_get_theme( $stylesheet );
+	$template = $theme->get_template();
 
-	if ( ! $parent_theme_slug ) {
+	if ( $template === $stylesheet ) {
 		error_log( "Jetpress: Can't symlink parent theme. Current theme is not a child theme." );
 
 		return false;
 	}
 
-	$parent_theme_type = jetpress_get_wpcom_theme_type( $parent_theme_slug );
+	error_log( 'Jetpress: Symlinking parent theme.' );
 
-	error_log('gonna symlink parent theme');
-
-	return jetpress_symlink_theme( $parent_theme_slug, $parent_theme_type );
+	return jetpress_symlink_theme( $template, jetpress_get_wpcom_theme_type( $template ) );
 }
