1b771745bb7a217c937502767c7fbf1d46db1cd9
Rastislav Lamos <lamos.rasto@gmail.com>
2016-12-08 14:06:14 +0000

    Export WPCom pub/premium themes paths constants to private-constants.php
    ...and put `private-constants.php` file to `.gitignore` so anyone can
    define their own paths to these themes.
    
    Props @obenland for the suggestion.

--- projects/plugins/wpcomsh/constants.php
+++ projects/plugins/wpcomsh/constants.php
@@ -1,8 +1,15 @@
 <?php
 
-// TODO: update to the real paths after getting an AT Pressable testing site
-define( 'JETPRESS_WPCOM_PUB_THEMES_PATH', '/srv/www/wpcom-pub-themes' );
-define( 'JETPRESS_WPCOM_PREMIUM_THEMES_PATH', '/srv/www/wpcom-premium-themes' );
+/**
+ * You also need to define two more constants in the `private-constants.php` file.
+ * These constants will point to WPCom pub (free) and premium themes.
+ * Example of `private-constants.php` file:
+ *
+ * define( 'JETPRESS_WPCOM_PUB_THEMES_PATH', '/srv/www/wpcom-pub-themes' );
+ * define( 'JETPRESS_WPCOM_PREMIUM_THEMES_PATH', '/srv/www/wpcom-premium-themes' );
+ *
+ */
+require_once( 'private-constants.php' );
 
 define( 'JETPRESS_WPCOM_PUB_THEME_TYPE', 'wpcom_pub_theme_type' );
 define( 'JETPRESS_WPCOM_PREMIUM_THEME_TYPE', 'wpcom_premium_theme_type' );
