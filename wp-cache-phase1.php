1cb160d83e82801b23ae622b92d2564f61804971
Weston Ruter <weston@xwp.co>
2016-12-08 10:55:34 -0800

    Disable caching in customize preview

--- wp-cache-phase1.php
+++ wp-cache-phase1.php
@@ -47,8 +47,9 @@ if ( isset( $wp_cache_make_known_anon ) && $wp_cache_make_known_anon )
 
 do_cacheaction( 'cache_init' );
 
-if (!$cache_enabled || $_SERVER["REQUEST_METHOD"] == 'POST')
+if ( ! $cache_enabled || $_SERVER["REQUEST_METHOD"] == 'POST' || isset( $_GET['customize_changeset_uuid'] ) ) {
 	return true;
+}
 
 $file_expired = false;
 $cache_filename = '';
