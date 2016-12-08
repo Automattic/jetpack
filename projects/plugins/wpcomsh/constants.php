01790ca556f4294f2a21e07fc55ee7c2b9a83a3b
Rastislav Lamos <lamos.rasto@gmail.com>
2016-12-08 15:59:36 +0000

    Constants: fall back to some folder if WPCom themes paths are not defined
    This prevents the plugin to crash. It will produce an error to error
    logs though.

--- projects/plugins/wpcomsh/constants.php
+++ projects/plugins/wpcomsh/constants.php
@@ -9,7 +9,21 @@
  * define( 'JETPRESS_WPCOM_PREMIUM_THEMES_PATH', '/srv/www/wpcom-premium-themes' );
  *
  */
-require_once( 'private-constants.php' );
+
+$current_dir = dirname( __FILE__ );
+
+if ( file_exists( "$current_dir/private-constants.php" ) ) {
+	require_once( 'private-constants.php' );
+} else {
+	// This won't work. Just a fallback so functions in this plugin return false instead of warning/error.
+	define( 'JETPRESS_WPCOM_PUB_THEMES_PATH', $current_dir );
+	define( 'JETPRESS_WPCOM_PREMIUM_THEMES_PATH', $current_dir );
+
+	error_log(
+		'Jetpress: define JETPRESS_WPCOM_PUB_THEMES_PATH and JETPRESS_WPCOM_PREMIUM_THEMES_PATH ' .
+		'so they point to the correct WPCom themes paths.'
+	);
+}
 
 define( 'JETPRESS_WPCOM_PUB_THEME_TYPE', 'wpcom_pub_theme_type' );
 define( 'JETPRESS_WPCOM_PREMIUM_THEME_TYPE', 'wpcom_premium_theme_type' );
---
+++ projects/plugins/wpcomsh/private-constantss.php
@@ -0,0 +1,4 @@
+<?php
+
+define( 'JETPRESS_WPCOM_PUB_THEMES_PATH', '/srv/www/wpcom-pub-themes' );
+define( 'JETPRESS_WPCOM_PREMIUM_THEMES_PATH', '/srv/www/wpcom-premium-themes' );
