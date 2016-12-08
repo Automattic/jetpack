b479e5b11f7bb658a4c2e68e31d9410a332a60cc
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2016-12-08 16:11:02 -0300

    Updates module coding style and removes business check function

--- modules/google-analytics.php
+++ modules/google-analytics.php
@@ -14,27 +14,9 @@
 /**
  * Removes the Google Analytics plugin settings page
  */
-function removeSettingsPage() {
+function remove_settings_page() {
 	remove_submenu_page( 'options-general.php', 'wp-google-analytics' );
 }
 
-/**
- * Does the site have a Jetpack plan attached to it that includes Google Analytics?
- *
- * @return bool
- */
-function isGoogleAnalyticsIncludedInJetpackPlan() {
-	$site_id = Jetpack_Options::get_option( 'id' );
-	$result  = Jetpack_Client::wpcom_json_api_request_as_blog( sprintf( '/sites/%d', $site_id ), '1.1' );
-
-	if ( is_wp_error( $result ) ) {
-		return false;
-	}
-
-	$response = json_decode( $result['body'], true );
-
-	return $response['plan']['product_slug'] == 'jetpack_business';
-}
-
 include dirname( __FILE__ ) . "/google-analytics/wp-google-analytics.php";
-add_action( 'admin_menu', 'removeSettingsPage' );
+add_action( 'admin_menu', 'remove_settings_page' );
