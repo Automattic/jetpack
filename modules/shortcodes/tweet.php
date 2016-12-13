94485fd4440dc5f513b3058c906e0c990473c1a4
Elio Rivero <eliorivero@gmail.com>
2016-12-13 12:12:17 -0300

    Shortcodes: if it's missing http_response or if there's not a tweet URL resolved, cache the request for 15 min to avoid making the failing request multiple times

--- modules/shortcodes/tweet.php
+++ modules/shortcodes/tweet.php
@@ -63,11 +63,17 @@ class Jetpack_Tweet {
 					return '';
 				}
 				if ( ! isset( $response['http_response'] ) ) {
+
+					// Cache for 15 minutes to avoid making the request over and over.
+					set_transient( $transient, '', 60 * 15 );
 					return '';
 				}
 				$http_response = $response['http_response'];
 				$http_response_object = $http_response->get_response_object();
 				if ( empty( $http_response_object->url ) ) {
+
+					// Cache for 15 minutes to avoid making the request over and over.
+					set_transient( $transient, '', 60 * 15 );
 					return '';
 				}
 				$cached_url = isset( $http_response_object->url ) && ! empty( $http_response_object->url )
