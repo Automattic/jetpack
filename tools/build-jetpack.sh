c105464550c0e917aadb76f6a597a673433e42c4
Igor Zinovyev <zinigor@gmail.com>
2016-12-14 17:42:20 +0300

    Added beta versioning option for the build script.

--- tools/build-jetpack.sh
+++ tools/build-jetpack.sh
@@ -12,16 +12,20 @@ OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
 # Initialize our own variables:
 CLEANUP=0
+ADD_BETA_VERSION=0
 
-while getopts "d" opt; do
+while getopts "db" opt; do
     case "$opt" in
     d)  CLEANUP=1
+        ;;
+    b)  ADD_BETA_VERSION=1
         ;;
     esac
 done
 
 shift $((OPTIND-1))
 
+GET_VERSION_SCRIPT="`pwd`/tools/get-version.sh"
 TARGET_BRANCH=${1:-master}
 TARGET_DIR=${2:-"/tmp/jetpack"}
 
@@ -33,9 +37,21 @@ if [[ $CLEANUP -eq 1 ]]; then
     echo "Done!"
 fi
 
-git clone --branch $TARGET_BRANCH --depth 1 git@github.com:Automattic/jetpack.git $TARGET_DIR
+git clone \
+    --branch $TARGET_BRANCH \
+    --depth 1000 \
+    https://github.com/Automattic/jetpack.git \
+    $TARGET_DIR
 
 cd $TARGET_DIR
+
+if [[ $ADD_BETA_VERSION -eq 1 ]]; then
+    echo "Changing version of the Jetpack to reflect the latest changes.."
+    CURRENT_VERSION="`$GET_VERSION_SCRIPT`"
+
+    sed -i -e 's/Version: .*$/Version: '$CURRENT_VERSION'/' jetpack.php
+    echo "Now at version $CURRENT_VERSION!"
+fi
 
 yarn
 yarn build-production
