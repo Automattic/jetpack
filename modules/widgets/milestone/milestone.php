51b75a1db79253ddfff074f8917365b1523e4ec2
Elio Rivero <eliorivero@gmail.com>
2016-12-15 15:33:02 -0300

    Widgets: always load Milestone front end stylesheet so it's stylized when it's first added and previewed in Customizer

--- modules/widgets/milestone/milestone.php
+++ modules/widgets/milestone/milestone.php
@@ -60,7 +60,7 @@ class Milestone_Widget extends WP_Widget {
 		add_action( 'admin_enqueue_scripts', array( __class__, 'enqueue_admin' ) );
 		add_action( 'wp_footer', array( $this, 'localize_script' ) );
 
-		if ( is_active_widget( false, false, $this->id_base, true ) || is_active_widget( false, false, 'monster', true ) ) {
+		if ( is_active_widget( false, false, $this->id_base, true ) || is_active_widget( false, false, 'monster', true ) || is_customize_preview() ) {
 			add_action( 'wp_head', array( __class__, 'styles_template' ) );
 		}
 	}
