cc6bd016c9944c62e553592408f1f34708006796
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-12-20 09:42:10 -0800

    Fix broken test.

--- json-endpoints/jetpack/class.jetpack-json-api-translations-endpoint.php
+++ json-endpoints/jetpack/class.jetpack-json-api-translations-endpoint.php
@@ -3,12 +3,12 @@
 // Translations
 class Jetpack_JSON_API_Translations_Endpoint extends Jetpack_JSON_API_Endpoint {
 	// GET /sites/%s/translations
+	// POST /sites/%s/translations
 	// POST /sites/%s/translations/update
 	protected $needed_capabilities = array( 'update_core', 'update_plugins', 'update_themes' );
 	protected $log;
 
 	public function result() {
-		error_log( 'show resulsts...' );
 		return array(
 			'translations' => wp_get_translation_updates(),
 			'autoupdate' => Jetpack_Options::get_option( 'autoupdate_translations', false ),
@@ -16,4 +16,3 @@ class Jetpack_JSON_API_Translations_Endpoint extends Jetpack_JSON_API_Endpoint {
 		);
 	}
 }
-
