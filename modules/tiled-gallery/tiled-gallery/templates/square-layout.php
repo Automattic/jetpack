ed3be9077267c128f651ca98fe37be904921a82e
Nathan Bloomfield <nbloomf@gmail.com>
2016-12-30 12:16:57 -0600

    Fix style errors

--- modules/tiled-gallery/tiled-gallery/templates/square-layout.php
+++ modules/tiled-gallery/tiled-gallery/templates/square-layout.php
@@ -1,12 +1,12 @@
 <?php
-foreach ( $rows as $row ): ?>
-	<div class="gallery-row<?php if ( 1 === count( $row->images ) ) echo ' gallery-singleton-row' ?>"
+foreach ( $rows as $row ) : ?>
+	<div class="gallery-row<?php if ( 1 === count( $row->images ) ) { echo ' gallery-singleton-row'; } ?>"
 		style="width: <?php echo esc_attr( $row->width ); ?>px; height: <?php echo esc_attr( $row->height ); ?>px;"
 		data-original-width="<?php echo esc_attr( $row->width ); ?>"
 		data-original-height="<?php echo esc_attr( $row->height ); ?>"
 	>
 		<?php $add_link = 'none' !== $link; ?>
-		<?php foreach ( $row->images as $item ): ?>
+		<?php foreach ( $row->images as $item ) : ?>
 			<div class="gallery-group"
 				style="width: <?php echo esc_attr( $row->group_size ); ?>px; height: <?php echo esc_attr( $row->group_size ); ?>px;"
 				data-original-width="<?php echo esc_attr( $row->group_size ); ?>"
