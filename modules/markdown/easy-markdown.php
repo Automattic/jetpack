bd6ac5e61a4105ecacacbebbaf73fa24a7beaff2
George Stephanis <georgestephanis@automattic.com>
2017-01-10 20:14:34 +0000

    Markdown Bug Fixes and Updates. Also, Jetpack_Options plan sync.
    This should bring four files into sync with Jetpack's versions. Hurrah!
    
    @see https://[private link]
    
    Merges r148841-wpcom.

--- modules/markdown/easy-markdown.php
+++ modules/markdown/easy-markdown.php
@@ -16,7 +16,7 @@ Author URI: http://automattic.com/
  * http://www.opensource.org/licenses/gpl-license.php
  *
  * This is an add-on for WordPress
- * http://wordpress.org/
+ * https://wordpress.org/
  *
  * **********************************************************************
  * This program is free software; you can redistribute it and/or modify
@@ -311,7 +311,7 @@ class WPCom_Markdown {
 	 * @return boolean
 	 */
 	public function is_posting_enabled() {
-		return (bool) get_option( self::POST_OPTION, '' );
+		return (bool) Jetpack_Options::get_option_and_ensure_autoload( self::POST_OPTION, '' );
 	}
 
 	/**
@@ -319,7 +319,7 @@ class WPCom_Markdown {
 	 * @return boolean
 	 */
 	public function is_commenting_enabled() {
-		return (bool) get_option( self::COMMENT_OPTION, '' );
+		return (bool) Jetpack_Options::get_option_and_ensure_autoload( self::COMMENT_OPTION, '' );
 	}
 
 	/**
