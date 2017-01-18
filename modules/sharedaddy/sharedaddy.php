0ffc176a5ec70763699b0455e23cb4e9debabad9
Clifton Griffin <clifgriffin@gmail.com>
2017-01-18 14:54:28 -0500

    When sharing via email, filter the share URL through get_share_url() like other sharing modules.

--- modules/sharedaddy/sharedaddy.php
+++ modules/sharedaddy/sharedaddy.php
@@ -68,7 +68,14 @@ function sharing_email_send_post_content( $data ) {
 	$content  = sprintf( __( '%1$s (%2$s) thinks you may be interested in the following post:', 'jetpack' ), $data['name'], $data['source'] );
 	$content .= "\n\n";
 	$content .= $data['post']->post_title."\n";
-	$content .= get_permalink( $data['post']->ID )."\n";
+	
+	// Filtered share URL. Allows backwards compatibility.
+	if ( isset( $data['share_url'] ) && ! empty( $data['share_url'] ) ) {
+		$content .= $data['share_url'] . "\n";
+	} else {
+		$content .= get_permalink( $data['post']->ID )."\n";
+	}
+
 	return $content;
 }
 
