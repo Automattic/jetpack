6d904b0e9b9c41f88bccca368d90ba9ae0c5e43a
Justin Cady <themoonisdown@users.noreply.github.com>
2017-01-21 11:28:54 -0500

    Use read/following/mine REST API for Blogs I Follow in Jetpack
    The API is used to retrieve the sites the user is following, then the data is
    converted into a format the plugin can use.

--- modules/widgets/blogs-i-follow/interface-blogs-i-follow-datastore.php
+++ modules/widgets/blogs-i-follow/interface-blogs-i-follow-datastore.php
@@ -7,24 +7,57 @@
  * leaving the data particulars to each flavor.
  */
 interface iBlogs_I_Follow_Datastore {
-	public function get_followed_blogs($args);
+	public function get_followed_blogs( $args );
 }
 
 class Blogs_I_Follow_Jetpack_Datastore implements iBlogs_I_Follow_Datastore {
-	public function get_followed_blogs($args) {
+	/**
+	 * Converts data from the WordPress.com REST API into a format usable by the plugin
+	 *
+	 * The read/following/mine API is not identical in its contents or format to the private
+	 * WPCOM tables used for Blogs I Follow. The data must be translated into a format that
+	 * loosely conforms to what the plugin expects.
+	 */
+	private function convert_rest_subscription( $subscription ) {
 		return array(
-			0 => array(
-				'id' => '304636440',
-				'blog_id' => '1537283',
-				'blog_name' => 'WordPress.com',
-				'blog_url' => 'www.wordpress.com/',
-				'date_subscribed' => '2016-10-21 17:42:03',
-				'site_id' => '2',
-				'public' => '1',
-				'feed_url' => 'http://www.wordpress.com/feed/rss2',
-				'feed_id' => '8903852',
-			),
+			'id' => $subscription->ID,
+			'blog_id' => $subscription->blog_ID,
+			'blog_url' => $subscription->URL,
+			'feed_url' => $subscription->URL,
+			'date_subscribed' => $subscription->date_subscribed,
 		);
+	}
+
+	/**
+	 * Retrieve the user's followed blogs from the WordPress.com REST API
+	 */
+	public function get_followed_blogs($args) {
+		$request_args = array(
+			'url' => 'https://public-api.wordpress.com/rest/v1.1/read/following/mine',
+			'user_id' => JETPACK_MASTER_USER,
+			'method' => 'GET',
+		);
+		$response = Jetpack_Client::remote_request( $request_args );
+		// TODO: Remove the placeholder false value
+		if ( false && is_wp_error( $response ) ) {
+			// TODO: Handle error appropriately
+			return array();
+		} else {
+			// TODO: Enable this and remove the dummy data once REST API authentication is working
+			/*
+			$response_body = wp_remote_retrieve_body( $response );
+
+			if ( empty( $response_body ) ) {
+				return array();
+			}
+			*/
+			$response_body = '{"subscriptions":[{"ID":"324825249","blog_ID":"114798305","URL":"http:\/\/design.blog","date_subscribed":"2017-01-17T03:12:32+00:00","meta":{"links":{"site":"https:\/\/public-api.wordpress.com\/rest\/v1\/sites\/114798305"}}},{"ID":"324824892","blog_ID":"0","URL":"http:\/\/daringfireball.net\/feeds\/main","date_subscribed":"2017-01-17T03:09:48+00:00","meta":{"links":{"feed":"https:\/\/public-api.wordpress.com\/rest\/v1\/read\/feed\/20787116"}}},{"ID":"324823266","blog_ID":"122690821","URL":"http:\/\/followtesting.wordpress.com","date_subscribed":"2017-01-17T02:57:51+00:00","meta":{"links":{"site":"https:\/\/public-api.wordpress.com\/rest\/v1\/sites\/122690821"}}}]}';
+
+			$response_body = json_decode( $response_body );
+			$followed_blogs = array_map( array( $this, 'convert_rest_subscription' ), $response_body->subscriptions );
+
+			return $followed_blogs;
+		}
 	}
 }
 
