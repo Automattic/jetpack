6b5698f7d6c3e5e3ad72a0b95093950420f1de9e
Justin Cady <themoonisdown@users.noreply.github.com>
2017-01-23 20:04:30 -0500

    Remove unused functions from compat.php in Blogs I Follow

--- modules/widgets/blogs-i-follow/compat.php
+++ modules/widgets/blogs-i-follow/compat.php
@@ -2,9 +2,7 @@
 // TODO: After everything is encapsulated by the WPCOM/Jetpack interface,
 // this file can be safely deleted. None of the undefined functions should
 // be invoked so long as the Jetpack plugin never believes it is WPCOM.
-function wpcom_subs_get_blogs() {}
 function enable_follow_buttons() {}
-function get_blog_locale() {}
 if ( ! function_exists( 'get_blog_option' ) ) :
 function get_blog_option() {
 	return 'ok';
