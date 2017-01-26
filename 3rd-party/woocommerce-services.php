f7c1aa291c658d0102fdbb0054bce47aaaa48814
Jeff Stieler <jeff.m.stieler@gmail.com>
2017-01-26 13:02:07 -0700

    Show an error message if the WooCommerce Services install fails, change entry hook to `admin_init`.

--- 3rd-party/woocommerce-services.php
+++ 3rd-party/woocommerce-services.php
@@ -6,18 +6,43 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class WC_Services_Installer {
 
+	public function init() {
+		$this->add_error_notice();
+		$this->try_install();
+	}
+
 	public function try_install() {
 		$action = filter_input( INPUT_GET, 'wc-services-action' );
 
 		if ( 'install' === $action ) {
 			check_admin_referer( 'wc-services-install' );
 
-			$this->install();
+			$result = $this->install();
 
-			wp_safe_redirect( remove_query_arg( array( 'wc-services-action', '_wpnonce' ) ) );
+			wp_safe_redirect( add_query_arg( array(
+				'wc-services-action'        => false,
+				'_wpnonce'                  => false,
+				'wc-services-install-error' => ( false === $result ),
+			) ) );
 
 			exit;
 		}
+	}
+
+	public function add_error_notice() {
+		$error = filter_input( INPUT_GET, 'wc-services-install-error' );
+
+		if ( $error ) {
+			add_action( 'admin_notices', array( $this, 'error_notice' ) );
+		}
+	}
+
+	public function error_notice() {
+		?>
+		<div class="notice notice-error is-dismissible">
+			<p><?php _e( 'There was an error installing WooCommerce Services.', 'jetpack' ); ?></p>
+		</div>
+		<?php
 	}
 
 	private function install() {
@@ -27,29 +52,27 @@ class WC_Services_Installer {
 		include_once( ABSPATH . '/wp-admin/includes/class-wp-upgrader.php' );
 		include_once( ABSPATH . '/wp-admin/includes/class-plugin-upgrader.php' );
 
-		$api = plugins_api( 'plugin_information', array(
-			'slug'   => 'connect-for-woocommerce',
-		) );
+		$api = plugins_api( 'plugin_information', array( 'slug' => 'connect-for-woocommerce' ) );
 
 		if ( is_wp_error( $api ) ) {
-			wp_die( $api );
+			return false;
 		}
 
 		$upgrader = new Plugin_Upgrader( new Automatic_Upgrader_Skin() );
 		$result   = $upgrader->install( $api->download_link );
 
-		if ( is_wp_error( $result ) ) {
-			wp_die( $result );
+		if ( true !== $result ) {
+			return false;
 		}
 
 		$result = activate_plugin( 'connect-for-woocommerce/woocommerce-connect-client.php' );
 
 		if ( is_wp_error( $result ) ) {
-			wp_die( $result );
+			return false;
 		}
 	}
 }
 
 $wc_services_installer = new WC_Services_Installer();
 
-add_action( 'init', array( $wc_services_installer, 'try_install' ) );
+add_action( 'admin_init', array( $wc_services_installer, 'init' ) );
