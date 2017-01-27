8c2a105fad22e885d0600612257b47b08e619fb6
Igor Zinovyev <zinigor@gmail.com>
2017-01-27 16:46:34 +0300

    Added a proof of concept banner to the Antispam settings.

--- _inc/client/security/antispam.jsx
+++ _inc/client/security/antispam.jsx
@@ -2,18 +2,29 @@
  * External dependencies
  */
 import React from 'react';
+import { connect } from 'react-redux';
 import { translate as __ } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
+import Banner from 'components/banner';
+import {
+	FEATURE_SPAM_AKISMET_PLUS,
+	PLAN_JETPACK_PREMIUM,
+	getPlanClass
+} from 'lib/plans/constants';
 import { FormFieldset } from 'components/forms';
 import { ModuleSettingsForm as moduleSettingsForm } from 'components/module-settings/module-settings-form';
 import { ModuleSettingCheckbox } from 'components/module-settings/form-components';
 import SettingsCard from 'components/settings-card';
 import SettingsGroup from 'components/settings-group';
+import {
+	getSitePlan,
+	isFetchingSiteData
+} from 'state/site';
 
-export const Antispam = moduleSettingsForm(
+const Antispam = moduleSettingsForm(
 	React.createClass( {
 
 		toggleModule( name, value ) {
@@ -21,6 +32,17 @@ export const Antispam = moduleSettingsForm(
 		},
 
 		render() {
+			let planClass = getPlanClass( this.props.sitePlan.product_slug );
+
+			let banner = (
+				<Banner
+					feature={ FEATURE_SPAM_AKISMET_PLUS }
+					title={ __( 'Spam protection is a paid feature.' ) }
+					description={ __( 'Pretty descriptive description.' ) }
+					callToAction={ __( 'Upgrade to get rid of that pesky spam.' ) }
+					plan={ PLAN_JETPACK_PREMIUM }
+				/>
+			);
 			return (
 				<SettingsCard
 					{ ...this.props }
@@ -33,8 +55,18 @@ export const Antispam = moduleSettingsForm(
 								label={ __( 'Show the number of approved comments beside each comment author' ) } />
 						</FormFieldset>
 					</SettingsGroup>
+					{ 'is-free-plan' === planClass && banner }
 				</SettingsCard>
 			);
 		}
 	} )
 );
+
+export default connect(
+	( state ) => {
+		return {
+			sitePlan: getSitePlan( state ),
+			fetchingSiteData: isFetchingSiteData( state )
+		};
+	}
+)( Antispam );
