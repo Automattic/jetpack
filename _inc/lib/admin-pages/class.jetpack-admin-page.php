9d5b9cadbc24999528be055d5d5725ffb4f77e4a
Derek Springer <derekspringer@gmail.com>
2017-01-31 16:59:14 -0800

    Ads: Deactivate module if plan changes (#6242)
    * Deactivate Ads if plan changes to no longer support it
    
    * Updated to use $to_deactivate instead of deactivate_module

--- _inc/lib/admin-pages/class.jetpack-admin-page.php
+++ _inc/lib/admin-pages/class.jetpack-admin-page.php
@@ -216,11 +216,11 @@ abstract class Jetpack_Admin_Page {
 				$active = Jetpack::get_active_modules();
 				switch ( $current->plan->product_slug ) {
 					case 'jetpack_free':
-						$to_deactivate = array( 'seo-tools', 'videopress', 'google-analytics' );
+						$to_deactivate = array( 'seo-tools', 'videopress', 'google-analytics', 'wordads' );
 						break;
 					case 'jetpack_personal':
 					case 'jetpack_personal_monthly':
-						$to_deactivate = array( 'seo-tools', 'videopress', 'google-analytics' );
+						$to_deactivate = array( 'seo-tools', 'videopress', 'google-analytics', 'wordads' );
 						break;
 					case 'jetpack_premium':
 					case 'jetpack_premium_monthly':
