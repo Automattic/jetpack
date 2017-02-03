0d5b1d012a3ab2ed8514137f76feff7aca6820b9
donncha <donncha@gmail.com>
2017-02-03 15:54:52 +0000

    Improve detection of multisite installs
    Fixes #97
    We should check for other constants to detect multisite installs.

--- wp-cache-base.php
+++ wp-cache-base.php
@@ -5,7 +5,7 @@ $WPSC_HTTP_HOST = htmlentities( $_SERVER[ 'HTTP_HOST' ] );
 
 // We want to be able to identify each blog in a WordPress MU install
 $blogcacheid = '';
-if ( defined( 'VHOST' ) || ( defined( 'WP_ALLOW_MULTISITE' ) && constant( 'WP_ALLOW_MULTISITE' ) == true ) ) {
+if ( ( defined( 'WP_ALLOW_MULTISITE' ) && constant( 'WP_ALLOW_MULTISITE' ) == true ) || defined( 'SUBDOMAIN_INSTALL' ) || defined( 'VHOST' ) || defined( 'SUNRISE' ) ) {
 	$blogcacheid = 'blog'; // main blog
 	if( defined( 'SUBDOMAIN_INSTALL' ) && constant( 'SUBDOMAIN_INSTALL' ) == true ) {
 		$blogcacheid = $WPSC_HTTP_HOST;
