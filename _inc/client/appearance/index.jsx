fbedc714c11fdb279af73e7eb51a2ea672ccc3b0
Mark George <markg@automattic.com>
2017-02-07 06:41:09 +0000

    Don't use Initial_State to fetch holiday snow option visibility

--- _inc/client/appearance/index.jsx
+++ _inc/client/appearance/index.jsx
@@ -23,6 +23,7 @@ import {
 	getModule as _getModule,
 	getModules
 } from 'state/modules';
+import { getShowHolidaySnow } from 'state/settings';
 import { ModuleToggle } from 'components/module-toggle';
 import { AllModuleSettings } from 'components/module-settings/modules-per-tab-page';
 import { isUnavailableInDevMode } from 'state/connection';
@@ -34,7 +35,8 @@ export const Page = ( props ) => {
 		toggleModule,
 		isModuleActivated,
 		isTogglingModule,
-		getModule
+		getModule,
+		showHolidaySnow
 	} = props,
 		isAdmin = props.userCanManageModules,
 		moduleList = Object.keys( props.moduleList );
@@ -90,7 +92,7 @@ export const Page = ( props ) => {
 		);
 	} );
 
-	let holidaySnowCard = Initial_State.showHolidaySnow ? (
+	let holidaySnowCard = showHolidaySnow ? (
 		<FoldableCard
 			header={ __( 'Holiday Snow' ) }
 			subheader={ __( 'Show falling snow in the holiday period.' ) }
@@ -135,7 +137,8 @@ export default connect(
 			getModule: ( module_name ) => _getModule( state, module_name ),
 			isUnavailableInDevMode: ( module_name ) => isUnavailableInDevMode( state, module_name ),
 			userCanManageModules: userCanManageModules( state ),
-			moduleList: getModules( state )
+			moduleList: getModules( state ),
+			showHolidaySnow: getShowHolidaySnow( state )
 		};
 	},
 	( dispatch ) => {
