d9bc6cf8e1001eaf83e8e7e26059a8a286471c81
Brian Colinger <briancolinger@gmail.com>
2017-02-09 16:20:46 -0600

    Pass the permalink through the sharing_permalink filter
    Fixes issue #5989.
    Pass the permalink through the sharing_permalink filter.

--- modules/sharedaddy/sharedaddy.php
+++ modules/sharedaddy/sharedaddy.php
@@ -68,7 +68,7 @@ function sharing_email_send_post_content( $data ) {
 	$content  = sprintf( __( '%1$s (%2$s) thinks you may be interested in the following post:', 'jetpack' ), $data['name'], $data['source'] );
 	$content .= "\n\n";
 	$content .= $data['post']->post_title."\n";
-	$content .= get_permalink( $data['post']->ID )."\n";
+	$content .= apply_filters( 'sharing_permalink', get_permalink( $data['post']->ID ) ) . "\n";
 	return $content;
 }
 
