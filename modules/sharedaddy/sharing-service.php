87965a6c94175d6c5c3437c68396c7b9e1439f22
Mark George <markg@automattic.com>
2017-02-10 03:56:01 +0000

    Don't clobber share settings when button_styles are missing; merge default global settings with custom settings to prevent overwriting custom share details

--- modules/sharedaddy/sharing-service.php
+++ modules/sharedaddy/sharing-service.php
@@ -176,7 +176,8 @@ class Sharing_Service {
 		 * @see https://github.com/Automattic/jetpack/issues/6121
 		 */
 		if ( ! is_array( $options ) || ! isset( $options['button_style'], $options['global'] ) ) {
-			$options = array( 'global' => $this->get_global_options() );
+			$global_options = $this->get_global_options();
+			$options = array_merge( is_array( $options ) ? $options : array(), $global_options );
 		}
 
 		$global = $options['global'];
