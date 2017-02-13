a515d1ec12f2879b50f7c560d9310fe8262c05d4
Dan Roundhill <dan@a8c.com>
2017-02-13 10:46:24 -0800

    Adding doc comment for parser.

--- json-endpoints/jetpack/class.jetpack-json-api-plugins-endpoint.php
+++ json-endpoints/jetpack/class.jetpack-json-api-plugins-endpoint.php
@@ -133,6 +133,7 @@ abstract class Jetpack_JSON_API_Plugins_Endpoint extends Jetpack_JSON_API_Endpoi
 	protected function get_plugins() {
 		// Do the admin_init action in order to capture plugin action links.
 		// See get_plugin_action_links()
+		/** This action is documented in wp-admin/admin.php */
 		do_action( 'admin_init' );
 		$plugins = array();
 		/** This filter is documented in wp-admin/includes/class-wp-plugins-list-table.php */
