c48c358a87fb93ed17d4c45178979db8d09aaf8c
Daniel Walmsley <goldsounds@gmail.com>
2017-02-13 11:50:45 -0800

    Remove sharing_services option, is not used by Jetpack

--- sync/class.jetpack-sync-defaults.php
+++ sync/class.jetpack-sync-defaults.php
@@ -100,7 +100,6 @@ class Jetpack_Sync_Defaults {
 		'post_by_email_address',
 		'jetpack_protect_key',
 		'jetpack_protect_global_whitelist',
-		'sharing_services',
 		'jetpack_sso_require_two_step',
 		'jetpack_relatedposts',
 		'verification_services_codes',
