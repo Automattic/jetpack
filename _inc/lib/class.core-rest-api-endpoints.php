215c86ce44368181645916362f3a69f99cab0938
Elio Rivero <eliorivero@gmail.com>
2017-02-13 22:55:45 -0300

    Endpoints: whitelist option for Markdown for posts. Remove code that forcefully enables Markdown for posts every time the option is about to be written.

--- _inc/lib/class.core-rest-api-endpoints.php
+++ _inc/lib/class.core-rest-api-endpoints.php
@@ -1178,6 +1178,13 @@ class Jetpack_Core_Json_Api_Endpoints {
 				'validate_callback' => __CLASS__ . '::validate_boolean',
 				'jp_group'          => 'markdown',
 			),
+			'wpcom_publish_posts_with_markdown' => array(
+				'description'       => esc_html__( 'Use Markdown for posts.', 'jetpack' ),
+				'type'              => 'boolean',
+				'default'           => 0,
+				'validate_callback' => __CLASS__ . '::validate_boolean',
+				'jp_group'          => 'markdown',
+			),
 
 			// Mobile Theme
 			'wp_mobile_excerpt' => array(
