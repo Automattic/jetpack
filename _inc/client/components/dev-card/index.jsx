878ab89eee0dbc17e636a08ce22c2ad8ff777073
Michael Arestad <marestad@gmail.com>
2017-02-20 18:00:17 -0700

    DevCard: added a bit of styling

--- _inc/client/components/dev-card/index.jsx
+++ _inc/client/components/dev-card/index.jsx
@@ -11,6 +11,7 @@ import classNames from 'classnames';
 import { isDevVersion as _isDevVersion } from 'state/initial-state';
 import { switchMichaelsPlan } from 'state/dev-version';
 import { getSitePlan } from 'state/site';
+import Card from 'components/card';
 
 export const DevCard = React.createClass( {
 	displayName: 'DevCard',
@@ -26,51 +27,64 @@ export const DevCard = React.createClass( {
 		);
 
 		return (
-			<div className={ classes }>
-				Switch plan view (Dev versions only):
+			<Card compact className={ classes }>
+				<div className="jp-dev-card__heading">Plan preview</div>
+				<div className="jp-dev-card__subheading">(Dev only)</div>
 				<ul>
 					<li>
-						<input
-							type='radio'
-							id='jetpack_free'
-							value='jetpack_free'
-							name='jetpack_free'
-							checked={ 'jetpack_free' === this.props.sitePlan.product_slug }
-							onChange={ this.onChange }
-						/> Free
+						<label>
+							<input
+								type='radio'
+								id='jetpack_free'
+								value='jetpack_free'
+								name='jetpack_free'
+								checked={ 'jetpack_free' === this.props.sitePlan.product_slug }
+								onChange={ this.onChange }
+							/>
+							Free
+						</label>
 					</li>
 					<li>
-						<input
-							type='radio'
-							id='jetpack_personal'
-							value='jetpack_personal'
-							name='jetpack_personal'
-							checked={ /jetpack_personal*/.test( this.props.sitePlan.product_slug ) }
-							onChange={ this.onChange }
-						/> Personal
+						<label>
+							<input
+								type='radio'
+								id='jetpack_personal'
+								value='jetpack_personal'
+								name='jetpack_personal'
+								checked={ /jetpack_personal*/.test( this.props.sitePlan.product_slug ) }
+								onChange={ this.onChange }
+							/>
+							Personal
+						</label>
 					</li>
 					<li>
-						<input
-							type='radio'
-							id='jetpack_premium'
-							value='jetpack_premium'
-							name='jetpack_premium'
-							checked={ /jetpack_premium*/.test( this.props.sitePlan.product_slug ) }
-							onChange={ this.onChange }
-						/> Premium
+						<label>
+							<input
+								type='radio'
+								id='jetpack_premium'
+								value='jetpack_premium'
+								name='jetpack_premium'
+								checked={ /jetpack_premium*/.test( this.props.sitePlan.product_slug ) }
+								onChange={ this.onChange }
+							/>
+							Premium
+						</label>
 					</li>
 					<li>
+						<label>
 						<input
-							type='radio'
-							id='jetpack_business'
-							value='jetpack_business'
-							name='jetpack_business'
-							checked={ /jetpack_business*/.test( this.props.sitePlan.product_slug ) }
-							onChange={ this.onChange }
-						/> Professional
+								type='radio'
+								id='jetpack_business'
+								value='jetpack_business'
+								name='jetpack_business'
+								checked={ /jetpack_business*/.test( this.props.sitePlan.product_slug ) }
+								onChange={ this.onChange }
+							/>
+							Pro
+						</label>
 					</li>
 				</ul>
-			</div>
+			</Card>
 		);
 	}
 } );
