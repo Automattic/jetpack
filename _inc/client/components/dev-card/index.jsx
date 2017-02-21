a76ee8724252b82a7df095160e9d7f9c827d5a58
Michael Arestad <marestad@gmail.com>
2017-02-20 17:36:34 -0700

    DevCard: resolved tab/spaces

--- _inc/client/components/dev-card/index.jsx
+++ _inc/client/components/dev-card/index.jsx
@@ -13,70 +13,80 @@ import { switchMichaelsPlan } from 'state/dev-version';
 import { getSitePlan } from 'state/site';
 
 export const DevCard = React.createClass( {
-    displayName: 'DevCard',
+	displayName: 'DevCard',
 
-    onChange( event ) {
-        this.props.switchMichaelsPlan( event.target.value );
-    },
+	onChange( event ) {
+		this.props.switchMichaelsPlan( event.target.value );
+	},
 
-    render() {
-        const classes = classNames(
-            this.props.className,
-            'jp-dev-card'
-        );
+	render() {
+		const classes = classNames(
+			this.props.className,
+			'jp-dev-card'
+		);
 
-        return (
-            <div className={ classes }>
-                Switch plan view (Dev versions only): <br/>
-                <input
-                    type='radio'
-                    id='jetpack_free'
-                    value='jetpack_free'
-                    name='jetpack_free'
-                    checked={ 'jetpack_free' === this.props.sitePlan.product_slug }
-                    onChange={ this.onChange }
-                /> Free
-                <input
-                    type='radio'
-                    id='jetpack_personal'
-                    value='jetpack_personal'
-                    name='jetpack_personal'
-                    checked={ /jetpack_personal*/.test( this.props.sitePlan.product_slug ) }
-                    onChange={ this.onChange }
-                /> Personal
-                <input
-                    type='radio'
-                    id='jetpack_premium'
-                    value='jetpack_premium'
-                    name='jetpack_premium'
-                    checked={ /jetpack_premium*/.test( this.props.sitePlan.product_slug ) }
-                    onChange={ this.onChange }
-                /> Premium
-                <input
-                    type='radio'
-                    id='jetpack_business'
-                    value='jetpack_business'
-                    name='jetpack_business'
-                    checked={ /jetpack_business*/.test( this.props.sitePlan.product_slug ) }
-                    onChange={ this.onChange }
-                /> Professional
-            </div>
-        );
-    }
+		return (
+			<div className={ classes }>
+				Switch plan view (Dev versions only):
+				<ul>
+					<li>
+						<input
+							type='radio'
+							id='jetpack_free'
+							value='jetpack_free'
+							name='jetpack_free'
+							checked={ 'jetpack_free' === this.props.sitePlan.product_slug }
+							onChange={ this.onChange }
+						/> Free
+					</li>
+					<li>
+						<input
+							type='radio'
+							id='jetpack_personal'
+							value='jetpack_personal'
+							name='jetpack_personal'
+							checked={ /jetpack_personal*/.test( this.props.sitePlan.product_slug ) }
+							onChange={ this.onChange }
+						/> Personal
+					</li>
+					<li>
+						<input
+							type='radio'
+							id='jetpack_premium'
+							value='jetpack_premium'
+							name='jetpack_premium'
+							checked={ /jetpack_premium*/.test( this.props.sitePlan.product_slug ) }
+							onChange={ this.onChange }
+						/> Premium
+					</li>
+					<li>
+						<input
+							type='radio'
+							id='jetpack_business'
+							value='jetpack_business'
+							name='jetpack_business'
+							checked={ /jetpack_business*/.test( this.props.sitePlan.product_slug ) }
+							onChange={ this.onChange }
+						/> Professional
+					</li>
+				</ul>
+			</div>
+		);
+	}
 } );
 
 export default connect(
-    state => {
-        return {
-            isDevVersion: _isDevVersion( state ),
-            sitePlan: getSitePlan( state )
-        }
-    },
-    ( dispatch ) => {
-        return {
-            switchMichaelsPlan: ( slug ) => {
-                return dispatch( switchMichaelsPlan( slug ) );
-            }
-        };
-    }
+	state => {
+		return {
+			isDevVersion: _isDevVersion( state ),
+			sitePlan: getSitePlan( state )
+		}
+	},
+	( dispatch ) => {
+		return {
+			switchMichaelsPlan: ( slug ) => {
+				return dispatch( switchMichaelsPlan( slug ) );
+			}
+		};
+	}
 )( DevCard );
