b07bbeec5ee93fd6c57f51d090ad84fa6f36b10f
Sean O'Shaughnessy <seanosh@gmail.com>
2017-02-21 23:21:59 -0500

    whoops

--- projects/plugins/wpcomsh/wpcomsh-loader.php
+++ projects/plugins/wpcomsh/wpcomsh-loader.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: WP.com Special Helper
+Plugin Name: WP.com Site Helper
 Description: WordPress.com provided functionality & tools
 */
 require_once( WPMU_PLUGIN_DIR . '/wpcomsh/wpcomsh.php' );
