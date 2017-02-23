20962858719ee8c00995ffb1ff26646b954f11d8
Michael Turk <turk.michael@gmail.com>
2017-02-23 17:35:30 -0500

    Jetpack Sync: Adding skeleton test file for WP Super Cache Sync module

---
+++ tests/php/sync/test_class.jetpack-sync-wp_super_cache.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Testing WP Super Cache Sync
+ */
+class WP_Test_Jetpack_Sync_WP_Super_Cache extends WP_Test_Jetpack_Sync_Base {
+	protected $post;
+	protected $callable_module;
+	protected $full_sync;
+	static $woo_enabled;
+
+	public static function setUpBeforeClass() {
+		parent::setUpBeforeClass();
+
+		if ( "1" != getenv( 'JETPACK_TEST_WP_SUPER_CACHE' ) ) {
+			return; 
+		} 
+		
+		self::$wp_super_cache_enabled = true;
+	}
+
+	public function setUp() {
+		if ( ! self::$wp_super_cache_enabled ) {
+			$this->markTestSkipped();
+			return;
+		}
+		parent::setUp();
+		$this->full_sync = Jetpack_Sync_Modules::get_module( 'full-sync' );
+	}
+
+	function test_module_is_enabled() {
+		$this->assertTrue( !! Jetpack_Sync_Modules::get_module( "wp-super-cache" ) );
+	}
+
+	//@todo test functionality
+}
+
