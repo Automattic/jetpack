0e554d7b8f5f7791e1d581cfb79494dfbf775e71
Stanimir <stanimir.k.stoyanov@gmail.com>
2017-02-28 15:41:20 +0200

    Namespace Twentyseventeen Infinite Scroll functions

--- modules/infinite-scroll/themes/twentyseventeen.php
+++ modules/infinite-scroll/themes/twentyseventeen.php
@@ -8,20 +8,20 @@
 /**
  * Add theme support for infinite scroll
  */
-function twentyseventeen_infinite_scroll_init() {
+function jetpack_twentyseventeen_infinite_scroll_init() {
 	add_theme_support( 'infinite-scroll', array(
 		'container'      => 'main',
-		'render'         => 'twentyseventeen_infinite_scroll_render',
+		'render'         => 'jetpack_twentyseventeen_infinite_scroll_render',
 		'footer'         => 'content',
-		'footer_widgets' => twentyseventeen_has_footer_widgets(),
+		'footer_widgets' => jetpack_twentyseventeen_has_footer_widgets(),
 	) );
 }
-add_action( 'init', 'twentyseventeen_infinite_scroll_init' );
+add_action( 'init', 'jetpack_twentyseventeen_infinite_scroll_init' );
 
 /**
  * Custom render function for Infinite Scroll.
  */
-function twentyseventeen_infinite_scroll_render() {
+function jetpack_twentyseventeen_infinite_scroll_render() {
 	while ( have_posts() ) {
 		the_post();
 		if ( is_search() ) {
@@ -35,7 +35,7 @@ function twentyseventeen_infinite_scroll_render() {
 /**
  * Custom function to check for the presence of footer widgets or the social links menu
  */
-function twentyseventeen_has_footer_widgets() {
+function jetpack_twentyseventeen_has_footer_widgets() {
 	if ( is_active_sidebar( 'sidebar-2' ) ||
 		 is_active_sidebar( 'sidebar-3' ) ||
 		 has_nav_menu( 'social' ) ) {
@@ -48,10 +48,10 @@ function twentyseventeen_has_footer_widgets() {
 /**
  * Enqueue CSS stylesheet with theme styles for Infinite Scroll.
  */
-function twentyseventeen_infinite_scroll_enqueue_styles() {
+function jetpack_twentyseventeen_infinite_scroll_enqueue_styles() {
 	if ( wp_script_is( 'the-neverending-homepage' ) ) {
 		wp_enqueue_style( 'infinity-twentyseventeen', plugins_url( 'twentyseventeen.css', __FILE__ ), array( 'the-neverending-homepage' ), '20161219' );
 		wp_style_add_data( 'infinity-twentyseventeen', 'rtl', 'replace' );
 	}
 }
-add_action( 'wp_enqueue_scripts', 'twentyseventeen_infinite_scroll_enqueue_styles', 25 );
+add_action( 'wp_enqueue_scripts', 'jetpack_twentyseventeen_infinite_scroll_enqueue_styles', 25 );
