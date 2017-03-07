54fabee169f64bb8e7d47c5863cf99ff66328e83
Kelly Dwan <kelly.dwan@gmail.com>
2017-03-07 13:28:58 -0500

    Add a screen-reader label for notifications, and initially-hide notifications bar (so screen readers don’t get trapped listening to everything)

--- modules/masterbar/masterbar.php
+++ modules/masterbar/masterbar.php
@@ -162,9 +162,10 @@ class A8C_WPCOM_Masterbar {
 		$wp_admin_bar->add_node( array(
 			'id'     => 'notes',
 			'title'  => '<span id="wpnt-notes-unread-count" class="wpnt-loading wpn-read"></span>
+						 <span class="screen-reader-text">Notifications</span>
 						 <span class="noticon noticon-bell"></span>',
 			'meta'   => array(
-				'html'  => '<div id="wpnt-notes-panel2" style="display:none" lang="'. esc_attr( $this->locale ) . '" dir="' . ( $this->is_rtl() ? 'rtl' : 'ltr' ) . '">' .
+				'html'  => '<div id="wpnt-notes-panel2" aria-hidden="true" style="display:none" lang="'. esc_attr( $this->locale ) . '" dir="' . ( $this->is_rtl() ? 'rtl' : 'ltr' ) . '">' .
 				           '<div class="wpnt-notes-panel-header">' .
 				           '<span class="wpnt-notes-header">' .
 				           __( 'Notifications', 'jetpack' ) .
