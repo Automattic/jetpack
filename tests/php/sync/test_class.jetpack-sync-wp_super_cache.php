6c2b66eb676f35f2e8d91133a968ab006399e770
Michael Turk <turk.michael@gmail.com>
2017-03-09 13:53:26 -0500

    Jetpack Sync: Adding tests for WP Super Cache Sync Module

--- tests/php/sync/test_class.jetpack-sync-wp_super_cache.php
+++ tests/php/sync/test_class.jetpack-sync-wp_super_cache.php
@@ -30,6 +30,32 @@ class WP_Test_Jetpack_Sync_WP_Super_Cache extends WP_Test_Jetpack_Sync_Base {
 		$this->assertTrue( !! Jetpack_Sync_Modules::get_module( "wp-super-cache" ) );
 	}
 
-	//@todo test functionality
+	function test_constants_are_synced() {
+		$this->sender->do_sync();
+
+		//Confirm that constants that aren't synced return null
+		$this->assertEquals( null, $this->server_replica_storage->get_constant( 'WP_SUPER_CACHE_NON_EXISTENT_CONSTANT' ) );
+
+		$this->server_replica_storage->reset();
+		$this->sender->do_sync();
+
+		foreach ( Jetpack_Sync_Module_WP_Super_Cache::$wp_super_cache_constants as $constant ) {
+			$this->assertNotEquals( null, $this->server_replica_storage->get_constant( $constant ) );
+		}
+	}
+
+	function test_globals_are_synced() {
+		$wp_super_cache_globals = Jetpack_Sync_Module_WP_Super_Cache::get_wp_super_cache_globals();
+		foreach ( $wp_super_cache_globals as $key => $value ) {
+			$GLOBALS[$key] = $key;
+		}
+		$this->sender->do_sync();
+
+		$synced_values = $this->server_replica_storage->get_callable( 'wp_super_cache_globals' );
+
+		foreach ( $wp_super_cache_globals as $key => $value ) {
+			$this->assertEquals( $key, $synced_values[$key] );
+		}
+	}
 }
 
