8abb597c27495be84e3b8dc5d04d0fd65cf15537
Enej Bajgoric <enej.bajgoric@gmail.com>
2017-03-10 12:32:56 -0800

    Move the 'plugin_loaded' actions higher up so that test in WP4.6 pass as expected.

--- sync/class.jetpack-sync-actions.php
+++ sync/class.jetpack-sync-actions.php
@@ -431,17 +431,19 @@ class Jetpack_Sync_Actions {
 	}
 }
 
+// Check for WooCommerce support
+add_action( 'plugins_loaded', array( 'Jetpack_Sync_Actions', 'initialize_woocommerce' ), 5 );
+
+// Check for WP Super Cache
+add_action( 'plugins_loaded', array( 'Jetpack_Sync_Actions', 'initialize_wp_super_cache' ), 5 );
+
 /*
  * Init after plugins loaded and before the `init` action. This helps with issues where plugins init
  * with a high priority or sites that use alternate cron.
  */
 add_action( 'plugins_loaded', array( 'Jetpack_Sync_Actions', 'init' ), 90 );
 
-// Check for WooCommerce support
-add_action( 'plugins_loaded', array( 'Jetpack_Sync_Actions', 'initialize_woocommerce' ), 5 );
 
-// Check for WP Super Cache
-add_action( 'plugins_loaded', array( 'Jetpack_Sync_Actions', 'initialize_wp_super_cache' ), 5 );
 
 // We need to define this here so that it's hooked before `updating_jetpack_version` is called
 add_action( 'updating_jetpack_version', array( 'Jetpack_Sync_Actions', 'do_initial_sync' ), 10, 2 );
