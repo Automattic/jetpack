6dd50a94f986b426d446f5b6ad29cb7c1f768eaf
vindl <markoandrijasevic@gmail.com>
2017-03-15 22:30:47 +0100

    Fix conflicts with site's RTL settings

--- modules/notes.php
+++ modules/notes.php
@@ -104,18 +104,22 @@ class Jetpack_Notifications {
 	}
 
 	function styles_and_scripts() {
-		/**
-		 * Can be used to force Notifications to display in RTL style.
-		 *
-		 * @module notes
-		 * 
-		 * @since 4.8.0
-		 *
-		 * @param bool true Should notifications be displayed in RTL style. Defaults to false.
-		 */
-		$enqueue_rtl = apply_filters( 'a8c_wpcom_masterbar_enqueue_rtl_notification_styles', false );
+		$is_rtl = is_rtl();
 
-		if ( ! is_rtl() && ! $enqueue_rtl ) {
+		if ( Jetpack::is_module_active( 'masterbar' ) ) {
+			/**
+			 * Can be used to force Notifications to display in RTL style.
+			 *
+			 * @module notes
+			 *
+			 * @since 4.8.0
+			 *
+			 * @param bool true Should notifications be displayed in RTL style. Defaults to false.
+			 */
+			$is_rtl = apply_filters( 'a8c_wpcom_masterbar_enqueue_rtl_notification_styles', false );
+		}
+
+		if ( ! $is_rtl ) {
 			wp_enqueue_style( 'wpcom-notes-admin-bar', $this->wpcom_static_url( '/wp-content/mu-plugins/notes/admin-bar-v2.css' ), array(), JETPACK_NOTES__CACHE_BUSTER );
 		} else {
 			wp_enqueue_style( 'wpcom-notes-admin-bar', $this->wpcom_static_url( '/wp-content/mu-plugins/notes/rtl/admin-bar-v2-rtl.css' ), array(), JETPACK_NOTES__CACHE_BUSTER );
