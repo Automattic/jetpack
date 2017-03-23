f844ee456e5d388e3e6beada336cc0c5c8678b97
Elio Rivero <eliorivero@gmail.com>
2017-03-23 13:45:59 -0300

    Settings UI: fix support block triple render (#6723)
    * Settings UI: don't re-render the support block unnecessarily
    
    * Settings UI: don't render support block until we have the plan data to know which text to display: the one for priority support or the regular one. Once it's rendered, don't re-render when switching tabs if we already have plan information available.

--- _inc/client/components/support-card/index.jsx
+++ _inc/client/components/support-card/index.jsx
@@ -26,12 +26,21 @@ import Banner from 'components/banner';
 const SupportCard = React.createClass( {
 	displayName: 'SupportCard',
 
+	getInitialState() {
+		return {
+			gravIds: sampleSize( this.props.happinessGravatarIds, 3 )
+		};
+	},
+
 	render() {
+		if ( 'undefined' === typeof this.props.sitePlan.product_slug && this.props.isFetchingSiteData ) {
+			return <div />;
+		}
+
 		const classes = classNames(
 				this.props.className,
 				'jp-support-card'
 			),
-			gravIds = sampleSize( this.props.happinessGravatarIds, 3 ),
 			noPrioritySupport = 'undefined' === typeof this.props.sitePlan.product_slug || 'jetpack_free' === this.props.sitePlan.product_slug;
 
 		return (
@@ -63,45 +72,45 @@ const SupportCard = React.createClass( {
 						<ul>
 							<li>
 								<img
-									src={ 'https://secure.gravatar.com/avatar/' + gravIds[ 0 ].avatar }
+									src={ 'https://secure.gravatar.com/avatar/' + this.state.gravIds[ 0 ].avatar }
 									alt={ __( 'Jetpack Happiness Engineer' ) }
 									className="jp-support-card__happiness-engineer-img"
 									width="72"
 									height="72"
 								/>
 								<div>
-									<em className="jp-support-card__happiness-engineer-name">{ gravIds[ 0 ].name }</em>
+									<em className="jp-support-card__happiness-engineer-name">{ this.state.gravIds[ 0 ].name }</em>
 								</div>
 							</li>
 							<li>
 								<img
-									src={ 'https://secure.gravatar.com/avatar/' + gravIds[ 1 ].avatar }
+									src={ 'https://secure.gravatar.com/avatar/' + this.state.gravIds[ 1 ].avatar }
 									alt={ __( 'Jetpack Happiness Engineer' ) }
 									className="jp-support-card__happiness-engineer-img"
 									width="72"
 									height="72"
 								/>
 								<div>
-									<em className="jp-support-card__happiness-engineer-name">{ gravIds[ 1 ].name }</em>
+									<em className="jp-support-card__happiness-engineer-name">{ this.state.gravIds[ 1 ].name }</em>
 								</div>
 							</li>
 							<li>
 								<img
-									src={ 'https://secure.gravatar.com/avatar/' + gravIds[ 2 ].avatar }
+									src={ 'https://secure.gravatar.com/avatar/' + this.state.gravIds[ 2 ].avatar }
 									alt={ __( 'Jetpack Happiness Engineer' ) }
 									className="jp-support-card__happiness-engineer-img"
 									width="72"
 									height="72"
 								/>
 								<div>
-									<em className="jp-support-card__happiness-engineer-name">{ gravIds[ 2 ].name }</em>
+									<em className="jp-support-card__happiness-engineer-name">{ this.state.gravIds[ 2 ].name }</em>
 								</div>
 							</li>
 						</ul>
 					</div>
 				</Card>
 				{
-					( ! this.props.fetchingSiteData && noPrioritySupport ) && (
+					noPrioritySupport && (
 						<Banner
 							title={ __( 'Need help quicker? Upgrade for priority support' ) }
 							plan={ PLAN_JETPACK_PERSONAL }
@@ -124,7 +133,7 @@ export default connect(
 		return {
 			sitePlan: getSitePlan( state ),
 			siteRawUrl: getSiteRawUrl( state ),
-			fetchingSiteData: isFetchingSiteData( state ),
+			isFetchingSiteData: isFetchingSiteData( state ),
 			happinessGravatarIds: getHappinessGravatarIds( state )
 		};
 	}
