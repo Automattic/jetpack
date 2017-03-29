6a5d230ebb13c375e04a4f7996350b3a81c4f05b
Elio Rivero <eliorivero@gmail.com>
2017-03-29 17:21:14 -0300

    Settings UI: make the QuerySite component fetch data only once

--- _inc/client/components/data/query-site/index.jsx
+++ _inc/client/components/data/query-site/index.jsx
@@ -1,40 +1,55 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import isEmpty from 'lodash/isEmpty';
 
 /**
  * Internal dependencies
  */
 import {
 	fetchSiteData,
-	isFetchingSiteData
+	isFetchingSiteData,
+	getSitePlan
 } from 'state/site';
 import { isDevMode } from 'state/connection';
 
-export const QuerySite = React.createClass( {
-	componentDidMount() {
-		if ( ! ( this.props.isFetchingSiteData || this.props.isDevMode ) ) {
+class QuerySite extends Component {
+	static propTypes = {
+		isFetchingSiteData: PropTypes.bool,
+		isDevMode: PropTypes.bool,
+		sitePlan: PropTypes.object
+	};
+
+	static defaultProps = {
+		isFetchingSiteData: false,
+		isDevMode: false,
+		sitePlan: {}
+	};
+
+	componentWillMount() {
+		if ( ! this.props.isFetchingSiteData && ! this.props.isDevMode && isEmpty( this.props.sitePlan ) ) {
 			this.props.fetchSiteData();
 		}
-	},
+	}
 
 	render() {
 		return null;
 	}
-} );
+}
 
 export default connect(
 	( state ) => {
 		return {
 			isFetchingSiteData: isFetchingSiteData( state ),
-			isDevMode: isDevMode( state )
+			isDevMode: isDevMode( state ),
+			sitePlan: getSitePlan( state )
 		};
 	},
 	( dispatch ) => {
 		return {
 			fetchSiteData: () => dispatch( fetchSiteData() )
-		}
+		};
 	}
 )( QuerySite );
