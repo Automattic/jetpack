3f9d14b7131cd4b31946e036359efc27b0fbb689
Payton Swick <payton@foolord.com>
2017-03-31 15:44:54 -0400

    Allow HTTP PUT data to be validated by Jetpack_Signature
    HTTP requests made by functions like `Jetpack_Client::remote_request()`
    generate signed body hashes for the request content. If the data is sent
    via the `PUT` method, however, it was ignored, causing a hash comparison
    failure on the server. This change allows `PUT` data to be validated as
    well.
    
    Just like the `POST` data, the `PUT` data is taken from the raw
    url-encoded input (I don't understand why), but if IS_WPCOM, we assume
    the data is JSON-encoded and use the decoded version.

--- class.jetpack-signature.php
+++ class.jetpack-signature.php
@@ -71,7 +71,17 @@ class Jetpack_Signature {
 					$body = $_POST;
 				}
 			}
+		} else if ( 'PUT' == strtoupper( $_SERVER['REQUEST_METHOD'] ) ) {
+			// This is a little strange-looking, but there doesn't seem to be another way to get the PUT body
+			$raw_put_data = file_get_contents( 'php://input' );
+			parse_str( $raw_put_data, $body );
 
+			if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
+				$put_data = json_decode( $raw_put_data, true );
+				if ( is_array( $put_data ) && count( $put_data ) > 0 ) {
+					$body = $put_data;
+				}
+			}
 		} else {
 			$body = null;
 		}
