55ed5f4450760268902e5c170c76d9e0a1e5439a
Igor Zinovyev <zinigor@gmail.com>
2017-04-05 12:11:02 +0300

    Added a test case that will run linting for all PHP files in the plugin, props @cfinke.

---
+++ tests/php/test_php-lint.php
@@ -0,0 +1,20 @@
+<?php
+
+class WP_Test_Jetpack_PHP_Lint extends WP_UnitTestCase {
+
+	/**
+	 * Props to cfinke for the amazing piece of code.
+	 * @author zinigor
+	 * @since 4.8.1
+	 */
+	public function test_php_lint() {
+		$command =
+			'for file in `find . -name "*.php"`; '
+			. 'do php -l "$file" | grep -v "No syntax errors detected"; '
+			. 'done';
+
+		exec( $command, $output );
+
+		$this->assertEmpty( $output );
+	}
+}
