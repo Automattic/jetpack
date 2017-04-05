6fe19253302a9fece0dcb0bcdb3c6fdc8839ddc4
Igor Zinovyev <zinigor@gmail.com>
2017-04-05 12:43:21 +0300

    Added exceptions to the linter, added output.

--- tests/php/test_php-lint.php
+++ tests/php/test_php-lint.php
@@ -10,11 +10,15 @@ class WP_Test_Jetpack_PHP_Lint extends WP_UnitTestCase {
 	public function test_php_lint() {
 		$command =
 			'for file in `find . -name "*.php"`; '
-			. 'do php -l "$file" | grep -v "No syntax errors detected"; '
+			. 'do php -l "$file" | '
+			. 'grep -v "No syntax errors detected" | '
+			. 'grep -v "./tools/" | '
+			. 'grep -v "jetpack-cli.php" | '
+			. 'grep -v -e \'^$\'; '
 			. 'done';
 
 		exec( $command, $output );
 
-		$this->assertEmpty( $output );
+		$this->assertEmpty( $output, join( PHP_EOL, $output ) );
 	}
 }
