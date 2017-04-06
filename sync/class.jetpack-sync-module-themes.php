24ed0934bc8f62f7f00ea5dba0a2e52d02fec1f1
Enej Bajgoric <enej.bajgoric@gmail.com>
2017-04-06 15:51:12 -0700

    Sync: Sidebar changes (#6909)
    * Sync: Sidebar changes
    
    This PR lets us sync changes as they happen to the sidebar.
    
    * Minor fixes
    * Add array_version to the test
    * Refactored the function to be easier to read
    * PHP Fatal fixes
    * PHP pre 5.4 version fixes.

--- sync/class.jetpack-sync-module-themes.php
+++ sync/class.jetpack-sync-module-themes.php
@@ -8,6 +8,14 @@ class Jetpack_Sync_Module_Themes extends Jetpack_Sync_Module {
 	public function init_listeners( $callable ) {
 		add_action( 'switch_theme', array( $this, 'sync_theme_support' ) );
 		add_action( 'jetpack_sync_current_theme_support', $callable );
+
+		// Sidebar updates.
+		add_action( 'update_option_sidebars_widgets', array( $this, 'sync_sidebar_widgets_actions' ), 10, 2 );
+		add_action( 'jetpack_widget_added', $callable, 10, 2 );
+		add_action( 'jetpack_widget_removed', $callable, 10, 2 );
+		add_action( 'jetpack_widget_moved_to_inactive', $callable );
+		add_action( 'jetpack_cleared_inactive_widgets', $callable );
+		add_action( 'jetpack_widget_reordered', $callable );
 	}
 
 	public function init_full_sync_listeners( $callable ) {
@@ -54,6 +62,128 @@ class Jetpack_Sync_Module_Themes extends Jetpack_Sync_Module {
 
 	function expand_theme_data() {
 		return array( $this->get_theme_support_info() );
+	}
+
+	function sync_add_widgets_to_sidebar( $new_widgets, $old_widgets, $sidebar ) {
+		$added_widgets = array_diff( $new_widgets, $old_widgets );
+		if ( empty( $added_widgets ) ) {
+			return array();
+		}
+		$moved_to_sidebar = array();
+		foreach ( $added_widgets as $added_widget ) {
+			$moved_to_sidebar[] = $added_widget;
+			/**
+			 * Helps Sync log that a widget got added
+			 *
+			 * @since 4.9.0
+			 *
+			 * @param string $sidebar, Sidebar id got changed
+			 * @param string $added_widget, Widget id got added
+			 */
+			do_action( 'jetpack_widget_added', $sidebar, $added_widget );
+		}
+		return $moved_to_sidebar;
+	}
+
+	function sync_remove_widgets_from_sidebar( $new_widgets, $old_widgets, $sidebar, $inactive_widgets  ) {
+		$removed_widgets = array_diff( $old_widgets, $new_widgets );
+
+		if ( empty( $removed_widgets ) ) {
+			return array();
+		}
+
+		$moved_to_inactive = array();
+
+		foreach( $removed_widgets as $removed_widget ) {
+			// Lets check if we didn't move the widget to in_active_widgets
+			if ( isset( $inactive_widgets ) && ! in_array( $removed_widget, $inactive_widgets ) ) {
+				/**
+				 * Helps Sync log that a widgte got removed
+				 *
+				 * @since 4.9.0
+				 *
+				 * @param string $sidebar, Sidebar id got changed
+				 * @param string $removed_widget, Widget id got removed
+				 */
+				do_action( 'jetpack_widget_removed', $sidebar, $removed_widget );
+			} else {
+				$moved_to_inactive[] = $removed_widget;
+			}
+		}
+		return $moved_to_inactive;
+
+	}
+
+	function sync_widgets_reordered( $new_widgets, $old_widgets, $sidebar ) {
+		$added_widgets = array_diff( $new_widgets, $old_widgets );
+		if ( ! empty( $added_widgets ) ) {
+			return;
+		}
+		$removed_widgets = array_diff( $old_widgets, $new_widgets );
+		if ( ! empty( $removed_widgets ) ) {
+			return;
+		}
+
+		if ( serialize( $old_widgets ) !== serialize( $new_widgets ) ) {
+			/**
+			 * Helps Sync log that a sidebar id got reordered
+			 *
+			 * @since 4.9.0
+			 *
+			 * @param string $sidebar, Sidebar id got changed
+			 */
+			do_action( 'jetpack_widget_reordered', $sidebar );
+		}
+
+	}
+
+	function sync_sidebar_widgets_actions( $old_value, $new_value ) {
+
+		// Don't really know how to deal with different array_values yet.
+		if ( $old_value['array_version'] !== 3 || $new_value['array_version'] !== 3 ) {
+			return;
+		}
+
+		$moved_to_inactive = array();
+		$moved_to_sidebar = array();
+
+		foreach ( $new_value as $sidebar => $new_widgets ) {
+			if ( in_array( $sidebar, array( 'array_version', 'wp_inactive_widgets' ) ) ) {
+				continue;
+			}
+			$old_widgets = $old_value[ $sidebar ];
+
+			$moved_to_inactive_recently = $this->sync_remove_widgets_from_sidebar( $new_widgets, $old_widgets, $sidebar, $new_value['wp_inactive_widgets'] );
+			$moved_to_inactive = array_merge( $moved_to_inactive, $moved_to_inactive_recently );
+
+
+			$moved_to_sidebar_recently = $this->sync_add_widgets_to_sidebar( $new_widgets, $old_widgets, $sidebar );
+			$moved_to_sidebar = array_merge( $moved_to_sidebar, $moved_to_sidebar_recently );
+
+			$this->sync_widgets_reordered( $new_widgets, $old_widgets, $sidebar );
+
+		}
+
+		// Treat inactive sidebar a bit differently
+		if ( ! empty( $moved_to_inactive ) ) {
+			/**
+			 * Helps Sync log that a widgets IDs got moved to in active
+			 *
+			 * @since 4.9.0
+			 *
+			 * @param array $sidebar, Sidebar id got changed
+			 */
+			do_action( 'jetpack_widget_moved_to_inactive', $moved_to_inactive );
+		} elseif ( empty( $moved_to_sidebar ) &&
+		           empty( $new_value['wp_inactive_widgets']) &&
+		           ! empty( $old_value['wp_inactive_widgets'] ) ) {
+			/**
+			 * Helps Sync log that a got cleared from inactive.
+			 *
+			 * @since 4.9.0
+			 */
+			do_action( 'jetpack_cleared_inactive_widgets' );
+		} 
 	}
 
 	private function get_theme_support_info() {
