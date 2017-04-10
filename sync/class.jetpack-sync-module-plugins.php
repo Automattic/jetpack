40fc073b9fb30d56f51f5d701568067b3445b8ac
Michael Turk <turk.michael@gmail.com>
2017-04-10 13:59:26 -0400

    Jetpack Sync: Adding plugin data upon plugin sync action

--- sync/class.jetpack-sync-module-plugins.php
+++ sync/class.jetpack-sync-module-plugins.php
@@ -11,4 +11,15 @@ class Jetpack_Sync_Module_Plugins extends Jetpack_Sync_Module {
 		add_action( 'activated_plugin', $callable, 10, 2 );
 		add_action( 'deactivated_plugin', $callable, 10, 2 );
 	}
+
+	public function init_before_send() {
+		add_filter( 'jetpack_sync_before_send_deleted_plugin', array( $this, 'expand_plugin_data' ) );
+		add_filter( 'jetpack_sync_before_send_activated_plugin', array( $this, 'expand_plugin_data' ) );
+		add_filter( 'jetpack_sync_before_send_deactivated_plugin', array( $this, 'expand_plugin_data' ) );
+	}
+
+	public function expand_plugin_data( $args ) {
+		$plugin_data = get_plugin_data( $args[0] );
+		return array( $args[0], $args[1], $plugin_data );
+	}
 }
