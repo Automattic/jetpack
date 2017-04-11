9707f37e86a56cf4ce340144c9244f54f62b7b2b
Michael Turk <turk.michael@gmail.com>
2017-04-11 14:57:10 -0400

    Jetpack Sync: Adding new jetpack_delete_plugin action to sync plugin name and version when a plugin deletion is attempted

--- sync/class.jetpack-sync-module-plugins.php
+++ sync/class.jetpack-sync-module-plugins.php
@@ -10,12 +10,32 @@ class Jetpack_Sync_Module_Plugins extends Jetpack_Sync_Module {
 		add_action( 'deleted_plugin', $callable, 10, 2 );
 		add_action( 'activated_plugin', $callable, 10, 2 );
 		add_action( 'deactivated_plugin', $callable, 10, 2 );
+		add_action( 'delete_plugin', array( $this, 'sync_deleted_plugin_info') );
+		add_action( 'jetpack_delete_plugin', $callable, 10, 2);
 	}
 
 	public function init_before_send() {
-		add_filter( 'jetpack_sync_before_send_deleted_plugin', array( $this, 'expand_plugin_data' ) );
 		add_filter( 'jetpack_sync_before_send_activated_plugin', array( $this, 'expand_plugin_data' ) );
 		add_filter( 'jetpack_sync_before_send_deactivated_plugin', array( $this, 'expand_plugin_data' ) );
+		//Note that we don't simply 'expand_plugin_data' on the 'delete_plugin' action here because the plugin file is deleted when that action finishes
+	}
+
+	public function sync_deleted_plugin_info( $plugin_path ) {
+		$all_plugin_data = get_plugin_data( WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . $plugin_path );
+		$data = array (
+			'name' => $all_plugin_data['Name'],
+			'version' => $all_plugin_data['Version'],
+		);
+
+		/**
+		 * Syncs information about a plugin whose deletion was attempted
+		 *
+		 * @since 4.8.3
+		 *
+		 * @param string $plugin_path Path of plugin whose deletion was attempted (deletion verified in deleted_plugin sync action)
+		 * @param mixed $data Array of plugin information fields (name, version, etc.)
+		 */
+		do_action( 'jetpack_delete_plugin', $plugin_path, $data );
 	}
 
 	public function expand_plugin_data( $args ) {
@@ -28,14 +48,6 @@ class Jetpack_Sync_Module_Plugins extends Jetpack_Sync_Module {
 			$all_plugin_data = $all_plugins[$plugin_path];
 			$plugin_data['name'] = $all_plugin_data['Name'];
 			$plugin_data['version'] = $all_plugin_data['Version'];
-		} else {
-			require_once ABSPATH . 'wp-admin/includes/plugin-install.php';
-			$slug = Jetpack_Autoupdate::get_plugin_slug( $plugin_path );
-			$all_plugin_data = plugins_api( 'plugin_information', array( 'slug' => $slug ) );
-			if ( ! is_wp_error( $all_plugin_data ) ) {
-				$plugin_data['name'] = $all_plugin_data->name;
-				$plugin_data['version'] = $all_plugin_data->version;
-			}
 		}
 
 		return array(
