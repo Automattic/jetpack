089401aca18606c4afeb55b2f0e60ee7301729e6
Michael Turk <turk.michael@gmail.com>
2017-04-11 10:39:32 -0400

    Jetpack Sync: Adding tests

--- sync/class.jetpack-sync-module-plugins.php
+++ sync/class.jetpack-sync-module-plugins.php
@@ -7,14 +7,13 @@ class Jetpack_Sync_Module_Plugins extends Jetpack_Sync_Module {
 	}
 
 	public function init_listeners( $callable ) {
-		add_action( 'delete_plugin', $callable ); //This fires before the deletion, so plugin data is still available for the activity log
-		add_action( 'deleted_plugin', $callable, 10, 2 ); //This fires after the deletion, and will inform us if the deletion failed
+		add_action( 'deleted_plugin', $callable, 10, 2 );
 		add_action( 'activated_plugin', $callable, 10, 2 );
 		add_action( 'deactivated_plugin', $callable, 10, 2 );
 	}
 
 	public function init_before_send() {
-		add_filter( 'jetpack_sync_before_send_delete_plugin', array( $this, 'expand_plugin_data' ) );
+		add_filter( 'jetpack_sync_before_send_deleted_plugin', array( $this, 'expand_plugin_data' ) );
 		add_filter( 'jetpack_sync_before_send_activated_plugin', array( $this, 'expand_plugin_data' ) );
 		add_filter( 'jetpack_sync_before_send_deactivated_plugin', array( $this, 'expand_plugin_data' ) );
 	}
@@ -29,15 +28,13 @@ class Jetpack_Sync_Module_Plugins extends Jetpack_Sync_Module {
 			$all_plugin_data = $all_plugins[$plugin_path];
 			$plugin_data['Name'] = $all_plugin_data['Name'];
 			$plugin_data['Version'] = $all_plugin_data['Version'];
+		} else {
+			$slug = Jetpack_Autoupdate::get_plugin_slug( $plugin_path );
+			$api = plugins_api( 'plugin_information', array( 'slug' => $slug ) );
+			print_r($api);
+
 		}
 
-		//If action is 'delete_plugin', it will have 1 argument, whereas it will have 2 if 'activated_plugin' or 'deactivated_plugin'. 
-		if ( 'delete_plugin' === current_filter() ) {
-			return array(
-				$args[0],
-				$plugin_data,
-			);
-		}
 
 		return array(
 			$args[0],
