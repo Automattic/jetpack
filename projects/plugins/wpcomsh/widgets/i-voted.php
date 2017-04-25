f4ace4a2a05735e1fd9212bca6137da7503f96e0
Rastislav Lamos <lamos.rasto@gmail.com>
2017-04-25 15:53:27 +0200

    Remove the deprecation warnings

--- projects/plugins/wpcomsh/widgets/i-voted.php
+++ projects/plugins/wpcomsh/widgets/i-voted.php
@@ -34,22 +34,6 @@ class Jetpack_I_Voted_Widget extends WP_Widget {
 	}
 
 	function widget( $args, $instance ) {
-		// [DEPRECATION]: Since we are after the dep. date show admins
-		// the warning message, otherwise just display a link to their
-		// about.me page
-		if ( current_user_can( 'edit_theme_options' ) ) {
-			?>
-			<h2>
-				<?php printf( wp_kses( __(
-					'The I Voted widget is no longer available. To remove this widget, ' .
-					'<a href="%s">visit your settings</a>. This message is not shown to visitors to your site.',
-					'jetpack' ),
-					array( 'a' => array( 'href' => array() ) ) ), admin_url( 'widgets.php' )
-				); ?>
-			</h2>
-			<?php
-		}
-
 		echo $args['before_widget'];
 
 		$title = apply_filters( 'widget_title', $instance['title'] );
@@ -79,15 +63,6 @@ class Jetpack_I_Voted_Widget extends WP_Widget {
 		$instance = wp_parse_args( (array) $instance, $defaults );
 
 		$title = esc_attr( $instance['title'] );
-
-		?>
-		<p>
-			<strong style="color: #ff6347;">
-				<?php _e( 'The I Voted widget is deprecated.', 'jetpack' );
-				?>
-			</strong>
-		</p>
-		<?php
 
 		echo '<p><label for="' . $this->get_field_id( 'title' ) . '">' . esc_html__( 'Title:' ) . '
 		<input class="widefat" id="' . $this->get_field_id( 'title' ) . '" name="' . $this->get_field_name( 'title' ) . '" type="text" value="' . $title . '" />
