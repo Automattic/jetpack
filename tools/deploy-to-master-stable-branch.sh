ad06e9c2783060bd6d220a96c6c9c25303d99442
Derek Smart <smart@automattic.com>
2017-04-27 10:34:51 -0400

    Add deprecation notice to old script

---
+++ tools/deploy-to-master-stable-branch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script is deprecated.
+# new script is build-release-branch.sh
+
+echo "This script has been moved and refactored."
+echo "Run 'build-release-branch.sh' for instructions on how to use new script."
