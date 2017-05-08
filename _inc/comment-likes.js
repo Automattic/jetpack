c61c1161d5cf6d53f44a164152a82f6e540d330f
Dan Roundhill <dan@a8c.com>
2017-05-08 15:41:04 -0700

    Cleaning up the js to pass the JS Hint warnings.

--- _inc/comment-likes.js
+++ _inc/comment-likes.js
@@ -1,41 +1,35 @@
-function isScrolledIntoView( el ) {
-    var elemTop = el.getBoundingClientRect().top;
-    var elemBottom = el.getBoundingClientRect().bottom;
+function processCommentLikes() {
+	var commentLikeButtons = document.getElementsByClassName( 'comment-likes' );
 
-    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
-
-    return isVisible;
-}
-
-var delayedExec = function( after, fn ) {
-    var timer;
-    return function() {
-        timer && clearTimeout(timer);
-        timer = setTimeout(fn, after);
-    };
-};
-
-function processCommentLike() {
-	const commentLikeButtons = document.getElementsByClassName( 'comment-likes' );
-
-	for( let i = 0; i < commentLikeButtons.length; i++ ) {
-		let commentLikeButton = commentLikeButtons[ i ];
+	for ( var i = 0; i < commentLikeButtons.length; i++ ) {
+		var commentLikeButton = commentLikeButtons[ i ];
 		if ( isScrolledIntoView( commentLikeButton ) ) {
 			if ( ! commentLikeButton.innerHTML.startsWith( '<iframe' ) ) {
 				commentLikeButton.innerHTML = '<iframe class="post-likes-widget jetpack-likes-widget" name="like-post-frame-124316610-1-58ff939ed8d6a" height="55px" width="100%" frameborder="0" src="//widgets.wp.com/notifications"></iframe>';
 			}
-		} else {
-			if ( ! commentLikeButton.innerHTML.startsWith( 'Loading' ) ) {
-				commentLikeButton.innerHTML = 'Loading iframe...';
-				console.log( 'unloaded iframe for: ' + commentLikeButton.id );
-			}
+		} else if ( ! commentLikeButton.innerHTML.startsWith( 'Loading' ) ) {
+			commentLikeButton.innerHTML = 'Loading iframe...';
 		}
 	}
 }
 
+function isScrolledIntoView( el ) {
+	var elemTop = el.getBoundingClientRect().top;
+	var elemBottom = el.getBoundingClientRect().bottom;
+
+	return ( elemTop >= 0 ) && ( elemBottom <= window.innerHeight );
+}
+
+var delayedExec = function( after, fn ) {
+	var timer;
+	return function() {
+		timer && clearTimeout( timer );
+		timer = setTimeout( fn, after );
+	};
+};
+
 var onScrollStopped = delayedExec( 250, function() {
-    console.log( 'end of scrolling' );
-		processCommentLike();
+	processCommentLikes();
 } );
 
 window.addEventListener( 'scroll', onScrollStopped, true );
