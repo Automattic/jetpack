44f86657a784d422f57c130394c1b155673c1509
Igor Zinovyev <igor.zinovyev@automattic.com>
2017-05-08 13:51:04 +0000

    Removed duplicate lines.

--- sal/class.json-api-site-base.php
+++ sal/class.json-api-site-base.php
@@ -85,12 +85,6 @@ abstract class SAL_Site {
 	abstract public function get_plan();
 
 	abstract public function get_ak_vp_bundle_enabled();
-	
-	abstract public function get_jetpack_seo_front_page_description();
-
-	abstract public function get_jetpack_seo_title_formats();
-
-	abstract public function get_verification_services_codes();
 
 	abstract public function get_podcasting_archive();
 
