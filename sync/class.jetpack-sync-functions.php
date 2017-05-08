12eb033b5b7f4dff9174fdf2219f7e1e27b7c852
Rob Landers <landers.robert@gmail.com>
2017-05-08 14:02:45 -0400

    Sync: Allow detecting active plugins

--- sync/class.jetpack-sync-functions.php
+++ sync/class.jetpack-sync-functions.php
@@ -213,12 +213,24 @@ class Jetpack_Sync_Functions {
 		return $normalized_url;
 	}
 
+	public static function get_plugins_filter( $plugins ) {
+		// todo: make sure this works with network sites
+		$active_plugins = get_option( 'active_plugins' );
+
+		foreach ( $plugins as $plugin_file => &$plugin_meta ) {
+			$plugin_meta['Active'] = in_array( $plugin_file, $active_plugins );
+		}
+
+		return $plugins;
+	}
+
 	public static function get_plugins() {
 		if ( ! function_exists( 'get_plugins' ) ) {
 			require_once ABSPATH . 'wp-admin/includes/plugin.php';
 		}
 
 		/** This filter is documented in wp-admin/includes/class-wp-plugins-list-table.php */
+		add_filter( 'all_plugins', array( 'Jetpack_Sync_Functions', 'get_plugins_filter' ) );
 		return apply_filters( 'all_plugins', get_plugins() );
 	}
 
