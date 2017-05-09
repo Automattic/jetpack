a27d514632966b7dd967eac502f74726ca1786e1
Enej Bajgoric <enej.bajgoric@gmail.com>
2017-05-09 13:25:52 -0700

    Sync: Add sync actions when core updates (#7108)
    * Sync: Add sync actions when core updates
    
    The new action lets us know when core gets updated to a specific
    version and it was autoupdated and if it was reinstalled.
    
    * Split the events into 3 different events.
    
    * Remove var_dump
    
    * Minor fixes, spelling and more clear variable names
    
    * Fix typo, make the tests pass
    
    * Update with fixes, added doc, fixed names
    
    * Minor grammer fix
    
    * minor now we user ===

--- tests/php/sync/test_class.jetpack-sync-updates.php
+++ tests/php/sync/test_class.jetpack-sync-updates.php
@@ -55,26 +55,78 @@ class WP_Test_Jetpack_Sync_Updates extends WP_Test_Jetpack_Sync_Base {
 
 	public function test_sync_wp_version() {
 		global $wp_version;
+		$previous_version = $wp_version;
 		$this->assertEquals( $wp_version, $this->server_replica_storage->get_callable( 'wp_version' ) );
 
 		// Lets pretend that we updated the wp_version to bar.
 		$wp_version = 'bar';
 		do_action( 'upgrader_process_complete', null, array( 'action' => 'update', 'type' => 'core' ) );
 		$this->sender->do_sync();
-		$this->assertEquals( $wp_version, $this->server_replica_storage->get_callable( 'wp_version' ) );
+		$wp_version = $previous_version;
+		$this->assertEquals( 'bar', $this->server_replica_storage->get_callable( 'wp_version' ) );
 	}
 
 	public function test_automatic_updates_complete_sync_action() {
 		// wp_maybe_auto_update();
 		do_action( 'automatic_updates_complete', array( 'core' => array(
-			'item'     => array('somedata'),
+			'item'     => array( 'somedata' ),
 			'result'   => 'some more data',
 			'name'     => 'WordPress 4.7',
-			'messages' => array('it worked.') ) ) );
+			'messages' => array( 'it worked.' ) ) ) );
 		$this->sender->do_sync();
 
 		$event = $this->server_event_storage->get_most_recent_event( 'automatic_updates_complete' );
 		$this->assertTrue( (bool) $event );
+	}
+
+	public function test_update_core_successfully_sync_action() {
+		global $wp_version, $pagenow;
+		$current_page = $pagenow;
+
+		$pagenow = 'update-core.php';
+		// Remove the _redirect_to_about_wordpress action
+		remove_action( '_core_updated_successfully', '_redirect_to_about_wordpress' );
+		do_action( '_core_updated_successfully', 'foo' );
+		$pagenow = $current_page; // revert page now
+		$this->sender->do_sync();
+
+		$event = $this->server_event_storage->get_most_recent_event( 'jetpack_sync_core_updated_successfully' );
+
+		$this->assertTrue( (bool) $event );
+		$this->assertEquals( $event->args[0], 'foo' ); // Old Version
+		$this->assertEquals( $event->args[1], $wp_version ); // New version
+	}
+
+	public function test_autoupdate_core_successfully_sync_action() {
+		global $wp_version;
+
+		// Remove the _redirect_to_about_wordpress action
+		remove_action( '_core_updated_successfully', '_redirect_to_about_wordpress' );
+		do_action( '_core_updated_successfully', 'foo' );
+		$this->sender->do_sync();
+
+		$event = $this->server_event_storage->get_most_recent_event( 'jetpack_sync_core_autoupdated_successfully' );
+
+		$this->assertTrue( (bool) $event );
+		$this->assertEquals( $event->args[0], 'foo' ); // Old Version
+		$this->assertEquals( $event->args[1], $wp_version ); // New version
+	}
+
+	public function test_reinstall_core_successfully_sync_action() {
+		global $_GET;
+		$_GET['action'] = 'do-core-reinstall';
+
+		// Remove the _redirect_to_about_wordpress action
+		remove_action( '_core_updated_successfully', '_redirect_to_about_wordpress' );
+		do_action( '_core_updated_successfully', 'foo' );
+		$this->sender->do_sync();
+
+		unset( $_GET['action'] );
+
+		$event = $this->server_event_storage->get_most_recent_event( 'jetpack_sync_core_reinstalled_successfully' );
+
+		$this->assertTrue( (bool) $event );
+		$this->assertEquals( $event->args[0], 'foo' ); // New version
 	}
 
 }
