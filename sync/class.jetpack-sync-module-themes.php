f29c81c8159936369afa0946e2c192cf2004e34e
Michael Turk <turk.michael@gmail.com>
2017-05-11 16:33:42 -0400

    Jetpack Sync: Update

--- sync/class.jetpack-sync-module-themes.php
+++ sync/class.jetpack-sync-module-themes.php
@@ -15,7 +15,7 @@ class Jetpack_Sync_Module_Themes extends Jetpack_Sync_Module {
 		add_action( 'jetpack_deleted_theme', $callable );
 		add_filter( 'wp_redirect', array( $this, 'detect_theme_edit' ) );
 		add_action( 'jetpack_edited_theme', $callable, 10, 2 );
-		add_action( 'update_site_option_allowedthemes', array( $this, 'sync_network_allowed_themes_change' ) );
+		add_action( 'update_site_option_allowedthemes', array( $this, 'sync_network_allowed_themes_change' ), 10, 4 );
 		add_action( 'jetpack_network_disabled_themes', $callable );
 		add_action( 'jetpack_network_enabled_themes', $callable );
 
