4da19d150b2ea1f1a90b5d88872e8a957e8470e9
Jason Johnston <jhnstn@users.noreply.github.com>
2017-05-12 14:26:10 -0700

    Revert "Add podcasting plugin via Composer"

--- projects/plugins/wpcomsh/wpcomsh.php
+++ projects/plugins/wpcomsh/wpcomsh.php
@@ -39,11 +39,6 @@ require_once( 'widgets/reservations.php' );
 require_once( 'widgets/tlkio/tlkio.php' );
 require_once( 'widgets/twitter.php' );
 
-# autoload composer sourced plugins
-if ( file_exists( 'vendor/autoload.php' ) ) {
-  require_once( 'vendor/autoload.php' );
-}
-
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	require_once WPCOMSH__PLUGIN_DIR_PATH . '/class.cli-commands.php';
 }
