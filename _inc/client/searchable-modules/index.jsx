d172e7c313db4cda9f699ca5cea5cd6467afee70
Derek Smart <smart@automattic.com>
2017-05-16 09:20:35 -0400

    Refactor to use settings actions instead of deprecated activateModule action.  Added  to the list

--- _inc/client/searchable-modules/index.jsx
+++ _inc/client/searchable-modules/index.jsx
@@ -12,77 +12,81 @@ import Banner from 'components/banner';
 /**
  * Internal dependencies
  */
-import { getModules, isModuleActivated, activateModule } from 'state/modules';
+import { ModuleSettingsForm as moduleSettingsForm } from 'components/module-settings/module-settings-form';
+import { getModules } from 'state/modules';
 import { isModuleFound } from 'state/search';
 import SettingsCard from 'components/settings-card';
 import SettingsGroup from 'components/settings-group';
 import { userCanManageModules } from 'state/initial-state';
 import { isDevMode, isUnavailableInDevMode } from 'state/connection';
 
-class SearchableModules extends Component {
-	render() {
-		// Only admins plz
-		if ( ! this.props.canManageModules ) {
-			return null;
-		}
-
-		// Only render if search terms present
-		const searchTerms = this.props.searchTerm;
-		if ( searchTerms.length < 3 ) {
-			return null;
-		}
-
-		// Only should be features that don't already have a UI, and we want to reveal in search.
-		const whitelist = [
-			'contact-form',
-			'custom-css',
-			'enhanced-distribution',
-			'json-api',
-			'latex',
-			'notes',
-			'omnisearch',
-			'shortcodes',
-			'shortlinks',
-			'widget-visibility',
-			'widgets'
-		];
-
-		const allModules = this.props.modules,
-			results = [];
-		forEach( allModules, ( moduleData, slug ) => {
-			if (
-				this.props.isModuleFound( slug ) &&
-				includes( whitelist, slug )
-			) {
-				// Not available in dev mode
-				if ( this.props.isDevMode && this.props.isUnavailableInDevMode( moduleData.module ) ) {
-					return results.push( <ActiveCard key={ slug } moduleData={ moduleData } devMode={ true } /> );
-				}
-
-				if ( this.props.isModuleActive( moduleData.module ) ) {
-					results.push( <ActiveCard key={ slug } moduleData={ moduleData } /> );
-				} else {
-					results.push(
-						<Banner
-							className="jp-searchable-banner"
-							key={ slug }
-							callToAction={ __( 'Activate' ) }
-							description={ moduleData.description }
-							href="javascript:void( 0 )"
-							icon="cog"
-							onClick={ this.props.activateModule.bind( null, moduleData.module ) }
-							title={ moduleData.name }
-						/>
-					);
-				}
+export const SearchableModules = moduleSettingsForm(
+	class extends Component {
+		render() {
+			// Only admins plz
+			if ( ! this.props.canManageModules ) {
+				return null;
 			}
-		} );
 
-		return (
-			<div>{ results }</div>
-		);
+			// Only render if search terms present
+			const searchTerms = this.props.searchTerm;
+			if ( searchTerms.length < 3 ) {
+				return null;
+			}
+
+			// Only should be features that don't already have a UI, and we want to reveal in search.
+			const whitelist = [
+				'contact-form',
+				'custom-css',
+				'enhanced-distribution',
+				'json-api',
+				'latex',
+				'monitor',
+				'notes',
+				'omnisearch',
+				'shortcodes',
+				'shortlinks',
+				'widget-visibility',
+				'widgets'
+			];
+
+			const allModules = this.props.modules,
+				results = [];
+			forEach( allModules, ( moduleData, slug ) => {
+				if (
+					this.props.isModuleFound( slug ) &&
+					includes( whitelist, slug )
+				) {
+					// Not available in dev mode
+					if ( this.props.isDevMode && this.props.isUnavailableInDevMode( moduleData.module ) ) {
+						return results.push( <ActiveCard key={ slug } moduleData={ moduleData } devMode={ true } /> );
+					}
+
+					if ( this.props.getOptionValue( moduleData.module ) ) {
+						results.push( <ActiveCard key={ slug } moduleData={ moduleData } /> );
+					} else {
+						results.push(
+							<Banner
+								className="jp-searchable-banner"
+								key={ slug }
+								callToAction={ __( 'Activate' ) }
+								description={ moduleData.description }
+								href="javascript:void( 0 )"
+								icon="cog"
+								onClick={ this.props.updateOptions.bind( null, { [ moduleData.module ]: true } ) }
+								title={ moduleData.name }
+							/>
+						);
+					}
+				}
+			} );
+
+			return (
+				<div>{ results }</div>
+			);
+		}
 	}
-}
+);
 
 SearchableModules.propTypes = {
 	searchTerm: React.PropTypes.string
@@ -119,17 +123,9 @@ export default connect(
 		return {
 			modules: getModules( state ),
 			isModuleFound: ( module_name ) => isModuleFound( state, module_name ),
-			isModuleActive: ( module_name ) => isModuleActivated( state, module_name ),
 			canManageModules: userCanManageModules( state ),
 			isUnavailableInDevMode: module_name => isUnavailableInDevMode( state, module_name ),
 			isDevMode: isDevMode( state )
-		};
-	},
-	( dispatch ) => {
-		return {
-			activateModule: ( slug ) => {
-				return dispatch( activateModule( slug ) );
-			}
 		};
 	}
 )( SearchableModules );
