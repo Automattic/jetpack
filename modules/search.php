19e5a5451136e6310fbd73460982fb1ce0c86a76
Nick Daugherty <ndaugherty987@gmail.com>
2017-05-18 16:20:42 -0600

    Initial commit of Search module
    Includes module file, main class, widget, and Elasticsearch helper
    library

---
+++ modules/search.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Module Name: Search
+ * Module Description: Enhanced search, powered by Elasticsearch
+ * First Introduced: 5.0
+ * Free: false
+ * Requires Connection: Yes
+ * Auto Activate: No
+ * Feature: Search
+ * Additional Search Queries: search
+ */
+
+require_once( __DIR__ . '/search/class.jetpack-search.php' );
