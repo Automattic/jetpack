ba8dcc38c5141c2c30130cd812a11b8b851c3469
Timmy Crawford <timmydcrawford@gmail.com>
2017-05-22 11:38:41 -0700

    Support selecting existing videopress videos from the library.

--- modules/videopress/js/media-video-widget-extensions.js
+++ modules/videopress/js/media-video-widget-extensions.js
@@ -6,13 +6,23 @@ window.wp = window.wp || {};
 		// Over-ride core media_video#mapMediaToModelProps to set the url based upon videopress_guid if it exists.
 		wp.mediaWidgets.controlConstructors.media_video.prototype.mapMediaToModelProps = ( function( originalMapMediaToModelProps ) {
 			return function( mediaFrameProps ) {
-				var newProps, originalProps;
+				var newProps, originalProps, videoPressGuid;
 				originalProps = originalMapMediaToModelProps.call( this, mediaFrameProps );
 				newProps = _.extend( {}, originalProps );
 				
+				// API response on new media will have the guid at videopress.guid.
 				if ( mediaFrameProps.videopress && mediaFrameProps.videopress.guid ) {
+					videoPressGuid = mediaFrameProps.videopress.guid;
+				}
+
+				// Selecting an existing VideoPress file will have the guid at .videopress_guid[ 0 ].
+				if ( ! videoPressGuid && mediaFrameProps.videopress_guid && mediaFrameProps.videopress_guid.length ) {
+					videoPressGuid = mediaFrameProps.videopress_guid[ 0 ];
+				}
+
+				if ( videoPressGuid ) {
 					newProps = _.extend( {}, originalProps, {
-						url: 'https://videopress.com/v/' + mediaFrameProps.videopress.guid,
+						url: 'https://videopress.com/v/' + videoPressGuid,
 						attachment_id: 0
 					});
 				}
