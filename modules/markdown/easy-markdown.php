edddd6bd8e02f383dc0a0bedc65ecdec09cda7f4
George Stephanis <George@Stephanis.info>
2017-05-24 10:16:14 -0400

    Sync/2may2017 markdown (#7107)
    * Easy Markdown: saving post_content_filtered even for autosaves
    
    There is an issue with autosaves as they can't be properly restored as the post_content_filtered is not being populated for them.
    
    If someone attempts to restore an autosave, they would get an empty texteditor, as the content is being read from post_content_filtered.
    
    Populating and storing the post_content_filtered even for autosaves addresses this issue.
    
    /z 50817
    
    Merges r134728-wpcom.
    
    * Markdown Bug Fixes and Updates. Also, Jetpack_Options plan sync.
    
    This should bring four files into sync with Jetpack's versions. Hurrah!
    
    @see https://[private link]
    
    Merges r148841-wpcom.
    
    * Markdown: add docblock to new filter.
    
    @see https://github.com/Automattic/jetpack/pull/7107/

--- modules/markdown/easy-markdown.php
+++ modules/markdown/easy-markdown.php
@@ -455,6 +455,8 @@ class WPCom_Markdown {
 			$post_data['post_content'] = apply_filters( 'content_save_pre', $post_data['post_content'] );
 		} elseif ( 0 === strpos( $post_data['post_name'], $post_data['post_parent'] . '-autosave' ) ) {
 			// autosaves for previews are weird
+			/** This filter is already documented in modules/markdown/easy-markdown.php */
+			$post_data['post_content_filtered'] = apply_filters( 'wpcom_untransformed_content', $post_data['post_content'] );
 			$post_data['post_content'] = $this->transform( $post_data['post_content'], array( 'id' => $post_data['post_parent'] ) );
 			/** This filter is already documented in core/wp-includes/default-filters.php */
 			$post_data['post_content'] = apply_filters( 'content_save_pre', $post_data['post_content'] );
