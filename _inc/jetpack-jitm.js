801c05c8db109cea04cfcaaae36f2026e6629575
Rob Landers <landers.robert@gmail.com>
2017-05-25 12:48:00 -0400

    Bring in Jeff's design work

--- _inc/jetpack-jitm.js
+++ _inc/jetpack-jitm.js
@@ -1,17 +1,26 @@
 jQuery( document ).ready( function( $ ) {
 	var templates = {
 		'default': function( envelope ) {
-			var html = '<div class="jp-jitm" data-stats_url="' + envelope.jitm_stats_url + '"> ' +
-	'<a href="#" data-module="' + envelope.feature_class + '" class="dismiss"><span class="genericon genericon-close"></span></a>' + envelope.content.icon +
-	'<p class="msg">' +
-		envelope.content.message +
-	'</p>';
-			if ( envelope.CTA.message ) {
-				html += '<p>' +
-		'<a href="' + envelope.url + '" target="_blank" title="' + envelope.CTA.message + '" data-module="' + envelope.id + '" data-jptracks-name="nudge_click" data-jptracks-prop="jitm-' + envelope.id + '" class="button button-jetpack launch jptracks">' + envelope.CTA.message + '</a>' +
-	'</p>';
+			var html = '<div class="jitm-card jitm-banner ' + (
+					envelope.CTA.message ? 'has-call-to-action' : ''
+				) + ' is-upgrade-premium ' + envelope.content.classes + '" data-stats_url="' + envelope.jitm_stats_url + '">';
+			html += '<div class="jitm-banner__icon-plan">' + envelope.content.icon + '</div>';
+			html += '<div class="jitm-banner__content">';
+			html += '<div class="jitm-banner__info">';
+			html += '<div class="jitm-banner__title">' + envelope.content.message + '</div>';
+			if ( envelope.content.description && envelope.content.description !== '' ) {
+				html += '<div class="jitm-banner__description">' + envelope.content.description + '</div>';
 			}
 			html += '</div>';
+			if ( envelope.CTA.message ) {
+				html += '<div class="jitm-banner__action">';
+				html += '<a href="' + envelope.url + '" target="_blank" title="' + envelope.CTA.message + '" data-module="' + envelope.feature_class + '" type="button" class="jitm-button is-compact ' + ( envelope.CTA.primary ? 'is-primary' : '' ) + ' jptracks" data-jptracks-name="nudge_click" data-jptracks-prop="jitm-' + envelope.id + '">' + envelope.CTA.message + '</a>';
+				html += '</div>';
+			}
+			html += '<a href="#" data-module="' + envelope.feature_class + '" class="jitm-banner__dismiss"></a>';
+			html += '</div>';
+			html += '</div>';
+
 			return $( html );
 		}
 	};
@@ -45,7 +54,7 @@ jQuery( document ).ready( function( $ ) {
 			}
 
 			var $template = templates[ template ]( response[ i ] );
-			$template.find( '.dismiss' ).click( render( i, $template ) );
+			$template.find( '.jitm-banner__dismiss' ).click( render( i, $template ) );
 			$el.append( $template );
 		}
 	};
