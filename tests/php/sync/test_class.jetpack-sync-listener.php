8a169490564e37e700eca298621d072c63e78d66
Enej Bajgoric <enej.bajgoric@gmail.com>
2017-05-30 10:38:59 -0700

    Format code to make the arrows align

--- tests/php/sync/test_class.jetpack-sync-listener.php
+++ tests/php/sync/test_class.jetpack-sync-listener.php
@@ -85,23 +85,23 @@ class WP_Test_Jetpack_Sync_Listener extends WP_Test_Jetpack_Sync_Base {
 		$queue->reset(); // remove any actions that already got queued
 
 		$this->factory->post->create();
-		$current_user =  wp_get_current_user();
+		$current_user  = wp_get_current_user();
 		$example_actor = array(
-			'display_name' => $current_user->display_name,
-			'user_email'   => $current_user->user_email,
-			'user_roles'   => $current_user->roles,
+			'display_name'    => $current_user->display_name,
+			'user_email'      => $current_user->user_email,
+			'user_roles'      => $current_user->roles,
 			'translated_role' => Jetpack::translate_current_user_to_role(),
-			'ip'           => $_SERVER['REMOTE_ADDR'],
-			'is_cron'      => defined( 'DOING_CRON' ) ? DOING_CRON : false,
-			'is_wp_admin'  => is_admin(),
-			'is_rest'      => defined( 'REST_API_REQUEST' )? REST_API_REQUEST : false,
-			'is_xmlrpc'    => defined( 'XMLRPC_REQUEST' ) ? XMLRPC_REQUEST : false,
-			'is_wp_rest'   => defined( 'REST_REQUEST' )? REST_REQUEST : false,
-			'is_ajax'      => defined( 'DOING_AJAX' ) ? DOING_AJAX : false,
+			'ip'              => $_SERVER['REMOTE_ADDR'],
+			'is_cron'         => defined( 'DOING_CRON' ) ? DOING_CRON : false,
+			'is_wp_admin'     => is_admin(),
+			'is_rest'         => defined( 'REST_API_REQUEST' ) ? REST_API_REQUEST : false,
+			'is_xmlrpc'       => defined( 'XMLRPC_REQUEST' ) ? XMLRPC_REQUEST : false,
+			'is_wp_rest'      => defined( 'REST_REQUEST' ) ? REST_REQUEST : false,
+			'is_ajax'         => defined( 'DOING_AJAX' ) ? DOING_AJAX : false,
 		);
 
 		$all = $queue->get_all();
-		foreach( $all as $queue_item ) {
+		foreach ( $all as $queue_item ) {
 			list( $current_filter, $args, $current_user_id, $microtime, $is_importing, $actor ) = $queue_item->value;
 			$this->assertEquals( $actor, $example_actor );
 		}
