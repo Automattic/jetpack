11f73fb6b8f42b554c7d531a795d9da02e8777e1
Daniel Walmsley <goldsounds@gmail.com>
2017-06-02 03:44:56 -0700

    Add support for --url for multisite (#7292)
    * Add support for --url for multisite
    
    * pass URL to plugin activation script too

--- bin/partner-cancel.sh
+++ bin/partner-cancel.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
 cd $SCRIPT_DIR
 
 usage () {
-    echo "Usage: partner-cancel.sh --partner_id=partner_id --partner_secret=partner_secret"
+    echo "Usage: partner-cancel.sh --partner_id=partner_id --partner_secret=partner_secret [--url=http://example.com]"
 }
 
 for i in "$@"; do
@@ -17,6 +17,9 @@ for i in "$@"; do
                                     shift
                                     ;;
         -s=* | --partner_secret=* ) CLIENT_SECRET="${i#*=}"
+                                    shift
+                                    ;;
+        -u=* | --url=* )            SITE_URL="${i#*=}"
                                     shift
                                     ;;
         -h | --help )               usage
@@ -41,8 +44,8 @@ fi
 ACCESS_TOKEN_JSON=`curl https://$JETPACK_START_API_HOST/oauth2/token --silent --header "Host: public-api.wordpress.com" -d "grant_type=client_credentials&client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&scope=jetpack-partner"`
 
 # silently ensure Jetpack is active
-wp plugin activate jetpack >/dev/null 2>&1
+wp plugin activate jetpack --url="$SITE_URL" >/dev/null 2>&1
 
 # cancel the partner plan
-wp jetpack partner_cancel "$ACCESS_TOKEN_JSON"
+wp jetpack partner_cancel "$ACCESS_TOKEN_JSON" --url="$SITE_URL"
 
