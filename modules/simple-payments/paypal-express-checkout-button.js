4fa6f1eba0ecef04caf19fd81012e96fe9c63d40
artpi <artur.piszek@gmail.com>
2017-07-03 14:41:26 +0200

    Pass proper JS

--- modules/simple-payments/paypal-express-checkout-button.js
+++ modules/simple-payments/paypal-express-checkout-button.js
@@ -1,3 +1,28 @@
-function PaypalExpressCheckoutButton( product ) {
-	console.log( 'pp button', product );
-}
+/* global paypal */
+/* exported PaypalExpressCheckout */
+var PaypalExpressCheckout = {
+	constants: {
+		createPaymentEndpoint: '',
+		executePaymentEndpoint: '',
+	},
+	renderButton: function( id ) {
+		paypal.Button.render( {
+			commit: true,
+			payment: function() {
+				return paypal.request.post( PaypalExpressCheckout.constants.createPaymentEndpoint ).then( function( data ) {
+					return data.id;
+				} );
+			},
+			onAuthorize: function( data ) {
+				return paypal.request.post( PaypalExpressCheckout.constants.executePaymentEndpoint, {
+					paymentID: data.paymentID,
+					payerID: data.payerID
+				} ).then( function( payment ) {
+					console.log( 'payment: ', payment );
+					alert( 'success!' );
+				} );
+			}
+
+		}, id );
+	}
+};
