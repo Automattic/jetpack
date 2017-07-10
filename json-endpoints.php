6cf98a0d58092b5011995c59c3b3c7bd5e282c4d
Eric Binnion <eric.binnion@automattic.com>
2017-07-10 16:37:18 +0000

    API: add net_neutrality to site settings endpoint
    See: https://[private link]
    
    Merges r158940-wpcom.

--- json-endpoints.php
+++ json-endpoints.php
@@ -2764,6 +2764,7 @@ new WPCOM_JSON_API_Site_Settings_Endpoint( array(
 		'site_icon'                    => '(int) Media attachment ID to use as site icon. Set to zero or an otherwise empty value to clear',
 		'api_cache'                    => '(bool) Turn on/off the Jetpack JSON API cache',
 		'posts_per_page'               => '(int) Number of posts to show on blog pages',
+		'net_neutrality'               => '(bool) Whether to show the net neutrality modal for a site',
 	),
 
 	'response_format' => array(
@@ -2848,7 +2849,8 @@ new WPCOM_JSON_API_Site_Settings_V1_2_Endpoint( array(
 		Jetpack_SEO_Titles::TITLE_FORMATS_OPTION  => '(array) SEO meta title formats. Allowed keys: front_page, posts, pages, groups, archives',
 		'amp_is_enabled'                       => '(bool) Whether AMP is enabled for this site',
 		'podcasting_archive'                   => '(string) The post category, if any, used for publishing podcasts',
-		'api_cache'                            => '(bool) Turn on/off the Jetpack JSON API cache'
+		'api_cache'                            => '(bool) Turn on/off the Jetpack JSON API cache',
+		'net_neutrality'                       => '(bool) Whether the net neutrality modal is enabled for this site',
 
 	),
 
