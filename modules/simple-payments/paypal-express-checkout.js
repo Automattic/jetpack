37fddc3b68d46b7dec980afc0faed9142fad17b4
retrofox <rdsuarez@gmail.com>
2017-07-20 12:13:29 -0300

    simple-payments: print message when the transaction ends.

--- modules/simple-payments/paypal-express-checkout.js
+++ modules/simple-payments/paypal-express-checkout.js
@@ -4,6 +4,44 @@
  * of simple-payments module.
  */
 
+/**
+ * Get the DOM element-placeholder used to show message
+ * about the transaction. If it doesn't exist then the function will create a new one.
+ * 
+ * @param  string buttonDomId id of the payment button placeholder
+ * @return Element the dom element to print the message
+ */
+var getButtonMessageElement = function ( buttonDomId ) {
+	var messageDomId = buttonDomId + '_message';
+
+	var domButtonElement = document.getElementById( buttonDomId );
+	var domMessageElement = document.getElementById( messageDomId );
+
+	if ( domMessageElement ) {
+		return domMessageElement;
+	}
+
+	// create dom message element
+	domMessageElement = document.createElement( 'div' );
+	domMessageElement.setAttribute( 'class', 'jetpack-simple-payments-message-placeholder' );
+	domMessageElement.setAttribute( 'id', messageDomId );
+	domMessageElement.style.display = 'none';
+
+	// inject into the DOM Tree
+	domButtonElement.appendChild( domMessageElement );
+
+	return domMessageElement;
+}
+
+var showMessage = function( message, el ) {
+	// show message 500ms after Paypal popup is closed
+	setTimeout( function() {
+		el.style.display = 'block';
+		el.style.color = '#06F';
+		el.innerHTML = message;
+	}, 1000 );
+}
+
 /* global paypal */
 /* exported PaypalExpressCheckout */
 /* jshint unused:false */
@@ -37,6 +75,10 @@ var PaypalExpressCheckout = {
 		if ( ! paypal ) {
 			throw new Error( 'PayPal module is required by PaypalExpressCheckout' );
 		}
+
+		// message DOM element instance
+		var paypalMessagePlaceholder;
+
 		paypal.Button.render( {
 			env: env,
 			commit: true,
@@ -44,26 +86,43 @@ var PaypalExpressCheckout = {
 				label: 'pay',
 				color: 'blue'
 			},
-			payment: function() {
+			payment: function( paymentData ) {
+				paypalMessagePlaceholder = getButtonMessageElement( domId + '_button' );
+
 				var payload = {
 					number: PaypalExpressCheckout.getNumberOfItems( domId + '_number', enableMultiple ),
 					buttonId: buttonId,
 					env: env
 				};
-				return paypal.request.post( PaypalExpressCheckout.getCreatePaymentEndpoint( blogId ), payload ).then( function( data ) {
-					return data.id;
+
+				return paypal.request.post( PaypalExpressCheckout.getCreatePaymentEndpoint( blogId ), payload )
+				.then( function( paymentResponse ) {
+					return paymentResponse.id;
+				} )
+				.catch( function( paymentError ) {
+					showMessage( paymentError, paypalMessagePlaceholder );
 				} );
 			},
-			onAuthorize: function( data ) {
-				return paypal.request.post( PaypalExpressCheckout.getExecutePaymentEndpoint( blogId, data.paymentID ), {
+			onAuthorize: function( onAuthData ) {
+				return paypal.request.post( PaypalExpressCheckout.getExecutePaymentEndpoint( blogId, onAuthData.paymentID ), {
 					buttonId: buttonId,
-					payerId: data.payerID,
+					payerId: onAuthData.payerID,
 					env: env
-				} ).then( function() {
+				} )
+				.then( function( authResponse ) {
+					var payerInfo = authResponse.payer.payer_info;
+					var message = 'Thanks <strong>' + payerInfo.first_name + '</strong>! ' +
+						'The purchase has been successful.<br />' +
+						'For more details, an email has been sent to the <em>' + payerInfo.email + '<em>.';
+
+					showMessage( message, paypalMessagePlaceholder );
+
 					// TODO: handle success, errors, messaging, etc, etc.
 					/* jshint ignore:start */
-					alert( 'success!' );
 					/* jshint ignore:end */
+				} )
+				.catch( function( authError ) {
+					// console.log( 'authError: %o', authError );
 				} );
 			}
 
