a6a1c97bd0e4a8d010a3a6f31664d5445c2b9ccd
artpi <artur.piszek@gmail.com>
2017-07-24 18:12:16 +0200

    html tweaks

--- modules/simple-payments/simple-payments.php
+++ modules/simple-payments/simple-payments.php
@@ -109,13 +109,17 @@ class Jetpack_Simple_Payments {
 		return "
 <div class='{$data['class']} ${cssPrefix}-wrapper'>
 	<p class='${cssPrefix}-purchase-message'></p>
-	{$image}
-	<div class='${cssPrefix}-title'><p>{$data['title']}</p></div>
-	<div class='${cssPrefix}-description'><p>{$data['description']}</p></div>
-	<div class='${cssPrefix}-purchase-box'>
-		<div class='${cssPrefix}-price'><p>{$data['price']}</p></div>
-		{$items}
-		<div class='${cssPrefix}-button' id='{$data['dom_id']}_button'></div>
+	<div class='${cssPrefix}-product'> 
+		{$image}
+		<div class='${cssPrefix}-details'> 
+			<div class='${cssPrefix}-title'><p>{$data['title']}</p></div>
+			<div class='${cssPrefix}-description'><p>{$data['description']}</p></div>
+			<div class='${cssPrefix}-purchase-box'>
+				<div class='${cssPrefix}-price'><p>{$data['price']}</p></div>
+				{$items}
+				<div class='${cssPrefix}-button' id='{$data['dom_id']}_button'></div>
+			</div>
+		</div>
 	</div>
 </div>
 		";
