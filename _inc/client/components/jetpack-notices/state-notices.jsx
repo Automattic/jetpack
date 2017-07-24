9b7fcd6434a7df3443a3f50469bfee59599cdf71
Elio Rivero <eliorivero@gmail.com>
2017-07-24 19:19:54 -0300

    Settings UI: link to a release post in the notice stating that it's a new Jetpack version

--- _inc/client/components/jetpack-notices/state-notices.jsx
+++ _inc/client/components/jetpack-notices/state-notices.jsx
@@ -167,12 +167,13 @@ const JetpackStateNotices = React.createClass( {
 		switch ( key ) {
 			// This is the message that is shown on first page load after a Jetpack plugin update.
 			case 'modules_activated' :
-				message = __( 'Welcome to {{s}}Jetpack %(jetpack_version)s{{/s}}!',
+				message = __( "Welcome to {{s}}Jetpack %(jetpack_version)s{{/s}}! {{a}}Learn about what's new{{/a}}.",
 					{
 						args: {
 							jetpack_version: this.props.currentVersion
 						},
 						components: {
+							a: <a href="https://jetpack.com/?p=20888" />,
 							s: <strong />
 						}
 					}
