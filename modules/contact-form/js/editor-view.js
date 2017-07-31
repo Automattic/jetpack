b677d2a6bc775923b697ba895964939547bd2136
George Stephanis <George@Stephanis.info>
2017-07-31 14:11:54 -0400

    Add in field template for url type. (#7554)
    * Add in field template for url type.
    
    * Add in name field.
    
    * URL field -- it's just a text field.
    
    I had been setting the type to url mistakenly -- but it's just a text
    field.

--- modules/contact-form/js/editor-view.js
+++ modules/contact-form/js/editor-view.js
@@ -17,7 +17,9 @@
 			'checkbox-multiple' : wp.template( 'grunion-field-checkbox-multiple' ),
 			select              : wp.template( 'grunion-field-select' ),
 			date                : wp.template( 'grunion-field-date' ),
-			text                : wp.template( 'grunion-field-text' )
+			text                : wp.template( 'grunion-field-text' ),
+			name                : wp.template( 'grunion-field-text' ),
+			url                 : wp.template( 'grunion-field-text' )
 		},
 		edit_template  : wp.template( 'grunion-field-edit' ),
 		editor_inline  : wp.template( 'grunion-editor-inline' ),
