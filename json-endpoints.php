ddd309fef408349f4137f575351928c377ee567a
Jonathan Sadowski <sadowski@automattic.com>
2017-08-11 12:39:14 -0500

    Add instantiation of bulk delete endpoint

--- json-endpoints.php
+++ json-endpoints.php
@@ -3389,3 +3389,32 @@ new WPCOM_JSON_API_Menus_Delete_Menu_Endpoint( array (
 		'headers' => array( 'authorization' => 'Bearer YOUR_API_TOKEN' ),
 	),
 ) );
+
+new WPCOM_JSON_API_Bulk_Delete_Post_Endpoint( array(
+	'description' => 'Delete multiple posts. Note: If the trash is enabled, this request will send non-trashed posts to the trash. Trashed posts will be permanently deleted.',
+	'group'       => 'posts',
+	'stat'        => 'posts:1:bulk-delete',
+	'min_version' => '1.1',
+	'max_version' => '1.1',
+	'method'      => 'POST',
+	'path'        => '/sites/%s/posts/delete',
+	'path_labels' => array(
+		'$site'    => '(int|string) Site ID or domain',
+	),
+	'request_format' => array(
+		'post_ids' => '(array) An array of Post IDs to delete or trash.',
+	),
+
+	'example_request'      => 'https://public-api.wordpress.com/rest/v1.1/sites/82974409/posts/delete',
+
+	'example_request_data' => array(
+		'headers' => array(
+			'authorization' => 'Bearer YOUR_API_TOKEN'
+		),
+
+		'body' => array(
+			'post_ids' => array( 881, 882 ),
+		),
+
+	)
+) );
