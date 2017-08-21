f0842c02b32b0456900e9c4edce6500710309b11
George Stephanis <George@Stephanis.info>
2017-08-21 11:30:20 -0400

    Remove omnisearch (#7590)
    * Purge the Omnisearch module itself.
    
    * React stuff
    
    * Build tooling
    
    * Legacy module info
    
    * Remove legacy plugin deactivation switch.
    
    * Omnisearch: replace in_array and array with boolean comparison

--- modules/omnisearch.php
+++ modules/omnisearch.php
@@ -1,18 +1,6 @@
 <?php
-
 /**
- * Module Name: Omnisearch
- * Module Description: Search your entire database from a single field in your dashboard.
- * Sort Order: 16
- * First Introduced: 2.3
- * Requires Connection: No
- * Auto Activate: Yes
- * Module Tags: Developers
- * Feature: General
- * Additional Search Queries: search
+ * Deprecated. No longer needed.
+ *
+ * @package Jetpack
  */
-
-// Only do Jetpack Omnisearch if there isn't already a Core WP_Omnisearch Class.
-if ( ! class_exists( 'WP_Omnisearch' ) ) {
-	require_once( dirname( __FILE__ ) . '/omnisearch/omnisearch-core.php' );
-}
