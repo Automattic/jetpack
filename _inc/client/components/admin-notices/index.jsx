866e45950f096563c4f46d06500ddea90d07dc6c
Derek Smart <smart@automattic.com>
2017-08-24 14:57:25 -0400

    make the success notice for vaultpress setup the correct design/colors

--- _inc/client/components/admin-notices/index.jsx
+++ _inc/client/components/admin-notices/index.jsx
@@ -7,11 +7,17 @@ const AdminNotices = React.createClass( {
 	componentDidMount() {
 		const $adminNotices = jQuery( this.refs.adminNotices );
 
-		const $vpNotice = jQuery( '.vp-notice' );
+		const $vpNotice = jQuery( '.vp-notice' ),
+			$success = jQuery( '#vp-notice' ).hasClass( 'vp-registered' );
+
+		const $warningOrSuccess = $success ? 'is-success' : 'is-warning';
+
 		if ( $vpNotice.length > 0 ) {
 			$vpNotice.each( function() {
-				let $notice = jQuery( this ).addClass( 'dops-notice is-warning vp-notice-jp' ).removeClass( 'wrap vp-notice' );
-				const icon = '<svg class="gridicon gridicons-notice dops-notice__icon" height="24" width="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g><path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm1 15h-2v-2h2v2zm0-4h-2l-.5-6h3l-.5 6z"></path></g></svg>';
+				const $notice = jQuery( this ).addClass( 'dops-notice vp-notice-jp ' + $warningOrSuccess ).removeClass( 'wrap vp-notice' );
+				const icon = $success
+					? '<svg class="gridicon gridicons-notice dops-notice__icon" height="24" width="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g><path d="M9 19.414l-6.707-6.707 1.414-1.414L9 16.586 20.293 5.293l1.414 1.414"/></g></svg>'
+					: '<svg class="gridicon gridicons-notice dops-notice__icon" height="24" width="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g><path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm1 15h-2v-2h2v2zm0-4h-2l-.5-6h3l-.5 6z"></path></g></svg>';
 				$notice.wrapInner( '<span class="dops-notice__content">' );
 				$notice.find( '.dops-notice__content' ).before( icon );
 				$notice.find( '.vp-message' ).removeClass( 'vp-message' ).addClass( 'dops-notice__text' );
