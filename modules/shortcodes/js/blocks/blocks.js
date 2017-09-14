6fecb77bd71edefa8e7ab4d10a7458294099e62e
Derek Smart <smart@automattic.com>
2017-09-14 12:02:52 -0700

    simplify the require for shortcodes, only need to include jsx file, no need for js

--- modules/shortcodes/js/blocks/blocks.js
+++ modules/shortcodes/js/blocks/blocks.js
@@ -1,1 +1,1 @@
-require( './block.js' );
+require( './block.jsx' );
--- modules/shortcodes/js/blocks/index.js
+++ modules/shortcodes/js/blocks/index.js
@@ -56,12 +56,12 @@
 
 	/* global wp, _, */
 	/* eslint react/react-in-jsx-scope: 0 */
-
 	var __ = wp.i18n.__;
 	var _wp$blocks = wp.blocks,
 	    registerBlockType = _wp$blocks.registerBlockType,
 	    Editable = _wp$blocks.Editable,
 	    children = _wp$blocks.source.children;
+
 
 	registerBlockType('gutenpack/test', {
 		title: __('Test'),
@@ -80,20 +80,28 @@
 				props.setFocus(_.extend({}, focus, { editable: 'title' }));
 			};
 
-			return wp.element.createElement('div', { className: props.className }, wp.element.createElement(Editable, {
-				tagName: 'label',
-				multiline: false,
-				placeholder: __('Write visible text…'),
-				value: attributes.title,
-				onChange: onChangeTitle,
-				focus: focusedEditable === 'title',
-				onFocus: onFocusTitle
-			}));
+			return React.createElement(
+				'div',
+				{ className: props.className },
+				React.createElement(Editable, {
+					tagName: 'label',
+					multiline: false,
+					placeholder: __('Write visible text…'),
+					value: attributes.title,
+					onChange: onChangeTitle,
+					focus: focusedEditable === 'title',
+					onFocus: onFocusTitle
+				})
+			);
 		},
 		save: function save(props) {
 			var title = props.attributes.title;
 
-			return wp.element.createElement('h4', null, title);
+			return React.createElement(
+				'h4',
+				null,
+				title
+			);
 		}
 	});
 
