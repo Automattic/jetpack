25d697999681d79e393c55e2268f459e3f6664d6
Thomas Guillot <thomasguillot@users.noreply.github.com>
2017-09-25 23:24:06 +0100

    Content Options: Keep Content Options up to date with wpcom (#7664)
    * Fix “Continue reading” translation issue with non-latin characters
    * Make sure a PayPal product Featured Image cannot be hidden via the
    options
    * Clearer ifs statements

--- modules/theme-tools/content-options/blog-display.php
+++ modules/theme-tools/content-options/blog-display.php
@@ -91,9 +91,16 @@ function jetpack_the_excerpt_to_the_content( $content ) {
 	if ( is_home() || is_archive() ) {
 		ob_start();
 		the_content( sprintf(
-			/* translators: %s: Name of current post. */
-			wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'jetpack' ), array( 'span' => array( 'class' => array() ) ) ),
-			the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			wp_kses(
+				/* translators: %s: Name of current post. Only visible to screen readers */
+				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'jetpack' ),
+				array(
+					'span' => array(
+						'class' => array(),
+					),
+				)
+			),
+			get_the_title()
 		) );
 		$content = ob_get_clean();
 	}
@@ -126,9 +133,16 @@ function jetpack_the_excerpt_customizer( $excerpt ) {
 	if ( is_home() || is_archive() ) {
 		ob_start();
 		the_content( sprintf(
-			/* translators: %s: Name of current post. */
-			wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'jetpack' ), array( 'span' => array( 'class' => array() ) ) ),
-			the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			wp_kses(
+				/* translators: %s: Name of current post. Only visible to screen readers */
+				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'jetpack' ),
+				array(
+					'span' => array(
+						'class' => array(),
+					),
+				)
+			),
+			get_the_title()
 		) );
 		$content = ob_get_clean();
 	}
