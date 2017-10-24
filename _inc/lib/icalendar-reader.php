0da0db61c7e814b099cb4f50f3ba536b58f87e42
Yoav Farhi <yoav@automattic.com>
2017-10-24 09:21:24 +0000

    i18n: Update translator comment prefix, since xgettext expects it to be lowercase
    This commit syncs r164390-wpcom.

--- _inc/lib/icalendar-reader.php
+++ _inc/lib/icalendar-reader.php
@@ -835,9 +835,9 @@ class iCalendarReader {
 		}
 		$single_day = $end ? ( $end - $start ) <= DAY_IN_SECONDS : true;
 
-		/* Translators: Date and time */
+		/* translators: Date and time */
 		$date_with_time = __( '%1$s at %2$s' , 'jetpack' );
-		/* Translators: Two dates with a separator */
+		/* translators: Two dates with a separator */
 		$two_dates = __( '%1$s &ndash; %2$s' , 'jetpack' );
 
 		// we'll always have the start date. Maybe with time
