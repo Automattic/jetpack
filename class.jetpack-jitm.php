c0d2c665ffb2308b522787ea6a42b1b75d63fece
Daniel Walmsley <goldsounds@gmail.com>
2017-10-31 11:08:34 -0700

    Minify regular JS files in _inc

--- class.jetpack-jitm.php
+++ class.jetpack-jitm.php
@@ -175,7 +175,7 @@ class Jetpack_JITM {
 		wp_style_add_data( 'jetpack-jitm-css', 'suffix', $min );
 		wp_enqueue_style( 'jetpack-jitm-css' );
 
-		wp_enqueue_script( 'jetpack-jitm-new', plugins_url( '_inc/jetpack-jitm.js', JETPACK__PLUGIN_FILE ), array( 'jquery' ), JETPACK__VERSION, true );
+		wp_enqueue_script( 'jetpack-jitm-new', plugins_url( '_inc/build/jetpack-jitm.min.js', JETPACK__PLUGIN_FILE ), array( 'jquery' ), JETPACK__VERSION, true );
 		wp_localize_script( 'jetpack-jitm-new', 'jitm_config', array(
 			'api_root' => esc_url_raw( rest_url() ),
 		) );
