4c68480945013e665c7f578e9f16cd5f62f39d1c
Miguel Lezama <miguel@automattic.com>
2017-10-31 15:55:12 -0300

    log valid failed login attempts

--- sync/class.jetpack-sync-module-protect.php
+++ sync/class.jetpack-sync-module-protect.php
@@ -1,6 +1,24 @@
 <?php
+
 /**
- * Deprecated. No longer needed.
- *
- * @package Jetpack
+ * logs bruteprotect failed logins via sync
  */
+class Jetpack_Sync_Module_Protect extends Jetpack_Sync_Module {
+
+	function name() {
+		return 'protect';
+	}
+
+	function init_listeners( $callback ) {
+		if ( Jetpack::is_module_active( 'protect' ) ) {
+			add_action( 'jpp_log_failed_attempt', array( $this, 'maybe_log_failed_login_attempt' ) );
+			add_action( 'jpp_log_failed_attempt', $callback );
+		}
+	}
+
+	function maybe_log_failed_login_attempt( $ip ) {
+		if ( Jetpack_Protect_Module::check_login_ability() ) {
+			do_action( 'jetpack_log_failed_login_attempt', $ip );
+		}
+	}
+}
