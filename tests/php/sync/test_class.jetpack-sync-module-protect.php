17b71b83754fc1ae7c3c970cd753e59c8eadf1f7
Enej Bajgoric <enej.bajgoric@gmail.com>
2017-11-03 20:03:00 +0000

    Minor fomratting

--- tests/php/sync/test_class.jetpack-sync-module-protect.php
+++ tests/php/sync/test_class.jetpack-sync-module-protect.php
@@ -10,12 +10,12 @@ class WP_Test_Jetpack_Sync_Module_Protect extends WP_Test_Jetpack_Sync_Base {
 
 	function test_sends_failed_login_message() {
 
-				Jetpack_Protect_Module::instance()->log_failed_attempt();
+			Jetpack_Protect_Module::instance()->log_failed_attempt();
 
-				$this->sender->do_sync();
+			$this->sender->do_sync();
 
-				$action = $this->server_event_storage->get_most_recent_event( 'jetpack_valid_failed_login_attempt' );
+			$action = $this->server_event_storage->get_most_recent_event( 'jetpack_valid_failed_login_attempt' );
 
-				$this->assertEquals( '127.0.0.1', $action->args[0] );
-			}
+			$this->assertEquals( '127.0.0.1', $action->args[0] );
+		}
 }
