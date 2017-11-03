97ef585c1bf08345931ee63171ce204d176abfd6
Enej Bajgoric <enej.bajgoric@gmail.com>
2017-11-03 17:42:38 +0000

    Add Test remove check.

--- sync/class.jetpack-sync-module-protect.php
+++ sync/class.jetpack-sync-module-protect.php
@@ -10,10 +10,8 @@ class Jetpack_Sync_Module_Protect extends Jetpack_Sync_Module {
 	}
 
 	function init_listeners( $callback ) {
-		if ( Jetpack::is_module_active( 'protect' ) ) {
-			add_action( 'jpp_log_failed_attempt', array( $this, 'maybe_log_failed_login_attempt' ) );
-			add_action( 'jetpack_valid_failed_login_attempt', $callback );
-		}
+		add_action( 'jpp_log_failed_attempt', array( $this, 'maybe_log_failed_login_attempt' ) );
+		add_action( 'jetpack_valid_failed_login_attempt', $callback );
 	}
 
 	function maybe_log_failed_login_attempt( $ip ) {
