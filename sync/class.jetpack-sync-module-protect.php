cccab679c209cb152b23e3e5b0310f8b6762d71e
Miguel Lezama <miguel@automattic.com>
2017-11-03 11:50:26 +0000

    rename jetpack_valid_failed_login_attempt

--- sync/class.jetpack-sync-module-protect.php
+++ sync/class.jetpack-sync-module-protect.php
@@ -12,13 +12,13 @@ class Jetpack_Sync_Module_Protect extends Jetpack_Sync_Module {
 	function init_listeners( $callback ) {
 		if ( Jetpack::is_module_active( 'protect' ) ) {
 			add_action( 'jpp_log_failed_attempt', array( $this, 'maybe_log_failed_login_attempt' ) );
-			add_action( 'jpp_log_failed_attempt', $callback );
+			add_action( 'jetpack_valid_failed_login_attempt', $callback );
 		}
 	}
 
 	function maybe_log_failed_login_attempt( $ip ) {
 		if ( Jetpack_Protect_Module::check_login_ability() ) {
-			do_action( 'jetpack_log_failed_login_attempt', $ip );
+			do_action( 'jetpack_valid_failed_login_attempt', $ip );
 		}
 	}
 }
