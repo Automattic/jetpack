43d1271ea9d834a4ed5d7a0d21488fab9eacd305
Eric Binnion <ericbinnion@gmail.com>
2017-11-06 13:48:11 -0600

    Lazy Images: Add attribution for basis of module

--- modules/lazy-images.php
+++ modules/lazy-images.php
@@ -13,7 +13,15 @@
  * Additional Search Queries: mobile, theme, performance
  */
 
-// TODO: Add attribution here
+/**
+ * This module relies heavily upon the Lazy Load plugin which was worked on by
+ * Mohammad Jangda (batmoo), the WordPress.com VIP team, the TechCrunch 2011
+ * redesign team, and Jake Goldman of 10up LLC.
+ *
+ * The JavaScript has been updated to rely on InterSection observer instead of
+ * jQuery Sonar. Many thanks to Dean Hume (deanhume) and his example:
+ * https://github.com/deanhume/lazy-observer-load
+ */
 
 require_once( JETPACK__PLUGIN_DIR . 'modules/lazy-images/lazy-images.php' );
 Jetpack_Lazy_Images::instance();
