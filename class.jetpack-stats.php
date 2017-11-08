2ab7e3f2023e3ec270f8b9e320e30f9b5dbdf0c6
Paul Sealock <psealock@gmail.com>
2017-11-08 22:34:51 +1300

    mucking around

--- class.jetpack-stats.php
+++ class.jetpack-stats.php
@@ -1,6 +1,15 @@
 <?php
 
 class Jetpack_Stats {
+	static private function woocommerce_loaded() {
+		echo "<pre>";
+//		print_r( WC_Data::get_data_store() );
+//		print_r( WC_Cart::get_cart_from_session() );
+		print_r( WC()->cart->cart_contents );
+//		global $wp_query;
+//		print_r($wp_query);
+		echo "</pre>";
+	}
 	static function init() {
 		if ( is_admin() ) {
 			return;
@@ -8,14 +17,11 @@ class Jetpack_Stats {
 		if ( ! Jetpack::is_active() ) {
 			return;
 		}
-		$plugins = get_plugins();
-		$is_store = isset( $plugins[ 'woocommerce/woocommerce.php' ] );
-		if ( ! $is_store ) {
+		if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 			return;
 		}
-//		print_r( WC_Data::get_data_store() );
-//		print_r( WC_Cart::get_cart_from_session() );
-		print_r( WC() );
+		Jetpack_Stats::woocommerce_loaded();
+//		add_action( ‘woocommerce_init’, array( $this, ‘woocommerce_loaded’ ) );
 	}
 }
 
