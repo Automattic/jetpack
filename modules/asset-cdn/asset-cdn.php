61c426595ba3aff62bc19834ba1d1790f6f68ad3
Daniel Walmsley <goldsounds@gmail.com>
2017-11-09 13:18:47 -0800

    Add JS minification

--- modules/asset-cdn/asset-cdn.php
+++ modules/asset-cdn/asset-cdn.php
@@ -25,6 +25,8 @@ class Asset_CDN {
 
 	private $cdn_server;
 	private $concat_style_groups = array();
+	private $concat_script_groups = array();
+	private $inject_critical_css = false;
 
 	/**
 	 * Singleton implementation
@@ -40,16 +42,20 @@ class Asset_CDN {
 	}
 
 	private function __construct() {
-		$this->cdn_server = 'https://cdn.wpvm.io';
+		// $this->cdn_server = 'https://cdn.wpvm.io';
+		$this->cdn_server = 'http://localhost:8090';
 
 		// allow smaller CSS by only minifying assets on the page
 		add_filter( 'jetpack_implode_frontend_css', '__return_false' );
 
 		// rewrite CSS tags
-		add_filter( 'style_loader_tag', array( $this, 'print_inline_styles' ), -100, 4 );
+		add_filter( 'script_loader_tag', array( $this, 'register_concat_scripts' ), -100, 3 );
+		add_filter( 'style_loader_tag', array( $this, 'register_concat_styles' ), -100, 4 );
 
 		add_action( 'wp_head', array( $this, 'render_concatenated_styles_head' ), PHP_INT_MAX );
+		add_action( 'wp_head', array( $this, 'render_concatenated_scripts_head' ), PHP_INT_MAX );
 		add_action( 'wp_footer', array( $this, 'render_concatenated_styles_footer' ), PHP_INT_MAX );
+		add_action( 'wp_footer', array( $this, 'render_concatenated_scripts_footer' ), PHP_INT_MAX );
 	}
 
 	/**
@@ -61,15 +67,30 @@ class Asset_CDN {
 			$this->render_concatenated_styles( $this->concat_style_groups[0] );
 		}
 	}
+
 	function render_concatenated_styles_footer() {
 		if ( isset( $this->concat_style_groups[1] ) ) {
 			$this->render_concatenated_styles( $this->concat_style_groups[1] );
 		}
 	}
-	private function render_concatenated_styles( $styles ) {
+
+	private function render_concatenated_styles( $style_groups ) {
 		// special URL to concatenation service
-		foreach( $styles as $media => $urls ) {
-			$cdn_url = $this->cdn_server . '/css?b=' . urlencode( site_url() ) . '&' . http_build_query( array( 'f' => $urls ) );
+		global $wp_styles;
+		$site_url = site_url();
+		foreach( $style_groups as $media => $styles ) {
+			$urls = array();
+			$vers = array();
+
+			foreach( $styles as $style ) {
+				$urls[] = str_replace( untrailingslashit( $site_url ), '', $style->src );
+				$vers[] = $style->ver ? $style->ver : $wp_styles->default_version;
+			}
+
+			$cdn_url = $this->cdn_server . '/css?b=' .
+				urlencode( $site_url ) . '&' .
+				http_build_query( array( 'f' => $urls ) ) . '&' .
+				http_build_query( array( 'v' => $vers ) );
 			// if we are injecting critical CSS, load the full CSS async
 			if ( $this->inject_critical_css ) {
 				echo '<!-- jetpack concat --><link rel="preload" onload="this.rel=\'stylesheet\'" as="style" type="text/css" media="' . $media . '" href="' . esc_attr( $cdn_url ) . '"/>';
@@ -79,11 +100,85 @@ class Asset_CDN {
 		}
 	}
 
+	function render_concatenated_scripts_head() {
+		if ( isset( $this->concat_script_groups[0] ) ) {
+			$this->render_concatenated_scripts( $this->concat_script_groups[0] );
+		}
+	}
+
+	function render_concatenated_scripts_footer() {
+		if ( isset( $this->concat_script_groups[1] ) ) {
+			$this->render_concatenated_scripts( $this->concat_script_groups[1] );
+		}
+	}
+
+	private function render_concatenated_scripts( $scripts ) {
+		// special URL to concatenation service
+		global $wp_scripts;
+		$site_url = site_url();
+		$urls = array();
+		$vers = array();
+
+		foreach( $scripts as $script ) {
+			$urls[] = str_replace( untrailingslashit( $site_url ), '', $script->src );
+			$vers[] = $script->ver ? $script->ver : $wp_scripts->default_version;
+		}
+
+		$cdn_url = $this->cdn_server . '/js?b=' .
+			urlencode( $site_url ) . '&' .
+			http_build_query( array( 'f' => $urls ) ) . '&' .
+			http_build_query( array( 'v' => $vers ) );
+		// TODO: if there is NO inline or external script tags in the body, render async (maybe?)
+		echo '<script type="text/javascript" src="' . esc_attr( $cdn_url ) . '"></script>';
+	}
+
 	/**
 	 * Asset modification functions
 	 */
 
-	public function print_inline_styles( $tag, $handle, $href, $media ) {
+	/**
+	 * Scripts
+	 */
+
+	public function register_concat_scripts( $tag, $handle, $src ) {
+		global $wp_scripts;
+
+		// don't do admin for now
+		if ( is_admin() || ! isset( $wp_scripts->registered[$handle] ) ) {
+			return $tag;
+		}
+
+		$script = $wp_scripts->registered[$handle];
+
+		if ( $this->should_concat_script( $script ) ) {
+			$this->buffer_script( $script );
+			return '';
+		}
+
+		return $tag;
+	}
+
+	private function should_concat_script( $script ) {
+		// only concat local scripts
+		$is_local       = $this->is_local_url( $script->src );
+		// don't concat conditional scripts
+		$is_conditional = isset( $script->extra['conditional'] );
+		return apply_filters( 'jetpack_perf_concat_script', $is_local && ! $is_conditional, $script->handle, $script->src );
+	}
+
+	private function buffer_script( $script ) {
+		$group = isset( $script->extra['group'] ) ? $script->extra['group'] : 0;
+		if ( ! isset( $this->concat_script_groups[$group] ) ) {
+			$this->concat_script_groups[$group] = array();
+		}
+		$this->concat_script_groups[$group][] = $script;
+	}
+
+	/**
+	 * Styles
+	 */
+
+	public function register_concat_styles( $tag, $handle, $href, $media ) {
 		global $wp_styles;
 
 		// don't do admin for now
@@ -113,8 +208,7 @@ class Asset_CDN {
 		if ( ! isset( $this->concat_style_groups[$group][$media] ) ) {
 			$this->concat_style_groups[$group][$media] = array();
 		}
-		$local_url = str_replace( untrailingslashit( site_url() ), '', $style->src );
-		$this->concat_style_groups[$group][$media][] = $local_url;
+		$this->concat_style_groups[$group][$media][] = $style;
 	}
 
 	private function should_concat_style( $style ) {
