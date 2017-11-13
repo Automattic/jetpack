2c2536df02d81b5e57aa862d456feb1d7ca8c034
Michael Turk <gititon@users.noreply.github.com>
2017-11-13 14:03:01 -0500

    Update/sync protect failed login attempts (#8108)
    * log valid failed login attempts
    
    * is this a feature?
    
    * rename jetpack_valid_failed_login_attempt
    
    * make it work
    
    * Add Test remove check.
    
    * Revert "is this a feature?"
    
    This reverts commit f2322987ee56ba7057c49601a5d9356533f7b43a.
    
    * remove unused variables
    
    * don't sync failed login attempts if server returns `math`
    
    some refactoring + cleanup the code a little
    
    * tansien t
    
    * Minor fomratting

--- sync/class.jetpack-sync-module-protect.php
+++ sync/class.jetpack-sync-module-protect.php
@@ -1,6 +1,23 @@
 <?php
+
 /**
- * Deprecated. No longer needed.
- *
- * @package Jetpack
+ * logs bruteprotect failed logins via sync
  */
+class Jetpack_Sync_Module_Protect extends Jetpack_Sync_Module {
+
+	function name() {
+		return 'protect';
+	}
+
+	function init_listeners( $callback ) {
+		add_action( 'jpp_log_failed_attempt', array( $this, 'maybe_log_failed_login_attempt' ) );
+		add_action( 'jetpack_valid_failed_login_attempt', $callback );
+	}
+
+	function maybe_log_failed_login_attempt( $ip ) {
+		$protect = Jetpack_Protect_Module::instance();
+		if ( $protect->has_login_ability() ) {
+			do_action( 'jetpack_valid_failed_login_attempt', $ip );
+		}
+	}
+}
