615b6f1c0098febfbfd98ad277207ac09304af86
Robert Elliott <robert.elliott@automattic.com>
2017-11-14 16:45:02 +0200

    putting the logic down for the js tracking required here for now

---
+++ _inc/lib/tracks/tracks-store.js
@@ -0,0 +1,12 @@
+/** Placeholder for drafting logic for this. May end up at stats.wp.com */
+
+/**
+ * Ensure there are 2 referrers:
+ * 1. original referrer tk_ro
+ * 2. latest referrer tk_rl
+ */
+
+// 1. Original Referrer - tk_ro
+// Check if tk_ro cookie exists && if there is a document.referrer
+// Check that referrer is not from current domain
+// 
