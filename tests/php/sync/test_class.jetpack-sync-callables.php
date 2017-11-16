9f0b030376c0952c3d8f63847d2b9bba7a238aaf
Enej Bajgoric <enej.bajgoric@gmail.com>
2017-11-16 08:30:05 -0800

    Sync: add broken test (#8167)
    * Add broken test
    
    * Add empty array

--- tests/php/sync/test_class.jetpack-sync-callables.php
+++ tests/php/sync/test_class.jetpack-sync-callables.php
@@ -623,6 +623,26 @@ class WP_Test_Jetpack_Sync_Functions extends WP_Test_Jetpack_Sync_Base {
 		$this->assertEquals( $plugins_action_links, $expected_array );
 	}
 
+	function cause_fatal_error( $actions ) {
+		unset( $actions['activate'] );
+		$actions[] = '<a href="/hello">world</a>';
+		return $actions;
+	}
+
+	function test_fixes_fatal_error( ) {
+
+		delete_transient( 'jetpack_plugin_api_action_links_refresh' );
+		add_filter( 'plugin_action_links', array( $this, 'cause_fatal_error' ) );
+		$callables_module = new Jetpack_Sync_Module_Callables(); // Do the admin init here so that we calculate the plugin links
+		$callables_module->set_plugin_action_links();
+
+		$this->resetCallableAndConstantTimeouts();
+		$callables_module->set_plugin_action_links();
+		$this->sender->do_sync();
+		$plugins_action_links = $this->server_replica_storage->get_callable( 'get_plugins_action_links' );
+		$this->assertTrue( isset( $plugins_action_links['hello.php']['world'] ) );
+	}
+
 	function __return_filtered_url() {
 		return 'http://filteredurl.com';
 	}
