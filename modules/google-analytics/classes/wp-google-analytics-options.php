fceff3fbdbb309b5964f247ac8c1c66b987d199c
Allen Snook <allen@allendav.com>
2017-11-17 15:26:36 -0800

    Remove some of the enh options for now - to be delivered in next PR

--- modules/google-analytics/classes/wp-google-analytics-options.php
+++ modules/google-analytics/classes/wp-google-analytics-options.php
@@ -47,26 +47,6 @@ class Jetpack_Google_Analytics_Options {
 		return self::get_option( 'enh_ec_tracking' );
 	}
 
-	public static function track_remove_from_cart_is_enabled() {
-		return self::get_option( 'enh_ec_track_remove_from_cart' );
-	}
-
-	public static function track_product_impressions_is_enabled() {
-		return self::get_option( 'enh_ec_track_prod_impression' );
-	}
-
-	public static function track_product_clicks_is_enabled() {
-		return self::get_option( 'enh_ec_track_prod_click' );
-	}
-
-	public static function track_product_detail_view_is_enabled() {
-		return self::get_option( 'enh_ec_track_prod_detail_view' );
-	}
-
-	public static function track_checkout_started_is_enabled() {
-		return self::get_option( 'enh_ec_track_checkout_started' );
-	}
-
 	public static function debug_dump() {
 		$messages = array( 'Jetpack_Google_Analytics_Options' );
 		$tracking_code = self::has_tracking_code() ? self::get_tracking_code() : '(empty)';
@@ -77,11 +57,6 @@ class Jetpack_Google_Analytics_Options {
 			'track_purchases_is_enabled',
 			'track_add_to_cart_is_enabled',
 			'enhanced_ecommerce_tracking_is_enabled',
-			'track_remove_from_cart_is_enabled',
-			'track_product_impressions_is_enabled',
-			'track_product_clicks_is_enabled',
-			'track_product_detail_view_is_enabled',
-			'track_checkout_started_is_enabled',
 		);
 
 		foreach( $flags as $flag ) {
