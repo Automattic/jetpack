e6eec707cc6022248c5fbe8abdef5a9299ecbb98
oskosk <oskosk@gmail.com>
2017-11-18 08:49:13 -0300

    Copy select-dropdown from dops-components

---
+++ _inc/client/components/select-dropdown/label.jsx
@@ -0,0 +1,30 @@
+/** @ssr-ready **/
+/* eslint-disable jsx-a11y/onclick-has-role */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/label-has-for */
+
+/**
+ * External Dependencies
+ */
+import React from 'react';
+
+/**
+ * Module variables
+ */
+const { Component } = React;
+const stopPropagation = event => event.stopPropagation();
+
+class SelectDropdownLabel extends Component {
+	render() {
+		return (
+			<li
+				onClick= { stopPropagation }
+				className="dops-select-dropdown__label"
+			>
+				<label>{ this.props.children }</label>
+			</li>
+		);
+	}
+}
+
+export default SelectDropdownLabel;
