b5f2e08c3891bcb98e3f1edb406487870dddc90f
oskosk <oskosk@gmail.com>
2017-11-18 09:11:32 -0300

    Copy popover from dops-components

---
+++ _inc/client/components/popover/menu-item.jsx
@@ -0,0 +1,35 @@
+/**
+ * External dependencies
+ */
+var React = require( 'react' ),
+	classnames = require( 'classnames' );
+
+var MenuItem = React.createClass( {
+	getDefaultProps: function() {
+		return {
+			isVisible: false,
+			className: '',
+			focusOnHover: true
+		};
+	},
+
+	render: function() {
+		var onMouseOver = this.props.focusOnHover ? this._onMouseOver : null;
+		return (
+			<button className={ classnames( 'dops-popover__menu-item', this.props.className ) }
+					role="menuitem"
+					disabled={ this.props.disabled }
+					onClick={ this.props.onClick }
+					onMouseOver={ onMouseOver }
+					tabIndex="-1">
+				{ this.props.children }
+			</button>
+		);
+	},
+
+	_onMouseOver: function( event ) {
+		event.target.focus();
+	}
+} );
+
+module.exports = MenuItem;
