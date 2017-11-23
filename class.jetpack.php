e3d08b7af1386ef2c57e8c633b4a5c8cee397696
Marin Atanasov <tyxla@abv.bg>
2017-11-23 17:07:13 +0200

    REST API: Allow CORS requests for JPO

--- class.jetpack.php
+++ class.jetpack.php
@@ -665,6 +665,22 @@ class Jetpack {
 		if ( ! has_action( 'shutdown', array( $this, 'push_stats' ) ) ) {
 			add_action( 'shutdown', array( $this, 'push_stats' ) );
 		}
+
+		// Allow CORS requests to the settings endpoint for onboarding requests
+		add_action( 'rest_api_init', function() {
+			add_filter( 'rest_pre_serve_request', function( $served, $result, $request ) {
+				if ( $request->get_route() === '/jetpack/v4/settings' ) {
+					remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+
+					header( 'Access-Control-Allow-Origin: *' );
+					header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE' );
+					header( 'Access-Control-Allow-Credentials: true' );
+				}
+
+				return $served;
+
+			}, 9, 3 );
+		}, 15 );
 	}
 
 	function point_edit_links_to_calypso( $default_url, $post_id ) {
