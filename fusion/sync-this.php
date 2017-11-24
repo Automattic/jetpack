652a619c2af2e91864889eba2eab51a10f95f4fc
Igor Zinovyev <zinigor@gmail.com>
2017-11-24 19:41:53 +0300

    Discard this PR.

--- fusion/sync-this.php
+++ fusion/sync-this.php
@@ -2,3 +2,6 @@
 /**
  * This will be used as mock code that we can use to test the development flow of Fusion.
  */
+/**
+ * This will be used as mock code that we can use to test the development flow of Fusion.
+ */
