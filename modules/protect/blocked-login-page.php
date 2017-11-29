a64d69a87d2a327798626ce8cde8ab53d9e2de1e
Enej Bajgoric <enej.bajgoric@gmail.com>
2017-11-29 11:13:34 -0800

    Minor fixes

--- modules/protect/blocked-login-page.php
+++ modules/protect/blocked-login-page.php
@@ -192,7 +192,7 @@ class Jetpack_Protect_Blocked_Login_Page {
 		}
 
 		if ( isset( $_GET['validate_jetpack_protect_recovery'] ) && $_GET['user_id'] ) {
-			$this->protect_die( __( 'Oops, we couldn’t validate the recovery recovery token.', 'jetpack' ) );
+			$this->protect_die( __( 'Oops, we couldn’t validate the recovery token.', 'jetpack' ) );
 
 			return;
 		}
@@ -533,7 +533,7 @@ class Jetpack_Protect_Blocked_Login_Page {
 				} ?>
 			</div>
 			<div id="error-footer">
-			<?php if ( $back_button ) {
+			<?php if ( $back_button && ! $recovery_form ) {
 				$back_button_icon = '<svg class="gridicon gridicons-arrow-left" height="24" width="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></g></svg>';
 			?>
 				<a href='javascript:history.back()'><?php printf( __( '%s Back' ), $back_button_icon ); ?></a>
