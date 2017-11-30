85ef5754c87c2216affd5f64da4daf0487c0962c
Paul Sealock <psealock@gmail.com>
2017-11-30 16:24:41 +1300

    rename properties

--- 3rd-party/woocommerce-stats.php
+++ 3rd-party/woocommerce-stats.php
@@ -93,33 +93,38 @@ class WC_Stats {
 		}
 	}
 
+	public function get_session_id() {
+		$session_handler = new WC_Session_Handler();
+		$session = $session_handler->get_session_cookie();
+		return $session ? $session[ 0 ] : null;
+	}
+
 	public function writeDataToDom() {
 		$cart = WC()->cart->get_cart();
 		$cart_ids = array_reduce( $cart, array( $this, 'get_cart_ids' ), '' );
 		$cart_quantities = array_reduce( $cart, array( $this, 'get_cart_quantities' ), '' );
 		// Is this the right id? Is WooCommerce id different?
-		$store_id = Jetpack::get_option( 'id' );
+		$blog_id = Jetpack::get_option( 'id' );
 		$post = get_post();
 		$post_id = $post->ID;
 		$post_type = get_post_type( $post_id );
 		$store_page = $this->get_store_page( $post_type, $post_id );
 		$order_number = $_GET['key'];
 		$tracks_identity = jetpack_tracks_get_identity( get_current_user_id() );
+//		data-ut='" . $tracks_identity[ '_ut' ] . "'
+//		data-ui='" . $tracks_identity[ '_ui' ] . "'>
+		$session_id = $this->get_session_id();
 
 		echo "
 			<div 
 				id='store_data'
 				style='display: none;' 
-				data-si='" . $store_id . "'
-				data-pt='" . $post_type . "'
-				data-pi='" . $post_id . "'
-				data-sp='" . $store_page . "'
-				data-ch='" . $_COOKIE[ 'woocommerce_cart_hash' ] . "'
-				data-ci='" . $cart_ids . "'
-				data-cq='" . $cart_quantities . "'
-				data-on='" . $order_number . "'
-				data-ut='" . $tracks_identity[ '_ut' ] . "'
-				data-ui='" . $tracks_identity[ '_ui' ] . "'>
+				data-blog_id='" . $blog_id . "'
+				data-post='" . $post_id . "'
+				data-cart='" . $session_id . "'
+				data-cart_i='" . $cart_ids . "'
+				data-cart_q='" . $cart_quantities . "'>
+				
 			</div>
 		";
 	}
