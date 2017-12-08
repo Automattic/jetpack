82a4cd64f3b80cb15b8ca156478b64ae2e34f5ee
robjarsen <29106074+robjarsen@users.noreply.github.com>
2017-12-08 13:55:28 -0500

    PHPCS fixes for wp-cache-base.php (#483)
    * PHPCS fixes for wp-cache-base.php
    
    * Remove trailing whitespace
    
    Minor edit to remove trailing whitespace.

--- wp-cache-base.php
+++ wp-cache-base.php
@@ -1,34 +1,35 @@
 <?php
-if ( false == isset( $_SERVER[ 'HTTP_HOST' ] ) ) {
-	$cache_enabled = false;
+if ( false == isset( $_SERVER['HTTP_HOST'] ) ) {
+	$cache_enabled  = false;
 	$WPSC_HTTP_HOST = '';
 } else {
-	$WPSC_HTTP_HOST = htmlentities( $_SERVER[ 'HTTP_HOST' ] );
+	$WPSC_HTTP_HOST = htmlentities( $_SERVER['HTTP_HOST'] );
 }
 
 // We want to be able to identify each blog in a WordPress MU install
 $blogcacheid = '';
 if ( ( defined( 'WP_ALLOW_MULTISITE' ) && constant( 'WP_ALLOW_MULTISITE' ) == true ) || defined( 'SUBDOMAIN_INSTALL' ) || defined( 'VHOST' ) || defined( 'SUNRISE' ) ) {
 	$blogcacheid = 'blog'; // main blog
-	if( defined( 'SUBDOMAIN_INSTALL' ) && constant( 'SUBDOMAIN_INSTALL' ) == true ) {
+	if ( defined( 'SUBDOMAIN_INSTALL' ) && constant( 'SUBDOMAIN_INSTALL' ) == true ) {
 		$blogcacheid = $WPSC_HTTP_HOST;
 	} else {
-		if ( isset( $base ) == false )
+		if ( isset( $base ) == false ) {
 			$base = '';
-		$request_uri = str_replace( '..', '', preg_replace('/[ <>\'\"\r\n\t\(\)]/', '', $_SERVER['REQUEST_URI'] ) );
-		if( strpos( $request_uri, '/', 1 ) ) {
-			if( $base == '/' ) {
+		}
+		$request_uri = str_replace( '..', '', preg_replace( '/[ <>\'\"\r\n\t\(\)]/', '', $_SERVER['REQUEST_URI'] ) );
+		if ( strpos( $request_uri, '/', 1 ) ) {
+			if ( $base == '/' ) {
 				$blogcacheid = substr( $request_uri, 1, strpos( $request_uri, '/', 1 ) - 1 );
 			} else {
 				$blogcacheid = str_replace( $base, '', $request_uri );
-				if ( $blogcacheid != '' )
+				if ( $blogcacheid != '' ) {
 					$blogcacheid = substr( $blogcacheid, 0, strpos( $blogcacheid, '/', 1 ) );
+				}
 			}
-			if ( '/' == substr($blogcacheid, -1))
-				$blogcacheid = substr($blogcacheid, 0, -1);
+			if ( '/' == substr( $blogcacheid, -1 ) ) {
+				$blogcacheid = substr( $blogcacheid, 0, -1 );
+			}
 		}
 		$blogcacheid = str_replace( '/', '', $blogcacheid );
 	}
 }
-
-?>
