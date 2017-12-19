d4ba24a8b00707a2ba8e066066f60c9fc870d7e0
Elio Rivero <eliorivero@gmail.com>
2017-12-19 13:17:33 -0300

    Update logic to display correct loading message when data is not available, either for site plan or for VP plan.

--- _inc/client/at-a-glance/backups.jsx
+++ _inc/client/at-a-glance/backups.jsx
@@ -6,7 +6,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import DashItem from 'components/dash-item';
 import { translate as __ } from 'i18n-calypso';
-import { noop } from 'lodash/noop';
+import noop from 'lodash/noop';
+import isEmpty from 'lodash/isEmpty';
+import get from 'lodash/get';
 
 /**
  * Internal dependencies
@@ -65,62 +67,62 @@ class DashBackups extends Component {
 			isVaultPressInstalled,
 			getOptionValue,
 			siteRawUrl,
+			vaultPressData,
 		} = this.props;
-		const hasSitePlan = false !== sitePlan && 'jetpack_free' !== sitePlan.product_slug;
-		const inactiveOrUninstalled = isVaultPressInstalled ? 'pro-inactive' : 'pro-uninstalled';
 
-		if ( getOptionValue( 'vaultpress' ) ) {
-			const vpData = this.props.vaultPressData;
-
-			if ( vpData === 'N/A' ) {
-				return renderCard( {
-					className: '',
-					status: '',
-					content: __( 'Loading…' )
-				} );
-			}
-
-			if ( vpData.code === 'success' ) {
-				return renderCard( {
-					className: 'jp-dash-item__is-active',
-					status: 'is-working',
-					content: (
-						<span>
-							{ vpData.message }
-							&nbsp;
-							{ __( '{{a}}View backup details{{/a}}.', {
-								components: {
-									a: <a href="https://dashboard.vaultpress.com" target="_blank" rel="noopener noreferrer" />
-								}
-							} ) }
-						</span>
-					),
-				} );
-			}
+		if ( getOptionValue( 'vaultpress' ) && 'success' === get( vaultPressData, 'code', '' ) ) {
+			return renderCard( {
+				className: 'jp-dash-item__is-active',
+				status: 'is-working',
+				content: (
+					<span>
+						{ get( vaultPressData, 'message', '' ) }
+						&nbsp;
+						{ __( '{{a}}View backup details{{/a}}.', {
+							components: {
+								a: <a href="https://dashboard.vaultpress.com" target="_blank" rel="noopener noreferrer" />
+							}
+						} ) }
+					</span>
+				),
+			} );
 		}
 
-		return renderCard( {
-			className: 'jp-dash-item__is-inactive',
-			status: hasSitePlan
-				? inactiveOrUninstalled
-				: 'no-pro-uninstalled-or-inactive',
-			content: hasSitePlan
-				? __( 'To automatically back up your entire site, please {{a}}install and activate{{/a}} VaultPress.', {
-					components: {
-						a: <a href="https://wordpress.com/plugins/vaultpress" target="_blank" rel="noopener noreferrer" />
-					}
-				} )
-				: __( 'To automatically back up your entire site, please {{a}}upgrade your account{{/a}}.', {
+		if ( ! isEmpty( sitePlan ) ) {
+			// If site has a paid plan
+			if ( 'jetpack_free' !== get( sitePlan, 'product_slug', 'jetpack_free' ) ) {
+				return renderCard( {
+					className: 'jp-dash-item__is-inactive',
+					status: isVaultPressInstalled ? 'pro-inactive' : 'pro-uninstalled',
+					content: __( 'To automatically back up your entire site, please {{a}}install and activate{{/a}} VaultPress.', {
+						components: {
+							a: <a href="https://wordpress.com/plugins/vaultpress" target="_blank" rel="noopener noreferrer" />
+						}
+					} )
+				} );
+			}
+
+			return renderCard( {
+				className: 'jp-dash-item__is-inactive',
+				status: 'no-pro-uninstalled-or-inactive',
+				content: __( 'To automatically back up your entire site, please {{a}}upgrade your account{{/a}}.', {
 					components: {
 						a:
 							<a href={ `https://jetpack.com/redirect/?source=aag-backups&site=${ siteRawUrl }` } target="_blank" rel="noopener noreferrer" />
 					}
 				} ),
+			} );
+		}
+
+		return renderCard( {
+			className: '',
+			status: '',
+			content: __( 'Loading…' )
 		} );
 	}
 
 	render() {
-		if ( this.props.inDevMode ) {
+		if ( this.props.isDevMode ) {
 			return (
 				<div className="jp-dash-item__interior">
 					{
@@ -162,7 +164,7 @@ export default connect(
 		return {
 			vaultPressData: getVaultPressData( state ),
 			sitePlan: getSitePlan( state ),
-			inDevMode: isDevMode( state ),
+			isDevMode: isDevMode( state ),
 			isVaultPressInstalled: isPluginInstalled( state, 'vaultpress/vaultpress.php' )
 		};
 	}
