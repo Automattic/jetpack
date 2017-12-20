4f40f8e191f397874ccffaa98a27b61b014a272b
Sam Hotchkiss <s@swh.me>
2017-12-20 10:58:18 -0500

    VaultPress Murder

--- 3rd-party/vaultpress.php
+++ 3rd-party/vaultpress.php
@@ -3,16 +3,11 @@
 function jetpack_vaultpress_rewind_enabled_notice() {
 	$plugin_file = 'vaultpress/vaultpress.php';
 
-	$query_args = array(
-		'action' => 'deactivate',
-		'plugin' => $plugin_file,
-	);
-	$deactivate_url = wp_nonce_url( add_query_arg( $query_args, admin_url( 'plugins.php' ) ), "deactivate-plugin_{$plugin_file}" );
+	deactivate_plugins( $plugin_file );
 	?>
 	<div class="notice notice-success">
 		<h2 style="margin-bottom: 0.25em;"><?php _e( 'Jetpack is now handling your backups.', 'jetpack' ); ?></h2>
-		<p><?php _e( 'Since Jetpack is taking care of business, you can now safely remove the VaultPress plugin.', 'jetpack' ); ?></p>
-		<p><a class="button button-primary" href="<?php echo esc_url( $deactivate_url ); ?>"><?php _e( 'Deactivate VaultPress', 'jetpack' ); ?></a></p>
+		<p><?php _e( 'VaultPress is no longer needed and has been deactivated.', 'jetpack' ); ?></p>
 	</div>
 	<?php
 }
