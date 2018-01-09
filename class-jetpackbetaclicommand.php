3720778582c245dd5e31e93f4806a2d2e538aa2c
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2018-01-10 08:24:10 +0900

    Add wp cli command to active PR branches (#42)

--- class-jetpackbetaclicommand.php
+++ class-jetpackbetaclicommand.php
@@ -10,53 +10,81 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 		 *
 		 * ## OPTIONS
 		 *
-		 *
 		 * activate master: Get a version of the master branch built every 15 minutes
-		 *
 		 * activate stable: Get the latest stable version of Jetpack
-		 *
+		 * activate branch_name: Get a version of PR. PR must be built and unit-tested before it become availabe
+		 * list: Get list of available jetpack branches to install 
+		 * 
 		 * ## EXAMPLES
 		 *
 		 * wp jetpack-beta branch activate master
 		 * wp jetpack-beta branch activate stable
+		 * wp jetpack-beta branch activate branch_name
+		 * wp jetpack-beta branch list
 		 *
 		 */
 		public function branch( $args ) {
 
 			$this->validation_checks( $args );
+
+			if ( 'list' === $args[0] ) {
+				return $this->branches_list();
+			}
 				
 			if ( 'master' === $args[1] ) {
-				$retvalue = Jetpack_Beta::install_and_activate( 'master', 'master' );
-				if ( is_wp_error( $retvalue ) ) {
-					return WP_CLI::error( __( 'Error', 'jetpack' ) . $retvalue->get_error_message() );
+				return $this->install_jetpack( 'master', 'master' );
+			} elseif ( 'stable' === $args[1] ) {
+				return $this->install_jetpack( 'stable', 'stable' );
+			} else {
+				$branch_name = str_replace( '/', '_', $args[1] ); 
+				$url = Jetpack_Beta::get_install_url( $branch_name, 'pr' );
+				if ( $url === null ) {
+					return WP_CLI::error( __( 'Invalid branch name. Try `wp jetpack-beta branch list` for list of available branches', 'jetpack' ) );
 				}
-				return WP_CLI::success( __( 'Jetpack is currently on Bleeding Edge', 'jetpack-beta' ) );
-			}
-			if ( 'stable' === $args[1] ) {
-				$retvalue = Jetpack_Beta::install_and_activate( 'stable', 'stable' );
-				if ( is_wp_error( $retvalue ) ) {
-					return WP_CLI::error( __( 'Error', 'jetpack' ) . $retvalue->get_error_message() );
-				}
-				return WP_CLI::success( __( 'Jetpack is currently on Latest Stable', 'jetpack-beta' ) );
+				return $this->install_jetpack( $branch_name, 'pr' );
 			}
 			return WP_CLI::error( __( 'Unrecognized branch version. ', 'jetpack' ) );
 		}
 
-		private function validation_checks($args) {
+		private function validation_checks( $args ) {
 			if ( is_multisite() && ! is_main_site() ) {
 				return WP_CLI::error( __( 'Secondary sites in multisite instalations are not supported', 'jetpack' ) );				
 			}
 
 			if ( empty( $args ) ) {
-				return WP_CLI::error( __( 'Specify subcommand', 'jetpack' ) );
+				return WP_CLI::error( __( 'Specify subcommand. "activate" and "list" subcommands are supported', 'jetpack' ) );
 			}
 
-			if ( 'activate' !== $args[0] ) {
-				return WP_CLI::error( __( 'Only "activate" subcommand is supported', 'jetpack' ) );				
+			if ( 'activate' !== $args[0] && 'list' !== $args[0] ) {
+				return WP_CLI::error( __( 'Only "activate" and "list" subcommands are supported', 'jetpack' ) );				
 			}
 
-			if ( empty( $args[1] ) ) {
-				return WP_CLI::error( __( 'Specify branch name', 'jetpack' ) );				
+			if ( 'activate' === $args[0] && empty( $args[1] ) ) {
+				return WP_CLI::error( __( 'Specify branch name. Try `wp jetpack-beta branch list` for list of available branches', 'jetpack' ) );				
+			}
+		}
+
+		private function install_jetpack( $branch, $section ) {
+			WP_CLI::line( 'Activating ' . $branch . ' branch...' );
+
+			$result = Jetpack_Beta::install_and_activate( $branch, $section  );
+			if ( is_wp_error( $result ) ) {
+				return WP_CLI::error( __( 'Error', 'jetpack' ) . $result->get_error_message() );
+			}
+			return WP_CLI::success( __( 'Jetpack is currently on ' . $branch . ' branch', 'jetpack-beta' ) );
+		}
+
+		private function branches_list() {
+			$manifest = Jetpack_Beta::get_beta_manifest();
+			$current_branch = str_replace( '_', '/', get_option( 'jetpack_beta_active' )[0] );
+			$branches = [ 'stable', 'master', 'rc' ];
+			foreach( get_object_vars( $manifest->pr ) as $key ) {
+				$branches[] = $key->branch;
+			}
+			sort($branches);
+			WP_CLI::line( 'Available branches: ');
+			foreach( $branches as $branch ) {
+				WP_CLI::line( $current_branch == $branch ? '* ' . $branch : '  ' . $branch );
 			}
 		}
 	}
