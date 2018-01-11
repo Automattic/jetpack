893380a554e103af66a61f5179319854aee800ca
Jeff Bowen <jeff@automattic.com>
2018-01-11 22:00:19 +0000

    Infinite Scroll: Provide feature info via the Site Settings REST API endpoint
    Summary:
    Return the "enabled" status for `infinite_scroll` and, when applicable, include a flag that the setting is blocked because the site has a footer widget enabled.
    
    This is to support viewing and modifying the setting in Calypso.
    
    ---
    
    In order for widget info to be read in a REST API context, we have to do a few things:
    * Suppress infinite scroll theme support declaration in a REST API context
    * Source and add to the allowed list the theme support files (if present)
    * Clear the in-memory (PHP global) cache of widget info so it's sourced from the blog's context in the REST API endpoint
    
    Test Plan:
    ## Manual tests
    
    * Apply this patch
    * Sandbox the WordPress.com REST API
    * Run the branch in https://github.com/Automattic/wp-calypso/pull/21120 & access site writing settings for a WPCOM simple site (https://calypso.live/settings/writing?branch=add/wpcom-infinity-setting)
    * The Infinite scroll setting should be present and functional (the setting toggle should be reflected on the front end of the site)
      * If a footer widget is enabled, the setting should be disabled & a notice shown
      * If a footer widget is not-enabled (or the theme does not support them), the setting should be enabled, user-controlled, & affect the site's behavior.  The notice should _NOT_ be shown.
    * Switch to an Atomic site (or other Jetpack site)
    * The Infinite scroll setting should be present and functional (the setting radio should be reflected on the front end of the site -- there should still be 3 options available).
    
    ## Automated tests (to prevent regressions)
    
    `cd ~/public_html/bin/tests/api-v1`
    `phpunit --testsuite site`
    
    Reviewers: #samus, mppfeiffer, biskobe
    
    Reviewed By: #samus, biskobe
    
    Subscribers: biskobe
    
    Differential Revision: https://[private link]
    
    Merges r168073-wpcom.

--- json-endpoints/class.wpcom-json-api-site-settings-endpoint.php
+++ json-endpoints/class.wpcom-json-api-site-settings-endpoint.php
@@ -131,6 +131,8 @@ class WPCOM_JSON_API_Site_Settings_Endpoint extends WPCOM_JSON_API_Endpoint {
 		}
 
 		if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
+			// Source & include the infinite scroll compatibility files prior to loading theme functions
+			add_filter( 'restapi_theme_action_copy_dirs', array( 'WPCOM_JSON_API_Site_Settings_Endpoint', 'wpcom_restapi_copy_theme_plugin_actions' ) );
 			$this->load_theme_functions();
 		}
 
@@ -158,6 +160,37 @@ class WPCOM_JSON_API_Site_Settings_Endpoint extends WPCOM_JSON_API_Endpoint {
 			return new WP_Error( 'bad_request', 'An unsupported request method was used.' );
 		}
 
+	}
+
+	/**
+	 * Includes additional theme-specific files to be included in REST API theme
+	 * context loading action copying.
+	 *
+	 * @see WPCOM_JSON_API_Endpoint#load_theme_functions
+	 * @see the_neverending_home_page_theme_support
+	 */
+	function wpcom_restapi_copy_theme_plugin_actions( $copy_dirs ) {
+		$theme_name = get_stylesheet();
+		$default_file_name = WP_CONTENT_DIR . "/mu-plugins/infinity/themes/{$theme_name}.php";
+
+		/**
+		 * Filter the path to the Infinite Scroll compatibility file.
+		 *
+		 * @module infinite-scroll
+		 *
+		 * @since 2.0.0
+		 *
+		 * @param string $str IS compatibility file path.
+		 * @param string $theme_name Theme name.
+		 */
+		$customization_file = apply_filters( 'infinite_scroll_customization_file', $default_file_name, $theme_name );
+
+		if ( is_readable( $customization_file ) ) {
+			require_once $customization_file;
+			$copy_dirs[] = $customization_file;
+		}
+
+		return $copy_dirs;
 	}
 
 	/**
@@ -360,6 +393,25 @@ class WPCOM_JSON_API_Site_Settings_Endpoint extends WPCOM_JSON_API_Endpoint {
 				if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
 					$response[ $key ]['wpcom_publish_posts_with_markdown'] = (bool) WPCom_Markdown::is_posting_enabled();
 					$response[ $key ]['wpcom_publish_comments_with_markdown'] = (bool) WPCom_Markdown::is_commenting_enabled();
+
+					// WPCOM-specific Infinite Scroll Settings
+					if ( is_callable( array( 'The_Neverending_Home_Page', 'get_settings' ) ) ) {
+						/**
+						 * Clear the cached copy of widget info so it's pulled fresh from blog options.
+						 * It was primed during the initial load under the __REST API site__'s context.
+						 * @see wp_get_sidebars_widgets https://core.trac.wordpress.org/browser/trunk/src/wp-includes/widgets.php?rev=42374#L931
+						 */
+						$GLOBALS['_wp_sidebars_widgets'] = array();
+
+						$infinite_scroll_settings = The_Neverending_Home_Page::get_settings();
+						$response[ $key ]['infinite_scroll'] = get_option( 'infinite_scroll', true ) && $infinite_scroll_settings->type === 'scroll';
+						if ( $infinite_scroll_settings->footer_widgets || 'click' == $infinite_scroll_settings->requested_type ) {
+							// The blog has footer widgets -- infinite scroll is blocked
+							$response[ $key ]['infinite_scroll_blocked'] = 'footer';
+						} else {
+							$response[ $key ]['infinite_scroll_blocked'] = false;
+						}
+					}
 				}
 
 				//allow future versions of this endpoint to support additional settings keys
