1804dbbc0fbf90770856db741e6c8df6444d894c
Simon Prosser <simon@pagelines.com>
2018-01-23 17:07:31 +0000

    Change function name.

--- 3rd-party/beaverbuilder.php
+++ 3rd-party/beaverbuilder.php
@@ -5,13 +5,13 @@
 class BeaverBuilderCompat {
 
 	function __construct() {
-		add_action( 'init', array( $this, 'masterbar_refresh' ) );
+		add_action( 'init', array( $this, 'beaverbuilder_refresh' ) );
 	}
 
 	/**
 	 * If masterbar module is active force BeaverBuilder to refresh when publishing a layout.
 	 */
-	function masterbar_refresh() {
+	function beaverbuilder_refresh() {
 		if ( Jetpack::is_module_active( 'masterbar' ) ) {
 			add_filter( 'fl_builder_should_refresh_on_publish', '__return_true' );
 		}
