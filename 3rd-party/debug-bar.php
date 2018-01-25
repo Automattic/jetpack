f1bdd31b372159bd71799e01b6895b2df6b84d84
Eric Binnion <ericbinnion@gmail.com>
2018-01-24 22:20:35 -0600

    Search: Do not show search debug if search not active

--- 3rd-party/debug-bar.php
+++ 3rd-party/debug-bar.php
@@ -2,6 +2,10 @@
 
 add_filter( 'debug_bar_panels', 'init_jetpack_search_debug_bar' );
 function init_jetpack_search_debug_bar( $panels ) {
+	if ( ! Jetpack::is_module_active( 'search' ) ) {
+		return $panels;
+	}
+
 	require_once( dirname( __FILE__ ) . '/debug-bar/class.jetpack-search-debug-bar.php' );
 	$panels[] = Jetpack_Search_Debug_Bar::instance();
 	return $panels;
