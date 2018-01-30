6982b133c0cf818f37f27255725466bb7b7cd3e6
roccotripaldi <tripaldir@gmail.com>
2018-01-29 22:23:12 -0500

    After a Slack conversation with @enejb, adding more flexibilty to the filter.

--- sync/class.jetpack-sync-listener.php
+++ sync/class.jetpack-sync-listener.php
@@ -271,22 +271,19 @@ class Jetpack_Sync_Listener {
 	}
 
 	function should_send_user_data_with_actor( $current_filter ) {
-		if ( ! in_array( $current_filter, array( 'wp_login', 'wp_logout', 'jetpack_valid_failed_login_attempt' ) ) ) {
-			// Only send data with login-related events
-			return false;
-		}
-
+		$should_send = in_array( $current_filter, array( 'wp_login', 'wp_logout', 'jetpack_valid_failed_login_attempt' ) ) ?
+			true : false;
 		/**
 		 * Allow or deny sending actor's user data ( IP and UA ) during a sync event
 		 *
 		 * @since 5.8.0
 		 *
+		 * @module sync
+		 *
 		 * @param bool True if we should send user data
+		 * @param string The current filter that is performing the sync action
 		 */
-		if ( ! apply_filters( 'jetpack_sync_actor_user_data', true ) ) {
-			return false;
-		}
-		return true;
+		return apply_filters( 'jetpack_sync_actor_user_data', $should_send, $current_filter );
 	}
 
 	function set_defaults() {
