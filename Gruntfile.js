1365bb352e64a92f0f1300ab60a32b26d031b9a1
Alex Mills <Viper007Bond@users.noreply.github.com>
2018-01-30 12:08:20 -0800

    Fix indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
                             '!node_modules/**',
                             '!tests/**',
                             '!tools/**',
-							'!vendor/**'
+                            '!vendor/**'
                         ]
                     }
                 }
