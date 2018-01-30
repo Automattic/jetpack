55e3dea7c3a6fd092888b33f20004f49730b346e
Igor Zinovyev <zinigor@gmail.com>
2018-01-30 17:37:38 +0300

    Merges changes to the GettyImages shortcode from WordPress.com. (#8591)
    * Merges changes to the GettyImages shortcode from WordPress.com.
    
    This is a merge of changes from WordPress.com related to the new format of markup returned by GettyImages oembed endpoints. D9498-wpcom is the mirror change.
    
    * Adapted caller parameter handling to the new logic.
    
    * Added shortcode initialization fix.

--- modules/shortcodes/getty.php
+++ modules/shortcodes/getty.php
@@ -9,32 +9,67 @@
 if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
 	add_action( 'init', 'jetpack_getty_enable_embeds' );
 } else {
-	jetpack_getty_enable_embeds( 'jetpack' );
+	jetpack_getty_enable_embeds();
 }
 
 /**
  * Register Getty as oembed provider. Add filter to reverse iframes to shortcode. Register [getty] shortcode.
  *
  * @since 4.5.0
- *
- * @param string $site Can be 'wpcom' or 'jetpack' and determines if we're in wpcom or in a Jetpack site.
+ * @since 5.8.0 removed string parameter.
  */
-function jetpack_getty_enable_embeds( $site = 'wpcom' ) {
-
-	// Set the caller argument to pass to Getty's oembed provider.
-	$caller = 'jetpack' === $site
-		? parse_url( get_home_url(), PHP_URL_HOST )
-		: 'wordpress.com';
+function jetpack_getty_enable_embeds() {
 
 	// Support their oEmbed Endpoint
-	wp_oembed_add_provider( '#https?://www\.gettyimages\.com/detail/.*#i', "https://embed.gettyimages.com/oembed/?caller=$caller", true );
-	wp_oembed_add_provider( '#https?://(www\.)?gty\.im/.*#i',              "https://embed.gettyimages.com/oembed/?caller=$caller", true );
+	wp_oembed_add_provider( '#https?://www\.gettyimages\.com/detail/.*#i', "https://embed.gettyimages.com/oembed/", true );
+	wp_oembed_add_provider( '#https?://(www\.)?gty\.im/.*#i',              "https://embed.gettyimages.com/oembed/", true );
 
 	// Allow iframes to be filtered to short code (so direct copy+paste can be done)
 	add_filter( 'pre_kses', 'wpcom_shortcodereverse_getty' );
 
 	// Actually display the Getty Embed
 	add_shortcode( 'getty', 'jetpack_getty_shortcode' );
+}
+
+/**
+ * Filters the oEmbed provider URL for Getty URLs to include site URL host as
+ * caller if available, falling back to "wordpress.com". Must be applied at
+ * time of embed in case that `init` is too early (WP.com REST API).
+ *
+ * @module shortcodes
+ *
+ * @since 5.8.0
+ *
+ * @see WP_oEmbed::fetch
+ *
+ * @return string oEmbed provider URL
+ */
+add_filter( 'oembed_fetch_url', 'getty_add_oembed_endpoint_caller' );
+
+function getty_add_oembed_endpoint_caller( $provider ) {
+	// By time filter is called, original provider URL has had url, maxwidth,
+	// maxheight query parameters added.
+	if ( 0 !== strpos( $provider, 'https://embed.gettyimages.com/oembed/' ) ) {
+		return $provider;
+	}
+
+	// Set the caller argument to pass to Getty's oembed provider.
+	if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
+
+		// Only include caller for non-private sites
+		if ( ! function_exists( 'is_private_blog' ) || ! is_private_blog() ) {
+			$host = parse_url( get_bloginfo( 'url' ), PHP_URL_HOST );
+		}
+
+		// Fall back to WordPress.com
+		if ( empty( $host ) ) {
+			$host = 'wordpress.com';
+		}
+	} else {
+		$host = parse_url( get_home_url(), PHP_URL_HOST );
+	}
+
+	return add_query_arg( 'caller', $host, $provider );
 }
 
 /**
@@ -47,38 +82,67 @@ function jetpack_getty_enable_embeds( $site = 'wpcom' ) {
  * @return mixed
  */
 function wpcom_shortcodereverse_getty( $content ) {
-	if ( ! is_string( $content ) || false === stripos( $content, 'embed.gettyimages.com/embed' ) ) {
+	if ( ! is_string( $content ) || false === stripos( $content, '.gettyimages.com/' ) ) {
 		return $content;
 	}
 
-	$regexp = '!<iframe\s+src=[\'"](https?:)?//embed\.gettyimages\.com/embed(/|/?\?assets=)(\d+(,\d+)*)[^\'"]*?[\'"]((?:\s+\w+=[\'"][^\'"]*[\'"])*)((?:[\s\w]*))></iframe>!i';
+	$regexp = '!<iframe\s+src=[\'"](https?:)?//embed\.gettyimages\.com/embed(/|/?\?assets=)([a-z0-9_-]+(,[a-z0-9_-]+)*)[^\'"]*?[\'"]((?:\s+\w+=[\'"][^\'"]*[\'"])*)((?:[\s\w]*))></iframe>!i';
 	$regexp_ent = str_replace( '&amp;#0*58;', '&amp;#0*58;|&#0*58;', htmlspecialchars( $regexp, ENT_NOQUOTES ) );
 
-	foreach ( array( 'regexp', 'regexp_ent' ) as $reg ) {
+
+	// Markup pattern for 2017 embed syntax with significant differences from
+	// the prior pattern:
+	$regexp_2017 = '!<a.+?class=\'gie-(single|slideshow)\'.+?gie\.widgets\.load\({([^}]+)}\).+?embed-cdn\.gettyimages\.com/widgets\.js.+?</script>!';
+	$regexp_2017_ent = str_replace( '&amp;#0*58;', '&amp;#0*58;|&#0*58;', htmlspecialchars( $regexp_2017, ENT_NOQUOTES ) );
+
+	foreach ( array( 'regexp_2017', 'regexp_2017_ent', 'regexp', 'regexp_ent' ) as $reg ) {
 		if ( ! preg_match_all( $$reg, $content, $matches, PREG_SET_ORDER ) ) {
 			continue;
 		}
 
 		foreach ( $matches as $match ) {
-			$ids = esc_html( $match[3] );
+			if ( 'regexp_2017' === $reg || 'regexp_2017_ent' === $reg ) {
+				// Extract individual keys from the matched JavaScript object
+				$params = $match[2];
+				if ( ! preg_match_all( '!(?P<key>\w+)\s*:\s*([\'"](?P<value>[^\'"]*?)(px)?[\'"])!', $params, $key_matches, PREG_SET_ORDER ) ) {
+					continue;
+				}
 
-			$params = $match[5];
+				foreach ( $key_matches as $key_match ) {
+					switch ( $key_match['key'] ) {
+						case 'items': $ids = $key_match['value']; break;
+						case 'w': $width = (int) $key_match['value']; break;
+						case 'h': $height = (int) $key_match['value']; break;
+						case 'tld': $tld = $key_match['value']; break;
+					}
+				}
+			} else {
+				$params = $match[5];
+				if ( 'regexp_ent' === $reg ) {
+					$params = html_entity_decode( $params );
+				}
+				$params = wp_kses_hair( $params, array( 'http' ) );
 
-			if ( 'regexp_ent' == $reg ) {
-				$params = html_entity_decode( $params );
+				$ids = esc_html( $match[3] );
+				$width = isset( $params['width'] ) ? (int) $params['width']['value'] : 0;
+				$height = isset( $params['height'] ) ? (int) $params['height']['value'] : 0;
 			}
 
-			$params = wp_kses_hair( $params, array( 'http' ) );
-
-			$width = isset( $params['width'] ) ? (int) $params['width']['value'] : 0;
-			$height = isset( $params['height'] ) ? (int) $params['height']['value'] : 0;
+			if ( empty( $ids ) ) {
+				continue;
+			}
 
 			$shortcode = '[getty src="' . esc_attr( $ids ) . '"';
-			if ( $width ) {
+			if ( ! empty( $width ) ) {
 				$shortcode .= ' width="' . esc_attr( $width ) . '"';
 			}
-			if ( $height ) {
+			if ( ! empty( $height ) ) {
 				$shortcode .= ' height="' . esc_attr( $height ) . '"';
+			}
+			// While it does not appear to have any practical impact, Getty has
+			// requested that we include TLD in the embed request
+			if ( ! empty( $tld ) ) {
+				$shortcode .= ' tld="' . esc_attr( $tld ). '"';
 			}
 			$shortcode .= ']';
 
@@ -128,11 +192,16 @@ function jetpack_getty_shortcode( $atts, $content = '' ) {
 		return '<!-- Missing Getty Source ID -->';
 	}
 
-	$src = preg_replace( '/^(\d+(,\d+)*).*$/', '$1', $src );
+	$src = preg_replace( '/^([\da-z-]+(,[\da-z-]+)*).*$/', '$1', $src );
 
-	$args = array();
-	$args['width'] = isset( $atts['width'] ) ? (int) $atts['width'] : '462';
-	$args['height'] = isset( $atts['height'] ) ? (int) $atts['height'] : '370';
+	$params = array(
+		'width'  => isset( $atts['width'] ) ? (int) $atts['width'] : null,
+		'height' => isset( $atts['height'] ) ? (int) $atts['height'] : null
+	);
 
-	return wp_oembed_get( 'https://gty.im/' . $src, $args );
+	if ( ! empty( $atts['tld'] ) ) {
+		$params['tld'] = $atts['tld'];
+	}
+
+	return wp_oembed_get( 'https://gty.im/' . $src, array_filter( $params ) );
 }
