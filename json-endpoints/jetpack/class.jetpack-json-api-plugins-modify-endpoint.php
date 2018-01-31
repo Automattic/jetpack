Automattic/jetpack

commit 98978c1ac91787c39a8cf84332e7b8590cc2e49d

Add: Sync  Plugin Updated event. (#8654)
* Add: Sync  Plugin Updated event.

This PR adds test for plugin updates and plugin update failures.

* Fixes: Plugin Update API returns error when there is an error

Currenly when a plugin update error happends we do not always return an error as expected.
This is due to the different types of errors that exists.

* ups

* Mark Autoupdate plugins as such so that we can tell them apart from regular plugin updates.

* empty() needs to access the value by reference prior to PHP 5.5

https://stackoverflow.com/questions/1075534/cant-use-method-return-value-in-write-context

* let's call empty, looks better

* Make sure that we do have an error code before returing false.

* Add jetpack_installed_plugin to the deprecated list of actions

* Format the code in an nicer way

json-endpoints/jetpack/class.jetpack-json-api-plugins-modify-endpoint.php
@@ -17,6 +17,9 @@ new Jetpack_JSON_API_Plugins_Modify_Endpoint(
 			'active'       => '(bool) Activate or deactivate the plugin',
 			'network_wide' => '(bool) Do action network wide (default value: false)',
 		),
+		'query_parameters'     => array(
+			'autoupdate' => '(bool=false) If the update is happening as a result of autoupdate event',
+		),
 		'response_format'      => Jetpack_JSON_API_Plugins_Endpoint::$_response_format,
 		'example_request_data' => array(
 			'headers' => array(
@@ -47,6 +50,9 @@ new Jetpack_JSON_API_Plugins_Modify_Endpoint(
 			'active'       => '(bool) Activate or deactivate the plugin',
 			'network_wide' => '(bool) Do action network wide (default value: false)',
 			'plugins'      => '(array) A list of plugin ids to modify',
+		),
+		'query_parameters'     => array(
+			'autoupdate' => '(bool=false) If the update is happening as a result of autoupdate event',
 		),
 		'response_format'      => array(
 			'plugins'     => '(array:plugin) An array of plugin objects.',
@@ -81,6 +87,9 @@ new Jetpack_JSON_API_Plugins_Modify_Endpoint(
 		'path_labels'          => array(
 			'$site'   => '(int|string) The site ID, The site domain',
 			'$plugin' => '(string) The plugin ID',
+		),
+		'query_parameters'     => array(
+			'invite_accepted' => '(bool=false) If the user is being created in the invite context',
 		),
 		'response_format'      => Jetpack_JSON_API_Plugins_Endpoint::$_response_format,
 		'example_request_data' => array(
@@ -186,20 +195,20 @@ class Jetpack_JSON_API_Plugins_Modify_Endpoint extends Jetpack_JSON_API_Plugins_
 		foreach ( $this->plugins as $plugin ) {
 
 			if ( ! $this->current_user_can( 'activate_plugin', $plugin ) ) {
-				$this->log[ $plugin ]['error'] = __( 'Sorry, you are not allowed to activate this plugin.' );
+				$this->log[$plugin]['error'] = __( 'Sorry, you are not allowed to activate this plugin.' );
 				$has_errors                  = true;
 				$permission_error            = true;
 				continue;
 			}
 
 			if ( ( ! $this->network_wide && Jetpack::is_plugin_active( $plugin ) ) || is_plugin_active_for_network( $plugin ) ) {
-				$this->log[ $plugin ]['error'] = __( 'The Plugin is already active.', 'jetpack' );
+				$this->log[$plugin]['error'] = __( 'The Plugin is already active.', 'jetpack' );
 				$has_errors                  = true;
 				continue;
 			}
 
 			if ( ! $this->network_wide && is_network_only_plugin( $plugin ) && is_multisite() ) {
-				$this->log[ $plugin ]['error'] = __( 'Plugin can only be Network Activated', 'jetpack' );
+				$this->log[$plugin]['error'] = __( 'Plugin can only be Network Activated', 'jetpack' );
 				$has_errors                  = true;
 				continue;
 			}
@@ -207,7 +216,7 @@ class Jetpack_JSON_API_Plugins_Modify_Endpoint extends Jetpack_JSON_API_Plugins_
 			$result = activate_plugin( $plugin, '', $this->network_wide );
 
 			if ( is_wp_error( $result ) ) {
-				$this->log[ $plugin ]['error'] = $result->get_error_messages();
+				$this->log[$plugin]['error'] = $result->get_error_messages();
 				$has_errors                  = true;
 				continue;
 			}
@@ -218,19 +227,20 @@ class Jetpack_JSON_API_Plugins_Modify_Endpoint extends Jetpack_JSON_API_Plugins_
 			}
 
 			if ( ! $success ) {
-				$this->log[ $plugin ]['error'] = $result->get_error_messages;
+				$this->log[$plugin]['error'] = $result->get_error_messages;
 				$has_errors                  = true;
 				continue;
 			}
-			$this->log[ $plugin ][] = __( 'Plugin activated.', 'jetpack' );
+			$this->log[$plugin][] = __( 'Plugin activated.', 'jetpack' );
 		}
 
-		if ( ! $this->bulk && isset( $has_errors )  ) {
+		if ( ! $this->bulk && isset( $has_errors ) ) {
 			$plugin = $this->plugins[0];
 			if ( $permission_error ) {
-				return new WP_Error( 'unauthorized_error', $this->log[ $plugin ]['error'], 403 );
+				return new WP_Error( 'unauthorized_error', $this->log[$plugin]['error'], 403 );
 			}
-			return new WP_Error( 'activation_error', $this->log[ $plugin ]['error'] );
+
+			return new WP_Error( 'activation_error', $this->log[$plugin]['error'] );
 		}
 	}
 
@@ -240,8 +250,10 @@ class Jetpack_JSON_API_Plugins_Modify_Endpoint extends Jetpack_JSON_API_Plugins_
 			if ( $plugin ) {
 				return current_user_can( $capability, $plugin );
 			}
+
 			return current_user_can( $capability );
 		}
+
 		// Assume that the user has the activate plugins capability.
 		return current_user_can( 'activate_plugins' );
 
@@ -250,14 +262,14 @@ class Jetpack_JSON_API_Plugins_Modify_Endpoint extends Jetpack_JSON_API_Plugins_
 	protected function deactivate() {
 		$permission_error = false;
 		foreach ( $this->plugins as $plugin ) {
-			if ( ! $this->current_user_can('deactivate_plugin', $plugin ) ) {
-				$error = $this->log[ $plugin ]['error'] = __( 'Sorry, you are not allowed to deactivate this plugin.', 'jetpack' );
-				$permission_error = true;
+			if ( ! $this->current_user_can( 'deactivate_plugin', $plugin ) ) {
+				$error = $this->log[$plugin]['error'] = __( 'Sorry, you are not allowed to deactivate this plugin.', 'jetpack' );
+				$permission_error                     = true;
 				continue;
 			}
 
 			if ( ! Jetpack::is_plugin_active( $plugin ) ) {
-				$error = $this->log[ $plugin ]['error'] = __( 'The Plugin is already deactivated.', 'jetpack' );
+				$error = $this->log[$plugin]['error'] = __( 'The Plugin is already deactivated.', 'jetpack' );
 				continue;
 			}
 
@@ -269,21 +281,25 @@ class Jetpack_JSON_API_Plugins_Modify_Endpoint extends Jetpack_JSON_API_Plugins_
 			}
 
 			if ( ! $success ) {
-				$error = $this->log[ $plugin ]['error'] = __( 'There was an error deactivating your plugin', 'jetpack' );
+				$error = $this->log[$plugin]['error'] = __( 'There was an error deactivating your plugin', 'jetpack' );
 				continue;
 			}
-			$this->log[ $plugin ][] = __( 'Plugin deactivated.', 'jetpack' );
+			$this->log[$plugin][] = __( 'Plugin deactivated.', 'jetpack' );
 		}
 		if ( ! $this->bulk && isset( $error ) ) {
 			if ( $permission_error ) {
 				return new WP_Error( 'unauthorized_error', $error, 403 );
 			}
+
 			return new WP_Error( 'deactivation_error', $error );
 		}
 	}
 
 	protected function update() {
-
+		$query_args = $this->query_args();
+		if ( isset( $query_args['autoupdate'] ) && $query_args['autoupdate'] ) {
+			Jetpack_Constants::set_constant( 'JETPACK_PLUGIN_AUTOUPDATE', true );
+		}
 		wp_clean_plugins_cache();
 		ob_start();
 		wp_update_plugins(); // Check for Plugin updates
@@ -311,7 +327,7 @@ class Jetpack_JSON_API_Plugins_Modify_Endpoint extends Jetpack_JSON_API_Plugins_
 		foreach ( $this->plugins as $plugin ) {
 
 			if ( ! in_array( $plugin, $plugin_updates_needed ) ) {
-				$this->log[ $plugin ][] = __( 'No update needed', 'jetpack' );
+				$this->log[$plugin][] = __( 'No update needed', 'jetpack' );
 				continue;
 			}
 
@@ -329,15 +345,19 @@ class Jetpack_JSON_API_Plugins_Modify_Endpoint extends Jetpack_JSON_API_Plugins_
 			$update_attempted = true;
 
 			// Object created inside the for loop to clean the messages for each plugin
-			$skin = new Automatic_Upgrader_Skin();
+			$skin = new WP_Ajax_Upgrader_Skin();
 			// The Automatic_Upgrader_Skin skin shouldn't output anything.
 			$upgrader = new Plugin_Upgrader( $skin );
 			$upgrader->init();
 			// This avoids the plugin to be deactivated.
 			// Using bulk upgrade puts the site into maintenance mode during the upgrades
-			$result = $upgrader->bulk_upgrade( array( $plugin ) );
+			$result             = $upgrader->bulk_upgrade( array( $plugin ) );
+			$errors             = $upgrader->skin->get_errors();
+			$this->log[$plugin] = $upgrader->skin->get_upgrade_messages();
 
-			$this->log[ $plugin ] = $upgrader->skin->get_upgrade_messages();
+			if ( is_wp_error( $errors ) && $errors->get_error_code() ) {
+				return $errors;
+			}
 		}
 
 		if ( ! $this->bulk && ! $result && $update_attempted ) {
@@ -368,7 +388,7 @@ class Jetpack_JSON_API_Plugins_Modify_Endpoint extends Jetpack_JSON_API_Plugins_
 			$translation = array_filter( $available_updates->translations, array( $this, 'get_translation' ) );
 
 			if ( empty( $translation ) ) {
-				$this->log[ $plugin ][] = __( 'No update needed', 'jetpack' );
+				$this->log[$plugin][] = __( 'No update needed', 'jetpack' );
 				continue;
 			}
 

class.jetpack.php
@@ -6486,6 +6486,7 @@ p {
 			'jetpack_has_identity_crisis'                            => 'jetpack_sync_error_idc_validation',
 			'jetpack_is_post_mailable'                               => null,
 			'jetpack_seo_site_host'                                  => null,
+			'jetpack_installed_plugin'                               => 'jetpack_plugin_installed',
 		);
 
 		// This is a silly loop depth. Better way?

json-endpoints/jetpack/class.jetpack-json-api-plugins-modify-v1-2-endpoint.php
@@ -16,6 +16,9 @@ new Jetpack_JSON_API_Plugins_Modify_v1_2_Endpoint(
 			'active'       => '(bool) Activate or deactivate the plugin',
 			'network_wide' => '(bool) Do action network wide (default value: false)',
 		),
+		'query_parameters'     => array(
+			'autoupdate' => '(bool=false) If the update is happening as a result of autoupdate event',
+		),
 		'response_format'      => Jetpack_JSON_API_Plugins_Endpoint::$_response_format_v1_2,
 		'example_request_data' => array(
 			'headers' => array(
@@ -45,6 +48,9 @@ new Jetpack_JSON_API_Plugins_Modify_v1_2_Endpoint(
 			'active'       => '(bool) Activate or deactivate the plugin',
 			'network_wide' => '(bool) Do action network wide (default value: false)',
 			'plugins'      => '(array) A list of plugin ids to modify',
+		),
+		'query_parameters'     => array(
+			'autoupdate' => '(bool=false) If the update is happening as a result of autoupdate event',
 		),
 		'response_format'      => array(
 			'plugins'     => '(array:plugin_v1_2) An array of plugin objects.',
@@ -79,6 +85,9 @@ new Jetpack_JSON_API_Plugins_Modify_v1_2_Endpoint(
 			'$site'   => '(int|string) The site ID, The site domain',
 			'$plugin' => '(string) The plugin ID',
 		),
+		'query_parameters'     => array(
+			'autoupdate' => '(bool=false) If the update is happening as a result of autoupdate event',
+		),
 		'response_format'      => Jetpack_JSON_API_Plugins_Endpoint::$_response_format_v1_2,
 		'example_request_data' => array(
 			'headers' => array(
@@ -93,7 +102,7 @@ class Jetpack_JSON_API_Plugins_Modify_v1_2_Endpoint extends Jetpack_JSON_API_Plu
 
 	protected function activate() {
 		$permission_error = false;
-		$has_errors = false;
+		$has_errors       = false;
 		foreach ( $this->plugins as $plugin ) {
 
 			if ( ! $this->current_user_can( 'activate_plugin', $plugin ) ) {
@@ -116,8 +125,8 @@ class Jetpack_JSON_API_Plugins_Modify_v1_2_Endpoint extends Jetpack_JSON_API_Plu
 			$result = activate_plugin( $plugin, '', $this->network_wide );
 
 			if ( is_wp_error( $result ) ) {
-				$this->log[ $plugin ]['error'] = $result->get_error_messages();
-				$has_errors = true;
+				$this->log[$plugin]['error'] = $result->get_error_messages();
+				$has_errors                  = true;
 				continue;
 			}
 
@@ -127,18 +136,19 @@ class Jetpack_JSON_API_Plugins_Modify_v1_2_Endpoint extends Jetpack_JSON_API_Plu
 			}
 
 			if ( ! $success ) {
-				$this->log[ $plugin ]['error'] = $result->get_error_messages;
-				$has_errors = true;
+				$this->log[$plugin]['error'] = $result->get_error_messages;
+				$has_errors                  = true;
 				continue;
 			}
-			$this->log[ $plugin ][] = __( 'Plugin activated.', 'jetpack' );
+			$this->log[$plugin][] = __( 'Plugin activated.', 'jetpack' );
 		}
 
 		if ( ! $this->bulk && $has_errors ) {
 			$plugin = $this->plugins[0];
 			if ( $permission_error ) {
-				return new WP_Error( 'unauthorized_error', $this->log[ $plugin ]['error'], 403 );
+				return new WP_Error( 'unauthorized_error', $this->log[$plugin]['error'], 403 );
 			}
+
 			return new WP_Error( 'activation_error', $this->log[$plugin]['error'] );
 		}
 	}
@@ -147,9 +157,9 @@ class Jetpack_JSON_API_Plugins_Modify_v1_2_Endpoint extends Jetpack_JSON_API_Plu
 	protected function deactivate() {
 		$permission_error = false;
 		foreach ( $this->plugins as $plugin ) {
-			if ( ! $this->current_user_can('deactivate_plugin', $plugin ) ) {
-				$error = $this->log[ $plugin ]['error'] = __( 'Sorry, you are not allowed to deactivate this plugin.', 'jetpack' );
-				$permission_error = true;
+			if ( ! $this->current_user_can( 'deactivate_plugin', $plugin ) ) {
+				$error = $this->log[$plugin]['error'] = __( 'Sorry, you are not allowed to deactivate this plugin.', 'jetpack' );
+				$permission_error                     = true;
 				continue;
 			}
 
@@ -165,15 +175,16 @@ class Jetpack_JSON_API_Plugins_Modify_v1_2_Endpoint extends Jetpack_JSON_API_Plu
 			}
 
 			if ( ! $success ) {
-				$error = $this->log[ $plugin ]['error'] = __( 'There was an error deactivating your plugin', 'jetpack' );
+				$error = $this->log[$plugin]['error'] = __( 'There was an error deactivating your plugin', 'jetpack' );
 				continue;
 			}
-			$this->log[ $plugin ][] = __( 'Plugin deactivated.', 'jetpack' );
+			$this->log[$plugin][] = __( 'Plugin deactivated.', 'jetpack' );
 		}
 		if ( ! $this->bulk && isset( $error ) ) {
 			if ( $permission_error ) {
 				return new WP_Error( 'unauthorized_error', $error, 403 );
 			}
+
 			return new WP_Error( 'deactivation_error', $error );
 		}
 	}

sync/class.jetpack-sync-module-plugins.php
@@ -16,9 +16,10 @@ class Jetpack_Sync_Module_Plugins extends Jetpack_Sync_Module {
 		add_action( 'activated_plugin', $callable, 10, 2 );
 		add_action( 'deactivated_plugin', $callable, 10, 2 );
 		add_action( 'delete_plugin',  array( $this, 'delete_plugin') );
-		add_action( 'upgrader_process_complete', array( $this, 'check_upgrader' ), 10, 2 );
-		add_action( 'jetpack_installed_plugin', $callable, 10, 2 );
-		add_action( 'jetpack_plugin_update_failed', $callable, 10, 3 );
+		add_action( 'upgrader_process_complete', array( $this, 'on_upgrader_completion' ), 10, 2 );
+		add_action( 'jetpack_plugin_installed', $callable, 10, 1 );
+		add_action( 'jetpack_plugin_update_failed', $callable, 10, 4 );
+		add_action( 'jetpack_plugins_updated', $callable, 10, 2 );
 		add_action( 'admin_action_update', array( $this, 'check_plugin_edit') );
 		add_action( 'jetpack_edited_plugin', $callable, 10, 2 );
 		add_action( 'wp_ajax_edit-theme-plugin-file', array( $this, 'plugin_edit_ajax' ), 0 );
@@ -30,68 +31,132 @@ class Jetpack_Sync_Module_Plugins extends Jetpack_Sync_Module {
 		//Note that we don't simply 'expand_plugin_data' on the 'delete_plugin' action here because the plugin file is deleted when that action finishes
 	}
 
-	public function check_upgrader( $upgrader, $details) {
-		/*
-		 * Handle plugin update errors
-		 */
-		if (
-			'Plugin_Upgrader' == get_class( $upgrader ) &&
-			isset( $details['type'] ) &&
-			'plugin' == $details['type'] &&
-			(
-				'WP_Ajax_Upgrader_Skin' == get_class( $upgrader->skin ) ||
-			    'Bulk_Plugin_Upgrader_Skin' == get_class( $upgrader->skin )
-			)
-		) {
-			if ( 'WP_Ajax_Upgrader_Skin' == get_class( $upgrader->skin ) ) {
-				$errors = $upgrader->skin->get_errors();
-			} else {
-				$errors = $upgrader->skin->result;
-			}
-
-			if ( is_wp_error( $errors) ) {
-				foreach ( $details['plugins'] as $slug ) {
-					/**
-					 * Sync that a plugin update failed
-					 *
-					 * @since 5.8.0
-					 * 
-					 * @module sync
-					 *
-					 * @param string $plugin, Plugin slug
-					 * @param string Error code
-					 * @param string Error message
-					 */
-					do_action( 'jetpack_plugin_update_failed', $slug, $errors->get_error_code(), $errors->get_error_message() );
-				}
-				return;
-			}
+	public function on_upgrader_completion( $upgrader, $details ) {
+		if ( ! isset( $details['type'] ) ) {
+			return;
 		}
-
-		if ( ! isset( $details['type'] ) ||
-			'plugin' !== $details['type'] ||
-			is_wp_error( $upgrader->skin->result ) ||
-			! method_exists( $upgrader, 'plugin_info' )
-		) {
+		if ( 'plugin' != $details['type'] ) {
 			return;
 		}
 
-		if ( 'install' === $details['action'] ) {
-			$plugin_path = $upgrader->plugin_info();
-			$plugins = get_plugins();
-			$plugin_info = $plugins[ $plugin_path ];
+		if ( ! isset( $details['action'] ) ) {
+			return;
+		}
 
+		$plugins = ( isset( $details['plugins'] ) ? $details['plugins'] : null );
+		if ( empty( $plugins ) ) {
+			$plugins = ( isset( $details['plugin'] ) ? array( $details['plugin'] ) : null );
+		}
+
+		// for plugin installer
+		if ( empty( $plugins ) && method_exists( $upgrader, 'plugin_info' ) ) {
+			$plugins = array( $upgrader->plugin_info() );
+		}
+
+		if ( empty( $plugins ) ) {
+			return; // We shouldn't be here
+		}
+
+		switch ( $details['action'] ) {
+			case 'update':
+				$state  = array(
+					'is_autoupdate' => Jetpack_Constants::is_true( 'JETPACK_PLUGIN_AUTOUPDATE' ),
+				);
+				$errors = $this->get_errors( $upgrader->skin );
+				if ( $errors ) {
+					foreach ( $plugins as $slug ) {
+						/**
+						 * Sync that a plugin update failed
+						 *
+						 * @since  5.8.0
+						 *
+						 * @module sync
+						 *
+						 * @param string $plugin , Plugin slug
+						 * @param        string  Error code
+						 * @param        string  Error message
+						 */
+						do_action( 'jetpack_plugin_update_failed', $this->get_plugin_info( $slug ), $errors['code'], $errors['message'], $state );
+					}
+
+					return;
+				}
+				/**
+				 * Sync that a plugin update
+				 *
+				 * @since  5.8.0
+				 *
+				 * @module sync
+				 *
+				 * @param array () $plugin, Plugin Data
+				 */
+				do_action( 'jetpack_plugins_updated', array_map( array( $this, 'get_plugin_info' ), $plugins ), $state );
+				break;
+			case 'install':
+
+		}
+
+		if ( 'install' === $details['action'] ) {
+			$errors = $this->get_errors( $upgrader->skin );
+			if ( $errors ) {
+				foreach ( $plugins as $slug ) {
+					/**
+					 * Sync that a plugin update failed
+					 *
+					 * @since  5.8.0
+					 *
+					 * @module sync
+					 *
+					 * @param string $plugin , Plugin slug
+					 * @param        string  Error code
+					 * @param        string  Error message
+					 */
+					do_action( 'jetpack_plugin_install_failed', $this->get_plugin_info( $slug ), $errors['code'], $errors['message'] );
+				}
+
+				return;
+			}
 			/**
 			 * Signals to the sync listener that a plugin was installed and a sync action
 			 * reflecting the installation and the plugin info should be sent
 			 *
-			 * @since 4.9.0
+			 * @since  5.8.0
 			 *
-			 * @param string $plugin_path Path of plugin installed
-			 * @param mixed $plugin_info Array of info describing plugin installed
+			 * @module sync
+			 *
+			 * @param array () $plugin, Plugin Data
 			 */
-			do_action( 'jetpack_installed_plugin', $plugin_path, $plugin_info );
+			do_action( 'jetpack_plugin_installed', array_map( array( $this, 'get_plugin_info' ), $plugins ) );
+
+			return;
 		}
+	}
+
+	private function get_plugin_info( $slug ) {
+		$plugins = get_plugins();
+		return isset( $plugins[ $slug ] ) ? array_merge( array( 'slug' => $slug), $plugins[ $slug ] ): array( 'slug' => $slug );
+	}
+
+	private function get_errors( $skin ) {
+		$errors = method_exists( $skin, 'get_errors' ) ? $skin->get_errors() : null;
+		if ( is_wp_error( $errors ) ) {
+			$error_code = $errors->get_error_code();
+			if ( ! empty( $error_code ) ) {
+				return array( 'code' => $error_code, 'message' => $errors->get_error_message() );
+			}
+		}
+
+		if ( isset( $skin->result ) ) {
+			$errors = $skin->result;
+			if ( is_wp_error( $errors ) ) {
+				return array( 'code' => $errors->get_error_code(), 'message' => $errors->get_error_message() );
+			}
+
+			if ( false == $skin->result ) {
+				return array( 'code' => 'unknown', 'message' => __( 'Unknown Plugin Update Failure', 'jetpack' ) );
+			}
+		}
+		return false;
 	}
 
 	public function check_plugin_edit() {

tests/php/sync/test_class.jetpack-sync-callables.php
@@ -830,7 +830,7 @@ class WP_Test_Jetpack_Sync_Functions extends WP_Test_Jetpack_Sync_Base {
 
 		$upgrader = (object) array(
 			'skin' => (object) array(
-				'result' => new WP_Error( 'fail' )
+				'result' => new WP_Error( 'fail', 'Fail' )
 			)
 		);
 
@@ -838,7 +838,7 @@ class WP_Test_Jetpack_Sync_Functions extends WP_Test_Jetpack_Sync_Base {
 			'action' => 'update',
 			'type' => 'plugin',
 			'bulk' => true,
-			'plugins' => 'the/the.php',
+			'plugins' => array( 'the/the.php' ),
 		) );
 
 		$this->sender->do_sync();

commit d0a6a040a5e2d73110aed6bf0a57836631dd9011

Fix a typo
Introduced in https://github.com/Automattic/jetpack/pull/8654

json-endpoints/jetpack/class.jetpack-json-api-plugins-modify-endpoint.php
@@ -89,7 +89,7 @@ new Jetpack_JSON_API_Plugins_Modify_Endpoint(
 			'$plugin' => '(string) The plugin ID',
 		),
 		'query_parameters'     => array(
-			'invite_accepted' => '(bool=false) If the user is being created in the invite context',
+			'autoupdate' => '(bool=false) If the user is being created in the invite context',
 		),
 		'response_format'      => Jetpack_JSON_API_Plugins_Endpoint::$_response_format,
 		'example_request_data' => array(