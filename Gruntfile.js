d49d5327045b44c2730ad75782247c3b0a81d661
Mikael Korpela <mikael@ihminen.org>
2018-02-16 23:19:42 +0200

    Build: es6ify build files
    1. fix with eslint:
      ```
      node_modules/.bin/eslint --ext .js --ext .jsx ./*.js -c .eslintrc --fix
      ```
    
    2. Manually fixing following warnings:
      ```
      Gruntfile.js
        4:6  error    'path' is assigned a value but never used        no-unused-vars
        4:6  warning  'path' is never reassigned. Use 'const' instead  prefer-const
        5:3  warning  'cfg' is never reassigned. Use 'const' instead   prefer-const
    
      webpack.config.js
          5:7  error  'fs' is assigned a value but never used             no-unused-vars
          9:7  error  'IS_HOT_UPDATE' is assigned a value but never used  no-unused-vars
         78:2  error  Mixed spaces and tabs                               no-mixed-spaces-and-tabs
         79:2  error  Mixed spaces and tabs                               no-mixed-spaces-and-tabs
        124:2  error  Do not use 'new' for side effects                   no-new
      ```
    
    3. Clean out commented out code from `webpack.config.js`
    
    4. Leave one eslint warning (`no-new` for `new webpack.DefinePlugin()`) in `webpack.config.js` and add ignore-rule
    
    5. Reorganise comments to fit 80 chars line width in `webpack.config.js`

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,43 @@
 /* global module, require */
 
-module.exports = function(grunt) {
-    var path = require( 'path' ),
-        cfg = {
-            pkg: grunt.file.readJSON('package.json'),
-            makepot: {
-                jetpack: {
-                    options: {
-                        domainPath: '/languages',
-                        exclude: [
-                            'node_modules',
-                            'tests',
-                            'tools',
-                            'vendor'
-                        ],
-                        mainFile:    'jetpack.php',
-                        potFilename: 'jetpack.pot'
-                    }
-                }
-            },
-            addtextdomain: {
-                jetpack: {
-                    options: {
-                        textdomain: 'jetpack'
-                    },
-                    files: {
-                        src: [
-                            '*.php',
-                            '**/*.php',
-                            '!node_modules/**',
-                            '!tests/**',
-                            '!tools/**',
-                            '!vendor/**'
-                        ]
-                    }
-                }
-            }
-        };
+module.exports = function( grunt ) {
+	const cfg = {
+		pkg: grunt.file.readJSON( 'package.json' ),
+		makepot: {
+			jetpack: {
+				options: {
+					domainPath: '/languages',
+					exclude: [
+						'node_modules',
+						'tests',
+						'tools',
+						'vendor'
+					],
+					mainFile: 'jetpack.php',
+					potFilename: 'jetpack.pot'
+				}
+			}
+		},
+		addtextdomain: {
+			jetpack: {
+				options: {
+					textdomain: 'jetpack'
+				},
+				files: {
+					src: [
+						'*.php',
+						'**/*.php',
+						'!node_modules/**',
+						'!tests/**',
+						'!tools/**',
+						'!vendor/**'
+					]
+				}
+			}
+		}
+	};
 
-    grunt.initConfig( cfg );
+	grunt.initConfig( cfg );
 
-    grunt.loadNpmTasks('grunt-wp-i18n');
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
 };
