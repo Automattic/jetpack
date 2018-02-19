b33be108f23999273c4e8be22e5aeaba981b9e86
Mikael Korpela <mikael@ihminen.org>
2018-02-20 00:37:26 +0200

    Build: `eslint --fix` build files (#8864)
    Mostly just `eslint --fix` build files and a couple extras:
    
    1. Fix with eslint:
      ```
      npx eslint *.js -c .eslintrc --fix
      ```
    
    2. Manually fixing these warnings:
      ```
      Gruntfile.js
        4:6  error    'path' is assigned a value but never used        no-unused-vars
        4:6  warning  'path' is never reassigned. Use 'const' instead  prefer-const
        5:3  warning  'cfg' is never reassigned. Use 'const' instead   prefer-const
    
      webpack.config.js
          5:7  error  'fs' is assigned a value but never used             no-unused-vars
          9:7  error  'IS_HOT_UPDATE' is assigned a value but never used  no-unused-vars
         78:2  error  Mixed spaces and tabs                               no-mixed-spaces-and-tabs
         79:2  error  Mixed spaces and tabs                               no-mixed-spaces-and-tabs
        124:2  error  Do not use 'new' for side effects                   no-new
      ```
    
    3. Clean out commented out code from `webpack.config.js`
    
    4. Leave one eslint warning ([`no-new`](https://eslint.org/docs/rules/no-new) for `new webpack.DefinePlugin()`) in `webpack.config.js` and just add ignore-rule. Adds a note explaining what this part of config does.
    
    5. Reorganise comments to fit 80 chars line width in `webpack.config.js`
    
    6. Remove `es6-promise` polyfill from `webpack.config.js` (since we're requiring Node.js 8 now)
    
    7. Test `*.js` files in the root folder with `yarn lint`

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,20 @@
-require( 'es6-promise' ).polyfill();
+const path = require( 'path' );
+const webpack = require( 'webpack' );
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
 
-var path = require( 'path' );
-var webpack = require( 'webpack' );
-var fs = require('fs');
-var NODE_ENV = process.env.NODE_ENV || 'development';
-var ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+const webpackConfig = {
 
-var IS_HOT_UPDATE = ( process.env.NODE_ENV !== 'production' );
-
-// This file is written in ES5 because it is run via Node.js and is not transpiled by babel. We want to support various versions of node, so it is best to not use any ES6 features even if newer versions support ES6 features out of the box.
-var webpackConfig = {
-
-	// Entry points point to the javascript module that is used to generate the script file.
+	// Entry points point to the javascript module
+	// that is used to generate the script file.
 	// The key is used as the name of the script.
 	entry: {
 		admin: './_inc/client/admin.js',
-		static: './_inc/client/static.jsx'
+		'static': './_inc/client/static.jsx'
 	},
 	output: {
 		path: path.join( __dirname, '_inc/build' ),
-		filename: "[name].js"
+		filename: '[name].js'
 	},
 	module: {
 
@@ -28,30 +23,22 @@ var webpackConfig = {
 			{
 				test: /\.jsx?$/,
 				use: [
-					// {
-					// 	loader: 'eslint-loader',
-					// 	query: {
-					// 		configFile: path.join(__dirname, '.eslintrc'),
-					// 		quiet: true
-					// 	},
-					// },
 					{
 						loader: 'babel-loader',
 						options: {
-							presets: ['es2015', 'stage-1', 'react'],
+							presets: [ 'es2015', 'stage-1', 'react' ],
 							plugins: [
-								"transform-runtime",
-								"add-module-exports",
-								"transform-es3-member-expression-literals",
-								"transform-export-extensions"
+								'transform-runtime',
+								'add-module-exports',
+								'transform-es3-member-expression-literals',
+								'transform-export-extensions'
 							]
 
 						}
 					},
 				],
-
-				// exclude: /node_modules/,
-				// include both typical npm-linked locations and default module locations to handle both cases
+				// include both typical npm-linked locations and default module
+				// locations to handle both cases
 				include: [
 					path.join( __dirname, 'test' ),
 					path.join( __dirname, '_inc/client' ),
@@ -63,10 +50,10 @@ var webpackConfig = {
 			},
 			{
 				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					fallback: "style-loader",
-					use: [ "css-loader", "autoprefixer-loader" ]
-				})
+				use: ExtractTextPlugin.extract( {
+					fallback: 'style-loader',
+					use: [ 'css-loader', 'autoprefixer-loader' ]
+				} )
 			},
 			{
 				test: /\.html$/,
@@ -74,10 +61,10 @@ var webpackConfig = {
 			},
 			{
 				test: /\.scss$/,
-				use: ExtractTextPlugin.extract({
-				  fallback: "style-loader",
-				  use: [ "css-loader", "sass-loader" ]
-				})
+				use: ExtractTextPlugin.extract( {
+					fallback: 'style-loader',
+					use: [ 'css-loader', 'sass-loader' ]
+				} )
 			},
 			{
 				test: /\.svg/,
@@ -88,7 +75,7 @@ var webpackConfig = {
 	resolve: {
 		extensions: [ '.js', '.jsx' ],
 		alias: {
-			"react": path.join(__dirname, "/node_modules/react")
+			react: path.join( __dirname, '/node_modules/react' )
 		},
 		modules: [
 			path.resolve( __dirname, 'node_modules' ),
@@ -99,18 +86,16 @@ var webpackConfig = {
 		modules: [ path.join( __dirname, 'node_modules' ) ]
 	},
 	node: {
-		fs: "empty",
+		fs: 'empty',
 		process: true
 	},
 	plugins: [
-		new webpack.DefinePlugin({
-
-			// NODE_ENV is used inside React to enable/disable features that should only
-			// be used in development
+		new webpack.DefinePlugin( {
+			// NODE_ENV is used inside React to enable/disable features that should
+			// only be used in development
 			'process.env.NODE_ENV': JSON.stringify( NODE_ENV )
-		}),
+		} ),
 		new ExtractTextPlugin( '[name].dops-style.css' ),
-		// new webpack.optimize.UglifyJsPlugin(),
 	],
 	externals: {
 		'react/addons': true,
@@ -121,10 +106,12 @@ var webpackConfig = {
 };
 
 if ( NODE_ENV === 'production' ) {
-
+	// Create global process.env.NODE_ENV constant available at the browser window
+	// eslint-disable-next-line no-new
 	new webpack.DefinePlugin( {
 		// This has effect on the react lib size
-		'process.env.NODE_ENV': JSON.stringify( process.env.NODE_ENV ) // TODO switch depending on actual environment
+		// TODO switch depending on actual environment
+		'process.env.NODE_ENV': JSON.stringify( process.env.NODE_ENV )
 	} );
 }
 
