ec7e6fbaaa7ba5de254dafbc83e34a37d6ab77b2
Alex Mills <Viper007Bond@users.noreply.github.com>
2018-03-02 19:12:22 -0800

    Simplify search filter HTML and JS.

--- modules/widgets/search/js/search-widget.js
+++ modules/widgets/search/js/search-widget.js
@@ -1,24 +1,15 @@
 jQuery( document ).ready( function() {
-	var filter_list = jQuery( '.jetpack-search-filters-widget__filter-list' );
+	const filter_list = jQuery( '.jetpack-search-filters-widget__filter-list' );
 
 	filter_list.on( 'click', 'a', function() {
-		var checkbox = jQuery( this ).children( 'input[type="checkbox"]' );
-
-		toggle_checkbox( checkbox );
+		const checkbox = jQuery( this ).siblings( 'input[type="checkbox"]' );
+		checkbox.prop( 'checked', ! checkbox.prop( 'checked' ) );
 	} );
 
 	filter_list.find( 'input[type="checkbox"]' ).prop( 'disabled', false ).css( 'cursor', 'inherit' ).on( 'click', function() {
-		var checkbox = jQuery( this );
-
-		toggle_checkbox( checkbox );
-
-		var anchor = checkbox.parent( 'a' );
+		const anchor = jQuery( this ).siblings( 'a' );
 		if ( anchor.length ) {
 			window.location.href = anchor.prop( 'href' );
 		}
 	} );
-
-	function toggle_checkbox( checkbox ) {
-		checkbox.prop( 'checked', ! checkbox.prop( 'checked' ) );
-	}
 } );
