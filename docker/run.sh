0c4e520cb6436eb3d262e2a1702917a459f1b36b
oskosk <oskosk@gmail.com>
2018-03-04 10:42:37 -0300

    Add better inline docs in run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# This file is the entrypoin for the docker image defined in Dockerfile.
+# These commands will be run each time the container is run.
+#
+
+# Configure PHP
 PHP_ERROR_REPORTING=${PHP_ERROR_REPORTING:-"E_ALL"}
 sed -ri 's/^display_errors\s*=\s*Off/display_errors = On/g' /etc/php/7.0/apache2/php.ini
 sed -ri 's/^display_errors\s*=\s*Off/display_errors = On/g' /etc/php/7.0/cli/php.ini
@@ -9,9 +14,14 @@ sed -ri "s/^error_reporting\s*=.*$//g" /etc/php/7.0/cli/php.ini
 echo "error_reporting = $PHP_ERROR_REPORTING" >> /etc/php/7.0/apache2/php.ini
 echo "error_reporting = $PHP_ERROR_REPORTING" >> /etc/php/7.0/cli/php.ini
 
+# Download WordPress
 cd /var/www/ && [ -f /var/www/xmlrpc.php ] || wp --allow-root core download
+# Create a wp-config file respecting Dockerfile-forwarded environment variables
 [ -f /var/www/wp-config.php ] || wp --allow-root config create --dbhost=$WORDPRESS_DB_HOST --dbname=wordpress --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD
-#chown -R www-data:www-data /var/www
+# Change ownership of /var/www to www-data
+# chown -R www-data:www-data /var/www
 
-echo "Starting Apache"
+# Run apache in the foreground so the container keeps running
+
+echo "Starting Apache in the foreground"
 apachectl -D FOREGROUND
