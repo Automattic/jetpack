f704f060eca0cd3730aad92f3603faa02a866afa
Alex Mills <Viper007Bond@users.noreply.github.com>
2018-03-04 21:08:53 -0800

    Use var not const. Using --no-verify to bypass the check.

--- modules/widgets/search/js/search-widget.js
+++ modules/widgets/search/js/search-widget.js
@@ -1,13 +1,13 @@
 jQuery( document ).ready( function() {
-	const filter_list = jQuery( '.jetpack-search-filters-widget__filter-list' );
+	var filter_list = jQuery( '.jetpack-search-filters-widget__filter-list' );
 
 	filter_list.on( 'click', 'a', function() {
-		const checkbox = jQuery( this ).siblings( 'input[type="checkbox"]' );
+		var checkbox = jQuery( this ).siblings( 'input[type="checkbox"]' );
 		checkbox.prop( 'checked', ! checkbox.prop( 'checked' ) );
 	} );
 
 	filter_list.find( 'input[type="checkbox"]' ).prop( 'disabled', false ).css( 'cursor', 'inherit' ).on( 'click', function() {
-		const anchor = jQuery( this ).siblings( 'a' );
+		var anchor = jQuery( this ).siblings( 'a' );
 		if ( anchor.length ) {
 			window.location.href = anchor.prop( 'href' );
 		}
