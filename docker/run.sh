c5268a2477336118b2605990bab101ac1d5f4d21
oskosk <oskosk@gmail.com>
2018-03-10 18:38:33 -0300

    Remove unnecessary configuration of php from run.sh
    This is now done by the php.ini file we copy into /etc

--- docker/run.sh
+++ docker/run.sh
@@ -7,15 +7,6 @@ set -e
 # If you modify anything here, remember to build the image again by running:
 # yarn docker:build
 
-# Configure PHP
-PHP_ERROR_REPORTING=${PHP_ERROR_REPORTING:-"E_ALL"}
-sed -ri 's/^display_errors\s*=\s*Off/display_errors = On/g' /etc/php/7.0/apache2/php.ini
-sed -ri 's/^display_errors\s*=\s*Off/display_errors = On/g' /etc/php/7.0/cli/php.ini
-sed -ri "s/^error_reporting\s*=.*$//g" /etc/php/7.0/apache2/php.ini
-sed -ri "s/^error_reporting\s*=.*$//g" /etc/php/7.0/cli/php.ini
-echo "error_reporting = $PHP_ERROR_REPORTING" >> /etc/php/7.0/apache2/php.ini
-echo "error_reporting = $PHP_ERROR_REPORTING" >> /etc/php/7.0/cli/php.ini
-
 # Download WordPress
 cd /var/www/html/ && [ -f /var/www/html/xmlrpc.php ] || wp --allow-root core download
 
