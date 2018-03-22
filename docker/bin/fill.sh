12696e73d06665fe821107471b50e993b94d096c
Mikael Korpela <mikael@ihminen.org>
2018-03-22 02:00:56 +0200

    Remove debug code

--- docker/bin/fill.sh
+++ docker/bin/fill.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-echo "👇"
-echo "WP_DOMAIN:  ${WP_DOMAIN}"
-echo "WP_ADMIN_USER:  ${WP_ADMIN_USER}"
-echo "WP_ADMIN_EMAIL:  ${WP_ADMIN_EMAIL}"
-echo "WP_ADMIN_PASSWORD:  ${WP_ADMIN_PASSWORD}"
-echo "WP_TITLE:  ${WP_TITLE}"
-echo "WP_DB_NAME:  ${WP_DB_NAME}"
-echo "WP_DB_USER:  ${WP_DB_USER}"
-echo "WP_DB_PASSWORD:  ${WP_DB_PASSWORD}"
-echo "WP_SFTP_USER:  ${WP_SFTP_USER}"
-echo "WP_SFTP_PASSWORD:  ${WP_SFTP_PASSWORD}"
-
-exit 0;
-
 if ! $(wp --allow-root core is-installed); then
 	echo
 	echo "WordPress has to be installed first. To install, run:"
