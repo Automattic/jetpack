92789429e1e4f645fda97e7c8f3eb0c9dfcb1718
Mike Jolley <mike.jolley@me.com>
2018-03-22 11:12:05 +0000

    WooCommerce infinite scroll handling

--- 3rd-party/woocommerce.php
+++ 3rd-party/woocommerce.php
@@ -1,6 +1,27 @@
 <?php
+/**
+ * This file contains compatibility functions for WooCommerce to improve Jetpack feature support.
+ */
+add_action( 'woocommerce_init', 'jetpack_woocommerce_integration' );
 
-add_action( 'woocommerce_share', 'jetpack_woocommerce_social_share_icons', 10 );
+function jetpack_woocommerce_integration() {
+	/**
+	 * Double check WooCommerce exists - unlikely to fail due to the hook being used but better safe than sorry.
+	 */
+	if ( ! class_exists( 'WooCommerce' ) ) {
+		return;
+	}
+
+	add_action( 'woocommerce_share', 'jetpack_woocommerce_social_share_icons', 10 );
+
+	/**
+	 * Wrap in function exists check since this requires WooCommerce 3.3+.
+	 */
+	if ( function_exists( 'wc_get_default_products_per_row' ) ) {
+		add_filter( 'infinite_scroll_render_callbacks', 'jetpack_woocommerce_infinite_scroll_render_callback', 10 );
+		add_filter( 'infinite_scroll_settings', 'jetpack_woocommerce_infinite_scroll_settings', 10 );
+	}
+}
 
 /*
  * Make sure the social sharing icons show up under the product's short description
@@ -11,4 +32,47 @@ function jetpack_woocommerce_social_share_icons() {
 		remove_filter( 'the_excerpt', 'sharing_display', 19 );
 		echo sharing_display();
 	}
+}
+
+/**
+ * Add a callback for WooCommerce product rendering in infinite scroll.
+ *
+ * @param array $callbacks
+ * @return array
+ */
+function jetpack_woocommerce_infinite_scroll_render_callback( $callbacks ) {
+	$callbacks[] = 'jetpack_woocommerce_infinite_scroll_render';
+	return $callbacks;
+}
+
+/**
+ * Add a default renderer for WooCommerce products within infinite scroll.
+ */
+function jetpack_woocommerce_infinite_scroll_render() {
+	if ( ! is_shop() && ! is_product_taxonomy() && ! is_product_category() && ! is_product_tag() ) {
+		return;
+	}
+
+	woocommerce_product_loop_start();
+
+	while ( have_posts() ) {
+		the_post();
+		wc_get_template_part( 'content', 'product' );
+	}
+
+	woocommerce_product_loop_end();
+}
+
+/**
+ * Adjust settings
+ *
+ * @param [type] $settings
+ * @return void
+ */
+function jetpack_woocommerce_infinite_scroll_settings( $settings ) {
+	if ( ! is_shop() && ! is_product_taxonomy() && ! is_product_category() && ! is_product_tag() ) {
+		return $settings;
+	}
+	$settings['posts_per_page'] = absint( apply_filters( 'loop_shop_per_page', wc_get_default_products_per_row() * wc_get_default_product_rows_per_page() ) );
+	return $settings;
 }
