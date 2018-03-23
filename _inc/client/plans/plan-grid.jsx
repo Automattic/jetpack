eeb4d70deffad0ec02635a88340803b3bb4cf875
Matt Wiebe <matt@somadesign.ca>
2018-03-23 11:48:58 -0500

    Plans: don't hide on small screens (#8969)
    Use a smaller card with links to Calypso for small screens

--- _inc/client/plans/plan-grid.jsx
+++ _inc/client/plans/plan-grid.jsx
@@ -16,6 +16,7 @@ import { getSiteRawUrl, getUserId } from 'state/initial-state';
 import { getSitePlan, getAvailablePlans } from 'state/site/reducer';
 import analytics from 'lib/analytics';
 import { getPlanClass } from 'lib/plans/constants';
+import { translate as __ } from 'i18n-calypso';
 
 class PlanGrid extends React.Component {
 
@@ -41,6 +42,7 @@ class PlanGrid extends React.Component {
 
 		return (
 			<div className="plan-features">
+				{ this.renderMobileCard() }
 				<div className="plan-features__content">
 					<table className={ tableClasses }>
 						<tbody>
@@ -52,6 +54,27 @@ class PlanGrid extends React.Component {
 						</tbody>
 					</table>
 				</div>
+			</div>
+		);
+	}
+
+	renderMobileCard() {
+		const currently = __( 'You’re currently on Jetpack %(plan)s.', {
+			args: { plan: this.props.sitePlan.product_name_short }
+		} );
+		const myPlanUrl = `https://wordpress.com/plans/my-plan/${ this.props.siteRawUrl }`;
+		const plansUrl = `https://wordpress.com/plans/${ this.props.siteRawUrl }`;
+
+		return (
+			<div className="plans-mobile-notice dops-card">
+				<h2>{ __( 'Your Plan' ) }</h2>
+				<p>{ currently }</p>
+				<Button href={ myPlanUrl }>
+					{ __( 'Manage your plan' ) }
+				</Button>
+				<Button href={ plansUrl } primary>
+					{ __( 'View all Jetpack plans' ) }
+				</Button>
 			</div>
 		);
 	}
