db3ff0951f3a718901b45c2f199f96d8b56d9fe8
Mikael Korpela <mikael@ihminen.org>
2018-03-27 11:39:17 +0300

    Don't override env vars with docker-compose `environment` list
    List of variables listed in `environment` key at docker-compose.yml would override variables from `.env` files.

--- docker/bin/install.sh
+++ docker/bin/install.sh
@@ -12,7 +12,7 @@ fi
 # Install WP core
 wp --allow-root core install \
 	--url=${WP_DOMAIN} \
-	--title=${WP_TITLE} \
+	--title="${WP_TITLE}" \
 	--admin_user=${WP_ADMIN_USER} \
 	--admin_password=${WP_ADMIN_PASSWORD} \
 	--admin_email=${WP_ADMIN_EMAIL} \
