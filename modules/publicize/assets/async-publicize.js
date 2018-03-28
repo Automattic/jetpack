756b2bbb61b7e596baec886f691d39042c29940b
Chris Shultz <cs.shultz@gmail.com>
2018-03-27 21:27:55 -0700

    Publicize: Utilize experimental HTTP header to differentiate Gutenberg posts
    Experimental Gutenberg editor update in c-shultz:try/publicize-integration
    provides X-WP-Source='Gutenberg' HTTP header to differentiate posts. This
    vastly simplifies the task of detecting if post came from Gutenberg.
    Removing publicize/posts/<post_id>/flag-no-publicize because there's
    no longer a need for it with this approach.

--- modules/publicize/assets/async-publicize.js
+++ modules/publicize/assets/async-publicize.js
@@ -13,34 +13,6 @@
 jQuery( function( $ ) {
 
 	/**
-	 * Flags a soon-to-be-published post for no automatic publicizing.
-	 *
-	 * Calls publicize/posts/<post_id>/flag-no-publicize to
-	 * flag a post to NOT be automatically shared via Publicize.
-	 * This is used to decouple publishing and publicizing of a post.
-	 * This should only be called immediately before publishing because
-	 * the flag expires after a timeout.
-	 *
-	 * @since      5.9.1
-	 *
-	 * @fires   ajaxSend
-	 *
-	 * @param {int}   post_id           Post id of post being flagged
-	 *
-	 * @return None
-	 */
-	flagPostNoPublicize = function( post_id ) {
-		$.ajax({
-			type: 'POST',
-			url: async_publicize_setup.base_url + '/wp-json/publicize/posts/'+ post_id.toString() + '/flag-no-publicize',
-			dataType: 'json',
-			beforeSend: function ( xhr ) {
-				xhr.setRequestHeader( 'X-WP-Nonce', async_publicize_setup.api_nonce );
-			}
-		});
-	}
-
-	/**
 	 * Shares a post to connected social with Publicize
 	 *
 	 * Calls publicize/posts/<post_id>/publicize to directly
