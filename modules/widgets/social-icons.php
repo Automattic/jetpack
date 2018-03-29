6b14aef4c4a162d9425ea3807bbb51192a1fb2db
oskosk <oskosk@gmail.com>
2018-03-29 20:31:10 -0300

    rename loading function too

--- modules/widgets/social-icons.php
+++ modules/widgets/social-icons.php
@@ -6,10 +6,10 @@
  * @access public
  * @return void
  */
-function jetpack_widget_social_icons_load() {
+function jetpack_widget_social_icons_load_rogue() {
 	if ( ! class_exists( 'Jetpack_Widget_Social_Icons' ) ) {
 		require_once( __DIR__ . '/jetpack-social-icons-class.php' );
 		register_widget( 'Jetpack_Widget_Social_Icons' );
 	}
 }
-add_action( 'widgets_init', 'jetpack_widget_social_icons_load' );
+add_action( 'widgets_init', 'jetpack_widget_social_icons_load_rogue' );
