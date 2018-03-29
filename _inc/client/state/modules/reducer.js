a14e528185c4211ce34db55437952da9e8853aac
Eric Binnion <ericbinnion@gmail.com>
2018-03-29 17:20:08 -0500

    Admin Page: Update getModuleOverride docblock

--- _inc/client/state/modules/reducer.js
+++ _inc/client/state/modules/reducer.js
@@ -253,12 +253,13 @@ export function isModuleAvailable( state, moduleSlug ) {
 /**
  * Returns the module override for a given module slug.
  *
- * Expected values are false if no override, active if module forced on,
- * or inactive if module forced off.
+ * Expected values are false if no override, 'active' if module forced on,
+ * or 'inactive' if module forced off.
+ *
  * @param {Object} state Global state tree
  * @param {String} name  A module's name
  *
- * @return {Boolean|String} Whether the override, and if so, how.
+ * @return {Boolean|String} Whether the module is overriden, and if so, how.
  */
 export function getModuleOverride( state, name ) {
 	return get( state.jetpack.modules.items, [ name, 'override' ], false );
