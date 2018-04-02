51861e73fffd5d7e2ee7d3bf8f95f86ca94b930c
Eric Binnion <ericbinnion@gmail.com>
2018-04-02 16:52:01 -0500

    Jetpack Start: XML-RPC: Secret necessary to complete authentication

--- class.jetpack-xmlrpc-server.php
+++ class.jetpack-xmlrpc-server.php
@@ -220,10 +220,6 @@ class Jetpack_XMLRPC_Server {
 			return $this->error( $result, 'jpc_remote_provision_fail' );
 		}
 
-		if ( ! empty( $result['secret'] ) ) {
-			unset( $result['secret'] );
-		}
-
 		$result['client_id'] = Jetpack_Options::get_option( 'id' );
 
 		return $result;
