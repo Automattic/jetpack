d7c8af485bee7018a5cedd0d94e5b53e4293c60a
Mikael Korpela <mikael@ihminen.org>
2018-04-04 18:36:08 +0300

    Add notification about possible errors when removing multisite settings

--- docker/bin/uninstall.sh
+++ docker/bin/uninstall.sh
@@ -14,6 +14,9 @@ rm -fr /var/www/html/wp-content/uploads /var/www/html/wp-content/upgrade
 truncate -s 0 /var/www/html/wp-content/debug.log
 
 # Ensure wp-config.php doesn't have multi-site settings
+echo
+echo "Clearing out possible multi-site related settings from wp-config.php"
+echo "It's okay to see errors if these did't exist..."
 wp --allow-root config delete WP_ALLOW_MULTISITE
 wp --allow-root config delete MULTISITE
 wp --allow-root config delete SUBDOMAIN_INSTALL
