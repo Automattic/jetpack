906bcf706fc19e7b891d440f5bf742c2f2863758
Osk <oskosk@users.noreply.github.com>
2018-04-04 15:23:01 -0300

    Include the file before calling request_filesystem_credentials (#9223)

--- sync/class.jetpack-sync-functions.php
+++ sync/class.jetpack-sync-functions.php
@@ -165,6 +165,11 @@ class Jetpack_Sync_Functions {
 		}
 
 		ob_start();
+
+		if ( ! function_exists( 'request_filesystem_credentials' ) ) {
+			require_once( ABSPATH . 'wp-admin/includes/file.php' );
+		}
+
 		$filesystem_credentials_are_stored = request_filesystem_credentials( self_admin_url() );
 		ob_end_clean();
 		if ( $filesystem_credentials_are_stored ) {
