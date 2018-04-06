cac71023b6455c5e0647027589f2c608faae7aa1
Sean O'Shaughnessy <seanosh@gmail.com>
2018-04-06 15:08:28 -0400

    bump version

--- projects/plugins/wpcomsh/wpcomsh.php
+++ projects/plugins/wpcomsh/wpcomsh.php
@@ -8,7 +8,7 @@
  */
 
 // Increase version number if you change something in wpcomsh.
-define( 'WPCOMSH_VERSION', '2.3.1' );
+define( 'WPCOMSH_VERSION', '2.3.2' );
 
 // If true, Typekit fonts will be available in addition to Google fonts
 add_filter( 'jetpack_fonts_enable_typekit', '__return_true' );
