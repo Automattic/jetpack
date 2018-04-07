af8991dcac1dda4fae82029cdff4e2fd98daae02
haszari <haszari@cartoonbeats.com>
2018-04-07 14:03:07 +1200

    refactor jetpack/gallery to use same component for rendering images in save and edit

--- modules/tiled-gallery/block-save.jsx
+++ modules/tiled-gallery/block-save.jsx
@@ -3,31 +3,16 @@
  */
 import React from 'react';
 
-// this should work great ...
-// ... it seems to fail validation on reloading in editor though, why?
+import jetpackGalleryImage from './gallery-image.jsx';
 
-function JetpackGalleryImage( image ) {
-	const href = image.link;
-	const img = <img src={ image.url } alt={ image.alt } data-id={ image.id } data-link={ image.link } />;
-
-	return (
-		<li key={ image.id || image.url } className="blocks-gallery-item">
-			<figure>
-				{ href ? <a href={ href }>{ img }</a> : img }
-				{ image.caption && image.caption.length > 0 && <figcaption>{ image.caption }</figcaption> }
-			</figure>
-		</li>
-	);
-}
-
-function JetpackGalleryBlockSave( { attributes } ) {
+function jetpackGalleryBlockSave( { attributes } ) {
 	const { images } = attributes;
 	return (
 		<ul>
-			{ images.map( JetpackGalleryImage ) }
+			{ images.map( jetpackGalleryImage ) }
 		</ul>
 	);
 }
 
-export default JetpackGalleryBlockSave;
+export default jetpackGalleryBlockSave;
 
