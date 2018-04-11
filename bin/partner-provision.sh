48eb9ed338fe7eb0bfe99f7b59ae00be30616734
Eric Binnion <ericbinnion@gmail.com>
2018-04-11 18:58:39 -0500

    CLI: JPS: Update partner-provision.sh usage; remove unused args

--- bin/partner-provision.sh
+++ bin/partner-provision.sh
@@ -4,7 +4,19 @@
 # executes wp-cli command to provision Jetpack site for given partner
 
 usage () {
-	echo "Usage: partner-provision.sh --partner_id=partner_id --partner_secret=partner_secret --user=wp_user_id [--plan=plan_name] [--onboarding=1] [--wpcom_user_id=1234] [--wpcom_user_email=wpcom_user_email] [--url=http://example.com] [--force_connect=1] [--force_register=1] [--allow-root] [--home_url] [--site_url] [--partner-tracking-id]"
+	echo 'Usage: partner-provision.sh \
+	--partner_id=partner_id \
+	--partner_secret=partner_secret \
+	--user=wp_user_id \
+	[--plan=plan_name] \
+	[--onboarding=1] \
+	[--wpcom_user_id=1234] \
+	[--wpcom_user_email=wpcom_user_email] \
+	[--url=http://example.com] \
+	[--force_connect=1] \
+	[--force_register=1] \
+	[--allow-root] \
+	[--partner-tracking-id=1]'
 }
 
 GLOBAL_ARGS=""
@@ -39,12 +51,6 @@ for i in "$@"; do
 			shift
 			;;
 		--force_connect=* )         FORCE_CONNECT="${i#*=}"
-			shift
-			;;
-		--site_url=* )              WP_SITEURL="${i#*=}"
-			shift
-			;;
-		--home_url=* )              WP_HOME="${i#*=}"
 			shift
 			;;
 		--partner-tracking-id=* )   PARTNER_TRACKING_ID="${i#*=}"
