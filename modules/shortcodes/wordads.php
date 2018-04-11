f00fd06f5a7115ceb7ef5e83a06ccdb262d834fa
oskosk <oskosk@gmail.com>
2018-04-11 12:55:41 +0300

    Add Insert Ad button

--- modules/shortcodes/wordads.php
+++ modules/shortcodes/wordads.php
@@ -10,8 +10,6 @@ class Jetpack_WordAds_Shortcode {
 
 	function __construct() {
 		add_action( 'init', array( $this, 'action_init' ) );
-
-		//add_filter( 'wp_kses_allowed_html', array( $this, 'add_recipes_kses_rules' ), 10, 2 );
 	}
 
 	/**
@@ -23,6 +21,36 @@ class Jetpack_WordAds_Shortcode {
 
 		// Render [recipe], along with other shortcodes that can be nested within.
 		add_shortcode( 'wordad', array( $this, 'wordads_shortcode' ) );
+	}
+
+	/**
+	 * Add hooks according to screen.
+	 *
+	 * @param WP_Screen $screen Data about current screen.
+	 */
+	public static function add_hooks( $screen ) {
+		if ( isset( $screen->base ) && 'post' === $screen->base ) {
+			add_action( 'admin_notices', array( __CLASS__, 'handle_editor_view_js' ) );
+			add_action( 'admin_head', array( __CLASS__, 'admin_head' ) );
+		}
+	}
+
+	/**
+	 * WordPress Shortcode Editor View JS Code
+	 */
+	public static function handle_editor_view_js() {
+		add_filter( 'mce_external_plugins', array( __CLASS__, 'mce_external_plugins' ) );
+		add_filter( 'mce_buttons', array( __CLASS__, 'mce_buttons' ) );
+		wp_enqueue_script(
+			'wordads-shortcode-editor-view',
+			Jetpack::get_file_url_for_environment(
+				'_inc/build/wordads/js/wordads-shortcode.min.js',
+				'modules/wordads/js/wordads-shortcode.js'
+			),
+			array( 'wp-util', 'jquery', 'quicktags' ),
+			false,
+			true
+		);
 	}
 	/**
 	 * Enqueue scripts and styles
@@ -112,12 +140,30 @@ class Jetpack_WordAds_Shortcode {
 
 		$html = '<div class="jetpack-wordad" itemscope itemtype="https://schema.org/WPAdBlock">';
 
-		$html .= 'Here\'s your ad sir</div>';
+		$html .= '</div>';
 
 		$html = $wordads->insert_ad( $html );
 
 		return $html;
 	}
+
+	public static function admin_head() {
+		remove_action( 'media_buttons', 'wordads_media_button', 9999 );
+		add_action( 'media_buttons', array( __CLASS__, 'wordads_media_button' ), 9999 );
+	}
+
+	public static function wordads_media_button() {
+		$title = __( 'Insert Ad', 'jetpack' );
+		?>
+
+		<button type="button" id="insert-jetpack-wordads-inline-ad" class="button" title="<?php echo esc_attr( $title ); ?>" href="javascript:;">
+			<span class="jetpack-wordads-inline-ad-icon jetpack-contact-form-icon"></span>
+			<?php echo esc_html( $title ); ?>
+		</button>
+
+		<?php
+	}
 }
 
 new Jetpack_WordAds_Shortcode();
+add_action( 'current_screen', array( 'Jetpack_WordAds_Shortcode', 'add_hooks' ) );
