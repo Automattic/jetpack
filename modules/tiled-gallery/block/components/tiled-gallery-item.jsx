8b8e3475c4e9123f4ce4ba120708aaa922e7a9f6
haszari <haszari@cartoonbeats.com>
2018-04-19 19:18:00 +1200

    set width & height attributes and render them in front-end markup (so we can use them for smart tiling layouts implemented in js)

--- modules/tiled-gallery/block/components/tiled-gallery-item.jsx
+++ modules/tiled-gallery/block/components/tiled-gallery-item.jsx
@@ -4,6 +4,7 @@
  * WordPress dependencies (npm)
  */
 const { withSelect } = wp.data;
+const { Component } = wp.element;
 
 /**
  * External Dependencies
@@ -11,23 +12,36 @@ const { withSelect } = wp.data;
 import React from 'react';
 import get from 'lodash/get';
 
-function TiledGalleryImage( props ) {
-	const { url, alt, id, link } = props;
-	const { width, height } = get( props.image, [ 'media_details' ], { width: -1, height: -1 } );
+class TiledGalleryImage extends Component {
 
-	return (
-    <figure>
-			<span>{ width } x { height }</span>
-			<meta itemprop="width" content={ width } />
-			<meta itemprop="height" content={ height } />
-			<img
-				src={ url } alt={ alt } data-id={ id } data-link={ link }
-				width={ width }
-				height={ height }
-				data-original-width={ width }
-				data-original-height={ height } />
-    </figure>
-	);
+	componentWillReceiveProps( { image, width, height } ) {
+		// very carefully set width & height attributes once only!
+		if ( image && ! width && ! height ) {
+			const mediaInfo = get( image, [ 'media_details' ], { width: null, height: null } );
+			this.props.setAttributes( {
+				width: mediaInfo.width,
+				height: mediaInfo.height
+			} );
+		}
+	}
+
+	render() {
+		const { url, alt, id, link, width, height } = this.props;
+
+		return (
+			<figure>
+				<span>{ width } x { height }</span>
+				<meta itemprop="width" content={ width } />
+				<meta itemprop="height" content={ height } />
+				<img
+					src={ url } alt={ alt } data-id={ id } data-link={ link }
+					width={ width }
+					height={ height }
+					data-original-width={ width }
+					data-original-height={ height } />
+			</figure>
+		);
+	}
 }
 
 function TiledGalleryItem( props ) {
@@ -36,10 +50,15 @@ function TiledGalleryItem( props ) {
 	const classes = [ 'tiled-gallery-item steve' ];
 	classes.push( 'tiled-gallery-item-small' );
 
-	const img = TiledGalleryImage( props );
+	const img = ( <TiledGalleryImage { ...props } /> );
 
 	return (
-		<li key={ props.id || props.url } className={ classes.join( ' ' ) } itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">
+		<li
+			key={ props.id || props.url }
+			className={ classes.join( ' ' ) }
+			itemprop="associatedMedia"
+			itemscope=""
+			itemtype="http://schema.org/ImageObject">
 			{ href ? <a href={ href }>{ img }</a> : img }
 		</li>
 	);
