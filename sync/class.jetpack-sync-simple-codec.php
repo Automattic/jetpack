cb5460664ec507c8bd96334e17cd9f98e918251b
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-04-19 15:59:01 -0700

    Revert "Simplify the cide file"
    This reverts commit 0d749e99721fe4f8b684b35cb81901fd6e7860e4.

--- sync/class.jetpack-sync-simple-codec.php
+++ sync/class.jetpack-sync-simple-codec.php
@@ -9,6 +9,10 @@ require_once dirname( __FILE__ ) . '/class.jetpack-sync-json-deflate-array-codec
 class Jetpack_Sync_Simple_Codec extends Jetpack_Sync_JSON_Deflate_Array_Codec {
 	const CODEC_NAME = "simple";
 
+	public function name() {
+		return self::CODEC_NAME;
+	}
+
 	public function encode( $object ) {
 		return base64_encode( $this->json_serialize( $object ) );
 	}
