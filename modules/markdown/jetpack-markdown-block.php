3e3a2eae4c49f5c6612dd371f368bd729c8b820c
Rene Cabral <rene@oki.io>
2018-04-19 07:52:43 -0400

    Prototype block structure and icon working
    Initial file seed.

---
+++ modules/markdown/jetpack-markdown-block.php
@@ -0,0 +1,41 @@
+<?php
+/*
+
+Description: Jetpack markdown block for Gutenberg
+Version: 0.1
+Author: Rene Cabral
+*/
+
+add_action( 'init', array( 'Jetpack_Markdown_Block', 'register_block_types' ) );
+add_action( 'enqueue_block_editor_assets', array( 'Jetpack_Markdown_Block', 'enqueue_block_editor_assets' ) );
+
+
+
+
+
+class Jetpack_Markdown_Block {
+
+	public static function register_block_types() {
+		register_block_type( 'jetpack/markdown', array(
+			'render_callback' => array( __CLASS__, 'render_markdown_block' ),
+		) );
+	}
+
+	public static function render_markdown_block( $args ) {
+		//return '<pre>' . print_r( $args, true ) . '</pre>';
+	}
+
+
+
+	public static function enqueue_block_editor_assets() {
+		wp_register_script(
+			'jetpack-markdown-block',
+			plugins_url( 'assets/js/jetpack-markdown-block.js', __FILE__ ),
+			array( 'wp-blocks', 'wp-element' )
+		);
+		wp_enqueue_script( 'jetpack-markdown-block' );
+
+	}
+
+}
+
