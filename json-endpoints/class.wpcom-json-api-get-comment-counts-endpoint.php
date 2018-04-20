0767768455aba84e79064ef14c8b002e9fbc7b32
Michael D Adams <michael.d.adams@gmail.com>
2018-04-20 16:32:00 -0700

    Comments: Fix counts endpoint's response format (#9385)
    The response is a "bare" object - no nesting.
    
    This also brings the code inline with the code running on WordPress.com.
    
    Example response:
    
    ```
    {
     "all": 0,
     "approved": 0,
     "pending": 0,
     "trash": 0,
     "spam": 0,
     "post_trashed": 4,
     "total_comments": 0
    }
    ```

--- json-endpoints/class.wpcom-json-api-get-comment-counts-endpoint.php
+++ json-endpoints/class.wpcom-json-api-get-comment-counts-endpoint.php
@@ -17,15 +17,13 @@ new WPCOM_JSON_API_GET_Comment_Counts_Endpoint( array(
 	'example_request' => 'https://public-api.wordpress.com/rest/v1/sites/en.blog.wordpress.com/comment-counts',
 
 	'response_format' => array(
-		'comment_counts' => array(
-			'all'            => '(int) Combined number of approved and unapproved comments',
-			'approved'       => '(int) Number of approved comments',
-			'pending'        => '(int) Number of unapproved comments',
-			'trash'          => '(int) Number of trash comments',
-			'spam'           => '(int) Number of spam comments',
-			'post_trashed'   => '(int) Number of comments whose parent post has been trashed',
-			'total_comments' => '(int) Combined number of comments in each category',
-		)
+		'all'            => '(int) Combined number of approved and unapproved comments',
+		'approved'       => '(int) Number of approved comments',
+		'pending'        => '(int) Number of unapproved comments',
+		'trash'          => '(int) Number of trash comments',
+		'spam'           => '(int) Number of spam comments',
+		'post_trashed'   => '(int) Number of comments whose parent post has been trashed',
+		'total_comments' => '(int) Combined number of comments in each category',
 	)
 ) );
 
