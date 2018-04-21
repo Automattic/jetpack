38e81b31c512dd0cc07d5ccd71160b24f8e75046
Daniel Walmsley <goldsounds@gmail.com>
2018-04-20 19:40:10 -0700

    Make wp-cli optional

--- bin/partner-provision.sh
+++ bin/partner-provision.sh
@@ -20,14 +20,17 @@ usage () {
 	[--partner-tracking-id=1]'
 }
 
-WP_CLI_PATH="wp"
-PROVISION_REQUEST_ARGS=""
-GLOBAL_ARGS="--skip-themes --skip-plugins=$($WP_CLI_PATH plugin list --field=name | grep -v ^jetpack$ | tr  '\n' ',')"
+# Note: this script should always be designed to keep wp-cli OPTIONAL
+# so that it can be run outside of the WordPress installation.
+WP_CLI_COMMAND="wp"
+WP_CLI_ARGS=""
 
 # Default API host that can be overridden.
 if [ -z "$JETPACK_START_API_HOST" ]; then
 	JETPACK_START_API_HOST='public-api.wordpress.com'
 fi
+
+PROVISION_REQUEST_ARGS=""
 PROVISION_REQUEST_URL="https://$JETPACK_START_API_HOST/rest/v1.3/jpphp/provision"
 
 for i in "$@"; do
@@ -42,7 +45,7 @@ for i in "$@"; do
 			;;
 		-i=* | --user_id=* | --user=* )
 			WP_USER="${i#*=}"
-			GLOBAL_ARGS="$GLOBAL_ARGS --user=${i#*=}"
+			WP_CLI_ARGS="$WP_CLI_ARGS --user=${i#*=}"
 			PROVISION_REQUEST_ARGS="$PROVISION_REQUEST_ARGS --form local_user=${i#*=}"
 			shift
 			;;
@@ -63,7 +66,7 @@ for i in "$@"; do
 			shift
 			;;
 		-u=* | --url=* )
-			GLOBAL_ARGS="$GLOBAL_ARGS --url=${i#*=}"
+			WP_CLI_ARGS="$WP_CLI_ARGS --url=${i#*=}"
 			SITEURL="${i#*=}"
 			shift
 			;;
@@ -80,11 +83,11 @@ for i in "$@"; do
 			shift
 			;;
 		--allow-root )
-			GLOBAL_ARGS="--allow-root"
+			WP_CLI_ARGS="$WP_CLI_ARGS --allow-root"
 			shift
 			;;
 		--wp-cli-path=* )
-			WP_CLI_PATH="${i#*=}"
+			WP_CLI_COMMAND="${i#*=}"
 			shift
 			;;
 		-h | --help )
@@ -96,6 +99,17 @@ for i in "$@"; do
 			exit 1
 	esac
 done
+
+# check WP CLI exists
+if ! WP_CLI_PATH="$(type -p "$WP_CLI_COMMAND")" || [[ -z $WP_CLI_PATH ]]; then
+	WP_CLI_EXISTS=0
+else
+	WP_CLI_EXISTS=1
+fi
+
+if [ "$WP_CLI_EXISTS" -eq "1" ]; then
+	WP_CLI_ARGS="$WP_CLI_ARGS --skip-themes --skip-plugins=$($WP_CLI_COMMAND plugin list --field=name | grep -v ^jetpack$ | tr  '\n' ',')"
+fi
 
 if [ "$CLIENT_ID" = "" ] || [ "$CLIENT_SECRET" = "" ] || [ "$WP_USER" = "" ]; then
 	echo $(usage) >&2
@@ -164,11 +178,13 @@ if [ -z "$ACCESS_TOKEN" ] || [ "$ACCESS_TOKEN" = "" ]; then
 fi
 
 # Silently ensure Jetpack is active
-# Intentionally not quoting $GLOBAL_ARGS so that words in the string are split
-$WP_CLI_PATH $GLOBAL_ARGS plugin activate jetpack >/dev/null 2>&1
+# Intentionally not quoting $WP_CLI_ARGS so that words in the string are split
+if [ "$WP_CLI_EXISTS" -eq "1" ]; then
+	$WP_CLI_COMMAND $WP_CLI_ARGS plugin activate jetpack >/dev/null 2>&1
+fi
 
-if [ -z "$SITEURL" ]; then
-	SITEURL=$( $WP_CLI_PATH $GLOBAL_ARGS option get siteurl | xargs echo )
+if [ -z "$SITEURL" ] && [ "$WP_CLI_EXISTS" -eq "1" ]; then
+	SITEURL=$( $WP_CLI_COMMAND $WP_CLI_ARGS option get siteurl | xargs echo )
 fi
 
 PROVISION_REQUEST_ARGS="$PROVISION_REQUEST_ARGS --form siteurl=$SITEURL"
@@ -197,8 +213,8 @@ fi
 ACCESS_TOKEN=$( jetpack_echo_key_from_json "$PROVISION_REQUEST" access_token | xargs echo )
 
 # If we have an access token, set it and activate default modules!
-if [ ! -z "$ACCESS_TOKEN" ] && [ "$ACCESS_TOKEN" != "" ] && [ ! -z "$WPCOM_USER_ID" ]; then
-	AUTHORIZE_RESULT=$( $WP_CLI_PATH $GLOBAL_ARGS jetpack authorize_user --token="$ACCESS_TOKEN" )
+if [ ! -z "$ACCESS_TOKEN" ] && [ "$ACCESS_TOKEN" != "" ] && [ ! -z "$WPCOM_USER_ID" ] && [ "$WP_CLI_EXISTS" -eq "1" ]; then
+	AUTHORIZE_RESULT=$( $WP_CLI_COMMAND $WP_CLI_ARGS jetpack authorize_user --token="$ACCESS_TOKEN" )
 	if jetpack_is_wp_cli_error "$AUTHORIZE_RESULT"; then
 		echo "{\"success\":false,\"error_code\":\"authorization_failure\",\"error_message\":\"$AUTHORIZE_RESULT\"}" >&2
 		exit 1
