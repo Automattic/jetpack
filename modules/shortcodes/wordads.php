86923fbfe11f0ebd1c289b29f17c42e55cba146d
Richard C Lations II <contact@rclations.com>
2018-04-22 00:01:20 -0400

    WordAds: Removing shortcode editor button from initial PR

--- modules/shortcodes/wordads.php
+++ modules/shortcodes/wordads.php
@@ -25,49 +25,6 @@ class Jetpack_WordAds_Shortcode {
 	}
 
 	/**
-	 * Add hooks according to screen.
-	 *
-	 * @param WP_Screen $screen Data about current screen.
-	 */
-	public static function add_hooks( $screen ) {
-		if ( isset( $screen->base ) && 'post' === $screen->base ) {
-			add_action( 'admin_notices', array( __CLASS__, 'handle_editor_view_js' ) );
-			add_action( 'admin_head', array( __CLASS__, 'admin_head' ) );
-		}
-	}
-
-	public static function mce_external_plugins( $plugin_array ) {
-		$plugin_array['wordads'] = Jetpack::get_file_url_for_environment(
-			'_inc/build/wordads/js/tinymce-plugin-wordads-button.min.js',
-			'modules/wordads/js/tinymce-plugin-wordads-button.js'
-		);
-		return $plugin_array;
-	}
-
-	/**
-	 * WordPress Shortcode Editor View JS Code
-	 */
-	public static function handle_editor_view_js() {
-		add_filter( 'mce_external_plugins', array( __CLASS__, 'mce_external_plugins' ) );
-		add_filter( 'mce_buttons', array( __CLASS__, 'mce_buttons' ) );
-		wp_enqueue_script(
-			'wordads-shortcode-editor-view',
-			Jetpack::get_file_url_for_environment(
-				'_inc/build/wordads/js/editor-view.min.js',
-				'modules/wordads/js/editor-view.js'
-			),
-			array( 'wp-util', 'jquery', 'quicktags' ),
-			false,
-			true
-		);
-		wp_localize_script( 'wordads-shortcode-editor-view', 'wordadsEditorView', array(
-			'labels' => array(
-				'tinymce_label' => __( 'Insert Ad', 'jetpack' ),
-			)
-		) );
-	}
-
-	/**
 	 * Our [wordad] shortcode.
 	 * Prints a WordAds Ad.
 	 *
@@ -108,24 +65,6 @@ class Jetpack_WordAds_Shortcode {
 
 		return $html;
 	}
-
-	public static function admin_head() {
-		remove_action( 'media_buttons', 'wordads_media_button', 9999 );
-		add_action( 'media_buttons', array( __CLASS__, 'wordads_media_button' ), 9999 );
-	}
-
-	public static function wordads_media_button() {
-		$title = __( 'Insert Ad', 'jetpack' );
-		?>
-
-		<button type="button" id="insert-jetpack-wordads-inline-ad" class="button" title="<?php echo esc_attr( $title ); ?>" href="javascript:;">
-			<span class="jetpack-wordads-inline-ad-icon jetpack-contact-form-icon"></span>
-			<?php echo esc_html( $title ); ?>
-		</button>
-
-		<?php
-	}
 }
 
 new Jetpack_WordAds_Shortcode();
-add_action( 'current_screen', array( 'Jetpack_WordAds_Shortcode', 'add_hooks' ) );
