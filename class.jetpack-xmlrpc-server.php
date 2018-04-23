c1be741513a59dc426d091a9d9c864e4c660c5ce
Eric Binnion <ericbinnion@gmail.com>
2018-04-23 15:34:31 -0500

    XML-RPC: JPS: Minor style fix; empty line above comment

--- class.jetpack-xmlrpc-server.php
+++ class.jetpack-xmlrpc-server.php
@@ -191,6 +191,7 @@ class Jetpack_XMLRPC_Server {
 
 		if ( ! Jetpack_Options::get_option( 'id' ) || ! Jetpack_Options::get_option( 'blog_token' ) || ! empty( $request['force'] ) ) {
 			wp_set_current_user( $user->ID );
+
 			// This code mostly copied from Jetpack::admin_page_load.
 			Jetpack::maybe_set_version_option();
 			$registered = Jetpack::try_registration();
