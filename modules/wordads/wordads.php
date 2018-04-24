5c6a7115376dfc78f466a85be22674e8ebea848b
Richard C Lations II <contact@rclations.com>
2018-04-24 16:43:18 -0400

    WordAds ads.txt: Catch and suppress WP_Errors in the ads.txt output

--- modules/wordads/wordads.php
+++ modules/wordads/wordads.php
@@ -94,7 +94,7 @@ class WordAds {
 		$this->insert_adcode();
 
 		if ( '/ads.txt' === $_SERVER['REQUEST_URI'] ) {
-			header( 'Content-Type: text/plain; charset=utf-8' );
+			$ads_txt_server_contents = ! is_wp_error( WordAds_API::get_wordads_ads_txt() ) ? WordAds_API::get_wordads_ads_txt() : '';
 
 			/**
 			 * Provide plugins a way of modifying the contents of the automatically-generated ads.txt file.
@@ -105,7 +105,9 @@ class WordAds {
 			 *
 			 * @param string WordAds_API::get_wordads_ads_txt() The contents of the ads.txt file.
 			 */
-			$ads_txt_content = apply_filters( 'wordads_ads_txt', WordAds_API::get_wordads_ads_txt() );
+			$ads_txt_content = apply_filters( 'wordads_ads_txt', $ads_txt_server_contents );
+
+			header( 'Content-Type: text/plain; charset=utf-8' );
 			echo esc_html( $ads_txt_content );
 			die();
 		}
