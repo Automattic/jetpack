9fa63c836891faf45ebb05350983a3ad3ad2b700
Derek Smart <smart@automattic.com>
2018-04-25 15:17:44 -0400

    Merge commit 'cdffaf0' into merge/responsive-videos (#9417)
    Conflicts:
    	modules/theme-tools/responsive-videos.php

--- modules/theme-tools/responsive-videos.php
+++ modules/theme-tools/responsive-videos.php
@@ -21,8 +21,8 @@ function jetpack_responsive_videos_init() {
 	/* Wrap videos in Buddypress */
 	add_filter( 'bp_embed_oembed_html', 'jetpack_responsive_videos_embed_html' );
 
-    /* Wrap Slideshare shortcodes */
-    add_filter( 'jetpack_slideshare_shortcode', 'jetpack_responsive_videos_embed_html' );
+	/* Wrap Slideshare shortcodes */
+	add_filter( 'jetpack_slideshare_shortcode', 'jetpack_responsive_videos_embed_html' );
 }
 add_action( 'after_setup_theme', 'jetpack_responsive_videos_init', 99 );
 
@@ -61,7 +61,7 @@ function jetpack_responsive_videos_embed_html( $html ) {
 }
 
 /**
- * Check if oEmbed is YouTube or Vimeo before wrapping..
+ * Check if oEmbed is YouTube or Vimeo before wrapping.
  *
  * @return string
  */
