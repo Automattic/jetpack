604addf0aba23f705816c63b792669f890f62e2f
jhnstn <jason.johnston@automattic.com>
2018-04-25 21:13:58 +0000

    remove omni search

--- modules/custom-post-types/nova.php
+++ modules/custom-post-types/nova.php
@@ -71,10 +71,6 @@ class Nova_Restaurant {
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_nova_styles'  ) );
 		add_action( 'admin_head',            array( $this, 'set_custom_font_icon' ) );
 
-		// Enable Omnisearch for Menu Items.
-		if ( class_exists( 'Jetpack_Omnisearch_Posts' ) )
-			new Jetpack_Omnisearch_Posts( self::MENU_ITEM_POST_TYPE );
-
 		// Always sort menu items correctly
 		add_action( 'parse_query',   array( $this, 'sort_menu_item_queries_by_menu_order'    ) );
 		add_filter( 'posts_results', array( $this, 'sort_menu_item_queries_by_menu_taxonomy' ), 10, 2 );
