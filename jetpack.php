313f0c926778d8dde9f54d0983ceddbc12093ec4
Igor Zinovyev <zinigor@gmail.com>
2018-05-07 21:30:02 +0300

    Adds a filter that switches minified assets on or off.
    This syncs changes introduced in r174775-wpcom that synchronised Sharing code. This is the first place where we use the new filter instead of directly checking for SCRIPT_DEBUG constant.

--- jetpack.php
+++ jetpack.php
@@ -42,6 +42,26 @@ function jetpack_require_lib_dir() {
 }
 add_filter( 'jetpack_require_lib_dir', 'jetpack_require_lib_dir' );
 
+/**
+ * Checks if the code debug mode turned on, and returns false if it is. When Jetpack is in
+ * code debug mode, it shouldn't use minified assets. Note that this filter is not being used
+ * in every place where assets are enqueued. The filter is added at priority 9 to be overridden
+ * by any default priority filter that runs after it.
+ *
+ * @since 6.2.0
+ *
+ * @return boolean
+ *
+ * @filter jetpack_should_use_minified_assets
+ */
+function jetpack_should_use_minified_assets() {
+	if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
+		return false;
+	}
+	return true;
+}
+add_filter( 'jetpack_should_use_minified_assets', 'jetpack_should_use_minified_assets', 9 );
+
 // @todo: Abstract out the admin functions, and only include them if is_admin()
 require_once( JETPACK__PLUGIN_DIR . 'class.jetpack.php'               );
 require_once( JETPACK__PLUGIN_DIR . 'class.jetpack-network.php'       );
