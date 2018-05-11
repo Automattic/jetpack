24741cf50397c9ee67aafe31cdc3275717bd6706
Michael Turk <gititon@users.noreply.github.com>
2018-05-11 15:14:06 -0400

    Fix/multiple themes (#9519)
    * Jetpack: Sync multiple theme install/update
    
    * Jetpack: Sync multiple theme install/update
    
    * Update to apply only to update
    
    * Testing muliple theme update
    
    * Removing blank line
    
    * Updating deprecated hook list
    
    * Updating deprecated hook list

--- sync/class.jetpack-sync-module-themes.php
+++ sync/class.jetpack-sync-module-themes.php
@@ -10,7 +10,7 @@ class Jetpack_Sync_Module_Themes extends Jetpack_Sync_Module {
 		add_action( 'jetpack_sync_current_theme_support', $callable );
 		add_action( 'upgrader_process_complete', array( $this, 'check_upgrader'), 10, 2 );
 		add_action( 'jetpack_installed_theme', $callable, 10, 2 );
-		add_action( 'jetpack_updated_theme', $callable, 10, 2 );
+		add_action( 'jetpack_updated_themes', $callable, 10, 2 );
 		add_action( 'delete_site_transient_update_themes', array( $this, 'detect_theme_deletion') );
 		add_action( 'jetpack_deleted_theme', $callable, 10, 2 );
 		add_filter( 'wp_redirect', array( $this, 'detect_theme_edit' ) );
@@ -286,17 +286,17 @@ class Jetpack_Sync_Module_Themes extends Jetpack_Sync_Module {
 			return;
 		}
 
-		$theme = $upgrader->theme_info();
-		if ( ! $theme instanceof WP_Theme ) {
-			return;
-		}
-		$theme_info = array(
-			'name' => $theme->get( 'Name' ),
-			'version' => $theme->get( 'Version' ),
-			'uri' => $theme->get( 'ThemeURI' ),
-		);
-
 		if ( 'install' === $details['action'] ) {
+			$theme = $upgrader->theme_info();
+			if ( ! $theme instanceof WP_Theme ) {
+				return;
+			}
+			$theme_info = array(
+				'name' => $theme->get( 'Name' ),
+				'version' => $theme->get( 'Version' ),
+				'uri' => $theme->get( 'ThemeURI' ),
+			);
+
 			/**
 			 * Signals to the sync listener that a theme was installed and a sync action
 			 * reflecting the installation and the theme info should be sent
@@ -310,17 +310,37 @@ class Jetpack_Sync_Module_Themes extends Jetpack_Sync_Module {
 		}
 
 		if ( 'update' === $details['action'] ) {
+			$themes = array();
+			foreach ( $details['themes'] as $theme_slug ) {
+				$theme = wp_get_theme( $theme_slug );
+
+				if ( ! $theme instanceof WP_Theme ) {
+					continue;
+				}
+
+				$themes[ $theme_slug ] = array(
+					'name' => $theme->get( 'Name' ),
+					'version' => $theme->get( 'Version' ),
+					'uri' => $theme->get( 'ThemeURI' ),
+					'stylesheet' => $theme->stylesheet,
+				);
+			}
+
+			if ( empty( $themes ) ) {
+				return;
+			}
+
 			/**
-			 * Signals to the sync listener that a theme was updated and a sync action
+			 * Signals to the sync listener that one or more themes was updated and a sync action
 			 * reflecting the update and the theme info should be sent
 			 *
-			 * @since 4.9.0
+			 * @since 6.2.0
 			 *
-			 * @param string $theme->theme_root Text domain of the theme
-			 * @param mixed $theme_info Array of abbreviated theme info
+			 * @param mixed $themes Array of abbreviated theme info
 			 */
-			do_action( 'jetpack_updated_theme', $theme->stylesheet, $theme_info );
+			do_action( 'jetpack_updated_themes', $themes );
 		}
+
 	}
 
 	public function init_full_sync_listeners( $callable ) {
