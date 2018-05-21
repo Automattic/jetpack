62661357d850c22d86d6907aa67f91be4c2340ab
Osk <oskosk@users.noreply.github.com>
2018-05-21 17:47:08 -0300

    Sharing: Refactor Jetpack::is_akismet_active() to also check for a valid key (#9585)

--- class.jetpack.php
+++ class.jetpack.php
@@ -7064,12 +7064,25 @@ p {
 
 	/**
 	 * Checks if Akismet is active and working.
+	 * 
+	 * We dropped support for Akismet 3.0 with Jetpack 6.1.1 while introducing a check for an Akismet valid key
+	 * that implied usage of methods present since more recent version.
+	 * See https://github.com/Automattic/jetpack/pull/9585
 	 *
 	 * @since  5.1.0
+	 * 
 	 * @return bool True = Akismet available. False = Aksimet not available.
 	 */
 	public static function is_akismet_active() {
-		if ( method_exists( 'Akismet' , 'http_post' ) || function_exists( 'akismet_http_post' ) ) {
+		if ( method_exists( 'Akismet' , 'http_post' ) ) {
+			$akismet_key = Akismet::get_api_key();
+			if ( ! $akismet_key ) {
+				return false;
+			}
+			$akismet_key_state = Akismet::verify_key( $akismet_key );
+			if ( 'invalid' === $akismet_key_state || 'failed' === $akismet_key_state ) {
+				return false;
+			}
 			return true;
 		}
 		return false;
