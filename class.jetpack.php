369d93ed7b4ca0f11dc794793b9189202bd49293
oskosk <oskosk@gmail.com>
2018-05-21 13:25:53 -0300

    remove check for akismet_http_post()

--- class.jetpack.php
+++ class.jetpack.php
@@ -7099,12 +7099,17 @@ p {
 
 	/**
 	 * Checks if Akismet is active and working.
+	 * 
+	 * We dropped support for Akismet 3.0 with Jetpack 6.1.1 while introducing a check for an Akismet valid key
+	 * that implied usage of methods present since more recent version.
+	 * See https://github.com/Automattic/jetpack/pull/9585
 	 *
 	 * @since  5.1.0
+	 * 
 	 * @return bool True = Akismet available. False = Aksimet not available.
 	 */
 	public static function is_akismet_active() {
-		if ( method_exists( 'Akismet' , 'http_post' ) || function_exists( 'akismet_http_post' ) ) {
+		if ( method_exists( 'Akismet' , 'http_post' ) ) {
 			$akismet_key = Akismet::verify_key( Akismet::get_api_key() );
 			if ( ! $akismet_key || 'invalid' === $akismet_key || 'failed' === $akismet_key ) {
 				return false;
