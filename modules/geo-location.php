8d0c2a92529955b39d63addab03c25874cc43598
Brad Griffith <bgriffith@deltasys.com>
2018-05-24 17:42:20 -0500

    Add geo-location shortcode.

--- modules/geo-location.php
+++ modules/geo-location.php
@@ -3,6 +3,22 @@
 require_once dirname( __FILE__ ) . '/geo-location/class.jetpack-geo-location.php';
 
 /**
+ * Geo-location shortcode for display of location data associated with a post.
+ *
+ * Usage with current global $post:
+ * [geo-location]
+ *
+ * Usage with specific post ID:
+ * [geo-location post=5]
+ */
+add_shortcode( 'geo-location', 'jetpack_geo_shortcode' );
+
+function jetpack_geo_shortcode( $attributes ) {
+	$attributes = shortcode_atts( array( 'post' => null, 'id' => null ), $attributes );
+	return jetpack_geo_get_location( $attributes['post'] ? $attributes['post'] : $attributes['id'] );
+}
+
+/**
  * Get the geo-location data associated with the supplied post ID, if it's available
  * and marked as being available for public display.  The returned array will contain
  * "latitude", "longitude" and "label" keys.
