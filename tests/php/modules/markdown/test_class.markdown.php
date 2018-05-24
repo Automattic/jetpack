4c3b5b223bbc4679229fb670e5ce0e777a560d4f
Rene Cabral <rene@oki.io>
2018-05-23 22:18:08 -0400

    changed gutenberg name to block editor in tests

--- tests/php/modules/markdown/test_class.markdown.php
+++ tests/php/modules/markdown/test_class.markdown.php
@@ -3,19 +3,19 @@ require_once( dirname( __FILE__ ) . '/../../../../modules/markdown.php');
 
 class WP_Test_Markdown extends WP_UnitTestCase {
 
-	private $gutenberg_post;
+	private $block_editor_post;
 
-	private $fixture_gutenberg_post_insert;
+	private $fixture_block_editor_post_insert;
 
-	private $fixture_gutenberg_post_rendered;
+	private $fixture_block_editor_post_rendered;
 
-	private function add_test_gutenberg_markdown_post() {
+	private function add_test_block_editor_markdown_post() {
 
 		$post_id = $this->factory->post->create(
 			array(
-				'post_content' => $this->fixture_gutenberg_post_insert,
-				'post_content_filtered' => $this->fixture_gutenberg_post_insert,
-				'post_title' => 'Test Gutenberg Post',
+				'post_content' => $this->fixture_block_editor_post_insert,
+				'post_content_filtered' => $this->fixture_block_editor_post_insert,
+				'post_title' => 'Test Block Editor Post',
 			)
 		);
 
@@ -31,11 +31,11 @@ class WP_Test_Markdown extends WP_UnitTestCase {
 
 		parent::setUp();
 
-		$this->fixture_gutenberg_post_insert = file_get_contents(
-			dirname( __FILE__ ) .'/fixtures/gutenberg-markdown-block-insert.txt'
+		$this->fixture_block_editor_post_insert = file_get_contents(
+			dirname( __FILE__ ) .'/fixtures/block-editor-markdown-block-insert.txt'
 		);
-		$this->fixture_gutenberg_post_rendered = file_get_contents(
-			dirname( __FILE__ ) .'/fixtures/gutenberg-markdown-block-rendered.txt'
+		$this->fixture_block_editor_post_rendered = file_get_contents(
+			dirname( __FILE__ ) .'/fixtures/block-editor-markdown-block-rendered.txt'
 		);
 
 		update_option( 'wpcom_publish_posts_with_markdown', true );
@@ -48,60 +48,60 @@ class WP_Test_Markdown extends WP_UnitTestCase {
 
 		wp_set_current_user( $author_id );
 
-		$post_id = $this->add_test_gutenberg_markdown_post();
-		$this->gutenberg_post = get_post( $post_id );
+		$post_id = $this->add_test_block_editor_markdown_post();
+		$this->block_editor_post = get_post( $post_id );
 		
 	}
 
 	/**
 	 * Test if _wpcom_is_markdown postmeta is set as true
-	 * after a Gutenberg insert containing Markdown blocks.
+	 * after a block_editor insert containing Markdown blocks.
 	 *
 	 * @since 4.6.0
 	 */
 	public function test_if_wpcom_is_markdown_meta_was_set_as_true() {
 
 		$this->assertEquals(
-			WPCom_Markdown::get_instance()->is_markdown($this->gutenberg_post->ID)
+			WPCom_Markdown::get_instance()->is_markdown($this->block_editor_post->ID)
 			, "1"
 		);
 	}
 
 	/**
-	 * Test if Markdown and other blocks from Gutenberg are saved intact
+	 * Test if Markdown and other blocks from block_editor are saved intact
 	 * in post_content_filtered after being processed by the Markdown module.
 	 *
 	 * @since 4.6.0
 	 */
-	public function test_if_gutenberg_markdown_post_content_filtered_is_saved_correctly() {
+	public function test_if_block_editor_markdown_post_content_filtered_is_saved_correctly() {
 
 		$this->assertEquals(
-			$this->fixture_gutenberg_post_insert,
-			$this->gutenberg_post->post_content_filtered
+			$this->fixture_block_editor_post_insert,
+			$this->block_editor_post->post_content_filtered
 		);
 	}
 
 	/**
 	 * Test if the Markdown block and other blocks are retrieved correctly when supplied
-	 * to Gutenberg for editing.
+	 * to block_editor for editing.
 	 *
 	 * @since 4.6.0
 	 */
-	public function test_if_gutenberg_markdown_post_is_retrieved_correctly_for_editing() {
+	public function test_if_block_editor_markdown_post_is_retrieved_correctly_for_editing() {
 
-		$post_type_object = get_post_type_object( $this->gutenberg_post->post_type );
+		$post_type_object = get_post_type_object( $this->block_editor_post->post_type );
 		$request = new WP_REST_Request(
 			'GET',
-			sprintf( '/wp/v2/%s/%d', $post_type_object->rest_base, $this->gutenberg_post->ID )
+			sprintf( '/wp/v2/%s/%d', $post_type_object->rest_base, $this->block_editor_post->ID )
 		);
 		$request->set_param( 'context', 'edit' );
 		$response = rest_do_request( $request );
 		$rest_post = rest_get_server()->response_to_data( $response, false );
 
-		$post_to_edit = apply_filters( 'after_gutenberg_gets_post_to_edit', $rest_post );
+		$post_to_edit = apply_filters( 'after_block_editor_gets_post_to_edit', $rest_post );
 
 		$this->assertEquals(
-			$this->fixture_gutenberg_post_insert,
+			$this->fixture_block_editor_post_insert,
 			$post_to_edit['content']['raw']
 		);
 
@@ -113,11 +113,11 @@ class WP_Test_Markdown extends WP_UnitTestCase {
 	 *
 	 * @since 4.6.0
 	 */
-	public function test_if_gutenberg_markdown_post_is_rendered_correctly_for_viewing() {
+	public function test_if_block_editor_markdown_post_is_rendered_correctly_for_viewing() {
 
 		$this->assertEquals(
-			$this->fixture_gutenberg_post_rendered,
-			$this->gutenberg_post->post_content
+			$this->fixture_block_editor_post_rendered,
+			$this->block_editor_post->post_content
 		);
 	}
 
