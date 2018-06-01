e8ef13f8580055f81786082fe768551e1d6f879a
James Nylen <jnylen@gmail.com>
2018-06-01 10:47:37 -0500

    REST API: Add tests for term feed URL (#9670)

--- tests/php/test_class.json-api-jetpack-endpoints.php
+++ tests/php/test_class.json-api-jetpack-endpoints.php
@@ -1,7 +1,5 @@
 <?php
-/**
- * @group external-http
- */
+
 class WP_Test_Jetpack_Json_Api_endpoints extends WP_UnitTestCase {
 
 	/**
@@ -16,14 +14,24 @@ class WP_Test_Jetpack_Json_Api_endpoints extends WP_UnitTestCase {
 	}
 
 	public function setUp() {
+		if ( ! defined( 'WPCOM_JSON_API__BASE' ) ) {
+			define( 'WPCOM_JSON_API__BASE', 'public-api.wordpress.com/rest/v1' );
+		}
 
 		parent::setUp();
+
+		if ( version_compare( PHP_VERSION, '5.3', '<' ) ) {
+			// Loading the API breaks in 5.2.x due to `const` declarations, and
+			// this will still happen even though all tests are skipped (why?)
+			return;
+		}
 
 		$this->set_globals();
 
 		// Force direct method. Running the upgrade via PHPUnit can't detect the correct filesystem method.
 		add_filter( 'filesystem_method', array( $this,  'filesystem_method_direct' ) );
 
+		require_once dirname( __FILE__ ) . '/../../modules/module-extras.php';
 		require_once dirname( __FILE__ ) . '/../../class.json-api.php';
 		require_once dirname( __FILE__ ) . '/../../class.json-api-endpoints.php';
 	}
@@ -31,10 +39,10 @@ class WP_Test_Jetpack_Json_Api_endpoints extends WP_UnitTestCase {
 	/**
 	 * @author lezama
 	 * @covers Jetpack_JSON_API_Plugins_Modify_Endpoint
+	 * @group external-http
 	 * @requires PHP 5.3.2
 	 */
 	public function test_Jetpack_JSON_API_Plugins_Modify_Endpoint() {
-
 		$endpoint = new Jetpack_JSON_API_Plugins_Modify_Endpoint( array(
 			'description'     => 'Update a Plugin on your Jetpack Site',
 			'group'           => 'plugins',
@@ -98,9 +106,90 @@ class WP_Test_Jetpack_Json_Api_endpoints extends WP_UnitTestCase {
 
 	}
 
+	public function create_get_category_endpoint() {
+		// From json-endpoints/class.wpcom-json-api-get-taxonomy-endpoint.php :(
+		return new WPCOM_JSON_API_Get_Taxonomy_Endpoint( array(
+			'description' => 'Get information about a single category.',
+			'group'       => 'taxonomy',
+			'stat'        => 'categories:1',
+
+			'method'      => 'GET',
+			'path'        => '/sites/%s/categories/slug:%s',
+			'path_labels' => array(
+				'$site'     => '(int|string) Site ID or domain',
+				'$category' => '(string) The category slug'
+			),
+
+			'example_request'  => 'https://public-api.wordpress.com/rest/v1/sites/en.blog.wordpress.com/categories/slug:community'
+		) );
+	}
+
+	/**
+	 * @author nylen
+	 * @covers WPCOM_JSON_API_Get_Taxonomy_Endpoint
+	 * @group json-api
+	 * @requires PHP 5.3
+	 */
+	public function test_get_term_feed_url_pretty_permalinks() {
+		global $blog_id;
+
+		$this->set_permalink_structure( '/%year%/%monthnum%/%postname%/' );
+		// Reset taxonomy URL structure after changing permalink structure
+		create_initial_taxonomies();
+
+		$category = wp_insert_term( 'test_category', 'category' );
+
+		$endpoint = $this->create_get_category_endpoint();
+		// Initialize some missing stuff for the API
+		WPCOM_JSON_API::init()->token_details = array( 'blog_id' => $blog_id );
+
+		$response = $endpoint->callback(
+			sprintf( '/sites/%d/categories/slug:test_category', $blog_id ),
+			$blog_id,
+			'test_category'
+		);
+
+		$this->assertStringEndsWith(
+			'/category/test_category/feed/',
+			$response->feed_url
+		);
+	}
+
+	/**
+	 * @author nylen
+	 * @covers WPCOM_JSON_API_Get_Taxonomy_Endpoint
+	 * @group json-api
+	 * @requires PHP 5.3
+	 */
+	public function test_get_term_feed_url_ugly_permalinks() {
+		global $blog_id;
+
+		$this->set_permalink_structure( '' );
+		// Reset taxonomy URL structure after changing permalink structure
+		create_initial_taxonomies();
+
+		$category = wp_insert_term( 'test_category', 'category' );
+
+		$endpoint = $this->create_get_category_endpoint();
+		// Initialize some missing stuff for the API
+		WPCOM_JSON_API::init()->token_details = array( 'blog_id' => $blog_id );
+
+		$response = $endpoint->callback(
+			sprintf( '/sites/%d/categories/slug:test_category', $blog_id ),
+			$blog_id,
+			'test_category'
+		);
+
+		$this->assertStringEndsWith(
+			'/?feed=rss2&amp;cat=' . $category['term_id'],
+			$response->feed_url
+		);
+	}
+
 	/**
 	 * @author tonykova
 	 * @covers Jetpack_API_Plugins_Install_Endpoint
+	 * @group external-http
 	 * @requires PHP 5.3.2
 	 */
 	public function test_Jetpack_API_Plugins_Install_Endpoint() {
