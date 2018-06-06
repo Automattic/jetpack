7f83b879370e7421721f5ec80970811450360465
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2018-06-06 15:01:21 -0300

    Widgets: fixes select image change for selective refresh

--- modules/widgets/simple-payments.php
+++ modules/widgets/simple-payments.php
@@ -214,13 +214,25 @@ if ( ! class_exists( 'Jetpack_Simple_Payments_Widget' ) ) {
 			if ( ! check_ajax_referer( 'customize-jetpack-simple-payments', 'customize-jetpack-simple-payments-nonce', false ) ) {
 				wp_send_json_error( 'bad_nonce', 400 );
 			}
+
 			if ( ! current_user_can( 'customize' ) ) {
 				wp_send_json_error( 'customize_not_allowed', 403 );
 			}
+
+			$post_type_object = get_post_type_object( Jetpack_Simple_Payments::$post_type_product );
+			if ( ! current_user_can( $post_type_object->cap->create_posts ) || ! current_user_can( $post_type_object->cap->publish_posts ) ) {
+				wp_send_json_error( 'insufficient_post_permissions', 403 );
+			}
+
 			if ( empty( $_POST['params'] ) || ! is_array( $_POST['params'] ) ) {
 				wp_send_json_error( 'missing_params', 400 );
 			}
+
 			$params = wp_unslash( $_POST['params'] );
+			$illegal_params = array_diff( array_keys( $params ), array( 'title', 'description', 'image_id', 'currency', 'price', 'multiple', 'email' ) );
+			if ( ! empty( $illegal_params ) ) {
+				wp_send_json_error( 'illegal_params', 400 );
+			}
 
 			$product_id = wp_insert_post( array(
 				'ID' => 0,
