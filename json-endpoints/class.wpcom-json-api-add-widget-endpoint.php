d8bef6f45da17946256f067e48df47f92b3f133b
Rocco Tripaldi <tripaldir@gmail.com>
2018-06-07 17:28:46 -0400

    Improve syncing on endpoint file

--- json-endpoints/class.wpcom-json-api-add-widget-endpoint.php
+++ json-endpoints/class.wpcom-json-api-add-widget-endpoint.php
@@ -1,57 +1,9 @@
 <?php
-
 /**
  * Activate a widget on a site.
  *
  * https://public-api.wordpress.com/rest/v1.1/sites/$site/widgets/new
  */
-
-new WPCOM_JSON_API_Add_Widgets_Endpoint( array (
-	'description'      => 'Activate a widget on a site.',
-	'group'            => 'sites',
-	'stat'             => 'widgets:new',
-	'method'           => 'POST',
-	'min_version'      => '1.1',
-	'path'             => '/sites/%s/widgets/new',
-	'path_labels'      => array(
-		'$site' => '(string) Site ID or domain.'
-	),
-	'request_format' => array(
-		'id_base' => '(string) The base ID of the widget.',
-		'sidebar' => '(string) Optional. The ID of the sidebar where this widget will be active. If empty, the widget will be added in the first sidebar available.',
-		'position' => '(int) Optional. The position of the widget in the sidebar.',
-		'settings' => '(object) Optional. The settings for the new widget.',
-	),
-	'response_format'  => array(
-		'id' => '(string) The actual ID of the widget.',
-		'sidebar' => '(string) The ID of the sidebar where this widget will be active.',
-		'position' => '(int) The final position of the widget in the sidebar.',
-		'settings' => '(array) The settings for the new widget.',
-	),
-	'example_request'  => 'https://public-api.wordpress.com/rest/v1.1/sites/12345678/widgets/new',
-	'example_request_data' => array(
-		'headers' => array(
-			'authorization' => 'Bearer YOUR_API_TOKEN'
-		),
-		'body' => array(
-			'id_base' => 'text',
-			'sidebar' => 'sidebar-2',
-			'position' => '0',
-			'settings' => array( 'title' => 'hello world' ),
-		)
-	),
-	'example_response' => '
-	{
-		"id": "text-3",
-		"id_base": "text",
-		"settings": {
-			"title": "hello world"
-		},
-		"sidebar": "sidebar-2",
-		"position": 0
-	}'
-) );
-
 
 
 new WPCOM_JSON_API_Add_Widgets_Endpoint( array (
@@ -146,13 +98,6 @@ new WPCOM_JSON_API_Add_Widgets_Endpoint( array (
 class WPCOM_JSON_API_Add_Widgets_Endpoint extends WPCOM_JSON_API_Endpoint {
 	/**
 	 * API callback.
-	 *
-	 * @param string $path
-	 * @param int    $blog_id
-	 * @uses jetpack_require_lib
-	 * @uses Jetpack_Widgets
-	 *
-	 * @return array|WP_Error
 	 */
 	function callback( $path = '', $blog_id = 0 ) {
 		// Switch to the given blog.
@@ -165,13 +110,13 @@ class WPCOM_JSON_API_Add_Widgets_Endpoint extends WPCOM_JSON_API_Endpoint {
 			return new WP_Error( 'unauthorized', 'User is not authorized to access widgets', 403 );
 		}
 
-		jetpack_require_lib( 'widgets' );
+		require_lib( 'widgets' );
 		$args = $this->input( false, false ); // Don't filter the input
 		if ( empty( $args ) || ! is_array( $args ) ) {
 			return new WP_Error( 'no_data', 'No data was provided.', 400 );
 		}
 		if ( isset( $args['widgets'] ) || ! empty( $args['widgets'] ) ) {
-			$widgets = Jetpack_Widgets::activate_widgets( $args['widgets'] );
+			$widgets = Widgets::activate_widgets( $args['widgets'] );
 			if ( is_wp_error( $widgets ) ) {
 				return $widgets;
 			}
@@ -182,12 +127,12 @@ class WPCOM_JSON_API_Add_Widgets_Endpoint extends WPCOM_JSON_API_Endpoint {
 		}
 
 		if ( empty( $args['sidebar'] ) ) {
-			$active_sidebars = Jetpack_Widgets::get_active_sidebars();
+			$active_sidebars = Widgets::get_active_sidebars();
 			reset( $active_sidebars );
 			$args['sidebar'] = key( $active_sidebars );
 		}
 
-		return Jetpack_Widgets::activate_widget( $args['id_base'], $args['sidebar'], $args['position'], $args['settings'] );
+		return Widgets::activate_widget( $args['id_base'], $args['sidebar'], $args['position'], $args['settings'] );
 	}
 
 }
