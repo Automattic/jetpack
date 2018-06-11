271e43bd7786a3b4a31e65e56cab82edfb9d4129
Fernando Espinosa <fer@ferdev.com>
2018-06-11 12:25:34 +0200

    Markdown block: renders markdown code as html
    First iteration of the Markdown block. When a user enters some Markdown
    code in a Markdown block, it is converted to HTML when saving the post.

--- modules/markdown/class-jetpack-markdown-block.php
+++ modules/markdown/class-jetpack-markdown-block.php
@@ -4,3 +4,25 @@ Description: Jetpack markdown block for Gutenberg
 Version: 0.1
 Author: Fernando Espinosa
 */
+
+add_action( 'init', array( 'Jetpack_Markdown_Block', 'register_block_types' ) );
+add_action( 'enqueue_block_editor_assets', array( 'Jetpack_Markdown_Block', 'enqueue_block_editor_assets' ) );
+
+class Jetpack_Markdown_Block {
+
+	public static function register_block_types() {
+		register_block_type(
+			'jetpack/markdown-block'
+		);
+	}
+
+	public static function enqueue_block_editor_assets() {
+		wp_register_script(
+			'jetpack-markdown-block',
+			plugins_url( '_inc/build/modules-markdown-block.js', JETPACK__PLUGIN_FILE ),
+			array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-components' )
+		);
+		wp_enqueue_script( 'jetpack-markdown-block' );
+	}
+
+}
