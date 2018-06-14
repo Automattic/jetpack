80b9f46749278d41bf381ee40cf47ccf12748329
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2018-06-14 14:39:12 +0300

    Fix linting error; fix travis

--- dangerfile.js
+++ dangerfile.js
@@ -1,9 +1,7 @@
-const {danger, warn} = require('danger')
+const { danger, warn } = require( 'danger' );
 
-  
 // No PR is too small to include a description of why you made a change
-if (danger.github.pr.body.length < 10) {
-  warn('Please include a description of your PR changes.');
+if ( danger.github.pr.body.length < 10 ) {
+	warn( 'Please include a description of your PR changes.' );
 }
 
-  
