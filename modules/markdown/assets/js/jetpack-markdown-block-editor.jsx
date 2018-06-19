16d77bf6d43e2f1b1a5d4337d96b6ed4bf6ad55c
Fernando Espinosa <fer@ferdev.com>
2018-06-19 13:17:03 +0200

    Markdown block: displays preview when not selected
    This commit removes the editor and preview panels, and instead switches
    the block to preview mode when the user deselects it.

--- modules/markdown/assets/js/jetpack-markdown-block-editor.jsx
+++ modules/markdown/assets/js/jetpack-markdown-block-editor.jsx
@@ -1,11 +1,6 @@
 /* global React:true */
 
 /**
- * External dependencies
- */
-import classNames from 'classnames';
-
-/**
  * Internal dependencies
  */
 import MarkdownPreview from './components/markdown-preview';
@@ -17,17 +12,6 @@ const {
 	Component,
 } = window.wp.element;
 
-const {
-	ButtonGroup
-} = window.wp.components;
-
-const {
-	BlockControls
-} = window.wp.editor;
-
-const PANEL_EDITOR = 'editor';
-const PANEL_PREVIEW = 'preview';
-
 class JetpackMarkdownBlockEditor extends Component {
 
 	constructor() {
@@ -37,10 +21,6 @@ class JetpackMarkdownBlockEditor extends Component {
 		this.showEditor = this.showEditor.bind( this );
 		this.showPreview = this.showPreview.bind( this );
 		this.isEmpty = this.isEmpty.bind( this );
-
-		this.state = {
-			activePanel: PANEL_EDITOR
-		};
 	}
 
 	isEmpty() {
@@ -67,54 +47,27 @@ class JetpackMarkdownBlockEditor extends Component {
 		const editorOrPreviewPanel = function() {
 			const source = attributes.source;
 
-			switch ( this.state.activePanel ) {
-				case PANEL_EDITOR:
-					const placeholderSource = __( 'Write your _Markdown_ **here**...' );
-
-					if ( ! isSelected && this.isEmpty() ) {
-						return <p className={ `${ className }-placeholder` }>
-							{ placeholderSource }
-						</p>;
-					}
-					return <MarkdownLivePreview
-						className={ `${ className }-live-preview` }
-						onChange={ this.updateSource }
-						aria-label={ __( 'Markdown' ) }
-						source={ source }
-						isSelected={ isSelected }
-					/>;
-
-				case PANEL_PREVIEW:
-					return <MarkdownPreview source={ source } />;
+			if ( ! isSelected && ! this.isEmpty() ) {
+				return <MarkdownPreview source={ source } />;
 			}
-		};
 
-		// Manages css classes for each panel based on component's state
-		const classesForPanel = function( panelName ) {
-			return classNames( {
-				'components-tab-button': true,
-				'is-active': this.state.activePanel === panelName
+			const placeholderSource = __( 'Write your _Markdown_ **here**...' );
 
-			} );
+			if ( ! isSelected && this.isEmpty() ) {
+				return <p className={ `${ className }-placeholder` }>
+					{ placeholderSource }
+				</p>;
+			}
+			return <MarkdownLivePreview
+				className={ `${ className }-live-preview` }
+				onChange={ this.updateSource }
+				aria-label={ __( 'Markdown' ) }
+				isSelected={ isSelected }
+				source={ source }
+			/>;
 		};
 
 		return [
-			<BlockControls >
-				<ButtonGroup>
-					<button
-						className={ classesForPanel.call( this, 'editor' ) }
-						onClick={ this.showEditor }
-					>
-						<span>{ __( 'Markdown' ) }</span>
-					</button>
-					<button
-						className={ classesForPanel.call( this, 'preview' ) }
-						onClick={ this.showPreview }
-					>
-						<span>{ __( 'Preview' ) }</span>
-					</button>
-				</ButtonGroup>
-			</BlockControls>,
 			editorOrPreviewPanel.call( this )
 		];
 	}
