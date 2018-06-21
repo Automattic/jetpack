8ab76d3935e9e2a66229e0ace874cff89c6f0887
Fernando Espinosa <fer@ferdev.com>
2018-06-21 12:03:07 +0200

    Markdown block: converts component to function
    There's no need to use a class for this component.

--- modules/markdown/assets/js/components/markdown-preview.jsx
+++ modules/markdown/assets/js/components/markdown-preview.jsx
@@ -6,23 +6,21 @@
 import MarkdownIt from 'markdown-it';
 
 const {
-	Component,
 	RawHTML
 } = window.wp.element;
 
 const markdownIt = new MarkdownIt();
 
-class MarkdownPreview extends Component {
+const MarkdownPreview = function( props ) {
+	const { source } = props;
 
-	render() {
-		const { source } = this.props;
-		let content = '';
+	let content = '';
 
-		if ( source ) {
-			// converts the markdown source to HTML
-			content = markdownIt.render( source );
-		}
-		return <RawHTML>{ content }</RawHTML>;
+	if ( source ) {
+		// converts the markdown source to HTML
+		content = markdownIt.render( source );
 	}
-}
+	return <RawHTML>{ content }</RawHTML>;
+};
+
 export default MarkdownPreview;
