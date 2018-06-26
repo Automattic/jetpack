a75efb15add5444e6fc9c2d88c03b64802d9ded4
Fernando Espinosa <fer@ferdev.com>
2018-06-26 19:59:24 +0200

    Markdown block: adds a toolbar to the editor
    Adds first buttons to the live editor toolbar.

--- modules/markdown/assets/js/jetpack-markdown-block-editor.jsx
+++ modules/markdown/assets/js/jetpack-markdown-block-editor.jsx
@@ -9,8 +9,29 @@ import MarkdownLivePreview from './components/markdown-live-preview';
 const { __ } = window.wp.i18n;
 
 const {
+	Toolbar
+} = window.wp.components;
+
+const {
+	BlockFormatControls
+} = window.wp.editor;
+
+const {
 	Component,
 } = window.wp.element;
+
+const FORMATTING_CONTROLS = [
+	{
+		icon: 'editor-bold',
+		title: __( 'Bold' ),
+		format: 'bold',
+	},
+	{
+		icon: 'editor-italic',
+		title: __( 'Italic' ),
+		format: 'italic',
+	},
+];
 
 class JetpackMarkdownBlockEditor extends Component {
 
@@ -59,13 +80,18 @@ class JetpackMarkdownBlockEditor extends Component {
 			);
 		}
 
-		return <MarkdownLivePreview
-			className={ `${ className }-live-preview` }
-			onChange={ this.updateSource }
-			aria-label={ __( 'Markdown' ) }
-			isSelected={ isSelected }
-			source={ source }
-		/>;
+		return [
+			<BlockFormatControls >
+				<Toolbar controls={ FORMATTING_CONTROLS } />
+			</BlockFormatControls>,
+			<MarkdownLivePreview
+				className={ `${ className }-live-preview` }
+				onChange={ this.updateSource }
+				aria-label={ __( 'Markdown' ) }
+				isSelected={ isSelected }
+				source={ source }
+			/>
+		];
 	}
 
 }
