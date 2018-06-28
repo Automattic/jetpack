1a5111bde9417429b6e7827d5886e79fd277ce4f
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2018-06-28 19:33:19 +0300

    Danger: Update message wording & add `danger skip` check (#9826)
    * Update wording & add `danger skip`
    
    * dummy commit [skip danger]
    
    * check only latest commit [skip danger]
    
    * Indent

--- dangerfile.js
+++ dangerfile.js
@@ -3,6 +3,17 @@
  */
 import { danger, warn } from 'danger';
 
+// Skip danger check if "no ci" or "no danger" in latest commit
+const lastCommit = danger.git.commits.slice( -1 )[ 0 ].message;
+if (
+	lastCommit.includes( 'no ci' ) ||
+	lastCommit.includes( 'skip ci' ) ||
+	lastCommit.includes( 'no danger' ) ||
+	lastCommit.includes( 'skip danger' )
+) {
+	process.exit( 0 ); // eslint-disable-line no-process-exit
+}
+
 // No PR is too small to include a description of why you made a change
 if ( danger.github.pr.body.length < 10 ) {
 	warn( 'Please include a description of your PR changes.' );
@@ -16,5 +27,5 @@ if ( ! ghLabels.find( l => l.name.toLowerCase().includes( '[status]' ) ) ) {
 
 // Test instructions
 if ( ! danger.github.pr.body.includes( 'Testing instructions' ) ) {
-	warn( 'Test instructions are missing for this PR. Please add some' );
+	warn( '"Testing instructions" are missing for this PR. Please add some' );
 }
