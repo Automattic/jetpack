0b35a627e1326d98816ca24e114a6222263c74cf
Abidh Ahmed <7570415+abidhahmed@users.noreply.github.com>
2018-07-02 16:41:07 +0530

    Test with fixes from #9772

--- _inc/client/test/main.js
+++ _inc/client/test/main.js
@@ -1,9 +1,9 @@
 /**
  * External dependencies
  */
-// import React from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
-// import { expect } from 'chai';
+import { expect } from 'chai';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 
@@ -12,8 +12,8 @@ Enzyme.configure( { adapter: new Adapter() } );
 /**
  * Internal dependencies
  */
-// import Main from '../main';
-// import store from 'state/redux-store';
+import Main from '../main';
+import store from 'state/redux-store';
 
 describe( 'Main', () => {
 	it( 'should render the Main component', () => {
