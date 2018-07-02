614eb8f6ebca0c0bf9809b6c96fd282ac93ee2aa
Copons <copons@users.noreply.github.com>
2018-07-02 20:14:09 +0100

    Add inline validation

--- modules/widgets/simple-payments/widgets-page.js
+++ modules/widgets/simple-payments/widgets-page.js
@@ -61,6 +61,29 @@
 		$( 'button, input', $widget ).prop( 'disabled', false );
 	}
 
+	function isFormValid( $form, values ) {
+		$( '.invalid', $form ).removeClass( 'invalid' );
+		var isValid = true;
+
+		if ( ! values.title ) {
+			$( '.jetpack-simple-payments-form-product-title', $form ).addClass( 'invalid' );
+			isValid = false;
+		}
+
+		if ( ! values.price || isNaN( parseFloat( values.price ) ) || parseFloat( values.price ) <= 0 ) {
+			$( '.jetpack-simple-payments-form-product-price', $form ).addClass( 'invalid' );
+			isValid = false;
+		}
+
+		var isEmailValid = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i.test( values.email );
+		if ( ! values.email || ! isEmailValid ) {
+			$( '.jetpack-simple-payments-form-product-email', $form ).addClass( 'invalid' );
+			isValid = false;
+		}
+
+		return isValid;
+	}
+
 	$widgetsArea.on( 'click', '.jetpack-simple-payments-add-product', function( event ) {
 		event.preventDefault();
 
@@ -79,10 +102,14 @@
 		event.preventDefault();
 
 		var $widget = getWidgetContainer( $( this ) );
-		disableWidget( $widget );
-
 		var $form = getForm( $( this ) );
 		var values = getFormValues( $form );
+
+		if ( ! isFormValid( $form, values ) ) {
+			return;
+		}
+
+		disableWidget( $widget );
 
 		var request = wp.ajax.post( 'customize-jetpack-simple-payments-button-save', {
 			'customize-jetpack-simple-payments-nonce': nonce,
