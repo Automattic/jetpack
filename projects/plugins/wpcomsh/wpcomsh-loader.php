e712902f7f5324ebbb30f589ed092c1f982c9d10
Brandon Kraft <public@brandonkraft.com>
2018-07-06 12:15:25 -0500

    Only load wpcomsh on Pressable
    Since all Atomic sites currently will have `IS_PRESSABLE` set, use that to determine if this should load.

--- projects/plugins/wpcomsh/wpcomsh-loader.php
+++ projects/plugins/wpcomsh/wpcomsh-loader.php
@@ -3,4 +3,7 @@
 Plugin Name: WP.com Site Helper
 Description: WordPress.com provided functionality & tools
 */
-require_once( WPMU_PLUGIN_DIR . '/wpcomsh/wpcomsh.php' );
+
+if ( defined( 'IS_PRESSABLE' ) && IS_PRESSABLE ) {
+  require_once( WPMU_PLUGIN_DIR . '/wpcomsh/wpcomsh.php' );
+}
