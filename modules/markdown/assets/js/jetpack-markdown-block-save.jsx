955570e67fcc535a1a841d5b1c7690c20b6b4723
Fernando Espinosa <fer@ferdev.com>
2018-07-11 12:27:39 +0200

    Markdown block: added test for save component
    Tests if the save component renders the markdown renderer.

--- modules/markdown/assets/js/jetpack-markdown-block-save.jsx
+++ modules/markdown/assets/js/jetpack-markdown-block-save.jsx
@@ -8,8 +8,11 @@ import React from 'react';
  */
 import MarkdownPreview from './components/markdown-renderer';
 
-function JetpackMarkdownBlockSave( { attributes } ) {
-	return <MarkdownPreview source={ attributes.source } />;
+function JetpackMarkdownBlockSave( { attributes, className } ) {
+	return <MarkdownPreview
+		className={ `${ className }-renderer` }
+		source={ attributes.source }
+	/>;
 }
 
 export default JetpackMarkdownBlockSave;
