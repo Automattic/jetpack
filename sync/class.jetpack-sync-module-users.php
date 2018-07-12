cfbddbf70c748aadf5d485e089f356c4497cd441
Michael Turk <turk.michael@gmail.com>
2018-07-12 10:28:11 -0400

    Update

--- sync/class.jetpack-sync-module-users.php
+++ sync/class.jetpack-sync-module-users.php
@@ -248,13 +248,15 @@ class Jetpack_Sync_Module_Users extends Jetpack_Sync_Module {
 			$old_user = $old_user_data;
 		}
 		error_log("Old user is: " . print_r($old_user, true));
-		
+
 		if ( $old_user !== null && $user->user_pass !== $old_user->user_pass ) {
 			$this->flags[ $user_id ]['password_changed'] = true;
 		}
 		if ( $old_user !== null && $user->user_email !== $old_user->user_email ) {
 			$this->flags[ $user_id ]['email_changed'] = true;
 		}
+
+		error_log(print_r( $this->flags[ $user_id ], true) );
 
 		$flags = $this->get_flags( $user_id );
 		error_log('flags are ' . print_r( $flags , true));
