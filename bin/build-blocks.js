174c5c96b224d68b56af497e1e199210018e0f8b
Mikael Korpela <mikael@ihminen.org>
2018-07-24 11:43:04 +0300

    Add a script to build blocks
    Requires linking Calypso to Jetpack manually and https://github.com/Automattic/wp-calypso/pull/26273

---
+++ bin/build-blocks.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+/* eslint no-console: 0 */
+
+const fs = require( 'fs' );
+const path = require( 'path' );
+const execSync = require( 'child_process' ).execSync;
+
+const blocks = [
+	'hello-dolly/hello-block.js',
+	// 'jetpack/tiled-gallery/tiled-gallery.jsx',
+];
+
+const outputDir = path.resolve( '_inc/build/blocks' );
+
+if ( ! fs.existsSync( outputDir ) ) {
+	fs.mkdirSync( outputDir );
+}
+
+console.log( 'Building Gutenberg blocks...' );
+
+blocks.forEach( block => {
+	console.log( `Building ${ block }` );
+	// Relative to wp-calypso
+	const inputFile = path.join( 'client/gutenberg/extensions/', block );
+	execSync( `npx wp-calypso-sdk ${ inputFile } ${ outputDir }`, {
+		shell: true,
+		stdio: 'inherit',
+	} );
+} );
