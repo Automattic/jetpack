f65872b37c208c3a16b350fb3c45bd5f73dcee16
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-07-24 12:05:06 -0700

    Combine some duplicate code

--- modules/publicize/publicize-jetpack.php
+++ modules/publicize/publicize-jetpack.php
@@ -631,8 +631,6 @@ class Publicize extends Publicize_Base {
 		// Nonce check
 		check_admin_referer( 'save_fb_token_' . $_REQUEST['connection'] );
 
-		$id = $_POST['connection'];
-
 		// Check for a numeric page ID
 		$page_id = $_POST['selected_id'];
 		if ( ! ctype_digit( $page_id ) ) {
@@ -649,16 +647,7 @@ class Publicize extends Publicize_Base {
 			'facebook_profile' => null
 		);
 
-
-		Jetpack::load_xml_rpc_client();
-		$xml = new Jetpack_IXR_Client();
-		$xml->query( 'jetpack.setPublicizeOptions', $id, $options );
-
-		if ( ! $xml->isError() ) {
-			$response = $xml->getResponse();
-			Jetpack_Options::update_option( 'publicize_connections', $response );
-			$this->globalization();
-		}
+		$this->set_remote_publicize_options( $_POST['connection'], $options );
 	}
 
 	function options_page_tumblr() {
@@ -742,11 +731,13 @@ class Publicize extends Publicize_Base {
 	function options_save_tumblr() {
 		// Nonce check
 		check_admin_referer( 'save_tumblr_blog_' . $_REQUEST['connection'] );
-
-		$id = $_POST['connection'];
-
 		$options = array( 'tumblr_base_hostname' => $_POST['selected_id'] );
 
+		$this->set_remote_publicize_options( $_POST['connection'], $options );
+
+	}
+
+	function set_remote_publicize_options( $id, $options ) {
 		Jetpack::load_xml_rpc_client();
 		$xml = new Jetpack_IXR_Client();
 		$xml->query( 'jetpack.setPublicizeOptions', $id, $options );
@@ -754,9 +745,8 @@ class Publicize extends Publicize_Base {
 		if ( ! $xml->isError() ) {
 			$response = $xml->getResponse();
 			Jetpack_Options::update_option( 'publicize_connections', $response );
+			$this->globalization();
 		}
-
-		$this->globalization();
 	}
 
 	function options_page_twitter() {
