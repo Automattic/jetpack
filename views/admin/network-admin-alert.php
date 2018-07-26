e3b28bf1d5249480ec1216e780193615f64ec93f
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-07-26 12:19:27 -0700

    Updated the network settings.

--- views/admin/network-admin-alert.php
+++
@@ -1,9 +0,0 @@
-<div id="message" class="updated jetpack-message jp-connect" style="display:block !important;">
-		<div class="jetpack-wrap-container">
-			<div class="jetpack-text-container">
-				<h2>
-					<?php echo $data['notice']; ?>
-				</h2>
-			</div>
-		</div>
-</div>
