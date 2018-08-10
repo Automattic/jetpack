1e22578faa89a07e80eccb334cf333e5b493618b
Mikael Korpela <mikael@ihminen.org>
2018-08-10 15:52:37 +0300

    Add `wp-compose|plugins` dependency as required by Publicize

--- class.jetpack.php
+++ class.jetpack.php
@@ -7304,10 +7304,12 @@ p {
 			array(
 				'wp-blocks',
 				'wp-components',
+				'wp-compose',
 				'wp-data',
 				'wp-editor',
 				'wp-element',
 				'wp-i18n',
+				'wp-plugins',
 			),
 			$version
 		);
