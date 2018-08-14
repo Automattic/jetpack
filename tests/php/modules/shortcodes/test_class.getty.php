9c28c3ae18375d8cf3543521c609245537f17452
Michael D Adams <michael.d.adams@gmail.com>
2018-08-14 14:56:53 -0700

    Tests: Shortcodes: Getty - Bypass HTTP Request
    The Getty shortcode makes a request to Getty's oEmbed endpoint to
    determine what HTML should be output for the given shortcode.
    
    These tests fail intermittently in CI: presumably because we get throttled
    by Getty/Travis for making too many requsets.
    
    Bypasse the HTTP requests by "manually" returning early from
    `WP_oEmbed::get_html()` via the `pre_oembed_result` filter.
    
    Add a single HTTP-based test to the `external-http` group (which is not
    run by default).

--- tests/php/modules/shortcodes/test_class.getty.php
+++ tests/php/modules/shortcodes/test_class.getty.php
@@ -38,6 +38,25 @@ class WP_Test_Jetpack_Shortcodes_Getty extends WP_UnitTestCase {
 		return preg_replace( '/((id=\'[:alpha:\-]+)|[\?&]|&amp;|&#038;)(et=[\w-]+|sig=[\w-=]+)/', '', $str );
 	}
 
+	function setUp() {
+		/*
+		 * We normally make an HTTP request to Getty's oEmbed endpoint to generate
+		 * the shortcode output.
+		 * This filter bypasses that HTTP request for these tests
+		 */
+		add_filter( 'pre_oembed_result', array( $this, 'getty_oembed_response' ), 10, 3 );
+	}
+
+	function getty_oembed_response( $html, $url, $args ) {
+		if ( 0 !== strpos( $url, 'https://gty.im/' ) ) {
+			return $html;
+		}
+
+		// Only works for https://gty.im/82278805
+		// If more inputs are needed, add more outputs.
+		return self::GETTY_EMBED_2017;
+	}
+
 	/**
 	 * Verify that [getty] exists.
 	 *
@@ -123,6 +142,25 @@ class WP_Test_Jetpack_Shortcodes_Getty extends WP_UnitTestCase {
 	 * @since 4.5.0
 	 */
 	public function test_shortcodes_getty_image() {
+		$image_id = '82278805';
+		$content = "[getty src='$image_id']";
+
+		$shortcode_content = do_shortcode( $content );
+
+		$this->assertContains( $image_id, $shortcode_content );
+	}
+
+	/**
+	 * Uses a real HTTP request to Getty's oEmbed endpoint to
+	 * verify that rendering the shortcode returns a Getty image.
+	 *
+	 * @group external-http
+	 *
+	 * @since 4.5.0
+	 */
+	public function test_shortcodes_getty_image_via_oembed_http_request() {
+		remove_filter( 'pre_oembed_result', array( $this, 'getty_oembed_response' ), 10, 3 );
+
 		$image_id = '82278805';
 		$content = "[getty src='$image_id']";
 
