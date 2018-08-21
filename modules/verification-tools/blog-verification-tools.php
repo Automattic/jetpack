0cc07fe12ec87bf2800d57f6873025b0ef1a2915
Daniel Walmsley <goldsounds@gmail.com>
2018-08-21 16:17:37 -0700

    Fix broken test

--- modules/verification-tools/blog-verification-tools.php
+++ modules/verification-tools/blog-verification-tools.php
@@ -3,7 +3,7 @@
 // Edit here to add new services
 function jetpack_verification_services() {
 	return array(
-			'google' => array(
+		'google' => array(
 			'name'   =>'Google Search Console',
 			'key'    =>'google-site-verification',
 			'format' =>'dBw5CvburAxi537Rp9qi5uG2174Vb6JwHwIRwPSLIK8',
--- tests/php/modules/verification-tools/test_functions.verification-tools-utils.php
+++ tests/php/modules/verification-tools/test_functions.verification-tools-utils.php
@@ -10,19 +10,19 @@ class WP_Test_Jetpack_Verification_Tools_Utils extends WP_UnitTestCase {
 	 */
 	public function test_jetpack_verification_validate_processes_and_returns_codes() {
 		$codes = array(
-			'         untrimmed code      ',
-			'some code that is going to be longer than 100 chars in order to test the trimming'
+			'google' => '         untrimmed code      ',
+			'bing' => 'some code that is going to be longer than 100 chars in order to test the trimming'
 			. ' some code that isgoing to be longer than 100 chars in order to test the trimming'
 			. ' some code that isgoing to be longer than 100 chars in order to test the trimming',
-			'some regular string with nothing special in it'
+			'yandex' => 'some regular string with nothing special in it'
 		);
 
 		$processed_codes = jetpack_verification_validate( $codes );
 
-		foreach( array_merge( $codes, $processed_codes ) as $code ) {
+		foreach( [ 'google', 'bing', 'yandex' ] as $key ) {
 			$this->assertEquals(
-				substr( esc_attr( trim( $code ) ), 0, 100 ),
-				$code,
+				substr( esc_attr( trim( $codes[ $key ] ) ), 0, 100 ),
+				$processed_codes[ $key ],
 				'the code should be processed'
 			);
 		}
