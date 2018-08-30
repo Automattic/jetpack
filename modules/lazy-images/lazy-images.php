Automattic/jetpack

commit f48c48b6f73d4be4100231d59f8c221e75d80393

Lazy Images: First pass at using srcset instead of src (#10054)

modules/lazy-images/lazy-images.php
@@ -162,7 +162,7 @@ class Jetpack_Lazy_Images {
 	 * @return string The image with updated lazy attributes
 	 */
 	static function process_image( $matches ) {
-		$old_attributes_str = $matches[2];
+		$old_attributes_str       = $matches[2];
 		$old_attributes_kses_hair = wp_kses_hair( $old_attributes_str, wp_allowed_protocols() );
 
 		if ( empty( $old_attributes_kses_hair['src'] ) ) {
@@ -173,12 +173,9 @@ class Jetpack_Lazy_Images {
 		$new_attributes = self::process_image_attributes( $old_attributes );
 
 		// If we didn't add lazy attributes, just return the original image source.
-		if ( empty( $new_attributes['data-lazy-src'] ) ) {
+		if ( false === strpos( $new_attributes['class'], 'jetpack-lazy-image' ) ) {
 			return $matches[0];
 		}
-
-		// Ensure we add the jetpack-lazy-image class to this image.
-		$new_attributes['class'] = sprintf( '%s jetpack-lazy-image', empty( $new_attributes['class'] ) ? '' : $new_attributes['class'] );
 
 		$new_attributes_str = self::build_attributes_string( $new_attributes );
 
@@ -237,21 +234,21 @@ class Jetpack_Lazy_Images {
 
 		$old_attributes = $attributes;
 
-		// Set placeholder and lazy-src
-		$attributes['src'] = self::get_placeholder_image();
-		$attributes['data-lazy-src'] = $old_attributes['src'];
-
-		// Handle `srcset`
-		if ( ! empty( $attributes['srcset'] ) ) {
-			$attributes['data-lazy-srcset'] = $old_attributes['srcset'];
-			unset( $attributes['srcset'] );
+		// Set placeholder and lazy-src.
+		foreach ( array( 'srcset', 'sizes' ) as $attribute ) {
+			if ( isset( $old_attributes[ $attribute ] ) ) {
+				$attributes[ "data-lazy-$attribute" ] = $old_attributes[ $attribute ];
+				unset( $attributes[ $attribute ] );
+			}
 		}
 
-		// Handle `sizes`
-		if ( ! empty( $attributes['sizes'] ) ) {
-			$attributes['data-lazy-sizes'] = $old_attributes['sizes'];
-			unset( $attributes['sizes'] );
-		}
+		$attributes['srcset'] = self::get_placeholder_image();
+		$attributes['class']  = sprintf(
+			'%s jetpack-lazy-image',
+			empty( $old_attributes['class'] )
+				? ''
+				: $old_attributes['class']
+		);
 
 		/**
 		 * Allow plugins and themes to override the attributes on the image before the content is updated.

modules/lazy-images/js/lazy-images.js
@@ -23,7 +23,7 @@ var jetpackLazyImagesModule = function( $ ) {
 	} );
 
 	function lazy_load_init() {
-		images = document.querySelectorAll( 'img[data-lazy-src]' );
+		images = document.querySelectorAll( 'img.jetpack-lazy-image:not(.jetpack-lazy-image--handled)' );
 		imageCount = images.length;
 
 		// If initialized, then disconnect the observer
@@ -97,7 +97,6 @@ var jetpackLazyImagesModule = function( $ ) {
 	 */
 	function applyImage( image ) {
 		var theImage = $( image ),
-			src,
 			srcset,
 			sizes,
 			theClone;
@@ -106,26 +105,20 @@ var jetpackLazyImagesModule = function( $ ) {
 			return;
 		}
 
-		src = theImage.attr( 'data-lazy-src' );
-		if ( ! src ) {
-			return;
-		}
-
 		srcset = theImage.attr( 'data-lazy-srcset' );
 		sizes = theImage.attr( 'data-lazy-sizes' );
 		theClone = theImage.clone();
 
 		// Remove lazy attributes from the clone.
-		theClone.removeAttr( 'data-lazy-src' ),
 		theClone.removeAttr( 'data-lazy-srcset' ),
 		theClone.removeAttr( 'data-lazy-sizes' );
 
 		// Add the attributes we want on the finished image.
 		theClone.addClass( 'jetpack-lazy-image--handled' );
 		theClone.attr( 'data-lazy-loaded', 1 );
-		theClone.attr( 'src', src );
-
-		if ( srcset ) {
+		if ( ! srcset ) {
+			theClone.removeAttr( 'srcset' );
+		} else {
 			theClone.attr( 'srcset', srcset );
 		}
 		if ( sizes ) {

tests/php/modules/lazy-images/post-image-placeholder-content.php
@@ -1,12 +1,12 @@
 <p>OK, so images can get quite complicated as we have a few variables to work with! For example the image below has had a caption entered in the WordPress image upload dialog box, this creates a [caption] shortcode which then in turn wraps the whole thing in a <code>div</code> with inline styling! Maybe one day they'll be able to use the <code>figure</code> and <code>figcaption</code> elements for all this. Additionally, images can be wrapped in links which, if you're using anything other than <code>color</code> or <code>text-decoration</code> to style your links can be problematic.</p>
-<div id="attachment_28" class="wp-caption alignnone" style="width: 510px"><a href="#"><img src="placeholder.jpg" alt="Your Alt Tag" title="bmxisbest" width="500" height="300" class="size-large wp-image-28 jetpack-lazy-image" data-lazy-src="http://jetpacksite.com/img/img_large.png"><noscript><img src="http://jetpacksite.com/img/img_large.png" alt="Your Alt Tag" title="bmxisbest" width="500" height="300" class="size-large wp-image-28"></noscript></a><p class="wp-caption-text">This is the optional caption.</p></div>
+<div id="attachment_28" class="wp-caption alignnone" style="width: 510px"><a href="#"><img src="http://jetpacksite.com/img/img_large.png" alt="Your Alt Tag" title="bmxisbest" width="500" height="300" class="size-large wp-image-28 jetpack-lazy-image" srcset="placeholder.jpg"><noscript><img src="http://jetpacksite.com/img/img_large.png" alt="Your Alt Tag" title="bmxisbest" width="500" height="300" class="size-large wp-image-28"></noscript></a><p class="wp-caption-text">This is the optional caption.</p></div>
 <p>The next issue we face is image alignment, users get the option of <em>None</em>, <em>Left</em>, <em>Right</em> &amp; <em>Center</em>. On top of this, they also get the options of <em>Thumbnail</em>, <em>Medium</em>, <em>Large</em> &amp; <em>Fullsize</em>. You'll probably want to add floats to style the image position so important to remember to clear these to stop images popping below the bottom of your articles.</p>
-<img src="placeholder.jpg" alt="Your Alt Title" title="Your Title" width="300" height="200" class="alignright size-medium wp-image-28 jetpack-lazy-image" data-lazy-src="http://jetpacksite.com/img/img_medium.png"><noscript><img src="http://jetpacksite.com/img/img_medium.png" alt="Your Alt Title" title="Your Title" width="300" height="200" class="alignright size-medium wp-image-28"></noscript>
-<img src="placeholder.jpg" alt="Your Alt Title" title="Your Title" width="150" height="150" class="alignleft size-thumbnail wp-image-28 jetpack-lazy-image" data-lazy-src="http://jetpacksite.com/img/img_thumb.png"><noscript><img src="http://jetpacksite.com/img/img_thumb.png" alt="Your Alt Title" title="Your Title" width="150" height="150" class="alignleft size-thumbnail wp-image-28"></noscript>
-<img class="aligncenter size-medium wp-image-28 jetpack-lazy-image" title="Your Title" src="placeholder.jpg" alt="Your Alt Title" width="300" height="200" data-lazy-src="http://jetpacksite.com/img/img_medium.png"><noscript><img class="aligncenter size-medium wp-image-28" title="Your Title" src="http://jetpacksite.com/img/img_medium.png" alt="Your Alt Title" width="300" height="200"></noscript>
-<img src="placeholder.jpg" alt="Your Alt Title" title="Your Title" width="840" height="300" class="alignnone size-full wp-image-28 jetpack-lazy-image" data-lazy-src="http://jetpacksite.com/img/img_full.png"><noscript><img src="http://jetpacksite.com/img/img_full.png" alt="Your Alt Title" title="Your Title" width="840" height="300" class="alignnone size-full wp-image-28"></noscript>
-<p>Additionally, to add further confusion, images can be wrapped inside paragraph content, lets test some examples here.<img src="placeholder.jpg" alt="Your Alt Title" title="Your Title" width="300" height="200" class="alignright size-medium wp-image-28 jetpack-lazy-image" data-lazy-src="http://jetpacksite.com/img/img_medium.png"><noscript><img src="http://jetpacksite.com/img/img_medium.png" alt="Your Alt Title" title="Your Title" width="300" height="200" class="alignright size-medium wp-image-28"></noscript>
-Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean lacinia bibendum nulla sed consectetur.<img src="placeholder.jpg" alt="Your Alt Title" title="Your Title" width="150" height="150" class="alignleft size-thumbnail wp-image-28 jetpack-lazy-image" data-lazy-src="http://jetpacksite.com/img/img_thumb.png"><noscript><img src="http://jetpacksite.com/img/img_thumb.png" alt="Your Alt Title" title="Your Title" width="150" height="150" class="alignleft size-thumbnail wp-image-28"></noscript>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean lacinia bibendum nulla sed consectetur.<img src="placeholder.jpg" alt="Your Alt Title" title="Your Title" width="150" height="150" class="aligncenter size-thumbnail wp-image-28 jetpack-lazy-image" data-lazy-src="http://jetpacksite.com/img/img_thumb.png"><noscript><img src="http://jetpacksite.com/img/img_thumb.png" alt="Your Alt Title" title="Your Title" width="150" height="150" class="aligncenter size-thumbnail wp-image-28"></noscript>Aenean lacinia bibendum nulla sed consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur.</p>
+<img src="http://jetpacksite.com/img/img_medium.png" alt="Your Alt Title" title="Your Title" width="300" height="200" class="alignright size-medium wp-image-28 jetpack-lazy-image" srcset="placeholder.jpg"><noscript><img src="http://jetpacksite.com/img/img_medium.png" alt="Your Alt Title" title="Your Title" width="300" height="200" class="alignright size-medium wp-image-28"></noscript>
+<img src="http://jetpacksite.com/img/img_thumb.png" alt="Your Alt Title" title="Your Title" width="150" height="150" class="alignleft size-thumbnail wp-image-28 jetpack-lazy-image" srcset="placeholder.jpg"><noscript><img src="http://jetpacksite.com/img/img_thumb.png" alt="Your Alt Title" title="Your Title" width="150" height="150" class="alignleft size-thumbnail wp-image-28"></noscript>
+<img class="aligncenter size-medium wp-image-28 jetpack-lazy-image" title="Your Title" src="http://jetpacksite.com/img/img_medium.png" alt="Your Alt Title" width="300" height="200" srcset="placeholder.jpg"><noscript><img class="aligncenter size-medium wp-image-28" title="Your Title" src="http://jetpacksite.com/img/img_medium.png" alt="Your Alt Title" width="300" height="200"></noscript>
+<img src="http://jetpacksite.com/img/img_full.png" alt="Your Alt Title" title="Your Title" width="840" height="300" class="alignnone size-full wp-image-28 jetpack-lazy-image" srcset="placeholder.jpg"><noscript><img src="http://jetpacksite.com/img/img_full.png" alt="Your Alt Title" title="Your Title" width="840" height="300" class="alignnone size-full wp-image-28"></noscript>
+<p>Additionally, to add further confusion, images can be wrapped inside paragraph content, lets test some examples here.<img src="http://jetpacksite.com/img/img_medium.png" alt="Your Alt Title" title="Your Title" width="300" height="200" class="alignright size-medium wp-image-28 jetpack-lazy-image" srcset="placeholder.jpg"><noscript><img src="http://jetpacksite.com/img/img_medium.png" alt="Your Alt Title" title="Your Title" width="300" height="200" class="alignright size-medium wp-image-28"></noscript>
+Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean lacinia bibendum nulla sed consectetur.<img src="http://jetpacksite.com/img/img_thumb.png" alt="Your Alt Title" title="Your Title" width="150" height="150" class="alignleft size-thumbnail wp-image-28 jetpack-lazy-image" srcset="placeholder.jpg"><noscript><img src="http://jetpacksite.com/img/img_thumb.png" alt="Your Alt Title" title="Your Title" width="150" height="150" class="alignleft size-thumbnail wp-image-28"></noscript>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean lacinia bibendum nulla sed consectetur.<img src="http://jetpacksite.com/img/img_thumb.png" alt="Your Alt Title" title="Your Title" width="150" height="150" class="aligncenter size-thumbnail wp-image-28 jetpack-lazy-image" srcset="placeholder.jpg"><noscript><img src="http://jetpacksite.com/img/img_thumb.png" alt="Your Alt Title" title="Your Title" width="150" height="150" class="aligncenter size-thumbnail wp-image-28"></noscript>Aenean lacinia bibendum nulla sed consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur.</p>
 <p>And then... Finally, users can insert a WordPress [gallery], which is kinda ugly and comes with some CSS stuck into the page to style it (which doesn't actually validate, nor does the markup for the gallery). The amount of columns in the gallery is also changable by the user, but the default is three so we'll work with that for our example with an added fouth image to test verticle spacing.</p>
 <style type="text/css">
 	#gallery-1 {
@@ -28,16 +28,16 @@ Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Maecenas 
 
 <div id="gallery-1" class="gallery galleryid-1 gallery-columns-3 gallery-size-thumbnail"><dl class="gallery-item">
 <dt class="gallery-icon">
-<a href="#" title="Your Title"><img width="150" height="150" src="placeholder.jpg" class="attachment-thumbnail jetpack-lazy-image" alt="Your Alt Title" title="Your Title" data-lazy-src="http://jetpacksite.com/img/img_thumb.png"><noscript><img width="150" height="150" src="http://jetpacksite.com/img/img_thumb.png" class="attachment-thumbnail" alt="Your Alt Title" title="Your Title"></noscript></a>
+<a href="#" title="Your Title"><img width="150" height="150" src="http://jetpacksite.com/img/img_thumb.png" class="attachment-thumbnail jetpack-lazy-image" alt="Your Alt Title" title="Your Title" srcset="placeholder.jpg"><noscript><img width="150" height="150" src="http://jetpacksite.com/img/img_thumb.png" class="attachment-thumbnail" alt="Your Alt Title" title="Your Title"></noscript></a>
 </dt></dl><dl class="gallery-item">
 <dt class="gallery-icon">
-<a href="#" title="Your Title"><img width="150" height="150" src="placeholder.jpg" class="attachment-thumbnail jetpack-lazy-image" alt="Your Alt Title" title="Your Title" data-lazy-src="http://jetpacksite.com/img/img_thumb.png"><noscript><img width="150" height="150" src="http://jetpacksite.com/img/img_thumb.png" class="attachment-thumbnail" alt="Your Alt Title" title="Your Title"></noscript></a>
+<a href="#" title="Your Title"><img width="150" height="150" src="http://jetpacksite.com/img/img_thumb.png" class="attachment-thumbnail jetpack-lazy-image" alt="Your Alt Title" title="Your Title" srcset="placeholder.jpg"><noscript><img width="150" height="150" src="http://jetpacksite.com/img/img_thumb.png" class="attachment-thumbnail" alt="Your Alt Title" title="Your Title"></noscript></a>
 </dt></dl><dl class="gallery-item">
 <dt class="gallery-icon">
-<a href="#" title="Your Title"><img width="150" height="150" src="placeholder.jpg" class="attachment-thumbnail jetpack-lazy-image" alt="Your Alt Title" title="Your Title" data-lazy-src="http://jetpacksite.com/img/img_thumb.png"><noscript><img width="150" height="150" src="http://jetpacksite.com/img/img_thumb.png" class="attachment-thumbnail" alt="Your Alt Title" title="Your Title"></noscript></a>
+<a href="#" title="Your Title"><img width="150" height="150" src="http://jetpacksite.com/img/img_thumb.png" class="attachment-thumbnail jetpack-lazy-image" alt="Your Alt Title" title="Your Title" srcset="placeholder.jpg"><noscript><img width="150" height="150" src="http://jetpacksite.com/img/img_thumb.png" class="attachment-thumbnail" alt="Your Alt Title" title="Your Title"></noscript></a>
 </dt></dl><br style="clear: both"><dl class="gallery-item">
 <dt class="gallery-icon">
-<a href="#" title="Your Title"><img width="150" height="150" src="placeholder.jpg" class="attachment-thumbnail jetpack-lazy-image" alt="Your Alt Title" title="Your Title" data-lazy-src="http://jetpacksite.com/img/img_thumb.png"><noscript><img width="150" height="150" src="http://jetpacksite.com/img/img_thumb.png" class="attachment-thumbnail" alt="Your Alt Title" title="Your Title"></noscript></a>
+<a href="#" title="Your Title"><img width="150" height="150" src="http://jetpacksite.com/img/img_thumb.png" class="attachment-thumbnail jetpack-lazy-image" alt="Your Alt Title" title="Your Title" srcset="placeholder.jpg"><noscript><img width="150" height="150" src="http://jetpacksite.com/img/img_thumb.png" class="attachment-thumbnail" alt="Your Alt Title" title="Your Title"></noscript></a>
 </dt></dl>
 <br style="clear: both;">
 </div>

tests/php/modules/lazy-images/test_class.lazy-images.php
@@ -26,7 +26,7 @@ class WP_Test_Lazy_Images extends WP_UnitTestCase {
 					'img',
 					' src="image.jpg"',
 				),
-				'<img src="placeholder.jpg" data-lazy-src="image.jpg" class=" jetpack-lazy-image"><noscript><img src="image.jpg" /></noscript>',
+				'<img src="image.jpg" srcset="placeholder.jpg" class=" jetpack-lazy-image"><noscript><img src="image.jpg" /></noscript>',
 			),
 
 			'img_with_other_attributes' => array(
@@ -35,7 +35,7 @@ class WP_Test_Lazy_Images extends WP_UnitTestCase {
 					'img',
 					' src="image.jpg" alt="Alt!"',
 				),
-				'<img src="placeholder.jpg" alt="Alt!" data-lazy-src="image.jpg" class=" jetpack-lazy-image"><noscript><img src="image.jpg" alt="Alt!" /></noscript>',
+				'<img src="image.jpg" alt="Alt!" srcset="placeholder.jpg" class=" jetpack-lazy-image"><noscript><img src="image.jpg" alt="Alt!" /></noscript>',
 			),
 
 			'img_with_srcset' => array(
@@ -45,7 +45,7 @@ class WP_Test_Lazy_Images extends WP_UnitTestCase {
 					' src="image.jpg" srcset="medium.jpg 1000w, large.jpg 2000w"',
 
 				),
-				'<img src="placeholder.jpg" data-lazy-src="image.jpg" data-lazy-srcset="medium.jpg 1000w, large.jpg 2000w" class=" jetpack-lazy-image"><noscript><img src="image.jpg" srcset="medium.jpg 1000w, large.jpg 2000w" /></noscript>',
+				'<img src="image.jpg" data-lazy-srcset="medium.jpg 1000w, large.jpg 2000w" srcset="placeholder.jpg" class=" jetpack-lazy-image"><noscript><img src="image.jpg" srcset="medium.jpg 1000w, large.jpg 2000w" /></noscript>',
 			),
 
 			'img_with_sizes' => array(
@@ -55,7 +55,7 @@ class WP_Test_Lazy_Images extends WP_UnitTestCase {
 					' src="image.jpg" sizes="(min-width: 36em) 33.3vw, 100vw"',
 
 				),
-				'<img src="placeholder.jpg" data-lazy-src="image.jpg" data-lazy-sizes="(min-width: 36em) 33.3vw, 100vw" class=" jetpack-lazy-image"><noscript><img src="image.jpg" sizes="(min-width: 36em) 33.3vw, 100vw" /></noscript>',
+				'<img src="image.jpg" data-lazy-sizes="(min-width: 36em) 33.3vw, 100vw" srcset="placeholder.jpg" class=" jetpack-lazy-image"><noscript><img src="image.jpg" sizes="(min-width: 36em) 33.3vw, 100vw" /></noscript>',
 			),
 		);
 	}
@@ -79,10 +79,11 @@ class WP_Test_Lazy_Images extends WP_UnitTestCase {
 					'height' => 10,
 				),
 				array(
-					'src' => 'placeholder.jpg',
+					'src' => 'image.jpg',
 					'width' => 10,
 					'height' => 10,
-					'data-lazy-src' => 'image.jpg',
+					'srcset' => 'placeholder.jpg',
+					'class' => ' jetpack-lazy-image',
 				),
 			),
 			'img_with_srcset' => array(
@@ -93,11 +94,12 @@ class WP_Test_Lazy_Images extends WP_UnitTestCase {
 					'srcset' => 'medium.jpg 1000w, large.jpg 2000w'
 				),
 				array(
-					'src' => 'placeholder.jpg',
+					'src' => 'image.jpg',
 					'width' => 10,
 					'height' => 10,
-					'data-lazy-src' => 'image.jpg',
-					'data-lazy-srcset' => 'medium.jpg 1000w, large.jpg 2000w'
+					'data-lazy-srcset' => 'medium.jpg 1000w, large.jpg 2000w',
+					'srcset' => 'placeholder.jpg',
+					'class' => ' jetpack-lazy-image',
 				)
 			),
 			'img_with_sizes' => array(
@@ -108,11 +110,12 @@ class WP_Test_Lazy_Images extends WP_UnitTestCase {
 					'sizes' => '(min-width: 36em) 33.3vw, 100vw'
 				),
 				array(
-					'src' => 'placeholder.jpg',
+					'src' => 'image.jpg',
 					'width' => 10,
 					'height' => 10,
-					'data-lazy-src' => 'image.jpg',
-					'data-lazy-sizes' => '(min-width: 36em) 33.3vw, 100vw'
+					'data-lazy-sizes' => '(min-width: 36em) 33.3vw, 100vw',
+					'srcset' => 'placeholder.jpg',
+					'class' => ' jetpack-lazy-image',
 				)
 			),
 			'gazette_theme_featured_image' => array(
@@ -154,10 +157,11 @@ class WP_Test_Lazy_Images extends WP_UnitTestCase {
 		$image = wp_get_attachment_image( $attachment_id );
 		remove_filter( 'wp_get_attachment_image_attributes', array( 'Jetpack_Lazy_Images', 'process_image_attributes' ), PHP_INT_MAX );
 
-		$image_src = wp_get_attachment_image_src( $attachment_id );
-
-		$this->assertContains( 'src="placeholder.jpg"', $image );
-		$this->assertContains( sprintf( 'data-lazy-src="%s"', $image_src[0] ), $image );
+		$this->assertContains( 'srcset="placeholder.jpg"', $image );
+		$this->assertContains(
+			sprintf( 'data-lazy-srcset="%s"', wp_get_attachment_image_srcset( $attachment_id, 'thumbnail' ) ),
+			$image
+		);
 	}
 
 	function test_wp_get_attachment_image_does_not_get_lazy_treatment_when_skip_lazy_added() {
@@ -169,7 +173,7 @@ class WP_Test_Lazy_Images extends WP_UnitTestCase {
 		$gallery_output = do_shortcode( $content );
 		$instance->remove_filters();
 
-		$this->assertContains( 'src="placeholder.jpg"', $gallery_output );
+		$this->assertContains( 'srcset="placeholder.jpg"', $gallery_output );
 
 		$instance->setup_filters();
 		add_filter( 'wp_get_attachment_image_attributes', array( $this, 'add_skip_lazy_class_to_attributes' ) );
@@ -177,7 +181,7 @@ class WP_Test_Lazy_Images extends WP_UnitTestCase {
 		remove_filter( 'wp_get_attachment_image_attributes', array( $this, 'add_skip_lazy_class_to_attributes' ) );
 		$instance->remove_filters();
 
-		$this->assertNotContains( 'src="placeholder.jpg"', $gallery_output );
+		$this->assertNotContains( 'srcset="placeholder.jpg"', $gallery_output );
 	}
 
 	/**
@@ -238,9 +242,9 @@ class WP_Test_Lazy_Images extends WP_UnitTestCase {
 		$output = $instance->add_image_placeholders( $input );
 
 		if ( $should_skip ) {
-			$this->assertNotContains( 'src="placeholder.jpg"', $output );
+			$this->assertNotContains( 'srcset="placeholder.jpg"', $output );
 		} else {
-			$this->assertContains( 'src="placeholder.jpg"', $output );
+			$this->assertContains( 'srcset="placeholder.jpg"', $output );
 		}
 	}
 
@@ -320,15 +324,15 @@ class WP_Test_Lazy_Images extends WP_UnitTestCase {
 		$instance = Jetpack_Lazy_Images::instance();
 		$src = '<img src="image.jpg" srcset="medium.jpg 1000w, large.jpg 2000w" class="wp-post-image"/>';
 
-		$this->assertContains( 'src="placeholder.jpg"', $instance->add_image_placeholders( $src ) );
+		$this->assertContains( 'srcset="placeholder.jpg"', $instance->add_image_placeholders( $src ) );
 
 		add_filter( 'jetpack_lazy_images_skip_image_with_attributes', '__return_true' );
-		$this->assertNotContains( 'src="placeholder.jpg"', $instance->add_image_placeholders( $src ) );
+		$this->assertNotContains( 'srcset="placeholder.jpg"', $instance->add_image_placeholders( $src ) );
 		remove_filter( 'jetpack_lazy_images_skip_image_with_attributes', '__return_true' );
 
 		add_filter( 'jetpack_lazy_images_skip_image_with_attributes', array( $this, '__skip_if_srcset' ), 10, 2 );
-		$this->assertNotContains( 'src="placeholder.jpg"', $instance->add_image_placeholders( $src ) );
-		$this->assertContains( 'src="placeholder.jpg"', $instance->add_image_placeholders( '<img src="image.jpg" />' ) );
+		$this->assertNotContains( 'srcset="placeholder.jpg"', $instance->add_image_placeholders( $src ) );
+		$this->assertContains( 'srcset="placeholder.jpg"', $instance->add_image_placeholders( '<img src="image.jpg" />' ) );
 		remove_filter( 'jetpack_lazy_images_skip_image_with_attributes', array( $this, '__skip_if_srcset' ), 10, 2 );
 	}
 

commit 7d3dfa43a803b31a42a3176bb6b8e007a5d8b5e2

Lazy Images: Fix issue introduced in #10054 where some images were double processed

modules/lazy-images/lazy-images.php
@@ -170,13 +170,13 @@ class Jetpack_Lazy_Images {
 		}
 
 		$old_attributes = self::flatten_kses_hair_data( $old_attributes_kses_hair );
-		$new_attributes = self::process_image_attributes( $old_attributes );
 
 		// If we didn't add lazy attributes, just return the original image source.
-		if ( false === strpos( $new_attributes['class'], 'jetpack-lazy-image' ) ) {
+		if (! empty( $old_attributes['class'] ) && false !== strpos( $old_attributes['class'], 'jetpack-lazy-image' ) ) {
 			return $matches[0];
 		}
 
+		$new_attributes     = self::process_image_attributes( $old_attributes );
 		$new_attributes_str = self::build_attributes_string( $new_attributes );
 
 		return sprintf( '<img %1$s><noscript>%2$s</noscript>', $new_attributes_str, $matches[0] );