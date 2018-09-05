745c7ccf995ddf3ef7f08e34fc727b3ff9331a8c
jrfnl <jrfnl@users.noreply.github.com>
2018-09-05 21:03:30 +0200

    PHPCS: whitelist a couple of correct uses of new PHP functionality
    When wrapped in a `function_exists()`, `class_exists()`, `defined()` etc, new PHP functionality can be safely used.

--- _inc/lib/class.media.php
+++ _inc/lib/class.media.php
@@ -111,6 +111,7 @@ class Jetpack_Media {
 	 */
 	protected static function is_file_supported_for_sideloading( $file ) {
 		if ( class_exists( 'finfo' ) ) { // php 5.3+
+			// phpcs:ignore PHPCompatibility.PHP.NewClasses.finfoFound
 			$finfo = new finfo( FILEINFO_MIME );
 			$mime = explode( '; ', $finfo->file( $file ) );
 			$type = $mime[0];
