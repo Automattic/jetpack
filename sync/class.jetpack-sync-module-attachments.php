afe3d20797e64f779ecc3f72ac4895c531d1ade9
Michael Turk <turk.michael@gmail.com>
2018-09-05 12:15:46 -0400

    Update

--- sync/class.jetpack-sync-module-attachments.php
+++ sync/class.jetpack-sync-module-attachments.php
@@ -6,39 +6,24 @@ class Jetpack_Sync_Module_Attachments extends Jetpack_Sync_Module {
 	}
 
 	public function init_listeners( $callable ) {
-		add_action( 'add_attachment', array( $this, 'send_attachment_info' ) );
+		add_action( 'add_attachment', array( $this, 'process_add' ) );
 		add_action( 'attachment_updated', array( $this, 'process_update'), 10, 3 );
 		add_action( 'jetpack_sync_save_update_attachment', $callable, 10, 2 );
 		add_action( 'jetpack_sync_save_add_attachment', $callable, 10, 2 );
 		add_action( 'jetpack_sync_save_attach_attachment', $callable, 10, 2 );
 	}
 
-	function send_attachment_info( $attachment_id ) {
+	function process_add( $attachment_id ) {
 		$attachment = get_post( $attachment_id );
-		if ( 'add_attachment' === current_filter() ) {
-			/**
-			 * Fires when the client needs to sync an new attachment
-			 *
-			 * @since 4.2.0
-			 *
-			 * @param int The attachment ID
-			 * @param object The attachment
-			 */
-			do_action( 'jetpack_sync_save_add_attachment', $attachment_id, $attachment );
-		} else {
-			/**
-			 * Fires when the client needs to sync an updated attachment
-			 *
-			 * @since 4.9.0
-			 *
-			 * @param int The attachment ID
-			 * @param object The attachment
-			 *
-			 * Previously this action was synced using jetpack_sync_save_add_attachment action.
-			 */
-			do_action( 'jetpack_sync_save_update_attachment', $attachment_id, $attachment );
-		}
-
+		/**
+		 * Fires when the client needs to sync an new attachment
+		 *
+		 * @since 4.2.0
+		 *
+		 * @param int The attachment ID
+		 * @param object The attachment
+		 */
+		do_action( 'jetpack_sync_save_add_attachment', $attachment_id, $attachment );
 	}
 
 	function process_update( $attachment_id, $attachment_after, $attachment_before ) {
