917181ffede4f8eafc4d5a279c5db2e3f10f442c
Brandon Kraft <public@brandonkraft.com>
2018-09-11 10:11:36 -0500

    Add strict comparison to bring inline with PHPCS.

--- functions.opengraph.php
+++ functions.opengraph.php
@@ -280,7 +280,7 @@ function jetpack_og_get_image( $width = 200, $height = 200, $deprecated = null )
 
 	if ( is_singular() && ! is_home() ) {
 		// Grab obvious image if post is an attachment page for an image
-		if ( is_attachment( get_the_ID() ) && 'image' == substr( get_post_mime_type(), 0, 5 ) ) {
+		if ( is_attachment( get_the_ID() ) && 'image' === substr( get_post_mime_type(), 0, 5 ) ) {
 			$image['src'] = wp_get_attachment_url( get_the_ID() );
 		}
 
