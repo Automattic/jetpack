e07addf220d02226f04d874b1b565ceb9f502839
Brandon Kraft <public@brandonkraft.com>
2018-09-19 12:25:31 -0500

    WPCS: Fix style issues via phpcbf (#10181)

--- modules/tiled-gallery/tiled-gallery/templates/carousel-container.php
+++ modules/tiled-gallery/tiled-gallery/templates/carousel-container.php
@@ -11,7 +11,7 @@ if ( defined( 'JSON_HEX_AMP' ) ) {
 <div
 	class="tiled-gallery type-<?php echo $this->type; ?> tiled-gallery-unresized"
 	data-original-width="<?php echo esc_attr( Jetpack_Tiled_Gallery::get_content_width() ); ?>"
-	<?php if ( isset( $extra ) ): ?>
+	<?php if ( isset( $extra ) ) : ?>
 		data-carousel-extra='<?php echo _wp_specialchars( wp_check_invalid_utf8( $extra ), ENT_QUOTES, false, true ); ?>'
 	<?php endif; ?>
 	itemscope itemtype="http://schema.org/ImageGallery"
