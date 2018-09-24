a317bb784823eb26e73eeeafdd9550eb88a4afc0
Derek Smart <smart@automattic.com>
2018-09-24 13:43:26 -0400

    Add initial Calypsoify integration

--- jetpack.php
+++ jetpack.php
@@ -89,6 +89,7 @@ require_once( JETPACK__PLUGIN_DIR . 'modules/module-headings.php');
 require_once( JETPACK__PLUGIN_DIR . 'class.jetpack-constants.php');
 require_once( JETPACK__PLUGIN_DIR . 'class.jetpack-idc.php'  );
 require_once( JETPACK__PLUGIN_DIR . 'class.jetpack-connection-banner.php'  );
+require_once( JETPACK__PLUGIN_DIR . 'class.jetpack-calypsoify.php'  );
 
 if ( is_admin() ) {
 	require_once( JETPACK__PLUGIN_DIR . 'class.jetpack-admin.php'     );
