7a14cadac667cca1ce9852d98c3a6adc99abe733
Jeremy Herve <jeremy@jeremy.hu>
2018-09-25 13:50:59 +0200

    Verification tools: remove extra tabs to avoid failing Travis tests

--- _inc/client/traffic/verification-services/google.jsx
+++ _inc/client/traffic/verification-services/google.jsx
@@ -188,8 +188,8 @@ class GoogleVerificationServiceComponent extends React.Component {
 									{ ' ' }
 									{
 										__( 'Note it will email you if any unusual events occur with your properties. ' +
-												'Unusual events include indications that your website has been {{a1}}hacked{{/a1}}, ' +
-												'or problems that Google had when {{a2}}crawling or indexing{{/a2}} your site.', {
+										'Unusual events include indications that your website has been {{a1}}hacked{{/a1}}, ' +
+										'or problems that Google had when {{a2}}crawling or indexing{{/a2}} your site.', {
 											components: {
 												a1: <ExternalLink
 													icon
