f15a3f8fb8ce3ef406559758c0d5704a8a69c53a
George Stephanis <georgestephanis@automattic.com>
2018-10-02 14:48:47 -0400

    Update some icons, renders, etc.

--- modules/contact-form/js/gutenblock.jsx
+++ modules/contact-form/js/gutenblock.jsx
@@ -1,6 +1,12 @@
 ( function( wp, _ ) {
 
-	const { Button, PanelBody, TextControl } = wp.components;
+	const {
+		Button,
+		PanelBody,
+		TextControl,
+		TextareaControl,
+		CheckboxControl
+	} = wp.components;
 	const { registerBlockType } = wp.blocks;
 	const { InnerBlocks, InspectorControls, BlockControls } = wp.editor;
 	const { Component, Fragment } = wp.element;
@@ -8,27 +14,27 @@
 
 	class GrunionForm extends Component {
 		render( /* { setState } */ ) {
-		    return (
-			    <Fragment>
-                    <InspectorControls>
-                        <PanelBody title={ __( 'Submission Details', 'jetpack' ) }>
-                            <TextControl
-                                label={ __( 'What would you like the subject of the email to be?' ) }
-                                value={ this.props.subject }
+			return (
+				<Fragment>
+					<InspectorControls>
+						<PanelBody title={ __( 'Submission Details', 'jetpack' ) }>
+							<TextControl
+								label={ __( 'What would you like the subject of the email to be?' ) }
+								value={ this.props.subject }
 							//	onChange={ ( subject ) => setState( { subject : subject } ) }
-                            />
-                            <TextControl
-                                label={ __( 'Which email address should we send the submissions to?' ) }
-                                value={ this.props.to }
-                            //    onChange={ ( to ) => setState( { to : to } ) }
-                            />
-                        </PanelBody>
-                    </InspectorControls>
-                    <div className="grunion-form">
-                        {this.props.children}
-                        <Button isPrimary isDefault>Submit</Button>
-                    </div>
-                </Fragment>
+							/>
+							<TextControl
+								label={ __( 'Which email address should we send the submissions to?' ) }
+								value={ this.props.to }
+							//    onChange={ ( to ) => setState( { to : to } ) }
+							/>
+						</PanelBody>
+					</InspectorControls>
+					<div className="grunion-form">
+						{this.props.children}
+						<Button isPrimary isDefault>Submit</Button>
+					</div>
+				</Fragment>
 			);
 		}
 	}
@@ -40,10 +46,10 @@
 		supportHTML : false,
 
 		attributes : {
-		    subject : {
-		        type    : 'string',
-                default : null
-            },
+			subject : {
+				type    : 'string',
+				default : null
+			},
 			to : {
 				type    : 'string',
 				default : null
@@ -52,15 +58,21 @@
 
 		edit: function( props ) {
 			return (
-                <GrunionForm key="grunion/form" className={props.className}>
-                    <InnerBlocks
+				<GrunionForm key="grunion/form" className={props.className}>
+					<InnerBlocks
 						allowedBlocks={ [
-                            'grunion/field-text',
-                            'grunion/field-email'
+							'grunion/field-text',
+							'grunion/field-email'
+						] }
+						template={ [
+							[ 'grunion/field-name',     { label : __( 'Name' ) } ],
+							[ 'grunion/field-email',    { label : __( 'Email' ) } ],
+							[ 'grunion/field-text',     { label : __( 'Subject' ) } ],
+							[ 'grunion/field-textarea', { label : __( 'Message' ) } ]
 						] }
 					/>
-                </GrunionForm>
-            );
+				</GrunionForm>
+			);
 		},
 
 		save: function( props ) {
@@ -72,15 +84,32 @@
 
 	class GrunionField extends Component {
 		render() {
-			console.log( this.props );
+			var field;
+			switch ( this.props.type ) {
+				case 'checkbox' :
+					field = ( <CheckboxControl
+						label={this.props.type + this.props.label}
+						disabled={true}
+					/> );
+					break;
+				case 'textarea' :
+					field = (<TextareaControl
+						label={this.props.type + this.props.label}
+						disabled={true}
+					/> );
+					break;
+				default :
+					field = ( <TextControl
+						type={this.props.type}
+						label={this.props.type + this.props.label}
+						disabled={true}
+					/> );
+			}
+
 			return (
 				<Fragment>
 					<div className="grunion-field">
-                        <TextControl
-							type={ this.props.type }
-                            label={ this.props.type + this.props.label }
-							disabled={true}
-                        />
+						{ field }
 					</div>
 				</Fragment>
 			);
@@ -103,12 +132,13 @@
 		}
 	};
 
-    registerBlockType( 'grunion/field-name', _.defaults({
-        title       : __( 'Name', 'jetpack' ),
-        edit: function( props ) {
-            return ( <GrunionField type="text" label={ props.label } /> );
-        }
-    }, FieldDefaults ) );
+	registerBlockType( 'grunion/field-name', _.defaults({
+		title       : __( 'Name', 'jetpack' ),
+		icon        : 'admin-users',
+		edit: function( props ) {
+			return ( <GrunionField type="text" label={ props.label } /> );
+		}
+	}, FieldDefaults ) );
 
 	registerBlockType( 'grunion/field-text', _.defaults({
 		title       : __( 'Text', 'jetpack' ),
@@ -117,60 +147,73 @@
 		}
 	}, FieldDefaults ) );
 
-    registerBlockType( 'grunion/field-email', _.defaults({
-        title       : __( 'Email', 'jetpack' ),
-        edit: function( props ) {
-            return ( <GrunionField type="email" label={ props.label } /> );
-        }
-    }, FieldDefaults ) );
+	registerBlockType( 'grunion/field-textarea', _.defaults({
+		title       : __( 'Textarea', 'jetpack' ),
+		edit: function( props ) {
+			return ( <GrunionField type="textarea" label={ props.label } /> );
+		}
+	}, FieldDefaults ) );
 
-    registerBlockType( 'grunion/field-url', _.defaults({
-        title       : __( 'URL', 'jetpack' ),
-        edit: function( props ) {
-            return ( <GrunionField type="url" label={ props.label } /> );
-        }
-    }, FieldDefaults ) );
+	registerBlockType( 'grunion/field-email', _.defaults({
+		title       : __( 'Email', 'jetpack' ),
+		icon        : 'email',
+		edit: function( props ) {
+			return ( <GrunionField type="email" label={ props.label } /> );
+		}
+	}, FieldDefaults ) );
 
-    registerBlockType( 'grunion/field-date', _.defaults({
-        title       : __( 'Date', 'jetpack' ),
-        edit: function( props ) {
-            return ( <GrunionField type="text" label={ props.label } /> );
-        }
-    }, FieldDefaults ) );
+	registerBlockType( 'grunion/field-url', _.defaults({
+		title       : __( 'URL', 'jetpack' ),
+		icon        : 'share-alt2',
+		edit: function( props ) {
+			return ( <GrunionField type="url" label={ props.label } /> );
+		}
+	}, FieldDefaults ) );
 
-    registerBlockType( 'grunion/field-telephone', _.defaults({
-        title       : __( 'Telephone', 'jetpack' ),
-        edit: function( props ) {
-            return ( <GrunionField type="tel" label={ props.label } /> );
-        }
-    }, FieldDefaults ) );
+	registerBlockType( 'grunion/field-date', _.defaults({
+		title       : __( 'Date', 'jetpack' ),
+		icon        : 'calendar-alt',
+		edit: function( props ) {
+			return ( <GrunionField type="text" label={ props.label } /> );
+		}
+	}, FieldDefaults ) );
 
-    registerBlockType( 'grunion/field-radio', _.defaults({
-        title       : __( 'Date', 'jetpack' ),
-        edit: function( props ) {
-            return ( <GrunionField type="radio" label={ props.label } /> );
-        }
-    }, FieldDefaults ) );
+	registerBlockType( 'grunion/field-telephone', _.defaults({
+		title       : __( 'Telephone', 'jetpack' ),
+		icon        : 'phone',
+		edit: function( props ) {
+			return ( <GrunionField type="tel" label={ props.label } /> );
+		}
+	}, FieldDefaults ) );
 
-    registerBlockType( 'grunion/field-select', _.defaults({
-        title       : __( 'Date', 'jetpack' ),
-        edit: function( props ) {
-            return ( <GrunionField type="select" label={ props.label } /> );
-        }
-    }, FieldDefaults ) );
+	registerBlockType( 'grunion/field-radio', _.defaults({
+		title       : __( 'Radio', 'jetpack' ),
+		edit: function( props ) {
+			return ( <GrunionField type="radio" label={ props.label } /> );
+		}
+	}, FieldDefaults ) );
 
-    registerBlockType( 'grunion/field-checkbox', _.defaults({
-        title       : __( 'Date', 'jetpack' ),
-        edit: function( props ) {
-            return ( <GrunionField type="checkbox" label={ props.label } /> );
-        }
-    }, FieldDefaults ) );
+	registerBlockType( 'grunion/field-select', _.defaults({
+		title       : __( 'Select', 'jetpack' ),
+		edit: function( props ) {
+			return ( <GrunionField type="select" label={ props.label } /> );
+		}
+	}, FieldDefaults ) );
 
-    registerBlockType( 'grunion/field-checkbox-multiple', _.defaults({
-        title       : __( 'Date', 'jetpack' ),
-        edit: function( props ) {
-            return ( <GrunionField type="checkbox-multiple" label={ props.label } /> );
-        }
-    }, FieldDefaults ) );
+	registerBlockType( 'grunion/field-checkbox', _.defaults({
+		title       : __( 'Checkbox', 'jetpack' ),
+		icon        : 'forms',
+		edit: function( props ) {
+			return ( <GrunionField type="checkbox" label={ props.label } /> );
+		}
+	}, FieldDefaults ) );
+
+	registerBlockType( 'grunion/field-checkbox-multiple', _.defaults({
+		title       : __( 'Checkbox Multiple', 'jetpack' ),
+		icon        : 'forms',
+		edit: function( props ) {
+			return ( <GrunionField type="checkbox-multiple" label={ props.label } /> );
+		}
+	}, FieldDefaults ) );
 
 } )( window.wp, _ );
