62aa01857cafee8d7bfe9c338f988f5baae246e8
Jeremy Herve <jeremy@tagada.hu>
2018-10-24 21:30:16 +0200

    Asset CDN / Photon: group the 2 features under a single module card in the admin. (#10311)
    * Photon: update admin UI description and support link.
    
    Fixes #10300
    
    - Update the support link to use the new name we've given to the Photon feature.
    - Remove mention of "saving space" since Photon does not do that.
    
    * Photon card: Tiled Galleries depend on Photon, not Carousel.
    
    * Photon: create new admin UI card with both Photon and Asset CDN
    
    @see p9jf6J-11u-p2
    
    * Photon: fix toggle handling when one option is on and the other off
    
    @see https://github.com/Automattic/jetpack/pull/10311#issuecomment-429359868
    
    * Photon toggle: handle potential module overrides.
    
    While the individual module toggles' appearance is automatically managed,
    the toggle that allows one to toggle both modules at once is not.
    This commit adds the necessary checks so one can't use the main toggle
    to change the status of a module when it is forbidden.
    
    * Asset CDN: Site Accelerator should be lowercase.
    
    @see https://github.com/Automattic/jetpack/pull/10311#issuecomment-430644874
    
    * Speed Up Site: simplify check if one of the modules is active
    
    * Admin Page: make sure the main toggle always appears in Search results.
    
    * Site Accelerator: track clicks and events when using main toggle
    
    @see https://github.com/Automattic/jetpack/pull/10311#issuecomment-430795435
    
    * Asset CDN: add new filter to short-circuit the module.
    
    It allows one to easily test any changes in Jetpack, even when the Asset CDN is on,
    without being served assets from the CDN.
    
    * Site accelerator: add toggle status to track click event.
    
    * Site Accelerator: handle toggling status for the main toggle.
    
    * Site Accelerator: rename variables to avoid confusion
    
    Photon, Photon CDN, Asset CDN, CDN, it all gets a bit confusing. Hopefully this will help:
    - Photon = Photon, the module
    - Asset CDN = the module previously known as Photon CDN.
    - Site Accelerator: the main toggle. Not a module per se, but a way to turn on the 2 modules at once.
    
    * Site Accelerator: add new support doc link.
    
    * Site Accelerator: update wording based on feedback from Editorial
    
    @see https://github.com/Automattic/wp-calypso/pull/27944#issuecomment-431178239
    
    * Asset CDN: add module info to filter docblocks
    
    * Lazy Images: update module description in admin UI.
    
    @see https://github.com/Automattic/jetpack/pull/10311#issuecomment-432438781

--- _inc/client/writing/speed-up-site.jsx
+++ _inc/client/writing/speed-up-site.jsx
@@ -8,18 +8,21 @@ import { translate as __ } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
+import { FormFieldset } from 'components/forms';
+import CompactFormToggle from 'components/form/form-toggle/compact';
 import { ModuleSettingsForm as moduleSettingsForm } from 'components/module-settings/module-settings-form';
-import { getModule } from 'state/modules';
+import { getModule, getModuleOverride } from 'state/modules';
 import { isModuleFound as _isModuleFound } from 'state/search';
 import SettingsCard from 'components/settings-card';
 import SettingsGroup from 'components/settings-group';
 import { ModuleToggle } from 'components/module-toggle';
+import analytics from 'lib/analytics';
 
 const SpeedUpSite = moduleSettingsForm(
 	class extends Component {
 		toggleModule = ( name, value ) => {
 			if ( 'photon' === name ) {
-				// Carousel depends on Photon. Deactivate it if Photon is deactivated.
+				// Tiled Galleries depends on Photon. Deactivate it if Photon is deactivated.
 				if ( false === ! value ) {
 					this.props.updateOptions( { photon: false, 'tiled-gallery': false, tiled_galleries: false } );
 				} else {
@@ -30,16 +33,146 @@ const SpeedUpSite = moduleSettingsForm(
 			}
 		};
 
+		handleSiteAcceleratorChange = () => {
+			// Initial status for both modules.
+			let newPhotonStatus = this.props.getOptionValue( 'photon' );
+			let newAssetCdnStatus = this.props.getOptionValue( 'photon-cdn' );
+
+			// Check if any of the CDN options are on.
+			const siteAcceleratorStatus = newPhotonStatus || newAssetCdnStatus;
+
+			// Are the modules available?
+			const photonStatus = this.props.getModuleOverride( 'photon' );
+			const assetCdnStatus = this.props.getModuleOverride( 'photon-cdn' );
+
+			// If one of them is on, we turn everything off, including Tiled Galleries that depend on Photon.
+			if ( true === siteAcceleratorStatus ) {
+				if ( false === ! newPhotonStatus && 'active' !== photonStatus ) {
+					newPhotonStatus = false;
+
+					this.props.updateOptions( {
+						photon: false,
+						'tiled-gallery': false,
+						tiled_galleries: false
+					} );
+				}
+				if ( false === ! newAssetCdnStatus && 'active' !== assetCdnStatus ) {
+					newAssetCdnStatus = false;
+
+					this.props.updateOptions( {
+						'photon-cdn': false
+					} );
+				}
+			} else {
+				if ( false === newPhotonStatus && 'inactive' !== photonStatus ) {
+					newPhotonStatus = true;
+
+					this.props.updateOptions( {
+						photon: true,
+						'tiled-gallery': true,
+						tiled_galleries: true
+					} );
+				}
+				if ( false === newAssetCdnStatus && 'inactive' !== assetCdnStatus ) {
+					newAssetCdnStatus = true;
+
+					this.props.updateOptions( {
+						'photon-cdn': true
+					} );
+				}
+			}
+
+			// If at least one of the modules is now on, let's reflect that with the status of our main toggle.
+			if ( true === newPhotonStatus || true === newAssetCdnStatus ) {
+				// Track the main toggle switch.
+				analytics.tracks.recordJetpackClick( {
+					target: 'jetpack_site_accelerator_toggle',
+					toggled: 'on'
+				} );
+			} else {
+				analytics.tracks.recordJetpackClick( {
+					target: 'jetpack_site_accelerator_toggle',
+					toggled: 'off'
+				} );
+			}
+
+			// Track any potential Photon toggle switch.
+			if ( this.props.getOptionValue( 'photon' ) !== newPhotonStatus ) {
+				analytics.tracks.recordEvent( 'jetpack_wpa_module_toggle', {
+					module: 'photon',
+					toggled: ( false === newPhotonStatus ) ? 'off' : 'on'
+				} );
+			}
+
+			// Track any potential Photon CDN toggle switch.
+			if ( this.props.getOptionValue( 'photon-cdn' ) !== newAssetCdnStatus ) {
+				analytics.tracks.recordEvent( 'jetpack_wpa_module_toggle', {
+					module: 'photon-cdn',
+					toggled: ( false === newAssetCdnStatus ) ? 'off' : 'on'
+				} );
+			}
+		};
+
 		render() {
 			const foundPhoton = this.props.isModuleFound( 'photon' );
+			const foundAssetCdn = this.props.isModuleFound( 'photon-cdn' );
 			const foundLazyImages = this.props.isModuleFound( 'lazy-images' );
 
-			if ( ! foundPhoton && ! foundLazyImages ) {
+			if ( ! foundPhoton && ! foundLazyImages && ! foundAssetCdn ) {
 				return null;
 			}
 
-			const photon = this.props.module( 'photon' );
 			const lazyImages = this.props.module( 'lazy-images' );
+
+			// Check if any of the CDN options are on.
+			const siteAcceleratorStatus = this.props.getOptionValue( 'photon' ) || this.props.getOptionValue( 'photon-cdn' );
+
+			// Is at least one of the 2 modules available (not hidden via a module override)?
+			const photonStatus = this.props.getModuleOverride( 'photon' );
+			const assetCdnStatus = this.props.getModuleOverride( 'photon-cdn' );
+			const canDisplaySiteAcceleratorSettings = ( foundPhoton && foundAssetCdn ) && ( 'inactive' !== photonStatus || 'inactive' !== assetCdnStatus );
+
+			// Display the main toggle in main search results as long as one of the modules is not hidden.
+			const canAppearInSearch = ( foundPhoton || foundAssetCdn ) && ( 'inactive' !== photonStatus || 'inactive' !== assetCdnStatus );
+
+			// Monitor any changes that should cause our main toggle to appear toggling.
+			let togglingSiteAccelerator;
+			// First Photon activating.
+			if ( ! this.props.getOptionValue( 'photon' ) && this.props.isSavingAnyOption( 'photon' ) ) {
+				if ( this.props.getOptionValue( 'photon-cdn' ) ) {
+					togglingSiteAccelerator = false;
+				} else {
+					togglingSiteAccelerator = true;
+				}
+			// Then Asset CDN activating.
+			} else if ( ! this.props.getOptionValue( 'photon-cdn' ) && this.props.isSavingAnyOption( 'photon-cdn' ) ) {
+				if ( this.props.getOptionValue( 'photon' ) ) {
+					togglingSiteAccelerator = false;
+				} else {
+					togglingSiteAccelerator = true;
+				}
+			// Then Photon deactivating.
+			} else if ( this.props.getOptionValue( 'photon' ) && this.props.isSavingAnyOption( 'photon' ) ) {
+				if ( this.props.getOptionValue( 'photon-cdn' ) ) {
+					togglingSiteAccelerator = false;
+				} else {
+					togglingSiteAccelerator = true;
+				}
+
+				// Is the Asset CDN being disabled as well?
+				if ( this.props.getOptionValue( 'photon-cdn' ) && this.props.isSavingAnyOption( 'photon-cdn' ) ) {
+					togglingSiteAccelerator = true;
+				}
+			// Then Asset CDN deactivating.
+			} else if ( this.props.getOptionValue( 'photon-cdn' ) && this.props.isSavingAnyOption( 'photon-cdn' ) ) {
+				if ( this.props.getOptionValue( 'photon' ) ) {
+					togglingSiteAccelerator = false;
+				} else {
+					togglingSiteAccelerator = true;
+				}
+			} else {
+				togglingSiteAccelerator = false;
+			}
 
 			return (
 				<SettingsCard
@@ -47,35 +180,58 @@ const SpeedUpSite = moduleSettingsForm(
 					header={ __( 'Performance & speed' ) }
 					hideButton>
 
-					{ foundPhoton &&
+					{ ( foundPhoton || foundAssetCdn ) &&
 						<SettingsGroup
 							hasChild
-							disableInDevMode
-							module={ photon }
 							support={ {
-								link: 'https://jetpack.com/support/photon/',
+								link: 'http://jetpack.com/support/site-accelerator/',
 							} }
 							>
 							<p>
 								{ __(
-									"Jetpack's global Content Delivery Network (CDN) optimizes " +
-										'images so your visitors enjoy the fastest experience ' +
-										'regardless of device or location. It also helps you ' +
-										'save space on your hosting plan, since images are ' +
-										'stored on our servers.'
+									'Load pages faster by allowing Jetpack to optimize your images and serve your images ' +
+										'and static files (like CSS and JavaScript) from our global network of servers.'
 								) }
 							</p>
-							<ModuleToggle
-								slug="photon"
-								disabled={ this.props.isUnavailableInDevMode( 'photon' ) }
-								activated={ this.props.getOptionValue( 'photon' ) }
-								toggling={ this.props.isSavingAnyOption( 'photon' ) }
-								toggleModule={ this.toggleModule }
-							>
-								<span className="jp-form-toggle-explanation">
-									{ __( 'Serve images from our global CDN' ) }
-								</span>
-							</ModuleToggle>
+							{ canAppearInSearch &&
+								<CompactFormToggle
+									checked={ siteAcceleratorStatus }
+									toggling={ togglingSiteAccelerator }
+									onChange={ this.handleSiteAcceleratorChange }
+									disabled={ ! canDisplaySiteAcceleratorSettings }
+								>
+									<span className="jp-form-toggle-explanation">
+										{ __( 'Enable site accelerator' ) }
+									</span>
+								</CompactFormToggle>
+							}
+							<FormFieldset>
+								{ foundPhoton &&
+									<ModuleToggle
+										slug="photon"
+										disabled={ this.props.isUnavailableInDevMode( 'photon' ) }
+										activated={ this.props.getOptionValue( 'photon' ) }
+										toggling={ this.props.isSavingAnyOption( 'photon' ) }
+										toggleModule={ this.toggleModule }
+									>
+										<span className="jp-form-toggle-explanation">
+											{ __( 'Speed up image load times' ) }
+										</span>
+									</ModuleToggle>
+								}
+								{ foundAssetCdn &&
+									<ModuleToggle
+										slug="photon-cdn"
+										activated={ this.props.getOptionValue( 'photon-cdn' ) }
+										toggling={ this.props.isSavingAnyOption( 'photon-cdn' ) }
+										toggleModule={ this.toggleModule }
+									>
+										<span className="jp-form-toggle-explanation">
+											{ __( 'Speed up static file load times' ) }
+										</span>
+									</ModuleToggle>
+								}
+							</FormFieldset>
 						</SettingsGroup>
 					}
 
@@ -89,7 +245,7 @@ const SpeedUpSite = moduleSettingsForm(
 							>
 							<p>
 								{ __(
-									"Lazy-loading images improve your site's speed and create a " +
+									'Lazy-loading images will improve your site’s speed and create a ' +
 										'smoother viewing experience. Images will load as visitors ' +
 										'scroll down the screen, instead of all at once.'
 								) }
@@ -117,7 +273,8 @@ export default connect(
 	( state ) => {
 		return {
 			module: ( module_name ) => getModule( state, module_name ),
-			isModuleFound: ( module_name ) => _isModuleFound( state, module_name )
+			isModuleFound: ( module_name ) => _isModuleFound( state, module_name ),
+			getModuleOverride: ( module_name ) => getModuleOverride( state, module_name )
 		};
 	}
 )( SpeedUpSite );
