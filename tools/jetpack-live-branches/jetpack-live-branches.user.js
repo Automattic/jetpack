0c4364cf33dfbfc836d843dfab75c8df0592d3b5
oskosk <oskosk@gmail.com>
2018-10-25 11:49:00 -0300

    Add ability to launch with WordPress 5 Beta to Jetpack Live Branches Tampermonkey script

--- tools/jetpack-live-branches/jetpack-live-branches.user.js
+++ tools/jetpack-live-branches/jetpack-live-branches.user.js
@@ -29,6 +29,7 @@
 			<li class="task-list-item enabled"><input type="checkbox" name="wp-debug-log" checked class="task-list-item-checkbox">Launch sites with WP_DEBUG and WP_DEBUG_LOG set to true</li>
 			<li class="task-list-item enabled"><input type="checkbox" name="gutenberg" checked class="task-list-item-checkbox">Launch with Gutenberg installed</li>
 			<li class="task-list-item enabled"><input type="checkbox" name="gutenpack" ${ isGutenbergPr ? 'checked' : '' } class="task-list-item-checkbox">Launch with built blocks</li>
+			<li class="task-list-item enabled"><input type="checkbox" name="wordpress-5-beta" class="task-list-item-checkbox">Launch with WordPress 5.0 Beta</li>
 			<li class="task-list-item enabled"><input type="checkbox" name="woocommerce" class="task-list-item-checkbox">Launch with WooCommerce installed</li>
 			<li class="task-list-item enabled"><input type="checkbox" name="code-snippets" class="task-list-item-checkbox">Launch with Code Snippets installed</li>
 			<li class="task-list-item enabled"><input type="checkbox" name="wp-rollback" class="task-list-item-checkbox">Launch with WP Rollback installed</li>
