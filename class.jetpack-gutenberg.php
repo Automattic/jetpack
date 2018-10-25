b4b023fc0bbeef59961eb684885d3ff4d060dff7
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-10-25 09:56:01 -0700

    Gutenblock: Add the frontend code only if the block is being called.
    This Removed the loading of the front end code that comes as a bundle of all the files.
    And make sure that it loads only the front end code that is needed when the block is going to be displayed.

--- class.jetpack-gutenberg.php
+++ class.jetpack-gutenberg.php
@@ -1,9 +1,51 @@
 <?php
+function jetpack_register_block( $type, $args = array() ) {
+	Jetpack_Gutenberg::add_block( $type, $args );
+}
 
 /**
  * General Gutenberg editor specific functionality
  */
 class Jetpack_Gutenberg {
+
+	public static $all_blocks = array();
+	public static function add_block( $type, $args ) {
+		self::$all_blocks[ $type ] = $args;
+	}
+
+	public static function load_all_blocks() {
+		if ( ! self::is_gutenberg_available() ) {
+			return;
+		}
+		foreach ( self::$all_blocks as $type => $args ) {
+			register_block_type(
+				'jetpack/' . $type,
+				$args
+			);
+		}
+	}
+
+	public static function load_assets_as_required( $type ) {
+		// Enqueue styles
+		$style_relative_path = '_inc/blocks/'. $type .'/view.'. ( is_rtl() ? '.rtl' : '' ) .'css';
+		if ( file_exists( JETPACK__PLUGIN_DIR . $style_relative_path ) ) {
+			$style_version       = Jetpack::is_development_version()
+				? filemtime( JETPACK__PLUGIN_DIR . $style_relative_path )
+				: JETPACK__VERSION;
+			$view_style  = plugins_url( $style_relative_path, JETPACK__PLUGIN_FILE );
+			wp_enqueue_style( 'jetpack-block-' . $type, $view_style, array(), $style_version );
+		}
+		// Enqueue script
+		$script_relative_path = '_inc/blocks/'. $type .'/view.js';
+		if ( file_exists( JETPACK__PLUGIN_DIR . $script_relative_path ) ) {
+			$script_version       = Jetpack::is_development_version()
+				? filemtime( JETPACK__PLUGIN_DIR . $script_relative_path )
+				: JETPACK__VERSION;
+			$view_script  = plugins_url( $script_relative_path, JETPACK__PLUGIN_FILE );
+			wp_enqueue_script( 'jetpack-block-' . $type, $view_script, array(), $script_version );
+		}
+	}
+
 	/**
 	 * Check if Gutenberg editor is available
 	 *
@@ -13,66 +55,6 @@ class Jetpack_Gutenberg {
 	 */
 	public static function is_gutenberg_available() {
 		return function_exists( 'register_block_type' );
-	}
-
-	/**
-	 * Load Gutenberg assets
-	 *
-	 * @since 6.7.0
-	 *
-	 * @return void
-	 */
-	public static function enqueue_block_assets() {
-		if ( ! self::should_load_blocks() ) {
-			return;
-		}
-
-		$rtl = is_rtl() ? '.rtl' : '';
-
-		/**
-		 * Filter to enable serving blocks via CDN
-		 *
-		 * CDN cache is busted once a day or when Jetpack version changes. To customize it:
-		 *   add_filter( 'jetpack_gutenberg_cdn_cache_buster', function( $version ) { return time(); }, 10, 1 );
-		 *
-		 * @since 6.5.0
-		 *
-		 * @param bool false Whether to load Gutenberg blocks from CDN
-		 */
-		if ( apply_filters( 'jetpack_gutenberg_cdn', false ) ) {
-			$cdn_base    = 'https://s0.wp.com/wp-content/mu-plugins/jetpack/_inc/blocks';
-			$view_script = "$cdn_base/view.js";
-			$view_style  = "$cdn_base/view$rtl.css";
-
-			/**
-			 * Filter to modify cache busting for Gutenberg block assets loaded from CDN
-			 *
-			 * @since 6.5.0
-			 *
-			 * @param string
-			 */
-			$version = apply_filters( 'jetpack_gutenberg_cdn_cache_buster', sprintf( '%s-%s', gmdate( 'd-m-Y' ), JETPACK__VERSION ) );
-		} else {
-			$view_script = plugins_url( '_inc/blocks/view.js', JETPACK__PLUGIN_FILE );
-			$view_style  = plugins_url( "_inc/blocks/view$rtl.css", JETPACK__PLUGIN_FILE );
-			$version     = Jetpack::is_development_version() && file_exists( JETPACK__PLUGIN_DIR . '_inc/blocks/view.js' )
-				? filemtime( JETPACK__PLUGIN_DIR . '_inc/blocks/view.js' )
-				: JETPACK__VERSION;
-		}
-
-		wp_enqueue_script(
-			'jetpack-blocks-view',
-			$view_script,
-			array(
-				'wp-element',
-				'wp-i18n',
-			),
-			$version
-		);
-
-		Jetpack::setup_wp_i18n_locale_data();
-
-		wp_enqueue_style( 'jetpack-blocks-view', $view_style, array(), $version );
 	}
 
 	/**
