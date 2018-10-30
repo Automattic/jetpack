751ac61e0b3ee0d0c4f37ee982380d9de503b995
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-10-30 11:08:22 -0700

    Sanitize the type again

--- class.jetpack-gutenberg.php
+++ class.jetpack-gutenberg.php
@@ -110,6 +110,7 @@ class Jetpack_Gutenberg {
 	 * @return void
 	 */
 	public static function load_assets_as_required( $type ) {
+		$type = sanitize_title_with_dashes( $type );
 		// Enqueue styles.
 		$style_relative_path = '_inc/blocks/' . $type . '/view' . ( is_rtl() ? '.rtl' : '' ) . '.css';
 		if ( self::block_has_asset( $style_relative_path ) ) {
