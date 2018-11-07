b647c60362aa7b3228bc07fd30fc813931cd8b6b
Filipe Varela <keoshi@keoshi.com>
2018-11-07 13:14:28 +0000

    Add my plan navigation item

--- _inc/client/components/navigation/index.jsx
+++ _inc/client/components/navigation/index.jsx
@@ -29,6 +29,10 @@ export class Navigation extends React.Component {
 		this.trackNavClick( 'dashboard' );
 	};
 
+	trackMyPlanClick = () => {
+		this.trackNavClick( 'my-plan' );
+	};
+
 	trackPlansClick = () => {
 		this.trackNavClick( 'dashboard' );
 	};
@@ -43,6 +47,12 @@ export class Navigation extends React.Component {
 						onClick={ this.trackDashboardClick }
 						selected={ ( this.props.route.path === '/dashboard' ) || ( this.props.route.path === '/' ) }>
 						{ __( 'At a Glance', { context: 'Navigation item.' } ) }
+					</NavItem>
+					<NavItem
+						path="#/my-plan"
+						onClick={ this.trackMyPlanClick }
+						selected={ this.props.route.path === '/my-plan' }>
+						{ __( 'My Plan', { context: 'Navigation item.' } ) }
 					</NavItem>
 					<NavItem
 						path="#/plans"
