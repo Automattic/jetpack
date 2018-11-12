73f54d9c7cc48e19196693adf59234c0e09761c7
Jason Johnston <jhnstn@users.noreply.github.com>
2018-11-12 10:07:46 -0800

    Update: add IS_ATOMIC autoload constant check https://github.com/Automattic/wpcomsh/pull/202
    * make check work on both pressable and v2
    
    * use is_atomic constant
    
    * version bump v2.3.36

--- projects/plugins/wpcomsh/wpcomsh-loader.php
+++ projects/plugins/wpcomsh/wpcomsh-loader.php
@@ -4,6 +4,7 @@ Plugin Name: WP.com Site Helper
 Description: WordPress.com provided functionality & tools
 */
 
-if ( defined( 'IS_PRESSABLE' ) && IS_PRESSABLE ) {
+if ( ( defined( 'IS_PRESSABLE' ) && IS_PRESSABLE ) ||
+     ( defined( 'IS_ATOMIC' ) && IS_ATOMIC ) ) {
   require_once( WPMU_PLUGIN_DIR . '/wpcomsh/wpcomsh.php' );
 }
