2b5e52651cea402009dcbfd69bb0648b966d1ef7
Jeff Bowen <jeff@automattic.com>
2018-11-12 11:01:52 -0500

    file picker

--- _inc/client/components/site-importer/index.jsx
+++ _inc/client/components/site-importer/index.jsx
@@ -1,11 +1,59 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { PureComponent } from 'react';
+import isEmpty from 'lodash/isEmpty';
+import get from 'lodash/get';
+import map from 'lodash/map';
 
-export default function SiteImporter( { active } ) {
-	if ( ! active ) {
-		return false;
+class SiteImporter extends PureComponent {
+	fileInput = {};
+
+	state = {
+		files: [],
+	};
+
+	setFileInputRef = element => {
+		this.fileInput = element;
+	};
+
+	onSubmit = ( event ) => {
+		event.preventDefault();
+		this.setState( {
+			files: get( this.fileInput, 'files', [] ),
+		} );
+	};
+
+	renderFilePicker = () => {
+		return <div>
+			hola mundo
+			<br />
+			<form onSubmit={ this.onSubmit } >
+				<input ref={ this.setFileInputRef } type="file" />
+				<button type="submit">Upload</button>
+			</form>
+		</div>;
 	}
-	return <div>hola mundo</div>;
+
+	render() {
+		const { active } = this.props;
+		const { files } = this.state;
+
+		if ( ! active ) {
+			return false;
+		}
+
+		if ( isEmpty( files ) ) {
+			return this.renderFilePicker();
+		}
+
+		return <div>
+			Uploading:
+			<ul>
+				{ map( files, file => file.name && <li>{ file.name }</li> ) }
+			</ul>
+		</div>;
+	}
 }
+
+export default SiteImporter;
