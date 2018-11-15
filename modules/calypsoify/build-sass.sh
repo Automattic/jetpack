7e187a0f6f6e681f9430bed48fb89f0197bff0d6
George Stephanis <George@Stephanis.info>
2018-11-15 12:16:15 -0600

    Some more small iterations on Calypsoify. (#10631)
    * Start migrating to new colors.
    
    * Swap to named and intelligible colors.  Names taken from Calypso and Core.
    
    * Switch back to .min instead of -min
    
    * Clear deprecated buildscript
    
    * Strip out built versions of the files.
    
    * Better break up the relevant bits, tidy up build tools.
    
    * paths are tricksy

--- modules/calypsoify/build-sass.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sass style.scss | perl -pe 's/^    /\t\t/g' | perl -pe 's/^  /\t/g' > style.css
-sass style-gutenberg.scss | perl -pe 's/^    /\t\t/g' | perl -pe 's/^  /\t/g' > style-gutenberg.css
