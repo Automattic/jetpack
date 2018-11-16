cf28ef0d2f42efbb965b0d25cf938c554fb576e1
Marin Atanasov <tyxla@abv.bg>
2018-11-16 12:37:26 +0200

    Add a TODO

--- class.jetpack-gutenberg.php
+++ class.jetpack-gutenberg.php
@@ -311,6 +311,7 @@ class Jetpack_Gutenberg {
 		wp_enqueue_style( 'jetpack-blocks-editor', $editor_style, array(), $version );
 
 		// The social-logos styles are used for Publicize service icons
+		// TODO: Remove when we ship the icons with the Gutenberg blocks build
 		wp_enqueue_style( 'social-logos' );
 	}
 }
