f2133f043495be31c0909f531d7d6d73eb1bfea0
Brandon Kraft <public@brandonkraft.com>
2018-11-19 18:40:06 -0600

    Photon: Do not image_downsize for REST API requests in the edit context (#10592)
    Photon: Do image_downsize for REST API requests on the wp/v2/media endpoint in the edit context.
    
    For tests, we need to clean the `Jetpack_Photon` state before each test, which is annoying because of the singleton/static class property. Unfortunately, the tests are not compatible with PHP 5.3, though the feature is.

--- tests/php/test_class.jetpack_photon.php
+++ tests/php/test_class.jetpack_photon.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * The tests require PHP 5.3 :( The feature does not.
+ *
+ * @requires PHP 5.3
+ */
 class WP_Test_Jetpack_Photon extends Jetpack_Attachment_Test_Case {
 	protected static $test_image;
 
@@ -8,15 +13,43 @@ class WP_Test_Jetpack_Photon extends Jetpack_Attachment_Test_Case {
 	public function setUp() {
 		parent::setUp();
 
+		// The version of PHPUnit we're using on TravisCI doesn't support @requires
+		// Skip manually.
+		if ( version_compare( phpversion(), '5.3', '<' ) ) {
+			$this->markTestSkipped( 'Testing the Jetpack_Photon singleton requires PHP 5.3' );
+		}
+
 		// Preserving global variables
 		global $content_width;
 		$this->_globals['content_width'] = $content_width;
+
+		// Setup the Photon filters.
+		// WP_UnitTestCase resets the action/filter state after
+		// every test:
+		// https://core.trac.wordpress.org/browser/trunk/tests/phpunit/includes/testcase.php?rev=43005#L273
+		// So we need to set these Photon filters for each test.
+		// see ::tearDown() ...
+		Jetpack_Photon::instance();
 	}
 
 	public function tearDown() {
+		// ::tearDown runs even if the test is skipped.
+		if ( version_compare( phpversion(), '5.3', '<' ) ) {
+			return parent::tearDown();
+		}
+
 		// Restoring global variables
 		global $content_width;
 		$content_width = $this->_globals['content_width'];
+
+		// ... see ::setUp()
+		// Unfortunately Jetpack_Photon::instance() won't run Jetpack_Photon->setup()
+		// each time Jetpack_Photon::instance() is called, since it's gated by a
+		// static variable.
+		// l337 h4X0Ring required:
+		$instance = new ReflectionProperty( 'Jetpack_Photon', '__instance' );
+		$instance->setAccessible( true );
+		$instance->setValue( null );
 
 		parent::tearDown();
 	}
@@ -869,5 +902,60 @@ class WP_Test_Jetpack_Photon extends Jetpack_Attachment_Test_Case {
 		return array(
 			'w' => '104'
 		);
+	}
+
+	/**
+	 * @group rest-api
+	 */
+	public function test_photon_cdn_in_rest_response_with_view_context() {
+		$test_image = $this->_get_image();
+
+		$request = new WP_REST_Request( 'GET', sprintf( '/wp/v2/media/%d', $test_image ) );
+		$request->set_query_params( array( 'context' => 'view' ) );
+		$response = rest_get_server()->dispatch( $request );
+		$data = $response->get_data();
+
+		$this->assertArrayHasKey( 'media_details', $data );
+		$this->assertArrayHasKey( 'sizes', $data['media_details'] );
+		$this->assertArrayHasKey( 'full', $data['media_details']['sizes'] );
+		$this->assertArrayHasKey( 'source_url', $data['media_details']['sizes']['full'] );
+
+		$this->assertContains( '?', $data['media_details']['sizes']['full']['source_url'] );
+	}
+
+	/**
+	 * @group rest-api
+	 */
+	public function test_photon_cdn_in_rest_response_with_edit_context() {
+		$test_image = $this->_get_image();
+
+		$admin = $this->factory->user->create( array( 'role' => 'administrator' ) );
+		wp_set_current_user( $admin );
+
+		$request = new WP_REST_Request( 'GET', sprintf( '/wp/v2/media/%d', $test_image ) );
+		$request->set_query_params( array( 'context' => 'edit' ) );
+		$response = rest_get_server()->dispatch( $request );
+		$data = $response->get_data();
+
+		$this->assertArrayHasKey( 'media_details', $data );
+		$this->assertArrayHasKey( 'sizes', $data['media_details'] );
+		$this->assertArrayHasKey( 'full', $data['media_details']['sizes'] );
+		$this->assertArrayHasKey( 'source_url', $data['media_details']['sizes']['full'] );
+
+		$this->assertNotContains( '?', $data['media_details']['sizes']['full']['source_url'] );
+
+
+		// Subsequent ?context=view requests should still be Photonized
+		$request = new WP_REST_Request( 'GET', sprintf( '/wp/v2/media/%d', $test_image ) );
+		$request->set_query_params( array( 'context' => 'view' ) );
+		$response = rest_get_server()->dispatch( $request );
+		$data = $response->get_data();
+
+		$this->assertArrayHasKey( 'media_details', $data );
+		$this->assertArrayHasKey( 'sizes', $data['media_details'] );
+		$this->assertArrayHasKey( 'full', $data['media_details']['sizes'] );
+		$this->assertArrayHasKey( 'source_url', $data['media_details']['sizes']['full'] );
+
+		$this->assertContains( '?', $data['media_details']['sizes']['full']['source_url'] );
 	}
 }
