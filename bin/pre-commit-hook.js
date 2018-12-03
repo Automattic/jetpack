098da76be5303aaf4d5913278b45ed46e00139c3
oskosk <oskosk@gmail.com>
2018-12-03 20:14:36 -0300

    Update bin/pre-commit-hook.js to not use eslint-eslines

--- bin/pre-commit-hook.js
+++ bin/pre-commit-hook.js
@@ -8,15 +8,50 @@ const gitFiles = execSync( 'git diff --cached --name-only --diff-filter=ACM' )
 	.toString()
 	.split( '\n' )
 	.map( name => name.trim() );
-const jsFiles = gitFiles.filter( name => name.startsWith( '_inc/client' ) )
-	.filter( name => name.endsWith( '.js' ) || name.endsWith( '.jsx' ) );
+
+/**
+ * Parses the output of a git diff command into javascript file paths.
+ *
+ * @param   {String} command Command to run. Expects output like `git diff --name-only […]`
+ * @returns {Array}          Paths output from git command
+ */
+function parseGitDiffToPathArray( command ) {
+	return execSync( command, { encoding: 'utf8' } )
+		.split( '\n' )
+		.map( name => name.trim() )
+		.filter( name => /\.(jsx?|scss)$/.test( name ) );
+}
+
+const dirtyFiles = new Set( parseGitDiffToPathArray( 'git diff --name-only --diff-filter=ACM' ) );
+const files = parseGitDiffToPathArray( 'git diff --cached --name-only --diff-filter=ACM' );
 const phpFiles = gitFiles.filter( name => name.endsWith( '.php' ) );
 
+dirtyFiles.forEach( file =>
+	// eslint-disable-next-line no-console
+	console.log(
+		chalk.red( `${ file } will not be auto-formatted because it has unstaged changes.` )
+	)
+);
+
 // linting should happen after formatting
-const jsLintResult = spawnSync( 'eslint-eslines', [ ...jsFiles, '--', '--diff=index' ], {
-	shell: true,
-	stdio: 'inherit',
-} );
+const toLint = files.filter( file => ! file.endsWith( '.scss' ) );
+if ( toLint.length ) {
+	const lintResult = spawnSync( './node_modules/.bin/eslint', [ '--quiet', ...toLint ], {
+		shell: true,
+		stdio: 'inherit',
+	} );
+	if ( lintResult.status ) {
+		// eslint-disable-next-line no-console
+		console.log(
+			chalk.red( 'COMMIT ABORTED:' ),
+			'The linter reported some problems. ' +
+				'If you are aware of them and it is OK, ' +
+				'repeat the commit command with --no-verify to avoid this check.'
+		);
+		// eslint-disable-next-line no-process-exit
+		process.exit( 1 );
+	}
+}
 
 let phpLintResult;
 if ( phpFiles.length > 0 ) {
@@ -26,12 +61,14 @@ if ( phpFiles.length > 0 ) {
 	} );
 }
 
-if ( jsLintResult.status || ( phpLintResult && phpLintResult.status ) ) {
+if ( ( phpLintResult && phpLintResult.status ) ) {
+	// eslint-disable-next-line no-console
 	console.log(
 		chalk.red( 'COMMIT ABORTED:' ),
 		'The linter reported some problems. ' +
 			'If you are aware of them and it is OK, ' +
 			'repeat the commit command with --no-verify to avoid this check.'
 	);
+	// eslint-disable-next-line no-process-exit
 	process.exit( 1 );
 }
