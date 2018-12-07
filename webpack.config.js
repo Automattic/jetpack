5d095040ff2ea10c53db1b3d4c3aa41a88bd5926
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2018-12-07 22:53:47 +0700

    Attempt to update babel dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -27,12 +27,29 @@ const webpackConfig = {
 					{
 						loader: 'babel-loader',
 						options: {
-							presets: [ 'es2015', 'stage-1', 'react' ],
+							presets: [
+								'@babel/preset-env',
+								'@babel/preset-react'
+							],
 							plugins: [
-								'transform-runtime',
-								'add-module-exports',
-								'transform-es3-member-expression-literals',
-								'transform-export-extensions'
+								[ '@babel/plugin-proposal-decorators', { legacy: true } ],
+								[ '@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' } ],
+								'@babel/plugin-proposal-class-properties',
+								'@babel/plugin-proposal-do-expressions',
+								'@babel/plugin-proposal-export-default-from',
+								'@babel/plugin-proposal-export-namespace-from',
+								'@babel/plugin-proposal-function-sent',
+								'@babel/plugin-proposal-json-strings',
+								'@babel/plugin-proposal-logical-assignment-operators',
+								'@babel/plugin-proposal-nullish-coalescing-operator',
+								'@babel/plugin-proposal-numeric-separator',
+								'@babel/plugin-proposal-optional-chaining',
+								'@babel/plugin-proposal-throw-expressions',
+								'@babel/plugin-syntax-dynamic-import',
+								'@babel/plugin-syntax-import-meta',
+								'@babel/plugin-transform-member-expression-literals',
+								'@babel/plugin-transform-react-jsx',
+								'@babel/plugin-transform-runtime'
 							]
 						}
 					},
