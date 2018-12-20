844819cf99198f46da28f111ef8e4762edaa7f1d
Marin Atanasov <tyxla@abv.bg>
2018-12-20 14:53:31 +0200

    Check WP_Block_Type_Registry existence before using it

--- tests/php/test_class.jetpack-gutenberg.php
+++ tests/php/test_class.jetpack-gutenberg.php
@@ -30,10 +30,12 @@ class WP_Test_Jetpack_Gutenberg extends WP_UnitTestCase {
 			wp_delete_user( $this->master_user_id );
 		}
 
-		$blocks = WP_Block_Type_Registry::get_instance()->get_all_registered();
-		foreach ( $blocks as $block_name => $block ) {
-			if ( strpos( $block_name, 'jetpack/' ) !== false ) {
-				unregister_block_type( $block_name );
+		if ( class_exists( 'WP_Block_Type_Registry' ) ) {
+			$blocks = WP_Block_Type_Registry::get_instance()->get_all_registered();
+			foreach ( $blocks as $block_name => $block ) {
+				if ( strpos( $block_name, 'jetpack/' ) !== false ) {
+					unregister_block_type( $block_name );
+				}
 			}
 		}
 	}
