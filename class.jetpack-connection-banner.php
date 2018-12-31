f4885f190a38df9c6af40573ffb94a0615e1adcd
oskosk <oskosk@gmail.com>
2018-12-31 10:31:48 -0300

    Address some whitespace issues

--- class.jetpack-connection-banner.php
+++ class.jetpack-connection-banner.php
@@ -169,8 +169,9 @@ class Jetpack_Connection_Banner {
 	function render_banner() { ?>
 		<div id="message" class="updated jp-wpcom-connect__container">
 			<div class="jp-wpcom-connect__container-top-text">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><rect x="0" fill="none" width="24" height="24"/><g><path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm1 15h-2v-2h2v2zm0-4h-2l-.5-6h3l-.5 6z"/></g></svg>
-					<span><?php esc_html_e( 'You’re almost done. Set up Jetpack to boost your site performance and get dozens of customization, marketing, and security tools.', 'jetpack' ); ?></span></div>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><rect x="0" fill="none" width="24" height="24"/><g><path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm1 15h-2v-2h2v2zm0-4h-2l-.5-6h3l-.5 6z"/></g></svg>
+				<span><?php esc_html_e( 'You’re almost done. Set up Jetpack to boost your site performance and get dozens of customization, marketing, and security tools.', 'jetpack' ); ?></span>
+			</div>
 			<div class="jp-wpcom-connect__inner-container">
 				<span
 					class="notice-dismiss connection-banner-dismiss"
@@ -230,9 +231,9 @@ class Jetpack_Connection_Banner {
 						<h2><?php esc_html_e( 'Jetpack simplifies website performance, security, customization, and management.', 'jetpack' ) ?></h2>
 
 						<div class="jp-wpcom-connect__content-icon jp-connect-illo">
-								<img src="<?php echo plugins_url( 'images/jetpack-powering-up.svg', JETPACK__PLUGIN_FILE ); ?>" alt="<?php
-									esc_attr_e(
-										'Jetpack premium services offer even more powerful performance, security, and revenue tools to help you keep your site safe, fast, and help generate income.',
+							<img src="<?php echo plugins_url( 'images/jetpack-powering-up.svg', JETPACK__PLUGIN_FILE ); ?>" alt="<?php
+								esc_attr_e(
+									'Jetpack premium services offer even more powerful performance, security, and revenue tools to help you keep your site safe, fast, and help generate income.',
 									'jetpack'
 								); ?>" height="auto" width="225" />
 						</div>
@@ -534,8 +535,8 @@ class Jetpack_Connection_Banner {
 
 						<div class="jp-wpcom-connect__content-icon jp-connect-illo">
 							<img src="<?php echo plugins_url( 'images/jetpack-welcome.svg', JETPACK__PLUGIN_FILE ); ?>" alt="<?php
-									esc_attr_e(
-										'Jetpack is a free plugin that utilizes powerful WordPress.com servers to enhance your site and simplify managing it.',
+								esc_attr_e(
+									'Jetpack is a free plugin that utilizes powerful WordPress.com servers to enhance your site and simplify managing it.',
 									'jetpack'
 								); ?>" height="auto" width="250" />
 						</div>
