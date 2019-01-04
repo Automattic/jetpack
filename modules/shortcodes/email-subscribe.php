23a83982f86c68e1cc55353877754218f7436871
artpi <artur.piszek@gmail.com>
2019-01-04 16:05:04 +0100

    File block comment fixes

--- modules/shortcodes/email-subscribe.php
+++ modules/shortcodes/email-subscribe.php
@@ -1,10 +1,12 @@
 <?php
 /**
  * Class Jetpack_Email_Subscribe
- * This class encapsulates a shortcode for subscribing to a MailChimp list.
- * It displays a simple signup form that gets an email address from the user and signs him for a list
- * for the Mailchimp list selected in the "Sharing" section of Calypso.
- * Other Email services can be implemented as well in the future.
+ * This class encapsulates:
+ * - a shortcode for subscribing to a MailChimp list.
+ * - a Gutenberg block for subscribing to a MailChimp list
+ * Both the shortcode and a block display a simple signup form with an "email" field.
+ * Submitting it subscribes the user to a MailChimp list selected in the "Sharing" section of Calypso.
+ * Other Email services and blocks can be implemented as well in the future.
  */
 class Jetpack_Email_Subscribe {
 
