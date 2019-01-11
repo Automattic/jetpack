cf99fb48f58172d3f19a1e18361acd2d0d8c8026
Jefferson Rabb <jeff@atavist.net>
2019-01-11 12:51:30 -0500

    Switch to server-side rendering for view only. This change is being made to address a WPCOM problem where certain tags (FORM, BUTTON, INPUT) are stripped out when post is saved.

--- modules/blocks.php
+++ modules/blocks.php
@@ -204,14 +204,58 @@ jetpack_register_block(
 /**
  * Mailchimp block registration/dependency declaration.
  *
- * @param array  $attr - Array containing the map block attributes.
- * @param string $content - String containing the map block content.
+ * @param array $attr - Array containing the map block attributes.
  *
  * @return string
  */
-function jetpack_mailchimp_block_load_assets( $attr, $content ) {
+function jetpack_mailchimp_block_load_assets( $attr ) {
+	$values  = array();
 	$blog_id = ( defined( 'IS_WPCOM' ) && IS_WPCOM ) ?
 		get_current_blog_id() : Jetpack_Options::get_option( 'id' );
-	Jetpack_Gutenberg::load_assets_as_required( 'mailchimp' );
-	return preg_replace( '/<div /', '<div data-blog-id="'. esc_attr( $blog_id ) .'" ', $content, 1 );
+	Jetpack_Gutenberg::load_assets_as_required( 'mailchimp', null );
+	$defaults = array(
+		'title'            => esc_html__( 'Join my email list', 'jetpack' ),
+		'emailPlaceholder' => esc_html__( 'Enter your email', 'jetpack' ),
+		'submitLabel'      => esc_html__( 'Join My Email List', 'jetpack' ),
+		'consentText'      => esc_html__( 'By clicking submit, you agree to share your email address with the site owner and MailChimp to receive marketing, updates, and other emails from the site owner. Use the unsubscribe link in those emails to opt out at any time.', 'jetpack' ),
+		'processingLabel'  => esc_html__( 'Processing...', 'jetpack' ),
+		'successLabel'     => esc_html__( 'Success! You\'ve been added to the list.', 'jetpack' ),
+		'errorLabel'       => esc_html__( 'Oh no! Unfortunately there was an error. Please try reloading this page and adding your email once more.', 'jetpack' ),
+	);
+	foreach ( $defaults as $id => $default ) {
+		$values[ $id ] = isset( $attr[ $id ] ) ? $attr[ $id ] : $default;
+	}
+	ob_start();
+	?>
+	<div class="wp-block-jetpack-mailchimp" data-blog-id="<?php echo( esc_attr( $blog_id ) ); ?>">
+		<div class="components-placeholder">
+			<h3><?php echo( esc_html( $values['title'] ) ); ?></h3>
+			<form>
+				<input
+					type="text"
+					class="components-text-control__input wp-block-jetpack-mailchimp-email"
+					required
+					placeholder="<?php echo( esc_attr( $values['emailPlaceholder'] ) ); ?>"
+				/>
+				<button type="submit" class="components-button is-button is-primary">
+					<?php echo( esc_html( $values['submitLabel'] ) ); ?>
+				</button>
+				<figcaption>
+					<?php echo( esc_html( $values['consentText'] ) ); ?>
+				</figcaption>
+			</form>
+			<div class="wp-block-jetpack-mailchimp-notification wp-block-jetpack-mailchimp-processing">
+				<?php echo( esc_html( $values['processingLabel'] ) ); ?>
+			</div>
+			<div class="wp-block-jetpack-mailchimp-notification wp-block-jetpack-mailchimp-success">
+				<?php echo( esc_html( $values['successLabel'] ) ); ?>
+			</div>
+			<div class="wp-block-jetpack-mailchimp-notification wp-block-jetpack-mailchimp-error">
+				<?php echo( esc_html( $values['errorLabel'] ) ); ?>
+			</div>
+		</div>
+	</div>
+	<?php
+	$html = ob_get_clean();
+	return $html;
 }
