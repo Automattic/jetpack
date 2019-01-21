a8bd9cea39da951189a883a8028090a1f9dabcc6
George Stephanis <georgestephanis@automattic.com>
2019-01-21 12:18:21 -0500

    Use noConflict mode, and some js tidying

--- modules/plugin-search/plugin-search.js
+++ modules/plugin-search/plugin-search.js
@@ -1,34 +1,42 @@
-jQuery( '#plugin-select-activate' ).click( function() {
-	ajaxActivateModule( window.jetpackModuleInfo.module );
-} );
 
-function ajaxActivateModule( moduleName ) {
-	var body = {};
-	body[ moduleName ] = true;
-	jQuery( '#plugin-select-activate' ).toggleClass( 'install-now updating-message' );
-	jQuery( '#plugin-select-activate' ).text( window.pluginSearchState.activatingString );
-	jQuery.ajax( {
-		url: '/wp-json/jetpack/v4/settings/',
-		method: 'post',
-		beforeSend: function( xhr ) {
-			xhr.setRequestHeader( 'X-WP-Nonce', window.pluginSearchState.jetpackWPNonce );
-		},
-		data: window.JSON.stringify( body ),
-		contentType: 'application/json',
-		dataType: 'json'
-	} ).done( function() {
-		updateButton();
-	} ).error( function() {
-		jQuery( '#plugin-select-activate' ).toggleClass( 'install-now updating-message' );
+( function( $, pSS ) {
+	const $pluginFilter = $( '#plugin-filter' );
+
+	$pluginFilter.on( 'click', 'button#plugin-select-activate', function( event ) {
+		event.preventDefault();
+		ajaxActivateModule( $( this ).data( 'module' ) );
 	} );
-}
 
-// Remove onclick handler, disable loading spinner, update button to redirect to module settings.
-function updateButton() {
-	jQuery( '#plugin-select-activate' ).prop( 'onclick', null ).off( 'click' );
-	jQuery( '#plugin-select-activate' ).toggleClass( 'install-now updating-message' );
-	jQuery( '#plugin-select-activate' ).text( window.pluginSearchState.activatedString );
-	setTimeout( function() {
-		jQuery( '#plugin-select-activate' ).replaceWith( '<a id="plugin-select-settings" class="button" href="' + window.jetpackModuleInfo.configure_url + '">' + window.pluginSearchState.manageSettingsString + '</a>' );
-	}, 1000 );
-}
+	function ajaxActivateModule( moduleName ) {
+		const body = {};
+		const $moduleBox = $pluginFilter.find( '#plugin-select-activate' );
+		body[ moduleName ] = true;
+		$moduleBox.toggleClass( 'install-now updating-message' );
+		$moduleBox.text( pSS.activatingString );
+		$.ajax( {
+			url: '/wp-json/jetpack/v4/settings/',
+			method: 'post',
+			beforeSend: function( xhr ) {
+				xhr.setRequestHeader( 'X-WP-Nonce', pSS.jetpackWPNonce );
+			},
+			data: window.JSON.stringify( body ),
+			contentType: 'application/json',
+			dataType: 'json'
+		} ).done( function() {
+			updateButton();
+		} ).error( function() {
+			$( '#plugin-select-activate' ).toggleClass( 'install-now updating-message' );
+		} );
+	}
+
+	// Remove onclick handler, disable loading spinner, update button to redirect to module settings.
+	function updateButton() {
+		const $moduleBox = $pluginFilter.find( '#plugin-select-activate' );
+		$moduleBox.prop( 'onclick', null ).off( 'click' );
+		$moduleBox.toggleClass( 'install-now updating-message' );
+		$moduleBox.text( pSS.activatedString );
+		setTimeout( function() {
+			$moduleBox.replaceWith( '<a id="plugin-select-settings" class="button" href="' + window.jetpackModuleInfo.configure_url + '">' + pSS.manageSettingsString + '</a>' );
+		}, 1000 );
+	}
+} )( jQuery, window.pluginSearchState );
