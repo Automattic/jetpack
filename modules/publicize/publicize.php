50bf83cdb1156da0a7d3bb33041a629a3571d7e8
belcherj <belcherj@gmail.com>
2019-01-22 22:27:12 -0500

    Manually push change

--- modules/publicize/publicize.php
+++ modules/publicize/publicize.php
@@ -498,7 +498,7 @@ abstract class Publicize_Base {
 					if ( ! $this->is_valid_facebook_connection( $connection ) ) {
 						$connection_test_passed = false;
 						$user_can_refresh = false;
-						$connection_test_message = __( 'Facebook no longer supports Publicize connections to Facebook Profiles, but you can still connect Facebook Pages. Please select a Facebook Page to publish updates to.' );
+						$connection_test_message = __( 'Please select a Facebook page to publish updates.' );
 					}
 				}
 
