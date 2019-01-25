53ac5279e851d329cf6a10e285d6864b17a873d7
Jeremy Herve <jeremy@jeremy.hu>
2019-01-25 11:38:24 +0100

    Multisite connection banner: update link text and class
    See https://github.com/Automattic/jetpack/pull/11189#issuecomment-457408570

--- views/admin/must-connect-main-blog.php
+++ views/admin/must-connect-main-blog.php
@@ -1,13 +1,13 @@
 <div class="wrap">
 	<div class="jetpack-wrap-container dops-card">
 		<div class="jetpack-text-container">
-			<h1><?php _e( 'Get started with Jetpack Multisite', 'jetpack' ); ?></h1>
+			<h1><?php esc_html_e( 'Get started with Jetpack Multisite', 'jetpack' ); ?></h1>
 			<p>
-				<?php _e( 'Get started managing your Multisite install of Jetpack by connecting.', 'jetpack' ) ?>
+				<?php esc_html_e( 'Get started managing your Multisite install of Jetpack by connecting.', 'jetpack' ); ?>
 			</p>
 		</div>
 		<div class="jetpack-install-container">
-			<p class="submit"><a href="<?php echo esc_url( $data['url'] ); ?>" class="button-connector" id="wpcom-connect"><?php _e( 'Connect to WordPress.com', 'jetpack' ); ?></a></p>
+			<p class="submit"><a href="<?php echo esc_url( $data['url'] ); ?>" class="button-connector dops-button is-primary" id="wpcom-connect"><?php esc_html_e( 'Set up Jetpack', 'jetpack' ); ?></a></p>
 			<p class="jetpack-install-blurb">
 				<?php jetpack_render_tos_blurb(); ?>
 			</p>
