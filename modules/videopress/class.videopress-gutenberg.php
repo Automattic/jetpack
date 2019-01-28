8450d19e02c1b68ce614cc0e62674ea968696085
mmtr <miguel.torres@automattic.com>
2019-01-28 20:21:05 +0100

    Use `get_current_blog_id` in WP.com environments

--- modules/videopress/class.videopress-gutenberg.php
+++ modules/videopress/class.videopress-gutenberg.php
@@ -62,7 +62,12 @@ class VideoPress_Gutenberg {
 			return $content;
 		}
 
-		$blog_id = Jetpack_Options::get_option( 'id' );
+		if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
+			$blog_id = get_current_blog_id();
+		} else {
+			$blog_id = Jetpack_Options::get_option( 'id' );
+		}
+
 		$post_id = absint( $attributes['id'] );
 		$videopress_id = video_get_info_by_blogpostid( $blog_id, $post_id )->guid;
 		$videopress_data = videopress_get_video_details( $videopress_id );
