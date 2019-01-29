09b077ba6c39b3904cb080693edc0d1afe8438d6
Jeremy Herve <jeremy@tagada.hu>
2019-01-29 08:40:30 +0100

    Publicize: shortcircuit API when Publicize isn't available anymore (#11170)
    * Publicize: shortcircuit API when Publicize isn't available anymore
    
    Fixes #10727
    
    In some cases (like site previously connected but put in dev mode),
    Publicize can be active but not available.
    In such cases, let's shortcircuit the API.
    
    * Publicize: shortcircuit API when Publicize isn't available anymore
    
    * Looser check for non-existance of Publicize
    * Check in each functionn `$GLOBALS['publicize']` is used (shouldn't
      happen in these other functions).

--- _inc/lib/core-api/wpcom-fields/post-fields-publicize-connections.php
+++ _inc/lib/core-api/wpcom-fields/post-fields-publicize-connections.php
@@ -115,6 +115,14 @@ class WPCOM_REST_API_V2_Post_Publicize_Connections_Field extends WPCOM_REST_API_
 	function permission_check( $post_id ) {
 		global $publicize;
 
+		if ( ! $publicize ) {
+			return new WP_Error(
+				'publicize_not_available',
+				__( 'Sorry, Publicize is not available on your site right now.', 'jetpack' ),
+				array( 'status' => rest_authorization_required_code() )
+			);
+		}
+
 		if ( $publicize->current_user_can_access_publicize_data( $post_id ) ) {
 			return true;
 		}
@@ -159,6 +167,10 @@ class WPCOM_REST_API_V2_Post_Publicize_Connections_Field extends WPCOM_REST_API_
 	 */
 	public function get( $post_array, $request ) {
 		global $publicize;
+
+		if ( ! $publicize ) {
+			return array();
+		}
 
 		$schema     = $this->post_connection_schema();
 		$properties = array_keys( $schema['properties'] );
@@ -237,6 +249,10 @@ class WPCOM_REST_API_V2_Post_Publicize_Connections_Field extends WPCOM_REST_API_
 
 	protected function get_meta_to_update( $requested_connections, $post_id = 0 ) {
 		global $publicize;
+
+		if ( ! $publicize ) {
+			return array();
+		}
 
 		if ( isset( $this->memoized_updates[$post_id] ) ) {
 			return $this->memoized_updates[$post_id];
