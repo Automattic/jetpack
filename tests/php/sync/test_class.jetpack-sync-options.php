7d071ac813fc6670e0d12916c01665f35612f8e1
Enej Bajgoric <enej.bajgoric@gmail.com>
2019-01-29 06:50:30 -0800

    Revert option sync

--- tests/php/sync/test_class.jetpack-sync-options.php
+++ tests/php/sync/test_class.jetpack-sync-options.php
@@ -197,8 +197,6 @@ class WP_Test_Jetpack_Sync_Options extends WP_Test_Jetpack_Sync_Base {
 			'wordads_display_page'                 => '1',
 			'wordads_display_archive'              => '1',
 			'wordads_custom_adstxt'                => 'pineapple',
-			'paused_plugins'                       => array( 'slug'=> 'error' ),
-			'paused_themes'                        => array( 'slug'=> 'error' ),
 		);
 
 		$theme_mod_key             = 'theme_mods_' . get_option( 'stylesheet' );
