56c349fcfffed90947d860037b2e03153ec239a7
Rocco Tripaldi <tripaldir@gmail.com>
2019-02-05 13:19:26 -0500

    Server Side: Business Hours block from George's Random Blocks (#10998)
    * an endpoint to get business hours info
    
    * Register the business hours block in blocks.php

--- modules/blocks.php
+++ modules/blocks.php
@@ -20,7 +20,7 @@ jetpack_register_block(
 /**
  * Map block registration/dependency declaration.
  *
- * @param array  $attr - Array containing the map block attributes.
+ * @param array $attr - Array containing the map block attributes.
  * @param string $content - String containing the map block content.
  *
  * @return string
@@ -35,14 +35,16 @@ function jetpack_map_block_load_assets( $attr, $content ) {
 	$api_key = Jetpack_Options::get_option( 'mapbox_api_key' );
 
 	Jetpack_Gutenberg::load_assets_as_required( 'map', $dependencies );
-	return preg_replace( '/<div /', '<div data-api-key="'. esc_attr( $api_key ) .'" ', $content, 1 );
+
+	return preg_replace( '/<div /', '<div data-api-key="' . esc_attr( $api_key ) . '" ', $content, 1 );
 }
+
 
 /**
  * Tiled Gallery block. Depends on the Photon module.
  *
  * @since 6.9.0
-*/
+ */
 if (
 	( defined( 'IS_WPCOM' ) && IS_WPCOM ) ||
 	class_exists( 'Jetpack_Photon' ) && Jetpack::is_module_active( 'photon' )
@@ -124,20 +126,23 @@ function jetpack_gif_block_render( $attr ) {
 
 	ob_start();
 	?>
-	<div class="<?php echo esc_attr( implode( $classes, ' ' ) ); ?>">
-		<figure>
-			<div class="wp-block-jetpack-gif-wrapper" style="<?php echo esc_attr( $style ); ?>">
-				<iframe src="<?php echo esc_url( $giphy_url ); ?>" title="<?php echo esc_attr( $search_text ); ?>"></iframe>
-			</div>
+    <div class="<?php echo esc_attr( implode( $classes, ' ' ) ); ?>">
+        <figure>
+            <div class="wp-block-jetpack-gif-wrapper" style="<?php echo esc_attr( $style ); ?>">
+                <iframe src="<?php echo esc_url( $giphy_url ); ?>"
+                        title="<?php echo esc_attr( $search_text ); ?>"></iframe>
+            </div>
 			<?php if ( $caption ) : ?>
-				<figcaption class="wp-block-jetpack-gif-caption gallery-caption"><?php echo wp_kses_post( $caption ); ?></figcaption>
+                <figcaption
+                        class="wp-block-jetpack-gif-caption gallery-caption"><?php echo wp_kses_post( $caption ); ?></figcaption>
 			<?php endif; ?>
-		</figure>
-	</div>
+        </figure>
+    </div>
 	<?php
 	$html = ob_get_clean();
 
 	Jetpack_Gutenberg::load_assets_as_required( 'gif' );
+
 	return $html;
 }
 
@@ -176,7 +181,7 @@ jetpack_register_block(
 /**
  * Slideshow block registration/dependency declaration.
  *
- * @param array  $attr - Array containing the map block attributes.
+ * @param array $attr - Array containing the map block attributes.
  * @param string $content - String containing the map block content.
  *
  * @return string
@@ -188,5 +193,87 @@ function jetpack_slideshow_block_load_assets( $attr, $content ) {
 		'wp-i18n',
 	);
 	Jetpack_Gutenberg::load_assets_as_required( 'slideshow', $dependencies );
+
+	return $content;
+}
+
+/**
+ * Business Hours Block.
+ */
+jetpack_register_block(
+	'business-hours',
+	array( 'render_callback' => 'jetpack_business_hours_render' )
+);
+/**
+ * Business Hours Block dynamic rending of the glock.
+ *
+ * @param array $attr - Array containing the business hours block attributes.
+ * @param string $content - String containing the business hours block content.
+ *
+ * @return string
+ */
+function jetpack_business_hours_render( $attributes, $content ) {
+	global $wp_locale;
+
+	if ( empty( $attributes['hours'] ) || ! is_array( $attributes['hours'] ) ) {
+		return $content;
+	}
+
+	$start_of_week     = (int) get_option( 'start_of_week', 0 );
+	$time_format       = get_option( 'time_format' );
+	$today             = current_time( 'D' );
+	$custom_class_name = isset( $attributes['className'] ) ? $attributes['className'] : '';
+	$content           = sprintf(
+		'<dl class="jetpack-business-hours %s">',
+		! empty( $attributes['className'] ) ? esc_attr( $attributes['className'] ) : ''
+	);
+
+	$days = array( 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' );
+
+	if ( $start_of_week ) {
+		$chunk1              = array_slice( $attributes['hours'], 0, $start_of_week );
+		$chunk2              = array_slice( $attributes['hours'], $start_of_week );
+		$attributes['hours'] = array_merge( $chunk2, $chunk1 );
+	}
+
+	foreach ( $attributes['hours'] as $day => $hours ) {
+		$opening = strtotime( $hours['opening'] );
+		$closing = strtotime( $hours['closing'] );
+
+		$content .= '<dt class="' . esc_attr( $day ) . '">' .
+		            ucfirst( $wp_locale->get_weekday( array_search( $day, $days ) ) ) .
+		            '</dt>';
+		$content .= '<dd class="' . esc_attr( $day ) . '">';
+		if ( $hours['opening'] && $hours['closing'] ) {
+			$content .= sprintf(
+				_x( 'From %1$s to %2$s', 'from business opening hour to closing hour', 'jetpack' ),
+				date( $time_format, $opening ),
+				date( $time_format, $closing )
+			);
+
+			if ( $today === $day ) {
+				$now = strtotime( current_time( 'H:i' ) );
+				if ( $now < $opening ) {
+					$content .= '<br />';
+					$content .= esc_html( sprintf(
+						_x( 'Opening in %s', 'Amount of time until business opens', 'jetpack' ),
+						human_time_diff( $now, $opening )
+					) );
+				} elseif ( $now >= $opening && $now < $closing ) {
+					$content .= '<br />';
+					$content .= esc_html( sprintf(
+						_x( 'Closing in %s', 'Amount of time until business closes', 'jetpack' ),
+						human_time_diff( $now, $closing )
+					) );
+				}
+			}
+		} else {
+			$content .= esc_html__( 'CLOSED', 'jetpack' );
+		}
+		$content .= '</dd>';
+	}
+
+	$content .= '</dl>';
+
 	return $content;
 }
