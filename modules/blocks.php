f39bb8427f62d2de974333fa68742604ee8a3694
Jefferson Rabb <j.max.rabb@gmail.com>
2019-02-07 07:39:50 -0500

    Mailchimp Block: Non Server-Side Render Approach (#11092)
    * Register Mailchimp block in simpler way, beginnings of API to determine whether Mailchimp list has been connected.
    
    * Remove unnecessary API, add blog_id to Jetpack_Editor_Initial_State global.
    
    * Mailchimp API endpoint in Jetpack.
    
    * Reinstate the shortcode PHP, removing only the Block registration line.
    
    * Revert "Reinstate the shortcode PHP, removing only the Block registration line."
    
    This reverts commit 366a283f8616afaeb371d51c8c021a3922f8e387.
    
    * Reinstate shortcode PHP, with block registration removed.
    
    * Revised API for determining Mailchimp connection status. Meant to be used as-is in Jetpack and WPCOM.
    
    * Switch to server-side rendering for view only. This change is being made to address a WPCOM problem where certain tags (FORM, BUTTON, INPUT) are stripped out when post is saved.
    
    * Handle errors when making server-side API call to WPCOM.
    
    * Remove unused block registration function.
    
    * Set wpcom-only on endpoint
    
    This was a change from review of D22995-code
    
    * Replace ... with …
    
    * Remove parens around echo
    
    * Change the data approach. According to https://a8c.slack.com/archives/C8A2EUTB9/p1548673654344800 discussion, this is trying out the approach that does not reach to wpcom on setup of the block. This uses local option that is already synced on list selection
    
    * Lint and spacing fixes
    
    * Correction to planned Jetpack release number.
    
    * Helper function to put together block classes, including alignment and additional CSS classes.
    
    * Check for WP.com or Jetpack is_active before registering Mailchimp block.
    
    * Return an error if site_id is unavailable.
    
    * Fix lint (equals alignment)
    
    * Remove shared function for server-side rendered block classes and move the logic back to the blocks' reader_callback functions. To be examined in a separate PR.
    
    * Encode site ID in Mailchimp API's connect URL.

--- modules/blocks.php
+++ modules/blocks.php
@@ -105,7 +105,6 @@ jetpack_register_block(
  * @return string
  */
 function jetpack_gif_block_render( $attr ) {
-	$align       = isset( $attr['align'] ) ? $attr['align'] : 'center';
 	$padding_top = isset( $attr['paddingTop'] ) ? $attr['paddingTop'] : 0;
 	$style       = 'padding-top:' . $padding_top;
 	$giphy_url   = isset( $attr['giphyUrl'] ) ? $attr['giphyUrl'] : null;
@@ -116,17 +115,21 @@ function jetpack_gif_block_render( $attr ) {
 		return null;
 	}
 
+	/* TODO: replace with centralized block_class function */
+	$align   = isset( $attr['align'] ) ? $attr['align'] : 'center';
+	$type    = 'gif';
 	$classes = array(
-		'wp-block-jetpack-gif',
+		'wp-block-jetpack-' . $type,
 		'align' . $align,
 	);
 	if ( isset( $attr['className'] ) ) {
 		array_push( $classes, $attr['className'] );
 	}
+	$classes = implode( $classes, ' ' );
 
 	ob_start();
 	?>
-	<div class="<?php echo esc_attr( implode( $classes, ' ' ) ); ?>">
+	<div class="<?php echo esc_attr( $classes ); ?>">
 		<figure>
 			<div class="wp-block-jetpack-gif-wrapper" style="<?php echo esc_attr( $style ); ?>">
 				<iframe src="<?php echo esc_url( $giphy_url ); ?>"
@@ -298,4 +301,88 @@ function jetpack_business_hours_render( $attributes, $content ) {
 	$content .= '</dl>';
 
 	return $content;
+}
+
+/**
+ * Mailchimp Block.
+ */
+if ( ( defined( 'IS_WPCOM' ) && IS_WPCOM ) || Jetpack::is_active() ) {
+	jetpack_register_block(
+		'mailchimp',
+		array(
+			'render_callback' => 'jetpack_mailchimp_block_load_assets',
+		)
+	);
+}
+
+/**
+ * Mailchimp block registration/dependency declaration.
+ *
+ * @param array $attr - Array containing the map block attributes.
+ *
+ * @return string
+ */
+function jetpack_mailchimp_block_load_assets( $attr ) {
+	$values  = array();
+	$blog_id = ( defined( 'IS_WPCOM' ) && IS_WPCOM ) ?
+		get_current_blog_id() : Jetpack_Options::get_option( 'id' );
+	Jetpack_Gutenberg::load_assets_as_required( 'mailchimp', null );
+	$defaults = array(
+		'title'            => esc_html__( 'Join my email list', 'jetpack' ),
+		'emailPlaceholder' => esc_html__( 'Enter your email', 'jetpack' ),
+		'submitLabel'      => esc_html__( 'Join My Email List', 'jetpack' ),
+		'consentText'      => esc_html__( 'By clicking submit, you agree to share your email address with the site owner and MailChimp to receive marketing, updates, and other emails from the site owner. Use the unsubscribe link in those emails to opt out at any time.', 'jetpack' ),
+		'processingLabel'  => esc_html__( 'Processing…', 'jetpack' ),
+		'successLabel'     => esc_html__( 'Success! You\'ve been added to the list.', 'jetpack' ),
+		'errorLabel'       => esc_html__( 'Oh no! Unfortunately there was an error. Please try reloading this page and adding your email once more.', 'jetpack' ),
+	);
+	foreach ( $defaults as $id => $default ) {
+		$values[ $id ] = isset( $attr[ $id ] ) ? $attr[ $id ] : $default;
+	}
+
+	/* TODO: replace with centralized block_class function */
+	$align   = isset( $attr['align'] ) ? $attr['align'] : 'center';
+	$type    = 'mailchimp';
+	$classes = array(
+		'wp-block-jetpack-' . $type,
+		'align' . $align,
+	);
+	if ( isset( $attr['className'] ) ) {
+		array_push( $classes, $attr['className'] );
+	}
+	$classes = implode( $classes, ' ' );
+
+	ob_start();
+	?>
+	<div class="<?php echo esc_attr( $classes ); ?>" data-blog-id="<?php echo esc_attr( $blog_id ); ?>">
+		<div class="components-placeholder">
+			<h3><?php echo esc_html( $values['title'] ); ?></h3>
+			<form>
+				<input
+					type="text"
+					class="components-text-control__input wp-block-jetpack-mailchimp-email"
+					required
+					placeholder="<?php echo esc_attr( $values['emailPlaceholder'] ); ?>"
+				/>
+				<button type="submit" class="components-button is-button is-primary">
+					<?php echo esc_html( $values['submitLabel'] ); ?>
+				</button>
+				<figcaption>
+					<?php echo esc_html( $values['consentText'] ); ?>
+				</figcaption>
+			</form>
+			<div class="wp-block-jetpack-mailchimp-notification wp-block-jetpack-mailchimp-processing">
+				<?php echo esc_html( $values['processingLabel'] ); ?>
+			</div>
+			<div class="wp-block-jetpack-mailchimp-notification wp-block-jetpack-mailchimp-success">
+				<?php echo esc_html( $values['successLabel'] ); ?>
+			</div>
+			<div class="wp-block-jetpack-mailchimp-notification wp-block-jetpack-mailchimp-error">
+				<?php echo esc_html( $values['errorLabel'] ); ?>
+			</div>
+		</div>
+	</div>
+	<?php
+	$html = ob_get_clean();
+	return $html;
 }
