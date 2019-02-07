3ad5d1fe8f43525b86da4273c1d3b45596465334
mmtr <miguel.torres@automattic.com>
2019-02-07 12:12:49 +0100

    Remove the field for non videos

--- _inc/lib/core-api/wpcom-fields/attachment-fields-videopress.php
+++ _inc/lib/core-api/wpcom-fields/attachment-fields-videopress.php
@@ -1,38 +1,64 @@
 <?php
+/**
+ * Extend the REST API functionality for VideoPress users.
+ *
+ * @package Jetpack
+ */
 
 /**
  * Add per-attachment VideoPress data.
  *
  * { # Attachment Object
  *   ...
- *   jetpack_videopress: (object) VideoPress metadata for the specified attachment
+ *   jetpack_videopress_guid: (string) VideoPress identifier
  *   ...
  * }
  *
  * @since 7.1.0
  */
 class WPCOM_REST_API_V2_Attachment_VideoPress_Field extends WPCOM_REST_API_V2_Field_Controller {
+	/**
+	 * The REST Object Type to which the jetpack_videopress_guid field will be added.
+	 *
+	 * @var string
+	 */
 	protected $object_type = 'attachment';
-	protected $field_name  = 'jetpack_videopress';
+
+	/**
+	 * The name of the REST API field to add.
+	 *
+	 * @var string $field_name
+	 */
+	protected $field_name = 'jetpack_videopress_guid';
+
+	/**
+	 * Registers the jetpack_videopress field and adds a filter to remove it for attachments that are not videos.
+	 */
+	public function register_fields() {
+		parent::register_fields();
+
+		add_filter( 'rest_prepare_attachment', array( $this, 'remove_field_for_non_videos' ), 10, 2 );
+	}
 
 	/**
 	 * Defines data structure and what elements are visible in which contexts
 	 */
 	public function get_schema() {
 		return array(
-			'$schema'    => 'http://json-schema.org/draft-04/schema#',
-			'title'      => $this->field_name,
-			'type'       => 'object|null',
-			'context'    => array( 'view', 'edit' ),
-			'default'    => null,
+			'$schema' => 'http://json-schema.org/draft-04/schema#',
+			'title'   => $this->field_name,
+			'type'    => 'string|null',
+			'context' => array( 'view', 'edit' ),
+			'default' => null,
 		);
 	}
 
 	/**
 	 * Getter: Retrieve current VideoPress data for a given attachment.
 	 *
-	 * @param mixed           $object Response from the attachment endpoint
-	 * @param WP_REST_Request $request
+	 * @param mixed           $object  Response from the attachment endpoint.
+	 * @param WP_REST_Request $request Request to the attachment endpoint.
+	 *
 	 * @return object
 	 */
 	public function get( $object, $request ) {
@@ -44,36 +70,64 @@ class WPCOM_REST_API_V2_Attachment_VideoPress_Field extends WPCOM_REST_API_V2_Fi
 
 		$post_id = absint( $object['id'] );
 
-		$videopress_data = $this->get_videopress_data( $post_id, $blog_id );
+		$videopress_guid = $this->get_videopress_guid( $post_id, $blog_id );
 
-		if ( ! $videopress_data || is_wp_error( $videopress_data ) ) {
+		if ( ! $videopress_guid ) {
 			return null;
 		}
 
-		return $videopress_data;
+		return $videopress_guid;
 	}
 
 	/**
-	 * Get VideoPress data for a given attachment.
+	 * Gets the VideoPress GUID for a given attachment.
 	 *
 	 * This is pulled out into a separate method to support unit test mocking.
 	 *
-	 * @param int $attachment_id Attachment ID
-	 * @param int $blog_id       Blog ID
+	 * @param int $attachment_id Attachment ID.
+	 * @param int $blog_id Blog ID.
 	 *
 	 * @return object
 	 */
-	public function get_videopress_data( $attachment_id, $blog_id ) {
-		$videopress_id = video_get_info_by_blogpostid( $blog_id, $attachment_id )->guid;
-		return videopress_get_video_details( $videopress_id );
+	public function get_videopress_guid( $attachment_id, $blog_id ) {
+		return video_get_info_by_blogpostid( $blog_id, $attachment_id )->guid;
+	}
+
+	/**
+	 * Checks if the given attachment is a video.
+	 *
+	 * @param object $attachment The attachment object.
+	 *
+	 * @return false|int
+	 */
+	public function is_video( $attachment ) {
+		return preg_match( '@^video/@', $attachment->post_mime_type );
+	}
+
+	/**
+	 * Removes the jetpack_videopress_guid field from the response if the
+	 * given attachment is not a video.
+	 *
+	 * @param WP_REST_Response $response Response from the attachment endpoint.
+	 * @param WP_Post          $attachment The original attachment object.
+	 *
+	 * @return mixed
+	 */
+	public function remove_field_for_non_videos( $response, $attachment ) {
+		if ( ! $this->is_video( $attachment ) ) {
+			unset( $response->data[ $this->field_name ] );
+		}
+
+		return $response;
 	}
 
 	/**
 	 * Setter: It does nothing since `jetpack_videopress` is a read-only field.
 	 *
 	 * @param mixed           $value The new value for the field.
-	 * @param WP_Post         $object The attachment object
-	 * @param WP_REST_Request $request
+	 * @param WP_Post         $object The attachment object.
+	 * @param WP_REST_Request $request The request object.
+	 *
 	 * @return null
 	 */
 	public function update( $value, $object, $request ) {
@@ -84,8 +138,9 @@ class WPCOM_REST_API_V2_Attachment_VideoPress_Field extends WPCOM_REST_API_V2_Fi
 	 * Permission Check for the field's getter. Delegate the responsibility to the
 	 * attachment endpoint, so it always returns true.
 	 *
-	 * @param mixed           $object Response from the attachment endpoint
-	 * @param WP_REST_Request $request
+	 * @param mixed           $object Response from the attachment endpoint.
+	 * @param WP_REST_Request $request Request to the attachment endpoint.
+	 *
 	 * @return true
 	 */
 	public function get_permission_check( $object, $request ) {
@@ -97,8 +152,9 @@ class WPCOM_REST_API_V2_Attachment_VideoPress_Field extends WPCOM_REST_API_V2_Fi
 	 * attachment endpoint, so it always returns true.
 	 *
 	 * @param mixed           $value The new value for the field.
-	 * @param WP_Post         $object The attachment object
-	 * @param WP_REST_Request $request
+	 * @param WP_Post         $object The attachment object.
+	 * @param WP_REST_Request $request Request to the attachment endpoint.
+	 *
 	 * @return true
 	 */
 	public function update_permission_check( $value, $object, $request ) {
