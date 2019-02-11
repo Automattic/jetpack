0822c7150506e84662168b607f2395592050e197
Weston Ruter <westonruter@google.com>
2019-02-11 18:01:33 +0100

    Add default values for width and height

--- extensions/blocks/gif/gif.php
+++ extensions/blocks/gif/gif.php
@@ -28,12 +28,8 @@ function jetpack_gif_block_render( $attr ) {
 	$search_text = isset( $attr['searchText'] ) ? $attr['searchText'] : '';
 	$caption     = isset( $attr['caption'] ) ? $attr['caption'] : null;
 
-	if ( ! $giphy_url ) {
-		return null;
-	}
-
 	$giphy_id = null;
-	if ( ! preg_match( '#^' . preg_quote( 'https://giphy.com/embed/', '#' ) . '(\w+)#', $giphy_url, $matches ) ) {
+	if ( ! $giphy_url || ! preg_match( '#^' . preg_quote( 'https://giphy.com/embed/', '#' ) . '(\w+)#', $giphy_url, $matches ) ) {
 		return null;
 	}
 
@@ -52,13 +48,13 @@ function jetpack_gif_block_render( $attr ) {
 	global $wp_embed;
 	$embed_html = $wp_embed->shortcode( array(), $giphy_url );
 
-	$width = null;
+	$width = 250; // @todo Is this the right default?
 	if ( preg_match( '/width="(\d+)"/', $embed_html, $matches ) ) {
-		$width = $matches[1];
+		$width = (int) $matches[1];
 	}
-	$height = null;
+	$height = 250; // @todo Is this the right default?
 	if ( preg_match( '/height="(\d+)"/', $embed_html, $matches ) ) {
-		$height = $matches[1];
+		$height = (int) $matches[1];
 	}
 
 	$placeholder = preg_replace( '#<img.*?alt="(.*?)".*?>#', '$1', $embed_html );
@@ -67,7 +63,7 @@ function jetpack_gif_block_render( $attr ) {
 	?>
 	<div class="<?php echo esc_attr( $classes ); ?>">
 		<figure>
-			<?php if ( Jetpack_AMP_Support::is_amp_request() && $width && $height ) : ?>
+			<?php if ( Jetpack_AMP_Support::is_amp_request() ) : ?>
 				<amp-iframe src="<?php echo esc_url( $giphy_url ); ?>" width="<?php echo esc_attr( $width ); ?>" height="<?php echo esc_attr( $height ); ?>" sandbox="allow-scripts allow-same-origin" layout="responsive">
 					<div placeholder>
 						<?php echo wp_kses_post( $placeholder ); ?>
