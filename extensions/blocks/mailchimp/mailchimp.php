bc074c8a92c9d922a2e0c141aff4a8c67103d32e
Jefferson Rabb <j.max.rabb@gmail.com>
2019-02-13 13:41:13 -0500

    Mailchimp Block: UI Review Updates (#11317)
    * UI Review: Remove title from Mailchimp block.
    
    * Remove consent text className. Use of SMALL is sufficient styling.
    
    * wp_kses_post() for consent text.
    
    * Support for HTML tags in Submit button text, including line breaks.

--- extensions/blocks/mailchimp/mailchimp.php
+++ extensions/blocks/mailchimp/mailchimp.php
@@ -29,10 +29,9 @@ function jetpack_mailchimp_block_load_assets( $attr ) {
 		get_current_blog_id() : Jetpack_Options::get_option( 'id' );
 	Jetpack_Gutenberg::load_assets_as_required( 'mailchimp', null );
 	$defaults = array(
-		'title'            => esc_html__( 'Join my email list', 'jetpack' ),
 		'emailPlaceholder' => esc_html__( 'Enter your email', 'jetpack' ),
 		'submitLabel'      => esc_html__( 'Join my email list', 'jetpack' ),
-		'consentText'      => esc_html__( 'By clicking submit, you agree to share your email address with the site owner and MailChimp to receive marketing, updates, and other emails from the site owner. Use the unsubscribe link in those emails to opt out at any time.', 'jetpack' ),
+		'consentText'      => esc_html__( 'By clicking submit, you agree to share your email address with the site owner and Mailchimp to receive marketing, updates, and other emails from the site owner. Use the unsubscribe link in those emails to opt out at any time.', 'jetpack' ),
 		'processingLabel'  => esc_html__( 'Processing…', 'jetpack' ),
 		'successLabel'     => esc_html__( 'Success! You\'re on the list.', 'jetpack' ),
 		'errorLabel'       => esc_html__( 'Whoops! There was an error and we couldn\'t process your subscription. Please reload the page and try again.', 'jetpack' ),
@@ -57,28 +56,28 @@ function jetpack_mailchimp_block_load_assets( $attr ) {
 	?>
 	<div class="<?php echo esc_attr( $classes ); ?>" data-blog-id="<?php echo esc_attr( $blog_id ); ?>">
 		<div class="components-placeholder">
-			<h3><?php echo esc_html( $values['title'] ); ?></h3>
 			<form>
 				<input
-					type="text"
-					class="components-text-control__input wp-block-jetpack-mailchimp-email"
+					type="email"
 					required
 					placeholder="<?php echo esc_attr( $values['emailPlaceholder'] ); ?>"
 				/>
 				<button type="submit" class="components-button is-button is-primary">
-					<?php echo esc_html( $values['submitLabel'] ); ?>
+					<?php echo wp_kses_post( $values['submitLabel'] ); ?>
 				</button>
-				<figcaption>
-					<?php echo esc_html( $values['consentText'] ); ?>
-				</figcaption>
+				<p>
+					<small>
+						<?php echo wp_kses_post( $values['consentText'] ); ?>
+					</small>
+				</p>
 			</form>
-			<div class="wp-block-jetpack-mailchimp-notification wp-block-jetpack-mailchimp-processing">
+			<div class="wp-block-jetpack-mailchimp_notification wp-block-jetpack-mailchimp_processing">
 				<?php echo esc_html( $values['processingLabel'] ); ?>
 			</div>
-			<div class="wp-block-jetpack-mailchimp-notification wp-block-jetpack-mailchimp-success">
+			<div class="wp-block-jetpack-mailchimp_notification wp-block-jetpack-mailchimp_success">
 				<?php echo esc_html( $values['successLabel'] ); ?>
 			</div>
-			<div class="wp-block-jetpack-mailchimp-notification wp-block-jetpack-mailchimp-error">
+			<div class="wp-block-jetpack-mailchimp_notification wp-block-jetpack-mailchimp_error">
 				<?php echo esc_html( $values['errorLabel'] ); ?>
 			</div>
 		</div>
