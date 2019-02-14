67ae7dfd0570f6dd216fa4d48b0e586497ac8868
Kirk Wight <kwight@kwight.ca>
2019-02-14 12:17:36 -0800

    Add asetting to allow the JP site to be iframed by WordPress.com.

--- _inc/client/writing/composing.jsx
+++ _inc/client/writing/composing.jsx
@@ -200,9 +200,10 @@ export class Composing extends React.Component {
 		const foundCopyPost = this.props.isModuleFound( 'copy-post' ),
 			foundAtD = this.props.isModuleFound( 'after-the-deadline' ),
 			foundLatex = this.props.isModuleFound( 'latex' ),
-			foundMarkdown = this.props.isModuleFound( 'markdown' );
+			foundMarkdown = this.props.isModuleFound( 'markdown' ),
+			foundWordPressComCompose = this.props.isModuleFound( 'wordpress-com-compose' );
 
-		if ( ! foundCopyPost && ! foundMarkdown && ! foundAtD ) {
+		if ( ! foundCopyPost && ! foundMarkdown && ! foundAtD && ! foundWordPressComCompose ) {
 			return null;
 		}
 
@@ -210,6 +211,7 @@ export class Composing extends React.Component {
 			latex = this.props.module( 'latex' ),
 			atd = this.props.module( 'after-the-deadline' ),
 			copyPost = this.props.module( 'copy-post' ),
+			wordPressComCompose = this.props.module( 'wordpress-com-compose' ),
 			unavailableInDevMode = this.props.isUnavailableInDevMode( 'after-the-deadline' ),
 			copyPostSettings = (
 				<SettingsGroup
@@ -231,6 +233,29 @@ export class Composing extends React.Component {
 							toggleModule={ this.props.toggleModuleNow }
 						>
 							<span className="jp-form-toggle-explanation">{ copyPost.description }</span>
+						</ModuleToggle>
+					</FormFieldset>
+				</SettingsGroup>
+			),
+			wordPressComComposeSettings = (
+				<SettingsGroup
+					module={ wordPressComCompose }
+					support={ {
+						text: __( 'Allow new block editor posts to be composed on WordPress.com.' ),
+						link: 'https://jetpack.com/support/copy-post-2/',
+					} }
+				>
+					<FormFieldset>
+						<ModuleToggle
+							slug="wordpress-com-compose"
+							activated={ !! this.props.getOptionValue( 'wordpress-com-compose' ) }
+							toggling={ this.props.isSavingAnyOption( 'wordpress-com-compose' ) }
+							disabled={ this.props.isSavingAnyOption( 'wordpress-com-compose' ) }
+							toggleModule={ this.props.toggleModuleNow }
+						>
+							<span className="jp-form-toggle-explanation">
+								{ wordPressComCompose.description }
+							</span>
 						</ModuleToggle>
 					</FormFieldset>
 				</SettingsGroup>
@@ -325,6 +350,7 @@ export class Composing extends React.Component {
 				saveDisabled={ this.props.isSavingAnyOption( 'ignored_phrases' ) }
 			>
 				{ foundCopyPost && copyPostSettings }
+				{ foundWordPressComCompose && wordPressComComposeSettings }
 				{ foundMarkdown && markdownSettings }
 				{ foundLatex && latexSettings }
 				{ foundAtD && atdSettings }
---
+++ modules/wordpress-com-compose.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Module Name: WordPress.com Compose
+ * Module Description: Allow new block editor posts to be composed on WordPress.com.
+ * Jumpstart Description: Allow new block editor posts to be composed on WordPress.com.
+ * Sort Order: 15
+ * First Introduced: 7.2
+ * Requires Connection: Yes
+ * Auto Activate: No
+ * Module Tags: Writing
+ * Feature: Writing
+ * Additional Search Queries: iframes, allow, compose, WordPress.com, block
+ */
+
+function jetpack_disable_send_frame_options_header() {
+	if ( jetpack_framing_allowed() ) {
+		remove_action( 'admin_init', 'send_frame_options_header' );
+	}
+}
+add_action( 'admin_init', 'jetpack_disable_send_frame_options_header', 1 ); // High priority to get ahead of send_frame_options_header
+
+function jetpack_get_frame_nonce() {
+	return wp_create_nonce( 'frame-' . Jetpack_Options::get_option( 'id' ) );
+}
+
+function jetpack_framing_allowed() {
+	if ( empty( $_GET['frame-nonce'] ) ) {
+		return false;
+	}
+
+	$verified = wp_verify_nonce( $_GET['frame-nonce'], 'frame-' . Jetpack_Options::get_option( 'id' ) );
+
+	if ( $verified ) {
+		define( 'IFRAME_REQUEST', true );
+		return true;
+	}
+	return false;
+}
+
+/**
+ * Automatically add frame-nonce to any admin_url() calls when the current page is framed.
+ */
+function jetpack_auto_frame_nonce( $url ) {
+	if ( jetpack_framing_allowed() ) {
+		$url = add_query_arg( array( 'frame-nonce' => jetpack_get_frame_nonce() ), $url );
+	}
+	return $url;
+}
+add_filter( 'admin_url', 'jetpack_auto_frame_nonce' );
+
+function jetpack_add_iframed_body_class( $classes ) {
+	if ( jetpack_framing_allowed() ) {
+		$classes .= ' is-iframed ';
+	}
+	return $classes;
+}
+add_filter( 'admin_body_class', 'jetpack_add_iframed_body_class' );
