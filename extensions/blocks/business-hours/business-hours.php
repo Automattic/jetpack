91f92594b6d469cebd7f0832f503167af986b083
Rocco Tripaldi <tripaldir@gmail.com>
2019-02-14 20:52:23 -0500

    Business Hours Block:
    adds support for openingHours schema: https://schema.org/openingHours

--- extensions/blocks/business-hours/business-hours.php
+++ extensions/blocks/business-hours/business-hours.php
@@ -13,6 +13,65 @@ jetpack_register_block_type(
 );
 
 /**
+ * Returns a filterable answer to the question: should we use a standardized schema in the business hours mark-up?
+ *
+ * @return bool
+ */
+function jetpack_business_hours_should_use_schema() {
+	/**
+	 * Filters if business hours html should use schema from https://schema.org/openingHours
+	 *
+	 * @since 7.1.0
+	 *
+	 * @param string $should_use_schema Defaults to true
+	 */
+	return apply_filters( 'jetpack_business_hours_should_use_schema', true );
+}
+
+/**
+ * Returns the opening html tag for the business hours block.
+ *
+ * @param array $attributes Attributes for the business hours block established in the block editor.
+ *
+ * @return string The opening tag for the business hours markup.
+ */
+function jetpack_business_hours_opening_tag( $attributes ) {
+	if ( jetpack_business_hours_should_use_schema() ) {
+		$itemtype = apply_filters( 'jetpack_business_hours_item_type', 'https://schema.org/LocalBusiness' );
+		return sprintf(
+			'<dl class="jetpack-business-hours %s" itemscope itemtype="%s">',
+			! empty( $attributes['className'] ) ? esc_attr( $attributes['className'] ) : '',
+			$itemtype
+		);
+	}
+	return sprintf(
+		'<dl class="jetpack-business-hours %s">',
+		! empty( $attributes['className'] ) ? esc_attr( $attributes['className'] ) : ''
+	);
+}
+
+/**
+ * Returns the opening `<span>` tag for a set of opening - closing hours for a given day.
+ *
+ * @param string $day eg 'Mon'
+ * @param int $opens timestamp
+ * @param int $closes timestamp
+ *
+ * @return string The tag
+ */
+function jetpack_business_hours_opening_tag_for_days_hours( $day, $opens, $closes ) {
+	if ( jetpack_business_hours_should_use_schema() ) {
+		return sprintf(
+			'<span itemprop="openingHours" content="%s %s-%s">',
+			substr( $day, 0, 2 ),
+			date( 'H:i', $opens ),
+			date( 'H:i', $closes )
+		);
+	}
+	return '<span>';
+}
+
+/**
  * Dynamic rendering of the block.
  *
  * @param array  $attributes Array containing the business hours block attributes.
@@ -30,10 +89,7 @@ function jetpack_business_hours_render( $attributes, $content ) {
 	$start_of_week = (int) get_option( 'start_of_week', 0 );
 	$time_format   = get_option( 'time_format' );
 	$today         = current_time( 'D' );
-	$content       = sprintf(
-		'<dl class="jetpack-business-hours %s">',
-		! empty( $attributes['className'] ) ? esc_attr( $attributes['className'] ) : ''
-	);
+	$content       = jetpack_business_hours_opening_tag();
 
 	$days = array( 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' );
 
@@ -51,11 +107,12 @@ function jetpack_business_hours_render( $attributes, $content ) {
 		$days_hours = '';
 
 		foreach ( $hours as $hour ) {
-			if ( ! $hour['opening'] && $hour['closing'] ) {
+			if ( empty( $hour['opening'] ) || empty( $hour['closing'] ) ) {
 				continue;
 			}
 			$opening    = strtotime( $hour['opening'] );
 			$closing    = strtotime( $hour['closing'] );
+			$days_hours .= jetpack_business_hours_opening_tag_for_days_hours( $day, $opening, $closing );
 			$days_hours .= sprintf(
 			/* Translators: Business opening hours info. */
 				_x( 'From %1$s to %2$s', 'from business opening hour to closing hour', 'jetpack' ),
@@ -81,11 +138,11 @@ function jetpack_business_hours_render( $attributes, $content ) {
 					) );
 				}
 			}
-			$days_hours .= '<br />';
+			$days_hours .= '</span><br />';
 		}
 
 		if ( empty( $days_hours ) ) {
-			$days_hours = esc_html__( 'CLOSED', 'jetpack' );
+			$days_hours = esc_html__( 'Closed', 'jetpack' );
 		}
 		$content .= $days_hours;
 		$content .= '</dd>';
