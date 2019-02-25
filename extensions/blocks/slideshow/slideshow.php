5a3408d06435500efd4bc521e8a6bf3dc480f289
Jefferson Rabb <j.max.rabb@gmail.com>
2019-02-25 16:05:57 -0500

    Slideshow: Remove View-Side Dependencies (#11350)
    * Remove all Slideshow block dependencies.
    
    * Reinstate lodash dependency.

--- extensions/blocks/slideshow/slideshow.php
+++ extensions/blocks/slideshow/slideshow.php
@@ -25,9 +25,8 @@ jetpack_register_block(
 function jetpack_slideshow_block_load_assets( $attr, $content ) {
 	$dependencies = array(
 		'lodash',
-		'wp-element',
-		'wp-i18n',
 	);
+
 	Jetpack_Gutenberg::load_assets_as_required( 'slideshow', $dependencies );
 
 	return $content;
