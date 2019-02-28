f8df1d5cb6986a96c04314711d866f502e20c932
Jeremy Herve <jeremy@jeremy.hu>
2019-02-28 19:20:52 +0100

    Ustream: update tests to match new HTTPS URLs.

--- tests/php/modules/shortcodes/test_class.ustream.php
+++ tests/php/modules/shortcodes/test_class.ustream.php
@@ -47,7 +47,7 @@ class WP_Test_Jetpack_Shortcodes_Ustream extends WP_UnitTestCase {
 
 		$shortcode_content = do_shortcode( $content );
 
-		$this->assertContains( '<iframe src="http://www.ustream.tv/embed/recorded/' . $id, $shortcode_content );
+		$this->assertContains( '<iframe src="https://www.ustream.tv/embed/recorded/' . $id, $shortcode_content );
 	}
 
 	/**
@@ -61,6 +61,6 @@ class WP_Test_Jetpack_Shortcodes_Ustream extends WP_UnitTestCase {
 
 		$shortcode_content = do_shortcode( $content );
 
-		$this->assertContains( '<iframe id="SocialStream" class="" name="SocialStream" width="500" height="420" scrolling="no" allowtransparency="true" src="http://www.ustream.tv/socialstream/' . $id, $shortcode_content );
+		$this->assertContains( '<iframe id="SocialStream" src="https://www.ustream.tv/socialstream/' . $id, $shortcode_content );
 	}
 }
