e7d847bbac7f0e95d31df27321d0cac092fd5d05
Michael D Adams <michael.d.adams@gmail.com>
2019-03-06 19:46:47 -0800

    PHPCS: Only look at changed lines
    Instead of reporting every error in every changed file, instead
    report every error on every changed line in every changed file.
    
    To do this, find all errors, and then filter for only the ones that
    occur on changed lines.

---
+++ bin/diff-changed-lines.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Prints FILE:(LINE_RANGE)+
+# Where LINE_RANGE is, e.g., "1-3," (note trailing comma, always a range, even for single line changes: "10-10,")
+# Only shows lines from the changed version of FILE. That is, it does not show deleted lines.
+# Example Output:
+# file.php:3-8,20-32,
+# file.js:4-4,22-22,
+
+EXTCMD=(
+	'/bin/echo -n "$MERGED:" &&' # Echo "FILE:"
+	'diff'                       # Echo "LINE_RANGES"
+		'--old-group-format="" --unchanged-group-format=""' # Don't output info for old (deleted) or unchanged (context) lines
+		'--new-group-format="%dF-%dL," --changed-group-format="%dF-%dL,"' # Echo LINE_RANGE
+		'"$LOCAL" "$REMOTE";' # diff old new
+	'echo && :' # Newline after each file, and ignore the rest of the stuff difftool sends
+)
+
+
+# git difftool --no-prompt --extcmd '/bin/echo -n "$MERGED:" && diff --old-group-format="" --unchanged-group-format="" --new-group-format="%dF-%dL," --changed-group-format="%dF-%dL," "$LOCAL" "$REMOTE"; echo && :'
+
+EXTCMD="${EXTCMD[@]}"
+
+git difftool --no-prompt --extcmd "$EXTCMD" $@
