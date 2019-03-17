923be2dc0e2b9ece6b519e3d0a1fd413ff4c055b
Antal <antal.tettinger@gmail.com>
2019-03-18 02:06:28 +0800

    Fix: Removal of the widget section DOM element following acceptance of cookie policy

--- modules/widgets/eu-cookie-law/eu-cookie-law.js
+++ modules/widgets/eu-cookie-law/eu-cookie-law.js
@@ -58,6 +58,8 @@
 
 		overlay.fadeOut( 400, function() {
 			overlay.remove();
+			var widgetSection = document.querySelector(".widget.widget_eu_cookie_law_widget");
+			widgetSection.parentNode.removeChild(elem);
 		} );
 	}
 } )( jQuery );
