6bda70ccf427a2a8b9d204f567864738d0fa7fd2
Jonathan Sadowski <sadowski@automattic.com>
2019-03-19 19:46:16 -0500

    Sync: improve importer detection and sync start action

--- sync/class.jetpack-sync-module-import.php
+++ sync/class.jetpack-sync-module-import.php
@@ -1,8 +1,42 @@
 <?php
 
+require_once dirname( __FILE__ ) . '/class.jetpack-sync-functions.php';
+
 class Jetpack_Sync_Module_Import extends Jetpack_Sync_Module {
 
-	private $import_end = false;
+	/**
+	 * Tracks which actions have already been synced for the import
+	 * to prevent the same event from being triggered a second time
+	 * @var array
+	 */
+	private $synced_actions = array();
+
+	/**
+	 * A mapping of known importers to friendly names.
+	 * Keys are the class name of the known importer.
+	 * Values are the friendly name.
+	 * @var array
+	 */
+	private static $known_importers = array(
+		'Blogger_Importer' => 'blogger',
+		'LJ_API_Import' => 'livejournal',
+		'MT_Import' => 'mt',
+		'RSS_Import' => 'rss',
+		'WC_Tax_Rate_Importer' => 'woo-tax-rate',
+		'WP_Import' => 'wordpress',
+	);
+
+	/**
+	 * A mapping of action types to sync action name.
+	 * Keys are the name of the import action.
+	 * Values are the resulting sync action.
+	 * @var array
+	 */
+	private static $import_sync_action_map = array(
+		'import_start' => 'jetpack_sync_import_start',
+		'import_done'  => 'jetpack_sync_import_end',
+		'import_end'   => 'jetpack_sync_import_end',
+	);
 
 	public function name() {
 		return 'import';
@@ -10,78 +44,54 @@ class Jetpack_Sync_Module_Import extends Jetpack_Sync_Module {
 
 	public function init_listeners( $callable ) {
 		add_action( 'export_wp', $callable );
-		add_action( 'jetpack_sync_import_end', $callable, 10, 2 );
+		add_action( 'jetpack_sync_import_start', $callable, 10, 2 );
+		add_action( 'jetpack_sync_import_end',   $callable, 10, 2 );
+
+		// WordPress
+		add_action( 'import_start', array( $this, 'sync_import_action' ) );
 
 		// Movable type, RSS, Livejournal
-		add_action( 'import_done', array( $this, 'sync_import_done' ) );
+		add_action( 'import_done',  array( $this, 'sync_import_action' ) );
 
 		// WordPress, Blogger, Livejournal, woo tax rate
-		add_action( 'import_end', array( $this, 'sync_import_end' ) );
+		add_action( 'import_end',   array( $this, 'sync_import_action' ) );
 	}
 
 	public function set_defaults() {
-		$this->import_end = false;
+		$this->synced_actions = array();
 	}
 
-	public function sync_import_done( $importer ) {
-		// We already ran an send the import
-		if ( $this->import_end ) {
+	public function sync_import_action( $importer ) {
+		$import_action = current_filter();
+		// map action to event name
+		$sync_action = self::$import_sync_action_map[ $import_action ];
+
+		// Only sync each action once per import
+		if ( array_key_exists( $sync_action, $this->synced_actions ) && $this->synced_actions[ $sync_action ] ) {
 			return;
+		}
+
+		// Mark this action as synced
+		$this->synced_actions[ $sync_action ] = true;
+
+		// prefer self-reported $importer value
+		if ( ! $importer ) {
+			// fall back to inferring by calling class name
+			$importer = Jetpack_Sync_Functions::get_calling_importer_class();
+		}
+
+		// Get $importer from known_importers
+		if ( isset( self::$known_importers[ $importer ] ) ) {
+			$importer = self::$known_importers[ $importer ];
 		}
 
 		$importer_name = $this->get_importer_name( $importer );
 
-		/**
-		 * Sync Event that tells that the import is finished
-		 *
-		 * @since 5.0.0
-		 *
-		 * $param string $importer
-		 */
-		do_action( 'jetpack_sync_import_end', $importer, $importer_name );
-		$this->import_end = true;
-	}
-
-	public function sync_import_end() {
-		// We already ran an send the import
-		if ( $this->import_end ) {
-			return;
-		}
-
-		$this->import_end = true;
-		$importer         = 'unknown';
-		$backtrace        = wp_debug_backtrace_summary( null, 0, false );
-		if ( $this->is_importer( $backtrace, 'Blogger_Importer' ) ) {
-			$importer = 'blogger';
-		}
-
-		if ( 'unknown' === $importer && $this->is_importer( $backtrace, 'WC_Tax_Rate_Importer' ) ) {
-			$importer = 'woo-tax-rate';
-		}
-
-		if ( 'unknown' === $importer && $this->is_importer( $backtrace, 'WP_Import' ) ) {
-			// phpcs:ignore WordPress.WP.CapitalPDangit
-			$importer = 'wordpress';
-		}
-
-		$importer_name = $this->get_importer_name( $importer );
-
-		/** This filter is already documented in sync/class.jetpack-sync-module-posts.php */
-		do_action( 'jetpack_sync_import_end', $importer, $importer_name );
+		do_action( $sync_action, $importer, $importer_name );
 	}
 
 	private function get_importer_name( $importer ) {
 		$importers = get_importers();
 		return isset( $importers[ $importer ] ) ? $importers[ $importer ][0] : 'Unknown Importer';
-	}
-
-	private function is_importer( $backtrace, $class_name ) {
-		foreach ( $backtrace as $trace ) {
-			if ( strpos( $trace, $class_name ) !== false ) {
-				return true;
-			}
-		}
-
-		return false;
 	}
 }
