3f9765afb8b2f01a83674f9ac29405e98ddd3353
Jon Surrell <jon.surrell@automattic.com>
2019-03-20 21:34:26 +0100

    Restore disable lint comment to correct line

--- modules/carousel/jetpack-carousel.js
+++ modules/carousel/jetpack-carousel.js
@@ -1552,8 +1552,11 @@ jQuery( document ).ready( function( $ ) {
 					// attachment id might no longer match the current attachment id by the time we get the data back or a now
 					// registered infiniscroll event kicks in, so we don't ever display comments for the wrong image by mistake.
 					var current = $( '.jp-carousel div.selected' );
-					if ( current && current.data && current.data( 'attachment-id' ) != args.attachment_id ) {
-						// jshint ignore:line
+					if (
+						current &&
+						current.data &&
+						current.data( 'attachment-id' ) != args.attachment_id // jshint ignore:line
+					) {
 						comments.fadeOut( 'fast' );
 						comments.empty();
 						return;
