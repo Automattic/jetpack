6d389492ff38f1433035635b3029ed6a1969a0f1
Igor Zinovyev <zinigor@gmail.com>
2019-03-21 20:04:12 +0300

    Added a test for secure passwords.

--- tests/php/sync/test_class.jetpack-sync-module-auth.php
+++ tests/php/sync/test_class.jetpack-sync-module-auth.php
@@ -6,16 +6,32 @@
 
 class WP_Test_Jetpack_Sync_Module_Auth extends WP_Test_Jetpack_Sync_Base {
 
-	function test_sends_failed_login_event() {
-		$user = get_user_by( 'ID', 1 );
+	protected $user;
 
-		do_action( 'authenticate', $user, $user->nickname, 'admin' );
+	function setUp() {
+		parent::setUp();
+		$this->user = get_user_by( 'ID', 1 );
+	}
+
+	function test_sends_insecure_password_event() {
+		do_action( 'authenticate', $this->user, $this->user->nickname, 'admin' );
 
 		$this->sender->do_sync();
 
 		$action = $this->server_event_storage->get_most_recent_event( 'authenticate' );
 
 		$this->assertIsObject( $action );
-		$this->assertEquals( 1, $action->args[0]['external_user_id'] );
+		$this->assertEquals( $this->user->ID, $action->args[0]['external_user_id'] );
+		$this->assertArrayHasKey( 'warning', $action->args[0] );
+	}
+
+	function test_does_not_send_insecure_password_event_on_secure_password() {
+		do_action( 'authenticate', $this->user, $this->user->nickname, wp_generate_password( 25 ) );
+
+		$this->sender->do_sync();
+
+		$action = $this->server_event_storage->get_most_recent_event( 'authenticate' );
+
+		$this->assertFalse( $action );
 	}
 }
