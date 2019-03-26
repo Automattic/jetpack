16831ea75b43ac1201d579df92a6a9a833ac920c
Enej Bajgoric <enej.bajgoric@gmail.com>
2019-03-26 09:53:11 -0700

    Revert the tests as well.

--- tests/php/sync/test_class.jetpack-sync-full.php
+++ tests/php/sync/test_class.jetpack-sync-full.php
@@ -1254,22 +1254,6 @@ class WP_Test_Jetpack_Sync_Full extends WP_Test_Jetpack_Sync_Base {
 		$this->assertEquals( intval( $previous_min_id ), $last_post->ID );
 	}
 
-	function test_full_sync_sends_previous_min_id_on_comments() {
-		Jetpack_Sync_Settings::update_settings( array( 'max_queue_size_full_sync' => 2, 'max_enqueue_full_sync' => 10 ) );
-		$this->post_id = $this->factory->post->create();
-
-		for( $i = 0; $i < 25; $i++ ) {
-			$this->factory->comment->create_post_comments( $this->post_id );
-		}
-
-		$this->full_sync->start( array( 'comments' => true ) );
-
-		$this->sender->do_full_sync();
-
-		$event = $this->server_event_storage->get_most_recent_event( 'jetpack_full_sync_comments' );
-		$this->assetEquals( $event->args[2], '~0');
-	}
-
 	function _do_cron() {
 		$_GET['check'] = wp_hash( '187425' );
 		require( ABSPATH . '/wp-cron.php' );
