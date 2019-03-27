0ef844dcbeaab40dc8091e6abb4d1deb3697c4cf
Jeremy Herve <jeremy@jeremy.hu>
2019-03-27 15:53:48 +0100

    Module Descriptions: add file to list of autolint files.

--- bin/pre-commit-hook.js
+++ bin/pre-commit-hook.js
@@ -35,6 +35,7 @@ function phpcsFilesToFilter( file ) {
 		'sync/class.jetpack-sync-module-auth.php',
 		'class.jetpack-gutenberg.php',
 		'class.jetpack-plan.php',
+		'modules/module-info.php',
 	];
 
 	if ( -1 !== whitelist.findIndex( filePath => file.startsWith( filePath ) ) ) {
