4b061e96f58edc700a289dc6b1ddccb008758ee1
artpi <artur.piszek@gmail.com>
2019-03-27 14:41:27 +0100

    Endpoint changes because I have to rebase

--- _inc/lib/core-api/wpcom-endpoints/memberships.php
+++ _inc/lib/core-api/wpcom-endpoints/memberships.php
@@ -30,6 +30,74 @@ class WPCOM_REST_API_V2_Endpoint_Memberships extends WP_REST_Controller {
 				),
 			)
 		);
+		register_rest_route(
+			$this->namespace,
+			$this->rest_base . '/product',
+			array(
+				array(
+					'methods'  => WP_REST_Server::CREATABLE,
+					'callback' => array( $this, 'create_product' ),
+					'args'     => array(
+						'title' => array(
+							'type'   => 'string',
+							'required' => true,
+						),
+						'price' => array(
+							'type'   => 'float',
+							'required' => true,
+						),
+						'currency' => array(
+							'type'   => 'string',
+							'required' => true,
+						),
+						'interval' => array(
+							'type'   => 'string',
+							'required' => true,
+						),
+					),
+				),
+			)
+		);
+	}
+
+	public function create_product( $request ) {
+		if ( ( defined( 'IS_WPCOM' ) && IS_WPCOM ) ) {
+			require_lib( 'memberships' );
+			$connected_destination_account_id = Jetpack_Memberships::get_connected_account_id();
+			if ( ! $connected_destination_account_id ) {
+				return new WP_Error( 'no-destination-account', __( 'Please set up Stripe account for this site first' ) );
+			}
+			$product = Memberships_Product::create( get_current_blog_id(), array(
+				'title' => $request['title'],
+				'price' => $request['price'],
+				'currency' => $request['currency'],
+				'interval' => $request['interval'],
+				'connected_destination_account_id' => $connected_destination_account_id,
+			) );
+			return $product->to_array();
+		} else {
+			$blog_id = Jetpack_Options::get_option( 'id' );
+			$response = Jetpack_Client::wpcom_json_api_request_as_blog(
+				"/sites/$blog_id/{$this->rest_base}/product",
+				'v2',
+				array(
+					'method' => 'POST',
+				),
+				array(
+					'title' => $request['title'],
+					'price' => $request['price'],
+					'currency' => $request['currency'],
+					'interval' => $request['interval'],
+				)
+			);
+			if ( is_wp_error( $response ) ) {
+				return new WP_Error( 'wpcom_connection_error', 'Could not connect to WP.com', 404 );
+			}
+			$data = isset( $response['body'] ) ? json_decode( $response['body'], true ) : null;
+			return $data;
+		}
+
+		return $request;
 	}
 	public function get_status() {
 		$connected_account_id = Jetpack_Memberships::get_connected_account_id();
@@ -38,7 +106,7 @@ class WPCOM_REST_API_V2_Endpoint_Memberships extends WP_REST_Controller {
 			require_lib( 'memberships' );
 			$blog_id = get_current_blog_id();
 			if ( ! $connected_account_id ) {
-				$connect_url = get_memberships_connected_account_redirect( get_current_user_id() );
+				$connect_url = get_memberships_connected_account_redirect( get_current_user_id(), $blog_id );
 			}
 			$products = get_memberships_plans( $blog_id );
 		} else {
