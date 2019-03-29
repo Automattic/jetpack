40e357e47824949f3c764a0d5c270651dcb0096f
Bernie Reiter <ockham@raz.or.at>
2019-03-29 14:00:42 +0100

    Manually add jetpack textdomain to extensions/blocks/map/component.js

--- extensions/blocks/map/component.js
+++ extensions/blocks/map/component.js
@@ -69,20 +69,20 @@ export class Map extends Component {
 				{ activeMarker && admin && (
 					<Fragment>
 						<TextControl
-							label={ __( 'Marker Title' ) }
+							label={ __( 'Marker Title', 'jetpack' ) }
 							value={ title }
 							onChange={ value => updateActiveMarker( { title: value } ) }
 						/>
 						<TextareaControl
 							className="wp-block-jetpack-map__marker-caption"
-							label={ __( 'Marker Caption' ) }
+							label={ __( 'Marker Caption', 'jetpack' ) }
 							value={ caption }
 							rows="2"
 							tag="textarea"
 							onChange={ value => updateActiveMarker( { caption: value } ) }
 						/>
 						<Button onClick={ deleteActiveMarker } className="wp-block-jetpack-map__delete-btn">
-							<Dashicon icon="trash" size="15" /> { __( 'Delete Marker' ) }
+							<Dashicon icon="trash" size="15" /> { __( 'Delete Marker', 'jetpack' ) }
 						</Button>
 					</Fragment>
 				) }
