255367226265b65bd9416ca73408929941e43a95
artpi <artur.piszek@gmail.com>
2019-04-01 20:53:22 +0200

    make it pull a proper file

--- extensions/blocks/membership-button/membership-button.php
+++ extensions/blocks/membership-button/membership-button.php
@@ -7,7 +7,11 @@
  * @package Jetpack
  */
 
-require_once JETPACK__PLUGIN_DIR . '/modules/memberships/class-jetpack-memberships.php';
+if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
+	require_once WP_CONTENT_DIR . '/mu-plugins/memberships/class-jetpack-memberships.php';
+} elseif ( Jetpack::is_active() ) {
+	require_once JETPACK__PLUGIN_DIR . '/modules/memberships/class-jetpack-memberships.php';
+}
 
 if ( ( defined( 'IS_WPCOM' ) && IS_WPCOM ) || Jetpack::is_active() ) {
 	jetpack_register_block(
