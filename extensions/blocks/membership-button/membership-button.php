cc0343af73d70bab3f68d669404d9d2135f132b8
artpi <artur.piszek@gmail.com>
2019-04-02 16:11:08 +0200

    jetpack-memberships class moved on wpcom

--- extensions/blocks/membership-button/membership-button.php
+++ extensions/blocks/membership-button/membership-button.php
@@ -1,4 +1,4 @@
-<?php
+<?php // phpcs:disable Squiz.Commenting.FileComment.Missing
 /**
  * Memberships block.
  *
@@ -7,17 +7,11 @@
  * @package Jetpack
  */
 
-if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
-	require_once WP_CONTENT_DIR . '/mu-plugins/memberships/class-jetpack-memberships.php';
-} elseif ( Jetpack::is_active() ) {
-	require_once JETPACK__PLUGIN_DIR . '/modules/memberships/class-jetpack-memberships.php';
-}
+require_once JETPACK__PLUGIN_DIR . '/modules/memberships/class-jetpack-memberships.php';
 
-if ( ( defined( 'IS_WPCOM' ) && IS_WPCOM ) || Jetpack::is_active() ) {
-	jetpack_register_block(
-		'jetpack/membership-button',
-		array(
-			'render_callback' => array( Jetpack_Memberships::get_instance(), 'render_button' ),
-		)
-	);
-}
+jetpack_register_block(
+	'jetpack/membership-button',
+	array(
+		'render_callback' => array( Jetpack_Memberships::get_instance(), 'render_button' ),
+	)
+);
