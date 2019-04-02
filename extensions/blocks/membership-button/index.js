94e6994bb7f652f497cc9eef8ca12a50536d9b58
Jon Surrell <jon.surrell@automattic.com>
2019-04-02 11:35:57 +0200

    Use unwrapped i18n depenencies instaed of relative path
    Co-Authored-By: artpi <artur.piszek@gmail.com>

--- extensions/blocks/membership-button/index.js
+++ extensions/blocks/membership-button/index.js
@@ -6,7 +6,7 @@ import { Path, Rect, SVG, G } from '@wordpress/components';
 /**
  * Internal dependencies
  */
-import { __, _x } from '../../utils/i18n';
+import { __, _x } from '@wordpress/i18n';
 import edit from './edit';
 import './editor.scss';
 
