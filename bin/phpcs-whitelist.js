c1aad4cc44b012234d6a3ad0c43c5d3f291652dc
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2019-04-03 00:37:02 +0200

    Rebase with master to add new linted file to new location (#11816)

---
+++ bin/phpcs-whitelist.js
@@ -0,0 +1,13 @@
+// If the file path starts with anything like in the array below, it should be linted.
+module.exports = [
+	'_inc/lib/debugger/',
+	'_inc/lib/class.jetpack-password-checker.php',
+	'extensions/',
+	'sync/class.jetpack-sync-module-auth.php',
+	'class.jetpack-gutenberg.php',
+	'class.jetpack-plan.php',
+	'modules/module-extras.php',
+	'modules/module-info.php',
+	'modules/theme-tools/social-menu/',
+	'functions.opengraph.php',
+];
