88ad471db3d5a1121bfd532ede5fb91a8242f921
Copons <copons@users.noreply.github.com>
2019-04-03 12:30:22 +0100

    Move the extension setter in the right place

---
+++ extensions/blocks/post-location/post-location.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Post Location Panel.
+ *
+ * @package Jetpack
+ */
+
+/**
+ * Set the Post Location Gutenberg extension as available.
+ */
+Jetpack_Gutenberg::set_extension_available( 'jetpack/post-location' );
