c0c02a8ad2899dc7c268bee697b81120f61c246c
Marek Hrabe <marekhrabe@me.com>
2019-04-10 16:18:10 +0900

    add UI for avatar edit/removal

--- extensions/blocks/testimonial/edit.js
+++ extensions/blocks/testimonial/edit.js
@@ -2,13 +2,22 @@
  * External dependencies
  */
 import { Component, Fragment } from '@wordpress/element';
-import { RichText, MediaPlaceholder, BlockControls, AlignmentToolbar } from '@wordpress/editor';
+import { RichText, BlockControls, AlignmentToolbar, MediaUpload } from '@wordpress/editor';
+import { Tooltip, Toolbar } from '@wordpress/components';
 import classnames from 'classnames';
+import { filter } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import { __ } from '../../utils/i18n';
+import './editor.scss';
+
+const getKeyDownHandler = callback => event => {
+	if ( event.key === 'Enter' || event.key === 'Space' ) {
+		callback( event );
+	}
+};
 
 export default class TestimonialEdit extends Component {
 	onChangeName = value => void this.props.setAttributes( { name: value } );
@@ -21,13 +30,34 @@ export default class TestimonialEdit extends Component {
 	render() {
 		const { attributes, className, isSelected } = this.props;
 		const hasMedia = attributes.mediaUrl && attributes.mediaId;
-
 		const alignmentClass = attributes.align && `is-aligned-${ attributes.align }`;
+		const avatarTooltip = hasMedia ? __( 'Change avatar' ) : __( 'Add avatar' );
 
 		return (
 			<Fragment>
 				<BlockControls>
 					<AlignmentToolbar value={ attributes.align } onChange={ this.onChangeAlign } />
+					<MediaUpload
+						allowedTypes={ [ 'image' ] }
+						onSelect={ this.onChangeMedia }
+						value={ attributes.mediaId }
+						render={ ( { open } ) => (
+							<Toolbar
+								controls={ filter( [
+									{
+										icon: 'edit',
+										title: avatarTooltip,
+										onClick: open,
+									},
+									hasMedia && {
+										icon: 'no-alt',
+										title: __( 'Remove avatar' ),
+										onClick: () => this.props.setAttributes( { mediaId: null, mediaUrl: null } ),
+									},
+								] ) }
+							/>
+						) }
+					/>
 				</BlockControls>
 				<div className={ classnames( className, alignmentClass ) }>
 					<RichText
@@ -41,23 +71,36 @@ export default class TestimonialEdit extends Component {
 						wrapperClassName="wp-block-jetpack-testimonial__content"
 					/>
 					<div className="wp-block-jetpack-testimonial__author">
-						{ hasMedia ? (
-							<img
-								src={ attributes.mediaUrl }
-								width={ 50 }
-								height={ 50 }
-								alt={ attributes.name || '' }
-								className="wp-block-jetpack-testimonial__media"
+						{ ( isSelected || hasMedia ) && (
+							<MediaUpload
+								allowedTypes={ [ 'image' ] }
+								onSelect={ this.onChangeMedia }
+								value={ attributes.mediaId }
+								render={ ( { open } ) => (
+									<Tooltip text={ avatarTooltip }>
+										<div
+											className={ classnames( 'wp-block-jetpack-testimonial__media', {
+												'is-placeholder': ! hasMedia,
+											} ) }
+											onClick={ open }
+											onKeyDown={ getKeyDownHandler( open ) }
+											role="button"
+											tabIndex="0"
+										>
+											{ hasMedia && (
+												<img
+													src={ attributes.mediaUrl }
+													width={ 50 }
+													height={ 50 }
+													alt={ attributes.name || '' }
+												/>
+											) }
+										</div>
+									</Tooltip>
+								) }
 							/>
-						) : (
-							isSelected && (
-								<MediaPlaceholder
-									onSelect={ this.onChangeMedia }
-									allowedTypes={ [ 'image' ] }
-									labels={ { title: 'The Image' } }
-								/>
-							)
 						) }
+
 						{ ( isSelected || attributes.name || attributes.title ) && (
 							<div className="wp-block-jetpack-testimonial__meta">
 								{ ( isSelected || attributes.name ) && (
