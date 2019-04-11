54852ebc740032303d63b4a724a512f5f4111bce
oskosk <oskosk@gmail.com>
2019-04-11 12:06:33 +0200

    Remove trailing comma

--- _inc/client/main.jsx
+++ _inc/client/main.jsx
@@ -119,11 +119,7 @@ class Main extends React.Component {
 	initializeAnalyitics = () => {
 		const tracksUser = this.props.tracksUserData;
 		if ( tracksUser ) {
-			analytics.initialize(
-				tracksUser.userid,
-				tracksUser.username,
-				tracksUser.blogid,
-			);
+			analytics.initialize( tracksUser.userid, tracksUser.username, tracksUser.blogid );
 		}
 	};
 
