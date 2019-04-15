b74f9defd3711935befef02347e5d4276eddbe00
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2019-04-15 15:45:46 -0300

    Update sync/class.jetpack-sync-settings.php
    Co-Authored-By: lezama <lezama@gmail.com>

--- sync/class.jetpack-sync-settings.php
+++ sync/class.jetpack-sync-settings.php
@@ -121,7 +121,7 @@ class Jetpack_Sync_Settings {
 			unset( self::$settings_cache[ $setting ] );
 
 			// if we set the disabled option to true, clear the queues
-			if ( ( 'disable' === $setting || 'network_disable' ) && ! ! $value ) {
+			if ( ( 'disable' === $setting || 'network_disable' === $setting ) && ! ! $value ) {
 				require_once dirname( __FILE__ ) . '/class.jetpack-sync-listener.php';
 				$listener = Jetpack_Sync_Listener::get_instance();
 				$listener->get_sync_queue()->reset();
