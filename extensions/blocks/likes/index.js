12e8ed7c8fb525d8c3f6bb549fd5361d6806bc15
Jon Surrell <jon.surrell@automattic.com>
2019-04-15 19:52:54 +0200

    Likes and sharing panel: Render on demand (#12052)
    * Decouple the likes and sharing panel from other fill
    
    * Likes and sharing panel is independent plugin

--- extensions/blocks/likes/index.js
+++ extensions/blocks/likes/index.js
@@ -1,15 +1,8 @@
 /**
  * Internal dependencies
  */
-import JetpackLikesAndSharingPanel from '../../shared/jetpack-likes-and-sharing-panel';
 import LikesCheckbox from './likes-checkbox';
 
 export const name = 'likes';
 
-export const settings = {
-	render: ( { props } ) => (
-		<JetpackLikesAndSharingPanel>
-			<LikesCheckbox props={ props } />
-		</JetpackLikesAndSharingPanel>
-	),
-};
+export const settings = { render: LikesCheckbox };
--- extensions/blocks/sharing/index.js
+++ extensions/blocks/sharing/index.js
@@ -1,15 +1,8 @@
 /**
  * Internal dependencies
  */
-import JetpackLikesAndSharingPanel from '../../shared/jetpack-likes-and-sharing-panel';
 import SharingCheckbox from './sharing-checkbox';
 
 export const name = 'sharing';
 
-export const settings = {
-	render: ( { props } ) => (
-		<JetpackLikesAndSharingPanel>
-			<SharingCheckbox props={ props } />
-		</JetpackLikesAndSharingPanel>
-	),
-};
+export const settings = { render: SharingCheckbox };
