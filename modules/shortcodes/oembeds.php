cefc17c660e2c0f9610a77c3543f9afe57120523
Jeremy Herve <jeremy@jeremy.hu>
2019-04-19 13:23:14 +0200

    Organize oEmbed list alphabetically

--- modules/shortcodes/oembeds.php
+++ modules/shortcodes/oembeds.php
@@ -5,10 +5,10 @@
  * @package Jetpack
  */
 
-wp_oembed_add_provider( 'https://cloudup.com/*', 'https://cloudup.com/oembed' );
-wp_oembed_add_provider( 'https://me.sh/*', 'https://me.sh/oembed?format=json' );
-wp_oembed_add_provider( '#https?://(www\.)?gfycat\.com/.*#i', 'https://api.gfycat.com/v1/oembed', true );
-wp_oembed_add_provider( '#https?://[^.]+\.(wistia\.com|wi\.st)/(medias|embed)/.*#', 'https://fast.wistia.com/oembed', true );
-wp_oembed_add_provider( '#https?://sketchfab\.com/.*#i', 'https://sketchfab.com/oembed', true );
-wp_oembed_add_provider( '#https?://(www\.)?icloud\.com/keynote/.*#i', 'https://iwmb.icloud.com/iwmb/oembed', true );
 wp_oembed_add_provider( '#^https?://(www.)?audiomack.com/([^/]+)/([^/]+)/([^/]+)[/]{0,1}#', 'https://www.audiomack.com/oembed', true );
+wp_oembed_add_provider( 'https://cloudup.com/*', 'https://cloudup.com/oembed' );
+wp_oembed_add_provider( '#https?://(www\.)?gfycat\.com/.*#i', 'https://api.gfycat.com/v1/oembed', true );
+wp_oembed_add_provider( '#https?://(www\.)?icloud\.com/keynote/.*#i', 'https://iwmb.icloud.com/iwmb/oembed', true );
+wp_oembed_add_provider( 'https://me.sh/*', 'https://me.sh/oembed?format=json' );
+wp_oembed_add_provider( '#https?://sketchfab\.com/.*#i', 'https://sketchfab.com/oembed', true );
+wp_oembed_add_provider( '#https?://[^.]+\.(wistia\.com|wi\.st)/(medias|embed)/.*#', 'https://fast.wistia.com/oembed', true );
