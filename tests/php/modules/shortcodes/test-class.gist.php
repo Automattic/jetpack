cbc4a434006fecb26946a5624d1383026362f76e
Jeremy Herve <jeremy@jeremy.hu>
2019-04-19 09:04:29 +0200

    Gist shortcode: new and updated tests.
    Existing tests are not updated to take into account that we strip GitHub usernames.
    New tests cover new use cases, so all potential gist shortcode formats are covered.

--- tests/php/modules/shortcodes/test-class.gist.php
+++ tests/php/modules/shortcodes/test-class.gist.php
@@ -113,7 +113,7 @@ class WP_Test_Jetpack_Shortcodes_Gist extends WP_UnitTestCase {
 
 		// Test HTML version.
 		$shortcode_content = do_shortcode( $content );
-		$this->assertContains( '<div class="gist-oembed" data-gist="' . $gist_id . '.json"></div>', $shortcode_content );
+		$this->assertContains( '<div class="gist-oembed" data-gist="' . basename( $gist_id ) . '.json"></div>', $shortcode_content );
 
 		// Test AMP version.
 		add_filter( 'jetpack_is_amp_request', '__return_true' );
@@ -137,13 +137,86 @@ class WP_Test_Jetpack_Shortcodes_Gist extends WP_UnitTestCase {
 
 		// Test HTML version.
 		$shortcode_content = do_shortcode( $content );
-		$this->assertContains( '<div class="gist-oembed" data-gist="' . $gist_id . '.json"></div>', $shortcode_content );
+		$this->assertContains( '<div class="gist-oembed" data-gist="' . basename( $gist_id ) . '.json"></div>', $shortcode_content );
 
 		// Test AMP version.
 		add_filter( 'jetpack_is_amp_request', '__return_true' );
 		$shortcode_content = do_shortcode( $content );
 		$this->assertEquals(
 			sprintf( '<amp-gist layout="fixed-height" data-gistid="%s" height="240"></amp-gist>', basename( $gist_id ) ),
+			$shortcode_content
+		);
+	}
+
+	/**
+	 * Verify that a shortcode linking to a specific file, with no username returns the expected embed code.
+	 *
+	 * @covers ::github_gist_shortcode
+	 *
+	 * @since 7.3.0
+	 */
+	public function test_shortcodes_gist_no_username_direct_file() {
+		$gist_id        = '57cc50246aab776e110060926a2face2';
+		$file_name_slug = '#file-wp-config-php';
+		$file_name      = 'wp-config.php';
+
+		$content = '[gist https://gist.github.com/' . $gist_id . $file_name_slug . ']';
+
+		$expected_gist_id = sprintf(
+			'%1$s.json?file=%2$s',
+			$gist_id,
+			$file_name
+		);
+		// Test HTML version.
+		$shortcode_content = do_shortcode( $content );
+		$this->assertContains( '<div class="gist-oembed" data-gist="' . $expected_gist_id . '"></div>', $shortcode_content );
+
+		// Test AMP version.
+		add_filter( 'jetpack_is_amp_request', '__return_true' );
+		$shortcode_content = do_shortcode( $content );
+		$this->assertEquals(
+			sprintf(
+				'<amp-gist layout="fixed-height" data-gistid="%1$s" height="240" data-file="%2$s"></amp-gist>',
+				$gist_id,
+				$file_name
+			),
+			$shortcode_content
+		);
+	}
+
+	/**
+	 * Verify that a shortcode linking to a specific file, with a username returns the expected embed code.
+	 *
+	 * @covers ::github_gist_shortcode
+	 *
+	 * @since 7.3.0
+	 */
+	public function test_shortcodes_gist_username_direct_file() {
+		$gist_id        = 'jeherve/57cc50246aab776e110060926a2face2';
+		$file_name_slug = '#file-wp-config-php';
+		$file_name      = 'wp-config.php';
+
+		$expected_gist_id = sprintf(
+			'%1$s.json?file=%2$s',
+			basename( $gist_id ),
+			$file_name
+		);
+
+		$content = '[gist https://gist.github.com/' . $gist_id . $file_name_slug . ']';
+
+		// Test HTML version.
+		$shortcode_content = do_shortcode( $content );
+		$this->assertContains( '<div class="gist-oembed" data-gist="' . $expected_gist_id . '"></div>', $shortcode_content );
+
+		// Test AMP version.
+		add_filter( 'jetpack_is_amp_request', '__return_true' );
+		$shortcode_content = do_shortcode( $content );
+		$this->assertEquals(
+			sprintf(
+				'<amp-gist layout="fixed-height" data-gistid="%1$s" height="240" data-file="%2$s"></amp-gist>',
+				basename( $gist_id ),
+				$file_name
+			),
 			$shortcode_content
 		);
 	}
@@ -269,7 +342,7 @@ class WP_Test_Jetpack_Shortcodes_Gist extends WP_UnitTestCase {
 		ob_start();
 		the_content();
 		$actual = ob_get_clean();
-		$this->assertContains( '<div class="gist-oembed" data-gist="' . $gist_id . '.json?file=wp-config.php"></div>', $actual );
+		$this->assertContains( '<div class="gist-oembed" data-gist="' . basename( $gist_id ) . '.json?file=wp-config.php"></div>', $actual );
 
 		// Test AMP version.
 		add_filter( 'jetpack_is_amp_request', '__return_true' );
@@ -277,7 +350,7 @@ class WP_Test_Jetpack_Shortcodes_Gist extends WP_UnitTestCase {
 		the_content();
 		$actual = ob_get_clean();
 		$this->assertEquals(
-			wpautop( sprintf( '<amp-gist layout="fixed-height" data-gistid="%s" height="240" data-file="wp.config.php"></amp-gist>', basename( $gist_id ) ) ),
+			wpautop( sprintf( '<amp-gist layout="fixed-height" data-gistid="%s" height="240" data-file="wp-config.php"></amp-gist>', basename( $gist_id ) ) ),
 			$actual
 		);
 	}
