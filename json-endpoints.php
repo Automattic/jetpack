081c7f04fd2c441853fcc371ba1e834e9e6f99ad
mmtr <miguel.torres@automattic.com>
2019-04-24 07:36:31 +0900

    Move require to the real v1.3 section

--- json-endpoints.php
+++ json-endpoints.php
@@ -117,13 +117,13 @@ $json_jetpack_endpoints_dir = dirname( __FILE__ ) . '/json-endpoints/jetpack/';
 
 // This files instantiates the endpoints
 require_once( $json_jetpack_endpoints_dir . 'json-api-jetpack-endpoints.php' );
-require_once( $json_endpoints_dir . 'class.wpcom-json-api-get-site-v1-3-endpoint.php' );
 
 // **********
 // v1.3
 // **********
 
 require_once( $json_endpoints_dir . 'class.wpcom-json-api-site-settings-v1-3-endpoint.php' );
+require_once( $json_endpoints_dir . 'class.wpcom-json-api-get-site-v1-3-endpoint.php' );
 
 // **********
 // v1.4
