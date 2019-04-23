92cd73e9005baf82a7937cd1f098a88d90cfe892
Bernie Reiter <ockham@raz.or.at>
2019-04-23 19:02:00 +0600

    Doesn't need to be an async action

--- _inc/client/state/initial-state/actions.js
+++ _inc/client/state/initial-state/actions.js
@@ -3,11 +3,7 @@
  */
 import { JETPACK_SET_INITIAL_STATE } from 'state/action-types';
 
-export const setInitialState = () => {
-	return dispatch => {
-		dispatch( {
-			type: JETPACK_SET_INITIAL_STATE,
-			initialState: window.Initial_State,
-		} );
-	};
-};
+export const setInitialState = () => ( {
+	type: JETPACK_SET_INITIAL_STATE,
+	initialState: window.Initial_State,
+} );
