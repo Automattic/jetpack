5e1d0c4b473ea31713808e9e0187944fa80275df
dereksmart <smart@automattic.com>
2019-04-24 13:38:34 +0000

    Reorder to match wpcom

--- modules/shortcodes/unavailable.php
+++ modules/shortcodes/unavailable.php
@@ -72,8 +72,8 @@ function jetpack_init_shortcode_unavailable() {
 		array(
 			'digg'                    => __( 'The Digg API was shut down in 2014.', 'jetpack' ),
 			'blip.tv'                 => __( 'The Blip.tv service has been shut down since August 20th, 2015.', 'jetpack' ),
-			'lytro'                   => __( 'Lytro has been shut down since March 2019.', 'jetpack' ),
 			'jetpack-email-subscribe' => __( 'The Email Subscribe shortcode is now available as a block in the Block editor.', 'jetpack' ),
+			'lytro'                   => __( 'Lytro has been shut down since March 2019.', 'jetpack' ),
 		)
 	);
 }
