794074f34e2e58ad38a4a66bbf6f5ec1fb93903e
Miguel Torres <miguelmariatorresrojas@gmail.com>
2019-04-26 05:24:24 +0900

    API: Switch site frame nonce to user+site value (#12095)
    * API: Switch site frame nonce to user+site value
    
    * Correct undefined method errors for `get_frame_nonce_site_only`.
    
    * Move the require down to the v1.3 section.
    
    * Move require to the real v1.3 section
    
    * Add method comments
    
    * Add more context to method docs

--- json-endpoints.php
+++ json-endpoints.php
@@ -123,6 +123,7 @@ require_once( $json_jetpack_endpoints_dir . 'json-api-jetpack-endpoints.php' );
 // **********
 
 require_once( $json_endpoints_dir . 'class.wpcom-json-api-site-settings-v1-3-endpoint.php' );
+require_once( $json_endpoints_dir . 'class.wpcom-json-api-get-site-v1-3-endpoint.php' );
 
 // **********
 // v1.4
