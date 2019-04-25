926490bc4a5e0f20ab72fc6e081292151b5f3d2d
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2019-04-25 16:48:10 -0300

    Removes all After the Deadline references from Jetpack (#11772)

--- _inc/client/components/navigation-settings/index.jsx
+++ _inc/client/components/navigation-settings/index.jsx
@@ -132,7 +132,6 @@ export const NavigationSettings = createReactClass( {
 					{ this.props.hasAnyOfTheseModules( [
 						'masterbar',
 						'markdown',
-						'after-the-deadline',
 						'custom-content-types',
 						'post-by-email',
 						'infinite-scroll',
@@ -207,7 +206,7 @@ export const NavigationSettings = createReactClass( {
 			}
 			navItems = (
 				<NavTabs selectedText={ this.props.route.name }>
-					{ this.props.hasAnyOfTheseModules( [ 'after-the-deadline', 'post-by-email' ] ) && (
+					{ this.props.hasAnyOfTheseModules( [ 'post-by-email' ] ) && (
 						<NavItem
 							path="#writing"
 							onClick={ this.handleClickForTracking( 'writing' ) }
