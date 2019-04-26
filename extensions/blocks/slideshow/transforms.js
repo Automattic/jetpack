d1c204c0c1ca7690244730eb91b4d433fef4a4a1
Mikael Korpela <mikael@ihminen.org>
2019-04-26 11:29:40 +0300

    and the same for Slideshow

--- extensions/blocks/slideshow/transforms.js
+++ extensions/blocks/slideshow/transforms.js
@@ -4,20 +4,48 @@
 import { createBlock } from '@wordpress/blocks';
 import { filter } from 'lodash';
 
+/**
+ * Filter valid images
+ *
+ * @param {array} images Array of image objects
+ * @return {array} Array of image objects which have id and url
+ */
+function getValidImages( images ) {
+	return filter( images, ( { id, url } ) => id && url );
+}
+
 const transforms = {
 	from: [
 		{
 			type: 'block',
+			isMultiBlock: true,
+			blocks: [ 'core/image' ],
+			isMatch: attributes => getValidImages( attributes ).length > 0,
+			transform: attributes => {
+				const validImages = getValidImages( attributes );
+				return createBlock( 'jetpack/slideshow', {
+					images: validImages.map( ( { alt, caption, id, url } ) => ( {
+						alt,
+						caption,
+						id,
+						url,
+					} ) ),
+					ids: validImages.map( ( { id } ) => id ),
+				} );
+			},
+		},
+		{
+			type: 'block',
 			blocks: [ 'core/gallery', 'jetpack/tiled-gallery' ],
 			transform: attributes => {
-				const validImages = filter( attributes.images, ( { id, url } ) => id && url );
+				const validImages = getValidImages( attributes );
 				if ( validImages.length > 0 ) {
 					return createBlock( 'jetpack/slideshow', {
-						images: validImages.map( ( { id, url, alt, caption } ) => ( {
-							id,
-							url,
+						images: validImages.map( ( { alt, caption, id, url } ) => ( {
 							alt,
 							caption,
+							id,
+							url,
 						} ) ),
 					} );
 				}
