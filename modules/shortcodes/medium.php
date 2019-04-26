f329a822d8c78ff05db31f750faf2d8bef31eab1
Jeremy Herve <jeremy@jeremy.hu>
2019-04-26 10:50:01 +0200

    Medium: fix all phpcs warnings
    Also change the display of the HTML markup added alongside the embed script. Instead of displaying a text in English all the time, let's make that text translatable. Since part of the English string came from the embed type before, I simplified the string.

--- modules/shortcodes/medium.php
+++ modules/shortcodes/medium.php
@@ -1,16 +1,24 @@
 <?php
-
-// Embed support for Medium https://medium.com/p/3eaed64aed8a
-
 /**
- * Faux-oembed support for Medium permalinks
+ * Embed support for Medium
  *
- * e.g.
- * https://medium.com/help-center
- * https://medium.com/@richroll
+ * Supported formats:
+ * https://medium.com/p/3eaed64aed8a
+ * [medium url="https://medium.com/help-center" width="100%" border="false" collapsed="true"]
+ *
+ * @package Jetpack
  */
+
+// Faux-oembed support for Medium permalinks.
 wp_embed_register_handler( 'medium', '#^https?://medium.com/([a-zA-z0-9-_@]+)#', 'jetpack_embed_medium_oembed' );
 
+/**
+ * Callback to modify output of embedded Medium posts.
+ *
+ * @param array $matches Regex partial matches against the URL passed.
+ * @param array $attr    Attributes received in embed response.
+ * @param array $url     Requested URL to be embedded.
+ */
 function jetpack_embed_medium_oembed( $matches, $attr, $url ) {
 	$attr        = jetpack_embed_medium_args( $attr );
 	$attr['url'] = $url;
@@ -18,6 +26,11 @@ function jetpack_embed_medium_oembed( $matches, $attr, $url ) {
 	return jetpack_embed_medium_embed_html( $attr );
 }
 
+/**
+ * Return custom markup to display a Medium profile, collection, or story.
+ *
+ * @param array $args Attributes received in embed response.
+ */
 function jetpack_embed_medium_embed_html( $args ) {
 	$args = jetpack_embed_medium_args( $args );
 
@@ -27,16 +40,30 @@ function jetpack_embed_medium_embed_html( $args ) {
 
 	$args['type'] = jetpack_embed_medium_get_embed_type( $args['url'] );
 
-	return sprintf( '<script async src="https://static.medium.com/embed.js"></script><a class="m-%1$s" href="%2$s" target="_blank" data-width="%3$s" data-border="%4$s" data-collapsed="%5$s">View %1$s at Medium.com</a>', esc_attr( $args['type'] ), esc_url( $args['url'] ), esc_attr( $args['width'] ), esc_attr( $args['border'] ), esc_attr( $args['collapsed'] ) );
+	wp_enqueue_script(
+		'medium-embed',
+		'https://static.medium.com/embed.js',
+		array(),
+		JETPACK__VERSION,
+		true
+	);
+
+	return sprintf(
+		'<a class="m-%1$s" href="%2$s" target="_blank" data-width="%3$s" data-border="%4$s" data-collapsed="%5$s">%6$s</a>',
+		esc_attr( $args['type'] ),
+		esc_url( $args['url'] ),
+		esc_attr( $args['width'] ),
+		esc_attr( $args['border'] ),
+		esc_attr( $args['collapsed'] ),
+		esc_html__( 'View at Medium.com', 'jetpack' )
+	);
 }
 
 /**
  * Shortcode support that allows passing in URL
  *
- * [medium url="https://medium.com/help-center" width="100%" border="false" collapsed="true"]
+ * @param array $atts Shortcode attributes.
  */
-add_shortcode( 'medium', 'jetpack_embed_medium_shortcode' );
-
 function jetpack_embed_medium_shortcode( $atts ) {
 	$atts = jetpack_embed_medium_args( $atts );
 
@@ -45,9 +72,15 @@ function jetpack_embed_medium_shortcode( $atts ) {
 		return $wp_embed->shortcode( $atts, $atts['url'] );
 	}
 }
+add_shortcode( 'medium', 'jetpack_embed_medium_shortcode' );
 
+/**
+ * Get embed type (profile, collection, or story) based on Medium URL.
+ *
+ * @param string $url Medium URL.
+ */
 function jetpack_embed_medium_get_embed_type( $url ) {
-	$url_path = parse_url( $url, PHP_URL_PATH );
+	$url_path = wp_parse_url( $url, PHP_URL_PATH );
 	if ( preg_match( '/^\/@[\.\w]+$/', $url_path ) ) {
 		return 'profile';
 	} elseif ( preg_match( '/^\/[\da-zA-Z-]+$/', $url_path ) ) {
@@ -57,6 +90,11 @@ function jetpack_embed_medium_get_embed_type( $url ) {
 	return 'story';
 }
 
+/**
+ * Process Medium shortcode attributes.
+ *
+ * @param array $atts Shortcode attributes.
+ */
 function jetpack_embed_medium_args( $atts ) {
 	return shortcode_atts(
 		array(
