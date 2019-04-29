0256b97c0049c7ec6ccd9e3e08e98e81c4f053d0
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2019-04-29 18:28:45 +0300

    add explanation why we removing all actions

--- tests/php/general/test-class.jetpack-gutenberg.php
+++ tests/php/general/test-class.jetpack-gutenberg.php
@@ -23,6 +23,9 @@ class WP_Test_Jetpack_Gutenberg extends WP_UnitTestCase {
 
 		add_filter( 'jetpack_set_available_extensions', array( __CLASS__, 'get_extensions_whitelist' ) );
 		delete_option( 'jetpack_excluded_extensions' );
+
+		// These action causing issues in tests in WPCOM context. Since we are not using any real block here,
+		// and we are testing block availability with block stubs - we safe to remove these actions for these tests.
 		remove_all_actions( 'jetpack_register_gutenberg_extensions' );
 		Jetpack_Gutenberg::init();
 	}
