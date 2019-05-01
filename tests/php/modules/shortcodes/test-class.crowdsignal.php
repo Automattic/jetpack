c6f8c665637c04013b0e9c3001eb418dc9b44b3e
Jeremy Herve <jeremy@jeremy.hu>
2019-05-01 17:26:37 +0200

    Add a first set of basic tests for Polldaddy and Crowdsignal polls

--- tests/php/modules/shortcodes/test-class.crowdsignal.php
+++ tests/php/modules/shortcodes/test-class.crowdsignal.php
@@ -38,4 +38,43 @@ class WP_Test_Jetpack_Shortcodes_CrowdSignal extends WP_UnitTestCase {
 		$this->assertNotEquals( $content, $shortcode_content );
 	}
 
+	/**
+	 * Test a basic legacy Polldaddy poll.
+	 *
+	 * @since 7.4.0
+	 */
+	public function test_shortcodes_polldaddy_poll() {
+		$id      = 9541291;
+		$content = '[polldaddy poll=' . $id . ']';
+
+		$shortcode_content = do_shortcode( $content );
+
+		$this->assertEquals(
+			sprintf(
+				'<a id="pd_a_%1$d"></a><div class="CSS_Poll PDS_Poll" id="PDI_container%1$d" style="display:inline-block;"></div><div id="PD_superContainer"></div><noscript><a href="https://polldaddy.com/p/%1$d" target="_blank">Take Our Poll</a></noscript>',
+				$id
+			),
+			$shortcode_content
+		);
+	}
+
+	/**
+	 * Test a basic Crowdsignal poll.
+	 *
+	 * @since 7.4.0
+	 */
+	public function test_shortcodes_crowdsignal_poll() {
+		$id      = 9541291;
+		$content = '[crowdsignal poll=' . $id . ']';
+
+		$shortcode_content = do_shortcode( $content );
+
+		$this->assertEquals(
+			sprintf(
+				'<a id="pd_a_%1$d"></a><div class="CSS_Poll PDS_Poll" id="PDI_container%1$d" style="display:inline-block;"></div><div id="PD_superContainer"></div><noscript><a href="https://poll.fm/%1$d" target="_blank">Take Our Poll</a></noscript>',
+				$id
+			),
+			$shortcode_content
+		);
+	}
 }
