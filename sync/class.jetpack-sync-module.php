293bdef9e899eb262c3f16f3ed675da60fa8a2d6
Enej Bajgoric <enej.bajgoric@gmail.com>
2019-05-02 20:57:18 -0700

    Fix the tests. enqueue_full_sync_actions should return null if nothing is used.

--- sync/class.jetpack-sync-module.php
+++ sync/class.jetpack-sync-module.php
@@ -30,7 +30,7 @@ abstract class Jetpack_Sync_Module {
 
 	public function enqueue_full_sync_actions( $config, $max_items_to_enqueue, $state ) {
 		// in subclasses, return the number of actions enqueued, and next module state (true == done)
-		return array( 0, true );
+		return array( null, true );
 	}
 
 	public function estimate_full_sync_actions( $config ) {
