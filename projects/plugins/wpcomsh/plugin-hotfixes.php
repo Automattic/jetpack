ae2ff67073d9605038fe6d0186de7fc141a40508
Jennifer M. Dodd <jmdodd@users.noreply.github.com>
2019-05-08 16:42:11 -0400

    Revert "Add Jetpack hamburger toggle hotfix https://github.com/Automattic/wpcomsh/pull/261" https://github.com/Automattic/wpcomsh/pull/262
    This reverts commit 3af9b87078c0b6fc4a80acd95244fc2e2a7f5252.

--- projects/plugins/wpcomsh/plugin-hotfixes.php
+++ projects/plugins/wpcomsh/plugin-hotfixes.php
@@ -5,10 +5,3 @@ function wpcomsh_hotfix_cherry_core_base_url( $url ) {
 	return str_replace( WP_CONTENT_DIR, '/wp-content/', $url );
 }
 add_filter( 'cherry_core_base_url', 'wpcomsh_hotfix_cherry_core_base_url' );
-
-// See: https://github.com/Automattic/jetpack/pull/12299
-function wpcomsh_hotfix_jetpack_add_hamburger_toggle() {
-	global $wp_admin_bar;
-	wp_admin_bar_sidebar_toggle( $wp_admin_bar );
-}
-add_action( 'wp_before_admin_bar_render', 'wpcomsh_hotfix_jetpack_add_hamburger_toggle', 100000 );
