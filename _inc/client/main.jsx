d9697b57e93a376e25f52e40dc5fbdbc686c9d69
AnnaMag <anna.mag.kedzierska@gmail.com>
2019-05-08 15:01:22 +0200

    Use blog_id property to be picked up but Tracks ETL and set in its respetive higher level property.

--- _inc/client/main.jsx
+++ _inc/client/main.jsx
@@ -120,7 +120,9 @@ class Main extends React.Component {
 		const tracksUser = this.props.tracksUserData;
 
 		if ( tracksUser ) {
-			analytics.initialize( tracksUser.userid, tracksUser.username, { blogid: tracksUser.blogid } );
+			analytics.initialize( tracksUser.userid, tracksUser.username, {
+				blog_id: tracksUser.blogid,
+			} );
 		}
 	};
 
