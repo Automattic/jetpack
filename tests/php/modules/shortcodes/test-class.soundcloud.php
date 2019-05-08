89eeaea3ccc1a2f1411213acc09c5d524f18dcdc
Jeremy Herve <jeremy@jeremy.hu>
2019-05-08 18:36:58 +0200

    Soundcloud: simplify shortcode and fix all phpcs warnings (#12221)
    * Soundcloud: simplify shortcode and fix all phpcs warnings
    
    The shortcode was previously built off the old Soundcloud plugin. This simplifies some of the code, and brings it more in line with our other shortcodes.
    Functionality should not change.
    
    * Add new tests to cover soundcloud albums/tracks, and their height
    
    * Make sure the color parameter is added to player url when valid
    
    Fixes #12265

--- tests/php/modules/shortcodes/test-class.soundcloud.php
+++ tests/php/modules/shortcodes/test-class.soundcloud.php
@@ -30,7 +30,7 @@ class WP_Test_Jetpack_Shortcodes_Soundcloud extends WP_UnitTestCase {
 		$shortcode_content = do_shortcode( $content );
 
 		$this->assertContains( '<iframe width="100%" height="450"', $shortcode_content );
-		$this->assertContains( 'w.soundcloud.com/player/?url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F156661852&visual=true&auto_play=false&hide_related=false', $shortcode_content );
+		$this->assertContains( 'w.soundcloud.com/player/?url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F156661852&auto_play=false&hide_related=false&visual=true', $shortcode_content );
 	}
 
 	public function test_shortcodes_implicit_non_visual() {
@@ -49,6 +49,48 @@ class WP_Test_Jetpack_Shortcodes_Soundcloud extends WP_UnitTestCase {
 
 		$this->assertContains( '<iframe width="100%" height="450"', $shortcode_content );
 		$this->assertContains( 'w.soundcloud.com/player/?url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F156661852&auto_play=false&hide_related=false', $shortcode_content );
+	}
+
+	/**
+	 * Test single tracks with no height specified.
+	 *
+	 * @since 7.4.0
+	 */
+	public function tests_shortcodes_soundcloud_single_track_no_height() {
+		$content = '[soundcloud url="https://soundcloud.com/closetorgan/paul-is-dead"]';
+
+		$shortcode_content = do_shortcode( $content );
+
+		$this->assertContains( '<iframe width="100%" height="166"', $shortcode_content );
+		$this->assertContains( 'w.soundcloud.com/player/?url=https%3A%2F%2Fsoundcloud.com%2Fclosetorgan%2Fpaul-is-dead&width=false&height=false&auto_play=false&hide_related=false&visual=false&show_comments=false&color=false&show_user=false&show_reposts=false', $shortcode_content );
+	}
+
+	/**
+	 * Tests albums with no height specified.
+	 *
+	 * @since 7.4.0
+	 */
+	public function tests_shortcodes_soundcloud_album_no_height() {
+		$content = '[soundcloud url="https://soundcloud.com/closetorgan/sets/smells-like-lynx-africa-private"]';
+
+		$shortcode_content = do_shortcode( $content );
+
+		$this->assertContains( '<iframe width="100%" height="450"', $shortcode_content );
+		$this->assertContains( 'w.soundcloud.com/player/?url=https%3A%2F%2Fsoundcloud.com%2Fclosetorgan%2Fsets%2Fsmells-like-lynx-africa-private&width=false&height=false&auto_play=false&hide_related=false&visual=false&show_comments=false&color=false&show_user=false&show_reposts=false', $shortcode_content );
+	}
+
+	/**
+	 * Tests albums with a custom color.
+	 *
+	 * @since 7.4.0
+	 */
+	public function tests_shortcodes_soundcloud_album_custom_color() {
+		$content = '[soundcloud url="https://soundcloud.com/closetorgan/sets/smells-like-lynx-africa-private" color="00cc11"]';
+
+		$shortcode_content = do_shortcode( $content );
+
+		$this->assertContains( '<iframe width="100%" height="450"', $shortcode_content );
+		$this->assertContains( 'w.soundcloud.com/player/?url=https%3A%2F%2Fsoundcloud.com%2Fclosetorgan%2Fsets%2Fsmells-like-lynx-africa-private&width=false&height=false&auto_play=false&hide_related=false&visual=false&show_comments=false&show_user=false&show_reposts=false&color=00cc11', $shortcode_content );
 	}
 
 	/**
