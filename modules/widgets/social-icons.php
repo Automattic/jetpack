22ef0cbdca4e79efe5e36c8c282949a2d499c264
Jeremy Herve <jeremy@jeremy.hu>
2019-05-10 18:54:55 +0200

    Social Icons SVG: switch to the presentation role
    Fixes #12338
    
    Since the image is mostly decorative, and since we already hide it from screen readers with `aria-hidden="true"`, let's also set it as a decorative element.

--- modules/widgets/social-icons.php
+++ modules/widgets/social-icons.php
@@ -364,7 +364,7 @@ class Jetpack_Widget_Social_Icons extends WP_Widget {
 		$aria_hidden = ' aria-hidden="true"';
 
 		// Begin SVG markup.
-		$svg = '<svg class="icon icon-' . esc_attr( $args['icon'] ) . '"' . $aria_hidden . ' role="img">';
+		$svg = '<svg class="icon icon-' . esc_attr( $args['icon'] ) . '"' . $aria_hidden . ' role="presentation">';
 
 		/*
 		 * Display the icon.
@@ -526,7 +526,7 @@ class Jetpack_Widget_Social_Icons extends WP_Widget {
 				'url'   => 'goodreads.com',
 				'icon'  => 'goodreads',
 				'label' => 'Goodreads',
-			),			
+			),
 			array(
 				'url'   => 'google.com',
 				'icon'  => 'google',
