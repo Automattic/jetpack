47c55bf18dc2e99963b1449e0fff672febd71db0
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2019-05-13 14:30:29 +0300

    Add a build script

---
+++ tools/build-jetpack-in-ci.php
@@ -0,0 +1,112 @@
+<?php
+
+$commit = get_env( 'TRAVIS_COMMIT' );
+$repo   = get_env( 'TRAVIS_REPO_SLUG' );
+$branch = get_env( 'TRAVIS_BRANCH' );
+$pr     = get_env( 'TRAVIS_PULL_REQUEST' );
+
+// Remove anything which isn't a word, whitespace, number
+// or any of the following caracters -_~,;[]().
+$sanitized_branch = mb_ereg_replace( "([^\w\s\d\-_~,;\[\]\(\).])", '_', $branch );
+
+if (
+	'master' !== $branch
+	&& stripos( $branch, 'branch-' ) !== 0
+	&& stripos( $branch, 'feature/' ) !== 0
+	&& ! $pr
+) {
+	// $cmd = 'mv ' . $argv[1] . ' ' . dirname( $argv[1] ) . '/processed-commit.txt';
+	// system( $cmd );
+	error_log( 'Did not process: because it isn\'t master, a release branch, or a PR.');
+	exit;
+}
+
+echo $commit . "\n";
+echo $repo . "\n";
+echo $branch . "\n";
+echo $pr . "\n";
+echo $sanitized_branch . "\n";
+
+define( 'BUILDER_DIR', __DIR__ );
+define( 'TEMP_DIR', '/tmp' );
+
+$destination_path = BUILDER_DIR . '/target';
+$build_path       = TEMP_DIR . '/jetpack';
+$cmd              = array();
+
+// Move into the Jetpack repo directory & build.
+$cmd[] = './tools/build-jetpack.sh -d -b '
+	. $branch . ' '
+	. $repo . ' '
+	. $build_path;
+
+if ( ! file_exists( $destination_path ) ) {
+	$cmd[] = "mkdir $destination_path";
+}
+
+// Grab version info for destination.
+$cmd[] = "mv $build_path/version.txt $destination_path";
+
+// Done in one command so that the working dir persists.
+$cmd[] = 'cd /tmp && '
+	. 'mv jetpack jetpack-dev && '
+	. 'zip -9r jetpack-dev.zip jetpack-dev && '
+	. 'mv /tmp/jetpack-dev.zip ' . $destination_path;
+
+foreach ( $cmd as $c ) {
+	// exec( $c, $output, $exit );
+	echo $c;
+	// if ( 0 !== $exit ) {
+	if ( 0 !== 0 ) {
+		echo( 'Something went wrong: ' );
+		echo( 'Command `' . $c . '\' exited with code ' . $exit );
+
+		exit;
+	}
+}
+
+$version = trim( file_get_contents( $destination_path . '/version.txt' ) );
+
+// Process the JSON file.
+// $branches = (object) json_decode( file_get_contents( PUBLIC_DIR . '/jetpack-branches.json' ) );
+
+$n = array(
+	'branch'       => $branch,
+	'commit'       => $commit,
+	'download_url' => 'https://betadownload.jetpack.me/branches/' . $sanitized_branch . '/jetpack-dev.zip',
+	'update_date'  => date( 'Y-m-d H:i:s' ),
+	'version'      => $version,
+	'pr'           => $pr,
+);
+
+
+echo $n . "\n";
+
+// if ( 'master' === $branch ) {
+// 	$branches->{ $sanitized_branch } = $n;
+// } elseif ( 1 === preg_match( '/^branch-\d+\.\d+$/', $branch ) ) {
+// 	$branches->rc = $n;
+// } elseif ( $pr ) {
+// 	$branches->pr->{ $sanitized_branch } = $n;
+// }
+
+// file_put_contents(
+// 	PUBLIC_DIR . '/jetpack-branches.json',
+// 	json_encode( (object) $branches )
+// );
+
+file_put_contents(
+	$destination_path . '/build-info.json',
+	json_encode(
+		(object) array(
+			'sanitized_path' => $sanitized_branch,
+			'repo'           => $repo,
+			'commit'         => $commit,
+			'branch'         => $branch,
+			'pr'             => $pr,
+			'running'        => date( 'Y-m-d H:i:s' ),
+			'version'        => $version,
+			'path'           => $destination_path,
+		)
+	)
+);
