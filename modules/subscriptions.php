b48cfa59daca7dbdc2f1d01e3dfa6f59823875f0
Jeremy Herve <jeremy@jeremy.hu>
2019-05-14 23:02:20 +0200

    Subscriptions: display checkboxes above submit button.
    Fixes https://github.com/Automattic/jetpack/pull/12273#discussion_r283994147

--- modules/subscriptions.php
+++ modules/subscriptions.php
@@ -670,7 +670,7 @@ class Jetpack_Subscriptions {
 		 */
 		$str = apply_filters( 'jetpack_comment_subscription_form', $str );
 
-		return $submit_button . $str;
+		return $str . $submit_button;
 	}
 
 	/**
