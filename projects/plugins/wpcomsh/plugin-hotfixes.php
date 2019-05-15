71c33e9805be8069d6ac3e2f296547d6b24199b0
Peter Westwood <peter.westwood@ftwr.co.uk>
2019-05-15 10:59:44 -0600

    Plugin Hotfix: Stop akismet verify-key being called by Jetpack as it is pointless when we have a platform defined key and it doesn't this every 10 minutes.

--- projects/plugins/wpcomsh/plugin-hotfixes.php
+++ projects/plugins/wpcomsh/plugin-hotfixes.php
@@ -5,3 +5,10 @@ function wpcomsh_hotfix_cherry_core_base_url( $url ) {
 	return str_replace( WP_CONTENT_DIR, '/wp-content/', $url );
 }
 add_filter( 'cherry_core_base_url', 'wpcomsh_hotfix_cherry_core_base_url' );
+
+// We define a Akismet Key at the Platform Level which is always assumed to be valid so don't check it all the time.
+// Related: https://github.com/Automattic/jetpack/issues/12382
+function wpcomsh_pre_transient_jetpack_akismet_key_is_valid( $_false ) {
+    return 'valid';
+}
+add_filter( 'pre_transient_jetpack_akismet_key_is_valid', 'wpcomsh_pre_transient_jetpack_akismet_key_is_valid' );
