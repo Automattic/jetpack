f36a4b1457fed8b7b34621aa9452a12cc56b2c90
Jeremy Herve <jeremy@jeremy.hu>
2019-05-15 15:12:13 +0200

    Connect flow: sanitize "from" parameter when building connect url
    Follow-up from #12351
    
    Since one can currently pass any "from" parameter when building that URL, let's sanitize that value.

--- class.jetpack.php
+++ class.jetpack.php
@@ -4519,7 +4519,7 @@ p {
 		}
 
 		if ( $from ) {
-			$url = add_query_arg( 'from', $from, $url );
+			$url = add_query_arg( 'from', sanitize_key( $from ), $url );
 		}
 
 		// Ensure that class to get the affiliate code is loaded
