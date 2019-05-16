286d10c356ad1d41e47ff2c408b3c7d171746562
Enej Bajgoric <enej.bajgoric@gmail.com>
2019-05-16 11:40:51 -0700

    Stash

---
+++ packages/jetpack-logo/tests/test_logo.php
@@ -0,0 +1,31 @@
+<?php
+
+use Jetpack\Assets\Logo;
+
+class WP_Test_Logo extends WP_UnitTestCase {
+
+	function test_render_default_logo() {
+		$output = Logo::render();
+		$expected = home_url( '/wp-content/plugins/jetpack/packages/logo/assets/logo.svg' );
+		$this->assertContains( $expected, $output );
+	}
+
+	function test_render_custom_logo() {
+		$example_logo = 'https://wordpress.com/logo.png';
+		$output = Logo::render( $example_logo );
+		$this->assertContains( $example_logo, $output );
+	}
+
+	function test_render_img_tag() {
+		$output = Logo::render();
+		$url = home_url( '/wp-content/plugins/jetpack/packages/logo/assets/logo.svg' );
+		// Contains only a valid img tag.
+		$this->assertRegExp( '/^<img.*\/>$/', $output );
+		// Contains the expected src attribute.
+		$this->assertRegExp( '/.+src="' . preg_quote( $url, '/' ) . '".+/', $output );
+		// Contains the expected class attribute.
+		$this->assertRegExp( '/.+class="jetpack-logo".+/', $output );
+		// Contains an alt attribute.
+		$this->assertRegExp( '/.+alt="[^"]+".+/', $output );
+	}
+}
---
+++ packages/jetpack-logo/tests/wp-tests-config.php
@@ -0,0 +1,11 @@
+<?php
+define( 'DB_NAME', 'tests' );
+define( 'DB_USER', 'user' );
+define( 'DB_PASSWORD', 'password' );
+define( 'DB_HOST', 'localhost' );
+
+define( 'WP_TESTS_DOMAIN', 'tests.loc' );
+//Root of your site
+define( 'WP_TESTS_DIR', dirname( __DIR__ ) );
+
+define( 'DOMAIN_CURRENT_SITE', 'starting-point.loc' );
