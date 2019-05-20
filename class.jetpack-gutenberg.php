3742ae98b6e275337a5a825f177e1407db780293
Weston Ruter <westonruter@google.com>
2019-05-20 13:13:56 -0700

    Add AMP support for GIF block (#11318)
    * Add AMP compatibility for Gif block, incl. oEmbed provider for Giphy
    
    * Skip enqueueing block JS asset in AMP response
    
    * Add default values for width and height
    
    * Implement Giphy oEmbed upgrade to replace GIF image with embed iframe
    
    * Remove Giphy oEmbed implementation. Adjust iframe styling selector to be ignored in AMP context. Use paddingTop attribute to accurately size amp-iframe.
    
    * Class existence checks on Jetpack_AMP_Support for smooth WP.com integration, and one absint() replacing an intval().
    
    
    Co-authored-by: Jefferson Rabb <jeff@atavist.net>

--- class.jetpack-gutenberg.php
+++ class.jetpack-gutenberg.php
@@ -497,7 +497,7 @@ class Jetpack_Gutenberg {
 			: array();
 		$script_dependencies = array_merge( $script_dependencies, $dependencies, array( 'wp-polyfill' ) );
 
-		if ( self::block_has_asset( $script_relative_path ) ) {
+		if ( ( ! class_exists( 'Jetpack_AMP_Support' ) || ! Jetpack_AMP_Support::is_amp_request() ) && self::block_has_asset( $script_relative_path ) ) {
 			$script_version = self::get_asset_version( $script_relative_path );
 			$view_script    = plugins_url( $script_relative_path, JETPACK__PLUGIN_FILE );
 			wp_enqueue_script( 'jetpack-block-' . $type, $view_script, $script_dependencies, $script_version, false );
