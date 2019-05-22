ec135988a715c74ccc429e7bed18adc4ce6068e0
Marin Atanasov <tyxla@abv.bg>
2019-05-22 12:12:35 +0300

    Fix tests

--- tests/php/packages/logo/test_Logo.php
+++ tests/php/packages/logo/test_Logo.php
@@ -6,29 +6,18 @@ class WP_Test_Logo extends WP_UnitTestCase {
 	function test_constructor_default_logo() {
 		$logo = new Logo();
 		$expected = home_url( '/wp-content/plugins/jetpack/packages/logo/assets/logo.svg' );
-		$this->assertContains( $expected, $logo->get_image() );
+		$this->assertContains( $expected, $logo->render() );
 	}
 
 	function test_constructor_custom_logo() {
 		$example_logo = 'https://wordpress.com/logo.png';
 		$logo = new Logo( $example_logo );
-		$this->assertContains( $example_logo, $logo->get_image() );
-	}
-
-	function test_render_default_logo() {
-		$output = Logo::render();
-		$expected = home_url( '/wp-content/plugins/jetpack/packages/logo/assets/logo.svg' );
-		$this->assertContains( $expected, $output );
-	}
-
-	function test_render_custom_logo() {
-		$example_logo = 'https://wordpress.com/logo.png';
-		$output = Logo::render( $example_logo );
-		$this->assertContains( $example_logo, $output );
+		$this->assertContains( $example_logo, $logo->render() );
 	}
 
 	function test_render_img_tag() {
-		$output = Logo::render();
+		$logo = new Logo();
+		$output = $logo->render();
 		$url = home_url( '/wp-content/plugins/jetpack/packages/logo/assets/logo.svg' );
 		// Contains only a valid img tag.
 		$this->assertRegExp( '/^<img.*\/>$/', $output );
