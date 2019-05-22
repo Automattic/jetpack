e2d4d447b7e52aedaa528dd671dc260245e835ce
Brandon Kraft <public@brandonkraft.com>
2019-05-22 16:31:44 -0500

    Improve inline comment

--- jetpack.php
+++ jetpack.php
@@ -156,7 +156,7 @@ if ( version_compare( phpversion(), JETPACK__MINIMUM_PHP_VERSION, '<' ) ) {
 
 // Load all the packages.
 $jetpack_autoloader = JETPACK__PLUGIN_DIR . '/vendor/autoload.php';
-if ( ( include $jetpack_autoloader ) === false ) { // include returns false if it can not the file. Otherwise, it returns 1 unless the included file returns a different value.
+if ( ( include $jetpack_autoloader ) === false ) { // include returns false if it can not include the file. Otherwise, it returns 1 or whatever value is returned by the file, if any.
 	if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
 		error_log(
 			sprintf(
