b4333ca38f47d39234e04f983a203978e3e1e6dd
Artur Piszek <artur.piszek@gmail.com>
2019-05-22 17:17:23 +0200

    Memberships: renewal period more visible (#12418)
    This shows renewal periods on button labels.
    
    * Surface the renewal period
    
    * Add space after /
    
    Suggestion from code review
    
    Co-Authored-By: Jeremy Herve <jeremy@tagada.hu>
    
    * Fix copypaste problem with one time subs
    
    * Fix currency issue

--- extensions/blocks/membership-button/edit.jsx
+++ extensions/blocks/membership-button/edit.jsx
@@ -5,7 +5,7 @@
 import classnames from 'classnames';
 import SubmitButton from '../../shared/submit-button';
 import apiFetch from '@wordpress/api-fetch';
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 import { trimEnd } from 'lodash';
 import formatCurrency, { getCurrencyDefaults } from '@automattic/format-currency';
 
@@ -143,6 +143,7 @@ class MembershipsButtonEdit extends Component {
 							title: result.title,
 							interval: result.interval,
 							price: result.price,
+							currency: result.currency,
 						},
 					] ),
 				} );
@@ -154,6 +155,20 @@ class MembershipsButtonEdit extends Component {
 				this.onError( result.message );
 			}
 		);
+	};
+
+	renderAmount = product => {
+		const amount = formatCurrency( parseFloat( product.price ), product.currency );
+		if ( product.interval === '1 month' ) {
+			return sprintf( __( '%s / month', 'jetpack' ), amount );
+		}
+		if ( product.interval === '1 year' ) {
+			return sprintf( __( '%s / year', 'jetpack' ), amount );
+		}
+		if ( product.interval === 'one-time' ) {
+			return amount;
+		}
+		return sprintf( __( '%s / %s', 'jetpack' ), amount, product.interval );
 	};
 
 	renderAddMembershipAmount = () => {
@@ -265,7 +280,7 @@ class MembershipsButtonEdit extends Component {
 					key={ product.id }
 					onClick={ () => this.setMembershipAmount( product.id ) }
 				>
-					{ formatCurrency( parseFloat( product.price ), product.currency ) }
+					{ this.renderAmount( product ) }
 				</Button>
 			) ) }
 		</div>
@@ -289,11 +304,11 @@ class MembershipsButtonEdit extends Component {
 			<InspectorControls>
 				<PanelBody title={ __( 'Product', 'jetpack' ) }>
 					<SelectControl
-						label="Membership plan"
+						label={ __( 'Membership plan', 'jetpack' ) }
 						value={ this.props.attributes.planId }
 						onChange={ this.setMembershipAmount }
 						options={ this.state.products.map( product => ( {
-							label: formatCurrency( parseFloat( product.price ), product.currency ),
+							label: this.renderAmount( product ),
 							value: product.id,
 							key: product.id,
 						} ) ) }
