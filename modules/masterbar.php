5e07f91cd0b00a21020e938cc8f90683be14ad97
Jeremy Herve <jeremy@jeremy.hu>
2019-05-23 22:12:15 +0200

    Masterbar: allow logging out of wpcom when logging out of site.
    Fixes #12456
    
    This change avoids initialiazing all of the Masterbar after admin_bar_init. We want the module to be available after logging out as well, so the function hooked into wp_logout can be triggered.

--- modules/masterbar.php
+++ modules/masterbar.php
@@ -13,14 +13,4 @@
 
 require dirname( __FILE__ ) . '/masterbar/masterbar.php';
 
-// In order to be able to tell if it's an AMP request or not we have to hook into parse_query at a later priority.
-add_action( 'admin_bar_init', 'jetpack_initialize_masterbar', 99 );
-
-/**
- * Initializes the Masterbar in case the request is not AMP.
- */
-function jetpack_initialize_masterbar() {
-	if ( ! Jetpack_AMP_Support::is_amp_request() ) {
-		new A8C_WPCOM_Masterbar();
-	}
-}
+new A8C_WPCOM_Masterbar();
