5e0603c7a3fe50d3c6d0d3420fcce3633abc1e7a
Peter Westwood <peter.westwood@ftwr.co.uk>
2019-05-24 17:46:47 -0600

    Remove the extra arguments we don't need to make the filtering work.

--- projects/plugins/wpcomsh/plugin-hotfixes.php
+++ projects/plugins/wpcomsh/plugin-hotfixes.php
@@ -7,11 +7,11 @@ function wpcomsh_hotfix_cherry_core_base_url( $url ) {
 add_filter( 'cherry_core_base_url', 'wpcomsh_hotfix_cherry_core_base_url' );
 
 // On Atomic v2 we require the path within the webroot to be the one passed to X-Accel-Redirect
-function wpcomsh_woocommerce_download_file_xsendfile_x_accel_redirect_file_path( $xsendfile_path, $file_path, $filename, $parsed_file_path ) {
+function wpcomsh_woocommerce_download_file_xsendfile_x_accel_redirect_file_path( $xsendfile_path ) {
 	if ( 0 === strpos( $xsendfile_path, 'srv/htdocs/' ) ) {
 		$xsendfile_path = substr_replace( $xsendfile_path, '', 0, /* strlen( 'srv/htdocs/' ) */ 11 );
 	}
 	return $xsendfile_path;
 }
-add_filter( 'woocommerce_download_file_xsendfile_x_accel_redirect_file_path', 'wpcomsh_woocommerce_download_file_xsendfile_x_accel_redirect_file_path', 10, 4 );
+add_filter( 'woocommerce_download_file_xsendfile_x_accel_redirect_file_path', 'wpcomsh_woocommerce_download_file_xsendfile_x_accel_redirect_file_path' );
 
