9d0d99689d2e80a2092ee317db04f429d20eebb6
Enej Bajgoric <enej.bajgoric@gmail.com>
2019-05-27 12:12:50 -0700

    fixes to the jetpack a custom plugin

--- docker/wordpress/wp-content/plugins/a/a.php
+++ docker/wordpress/wp-content/plugins/a/a.php
@@ -5,7 +5,7 @@
 
 use Jetpack\Assets\Logo;
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload_packages.php';
 
 add_action( 'plugins_loaded', function() {
 	add_filter( 'the_content', function() {
