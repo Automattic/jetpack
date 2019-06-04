8e0ceb4977f6524de6c2195cc143594c96928fd6
Michael D Adams <michael.d.adams@gmail.com>
2019-06-04 14:17:23 -0700

    Fix tests

--- tests/php/general/test_class.jetpack-xmlrpc-server.php
+++ tests/php/general/test_class.jetpack-xmlrpc-server.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once dirname( __FILE__ ) . '/../../../class.jetpack-xmlrpc-server.php';
-
 class WP_Test_Jetpack_XMLRPC_Server extends WP_UnitTestCase {
 	function test_xmlrpc_features_available() {
 		$server = new Jetpack_XMLRPC_Server();
