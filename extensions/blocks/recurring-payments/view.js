a9db5186cc192e2061a42fbfc62b539df9904d32
artpi <artur.piszek@gmail.com>
2019-06-04 10:51:25 +0200

    Fix call

--- extensions/blocks/recurring-payments/view.js
+++ extensions/blocks/recurring-payments/view.js
@@ -25,7 +25,7 @@ function handleIframeResult( eventFromIframe ) {
 
 function activateSubscription( block, blogId, planId, lang ) {
 	block.addEventListener( 'click', () => {
-		window.scrollTop( 0, 0 );
+		window.scrollTo( 0, 0 );
 		tb_show(
 			null,
 			'https://subscribe.wordpress.com/memberships/?blog=' +
