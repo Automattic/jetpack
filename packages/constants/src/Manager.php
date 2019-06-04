e1b5b2d3a58131ffa487bf9478521ddcda96ee48
Marin Atanasov <tyxla@abv.bg>
2019-06-04 14:16:01 +0300

    Actually add namespacing to the constants class

--- packages/constants/src/Manager.php
+++ packages/constants/src/Manager.php
@@ -1,25 +1,27 @@
 <?php
 
+namespace Automattic\Jetpack\Constants;
+
 /**
- * Class Jetpack_Constants
+ * Class Automattic\Jetpack\Constants\Manager
  *
- * Testing constants is hard. Once you define a constant, it's defined. Jetpack_Constants is an
+ * Testing constants is hard. Once you define a constant, it's defined. Constants Manager is an
  * abstraction layer so that unit tests can set "constants" for tests.
  *
- * To test your code, you'll need to swap out `defined( 'CONSTANT' )` with `Jetpack_Constants::is_defined( 'CONSTANT' )`
- * and replace `CONSTANT` with `Jetpack_Constants::get_constant( 'CONSTANT' )`. Then in the unit test, you can set the
- * constant with `Jetpack::set_constant( 'CONSTANT', $value )` and then clean up after each test with something like
+ * To test your code, you'll need to swap out `defined( 'CONSTANT' )` with `Automattic\Jetpack\Constants\Manager::is_defined( 'CONSTANT' )`
+ * and replace `CONSTANT` with `Automattic\Jetpack\Constants\Manager::get_constant( 'CONSTANT' )`. Then in the unit test, you can set the
+ * constant with `Automattic\Jetpack\Constants\Manager::set_constant( 'CONSTANT', $value )` and then clean up after each test with something like
  * this:
  *
  * function tearDown() {
- *     Jetpack_Constants::clear_constants();
+ *     Automattic\Jetpack\Constants\Manager::clear_constants();
  * }
  */
-class Jetpack_Constants {
+class Manager {
 	static $set_constants = array();
 
 	/**
-	 * Checks if a "constant" has been set in Jetpack_Constants, and if not,
+	 * Checks if a "constant" has been set in constants Manager, and if not,
 	 * checks if the constant was defined with define( 'name', 'value ).
 	 *
 	 * @param $name string The name of the constant
@@ -32,7 +34,7 @@ class Jetpack_Constants {
 			: defined( $name );
 	}
 	/**
-	 * Checks if a "constant" has been set in Jetpack_Constants
+	 * Checks if a "constant" has been set in constants Manager
 	 * and has the value of true
 	 *
 	 * @param $name string The name of the constant
@@ -40,11 +42,11 @@ class Jetpack_Constants {
 	 * @return bool
 	 */
 	public static function is_true( $name ) {
-		return self::is_defined( $name) && self::get_constant( $name );
+		return self::is_defined( $name ) && self::get_constant( $name );
 	}
 
 	/**
-	 * Attempts to retrieve the "constant" from Jetpack_Constants, and if it hasn't been set,
+	 * Attempts to retrieve the "constant" from constants Manager, and if it hasn't been set,
 	 * then attempts to get the constant with the constant() function.
 	 *
 	 * @param $name
@@ -52,7 +54,7 @@ class Jetpack_Constants {
 	 * @return mixed null if the constant does not exist or the value of the constant.
 	 */
 	public static function get_constant( $name ) {
-		if ( array_key_exists( $name,  self::$set_constants ) ) {
+		if ( array_key_exists( $name, self::$set_constants ) ) {
 			return self::$set_constants[ $name ];
 		}
 
@@ -60,7 +62,7 @@ class Jetpack_Constants {
 	}
 
 	/**
-	 * Sets the value of the "constant" within Jetpack_Constants.
+	 * Sets the value of the "constant" within constants Manager.
 	 *
 	 * @param $name  string The name of the "constant"
 	 * @param $value string The value of the "constant"
@@ -70,7 +72,7 @@ class Jetpack_Constants {
 	}
 
 	/**
-	 * Will unset a "constant" from Jetpack_Constants if the constant exists.
+	 * Will unset a "constant" from constants Manager if the constant exists.
 	 *
 	 * @param $name string The name of the "constant"
 	 *
@@ -86,7 +88,7 @@ class Jetpack_Constants {
 	}
 
 	/**
-	 * Resets all of the constants within Jetpack_Constants.
+	 * Resets all of the constants within constants Manager.
 	 */
 	public static function clear_constants() {
 		self::$set_constants = array();
