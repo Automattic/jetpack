014609b69f3eb5428aeeea84820dfb3dbcb989da
Rocco Tripaldi <tripaldir@gmail.com>
2019-06-05 15:45:00 -0400

    removing a require

--- tests/php/test_class.jetpack-jitm.php
+++ tests/php/test_class.jetpack-jitm.php
@@ -2,8 +2,6 @@
 
 use Automattic\Jetpack\JITM\Manager as Jetpack_JITM;
 
-//require_once( JETPACK__PLUGIN_DIR . 'class.jetpack-jitm.php' );
-
 class WP_Test_Jetpack_JITM extends WP_UnitTestCase {
 	function test_jitm_disabled_by_filter() {
 		add_filter( 'jetpack_just_in_time_msgs', '__return_false', 50 );
