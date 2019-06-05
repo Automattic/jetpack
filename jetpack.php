0c0b65c314716ecbf7f8f2f0b6e1407507f57dc0
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2019-06-05 11:47:38 +0300

    Revert "Jetpack Options Package: just class map existing options class (#12562)"
    This reverts commit 0e3ac7ae22ba39c9696abdac4b2d6cd76c7cd193.

--- jetpack.php
+++ jetpack.php
@@ -210,6 +210,7 @@ require_once( JETPACK__PLUGIN_DIR . 'class.jetpack-client.php'        );
 require_once( JETPACK__PLUGIN_DIR . 'class.jetpack-data.php'          );
 require_once( JETPACK__PLUGIN_DIR . 'class.jetpack-client-server.php' );
 require_once( JETPACK__PLUGIN_DIR . 'sync/class.jetpack-sync-actions.php' );
+require_once( JETPACK__PLUGIN_DIR . 'class.jetpack-options.php'       );
 require_once( JETPACK__PLUGIN_DIR . 'class.jetpack-user-agent.php'    );
 require_once( JETPACK__PLUGIN_DIR . 'class.jetpack-post-images.php'   );
 require_once( JETPACK__PLUGIN_DIR . 'class.jetpack-error.php'         );
