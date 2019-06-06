2c93cb12f77807782b9000642b2d02bbec75bb96
Rocco Tripaldi <tripaldir@gmail.com>
2019-06-05 20:19:34 -0400

    Moves `get_access_token` to Connection package.

--- packages/connection/src/Manager.php
+++ packages/connection/src/Manager.php
@@ -8,6 +8,7 @@
 namespace Automattic\Jetpack\Connection;
 
 use Automattic\Jetpack\Connection\Manager_Interface;
+use Automattic\Jetpack\Constants\Manager as Constants_Manager;
 
 /**
  * The Jetpack Connection Manager class that is used as a single gateway between WordPress.com
@@ -15,9 +16,11 @@ use Automattic\Jetpack\Connection\Manager_Interface;
  */
 class Manager implements Manager_Interface {
 
-	const SECRETS_MISSING     = 'secrets_missing';
-	const SECRETS_EXPIRED     = 'secrets_expired';
-	const SECRETS_OPTION_NAME = 'jetpack_secrets';
+	const SECRETS_MISSING        = 'secrets_missing';
+	const SECRETS_EXPIRED        = 'secrets_expired';
+	const SECRETS_OPTION_NAME    = 'jetpack_secrets';
+	const MAGIC_NORMAL_TOKEN_KEY = ';normal;';
+	const JETPACK_MASTER_USER    = true;
 
 	/**
 	 * The procedure that should be run to generate secrets.
@@ -356,5 +359,134 @@ class Manager implements Manager_Interface {
 		}
 
 		return true;
+	}
+
+	/**
+	 * Gets the requested token.
+	 *
+	 * Tokens are one of two types:
+	 * 1. Blog Tokens: These are the "main" tokens. Each site typically has one Blog Token,
+	 *    though some sites can have multiple "Special" Blog Tokens (see below). These tokens
+	 *    are not associated with a user account. They represent the site's connection with
+	 *    the Jetpack servers.
+	 * 2. User Tokens: These are "sub-"tokens. Each connected user account has one User Token.
+	 *
+	 * All tokens look like "{$token_key}.{$private}". $token_key is a public ID for the
+	 * token, and $private is a secret that should never be displayed anywhere or sent
+	 * over the network; it's used only for signing things.
+	 *
+	 * Blog Tokens can be "Normal" or "Special".
+	 * * Normal: The result of a normal connection flow. They look like
+	 *   "{$random_string_1}.{$random_string_2}"
+	 *   That is, $token_key and $private are both random strings.
+	 *   Sites only have one Normal Blog Token. Normal Tokens are found in either
+	 *   Jetpack_Options::get_option( 'blog_token' ) (usual) or the JETPACK_BLOG_TOKEN
+	 *   constant (rare).
+	 * * Special: A connection token for sites that have gone through an alternative
+	 *   connection flow. They look like:
+	 *   ";{$special_id}{$special_version};{$wpcom_blog_id};.{$random_string}"
+	 *   That is, $private is a random string and $token_key has a special structure with
+	 *   lots of semicolons.
+	 *   Most sites have zero Special Blog Tokens. Special tokens are only found in the
+	 *   JETPACK_BLOG_TOKEN constant.
+	 *
+	 * In particular, note that Normal Blog Tokens never start with ";" and that
+	 * Special Blog Tokens always do.
+	 *
+	 * When searching for a matching Blog Tokens, Blog Tokens are examined in the following
+	 * order:
+	 * 1. Defined Special Blog Tokens (via the JETPACK_BLOG_TOKEN constant)
+	 * 2. Stored Normal Tokens (via Jetpack_Options::get_option( 'blog_token' ))
+	 * 3. Defined Normal Tokens (via the JETPACK_BLOG_TOKEN constant)
+	 *
+	 * @param int|false    $user_id   false: Return the Blog Token. int: Return that user's User Token.
+	 * @param string|false $token_key If provided, check that the token matches the provided input.
+	 *
+	 * @return object|false
+	 */
+	public function get_access_token( $user_id = false, $token_key = false ) {
+		$possible_special_tokens = array();
+		$possible_normal_tokens  = array();
+		$user_tokens             = \Jetpack_Options::get_option( 'user_tokens' );
+
+		if ( $user_id ) {
+			if ( ! $user_tokens ) {
+				return false;
+			}
+			if ( self::JETPACK_MASTER_USER === $user_id ) {
+				$user_id = \Jetpack_Options::get_option( 'master_user' );
+				if ( ! $user_id ) {
+					return false;
+				}
+			}
+			if ( ! isset( $user_tokens[ $user_id ] ) || ! $user_tokens[ $user_id ] ) {
+				return false;
+			}
+			$user_token_chunks = explode( '.', $user_tokens[ $user_id ] );
+			if ( empty( $user_token_chunks[1] ) || empty( $user_token_chunks[2] ) ) {
+				return false;
+			}
+			if ( $user_id != $user_token_chunks[2] ) {
+				return false;
+			}
+			$possible_normal_tokens[] = "{$user_token_chunks[0]}.{$user_token_chunks[1]}";
+		} else {
+			$stored_blog_token = \Jetpack_Options::get_option( 'blog_token' );
+			if ( $stored_blog_token ) {
+				$possible_normal_tokens[] = $stored_blog_token;
+			}
+
+			$defined_tokens = Constants_Manager::is_defined( 'JETPACK_BLOG_TOKEN' )
+				? explode( ',', Constants_Manager::get_constant( 'JETPACK_BLOG_TOKEN' ) )
+				: array();
+
+			foreach ( $defined_tokens as $defined_token ) {
+				if ( ';' === $defined_token[0] ) {
+					$possible_special_tokens[] = $defined_token;
+				} else {
+					$possible_normal_tokens[] = $defined_token;
+				}
+			}
+		}
+
+		if ( self::MAGIC_NORMAL_TOKEN_KEY === $token_key ) {
+			$possible_tokens = $possible_normal_tokens;
+		} else {
+			$possible_tokens = array_merge( $possible_special_tokens, $possible_normal_tokens );
+		}
+
+		if ( ! $possible_tokens ) {
+			return false;
+		}
+
+		$valid_token = false;
+
+		if ( false === $token_key ) {
+			// Use first token.
+			$valid_token = $possible_tokens[0];
+		} elseif ( self::MAGIC_NORMAL_TOKEN_KEY === $token_key ) {
+			// Use first normal token.
+			$valid_token = $possible_tokens[0]; // $possible_tokens only contains normal tokens because of earlier check.
+		} else {
+			// Use the token matching $token_key or false if none.
+			// Ensure we check the full key.
+			$token_check = rtrim( $token_key, '.' ) . '.';
+
+			foreach ( $possible_tokens as $possible_token ) {
+				if ( hash_equals( substr( $possible_token, 0, strlen( $token_check ) ), $token_check ) ) {
+					$valid_token = $possible_token;
+					break;
+				}
+			}
+		}
+
+		if ( ! $valid_token ) {
+			return false;
+		}
+
+		return (object) array(
+			'secret'           => $valid_token,
+			'external_user_id' => (int) $user_id,
+		);
 	}
 }
