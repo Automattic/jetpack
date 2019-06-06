bef31fe92a9f4387a4921822e05b134f3387ed2f
Marin Atanasov <tyxla@abv.bg>
2019-06-06 13:19:06 +0300

    Move tests and add bootstrap

--- packages/jitm/tests/php/test_Manager.php
+++ packages/jitm/tests/php/test_Manager.php
@@ -1,14 +1,15 @@
 <?php
 
 use Automattic\Jetpack\JITM\Manager as Jetpack_JITM;
+use PHPUnit\Framework\TestCase;
 
-class WP_Test_Jetpack_JITM extends WP_UnitTestCase {
-	function test_jitm_disabled_by_filter() {
+class Test_Jetpack_JITM extends TestCase {
+	public function test_jitm_disabled_by_filter() {
 		add_filter( 'jetpack_just_in_time_msgs', '__return_false', 50 );
 		$this->assertFalse( Jetpack_JITM::init() );
 	}
 
-	function test_jitm_enabled_by_default() {
+	public function test_jitm_enabled_by_default() {
 		$this->assertTrue( ! ! Jetpack_JITM::init() );
 	}
 }
