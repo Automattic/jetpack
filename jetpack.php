db1c23967b8dddb66a208dc118a023713e507840
Miguel Lezama <miguel@automattic.com>
2019-06-07 16:11:59 -0300

    dont require composer autoload.php

--- jetpack.php
+++ jetpack.php
@@ -14,7 +14,6 @@ use Automattic\Jetpack\JITM\Manager as JITM_Manager;
  * Domain Path: /languages/
  */
 
-require plugin_dir_path( __FILE__ ) . '/vendor/autoload.php';
 define( 'JETPACK__MINIMUM_WP_VERSION', '5.1' );
 define( 'JETPACK__MINIMUM_PHP_VERSION', '5.3.2' );
 
