ef8f994f441e1624438d02e2d566f9802b39bb41
Rocco Tripaldi <tripaldir@gmail.com>
2019-06-18 15:22:37 -0400

    Moves the ol' classmapped Options Sync Module to a psr-4 class.

--- packages/sync/legacy/class.jetpack-sync-modules.php
+++ packages/sync/legacy/class.jetpack-sync-modules.php
@@ -10,8 +10,8 @@ class Jetpack_Sync_Modules {
 	private static $default_sync_modules = array(
 		'Jetpack_Sync_Module_Constants',
 		'Automattic\\Jetpack\\Sync\\Modules\\Callables',
-		'Jetpack_Sync_Module_Options',
 		'Jetpack_Sync_Module_Network_Options',
+		'Automattic\\Jetpack\\Sync\\Modules\\Options',
 		'Automattic\\Jetpack\\Sync\\Modules\\Terms',
 		'Jetpack_Sync_Module_Themes',
 		'Jetpack_Sync_Module_Menus',
