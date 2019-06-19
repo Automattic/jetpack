2f937f3dc7bdfe44a1d45ed1d2bb0af680e17b7d
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2019-06-19 13:17:47 +0300

    Try to set cookie after connection process

--- tests/e2e/lib/flows/jetpack-connect.js
+++ tests/e2e/lib/flows/jetpack-connect.js
@@ -67,6 +67,7 @@ export async function connectThroughWPAdminIfNeeded( {
 	await ( await MyPlanPage.init( page ) ).returnToWPAdmin();
 
 	await ( await JetpackPage.init( page ) ).waitForPage();
+	await ( await JetpackPage.init( page ) ).setSandboxModeForPayments( cookie, '.eu.ngrok.io' );
 
 	if ( await jetpackPage.isPlan( plan ) ) {
 		console.log( 'Can see activated plan' );
