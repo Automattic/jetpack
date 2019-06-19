0b1fa54c2e3b90f0bc46495f1293a71bd3df7804
Michael Turk <turk.michael@gmail.com>
2019-06-19 16:01:09 -0400

    try to fix failing test

--- packages/sync/src/modules/Import.php
+++ packages/sync/src/modules/Import.php
@@ -124,7 +124,7 @@ class Import extends \Jetpack_Sync_Module {
 	 */
 	private static function get_calling_importer_class() {
 		// If WP_Importer doesn't exist, neither will any importer that extends it.
-		if ( ! class_exists( '\WP_Importer', false ) ) {
+		if ( ! class_exists( 'WP_Importer', false ) ) {
 			return 'unknown';
 		}
 
