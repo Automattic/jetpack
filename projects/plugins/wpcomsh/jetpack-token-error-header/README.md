3a46a67e49c5d8adb22409a370a1371898508784
oskosk <oskosk@gmail.com>
2019-06-21 11:31:00 -0300

    Remove some security sensitive comments and descriptions

--- projects/plugins/wpcomsh/jetpack-token-error-header/README.md
+++ projects/plugins/wpcomsh/jetpack-token-error-header/README.md
@@ -8,10 +8,3 @@ Logs Jetpack token errors as response headers like "X-Jetpack-Signature-Error" b
 
 See https://github.com/Automattic/jetpack/pull/12657 for more details on this hook.
 
-# Securing the headers
-
-**This plugin requires that $_SERVER['UNSAFELY_REPORT_JETPACK_TOKEN_STATUS'] is set to a truthy value before it does anything**
-
-It is recommended to only set this value for inbound requests coming from a WordPress.com IP range.
-
-This will ensure that the X-Forwarded-For header is respected for trusted local proxies. Therefore, the plugin will only set the header if the request is truly coming from WordPress.com.
--- projects/plugins/wpcomsh/jetpack-token-error-header/jetpack-token-header-error.php
+++ projects/plugins/wpcomsh/jetpack-token-error-header/jetpack-token-header-error.php
@@ -2,15 +2,6 @@
 
 /**
  * $error is a WP_Error (always) and contains a "signature_details" data property with this structure:
- *
- * 'token'     => isset( $_GET['token'] )     ? wp_unslash( $_GET['token'] )     : '',
- * 'timestamp' => isset( $_GET['timestamp'] ) ? wp_unslash( $_GET['timestamp'] ) : '',
- * 'nonce'     => isset( $_GET['nonce'] )     ? wp_unslash( $_GET['nonce'] )     : '',
- * 'body_hash' => isset( $_GET['body-hash'] ) ? wp_unslash( $_GET['body-hash'] ) : '',
- * 'method'    => wp_unslash( $_SERVER['REQUEST_METHOD'] ),
- * 'url'       => wp_unslash( $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ), // Temp - will get real signature URL later.
- * 'signature' => isset( $_GET['signature'] ) ? wp_unslash( $_GET['signature'] ) : '',
- *
  * The error_code has one of the following values:
  * - malformed_token
  * - malformed_user_id
