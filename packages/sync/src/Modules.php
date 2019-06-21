fbe6d90952b96df2ccd8677ed8184b2870ba6d58
Rocco Tripaldi <tripaldir@gmail.com>
2019-06-21 17:15:38 -0400

    Maps legacy sync modules. (#12819)
    * Maps legacy sync modules.
    
    * use CONST instead of private static var
    
    * PHP 5.6

--- packages/sync/src/Modules.php
+++ packages/sync/src/Modules.php
@@ -9,25 +9,46 @@ namespace Automattic\Jetpack\Sync;
 
 class Modules {
 
-	private static $default_sync_modules = array(
-		'Automattic\\Jetpack\\Sync\\Modules\\Constants',
-		'Automattic\\Jetpack\\Sync\\Modules\\Callables',
-		'Automattic\\Jetpack\\Sync\\Modules\\Network_Options',
-		'Automattic\\Jetpack\\Sync\\Modules\\Options',
-		'Automattic\\Jetpack\\Sync\\Modules\\Terms',
-		'Automattic\\Jetpack\\Sync\\Modules\\Menus',
-		'Automattic\\Jetpack\\Sync\\Modules\\Themes',
-		'Automattic\\Jetpack\\Sync\\Modules\\Users',
-		'Automattic\\Jetpack\\Sync\\Modules\\Import',
-		'Automattic\\Jetpack\\Sync\\Modules\\Posts',
-		'Automattic\\Jetpack\\Sync\\Modules\\Protect',
-		'Automattic\\Jetpack\\Sync\\Modules\\Comments',
-		'Automattic\\Jetpack\\Sync\\Modules\\Updates',
-		'Automattic\\Jetpack\\Sync\\Modules\\Attachments',
-		'Automattic\\Jetpack\\Sync\\Modules\\Meta',
-		'Automattic\\Jetpack\\Sync\\Modules\\Plugins',
-		'Automattic\\Jetpack\\Sync\\Modules\\Stats',
-		'Automattic\\Jetpack\\Sync\\Modules\\Full_Sync',
+	const DEFAULT_SYNC_MODULES = array(
+		'Jetpack_Sync_Modules_Constants',
+		'Jetpack_Sync_Modules_Callables',
+		'Jetpack_Sync_Modules_Network_Options',
+		'Jetpack_Sync_Modules_Options',
+		'Jetpack_Sync_Modules_Terms',
+		'Jetpack_Sync_Modules_Menus',
+		'Jetpack_Sync_Modules_Themes',
+		'Jetpack_Sync_Modules_Users',
+		'Jetpack_Sync_Modules_Import',
+		'Jetpack_Sync_Modules_Posts',
+		'Jetpack_Sync_Modules_Protect',
+		'Jetpack_Sync_Modules_Comments',
+		'Jetpack_Sync_Modules_Updates',
+		'Jetpack_Sync_Modules_Attachments',
+		'Jetpack_Sync_Modules_Meta',
+		'Jetpack_Sync_Modules_Plugins',
+		'Jetpack_Sync_Modules_Stats',
+		'Jetpack_Sync_Modules_Full_Sync',
+	);
+
+	const LEGACY_SYNC_MODULES_MAP = array(
+		'Jetpack_Sync_Modules_Constants'       => 'Automattic\\Jetpack\\Sync\\Modules\\Constants',
+		'Jetpack_Sync_Modules_Callables'       => 'Automattic\\Jetpack\\Sync\\Modules\\Callables',
+		'Jetpack_Sync_Modules_Network_Options' => 'Automattic\\Jetpack\\Sync\\Modules\\Network_Options',
+		'Jetpack_Sync_Modules_Options'         => 'Automattic\\Jetpack\\Sync\\Modules\\Options',
+		'Jetpack_Sync_Modules_Terms'           => 'Automattic\\Jetpack\\Sync\\Modules\\Terms',
+		'Jetpack_Sync_Modules_Menus'           => 'Automattic\\Jetpack\\Sync\\Modules\\Menus',
+		'Jetpack_Sync_Modules_Themes'          => 'Automattic\\Jetpack\\Sync\\Modules\\Themes',
+		'Jetpack_Sync_Modules_Users'           => 'Automattic\\Jetpack\\Sync\\Modules\\Users',
+		'Jetpack_Sync_Modules_Import'          => 'Automattic\\Jetpack\\Sync\\Modules\\Import',
+		'Jetpack_Sync_Modules_Posts'           => 'Automattic\\Jetpack\\Sync\\Modules\\Posts',
+		'Jetpack_Sync_Modules_Protect'         => 'Automattic\\Jetpack\\Sync\\Modules\\Protect',
+		'Jetpack_Sync_Modules_Comments'        => 'Automattic\\Jetpack\\Sync\\Modules\\Comments',
+		'Jetpack_Sync_Modules_Updates'         => 'Automattic\\Jetpack\\Sync\\Modules\\Updates',
+		'Jetpack_Sync_Modules_Attachments'     => 'Automattic\\Jetpack\\Sync\\Modules\\Attachments',
+		'Jetpack_Sync_Modules_Meta'            => 'Automattic\\Jetpack\\Sync\\Modules\\Meta',
+		'Jetpack_Sync_Modules_Plugins'         => 'Automattic\\Jetpack\\Sync\\Modules\\Plugins',
+		'Jetpack_Sync_Modules_Stats'           => 'Automattic\\Jetpack\\Sync\\Modules\\Stats',
+		'Jetpack_Sync_Modules_Full_Sync'       => 'Automattic\\Jetpack\\Sync\\Modules\\Full_Sync',
 	);
 
 	private static $initialized_modules = null;
@@ -64,15 +85,25 @@ class Modules {
 		 *
 		 * @since 4.2.0
 		 */
-		$modules = apply_filters( 'jetpack_sync_modules', self::$default_sync_modules );
+		$modules = apply_filters( 'jetpack_sync_modules', self::DEFAULT_SYNC_MODULES );
+
+		$modules = array_map( array( 'Automattic\\Jetpack\\Sync\\Modules', 'map_legacy_modules' ), $modules );
 
 		$modules = array_map( array( 'Automattic\\Jetpack\\Sync\\Modules', 'load_module' ), $modules );
 
 		return array_map( array( 'Automattic\\Jetpack\\Sync\\Modules', 'set_module_defaults' ), $modules );
 	}
 
-	static function load_module( $module_name ) {
-		return new $module_name();
+	static function load_module( $module_class ) {
+		return new $module_class();
+	}
+
+	static function map_legacy_modules( $module_class ) {
+		$legacy_map = self::LEGACY_SYNC_MODULES_MAP;
+		if ( isset( $legacy_map[ $module_class ] ) ) {
+			return $legacy_map[ $module_class ];
+		}
+		return $module_class;
 	}
 
 	static function set_module_defaults( $module ) {
