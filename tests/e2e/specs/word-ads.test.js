a09f8b43030cbb19e5ae5002b56bad910bb7674b
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2019-06-22 15:48:34 +0300

    Add WordAds block test - add a WordAds block - run tests inBand in CI

---
+++ tests/e2e/specs/word-ads.test.js
@@ -0,0 +1,30 @@
+/**
+ * WordPress dependencies
+ */
+import { createNewPost } from '@wordpress/e2e-test-utils/build/create-new-post';
+/**
+ * Internal dependencies
+ */
+import BlockEditorPage from '../lib/pages/wp-admin/block-editor';
+import PostFrontendPage from '../lib/pages/postFrontend';
+import WordAdsBlock from '../lib/blocks/word-ads';
+
+describe( 'WordAds block', () => {
+	it( 'Can publish a post with a WordAds block', async () => {
+		await createNewPost();
+
+		const blockEditor = await BlockEditorPage.init( page );
+		const blockInfo = await blockEditor.insertBlock( WordAdsBlock.name() );
+
+		const adBlock = new WordAdsBlock( blockInfo, page );
+		await adBlock.switchFormat( 3 ); // switch to Wide Skyscraper ad format
+
+		await blockEditor.focus();
+
+		await blockEditor.publishPost();
+		await blockEditor.viewPost();
+
+		const frontend = await PostFrontendPage.init( page );
+		await frontend.isRenderedBlockPresent( WordAdsBlock );
+	} );
+} );
