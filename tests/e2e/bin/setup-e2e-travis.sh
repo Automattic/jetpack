e053d22aa86ef51fae3c9867bd591ca50948b441
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2019-06-22 18:59:58 +0300

    Use ngrok auth key

--- tests/e2e/bin/setup-e2e-travis.sh
+++ tests/e2e/bin/setup-e2e-travis.sh
@@ -29,6 +29,7 @@ install_ngrok() {
 	# download and install ngrok
 	curl -s https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > ngrok.zip
 	unzip ngrok.zip
+	./ngrok authtoken $NGROK_KEY
 	./ngrok http -log=stdout 80 > /dev/null &
 	sleep 3
 	WP_SITE_URL=$(curl -s localhost:4040/api/tunnels/command_line | jq --raw-output .public_url)
