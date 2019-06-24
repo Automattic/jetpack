69eb9804abfc55e6b24e96d3abda50d8cb294166
Michael Arestad <marestad@gmail.com>
2019-06-24 10:46:30 -0700

    At a Glance - update DashItem card titles (#12834)
    * At a Glance - update DashItem card titles
    
    * Jetpack Protect now is labeled as Jetpack Protect
    
    * Remove Jetpack from titles of DashItems
    
    * Removed "Jetpack" from Anti-Spam title
    
    * Refactored protect.jsx a tiny bit to make title changes simpler
    
    * Changed from Backups to Backup
    
    * Changed Downtime monitoring to Downtime Monitor

--- _inc/client/at-a-glance/scan.jsx
+++ _inc/client/at-a-glance/scan.jsx
@@ -29,7 +29,7 @@ import JetpackBanner from 'components/jetpack-banner';
  */
 const renderCard = props => (
 	<DashItem
-		label={ __( 'Security Scanning' ) }
+		label={ __( 'Scan' ) }
 		module={ props.feature || 'scan' }
 		support={ {
 			text: __(
