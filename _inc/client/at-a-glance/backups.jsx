356eb9b73f74db671903a493eb5b8cf2c7f57b37
Michael Arestad <marestad@gmail.com>
2019-06-24 10:20:02 -0600

    Changed from Backups to Backup

--- _inc/client/at-a-glance/backups.jsx
+++ _inc/client/at-a-glance/backups.jsx
@@ -28,7 +28,7 @@ import { showBackups } from 'state/initial-state';
  */
 const renderCard = props => (
 	<DashItem
-		label={ __( 'Backups' ) }
+		label={ __( 'Backup' ) }
 		module={ props.feature || 'backups' }
 		support={ {
 			text: __(
