3ab69018ca9a9735f2498b3945581f3cc5b99672
Bernie Reiter <ockham@raz.or.at>
2019-06-24 13:37:47 +0200

    Use @wp/i18n

--- extensions/shared/upgrade-nudge/index.jsx
+++ extensions/shared/upgrade-nudge/index.jsx
@@ -1,22 +1,18 @@
 /**
  * External dependencies
  */
-import { localize } from 'i18n-calypso';
+import { __ } from '@wordpress/i18n';
 import Gridicon from 'gridicons';
 
 import './style.scss';
 
-const UpgradeNudge = ( { translate } ) => (
+export default () => (
 	<div className="upgrade-nudge" href="/plans?customerType=business">
 		<Gridicon className="upgrade-nudge__icon" icon="star" size={ 18 } />
 		<div className="upgrade-nudge__info">
-			<span className="upgrade-nudge__title">{ translate( 'Upgrade to Premium' ) }</span>
-			<span className="upgrade-nudge__message">
-				{ translate( 'To gain access to this block.' ) }
-			</span>
+			<span className="upgrade-nudge__title">{ __( 'Upgrade to Premium' ) }</span>
+			<span className="upgrade-nudge__message">{ __( 'To gain access to this block.' ) }</span>
 		</div>
-		<button className="upgrade-nudge__button">{ translate( 'Upgrade' ) }</button>
+		<button className="upgrade-nudge__button">{ __( 'Upgrade' ) }</button>
 	</div>
 );
-
-export default localize( UpgradeNudge );
