73f99f8575303294c5cb6103cf604276e4d39ddb
Elio Rivero <eliorivero@gmail.com>
2019-06-26 12:41:22 -0300

    Add deprecated method. Fix all phpcs warnings.

--- packages/compat/legacy/class.jetpack-client.php
+++ packages/compat/legacy/class.jetpack-client.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Jetpack Client
+ *
+ * Methods for Jetpack to act as client with wpcom.
+ *
+ * @category   Connection
+ * @package    Client
+ */
 
 use Automattic\Jetpack\Connection\Client;
 
@@ -10,10 +18,35 @@ use Automattic\Jetpack\Connection\Client;
 class Jetpack_Client {
 
 	/**
+	 * Perform remote request.
+	 *
 	 * @deprecated use Automattic\Jetpack\Connection\Client::remote_request
+	 *
+	 * @param array $args Arguments.
+	 * @param null  $body Request body.
+	 *
+	 * @return array|\Automattic\Jetpack\Connection\WP_Error
 	 */
-	static function remote_request( $args, $body = null ) {
+	public static function remote_request( $args, $body = null ) {
 		_deprecated_function( __METHOD__, 'jetpack-7.5', 'Automattic\Jetpack\Connection\Client' );
 		return Client::remote_request( $args, $body );
+	}
+
+	/**
+	 * Request to wpcom using the blog id.
+	 *
+	 * @deprecated use Automattic\Jetpack\Connection\Client::wpcom_json_api_request_as_blog
+	 *
+	 * @param string $path Endpoint path.
+	 * @param string $version Endpoint version.
+	 * @param array  $args Arguments.
+	 * @param null   $body Request body.
+	 * @param string $base_api_path Endpoint base prefix.
+	 *
+	 * @return \Automattic\Jetpack\Connection\Array|\Automattic\Jetpack\Connection\WP_Error
+	 */
+	public static function wpcom_json_api_request_as_blog( $path, $version, $args, $body, $base_api_path ) {
+		_deprecated_function( __METHOD__, 'jetpack-7.5', 'Automattic\Jetpack\Connection\Client' );
+		return Client::wpcom_json_api_request_as_blog( $path, $version, $args, $body, $base_api_path );
 	}
 }
