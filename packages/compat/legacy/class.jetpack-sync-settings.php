b6e04e3892458751063cadf058ec7ad6df04d594
Michael Turk <turk.michael@gmail.com>
2019-06-26 14:06:12 -0400

    Delegating to new class

--- packages/compat/legacy/class.jetpack-sync-settings.php
+++ packages/compat/legacy/class.jetpack-sync-settings.php
@@ -1,18 +1,17 @@
 <?php
 
+use Automattic\Jetpack\Sync\Settings;
+
 /**
  * Class Jetpack_Sync_Settings
  *
  * @deprecated Use Automattic\Jetpack\Sync\Settings
  */
-class Jetpack_Sync_Settings extends Automattic\Jetpack\Sync\Settings {
+class Jetpack_Sync_Settings {
 
-	public function __callStatic( $method, $args ) {
-		if ( method_exists( parent, $method ) ) {
-			_deprecated_function( __METHOD__, 'jetpack-7.5', 'Automattic\Jetpack\Sync\Settings' );
-			return call_user_func_array( [ parent, $method ], $args );
-		}
-		throw new Exception( "Method doesn't exist" );
+	static function is_syncing() {
+		_deprecated_function( __METHOD__, 'jetpack-7.5', 'Automattic\Jetpack\Sync\Settings' );
+		return Settings::is_syncing();
 	}
 
 }
