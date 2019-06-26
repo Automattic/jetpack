bcb9b786d7ae7d7aef79cd604a111a2949ac7a84
Igor Zinovyev <zinigor@gmail.com>
2019-06-26 17:02:06 +0200

    Removed a redundant method declaration.

--- class.jetpack-client.php
+++ class.jetpack-client.php
@@ -1,18 +1,6 @@
 <?php
 
-use Automattic\Jetpack\Connection\Client;
-
 /**
  * Deprecated since 7.5 in favor of packages/connection/src/Client.php
  */
 _deprecated_file( basename( __FILE__ ), 'jetpack-7.5', 'packages/connection/src/Client.php' );
-
-class Jetpack_Client {
-	/**
-	 * @deprecated use Automattic\Jetpack\Connection\Client::remote_request
-	 */
-	static function remote_request( $args, $body = null ) {
-		_deprecated_function( __METHOD__, 'jetpack-7.5', 'Automattic\Jetpack\Connection\Client' );
-		return Client::remote_request( $args, $body );
-	}
-}
