5a44ab9d02d43cac005045d3bd8f39bc7545d233
Derek Smart <smart@automattic.com>
2019-06-25 21:18:10 -0400

    Add deprecated notices to file and method

--- class.jetpack-client.php
+++ class.jetpack-client.php
@@ -2,3 +2,4 @@
 /**
  * Deprecated since 7.5 in favor of packages/connection/src/Client.php
  */
+_deprecated_file( basename( __FILE__ ), 'jetpack-7.5', 'packages/connection/src/Client.php' );
--- packages/compat/legacy/class.jetpack-client.php
+++ packages/compat/legacy/class.jetpack-client.php
@@ -13,6 +13,7 @@ class Jetpack_Client {
 	 * @deprecated use Automattic\Jetpack\Connection\Client::remote_request
 	 */
 	static function remote_request( $args, $body = null ) {
+		_deprecated_function( __METHOD__, 'jetpack-7.5', 'Automattic\Jetpack\Connection\Client' );
 		return Client::remote_request( $args, $body );
 	}
 }
