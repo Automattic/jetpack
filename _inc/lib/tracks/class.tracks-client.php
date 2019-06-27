26ad7efbd8b0c7b81f2a6d56a33b4d7810202890
Derek Smart <smart@automattic.com>
2019-06-27 10:15:47 -0400

    Jetpack 7.5: Back compatibility package (#12857)
    * DNA: Compat Package (#12856)
    
    * First pass
    
    * load autolod_files.php
    
    * remove echo, add end of line
    
    * Add docblock annotation and empty file
    
    * Add deprecated notices to file and method
    
    * dummy class.jetpack-options.php + tests (#12859)
    
    * Adds a compatibility stub for the remote_request method. (#12865)
    
    * Add deprecation notice to the remote_request method.
    
    * Removed a redundant method declaration.
    
    * Whitespace cleanup.
    
    * Removed a redundant test.
    
    * Add deprecation to old file (#12869)
    
    * DNA: Add legacy class.jetpack-sync-settings.php (#12870)
    
    * add legacy class.jetpack-sync-settings.php
    
    * add test
    
    * Update packages/compat/legacy/class.jetpack-sync-settings.php
    
    Co-Authored-By: Miguel Lezama <lezama@gmail.com>
    
    * Delegating to new class
    
    * Add a method which summarises code compatibility issues with counts (#12872)
    
    * Add a method which summarises issues with counts
    
    * Remove unused class and return empty string for no issues
    
    * Remove unnecessary check
    
    * Added a stringify function to avoid unneeded recursion when scanning code. (#12858)
    
    When an invocation is complex enough, it will go further than our logic in the analyzer, but we don't need to follow the properties or class names based on other variables - we still won't find out whether the class or property was moved in Jetpack without runtime analysis. Therefore we replace these instances with just a node's class name for want of a better string.
    
    * add all jetpack sync settings methods (#12876)
    
    * Update/7.5 compat request as blog (#12868)
    
    * Add deprecated method. Fix all phpcs warnings.
    
    * Update packages/compat/legacy/class.jetpack-client.php
    
    Co-Authored-By: Derek Smart <smart@automattic.com>
    
    * Merge namespace fix
    
    Co-Authored-By: Miguel Lezama <lezama@gmail.com>
    
    * Merge namespace fix
    
    Co-Authored-By: Miguel Lezama <lezama@gmail.com>
    
    * Do not warn for things that have moved to a package and been replaced with an empty file (#12875)
    
    * Jetpack_Client::_wp_remote_request
    
    * Update example script to use path param with ->find()
    
    * DNA: Compat: class.jetpack-sync-actions.php (#12879)
    
    * add tests
    
    * add compat legacy class.jetpack-sync-actions.php
    
    * add test for initialize_sender
    
    * Add deprecated file stub
    
    * Fix typo in jetpack-sync-actions file stub
    
    * Add file stub for sync/class.jetpack-sync-settings.php
    
    * DNA: Jetpack_Sync_Modules stub methods (#12880)
    
    * stab methods for class.jetpack-tracks.php
    
    * add tests for all vipgo issues (#12874)
    
    * DNA: Compat: Add files back (#12871)
    
    Add back deprecated files

---
+++ _inc/lib/tracks/class.tracks-client.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * Deprecated file since 7.5 - Jetpack_Tracks_Client is now autoloaded from 'vendor/automattic/jetpack-tracking/legacy/class.tracks-client.php'
+ */
+_deprecated_file( basename( __FILE__ ), 'jetpack-7.5' );
