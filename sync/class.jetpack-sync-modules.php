341aa252f2baa2b8d1e930b26c9a5cbf82f198c0
Daniel Walmsley <goldsounds@gmail.com>
2019-06-26 22:09:58 -0700

    Fix small typo in comment

--- sync/class.jetpack-sync-modules.php
+++ sync/class.jetpack-sync-modules.php
@@ -1,5 +1,5 @@
 <?php
 /**
- * Deprecated since 7.5 – Jetpack_Options are autoloaded from packages/compat/legacy/class.jetpack-sync-modules.php
+ * Deprecated since 7.5 – Jetpack_Sync_Modules are autoloaded from packages/compat/legacy/class.jetpack-sync-modules.php
  */
 _deprecated_file( basename( __FILE__ ), 'jetpack-7.5', 'packages/compat/legacy/class.jetpack-sync-modules.php' );
