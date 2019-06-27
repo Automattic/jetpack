bef25d82ec60761ef6cedf6424eea275aebd2f57
Jeremy Herve <jeremy@jeremy.hu>
2019-06-27 14:46:59 +0200

    Update replacement path to a file that ships with the release

--- class.jetpack-client.php
+++ class.jetpack-client.php
@@ -2,4 +2,4 @@
 /**
  * Deprecated since 7.5
  */
-_deprecated_file( basename( __FILE__ ), 'jetpack-7.5', 'packages/connection/src/Client.php' );
+_deprecated_file( basename( __FILE__ ), 'jetpack-7.5', 'vendor/automattic/jetpack-connection/src/Client.php' );
