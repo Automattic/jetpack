b80557c2b3320b2f16a7bde060aa55bf09f854d6
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2019-06-27 14:36:47 +0300

    Add a mailchimp test

---
+++ tests/e2e/lib/blocks/mailchimp.js
@@ -0,0 +1,56 @@
+/**
+ * Internal dependencies
+ */
+import { waitAndClick, waitForSelector, clickAndWaitForNewPage } from '../../page-helper';
+import LoginPage from '../wpcom/login';
+import ConnectionsPage from '../wpcom/connections';
+
+export default class MailchimpBlock {
+	constructor( block, page ) {
+		this.blockName = MailchimpBlock.name();
+		this.block = block;
+		this.page = page;
+		this.blockSelector = '#block-' + block.clientId;
+	}
+
+	static name() {
+		return 'Mailchimp';
+	}
+
+	/**
+	 * Sets-up a mailchimp connection. Method expects to see a "Set up Mailchimp from" button in block editor.
+	 * - Starts by logging in to WPCOM account in page opened in new tab
+	 * - Connects to Mailchimp once Connection page is loaded
+	 * - Closes WPCOM tab
+	 */
+	async connect() {
+		const setupFormSelector = this.getSelector( "a[href*='marketing/connections']" );
+		const loginTab = await clickAndWaitForNewPage( this.page, setupFormSelector );
+
+		await ( await LoginPage.init( loginTab ) ).login( 'defaultUser' );
+		await ( await ConnectionsPage.init( loginTab ) ).connectMailchimp();
+
+		const reCheckSelector = this.getSelector( 'button.is-link' );
+		await waitAndClick( this.page, reCheckSelector );
+	}
+
+	getSelector( selector ) {
+		return `${ this.blockSelector } ${ selector }`;
+	}
+
+	/**
+	 * Checks whether block is rendered on frontend
+	 * @param {Page} page Puppeteer page instance
+	 */
+	static async isRendered( page ) {
+		const containerSelector = '.wp-block-jetpack-mailchimp';
+		const emailSelector = containerSelector + " input[type='email']";
+		const submitSelector = containerSelector + " button[type='submit']";
+		const consentSelector = containerSelector + ' #wp-block-jetpack-mailchimp_consent-text';
+
+		await waitForSelector( page, containerSelector );
+		await waitForSelector( page, emailSelector );
+		await waitForSelector( page, submitSelector );
+		await waitForSelector( page, consentSelector );
+	}
+}
---
+++ tests/e2e/lib/pages/wpcom/connections.js
@@ -0,0 +1,47 @@
+/**
+ * Internal dependencies
+ */
+import Page from '../page';
+import {
+	waitAndClick,
+	waitAndType,
+	clickAndWaitForNewPage,
+	getAccountCredentials,
+} from '../../page-helper';
+
+export default class ConnectionsPage extends Page {
+	constructor( page ) {
+		const expectedSelector = '.sharing-settings.sharing-connections';
+		super( page, { expectedSelector } );
+	}
+
+	async connectMailchimp( mailchimpList = 'e2etesting' ) {
+		const mailchimpConnectSelector = 'div.mailchimp .foldable-card__summary button:not([disabled])';
+		const marketingSelectSelector = '.mailchimp select';
+		const mcOptionXpathSelector = `//option[contains(text(), '${ mailchimpList }')]`;
+		const successNoticeSelector = `//span[contains(text(), '${ mailchimpList }')]`;
+
+		const mcPopupPage = await clickAndWaitForNewPage( this.page, mailchimpConnectSelector );
+
+		// MC Login pop-up page. TODO: maybe extract to a new page
+		const [ mcLogin, mcPassword ] = getAccountCredentials( 'mailchimpLogin' );
+		// Locators
+		const mcUsernameSelector = '#login #username';
+		const mcPasswordSelector = '#login #password';
+		const mcSubmitSelector = "#login input[type='submit']";
+
+		await waitAndType( mcPopupPage, mcUsernameSelector, mcLogin );
+		await waitAndType( mcPopupPage, mcPasswordSelector, mcPassword );
+		await waitAndClick( mcPopupPage, mcSubmitSelector );
+
+		// WPCOM Connections page
+		await this.page.waitForXPath( mcOptionXpathSelector );
+
+		const optionHandle = ( await this.page.$x( mcOptionXpathSelector ) )[ 0 ];
+		const optionValue = await ( await optionHandle.getProperty( 'value' ) ).jsonValue();
+		await this.page.select( marketingSelectSelector, optionValue );
+
+		await this.page.waitForXPath( successNoticeSelector );
+		await this.page.close();
+	}
+}
