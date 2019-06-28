ee87825bd5aa92e595a1707f51bfabe2fd365908
Bernie Reiter <ockham@raz.or.at>
2019-06-28 11:27:12 +0200

    Add plans store

--- extensions/shared/upgrade-nudge/index.jsx
+++ extensions/shared/upgrade-nudge/index.jsx
@@ -4,16 +4,18 @@
 import { __, sprintf } from '@wordpress/i18n';
 import { addQueryArgs } from '@wordpress/url';
 import { Button } from '@wordpress/components';
+import { withSelect } from '@wordpress/data';
 import Gridicon from 'gridicons';
 
 /**
  * Internal dependencies
  */
 import getSiteFragment from '../get-site-fragment';
+import './store';
 
 import './style.scss';
 
-export default ( { feature, plan } ) => (
+const UpgradeNudge = ( { feature, plan } ) => (
 	<div className="upgrade-nudge">
 		<Gridicon className="upgrade-nudge__icon" icon="star" size={ 18 } />
 		<div className="upgrade-nudge__info">
@@ -24,10 +26,18 @@ export default ( { feature, plan } ) => (
 		</div>
 		<Button
 			className="upgrade-nudge__button"
-			href={ addQueryArgs( `https://wordpress.com/plans/${ getSiteFragment() }`, { feature, plan } ) }
+			href={ addQueryArgs( `https://wordpress.com/plans/${ getSiteFragment() }`, {
+				feature,
+				plan,
+			} ) }
 			isDefault
 		>
 			{ __( 'Upgrade' ) }
 		</Button>
 	</div>
 );
+export default withSelect( ( select, { plan } ) => {
+	const plans = select( 'wordpress-com/plans' ).getPlan();
+	console.log( plan, plans );
+	return { plans };
+} )( UpgradeNudge );
---
+++ extensions/shared/upgrade-nudge/store.js
@@ -0,0 +1,54 @@
+/**
+ * External dependencies
+ */
+import apiFetch from '@wordpress/api-fetch';
+import { registerStore } from '@wordpress/data';
+
+const actions = {
+	setPlans( plans ) {
+		return {
+			type: 'SET_PLANS',
+			plans,
+		};
+	},
+
+	fetchFromAPI( url ) {
+		return {
+			type: 'FETCH_FROM_API',
+			url,
+		};
+	},
+};
+
+registerStore( 'wordpress-com/plans', {
+	reducer( state = {}, action ) {
+		switch ( action.type ) {
+			case 'SET_PLANS':
+				return action.plans;
+		}
+
+		return state;
+	},
+
+	actions,
+
+	selectors: {
+		getPlan( state ) {
+			return state; // TODO: Find and return plan by planSlug
+		},
+	},
+
+	controls: {
+		FETCH_FROM_API( { url } ) {
+			return apiFetch( { url } );
+		},
+	},
+
+	resolvers: {
+		*getPlan() {
+			const url = 'https://public-api.wordpress.com/rest/v1.5/plans';
+			const plans = yield actions.fetchFromAPI( url );
+			return actions.setPlans( plans );
+		},
+	},
+} );
