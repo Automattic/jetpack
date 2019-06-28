2907733029a3da15930bc5dab1ae9fa40a463624
Miguel Lezama <miguel@automattic.com>
2019-06-28 14:38:32 -0300

    add `_deprecated_function`

--- packages/compat/lib/tracks/client.php
+++ packages/compat/lib/tracks/client.php
@@ -4,6 +4,8 @@
  * @deprecated 7.5.0 use Automattic\Jetpack\Tracking->tracks_record_event_raw instead
  */
 function jetpack_tracks_record_event_raw( $event ) {
+	_deprecated_function( __METHOD__, 'jetpack-7.5', 'Automattic\Jetpack\Tracking->tracks_record_event_raw' );
+
 	$tracking = new Automattic\Jetpack\Tracking( 'jetpack', Jetpack::connection() );
 	return $tracking->jetpack_tracks_record_event_raw( $event );
 }
@@ -12,6 +14,8 @@ function jetpack_tracks_record_event_raw( $event ) {
  * @deprecated 7.5.0 use Automattic\Jetpack\Tracking->tracks_get_identity instead
  */
 function jetpack_tracks_get_identity( $user_id ) {
+	_deprecated_function( __METHOD__, 'jetpack-7.5', 'Automattic\Jetpack\Tracking->tracks_get_identity' );
+
 	$tracking = new Automattic\Jetpack\Tracking( 'jetpack', Jetpack::connection() );
 	return $tracking->jetpack_tracks_get_identity( $user_id );
 }
@@ -20,6 +24,8 @@ function jetpack_tracks_get_identity( $user_id ) {
  * @deprecated 7.5.0 use Automattic\Jetpack\Tracking->tracks_record_event instead
  */
 function jetpack_tracks_record_event( $user, $event_name, $properties = array(), $event_timestamp_millis = false ) {
+	_deprecated_function( __METHOD__, 'jetpack-7.5', 'Automattic\Jetpack\Tracking->tracks_record_event' );
+
 	$tracking = new Automattic\Jetpack\Tracking( 'jetpack', Jetpack::connection() );
 	return $tracking->jetpack_tracks_record_event( $user, $event_name, $properties, $event_timestamp_millis );
 }
