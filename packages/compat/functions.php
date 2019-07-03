2fe3afe9ebd794a5e6ae3a17af3f37ac5284e7d8
Elio Rivero <eliorivero@gmail.com>
2019-07-03 15:25:29 -0300

    Don't exit if ABSPATH is not defined. Work in the opposite way: if it's defined, run the code. This avoid exiting when PHPCS is running. (#12943)

--- packages/compat/functions.php
+++ packages/compat/functions.php
@@ -1,15 +1,25 @@
 <?php
-
 /**
- * TODO: Legacy global scope functions here
+ * Legacy global scope functions.
+ *
+ * @package global-functions
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+	return;
+}
+
+// Add here, after the condition above, any code that should only run when WordPress is running.
+// Autoload will load everything even when PHPCS is running and we don't want to run these
+// in such case because they will fatal, for example, due to 'add_action' being undefined.
+
+/**
+ * Load necessary functions.
+ */
+function jetpack_compat_require_defined_functions() {
+	jetpack_require_lib( 'tracks/client' );
 }
 
 add_action( 'plugins_loaded', 'jetpack_compat_require_defined_functions' );
 
-function jetpack_compat_require_defined_functions() {
-	jetpack_require_lib( 'tracks/client' );
-}
+
