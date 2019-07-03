a51126d83845e3e5cec05f43a418299d5e306d8a
Elio Rivero <eliorivero@gmail.com>
2019-07-03 05:43:48 -0300

    Avoid running these functions on commit. Only run them when it's a WordPress enviroment (#12923)

--- packages/compat/functions.php
+++ packages/compat/functions.php
@@ -4,6 +4,10 @@
  * TODO: Legacy global scope functions here
  */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
 add_action( 'plugins_loaded', 'jetpack_compat_require_defined_functions' );
 
 function jetpack_compat_require_defined_functions() {
