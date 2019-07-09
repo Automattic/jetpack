e9afb4394b1c3c6b3da454a185faf5cea42e7c8e
Jefferson Rabb <jeff@atavist.net>
2019-07-09 07:56:20 -0400

    Slideshow pagination control in AMP.

--- extensions/blocks/slideshow/slideshow.php
+++ extensions/blocks/slideshow/slideshow.php
@@ -26,17 +26,21 @@ function jetpack_slideshow_block_load_assets( $attr, $content ) {
 	$type = 'slideshow';
 	Jetpack_Gutenberg::load_assets_as_required( $type );
 	if ( class_exists( 'Jetpack_AMP_Support' ) && Jetpack_AMP_Support::is_amp_request() ) {
-		$ids      = empty( $attr['ids'] ) ? array() : $attr['ids'];
-		$autoplay = empty( $attr['autoplay'] ) ? false : $attr['autoplay'];
-		$autoplay = false;
-		$delay    = empty( $attr['delay'] ) ? 3 : intval( $attr['delay'] );
-		$align    = isset( $attr['align'] ) ? $attr['align'] : 'center';
-		$classes  = array(
+		global $slideshow_id;
+		$slideshow_id    = ( $slideshow_id || 0 ) + 1;
+		$amp_carousel_id = sprintf( 'wp-block-jetpack-slideshow__%s', $slideshow_id );
+
+		$ids        = empty( $attr['ids'] ) ? array() : $attr['ids'];
+		$autoplay   = empty( $attr['autoplay'] ) ? false : $attr['autoplay'];
+		$autoplay   = false;
+		$delay      = empty( $attr['delay'] ) ? 3 : intval( $attr['delay'] );
+		$align      = isset( $attr['align'] ) ? $attr['align'] : 'center';
+		$classes    = array(
 			'wp-block-jetpack-' . $type,
 			'wp-amp-block',
 			'align' . $align,
 		);
-		$slides   = array_map(
+		$slides     = array_map(
 			function( $id ) {
 				$caption    = wp_get_attachment_caption( $id );
 				$src        = wp_get_attachment_image_src( $id, 'full' );
@@ -59,18 +63,35 @@ function jetpack_slideshow_block_load_assets( $attr, $content ) {
 			},
 			$ids
 		);
-		$carousel = sprintf(
-			'<amp-carousel height="300" width="400" layout="responsive" type="slides" data-next-button-aria-label="%s" data-prev-button-aria-label="%s" controls loop %s>%s</amp-carousel>',
+		$bullets    = array_map(
+			function( $index ) use ( $amp_carousel_id ) {
+				return sprintf(
+					'<button class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide %s" on="tap:%s.goToSlide(index=%s)"></button>',
+					( $index + 1 ),
+					$amp_carousel_id,
+					$index
+				);
+			},
+			array_keys( $ids )
+		);
+		$pagination = sprintf(
+			'<div class="wp-block-jetpack-slideshow_pagination swiper-pagination swiper-pagination-bullets amp-pagination">%s</div>',
+			implode( '', $bullets )
+		);
+		$carousel   = sprintf(
+			'<amp-carousel height="300" width="400" layout="responsive" type="slides" data-next-button-aria-label="%s" data-prev-button-aria-label="%s" controls loop %s id="%s">%s</amp-carousel>',
 			__( 'Next Slide', 'jetpack' ),
 			__( 'Previous Slide', 'jetpack' ),
 			$autoplay ? 'autoplay delay=' . ( $delay * 1000 ) : '',
+			$amp_carousel_id,
 			implode( '', $slides )
 		);
 
 		return sprintf(
-			'<div class="%s"><div class="wp-block-jetpack-slideshow_container swiper-container">%s</div></div>',
+			'<div class="%s"><div class="wp-block-jetpack-slideshow_container swiper-container">%s%s</div></div>',
 			implode( $classes, ' ' ),
-			$carousel
+			$carousel,
+			$pagination
 		);
 	}
 	return $content;
