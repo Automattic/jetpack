49cb5bf21be68bebcc97e5bf0ebad4e99051c494
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2019-07-10 10:25:45 +0100

    remove the error log

--- bin/prepare-commit-msg.js
+++ bin/prepare-commit-msg.js
@@ -3,9 +3,7 @@ const fs = require( 'fs' );
 
 fs.readFile( '.git/last-commit-date', ( err, data ) => {
 	if ( err ) {
-		console.log( err );
 		console.log( 'skipping prepare-commit-msg hook' );
-
 		return;
 	}
 	const commitDate = data.toString();
