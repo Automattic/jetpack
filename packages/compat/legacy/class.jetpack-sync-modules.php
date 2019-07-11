93a108176335c11cadb0384cba5d9baca4205d60
Igor Zinovyev <zinigor@gmail.com>
2019-07-11 20:53:24 +0300

    Add the missing use declaration to the compat layer file. (#13034)
    * Add the missing use declaration to the compat layer file.
    
    Without this declaration any plugin that would attempt to use Jetpack_Sync_Actions static methods will result
    in a fatal error because of the missing Actions class.
    
    * Adds the use statement to Jetpack_Sync_Modules shim.

--- packages/compat/legacy/class.jetpack-sync-modules.php
+++ packages/compat/legacy/class.jetpack-sync-modules.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * A compatibility shim for the sync modules class.
+ *
+ * @package jetpack-compat
+ */
+
+use Automattic\Jetpack\Sync\Modules;
 
 /**
  * Class Jetpack_Sync_Modules
@@ -6,7 +13,14 @@
  * @deprecated Use Automattic\Jetpack\Sync\Modules
  */
 class Jetpack_Sync_Modules {
-	static function get_module( $module_name ) {
+
+	/**
+	 * Returns the sync module object.
+	 *
+	 * @param String $module_name the module name.
+	 * @return Automattic\Jetpack\Sync\Modules\Module the module object.
+	 */
+	public static function get_module( $module_name ) {
 		_deprecated_function( __METHOD__, 'jetpack-7.5', 'Automattic\Jetpack\Sync\Modules' );
 
 		return Modules::get_module( $module_name );
