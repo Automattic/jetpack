c409f8a977a92e509b282cf54ca477b730e557c7
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2019-07-12 17:20:37 +0100

    Fix prepare-commit-msg hook when file is missing (#12970)
    * Fix prepare-commit-msg hook when file is missing
    update danger to warn only once about not verified commits
    
    * return if file is missing
    
    * remove the error log
    
    * remove unneeded changes

--- bin/prepare-commit-msg.js
+++ bin/prepare-commit-msg.js
@@ -1,13 +1,18 @@
 /* eslint-disable no-console */
 const fs = require( 'fs' );
 
-const file = fs.readFileSync( '.git/last-commit-date' );
-const commitDate = file.toString();
+fs.readFile( '.git/last-commit-date', ( err, data ) => {
+	if ( err ) {
+		console.log( 'skipping prepare-commit-msg hook' );
+		return;
+	}
+	const commitDate = data.toString();
 
-if ( Date.now() - commitDate > 2000 /* 2sec*/ ) {
-	console.log( 'WARNING: git pre-commit was hook skipped!' );
-	const commitMsg = fs.readFileSync( '.git/COMMIT_EDITMSG' );
-	const newCommitMsg = '[not verified] ' + commitMsg.toString();
+	if ( Date.now() - commitDate > 2000 /* 2sec*/ ) {
+		console.log( 'WARNING: git pre-commit hook was skipped!' );
+		const commitMsg = fs.readFileSync( '.git/COMMIT_EDITMSG' );
+		const newCommitMsg = '[not verified] ' + commitMsg.toString();
 
-	fs.writeFileSync( '.git/COMMIT_EDITMSG', newCommitMsg );
-}
+		fs.writeFileSync( '.git/COMMIT_EDITMSG', newCommitMsg );
+	}
+} );
