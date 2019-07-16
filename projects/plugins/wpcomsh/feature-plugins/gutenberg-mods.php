8754e97209bbfc82517535b044352d2a6ae6d6c5
Marko Andrijasevic <markoandrijasevic@gmail.com>
2019-07-16 18:49:48 +0200

    Ensure that our actions fire at proper time

--- projects/plugins/wpcomsh/feature-plugins/gutenberg-mods.php
+++ projects/plugins/wpcomsh/feature-plugins/gutenberg-mods.php
@@ -6,10 +6,15 @@
  * we need to ensure that some experimental functionality is not exposed yet.
  */
 
-// Hide the experimental Widget Blocks section from the Customizer.
-// This filter is added in https://github.com/WordPress/gutenberg/blob/cf1da64370c209b25bb005c44083097a6137a119/lib/customizer.php#L73
-remove_action( 'customize_register', 'gutenberg_customize_register' );
+/**
+ * Removes Gutenberg experimental Widget Blocks section from the Customizer.
+ */
+function wpcomsh_remove_gutenberg_experimental_sections() {
+	// This filter is added in https://github.com/WordPress/gutenberg/blob/cf1da64370c209b25bb005c44083097a6137a119/lib/customizer.php#L73
+	remove_action( 'customize_register', 'gutenberg_customize_register' );
 
-// Remove some additional actions and filters that experimental Widget Blocks functionality adds.
-remove_action( 'customize_update_gutenberg_widget_blocks', 'gutenberg_customize_update' );
-remove_filter( 'widget_customizer_setting_args', 'filter_widget_customizer_setting_args' );
+	// Remove some additional actions and filters that experimental Widget Blocks functionality adds.
+	remove_action( 'customize_update_gutenberg_widget_blocks', 'gutenberg_customize_update' );
+	remove_filter( 'widget_customizer_setting_args', 'filter_widget_customizer_setting_args' );
+}
+add_action( 'plugins_loaded', 'wpcomsh_remove_gutenberg_experimental_sections' );
