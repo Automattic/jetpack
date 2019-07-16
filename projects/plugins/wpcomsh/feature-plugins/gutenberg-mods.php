15d183a6b0f4a1dcc92b355fda0a1b4524f01e40
Marko Andrijasevic <markoandrijasevic@gmail.com>
2019-07-16 23:37:22 +0200

    Gutenberg: update comments for plugin modifications

--- projects/plugins/wpcomsh/feature-plugins/gutenberg-mods.php
+++ projects/plugins/wpcomsh/feature-plugins/gutenberg-mods.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Customizations for the Full Site Editing plugin.
+ * Customizations for the Gutenberg plugin.
  *
  * Since we'll be trying to keep up with latest Gutenberg versions both on Simple and Atomic sites,
  * we need to ensure that some experimental functionality is not exposed yet.
  */
 
 /**
- * Removes Gutenberg experimental Widget Blocks section from the Customizer.
+ * Removes Gutenberg's experimental Widget Blocks section from the Customizer.
  */
 function wpcomsh_remove_gutenberg_experimental_sections() {
 	// This filter is added in https://github.com/WordPress/gutenberg/blob/cf1da64370c209b25bb005c44083097a6137a119/lib/customizer.php#L73
