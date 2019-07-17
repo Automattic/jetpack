ec44d8431cd766df9f33ba302da256f558f7b2b3
Bernie Reiter <ockham@raz.or.at>
2019-07-17 20:44:28 +0600

    Whitespace

--- _inc/lib/upgrade-nudge.php
+++ _inc/lib/upgrade-nudge.php
@@ -14,17 +14,17 @@ class Jetpack_Upgrade_Nudge {
 	 *
 	 * @return string The message telling the user to upgrade
 	 */
-    public static function get_upgrade_message() {
-        $support_url = ( defined( 'IS_WPCOM' ) && IS_WPCOM )
-        ? 'https://support.wordpress.com/simple-payments/'
-        : 'https://jetpack.com/support/simple-payment-button/';
+	public static function get_upgrade_message() {
+		$support_url = ( defined( 'IS_WPCOM' ) && IS_WPCOM )
+		? 'https://support.wordpress.com/simple-payments/'
+		: 'https://jetpack.com/support/simple-payment-button/';
 
-        return sprintf(
-            wp_kses(
-                __( 'Your plan doesn\'t include Simple Payments. <a href="%s" rel="noopener noreferrer" target="_blank">Learn more and upgrade</a>.', 'jetpack' ),
-                array( 'a' => array( 'href' => array(), 'rel' => array(), 'target' => array() ) )
-            ),
-            esc_url( $support_url )
-        );
-    }
+		return sprintf(
+			wp_kses(
+				__( 'Your plan doesn\'t include Simple Payments. <a href="%s" rel="noopener noreferrer" target="_blank">Learn more and upgrade</a>.', 'jetpack' ),
+				array( 'a' => array( 'href' => array(), 'rel' => array(), 'target' => array() ) )
+			),
+			esc_url( $support_url )
+		);
+	}
 }
