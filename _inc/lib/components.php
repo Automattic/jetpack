63c7b443990fcb016664b0638639d56f77cac932
Bernie Reiter <ockham@raz.or.at>
2019-07-19 17:07:58 +0600

    [not verified] Whitespace :roll_eyes:

--- _inc/lib/components.php
+++ _inc/lib/components.php
@@ -6,14 +6,14 @@
  */
 class Jetpack_Components {
 	/**
-     * Load and display a pre-rendered component
+	 * Load and display a pre-rendered component
 	 *
 	 * @since 7.6.0
 	 *
 	 * @return string The component markup
 	 */
 	public static function render_component( $name, $props ) {
-        $markup = @file_get_contents( JETPACK__PLUGIN_DIR . "_inc/build/$name.html" );
+		$markup = @file_get_contents( JETPACK__PLUGIN_DIR . "_inc/build/$name.html" );
 
 		foreach ( $props as $key => $value ) {
 			$markup = str_replace(
