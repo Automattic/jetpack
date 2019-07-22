b7e001b1b6be68cd2281af4c8280fdb2485cb581
Michael Turk <turk.michael@gmail.com>
2019-07-22 10:35:57 -0400

    sync always sent callables on wp-cli

--- packages/sync/src/modules/Callables.php
+++ packages/sync/src/modules/Callables.php
@@ -394,8 +394,10 @@ class Callables extends Module {
 		$callables = $this->get_all_callables();
 		if ( ! apply_filters( 'jetpack_check_and_send_callables', false ) ) {
 			if ( ! is_admin() ) {
-				// If we're not an admin and we're not doing cron, don't sync anything.
-				if ( ! Settings::is_doing_cron() ) {
+				// If we're not an admin and we're not doing cron and this isn't WP_CLI, don't sync anything.
+				if ( ! Settings::is_doing_cron() &&
+					 ! ( defined( 'WP_CLI' ) && WP_CLI )
+				) {
 					return;
 				}
 				// If we're not an admin and we are doing cron, sync the Callables that are always supposed to sync ( See https://github.com/Automattic/jetpack/issues/12924 ).
