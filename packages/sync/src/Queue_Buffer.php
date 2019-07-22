14070996a7df66b5562066180d0288ac438cf317
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2019-07-22 15:28:02 +0300

    Sync: Fix PHPCS errors in the Queue_Buffer (#13100)

--- packages/sync/src/Queue_Buffer.php
+++ packages/sync/src/Queue_Buffer.php
@@ -1,27 +1,77 @@
 <?php
+/**
+ * Sync queue buffer.
+ *
+ * @package automattic/jetpack-sync
+ */
 
 namespace Automattic\Jetpack\Sync;
 
 /**
- * A buffer of items from the queue that can be checked out
+ * A buffer of items from the queue that can be checked out.
  */
 class Queue_Buffer {
+	/**
+	 * Sync queue buffer ID.
+	 *
+	 * @access public
+	 *
+	 * @var int
+	 */
 	public $id;
+
+	/**
+	 * Sync items.
+	 *
+	 * @access public
+	 *
+	 * @var array
+	 */
 	public $items_with_ids;
 
+	/**
+	 * Constructor.
+	 * Initializes the queue buffer.
+	 *
+	 * @access public
+	 *
+	 * @param int   $id             Sync queue buffer ID.
+	 * @param array $items_with_ids Items for the buffer to work with.
+	 */
 	public function __construct( $id, $items_with_ids ) {
 		$this->id             = $id;
 		$this->items_with_ids = $items_with_ids;
 	}
 
+	/**
+	 * Retrieve the sync items in the buffer, in an ID => value form.
+	 *
+	 * @access public
+	 *
+	 * @return array Sync items in the buffer.
+	 */
 	public function get_items() {
 		return array_combine( $this->get_item_ids(), $this->get_item_values() );
 	}
 
+	/**
+	 * Retrieve the values of the sync items in the buffer.
+	 *
+	 * @access public
+	 *
+	 * @return array Sync items values.
+	 */
 	public function get_item_values() {
 		return Utils::get_item_values( $this->items_with_ids );
 	}
 
+	/**
+	 * Retrieve the IDs of the sync items in the buffer.
+	 *
+	 * @access public
+	 *
+	 * @return array Sync items IDs.
+	 */
 	public function get_item_ids() {
 		return Utils::get_item_ids( $this->items_with_ids );
 	}
