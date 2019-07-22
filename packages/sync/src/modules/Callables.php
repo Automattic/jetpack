a078f529cab38a18a7a7037df8a75b53fcf1f0b7
Michael Turk <gititon@users.noreply.github.com>
2019-07-22 14:39:52 -0400

    Sync Callables that should be always sent on WP_CLI (#13104)
    Sync Callables that should be always sent on WP_CLI

--- packages/sync/src/modules/Callables.php
+++ packages/sync/src/modules/Callables.php
@@ -10,6 +10,7 @@ namespace Automattic\Jetpack\Sync\Modules;
 use Automattic\Jetpack\Sync\Functions;
 use Automattic\Jetpack\Sync\Defaults;
 use Automattic\Jetpack\Sync\Settings;
+use Automattic\Jetpack\Constants as Jetpack_Constants;
 
 /**
  * Class to handle sync for callables.
@@ -394,8 +395,8 @@ class Callables extends Module {
 		$callables = $this->get_all_callables();
 		if ( ! apply_filters( 'jetpack_check_and_send_callables', false ) ) {
 			if ( ! is_admin() ) {
-				// If we're not an admin and we're not doing cron, don't sync anything.
-				if ( ! Settings::is_doing_cron() ) {
+				// If we're not an admin and we're not doing cron and this isn't WP_CLI, don't sync anything.
+				if ( ! Settings::is_doing_cron() && ! Jetpack_Constants::get_constant( 'WP_CLI' ) ) {
 					return;
 				}
 				// If we're not an admin and we are doing cron, sync the Callables that are always supposed to sync ( See https://github.com/Automattic/jetpack/issues/12924 ).
