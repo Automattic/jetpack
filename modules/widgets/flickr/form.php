27318e368c0e068222a18f639e4170d1dba697f5
Rasmy Nguyen <rtnguyen87@gmail.com>
2019-07-24 16:08:53 +0900

    Flickr Widget: Adds option to open Flickr images in a new tab
    Addresses this issue: https://github.com/Automattic/jetpack/issues/11405
    
    Previously, the Flickr widget images could only be opened via the same window which
    directs userss away from the site owners' site. This PR adds an
    "Open images in new tab?" option to flickr widget settings, allowing
    site owners to choose whether their users are directed away from their
    site or not.

--- modules/widgets/flickr/form.php
+++ modules/widgets/flickr/form.php
@@ -52,6 +52,15 @@
 </p>
 
 <p>
+	<label>
+		<?php esc_html_e( 'Open images in new tab?', 'jetpack' ); ?>
+	</label>
+    <input type="checkbox"
+           name="<?php echo esc_attr( $this->get_field_name( 'target' ) ); ?>"
+           <?php checked( $instance['target'] ); ?>
+    />
+</p>
+<p>
 	<div>
 		<?php esc_html_e( 'What size photos would you like to display?', 'jetpack' ); ?>
 	</div>
