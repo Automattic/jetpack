f1e73f5387589821c4294c99f457c37e74cf6b4b
Igor Zinovyev <zinigor@gmail.com>
2019-07-24 23:11:19 +0300

    Added a custom error handler that saves to Jetpack log.

--- jetpack-beta.php
+++ jetpack-beta.php
@@ -60,6 +60,8 @@ require_once 'autoupdate-self.php';
 require_once 'class-jetpackbetaclicommand.php';
 add_action( 'init', array( 'Jetpack_Beta_Autoupdate_Self', 'instance' ) );
 
+set_error_handler( array( 'Jetpack_Beta', 'custom_error_handler' ) );
+
 class Jetpack_Beta {
 
 	protected static $_instance = null;
@@ -1146,7 +1148,7 @@ class Jetpack_Beta {
 
 	/**
 	 * Callback function to include Jetpack beta options into Jetpack sync whitelist
-	 * 
+	 *
 	 * @param Array $whitelist List of whitelisted options to sync
 	 */
 	public function add_to_options_whitelist( $whitelist ) {
@@ -1155,6 +1157,32 @@ class Jetpack_Beta {
 		$whitelist[] = self::$option_autoupdate;
 		$whitelist[] = self::$option_email_notif;
 		return $whitelist;
+	}
+
+	/**
+	 * Custom error handler to intercept errors and log them using Jetpack's own logger.
+	 *
+	 * @param int $errno error code.
+	 * @param string $errstr error message.
+	 * @param string $errfile file name where the error happened.
+	 * @param int $errline line in the code.
+	 *
+	 * @return bool whether to make the default handler handle the error as well.
+	 */
+	public static function custom_error_handler( $errno, $errstr, $errfile, $errline ) {
+
+		if ( class_exists( 'Jetpack' ) && method_exists( 'Jetpack', 'log' ) ) {
+			$error_string = sprintf( "%s, %s:%d", $errstr, $errfile, $errline );
+			Jetpack::log( $errno, $error_string );
+		}
+
+		// If this error is not being reported in the current settings, stop reporting here by returning true.
+		if ( ! ( error_reporting() & $errno ) ) {
+			return true;
+		}
+
+		// Returning false makes the error go through the standard error handler as well.
+		return false;
 	}
 }
 
