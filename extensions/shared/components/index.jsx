f700e72ffcfcada26b1fe5f528f40282eba2026f
Bernie Reiter <ockham@raz.or.at>
2019-07-25 17:29:02 +0600

    Add explanatory comment

--- extensions/shared/components/index.jsx
+++ extensions/shared/components/index.jsx
@@ -11,6 +11,13 @@ import { UpgradeNudge } from '../upgrade-nudge';
 import './style.scss';
 
 // Use dummy props that can be overwritten by a str_replace() on the server.
+//
+// Note that we're using the 'dumb' component exported from `upgrade-nudge.jsx` here,
+// rather than the 'smart' one (which is wrapped in `withSelect` and `withDispatch` calls).
+// This means putting the burden of props computation on PHP (`components.php`).
+// If we wanted to use the 'smart' component instead, we'd need to provide sufficiently
+// initialised Redux state when rendering ir (probably through globals set as arguments
+// to the `StaticSiteGeneratorPlugin` call in `webpack.config.extensions.js`).
 const upgradeNudge = renderToStaticMarkup(
 	<UpgradeNudge planName="#planName#" upgradeUrl="#upgradeUrl#" />
 );
