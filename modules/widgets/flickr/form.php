363e4fde5b94d3413ec92fdb1eb43e5b45110dc3
Rasmy Nguyen <rtnguyen87@gmail.com>
2019-07-26 01:18:10 +0900

    Places checkbox input element before label text
    This commit places the checkbox input element for the "Open images in
    new tab?" option" before the label text and nested within label tags to
    be in line with similar Jetpack widget settings.

--- modules/widgets/flickr/form.php
+++ modules/widgets/flickr/form.php
@@ -53,13 +53,13 @@
 
 <p>
 	<label>
+		<input
+			type="checkbox"
+			name="<?php echo esc_attr( $this->get_field_name( 'target' ) ); ?>"
+			<?php checked( $instance['target'] ); ?>
+		/>
 		<?php esc_html_e( 'Open images in new tab?', 'jetpack' ); ?>
 	</label>
-	<input
-		type="checkbox"
-		name="<?php echo esc_attr( $this->get_field_name( 'target' ) ); ?>"
-		<?php checked( $instance['target'] ); ?>
-	/>
 </p>
 <p>
 	<div>
