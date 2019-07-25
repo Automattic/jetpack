8b5823963e5893ad64211f68629f4472c1a2896a
Peter Fabian <peter.fabian.github@gmail.com>
2019-07-25 19:55:48 +0200

    Fixed variable name to the defined one. (#13083)
    As declaraion is not defined, it generates a lot of notices and does not work correctly. I'm guessing the invociation should check for old_declaration.

--- packages/analyzer/src/Differences/Class_Moved.php
+++ packages/analyzer/src/Differences/Class_Moved.php
@@ -29,7 +29,7 @@ class Class_Moved extends PersistentListItem implements Invocation_Warner {
 	}
 
 	public function find_invocation_warnings( $invocation, $warnings ) {
-		if ( $invocation->depends_on( $this->declaration ) ) {
+		if ( $invocation->depends_on( $this->old_declaration ) ) {
 			$warnings->add(
 				new Warning( $this->type(), $invocation->path, $invocation->line, 'Class ' . $this->old_declaration->display_name() . ' was moved from ' . $this->old_declaration->path . ' to ' . $this->new_declaration->path, $this->old_declaration )
 			);
