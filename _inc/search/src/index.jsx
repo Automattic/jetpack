e17c5e36e35186ab64986c46f7efbd61af3dddf7
Jason Moon <jason.moon@automattic.com>
2019-07-26 15:03:00 -0600

    Instant Search: Render interface on search pages

--- _inc/search/src/index.jsx
+++ _inc/search/src/index.jsx
@@ -10,12 +10,38 @@ import { h, render } from 'preact';
  */
 import SearchWidget from '../components/search-widget';
 
-const injectSearchWidget = ( initialValue, target, grabFocus ) => {
-	render(
-		<SearchWidget initialValue={ initialValue } grabFocus={ grabFocus } />,
-		document.body,
-		target
-	);
+function removeChildren( htmlElement ) {
+	while ( htmlElement.lastChild ) {
+		htmlElement.removeChild( htmlElement.lastChild );
+	}
+}
+
+const hideSearchHeader = () => {
+	const titleElements = document.getElementById( 'content' ).getElementsByClassName( 'page-title' );
+	if ( titleElements.length > 0 ) {
+		titleElements[ 0 ].style.display = 'none';
+	}
 };
 
-window.injectSearchWidget = injectSearchWidget;
+const injectSearchWidget = ( initialValue, target, grabFocus ) => {
+	render( <SearchWidget initialValue={ initialValue } grabFocus={ grabFocus } />, target );
+};
+
+document.addEventListener( 'DOMContentLoaded', function() {
+	if ( document.body && document.body.classList.contains( 'search' ) ) {
+		const element = document.getElementsByTagName( 'main' ).namedItem( 'main' );
+		if ( !! element ) {
+			let initialValue = '';
+			const existingSearch = document.getElementsByClassName( 'widget_search' );
+			if ( existingSearch.length > 0 ) {
+				const inputs = existingSearch[ 0 ].getElementsByTagName( 'input' );
+				if ( inputs.length > 0 ) {
+					initialValue = inputs[ 0 ].value;
+				}
+			}
+			removeChildren( element );
+			hideSearchHeader();
+			injectSearchWidget( initialValue, element );
+		}
+	}
+} );
