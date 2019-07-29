2c8fe44fc386324040fa9f8e9bc15729322f9f98
Weston Ruter <westonruter@google.com>
2019-07-29 12:26:39 -0700

    Improve AMP support for Photon (#12821)
    * Update parse_images_from_html to find amp-img and amp-anim
    
    * Skip adding data-recalc-dims=1 attr in AMP
    
    * Pass original width/height/transform to jetpack_photon_post_image_args
    
    * Add jetpack_photon_post_image_args filter to constrain image sizes for AMP Stories
    
    * Add helper method to ensure Jetpack_AMP_Support exists
    
    * Include transform in details passed to jetpack_photon_post_image_args
    
    * Add tests for jetpack_photon_post_image_args filter, including details param and AMP support
    
    * Fix broken tests due to variability of subdomain

--- tests/php/test_class.jetpack_photon.php
+++ tests/php/test_class.jetpack_photon.php
@@ -804,6 +804,7 @@ class WP_Test_Jetpack_Photon extends Jetpack_Attachment_Test_Case {
 
 		$this->assertArrayNotHasKey( 'width', $attributes );
 		$this->assertArrayNotHasKey( 'height', $attributes );
+		$this->assertContains( 'data-recalc-dims', $filtered_content );
 	}
 
 	/**
@@ -815,9 +816,9 @@ class WP_Test_Jetpack_Photon extends Jetpack_Attachment_Test_Case {
 	public function test_photon_filter_the_content_width_height_attributes_when_image_args_filtered( $filter_callback, $has_attributes, $width, $height ) {
 		list( $sample_html ) = $this->get_photon_sample_content( 'a-tags-without-images.html' );
 
-		add_filter( 'jetpack_photon_post_image_args', array( $this, $filter_callback ) );
+		add_filter( 'jetpack_photon_post_image_args', $filter_callback, 10, 2 );
 		$filtered_content = Jetpack_Photon::filter_the_content( $sample_html );
-		remove_filter( 'jetpack_photon_post_image_args', array( $this, $filter_callback ) );
+		remove_filter( 'jetpack_photon_post_image_args', $filter_callback, 10, 2 );
 
 		$first_line = strtok( $filtered_content, "\n" ); // Should contain an image tag on the first line
 		$attributes = wp_kses_hair( $first_line, wp_allowed_protocols() );
@@ -836,27 +837,62 @@ class WP_Test_Jetpack_Photon extends Jetpack_Attachment_Test_Case {
 	}
 
 	public function photon_attributes_when_filtered_data_provider() {
+		$that = $this; // For sake of PHP 5.3.
+
+		$assert_details = function ( $details ) use ( $that ) {
+			$that->assertInternalType( 'array', $details );
+			$that->assertArrayHasKey( 'tag', $details );
+			$that->assertArrayHasKey( 'src', $details );
+			$that->assertArrayHasKey( 'src_orig', $details );
+			$that->assertArrayHasKey( 'width', $details );
+			$that->assertArrayHasKey( 'width_orig', $details );
+			$that->assertArrayHasKey( 'height', $details );
+			$that->assertArrayHasKey( 'height_orig', $details );
+			$that->assertArrayHasKey( 'transform', $details );
+			$that->assertArrayHasKey( 'transform_orig', $details );
+		};
+
 		return array(
 			'photon_post_image_args_force_resize' => array(
-				'photon_post_image_args_force_resize',
+				function( $args, $details ) use ( $assert_details ) {
+					$assert_details( $details );
+					return array(
+						'resize' => '300,250'
+					);
+				},
 				true,
 				300,
 				250
 			),
 			'photon_post_image_args_force_fit' => array(
-				'photon_post_image_args_force_fit',
+				function ( $args, $details ) use ( $assert_details ) {
+					$assert_details( $details );
+					return array(
+						'fit' => '600,600'
+					);
+				},
 				true,
 				600,
 				600
 			),
 			'photon_post_image_args_force_lb' => array(
-				'photon_post_image_args_force_lb',
+				function ( $args, $details ) use ( $assert_details ) {
+					$assert_details( $details );
+					return array(
+						'lb' => '800,100,000000'
+					);
+				},
 				true,
 				800,
 				100
 			),
 			'photon_post_image_args_force_width_only' => array(
-				'photon_post_image_args_force_width_only',
+				function ( $args, $details ) use ( $assert_details ) {
+					$assert_details( $details );
+					return array(
+						'w' => '104'
+					);
+				},
 				false,
 				false,
 				false
@@ -864,28 +900,75 @@ class WP_Test_Jetpack_Photon extends Jetpack_Attachment_Test_Case {
 		);
 	}
 
-	public function photon_post_image_args_force_resize() {
+	/**
+	 * @author westonruter
+	 * @covers Jetpack_Photon::filter_the_content
+	 * @dataProvider photon_attributes_when_amp_response
+	 * @since 7.6.0
+	 *
+	 * @param string $sample_html Sample HTML.
+	 * @param string $photon_src  Photon URL suffix (after the subdomain).
+	 */
+	public function test_photon_filter_the_content_for_amp_responses( $sample_html, $photon_src ) {
+		add_filter( 'jetpack_is_amp_request', '__return_true' );
+		$filtered_content = Jetpack_Photon::filter_the_content( $sample_html );
+		$attributes = wp_kses_hair( $filtered_content, wp_allowed_protocols() );
+		$this->assertStringEndsWith( $photon_src, html_entity_decode( $attributes['src']['value'] ) );
+		$this->assertArrayHasKey( 'width', $attributes );
+		$this->assertArrayHasKey( 'height', $attributes );
+		$this->assertNotContains( 'data-recalc-dims', $filtered_content );
+	}
+
+	/**
+	 * Data provider for testing AMP responses.
+	 *
+	 * @return array
+	 */
+	public function photon_attributes_when_amp_response() {
 		return array(
-			'resize' => '300,250'
+			'amp-img' => array(
+				'<amp-img class="aligncenter wp-image-6372" title="Tube Bomber salmon dry fly" alt="Tube Bomber salmon dry fly" src="http://www.fishmadman.com/pages/wp-content/uploads/2012/02/Rav-fra-2004-2009-11-1024x611.jpg" width="102" height="61"></amp-img>',
+				'.wp.com/www.fishmadman.com/pages/wp-content/uploads/2012/02/Rav-fra-2004-2009-11-1024x611.jpg?resize=102%2C61',
+			),
+			'amp-anim' => array(
+				'<amp-anim alt="LOL" src="https://example.com/lol.gif" width="32" height="32"></amp-anim>',
+				'.wp.com/example.com/lol.gif?resize=32%2C32&ssl=1',
+			),
 		);
 	}
 
-	public function photon_post_image_args_force_fit() {
-		return array(
-			'fit' => '600,600'
-		);
-	}
+	/**
+	 * @author westonruter
+	 * @covers Jetpack_Photon::filter_the_content
+	 * @covers Jetpack_AMP_Support::filter_photon_post_image_args_for_stories
+	 * @since 7.6.0
+	 */
+	public function test_photon_filter_the_content_for_amp_story() {
+		$post_type = 'amp_story';
+		add_filter( 'jetpack_is_amp_request', '__return_true' );
+		register_post_type( $post_type, array( 'public' => true ) );
+		Jetpack_AMP_Support::init();
+		$post = $this->factory()->post->create_and_get( compact( 'post_type' ) );
+		$this->go_to( get_permalink( $post ) );
+		$this->assertTrue( is_singular( $post_type ) );
 
-	public function photon_post_image_args_force_lb() {
-		return array(
-			'lb' => '800,100,000000'
+		$content = implode(
+			"\n",
+			array(
+				'<!-- wp:amp/amp-story-page {"mediaId":2414,"mediaType":"image","focalPoint":{"x":0.4900990099009901,"y":0.5131578947368421}} -->',
+				'<amp-story-page style="background-color:#ffffff" id="a6c81a13-14a0-464b-88fa-9612e86bacf7" class="wp-block-amp-amp-story-page"><amp-story-grid-layer template="fill"><amp-img layout="fill" src="https://example.com/wp-content/uploads/2019/06/huge.jpg" style="object-position:49.00990099009901% 51.31578947368421%"></amp-img></amp-story-grid-layer><amp-story-grid-layer template="fill"></amp-story-grid-layer></amp-story-page>',
+				'<!-- /wp:amp/amp-story-page -->',
+			)
 		);
-	}
 
-	public function photon_post_image_args_force_width_only() {
-		return array(
-			'w' => '104'
+		$filtered_content = apply_filters( 'the_content', $content, $post->ID );
+
+		$this->assertContains(
+			'.wp.com/example.com/wp-content/uploads/2019/06/huge.jpg?h=1440&#038;ssl=1',
+			$filtered_content
 		);
+
+		unregister_post_type( $post_type );
 	}
 
 	/**
