8b867193de38249afb8f425eec66f8b06a9bc088
Bernie Reiter <ockham@raz.or.at>
2019-08-01 13:40:01 +0600

    Add unit test for _n() (currently failing)

--- extensions/shared/test/i18n-to-php.js
+++ extensions/shared/test/i18n-to-php.js
@@ -18,4 +18,10 @@ describe( 'i18n-to-php', () => {
 			"<span><?php esc_html_e( 'Upgrade to a paid plan to use this block on your site.', 'text-domain' ) ?></span>"
 		);
 	} );
+
+	test( 'renders _n() to its PHP counterpart as expected', () => {
+		expect( renderToStaticMarkup( _n( '%d person', '%d people', 1 + 2, 'text-domain' ) ) ).toBe(
+			"<span><?php echo esc_html( _n( '%d person', '%d people', 3, 'text-domain' ) ) ?></span>"
+		);
+	} );
 } );
