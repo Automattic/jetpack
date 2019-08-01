6fe8af95334195b6796b8fa32d2a6c42e7577941
Bernie Reiter <ockham@raz.or.at>
2019-08-01 13:34:51 +0600

    Add unit test for i18n-to-php.js

---
+++ extensions/shared/test/i18n-to-php.js
@@ -0,0 +1,21 @@
+/**
+ * External dependencies
+ */
+import { renderToStaticMarkup } from 'react-dom/server';
+
+/**
+ * Internal dependencies
+ */
+import { __, _n, _x, _nx } from '../i18n-to-php';
+
+describe( 'i18n-to-php', () => {
+	test( 'renders __ as to its PHP counterpart as expected', () => {
+		expect(
+			renderToStaticMarkup(
+				__( 'Upgrade to a paid plan to use this block on your site.', 'jetpack' )
+			)
+		).toBe(
+			"<span><?php esc_html_e( 'Upgrade to a paid plan to use this block on your site.', 'jetpack' ) ?></span>"
+		);
+	} );
+} );
