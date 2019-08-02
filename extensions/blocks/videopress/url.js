765e3728294604df851db4f73f0a200f9aaf6c80
mmtr <miguel.torres@automattic.com>
2019-08-02 09:09:09 +0900

    Cleaner URLs by avoiding setting default options

--- extensions/blocks/videopress/url.js
+++ extensions/blocks/videopress/url.js
@@ -8,14 +8,20 @@ export const getVideoPressUrl = ( guid, { autoplay, controls, loop, muted, poste
 		return null;
 	}
 
+	// In order to have a cleaner URL, we only set the options differing from the default VideoPress player settings:
+	// - Autoplay: Turned off by default.
+	// - Controls: Turned on by default.
+	// - Loop: Turned off by default.
+	// - Muted: Turned off by default.
+	// - Poster: No image by default.
+	// - Preload: None by default.
 	const options = {
-		autoPlay: autoplay,
-		controls,
-		loop,
-		muted,
-		...( muted && { persistVolume: false } ),
-		posterUrl: poster,
-		preload,
+		...( autoplay && { autoPlay: true } ),
+		...( ! controls && { controls: false } ),
+		...( loop && { loop: true } ),
+		...( muted && { muted: true, persistVolume: false } ),
+		...( poster && { posterUrl: poster } ),
+		...( preload !== 'none' && { preloadContent: preload } ),
 	};
 	return addQueryArgs( `https://videopress.com/v/${ guid }`, options );
 };
