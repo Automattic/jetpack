2c21de0cc6189114bfa91911fc2058960174907e
Jason Moon <jason.moon@automattic.com>
2019-08-05 13:23:05 -0600

    ESLint: Simplify via configuration cascading
    Simplify linting configuration and `yarn lint` by using config cascading.
    
    See https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy.

--- .eslintrc.js
+++ .eslintrc.js
@@ -99,32 +99,4 @@ module.exports = {
 		'react/no-string-refs': 0,
 		'jsx-a11y/anchor-is-valid': 0,
 	},
-	overrides: [
-		{
-			files: [ 'extensions/**/*', 'webpack.config.extensions.js' ],
-			rules: {
-				'react/forbid-elements': [
-					'error',
-					{
-						forbid: [
-							[ 'circle', 'Circle' ],
-							[ 'g', 'G' ],
-							[ 'path', 'Path' ],
-							[ 'polygon', 'Polygon' ],
-							[ 'rect', 'Rect' ],
-							[ 'svg', 'SVG' ],
-						].map( ( [ element, componentName ] ) => ( {
-							element,
-							message: `use <${ componentName }> from @wordpress/components`,
-						} ) ),
-					},
-				],
-				'react/jsx-no-bind': 0,
-				'react/react-in-jsx-scope': 0,
-				'space-unary-ops': 0,
-				'space-before-function-paren': 0,
-				'wpcalypso/jsx-classname-namespace': 0,
-			},
-		},
-	],
 };
