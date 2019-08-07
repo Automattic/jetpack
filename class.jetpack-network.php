ac97289e8b227e867551d3566b99d51286232a64
Marin Atanasov <tyxla@abv.bg>
2019-08-07 18:23:55 +0300

    Use esc_html__()

--- class.jetpack-network.php
+++ class.jetpack-network.php
@@ -553,7 +553,7 @@ class Jetpack_Network {
 		require_once 'class.jetpack-network-sites-list-table.php';
 
 		$network_sites_table = new Jetpack_Network_Sites_List_Table();
-		echo '<div class="wrap"><h2>' . esc_html( __( 'Sites', 'jetpack' ) ) . '</h2>';
+		echo '<div class="wrap"><h2>' . esc_html__( 'Sites', 'jetpack' ) . '</h2>';
 		echo '<form method="post">';
 		$network_sites_table->prepare_items();
 		$network_sites_table->display();
