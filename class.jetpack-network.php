1d3c393bd953ac0bf2bc128af3a88c6b9b454e7d
Marin Atanasov <tyxla@abv.bg>
2019-08-07 18:26:05 +0300

    Fix code in comment

--- class.jetpack-network.php
+++ class.jetpack-network.php
@@ -628,21 +628,21 @@ class Jetpack_Network {
 
 		/**
 		 * Remove the toggles for 2.9, re-evaluate how they're done and added for a 3.0 release. They don't feel quite right yet.
-
-		$manage_auto_activated_modules = 0;
-		if ( isset( $_POST['manage_auto_activated_modules'] ) ) {
-			$manage_auto_activated_modules = 1;
-		}
-
-		$modules = array();
-		if ( isset( $_POST['modules'] ) ) {
-			$modules = $_POST['modules'];
-		}
-
-		Removed from the data array below:
-		// 'manage_auto_activated_modules' => $manage_auto_activated_modules,
-		// 'modules'                       => $modules,
-		*/
+		 *
+		 * $manage_auto_activated_modules = 0;
+		 * if ( isset( $_POST['manage_auto_activated_modules'] ) ) {
+		 *     $manage_auto_activated_modules = 1;
+		 * }
+		 *
+		 * $modules = array();
+		 * if ( isset( $_POST['modules'] ) ) {
+		 *     $modules = $_POST['modules'];
+		 * }
+		 *
+		 * Removed from the data array below:
+		 * // 'manage_auto_activated_modules' => $manage_auto_activated_modules,
+		 * // 'modules'                       => $modules,
+		 */
 
 		$data = array(
 			'auto-connect'                 => $auto_connect,
