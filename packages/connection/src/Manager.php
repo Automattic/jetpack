58b84e5397cbc0f4ee118bca13e69c0638b0244a
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2019-08-08 12:06:18 +0300

    Use double backslash to specify global namespace properly

--- packages/connection/src/Manager.php
+++ packages/connection/src/Manager.php
@@ -619,7 +619,7 @@ class Manager implements Manager_Interface {
 	 * @return Integer zero on success, or a bitmask on failure.
 	 */
 	public function register( $api_endpoint = 'register' ) {
-		add_action( 'pre_update_jetpack_option_register', array( '\Jetpack_Options', 'delete_option' ) );
+		add_action( 'pre_update_jetpack_option_register', array( '\\Jetpack_Options', 'delete_option' ) );
 		$secrets = $this->generate_secrets( 'register', get_current_user_id(), 600 );
 
 		if (
