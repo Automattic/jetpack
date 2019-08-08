0adfb50db68d8fe620627a9f782ca57f783d0e14
Marin Atanasov <tyxla@abv.bg>
2019-08-08 11:39:20 +0300

    🔪 Remove PHP 5.2.2 hack (see https://bugs.php.net/bug.php?id=41293)

--- packages/connection/src/Manager.php
+++ packages/connection/src/Manager.php
@@ -204,12 +204,6 @@ class Manager implements Manager_Interface {
 		// Some browser-embedded clients send cookies. We don't want them.
 		$_COOKIE = array();
 
-		// A bug in PHP < 5.2.2 makes $HTTP_RAW_POST_DATA not set by default,
-		// but we can do it ourself.
-		if ( ! isset( $HTTP_RAW_POST_DATA ) ) {
-			$HTTP_RAW_POST_DATA = file_get_contents( 'php://input' );
-		}
-
 		// A fix for mozBlog and other cases where '<?xml' isn't on the very first line.
 		if ( isset( $HTTP_RAW_POST_DATA ) ) {
 			$HTTP_RAW_POST_DATA = trim( $HTTP_RAW_POST_DATA );
