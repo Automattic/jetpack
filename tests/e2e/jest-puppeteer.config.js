ef7b0fa1c8866b0a316eb739051c80519cec3427
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2019-08-13 20:33:41 +0300

    fix the tests!

--- tests/e2e/jest-puppeteer.config.js
+++ tests/e2e/jest-puppeteer.config.js
@@ -14,5 +14,8 @@ module.exports = {
 		devtools: process.env.PUPPETEER_HEADLESS === 'false',
 		slowMo: parseInt( process.env.PUPPETEER_SLOWMO, 10 ) || 0,
 		executablePath,
+		// args: [ '--no-sandbox' ],
+		dumpio: true,
 	},
+	exitOnPageError: false,
 };
