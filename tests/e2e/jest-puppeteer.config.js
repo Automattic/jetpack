476f303f3784af34f78df406174798e0141d3d98
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2019-08-19 15:33:54 +0300

    address feedback

--- tests/e2e/jest-puppeteer.config.js
+++ tests/e2e/jest-puppeteer.config.js
@@ -19,7 +19,6 @@ module.exports = {
 		devtools: process.env.PUPPETEER_HEADLESS === 'false',
 		slowMo: parseInt( process.env.PUPPETEER_SLOWMO, 10 ) || 0,
 		executablePath,
-		// args: [ '--no-sandbox' ],
 		dumpio,
 	},
 	/**
