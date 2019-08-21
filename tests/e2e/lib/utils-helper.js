71b2d16ae1036be00f9c570d59e8bb37b4c8b7d8
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2019-08-21 16:03:20 +0300

    refactor

---
+++ tests/e2e/lib/utils-helper.js
@@ -0,0 +1,16 @@
+/**
+ * Executes a shell command and return it as a Promise.
+ * @param {string} cmd  shell command
+ * @return {Promise<string>} output
+ */
+export function execShellCommand( cmd ) {
+	const exec = require( 'child_process' ).exec;
+	return new Promise( resolve => {
+		exec( cmd, ( error, stdout, stderr ) => {
+			if ( error ) {
+				console.warn( error );
+			}
+			resolve( stdout ? stdout : stderr );
+		} );
+	} );
+}
