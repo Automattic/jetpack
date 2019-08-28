beb8496f350f6f86f1657b3970e1eeabe834d8f2
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2019-08-28 18:03:25 +0300

    Force old connection flow

--- tests/e2e/bin/setup-e2e-travis.sh
+++ tests/e2e/bin/setup-e2e-travis.sh
@@ -139,6 +139,11 @@ PHP
 	wp --allow-root config set WP_DEBUG_LOG true --raw --type=constant
 	wp --allow-root config set WP_DEBUG_DISPLAY false --raw --type=constant
 
+	# NOTE: Force classic connection flow
+	# https://github.com/Automattic/jetpack/pull/13288
+	wp --allow-root config set JETPACK_SHOULD_USE_CONNECTION_IFRAME false --raw --type=constant
+
+
 	wp db create
 
 	wp core install --url="$WP_SITE_URL" --title="E2E Gutenpack blocks" --admin_user=wordpress --admin_password=wordpress --admin_email=wordpress@example.com --path=$WP_CORE_DIR
