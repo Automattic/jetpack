e364101e4b72ec6f4eb5451989993a7ff74b8f7f
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2019-08-28 14:44:19 +0300

    write log into file

--- tests/e2e/specs/connection.test.js
+++ tests/e2e/specs/connection.test.js
@@ -11,23 +11,27 @@ import JetpackPage from '../lib/pages/wp-admin/jetpack';
  */
 import { execSyncShellCommand, execShellCommand, execShellFile } from '../lib/utils-helper';
 import { spawn } from 'child_process';
+const fs = require( 'fs' );
 
 jest.setTimeout( 600000 );
 
 const execWithPromise = async command => {
+	const out = fs.openSync( './out.log', 'a' );
+	const err = fs.openSync( './out.log', 'a' );
 	return new Promise( async ( resolve, reject ) => {
 		const process = spawn( command, {
 			detached: true,
-			stdio: 'ignore',
+			stdio: [ 'ignore', out, err ],
 		} );
-		process.stdout.on( 'data', data => console.log( data ) );
-		process.stderr.on( 'data', data => console.log( 'ERR: ' + data ) );
+		process.unref();
+		// process.stdout.on( 'data', data => console.log( data ) );
+		// process.stderr.on( 'data', data => console.log( 'ERR: ' + data ) );
 		process.on( 'data', data => {
 			console.log( data );
 			return resolve( data );
 		} );
-		process.on( 'error', err => reject( err ) );
-		process.on( 'close', err => reject( err ) );
+		process.on( 'error', e => reject( e ) );
+		process.on( 'close', e => reject( e ) );
 	} );
 };
 
@@ -36,6 +40,10 @@ async function resetWordpressInstall() {
 	// const out = await execShellFile( './tests/e2e/bin/setup-e2e-travis.sh', [ 'reset_wp' ] );
 	// const out = await execShellCommand( 'sh ./tests/e2e/bin/setup-e2e-travis.sh reset_wp' );
 	console.log( '!!!!!!!!!!!!!!!!!' );
+	const file = fs.readFileSync( './out.log' );
+	console.log( file );
+	console.log( '======' );
+
 	console.log( out );
 }
 
