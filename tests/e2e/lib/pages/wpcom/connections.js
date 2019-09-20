491d25b363028b5c6f6e6376bc1e6bd4e7584f10
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2019-09-20 12:08:13 +0300

    fix typo

--- tests/e2e/lib/pages/wpcom/connections.js
+++ tests/e2e/lib/pages/wpcom/connections.js
@@ -24,7 +24,7 @@ export default class ConnectionsPage extends Page {
 
 		// Trying to reload page, maybe it will help?
 		await this.page.reload();
-		await this.page.waitForPage();
+		await this.waitForPage();
 
 		await waitAndClick( this.page, mailchimpExpandSelector );
 
