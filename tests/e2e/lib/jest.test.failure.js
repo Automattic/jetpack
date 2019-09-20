b85eece280be99de911bb9dd4604dad61741f53a
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2019-09-20 13:22:56 +0300

    a bit more debug logging

--- tests/e2e/lib/jest.test.failure.js
+++ tests/e2e/lib/jest.test.failure.js
@@ -8,7 +8,7 @@ import { wrap } from 'lodash';
 import { sendFailedTestScreenshotToSlack, sendFailedTestMessageToSlack } from './reporters/slack';
 import { takeScreenshot } from './reporters/screenshot';
 import { readFileSync } from 'fs';
-import { execShellCommand } from './utils-helper';
+import { execShellCommand, execSyncShellCommand } from './utils-helper';
 
 /**
  * Override the test case method so we can take screenshots of assertion failures.
@@ -38,7 +38,10 @@ global.it = async ( name, func ) => {
 				const filePath = await takeScreenshot( currentBlock, name );
 				await sendFailedTestMessageToSlack( { block: currentBlock, name, error } );
 				await sendFailedTestScreenshotToSlack( filePath );
-				const fileContents = readFileSync( '~/wordpress/wp-content/debug.log' );
+				console.log( execSyncShellCommand( 'ls /home/travis/wordpress/wp-content/' ) );
+				console.log( execSyncShellCommand( 'cat /home/travis/wordpress/wp-content/' ) );
+
+				const fileContents = readFileSync( '/home/travis/wordpress/wp-content/debug.log' );
 				console.log( fileContents.toString() );
 			}
 
