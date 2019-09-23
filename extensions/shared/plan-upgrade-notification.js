b62c5aa1790033d0a3d9ac4b3019852a53b6294a
chris <chriskmnds@gmail.com>
2019-09-23 15:17:20 +0300

    Premium blocks: Plan upgrade notif, show current plan - jetpack site.

--- extensions/shared/plan-upgrade-notification.js
+++ extensions/shared/plan-upgrade-notification.js
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+import apiFetch from '@wordpress/api-fetch';
 import { dispatch } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 import '@wordpress/notices';
@@ -15,9 +16,44 @@ if ( undefined !== typeof window && window.location ) {
 	const { query } = parseUrl( window.location.href, true );
 
 	if ( query.plan_upgraded ) {
-		dispatch( 'core/notices' ).createNotice(
-			'success',
-			__( 'Plan upgraded. You may use premium blocks now.', 'jetpack' )
-		);
+		const path = '/jetpack/v4/site';
+
+		apiFetch( { path } )
+			.then( response => {
+				const data = JSON.parse( response.data );
+				const productName = data.plan.product_name;
+				const viewSitePlanlink = `https://wordpress.com/plans/my-plan/${ window.location.host }`;
+
+				dispatch( 'core/notices' ).createNotice(
+					'success',
+					__( `Congratulations! Your site is now on the ${ productName } plan.`, 'jetpack' ),
+					{
+						isDismissible: true,
+						actions: [
+							{
+								url: viewSitePlanlink,
+								label: __( 'View my plan', 'jetpack' ),
+							},
+						],
+					}
+				);
+			} )
+			.catch( () => {
+				const viewSitePlanlink = `https://wordpress.com/plans/my-plan/${ window.location.host }`;
+
+				dispatch( 'core/notices' ).createNotice(
+					'success',
+					__( `Congratulations! Your site is now on a paid plan.`, 'jetpack' ),
+					{
+						isDismissible: true,
+						actions: [
+							{
+								url: viewSitePlanlink,
+								label: __( '[View my plan]', 'jetpack' ),
+							},
+						],
+					}
+				);
+			} );
 	}
 }
