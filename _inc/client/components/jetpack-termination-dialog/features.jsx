ff4d9d2900addab8efd9e070c583616ebec86af0
Caleb Bauermeister <cbauerman@gmail.com>
2019-09-23 16:12:46 -0700

    add copy if site benefits are <= 2

--- _inc/client/components/jetpack-termination-dialog/features.jsx
+++ _inc/client/components/jetpack-termination-dialog/features.jsx
@@ -34,12 +34,12 @@ const JetpackDisconnectDialogFeatures = ( {
 			amount: 32030,
 			gridIcon: 'lock',
 		},
-		// {
-		// 	title: 'Contact Forms',
-		// 	description: 'The number of live Jetpack forms on your site right now.',
-		// 	amount: 31,
-		// 	gridIcon: 'align-image-center',
-		// },
+		{
+			title: 'Contact Forms',
+			description: 'The number of live Jetpack forms on your site right now.',
+			amount: 31,
+			gridIcon: 'align-image-center',
+		},
 		// {
 		// 	title: 'Publicize',
 		// 	description: 'The number of live social media connections, powered by Jetpack.',
@@ -73,13 +73,15 @@ const JetpackDisconnectDialogFeatures = ( {
 			<Card>
 				<p className="jetpack-termination-dialog__info">
 					{ __(
-						'Jetpack is currently powering several features of %(siteName)s. Once you disable Jetpack, these features will no longer be available and your site may no longer function the same way. We’ve highlighted some of the features you rely on below.',
+						'Jetpack is currently powering several features of %(siteName)s. Once you disable Jetpack, these features will no longer be available and your site may no longer function the same way.',
 						{
 							args: {
 								siteName,
 							},
 						}
 					) }
+					{ siteBenefitCount > 0 &&
+						__( ' We’ve highlighted some of the features you rely on below.' ) }
 				</p>
 				<div
 					className={
@@ -88,20 +90,69 @@ const JetpackDisconnectDialogFeatures = ( {
 							: 'jetpack-termination-dialog__features-list'
 					}
 				>
-					{ siteBenefitCount ? (
-						siteBenefits.map( ( { title, description, amount, gridIcon } ) => (
-							<SingleFeature
-								amount={ amount }
-								description={ description }
-								gridIcon={ gridIcon }
-								title={ title }
-							/>
-						) )
-					) : (
-						<div />
-					) }
+					{ siteBenefits.map( ( { title, description, amount, gridIcon } ) => (
+						<SingleFeature
+							amount={ amount }
+							description={ description }
+							gridIcon={ gridIcon }
+							title={ title }
+						/>
+					) ) }
 				</div>
-
+				{ siteBenefitCount <= 2 && (
+					<div className="jetpack-termination-dialog__generic-info">
+						<h2>
+							{ __( 'Jetpack has many powerful tools that can help you achieve your goals  ' ) }
+						</h2>
+						<ul>
+							<li key="reason-1">
+								{ __(
+									'Speed up your site and provide mobile-ready images with {{a}}our CDN{{/a}}',
+									{
+										components: {
+											a: (
+												<a
+													href="https://jetpack.com/support/site-accelerator/"
+													target="_blank"
+													rel="noopener noreferrer"
+												/>
+											),
+										},
+									}
+								) }
+							</li>
+							<li key="reason-2">
+								{ __(
+									'Block {{a}}brute force attacks{{/a}} and get immediate notifications if your site is down',
+									{
+										components: {
+											a: (
+												<a
+													href="https://jetpack.com/support/protect/"
+													target="_blank"
+													rel="noopener noreferrer"
+												/>
+											),
+										},
+									}
+								) }
+							</li>
+							<li key="reason-3">
+								{ __( 'Grow your traffic with automated social {{a}}publishing and sharing{{/a}}', {
+									components: {
+										a: (
+											<a
+												href="https://jetpack.com/support/social/"
+												target="_blank"
+												rel="noopener noreferrer"
+											/>
+										),
+									},
+								} ) }
+							</li>
+						</ul>
+					</div>
+				) }
 				<div className="jetpack-termination-dialog__get-help">
 					<p>
 						{ __( 'Have a question? We’d love to help!' ) }{' '}
