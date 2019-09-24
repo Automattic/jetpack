35e602596dace860a49337911fae415f12ed0093
Mikael Korpela <mikael@ihminen.org>
2019-09-24 14:02:24 +0300

    Remove lodash import

--- extensions/shared/is-wpcom-site.js
+++ extensions/shared/is-wpcom-site.js
@@ -1,10 +1,3 @@
-/**
- * External Dependencies
- */
-import { get } from 'lodash';
-
 export default function isWpcomSite() {
-	return (
-		'object' === typeof window ? window._currentSiteType === 'simple' : false
-	);
+	return 'object' === typeof window ? window._currentSiteType === 'simple' : false;
 }
