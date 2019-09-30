259fdd77d6b137e60cce97e166746fe1c532b83d
chris <chriskmnds@gmail.com>
2019-09-30 13:13:16 +0300

    Some cleanup, use global function for site fragment.

--- extensions/shared/plan-upgrade-notification.js
+++ extensions/shared/plan-upgrade-notification.js
@@ -8,6 +8,11 @@ import '@wordpress/notices';
 import { parse as parseUrl } from 'url';
 
 /**
+ * Internal dependencies
+ */
+import { getSiteFragment } from 'extensions/shared/get-site-fragment';
+
+/**
  * Shows a notification when a plan is marked as purchased
  * after redirection from WPCOM.
  */
@@ -16,11 +21,12 @@ if ( undefined !== typeof window && window.location ) {
 	const { query } = parseUrl( window.location.href, true );
 
 	if ( query.plan_upgraded ) {
-		const planUrl = `https://wordpress.com/plans/my-plan/${ window.location.host }`;
+		const planUrl = `https://wordpress.com/plans/my-plan/${ getSiteFragment() }`;
 
-		apiFetch( { path: '/jetpack/v4/site', parse: true } )
+		apiFetch( { path: '/jetpack/v4/site' } )
 			.then( response => {
-				const planName = response.data.plan.product_name;
+				const data = JSON.parse( response.data );
+				const planName = data.plan.product_name;
 
 				dispatch( 'core/notices' ).createNotice(
 					'success',
