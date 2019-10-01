d6824f0546a80a641d6253ceba020083ff8ca134
Michael Arestad <marestad@gmail.com>
2019-10-01 15:51:13 -0600

    Markdown block - added example * props @gravityrail

--- extensions/blocks/markdown/index.js
+++ extensions/blocks/markdown/index.js
@@ -67,4 +67,14 @@ export const settings = {
 	edit,
 
 	save,
+
+	example: {
+		attributes: {
+			source:
+				'## ## Try Markdown\n\n' +
+				'Markdown is a text formatting syntax that is converted into HTML. ' +
+				'You can _emphasize_ text or **make it strong** with just a few characters.\n\n' +
+				'Search up ["Markdown"](https://daringfireball.net/projects/markdown/) to learn more.',
+		},
+	},
 };
