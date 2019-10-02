e3ff9c43fd0591516652722814de0c3cfff1c1b0
bluefuton <chris@bluefuton.com>
2019-10-03 11:24:06 +1300

    Update sort options

--- modules/search/instant-search/lib/sort.js
+++ modules/search/instant-search/lib/sort.js
@@ -5,13 +5,8 @@ import { __ } from '@wordpress/i18n';
 
 export function getSortOptions() {
 	return [
-		{ name: 'date', label: __( 'Date' ) },
-		{ name: 'price', label: __( 'Price' ) },
-		{ name: 'rating', label: __( 'Rating' ) },
-		{ name: 'recency', label: __( 'Recency' ) },
-		{ name: 'score_keyword', label: __( 'Keyword' ) },
-		{ name: 'score_popularity', label: __( 'Popularity' ) },
-		{ name: 'score_relevance', label: __( 'Relevance' ) },
-		{ name: 'score_default', label: __( 'Score' ) },
+		{ name: 'date_asc', label: __( 'Oldest' ) },
+		{ name: 'date_desc', label: __( 'Newest' ) },
+		{ name: 'score_default', label: __( 'Relevance' ) },
 	];
 }
