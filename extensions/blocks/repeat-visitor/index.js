312b0a4efcec716e89f952418c45b4d09709d02a
Michael Arestad <marestad@gmail.com>
2019-10-03 11:45:04 -0600

    Reveat visitor block - add example * props @gravityrail * Inlcudes CSS overrides for preview

--- extensions/blocks/repeat-visitor/index.js
+++ extensions/blocks/repeat-visitor/index.js
@@ -3,6 +3,7 @@
  */
 import { __, _x } from '@wordpress/i18n';
 import { Path } from '@wordpress/components';
+import { InnerBlocks } from '@wordpress/block-editor';
 
 /**
  * Internal dependencies
@@ -43,4 +44,19 @@ export const settings = {
 	title: __( 'Repeat Visitor', 'jetpack' ),
 	edit,
 	save,
+	example: {
+		attributes: {
+			criteria: CRITERIA_AFTER,
+			threshold: DEFAULT_THRESHOLD,
+		},
+		innerBlocks: [
+			{
+				name: 'core/paragraph',
+				attributes: {
+					content:
+						__( 'This block will only appear to a visitor who visited the page three or more times.', 'jetpack' ),
+				},
+			},
+		],
+	},
 };
