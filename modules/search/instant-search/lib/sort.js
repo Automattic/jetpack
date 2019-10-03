bb9ce1822dbf235add9cd89ba169e60c72c484de
bluefuton <chris@bluefuton.com>
2019-10-04 10:14:27 +1300

    Add jetpack context to all translations

--- modules/search/instant-search/lib/sort.js
+++ modules/search/instant-search/lib/sort.js
@@ -11,16 +11,20 @@ import { SORT_DIRECTION_ASC, SORT_DIRECTION_DESC } from './constants';
 export function getSortOptions() {
 	return {
 		date_asc: {
-			label: __( 'Oldest' ),
+			label: __( 'Oldest', 'jetpack' ),
 			field: 'date',
 			direction: SORT_DIRECTION_ASC,
 		},
 		date_desc: {
-			label: __( 'Newest' ),
+			label: __( 'Newest', 'jetpack' ),
 			field: 'date',
 			direction: SORT_DIRECTION_DESC,
 		},
-		score_default: { label: __( 'Relevance' ), field: 'relevance', direction: SORT_DIRECTION_DESC },
+		score_default: {
+			label: __( 'Relevance', 'jetpack' ),
+			field: 'relevance',
+			direction: SORT_DIRECTION_DESC,
+		},
 	};
 }
 
