2e9ac07d7bba352b6fe24fcc1ca524fcae40a932
Michael Arestad <marestad@gmail.com>
2019-10-03 06:05:41 -0600

    Tiled Gallery Block - added example (#13580)
    * Updated Tiled Gallery Block to have a preview
    
    * props @gravityrail
    
    * Tiled Gallery Block - added images
    
    * does not work
    * will resize images before we merge to master
    
    * Add an example image
    
    * Remove unnecessary media IDs
    
    * Make image previews work on .local domains
    
    * Tiled Gallery - preview images now work and are much smaller
    
    
    Co-authored-by: Daniel Walmsley <goldsounds@gmail.com>

--- extensions/blocks/tiled-gallery/index.js
+++ extensions/blocks/tiled-gallery/index.js
@@ -26,6 +26,16 @@ import './editor.scss';
 
 import * as deprecatedV1 from './deprecated/v1';
 
+/**
+ * Example Images
+ */
+import tiledGalleryExample1 from './tiled-gallery_example-1.jpg';
+import tiledGalleryExample2 from './tiled-gallery_example-2.jpg';
+import tiledGalleryExample3 from './tiled-gallery_example-3.jpg';
+import tiledGalleryExample4 from './tiled-gallery_example-4.jpg';
+import tiledGalleryExample5 from './tiled-gallery_example-5.jpg';
+import tiledGalleryExample6 from './tiled-gallery_example-6.jpg';
+
 // Style names are translated. Avoid introducing an i18n dependency elsewhere (view)
 // by only including the labels here, the only place they're needed.
 //
@@ -117,6 +127,56 @@ const blockAttributes = {
 		default: 'none',
 		type: 'string',
 	},
+};
+
+const exampleAttributes = {
+	align: 'center',
+	className: 'is-style-rectangular',
+	images: [
+		{
+			alt: '',
+			link: '',
+			url: tiledGalleryExample1,
+			width: 160,
+			height: 95,
+		},
+		{
+			alt: '',
+			link: '',
+			url: tiledGalleryExample2,
+			width: 160,
+			height: 107,
+		},
+		{
+			alt: '',
+			link: '',
+			url: tiledGalleryExample3,
+			width: 304,
+			height: 203,
+		},
+		{
+			alt: '',
+			link: '',
+			url: tiledGalleryExample4,
+			width: 312,
+			height: 207,
+		},
+		{
+			alt: '',
+			link: '',
+			url: tiledGalleryExample5,
+			width: 152,
+			height: 101,
+		},
+		{
+			alt: '',
+			link: '',
+			url: tiledGalleryExample6,
+			width: 152,
+			height: 105,
+		},
+	],
+	linkTo: 'none',
 };
 
 export const name = 'tiled-gallery';
@@ -212,4 +272,7 @@ export const settings = {
 	edit,
 	save,
 	deprecated: [ deprecatedV1 ],
+	example: {
+		attributes: exampleAttributes,
+	},
 };
