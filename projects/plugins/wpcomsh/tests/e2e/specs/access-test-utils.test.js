50943a9592aa40015092365614e757a048c3450b
Jeff Bowen <jblz@users.noreply.github.com>
2019-10-07 12:18:56 -0400

    use toBeTruthy in env tests

--- projects/plugins/wpcomsh/tests/e2e/specs/access-test-utils.test.js
+++ projects/plugins/wpcomsh/tests/e2e/specs/access-test-utils.test.js
@@ -2,15 +2,15 @@ const envVars = require( './access-test-utils' );
 
 describe( 'Environment', () => {
 	it( 'Should have an auth cookie Name', async () => {
-		expect( !! envVars.AUTH_COOKIE_NAME ).toBe( true );
+		expect( envVars.AUTH_COOKIE_NAME ).toBeTruthy();
 	} );
 	it( 'Should have a user id for a Subscriber user', async () => {
-		expect( !! envVars.SUBSCRIBER_USER_ID ).toBe( true );
+		expect( envVars.SUBSCRIBER_USER_ID ).toBeTruthy();
 	} );
 	it( 'Should have a rest api nonce for a Subscriber user', async () => {
-		expect( !! envVars.SUBSCRIBER_RESTAPI_NONCE ).toBe( true );
+		expect( envVars.SUBSCRIBER_RESTAPI_NONCE ).toBeTruthy();
 	} );
 	it( 'Should have an auth cookie for a Subscriber user', async () => {
-		expect( !! envVars.SUBSCRIBER_AUTH_COOKIE ).toBe( true );
+		expect( envVars.SUBSCRIBER_AUTH_COOKIE ).toBeTruthy();
 	} );
 } );
