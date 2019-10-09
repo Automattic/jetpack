3693d259d6647efd8c5c650ca8b7a6e26ba2163f
Ben Dwyer <ben@scruffian.com>
2019-10-09 14:00:02 +0100

    update event name

--- modules/contact-form/tracks-events.php
+++ modules/contact-form/tracks-events.php
@@ -5,7 +5,7 @@ use Automattic\Jetpack\Tracking;
 function tracks_record_grunion_pre_message_sent( $post_id, $all_values, $extra_values ) {
 	$tracking = new Automattic\Jetpack\Tracking();
 	if ( $extra_values['is_block'] ) {
-		$tracking->tracks_record_event( wp_get_current_user(), 'jetpack_contact_form_gutenberg_block_message_sent', array(
+		$tracking->tracks_record_event( wp_get_current_user(), 'jetpack_contact_form_block_message_sent', array(
 			'entry_permalink' => $all_values['entry_permalink'],
 			'feedback_id' => $all_values['feedback_id'],
 		) );
