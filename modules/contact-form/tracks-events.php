73ebfee89a6a859183feb6d09c457858aa966482
Ben Dwyer <ben@scruffian.com>
2019-10-09 14:04:34 +0100

    escape data sent to tracks

--- modules/contact-form/tracks-events.php
+++ modules/contact-form/tracks-events.php
@@ -6,8 +6,8 @@ function tracks_record_grunion_pre_message_sent( $post_id, $all_values, $extra_v
 	$tracking = new Automattic\Jetpack\Tracking();
 	if ( $extra_values['is_block'] ) {
 		$tracking->tracks_record_event( wp_get_current_user(), 'jetpack_contact_form_block_message_sent', array(
-			'entry_permalink' => $all_values['entry_permalink'],
-			'feedback_id' => $all_values['feedback_id'],
+			'entry_permalink' => esc_url( $all_values['entry_permalink'] ),
+			'feedback_id' => absint( $all_values['feedback_id'] ),
 		) );
 	}
 }
