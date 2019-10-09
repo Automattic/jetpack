4a210fe738a7244cfa694d3452557866dbe3dd23
Ben Dwyer <ben@scruffian.com>
2019-10-09 15:46:34 +0100

    fix undefined index error

--- modules/contact-form/tracks-events.php
+++ modules/contact-form/tracks-events.php
@@ -1,14 +1,33 @@
 <?php
+/**
+ * Jetpack_Contact_Form: Add tracking for submitting contact form blocks
+ *
+ * @package    Jetpack
+ * @since      ?
+ */
 
 use Automattic\Jetpack\Tracking;
 
+/**
+ * Function that sends a `jetpack_contact_form_block_message_sent` event to Tracks
+ *
+ * @param int   $post_id - the post_id for the CPT that is created.
+ * @param array $all_values - fields from the default contact form.
+ * @param array $extra_values - extra fields added to from the contact form.
+
+ * @return void
+ */
 function jetpack_tracks_record_grunion_pre_message_sent( $post_id, $all_values, $extra_values ) {
 	$tracking = new Automattic\Jetpack\Tracking();
-	if ( $extra_values['is_block'] ) {
-		$tracking->tracks_record_event( wp_get_current_user(), 'jetpack_contact_form_block_message_sent', array(
-			'entry_permalink' => esc_url( $all_values['entry_permalink'] ),
-			'feedback_id' => absint( $all_values['feedback_id'] ),
-		) );
+	if ( isset( $extra_values['is_block'] ) && $extra_values['is_block'] ) {
+		$tracking->tracks_record_event(
+			wp_get_current_user(),
+			'jetpack_contact_form_block_message_sent',
+			array(
+				'entry_permalink' => esc_url( $all_values['entry_permalink'] ),
+				'feedback_id'     => absint( $all_values['feedback_id'] ),
+			)
+		);
 	}
 }
 
