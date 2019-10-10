90ec446e8c3afb6e929ef4d79e16c222433d9a53
Brandon Kraft <public@brandonkraft.com>
2019-10-10 10:09:35 -0500

    [not verified] Opt out of tracking in dev mode in legacy branch.

--- packages/tracking/legacy/class.tracks-client.php
+++ packages/tracking/legacy/class.tracks-client.php
@@ -53,7 +53,7 @@ class Jetpack_Tracks_Client {
 	 * @return mixed         True on success, WP_Error on failure
 	 */
 	static function record_event( $event ) {
-		if ( ! Jetpack::jetpack_tos_agreed() || ! empty( $_COOKIE['tk_opt-out'] ) ) {
+		if ( ! Jetpack::jetpack_tos_agreed() || ! empty( $_COOKIE['tk_opt-out'] || Jetpack::is_development_mode() ) ) {
 			return false;
 		}
 
