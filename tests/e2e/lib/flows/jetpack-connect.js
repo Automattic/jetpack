1558dede21b37f364a6c6b481a67b5ff2ad24341
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2019-10-10 15:09:56 +0300

    ......

--- tests/e2e/lib/flows/jetpack-connect.js
+++ tests/e2e/lib/flows/jetpack-connect.js
@@ -100,11 +100,11 @@ export async function connectThroughJetpackStart( {
 	}
 
 	const nextUrl = provisionJetpackStartConnection();
-
+	await page.waitFor( 5000 );
 	await ( await AuthorizePage.visit( page, nextUrl ) ).approve();
 	await ( await PlansPage.init( page ) ).isCurrentPlan( 'business' );
 
-	// await page.waitFor( 30000 );
+	await page.waitFor( 10000 );
 	const siteUrl = getNgrokSiteUrl();
 
 	await ( await WPLoginPage.visit( page, siteUrl + '/wp-login.php' ) ).login();
