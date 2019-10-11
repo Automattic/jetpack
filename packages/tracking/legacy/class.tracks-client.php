8a14c13d207c4b3a9650df6f453f99579ed07835
Jeremy Herve <jeremy@jeremy.hu>
2019-10-11 14:13:19 +0200

    Bring cookie check back
    Just to be sure. See https://github.com/Automattic/jetpack/pull/13698#discussion_r333960203

--- packages/tracking/legacy/class.tracks-client.php
+++ packages/tracking/legacy/class.tracks-client.php
@@ -57,7 +57,7 @@ class Jetpack_Tracks_Client {
 	 * @return mixed         True on success, WP_Error on failure
 	 */
 	public static function record_event( $event ) {
-		if ( ! Jetpack::jetpack_tos_agreed() ) {
+		if ( ! Jetpack::jetpack_tos_agreed() || ! empty( $_COOKIE['tk_opt-out'] ) ) {
 			return false;
 		}
 
