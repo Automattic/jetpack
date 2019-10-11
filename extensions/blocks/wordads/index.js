7e5a4d916c9f44c441fc26166998b1fed9ed83e1
Jeremy Herve <jeremy@jeremy.hu>
2019-10-11 18:36:13 +0200

    Ad Block: add fill color to Ad block icon.
    See https://github.com/Automattic/jetpack/pull/13716#issuecomment-541080558

--- extensions/blocks/wordads/index.js
+++ extensions/blocks/wordads/index.js
@@ -17,7 +17,10 @@ export const title = __( 'Ad', 'jetpack' );
 export const icon = (
 	<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
 		<Path fill="none" d="M0 0h24v24H0V0z" />
-		<Path d="M12,8H4A2,2 0 0,0 2,10V14A2,2 0 0,0 4,16H5V20A1,1 0 0,0 6,21H8A1,1 0 0,0 9,20V16H12L17,20V4L12,8M15,15.6L13,14H4V10H13L15,8.4V15.6M21.5,12C21.5,13.71 20.54,15.26 19,16V8C20.53,8.75 21.5,10.3 21.5,12Z" />
+		<Path
+			fill="currentColor"
+			d="M12,8H4A2,2 0 0,0 2,10V14A2,2 0 0,0 4,16H5V20A1,1 0 0,0 6,21H8A1,1 0 0,0 9,20V16H12L17,20V4L12,8M15,15.6L13,14H4V10H13L15,8.4V15.6M21.5,12C21.5,13.71 20.54,15.26 19,16V8C20.53,8.75 21.5,10.3 21.5,12Z"
+		/>
 	</SVG>
 );
 
