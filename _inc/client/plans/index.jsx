3702495748b1fac98fb05e0fc90cc26f39a909a7
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2019-10-18 15:08:00 -0500

    Started work on displaying the price

--- _inc/client/plans/index.jsx
+++ _inc/client/plans/index.jsx
@@ -7,12 +7,43 @@ import { connect } from 'react-redux';
 /**
  * Internal dependencies
  */
-import { getSiteConnectionStatus } from 'state/connection';
 import { PlanCard, PlanCardHeader, PlanCardBody } from './plan-card';
 import PlanGrid from './plan-grid';
 import QuerySite from 'components/data/query-site';
+import { getAvailablePlans } from 'state/site/reducer';
+
+function PlanPriceDisplay( props ) {
+	const { price } = props;
+	return (
+		<span
+			className="plan-price__yearly"
+			// using dangerouslySetInnerHTML because formatting localized
+			// currencies is best left to our server and it includes the <abbr> element
+			/*eslint-disable react/no-danger*/
+			dangerouslySetInnerHTML={ { __html: price.per.replace( 'per ', '/' ) } }
+		/>
+	); // TODO: don't use this replace
+}
 
 export class Plans extends React.Component {
+	state = {
+		period: 'yearly',
+	};
+
+	renderHeaderContent() {
+		const { sitePlans } = this.props;
+
+		return (
+			<div>
+				<h3>This is the header.</h3>
+				{ // TODO: make sitePlans dynamic
+				sitePlans && (
+					<PlanPriceDisplay price={ sitePlans[ 'daily-backup' ].price[ this.state.period ] } />
+				) }
+			</div>
+		);
+	}
+
 	render() {
 		// TODO: remove
 		// const planType = 'is-backup-daily-plan';
@@ -39,7 +70,7 @@ export class Plans extends React.Component {
 				<div style={ { display: 'flex', justifyContent: 'center', marginBottom: '10px' } }>
 					<PlanCard>
 						{ {
-							header: <PlanCardHeader>{ <h1>This is the header.</h1> }</PlanCardHeader>,
+							header: <PlanCardHeader>{ this.renderHeaderContent() }</PlanCardHeader>,
 							body: <PlanCardBody>{ <p>This is the body.</p> }</PlanCardBody>,
 						} }
 					</PlanCard>
@@ -51,7 +82,51 @@ export class Plans extends React.Component {
 }
 
 export default connect( state => {
+	const fakedSitePlans = getAvailablePlans( state );
+	// TODO: this is faked data; remove before releasing
+	if ( fakedSitePlans ) {
+		fakedSitePlans[ 'daily-backup' ] = {
+			price: {
+				yearly: {
+					html: '<abbr title="United States Dollars">$</abbr>39',
+					text: '$39',
+					amount: 39,
+					symbol: '$',
+					per: '<abbr title="United States Dollars">$</abbr>39 per year',
+				},
+				monthly: {
+					html: '<abbr title="United States Dollars">$</abbr>3.50',
+					text: '$3.50',
+					amount: 3.5,
+					symbol: '$',
+					per: '<abbr title="United States Dollars">$</abbr>3.50 per month',
+				},
+			},
+			features: [],
+		};
+		fakedSitePlans[ 'realtime-backup' ] = {
+			price: {
+				yearly: {
+					html: '<abbr title="United States Dollars">$</abbr>39',
+					text: '$39',
+					amount: 39,
+					symbol: '$',
+					per: '<abbr title="United States Dollars">$</abbr>39 per year',
+				},
+				monthly: {
+					html: '<abbr title="United States Dollars">$</abbr>3.50',
+					text: '$3.50',
+					amount: 3.5,
+					symbol: '$',
+					per: '<abbr title="United States Dollars">$</abbr>3.50 per month',
+				},
+			},
+			features: [],
+		};
+	}
+
 	return {
-		getSiteConnectionStatus: () => getSiteConnectionStatus( state ),
+		// sitePlans: getAvailablePlans( state ),
+		sitePlans: fakedSitePlans,
 	};
 } )( Plans );
--- _inc/client/plans/plan-card.jsx
+++ _inc/client/plans/plan-card.jsx
@@ -2,7 +2,6 @@
  * External dependencies
  */
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 const PlanCard = props => {
@@ -10,6 +9,7 @@ const PlanCard = props => {
 
 	return (
 		<div className="plan-card">
+			{ /* TODO: make the className dynamic */ }
 			<div className={ classNames( 'plan-card__header', 'is-backup-daily-plan' ) }>{ header }</div>
 			<div className="plan-card__body">{ body }</div>
 		</div>
@@ -19,9 +19,10 @@ const PlanCard = props => {
 const PlanCardHeader = props => props.children;
 const PlanCardBody = props => props.children;
 
-PlanCard.PropTypes = {
-	header: PropTypes.objectOf( PlanCardHeader ).isRequired,
-	body: PropTypes.objectOf( PlanCardBody ).isRequired,
-};
+// TODO: change to work with children
+// PlanCard.PropTypes = {
+// 	header: PropTypes.objectOf( PlanCardHeader ).isRequired,
+// 	body: PropTypes.objectOf( PlanCardBody ).isRequired,
+// };
 
 export { PlanCard, PlanCardHeader, PlanCardBody };
