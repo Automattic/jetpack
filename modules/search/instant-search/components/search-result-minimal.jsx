9932d3dc2fdad603c247619c223c6e88f3353928
bluefuton <chris@bluefuton.com>
2019-10-23 15:22:28 +1300

    [not verified] Move Tracks out to common SearchResult

--- modules/search/instant-search/components/search-result-minimal.jsx
+++ modules/search/instant-search/components/search-result-minimal.jsx
@@ -11,31 +11,8 @@ import { h, Component } from 'preact';
 import Gridicon from './gridicon';
 import PostTypeIcon from './post-type-icon';
 import SearchResultComments from './search-result-comments';
-import { recordTrainTracksRender, recordTrainTracksInteract } from '../lib/tracks';
 
 class SearchResultMinimal extends Component {
-	componentDidMount() {
-		recordTrainTracksRender( this.getCommonTrainTracksProps() );
-	}
-
-	getCommonTrainTracksProps() {
-		return {
-			fetch_algo: this.props.result.railcar.fetch_algo,
-			fetch_position: this.props.result.railcar.fetch_position,
-			fetch_query: this.props.result.railcar.fetch_query,
-			railcar: this.props.result.railcar.railcar,
-			rec_blog_id: this.props.result.railcar.rec_blog_id,
-			rec_post_id: this.props.result.railcar.rec_post_id,
-			ui_algo: 'jetpack-instant-search-ui/v1',
-			ui_position: this.props.index,
-		};
-	}
-
-	onClick = () => {
-		// Send out analytics call
-		recordTrainTracksInteract( { ...this.getCommonTrainTracksProps(), action: 'click' } );
-	};
-
 	getIconSize() {
 		return 18;
 	}
