1a43b0f2fb46ffba4cc4d1af8af9636d8bc819f1
Mark George <thingalon@gmail.com>
2019-10-28 19:10:33 +1100

    Document API endpoints as per linter requirements

--- json-endpoints/jetpack/class-jetpack-json-api-delete-backup-helper-script-endpoint.php
+++ json-endpoints/jetpack/class-jetpack-json-api-delete-backup-helper-script-endpoint.php
@@ -1,13 +1,43 @@
 <?php
-
+/**
+ * API endpoint /sites/%s/delete-backup-helper-script
+ * This API endpoint deletes a Jetpack Backup Helper Script
+ */
 class Jetpack_JSON_API_Delete_Backup_Helper_Script_Endpoint extends Jetpack_JSON_API_Endpoint {
-	// POST /sites/%s/install-backup-helper-script
+	/**
+	 * This endpoint is only accessible from Jetpack Backup; it requires no further capabilities.
+	 *
+	 * @var array
+	 */
+	protected $needed_capabilities = array();
 
-	protected $needed_capabilities = array(); // This endpoint is only accessible from Jetpack Backup
-	protected $action              = 'delete';
-	protected $script_path         = null;
-	protected $result              = false;
+	/**
+	 * Method to call when running this endpoint (delete)
+	 *
+	 * @var string
+	 */
+	protected $action = 'delete';
 
+	/**
+	 * Local path to the Helper Script to delete.
+	 *
+	 * @var string|null
+	 */
+	protected $script_path = null;
+
+	/**
+	 * True if the specified file has been successfully deleted.
+	 *
+	 * @var boolean
+	 */
+	protected $result = false;
+
+	/**
+	 * Checks that the input args look like a valid Helper Script path.
+	 *
+	 * @param  null $object  Unused.
+	 * @return bool|WP_Error a WP_Error object or true if the input seems ok.
+	 */
 	protected function validate_input( $object ) {
 		$args = $this->input();
 
@@ -19,11 +49,19 @@ class Jetpack_JSON_API_Delete_Backup_Helper_Script_Endpoint extends Jetpack_JSON
 		return true;
 	}
 
+	/**
+	 * Deletes the specified Helper Script.
+	 */
 	protected function delete() {
 		$this->result = Automattic\Jetpack\Backup\Helper_Script_Manager::delete_helper_script( $this->script_path );
 		Automattic\Jetpack\Backup\Helper_Script_Manager::cleanup_expired_helper_scripts();
 	}
 
+	/**
+	 * Returns the success or failure of the deletion operation
+	 *
+	 * @return array An array containing one key; 'success', which specifies whether the operation was successful.
+	 */
 	protected function result() {
 		return array(
 			'success' => $this->result,
--- json-endpoints/jetpack/class-jetpack-json-api-install-backup-helper-script-endpoint.php
+++ json-endpoints/jetpack/class-jetpack-json-api-install-backup-helper-script-endpoint.php
@@ -1,13 +1,43 @@
 <?php
+/**
+ * API endpoint /sites/%s/install-backup-helper-script
+ * This API endpoint installs a Helper Script to assist Jetpack Backup fetch data
+ */
+class Jetpack_JSON_API_Install_Backup_Helper_Script_Endpoint extends Jetpack_JSON_API_Endpoint { 
+	/**
+	 * This endpoint is only accessible from Jetpack Backup; it requires no further capabilities.
+	 *
+	 * @var array
+	 */
+	protected $needed_capabilities = array();
 
-class Jetpack_JSON_API_Install_Backup_Helper_Script_Endpoint extends Jetpack_JSON_API_Endpoint {
-	// /sites/%s/install-backup-helper-script
+	/**
+	 * Method to call when running this endpoint (install)
+	 *
+	 * @var string
+	 */
+	protected $action = 'install';
 
-	protected $needed_capabilities = array(); // This endpoint is only accessible from Jetpack Backups
-	protected $action              = 'install';
-	protected $helper_script       = null;
-	protected $result              = null;
+	/**
+	 * Contents of the Helper Script to install
+	 *
+	 * @var string|null
+	 */
+	protected $helper_script = null;
 
+	/**
+	 * Contains the result of installing the Helper Script.
+	 *
+	 * @var null|WP_Error|array
+	 */
+	protected $result = null;
+
+	/**
+	 * Checks that the input args look like a valid Helper Script.
+	 *
+	 * @param  null $object  Unused.
+	 * @return bool|WP_Error a WP_Error object or true if the input seems ok.
+	 */
 	protected function validate_input( $object ) {
 		$args = $this->input();
 
@@ -23,11 +53,21 @@ class Jetpack_JSON_API_Install_Backup_Helper_Script_Endpoint extends Jetpack_JSO
 		return true;
 	}
 
+	/**
+	 * Installs the uploaded Helper Script.
+	 */
 	protected function install() {
 		$this->result = Automattic\Jetpack\Backup\Helper_Script_Manager::install_helper_script( $this->helper_script );
 		Automattic\Jetpack\Backup\Helper_Script_Manager::cleanup_expired_helper_scripts();
 	}
 
+	/**
+	 * Returns the result of Helper Script installation. Returns one of:
+	 * - WP_Error on failure, or
+	 * - An array containing the access url ('url') and installation path ('path') on success.
+	 *
+	 * @return array|WP_Error Success or failure information.
+	 */
 	protected function result() {
 		// Include ABSPATH with successful result.
 		if ( ! is_wp_error( $this->result ) ) {
