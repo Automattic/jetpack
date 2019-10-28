4a5a384208312b9550834c46b174ec96481bfe9d
Bernie Reiter <ockham@raz.or.at>
2019-10-28 21:33:17 +0600

    Transpile config files

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,13 @@ const config = {
 			test: './extensions/',
 			presets: [ require.resolve( '@automattic/calypso-build/babel/wordpress-element' ) ],
 		},
+		{
+			// Transpile ES Modules syntax (`import`) in config files (but not elsewhere)
+			test: [ './gulpfile.babel.js', './webpack.config.js', './tools/builder/' ],
+			presets: [
+				[ require.resolve( '@automattic/calypso-build/babel/default' ), { modules: 'commonjs' } ],
+			],
+		},
 	],
 	env: {
 		test: {
