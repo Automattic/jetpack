eef82a5531d1736e5b1e8f7b93ba4227e23abbc1
Michael Arestad <marestad@gmail.com>
2019-10-29 04:30:19 -0600

    Ad block - add preview (#13822)
    * Ad block - add preview
    
    * Added example images - hid controls when unselected
    
    * Optimized and updated images
    
    * Updated images, added default switch case, optimized images

--- extensions/blocks/wordads/index.js
+++ extensions/blocks/wordads/index.js
@@ -52,6 +52,10 @@ export const settings = {
 		},
 	},
 
+	example: {
+		attributes: {},
+	},
+
 	category: 'jetpack',
 
 	keywords: [ __( 'ads', 'jetpack' ), 'WordAds', __( 'Advertisement', 'jetpack' ) ],
