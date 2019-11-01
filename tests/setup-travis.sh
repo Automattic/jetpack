727697c9da1fff8e59dae9f8675fa3699876cd89
Brandon Kraft <public@brandonkraft.com>
2019-11-01 12:41:09 -0500

    Reduce number of times composer auto-installs

--- tests/setup-travis.sh
+++ tests/setup-travis.sh
@@ -9,13 +9,13 @@ phpenv config-rm xdebug.ini
 
 # Configure PHP and PHPUnit environment
 if [[ ${TRAVIS_PHP_VERSION:0:3} == "7.0" ]]; then
-  COMPOSER=tests/composer-travis-prephp71.json  composer install
+  COMPOSER=tests/composer-travis-prephp71.json composer install
 	composer global require "phpunit/phpunit=5.7.*" --no-suggest
 elif [[ ${TRAVIS_PHP_VERSION:0:2} == "7." ]]; then
   composer install
   composer global require "phpunit/phpunit=5.7.*" --no-suggest
 elif [[ ${TRAVIS_PHP_VERSION:0:3} != "5.2" ]]; then
-  COMPOSER=tests/composer-travis-prephp71.json  composer install
+  COMPOSER=tests/composer-travis-prephp71.json composer install
 	composer global require "phpunit/phpunit=4.8.*" --no-suggest
 fi
 
