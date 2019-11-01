ed554927327d2d25dbb592cded42211cccc11509
Enej Bajgoric <enej.bajgoric@gmail.com>
2019-11-01 09:11:45 +0200

    Plans Grid: Add tracking event for see all features button
    Currently we don't have a tracking event for the see all features buttons on the plans grid. This PR adds it.
    
    The tracking even it useful in terms to see how many users click on the see all features.

--- _inc/client/plans/plan-grid.jsx
+++ _inc/client/plans/plan-grid.jsx
@@ -50,6 +50,16 @@ class PlanGrid extends React.Component {
 		};
 	}
 
+	handleFeatureClick( planType ) {
+		return () => {
+			analytics.tracks.recordJetpackClick( {
+				target: 'see-all-features-' + planType,
+				feature: 'plans-grid',
+				extra: 'see-all-features-link',
+			} );
+		};
+	}
+
 	render() {
 		if ( ! this.props.plans ) {
 			return (
@@ -330,7 +340,12 @@ class PlanGrid extends React.Component {
 					key={ 'bottom-' + planType }
 					className="plan-features__table-item is-bottom-buttons has-border-bottom"
 				>
-					<Button href={ this.props.plansLearnMoreUpgradeUrl }>{ plan.strings.see_all }</Button>
+					<Button
+						href={ this.props.plansLearnMoreUpgradeUrl }
+						onClick={ this.handleFeatureClick( planType ) }
+					>
+						{ plan.strings.see_all }
+					</Button>
 				</td>
 			);
 		} );
