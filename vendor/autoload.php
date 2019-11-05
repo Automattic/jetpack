e4c7e510d0ecd8cb98c7c4fb7f078982bc2df269
Jeremy Herve <jeremy@jeremy.hu>
2019-11-05 14:13:46 +0100

    New build

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit1204a8563bd9d9682c9f350c608b21b4::getLoader();
+return ComposerAutoloaderInit6e2d8fcc9a148e8377da3ad63915ef6b::getLoader();
