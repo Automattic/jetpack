d05b8a1e88cd566206b0f68438237d1b92361903
Brandon Kraft <public@brandonkraft.com>
2019-11-06 16:27:01 -0600

    Minor inline documentation

--- _inc/lib/debugger/0-load.php
+++ _inc/lib/debugger/0-load.php
@@ -13,8 +13,9 @@ require_once 'class-jetpack-cxn-tests.php';
 require_once 'class-jetpack-debug-data.php';
 /* The "In-Plugin Debugger" admin page. */
 require_once 'class-jetpack-debugger.php';
-
+/* General Debugging Functions */
 require_once 'debug-functions.php';
+
 add_filter( 'debug_information', array( 'Jetpack_Debug_Data', 'core_debug_data' ) );
 add_filter( 'site_status_tests', 'jetpack_debugger_site_status_tests' );
 add_action( 'wp_ajax_health-check-jetpack-local_testing_suite', 'jetpack_debugger_ajax_local_testing_suite' );
