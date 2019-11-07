d98bb91d03f454b89a5c4cf2213039477b651337
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2019-11-06 22:18:23 -0600

    Removed old css

--- _inc/client/plans/accented-card.jsx
+++ _inc/client/plans/accented-card.jsx
@@ -2,17 +2,13 @@
  * External dependencies
  */
 import React from 'react';
-import classNames from 'classnames';
 
 const AccentedCard = props => {
 	const { header, body } = props.children;
 
 	return (
 		<div className="accented-card">
-			{ /* TODO: make the className dynamic */ }
-			<div className={ classNames( 'accented-card__header', 'is-backup-daily-plan' ) }>
-				{ header }
-			</div>
+			<div className={ 'accented-card__header' }>{ header }</div>
 			<div className="accented-card__body">{ body }</div>
 		</div>
 	);
