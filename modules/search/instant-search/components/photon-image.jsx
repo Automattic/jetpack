3f89903820e7220da57cb0608b2bb2237da34a75
bluefuton <chris@bluefuton.com>
2019-11-11 10:50:07 +1300

    Bump default dimensions to 300x300

--- modules/search/instant-search/components/photon-image.jsx
+++ modules/search/instant-search/components/photon-image.jsx
@@ -6,7 +6,7 @@
 import { h } from 'preact';
 import photon from 'photon';
 
-const PhotonImage = ( { src, maxWidth = 160, maxHeight = 160, alt, ...otherProps } ) => {
+const PhotonImage = ( { src, maxWidth = 300, maxHeight = 300, alt, ...otherProps } ) => {
 	const photonSrc = photon( src, { resize: `${ maxWidth },${ maxHeight }` } );
 
 	return <img src={ photonSrc } alt={ alt } { ...otherProps } />;
