56c9751e1439eb86d7c802ab02b8ed2d6ba77206
david-binda <david.binovec@gmail.com>
2019-11-12 11:39:27 +0100

    Disable the big image threshold in anticipation of WordPress 5.3 roll out
    WordPress 5.3 comes with a new feature capping the max size of uploaded images. This is not needed on WPCOM due to Photon we are using. This does not seem to be needed on Atomic as well.
    
    A possible side-effects of limitting the max size of images are users unable to upload hi-res images to their sites.
    
    This can safely land prior 5.3 rollout.

--- projects/plugins/wpcomsh/wpcomsh.php
+++ projects/plugins/wpcomsh/wpcomsh.php
@@ -1018,3 +1018,9 @@ function wpcomsh_display_disk_space_usage() {
 }
 
 add_action( 'pre-upload-ui', 'wpcomsh_display_disk_space_usage' );
+
+/**
+ * WordPress 5.3 adds "big image" processing, for images over 2560px (by default).
+ * This is not needed on Atomic since we use Photon for dynamic image work.
+ */
+add_filter( 'big_image_size_threshold', '__return_false' );
