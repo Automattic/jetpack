741a3b11b91cbe6c7db1b0522209461f15e24163
Jeremy Herve <jeremy@jeremy.hu>
2019-11-13 16:22:24 +0100

    Create new Subscriptions Toggle Gutenberg extension
    This will allow us to control whether a post should be sent to subs or not

---
+++ extensions/blocks/subscriptions-toggle/index.js
@@ -0,0 +1,8 @@
+/**
+ * Internal dependencies
+ */
+import SubscriptionsCheckbox from './subscriptions-checkbox';
+
+export const name = 'subscriptions-toggle';
+
+export const settings = { render: SubscriptionsCheckbox };
---
+++ extensions/blocks/subscriptions-toggle/subscriptions-checkbox.js
@@ -0,0 +1,45 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { CheckboxControl } from '@wordpress/components';
+import { compose } from '@wordpress/compose';
+import { PostTypeSupportCheck } from '@wordpress/editor';
+import { withDispatch, withSelect } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import JetpackSubscriptionsPanel from '../../shared/jetpack-subscriptions-panel';
+
+const SubscriptionsCheckbox = ( { isPostExcludedFromSubs, editPost } ) => (
+	<PostTypeSupportCheck supportKeys="jetpack-post-subscriptions">
+		<JetpackSubscriptionsPanel>
+			<CheckboxControl
+				label={ __( 'Don’t send this to subscribers', 'jetpack' ) }
+				checked={ isPostExcludedFromSubs }
+				onChange={ value => {
+					editPost( { jetpack_dont_email_post_to_subs: value } );
+				} }
+			/>
+		</JetpackSubscriptionsPanel>
+	</PostTypeSupportCheck>
+);
+
+// Fetch the post meta.
+const applyWithSelect = withSelect( select => {
+	const { getEditedPostAttribute } = select( 'core/editor' );
+	const isPostExcludedFromSubs = getEditedPostAttribute( 'jetpack_dont_email_post_to_subs' );
+
+	return { isPostExcludedFromSubs };
+} );
+
+// Provide method to update post meta.
+const applyWithDispatch = withDispatch( dispatch => {
+	const { editPost } = dispatch( 'core/editor' );
+
+	return { editPost };
+} );
+
+// Combine the higher-order components.
+export default compose( [ applyWithSelect, applyWithDispatch ] )( SubscriptionsCheckbox );
