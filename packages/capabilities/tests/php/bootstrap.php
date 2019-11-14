1eedd184d488cb5647037fa5148bf5292c44cf7c
Daniel Walmsley <goldsounds@gmail.com>
2019-11-14 13:55:09 -0800

    Remove debug statement

--- packages/capabilities/tests/php/bootstrap.php
+++ packages/capabilities/tests/php/bootstrap.php
@@ -8,3 +8,9 @@ require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
 
 // Start up the WP testing environment.
 require getenv( 'WP_PHPUNIT__DIR' ) . '/includes/bootstrap.php';
+
+/*
+ * Using the Speed Trap Listener provided by WordPress Core testing suite to expose
+ * slowest running tests. See the configuration in phpunit.xml.dist
+ */
+require getenv( 'WP_PHPUNIT__DIR' ) . '/includes/listener-loader.php';
