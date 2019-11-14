190a7c1ec85f5a4a33c25c45d26389b4a9baa392
robertf4 <42627630+robertf4@users.noreply.github.com>
2019-11-14 08:55:09 -0600

    REST API: Add products endpoint (#14012)
    * Add products endpoint
    
    * [not verified] Add error code
    
    * Update _inc/lib/class.core-rest-api-endpoints.php
    
    Co-Authored-By: Marin Atanasov <8436925+tyxla@users.noreply.github.com>
    
    * Update _inc/lib/class.core-rest-api-endpoints.php
    
    Co-Authored-By: Marin Atanasov <8436925+tyxla@users.noreply.github.com>
    
    * Add a type attribute that will help us query for just the jetpack products.
    * Update the comment to be a but more clear

--- _inc/lib/class.core-rest-api-endpoints.php
+++ _inc/lib/class.core-rest-api-endpoints.php
@@ -80,14 +80,18 @@ class Jetpack_Core_Json_Api_Endpoints {
 			'methods'             => WP_REST_Server::READABLE,
 			'callback'            => __CLASS__ . '::get_plans',
 			'permission_callback' => __CLASS__ . '::connect_url_permission_callback',
+		) );
 
+		register_rest_route( 'jetpack/v4', 'products', array(
+			'methods'             => WP_REST_Server::READABLE,
+			'callback'            => __CLASS__ . '::get_products',
+			'permission_callback' => __CLASS__ . '::connect_url_permission_callback',
 		) );
 
 		register_rest_route( 'jetpack/v4', 'marketing/survey', array(
 			'methods'             => WP_REST_Server::CREATABLE,
 			'callback'            => __CLASS__ . '::submit_survey',
 			'permission_callback' => __CLASS__ . '::disconnect_site_permission_callback',
-
 		) );
 
 		register_rest_route( 'jetpack/v4', '/jitm', array(
@@ -502,6 +506,41 @@ class Jetpack_Core_Json_Api_Endpoints {
 		}
 
 		return $data;
+	}
+
+	/**
+	 * Gets the WP.com products that are in use on wpcom.
+	 * Similar to the WP.com plans that we currently in user on WPCOM.
+	 *
+	 * @param WP_REST_Request $request The request.
+	 *
+	 * @return string|WP_Error A JSON object of wpcom products if the request was successful, or a WP_Error otherwise.
+	 */
+	public static function get_products( $request ) {
+		$wpcom_request = Client::wpcom_json_api_request_as_user(
+			'/products?_locale=' . get_user_locale() . '&type=jetpack',
+			'v1.1',
+			array(
+				'method'  => 'GET',
+				'headers' => array(
+					'X-Forwarded-For' => Jetpack::current_user_ip( true ),
+				),
+			),
+			null,
+			'rest'
+		);
+
+		$response_code = wp_remote_retrieve_response_code( $wpcom_request );
+		if ( 200 === $response_code ) {
+			return json_decode( wp_remote_retrieve_body( $wpcom_request ) );
+		} else {
+			// Something went wrong so we'll just return the response without caching.
+			return new WP_Error(
+				'failed_to_fetch_data',
+				esc_html__( 'Unable to fetch the requested data.', 'jetpack' ),
+				array( 'status' => $response_code )
+			);
+		}
 	}
 
 	public static function submit_survey( $request ) {
