e03176649847a6ea8358ccea06bbc0be33353e95
Grant Kinney <hi@grant.mk>
2019-11-19 15:28:51 -0600

    Displays select dropdown of events from Eventbrite

--- extensions/blocks/eventbrite/edit.js
+++ extensions/blocks/eventbrite/edit.js
@@ -1,18 +1,49 @@
 /**
  * External dependencies
  */
+import { __ } from '@wordpress/i18n';
 import { Component } from '@wordpress/element';
 import apiFetch from '@wordpress/api-fetch';
+import { SelectControl } from '@wordpress/components';
+
+const defaultEvent = { label: __( 'Select event', 'jetpack' ), value: '' };
 
 class EventbriteEdit extends Component {
-	componentDidMount = () => {
-		apiFetch( {
+	state = {
+		events: [],
+		selectedEventID: null,
+	};
+
+	componentDidMount() {
+		this.getEvents();
+	}
+
+	getEvents = async () => {
+		const response = await apiFetch( {
 			path: '/jetpack/v4/integrations/eventbrite',
 		} );
+
+		const events = response.events.map( event => ( {
+			label: event.post_title,
+			value: event.ID,
+		} ) );
+
+		this.setState( { events } );
 	};
 
 	render() {
-		return 'HELLO WORLD!';
+		const { events, selectedEventID } = this.state;
+
+		return (
+			<SelectControl
+				label={ __( 'Event', 'jetpack' ) }
+				value={ selectedEventID }
+				options={ [ defaultEvent, ...events ] }
+				onChange={ eventID => {
+					this.setState( { selectedEventID: eventID } );
+				} }
+			/>
+		);
 	}
 }
 
