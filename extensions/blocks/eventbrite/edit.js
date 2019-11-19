af596c63bd9f726fc87f24de12744fa4278f185d
Grant Kinney <hi@grant.mk>
2019-11-19 11:26:32 -0600

    Initial scaffolding for Eventbrite block

---
+++ extensions/blocks/eventbrite/edit.js
@@ -0,0 +1,12 @@
+/**
+ * External dependencies
+ */
+import { Component } from '@wordpress/element';
+
+class EventbriteEdit extends Component {
+	render() {
+		return 'HELLO WORLD!';
+	}
+}
+
+export default EventbriteEdit;
