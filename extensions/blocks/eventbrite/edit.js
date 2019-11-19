515534bc677f9bb8f56b44c38b8c36231b6f1cc1
Grant Kinney <hi@grant.mk>
2019-11-19 14:47:24 -0600

    Request Eventbrite events when block is inserted

--- extensions/blocks/eventbrite/edit.js
+++ extensions/blocks/eventbrite/edit.js
@@ -2,8 +2,15 @@
  * External dependencies
  */
 import { Component } from '@wordpress/element';
+import apiFetch from '@wordpress/api-fetch';
 
 class EventbriteEdit extends Component {
+	componentDidMount = () => {
+		apiFetch( {
+			path: '/jetpack/v4/integrations/eventbrite',
+		} );
+	};
+
 	render() {
 		return 'HELLO WORLD!';
 	}
