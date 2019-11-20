3eeeb5fcb42f53a405bf754214f74083a8eec5e2
Gary Pendergast <gary@pento.net>
2019-11-20 06:11:19 +0000

    Render the Eventbrite widget code in PHP, rather than saving it in the block.

--- extensions/blocks/eventbrite/eventbrite.php
+++ extensions/blocks/eventbrite/eventbrite.php
@@ -9,19 +9,38 @@
 
 jetpack_register_block(
 	'jetpack/eventbrite',
-	array( 'render_callback' => 'jetpack_eventbrite_block_load_assets' )
+	array(
+		'attributes'      => array(
+			'eventId' => array(
+				'type' => 'string',
+			),
+		),
+		'render_callback' => 'jetpack_eventbrite_block_load_assets',
+	)
 );
 
 /**
  * Eventbrite block registration/dependency delclaration.
  *
- * @param array  $attr    Eventbrite block attributes.
- * @param string $content Eventbrite block content.
+ * @param array $attr    Eventbrite block attributes.
  *
  * @return string
  */
-function jetpack_eventbrite_block_load_assets( $attr, $content ) {
+function jetpack_eventbrite_block_load_assets( $attr ) {
 	wp_enqueue_script( 'eventbrite-widget', 'https://www.eventbrite.com/static/widgets/eb_widgets.js', array(), JETPACK__VERSION, true );
+	wp_add_inline_script(
+		'eventbrite-widget',
+		"window.EBWidgets.createWidget({
+			widgetType: 'checkout',
+			eventId: ${attr['eventId']},
+			iframeContainerId: 'eventbrite-widget-container-${attr['eventId']}',
+		});"
+	);
 
-	return $content;
+	return <<<EOT
+<div id="eventbrite-widget-container-${attr['eventId']}"></div>
+<noscript>
+	<a href="https://www.eventbrite.com/e/${attr['eventId']}" rel="noopener noreferrer" target="_blank">Buy Tickets on Eventbrite</a>
+</noscript>
+EOT;
 }
