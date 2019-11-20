9bca5ce132b5a6d3641d75745899757d1a4c8f9f
Gary Pendergast <gary@pento.net>
2019-11-20 06:11:19 +0000

    Render the Eventbrite widget code in PHP, rather than saving it in the block.

--- extensions/blocks/eventbrite/index.js
+++ extensions/blocks/eventbrite/index.js
@@ -39,25 +39,5 @@ export const settings = {
 
 	edit,
 
-	save: ( { attributes } ) => {
-		const { eventId } = attributes;
-		const html = `
-			<noscript>
-				<a href="https://www.eventbrite.com/e/${ eventId }" rel="noopener noreferrer" target="_blank">Buy Tickets on Eventbrite</a>
-			</noscript>
-
-			<script src="https://www.eventbrite.com/static/widgets/eb_widgets.js"></script>
-			<script type="text/javascript">
-				window.EBWidgets.createWidget({
-					widgetType: 'checkout',
-					eventId: ${ eventId },
-					modal: true,
-					modalTriggerElementId: { 'eventbrite-widget-modal-trigger-${ eventId }' },
-				});
-			</script>
-		`;
-
-		/* eslint-disable */
-		return <div dangerouslySetInnerHTML={ { __html: html } } />;
-	},
+	save: () => null,
 };
