67d30d831fe1432192ba7c8555561acfd116313c
Gary Pendergast <gary@pento.net>
2019-11-21 05:57:39 +0000

    Add an option for switching between inline and modal embeds

--- extensions/blocks/eventbrite/eventbrite.php
+++ extensions/blocks/eventbrite/eventbrite.php
@@ -11,8 +11,11 @@ jetpack_register_block(
 	'jetpack/eventbrite',
 	array(
 		'attributes'      => array(
-			'eventId' => array(
+			'eventId'  => array(
 				'type' => 'string',
+			),
+			'useModal' => array(
+				'type' => 'boolean',
 			),
 		),
 		'render_callback' => 'jetpack_eventbrite_block_load_assets',
@@ -32,19 +35,40 @@ function jetpack_eventbrite_block_load_assets( $attr ) {
 	}
 
 	wp_enqueue_script( 'eventbrite-widget', 'https://www.eventbrite.com/static/widgets/eb_widgets.js', array(), JETPACK__VERSION, true );
+
+	// Show the embedded version.
+	if ( empty( $attr['useModal'] ) ) {
+		wp_add_inline_script(
+			'eventbrite-widget',
+			"window.EBWidgets.createWidget({
+				widgetType: 'checkout',
+				eventId: ${attr['eventId']},
+				iframeContainerId: 'eventbrite-widget-container-${attr['eventId']}',
+			});"
+		);
+
+		return <<<EOT
+<div id="eventbrite-widget-container-${attr['eventId']}"></div>
+<noscript>
+	<a href="https://www.eventbrite.com/e/${attr['eventId']}" rel="noopener noreferrer" target="_blank">Buy Tickets on Eventbrite</a>
+</noscript>
+EOT;
+	}
+
+	// Show the modal version.
 	wp_add_inline_script(
 		'eventbrite-widget',
 		"window.EBWidgets.createWidget({
 			widgetType: 'checkout',
 			eventId: ${attr['eventId']},
-			iframeContainerId: 'eventbrite-widget-container-${attr['eventId']}',
+			modal: true,
+			modalTriggerElementId: 'eventbrite-widget-modal-trigger-${attr['eventId']}',
 		});"
 	);
 
 	return <<<EOT
-<div id="eventbrite-widget-container-${attr['eventId']}"></div>
-<noscript>
-	<a href="https://www.eventbrite.com/e/${attr['eventId']}" rel="noopener noreferrer" target="_blank">Buy Tickets on Eventbrite</a>
-</noscript>
+	<noscript><a href="https://www.eventbrite.com.au/e/${attr['eventId']}" rel="noopener noreferrer" target="_blank"></noscript>
+	<button id="eventbrite-widget-modal-trigger-${attr['eventId']}" type="button">Buy Tickets</button>
+	<noscript></a>Buy Tickets on Eventbrite</noscript>
 EOT;
 }
