2007e66e6ca21802a58ce6649f6cfb047f84d8d0
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2019-11-22 14:33:31 -0600

    [not verified] Copy ProductCard from Calypso

---
+++ _inc/client/components/product-card/index.jsx
@@ -0,0 +1,114 @@
+/**
+ * External dependencies
+ */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import Card from 'components/card';
+import Gridicon from 'components/gridicon';
+// TODO:
+// import { managePurchase } from 'me/purchases/paths';
+const managePurchase = () => {};
+import ProductCardAction from './action';
+import ProductCardPriceGroup from './price-group';
+// import { recordTracksEvent } from 'state/analytics/actions';
+const recordTracksEvent = () => {};
+
+/**
+ * Style dependencies
+ */
+import './style.scss';
+
+class ProductCard extends Component {
+	static propTypes = {
+		billingTimeFrame: PropTypes.string,
+		currencyCode: PropTypes.string,
+		description: PropTypes.oneOfType( [ PropTypes.string, PropTypes.element, PropTypes.node ] ),
+		discountedPrice: PropTypes.oneOfType( [
+			PropTypes.number,
+			PropTypes.arrayOf( PropTypes.number ),
+		] ),
+		fullPrice: PropTypes.oneOfType( [ PropTypes.number, PropTypes.arrayOf( PropTypes.number ) ] ),
+		isCurrent: PropTypes.bool,
+		isPlaceholder: PropTypes.bool,
+		purchase: PropTypes.object,
+		subtitle: PropTypes.oneOfType( [ PropTypes.string, PropTypes.element, PropTypes.node ] ),
+		title: PropTypes.oneOfType( [ PropTypes.string, PropTypes.element ] ),
+	};
+
+	handleManagePurchase( productSlug ) {
+		return () => {
+			this.props.recordTracksEvent( 'calypso_manage_purchase_click', {
+				slug: productSlug,
+			} );
+		};
+	}
+
+	render() {
+		const {
+			billingTimeFrame,
+			children,
+			currencyCode,
+			description,
+			discountedPrice,
+			fullPrice,
+			isCurrent,
+			isPlaceholder,
+			purchase,
+			subtitle,
+			title,
+			translate,
+		} = this.props;
+		const cardClassNames = classNames( 'product-card', {
+			'is-placeholder': isPlaceholder,
+			'is-purchased': !! purchase,
+		} );
+
+		return (
+			<Card className={ cardClassNames }>
+				<div className="product-card__header">
+					{ title && (
+						<div className="product-card__header-primary">
+							{ purchase && <Gridicon icon="checkmark" size={ 18 } /> }
+							<h3 className="product-card__title">{ title }</h3>
+						</div>
+					) }
+					<div className="product-card__header-secondary">
+						{ subtitle && <div className="product-card__subtitle">{ subtitle }</div> }
+						{ ! purchase && (
+							<ProductCardPriceGroup
+								billingTimeFrame={ billingTimeFrame }
+								currencyCode={ currencyCode }
+								discountedPrice={ discountedPrice }
+								fullPrice={ fullPrice }
+							/>
+						) }
+					</div>
+				</div>
+				<div className="product-card__description">
+					{ description && <p>{ description }</p> }
+					{ purchase && isCurrent && (
+						<ProductCardAction
+							onClick={ this.handleManagePurchase( purchase.productSlug ) }
+							href={ managePurchase( purchase.domain, purchase.id ) }
+							label={ translate( 'Manage Subscription' ) }
+							primary={ false }
+						/>
+					) }
+				</div>
+				{ children }
+			</Card>
+		);
+	}
+}
+
+export default connect(
+	null,
+	{ recordTracksEvent }
+)( localize( ProductCard ) );
