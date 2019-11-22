d56a04c8b734f6bc59e51b11339ae36401aa9bfe
Jeremy Herve <jeremy@jeremy.hu>
2019-11-22 21:10:18 +0100

    Feature Hints: disable when plugins cannot be installed on site.
    When one cannot install plugins, we do not have to do anything with feature hints.

--- modules/plugin-search.php
+++ modules/plugin-search.php
@@ -1,5 +1,6 @@
 <?php
 
+use Automattic\Jetpack\Constants;
 use Automattic\Jetpack\Tracking;
 
 /**
@@ -15,6 +16,7 @@ if (
 	Jetpack::is_active() &&
 	/** This filter is documented in _inc/lib/admin-pages/class.jetpack-react-page.php */
 	apply_filters( 'jetpack_show_promotions', true ) &&
+	! Constants::is_true( 'DISALLOW_FILE_MODS' ) &&
 	jetpack_is_psh_active()
 ) {
 	Jetpack_Plugin_Search::init();
