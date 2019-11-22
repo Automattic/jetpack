60f228c1ed54d1a233f436fd6197d308e0eb1c42
Piotr Delawski <piotr.delawski@gmail.com>
2019-11-22 14:18:17 +0100

    Fix rebase issue

--- _inc/client/plans/index.jsx
+++ _inc/client/plans/index.jsx
@@ -13,8 +13,8 @@ import { SingleProductBackup } from './single-product-backup';
 import QueryProducts from 'components/data/query-products';
 import QuerySite from 'components/data/query-site';
 import { getUpgradeUrl } from 'state/initial-state';
-import { getSitePlan, isFetchingSiteData, isFetchingProducts } from 'state/site';
-import { getProducts } from 'state/products';
+import { getSitePlan, isFetchingSiteData } from 'state/site';
+import { getProducts, isFetchingProducts } from 'state/products';
 
 export class Plans extends React.Component {
 	render() {
