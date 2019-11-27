908c2153c42f474ac0097b0f3ba45806519403f8
Jason Moon <4044428+jsnmoon@users.noreply.github.com>
2019-11-27 16:23:42 -0700

    Instant Search: Add error/offline handling for API (#14125)

--- modules/search/instant-search/lib/constants.js
+++ modules/search/instant-search/lib/constants.js
@@ -3,4 +3,4 @@ export const SORT_DIRECTION_ASC = 'ASC';
 export const SORT_DIRECTION_DESC = 'DESC';
 export const RESULT_FORMAT_MINIMAL = 'minimal';
 export const RESULT_FORMAT_PRODUCT = 'product';
-export const FIVE_MINUTES_IN_MILLISECONDS = 5 * 60 * 1000;
+export const MINUTE_IN_MILLISECONDS = 60 * 1000;
