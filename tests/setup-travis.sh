013a645ac83068814482784967a7d8df19337932
Brandon Kraft <public@brandonkraft.com>
2019-12-05 15:35:39 -0600

    Try 5.7

--- tests/setup-travis.sh
+++ tests/setup-travis.sh
@@ -10,14 +10,14 @@ phpenv config-rm xdebug.ini
 # Configure PHP and PHPUnit environment
 if [[ ${TRAVIS_PHP_VERSION:0:2} == "8." ]]; then
 	composer install
-	composer global require "phpunit/phpunit=7.5.*"
+	composer global require "phpunit/phpunit=5.7.*"
 elif [[ ${TRAVIS_PHP_VERSION:0:3} == "7.0" ]]; then
   composer remove sirbrillig/phpcs-changed --dev
   composer install
-	composer global require "phpunit/phpunit=6.5.*" --no-suggest
+	composer global require "phpunit/phpunit=5.7.*" --no-suggest
 elif [[ ${TRAVIS_PHP_VERSION:0:2} == "7." ]]; then
   composer install
-  composer global require "phpunit/phpunit=7.5.*" --no-suggest
+  composer global require "phpunit/phpunit=5.7.*" --no-suggest
 elif [[ ${TRAVIS_PHP_VERSION:0:3} != "5.2" ]]; then
   composer remove sirbrillig/phpcs-changed --dev
   composer install
