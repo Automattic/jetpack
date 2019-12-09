84727755f71988299f54bf4e06f3dcc99d6171ab
Echo G <ChaosExAnima@users.noreply.github.com>
2019-12-09 12:48:31 -0500

    Updates to wording and props based on CR

--- _inc/client/components/product-expiration/index.jsx
+++ _inc/client/components/product-expiration/index.jsx
@@ -7,51 +7,50 @@ import { moment, translate as __ } from 'i18n-calypso';
 
 class ProductExpiration extends React.PureComponent {
 	static propTypes = {
-		expiryDate: PropTypes.string.isRequired,
+		expiryDate: PropTypes.string,
 		purchaseDate: PropTypes.string,
 		isRefundable: PropTypes.bool,
 		dateFormat: PropTypes.string,
-		fallback: PropTypes.string,
 	};
 
 	static defaultProps = {
+		expiryDate: '',
 		purchaseDate: '',
 		isRefundable: false,
 		dateFormat: 'LL',
-		fallback: '',
 	};
 
 	render() {
-		const { expiryDate, purchaseDate, isRefundable, dateFormat, fallback } = this.props;
+		const { expiryDate, purchaseDate, isRefundable, dateFormat } = this.props;
 
-		// Return nothing if we don't have any dates.
+		// Return null if we don't have any dates.
 		if ( ! expiryDate && ! purchaseDate ) {
-			return fallback;
+			return null;
 		}
 
 		// Return the subscription date if we don't have the expiry date or the plan is refundable.
 		if ( ! expiryDate || isRefundable ) {
-			const purchase = moment( purchaseDate );
-			if ( purchase.isValid() ) {
-				return __( 'Purchased on %s.', { args: purchase.format( dateFormat ) } );
+			const purchaseMoment = moment( purchaseDate );
+			if ( purchaseMoment.isValid() ) {
+				return __( 'Purchased on %s.', { args: purchaseMoment.format( dateFormat ) } );
 			}
-			return fallback;
+			return null;
 		}
 
-		const expiry = moment( expiryDate );
+		const expiryMoment = moment( expiryDate );
 
-		// Return fallback if date is not parsable.
-		if ( ! expiry.isValid() ) {
-			return fallback;
+		// Return null if date is not parsable.
+		if ( ! expiryMoment.isValid() ) {
+			return null;
 		}
 
 		// If the expiry date is in the past, show the expiration date.
-		if ( expiry.diff( new Date() ) < 0 ) {
-			return __( 'Expired on %s.', { args: expiry.format( dateFormat ) } );
+		if ( expiryMoment.diff( new Date() ) < 0 ) {
+			return __( 'Expired on %s.', { args: expiryMoment.format( dateFormat ) } );
 		}
 
 		// Lastly, return the renewal date.
-		return __( 'Renews on %s.', { args: expiry.format( dateFormat ) } );
+		return __( 'Renews on %s.', { args: expiryMoment.format( dateFormat ) } );
 	}
 }
 
