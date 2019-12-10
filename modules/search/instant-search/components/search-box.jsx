895626c449d340d333e7d78f4636e49412eab9fd
Chris R <chris.rosser@automattic.com>
2019-12-10 14:10:22 -0700

    Instant Search: add filter icon toggle (#14176)
    * Add filter icon
    
    * Simple show/hide toggle
    
    * Make it an interactive element
    
    * Add aria-hidden to gridicon

--- modules/search/instant-search/components/search-box.jsx
+++ modules/search/instant-search/components/search-box.jsx
@@ -9,6 +9,11 @@ import { __ } from '@wordpress/i18n';
 // eslint-disable-next-line lodash/import-scope
 import uniqueId from 'lodash/uniqueId';
 
+/**
+ * Internal dependencies
+ */
+import Gridicon from './gridicon';
+
 function ignoreEnterKey( event ) {
 	if ( event.key === 'Enter' ) {
 		// Prevent form submission
@@ -18,6 +23,7 @@ function ignoreEnterKey( event ) {
 
 const SearchBox = props => {
 	const [ inputId ] = useState( () => uniqueId( 'jetpack-instant-search__box-input-' ) );
+
 	return (
 		<div className="jetpack-instant-search__box">
 			{ /* TODO: Add support for preserving label text */ }
@@ -36,6 +42,21 @@ const SearchBox = props => {
 				type="search"
 				value={ props.query }
 			/>
+			{ ! props.widget && (
+				/* Using role='button' rather than button element so we retain control over styling */
+				<div
+					role="button"
+					onClick={ props.toggleFilters }
+					onKeyDown={ props.toggleFilters }
+					tabIndex="0"
+					className="jetpack-instant-search__box-filter-icon"
+				>
+					<Gridicon icon="filter" alt="Search filter icon" aria-hidden="true" />
+					<span class="screen-reader-text">
+						{ props.showFilters ? __( 'Hide filters' ) : __( 'Show filters ' ) }
+					</span>
+				</div>
+			) }
 			<button className="screen-reader-text">{ __( 'Search', 'jetpack' ) }</button>
 		</div>
 	);
