e9f027646b5ca710044dafe8a28bd810c0ff10ee
Echo G <ChaosExAnima@users.noreply.github.com>
2019-12-10 10:41:34 -0500

    Reverts changes in previous commit

--- _inc/client/plans/index.jsx
+++ _inc/client/plans/index.jsx
@@ -6,59 +6,19 @@ import React, { Fragment } from 'react';
 /**
  * Internal dependencies
  */
-import analytics from 'lib/analytics';
-import { translate as __ } from 'i18n-calypso';
 import QueryProducts from 'components/data/query-products';
 import QuerySite from 'components/data/query-site';
 import PlanGrid from './plan-grid';
 import ProductSelector from './product-selector';
-import PlanSwitcher from './plan-switcher';
 
 export class Plans extends React.Component {
-	state = {
-		period: 'yearly',
-	};
-
-	periods = {
-		monthly: __( 'Monthly Billing' ),
-		yearly: __( 'Yearly Billing' ),
-	};
-
-	constructor( ...args ) {
-		super( ...args );
-		this.handlePeriodChange = this.handlePeriodChange.bind( this );
-	}
-
-	handlePeriodChange( newPeriod ) {
-		if ( newPeriod === this.state.period || ! ( newPeriod in this.periods ) ) {
-			return null;
-		}
-
-		return () => {
-			analytics.tracks.recordJetpackClick( {
-				target: 'change-period-' + newPeriod,
-				feature: 'plans-grid',
-			} );
-
-			this.setState( {
-				period: newPeriod,
-			} );
-		};
-	}
-
 	render() {
-		const { period } = this.state;
 		return (
 			<Fragment>
 				<QueryProducts />
 				<QuerySite />
-				<PlanSwitcher
-					period={ period }
-					periods={ this.periods }
-					setPeriod={ this.handlePeriodChange }
-				/>
-				<ProductSelector period={ period } />
-				<PlanGrid period={ period } />
+				<ProductSelector />
+				<PlanGrid />
 			</Fragment>
 		);
 	}
