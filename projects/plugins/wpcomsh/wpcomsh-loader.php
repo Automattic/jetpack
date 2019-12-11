b773c588053e8fb26be5e6877a5f174956f79f3c
Brandon Payton <brandon@happycode.net>
2019-12-11 13:09:42 -0700

    Stop loading wpcomsh based on IS_PRESSABLE constant https://github.com/Automattic/wpcomsh/pull/367
    All sites using wpcomsh are now on Atomic v2 infrastructure, so we no longer need an IS_PRESSABLE constant.

--- projects/plugins/wpcomsh/wpcomsh-loader.php
+++ projects/plugins/wpcomsh/wpcomsh-loader.php
@@ -4,8 +4,7 @@ Plugin Name: WP.com Site Helper
 Description: WordPress.com provided functionality & tools
 */
 
-if ( ( defined( 'IS_PRESSABLE' ) && IS_PRESSABLE ) ||
-     ( defined( 'IS_ATOMIC' ) && IS_ATOMIC ) ) {
+if ( defined( 'IS_ATOMIC' ) && IS_ATOMIC ) {
   require_once( WPMU_PLUGIN_DIR . '/wpcomsh/wpcomsh.php' );
 
   // load and init the plan manager
