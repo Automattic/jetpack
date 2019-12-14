ae4d6b8c4c15883e995207e5b111d3bab1dbe860
Dat A8C19 <htdat@users.noreply.github.com>
2019-12-15 00:07:52 +0700

    Add the social_notifications_subscribe option to wp-admin/options-discussion.php

--- _inc/lib/class.core-rest-api-endpoints.php
+++ _inc/lib/class.core-rest-api-endpoints.php
@@ -2056,6 +2056,13 @@ class Jetpack_Core_Json_Api_Endpoints {
 				'validate_callback' => __CLASS__ . '::validate_boolean',
 				'jp_group'          => 'subscriptions',
 			),
+			'social_notifications_subscribe' => array(
+				'description'       => esc_html__( 'Send email notification when someone follows my blog', 'jetpack' ),
+				'type'              => 'boolean',
+				'default'           => 0,
+				'validate_callback' => __CLASS__ . '::validate_boolean',
+				'jp_group'          => 'subscriptions',
+			),
 
 			// Related Posts
 			'show_headline' => array(
