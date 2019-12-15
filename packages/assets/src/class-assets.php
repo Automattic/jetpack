e5a25ce07fcb8ac124d70e3be919c8c6cbd60c91
Brandon Kraft <public@brandonkraft.com>
2019-12-14 21:54:43 -0600

    Adds filter for the get_file_for_environment function

--- packages/assets/src/class-assets.php
+++ packages/assets/src/class-assets.php
@@ -37,6 +37,18 @@ class Assets {
 		$path = ( Jetpack_Constants::is_defined( 'SCRIPT_DEBUG' ) && Jetpack_Constants::get_constant( 'SCRIPT_DEBUG' ) )
 			? $non_min_path
 			: $min_path;
-		return plugins_url( $path, Jetpack_Constants::get_constant( 'JETPACK__PLUGIN_FILE' ) );
+
+		$url = plugins_url( $path, Jetpack_Constants::get_constant( 'JETPACK__PLUGIN_FILE' ) );
+
+		/**
+		 * Filters the URL for a file passed through the get_file_url_for_environment function.
+		 *
+		 * @since 8.1.0
+		 *
+		 * @parm string $url The URL to the file.
+		 * @param string $min_path The minified path.
+		 * @param string $non_min_path The non-minified path.
+		 */
+		return \apply_filters( 'jetpack_get_file_for_environment', $url, $min_path, $non_min_path );
 	}
 }
