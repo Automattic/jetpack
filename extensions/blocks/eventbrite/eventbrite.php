51639b2fb045faef34e118dec47ec9f8b56a242e
Gary <gary@pento.net>
2019-12-16 13:54:40 +1100

    Use @wordpress/embed-block to generate the block.

--- extensions/blocks/eventbrite/eventbrite.php
+++ extensions/blocks/eventbrite/eventbrite.php
@@ -7,22 +7,20 @@
  * @package Jetpack
  */
 
-if ( ( defined( 'IS_WPCOM' ) && IS_WPCOM ) || Jetpack::is_active() ) {
-	jetpack_register_block(
-		'jetpack/eventbrite',
-		array(
-			'attributes'      => array(
-				'eventId'  => array(
-					'type' => 'string',
-				),
-				'useModal' => array(
-					'type' => 'boolean',
-				),
+jetpack_register_block(
+	'jetpack/eventbrite',
+	array(
+		'attributes'      => array(
+			'eventId'  => array(
+				'type' => 'string',
 			),
-			'render_callback' => 'jetpack_eventbrite_block_load_assets',
-		)
-	);
-}
+			'useModal' => array(
+				'type' => 'boolean',
+			),
+		),
+		'render_callback' => 'jetpack_eventbrite_block_load_assets',
+	)
+);
 
 /**
  * Eventbrite block registration/dependency delclaration.
@@ -73,4 +71,77 @@ EOT;
 	<button id="eventbrite-widget-modal-trigger-${attr['eventId']}" type="button">Buy Tickets</button>
 	<noscript></a>Buy Tickets on Eventbrite</noscript>
 EOT;
+}
+
+
+/**
+ * Return fake embed data.
+ *
+ * @param array $matches Foo.
+ * @return string
+ */
+function jetpack_eventbrite_embed_handler( $matches ) {
+	if ( is_ssl() ) {
+		// phpcs:disable WordPress.WP.EnqueuedResources
+		return <<<SSLEMBED
+			<script src="https://www.eventbrite.com/static/widgets/eb_widgets.js"></script>
+			<script>
+			window.EBWidgets.createWidget({
+					widgetType: 'checkout',
+					eventId: ${matches['eventId']},
+					iframeContainerId: 'eventbrite-widget-container-${matches['eventId']}',
+				});
+			</script>
+			<div id="eventbrite-widget-container-${matches['eventId']}"></div>
+SSLEMBED;
+		// phpcs:enable
+	}
+
+	return <<<SSLINFO
+		SSL REQUIRED, Y'ALL
+SSLINFO;
+}
+wp_embed_register_handler( 'jetpack-eventbrite', '/^https?:\/\/www\.eventbrite\.com(.[a-z]+)*\/e\/.*?(?P<eventId>\d+)\/?$/', 'jetpack_eventbrite_embed_handler' );
+
+/**
+ * Handle a failing oEmbed proxy request to try embedding as a shortcode.
+ *
+ * @see https://core.trac.wordpress.org/ticket/45447
+ *
+ * @param  WP_HTTP_Response|WP_Error $response The REST Request response.
+ * @param  WP_REST_Server            $handler  ResponseHandler instance (usually WP_REST_Server).
+ * @param  WP_REST_Request           $request  Request used to generate the response.
+ * @return WP_HTTP_Response|object|WP_Error    The REST Request response.
+ */
+function jetpack_filter_oembed_result( $response, $handler, $request ) {
+	if ( ! is_wp_error( $response ) || 'oembed_invalid_url' !== $response->get_error_code() ||
+			'/oembed/1.0/proxy' !== $request->get_route() ) {
+		return $response;
+	}
+
+	// Try using a classic embed instead.
+	global $wp_embed;
+	// phpcs:ignore WordPress.Security.NonceVerification
+	$html = $wp_embed->shortcode( array(), $_GET['url'] );
+	if ( ! $html ) {
+		return $response;
+	}
+
+	global $wp_scripts;
+
+	// Check if any scripts were enqueued by the shortcode, and include them in
+	// the response.
+	$enqueued_scripts = array();
+	foreach ( $wp_scripts->queue as $script ) {
+		$enqueued_scripts[] = $wp_scripts->registered[ $script ]->src;
+	}
+
+	return array(
+		'provider_name' => __( 'Embed Handler', 'jetpack' ),
+		'html'          => $html,
+		'scripts'       => $enqueued_scripts,
+	);
+}
+if ( ! function_exists( 'gutenberg_filter_oembed_result' ) ) {
+	add_filter( 'rest_request_after_callbacks', 'jetpack_filter_oembed_result', 10, 3 );
 }
