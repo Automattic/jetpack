0c57e0ae8fc066c00f21d29d2a077cca1a4312a5
bluefuton <chris@bluefuton.com>
2019-12-17 14:26:18 +1300

    Tidying

--- modules/search/instant-search/components/test/notice.test.js
+++ modules/search/instant-search/components/test/notice.test.js
@@ -6,14 +6,7 @@ import '@testing-library/jest-dom/extend-expect';
 import { h } from 'preact';
 import { render } from '@testing-library/preact';
 
-//import PostTypeIcon from '../post-type-icon';
 import Notice from '../notice';
-
-// test( 'returns null for an unknown post type', () => {
-//   const { queryByText } = render( <PostTypeIcon postType="banana" /> );
-
-//   expect( queryByText( 'gridicon' ) ).toBeNull();
-// } );
 
 test( 'returns a notice if the type is warning', () => {
 	const { asFragment, debug } = render( <Notice type="warning" /> );
