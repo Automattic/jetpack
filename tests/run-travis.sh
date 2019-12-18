6c6fb8d23cdecd660f976db8754e010a691dc510
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2019-12-18 20:28:36 +0200

    ...

--- tests/run-travis.sh
+++ tests/run-travis.sh
@@ -76,8 +76,7 @@ if [ "$WP_TRAVISCI" == "phpunit" ]; then
 	elif [[ "$TRAVIS_EVENT_TYPE" == "api" && ! -z $PHPUNIT_COMMAND_OVERRIDE ]]; then
 		export WP_TRAVISCI="${PHPUNIT_COMMAND_OVERRIDE}"
 	elif [[ "$DO_COVERAGE" == "true" && -x "$(command -v phpdbg)" ]]; then
-		mkdir -p $TRAVIS_BUILD_DIR/build/logs
-		export WP_TRAVISCI="phpdbg -qrr /home/travis/.phpenv/shims/phpunit --coverage-clover $TRAVIS_BUILD_DIR/clover.xml"
+		export WP_TRAVISCI="phpdbg -qrr $HOME/.composer/vendor/bin/phpunit --coverage-clover $TRAVIS_BUILD_DIR/clover.xml"
 	fi
 
   if [ "$SYNC_BETA" == "1" ]; then
