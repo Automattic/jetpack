ce465d5d880b79799f454035ee0f4a13cd713584
bluefuton <chris@bluefuton.com>
2019-12-19 12:06:14 +1300

    Enable test and added dependency docblocks

--- modules/search/instant-search/components/test/notice.test.js
+++ modules/search/instant-search/components/test/notice.test.js
@@ -2,11 +2,17 @@
  * @jsx h
  * @jest-environment jsdom
  */
-import '@testing-library/jest-dom/extend-expect';
 
+/**
+ * External dependencies
+ */
 import { h } from 'preact';
 import { render } from '@testing-library/preact';
+import '@testing-library/jest-dom/extend-expect';
 
+/**
+ * Internal dependencies
+ */
 import Notice from '../notice';
 
 test( 'returns a notice if the type is warning', () => {
