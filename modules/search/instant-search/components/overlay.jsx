2220818e97000fa10ade0dc92afe9efef9eaeea0
bluefuton <chris@bluefuton.com>
2019-12-19 10:57:33 +1300

    Close overlay with Esc key

--- modules/search/instant-search/components/overlay.jsx
+++ modules/search/instant-search/components/overlay.jsx
@@ -3,23 +3,41 @@
 /**
  * External dependencies
  */
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import { __ } from '@wordpress/i18n';
 
-const Overlay = ( { showOverlay, toggleOverlay, children } ) => {
-	const classNames = [ 'jetpack-instant-search__overlay' ];
-	if ( ! showOverlay ) {
-		classNames.push( 'is-hidden' );
+class Overlay extends Component {
+	closeOnEscapeKey = event => {
+		if ( event.key === 'Escape' ) {
+			this.props.toggleOverlay();
+		}
+	};
+
+	componentDidMount() {
+		window.addEventListener( 'keydown', this.closeOnEscapeKey );
 	}
 
-	return (
-		<div className={ classNames.join( ' ' ) }>
-			<button className="jetpack-instant-search__overlay-close" onClick={ toggleOverlay }>
-				{ __( 'Close', 'jetpack' ) }
-			</button>
-			{ children }
-		</div>
-	);
-};
+	componentWillUnmount() {
+		window.removeEventListener( 'keydown', this.closeOnEscapeKey );
+	}
+
+	render() {
+		const { showOverlay, toggleOverlay, children } = this.props;
+
+		const classNames = [ 'jetpack-instant-search__overlay' ];
+		if ( ! showOverlay ) {
+			classNames.push( 'is-hidden' );
+		}
+
+		return (
+			<div className={ classNames.join( ' ' ) }>
+				<button className="jetpack-instant-search__overlay-close" onClick={ toggleOverlay }>
+					{ __( 'Close', 'jetpack' ) }
+				</button>
+				{ children }
+			</div>
+		);
+	}
+}
 
 export default Overlay;
