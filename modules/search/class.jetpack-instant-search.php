d6a69ca3025e475c1b5b75614bd6b47388785230
Greg Ichneumon Brown <greg@automattic.com>
2019-12-23 15:10:03 +0000

    Fix up  code bugs. Prep for rebasing this branch
    [not verified]

--- modules/search/class.jetpack-instant-search.php
+++ modules/search/class.jetpack-instant-search.php
@@ -24,6 +24,7 @@ class Jetpack_Instant_Search extends Jetpack_Search {
 	 * @since 8.3.0
 	 */
 	public function load_php() {
+		require_once dirname( __FILE__ ) . '/class.jetpack-search-template-tags.php';
 		require_once JETPACK__PLUGIN_DIR . 'modules/widgets/search.php';
 	}
 
@@ -36,6 +37,7 @@ class Jetpack_Instant_Search extends Jetpack_Search {
 	public function init_hooks() {
 		if ( ! is_admin() ) {
 			add_filter( 'posts_pre_query', array( $this, 'filter__posts_pre_query' ), 10, 2 );
+			add_action( 'parse_query', array( $this, 'action__parse_query' ), 10, 1 );
 
 			add_action( 'init', array( $this, 'set_filters_from_widgets' ) );
 
@@ -150,31 +152,183 @@ class Jetpack_Instant_Search extends Jetpack_Search {
 	 * @return array Array of matching posts.
 	 */
 	public function filter__posts_pre_query( $posts, $query ) {
-		//TODO: run search aggs for the filtering
-
 		if ( ! $this->should_handle_query( $query ) ) {
 			// Intentionally not adding the 'jetpack_search_abort' action since this should fire for every request except for search.
 			return $posts;
 		}
 
-		//TODO: pre-render search results
+		/**
+		 * Bypass the main query and return dummy data
+		 *  WP Core doesn't call the set_found_posts and its filters when filtering
+		 *  posts_pre_query like we do, so need to do these manually.
+		 */
+		$query->found_posts   = 1;
+		$query->max_num_pages = 1;
 
 		return array(
-			new WP_Post( array(
-				'ID' => 0;
-				'post_author'     => 1,
-				'post_date'       => current_time( 'mysql' ),
-				'post_date_gmt'   => current_time( 'mysql', 1 ),
-				'post_title'      => 'Some title or other',
-				'post_content'    => 'Whatever you want here. Maybe some cat pictures....',
-				'post_status'     => 'publish',
-				'comment_status'  => 'closed',
-				'ping_status'     => 'closed',
-				'post_name'       => 'fake-page-' . rand( 1, 99999 ),
-				'post_type'       => 'page',
-				'filter'          => 'raw',
-			) );
+			new WP_Post(
+				array(
+					'ID'             => 1,
+					'post_author'    => 1,
+					'post_date'      => current_time( 'mysql' ),
+					'post_date_gmt'  => current_time( 'mysql', 1 ),
+					'post_title'     => 'Some title or other',
+					'post_content'   => 'Whatever you want here. Maybe some cat pictures....',
+					'post_status'    => 'publish',
+					'comment_status' => 'closed',
+					'ping_status'    => 'closed',
+					'post_name'      => 'fake-page',
+					'post_type'      => 'page',
+					'filter'         => 'raw',
+				)
+			),
 		);
 	}
+
+	/**
+	 * Run the aggregations API query for any filtering
+	 *
+	 * @since 8.3.0
+	 *
+	 * @param WP_Query $query The WP_Query being filtered.
+	 */
+	public function action__parse_query( $query ) {
+		if ( is_admin() ) {
+			return;
+		}
+
+		if ( empty( $this->aggregations ) ) {
+			return;
+		}
+
+		jetpack_require_lib( 'jetpack-wpes-query-builder/jetpack-wpes-query-builder' );
+
+		$builder = new Jetpack_WPES_Query_Builder();
+		$this->add_aggregations_to_es_query_builder( $this->aggregations, $builder );
+		$es_args = array(
+			'aggregations' => $builder->build_aggregation(),
+			'size'         => 0,
+		);
+
+	}
+
+	/**
+	 * Run an instant search on the WordPress.com public API.
+	 *
+	 * @since 8.3.0
+	 *
+	 * @param array $args Args conforming to the WP.com /sites/<blog_id>/search endpoint.
+	 *
+	 * @return object|WP_Error The response from the public API, or a WP_Error.
+	 */
+	public function instant_api( array $args ) {
+		$endpoint    = sprintf( '/sites/%s/search', $this->jetpack_blog_id );
+		$service_url = 'https://public-api.wordpress.com/rest/v1.3' . $endpoint;
+
+		$do_authenticated_request = false;
+		if ( class_exists( 'Automattic\\Jetpack\\Connection\\Client' ) &&
+			isset( $args['authenticated_request'] ) &&
+			true === $args['authenticated_request'] ) {
+			$do_authenticated_request = true;
+		}
+
+		unset( $args['authenticated_request'] );
+
+		$request_args = array(
+			'headers'    => array(
+				'Content-Type' => 'application/json',
+			),
+			'timeout'    => 10,
+			'user-agent' => 'jetpack_search',
+		);
+
+		$request_body = wp_json_encode( $args );
+
+		$start_time = microtime( true );
+
+		if ( $do_authenticated_request ) {
+			$request_args['method'] = 'POST';
+
+			$request = Client::wpcom_json_api_request_as_blog( $endpoint, Client::WPCOM_JSON_API_VERSION, $request_args, $request_body );
+		} else {
+			$request_args = array_merge(
+				$request_args,
+				array(
+					'body' => $request_body,
+				)
+			);
+
+			$request = wp_remote_post( $service_url, $request_args );
+		}
+
+		$end_time = microtime( true );
+
+		if ( is_wp_error( $request ) ) {
+			return $request;
+		}
+
+		$response_code = wp_remote_retrieve_response_code( $request );
+
+		$response = json_decode( wp_remote_retrieve_body( $request ), true );
+
+		$took = is_array( $response ) && ! empty( $response['took'] )
+			? $response['took']
+			: null;
+
+		$query = array(
+			'args'          => $args,
+			'response'      => $response,
+			'response_code' => $response_code,
+			'elapsed_time'  => ( $end_time - $start_time ) * 1000, // Convert from float seconds to ms.
+			'es_time'       => $took,
+			'url'           => $service_url,
+		);
+
+		/**
+		 * Fires after a search request has been performed.
+		 *
+		 * Includes the following info in the $query parameter:
+		 *
+		 * array args Array of Elasticsearch arguments for the search
+		 * array response Raw API response, JSON decoded
+		 * int response_code HTTP response code of the request
+		 * float elapsed_time Roundtrip time of the search request, in milliseconds
+		 * float es_time Amount of time Elasticsearch spent running the request, in milliseconds
+		 * string url API url that was queried
+		 *
+		 * @module search
+		 *
+		 * @since  5.0.0
+		 * @since  5.8.0 This action now fires on all queries instead of just successful queries.
+		 *
+		 * @param array $query Array of information about the query performed
+		 */
+		do_action( 'did_jetpack_search_query', $query );
+
+		if ( ! $response_code || $response_code < 200 || $response_code >= 300 ) {
+			/**
+			 * Fires after a search query request has failed
+			 *
+			 * @module search
+			 *
+			 * @since  5.6.0
+			 *
+			 * @param array Array containing the response code and response from the failed search query
+			 */
+			do_action(
+				'failed_jetpack_search_query',
+				array(
+					'response_code' => $response_code,
+					'json'          => $response,
+				)
+			);
+
+			return new WP_Error( 'invalid_search_api_response', 'Invalid response from API - ' . $response_code );
+		}
+
+		return $response;
+	}
+
+
 
 }
