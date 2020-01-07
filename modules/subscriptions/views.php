b36816700884b84ef6987d0b327ba4a9dced68b1
Dat Hoang <htdat@users.noreply.github.com>
2020-01-07 21:24:07 +0700

    Improve per feedback and follow up coding standards

--- modules/subscriptions/views.php
+++ modules/subscriptions/views.php
@@ -6,6 +6,19 @@ class Jetpack_Subscriptions_Widget extends WP_Widget {
 	 * @var array When printing the submit button, what tags are allowed
 	 */
 	static $allowed_html_tags_for_submit_button = array( 'br' => array() );
+	/**
+	 * Use this variable when printing the message after submitting an email in subscription widgets
+	 *
+	 * @var array what tags are allowed
+	 */
+	public static $allowed_html_tags_for_message = array(
+		'a'  => array(
+			'href'  => array(),
+			'title' => array(),
+			'rel'   => array(),
+		),
+		'br' => array(),
+	);
 
 	function __construct() {
 		$widget_ops = array(
@@ -150,11 +163,21 @@ class Jetpack_Subscriptions_Widget extends WP_Widget {
 							__( 'Manage your email preferences.', 'jetpack' )
 						); ?></p>
 					<?php break;
-				case 'many_pending_subs' : ?>
-					<p class="error"><?php printf( __( 'You already have several pending email subscriptions. <br /> Approve or delete a few subscriptions at <a href="%1$s" title="%2$s" target="_blank">subscribe.wordpress.com</a> before continuing.', 'jetpack' ),
+				case 'many_pending_subs':
+					?>
+					<p class="error">
+						<?php
+						printf(
+							wp_kses(
+								/* translators: this message is displayed after submitting an email in subscription widgets  */
+								__( 'You already have several pending email subscriptions. <br /> Approve or delete a few subscriptions at <a href="%1$s" title="%2$s" target="_blank" rel="noopener noreferrer">subscribe.wordpress.com</a> before continuing.', 'jetpack' ),
+								self::$allowed_html_tags_for_message
+							),
 							'https://subscribe.wordpress.com/',
-							__( 'Manage your email preferences.', 'jetpack' )
-						); ?></p>
+							esc_attr__( 'Manage your email preferences.', 'jetpack' )
+						);
+						?>
+					</p>
 					<?php break;
 				case 'success' : ?>
                     <div class="success"><?php echo wpautop( str_replace( '[total-subscribers]', number_format_i18n( $subscribers_total['value'] ), $success_message ) ); ?></div>
