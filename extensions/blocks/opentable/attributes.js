6788464f5f73bf0af183d4575d1cd40ccb270dc2
Ben Dwyer <ben@scruffian.com>
2020-01-16 11:39:21 +0000

    remove country codes from open table language selector (#14353)

--- extensions/blocks/opentable/attributes.js
+++ extensions/blocks/opentable/attributes.js
@@ -8,13 +8,13 @@ import { compact, reduce, isEmpty } from 'lodash';
 const optionValues = options => options.map( option => option.value );
 
 export const languageOptions = [
-	{ value: 'en-US', label: 'English-US' },
-	{ value: 'fr-CA', label: 'Français-CA' },
-	{ value: 'de-DE', label: 'Deutsch-DE' },
-	{ value: 'es-MX', label: 'Español-MX' },
-	{ value: 'ja-JP', label: '日本語-JP' },
-	{ value: 'nl-NL', label: 'Nederlands-NL' },
-	{ value: 'it-IT', label: 'Italiano-IT' },
+	{ value: 'en-US', label: 'English' },
+	{ value: 'fr-CA', label: 'Français' },
+	{ value: 'de-DE', label: 'Deutsch' },
+	{ value: 'es-MX', label: 'Español' },
+	{ value: 'ja-JP', label: '日本語' },
+	{ value: 'nl-NL', label: 'Nederlands' },
+	{ value: 'it-IT', label: 'Italiano' },
 ];
 export const languageValues = optionValues( languageOptions );
 
