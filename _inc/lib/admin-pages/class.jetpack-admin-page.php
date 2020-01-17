7f03ca1183d1b466c8a8aeca47cc9d29bd9b819f
Matthew Denton <matt@mdbitz.com>
2020-01-17 14:03:24 -0500

    Update Privacy Policy link based on connection status

--- _inc/lib/admin-pages/class.jetpack-admin-page.php
+++ _inc/lib/admin-pages/class.jetpack-admin-page.php
@@ -279,6 +279,10 @@ abstract class Jetpack_Admin_Page {
 			? admin_url( 'admin.php?page=jetpack_about' )
 			: 'https://jetpack.com';
 
+		$jetpack_privacy_url = ( Jetpack::is_active() || Jetpack::is_development_mode() )
+			? $jetpack_admin_url . '#/privacy'
+			: 'https://automattic.com/privacy/';
+
 		?>
 		<div id="jp-plugin-container" class="
 		<?php
@@ -362,7 +366,7 @@ abstract class Jetpack_Admin_Page {
 						<a href="https://wordpress.com/tos/" target="_blank" rel="noopener noreferrer" title="<?php esc_html__( 'WordPress.com Terms of Service', 'jetpack' ); ?>" class="jp-footer__link"><?php echo esc_html_x( 'Terms', 'Navigation item', 'jetpack' ); ?></a>
 					</li>
 					<li class="jp-footer__link-item">
-						<a href="<?php echo esc_url( $jetpack_admin_url . '#/privacy' ); ?>" rel="noopener noreferrer" title="<?php esc_html_e( "Automattic's Privacy Policy", 'jetpack' ); ?>" class="jp-footer__link"><?php echo esc_html_x( 'Privacy', 'Navigation item', 'jetpack' ); ?></a>
+						<a href="<?php echo esc_url( $jetpack_privacy_url ); ?>" rel="noopener noreferrer" title="<?php esc_html_e( "Automattic's Privacy Policy", 'jetpack' ); ?>" class="jp-footer__link"><?php echo esc_html_x( 'Privacy', 'Navigation item', 'jetpack' ); ?></a>
 					</li>
 					<?php if ( is_multisite() && current_user_can( 'jetpack_network_sites_page' ) ) { ?>
 						<li class="jp-footer__link-item">
