d9816a55eb51ba25522d0f0ad159c0a21354b52b
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-01-21 13:19:05 +0200

    linting fixes

--- tests/e2e/plugins/e2e-plan-data-interceptor.php
+++ tests/e2e/plugins/e2e-plan-data-interceptor.php
@@ -10,21 +10,28 @@
 
 add_filter( 'pre_http_request', 'e2e_intercept_plan_data_request', 10, 3 );
 
+/**
+ * Intercept WPCOM plan data request and replaces it with mocked data
+ *
+ * @param result $return result.
+ * @param r      $r not used.
+ * @param string $url request URL.
+ */
 function e2e_intercept_plan_data_request( $return, $r, $url ) {
 	$site_id = Jetpack_Options::get_option( 'id' );
 
 	// shortcut the api call...
-  if( false !== stripos( $url, sprintf( '/sites/%d?', $site_id ) ) ) {
+	if ( false !== stripos( $url, sprintf( '/sites/%d?', $site_id ) ) ) {
 
 		$plan_data = get_option( 'e2e_jetpack_plan_data' );
-		if (empty( $plan_data) ) {
+		if ( empty( $plan_data ) ) {
 			return $return;
 		}
 
-    return [
-      'response' => [ 'code' => 200 ],
-      'body' => $plan_data,
-    ];
-  }
-  return $return;
+		return array(
+			'response' => array( 'code' => 200 ),
+			'body'     => $plan_data,
+		);
+	}
+	return $return;
 }
