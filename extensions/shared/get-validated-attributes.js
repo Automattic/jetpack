0e56d8626aaf2caff8a5b0ac6e06de39e4e76e33
Ben Dwyer <ben@scruffian.com>
2020-01-21 14:37:45 +0000

    Add a doc block to make it easier to reuse this function

--- extensions/shared/get-validated-attributes.js
+++ extensions/shared/get-validated-attributes.js
@@ -3,6 +3,13 @@
  */
 import { reduce } from 'lodash';
 
+/**
+ * Take user set attributes and validate them against the attribute definition.
+ *
+ * @param {string} attributeDetails An object representing the attributes for a block, formatted as an object with these properties: type, default, validator.
+ * @param {string} attributesToValidate The attributes for an instance of the block, which may have been edited by a user
+ * @returns {object} Block attributes that have been validated.
+ */
 export const getValidatedAttributes = ( attributeDetails, attributesToValidate ) =>
 	reduce(
 		attributesToValidate,
