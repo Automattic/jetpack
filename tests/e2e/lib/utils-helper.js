41ec56f45ee39a157385ccb644321d1dbd0cfdc3
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-01-24 12:58:54 +0200

    fancier module activation logic

--- tests/e2e/lib/utils-helper.js
+++ tests/e2e/lib/utils-helper.js
@@ -3,6 +3,10 @@
  */
 import { execSync, exec } from 'child_process';
 import config from 'config';
+/**
+ * Internal dependencies
+ */
+import Page from './pages/page';
 
 /**
  * Executes a shell command and return it as a Promise.
@@ -64,20 +68,32 @@ export function provisionJetpackStartConnection( plan = 'professional', user = '
 }
 
 /**
- * Runs wp cli command to activate jetpack module
+ * Runs wp cli command to activate jetpack module, also checks if the module is available in the list of active modules.
+ * @param {Page} page Puppeteer page object
  * @param {string} module Jetpack module name
  */
-export async function activateModule( module ) {
+export async function activateModule( page, module ) {
 	const cliCmd = `wp jetpack module activate ${ module }`;
+	const activeModulesCmd = 'wp option get jetpack_active_modules --format=json';
 	await execWpCommand( cliCmd );
 
-	const modulesList = JSON.parse(
-		await execWpCommand( 'wp option get jetpack_active_modules --format=json' )
-	);
+	let modulesList = JSON.parse( await execWpCommand( activeModulesCmd ) );
+
+	if ( modulesList.includes( module ) ) {
+		return true;
+	}
+
+	// Give it another try to activate the module
+	page.reload( { waitFor: 'networkidle0' } );
+	await execWpCommand( cliCmd );
+
+	modulesList = JSON.parse( await execWpCommand( activeModulesCmd ) );
 
 	if ( ! modulesList.includes( module ) ) {
 		throw new Error( `${ module } is failed to activate` );
 	}
+
+	return true;
 }
 
 export async function execWpCommand( wpCmd, suffix = null ) {
