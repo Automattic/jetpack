f6df0d2c97a6afc5cd488f08db7d314600115e52
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-01-24 16:36:10 +0200

    [not verified] clean up in interceptor plugin

--- tests/e2e/plugins/e2e-plan-data-interceptor.php
+++ tests/e2e/plugins/e2e-plan-data-interceptor.php
@@ -24,15 +24,8 @@ function e2e_intercept_plan_data_request( $return, $r, $url ) {
 		return $return;
 	}
 	// match both /sites/$site_id && /sites/$site_id? urls
-	$regex = sprintf( '/\/sites\/%d($|\?)/', $site_id );
-	error_log(print_r( 'PRE_REQUEST0', 1 ));
-	error_log(print_r( $url, 1 ));
-	error_log(print_r( $url === trim($url), 1 ));
-	error_log(print_r( $regex, 1 ));
-	error_log(print_r( preg_match( $regex, $url ), 1 ));
-	if ( 1 === preg_match( $regex, $url ) ) {
+	if ( 1 === preg_match( sprintf( '/\/sites\/%d($|\?)/', $site_id ), $url ) ) {
 		$plan_data = get_option( 'e2e_jetpack_plan_data' );
-
 		error_log(print_r( 'PRE_REQUEST', 1 ));
 		error_log(print_r( $url, 1 ));
 		error_log(print_r( $plan_data, 1 ));
