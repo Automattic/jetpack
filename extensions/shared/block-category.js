dd89a23e7081bd85e882954a5c24e20140ab6842
Brandon Kraft <public@brandonkraft.com>
2020-01-23 21:09:35 -0600

    Add Jetpack blocks collection

--- extensions/shared/block-category.js
+++ extensions/shared/block-category.js
@@ -1,19 +1,27 @@
 /**
  * External dependencies
  */
-import { getCategories, setCategories } from '@wordpress/blocks';
+import { getCategories, setCategories, registerBlockCollection } from '@wordpress/blocks';
 
 /**
  * Internal dependencies
  */
 import JetpackLogo from '../shared/jetpack-logo';
 
-setCategories( [
-	...getCategories().filter( ( { slug } ) => slug !== 'jetpack' ),
-	// Add a Jetpack block category
-	{
-		slug: 'jetpack',
+if ( typeof registerBlockCollection === 'function' ) {
+	registerBlockCollection( 'jetpack', {
 		title: 'Jetpack',
 		icon: <JetpackLogo />,
-	},
-] );
+	} );
+} else {
+	// This can be removed once G 7.3 is shipped in the Core version that is JP's minimum.
+	setCategories( [
+		...getCategories().filter( ( { slug } ) => slug !== 'jetpack' ),
+		// Add a Jetpack block category
+		{
+			slug: 'jetpack',
+			title: 'Jetpack',
+			icon: <JetpackLogo />,
+		},
+	] );
+}
