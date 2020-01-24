e6f2db33b4b9898523a62cf0464917ddb9d37a06
Paul Bunkham <paul@dobit.co.uk>
2020-01-24 20:03:21 +0000

    OpenTable: Separate block registration from setting the availability (#14464)
    * Separate block registration from setting the availability
    
    As with other blocks that rely on server side rendering, we need to make
    sure that the block is registered so that it is rendered in the frontend
    of the site.
    
    This change registers the block during the `init` hook, but keeps the
    setting of the block availability to the
    `jetpack_register_gutenberg_extensions` hook.
    
    * Corrected the function being called to set the availability

--- extensions/blocks/opentable/opentable.php
+++ extensions/blocks/opentable/opentable.php
@@ -46,6 +46,17 @@ function register_block() {
 			BLOCK_NAME,
 			array( 'render_callback' => 'Jetpack\OpenTable_Block\load_assets' )
 		);
+	}
+}
+add_action( 'init', 'Jetpack\OpenTable_Block\register_block' );
+
+/**
+ * Set the availability of the block as the editor
+ * is loaded.
+ */
+function set_availability() {
+	if ( is_available() ) {
+		\Jetpack_Gutenberg::set_extension_available( BLOCK_NAME );
 	} else {
 		\Jetpack_Gutenberg::set_extension_unavailable(
 			BLOCK_NAME,
@@ -57,7 +68,7 @@ function register_block() {
 		);
 	}
 }
-add_action( 'jetpack_register_gutenberg_extensions', 'Jetpack\OpenTable_Block\register_block' );
+add_action( 'jetpack_register_gutenberg_extensions', 'Jetpack\OpenTable_Block\set_availability' );
 
 /**
  * Adds an inline script which updates the block editor settings to
