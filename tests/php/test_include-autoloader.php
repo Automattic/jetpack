fd4abcb8b9293545c621dd461171a8695a0d7828
Kim Brown <50059399+kbrown9@users.noreply.github.com>
2020-01-25 22:05:30 -0500

    [not verified] Autoloader: code cleanup
    - Change test class name to 'WP_Test_Jetpack_Include_Autoloader'.
    - Fix PHPCS warnings.
    - Remove an error_log().

--- tests/php/test_include-autoloader.php
+++ tests/php/test_include-autoloader.php
@@ -1,25 +1,40 @@
-<?php
+<?php // phpcs:ignore WordPress.Files.FileName.InvalidClassFileName
 
-class WP_Test_Jetpack_Autoloader_Lint extends WP_UnitTestCase {
+/**
+ * Contains tests for the Jetpack Autoloader.
+ */
+class WP_Test_Jetpack_Include_Autoloader extends WP_UnitTestCase {
 
+	/**
+	 * Tests that the Autoloader properly includes class files when the files
+	 * are renamed.
+	 */
 	public function test_renaming_files_in_autoloader_does_not_cause_errors() {
 		$packages = $this->get_packages_classes();
-		 $this->unset_package_classes();
+		$this->unset_package_classes();
 
-		 $empty_packages = $this->get_packages_classes();
-		 $this->assertTrue( empty( $empty_packages ), 'package classes are not empty!' );
+		$empty_packages = $this->get_packages_classes();
+		$this->assertTrue( empty( $empty_packages ), 'package classes are not empty!' );
 
-		// Fake the plugin update
+		// Fake the plugin update.
 		apply_filters( 'upgrader_post_install', true, array( 'plugin' => JETPACK__PLUGIN_FILE ), array() );
 
 		$this->assertEquals( $packages, $this->get_packages_classes(), 'package classes are not the same as before' );
 	}
 
-	public function get_packages_classes( ) {
+	/**
+	 * Returns the global $jetpack_packages_classes variable.
+	 *
+	 * @return array
+	 */
+	public function get_packages_classes() {
 		global $jetpack_packages_classes;
 		return $jetpack_packages_classes;
 	}
 
+	/**
+	 * Unsets the global $jetpack_packages_classes variable.
+	 */
 	public function unset_package_classes() {
 		global $jetpack_packages_classes;
 		$jetpack_packages_classes = array();
