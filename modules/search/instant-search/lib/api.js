607dbb34b7c171b3eadc2d3f47213c8dac9c33e0
Chris R <chris.rosser@automattic.com>
2020-01-28 11:33:16 +1300

    Instant Search: use taxonomy name rather than slug for filters (#14437)
    * Instant Search: use taxonomy name rather than slug
    
    * Fix display of category filters
    
    * Update buildFilterObject()

--- modules/search/instant-search/lib/api.js
+++ modules/search/instant-search/lib/api.js
@@ -37,10 +37,10 @@ function generateAggregation( filter ) {
 			};
 		}
 		case 'taxonomy': {
-			let field = `taxonomy.${ filter.taxonomy }.slug`;
+			let field = `taxonomy.${ filter.taxonomy }.name.raw`;
 			if ( filter.taxonomy === 'post_tag' ) {
 				field = 'tag.slug';
-			} else if ( filter.type === 'category' ) {
+			} else if ( filter.taxonomy === 'category' ) {
 				field = 'category.slug';
 			}
 			return { terms: { field, size: filter.count } };
@@ -116,7 +116,7 @@ function buildFilterObject( filterQuery ) {
 					filter.bool.must.push( filterKeyToEsFilter.get( key )( item ) );
 				} else {
 					// If key is not in the standard map, assume to be a custom taxonomy
-					filter.bool.must.push( { term: { [ `taxonomy.${ key }.slug` ]: item } } );
+					filter.bool.must.push( { term: { [ `taxonomy.${ key }.name.raw` ]: item } } );
 				}
 			} );
 		} );
