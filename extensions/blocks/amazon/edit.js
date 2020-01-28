f0182956c80ab9054c800c825f6f69d1f4c42fca
Ben Dwyer <ben@scruffian.com>
2020-01-28 13:35:27 +0000

    add a block placeholder and edit features

--- extensions/blocks/amazon/edit.js
+++ extensions/blocks/amazon/edit.js
@@ -1,23 +1,261 @@
 /**
  * External dependencies
  */
+import tinycolor from 'tinycolor2';
 import { __ } from '@wordpress/i18n';
-import { Component } from '@wordpress/element';
+
+/**
+ * WordPress dependencies
+ */
+import {
+	BlockIcon,
+	ContrastChecker,
+	InspectorControls,
+	PanelColorSettings,
+} from '@wordpress/block-editor';
+import {
+	Button,
+	FormTokenField,
+	Notice,
+	PanelBody,
+	Placeholder,
+	ToggleControl,
+} from '@wordpress/components';
+import { useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
+import icon from './icon';
+import data from './dummy-data';
 import './editor.scss';
 
-class AmazonEdit extends Component {
-	/**
-	 * Write the block editor UI.
-	 *
-	 * @returns {object} The UI displayed when user edits this block.
-	 */
-	render() {
-		return <p>{ __( 'Coming soon', 'jetpack' ) }</p>;
-	}
-}
+export default function AmazonEdit( {
+	attributes: {
+		backgroundColor,
+		textColor,
+		buttonAndLinkColor,
+		product,
+		showImage,
+		showTitle,
+		showSeller,
+		showPrice,
+		showPurchaseButton,
+	},
+	className,
+	setAttributes,
+} ) {
+	const notice = false; // TODO
 
-export default AmazonEdit;
+	const [ suggestions, setSuggestions ] = useState( [] );
+	const onInputChange = () => {
+		// TODO get suggestions from API
+		// It would be great if we didn't have to embed the ASIN like this but I think that
+		// requires changes to core
+		setSuggestions(
+			data.products.map( dataProduct => dataProduct.title + ' (ASIN:' + dataProduct.asin + ')' )
+		);
+	};
+
+	const idRegex = /^(\d+)$|\(ASIN:(.+)\)$/;
+	const onChange = selectedProducts => {
+		const selectedIds = selectedProducts.map( selectedProduct => {
+			const parsed = idRegex.exec( selectedProduct );
+			const selectedId = parsed[ 1 ] || parsed[ 2 ];
+			return data.products.filter( filteredProduct => filteredProduct.asin === selectedId );
+		} );
+		setAttributes( { product: selectedIds[ 0 ][ 0 ].asin } );
+	};
+
+	const blockPlaceholder = (
+		<Placeholder
+			label={ __( 'Amazon', 'jetpack' ) }
+			instructions={ __( 'Search by entering an Amazon product name or ID below.', 'jetpack' ) }
+			icon={ <BlockIcon icon={ icon } /> }
+			notices={
+				notice && (
+					<Notice status="error" isDismissible={ false }>
+						{ notice }
+					</Notice>
+				)
+			}
+		>
+			<form>
+				<FormTokenField
+					value={ product }
+					suggestions={ suggestions }
+					onInputChange={ onInputChange }
+					maxSuggestions={ 10 }
+					label={ __( 'Products', 'jetpack' ) }
+					onChange={ onChange }
+				/>
+				<Button isSecondary isLarge type="submit">
+					{ __( 'Preview', 'jetpack' ) }
+				</Button>
+			</form>
+		</Placeholder>
+	);
+
+	const inspectorControls = (
+		<InspectorControls>
+			{ product && (
+				<>
+					<PanelBody title={ __( 'Promotion Settings', 'jetpack' ) }>
+						<ToggleControl
+							label={ __( 'Show Image', 'jetpack' ) }
+							checked={ showImage }
+							onChange={ () => setAttributes( { showImage: ! showImage } ) }
+						/>
+						<ToggleControl
+							label={ __( 'Show Title', 'jetpack' ) }
+							checked={ showTitle }
+							onChange={ () => setAttributes( { showTitle: ! showTitle } ) }
+						/>
+						<ToggleControl
+							label={ __( 'Show Author/Seller', 'jetpack' ) }
+							checked={ showSeller }
+							onChange={ () => setAttributes( { showSeller: ! showSeller } ) }
+						/>
+						<ToggleControl
+							label={ __( 'Show Price', 'jetpack' ) }
+							checked={ showPrice }
+							onChange={ () => setAttributes( { showPrice: ! showPrice } ) }
+						/>
+						<ToggleControl
+							label={ __( 'Show Purchase Button', 'jetpack' ) }
+							checked={ showPurchaseButton }
+							onChange={ () => setAttributes( { showPurchaseButton: ! showPurchaseButton } ) }
+						/>
+					</PanelBody>
+					<PanelColorSettings
+						title={ __( 'Color Settings', 'jetpack' ) }
+						colorSettings={ [
+							{
+								value: backgroundColor,
+								onChange: newBackgroundColor =>
+									setAttributes( { backgroundColor: newBackgroundColor } ),
+								label: __( 'Background Color', 'jetpack' ),
+							},
+							{
+								value: textColor,
+								onChange: newTextColor => setAttributes( { textColor: newTextColor } ),
+								label: __( 'Text Color', 'jetpack' ),
+							},
+							{
+								value: buttonAndLinkColor,
+								onChange: newButtonAndLinkColor =>
+									setAttributes( { buttonAndLinkColor: newButtonAndLinkColor } ),
+								label: __( 'Button & Link Color', 'jetpack' ),
+							},
+						] }
+					>
+						{
+							<ContrastChecker
+								{ ...{
+									isLargeText: false,
+									textColor: textColor,
+									backgroundColor: backgroundColor,
+								} }
+							/>
+						}
+					</PanelColorSettings>
+				</>
+			) }
+		</InspectorControls>
+	);
+
+	const blockPreview = () => {
+		const {
+			title,
+			authors,
+			artists,
+			actors,
+			manufacturer,
+			detailPageUrl,
+			listPrice,
+			imageUrlMedium,
+			imageWidthMedium,
+			imageHeightMedium,
+			authorshipInfo,
+		} = data.products.filter( productDataItem => productDataItem.asin === product )[ 0 ];
+
+		// TODO - we should be able to get this from API in a neater way once we have access
+		const seller = authors.length
+			? authors
+			: artists.length
+			? artists
+			: actors.length
+			? actors
+			: manufacturer
+			? manufacturer
+			: authorshipInfo
+			? authorshipInfo
+			: null;
+
+		// TODO - we have different image sizes in the API
+		const image = imageUrlMedium && (
+			<a href={ detailPageUrl }>
+				<img
+					alt={ title }
+					src={ imageUrlMedium }
+					width={ imageWidthMedium }
+					heigth={ imageHeightMedium }
+				/>
+			</a>
+		);
+
+		const buttonTextColor = tinycolor
+			.mostReadable( buttonAndLinkColor, [ '#ffffff' ], {
+				includeFallbackColors: true,
+				size: 'small',
+			} )
+			.toHexString();
+
+		return (
+			product && (
+				<div
+					style={ { backgroundColor: backgroundColor, color: textColor, width: imageWidthMedium } }
+				>
+					{ showImage && image }
+					{ showTitle && (
+						<div className={ `${ className }-title` }>
+							<a href={ detailPageUrl } style={ { color: buttonAndLinkColor } }>
+								{ title }
+							</a>
+						</div>
+					) }
+					{ showSeller && seller && (
+						<div className={ `${ className }-seller` }>
+							{ seller.length > 0 && typeof seller !== 'string'
+								? seller.map( singleSeller => singleSeller )
+								: seller }
+						</div>
+					) }
+					{ showPrice && <div className={ `${ className }-list-price` }>{ listPrice }</div> }
+					{ showPurchaseButton && (
+						<Button
+							href={ detailPageUrl }
+							icon={ icon }
+							isPrimary
+							className={ `${ className }-button` }
+							style={ {
+								color: buttonTextColor,
+								backgroundColor: buttonAndLinkColor,
+								borderColor: buttonAndLinkColor,
+							} }
+						>
+							{ __( 'Shop Now', 'jetpack' ) }
+						</Button>
+					) }
+				</div>
+			)
+		);
+	};
+
+	return (
+		<div className={ className }>
+			{ inspectorControls }
+			{ product ? blockPreview() : blockPlaceholder }
+		</div>
+	);
+}
