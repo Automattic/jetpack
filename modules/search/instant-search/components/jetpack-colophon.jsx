11f69ba9e1785d48386a2a09c252b6def8c78eba
bluefuton <chris@bluefuton.com>
2020-01-30 11:31:39 +1300

    Show JetpackColophon when setting is activated

---
+++ modules/search/instant-search/components/jetpack-colophon.jsx
@@ -0,0 +1,12 @@
+/** @jsx h */
+
+/**
+ * External dependencies
+ */
+import { h } from 'preact';
+
+const JetpackColophon = () => {
+	return <div className="jetpack-instant-search__jetpack-colophon">Powered by Jetpack</div>;
+};
+
+export default JetpackColophon;
