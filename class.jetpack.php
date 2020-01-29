Automattic/jetpack

commit 706cdfdd1b2f3133305c7fc326928d5b5c6cbe64

Mobile: Hide edit post link in content (#12867)
* hide edit post link in content if mobile app

* simpler 'sandcastle' version

* move to class.jetpack.php

* use tabs

class.jetpack.php
@@ -717,6 +717,11 @@ class Jetpack {
 			jetpack_require_lib( 'functions.wp-notify' );
 		}
 
+		// Hide edit post link if mobile app.
+		if ( Jetpack_User_Agent_Info::is_mobile_app() ) {
+			add_filter( 'edit_post_link', '__return_empty_string' );
+		}
+
 		// Update the Jetpack plan from API on heartbeats
 		add_action( 'jetpack_heartbeat', array( 'Jetpack_Plan', 'refresh_from_wpcom' ) );
 

commit 36fe2713ddc8a2387ab6e90628b14de137b050b7

Mobile Apps: completely remove the edit post link when in app
Fixes https://github.com/wordpress-mobile/WordPress-iOS/issues/13314

This issue was introduced in #12867.

The original goal of this change was to to hide the Edit Post link
whenever you would access your site via the mobile app's in-app browser.

To hide the Edit Post link, we originally opted to hook into the edit_post_link filter.
While this works, it only changes the string returned for the Edit Post link itself.
It does not account for any "before" or "after" parameters that may be passed
when calling the edit_post_link function; those are not part of the filter:
https://github.com/WordPress/WordPress/blob/001ffe81fbec4438a9f594f330e18103d21fbcd7/wp-includes/link-template.php#L1439

Instead, let's shortcircuit that link earlier, by hooking into the get_edit_post_link filter.
If that filter is set to return an empty string, the edit_post_link function will return early:
https://github.com/WordPress/WordPress/blob/001ffe81fbec4438a9f594f330e18103d21fbcd7/wp-includes/link-template.php#L1420

class.jetpack.php
@@ -641,7 +641,7 @@ class Jetpack {
 
 		// Hide edit post link if mobile app.
 		if ( Jetpack_User_Agent_Info::is_mobile_app() ) {
-			add_filter( 'edit_post_link', '__return_empty_string' );
+			add_filter( 'get_edit_post_link', '__return_empty_string' );
 		}
 
 		// Update the Jetpack plan from API on heartbeats