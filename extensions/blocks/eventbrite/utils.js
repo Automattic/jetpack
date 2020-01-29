7de9b29b313d725166a82ee48ab54950dde38bc4
Grant Kinney <hi@grant.mk>
2020-01-29 09:41:51 -0600

    Fix jsdoc spacing

--- extensions/blocks/eventbrite/utils.js
+++ extensions/blocks/eventbrite/utils.js
@@ -20,7 +20,7 @@ export function convertToLink( url, onReplace ) {
  * Extracts an event id from an Eventbrite URL.
  *
  * @param   {string} url Eventbrite URL.
- * @returns {?number}     Event id.
+ * @returns {?number}    Event id.
  */
 export function eventIdFromUrl( url ) {
 	if ( ! url ) {
