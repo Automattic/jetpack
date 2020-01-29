62f399e436ba6a1be812e642dd9ead1d15a741ce
bluefuton <chris@bluefuton.com>
2020-01-30 11:36:24 +1300

    Translate text

--- modules/search/instant-search/components/jetpack-colophon.jsx
+++ modules/search/instant-search/components/jetpack-colophon.jsx
@@ -4,9 +4,14 @@
  * External dependencies
  */
 import { h } from 'preact';
+import { __ } from '@wordpress/i18n';
 
 const JetpackColophon = () => {
-	return <div className="jetpack-instant-search__jetpack-colophon">Powered by Jetpack</div>;
+	return (
+		<div className="jetpack-instant-search__jetpack-colophon">
+			{ __( 'Search powered by Jetpack', 'jetpack' ) }
+		</div>
+	);
 };
 
 export default JetpackColophon;
