8be23dd5fb30157cdbd239c73440521cee053bad
Kirk Wight <kwight@kwight.ca>
2020-02-07 13:53:41 -0800

    Customizer: Update Publish slideout labels. https://github.com/Automattic/wpcomsh/pull/381

--- projects/plugins/wpcomsh/customizer-fixes/customizer-labels-site-not-launched.js
+++ projects/plugins/wpcomsh/customizer-fixes/customizer-labels-site-not-launched.js
@@ -3,4 +3,10 @@
 	keys.forEach( function ( key ) {
 		window._wpCustomizeControlsL10n[ key ] = window._wpCustomizeControlsL10nSitePrivate[ key ];
 	} );
+
+	document.addEventListener( 'DOMContentLoaded', function() {
+		// Update Publish Label in Gear Status Options
+		var statusPublishInput = document.querySelector( "[id^='customize-selected-changeset-status-control-input-'][value='publish']" );
+		statusPublishInput.parentElement.querySelector( 'label' ).textContent = window._wpCustomizeControlsL10nSitePrivate.publish;
+	} );
 } )();
