f2fa719b86bd7ecc61781d072a6be9de45aa6be0
Glen Davies <glen.davies@a8c.com>
2020-02-10 10:17:12 +1300

    Add alignment class to front end

--- extensions/blocks/google-calendar/google-calendar.php
+++ extensions/blocks/google-calendar/google-calendar.php
@@ -41,16 +41,21 @@ function jetpack_google_calendar_block_load_assets( $attr ) {
 	$width  = isset( $attr['width'] ) ? $attr['width'] : '800';
 	$height = isset( $attr['height'] ) ? $attr['height'] : '600';
 	$url    = isset( $attr['url'] ) ? $attr['url'] : '';
+	$align  = isset( $attr['align'] ) ? 'align' . $attr['align'] : '';
+
+	if ( empty( $url ) ) {
+		return;
+	}
 
 	if ( class_exists( 'Jetpack_AMP_Support' ) && Jetpack_AMP_Support::is_amp_request() ) {
 		return <<<EOT
-<div class="wp-block-jetpack-google-calendar">
+<div class="wp-block-jetpack-google-calendar ${align}">
 	<amp-iframe src="${url}" frameborder="0" style="border:0" scrolling="no" width="${width}" height="${height}" sandbox="allow-scripts allow-same-origin" layout="responsive"></amp-iframe>
 </div>
 EOT;
 	} else {
 		return <<<EOT
-<div class="wp-block-jetpack-google-calendar">
+<div class="wp-block-jetpack-google-calendar ${align}">
 	<iframe src="${url}" frameborder="0" style="border:0" scrolling="no" width="${width}" height="${height}"></iframe>
 </div>
 EOT;
