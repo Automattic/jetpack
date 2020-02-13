5666907a2751054d44e6105b81a4883d2c19bc17
Filipe Varela <keoshi@keoshi.com>
2020-02-13 13:19:49 +0000

    Fix Gridicon's viewBox values
    We shouldn't be messing with the values in viewBox at all since all icons have a baseline of 24 x 24 px. While sizing changes, their set up should be consistent.
    
    Reported in: https://github.com/Automattic/jetpack/pull/14629#issuecomment-585474931
    
    Thanks for catching this @jsnmoon !

--- modules/search/instant-search/components/gridicon/index.jsx
+++ modules/search/instant-search/components/gridicon/index.jsx
@@ -167,7 +167,7 @@ class Gridicon extends Component {
 				height={ size }
 				onClick={ this.props.onClick }
 				style={ { height: size, width: size } }
-				viewBox={ '0 0 ' + size + ' ' + size }
+				viewBox="0 0 24 24"
 				width={ size }
 				xmlns="http://www.w3.org/2000/svg"
 			>
