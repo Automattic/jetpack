7403d9a60e77331e0f973e4be4174471c62ac6ad
Jason Moon <jason.moon@automattic.com>
2020-02-18 15:18:13 -0700

    Hide popover on clearing filters

--- modules/search/instant-search/components/search-filters.jsx
+++ modules/search/instant-search/components/search-filters.jsx
@@ -24,6 +24,7 @@ export default class SearchFilters extends Component {
 
 	onClearFilters = () => {
 		clearFiltersFromQuery();
+		this.props.onChange && this.props.onChange();
 	};
 
 	hasActiveFilters() {
