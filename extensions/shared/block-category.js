ea0ae07073c6603d9ded57bb5a012e353b06a37f
retrofox <rdsuarez@gmail.com>
2020-02-18 15:12:39 -0300

    [not verified] [not verified] extensions: add custom extension categories

--- extensions/shared/block-category.js
+++ extensions/shared/block-category.js
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+import { map, includes } from 'lodash';
 import { getCategories, setCategories } from '@wordpress/blocks';
 
 /**
@@ -8,12 +9,36 @@ import { getCategories, setCategories } from '@wordpress/blocks';
  */
 import JetpackLogo from '../shared/jetpack-logo';
 
-setCategories( [
-	...getCategories().filter( ( { slug } ) => slug !== 'jetpack' ),
-	// Add a Jetpack block category
+/**
+ * Add custom categories, checking that the new category
+ * doesn't already exist.
+ */
+const categories = getCategories();
+const customCategories = [
 	{
 		slug: 'jetpack',
 		title: 'Jetpack',
 		icon: <JetpackLogo />,
 	},
+	{
+		slug: 'earn',
+		title: 'Earn',
+	},
+	{
+		slug: 'marketing',
+		title: 'Marketing',
+	},
+	{
+		slug: 'social',
+		title: 'Social',
+	},
+	{
+		slug: 'widget',
+		title: 'Widget',
+	},
+];
+
+setCategories( [
+	...categories,
+	...customCategories.filter( ( { slug } ) => ! includes( map( categories, 'slug' ), slug ) ),
 ] );
