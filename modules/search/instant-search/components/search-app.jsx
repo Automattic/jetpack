9f8c1693e73b6ac5ada53f15929ea10854498f2b
Jason Moon <jason.moon@automattic.com>
2020-02-21 15:26:40 -0700

    Remove unused showLogo property

--- modules/search/instant-search/components/search-app.jsx
+++ modules/search/instant-search/components/search-app.jsx
@@ -257,7 +257,6 @@ class SearchApp extends Component {
 					query={ getSearchQuery() }
 					response={ this.state.response }
 					resultFormat={ getResultFormatQuery() }
-					showLogo={ this.state.overlayOptions.showLogo }
 					showPoweredBy={ this.state.overlayOptions.showPoweredBy }
 					widgets={ this.props.options.widgets }
 				/>
