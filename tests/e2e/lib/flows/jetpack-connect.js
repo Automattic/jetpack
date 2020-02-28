9836b72d581fa5436b538060efb2867e327ac96a
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-02-28 18:17:14 +0200

    use `defaultUser2`

--- tests/e2e/lib/flows/jetpack-connect.js
+++ tests/e2e/lib/flows/jetpack-connect.js
@@ -32,7 +32,7 @@ const cardCredentials = config.get( 'testCardCredentials' );
  * @param {Object} o Optional object with params such as `wpcomUser` and expected Jetpack plan
  */
 export async function connectThroughWPAdminIfNeeded( {
-	wpcomUser = 'defaultUser',
+	wpcomUser = 'defaultUser2',
 	plan = 'pro',
 	mockPlanData = false,
 } = {} ) {
@@ -109,7 +109,7 @@ export async function connectThroughWPAdminIfNeeded( {
 }
 
 export async function connectThroughJetpackStart( {
-	wpcomUser = 'defaultUser',
+	wpcomUser = 'defaultUser2',
 	plan = 'pro',
 } = {} ) {
 	// remove Sandbox cookie
