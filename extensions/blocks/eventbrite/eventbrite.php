f8e485344ea0620562940e3be6730abcf66382c2
apeatling <apeatling@gmail.com>
2020-03-02 14:13:39 -0800

    Eventbrite Alignment: Make sure the alignment classes are added to the server side rendered iframe embed.

--- extensions/blocks/eventbrite/eventbrite.php
+++ extensions/blocks/eventbrite/eventbrite.php
@@ -47,8 +47,15 @@ function jetpack_render_eventbrite_block( $attr, $content ) {
 
 		// $content contains a fallback link to the event that's saved in the post_content.
 		// Append a div that will hold the iframe embed created by the Eventbrite widget.js.
+		$class_names = array();
+		array_push( $class_names, 'eventbrite__in-page-checkout' );
+
+		if ( isset( $attr['align'] ) ) {
+			array_push( $class_names, 'align' . esc_attr( $attr['align'] ) );
+		}
+
 		$content .= sprintf(
-			'<div id="%s" class="eventbrite__in-page-checkout"></div>',
+			'<div id="%s" class="' . implode( ' ', $class_names ) . '"></div>',
 			esc_attr( $widget_id )
 		);
 
