Automattic/jetpack

commit c0a8bbcec7b6bc15b900faa7883054e4e2660e19

Instant Search: Display filters selected outside of overlay (#14834)
* Fix off-by-one bug with date formatting in filters
* Derive preselected filters from the query string
* Pass widgets outside overlay to JS client
* Render preselected filters
* Refactor duplicated code in SearchFilters
* Fix selected filters regression

modules/search/instant-search/lib/filters.js
@@ -1,0 +1,74 @@
+// NOTE: We only import the difference package here for to reduced bundle size.
+//       Do not import the entire lodash library!
+// eslint-disable-next-line lodash/import-scope
+import difference from 'lodash/difference';
+
+/**
+ * Internal dependencies
+ */
+import { SERVER_OBJECT_NAME } from './constants';
+
+const FILTER_KEYS = Object.freeze( [
+	// Post types
+	'post_types',
+	// Date filters
+	'month_post_date',
+	'month_post_date_gmt',
+	'month_post_modified',
+	'month_post_modified_gmt',
+	'year_post_date',
+	'year_post_date_gmt',
+	'year_post_modified',
+	'year_post_modified_gmt',
+] );
+
+export function getFilterKeys() {
+	// Extract taxonomy names from server widget data
+	const taxonomies = window[ SERVER_OBJECT_NAME ].widgets
+		.map( w => w.filters )
+		.filter( filters => Array.isArray( filters ) )
+		.reduce( ( filtersA, filtersB ) => filtersA.concat( filtersB ), [] )
+		.filter( filter => filter.type === 'taxonomy' )
+		.map( filter => filter.taxonomy );
+	return [ ...FILTER_KEYS, ...taxonomies ];
+}
+
+// These filter keys are selectable from sidebar filters
+function getSelectableFilterKeys( overlayWidgets ) {
+	return overlayWidgets
+		.map( extractFilters )
+		.reduce( ( prev, current ) => prev.concat( current ), [] );
+}
+
+// These filter keys are not selectable from sidebar filters
+// In other words, they were selected via filters outside the search sidebar
+export function getUnselectableFilterKeys( overlayWidgets ) {
+	return difference( getFilterKeys(), getSelectableFilterKeys( overlayWidgets ) );
+}
+
+function extractFilters( widget ) {
+	return widget.filters
+		.map( mapFilterToFilterKey )
+		.filter( filterName => typeof filterName === 'string' );
+}
+
+export function mapFilterToFilterKey( filter ) {
+	if ( filter.type === 'date_histogram' ) {
+		return `${ filter.interval }_${ filter.field }`;
+	} else if ( filter.type === 'taxonomy' ) {
+		return 'taxonomy';
+	} else if ( filter.type === 'post_type' ) {
+		return 'post_types';
+	}
+	return null;
+}
+
+export function mapFilterToType( filter ) {
+	if ( filter.type === 'date_histogram' ) {
+		return 'date';
+	} else if ( filter.type === 'taxonomy' ) {
+		return 'taxonomy';
+	} else if ( filter.type === 'post_type' ) {
+		return 'postType';
+	}
+}

modules/search/class-jetpack-instant-search.php
@@ -83,19 +83,29 @@ class Jetpack_Instant_Search extends Jetpack_Search {
 			$widget_options = end( $widget_options );
 		}
 
-		$overlay_widget_ids = array_key_exists( 'jetpack-instant-search-sidebar', get_option( 'sidebars_widgets', array() ) ) ?
+		$overlay_widget_ids      = array_key_exists( 'jetpack-instant-search-sidebar', get_option( 'sidebars_widgets', array() ) ) ?
 			get_option( 'sidebars_widgets', array() )['jetpack-instant-search-sidebar'] : array();
-		$filters            = Jetpack_Search_Helpers::get_filters_from_widgets( $overlay_widget_ids );
-		$widgets            = array();
-		foreach ( $filters as $key => $filter ) {
-			if ( ! isset( $widgets[ $filter['widget_id'] ] ) ) {
-				$widgets[ $filter['widget_id'] ]['filters']   = array();
-				$widgets[ $filter['widget_id'] ]['widget_id'] = $filter['widget_id'];
+		$filters                 = Jetpack_Search_Helpers::get_filters_from_widgets();
+		$widgets                 = array();
+		$widgets_outside_overlay = array();
+		foreach ( $filters as $key => &$filter ) {
+			$filter['filter_id'] = $key;
+
+			if ( in_array( $filter['widget_id'], $overlay_widget_ids, true ) ) {
+				if ( ! isset( $widgets[ $filter['widget_id'] ] ) ) {
+					$widgets[ $filter['widget_id'] ]['filters']   = array();
+					$widgets[ $filter['widget_id'] ]['widget_id'] = $filter['widget_id'];
+				}
+				$widgets[ $filter['widget_id'] ]['filters'][] = $filter;
+			} else {
+				if ( ! isset( $widgets_outside_overlay[ $filter['widget_id'] ] ) ) {
+					$widgets_outside_overlay[ $filter['widget_id'] ]['filters']   = array();
+					$widgets_outside_overlay[ $filter['widget_id'] ]['widget_id'] = $filter['widget_id'];
+				}
+				$widgets_outside_overlay[ $filter['widget_id'] ]['filters'][] = $filter;
 			}
-			$new_filter                                   = $filter;
-			$new_filter['filter_id']                      = $key;
-			$widgets[ $filter['widget_id'] ]['filters'][] = $new_filter;
 		}
+		unset( $filter );
 
 		$post_type_objs   = get_post_types( array(), 'objects' );
 		$post_type_labels = array();
@@ -108,7 +118,7 @@ class Jetpack_Instant_Search extends Jetpack_Search {
 
 		$prefix  = Jetpack_Search_Options::OPTION_PREFIX;
 		$options = array(
-			'overlayOptions'  => array(
+			'overlayOptions'        => array(
 				'colorTheme'      => get_option( $prefix . 'color_theme', 'light' ),
 				'enableInfScroll' => (bool) get_option( $prefix . 'inf_scroll', false ),
 				'highlightColor'  => get_option( $prefix . 'highlight_color', '#FFC' ),
@@ -117,16 +127,17 @@ class Jetpack_Instant_Search extends Jetpack_Search {
 			),
 
 			// core config.
-			'homeUrl'         => home_url(),
-			'locale'          => str_replace( '_', '-', Jetpack_Search_Helpers::is_valid_locale( get_locale() ) ? get_locale() : 'en_US' ),
-			'postsPerPage'    => get_option( 'posts_per_page' ),
-			'siteId'          => Jetpack::get_option( 'id' ),
+			'homeUrl'               => home_url(),
+			'locale'                => str_replace( '_', '-', Jetpack_Search_Helpers::is_valid_locale( get_locale() ) ? get_locale() : 'en_US' ),
+			'postsPerPage'          => get_option( 'posts_per_page' ),
+			'siteId'                => Jetpack::get_option( 'id' ),
 
 			// filtering.
-			'postTypeFilters' => isset( $widget_options['post_types'] ) ? $widget_options['post_types'] : array(),
-			'postTypes'       => $post_type_labels,
-			'sort'            => isset( $widget_options['sort'] ) ? $widget_options['sort'] : null,
-			'widgets'         => array_values( $widgets ),
+			'postTypeFilters'       => isset( $widget_options['post_types'] ) ? $widget_options['post_types'] : array(),
+			'postTypes'             => $post_type_labels,
+			'sort'                  => isset( $widget_options['sort'] ) ? $widget_options['sort'] : null,
+			'widgets'               => array_values( $widgets ),
+			'widgetsOutsideOverlay' => array_values( $widgets_outside_overlay ),
 		);
 
 		/**

modules/search/instant-search/components/preselected-search-filters.jsx
@@ -1,0 +1,31 @@
+/** @jsx h */
+
+/**
+ * External dependencies
+ */
+import { h } from 'preact';
+
+/**
+ * Internal dependencies
+ */
+import { getPreselectedFilters } from '../lib/query-string';
+import SearchFilters from './search-filters';
+
+const PreselectedSearchFilters = props => {
+	const preselectedFilters = getPreselectedFilters( props.widgets, props.widgetsOutsideOverlay );
+
+	return (
+		<div className="jetpack-instant-search__testing">
+			{ preselectedFilters.length > 0 && (
+				<SearchFilters
+					loading={ props.isLoading }
+					locale={ props.locale }
+					postTypes={ props.postTypes }
+					results={ props.results }
+					widget={ { filters: preselectedFilters } }
+				/>
+			) }
+		</div>
+	);
+};
+export default PreselectedSearchFilters;

modules/search/instant-search/components/search-app.jsx
@@ -272,6 +272,7 @@ class SearchApp extends Component {
 					resultFormat={ getResultFormatQuery() }
 					showPoweredBy={ this.state.overlayOptions.showPoweredBy }
 					widgets={ this.props.options.widgets }
+					widgetsOutsideOverlay={ this.props.options.widgetsOutsideOverlay }
 				/>
 			</Overlay>,
 			document.body

modules/search/instant-search/components/search-filter.jsx
@@ -28,9 +28,6 @@ function getDateOptions( interval ) {
 // TODO: Fix this in the API
 // TODO: Remove once format is fixed in the API
 function fixDateFormat( dateString ) {
-	if ( dateString[ dateString.length - 1 ] !== 'Z' ) {
-		dateString += 'Z';
-	}
 	return dateString.split( ' ' ).join( 'T' );
 }
 
@@ -77,7 +74,7 @@ export default class SearchFilter extends Component {
 					{ new Date( fixDateFormat( key ) ).toLocaleString(
 						locale,
 						getDateOptions( this.props.configuration.interval )
-					) }{' '}
+					) }{ ' ' }
 					({ count })
 				</label>
 			</div>

modules/search/instant-search/components/search-filters.jsx
@@ -15,6 +15,7 @@ import get from 'lodash/get';
  */
 import SearchFilter from './search-filter';
 import { setFilterQuery, getFilterQuery, clearFiltersFromQuery } from '../lib/query-string';
+import { mapFilterToFilterKey, mapFilterToType } from '../lib/filters';
 
 export default class SearchFilters extends Component {
 	onChangeFilter = ( filterName, filterValue ) => {
@@ -44,48 +45,18 @@ export default class SearchFilters extends Component {
 		return getFilterQuery();
 	}
 
-	renderFilterComponent = ( { configuration, results } ) => {
-		switch ( configuration.type ) {
-			case 'date_histogram':
-				return (
-					results && (
-						<SearchFilter
-							aggregation={ results }
-							configuration={ configuration }
-							locale={ this.props.locale }
-							type="date"
-							value={ this.getFilters()[ `${ configuration.interval }_${ configuration.field }` ] }
-							onChange={ this.onChangeFilter }
-						/>
-					)
-				);
-			case 'taxonomy':
-				return (
-					results && (
-						<SearchFilter
-							aggregation={ results }
-							configuration={ configuration }
-							value={ this.getFilters()[ configuration.taxonomy ] }
-							onChange={ this.onChangeFilter }
-							type="taxonomy"
-						/>
-					)
-				);
-			case 'post_type':
-				return (
-					results && (
-						<SearchFilter
-							aggregation={ results }
-							configuration={ configuration }
-							value={ this.getFilters().post_types }
-							onChange={ this.onChangeFilter }
-							postTypes={ this.props.postTypes }
-							type="postType"
-						/>
-					)
-				);
-		}
-	};
+	renderFilterComponent = ( { configuration, results } ) =>
+		results && (
+			<SearchFilter
+				aggregation={ results }
+				configuration={ configuration }
+				locale={ this.props.locale }
+				onChange={ this.onChangeFilter }
+				postTypes={ this.props.postTypes }
+				type={ mapFilterToType( configuration ) }
+				value={ this.getFilters()[ mapFilterToFilterKey( configuration ) ] }
+			/>
+		);
 
 	render() {
 		if ( ! this.props.widget ) {

modules/search/instant-search/components/search-form.jsx
@@ -13,6 +13,7 @@ import SearchBox from './search-box';
 import SearchFilters from './search-filters';
 
 import { getFilterQuery, getSearchQuery, setSearchQuery, setSortQuery } from '../lib/query-string';
+import PreselectedSearchFilters from './preselected-search-filters';
 
 const noop = event => event.preventDefault();
 
@@ -58,6 +59,14 @@ class SearchForm extends Component {
 				{ this.state.showFilters && (
 					<div className="jetpack-instant-search__search-form-filters">
 						<div className="jetpack-instant-search__search-form-filters-arrow" />
+						<PreselectedSearchFilters
+							loading={ this.props.isLoading }
+							locale={ this.props.locale }
+							postTypes={ this.props.postTypes }
+							results={ this.props.response }
+							widgets={ this.props.widgets }
+							widgetsOutsideOverlay={ this.props.widgetsOutsideOverlay }
+						/>
 						{ this.props.widgets.map( widget => (
 							<SearchFilters
 								filters={ getFilterQuery() }

modules/search/instant-search/components/search-results.jsx
@@ -67,6 +67,7 @@ class SearchResults extends Component {
 					postTypes={ this.props.postTypes }
 					response={ this.props.response }
 					widgets={ this.props.widgets }
+					widgetsOutsideOverlay={ this.props.widgetsOutsideOverlay }
 				/>
 
 				<div
@@ -137,6 +138,7 @@ class SearchResults extends Component {
 				response={ this.props.response }
 				showPoweredBy={ this.props.showPoweredBy }
 				widgets={ this.props.widgets }
+				widgetsOutsideOverlay={ this.props.widgetsOutsideOverlay }
 			/>
 		);
 	}

commit f7a1a5b7973efaf29e566e2476d27b4a6cfdfef1

Instant Search: Hotfix taxonomy filter bug
Introduced by #14834.

modules/search/instant-search/lib/filters.js
@@ -56,7 +56,7 @@ export function mapFilterToFilterKey( filter ) {
 	if ( filter.type === 'date_histogram' ) {
 		return `${ filter.interval }_${ filter.field }`;
 	} else if ( filter.type === 'taxonomy' ) {
-		return 'taxonomy';
+		return `${ filter.taxonomy }`;
 	} else if ( filter.type === 'post_type' ) {
 		return 'post_types';
 	}