d72ba80c3771748a99095fe05d6d5548d9aebcb6
Brandon Kraft <public@brandonkraft.com>
2020-03-05 10:02:00 -0600

    Correct version number

--- modules/shortcodes/crowdsignal.php
+++ modules/shortcodes/crowdsignal.php
@@ -446,7 +446,7 @@ if (
 							/**
 							 * Hook into the Crowdsignal shortcode before rendering.
 							 *
-							 * @since 8.3.0
+							 * @since 8.4.0
 							 *
 							 * @param int $poll Poll ID.
 							 */
