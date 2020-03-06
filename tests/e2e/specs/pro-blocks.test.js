4bd4ad5a1baa03127a849e14c6ee73681e7c774c
Glen Davies <glen.davies@a8c.com>
2020-03-06 07:42:01 -0600

    Remove unused import

--- tests/e2e/specs/pro-blocks.test.js
+++ tests/e2e/specs/pro-blocks.test.js
@@ -8,7 +8,6 @@ import { connectThroughWPAdminIfNeeded } from '../lib/flows/jetpack-connect';
 import { resetWordpressInstall, getNgrokSiteUrl, activateModule } from '../lib/utils-helper';
 import SimplePaymentBlock from '../lib/blocks/simple-payments';
 import WordAdsBlock from '../lib/blocks/word-ads';
-import PinterestBlock from '../lib/blocks/pinterest';
 import { catchBeforeAll } from '../lib/jest.test.failure';
 
 describe( 'Paid blocks', () => {
