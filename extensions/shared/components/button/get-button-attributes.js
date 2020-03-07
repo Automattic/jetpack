a645abf3e9dadcd514a49eae94534016db212729
Copons <Copons@users.noreply.github.com>
2020-03-07 15:48:45 -0600

    Document the getButtonAttributes function

--- extensions/shared/components/button/get-button-attributes.js
+++ extensions/shared/components/button/get-button-attributes.js
@@ -3,11 +3,23 @@
  */
 import colorValidator from '../../colorValidator';
 
+/**
+ * Create an object of Button attributes.
+ * The parameter is an optional object, and all its properties are optional as well.
+ *
+ * @param {string} [defaultPlaceholder] The default button placeholder text.
+ * @param {string} [defaultText] The default button text.
+ * @param {boolean} [hasPlaceholder] Whether the button has a `buttonPlaceholder` attribute.
+ * @param {boolean} [hasUrl] Whether the button has a `buttonUrl` attribute.
+ * @param {Function} [urlValidator] A function to validate the `buttonUrl` attribute.
+ * @returns {Object} The button attributes.
+ */
 export default function getButtonAttributes( {
 	defaultPlaceholder,
 	defaultText,
 	hasPlaceholder,
 	hasUrl,
+	urlValidator,
 } = {} ) {
 	return {
 		buttonText: {
@@ -23,6 +35,7 @@ export default function getButtonAttributes( {
 		...( hasUrl && {
 			buttonUrl: {
 				type: 'string',
+				validator: urlValidator,
 			},
 		} ),
 		buttonTextColor: {
