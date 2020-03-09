8862522957e2f2c59c4609456c5c1b27df928fbb
Konstantin Obenland <obenland@gmx.de>
2020-03-09 12:00:38 -0700

    Add proper icon

--- extensions/blocks/podcast-episodes/icon.js
+++ extensions/blocks/podcast-episodes/icon.js
@@ -1,11 +1,22 @@
 /**
  * External dependencies
  */
-import { SVG, Path } from '@wordpress/components';
+import { G, Path, Rect, SVG } from '@wordpress/components';
 
 export default (
-	/* @TODO Add the icon. You can use one of these https://material.io/tools/icons/?style=outline */
 	<SVG height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-		<Path d="M9 15h2V9H9v6zm1-10c-.5 0-1 .5-1 1s.5 1 1 1 1-.5 1-1-.5-1-1-1zm0-4c-5 0-9 4-9 9s4 9 9 9 9-4 9-9-4-9-9-9zm0 16c-3.9 0-7-3.1-7-7s3.1-7 7-7 7 3.1 7 7-3.1 7-7 7z" />
+		<G>
+			<Rect fill="none" height="24" width="24" />
+			<Rect fill="none" height="24" width="24" />
+			<Rect fill="none" height="24" width="24" />
+		</G>
+		<G>
+			<G />
+			<G>
+				<G transform="matrix(1 0 0 1 144 120)">
+					<Path d="M-122-114h-5v8.18c-0.31-0.11-0.65-0.18-1-0.18c-1.66,0-3,1.34-3,3s1.34,3,3,3s3-1.34,3-3v-9h3V-114 L-122-114z M-129-114h-12v2h12V-114L-129-114z M-129-110h-12v2h12V-110L-129-110z M-133-106h-8v2h8V-106L-133-106z M-129-103 c0-0.55,0.45-1,1-1c0.55,0,1,0.45,1,1s-0.45,1-1,1C-128.55-102-129-102.45-129-103z" />
+				</G>
+			</G>
+		</G>
 	</SVG>
 );
