83fd27059577fb806d6bc51f3fe58014e2fe8ff1
Lance Willett <lance@automattic.com>
2020-03-11 15:06:38 -0700

    Fix sprintf replacement

--- modules/shortcodes/inline-pdfs.php
+++ modules/shortcodes/inline-pdfs.php
@@ -9,7 +9,7 @@ wp_embed_register_handler( 'inline-pdfs', '#https?://[^<]*\.pdf$#i', 'inline_pdf
 function inline_pdf_embed_handler( $matches, $attr, $url ) {
 	return sprintf(
 		'<object data="%1$s" type="application/pdf" width="100%%" height="800">
-		  <p><a href="%1$s">%2$s</a></p>
+		  <p><a href="%1$s">%1$s</a></p>
 		</object>',
 		esc_attr( $url )
 	);
