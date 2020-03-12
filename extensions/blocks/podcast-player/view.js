c7ccf290c247b34b934e8e230e8d21988637023b
Marek Hrabe <marekhrabe@me.com>
2020-03-12 18:27:53 +0100

    add extremely simple audio playlist handling

--- extensions/blocks/podcast-player/view.js
+++ extensions/blocks/podcast-player/view.js
@@ -2,3 +2,22 @@
  * Internal dependencies
  */
 import './style.scss';
+
+window.addEventListener( 'click', function( e ) {
+	// Prevent handling clicks if a modifier is in use.
+	if ( e.shiftKey || e.metaKey || e.altKey ) {
+		return;
+	}
+
+	// Check if the clicked element was episode link.
+	const audioUrl = e.target.getAttribute( 'data-podcast-audio' );
+	if ( audioUrl ) {
+		const block = e.target.closest( '.wp-block-jetpack-podcast-player' );
+		const audioPlayer = block && block.querySelector( 'audio' );
+		if ( audioPlayer ) {
+			audioPlayer.src = audioUrl;
+			audioPlayer.play();
+			e.preventDefault();
+		}
+	}
+} );
