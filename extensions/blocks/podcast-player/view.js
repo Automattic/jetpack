c0e7cc91149adb36bb21c1daf3a9e1589f2eae76
Marek Hrabe <marekhrabe@me.com>
2020-03-13 11:21:21 +0100

    add player registration handling, basic player state

--- extensions/blocks/podcast-player/view.js
+++ extensions/blocks/podcast-player/view.js
@@ -1,8 +1,46 @@
+/* global _wpmejsSettings, MediaElementPlayer, jetpackPodcastPlayers */
 /**
  * Internal dependencies
  */
 import './style.scss';
 
+const playerInstances = {};
+const meJsSettings = typeof _wpmejsSettings !== undefined ? _wpmejsSettings : {};
+
+const initializeBlock = function( id ) {
+	const block = document.getElementById( id );
+	if ( block ) {
+		const player = {
+			id,
+			block,
+			audio: block.querySelector( 'audio' ),
+			currentTrack: 0,
+		};
+		playerInstances[ id ] = player;
+
+		// Initialize player UI.
+		player.mediaElement = new MediaElementPlayer( player.audio, meJsSettings );
+
+		// // Load track data.
+		// try {
+		// 	player.meta = block.querySelector( 'script[type="application/json"]' ).innerHTML;
+		// } catch ( e ) {
+		// 	player.meta = {};
+		// }
+	}
+};
+
+// Initialze queued players.
+if ( window.jetpackPodcastPlayers !== undefined ) {
+	jetpackPodcastPlayers.forEach( initializeBlock );
+}
+
+// Replace the queue with an immediate initialization for async loaded players.
+window.jetpackPodcastPlayers = {
+	push: initializeBlock,
+};
+
+// Add global handler for clicks.
 window.addEventListener( 'click', function( e ) {
 	// Prevent handling clicks if a modifier is in use.
 	if ( e.shiftKey || e.metaKey || e.altKey ) {
@@ -13,10 +51,11 @@ window.addEventListener( 'click', function( e ) {
 	const audioUrl = e.target.getAttribute( 'data-podcast-audio' );
 	if ( audioUrl ) {
 		const block = e.target.closest( '.wp-block-jetpack-podcast-player' );
-		const audioPlayer = block && block.querySelector( 'audio' );
-		if ( audioPlayer ) {
-			audioPlayer.src = audioUrl;
-			audioPlayer.play();
+		const player = playerInstances[ block.id ];
+		if ( player ) {
+			player.audio.pause();
+			player.audio.src = audioUrl;
+			player.audio.play();
 			e.preventDefault();
 		}
 	}
