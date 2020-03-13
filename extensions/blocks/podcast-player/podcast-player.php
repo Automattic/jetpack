af90a04e4cee0f8b84fe65a9ef720ddbbfac291e
Marek Hrabe <marekhrabe@me.com>
2020-03-13 09:49:17 +0100

    remove unused variables

--- extensions/blocks/podcast-player/podcast-player.php
+++ extensions/blocks/podcast-player/podcast-player.php
@@ -107,7 +107,7 @@ function render_player( $track_list, $attributes ) {
 	?>
 	<div class="<?php echo esc_attr( $block_classname ); ?>">
 		<div class="<?php echo esc_attr( $block_classname ); ?>-current-item"></div>
-		<audio src="<?php echo esc_attr( $initial_track_src ); ?>" controls="controls" preload="none" width="<?php echo esc_attr( (int) $theme_width ); ?>"></audio>
+		<audio src="<?php echo esc_attr( $initial_track_src ); ?>" controls="controls" preload="none"></audio>
 		<div class="<?php echo esc_attr( $block_classname ); ?>-next"></div>
 		<div class="<?php echo esc_attr( $block_classname ); ?>-prev"></div>
 		<ol>
