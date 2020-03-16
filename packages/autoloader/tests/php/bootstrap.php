3b71256ab836f8799666af44987c52654f90ea6a
Igor Zinovyev <zinigor@gmail.com>
2020-03-16 11:01:38 +0300

    Find the latest autoloader on first load. (#14992)
    * [not verified] Autoloader: add the set_up_autoloader function.
    
    * [not verified] Autoloader: update global array names and remove spl_autoload_register
    
    Update the global classmap and filemap array names so the old autoloaders
    can't overwrite the new autoloader's version selections.
    
    Remove the call to spl_autoload_register(), because that it now handled
    in set_up_autoloader().
    
    * [not verified] Moved the autoloader functions into a separate include.
    
    * Autoloader: remove setup() from test_file_loader.php
    
    The spl_autoload_register() call isn't needed in setup() and the
    setup() method doesn't do anything else, so remove the entire setup()
    method.
    
    Co-authored-by: Kim Brown <50059399+kbrown9@users.noreply.github.com>

--- packages/autoloader/tests/php/bootstrap.php
+++ packages/autoloader/tests/php/bootstrap.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * Bootstrap file for the autoloader test suite.
+ *
+ * @package automattic/jetpack-autoloader
+ */
+
+if ( ! function_exists( 'trailingslashit' ) ) {
+
+	/**
+	 * A drop-in for a WordPress core function.
+	 *
+	 * @param String $string string.
+	 * @return String
+	 */
+	function trailingslashit( $string ) {
+		return rtrim( $string, '/\\' ) . '/';
+	}
+}
 
 require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/../../src/autoload.php';
+require_once __DIR__ . '/../../src/functions.php';
