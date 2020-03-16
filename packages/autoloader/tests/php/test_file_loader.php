1ce55004359e7a315c9850d60d9a60d82ce33efb
Igor Zinovyev <zinigor@gmail.com>
2020-03-16 11:01:38 +0300

    Find the latest autoloader on first load. (#14992)
    * [not verified] Autoloader: add the set_up_autoloader function.
    
    * [not verified] Autoloader: update global array names and remove spl_autoload_register
    
    Update the global classmap and filemap array names so the old autoloaders
    can't overwrite the new autoloader's version selections.
    
    Remove the call to spl_autoload_register(), because that it now handled
    in set_up_autoloader().
    
    * [not verified] Moved the autoloader functions into a separate include.
    
    * Autoloader: remove setup() from test_file_loader.php
    
    The spl_autoload_register() call isn't needed in setup() and the
    setup() method doesn't do anything else, so remove the entire setup()
    method.
    
    Co-authored-by: Kim Brown <50059399+kbrown9@users.noreply.github.com>

--- packages/autoloader/tests/php/test_file_loader.php
+++ packages/autoloader/tests/php/test_file_loader.php
@@ -1,66 +1,71 @@
-<?php
+<?php // phpcs:ignore WordPress.Files.FileName.NotHyphenatedLowerCase
+/**
+ * File loader test suite.
+ *
+ * @package automattic/jetpack-autoloader
+ */
 
-use Automattic\Jetpack\Autoloader as Autoloader;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Test suite class for the Autoloader part that handles file loading.
+ */
 class WP_Test_File_Loader extends TestCase {
-	static $jetpack_packages_files;
-	function setup() {
-		parent::setup();
-		global $jetpack_packages_files;
-		self::$jetpack_packages_files = $jetpack_packages_files;
-		$jetpack_packages_files = array();
+
+	/**
+	 * Tests whether enqueueing adds a file to the global array.
+	 */
+	public function test_enqueueing_adds_to_the_global_array() {
+		enqueue_package_file( 'file_id_10', '1', 'path_to_file.php' );
+
+		global $jetpack_packages_filemap;
+		$this->assertTrue( isset( $jetpack_packages_filemap['file_id_10'] ) );
+		$this->assertEquals( $jetpack_packages_filemap['file_id_10']['version'], '1' );
+		$this->assertEquals( $jetpack_packages_filemap['file_id_10']['path'], 'path_to_file.php' );
 	}
 
-	function tearDown() {
-		parent::tearDown();
-		// re-apply the global
-		global $jetpack_packages_files;
-		$jetpack_packages_files = self::$jetpack_packages_files;
+	/**
+	 * Tests whether enqueueing adds the latest file version to the global array.
+	 */
+	public function test_enqueueing_adds_the_latest_version_to_the_global_array() {
+		enqueue_package_file( 'file_id', '1', 'path_to_file' );
+		enqueue_package_file( 'file_id', '2', 'path_to_file_v2' );
+
+		global $jetpack_packages_filemap;
+		$this->assertTrue( isset( $jetpack_packages_filemap['file_id'] ) );
+		$this->assertEquals( $jetpack_packages_filemap['file_id']['version'], '2' );
+		$this->assertEquals( $jetpack_packages_filemap['file_id']['path'], 'path_to_file_v2' );
 	}
 
-	function test_enqueueing_adds_to_the_global_array() {
-		Autoloader\enqueue_package_file( 'file_id_10', '1', 'path_to_file.php' );
+	/**
+	 * Tests whether enqueueing prioritizes the dev version of the file.
+	 */
+	public function test_enqueueing_always_adds_the_dev_version_to_the_global_array() {
 
-		global $jetpack_packages_files;
-		$this->assertTrue( isset( $jetpack_packages_files['file_id_10'] ) );
-		$this->assertEquals( $jetpack_packages_files['file_id_10']['version'], '1' );
-		$this->assertEquals( $jetpack_packages_files['file_id_10']['path'], 'path_to_file.php' );
+		enqueue_package_file( 'file_id', '1', 'path_to_file' );
+		enqueue_package_file( 'file_id', 'dev-howdy', 'path_to_file_dev' );
+		enqueue_package_file( 'file_id', '2', 'path_to_file_v2' );
+
+		global $jetpack_packages_filemap;
+		$this->assertTrue( isset( $jetpack_packages_filemap['file_id'] ) );
+		$this->assertEquals( $jetpack_packages_filemap['file_id']['version'], 'dev-howdy' );
+		$this->assertEquals( $jetpack_packages_filemap['file_id']['path'], 'path_to_file_dev' );
 	}
 
-	function test_enqueueing_adds_the_latest_version_to_the_global_array() {
-		Autoloader\enqueue_package_file( 'file_id', '1', 'path_to_file' );
-		Autoloader\enqueue_package_file( 'file_id', '2', 'path_to_file_v2' );
+	/**
+	 * Tests whether enqueueing works with autoloading.
+	 */
+	public function test_enqueued_file_is_actually_loaded() {
 
-		global $jetpack_packages_files;
-		$this->assertTrue( isset( $jetpack_packages_files['file_id'] ) );
-		$this->assertEquals( $jetpack_packages_files['file_id']['version'], '2' );
-		$this->assertEquals( $jetpack_packages_files['file_id']['path'], 'path_to_file_v2' );
-	}
+		enqueue_package_file( 'file_id', '1', __DIR__ . '/path_to_file.php' );
 
-	function test_enqueueing_always_adds_the_dev_version_to_the_global_array() {
-
-		Autoloader\enqueue_package_file( 'file_id', '1', 'path_to_file' );
-		Autoloader\enqueue_package_file( 'file_id', 'dev-howdy', 'path_to_file_dev' );
-		Autoloader\enqueue_package_file( 'file_id', '2', 'path_to_file_v2' );
-
-		global $jetpack_packages_files;
-		$this->assertTrue( isset( $jetpack_packages_files['file_id'] ) );
-		$this->assertEquals( $jetpack_packages_files['file_id']['version'], 'dev-howdy' );
-		$this->assertEquals( $jetpack_packages_files['file_id']['path'], 'path_to_file_dev' );
-	}
-
-	function test_enqueued_file_is_actually_loaded() {
-
-		Autoloader\enqueue_package_file( 'file_id', '1', __DIR__ . '/path_to_file.php' );
-
-		Autoloader\file_loader();
+		file_loader();
 		$this->assertTrue( function_exists( 'if_i_exist_then_this_test_passed' ) );
 		$this->assertTrue( if_i_exist_then_this_test_passed() );
 
-		Autoloader\enqueue_package_file( 'file_id', '2', __DIR__ . '/bogus_path_to_file.php' );
+		enqueue_package_file( 'file_id', '2', __DIR__ . '/bogus_path_to_file.php' );
 
-		Autoloader\file_loader(); // file_loader should not include same file twice.
+		file_loader(); // file_loader should not include same file twice.
 
 		$this->assertTrue( if_i_exist_then_this_test_passed() );
 	}
