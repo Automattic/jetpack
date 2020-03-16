d6eab5edbf4900d8facbc4818ba7de3f2a1fce35
Igor Zinovyev <zinigor@gmail.com>
2020-03-16 11:01:38 +0300

    Find the latest autoloader on first load. (#14992)
    * [not verified] Autoloader: add the set_up_autoloader function.
    
    * [not verified] Autoloader: update global array names and remove spl_autoload_register
    
    Update the global classmap and filemap array names so the old autoloaders
    can't overwrite the new autoloader's version selections.
    
    Remove the call to spl_autoload_register(), because that it now handled
    in set_up_autoloader().
    
    * [not verified] Moved the autoloader functions into a separate include.
    
    * Autoloader: remove setup() from test_file_loader.php
    
    The spl_autoload_register() call isn't needed in setup() and the
    setup() method doesn't do anything else, so remove the entire setup()
    method.
    
    Co-authored-by: Kim Brown <50059399+kbrown9@users.noreply.github.com>

--- packages/autoloader/tests/php/test_Autoloader.php
+++ packages/autoloader/tests/php/test_Autoloader.php
@@ -1,91 +1,73 @@
 <?php // phpcs:ignore WordPress.Files.FileName
 /**
- * Testing file for the autoloader.
+ * Autoloader test suite.
  *
  * @package automattic/jetpack-autoloader
  */
 
-use Automattic\Jetpack\Autoloader as Autoloader;
 use PHPUnit\Framework\TestCase;
 use Jetpack\TestCase_ABC\ClassName_ABC;
 
 /**
- * Class WP_Test_Autoloader
+ * Test suite class for the Autoloader.
  */
 class WP_Test_Autoloader extends TestCase {
-	/**
-	 * Jetpack package classes.
-	 *
-	 * @var $jetpack_packages_classes
-	 */
-	public static $jetpack_packages_classes;
 
 	/**
-	 * Test setup.
+	 * Setup runs before each test.
 	 */
 	public function setup() {
 		parent::setup();
-		global $jetpack_packages_classes;
-		self::$jetpack_packages_classes = $jetpack_packages_classes;
-		$jetpack_packages_classes       = array();
+
+		spl_autoload_register( 'autoloader' );
 	}
 
 	/**
-	 * Test tear down.
-	 */
-	public function tearDown() {
-		parent::tearDown();
-		// re-apply the global.
-		global $jetpack_packages_classes;
-		$jetpack_packages_classes = self::$jetpack_packages_classes;
-	}
-
-	/**
-	 * Ensure enqueuing adds to the global array.
+	 * Tests whether enqueueing adds a class to the global array.
 	 */
 	public function test_enqueueing_adds_to_the_global_array() {
-		Autoloader\enqueue_package_class( 'className', '1', 'path_to_class' );
+		enqueue_package_class( 'className', '1', 'path_to_class' );
 
-		global $jetpack_packages_classes;
-		$this->assertTrue( isset( $jetpack_packages_classes['className'] ) );
-		$this->assertEquals( $jetpack_packages_classes['className']['version'], '1' );
-		$this->assertEquals( $jetpack_packages_classes['className']['path'], 'path_to_class' );
+		global $jetpack_packages_classmap;
+		$this->assertTrue( isset( $jetpack_packages_classmap['className'] ) );
+		$this->assertEquals( $jetpack_packages_classmap['className']['version'], '1' );
+		$this->assertEquals( $jetpack_packages_classmap['className']['path'], 'path_to_class' );
 	}
 
 	/**
-	 * Tests that the latest version is added to the global array.
+	 * Tests whether enqueueing adds the latest class version to the global array.
 	 */
 	public function test_enqueueing_adds_the_latest_version_to_the_global_array() {
-		Autoloader\enqueue_package_class( 'className', '1', 'path_to_class' );
-		Autoloader\enqueue_package_class( 'className', '2', 'path_to_class_v2' );
+		enqueue_package_class( 'className', '1', 'path_to_class' );
+		enqueue_package_class( 'className', '2', 'path_to_class_v2' );
 
-		global $jetpack_packages_classes;
-		$this->assertTrue( isset( $jetpack_packages_classes['className'] ) );
-		$this->assertEquals( $jetpack_packages_classes['className']['version'], '2' );
-		$this->assertEquals( $jetpack_packages_classes['className']['path'], 'path_to_class_v2' );
+		global $jetpack_packages_classmap;
+		$this->assertTrue( isset( $jetpack_packages_classmap['className'] ) );
+		$this->assertEquals( $jetpack_packages_classmap['className']['version'], '2' );
+		$this->assertEquals( $jetpack_packages_classmap['className']['path'], 'path_to_class_v2' );
 
 	}
 
 	/**
-	 * Tests that the dev version is added to the global array.
+	 * Tests whether enqueueing prioritizes the dev version of the class.
 	 */
 	public function test_enqueueing_always_adds_the_dev_version_to_the_global_array() {
 
-		Autoloader\enqueue_package_class( 'className', '1', 'path_to_class' );
-		Autoloader\enqueue_package_class( 'className', 'dev-howdy', 'path_to_class_dev' );
-		Autoloader\enqueue_package_class( 'className', '2', 'path_to_class_v2' );
+		enqueue_package_class( 'className', '1', 'path_to_class' );
+		enqueue_package_class( 'className', 'dev-howdy', 'path_to_class_dev' );
+		enqueue_package_class( 'className', '2', 'path_to_class_v2' );
 
-		global $jetpack_packages_classes;
-		$this->assertTrue( isset( $jetpack_packages_classes['className'] ) );
-		$this->assertEquals( $jetpack_packages_classes['className']['version'], 'dev-howdy' );
-		$this->assertEquals( $jetpack_packages_classes['className']['path'], 'path_to_class_dev' );
+		global $jetpack_packages_classmap;
+		$this->assertTrue( isset( $jetpack_packages_classmap['className'] ) );
+		$this->assertEquals( $jetpack_packages_classmap['className']['version'], 'dev-howdy' );
+		$this->assertEquals( $jetpack_packages_classmap['className']['path'], 'path_to_class_dev' );
 	}
 
 	/**
-	 * Ensures that an autoloaded class is available.
+	 * Tests whether enqueueing works with autoloading.
 	 */
 	public function test_enqueue_class_to_autoload_works_as_expected() {
-		Autoloader\enqueue_package_class( 'Jetpack\TestCase_ABC\ClassName_ABC', '1', dirname( __FILE__ ) . '/path_to_class.php' );
+		enqueue_package_class( 'Jetpack\TestCase_ABC\className_ABC', '1', dirname( __FILE__ ) . '/path_to_class.php' );
 
 		$class = new ClassName_ABC();
 
