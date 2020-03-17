1d1d65130c7242a269981fc742aca1d7bf5adf6e
Kim Brown <50059399+kbrown9@users.noreply.github.com>
2020-03-16 23:54:00 -0400

    Autoloader: remove plugins_loaded action conditional around file_loader
    We no longer need to wait until the plugins_loaded action to call file_loader(),
    so remove the conditionals and the plugins_loaded hook.

--- packages/autoloader/src/functions.php
+++ packages/autoloader/src/functions.php
@@ -164,20 +164,7 @@ function enqueue_files() {
 		}
 	}
 
-	// TODO: The plugins_loaded action checks aren't necessary anymore.
-	if (
-		function_exists( 'has_action' )
-		&& function_exists( 'did_action' )
-		&& ! did_action( 'plugins_loaded' )
-		&& false === has_action( 'plugins_loaded', __NAMESPACE__ . '\file_loader' )
-	) {
-		// Add action if it has not been added and has not happened yet.
-		// Priority -10 to load files as early as possible in case plugins try to use them during `plugins_loaded`.
-		add_action( 'plugins_loaded', __NAMESPACE__ . '\file_loader', 0, -10 );
-
-	} elseif ( ! function_exists( 'did_action' ) || did_action( 'plugins_loaded' ) ) {
-		file_loader(); // Either WordPress is not loaded or plugin is doing it wrong. Either way we'll load the files so nothing breaks.
-	}
+	file_loader();
 }
 
 /**
