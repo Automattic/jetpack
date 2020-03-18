c16345101c1f96c216634b8f8b2502989e6a8fd9
Andy Peatling <apeatling@users.noreply.github.com>
2020-03-18 04:33:07 -0700

    Google Calendar block: Fix full width support (#14835)
    Make sure that the Google Calendar block widths match in the editor and front end, and that full and wide widths are supported correctly.
    
    Co-authored-by: Copons <Copons@users.noreply.github.com>

--- extensions/blocks/google-calendar/google-calendar.php
+++ extensions/blocks/google-calendar/google-calendar.php
@@ -35,12 +35,13 @@ add_action( 'init', 'Jetpack\Google_Calendar_Block\register_block' );
  * @return string
  */
 function load_assets( $attr ) {
-	$width   = isset( $attr['width'] ) ? $attr['width'] : '800';
 	$height  = isset( $attr['height'] ) ? $attr['height'] : '600';
 	$url     = isset( $attr['url'] )
 		? \Jetpack_Gutenberg::validate_block_embed_url( $attr['url'], array( 'calendar.google.com' ) ) :
 		'';
 	$classes = \Jetpack_Gutenberg::block_classes( 'google-calendar', $attr );
+
+	\Jetpack_Gutenberg::load_assets_as_required( 'google-calendar' );
 
 	if ( empty( $url ) ) {
 		return;
@@ -48,18 +49,16 @@ function load_assets( $attr ) {
 
 	if ( class_exists( 'Jetpack_AMP_Support' ) && \Jetpack_AMP_Support::is_amp_request() ) {
 		return sprintf(
-			'<div class="%1$s"><amp-iframe src="%2$s" frameborder="0" style="border:0" scrolling="no" width="%3$d" height="%4$d" sandbox="allow-scripts allow-same-origin" layout="responsive"></amp-iframe></div>',
+			'<div class="%1$s"><amp-iframe src="%2$s" frameborder="0" style="border:0" scrolling="no" height="%3$d" sandbox="allow-scripts allow-same-origin" layout="responsive"></amp-iframe></div>',
 			esc_attr( $classes ),
 			esc_url( $url ),
-			absint( $width ),
 			absint( $height )
 		);
 	} else {
 		return sprintf(
-			'<div class="%1$s"><iframe src="%2$s" frameborder="0" style="border:0" scrolling="no" width="%3$d" height="%4$d"></iframe></div>',
+			'<div class="%1$s"><iframe src="%2$s" frameborder="0" style="border:0" scrolling="no" height="%3$d"></iframe></div>',
 			esc_attr( $classes ),
 			esc_url( $url ),
-			absint( $width ),
 			absint( $height )
 		);
 	}
