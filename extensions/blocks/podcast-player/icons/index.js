1803897d436333d6120686cb817c1dccf5782810
Bart <bartlomiej.kalisz@gmail.com>
2020-03-24 16:36:36 +0100

    Add icon elements to be used with React

--- extensions/blocks/podcast-player/icons/index.js
+++ extensions/blocks/podcast-player/icons/index.js
@@ -2,3 +2,5 @@
  * Internal dependency
  */
 export { default as queueMusic } from './queueMusic';
+export { default as episodePlayingIcon } from './episode-playing-icon';
+export { default as episodeErrorIcon } from './episode-error-icon';
