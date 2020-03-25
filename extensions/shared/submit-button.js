9df8f56eed04196a451351002a5f650ca0e7dc4c
apeatling <apeatling@gmail.com>
2020-03-24 20:01:53 -0700

    Add support for gradients, border radius, and font size to the shared submit button component. This will need some retrofitting to other blocks that are using it. Props Jacopo for much of the original work for this in the Revue block.

--- extensions/shared/submit-button.js
+++ extensions/shared/submit-button.js
@@ -1,138 +1,191 @@
+// @see https://github.com/WordPress/gutenberg/blob/015555fcdf648b13af57e08cee60bf3f3501ff63/packages/block-library/src/button/edit.js
 /**
  * External dependencies
  */
-import { __ } from '@wordpress/i18n';
 import classnames from 'classnames';
-import { Component, Fragment } from '@wordpress/element';
-import { compose } from '@wordpress/compose';
-import { withFallbackStyles } from '@wordpress/components';
+
+/**
+ * WordPress dependencies
+ */
 import {
+	ContrastChecker,
 	InspectorControls,
 	PanelColorSettings,
-	ContrastChecker,
 	RichText,
 	withColors,
+	FontSizePicker,
+	withFontSizes,
+	__experimentalPanelColorGradientSettings as PanelColorGradientSettings,
+	__experimentalUseGradient as useGradient,
 } from '@wordpress/block-editor';
-import { isEqual, get } from 'lodash';
+import { PanelBody, RangeControl, withFallbackStyles } from '@wordpress/components';
+import { compose } from '@wordpress/compose';
+import { useCallback } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
 
 const { getComputedStyle } = window;
 
+const isGradientAvailable = !! useGradient;
+
 const applyFallbackStyles = withFallbackStyles( ( node, ownProps ) => {
-	const { textButtonColor, backgroundButtonColor } = ownProps;
-	const backgroundColorValue = backgroundButtonColor && backgroundButtonColor.color;
-	const textColorValue = textButtonColor && textButtonColor.color;
+	const { textColor, backgroundColor } = ownProps;
+	const backgroundColorValue = backgroundColor && backgroundColor.color;
+	const textColorValue = textColor && textColor.color;
 	//avoid the use of querySelector if textColor color is known and verify if node is available.
-
-	let textNode;
-	let button;
-
-	if ( ! textColorValue && node ) {
-		textNode = node.querySelector( '[contenteditable="true"]' );
-	}
-
-	if ( node.querySelector( '.wp-block-button__link' ) ) {
-		button = node.querySelector( '.wp-block-button__link' );
-	} else {
-		button = node;
-	}
-
-	let fallbackBackgroundColor;
-	let fallbackTextColor;
-
-	if ( node ) {
-		fallbackBackgroundColor = getComputedStyle( button ).backgroundColor;
-	}
-
-	if ( textNode ) {
-		fallbackTextColor = getComputedStyle( textNode ).color;
-	}
-
+	const textNode =
+		! textColorValue && node ? node.querySelector( '[contenteditable="true"]' ) : null;
 	return {
-		fallbackBackgroundColor: backgroundColorValue || fallbackBackgroundColor,
-		fallbackTextColor: textColorValue || fallbackTextColor,
+		fallbackBackgroundColor:
+			backgroundColorValue || ! node ? undefined : getComputedStyle( node ).backgroundColor,
+		fallbackTextColor:
+			textColorValue || ! textNode ? undefined : getComputedStyle( textNode ).color,
 	};
 } );
 
-class SubmitButton extends Component {
-	componentDidUpdate( prevProps ) {
-		if (
-			! isEqual( this.props.textButtonColor, prevProps.textButtonColor ) ||
-			! isEqual( this.props.backgroundButtonColor, prevProps.backgroundButtonColor )
-		) {
-			const buttonClasses = this.getButtonClasses();
-			this.props.setAttributes( { submitButtonClasses: buttonClasses } );
-		}
-	}
-	getButtonClasses() {
-		const { textButtonColor, backgroundButtonColor } = this.props;
-		const textClass = get( textButtonColor, 'class' );
-		const backgroundClass = get( backgroundButtonColor, 'class' );
-		return classnames( 'wp-block-button__link', {
-			'has-text-color': textButtonColor.color,
-			[ textClass ]: textClass,
-			'has-background': backgroundButtonColor.color,
-			[ backgroundClass ]: backgroundClass,
-		} );
-	}
-	render() {
-		const {
-			attributes,
-			backgroundButtonColor,
-			textButtonColor,
-			fallbackBackgroundColor,
-			fallbackTextColor,
-			setAttributes,
-			setBackgroundButtonColor,
-			setTextButtonColor,
-		} = this.props;
+const MIN_BORDER_RADIUS_VALUE = 0;
+const MAX_BORDER_RADIUS_VALUE = 50;
+const INITIAL_BORDER_RADIUS_POSITION = 5;
 
-		const backgroundColor = backgroundButtonColor.color || fallbackBackgroundColor;
-		const color = textButtonColor.color || fallbackTextColor;
-		const buttonStyle = { border: 'none', backgroundColor, color };
-		const buttonClasses = this.getButtonClasses();
+function BorderPanel( { borderRadius = '', setAttributes } ) {
+	const setBorderRadius = useCallback(
+		newBorderRadius => {
+			setAttributes( { borderRadius: newBorderRadius } );
+		},
+		[ setAttributes ]
+	);
+	return (
+		<PanelBody title={ __( 'Button Border Settings', 'jetpack' ) } initialOpen={ false }>
+			<RangeControl
+				value={ borderRadius }
+				label={ __( 'Border Radius', 'jetpack' ) }
+				min={ MIN_BORDER_RADIUS_VALUE }
+				max={ MAX_BORDER_RADIUS_VALUE }
+				initialPosition={ INITIAL_BORDER_RADIUS_POSITION }
+				allowReset
+				onChange={ setBorderRadius }
+			/>
+		</PanelBody>
+	);
+}
 
-		return (
-			<Fragment>
-				<div className="wp-block-button jetpack-submit-button">
-					<RichText
-						placeholder={ __( 'Add text…', 'jetpack' ) }
-						value={ attributes.submitButtonText }
-						onChange={ nextValue => setAttributes( { submitButtonText: nextValue } ) }
-						className={ buttonClasses }
-						style={ buttonStyle }
-						keepPlaceholderOnFocus
-						allowedFormats={ [] }
-					/>
-				</div>
-				<InspectorControls>
+function SubmitButton( {
+	attributes,
+	backgroundColor,
+	className,
+	fallbackBackgroundColor,
+	fallbackTextColor,
+	setAttributes,
+	setBackgroundColor,
+	setTextColor,
+	textColor,
+	fontSize,
+	setFontSize,
+} ) {
+	const { borderRadius, submitButtonText } = attributes;
+
+	const { gradientClass, gradientValue, setGradient } = isGradientAvailable ? useGradient() : {};
+
+	const classes = classnames( className, 'wp-block-button__link', {
+		'has-background': backgroundColor.color || gradientValue,
+		[ backgroundColor.class ]: ! gradientValue && backgroundColor.class,
+		'has-text-color': textColor.color,
+		[ textColor.class ]: textColor.class,
+		[ gradientClass ]: gradientClass,
+		'no-border-radius': borderRadius === 0,
+		[ fontSize.class ]: fontSize.class,
+	} );
+
+	const styles = {
+		...( ! backgroundColor.color && gradientValue
+			? { background: gradientValue }
+			: { backgroundColor: backgroundColor.color } ),
+		color: textColor.color,
+		borderRadius: borderRadius ? borderRadius + 'px' : undefined,
+		fontSize: fontSize.size ? fontSize.size + 'px' : undefined,
+	};
+
+	return (
+		<div className="wp-block-button">
+			<RichText
+				allowedFormats={ [] }
+				className={ classes }
+				onChange={ value => setAttributes( { submitButtonText: value } ) }
+				placeholder={ __( 'Add text…', 'jetpack' ) }
+				style={ styles }
+				value={ submitButtonText }
+				withoutInteractiveFormatting
+			/>
+
+			<InspectorControls>
+				<PanelBody title={ __( 'Button Text Settings' ) } initialOpen={ false }>
+					<FontSizePicker value={ fontSize.size } onChange={ setFontSize } />
+				</PanelBody>
+				{ isGradientAvailable && (
+					<PanelColorGradientSettings
+						title={ __( 'Button Color Settings', 'jetpack' ) }
+						settings={ [
+							{
+								colorValue: textColor.color,
+								onColorChange: setTextColor,
+								label: __( 'Text Color', 'jetpack' ),
+							},
+							{
+								colorValue: backgroundColor.color,
+								onColorChange: setBackgroundColor,
+								gradientValue,
+								onGradientChange: setGradient,
+								label: __( 'Background', 'jetpack' ),
+							},
+						] }
+						initialOpen={ false }
+					>
+						<ContrastChecker
+							{ ...{
+								fontSize: fontSize.size,
+								textColor: textColor.color,
+								backgroundColor: backgroundColor.color,
+								fallbackBackgroundColor,
+								fallbackTextColor,
+							} }
+						/>
+					</PanelColorGradientSettings>
+				) }
+				{ ! isGradientAvailable && (
 					<PanelColorSettings
 						title={ __( 'Button Color Settings', 'jetpack' ) }
 						colorSettings={ [
 							{
-								value: backgroundColor,
-								onChange: setBackgroundButtonColor,
-								label: __( 'Background Color', 'jetpack' ),
-							},
-							{
-								value: color,
-								onChange: setTextButtonColor,
+								value: textColor.color,
+								onChange: setTextColor,
 								label: __( 'Text Color', 'jetpack' ),
 							},
+							{
+								value: backgroundColor.color,
+								onChange: setBackgroundColor,
+								label: __( 'Background', 'jetpack' ),
+							},
 						] }
-					/>
-					<ContrastChecker
-						textColor={ color }
-						backgroundColor={ backgroundColor }
-						fallbackBackgroundColor
-						fallbackTextColor
-					/>
-				</InspectorControls>
-			</Fragment>
-		);
-	}
+						initialOpen={ false }
+					>
+						<ContrastChecker
+							{ ...{
+								fontSize: fontSize.size,
+								textColor: textColor.color,
+								backgroundColor: backgroundColor.color,
+								fallbackBackgroundColor,
+								fallbackTextColor,
+							} }
+						/>
+					</PanelColorSettings>
+				) }
+				<BorderPanel borderRadius={ borderRadius } setAttributes={ setAttributes } />
+			</InspectorControls>
+		</div>
+	);
 }
 
 export default compose( [
-	withColors( { backgroundButtonColor: 'background-color' }, { textButtonColor: 'color' } ),
+	withColors( 'backgroundColor', { textColor: 'color' } ),
+	withFontSizes( 'fontSize' ),
 	applyFallbackStyles,
 ] )( SubmitButton );
