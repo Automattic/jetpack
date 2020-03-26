bd1b6ec16a48c9c934232ae00291e7e187c07339
Adam Zielinski <adam@adamziel.com>
2020-03-26 11:38:28 +0100

    Private sites / Coming Soon SAL changes (#14853)
    Summary:
    In D38388-code and D39262-code we updated logic that computes `is_private` and `is_coming_soon` values returned by /sites endpoint. This was done do support Coming Soon mode and Private atomic sites. As we didn't expect SAL changes to be short or easy, we used filters instead.
    
    This diffs moves the filter-based logic to SAL, where it should live.
    
    I think we're going to need to change one more Site class in Jetpack PR, but it doesn't live on WP.com so this diff seems to be pretty complete.
    
    Test Plan:
    1. Make sure you have a non-atomic Jetpack site, an atomic Jetpack site, and a simple site tied to your WP.com account.
    1. Update `blog_public` to -1 on shadow site of one of your existing atomic sites
    1. Go to calypso, inspect call to `/sites` endpoint, confirm it says `is_private: false` and `is_coming_soon: false`
    1. Set `wpcom_coming_soon` option on that site to 1
    1. Go to calypso, inspect call to `/sites` endpoint, confirm it says `is_private: true` and `is_coming_soon: true`
    1. Set `wpcom_coming_soon` option on that site to 0
    1. Go to calypso, inspect call to `/sites` endpoint, confirm it says `is_private: true` and `is_coming_soon: false`
    1. Repeat with a simple site
    
    Reviewers: #jetpack, jblz
    
    Subscribers: jeherve
    
    Tags: #touches_jetpack_files
    
    Differential Revision: D39264-code
    
    This commit syncs r203615-wpcom.
    
    * Update class.json-api-site-jetpack.php
    
    * Only request cloud site options for atomic sites
    
    * Call site->is_private() and site->is_coming_soon() in /site endpoint
    
    * Update class.json-api-site-jetpack.php
    
    * Update class.json-api-site-jetpack.php
    
    * Add get_launch_status to Jetpack_Site
    
    * Move launch_status up in get-site-endpoint
    
    * Fix typo in function name
    
    Co-authored-by: Adam Zielinski <adam.zielinski@automattic.com>

--- json-endpoints/class.wpcom-json-api-get-site-endpoint.php
+++ json-endpoints/class.wpcom-json-api-get-site-endpoint.php
@@ -309,28 +309,15 @@ class WPCOM_JSON_API_GET_Site_Endpoint extends WPCOM_JSON_API_Endpoint {
 			case 'user_can_manage' :
 				$response[ $key ] = $this->site->user_can_manage();
 			case 'is_private' :
-				/**
-				 * Filters the value of `is_private` value about the site to return.
-				 *
-				 * @module json-api
-				 *
-				 * @since 8.4.0
-				 *
-				 * @param bool $is_private Is site private.
-				 */
-				$response[ $key ] = apply_filters( 'sites_site_is_private', $this->site->is_private() );
+				$response[ $key ] = $this->site->is_private();
 				break;
 			case 'is_coming_soon' :
-				/**
-				 * Filters the value of `is_coming_soon` value about the site to return.
-				 *
-				 * @module json-api
-				 *
-				 * @since 8.4.0
-				 *
-				 * @param bool $is_coming_soon Is site in the Coming Soon mode.
-				 */
-				$response[ $key ] = apply_filters( 'sites_site_is_coming_soon', false );
+				// This option is stored on wp.com for both simple and atomic sites. @see mu-plugins/private-blog.php
+				$response[ $key ] = $this->site->is_coming_soon();;
+				break;
+			case 'launch_status' :
+				$response[ $key ] = $this->site->get_launch_status();
+				break;
 			case 'visible' :
 				$response[ $key ] = $this->site->is_visible();
 				break;
@@ -403,9 +390,6 @@ class WPCOM_JSON_API_GET_Site_Endpoint extends WPCOM_JSON_API_Endpoint {
 				break;
 			case 'quota' :
 				$response[ $key ] = $this->site->get_quota();
-				break;
-			case 'launch_status' :
-				$response[ $key ] = $this->site->get_launch_status();
 				break;
 			case 'site_migration' :
 				$response[ $key ] = $this->site->get_migration_meta();
