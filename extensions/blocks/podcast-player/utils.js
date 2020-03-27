a86e6ba391644371835c8b6dc6226bb01d70ada0
retrofox <rdsuarez@gmail.com>
2020-03-27 08:51:45 -0300

    [not verified] podcast-player: improve getColorClassName jsdoc

--- extensions/blocks/podcast-player/utils.js
+++ extensions/blocks/podcast-player/utils.js
@@ -1,5 +1,11 @@
 /**
  * Returns a class based on the context a color is being used and its slug.
+ * Note: This helper function was copied from core @wordpress/block-editor lib,
+ * in order to avoid requiring not-needed dependencies to reduce the size
+ * of compiled files used in the front-end.
+ *
+ * @example
+ *     const className = getColorClassName( 'color', canvasPrimaryColor );
  *
  * @param {string} colorContextName Context/place where color is being used e.g: background, text etc...
  * @param {string} colorSlug        Slug of the color.
