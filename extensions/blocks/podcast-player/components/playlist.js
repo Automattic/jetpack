4804167a510f02b2214c16225ddfc1c7e93d790d
Jerry Jones <jones.jeremydavid@gmail.com>
2020-03-27 14:33:19 -0500

    Start on menuradioitem role and keyboard interactions

--- extensions/blocks/podcast-player/components/playlist.js
+++ extensions/blocks/podcast-player/components/playlist.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import classnames from 'classnames';
-import { memo } from '@wordpress/element';
+import { memo, useRef, createRef, forwardRef } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -10,6 +10,8 @@ import { __ } from '@wordpress/i18n';
  */
 import * as episodeIcons from '../icons/episode-icons';
 import { STATE_ERROR, STATE_PLAYING } from '../constants';
+
+// const trackRefs = [];
 
 const TrackIcon = ( { isPlaying, isError, className } ) => {
 	let hiddenText, name;
@@ -52,7 +54,19 @@ const TrackError = memo( ( { link } ) => (
 	</div>
 ) );
 
-const Track = memo( ( { track, isActive, isPlaying, isError, selectTrack, index } ) => {
+const focusTrack = ( trackRefs, focusIndex ) => {
+	if ( focusIndex < 0 ) {
+		// focus the last track
+		focusIndex = trackRefs.length - 1;
+	} else if ( focusIndex > trackRefs.length - 1 ) {
+		// focus the first item
+		focusIndex = 0;
+	}
+
+	trackRefs[ focusIndex ].current.focus();
+};
+
+const Track = memo( ( { track, trackRefs, isActive, isPlaying, isError, selectTrack, index } ) => {
 	return (
 		<li
 			className={ classnames( 'jetpack-podcast-player__episode', {
@@ -60,10 +74,12 @@ const Track = memo( ( { track, isActive, isPlaying, isError, selectTrack, index 
 			} ) }
 		>
 			<a
+				ref={ trackRefs[ index ] }
 				className="jetpack-podcast-player__episode-link"
 				href={ track.link }
-				role="button"
-				aria-pressed="false"
+				role="menuitemradio"
+				aria-checked={ isActive }
+				tabIndex={ isActive ? '0' : '-1' }
 				onClick={ e => {
 					// Prevent handling clicks if a modifier is in use.
 					if ( e.shiftKey || e.metaKey || e.altKey ) {
@@ -77,16 +93,25 @@ const Track = memo( ( { track, isActive, isPlaying, isError, selectTrack, index 
 					selectTrack( index );
 				} }
 				onKeyDown={ e => {
-					// Only handle the Space key.
-					if ( event.key !== ' ' ) {
-						return;
+					// spacebar
+					if ( e.keyCode === 32 ) {
+						e.preventDefault();
+						selectTrack( index );
+						e.currentTarget.focus();
+					}
+					// arrow down / arrow right
+					else if ( e.keyCode === 40 || e.keycode === 39 ) {
+						e.preventDefault();
+						// send focus to the next item (or the first if none left)
+						focusTrack( trackRefs, index + 1 );
 					}
 
-					// Prevent default behavior (scrolling one page down).
-					e.preventDefault();
-
-					// Select track.
-					selectTrack( index );
+					// arrow up / arrow left
+					else if ( e.keyCode === 38 || e.keycode === 37 ) {
+						e.preventDefault();
+						// send focus to the previous item (or the last if none left)
+						focusTrack( trackRefs, index - 1 );
+					}
 				} }
 			>
 				<TrackIcon
@@ -105,16 +130,19 @@ const Track = memo( ( { track, isActive, isPlaying, isError, selectTrack, index 
 } );
 
 const Playlist = memo( ( { tracks, selectTrack, currentTrack, playerState } ) => {
+	const trackRefs = [];
 	return (
-		<ol className="jetpack-podcast-player__episodes">
+		<ol className="jetpack-podcast-player__episodes" role="menu">
 			{ tracks.map( ( track, index ) => {
 				const isActive = currentTrack === index;
-
+				const trackRef = useRef( null );
+				trackRefs[ index ] = trackRef;
 				return (
 					<Track
 						key={ track.id }
 						index={ index }
 						track={ track }
+						trackRefs={ trackRefs }
 						selectTrack={ selectTrack }
 						isActive={ isActive }
 						isPlaying={ isActive && playerState === STATE_PLAYING }
