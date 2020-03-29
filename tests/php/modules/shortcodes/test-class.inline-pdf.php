f25dafc5823bb5ac2bda675901a905b069cbf709
Lance Willett <lance@automattic.com>
2020-03-29 16:32:51 -0700

    Add unit tests and fix lint errors

---
+++ tests/php/modules/shortcodes/test-class.inline-pdf.php
@@ -0,0 +1,32 @@
+<?php
+
+class WP_Test_Jetpack_Shortcodes_Inline_Pdfs extends WP_UnitTestCase {
+
+	/**
+	 * @author lancewillett
+	 * @covers ::jetpack_inline_pdf_embed_handler
+	 * @since  8.4
+	 */
+	public function test_shortcodes_inline_pdf() {
+		global $post;
+
+		$url         = 'https://jetpackme.files.wordpress.com/2017/08/jetpack-tips-for-hosts.pdf';
+		$post        = $this->factory()->post->create_and_get( array( 'post_content' => $url ) );
+
+		setup_postdata( $post );
+
+		// Test HTML version.
+		ob_start();
+		the_content();
+		$actual = ob_get_clean();
+		$this->assertContains(
+			sprintf(
+				'<object data="%1$s" type="application/pdf" width="100%%" height="800">
+					<p><a href="%1$s">%1$s</a></p>
+				</object>',
+				$url
+			),
+			$actual
+		);
+	}
+}
