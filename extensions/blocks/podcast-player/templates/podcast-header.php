6d416b61738fb790643756521a425afd6ecb6e41
retrofox <rdsuarez@gmail.com>
2020-03-31 17:29:20 -0300

    podcast-player: add header template

---
+++ extensions/blocks/podcast-player/templates/podcast-header.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Podcast Header template.
+ *
+ * @package Jetpack
+ */
+
+/**
+ * Block attributes
+ */
+
+$attributes = (array) $data['attributes'];
+
+/**
+ * Player data.
+ */
+
+$player_id                = (string) $data['playerId'];
+$cover                    = (string) $data['cover'];
+$link                     = (string) $data['link'];
+$track                    = ! empty( $data['tracks'] ) ? $data['tracks'][0] : array();
+$show_cover_art           = (bool) $attributes['showCoverArt'];
+$show_episode_description = (bool) $attributes['showEpisodeDescription'];
+?>
+
+<div class="jetpack-podcast-player__header">
+	<div class="jetpack-podcast-player__current-track-info" aria-live="polite">
+		<?php if ( $show_cover_art && isset( $cover ) ) : ?>
+		<div class="jetpack-podcast-player__cover">
+			<img class="jetpack-podcast-player__cover-image" src=<?php echo esc_url( $cover ); ?>alt="" />
+		</div>
+		<?php endif; ?>
+	</div>
+
+	<?php
+	if ( $show_episode_description && ! empty( $track ) && isset( $track['description'] ) ) :
+		?>
+
+	<div
+		id="<?php echo esc_attr( $player_id ); ?>__track-description"
+		class="jetpack-podcast-player__track-description"
+	>
+		<?php echo esc_attr( $track['description'] ); ?>
+	</div>
+	<?php endif; ?>
+</div>
