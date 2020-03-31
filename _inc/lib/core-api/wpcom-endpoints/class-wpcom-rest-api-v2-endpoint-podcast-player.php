9696cbd26234b6cf1468fb3f8285c2953f6fbe7d
Marek Hrabe <marekhrabe@me.com>
2020-03-31 14:40:08 +0200

    Podcast Player: Add API schema (#15197)
    * add schema
    
    * filter response by _fields

--- _inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-podcast-player.php
+++ _inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-podcast-player.php
@@ -47,6 +47,7 @@ class WPCOM_REST_API_V2_Endpoint_Podcast_Player extends WP_REST_Controller {
 							},
 						),
 					),
+					'schema'              => array( $this, 'get_public_item_schema' ),
 				),
 			)
 		);
@@ -64,9 +65,99 @@ class WPCOM_REST_API_V2_Endpoint_Podcast_Player extends WP_REST_Controller {
 		}
 
 		$player_data = Jetpack_Podcast_Helper::get_player_data( $request['url'] );
-		// $player_data can be the actual data or WP_Error.
-		// rest_ensure_response handles both.
+
+		if ( is_wp_error( $player_data ) ) {
+			return rest_ensure_response( $player_data );
+		}
+
+		$player_data = $this->prepare_for_response( $player_data, $request );
 		return rest_ensure_response( $player_data );
+	}
+
+	/**
+	 * Filters out data based on ?_fields= request parameter
+	 *
+	 * @param array           $player_data Data for the player.
+	 * @param WP_REST_Request $request The request.
+	 * @return array filtered $player_data
+	 */
+	public function prepare_for_response( $player_data, $request ) {
+		if ( ! is_callable( array( $this, 'get_fields_for_response' ) ) ) {
+			return $player_data;
+		}
+
+		$fields = $this->get_fields_for_response( $request );
+
+		$response_data = array();
+		foreach ( $player_data as $field => $value ) {
+			if ( in_array( $field, $fields, true ) ) {
+				$response_data[ $field ] = $value;
+			}
+		}
+
+		return $response_data;
+	}
+
+	/**
+	 * Retrieves the response schema, conforming to JSON Schema.
+	 *
+	 * @return array
+	 */
+	public function get_item_schema() {
+		$schema = array(
+			'$schema'    => 'http://json-schema.org/draft-04/schema#',
+			'title'      => 'jetpack-podcast-player',
+			'type'       => 'object',
+			'properties' => array(
+				'title'  => array(
+					'description' => __( 'The title of the podcast.', 'jetpack' ),
+					'type'        => 'string',
+				),
+				'link'   => array(
+					'description' => __( 'The URL of the podcast website.', 'jetpack' ),
+					'type'        => 'string',
+				),
+				'cover'  => array(
+					'description' => __( 'The URL of the podcast cover image.', 'jetpack' ),
+					'type'        => 'string',
+				),
+				'tracks' => array(
+					'description' => __( 'Latest episodes of the podcast.', 'jetpack' ),
+					'type'        => 'array',
+					'items'       => array(
+						'type'       => 'object',
+						'properties' => array(
+							'id'          => array(
+								'description' => __( 'The episode id. Generated per request, not globally unique.', 'jetpack' ),
+								'type'        => 'string',
+							),
+							'link'        => array(
+								'description' => __( 'The external link for the episode.', 'jetpack' ),
+								'type'        => 'string',
+							),
+							'src'         => array(
+								'description' => __( 'The audio file URL of the episode.', 'jetpack' ),
+								'type'        => 'string',
+							),
+							'type'        => array(
+								'description' => __( 'The mime type of the episode.', 'jetpack' ),
+								'type'        => 'string',
+							),
+							'description' => array(
+								'description' => __( 'The episode description, in plaintext.', 'jetpack' ),
+								'type'        => 'string',
+							),
+							'title'       => array(
+								'description' => __( 'The episode title.', 'jetpack' ),
+								'type'        => 'string',
+							),
+						),
+					),
+				),
+			),
+		);
+
+		return $schema;
 	}
 }
 wpcom_rest_api_v2_load_plugin( 'WPCOM_REST_API_V2_Endpoint_Podcast_Player' );
