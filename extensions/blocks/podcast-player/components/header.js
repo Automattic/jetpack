7d122385db2ada5babfd30df5e6c94b03bacd30b
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2020-03-31 20:32:30 +0200

    Podcast Player: Rename "episode" to "track" for consistency (#15211)
    * [not verified] Rename 'episode' to 'track'
    
    * [not verified] Prefix header class names to avoid accidental overrides
    
    * [not verified] Replace render ternaries with &&
    
    * [not verified] Increase image selector specificity
    
    * Simplify podcast title selector
    + add target & rel attributes if is a link
    
    * [not verified] A few more class name changes before scoping
    
    * Rename "cover" class name to "current-track-info"
    
    * Add class to the cover img element
    
    * Make sure &&s don't render any extras when falsy

--- extensions/blocks/podcast-player/components/header.js
+++ extensions/blocks/podcast-player/components/header.js
@@ -5,31 +5,29 @@ import { memo } from '@wordpress/element';
 
 const Header = memo(
 	( { playerId, title, cover, link, track, children, showCoverArt, showEpisodeDescription } ) => (
-		<div className="jetpack-podcast-player__header-wrapper">
-			<div className="jetpack-podcast-player__header" aria-live="polite">
-				{ showCoverArt && cover ? (
-					<div className="jetpack-podcast-player__track-image-wrapper">
+		<div className="jetpack-podcast-player__header">
+			<div className="jetpack-podcast-player__current-track-info" aria-live="polite">
+				{ showCoverArt && cover && (
+					<div className="jetpack-podcast-player__cover">
 						{ /* alt="" will prevent the src from being announced. Ideally we'd have a cover.alt, but we can't get that from the RSS */ }
-						<img className="jetpack-podcast-player__track-image" src={ cover } alt="" />
+						<img className="jetpack-podcast-player__cover-image" src={ cover } alt="" />
 					</div>
-				) : null }
+				) }
 
-				{ title || ( track && track.title ) ? (
-					<div className="jetpack-podcast-player__titles">
-						<Title playerId={ playerId } title={ title } link={ link } track={ track } />
-					</div>
-				) : null }
+				{ !! ( title || ( track && track.title ) ) && (
+					<Title playerId={ playerId } title={ title } link={ link } track={ track } />
+				) }
 			</div>
 
 			{ /* putting this above the audio player for source order HTML with screen readers, then visually switching it with the audio player via flex */ }
-			{ showEpisodeDescription && track && track.description ? (
+			{ !! ( showEpisodeDescription && track && track.description ) && (
 				<div
 					id={ `${ playerId }__track-description` }
 					className="jetpack-podcast-player__track-description"
 				>
 					{ track.description }
 				</div>
-			) : null }
+			) }
 
 			{ /* children contains the audio player */ }
 			{ children }
@@ -38,30 +36,32 @@ const Header = memo(
 );
 
 const Title = memo( ( { playerId, title, link, track } ) => (
-	<h2 id={ `${ playerId }__title` } className="jetpack-podcast-player__titles">
-		{ track && track.title ? (
-			<span className="jetpack-podcast-player__track-title">{ track.title }</span>
-		) : null }
+	<h2 id={ `${ playerId }__title` } className="jetpack-podcast-player__title">
+		{ !! ( track && track.title ) && (
+			<span className="jetpack-podcast-player__current-track-title">{ track.title }</span>
+		) }
 
 		{ /* Adds a visually hidden dash when both a track and a podcast titles are present */ }
-		{ track && track.title && title ? (
+		{ !! ( track && track.title && title ) && (
 			<span className="jetpack-podcast-player--visually-hidden"> - </span>
-		) : null }
+		) }
 
-		{ title ? <PodcastTitle title={ title } link={ link } /> : null }
+		{ !! title && <PodcastTitle title={ title } link={ link } /> }
 	</h2>
 ) );
 
-const PodcastTitle = memo( ( { title, link } ) => (
-	<span className="jetpack-podcast-player__title">
-		{ link ? (
-			<a className="jetpack-podcast-player__title-link" href={ link }>
+const PodcastTitle = memo( ( { title, link } ) => {
+	const className = 'jetpack-podcast-player__podcast-title';
+
+	if ( link ) {
+		return (
+			<a className={ className } href={ link } target="_blank" rel="noopener noreferrer nofollow">
 				{ title }
 			</a>
-		) : (
-			title
-		) }
-	</span>
-) );
+		);
+	}
+
+	return <span className={ className }>{ title }</span>;
+} );
 
 export default Header;
