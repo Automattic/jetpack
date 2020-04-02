1cbafb5a4381fd19f4a066eda52a399ba034fa4c
Brandon Kraft <public@brandonkraft.com>
2020-04-02 16:05:40 -0500

    bbPress Compat: Tidy up locations and PHPCS (#15216)
    * Move bbpress compat file to 3rd-party directory
    
    * Bring changed files into full PHPCS compliance and add to whitelist
    
    * Remove require from class.jetpack.php that is no longer needed.
    
    * Listening to Code Climate

--- bin/phpcs-whitelist.js
+++ bin/phpcs-whitelist.js
@@ -1,8 +1,11 @@
 // If the file path starts with anything like in the array below, it should be linted.
 module.exports = [
 	'3rd-party/3rd-party.php',
+	'3rd-party/bbpress.php',
 	'3rd-party/class.jetpack-amp-support.php',
+	'3rd-party/class-jetpack-bbpress-rest-api.php',
 	'3rd-party/woocommerce-services.php',
+	'class.jetpack-bbpress-json-api.compat.php',
 	'class.jetpack-gutenberg.php',
 	'class.jetpack-plan.php',
 	'docker',
