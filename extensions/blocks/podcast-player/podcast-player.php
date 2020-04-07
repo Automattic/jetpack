00572b237ad348b6b5e9758ccf0c45f214e8f259
Damián Suárez <rdsuarez@gmail.com>
2020-04-07 08:34:47 -0300

    Podcast Player: introduce CSS vars for colors (#15321)
    * add attributes to store the colors in HEX format
    
    * store color in HEX
    
    * podcast-player: add CSS variables in clint side
    
    * add CSS vars in server side markup
    
    * remove unused dependency
    
    * Scope CSS variables to podcast player block
    
    * sccope CSS variables, in server-side rendering
    
    * improve doc, checking colors
    
    * rename color handler
    
    * add doc using multiline format
    
    Co-authored-by: Michael P. Pfeiffer <michael@cssconf.eu>

--- extensions/blocks/podcast-player/podcast-player.php
+++ extensions/blocks/podcast-player/podcast-player.php
@@ -119,8 +119,9 @@ function render_player( $player_data, $attributes ) {
 	$secondary_colors  = get_colors( 'secondary', $attributes, 'color' );
 	$background_colors = get_colors( 'background', $attributes, 'background-color' );
 
-	$player_classes_name = trim( "{$secondary_colors['class']} {$background_colors['class']}" );
-	$player_inline_style = trim( "{$secondary_colors['style']} ${background_colors['style']}" );
+	$player_classes_name  = trim( "{$secondary_colors['class']} {$background_colors['class']}" );
+	$player_inline_style  = trim( "{$secondary_colors['style']} ${background_colors['style']}" );
+	$player_inline_style .= get_css_vars( $attributes );
 
 	$block_classname = Jetpack_Gutenberg::block_classes( FEATURE_NAME, $attributes, array( 'is-default' ) );
 	$is_amp          = ( class_exists( 'Jetpack_AMP_Support' ) && Jetpack_AMP_Support::is_amp_request() );
@@ -222,6 +223,26 @@ function get_colors( $name, $attrs, $property ) {
 	}
 
 	return $colors;
+}
+
+/**
+ * It generates a string with CSS variables according to the
+ * block colors, prefixing each one with `--jetpack-podcast-player'.
+ *
+ * @param array $attrs Podcast Block attributes object.
+ * @return string      CSS variables depending on block colors.
+ */
+function get_css_vars( $attrs ) {
+	$colors_name = array( 'primary', 'secondary', 'background' );
+
+	$inline_style = '';
+	foreach ( $colors_name as $color ) {
+		$hex_color = 'hex' . ucfirst( $color ) . 'Color';
+		if ( ! empty( $attrs[ $hex_color ] ) ) {
+			$inline_style .= " --jetpack-podcast-player-{$color}: {$attrs[ $hex_color ]};";
+		}
+	}
+	return $inline_style;
 }
 
 /**
