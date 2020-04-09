46af0f1923e22ddebef78b2974c31eef5f2e57e9
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-04-09 18:37:56 +0300

    fix push script

--- tests/e2e/bin/push-allure-artifacts.sh
+++ tests/e2e/bin/push-allure-artifacts.sh
@@ -3,7 +3,7 @@
 
 set -ex
 
-USER="brbrr"
+ORG="brbrr"
 REPO="test-dashboard-pages"
 
 RESULTS_DIR="allure-results"
@@ -12,22 +12,23 @@ USERNAME="jetpackbot"
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-git clone https://${USERNAME}:${GH_TEST_REPORT_TOKEN}@github.com/brbrr/$REPO.git
+git clone https://${USERNAME}:${GH_TEST_REPORT_TOKEN}@github.com/$ORG/$REPO.git
 cd $REPO
 
 if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
 	# master branch
-	$REPORT_DIR="docs"
+	REPORT_DIR="docs"
 elif [ ! "$TRAVIS_PULL_REQUEST" = "false" ]; then
 	# pull request
-	$REPORT_DIR="docs/PR-$TRAVIS_PULL_REQUEST"
+	REPORT_DIR="docs/PR-$TRAVIS_PULL_REQUEST"
 else
 	# other branch
-	$BRANCH=$(echo "$TRAVIS_BRANCH" | tr / _)
-	$REPORT_DIR="docs/$BRANCH"
+	BRANCH=$(echo "$TRAVIS_BRANCH" | tr / _)
+	REPORT_DIR="docs/$BRANCH"
 fi
 
 ls -la $RESULTS_DIR
+ls -la $REPORT_DIR
 
 cp -R ../$RESULTS_DIR .
 
