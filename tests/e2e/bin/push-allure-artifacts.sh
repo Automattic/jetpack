dd6ebf567922f504382fbe251cbf9ff4150eccb3
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-04-09 13:09:48 +0300

    try pushing to the dashboard repo

---
+++ tests/e2e/bin/push-allure-artifacts.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# see https://github.com/wp-cli/wp-cli/blob/master/templates/install-wp-tests.sh
+
+set -ex
+
+
+pwd
+
+ls -la
+
+DIR="test-dashboard-pages"
+RESULTS_DIR="allure-results"
+USERNAME="jetpackbot"
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
+git clone https://${USERNAME}:${GH_TEST_REPORT_TOKEN}@github.com/brbrr/$DIR.git
+
+cd $DIR
+
+cp -R ../$RESULTS_DIR ./$RESULTS_DIR
+
+git add .
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+git push
