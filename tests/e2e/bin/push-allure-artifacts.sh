b0ecf2ad34446f435f42f457cd775f8e0556e7c4
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-04-10 19:10:44 +0300

    rewrite the push logic to handle the fact that report dir should be clean

--- tests/e2e/bin/push-allure-artifacts.sh
+++ tests/e2e/bin/push-allure-artifacts.sh
@@ -10,47 +10,59 @@ REPO_DIR=$ROOT_DIR/$REPO
 RESULTS_DIR="allure-results"
 USERNAME="jetpackbot"
 
+if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
+	# master branch
+	ARTIFACT="master"
+elif [ ! "$TRAVIS_PULL_REQUEST" = "false" ]; then
+	# pull request
+	ARTIFACT="pr-$TRAVIS_PULL_REQUEST"
+else
+	# other branch
+	ARTIFACT=$(echo "$TRAVIS_BRANCH" | tr / _)
+fi
+
+REPORT_DIR="$REPO_DIR/docs/$ARTIFACT"
+ARTIFACT_DIR="$REPO_DIR/$ARTIFACT"
+
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
-	# master branch
-	REPORT_DIR="docs"
-elif [ ! "$TRAVIS_PULL_REQUEST" = "false" ]; then
-	# pull request
-	REPORT_DIR="docs/pr-$TRAVIS_PULL_REQUEST"
-else
-	# other branch
-	BRANCH=$(echo "$TRAVIS_BRANCH" | tr / _)
-	REPORT_DIR="docs/$BRANCH"
-fi
-
 git clone https://${USERNAME}:${GH_TEST_REPORT_TOKEN}@github.com/$ORG/$REPO.git
 
-# mkdir -p $REPO_DIR/$REPORT_DIR/$RESULTS_DIR
+mkdir -p $ARTIFACT_DIR/allure-results $REPORT_DIR
+cp -a $ROOT_DIR/$RESULTS_DIR/. $ARTIFACT_DIR/allure-results
+cd $ARTIFACT_DIR
+allure generate --clean -o $REPORT_DIR
 
-# allure generate -o $REPO_DIR/$REPORT_DIR
+if [ "$ARTIFACT" = "master" ]; then
+	# master branch
 
+	cd $REPO_DIR/docs
+	rm -rf app.js data export favicon.ico history index.html plugins styles.css
+	cp -a master/. ./
+fi
 
-ls -la $ROOT_DIR/$RESULTS_DIR
+# for PR:
+#   mkdir -p pr-1111/allure-results docs/pr-1111
+#   cp current-results pr-1111/allure-results
+#   cd pr-1111
+#   allure generate --clean -o ../docs/pr-1111
+#
+# for Branch
+#   mkdir -p some-branch/allure-results docs/some-branch
+#   cp current-results some-branch/allure-results
+#   cd some-branch
+#   allure generate --clean -o ../docs/some-branch
+#
+# for Master
+#   mkdir -p master/allure-results docs/master
+#   cp current-results master/allure-results
+#   cd master
+#   allure generate --clean -o ../docs/master
 
-mkdir -p $REPO_DIR/$REPORT_DIR/$RESULTS_DIR
-
-cp -a $ROOT_DIR/$RESULTS_DIR/. $REPO_DIR/$REPORT_DIR/$RESULTS_DIR
-
-ls -la $REPO_DIR/$REPORT_DIR/$RESULTS_DIR
-
-cd $REPO_DIR/$REPORT_DIR
-
-allure generate -o .
-
-ls -la $REPO_DIR/$REPORT_DIR
-
-cp -a $ROOT_DIR/$RESULTS_DIR/. $REPO_DIR/$REPORT_DIR/$RESULTS_DIR
-
-ls -la $REPO_DIR/docs
-ls -la $REPO_DIR/$REPORT_DIR
-ls -la $REPO_DIR/$REPORT_DIR/$RESULTS_DIR
+#   cd ../docs
+#   rm -rf app.js data export favicon.ico history index.html plugins styles.css
+#   cp -a master/. ./
 
 cd $REPO_DIR
 
