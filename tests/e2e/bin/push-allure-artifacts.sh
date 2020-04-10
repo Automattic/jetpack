07632752ece1f94096cbd7615f9f7c8a70b09ab3
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-04-10 19:36:51 +0300

    add some docs explaining whats going on

--- tests/e2e/bin/push-allure-artifacts.sh
+++ tests/e2e/bin/push-allure-artifacts.sh
@@ -10,6 +10,10 @@ REPO_DIR=$ROOT_DIR/$REPO
 RESULTS_DIR="allure-results"
 USERNAME="jetpackbot"
 
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git clone https://${USERNAME}:${GH_TEST_REPORT_TOKEN}@github.com/$ORG/$REPO.git
+
 if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
 	# master branch
 	ARTIFACT="master"
@@ -17,17 +21,25 @@ elif [ ! "$TRAVIS_PULL_REQUEST" = "false" ]; then
 	# pull request
 	ARTIFACT="pr-$TRAVIS_PULL_REQUEST"
 else
-	# other branch
+	# other branch. replace / with _
 	ARTIFACT=$(echo "$TRAVIS_BRANCH" | tr / _)
 fi
 
+##
+# Dashboard repo configured to serve static files from `/docs` folder
+# The above script allows us to maintain the following site/content structure (site path vs folders):
+#		/ => /docs (this is a master branch)
+#		/pr-1234 => /docs/pr-1234
+#		/branch-name => /docs/branch-name (works for both master and other branches)
+#
+# What this script does is basically moves new test results into existing results from previous builds into $ARTIFACT_DIR
+# This allows us to generate the report with previous build history.
+#
+# Master branch is a bit of special case, since we want to serve it from /master and / and the limitation of `allure generate` which unable to generate into dirty folder. We need to copy /docs/master into /docs to handle it
+##
+
 REPORT_DIR="$REPO_DIR/docs/$ARTIFACT"
 ARTIFACT_DIR="$REPO_DIR/$ARTIFACT"
-
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-
-git clone https://${USERNAME}:${GH_TEST_REPORT_TOKEN}@github.com/$ORG/$REPO.git
 
 mkdir -p $ARTIFACT_DIR/allure-results $REPORT_DIR
 cp -a $ROOT_DIR/$RESULTS_DIR/. $ARTIFACT_DIR/allure-results
@@ -35,39 +47,16 @@ cd $ARTIFACT_DIR
 allure generate --clean -o $REPORT_DIR
 
 if [ "$ARTIFACT" = "master" ]; then
-	# master branch
-
 	cd $REPO_DIR/docs
 	rm -rf app.js data export favicon.ico history index.html plugins styles.css
 	cp -a master/. ./
 fi
 
-# for PR:
-#   mkdir -p pr-1111/allure-results docs/pr-1111
-#   cp current-results pr-1111/allure-results
-#   cd pr-1111
-#   allure generate --clean -o ../docs/pr-1111
-#
-# for Branch
-#   mkdir -p some-branch/allure-results docs/some-branch
-#   cp current-results some-branch/allure-results
-#   cd some-branch
-#   allure generate --clean -o ../docs/some-branch
-#
-# for Master
-#   mkdir -p master/allure-results docs/master
-#   cp current-results master/allure-results
-#   cd master
-#   allure generate --clean -o ../docs/master
-
-#   cd ../docs
-#   rm -rf app.js data export favicon.ico history index.html plugins styles.css
-#   cp -a master/. ./
-
+# Push the changes
 cd $REPO_DIR
 
 git status
 
-git add docs
+git add .
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 git push
