148d4449ccb4b48bc287f3c0d5f0ad8df0d701b7
Jeremy Herve <jeremy@jeremy.hu>
2020-04-15 19:03:30 +0200

    Inline PDFs: better fallback link with more instructions
    object is not always supported in mobile browsers.
    Instead of only displaying the file URL as a link, let's make that link more helpful with clear instructions ("click to access PDF document").

--- modules/shortcodes/inline-pdfs.php
+++ modules/shortcodes/inline-pdfs.php
@@ -30,8 +30,9 @@ function jetpack_inline_pdf_embed_handler( $matches, $attr, $url ) {
 	}
 	return sprintf(
 		'<object data="%1$s" type="application/pdf" width="100%%" height="800">
-			<p><a href="%1$s">%1$s</a></p>
+			<p><a href="%1$s">%2$s</a></p>
 		</object>',
-		esc_attr( $url )
+		esc_attr( $url ),
+		esc_html__( 'Click to access PDF Document', 'jetpack' )
 	);
 }
