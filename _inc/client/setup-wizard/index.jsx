df89ebe38dc01648acb77d1c9fb9465da4377ed1
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2020-04-15 10:56:59 -0500

    Use SiteData to get the siteUrl

--- _inc/client/setup-wizard/index.jsx
+++ _inc/client/setup-wizard/index.jsx
@@ -2,50 +2,62 @@
  * External dependencies
  */
 import React from 'react';
+import { connect } from 'react-redux';
 import { translate as __ } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
 import Button from 'components/button';
+import QuerySite from 'components/data/query-site';
 import { imagePath } from 'constants/urls';
+import { getSiteRawUrl } from 'state/initial-state';
 
-export function SetupWizard() {
+const SetupWizardComponent = props => {
 	return (
-		<div className="jp-setup-wizard-intro">
-			<img
-				width="200px"
-				height="200px"
-				src={ imagePath + '/jetpack-powering-up.svg' }
-				alt={ __( 'A jetpack site powering up' ) }
-			/>
-			<h1 className="jp-setup-wizard-header">
-				{ __( 'Set up Jetpack for better site security, performance, and more' ) }
-			</h1>
-			<p className="jp-setup-wizard-paragraph">
-				{ __(
-					'Jetpack is a cloud-powered tool built by Automattic and brought to you by Bluehost.'
-				) }
-			</p>
-			<p className="jp-setup-wizard-paragraph">
-				{ __(
-					'Answer a few questions and we’ll help you secure, speed up, customize, and grow your WordPress website.'
-				) }
-			</p>
-			<div className="jp-setup-wizard-intro-question">
-				<h2>{ __( 'What will YourGroovyDomain.com be used for?' ) }</h2>
-				<div>
-					<Button primary className="jp-setup-wizard-button">
-						{ __( 'Personal Use' ) }
-					</Button>
-					<Button className="jp-setup-wizard-button" primary>
-						{ __( 'Business Use' ) }
-					</Button>
+		<>
+			<QuerySite />
+			<div className="jp-setup-wizard-intro">
+				<img
+					width="200px"
+					height="200px"
+					src={ imagePath + '/jetpack-powering-up.svg' }
+					alt={ __( 'A jetpack site powering up' ) }
+				/>
+				<h1 className="jp-setup-wizard-header">
+					{ __( 'Set up Jetpack for better site security, performance, and more' ) }
+				</h1>
+				<p className="jp-setup-wizard-paragraph">
+					{ __(
+						'Jetpack is a cloud-powered tool built by Automattic and brought to you by Bluehost.'
+					) }
+				</p>
+				<p className="jp-setup-wizard-paragraph">
+					{ __(
+						'Answer a few questions and we’ll help you secure, speed up, customize, and grow your WordPress website.'
+					) }
+				</p>
+				<div className="jp-setup-wizard-intro-question">
+					<h2>
+						{ __( 'What will %(siteUrl)s be used for?', { args: { siteUrl: props.siteRawUrl } } ) }
+					</h2>
+					<div>
+						<Button primary className="jp-setup-wizard-button">
+							{ __( 'Personal Use' ) }
+						</Button>
+						<Button className="jp-setup-wizard-button" primary>
+							{ __( 'Business Use' ) }
+						</Button>
+					</div>
+					<a className="jp-setup-wizard-skip-link" href="">
+						{ __( 'Skip to recommended features' ) }
+					</a>
 				</div>
-				<a className="jp-setup-wizard-skip-link" href="">
-					{ __( 'Skip to recommended features' ) }
-				</a>
 			</div>
-		</div>
+		</>
 	);
-}
+};
+
+export const SetupWizard = connect( state => {
+	return { siteRawUrl: getSiteRawUrl( state ) };
+} )( SetupWizardComponent );
