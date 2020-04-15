dc854950c1af670b264d3c2ac19da22c9e14b939
apeatling <apeatling@gmail.com>
2020-04-15 13:13:07 -0700

    Move the loading of styles to the render callback.

--- extensions/blocks/subscriptions/subscriptions.php
+++ extensions/blocks/subscriptions/subscriptions.php
@@ -18,8 +18,23 @@ const BLOCK_NAME   = 'jetpack/' . FEATURE_NAME;
  * registration if we need to.
  */
 function register_block() {
-	jetpack_register_block( BLOCK_NAME );
-
-	Jetpack_Gutenberg::load_styles_as_required( FEATURE_NAME );
+	jetpack_register_block(
+		BLOCK_NAME,
+		array( 'render_callback' => __NAMESPACE__ . '\render_block' )
+	);
 }
 add_action( 'init', __NAMESPACE__ . '\register_block' );
+
+/**
+ * Subscriptions block render callback.
+ *
+ * @param array  $attributes Array containing the block attributes.
+ * @param string $content    String containing the block content.
+ *
+ * @return string
+ */
+function render_block( $attributes, $content ) {
+	Jetpack_Gutenberg::load_styles_as_required( FEATURE_NAME );
+
+	return $content;
+}
