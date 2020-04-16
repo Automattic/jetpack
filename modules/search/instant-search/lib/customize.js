0208d8c3ca251ea805bc53d233344ba5c0d1ef75
Jason Moon <jason.moon@automattic.com>
2020-04-16 14:45:26 -0600

    Add overlay trigger config to customize library

--- modules/search/instant-search/lib/customize.js
+++ modules/search/instant-search/lib/customize.js
@@ -5,17 +5,19 @@ import { SERVER_OBJECT_NAME } from './constants';
 
 const CUSTOMIZE_SETTINGS = [
 	'jetpack_search_color_theme',
-	'jetpack_search_inf_scroll',
 	'jetpack_search_highlight_color',
+	'jetpack_search_inf_scroll',
 	'jetpack_search_opacity',
+	'jetpack_search_overlay_trigger',
 	'jetpack_search_show_powered_by',
 ];
 
 const SETTINGS_TO_STATE_MAP = new Map( [
 	[ 'jetpack_search_color_theme', 'colorTheme' ],
-	[ 'jetpack_search_inf_scroll', 'enableInfScroll' ],
 	[ 'jetpack_search_highlight_color', 'highlightColor' ],
+	[ 'jetpack_search_inf_scroll', 'enableInfScroll' ],
 	[ 'jetpack_search_opacity', 'opacity' ],
+	[ 'jetpack_search_overlay_trigger', 'overlayTrigger' ],
 	[ 'jetpack_search_show_powered_by', 'showPoweredBy' ],
 ] );
 
