a03cb2148e1ff4a7c1ed2c3e85158e09fe2c118a
Jeremy Herve <jeremy@jeremy.hu>
2020-04-20 16:49:17 +0200

    Handle both old and new comment type defaults

--- json-endpoints/class.wpcom-json-api-get-comments-tree-endpoint.php
+++ json-endpoints/class.wpcom-json-api-get-comments-tree-endpoint.php
@@ -102,9 +102,14 @@ class WPCOM_JSON_API_Get_Comments_Tree_Endpoint extends WPCOM_JSON_API_Endpoint 
 	 */
 	function get_site_tree_total_count( $status, $type ) {
 		global $wpdb;
+
 		$db_status = $this->get_comment_db_status( $status );
-		$type = $this->get_sanitized_comment_type( $type );
-		// An empty value in the comments_type column denotes a regular comment.
+		$type      = $this->get_sanitized_comment_type( $type );
+
+		/*
+		 * An empty value in the comments_type column denotes a regular comment in WP 5.5-
+		 * @to-do: remove empty value logic when the minimum required WP is 5.5.
+		 */
 		$type = ( 'comment' === $type ) ? 'comment' : $type;
 
 		$result = $wpdb->get_var(
@@ -112,8 +117,10 @@ class WPCOM_JSON_API_Get_Comments_Tree_Endpoint extends WPCOM_JSON_API_Endpoint 
 				"SELECT COUNT(1) " .
 				"FROM $wpdb->comments AS comments " .
 				"INNER JOIN $wpdb->posts AS posts ON comments.comment_post_ID = posts.ID " .
-				"WHERE comment_type = %s AND ( %s = 'all' OR comment_approved = %s )",
-				$type, $db_status, $db_status
+				"WHERE comment_type %s AND ( %s = 'all' OR comment_approved = %s )",
+				( 'comment' === $type ? "in ( '', 'comment' )" : '= ' . $type ),
+				$db_status,
+				$db_status
 			)
 		);
 		return intval( $result );
