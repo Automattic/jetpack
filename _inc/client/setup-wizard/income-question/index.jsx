39f7023000111703c7dea486926f2668c6475056
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2020-04-20 14:53:46 -0500

    Style the subtitle

--- _inc/client/setup-wizard/income-question/index.jsx
+++ _inc/client/setup-wizard/income-question/index.jsx
@@ -24,7 +24,7 @@ const IncomeQuestion = props => {
 					args: { siteUrl: props.siteRawUrl },
 				} ) }
 			</h1>
-			<p>{ __( 'Check all that apply' ) }</p>
+			<p className="jp-setup-wizard-subtitle">{ __( 'Check all that apply' ) }</p>
 		</div>
 	);
 };
