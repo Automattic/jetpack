dd3e48cadef84a593131a70ec6580db4b684ad85
Copons <Copons@users.noreply.github.com>
2020-04-20 11:23:18 +0100

    Remove import @wordpress/icons as not supported yet

--- extensions/blocks/button/index.js
+++ extensions/blocks/button/index.js
@@ -2,13 +2,13 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { button as icon } from '@wordpress/icons';
 
 /**
  * Internal dependencies
  */
 import attributes from './attributes';
 import edit from './edit';
+import icon from './icon';
 import save from './save';
 
 export const name = 'button';
