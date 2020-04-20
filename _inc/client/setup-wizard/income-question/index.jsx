dc5f01910d44fb008bb9ffe223388b64ea9cd6b2
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2020-04-20 14:26:26 -0500

    Add illustration

--- _inc/client/setup-wizard/income-question/index.jsx
+++ _inc/client/setup-wizard/income-question/index.jsx
@@ -2,9 +2,24 @@
  * External dependencies
  */
 import React from 'react';
+import { translate as __ } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import { imagePath } from 'constants/urls';
 
 const IncomeQuestion = props => {
-	return <div className="jp-setup-wizard-main">Hello world! from the income question page</div>;
+	return (
+		<div className="jp-setup-wizard-main">
+			<img
+				width="200px"
+				height="200px"
+				src={ imagePath + '/generating-cash-2.svg' }
+				alt={ __( 'A jetpack site generating revenue' ) }
+			/>
+		</div>
+	);
 };
 
 export { IncomeQuestion };
