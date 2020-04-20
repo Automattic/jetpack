5a335f6f164210509c69f36fbf6da5741cc26bc4
Marek Hrabe <marekhrabe@me.com>
2020-04-20 14:14:37 +0200

    move amp audio to its own template

---
+++ extensions/blocks/podcast-player/templates/amp-audio.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Podcast AMP Audio template.
+ *
+ * @package Jetpack
+ */
+
+namespace Automattic\Jetpack\Extensions\Podcast_Player;
+
+/**
+ * Template variables.
+ *
+ * @var string $title
+ * @var string $cover
+ * @var array  $track
+ */
+?>
+
+<amp-audio
+	width="100%"
+	height="40"
+	artwork="<?php echo esc_url( $cover ); ?>"
+	artist="<?php echo esc_attr( $title ); ?>"
+	src="<?php echo esc_url( $track['src'] ); ?>"
+	title="<?php echo esc_attr( $track['title'] ); ?>"
+	[src]="podcastPlayer.tracks[currentTrack].src"
+	[title]="podcastPlayer.tracks[currentTrack].title"
+>
+	<p fallback>
+		<a [href]="podcastPlayer.tracks[currentTrack].link" href="<?php echo esc_url( $track['link'] ); ?>">
+			<?php esc_html_e( 'Open episode page', 'jetpack' ); ?>
+		</a>
+		<a download [href]="podcastPlayer.tracks[currentTrack].src" href="<?php echo esc_url( $track['src'] ); ?>">
+			<?php esc_html_e( 'Download audio', 'jetpack' ); ?>
+		</a>
+	</p>
+</amp-audio>
--- extensions/blocks/podcast-player/templates/podcast-header.php
+++ extensions/blocks/podcast-player/templates/podcast-header.php
@@ -71,25 +71,16 @@ $track  = ( is_array( $tracks ) && ! empty( $tracks ) ) ? $tracks[0] : array();
 
 	<div class="jetpack-podcast-player__audio-player">
 		<?php if ( $is_amp ) : ?>
-			<amp-audio
-				width="100%"
-				height="40"
-				artwork="<?php echo esc_url( $cover ); ?>"
-				artist="<?php echo esc_attr( $title ); ?>"
-				src="<?php echo esc_url( $track['src'] ); ?>"
-				title="<?php echo esc_attr( $track['title'] ); ?>"
-				[src]="podcastPlayer.tracks[currentTrack].src"
-				[title]="podcastPlayer.tracks[currentTrack].title"
-			>
-				<p fallback>
-					<a [href]="podcastPlayer.tracks[currentTrack].link" href="<?php echo esc_url( $track['link'] ); ?>">
-						<?php esc_html_e( 'Open episode page', 'jetpack' ); ?>
-					</a>
-					<a download [href]="podcastPlayer.tracks[currentTrack].src" href="<?php echo esc_url( $track['src'] ); ?>">
-						<?php esc_html_e( 'Download audio', 'jetpack' ); ?>
-					</a>
-				</p>
-			</amp-audio>
+			<?php
+			render(
+				'amp-audio',
+				array(
+					'cover' => $cover,
+					'title' => $title,
+					'track' => $track,
+				)
+			);
+			?>
 		<?php else : ?>
 			<div class="jetpack-podcast-player--audio-player-loading"></div>
 		<?php endif; ?>
