afacc22062397046d958e5b8e060df1e77ed80f0
pidgeonman <2166135+pidgeonman@users.noreply.github.com>
2020-04-22 23:39:11 -0300

    More refactoring.

--- packages/jitm/src/class-engine.php
+++ packages/jitm/src/class-engine.php
@@ -17,93 +17,43 @@ use Automattic\Jetpack\JITMS\Message;
 class Engine {
 
 	/**
-	 * Is mobile browser
+	 * Returns the pre-connection JITMs rules
 	 *
-	 * @var bool $mobile_browser.
+	 * @return array Pre-connection JITMs rules.
 	 */
-	private $mobile_browser;
-
-	/**
-	 * Returns the default rules
-	 *
-	 * @return array Default rules.
-	 */
-	public function default_rules() {
-		$rules = array_merge(
-			$this->preconnection_default_rules()
-		);
-
-		return apply_filters( 'jetpack_jitm_rules', $rules );
-	}
-
-	/**
-	 * Returns the pre-connection JITMs default rules
-	 *
-	 * @return array Pre-connection JITMs default rules.
-	 */
-	private function preconnection_default_rules() {
+	private function preconnection_rules() {
 
 		return array(
-			( new Message( 'jpsetup-posts', 'pre-connect' ) )
-				->user_is( 'administrator' )
-				->with_icon()
+			( new Message( 'jpsetup-posts' ) )
 				->message_path( '/wp:edit-post:admin_notices/' )
 				->show(
 					__( 'Do you know which of these posts gets the most traffic?', 'jetpack' ),
 					__( 'Setup Jetpack to get in-depth stats about your content and visitors.', 'jetpack' )
 				)
-				->priority( 100 )
 				->with_cta(
 					__( 'Setup Jetpack', 'jetpack' ),
-					'',
-					function() {
-						return esc_url( \Jetpack::init()->build_connect_url( true, false, 'pre-connection-jitm-posts' ) );
-					}
-				)
-				->open_cta_in_same_window()
-				->is_dismissible( true )
-				->priority( 1000 )
-				->is_hosted_with_partner( 'bluehost' ),
-			( new Message( 'jpsetup-upload', 'pre-connect' ) )
-				->user_is( 'administrator' )
-				->with_icon()
+					esc_url( \Jetpack::init()->build_connect_url( true, false, 'pre-connection-jitm-posts' ) )
+				),
+			( new Message( 'jpsetup-upload' ) )
 				->message_path( '/wp:upload:admin_notices/' )
 				->show(
 					__( 'Do you want lightning-fast images?', 'jetpack' ),
 					__( 'Setup Jetpack, enable Site Accelerator, and start serving your images lightning fast, for free.', 'jetpack' )
 				)
-				->priority( 100 )
 				->with_cta(
 					__( 'Setup Jetpack', 'jetpack' ),
-					'',
-					function() {
-						return esc_url( \Jetpack::init()->build_connect_url( true, false, 'pre-connection-jitm-upload' ) );
-					}
-				)
-				->open_cta_in_same_window()
-				->is_dismissible( true )
-				->priority( 1000 )
-				->is_hosted_with_partner( 'bluehost' ),
-			( new Message( 'jpsetup-widgets', 'pre-connect' ) )
-				->user_is( 'administrator' )
-				->with_icon()
+					esc_url( \Jetpack::init()->build_connect_url( true, false, 'pre-connection-jitm-upload' ) )
+				),
+			( new Message( 'jpsetup-widgets' ) )
 				->message_path( '/wp:widgets:admin_notices/' )
 				->show(
 					__( 'Looking for even more widgets?', 'jetpack' ),
 					__( 'Setup Jetpack for great additional widgets like business hours and maps.', 'jetpack' )
 				)
-				->priority( 100 )
 				->with_cta(
 					__( 'Setup Jetpack', 'jetpack' ),
-					'',
-					function() {
-						return esc_url( \Jetpack::init()->build_connect_url( true, false, 'pre-connection-jitm-widgets' ) );
-					}
-				)
-				->open_cta_in_same_window()
-				->is_dismissible( true )
-				->priority( 1000 )
-				->is_hosted_with_partner( 'bluehost' ),
+					esc_url( \Jetpack::init()->build_connect_url( true, false, 'pre-connection-jitm-widgets' ) )
+				),
 		);
 	}
 
@@ -111,76 +61,26 @@ class Engine {
 	 * Gets the top messages
 	 *
 	 * @param string $message_path Message path.
-	 * @param string $user_level User level.
-	 * @param string $query Message query.
-	 * @param bool   $mobile_browser Uses mobile browser.
 	 *
 	 * @return array Rendered messages.
 	 */
-	public function get_top_messages( $message_path, $user_level = '', $query = '', $mobile_browser = false ) {
-		$rules = $this->default_rules();
-
-		if ( is_string( $user_level ) ) {
-			$user_level = explode( ',', $user_level );
-		} else {
-			$user_level = array();
-		}
-
-		$rules = array_reduce(
-			$rules,
-			function ( $return, $rule ) use ( $message_path, $user_level, $query, $mobile_browser ) {
-				$score = $rule->score( $message_path, $user_level, $query, $mobile_browser );
-				if ( $score > $return[0] ) {
-					$return = array( $score, array( $rule->render() ), array( $rule ) );
-				} elseif ( $score === $return[0] && $score > 0 ) {
-					$return[1][] = $rule->render();
-					$return[2][] = $rule;
-				}
-
-				return $return;
-			},
-			array( 0, array(), array() )
-		);
+	public function render_messages( $message_path ) {
+		$rules = $this->preconnection_rules();
 
 		$rendered_rules = array();
 
-		foreach ( $rules[2] as $rule ) {
+		foreach ( $rules as $rule ) {
+
+			if ( ! preg_match( $rule->message_path_regex, $message_path ) ) {
+				continue;
+			}
+
+			// TODO: add partner filter here.
+
 			$rendered_rules[] = $rule->render();
 		}
 
-		// get the top message which hasn't been dismissed.
 		return $rendered_rules;
-	}
-
-	/**
-	 * Dismisses a JITM feature class so that it will no longer be shown
-	 *
-	 * @param string $id The id of the JITM that was dismissed.
-	 * @param string $feature_class The feature class of the JITM that was dismissed.
-	 *
-	 * @return bool Always true
-	 */
-	public static function dismiss( $id, $feature_class ) {
-		$hide_jitm = \Jetpack_Options::get_option( 'hide_jitm' );
-		if ( ! is_array( $hide_jitm ) ) {
-			$hide_jitm = array();
-		}
-
-		if ( empty( $hide_jitm[ $feature_class ] ) || ! is_array( $hide_jitm[ $feature_class ] ) ) {
-			$hide_jitm[ $feature_class ] = array(
-				'last_dismissal' => 0,
-				'number'         => 0,
-			);
-		}
-
-		$hide_jitm[ $feature_class ] = array(
-			'last_dismissal' => time(),
-			'number'         => intval( $hide_jitm[ $feature_class ]['number'] ) + 1,
-		);
-
-		\Jetpack_Options::update_option( 'hide_jitm', $hide_jitm );
-
-		return true;
 	}
 
 }
--- packages/jitm/src/class-pre-connection-jitm.php
+++ packages/jitm/src/class-pre-connection-jitm.php
@@ -7,15 +7,11 @@
 
 namespace Automattic\Jetpack\JITMS;
 
-use Automattic\Jetpack\Connection\Client;
-use Automattic\Jetpack\Partner;
 use Automattic\Jetpack\JITMS\JITM;
 use Automattic\Jetpack\JITMS\Engine;
 
 /**
  * Jetpack just in time messaging through out the admin
- *
- * @since 5.6.0
  */
 class Pre_Connection_JITM extends JITM {
 
@@ -35,7 +31,7 @@ class Pre_Connection_JITM extends JITM {
 	 * Retrieve the current message to display keyed on query string and message path
 	 *
 	 * @param string $message_path The message path to ask for.
-	 * @param string $query The query string originally from the front end.
+	 * @param string $query The query string originally from the front end. Unused in this subclass.
 	 * @param bool   $full_jp_logo_exists If there is a full Jetpack logo already on the page.
 	 *
 	 * @return array The JITM's to show, or an empty array if there is nothing to show
@@ -43,26 +39,7 @@ class Pre_Connection_JITM extends JITM {
 	public function get_messages( $message_path, $query, $full_jp_logo_exists ) {
 		$jitm_engine = new Engine();
 
-		$query_string = array();
-		if ( isset( $query ) ) {
-			foreach ( explode( ',', $query ) as $query_item ) {
-				$query_item                     = explode( '=', $query_item );
-				$query_string[ $query_item[0] ] = isset( $query_item[1] ) ? $query_item[1] : null;
-			}
-			unset( $query_item );
-		}
-
-		$mobile_browser = jetpack_is_mobile( 'smart' );
-		$user           = wp_get_current_user();
-
-		// Unauthenticated or invalid requests just bail.
-		if ( ! $user ) {
-			return array();
-		}
-
-		$user_roles = implode( ',', $user->roles );
-
-		$envelopes = $jitm_engine->get_top_messages( $message_path, $user_roles, $query_string, $mobile_browser );
+		$envelopes = $jitm_engine->render_messages( $message_path );
 
 		if ( ! is_array( $envelopes ) ) {
 			return array();
@@ -70,57 +47,11 @@ class Pre_Connection_JITM extends JITM {
 
 		$hidden_jitms = \Jetpack_Options::get_option( 'hide_jitm' );
 
-		unset( $envelopes['last_response_time'] );
-
-		/**
-		 * Allow adding your own custom JITMs after a set of JITMs has been received.
-		 *
-		 * @since 6.9.0
-		 * @since 8.3.0 - Added Message path.
-		 *
-		 * @param array  $envelopes    array of existing JITMs.
-		 * @param string $message_path The message path to ask for.
-		 */
-		$envelopes = apply_filters( 'jetpack_jitm_received_envelopes', $envelopes, $message_path );
-
 		foreach ( $envelopes as $idx => &$envelope ) {
+			$dismissed_feature = isset( $hidden_jitms[ 'pre-connection-' . $envelope->id ] ) &&
+				is_array( $hidden_jitms[ 'pre-connection-' . $envelope->id ] ) ? $hidden_jitms[ 'pre-connection-' . $envelope->id ] : null;
 
-			$dismissed_feature = isset( $hidden_jitms[ $envelope->feature_class ] ) && is_array( $hidden_jitms[ $envelope->feature_class ] ) ? $hidden_jitms[ $envelope->feature_class ] : null;
-
-			// If the this feature class has been dismissed and the request has not passed the ttl, skip it as it's been dismissed.
-			if ( is_array( $dismissed_feature ) && ( time() - $dismissed_feature['last_dismissal'] < $envelope->expires || $dismissed_feature['number'] >= $envelope->max_dismissal ) ) {
-				unset( $envelopes[ $idx ] );
-				continue;
-			}
-
-			$normalized_site_url = \Jetpack::build_raw_urls( get_home_url() );
-
-			$url_params = array(
-				'source' => "jitm-$envelope->id",
-				'site'   => $normalized_site_url,
-				'u'      => $user->ID,
-			);
-
-			// Get affiliate code and add it to the array of URL parameters.
-			$aff = Partner::init()->get_partner_code( Partner::AFFILIATE_CODE );
-			if ( '' !== $aff ) {
-				$url_params['aff'] = $aff;
-			}
-
-			$envelope->url = add_query_arg( $url_params, 'https://jetpack.com/redirect/' );
-
-			if ( $envelope->cta['hook'] ) {
-				$envelope->url = apply_filters( 'jitm_' . $envelope->cta['hook'], $envelope->url );
-				unset( $envelope->cta['hook'] );
-			}
-
-			if ( isset( $envelope->content['hook'] ) ) {
-				$envelope->content = apply_filters( 'jitm_' . $envelope->content['hook'], $envelope->content );
-				unset( $envelope->content['hook'] );
-			}
-
-			// No point in showing an empty message.
-			if ( empty( $envelope->content['message'] ) ) {
+			if ( is_array( $dismissed_feature ) ) {
 				unset( $envelopes[ $idx ] );
 				continue;
 			}
@@ -131,4 +62,34 @@ class Pre_Connection_JITM extends JITM {
 		return $envelopes;
 	}
 
+	/**
+	 * Dismisses a JITM ID so that it will no longer be shown.
+	 *
+	 * @param string $id The id of the JITM that was dismissed.
+	 * @param string $feature_class The feature class of the JITM that was dismissed. Unused in this subclass.
+	 *
+	 * @return bool Always true
+	 */
+	public function dismiss( $id, $feature_class ) {
+		$hide_jitm = \Jetpack_Options::get_option( 'hide_jitm' );
+		if ( ! is_array( $hide_jitm ) ) {
+			$hide_jitm = array();
+		}
+
+		if ( empty( $hide_jitm[ 'pre-connection-' . $id ] ) || ! is_array( $hide_jitm[ 'pre-connection-' . $id ] ) ) {
+			$hide_jitm[ $id ] = array(
+				'last_dismissal' => 0,
+				'number'         => 0,
+			);
+		}
+
+		$hide_jitm[ 'pre-connection-' . $id ] = array(
+			'last_dismissal' => time(),
+			'number'         => intval( $hide_jitm[ 'pre-connection-' . $id ]['number'] ) + 1,
+		);
+
+		\Jetpack_Options::update_option( 'hide_jitm', $hide_jitm );
+
+		return true;
+	}
 }
