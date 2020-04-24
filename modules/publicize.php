8681798f5350d780af7ec1075d47170fa3c1be44
Brandon Kraft <public@brandonkraft.com>
2020-04-24 15:33:10 -0500

    Look at a filtered option for jetpack_publicize_options

--- modules/publicize.php
+++ modules/publicize.php
@@ -58,6 +58,38 @@ class Jetpack_Publicize {
 			if ( in_array( 'publicize', $active, true ) && ! in_array( 'sharedaddy', $active, true ) ) {
 				add_action( 'admin_menu', array( &$publicize_ui, 'sharing_menu' ) );
 			}
+
+			/*
+			 * The Publicize Options array does not currently have UI since it is being added
+			 * for a specific purpose and not part of a broader Publicize sprint.
+			 *
+			 * In order to pass the settings up to WordPress.com, we are updating an option to Sync will pass it up.
+			 * To make it relatively easy for use, we are creating a filter that checks if the option and filter match.
+			 *
+			 * This only runs when a post is saved to avoid it running too much.
+			 */
+			add_action(
+				'save_post',
+				function() {
+					$publicize_options = get_option( 'jetpack_publicize_options', array() );
+
+					/**
+					 * Filters the options for Publicize.
+					 *
+					 * As of Jetpack 8.5, the array keys could be:
+					 * attach_media bool If Publicize should send the image to the social media platform. Default false.
+					 *
+					 * @since 8.5.0
+					 *
+					 * @param array $options Array of Publicize options.
+					 */
+					$filtered = (array) apply_filters( 'jetpack_publicize_options', $publicize_options );
+
+					if ( $publicize_options !== $filtered ) {
+						update_option( 'jetpack_publicize_options', $filtered, false );
+					}
+				}
+			);
 		}
 	}
 }
