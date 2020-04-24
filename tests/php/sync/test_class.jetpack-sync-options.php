adb23a4e7424c88bce3f2f831248326d6ce1f569
Brandon Kraft <public@brandonkraft.com>
2020-04-24 15:38:41 -0500

    Replace test.

--- tests/php/sync/test_class.jetpack-sync-options.php
+++ tests/php/sync/test_class.jetpack-sync-options.php
@@ -203,6 +203,7 @@ class WP_Test_Jetpack_Sync_Options extends WP_Test_Jetpack_Sync_Base {
 			'site_vertical'                            => 'pineapple',
 			'jetpack_excluded_extensions'              => 'pineapple',
 			'jetpack-memberships-connected-account-id' => '340',
+			'jetpack_publicize_options'                => array(),
 		);
 
 		$theme_mod_key             = 'theme_mods_' . get_option( 'stylesheet' );
