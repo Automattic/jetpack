fd732bd93471b44eef63fc4908f09c33485e62ba
leogermani <leogermani@gmail.com>
2020-04-27 18:59:57 -0300

    adds support for direct URL to getRedirectUrl lib (#15585)

--- _inc/client/lib/jp-redirect/index.js
+++ _inc/client/lib/jp-redirect/index.js
@@ -1,9 +1,15 @@
 /**
  * Builds an URL using the jetpack.com/redirect service
  *
+ * If $source is a simple slug, it will be sent using the source query parameter. e.g. jetpack.com/redirect?source=slug
+ *
+ * If $source is a full URL, starting with https://, it will be sent using the url query parameter. e.g. jetpack.com/redirect?url=https://wordpress.com
+ *
+ * Note: if using full URL, query parameters and anchor must be passed in $args. Any querystring of url fragment in the URL will be discarded.
+ *
  * @since 8.5.0
  *
- * @param {string}  source The URL handler registered in the server
+ * @param {string}  source The URL handler registered in the server or the full destination URL (starting with https://).
  * @param {object}  args {
  *
  * 		Additional arguments to build the url
@@ -14,12 +20,20 @@
  * 		@type {string} anchor Anchor to be added to the URL
  * }
  *
- * @return {string} The redirect URL
+ * @returns {string} The redirect URL
  */
 export default function getRedirectUrl( source, args = {} ) {
-	const queryVars = {
-		source: source,
-	};
+	const queryVars = {};
+
+	if ( source.search( 'https://' ) === 0 ) {
+		const parsedUrl = new URL( source );
+
+		// discard any query and fragments.
+		source = `https://${ parsedUrl.host }${ parsedUrl.pathname }`;
+		queryVars.url = encodeURIComponent( source );
+	} else {
+		queryVars.source = encodeURIComponent( source );
+	}
 
 	const acceptedArgs = [ 'site', 'path', 'query', 'anchor' ];
 
