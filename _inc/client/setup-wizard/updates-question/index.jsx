5f1f7a7445823061aff4cd47170dce320218ec33
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2020-04-27 17:20:13 -0500

    Create UpdatesQuestion component

---
+++ _inc/client/setup-wizard/updates-question/index.jsx
@@ -0,0 +1,11 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { translate as __ } from 'i18n-calypso';
+
+const UpdatesPage = () => {
+	return <div>Hello world! from the updates page</div>;
+};
+
+export { UpdatesPage };
