b84d217ae10b26f997f81be23d7e5a0e888750d6
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2020-04-28 11:56:46 -0500

    Add updates answers and skip link

--- _inc/client/setup-wizard/updates-question/index.jsx
+++ _inc/client/setup-wizard/updates-question/index.jsx
@@ -7,11 +7,14 @@ import { translate as __ } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
+import Button from 'components/button';
 import { imagePath } from 'constants/urls';
+
+import './style.scss';
 
 const UpdatesQuestion = props => {
 	return (
-		<div className="jp-setup-wizard-main">
+		<div className="jp-setup-wizard-main jp-setup-wizard-updates-main">
 			<img
 				width="200px"
 				height="200px"
@@ -23,6 +26,17 @@ const UpdatesQuestion = props => {
 					args: { siteTitle: props.siteTitle },
 				} ) }
 			</h1>
+			<div className="jp-setup-wizard-updates-answer-buttons-container">
+				<Button href="" primary className="jp-setup-wizard-updates-button">
+					{ __( 'Yes' ) }
+				</Button>
+				<Button href="" className="jp-setup-wizard-updates-button">
+					{ __( 'No' ) }
+				</Button>
+			</div>
+			<a className="jp-setup-wizard-skip-link" href="">
+				{ __( 'Skip' ) }
+			</a>
 		</div>
 	);
 };
