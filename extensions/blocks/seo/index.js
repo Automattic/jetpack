5071e945607056fcbbe18cf9f4c57d2a46669cee
Mikael Korpela <mikael@ihminen.org>
2020-04-29 10:14:27 +0300

    SEO description: enable in Editor's Document sidebar for .com sites

--- extensions/blocks/seo/index.js
+++ extensions/blocks/seo/index.js
@@ -4,22 +4,37 @@
 import { __ } from '@wordpress/i18n';
 import { Fragment } from '@wordpress/element';
 import { PanelBody } from '@wordpress/components';
-import { PluginPrePublishPanel } from '@wordpress/edit-post';
+import { PluginDocumentSettingPanel, PluginPrePublishPanel } from '@wordpress/edit-post';
 
 /**
  * Internal dependencies
  */
 import './editor.scss';
+import { isSimpleSite, isAtomicSite } from '../../shared/site-type-utils';
+import JetpackLogo from '../../shared/jetpack-logo';
 import JetpackPluginSidebar from '../../shared/jetpack-plugin-sidebar';
 import SeoPanel from './panel';
+
+const isWpcom = isSimpleSite() || isAtomicSite();
+const title = __( 'SEO Description', 'jetpack' );
 
 export const name = 'seo';
 
 export const settings = {
 	render: () => (
 		<Fragment>
+			{ // On WordPress.com the panel is in "Document" tab of the default editor sidebar
+			isWpcom && (
+				<PluginDocumentSettingPanel
+					icon={ <JetpackLogo /> }
+					name="jetpack-seo-description"
+					title={ title }
+				>
+					<SeoPanel />
+				</PluginDocumentSettingPanel>
+			) }
 			<JetpackPluginSidebar>
-				<PanelBody title={ __( 'SEO Description', 'jetpack' ) }>
+				<PanelBody title={ title }>
 					<SeoPanel />
 				</PanelBody>
 			</JetpackPluginSidebar>
@@ -28,7 +43,7 @@ export const settings = {
 				id="seo-title"
 				title={
 					<span id="seo-defaults" key="seo-title-span">
-						{ __( 'SEO Description', 'jetpack' ) }
+						{ title }
 					</span>
 				}
 			>
