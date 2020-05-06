5b344dd285b46b74d6d6e3ce1765a9d6e45e4104
Brandon Kraft <public@brandonkraft.com>
2020-05-05 23:34:49 -0500

    Verify docblock has module and since information

--- packages/coding-standards/Jetpack/Sniffs/InlineDocs/HooksMustHaveDocblockSniff.php
+++ packages/coding-standards/Jetpack/Sniffs/InlineDocs/HooksMustHaveDocblockSniff.php
@@ -10,15 +10,25 @@ namespace Automattic\Jetpack\CodingStandards\Sniffs\InlineDocs;
 use WordPressCS\WordPress\AbstractFunctionRestrictionsSniff;
 use PHP_CodeSniffer\Util\Tokens;
 
+/**
+ * Class HooksMustHaveDocblockSniff
+ *
+ * @package Automattic\Jetpack\CodingStandards\Sniffs\InlineDocs
+ */
 class HooksMustHaveDocblockSniff extends AbstractFunctionRestrictionsSniff {
 
+	/**
+	 * Array of WordPress hook execution functions.
+	 *
+	 * @var array WordPress hook execution function name => filter or action.
+	 */
 	protected $hook_functions = array(
-		'apply_filters' => 'filter',
-		'apply_filters_ref_array' => 'filter',
+		'apply_filters'            => 'filter',
+		'apply_filters_ref_array'  => 'filter',
 		'apply_filters_deprecated' => 'filter',
-		'do_action' => 'action',
-		'do_action_ref_array' => 'action',
-		'do_action_deprecated' => 'action',
+		'do_action'                => 'action',
+		'do_action_ref_array'      => 'action',
+		'do_action_deprecated'     => 'action',
 	);
 
 	/**
@@ -58,28 +68,19 @@ class HooksMustHaveDocblockSniff extends AbstractFunctionRestrictionsSniff {
 
 		$func_open_paren_token = $this->phpcsFile->findNext( Tokens::$emptyTokens, ( $stack_ptr + 1 ), null, true );
 		if ( false === $func_open_paren_token
-		     || \T_OPEN_PARENTHESIS !== $this->tokens[ $func_open_paren_token ]['code']
-		     || ! isset( $this->tokens[ $func_open_paren_token ]['parenthesis_closer'] )
+			|| \T_OPEN_PARENTHESIS !== $this->tokens[ $func_open_paren_token ]['code']
+			|| ! isset( $this->tokens[ $func_open_paren_token ]['parenthesis_closer'] )
 		) {
 			// Live coding, parse error or not a function call.
 			return;
 		}
 
-		/*
-		if ( \in_array( $matched_content, array( 'apply_filters' ), true ) ) {
-			$this->phpcsFile->addWarning( 'Use of the "%s()" function is reserved for low-level API usage.', $stack_ptr, 'LowLevelTranslationFunction', array( $matched_content ) );
-		}
-		*/
-
 		$previous_comment = $this->phpcsFile->findPrevious( Tokens::$commentTokens, ( $stack_ptr - 1 ) );
 
 		if ( false !== $previous_comment ) {
 			/*
-			 * Check that the comment is either on the line before the gettext call or
-			 * if it's not, that there is only whitespace between.
+			 * Check to determine if there is a comment immediately preceding the function call.
 			 */
-			$correctly_placed = false;
-
 			if ( ( $this->tokens[ $previous_comment ]['line'] + 1 ) !== $this->tokens[ $stack_ptr ]['line'] ) {
 				$this->phpcsFile->addError(
 					'The inline documentation for a hook must be on the line immediately before the function call.',
@@ -97,26 +98,60 @@ class HooksMustHaveDocblockSniff extends AbstractFunctionRestrictionsSniff {
 					$stack_ptr,
 					'NoDocblockFound'
 				);
-			//	return; Do we need to return here? Can we keep going?
+				// return; Do we need to return here? Can we keep going?
 			}
-return; //stuff below this isn't ready. We probably need to create an interator class that walks the docblock comment looking for particular things.
 
-				if ( \T_DOC_COMMENT_CLOSE_TAG === $this->tokens[ $previous_comment ]['code'] ) {
-					$db_start = $this->phpcsFile->findPrevious( \T_DOC_COMMENT_OPEN_TAG, ( $previous_comment - 1 ) );
-					$db_text  = $this->phpcsFile->findNext( \T_DOC_COMMENT_STRING, ( $db_start + 1 ), $previous_comment );
+			/*
+			 * Process docblock tags.
+			 */
+			$comment_end   = $previous_comment;
+			$comment_start = ( isset( $this->tokens[ $comment_end ]['comment_opener'] ) ) ? $this->tokens[ $comment_end ]['comment_opener'] : false;
+			$has           = array(
+				'module' => false,
+				'since'  => false,
+			);
 
-					if ( 0 === strpos( $db_text, '@since') ) {
+			// The comment isn't a docblock, so we're going to stop here.
+			if ( ! $comment_start ) {
+				return;
+			}
 
+			$string = $this->phpcsFile->findNext( T_DOC_COMMENT_STRING, $comment_start, $comment_end );
+			// If the call is documented elsewhere, stop here.
+			if ( 0 === strpos( $this->tokens[ $string ]['content'], 'This filter is documented in' ) ) {
+				return;
+			}
+
+			foreach ( $this->tokens[ $comment_start ]['comment_tags'] as $tag ) {
+				// Is the next tag of the docblock the "@module" tag?
+				if ( '@module' === $this->tokens[ $tag ]['content'] ) {
+					// This is used later to determine if we need to throw an error for no module tag.
+					$has['module'] = true;
+
+					// Find the next string, which will be the text after the @module.
+					$string = $this->phpcsFile->findNext( T_DOC_COMMENT_STRING, $tag, $comment_end );
+					// If it is false, there is no text or if the text is on the another line, error.
+					if ( false === $string || $this->tokens[ $string ]['line'] !== $this->tokens[ $tag ]['line'] ) {
+						$this->phpcsFile->addError( 'Module tag must have a value.', $tag, 'EmptyModule' );
 					}
-
-
-						$this->phpcsFile->addWarning(
-							 'example: ' . $this->tokens[ $db_first_text ]['content'],
-							 $stack_ptr,
-							'NoSinceParam'
-						);
-						return;
 				}
+
+				if ( '@since' === $this->tokens[ $tag ]['content'] ) {
+					$has['since'] = true;
+					$string       = $this->phpcsFile->findNext( T_DOC_COMMENT_STRING, $tag, $comment_end );
+					if ( false === $string || $this->tokens[ $string ]['line'] !== $this->tokens[ $tag ]['line'] ) {
+						$this->phpcsFile->addError( 'Since tag must have a value.', $tag, 'EmptySince' );
+					} elseif ( ! preg_match( '\'/^\d+\.\d+\.\d+/\'', $string ) ) { // Requires X.Y.Z. Trailing 0 is needed for a major release.
+						$this->phpcsFile->addError( 'Since tag must have a X.Y.Z. version number.', $tag, 'InvalidSince' );
+					}
+				}
+			}
+
+			foreach ( $has as $name => $present ) {
+				if ( ! $present ) {
+					$this->phpcsFile->addError( 'Hook documentation is missing a tag: ' . $name, $comment_start, 'No' . ucfirst( $name ) );
+				}
+			}
 		}
 	}
 }
