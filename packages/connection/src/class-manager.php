aea53d247f57c92721ae076f9e57fd0939b80617
Sergey Mitroshin <sergeymitr@gmail.com>
2020-05-07 08:50:09 -0500

    Connection Package: Handle disconnections gracefully (#15456)
    * Connection Package: Handle disconnections gracefully, basic functionality and tests.
    
    The commit introduces following improvements:
    - Classes `Plugin` and `Plugin_Storage` that handle the plugin connections.
    - Changes in the `Manager` class that check if the disconnecting plugin is the only active connection, and don't let it remove the connection otherwise.
    - Unit tests for `Plugin`, `Plugin_Storage` and `Manager` classes, including new function mocks to imitate WordPress behavior.
    - Adjustment of the existing package code to make it in line with WordPress coding standards.
    
    * Connection Package: initialization improvements, plugin storage changes.
    
    Following improvements are introduced by the commit:
    - Initialization of the connection via `Jetpack_Config` now allows providing connection options.
    - Jetpack plugin (`class.jetpack.php`) provides its information to the connection manager.
    - Plugin information is no longer stored in the `options`. In fact, it's not saved to any permanent storage now, the data is retrieved dynamically via action call.
    - Cleaner `Manager` initialization: constructor accepts `plugin_slug` instead of the Plugin object.
    - Unit tests are modified to accompany the changes above.
    
    * Connection Package: display other connected plugins in the "Jetpack Disconnect" modal.
    
    - The "load active plugins" REST API endpoint.
    - The UI changes to load these plugins and display them in the modal.
    
    * Connection Package: Minor fixes and refactoring.
    
    - plural for messages
    - safer data handling by React
    - stricter endpoint permissions callback
    - elaborate PHPDoc class descriptions
    - reverting the checks for other connected plugins before disconnecting (will be introduced in a separate PR)
    - adjusting documentation according to the changes in the package API
    
    * Connection Package: Refactoring, improved unit testing.
    
    - Minor refactoring
    - Fixed whitelist filter for plugin arguments
    - Unit testing of the plugin arguments filter
    
    * Connection Package: Connected plugins list can't be retrieved before configuration is completed.
    
    - I added a static method into `Jetpack\Config` to check whether the configuration is pending (if needed).
    - Modified the `Plugin_Storage` to not allow retrieving the list of connected plugins until the configuration has finished (if at least one `Config` object has been created).
    - Adjusted the `connection/plugins` REST endpoint to return `WP_Error` if the list of connected plugins is not available.
    
    * Connection Package: Connected Plugins REST API error handling, minor adjustments.
    
    - if the Connected Plugins endpoint returns an error, we still display the "Disconnect" modal with no "Connected Plugins" section;
    - better wording for the "Connected Plugins" section in the "Disconnect" modal;
    - making sure that `Plugin_Storage::get_one()` and `delete()` don't get called too early (it was previously implemented for `Plugin_Storage::get_all()`).

--- packages/connection/src/class-manager.php
+++ packages/connection/src/class-manager.php
@@ -45,6 +45,27 @@ class Manager {
 	private $xmlrpc_verification = null;
 
 	/**
+	 * Plugin management object.
+	 *
+	 * @var Plugin
+	 */
+	private $plugin = null;
+
+	/**
+	 * Initialize the object.
+	 * Make sure to call the "Configure" first.
+	 *
+	 * @param string $plugin_slug Slug of the plugin using the connection (optional, but encouraged).
+	 *
+	 * @see \Automattic\Jetpack\Config
+	 */
+	public function __construct( $plugin_slug = null ) {
+		if ( $plugin_slug && is_string( $plugin_slug ) ) {
+			$this->set_plugin_instance( new Plugin( $plugin_slug ) );
+		}
+	}
+
+	/**
 	 * Initializes required listeners. This is done separately from the constructors
 	 * because some objects sometimes need to instantiate separate objects of this class.
 	 *
@@ -1607,6 +1628,8 @@ class Manager {
 			return new \WP_Error( 'scope', 'No Scope', $code );
 		}
 
+		// TODO: get rid of the error silencer.
+		// phpcs:ignore WordPress.PHP.NoSilencedErrors.Discouraged
 		@list( $role, $hmac ) = explode( ':', $json->scope );
 		if ( empty( $role ) || empty( $hmac ) ) {
 			return new \WP_Error( 'scope', 'Malformed Scope', $code );
@@ -2206,4 +2229,36 @@ class Manager {
 
 		return $role . ':' . hash_hmac( 'md5', "{$role}|{$user_id}", $token->secret );
 	}
+
+	/**
+	 * Set the plugin instance.
+	 *
+	 * @param Plugin $plugin_instance The plugin instance.
+	 *
+	 * @return $this
+	 */
+	public function set_plugin_instance( Plugin $plugin_instance ) {
+		$this->plugin = $plugin_instance;
+
+		return $this;
+	}
+
+	/**
+	 * Retrieve the plugin management object.
+	 *
+	 * @return Plugin
+	 */
+	public function get_plugin() {
+		return $this->plugin;
+	}
+
+	/**
+	 * Get all connected plugins information.
+	 *
+	 * @return array|\WP_Error
+	 */
+	public function get_connected_plugins() {
+		return Plugin_Storage::get_all();
+	}
+
 }
