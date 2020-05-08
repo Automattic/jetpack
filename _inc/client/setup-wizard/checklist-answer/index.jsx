10a4a96c1f978ae5e62c52f8980faba12b93ee86
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2020-05-08 10:22:19 -0500

    Convert arrow functions to function functions

--- _inc/client/setup-wizard/checklist-answer/index.jsx
+++ _inc/client/setup-wizard/checklist-answer/index.jsx
@@ -3,7 +3,7 @@
  */
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 /**
  * Internal dependencies
@@ -12,96 +12,89 @@ import Gridicon from 'components/gridicon';
 
 import './style.scss';
 
-class ChecklistAnswer extends Component {
-	constructor( props ) {
-		super( props );
-		this.state = { checked: false, expanded: false, windowWidth: '' };
+const ChecklistAnswer = props => {
+	const [ checked, setChecked ] = useState( false );
+	const [ expanded, setExpanded ] = useState( false );
+	const [ windowWidth, setWindowWidth ] = useState( false );
+
+	function handleResize() {
+		setWindowWidth( window.innerWidth <= 660 ? 'small' : 'large' );
 	}
 
-	componentDidMount = () => {
-		this.handleResize(); // Call this once on mount to initialize state.windowWidth
-		window.addEventListener( 'resize', this.handleResize );
-	};
+	useEffect( () => {
+		handleResize(); // Call this once to make sure windowWidth is initialized
+		window.addEventListener( 'resize', handleResize );
+		return () => {
+			window.removeEventListener( 'resize', handleResize );
+		};
+	} );
 
-	componentWillUnmount = () => {
-		window.removeEventListener( 'resize', this.handleResize );
-	};
-
-	handleResize = () => {
-		const windowWidth = window.innerWidth <= 660 ? 'small' : 'large';
-		this.setState( { windowWidth } );
-	};
-
-	toggleCheckboxLargeWindow = () => {
-		if ( 'small' === this.state.windowWidth ) {
+	function toggleCheckboxLargeWindow() {
+		if ( 'small' === windowWidth ) {
 			return;
 		}
 
-		this.setState( { checked: ! this.state.checked } );
-	};
+		setChecked( ! checked );
+	}
 
-	toggleCheckboxSmallWindow = () => {
-		if ( 'large' === this.state.windowWidth ) {
+	function toggleCheckboxSmallWindow() {
+		if ( 'large' === windowWidth ) {
 			return;
 		}
 
-		this.setState( { checked: ! this.state.checked } );
-	};
+		setChecked( ! checked );
+	}
 
-	toggleExpanded = () => {
-		this.setState( { expanded: ! this.state.expanded } );
-	};
+	function toggleExpanded() {
+		setExpanded( ! expanded );
+	}
 
-	render() {
-		const { checked, expanded } = this.state;
+	const smallWindow = 'small' === windowWidth;
 
-		const smallWindow = 'small' === this.state.windowWidth;
+	const chevronIcon = expanded ? 'chevron-up' : 'chevron-down';
 
-		const chevronIcon = expanded ? 'chevron-up' : 'chevron-down';
-
-		return (
-			<div
-				className={ classNames( 'jp-checklist-answer-container', { checked } ) }
-				onClick={ this.toggleCheckboxLargeWindow }
-				onKeyPress={ this.toggleCheckboxLargeWindow }
-				role="checkbox"
-				aria-checked={ checked }
-				tabIndex={ smallWindow ? -1 : 0 }
-			>
-				<div className="jp-checklist-answer-checkbox-container">
-					<input
-						type="checkbox"
-						onClick={ this.toggleCheckboxSmallWindow }
-						onKeyPress={ this.toggleCheckboxSmallWindow }
-						tabIndex={ smallWindow ? 0 : -1 }
-						checked={ checked }
-					/>
-				</div>
-				<div className="jp-checklist-answer-title">
-					<p>{ this.props.title }</p>
-				</div>
-				<div
-					className={ classNames( 'jp-checklist-answer-details', {
-						expanded,
-					} ) }
-				>
-					<p>{ this.props.details }</p>
-				</div>
-				<div
-					className={ classNames( 'jp-checklist-answer-chevron-container', {
-						expanded,
-					} ) }
-					onClick={ this.toggleExpanded }
-					onKeyPress={ this.toggleExpanded }
-					role="button"
+	return (
+		<div
+			className={ classNames( 'jp-checklist-answer-container', { checked } ) }
+			onClick={ toggleCheckboxLargeWindow }
+			onKeyPress={ toggleCheckboxLargeWindow }
+			role="checkbox"
+			aria-checked={ checked }
+			tabIndex={ smallWindow ? -1 : 0 }
+		>
+			<div className="jp-checklist-answer-checkbox-container">
+				<input
+					type="checkbox"
+					onClick={ toggleCheckboxSmallWindow }
+					onKeyPress={ toggleCheckboxSmallWindow }
 					tabIndex={ smallWindow ? 0 : -1 }
-				>
-					<Gridicon icon={ chevronIcon } size={ 21 } />
-				</div>
+					checked={ checked }
+				/>
 			</div>
-		);
-	}
-}
+			<div className="jp-checklist-answer-title">
+				<p>{ props.title }</p>
+			</div>
+			<div
+				className={ classNames( 'jp-checklist-answer-details', {
+					expanded,
+				} ) }
+			>
+				<p>{ props.details }</p>
+			</div>
+			<div
+				className={ classNames( 'jp-checklist-answer-chevron-container', {
+					expanded,
+				} ) }
+				onClick={ toggleExpanded }
+				onKeyPress={ toggleExpanded }
+				role="button"
+				tabIndex={ smallWindow ? 0 : -1 }
+			>
+				<Gridicon icon={ chevronIcon } size={ 21 } />
+			</div>
+		</div>
+	);
+};
 
 ChecklistAnswer.propTypes = {
 	title: PropTypes.string.isRequired,
