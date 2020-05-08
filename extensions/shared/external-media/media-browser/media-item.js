b5c64e6ee862393a9053b161a2f12ba44da8f557
Marek Hrabe <marekhrabe@me.com>
2020-05-08 15:40:31 +0200

    rewrite MediaItem to functional

--- extensions/shared/external-media/media-browser/media-item.js
+++ extensions/shared/external-media/media-browser/media-item.js
@@ -7,44 +7,42 @@ import classnames from 'classnames';
 /**
  * WordPress dependencies
  */
-import { Component } from '@wordpress/element';
+import { useCallback } from '@wordpress/element';
 import { Spinner } from '@wordpress/components';
 
-class MediaItem extends Component {
-	onClick = () => {
-		if ( this.props.onClick ) {
-			this.props.onClick( this.props.item );
+function MediaItem( props ) {
+	const onClick = useCallback( () => {
+		if ( props.onClick ) {
+			props.onClick( props.item );
 		}
-	};
+	}, [ props.onClick ] );
 
-	render() {
-		const { item, isSelected, isCopying = false } = this.props;
-		const { thumbnails, caption, name, title, type, children = 0 } = item;
-		const { medium = null, fmt_hd = null } = thumbnails;
-		const alt = title || caption || name;
-		const classes = classnames( {
-			'jetpack-external-media-browser__media__item': true,
-			'jetpack-external-media-browser__media__item__selected': isSelected,
-			'jetpack-external-media-browser__media__folder': type === 'folder',
-			'is-transient': isSelected && isCopying,
-		} );
+	const { item, isSelected, isCopying = false } = props;
+	const { thumbnails, caption, name, title, type, children = 0 } = item;
+	const { medium = null, fmt_hd = null } = thumbnails;
+	const alt = title || caption || name;
+	const classes = classnames( {
+		'jetpack-external-media-browser__media__item': true,
+		'jetpack-external-media-browser__media__item__selected': isSelected,
+		'jetpack-external-media-browser__media__folder': type === 'folder',
+		'is-transient': isSelected && isCopying,
+	} );
 
-		/* eslint-disable jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */
-		return (
-			<button className={ classes } onClick={ this.onClick }>
-				<img src={ medium || fmt_hd } alt={ alt } title={ alt } />
+	/* eslint-disable jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */
+	return (
+		<button className={ classes } onClick={ onClick }>
+			<img src={ medium || fmt_hd } alt={ alt } title={ alt } />
 
-				{ type === 'folder' && (
-					<div className="jetpack-external-media-browser__media__info">
-						<div className="jetpack-external-media-browser__media__name">{ name }</div>
-						<div className="jetpack-external-media-browser__media__count">{ children }</div>
-					</div>
-				) }
+			{ type === 'folder' && (
+				<div className="jetpack-external-media-browser__media__info">
+					<div className="jetpack-external-media-browser__media__name">{ name }</div>
+					<div className="jetpack-external-media-browser__media__count">{ children }</div>
+				</div>
+			) }
 
-				{ isSelected && isCopying && <Spinner /> }
-			</button>
-		);
-	}
+			{ isSelected && isCopying && <Spinner /> }
+		</button>
+	);
 }
 
 export default MediaItem;
