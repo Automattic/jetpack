a82b27c56a34773aaaee2576fb5d25e3d073e97b
retrofox <rdsuarez@gmail.com>
2020-05-11 13:08:17 -0300

    fse: add `/copy` external media endpoint

--- _inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-external-media.php
+++ _inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-external-media.php
@@ -14,6 +14,9 @@ use Automattic\Jetpack\Connection\Client;
  * @since 8.5
  */
 class WPCOM_REST_API_V2_Endpoint_External_Media extends WP_REST_Controller {
+
+	private static $services_regex = '(?P<service>google_photos|pexels)';
+
 	/**
 	 * Constructor.
 	 */
@@ -29,18 +32,28 @@ class WPCOM_REST_API_V2_Endpoint_External_Media extends WP_REST_Controller {
 	public function register_routes() {
 		register_rest_route(
 			$this->namespace,
-			$this->rest_base . '/list/(?P<service>google_photos|pexels)',
+			$this->rest_base . '/list/' . self::$services_regex,
 			array(
-				'methods'  => WP_REST_Server::READABLE,
-				'callback' => array( $this, 'get_external_media' ),
+				'methods'             => WP_REST_Server::READABLE,
+				'callback'            => array( $this, 'get_external_media' ),
 				'permission_callback' => array( $this, 'permission_callback' ),
-				'args'                => array(
+				'args' => array(
 					'search' => array(
 						'description' => __( 'Media collection search term' ),
 						'type'        => 'string',
 						'required'    => 'false',
 					),
 				),
+			)
+		);
+
+		register_rest_route(
+			$this->namespace,
+			$this->rest_base . '/copy/' . self::$services_regex,
+			array(
+				'methods'             => \WP_REST_Server::CREATABLE,
+				'callback'            => array( $this, 'copy_external_media' ),
+				'permission_callback' => array( $this, 'permission_callback' ),
 			)
 		);
 	}
@@ -69,6 +82,106 @@ class WPCOM_REST_API_V2_Endpoint_External_Media extends WP_REST_Controller {
 		$body = wp_remote_retrieve_body( $response );
 		return json_decode( $body );
 	}
+
+	public function wp_unique_filename() {
+		return $this->name;
+	}
+
+	public function copy_external_media( \WP_REST_Request $request ) {
+		if ( ! function_exists( 'download_url' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/file.php';
+		}
+
+		if ( ! function_exists( 'media_handle_sideload' ) ) {
+			require_once( ABSPATH . 'wp-admin/includes/media.php' );
+		}
+
+		if ( ! function_exists( 'wp_read_image_metadata' ) ) {
+			require_once( ABSPATH . 'wp-admin/includes/image.php' );
+		}
+
+		$params = $request->get_params();
+
+		$media = isset( $params['media'] ) && is_array( $params['media'] ) ? $params['media'] : [];
+
+//		add_filter( 'wp_unique_filename', array( $this, 'wp_unique_filename' ) );
+
+		$responses = [];
+
+		foreach ( $media as $item ) {
+			$caption = '';
+			$title = '';
+			$guid = false;
+
+			if ( isset( $item['guid'] ) ) {
+				$guid = $item['guid'];
+			}
+
+			if ( isset( $item['caption'] ) ) {
+				$caption = $item['caption'];
+			}
+
+			if ( isset( $item['title'] ) ) {
+				$title = $item['title'];
+			}
+
+			if ( ! $guid ) {
+				return new \WP_Error( 'external_media_guid', 'No GUID is provided' );
+			}
+
+			$guid = json_decode( $guid, true );
+
+//			$this->name = $guid['name'];
+
+			// Download file to temp dir
+			$file = [
+				'name' => wp_basename( $guid['name'] ),
+				'tmp_name' => download_url( $guid['url'] ),
+			];
+
+			if ( is_wp_error( $file['tmp_name'] ) ) {
+				$responses[] = $file['tmp_name'];
+				continue;
+			}
+
+			$id = media_handle_sideload( $file, 0, null );
+			if ( is_wp_error( $id ) ) {
+				@unlink( $file['tmp_name'] );
+				$responses[] = $id;
+				continue;
+			}
+
+			$meta = wp_get_attachment_metadata( $id );
+			$meta['image_meta']['title'] = $title;
+			$meta['image_meta']['caption'] = $caption;
+
+			wp_update_attachment_metadata( $id, $meta );
+
+			update_post_meta( $id, '_wp_attachment_image_alt', $title );
+			wp_update_post( [
+				'ID' => $id,
+				'post_excerpt' => $caption,
+			] );
+
+			$request = new \WP_REST_Request( 'GET', '/wp/v2/media/' . $id );
+			$response = rest_do_request( $request );
+
+			if ( is_wp_error( $response ) ) {
+				$responses[] = $response;
+			} else {
+				$responses[] = [
+					'id' => $id,
+					'caption' => $caption,
+					'alt' => $title,
+					'url' => $response->data['source_url'],
+				];
+			}
+		}
+
+		return $responses;
+
+	}
+
 }
 
 wpcom_rest_api_v2_load_plugin( 'WPCOM_REST_API_V2_Endpoint_External_Media' );
