cdb318041217c5b7e30b97add3ce4b80b5cacf70
retrofox <rdsuarez@gmail.com>
2020-05-11 11:23:14 -0300

    external-media: propagate `search` term to the end

--- _inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-external-media.php
+++ _inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-external-media.php
@@ -33,15 +33,35 @@ class WPCOM_REST_API_V2_Endpoint_External_Media extends WP_REST_Controller {
 			array(
 				'methods'  => WP_REST_Server::READABLE,
 				'callback' => array( $this, 'get_external_media' ),
-				'permission_callback' => 'is_user_logged_in',
+				'permission_callback' => array( $this, 'permission_callback' ),
+				'args'                => array(
+					'search' => array(
+						'description' => __( 'Media collection search term' ),
+						'type'        => 'string',
+						'required'    => 'false',
+					),
+				),
 			)
 		);
+	}
+
+	public function permission_callback() {
+		return current_user_can( 'edit_posts' );
 	}
 
 	public function get_external_media( \WP_REST_Request $request ) {
 		$params = $request->get_params();
 		$wpcom_path = sprintf( '/meta/external-media/%s', urlencode( $params['service'] ) );
-		$response = Client::wpcom_json_api_request_as_user( $wpcom_path );
+
+		// Build query string to pass to wpcom endpoint.
+		$service_args = array_filter( $params, function( $key ) {
+			return in_array( $key, array( 'search' ) );
+		}, ARRAY_FILTER_USE_KEY );
+		if ( ! empty($service_args ) ) {
+			$wpcom_path .= '?' . http_build_query( $service_args );
+		}
+
+		$response = Client::wpcom_json_api_request_as_user( $wpcom_path, '2' );
 		if ( is_wp_error( $response ) ) {
 			return $response;
 		}
