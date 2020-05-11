4222852d58a4e30446163f6c727c2e5944e52f59
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-05-11 18:12:34 +0300

    Add in-place upgrade flow

--- tests/e2e/specs/connection.test.js
+++ tests/e2e/specs/connection.test.js
@@ -1,19 +1,35 @@
 /**
+ * External dependencies
+ */
+import config from 'config';
+/**
  * Internal dependencies
  */
-import { catchBeforeAll, step } from '../lib/setup-env';
-import { doInPlaceConnection } from '../lib/flows/jetpack-connect';
-import { execWpCommand } from '../lib/utils-helper';
+import { step } from '../lib/setup-env';
+import {
+	doInPlaceConnection,
+	syncJetpackPlanData,
+	loginToWpSite,
+	loginToWpcomIfNeeded,
+} from '../lib/flows/jetpack-connect';
+import { execWpCommand, getNgrokSiteUrl, resetWordpressInstall } from '../lib/utils-helper';
 import Sidebar from '../lib/pages/wp-admin/sidebar';
 import JetpackPage from '../lib/pages/wp-admin/jetpack';
+import CheckoutPage from '../lib/pages/wpcom/checkout';
+import ThankYouPage from '../lib/pages/wpcom/thank-you';
+import MyPlanPage from '../lib/pages/wpcom/my-plan';
+import PlansPage from '../lib/pages/wp-admin/plans';
+const cookie = config.get( 'storeSandboxCookieValue' );
+const cardCredentials = config.get( 'testCardCredentials' );
 
 describe( 'Connection', () => {
-	catchBeforeAll( async () => {
-		await execWpCommand( 'wp option delete jetpack_private_options' );
+	beforeEach( async () => {
+		await resetWordpressInstall();
 		await execWpCommand( 'wp config set --raw JETPACK_SHOULD_NOT_USE_CONNECTION_IFRAME false' );
-		// For some reason it need 2 reloads to make constant actually work.
-		await page.reload();
-		await page.reload();
+		await execWpCommand( 'wp plugin deactivate e2e-plan-data-interceptor' );
+
+		await loginToWpcomIfNeeded( 'defaultUser' );
+		await loginToWpSite();
 	} );
 
 	afterAll( async () => {
@@ -21,9 +37,12 @@ describe( 'Connection', () => {
 			'wp option update jetpack_private_options --format=json < jetpack_private_options.txt'
 		);
 		await execWpCommand( 'wp config set --raw JETPACK_SHOULD_NOT_USE_CONNECTION_IFRAME true' );
+		await execWpCommand( 'wp plugin activate e2e-plan-data-interceptor' );
+
+		// await resetWordpressInstall();
 	} );
 
-	it( 'In-place', async () => {
+	it( 'In-place with Free plan', async () => {
 		await step( 'Can start in-place connection', async () => {
 			await ( await Sidebar.init( page ) ).selectJetpack();
 			await doInPlaceConnection();
@@ -32,6 +51,52 @@ describe( 'Connection', () => {
 		await step( 'Can assert that site is connected', async () => {
 			const jetpackPage = await JetpackPage.init( page );
 			expect( await jetpackPage.isConnected() ).toBeTruthy();
+		} );
+	} );
+
+	it( 'In-place upgrading a plan from personal to premium', async () => {
+		await step( 'Can set a sandbox cookie', async () => {
+			const siteUrl = getNgrokSiteUrl();
+			const host = new URL( siteUrl ).host;
+			const sidebar = await Sidebar.init( page );
+			await sidebar.setSandboxModeForPayments( cookie );
+			await sidebar.setSandboxModeForPayments( cookie, host );
+		} );
+
+		await step( 'Can start in-place connection', async () => {
+			await ( await Sidebar.init( page ) ).selectJetpack();
+			await doInPlaceConnection( 'personal' );
+		} );
+
+		await step( 'Can process payment', async () => {
+			await ( await CheckoutPage.init( page ) ).processPurchase( cardCredentials );
+			await ( await ThankYouPage.init( page ) ).waitForSetupAndProceed();
+			await ( await MyPlanPage.init( page ) ).returnToWPAdmin();
+			await syncJetpackPlanData( 'personal' );
+		} );
+
+		await step( 'Can assert that site has a Personal plan', async () => {
+			const jetpackPage = await JetpackPage.init( page );
+			expect( await jetpackPage.isPlan( 'personal' ) ).toBeTruthy();
+		} );
+
+		await step( 'Can visit plans page and select a Premium plan', async () => {
+			const jetpackPage = await JetpackPage.init( page );
+			await jetpackPage.openPlans();
+			const plansPage = await PlansPage.init( page );
+			await plansPage.select( 'premium' );
+		} );
+
+		await step( 'Can process payment', async () => {
+			await ( await CheckoutPage.init( page ) ).processPurchase( cardCredentials );
+			await ( await ThankYouPage.init( page ) ).waitForSetupAndProceed();
+			await ( await MyPlanPage.init( page ) ).returnToWPAdmin();
+			await syncJetpackPlanData( 'premium' );
+		} );
+
+		await step( 'Can assert that site has a Premium plan', async () => {
+			const jetpackPage = await JetpackPage.init( page );
+			expect( await jetpackPage.isPlan( 'premium' ) ).toBeTruthy();
 		} );
 	} );
 } );
