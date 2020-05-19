007599ee3e70bdfc5889ad3ce275714a8ad3554c
sixhours <calobee@gmail.com>
2020-05-19 12:21:14 -0400

    Nope, still breaking things. Need to apply the formatting in a different place.

--- extensions/blocks/recurring-payments/edit.jsx
+++ extensions/blocks/recurring-payments/edit.jsx
@@ -231,7 +231,6 @@ class MembershipsButtonEdit extends Component {
 			this.state.editedProductCurrency
 		);
 		const minimumPriceNote = sprintf( __( 'Minimum allowed price is %s.' ), minPrice );
-		const formattedMinPrice = parseFloat( this.state.editedProductPrice );
 		return (
 			<div>
 				<div className="membership-button__price-container">
@@ -254,7 +253,7 @@ class MembershipsButtonEdit extends Component {
 							min="0"
 							step="1"
 							type="number"
-							value={ formattedMinPrice || '' }
+							value={ this.state.editedProductPrice || '' }
 						/>
 						<p>{ minimumPriceNote }</p>
 					</div>
