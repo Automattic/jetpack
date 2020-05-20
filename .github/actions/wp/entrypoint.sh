85d87f9472a09b070115a0760c0f8d01fd607eef
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-05-20 11:37:54 +0300

    Test new action in cloud

--- .github/actions/wp/entrypoint.sh
+++ .github/actions/wp/entrypoint.sh
@@ -3,3 +3,11 @@
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
+
+
+which wp || exit 0
+
+sudo apt update
+sudo apt install -y php-fpm nginx
+sudo systemctl start mysql.service
+ls -la ./tests/e2e/bin/
