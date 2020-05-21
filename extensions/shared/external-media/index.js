0a777c3d77ea883d879d9cba0c8243fd04d36d91
Konstantin Obenland <obenland@gmx.de>
2020-05-21 11:12:44 -0700

    Only show External MEdia to connected users

--- extensions/shared/external-media/index.js
+++ extensions/shared/external-media/index.js
@@ -6,6 +6,7 @@ import { addFilter } from '@wordpress/hooks';
 /**
  * Internal dependencies
  */
+import isCurrentUserConnected from '../is-current-user-connected';
 import MediaButton from './media-button';
 import { mediaSources } from './sources';
 import './editor.scss';
@@ -21,22 +22,24 @@ function insertExternalMediaBlocks( settings, name ) {
 	};
 }
 
-// Register the new 'browse media' button
-addFilter(
-	'editor.MediaUpload',
-	'external-media/replace-media-upload',
-	OriginalComponent => props => (
-		<OriginalComponent
-			{ ...props }
-			render={ button => <MediaButton { ...button } mediaProps={ props } /> }
-		/>
-	),
-	11
-);
+if ( isCurrentUserConnected() ) {
+	// Register the new 'browse media' button.
+	addFilter(
+		'editor.MediaUpload',
+		'external-media/replace-media-upload',
+		OriginalComponent => props => (
+			<OriginalComponent
+				{ ...props }
+				render={ button => <MediaButton { ...button } mediaProps={ props } /> }
+			/>
+		),
+		11
+	);
 
-// Register the individual external media blocks
-addFilter(
-	'blocks.registerBlockType',
-	'external-media/individual-blocks',
-	insertExternalMediaBlocks
-);
+	// Register the individual external media blocks.
+	addFilter(
+		'blocks.registerBlockType',
+		'external-media/individual-blocks',
+		insertExternalMediaBlocks
+	);
+}
