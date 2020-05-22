a8a953343fea46713d2f5ee28489c0ed5825cc24
apeatling <apeatling@gmail.com>
2020-05-22 13:11:01 -0700

    Update settings icon to something more appropriate

--- extensions/blocks/send-a-message/whatsapp-button/edit.js
+++ extensions/blocks/send-a-message/whatsapp-button/edit.js
@@ -1,7 +1,6 @@
 /**
  * External dependencies
  */
-
 import { __, _x } from '@wordpress/i18n';
 import { useEffect, useState } from '@wordpress/element';
 import {
@@ -10,10 +9,10 @@ import {
 	TextControl,
 	TextareaControl,
 	SelectControl,
-	Icon,
 	PanelBody,
 	ToolbarGroup,
 	Dropdown,
+	Path,
 } from '@wordpress/components';
 import {
 	BlockControls,
@@ -26,8 +25,8 @@ import { DOWN } from '@wordpress/keycodes';
 /**
  * Internal dependencies
  */
-
 import { countryCodes } from '../shared/countrycodes.js';
+import renderMaterialIcon from '../../../shared/render-material-icon';
 import './view.scss';
 
 export default function WhatsAppButtonEdit( { attributes, setAttributes, className } ) {
@@ -43,10 +42,6 @@ export default function WhatsAppButtonEdit( { attributes, setAttributes, classNa
 	const [ isValidPhoneNumber, setIsValidPhoneNumber ] = useState( false );
 
 	const getCountryCode = async () => {
-		if ( undefined !== countryCode ) {
-			return;
-		}
-
 		setAttributes( { countryCode: '1' } );
 
 		const geoFetch = await fetch( 'http://ip-api.com/json/?fields=countryCode' )
@@ -73,6 +68,10 @@ export default function WhatsAppButtonEdit( { attributes, setAttributes, classNa
 	};
 
 	useEffect( () => {
+		if ( undefined !== countryCode ) {
+			return;
+		}
+
 		getCountryCode();
 	} );
 
@@ -115,7 +114,9 @@ export default function WhatsAppButtonEdit( { attributes, setAttributes, classNa
 				label={ __( 'Edit Form Settings' ) }
 				onClick={ onToggle }
 				onKeyDown={ openOnArrowDown }
-				icon={ <Icon icon="edit" /> }
+				icon={ renderMaterialIcon(
+					<Path d="M10.82 12.49c.02-.16.04-.32.04-.49 0-.17-.02-.33-.04-.49l1.08-.82c.1-.07.12-.21.06-.32l-1.03-1.73c-.06-.11-.2-.15-.31-.11l-1.28.5c-.27-.2-.56-.36-.87-.49l-.2-1.33c0-.12-.11-.21-.24-.21H5.98c-.13 0-.24.09-.26.21l-.2 1.32c-.31.12-.6.3-.87.49l-1.28-.5c-.12-.05-.25 0-.31.11l-1.03 1.73c-.06.12-.03.25.07.33l1.08.82c-.02.16-.03.33-.03.49 0 .17.02.33.04.49l-1.09.83c-.1.07-.12.21-.06.32l1.03 1.73c.06.11.2.15.31.11l1.28-.5c.27.2.56.36.87.49l.2 1.32c.01.12.12.21.25.21h2.06c.13 0 .24-.09.25-.21l.2-1.32c.31-.12.6-.3.87-.49l1.28.5c.12.05.25 0 .31-.11l1.03-1.73c.06-.11.04-.24-.06-.32l-1.1-.83zM7 13.75c-.99 0-1.8-.78-1.8-1.75s.81-1.75 1.8-1.75 1.8.78 1.8 1.75S8 13.75 7 13.75zM18 1.01L8 1c-1.1 0-2 .9-2 2v3h2V5h10v14H8v-1H6v3c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V3c0-1.1-.9-1.99-2-1.99z" />
+				) }
 			/>
 		);
 	};
@@ -139,6 +140,7 @@ export default function WhatsAppButtonEdit( { attributes, setAttributes, classNa
 					<TextControl
 						placeholder={ __( 'Your phone number…', 'jetpack' ) }
 						onBlur={ newPhoneNumber => onBlurPhoneNumber( newPhoneNumber ) }
+						onChange={ newPhoneNumber => setAttributes( { phoneNumber: newPhoneNumber } ) }
 						value={ phoneNumber }
 					/>
 				</BaseControl>
