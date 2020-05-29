f58b82e3fbb8b989f3a6154b146edfb9b34498a8
Konstantin Obenland <obenland@gmx.de>
2020-05-29 10:28:33 -0700

    Add class coverage info

--- tests/php/core-api/wpcom-endpoints/test_class-wpcom-rest-api-v2-endpoint-external-media.php.php
+++ tests/php/core-api/wpcom-endpoints/test_class-wpcom-rest-api-v2-endpoint-external-media.php.php
@@ -7,6 +7,8 @@ require_once dirname( dirname( __DIR__ ) ) . '/lib/class-wp-test-jetpack-rest-te
 
 /**
  * Class WP_Test_WPCOM_REST_API_V2_Endpoint_External_Media
+ *
+ * @coversDefaultClass WPCOM_REST_API_V2_Endpoint_External_Media
  */
 class WP_Test_WPCOM_REST_API_V2_Endpoint_External_Media extends WP_Test_Jetpack_REST_Testcase {
 
