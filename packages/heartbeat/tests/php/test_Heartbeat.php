c7696bc8d08be7bbecf87ab3345ed47ac8180992
leogermani <leogermani@gmail.com>
2020-05-29 17:24:16 -0300

    Creates the Jetpack Heartbeat package

---
+++ packages/heartbeat/tests/php/test_Heartbeat.php
@@ -0,0 +1,14 @@
+<?php // phpcs:ignore Squiz.Commenting.FileComment.Missing
+
+namespace Automattic\Jetpack;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test Heartbeat class
+ */
+class HeartbeatTest extends TestCase {
+
+
+
+}
