cedb55d4112166564ad648b2817c34d209d2a7c2
Jeremy Herve <jeremy@tagada.hu>
2020-06-02 12:09:44 +0200

    Update version in docblock

--- modules/contact-form/admin.php
+++ modules/contact-form/admin.php
@@ -938,7 +938,7 @@ function grunion_delete_spam_feedbacks() {
 	 *
 	 * @module contact-form
 	 *
-	 * @since 8.6.0
+	 * @since 8.7.0
 	 *
 	 * @param int $delete_limit Number of spam to process at once. Default to 25.
 	 */
