13d6bae8392e0ca869fc9abdf68f5662faf50d7c
Marco Pereirinha <marco.pereirinha@xwp.co>
2020-06-05 18:31:39 +0100

    [not verified] [not verified] Add support for AMP infinite scroll on twentyten

--- modules/infinite-scroll/themes/twentyten.php
+++ modules/infinite-scroll/themes/twentyten.php
@@ -53,3 +53,100 @@ function jetpack_twentyten_has_footer_widgets() {
 
 	return false;
 }
+
+/**
+ * Load AMP theme specific hooks for infinite scroll.
+ *
+ * @return void
+ */
+function amp_jetpack_twentyten_infinite_scroll_render_hooks() {
+	add_filter( 'jetpack_amp_infinite_footers', 'twentyten_amp_infinite_footers', 10, 2 );
+	add_filter( 'jetpack_amp_infinite_output', 'twentyten_amp_infinite_output' );
+	add_filter( 'jetpack_amp_infinite_older_posts', 'twentyten_amp_infinite_older_posts' );
+}
+
+/**
+ * Get the theme specific footers.
+ *
+ * @param array  $footers The footers of the themes.
+ * @param string $buffer  Contents of the output buffer.
+ *
+ * @return mixed
+ */
+function twentyten_amp_infinite_footers( $footers, $buffer ) {
+	// Collect the footer wrapper.
+	preg_match(
+		'/<div id="footer" role="contentinfo".*<!-- #footer -->/s',
+		$buffer,
+		$footer
+	);
+	$footers[] = '<div style="background: #fff; margin: 0 auto; padding: 0 20px; width: 940px;">' . reset( $footer ) . '</div>';
+
+	return $footers;
+}
+
+/**
+ * Hide and remove various elements from next page load.
+ *
+ * @param string $buffer Contents of the output buffer.
+ *
+ * @return string
+ */
+function twentyten_amp_infinite_output( $buffer ) {
+	// Hide site header on next page load.
+	$buffer = preg_replace(
+		'/<div id="header"/',
+		'$0 next-page-hide',
+		$buffer
+	);
+
+	// Hide sidebar on next page load.
+	$buffer = preg_replace(
+		'/<div id="primary"/',
+		'$0 next-page-hide',
+		$buffer
+	);
+
+	// Hide pagination on next page load.
+	$buffer = preg_replace(
+		'/<div id="nav-above" class="navigation"/',
+		'$0 next-page-hide hidden',
+		$buffer
+	);
+
+	$buffer = preg_replace(
+		'/<div id="nav-below" class="navigation"/',
+		'$0 next-page-hide hidden',
+		$buffer
+	);
+
+	// Remove the footer as it will be added back to amp next page footer.
+	$buffer = preg_replace(
+		'/<div id="footer" role="contentinfo".*<!-- #footer -->/s',
+		'',
+		$buffer
+	);
+
+	return $buffer;
+}
+
+/**
+ * Filter the AMP infinite scroll older posts button
+ *
+ * @return string
+ */
+function twentyten_amp_infinite_older_posts() {
+	ob_start();
+	?>
+<div id="infinite-handle" style="background: #fff; margin: 0 auto; padding: 0 20px 20px; width: 940px;">
+	<span>
+		<a href="{{url}}">
+			<button>
+				{{title}}
+			</button>
+		</a>
+	</span>
+</div>
+	<?php
+	return ob_get_clean();
+}
