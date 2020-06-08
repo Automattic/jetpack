f794b6c9e3c36861139732bccc0bc0e3db1c5d27
retrofox <rdsuarez@gmail.com>
2020-06-08 15:40:30 -0300

    cover: show upgrade nudge

--- extensions/shared/blocks/cover/cover-media-placeholder.js
+++ extensions/shared/blocks/cover/cover-media-placeholder.js
@@ -7,13 +7,37 @@
  * WordPress dependencies
  */
 import { createHigherOrderComponent } from '@wordpress/compose';
-import { Fragment } from '@wordpress/element';
+import { Fragment, useState } from '@wordpress/element';
 import { useBlockEditContext } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import UpgradeNudge from "../../components/upgrade-nudge";
 
 /**
  * Module Constants
  */
 const ALLOWED_MEDIA_TYPES = [ 'image' ];
+
+const JetpackCoverUpgradeNudge = ( { name, show } ) =>
+	show
+		? <UpgradeNudge
+			plan="value_bundle"
+			blockName={ name }
+			title={ {
+				knownPlan: __( 'To use a video in this block, upgrade to %(planName) plan.', 'jetpack' ),
+				unknownPlan: __( 'To use a video in this block, upgrade to a paid plan.', 'jetpack' ),
+			} }
+			subtitle={ __(
+				'Upload unlimited videos to your website and \
+				display them using a fast, unbranded, \
+				customizable player.',
+				'jetpack'
+			) }
+		/>
+		: null;
 
 export default createHigherOrderComponent(
 	CoreMediaPlaceholder => props => {
@@ -22,10 +46,18 @@ export default createHigherOrderComponent(
 			return <CoreMediaPlaceholder { ...props } />;
 		}
 
+		const [ uploadingError, setUploadingError ] = useState( false );
+		const { onError } = props;
+
 		return (
 			<Fragment>
+				<JetpackCoverUpgradeNudge name={ name } show={ uploadingError } />
 				<CoreMediaPlaceholder
 					{ ...props }
+					onError = { ( message ) => {
+						setUploadingError( true );
+						return onError( message );
+					} }
 					allowedTypes={ ALLOWED_MEDIA_TYPES }
 				/>
 			</Fragment>
