d936cdbd515f0665b77c29d6902a2b4a767c812f
retrofox <rdsuarez@gmail.com>
2020-06-11 09:21:01 -0300

    cover: use HOC helper

--- extensions/shared/blocks/cover/cover-replace-control-button.js
+++ extensions/shared/blocks/cover/cover-replace-control-button.js
@@ -7,9 +7,14 @@
  * WordPress dependencies
  */
 import { useBlockEditContext } from '@wordpress/block-editor';
+import { createHigherOrderComponent } from '@wordpress/compose';
+
+/**
+ * Internal dependencies
+ */
 import { isUpgradable } from "./utils";
 
-export default ( onNotice ) => ( MediaReplaceFlow => props => {
+export default ( onNudgeShow ) => createHigherOrderComponent( MediaReplaceFlow => props => {
 	const { name } = useBlockEditContext();
 	if ( ! isUpgradable( name ) ) {
 		return <MediaReplaceFlow { ...props } />;
@@ -20,8 +25,9 @@ export default ( onNotice ) => ( MediaReplaceFlow => props => {
 	return <MediaReplaceFlow
 		{ ...props }
 		createNotice={ ( status, msg, options ) => {
-			onNotice( status, msg, options );
+			onNudgeShow( status, msg, options );
 			createNotice( status, msg, options );
 		} }
 	/>;
-} );
+}, 'withNudgeHandling' );
+
