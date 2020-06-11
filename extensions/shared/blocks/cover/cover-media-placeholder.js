ac7bde5c9bbb68ddc10e8a8aac23a44e885ba811
retrofox <rdsuarez@gmail.com>
2020-06-11 06:40:19 -0300

    cover: do not create an HOC unecessarely

--- extensions/shared/blocks/cover/cover-media-placeholder.js
+++ extensions/shared/blocks/cover/cover-media-placeholder.js
@@ -46,45 +46,42 @@ const JetpackCoverUpgradeNudge = ( { name, show } ) =>
 		/>
 		: null;
 
-export default createHigherOrderComponent(
-	CoreMediaPlaceholder => props => {
-		const [ error, setError ] = useState( false );
-		const { name } = useBlockEditContext();
-		const { unavailableReason } = getJetpackExtensionAvailability( 'videopress' );
-		if (
-			( ! name || name !== 'core/cover' ) || // extend only for cover block
-			! isSimpleSite() || // only for Simple sites
-			! [ 'missing_plan', 'unknown' ].includes( unavailableReason )
-		) {
-			return <CoreMediaPlaceholder { ...props } />;
-		}
+export default CoreMediaPlaceholder => props => {
+	const [ error, setError ] = useState( false );
+	const { name } = useBlockEditContext();
+	const { unavailableReason } = getJetpackExtensionAvailability( 'videopress' );
+	if (
+		( ! name || name !== 'core/cover' ) || // extend only for cover block
+		! isSimpleSite() || // only for Simple sites
+		! [ 'missing_plan', 'unknown' ].includes( unavailableReason )
+	) {
+		return <CoreMediaPlaceholder { ...props } />;
+	}
 
-		const { onError } = props;
-		return (
-			<div className="jetpack-cover-media-placeholder">
-				<JetpackCoverUpgradeNudge name={ name } show={ !! error } />
-				<CoreMediaPlaceholder
-					{ ...props }
-					multiple={ false }
-					onError = { ( message ) => {
-						// Try to pick up filename from the error message.
-						// We should find a better way to do it. Unstable.
-						const filename = message?.[0]?.props?.children;
-						if ( ! filename ) {
-							return onError( message );
-						}
+	const { onError } = props;
+	return (
+		<div className="jetpack-cover-media-placeholder">
+			<JetpackCoverUpgradeNudge name={ name } show={ !! error } />
+			<CoreMediaPlaceholder
+				{ ...props }
+				multiple={ false }
+				onError = { ( message ) => {
+					// Try to pick up filename from the error message.
+					// We should find a better way to do it. Unstable.
+					const filename = message?.[0]?.props?.children;
+					if ( ! filename ) {
+						return onError( message );
+					}
 
-						const fileExtension = ( filename.split( '.' ) )?.[ 1 ];
-						if ( ! videoFileExtensions.includes( fileExtension ) ) {
-							return onError( message );
-						}
+					const fileExtension = ( filename.split( '.' ) )?.[ 1 ];
+					if ( ! videoFileExtensions.includes( fileExtension ) ) {
+						return onError( message );
+					}
 
-						return setError( message );
-					} }
-					allowedTypes={ ALLOWED_MEDIA_TYPES }
-				/>
-			</div>
-		);
-	},
-	'JetpackCoverMediaPlaceholder'
-);
+					return setError( message );
+				} }
+				allowedTypes={ ALLOWED_MEDIA_TYPES }
+			/>
+		</div>
+	);
+};
