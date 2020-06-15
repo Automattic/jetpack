e7a61ea231c1304525e4126adaafbe9eb5956103
retrofox <rdsuarez@gmail.com>
2020-06-15 06:29:36 -0300

    cover: apply filter only when user is connected

--- extensions/shared/blocks/cover/index.js
+++ extensions/shared/blocks/cover/index.js
@@ -11,11 +11,14 @@ import { addFilter } from '@wordpress/hooks';
  * Internal dependencies
  */
 import coverEditMediaPlaceholder from './cover-media-placeholder';
+import isCurrentUserConnected from "../../is-current-user-connected";
 import './editor.scss';
 
-// Take the control of MediaPlaceholder.
-addFilter(
-	'editor.MediaPlaceholder',
-	'jetpack/cover-edit-media-placeholder',
-	coverEditMediaPlaceholder
-);
+if ( isCurrentUserConnected() ) {
+	// Take the control of MediaPlaceholder.
+	addFilter(
+		'editor.MediaPlaceholder',
+		'jetpack/cover-edit-media-placeholder',
+		coverEditMediaPlaceholder
+	);
+}
