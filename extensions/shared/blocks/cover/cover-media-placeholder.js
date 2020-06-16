a247838726ec3241a8ae3538c4cea5a9933e41e3
retrofox <rdsuarez@gmail.com>
2020-06-16 04:55:02 -0300

    cover: connect MediaPlaceholder with CoverEdit

--- extensions/shared/blocks/cover/cover-media-placeholder.js
+++ extensions/shared/blocks/cover/cover-media-placeholder.js
@@ -3,46 +3,50 @@
  * WordPress dependencies
  */
 import { useBlockEditContext } from '@wordpress/block-editor';
-import { useState } from '@wordpress/element';
+import { createHigherOrderComponent } from '@wordpress/compose';
 
 /**
  * Internal dependencies
  */
-import { JetpackCoverUpgradeNudge } from './components';
 import { isUpgradable, isVideoFile } from './utils';
+import { CoverMediaContext } from './components';
 
 /**
  * Module Constants
  */
 const ALLOWED_MEDIA_TYPES = [ 'image', 'video' ];
 
+export default createHigherOrderComponent(
+	CoreMediaPlaceholder => props => {
+		const { name } = useBlockEditContext();
+		if ( ! isUpgradable( name ) ) {
+			return <CoreMediaPlaceholder { ...props } />;
+		}
 
-export default CoreMediaPlaceholder => props => {
-	const [ error, setError ] = useState( false );
-
-	const { name } = useBlockEditContext();
-	if ( ! isUpgradable( name ) ) {
-		return <CoreMediaPlaceholder { ...props } />;
-	}
-
-	const { onError } = props;
-	return (
-		<div className="jetpack-cover-media-placeholder">
-			<JetpackCoverUpgradeNudge name={ name } show={ !! error } />
-			<CoreMediaPlaceholder
-				{ ...props }
-				multiple={ false }
-				onError = { ( message ) => {
-					// Try to pick up filename from the error message.
-					// We should find a better way to do it. Unstable.
-					const filename = message?.[0]?.props?.children;
-					if ( filename && isVideoFile( filename ) ) {
-						return setError( message );
-					}
-					return onError( message );
-				} }
-				allowedTypes={ ALLOWED_MEDIA_TYPES }
-			/>
-		</div>
-	);
-};
+		const { onError } = props;
+		return (
+			<div className="jetpack-cover-media-placeholder">
+				<CoverMediaContext.Consumer>
+					{ ( { onFilesUpload } ) => (
+						<CoreMediaPlaceholder
+							{ ...props }
+							onFilesPreUpload={ onFilesUpload }
+							multiple={ false }
+							onError = { ( message ) => {
+								// Try to pick up filename from the error message.
+								// We should find a better way to do it. Unstable.
+								const filename = message?.[0]?.props?.children;
+								if ( filename && isVideoFile( filename ) ) {
+									return onFilesUpload( [ filename ] );
+								}
+								return onError( message );
+							} }
+							allowedTypes={ ALLOWED_MEDIA_TYPES }
+						/>
+					) }
+				</CoverMediaContext.Consumer>
+			</div>
+		);
+	},
+	'JepackCoverMediaPlaceholder'
+);
