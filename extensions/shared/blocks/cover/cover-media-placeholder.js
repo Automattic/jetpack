f442eab9b288dac5414a359ed1980868063b8372
retrofox <rdsuarez@gmail.com>
2020-06-17 08:51:56 -0300

    cover: handle updagre nudge with a context

--- extensions/shared/blocks/cover/cover-media-placeholder.js
+++ extensions/shared/blocks/cover/cover-media-placeholder.js
@@ -1,73 +1,57 @@
+
+/**
+ * External dependencies
+ */
+import { noop } from 'lodash';
+
 /**
  * WordPress dependencies
  */
 import { useBlockEditContext } from '@wordpress/block-editor';
-import { useState } from '@wordpress/element';
-import { __ } from '@wordpress/i18n';
+import { createHigherOrderComponent } from '@wordpress/compose';
 
 /**
  * Internal dependencies
  */
-import UpgradeNudge from '../../components/upgrade-nudge';
 import { isUpgradable, isVideoFile } from './utils';
+import { CoverMediaContext } from './components';
 
 /**
  * Module Constants
  */
 const ALLOWED_MEDIA_TYPES = [ 'image', 'video' ];
 
-/**
- * Nudge shows when the user tries to upload a video file.
- * Unlike the core/video block, handled/extended by the videopress block,
- * the nudge is not shown permanently.
- * It's handled by the MediaPlaceholder component
- * when the user tries to upload a video file.
- * For this reason, we can't wrap the edit setting
- * with the wrapPaidBlock() HOC, as the videopress does.
- *
- * @param {object}  props - Information about the user.
- * @param {string}  props.name - Show the Nudge component.
- * @param {boolean} props.show - Show the Nudge component.
- * @returns {*} Nudge component or Null.
- */
-const JetpackCoverUpgradeNudge = ( { name, show } ) =>
-	show ? (
-		<UpgradeNudge
-			plan="value_bundle"
-			blockName={ name }
-			title={ {
-				knownPlan: __( 'To use a video in this block, upgrade to %(planName)s.', 'jetpack' ),
-				unknownPlan: __( 'To use a video in this block, upgrade to a paid plan.', 'jetpack' ),
-			} }
-			subtitle={ false }
-		/>
-	) : null;
+export default createHigherOrderComponent(
+	CoreMediaPlaceholder => props => {
+		const { name } = useBlockEditContext();
+		if ( ! name || ! isUpgradable( name ) ) {
+			return <CoreMediaPlaceholder { ...props } />;
+		}
 
-export default CoreMediaPlaceholder => props => {
-	const [ error, setError ] = useState( false );
-	const { name } = useBlockEditContext();
-	if ( ! name || ! isUpgradable( name ) ) {
-		return <CoreMediaPlaceholder { ...props } />;
-	}
-
-	const { onError } = props;
-	return (
-		<div className="jetpack-cover-media-placeholder">
-			<JetpackCoverUpgradeNudge name={ name } show={ !! error } />
-			<CoreMediaPlaceholder
-				{ ...props }
-				multiple={ false }
-				onError={ message => {
-					// Try to pick up filename from the error message.
-					// We should find a better way to do it. Unstable.
-					const filename = message?.[ 0 ]?.props?.children;
-					if ( filename && isVideoFile( filename ) ) {
-						return setError( message );
-					}
-					return onError( message );
-				} }
-				allowedTypes={ ALLOWED_MEDIA_TYPES }
-			/>
-		</div>
-	);
-};
+		const { onError } = props;
+		return (
+			<div className="jetpack-cover-media-placeholder">
+				<CoverMediaContext.Consumer>
+					{ ( onFilesUpload = noop ) => (
+						<CoreMediaPlaceholder
+							{ ...props }
+							onFilesPreUpload={ onFilesUpload }
+							multiple={ false }
+							onError = { ( message ) => {
+								// Try to pick up filename from the error message.
+								// We should find a better way to do it. Unstable.
+								const filename = message?.[ 0 ]?.props?.children;
+								if ( filename && isVideoFile( filename ) ) {
+									return onFilesUpload( [ filename ] );
+								}
+								return onError( message );
+							} }
+							allowedTypes={ ALLOWED_MEDIA_TYPES }
+						/>
+					) }
+				</CoverMediaContext.Consumer>
+			</div>
+		);
+	},
+	'JetpackCoverMediaPlaceholder'
+);
--- extensions/shared/blocks/cover/index.js
+++ extensions/shared/blocks/cover/index.js
@@ -12,13 +12,14 @@ import { addFilter } from '@wordpress/hooks';
  */
 import coverEditMediaPlaceholder from './cover-media-placeholder';
 import isCurrentUserConnected from '../../is-current-user-connected';
+import jetpackCoverBlockEdit from './edit';
+
 import './editor.scss';
 
 if ( isCurrentUserConnected() ) {
 	// Take the control of MediaPlaceholder.
-	addFilter(
-		'editor.MediaPlaceholder',
-		'jetpack/cover-edit-media-placeholder',
-		coverEditMediaPlaceholder
-	);
+	addFilter( 'editor.MediaPlaceholder', 'jetpack/cover-edit-media-placeholder', coverEditMediaPlaceholder );
+
+	// Extend Core CoverEditBlock.
+	addFilter( 'editor.BlockEdit', 'jetpack/cover-block-edit', jetpackCoverBlockEdit );
 }
