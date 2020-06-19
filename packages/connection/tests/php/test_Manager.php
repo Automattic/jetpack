cf40ec33b8fa5da6938cc64f9c9bf9788f7ccbd8
Kim Brown <50059399+kbrown9@users.noreply.github.com>
2020-06-19 12:18:12 -0400

    Connection: move connection custom caps to package
    Move the connection-related custom capabilities to the Connection package.
    This will allow the custom capabilities to be used when the package is required by
    another plugin and Jetpack isn't installed.
    
    Add unit tests for the new Manager::jetpack_connection_custom_caps method.
    
    Add a new parameter, $namespace, to the ManagerTest::mock_function method. This allows
    mocking of functions with different namespaces.

--- packages/connection/tests/php/test_Manager.php
+++ packages/connection/tests/php/test_Manager.php
@@ -32,6 +32,8 @@ class ManagerTest extends TestCase {
 	 */
 	protected $arguments_stack = array();
 
+	const DEFAULT_TEST_CAPS = array( 'default_test_caps' );
+
 	/**
 	 * Initialize the object before running the test method.
 	 */
@@ -315,17 +317,65 @@ class ManagerTest extends TestCase {
 	}
 
 	/**
+	 * Test the `jetpack_connection_custom_caps' method.
+	 *
+	 * @covers Automattic\Jetpack\Connection\Manager::jetpack_connection_custom_caps
+	 * @dataProvider jetpack_connection_custom_caps_data_provider
+	 *
+	 * @param bool   $in_dev_mode Whether development mode is active.
+	 * @param string $custom_cap The custom capability that is being tested.
+	 * @param array  $expected_caps The expected output.
+	 */
+	public function test_jetpack_connection_custom_caps( $in_dev_mode, $custom_cap, $expected_caps ) {
+		// Mock the site_url call in Status::is_development_mode.
+		$this->mock_function( 'site_url', false, 'Automattic\Jetpack' );
+
+		// Mock the apply_filters( 'jetpack_development_mode', ) call in Status::is_development_mode.
+		$this->mock_function( 'apply_filters', $in_dev_mode, 'Automattic\Jetpack' );
+
+		// Mock the apply_filters( 'jetpack_disconnect_cap', ) call in jetpack_connection_custom_caps.
+		$this->mock_function( 'apply_filters', array( 'manage_options' ) );
+
+		$caps = $this->manager->jetpack_connection_custom_caps( self::DEFAULT_TEST_CAPS, $custom_cap, 1, array() );
+		$this->assertEquals( $expected_caps, $caps );
+	}
+
+	/**
+	 * Data provider test_jetpack_connection_custom_caps.
+	 *
+	 * The test data arrays contain:
+	 *    'in dev mode': Whether development mode is active.
+	 *    'custom cap': The custom capbability that is being tested.
+	 *    'expected caps': The expected output of the call to jetpack_connection_custom_caps. An array of strings.
+	 */
+	public function jetpack_connection_custom_caps_data_provider() {
+		return array(
+			'dev mode, jetpack_connect'          => array( true, 'jetpack_connect', array( 'do_not_allow' ) ),
+			'dev mode, jetpack_reconnect'        => array( true, 'jetpack_reconnect', array( 'do_not_allow' ) ),
+			'dev mode, jetpack_disconnect'       => array( true, 'jetpack_disconnect', array( 'manage_options' ) ),
+			'dev mode, jetpack_connect_user'     => array( true, 'jetpack_connect_user', array( 'do_not_allow' ) ),
+			'dev mode, unknown cap'              => array( true, 'unknown_cap', self::DEFAULT_TEST_CAPS ),
+			'not dev mode, jetpack_connect'      => array( false, 'jetpack_connect', array( 'manage_options' ) ),
+			'not dev mode, jetpack_reconnect'    => array( false, 'jetpack_reconnect', array( 'manage_options' ) ),
+			'not dev mode, jetpack_disconnect'   => array( false, 'jetpack_disconnect', array( 'manage_options' ) ),
+			'not dev mode, jetpack_connect_user' => array( false, 'jetpack_connect_user', array( 'read' ) ),
+			'not dev mode, unknown cap'          => array( false, 'unknown_cap', self::DEFAULT_TEST_CAPS ),
+		);
+	}
+
+	/**
 	 * Mock a global function and make it return a certain value.
 	 *
 	 * @param string $function_name Name of the function.
 	 * @param mixed  $return_value Return value of the function.
+	 * @param string $namespace The namespace of the function.
 	 *
 	 * @return Mock The mock object.
 	 * @throws MockEnabledException PHPUnit wasn't able to enable mock functions.
 	 */
-	protected function mock_function( $function_name, $return_value = null ) {
+	protected function mock_function( $function_name, $return_value = null, $namespace = __NAMESPACE__ ) {
 		$builder = new MockBuilder();
-		$builder->setNamespace( __NAMESPACE__ )
+		$builder->setNamespace( $namespace )
 			->setName( $function_name )
 			->setFunction(
 				function() use ( &$return_value ) {
