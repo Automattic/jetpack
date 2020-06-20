1149b8c23d0bb7a68fb44acbadd53581dbef9dfd
Derek Smart <smart@automattic.com>
2020-06-19 20:07:33 -0400

    [not verified] Remove status request to wp.com.

--- modules/plugin-search.php
+++ modules/plugin-search.php
@@ -18,8 +18,7 @@ if (
 	/** This filter is documented in _inc/lib/admin-pages/class.jetpack-react-page.php */
 	apply_filters( 'jetpack_show_promotions', true ) &&
 	// Disable feature hints when plugins cannot be installed.
-	! Constants::is_true( 'DISALLOW_FILE_MODS' ) &&
-	jetpack_is_psh_active()
+	! Constants::is_true( 'DISALLOW_FILE_MODS' )
 ) {
 	Jetpack_Plugin_Search::init();
 }
@@ -538,62 +537,4 @@ class Jetpack_Plugin_Search {
 		return $links;
 	}
 
-}
-
-/**
- * Master control that checks if Plugin search hints is active.
- *
- * @since 7.1.1
- *
- * @return bool True if PSH is active.
- */
-function jetpack_is_psh_active() {
-	// false means unset, 1 means active, 0 means inactive.
-	$status = get_transient( 'jetpack_psh_status' );
-
-	if ( false === $status ) {
-		$error = false;
-		$status = jetpack_get_remote_is_psh_active( $error );
-		set_transient(
-			'jetpack_psh_status',
-			// Cache as int
-			(int) $status,
-			// If there was an error, still cache but for a shorter time
-			( $error ? 5 : 15 ) * MINUTE_IN_SECONDS
-		);
-	}
-
-	return (bool) $status;
-}
-
-/**
- * Makes remote request to determine if Plugin search hints is active.
- *
- * @since 7.1.1
- * @internal
- *
- * @param bool &$error Did the remote request result in an error?
- * @return bool True if PSH is active.
- */
-function jetpack_get_remote_is_psh_active( &$error ) {
-	$response = wp_remote_get( 'https://jetpack.com/psh-status/' );
-	if ( is_wp_error( $response ) ) {
-		$error = true;
-		return true;
-	}
-
-	$body = wp_remote_retrieve_body( $response );
-	if ( empty( $body ) ) {
-		$error = true;
-		return true;
-	}
-
-	$json = json_decode( $body );
-	if ( ! isset( $json->active ) ) {
-		$error = true;
-		return true;
-	}
-
-	$error = false;
-	return (bool) $json->active;
 }
