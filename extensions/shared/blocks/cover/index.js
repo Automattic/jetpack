6572c94e3f07c30a57cc522a405f745663be1e50
Konstantin Obenland <obenland@gmx.de>
2020-06-22 15:11:20 -0700

    Cover: Only add filter if needed

--- extensions/shared/blocks/cover/index.js
+++ extensions/shared/blocks/cover/index.js
@@ -11,15 +11,23 @@ import { addFilter } from '@wordpress/hooks';
  * Internal dependencies
  */
 import coverEditMediaPlaceholder from './cover-media-placeholder';
-import isCurrentUserConnected from '../../is-current-user-connected';
 import jetpackCoverBlockEdit from './edit';
-
+import { isUpgradable } from './utils';
 import './editor.scss';
 
-if ( isCurrentUserConnected() ) {
-	// Take the control of MediaPlaceholder.
-	addFilter( 'editor.MediaPlaceholder', 'jetpack/cover-edit-media-placeholder', coverEditMediaPlaceholder );
+const addVideoUploadPlanCheck = ( settings, name ) => {
+	if ( ! settings.isDeprecation && isUpgradable( name ) ) {
+		// Take the control of MediaPlaceholder.
+		addFilter(
+			'editor.MediaPlaceholder',
+			'jetpack/cover-edit-media-placeholder',
+			coverEditMediaPlaceholder
+		);
 
-	// Extend Core CoverEditBlock.
-	addFilter( 'editor.BlockEdit', 'jetpack/cover-block-edit', jetpackCoverBlockEdit );
-}
+		// Extend Core CoverEditBlock.
+		addFilter( 'editor.BlockEdit', 'jetpack/cover-block-edit', jetpackCoverBlockEdit );
+	}
+
+	return settings;
+};
+addFilter( 'blocks.registerBlockType', 'core/cover', addVideoUploadPlanCheck );
