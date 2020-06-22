1e6b8b4f9ffd382f9e993eecd2bbc1408791ccd1
Konstantin Obenland <obenland@gmx.de>
2020-06-22 15:11:20 -0700

    Cover: Only add filter if needed

--- extensions/shared/blocks/cover/cover-media-placeholder.js
+++ extensions/shared/blocks/cover/cover-media-placeholder.js
@@ -1,4 +1,3 @@
-
 /**
  * WordPress dependencies
  */
@@ -8,7 +7,7 @@ import { createHigherOrderComponent } from '@wordpress/compose';
 /**
  * Internal dependencies
  */
-import { isUpgradable, isVideoFile } from './utils';
+import { isVideoFile } from './utils';
 import { CoverMediaContext } from './components';
 
 /**
@@ -19,7 +18,7 @@ const ALLOWED_MEDIA_TYPES = [ 'image', 'video' ];
 export default createHigherOrderComponent(
 	CoreMediaPlaceholder => props => {
 		const { name } = useBlockEditContext();
-		if ( ! isUpgradable( name ) ) {
+		if ( 'core/cover' !== name ) {
 			return <CoreMediaPlaceholder { ...props } />;
 		}
 
@@ -27,15 +26,15 @@ export default createHigherOrderComponent(
 		return (
 			<div className="jetpack-cover-media-placeholder">
 				<CoverMediaContext.Consumer>
-					{ ( onFilesUpload ) => (
+					{ onFilesUpload => (
 						<CoreMediaPlaceholder
 							{ ...props }
 							onFilesPreUpload={ onFilesUpload }
 							multiple={ false }
-							onError = { ( message ) => {
+							onError={ message => {
 								// Try to pick up filename from the error message.
 								// We should find a better way to do it. Unstable.
-								const filename = message?.[0]?.props?.children;
+								const filename = message?.[ 0 ]?.props?.children;
 								if ( filename && isVideoFile( filename ) ) {
 									return onFilesUpload( [ filename ] );
 								}
