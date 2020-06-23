bb429b2941118244eed857f192dc0d84c10322de
Takashi Irie <takashi@automattic.com>
2020-06-23 17:01:25 +0100

    Update Coming Soon Landing Page https://github.com/Automattic/wpcomsh/pull/473
    * Coming Soon Landing Page: Update markup and style to align with the current branding
    
    * Coming Soon Landing Page: Change the image path to a full path
    
    * Coming Soon Landing Page: Update markup and style to align with the current branding
    
    * Remove unused styles and fonts; move inline styles to enqueued sheet
    
    * Fix stylesheet name
    
    * Dequeue Jetpack CSS on landing page also
    
    * Bump version number
    
    * Update language files
    
    * Coming Soon Landing Page: Remove functions that are not available in Atomic. Also, some minor style changes to accommodate translations
    
    Co-authored-by: Michael Cain <michael@michaeldcain.com>
    Co-authored-by: Alex Kirk <alex.git@kirk.at>

--- projects/plugins/wpcomsh/private-site/access-denied-coming-soon-template.php
+++ projects/plugins/wpcomsh/private-site/access-denied-coming-soon-template.php
@@ -1,9 +1,10 @@
 <?php
-
 /**
  * WordPress.com Site Helper Private Site Template
  */
 namespace Private_Site;
+
+add_filter( 'jetpack_implode_frontend_css', '__return_false', 99 );
 
 nocache_headers();
 header( 'Content-Type: ' . get_bloginfo( 'html_type' ) . '; charset=' . get_bloginfo( 'charset' ) );
@@ -19,12 +20,10 @@ header( 'Content-Type: ' . get_bloginfo( 'html_type' ) . '; charset=' . get_blog
 	<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php echo bloginfo( 'name' ); ?></title>
- 	<link rel='stylesheet' id='all-css-0-1' href='/wp-content/mu-plugins/wpcomsh/private-site/style.css' type='text/css' media='all' />
 
 	<?php
-	wp_enqueue_style( 'domain-landing-style', '/wp-content/themes/a8c/domain-landing-page/style.css' );
-	wp_enqueue_style( 'merriweather', '//s1.wp.com/i/fonts/merriweather/merriweather.css' );
-	wp_enqueue_style( 'merriweathersans', '//s1.wp.com/i/fonts/merriweathersans/merriweathersans.css' );
+	wp_enqueue_style( 'recoleta-font', '//s1.wp.com/i/fonts/recoleta/css/400.min.css' );
+	wp_enqueue_style( 'wpcomsh-coming-soon-style', plugins_url( 'style.css', __FILE__ ) );
 	do_action( 'login_enqueue_scripts' );
 	do_action( 'login_head' );
 
@@ -36,114 +35,25 @@ header( 'Content-Type: ' . get_bloginfo( 'html_type' ) . '; charset=' . get_blog
 
 	$login_link = site_url() . '/wp-login.php?redirect_to=' . set_url_scheme( original_request_url() );
 	?>
-
-	<style type="text/css">
-		.body {
-			background: #006088;
-			font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen-Sans", "Ubuntu", "Cantarell", "Helvetica Neue", sans-serif;
-			justify-content: center;
-		}
-		.container {
-			flex-direction: row;
-			align-items: center;
-			justify-content: flex-start;
-			max-width: 90%;
-		}
-		.name {
-			color: #f7f4f5;
-			font-size: .9em;
-			text-align: left;
-			margin-bottom: 10px;
-		}
-		.description {
-			color: #fff;
-			font-size: 1.8em;
-			font-weight: bold;
-			line-height: 1.2;
-			text-align: left;
-			margin-bottom: 25px;
-			overflow: visible;
-		}
-		.image {
-			display: none;
-		}
-		@media screen and ( min-width: 660px ) {
-			.container {
-				max-width: 75%;
-			}
-			.inner {
-				max-width: 68%;
-			}
-			.image {
-				display: block;
-				max-width: 35%;
-				margin-left: 2%;
-				order: 4;
-			}
-			.image img {
-				max-width: 100%;
-				height: auto;
-				margin-top: -15px;
-			}
-		}
-		.marketing-bar-text {
-			color: #006088;
-		}
-		.marketing-bar .marketing-bar-button {
-			background: #d52c82;
-			border-color: #992053;
-			color: #fff;
-			text-shadow: none;
-		}
-		.wp-core-ui .button-primary {
-			background: #d52c82;
-			border-color: #992053;
-			color: #fff;
-			text-shadow: none;
-			border-style: solid;
-			border-width: 1px 1px 2px;
-			box-shadow: none;
-			cursor: pointer;
-			display: inline-block;
-			height: auto;
-			margin: 0;
-			outline: 0;
-			overflow: hidden;
-			font-weight: bold;
-			text-overflow: ellipsis;
-			text-decoration: none;
-			vertical-align: top;
-			box-sizing: border-box;
-			font-size: 14px;
-			line-height: 21px;
-			letter-spacing: 0;
-			border-radius: 4px;
-			padding: 7px 14px 9px;
-		}
-		.wp-core-ui .button-primary {
-			margin-left: 11px;
-		}
-		.wp-core-ui .button-primary:hover,
-		.wp-core-ui .button-primary:focus,
-		.marketing-bar .marketing-bar-button:hover,
-		.marketing-bar .marketing-bar-button:focus {
-			background: #ff3997;
-			border-color: #992053;
-			color: #fff;
-		}
-	</style>
 </head>
 <body class="body">
-<div class="container">
 	<div class="inner">
-		<div class="name"><?php echo esc_html( get_bloginfo( 'name' ) ) ?></div>
-		<div class="description"><?php echo esc_html( __( 'Something new is coming.', 'wpcomsh' ) ) ?></div>
-		<?php if ( ! is_user_logged_in() ) : ?>
-			<p class="wp-core-ui"><a class="button button-primary" href="<?php echo esc_url( $login_link ) ?>" style="float: none;"><?php esc_html_e( 'Log in here', 'wpcomsh' ) ?></a></p>
-		<?php endif; ?>
-	</div>
-	<div class="image">
-		<img src="https://s2.wp.com/i/private.svg" />
+		<div class="main">
+			<div class="name"><?php echo esc_html( get_bloginfo( 'name' ) ) ?></div>
+			<div class="description"><?php esc_html_e( 'Coming Soon', 'wpcomsh' ) ?></div>
+		</div>
+		<div class="marketing">
+			<?php if ( ! is_user_logged_in() ) : ?>
+				<div class="marketing-copy">
+					<img src="https://s2.wp.com/wp-content/themes/a8c/domain-landing-page/wpcom-wmark-white.svg" alt="WordPress.com" class="logo" />
+					<p class="copy"><?php echo esc_html_e( 'Build a website. Sell your stuff. Write a blog. And so much more.', 'wpcomsh' ) ?></p>
+				</div>
+				<div class="marketing-buttons">
+					<p><a class="button button-secondary" href="<?php echo esc_url( $login_link ) ?>"><?php esc_html_e( 'Log in', 'wpcomsh' ) ?></a></p>
+					<p><a class="button button-primary " href="https://wordpress.com/start/?ref=coming_soon"><?php esc_html_e( 'Start your website', 'wpcomsh' ) ?></a></p>
+				</div>
+			<?php endif; ?>
+		</div>
 	</div>
 </div>
 <?php wp_footer(); ?>
