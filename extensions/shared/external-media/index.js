d2c4f68772e755d083fd64fade7605ab952972fb
Konstantin Obenland <obenland@gmx.de>
2020-06-24 14:42:50 -0700

    Use `value` to disable for existing galleries
    `addToGallery` is not used by all gallery-type blocks, like tiled-gallery or slideshow.
    
    Props @jeherve.

--- extensions/shared/external-media/index.js
+++ extensions/shared/external-media/index.js
@@ -28,11 +28,11 @@ if ( isCurrentUserConnected() ) {
 		'editor.MediaUpload',
 		'external-media/replace-media-upload',
 		OriginalComponent => props => {
-			const { addToGallery = false, allowedTypes } = props;
+			const { allowedTypes, gallery = false, value = false } = props;
 			let { render } = props;
 
 			// Only replace button for components that expect images, except existing galleries.
-			if ( allowedTypes.indexOf( 'image' ) > -1 && ! addToGallery ) {
+			if ( allowedTypes.indexOf( 'image' ) > -1 && ! ( gallery && value ) ) {
 				render = button => <MediaButton { ...button } mediaProps={ props } />;
 			}
 
