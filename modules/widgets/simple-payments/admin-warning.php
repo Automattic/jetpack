4e8c7e73cb4e6660132deb3698d972fddd32af45
Eoin Gallagher <eoin@automattic.com>
2020-06-24 10:12:03 +0100

    Renaming Earn features (#16133)
    * Adds a one-time payment feature to Recurring Payments and rename the entire Recurring Payments feature to “Payments”. The bet is that this will simplify the user experience and increase usage, as customers will now have only one place to go when they want to accept payments.
    * “Simple Payments” as a feature will still be available to customers already utilizing it, but it's renamed to "Pay with PayPal"

--- modules/widgets/simple-payments/admin-warning.php
+++ modules/widgets/simple-payments/admin-warning.php
@@ -2,11 +2,11 @@
 	<p>
 		<?php
 			$support_url = ( defined( 'IS_WPCOM' ) && IS_WPCOM )
-				? 'https://support.wordpress.com/simple-payments/'
-				: 'https://jetpack.com/support/simple-payment-button/';
+				? 'https://wordpress.com/support/pay-with-paypal/'
+				: 'https://jetpack.com/support/pay-with-paypal/';
 			printf(
 				wp_kses(
-					__( 'Your plan doesn\'t include Simple Payments. <a href="%s" rel="noopener noreferrer" target="_blank">Learn more and upgrade</a>.', 'jetpack' ),
+					__( 'Your plan doesn\'t include Pay with PayPal. <a href="%s" rel="noopener noreferrer" target="_blank">Learn more and upgrade</a>.', 'jetpack' ),
 					array( 'a' => array( 'href' => array(), 'rel' => array(), 'target' => array() ) )
 				),
 				esc_url( $support_url )
