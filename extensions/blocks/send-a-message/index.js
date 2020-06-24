8d7c32dcc13bab80bc221eadfd28ad69885fcc9b
Andy Peatling <apeatling@users.noreply.github.com>
2020-06-24 09:34:06 -0700

    Update extensions/blocks/send-a-message/index.js
    Co-authored-by: Foteini Giannaropoulou <giannaropoulou.foteini@gmail.com>

--- extensions/blocks/send-a-message/index.js
+++ extensions/blocks/send-a-message/index.js
@@ -23,7 +23,7 @@ export const title = __( 'Send A Message', 'jetpack' );
 
 export const settings = {
 	title,
-	description: __( 'Let your visitors to send you messages with the tap of a button.', 'jetpack' ),
+	description: __( 'Let your visitors send you messages with the tap of a button.', 'jetpack' ),
 	icon: renderMaterialIcon(
 		<Path d="M21 6h-2v9H6v2c0 .55.45 1 1 1h11l4 4V7c0-.55-.45-1-1-1zm-4 6V3c0-.55-.45-1-1-1H3c-.55 0-1 .45-1 1v14l4-4h10c.55 0 1-.45 1-1z" />
 	),
