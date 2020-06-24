72178668581ed2946e3ef83788e426863bed784f
Brandon Kraft <public@brandonkraft.com>
2020-06-24 09:31:26 -0500

    [not verified] Revert "Autoloader: use JETPACK__PLUGIN_DIR when looking for the jetpack plugin directory. (#16251)"
    This reverts commit a477be1564da05975c8997f93182a1159dd05593.

--- packages/autoloader/src/class-files-handler.php
+++ packages/autoloader/src/class-files-handler.php
@@ -65,12 +65,7 @@ class Files_Handler {
 	 * @return Array An array of plugin names and filemap paths.
 	 */
 	public function create_filemap_path_array( $plugin ) {
-		if ( 'jetpack' === $plugin ) {
-			$plugin_path = JETPACK__PLUGIN_DIR;
-		} else {
-			$plugin_path = trailingslashit( WP_PLUGIN_DIR ) . $plugin;
-		}
-
+		$plugin_path = trailingslashit( WP_PLUGIN_DIR ) . $plugin;
 		return trailingslashit( $plugin_path ) . 'vendor/composer/jetpack_autoload_filemap.php';
 	}
 
