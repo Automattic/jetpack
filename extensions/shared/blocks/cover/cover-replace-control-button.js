1e1b2087c306a728332f1c1d8f9e14d6bd9407f0
Damián Suárez <rdsuarez@gmail.com>
2020-06-25 07:02:34 -0300

    Cover: Handle upgrade nudge when replace background (#16272)
    * cover: add media replace flow extention
    
    * cover: extend media replace flow to handle nudge
    
    * Update extensions/shared/blocks/cover/cover-replace-control-button.js
    
    Co-authored-by: Konstantin Obenland <obenland@gmx.de>
    
    * cover: single quotes. remove unused module
    
    Co-authored-by: Konstantin Obenland <obenland@gmx.de>

---
+++ extensions/shared/blocks/cover/cover-replace-control-button.js
@@ -0,0 +1,46 @@
+
+/**
+ * External dependencies
+ */
+
+/**
+ * WordPress dependencies
+ */
+import { useBlockEditContext } from '@wordpress/block-editor';
+import { createHigherOrderComponent } from '@wordpress/compose';
+import { useRef, useContext } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import { CoverMediaContext } from './components';
+import { isVideoFile } from './utils';
+
+export default createHigherOrderComponent( MediaReplaceFlow => props => {
+	const { name } = useBlockEditContext();
+	const preUploadFile = useRef();
+	if ( 'core/cover' !== name ) {
+		return <MediaReplaceFlow { ...props } />;
+	}
+
+	const onFilesUpload = useContext( CoverMediaContext );
+
+	return (
+		<MediaReplaceFlow
+			{ ...props }
+			onFilesUpload={ ( files ) => {
+				preUploadFile.current = files?.length ? files[ 0 ] : null;
+				onFilesUpload( files );
+			} }
+			createNotice={ ( status, msg, options ) => {
+				// Detect video file from callback and reference instance.
+				if ( isVideoFile( preUploadFile.current ) ) {
+					preUploadFile.current = null; // clean up the file reference.
+					return null;
+				}
+
+				props.createNotice( status, msg, options );
+			} }
+		/>
+	);
+}, 'JetpackCoverMediaReplaceFlow' );
