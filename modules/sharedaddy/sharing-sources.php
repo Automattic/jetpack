50016c8b32311ae6f3077faf78c8cc8c8e5e67e6
Jeremy Herve <jeremy@jeremy.hu>
2020-06-26 16:53:45 +0200

    Facebook: avoid relying on jQuery for the official button (#16295)
    Jetpack's Sharing feature does not rely on jQuery, and not all sites load jQuery via their theme or other plugins so we cannot expect it to be available.

--- modules/sharedaddy/sharing-sources.php
+++ modules/sharedaddy/sharing-sources.php
@@ -1163,7 +1163,7 @@ class Share_Facebook extends Sharing_Source {
 			?><div id="fb-root"></div>
 			<script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = 'https://connect.facebook.net/<?php echo $locale; ?>/sdk.js#xfbml=1<?php echo $fb_app_id; ?>&version=v2.3'; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));</script>
 			<script>
-			jQuery( document.body ).on( 'post-load', function() {
+			document.body.addEventListener( 'post-load', function() {
 				if ( 'undefined' !== typeof FB ) {
 					FB.XFBML.parse();
 				}
