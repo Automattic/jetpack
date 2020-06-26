7710513a7dea4a162cce939cca248210d407875a
leogermani <leogermani@gmail.com>
2020-06-26 20:10:21 -0300

    add errors to the jetpack react dashboard

--- packages/connection/src/error-handlers/class-invalid-blog-token.php
+++ packages/connection/src/error-handlers/class-invalid-blog-token.php
@@ -24,8 +24,29 @@ class Invalid_Blog_Token {
 	 */
 	public function __construct( $errors ) {
 
+		/**
+		 * Filters the message to be displayed in the admin notices area when there's a invalid blog token xmlrpc error
+		 *
+		 * Return an empty value to disable the message.
+		 *
+		 * @since 8.7.0
+		 *
+		 * @param string $message The error message.
+		 */
+		$this->message = apply_filters( 'jetpack_connection_invalid_blog_token_admin_notice', __( 'Your connection with WordPress.com seems to be broken. If you\'re experiencing issues, please try reconnecting.', 'jetpack' ) );
+
+		if ( empty( $this->message ) ) {
+			return;
+		}
+
 		// In this class, we will only handle errors with the blog token, so ignoring if there are only errors with user tokens.
 		if ( ! isset( $errors[0] ) || ! isset( $errors['invalid'] ) ) {
+			add_action( 'react_connection_errors_initial_state', array( $this, 'jetpack_react_dashboard_error' ) );
+			// do not add admin notice to the jetpack dashboard.
+			global $pagenow;
+			if ( 'admin.php' === $pagenow || isset( $_GET['page'] ) && 'jetpack' === $_GET['page'] ) { // phpcs:ignore
+				return;
+			}
 			add_action( 'admin_notices', array( $this, 'admin_notice' ) );
 		}
 
@@ -39,27 +60,27 @@ class Invalid_Blog_Token {
 	 * @return void
 	 */
 	public function admin_notice() {
-
-		/**
-		 * Filters the message to be displayed in the admin notices area when there's a invalid blog token xmlrpc error
-		 *
-		 * Return an empty value to disable the message.
-		 *
-		 * @since 8.7.0
-		 *
-		 * @param string $message The error message.
-		 */
-		$message = apply_filters( 'jetpack_connection_invalid_blog_token_admin_notice', __( 'Your connection with WordPress.com seems to be broken. If you\'re experiencing issues, please try reconnecting.', 'jetpack' ) );
-
-		if ( empty( $message ) ) {
-			return;
-		}
-
 		?>
 		<div class="notice notice-error is-dismissible jetpack-message jp-connect" style="display:block !important;">
-			<p><?php echo esc_html( $message ); ?></p>
+			<p><?php echo esc_html( $this->message ); ?></p>
 		</div>
 		<?php
+	}
+
+	/**
+	 * Adds the error message to the Jetpack React Dashboard
+	 *
+	 * @param array $errors The array of errors.
+	 * @return array
+	 */
+	public function jetpack_react_dashboard_error( $errors ) {
+
+		$errors[] = array(
+			'code'    => 'invalid_blog_token',
+			'message' => $this->message,
+			'action'  => 'reconnect',
+		);
+		return $errors;
 	}
 
 
