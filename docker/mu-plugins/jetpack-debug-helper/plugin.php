6280b2e4702dc6e3f431119e3305ba44374d2e07
Brandon Kraft <public@brandonkraft.com>
2020-06-29 14:47:51 -0500

    Make the broken token tool off by default

--- docker/mu-plugins/jetpack-debug-helper/plugin.php
+++ docker/mu-plugins/jetpack-debug-helper/plugin.php
@@ -9,7 +9,53 @@
  * @package Jetpack.
  */
 
-/**
- * List "plugins" from the src directory here.
+namespace Automattic\Jetpack\Debug_Helper;
+
+// phpcs:disable WordPress.WhiteSpace.PrecisionAlignment.Found
+
+/*
+ .--..--..--..--..--..--..--..--..--..--..--..--..--..--..--..--.
+/ .. \.. \.. \.. \.. \.. \.. \.. \.. \.. \.. \.. \.. \.. \.. \.. \
+\ \/\ \/\ \/\ \/\ \/\ \/\ \/\ \/\ \/\ \/\ \/\ \/\ \/\ \/\ \/\ \/ /
+ \/ /\/ /\/ /\/ /\/ /\/ /\/ /\/ /\/ /\/ /\/ /\/ /\/ /\/ /\/ /\/ /
+ / /\/ /`' /`' /`' /`' /`' /`' /`' /`' /`' /`' /`' /`' /`' /\/ /\
+/ /\ \/`--'`--'`--'`--'`--'`--'`--'`--'`--'`--'`--'`--'`--'\ \/\ \
+\ \/\ \                                                    /\ \/ /
+ \/ /\ \                                                  / /\/ /
+ / /\/ /    MUST COMMENT OUT THE LINE BELOW               \ \/ /\
+/ /\ \/                                                    \ \/\ \
+\ \/\ \             TO ACTIVATE THE BROKEN TOKEN TOOL.     /\ \/ /
+ \/ /\ \                                                  / /\/ /
+ / /\/ /                                                  \ \/ /\
+/ /\ \/                                                    \ \/\ \
+\ \/\ \.--..--..--..--..--..--..--..--..--..--..--..--..--./\ \/ /
+ \/ /\/ ../ ../ ../ ../ ../ ../ ../ ../ ../ ../ ../ ../ ../ /\/ /
+ / /\/ /\/ /\/ /\/ /\/ /\/ /\/ /\/ /\/ /\/ /\/ /\/ /\/ /\/ /\/ /\
+/ /\ \/\ \/\ \/\ \/\ \/\ \/\ \/\ \/\ \/\ \/\ \/\ \/\ \/\ \/\ \/\ \
+\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `' /
+ `--'`--'`--'`--'`--'`--'`--'`--'`--'`--'`--'`--'`--'`--'`--'`--'
  */
-require_once plugin_dir_path( __FILE__ ) . 'class-broken-token.php';
+add_filter( 'jetpack_debug_helper_modules', '__return_empty_array' );
+
+// phpcs:enable
+
+/**
+ * Include file names from the modules directory here.
+ *
+ * @todo Add UI to make this easier to use in a testing situation.
+ */
+$modules = array(
+	'class-broken-token.php',
+);
+
+/**
+ * Filter the features of the Jetpack Debug Helper.
+ *
+ * This is part of the mu-plugins folder within Jetpack's built-in local Docker environment.
+ * This filter does not exist and is non-functional in production code.
+ *
+ * @param array $modules Array of file names. File names are based on the docker/mu-plugins/jetpack-debug-helper/inc folder.
+ */
+foreach ( (array) apply_filters( 'jetpack_debug_helper_modules', $modules ) as $module ) {
+	include_once plugin_dir_path( __FILE__ ) . 'modules/' . $module;
+}
