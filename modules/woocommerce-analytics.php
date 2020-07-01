a783d7b36765ec8c34aa09afae801b4512349cbe
Jeremy Herve <jeremy@jeremy.hu>
2020-07-01 13:00:49 +0200

    Deprecate and deactivate module
    First time the site is loaded post-update, the module will be deactivated.
    Then nothing will happen anymore.
    
    We have no functions that were available or used outside of the module before, so we don't need to issue deprecation notices for anything.

--- modules/woocommerce-analytics.php
+++ modules/woocommerce-analytics.php
@@ -1,22 +1,25 @@
 <?php
 /**
- * Module Name: WooCommerce Analytics
- * Module Description: Enhanced analytics for WooCommerce and Jetpack users.
- * Sort Order: 13
- * First Introduced: 8.4
- * Requires Connection: Yes
- * Auto Activate: Yes
- * Module Tags: Other, Recommended
- * Feature: Engagement
- * Additional Search Queries: woocommerce, analytics, stats, statistics, tracking, analytics, views
+ * Deprecated since 8.8.0
  *
+ * Originally we created the WooCommerce Analytics feature
+ * to bring more information to Store Stats available
+ * in the WordPress.com Stats dashboard:
+ * https://wordpress.com/store/stats/orders/day/
+ *
+ * We're not currently working on this Store Stats screen
+ * and may now be able to use the WooCommerce plugin directly
+ * to access that information.
+ *
+ * @deprecated
  * @package Jetpack
  */
 
 /**
- * Load module functionality.
+ * Deactivate module if it is still active.
+ *
+ * @since 8.8.0
  */
-function jetpack_load_woocommerce_analytics() {
-	require_once dirname( __FILE__ ) . '/woocommerce-analytics/class-jetpack-woocommerce-analytics.php';
+if ( Jetpack::is_module_active( 'woocommerce-analytics' ) ) {
+	Jetpack::deactivate_module( 'woocommerce-analytics' );
 }
-jetpack_load_woocommerce_analytics();
