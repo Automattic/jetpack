f6c89061dbc6c26552621a7299e2307aa5dcfb59
Jeremy Herve <jeremy@jeremy.hu>
2020-07-01 13:02:13 +0200

    Remove module check from module availalibity

--- class.jetpack-admin.php
+++ class.jetpack-admin.php
@@ -193,20 +193,6 @@ class Jetpack_Admin {
 			return false;
 		}
 
-		/*
-		 * WooCommerce Analytics should only be available
-		 * when running WooCommerce 3+
-		 */
-		if (
-			'woocommerce-analytics' === $module['module']
-			&& (
-				! class_exists( 'WooCommerce' )
-				|| version_compare( WC_VERSION, '3.0', '<' )
-			)
-		) {
-			return false;
-		}
-
 		if ( ( new Status() )->is_development_mode() ) {
 			return ! ( $module['requires_connection'] );
 		} else {
