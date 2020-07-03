f1c89760f3e65ad251e505bf036af446a6126368
leogermani <leogermani@gmail.com>
2020-07-03 11:38:29 -0300

    Move debug helper to packages folder (#16376)
    * Move debug helper to packages folder
    
    * fix PHP lint for 5.6
    
    * rename variable for better readability

--- bin/update-package.sh
+++ bin/update-package.sh
@@ -54,6 +54,12 @@ for package in packages/*; do
 
 	cd $CLONE_DIR
 
+	# check if composer.json exists
+	COMPOSER_JSON_EXISTED=false
+	if [ -f "composer.json" ]; then
+		COMPOSER_JSON_EXISTED=true
+	fi
+
 	find . | grep -v ".git" | grep -v "^\.*$" | xargs rm -rf # delete all files (to handle deletions in monorepo)
 
 	echo "  Copying from ${BASE}/packages/${NAME}/."
@@ -61,7 +67,7 @@ for package in packages/*; do
 	cp -r $BASE/packages/$NAME/. .
 
 	# Before we commit any changes, ensure that the repo has the basics we need for any package.
-	if [ ! -f "composer.json" ]; then
+	if $COMPOSER_JSON_EXISTED && [ ! -f "composer.json" ]; then
 		echo "  Those changes remove essential parts of the package. They will not be committed."
 	# Commit if there is any change that could be committed
 	elif [ -n "$(git status --porcelain)" ]; then
