b861e0f0a9eaeea678cc3a41d60eb060ee713e8a
Tugdual de Kerviler <dekervit@gmail.com>
2020-07-04 00:23:22 +0200

    Blur image when backdrop-filter is not available

--- extensions/blocks/story/player/components/background.js
+++ extensions/blocks/story/player/components/background.js
@@ -12,6 +12,11 @@ export default function Background( { currentMedia } ) {
 
 	return html`
 		<div class="wp-story-background">
+			<svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+				<filter id="gaussian-blur-10">
+					<feGaussianBlur stdDeviation="10" />
+				</filter>
+			</svg>
 			<img src=${url} alt="" />
 			<div class="wp-story-background-blur"></div>
 		</div>
