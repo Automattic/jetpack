8e05cc4b91d9771a33477cfc5807797e0599877d
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-07-08 13:09:42 +0300

    try reset wordpress

--- tests/e2e/specs/connection.test.js
+++ tests/e2e/specs/connection.test.js
@@ -3,25 +3,28 @@
  */
 import { catchBeforeAll, step } from '../lib/setup-env';
 import { doInPlaceConnection } from '../lib/flows/jetpack-connect';
-import { execWpCommand } from '../lib/utils-helper';
+import { execWpCommand, resetWordpressInstall } from '../lib/utils-helper';
 import Sidebar from '../lib/pages/wp-admin/sidebar';
 import JetpackPage from '../lib/pages/wp-admin/jetpack';
 
 describe( 'Connection', () => {
 	catchBeforeAll( async () => {
-		await execWpCommand( 'wp option delete jetpack_private_options' );
-		await execWpCommand( 'wp config set JETPACK_SHOULD_NOT_USE_CONNECTION_IFRAME false' );
-		await page.reload();
-		// For some reason it need 2 reloads to make constant actually work.
-		await page.reload();
+		// await execWpCommand( 'wp option delete jetpack_private_options' );
+		// await execWpCommand( 'wp config set JETPACK_SHOULD_NOT_USE_CONNECTION_IFRAME false' );
+		// await page.reload();
+		// // For some reason it need 2 reloads to make constant actually work.
+		// await page.reload();
+		await resetWordpressInstall();
+
 	} );
 
 	afterAll( async () => {
-		await execWpCommand(
-			'wp option update jetpack_private_options --format=json',
-			'< jetpack_private_options.txt'
-		);
-		await execWpCommand( 'wp config set JETPACK_SHOULD_NOT_USE_CONNECTION_IFRAME true' );
+		// await execWpCommand(
+		// 	'wp option update jetpack_private_options --format=json',
+		// 	'< jetpack_private_options.txt'
+		// );
+		// await execWpCommand( 'wp config set JETPACK_SHOULD_NOT_USE_CONNECTION_IFRAME true' );
+		// await resetWordpressInstall();
 	} );
 
 	it( 'In-place', async () => {
