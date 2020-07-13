64c69436323ce3e668bd5f990f8131978d6056bf
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2020-07-13 14:54:49 +1000

    Wrap saved opentable block content to allow custom classes (#16326)
    When there is no root HTML element in the saved content, Gutenberg's automatically injected className attribute doesn't get determined correctly when populating the editor, so gets overwritten.

---
+++ extensions/blocks/opentable/deprecated/v1/index.js
@@ -0,0 +1,21 @@
+/**
+ * Internal dependencies
+ */
+import { defaultAttributes } from '../../attributes';
+
+export default {
+	attributes: defaultAttributes,
+	supports: {
+		align: true,
+		html: false,
+	},
+	save: ( { attributes: { rid } } ) => (
+		<>
+			{ rid.map( restaurantId => (
+				<a href={ `https://www.opentable.com/restref/client/?rid=${ restaurantId }` }>
+					{ `https://www.opentable.com/restref/client/?rid=${ restaurantId }` }
+				</a>
+			) ) }
+		</>
+	),
+};
--- extensions/blocks/opentable/index.js
+++ extensions/blocks/opentable/index.js
@@ -8,6 +8,7 @@ import { createBlock } from '@wordpress/blocks';
  * Internal dependencies
  */
 import { defaultAttributes } from './attributes';
+import deprecatedV1 from './deprecated/v1';
 import edit from './edit';
 import icon from './icon';
 
@@ -37,13 +38,13 @@ export const settings = {
 	},
 	edit,
 	save: ( { attributes: { rid } } ) => (
-		<>
+		<div>
 			{ rid.map( restaurantId => (
 				<a href={ `https://www.opentable.com/restref/client/?rid=${ restaurantId }` }>
 					{ `https://www.opentable.com/restref/client/?rid=${ restaurantId }` }
 				</a>
 			) ) }
-		</>
+		</div>
 	),
 	attributes: defaultAttributes,
 	example: {
@@ -72,4 +73,5 @@ export const settings = {
 			},
 		],
 	},
+	deprecated: [ deprecatedV1 ],
 };
