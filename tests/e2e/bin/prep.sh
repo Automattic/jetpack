cc29ad8369d3685f3afa609909c7d6fdd73160f4
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-07-15 19:35:20 +0300

    [not verified] update CI environment setup script

--- tests/e2e/bin/prep.sh
+++ tests/e2e/bin/prep.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 # Exit if any command fails.
-set -e
+set -ex
 
-WP_CORE_DIR="/var/www/html"
-WORKING_DIR="/var/www/html/wp-content/jetpack-dev"
-JP_DIR="wp-content/jetpack"
+##
+# This script creates a jetpack .zip that is accessible externaly via site/wp-content/jetpack.zip
+# Also it creates a symlink from Jetpack directory to the wp-content/plugins
 
-rm -rf $JP_DIR $JP_DIR.zip wp-content/plugins/jetpack/
+# Parameters
+WP_CORE_DIR=${1-"/var/www/html"}
+WORKING_DIR=${2-"$WP_CORE_DIR/wp-content/jetpack-dev"}
+
+ZIP_FILE="$WP_CORE_DIR/wp-content/jetpack.zip"
+JP_DIR="/tmp/jetpack"
+
+rm -rf $JP_DIR $ZIP_FILE wp-content/plugins/jetpack/
 # removing symlink
 rm wp-content/plugins/jetpack-dev || true
 mkdir -p $JP_DIR
@@ -14,17 +21,15 @@ mkdir -p $JP_DIR
 FILES=$(ls -Ad $WORKING_DIR/* | grep -Ev "node_modules|docker|docs|extensions|.git")
 cp -r $FILES $JP_DIR
 
-echo "Zipping jetpack bundle"
+apt update > /dev/null 2>&1
+apt install zip -y > /dev/null 2>&1
 
-apt update > /dev/null
-apt install zip -y > /dev/null
+cd $(dirname "$JP_DIR")
 
-cd /var/www/html/wp-content
-
-zip -qr jetpack.zip jetpack/
-rm -rf jetpack/
-
-echo "Done!"
+zip -qr $ZIP_FILE jetpack/
+rm -rf $JP_DIR
 
 # Symlink Jetpack into plugins directory
-ln -s $WORKING_DIR $WP_CORE_DIR/wp-content/plugins/
+ln -s $WORKING_DIR $WP_CORE_DIR/wp-content/plugins/jetpack-dev
+
+echo "Done with jetpack.zip preparation!"
