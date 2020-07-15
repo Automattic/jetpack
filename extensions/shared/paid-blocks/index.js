0b5873df6cbd9f3c9ad2a90038f6861d62d4700f
retrofox <rdsuarez@gmail.com>
2020-07-15 20:30:39 -0300

    upgrade-banner: add styles

--- extensions/shared/paid-blocks/index.js
+++ extensions/shared/paid-blocks/index.js
@@ -10,13 +10,21 @@ import { addFilter } from '@wordpress/hooks';
 /**
  * Internal dependencies
  */
+import withCustomClassNames from '../with-custom-class-names';
 import jetpackPaidBlockEdit from './paid-block-edit';
 import { isUpgradable } from '../plan-utils';
+import './editor.scss';
 
 const jetpackPaidBlock = ( settings, name ) => {
 	if ( ! isUpgradable( name ) ) {
 		return settings;
 	}
+
+	addFilter(
+		'editor.BlockListBlock',
+		`jetpack/videopress-with-has-warning-is-interactive-class-names`,
+		withCustomClassNames( name, 'has-warning is-interactive is-upgradable' )
+	);
 
 	return {
 		...settings,
