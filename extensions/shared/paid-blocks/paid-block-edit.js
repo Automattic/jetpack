fd8174894fe4f454da3130e826b23ffa461dba2d
retrofox <rdsuarez@gmail.com>
2020-07-15 09:35:24 -0300

    paid-blocks: composition + HOC instead of hooks

--- extensions/shared/paid-blocks/paid-block-edit.js
+++ extensions/shared/paid-blocks/paid-block-edit.js
@@ -3,61 +3,68 @@
  * WordPress dependencies
  */
 import { Fragment } from '@wordpress/element';
-import { createHigherOrderComponent } from '@wordpress/compose';
+import { createHigherOrderComponent, compose } from '@wordpress/compose';
 import { BlockControls } from '@wordpress/block-editor';
 import { ToolbarGroup, Button } from '@wordpress/components';
-import { useSelect } from '@wordpress/data';
+import { withSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
-import { getUpgradeUrl, isUpgradable } from './utils';
+import { getUpgradeUrl } from './utils';
 
-export default createHigherOrderComponent(
-	OriginalBlockEdit => props => {
-		const planSlug = 'value_bundle';
-		const { plan, postId, postType } = useSelect( select => {
-			return {
-				plan: select( 'wordpress-com/plans' ).getPlan( planSlug ),
-				postId: select( 'core/editor' ).getCurrentPostId(),
-				postType: select( 'core/editor' ).getCurrentPostType(),
-			};
-		} );
+const JetpackPaidBlockEdit = OriginalBlockEdit => props => {
+	const { plan, postId, postType, planSlug } = props;
 
-		if ( ! isUpgradable( props?.name ) ) {
-			return <OriginalBlockEdit { ...props } />;
+	const goToCheckoutPage = () => {
+		if ( ! window?.location?.href ) {
+			return;
 		}
 
-		const goToCheckoutPage = () => {
-			if ( ! window?.location?.href ) {
-				return;
-			}
-			// Redirect to checkout page.
-			window.location.href = getUpgradeUrl( { plan, planSlug, postId, postType } );
-		};
+		// Redirect to checkout page.
+		window.location.href = getUpgradeUrl( { plan, planSlug, postId, postType } );
+	};
 
-		return (
-			<Fragment>
-				<BlockControls>
-					<ToolbarGroup>
-						<Button
-							aria-label={ __( 'Upgrade to Premium to use this block', 'jetpack' ) }
-							onClick={ goToCheckoutPage }
-							label={ __(
-								'Upgrade to Premium to use this block.',
-								'jetpack'
-							) }
-							showTooltip={ true }
-						>
-							{ __( 'Upgrade', 'jetpack' ) }
-						</Button>
-					</ToolbarGroup>
-				</BlockControls>
+	return (
+		<Fragment>
+			<BlockControls>
+				<ToolbarGroup>
+					<Button
+						aria-label={ __( 'Upgrade to Premium to use this block', 'jetpack' ) }
+						onClick={ goToCheckoutPage }
+						label={ __(
+							'Upgrade to Premium to use this block.',
+							'jetpack'
+						) }
+						showTooltip={ true }
+					>
+						{ __( 'Upgrade', 'jetpack' ) }
+					</Button>
+				</ToolbarGroup>
+			</BlockControls>
 
-				<OriginalBlockEdit { ...props } />
-			</Fragment>
-		);
-	},
+			<OriginalBlockEdit { ...props } />
+		</Fragment>
+	);
+};
+
+export default createHigherOrderComponent(
+	compose( [
+		withSelect( select => {
+			const editorSelector = select( 'core/editor' );
+			const post = editorSelector.getCurrentPost();
+			const planSlug = 'value_bundle';
+
+			return {
+				plan: select( 'wordpress-com/plans' ).getPlan( planSlug ),
+				planSlug,
+				postId: post.id,
+				postType: post.type,
+				postStatus: post.status,
+			};
+		} ),
+		JetpackPaidBlockEdit,
+	] ),
 	'JetpackPaidBlockEdit'
 );
