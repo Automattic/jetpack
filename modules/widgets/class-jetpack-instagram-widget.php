b0fa8a06b2174dd02cea378781e3225e42a957b3
Ben Dwyer <ben@scruffian.com>
2020-07-16 16:05:11 +0100

    Update modules/widgets/class-jetpack-instagram-widget.php
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>

--- modules/widgets/class-jetpack-instagram-widget.php
+++ modules/widgets/class-jetpack-instagram-widget.php
@@ -276,7 +276,7 @@ class Jetpack_Instagram_Widget extends WP_Widget {
 						),
 					)
 				),
-				esc_attr( add_query_arg( 'instagram_widget_id', $this->number, admin_url( 'widgets.php' ) ) )
+				esc_url( add_query_arg( 'instagram_widget_id', $this->number, admin_url( 'widgets.php' ) ) )
 			) . '</em></p>';
 		}
 
