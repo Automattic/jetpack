9aed608029a6df95534c9cd6b98a8421bf48df3a
retrofox <rdsuarez@gmail.com>
2020-07-16 08:42:04 -0300

    upgrade-banner: add to the inspector control (try)

--- extensions/shared/paid-blocks/paid-block-edit.js
+++ extensions/shared/paid-blocks/paid-block-edit.js
@@ -3,6 +3,9 @@
  * WordPress dependencies
  */
 import { Fragment } from '@wordpress/element';
+import { InspectorControls } from '@wordpress/block-editor';
+
+import { PanelBody } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -12,6 +15,12 @@ import UpgradePlanBanner from './upgrade-plan-banner';
 export default OriginalBlockEdit => props => {
 	return (
 		<Fragment>
+			<InspectorControls>
+				<PanelBody>
+					<UpgradePlanBanner description={ null } />
+				</PanelBody>
+			</InspectorControls>
+
 			<UpgradePlanBanner align={ props?.attributes?.align } />
 			<OriginalBlockEdit { ...props } />
 		</Fragment>
