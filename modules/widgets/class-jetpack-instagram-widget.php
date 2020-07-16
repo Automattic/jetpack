b9caed76a764ad2170d54e73503b8bfd6a734210
Ben Dwyer <ben@scruffian.com>
2020-07-16 13:47:11 +0100

    Update modules/widgets/class-jetpack-instagram-widget.php
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>

--- modules/widgets/class-jetpack-instagram-widget.php
+++ modules/widgets/class-jetpack-instagram-widget.php
@@ -101,7 +101,13 @@ class Jetpack_Instagram_Widget extends WP_Widget {
 	 * Sends a json success or error response.
 	 */
 	public function ajax_update_widget_token_id() {
-		check_ajax_referer( 'instagram-widget-save-token', 'savetoken' );
+		if ( ! check_ajax_referer( 'instagram-widget-save-token', 'savetoken', false ) ) {
+			wp_send_json_error( array( 'message' => 'bad_nonce' ), 403 )
+		}
+
+		if ( ! current_user_can( 'customize' ) ) {
+			wp_send_json_error( array( 'message' => 'not_authorized' ), 403 )
+		}
 
 		$token_id  = (int) $_POST['keyring_id'];
 		$widget_id = (int) $_POST['instagram_widget_id'];
