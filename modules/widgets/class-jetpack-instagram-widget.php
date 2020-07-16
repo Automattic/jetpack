290b6be2263a7f65941405ca9c6eba8df0fed3c5
Ben Dwyer <ben@scruffian.com>
2020-07-16 16:05:55 +0100

    Update modules/widgets/class-jetpack-instagram-widget.php
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>

--- modules/widgets/class-jetpack-instagram-widget.php
+++ modules/widgets/class-jetpack-instagram-widget.php
@@ -302,7 +302,17 @@ class Jetpack_Instagram_Widget extends WP_Widget {
 
 				echo '<div class="' . esc_attr( 'wpcom-instagram-images wpcom-instagram-columns-' . (int) $instance['columns'] ) . '">' . "\n";
 				foreach ( $images as $image ) {
-					echo '<a href="' . esc_url( $image['link'] ) . '" target="' . esc_attr( apply_filters( 'wpcom_instagram_widget_target', '_self' ) ) . '"><div class="sq-bg-image" style="background-image: url(' . esc_url( set_url_scheme( $image['url'] ) ) . ')"><span class="screen-reader-text">' . esc_attr( $image['title'] ) . '</span></div></a>' . "\n";
+					/**
+					 * Filter how Instagram image links open in the Instagram widget.
+					 *
+					 * @module widgets
+					 *
+					 * @since 8.8.0
+					 *
+					 * @param string $target Target attribute.
+					 */
+					$image_target = apply_filters( 'wpcom_instagram_widget_target', '_self' );
+					echo '<a href="' . esc_url( $image['link'] ) . '" target="' . esc_attr( $image_target ) . '"><div class="sq-bg-image" style="background-image: url(' . esc_url( set_url_scheme( $image['url'] ) ) . ')"><span class="screen-reader-text">' . esc_attr( $image['title'] ) . '</span></div></a>' . "\n";
 				}
 				echo "</div>\n";
 			}
