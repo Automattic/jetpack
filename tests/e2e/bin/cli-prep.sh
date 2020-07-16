4cb03f792da0646b8ac4b341c73b8d9053c093d2
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-07-16 14:07:01 +0300

    [not verified] get rid of includes functions in cli-prep script

--- tests/e2e/bin/cli-prep.sh
+++ tests/e2e/bin/cli-prep.sh
@@ -9,29 +9,29 @@ WP_CORE_DIR=${2-$PWD}
 
 # . "$(dirname "$0")/includes.sh"
 
-echo "#############"
+# echo "#############"
 
-pwd
-echo $0
-echo "$(dirname "$0")/includes.sh"
-QQQ=dirname "$(readlink -f "$0")"
-echo $QQQ
-. $QQQ/includes.sh
+# pwd
+# echo $0
+# echo "$(dirname "$0")/includes.sh"
+# QQQ=dirname "$(readlink -f "$0")"
+# echo $QQQ
+# . $QQQ/includes.sh
 
 # Reset the database so no posts/comments/etc.
-echo -e $(status_message "Resetting test database...")
+echo -e  "Resetting test database..."
 wp db reset --yes --quiet
 
 # Install WordPress.
-echo -e $(status_message "Installing WordPress...")
+echo -e "Installing WordPress..."
 wp core install --title="$SITE_TITLE" --admin_user=wordpress --admin_password=wordpress --admin_email=test@example.com --skip-email --url=$WP_SITE_URL --path=$WP_CORE_DIR --quiet
 
-echo -e $(status_message "Setting up dynamic WP_HOME & SITE_URL...")
+echo -e "Setting up dynamic WP_HOME & SITE_URL..."
 wp config set WP_SITEURL "'http://' . \$_SERVER['HTTP_HOST']" --raw --type=constant --quiet
 wp config set WP_HOME "'http://' . \$_SERVER['HTTP_HOST']" --raw --type=constant --quiet
 
 
-echo -e $(status_message "Configuring site constants...")
+echo -e "Configuring site constants..."
 wp config set WP_DEBUG true --raw --type=constant --quiet
 wp config set WP_DEBUG_LOG true --raw --type=constant --quiet
 wp config set WP_DEBUG_DISPLAY false --raw --type=constant --quiet
