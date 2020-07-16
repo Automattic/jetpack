0e71dc234e77542d274fdcbde8e4a7abe68b85f5
retrofox <rdsuarez@gmail.com>
2020-07-16 16:44:47 -0300

    paid-blocks: hook edit using edit filter

--- extensions/shared/paid-blocks/paid-block-edit.js
+++ extensions/shared/paid-blocks/paid-block-edit.js
@@ -4,15 +4,19 @@
  */
 import { Fragment } from '@wordpress/element';
 import { InspectorControls } from '@wordpress/block-editor';
-
 import { PanelBody } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
 import UpgradePlanBanner from './upgrade-plan-banner';
+import { isUpgradable } from '../plan-utils';
 
 export default OriginalBlockEdit => props => {
+	if ( ! isUpgradable( props?.name ) ) {
+		return <OriginalBlockEdit { ...props } />;
+	}
+
 	return (
 		<Fragment>
 			<InspectorControls>
