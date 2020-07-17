d9343777deab797b413aeaa57229f4683a770e79
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-07-17 19:11:17 +0300

    Fix e2e tests (#16427)

--- tests/e2e/specs/connection.test.js
+++ tests/e2e/specs/connection.test.js
@@ -10,8 +10,9 @@ import JetpackPage from '../lib/pages/wp-admin/jetpack';
 describe( 'Connection', () => {
 	catchBeforeAll( async () => {
 		await execWpCommand( 'wp option delete jetpack_private_options' );
-		await page.reload();
+		await execWpCommand( 'wp config set --raw JETPACK_SHOULD_NOT_USE_CONNECTION_IFRAME false' );
 		// For some reason it need 2 reloads to make constant actually work.
+		await page.reload();
 		await page.reload();
 	} );
 
@@ -20,6 +21,7 @@ describe( 'Connection', () => {
 			'wp option update jetpack_private_options --format=json',
 			'< jetpack_private_options.txt'
 		);
+		await execWpCommand( 'wp config set --raw JETPACK_SHOULD_NOT_USE_CONNECTION_IFRAME true' );
 	} );
 
 	it( 'In-place', async () => {
