7cfcf07d6c0d5fade96db10c3b155b462df47420
retrofox <rdsuarez@gmail.com>
2020-07-20 10:08:05 -0300

    paid-plan: add banner only for top level blocks

--- extensions/shared/paid-blocks/paid-block-edit.js
+++ extensions/shared/paid-blocks/paid-block-edit.js
@@ -2,6 +2,7 @@
  * WordPress dependencies
  */
 import { Fragment } from '@wordpress/element';
+import { useSelect } from '@wordpress/data';
 import { InspectorControls } from '@wordpress/block-editor';
 
 /**
@@ -12,6 +13,14 @@ import { isUpgradable } from '../plan-utils';
 
 export default OriginalBlockEdit => props => {
 	if ( ! isUpgradable( props?.name ) ) {
+		return <OriginalBlockEdit { ...props } />;
+	}
+
+	const isTopLevelBlock = ! useSelect( select => {
+		return select( 'core/block-editor' ).getBlockRootClientId( props.clientId, 'core/block-preview' );
+	} );
+
+	if ( ! isTopLevelBlock ) {
 		return <OriginalBlockEdit { ...props } />;
 	}
 
