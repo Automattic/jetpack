3617ecba01092f32504b1eb62457d86ea50960f6
retrofox <rdsuarez@gmail.com>
2020-07-21 20:39:30 -0300

    paid-blocks: change hook to apply custom class names

--- extensions/shared/with-custom-class-names/index.jsx
+++ extensions/shared/with-custom-class-names/index.jsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { createHigherOrderComponent } from '@wordpress/compose';
+import classNames from 'classnames';
 
 /**
  * Internal dependencies
@@ -9,17 +9,8 @@ import { createHigherOrderComponent } from '@wordpress/compose';
 import './style.scss';
 import { isUpgradable } from '../plan-utils';
 
-// Injecting the `has-warning` class into the block wrapper component gives us
-// the right kind of borders around the block, both visually and conceptually.
-// However, it also adds styling to prevent user interaction with that block.
-// We thus add a new `is-interactive` class to be able to override that behavior.
-export default ( classNames ) =>
-	createHigherOrderComponent(
-		BlockListBlock => props => (
-			<BlockListBlock
-				{ ...props }
-				className={ isUpgradable( props.name ) ? classNames : props.className }
-			/>
-		),
-		'withCustomClassNames'
-	);
+export default ( customClassNames ) => ( className, blockName ) => (
+	classNames( className, {
+		[ customClassNames ]: isUpgradable( blockName ),
+	} )
+);
