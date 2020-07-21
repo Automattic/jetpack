d26778077ae09256ecdf7ae1b194a0029f771a19
retrofox <rdsuarez@gmail.com>
2020-07-21 09:37:46 -0300

    cover: do not nest filters/hooked funtions

--- extensions/shared/blocks/cover/index.js
+++ extensions/shared/blocks/cover/index.js
@@ -13,29 +13,13 @@ import { addFilter } from '@wordpress/hooks';
 import coverEditMediaPlaceholder from './cover-media-placeholder';
 import coverMediaReplaceFlow from './cover-replace-control-button';
 import jetpackCoverBlockEdit from './edit';
-import { isUpgradable } from './utils';
 import './editor.scss';
 
-const addVideoUploadPlanCheck = ( settings, name ) => {
-	if ( ! settings.isDeprecation && isUpgradable( name ) ) {
-		// Take the control of MediaPlaceholder.
-		addFilter(
-			'editor.MediaPlaceholder',
-			'jetpack/cover-edit-media-placeholder',
-			coverEditMediaPlaceholder
-		);
+// Take the control the Replace block button control.
+addFilter( 'editor.MediaReplaceFlow', 'jetpack/cover-media-replace-flow', coverMediaReplaceFlow );
 
-		// Take the control of the Replace block button control.
-		addFilter(
-			'editor.MediaReplaceFlow',
-			'jetpack/cover-media-replace-flow',
-			coverMediaReplaceFlow
-		);
+// Take the control oMediaPlaceholder.
+addFilter( 'editor.MediaPlaceholder', 'jetpack/cover-edit-media-placeholder', coverEditMediaPlaceholder );
 
-		// Extend Core CoverEditBlock.
-		addFilter( 'editor.BlockEdit', 'jetpack/cover-block-edit', jetpackCoverBlockEdit );
-	}
-
-	return settings;
-};
-addFilter( 'blocks.registerBlockType', 'core/cover', addVideoUploadPlanCheck );
+// Extend Core CoverEditBlock.
+addFilter( 'editor.BlockEdit', 'jetpack/cover-block-edit', jetpackCoverBlockEdit );
