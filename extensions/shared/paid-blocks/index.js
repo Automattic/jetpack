65c17ee871b647c66bd665b61332ca672f949f9c
retrofox <rdsuarez@gmail.com>
2020-07-21 17:49:07 -0300

    paid-block: tweak hooking filters

--- extensions/shared/paid-blocks/index.js
+++ extensions/shared/paid-blocks/index.js
@@ -21,12 +21,13 @@ import './editor.scss';
 const jetpackPaidBlock = ( settings, name ) => {
 	// Extend Premium Blocks.
 	if ( isUpgradable( name ) ) {
+		// TODO
 		// Add custom CSS classes.
-		addFilter(
-			'editor.BlockListBlock',
-			`jetpack/videopress-with-has-warning-is-interactive-class-names`,
-			withCustomClassNames( name, 'has-warning is-interactive is-upgradable' )
-		);
+		// addFilter(
+		// 	'editor.BlockListBlock',
+		// 	`jetpack/videopress-with-has-warning-is-interactive-class-names`,
+		// 	withCustomClassNames( name, 'has-warning is-interactive is-upgradable' )
+		// );
 
 		settings.icon = {
 			src: <PremiumIcon icon={ settings.icon } />,
@@ -34,11 +35,12 @@ const jetpackPaidBlock = ( settings, name ) => {
 
 		// Populate block keywords.
 		settings.keywords = uniq( [ ...settings.keywords, 'premium', __( 'premium' ) ] );
+
+		settings.edit = jetpackPaidBlockEdit( settings.edit );
 	}
 
 	return settings;
 };
 
-addFilter( 'blocks.registerBlockType', 'jetpack/paid-block', jetpackPaidBlock );
-
-addFilter( 'editor.BlockEdit', 'jetpack/paid-block', jetpackPaidBlockEdit, 20 );
+addFilter( 'blocks.registerBlockType', 'jetpack/paid-block', jetpackPaidBlock, 1 );
+// addFilter( 'editor.BlockEdit', 'jetpack/paid-block', jetpackPaidBlockEdit, priority );
