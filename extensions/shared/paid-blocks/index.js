a8bf4a4e7fde2696a2eec1b01a795117719e2469
Jerry Jones <jones.jeremydavid@gmail.com>
2020-07-22 14:21:56 -0500

    Added upgrade nudge body class

--- extensions/shared/paid-blocks/index.js
+++ extensions/shared/paid-blocks/index.js
@@ -7,11 +7,13 @@ import { uniq } from 'lodash';
  */
 import { addFilter } from '@wordpress/hooks';
 import { __ } from '@wordpress/i18n';
+import domReady from '@wordpress/dom-ready';
 
 /**
  * Internal dependencies
  */
 import withCustomClassNames from '../with-custom-class-names';
+import isUpgradeNudgeEnabled from '../is-upgrade-nudge-enabled';
 import jetpackPaidBlockEdit from './paid-block-edit';
 import { isUpgradable } from '../plan-utils';
 
@@ -32,3 +34,9 @@ addFilter(
 	`jetpack/set-upgradable-classnames`,
 	withCustomClassNames( 'has-warning is-interactive is-upgradable' )
 );
+
+domReady( function() {
+	if ( isUpgradeNudgeEnabled() ) {
+		document.body.classList.add( 'jetpack-enable-upgrade-nudge' );
+	}
+} );
