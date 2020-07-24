3581dc9a9a2ce7a2fd923cf319bbfb6813a5be5b
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-07-24 23:01:01 +0300

    run wp-env before ngrok

--- tests/e2e/bin/wp-env.sh
+++ tests/e2e/bin/wp-env.sh
@@ -8,13 +8,13 @@ set -e
 
 . "$(dirname "$0")/includes.sh"
 
+yarn wp-env start
+
 check_for_ngrok
 check_for_jq
 start_ngrok
 
 URL=$(get_ngrok_url)
-
-yarn wp-env start
 
 yarn wp-env run tests-cli wp option set siteurl "$URL"
 yarn wp-env run tests-cli wp option set home "$URL"
