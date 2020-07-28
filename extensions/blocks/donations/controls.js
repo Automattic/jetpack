1f5cf895ea595ea45a5c362c4cd5d1300c02b846
mmtr <miguel.torres@automattic.com>
2020-07-28 16:13:46 +0200

    Edit currency in block toolbar

--- extensions/blocks/donations/controls.js
+++ extensions/blocks/donations/controls.js
@@ -1,21 +1,28 @@
 /**
  * WordPress dependencies
  */
-import { InspectorControls } from '@wordpress/block-editor';
+import { BlockControls, InspectorControls } from '@wordpress/block-editor';
 import {
+	Button,
+	Dashicon,
+	Dropdown,
 	ExternalLink,
-	TextControl,
+	MenuGroup,
+	MenuItem,
 	PanelBody,
-	SelectControl,
+	TextControl,
 	ToggleControl,
+	ToolbarGroup,
 } from '@wordpress/components';
 import { useEffect, useState } from '@wordpress/element';
 import { __, sprintf } from '@wordpress/i18n';
+import { DOWN } from '@wordpress/keycodes';
 
 /**
  * Internal dependencies
  */
 import { SUPPORTED_CURRENCIES, minimumTransactionAmountForCurrency } from '../../shared/currencies';
+import { CURRENCIES } from '@automattic/format-currency';
 
 const Controls = props => {
 	const { attributes, setAttributes, products, siteSlug } = props;
@@ -47,57 +54,96 @@ const Controls = props => {
 	}, [ amounts ] );
 
 	return (
-		<InspectorControls>
-			<PanelBody title={ __( 'Settings', 'jetpack' ) }>
-				<SelectControl
-					label={ __( 'Currency', 'jetpack' ) }
-					value={ currency }
-					options={ Object.keys( SUPPORTED_CURRENCIES ).map( ccy => ( {
-						label: ccy,
-						value: ccy,
-					} ) ) }
-					onChange={ ccy => setAttributes( { currency: ccy } ) }
-				/>
-				<div className="donations__controls-amounts">
-					{ editedAmounts.map( ( amount, index ) => (
-						<TextControl
-							type="number"
-							min={ minAmount }
-							step="1"
-							value={ amount }
-							label={ sprintf(
-								// translators: %d: Tier level e.g: "1", "2", "3"
-								__( 'Tier %d', 'jetpack' ),
-								index + 1
-							) }
-							onChange={ value => setAmount( value, index ) }
-						/>
-					) ) }
-				</div>
-				<ToggleControl
-					checked={ !! monthlyPlanId }
-					onChange={ value =>
-						setAttributes( { monthlyPlanId: value ? products[ '1 month' ] : null } )
-					}
-					label={ __( 'Show monthly donations', 'jetpack' ) }
-				/>
-				<ToggleControl
-					checked={ !! annuallyPlanId }
-					onChange={ value =>
-						setAttributes( { annuallyPlanId: value ? products[ '1 year' ] : null } )
-					}
-					label={ __( 'Show annual donations', 'jetpack' ) }
-				/>
-				<ToggleControl
-					checked={ showCustomAmount }
-					onChange={ value => setAttributes( { showCustomAmount: value } ) }
-					label={ __( 'Show custom amount option', 'jetpack' ) }
-				/>
-				<ExternalLink href={ `https://wordpress.com/earn/payments/${ siteSlug }` }>
-					{ __( 'View donation earnings', 'jetpack' ) }
-				</ExternalLink>
-			</PanelBody>
-		</InspectorControls>
+		<>
+			<BlockControls>
+				<ToolbarGroup>
+					<Dropdown
+						contentClassName="jetpack-donations__currency-popover"
+						renderToggle={ ( { onToggle, isOpen } ) => {
+							const openOnArrowDown = event => {
+								if ( ! isOpen && event.keyCode === DOWN ) {
+									event.preventDefault();
+									event.stopPropagation();
+									onToggle();
+								}
+							};
+
+							return (
+								<Button
+									className="jetpack-donations__currency-toggle"
+									icon={
+										<>
+											{ CURRENCIES[ currency ].symbol + ' - ' + currency }
+											<Dashicon icon="arrow-down" />
+										</>
+									}
+									label={ __( 'Change currency', 'jetpack' ) }
+									onClick={ onToggle }
+									onKeyDown={ openOnArrowDown }
+								/>
+							);
+						} }
+						renderContent={ ( { onClose } ) => (
+							<MenuGroup>
+								{ Object.keys( SUPPORTED_CURRENCIES ).map( ccy => (
+									<MenuItem
+										isSelected={ ccy === currency }
+										onClick={ () => {
+											setAttributes( { currency: ccy } );
+											onClose();
+										} }
+									>
+										{ CURRENCIES[ ccy ].symbol + ' - ' + ccy }
+									</MenuItem>
+								) ) }
+							</MenuGroup>
+						) }
+					/>
+				</ToolbarGroup>
+			</BlockControls>
+			<InspectorControls>
+				<PanelBody title={ __( 'Settings', 'jetpack' ) }>
+					<div className="donations__controls-amounts">
+						{ editedAmounts.map( ( amount, index ) => (
+							<TextControl
+								type="number"
+								min={ minAmount }
+								step="1"
+								value={ amount }
+								label={ sprintf(
+									// translators: %d: Tier level e.g: "1", "2", "3"
+									__( 'Tier %d', 'jetpack' ),
+									index + 1
+								) }
+								onChange={ value => setAmount( value, index ) }
+							/>
+						) ) }
+					</div>
+					<ToggleControl
+						checked={ !! monthlyPlanId }
+						onChange={ value =>
+							setAttributes( { monthlyPlanId: value ? products[ '1 month' ] : null } )
+						}
+						label={ __( 'Show monthly donations', 'jetpack' ) }
+					/>
+					<ToggleControl
+						checked={ !! annuallyPlanId }
+						onChange={ value =>
+							setAttributes( { annuallyPlanId: value ? products[ '1 year' ] : null } )
+						}
+						label={ __( 'Show annual donations', 'jetpack' ) }
+					/>
+					<ToggleControl
+						checked={ showCustomAmount }
+						onChange={ value => setAttributes( { showCustomAmount: value } ) }
+						label={ __( 'Show custom amount option', 'jetpack' ) }
+					/>
+					<ExternalLink href={ `https://wordpress.com/earn/payments/${ siteSlug }` }>
+						{ __( 'View donation earnings', 'jetpack' ) }
+					</ExternalLink>
+				</PanelBody>
+			</InspectorControls>
+		</>
 	);
 };
 
