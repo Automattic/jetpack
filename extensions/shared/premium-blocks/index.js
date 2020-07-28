a1ce87a871db55345de7f9c4c911157e312ae61e
retrofox <rdsuarez@gmail.com>
2020-07-27 21:55:46 -0300

    premium-block: extend block edit for premium

--- extensions/shared/premium-blocks/index.js
+++ extensions/shared/premium-blocks/index.js
@@ -1,12 +1,32 @@
 /**
+ * External dependencies
+ */
+import { uniq } from 'lodash';
+
+/**
  * WordPress dependencies
  */
 import domReady from '@wordpress/dom-ready';
+import { addFilter } from '@wordpress/hooks';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
-import { isUpgradeNudgeEnabled } from '../plan-utils';
+import { isUpgradeNudgeEnabled, isUpgradable } from '../plan-utils';
+import premiumBlockEdit from './edit';
+
+import './editor.scss';
+
+const jetpackPaidBlock = ( settings, name ) => {
+	if ( isUpgradable( name ) ) {
+		settings.edit = premiumBlockEdit( settings.edit );
+	}
+
+	return settings;
+};
+
+addFilter( 'blocks.registerBlockType', 'jetpack/paid-block', jetpackPaidBlock );
 
 import './editor.scss';
 
