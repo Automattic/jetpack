787efe4e8892ada14e99d4ac6a2f506c4d758765
Jeremy Herve <jeremy@jeremy.hu>
2020-07-28 09:12:37 +0200

    Form Block: add new variation allowing site email collection

--- extensions/blocks/contact-form/variations.js
+++ extensions/blocks/contact-form/variations.js
@@ -34,6 +34,27 @@ const variations = [
 		],
 	},
 	{
+		name: 'collect-emails',
+		title: __( 'Signup Form', 'jetpack' ),
+		description: __( 'Collect email addresses on your site', 'jetpack' ),
+		icon: renderMaterialIcon(
+			<Path d="M21.99 8c0-.72-.37-1.35-.94-1.7l-8.04-4.71c-.62-.37-1.4-.37-2.02 0L2.95 6.3C2.38 6.65 2 7.28 2 8v10c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2l-.01-10zm-11.05 4.34l-7.2-4.5 7.25-4.25c.62-.37 1.4-.37 2.02 0l7.25 4.25-7.2 4.5c-.65.4-1.47.4-2.12 0z" />,
+			48,
+			48,
+			'-4 -4 32 32'
+		),
+		innerBlocks: [
+			[ 'jetpack/field-email', { required: true } ],
+			[
+				'jetpack/button',
+				{
+					text: __( 'Subscribe', 'jetpack' ),
+					element: 'button',
+				},
+			],
+		],
+	},
+	{
 		name: 'rsvp-form',
 		title: __( 'RSVP Form', 'jetpack' ),
 		description: __( 'Add an RSVP form to your page', 'jetpack' ),
