d2537cda884c10bdd4a0170b500d64b6143311d3
Damián Suárez <rdsuarez@gmail.com>
2020-07-29 07:31:45 -0300

    Premium Blocks: Add upgrade banner to block inspector (#16603)
    * refact -> create getUpgradeUrl() util helper
    
    * premium-block: add upgrade banner component
    
    * regiter-jetpack-blocks: export requiresPaidPlan fn
    
    * plan-utils: add isUpgradable() helper fn
    
    * premium-blocks: add block edit component
    
    * premium-block: extend block edit for premium
    
    * premium-blocks: fix rebasing issues
    
    * upgrade-banner: replace HOC by hooks
    
    * plan-utils: minor jsdoc improvements
    
    * upgrade-banner: render button with a valid URL

--- extensions/shared/plan-utils.js
+++ extensions/shared/plan-utils.js
@@ -1,8 +1,21 @@
 /**
  * External dependencies
  */
-import { get } from 'lodash';
+import { compact, get, startsWith } from 'lodash';
+
+/**
+ * WordPress dependencies
+ */
+import { addQueryArgs } from '@wordpress/url';
+
+/**
+ * Internal dependencies
+ */
 import getJetpackData from './get-jetpack-data';
+import { isSimpleSite } from './site-type-utils';
+import getSiteFragment from './get-site-fragment';
+import getJetpackExtensionAvailability from './get-jetpack-extension-availability';
+import { requiresPaidPlan } from './register-jetpack-block';
 
 /**
  * Return whether upgrade nudges are enabled or not.
@@ -11,4 +24,78 @@ import getJetpackData from './get-jetpack-data';
  */
 export function isUpgradeNudgeEnabled() {
 	return get( getJetpackData(), 'jetpack.enable_upgrade_nudge', false );
+}
+
+/**
+ * Return the checkout URL to upgrade the site plan,
+ * depending on the plan, postId, and postType site values.
+ *
+ * @param {object} siteParams -          Site params used to build the URL.
+ * @param {string} siteParams.planSlug - Plan slug.
+ * @param {string} siteParams.plan -     An object with details about the plan.
+ * @param {number} siteParams.postId -   Post id.
+ * @param {string} siteParams.postType - Post type.
+ * @returns {string}                     Upgrade URL.
+ */
+export function getUpgradeUrl( { planSlug, plan, postId, postType } ) {
+	// WP.com plan objects have a dedicated `path_slug` field, Jetpack plan objects don't
+	// For Jetpack, we thus use the plan slug with the 'jetpack_' prefix removed.
+	const planPathSlug = startsWith( planSlug, 'jetpack_' )
+		? planSlug.substr( 'jetpack_'.length )
+		: get( plan, [ 'path_slug' ] );
+
+	// The editor for CPTs has an `edit/` route fragment prefixed
+	const postTypeEditorRoutePrefix = [ 'page', 'post' ].includes( postType ) ? '' : 'edit';
+
+	// Post-checkout: redirect back here
+	const redirect_to = isSimpleSite()
+		? addQueryArgs(
+			'/' +
+			compact( [ postTypeEditorRoutePrefix, postType, getSiteFragment(), postId ] ).join(
+				'/'
+			),
+			{
+				plan_upgraded: 1,
+			}
+		)
+		: addQueryArgs(
+			window.location.protocol +
+			`//${ getSiteFragment().replace( '::', '/' ) }/wp-admin/post.php`,
+			{
+				action: 'edit',
+				post: postId,
+				plan_upgraded: 1,
+			}
+		);
+
+	return planPathSlug &&
+		addQueryArgs( `https://wordpress.com/checkout/${ getSiteFragment() }/${ planPathSlug }`, {
+			redirect_to,
+		} );
+}
+
+/**
+ * Check if the block is upgradable, based on whether
+ * the block requires a paid plan.
+ *
+ * @param {string} name - Block name.
+ * @returns {boolean} True if it should show the nudge. Otherwise, False.
+ */
+export function isUpgradable( name ) {
+	if ( ! name ) {
+		return false;
+	}
+
+	// core/cover is handled in ./extensions/shared/blocks/cover.
+	if ( name === 'core/cover' ) {
+		return false;
+	}
+
+	let blockName = /^jetpack\//.test( name ) ? name.substr( 8, name.length ) : name;
+
+	// hardcode core/video block.
+	blockName = blockName === 'core/video' ? 'video' : blockName;
+
+	const { details, unavailableReason } = getJetpackExtensionAvailability( blockName );
+	return isSimpleSite() && requiresPaidPlan( unavailableReason, details );
 }
--- extensions/shared/premium-blocks/index.js
+++ extensions/shared/premium-blocks/index.js
@@ -1,14 +1,30 @@
 /**
+ * External dependencies
+ */
+
+/**
  * WordPress dependencies
  */
 import domReady from '@wordpress/dom-ready';
+import { addFilter } from '@wordpress/hooks';
 
 /**
  * Internal dependencies
  */
-import { isUpgradeNudgeEnabled } from '../plan-utils';
+import { isUpgradeNudgeEnabled, isUpgradable } from '../plan-utils';
+import premiumBlockEdit from './edit';
 
 import './editor.scss';
+
+const jetpackPaidBlock = ( settings, name ) => {
+	if ( isUpgradable( name ) ) {
+		settings.edit = premiumBlockEdit( settings.edit );
+	}
+
+	return settings;
+};
+
+addFilter( 'blocks.registerBlockType', 'jetpack/paid-block', jetpackPaidBlock );
 
 /*
  * Add the `jetpack-enable-upgrade-nudge` css Class
