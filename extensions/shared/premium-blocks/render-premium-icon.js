27903ddc43ab10e83e43e8dcccfd746e6597137d
cpap <papazoglou.charalampos@gmail.com>
2020-08-03 23:35:19 +0300

    Paid Blocks: fixes typo

--- extensions/shared/premium-blocks/render-premium-icon.js
+++ extensions/shared/premium-blocks/render-premium-icon.js
@@ -5,7 +5,7 @@ import PaidSymbol from './paid-symbol';
 
 const renderPremiumIcon = icon => {
 	if ( icon.src ) {
-		icon.src.props.children = [ icon.src.props.children, PaidSymbol ];
+		icon.src.props.children = [ icon.src.props.children, <PaidSymbol /> ];
 	} else if ( icon?.props?.children ) {
 		icon.props.children = [ icon.props.children, <PaidSymbol /> ];
 	}
