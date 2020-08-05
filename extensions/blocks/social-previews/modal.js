b3d51a0e9602584a166001aab2ca422644c49852
Marek Hrabe <marekhrabe@me.com>
2020-08-05 15:54:04 +0200

    connect modal to post data

--- extensions/blocks/social-previews/modal.js
+++ extensions/blocks/social-previews/modal.js
@@ -9,35 +9,104 @@
  */
 import { __ } from '@wordpress/i18n';
 import { Button, Modal, TabPanel } from '@wordpress/components';
+import { withSelect } from '@wordpress/data';
+import { applyFilters } from '@wordpress/hooks';
+import { has } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import { AVAILABLE_SERVICES } from './constants';
-import { SocialServiceIcon } from '../../shared/icons';
 
-const SocialPreviewsModal = function SocialPreviewsModal( { onClose } ) {
+const SocialPreviewsModal = function SocialPreviewsModal( props ) {
+	const {
+		onClose,
+		showUpgradeNudge,
+		media,
+		currentPostId,
+		title,
+		description,
+		url,
+		author,
+	} = props;
+
+	let mediaSourceUrl;
+	if ( media ) {
+		const mediaSize = applyFilters(
+			'editor.PostFeaturedImage.imageSize',
+			'post-thumbnail',
+			media.id,
+			currentPostId
+		);
+		if ( has( media, [ 'media_details', 'sizes', mediaSize ] ) ) {
+			// use mediaSize when available
+			mediaSourceUrl = media.media_details.sizes[ mediaSize ].source_url;
+		} else {
+			// get fallbackMediaSize if mediaSize is not available
+			const fallbackMediaSize = applyFilters(
+				'editor.PostFeaturedImage.imageSize',
+				'thumbnail',
+				media.id,
+				currentPostId
+			);
+			if ( has( media, [ 'media_details', 'sizes', fallbackMediaSize ] ) ) {
+				// use fallbackMediaSize when mediaSize is not available
+				mediaSourceUrl = media.media_details.sizes[ fallbackMediaSize ].source_url;
+			} else {
+				// use full image size when mediaFallbackSize and mediaSize are not available
+				mediaSourceUrl = media.source_url;
+			}
+		}
+	}
+
 	return (
 		<Modal
 			onRequestClose={ onClose }
 			title={ __( 'Social Previews', 'jetpack' ) }
 			className="jetpack-social-previews__modal"
 		>
-			<TabPanel className="jetpack-social-previews__tabs" tabs={ AVAILABLE_SERVICES }>
-				{ tab => (
-					<div>
-						Selected preview: { tab.title }
-						<tab.preview
-							title="Five for the Future"
-							description="Launched in 2014, Five for the Future encourages organizations to contribute five percent of their resources to WordPress development. WordPress co-founder Matt Mullenweg proposed this benchmark to maintain a “golden ratio” of contributors to users."
-							url="https://wordpress.org/five-for-the-future/"
-							author="Matt Mullenweg"
-						/>
-					</div>
-				) }
-			</TabPanel>
+			{ showUpgradeNudge ? (
+				<p>Upgrade Needed</p>
+			) : (
+				<TabPanel className="jetpack-social-previews__tabs" tabs={ AVAILABLE_SERVICES }>
+					{ tab => (
+						<div>
+							<tab.preview
+								title={ title }
+								description={ description }
+								url={ url }
+								author={ author }
+								image={ mediaSourceUrl }
+							/>
+						</div>
+					) }
+				</TabPanel>
+			) }
 		</Modal>
 	);
 };
 
-export default SocialPreviewsModal;
+export default withSelect( ( select, props ) => {
+	// No need to load anything when the feature is not active.
+	if ( props.showUpgradeNudge ) {
+		return {};
+	}
+
+	const { getMedia, getPostType, getUser } = select( 'core' );
+	const { getCurrentPost, getCurrentPostId, getEditedPostAttribute } = select( 'core/editor' );
+
+	const featuredImageId = getEditedPostAttribute( 'featured_media' );
+	const authorId = getEditedPostAttribute( 'author' );
+	const user = authorId && getUser( authorId );
+
+	return {
+		post: getCurrentPost(),
+		media: featuredImageId ? getMedia( featuredImageId ) : null,
+		currentPostId: getCurrentPostId(),
+		postType: getPostType( getEditedPostAttribute( 'type' ) ),
+		title: getEditedPostAttribute( 'title' ),
+		description: getEditedPostAttribute( 'excerpt' ) || getEditedPostAttribute( 'content' ),
+		url: getEditedPostAttribute( 'link' ),
+		author: user && user.name,
+	};
+} )( SocialPreviewsModal );
