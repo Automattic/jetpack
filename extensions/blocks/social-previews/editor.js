5b5823bdf9d1210ef17450f4170ff27ebc8e69ba
Dave Smith <getdavemail@gmail.com>
2020-08-05 13:10:24 +0100

    Add documentation to code.

--- extensions/blocks/social-previews/editor.js
+++ extensions/blocks/social-previews/editor.js
@@ -4,21 +4,28 @@
 import { name, settings, SocialPreviews } from '.';
 import registerJetpackPlugin from '../../shared/register-jetpack-plugin';
 import { registerPlugin } from '@wordpress/plugins';
+import getJetpackExtensionAvailability from '../../shared/get-jetpack-extension-availability';
 
+// Register the main "social-previews" extension if the feature is available
+// on the current plan.
 registerJetpackPlugin( name, settings );
 
-// const blockAvailableOnPlan = Jetpack_Editor_Initial_State.available_blocks[ 'social-previews' ].available;
-const blockAvailableOnPlan = false;
+// TODO: remove in favour of `getJetpackExtensionAvailability()` call.
+// Testing purposes only to force reveal of upgrade nudge UI.
+const extensionAvailableOnPlan = false;
+// const extensionAvailableOnPlan = getJetpackExtensionAvailability( 'social-previews' );
 
-registerPlugin( `jetpack-${name}-upgrade-nudge`, {
-    render: () => {
-        if ( blockAvailableOnPlan ) {
-            return null;
+// If the social previews extension is **not** available on this plan (WP.com only)
+// then manually register a near identical Plugin which shows the upgrade nudge.
+// Note this is necessary because the official `registerJetpackPlugin` checks the
+// extension availability so will not render the Plugin if the extension is not
+// availabile.
+if ( !extensionAvailableOnPlan ) {
+    registerPlugin( `jetpack-${name}-upgrade-nudge`, {
+        render: () => {
+            return (
+                <SocialPreviews showUpgradeNudge={ true } />
+            );
         }
-
-        return (
-            <SocialPreviews showUpgradeNudge={ true } />
-        );
-    }
-
-} );
+    } );
+}
