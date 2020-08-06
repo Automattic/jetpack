c4f7477521532816fce8be725150c00b6e90d8e3
Marek Hrabe <marekhrabe@me.com>
2020-08-06 10:57:49 +0200

    [not verified] extracted the upgrade component to avoid future conflicts

---
+++ extensions/blocks/social-previews/upgrade.js
@@ -0,0 +1,7 @@
+const SocialPreviewsUpgrade = function SocialPreviewsUpgrade() {
+	return (
+		<div className="jetpack-social-previews__modal-upgrade">{ /* TODO: Add upgrade nudge. */ }</div>
+	);
+};
+
+export default SocialPreviewsUpgrade;
