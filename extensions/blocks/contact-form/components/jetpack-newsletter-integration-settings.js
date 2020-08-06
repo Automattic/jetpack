1ebef754d16ea2fc4bf54af46efbdfa512bff63a
Reint-Jan Hoiting <reint-jan.hoiting@endurance.com>
2020-08-06 10:12:51 +0200

    remove the conditional text add translatable strings add isSimpleSite check

--- extensions/blocks/contact-form/components/jetpack-newsletter-integration-settings.js
+++ extensions/blocks/contact-form/components/jetpack-newsletter-integration-settings.js
@@ -1,10 +1,9 @@
 /**
  * External dependencies
  */
-import { BaseControl, Button, ExternalLink } from '@wordpress/components';
-import { __, sprintf } from '@wordpress/i18n';
-import apiFetch from '@wordpress/api-fetch';
-import { useCallback, useEffect, useState, useMemo } from '@wordpress/element';
+import { BaseControl, Button, ExternalLink, PanelBody } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { useCallback, useMemo } from '@wordpress/element';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { createBlock } from '@wordpress/blocks';
 
@@ -30,35 +29,14 @@ const useInsertConsentBlock = () => {
 const NoConsentBlockSettings = () => {
 	const { insertConsentBlock } = useInsertConsentBlock();
 
-	const [ numberOfContacts, setNumberOfContacts ] = useState( 0 );
-
-	useEffect( () => {
-		apiFetch( { path: '/wp/v2/feedback?per_page=1', parse: false } )
-			.then( request => request.headers.get( 'X-WP-Total' ) )
-			.then( Number.parseInt )
-			.then( setNumberOfContacts );
-	}, [] );
-
 	return (
 		<>
-			{ numberOfContacts >= 10 ? (
-				<p>
-					{ sprintf(
-						__(
-							'You’ve already collected %d email contacts. Why not make sure you have permission to email them too?',
-							'jetpack'
-						),
-						numberOfContacts
-					) }
-				</p>
-			) : (
-				<p>
-					{ __(
-						'You’re already collecting email contacts. Why not make sure you have permission to email them too?',
-						'jetpack'
-					) }
-				</p>
-			) }
+			<p>
+				{ __(
+					'You’re already collecting email contacts. Why not make sure you have permission to email them too?',
+					'jetpack'
+				) }
+			</p>
 			<Button isSecondary onClick={ insertConsentBlock } style={ { marginBottom: '1em' } }>
 				{ __( 'Add email permission request', 'jetpack' ) }
 			</Button>
@@ -75,20 +53,22 @@ const NewsletterIntegrationSettings = () => {
 	);
 
 	return (
-		<BaseControl>
-			{ ! hasConsentBlock && <NoConsentBlockSettings /> }
-			<p>
-				<em>
-					{ __(
-						'To start sending email campaigns, install the Creative Mail plugin for WordPress. ',
-						'jetpack'
-					) }
-					<ExternalLink href="https://wordpress.org/plugins/creative-mail-by-constant-contact/">
-						Get the plugin now
-					</ExternalLink>
-				</em>
-			</p>
-		</BaseControl>
+		<PanelBody title={ __( 'Newsletter Integration', 'jetpack' ) } initialOpen={ false }>
+			<BaseControl>
+				{ ! hasConsentBlock && <NoConsentBlockSettings /> }
+				<p>
+					<em>
+						{ __(
+							'To start sending email campaigns, install the Creative Mail plugin for WordPress. ',
+							'jetpack'
+						) }
+						<ExternalLink href="https://wordpress.org/plugins/creative-mail-by-constant-contact/">
+							{ __( 'Get the plugin now', 'jetpack' ) }
+						</ExternalLink>
+					</em>
+				</p>
+			</BaseControl>
+		</PanelBody>
 	);
 };
 
