d573265562643eced373ea2e54a06a54b9fa1686
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-08-07 18:41:33 +0300

    Add retry to .org login method

--- tests/e2e/lib/pages/wp-admin/login.js
+++ tests/e2e/lib/pages/wp-admin/login.js
@@ -7,8 +7,9 @@ import { createURL } from '@wordpress/e2e-test-utils';
  * Internal dependencies
  */
 import Page from '../page';
-import { waitAndClick, waitAndType } from '../../page-helper';
+import { waitAndClick, waitAndType, waitForSelector } from '../../page-helper';
 import { WP_USERNAME, WP_PASSWORD } from '../../setup';
+import logger from '../../logger';
 
 export default class WPLoginPage extends Page {
 	constructor( page ) {
@@ -17,7 +18,7 @@ export default class WPLoginPage extends Page {
 		super( page, { expectedSelector, url } );
 	}
 
-	async login( username = WP_USERNAME, password = WP_PASSWORD ) {
+	async login( username = WP_USERNAME, password = WP_PASSWORD, { retry = true } = {} ) {
 		const ssoLoginButton = '.jetpack-sso.button';
 		if ( ( await this.page.$( ssoLoginButton ) ) !== null ) {
 			await this.toggleSSOLogin();
@@ -26,8 +27,22 @@ export default class WPLoginPage extends Page {
 		await waitAndType( this.page, '#user_login', username );
 		await waitAndType( this.page, '#user_pass', password );
 
-		// await Promise.all( [ this.page.waitForNavigation(), this.page.click( '#wp-submit' ) ] );
-		return await this.page.click( '#wp-submit' );
+		await this.page.click( '#wp-submit' );
+
+		if ( await waitForSelector( this.page, this.expectedSelector, { hidden: true } ) ) {
+		}
+
+		try {
+			await waitForSelector( this.page, this.expectedSelector, {
+				hidden: true,
+			} );
+		} catch ( e ) {
+			if ( retry === true ) {
+				logger.info( `The WPORG login didn't work as expected - retrying now: '${ e }'` );
+				return await this.login( username, password, { retry: false } );
+			}
+			throw e;
+		}
 	}
 
 	async loginSSO() {
