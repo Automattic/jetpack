8f5676046219dc00c5bfb30dd62a1ce9375af886
Konstantin Obenland <obenland@gmx.de>
2020-08-10 15:16:55 -0700

    [not verified] Social Previews: Add upgrade path
    Takes inspiration from `UpgradeNudge` and `BlockNudge` to save the post and redirect to the checkout flow of the Business plan.

--- extensions/blocks/social-previews/upgrade.js
+++ extensions/blocks/social-previews/upgrade.js
@@ -1,15 +1,28 @@
 /**
  * External dependencies
  */
+import { compact, get } from 'lodash';
+import { Button } from '@wordpress/components';
+import { compose } from '@wordpress/compose';
+import { withDispatch, withSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
+import { addQueryArgs } from '@wordpress/url';
 
 /**
  * Internal dependencies
  */
-import { Button } from '@wordpress/components';
+import analytics from '../../../_inc/client/lib/analytics';
+import { isSimpleSite } from '../../shared/site-type-utils';
+import getSiteFragment from '../../shared/get-site-fragment';
 import upgradeImageUrl from './upgrade-illustration.svg';
 
-const SocialPreviewsUpgrade = function SocialPreviewsUpgrade() {
+const SocialPreviewsUpgrade = function SocialPreviewsUpgrade( {
+	autosaveAndRedirect,
+	href,
+	trackViewEvent,
+} ) {
+	trackViewEvent();
+
 	return (
 		<div className="jetpack-social-previews__modal-upgrade">
 			<img
@@ -43,10 +56,82 @@ const SocialPreviewsUpgrade = function SocialPreviewsUpgrade() {
 						) }
 					</li>
 				</ul>
-				<Button isPrimary>{ __( 'Upgrade', 'jetpack' ) }</Button>
+
+				<Button
+					href={ href } // Only for server-side rendering, since onClick doesn't work there.
+					isPrimary
+					label={ __( 'Purchase a business plan to access social previews', 'jetpack' ) }
+					onClick={ autosaveAndRedirect }
+					target="_top"
+				>
+					{ __( 'Upgrade', 'jetpack' ) }
+				</Button>
 			</div>
 		</div>
 	);
 };
 
-export default SocialPreviewsUpgrade;
+export default compose( [
+	withSelect( select => {
+		const plan = select( 'wordpress-com/plans' ).getPlan( 'business-bundle' );
+		const planPathSlug = get( plan, [ 'path_slug' ] );
+
+		const postId = select( 'core/editor' ).getCurrentPostId();
+		const postType = select( 'core/editor' ).getCurrentPostType();
+
+		// The editor for CPTs has an `edit/` route fragment prefixed.
+		const postTypeEditorRoutePrefix = [ 'page', 'post' ].includes( postType ) ? '' : 'edit';
+
+		// Post-checkout: redirect back here.
+		const redirect_to = isSimpleSite()
+			? addQueryArgs(
+					'/' +
+						compact( [ postTypeEditorRoutePrefix, postType, getSiteFragment(), postId ] ).join(
+							'/'
+						),
+					{
+						plan_upgraded: 1,
+					}
+			  )
+			: addQueryArgs(
+					window.location.protocol +
+						`//${ getSiteFragment().replace( '::', '/' ) }/wp-admin/post.php`,
+					{
+						action: 'edit',
+						post: postId,
+						plan_upgraded: 1,
+					}
+			  );
+
+		const href =
+			planPathSlug &&
+			addQueryArgs( `https://wordpress.com/checkout/${ getSiteFragment() }/${ planPathSlug }`, {
+				redirect_to,
+			} );
+
+		return {
+			href,
+			trackViewEvent: () =>
+				void analytics.tracks.recordEvent( 'jetpack_editor_block_upgrade_nudge_impression', {
+					plan: planPathSlug,
+					block: 'social-previews',
+				} ),
+			trackClickEvent: () =>
+				void analytics.tracks.recordEvent( 'jetpack_editor_block_upgrade_click', {
+					plan: planPathSlug,
+					block: 'social-previews',
+				} ),
+		};
+	} ),
+	withDispatch( ( dispatch, { href, trackClickEvent } ) => ( {
+		autosaveAndRedirect: async event => {
+			event.preventDefault(); // Don't follow the href before auto-saving.
+
+			trackClickEvent();
+			await dispatch( 'core/editor' ).autosave();
+
+			// Using window.top to escape from the editor iframe on WordPress.com.
+			window.top.location.href = href;
+		},
+	} ) ),
+] )( SocialPreviewsUpgrade );
