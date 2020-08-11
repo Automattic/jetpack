87901abb37db0ecab535902f46057983a8889113
Dave Smith <getdavemail@gmail.com>
2020-08-11 11:16:08 +0100

    Apply to Social Previews

--- extensions/blocks/social-previews/social-previews.php
+++ extensions/blocks/social-previews/social-previews.php
@@ -12,17 +12,17 @@ namespace Automattic\Jetpack\Extensions\Social_Previews;
 use Jetpack_Gutenberg;
 
 const FEATURE_NAME = 'social-previews';
-const BLOCK_NAME   = 'jetpack/' . FEATURE_NAME;
+const EXTENSION_NAME   = 'jetpack/' . FEATURE_NAME;
 
 /**
  * Registers the Social Previews feature with the block editor.
  */
-function register_block() {
-	jetpack_register_block(
-		BLOCK_NAME,
+function register_extension() {
+	jetpack_register_extension(
+		EXTENSION_NAME,
 		array(
 			'plan_check' => true,
 		)
 	);
 }
-add_action( 'init', __NAMESPACE__ . '\register_block' );
+add_action( 'init', __NAMESPACE__ . '\register_extension' );
