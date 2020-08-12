d1404cb5ba9001569d6d37fac1e713f158ee9617
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-08-12 18:27:20 +0300

    fix js coverage collection

--- tests/process-coverage.sh
+++ tests/process-coverage.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ./cc-test-reporter format-coverage -t clover -o coverage/codeclimate.backend.json coverage/backend-clover.xml # Format backend coverage
-./cc-test-reporter format-coverage -t lcov -o coverage/codeclimate.frontend.json coverage/lcov.info  # Format frontend coverage
+./cc-test-reporter format-coverage -t lcov -o coverage/codeclimate.adminpage.json coverage/adminpage/lcov.info  # Format frontend coverage
+./cc-test-reporter format-coverage -t lcov -o coverage/codeclimate.extensions.json coverage/extensions/lcov.info  # Format frontend coverage
 
 
 ls -la coverage/packages
 ls -la coverage
 ls -la .
-cat coverage/clover.xml
 export PACKAGES='./coverage/packages/*-clover.xml'
 for PACKAGE in $PACKAGES
 do
