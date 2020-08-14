98d6422c8504d1e22e0f1a52c816ea7f5da36336
Miguel Torres <miguel.torres@automattic.com>
2020-08-14 14:35:46 +0200

    Donations: Process payment after choosing an amount (#16748)
    Connect the donations block with the existing memberships functionality, so once a visitor chooses a donation amount and click on the donate button we display the subscribe dialog to process the payment.

--- extensions/blocks/donations/donations.php
+++ extensions/blocks/donations/donations.php
@@ -23,7 +23,7 @@ function register_block() {
 	jetpack_register_block(
 		BLOCK_NAME,
 		array(
-			'render_callback' => __NAMESPACE__ . '\load_assets',
+			'render_callback' => __NAMESPACE__ . '\render_block',
 			'plan_check'      => true,
 		)
 	);
@@ -31,14 +31,37 @@ function register_block() {
 add_action( 'init', __NAMESPACE__ . '\register_block' );
 
 /**
- * Donations block registration/dependency declaration.
+ * Donations block dynamic rendering.
  *
  * @param array  $attr    Array containing the Donations block attributes.
  * @param string $content String containing the Donations block content.
  *
  * @return string
  */
-function load_assets( $attr, $content ) {
+function render_block( $attr, $content ) {
 	Jetpack_Gutenberg::load_assets_as_required( FEATURE_NAME );
+
+	require_once JETPACK__PLUGIN_DIR . '/modules/memberships/class-jetpack-memberships.php';
+	add_thickbox();
+
+	$donations = array(
+		'one-time' => $attr['oneTimeDonation'],
+		'monthly'  => $attr['monthlyDonation'],
+		'annual'   => $attr['annualDonation'],
+	);
+	foreach ( $donations as $interval => $donation ) {
+		if ( ! $donation['show'] ) {
+			continue;
+		}
+		$plan_id = intval( $donation['planId'] );
+		$plan    = get_post( $plan_id );
+		if ( ! $plan || is_wp_error( $plan ) ) {
+			continue;
+		}
+
+		$url     = \Jetpack_Memberships::get_instance()->get_subscription_url( $plan_id );
+		$content = preg_replace( '/(donations__donate-button donations__' . $interval . '-item")/i', '$1 href="' . esc_url( $url ) . '"', $content );
+	}
+
 	return $content;
 }
