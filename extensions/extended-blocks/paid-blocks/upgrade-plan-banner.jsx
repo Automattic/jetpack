b3eaf6c79f75da24c14fd4df3528b34770fafac0
retrofox <rdsuarez@gmail.com>
2020-08-14 07:34:08 -0300

    upgrade-banner: use useUpgradeFlow() hook

--- extensions/extended-blocks/paid-blocks/upgrade-plan-banner.jsx
+++ extensions/extended-blocks/paid-blocks/upgrade-plan-banner.jsx
@@ -6,24 +6,13 @@ import classNames from 'classnames';
 /**
  * WordPress dependencies
  */
-import { useSelect, dispatch } from '@wordpress/data';
 import { Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
-import { getUpgradeUrl } from '../../shared/plan-utils';
-
-// Provably we should move this store to somewhere more generic.
-import '../../shared/components/upgrade-nudge/store';
-
-function redirect( url, callback ) {
-	if ( callback ) {
-		callback( url );
-	}
-	window.top.location.href = url;
-}
+import useUpgradeFlow from '../../shared/use-upgrade-flow/index';
 
 const UpgradePlanBanner = ( {
 	onRedirect,
@@ -34,47 +23,11 @@ const UpgradePlanBanner = ( {
 	buttonText = __( 'Upgrade', 'jetpack' ),
 	visible = true,
 } ) => {
-	const { checkoutUrl, isAutosaveablePost, isDirtyPost } = useSelect( select => {
-		const editorSelector = select( 'core/editor' );
-		const planSelector = select( 'wordpress-com/plans' );
-
-		const { id: postId, type: postType } = editorSelector.getCurrentPost();
-		const PLAN_SLUG = 'value_bundle';
-		const plan = planSelector && planSelector.getPlan( PLAN_SLUG );
-
-		return {
-			checkoutUrl: getUpgradeUrl( { plan, PLAN_SLUG, postId, postType } ),
-			isAutosaveablePost: editorSelector.isEditedPostAutosaveable(),
-			isDirtyPost: editorSelector.isEditedPostDirty(),
-		};
-	}, [] );
+	const [ checkoutUrl, goToCheckoutPage ] = useUpgradeFlow( onRedirect );
 
 	if ( ! visible ) {
 		return null;
 	}
-
-	// Alias. Save post by dispatch.
-	const savePost = dispatch( 'core/editor' ).savePost;
-
-	const goToCheckoutPage = event => {
-		if ( ! window?.top?.location?.href ) {
-			return;
-		}
-
-		event.preventDefault();
-
-		/*
-		 * If there are not unsaved values, redirect.
-		 * If the post is not autosaveable, redirect.
-		 */
-		if ( ! isDirtyPost || ! isAutosaveablePost ) {
-			// Using window.top to escape from the editor iframe on WordPress.com
-			return redirect( checkoutUrl, onRedirect );
-		}
-
-		// Save the post. Then redirect.
-		savePost( event ).then( () => redirect( checkoutUrl, onRedirect ) );
-	};
 
 	const cssClasses = classNames( className, 'jetpack-upgrade-plan-banner', 'wp-block' );
 
