017a0321746c659c0ce7df0508814f9f669d23dd
retrofox <rdsuarez@gmail.com>
2020-08-17 10:04:02 -0300

    paid-blocks: add utils tracking helper fn

---
+++ extensions/extended-blocks/paid-blocks/utils.js
@@ -0,0 +1,25 @@
+/**
+ * External dependencies
+ */
+
+/**
+ * Internal dependencies
+ */
+import analytics from '../../../_inc/client/lib/analytics';
+
+/**
+ * Record event helper.
+ * Use it when the user clicks on the upgrade banner button.
+ *
+ * @param { object } props - Event properties.
+ * @param { string } props.plan -      Plan slug.
+ * @param { string } props.blockName - Block name where the banner is mounted.
+ * @param { string } props.context -   Banner context: sidebar, editor.
+ * @returns { Function }               Rector event helper function.
+ */
+export const trackUpgradeClickEvent = ( { plan, blockName, context } ) =>
+	void analytics.tracks.recordEvent( 'jetpack_editor_block_upgrade_click', {
+		plan,
+		block: blockName,
+		context,
+	} );
