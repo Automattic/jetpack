2ccd08624358a37caaf0b0c6398800035dfdd60e
Daniel Walmsley <goldsounds@gmail.com>
2020-08-19 14:46:53 -0700

    Add eventbrite support

--- 3rd-party/class-jetpack-amp-feature-assets-sanitizer.php
+++ 3rd-party/class-jetpack-amp-feature-assets-sanitizer.php
@@ -18,6 +18,8 @@ class Jetpack_AMP_Feature_Assets_Sanitizer extends AMP_Base_Sanitizer {
 	private static $asset_xpaths = array(
 		'//script[contains(@src,\'//www.opentable.com/widget\')]',
 		'//script[contains(@src,\'//assets.pinterest.com/js/pinit.js\')]',
+		'//script[ @id = \'eventbrite-widget-js\' ]',
+		'//script[ @id = \'eventbrite-widget-js-after\' ]',
 	);
 
 	/**
