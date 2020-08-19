c55c7618a42bfeac6e181232dd6afdaae0be1e74
Paul Bunkham <paul@dobit.co.uk>
2020-08-19 17:18:12 +0100

    Bump version to 137 and update custom-fonts version https://github.com/Automattic/wpcomsh/pull/507

--- projects/plugins/wpcomsh/wpcomsh.php
+++ projects/plugins/wpcomsh/wpcomsh.php
@@ -2,13 +2,13 @@
 /**
  * Plugin Name: WordPress.com Site Helper
  * Description: A helper for connecting WordPress.com sites to external host infrastructure.
- * Version: 2.4.136
+ * Version: 2.4.137
  * Author: Automattic
  * Author URI: http://automattic.com/
  */
 
 // Increase version number if you change something in wpcomsh.
-define( 'WPCOMSH_VERSION', '2.4.136' );
+define( 'WPCOMSH_VERSION', '2.4.137' );
 
 // If true, Typekit fonts will be available in addition to Google fonts
 add_filter( 'jetpack_fonts_enable_typekit', '__return_true' );
