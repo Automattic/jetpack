f8424174e292a601ffeb1a47b16a41c7ef3a876d
retrofox <rdsuarez@gmail.com>
2020-08-19 16:46:06 -0300

    extended-blocks: set blocks availability

---
+++ extensions/extended-blocks/core-audio/core-audio.php
@@ -0,0 +1,21 @@
+<?php
+// Populate the available extensions with core/audio.
+add_filter(
+	'jetpack_set_available_extensions',
+	function ( $extensions ) {
+		return array_merge(
+			$extensions,
+			array(
+				'core/audio',
+			)
+		);
+	}
+);
+
+// Set the core/audio block availability, depending on the site plan.
+add_action(
+	'jetpack_register_gutenberg_extensions',
+	function() {
+		\Jetpack_Gutenberg::set_availability_for_plan( 'core/audio' );
+	}
+);
