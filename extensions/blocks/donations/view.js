79c60a88d6cdf6e97eedf52d6d712b7430f3e7b8
mmtr <miguel.torres@automattic.com>
2020-08-28 09:59:19 +0200

    Donations block: Handle lack of nav items

--- extensions/blocks/donations/view.js
+++ extensions/blocks/donations/view.js
@@ -125,9 +125,13 @@ class JetpackDonations {
 
 			// Toggle nav item.
 			const prevNavItem = this.getNavItem( prevInterval );
-			prevNavItem.classList.remove( 'is-active' );
+			if ( prevNavItem ) {
+				prevNavItem.classList.remove( 'is-active' );
+			}
 			const newNavItem = this.getNavItem( newInterval );
-			newNavItem.classList.add( 'is-active' );
+			if ( newNavItem ) {
+				newNavItem.classList.add( 'is-active' );
+			}
 
 			// Toggle tab content.
 			tabContent.classList.remove( tabContentClasses[ prevInterval ] );
@@ -150,7 +154,9 @@ class JetpackDonations {
 
 		// Activates the default tab on first execution.
 		const navItem = this.getNavItem( this.interval );
-		navItem.classList.add( 'is-active' );
+		if ( navItem ) {
+			navItem.classList.add( 'is-active' );
+		}
 		tabContent.classList.add( tabContentClasses[ this.interval ] );
 	}
 
