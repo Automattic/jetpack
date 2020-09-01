ec88783ffaf578b05fe0d1f44427d790d69743a4
Jason Moon <4044428+jsnmoon@users.noreply.github.com>
2020-09-01 14:34:55 -0600

    Instant Search: Fix photon integration (#17037)

--- modules/search/instant-search/components/photon-image.jsx
+++ modules/search/instant-search/components/photon-image.jsx
@@ -6,13 +6,29 @@
 import { h } from 'preact';
 import photon from 'photon';
 
+/**
+ * Strips query string values from URLs; photon can't handle them.
+ *
+ * @param {string} url - Image URL
+ *
+ * @returns {string} - Image URL without any query strings.
+ */
+function stripQueryString( url ) {
+	return url.split( '?', 1 )[ 0 ];
+}
+
 const PhotonImage = ( { useDiv, src, maxWidth = 300, maxHeight = 300, alt, ...otherProps } ) => {
-	const photonSrc = photon( src, { resize: `${ maxWidth },${ maxHeight }` } );
+	const photonSrc = photon( stripQueryString( src ), { resize: `${ maxWidth },${ maxHeight }` } );
+	const srcToDisplay = photonSrc !== null ? photonSrc : src;
 
 	return useDiv ? (
-		<div style={ { backgroundImage: `url("${ src }")` } } title={ alt } { ...otherProps } />
+		<div
+			style={ { backgroundImage: `url("${ srcToDisplay }")` } }
+			title={ alt }
+			{ ...otherProps }
+		/>
 	) : (
-		<img src={ photonSrc !== null ? photonSrc : src } alt={ alt } { ...otherProps } />
+		<img src={ srcToDisplay } alt={ alt } { ...otherProps } />
 	);
 };
 
