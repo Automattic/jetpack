0ec53969c5cde27ac09d017466b5d792b4f9645b
Mario Zorz <mariozorz@gmail.com>
2020-09-04 12:13:08 -0300

    added flex alignment and actual mediaFiles attribute rendering

--- extensions/blocks/story/edit.native.js
+++ extensions/blocks/story/edit.native.js
@@ -2,11 +2,38 @@
  * External dependencies
  */
 import { Text, View } from 'react-native';
+/**
+ * WordPress dependencies
+ */
+import {
+	Image,
+} from '@wordpress/components';
 
-const StoryEdit = () => {
+const alignToFlex = {
+	left: 'flex-start',
+	center: 'center',
+	right: 'flex-end',
+	full: 'center',
+	wide: 'center',
+};
+
+const StoryEdit = ({
+		attributes,
+}) => {
+	const { align, content, level, placeholder, width, mediaFiles } = attributes;
+	const hasImages = !! mediaFiles.length;
 	return (
 		<View>
-			<Text>this is some example text</Text>
+			{! hasImages && 
+				<Text>Empty Story placeholder here</Text>
+			}
+			{ hasImages && 
+				<Image
+					align={ align && alignToFlex[ align ] }
+					url={ mediaFiles[0].url } // just select the first one
+					width={ width }
+				/>
+			}
 		</View>
 	);
 };
