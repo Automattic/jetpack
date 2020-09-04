fea60b0336546c8cd8bcf97ea8f7ba5246b093e0
Mario Zorz <mariozorz@gmail.com>
2020-09-04 09:00:58 -0300

    basic scaffolding for the Story block on mobile editor

---
+++ extensions/blocks/story/edit.native.js
@@ -0,0 +1,14 @@
+/**
+ * External dependencies
+ */
+import { Text, View } from 'react-native';
+
+const StoryEdit = () => {
+	return (
+		<View>
+			<Text>this is some example text</Text>
+		</View>
+	);
+};
+
+export default StoryEdit;
