a6093c9a113e8e07db021b60bea0179b22759cc7
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-09-08 14:52:49 +0300

    minor update

--- tests/e2e/bin/includes.sh
+++ tests/e2e/bin/includes.sh
@@ -128,6 +128,13 @@ reset_env() {
 }
 
 configure_wp_env() {
+	WP_SITE_URL=$(get_ngrok_url)
+
+	if [ -z "$WP_SITE_URL" ]; then
+		echo -e $(error_message "WP_SITE_URL is not set after launching an ngrok")
+		exit 1
+	fi
+
 	yarn wp-env run tests-wordpress touch wp-content/debug.log
 	yarn wp-env run tests-wordpress chown www-data:www-data wp-content/debug.log
 
