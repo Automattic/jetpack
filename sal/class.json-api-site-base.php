8b4dbeee953d9a8d6bd3975a05958ea720bf480f
roo2 <roo2@users.noreply.github.com>
2020-09-10 10:41:32 +1000

    Coming soon: add coming soon ID to homepage and sites APIs (#17105)
    Summary:
    Allows getting and setting `wpcom_public_coming_soon_page_id` vie the api, as will be use by calypso on the *pages* page on this PR
    
    This commit syncs r213352-wpcom.
    
    Co-authored-by: Eru Penkman <eru.penkman@automattic.com>

--- sal/class.json-api-site-base.php
+++ sal/class.json-api-site-base.php
@@ -616,6 +616,10 @@ abstract class SAL_Site {
 		return (int) get_option( 'page_for_posts' );
 	}
 
+	public function get_wpcom_public_coming_soon_page_id() {
+		return (int) get_option( 'wpcom_public_coming_soon_page_id' );
+	}
+
 	function is_headstart() {
 		return get_option( 'headstart' );
 	}
