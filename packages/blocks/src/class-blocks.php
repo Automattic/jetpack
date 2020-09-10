933a090093dec8b317511f2f5ab82beb305f2d8d
Jeremy Herve <jeremy@jeremy.hu>
2020-09-10 12:16:24 +0200

    Move block_classes method to new Blocks package.

--- packages/blocks/src/class-blocks.php
+++ packages/blocks/src/class-blocks.php
@@ -17,7 +17,47 @@ namespace Automattic\Jetpack;
  * @since 9.0.0
  */
 class Blocks {
+	/**
+	 * Get CSS classes for a block.
+	 *
+	 * @since 9.0.0
+	 *
+	 * @param string $slug  Block slug.
+	 * @param array  $attr  Block attributes.
+	 * @param array  $extra Potential extra classes you may want to provide.
+	 *
+	 * @return string $classes List of CSS classes for a block.
+	 */
+	public static function classes( $slug = '', $attr, $extra = array() ) {
+		if ( empty( $slug ) ) {
+			return '';
+		}
 
+		// Basic block name class.
+		$classes = array(
+			'wp-block-jetpack-' . $slug,
+		);
+
+		// Add alignment if provided.
+		if (
+			! empty( $attr['align'] )
+			&& in_array( $attr['align'], array( 'left', 'center', 'right', 'wide', 'full' ), true )
+		) {
+			array_push( $classes, 'align' . $attr['align'] );
+		}
+
+		// Add custom classes if provided in the block editor.
+		if ( ! empty( $attr['className'] ) ) {
+			array_push( $classes, $attr['className'] );
+		}
+
+		// Add any extra classes.
+		if ( is_array( $extra ) && ! empty( $extra ) ) {
+			$classes = array_merge( $classes, array_filter( $extra ) );
+		}
+
+		return implode( ' ', $classes );
+	}
 }
 
 
