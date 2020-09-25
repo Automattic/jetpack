b0ebc9d58712c833b1423ff5a808b1ba96e0ff44
Allison Levine <allison.levine@automattic.com>
2020-09-25 12:04:37 -0400

    Add new line to script and bump down JS_END

--- extensions/blocks/calendly/calendly.php
+++ extensions/blocks/calendly/calendly.php
@@ -135,6 +135,7 @@ if ( window.jQuery && window.o2 ) {
 } else {
 	jetpackInitCalendly{$escaped_block_id}();
 }
+
 JS_END;
 			wp_add_inline_script( 'jetpack-calendly-external-js', sprintf( $script, esc_url( $url ), esc_js( $html_id ) ) );
 		}
