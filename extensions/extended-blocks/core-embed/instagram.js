26bccafb8001fa268c84329c7399c3d6806e2a19
Bernie Reiter <ockham@raz.or.at>
2020-09-29 20:47:07 +0200

    Make IG embed depend on WP.com connection

--- extensions/extended-blocks/core-embed/instagram.js
+++ extensions/extended-blocks/core-embed/instagram.js
@@ -1,15 +1,14 @@
 /**
  * External dependencies
  */
-import apiFetch from '@wordpress/api-fetch';
 import { addFilter } from '@wordpress/hooks';
-import { registerBlockVariation, unregisterBlockVariation } from '@wordpress/blocks';
 import { __, _x } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
 import { getIconColor } from '../../shared/block-icons';
+import isActive from '../../shared/is-active';
 
 const instagramVariation = {
 	name: 'instagram',
@@ -47,26 +46,16 @@ const instagramVariation = {
  * @returns {object} The settings for the given block with the patched variations.
  */
 function reactivateInstagramEmbedBlockVariation( settings, name ) {
-	if ( name !== 'core/embed' || ! settings.variations ) {
+	if ( name !== 'core/embed' || ! settings.variations || ! isActive() ) {
 		return settings;
 	}
 
-	// Only enable the embed variation if it's supported by the backend,
-	// i.e. if the `shortcodes` modules is enabled.
-	apiFetch( { path: '/jetpack/v4/module/shortcodes' } ).then( shortcodesModule => {
-		if ( ! shortcodesModule.activated ) {
-			return;
-		}
-
-		// If `addFilter` supported async functions, we could do the following:
-		//
-		// const variations = settings.variations.filter( variation => variation.name !== instagramVariation.name );
-		// settings.variations = [ ...variations, instagramVariation ];
-
-		// Alas, it doesn't (yet), so we have to do it like this:
-		unregisterBlockVariation( 'core/embed', instagramVariation.name );
-		registerBlockVariation( 'core/embed', instagramVariation );
-	} );
+	// Remove potentially existing variation.
+	const variations = settings.variations.filter(
+		variation => variation.name !== instagramVariation.name
+	);
+	// Add 'our' variation.
+	settings.variations = [ ...variations, instagramVariation ];
 
 	return settings;
 }
