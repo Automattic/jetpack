4a52b34eef6ea7d5eba357adf9e37e3f85e2dd7c
leogermani <leogermani@gmail.com>
2020-09-30 13:35:05 -0300

    renaming constant

--- class.jetpack.php
+++ class.jetpack.php
@@ -5793,7 +5793,7 @@ endif;
 		$client_blog_id = is_multisite() ? $blog_id : 0;
 
 		if ( ! isset( $clients[ $client_blog_id ] ) ) {
-			$clients[ $client_blog_id ] = new Jetpack_IXR_ClientMulticall( array( 'user_id' => Connection_Manager::MASTER_USER ) );
+			$clients[ $client_blog_id ] = new Jetpack_IXR_ClientMulticall( array( 'user_id' => Connection_Manager::CONNECTION_OWNER ) );
 			if ( function_exists( 'ignore_user_abort' ) ) {
 				ignore_user_abort( true );
 			}
