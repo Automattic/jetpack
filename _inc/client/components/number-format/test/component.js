92938a392cfdf972af4f4fc26e397fe5d0ab4be0
Jeremy Herve <jeremy@jeremy.hu>
2020-10-02 19:03:26 +0200

    Try to loop through test values differently

--- _inc/client/components/number-format/test/component.js
+++ _inc/client/components/number-format/test/component.js
@@ -1,16 +1,21 @@
 /**
+ * External dependencies
+ */
+import { expect } from 'chai';
+
+/**
  * Internal dependencies
  */
 import { cleanLocale } from '../';
 
 describe( 'cleanLocale', () => {
-	const wpLocales = [
-		[ 'en_US', 'en-US' ],
-		[ 'de_DE_formal', 'de-DE' ],
-		[ 'en', 'en' ],
-	];
-
-	it.each( wpLocales )( 'WP locale %s is returned as %s', ( value, expected ) => {
-		expect( cleanLocale( value ) ).toStrictEqual( expected );
+	it( 'en_US locale is cleaned into en-US', () => {
+		expect( cleanLocale( 'en_US' ) ).to.be.equal( 'en-US' );
+	} );
+	it( 'de_DE_formal locale is cleaned into de-DE', () => {
+		expect( cleanLocale( 'de_DE_formal' ) ).to.be.equal( 'de-DE' );
+	} );
+	it( 'en locale does not change', () => {
+		expect( cleanLocale( 'en' ) ).to.be.equal( 'en' );
 	} );
 } );
