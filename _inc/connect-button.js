1412f07d3731a09ab3501c0704e142b10455c338
Derek Smart <smart@automattic.com>
2020-10-02 11:08:09 -0400

    [not verified] Connect button: Carry the "from" param when redirecting. (#17350)
    * Connect button: Carry the  param when redirecting.
    
    * Only append parameter if it's already set. Don't need an empty one :)

--- _inc/connect-button.js
+++ _inc/connect-button.js
@@ -52,7 +52,9 @@ jQuery( document ).ready( function ( $ ) {
 		handleConnectInPlaceFlow: function () {
 			// Alternative connection buttons should redirect to the main one for the "connect in place" flow.
 			if ( connectButton.hasClass( 'jp-banner__alt-connect-button' ) ) {
-				window.location = jpConnect.connectInPlaceUrl;
+				// Make sure we don't lose the `from` parameter, if set.
+				var fromParam = ( connectButtonFrom && '&from=' + connectButtonFrom ) || '';
+				window.location = jpConnect.connectInPlaceUrl + fromParam;
 				return;
 			}
 
@@ -146,24 +148,24 @@ jQuery( document ).ready( function ( $ ) {
 
 			// Fetch plan type late to make sure any stored license keys have been
 			// attached to the site during the connection.
-			jetpackConnectButton.fetchPlanType()
-				.always( function () {
-					if ( ! jetpackConnectButton.isPaidPlan ) {
-						window.location.assign( jpConnect.plansPromptUrl );
-						return;
-					}
+			jetpackConnectButton.fetchPlanType().always( function () {
+				if ( ! jetpackConnectButton.isPaidPlan ) {
+					window.location.assign( jpConnect.plansPromptUrl );
+					return;
+				}
 
-					var parser = document.createElement('a');
-					parser.href = jpConnect.dashboardUrl;
-					var reload = window.location.pathname === parser.pathname && window.location.hash !== parser.hash;
+				var parser = document.createElement( 'a' );
+				parser.href = jpConnect.dashboardUrl;
+				var reload =
+					window.location.pathname === parser.pathname && window.location.hash !== parser.hash;
 
-					window.location.assign( jpConnect.dashboardUrl );
+				window.location.assign( jpConnect.dashboardUrl );
 
-					if ( reload ) {
-						// The Jetpack admin page has hashes in the URLs, so we need to reload the page after .assign()
-						window.location.reload( true );
-					}
-				} );
+				if ( reload ) {
+					// The Jetpack admin page has hashes in the URLs, so we need to reload the page after .assign()
+					window.location.reload( true );
+				}
+			} );
 		},
 		handleConnectionError: function ( error ) {
 			jetpackConnectButton.isRegistering = false;
@@ -172,7 +174,11 @@ jQuery( document ).ready( function ( $ ) {
 	};
 
 	// When we visit /wp-admin/admin.php?page=jetpack#/setup, immediately start the connection flow.
-	var hash = location.hash.replace( /#\//, '' );
+	var hash = location.hash.replace( /(#\/setup).*/, 'setup' );
+
+	// In case the parameter has been manually set in the URL after redirect.
+	connectButtonFrom = location.hash.split( '&from=' )[ 1 ];
+
 	if ( 'setup' === hash ) {
 		if ( connectionHelpSections.length ) {
 			connectionHelpSections.hide();
