2367a1811f9805c6a72f8cc6bd989773e45c140b
Brad Jorsch <brad.jorsch@automattic.com>
2020-10-02 16:07:49 -0400

    Make UnsortedUseStatementsSniff.php comply with WordPress coding conventions
    1 file changed, 103 insertions(+), 85 deletions(-)

--- bin/PHPCSStandards/Jetpack/Sniffs/Classes/UnsortedUseStatementsSniff.php
+++ bin/PHPCSStandards/Jetpack/Sniffs/Classes/UnsortedUseStatementsSniff.php
@@ -1,8 +1,9 @@
-<?php
+<?php // phpcs:ignore WordPress.Files.FileName.NotHyphenatedLowercase
 /**
  * Sniff for sorting of `use` statements.
  *
- * Originally taken from MediaWiki's coding conventions.
+ * Originally taken from MediaWiki's coding conventions, then heavily munged
+ * for WordPress's coding conventions.
  *
  * @link https://github.com/wikimedia/mediawiki-tools-codesniffer
  *
@@ -22,6 +23,7 @@
  * http://www.gnu.org/copyleft/gpl.html
  *
  * @file
+ * @package Jetpack
  */
 
 namespace Jetpack\Sniffs\Classes;
@@ -30,154 +32,170 @@ use PHP_CodeSniffer\Files\File;
 use PHP_CodeSniffer\Sniffs\Sniff;
 use PHP_CodeSniffer\Util\Tokens;
 
+/**
+ * Sniff for sorting of `use` statements.
+ */
 class UnsortedUseStatementsSniff implements Sniff {
 
-	// Preferred order is classes → functions → constants
-	const ORDER = [ 'function' => 1, 'const' => 2 ];
+	// Preferred order is classes → functions → constants.
+	const ORDER = array(
+		'function' => 1,
+		'const'    => 2,
+	);
 
 	/**
-	 * @inheritDoc
+	 * Returns the token types that this sniff is interested in.
+	 *
+	 * @return array(int)
 	 */
 	public function register() {
-		return [ T_USE ];
+		return array( T_USE );
 	}
 
 	/**
-	 * @inheritDoc
+	 * Processes the tokens that this sniff is interested in.
 	 *
-	 * @param File $phpcsFile
-	 * @param int $stackPtr
+	 * @param File $phpcs_file The file where the token was found.
+	 * @param int  $stack_ptr The position in the stack where the token was found.
 	 * @return int|void
 	 */
-	public function process( File $phpcsFile, $stackPtr ) {
-		$tokens = $phpcsFile->getTokens();
+	public function process( File $phpcs_file, $stack_ptr ) {
+		$tokens = $phpcs_file->getTokens();
 
 		// Only check use statements in the global scope.
-		if ( !empty( $tokens[$stackPtr]['conditions'] ) ) {
-			// TODO: Use array_key_first() if available
-			$scope = key( $tokens[$stackPtr]['conditions'] );
-			return $tokens[$scope]['scope_closer'] === null ? $stackPtr : $tokens[$scope]['scope_closer'];
+		if ( ! empty( $tokens[ $stack_ptr ]['conditions'] ) ) {
+			// TODO: Use array_key_first() if available.
+			$scope = key( $tokens[ $stack_ptr ]['conditions'] );
+			return null === $tokens[ $scope ]['scope_closer'] ? $stack_ptr : $tokens[ $scope ]['scope_closer'];
 		}
 
-		$useStatementList = $this->makeUseStatementList( $phpcsFile, $stackPtr );
-		// Nothing to do, bail out as fast as possible
-		if ( count( $useStatementList ) <= 1 ) {
+		$use_statement_list = $this->makeUseStatementList( $phpcs_file, $stack_ptr );
+		// Nothing to do, bail out as fast as possible.
+		if ( count( $use_statement_list ) <= 1 ) {
 			return;
 		}
 
-		$sortedStatements = $this->sortByFullQualifiedClassName( $useStatementList );
+		$sorted_statements = $this->sortByFullQualifiedClassName( $use_statement_list );
 
-		if ( $useStatementList !== $sortedStatements ) {
-			$fix = $phpcsFile->addFixableWarning(
+		if ( $use_statement_list !== $sorted_statements ) {
+			$fix = $phpcs_file->addFixableWarning(
 				'Use statements are not alphabetically sorted',
-				$stackPtr,
+				$stack_ptr,
 				'UnsortedUse'
 			);
 
 			if ( $fix ) {
-				$phpcsFile->fixer->beginChangeset();
+				$phpcs_file->fixer->beginChangeset();
 
-				foreach ( $useStatementList as $statement ) {
+				foreach ( $use_statement_list as $statement ) {
 					for ( $i = $statement['start']; $i <= $statement['end']; $i++ ) {
-						$phpcsFile->fixer->replaceToken( $i, '' );
+						$phpcs_file->fixer->replaceToken( $i, '' );
 					}
-					// Also remove the newline at the end of the line, if there is one
-					if ( $tokens[$i]['code'] === T_WHITESPACE
-						&& $tokens[$i]['line'] < $tokens[$i + 1]['line']
+					// Also remove the newline at the end of the line, if there is one.
+					if ( T_WHITESPACE === $tokens[ $i ]['code']
+						&& $tokens[ $i ]['line'] < $tokens[ $i + 1 ]['line']
 					) {
-						$phpcsFile->fixer->replaceToken( $i, '' );
+						$phpcs_file->fixer->replaceToken( $i, '' );
 					}
 				}
 
-				foreach ( $sortedStatements as $statement ) {
-					$phpcsFile->fixer->addContent( $stackPtr, $statement['originalContent'] );
-					$phpcsFile->fixer->addNewline( $stackPtr );
+				foreach ( $sorted_statements as $statement ) {
+					$phpcs_file->fixer->addContent( $stack_ptr, $statement['originalContent'] );
+					$phpcs_file->fixer->addNewline( $stack_ptr );
 				}
 
-				$phpcsFile->fixer->endChangeset();
+				$phpcs_file->fixer->endChangeset();
 			}
 		}
 
-		// Continue *after* the last use token, to not process it twice
-		return end( $useStatementList )['end'] + 1;
+		// Continue *after* the last use token, to not process it twice.
+		return end( $use_statement_list )['end'] + 1;
 	}
 
 	/**
-	 * @param File $phpcsFile
-	 * @param int $stackPtr
+	 * Collect the list of `use` statements.
+	 *
+	 * @param File $phpcs_file The file where the token was found.
+	 * @param int  $stack_ptr The position in the stack where the token was found.
 	 * @return array[]
 	 */
-	private function makeUseStatementList( File $phpcsFile, $stackPtr ) {
-		$tokens = $phpcsFile->getTokens();
-		$list = [];
+	private function makeUseStatementList( File $phpcs_file, $stack_ptr ) {
+		$tokens = $phpcs_file->getTokens();
+		$list   = array();
 
 		do {
-			$originalContent = '';
-			$group = 0;
-			$sortKey = '';
-			$collectSortKey = false;
+			$original_content = '';
+			$group            = 0;
+			$sort_key         = '';
+			$collect_sort_key = false;
 
-			// The end condition here is for when a file ends directly after a "use"
-			for ( $i = $stackPtr; $i < $phpcsFile->numTokens; $i++ ) {
-				$code = $tokens[$i]['code'];
-				$content = $tokens[$i]['content'];
-				$originalContent .= $content;
+			// The end condition here is for when a file ends directly after a "use".
+			for ( $i = $stack_ptr; $i < $phpcs_file->numTokens; $i++ ) { // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+				$code              = $tokens[ $i ]['code'];
+				$content           = $tokens[ $i ]['content'];
+				$original_content .= $content;
 
-				if ( $code === T_STRING ) {
-					// Reserved keywords "function" and "const" can not appear anywhere else
+				if ( T_STRING === $code ) {
+					// Reserved keywords "function" and "const" can not appear anywhere else.
 					if ( strcasecmp( $content, 'function' ) === 0
 						|| strcasecmp( $content, 'const' ) === 0
 					) {
 						$group = self::ORDER[ strtolower( $content ) ];
-					} elseif ( !$sortKey ) {
-						// The first non-reserved string is where the class name starts
-						$collectSortKey = true;
+					} elseif ( ! $sort_key ) {
+						// The first non-reserved string is where the class name starts.
+						$collect_sort_key = true;
 					}
-				} elseif ( $code === T_AS ) {
-					// The string after an "as" is not part of the class name any more
-					$collectSortKey = false;
-				} elseif ( $code === T_SEMICOLON && $sortKey ) {
-					$list[] = [
-						'start' => $stackPtr,
-						'end' => $i,
-						'originalContent' => $originalContent,
-						'group' => $group,
-						// No need to trim(), no spaces or leading backslashes have been collected
-						'sortKey' => strtolower( $sortKey ),
-					];
+				} elseif ( T_AS === $code ) {
+					// The string after an "as" is not part of the class name any more.
+					$collect_sort_key = false;
+				} elseif ( T_SEMICOLON === $code && $sort_key ) {
+					$list[] = array(
+						'start'           => $stack_ptr,
+						'end'             => $i,
+						'originalContent' => $original_content,
+						'group'           => $group,
+						// No need to trim(), no spaces or leading backslashes have been collected.
+						'sortKey'         => strtolower( $sort_key ),
+					);
 
-					// Try to find the next "use" token after the current one
-					$stackPtr = $phpcsFile->findNext( Tokens::$emptyTokens, $i + 1, null, true );
+					// Try to find the next "use" token after the current one.
+					// phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+					$stack_ptr = $phpcs_file->findNext( Tokens::$emptyTokens, $i + 1, null, true );
 					break;
-				} elseif ( isset( Tokens::$emptyTokens[$code] ) ) {
-					// We never want any space or comment in the sort key
+				} elseif ( isset( Tokens::$emptyTokens[ $code ] ) ) { // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+					// We never want any space or comment in the sort key.
 					continue;
-				} elseif ( $code !== T_USE && $code !== T_NS_SEPARATOR ) {
-					// Unexpected token, stop searching for more "use" keywords
+				} elseif ( T_USE !== $code && T_NS_SEPARATOR !== $code ) {
+					// Unexpected token, stop searching for more "use" keywords.
 					break 2;
 				}
 
-				if ( $collectSortKey ) {
-					$sortKey .= $content;
+				if ( $collect_sort_key ) {
+					$sort_key .= $content;
 				}
 			}
-		} while ( $stackPtr && $tokens[$stackPtr]['code'] === T_USE );
+		} while ( $stack_ptr && T_USE === $tokens[ $stack_ptr ]['code'] );
 
 		return $list;
 	}
 
 	/**
-	 * @param array[] $list
+	 * Sort a list of `use` data structures by class name.
+	 *
+	 * @param array[] $list List to sort.
 	 * @return array[]
 	 */
 	private function sortByFullQualifiedClassName( array $list ) {
-		usort( $list, function ( array $a, array $b ) {
-			if ( $a['group'] !== $b['group'] ) {
-				return $b['group'] - $a['group'];
+		usort(
+			$list,
+			function ( array $a, array $b ) {
+				if ( $a['group'] !== $b['group'] ) {
+					return $b['group'] - $a['group'];
+				}
+				// Can't use strnatcasecmp() because it behaves different, compared to e.g. PHPStorm.
+				return strnatcmp( $a['sortKey'], $b['sortKey'] );
 			}
-			// Can't use strnatcasecmp() because it behaves different, compared to e.g. PHPStorm
-			return strnatcmp( $a['sortKey'], $b['sortKey'] );
-		} );
+		);
 
 		return $list;
 	}
