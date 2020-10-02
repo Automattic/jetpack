985203103a0aa3f4de1cfd357d3077bf3a81f424
Brad Jorsch <brad.jorsch@automattic.com>
2020-10-02 15:26:52 -0400

    PHPCS: Update directory structure, and modernize class usage
    In starting to add another sniff, I noticed several inconsistencies in
    the configuration of the current MasterUserConstant sniff.
    
    * We seem to want our phpcs standard to be named "Jetpack", but phpcs was
      calling it "PHPCSSniffs".
    * The MasterUserConstant sniff produces warnings with ID
      "..Jetpack_Sniffs_MasterUserConstant.ShouldNotBeUsed", rather than
      "Jetpack.Something.MasterUserConstant.ShouldNotBeUsed" like all other
      sniffs.
    * MasterUserConstantSniff.php is using old phpcs 2.x aliases for the
      phpcs classes, which don't get loaded if you use phpcs --standard to
      run only Jetpack's sniffs.
    
    This patch fixes the above with the following changes:
    
    * As phpcs seems to want the standard to be in an eponymous directory,
      move bin/PHPCSSniffs to bin/PHPCSStandards/Jetpack.
    * Rename Jetpack_Sniffs_MasterUserConstantSniff to
      Jetpack\Sniffs\Constants\MasterUserConstantSniff so the ID is
      determined correctly.
    * Update MasterUserConstantSniff.php to use the phpcs 3.x class names.
    * Move MasterUserConstantSniff.php to a PSR-4 style directory structure,
      since even the WordPress phpcs standards do that.

--- bin/PHPCSStandards/Jetpack/Sniffs/Constants/MasterUserConstantSniff.php
+++ bin/PHPCSStandards/Jetpack/Sniffs/Constants/MasterUserConstantSniff.php
@@ -1,8 +1,15 @@
 <?php // phpcs:ignore WordPress.Files.FileName.NotHyphenatedLowercase
+
+namespace Jetpack\Sniffs\Constants;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Util\Tokens;
+
 /**
  * Sniffer that looks for JETPACK_MASTER_USER constant usage
  */
-class Jetpack_Sniffs_MasterUserConstantSniff implements PHP_CodeSniffer_Sniff {
+class MasterUserConstantSniff implements Sniff {
 	/**
 	 * Returns the token types that this sniff is interested in.
 	 *
@@ -16,10 +23,10 @@ class Jetpack_Sniffs_MasterUserConstantSniff implements PHP_CodeSniffer_Sniff {
 	/**
 	 * Processes the tokens that this sniff is interested in.
 	 *
-	 * @param PHP_CodeSniffer_File $phpcs_file The file where the token was found.
-	 * @param int                  $stack_ptr The position in the stack where the token was found.
+	 * @param File $phpcs_file The file where the token was found.
+	 * @param int  $stack_ptr The position in the stack where the token was found.
 	 */
-	public function process( PHP_CodeSniffer_File $phpcs_file, $stack_ptr ) {
+	public function process( File $phpcs_file, $stack_ptr ) {
 
 		$tokens = $phpcs_file->getTokens();
 
