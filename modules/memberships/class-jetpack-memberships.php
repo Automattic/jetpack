63c86b38267d79fe255e09366af0b7a8ebe91f28
Brandon Kraft <public@brandonkraft.com>
2020-10-05 16:41:06 -0500

    Bump to new plan name

--- modules/memberships/class-jetpack-memberships.php
+++ modules/memberships/class-jetpack-memberships.php
@@ -80,8 +80,8 @@ class Jetpack_Memberships {
 		if ( ! self::$instance ) {
 			self::$instance = new self();
 			self::$instance->register_init_hook();
-			// Yes, `personal-bundle` with a dash, `jetpack_personal` with an underscore. Check the v1.5 endpoint to verify.
-			self::$required_plan = ( defined( 'IS_WPCOM' ) && IS_WPCOM ) ? 'personal-bundle' : 'jetpack_personal';
+			// Yes, `personal-bundle` with a dash, `jetpack_security_daily` with an underscore. Check the v1.5 endpoint to verify.
+			self::$required_plan = ( defined( 'IS_WPCOM' ) && IS_WPCOM ) ? 'personal-bundle' : 'jetpack_security_daily';
 		}
 
 		return self::$instance;
