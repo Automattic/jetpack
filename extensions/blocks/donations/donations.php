130befafb3afb703bdbb83dc902da3c43335e93d
mmtr <miguel.torres@automattic.com>
2020-10-12 18:01:06 +0200

    Add one time text to save

--- extensions/blocks/donations/donations.php
+++ extensions/blocks/donations/donations.php
@@ -40,16 +40,41 @@ add_action( 'init', __NAMESPACE__ . '\register_block' );
  * @return string
  */
 function render_block( $attr, $content ) {
+	// Keep content as-is if rendered in other contexts than frontend (i.e. feed, emails, API, etc.).
+	if ( ! jetpack_is_frontend() ) {
+		return $content;
+	}
+
 	Jetpack_Gutenberg::load_assets_as_required( FEATURE_NAME, array( 'thickbox' ) );
 
 	require_once JETPACK__PLUGIN_DIR . '/modules/memberships/class-jetpack-memberships.php';
 	add_thickbox();
 
-	$donations = array(
-		'one-time' => $attr['oneTimeDonation'],
-		'monthly'  => $attr['monthlyDonation'],
-		'annual'   => $attr['annualDonation'],
-	);
+	$donations = array();
+	foreach (
+		array(
+			'one-time' => $attr['oneTimeDonation'],
+			'monthly'  => $attr['monthlyDonation'],
+			'annual'   => $attr['annualDonation'],
+		) as $interval => $donation
+	) {
+		if ( ! $donation['show'] ) {
+			continue;
+		}
+		$donations[ $interval ] = $donation;
+	}
+
+	/*
+	$tabs = '';
+	if ( count( $donations ) > 1 ) {
+		$tabs .= '<div className="donations__nav">';
+		foreach ( $donations as $interval => $donation ) {
+			$tabs .= '<div role="button" tabIndex={ 0 } className="donations__nav-item" data-interval="' . $interval . '">' . $interval . '</div>
+		}
+		$tabs .= '</div>';
+	}
+	*/
+
 	foreach ( $donations as $interval => $donation ) {
 		if ( ! $donation['show'] ) {
 			continue;
@@ -66,3 +91,22 @@ function render_block( $attr, $content ) {
 
 	return $content;
 }
+
+/**
+ * Determine if AMP should be disabled on posts having Donations blocks.
+ *
+ * @param bool    $skip Skipped.
+ * @param int     $post_id Post ID.
+ * @param WP_Post $post Post.
+ *
+ * @return bool Whether to skip the post from AMP.
+ */
+function amp_skip_post( $skip, $post_id, $post ) {
+	// When AMP is on standard mode, there are no non-AMP posts to link to where the donation can be completed, so let's
+	// prevent the post from being available in AMP.
+	if ( function_exists( 'amp_is_canonical' ) && \amp_is_canonical() && has_block( 'jetpack/donations', $post->post_content ) ) {
+		return true;
+	}
+	return $skip;
+}
+add_filter( 'amp_skip_post', __NAMESPACE__ . '\amp_skip_post', 10, 3 );
--- extensions/blocks/donations/save.js
+++ extensions/blocks/donations/save.js
@@ -1,19 +1,42 @@
+/**
+ * WordPress dependencies
+ */
+import { RichText } from '@wordpress/block-editor';
+
 const Save = ( { attributes } ) => {
-	const { fallbackLinkUrl, fallbackLinkText, oneTimeDonation } = attributes;
+	const {
+		fallbackLinkUrl,
+		fallbackLinkText,
+		oneTimeDonation,
+		monthlyDonation,
+		annualDonation,
+	} = attributes;
 
 	if ( ! oneTimeDonation || ! oneTimeDonation.show || oneTimeDonation.planId === -1 ) {
 		return null;
 	}
 
+	const isOneTimeOnly = ! monthlyDonation.show && ! annualDonation.show;
+
 	return (
-		<a
-			className="jetpack-donations-fallback-link"
-			href={ fallbackLinkUrl }
-			target="_blank"
-			rel="noopener noreferrer"
-		>
-			{ fallbackLinkText }
-		</a>
+		<div>
+			<div className="donations__container">
+				{ isOneTimeOnly && (
+					<>
+						<RichText.Content tagName="h4" value={ oneTimeDonation.heading } />
+						<RichText.Content tagName="p" value={ oneTimeDonation.extraText } />
+					</>
+				) }
+				<a
+					className="jetpack-donations-fallback-link"
+					href={ fallbackLinkUrl }
+					target="_blank"
+					rel="noopener noreferrer noamphtml"
+				>
+					{ fallbackLinkText }
+				</a>
+			</div>
+		</div>
 	);
 };
 
