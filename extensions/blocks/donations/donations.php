252138d0a00fca7bd59f60b21775abbcd948a92d
mmtr <miguel.torres@automattic.com>
2020-10-14 17:10:17 +0200

    Augment block UI when rendered on frontend

--- extensions/blocks/donations/donations.php
+++ extensions/blocks/donations/donations.php
@@ -50,7 +50,141 @@ function render_block( $attr, $content ) {
 	require_once JETPACK__PLUGIN_DIR . '/modules/memberships/class-jetpack-memberships.php';
 	add_thickbox();
 
-	return $content;
+	$donations = array(
+		'one-time' => array_merge(
+			array(
+				'title' => __( 'One-Time', 'jetpack' ),
+				'class' => 'donations__one-time-item',
+			),
+			$attr['oneTimeDonation']
+		),
+	);
+	if ( $attr['monthlyDonation']['show'] ) {
+		$donations['1 month'] = array_merge(
+			array(
+				'title' => __( 'Monthly', 'jetpack' ),
+				'class' => 'donations__monthly-item',
+			),
+			$attr['monthlyDonation']
+		);
+	}
+	if ( $attr['annualDonation']['show'] ) {
+		$donations['1 year'] = array_merge(
+			array(
+				'title' => __( 'Yearly', 'jetpack' ),
+				'class' => 'donations__annual-item',
+			),
+			$attr['annualDonation']
+		);
+	}
+
+	$currency = $attr['currency'];
+
+	$classes = 'wp-block-jetpack-donations';
+	if ( ! empty( $attr['className'] ) ) {
+		$classes .= ' ' . $attr['className'];
+	}
+
+	$nav        = '';
+	$headings   = '';
+	$amounts    = '';
+	$extra_text = '';
+	$buttons    = '';
+	foreach ( $donations as $interval => $donation ) {
+		$plan_id = intval( $donation['planId'] );
+		$plan    = get_post( $plan_id );
+		if ( ! $plan || is_wp_error( $plan ) ) {
+			continue;
+		}
+
+		if ( count( $donations ) > 1 ) {
+			if ( ! $nav ) {
+				$nav .= '<div class="donations__nav">';
+			}
+			$nav .= sprintf(
+				'<div role="button" tabindex="0" class="donations__nav-item" data-interval="%1$s">%2$s</div>',
+				esc_attr( $interval ),
+				$donation['title']
+			);
+		}
+		$headings .= sprintf(
+			'<h4 class="%1$s">%2$s</h4>',
+			esc_attr( $donation['class'] ),
+			$donation['heading']
+		);
+		$amounts  .= sprintf(
+			'<div class="donations__amounts %s">',
+			esc_attr( $donation['class'] )
+		);
+		foreach ( $donation['amounts'] as $amount ) {
+			$amounts .= sprintf(
+				'<div class="donations__amount" data-amount="%1$s" data-currency="%2$s">%3$s%4$s</div>',
+				esc_attr( $amount ),
+				esc_attr( $currency ),
+				$currency,
+				$amount
+			);
+		}
+		$amounts    .= '</div>';
+		$extra_text .= sprintf(
+			'<p class="%1$s">%2$s</p>',
+			esc_attr( $donation['class'] ),
+			$donation['extraText']
+		);
+		$buttons    .= sprintf(
+			'<a class="wp-block-button__link donations__donate-button %1$s" href="%2$s">%3$s</a>',
+			esc_attr( $donation['class'] ),
+			\Jetpack_Memberships::get_instance()->get_subscription_url( $plan_id ),
+			$donation['buttonText']
+		);
+	}
+	if ( $nav ) {
+		$nav .= '</div>';
+	}
+
+	$custom_amount = '';
+	if ( $attr['showCustomAmount'] ) {
+		$custom_amount .= sprintf(
+			'<p>%s</div>',
+			$attr['customAmountText']
+		);
+		$custom_amount .= sprintf(
+			'<div class="donations__amount donations__custom-amount">
+				%1$s
+				<div class="donations__amount-value" data-currency="%2$s"></div>
+			</div>',
+			$attr['currency'],
+			$attr['currency']
+		);
+	}
+
+	return sprintf(
+		'
+<div class="%1$s">
+	<div className="donations__container">
+	%2$s
+	<div className="donations__content">
+		<div className="donations__tab">
+			%3$s
+			<p>%4$s</p>
+			%5$s
+			%6$s
+			<hr className="donations__separator">
+			%7$s
+			%8$s
+		</div>
+	</div>
+</div>
+',
+		esc_attr( $classes ),
+		$nav,
+		$headings,
+		$attr['chooseAmountText'],
+		$amounts,
+		$custom_amount,
+		$extra_text,
+		$buttons
+	);
 }
 
 /**
