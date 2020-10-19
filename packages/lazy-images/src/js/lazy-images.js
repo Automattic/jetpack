312394f711a300f71872027ac6cc27213cc0a0fb
Brad Jorsch <anomiex@users.noreply.github.com>
2020-10-19 15:52:47 -0400

    Lazy Images: Attempt to prevent printing before images are loaded (#17490)
    * Lazy Images: Attempt to prevent printing before images are loaded
    
    There's not a whole lot we can do, as browsers don't allow us to
    actually delay or prevent the attempted print.
    
    What we can do is:
    
    * Trigger the loading of all lazy images on the page on print. They'll
      probably not load in time for the print though.
    * Attempt to present an `alert()` informing the user that images aren't
      finished loading, and that they'll want to cancel the print and try
      again.
    * Attempt to replace the printed page content with a message that
      images aren't finished loading, which will hopefully be seen in a
      "print preview" and at worst will avoid wastng ink.
    
    Fixes #9232
    
    * Add testing instructions
    
    Co-authored-by: Brandon Kraft <public@brandonkraft.com>

--- packages/lazy-images/src/js/lazy-images.js
+++ packages/lazy-images/src/js/lazy-images.js
@@ -1,12 +1,13 @@
+/* global objectL10n */
+
 const jetpackLazyImagesModule = function () {
 	const config = {
 		// If the image gets within 200px in the Y axis, start the download.
 		rootMargin: '200px 0px',
 		threshold: 0.01,
 	};
-	let images,
-		imageCount = 0,
-		observer;
+	const loadingImages = [];
+	let lazyImages, loadingWarning, observer;
 
 	lazy_load_init();
 
@@ -23,10 +24,9 @@ const jetpackLazyImagesModule = function () {
 	 * Initialize the module.
 	 */
 	function lazy_load_init() {
-		images = document.querySelectorAll(
-			'img.jetpack-lazy-image:not(.jetpack-lazy-image--handled)'
+		lazyImages = Array.from(
+			document.querySelectorAll( 'img.jetpack-lazy-image:not(.jetpack-lazy-image--handled)' )
 		);
-		imageCount = images.length;
 
 		// If initialized, then disconnect the observer
 		if ( observer ) {
@@ -35,33 +35,41 @@ const jetpackLazyImagesModule = function () {
 
 		// If we don't have support for intersection observer, loads the images immediately
 		if ( ! ( 'IntersectionObserver' in window ) ) {
-			loadImagesImmediately( images );
+			loadAllImages();
 		} else {
 			// It is supported, load the images
 			observer = new IntersectionObserver( onIntersection, config );
 
-			// foreach() is not supported in IE
-			for ( let i = 0; i < images.length; i++ ) {
-				const image = images[ i ];
-				if ( image.getAttribute( 'data-lazy-loaded' ) ) {
-					continue;
+			lazyImages.forEach( function ( image ) {
+				if ( ! image.getAttribute( 'data-lazy-loaded' ) ) {
+					observer.observe( image );
 				}
+			} );
 
-				observer.observe( image );
+			// Watch for attempts to print, and load all images. Most browsers
+			// support beforeprint, Safari needs a media listener. Doesn't hurt
+			// to double-fire if a browser supports both.
+			window.addEventListener( 'beforeprint', onPrint );
+			if ( window.matchMedia ) {
+				window.matchMedia( 'print' ).addListener( function ( mql ) {
+					if ( mql.matches ) {
+						onPrint();
+					}
+				} );
 			}
 		}
 	}
 
 	/**
 	 * Load all of the images immediately
-	 *
-	 * @param {NodeList} immediateImages - List of lazy-loaded images to load immediately.
 	 */
-	function loadImagesImmediately( immediateImages ) {
-		// foreach() is not supported in IE
-		for ( let i = 0; i < immediateImages.length; i++ ) {
-			const image = immediateImages[ i ];
-			applyImage( image );
+	function loadAllImages() {
+		if ( observer ) {
+			observer.disconnect();
+		}
+
+		while ( lazyImages.length > 0 ) {
+			applyImage( lazyImages[ 0 ] );
 		}
 	}
 
@@ -71,23 +79,53 @@ const jetpackLazyImagesModule = function () {
 	 * @param {Array} entries - List of elements being observed.
 	 */
 	function onIntersection( entries ) {
-		// Disconnect if we've already loaded all of the images
-		if ( imageCount === 0 ) {
-			observer.disconnect();
-		}
-
 		// Loop through the entries
 		for ( let i = 0; i < entries.length; i++ ) {
 			const entry = entries[ i ];
 
 			// Are we in viewport?
 			if ( entry.intersectionRatio > 0 ) {
-				imageCount--;
-
 				// Stop watching and load the image
 				observer.unobserve( entry.target );
 				applyImage( entry.target );
 			}
+		}
+
+		// Disconnect if we've already loaded all of the images
+		if ( lazyImages.length === 0 ) {
+			observer.disconnect();
+		}
+	}
+
+	/**
+	 * On print
+	 */
+	function onPrint() {
+		if ( ! loadingWarning && ( lazyImages.length > 0 || loadingImages.length > 0 ) ) {
+			// Replace the printed page with a notice that images are still loading.
+			// Hopefully the user won't actually print this, but if they do at least it'll not
+			// waste too much ink.
+			loadingWarning = document.createElement( 'div' );
+			loadingWarning.id = 'loadingWarning';
+			loadingWarning.style.fontWeight = 'bold';
+			loadingWarning.innerText = objectL10n.loading_warning;
+
+			const s = document.createElement( 'style' );
+			s.innerHTML =
+				'#loadingWarning { display: none; }\n@media print {\n#loadingWarning { display: block; }\nbody > #loadingWarning ~ * { display: none !important; }\n}';
+			loadingWarning.appendChild( s );
+
+			bodyEl.insertBefore( loadingWarning, bodyEl.firstChild );
+		}
+
+		if ( lazyImages.length > 0 ) {
+			loadAllImages();
+		}
+
+		// May as well try an alert() too. The browser may block it, but if not
+		// it could save them some trouble.
+		if ( loadingWarning ) {
+			alert( objectL10n.loading_warning );
 		}
 	}
 
@@ -125,6 +163,23 @@ const jetpackLazyImagesModule = function () {
 			image.setAttribute( 'srcset', srcset );
 		}
 
+		// Force eager loading, otherwise the browser-native loading=lazy support will still
+		// prevent the loading.
+		image.setAttribute( 'loading', 'eager' );
+
+		loadingImages.push( image );
+		const idx = lazyImages.indexOf( image );
+		if ( idx >= 0 ) {
+			lazyImages.splice( idx, 1 );
+		}
+
+		if ( image.complete ) {
+			loadedImage.call( image, null );
+		} else {
+			image.addEventListener( 'load', loadedImage, { once: true } );
+			image.addEventListener( 'error', loadedImage, { once: true } );
+		}
+
 		// Fire an event so that third-party code can perform actions after an image is loaded.
 		try {
 			lazyLoadedImageEvent = new Event( 'jetpack-lazy-loaded-image', {
@@ -137,6 +192,21 @@ const jetpackLazyImagesModule = function () {
 		}
 
 		image.dispatchEvent( lazyLoadedImageEvent );
+	}
+
+	/**
+	 * An image from applyImage() finished loading.
+	 */
+	function loadedImage() {
+		const idx = loadingImages.indexOf( this );
+		if ( idx >= 0 ) {
+			loadingImages.splice( idx, 1 );
+		}
+
+		if ( loadingWarning && lazyImages.length === 0 && loadingImages.length === 0 ) {
+			loadingWarning.parentNode.removeChild( loadingWarning );
+			loadingWarning = null;
+		}
 	}
 };
 
