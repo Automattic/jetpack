de87b01f3bc6cf4d811f607dce196654e83c1850
Mario Zorz <mariozorz@gmail.com>
2020-10-20 08:06:55 -0300

    added MediaPlaceholder, wrapped in a pointerEvents=none View so we can handle media picking from the StoryComposer

--- extensions/blocks/story/edit.native.js
+++ extensions/blocks/story/edit.native.js
@@ -7,7 +7,7 @@ import { Text, View, TouchableWithoutFeedback } from 'react-native';
  * WordPress dependencies
  */
 import { Image } from '@wordpress/components';
-import { BlockMediaUpdateProgress } from '@wordpress/block-editor';
+import { BlockIcon, MediaPlaceholder, BlockMediaUpdateProgress } from '@wordpress/block-editor';
 import { __, sprintf } from '@wordpress/i18n';
 import { getProtocol } from '@wordpress/url';
 import { doAction, hasAction } from '@wordpress/hooks';
@@ -23,6 +23,7 @@ import {
 /**
  * Internal dependencies
  */
+import { icon } from '.';
 import styles from './editor.scss';
 import StoryEditingButton from './story-editing-button';
 
@@ -249,6 +250,22 @@ class StoryEdit extends React.Component {
 		const hasContent = !! mediaFiles.length;
 		const { isUploadInProgress, isSaveInProgress, didUploadFail, didSaveFail } = this.state;
 
+		const mediaPlaceholder = (
+			// TODO this we are wrapping in a pointerEvents=none because we don't want to
+			// trigger the ADD MEDIA bottom sheet just yet, but only give the placedholder the right appearance.
+			<View pointerEvents="none" style={ styles[ 'wp-story-container' ] }>
+				<MediaPlaceholder
+					icon={ <BlockIcon icon={ icon } /> }
+					labels={ {
+						title: __( 'Story' ),
+						instructions: __( 'ADD MEDIA' ),
+					} }
+					allowedTypes={ [ 'image', 'video' ] }
+					onFocus={ this.props.onFocus }
+				/>
+			</View>
+		);
+
 		return (
 			<TouchableWithoutFeedback
 				accessible={ ! isSelected }
@@ -256,11 +273,7 @@ class StoryEdit extends React.Component {
 				disabled={ ! isSelected }
 			>
 				<View style={ styles[ 'wp-story-container' ] }>
-					{ ! hasContent && (
-						<Text style={ styles[ 'wp-story-wrapper' ] }>
-							This Story is empty. Tap to add media.
-						</Text>
-					) }
+					{ ! hasContent && mediaPlaceholder }
 					{ hasContent && (
 						<View style={ { flex: 1 } }>
 							{ ! isUploadInProgress && ! isSaveInProgress && isSelected && (
