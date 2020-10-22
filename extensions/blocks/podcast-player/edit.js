9b363169c01488c7c6eb429cbf4a24828ee60dea
Marek Hrabe <marekhrabe@me.com>
2020-10-22 11:48:41 +0200

    Podcast Player: Show error from API (#17537)

--- extensions/blocks/podcast-player/edit.js
+++ extensions/blocks/podcast-player/edit.js
@@ -124,7 +124,10 @@ const PodcastPlayerEdit = ( {
 					// Show error and allow to edit URL.
 					debug( 'feed error', error );
 					createErrorNotice(
-						__( "Your podcast couldn't be embedded. Please double check your URL.", 'jetpack' )
+						// Error messages already come localized.
+						error.message ||
+							// Fallback to a generic message.
+							__( "Your podcast couldn't be embedded. Please double check your URL.", 'jetpack' )
 					);
 					setIsEditing( true );
 				}
