8a413cb4547569b0df315db474e5135ae6e74e36
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-10-23 14:06:05 +0300

    fix wpcom login

--- tests/e2e/lib/pages/wpcom/login.js
+++ tests/e2e/lib/pages/wpcom/login.js
@@ -35,11 +35,21 @@ export default class LoginPage extends Page {
 		await waitAndClick( this.page, continueButtonSelector );
 
 		// sometimes it failing to type the whole password correctly. Trying to wait for the transition to happen
-		this.page.waitFor( 500 );
-		await waitAndClick( this.page, passwordSelector );
-		this.page.waitFor( 100 );
-		await waitAndType( this.page, passwordSelector, password, { delay: 10 } );
-		this.page.waitFor( 500 );
+		// await this.page.waitFor( 1000 );
+		let count = 0;
+		while ( count < 5 ) {
+			await waitAndType( this.page, passwordSelector, password, { delay: 10 } );
+			const passwordEl = await this.page.$( passwordSelector );
+			await page.focus( submitButtonSelector );
+
+			const fieldValue = await page.evaluate( x => x.value, passwordEl );
+			if ( fieldValue === password ) {
+				break;
+			}
+			logger.info( `Failed to type password properly. retrying...` );
+
+			count += 1;
+		}
 
 		const submitButton = await waitForSelector( this.page, submitButtonSelector );
 		await submitButton.press( 'Enter' );
@@ -47,7 +57,7 @@ export default class LoginPage extends Page {
 		try {
 			await waitForSelector( this.page, this.expectedSelector, {
 				hidden: true,
-				timeout: 60000 /* 1 minute */,
+				timeout: 30000 /* 1 minute */,
 			} );
 		} catch ( e ) {
 			if ( retry === true ) {
@@ -62,6 +72,6 @@ export default class LoginPage extends Page {
 
 	async isLoggedIn() {
 		const continueAsUserSelector = '#content .continue-as-user';
-		return await isEventuallyVisible( this.page, continueAsUserSelector, 4000 );
+		return await isEventuallyVisible( this.page, continueAsUserSelector, 2000 );
 	}
 }
