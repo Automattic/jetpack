bd5869b833a40552008092d0a960cd0fea5cd4db
Jeremy Herve <jeremy@jeremy.hu>
2020-11-03 18:22:06 +0100

    Simplify test with data provider

--- tests/php/extensions/blocks/test-class.pinterest.php
+++ tests/php/extensions/blocks/test-class.pinterest.php
@@ -13,77 +13,69 @@ require_once JETPACK__PLUGIN_DIR . '/extensions/blocks/pinterest/pinterest.php';
  */
 class WP_Test_Pinterest extends \WP_UnitTestCase {
 	/**
-	 * `pin_type` with null URL
+	 * Test the Pin type detected for a given Pinterest URL.
+	 *
+	 * @covers Automattic\Jetpack\Extensions\Pinterest::pin_type
+	 * @dataProvider get_pinterest_urls
+	 *
+	 * @since 9.2.0
+	 *
+	 * @param null|string $url      Pinterest URL.
+	 * @param string      $expected Pinterest pin type.
 	 */
-	public function test_pin_type_with_null_url() {
-		$pin_type = Pinterest\pin_type( null );
-		$this->assertSame( '', $pin_type );
+	public function test_pin_type( $url, $expected ) {
+		$pin_type = Pinterest\pin_type( $url );
+
+		$this->assertSame( $expected, $pin_type );
 	}
 
 	/**
-	 * `pin_type` with empty string URL
+	 * URL variations to be used by the Pinterest block.
+	 *
+	 * @covers Automattic\Jetpack\Extensions\Pinterest::pin_type
 	 */
-	public function test_pin_type_with_empty_url() {
-		$pin_type = Pinterest\pin_type( '' );
-		$this->assertSame( '', $pin_type );
-	}
-
-	/**
-	 * `pin_type` with invalid URL
-	 */
-	public function test_pin_type_with_invalid_url() {
-		$pin_type = Pinterest\pin_type( 'abcdefghijk' );
-		$this->assertSame( '', $pin_type );
-
-		$pin_type = Pinterest\pin_type( 'file://www.pinterest.com/pin/12345' );
-		$this->assertSame( '', $pin_type );
-	}
-
-	/**
-	 * `pin_type` with invalid subdomain to Pinterest URL
-	 */
-	public function test_pin_type_with_invalid_subdomain_url() {
-		$pin_type = Pinterest\pin_type( 'https://abc.pinterest.com/pin/12345' );
-		$this->assertSame( '', $pin_type );
-	}
-
-	/**
-	 * `pin_type` with invalid path in Pinterest URL
-	 */
-	public function test_pin_type_with_invalid_path() {
-		$pin_type = Pinterest\pin_type( 'https://www.pinterest.com/' );
-		$this->assertSame( '', $pin_type );
-	}
-
-	/**
-	 * `pin_type` with www subdomain to valid Pinterest URL
-	 */
-	public function test_pin_type_with_www_subdomain() {
-		$pin_type = Pinterest\pin_type( 'https://www.pinterest.com/pin/12345' );
-		$this->assertSame( 'embedPin', $pin_type );
-	}
-
-	/**
-	 * `pin_type` with locale subdomain to valid Pinterest URL
-	 */
-	public function test_pin_type_with_locale_subdomain() {
-		$pin_type = Pinterest\pin_type( 'https://in.pinterest.com/pin/12345' );
-		$this->assertSame( 'embedPin', $pin_type );
-	}
-
-	/**
-	 * `pin_type` with username in Pinterest URL
-	 */
-	public function test_pin_type_with_username_url() {
-		$pin_type = Pinterest\pin_type( 'https://www.pinterest.ca/foo/' );
-		$this->assertSame( 'embedUser', $pin_type );
-	}
-
-	/**
-	 * `pin_type` with username and board in Pinterest URL
-	 */
-	public function test_pin_type_with_username_and_board_url() {
-		$pin_type = Pinterest\pin_type( 'https://www.pinterest.ca/foo/bar/' );
-		$this->assertSame( 'embedBoard', $pin_type );
+	public function get_pinterest_urls() {
+		return array(
+			'null_url'               => array(
+				null,
+				'',
+			),
+			'empty_url'              => array(
+				'',
+				'',
+			),
+			'invalid_url'            => array(
+				'abcdefghijk',
+				'',
+			),
+			'invalid_protocol'       => array(
+				'file://www.pinterest.com/pin/12345',
+				'',
+			),
+			'invalid_subdomain_url'  => array(
+				'https://abc.pinterest.com/pin/12345',
+				'',
+			),
+			'invalid_path'           => array(
+				'https://www.pinterest.com/',
+				'',
+			),
+			'www_subdomain'          => array(
+				'https://www.pinterest.com/pin/12345',
+				'embedPin',
+			),
+			'locale_subdomain'       => array(
+				'https://in.pinterest.com/pin/12345',
+				'embedPin',
+			),
+			'username_url'           => array(
+				'https://www.pinterest.ca/foo/',
+				'embedUser',
+			),
+			'username_and_board_url' => array(
+				'https://www.pinterest.ca/foo/bar/',
+				'embedBoard',
+			),
+		);
 	}
 }
