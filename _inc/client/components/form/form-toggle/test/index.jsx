68aa701081d8606d0fe82df2ebfaab9f5a0799ed
Jeremy Herve <jeremy@jeremy.hu>
2020-11-03 09:00:10 +0100

    Remove compact toggle

--- _inc/client/components/form/form-toggle/test/index.jsx
+++ _inc/client/components/form/form-toggle/test/index.jsx
@@ -9,22 +9,7 @@ import { unique } from 'lodash';
 /**
  * Internal dependencies
  */
-import CompactFormToggle from 'components/forms/form-toggle/compact';
-
-describe( 'CompactFormToggle', function () {
-	describe( 'rendering', function () {
-		it( 'should have is-compact class', function () {
-			const toggle = TestUtils.renderIntoDocument( <CompactFormToggle /> ),
-				toggleInput = TestUtils.scryRenderedDOMComponentsWithClass( toggle, 'form-toggle' );
-
-			assert( 0 < toggleInput.length, 'a form toggle was rendered' );
-			assert(
-				toggleInput[ 0 ].getDOMNode().className.indexOf( 'is-compact' ) >= 0,
-				'is-compact class exists'
-			);
-		} );
-	} );
-} );
+import FormToggle from 'components/forms/form-toggle';
 
 describe( 'FormToggle', function () {
 	afterEach( function () {
@@ -33,22 +18,15 @@ describe( 'FormToggle', function () {
 
 	describe( 'rendering', function () {
 		it( 'should have form-toggle class', function () {
-			const toggle = TestUtils.renderIntoDocument( <CompactFormToggle /> ),
+			const toggle = TestUtils.renderIntoDocument( <FormToggle /> ),
 				toggleInput = TestUtils.scryRenderedDOMComponentsWithClass( toggle, 'form-toggle' );
 
 			assert( 0 < toggleInput.length, 'a form toggle was rendered' );
 		} );
 
-		it( 'should not have is-compact class', function () {
-			const toggle = TestUtils.renderIntoDocument( <CompactFormToggle /> ),
-				toggleInput = TestUtils.scryRenderedDOMComponentsWithClass( toggle, 'is-compact' );
-
-			assert( 0 === toggleInput.length, 'no form toggle with is-compact class' );
-		} );
-
 		it( 'should be checked when checked is true', function () {
 			[ true, false ].forEach( function ( bool ) {
-				const toggle = TestUtils.renderIntoDocument( <CompactFormToggle checked={ bool } /> ),
+				const toggle = TestUtils.renderIntoDocument( <FormToggle checked={ bool } /> ),
 					toggleInput = TestUtils.scryRenderedDOMComponentsWithClass( toggle, 'form-toggle' );
 
 				assert( 0 < toggleInput.length, 'a form toggle was rendered' );
@@ -61,7 +39,7 @@ describe( 'FormToggle', function () {
 
 		it( 'should not be disabled when disabled is false', function () {
 			const toggle = TestUtils.renderIntoDocument(
-					<CompactFormToggle checked={ false } disabled={ false } />
+					<FormToggle checked={ false } disabled={ false } />
 				),
 				toggleInput = TestUtils.scryRenderedDOMComponentsWithClass( toggle, 'form-toggle' );
 
@@ -74,7 +52,7 @@ describe( 'FormToggle', function () {
 
 		it( 'should be disabled when disabled is true', function () {
 			const toggle = TestUtils.renderIntoDocument(
-					<CompactFormToggle checked={ false } disabled={ true } />
+					<FormToggle checked={ false } disabled={ true } />
 				),
 				toggleInput = TestUtils.scryRenderedDOMComponentsWithClass( toggle, 'form-toggle' );
 
@@ -86,7 +64,7 @@ describe( 'FormToggle', function () {
 		} );
 
 		it( 'should have a label whose htmlFor matches the checkbox id', function () {
-			const toggle = TestUtils.renderIntoDocument( <CompactFormToggle checked={ false } /> ),
+			const toggle = TestUtils.renderIntoDocument( <FormToggle checked={ false } /> ),
 				toggleInput = TestUtils.scryRenderedDOMComponentsWithClass( toggle, 'form-toggle__switch' ),
 				toggleLabel = TestUtils.scryRenderedDOMComponentsWithTag( toggle, 'label' );
 
@@ -96,9 +74,9 @@ describe( 'FormToggle', function () {
 		it( 'should create unique ids for each toggle', function () {
 			const toggles = TestUtils.renderIntoDocument(
 					<div>
-						<CompactFormToggle checked={ false } />
-						<CompactFormToggle checked={ false } />
-						<CompactFormToggle checked={ false } />
+						<FormToggle checked={ false } />
+						<FormToggle checked={ false } />
+						<FormToggle checked={ false } />
 					</div>
 				),
 				toggleInputs = TestUtils.scryRenderedDOMComponentsWithClass( toggles, 'form-toggle' ),
