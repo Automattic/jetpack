Automattic/jetpack

commit c0b1f8b0490ba06783d3ffa557d08d1a73fdc94f

Fix memory and performance issues of the Widget Visibility module (#17434)

modules/widget-visibility/widget-conditions.php
@@ -74,6 +74,7 @@ class Jetpack_Widget_Conditions {
 			array(
 				'orderby' => 'name',
 				'who'     => 'authors',
+				'fields'  => array( 'ID', 'display_name' ),
 			)
 		);
 
@@ -132,10 +133,23 @@ class Jetpack_Widget_Conditions {
 
 		$widget_conditions_data['page'][] = array( __( 'Post type Archives:', 'jetpack' ), $widget_conditions_post_type_archives );
 
-		$pages_dropdown = preg_replace( '/<\/?select[^>]*?>/i', '', wp_dropdown_pages( array( 'echo' => false ) ) );
+		$pages = self::get_pages();
 
+		$dropdown_tree_args = array(
+			'depth'                 => 0,
+			'child_of'              => 0,
+			'selected'              => 0,
+			'echo'                  => false,
+			'name'                  => 'page_id',
+			'id'                    => '',
+			'class'                 => '',
+			'show_option_none'      => '',
+			'show_option_no_change' => '',
+			'option_none_value'     => '',
+			'value_field'           => 'ID',
+		);
+		$pages_dropdown = walk_page_dropdown_tree( $pages, 0, $dropdown_tree_args );
 		preg_match_all( '/value=.([0-9]+).[^>]*>([^<]+)</', $pages_dropdown, $page_ids_and_titles, PREG_SET_ORDER );
-
 		$static_pages = array();
 
 		foreach ( $page_ids_and_titles as $page_id_and_title ) {
@@ -187,7 +201,7 @@ class Jetpack_Widget_Conditions {
 		wp_localize_script( 'widget-conditions', 'widget_conditions_data', $widget_conditions_data );
 
 		// Save a list of the IDs of all pages that have children for dynamically showing the "Include children" checkbox.
-		$all_pages   = get_pages();
+		$all_pages   = self::get_pages();
 		$all_parents = array();
 
 		foreach ( $all_pages as $page ) {
@@ -203,6 +217,62 @@ class Jetpack_Widget_Conditions {
 		}
 
 		wp_localize_script( 'widget-conditions', 'widget_conditions_parent_pages', $all_parents );
+	}
+
+	/**
+ 	 * Retrieves a full list of all pages, containing just the IDs, post_parent, and post_title fields.
+ 	 *
+ 	 * Since the WordPress' `get_pages` function does not allow us to fetch only the fields mentioned
+ 	 * above, we need to introduce a custom method using a direct SQL query fetching those.
+ 	 *
+ 	 * By fetching only those 3 fields and not populating the object cache for all the pages, we can 
+ 	 * improve the performance of the query on sites having a lot of pages.
+ 	 *
+ 	 * @see https://core.trac.wordpress.org/ticket/51469
+ 	 *
+ 	 * @return array List of all pages on the site (stdClass objects containing ID, post_title, and post_parent only).
+ 	 */
+	public static function get_pages() {
+		global $wpdb;
+
+		$last_changed = wp_cache_get_last_changed( 'posts' );
+		$cache_key    = "get_pages:$last_changed";
+		$pages        = wp_cache_get( $cache_key, 'widget_conditions' );
+		if ( false === $pages ) {
+			$pages = $wpdb->get_results( "SELECT {$wpdb->posts}.ID, {$wpdb->posts}.post_parent, {$wpdb->posts}.post_title FROM {$wpdb->posts} WHERE {$wpdb->posts}.post_type = 'page' AND {$wpdb->posts}.post_status = 'publish' ORDER BY {$wpdb->posts}.post_title ASC" );
+			wp_cache_set( $cache_key, $pages, 'widget_conditions' );
+		}
+
+		// Copy-pasted from the get_pages function. For usage in the `widget_conditions_get_pages` filter.
+		$parsed_args = array(
+			'child_of'     => 0,
+			'sort_order'   => 'ASC',
+			'sort_column'  => 'post_title',
+			'hierarchical' => 1,
+			'exclude'      => array(),
+			'include'      => array(),
+			'meta_key'     => '',
+			'meta_value'   => '',
+			'authors'      => '',
+			'parent'       => -1,
+			'exclude_tree' => array(),
+			'number'       => '',
+			'offset'       => 0,
+			'post_type'    => 'page',
+			'post_status'  => 'publish',
+		);
+
+		/**
+		 * Filters the retrieved list of pages.
+		 *
+		 * @since 9.1.0
+		 *
+		 * @module widget-visibility
+		 *
+		 * @param stdClass[] $pages       Array of objects containing only the ID, post_parent, and post_title fields.
+		 * @param array      $parsed_args Array of get_pages() arguments.
+		 */
+		return apply_filters( 'jetpack_widget_visibility_get_pages', $pages, $parsed_args );
 	}
 
 	/**

commit 812e2bd7cc0876fc10d596b0be0add92ebe7043d

Widget conditions: fix indentation issues introduced in #17434

modules/widget-visibility/widget-conditions.php
@@ -238,18 +238,18 @@ class Jetpack_Widget_Conditions {
 	}
 
 	/**
- 	 * Retrieves a full list of all pages, containing just the IDs, post_parent, and post_title fields.
- 	 *
- 	 * Since the WordPress' `get_pages` function does not allow us to fetch only the fields mentioned
- 	 * above, we need to introduce a custom method using a direct SQL query fetching those.
- 	 *
- 	 * By fetching only those 3 fields and not populating the object cache for all the pages, we can 
- 	 * improve the performance of the query on sites having a lot of pages.
- 	 *
- 	 * @see https://core.trac.wordpress.org/ticket/51469
- 	 *
- 	 * @return array List of all pages on the site (stdClass objects containing ID, post_title, and post_parent only).
- 	 */
+	 * Retrieves a full list of all pages, containing just the IDs, post_parent, and post_title fields.
+	 *
+	 * Since the WordPress' `get_pages` function does not allow us to fetch only the fields mentioned
+	 * above, we need to introduce a custom method using a direct SQL query fetching those.
+	 *
+	 * By fetching only those 3 fields and not populating the object cache for all the pages, we can
+	 * improve the performance of the query on sites having a lot of pages.
+	 *
+	 * @see https://core.trac.wordpress.org/ticket/51469
+	 *
+	 * @return array List of all pages on the site (stdClass objects containing ID, post_title, and post_parent only).
+	 */
 	public static function get_pages() {
 		global $wpdb;
 