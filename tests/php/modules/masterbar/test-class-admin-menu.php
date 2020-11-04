d7fca1c9aab9963dd90b7fcd057670a082e2d20b
Konstantin Obenland <obenland@gmx.de>
2020-11-04 14:37:43 -0800

    Make unit tests work

--- tests/php/modules/masterbar/test-class-admin-menu.php
+++ tests/php/modules/masterbar/test-class-admin-menu.php
@@ -8,6 +8,7 @@
 use Automattic\Jetpack\Admin_Menu;
 
 require_jetpack_file( 'modules/masterbar/class-admin-menu.php' );
+require_jetpack_file( 'tests/php/modules/masterbar/data/admin-menu.php' );
 
 /**
  * Class Test_Admin_Menu
@@ -15,12 +16,44 @@ require_jetpack_file( 'modules/masterbar/class-admin-menu.php' );
  * @coversDefaultClass Automattic\Jetpack\Admin_Menu
  */
 class Test_Admin_Menu extends WP_UnitTestCase {
+
+	/**
+	 * Menu data fixture.
+	 *
+	 * @var array
+	 */
+	public static $menu_data;
+
+	/**
+	 * Submenu data fixture.
+	 *
+	 * @var array
+	 */
+	public static $submenu_data;
+
+	/**
+	 * Create shared fixtures.
+	 */
+	public static function wpSetUpBeforeClass() {
+		global $menu, $submenu;
+
+		static::$menu_data    = $menu;
+		static::$submenu_data = $submenu;
+
+		// Set up actions.
+		Admin_Menu::get_instance();
+
+		// Execute actions.
+		do_action( 'admin_menu' );
+	}
+
 	/**
 	 * Test_Admin_Menu.
 	 */
 	public function test_admin_menu_output() {
-		$this->markTestIncomplete();
+		global $menu, $submenu;
 
-		Admin_Menu::get_instance();
+		$this->assertEquals( static::$menu_data[80], $menu[80], 'Settings menu should stay the same.' );
+		$this->assertEquals( static::$submenu_data[''], $submenu[''], 'Submenu items without parent should stay the same.' );
 	}
 }
