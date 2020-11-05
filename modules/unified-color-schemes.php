c8fe0b8f50d53ebf1bb24d219fe2bb017072c326
Dave Smith <getdavemail@gmail.com>
2020-11-05 10:26:08 +0000

    Add error handling

--- modules/unified-color-schemes.php
+++ modules/unified-color-schemes.php
@@ -4,20 +4,25 @@ use Automattic\Jetpack\Connection\Client;
 
 function ucs_admin_color_override() {
 
+	$default_color_scheme = 'default';
+
 	$response = Automattic\Jetpack\Connection\Client::wpcom_json_api_request_as_user(
-		'me/preferences', // path
-		'2', // REST API version
+		'me/preferences',
+		'2',
 		array(
 			'method' => 'GET',
 		),
-		null, // body
-		'wpcom' // REST API root. Default is `wpcom`.
+		null,
+		'wpcom'
 	);
+
+	if ( 200 !== wp_remote_retrieve_response_code( $response ) ) {
+		return $default_color_scheme;
+	}
 
 	$response_body = json_decode( wp_remote_retrieve_body( $response ), true );
 
-
-	return $response_body['colorScheme'];
+	return ! empty( $response_body['colorScheme'] ) ? $response_body['colorScheme'] : $default_color_scheme;
 }
 
 function ucs_setup_admin() {
