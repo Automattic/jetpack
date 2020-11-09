4b8faf772cd7741980373b2412971fca2761e349
Konstantin Obenland <obenland@gmx.de>
2020-11-09 11:09:42 -0800

    Move unregistration before assertions
    Props @mdbitz

--- tests/php/sync/test_class.jetpack-sync-posts.php
+++ tests/php/sync/test_class.jetpack-sync-posts.php
@@ -627,12 +627,12 @@ class WP_Test_Jetpack_Sync_Post extends WP_Test_Jetpack_Sync_Base {
 
 		$this->sender->do_sync();
 
+		// Clean up.
+		unregister_post_type( 'snitch' );
+
 		$this->assertFalse( $this->server_replica_storage->get_post( $post_id ) );
 		$sync_event = $this->server_event_storage->get_most_recent_event( 'jetpack_sync_save_post' );
 		$this->assertFalse( $sync_event );
-
-		// Clean up.
-		unregister_post_type( 'snitch' );
 	}
 
 	/**
@@ -650,19 +650,18 @@ class WP_Test_Jetpack_Sync_Post extends WP_Test_Jetpack_Sync_Base {
 
 		$this->sender->do_sync();
 
+		// Clean up.
+		unregister_post_type( 'snitch' );
+
 		$this->assertFalse( $this->server_replica_storage->get_post( $post_id ) );
 		$sync_event = $this->server_event_storage->get_most_recent_event( 'jetpack_published_post' );
 		$this->assertFalse( $sync_event );
-
-		// Clean up.
-		unregister_post_type( 'snitch' );
 	}
 
 	/**
 	 * Tests that deleted_post events are not sent for blacklisted post_types.
 	 */
 	public function test_filters_out_blacklisted_post_types_deleted_posts() {
-
 		$args = array(
 			'public' => true,
 			'label'  => 'Snitch',
@@ -676,10 +675,10 @@ class WP_Test_Jetpack_Sync_Post extends WP_Test_Jetpack_Sync_Base {
 		$this->sender->do_sync();
 		$deleted_event = $this->server_event_storage->get_most_recent_event( 'deleted_post' );
 
-		$this->assertFalse( $deleted_event );
-
 		// Clean up.
 		unregister_post_type( 'snitch' );
+
+		$this->assertFalse( $deleted_event );
 	}
 
 	function test_filters_out_blacklisted_post_types_and_their_post_meta() {
@@ -694,29 +693,35 @@ class WP_Test_Jetpack_Sync_Post extends WP_Test_Jetpack_Sync_Base {
 
 		$this->sender->do_sync();
 
+		// Clean up.
+		unregister_post_type( 'snitch' );
+
 		$this->assertFalse( $this->server_replica_storage->get_post( $post_id ) );
 
 		$this->assertEquals( null, $this->server_replica_storage->get_metadata( 'post', $post_id, 'hello', true ) );
-
-		// Clean up.
-		unregister_post_type( 'snitch' );
 	}
 
 	function test_post_types_blacklist_can_be_appended_in_settings() {
 		register_post_type( 'filter_me', array( 'public' => true, 'label' => 'Filter Me' ) );
-
 		$post_id = $this->factory->post->create( array( 'post_type' => 'filter_me' ) );
-
 		$this->sender->do_sync();
+		unregister_post_type( 'filter_me' );
 
 		// first, show that post is being synced
 		$this->assertTrue( !! $this->server_replica_storage->get_post( $post_id ) );
 
 		Settings::update_settings( array( 'post_types_blacklist' => array( 'filter_me' ) ) );
 
+		register_post_type(
+			'filter_me',
+			array(
+				'public' => true,
+				'label'  => 'Filter Me',
+			)
+		);
 		$post_id = $this->factory->post->create( array( 'post_type' => 'filter_me' ) );
-
 		$this->sender->do_sync();
+		unregister_post_type( 'filter_me' );
 
 		$this->assertFalse( $this->server_replica_storage->get_post( $post_id ) );
 
@@ -728,14 +733,14 @@ class WP_Test_Jetpack_Sync_Post extends WP_Test_Jetpack_Sync_Base {
 		foreach( Defaults::$blacklisted_post_types as $hardcoded_blacklist_post_type ) {
 			$this->assertTrue( in_array( $hardcoded_blacklist_post_type, $setting ) );
 		}
-
-		// Clean up.
-		unregister_post_type( 'filter_me' );
 	}
 
 	function test_does_not_publicize_blacklisted_post_types() {
 		register_post_type( 'dont_publicize_me', array( 'public' => true, 'label' => 'Filter Me' ) );
 		$post_id = $this->factory->post->create( array( 'post_type' => 'dont_publicize_me' ) );
+
+		// Clean up.
+		unregister_post_type( 'dont_publicize_me' );
 
 		$this->assertTrue( apply_filters( 'publicize_should_publicize_published_post', true, get_post( $post_id ) ) );
 
@@ -746,9 +751,6 @@ class WP_Test_Jetpack_Sync_Post extends WP_Test_Jetpack_Sync_Base {
 		$good_post_id = $this->factory->post->create( array( 'post_type' => 'post' ) );
 
 		$this->assertTrue( apply_filters( 'publicize_should_publicize_published_post', true, get_post( $good_post_id ) ) );
-
-		// Clean up.
-		unregister_post_type( 'dont_publicize_me' );
 	}
 
 	function test_returns_post_object_by_id() {
@@ -1009,11 +1011,11 @@ POST_CONTENT;
 		$this->sender->do_sync();
 		$synced_post = $this->server_replica_storage->get_post( $post_id );
 
-		$this->assertEquals( '', $synced_post->post_content_filtered );
-		$this->assertEquals( '', $synced_post->post_excerpt_filtered );
-
 		// Clean up.
 		unregister_post_type( 'non_public' );
+
+		$this->assertSame( '', $synced_post->post_content_filtered );
+		$this->assertSame( '', $synced_post->post_excerpt_filtered );
 	}
 
 	function test_embed_shortcode_is_disabled_on_the_content_filter_during_sync() {
