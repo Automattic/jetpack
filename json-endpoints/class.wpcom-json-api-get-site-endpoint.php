236a2d89ba825409c0070215a9af857d0ca4094c
Ramon <ramonjd@users.noreply.github.com>
2020-11-10 19:27:10 +1100

    This comment rolls back https://github.com/Automattic/jetpack/pull/17105 and syncs changes from D52434 and D52433 (which rolled back the corresponding code in WPCOM) (#17749)

--- json-endpoints/class.wpcom-json-api-get-site-endpoint.php
+++ json-endpoints/class.wpcom-json-api-get-site-endpoint.php
@@ -129,7 +129,6 @@ class WPCOM_JSON_API_GET_Site_Endpoint extends WPCOM_JSON_API_Endpoint {
 		'jetpack_frame_nonce',
 		'page_on_front',
 		'page_for_posts',
-		'wpcom_public_coming_soon_page_id',
 		'headstart',
 		'headstart_is_fresh',
 		'ak_vp_bundle_enabled',
@@ -556,9 +555,6 @@ class WPCOM_JSON_API_GET_Site_Endpoint extends WPCOM_JSON_API_Endpoint {
 					if ( $custom_front_page ) {
 						$options[ $key ] = $site->get_page_for_posts();
 					}
-					break;
-				case 'wpcom_public_coming_soon_page_id':
-					$options[ $key ] = $site->get_wpcom_public_coming_soon_page_id();
 					break;
 				case 'headstart' :
 					$options[ $key ] = $site->is_headstart();
