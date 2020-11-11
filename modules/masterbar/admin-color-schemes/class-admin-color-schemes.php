b378c2d13b02ee82c289a508c5b7d95541461b72
Konstantin Obenland <obenland@gmx.de>
2020-11-11 14:00:39 -0800

    Masterbar: Register admin_color as user meta
    What's better than writing an API endpoint? Not writing an API endpoint.

---
+++ modules/masterbar/admin-color-schemes/class-admin-color-schemes.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Admin color schemes file.
+ *
+ * @package Jetpack
+ */
+
+namespace Automattic\Jetpack\Dashboard_Customizations;
+
+/**
+ * Unifies admin color scheme selection across WP.com sites.
+ */
+class Admin_Color_Schemes {
+
+	/**
+	 * Admin_Color_Schemes constructor.
+	 */
+	public function __construct() {
+		add_action( 'rest_api_init', array( $this, 'register_admin_color_meta' ) );
+	}
+
+	/**
+	 * Makes admin_color available in users REST API endpoint.
+	 */
+	public function register_admin_color_meta() {
+		register_meta(
+			'user',
+			'admin_color',
+			array(
+				'default'      => 'fresh',
+				'description'  => 'Slug of the admin color scheme.',
+				'single'       => true,
+				'show_in_rest' => true,
+			)
+		);
+	}
+}
