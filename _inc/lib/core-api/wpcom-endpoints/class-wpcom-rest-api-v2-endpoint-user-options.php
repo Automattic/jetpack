faddf78ac3f4f935046f8d537bcef0c3fcd144c4
Dave Smith <getdavemail@gmail.com>
2020-11-11 12:26:40 +0000

    Add ability to read a single option with error handling

--- _inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-user-options.php
+++ _inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-user-options.php
@@ -41,12 +41,33 @@ class WPCOM_REST_API_V2_Endpoint_User_Option extends WP_REST_Controller {
 			$this->rest_base . '/',
 			array(
 				array(
-					'methods'  => WP_REST_Server::READABLE,
-					'callback' => array( $this, 'get_option' ),
-					// 'permission_callback' => array( $this, 'get_option_permissions_check' ),
+					'methods'             => WP_REST_Server::READABLE,
+					'callback'            => array( $this, 'get_option' ),
+					'permission_callback' => array( $this, 'get_option_permissions_check' ),
 				),
 			)
 		);
+	}
+
+
+	/**
+	 * Checks if a given request has access to admin menus.
+	 *
+	 * @param WP_REST_Request $request Full details about the request.
+	 * @return true|WP_Error True if the request has read access for the item, WP_Error object otherwise.
+	 */
+	public function get_option_permissions_check( $request ) { // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter, VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
+
+		// https://wordpress.org/support/article/roles-and-capabilities/#read
+		if ( ! current_user_can( 'read' ) ) {
+			return new WP_Error(
+				'rest_forbidden',
+				__( 'Sorry, you are not allowed to read user options on this site.', 'jetpack' ),
+				array( 'status' => rest_authorization_required_code() )
+			);
+		}
+
+		return true;
 	}
 
 
@@ -57,8 +78,28 @@ class WPCOM_REST_API_V2_Endpoint_User_Option extends WP_REST_Controller {
 	 * @param WP_REST_Request $request Full details about the request.
 	 * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
 	 */
-	public function get_option( $request ) { // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter, VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
-		$option = get_user_option( 'admin_color' );
+	public function get_option( $request ) {
+
+		if ( empty( $request['option'] ) ) {
+			return new WP_Error(
+				'rest_forbidden',
+				__( 'You must provide a valid option.', 'jetpack' ),
+				array( 'status' => rest_authorization_required_code() )
+			);
+		}
+
+		$option_key = $request['option'];
+
+		$option = get_user_option( $option_key );
+
+		if ( false === $option ) {
+			return new WP_Error(
+				'rest_forbidden',
+				__( "No option found with key '$option_key'.", 'jetpack' ),
+				array( 'status' => rest_authorization_required_code() )
+			);
+		}
+
 		return rest_ensure_response( $option );
 	}
 }
