b214f6e6edc983f97a525e2938f179bdb59a1930
Biser Perchinkov <biser@bisko.be>
2020-11-11 18:09:15 +0200

    Update table configuration to be more precise and deterministic.
    [not verified]

--- packages/sync/src/replicastore/class-table-checksum.php
+++ packages/sync/src/replicastore/class-table-checksum.php
@@ -9,6 +9,7 @@ use WP_error;
 
 class Table_Checksum {
 	public $table           = '';
+	public $table_configuration = array();
 	public $range_field     = '';
 	public $key_fields      = array();
 	public $checksum_fields = array();
@@ -16,7 +17,7 @@ class Table_Checksum {
 
 	public $salt = '';
 
-	public $allowed_table_names = array();
+	public $allowed_tables = array();
 
 	/**
 	 * Table_Checksum constructor.
@@ -29,32 +30,73 @@ class Table_Checksum {
 	 * @param array  $checksum_fields
 	 */
 
-	public function __construct( $table, $salt = null, $range_field = null, $key_fields = null, $filter_field = null, $checksum_fields = null ) {
-		// TODO $table, $key_fields, $checksum_fields, $range_field, $filter_field,
-		$this->range_field     = $range_field;
-		$this->checksum_fields = $checksum_fields;
-		$this->key_fields      = $key_fields;
+	public function __construct( $table, $salt = null ) {
 		$this->salt            = $salt;
 
 		global $wpdb;
 
 		$this->default_tables = array(
-			'posts'              => $wpdb->posts,
-			'postmeta'           => $wpdb->postmeta,
-			'comments'           => $wpdb->comments,
-			'commentmeta'        => $wpdb->commentmeta,
-			'terms'              => $wpdb->terms,
-			'termmeta'           => $wpdb->termmeta,
-			'term_relationships' => $wpdb->term_relationships,
-			'term_taxonomy'      => $wpdb->term_taxonomy,
-			'links'              => $wpdb->links,
-			'options'            => $wpdb->options,
+			'posts'              => array(
+				'table'           => $wpdb->posts,
+				'range_field'     => 'ID',
+				'key_fields'      => array( 'ID' ),
+				'checksum_fields' => array( 'post_modified' ),
+			),
+			'postmeta'           => array(
+				'table'           => $wpdb->postmeta,
+				'range_field'     => 'post_id',
+				'key_fields'      => array( 'post_id', 'meta_key' ),
+				'checksum_fields' => array( 'meta_key', 'meta_value' ),
+			),
+			'comments'           => array(
+				'table'           => $wpdb->comments,
+				'range_field'     => 'comment_ID',
+				'key_fields'      => array( 'comment_ID' ),
+				'checksum_fields' => array( 'comment_content' ),
+			),
+			'commentmeta'        => array(
+				'table'           => $wpdb->commentmeta,
+				'range_field'     => 'comment_id',
+				'key_fields'      => array( 'comment_id', 'meta_key' ),
+				'checksum_fields' => array( 'meta_key', 'meta_value' ),
+			),
+			'terms'              => array(
+				'table'           => $wpdb->terms,
+				'range_field'     => 'term_id',
+				'key_fields'      => array( 'term_id' ),
+				'checksum_fields' => array( 'term_id', 'name', 'slug' ),
+			),
+			'termmeta'           => array(
+				'table'           => $wpdb->termmeta,
+				'range_field'     => 'term_id',
+				'key_fields'      => array( 'term_id', 'meta_key' ),
+				'checksum_fields' => array( 'meta_key', 'meta_value' ),
+			),
+			'term_relationships' => $wpdb->term_relationships, // TODO describe in the array format or add exceptions
+			'term_taxonomy'      => $wpdb->term_taxonomy, // TODO describe in the array format or add exceptions
+			'links'              => $wpdb->links, // TODO describe in the array format or add exceptions
+			'options'            => $wpdb->options, // TODO describe in the array format or add exceptions
 		);
 
-		$this->allowed_table_names = apply_filters( 'jetpack_sync_checksum_allowed_tables', $this->default_tables );
+		// TODO change filters to allow the array format
+		// TODO add get_fields or similar method to get things out of the table
+		// TODO extract this configuration in a better way, still make it work with `$wpdb` names.
+		// TODO take over the replicastore functions and move them over to this class
+		// TODO make the API work
 
-		$this->table = $this->validate_table_name( $table );
+		$this->allowed_tables = apply_filters( 'jetpack_sync_checksum_allowed_tables', $this->default_tables );
 
+		$this->table               = $this->validate_table_name( $table );
+		$this->table_configuration = $this->allowed_tables[ $table ];
+
+		$this->prepare_fields( $this->table_configuration );
+
+	}
+
+	private function prepare_fields( $table_configuration ) {
+		$this->key_fields = $table_configuration['key_fields'];
+		$this->range_field = $table_configuration['range_field'];
+		$this->checksum_fields = $table_configuration['checksum_fields'];
 	}
 
 	private function validate_table_name( $table ) {
@@ -62,13 +104,13 @@ class Table_Checksum {
 			throw new Exception( 'Invalid table name: empty' );
 		}
 
-		if ( ! array_key_exists( $table, $this->allowed_table_names ) ) {
+		if ( ! array_key_exists( $table, $this->allowed_tables ) ) {
 			throw new Exception( 'Invalid table name: not allowed' );
 		}
 
 		// TODO other checks if such are needed.
 
-		return $this->allowed_table_names[ $table ];
+		return $this->allowed_tables[$table]['table'];
 	}
 
 	private function validate_fields( $fields ) {
@@ -192,10 +234,31 @@ class Table_Checksum {
 		return $result;
 	}
 
+	public function prepare_results_for_output(&$results) {
+		// get the compound key
+		// only return range and compound key for granular results
+
+		foreach ($results as &$result) {
+			// Working on reference to save memory here.
+
+			$key = array();
+			foreach($this->key_fields as $field) {
+				$key[] = $result[$field];
+			}
+
+			$result = array(
+				'key' => implode('-', $key),
+				'checksum' => $result['checksum'],
+			);
+
+		}
+	}
+
 	public function calculate_checksum( $range_from, $range_to, $filter_values, $granular_result = false ) {
 		try {
 			$this->validate_input();
-		} catch ( Exception $ex ) {
+		}
+		catch ( Exception $ex ) {
 			return new WP_error( 'invalid_input', $ex->getMessage() );
 		}
 
@@ -203,11 +266,22 @@ class Table_Checksum {
 
 		global $wpdb;
 
-		$result = $wpdb->get_results( $query, ARRAY_A );
+		if ( ! $granular_result ) {
+			$result = $wpdb->get_row( $query, ARRAY_A );
 
-		var_dump( $result );
+			if ( ! is_array( $result ) ) {
+				return new WP_Error( 'invalid_query', "Result wasn't an array" );
+			}
 
-		// TODO fix
-		return false;
+			return array(
+				'range'    => $range_from . '-' . $range_to,
+				'checksum' => $result['checksum'],
+			);
+		} else {
+			$result = $wpdb->get_results( $query, ARRAY_A );
+			$this->prepare_results_for_output( $result );
+
+			return $result;
+		}
 	}
 }
