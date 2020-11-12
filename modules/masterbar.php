5eec7880fad0e621d91c49ab09c34e276d8639a2
Brad Jorsch <brad.jorsch@automattic.com>
2020-11-12 11:52:59 -0500

    [not verified] Modify more files for phpcs-changed behavior

--- modules/masterbar.php
+++ modules/masterbar.php
@@ -14,7 +14,7 @@
 namespace Automattic\Jetpack\Dashboard_Customizations;
 
 require __DIR__ . '/masterbar/masterbar/masterbar.php';
-require __DIR__ . '/masterbar/admin-color-schemes/class-admin-color-schemes.php';
+require dirname( __FILE__ ) . '/masterbar/admin-color-schemes/class-admin-color-schemes.php';
 
 new Masterbar();
 new Admin_Color_Schemes();
