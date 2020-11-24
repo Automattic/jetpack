2e0d21b7878f61dd66af66fbf737b531dcda50c3
Henry <66652282+krymson24@users.noreply.github.com>
2020-11-24 09:08:15 -0500

    Google Analytics: Add Support for GA4 Tags with GA4 JS (#17795)

--- tests/php/modules/google-analytics/test-class.google-analytics.php
+++ tests/php/modules/google-analytics/test-class.google-analytics.php
@@ -13,11 +13,18 @@ require_jetpack_file( 'modules/google-analytics/wp-google-analytics.php' );
 class WP_Test_Jetpack_Google_Analytics extends WP_UnitTestCase {
 
 	/**
-	 * Testing Google Analytics account.
+	 * Testing Google Analytics account (UA).
 	 *
 	 * @var string
 	 */
-	const GA_ID = 'UA-000000000-1';
+	const UA_ID = 'UA-000000000-1';
+
+	/**
+	 * Testing Google Analytics account (GA4).
+	 *
+	 * @var string
+	 */
+	const GA4_ID = 'G-XXXXXXX';
 
 	/**
 	 * Runs the routine before each test is executed.
@@ -31,7 +38,7 @@ class WP_Test_Jetpack_Google_Analytics extends WP_UnitTestCase {
 		add_filter(
 			'pre_option_jetpack_wga',
 			function( $option ) {
-				$option['code'] = self::GA_ID;
+				$option['code'] = self::UA_ID;
 				return $option;
 			}
 		);
@@ -48,7 +55,7 @@ class WP_Test_Jetpack_Google_Analytics extends WP_UnitTestCase {
 		$config_data = wp_json_encode(
 			array(
 				'vars'     => array(
-					'account' => self::GA_ID,
+					'account' => self::UA_ID,
 				),
 				'triggers' => array(
 					'trackPageview' => array(
@@ -87,6 +94,70 @@ class WP_Test_Jetpack_Google_Analytics extends WP_UnitTestCase {
 		$this->assertEquals(
 			Jetpack_Google_Analytics::amp_analytics_entries( $this->get_sample() ),
 			$this->get_sample()
+		);
+	}
+
+	/**
+	 * Verify additional gtag universal commands can run.
+	 */
+	public function test_filter_jetpack_gtag_universal_commands() {
+		// Hijack the option for Jetpack_Google_Analytics_Options::get_tracking_code().
+		add_filter(
+			'pre_option_jetpack_wga',
+			function ( $option ) {
+				$option['code'] = self::GA4_ID;
+				return $option;
+			}
+		);
+
+		// Filter being tested.
+		add_filter(
+			'jetpack_gtag_universal_commands',
+			function () {
+				return array(
+					array(
+						'event',
+						'jetpack_testing_event',
+						array(
+							'event_category' => 'somecat',
+							'event_label'    => 'somelabel',
+							'value'          => 'someval',
+						),
+					),
+					array(
+						'event',
+						'another_jetpack_testing_event',
+						array(
+							'event_category' => 'foo',
+							'event_label'    => 'bar',
+							'value'          => 'baz',
+						),
+					),
+				);
+			}
+		);
+
+		// GA code is only inserted in non-admin screens.
+		set_current_screen( 'front' );
+
+		// Mock `Jetpack_Google_Analytics_Legacy` instance to disable the constructor class.
+		$instance = $this->getMockBuilder( Jetpack_Google_Analytics_Legacy::class )
+			->setMethods( null )
+			->disableOriginalConstructor()
+			->getMock();
+
+		ob_start();
+		$instance->insert_code();
+		$actual = ob_get_clean();
+
+		$this->assertContains(
+			'gtag( "event", "jetpack_testing_event", {"event_category":"somecat","event_label":"somelabel","value":"someval"} )',
+			$actual
+		);
+
+		$this->assertContains(
+			'gtag( "event", "another_jetpack_testing_event", {"event_category":"foo","event_label":"bar","value":"baz"} );',
+			$actual
 		);
 	}
 }
