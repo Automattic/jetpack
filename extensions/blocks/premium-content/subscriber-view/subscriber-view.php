99ecd3f2251395bff1a83e3bc752af96ac847363
apeatling <apeatling@gmail.com>
2020-12-01 14:44:34 -0800

    Include the server side render functions for child blocks.

--- extensions/blocks/premium-content/subscriber-view/subscriber-view.php
+++ extensions/blocks/premium-content/subscriber-view/subscriber-view.php
@@ -10,26 +10,29 @@ namespace Automattic\Jetpack\Extensions\Premium_Content;
 use Automattic\Jetpack\Blocks;
 use Jetpack_Gutenberg;
 
-const FEATURE_NAME = 'premium-content/subscriber-view';
+const SUBSCRIBER_VIEW_NAME = 'premium-content/subscriber-view';
 
-require_once '../_inc/access-check.php';
+require_once dirname( __DIR__ ) . '/_inc/access-check.php';
 
 /**
  * Registers the block for use in Gutenberg
  * This is done via an action so that we can disable
  * registration if we need to.
  */
-function register_block() {
+function register_subscriber_view_block() {
+	// Determine required `context` key based on Gutenberg version.
+	$deprecated = function_exists( 'gutenberg_get_post_from_context' );
+	$uses       = $deprecated ? 'context' : 'uses_context';
+
 	Blocks::jetpack_register_block(
-		FEATURE_NAME,
+		SUBSCRIBER_VIEW_NAME,
 		array(
-			'render_callback' => __NAMESPACE__ . '\render_block',
-			'plan_check'	  => true,
-            $uses             => array( 'premium-content/planId' ),
-        )
+			'render_callback' => __NAMESPACE__ . '\render_subscriber_view_block',
+			$uses             => array( 'premium-content/planId' ),
+		)
 	);
 }
-add_action( 'init', __NAMESPACE__ . '\register_block' );
+add_action( 'init', __NAMESPACE__ . '\register_subscriber_view_block' );
 
 /**
  * Render callback.
@@ -39,14 +42,15 @@ add_action( 'init', __NAMESPACE__ . '\register_block' );
  *
  * @return string
  */
-function render_block( $attributes, $content, $block = null ) {
+function render_subscriber_view_block( $attributes, $content, $block = null ) {
 	if ( ! pre_render_checks() ) {
 		return '';
 	}
 
 	$visitor_has_access = current_visitor_can_access( $attributes, $block );
+
 	if ( $visitor_has_access ) {
-		Jetpack_Gutenberg::load_styles_as_required( FEATURE_NAME );
+		Jetpack_Gutenberg::load_styles_as_required( SUBSCRIBER_VIEW_NAME );
 
 		// The viewer has access to premium content, so the viewer can see the subscriber view content.
 		return $content;
