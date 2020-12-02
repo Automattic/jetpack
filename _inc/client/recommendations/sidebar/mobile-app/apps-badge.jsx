de1c0776c2d6fa3023ef6d7ded5d28804e8ba5da
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2020-12-02 13:02:24 -0600

    Add todo

--- _inc/client/recommendations/sidebar/mobile-app/apps-badge.jsx
+++ _inc/client/recommendations/sidebar/mobile-app/apps-badge.jsx
@@ -93,6 +93,7 @@ class AppsBadge extends PureComponent {
 		}
 	}
 
+	// TODO: test loading of images in non en-US context
 	loadImage() {
 		this.image = new window.Image();
 		this.image.src = this.state.imageSrc;
