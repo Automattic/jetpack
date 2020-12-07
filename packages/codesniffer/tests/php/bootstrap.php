c2ea3336565951dbf986f5670e18857afabcbb22
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2020-12-07 14:10:48 +0200

    get rid of travis-can-run check

--- packages/codesniffer/tests/php/bootstrap.php
+++ packages/codesniffer/tests/php/bootstrap.php
@@ -5,6 +5,11 @@
  * @package automattic/jetpack-codesniffer
  */
 
+if ( version_compare( PHP_VERSION, '7.2.0', '<' ) ) {
+	echo 'PHP version is too old to run tests. 7.2 is required, but ' . PHP_VERSION . " is installed.\n";
+	exit( 1 );
+}
+
 // Include the Composer autoloader.
 require_once __DIR__ . '/../../vendor/autoload.php';
 
