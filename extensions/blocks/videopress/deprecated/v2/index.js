04bd58996129e6ddfd0eafd23013da1c102d23dc
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2020-12-08 18:47:48 +1000

    Fix application of videopress block classes
    * Fixes missing alignment CSS classes
    * Fixes deprecations after Gutenberg introduction of wp-block-video CSS class
    * Fixes bug introduced in prior patch addressing aspect ratio classes

---
+++ extensions/blocks/videopress/deprecated/v2/index.js
@@ -0,0 +1,78 @@
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
+ * Internal dependencies
+ */
+import save from './save';
+
+// This deprecation is a result of switching to useBlockProps in save to allow
+// Gutenberg align hook to inject CSS alignment class e.g. `alignwide`.
+export default {
+	attributes: {
+		autoplay: {
+			type: 'boolean',
+		},
+		caption: {
+			type: 'string',
+			source: 'html',
+			selector: 'figcaption',
+		},
+		controls: {
+			type: 'boolean',
+			default: true,
+		},
+		guid: {
+			type: 'string',
+		},
+		id: {
+			type: 'number',
+		},
+		loop: {
+			type: 'boolean',
+		},
+		muted: {
+			type: 'boolean',
+		},
+		playsInline: {
+			type: 'boolean',
+		},
+		poster: {
+			type: 'string',
+		},
+		preload: {
+			type: 'string',
+			default: 'metadata',
+		},
+		src: {
+			type: 'string',
+		},
+		classNames: {
+			type: 'string',
+		},
+		align: {
+			type: 'wide',
+		},
+	},
+	support: {
+		reusable: false,
+	},
+	isEligible: ( { className, classNames, guid } ) => {
+		const missingClassNames = classNames === undefined;
+		const missingVideoClass = ! className?.indexOf( 'wp-block-video' ) >= 0;
+
+		return guid && ( missingClassNames || missingVideoClass );
+	},
+	migrate: attributes => {
+		const { align, className, classNames } = attributes;
+		return {
+			...attributes,
+			className: 'wp-block-video',
+			classNames: classnames( className, classNames, { [ `align${ align }` ]: align } ),
+		};
+	},
+	save,
+	isDeprecation: true,
+};
