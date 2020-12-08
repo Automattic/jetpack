1ea502e5e11b7c0389313c0095fcf17578777193
apeatling <apeatling@gmail.com>
2020-12-08 13:38:31 -0800

    Keep deprectated setup consistent across blocks.

--- extensions/blocks/premium-content/subscriber-view/deprecated/v1/index.js
+++ extensions/blocks/premium-content/subscriber-view/deprecated/v1/index.js
@@ -1,10 +1,8 @@
-export default [
-	{
-		attributes: {},
-		supports: {
-			inserter: false,
-			html: false,
-		},
-		save: () => <div className="wp-block-premium-content-subscriber-view"></div>,
+export default {
+	attributes: {},
+	supports: {
+		inserter: false,
+		html: false,
 	},
-];
+	save: () => <div className="wp-block-premium-content-subscriber-view"></div>,
+};
