4e2bc61eb6f7ed302b2e018c8c43d1b958b7c4ba
Matthew Denton <matt@mdbitz.com>
2020-12-09 11:49:05 -0500

    get_range_edges Integration Tests

--- tests/php/sync/test_class.jetpack-sync-checksum.php
+++ tests/php/sync/test_class.jetpack-sync-checksum.php
@@ -71,7 +71,7 @@ class WP_Test_Jetpack_Sync_Checksum extends WP_UnitTestCase {
 		global $wpdb;
 
 		return array(
-			array (
+			array(
 				array(
 					'posts' => array(
 						'table'           => $wpdb->posts,
@@ -84,7 +84,7 @@ class WP_Test_Jetpack_Sync_Checksum extends WP_UnitTestCase {
 				true,
 				null,
 			),
-			array (
+			array(
 				array(
 					'posts' => array(
 						'table'           => $wpdb->posts,
@@ -97,7 +97,7 @@ class WP_Test_Jetpack_Sync_Checksum extends WP_UnitTestCase {
 				false,
 				'ID!',
 			),
-			array (
+			array(
 				array(
 					'posts' => array(
 						'table'           => $wpdb->posts,
@@ -110,7 +110,7 @@ class WP_Test_Jetpack_Sync_Checksum extends WP_UnitTestCase {
 				false,
 				'post_modified_gmt*/'
 			),
-			array (
+			array(
 				array(
 					'posts' => array(
 						'table'           => $wpdb->posts,
@@ -179,7 +179,7 @@ class WP_Test_Jetpack_Sync_Checksum extends WP_UnitTestCase {
 		global $wpdb;
 
 		return array(
-			array (
+			array(
 				array(
 					'posts' => array(
 						'table'           => $wpdb->posts,
@@ -192,7 +192,7 @@ class WP_Test_Jetpack_Sync_Checksum extends WP_UnitTestCase {
 				true,
 				null,
 			),
-			array (
+			array(
 				array(
 					'posts' => array(
 						'table'           => $wpdb->posts,
@@ -205,7 +205,7 @@ class WP_Test_Jetpack_Sync_Checksum extends WP_UnitTestCase {
 				false,
 				'ID_2',
 			),
-			array (
+			array(
 				array(
 					'posts' => array(
 						'table'           => $wpdb->posts,
@@ -218,7 +218,7 @@ class WP_Test_Jetpack_Sync_Checksum extends WP_UnitTestCase {
 				false,
 				'post_modified_gmt_2'
 			),
-			array (
+			array(
 				array(
 					'posts' => array(
 						'table'           => $wpdb->posts,
@@ -264,6 +264,241 @@ class WP_Test_Jetpack_Sync_Checksum extends WP_UnitTestCase {
 			$this->assertSame( $result->get_error_message(), $expected_message );
 		} else {
 			$this->assertFalse( is_wp_error( $result ) );
+		}
+
+	}
+
+	/**
+	 * Data Provider for get_field_ranges_posts tests.
+	 *
+	 * @return int[][]
+	 */
+	public function get_field_ranges_posts_provider() {
+		/*
+		 * Data Format.
+		 *
+		 * param 1 -> # of posts to create.
+		 * param 2 -> # at which to create disallowed post.
+		 * param 3 -> expected item count.
+		 *
+		 */
+		return array(
+			array(
+				0,
+				-1,
+				0,
+			),
+			array(
+				1,
+				1,
+				0,
+			),
+			array(
+				1,
+				-1,
+				1,
+			),
+			array(
+				10,
+				-1,
+				10,
+			),
+			array(
+				5,
+				1,
+				4,
+			),
+			array(
+				20,
+				10,
+				19,
+			),
+			array(
+				16,
+				16,
+				15,
+			),
+		);
+	}
+
+	/**
+	 * Verify get_range_edges returns expected values for Posts.
+	 *
+	 * @dataProvider get_field_ranges_posts_provider
+	 *
+	 * @param int $num_posts Number of Posts to Generate.
+	 * @param int $disallow_index Index of generated post to be of disallowed post_type.
+	 * @param int $expected_count $expected number of posts to be returned.
+	 */
+	public function test_get_range_edges_posts( $num_posts, $disallow_index, $expected_count ) {
+
+		// Generate Test Content.
+		$user_id            = $this->factory->user->create();
+		$min_range_expected = null;
+		$max_range_expected = null;
+
+		for ( $i = 1; $i <= $num_posts; $i ++ ) {
+			if ( $disallow_index === $i ) {
+				// create a disallowed post_type post.
+				$post_id = $this->factory->post->create(
+					array(
+						'post_author' => $user_id,
+						'post_type'   => 'snitch',
+					)
+				);
+			} else {
+				// create an allowed post_type post.
+				$post_id = $this->factory->post->create( array( 'post_author' => $user_id ) );
+				if ( is_null( $min_range_expected ) ) {
+					$min_range_expected = $post_id; // set initial post_id.
+				}
+				$max_range_expected = $post_id; // update last post_id.
+			}
+		}
+
+		// Get Range Edges.
+		$tc    = new Table_Checksum( 'posts' );
+		$range = $tc->get_range_edges();
+
+		$this->assertSame( $expected_count, (int) $range['item_count'] );
+
+		// Verify min/max of range only when we have results.
+		if ( 0 !== $expected_count ) {
+			$this->assertSame( $min_range_expected, (int) $range['min_range'] );
+			$this->assertSame( $max_range_expected, (int) $range['max_range'] );
+		}
+
+	}
+
+	/**
+	 * Data Provider for get_field_ranges_posts_args tests.
+	 *
+	 * @return int[][]
+	 */
+	public function get_field_ranges_posts_args_provider() {
+		/*
+		 * Data Format.
+		 *
+		 * param 1 -> # of posts to create.
+		 * param 2 -> expected item count to be returned.
+		 * param 3 -> offset to be passed to range_from of get_field_ranges.
+		 * param 4 -> offset to be passed to range_to of get_field_ranges.
+		 * param 5 -> limit to be passed to get_field_ranges.
+		 *
+		 */
+		return array(
+			array( // limit less than # of posts.
+				10,
+				5,
+				null,
+				null,
+				5,
+			),
+			array( // limit higher than # of posts.
+				5,
+				5,
+				null,
+				null,
+				10,
+			),
+			array( // range_from lower than first post.
+				5,
+				5,
+				-20,
+				null,
+				null,
+			),
+			array( // range_to higher than last post.
+				10,
+				10,
+				null,
+				20,
+				null,
+			),
+			array( // range_from excludes first 5.
+				10,
+				5,
+				5,
+				null,
+				null,
+			),
+			array( // range_to excluded last 5.
+				10,
+				5,
+				null,
+				-5,
+				null,
+			),
+			array( // range excludes post at start and end.
+				10,
+				8,
+				1,
+				-1,
+				null,
+			),
+			array( // range_from is after any posts.
+				5,
+				0,
+				30,
+				null,
+				null,
+			),
+			array( // range_to is before any posts.
+				5,
+				0,
+				null,
+				-10,
+				null,
+			),
+		);
+	}
+
+	/**
+	 * Test that Range parameters modify returned results.
+	 *
+	 * @dataProvider get_field_ranges_posts_args_provider
+	 *
+	 * @param int      $num_posts Number of Posts to Generate.
+	 * @param int      $expected_item_count Expected item_count to return in range.
+	 * @param int|null $range_from_offset Offset to set on the range_from based on first post.
+	 * @param int|null $range_to_offset Offset to set on the range_to based on last post.
+	 * @param int|null $limit limit to be passed to get_range_edges.
+	 */
+	public function test_get_range_edges_posts_args( $num_posts, $expected_item_count, $range_from_offset, $range_to_offset, $limit ) {
+
+		// Generate Test Content.
+		$user_id            = $this->factory->user->create();
+		$min_range_expected = null;
+		$max_range_expected = null;
+
+		for ( $i = 1; $i <= $num_posts; $i ++ ) {
+			$post_id = $this->factory->post->create( array( 'post_author' => $user_id ) );
+			if ( is_null( $min_range_expected ) ) {
+				$min_range_expected = $post_id; // set initial post_id.
+			}
+			$max_range_expected = $post_id; // update last post_id.
+		}
+
+		// Update offsets.
+		$range_from = null;
+		$range_to   = null;
+		if ( ! is_null( $range_from_offset ) ) {
+			$range_from = $min_range_expected + $range_from_offset;
+		}
+		if ( ! is_null( $range_to_offset ) ) {
+			$range_to = $max_range_expected + $range_to_offset;
+		}
+
+		// Get Range Edges.
+		$tc    = new Table_Checksum( 'posts' );
+		$range = $tc->get_range_edges( $range_from, $range_to, $limit );
+
+		$this->assertSame( $expected_item_count, (int) $range['item_count'] );
+		if ( ! is_null( $range_from ) && $expected_item_count > 0 ) {
+			$this->assertGreaterThanOrEqual( $range_from, (int) $range['min_range'] );
+		}
+		if ( ! is_null( $range_to ) && $expected_item_count > 0 ) {
+			$this->assertLessThanOrEqual( $range_to, (int) $range['max_range'] );
 		}
 
 	}
