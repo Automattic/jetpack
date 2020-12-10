d543f8ca4eec779cdd158ced4297d8659be26c0a
Marek Hrabe <marekhrabe@me.com>
2020-12-10 22:11:41 +0100

    Add Anchor.fm Badge Inserter (#17936)

---
+++ extensions/blocks/anchor-fm/editor.js
@@ -0,0 +1,56 @@
+/**
+ * External dependencies
+ */
+import { createBlock } from '@wordpress/blocks';
+import { dispatch } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import { name } from '.';
+import getJetpackExtensionAvailability from '../../shared/get-jetpack-extension-availability';
+import { waitForEditor } from '../../shared/wait-for-editor';
+
+async function insertSpotifyBadge() {
+	const { image, spotifyShowUrl } = window.Jetpack_AnchorFm;
+	if ( ! spotifyShowUrl ) {
+		return;
+	}
+
+	await waitForEditor();
+
+	dispatch( 'core/block-editor' ).insertBlock(
+		createBlock( 'core/image', {
+			url: image,
+			linkDestination: 'none',
+			href: spotifyShowUrl,
+			align: 'center',
+			width: 165,
+			height: 40,
+			className: 'is-spotify-podcast-badge',
+		} ),
+		0,
+		undefined,
+		false
+	);
+}
+
+function initAnchor() {
+	const isExtensionAvailable = getJetpackExtensionAvailability( name )?.available;
+	if ( ! isExtensionAvailable ) {
+		return;
+	}
+
+	const data = window.Jetpack_AnchorFm;
+	if ( typeof data !== 'object' ) {
+		return;
+	}
+
+	switch ( data.action ) {
+		case 'insert-spotify-badge':
+			insertSpotifyBadge();
+			break;
+	}
+}
+
+initAnchor();
