48607bf1545989996f5c3481328fda78116e2051
Chris R <chris.rosser@automattic.com>
2020-12-10 17:30:23 +1300

    Jetpack Search: fix PHP notice raised when generate_widget_filter_name is called without a widget filter type (#17930)
    * Jetpack Search: fix PHP notice raised when generate_widget_filter_name is called without a widget filter type
    
    * Simplify widget filter type check
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>

--- modules/search/class.jetpack-search-helpers.php
+++ modules/search/class.jetpack-search-helpers.php
@@ -227,6 +227,10 @@ class Jetpack_Search_Helpers {
 	static function generate_widget_filter_name( $widget_filter ) {
 		$name = '';
 
+		if ( ! isset( $widget_filter['type'] ) ) {
+			return $name;
+		}
+
 		switch ( $widget_filter['type'] ) {
 			case 'post_type':
 				$name = _x( 'Post Types', 'label for filtering posts', 'jetpack' );
