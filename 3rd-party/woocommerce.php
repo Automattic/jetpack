24759177f8d50fc63a693e9ee91893bac49654bf
Brandon Kraft <public@brandonkraft.com>
2020-12-11 13:10:25 -0600

    Apply suggestions from code review
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>

--- 3rd-party/woocommerce.php
+++ 3rd-party/woocommerce.php
@@ -38,7 +38,7 @@ function jetpack_woocommerce_social_share_icons() {
 	if ( function_exists( 'sharing_display' ) ) {
 		remove_filter( 'the_content', 'sharing_display', 19 );
 		remove_filter( 'the_excerpt', 'sharing_display', 19 );
-		sharing_display( '' . true );
+		sharing_display( '', true );
 	}
 }
 
@@ -122,7 +122,7 @@ function jetpack_woocommerce_lazy_images_compat() {
 			}
 			jQuery( 'body' ).get( 0 ).dispatchEvent( jetpackLazyImagesLoadEvent );
 		} );
-	"
+		"
 	);
 }
 
