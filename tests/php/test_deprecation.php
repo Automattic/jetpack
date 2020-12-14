03540a28d9523e8a1fc573f21657c96d8e2df771
Brad Jorsch <anomiex@users.noreply.github.com>
2020-12-14 17:20:37 -0500

    Tests: Update Jetpack PHPUnit tests for PHP 8 (#17938)

--- tests/php/test_deprecation.php
+++ tests/php/test_deprecation.php
@@ -2,54 +2,19 @@
 
 class WP_Test_Jetpack_Deprecation extends WP_UnitTestCase {
 
-	private $errors;
-
-	public function setUp() {
-		parent::setUp();
-		$this->set_error_handler();
-	}
-
-	private function set_error_handler() {
-		$this->errors = array();
-		set_error_handler( array( $this, "errorHandler" ) );
-	}
-
-	public function tearDown() {
-		parent::tearDown();
-		restore_error_handler();
-	}
-
-	public function errorHandler( $errno, $errstr, $errfile, $errline, $errcontext ) {
-		$this->errors[] = compact( "errno", "errstr", "errfile",
-			"errline", "errcontext" );
-	}
-
 	/**
 	 * @dataProvider provider_deprecated_file_paths
 	 */
-	function test_deprecated_file_paths( $file_path, $replacement_path, $error_level ) {
+	public function test_deprecated_file_paths( $file_path, $replacement_path ) {
+		$mock = $this->getMockBuilder( stdClass::class )
+			->setMethods( array( 'action' ) )
+			->getMock();
+		$mock->expects( $this->once() )->method( 'action' )->with( $file_path, $replacement_path );
+
+		add_action( 'deprecated_file_included', array( $mock, 'action' ), 10, 2 );
+		add_filter( 'deprecated_file_trigger_error', '__return_false' );
+
 		require_once JETPACK__PLUGIN_DIR . $file_path;
-
-		$this->assertDeprecatedFileError( $file_path, $replacement_path, $error_level );
-	}
-
-	public function assertDeprecatedFileError( $deprecated, $replacement, $errno ) {
-		foreach ( $this->errors as $error ) {
-			if ( $error['errno'] === $errno ) {
-				// The error uses only the file name, not the path. Remove relative path if present so they can match.
-				if ( false !== strrpos( $deprecated, DIRECTORY_SEPARATOR ) ) {
-					$deprecated = substr( $deprecated, strrpos( $deprecated, DIRECTORY_SEPARATOR ) + 1 );
-				}
-
-				if ( $error['errcontext']['file'] === $deprecated && $error['errcontext']['replacement'] === $replacement ) {
-					self::assertTrue( true );
-
-					return;
-				}
-			}
-
-		}
-		$this->fail( "Error for $deprecated not found" );
 	}
 
 	/**
@@ -61,22 +26,22 @@ class WP_Test_Jetpack_Deprecation extends WP_UnitTestCase {
 
 	function provider_deprecated_method_stubs() {
 		return array(
-			array( 'JetpackTracking', 'record_user_event' ),
-			array( 'Jetpack_Client', '_wp_remote_request' ),
-			array( 'Jetpack_Client', 'remote_request' ),
-			array( 'Jetpack_Client', 'wpcom_json_api_request_as_blog' ),
-			array( 'Jetpack_Options', 'get_option' ),
-			array( 'Jetpack_Options', 'get_option_and_ensure_autoload' ),
-			array( 'Jetpack_Options', 'update_option' ),
-			array( 'Jetpack_Sync_Actions', 'initialize_listener' ),
-			array( 'Jetpack_Sync_Actions', 'initialize_sender' ),
-			array( 'Jetpack_Sync_Actions', 'sync_via_cron_allowed' ),
-			array( 'Jetpack_Sync_Modules', 'get_module' ),
-			array( 'Jetpack_Sync_Settings', 'is_syncing' ),
-			array( 'Jetpack_Sync_Settings', 'reset_data' ),
-			array( 'Jetpack_Sync_Settings', 'update_settings' ),
-			array( 'Jetpack_Tracks_Client', 'get_connected_user_tracks_identity' ),
-			array( 'Jetpack_Sync_Settings', 'is_syncing' ),
+			array( 'JetpackTracking', 'record_user_event', array( 'Bogus' ) ),
+			array( 'Jetpack_Client', '_wp_remote_request', array( 'Bogus', 'Bogus' ) ),
+			array( 'Jetpack_Client', 'remote_request', array( 'Bogus' ) ),
+			array( 'Jetpack_Client', 'wpcom_json_api_request_as_blog', array( 'Bogus' ) ),
+			array( 'Jetpack_Options', 'get_option', array( 'Bogus' ), false ),
+			array( 'Jetpack_Options', 'get_option_and_ensure_autoload', array( 'Bogus', 'Bogus' ), false ),
+			array( 'Jetpack_Options', 'update_option', array( 'Bogus', 'Bogus' ), false ),
+			array( 'Jetpack_Sync_Actions', 'initialize_listener', array() ),
+			array( 'Jetpack_Sync_Actions', 'initialize_sender', array() ),
+			array( 'Jetpack_Sync_Actions', 'sync_via_cron_allowed', array() ),
+			array( 'Jetpack_Sync_Modules', 'get_module', array( 'Bogus' ) ),
+			array( 'Jetpack_Sync_Settings', 'is_syncing', array() ),
+			array( 'Jetpack_Sync_Settings', 'reset_data', array() ),
+			array( 'Jetpack_Sync_Settings', 'update_settings', array( array( 'Bogus' => 1 ) ) ),
+			array( 'Jetpack_Tracks_Client', 'get_connected_user_tracks_identity', array(), false ),
+			array( 'Jetpack_Sync_Settings', 'is_syncing', array() ),
 		);
 	}
 
@@ -90,34 +55,24 @@ class WP_Test_Jetpack_Deprecation extends WP_UnitTestCase {
 	/**
 	 * @dataProvider provider_deprecated_method_stubs
 	 */
-	function test_deprecated_method_smoke_test( $class, $method ) {
+	public function test_deprecated_method_smoke_test( $class, $method, $arguments, $expect_notice = true ) {
+		if ( $expect_notice ) {
+			$this->setExpectedDeprecated( "$class::$method" );
+		}
+
 		$class = new ReflectionClass( $class );
 		$method = $class->getMethod( $method );
-		$parameters = $method->getParameters();
 
-		$arguments = array();
-
-		// Generating enough parameters for the method call to successfully complete.
-		foreach ( $parameters as $parameter ) {
-			if( $parameter->isDefaultValueAvailable() ) {
-
-				// No more parameters needed to successfully call the method.
-				break;
-			}
-
-			$arguments[] = 'Bogus argument';
-		}
-
+		// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_set_error_handler
+		set_error_handler( '__return_null' );
 		try {
 			$method->invokeArgs( null, $arguments );
+			$this->assertTrue( true );
 		} catch ( Error $e ) {
-			$this->fail( $class->getName() . '::' . $method->getName() . ' is throwing fatal errors.' );
-			return;
+			$this->fail( "{$class->getName()}::{$method->getName()} is throwing fatal errors.\n$e" );
+		} finally {
+			restore_error_handler();
 		}
-
-		// Marking as skipped instead of artificially passing to account for any warnings or notices
-		// the methods might throw when called with bogus arguments.
-		$this->markTestSkipped();
 	}
 
 	function provider_deprecated_defined_functions() {
@@ -131,7 +86,13 @@ class WP_Test_Jetpack_Deprecation extends WP_UnitTestCase {
 	 * @dataProvider provider_deprecated_lib_stubs
 	 */
 	function test_deprecated_lib( $lib, $functions = array() ) {
-		jetpack_require_lib( $lib );
+		// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_set_error_handler
+		set_error_handler( '__return_null' );
+		try {
+			jetpack_require_lib( $lib );
+		} finally {
+			restore_error_handler();
+		}
 		foreach ( $functions as $function ) {
 			$this->assertTrue( function_exists( $function ) );
 		}
@@ -157,12 +118,10 @@ class WP_Test_Jetpack_Deprecation extends WP_UnitTestCase {
 			array(
 				'class.jetpack-ixr-client.php',
 				'',
-				E_USER_DEPRECATED,
 			),
 			array(
 				'class.jetpack-xmlrpc-server.php',
 				'',
-				E_USER_DEPRECATED,
 			),
 		);
 	}
