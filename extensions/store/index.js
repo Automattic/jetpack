df3c9f167f08314b557246e6f97116765509c876
retrofox <rdsuarez@gmail.com>
2020-12-17 06:50:30 -0300

    store

---
+++ extensions/store/index.js
@@ -0,0 +1,127 @@
+/**
+ * External dependencies
+ */
+import { castArray } from 'lodash';
+
+/**
+ * External dependencies
+ */
+import { use, select } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import './media-player';
+
+const PLAYER_BLOCK_NAMES = [
+	'jetpack/podcast-player'
+];
+
+/**
+ * Ensure you are working with block object. This either returns the object
+ * or tries to lookup the block by id.
+ *
+ * @param {string|object} block Block object or string identifier.
+ * @returns {object} block object or an empty object if not found.
+ */
+const ensureBlockObject = ( block ) => {
+	if ( typeof block === 'object' ) {
+		return block;
+	}
+	return select( 'core/block-editor' ).getBlock( block ) || {};
+};
+
+/**
+ * This helper function walk the given blocks recursively,
+ * to trigger the state tree actions.
+ *
+ * @param {Array}    blocks -            Block instances object or an array of such objects
+ * @param {string}   blockEvent -        Event name used to track.
+ * @param {Function} propertiesHandler - Callback function to populate event properties
+ * @param {object}   parentBlock -       parent block. optional.
+ * @returns {void}
+ */
+function registerMediaAction( blocks, blockEvent, propertiesHandler = () => {}, parentBlock ) {
+	const castBlocks = castArray( blocks );
+	if ( ! castBlocks || ! castBlocks.length ) {
+		return;
+	}
+
+	castBlocks.forEach( ( block ) => {
+		// Make this compatible with actions that pass only block id, not objects.
+		block = ensureBlockObject( block );
+
+		const eventProperties = propertiesHandler( block, parentBlock );
+
+		if (
+			eventProperties?.name &&
+			PLAYER_BLOCK_NAMES.indexOf( eventProperties.name ) >= 0
+		) {
+			const blockAttributes = select( 'core/block-editor' ).getBlockAttributes( eventProperties.clientId );
+			console.log( 'blockAttributes: ', blockAttributes );
+
+			console.error( 'blockEvent: ', blockEvent );
+			console.error( 'eventProperties: ', eventProperties );
+		}
+
+		if ( block.innerBlocks && block.innerBlocks.length ) {
+			registerMediaAction( block.innerBlocks, blockEvent, propertiesHandler, block );
+		}
+	} );
+}
+
+const regiterMediaPlayer = ( blocks ) => {
+	registerMediaAction( blocks, 'onBlockInsert', ( { name, clientId } ) => ( {
+		name,
+		clientId,
+	} ) );
+};
+
+const removeMediaPlayer = ( blocks ) => {
+	registerMediaAction( blocks, 'onBlockRemove', ( { name, clientId } ) => ( {
+		name,
+		clientId,
+	} ) );
+};
+
+const replaceMediaPlayer = ( blocks ) => {
+	registerMediaAction( blocks, 'onBlockReplace', ( { name, clientId } ) => ( {
+		name,
+		clientId,
+	} ) );
+};
+
+const REDUX_TRACKING = {
+	'core/block-editor': {
+		insertBlock: regiterMediaPlayer,
+		insertBlocks: regiterMediaPlayer,
+		removeBlock: removeMediaPlayer,
+		removeBlocks: removeMediaPlayer,
+		replaceBlock: replaceMediaPlayer,
+		replaceBlocks: replaceMediaPlayer,
+	},
+};
+
+
+use( ( registry ) => ( {
+	dispatch: ( namespace ) => {
+		const actions = { ...registry.dispatch( namespace ) };
+		const storeNamespace = typeof namespace === 'string'
+			? namespace
+			: namespace?.name;
+
+		const trackers = REDUX_TRACKING[ storeNamespace ];
+
+		if ( trackers ) {
+			Object.keys( trackers ).forEach( ( actionName ) => {
+				const originalAction = actions[ actionName ];
+				const tracker = trackers[ actionName ];
+				actions[ actionName ] = ( ...args ) => {
+					tracker( ...args );
+					return originalAction( ...args );
+				};
+			} );
+		}
+		return actions;
+	},
+} ) );
---
+++ extensions/store/media-player/index.js
@@ -0,0 +1,196 @@
+/**
+ * WordPress dependencies
+ */
+import { createReduxStore, register } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+// import {
+// 	STATE_PAUSED,
+// 	STATE_PLAYING,
+// } from '../../shared/components/audio-player/constants';
+
+export const STORE_ID = 'jetpack/media-player';
+
+const DEFAULT_STATE = {
+    players: {},
+};
+
+const actions = {
+	registerMediaSource( id, playerState ) {
+		return {
+			type: 'REGISTER_MEDIA_PLAYER',
+			id,
+			playerState,
+		};
+	},
+
+	play() {
+		return {
+			type: 'PLAY_MEDIA',
+		};
+	},
+
+	stop() {
+		return {
+			type: 'STOP_MEDIA',
+		};
+	},
+
+	toggle() {
+		return {
+			type: 'TOGGLE_MEDIA',
+		};
+	},
+
+	moveTo( position ) {
+		return {
+			type: 'SET_PLAYER_POSITION',
+			position,
+		};
+	},
+
+	moveBack( ) {
+		return {
+			type: 'MOVE_PLAYER_BACK',
+		};
+	},
+
+	moveForward() {
+		return {
+			type: 'MOVE_PLAYER_FORWARD',
+		};
+	}
+};
+
+function getDefaultPlayerId( state ) {
+	if ( state?.current ) {
+		return state.current;
+	}
+
+	if ( Object.keys( state?.players )?.length ) {
+		return Object.keys( state.players )?.[ 0 ];
+	}
+
+	return null;
+}
+
+const store = createReduxStore( STORE_ID, {
+    reducer( state = DEFAULT_STATE, action ) {
+        switch ( action.type ) {
+			case 'REGISTER_MEDIA_PLAYER':
+				if ( state.players[ action?.id ] ) {
+					// do not re-register.
+					return state;
+				}
+
+				return {
+					...state,
+					current: action?.playerState?.current ? action.id : state.current,
+					players: {
+						...state.players,
+						[ action.id ]: { id: action.id, ...action.playerState, current: undefined },
+					},
+				};
+
+			case 'PLAY_MEDIA':
+				return {
+					...state,
+					players: {
+						...state.players,
+						[ state.current ]: {
+							...state.players[ state.current ],
+							isPlaying: true,
+						},
+					},
+				};
+
+			case 'STOP_MEDIA':
+				return {
+					...state,
+					players: {
+						...state.players,
+						[ state.current ]: {
+							...state.players[ state.current ],
+							isPlaying: false,
+						},
+					},
+				};
+
+			case 'TOGGLE_MEDIA':
+				return {
+					...state,
+					players: {
+						...state.players,
+						[ state.current ]: {
+							...state.players[ state.current ],
+							isPlaying: ! state.players[ state.current ].isPlaying,
+						},
+					},
+				};
+
+			case 'SET_PLAYER_POSITION':
+				return {
+					...state,
+					players: {
+						...state.players,
+						[ state.current ]: {
+							...state.players[ state.current ],
+							position: action.position,
+						},
+					},
+				};
+
+			case 'MOVE_PLAYER_BACK':
+				return {
+					...state,
+					players: {
+						...state.players,
+						[ state.current ]: {
+							...state.players[ state.current ],
+							position: state.players[ state.current ].position - 5,
+						},
+					},
+				};
+
+			case 'MOVE_PLAYER_FORWARD':
+				return {
+					...state,
+					players: {
+						...state.players,
+						[ state.current ]: {
+							...state.players[ state.current ],
+							position: state.players[ state.current ].position + 5,
+						},
+					},
+				};
+        }
+
+        return state;
+    },
+
+    actions,
+
+    selectors: {
+		isPlaying( state ) {
+			const id = getDefaultPlayerId( state );
+            return state?.players[ id ]?.isPlaying;
+		},
+
+		getPosition( state ) {
+			const id = getDefaultPlayerId( state );
+            return state?.players[ id ]?.position;
+		},
+
+		isPlayingById( state, id ) {
+            return state?.players[ id ]?.isPlaying;
+		},
+
+		getPositionById( state, id ) {
+            return state?.players[ id ]?.position;
+		},
+    },
+} );
+
+register( store );
