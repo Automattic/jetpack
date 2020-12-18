fe0217931aca49b45a7de554186f782145e2d8f1
Kim Brown <50059399+kbrown9@users.noreply.github.com>
2020-12-18 00:13:15 -0500

    Subscriptions: cast values to strings and add unit tests
    Cast the option and new setting values to strings to make sure that the comparison is correct.
    
    Add unit tests for the comment subscription setting case in Jetpack_Core_API_Data::update_data().

--- tests/php/core-api/test_class.jetpack-core-api-module-endpoints.php
+++ tests/php/core-api/test_class.jetpack-core-api-module-endpoints.php
@@ -46,4 +46,72 @@ class WP_Test_Jetpack_Core_Api_Module_Activate_Endpoint extends WP_Test_Jetpack_
 			array( '/jetpack/v4/settings/(?P<slug>[a-z\-]+)', 'POST', 'Jetpack_Core_API_Data' ),
 		);
 	}
+
+	/**
+	 * Tests the update of a comment subscription setting in the Jetpack_Core_API_Data::update_data() method.
+	 *
+	 * @param int         $new_value The new value of the comment subscription setting.
+	 * @param string|null $option_value The existing value of the comment subscription option.
+	 *
+	 * @dataProvider update_comment_subscription_option_data_provider
+	 */
+	public function test_update_data_comment_subscription_option( $new_value, $option_value ) {
+		$option_name = 'stb_enabled';
+		delete_option( $option_name );
+
+		$request = new WP_REST_Request();
+		$request->set_body_params(
+			array(
+				$option_name => $new_value,
+			)
+		);
+
+		if ( null !== $option_value ) {
+			update_option( $option_name, $option_value );
+		}
+
+		$result = ( new Jetpack_Core_API_Data() )->update_data( $request );
+
+		$this->assertInstanceOf( WP_REST_Response::class, $result );
+		$this->assertSame( 200, $result->get_status() );
+		$this->assertSame( 'success', $result->get_data()['code'] );
+	}
+
+	/**
+	 * The data provider for test_update_data_comment_subscription_option.
+	 *
+	 * @return array The test data array:
+	 *   [
+	 *     'new_value' => The new value of the comment subscription setting,
+	 *     'option_value' => The existing value of the comment subscription option
+	 *   ]
+	 */
+	public function update_comment_subscription_option_data_provider() {
+		return array(
+			'new value: int 1, option: no option' => array(
+				'new_value'    => 1,
+				'option_value' => null,
+			),
+			'new value: int 0, option: 1'         => array(
+				'new_value'    => 0,
+				'option_value' => '1',
+			),
+			'new value: int 1, option: 0'         => array(
+				'new_value'    => 1,
+				'option_value' => '0',
+			),
+			'new value: int 1, option: 1'         => array(
+				'new_value'    => 1,
+				'option_value' => '1',
+			),
+			'new value: int 0, option: 0'         => array(
+				'new_value'    => 0,
+				'option_value' => '0',
+			),
+			'new value: int 0, option: no option' => array(
+				'new_value'    => 0,
+				'option_value' => null,
+			),
+		);
+	}
 }
