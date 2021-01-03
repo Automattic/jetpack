05d1cd23f4495d84f3ae7ad2cff7d439734566c6
Brandon Kraft <public@brandonkraft.com>
2021-01-02 23:14:36 -0600

    Add basic CLI and build command

---
+++ tools/cli/cliRouter.js
@@ -0,0 +1,38 @@
+/**
+ * External dependencies
+ */
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+/**
+ * Internal dependencies
+ */
+import { buildDefine } from './commands/build';
+// import { dockerDefine } from "./commands/docker";
+
+/**
+ * The main CLI router function.
+ *
+ * This commands brings in the command definitions for the second level (e.g. jetpack SOMETHING)
+ * thus routes the command correctly.
+ */
+export async function cli() {
+	// Sets up the yargs instance.
+	let argv = yargs( hideBin( process.argv ) );
+
+	// Adds the commands to the yargs instance.
+	argv = buildDefine( argv );
+	// argv = dockerDefine( argv );
+
+	// This adds usage information on failure and demands that a subcommand must be passed.
+	argv.showHelpOnFail().demandCommand();
+
+	// Parse the args!
+	argv.argv;
+
+	// If verbose flag is set, output all of the argv info. Only applies if a command above doesn't execute.
+	if ( argv.v ) {
+		// eslint-disable-next-line no-console
+		console.log( argv );
+	}
+}
---
+++ tools/cli/helpers/promptForProject.js
@@ -0,0 +1,60 @@
+/**
+ * External dependencies
+ */
+import inquirer from 'inquirer';
+import { readdirSync } from 'fs';
+
+/**
+ * Prompt for project.
+ *
+ * If the --default flag is set, it will select the Jetpack plugin.
+ *
+ * If no project is passed via `options`, then it will prompt for the type of project and the project itself.
+ *
+ * @param {object} options - Passthrough of the argv object.
+ *
+ * @returns {object} argv object with the project property.
+ */
+export async function promptForProject( options ) {
+	const defaultProject = 'plugins/jetpack';
+	const dirs = source =>
+		readdirSync( source, { withFileTypes: true } )
+			.filter( dirent => dirent.isDirectory() )
+			.map( dirent => dirent.name );
+
+	/**
+	 * Held over from previous attempt. need to convert to new way.
+	 */
+	if ( options.default ) {
+		return {
+			...options,
+			project: options.project || defaultProject,
+		};
+	}
+
+	const questions = [];
+	let typeAnswer;
+
+	if ( ! options.project || options.project.length === 0 ) {
+		typeAnswer = await inquirer.prompt( {
+			type: 'list',
+			name: 'type',
+			message: 'What type of project are you building today?',
+			choices: [ 'packages', 'plugins' ],
+			// choices: [ 'editor-extensions', 'packages', 'plugins' ], // Swap out line above once there's editor-extensions in place.
+		} );
+		questions.push( {
+			type: 'list',
+			name: 'project',
+			message: 'Please choose which project to build',
+			choices: dirs( './projects/' + typeAnswer.type ),
+		} );
+	}
+
+	const finalAnswers = await inquirer.prompt( questions );
+
+	return {
+		...options,
+		project: options.project || typeAnswer.type + '/' + finalAnswers.project,
+	};
+}
