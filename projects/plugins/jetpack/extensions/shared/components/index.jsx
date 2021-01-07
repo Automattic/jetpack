2a1d756735ff85213df1b790333552c83381154b
Staci Cooper <staci.cooper@automattic.com>
2021-01-07 14:04:20 -0800

    Update nudge to use the same styles as editor nudge
    Updates the SSR rendered upgrade nudge for the frontend to use the
    same styling as the upgrade nudge that displays in the editor.
    Also does some refactoring in order to allow for some reuse of
    existing components.

--- projects/plugins/jetpack/extensions/shared/components/index.jsx
+++ projects/plugins/jetpack/extensions/shared/components/index.jsx
@@ -6,22 +6,31 @@ import { renderToStaticMarkup } from 'react-dom/server';
 /**
  * Internal dependencies
  */
-import { UpgradeNudge } from './upgrade-nudge';
+import { Nudge } from './upgrade-nudge';
 import { StripeNudge } from './stripe-nudge';
+import {
+	UPGRADE_NUDGE_BUTTON_TEXT,
+	UPGRADE_NUDGE_DESCRIPTION
+} from '../../extended-blocks/paid-blocks/upgrade-plan-banner';
+
 
 import './style.scss';
 
 // Use dummy props that can be overwritten by a str_replace() on the server.
 //
-// Note that we're using the 'dumb' component exported from `upgrade-nudge.jsx` and
-// 'stripe-nudge.jsx' here, rather than the 'smart' one (which is wrapped in `withSelect`
+// Note that we're using the 'dumb' component exported from `upgrade-nudge.jsx`
+// rather than the 'smart' `UpgradePlanBanner` (which contains `withSelect`
 // and `withDispatch` calls).
 // This means putting the burden of props computation on PHP (`components.php`).
 // If we wanted to use the 'smart' component instead, we'd need to provide sufficiently
 // initialised Redux state when rendering ir (probably through globals set as arguments
 // to the `StaticSiteGeneratorPlugin` call in `webpack.config.extensions.js`).
 const upgradeNudge = renderToStaticMarkup(
-	<UpgradeNudge planName="#planName#" upgradeUrl="#upgradeUrl#" />
+	<Nudge
+		checkoutUrl="#checkoutUrl#"
+		description={ UPGRADE_NUDGE_DESCRIPTION }
+		buttonText={ UPGRADE_NUDGE_BUTTON_TEXT }
+	/>
 );
 
 const stripeNudge = renderToStaticMarkup(
