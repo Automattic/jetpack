ad7b7d9aac48c2bf657da436e783e021afcc5008
Jason Moon <jason.moon@automattic.com>
2021-01-07 15:36:33 -0700

    Add additional testing and documentation

--- modules/search/instant-search/lib/filters.js
+++ modules/search/instant-search/lib/filters.js
@@ -24,6 +24,13 @@ const FILTER_KEYS = Object.freeze( [
 	'year_post_modified_gmt',
 ] );
 
+/**
+ * Returns an array of valid filter key strings.
+ *
+ * @param {object[]} widgets - Array of Jetpack Search widget objects inside the overlay sidebar.
+ * @param {object[]} widgetsOutsideOverlay - Array of Jetpack Search widget objects outside the overlay sidebar.
+ * @returns {string[]} filterKeys
+ */
 export function getFilterKeys(
 	widgets = window[ SERVER_OBJECT_NAME ]?.widgets,
 	widgetsOutsideOverlay = window[ SERVER_OBJECT_NAME ]?.widgetsOutsideOverlay
@@ -40,25 +47,48 @@ export function getFilterKeys(
 	return [ ...keys ];
 }
 
-// These filter keys are selectable from sidebar filters
+/**
+ * Returns an array of filter keys selectable from within the overlay.
+ *
+ * @param {object[]} widgets - Array of Jetpack Search widget objects inside the overlay sidebar.
+ * @returns {string[]} filterKeys
+ */
 export function getSelectableFilterKeys( widgets = window[ SERVER_OBJECT_NAME ]?.widgets ) {
 	return (
-		widgets?.map( extractFilters ).reduce( ( prev, current ) => prev.concat( current ), [] ) ?? []
+		widgets?.map( extractFilterKeys ).reduce( ( prev, current ) => prev.concat( current ), [] ) ??
+		[]
 	);
 }
 
-// These filter keys are not selectable from sidebar filters
-// In other words, they were selected via filters outside the search sidebar
+/**
+ * Returns an array of filter keys not selectable from within the overlay.
+ * In other words, they were either selected via filters outside the search sidebar or entered manually.
+ *
+ * @param {object[]} widgets - Array of Jetpack Search widget objects inside the overlay sidebar.
+ * @returns {string[]} filterKeys
+ */
 export function getUnselectableFilterKeys( widgets = window[ SERVER_OBJECT_NAME ]?.widgets ) {
 	return difference( getFilterKeys(), getSelectableFilterKeys( widgets ) );
 }
 
-function extractFilters( widget ) {
+/**
+ * Returns an array of filter keys from a given widget.
+ *
+ * @param {object} widget - a Jetpack Search widget object
+ * @returns {string[]} filterKeys
+ */
+function extractFilterKeys( widget ) {
 	return widget.filters
 		.map( mapFilterToFilterKey )
 		.filter( filterName => typeof filterName === 'string' );
 }
 
+/**
+ * Returns a filter key given a filter object.
+ *
+ * @param {object} filter - a Jetpack Search filter object
+ * @returns {string} filterKeys
+ */
 export function mapFilterToFilterKey( filter ) {
 	if ( filter.type === 'date_histogram' ) {
 		return `${ filter.interval }_${ filter.field }`;
@@ -70,8 +100,14 @@ export function mapFilterToFilterKey( filter ) {
 	return null;
 }
 
+/**
+ * Returns a filter object corresponding to the filterKey input.
+ * Inverse of `mapFilterToFilterKey`.
+ *
+ * @param {string} filterKey - filter key string to be mapped.
+ * @returns {object} filterObject
+ */
 export function mapFilterKeyToFilter( filterKey ) {
-	// TODO: Write a test for this function
 	if ( filterKey.includes( 'month' ) ) {
 		return {
 			field: filterKey.split( 'month_' ).pop(),
@@ -96,6 +132,12 @@ export function mapFilterKeyToFilter( filterKey ) {
 	};
 }
 
+/**
+ * Returns the type of the inputted filter object.
+ *
+ * @param {object} filter - filter key string to be mapped.
+ * @returns {string} output
+ */
 export function mapFilterToType( filter ) {
 	if ( filter.type === 'date_histogram' ) {
 		return 'date';
