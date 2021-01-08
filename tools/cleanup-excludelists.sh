d1a396f8f3f0ab026a24d4a9ffb0e05ce66b4831
Brad Jorsch <anomiex@users.noreply.github.com>
2021-01-08 17:32:21 -0500

    Tools: Add excludelist cleanup, and use in CI (#18242)

---
+++ tools/cleanup-excludelists.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eo pipefail
+
+cd $(dirname "${BASH_SOURCE[0]}")/..
+
+TEMP=$(mktemp --tmpdir cleanup-excludelists-XXXXXXXX)
+if [[ ! -e "$TEMP" ]]; then
+	exit 1
+fi
+trap "rm \"$TEMP\"" EXIT
+
+: > "$TEMP"
+yarn lint-file --max-warnings=0 --format=json --output-file="$TEMP" --ignore-path=<(echo '*.js'; echo '*.jsx'; jq -r '"!/" + .[]' bin/eslint-excludelist.json) . || true
+[[ -s "$TEMP" ]] && jq -e '.' < "$TEMP" >/dev/null || { echo "No JSON data found"; cat "$TEMP"; exit 1; }
+jq -r --arg pwd "$PWD/" '[ .[] | select( .messages[0] ) | .filePath | ltrimstr($pwd) ] | sort' "$TEMP" | yarn run -s prettier --parser=json > bin/eslint-excludelist.json
+
+: > "$TEMP"
+composer phpcs:lint -- -m --file-list=<(for f in $(jq -r '.[]' bin/phpcs-excludelist.json); do [[ -e "$f" ]] && echo $f; done) --report=json --report-file="$TEMP" || true
+[[ -s "$TEMP" ]] && jq -e '.' < "$TEMP" >/dev/null || { echo "No JSON data found"; cat "$TEMP"; exit 1; }
+jq -r --arg pwd "$PWD/" '[ .files | to_entries | .[] | select( .value.errors > 0 or .value.warnings > 0 ) | .key | ltrimstr($pwd) ] | sort' "$TEMP" | yarn run -s prettier --parser=json > bin/phpcs-excludelist.json
+
+git diff bin/eslint-excludelist.json bin/phpcs-excludelist.json
