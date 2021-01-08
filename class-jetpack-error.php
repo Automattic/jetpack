bb317d53bd444e233362554ede7135aa2b667276
Brandon Kraft <public@brandonkraft.com>
2021-01-08 13:31:38 -0600

    Re-add error class and formally deprecate

---
+++ class-jetpack-error.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Class Jetpack_Error
+ *
+ * @deprecated
+ *
+ * @package Jetpack
+ */
+
+/**
+ * Class Jetpack_Error
+ *
+ * @deprecated
+ */
+class Jetpack_Error extends WP_Error {}
