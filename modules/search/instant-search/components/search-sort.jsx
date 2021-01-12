3e2a797a8da9ad73e3436819ef5cdae0b508cd59
bluefuton <chris@bluefuton.com>
2021-01-12 15:58:31 +1300

    When there are more than 3 sort options, show a select

--- modules/search/instant-search/components/search-sort.jsx
+++ modules/search/instant-search/components/search-sort.jsx
@@ -4,6 +4,7 @@
  * External dependencies
  */
 import { h, Component } from 'preact';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
@@ -17,6 +18,7 @@ export default class SearchSort extends Component {
 			this.props.onChange( event.currentTarget.dataset.value );
 		}
 	};
+
 	handleClick = event => {
 		if ( this.props.value !== event.currentTarget.value ) {
 			event.preventDefault();
@@ -24,10 +26,45 @@ export default class SearchSort extends Component {
 		}
 	};
 
+	handleSelectChange = event => {
+		if ( this.props.value !== event.currentTarget.value ) {
+			event.preventDefault();
+			this.props.onChange( event.currentTarget.value );
+		}
+	};
+
 	render() {
 		const sortOptions = getSortOptions( this.props.resultFormat );
+
+		// If there are more than 3 sort options, use a select
+		if ( sortOptions.size > 3 ) {
+			return (
+				<div className="jetpack-instant-search__sort">
+					<label htmlFor="jetpack-instant-search__sort-select">
+						{ __( 'Sort by: ', 'jetpack' ) }
+					</label>
+					<select
+						id="jetpack-instant-search__sort-select"
+						onBlur={ this.handleSelectChange }
+						onChange={ this.handleSelectChange }
+					>
+						{ [ ...sortOptions.entries() ].map( ( [ sortKey, label ] ) => (
+							<option
+								value={ sortKey }
+								key={ sortKey }
+								selected={ this.props.value === sortKey ? 'selected' : '' }
+							>
+								{ label }
+							</option>
+						) ) }
+					</select>
+				</div>
+			);
+		}
+
 		return (
 			<div className="jetpack-instant-search__box-filter-order">
+				<div className="screen-reader-text">{ __( 'Sort by: ', 'jetpack' ) }</div>
 				{ [ ...sortOptions.entries() ].map( ( [ sortKey, label ] ) => (
 					<a
 						class={ `jetpack-instant-search__box-filter-option ${
