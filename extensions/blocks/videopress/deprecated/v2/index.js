43ffe09541ca05638aac0a1f71367ed9f9eff6c4
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2021-01-14 12:39:12 +1000

    Add class filtering to maintain custom CSS classes

--- extensions/blocks/videopress/deprecated/v2/index.js
+++ extensions/blocks/videopress/deprecated/v2/index.js
@@ -1,15 +1,71 @@
 /**
  * External dependencies
  */
-import classnames from 'classnames';
+import classnames from 'classnames/dedupe';
+import TokenList from '@wordpress/token-list';
 
 /**
  * Internal dependencies
  */
 import save from './save';
 
-// This deprecation is a result of switching to useBlockProps in save to allow
-// Gutenberg align hook to inject CSS alignment class e.g. `alignwide`.
+// Aspect ratios at the time of deprecation.
+const ASPECT_RATIOS = [
+	// Common video resolutions.
+	{ ratio: '2.33', className: 'wp-embed-aspect-21-9' },
+	{ ratio: '2.00', className: 'wp-embed-aspect-18-9' },
+	{ ratio: '1.77', className: 'wp-embed-aspect-16-9' },
+	{ ratio: '1.33', className: 'wp-embed-aspect-4-3' },
+	// Vertical video and instagram square video support.
+	{ ratio: '1.00', className: 'wp-embed-aspect-1-1' },
+	{ ratio: '0.56', className: 'wp-embed-aspect-9-16' },
+	{ ratio: '0.50', className: 'wp-embed-aspect-1-2' },
+];
+
+/**
+ * Filters out VideoPress specific CSS classes from the default classNames
+ * attribute, leaving any custom CSS classes alone. Then filters those custom
+ * CSS classes from the VideoPress specific class list.
+ *
+ * @param   {string} className            - CSS classes in standard className attribute.
+ * @param   {string} videoPressClassNames - VideoPress block's custom CSS classes attribute.
+ * @returns {object}                      - Filtered CSS class attributes.
+ */
+const filterVideoPressClasses = ( className, videoPressClassNames ) => {
+	// Filter out VideoPress specific styles from className attribute.
+	const aspectRatioClassNames = ASPECT_RATIOS.reduce(
+		( accumulator, { className: ratioClass } ) => {
+			accumulator[ ratioClass ] = false;
+			return accumulator;
+		},
+		{ 'wp-has-aspect-ratio': false }
+	);
+
+	const customClasses = classnames( className, {
+		...aspectRatioClassNames,
+		'wp-block-embed': false,
+		'is-type-video': false,
+		'is-provider-videopress': false,
+	} );
+
+	// Filter the custom CSS classes from the VideoPress specific class list.
+	const customClassList = new TokenList( customClasses );
+	let videoPressClasses = classnames( className, videoPressClassNames );
+
+	customClassList.forEach( customClass => {
+		videoPressClasses = videoPressClasses.replace( customClass, '' );
+	} );
+
+	return {
+		className: customClasses,
+		videoPressClassNames: videoPressClasses.trim(),
+	};
+};
+
+// This deprecation is a result of:
+// - fixing regression from Gutenberg update forcing `wp-block-video` class,
+// - bug introduced when VideoPress aspect ratio classes were added,
+// - fixing inclusion of alignment classes that were omitted previously.
 export default {
 	attributes: {
 		autoplay: {
@@ -65,13 +121,10 @@ export default {
 		return guid && missingClassNames;
 	},
 	migrate: attributes => {
-		const { align, className, videoPressClassNames } = attributes;
+		const { className, videoPressClassNames } = attributes;
 		return {
 			...attributes,
-			className: '',
-			videoPressClassNames: classnames( className, videoPressClassNames, {
-				[ `align${ align }` ]: align,
-			} ),
+			...filterVideoPressClasses( className, videoPressClassNames ),
 		};
 	},
 	save,
