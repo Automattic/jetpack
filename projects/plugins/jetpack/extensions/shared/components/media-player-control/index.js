3559e642e46fb557e15175bca3f755166a20bf14
retrofox <rdsuarez@gmail.com>
2021-01-15 09:58:49 -0300

    media-player-control: first approach

---
+++ projects/plugins/jetpack/extensions/shared/components/media-player-control/index.js
@@ -0,0 +1,58 @@
+/**
+ * WordPress dependencies
+ */
+import { ToolbarGroup, ToolbarButton } from '@wordpress/components';
+
+/**
+ * Internal dependencies
+ */
+import { ControlBackFiveIcon, ControlForwardFiveIcon } from '../../icons';
+import { STATE_PAUSED, STATE_PLAYING } from '../../../store/media-source/constants';
+import { convertSecondsToTimeCode } from './utils';
+
+function noop () {};
+
+export default function MediaPlayerControl( {
+	time,
+	state = STATE_PAUSED,
+	skipForwardTime = 5,
+	jumpBackTime = 5,
+	playIcon = 'controls-play',
+	pauseIcon = 'controls-pause',
+	isDisabled = false,
+
+	onTimeChange = noop,
+	onToggle = noop,
+} ) {
+
+	return (
+		<ToolbarGroup>
+			<ToolbarButton
+				icon={ ControlBackFiveIcon }
+				isDisabled={ isDisabled }
+				onClick={ () => onTimeChange( time - jumpBackTime ) }
+			/>
+
+			<ToolbarButton
+				icon={ state === STATE_PAUSED ? playIcon : pauseIcon }
+				isDisabled={ isDisabled }
+				onClick={ () => {
+					if ( state === STATE_PLAYING ) {
+						return onToggle && onToggle( STATE_PAUSED );
+					}
+					
+					return onToggle( STATE_PLAYING );
+				} }
+			/>
+			<ToolbarButton
+				icon={ ControlForwardFiveIcon }
+				isDisabled={ isDisabled }
+				onClick={ () => onTimeChange( time + skipForwardTime ) }
+			/>
+
+			<ToolbarButton className="media-player-control__current-time">
+				{ convertSecondsToTimeCode( time ) }
+			</ToolbarButton>
+		</ToolbarGroup>
+	);
+}
