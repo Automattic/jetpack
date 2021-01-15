6f752d71c31aa6e0d83186b82f7f34fc7a459231
Brad Jorsch <brad.jorsch@automattic.com>
2021-01-15 15:17:47 -0500

    Add more debugging support to push-all-projects.sh
    A list of the environment variables used is useful, as is output of the
    diffs that would be pushed.

--- .github/files/push-all-projects.sh
+++ .github/files/push-all-projects.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+## Environment used by this script:
+#
+# Current directory must be within the repo being mirrored from, so the commit message can be read.
+#
+# Required:
+# - BUILD_BASE: Path to the build directory, which contains "projects.txt" and directories for each repo to mirror to.
+# - GITHUB_ACTOR: GitHub username for the commit being mirrored.
+# - GITHUB_REF: Git ref being mirrored from, e.g. "refs/heads/master". Must begin with "refs/heads/".
+#
+# Other:
+# - API_TOKEN_GITHUB: Personal access token to use when accessing GitHub.
+# - CI: If unset or empty, the commits will be prepared but the actual push will not happen.
+# - GITHUB_RUN_ID: GH Actions run ID, used in the commit message.
+# - GITHUB_SHA: Head SHA1 from which to fetch the commit message for the commit being mirrored. HEAD will be assumed if not specified.
+# - USER_NAME: Git user name to use when making the commit to the mirror repo.
+# - USER_EMAIL: Email address to use when making the commit to the mirror repo. Defaults to "$USER_NAME@users.noreply.github.com"
+
 # Halt on error
 set -eo pipefail
 
@@ -31,6 +48,8 @@ elif [[ ! -s "$BUILD_BASE/projects.txt" ]]; then
 	echo "No projects were successfully built. Skipping."
 	exit 0
 fi
+
+: > "$BUILD_BASE/changes.diff"
 
 EXIT=0
 while read -r GIT_SLUG; do
@@ -77,6 +96,7 @@ while read -r GIT_SLUG; do
 		if git commit --quiet $FORCE_COMMIT --author="${COMMIT_ORIGINAL_AUTHOR}" -m "${COMMIT_MESSAGE}" &&
 			{ [[ -z "$CI" ]] || git push origin "$BRANCH"; } # Only do the actual push from the GitHub Action
 		then
+			git show --src-prefix="a/$GIT_SLUG/" --dst-prefix="b/$GIT_SLUG/" >> "$BUILD_BASE/changes.diff"
 			echo "https://github.com/$GIT_SLUG/commit/$(git rev-parse HEAD)"
 			echo "Completed $GIT_SLUG"
 		else
