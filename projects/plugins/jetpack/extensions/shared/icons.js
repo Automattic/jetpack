35cdc4acf0c5ab5473a63c34575638229bb835d6
retrofox <rdsuarez@gmail.com>
2021-01-18 15:33:37 -0300

    icons: add sync and insync icons

--- projects/plugins/jetpack/extensions/shared/icons.js
+++ projects/plugins/jetpack/extensions/shared/icons.js
@@ -216,6 +216,18 @@ export const ControlPlayInTimeIcon = (
 	</SVG>
 );
 
+export const ControlSyncIcon = (
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+		<Path d="M12 4V1L8 5l4 4V6c3.31 0 6 2.69 6 6 0 1.01-.25 1.97-.7 2.8l1.46 1.46C19.54 15.03 20 13.57 20 12c0-4.42-3.58-8-8-8zm0 14c-3.31 0-6-2.69-6-6 0-1.01.25-1.97.7-2.8L5.24 7.74C4.46 8.97 4 10.43 4 12c0 4.42 3.58 8 8 8v3l4-4-4-4v3z" />
+	</SVG>
+);
+
+export const ControlUnsyncIcon = (
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+		<Path d="M10 6.35V4.26c-.8.21-1.55.54-2.23.96l1.46 1.46c.25-.12.5-.24.77-.33zm-7.14-.94l2.36 2.36C4.45 8.99 4 10.44 4 12c0 2.21.91 4.2 2.36 5.64L4 20h6v-6l-2.24 2.24C6.68 15.15 6 13.66 6 12c0-1 .25-1.94.68-2.77l8.08 8.08c-.25.13-.5.25-.77.34v2.09c.8-.21 1.55-.54 2.23-.96l2.36 2.36 1.27-1.27L4.14 4.14 2.86 5.41zM20 4h-6v6l2.24-2.24C17.32 8.85 18 10.34 18 12c0 1-.25 1.94-.68 2.77l1.46 1.46C19.55 15.01 20 13.56 20 12c0-2.21-.91-4.2-2.36-5.64L20 4z" />
+	</SVG>
+);
+
 export const SocialServiceIcon = ( { serviceName, className } ) => {
 	const defaultProps = {
 		className: classNames( 'jetpack-gutenberg-social-icon', `is-${ serviceName }`, className ),
