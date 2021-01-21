71ace37fa3700f1a6002c7f31ff17d637914b346
Brad Jorsch <brad.jorsch@automattic.com>
2021-01-21 14:52:29 -0500

    Update check-development-environment.sh
    Use the new colorization, and add checks for jq and a modern version of
    bash.

--- tools/check-development-environment.sh
+++ tools/check-development-environment.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-grn=$'\e[1;32m'
-red=$'\e[1;31m'
-ylw=$'\e[1;33m'
-white=$'\e[0m'
-cyn=$'\e[1;36m'
+cd "$( dirname "${BASH_SOURCE[0]}" )/.."
+. tools/includes/chalk-lite.sh
 
 SUPPORT_BASE_URL=https://github.com/Automattic/jetpack/blob/master/docs/development-environment-cli-support.md
 
-function output { printf $white"$@";  }
-function success { printf $grn"$@";  }
-function danger { printf $red"$@";  }
-function info { printf $cyn"$@";  }
-function warning { printf $ylw"$@";  }
+function output { printf "$@";  }
 
 function command_exists_as_alias {
 	alias $1 2>/dev/null >/dev/null
@@ -32,8 +25,9 @@ function which {
 function command_is_available {
 	which $1 || command_exists_as_alias $1 || type $1 >/dev/null 2>/dev/null
 }
+
 function nvm_is_available {
-    [ -f ~/.nvm/nvm.sh ] && source ~/.nvm/nvm.sh && command_is_available nvm
+	[ -f ~/.nvm/nvm.sh ] && source ~/.nvm/nvm.sh && command_is_available nvm
 }
 
 function docker_is_running {
@@ -75,6 +69,10 @@ function docker_images_are_available {
 	docker_is_running && docker image ls | grep jetpack_wordpress >>/dev/null
 }
 
+function bash_version_is_proper {
+	bash tools/includes/check-osx-bash-version.sh 2>/dev/null
+}
+
 function support_url {
 	if [ -z $2 ]; then
 		slug=`echo $1 | tr _ -`
@@ -87,14 +85,15 @@ function support_url {
 
 function assert {
 	output "* $1 $2":
-	$1 $2 && success " SUCCESS\n"
-	$1 $2 || danger " FAILED.\n\tCheck $(support_url "$1" "$2" )\n"
+	$1 $2 && success " SUCCESS" || error <<-EOM 2>&1
+		 FAILED.
+		    Check $(support_url "$1" "$2" )
+	EOM
 }
 
 function check {
 	output "* $1 $2":
-	$1 $2 && success " SUCCESS\n"
-	$1 $2 || output " NOPE.\n\tCheck $(support_url "$1" "$2" )\n"
+	$1 $2 && success " SUCCESS" || output " NOPE.\n\tCheck $(support_url "$1" "$2" )\n"
 }
 
 main() {
@@ -110,7 +109,7 @@ main() {
 	assert vendor_dir_is_available
 
 	output "\n\nTools for development, linting, unit testing JavaScript"
-	output "\n ======================================================\n\n"
+	output   "\n=======================================================\n\n"
 
 	assert command_is_available node
 	assert node_version_is_proper
@@ -118,27 +117,29 @@ main() {
 	check  nvm_is_available || check command_is_available n
 	assert node_modules_are_available
 
-	output "\nDocker Development Environment"
-	output "\n==============================\n\n"
+	output "\n\nTools used by scripts"
+	output   "\n=====================\n\n"
 
-	check command_is_available docker
-	check command_is_available docker-compose
+	assert modern_version_of_bash
+	assert command_is_available jq
+
+	output "\n\nDocker Development Environment"
+	output   "\n==============================\n\n"
+
+	check  command_is_available docker
+	check  command_is_available docker-compose
 	check  docker_is_running
 	check  docker_images_are_available
 	check  docker_containers_are_available
 	check  docker_containers_are_running
 
 	output "\n\nTools for contributing"
-	output "\n======================\n\n"
+	output   "\n======================\n\n"
 
 	assert command_is_available git
 	assert is_git_dir
 	assert repo_origin_scheme_is_git
 	assert repo_is_up_to_date
-
-	# Clean terminal colors
-	echo $white
-
 }
 
 ## Run main only if not source by another file
