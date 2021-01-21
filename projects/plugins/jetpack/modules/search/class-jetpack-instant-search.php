a7858c1f0b17bb158d4b842e3c69eb354bdf8571
Hew <github@mhs.fastmail.com>
2021-01-21 16:18:13 -0500

    Switch to _self

--- projects/plugins/jetpack/modules/search/class-jetpack-instant-search.php
+++ projects/plugins/jetpack/modules/search/class-jetpack-instant-search.php
@@ -163,7 +163,7 @@ class Jetpack_Instant_Search extends Jetpack_Search {
 			// search options.
 			'defaultSort'           => get_option( $prefix . 'default_sort', 'relevance' ),
 			'excludedPostTypes'     => $excluded_post_types,
-			'searchResultTarget'    => '_blank',
+			'searchResultTarget'    => '_self',
 
 			// widget info.
 			'hasOverlayWidgets'     => count( $overlay_widget_ids ) > 0,
