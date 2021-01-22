d2f879da09fd16ff4642e4e45f803907b37fb6d9
Paul Bunkham <paul@dobit.co.uk>
2021-01-22 19:49:06 +0000

    Anchor.FM: Pass the cover art and feed URL to the episode template (#18486)
    * [not verified] Get the feed URL and pass it to the template.
    
    We've been relying on the `link` element of the podcast episode in the
    feed, but this isn't always populated and, if it is, will point to the
    episode URL, rather than the feed URL.
    
    * [not verified] Fall back to the podcast cover art when there is no episode image.
    
    * Delay escaping.

--- projects/plugins/jetpack/extensions/blocks/anchor-fm/templates/index.js
+++ projects/plugins/jetpack/extensions/blocks/anchor-fm/templates/index.js
@@ -27,8 +27,8 @@ function spotifyTemplate( { spotifyShowUrl, spotifyImageUrl } ) {
 	];
 }
 
-function podcastSection( { episodeTrack } ) {
-	const { image, link, guid } = episodeTrack;
+function podcastSection( { episodeTrack, feedUrl, coverImage } ) {
+	const { image, guid } = episodeTrack;
 
 	return [
 		'core/columns',
@@ -37,20 +37,22 @@ function podcastSection( { episodeTrack } ) {
 		},
 		[
 			[
-				'core/column', {
+				'core/column',
+				{
 					width: '30%',
 				},
 				[
 					[
 						'core/image',
 						{
-							url: image ? image : null,
+							url: image ? image : coverImage,
 						},
 					],
 				],
 			],
 			[
-				'core/column', {
+				'core/column',
+				{
 					width: '70%',
 					verticalAlignment: 'center',
 				},
@@ -60,7 +62,7 @@ function podcastSection( { episodeTrack } ) {
 						{
 							customPrimaryColor: getIconColor(),
 							hexPrimaryColor: getIconColor(),
-							url: link,
+							url: feedUrl,
 							selectedEpisodes: guid ? [ { guid } ] : [],
 							showCoverArt: false,
 							showEpisodeTitle: false,
@@ -183,8 +185,14 @@ function podcastConversationSection() {
 /*
  * Template parts
  */
-function episodeBasicTemplate( { spotifyShowUrl, spotifyImageUrl, episodeTrack = {} } ) {
-	const tpl = [ podcastSection( { episodeTrack } ) ];
+function episodeBasicTemplate( {
+	spotifyShowUrl,
+	spotifyImageUrl,
+	episodeTrack = {},
+	feedUrl,
+	coverImage,
+} ) {
+	const tpl = [ podcastSection( { episodeTrack, feedUrl, coverImage } ) ];
 
 	if ( spotifyShowUrl && spotifyImageUrl ) {
 		tpl.push( spotifyTemplate( { spotifyShowUrl, spotifyImageUrl } ) );
