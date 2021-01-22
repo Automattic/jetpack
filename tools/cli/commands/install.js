44d237f3c93850ff7222bfdd6b36caa41eb96d5d
Brandon Kraft <public@brandonkraft.com>
2021-01-22 15:13:20 -0600

    Add support for building the root monorepo

--- tools/cli/commands/install.js
+++ tools/cli/commands/install.js
@@ -5,6 +5,7 @@ import chalk from 'chalk';
 import path from 'path';
 import Listr from 'listr';
 import execa from 'execa';
+import process from 'process';
 
 /**
  * Internal dependencies
@@ -17,39 +18,69 @@ import { readComposerJson, readPackageJson } from '../helpers/readJson';
  * Installs a project.
  *
  * @param {string} project - The project.
+ * @param {boolean} root - If the monorepo should be installed via --root arg.
  */
-export async function install( project ) {
-	const cwd = path.resolve( `projects/${ project }` );
-	const installs = new Listr( [
-		{
-			title: `Installing ${ project }`,
-			task: () => {
-				return new Listr(
-					[
-						{
-							title: chalkJetpackGreen( 'Installing Composer Dependencies' ),
-							enabled: () => {
-								return Boolean( readComposerJson( project, false ) );
-							},
-							task: () => execa.command( 'composer install', { cwd: cwd } ),
-						},
-						{
-							title: chalkJetpackGreen( 'Installing Yarn Dependencies' ),
-							enabled: () => {
-								return Boolean( readPackageJson( project, false ) );
-							},
-							task: () => execa.command( 'yarn install', { cwd: cwd } ),
-						},
-					],
-					{ concurrent: true }
-				);
-			},
-		},
-	] );
+export async function install( project, root ) {
+	// Special hack for installing just the monorepo.
+	if ( project === 'monorepo' ) {
+		project = '';
+		root = true;
+	}
+
+	const tasks = [];
+	root ? tasks.push( installProjectTask( project, root ) ) : null;
+	project ? tasks.push( installProjectTask( project ) ) : null;
+
+	const installs = new Listr( tasks, { concurrent: true } );
 
 	installs.run().catch( err => {
 		console.error( err );
 	} );
+}
+
+/**
+ * Preps the task for an individual project.
+ *
+ * @param {string} project - A monorepo project.
+ * @param {boolean} root - If we want to install the monorepo.
+ *
+ * @returns {object} - The project install task per Listr format.
+ */
+function installProjectTask( project, root = false ) {
+	// This should never happen. Hard exit to avoid errors in consuming code.
+	if ( ! project && ! root ) {
+		console.error( 'You cannot create an install task for nothing.' );
+		process.exit( 1 );
+	}
+	const cwd = root ? process.cwd() : path.resolve( `projects/${ project }` );
+	const composerEnabled = root ? true : Boolean( readComposerJson( project, false ) );
+	const yarnEnabled = root ? true : Boolean( readPackageJson( project, false ) );
+	project = root ? 'Monorepo' : project;
+
+	return {
+		title: `Installing ${ project }`,
+		task: () => {
+			return new Listr(
+				[
+					{
+						title: chalkJetpackGreen( 'Installing Composer Dependencies' ),
+						enabled: () => {
+							return composerEnabled;
+						},
+						task: () => execa.command( 'composer install', { cwd: cwd } ),
+					},
+					{
+						title: chalkJetpackGreen( 'Installing Yarn Dependencies' ),
+						enabled: () => {
+							return yarnEnabled;
+						},
+						task: () => execa.command( 'yarn install', { cwd: cwd } ),
+					},
+				],
+				{ concurrent: true }
+			);
+		},
+	};
 }
 
 /**
@@ -62,10 +93,11 @@ export async function installCli( argv ) {
 	argv = {
 		...argv,
 		project: argv.project || '',
+		root: argv.root || false,
 	};
 
-	if ( argv.project ) {
-		await install( argv.project );
+	if ( argv.project || argv.root ) {
+		await install( argv.project, argv.root );
 	} else {
 		console.log( chalk.red( 'You did not choose a project!' ) );
 	}
@@ -83,10 +115,16 @@ export function installDefine( yargs ) {
 		'install [project]',
 		'Installs a monorepo project',
 		yarg => {
-			yarg.positional( 'project', {
-				describe: 'Project in the form of type/name, e.g. plugins/jetpack',
-				type: 'string',
-			} );
+			yarg
+				.positional( 'project', {
+					describe: 'Project in the form of type/name, e.g. plugins/jetpack',
+					type: 'string',
+				} )
+				.option( 'root', {
+					alias: 'r',
+					type: 'boolean',
+					description: 'Install the monorepo dependencies',
+				} );
 		},
 		async argv => {
 			await installCli( argv );
