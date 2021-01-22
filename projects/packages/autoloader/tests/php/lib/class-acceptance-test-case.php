6aa82bb4e900f8b6597dd371dfbb5b266e4d9855
Christopher Allford <christopher.allford@automattic.com>
2021-01-21 23:14:39 -0800

    [not verified] Autoloader: refactored test suite
    Since the autoloader is intended to be used in a generated state it
    makes sense that our tests would be against that. This commit
    removes all of the dummy plugins in favor of a solution that
    supports generating plugins using a factory.
    
    I also had to rename all of the test suite classes to deal with a
    deprecation in PHPUnit 9.5.1 as well as improved the separation
    between the different types of tests in the package.

---
+++ projects/packages/autoloader/tests/php/lib/class-acceptance-test-case.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Base class file for all acceptance tests.
+ *
+ * @package automattic/jetpack-autoloader
+ */
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class Acceptance_Test_Case.
+ */
+abstract class Acceptance_Test_Case extends TestCase {
+
+}
