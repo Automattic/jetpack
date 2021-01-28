9764367de77ebf22808e0f59980a34a228926021
Andy Peatling <apeatling@Andys-MacBook-Air.local>
2021-01-28 10:56:53 -0800

    Fix issue where shortcode with no line breaks in between would not transform all fields.

--- projects/plugins/jetpack/extensions/blocks/contact-form/transforms.js
+++ projects/plugins/jetpack/extensions/blocks/contact-form/transforms.js
@@ -6,133 +6,135 @@ import { createBlock } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
 
 const getAttributeValue = ( tag, att, content ) => {
-	const dbqts = content.match( new RegExp( `\\[${tag}[^\\]]* ${att}="([^"]*)"`, 'im' ) );
-    if ( dbqts != null && dbqts.length > 0 ) {
-        return dbqts[ 1 ];
-    }
+	const doubleQuotes = content.match( new RegExp( `\\[${ tag }[^\\]]* ${ att }="([^"]*)"`, 'im' ) );
+	if ( doubleQuotes != null && doubleQuotes.length > 0 ) {
+		return doubleQuotes[ 1 ];
+	}
 
-	const sinqts = content.match( new RegExp( `\\[${tag}[^\\]]* ${att}='([^']*)'`, 'im' ) );
-	if ( sinqts != null && sinqts.length > 0 ) {
-		return sinqts[ 1 ];
-    }
+	const singleQuotes = content.match( new RegExp( `\\[${ tag }[^\\]]* ${ att }='([^']*)'`, 'im' ) );
+	if ( singleQuotes != null && singleQuotes.length > 0 ) {
+		return singleQuotes[ 1 ];
+	}
 
-	const noqts = content.match( new RegExp( `\\[${tag}[^\\]]* ${att}=([^\\s]*)\\s`, 'im' ) );
-    if ( noqts != null && noqts.length > 0 ) {
-       return noqts[ 1 ];
-    }
+	const noQuotes = content.match( new RegExp( `\\[${ tag }[^\\]]* ${ att }=([^\\s]*)\\s`, 'im' ) );
+	if ( noQuotes != null && noQuotes.length > 0 ) {
+		return noQuotes[ 1 ];
+	}
 
 	return false;
 };
 
-const getContactFieldBlockName = ( type ) => {
-    const prefix = 'jetpack';
+const getContactFieldBlockName = type => {
+	const prefix = 'jetpack';
 
-    switch ( type ) {
-        case 'text': case 'url':
-            return `${prefix}/field-text`;
-        case 'textarea':
-            return `${prefix}/field-textarea`;
-        case 'radio':
-            return `${prefix}/field-radio`;
-        case 'checkbox':
-            return `${prefix}/field-checkbox`;
-        case 'select':
-            return `${prefix}/field-select`;
-        case 'email':
-            return `${prefix}/field-email`;
-        case 'name':
-            return `${prefix}/field-name`;
-        default:
-            return `${prefix}/field-text`;
-    }
+	const fieldTypes = {
+		text: `${ prefix }/field-text`,
+		url: `${ prefix }/field-text`,
+		textarea: `${ prefix }/field-textarea`,
+		radio: `${ prefix }/field-radio`,
+		checkbox: `${ prefix }/field-checkbox`,
+		select: `${ prefix }/field-select`,
+		email: `${ prefix }/field-email`,
+		name: `${ prefix }/field-name`,
+		default: `${ prefix }/field-text`,
+	};
+	return fieldTypes[ type ] ? fieldTypes[ type ] : fieldTypes.default;
 };
 
-const transformContactFormShortcode = ( shortcode ) => {
-    const blockAttrs = {
-        to: getAttributeValue( 'contact-form', 'to', shortcode ),
-        subject: getAttributeValue( 'contact-form', 'subject', shortcode ),
-        submitButtonText: getAttributeValue( 'contact-form', 'submit_button_text', shortcode )
-    };
+const transformContactFormShortcode = shortcode => {
+	const blockAttrs = {
+		to: getAttributeValue( 'contact-form', 'to', shortcode ),
+		subject: getAttributeValue( 'contact-form', 'subject', shortcode ),
+		submitButtonText: getAttributeValue( 'contact-form', 'submit_button_text', shortcode ),
+	};
 
-    return {
-        blockName: 'jetpack/contact-form',
-        attrs: pickBy( blockAttrs, identity )
-    };
+	return {
+		blockName: 'jetpack/contact-form',
+		attrs: pickBy( blockAttrs, identity ),
+	};
 };
 
-const transformContactFieldShortcode = ( shortcode ) => {
-    const blockAttrs = {
-        label: getAttributeValue( 'contact-field', 'label', shortcode ),
-        placeholder: getAttributeValue( 'contact-field', 'placeholder', shortcode ),
-        required: getAttributeValue( 'contact-field', 'required', shortcode ),
-        options: getAttributeValue( 'contact-field', 'options', shortcode ),
-    };
+const transformContactFieldShortcode = shortcode => {
+	const blockAttrs = {
+		label: getAttributeValue( 'contact-field', 'label', shortcode ),
+		placeholder: getAttributeValue( 'contact-field', 'placeholder', shortcode ),
+		required: getAttributeValue( 'contact-field', 'required', shortcode ),
+		options: getAttributeValue( 'contact-field', 'options', shortcode ),
+	};
 
-    const blockName = getContactFieldBlockName( getAttributeValue( 'contact-field', 'type', shortcode ) );
+	const blockName = getContactFieldBlockName(
+		getAttributeValue( 'contact-field', 'type', shortcode )
+	);
 
-    // Split block option values into an array.
-    if ( blockAttrs.options ) {
-        blockAttrs.options = blockAttrs.options.split( ',' );
-    }
+	// Split block option values into an array.
+	if ( blockAttrs.options ) {
+		blockAttrs.options = blockAttrs.options.split( ',' );
+	}
 
-    return createBlock( blockName, pickBy( blockAttrs, identity ) );
+	return createBlock( blockName, pickBy( blockAttrs, identity ) );
 };
 
 const blockData = {
-    root: {},
-    innerBlocks: []
+	root: {},
+	innerBlocks: [],
 };
 
 export default {
-    from: [
-        {
-            type: 'raw',
-            priority: 1,
-            isMatch: ( node ) => {
-                if (
-                    node.nodeName === 'P' &&
-                    ( /\[contact-form(\s.*?)?\](?:([^\[]+)?)?/g.test( node.textContent ) ||
-                    /\[contact-field(\s.*?)?\](?:([^\[]+)?)?/g.test( node.textContent ) ||
-                    /\[\/contact-form]/g.test( node.textContent ) )
-                ) {
-                    return true;
-                }
+	from: [
+		{
+			type: 'raw',
+			priority: 1,
+			isMatch: node => {
+				if (
+					node.nodeName === 'P' &&
+					( /\[contact-form(\s.*?)?\](?:([^\[]+)?)?/g.test( node.textContent ) ||
+						/\[contact-field(\s.*?)?\](?:([^\[]+)?)?/g.test( node.textContent ) ||
+						/\[\/contact-form]/g.test( node.textContent ) )
+				) {
+					return true;
+				}
 
-                return false;
-            },
-            transform: ( node ) => {
-                const shortCode = node.textContent;
+				return false;
+			},
+			transform: node => {
+				const shortCode = node.textContent.replace( '<br>', '' );
 
-                if ( shortCode.includes( '[contact-form' ) ) {
-                    blockData.root = {};
-                    blockData.innerBlocks = [];
+				if ( shortCode.includes( '[contact-form' ) ) {
+					blockData.root = {};
+					blockData.innerBlocks = [];
 
-                    blockData.root = transformContactFormShortcode( shortCode );
-                }
+					blockData.root = transformContactFormShortcode( shortCode );
+				}
 
-                if ( shortCode.includes( '[contact-field' ) ) {
-                    blockData.innerBlocks.push( transformContactFieldShortcode( shortCode ) );
-                }
+				if ( shortCode.includes( '[contact-field' ) ) {
+					const fields = shortCode.match( /(\[contact-field[\s\S]*?\/])/g );
 
-                if ( shortCode.includes( '[/contact-form]' ) ) {
-                    blockData.innerBlocks.push(
-                        createBlock( 'jetpack/button', {
-                            element: 'button',
-					        text: blockData.root.attrs.submitButtonText || __( 'Contact Us', 'jetpack' ),
-				        } )
-                    );
+					if ( fields.length > 0 ) {
+						fields.forEach( field => {
+							blockData.innerBlocks.push( transformContactFieldShortcode( field ) );
+						} );
+					}
+				}
 
-                    const block = createBlock(
-                        blockData.root.blockName,
-                        blockData.root.attrs,
-                        blockData.innerBlocks
-                    );
+				if ( shortCode.includes( '[/contact-form]' ) ) {
+					blockData.innerBlocks.push(
+						createBlock( 'jetpack/button', {
+							element: 'button',
+							text: blockData.root.attrs.submitButtonText || __( 'Contact Us', 'jetpack' ),
+						} )
+					);
 
-                    return block;
-                }
+					const block = createBlock(
+						blockData.root.blockName,
+						blockData.root.attrs,
+						blockData.innerBlocks
+					);
 
-                return false;
-            }
-        }
-    ]
+					return block;
+				}
+
+				return false;
+			},
+		},
+	],
 };
