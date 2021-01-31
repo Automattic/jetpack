fc6c21081e46f467cc4f94fbe2fb308e71549697
Brandon Kraft <public@brandonkraft.com>
2021-01-30 23:07:45 -0600

    Initial commit of docker cmd

--- tools/cli/cliRouter.js
+++ tools/cli/cliRouter.js
@@ -13,8 +13,7 @@ import { installDefine } from './commands/install';
 import { cliDefine } from './commands/cli';
 import { generateDefine } from './commands/generate';
 import { changelogDefine } from './commands/changelog';
-
-// import { dockerDefine } from "./commands/docker";
+import { dockerDefine } from './commands/docker';
 
 /**
  * The main CLI router function.
@@ -33,7 +32,7 @@ export async function cli() {
 	argv = buildDefine( argv );
 	argv = changelogDefine( argv );
 	argv = cliDefine( argv );
-	// argv = dockerDefine( argv );
+	argv = dockerDefine( argv );
 	argv = generateDefine( argv );
 	argv = installDefine( argv );
 	argv = watchDefine( argv );
