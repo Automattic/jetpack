02fc13de71e282aa220cb266da08a0b20a54f3ad
retrofox <rdsuarez@gmail.com>
2021-02-01 19:00:28 -0300

    dialogue: focus on content when participant is selected/added

--- projects/plugins/jetpack/extensions/blocks/dialogue/edit.js
+++ projects/plugins/jetpack/extensions/blocks/dialogue/edit.js
@@ -170,6 +170,9 @@ export default function DialogueEdit( {
 							participantValue: value,
 							participantSlug: slug,
 						} );
+
+						// Focus on block content.
+						richTextRef?.current?.focus();
 					} }
 
 					onClean = { () => {
@@ -184,6 +187,9 @@ export default function DialogueEdit( {
 						if ( ! newParticipant ) {
 							return;
 						}
+
+						// Focus on block content.
+						richTextRef?.current?.focus();
 
 						setAttributes( {
 							participantValue: newParticipant.value,
