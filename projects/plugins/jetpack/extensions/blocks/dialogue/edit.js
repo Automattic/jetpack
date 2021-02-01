237e2b1f1047753466fa634e28bae4078dadc355
retrofox <rdsuarez@gmail.com>
2021-02-01 19:29:34 -0300

    dialogue: remove focusing when select/edit

--- projects/plugins/jetpack/extensions/blocks/dialogue/edit.js
+++ projects/plugins/jetpack/extensions/blocks/dialogue/edit.js
@@ -167,9 +167,6 @@ export default function DialogueEdit( {
 							participantValue: value,
 							participantSlug: slug,
 						} );
-
-						// Focus on block content.
-						richTextRef?.current?.focus();
 					} }
 
 					onClean = { () => {
@@ -184,9 +181,6 @@ export default function DialogueEdit( {
 						if ( ! newParticipant ) {
 							return;
 						}
-
-						// Focus on block content.
-						richTextRef?.current?.focus();
 
 						setAttributes( {
 							participantValue: newParticipant.value,
