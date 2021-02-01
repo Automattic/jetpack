c682476f4f12bb0ac92cf078ca4fff87d817238d
Sergey Mitroshin <sergeymitr@gmail.com>
2021-02-01 09:58:31 -0600

    Fixing the `Code coverage` test.

--- projects/packages/connection/tests/php/bootstrap.php
+++ projects/packages/connection/tests/php/bootstrap.php
@@ -10,9 +10,6 @@
  */
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-// Preloading the file to reconcile Brain\Monkey with Wordbless.
-require_once __DIR__ . '/../../vendor/antecedent/patchwork/Patchwork.php';
-
 // Work around WordPress bug when `@runInSeparateProcess` is used.
 if ( empty( $_SERVER['SCRIPT_FILENAME'] ) ) {
 	$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/vendor/phpunit/phpunit/phpunit';
@@ -25,6 +22,9 @@ if ( empty( $_SERVER['SCRIPT_NAME'] ) ) {
 if ( empty( $_SERVER['PHP_SELF'] ) ) {
 	$_SERVER['PHP_SELF'] = '';
 }
+
+// Preloading the file to reconcile Brain\Monkey with Wordbless.
+require_once __DIR__ . '/../../vendor/antecedent/patchwork/Patchwork.php';
 
 \WorDBless\Load::load();
 require_once ABSPATH . WPINC . '/class-IXR.php';
