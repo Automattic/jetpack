1df82dc1c7c30f7f7b6e946cf001b9c52f3aad32
Aurorum <torres126@clubpenguinmountains.com>
2021-02-03 10:42:06 +0000

    API: Add File Size to Media Endpoint (#18637)

--- projects/plugins/jetpack/class.json-api-endpoints.php
+++ projects/plugins/jetpack/class.json-api-endpoints.php
@@ -1353,6 +1353,7 @@ abstract class WPCOM_JSON_API_Endpoint {
 			'description' => $media_item->post_content,
 			'alt'         => get_post_meta( $media_item->ID, '_wp_attachment_image_alt', true ),
 			'icon'        => wp_mime_type_icon( $media_item->ID ),
+			'size'        => size_format( filesize( $attachment_file ), 2 ),
 			'thumbnails'  => array(),
 		);
 
