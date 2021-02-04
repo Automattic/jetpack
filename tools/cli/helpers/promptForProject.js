cf60d3b3803408ee0e325ea0b2c4f09c4043c3a6
Brandon Kraft <public@brandonkraft.com>
2021-02-04 16:22:03 -0600

    Expand project prompt to offer type prompt

--- tools/cli/helpers/promptForProject.js
+++ tools/cli/helpers/promptForProject.js
@@ -6,12 +6,10 @@ import inquirer from 'inquirer';
 /**
  * Internal dependencies
  */
-import { dirs, projectTypes } from './projectHelpers';
+import { dirs, projectTypes, allProjects } from './projectHelpers';
 
 /**
  * Prompt for project.
- *
- * If the --default flag is set, it will select the Jetpack plugin.
  *
  * If no project is passed via `options`, then it will prompt for the type of project and the project itself.
  *
@@ -24,18 +22,15 @@ export async function promptForProject( options ) {
 	let typeAnswer;
 
 	if ( ! options.project || options.project.length === 0 ) {
-		typeAnswer = await inquirer.prompt( {
-			type: 'list',
-			name: 'type',
-			message: 'What type of project are you working on today?',
-			choices: projectTypes,
-		} );
+		typeAnswer = await promptForType();
 		questions.push( {
 			type: 'list',
 			name: 'project',
 			message: 'Please choose which project',
 			choices: dirs( './projects/' + typeAnswer.type ),
 		} );
+	} else if ( ! allProjects().includes( options.project ) ) {
+		return new Error( 'Must be an existing project.' );
 	}
 
 	const finalAnswers = await inquirer.prompt( questions );
@@ -43,5 +38,34 @@ export async function promptForProject( options ) {
 	return {
 		...options,
 		project: options.project || typeAnswer.type + '/' + finalAnswers.project,
+	};
+}
+
+/**
+ * Prompt for type.
+ *
+ * If no type is passed via `options`, then it will prompt for the type of project.
+ *
+ * @param {object} options - Passthrough of an object, meant to accept argv.
+ *
+ * @returns {object} object with the type property appended.
+ */
+export async function promptForType( options = { type: '' } ) {
+	let typeAnswer;
+
+	if ( ! options.type || options.type.length === 0 ) {
+		typeAnswer = await inquirer.prompt( {
+			type: 'list',
+			name: 'type',
+			message: 'What type of project are you working on today?',
+			choices: projectTypes,
+		} );
+	} else if ( ! projectTypes.includes( options.type ) ) {
+		return new Error( 'Must be an accepted project type.' );
+	}
+
+	return {
+		...options,
+		type: options.type || typeAnswer.type,
 	};
 }
