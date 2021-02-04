3142eb7d8e3bbdfbf4e582779d5b47d68760a4c6
Jason Moon <jason.moon@automattic.com>
2021-02-04 15:09:18 -0700

    Replace SearchForm filters w/ mobile SearchSidebar

--- projects/plugins/jetpack/modules/search/instant-search/components/search-form.jsx
+++ projects/plugins/jetpack/modules/search/instant-search/components/search-form.jsx
@@ -4,53 +4,19 @@
  * External dependencies
  */
 import { Component, h } from 'preact';
-import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
-import Gridicon from './gridicon';
-import JetpackColophon from './jetpack-colophon';
 import SearchBox from './search-box';
-import SearchFilters from './search-filters';
 import SearchSort from './search-sort';
 import './search-form.scss';
 
 const noop = event => event.preventDefault();
 
 class SearchForm extends Component {
-	state = {
-		showFilters: !! this.props.widget,
-	};
-
-	static getDerivedStateFromProps( props, state ) {
-		// Combine widgets and widgetOutsideOverlay into one reference.
-		let widgets = [ ...props.widgets ];
-		if ( props.widgetOutsideOverlay?.filters?.length > 0 ) {
-			widgets = [ props.widgetOutsideOverlay, ...widgets ];
-		}
-		return { ...state, widgets };
-	}
-
 	onChangeSearch = event => this.props.onChangeSearch( event.currentTarget.value );
-	onChangeSort = sort => {
-		this.props.onChangeSort( sort );
-		this.hideFilters();
-	};
-
-	hideFilters = () => this.setState( () => ( { showFilters: false } ) );
-	toggleFilters = event => {
-		if (
-			event.type === 'click' ||
-			( event.type === 'keydown' && ( event.key === 'Enter' || event.key === ' ' ) )
-		) {
-			// Prevent page scroll from pressing spacebar
-			if ( event.key === ' ' ) {
-				event.preventDefault();
-			}
-			this.setState( state => ( { showFilters: ! state.showFilters } ) );
-		}
-	};
+	onChangeSort = sort => this.props.onChangeSort( sort );
 
 	render() {
 		return (
@@ -64,28 +30,7 @@ class SearchForm extends Component {
 					/>
 
 					<div className="jetpack-instant-search__form-controls">
-						{ this.state.widgets.length > 0 && (
-							<div
-								role="button"
-								onClick={ this.toggleFilters }
-								onKeyDown={ this.toggleFilters }
-								tabIndex="0"
-								className="jetpack-instant-search__box-filter-button"
-							>
-								{ __( 'Filters', 'jetpack' ) }
-								<Gridicon
-									icon="chevron-down"
-									size={ 16 }
-									alt="Show search filters"
-									aria-hidden="true"
-								/>
-								<span className="screen-reader-text assistive-text">
-									{ this.state.showFilters
-										? __( 'Hide filters', 'jetpack' )
-										: __( 'Show filters', 'jetpack' ) }
-								</span>
-							</div>
-						) }
+						{ this.props.children }
 						{ this.props.enableSort && (
 							<SearchSort
 								onChange={ this.onChangeSort }
@@ -95,25 +40,6 @@ class SearchForm extends Component {
 						) }
 					</div>
 				</div>
-
-				{ /* TODO: Remove this section entirely and use SearchSidebar to show mobile version of filters. */ }
-				{ this.state.widgets.length > 0 && this.state.showFilters && (
-					<div className="jetpack-instant-search__form-filters">
-						{ this.state.widgets.map( ( widget, index ) => (
-							<SearchFilters
-								filters={ this.props.filters }
-								loading={ this.props.isLoading }
-								locale={ this.props.locale }
-								onChange={ this.hideFilters }
-								postTypes={ this.props.postTypes }
-								results={ this.props.response }
-								showClearFiltersButton={ index === 0 }
-								widget={ widget }
-							/>
-						) ) }
-						<JetpackColophon locale={ this.props.locale } />
-					</div>
-				) }
 			</form>
 		);
 	}
--- projects/plugins/jetpack/modules/search/instant-search/components/search-results.jsx
+++ projects/plugins/jetpack/modules/search/instant-search/components/search-results.jsx
@@ -24,6 +24,33 @@ import { getConstrastingColor } from '../lib/colors';
 import './search-results.scss';
 
 class SearchResults extends Component {
+	state = {
+		shouldShowMobileSecondary: false,
+	};
+
+	toggleMobileSecondary = event => {
+		if (
+			event.type === 'click' ||
+			( event.type === 'keydown' && ( event.key === 'Enter' || event.key === ' ' ) )
+		) {
+			// Prevent page scroll from pressing spacebar
+			if ( event.key === ' ' ) {
+				event.preventDefault();
+			}
+			this.setState( state => ( {
+				shouldShowMobileSecondary: ! state.shouldShowMobileSecondary,
+			} ) );
+		}
+	};
+
+	hasAnyWidgets() {
+		let widgets = [ ...this.props.widgets ];
+		if ( this.props.widgetOutsideOverlay?.filters?.length > 0 ) {
+			widgets = [ this.props.widgetOutsideOverlay, ...widgets ];
+		}
+		return widgets;
+	}
+
 	getSearchTitle() {
 		const { total = 0, corrected_query = false } = this.props.response;
 		const hasQuery = this.props.query !== '';
@@ -191,11 +218,43 @@ class SearchResults extends Component {
 					sort={ this.props.sort }
 					widgets={ this.props.widgets }
 					widgetOutsideOverlay={ this.props.widgetOutsideOverlay }
-				/>
+				>
+					{ this.hasAnyWidgets() && (
+						<div
+							role="button"
+							onClick={ this.toggleMobileSecondary }
+							onKeyDown={ this.toggleMobileSecondary }
+							tabIndex="0"
+							className="jetpack-instant-search__results-filter-button"
+						>
+							{ __( 'Filters', 'jetpack' ) }
+							<Gridicon
+								icon="chevron-down"
+								size={ 16 }
+								alt={ __( 'Show search filters', 'jetpack' ) }
+								aria-hidden="true"
+							/>
+							<span className="screen-reader-text assistive-text">
+								{ this.state.shouldShowMobileSecondary
+									? __( 'Hide filters', 'jetpack' )
+									: __( 'Show filters', 'jetpack' ) }
+							</span>
+						</div>
+					) }
+				</SearchForm>
 				<div className="jetpack-instant-search__search-results-primary">
 					{ this.renderPrimarySection() }
 				</div>
-				<div className="jetpack-instant-search__search-results-secondary">
+				<div
+					className={ [
+						'jetpack-instant-search__search-results-secondary',
+						`${
+							this.state.shouldShowMobileSecondary
+								? 'jetpack-instant-search__search-results-secondary--show-as-modal'
+								: ''
+						} `,
+					].join( ' ' ) }
+				>
 					{ this.renderSecondarySection() }
 				</div>
 			</main>
