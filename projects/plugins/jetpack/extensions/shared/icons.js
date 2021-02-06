157364d4db4ed18e7ccae813e609f17776588e36
retrofox <rdsuarez@gmail.com>
2021-02-06 15:52:25 -0300

    icons: applt Jetpack colors to transcript icons

--- projects/plugins/jetpack/extensions/shared/icons.js
+++ projects/plugins/jetpack/extensions/shared/icons.js
@@ -198,9 +198,13 @@ export const TranscriptIcon = {
 		<SVG viewBox="0 0 24 24">
 			<Rect x="0" fill="none" width="24" height="24" />
 			<G>
-				<Path d="M11.1114 8H20.0002M11.1113 15H20.0002" stroke="#00101C" stroke-width="1.5" />
-				<Path d="M4 10V6L8 8L4 10Z" fill="#1E1E1E" />
-				<Path d="M4 17V13L8 15L4 17Z" fill="#1E1E1E" />
+				<Path
+					d="M11.1114 8H20.0002M11.1113 15H20.0002"
+					stroke={ getIconColor() }
+					stroke-width="1.5"
+				/>
+				<Path d="M4 10V6L8 8L4 10Z" fill={ getIconColor() } />
+				<Path d="M4 17V13L8 15L4 17Z" fill={ getIconColor() } />
 			</G>
 		</SVG>
 	),
@@ -212,9 +216,9 @@ export const TranscriptSpeakerIcon = {
 		<SVG viewBox="0 0 24 24">
 			<Rect x="0" fill="none" width="24" height="24" />
 			<G>
-				<Path d="M4 12V4L11 8L4 12Z" fill="#1E1E1E" />
-				<Path d="M4 14.5H20V16H4V14.5Z" fill="#1E1E1E" />
-				<Path d="M4 18.5H13V20H4V18.5Z" fill="#1E1E1E" />
+				<Path d="M4 12V4L11 8L4 12Z" />
+				<Path d="M4 14.5H20V16H4V14.5Z" />
+				<Path d="M4 18.5H13V20H4V18.5Z" />
 			</G>
 		</SVG>
 	),
