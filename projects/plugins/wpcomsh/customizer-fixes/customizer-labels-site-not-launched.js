2397004eb512d7f2bcac9c5fc22729b2314e1966
Andrija Vučinić <andrija.vucinic@gmail.com>
2021-02-06 13:43:52 +0100

    Customizer JS was firing too early and erroring
    - Update Publish label before it can be used
    - Be explicit that we're referencing a global
    - Update language files
    
    Co-authored-by: Brandon Payton <brandon@happycode.net>

--- projects/plugins/wpcomsh/customizer-fixes/customizer-labels-site-not-launched.js
+++ projects/plugins/wpcomsh/customizer-fixes/customizer-labels-site-not-launched.js
@@ -4,9 +4,19 @@
 		window._wpCustomizeControlsL10n[ key ] = window._wpCustomizeControlsL10nSitePrivate[ key ];
 	} );
 
-	document.addEventListener( 'DOMContentLoaded', function() {
-		// Update Publish Label in Gear Status Options
-		var statusPublishInput = document.querySelector( "[id^='customize-selected-changeset-status-control-input-'][value='publish']" );
-		statusPublishInput.parentElement.querySelector( 'label' ).textContent = window._wpCustomizeControlsL10nSitePrivate.publish;
-	} );
+	document.addEventListener(
+		'DOMContentLoaded',
+		function() {
+			// Update Publish label used to populate Customizer save options.
+			var choices = window._wpCustomizeSettings.changeset.statusChoices;
+			for ( var i = 0; i < choices.length; ++i ) {
+				if ( 'publish' === choices[i].status ) {
+					choices[i].label = window._wpCustomizeControlsL10nSitePrivate.publish;
+					return;
+				}
+			}
+		},
+		// Use capture phase because it won't hurt to do this earlier
+		true
+	);
 } )();
