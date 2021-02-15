715ed124302cbca0bb3945b8fa6ce879dfcae3a1
Brian Colinger <briancolinger@users.noreply.github.com>
2021-02-14 23:02:35 -0600

    Using the Password_Checker package. Replacing the usage of jetpack_require_lib.

--- projects/packages/sync/src/modules/class-users.php
+++ projects/packages/sync/src/modules/class-users.php
@@ -8,7 +8,9 @@
 namespace Automattic\Jetpack\Sync\Modules;
 
 use Automattic\Jetpack\Constants as Jetpack_Constants;
+use Automattic\Jetpack\Password_Checker;
 use Automattic\Jetpack\Sync\Defaults;
+namespace Automattic\Jetpack;
 
 /**
  * Class to handle sync for users.
@@ -362,8 +364,7 @@ class Users extends Module {
 			return $user;
 		}
 
-		jetpack_require_lib( 'class.jetpack-password-checker' );
-		$password_checker = new \Jetpack_Password_Checker( $user->ID );
+		$password_checker = new Password_Checker( $user->ID );
 
 		$test_results = $password_checker->test( $password, true );
 
