fa4ef7e23b8e20f8e78e024de142da27fc02e691
Damián Suárez <rdsuarez@gmail.com>
2021-02-16 07:45:26 -0300

    [not verified] Dialogue: fix copy/paste block into block content (#18833)

--- projects/plugins/jetpack/extensions/blocks/dialogue/edit.js
+++ projects/plugins/jetpack/extensions/blocks/dialogue/edit.js
@@ -238,11 +238,6 @@ export default function DialogueEdit( {
 						return onReplace( [ blocks[ 0 ] ], ...args );
 					}
 
-					// Update new block attributes.
-					blocks[ 1 ].attributes = {
-						timestamp: attributes.timestamp, // <- keep same timestamp value.
-					};
-
 					onReplace( blocks, ...args );
 				} }
 				onRemove={ onReplace ? () => onReplace( [] ) : undefined }
