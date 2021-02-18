a314deaa973b56caebfbf1c5a90e1927094daf36
Jeremy Herve <jeremy@jeremy.hu>
2021-02-18 15:50:36 +0100

    Remove the classes from the 2 files
    We don't really need them since they are not called anywhere. We only want to keep the files around for any sites that may continue to require the files after the plugin update because of opcache.

--- projects/plugins/jetpack/_inc/lib/class-jetpack-wizard.php
+++ projects/plugins/jetpack/_inc/lib/class-jetpack-wizard.php
@@ -8,25 +8,3 @@
 
 // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 _deprecated_file( basename( __FILE__ ), 'jetpack-9.5.0' );
-
-/**
- * Jetpack_Wizard class
- */
-class Jetpack_Wizard {
-	/**
-	 * Can the Wizard be displayed?
-	 *
-	 * @return bool
-	 */
-	public static function can_be_displayed() {
-		/**
-		 * Determines if the Setup Wizard is displayed or not.
-		 *
-		 * @since 8.5.0
-		 * @deprecated 9.4.0
-		 *
-		 * @param array $jetpack_show_setup_wizard If true, the Setup Wizard will be displayed. Otherwise it will not display.
-		 */
-		return false;
-	}
-}
