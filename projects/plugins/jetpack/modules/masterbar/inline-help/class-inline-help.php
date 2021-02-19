6c9d0ce605843b0c24a3f80a1f5d50e9b1778ece
Dave Smith <getdavemail@gmail.com>
2021-02-19 15:56:50 +0000

    Remove unwanted classes to avoid inheriting WPAdmin Core styles

--- projects/plugins/jetpack/modules/masterbar/inline-help/class-inline-help.php
+++ projects/plugins/jetpack/modules/masterbar/inline-help/class-inline-help.php
@@ -32,7 +32,7 @@ class Inline_Help {
 			return $data;
 		}
 
-		echo '<div class="inline-help"><a href="https://wordpress.com/help" target="_blank" rel="noopener noreferrer" title="Help" class="button inline-help__button is-borderless"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="gridicon gridicons-help" height="48" width="48"><use xlink:href="/calypso/images/gridicons-84d04a83ed8c3cfc40de995e9bd32649.svg#gridicons-help"></use></svg></a></div>';
+		echo '<div class="inline-help"><a href="https://wordpress.com/help" target="_blank" rel="noopener noreferrer" title="Help" class="inline-help__button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="gridicon gridicons-help" height="48" width="48"><use xlink:href="/calypso/images/gridicons-84d04a83ed8c3cfc40de995e9bd32649.svg#gridicons-help"></use></svg></a></div>';
 	}
 
 	public function add_fab_styles() {
