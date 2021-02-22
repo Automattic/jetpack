3034ea44253815aae4b26c2c04f92790f03c5b57
Dave Smith <getdavemail@gmail.com>
2021-02-22 17:17:16 +0000

    Fix some linting

--- projects/plugins/jetpack/modules/masterbar/inline-help/class-inline-help.php
+++ projects/plugins/jetpack/modules/masterbar/inline-help/class-inline-help.php
@@ -18,11 +18,18 @@ use Automattic\Jetpack\Status;
  */
 class Inline_Help {
 
-
+	/**
+	 * Inline_Help constructor.
+	 */
 	public function __construct() {
 		$this->register_actions();
 	}
 
+	/**
+	 * Registers actions
+	 *
+	 * @return void
+	 */
 	public function register_actions() {
 
 		// Do not inject the FAB icon on embedded screens since the parent window may already contain a FAB icon.
@@ -37,7 +44,12 @@ class Inline_Help {
 		add_action( 'admin_enqueue_scripts', array( $this, 'add_fab_styles' ) );
 	}
 
-	public function add_fab_icon( $data ) {
+	/**
+	 * Outputs "FAB" icon markup and SVG.
+	 *
+	 * @return void|string the HTML markup for the FAB or early exit.
+	 */
+	public function add_fab_icon() {
 
 		if ( wp_doing_ajax() ) {
 			return;
@@ -48,7 +60,11 @@ class Inline_Help {
 		echo '<div class="inline-help"><a href="https://wordpress.com/help" target="_blank" rel="noopener noreferrer" title="' . esc_attr__( 'Help', 'jetpack' ) . '" class="inline-help__button">' . $gridicon_help . '</a></div>';
 	}
 
-
+	/**
+	 * Enqueues FAB CSS styles.
+	 *
+	 * @return void
+	 */
 	public function add_fab_styles() {
 		wp_enqueue_style( 'a8c-wpcom-inline-help', plugins_url( 'inline-help.css', __FILE__ ), array(), JETPACK__VERSION );
 	}
