0af07d9c3a54f38ae43cbe9d950476d5b1cf020c
Brandon Payton <brandon@happycode.net>
2021-02-22 11:55:02 -0500

    Avoid accepting ToS on behalf of users: Part 2 https://github.com/Automattic/wpcomsh/pull/605
    * Add browser-based support session detection

--- projects/plugins/wpcomsh/support-session.php
+++ projects/plugins/wpcomsh/support-session.php
@@ -20,10 +20,39 @@ if ( defined( 'AT_PROXIED_REQUEST' ) && AT_PROXIED_REQUEST ) {
  */
 class WPCOMSH_Support_Session_Detect {
 	const COOKIE_NAME = '_wpcomsh_support_session_detected';
+	const DETECTION_URI = '/_wpcomsh_detect_support_session';
+	const NONCE_ACTION = 'support-session-detect';
+	const NONCE_NAME = 'nonce';
+	const LOGIN_PATH = '/wp-login.php';
+	const QUERY_PARAM_TO_SHORT_CIRCUIT = 'disable-support-session-detection';
+	const EMERGENCY_LOGIN_PATH = '/wp-login.php?' . WPCOMSH_Support_Session_Detect::QUERY_PARAM_TO_SHORT_CIRCUIT;
 
 	public function __construct() {
-		// Detect support session on Jetpack SSO success
+		// Detect support session on WordPress.com SSO success
 		add_action( 'muplugins_loaded', array( __CLASS__, 'detect_support_session_sso_success' ) );
+
+		// Detect support session via client-side check when both of the following are true:
+		// - User is not logged in
+		// - Jetpack is disconnected or Jetpack SSO is disabled
+		add_action( 'login_init', array( __CLASS__, 'handle_detection_redirect' ), -1 );
+		add_action( 'plugins_loaded', array( __CLASS__, 'handle_detection_requests' ), -1 );
+	}
+
+	/**
+	 * Answers whether we need to detect whether the request is probably part of a support session.
+	 * 
+	 * NOTE: This method is marked private because it is for internal use and can only be called
+	 * safely after pluggable functions have been declared.
+	 * 
+	 * @return bool
+	 */
+	private static function need_to_detect() {
+		return (
+			! is_user_logged_in() &&
+			! static::has_detection_result() &&
+			! ( class_exists( 'Jetpack' ) && Jetpack::is_active() && Jetpack::is_module_active( 'sso' ) ) &&
+			! isset( $_GET[ static::QUERY_PARAM_TO_SHORT_CIRCUIT ] )
+		);
 	}
 
 	/**
@@ -52,11 +81,7 @@ class WPCOMSH_Support_Session_Detect {
 	 * @return bool
 	 */
 	public static function is_valid_detection_result( $candidate_result ) {
-		return (
-			'true' === $candidate_result ||
-			'false' === $candidate_result ||
-			'error' === $candidate_result
-		);
+		return in_array( $candidate_result, array( 'true', 'false' ) );
 	}
 
 	/**
@@ -83,7 +108,7 @@ class WPCOMSH_Support_Session_Detect {
 	}
 
 	/**
-	 * Looks for an is_support_session flag on a Jetpack SSO success request
+	 * Looks for an is_support_session flag on a WordPress.com SSO success request
 	 */
 	public static function detect_support_session_sso_success() {
 		$login_path = '/wp-login.php?';
@@ -103,6 +128,206 @@ class WPCOMSH_Support_Session_Detect {
 
 			static::set_detection_result( $is_probably_support_session, $expires );
 		}
+	}
+
+	/**
+	 * Redirects unauthenticated wp-login requests to a client-side detection page
+	 * when Jetpack is disconnected or SSO is disabled.
+	 */
+	public static function handle_detection_redirect() {
+		if ( ! static::need_to_detect() ) {
+			return;
+		}
+
+		$is_simple_login_page_request =
+			'GET' === $_SERVER['REQUEST_METHOD'] &&
+			static::LOGIN_PATH === parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH ) &&
+			( empty( $_GET['action'] ) || 'jetpack-sso' !== $_GET['action'] );
+
+		if ( $is_simple_login_page_request ) {
+			// After detection, we will redirect to this login URL.
+			// Add a query param to short-circuit detection so that if something goes wrong
+			// we do not end up in a login->detect redirect loop.
+			$destination_login_uri = add_query_arg(
+				static::QUERY_PARAM_TO_SHORT_CIRCUIT,
+				// empty value because this query param is just a flag
+				'',
+				$_SERVER['REQUEST_URI']
+			);
+
+			$detection_uri = add_query_arg(
+				array(
+					'redirect' => urlencode( $destination_login_uri ),
+					'nonce' => wp_create_nonce( static::NONCE_ACTION ),
+				),
+				static::DETECTION_URI
+			);
+			wp_redirect( $detection_uri );
+			die;
+		}
+	}
+
+	/**
+	 * Handles GETs and POSTs to the client-side support session detection page
+	 */
+	public static function handle_detection_requests() {
+		if ( 0 !== strncmp( $_SERVER['REQUEST_URI'], static::DETECTION_URI, strlen( static::DETECTION_URI ) ) ) {
+			return;
+		}
+
+		if ( 'GET' === $_SERVER['REQUEST_METHOD'] ) {
+			static::print_detection_ui();
+			die;
+		}
+
+		if ( 'POST' === $_SERVER['REQUEST_METHOD'] ) {
+			if ( isset( $_POST['result'] ) && static::is_valid_detection_result( $_POST['result'] ) ) {
+				static::set_detection_result( $_POST['result'] );
+			}
+
+			// Return to original login path
+			if (
+				'/' === substr( $_GET['redirect'], 0, 1 ) &&
+				static::LOGIN_PATH === parse_url( $_GET['redirect'], PHP_URL_PATH )
+			) {
+				$redirect = $_GET['redirect'];
+			} else {
+				// Use "emergency" login path to avoid infinite login->detect redirect loop
+				$redirect = static::EMERGENCY_LOGIN_PATH;
+			}
+			wp_redirect( $redirect );
+			die;
+		}
+	}
+
+	/**
+	 * Prints a page to attempt support session detection on the client side,
+	 * since it is the browser that may own either support session cookies
+	 * or an extension-managed support session.
+	 */
+	public static function print_detection_ui() {
+		?>
+		<!DOCTYPE html>
+		<html>
+			<head>
+				<meta charset="UTF-8">
+				<title>Detect Support Session</title>
+				<style>
+					body {
+						/* add enough padding to render below the support session status overlay */
+						padding: 57px;
+						display: flex;
+						flex-direction: column;
+						align-items: center;
+						font-size: larger;
+						font-family: sans-serif;
+					}
+
+					body > * {
+						margin: 42px;
+					}
+
+					#error-report {
+						color: red;
+					}
+
+					#escape-hatch {
+						background: lavender;
+						padding: 0 13px;
+						width: min( 37em, 90vw );
+						font-size: 80%;
+					}
+				</style>
+			</head>
+			<body>
+				<p>Asking WordPress.com whether we are in a support session...</p>
+				<div id="escape-hatch">
+					<p>This check is for Automatticians only and should normally complete in a few seconds.</p>
+					<p>
+						We normally detect support sessions via WordPress.com SSO and only
+						resort to this check when SSO is disabled or Jetpack is disconnected.
+					</p>
+					<p>If you encounter an error or the page appears to be stalled, please do the following:</p>
+					<ul>
+						<li>Check the dev tools console for errors</li>
+						<li>Let us know on the Atomic Requests P2</li>
+						<li>
+							Continue login without support session detection by clicking
+							<a target="_blank" href="<?php echo esc_url( static::EMERGENCY_LOGIN_PATH ); ?>">here</a>
+						</li>
+					</ul>
+				</div>
+				<form id="result-form" method="POST">
+					<input id="result-field" type="hidden" name="result" value="">
+				</form>
+				<script>
+					function wpcomshHandleFailure( message ) {
+						if ( ! message ) {
+							message = 'Unknown error';
+						}
+						console.error( message );
+
+						var errorElement = document.createElement( 'p' );
+						errorElement.id = 'error-report';
+						errorElement.textContent = 'Error: ' + message;
+
+						var escapeHatchElement = document.getElementById( 'escape-hatch' );
+						escapeHatchElement.parentNode.insertBefore( errorElement, escapeHatchElement );
+					}
+
+					function wpcomshPostDetectionResult( resultValue ) {
+						var resultForm = document.getElementById( 'result-form' );
+						var resultField = document.getElementById( 'result-field' );
+
+						resultField.value = resultValue;
+						resultForm.submit();
+					}
+					
+					function wpcomshHandleReadyStateChange() {
+						if ( XMLHttpRequest.DONE !== xhr.readyState ) {
+							return;
+						}
+
+						if ( 200 !== xhr.status ) {
+							wpcomshHandleFailure( 'Unexpected HTTP status code: ' + xhr.status );
+							return;
+						}
+
+						if ( ! xhr.responseText ) {
+							wpcomshHandleFailure( 'Empty response' );
+							return;
+						}
+
+						var parsedResponse = JSON.parse( xhr.responseText );
+						if ( typeof parsedResponse === 'boolean' ) {
+							wpcomshPostDetectionResult( parsedResponse.toString() );
+						} else {
+							wpcomHandleFailure( 'Unexpected result type: ' + ( typeof parsedResponse ) );
+						}
+					}
+
+					// Handle exceptional errors in one place rather than using scattered try/catch blocks
+					window.addEventListener( 'error', function ( errorEvent ) {
+						var message = errorEvent.message || 'Unknown unhandled error';
+						wpcomshHandleFailure( message );
+					} );
+
+					// Use basic XMLHttpRequest to avoid errors in older browsers
+					var xhr = new XMLHttpRequest();
+					xhr.open(
+						'POST',
+						'https://public-api.wordpress.com/wpcom/v2/atomic/is-probably-support-session',
+					);
+					xhr.onreadystatechange = wpcomshHandleReadyStateChange;
+					xhr.timeout = 15 * 1000;
+					xhr.ontimeout = function () {
+						wpcomshHandleFailure( 'Support session detection timed out' );
+					};
+					xhr.send();
+				</script>
+			</body>
+		</html>
+		<?php
 	}
 }
 
