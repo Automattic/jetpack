dea6f109a86fd42892f33ecda343317e3b967824
retrofox <rdsuarez@gmail.com>
2021-03-01 16:57:11 -0300

    transcript-utils: add SRT_parse helper function

---
+++ projects/plugins/jetpack/extensions/shared/transcript-utils.js
@@ -0,0 +1,24 @@
+const toLineObj = function( group ) {
+	return {
+		line: group[ 1 ],
+		startTime: group[ 2 ],
+		endTime: group[ 3 ],
+		text: group[ 4 ]
+	};
+};
+
+export const SRT_parse = function( content ) {
+	// SRT format.
+	const pattern = /(\d+)\n([\d:,]+)\s+-{2}>\s+([\d:,]+)\n([\s\S]*?(?=\n{2}|$))/gm;
+
+	const result = [];
+	let matches;
+
+	content = content.replace( /\r\n|\r|\n/g, '\n' );
+
+	while ( ( matches = pattern.exec( content ) ) != null ) {
+		result.push( toLineObj( matches ) );
+	}
+
+	return result;
+};
