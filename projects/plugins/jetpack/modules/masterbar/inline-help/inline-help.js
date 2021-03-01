e700271b1cef8902c73ddea7a0cf5c5f9e5b0c34
Dave Smith <getdavemail@gmail.com>
2021-03-01 12:35:59 +0000

    Ensure always reload URL in re-focused tab

--- projects/plugins/jetpack/modules/masterbar/inline-help/inline-help.js
+++ projects/plugins/jetpack/modules/masterbar/inline-help/inline-help.js
@@ -1,16 +1,10 @@
 ( function () {
-	var windowObjectReference = null;
 	function init() {
 		var fauxInlineHelpButton = document.querySelector( '[data-faux-inline-help]' );
 		fauxInlineHelpButton &&
 			fauxInlineHelpButton.addEventListener( 'click', function ( e ) {
 				e.preventDefault();
-
-				if ( windowObjectReference === null || windowObjectReference.closed ) {
-					windowObjectReference = window.open( e.target.href, e.target.target );
-				} else {
-					windowObjectReference.focus();
-				}
+				window.open( e.target.href, e.target.target );
 			} );
 	}
 
