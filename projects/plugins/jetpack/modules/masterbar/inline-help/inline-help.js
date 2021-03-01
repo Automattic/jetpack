4683263fd5b247781b39e11b559251d9209b53d2
Dave Smith <getdavemail@gmail.com>
2021-03-01 11:55:30 +0000

    Guard possible null on querySelector

--- projects/plugins/jetpack/modules/masterbar/inline-help/inline-help.js
+++ projects/plugins/jetpack/modules/masterbar/inline-help/inline-help.js
@@ -1,15 +1,17 @@
 ( function () {
 	var windowObjectReference = null;
 	function init() {
-		document.querySelector( '[data-faux-inline-help]' ).addEventListener( 'click', function ( e ) {
-			e.preventDefault();
+		var fauxInlineHelpButton = document.querySelector( '[data-faux-inline-help]' );
+		fauxInlineHelpButton &&
+			fauxInlineHelpButton.addEventListener( 'click', function ( e ) {
+				e.preventDefault();
 
-			if ( windowObjectReference === null || windowObjectReference.closed ) {
-				windowObjectReference = window.open( e.target.href, e.target.target );
-			} else {
-				windowObjectReference.focus();
-			}
-		} );
+				if ( windowObjectReference === null || windowObjectReference.closed ) {
+					windowObjectReference = window.open( e.target.href, e.target.target );
+				} else {
+					windowObjectReference.focus();
+				}
+			} );
 	}
 
 	if ( document.readyState === 'loading' ) {
