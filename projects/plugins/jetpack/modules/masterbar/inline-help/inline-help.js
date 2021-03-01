14b7238cf60a629ee682f49647ab42e2a278e290
Dave Smith <getdavemail@gmail.com>
2021-03-01 14:32:55 +0000

    Update projects/plugins/jetpack/modules/masterbar/inline-help/inline-help.js
    Co-authored-by: Tom Cafferkey <tjcafferkey@gmail.com>

--- projects/plugins/jetpack/modules/masterbar/inline-help/inline-help.js
+++ projects/plugins/jetpack/modules/masterbar/inline-help/inline-help.js
@@ -1,7 +1,12 @@
 ( function () {
 	function init() {
 		var fauxInlineHelpButton = document.querySelector( '[data-faux-inline-help]' );
-		fauxInlineHelpButton &&
+		if ( fauxInlineHelpButton ) {
+			fauxInlineHelpButton.addEventListener( 'click', function ( e ) {
+				e.preventDefault();
+				window.open( e.target.href, e.target.target );
+			} );
+		}
 			fauxInlineHelpButton.addEventListener( 'click', function ( e ) {
 				e.preventDefault();
 				window.open( e.target.href, e.target.target );
