58aa61160fbaf960bbfe765bd3bb1bf992f2badd
Jeremy Herve <jeremy@jeremy.hu>
2021-03-02 17:35:45 +0100

    Block editor: remove custom block registration for SEO https://github.com/Automattic/wpcomsh/pull/608
    * Block editor: remove custom block registration for SEO
    
    The block will now be available for everyone, regardless of plan, and as a production block, so we won't need to register it here.
    
    See https://github.com/Automattic/jetpack/pull/17309
    
    * Update version numbers for new release

--- projects/plugins/wpcomsh/wpcomsh.php
+++ projects/plugins/wpcomsh/wpcomsh.php
@@ -2,13 +2,13 @@
 /**
  * Plugin Name: WordPress.com Site Helper
  * Description: A helper for connecting WordPress.com sites to external host infrastructure.
- * Version: 2.7.6
+ * Version: 2.7.7
  * Author: Automattic
  * Author URI: http://automattic.com/
  */
 
 // Increase version number if you change something in wpcomsh.
-define( 'WPCOMSH_VERSION', '2.7.6' );
+define( 'WPCOMSH_VERSION', '2.7.7' );
 
 // If true, Typekit fonts will be available in addition to Google fonts
 add_filter( 'jetpack_fonts_enable_typekit', '__return_true' );
@@ -83,7 +83,6 @@ require_once __DIR__ . '/feature-plugins/full-site-editing.php';
 require_once __DIR__ . '/feature-plugins/gutenberg-mods.php';
 require_once __DIR__ . '/feature-plugins/coblocks-mods.php';
 require_once __DIR__ . '/feature-plugins/autosave-revision.php';
-require_once __DIR__ . '/feature-plugins/seo.php';
 require_once __DIR__ . '/feature-plugins/masterbar.php';
 
 // wp-admin Notices
