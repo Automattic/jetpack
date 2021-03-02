4f9098f0a89423e34ab4667205e762623f3be57d
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2021-03-02 17:02:55 +0200

    add logging and change retry policy for slack reporter

--- projects/plugins/jetpack/tests/e2e/lib/reporters/slack.js
+++ projects/plugins/jetpack/tests/e2e/lib/reporters/slack.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { readFileSync, createReadStream } from 'fs';
-import { WebClient, ErrorCode } from '@slack/web-api';
+import { WebClient, ErrorCode, LogLevel, retryPolicies } from '@slack/web-api';
 import config from 'config';
 
 const { GITHUB_EVENT_PATH, GITHUB_RUN_ID, GUTENBERG } = process.env;
@@ -10,7 +10,10 @@ const { GITHUB_EVENT_PATH, GITHUB_RUN_ID, GUTENBERG } = process.env;
 export default class SlackReporter {
 	constructor() {
 		const token = config.get( 'slackToken' );
-		this.webCli = new WebClient( token );
+		this.webCli = new WebClient( token, {
+			logLevel: LogLevel.DEBUG,
+			retryConfig: retryPolicies.fiveRetriesInFiveMinutes,
+		} );
 		this.runURL = `https://github.com/Automattic/jetpack/actions/runs/${ GITHUB_RUN_ID }`;
 		this.runType =
 			GUTENBERG === 'latest' ? 'with latest :gutenberg: plugin' : 'with no :gutenberg: plugin';
