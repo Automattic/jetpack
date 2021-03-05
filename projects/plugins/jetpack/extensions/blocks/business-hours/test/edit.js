1639bcff797467ca227fe3ffaa587b277609a633
Andrew Serong <andrewserong@users.noreply.github.com>
2021-03-05 17:37:31 +1100

    Add mock for apiFetch to get default day strings

--- projects/plugins/jetpack/extensions/blocks/business-hours/test/edit.js
+++ projects/plugins/jetpack/extensions/blocks/business-hours/test/edit.js
@@ -7,23 +7,24 @@
  */
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 
 /**
  * Internal dependencies
  */
-import BusinessHours from '../edit';
+import BusinessHours, { defaultLocalization } from '../edit';
 
-describe( '', () => {
+describe( 'Business Hours', () => {
 	const dayStrings = [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ];
 	const defaultDays = dayStrings.map( day => {
+		const hours = [ 'Sun', 'Sat' ].includes( day ) ? [] : [ { opening: '09:00', closing: '17:00' } ]
 		return {
 			name: day,
-			hours: day in [ 'Sun', 'Sat' ] ? [] : [ { opening: '09:00', closing: '17:00' } ],
+			hours,
 		};
 	} );
 	const defaultAttributes = {
-		days: defaultDays
+		days: defaultDays,
 	};
 
 	const setAttributes = jest.fn();
@@ -31,14 +32,41 @@ describe( '', () => {
 		attributes: defaultAttributes,
 		setAttributes,
 		clientId: 1,
+		isSelected: false,
 	};
+
+	const originalFetch = window.fetch;
 
 	beforeEach( () => {
 		setAttributes.mockClear();
+		window.fetch = jest.fn();
 	} );
 
-	test( 'renders the default business hours when not selected', () => {
-		render( <BusinessHours { ...defaultProps } /> );
-		expect( screen.getByText( 'Saturday' ) ).toBeInTheDocument();
+	afterAll( () => {
+		window.fetch = originalFetch;
+	} );
+
+	test( 'renders the default business hours when not selected', async () => {
+		window.fetch.mockReturnValue(
+			Promise.resolve( { status: 200, json: () => Promise.resolve( defaultLocalization ) } )
+		);
+
+		const propsNotSelected = { ...defaultProps, isSelected: false };
+		render( <BusinessHours { ...propsNotSelected } /> );
+
+		expect( window.fetch.mock.calls[0][0] ).toEqual( '/wpcom/v2/business-hours/localized-week?_locale=user' );
+
+		// Displays loading state
+		expect( screen.getByText( 'Loading business hours' ) ).toBeInTheDocument();
+		expect( screen.queryByText( 'Saturday' ) ).not.toBeInTheDocument();
+
+		// Displays rendered default business hours
+		await waitFor( () => expect( screen.getByText( 'Monday') ).toBeInTheDocument() );
+		await waitFor( () => expect( screen.getByText( 'Tuesday') ).toBeInTheDocument() );
+		await waitFor( () => expect( screen.getByText( 'Wednesday') ).toBeInTheDocument() );
+		await waitFor( () => expect( screen.getByText( 'Thursday') ).toBeInTheDocument() );
+		await waitFor( () => expect( screen.getByText( 'Friday') ).toBeInTheDocument() );
+		await waitFor( () => expect( screen.getByText( 'Saturday') ).toBeInTheDocument() );
+		await waitFor( () => expect( screen.getByText( 'Sunday') ).toBeInTheDocument() );
 	} );
 } );
