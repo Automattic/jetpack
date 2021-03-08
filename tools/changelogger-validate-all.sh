ec0b95977ea9d018aa594b0037055e9c150f5602
Brad Jorsch <anomiex@users.noreply.github.com>
2021-03-08 16:29:02 -0500

    Add changelogs for all projects (#19031)
    Create a CHANGELOG.md for all existing projects by exporting the git
    log, using auto-changelog as a base.
    
    Note this doesn't try to do anything about PRs that have been merged
    since the last releases of anything. We'll get those in a follow-up
    after changelogger is being required for new PRs.

--- tools/changelogger-validate-all.sh
+++ tools/changelogger-validate-all.sh
@@ -20,6 +20,11 @@ if [[ "$1" == "--debug" ]]; then
 	ARGS2+=( '-v' )
 fi
 
+if [[ ! -e projects/packages/changelogger/vendor/autoload.php ]]; then
+	info "Executing composer update in projects/packages/changelogger"
+	(cd projects/packages/changelogger && composer update)
+fi
+
 EXIT=0
 for FILE in projects/*/*/composer.json; do
 	DIR="${FILE%/composer.json}"
@@ -31,7 +36,7 @@ for FILE in projects/*/*/composer.json; do
 	elif [[ "$DIR" == "projects/packages/changelogger" ]]; then
 		CHANGELOGGER=bin/changelogger
 	elif jq -e '.["require"]["automattic/jetpack-changelogger"] // .["require-dev"]["automattic/jetpack-changelogger"] // false' composer.json > /dev/null; then
-		CHANGELOGGER=vendor/bin/changelogger
+		CHANGELOGGER="$BASE/projects/packages/changelogger/bin/changelogger"
 	else
 		# For now, do nothing. At some point in the future, maybe this will become an error.
 		$DEBUG && debug "$SLUG does not use changelogger"
@@ -39,18 +44,6 @@ for FILE in projects/*/*/composer.json; do
 	fi
 
 	info "Validating change entries for $SLUG"
-	if [[ "$(git ls-files ./composer.lock)" ]]; then
-		CMD=install
-	else
-		CMD=update
-	fi
-	if $DEBUG && [[ -n "$CI" ]]; then
-		echo "::group::Executing composer $CMD for $SLUG"
-		composer $CMD
-		echo "::endgroup::"
-	else
-		composer $CMD --quiet
-	fi
 	if ! $CHANGELOGGER validate "${ARGS[@]}"; then
 		EXIT=1
 		continue
@@ -59,9 +52,9 @@ for FILE in projects/*/*/composer.json; do
 	info "Checking version numbers $SLUG"
 	CHANGES_DIR="$(jq -r '.extra.changelogger["changes-dir"] // "changelog"' composer.json)"
 	if [[ -d "$CHANGES_DIR" && "$(ls -- "$CHANGES_DIR")" ]]; then
-		VER=$($CHANGELOGGER version next --default-first-version --prerelease=alpha)
+		VER=$($CHANGELOGGER version next --default-first-version --prerelease=alpha) || { echo "$VER"; EXIT=1; continue; }
 	else
-		VER=$($CHANGELOGGER version current --default-first-version --prerelease=alpha)
+		VER=$($CHANGELOGGER version current --default-first-version --prerelease=alpha) || { echo "$VER"; EXIT=1; continue; }
 	fi
 	if ! $BASE/tools/project-version.sh "${ARGS2[@]}" -c "$VER" "$SLUG"; then
 		EXIT=1
