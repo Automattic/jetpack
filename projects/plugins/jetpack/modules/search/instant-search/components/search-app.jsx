2815db5e47b7c7347f27cc342c4867ca4652cd4e
Chris R <chris.rosser@automattic.com>
2021-03-08 20:18:34 +1300

    Jetpack Search: add 'product' as a result format option in the Customizer (#19015)

--- projects/plugins/jetpack/modules/search/instant-search/components/search-app.jsx
+++ projects/plugins/jetpack/modules/search/instant-search/components/search-app.jsx
@@ -38,7 +38,7 @@ import {
 	isHistoryNavigation,
 	isLoading,
 } from '../store/selectors';
-import { bindCustomizerChanges } from '../lib/customize';
+import { bindCustomizerChanges, isInCustomizer } from '../lib/customize';
 import './search-app.scss';
 
 class SearchApp extends Component {
@@ -273,6 +273,7 @@ class SearchApp extends Component {
 			sort: this.props.sort,
 			postsPerPage: this.props.options.postsPerPage,
 			adminQueryFilter: this.props.options.adminQueryFilter,
+			isInCustomizer: isInCustomizer(),
 		} );
 	};
 
