0e2684964e94c85c4840c5cfe8248dfcc21b79d1
Glen Davies <glen.davies@a8c.com>
2021-03-09 08:47:51 +1300

    [not verified] Add some more tests

--- projects/plugins/jetpack/extensions/blocks/simple-payments/test/edit.js
+++ projects/plugins/jetpack/extensions/blocks/simple-payments/test/edit.js
@@ -18,7 +18,7 @@ const props = {
 	},
 	postLinKText: 'Click here to buy',
 	setAttributes,
-	isSelected: false,
+	isSelected: true,
 };
 
 beforeEach( () => {
@@ -27,18 +27,37 @@ beforeEach( () => {
 
 describe( 'Edit component', () => {
 	test( 'shows Pay with Paypal image but no input fields if not selected', () => {
-		render( <SimplePaymentsEdit { ...props } /> );
+		const notSelectedProps = { ...props, isSelected: false };
+		render( <SimplePaymentsEdit { ...notSelectedProps } /> );
 		expect( screen.getByAltText( 'Pay with PayPal' ) ).toBeInTheDocument();
 		expect( screen.queryByPlaceholderText( 'Item name' ) ).not.toBeInTheDocument();
 	} );
 
-	test( 'shows input fields if  selected', () => {
-		const selectedProps = { ...props, isSelected: true };
-		render( <SimplePaymentsEdit { ...selectedProps } /> );
+	test( 'shows input fields if selected', () => {
+		render( <SimplePaymentsEdit { ...props } /> );
 		expect( screen.getByLabelText( 'Item name' ) ).toBeInTheDocument();
 		expect( screen.getByLabelText( 'Describe your item in a few words' ) ).toBeInTheDocument();
 		expect( screen.getByLabelText( 'Currency' ) ).toBeInTheDocument();
 		expect( screen.getByLabelText( 'Price' ) ).toBeInTheDocument();
 		expect( screen.getByLabelText( 'Email' ) ).toBeInTheDocument();
+	} );
+
+	test( 'updates item name attribute if item name input updated', () => {
+		render( <SimplePaymentsEdit { ...props } /> );
+		userEvent.type( screen.getByLabelText( 'Item name' ), 'A' );
+		expect( setAttributes ).toHaveBeenCalledWith( { title: 'A' } );
+	} );
+
+	test( 'updates attribute attribute if description input updated', () => {
+		render( <SimplePaymentsEdit { ...props } /> );
+		userEvent.type( screen.getByLabelText( 'Describe your item in a few words' ), 'B' );
+		expect( setAttributes ).toHaveBeenCalledWith( { content: 'B' } );
+	} );
+
+	test.only( 'updates price attribute if price input updated', () => {
+		render( <SimplePaymentsEdit { ...props } /> );
+		// screen.debug();
+		await userEvent.type( screen.getByLabelText( 'Price' ), 1 );
+		expect( setAttributes ).toHaveBeenCalledWith( { price: 1 } );
 	} );
 } );
