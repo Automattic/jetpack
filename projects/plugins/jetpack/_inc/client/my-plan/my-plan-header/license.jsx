7a90b7817005e20e8749a18d33d33adc64fa7045
Atanas Angelov <hi@atanas.dev>
2021-03-10 19:44:17 +0200

    License UI: Reword input messaging to better indicate the license application process

--- projects/plugins/jetpack/_inc/client/my-plan/my-plan-header/license.jsx
+++ projects/plugins/jetpack/_inc/client/my-plan/my-plan-header/license.jsx
@@ -53,7 +53,12 @@ const License = ( { errorNotice, successNotice } ) => {
 	return (
 		<div className="jp-landing__plan-features-header-jetpack-license">
 			<h3>{ __( 'Jetpack License', 'jetpack' ) }</h3>
-			<p>{ __( 'If you have a Jetpack license key add it here.', 'jetpack' ) }</p>
+			<p>
+				{ __(
+					'If you have a Jetpack license key paste it here to queue it for activation. Once your license is processed your product will appear under "My Plan".',
+					'jetpack'
+				) }
+			</p>
 			<TextInput
 				name="jetpack_license_key"
 				className="code"
@@ -64,8 +69,8 @@ const License = ( { errorNotice, successNotice } ) => {
 			/>
 			<Button primary compact onClick={ saveJetpackLicense }>
 				{ isSaving
-					? _x( 'Saving…', 'Button caption', 'jetpack' )
-					: _x( 'Save license', 'Button caption', 'jetpack' ) }
+					? _x( 'Applying…', 'Button caption', 'jetpack' )
+					: _x( 'Apply license', 'Button caption', 'jetpack' ) }
 			</Button>
 		</div>
 	);
