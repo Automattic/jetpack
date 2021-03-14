61966fb578e6a240cbba12e63355c2be8073b626
Glen Davies <glen.davies@a8c.com>
2021-03-15 11:49:35 +1300

    [not verified] Finish controls tests

--- projects/plugins/jetpack/extensions/blocks/mailchimp/test/controls.js
+++ projects/plugins/jetpack/extensions/blocks/mailchimp/test/controls.js
@@ -7,14 +7,49 @@
  */
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, act, waitFor } from '@testing-library/react';
 
 /**
  * Internal dependencies
  */
 import { MailChimpBlockControls } from '../controls';
 
+const originalFetch = window.fetch;
+
+/**
+ * Mock return value for a successful fetch JSON return value.
+ *
+ * @return {Promise} Mock return value.
+ */
+const RESOLVED_FETCH_PROMISE = Promise.resolve( {
+	interest_categories: [
+		{
+			interests: [
+				{ id: 1, name: 'golf' },
+				{ id: 2, name: 'baseball' },
+			],
+		},
+	],
+} );
+const DEFAULT_FETCH_MOCK_RETURN = Promise.resolve( {
+	status: 200,
+	json: () => RESOLVED_FETCH_PROMISE,
+} );
+
 describe( '', () => {
+	beforeEach( () => {
+		window.fetch = jest.fn();
+		window.fetch.mockReturnValue( DEFAULT_FETCH_MOCK_RETURN );
+	} );
+
+	afterEach( async () => {
+		await act( () => RESOLVED_FETCH_PROMISE );
+	} );
+
+	afterAll( () => {
+		window.fetch = originalFetch;
+	} );
+
 	const setAttributes = jest.fn();
 	const auditionNotification = jest.fn();
 	const clearAudition = jest.fn();
@@ -27,7 +62,7 @@ describe( '', () => {
 		processingLabel: 'Processing ...',
 		successLabel: 'Woop woop!',
 		errorLabel: 'Dang!',
-		interests: 'Darning socks',
+		interests: [],
 		signupFieldTag: 'SIGNUP',
 		signupFieldValue: 'Sign up',
 		connectURL: 'https://mailchimp.com',
@@ -39,18 +74,66 @@ describe( '', () => {
 		clearAudition.mockClear();
 	} );
 
-	test( 'loads email placeholder input', async () => {
-		render( <MailChimpBlockControls { ...defaultProps } /> );
-
-		expect( screen.getByLabelText( 'Email Placeholder' ) ).toBeInTheDocument();
-	} );
-
 	test( 'updates email placeholder attribute', async () => {
 		render( <MailChimpBlockControls { ...defaultProps } /> );
-
 		userEvent.paste( screen.getByLabelText( 'Email Placeholder' ), 'Enter an email address' );
+
 		expect( setAttributes ).toHaveBeenCalledWith( {
 			emailPlaceholder: 'Enter your emailEnter an email address',
+		} );
+	} );
+
+	test( 'updates processing text attribute', async () => {
+		render( <MailChimpBlockControls { ...defaultProps } /> );
+		userEvent.paste( screen.getByLabelText( 'Processing text' ), ' Relax!' );
+
+		expect( setAttributes ).toHaveBeenCalledWith( {
+			processingLabel: 'Processing ... Relax!',
+		} );
+	} );
+
+	test( 'updates success text attribute', async () => {
+		render( <MailChimpBlockControls { ...defaultProps } /> );
+		userEvent.paste( screen.getByLabelText( 'Success text' ), ' It Worked!' );
+
+		expect( setAttributes ).toHaveBeenCalledWith( {
+			successLabel: 'Woop woop! It Worked!',
+		} );
+	} );
+
+	test( 'updates error text attribute', async () => {
+		render( <MailChimpBlockControls { ...defaultProps } /> );
+		userEvent.paste( screen.getByLabelText( 'Error text' ), ' Epic fail!' );
+
+		expect( setAttributes ).toHaveBeenCalledWith( {
+			errorLabel: 'Dang! Epic fail!',
+		} );
+	} );
+
+	test( 'updates signup field tag attribute', async () => {
+		render( <MailChimpBlockControls { ...defaultProps } /> );
+		userEvent.paste( screen.getByLabelText( 'Signup Field Tag' ), 'NOW' );
+
+		expect( setAttributes ).toHaveBeenCalledWith( {
+			signupFieldTag: 'SIGNUPNOW',
+		} );
+	} );
+
+	test( 'updates signup field value attribute', async () => {
+		render( <MailChimpBlockControls { ...defaultProps } /> );
+		userEvent.paste( screen.getByLabelText( 'Signup Field Value' ), ' please' );
+		screen.debug();
+		expect( setAttributes ).toHaveBeenCalledWith( {
+			signupFieldValue: 'Sign up please',
+		} );
+	} );
+
+	test( 'updates selected groups', async () => {
+		render( <MailChimpBlockControls { ...defaultProps } /> );
+		await waitFor( () => screen.getByLabelText( 'golf' ) );
+		userEvent.click( screen.getByLabelText( 'golf' ) );
+		expect( setAttributes ).toHaveBeenCalledWith( {
+			interests: [ 1 ],
 		} );
 	} );
 } );
