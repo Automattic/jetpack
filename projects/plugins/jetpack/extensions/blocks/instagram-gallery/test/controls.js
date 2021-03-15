3919ab4669348aab812d8b02e301269b8f4c078d
Andrew Serong <andrewserong@users.noreply.github.com>
2021-03-15 14:10:12 +1100

    Add tests for sidebar notices

--- projects/plugins/jetpack/extensions/blocks/instagram-gallery/test/controls.js
+++ projects/plugins/jetpack/extensions/blocks/instagram-gallery/test/controls.js
@@ -16,7 +16,7 @@ import InstagramGalleryInspectorControls from '../controls';
 
 describe( 'InstagramGalleryInspectorControls', () => {
 	const defaultAttributes = {
-		accessToken: 'abcdef',
+		accessToken: 'test-access-token',
 		columns: 3,
 		count: 9,
 		instagramUser: 'testjetpackuser',
@@ -25,14 +25,21 @@ describe( 'InstagramGalleryInspectorControls', () => {
 	};
 
 	const setAttributes = jest.fn();
+	const disconnectFromService = jest.fn();
+
 	const defaultProps = {
+		accountImageTotal: 15,
 		attributes: defaultAttributes,
+		currentUserConnected: true,
+		disconnectFromService,
 		setAttributes,
+		shouldRenderSidebarNotice: false,
 		clientId: 1,
 	};
 
 	// 👀 Tests setup.
 	beforeEach( () => {
+		disconnectFromService.mockClear();
 		setAttributes.mockClear();
 	} );
 
@@ -58,13 +65,33 @@ describe( 'InstagramGalleryInspectorControls', () => {
 	 *		expect( screen.getByLabelText( 'Country code' ) ).toBeInTheDocument();
 	 * } );
 	 */
-	test( 'Renders account settings and allows the connected account to be disconnected', () => {
+	test( 'Renders account settings and allows the connected account to be disconnected', async () => {
 		render( <InstagramGalleryInspectorControls { ...defaultProps } /> );
 
 		await waitFor( () => screen.getByText( 'Account Settings' ) );
-
 		userEvent.click( screen.getByText( 'Disconnect your account' ) );
 
-		expect( setAttributes ).toHaveBeenCalledWith( 'hello' );
+		expect( disconnectFromService ).toHaveBeenCalledWith( 'test-access-token' );
+	} );
+
+	test( 'Renders notice that there are no images available', async () => {
+		const propsNoImages = { ...defaultProps, accountImageTotal: 0, shouldRenderSidebarNotice: true };
+		render( <InstagramGalleryInspectorControls { ...propsNoImages } /> );
+
+		await waitFor( () => expect( screen.getAllByText( 'There are currently no posts in your Instagram account.' )[0] ).toBeInTheDocument() );
+	} );
+
+	test( 'Renders notice that there is only one image available', async () => {
+		const propsOneImage = { ...defaultProps, accountImageTotal: 1, shouldRenderSidebarNotice: true };
+		render( <InstagramGalleryInspectorControls { ...propsOneImage } /> );
+
+		await waitFor( () => expect( screen.getAllByText( 'There is currently only 1 post in your Instagram account.' )[0] ).toBeInTheDocument() );
+	} );
+
+	test( 'Renders notice that there is only a small number of images available', async () => {
+		const propsSmallNumberOfImages = { ...defaultProps, accountImageTotal: 3, shouldRenderSidebarNotice: true };
+		render( <InstagramGalleryInspectorControls { ...propsSmallNumberOfImages } /> );
+
+		await waitFor( () => expect( screen.getAllByText( 'There are currently only 3 posts in your Instagram account.' )[0] ).toBeInTheDocument() );
 	} );
 } );
