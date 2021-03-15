cb19619dca90de4ddad93a97e05972624c408a5d
Andrew Serong <andrewserong@users.noreply.github.com>
2021-03-15 14:33:16 +1100

    Add remaining tests for controls

--- projects/plugins/jetpack/extensions/blocks/instagram-gallery/test/controls.js
+++ projects/plugins/jetpack/extensions/blocks/instagram-gallery/test/controls.js
@@ -37,35 +37,12 @@ describe( 'InstagramGalleryInspectorControls', () => {
 		clientId: 1,
 	};
 
-	// 👀 Tests setup.
 	beforeEach( () => {
 		disconnectFromService.mockClear();
 		setAttributes.mockClear();
 	} );
 
-	/**
-	 * 👀 Write tests specific to this block controls.
-	 *
-	 * These may cover whatever controls are added by the block
-	 * e.g. Inspector or Block Toolbar controls.
-	 *
-	 * Tests may cover behaviour such as:
-	 * - Correct fields included in controls
-	 * - Appropriate attributes and defaults are applied
-	 * - User interactions trigger correct event handlers etc.
-	 */
-
-	/**
-	 * 👀 Example:
-	 * test( 'loads settings when toolbar button clicked', async () => {
-	 *		render( <WhatsAppButtonConfiguration { ...props } /> );
-	 *		userEvent.click( screen.getByLabelText( 'WhatsApp Button Settings' ) );
-	 *		await waitFor( () => screen.getByLabelText( 'Country code' ) );
-	 *
-	 *		expect( screen.getByLabelText( 'Country code' ) ).toBeInTheDocument();
-	 * } );
-	 */
-	test( 'Renders account settings and allows the connected account to be disconnected', async () => {
+	test( 'renders account settings and allows the connected account to be disconnected', async () => {
 		render( <InstagramGalleryInspectorControls { ...defaultProps } /> );
 
 		await waitFor( () => screen.getByText( 'Account Settings' ) );
@@ -74,24 +51,60 @@ describe( 'InstagramGalleryInspectorControls', () => {
 		expect( disconnectFromService ).toHaveBeenCalledWith( 'test-access-token' );
 	} );
 
-	test( 'Renders notice that there are no images available', async () => {
+	test( 'renders notice that there are no images available', async () => {
 		const propsNoImages = { ...defaultProps, accountImageTotal: 0, shouldRenderSidebarNotice: true };
 		render( <InstagramGalleryInspectorControls { ...propsNoImages } /> );
 
 		await waitFor( () => expect( screen.getAllByText( 'There are currently no posts in your Instagram account.' )[0] ).toBeInTheDocument() );
 	} );
 
-	test( 'Renders notice that there is only one image available', async () => {
+	test( 'renders notice that there is only one image available', async () => {
 		const propsOneImage = { ...defaultProps, accountImageTotal: 1, shouldRenderSidebarNotice: true };
 		render( <InstagramGalleryInspectorControls { ...propsOneImage } /> );
 
 		await waitFor( () => expect( screen.getAllByText( 'There is currently only 1 post in your Instagram account.' )[0] ).toBeInTheDocument() );
 	} );
 
-	test( 'Renders notice that there is only a small number of images available', async () => {
+	test( 'renders notice that there is only a small number of images available', async () => {
 		const propsSmallNumberOfImages = { ...defaultProps, accountImageTotal: 3, shouldRenderSidebarNotice: true };
 		render( <InstagramGalleryInspectorControls { ...propsSmallNumberOfImages } /> );
 
 		await waitFor( () => expect( screen.getAllByText( 'There are currently only 3 posts in your Instagram account.' )[0] ).toBeInTheDocument() );
+	} );
+
+	test( 'updates count when changing number of posts', async () => {
+		const propsSmallCount = { ...defaultProps, attributes: { ...defaultAttributes, count: 1 } };
+		render( <InstagramGalleryInspectorControls { ...propsSmallCount } /> );
+
+		userEvent.paste( screen.getAllByLabelText( 'Number of Posts' )[1], '5' );
+
+		expect( setAttributes ).toHaveBeenCalledWith( { count: 15 } );
+	} );
+
+	test( 'updates columns when changing number of columns', async () => {
+		const propsSmallCount = { ...defaultProps, attributes: { ...defaultAttributes, columns: 0 } };
+		render( <InstagramGalleryInspectorControls { ...propsSmallCount } /> );
+
+		userEvent.paste( screen.getAllByLabelText( 'Number of Columns' )[1], '3' );
+
+		expect( setAttributes ).toHaveBeenCalledWith( { columns: 3 } );
+	} );
+
+	test( 'updates spacing when changing image spacing', async () => {
+		const propsSmallCount = { ...defaultProps, attributes: { ...defaultAttributes, spacing: 0 } };
+		render( <InstagramGalleryInspectorControls { ...propsSmallCount } /> );
+
+		userEvent.paste( screen.getAllByLabelText( 'Image Spacing (px)' )[1], '5' );
+
+		expect( setAttributes ).toHaveBeenCalledWith( { spacing: 5 } );
+	} );
+
+	test( 'updates isStackedOnMobile when toggling stack on mobile', async () => {
+		const propsSmallCount = { ...defaultProps, attributes: { ...defaultAttributes, isStackedOnMobile: true } };
+		render( <InstagramGalleryInspectorControls { ...propsSmallCount } /> );
+
+		userEvent.click( screen.getByLabelText( 'Stack on mobile' ) );
+
+		expect( setAttributes ).toHaveBeenCalledWith( { isStackedOnMobile: false } );
 	} );
 } );
