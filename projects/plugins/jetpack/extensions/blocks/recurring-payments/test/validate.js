d4e10489416c973908d2a9fa75e7f8be18b57904
Staci Cooper <staci.cooper@automattic.com>
2021-03-16 10:47:54 -0700

    [not verified] Fix mocks and add additional tests

--- projects/plugins/jetpack/extensions/blocks/recurring-payments/test/validate.js
+++ projects/plugins/jetpack/extensions/blocks/recurring-payments/test/validate.js
@@ -2,7 +2,12 @@
  * Internal dependencies
  */
 import { name, settings } from '../';
+import { settings as buttonSettings } from '../../button';
 import runBlockFixtureTests from '../../../shared/test/block-fixtures';
 
-const blocks = [ { name: `jetpack/${ name }`, settings } ];
+const blocks = [
+    { name: `jetpack/${ name }`, settings },
+    { name: `jetpack/button`, settings: buttonSettings },
+];
+
 runBlockFixtureTests( `jetpack/${ name }`, blocks, __dirname );
