c3fe45aa537912b0af74d01f02bb332c73a655ce
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2021-03-17 14:42:04 +0200

    moar logging

--- .github/actions/repo-gardening/src/tasks/check-description/index.js
+++ .github/actions/repo-gardening/src/tasks/check-description/index.js
@@ -220,14 +220,17 @@ function statusEntry( isFailure, checkMessage, severity = 'error' ) {
 async function getChangelogEntries( octokit, owner, repo, number ) {
 	const files = await getFiles( octokit, owner, repo, number );
 	const affectedProjects = getAffectedChangeloggerProjects( files );
-
-	return affectedProjects.reduce( ( acc, project ) => {
+	const q = affectedProjects.reduce( ( acc, project ) => {
 		const found = files.find( file => file.includes( '/changelog/' ) );
 		if ( ! found ) {
 			acc.push( project );
 		}
 		return acc;
 	}, [] );
+	debug( `check-description: getChangelogEntries affectedProjects: ${ affectedProjects }` );
+	debug( `check-description: getChangelogEntries reduce: ${ q }` );
+
+	return q;
 }
 
 /**
