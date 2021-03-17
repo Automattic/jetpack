e87de291ccd3b5aaf1e840fded305241c9b0dee5
Brad Jorsch <anomiex@users.noreply.github.com>
2021-03-17 09:42:31 -0400

    tools: Add a generic js test runner (#19140)
    This combines the existing Jetpack runner.js with the very similar
    runner proposed in #19134 to create a runner that is intended to be used
    in both places.
    
    Co-authored-by: Sergey Mitroshin <sergeymitr@gmail.com>

--- .github/files/build-all-projects.sh
+++ .github/files/build-all-projects.sh
@@ -74,7 +74,7 @@ for project in projects/packages/* projects/plugins/* projects/github-actions/*;
 	fi
 	# Need to remove the "projects/" from the string since the CLI only looks for {type}/{project-name}.
 	SLUG="${project#projects/}"
-	if node "$BASE"/tools/cli/bin/jetpack build "${SLUG}" -v --production; then
+	if (cd $BASE && yarn jetpack build "${SLUG}" -v --production); then
 		FAIL=false
 	else
 		FAIL=true
