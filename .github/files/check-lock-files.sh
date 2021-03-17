a136b38cad96296182f83afe4aa17c48928b812e
Brad Jorsch <brad.jorsch@automattic.com>
2021-03-17 13:59:59 -0400

    linting: Add check that yarn.lock is up to date
    Much as we do for composer.lock, let's also ensure that any checked-in
    yarn.lock files are up to date. Yarn seems to make it a little easier
    than composer does, as `yarn install` doesn't blindly trust the lock
    file and it even has a `--frozen-lockfile` option to directly indicate
    when an update is needed.
    
    While doing so, let's merge the "Composer" workflow into "Linting" since
    the cleanest place to add the yarn.lock check is in the same job as the
    existing composer.lock check.
    
    This renames one of the required workflows, so merging will require
    disabling the old name and enabling the new.
    
    Fixes #19027

--- .github/files/check-lock-files.sh
+++ .github/files/check-lock-files.sh
@@ -18,4 +18,17 @@ for FILE in $(git ls-files 'composer.lock' '**/composer.lock'); do
 	cd "$OLDPWD"
 done
 
+for FILE in $(git ls-files 'yarn.lock' '**/yarn.lock'); do
+	cd $(dirname "$FILE")
+	echo "::group::$FILE - yarn install --frozen-lockfile"
+	if yarn install --frozen-lockfile; then
+		echo "::endgroup::"
+	else
+		echo "::endgroup::"
+		echo "::error file=$FILE::$FILE is not up to date!%0AYou can probably fix this by running \`yarn install\` in the appropriate directory."
+		EXIT=1
+	fi
+	cd "$OLDPWD"
+done
+
 exit $EXIT
