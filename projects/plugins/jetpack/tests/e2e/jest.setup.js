484a925452248f5c3cc06363a29755a80138c7bc
Adrian Moldovan <adim.moldovan@gmail.com>
2021-03-18 18:35:57 +0200

    Revert jest timeout

--- projects/plugins/jetpack/tests/e2e/jest.setup.js
+++ projects/plugins/jetpack/tests/e2e/jest.setup.js
@@ -1,7 +1,7 @@
 const { E2E_TIMEOUT, E2E_DEBUG, E2E_RETRY_TIMES } = process.env;
 
 // The Jest timeout is increased because these tests are a bit slow
-jest.setTimeout( E2E_TIMEOUT || 60000 );
+jest.setTimeout( E2E_TIMEOUT || 300000 );
 if ( E2E_DEBUG ) {
 	jest.setTimeout( 2147483647 ); // max 32-bit signed integer
 }
