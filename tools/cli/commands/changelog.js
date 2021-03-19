005d8da26d04be199a71e049f93a524097504189
sdixon194 <steve.dixon@automattic.com>
2021-03-19 10:46:50 -0400

    better error handling

--- tools/cli/commands/changelog.js
+++ tools/cli/commands/changelog.js
@@ -4,6 +4,7 @@
 import chalk from 'chalk';
 import child_process from 'child_process';
 import path from 'path';
+import fs from 'fs';
 
 /**
  * Internal dependencies
@@ -50,19 +51,21 @@ export function changelogDefine( yargs ) {
 export async function changeloggerCli( argv ) {
 	// @todo Add validation of changelogger commands? See projects/packages/changelogger/README.md
 	// @todo refactor? .github/files/require-change-file-for-touched-projects.php to a common function that we could use here. Would allow us to run a "jetpack changelog add" without a project to walk us through all of them?
-	validateArgs( argv );
+	validateCmd( argv );
 	argv = normalizeProject( argv );
 	argv = await promptForProject( argv );
 	const projDir = path.resolve( `projects/${ argv.project }` );
-	const process = child_process.spawnSync( `vendor/bin/changelogger ${ argv.cmd }`, [ '' ], {
+	validatePath( argv, projDir );
+
+	const data = child_process.spawnSync( `vendor/bin/changelogger ${ argv.cmd }`, [ '' ], {
 		cwd: projDir,
 		stdio: 'inherit',
 		shell: true,
 	} );
 
 	// Node.js exit code status 0 === success
-	if ( process.status !== 0 ) {
-		console.error( chalk.red( 'Something went wrong! Check your file path?' ), process.error );
+	if ( data.status !== 0 ) {
+		console.error( chalk.red( `Something went wrong! Status Code: ${ data.status }` ), data.error );
 	} else {
 		console.log( chalkJetpackGreen( `Changelog for ${ argv.project } added successfully!` ) );
 	}
@@ -72,7 +75,7 @@ export async function changeloggerCli( argv ) {
  *
  * @param {object} argv - arguments passed to changelogger.
  */
-function validateArgs( argv ) {
+function validateCmd( argv ) {
 	// make sure we're using a valid command
 	switch ( argv.cmd ) {
 		case 'add':
@@ -90,4 +93,23 @@ function validateArgs( argv ) {
 				}\`. Use \`jetpack changelog --help\` for help.`
 			);
 	}
+}
+
+/** Validate that the vendor/bin/changelogger file exists
+ *
+ * @param {object} argv - arguments passed to the wizard.
+ * @param {string} dir - path to file we're adding changlog too.
+ */
+function validatePath( argv, dir ) {
+	if ( ! fs.existsSync( dir ) ) {
+		throw new Error( chalk.red( `Project doesn't exist! Typo?` ) );
+	}
+	if ( fs.existsSync( dir + `/vendor/bin/changelogger` ) ) {
+		return;
+	}
+	throw new Error(
+		chalk.red(
+			`Path to changelogger script doesn't exist. Try running 'jetpack install ${ argv.project }' first!`
+		)
+	);
 }
