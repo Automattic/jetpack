92ce6559b889eb467abb37d40f3e582563720ea6
Sergey Mitroshin <sergeymitr@gmail.com>
2021-03-25 15:14:55 -0500

    Building JS.

--- projects/packages/identity-crisis/webpack.config.js
+++ projects/packages/identity-crisis/webpack.config.js
@@ -3,6 +3,7 @@
  */
 const getBaseWebpackConfig = require( '@automattic/calypso-build/webpack.config.js' );
 const DependencyExtractionWebpackPlugin = require( '@wordpress/dependency-extraction-webpack-plugin' );
+const MinifyPlugin = require( 'babel-minify-webpack-plugin' );
 const path = require( 'path' );
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
@@ -16,18 +17,22 @@ const baseConfig = getBaseWebpackConfig(
 	}
 );
 
-module.exports = [
-	{
-		...baseConfig,
-		resolve: {
-			...baseConfig.resolve,
-			modules: [ 'node_modules' ],
-		},
-		devtool: isDevelopment ? 'source-map' : false,
-		entry: { index: path.join( __dirname, '_inc/idc-notice.js' ) },
-		plugins: [
-			...baseConfig.plugins,
-			new DependencyExtractionWebpackPlugin( { injectPolyfill: true } ),
-		],
-	},
+const plugins = [
+	...baseConfig.plugins,
+	new DependencyExtractionWebpackPlugin( { injectPolyfill: true } ),
 ];
+
+if ( ! isDevelopment ) {
+	plugins.push( new MinifyPlugin() );
+}
+
+module.exports = {
+	...baseConfig,
+	resolve: {
+		...baseConfig.resolve,
+		modules: [ 'node_modules' ],
+	},
+	devtool: isDevelopment ? 'source-map' : false,
+	entry: { index: path.join( __dirname, 'src/_inc/idc-notice.js' ) },
+	plugins,
+};
