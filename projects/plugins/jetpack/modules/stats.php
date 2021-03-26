c09565d90c856f53ef227e69f6ec5874d62bbb37
Brandon Kraft <public@brandonkraft.com>
2021-03-26 14:02:39 -0500

    Move to separate var to improve indent

--- projects/plugins/jetpack/modules/stats.php
+++ projects/plugins/jetpack/modules/stats.php
@@ -599,26 +599,24 @@ function stats_reports_page( $main_chart_only = false ) {
 				</a>
 				<?php
 				endif;
+
+			/**
+			 * Sets external resource URL.
+			 *
+			 * @module stats
+			 *
+			 * @since 1.4.0
+			 * @todo Clean up various uses of this filter. It's seemingly filtering different types of images in different places.
+			 *
+			 * @param string $args URL of external resource.
+			 */
+			$static_url = apply_filters( 'jetpack_static_url', "{$http}://en.wordpress.com/i/loading/loading-64.gif" );
+																					);
 			?>
 			</h2>
 		</div>
 		<div id="stats-loading-wrap" class="wrap">
-		<p class="hide-if-no-js"><img width="32" height="32" alt="<?php esc_attr_e( 'Loading&hellip;', 'jetpack' ); ?>" src="
-																					<?php
-																					echo esc_url(
-																					/**
-																					 * Sets external resource URL.
-																					 *
-																					 * @module stats
-																					 *
-																					 * @since 1.4.0
-																					 *
-																					 * @param string $args URL of external resource.
-																					 */
-																						apply_filters( 'jetpack_static_url', "{$http}://en.wordpress.com/i/loading/loading-64.gif" )
-																					);
-																					?>
-				" /></p>
+		<p class="hide-if-no-js"><img width="32" height="32" alt="<?php esc_attr_e( 'Loading&hellip;', 'jetpack' ); ?>" src="<?php echo esc_url( $static_url ); ?>" /></p>
 		<p style="font-size: 11pt; margin: 0;"><a href="<?php echo esc_url( $stats_url ); ?>" rel="noopener noreferrer" target="_blank"><?php esc_html_e( 'View stats on WordPress.com right now', 'jetpack' ); ?></a></p>
 		<p class="hide-if-js"><?php esc_html_e( 'Your Site Stats work better with JavaScript enabled.', 'jetpack' ); ?><br />
 		<a href="<?php echo esc_url( $nojs_url ); ?>"><?php esc_html_e( 'View Site Stats without JavaScript', 'jetpack' ); ?></a>.</p>
