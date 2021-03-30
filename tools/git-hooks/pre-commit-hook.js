a508e34b76057abee2385ff4a3dd25ffbcdd2908
Brandon Kraft <public@brandonkraft.com>
2021-03-30 10:21:37 -0500

    Reorganize execution a tad

--- tools/git-hooks/pre-commit-hook.js
+++ tools/git-hooks/pre-commit-hook.js
@@ -330,8 +330,11 @@ function exit( exitCodePassed ) {
 	process.exit( exitCodePassed );
 }
 
+// Start of pre-commit checks execution.
+
 runCheckCopiedFiles();
 runCheckRenovateIgnoreList();
+sortPackageJson( jsFiles );
 
 dirtyFiles.forEach( file =>
 	console.log(
@@ -339,7 +342,7 @@ dirtyFiles.forEach( file =>
 	)
 );
 
-sortPackageJson( jsFiles );
+// Start JS work—linting, prettify, etc.
 
 const toPrettify = jsFiles.filter( file => checkFileAgainstDirtyList( file, dirtyFiles ) );
 toPrettify.forEach( file => console.log( `Prettier formatting staged file: ${ file }` ) );
@@ -359,18 +362,21 @@ if ( jsOnlyFiles.length > 0 ) {
 	runEslintChanged( jsOnlyFiles );
 }
 
-let phpLintResult;
+// Start PHP work.
+
 if ( phpFiles.length > 0 ) {
-	phpLintResult = spawnSync( 'composer', [ 'phpcs:compatibility', ...phpFiles ], {
+	runPHPLinter( phpFiles );
+}
+
+if ( phpFiles.length > 0 ) {
+	const phpLintResult = spawnSync( 'composer', [ 'phpcs:compatibility', ...phpFiles ], {
 		shell: true,
 		stdio: 'inherit',
 	} );
 
-	runPHPLinter( phpFiles );
-}
-
-if ( phpLintResult && phpLintResult.status ) {
-	checkFailed();
+	if ( phpLintResult && phpLintResult.status ) {
+		checkFailed();
+	}
 }
 
 if ( phpcsFiles.length > 0 ) {
