4852dc0270631ffe16b6f01fd01fe4ba86247f4e
mmtr <miguel.torres@automattic.com>
2021-03-30 11:07:37 +0200

    Use ::class property

--- projects/plugins/jetpack/modules/masterbar/admin-menu/load.php
+++ projects/plugins/jetpack/modules/masterbar/admin-menu/load.php
@@ -16,7 +16,7 @@ function get_admin_menu_class() {
 	// WordPress.com Atomic sites.
 	if ( jetpack_is_atomic_site() ) {
 		require_once __DIR__ . '/class-atomic-admin-menu.php';
-		return __NAMESPACE__ . '\Atomic_Admin_Menu';
+		return Atomic_Admin_Menu::class;
 	}
 
 	// WordPress.com Simple sites.
@@ -28,24 +28,24 @@ function get_admin_menu_class() {
 		$is_domain_only = ! empty( $blog_options['is_domain_only'] );
 		if ( $is_domain_only ) {
 			require_once __DIR__ . '/class-domain-only-admin-menu.php';
-			return __NAMESPACE__ . '\Domain_Only_Admin_Menu';
+			return Domain_Only_Admin_Menu::class;
 		}
 
 		// P2 sites.
 		require_once WP_CONTENT_DIR . '/lib/wpforteams/functions.php';
 		if ( \WPForTeams\is_wpforteams_site( $blog_id ) ) {
 			require_once __DIR__ . '/class-p2-admin-menu.php';
-			return __NAMESPACE__ . '\P2_Admin_Menu';
+			return P2_Admin_Menu::class;
 		}
 
 		// Rest of simple sites.
 		require_once __DIR__ . '/class-wpcom-admin-menu.php';
-		return __NAMESPACE__ . '\WPcom_Admin_Menu';
+		return WPcom_Admin_Menu::class;
 	}
 
 	// Jetpack sites.
 	require_once __DIR__ . '/class-jetpack-admin-menu.php';
-	return __NAMESPACE__ . '\Jetpack_Admin_Menu';
+	return Jetpack_Admin_Menu::class;
 }
 
 /**
@@ -58,6 +58,6 @@ function get_admin_menu_class() {
  * @param string $admin_menu_class Class name.
  */
 $admin_menu_class = apply_filters( 'jetpack_admin_menu_class', get_admin_menu_class() );
-if ( is_subclass_of( $admin_menu_class, __NAMESPACE__ . '\Base_Admin_Menu' ) ) {
+if ( is_subclass_of( $admin_menu_class, Base_Admin_Menu::class ) ) {
 	$admin_menu_class::get_instance();
 }
