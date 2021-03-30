f309f7105266d10e1eacb4614786f7a743393139
Brad Jorsch <brad.jorsch@automattic.com>
2021-03-30 12:40:49 -0400

    Update version

--- projects/packages/changelogger/src/Application.php
+++ projects/packages/changelogger/src/Application.php
@@ -17,7 +17,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class Application extends SymfonyApplication {
 
-	const VERSION = '1.1.1';
+	const VERSION = '1.1.2-alpha';
 
 	/**
 	 * Constructor.
