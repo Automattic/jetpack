85db68f21b20b640139288a5df8ddc69b50cc749
Samiff <samiff@users.noreply.github.com>
2021-03-31 16:06:18 -0600

    CLI: add docker command descriptions

--- tools/cli/commands/docker.js
+++ tools/cli/commands/docker.js
@@ -40,137 +40,117 @@ export function dockerDefine( yargs ) {
 			return builder
 				.command( {
 					command: 'build',
-					description: 'todo',
+					description: 'build jetpack within the docker env',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'build-image',
-					description: 'todo',
+					description: 'build docker image',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'clean',
-					description: 'todo',
-					handler: defaultDockerCmdHandler,
-				} )
-				.command( {
-					command: 'compose',
-					description: 'todo',
-					handler: defaultDockerCmdHandler,
-				} )
-				.command( {
-					command: 'compose-extras',
-					description: 'todo',
-					handler: defaultDockerCmdHandler,
-				} )
-				.command( {
-					command: 'compose-volumes',
-					description: 'todo',
+					description: 'remove docker images, mysql data, etc.',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'db',
-					description: 'todo',
+					description: 'access mysql cli for the wp install',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'down',
-					description: 'todo',
-					handler: defaultDockerCmdHandler,
-				} )
-				.command( {
-					command: 'env',
-					description: 'todo',
+					description: 'stop all docker containers',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'install',
-					description: 'todo',
+					description: 'create a wp install',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'jt-config',
-					description: 'set Jurassic Tube config',
+					description: 'set jurassic tube config',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'jt-up',
-					description: 'start Jurassic Tube tunnel',
+					description: 'start jurassic tube tunnel',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'jt-down',
-					description: 'stop Jurassic Tube tunnel',
+					description: 'stop jurassic tube tunnel',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'multisite-convert',
-					description: 'todo',
+					description: 'convert wp install to multisite',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'ngrok',
-					description: 'todo',
+					description: 'see tools/docker/README.md',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'ngrok-down',
-					description: 'todo',
+					description: 'stop docker with ngrok',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'ngrok-up',
-					description: 'todo',
+					description: 'start docker with ngrok',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'phpunit',
-					description: 'todo',
+					description: 'run tests',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'phpunit:multisite',
-					description: 'todo',
+					description: 'runs tests for wp multisite install',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'run-extras',
-					description: 'todo',
+					description: 'see tools/docker/bin/run-extras.sh',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'sh',
-					description: 'todo',
+					description: 'access shell for the wp install',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'stop',
-					description: 'todo',
+					description: 'stop docker containers',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'tail',
-					description: 'todo',
+					description: 'watch wp debug.log output in console',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'uninstall',
-					description: 'todo',
+					description: 'remove current wordpress install',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'up',
-					description: 'todo',
+					description: 'start docker containers',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'update-core-unit-tests',
-					description: 'todo',
+					description: 'updates unit test dirs',
 					handler: defaultDockerCmdHandler,
 				} )
 				.command( {
 					command: 'wp',
-					description: 'todo',
+					description: 'pass commands to wp-cli',
 					handler: defaultDockerCmdHandler,
 				} );
 		},
