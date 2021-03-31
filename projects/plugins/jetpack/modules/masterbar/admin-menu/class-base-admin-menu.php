fd95c1e7922878961c58380d1bcb3e0076f791b0
Paul Bunkham <paul@dobit.co.uk>
2021-04-01 00:03:16 +0100

    [not verified] Don't override on an API request

--- projects/plugins/jetpack/modules/masterbar/admin-menu/class-base-admin-menu.php
+++ projects/plugins/jetpack/modules/masterbar/admin-menu/class-base-admin-menu.php
@@ -256,6 +256,12 @@ abstract class Base_Admin_Menu {
 	 */
 	public function override_svg_icons() {
 		global $menu;
+
+		// Only do this if we're not in an API request, as we override the $menu global.
+		if ( $this->is_api_request ) {
+			return;
+		}
+
 		$svg_items = array();
 		foreach ( $menu as $idx => $menu_item ) {
 			if ( count( $menu_item ) > 6 && 0 === strpos( $menu_item[6], 'data:image/svg+xml' ) && 'site-card' !== $menu_item[3] ) {
