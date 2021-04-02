5c74c6c28534ddae79fd4b5a05a6f1bda8987b1a
Renzo Canepa <rcanepag@gmail.com>
2021-04-02 10:08:20 -0300

    [not verified] Remove unnecessary comment

--- projects/plugins/jetpack/_inc/client/test/test-utils.js
+++ projects/plugins/jetpack/_inc/client/test/test-utils.js
@@ -1,4 +1,3 @@
-// test-utils.js
 import React from 'react';
 import { render as rtlRender } from '@testing-library/react';
 import { createStore } from 'redux';
@@ -25,5 +24,6 @@ function render(
 
 // re-export everything
 export * from '@testing-library/react';
+
 // override render method
 export { render };
