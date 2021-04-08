9e7931be97c8f58cab96a0ac70132ebd61248eca
Bogdan Ungureanu <bogdanungureanu21@gmail.com>
2021-04-08 12:24:35 +0300

    Added hotfix for plugins that depend on index.php/index.php submenu items https://github.com/Automattic/wpcomsh/pull/666
    * Added hotfix for plugins that depend on index.php/index.php submenu item to register their pages.
    
    * Added wpcomsh prefix and added the check for disabling nav-unification.
    
    * Version bump

--- projects/plugins/wpcomsh/feature-plugins/nav-unification-hotfixes.php
+++ projects/plugins/wpcomsh/feature-plugins/nav-unification-hotfixes.php
@@ -9,6 +9,42 @@
 use Automattic\Jetpack\Status;
 
 /**
+ * Fix third-party dependencies that expect index.php submenu item to be available.
+ */
+function wpcomsh_add_index_page_hotfix() {
+	global $submenu;
+
+	// Do not run if Jetpack is not enabled.
+	if ( ! defined( 'JETPACK__VERSION' ) ) {
+		return;
+	}
+
+	// Do not clash with the fix already shipped in Jetpack 9.6.
+	if ( version_compare( JETPACK__VERSION, '9.7-alpha', '>=' ) ) {
+		return;
+	}
+
+	// Safety - don't alter anything if Nav Unification is not enabled.
+	if ( ! wpcomsh_activate_nav_unification( false ) ) {
+		return;
+	}
+
+	add_submenu_page( 'index.php', '', '', 'read', 'index.php', '', 10 );
+
+
+	foreach ( $submenu['index.php'] as $index => $item ) {
+		if ( 'index.php' !== $item[2] ) {
+			continue;
+		}
+
+		// phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+		$submenu['index.php'][ $index ][4] = 'hide-if-no-js hide-if-js';
+	}
+}
+
+add_action( 'admin_menu', 'wpcomsh_add_index_page_hotfix', 1 );
+
+/**
  * Deprecates gracefully Links menu item.
  * Can be removed after Jetpack 9.6 release.
  */
