ecaf3d7a7da542bc02ef2c47c303a9ec77ced98c
Jasper Kang <jasper.kang@automattic.com>
2021-04-13 16:51:49 +1200

    [not verified] finished search with default settings e2e test

--- projects/plugins/jetpack/tests/e2e/lib/pages/homepage.js
+++ projects/plugins/jetpack/tests/e2e/lib/pages/homepage.js
@@ -2,8 +2,12 @@
  * Internal dependencies
  */
 import WpPage from './wp-page';
+import logger from '../logger';
+import testResponse1 from './data/search-results-for-test1.json';
+import testResponse2 from './data/search-results-for-test2.json';
 
 export default class Homepage extends WpPage {
+	static SEARCH_API_PATTERN = /^https:\/\/public-api.wordpress.com\/rest\/v1.3\/sites\/\d+\/search.*/;
 	constructor( page ) {
 		const url = `${ siteUrl }/`;
 		super( page, { expectedSelectors: [ '.post' ], url } );
@@ -14,37 +18,115 @@ export default class Homepage extends WpPage {
 	}
 
 	searchAPIRoute() {
-		return this.page.route(
-			/^https:\/\/public-api.wordpress.com\/rest\/v1.3\/sites\/\d+\/search.*/,
-			( route, request ) => {
-				console.log( request.url() );
-				route.fulfill( {
-					content: 'application/json',
-					headers: { 'Access-Control-Allow-Origin': '*' },
-					path: __dirname + '/data/search-results.json',
-				} );
+		return this.page.route( Homepage.SEARCH_API_PATTERN, ( route, request ) => {
+			logger.info( `intercepted search API call: ${ request.url() }` );
+			const url = new URL( request.url() );
+			const params = url.searchParams;
+
+			// load response for queries
+			let body = {};
+			switch ( params.get( 'query' ) ) {
+				case 'test1':
+					body = { ...testResponse1 };
+					break;
+				case 'test2':
+					body = { ...testResponse2 };
+					break;
 			}
-		);
+
+			// sorting
+			switch ( params.get( 'sort' ) ) {
+				case 'date_asc':
+					// put record 2 first
+					const tmpResult1 = body.results[ 0 ];
+					body.results[ 0 ] = body.results[ 1 ];
+					body.results[ 1 ] = tmpResult1;
+					break;
+				case 'date_desc':
+					// put record 3 first
+					const tmpResult2 = body.results[ 0 ];
+					body.results[ 0 ] = body.results[ 2 ];
+					body.results[ 2 ] = tmpResult2;
+					break;
+				case 'score_default':
+				default:
+					// the original sorting
+					break;
+			}
+
+			// filtering
+			// filter[bool][must][0][term][category.slug]=category-1
+			// filter[bool][must][2][term][tag.slug]=tag-1
+
+			route.fulfill( {
+				content: 'application/json',
+				headers: { 'Access-Control-Allow-Origin': '*' },
+				body: JSON.stringify( body ),
+			} );
+		} );
 	}
 
-	focusSearchInput() {
+	async focusSearchInput() {
 		const searchInputSelector = 'input.search-field';
 		return this.focus( searchInputSelector );
 	}
 
-	enterQuery( query = 'test' ) {
+	async enterQuery( query = 'test1', clear = true ) {
 		const searchInputSelector = 'input.search-field';
+		if ( clear ) {
+			await this.clear( searchInputSelector );
+		}
 		return this.fill( searchInputSelector, query );
 	}
 
-	isSearchResultOverlayVisible() {
-		const overlaySelector = '.jetpack-instant-search__overlay';
-		return this.isElementVisible( overlaySelector );
+	async clickSortingOption( sorting = 'relevance' ) {
+		const sortingNewestSelector = `.jetpack-instant-search__search-sort-option[data-value="${ sorting }"]`;
+		return this.click( sortingNewestSelector );
 	}
 
-	async isSearchResultAvailable() {
+	async clickCrossToCloseOverlay() {
+		const crossSelector = 'button.jetpack-instant-search__overlay-close';
+		return this.click( crossSelector );
+	}
+
+	async waitForSearchResponse() {
+		await this.page.waitForResponse( resp => Homepage.SEARCH_API_PATTERN.test( resp.url() ) );
+		return await this.waitForTimeout( 1000 );
+	}
+
+	async isSortOptionSelected( sorting = 'relevance' ) {
+		const sortingNewestSelector = `.is-selected.jetpack-instant-search__search-sort-option[data-value="${ sorting }"]`;
+		return this.isElementVisible( sortingNewestSelector );
+	}
+
+	async isOverlayVisible() {
+		const overlaySelector = '.jetpack-instant-search__overlay.is-hidden';
+		return ! ( await this.isElementVisible( overlaySelector ) );
+	}
+
+	async isSearchResultVisible() {
 		const searchResultTitleSelector = '.jetpack-instant-search__search-result';
-		await this.waitForNetworkIdle();
+		await this.waitForElementToBeAttached( searchResultTitleSelector );
 		return this.isElementVisible( searchResultTitleSelector );
+	}
+
+	async getFirstResultTitle() {
+		const resultTitleSelector = '.jetpack-instant-search__search-result-title-link';
+		return page.innerHTML( resultTitleSelector );
+	}
+
+	async isSearchFormVisible() {
+		const searchFormSelector = '.jetpack-instant-search__search-results-search-form';
+		return this.isElementVisible( searchFormSelector );
+	}
+
+	async isSortingVisible() {
+		const sortingSelector = '.jetpack-instant-search__search-results-search-form';
+		return this.isElementVisible( sortingSelector );
+	}
+
+	async isFilteringVisible() {
+		const filteringSelector = '.jetpack-instant-search__search-sort-option';
+		return this.isElementVisible( filteringSelector );
 	}
 }
--- projects/plugins/jetpack/tests/e2e/specs/search.test.js
+++ projects/plugins/jetpack/tests/e2e/specs/search.test.js
@@ -4,8 +4,11 @@
 import { syncJetpackPlanData } from '../lib/flows/jetpack-connect';
 import { activateModule, execMultipleWpCommands } from '../lib/utils-helper';
 import Homepage from '../lib/pages/homepage';
+import { step } from '../lib/env/test-setup';
 
 describe( 'Search', () => {
+	let homepage;
+
 	beforeAll( async () => {
 		await syncJetpackPlanData( 'complete' );
 		await activateModule( page, 'search' );
@@ -16,14 +19,67 @@ describe( 'Search', () => {
 		await execMultipleWpCommands( 'wp jetpack module deactivate search' );
 	} );
 
-	it( 'can open the search modal', async () => {
-		const homepage = await Homepage.visit( page );
-		// NOTE: This ideally would better be in init, not to to be called directly
+	beforeEach( async () => {
+		homepage = await Homepage.visit( page );
 		await homepage.registerRouteInterceptions();
-		await homepage.focusSearchInput();
-		await homepage.enterQuery();
-		await page.pause();
-		expect( await homepage.isSearchResultOverlayVisible() ).toBeTruthy();
-		expect( await homepage.isSearchResultAvailable() ).toBeTruthy();
+	} );
+
+	it( 'Can perform search with default settings', async () => {
+		await step( 'Can open the overlay by entering a query', async () => {
+			await homepage.focusSearchInput();
+			await homepage.enterQuery();
+			await homepage.waitForSearchResponse();
+
+			expect( await homepage.isOverlayVisible() ).toBeTruthy();
+		} );
+
+		await step( 'Can show search controls in the overlay', async () => {
+			expect( await homepage.isSearchFormVisible() ).toBeTruthy();
+			expect( await homepage.isSortingVisible() ).toBeTruthy();
+		} );
+
+		await step( 'Can show search results in the overlay', async () => {
+			expect( await homepage.isSearchResultVisible() ).toBeTruthy();
+		} );
+
+		await step( 'Can default show results by relevance', async () => {
+			expect( await homepage.getFirstResultTitle() ).toBe( '<mark>Test1</mark> Record 1' );
+		} );
+
+		await step( 'Can edit query in search form', async () => {
+			await homepage.enterQuery( 'test2' );
+			await homepage.waitForSearchResponse();
+
+			expect( await homepage.getFirstResultTitle() ).toBe( '<mark>Test2</mark> Record 1' );
+		} );
+
+		await step( 'Can change sorting', async () => {
+			await homepage.clickSortingOption( 'newest' );
+			await homepage.isSortOptionSelected( 'newest' );
+			await homepage.waitForSearchResponse();
+
+			expect( await homepage.getFirstResultTitle() ).toBe( '<mark>Test2</mark> Record 3' );
+
+			await homepage.clickSortingOption( 'oldest' );
+			await homepage.isSortOptionSelected( 'oldest' );
+			await homepage.waitForSearchResponse();
+
+			expect( await homepage.getFirstResultTitle() ).toBe( '<mark>Test2</mark> Record 2' );
+
+			await homepage.clickSortingOption( 'relevance' );
+			await homepage.isSortOptionSelected( 'relevance' );
+			// wait for animation and rendering
+			await page.waitForTimeout( 1000 );
+
+			expect( await homepage.getFirstResultTitle() ).toBe( '<mark>Test2</mark> Record 1' );
+		} );
+
+		await step( 'Can close overlay by clicking the cross', async () => {
+			await homepage.clickCrossToCloseOverlay();
+			// wait for animation and rendering
+			await page.waitForTimeout( 1000 );
+
+			expect( await homepage.isOverlayVisible() ).toBe( false );
+		} );
 	} );
 } );
