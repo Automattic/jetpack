1dc9884ccd5fd5e3e4b84cd9c13c6b439f045572
Allison Levine <allison.levine@automattic.com>
2021-04-20 18:23:44 -0400

    Add unit tests

--- projects/plugins/jetpack/tests/php/json-api/test-class.json-api-jetpack-sync-endpoints.php
+++ projects/plugins/jetpack/tests/php/json-api/test-class.json-api-jetpack-sync-endpoints.php
@@ -110,4 +110,77 @@ class WP_Test_Jetpack_Sync_Json_Api_Endpoints extends WP_UnitTestCase {
 		// Verify WP_Error returned on invalid stati.
 		$this->assertInstanceOf( 'WP_Error', $response );
 	}
+
+	/**
+	 * Unit test for the `/sites/%s/clear/transient` endpoint with valid input.
+	 */
+	public function test_clear_transient() {
+		global $blog_id;
+
+		$endpoint = new Jetpack_JSON_API_Sync_Clear_Transient_Endpoint(
+			array(
+				'description'             => 'Clear transient',
+				'method'                  => 'POST',
+				'group'                   => '__do_not_document',
+				'path'                    => '/sites/%s/clear/transient',
+				'stat'                    => 'clear-transient',
+				'allow_jetpack_site_auth' => true,
+				'path_labels'             => array(
+					'$site' => '(int|string) The site ID, The site domain',
+				),
+				'request_format'          => array(
+					'name' => '(string) Name of transient to clear',
+				),
+				'response_format'         => array(
+					'response' => '(bool) Whether transient was deleted',
+				),
+				'example_request'         => 'https://public-api.wordpress.com/rest/v1.1/sites/example.wordpress.org/clear/transient',
+			)
+		);
+
+		// set input.
+		$endpoint->api->post_body    = '{ "name": "jetpack_connected_user_data_1" }';
+		$endpoint->api->content_type = 'application/json';
+
+		$response = $endpoint->callback( 'clear/transient', $blog_id );
+
+		$this->assertEquals( 'in_sync', $response['success'] );
+	}
+
+	/**
+	 * Unit test for the `/sites/%s/clear/transient` endpoint with invalid input.
+	 */
+	public function test_clear_transient_error() {
+		global $blog_id;
+
+		$endpoint = new Jetpack_JSON_API_Sync_Clear_Transient_Endpoint(
+			array(
+				'description'             => 'Clear transient',
+				'method'                  => 'POST',
+				'group'                   => '__do_not_document',
+				'path'                    => '/sites/%s/clear/transient',
+				'stat'                    => 'clear-transient',
+				'allow_jetpack_site_auth' => true,
+				'path_labels'             => array(
+					'$site' => '(int|string) The site ID, The site domain',
+				),
+				'request_format'          => array(
+					'name' => '(string) Name of transient to clear',
+				),
+				'response_format'         => array(
+					'response' => '(bool) Whether transient was deleted',
+				),
+				'example_request'         => 'https://public-api.wordpress.com/rest/v1.1/sites/example.wordpress.org/clear/transient',
+			)
+		);
+
+		// set input.
+		$endpoint->api->post_body    = '{}';
+		$endpoint->api->content_type = 'application/json';
+
+		$response = $endpoint->callback( 'clear/transient', $blog_id );
+
+		// Verify WP_Error returned if no transient name provided.
+		$this->assertInstanceOf( 'WP_Error', $response );
+	}
 }
