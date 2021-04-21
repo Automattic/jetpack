23026cb2fbf618c75287ce991bf640b2c6f6f433
Staci Cooper <staci.cooper@automattic.com>
2021-04-21 16:31:04 -0700

    [not verified] Return early if related-posts block is not present on the page
    Temporarily, the Related Posts assets will be enqueued for FSE sites
    with the feature enabled even if the block is not present (this will
    be fixed in a future iteration). To avoid console errors in this
    state we should return early from the script if the block is not
    found.

--- projects/plugins/jetpack/modules/related-posts/related-posts.js
+++ projects/plugins/jetpack/modules/related-posts/related-posts.js
@@ -39,6 +39,9 @@
 
 			var args = 'relatedposts=1';
 			var relatedPosts = document.querySelector( '#jp-relatedposts' );
+			if ( relatedPosts === null ) {
+				return;
+			}
 			if ( relatedPosts.hasAttribute( 'data-exclude' ) ) {
 				args += '&relatedposts_exclude=' + relatedPosts.getAttribute( 'data-exclude' );
 			}
