d35bbfd70972f93e79f5709fc35ae33f4a3808c3
Foteini Giannaropoulou <giannaropoulou.foteini@gmail.com>
2021-04-21 15:02:31 +0300

    REST API: Update list modules v1.2 endpoint

--- projects/plugins/jetpack/json-endpoints/jetpack/class-jetpack-json-api-modules-list-v1-2-endpoint.php
+++ projects/plugins/jetpack/json-endpoints/jetpack/class-jetpack-json-api-modules-list-v1-2-endpoint.php
@@ -9,19 +9,28 @@ use Automattic\Jetpack\Status;
 /**
  * List modules v1.2 endpoint.
  */
-class Jetpack_JSON_API_Modules_List_V1_2_Endpoint extends Jetpack_JSON_API_Modules_List_Endpoint {
+class Jetpack_JSON_API_Modules_List_V1_2_Endpoint extends Jetpack_JSON_API_Endpoint {
 
 	/**
-	 * Override parent method to set the modules class property.
+	 * This endpoint allows authentication both via a blog and a user token.
+	 * If a user token is used, that user should have `jetpack_manage_modules` capability.
 	 *
-	 * @param  string $module The module slug.
-	 * @return bool true
+	 * @var array|string
 	 */
-	public function validate_input( $module ) { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
+	protected $needed_capabilities = 'jetpack_manage_modules';
+
+	/**
+	 * Fetch modules list.
+	 *
+	 * @return array An array of module objects.
+	 */
+	protected function result() {
 		require_once JETPACK__PLUGIN_DIR . 'class.jetpack-admin.php';
+
 		$is_offline_mode = ( new Status() )->is_offline_mode();
 
 		$modules = Jetpack_Admin::init()->get_modules();
+
 		foreach ( $modules as $slug => $properties ) {
 			if ( $is_offline_mode ) {
 				$requires_connection      = isset( $modules[ $slug ]['requires_connection'] ) && $modules[ $slug ]['requires_connection'];
@@ -36,29 +45,6 @@ class Jetpack_JSON_API_Modules_List_V1_2_Endpoint extends Jetpack_JSON_API_Modul
 
 		$modules = Jetpack::get_translated_modules( $modules );
 
-		$this->modules = $modules;
-
-		return true;
-	}
-
-	/**
-	 * Format a list of modules for public display, using the supplied offset and limit args.
-	 *
-	 * @uses   WPCOM_JSON_API_Endpoint::query_args()
-	 * @return array         Public API modules objects
-	 */
-	protected function get_modules() {
-		$modules = array_values( $this->modules );
-		// do offset & limit - we've already returned a 400 error if they're bad numbers.
-		$args = $this->query_args();
-
-		if ( isset( $args['offset'] ) ) {
-			$modules = array_slice( $modules, (int) $args['offset'] );
-		}
-		if ( isset( $args['limit'] ) ) {
-			$modules = array_slice( $modules, 0, (int) $args['limit'] );
-		}
-
-		return $modules;
+		return array( 'modules' => $modules );
 	}
 }
