68d366e9e492338fd53c03c3ef77ef7fdf5565d1
Paul Bunkham <paul@dobit.co.uk>
2021-04-22 22:55:19 +0100

    [not verified] Admin Menu: Fix invalid menu item ID attributes
    Some menu items are pointing to full URLs. It turns out these have a
    number of invalid characters that aren't getting stripped out by the
    core WordPress functions. This meant that invalid ID attributes were
    being set and that in turn broke the SVG styling introduced in #19364.
    
    This PR fixes the ID, allowing the icon to be properly displayed.

--- projects/plugins/jetpack/modules/masterbar/admin-menu/class-base-admin-menu.php
+++ projects/plugins/jetpack/modules/masterbar/admin-menu/class-base-admin-menu.php
@@ -389,16 +389,21 @@ abstract class Base_Admin_Menu {
 
 		$svg_items = array();
 		foreach ( $menu as $idx => $menu_item ) {
-			if ( count( $menu_item ) > 6 && 0 === strpos( $menu_item[6], 'data:image/svg+xml' ) && 'site-card' !== $menu_item[3] ) {
-				$svg_items[] = array(
-					'icon' => $menu[ $idx ][6],
-					'id'   => $menu[ $idx ][5],
-				);
-				// phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-				$menu[ $idx ][6] = 'none';
-				// phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-				$menu[ $idx ][4] .= ' menu-svg-icon';
+			// If the hookname looks to contain a URL than sanitize it by remplacing invalid characters.
+			if ( false !== strpos( $menu_item[5], '://' ) ) {
+				$menu_item[5] = preg_replace( '![:/.]+!', '_', $menu_item[5] );
 			}
+
+			if ( count( $menu_item ) > 6 && 0 === strpos( $menu_item[6], 'data:image/svg+xml' ) && 'site-card' !== $menu_item[3] ) {
+				$svg_items[]   = array(
+					'icon' => $menu_item[6],
+					'id'   => $menu_item[5],
+				);
+				$menu_item[4] .= ' menu-svg-icon';
+				$menu_item[6]  = 'none';
+			}
+			// phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+			$menu[ $idx ] = $menu_item;
 		}
 		if ( count( $svg_items ) > 0 ) {
 			$styles = '.menu-svg-icon .wp-menu-image { background-repeat: no-repeat; background-position: center center } ';

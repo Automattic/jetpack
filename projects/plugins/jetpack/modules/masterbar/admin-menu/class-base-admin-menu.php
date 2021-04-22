Automattic/jetpack

commit 466f18e1c85d3410e483fd60d5beb2b5166a2576

Admin Menu: Update Jetpack logo SVG and menu SVG icon styling (#19364)
* [not verified] Admin Menu: Update Jetpack Logo SVG

The current SVG had the inner triangles set as white polygons. This
version is a single path with the triangles cut out, so that it will sit
better on any background colour.

* [not verified] Adding changelog entry

* [not verified] Override styles for wp-admin

* [not verified] Remove the use of opacity.

* [not verified] Don't override on an API request

* [not verified] Add -webkit prefixed mask properties

* [not verified] Fix the unit tests

* [not verified] Add mask-size property

projects/plugins/jetpack/modules/masterbar/admin-menu/class-base-admin-menu.php
@@ -38,8 +38,9 @@ abstract class Base_Admin_Menu {
 	 * Base_Admin_Menu constructor.
 	 */
 	protected function __construct() {
-		add_action( 'admin_menu', array( $this, 'set_is_api_request' ), 99998 );
-		add_action( 'admin_menu', array( $this, 'reregister_menu_items' ), 99999 );
+		add_action( 'admin_menu', array( $this, 'set_is_api_request' ), 99997 );
+		add_action( 'admin_menu', array( $this, 'reregister_menu_items' ), 99998 );
+		add_filter( 'admin_menu', array( $this, 'override_svg_icons' ), 99999 );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 		add_filter( 'rest_request_before_callbacks', array( $this, 'rest_api_init' ), 11 );
 
@@ -247,6 +248,58 @@ abstract class Base_Admin_Menu {
 	 */
 	public function is_rtl() {
 		return is_rtl();
+	}
+
+	/**
+	 * Checks for any SVG icons in the menu, and overrides things so that
+	 * we can display the icon in the correct colour for the theme.
+	 */
+	public function override_svg_icons() {
+		global $menu;
+
+		// Only do this if we're not in an API request, as we override the $menu global.
+		if ( $this->is_api_request ) {
+			return;
+		}
+
+		$svg_items = array();
+		foreach ( $menu as $idx => $menu_item ) {
+			if ( count( $menu_item ) > 6 && 0 === strpos( $menu_item[6], 'data:image/svg+xml' ) && 'site-card' !== $menu_item[3] ) {
+				$svg_items[] = array(
+					'icon' => $menu[ $idx ][6],
+					'id'   => $menu[ $idx ][5],
+				);
+				// phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+				$menu[ $idx ][6] = 'none';
+				// phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+				$menu[ $idx ][4] .= ' menu-svg-icon';
+			}
+		}
+		if ( count( $svg_items ) > 0 ) {
+			$styles = '.menu-svg-icon .wp-menu-image { background-repeat: no-repeat; background-position: center center } ';
+			foreach ( $svg_items as $svg_item ) {
+				$styles .= sprintf( '#%s .wp-menu-image { background-image: url( "%s" ) }', $svg_item['id'], $svg_item['icon'] );
+			}
+			$styles .= '@supports ( mask-image: none ) or ( -webkit-mask-image: none ) { ';
+			$styles .= '.menu-svg-icon .wp-menu-image { background-image: none; } ';
+			$styles .= '.menu-svg-icon .wp-menu-image::before { background-color: currentColor; ';
+			$styles .= 'mask-size: contain; mask-position: center center; mask-repeat: no-repeat; ';
+			$styles .= '-webkit-mask-size: contain; -webkit-mask-position: center center; -webkit-mask-repeat: no-repeat; content:"" } ';
+			foreach ( $svg_items as $svg_item ) {
+				$styles .= sprintf(
+					'#%s .wp-menu-image { background-image: none; } #%s .wp-menu-image::before{ mask-image: url( "%s" ); -webkit-mask-image: url( "%s" ) }',
+					$svg_item['id'],
+					$svg_item['id'],
+					$svg_item['icon'],
+					$svg_item['icon']
+				);
+			}
+			$styles .= '}';
+
+			wp_register_style( 'svg-menu-overrides', false, array(), '20210331' );
+			wp_enqueue_style( 'svg-menu-overrides' );
+			wp_add_inline_style( 'svg-menu-overrides', $styles );
+		}
 	}
 
 	/**

projects/plugins/jetpack/modules/masterbar/admin-menu/class-admin-menu.php
@@ -380,7 +380,7 @@ class Admin_Menu extends Base_Admin_Menu {
 		$this->add_admin_menu_separator( 50, 'manage_options' );
 
 		// TODO: Replace with proper SVG data url.
-		$icon = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 32 32' %3E%3Cpath fill='%23a0a5aa' d='M16,0C7.2,0,0,7.2,0,16s7.2,16,16,16s16-7.2,16-16S24.8,0,16,0z'%3E%3C/path%3E%3Cpolygon fill='%23fff' points='15,19 7,19 15,3 '%3E%3C/polygon%3E%3Cpolygon fill='%23fff' points='17,29 17,13 25,13 '%3E%3C/polygon%3E%3C/svg%3E";
+		$icon = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 40 40' %3E%3Cpath fill='%23a0a5aa' d='M20 0c11.046 0 20 8.954 20 20s-8.954 20-20 20S0 31.046 0 20 8.954 0 20 0zm11 17H21v19l10-19zM19 4L9 23h10V4z'/%3E%3C/svg%3E";
 
 		$is_menu_updated = $this->update_menu( 'jetpack', null, null, null, $icon, 51 );
 		if ( ! $is_menu_updated ) {

projects/plugins/jetpack/tests/php/modules/masterbar/test-class-atomic-admin-menu.php
@@ -100,7 +100,7 @@ class Test_Atomic_Admin_Menu extends WP_UnitTestCase {
 		$this->assertInstanceOf( Atomic_Admin_Menu::class, $instance );
 		$this->assertSame( $instance, static::$admin_menu );
 
-		$this->assertSame( 99999, has_action( 'admin_menu', array( $instance, 'reregister_menu_items' ) ) );
+		$this->assertSame( 99998, has_action( 'admin_menu', array( $instance, 'reregister_menu_items' ) ) );
 		$this->assertSame( 10, has_action( 'admin_enqueue_scripts', array( $instance, 'enqueue_scripts' ) ) );
 	}
 

projects/plugins/jetpack/tests/php/modules/masterbar/test-class-base-admin-menu.php
@@ -84,7 +84,7 @@ class Test_Base_Admin_Menu extends WP_UnitTestCase {
 		$this->assertInstanceOf( Base_Admin_Menu::class, $instance );
 		$this->assertSame( $instance, static::$admin_menu );
 
-		$this->assertSame( 99999, has_action( 'admin_menu', array( $instance, 'reregister_menu_items' ) ) );
+		$this->assertSame( 99998, has_action( 'admin_menu', array( $instance, 'reregister_menu_items' ) ) );
 		$this->assertSame( 10, has_action( 'admin_enqueue_scripts', array( $instance, 'enqueue_scripts' ) ) );
 	}
 

projects/plugins/jetpack/tests/php/modules/masterbar/test-class-wpcom-admin-menu.php
@@ -100,7 +100,7 @@ class Test_WPcom_Admin_Menu extends WP_UnitTestCase {
 		$this->assertInstanceOf( WPcom_Admin_Menu::class, $instance );
 		$this->assertSame( $instance, static::$admin_menu );
 
-		$this->assertSame( 99999, has_action( 'admin_menu', array( $instance, 'reregister_menu_items' ) ) );
+		$this->assertSame( 99998, has_action( 'admin_menu', array( $instance, 'reregister_menu_items' ) ) );
 		$this->assertSame( 10, has_action( 'admin_enqueue_scripts', array( $instance, 'enqueue_scripts' ) ) );
 	}
 

commit 68d366e9e492338fd53c03c3ef77ef7fdf5565d1

[not verified] Admin Menu: Fix invalid menu item ID attributes
Some menu items are pointing to full URLs. It turns out these have a
number of invalid characters that aren't getting stripped out by the
core WordPress functions. This meant that invalid ID attributes were
being set and that in turn broke the SVG styling introduced in #19364.

This PR fixes the ID, allowing the icon to be properly displayed.

projects/plugins/jetpack/modules/masterbar/admin-menu/class-base-admin-menu.php
@@ -389,16 +389,21 @@ abstract class Base_Admin_Menu {
 
 		$svg_items = array();
 		foreach ( $menu as $idx => $menu_item ) {
-			if ( count( $menu_item ) > 6 && 0 === strpos( $menu_item[6], 'data:image/svg+xml' ) && 'site-card' !== $menu_item[3] ) {
-				$svg_items[] = array(
-					'icon' => $menu[ $idx ][6],
-					'id'   => $menu[ $idx ][5],
-				);
-				// phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-				$menu[ $idx ][6] = 'none';
-				// phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-				$menu[ $idx ][4] .= ' menu-svg-icon';
+			// If the hookname looks to contain a URL than sanitize it by remplacing invalid characters.
+			if ( false !== strpos( $menu_item[5], '://' ) ) {
+				$menu_item[5] = preg_replace( '![:/.]+!', '_', $menu_item[5] );
 			}
+
+			if ( count( $menu_item ) > 6 && 0 === strpos( $menu_item[6], 'data:image/svg+xml' ) && 'site-card' !== $menu_item[3] ) {
+				$svg_items[]   = array(
+					'icon' => $menu_item[6],
+					'id'   => $menu_item[5],
+				);
+				$menu_item[4] .= ' menu-svg-icon';
+				$menu_item[6]  = 'none';
+			}
+			// phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+			$menu[ $idx ] = $menu_item;
 		}
 		if ( count( $svg_items ) > 0 ) {
 			$styles = '.menu-svg-icon .wp-menu-image { background-repeat: no-repeat; background-position: center center } ';