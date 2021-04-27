6cfb78476776dc73e76c78f77a5108e5b13518b4
Kim Brown <50059399+kbrown9@users.noreply.github.com>
2021-04-27 15:38:47 -0400

    JITM: Use the Device_Detection package to determine if the device is mobile (#19567)
    * JITM: Use the Device_Detection package to determine if the device is mobile.
    
    The JITM package needs to be independent of the Jetpack plugin. Replace the
    jetpack_is_mobile() function call with Device_Detection::is_smartphone(). Also
    add the jetpack-device-detection package to the JITM package's requirements.

--- projects/packages/jitm/src/class-post-connection-jitm.php
+++ projects/packages/jitm/src/class-post-connection-jitm.php
@@ -10,6 +10,7 @@ namespace Automattic\Jetpack\JITMS;
 use Automattic\Jetpack\A8c_Mc_Stats;
 use Automattic\Jetpack\Connection\Client;
 use Automattic\Jetpack\Connection\Manager;
+use Automattic\Jetpack\Device_Detection;
 use Automattic\Jetpack\Partner;
 use Automattic\Jetpack\Redirect;
 use Automattic\Jetpack\Status;
@@ -378,7 +379,7 @@ class Post_Connection_JITM extends JITM {
 				'external_user_id' => urlencode_deep( $user->ID ),
 				'user_roles'       => urlencode_deep( $user_roles ),
 				'query_string'     => urlencode_deep( $query ),
-				'mobile_browser'   => jetpack_is_mobile( 'smart' ) ? 1 : 0,
+				'mobile_browser'   => Device_Detection::is_smartphone() ? 1 : 0,
 				'_locale'          => get_user_locale(),
 			),
 			sprintf( '/sites/%d/jitm/%s', $site_id, $message_path )
