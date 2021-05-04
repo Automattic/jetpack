62892ec2befa2aab15e9249f860347e571a2a5bc
Aurorum <torres126@clubpenguinmountains.com>
2021-05-04 16:35:39 +0100

    Update link

--- projects/plugins/jetpack/_inc/client/traffic/verification-services.jsx
+++ projects/plugins/jetpack/_inc/client/traffic/verification-services.jsx
@@ -121,7 +121,7 @@ class VerificationServicesComponent extends React.Component {
 						{ createInterpolateElement(
 							/* translators: placeholders are links to external sites. */
 							__(
-								'Note that <b>verifying your site with these services is not necessary</b> in order for your site to be indexed by search engines. To use these advanced search engine tools and verify your site with a service, paste the HTML Tag code below. Read the <support>full instructions</support> if you are having trouble. Supported verification services: <google>Google Search Console</google>, <bing>Bing Webmaster Center</bing>, <pinterest>Pinterest Site Verification</pinterest>, <yandex>Yandex.Webmaster</yandex>, and <facebook>Facebook</facebook>.',
+								'Note that <b>verifying your site with these services is not necessary</b> in order for your site to be indexed by search engines. To use these advanced search engine tools and verify your site with a service, paste the HTML Tag code below. Read the <support>full instructions</support> if you are having trouble. Supported verification services: <google>Google Search Console</google>, <bing>Bing Webmaster Center</bing>, <pinterest>Pinterest Site Verification</pinterest>, <yandex>Yandex.Webmaster</yandex>, and <facebook>Facebook Domain Verification</facebook>.',
 								'jetpack'
 							),
 							{
@@ -164,7 +164,7 @@ class VerificationServicesComponent extends React.Component {
 										icon={ true }
 										target="_blank"
 										rel="noopener noreferrer"
-										href="https://developers.facebook.com/"
+										href="https://business.facebook.com/settings/"
 									/>
 								),
 							}
