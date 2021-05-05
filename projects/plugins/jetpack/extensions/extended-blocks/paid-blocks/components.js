238cd7c722ee6dbfe736346bc603e96602222c8c
Staci Cooper <staci.cooper@automattic.com>
2021-05-05 13:10:55 -0700

    [not verified] Add banner visibility state to the banner context

--- projects/plugins/jetpack/extensions/extended-blocks/paid-blocks/components.js
+++ projects/plugins/jetpack/extensions/extended-blocks/paid-blocks/components.js
@@ -14,9 +14,19 @@ export const PaidBlockContext = createContext();
  *
  * @param {object}  props - Provider properties.
  * @param {Function}  props.onBannerVisibilityChange - Callback to set banner visibility.
+ * @param {boolean} props.isParentBannerVisible - True if the banner is visible for a parent of this block.
+ * @param {boolean} props.hasParentBanner - True if a parent of this block has a banner, which may or may not be visible.
  * @param {boolean} props.children - Provider Children.
  * @returns {*} Provider component.
  */
-export const PaidBlockProvider = ( { onBannerVisibilityChange, children } ) => (
-	<PaidBlockContext.Provider value={ onBannerVisibilityChange } children={ children } />
+export const PaidBlockProvider = ( {
+	onBannerVisibilityChange,
+	isParentBannerVisible,
+	hasParentBanner,
+	children,
+} ) => (
+	<PaidBlockContext.Provider
+		value={ { onBannerVisibilityChange, isParentBannerVisible, hasParentBanner } }
+		children={ children }
+	/>
 );
