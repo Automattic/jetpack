e3d5b478c1315780525d7dd2b639a008f6ddf1e1
cpap <papazoglou.charalampos@gmail.com>
2021-05-07 18:08:52 +0300

    [not verified] Reverts domain only changes.

--- projects/plugins/jetpack/modules/masterbar/admin-menu/class-domain-only-admin-menu.php
+++ projects/plugins/jetpack/modules/masterbar/admin-menu/class-domain-only-admin-menu.php
@@ -36,46 +36,6 @@ class Domain_Only_Admin_Menu extends Base_Admin_Menu {
 		$menu    = array(); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
 		$submenu = array(); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
 
-		if ( ! $this->is_api_request ) {
-			$nudge = $this->get_upsell_nudge();
-			if ( $nudge ) {
-				parent::add_upsell_nudge( $nudge );
-			}
-		}
-
 		add_menu_page( esc_attr__( 'Settings', 'jetpack' ), __( 'Settings', 'jetpack' ), 'manage_options', 'https://wordpress.com/domains/manage/' . $this->domain, null, 'dashicons-admin-settings' );
-	}
-
-	/**
-	 * Returns the first available upsell nudge.
-	 *
-	 * @return array
-	 */
-	public function get_upsell_nudge() {
-		require_lib( 'jetpack-jitm/jitm-engine' );
-		$jitm_engine = new JITM\Engine();
-
-		$message_path = 'calypso:sites:sidebar_notice';
-		$current_user = wp_get_current_user();
-		$user_id      = $current_user->ID;
-		$user_roles   = implode( ',', $current_user->roles );
-		$query_string = array(
-			'message_path' => 'calypso:sites:sidebar_notice',
-		);
-
-		// Get the top message only.
-		$message = $jitm_engine->get_top_messages( $message_path, $user_id, $user_roles, $query_string )[0];
-
-		if ( $message ) {
-			return array(
-				'content'                      => $message->content['message'],
-				'cta'                          => $message->CTA['message'], // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
-				'link'                         => $message->CTA['link'], // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
-				'tracks_impression_event_name' => $message->tracks['display']['name'],
-				'tracks_impression_cta_name'   => $message->tracks['display']['props']['cta_name'],
-				'tracks_click_event_name'      => $message->tracks['click']['name'],
-				'tracks_click_cta_name'        => $message->tracks['click']['props']['cta_name'],
-			);
-		}
 	}
 }
