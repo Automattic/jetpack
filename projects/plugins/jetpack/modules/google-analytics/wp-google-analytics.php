bc55058444f01c0c171cc7605ad6db7f23fa3024
Douglas Johnson <doug@thoughtcatalog.com>
2021-05-10 12:04:02 -0400

    correction, self::$

--- projects/plugins/jetpack/modules/google-analytics/wp-google-analytics.php
+++ projects/plugins/jetpack/modules/google-analytics/wp-google-analytics.php
@@ -54,10 +54,10 @@ class Jetpack_Google_Analytics {
 		// At this time, we only leverage universal analytics when enhanced ecommerce is selected and WooCommerce is active.
 		// Otherwise, don't bother emitting the tracking ID or fetching analytics.js
 		if ( class_exists( 'WooCommerce' ) && Jetpack_Google_Analytics_Options::enhanced_ecommerce_tracking_is_enabled() ) {
-			self::analytics = new Jetpack_Google_Analytics_Universal();
+			self::$analytics = new Jetpack_Google_Analytics_Universal();
 			$amp_analytics  = new Jetpack_Google_AMP_Analytics();
 		} else {
-			self::analytics = new Jetpack_Google_Analytics_Legacy();
+			self::$analytics = new Jetpack_Google_Analytics_Legacy();
 		}
 	}
 
