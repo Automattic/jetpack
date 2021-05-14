5a31ec18c2e502e76fc60701513a6df149456ed3
Aurorum <torres126@clubpenguinmountains.com>
2021-05-14 12:03:47 +0100

    Site Verification Tools: Add Facebook Domain Verification (#19725)
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>

--- projects/plugins/jetpack/_inc/lib/class.core-rest-api-endpoints.php
+++ projects/plugins/jetpack/_inc/lib/class.core-rest-api-endpoints.php
@@ -2708,6 +2708,13 @@ class Jetpack_Core_Json_Api_Endpoints {
 				'validate_callback' => __CLASS__ . '::validate_verification_service',
 				'jp_group'          => 'verification-tools',
 			),
+			'facebook'                             => array(
+				'description'       => esc_html__( 'Facebook Domain Verification', 'jetpack' ),
+				'type'              => 'string',
+				'default'           => '',
+				'validate_callback' => __CLASS__ . '::validate_verification_service',
+				'jp_group'          => 'verification-tools',
+			),
 
 			// WordAds.
 			'enable_header_ad'                     => array(
