6874bf9ddeb0b0f0a882a37c004aed1dac3d34d0
Jason Moon <jason.moon@automattic.com>
2021-05-14 15:57:20 -0600

    Tweak styling for search dashboard

--- projects/plugins/jetpack/_inc/client/search-dashboard/index.js
+++ projects/plugins/jetpack/_inc/client/search-dashboard/index.js
@@ -1,8 +1,8 @@
 /**
  * External dependencies
  */
-import React from 'react';
-import { Card, SelectControl, ToggleControl } from '@wordpress/components';
+import React, { Fragment, useCallback } from 'react';
+import { SelectControl, ToggleControl } from '@wordpress/components';
 import { useEntityProp } from '@wordpress/core-data';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
@@ -10,6 +10,8 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
+import Button from 'components/button';
+import SectionHeader from 'components/section-header';
 import normalizeColors from './normalize-colors';
 import './style.scss';
 
@@ -52,100 +54,112 @@ export default function SearchDashboard() {
 	);
 	const isSaving = useSelect( select => select( 'core' ).isSavingEntityRecord( 'root', 'site' ) );
 	const { saveEntityRecord } = useDispatch( 'core' );
-	const saveEditedEntityRecords = () => {
+	const saveEditedEntityRecords = useCallback( () => {
 		editedEntities && saveEntityRecord( 'root', 'site', editedEntities );
-	};
+	}, [ editedEntities, saveEntityRecord ] );
 	const hasEditedEntities = editedEntities && Object.keys( editedEntities ).length > 0;
 	const isLoading = ! site;
 
 	return (
-		<Card>
-			<h3>{ __( 'Search Configuration', 'jetpack' ) }</h3>
-			<SelectControl
-				disabled={ isLoading }
-				label={ __( 'Theme', 'jetpack' ) }
-				value={ theme }
-				options={ [
-					{ label: __( 'Light', 'jetpack' ), value: 'light' },
-					{ label: __( 'Dark', 'jetpack' ), value: 'dark' },
-				] }
-				onChange={ setTheme }
-			/>
-			<SelectControl
-				disabled={ isLoading }
-				label={ __( 'Result Format', 'jetpack' ) }
-				value={ resultFormat }
-				options={ [
-					{ label: __( 'Minimal', 'jetpack' ), value: 'minimal' },
-					{ label: __( 'Expanded (shows images)', 'jetpack' ), value: 'expanded' },
-					{ label: __( 'Product (for WooCommerce stores)', 'jetpack' ), value: 'product' },
-				] }
-				onChange={ setResultFormat }
-			/>
-			<SelectControl
-				disabled={ isLoading }
-				label={ __( 'Sort', 'jetpack' ) }
-				value={ sort }
-				options={ [
-					{ label: __( 'Relevance (recommended)', 'jetpack' ), value: 'relevance' },
-					{ label: __( 'Newest first', 'jetpack' ), value: 'newest' },
-					{ label: __( 'Oldest first', 'jetpack' ), value: 'oldest' },
-				] }
-				onChange={ setSort }
-			/>
-			<SelectControl
-				disabled={ isLoading }
-				label={ __( 'Overlay Trigger', 'jetpack' ) }
-				value={ trigger }
-				options={ [
-					{ label: __( 'Open when the user starts typing', 'jetpack' ), value: 'immediate' },
-					{ label: __( 'Open when results are available', 'jetpack' ), value: 'results' },
-				] }
-				onChange={ setTrigger }
-			/>
-			<div className="jetpack-search-color-inputs">
-				<label htmlFor="jetpack-search-highlight-color">Search Highlight Color</label>
-				<input
-					type="color"
-					id="jetpack-search-highlight-color"
-					value={ normalizedColor }
-					// eslint-disable-next-line react/jsx-no-bind
-					onChange={ event => setColor( event.target.value ) }
-				/>
-				<input
-					type="text"
-					value={ isLoading ? __( 'Loading…', 'jetpack' ) : normalizedColor }
-					disabled
-				/>
-			</div>
-			<ToggleControl
-				checked={ sortEnabled }
-				disabled={ isLoading }
-				label={ __( 'Enable Sort', 'jetpack' ) }
-				onChange={ setSortEnabled }
-			/>
-			<ToggleControl
-				checked={ infiniteScroll }
-				disabled={ isLoading }
-				label={ __( 'Enable Infinite Scroll', 'jetpack' ) }
-				onChange={ setInfiniteScroll }
-			/>
-			<ToggleControl
-				checked={ showLogo }
-				disabled={ isLoading }
-				label={ __( "Show 'Powered by Jetpack'", 'jetpack' ) }
-				onChange={ setShowLogo }
-			/>
-			<hr />
+		<Fragment>
 			<div>
-				<button
-					disabled={ isLoading || ! hasEditedEntities || isSaving }
-					/* eslint-disable-next-line react/jsx-no-bind */
-					onClick={ saveEditedEntityRecords }
-				>
-					{ isSaving ? 'Saving...' : 'Save' }
-				</button>
+				<SectionHeader label={ __( 'Jetpack Search', 'jetpack' ) }>
+					{ /* <Button primary compact type="submit">
+						{ isSaving ? __( 'Saving…', 'jetpack' ) : __( 'Save', 'jetpack' ) }
+					</Button> */ }
+				</SectionHeader>
+				<div className="dops-card">TODO: Add Search module and Instant Search toggles here.</div>
 			</div>
-		</Card>
+			<div className="jp-search-dashboard-customization">
+				<SectionHeader label={ __( 'Search Customization', 'jetpack' ) }>
+					<Button
+						compact
+						disabled={ isLoading || ! hasEditedEntities || isSaving }
+						onClick={ saveEditedEntityRecords }
+						primary
+						type="submit"
+					>
+						{ isSaving ? __( 'Saving…', 'jetpack' ) : __( 'Save', 'jetpack' ) }
+					</Button>
+				</SectionHeader>
+				<div className="dops-card">
+					<SelectControl
+						disabled={ isLoading }
+						label={ __( 'Theme', 'jetpack' ) }
+						value={ theme }
+						options={ [
+							{ label: __( 'Light', 'jetpack' ), value: 'light' },
+							{ label: __( 'Dark', 'jetpack' ), value: 'dark' },
+						] }
+						onChange={ setTheme }
+					/>
+					<SelectControl
+						disabled={ isLoading }
+						label={ __( 'Result Format', 'jetpack' ) }
+						value={ resultFormat }
+						options={ [
+							{ label: __( 'Minimal', 'jetpack' ), value: 'minimal' },
+							{ label: __( 'Expanded (shows images)', 'jetpack' ), value: 'expanded' },
+							{ label: __( 'Product (for WooCommerce stores)', 'jetpack' ), value: 'product' },
+						] }
+						onChange={ setResultFormat }
+					/>
+					<SelectControl
+						disabled={ isLoading }
+						label={ __( 'Sort', 'jetpack' ) }
+						value={ sort }
+						options={ [
+							{ label: __( 'Relevance (recommended)', 'jetpack' ), value: 'relevance' },
+							{ label: __( 'Newest first', 'jetpack' ), value: 'newest' },
+							{ label: __( 'Oldest first', 'jetpack' ), value: 'oldest' },
+						] }
+						onChange={ setSort }
+					/>
+					<SelectControl
+						disabled={ isLoading }
+						label={ __( 'Overlay Trigger', 'jetpack' ) }
+						value={ trigger }
+						options={ [
+							{ label: __( 'Open when the user starts typing', 'jetpack' ), value: 'immediate' },
+							{ label: __( 'Open when results are available', 'jetpack' ), value: 'results' },
+						] }
+						onChange={ setTrigger }
+					/>
+					<div className="jetpack-search-color-inputs">
+						<label htmlFor="jetpack-search-highlight-color">Search Highlight Color</label>
+						<input
+							type="color"
+							id="jetpack-search-highlight-color"
+							value={ normalizedColor }
+							// eslint-disable-next-line react/jsx-no-bind
+							onChange={ event => setColor( event.target.value ) }
+						/>
+						<input
+							type="text"
+							value={ isLoading ? __( 'Loading…', 'jetpack' ) : normalizedColor }
+							disabled
+						/>
+					</div>
+					<ToggleControl
+						checked={ sortEnabled }
+						disabled={ isLoading }
+						label={ __( 'Enable Sort', 'jetpack' ) }
+						onChange={ setSortEnabled }
+					/>
+					<ToggleControl
+						checked={ infiniteScroll }
+						disabled={ isLoading }
+						label={ __( 'Enable Infinite Scroll', 'jetpack' ) }
+						onChange={ setInfiniteScroll }
+					/>
+					<ToggleControl
+						checked={ showLogo }
+						disabled={ isLoading }
+						label={ __( "Show 'Powered by Jetpack'", 'jetpack' ) }
+						onChange={ setShowLogo }
+					/>
+				</div>
+			</div>
+		</Fragment>
 	);
 }
