7c3e05be75b70b04c80f18e73e27193e9ccf494e
Jeremy Herve <jeremy@jeremy.hu>
2021-05-17 19:06:05 +0200

    Update projects/plugins/jetpack/modules/related-posts.php
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>

--- projects/plugins/jetpack/modules/related-posts.php
+++ projects/plugins/jetpack/modules/related-posts.php
@@ -53,7 +53,7 @@ class Jetpack_RelatedPosts_Module {
 		}
 
 		// Load Customizer controls.
-		if ( class_exists( 'WP_Customize_Manager' ) && class_exists( 'WP_Customize_Control' ) ) {
+		if ( class_exists( WP_Customize_Manager::class ) && class_exists( WP_Customize_Control::class ) ) {
 			require_once 'related-posts/class.related-posts-customize.php';
 		}
 	}
