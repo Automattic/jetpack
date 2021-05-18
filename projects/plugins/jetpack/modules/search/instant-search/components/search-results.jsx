b9a8a3b9398eabd382e4ac539bce4fda51225ba0
Jason Moon <4044428+jsnmoon@users.noreply.github.com>
2021-05-18 15:29:20 -0600

    Search: Apply configured highlight color to text selection (#19851)

--- projects/plugins/jetpack/modules/search/instant-search/components/search-results.jsx
+++ projects/plugins/jetpack/modules/search/instant-search/components/search-results.jsx
@@ -99,6 +99,7 @@ class SearchResults extends Component {
 					// eslint-disable-next-line react/no-danger
 					dangerouslySetInnerHTML={ {
 						__html: `
+							.jetpack-instant-search *::selection,
 							.jetpack-instant-search .jetpack-instant-search__search-results .jetpack-instant-search__search-results-primary mark { 
 								color: ${ textColor };
 								background-color: ${ highlightColor };
