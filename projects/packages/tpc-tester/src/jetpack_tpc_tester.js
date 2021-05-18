aa8bae8d7458f590c99e23d02abcd5035db1c1be
leogermani <leogermani@gmail.com>
2021-05-18 17:58:18 -0300

    [not verified] adjust cookie creation

--- projects/packages/tpc-tester/src/jetpack_tpc_tester.js
+++ projects/packages/tpc-tester/src/jetpack_tpc_tester.js
@@ -1,22 +1,28 @@
-window.jetpack_3rdpc_test_verify = function() {
-	var testElement = document.createElement('script');
-	testElement.setAttribute('src', jetpack_tpc_tester_client.test_url );
+window.jetpack_3rdpc_test_verify = function () {
+	var testElement = document.createElement( 'script' );
+	testElement.setAttribute( 'src', jetpack_tpc_tester_client.test_url );
 	document.head.appendChild( testElement );
-}
+};
 
-window.jetpack_3rdpc_test_supported = function() {
+window.jetpack_3rdpc_test_supported = function () {
 	window.jetpack_3rdpc_test_set_cookie( jetpack_tpc_tester_client.supported_value );
-}
+};
 
-window.jetpack_3rdpc_test_unsupported = function() {
+window.jetpack_3rdpc_test_unsupported = function () {
 	window.jetpack_3rdpc_test_set_cookie( jetpack_tpc_tester_client.unsupported_value );
-}
+};
 
-window.jetpack_3rdpc_test_set_cookie = function( value ) {
-	console.log( 'Test result: ' + value) ;
-	var expires = "";
+window.jetpack_3rdpc_test_set_cookie = function ( value ) {
+	console.log( 'Test result: ' + value );
+	var expires = '';
 	var date = new Date();
 	date.setTime( date.getTime() + 3600 );
-	expires = "; expires=" + date.toUTCString();
-	document.cookie = jetpack_tpc_tester_client.cookie_name + "=" + value  + expires + "; path=/";
-}
+	expires = '; expires=' + date.toUTCString();
+	// TODO: use window.wpCookies ?
+	document.cookie =
+		jetpack_tpc_tester_client.cookie_name +
+		'=' +
+		value +
+		expires +
+		'; Max-Age=600; SameSite=Strict; path=/;';
+};
