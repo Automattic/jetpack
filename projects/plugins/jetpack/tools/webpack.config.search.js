6d39f1956d6465d1c0a299e0abf3ccb9f6b30457
Brad Jorsch <brad.jorsch@automattic.com>
2021-05-18 14:40:27 -0400

    Jetpack: Fix non-exclusion regex in webpack.config.search.js
    Paths now have two `/node_modules/`, one followed by `.pnpm/` and the
    other by the package name.

--- projects/plugins/jetpack/tools/webpack.config.search.js
+++ projects/plugins/jetpack/tools/webpack.config.search.js
@@ -50,7 +50,7 @@ function requestToExternal( request ) {
 
 const moduleConfig = { ...baseWebpackConfig.module };
 // NOTE: tiny-lru publishes non-ES5 as a browser target. It's necessary to let babel-loader transpile this module.
-moduleConfig.rules[ 0 ].exclude = /[\\/]node_modules[\\/](?!(tiny-lru)[\\/])/;
+moduleConfig.rules[ 0 ].exclude = /[\\/]node_modules[\\/](?!(\.pnpm|tiny-lru)[\\/])/;
 
 module.exports = {
 	...baseWebpackConfig,
