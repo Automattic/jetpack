d7376d46907324381d211723c4de3a85f5cb3422
Brandon Kraft <public@brandonkraft.com>
2021-05-19 17:08:08 -0500

    Update docblock

--- projects/plugins/jetpack/class.jetpack-idc.php
+++ projects/plugins/jetpack/class.jetpack-idc.php
@@ -1,7 +1,9 @@
 <?php
 /**
- * Deprecated since 9.8.0.
- * Functionality moved to the automattic/identity-crisis package.
+ * Identity Crisis handler.
+ *
+ * @deprecated 9.8.0. Functionality moved to the automattic/identity-crisis package.
+ * @package automattic/jetpack
  */
 
 use Automattic\Jetpack\Assets;
