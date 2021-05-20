ae21b2c5d54a09b284513bd0faf157814209857c
Bogdan Ungureanu <bogdanungureanu21@gmail.com>
2021-05-20 19:23:58 +0300

    [not verified] Restricted the loading to only edit.php and post.php

--- projects/plugins/jetpack/modules/masterbar/wp-posts-list/bootstrap.php
+++ projects/plugins/jetpack/modules/masterbar/wp-posts-list/bootstrap.php
@@ -5,8 +5,13 @@
  * @package Jetpack
  */
 
-use Automattic\Jetpack\Dashboard_Customizations\Posts_List_Page_Notification;
+/**
+ * Load the Posts_List_Notification.
+ */
+global $pagenow;
 
-require_once __DIR__ . '/class-posts-list-page-notification.php';
-
-Posts_List_Page_Notification::init();
+// phpcs:ignore
+if ( ( $pagenow === 'edit.php' && isset( $_GET['post_type'] ) && $_GET['post_type'] === 'page' ) || $pagenow === 'post.php' ) {
+	require_once __DIR__ . '/class-posts-list-page-notification.php';
+	Automattic\Jetpack\Dashboard_Customizations\Posts_List_Page_Notification::init();
+}
