cd6e0da8f9c13b268349567b267596dacb7792b8
Glen Davies <glen.davies@a8c.com>
2021-05-28 09:22:24 +1200

    [not verified] Fix unit tests

--- projects/plugins/jetpack/tests/php/modules/csstidy/test-class.jetpack-csstidy.php
+++ projects/plugins/jetpack/tests/php/modules/csstidy/test-class.jetpack-csstidy.php
@@ -28,13 +28,13 @@ class WP_Test_Jetpack_CSSTidy extends WP_UnitTestCase {
 		parent::setUp();
 		$this->instance = new csstidy();
 		$this->instance->set_cfg( 'optimise_shorthands', 0 );
+		$this->instance->set_cfg( 'discard_invalid_properties', true );
 	}
 
 	/**
 	 * Test that css variable properties are removed by default.
 	 */
 	public function test_removes_css_var_properties_by_default() {
-		// $this->instance->set_cfg( 'preserve_css_variables', false ); //this should be default but even if set manually css var not removed
 		$css      = 'div {--base-color:red;color:var(--base-color)}';
 		$expected = "div {\ncolor:var(--base-color)\n}";
 		$this->instance->parse( $css );
@@ -47,10 +47,38 @@ class WP_Test_Jetpack_CSSTidy extends WP_UnitTestCase {
 	/**
 	 * Test that css variable properties are preserved when flag toggled.
 	 */
-	public function test_removes_css_var_properties_preserved() {
+	public function test_css_var_properties_preserved() {
 		$this->instance->set_cfg( 'preserve_css_variables', true );
-		$css      = 'div {--base-color:red;color:var(--base-color)}';
-		$expected = "div {\n--base-color:red;\ncolor:var(--base-color)\n}";
+		$css      = 'div {--base-color_for_1st-child:red;color:var(--base-color)}';
+		$expected = "div {\n--base-color_for_1st-child:red;\ncolor:var(--base-color)\n}";
+		$this->instance->parse( $css );
+		$this->assertEquals(
+			$this->instance->print->plain(),
+			$expected
+		);
+	}
+
+	/**
+	 * Test that unkown properties still removed when css variables enabled.
+	 */
+	public function test_unknown_properties_removed() {
+		$this->instance->set_cfg( 'preserve_css_variables', true );
+		$css      = 'div {clowns-nose:red;color:var(--base-color)}';
+		$expected = "div {\ncolor:var(--base-color)\n}";
+		$this->instance->parse( $css );
+		$this->assertEquals(
+			$this->instance->print->plain(),
+			$expected
+		);
+	}
+
+	/**
+	 * Test that broken or potentially dangerious css still removed when css variables enabled.
+	 */
+	public function test_broken_or_dangerous_css_removed() {
+		$this->instance->set_cfg( 'preserve_css_variables', true );
+		$css      = 'div {xss-trap-be-careful:red;}</style><script>alert(\'Gotcha!\')</script>color:var(--base-color)}';
+		$expected = '';
 		$this->instance->parse( $css );
 		$this->assertEquals(
 			$this->instance->print->plain(),
