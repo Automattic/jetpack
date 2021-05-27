c86002f54e3244b4829774f9ff2f0749bda01817
Bogdan Ungureanu <bogdanungureanu21@gmail.com>
2021-05-27 17:27:14 +0300

    [not verified] Fixed regression introduced in posts page icon notification in WP-Admin edit.php page (#19953)
    * Fixed the Blog Posts Page restriction icon when the show_on_front option is not "page" and a conflict with the Customizer.
    
    * Handled a scenario where page_for_posts and page_on_front have the same value and added changelog.
    
    * Fixed failing test

--- projects/plugins/jetpack/modules/masterbar/wp-posts-list/bootstrap.php
+++ projects/plugins/jetpack/modules/masterbar/wp-posts-list/bootstrap.php
@@ -13,7 +13,6 @@ function masterbar_init_wp_posts_list() {
 
 	if (
 		( 'edit.php' === $pagenow && isset( $_GET['post_type'] ) && 'page' === $_GET['post_type'] ) // phpcs:ignore WordPress.Security.NonceVerification.Recommended
-		|| 'post.php' === $pagenow
 	) {
 		require_once __DIR__ . '/class-posts-list-page-notification.php';
 		Automattic\Jetpack\Dashboard_Customizations\Posts_List_Page_Notification::init();
