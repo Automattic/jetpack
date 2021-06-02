4b31de7fd824320bccda7707f19b331f78cd1416
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2021-06-02 17:44:44 +0300

    Add eslint support to jetpack cli

--- tools/cli/.eslintrc.js
+++ tools/cli/.eslintrc.js
@@ -1,9 +1,13 @@
+// .eslintrc.js
 module.exports = {
+	root: true,
+	extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
 	env: {
 		browser: false,
 	},
 	rules: {
-		'no-console': 'off',
+		'arrow-parens': [ 0, 'as-needed' ],
+		'no-console': 0,
 		'no-process-exit': 'off',
 	},
 };
