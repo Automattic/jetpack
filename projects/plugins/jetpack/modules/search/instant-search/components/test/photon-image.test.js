0cf4ccb05df85ae7249a80cdcaedb41a9390bdc2
Jason Moon <jason.moon@automattic.com>
2021-06-04 14:38:21 -0600

    Use React testing library

--- projects/plugins/jetpack/modules/search/instant-search/components/test/photon-image.test.js
+++ projects/plugins/jetpack/modules/search/instant-search/components/test/photon-image.test.js
@@ -7,7 +7,7 @@
  * External dependencies
  */
 import React from 'react';
-import { render } from '@testing-library/preact';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 /**
