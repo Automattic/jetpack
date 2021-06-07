20ac74d9d2f57491c2f8f41ec9e51fd2d67c8f7b
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2021-06-07 17:27:19 +0300

    revert eslintrc changes

--- tools/cli/.eslintrc.js
+++ tools/cli/.eslintrc.js
@@ -1,8 +1,4 @@
 module.exports = {
-	// Use root level ESlint configuration.
-	// JavaScript files inside this folder are meant to be transpiled by Webpack.
-	root: true,
-	extends: [ '../../.eslintrc.js' ],
 	env: {
 		browser: false,
 	},
