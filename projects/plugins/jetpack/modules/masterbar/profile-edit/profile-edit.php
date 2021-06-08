9f69b84beeb1c66f1110cae4f350c38617fd0cd5
Harris Papazolgou <hrrsppzgl@gmail.com>
2021-06-08 15:02:45 +0300

    Update projects/plugins/jetpack/modules/masterbar/profile-edit/profile-edit.php
    Uses apostrophe instead of single quote.
    
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>

--- projects/plugins/jetpack/modules/masterbar/profile-edit/profile-edit.php
+++ projects/plugins/jetpack/modules/masterbar/profile-edit/profile-edit.php
@@ -28,7 +28,7 @@ function jetpack_masterbar_hide_profile_fields( $user ) {
 	// Since there is no hook for altering profile fields, we will use CSS and JS.
 	$name_info_wpcom_link_message = sprintf(
 		/* translators: 1 link */
-		__( 'WordPress.com users can change their profile\\\'s basic details ( i.e., First Name, Last Name, Display Name, About ) in <a href="%1$s" target="_blank" rel="noopener noreferrer">WordPress.com Profile settings.</a>', 'jetpack' ),
+		__( 'WordPress.com users can change their profile’s basic details ( i.e., First Name, Last Name, Display Name, About ) in <a href="%1$s" target="_blank" rel="noopener noreferrer">WordPress.com Profile settings.</a>', 'jetpack' ),
 		'https://wordpress.com/me'
 	);
 	$contact_info_wpcom_link_message = sprintf(
