996673553007fed84dcefc028395d4451afda1ac
cpap <papazoglou.charalampos@gmail.com>
2021-06-10 15:24:03 +0300

    [not verified] Always shows carousel toggle.

--- projects/plugins/jetpack/modules/carousel/jetpack-carousel.php
+++ projects/plugins/jetpack/modules/carousel/jetpack-carousel.php
@@ -952,10 +952,8 @@ class Jetpack_Carousel {
 	function register_settings() {
 		add_settings_section( 'carousel_section', __( 'Image Gallery Carousel', 'jetpack' ), array( $this, 'carousel_section_callback' ), 'media' );
 
-		if ( ! $this->in_jetpack ) {
-			add_settings_field( 'carousel_enable_it', __( 'Enable carousel', 'jetpack' ), array( $this, 'carousel_enable_it_callback' ), 'media', 'carousel_section' );
-			register_setting( 'media', 'carousel_enable_it', array( $this, 'carousel_enable_it_sanitize' ) );
-		}
+		add_settings_field( 'carousel_enable_it', __( 'Enable carousel', 'jetpack' ), array( $this, 'carousel_enable_it_callback' ), 'media', 'carousel_section' );
+		register_setting( 'media', 'carousel_enable_it', array( $this, 'carousel_enable_it_sanitize' ) );
 
 		add_settings_field( 'carousel_background_color', __( 'Background color', 'jetpack' ), array( $this, 'carousel_background_color_callback' ), 'media', 'carousel_section' );
 		register_setting( 'media', 'carousel_background_color', array( $this, 'carousel_background_color_sanitize' ) );
