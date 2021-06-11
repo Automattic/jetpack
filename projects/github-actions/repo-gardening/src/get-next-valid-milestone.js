ca1ffb8afe968d5069092caa1af5d63f7b887e67
Jeremy Herve <jeremy@jeremy.hu>
2021-06-11 16:45:38 +0200

    Refactor check
    See https://github.com/Automattic/jetpack/pull/20042#discussion_r648412902

--- projects/github-actions/repo-gardening/src/get-next-valid-milestone.js
+++ projects/github-actions/repo-gardening/src/get-next-valid-milestone.js
@@ -28,21 +28,20 @@ async function getNextValidMilestone( octokit, owner, repo, plugin = 'jetpack' )
 	const responses = octokit.paginate.iterator( options );
 
 	for await ( const response of responses ) {
-		// Find a milestone which name is {plugin}/version number
-		// and its due date is earliest in a future.
-		// If we cannot find a milestone with a due date set,
-		// fallback to one without a due date.
+		// Find all valid milestones for the specified plugin.
 		const reg = new RegExp( '^' + plugin + '\\/\\d+\\.\\d' );
-		const nextMilestone = response.data
+		const milestones = response.data
 			.filter( m => m.title.match( reg ) )
 			.sort( ( m1, m2 ) =>
 				compareVersions( m1.title.split( '/' )[ 1 ], m2.title.split( '/' )[ 1 ] )
-			)
-			.find(
-				milestone => ( milestone.due_on && moment( milestone.due_on ) > moment() ) || milestone
 			);
 
-		return nextMilestone;
+		// Return the first milestone with a future due date,
+		// or failing that the first milestone with no due date.
+		return (
+			milestones.find( milestone => milestone.due_on && moment( milestone.due_on ) > moment() ) ||
+			milestones.find( milestone => ! milestone.due_on )
+		);
 	}
 }
 
