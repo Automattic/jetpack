dc2222bcd5e24adaea3b8b1cef1041ccddfbb6f5
Ben Dwyer <ben@scruffian.com>
2021-06-18 12:43:06 +0100

    This updates the custom colors plugin to also add the custom colors in the editor. https://github.com/Automattic/wpcomsh/pull/700
    * This updates the custom colors plugin to also add the custom colors in the editor
    
    * update custom-colors to the new version

--- projects/plugins/wpcomsh/wpcomsh.php
+++ projects/plugins/wpcomsh/wpcomsh.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WordPress.com Site Helper
  * Description: A helper for connecting WordPress.com sites to external host infrastructure.
- * Version: 2.7.61
+ * Version: 2.7.62
  * Author: Automattic
  * Author URI: http://automattic.com/
  */
