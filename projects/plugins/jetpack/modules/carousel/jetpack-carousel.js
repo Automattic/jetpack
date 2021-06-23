7f0853d0248c6045f52462610292f8268d5f76c1
Glen Davies <glen.davies@a8c.com>
2021-06-24 11:51:21 +1200

    Try a more efficient way of getting background color

--- projects/plugins/jetpack/modules/carousel/jetpack-carousel.js
+++ projects/plugins/jetpack/modules/carousel/jetpack-carousel.js
@@ -41,34 +41,19 @@
 		function getAverageColor( imgEl ) {
 			var canvas = document.createElement( 'canvas' ),
 				context = canvas.getContext && canvas.getContext( '2d' ),
-				imgData,
-				width,
-				height,
-				length,
-				rgb = { r: 0, g: 0, b: 0 },
-				count = 0;
+				rgb = { r: 0, g: 0, b: 0 };
 
 			if ( ! imgEl ) {
 				return rgb;
 			}
+			canvas.width = 1;
+			canvas.height = 1;
+			context.drawImage( imgEl, 0, 0, 1, 1 );
+			var i = context.getImageData( 0, 0, 1, 1 ).data;
 
-			height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
-			width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
-
-			context.drawImage( imgEl, 0, 0 );
-			imgData = context.getImageData( 0, 0, width, height );
-			length = imgData.data.length;
-
-			for ( var i = 0; i < length; i += 4 ) {
-				rgb.r += imgData.data[ i ];
-				rgb.g += imgData.data[ i + 1 ];
-				rgb.b += imgData.data[ i + 2 ];
-				count++;
-			}
-
-			rgb.r = Math.floor( rgb.r / count );
-			rgb.g = Math.floor( rgb.g / count );
-			rgb.b = Math.floor( rgb.b / count );
+			rgb.r = i[ 0 ];
+			rgb.g = i[ 1 ];
+			rgb.b = i[ 2 ];
 
 			return rgb;
 		}
