c3599dc530ca17abc407c1af6e405c65593b0b9a
Brad Jorsch <brad.jorsch@automattic.com>
2021-06-23 10:45:17 -0400

    Also lint .cjs files
    .cjs is the extension to use CommonJS syntax in projects configured as ES modules.

--- tools/git-hooks/pre-commit-hook.js
+++ tools/git-hooks/pre-commit-hook.js
@@ -70,7 +70,7 @@ function phpcsFilesToFilter( file ) {
  * @returns {boolean} If the file matches the requirelist.
  */
 function filterJsFiles( file ) {
-	return [ '.js', '.json', '.jsx' ].some( extension => file.endsWith( extension ) );
+	return [ '.js', '.json', '.jsx', '.cjs' ].some( extension => file.endsWith( extension ) );
 }
 
 /**
