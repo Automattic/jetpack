e3c4f753337538b26ada8cc5eb169ca4cfda5b27
Miguel Torres <miguel.torres@automattic.com>
2021-06-23 15:17:12 +0200

    Nav Unification: Register Calypso settings pages as independent submenus (hotfix) https://github.com/Automattic/wpcomsh/pull/703

--- projects/plugins/wpcomsh/feature-plugins/nav-unification-hotfixes.php
+++ projects/plugins/wpcomsh/feature-plugins/nav-unification-hotfixes.php
@@ -6,38 +6,69 @@
  * @package wpcomsh
  */
 
-/**
- * Fix third-party dependencies that expect index.php submenu item to be available.
- */
-function wpcomsh_add_index_page_hotfix() {
-	global $submenu;
+use Automattic\Jetpack\Dashboard_Customizations\Atomic_Admin_Menu;
 
+/**
+ * Overrides the `Atomic_Admin_Menu` menu class with hotfixes that have not been released yet on Jetpack.
+ *
+ * @param string $admin_menu_class Class name.
+ */
+function wpcomsh_use_nav_unification_hotfixes( $admin_menu_class ) {
 	// Do not run if Jetpack is not enabled.
 	if ( ! defined( 'JETPACK__VERSION' ) ) {
-		return;
+		return $admin_menu_class;
 	}
 
-	// Do not clash with the fix already shipped in Jetpack 9.6.
-	if ( version_compare( JETPACK__VERSION, '9.7-alpha', '>=' ) ) {
-		return;
+	// Do not clash with fixes already shipped.
+	if ( version_compare( JETPACK__VERSION, '9.9-alpha', '>=' ) ) {
+		return $admin_menu_class;
 	}
 
-	// Safety - don't alter anything if Nav Unification is not enabled.
-	if ( ! wpcomsh_activate_nav_unification( false ) ) {
-		return;
-	}
+	require_once JETPACK__PLUGIN_DIR . 'modules/masterbar/admin-menu/class-atomic-admin-menu.php';
 
-	add_submenu_page( 'index.php', '', '', 'read', 'index.php', '', 10 );
+	class Atomic_Hotfixes_Admin_Menu extends Atomic_Admin_Menu {
+		/**
+		 * Adds Settings menu.
+		 *
+		 * Hotfix of https://github.com/Automattic/jetpack/pull/20100.
+		 *
+		 * @param bool $wp_admin Optional. Whether links should point to Calypso or wp-admin. Default false (Calypso).
+		 */
+		public function add_options_menu( $wp_admin = false ) {
+			$this->hide_submenu_page( 'options-general.php', 'sharing' );
 
+			$this->update_submenus( 'options-general.php', array( 'options-general.php' => 'https://wordpress.com/settings/general/' . $this->domain ) );
+			add_submenu_page( 'options-general.php', esc_attr__( 'Advanced General', 'jetpack' ), __( 'Advanced General', 'jetpack' ), 'manage_options', 'options-general.php', null, 1 );
 
-	foreach ( $submenu['index.php'] as $index => $item ) {
-		if ( 'index.php' !== $item[2] ) {
-			continue;
+			add_submenu_page( 'options-general.php', esc_attr__( 'Performance', 'jetpack' ), __( 'Performance', 'jetpack' ), 'manage_options', 'https://wordpress.com/settings/performance/' . $this->domain, null, 2 );
+
+			if ( ! $wp_admin ) {
+				$submenus_to_update = array(
+					'options-writing.php'    => 'https://wordpress.com/settings/writing/' . $this->domain,
+					'options-discussion.php' => 'https://wordpress.com/settings/discussion/' . $this->domain,
+				);
+				$this->update_submenus( 'options-general.php', $submenus_to_update );
+			}
+
+			add_submenu_page( 'options-general.php', esc_attr__( 'Security', 'jetpack' ), __( 'Security', 'jetpack' ), 'manage_options', 'https://wordpress.com/settings/security/' . $this->domain, null, 2 );
+			add_submenu_page( 'options-general.php', esc_attr__( 'Hosting Configuration', 'jetpack' ), __( 'Hosting Configuration', 'jetpack' ), 'manage_options', 'https://wordpress.com/hosting-config/' . $this->domain, null, 11 );
+			add_submenu_page( 'options-general.php', esc_attr__( 'Jetpack', 'jetpack' ), __( 'Jetpack', 'jetpack' ), 'manage_options', 'https://wordpress.com/settings/jetpack/' . $this->domain, 12 );
+
+			// Page Optimize is active by default on all Atomic sites and registers a Settings > Performance submenu which
+			// would conflict with our own Settings > Performance that links to Calypso, so we hide it it since the Calypso
+			// performance settings already have a link to Page Optimize settings page.
+			$this->hide_submenu_page( 'options-general.php', 'page-optimize' );
+
+			// No need to add a menu linking to WP Admin if there is already one.
+			if ( ! $wp_admin ) {
+				add_submenu_page( 'options-general.php', esc_attr__( 'Advanced Writing', 'jetpack' ), __( 'Advanced Writing', 'jetpack' ), 'manage_options', 'options-writing.php' );
+			}
 		}
-
-		// phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-		$submenu['index.php'][ $index ][4] = 'hide-if-no-js hide-if-js';
 	}
+
+	return Atomic_Hotfixes_Admin_Menu::class;
 }
 
-add_action( 'admin_menu', 'wpcomsh_add_index_page_hotfix', 100000 );
+
+
+add_action( 'jetpack_admin_menu_class', 'wpcomsh_use_nav_unification_hotfixes' );
