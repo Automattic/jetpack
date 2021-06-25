586296a073d8e34338443fdce49d838f8c9496d5
Jeremy Herve <jeremy@jeremy.hu>
2021-06-25 16:03:29 +0200

    Stop requiring deleted file
    Also add blank file back to avoid issues with sites with strong caching mechanisms.

---
+++ projects/plugins/jetpack/class.jetpack-user-agent.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Deprecated. Use Automattic\Jetpack\Device_Detection\User_Agent_Info instead.
+ *
+ * @package automattic/jetpack
+ *
+ * @deprecated 8.7.0 Use Automattic\Jetpack\Device_Detection\User_Agent_Info
+ */
+
+// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+_deprecated_file( basename( __FILE__ ), 'jetpack-8.7.0' );
