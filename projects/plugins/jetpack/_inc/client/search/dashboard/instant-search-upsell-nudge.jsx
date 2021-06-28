bcec9ee030bde41007ff84f5831a1e0b0ffeefae
Jasper Kang <jasper.kang@automattic.com>
2021-06-29 09:59:05 +1200

    refactor css naming for upsell nudge

--- projects/plugins/jetpack/_inc/client/search/dashboard/instant-search-upsell-nudge.jsx
+++ projects/plugins/jetpack/_inc/client/search/dashboard/instant-search-upsell-nudge.jsx
@@ -11,18 +11,16 @@ import './instant-search-upsell-nudge.scss';
 
 const InstantSearchUpsellNudge = props => {
 	return (
-		<div className="jp-search-dashboard__instant-search-upsell-nudge">
-			<a className="jp-search-dashboard__instant-search-upsell-description" href={ props.href }>
-				<span>
-					{ __(
-						'Offer instant search results to your visitors as soon as they start typing. ',
-						'jetpack'
-					) }
-					<b>{ __( 'Upgrade to Jetpack Instant Search now', 'jetpack' ) }</b>
-				</span>
-				<div className="jp-search-dashboard__instant-search-upsell-arrow">&rarr;</div>
-			</a>
-		</div>
+		<a className="jp-instant-search-upsell-nudge" href={ props.href }>
+			<span>
+				{ __(
+					'Offer instant search results to your visitors as soon as they start typing. ',
+					'jetpack'
+				) }
+				<b>{ __( 'Upgrade to Jetpack Instant Search now', 'jetpack' ) }</b>
+			</span>
+			<div className="jp-instant-search-upsell-nudge__button-arrow">&rarr;</div>
+		</a>
 	);
 };
 
