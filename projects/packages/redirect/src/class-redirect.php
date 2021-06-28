33dcf25df81314294dd15192de63c7ce85483fdd
leogermani <leogermani@gmail.com>
2021-06-28 12:30:59 -0300

    updates redirect package to accept all params (#20158)
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    Co-authored-by: sdixon194 <steve.dixon@automattic.com>

--- projects/packages/redirect/src/class-redirect.php
+++ projects/packages/redirect/src/class-redirect.php
@@ -29,22 +29,22 @@ class Redirect {
 	 *
 	 * @param string       $source The URL handler registered in the server or the full destination URL (starting with https://).
 	 * @param array|string $args {
-	 *    Optional. Additional arguments to build the url.
+	 *    Optional. Additional arguments to build the url. This is not a complete list as any argument passed here will be sent to as a query parameter to the Redirect server. These parameters will not necessarily be passed over to the final destination URL. If you want to add a parameter to the final destination URL, use the `query` argument.
 	 *
-	 *    @type string $site URL of the site; Default is current site.
-	 *    @type string $path Additional path to be appended to the URL.
-	 *    @type string $query Query parameters to be added to the URL.
-	 *    @type string $anchor Anchor to be added to the URL.
+	 *    @type string  $site URL of the site; Default is current site.
+	 *    @type string  $path Additional path to be appended to the URL.
+	 *    @type string  $query Query parameters to be added to the final destination URL. should be in query string format (e.g. 'key=value&foo=bar').
+	 *    @type string  $anchor Anchor to be added to the URL.
+	 *    @type integer $u The user ID.
 	 * }
 	 *
 	 * @return string The built URL
 	 */
 	public static function get_url( $source, $args = array() ) {
 
-		$url           = 'https://jetpack.com/redirect/';
-		$site_suffix   = ( new Status() )->get_site_suffix();
-		$args          = wp_parse_args( $args, array( 'site' => $site_suffix ) );
-		$accepted_args = array( 'site', 'path', 'query', 'anchor', 'unlinked' );
+		$url         = 'https://jetpack.com/redirect/';
+		$site_suffix = ( new Status() )->get_site_suffix();
+		$args        = wp_parse_args( $args, array( 'site' => $site_suffix ) );
 
 		$source_key = 'source';
 
@@ -62,7 +62,7 @@ class Redirect {
 
 		foreach ( $args as $arg_name => $arg_value ) {
 
-			if ( ! in_array( $arg_name, $accepted_args, true ) || empty( $arg_value ) ) {
+			if ( empty( $arg_value ) ) {
 				continue;
 			}
 
