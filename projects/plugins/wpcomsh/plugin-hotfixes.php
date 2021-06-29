64b592f6c12af0002cc2cfcde40f47db796eb443
Rommel Castro <rommel@automattic.com>
2021-06-29 08:51:58 -0600

    Disable facebook-for-woocommerce full batch API sync on sites with many products https://github.com/Automattic/wpcomsh/pull/707
    * Disallow facebook-for-woocommerce full batch API sync
    
    * Disable facebook-for-woocommerce full sync only on sites with many products
    
    * specify hook priority and num args - prevents fatal with default (1) arg
    
    * Bump version to 2.7.67
    
    Co-authored-by: rua <rua@automattic.com>

--- projects/plugins/wpcomsh/plugin-hotfixes.php
+++ projects/plugins/wpcomsh/plugin-hotfixes.php
@@ -21,3 +21,17 @@ function wpcomsh_pre_transient_jetpack_akismet_key_is_valid( $_false ) {
     return 'valid';
 }
 add_filter( 'pre_transient_jetpack_akismet_key_is_valid', 'wpcomsh_pre_transient_jetpack_akismet_key_is_valid' );
+
+// We disallow facebook-for-woocommerce full batch API sync because of large option DB churn
+// Related: pcTzPI-6r-p2
+// Related: pcTzPI-64-p2
+function wpcomsh_disallow_fb_for_woo_full_batch_api_sync( $allow_full_sync, $product_count ) {
+	// Disable only for sites with a large number of products.
+	$max_products_for_safe_full_sync = 5000;
+	if ( $product_count > $max_products_for_safe_full_sync ) {
+		return false;
+	}
+
+	return $allow_full_sync;
+}
+add_filter( 'facebook_for_woocommerce_allow_full_batch_api_sync', 'wpcomsh_disallow_fb_for_woo_full_batch_api_sync', 10, 2 );
