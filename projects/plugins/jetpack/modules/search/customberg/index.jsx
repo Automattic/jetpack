80389fd115a926e7d818311b73e1d6e21bdc7a20
Jason Moon <jason.moon@automattic.com>
2021-06-29 15:41:35 -0600

    Collapse wp-admin sidebar for extra horizontal space

--- projects/plugins/jetpack/modules/search/customberg/index.jsx
+++ projects/plugins/jetpack/modules/search/customberg/index.jsx
@@ -15,11 +15,19 @@ import Layout from './components/layout';
 import './styles.scss';
 
 /**
+ * Collapses wp-admin's sidebar menu for additional space.
+ */
+function collapseWpAdminSidebar() {
+	document.body.classList.add( 'folded' );
+}
+
+/**
  * Initilizes the widgets screen
  *
  * @param {string} id - Id of the root element to render the screen.
  */
 export function initialize( id ) {
+	collapseWpAdminSidebar();
 	render( <Layout />, document.getElementById( id ) );
 }
 
