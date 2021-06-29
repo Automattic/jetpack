cce4efd1b9fe41dee604ab6eb44ff0347a7a9330
Grant Kinney <hi@grant.mk>
2021-06-29 17:17:19 -0500

    Update site_logo -> custom_logo only on theme swtich

--- projects/plugins/jetpack/modules/theme-tools/site-logo.php
+++ projects/plugins/jetpack/modules/theme-tools/site-logo.php
@@ -26,11 +26,6 @@
  * @since 9.9.0 Uses Core site_logo option format universally.
  */
 function site_logo_init() {
-	// For transferring existing site logo from Jetpack -> Core
-	if ( current_theme_supports( 'custom-logo' ) && ! get_theme_mod( 'custom_logo' ) && $jp_logo = get_option( 'site_logo' ) ) {
-		set_theme_mod( 'custom_logo', $jp_logo );
-	}
-
 	// Only load our code if our theme declares support, and the standalone plugin is not activated.
 	if ( current_theme_supports( 'site-logo' ) && ! class_exists( 'Site_Logo', false ) ) {
 		// Load our class for namespacing.
@@ -44,6 +39,21 @@ function site_logo_init() {
 	}
 }
 add_action( 'init', 'site_logo_init' );
+
+/**
+ * When switching from a legacy theme that uses `site-logo` to a theme that uses `custom-logo`,
+ * update the theme's custom logo if it doesn't already have one.
+ *
+ * @return void
+ */
+function jetpack_update_custom_logo_from_site_logo() {
+	$site_logo = get_option( 'site_logo' );
+
+	if ( current_theme_supports( 'custom-logo' ) && ! get_theme_mod( 'custom_logo' ) && $site_logo ) {
+		set_theme_mod( 'custom_logo', $site_logo );
+	}
+}
+add_action( 'after_switch_theme', 'jetpack_update_custom_logo_from_site_logo', 10, 0 );
 
 /**
  * Transforms the legacy site_logo array, when present, into an attachment ID.
