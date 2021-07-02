d7b3ffb7f83d6d31712cd4242280deda7ee0b79a
Glen Davies <glen.davies@a8c.com>
2021-07-02 14:51:45 +1200

    Fix matchMedia global

--- projects/plugins/jetpack/tests/jest-globals.js
+++ projects/plugins/jetpack/tests/jest-globals.js
@@ -3,18 +3,15 @@
 require( 'regenerator-runtime/runtime' );
 
 if ( ! window.matchMedia ) {
-	Object.defineProperty( window, 'matchMedia', {
-		writable: true,
-		value: jest.fn().mockImplementation( query => ( {
-			matches: false,
-			media: query,
-			onchange: null,
-			addListener: jest.fn(), // deprecated
-			removeListener: jest.fn(), // deprecated
-			addEventListener: jest.fn(),
-			removeEventListener: jest.fn(),
-			dispatchEvent: jest.fn(),
-		} ) ),
+	window.matchMedia = query => ( {
+		matches: false,
+		media: query,
+		onchange: null,
+		addListener: jest.fn(), // deprecated
+		removeListener: jest.fn(), // deprecated
+		addEventListener: jest.fn(),
+		removeEventListener: jest.fn(),
+		dispatchEvent: jest.fn(),
 	} );
 }
 
