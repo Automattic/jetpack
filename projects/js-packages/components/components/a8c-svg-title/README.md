fc1ffe7fda81cac27d55cd980cb7c1ef74dab05d
Jasper Kang <jasper.kang@automattic.com>
2021-07-07 15:34:50 +1200

    [not verified] refactoring

--- projects/js-packages/components/components/a8c-svg-title/README.md
+++ projects/js-packages/components/components/a8c-svg-title/README.md
@@ -1,16 +1,16 @@
 # A8C SVG Title
 
 Component that renders the stylish `AN AUTOMATTIC AIRLINE`.
-It takes width and height properties but defaults to 32px in height.
+It takes width and height properties but defaults to 7 in height.
 
 #### How to use:
 
 ```js
-<JetpackLogo height={ 8 } className="jp-a8c-svg-title" />
+<JetpackLogo height={ 7 } className="jp-a8c-svg-title" />
 ```
 
 #### Props
 
 - `className`: String - (default: `jp-a8c-svg-title`) the class name set on the SVG element.
 - `height`: Number - (default: 8) set the height of the title.
-- `width`: Number - (optional) set the width of the title.
+- `title`: String - (default: `AN AUTOMATTIC AIRLINE`) title of the SVG.
--- projects/js-packages/components/components/jetpack-footer/index.jsx
+++ projects/js-packages/components/components/jetpack-footer/index.jsx
@@ -3,6 +3,7 @@
  */
 import React from 'react';
 import { __ } from '@wordpress/i18n';
+import classnames from 'classnames';
 
 /**
  * Internal dependencies
@@ -14,26 +15,28 @@ import './style.scss';
  * JetpackFooter component definition.
  *
  * @param {object} props - Component properties.
- * @param {object} props.aboutPageUrl - URL for 'An Automattic Airline'.
+ * @param {object} props.aboutPageUrl - Link for 'An Automattic Airline'.
  * @param {object} props.moduelName - Name of the moduel, e.g. 'Jetpack Search'.
- * @param {object} props.className - default: jp-dashboard-footer.
+ * @param {object} props.className - className of the wrapper, default: `jp-dashboard-footer`.
  *
  * @returns {React.Component} JetpackFooter component.
  */
 export default function JetpackFooter( {
 	aboutPageUrl,
 	moduelName = 'Jetpack',
-	className = 'jp-dashboard-footer',
+	className = '',
 	...otherProps
 } ) {
 	return (
-		<div className={ className } { ...otherProps }>
+		<div className={ classnames( 'jp-dashboard-footer', className ) } { ...otherProps }>
 			<div className="jp-dashboard-footer__footer-left">
 				<span className="jp-dashboard-footer__logo"></span>
 				<span>{ moduelName }</span>
 			</div>
 			<div className="jp-dashboard-footer__footer-right">
-				<A8cSvgTitle href={ aboutPageUrl } title={ __( 'An Automattic Airline', 'jetpack' ) } />
+				<a href={ aboutPageUrl }>
+					<A8cSvgTitle title={ __( 'An Automattic Airline', 'jetpack' ) } />
+				</a>
 			</div>
 		</div>
 	);
