e004f3eb2380cac0d1e47e522cabc631f15aab67
sdixon194 <steve.dixon@automattic.com>
2021-07-08 14:24:23 -0400

    [not verified] start requiring esm modules

--- tools/git-hooks/pre-push-hook.js
+++ tools/git-hooks/pre-push-hook.js
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
 
+// eslint-disable-next-line no-global-assign
+require = require( 'esm' )( module /*, options*/ );
 /* eslint-disable no-console, no-process-exit */
 const execSync = require( 'child_process' ).execSync;
 const spawnSync = require( 'child_process' ).spawnSync;
 const chalk = require( 'chalk' );
 const fs = require( 'fs' );
+const allProjects = require( '../cli/helpers/projectHelpers' ).allProjects;
+//import { allProjects, allProjectsByType } from '../cli/helpers/projectHelpers.js';
 
-console.log('I am pre-pushing');
+// Check the diff for files changed
+// store the results in git diff --name-only
+// See if any of the files changed require a changelog
+// Check the diff for a changelog file
+console.log( allProjects() );
