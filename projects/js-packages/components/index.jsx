ca0ab25e1a6430b651dce791ca8e61234126ed86
Jasper Kang <kangzjnet@gmail.com>
2021-07-13 10:02:00 +1200

    RNA: Add Jetpack footer (#20287)
    * added A8C byline logo component
    
    * added Jetpack shared admin footer
    
    * add i18n text domain check to eslint
    
    
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    Co-authored-by: Jason Moon <4044428+jsnmoon@users.noreply.github.com>

--- projects/js-packages/components/index.jsx
+++ projects/js-packages/components/index.jsx
@@ -15,3 +15,5 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 export { default as JetpackLogo } from './components/jetpack-logo';
+export { default as AutomatticBylineLogo } from './components/automattic-byline-logo';
+export { default as JetpackFooter } from './components/jetpack-footer';
