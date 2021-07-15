af05f87a6276efbc8f94414637fa9a178d256478
Glen Davies <glen.davies@a8c.com>
2021-07-15 16:00:23 +1200

    [not verified] Add escaping of translated string

--- projects/plugins/jetpack/class.jetpack-gutenberg.php
+++ projects/plugins/jetpack/class.jetpack-gutenberg.php
@@ -629,7 +629,7 @@ class Jetpack_Gutenberg {
 				// The Map block is dependent on wp-element, and it doesn't appear to to be possible to load
 				// this dynamically into the customizer iframe currently.
 				if ( 'map' === $type ) {
-					echo '<div>' . __( 'No preview available. Publish and refresh to see this widget', 'jetpack' ) . '</div>';
+					echo '<div>' . esc_html_e( 'No preview available. Publish and refresh to see this widget', 'jetpack' ) . '</div>';
 				} else {
 					echo '<script id="jetpack-block-' . esc_attr( $type ) . '" src="' . esc_attr( $view_script ) . '?ver=' . esc_attr( $script_version ) . '"></script>';
 				}
