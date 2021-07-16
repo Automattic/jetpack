3fe9b4691bdc7ba9db2b650e54cddf61fa80853f
sdixon194 <steve.dixon@automattic.com>
2021-07-16 17:49:44 -0400

    [not verified] adds prompt if changelog file needed

--- tools/js-tools/git-hooks/pre-push-hook.js
+++ tools/js-tools/git-hooks/pre-push-hook.js
@@ -6,67 +6,80 @@ require = require( 'esm' )( module /*, options*/ );
 const execSync = require( 'child_process' ).execSync;
 const spawnSync = require( 'child_process' ).spawnSync;
 const chalk = require( 'chalk' );
-const fs = require( 'fs' );
-const path = require( 'path' );
+const inquirer = require( 'inquirer' );
 const allProjects = require( '../../cli/helpers/projectHelpers' ).allProjects;
 
+// Initialize variables
 let exitCode = 0;
 let branch = getCurrentBranch();
 const diffFiles = getDiffFiles();
 const needChangelog = checkNeedChangelog();
+console.log( chalk.green( 'Checking if changelog files are needed. Just a sec...' ) );
+// If files require a changelog, check and see if one is included already
+if ( needChangelog ) {
+	// Iterate through projects that may need a changelog
+	for ( const proj of needChangelog ) {
+		const regexString = '^projects/' + proj + '/changelog/([^/]+)'; // regex matching a changelog file, ex: projects/plugins/jetpack/changelog/file_name
+		const regex = new RegExp( regexString );
+		// See if any diffed files indicate a changelog file was added
+		for ( const file of diffFiles ) {
+			const match = file.match( regex );
+			if ( match ) {
+				console.log( `Found changelog file for ${ proj }` );
+				// If match, remove from needChangelog array.
+				//needChangelog.splice( needChangelog.indexOf(proj), 1);
+			}
+		}
+	}
+}
 
-// Get a list of files that were just committed and total files from the diff.
+// If there are projects that still need a changelog file, notify the pusher.
+if ( needChangelog.length ) {
+	console.log( chalk.green( 'Looks like some projects still need changelog files: ' ) );
+	for ( const proj of needChangelog ) {
+		console.log( proj );
+	}
+
+	// This just runs everything at once without waiting for a prompt. May have to try running the prompt in a child process?
+	( async function () {
+		const result = await promptForChangelog();
+		console.log( result );
+	} )();
+}
+
 // Get the current branch name that we're on
 function getCurrentBranch() {
-    return 	execSync(
-        `git branch --show-current`
-    )
-    .toString()
-    .trim();
+	return execSync( `git branch --show-current` ).toString().trim();
 }
 
-// Get list of files that this branch has touched
+// Get list of files that this branch has touched.
 function getDiffFiles() {
-    return 	execSync(
-        `git diff master...${branch} --name-only`
-    )
-    .toString()
-    .trim()
-    .split( '\n' );
+	return execSync( `git diff master...${ branch } --name-only` ).toString().trim().split( '\n' );
 }
 
-// If none of the files require a changelog, bail.
+// Return a list of projects that this diff has touched that require a changelog.
 function checkNeedChangelog() {
-    const re = /^projects\/([^/]+\/[^/]+)\//; // regex matches project file path, ie 'project/packages/connection/..'
-    const modifiedProjects = new Set();
+	const re = /^projects\/([^/]+\/[^/]+)\//; // regex matches project file path, ie 'project/packages/connection/..'
+	const modifiedProjects = new Set();
 	for ( const file of diffFiles ) {
 		const match = file.match( re );
 		if ( match ) {
 			modifiedProjects.add( match[ 1 ] );
 		}
 	}
-    
-    return allProjects().filter( proj => modifiedProjects.has( proj ) );
+
+	return allProjects().filter( proj => modifiedProjects.has( proj ) );
 }
 
-// If files require a changelog, check and see if one is included already
-if ( needChangelog ) {
-    for ( const proj of needChangelog ){
-        const re = '^projects\/' + proj + '\/changelog\/([^\/]+)'
-        const regex = new RegExp( re );
-        console.log(regex);
-        for (const file of diffFiles ) {
-            const match = file.match( regex );
-            if ( match ) {
-                console.log( `Found changelog file for ${proj}` );
-            }
-        }
-    }
-    // See how many projects need a changelog file
-    // Check all diffed files and see if a changelog file has been added that matches one of the projects.
-    // Check all the diffed files and see if they match
- 
+// Prompt if user wants to stop the push to add any changelog files.
+async function promptForChangelog() {
+	const confirm = execSync(
+		await inquirer.prompt( {
+			type: 'confirm',
+			name: 'confirm',
+			message: chalk.green( 'Would you like to add required changelog files before pushing?' ),
+		} )
+	);
+	return confirm;
 }
-
-// If there isn't, prompt for one
 process.exit( 1 );
