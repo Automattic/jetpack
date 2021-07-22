87e9c91a13a458736806810f9dc1b0925116fd85
apeatling <apeatling@gmail.com>
2021-07-22 11:15:54 -0700

    [not verified] Restore original color calculation

--- projects/plugins/jetpack/modules/carousel/jetpack-carousel.js
+++ projects/plugins/jetpack/modules/carousel/jetpack-carousel.js
@@ -39,18 +39,34 @@
 			var canvas = document.createElement( 'canvas' ),
 				context = canvas.getContext && canvas.getContext( '2d' ),
 				imgData,
-				rgb = { r: 0, g: 0, b: 0 };
+				width,
+				height,
+				length,
+				rgb = { r: 0, g: 0, b: 0 },
+				count = 0;
 
 			if ( ! imgEl ) {
 				return rgb;
 			}
 
-			context.drawImage( imgEl, 0, 0 );
-			imgData = context.getImageData( 0, 0, 1, 1 );
+			height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
+			width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
 
-			rgb.r = imgData.data[ 0 ];
-			rgb.g = imgData.data[ 1 ];
-			rgb.b = imgData.data[ 2 ];
+			context.drawImage( imgEl, 0, 0 );
+			imgData = context.getImageData( 0, 0, width, height );
+
+			length = imgData.data.length;
+
+			for ( var i = 0; i < length; i += 4 ) {
+				rgb.r += imgData.data[ i ];
+				rgb.g += imgData.data[ i + 1 ];
+				rgb.b += imgData.data[ i + 2 ];
+				count++;
+			}
+
+			rgb.r = Math.floor( rgb.r / count );
+			rgb.g = Math.floor( rgb.g / count );
+			rgb.b = Math.floor( rgb.b / count );
 
 			return rgb;
 		}
