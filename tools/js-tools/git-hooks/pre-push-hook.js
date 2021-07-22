8305f397c509720b44d54474b417acbd6b5fbd35
sdixon194 <steve.dixon@automattic.com>
2021-07-22 17:07:40 -0400

    [not verified] changelog test

--- tools/js-tools/git-hooks/pre-push-hook.js
+++ tools/js-tools/git-hooks/pre-push-hook.js
@@ -1,24 +1,29 @@
 #!/usr/bin/env node
 
 /* eslint-disable no-console, no-process-exit */
-const { execSync, spawnSync } = require( 'child_process' );
+const { spawnSync } = require( 'child_process' );
 const chalk = require( 'chalk' );
-const glob = require( 'glob' );
 
 // Check if any touched files need a changelog file
 console.log( chalk.green( 'Checking if changelog files are needed. Just a sec...' ) );
 const needChangelog = checkChangelogFiles();
-console.log( needChangelog );
+
+// If a changelog file is needed, quit the push. 
+if ( needChangelog.status === 1 ) {
+	process.exitCode = 1;
+}
+
+if ( needChangelog.status === 0 ) {
+	console.log( chalk.green( 'Changelog check passed.' ) );
+}
 /**
- * Parses the output of a git diff command into file paths.
+ * Checks if changelog files are required.
  *
  * @param {string} command - Command to run. Expects output like `git diff --name-only […]`
  * @returns {Array} Paths output from git command
  */
 function checkChangelogFiles() {
-	const data = spawnSync( 'tools/check-changelogger-use.php', [ 'origin/master', 'HEAD' ], { stdio: 'inherit' });
-	console.log(data);
+	return spawnSync( 'tools/check-changelogger-use.php', [ 'origin/master', 'HEAD' ], { stdio: 'inherit' } );
 }
 
-process.exitCode = 1;
 process.exitCode;
