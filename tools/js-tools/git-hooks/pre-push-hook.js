5453055939660cc18c3177c1710ceb24615b442e
sdixon194 <steve.dixon@automattic.com>
2021-07-23 10:59:00 -0400

    move logic into function

--- tools/js-tools/git-hooks/pre-push-hook.js
+++ tools/js-tools/git-hooks/pre-push-hook.js
@@ -6,26 +6,29 @@ const chalk = require( 'chalk' );
 
 // Check if any touched files need a changelog file
 console.log( chalk.green( 'Checking if changelog files are needed. Just a sec...' ) );
-const needChangelog = checkChangelogFiles();
 
-// If a changelog file is needed, quit the push.
-if ( needChangelog.status === 1 ) {
-	process.exitCode = 1;
-}
-
-if ( needChangelog.status === 0 ) {
-	console.log( chalk.green( 'Changelog check passed.' ) );
-}
 /**
  * Checks if changelog files are required.
- *
- * @param {string} command - Command to run. Expects output like `git diff --name-only […]`
- * @returns {Array} Paths output from git command
  */
 function checkChangelogFiles() {
-	return spawnSync( 'tools/check-changelogger-use.php', [ 'origin/master', 'HEAD' ], {
-		stdio: 'inherit',
-	} );
+	const needChangelog = spawnSync(
+		'tools/check-changelogger-use.php',
+		[ 'origin/master', 'HEAD' ],
+		{
+			stdio: 'inherit',
+		}
+	);
+
+	// If a changelog file is needed, quit the push.
+	if ( needChangelog.status === 1 ) {
+		process.exitCode = 1;
+	}
+
+	if ( needChangelog.status === 0 ) {
+		console.log( chalk.green( 'Changelog check passed.' ) );
+	}
 }
+
+checkChangelogFiles();
 
 process.exitCode;
