07901b4cdcf8f0b6468c37890539b21cff0a91a5
Anna <anna.mcphee@gmail.com>
2021-07-30 15:33:18 +1200

    [not verified] change splitDomainPath call to breadcrumbPieces

--- projects/plugins/jetpack/modules/search/instant-search/components/path-breadcrumbs.jsx
+++ projects/plugins/jetpack/modules/search/instant-search/components/path-breadcrumbs.jsx
@@ -25,7 +25,7 @@ const PathBreadcrumbs = ( { className, onClick, url } ) => {
 				href={ `//${ url }` }
 				onClick={ onClick }
 			>
-				{ splitDomainPath( url ).map( ( piece, index, pieces ) => (
+				{ breadcrumbPieces.map( ( piece, index, pieces ) => (
 					<span className="jetpack-instant-search__path-breadcrumb-piece">
 						{ decodeURIComponent( piece ) }
 						{ index !== pieces.length - 1 ? ' › ' : '' }
