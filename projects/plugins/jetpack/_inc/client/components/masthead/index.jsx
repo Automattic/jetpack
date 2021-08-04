bea4e7578a8393e5279ebdf86779223dfbfd580d
Daniel Walmsley <goldsounds@gmail.com>
2021-08-04 10:37:13 -0700

    Use new shared Masthead component

--- projects/plugins/jetpack/_inc/client/components/masthead/index.jsx
+++ projects/plugins/jetpack/_inc/client/components/masthead/index.jsx
@@ -3,12 +3,11 @@
  */
 import React from 'react';
 import { connect } from 'react-redux';
-import { JetpackLogo } from '@automattic/jetpack-components';
+import { Masthead } from '@automattic/jetpack-components';
 
 /**
  * Internal dependencies
  */
-import analytics from 'lib/analytics';
 import { HeaderNav } from './header-nav';
 import {
 	getSiteConnectionStatus,
@@ -16,50 +15,20 @@ import {
 	fetchSiteConnectionTest,
 } from 'state/connection';
 
-export class Masthead extends React.Component {
-	trackLogoClick = () => {
-		analytics.tracks.recordJetpackClick( {
-			target: 'masthead',
-			path: 'logo',
-		} );
-	};
-
+export class JetpackMasthead extends React.Component {
 	testConnection = () => {
 		return this.props.testConnection();
 	};
 
 	render() {
-		const { sandboxDomain, siteConnectionStatus } = this.props;
-
-		const offlineNotice = siteConnectionStatus === 'offline' ? <code>Offline Mode</code> : '',
-			sandboxedBadge = sandboxDomain ? (
-				<code
-					id="sandbox-domain-badge"
-					onClick={ this.testConnection }
-					onKeyDown={ this.testConnection }
-					role="button"
-					tabIndex={ 0 }
-					title={ `Sandboxing via ${ sandboxDomain }. Click to test connection.` }
-				>
-					API Sandboxed
-				</code>
-			) : (
-				''
-			);
-
 		return (
-			<div className="jp-masthead">
-				<div className="jp-masthead__inside-container">
-					<div className="jp-masthead__logo-container">
-						<a onClick={ this.trackLogoClick } className="jp-masthead__logo-link" href="#dashboard">
-							<JetpackLogo className="jetpack-logo__masthead" />
-						</a>
-						{ offlineNotice }
-						{ sandboxedBadge }
-					</div>
-					<HeaderNav location={ this.props.location } />
-				</div>
-			</div>
+			<Masthead
+				sandboxDomain={ this.props.sandboxDomain }
+				siteConnectionStatus={ this.props.siteConnectionStatus }
+				testConnection={ this.props.testConnection }
+			>
+				<HeaderNav location={ this.props.location } />
+			</Masthead>
 		);
 	}
 }
@@ -76,4 +45,4 @@ export default connect(
 			testConnection: () => dispatch( fetchSiteConnectionTest() ),
 		};
 	}
-)( Masthead );
+)( JetpackMasthead );
