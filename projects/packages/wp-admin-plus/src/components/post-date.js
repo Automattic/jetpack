538d6b94cfedceda91a90b401d7a65238932970b
retrofox <rdsuarez@gmail.com>
2021-08-06 10:17:58 -0300

    [not verified] twaeking post date label

--- projects/packages/wp-admin-plus/src/components/post-date.js
+++ projects/packages/wp-admin-plus/src/components/post-date.js
@@ -6,8 +6,9 @@ import { useSelect, useDispatch } from '@wordpress/data';
 import { store as coreStore } from '@wordpress/core-data';
 import { dateI18n } from '@wordpress/date';
 import { useState } from '@wordpress/element';
+import { __, sprintf } from '@wordpress/i18n';
 
-export default function PostDateEdit( { id, postIds, type, fallbackText } ) {
+export default function PostDateEdit( { id, postIds, type, fallbackText, status } ) {
 	const [ localPostDate, setLocalPostDate ] = useState();
 	const posts = useSelect(
 		select => {
@@ -50,6 +51,37 @@ export default function PostDateEdit( { id, postIds, type, fallbackText } ) {
 	const singlePost = post[ 0 ];
 	const postDate = localPostDate || singlePost.date_gmt;
 
+	let dateLabel = '';
+
+	switch ( status ) {
+		case 'publish':
+			dateLabel = sprintf(
+				/* translators: %s: Publish state and date of the post. */
+				__( 'Published %s' ),
+				dateI18n( 'Y/m/d \\a\\t g:i a', postDate )
+			);
+			break;
+
+		case 'future':
+			dateLabel = sprintf(
+				/* translators: %s: Future state and date of the post. */
+				__( 'Scheduled %s' ),
+				dateI18n( 'Y/m/d \\a\\t g:i a', postDate )
+			);
+			break;
+
+		case 'draft':
+			dateLabel = sprintf(
+				/* translators: %s: Draft state and date of the post. */
+				__( 'Last Modified %s' ),
+				dateI18n( 'Y/m/d \\a\\t g:i a', postDate )
+			);
+			break;
+
+		default:
+			dateLabel = dateI18n( 'Y/m/d \\a\\t g:i a', postDate );
+	}
+
 	return (
 		<Dropdown
 			position="bottom left"
@@ -62,7 +94,7 @@ export default function PostDateEdit( { id, postIds, type, fallbackText } ) {
 						aria-expanded={ isOpen }
 						variant="tertiary"
 					>
-						{ dateI18n( 'Y/m/d \\a\\t g:i a', postDate ) }
+						{ dateLabel }
 					</Button>
 				</>
 			) }
