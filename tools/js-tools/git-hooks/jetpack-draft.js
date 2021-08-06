e1503478f90c4deaa4b64a645c28238fbb951c6c
Daniel Walmsley <goldsounds@gmail.com>
2021-08-05 18:00:54 -0700

    Don't print anything if draft mode disabled

--- tools/js-tools/git-hooks/jetpack-draft.js
+++ tools/js-tools/git-hooks/jetpack-draft.js
@@ -8,15 +8,18 @@ const chalk = require( 'chalk' );
 
 let draftMode = false;
 
+/**
+ * Run the draft mode check
+ */
 function runCheckDraftMode() {
 	draftMode = fs.existsSync( '.jetpack-draft' );
-	draftMode
-		? console.log(
-				chalk.yellow(
-					"You're in draft mode. Skipping some checks. To exit draft mode, run `jetpack draft disable`."
-				)
-		  )
-		: console.log( chalk.green( 'Draft mode disabled. All checks enabled.' ) );
+	if ( draftMode ) {
+		console.log(
+			chalk.yellow(
+				"You're in draft mode. Skipping some checks. To exit draft mode, run `jetpack draft disable`."
+			)
+		);
+	}
 }
 
 runCheckDraftMode();
