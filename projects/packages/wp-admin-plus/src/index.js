213ce6064311b204cb1e019b430d795a6129718d
retrofox <rdsuarez@gmail.com>
2021-08-06 08:53:48 -0300

    [not verified] set post date optimistically

--- projects/packages/wp-admin-plus/src/index.js
+++ projects/packages/wp-admin-plus/src/index.js
@@ -20,28 +20,36 @@ domReady( () => {
 		return;
 	}
 
+	const postIds = [];
 	postRows.forEach( postRow => {
-		// Row data
+		const postDateElementWrapper = postRow.querySelector( '.column-date' );
+		const fallbackText = postDateElementWrapper.innerText;
+
+		// Try to pick post data from custom column.
+		let data;
 		const rowDataContainer = postRow.querySelector(
 			'.column-wp-admin-plus-column script[type="application/json"]'
 		);
-		const postDateElement = postRow.querySelector( '.column-date' );
-		const fallbackText = postDateElement.innerText;
-
-		let data;
 		try {
 			data = JSON.parse( rowDataContainer.text );
 		} catch ( e ) {
 			// @TODO: improve error handling.
 			// eslint-disable-next-line no-console
-			return console.error( 'error parsing json', e );
+			return console.error( 'error parsing json: ', e );
 		}
 
-		// clean data element.
+		// Clean data-container element.
 		if ( data ) {
 			rowDataContainer.remove();
 		}
 
-		render( <PostDate { ...data } fallbackText={ fallbackText } />, postDateElement );
+		// Collect all post ids in the current admin page.
+		postIds.push( data.id );
+
+		// Render a component for each post row.
+		render(
+			<PostDate { ...data } fallbackText={ fallbackText } postIds={ postIds } />,
+			postDateElementWrapper
+		);
 	} );
 } );
