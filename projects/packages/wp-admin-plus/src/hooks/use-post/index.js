4d2425d63a4db9a713941f0ada4bf6fcc1fda0fb
retrofox <rdsuarez@gmail.com>
2021-08-09 10:08:31 -0300

    [not verified] usePost: refactoring hook

--- projects/packages/wp-admin-plus/src/hooks/use-post/index.js
+++ projects/packages/wp-admin-plus/src/hooks/use-post/index.js
@@ -5,24 +5,24 @@ import { useSelect } from '@wordpress/data';
 import { store as coreStore } from '@wordpress/core-data';
 
 export default function usePost( { id, postIds, status = 'any', type = 'post' } ) {
-	return useSelect(
+	const posts = useSelect(
 		select => {
-			const posts = select( coreStore ).getEntityRecords( 'postType', type, {
+			return select( coreStore ).getEntityRecords( 'postType', type, {
 				include: postIds,
 				status,
 			} );
-
-			if ( ! posts?.length ) {
-				return;
-			}
-
-			const filteredPostById = posts.filter( item => item?.id === id );
-			if ( ! filteredPostById?.length ) {
-				return;
-			}
-
-			return filteredPostById[ 0 ];
 		},
-		[ postIds, id ]
+		[ postIds ]
 	);
+
+	if ( ! posts?.length ) {
+		return;
+	}
+
+	const filteredPostById = posts.filter( item => item?.id === id );
+	if ( ! filteredPostById?.length ) {
+		return;
+	}
+
+	return filteredPostById[ 0 ];
 }
