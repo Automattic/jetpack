69eb8f7c16a630428de9bee6344f0caaa9ce851f
retrofox <rdsuarez@gmail.com>
2021-08-09 11:07:44 -0300

    [not verified] post status label: pass status to hook

--- projects/packages/wp-admin-plus/src/components/post-status-label/index.js
+++ projects/packages/wp-admin-plus/src/components/post-status-label/index.js
@@ -5,9 +5,10 @@ import { Fragment } from '@wordpress/element';
 import usePost from '../../hooks/use-post';
 
 export default function PostStatusLabel( { id, postIds, status, type, statuses, fallbackText } ) {
-	const post = usePost( { id, postIds, type } );
+	const post = usePost( { id, postIds, type, status } );
 	if ( ! post?.status ) {
 		return fallbackText;
 	}
+
 	return <Fragment>{ statuses?.[ post.status ] }</Fragment>;
 }
