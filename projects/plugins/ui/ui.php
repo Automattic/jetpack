3e739c62fe0ad3c99172ccb94503347380d9d4db
Paul Sealock <psealock@gmail.com>
2021-08-12 11:52:39 +1200

    ui

---
+++ projects/plugins/ui/ui.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *
+ * Plugin Name: Jetpack ui
+ * Plugin URI: TBD
+ * Description: ui
+ * Version: 0.1.0-alpha
+ * Author: Automattic
+ * Author URI: https://jetpack.com/
+ * License: GPLv2 or later
+ * Text Domain: jetpack
+ *
+ * @package automattic/jetpack-ui
+ */
+
+// Code some good stuff!
