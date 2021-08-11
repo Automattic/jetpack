910cf019eda59294d96589ff4510912c4a611fbb
Brandon Kraft <public@brandonkraft.com>
2021-08-11 14:01:35 -0500

    Soft fail for Jetpack older versions

--- projects/plugins/backup/jetpack-backup.php
+++ projects/plugins/backup/jetpack-backup.php
@@ -40,16 +40,64 @@ define( 'JETPACK_BACKUP_PLUGIN_SLUG', 'jetpack-backup' );
 define( 'JETPACK_BACKUP_PLUGIN_NAME', 'Jetpack Backup' );
 define( 'JETPACK_BACKUP_PLUGIN_URI', 'https://jetpack.com/jetpack-backup' );
 
+// Require Jetpack 10+, if Jetpack is installed.
+if ( defined( 'JETPACK__VERSION' ) && version_compare( '10.0.0', JETPACK__VERSION, '>' ) ) {
+	add_action(
+		'admin_notices',
+		function () {
+			?>
+		<div class="notice notice-error is-dismissible">
+			<p>
+				<?php
+				esc_html_e( 'Jetpack Backup does not require Jetpack; however, if you are running both plugins, you must have version 10 or higher of Jetpack to use Jetpack Backup', 'jetpack-backup' );
+				?>
+			</p>
+		</div>
+			<?php
+		}
+	);
+	exit;
+}
+
 // Jetpack Autoloader.
 $jetpack_autoloader = JETPACK_BACKUP_PLUGIN_DIR . 'vendor/autoload_packages.php';
 if ( is_readable( $jetpack_autoloader ) ) {
 	require_once $jetpack_autoloader;
-} else {
+} else { // Something very unexpected. Error out gently with an admin_notice and exit loading.
 	if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
 		error_log( // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
 			__( 'Error loading autoloader file for Jetpack Backup plugin', 'jetpack-backup' )
 		);
 	}
+
+	add_action(
+		'admin_notices',
+		function () {
+			?>
+		<div class="notice notice-error is-dismissible">
+			<p>
+				<?php
+				printf(
+					wp_kses(
+					/* translators: Placeholder is a link to a support document. */
+						__( 'Your installation of Jetpack Backup is incomplete. If you installed Jetpack Backup from GitHub, please refer to <a href="%1$s" target="_blank" rel="noopener noreferrer">this document</a> to set up your development environment. Jetpack Backup must have Composer dependencies installed and built via the build command.', 'jetpack-backup' ),
+						array(
+							'a' => array(
+								'href'   => array(),
+								'target' => array(),
+								'rel'    => array(),
+							),
+						)
+					),
+					'https://github.com/Automattic/jetpack/blob/master/docs/development-environment.md#building-your-project'
+				);
+				?>
+			</p>
+		</div>
+			<?php
+		}
+	);
+
 	exit;
 }
 
