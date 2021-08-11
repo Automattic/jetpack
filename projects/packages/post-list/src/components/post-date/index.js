b340c71cb856220d7efe9ae77001668b0a32ae61
retrofox <rdsuarez@gmail.com>
2021-08-11 16:56:37 -0300

    [not verified] communicate: init component

--- projects/packages/post-list/src/components/post-date/index.js
+++ projects/packages/post-list/src/components/post-date/index.js
@@ -5,15 +5,52 @@ import { Button, Dropdown, DateTimePicker } from '@wordpress/components';
 import { useDispatch } from '@wordpress/data';
 import { store as coreStore } from '@wordpress/core-data';
 import { dateI18n } from '@wordpress/date';
-import { useState } from '@wordpress/element';
+import { useState, Fragment } from '@wordpress/element';
 import { __, sprintf } from '@wordpress/i18n';
 import usePost from '../../hooks/use-post';
+import { store as noticesStore } from '@wordpress/notices';
+
+function getDateLabel( status, date ) {
+	let dateLabel = '';
+
+	switch ( status ) {
+		case 'publish':
+			dateLabel = sprintf(
+				/* translators: %s: Publish state and date of the post. */
+				__( 'Published %s', 'jetpack-post-list' ),
+				dateI18n( 'Y/m/d \\a\\t g:i a', date )
+			);
+			break;
+
+		case 'future':
+			dateLabel = sprintf(
+				/* translators: %s: Future state and date of the post. */
+				__( 'Scheduled %s', 'jetpack-post-list' ),
+				dateI18n( 'Y/m/d \\a\\t g:i a', date )
+			);
+			break;
+
+		case 'draft':
+			dateLabel = sprintf(
+				/* translators: %s: Draft state and date of the post. */
+				__( 'Last Modified %s', 'jetpack-post-list' ),
+				dateI18n( 'Y/m/d \\a\\t g:i a', date )
+			);
+			break;
+
+		default:
+			dateLabel = dateI18n( 'Y/m/d \\a\\t g:i a', date );
+	}
+
+	return dateLabel;
+}
 
 export default function PostDateEdit( { id, postIds, type, fallbackText, status } ) {
 	const [ localPostDate, setLocalPostDate ] = useState();
 	const [ localPostStatus, setLocalPostStatus ] = useState();
 	const post = usePost( { id, postIds, type } );
 	const { saveEntityRecord } = useDispatch( coreStore );
+	const { createSuccessNotice } = useDispatch( noticesStore );
 
 	async function handleUpdatePost( date ) {
 		// 100% optimistic.
@@ -33,6 +70,16 @@ export default function PostDateEdit( { id, postIds, type, fallbackText, status 
 
 		// Update the Post Status once the post saves.
 		setLocalPostStatus( savedPost?.status );
+
+		createSuccessNotice( getDateLabel( savedPost?.status, savedPost?.date ), {
+			type: 'snackbar',
+			actions: [
+				{
+					label: __( 'Undo', 'jetpack-post-list' ),
+					onClick: console.warn,
+				},
+			],
+		} );
 	}
 
 	if ( ! post ) {
@@ -42,61 +89,32 @@ export default function PostDateEdit( { id, postIds, type, fallbackText, status 
 	const postDate = localPostDate || post.date_gmt;
 	const postStatus = localPostStatus || post.status || status;
 
-	let dateLabel = '';
-
-	switch ( postStatus ) {
-		case 'publish':
-			dateLabel = sprintf(
-				/* translators: %s: Publish state and date of the post. */
-				__( 'Published %s' ),
-				dateI18n( 'Y/m/d \\a\\t g:i a', postDate )
-			);
-			break;
-
-		case 'future':
-			dateLabel = sprintf(
-				/* translators: %s: Future state and date of the post. */
-				__( 'Scheduled %s' ),
-				dateI18n( 'Y/m/d \\a\\t g:i a', postDate )
-			);
-			break;
-
-		case 'draft':
-			dateLabel = sprintf(
-				/* translators: %s: Draft state and date of the post. */
-				__( 'Last Modified %s' ),
-				dateI18n( 'Y/m/d \\a\\t g:i a', postDate )
-			);
-			break;
-
-		default:
-			dateLabel = dateI18n( 'Y/m/d \\a\\t g:i a', postDate );
-	}
-
 	return (
-		<Dropdown
-			position="bottom left"
-			contentClassName="edit-post-post-schedule__dialog"
-			renderToggle={ ( { onToggle, isOpen } ) => (
-				<>
-					<Button
-						className="post-row__date-button"
-						onClick={ onToggle }
-						aria-expanded={ isOpen }
-						variant="tertiary"
-					>
-						{ dateLabel }
-					</Button>
-				</>
-			) }
-			renderContent={ () => (
-				<DateTimePicker
-					currentDate={ postDate }
-					onChange={ handleUpdatePost }
-					is12Hour={ true }
-					events={ [] }
-				/>
-			) }
-		/>
+		<Fragment>
+			<Dropdown
+				position="bottom left"
+				contentClassName="edit-post-post-schedule__dialog"
+				renderToggle={ ( { onToggle, isOpen } ) => (
+					<>
+						<Button
+							className="post-row__date-button"
+							onClick={ onToggle }
+							aria-expanded={ isOpen }
+							variant="tertiary"
+						>
+							{ getDateLabel( postStatus, postDate ) }
+						</Button>
+					</>
+				) }
+				renderContent={ () => (
+					<DateTimePicker
+						currentDate={ postDate }
+						onChange={ handleUpdatePost }
+						is12Hour={ true }
+						events={ [] }
+					/>
+				) }
+			/>
+		</Fragment>
 	);
 }
