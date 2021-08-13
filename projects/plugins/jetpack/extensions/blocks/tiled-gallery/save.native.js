86641f40d8faf3de83b5acc560500296cd2aabbd
Paul Von Schrottky <paul.von.schrottky@automattic.com>
2021-08-13 17:23:44 -0400

    Added native files

---
+++ projects/plugins/jetpack/extensions/blocks/tiled-gallery/save.native.js
@@ -0,0 +1,39 @@
+/**
+ * Internal dependencies
+ */
+import Layout from './layout';
+import { defaultColumnsNumber } from './edit';
+import { getActiveStyleName } from '../../shared/block-styles';
+import { LAYOUT_STYLES } from './constants';
+
+export default function TiledGallerySave( { attributes } ) {
+	const { imageFilter, images } = attributes;
+
+	if ( ! images.length ) {
+		return null;
+	}
+
+	const {
+		align,
+		className,
+		columns = defaultColumnsNumber( attributes ),
+		linkTo,
+		roundedCorners,
+		columnWidths,
+	} = attributes;
+
+	return (
+		<Layout
+			align={ align }
+			className={ className }
+			columns={ columns }
+			imageFilter={ imageFilter }
+			images={ images }
+			isSave
+			layoutStyle={ getActiveStyleName( LAYOUT_STYLES, className ) }
+			linkTo={ linkTo }
+			roundedCorners={ roundedCorners }
+			columnWidths={ columnWidths }
+		/>
+	);
+}
