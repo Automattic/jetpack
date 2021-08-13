45bd3eb8e002552570a292551373478206b721c5
Tony Tahmouch <tony.tahmouch@gm.com>
2021-08-13 16:02:25 -0400

    TT | 3471 | "Introduce allow-list and disallow-list for blocks."
    Updating the entry point for the registration of `jetpack/contact-info` and `jetpack/story` block types to export `register*Block` functions to allow for them to be conditionally registered independently of each other on demand instead of implicitly when this module is imported in `gutenberg-mobile`.

--- projects/plugins/jetpack/extensions/editor.native.js
+++ projects/plugins/jetpack/extensions/editor.native.js
@@ -3,6 +3,5 @@
  */
 import './shared/block-category';
 
-// Register blocks
-import './blocks/contact-info/editor';
-import './blocks/story/editor';
+export const registerContactInfoBlock = () => require( './blocks/contact-info/editor' );
+export const registerStoryBlock = () => require( './blocks/story/editor' );
