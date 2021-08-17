612d52c71820124587260d0c5c8d04c57329b137
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2021-08-17 15:05:22 +0300

    messing with fs permissions

--- projects/plugins/jetpack/tests/e2e/specs/plugin-updater.test.js
+++ projects/plugins/jetpack/tests/e2e/specs/plugin-updater.test.js
@@ -35,12 +35,23 @@ describe( 'Jetpack updater', () => {
 			'pnpx jetpack docker --type e2e --name t1 -v exec -- chown -R www-data:www-data /var/www'
 		);
 
+		await execShellCommand(
+			'pnpx jetpack docker --type e2e --name t1 -v exec -- chown -R 755 /var/www'
+		);
+
 		await execWpCommand( 'plugin install --activate jetpack' );
 		await execWpCommand( 'plugin activate e2e-plugin-updater' );
 
 		await execWpCommand( 'option set e2e_jetpack_upgrader_update_version 99.9-alpha' );
 		await execWpCommand(
 			`option set e2e_jetpack_upgrader_plugin_url ${ siteUrl }/wp-content/uploads/jetpack.zip`
+		);
+
+		await execShellCommand(
+			'pnpx jetpack docker --type e2e --name t1 -v exec -- ls -la /var/www/html/wp-content'
+		);
+		await execShellCommand(
+			'pnpx jetpack docker --type e2e --name t1 -v exec -- ls -la /var/www/html/wp-content/plugins'
 		);
 	} );
 
