233ea06026ba3597f695fb40730a94ed0142eb7a
Jasper Kang <jasper.kang@automattic.com>
2021-08-19 09:59:33 +1200

    added aria label for Jetpack Logo

--- projects/js-packages/components/components/jetpack-logo/index.jsx
+++ projects/js-packages/components/components/jetpack-logo/index.jsx
@@ -32,8 +32,10 @@ class JetpackLogo extends React.Component {
 				y="0px"
 				viewBox={ viewBox }
 				className={ classnames( 'jetpack-logo', className ) }
+				aria-labelledby="jetpack-logo-title"
 				{ ...otherProps }
 			>
+				<title id="jetpack-logo-title">Jetpack Logo</title>
 				<path
 					fill={ logoColor }
 					d="M16,0C7.2,0,0,7.2,0,16s7.2,16,16,16s16-7.2,16-16S24.8,0,16,0z M15,19H7l8-16V19z M17,29V13h8L17,29z"
