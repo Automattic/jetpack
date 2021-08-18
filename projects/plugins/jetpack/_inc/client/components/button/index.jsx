2f958509a8187dd68f7e848c290bab7aa783a8a7
Jasper Kang <jasper.kang@automattic.com>
2021-08-19 11:26:18 +1200

    [not verified] added role for a as button

--- projects/plugins/jetpack/_inc/client/components/button/index.jsx
+++ projects/plugins/jetpack/_inc/client/components/button/index.jsx
@@ -36,6 +36,9 @@ export default class Button extends React.Component {
 	render() {
 		const element = this.props.href ? 'a' : 'button';
 		const { primary, compact, scary, borderless, className, ...props } = this.props;
+		if ( element === 'a' ) {
+			props.role = 'button';
+		}
 
 		const buttonClasses = classNames( {
 			'dops-button': true,
