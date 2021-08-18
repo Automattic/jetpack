0bc08907f61402159b068b2530bb1b80d1c14048
Adnan Haque <adnan007.id@gmail.com>
2021-08-18 16:56:37 +0600

    Implement a function to find cache entries by prefix

--- projects/plugins/boost/app/lib/class-transient.php
+++ projects/plugins/boost/app/lib/class-transient.php
@@ -14,13 +14,15 @@ namespace Automattic\Jetpack_Boost\Lib;
  */
 class Transient {
 
+	const OPTION_PREFIX = 'jb_transient_';
+
 	/**
 	 * Get the key with prefix.
 	 *
 	 * @param string $key the key to be prefixed.
 	 */
 	public static function key( $key ) {
-		return 'jb_transient_' . $key;
+		return static::OPTION_PREFIX . $key;
 	}
 
 	/**
@@ -70,6 +72,46 @@ class Transient {
 		}
 
 		return $data;
+	}
+
+	/**
+	 * Find all cache keys by prefix.
+	 *
+	 * @param string $prefix    Cache key prefix.
+	 *
+	 * return string[] Array of string representing the keys that match the prefix.
+	 */
+	public static function find_keys_by_prefix( $prefix ) {
+		global $wpdb;
+
+		/**
+		 * The prefix used in option_name.
+		 */
+		$option_prefix = static::key( $prefix );
+
+		/*
+		 * Find the length of prefix used by all transients.
+		 */
+		$transient_prefix_length = strlen( static::OPTION_PREFIX );
+
+		/*
+		 * We are looking for all cache entries in the database for supplied cache prefix. As the `Transient` class adds
+		 * another prefix before storing the cache in database, we will remove that during the query to return the
+		 * proper keys.
+		 */
+		$keys = $wpdb->get_col(
+			$wpdb->prepare(
+				"
+				SELECT  SUBSTRING(`option_name`, %d)
+				FROM    $wpdb->options
+				WHERE   `option_name` LIKE %s
+			",
+				$transient_prefix_length + 1,
+				$option_prefix . '%'
+			)
+		);
+
+		return $keys;
 	}
 
 	/**
