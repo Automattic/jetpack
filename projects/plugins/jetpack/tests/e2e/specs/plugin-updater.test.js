c9e1d2dd3c2de300e0da073556c8b417459a142f
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2021-08-18 15:47:21 +0300

    .

--- projects/plugins/jetpack/tests/e2e/specs/plugin-updater.test.js
+++ projects/plugins/jetpack/tests/e2e/specs/plugin-updater.test.js
@@ -19,6 +19,9 @@ import { prerequisitesBuilder } from '../lib/env/prerequisites';
  */
 describe( 'Jetpack updater', () => {
 	beforeAll( async () => {
+		// currentVersion = await execShellCommand( './../../../../../tools/plugin-version.sh jetpack' );
+		// await execShellCommand( './../../../../../tools/plugin-version.sh -v 99.9 jetpack' );
+
 		await prerequisitesBuilder()
 			.withCleanEnv()
 			.withLoggedIn( true )
@@ -28,11 +31,13 @@ describe( 'Jetpack updater', () => {
 		await prepareUpdaterTest();
 
 		await execWpCommand( `plugin install --activate jetpack` );
+		await execWpCommand( `plugin activate e2e-plugin-updater` );
 		await execWpCommand( `option set e2e_jetpack_upgrader_update_version 99.9-alpha` );
 		await execWpCommand(
 			`option set e2e_jetpack_upgrader_plugin_url ${ siteUrl }/wp-content/uploads/jetpack.zip`
 		);
-		await execWpCommand( `plugin activate e2e-plugin-updater` );
+
+		await execWpCommand( `plugin list` );
 
 		await execShellCommand(
 			'pnpx jetpack docker --type e2e --name t1 -v exec -- chmod -R 755 /var/www/html/'
@@ -40,9 +45,17 @@ describe( 'Jetpack updater', () => {
 		await execShellCommand(
 			'pnpx jetpack docker --type e2e --name t1 -v exec -- chown -R www-data:www-data /var/www/html/'
 		);
+
+		await execShellCommand(
+			'pnpx jetpack docker --type e2e --name t1 -v exec -- ls -la wp-content/plugins'
+		);
 	} );
 
 	afterAll( async () => {
+		// await execShellCommand(
+		// 	`./../../../../../tools/plugin-version.sh -v ${ currentVersion } jetpack`
+		// );
+
 		await execWpCommand( 'plugin uninstall --deactivate jetpack' );
 		await execShellCommand(
 			'pnpx jetpack docker --type e2e --name t1 -v exec -- rm /var/www/html/wp-content/plugins/jetpack'
