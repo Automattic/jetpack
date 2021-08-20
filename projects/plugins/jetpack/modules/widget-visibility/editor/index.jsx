0c51a71ab21001ffb8959a489c4c44ec7bf6ad4d
Jeremy Herve <jeremy@jeremy.hu>
2021-08-20 13:56:47 +0200

    [not verified] Scaffolding for a new entry point for Widget Visibility

--- projects/plugins/jetpack/modules/widget-visibility/editor/index.jsx
+++ projects/plugins/jetpack/modules/widget-visibility/editor/index.jsx
@@ -1,9 +1,13 @@
-/* eslint-disable react/react-in-jsx-scope */
 /**
  * WordPress dependencies
  */
-import { Fragment } from '@wordpress/element';
+import { render } from '@wordpress/element';
 
-export const WidgetVisibility = () => {
-	return <Fragment>Widget Visibility</Fragment>;
-};
+/**
+ * Render things.
+ *
+ * @param {string} id - The selector to render.
+ */
+export function init( id ) {
+	render( <div>Widget Visibility</div>, document.getElementById( id ) );
+}
