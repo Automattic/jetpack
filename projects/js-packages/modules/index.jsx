8fe65493f9394124e827f6b803f59d78a33b06b9
Daniel Walmsley <goldsounds@gmail.com>
2021-08-20 17:37:26 -0700

    Cleanup some linting errors

---
+++ projects/js-packages/modules/index.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable wpcalypso/import-docblock */
+
+/**
+ * State.
+ */
+import * as ActionTypes from './src/state/action-types';
+export { ActionTypes };
+import * as State from './src/state';
+export { State };
