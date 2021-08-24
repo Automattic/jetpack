9ede148f14acdda8643751f56a72c18714b36990
André Kallehauge <3846700+kallehauge@users.noreply.github.com>
2021-08-25 00:38:45 +0200

    Recommendation Prompt Layout: Make illustration optional
    We do this so we can use the same template with progress and header (question) for content that wants to use 100% width.

--- projects/plugins/jetpack/_inc/client/recommendations/prompts/prompt-layout/index.jsx
+++ projects/plugins/jetpack/_inc/client/recommendations/prompts/prompt-layout/index.jsx
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 
@@ -18,25 +19,31 @@ const PromptLayout = props => {
 	const { answer, description, illustrationPath, progressBar, question } = props;
 
 	return (
-		<div className="jp-recommendations-question__main">
+		<div
+			className={ classNames( 'jp-recommendations-question__main', {
+				'jp-recommendations-question__main--with-illustration': !! illustrationPath,
+			} ) }
+		>
 			<div className="jp-recommendations-question__content">
 				<div className="jp-recommendations-question__progress-bar">{ progressBar }</div>
 				<h1 className="jp-recommendations-question__question">{ question }</h1>
 				<p className="jp-recommendations-question__description">{ description }</p>
 				<div className="jp-recommendations-question__answer">{ answer }</div>
 			</div>
-			<div className="jp-recommendations-question__illustration-container">
-				<img
-					className="jp-recommendations-question__illustration-background"
-					src={ imagePath + 'recommendations/background.svg' }
-					alt=""
-				/>
-				<img
-					className="jp-recommendations-question__illustration-foreground"
-					src={ imagePath + illustrationPath }
-					alt=""
-				/>
-			</div>
+			{ illustrationPath && (
+				<div className="jp-recommendations-question__illustration-container">
+					<img
+						className="jp-recommendations-question__illustration-background"
+						src={ imagePath + 'recommendations/background.svg' }
+						alt=""
+					/>
+					<img
+						className="jp-recommendations-question__illustration-foreground"
+						src={ imagePath + illustrationPath }
+						alt=""
+					/>
+				</div>
+			) }
 		</div>
 	);
 };
@@ -44,7 +51,7 @@ const PromptLayout = props => {
 PromptLayout.propTypes = {
 	answer: PropTypes.element.isRequired,
 	description: PropTypes.oneOfType( [ PropTypes.string, PropTypes.element ] ).isRequired,
-	illustrationPath: PropTypes.string.isRequired,
+	illustrationPath: PropTypes.string,
 	progressBar: PropTypes.element.isRequired,
 	question: PropTypes.oneOfType( [ PropTypes.string, PropTypes.element ] ).isRequired,
 };
