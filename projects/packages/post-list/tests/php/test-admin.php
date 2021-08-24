d1589ef7ff8ebc6d70986d878d66ebccb4fb4af6
DustyReagan <dusty@dustyreagan.com>
2021-08-24 17:48:49 -0500

    [not verified] Added unit test stub files.

---
+++ projects/packages/post-list/tests/php/test-admin.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Automattic\Jetpack\PostList;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Unit tests for the admin class.
+ *
+ * @package automattic/jetpack-post-list
+ */
+class Test_Admin extends TestCase {
+
+	/**
+	 * Just getting things stubbed out.
+	 */
+	public function test_hello_tests() {
+		$this->assertSame( 1, 1 );
+	}
+}
