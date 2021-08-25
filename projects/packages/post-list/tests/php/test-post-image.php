48ed0d774877acc3c041540c1f38a1812bdaeb9a
DustyReagan <dusty@dustyreagan.com>
2021-08-25 14:40:54 -0500

    [not verified] Added new tests for Post_Image.

--- projects/packages/post-list/tests/php/test-post-image.php
+++ projects/packages/post-list/tests/php/test-post-image.php
@@ -5,14 +5,73 @@ namespace Automattic\Jetpack\PostList;
 use PHPUnit\Framework\TestCase;
 
 /**
- * Unit tests for the admin class.
+ * Unit tests for the Post_Image class.
  *
  * @package automattic/jetpack-post-list
  */
 class Test_Post_Image extends TestCase {
+	/**
+	 * Return null if our class value exists, but it's outside a class attribute.
+	 */
+	public function test_return_null_when_not_in_class() {
+		$post_content = 'wp-image-24';
+
+		$attachment_id = Post_Image::get_first_image_id_from_post_content( $post_content );
+		$this->assertSame( null, $attachment_id );
+	}
 
 	/**
-	 * Just getting things stubbed out.
+	 * Return null if our class value exists, but it's in something other than an image tag.
+	 */
+	public function test_return_null_when_not_on_an_img() {
+		$post_content = '<div class="wp-image-24"></div>';
+
+		$attachment_id = Post_Image::get_first_image_id_from_post_content( $post_content );
+		$this->assertSame( null, $attachment_id );
+	}
+
+	/**
+	 * Returns ID when it is the only class value on the img tag.
+	 */
+	public function test_find_id_only_class_value() {
+		$post_content = '<img class="wp-image-24" />';
+
+		$attachment_id = Post_Image::get_first_image_id_from_post_content( $post_content );
+		$this->assertSame( 24, $attachment_id );
+	}
+
+	/**
+	 * Returns ID when found in the middle of class value.
+	 */
+	public function test_find_id_in_the_middle() {
+		$post_content = '<img class="first wp-image-24 end" />';
+
+		$attachment_id = Post_Image::get_first_image_id_from_post_content( $post_content );
+		$this->assertSame( 24, $attachment_id );
+	}
+
+	/**
+	 * Returns ID when found on the end of class value.
+	 */
+	public function test_find_id_on_the_end() {
+		$post_content = '<img class="first wp-image-24" />';
+
+		$attachment_id = Post_Image::get_first_image_id_from_post_content( $post_content );
+		$this->assertSame( 24, $attachment_id );
+	}
+
+	/**
+	 * Returns ID when found on the front of class value.
+	 */
+	public function test_find_id_on_the_front() {
+		$post_content = '<img class="wp-image-24 end" />';
+
+		$attachment_id = Post_Image::get_first_image_id_from_post_content( $post_content );
+		$this->assertSame( 24, $attachment_id );
+	}
+
+	/**
+	 * Get the image attachment ID from an example post with a gallery.
 	 */
 	public function test_get_id_from_gallery_image() {
 		$post_content_gallery_image = '<!-- wp:paragraph -->
@@ -25,5 +84,29 @@ class Test_Post_Image extends TestCase {
 
 		$attachment_id = Post_Image::get_first_image_id_from_post_content( $post_content_gallery_image );
 		$this->assertSame( 14, $attachment_id );
+	}
+
+	/**
+	 * Get the image attachment ID from an example post with a slideshow.
+	 */
+	public function test_get_id_from_slideshow_image() {
+		$post_content_slideshow_image = '<!-- wp:jetpack/slideshow {"ids":[18,5],"sizeSlug":"large"} -->
+<div class="wp-block-jetpack-slideshow aligncenter" data-effect="slide"><div class="wp-block-jetpack-slideshow_container swiper-container"><ul class="wp-block-jetpack-slideshow_swiper-wrapper swiper-wrapper"><li class="wp-block-jetpack-slideshow_slide swiper-slide"><figure><img alt="" class="wp-block-jetpack-slideshow_image wp-image-18" data-id="18" src="http://localhost/wp-content/uploads/2021/08/b311c1481829913164f33a353de10a66-2-1024x448.jpeg"/></figure></li><li class="wp-block-jetpack-slideshow_slide swiper-slide"><figure><img alt="" class="wp-block-jetpack-slideshow_image wp-image-5" data-id="5" src="http://localhost/wp-content/uploads/2021/08/201D3C7E-1BAE-4896-81A5-C848E2FC2AD1-1024x768.jpeg"/></figure></li></ul><a class="wp-block-jetpack-slideshow_button-prev swiper-button-prev swiper-button-white" role="button"></a><a class="wp-block-jetpack-slideshow_button-next swiper-button-next swiper-button-white" role="button"></a><a aria-label="Pause Slideshow" class="wp-block-jetpack-slideshow_button-pause" role="button"></a><div class="wp-block-jetpack-slideshow_pagination swiper-pagination swiper-pagination-white"></div></div></div>
+<!-- /wp:jetpack/slideshow -->';
+
+		$attachment_id = Post_Image::get_first_image_id_from_post_content( $post_content_slideshow_image );
+		$this->assertSame( 18, $attachment_id );
+	}
+
+	/**
+	 * Get the image attachment ID from an example post with an image block.
+	 */
+	public function test_get_id_from_image_block() {
+		$post_content_image_block = '<!-- wp:image {"id":24,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="http://localhost/wp-content/uploads/2021/08/b311c1481829913164f33a353de10a66-3-1024x448.jpeg" alt="" class="wp-image-24"/></figure>
+<!-- /wp:image -->';
+
+		$attachment_id = Post_Image::get_first_image_id_from_post_content( $post_content_image_block );
+		$this->assertSame( 24, $attachment_id );
 	}
 }
