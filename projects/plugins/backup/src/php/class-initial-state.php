a75d1d6414844b2113d8554ebded78abc053590a
leogermani <leogermani@gmail.com>
2021-08-26 09:36:17 -0300

    remove unused methods (#20828)

--- projects/plugins/backup/src/php/class-initial-state.php
+++ projects/plugins/backup/src/php/class-initial-state.php
@@ -5,8 +5,6 @@
  * @package automattic/jetpack-backup-plugin
  */
 
-use Automattic\Jetpack\Constants;
-use Automattic\Jetpack\Device_Detection\User_Agent_Info;
 use Automattic\Jetpack\Status;
 
 /**
@@ -32,31 +30,6 @@ class Initial_State {
 				'buildUrl' => plugins_url( 'build/', JETPACK_BACKUP_PLUGIN_ROOT_FILE ),
 			),
 		);
-	}
-
-	/**
-	 * Whether we can the connection iframe.
-	 *
-	 * @return bool
-	 */
-	private function can_use_connection_iframe() {
-		global $is_safari;
-
-		/**
-		 * Filters whether the connection manager should use the iframe authorization
-		 * flow instead of the regular redirect-based flow.
-		 *
-		 * @since 8.3.0
-		 *
-		 * @param Boolean $is_iframe_flow_used should the iframe flow be used, defaults to false.
-		 */
-		$iframe_flow = apply_filters( 'jetpack_use_iframe_authorization_flow', false );
-
-		if ( ! $iframe_flow ) {
-			return false;
-		}
-
-		return ! $is_safari && ! User_Agent_Info::is_opera_desktop() && ! Constants::is_true( 'JETPACK_SHOULD_NOT_USE_CONNECTION_IFRAME' );
 	}
 
 	/**
