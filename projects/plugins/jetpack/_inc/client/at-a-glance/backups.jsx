8a4d3f83b06f708bb56d210a745982d408a7eeeb
Caleb Bauermeister <cbauerman@gmail.com>
2021-08-27 13:11:26 -0700

    linter fixes

--- projects/plugins/jetpack/_inc/client/at-a-glance/backups.jsx
+++ projects/plugins/jetpack/_inc/client/at-a-glance/backups.jsx
@@ -235,23 +235,22 @@ class DashBackups extends Component {
 		return false;
 	}
 
-
 	renderFromRewindStatus () {
 		if ( 'unavailable' === this.props.rewindStatus && 'site_new' === this.props.rewindStatusReason ) {
 			return renderCard( {
 				className: 'jp-dash-item__is-inactive',
 				status: 'pro-inactive',
-				content: 
+				content:
 					__(
 						"Your site is new and may still be preparing it's backup config.",
 						'jetpack'
 					),
 			} );
 		// this.props.rewindStatus is empty string on API error.
-		} else if ( 'unavailable' === this.props.rewindStatus || '' === this.props.rewindStatus) {
-			return this.getVPContent()
+		} else if ( 'unavailable' === this.props.rewindStatus || '' === this.props.rewindStatus ) {
+			return this.getVPContent();
 		}
-		return <div className="jp-dash-item">{ this.getRewindContent() }</div>
+		return <div className="jp-dash-item">{ this.getRewindContent() }</div>;
 	}
 
 	render() {
