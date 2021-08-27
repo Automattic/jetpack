b88d38378852db9ffc77619cd6ebe50624a90322
Jasper Kang <kangzjnet@gmail.com>
2021-08-28 01:43:30 +1200

    Search: further layout and accessibility improvements for wp-admin page (#20736)

--- projects/plugins/jetpack/_inc/client/search/dashboard/mocked-legacy-search.jsx
+++ projects/plugins/jetpack/_inc/client/search/dashboard/mocked-legacy-search.jsx
@@ -17,13 +17,13 @@ import './mocked-legacy-search.scss';
  */
 export default function MockedLegacySearch() {
 	return (
-		<div className="jp-mocked-legacy-search">
+		<div className="jp-mocked-legacy-search" aria-hidden="true">
 			<div className="jp-mocked-legacy-search__search-controls">
 				<div className="jp-mocked-legacy-search__search-icon">
 					<Gridicon icon="search" size={ 24 } />
 				</div>
 				<div className="jp-mocked-legacy-search__search-input">
-					<TextRowPlaceHolder style={ { height: '50px', width: '80%', 'max-width': '212px' } } />
+					<TextRowPlaceHolder style={ { height: '50px', width: '80%', maxWidth: '212px' } } />
 				</div>
 			</div>
 		</div>
