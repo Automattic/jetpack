5471870a322cda1e060069579122954b582df3d8
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2021-08-31 12:46:33 +0300

    Dont trigger sync cron

--- projects/plugins/jetpack/tests/e2e/lib/env/prerequisites.js
+++ projects/plugins/jetpack/tests/e2e/lib/env/prerequisites.js
@@ -182,7 +182,6 @@ export async function activateModules( modulesList ) {
 		logger.prerequisites( `Activating module ${ module }` );
 		const result = await execWpCommand( `jetpack module activate ${ module }` );
 		expect( result ).toMatch( new RegExp( `Success: .* has been activated.`, 'i' ) );
-		await execWpCommand( 'cron event run jetpack_v2_heartbeat' );
 	}
 }
 
