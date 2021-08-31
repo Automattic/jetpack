68f409f01099df0b0ed9d301582543ec86882877
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2021-08-31 13:07:24 +0300

    update comment and properly log URL

--- projects/plugins/jetpack/tests/e2e/lib/pages/wp-admin/blocks/mailchimp.js
+++ projects/plugins/jetpack/tests/e2e/lib/pages/wp-admin/blocks/mailchimp.js
@@ -59,7 +59,7 @@ export default class MailchimpBlock extends PageActions {
 				await ( await LoginPage.init( wpComTab ) ).login( 'defaultUser' );
 			}
 
-			// Hacky way to force-sync Publicize activation. The first attempt is always get redirected to stats page.
+			// Hacky way to make sure the Calypso knows about newly created site (aka waiting for a site to get synced)
 			// TODO:
 			// explore a better way to sync the site. Maybe enable all the required modules as part of connection flow
 			// Or implement a way to trigger a sync manually.
@@ -69,15 +69,10 @@ export default class MailchimpBlock extends PageActions {
 				try {
 					count++;
 					await ConnectionsPage.init( wpComTab );
-					// if ( ! ( await connections.isEnabled() ) ) {
-					// 	throw 'Publicise not enabled/synced';
-					// }
-
 					loaded = true;
 				} catch ( e ) {
 					logger.warn(
-						'ConnectionsPage is not available yet. Attempt: ' + count,
-						' URL: ' + connectionsUrl
+						'ConnectionsPage is not available yet. Attempt: ' + count + '; URL: ' + connectionsUrl
 					);
 					await wpComTab.goto( connectionsUrl );
 					if ( count > 4 ) {
