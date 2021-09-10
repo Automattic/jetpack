308fa4b7946a24d075d30153ace81478afca7b7d
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2021-09-10 12:23:24 +0300

    fix abtest annotations

--- projects/packages/abtest/tests/php/test-abtest.php
+++ projects/packages/abtest/tests/php/test-abtest.php
@@ -29,7 +29,7 @@ class Test_Abtest extends TestCase {
 	/**
 	 * Tests with no test name provided.
 	 *
-	 * @covers Automattic\Jetpack\Abtest::get_variation
+	 * @covers Automattic\Jetpack\Abtest
 	 */
 	public function test_with_no_test_name_provided() {
 		$result = $this->abtest->get_variation( null );
@@ -39,7 +39,7 @@ class Test_Abtest extends TestCase {
 	/**
 	 * Tests when incorrect test name is provided.
 	 *
-	 * @covers Automattic\Jetpack\Abtest::get_variation
+	 * @covers Automattic\Jetpack\Abtest
 	 */
 	public function test_with_incorrect_test_name_provided() {
 		$result = $this->abtest->get_variation( 'example-test' );
@@ -49,7 +49,7 @@ class Test_Abtest extends TestCase {
 	/**
 	 * Tests when a test is inactive or does not exist.
 	 *
-	 * @covers Automattic\Jetpack\Abtest::get_variation
+	 * @covers Automattic\Jetpack\Abtest
 	 */
 	public function test_when_test_inactive_or_does_not_exist() {
 		$this->abtest->expects( $this->once() )
@@ -72,7 +72,7 @@ class Test_Abtest extends TestCase {
 	/**
 	 * Tests an error or malformed response.
 	 *
-	 * @covers Automattic\Jetpack\Abtest::get_variation
+	 * @covers Automattic\Jetpack\Abtest
 	 */
 	public function test_when_error_or_malformed_response() {
 		$this->abtest->expects( $this->once() )
@@ -90,7 +90,7 @@ class Test_Abtest extends TestCase {
 	/**
 	 * Tests when the response is in an unexpected format.
 	 *
-	 * @covers Automattic\Jetpack\Abtest::get_variation
+	 * @covers Automattic\Jetpack\Abtest
 	 */
 	public function test_when_response_in_unexpected_format() {
 		$this->abtest->expects( $this->once() )
@@ -112,7 +112,7 @@ class Test_Abtest extends TestCase {
 	/**
 	 * Test with an active test.
 	 *
-	 * @covers Automattic\Jetpack\Abtest::get_variation
+	 * @covers Automattic\Jetpack\Abtest
 	 */
 	public function test_with_valid_active_test() {
 		$variation = 'original';
