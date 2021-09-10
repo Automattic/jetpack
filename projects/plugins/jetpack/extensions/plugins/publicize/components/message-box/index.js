1086bbebeac7d902932ad8253440a879da7801d8
retrofox <rdsuarez@gmail.com>
2021-09-10 10:07:06 -0300

    [not verified] publicize: show chars counter in the help prop

--- projects/plugins/jetpack/extensions/plugins/publicize/components/message-box/index.js
+++ projects/plugins/jetpack/extensions/plugins/publicize/components/message-box/index.js
@@ -1,9 +1,4 @@
 /**
- * External dependencies
- */
-import classnames from 'classnames';
-
-/**
  * WordPress dependencies
  */
 import { __, _n, sprintf } from '@wordpress/i18n';
@@ -11,27 +6,20 @@ import { TextareaControl } from '@wordpress/components';
 
 export default function MessageBox( { message = '', onChange, disabled, maxLength } ) {
 	const charactersRemaining = maxLength - message.length;
-	const characterCountClass = classnames( 'jetpack-publicize-character-count', {
-		'wpas-twitter-length-limit': charactersRemaining <= 0,
-	} );
 
 	return (
-		<div className="jetpack-publicize-message-box">
-			<TextareaControl
-				value={ message }
-				onChange={ onChange }
-				disabled={ disabled }
-				maxLength={ maxLength }
-				placeholder={ __( 'Write a message for your audience here.', 'jetpack' ) }
-				rows={ 4 }
-			/>
-			<div className={ characterCountClass }>
-				{ sprintf(
-					/* translators: placeholder is a number. */
-					_n( '%d character remaining', '%d characters remaining', charactersRemaining, 'jetpack' ),
-					charactersRemaining
-				) }
-			</div>
-		</div>
+		<TextareaControl
+			value={ message }
+			onChange={ onChange }
+			disabled={ disabled }
+			maxLength={ maxLength }
+			placeholder={ __( 'Write a message for your audience here.', 'jetpack' ) }
+			rows={ 4 }
+			help={ sprintf(
+				/* translators: placeholder is a number. */
+				_n( '%d character remaining', '%d characters remaining', charactersRemaining, 'jetpack' ),
+				charactersRemaining
+			) }
+		/>
 	);
 }
