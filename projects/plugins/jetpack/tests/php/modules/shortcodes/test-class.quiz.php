c2f11d5911534a9fe0a817e28240294b728c332b
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2021-09-10 20:28:50 +0300

    Unit tests: Fix silent errors in Jetpack tests (#21030)
    * remove some tests
    
    * Add back test_handle_preferred_view
    
    * add test_register_dashboard_switcher back
    
    * remove fataling test once again
    
    * Fix masterbar test
    
    * fix quiz tests
    
    * Fix Test_Admin_Menu tests
    
    * Add back removed tests
    
    * Remove tests again (they are still silencing test failures)
    
    * Skip test_embed_shortcode_is_disabled_on_the_content_filter_during_sync
    
    * Add back masterbar tests and mark them as skipped
    
    Co-authored-by: mmtr <miguel.torres@automattic.com>

--- projects/plugins/jetpack/tests/php/modules/shortcodes/test-class.quiz.php
+++ projects/plugins/jetpack/tests/php/modules/shortcodes/test-class.quiz.php
@@ -35,7 +35,7 @@ class WP_Test_Jetpack_Shortcodes_Quiz extends WP_UnitTestCase {
 	 */
 	public function test_shortcodes_quiz_id() {
 		$shortcode_content = do_shortcode( '[quiz trackid="the-quiz"][question]What is the right answer?[/question][/quiz]' );
-		$this->assertEquals( '<div class="jetpack-quiz quiz" data-trackid="the-quiz"><div class="jetpack-quiz-question question">What is the right answer?</div></div>', $shortcode_content );
+		$this->assertEquals( '<div class="jetpack-quiz quiz" data-trackid="the-quiz"><div class="jetpack-quiz-question question" tabindex="-1">What is the right answer?</div></div>', $shortcode_content );
 	}
 
 	/**
@@ -48,7 +48,7 @@ class WP_Test_Jetpack_Shortcodes_Quiz extends WP_UnitTestCase {
 		$this->assertEquals( '', $shortcode_content );
 
 		$shortcode_content = do_shortcode( '[quiz][question]What is the right answer?[/question][/quiz]' );
-		$this->assertEquals( '<div class="jetpack-quiz quiz"><div class="jetpack-quiz-question question">What is the right answer?</div></div>', $shortcode_content );
+		$this->assertEquals( '<div class="jetpack-quiz quiz"><div class="jetpack-quiz-question question" tabindex="-1">What is the right answer?</div></div>', $shortcode_content );
 	}
 
 	/**
@@ -97,7 +97,7 @@ class WP_Test_Jetpack_Shortcodes_Quiz extends WP_UnitTestCase {
 	 */
 	public function test_shortcodes_complete() {
 		$shortcode_content = do_shortcode( '[quiz][question]What is the right answer?[/question][wrong]This is so wrong...[explanation]This is why this is wrong.[/explanation][/wrong][answer]Yes, this is right![explanation]Yay![/explanation][/answer][/quiz]' );
-		$this->assertEquals( '<div class="jetpack-quiz quiz"><div class="jetpack-quiz-question question">What is the right answer?</div><div class="jetpack-quiz-answer answer">This is so wrong...<div class="jetpack-quiz-explanation explanation">This is why this is wrong.</div></div><div class="jetpack-quiz-answer answer" data-correct="1">Yes, this is right!<div class="jetpack-quiz-explanation explanation">Yay!</div></div></div>', $shortcode_content );
+		$this->assertEquals( '<div class="jetpack-quiz quiz"><div class="jetpack-quiz-question question" tabindex="-1">What is the right answer?</div><div class="jetpack-quiz-answer answer">This is so wrong...<div class="jetpack-quiz-explanation explanation">This is why this is wrong.</div></div><div class="jetpack-quiz-answer answer" data-correct="1">Yes, this is right!<div class="jetpack-quiz-explanation explanation">Yay!</div></div></div>', $shortcode_content );
 	}
 
 
