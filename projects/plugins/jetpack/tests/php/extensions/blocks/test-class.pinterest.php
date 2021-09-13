60362182d89f84cf683dd9e8cfd4c38197b326cf
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2021-09-13 14:23:14 +0300

    try namespaced annotation for Pinterest test

--- projects/plugins/jetpack/tests/php/extensions/blocks/test-class.pinterest.php
+++ projects/plugins/jetpack/tests/php/extensions/blocks/test-class.pinterest.php
@@ -15,7 +15,7 @@ class WP_Test_Pinterest extends \WP_UnitTestCase {
 	/**
 	 * Test the Pin type detected for a given Pinterest URL.
 	 *
-	 * @covers ::pin_type
+	 * @covers Pinterest\pin_type
 	 * @dataProvider get_pinterest_urls
 	 *
 	 * @since 9.2.0
