a9fda594e52885042f7d0b39ff98d8b089e72ca2
Karthik Bhat <karthik.ax.ax@gmail.com>
2021-09-13 13:17:44 +0530

    Add track script enqueue

--- projects/plugins/boost/app/admin/class-admin.php
+++ projects/plugins/boost/app/admin/class-admin.php
@@ -9,6 +9,7 @@
 namespace Automattic\Jetpack_Boost\Admin;
 
 use Automattic\Jetpack\Status;
+use Automattic\Jetpack\Tracking;
 use Automattic\Jetpack_Boost\Jetpack_Boost;
 use Automattic\Jetpack_Boost\Lib\Environment_Change_Detector;
 use Automattic\Jetpack_Boost\Lib\Speed_Score;
@@ -55,6 +56,7 @@ class Admin {
 		Environment_Change_Detector::init();
 
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+		add_action( 'admin_enqueue_scripts', array( new Tracking( 'jetpack-boost' ), 'enqueue_tracks_scripts' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_styles' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 		add_filter( 'plugin_action_links_' . JETPACK_BOOST_PLUGIN_BASE, array( $this, 'plugin_page_settings_link' ) );
