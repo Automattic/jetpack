59034bc6f8ac61a8ed421b963388b22ed1825ccc
retrofox <rdsuarez@gmail.com>
2021-09-13 09:44:15 -0300

    extensions: minor jsdoc improvement

--- projects/plugins/jetpack/extensions/plugins/index.js
+++ projects/plugins/jetpack/extensions/plugins/index.js
@@ -11,7 +11,10 @@ import { name as sharingPluginName, settings as sharingPluginSettings } from './
 
 /*
  * Register plugins.
- * Order matters: https://github.com/Automattic/jetpack/issues/21036
+ * Order matters. The plugins will be registered in the order they are listed here,
+ * impacting the order in which they are rendered in the UI.
+ *
+ * https://github.com/Automattic/jetpack/issues/21036
  */
 registerJetpackPlugin( publicizePluginName, publicizePluginSettings );
 registerJetpackPlugin( sharingPluginName, sharingPluginSettings );
