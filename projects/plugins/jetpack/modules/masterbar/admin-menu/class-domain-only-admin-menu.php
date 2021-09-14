3ae08e4893079f580601bec084d0f292df6402a4
cpap <papazoglou.charalampos@gmail.com>
2021-09-14 17:26:49 +0300

    Fix comment to read Domain_Only_Admin_Menu.

--- projects/plugins/jetpack/modules/masterbar/admin-menu/class-domain-only-admin-menu.php
+++ projects/plugins/jetpack/modules/masterbar/admin-menu/class-domain-only-admin-menu.php
@@ -26,7 +26,7 @@ class Domain_Only_Admin_Menu extends Base_Admin_Menu {
 		add_menu_page( esc_attr__( 'Settings', 'jetpack' ), __( 'Settings', 'jetpack' ), 'manage_options', 'https://wordpress.com/domains/manage/' . $this->domain, null, 'dashicons-admin-settings' );
 
 		/**
-		 * Action fired after the Admin_Menu is registered,
+		 * Action fired after the Domain_Only_Admin_Menu is registered,
 		 * which allows for further manipulation.
 		 *
 		 * @since-jetpack 10.2
