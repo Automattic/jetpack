3a9790d696506234549738926809b5ae88df3299
Adrian Moldovan <adim.moldovan@gmail.com>
2021-09-14 17:03:08 +0300

    Do not generate app password for non-local sites

--- projects/plugins/jetpack/tests/e2e/bin/env.sh
+++ projects/plugins/jetpack/tests/e2e/bin/env.sh
@@ -51,8 +51,10 @@ configure_wp_env() {
 }
 
 configure_app_password() {
-	OUTPUT=$($BASE_CMD wp eval 'print_r(WP_Application_Passwords::create_new_application_password(1,array("name"=>"e2e-".rand()))[0]);')
-	export API_PASSWORD="${OUTPUT##*php}"
+	if [[ -z "$TEST_SITE" ]]; then
+		OUTPUT=$($BASE_CMD wp eval 'print_r(WP_Application_Passwords::create_new_application_password(1,array("name"=>"e2e-".rand()))[0]);')
+		export API_PASSWORD="${OUTPUT##*php}"
+	fi
 }
 
 if [ "${1}" == "start" ]; then
