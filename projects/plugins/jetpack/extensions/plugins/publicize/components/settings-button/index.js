14bbef329f144856f2a0fe098204ee50b5271828
retrofox <rdsuarez@gmail.com>
2021-09-14 17:06:15 -0300

    [not verified] publicize: tidy settings button cmp

--- projects/plugins/jetpack/extensions/plugins/publicize/components/settings-button/index.js
+++ projects/plugins/jetpack/extensions/plugins/publicize/components/settings-button/index.js
@@ -18,18 +18,19 @@ import { ExternalLink } from '@wordpress/components';
  */
 import getSiteFragment from '../../../../shared/get-site-fragment';
 
-export default function PublicizeSettingsButton( props ) {
-	function getButtonLink() {
-		const siteFragment = getSiteFragment();
+export default function PublicizeSettingsButton( { refreshCallback, className } ) {
+	const siteFragment = getSiteFragment();
 
-		// If running in WP.com wp-admin or in Calypso, we redirect to Calypso sharing settings.
-		if ( siteFragment ) {
-			return `https://wordpress.com/marketing/connections/${ siteFragment }`;
-		}
-
-		// If running in WordPress.org wp-admin we redirect to Sharing settings in wp-admin.
-		return 'options-general.php?page=sharing&publicize_popup=true';
-	}
+	/*
+	 * If running in WP.com wp-admin or in Calypso,
+	 * we redirect to Calypso sharing settings.
+	 *
+	 * If running in WordPress.org wp-admin,
+	 * we redirect to Sharing settings in wp-admin.
+	 */
+	const href = siteFragment
+		? `https://wordpress.com/marketing/connections/${ siteFragment }`
+		: 'options-general.php?page=sharing&publicize_popup=true';
 
 	/**
 	 * Opens up popup so user can view/modify connections
@@ -42,13 +43,11 @@ export default function PublicizeSettingsButton( props ) {
 			return;
 		}
 
-		const href = event.target.href;
-		const { refreshCallback } = props;
 		/**
 		 * Open a popup window, and
 		 * when it is closed, refresh connections
 		 */
-		const popupWin = window.open( href, '', '' );
+		const popupWin = window.open( event.target.href, '', '' );
 		const popupTimer = window.setInterval( () => {
 			if ( false !== popupWin.closed ) {
 				window.clearInterval( popupTimer );
@@ -57,11 +56,9 @@ export default function PublicizeSettingsButton( props ) {
 		}, 500 );
 	}
 
-	const className = classnames( 'jetpack-publicize-add-connection-container', props.className );
-
 	return (
-		<div className={ className }>
-			<ExternalLink href={ getButtonLink() } onClick={ settingsClick }>
+		<div className={ classnames( 'jetpack-publicize-add-connection-container', className ) }>
+			<ExternalLink href={ href } onClick={ settingsClick }>
 				{ __( 'Connect an account', 'jetpack' ) }
 			</ExternalLink>
 		</div>
