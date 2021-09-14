bfca0e0bc3fb24103b2c0486a97ecc7b1276d047
cpap <papazoglou.charalampos@gmail.com>
2021-09-14 17:26:10 +0300

    Adds action to Domain_Only_Admin_Menu also, for consistency.

--- projects/plugins/jetpack/modules/masterbar/admin-menu/class-domain-only-admin-menu.php
+++ projects/plugins/jetpack/modules/masterbar/admin-menu/class-domain-only-admin-menu.php
@@ -24,5 +24,13 @@ class Domain_Only_Admin_Menu extends Base_Admin_Menu {
 		$submenu = array(); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
 
 		add_menu_page( esc_attr__( 'Settings', 'jetpack' ), __( 'Settings', 'jetpack' ), 'manage_options', 'https://wordpress.com/domains/manage/' . $this->domain, null, 'dashicons-admin-settings' );
+
+		/**
+		 * Action fired after the Admin_Menu is registered,
+		 * which allows for further manipulation.
+		 *
+		 * @since-jetpack 10.2
+		 */
+		do_action( 'after_admin_menu_registered', $this );
 	}
 }
