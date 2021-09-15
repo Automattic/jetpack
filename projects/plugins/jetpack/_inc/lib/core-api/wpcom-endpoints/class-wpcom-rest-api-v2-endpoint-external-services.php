4b126b87cf4cc08dc6b1665663feb1b2876c420e
DustyReagan <dusty@dustyreagan.com>
2021-09-15 13:35:02 -0500

    [not verified] Removed "running on WPCOM" logic. Added permissions_check. Added detailed comments.

--- projects/plugins/jetpack/_inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-external-services.php
+++ projects/plugins/jetpack/_inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-external-services.php
@@ -1,63 +1,85 @@
 <?php
 /**
  * REST API endpoint for External Services.
+ * This endpoint is used by the Publicize feature to avoid client-side CORS issues.
  *
  * @package automattic/jetpack
- * @since
+ * @since 10.1
  */
 
 use Automattic\Jetpack\Connection\Client;
 
 /**
- * External Services proxy endpoint.
+ * This class creates the Jetpack endpoint: http://{$site}/wp-json/wpcom/v2/external-services
+ * It is a proxy of the WPCOM endpoint: https://public-api.wordpress.com/wpcom/v2/sites/{$site}/external-services
+ * This give us the same data found in the WPCOM API, but returned on our customized domain.
+ * Below we simply register the Jetpack route, call the WPCOM endpoint, and return the response body.
  *
- * @since
+ * @since 10.1
  */
 class WPCOM_REST_API_V2_Endpoint_External_Services extends WP_REST_Controller {
 	/**
-	 * Constructor.
+	 * The constructor sets the route namespace and rest_base.
+	 * It also adds the action that registers our API endpoint route.
 	 */
 	public function __construct() {
 		$this->namespace = 'wpcom/v2';
 		$this->rest_base = 'external-services';
-		$this->is_wpcom  = false;
 
-		if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
-			$this->is_wpcom = true;
-
-			if ( ! class_exists( 'WPCOM_External_Connections' ) ) {
-				\jetpack_require_lib( 'external-connections' );
-			}
-		}
-
-		add_action( 'rest_api_init', array( $this, 'register_routes' ) );
+		add_action( 'rest_api_init', array( $this, 'register_route' ) );
 	}
 
 	/**
-	 * Register the route.
+	 * This is the function we reference in the add_action callback in the constructor.
+	 * It registers the endpoint route: http://{$site}/wp-json/wpcom/v2/external-services
 	 */
-	public function register_routes() {
+	public function register_route() {
 		register_rest_route(
 			$this->namespace,
 			$this->rest_base,
 			array(
 				'methods'             => WP_REST_Server::READABLE,
 				'callback'            => array( $this, 'get_external_services' ),
-				'permission_callback' => '__return_true',
+				'permission_callback' => array( $this, 'permissions_check' ),
 			)
 		);
 	}
 
 	/**
-	 * Get External Services.
+	 * This is the function we reference in the register_rest_route permission_callback in the register_route function.
+	 * It returns true if the user has access to the Publicize feature or WP_Error if not.
+	 *
+	 * @return true|WP_Error
+	 */
+	public function permissions_check() {
+		global $publicize;
+
+		if ( ! $publicize ) {
+			return new WP_Error(
+				'publicize_not_available',
+				__( 'Sorry, Publicize is not available on your site right now.', 'jetpack' ),
+				array( 'status' => rest_authorization_required_code() )
+			);
+		}
+
+		if ( $publicize->current_user_can_access_publicize_data() ) {
+			return true;
+		}
+
+		return new WP_Error(
+			'invalid_user_permission_publicize',
+			__( 'Sorry, you are not allowed to access Publicize data on this site.', 'jetpack' ),
+			array( 'status' => rest_authorization_required_code() )
+		);
+	}
+
+	/**
+	 * This method gets the current site ID, then calls a GET request to /wpcom/v2/sites/{$site}/external-services
+	 * as the user, and finally returns the response body.
 	 *
 	 * @return mixed
 	 */
 	public function get_external_services() {
-		if ( $this->is_wpcom ) {
-			return WPCOM_External_Connections::get_external_services_list();
-		}
-
 		$site_id = self::get_site_id();
 		if ( is_wp_error( $site_id ) ) {
 			return $site_id;
@@ -82,13 +104,12 @@ class WPCOM_REST_API_V2_Endpoint_External_Services extends WP_REST_Controller {
 	}
 
 	/**
-	 * Get the WPCOM or self-hosted site ID.
+	 * Gets the Jetpack site_id and returns it as an int. If the site_id is not found it returns a WP_Error.
 	 *
-	 * @return mixed
+	 * @return int|WP_Error
 	 */
 	private static function get_site_id() {
-		$is_wpcom = ( defined( 'IS_WPCOM' ) && IS_WPCOM );
-		$site_id  = $is_wpcom ? get_current_blog_id() : Jetpack_Options::get_option( 'id' );
+		$site_id = Jetpack_Options::get_option( 'id' );
 		if ( ! $site_id ) {
 			return new WP_Error(
 				'unavailable_site_id',
