96e81689aea9fa214692e25d63b7ef785f8e4035
Adrian Moldovan <adim.moldovan@gmail.com>
2021-09-15 15:25:28 +0300

    Syntax changes to make it work in dash

--- projects/plugins/jetpack/tests/e2e/bin/app-password.sh
+++ projects/plugins/jetpack/tests/e2e/bin/app-password.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [[ -z "$TEST_SITE" ]]; then
-	OUTPUT=$($BASE_CMD wp eval 'print_r(WP_Application_Passwords::create_new_application_password(1,array("name"=>"e2e-".rand()))[0]);')
+if [ -z "$TEST_SITE" ]; then
+	OUTPUT=$(pnpx jetpack docker --type e2e --name t1 wp eval 'print_r(WP_Application_Passwords::create_new_application_password(1,array("name"=>"e2e-".rand()))[0]);')
 	export API_PASSWORD="${OUTPUT##*php}"
 fi
