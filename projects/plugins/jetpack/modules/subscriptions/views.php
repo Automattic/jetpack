c49bc6328c1edee688e7bfb42339ffecdd56df97
Andres Blanco <andresblanco@gmail.com>
2021-09-15 15:52:31 -0700

    Fix scroll on wpcom
    On wpcom the form is not rendered when there's a message. This
    commit makes sure the url fragment is added to the message so the
    viewport scrolls.

--- projects/plugins/jetpack/modules/subscriptions/views.php
+++ projects/plugins/jetpack/modules/subscriptions/views.php
@@ -238,12 +238,22 @@ class Jetpack_Subscriptions_Widget extends WP_Widget {
 			}
 
 			$border_color = isset( $themecolors['border'] ) ? " #{$themecolors['border']}" : '';
+
+			$redirect_fragment = self::get_wpcom_redirect_fragment();
 			printf(
-				'<div class="jetpack-sub-notification" style="border: 1px solid%1$s; padding-left: 5px; padding-right: 5px; margin-bottom: 10px;">%2$s</div>',
+				'<div id="%1$s" class="jetpack-sub-notification" style="border: 1px solid%2$s; padding-left: 5px; padding-right: 5px; margin-bottom: 10px;">%3$s</div>',
+				esc_attr( $redirect_fragment ),
 				esc_attr( $border_color ),
 				wp_kses_post( $message )
 			);
 		}
+	}
+
+	/**
+	 * Generates the redirect fragment used after form submission.
+	 */
+	protected static function get_wpcom_redirect_fragment() {
+		return 'subscribe-blog' . ( self::$instance_count > 1 ? '-' . self::$instance_count : '' );
 	}
 
 	/**
@@ -275,10 +285,9 @@ class Jetpack_Subscriptions_Widget extends WP_Widget {
 			global $current_blog;
 
 			$url     = defined( 'SUBSCRIBE_BLOG_URL' ) ? SUBSCRIBE_BLOG_URL : '';
-			$form_id = 'subscribe-blog' . self::$instance_count > 1
-				? '-' . self::$instance_count
-				: '';
+			$form_id = self::get_wpcom_redirect_fragment();
 			?>
+
 			<form
 				action="<?php echo esc_url( $url ); ?>"
 				method="post"
