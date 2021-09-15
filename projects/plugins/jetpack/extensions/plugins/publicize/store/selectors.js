f3c313faef1339c282387c7baa1b362003863260
Allison Levine <allison.levine@automattic.com>
2021-09-15 11:24:19 -0400

    [not verified] Rebase with master

--- projects/plugins/jetpack/extensions/plugins/publicize/store/selectors.js
+++ projects/plugins/jetpack/extensions/plugins/publicize/store/selectors.js
@@ -279,6 +279,16 @@ export function getShareMessage() {
 }
 
 /**
+ * Get the connections
+ *
+ * @param {object} state - State object.
+ * @returns {Array} The connections.
+ */
+export function getConnections( state ) {
+	return state.connections;
+}
+
+/**
  * Get the maximum length that a share message can be.
  *
  * @returns {number} The maximum length of a share message.
