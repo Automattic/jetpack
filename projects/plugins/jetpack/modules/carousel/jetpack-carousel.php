360fc32cfd6fcf0c0c4122bf1c1932b94d8b7e58
Glen Davies <glen.davies@a8c.com>
2021-09-22 17:43:29 +1200

    fix linting errors

--- projects/plugins/jetpack/modules/carousel/jetpack-carousel.php
+++ projects/plugins/jetpack/modules/carousel/jetpack-carousel.php
@@ -609,7 +609,7 @@ class Jetpack_Carousel {
 				 * The same image tag may be used more than once but with different attribs,
 				 * so save each of them against the attachment id.
 				 */
-				if ( ! isset( $selected_images[ $attachment_id ] ) || ! in_array ( $image_html, $selected_images[ $attachment_id ] ) ) {
+				if ( ! isset( $selected_images[ $attachment_id ] ) || ! in_array( $image_html, $selected_images[ $attachment_id ], true ) ) {
 					$selected_images[ $attachment_id ][] = $image_html;
 				}
 			}
