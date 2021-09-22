22ccb08aa77ba40029b820900f7f7a8891b4b0a8
Dusty Reagan <dusty@dustyreagan.com>
2021-09-22 11:43:56 -0500

    Refactor post list thumbnail preview to function server side. All javascript removed. (#21126)
    * Reworking post-list thumbnails to be server side.
    
    * Added changelog file.
    
    * Added CSS and placeholder image for empty thumbnails.
    
    * CSS fix placeholder disappearing.
    
    * Updated unit tests. Added alt tag to thumbnail image.
    
    * Updated comments.
    
    * Added rtl.css.
    
    * Don't add thumbnail column if 'title' is missing for some reason.
    
    * Add thumbnails to "Pages" admin table. Removed unused class name from thumbnail img.
    
    * Created add_thumbnail_filters_and_actions and moved column filter and action out of enqueue_scripts and into current_screen.
    
    * Removed unused in_admin_footer action.

--- projects/packages/post-list/src/class-post-list.php
+++ projects/packages/post-list/src/class-post-list.php
@@ -7,8 +7,6 @@
 
 namespace Automattic\Jetpack\Post_List;
 
-use Automattic\Jetpack\Assets;
-
 /**
  * The Post_List Admin Area
  */
@@ -34,13 +32,12 @@ class Post_List {
 	}
 
 	/**
-	 * Sets up Post List action callbacks if needed.
+	 * Sets up Post List action callbacks.
 	 */
 	public function register() {
 		if ( ! did_action( 'jetpack_on_posts_list_init' ) ) {
 			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
-			add_action( 'in_admin_footer', array( $this, 'create_app_root_element' ) );
-
+			add_action( 'current_screen', array( $this, 'add_thumbnail_filters_and_actions' ) );
 			add_filter( 'default_hidden_columns', array( $this, 'adjust_default_columns' ), 10, 2 );
 
 			/**
@@ -53,72 +50,80 @@ class Post_List {
 	}
 
 	/**
-	 * Enqueue scripts depending on the post-list query var.
+	 * Enqueue scripts.
 	 *
 	 * @param string $hook Page hook.
 	 */
 	public function enqueue_scripts( $hook ) {
 		if ( 'edit.php' === $hook ) {
-			$plugin_path = Assets::get_file_url_for_environment( './index.js', './index.js', __FILE__ );
-
-			wp_enqueue_script(
-				'jetpack_posts_list_ui_script',
-				$plugin_path,
-				array(),
-				self::PACKAGE_VERSION,
-				true
-			);
-
 			wp_enqueue_style(
 				'jetpack_posts_list_ui_style',
 				plugin_dir_url( __DIR__ ) . './src/style.css',
 				array(),
 				self::PACKAGE_VERSION
 			);
-
 			wp_style_add_data(
 				'jetpack_posts_list_ui_style',
 				'rtl',
-				plugin_dir_url( __DIR__ ) . 'build/style.rtl.css'
+				plugin_dir_url( __DIR__ ) . './src/rtl.css'
 			);
-
-			wp_set_script_translations( 'jetpack_posts_list_ui_script', 'jetpack' );
-
-			add_action( 'admin_footer', array( $this, 'print_post_data' ) );
 		}
 	}
 
 	/**
-	 * Outputs a JSON blob to the global `wp_admin_posts` variable, for use
-	 * by the JS application
+	 * If the current_screen has 'edit' as the base, add filters and actions to add the thumbnail column to the Posts
+	 * and Pages admin tables.
+	 *
+	 * @param object $current_screen The current screen.
 	 */
-	public function print_post_data() {
-		global $wp_query;
+	public function add_thumbnail_filters_and_actions( $current_screen ) {
+		if ( 'edit' === $current_screen->base ) {
+			// Add the thumbnail column to the "Posts" admin table.
+			add_filter( 'manage_posts_columns', array( $this, 'add_thumbnail_column' ) );
+			add_action( 'manage_posts_custom_column', array( $this, 'populate_thumbnail_rows' ), 10, 2 );
 
-		if ( ! post_type_supports( $wp_query->query['post_type'], 'thumbnail' ) ) {
+			// Add the thumbnail column to the "Pages" admin table.
+			add_filter( 'manage_pages_columns', array( $this, 'add_thumbnail_column' ) );
+			add_action( 'manage_pages_custom_column', array( $this, 'populate_thumbnail_rows' ), 10, 2 );
+		}
+	}
+
+	/**
+	 * Adds a new column header for displaying the thumbnail of a post.
+	 *
+	 * @param array $columns An array of column names.
+	 * @return array An array of column names.
+	 */
+	public function add_thumbnail_column( $columns ) {
+		$new_column = array( 'thumbnail' => '<span>' . __( 'Thumbnail', 'jetpack' ) . '</span>' );
+		$keys       = array_keys( $columns );
+		$position   = array_search( 'title', $keys, true );
+
+		// If 'title' not found, don't insert the thumbnail column.
+		if ( false !== $position ) {
+			$columns = array_merge( array_slice( $columns, 0, $position ), $new_column, array_slice( $columns, $position ) );
+		}
+
+		return $columns;
+	}
+
+	/**
+	 * Displays the thumbnail content.
+	 *
+	 * @param string $column  The name of the column to display.
+	 * @param int    $post_id The current post ID.
+	 */
+	public function populate_thumbnail_rows( $column, $post_id ) {
+		if ( 'thumbnail' !== $column ) {
 			return;
 		}
 
-		$post_data = array_map(
-			function ( $post ) {
-				$thumbnail = Post_Thumbnail::get_post_thumbnail( $post );
-				return array(
-					'id'             => $post->ID,
-					'type'           => $post->post_type,
-					'featured_image' => $thumbnail,
-				);
-			},
-			$wp_query->posts
-		);
-		wp_add_inline_script( 'jetpack_posts_list_ui_script', 'var wpAdminPosts = ' . wp_json_encode( $post_data ) );
-	}
-
-	/**
-	 * Add a placeholder element for the
-	 * to mount the client app (root element).
-	 */
-	public function create_app_root_element() {
-		echo '<div id="wp-post-list-app" style="display: none;"></div>';
+		$thumbnail = Post_Thumbnail::get_post_thumbnail( get_post( $post_id ) );
+		if ( $thumbnail ) {
+			echo '<img src="' . esc_url( $thumbnail['thumb'] ) . '" alt="' . esc_attr( $thumbnail['alt'] ) . '" height="50" width="50" />';
+		} else {
+			echo '<span class="dashicons dashicons-format-image" title="No thumbnail found."></span>';
+		}
 	}
 
 	/**
