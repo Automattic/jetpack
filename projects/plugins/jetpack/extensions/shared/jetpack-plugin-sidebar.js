182ccf69e0c2c82b5f5a8125a8418470418e1136
Allison Levine <allison.levine@automattic.com>
2021-09-22 13:48:13 -0400

    [not verified] Open Jetpack plugin sidebar by default when URL includes query param.

--- projects/plugins/jetpack/extensions/shared/jetpack-plugin-sidebar.js
+++ projects/plugins/jetpack/extensions/shared/jetpack-plugin-sidebar.js
@@ -5,6 +5,7 @@ import { createSlotFill } from '@wordpress/components';
 import { Fragment } from '@wordpress/element';
 import { PluginSidebar, PluginSidebarMoreMenuItem } from '@wordpress/edit-post';
 import { registerPlugin } from '@wordpress/plugins';
+import { dispatch } from '@wordpress/data';
 
 /**
  * Internal dependencies
@@ -15,6 +16,19 @@ import { JetpackLogo } from './icons';
 const { Fill, Slot } = createSlotFill( 'JetpackPluginSidebar' );
 
 export { Fill as default };
+
+/**
+ * Open Jetpack plugin sidebar by default when URL includes query param.
+ */
+function openJetpackSidebar() {
+	dispatch( 'core/interface' ).enableComplementaryArea(
+		'core/edit-post',
+		'jetpack-sidebar/jetpack'
+	);
+}
+const queryParams = new URLSearchParams( window.location.search );
+const jetpackSidebarIsOpen = queryParams.get( 'jetpackSidebarIsOpen' );
+jetpackSidebarIsOpen && openJetpackSidebar();
 
 registerPlugin( 'jetpack-sidebar', {
 	render: () => (
