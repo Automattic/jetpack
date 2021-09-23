4684c69400e6f922551cf48074a26bf913c0952d
Damián Suárez <rdsuarez@gmail.com>
2021-09-23 13:35:29 -0300

    Publicize: fix refreshing social media connections (#21107)
    * [not verified] publicize: replace native textarea with MessageBoxControl
    
    * [not verified] publicize: set href attr in settings button
    
    * [not verified] publicize: tidy settings button cmp
    
    * [not verified] publicize: improve refreshing hook methods
    
    * [not verified] publicize: refresh connections via hooks
    
    * [not verified] publicize: simplify implementation
    
    * [not verified] publicize: save the post when refresh connections
    
    * [not verified] publicize: do not change store when refreshing connections
    
    * [not verified] publicize: add getConnections() selector
    
    * [not verified] publicize: improve getConnections() selector
    
    * [not verified] publicize: remove settings button from panel
    
    * [not verified] publicize: add react-page-visibility dependency
    
    * [not verified] publicize: refresh connections via visibility
    
    * [not verified] changelog
    
    * [not verified] publicize: define refresh only when cmp mounted
    
    * [not verified] publicize: show settings button properly
    
    * [not verified] publicize: populate getConnections() selector with profile picture
    
    * [not verified] publicize: refresh when no connections too
    
    * [not verified] publicize: adjust after rebasing
    
    * [not verified] pkg: add react-page-visibility dep
    
    * [not verified] publicize: rename debounced function
    
    * [not verified] publicize: do not save post to refresh connections
    
    * [not verified] process connections list in the reducer
    
    * [not verified] check profile_picture properly
    
    * [not verified] refresh connections when toggleById
    
    * [not verified] process the connections list at effects level
    
    * [not verified] add actions to toggle connection enable state
    
    * [not verified] expose toggleById in the connections hook
    
    * [not verified] refresh connections when settings button mounted
    
    * [not verified] publicize: do not refresh when mounted
    
    * [not verified] remove unused dispaching prop
    
    * [not verified] clean piblicize Panel
    
    * [not verified] add usePostJustSaved() hook
    
    * [not verified] add hasEnabledConnections to connections hook
    
    * [not verified] refresh connections when post is just saved

--- projects/plugins/jetpack/extensions/plugins/publicize/hooks/use-social-media-connections/index.js
+++ projects/plugins/jetpack/extensions/plugins/publicize/hooks/use-social-media-connections/index.js
@@ -2,32 +2,24 @@
  * WordPress dependencies
  */
 import { useDispatch, useSelect } from '@wordpress/data';
-import { store as editorStore } from '@wordpress/editor';
 
 /**
  * Hooks to deal with the social media connections.
  *
  * @returns {Function} Social media connection handler.
  */
-export default function useSocialMediaConnections() {
-	const { editPost } = useDispatch( editorStore );
 
-	const connections = useSelect(
-		select => select( editorStore ).getEditedPostAttribute( 'jetpack_publicize_connections' ),
-		[]
+export default function useSocialMediaConnections() {
+	const { refreshConnectionTestResults: refresh, toggleConnectionById } = useDispatch(
+		'jetpack/publicize'
 	);
+
+	const connections = useSelect( select => select( 'jetpack/publicize' ).getConnections(), [] );
 
 	return {
 		connections,
-		toggleById: function ( id ) {
-			const filteredConnections = connections.map( connection => ( {
-				...connection,
-				enabled: connection.id === id ? ! connection.enabled : connection.enabled,
-			} ) );
-
-			editPost( {
-				jetpack_publicize_connections: filteredConnections,
-			} );
-		},
+		hasEnabledConnections: connections && connections.some( connection => connection.enabled ),
+		toggleById: toggleConnectionById,
+		refresh,
 	};
 }
