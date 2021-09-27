03f42a63d5e4c65c792e55cce46fa9d0ef2280c2
André Kallehauge <3846700+kallehauge@users.noreply.github.com>
2021-09-27 16:27:24 +0200

    Unify constant code styles and remove commented out code

--- projects/plugins/jetpack/_inc/client/product-descriptions/constants.js
+++ projects/plugins/jetpack/_inc/client/product-descriptions/constants.js
@@ -18,10 +18,8 @@ export const productDescriptionRoutes = [
 ];
 
 export const productIllustrations = {
-	[ PRODUCT_DESCRIPTION_PRODUCTS.JETPACK_ANTI_SPAM ]:
-		imagePath + 'products/illustration-anti-spam.png',
-	[ PRODUCT_DESCRIPTION_PRODUCTS.JETPACK_BACKUP_DAILY ]:
-		imagePath + 'products/illustration-backup.png',
-	[ PRODUCT_DESCRIPTION_PRODUCTS.JETPACK_SCAN ]: imagePath + 'products/illustration-scan.png',
-	[ PRODUCT_DESCRIPTION_PRODUCTS.JETPACK_SEARCH ]: imagePath + 'products/illustration-search.png',
+	[ PRODUCT_DESCRIPTION_PRODUCTS.JETPACK_ANTI_SPAM ]: `${ imagePath }products/illustration-anti-spam.png`,
+	[ PRODUCT_DESCRIPTION_PRODUCTS.JETPACK_BACKUP_DAILY ]: `${ imagePath }products/illustration-backup.png`,
+	[ PRODUCT_DESCRIPTION_PRODUCTS.JETPACK_SCAN ]: `${ imagePath }products/illustration-scan.png`,
+	[ PRODUCT_DESCRIPTION_PRODUCTS.JETPACK_SEARCH ]: `${ imagePath }products/illustration-search.png`,
 };
