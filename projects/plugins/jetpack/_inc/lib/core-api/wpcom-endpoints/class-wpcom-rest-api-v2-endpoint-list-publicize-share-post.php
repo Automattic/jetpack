d06743939e62f3e72dbc8409a1c6b826bc1e126f
DustyReagan <dusty@dustyreagan.com>
2021-09-29 18:26:52 -0500

    [not verified] Setup to work inside WPCOM or Jetpack.

--- projects/plugins/jetpack/_inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-list-publicize-share-post.php
+++ projects/plugins/jetpack/_inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-list-publicize-share-post.php
@@ -5,7 +5,7 @@
  * Share the post.
  *
  * @package automattic/jetpack
- * @since 6.8
+ * @since
  */
 
 use Automattic\Jetpack\Connection\Client;
@@ -15,96 +15,153 @@ require_once __DIR__ . '/publicize-connections.php';
 /**
  * Publicize Share post class.
  */
-class WPCOM_REST_API_V2_Endpoint_List_Publicize_Share_Post extends WPCOM_REST_API_V2_Endpoint_List_Publicize_Connections {
+class WPCOM_REST_API_V2_Endpoint_List_Publicize_Share_Post extends WP_REST_Controller {
 	/**
-	 * Constructor.
+	 * The constructor sets the route namespace and rest_base.
+	 * It also adds the action that registers our API endpoint route.
 	 */
 	public function __construct() {
 		$this->namespace = 'wpcom/v2';
 		$this->rest_base = 'publicize/share';
+		$this->is_wpcom  = false;
+
+		if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
+			$this->is_wpcom = true;
+		}
 
 		add_action( 'rest_api_init', array( $this, 'register_routes' ) );
 	}
 
 	/**
-	 * Called automatically on `rest_api_init()`.
+	 * This is the function we reference in the add_action callback in the constructor.
+	 * It registers the endpoint route: http://{$site}/wp-json/wpcom/v2/publicize/share/{$postId}
 	 */
 	public function register_routes() {
 		register_rest_route(
 			$this->namespace,
 			'/' . $this->rest_base . '/(?P<postId>[\d]+)',
 			array(
-				array(
-					'methods'             => WP_REST_Server::CREATABLE,
-					'callback'            => array( $this, 'share_post' ),
-					'permission_callback' => array( $this, 'get_items_permission_check' ),
+				'methods'             => WP_REST_Server::CREATABLE,
+				'callback'            => array( $this, 'share_post' ),
+				'permission_callback' => array( $this, 'permissions_check' ),
+				'args'                => array(
+					'postId'               => array(
+						'description'       => __( 'The WordPress Post ID.', 'jetpack' ),
+						'type'              => 'int',
+						'required'          => 'true',
+						'validate_callback' => function ( $param ) {
+							return is_numeric( $param );
+						},
+					),
+					'message'              => array(
+						'description'       => __( 'The message to share.', 'jetpack' ),
+						'type'              => 'string',
+						'required'          => 'true',
+						'validate_callback' => function ( $param ) {
+							return is_string( $param );
+						},
+						'sanitize_callback' => 'sanitize_textarea_field',
+					),
+					'skippedConnectionIds' => array(
+						'description'       => __( 'Array of external connection IDs to skip sharing.', 'jetpack' ),
+						'type'              => 'array',
+						'required'          => 'true',
+						'validate_callback' => function ( $param ) {
+							return is_array( $param );
+						},
+						'sanitize_callback' => function ( $param ) {
+							return array_map( 'absint', $param );
+						},
+					),
 				),
-				'schema' => array( $this, 'get_item_schema' ),
 			)
 		);
 	}
 
 	/**
-	 * Adds the test results properties to the Connection schema.
+	 * Ensure the user has proper permissions.
 	 *
-	 * @return array
+	 * @return WP_Error|boolean
 	 */
-	public function get_item_schema() {
-		$schema = array(
-			'$schema' => 'http://json-schema.org/draft-04/schema#',
-			'title'   => 'jetpack-publicize-share-post',
-			'type'    => 'object',
-		);
-
-		return $this->add_additional_fields_schema( $schema );
+	public function permissions_check() {
+		if ( ! get_current_user_id() ) {
+			return new WP_Error(
+				'rest_cannot_view',
+				__( 'Sorry, you cannot view this resource without a valid token for this blog.', 'jetpack' ),
+				array( 'status' => rest_authorization_required_code() )
+			);
+		}
+		if ( ! current_user_can( 'publish_posts' ) ) {
+			return new WP_Error( 'unauthorized', 'Your token must have permission to publish posts.', array( 'status' => 401 ) );
+		}
+		return true;
 	}
 
 	/**
-	 * Share the post hitting the publicize wpcom endpoint.
+	 * Share a post
 	 *
-	 * @param  WP_REST_Request $request The request.
-	 * @return WP_REST_Response          share post response
+	 * @param  WP_REST_Request $request Full details about the request.
+	 * @return WP_Error|array The publicize results, including two arrays: `results` and `errors`
 	 */
 	public function share_post( $request ) {
 		$post_id             = $request->get_param( 'postId' );
-		$skipped_connections = $request->get_param( 'skippedConnections' );
-		$message             = $request->get_param( 'message' );
+		$message             = trim( $request->get_param( 'message' ) );
+		$skipped_connections = $request->get_param( 'skipped_connections' );
 
-		/*
-		 * Publicize endpoint
-		 * [POST] wpcom/v2/sites/<site-id>/posts/<post-id>/publicize
-		 * body:
-		 *   - message: string
-		 *   - skipped_connections: array of connection ids to skip
-		 */
-		$url = sprintf(
-			'/sites/%d/posts/%d/publicize',
-			Jetpack_Options::get_option( 'id' ),
-			$post_id
-		);
+		if ( $this->is_wpcom ) {
+			$post = get_post( $post_id );
 
-		$response = Client::wpcom_json_api_request_as_user(
-			$url,
-			'v2',
-			array(
-				'method'  => 'POST',
-				'headers' => array(
-					'Content-Type'    => 'application/json',
-					'X-Forwarded-For' => Jetpack::current_user_ip( true ),
+			if ( empty( $post ) ) {
+				return new WP_Error( 'not_found', 'Cannot find that post', array( 'status' => 404 ) );
+			}
+			if ( 'publish' !== $post->post_status ) {
+				return new WP_Error( 'not_published', 'Cannot publicize an unpublished post', array( 'status' => 400 ) );
+			}
+
+			$publicize = publicize_init();
+			$result    = $publicize->republicize_post( (int) $post_id, $message, $skipped_connections );
+			if ( false === $result ) {
+				return new WP_Error( 'not_found', 'Cannot find that post', array( 'status' => 404 ) );
+			}
+
+			return $result;
+		} else {
+			/*
+			 * Publicize endpoint
+			 * [POST] wpcom/v2/sites/<site-id>/posts/<post-id>/share
+			 * body:
+			 *   - message: string
+			 *   - skipped_connections: array of connection ids to skip
+			 */
+			$url = sprintf(
+				'/sites/%d/publicize/share/%d',
+				Jetpack_Options::get_option( 'id' ),
+				$post_id
+			);
+
+			$response = Client::wpcom_json_api_request_as_user(
+				$url,
+				'v2',
+				array(
+					'method'  => 'POST',
+					'headers' => array(
+						'Content-Type'    => 'application/json',
+						'X-Forwarded-For' => Jetpack::current_user_ip( true ),
+					),
 				),
-			),
-			array(
-				'message'             => $message,
-				'skipped_connections' => $skipped_connections,
-			)
-		);
+				array(
+					'message'             => $message,
+					'skipped_connections' => $skipped_connections,
+				)
+			);
 
-		if ( is_wp_error( $response ) ) {
-			return rest_ensure_response( $response );
+			if ( is_wp_error( $response ) ) {
+				return rest_ensure_response( $response );
+			}
+
+			return json_decode( wp_remote_retrieve_body( $response ), true );
 		}
-
-		return json_decode( wp_remote_retrieve_body( $response ), true );
 	}
 }
 
-wpcom_rest_api_v2_load_plugin( 'WPCOM_REST_API_V2_Endpoint_List_Publicize_Share_Post' );
+wpcom_rest_api_v2_load_plugin( 'WPCOM_REST_API_V2_Endpoint_List_Publicize_Connections' );
