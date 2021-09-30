e890cc63300426cbda786e6dbb67083e5eebe719
sdixon194 <steve.dixon@automattic.com>
2021-09-30 15:26:25 -0400

    [not verified] add ability to run scripts

--- tools/cli/commands/release.js
+++ tools/cli/commands/release.js
@@ -39,7 +39,7 @@ export function releaseDefine( yargs ) {
 					alias: 's',
 					describe: 'The release script to run',
 					type: 'string',
-                    choices: [ 'changelog', 'readme', 'release-branch', 'amend' ]
+					choices: [ 'changelog', 'readme', 'release-branch', 'amend' ],
 				} )
 				.option( 'beta', {
 					alias: 'b',
@@ -71,40 +71,54 @@ export async function releaseCli( argv ) {
 		argv = await promptForProject( argv );
 	}
 
-    if ( ! argv.beta ){
-        argv = await promptBeta( argv );
-    }
+	if ( ! argv.beta ) {
+		argv = await promptBeta( argv );
+	}
 
 	if ( ! argv.script || argv.script === '' ) {
 		argv = await promptForScript( argv );
 	}
 
-    scriptRouter( argv );
+	scriptRouter( argv );
+	await runScript( argv );
 	console.log( argv );
+}
+
+/**
+ * Run the script.
+ *
+ * @param {object} argv the arguments passed
+ * @returns argv
+ */
+export async function runScript( argv ) {
+	child_process.execSync( `${ argv.script }` );
 }
 
 /**
  * Determine which script to run.
  *
- * @param {object} argv the arguments passed 
- * @returns argv 
+ * @param {object} argv the arguments passed
+ * @returns argv
  */
- export async function scriptRouter( argv ) {
-     switch ( argv.script ) {
-        case 'changelog':
-            argv.script = 
-        case 'readme':
-        case 'release-branch':
-            console.log( 'Not implemented yet!' );
-            break;
-     }
- 
+export async function scriptRouter( argv ) {
+	const isBeta = argv.beta ? '-b' : '';
+	switch ( argv.script ) {
+		case 'changelog':
+			argv.script = `tools/changelogger-release.sh ${ isBeta } ${ argv.project }`;
+			argv.next = 'Next, review the changes in ';
+			break;
+		case 'readme':
+		case 'release-branch':
+		case 'append':
+			console.log( 'Not implemented yet!' );
+			break;
+	}
 }
 /**
  * Checks the project we're releasing.
  *
- * @param {object} argv the arguments passed 
- * @returns argv 
+ * @param {object} argv the arguments passed
+ * @returns argv
  */
 export async function parseProj( argv ) {
 	// If we're passing a specific project
@@ -124,49 +138,49 @@ export async function parseProj( argv ) {
 /**
  * Checks the project we're releasing.
  *
- * @param {object} argv the arguments passed 
- * @returns argv 
+ * @param {object} argv the arguments passed
+ * @returns argv
  */
- export async function promptBeta( argv ) {
-    const response = await inquirer.prompt( [
+export async function promptBeta( argv ) {
+	const response = await inquirer.prompt( [
 		{
 			type: 'confirm',
 			name: 'beta',
-			message: `Are you releasing a beta version of ${argv.project}`,
-            default: false,
+			message: `Are you releasing a beta version of ${ argv.project }?`,
+			default: false,
 		},
 	] );
 	argv.beta = response.beta;
-    argv.b = response.beta;
+	argv.b = response.beta;
 	return argv;
 }
 
 /**
  * Asks for what part of the release process we want to run.
  *
- * @param {object} argv the arguments passed 
- * @returns argv 
+ * @param {object} argv the arguments passed
+ * @returns argv
  */
- export async function promptForScript( argv ) {
-    const response = await inquirer.prompt( [
+export async function promptForScript( argv ) {
+	const response = await inquirer.prompt( [
 		{
 			type: 'list',
 			name: 'script',
-			message: `What step of the release process are you looking to do for ${argv.project}?`,
+			message: `What step of the release process are you looking to do for ${ argv.project }?`,
 			choices: [
 				{
-					name: `[Create changelog.md  ] - Compile all changelog files into ${argv.project}'s CHANGELOG.md `,
+					name: `[Create changelog.md  ] - Compile all changelog files into ${ argv.project }'s CHANGELOG.md `,
 					value: 'changelog',
 				},
 				{
-					name: `[Update readme.txt    ] - Update ${argv.project}'s readme.txt file based on the updated changelog.`,
+					name: `[Update readme.txt    ] - Update ${ argv.project }'s readme.txt file based on the updated changelog.`,
 					value: 'readme',
 				},
 				{
-					name: `[Create release branch] - Create a release branch for  ${argv.project}`,
+					name: `[Create release branch] - Create a release branch for  ${ argv.project }`,
 					value: 'release-branch',
 				},
-                {
+				{
 					name: `[Amend changelog.md  ] - Updates changelog.md with any files cherry picked to release branch prior to release.`,
 					value: 'amend',
 				},
