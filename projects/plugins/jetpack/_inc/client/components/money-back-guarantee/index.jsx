43de2d98e22c343224ee105a8952216aa4151262
André Kallehauge <3846700+kallehauge@users.noreply.github.com>
2021-10-01 16:16:04 +0200

    Simplify component to only change looks

--- projects/plugins/jetpack/_inc/client/components/money-back-guarantee/index.jsx
+++ projects/plugins/jetpack/_inc/client/components/money-back-guarantee/index.jsx
@@ -1,37 +1,23 @@
 /**
  * External dependencies
  */
-import classNames from 'classnames';
 import React from 'react';
 import PropTypes from 'prop-types';
 
 /**
  * Internal dependencies
  */
-import { solidIcon, outlinedIcon } from './icons';
+import { yearly } from './icons';
 
 /**
  * Style dependencies
  */
 import './style.scss';
 
-const MoneyBackGuarantee = ( { frequency, iconStyle, text } ) => {
-	const classes = classNames(
-		'jetpack-money-back-guarantee',
-		`jetpack-money-back-guarantee--${ iconStyle }`
-	);
-
+const MoneyBackGuarantee = ( { text } ) => {
 	return (
-		<div className={ classes }>
-			<div className="jetpack-money-back-guarantee__icon">
-				{ 'solid' === iconStyle && solidIcon() }
-				{ 'outlined' === iconStyle && outlinedIcon() }
-
-				<span className="jetpack-money-back-guarantee__frequency">
-					{ 'monthly' === frequency && 7 }
-					{ 'yearly' === frequency && 14 }
-				</span>
-			</div>
+		<div className="jetpack-money-back-guarantee">
+			<div className="jetpack-money-back-guarantee__icon">{ yearly }</div>
 
 			<div className="jetpack-money-back-guarantee__text">{ text }</div>
 		</div>
@@ -39,14 +25,7 @@ const MoneyBackGuarantee = ( { frequency, iconStyle, text } ) => {
 };
 
 MoneyBackGuarantee.propTypes = {
-	frequency: PropTypes.string,
-	iconStyle: PropTypes.string,
 	text: PropTypes.string.isRequired,
-};
-
-MoneyBackGuarantee.defaultProps = {
-	frequency: 'yearly',
-	iconStyle: 'solid',
 };
 
 export { MoneyBackGuarantee };
