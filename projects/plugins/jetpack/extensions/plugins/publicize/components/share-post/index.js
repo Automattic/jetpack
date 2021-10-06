d307da85bc82a7d4ef7034a0cbc2120a87080e98
retrofox <rdsuarez@gmail.com>
2021-10-06 06:53:43 -0300

    [not verified] first RePublicize implementation

--- projects/plugins/jetpack/extensions/plugins/publicize/components/share-post/index.js
+++ projects/plugins/jetpack/extensions/plugins/publicize/components/share-post/index.js
@@ -4,6 +4,9 @@
 import { Button, PanelRow } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { useState } from '@wordpress/element';
+import { useDispatch } from '@wordpress/data';
+import { store as noticesStore } from '@wordpress/notices';
+import { store as editorStore } from '@wordpress/editor';
 
 /**
  * Internal dependencies
@@ -12,12 +15,22 @@ import { useSharePost } from '../../hooks/use-share-post';
 import useSocialMediaConnections from '../../hooks/use-social-media-connections';
 
 export function SharePostButton() {
+	const { createErrorNotice, removeNotice, createSuccessNotice } = useDispatch( noticesStore );
+	const { savePost } = useDispatch( editorStore );
 	const [ isSharing, setIsSharing ] = useState( false );
 	const { hasEnabledConnections } = useSocialMediaConnections();
 
-	const onPostShareHander = useSharePost( function ( errors ) {
-		if ( errors?.length ) {
-			// console.error( errors );
+	const onPostShareHander = useSharePost( function ( error, results ) {
+		if ( error ) {
+			createErrorNotice( error.message, {
+				id: 'publicize-post-share-message',
+			} );
+		} else if ( results.shared?.length ) {
+			createSuccessNotice( __( 'Post shared', 'jetpack' ), {
+				id: 'publicize-post-share-message',
+				type: 'snackbar',
+				actions: results.shared.map( ( { url } ) => ( { url, label: 'View' } ) ),
+			} );
 		}
 
 		setIsSharing( false );
@@ -27,7 +40,14 @@ export function SharePostButton() {
 		<Button
 			isSecondary
 			onClick={ function () {
+				savePost();
 				setIsSharing( true );
+				removeNotice( 'publicize-post-share-message' );
+				/*
+				 * @ToDo:
+				 * We need to ensure that the post is saved before sharing,
+				 * using the `usePostJustSaved` hook.
+				 */
 				onPostShareHander();
 			} }
 			disabled={ isSharing || ! hasEnabledConnections }
