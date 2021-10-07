a8ff1b4d6fc65099b9d97145832c6fb2a0d2932a
Daniel <daniel.lopez@automattic.com>
2021-10-07 15:40:01 +0200

    Revert "Emails: Allow wpcomsh users to see the inbox menu https://github.com/Automattic/wpcomsh/pull/756" https://github.com/Automattic/wpcomsh/pull/760
    This reverts commit 95d563b9d8997b999f6360e77d2bd7dbe896abb4.

--- projects/plugins/wpcomsh/wpcomsh.php
+++ projects/plugins/wpcomsh/wpcomsh.php
@@ -2,13 +2,13 @@
 /**
  * Plugin Name: WordPress.com Site Helper
  * Description: A helper for connecting WordPress.com sites to external host infrastructure.
- * Version: 2.8.34
+ * Version: 2.8.33
  * Author: Automattic
  * Author URI: http://automattic.com/
  */
 
 // Increase version number if you change something in wpcomsh.
-define( 'WPCOMSH_VERSION', '2.8.34' );
+define( 'WPCOMSH_VERSION', '2.8.33' );
 
 // If true, Typekit fonts will be available in addition to Google fonts
 add_filter( 'jetpack_fonts_enable_typekit', '__return_true' );
