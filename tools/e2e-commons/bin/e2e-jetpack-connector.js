3440538e040ce4159d9174713e95f49c0ab7c468
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2021-10-08 19:05:22 +0300

    clean up comment

--- tools/e2e-commons/bin/e2e-jetpack-connector.js
+++ tools/e2e-commons/bin/e2e-jetpack-connector.js
@@ -1,13 +1,5 @@
 import { prerequisitesBuilder } from '../env/prerequisites';
 import { resolveSiteUrl } from '../helpers/utils-helper';
 
-/**
- * 1. pnpm tunnel:on
- * 2. pnpm env:start
- * 3. export NODE_ENV=test
- * 4. export NODE_CONFIG_DIR="/Users/brbrr/Developer/a8c/jetpack/projects/plugins/jetpack/tests/e2e/config"
- * 5. pnpm babel-node bin/e2e-jetpack-connector.js
- */
-
 global.siteUrl = resolveSiteUrl();
 prerequisitesBuilder().withConnection( true ).build();
