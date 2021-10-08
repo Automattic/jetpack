9c2b973e5ca3b4db189fc46fd6ff39ff4471dc40
jboland88 <18016357+jboland88@users.noreply.github.com>
2021-10-08 17:50:57 -0400

    [not verified] Update disconnect step with output for connected plugins, add component to show connected plugins and Jetpack benefit information

---
+++ projects/js-packages/connection/components/disconnect-dialog/disconnect-card.jsx
@@ -0,0 +1,35 @@
+/**
+ * External Dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal Dependencies
+ */
+import './_jp-disconnect-card.scss';
+
+const DisconnectCard = props => {
+	const { title, value, description } = props;
+
+	const renderStatBlock = () => {
+		if ( value || description ) {
+			return (
+				<div className="jp-disconnect-card__card-stat-block">
+					<span className="jp-disconnect-card__card-stat">{ value }</span>
+					<div className="jp-disconnect-card__card-description">{ description }</div>
+				</div>
+			);
+		}
+	};
+
+	return (
+		<div className="jp-disconnect-card card">
+			<div className="jp-disconnect-card__card-content">
+				<p className="jp-disconnect-card__card-headline">{ title }</p>
+				{ renderStatBlock() }
+			</div>
+		</div>
+	);
+};
+
+export default DisconnectCard;
