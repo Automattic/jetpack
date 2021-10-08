770c7f6c5dbe537c458a36599805c80ce563c3ea
John Webb <john.webb@automattic.com>
2021-10-08 13:49:20 -0400

    [Plugin] Backup: Disconnect on Deactivation if last plugin (#21252)
    * Add deactivation hook to disconnect plugin
    
    * Added changelog entry
    
    * Updated to major for versioning purposes

--- projects/plugins/backup/src/php/class-jetpack-backup.php
+++ projects/plugins/backup/src/php/class-jetpack-backup.php
@@ -10,6 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 use Automattic\Jetpack\Admin_UI\Admin_Menu;
+use Automattic\Jetpack\Connection\Manager as Connection_Manager;
 use Automattic\Jetpack\Connection\Rest_Authentication as Connection_Rest_Authentication;
 
 /**
@@ -231,5 +232,17 @@ class Jetpack_Backup {
 		return rest_ensure_response(
 			json_decode( $response['body'], true )
 		);
+	}
+
+	/**
+	 * Removes plugin from the connection manager
+	 * If it's the last plugin using the connection, the site will be disconnected.
+	 *
+	 * @access public
+	 * @static
+	 */
+	public static function plugin_deactivation() {
+		$manager = new Connection_Manager( 'jetpack-backup' );
+		$manager->remove_connection();
 	}
 }
