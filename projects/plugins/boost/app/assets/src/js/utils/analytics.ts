fe7c901027d9d5547aaee5409d7649b70f9887e0
Karthik Bhat <karthik.ax.ax@gmail.com>
2021-10-08 13:29:49 +0530

    Add Boost version to JS tracks events

--- projects/plugins/boost/app/assets/src/js/utils/analytics.ts
+++ projects/plugins/boost/app/assets/src/js/utils/analytics.ts
@@ -7,5 +7,9 @@ export function recordBoostEvent(
 	eventType: string,
 	eventProp: TracksEventProperties
 ): void {
+	if ( ! ( 'boost_version' in eventProp ) ) {
+		eventProp.boost_version = Jetpack_Boost.version;
+	}
+
 	jpTracksAJAX.record_ajax_event( `boost_${ eventName }`, 'click', eventProp );
 }
