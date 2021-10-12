8352260c8d3aed318175a3d22d4240ee2d89c4c8
Jasper Kang <kangzjnet@gmail.com>
2021-10-12 13:04:13 +1300

    [not verified] Update projects/plugins/jetpack/tools/webpack.helpers.js
    Co-authored-by: Jason Moon <4044428+jsnmoon@users.noreply.github.com>

--- projects/plugins/jetpack/tools/webpack.helpers.js
+++ projects/plugins/jetpack/tools/webpack.helpers.js
@@ -32,7 +32,7 @@ function definePaletteColorsAsStaticVariables() {
  * Returns an instance of the AddReadableJsAssetsWebpackPlugin that adds readable JS assets.
  * The plugin now only works for Search assets, i.e. asset file name pattern /(\.[a-f0-9]{20})?\.min\.js$/
  *
- * Original webpack plugin: https://github.com/WordPress/gutenberg/blob/b1fd6b2ce6221f1297da78b6c48aeee56130501b/packages/readable-js-assets-webpack-plugin/index.js
+ * Forked from: https://github.com/WordPress/gutenberg/blob/b1fd6b2ce6221f1297da78b6c48aeee56130501b/packages/readable-js-assets-webpack-plugin/index.js
  *
  * 1. Our translations pipeline ignores all files suffixed with `.min.js`.
  * 2. This plugin will generate a non-minified JS file for each `.min.js` asset.
