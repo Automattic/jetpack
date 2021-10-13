363123b6072a64a16fee4075c3ae650066ee72e2
retrofox <rdsuarez@gmail.com>
2021-10-13 16:11:08 -0300

    [not verified] Add available and upgradable props to the usePublicizeConfig hook

--- projects/plugins/jetpack/extensions/plugins/publicize/hooks/use-publicize-config/index.js
+++ projects/plugins/jetpack/extensions/plugins/publicize/hooks/use-publicize-config/index.js
@@ -3,9 +3,18 @@
  */
 import { useSelect, useDispatch } from '@wordpress/data';
 
+/*
+ * Internal dependencies
+ */
+import getJetpackExtensionAvailability from '../../../../shared/get-jetpack-extension-availability';
+import { isUpgradable } from '../../../../shared/plan-utils';
+
+const republicizeFeatureName = 'republicize';
+
 export default function usePublicizeConfig() {
 	// Actions.
 	const { togglePublicizeFeature } = useDispatch( 'jetpack/publicize' );
+	const { available } = getJetpackExtensionAvailability( republicizeFeatureName );
 
 	// Data.
 	const isPublicizeEnabled = useSelect(
@@ -17,7 +26,8 @@ export default function usePublicizeConfig() {
 		isRePublicizeFeatureEnabled: !! window?.Jetpack_Editor_Initial_State.jetpack
 			?.republicize_enabled,
 		isPublicizeEnabled,
-
 		togglePublicizeFeature,
+		isRePublicizeFeatureAvailable: available,
+		isRePublicizeFeatureUpgradable: isUpgradable( republicizeFeatureName ),
 	};
 }
