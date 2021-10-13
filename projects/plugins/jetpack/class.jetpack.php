211ef231666c7d0b429726ee2df98b0d4620f1b1
retrofox <rdsuarez@gmail.com>
2021-10-13 09:53:50 -0300

    [not verified] load block edutor plugins

--- projects/plugins/jetpack/class.jetpack.php
+++ projects/plugins/jetpack/class.jetpack.php
@@ -674,6 +674,7 @@ class Jetpack {
 		add_action( 'plugins_loaded', array( 'Jetpack_Gutenberg', 'init' ) );
 		add_action( 'plugins_loaded', array( 'Jetpack_Gutenberg', 'load_independent_blocks' ) );
 		add_action( 'plugins_loaded', array( 'Jetpack_Gutenberg', 'load_extended_blocks' ), 9 );
+		add_action( 'plugins_loaded', array( 'Jetpack_Gutenberg', 'load_block_editor_plugins' ), 9 );
 		add_action( 'enqueue_block_editor_assets', array( 'Jetpack_Gutenberg', 'enqueue_block_editor_assets' ) );
 
 		add_action( 'set_user_role', array( $this, 'maybe_clear_other_linked_admins_transient' ), 10, 3 );
