9ef32f38f07b9e79ff0dc044070eca4a4f525d83
Jason Moon <jason.moon@automattic.com>
2021-10-12 18:54:17 -0600

    WIP 2

---
+++ projects/packages/search/src/dashboard/components/mocked-legacy-search.jsx
@@ -0,0 +1,33 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+// import Gridicon from 'components/gridicon';
+import TextRowPlaceHolder from './placeholder';
+import './mocked-legacy-search.scss';
+
+const Gridicon = () => <span></span>;
+
+/**
+ * Generate mocked search dialog
+ *
+ * @returns {React.Component}	Mocked Search dialog component.
+ */
+export default function MockedLegacySearch() {
+	return (
+		<div className="jp-mocked-legacy-search" aria-hidden="true">
+			<div className="jp-mocked-legacy-search__search-controls">
+				<div className="jp-mocked-legacy-search__search-icon">
+					<Gridicon icon="search" size={24} />
+				</div>
+				<div className="jp-mocked-legacy-search__search-input">
+					<TextRowPlaceHolder style={{ height: '50px', width: '80%', maxWidth: '212px' }} />
+				</div>
+			</div>
+		</div>
+	);
+}
