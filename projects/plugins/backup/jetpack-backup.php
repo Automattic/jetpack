fe3b36e6df5c3ce1e1e79d28abdfb8226357dc43
John Webb <john.webb@automattic.com>
2021-10-13 15:29:11 -0400

    [not verified] Backup: Updating version to 1.0.0

--- projects/plugins/backup/jetpack-backup.php
+++ projects/plugins/backup/jetpack-backup.php
@@ -4,7 +4,7 @@
  * Plugin Name: Jetpack Backup
  * Plugin URI: https://jetpack.com/jetpack-backup
  * Description: Easily restore or download a backup of your site from a specific moment in time.
- * Version: 1.0.0-beta
+ * Version: 1.0.0
  * Author: Automattic
  * Author URI: https://jetpack.com/
  * License: GPLv2 or later
