3dde1f37ee23b41a184712c776b3a2336a45727d
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2021-10-14 15:13:25 +0200

    Add `getCoordinates` function
    The `getCoordinates` function will take care of the Mapbox API request.
    
    The function accepts physical address and Mapbox API key as parameters.
    
    The output is the promise / Mapbox API response that contains geo coordinates.

---
+++ projects/plugins/jetpack/extensions/blocks/map/get-coordinates.js
@@ -0,0 +1,14 @@
+/**
+ * getCoordinates() geocodes provided address into coordinates
+ *
+ * @param  {string} address - physical / location address (e.g. "London") that we want to geocode into coordinates (longitude and latitude)
+ * @param  {string} apiKey - Mapbox API key
+ * @returns {Promise} When the fetch() is successeful, `response` will contain the response from Mapbox API
+ */
+export function getCoordinates( address, apiKey ) {
+	return fetch(
+		`https://api.mapbox.com/geocoding/v5/mapbox.places/${ encodeURIComponent(
+			address
+		) }.json?access_token=${ apiKey }`
+	).then( response => response.json() );
+}
