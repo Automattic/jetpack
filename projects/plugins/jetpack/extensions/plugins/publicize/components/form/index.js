eca1ee6b47bd27d2f98845724cef03befd23b424
Allison Levine <allison.levine@automattic.com>
2021-10-15 18:26:35 -0400

    [not verified] Trigger state change when connections change

--- projects/plugins/jetpack/extensions/plugins/publicize/components/form/index.js
+++ projects/plugins/jetpack/extensions/plugins/publicize/components/form/index.js
@@ -9,7 +9,7 @@
 /**
  * External dependencies
  */
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 /**
  * WordPress dependencies
@@ -27,35 +27,37 @@ import useSocialMediaConnections from '../../hooks/use-social-media-connections'
 import useSocialMediaMessage from '../../hooks/use-social-media-message';
 
 export default function PublicizeForm() {
-	const { connections, toggleById, refresh } = useSocialMediaConnections();
+	const { connections, toggleById } = useSocialMediaConnections();
 	const { message, updateMessage, maxLength } = useSocialMediaMessage();
+	const [ activeConnections, setActiveConnections ] = useState( {} );
 
 	function isDisabled() {
 		return connections.every( connection => ! connection.toggleable );
 	}
 
 	useEffect( () => {
-		refresh();
-	}, [ connections, refresh ] );
+		setActiveConnections( connections );
+	}, [ connections ] );
 
 	return (
 		<Fragment>
 			<PanelRow>
 				<ul className="jetpack-publicize__connections-list">
-					{ connections.map(
-						( { display_name, enabled, id, service_name, toggleable, profile_picture } ) => (
-							<PublicizeConnection
-								disabled={ ! toggleable }
-								enabled={ enabled }
-								key={ id }
-								id={ id }
-								label={ display_name }
-								name={ service_name }
-								toggleConnection={ toggleById }
-								profilePicture={ profile_picture }
-							/>
-						)
-					) }
+					{ activeConnections.length > 0 &&
+						activeConnections.map(
+							( { display_name, enabled, id, service_name, toggleable, profile_picture } ) => (
+								<PublicizeConnection
+									disabled={ ! toggleable }
+									enabled={ enabled }
+									key={ id }
+									id={ id }
+									label={ display_name }
+									name={ service_name }
+									toggleConnection={ toggleById }
+									profilePicture={ profile_picture }
+								/>
+							)
+						) }
 				</ul>
 			</PanelRow>
 
