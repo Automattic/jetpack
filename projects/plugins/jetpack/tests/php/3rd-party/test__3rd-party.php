a00e1ccd0bf873e71116418b517ea7943128e6a8
Brandon Kraft <public@brandonkraft.com>
2021-10-19 14:44:54 -0500

    Add test for Jetpack Beta-provided versions

--- projects/plugins/jetpack/tests/php/3rd-party/test__3rd-party.php
+++ projects/plugins/jetpack/tests/php/3rd-party/test__3rd-party.php
@@ -13,17 +13,39 @@ require_once JETPACK__PLUGIN_DIR . '3rd-party/3rd-party.php';
 class WP_Test_Third_Party_Support extends WP_UnitTestCase {
 
 	/**
-	 * Test that Development Versions are suppressed on Atomic.
+	 * Setup tests.
 	 */
-	public function test_atomic_returns_false_on_dev_version() {
+	public function set_up() {
+		parent::set_up();
 		Constants::set_constant( 'ATOMIC_CLIENT_ID', 999 );
 		Constants::set_constant( 'ATOMIC_SITE_ID', 999 );
 		Constants::set_constant( 'JETPACK__VERSION', '10.3-a.1' );
+	}
 
-		$this->assertFalse( Jetpack::is_development_version() );
-
+	/**
+	 * Clean up tests.
+	 */
+	public function tear_down() {
 		Constants::clear_single_constant( 'ATOMIC_SITE_ID' );
 		Constants::clear_single_constant( 'ATOMIC_CLIENT_ID' );
+		Constants::clear_single_constant( 'JETPACK__VERSION', '10.3-a.1' );
+		parent::tear_down();
+	}
+
+	/**
+	 * Test that Development Versions are suppressed on Atomic.
+	 */
+	public function test_atomic_returns_false_on_dev_version() {
+		$this->assertFalse( Jetpack::is_development_version() );
+	}
+
+	/**
+	 * Test that Development Versions via the Beta plugin are still considered as Development versions.
+	 */
+	public function test_atomic_returns_true_on_beta_plugin_version() {
+		Constants::set_constant( 'JETPACK__PLUGIN_DIR', '/srv/www/public/wp-content/plugins/jetpack-dev/' );
+		$this->assertTrue( Jetpack::is_development_version() );
+		Constants::clear_single_constant( 'JETPACK__PLUGIN_DIR' );
 	}
 
 }
