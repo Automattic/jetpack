6d377f948ad6f0e087efb01cdb42a06017e20cfe
Paul Bunkham <paul@dobit.co.uk>
2021-10-21 16:52:53 +0100

    Post List: Enable the Share post action https://github.com/Automattic/wpcomsh/pull/770
    * Post List: Enable the Share post action
    
    This change adjusts the configuration of the post list package and
    enables the Share post action when the Republicize feature flag is set
    and the feature is available (i.e. the site has an appropriate plan).
    
    * Enable republicize if the blog sticker is present
    
    * Bumped wpcomsh version number to 2.8.42.
    
    Co-authored-by: DustyReagan <dusty@dustyreagan.com>

--- projects/plugins/wpcomsh/feature-plugins/post-list.php
+++ projects/plugins/wpcomsh/feature-plugins/post-list.php
@@ -17,10 +17,38 @@ use Automattic\Jetpack\Config;
 function wpcomsh_post_list_init() {
 	if ( ! wpcomsh_is_site_sticker_active( 'wpcom-post-list-enhancements' ) ) {
 		return;
-
 	}
 	$config = new Config();
 	$config->ensure( 'post_list' );
+
+	if ( wpcomsh_is_site_sticker_active( 'wpcom-gutenberg-republicize' ) ) {
+		add_filter( 'jetpack_block_editor_republicize_feature', '__return_true' );
+		add_action( 'admin_init', 'wpcomsh_maybe_enable_share_action' );
+	}
+
 }
 
 add_action( 'plugins_loaded', 'wpcomsh_post_list_init', 1 );
+
+/**
+ * Checks if Republicize is available and that the classic editor plugin
+ * in not active. If so, then it enables the Share post action in the
+ * post list.
+ */
+function wpcomsh_maybe_enable_share_action() {
+	// Jetpack isn't available. Unlikely if we got here, but worth checking.
+	if ( ! class_exists( 'Jetpack' ) || ! class_exists( 'Jetpack_Gutenberg' ) ) {
+		return;
+	}
+
+	if ( Jetpack::is_plugin_active( 'classic-editor/classic-editor.php') ||
+		array_key_exists( 'classic-editor.php', get_mu_plugins() ) ) {
+		return;
+	}
+
+	// Needed to ensure that the jetpack_register_gutenberg_extensions action has fired.
+	Jetpack_Gutenberg::get_cached_availability();
+	if ( wpcomsh_is_site_sticker_active( 'wpcom-gutenberg-republicize' ) && Jetpack_Gutenberg::is_available( 'republicize' ) ) {
+		add_filter( 'jetpack_post_list_display_share_action', '__return_true' );
+	}
+}
