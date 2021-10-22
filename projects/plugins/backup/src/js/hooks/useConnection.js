5f1d50d76f5d9259251ee8ca3d1c36fc0407cb03
jboland88 <18016357+jboland88@users.noreply.github.com>
2021-10-22 15:54:52 -0400

    [not verified] Pass assetBaseUrl to the ConnectionStatusCard to be used for images in disconnect flow

--- projects/plugins/backup/src/js/hooks/useConnection.js
+++ projects/plugins/backup/src/js/hooks/useConnection.js
@@ -61,6 +61,7 @@ export default function useConnection() {
 				currentPlugin="jetpack-backup"
 				connectedPlugins={ connectedPlugins }
 				redirectUri="admin.php?page=jetpack-backup"
+				assetBaseUrl={ assetBuildUrl }
 			/>
 		);
 	};
