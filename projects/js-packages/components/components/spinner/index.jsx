a0986fc324017079e8407e929ff52c56e48cc3fd
Filipe Varela <keoshi@keoshi.com>
2021-10-22 17:12:19 +0100

    Storybook: add Jetpack Spinner

--- projects/js-packages/components/components/spinner/index.jsx
+++ projects/js-packages/components/components/spinner/index.jsx
@@ -12,29 +12,37 @@ import './style.scss';
 const Spinner = props => {
 	const className = props.className + ' jp-components-spinner';
 
-	const style = {
+	const styleOuter = {
 		width: props.size,
 		height: props.size,
 		fontSize: props.size, // allows border-width to be specified in em units
+		borderTopColor: props.color,
+	};
+
+	const styleInner = {
+		borderTopColor: props.color,
+		borderRightColor: props.color,
 	};
 
 	return (
 		<div className={ className }>
-			<div className="jp-components-spinner__outer" style={ style }>
-				<div className="jp-components-spinner__inner" />
+			<div className="jp-components-spinner__outer" style={ styleOuter }>
+				<div className="jp-components-spinner__inner" style={ styleInner } />
 			</div>
 		</div>
 	);
 };
 
 Spinner.propTypes = {
+	color: PropTypes.string,
 	className: PropTypes.string,
 	size: PropTypes.number,
 };
 
 Spinner.defaultProps = {
-	size: 20,
+	color: '#000000',
 	className: '',
+	size: 20,
 };
 
 export default Spinner;
---
+++ projects/js-packages/components/components/spinner/stories/index.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/react-in-jsx-scope */
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import Spinner from '../index.jsx';
+
+export default {
+	title: 'Playground/Jetpack Spinner',
+	component: Spinner,
+};
+
+const Template = args => <Spinner { ...args } />;
+
+const DefaultArgs = {
+	moduleName: 'The Module Name',
+};
+
+export const _default = Template.bind( {} );
+_default.args = DefaultArgs;
