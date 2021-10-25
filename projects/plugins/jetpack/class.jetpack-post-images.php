b377a589de83d8d8434bb5567282ec91d16d33b5
Daniel Post <daniel.post@automattic.com>
2021-10-25 11:20:20 +0200

    Fix typo in Docblock

--- projects/plugins/jetpack/class.jetpack-post-images.php
+++ projects/plugins/jetpack/class.jetpack-post-images.php
@@ -834,7 +834,7 @@ class Jetpack_PostImages {
 	 * @since 7.1
 	 *
 	 * @param int $attachment_id The Post ID of the media.
-	 * @return string The alt text value or an emptry string.
+	 * @return string The alt text value or an empty string.
 	 */
 	public static function get_alt_text( $attachment_id ) {
 		return get_post_meta( $attachment_id, '_wp_attachment_image_alt', true );
