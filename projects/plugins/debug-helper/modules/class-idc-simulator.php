93dd6dcf9a7dc45fae8397753fe7ee36a904e7c4
Kim Brown <50059399+kbrown9@users.noreply.github.com>
2021-10-25 22:37:05 -0400

    Debug Helper IDC Simulator: add a button to trigger a remote request
    Remote requests need to be sent to WPCOM  when testing the IDC process. To make testing easier,
    add a button to the IDC Simulator page which triggers an authenicated remote request to WPCOM.

--- projects/plugins/debug-helper/modules/class-idc-simulator.php
+++ projects/plugins/debug-helper/modules/class-idc-simulator.php
@@ -18,7 +18,22 @@ class IDC_Simulator {
 	 *
 	 * @var mixed|string
 	 */
-	public $notice_type = 'success';
+	public $notice_type = '';
+
+	/**
+	 * Stored success notice type.
+	 */
+	const STORED_SUCCESS_NOTICE_TYPE = 'stored_success';
+
+	/**
+	 * Request success notice type.
+	 */
+	const REQUEST_SUCCESS_NOTICE_TYPE = 'request_success';
+
+	/**
+	 * Error notice type.
+	 */
+	const UNKNOWN_ERROR_NOTICE_TYPE = 'unknown_error';
 
 	/**
 	 * Options.
@@ -36,8 +51,10 @@ class IDC_Simulator {
 		// Stored options.
 		add_action( 'admin_post_store_current_options', array( $this, 'admin_post_store_current_options' ) );
 
+		add_action( 'admin_post_idc_send_remote_request', array( $this, 'admin_post_idc_send_remote_request' ) );
+
 		if ( isset( $_GET['idc_notice'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
-			add_action( 'admin_notices', array( $this, 'admin_notice__success' ) );
+			add_action( 'admin_notices', array( $this, 'display_notice' ) );
 		}
 	}
 
@@ -156,12 +173,35 @@ class IDC_Simulator {
 		</form>
 
 		<hr>
+		<hr>
+
+		<?php $this->display_request_button(); ?>
+
+		<hr>
 
 		<h2>Current IDC transient values</h2>
 		<h3>jetpack_idc_local</h3>
 		<pre><?php var_dump( get_transient( 'jetpack_idc_local' ) ); //phpcs:ignore ?></pre>
 
 		<hr>
+
+		<?php
+	}
+
+	/**
+	 * Displays the remote request button.
+	 */
+	private function display_request_button() {
+		?>
+		<h2>Send an authenticated remote request to WPCOM</h2>
+
+		<p>Sends an authenticated remote request to the <code>wpcom/v2/sites/{blog id}/jetpack-token-health/blog</code> endpoint.</p>
+
+		<form action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>" method="post">
+			<input type="hidden" name="action" value="idc_send_remote_request">
+				<?php wp_nonce_field( 'idc-send-remote-request' ); ?>
+			<input type="submit" value="Send Remote Request" class="button button-primary">
+		</form>
 		<?php
 	}
 
@@ -181,18 +221,95 @@ class IDC_Simulator {
 			)
 		);
 
+		$this->notice_type = 'stored_success';
+		$this->admin_post_redirect_referrer();
+	}
+
+	/**
+	 * Send an authenticated remote request to WPCOM.
+	 */
+	public function admin_post_idc_send_remote_request() {
+		check_admin_referer( 'idc-send-remote-request' );
+		$error = false;
+
+		if ( ! class_exists( 'Automattic\Jetpack\Connection\Client' ) ) {
+			$this->notice_type = 'unknown_error';
+			$error             = true;
+		}
+
+		$blog_id = Jetpack_Options::get_option( 'id' );
+		if ( ! $blog_id ) {
+			$this->notice_type = 'unknown_error';
+			$error             = true;
+		}
+
+		if ( ! $error ) {
+			$url    = sprintf(
+				'%s/%s/v%s/%s',
+				Automattic\Jetpack\Constants::get_constant( 'JETPACK__WPCOM_JSON_API_BASE' ),
+				'wpcom',
+				'2',
+				'sites/' . $blog_id . '/jetpack-token-health/blog'
+			);
+			$method = 'GET';
+			Automattic\Jetpack\Connection\Client::remote_request( compact( 'url', 'method' ) );
+
+			$this->notice_type = 'request_success';
+		}
+
 		$this->admin_post_redirect_referrer();
 	}
 
 	/**
 	 * Shows a simple success notice.
 	 */
-	public function admin_notice__success() {
+	public function admin_notice__stored_success() {
 		?>
 		<div class="notice notice-success is-dismissible">
 			<p>IDC simulation settings have been saved!</p>
 		</div>
 		<?php
+	}
+
+	/**
+	 * Shows a simple success notice.
+	 */
+	public function admin_notice__request_success() {
+		?>
+		<div class="notice notice-success is-dismissible">
+			<p>The remote request was successfully sent!</p>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Shows a simple error notice.
+	 */
+	public function admin_notice__unknown_error() {
+		?>
+		<div class="notice notice-error is-dismissible">
+			<p>Something went wrong.</p>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Display a notice if necessary.
+	 */
+	public function display_notice() {
+		switch ( $_GET['idc_notice'] ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+			case self::STORED_SUCCESS_NOTICE_TYPE:
+				return $this->admin_notice__stored_success();
+
+			case self::REQUEST_SUCCESS_NOTICE_TYPE:
+				return $this->admin_notice__request_success();
+
+			case self::UNKNOWN_ERROR_NOTICE_TYPE:
+				return $this->admin_notice__unknown_error();
+
+			default:
+				return;
+		}
 	}
 
 	/**
