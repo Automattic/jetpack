46f6454ea2b72725e4878a0da1315e0bacf89bdb
retrofox <rdsuarez@gmail.com>
2021-10-29 10:17:05 -0300

    [not verified] do not hide RePublicize cmps when the feaure flag is disabled

--- projects/plugins/jetpack/extensions/plugins/publicize/components/panel/index.js
+++ projects/plugins/jetpack/extensions/plugins/publicize/components/panel/index.js
@@ -28,52 +28,6 @@ import usePublicizeConfig from '../../hooks/use-publicize-config';
 import { SharePostRow } from '../../components/share-post';
 import UpsellNotice from '../upsell';
 
-function getPanelDescription(
-	isPostPublished,
-	isRePublicizeFeatureEnabled,
-	isPublicizeEnabled,
-	hasConnections,
-	hasEnabledConnections
-) {
-	// Use constants when the string is used in multiple places.
-	const start_your_posts_string = __(
-		'Start sharing your posts by connecting your social media accounts.',
-		'jetpack'
-	);
-	const this_post_will_string = __(
-		'This post will be shared on all your enabled social media accounts the moment you publish the post.',
-		'jetpack'
-	);
-
-	// RePublicize feature is disabled.
-	if ( ! isRePublicizeFeatureEnabled ) {
-		if ( isPostPublished ) {
-			return start_your_posts_string;
-		}
-
-		return this_post_will_string;
-	}
-
-	// RePublicize feature is enabled.
-	// No connections.
-	if ( ! hasConnections ) {
-		return start_your_posts_string;
-	}
-
-	if ( ! isPublicizeEnabled || ! hasEnabledConnections ) {
-		return __( 'Use this tool to share your post on all your social media accounts.', 'jetpack' );
-	}
-
-	if ( isPublicizeEnabled && hasEnabledConnections && ! isPostPublished ) {
-		return this_post_will_string;
-	}
-
-	return __(
-		'Share this post on all your enabled social media accounts by clicking on the share post button.',
-		'jetpack'
-	);
-}
-
 const PublicizePanel = ( { prePublish } ) => {
 	const { refresh, hasConnections, hasEnabledConnections } = useSelectSocialMediaConnections();
 	const isPostPublished = useSelect( select => select( editorStore ).isCurrentPostPublished(), [] );
@@ -106,7 +60,10 @@ const PublicizePanel = ( { prePublish } ) => {
 	 * it needs to hide part of the Publicize feature.
 	 */
 	const hideRePublicizeFeature =
-		isPostPublished && ! isRePublicizeFeatureAvailable && ! isRePublicizeUpgradableViaUpsell;
+		isPostPublished &&
+		! isRePublicizeFeatureAvailable &&
+		! isRePublicizeUpgradableViaUpsell &&
+		isRePublicizeFeatureEnabled;
 
 	const isPublicizeEnabled = isPublicizeEnabledFromConfig && ! isPublicizeDisabledBySitePlan;
 
@@ -136,16 +93,6 @@ const PublicizePanel = ( { prePublish } ) => {
 
 	return (
 		<PanelWrapper { ...wrapperProps }>
-			<div>
-				{ getPanelDescription(
-					isPostPublished,
-					isRePublicizeFeatureEnabled,
-					isPublicizeEnabled,
-					hasConnections,
-					hasEnabledConnections
-				) }
-			</div>
-
 			<UpsellNotice isPostPublished={ isPostPublished } />
 
 			{ ! hideRePublicizeFeature && (
