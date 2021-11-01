b57a3d49373a2766d40c442205472e15357b0709
Adrian Moldovan <adim.moldovan@gmail.com>
2021-11-01 16:21:49 +0200

    Move jest.setup.js in e2e-commons

--- projects/plugins/jetpack/tests/e2e/jest.config.js
+++ projects/plugins/jetpack/tests/e2e/jest.config.js
@@ -12,7 +12,7 @@ module.exports = {
 	testEnvironment: require.resolve( 'jetpack-e2e-commons/env/playwright-environment.js' ),
 	globalSetup: require.resolve( 'jetpack-e2e-commons/env/global-setup.js' ),
 	globalTeardown: require.resolve( 'jetpack-e2e-commons/env/global-teardown.js' ),
-	setupFilesAfterEnv: [ '<rootDir>/jest.setup.js' ],
+	setupFilesAfterEnv: [ require.resolve( 'jetpack-e2e-commons/jest.setup.js' ) ],
 	testRunner: 'jest-circus/runner',
 	runner: 'groups',
 	testEnvironmentOptions: {
