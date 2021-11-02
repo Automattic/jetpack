04a8a65b32627588cde1a01bfd6df179ec23fc7c
Siobhan <siobhan@automattic.com>
2021-11-02 16:34:49 +0000

    Use default colour for block icon
    By removing 'fill="currentColor"', the block icon will go back to the general defaul colour that's used for all block icons, thus restoring consistency.

--- projects/plugins/jetpack/extensions/blocks/tiled-gallery/index.js
+++ projects/plugins/jetpack/extensions/blocks/tiled-gallery/index.js
@@ -198,10 +198,7 @@ export const name = 'tiled-gallery';
 
 export const icon = (
 	<SVG viewBox="0 0 24 24" width={ 24 } height={ 24 }>
-		<Path
-			fill="currentColor"
-			d="M19 5v2h-4V5h4M9 5v6H5V5h4m10 8v6h-4v-6h4M9 17v2H5v-2h4M21 3h-8v6h8V3zM11 3H3v10h8V3zm10 8h-8v10h8V11zm-10 4H3v6h8v-6z"
-		/>
+		<Path d="M19 5v2h-4V5h4M9 5v6H5V5h4m10 8v6h-4v-6h4M9 17v2H5v-2h4M21 3h-8v6h8V3zM11 3H3v10h8V3zm10 8h-8v10h8V11zm-10 4H3v6h8v-6z" />
 	</SVG>
 );
 
