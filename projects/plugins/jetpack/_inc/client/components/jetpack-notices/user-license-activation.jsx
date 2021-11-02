4f98862a7a853b3c480fb83fbedda876026b26f0
elliottprogrammer <bryan@elliottprogrammer.com>
2021-11-02 15:08:25 -0400

    Convert to function component - UserLicenseActivationNotice.

--- projects/plugins/jetpack/_inc/client/components/jetpack-notices/user-license-activation.jsx
+++ projects/plugins/jetpack/_inc/client/components/jetpack-notices/user-license-activation.jsx
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { createElement, useCallback } from 'react';
 import { connect } from 'react-redux';
 import { createInterpolateElement } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
@@ -18,71 +18,76 @@ import {
 } from 'state/licensing';
 import SimpleNotice from 'components/notice';
 
-class UserLicenseActivationNotice extends React.Component {
-	static propTypes = {
-		detachedLicensesCount: PropTypes.number.isRequired,
-		activationNoticeDismissInfo: PropTypes.shape( {
-			last_detached_count: PropTypes.number,
-			last_dismiss_time: PropTypes.string,
-		} ),
-	};
+/**
+ * Jetpack "user"-licenses activation notice. (a license key is available for activation)
+ *
+ * @param {object} props - The properties.
+ * @param {number} props.detachedLicensesCount - The user's number of "detached" licenses.
+ * @param {object} props.activationNoticeDismissInfo - Object containing `last_detached_count` and `last_dismissed_time`.
+ * @param {Function} props.updateLicensingActivationNoticeDismiss - Function to update the notification dismiss info.
+ * @returns {React.Component} The `UserLicenseActivationNotice` component.
+ */
+const UserLicenseActivationNotice = props => {
+	// TODO: Update this link to point to the user-license activation route.
+	const USER_LICENSE_ACTIVATION_ROUTE = '/wp-admin/admin.php?page=jetpack#/my-plan';
+	const DAY_IN_MILLISECONDS = 24 * 3600 * 1000;
+	const MAX_DAYS_DISMISSED = 14;
 
-	trackClick = () => {
-		analytics.tracks.recordJetpackClick( 'activate-user-license' );
-	};
+	const {
+		detachedLicensesCount,
+		activationNoticeDismissInfo,
+		updateLicensingActivationNoticeDismiss,
+	} = props;
 
-	render() {
-		// TODO: Update this link to point to the user-license activation route.
-		const USER_LICENSE_ACTIVATION_ROUTE = '/wp-admin/admin.php?page=jetpack#/my-plan';
-		const DAY_IN_MILLISECONDS = 24 * 3600 * 1000;
-		const MAX_DAYS_DISMISSED = 14;
+	const {
+		last_detached_count: lastDetachedCount,
+		last_dismissed_time: lastDismissedDateTime,
+	} = activationNoticeDismissInfo;
 
-		const {
-			detachedLicensesCount: currentDetachedCount,
-			updateLicensingActivationNoticeDismiss,
-		} = this.props;
+	const userHasDetachedLicenses = !! detachedLicensesCount;
+	const userHasNewDetachedLicenses = detachedLicensesCount > ( lastDetachedCount || 0 );
 
-		const {
-			last_detached_count: lastDetachedCount,
-			last_dismissed_time: lastDismissedDateTime,
-		} = this.props.activationNoticeDismissInfo;
+	const now = new Date();
+	const lastDismissedTime = new Date( lastDismissedDateTime ? lastDismissedDateTime : new Date() );
+	const daysNoticeHasBeenDismissed = ( now - lastDismissedTime ) / DAY_IN_MILLISECONDS;
 
-		const userHasDetachedLicenses = !! currentDetachedCount;
-		const userHasNewDetachedLicenses = currentDetachedCount > ( lastDetachedCount || 0 );
+	const trackClick = useCallback( () => {
+		analytics.tracks.recordJetpackClick( 'activate-user-license-notice-click' );
+	}, [] );
 
-		const now = new Date();
-		const lastDismissedTime = new Date(
-			lastDismissedDateTime ? lastDismissedDateTime : new Date()
+	// Show the notice when the user acquires a new license, Or when the user has an available
+	// license(s), but has dismissed the notice and it's been over 2 weeks without activating it.
+	if (
+		userHasDetachedLicenses &&
+		( userHasNewDetachedLicenses || daysNoticeHasBeenDismissed > MAX_DAYS_DISMISSED )
+	) {
+		return (
+			<SimpleNotice
+				className="jp-license-activation-notice"
+				showDismiss={ true }
+				onDismissClick={ updateLicensingActivationNoticeDismiss }
+				text={ createInterpolateElement(
+					__( 'You have an inactive product. <a>Activate now</a>', 'jetpack' ),
+					{
+						a: createElement( 'a', {
+							href: USER_LICENSE_ACTIVATION_ROUTE,
+							onClick: trackClick,
+						} ),
+					}
+				) }
+			/>
 		);
-		const daysNoticeHasBeenDismissed = ( now - lastDismissedTime ) / DAY_IN_MILLISECONDS;
-
-		// Show the notice when the user acquires a new license, Or when the user has an available
-		// license(s), but has dismissed the notice and it's been over 2 weeks without activating it.
-		if (
-			userHasDetachedLicenses &&
-			( userHasNewDetachedLicenses || daysNoticeHasBeenDismissed > MAX_DAYS_DISMISSED )
-		) {
-			return (
-				<SimpleNotice
-					className="jp-license-activation-notice"
-					showDismiss={ true }
-					onDismissClick={ updateLicensingActivationNoticeDismiss }
-					text={ createInterpolateElement(
-						__( 'You have an inactive product. <a>Activate now</a>', 'jetpack' ),
-						{
-							a: (
-								<a href={ USER_LICENSE_ACTIVATION_ROUTE } onClick={ this.trackClick }>
-									{ this.props.children }
-								</a>
-							),
-						}
-					) }
-				/>
-			);
-		}
-		return false;
 	}
-}
+	return null;
+};
+
+UserLicenseActivationNotice.propTypes = {
+	detachedLicensesCount: PropTypes.number.isRequired,
+	activationNoticeDismissInfo: PropTypes.shape( {
+		last_detached_count: PropTypes.number,
+		last_dismiss_time: PropTypes.string,
+	} ),
+};
 
 export default connect(
 	state => {
