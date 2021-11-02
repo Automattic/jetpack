c550b8ed400040484071ff52c2d0709d64f33050
mmtr <miguel.torres@automattic.com>
2021-11-02 13:14:08 +0100

    Nav Unification: Set default view with a direct server request

--- projects/plugins/jetpack/modules/masterbar/admin-menu/class-base-admin-menu.php
+++ projects/plugins/jetpack/modules/masterbar/admin-menu/class-base-admin-menu.php
@@ -78,7 +78,6 @@ abstract class Base_Admin_Menu {
 			add_action( 'admin_head', array( $this, 'set_site_icon_inline_styles' ) );
 			add_filter( 'screen_settings', array( $this, 'register_dashboard_switcher' ), 99999 );
 			add_action( 'admin_menu', array( $this, 'handle_preferred_view' ), 99997 );
-			add_action( 'wp_ajax_set_preferred_view', array( $this, 'handle_preferred_view_ajax' ) );
 		}
 	}
 
@@ -647,15 +646,17 @@ abstract class Base_Admin_Menu {
 		 * @param string The preferred view the user selected.
 		 */
 		\do_action( 'jetpack_dashboard_switcher_changed_view', $current_screen, $preferred_view );
-		// phpcs:enable WordPress.Security.NonceVerification
-	}
 
-	/**
-	 * Handles AJAX requests setting a preferred view for a given screen.
-	 */
-	public function handle_preferred_view_ajax() {
-		$this->handle_preferred_view();
-		wp_die();
+		// Redirect to default view if that's the newly preferred view.
+		if ( self::DEFAULT_VIEW === $preferred_view ) {
+			$menu_mappings = require __DIR__ . '/menu-mappings.php';
+			if ( isset( $menu_mappings[ $current_screen ] ) ) {
+				\Jetpack::$instance->add_wpcom_to_allowed_redirect_hosts();
+				wp_safe_redirect( $menu_mappings[ $current_screen ] . $this->domain );
+				exit;
+			}
+		}
+		// phpcs:enable WordPress.Security.NonceVerification
 	}
 
 	/**
