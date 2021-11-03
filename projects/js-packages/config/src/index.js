0dd7ead666f43659c943c81fdfbfe7af3ffce145
leogermani <leogermani@gmail.com>
2021-11-03 11:56:42 -0300

    consistently use commonjs to export the modules

--- projects/js-packages/config/src/index.js
+++ projects/js-packages/config/src/index.js
@@ -11,11 +11,11 @@ try {
 	jetpackConfig = { missingConfig: true };
 }
 
-export const jetpackConfigHas = key => {
+const jetpackConfigHas = key => {
 	return jetpackConfig.hasOwnProperty( key );
 };
 
-export const jetpackConfigGet = key => {
+const jetpackConfigGet = key => {
 	if ( ! jetpackConfigHas( key ) ) {
 		throw (
 			'This app requires the "' +
@@ -24,4 +24,13 @@ export const jetpackConfigGet = key => {
 		);
 	}
 	return jetpackConfig[ key ];
+};
+
+// Note: For this cjs module to be used with named exports in an mjs context, modules.exports
+// needs to contain only simple variables like `a` or `a: b`. Define anything more complex
+// as a variable above, then use the variable here.
+// @see https://github.com/nodejs/node/blob/master/deps/cjs-module-lexer/README.md#exports-object-assignment
+module.exports = {
+	jetpackConfigHas,
+	jetpackConfigGet,
 };
