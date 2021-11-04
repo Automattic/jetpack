6756c4f7edbfdc4df0c87d24171a110e461ad3e8
Daniel Post <daniel@danielpost.com>
2021-11-04 17:35:56 +0100

    Remove extra space in docblock
    Co-authored-by: Steve D. <33553323+sdixon194@users.noreply.github.com>

--- projects/plugins/jetpack/tests/php/test_class.functions.opengraph.php
+++ projects/plugins/jetpack/tests/php/test_class.functions.opengraph.php
@@ -237,7 +237,7 @@ class WP_Test_Functions_OpenGraph extends Jetpack_Attachment_Test_Case {
 	 *
 	 * @author automattic
 	 * @covers ::jetpack_og_get_image
-	 * @since  $$next-version$$
+	 * @since $$next-version$$
 	 */
 	public function test_jetpack_og_get_image_alt_text_default() {
 		$this->go_to( get_permalink( $this->icon_id ) );
