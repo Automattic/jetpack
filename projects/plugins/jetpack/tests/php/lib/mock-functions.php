4aa63c473c2168fb59233e52faf347c5b7493ab9
Konstantin Obenland <obenland@gmx.de>
2021-11-04 12:07:48 -0700

    Tests: Collect mocked functions in
    Since we're starting to accumulate more mocked functions, let's collect them all in a single file.

---
+++ projects/plugins/jetpack/tests/php/lib/mock-functions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Contains functions that need to be (conditionally) mocked.
+ *
+ * @package automattic/jetpa
+ */
+
+if ( ! function_exists( 'wp_cache_is_enabled' ) ) {
+	/**
+	 * "Mocking" function so that it exists and Automattic\Jetpack\Sync\Actions will load Automattic\Jetpack\Sync\Modules\WP_Super_Cache.
+	 */
+	function wp_cache_is_enabled() {}
+}
+
+if ( ! function_exists( 'gutenberg_is_fse_theme' ) ) {
+	/**
+	 * Assume all themes are an FSE theme.
+	 */
+	function gutenberg_is_fse_theme() {
+		return true;
+	}
+}
+
