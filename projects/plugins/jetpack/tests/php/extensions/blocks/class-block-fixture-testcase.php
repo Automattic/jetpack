c4c543432e5fbf666530dc9caf48ec0f720d29ff
Gary <gary@pento.net>
2021-11-05 15:50:16 +1100

    Try running the block fixtures through KSES.

--- projects/plugins/jetpack/tests/php/extensions/blocks/class-block-fixture-testcase.php
+++ projects/plugins/jetpack/tests/php/extensions/blocks/class-block-fixture-testcase.php
@@ -76,4 +76,44 @@ abstract class Jetpack_Block_Fixture_TestCase extends WP_UnitTestCase {
 		}
 		// phpcs:enable WordPress.WP.AlternativeFunctions
 	}
+
+	/**
+	 * Tests that running the serialised block content through KSES doesn't cause the
+	 * HTML to change.
+	 *
+	 * @dataProvider data_block_fixtures
+	 */
+	function test_kses_doesnt_change_fixtures( $block, $filename ) {
+
+		// KSES doesn't allow data: URLs, so we need to replace any of them in fixtures.
+		// $block = preg_replace( "/src=['\"]data:[^'\"]+['\"]/", 'src="https://wordpress.org/foo.jpg"', $block );
+		// $block = preg_replace( "/href=['\"]data:[^'\"]+['\"]/", 'href="https://wordpress.org/foo.jpg"', $block );
+		// $block = preg_replace( '/url\(data:[^)]+\)/', 'url(https://wordpress.org/foo.jpg)', $block );
+
+		$kses_block = wp_kses_post( $block );
+
+		// KSES adds a space at the end of self-closing tags, add it to the original to match.
+		$block = preg_replace( '|([^ ])/>|', '$1 />', $block );
+
+		// KSES removes the last semi-colon from style attributes, remove it from the original to match.
+		$block = preg_replace( '/style="([^"]*);"/', 'style="$1"', $block );
+
+		$this->assertSame( $block, $kses_block, "Failed to match $filename" );
+	}
+
+	function data_block_fixtures() {
+		$fixtures_path = "extensions/blocks/*/test/fixtures/";
+		$file_pattern  = '*.serialized.html';
+		$files         = glob( JETPACK__PLUGIN_DIR . $fixtures_path . $file_pattern );
+
+		$data = array();
+
+		foreach ( $files as $file ) {
+			$filename = basename( $file );
+			$block    = file_get_contents( $file );
+			$data[]   = array( $block, $filename );
+		}
+
+		return $data;
+	}
 }
