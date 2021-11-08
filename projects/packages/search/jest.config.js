efdb64e537fd7b165f8b93fd009abc4bb8118295
Jasper Kang <jasper.kang@automattic.com>
2021-11-08 14:08:29 +1300

    added jest config

---
+++ projects/packages/search/jest.config.js
@@ -0,0 +1,23 @@
+/**
+ * External dependencies
+ */
+const path = require( 'path' );
+
+module.exports = {
+	preset: '@automattic/calypso-build',
+	testEnvironment: 'jsdom',
+	roots: [ '<rootDir>/extensions/', '<rootDir>/modules/search/instant-search' ],
+	transform: {
+		'\\.[jt]sx?$': path.join( __dirname, 'tests', 'jest-extensions-babel-transform' ),
+		'\\.(gif|jpg|jpeg|png|svg|scss|sass|css)$': require.resolve(
+			'@automattic/calypso-build/jest/transform/asset'
+		),
+	},
+	coverageDirectory: 'coverage/extensions',
+	setupFiles: [ '<rootDir>/tests/jest-globals.js' ],
+	testPathIgnorePatterns: [ 'node_modules', 'extensions/shared/test/block-fixtures.js' ],
+	moduleNameMapper: {
+		'tiny-lru/lib/tiny-lru.esm$':
+			'<rootDir>/modules/search/instant-search/lib/test-helpers/tiny-lru.mock.js',
+	},
+};
