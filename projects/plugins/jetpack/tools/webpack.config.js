242e265fb041259becd909fc65d8c24cc43f61c5
leogermani <leogermani@gmail.com>
2021-11-08 17:11:56 -0300

    add consumer_slug js config to jetpack

--- projects/plugins/jetpack/tools/webpack.config.js
+++ projects/plugins/jetpack/tools/webpack.config.js
@@ -94,6 +94,12 @@ module.exports = [
 			...jetpackWebpackConfig.DependencyExtractionPlugin( { injectPolyfill: true } ),
 			new NodePolyfillPlugin(),
 		],
+		externals: {
+			...sharedWebpackConfig.externals,
+			jetpackConfig: JSON.stringify( {
+				consumer_slug: 'jetpack',
+			} ),
+		},
 	},
 	{
 		...sharedWebpackConfig,
