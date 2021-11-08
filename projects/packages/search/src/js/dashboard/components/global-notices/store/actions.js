e62ec80c579bfb72c7271889c68bb52eedfe9a13
Jasper Kang <jasper.kang@automattic.com>
2021-11-08 16:21:46 +1300

    set update duration to 30s

--- projects/packages/search/src/js/dashboard/components/global-notices/store/actions.js
+++ projects/packages/search/src/js/dashboard/components/global-notices/store/actions.js
@@ -4,12 +4,17 @@
 import { uniqueId } from 'lodash';
 import { __ } from '@wordpress/i18n';
 
-/**
- * @returns {object} - an action to set network busy.
- */
 export const CREATE_NOTICE = 'CREATE_NOTICE';
 export const REMOVE_NOTICE = 'REMOVE_NOTICE';
 
+/**
+ * Create global notice
+ *
+ * @param {*} status - success, error, info or warning.
+ * @param {*} text - the text to show.
+ * @param {*} options - Options.
+ * @returns {object} - action object.
+ */
 export function createNotice( status, text, options = {} ) {
 	const notice = {
 		id: options.id || uniqueId(),
@@ -27,17 +32,23 @@ export function createNotice( status, text, options = {} ) {
 	};
 }
 
+/**
+ * Remove notice by ID
+ *
+ * @param {*} noticeId - noticeID.
+ * @returns {object} - action object.
+ */
+export function removeNotice( noticeId ) {
+	return { type: REMOVE_NOTICE, notice: { id: noticeId } };
+}
+
 export const successNotice = createNotice.bind( null, 'is-success' );
 export const errorNotice = createNotice.bind( null, 'is-error' );
 export const infoNotice = createNotice.bind( null, 'is-info' );
 export const warningNotice = createNotice.bind( null, 'is-warning' );
-export const updatingNotice = ( text = __( 'Updating settings...' ) ) =>
-	createNotice( 'is-info', text, { duration: 2000, id: 'search-updating-settings' } );
+export const updatingNotice = ( text = __( 'Updating settings…' ) ) =>
+	createNotice( 'is-info', text, { duration: 30000, id: 'search-updating-settings' } );
 export const removeUpdatingNotice = () => removeNotice( 'search-updating-settings' );
-
-export function removeNotice( noticeId ) {
-	return { type: REMOVE_NOTICE, notice: { id: noticeId } };
-}
 
 export default {
 	createNotice,
