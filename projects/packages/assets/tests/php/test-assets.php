1506603cea32ec8ee6bf5e5035b949c8d008dced
Brad Jorsch <brad.jorsch@automattic.com>
2021-11-08 17:55:45 -0500

    assets: Add `wp_register_script` wrapper
    Our Webpack-built scripts tend to need a bunch of boilerplate:
     - A call to `Assets::get_file_url_for_environment()` for possible
       debugging.
     - A call to `wp_register_style()` for extracted CSS. Possibly with
       detection of RTL.
     - Loading of dependencies and version provided by
       `@wordpress/dependency-extraction-webpack-plugin`.
     - Avoiding WPCom's broken minifier.
    
    The wrapper handles all of that.

--- projects/packages/assets/tests/php/test-assets.php
+++ projects/packages/assets/tests/php/test-assets.php
@@ -14,57 +14,10 @@ use Brain\Monkey\Functions;
 use PHPUnit\Framework\TestCase;
 
 /**
- * Retrieves a URL within the plugins or mu-plugins directory.
- *
- * @param string $path        Extra path appended to the end of the URL, including the relative directory if $plugin is supplied.
- * @param string $plugin_path A full path to a file inside a plugin or mu-plugin.
- *                            The URL will be relative to its directory.
- *                            Typically this is done by passing __FILE__ as the argument.
- */
-function plugins_url( $path, $plugin_path ) {
-	if ( strpos( $plugin_path, 'test-package.php' ) ) {
-		return 'http://www.example.com/wp-content/plugins/jetpack/packages/test-package/' . $path;
-	}
-
-	return 'http://www.example.com//wp-content/plugins/jetpack/' . $path;
-}
-
-/**
- * Enqueue a script.
- *
- * Registers the script if $src provided (does NOT overwrite), and enqueues it.
- *
- * @param string           $handle    Name of the script. Should be unique.
- * @param string           $src       Full URL of the script, or path of the script relative to the WordPress root directory.
- *                                    Default empty.
- * @param string[]         $deps      Optional. An array of registered script handles this script depends on. Default empty array.
- * @param string|bool|null $ver       Optional. String specifying script version number, if it has one, which is added to the URL
- *                                    as a query string for cache busting purposes. If version is set to false, a version
- *                                    number is automatically added equal to current installed WordPress version.
- *                                    If set to null, no version is added.
- * @param bool             $in_footer Optional. Whether to enqueue the script before </body> instead of in the <head>.
- *                                    Default 'false'.
- */
-function wp_enqueue_script( $handle, $src = '', $deps = array(), $ver = false, $in_footer = false ) {
-	$GLOBALS['_was_called_wp_enqueue_script'][] = array( $handle, $src, $deps, $ver, $in_footer );
-}
-
-/**
- * A wrapper for PHP's parse_url()
- *
- * @param string $url       The URL to parse.
- * @param int    $component The specific component to retrieve. Use one of the PHP
- *                          predefined constants to specify which one.
- *                          Defaults to -1 (= return all parts as an array).
- */
-function wp_parse_url( $url, $component = -1 ) { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
-	return parse_url( $url, $component ); // phpcs:ignore WordPress.WP.AlternativeFunctions.parse_url_parse_url
-}
-
-/**
  * Assets test suite.
  */
 class AssetsTest extends TestCase {
+	use \Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 	use \Yoast\PHPUnitPolyfills\Polyfills\AssertStringContains;
 
 	/**
@@ -74,9 +27,25 @@ class AssetsTest extends TestCase {
 	 */
 	public function set_up() {
 		Monkey\setUp();
-		$plugin_file = dirname( dirname( dirname( dirname( __DIR__ ) ) ) ) . '/jetpack.php';
-		Jetpack_Constants::set_constant( 'JETPACK__PLUGIN_FILE', $plugin_file );
+		$plugin_dir = dirname( dirname( dirname( dirname( __DIR__ ) ) ) ) . '/';
+		Jetpack_Constants::set_constant( 'JETPACK__PLUGIN_FILE', $plugin_dir . 'jetpack.php' );
 
+		Functions\stubs(
+			array(
+				'wp_parse_url'  => 'parse_url',
+				'add_query_arg' => function ( ...$args ) {
+					$this->assertCount( 3, $args );
+					list( $k, $v, $url ) = $args;
+					$url .= ( strpos( $url, '?' ) === false ? '?' : '&' ) . rawurlencode( $k ) . '=' . rawurlencode( $v );
+					return $url;
+				},
+				'plugins_url'   => function ( $path, $plugin_path ) use ( $plugin_dir ) {
+					$plugin_path = dirname( $plugin_path );
+					$this->stringStartsWith( $plugin_dir, $plugin_path );
+					return 'http://www.example.com/wp-content/plugins/jetpack/' . substr( $plugin_path, strlen( $plugin_dir ) ) . '/' . $path;
+				},
+			)
+		);
 	}
 
 	/**
@@ -86,7 +55,12 @@ class AssetsTest extends TestCase {
 	 */
 	public function tear_down() {
 		Monkey\tearDown();
-		$GLOBALS['_was_called_wp_enqueue_script'] = array();
+
+		// Clear the instance.
+		$rc = new \ReflectionClass( Assets::class );
+		$rp = $rc->getProperty( 'instance' );
+		$rp->setAccessible( true );
+		$rp->setValue( null );
 	}
 
 	/**
@@ -197,7 +171,7 @@ class AssetsTest extends TestCase {
 	public function get_file_url_for_environment_package_path_data_provider() {
 		$min_path     = 'src/js/test.min.js';
 		$non_min_path = 'src/js/test.js';
-		$package_path = '/var/html/wp-content/plugins/jetpack/packages/test-package/test-package.php';
+		$package_path = dirname( dirname( dirname( dirname( __DIR__ ) ) ) ) . '/packages/test-package/test-package.php';
 
 		return array(
 			'script-debug-true'  => array(
@@ -221,23 +195,15 @@ class AssetsTest extends TestCase {
 	}
 
 	/**
-	 * Test that enqueue_async_script calls adds the script_loader_tag filter
-	 */
-	public function test_enqueue_async_script_adds_script_loader_tag_filter() {
-		Assets::enqueue_async_script( 'handle', 'minpath.js', 'path.js', array(), '123', true );
-		$asset_instance = Assets::instance();
-		self::assertEquals( 10, (int) has_filter( 'script_loader_tag', array( $asset_instance, 'script_add_async' ) ) );
-	}
-
-	/**
 	 * Test that enqueue_async_script calls wp_enqueue_script
 	 */
 	public function test_enqueue_async_script_calls_wp_enqueue_script() {
+		Functions\expect( 'wp_enqueue_script' )
+			->once()
+			->with( 'handle', Assets::get_file_url_for_environment( '/minpath.js', '/path.js' ), array(), '123', true );
 		Assets::enqueue_async_script( 'handle', '/minpath.js', '/path.js', array(), '123', true );
-		$this->assertEquals(
-			$GLOBALS['_was_called_wp_enqueue_script'],
-			array( array( 'handle', Assets::get_file_url_for_environment( '/minpath.js', '/path.js' ), array(), '123', true ) )
-		);
+		$asset_instance = Assets::instance();
+		$this->assertEquals( 10, (int) has_filter( 'script_loader_tag', array( $asset_instance, 'script_add_async' ) ) );
 	}
 
 	/**
@@ -299,6 +265,315 @@ class AssetsTest extends TestCase {
 				'http://wordpress.com/i/blank.jpg',
 				'.wp.com/i/blank.jpg',
 				'https://s-ssl.wordpress.com/i/blank.jpg',
+			),
+		);
+	}
+
+	/**
+	 * Test `normalize_path`.
+	 *
+	 * @dataProvider provide_normalize_path
+	 * @param string $path Path.
+	 * @param string $expect Expect.
+	 */
+	public function test_normalize_path( $path, $expect ) {
+		$this->assertSame( $expect, Assets::normalize_path( $path ) );
+	}
+
+	/** Data provider for `test_normalize_path` */
+	public static function provide_normalize_path() {
+		return array(
+			array( 'foo/bar/baz', 'foo/bar/baz' ),
+			array( '/foo/bar/baz', '/foo/bar/baz' ),
+			array( '/foo/../bar/baz', '/bar/baz' ),
+			array( '/foo//../bar/baz', '/bar/baz' ),
+			array( './foo', 'foo' ),
+			array( '../foo/bar', 'foo/bar' ),
+			array( '/foo/bar/bar/../../baz', '/foo/baz' ),
+			array( '/foo/../../../bar/baz', '/bar/baz' ),
+			array( '/foo/bar/./baz', '/foo/bar/baz' ),
+			array( '/foo/bar/./../baz', '/foo/baz' ),
+			array( 'http://example.com', 'http://example.com' ),
+			array( 'http://example.com?', 'http://example.com?' ),
+			array( 'http://example.com/foo/bar/../baz', 'http://example.com/foo/baz' ),
+			array( 'http://example.com/foo/../../bar/', 'http://example.com/bar' ),
+			array( 'http://example.com/foo/bar/../baz?path=a/../b', 'http://example.com/foo/baz?path=a/../b' ),
+			array( 'http://user@example.com/foo/../../bar/', 'http://user@example.com/bar' ),
+			array( 'http://user:pass@example.com:81/foo/.#frag', 'http://user:pass@example.com:81/foo#frag' ),
+		);
+	}
+
+	/**
+	 * Test `register_script` and `enqueue_script`.
+	 *
+	 * @dataProvider provide_register_script
+	 * @param array $args Arguments for `register_script`.
+	 * @param array $expect Map of function to expected arguments.
+	 * @param array $extra Map of additional settings:
+	 *   - is_script_debug: Value for `SCRIPT_DEBUG`.
+	 *   - is_rtl: Value for `is_rtl()`.
+	 *   - exception: Exception expected to be thrown.
+	 */
+	public function test_register_script( $args, $expect, $extra = array() ) {
+		Functions\stubs(
+			array(
+				'filemtime'   => function ( $v ) {
+					return crc32( $v );
+				},
+				'wp_style_is' => function ( $handle, $list = 'enqueued' ) {
+					return ( 'wp-components' === $handle && 'registered' === $list );
+				},
+			)
+		);
+		foreach ( $expect as $func => $with ) {
+			Functions\expect( $func )
+				->once()
+				->with( ...$with );
+		}
+
+		if ( isset( $extra['is_script_debug'] ) ) {
+			Constants::set_constant( 'SCRIPT_DEBUG', $extra['is_script_debug'] );
+		}
+		if ( isset( $extra['is_rtl'] ) ) {
+			Functions\when( 'is_rtl' )->justReturn( $extra['is_rtl'] );
+		}
+		if ( isset( $extra['exception'] ) ) {
+			$this->expectException( get_class( $extra['exception'] ) );
+			$this->expectExceptionMessage( $extra['exception']->getMessage() );
+		}
+		if ( isset( $extra['enqueue'] ) ) {
+			$obj = $this->getMockBuilder( \stdClass::class )
+				->setMethods( array( 'get_data' ) )
+				->getMock();
+			$obj->expects( $this->once() )->method( 'get_data' )->with( ...$extra['enqueue'][0] )->willReturn( $extra['enqueue'][1] );
+			Functions\expect( 'wp_scripts' )->once()->andReturn( $obj );
+		}
+
+		Assets::register_script( ...$args );
+
+		// Check whether $options['async'] was honored.
+		$rc = new \ReflectionClass( Assets::instance() );
+		$rp = $rc->getProperty( 'defer_script_handles' );
+		$rp->setAccessible( true );
+		$this->assertSame(
+			isset( $extra['async'] ) ? $extra['async'] : array(),
+			$rp->getValue( Assets::instance() )
+		);
+	}
+
+	/** Data provider for `test_register_script` */
+	public static function provide_register_script() {
+		$url_base = 'http://www.example.com/wp-content/plugins/jetpack/packages/assets/tests/php/test-assets-files';
+		return array(
+			'Single JS file'                            => array(
+				array( 'single-file', 'test-assets-files/single-js-file.js', __FILE__ ),
+				array(
+					'wp_register_script' => array(
+						'single-file',
+						"$url_base/single-js-file.js?minify=false",
+						array(),
+						231206287,
+						false,
+					),
+					'wp_script_add_data' => array( 'single-file', 'Jetpack::Assets::hascss', false ),
+				),
+			),
+			'Single JS file, override version and deps' => array(
+				array(
+					'single-file',
+					'test-assets-files/single-js-file.js',
+					__FILE__,
+					array(
+						'version'      => 'foobar',
+						'dependencies' => array( 'xyz' ),
+					),
+				),
+				array(
+					'wp_register_script' => array(
+						'single-file',
+						"$url_base/single-js-file.js?minify=false",
+						array( 'xyz' ),
+						'foobar',
+						false,
+					),
+					'wp_script_add_data' => array( 'single-file', 'Jetpack::Assets::hascss', false ),
+				),
+			),
+
+			'JS and CSS'                                => array(
+				array( 'handle', 'test-assets-files/js-and-css.js', __FILE__ ),
+				array(
+					'wp_register_script' => array(
+						'handle',
+						"$url_base/js-and-css.js?minify=false",
+						array( 'wp-polyfill' ),
+						'ver-from-js-and-css',
+						false,
+					),
+					'wp_register_style'  => array( 'handle', "$url_base/js-and-css.css?minify=false", array(), 'ver-from-js-and-css', 'all' ),
+					'wp_script_add_data' => array( 'handle', 'Jetpack::Assets::hascss', true ),
+				),
+				array( 'is_rtl' => false ),
+			),
+			'JS and CSS, override version, deps, minify, is_rtl, in_footer, css_deps, and media' => array(
+				array(
+					'handle',
+					'test-assets-files/js-and-css.js',
+					__FILE__,
+					array(
+						'version'          => 'foobaz',
+						'in_footer'        => true,
+						'minify'           => null,
+						'media'            => 'screen',
+						'dependencies'     => array( 'qwerty', 'uiop' ),
+						'css_dependencies' => array( 'asdf' ),
+					),
+				),
+				array(
+					'wp_register_script' => array(
+						'handle',
+						"$url_base/js-and-css.js",
+						array( 'wp-polyfill', 'qwerty', 'uiop' ),
+						'foobaz',
+						true,
+					),
+					'wp_register_style'  => array( 'handle', "$url_base/js-and-css.css", array( 'asdf' ), 'foobaz', 'screen' ),
+					'wp_script_add_data' => array( 'handle', 'Jetpack::Assets::hascss', true ),
+				),
+				array( 'is_rtl' => true ),
+			),
+
+			'Everything'                                => array(
+				array( 'handle', 'test-assets-files/everything.js', __FILE__, array( 'nonmin_path' => 'test-assets-files/everything.src.js' ) ),
+				array(
+					'wp_register_script' => array(
+						'handle',
+						"$url_base/everything.js?minify=false",
+						array( 'wp-polyfill', 'wp-components', 'wp-i18n' ),
+						'ver-from-everything',
+						false,
+					),
+					'wp_register_style'  => array( 'handle', "$url_base/everything.css?minify=false", array( 'wp-components' ), 'ver-from-everything', 'all' ),
+					'wp_script_add_data' => array( 'handle', 'Jetpack::Assets::hascss', true ),
+				),
+				array( 'is_rtl' => false ),
+			),
+			'Everything, override version, deps, css_deps, minify, is_rtl, in_footer, is_script_debug, and media' => array(
+				array(
+					'handle',
+					'test-assets-files/everything.js',
+					__FILE__,
+					array(
+						'nonmin_path'      => 'test-assets-files/everything.src.js',
+						'version'          => 'foobaz',
+						'in_footer'        => true,
+						'minify'           => true,
+						'media'            => 'screen',
+						'dependencies'     => array( 'qwerty', 'uiop' ),
+						'css_dependencies' => array( 'asdf' ),
+					),
+				),
+				array(
+					'wp_register_script' => array(
+						'handle',
+						"$url_base/everything.src.js?minify=true",
+						array( 'wp-polyfill', 'wp-components', 'wp-i18n', 'qwerty', 'uiop' ),
+						'foobaz',
+						true,
+					),
+					'wp_register_style'  => array( 'handle', "$url_base/everything.rtl.css?minify=true", array( 'wp-components', 'asdf' ), 'foobaz', 'screen' ),
+					'wp_script_add_data' => array( 'handle', 'Jetpack::Assets::hascss', true ),
+				),
+				array(
+					'is_script_debug' => true,
+					'is_rtl'          => true,
+				),
+			),
+
+			'Override paths'                            => array(
+				array(
+					'single-file',
+					'test-assets-files/single-js-file.js',
+					__FILE__,
+					array(
+						'css_path'   => 'test-assets-files/everything.css',
+						'asset_path' => 'test-assets-files/js-and-css.asset.php',
+					),
+				),
+				array(
+					'wp_register_script' => array(
+						'single-file',
+						"$url_base/single-js-file.js?minify=false",
+						array( 'wp-polyfill' ),
+						'ver-from-js-and-css',
+						false,
+					),
+					'wp_register_style'  => array( 'single-file', "$url_base/everything.rtl.css?minify=false", array(), 'ver-from-js-and-css', 'all' ),
+					'wp_script_add_data' => array( 'single-file', 'Jetpack::Assets::hascss', true ),
+				),
+				array( 'is_rtl' => true ),
+			),
+
+			'Async'                                     => array(
+				array( 'single-file', 'test-assets-files/single-js-file.js', __FILE__, array( 'async' => true ) ),
+				array(
+					'wp_register_script' => array(
+						'single-file',
+						"$url_base/single-js-file.js?minify=false",
+						array(),
+						231206287,
+						false,
+					),
+					'wp_script_add_data' => array( 'single-file', 'Jetpack::Assets::hascss', false ),
+				),
+				array( 'async' => array( 'single-file' ) ),
+			),
+
+			'Enqueue'                                   => array(
+				array( 'single-file', 'test-assets-files/single-js-file.js', __FILE__, array( 'enqueue' => true ) ),
+				array(
+					'wp_register_script' => array(
+						'single-file',
+						"$url_base/single-js-file.js?minify=false",
+						array(),
+						231206287,
+						false,
+					),
+					'wp_script_add_data' => array( 'single-file', 'Jetpack::Assets::hascss', false ),
+					'wp_enqueue_script'  => array( 'single-file' ),
+				),
+				array( 'enqueue' => array( array( 'single-file', 'Jetpack::Assets::hascss' ), false ) ),
+			),
+			'Enqueue, with CSS'                         => array(
+				array( 'everything', 'test-assets-files/everything.js', __FILE__, array( 'enqueue' => true ) ),
+				array(
+					'wp_register_script' => array(
+						'everything',
+						"$url_base/everything.js?minify=false",
+						array( 'wp-polyfill', 'wp-components', 'wp-i18n' ),
+						'ver-from-everything',
+						false,
+					),
+					'wp_register_style'  => array( 'everything', "$url_base/everything.css?minify=false", array( 'wp-components' ), 'ver-from-everything', 'all' ),
+					'wp_script_add_data' => array( 'everything', 'Jetpack::Assets::hascss', true ),
+					'wp_enqueue_script'  => array( 'everything' ),
+					'wp_enqueue_style'   => array( 'everything' ),
+				),
+				array(
+					'is_rtl'  => false,
+					'enqueue' => array( array( 'everything', 'Jetpack::Assets::hascss' ), true ),
+				),
+			),
+
+			'Bad path'                                  => array(
+				array( 'single-file', 'test-assets-files/single-js-file.jsx', __FILE__ ),
+				array(),
+				array( 'exception' => new \InvalidArgumentException( '$path must end in ".js"' ) ),
+			),
+			'Bad css_path'                              => array(
+				array( 'single-file', 'test-assets-files/single-js-file.js', __FILE__, array( 'css_path' => 'foo.js' ) ),
+				array(),
+				array( 'exception' => new \InvalidArgumentException( '$options[\'css_path\'] must end in ".css"' ) ),
 			),
 		);
 	}
