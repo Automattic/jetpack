b02a917735d5be82fd7e37d0c8f762bf3973b4fe
Kim Brown <50059399+kbrown9@users.noreply.github.com>
2021-11-10 14:33:17 -0500

    Sync: allow plugins to adjust the sync data settings.

--- projects/packages/sync/src/class-main.php
+++ projects/packages/sync/src/class-main.php
@@ -46,6 +46,16 @@ class Main {
 	}
 
 	/**
+	 * Sets the Sync data settings.
+	 *
+	 * @param array $data_settings An array containing the Sync data options. An empty array indicates that the default
+	 *                             values will be used for all Sync data.
+	 */
+	public static function set_sync_data_options( $data_settings = array() ) {
+		( new Data_Settings() )->add_settings_list( $data_settings );
+	}
+
+	/**
 	 * Initialize the main sync actions.
 	 *
 	 * @action plugins_loaded
