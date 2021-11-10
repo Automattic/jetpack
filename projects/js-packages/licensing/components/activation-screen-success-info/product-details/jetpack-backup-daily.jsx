4d044778fb15dff8cf3d1754b4b987d63df6ddb6
Caleb Bauermeister <cbauerman@gmail.com>
2021-11-10 14:13:58 -0800

    Add new `licensing` js package

---
+++ projects/js-packages/licensing/components/activation-screen-success-info/product-details/jetpack-backup-daily.jsx
@@ -0,0 +1,44 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { createInterpolateElement } from '@wordpress/element';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+/**
+ * The Jetpack Backup Daily Product Details component.
+ *
+ * @param {object} props -- The properties.
+ * @param {number} props.dashboardUrl -- The url that links to the site dashboard
+ * @returns {React.Component} The `JetpackBackupDailyProductDetails` component.
+ */
+const JetpackBackupDailyProductDetails = props => {
+	const { dashboardUrl } = props;
+	return (
+		<div>
+			<h1>
+				{ __( 'Your Jetpack Daily Backup is active!', 'jetpack' ) }{ ' ' }
+				{ String.fromCodePoint( 0x1f389 ) }
+				{ /* Celebration emoji 🎉 */ }
+			</h1>
+			<p>
+				{ createInterpolateElement(
+					__(
+						'You can see your backups, restore your site on <a>cloud.jetpack.com</a>. If you ever lose access to your site, you can restore it there.',
+						'jetpack'
+					),
+					{
+						a: <a href={ dashboardUrl } />,
+					}
+				) }
+			</p>
+		</div>
+	);
+};
+
+JetpackBackupDailyProductDetails.PropTypes = {
+	dashboardUrl: PropTypes.string,
+};
+
+export default JetpackBackupDailyProductDetails;
