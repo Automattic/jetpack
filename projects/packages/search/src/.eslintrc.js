282d6762bb1ef81ae614b0853d4649f34ebe0d34
Jason Moon <jason.moon@automattic.com>
2021-11-10 17:22:29 -0700

    Move eslint config

--- projects/packages/search/src/.eslintrc.js
+++ projects/packages/search/src/.eslintrc.js
@@ -2,16 +2,21 @@ module.exports = {
 	// Use root level ESlint configuration.
 	// JavaScript files inside this folder are meant to be transpiled by Webpack.
 	root: true,
-	extends: [ '../../.eslintrc.js' ],
+	extends: [ '../../../../.eslintrc.react.js' ],
+	parserOptions: {
+		babelOptions: {
+			configFile: require.resolve( './babel.config.js' ),
+		},
+	},
 	rules: {
 		'jsdoc/check-tag-names': [ 1, { definedTags: [ 'jsx' ] } ],
 		'react/jsx-no-bind': 0,
 	},
 	overrides: [
 		{
-			files: './customberg/**/*',
+			files: './src/customberg/**/*',
 			rules: {
-				// Uses @wordpress/babel-preset-default, which auto-imports React as necessary.
+				// Customberg uses @wordpress/babel-preset-default, which auto-imports React as necessary.
 				'react/react-in-jsx-scope': 0,
 			},
 		},
