545a62416d03f261e4dc6caa41393dc09cd03e35
Sérgio Gomes <mail@sgomes.com>
2021-11-12 16:24:26 +0000

    Move ESLint configuration to module root

--- projects/plugins/jetpack/modules/.eslintrc.js
+++ projects/plugins/jetpack/modules/.eslintrc.js
@@ -1,39 +1,31 @@
 module.exports = {
 	root: true,
-	plugins: [ 'es5' ],
-	extends: [ 'eslint:recommended', 'plugin:es5/no-es2015' ],
+	extends: [ 'eslint:recommended' ],
 	env: {
 		browser: true,
-		jquery: true,
+		es6: true,
 	},
 	globals: {
-		_: false,
-		Backbone: false,
-		// jQuery: false,
 		wp: false,
 	},
 	parserOptions: {
-		ecmaVersion: 5,
+		ecmaVersion: 2019,
 	},
 	rules: {
-		// TODO: maybe fix the above warnings
 		eqeqeq: 'warn',
 		curly: 'warn',
 		'no-console': 'warn',
 		'no-empty': 'warn',
 		'no-unused-vars': 'warn',
-		'es5/no-es6-methods': 0, // TODO: maybe disable it on line/file basis
 		'no-useless-escape': 0,
 		'no-control-regex': 0,
 		'no-unused-expressions': 'warn',
 
-		'es5/no-block-scoping': [ 'error' ],
 		'no-caller': 'error',
 		'no-cond-assign': [ 'error', 'except-parens' ],
 		'no-eq-null': 'error',
 		'no-irregular-whitespace': 'error',
 		'no-trailing-spaces': 'error',
-		// 'one-var': [ 'error', 'always' ],
 		quotes: [ 'warn', 'single', { avoidEscape: true } ],
 		'wrap-iife': [ 'error', 'any' ],
 
