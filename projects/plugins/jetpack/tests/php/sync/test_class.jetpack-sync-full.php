771a2b1abd11038bc3914210575a984dafa50d45
Kim Brown <50059399+kbrown9@users.noreply.github.com>
2021-11-12 13:14:22 -0500

    [not verified] Remove another unnecessary unit test change

--- projects/plugins/jetpack/tests/php/sync/test_class.jetpack-sync-full.php
+++ projects/plugins/jetpack/tests/php/sync/test_class.jetpack-sync-full.php
@@ -1508,7 +1508,7 @@ class WP_Test_Jetpack_Sync_Full extends WP_Test_Jetpack_Sync_Base {
 		$this->full_sync->start( array( 'users' => 'initial' ) );
 		$this->sender->do_full_sync();
 		$this->assertEquals( 3, $this->server_replica_storage->user_count() );
-		// Finally, let's make sure that the initial sync method actually invokes our initial sync user config.
+		// finally, let's make sure that the initial sync method actually invokes our initial sync user config
 		Actions::do_initial_sync( '4.2', '4.1' );
 		$current_user = wp_get_current_user();
 
