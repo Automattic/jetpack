1355b11c15c7b166e3000255a341092992509c43
jboland88 <18016357+jboland88@users.noreply.github.com>
2021-11-15 15:02:25 -0500

    [not verified] Update survey submission method

--- projects/js-packages/connection/components/disconnect-survey/index.jsx
+++ projects/js-packages/connection/components/disconnect-survey/index.jsx
@@ -148,6 +148,7 @@ const DisconnectSurvey = props => {
 						type="text"
 						value={ customResponse }
 						onChange={ handleCustomResponse }
+						max={ 1000 } // Limit response length.
 					/>
 				</p>
 			</SurveyChoice>
