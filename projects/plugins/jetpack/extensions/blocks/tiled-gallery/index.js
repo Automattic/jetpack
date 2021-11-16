017ee696a6b556eb6522c9ee45b0e3960f8193bb
Siobhan Bamber <siobhan@automattic.com>
2021-11-16 12:18:57 +0000

    [RNMobile] [WIP] Tiled Gallery: Hide inner block settings (#21694)
    This PR leverages the `__experimentalHideChildBlockControls` block supports introduced in https://github.com/WordPress/gutenberg/pull/36371 to hide the settings tab for the inner image blocks.

--- projects/plugins/jetpack/extensions/blocks/tiled-gallery/index.js
+++ projects/plugins/jetpack/extensions/blocks/tiled-gallery/index.js
@@ -228,6 +228,7 @@ export const settings = {
 		align: [ 'center', 'wide', 'full' ],
 		customClassName: false,
 		html: false,
+		__experimentalHideChildBlockControls: true,
 	},
 	title: __( 'Tiled Gallery', 'jetpack' ),
 	transforms: {
