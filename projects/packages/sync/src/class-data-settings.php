99efd20e37e398491e7005b1088f77754c1b95b0
Kim Brown <50059399+kbrown9@users.noreply.github.com>
2021-11-17 09:30:24 -0500

    Add a few unit tests and some code cleanup

--- projects/packages/sync/src/class-data-settings.php
+++ projects/packages/sync/src/class-data-settings.php
@@ -13,16 +13,56 @@ namespace Automattic\Jetpack\Sync;
 class Data_Settings {
 
 	const DATA_FILTER_DEFAULTS = array(
-		'jetpack_sync_modules'                      => array( 'Automattic\\Jetpack\\Sync\\Modules', 'DEFAULT_SYNC_MODULES' ),
-		'jetpack_sync_options_whitelist'            => array( 'Automattic\Jetpack\Sync\Defaults', 'default_options_whitelist' ),
-		'jetpack_sync_options_contentless'          => array( 'Automattic\Jetpack\Sync\Defaults', 'default_options_contentless' ),
-		'jetpack_sync_constants_whitelist'          => array( 'Automattic\Jetpack\Sync\Defaults', 'default_constants_whitelist' ),
-		'jetpack_sync_callable_whitelist'           => array( 'Automattic\Jetpack\Sync\Defaults', 'default_callable_whitelist' ),
-		'jetpack_sync_multisite_callable_whitelist' => array( 'Automattic\Jetpack\Sync\Defaults', 'default_multisite_callable_whitelist' ),
-		'jetpack_sync_post_meta_whitelist'          => array( 'Automattic\Jetpack\Sync\Defaults', 'post_meta_whitelist' ),
-		'jetpack_sync_comment_meta_whitelist'       => array( 'Automattic\Jetpack\Sync\Defaults', 'comment_meta_whitelist' ),
-		'jetpack_sync_capabilities_whitelist'       => array( 'Automattic\Jetpack\Sync\Defaults', 'default_capabilities_whitelist' ),
-		'jetpack_sync_known_importers'              => array( 'Automattic\Jetpack\Sync\Defaults', 'default_known_importers' ),
+		'jetpack_sync_modules'                      =>
+			array(
+				'class'    => 'Automattic\\Jetpack\\Sync\\Modules',
+				'constant' => 'DEFAULT_SYNC_MODULES',
+			),
+		'jetpack_sync_options_whitelist'            =>
+			array(
+				'class'    => 'Automattic\Jetpack\Sync\Defaults',
+				'property' => 'default_options_whitelist',
+			),
+		'jetpack_sync_options_contentless'          =>
+			array(
+				'class'    => 'Automattic\Jetpack\Sync\Defaults',
+				'property' => 'default_options_contentless',
+			),
+		'jetpack_sync_constants_whitelist'          =>
+			array(
+				'class'    => 'Automattic\Jetpack\Sync\Defaults',
+				'property' => 'default_constants_whitelist',
+			),
+		'jetpack_sync_callable_whitelist'           =>
+			array(
+				'class'    => 'Automattic\Jetpack\Sync\Defaults',
+				'property' => 'default_callable_whitelist',
+			),
+		'jetpack_sync_multisite_callable_whitelist' =>
+			array(
+				'class'    => 'Automattic\Jetpack\Sync\Defaults',
+				'property' => 'default_multisite_callable_whitelist',
+			),
+		'jetpack_sync_post_meta_whitelist'          =>
+			array(
+				'class'    => 'Automattic\Jetpack\Sync\Defaults',
+				'property' => 'post_meta_whitelist',
+			),
+		'jetpack_sync_comment_meta_whitelist'       =>
+			array(
+				'class'    => 'Automattic\Jetpack\Sync\Defaults',
+				'property' => 'comment_meta_whitelist',
+			),
+		'jetpack_sync_capabilities_whitelist'       =>
+			array(
+				'class'    => 'Automattic\Jetpack\Sync\Defaults',
+				'property' => 'default_capabilities_whitelist',
+			),
+		'jetpack_sync_known_importers'              =>
+			array(
+				'class'    => 'Automattic\Jetpack\Sync\Defaults',
+				'property' => 'default_known_importers',
+			),
 	);
 
 	/**
@@ -83,14 +123,13 @@ class Data_Settings {
 	private function get_default_value_for_filter( $filter ) {
 		$default_value = self::DATA_FILTER_DEFAULTS[ $filter ];
 
-		if ( 'jetpack_sync_modules' === $filter ) {
+		if ( array_key_exists( 'constant', $default_value ) ) {
 			// The modules list is a class constant.
-			$setting = constant( $default_value[0] . '::' . $default_value[1] );
-
+			$setting = constant( $default_value['class'] . '::' . $default_value['constant'] );
 		} else {
-			// The default lists are class properties.
-			$value   = $default_value[1];
-			$setting = $default_value[0]::$$value;
+			// The other default lists are class properties.
+			$property = $default_value['property'];
+			$setting  = $default_value['class']::$$property;
 		}
 
 		return $setting;
@@ -119,11 +158,11 @@ class Data_Settings {
 	 * Adds the provided data setting for the provided filter. This method handles
 	 * adding settings to data that is stored as an associative array.
 	 *
-	 * @param string $filter The filter name.
-	 * @param array  $value The data setting.
+	 * @param string $filter  The filter name.
+	 * @param array  $settings The data settings.
 	 */
-	private function add_associative_filter_setting( $filter, $value ) {
-		foreach ( $value as $key => $item ) {
+	private function add_associative_filter_setting( $filter, $settings ) {
+		foreach ( $settings as $key => $item ) {
 			if ( ! array_key_exists( $key, static::$data_settings[ $filter ] ) ) {
 				static::$data_settings[ $filter ][ $key ] = $item;
 			}
@@ -134,11 +173,11 @@ class Data_Settings {
 	 * Adds the provided data setting for the provided filter. This method handles
 	 * adding settings to data that is stored as an indexed array.
 	 *
-	 * @param string $filter The filter name.
-	 * @param array  $value The data setting.
+	 * @param string $filter  The filter name.
+	 * @param array  $settings The data settings.
 	 */
-	private function add_indexed_filter_setting( $filter, $value ) {
-		foreach ( $value as $item ) {
+	private function add_indexed_filter_setting( $filter, $settings ) {
+		foreach ( $settings as $item ) {
 			if ( ! in_array( $item, static::$data_settings[ $filter ], true ) ) {
 				static::$data_settings[ $filter ][] = $item;
 			}
@@ -146,23 +185,28 @@ class Data_Settings {
 	}
 
 	/**
-	 * Sets up the Sync data setting filters. Each filter will return the list of
-	 * data settings contained in the $data_settings property.
+	 * Sets up the Sync data setting filters.
 	 */
 	public function set_sync_data_filters() {
 		foreach ( static::$data_settings as $filter => $value ) {
-			add_filter( $filter, array( $this, 'add_sync_data_settings' ), 999, 1 );
+			add_filter( $filter, array( $this, 'add_sync_data_settings' ) );
 		}
 	}
 
 	/**
-	 * The callback function added to the sync data filters.
+	 * The callback function added to the sync data filters. Combines the list in the $data_settings property
+	 * with any non-default values from the received array.
 	 *
 	 * @param array $filtered_values The data revieved from the filter.
 	 *
 	 * @return array The data settings for the filter.
 	 */
 	public function add_sync_data_settings( $filtered_values ) {
+		if ( ! is_array( $filtered_values ) ) {
+			// Something is wrong with the input, so set it to an empty array.
+			$filtered_values = array();
+		}
+
 		$current_filter = current_filter();
 
 		if ( in_array( $current_filter, self::ASSOCIATIVE_FILTERS, true ) ) {
@@ -183,14 +227,18 @@ class Data_Settings {
 	}
 
 	/**
-	 * Resets the $data_settings property to the default values.
+	 * Sets the $data_settings property to an empty array. This is useful for testing.
 	 */
-	public function reset_data_settings_to_defaults() {
+	public function empty_data_settings() {
 		static::$data_settings = array();
-		foreach ( self::DATA_FILTER_DEFAULTS as $filter => $default_value ) {
-			// If the plugin didn't provide a data setting for this filter, use the default.
-			$setting = $this->get_default_value_for_filter( $filter );
-			$this->add_filter_setting( $filter, $setting );
-		}
+	}
+
+	/**
+	 * Returns the $data_settings property.
+	 *
+	 * @return array The data_settings property.
+	 */
+	public function get_data_settings() {
+		return static::$data_settings;
 	}
 }
