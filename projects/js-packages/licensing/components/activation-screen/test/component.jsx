5fcbe7117616f9b411e42d6141a5f2203ae35a44
Caleb Bauermeister <cbauerman@gmail.com>
2021-11-16 20:28:44 -0800

    Add test

---
+++ projects/js-packages/licensing/components/activation-screen/test/component.jsx
@@ -0,0 +1,35 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import restApi from '@automattic/jetpack-api';
+
+jest.mock( 'restApi' );
+
+/**
+ * Internal dependencies
+ */
+import ActivationScreen from '..';
+import ActivationScreenControls from '../../activation-screen-controls';
+
+describe( 'ActivationScreen', () => {
+
+	describe( 'Render the ActivationScreen with fresh props', () => {
+		const testProps = {
+			assetBaseUrl: 'jetpack.com',
+			lockImage: '/lock.png',
+			siteRawUrl: 'jetpack.com',
+			successImage: '/success.png',
+		};
+
+		const wrapper = shallow( <ActivationScreen { ...testProps } /> );
+
+		it( 'Renders ActivationScreenControls first', () => {
+			const activationScreenControls = wrapper.find( ActivationScreenControls );
+
+			expect( activationScreenControls ).to.have.lengthOf( 1 );
+		} );
+	} );
+} );
