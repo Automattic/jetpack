6d27093e8e3248d697b06c871fb14daf157057a3
Jason Moon <jason.moon@automattic.com>
2021-11-17 17:19:33 -0700

    Port instant search jest and size-limit tests

---
+++ projects/packages/search/.size-limit.js
@@ -0,0 +1,12 @@
+module.exports = [
+	{
+		path: 'dist/instant-search/jp-search-main.bundle.min.js',
+		running: false,
+		limit: '4 KiB',
+	},
+	{
+		path: 'dist/instant-search/jp-search.chunk-main-payload.*.min.js',
+		running: false,
+		limit: '50 KiB',
+	},
+];
