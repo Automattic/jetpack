520b2077ae58c879d75d4deb75665293e606201c
Jason Moon <jason.moon@automattic.com>
2021-11-18 17:45:01 -0700

    Add jest config

---
+++ projects/packages/search/jest.config.js
@@ -0,0 +1,22 @@
+/**
+ * External dependencies
+ */
+const path = require( 'path' );
+
+module.exports = {
+	testMatch: [ '<rootDir>/**/test/*.[jt]s?(x)', '!**/.eslintrc.*' ],
+	roots: [ '<rootDir>/src/instant-search' ],
+	transform: {
+		'\\.[jt]sx?$': path.join(
+			__dirname,
+			'../../plugins/jetpack/tests/jest-extensions-babel-transform'
+		),
+		'\\.(gif|jpg|jpeg|png|svg|scss|sass|css)$': path.join(
+			__dirname,
+			'../../plugins/jetpack/tests/jest-extensions-asset-stub'
+		),
+	},
+	moduleNameMapper: {
+		'tiny-lru/lib/tiny-lru.esm$': '<rootDir>/src/instant-search/lib/test-helpers/tiny-lru.mock.js',
+	},
+};
