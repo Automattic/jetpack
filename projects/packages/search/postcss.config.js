56900ed54248085b1fa8cf73bcdda6d325993f3f
Jason Moon <jason.moon@automattic.com>
2021-11-18 17:35:44 -0700

    Duplicate postcss config instead of importing

--- projects/packages/search/postcss.config.js
+++ projects/packages/search/postcss.config.js
@@ -1,2 +1,12 @@
-// Use Jetpack plugin's postcss config.
-module.exports = require( '../../plugins/jetpack/postcss.config' );
+// Uses Jetpack plugin's postcss config.
+module.exports = () => ( {
+	plugins: {
+		'postcss-custom-properties': {
+			importFrom: [ require.resolve( '@automattic/calypso-color-schemes' ) ],
+			// @TODO: Drop `preserve: false` workaround if possible
+			// See https://github.com/Automattic/jetpack/pull/13854#issuecomment-550898168
+			preserve: false,
+		},
+		autoprefixer: {},
+	},
+} );
