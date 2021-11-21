0610cceb528949b56e0de27bcf1a59e60d7fe6ee
Jasper Kang <jasper.kang@automattic.com>
2021-11-22 10:15:51 +1300

    copied config from jetpack

--- projects/packages/search/jest.config.js
+++ projects/packages/search/jest.config.js
@@ -5,19 +5,16 @@ const path = require( 'path' );
 
 module.exports = {
 	testMatch: [ '<rootDir>/**/test/*.[jt]s?(x)', '!**/.eslintrc.*' ],
-	// TODO: Enable once instant search has been migrated.
-	// roots: [ '<rootDir>/src/instant-search' ],
+	roots: [ '<rootDir>/src' ],
 	transform: {
-		'\\.[jt]sx?$': path.join(
+		'\\.[jt]sx?$': path.join( __dirname, 'tests/jest-extensions-babel-transform' ),
+		'\\.(gif|jpg|jpeg|png|svg|scss|sass|css|ttf|woff|woff2)$': path.join(
 			__dirname,
-			'../../plugins/jetpack/tests/jest-extensions-babel-transform'
-		),
-		'\\.(gif|jpg|jpeg|png|svg|scss|sass|css)$': path.join(
-			__dirname,
-			'../../plugins/jetpack/tests/jest-extensions-asset-stub'
+			'tests/jest-extensions-asset-stub'
 		),
 	},
 	moduleNameMapper: {
 		'tiny-lru/lib/tiny-lru.esm$': '<rootDir>/src/instant-search/lib/test-helpers/tiny-lru.mock.js',
 	},
+	moduleDirectories: [ 'node_modules' ],
 };
