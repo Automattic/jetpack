3d838da08849f1b1ff579592e5c06d355d4e1455
Adrian Moldovan <adim.moldovan@gmail.com>
2021-11-22 14:19:31 +0200

    Tests functional with playwright-test

--- tools/e2e-commons/config/ecosystem.config.cjs
+++ tools/e2e-commons/config/ecosystem.config.cjs
@@ -1,15 +1,12 @@
 module.exports = {
 	apps: [
 		{
-			script: __dirname + '/../bin/tunnel.js',
+			script: __dirname + '/../bin/tunnel.cjs',
 			args: 'on',
 			name: 'tunnel',
 			time: true,
 			wait_ready: true,
 			listen_timeout: 15000,
-			env: {
-				NODE_ENV: 'test',
-			},
 		},
 	],
 };
