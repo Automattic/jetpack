2fb81794137ed9ddf4c611e9a80e8ebdb0651d79
Jasper Kang <kangzjnet@gmail.com>
2021-11-26 03:09:32 +1300

    Search: migrate/add search rest API (#21584)
    * [not verified] search plan API
    
    * [not verified] phpunit tests
    
    * [not verified] added module settings api
    
    * [not verified] added private search API

--- projects/plugins/jetpack/_inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-search.php
+++ projects/plugins/jetpack/_inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-search.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack
  */
 
-use Automattic\Jetpack\Connection\Client;
+use Automattic\Jetpack\Search\REST_Controller;
 
 /**
  * Jetpack Search: Makes authenticated requests to the site search API using blog tokens.
@@ -15,11 +15,19 @@ use Automattic\Jetpack\Connection\Client;
  */
 class WPCOM_REST_API_V2_Endpoint_Search extends WP_REST_Controller {
 	/**
+	 * Forward request to controller in Search package.
+	 *
+	 * @var REST_Controller
+	 */
+	protected $controller;
+
+	/**
 	 * Constructor.
 	 */
 	public function __construct() {
-		$this->namespace = 'wpcom/v2';
-		$this->rest_base = 'search';
+		$this->namespace  = 'wpcom/v2';
+		$this->rest_base  = 'search';
+		$this->controller = new REST_Controller( defined( 'IS_WPCOM' ) && IS_WPCOM );
 
 		add_action( 'rest_api_init', array( $this, 'register_routes' ) );
 	}
@@ -32,46 +40,10 @@ class WPCOM_REST_API_V2_Endpoint_Search extends WP_REST_Controller {
 			$this->namespace,
 			$this->rest_base,
 			array(
-				array(
-					'methods'             => WP_REST_Server::READABLE,
-					'callback'            => array( $this, 'get_search_results' ),
-					'permission_callback' => 'is_user_logged_in',
-				),
+				'methods'             => WP_REST_Server::READABLE,
+				'callback'            => array( $this->controller, 'get_search_results' ),
+				'permission_callback' => 'is_user_logged_in',
 			)
-		);
-	}
-
-	/**
-	 * Returns search results for the current blog.
-	 *
-	 * @param WP_REST_Request $request The REST API request data.
-	 * @return mixed The REST API response from public-api.
-	 */
-	public function get_search_results( $request ) {
-		$is_wpcom = ( defined( 'IS_WPCOM' ) && IS_WPCOM );
-		$site_id  = $is_wpcom ? get_current_blog_id() : Jetpack_Options::get_option( 'id' );
-		if ( ! $site_id ) {
-			return new WP_Error(
-				'unavailable_site_id',
-				__( 'Sorry, something is wrong with your Jetpack connection.', 'jetpack' ),
-				403
-			);
-		}
-
-		$path    = add_query_arg(
-			$request->get_query_params(),
-			sprintf( '/sites/%d/search', absint( $site_id ) )
-		);
-		$request = Client::wpcom_json_api_request_as_blog( $path, '1.3' );
-		$body    = json_decode( wp_remote_retrieve_body( $request ) );
-		if ( 200 === wp_remote_retrieve_response_code( $request ) ) {
-			return $body;
-		}
-
-		return new WP_Error(
-			$body->error,
-			$body->message,
-			array( 'status' => wp_remote_retrieve_response_code( $request ) )
 		);
 	}
 }
