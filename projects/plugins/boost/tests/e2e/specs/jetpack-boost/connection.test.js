f6234f063528df94b0845daebf2ac8e1381350b8
David Lonjon <david.lonjon@gmail.com>
2021-11-26 16:06:28 +0900

    WIP: Refactor tests

--- projects/plugins/boost/tests/e2e/specs/jetpack-boost/connection.test.js
+++ projects/plugins/boost/tests/e2e/specs/jetpack-boost/connection.test.js
@@ -7,26 +7,23 @@ import { boostPrerequisitesBuilder } from '../../lib/env/prerequisites.js';
 import { prerequisitesBuilder } from 'jetpack-e2e-commons/env/prerequisites.js';
 
 test.describe( 'Settings Page', () => {
-	test.beforeEach( async ( { page } ) => {
-		await boostPrerequisitesBuilder( page ).withCleanEnv( true ).withConnection( false ).build();
-	} );
-
 	test( 'Should connect to WP.com on a fresh install without Jetpack plugin activated', async ( {
 		page,
 	} ) => {
 		await prerequisitesBuilder().withInactivePlugins( [ 'jetpack' ] ).build();
+		await boostPrerequisitesBuilder( page ).withCleanEnv( true ).withConnection( false ).build();
 		const jetpackBoostPage = await JetpackBoostPage.visit( page );
 		expect( await jetpackBoostPage.isFreshlyConnected() ).toEqual( true );
 	} );
 
-	// eslint-disable-next-line jest/no-commented-out-tests
-	// test( 'Should connect to WP.com on a fresh install with Jetpack plugin activated', async ( {
-	// 	page,
-	// } ) => {
-	// 	await prerequisitesBuilder().withActivePlugins( [ 'jetpack' ] ).build();
-	// 	const jetpackBoostPage = await JetpackBoostPage.visit( page );
-	// 	expect( await jetpackBoostPage.isFreshlyConnected() ).toEqual( true );
-	// } );
+	test( 'Should connect to WP.com on a fresh install with Jetpack plugin activated', async ( {
+		page,
+	} ) => {
+		await prerequisitesBuilder().withActivePlugins( [ 'jetpack' ] ).build();
+		await boostPrerequisitesBuilder( page ).withCleanEnv( true ).withConnection( false ).build();
+		const jetpackBoostPage = await JetpackBoostPage.visit( page );
+		expect( await jetpackBoostPage.isFreshlyConnected() ).toEqual( true );
+	} );
 
 	test( 'Should connect to WP.com on a fresh install with Jetpack plugin activated and Jetpack already connected', async ( {
 		page,
