baf7dec2c57feee0edd778eb60babe7d055a2525
Caleb Bauermeister <cbauerman@gmail.com>
2021-11-29 13:03:32 -0800

    [not verified] Add remaining elements to bundle card

--- projects/plugins/jetpack/_inc/client/at-a-glance/security-bundle/index.jsx
+++ projects/plugins/jetpack/_inc/client/at-a-glance/security-bundle/index.jsx
@@ -3,23 +3,32 @@
  */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { getRedirectUrl } from '@automattic/jetpack-components';
 
 /**
  * WordPress dependencies
  */
- import { __ } from '@wordpress/i18n';
- import { Button } from '@wordpress/components';
- import { getProductDescriptionUrl } from 'product-descriptions/utils';
+import { __ } from '@wordpress/i18n';
+import { Button } from '@wordpress/components';
+import { createInterpolateElement } from '@wordpress/element';
+
 
 /**
  * Internal dependencies
  */
- import Card from 'components/card';
+import Card from 'components/card';
+import bundleImageUrl from './shield-with-check.svg';
+import cloudImageUrl from './cloud.svg';
+import shieldImageUrl from './shield.svg';
+import removeBugImageUrl from './remove-bug.svg';
+import { getProductDescriptionUrl } from 'product-descriptions/utils';
 
 /**
  * Style dependencies
  */
 import './style.scss'
+import { padStart } from 'lodash';
+
 
 class DashSecurityBundle extends Component {
 
@@ -27,6 +36,36 @@ class DashSecurityBundle extends Component {
 		return ( 
 			<Card>
 				<h2>{ __( 'Comprehensive Site Security', 'jetpack' ) }</h2>
+				<img src={ bundleImageUrl } />
+				{ createInterpolateElement(
+					__(
+						'Total protection for your site, including Backup, Scan, and Anti-spam. <a>Learn More.</a>',
+						'jetpack'
+					),
+					{
+						a: (
+							<a
+								href={ getRedirectUrl( 'jetpack-features-security' ) }
+								rel="noopener noreferrer"
+								target="_blank"
+							></a>
+						),
+					}
+				) }
+				<div>
+					<div>
+						<img src={ cloudImageUrl } />
+						<p>{ __( 'Backup', 'jetpack' ) }</p>
+					</div>
+					<div>
+						<img src={ shieldImageUrl } />
+						<p>{ __( 'Scan', 'jetpack' ) }</p>
+					</div>
+					<div>
+						<img src={ removeBugImageUrl } />
+						<p>{ __( 'Anti-spam', 'jetpack' ) }</p>
+					</div>
+				</div>
 				<Button href={ this.props.productDescriptionUrl }>{ __( 'Upgrade', 'jetpack' ) }</Button>
 			</Card>
 		);
