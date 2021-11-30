d082ba70f1bef06e734ff2889d45d9fb084db020
David Lonjon <david.lonjon@gmail.com>
2021-11-30 10:59:58 +0900

    Fix coding standards for consistency

--- projects/plugins/boost/tests/e2e/specs/jetpack-boost/connection.test.js
+++ projects/plugins/boost/tests/e2e/specs/jetpack-boost/connection.test.js
@@ -1,10 +1,7 @@
-/**
- * Internal dependencies
- */
+import { prerequisitesBuilder } from 'jetpack-e2e-commons/env/prerequisites.js';
 import { test, expect } from '../../fixtures/base-test.js';
 import { JetpackBoostPage } from '../../lib/pages/index.js';
 import { boostPrerequisitesBuilder } from '../../lib/env/prerequisites.js';
-import { prerequisitesBuilder } from 'jetpack-e2e-commons/env/prerequisites.js';
 
 test.describe( 'Settings Page', () => {
 	test( 'Should connect to WP.com on a fresh install without Jetpack plugin activated', async ( {
