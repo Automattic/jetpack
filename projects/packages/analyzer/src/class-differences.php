5915dd6536ab436302437dd158d4bc8ada4ed3ea
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2021-11-30 23:11:34 +0800

    Add json load API

--- projects/packages/analyzer/src/class-differences.php
+++ projects/packages/analyzer/src/class-differences.php
@@ -140,4 +140,42 @@ class Differences extends PersistentList {
 			echo 'Deprecated: ' . $deprecated_total . "\n";
 		}
 	}
+
+	/**
+	 * Recreates list of differences from a passed JSON file
+	 *
+	 * @param string $file_path JSON file path.
+	 */
+	public function load( $file_path ) {
+		// phpcs:ignore
+		$contents = json_decode( file_get_contents( $file_path ) );
+
+		foreach ( $contents as $obj ) {
+			switch ( $obj->diff_type ) {
+				case 'function_missing':
+					$this->add( new Differences\Function_Missing( Declarations\Function_::from_map( $obj->old_declaration ) ) );
+					break;
+				case 'method_missing':
+					$this->add( new Differences\Class_Method_Missing( Declarations\Class_Method::from_map( $obj->old_declaration ) ) );
+					break;
+
+				case 'class_missing':
+					$this->add( new Differences\Class_Missing( Declarations\Class_::from_map( $obj->old_declaration ) ) );
+					break;
+
+				case 'class_const_missing':
+					$this->add( new Differences\Class_Const_Missing( Declarations\Class_Const::from_map( $obj->old_declaration ) ) );
+					break;
+
+				case 'property_missing':
+					$this->add( new Differences\Class_Property_Missing( Declarations\Class_Property::from_map( $obj->old_declaration ) ) );
+					break;
+
+				default:
+					// TODO: Implement handlers to other difference types.
+					echo $obj->diff_type . " not implemented!\n"; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+					break;
+			}
+		}
+	}
 }
