0e5572ce4f4e94ea507a042575e0152c5d68c81c
Brandon Kraft <public@brandonkraft.com>
2021-12-01 10:49:02 -0600

    Launchpad: Initial commit

---
+++ projects/plugins/launchpad/launchpad.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *
+ * Plugin Name: Jetpack launchpad
+ * Plugin URI: TBD
+ * Description: Jetpack Launchpad to quickly configure purchased products.
+ * Version: 0.1.0-alpha
+ * Author: Automattic
+ * Author URI: https://jetpack.com/
+ * License: GPLv2 or later
+ * Text Domain: jetpack
+ *
+ * @package automattic/jetpack-launchpad
+ */
+
+// Code some good stuff!
