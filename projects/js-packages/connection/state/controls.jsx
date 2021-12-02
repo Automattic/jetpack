126268575f530e1493c783958921e371d047ea1d
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2021-12-01 22:18:22 -0300

    Improve actions and controls to new approach at connection

--- projects/js-packages/connection/state/controls.jsx
+++ projects/js-packages/connection/state/controls.jsx
@@ -2,6 +2,12 @@
  * External dependencies
  */
 import restApi from '@automattic/jetpack-api';
+import { createRegistryControl } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import { STORE_ID } from './store';
 
 const FETCH_CONNECTION_STATUS = () => {
 	return new Promise( ( resolve, reject ) => {
@@ -12,6 +18,26 @@ const FETCH_CONNECTION_STATUS = () => {
 	} );
 };
 
+const REGISTER_SITE = ( { registrationNonce, redirectUri } ) =>
+	restApi.registerSite( registrationNonce, redirectUri );
+
+const CONNECT_USER = createRegistryControl( ( { select } ) => ( { from, redirectFunc } = {} ) => {
+	const authorizationUrl = select( STORE_ID ).getAuthorizationUrl() || '';
+	const redirect = redirectFunc || ( url => window.location.assign( url ) );
+
+	redirect(
+		authorizationUrl +
+			( from
+				? ( authorizationUrl.includes( '?' ) ? '&' : '?' ) + 'from=' + encodeURIComponent( from )
+				: '' )
+	);
+} );
+
+const FETCH_AUTHORIZATION_URL = ( { redirectUri } ) => restApi.fetchAuthorizationUrl( redirectUri );
+
 export default {
 	FETCH_CONNECTION_STATUS,
+	FETCH_AUTHORIZATION_URL,
+	REGISTER_SITE,
+	CONNECT_USER,
 };
