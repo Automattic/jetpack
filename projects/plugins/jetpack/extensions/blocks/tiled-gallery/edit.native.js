b8caf9ba9a8c2c0c6752b0388b4bbba284411dca
Wendy Chen <wendy.chen@automattic.com>
2021-12-02 13:55:26 -0600

    [not verified] WIP Decoupling columns setting from columns shown in editor for tiled gallery.

--- projects/plugins/jetpack/extensions/blocks/tiled-gallery/edit.native.js
+++ projects/plugins/jetpack/extensions/blocks/tiled-gallery/edit.native.js
@@ -10,29 +10,127 @@ import { concat } from 'lodash';
 import {
 	store as blockEditorStore,
 	BlockIcon,
+	InspectorControls,
 	MediaPlaceholder,
 	useBlockProps,
 	useInnerBlocksProps,
 } from '@wordpress/block-editor';
 import { createBlock } from '@wordpress/blocks';
+import {
+	LinkSettingsNavigation,
+	PanelBody,
+	RangeControl,
+	UnitControl,
+} from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { useState, useEffect, useMemo } from '@wordpress/element';
 import { useSelect, useDispatch } from '@wordpress/data';
-import { useResizeObserver } from '@wordpress/compose';
+import { usePreferredColorSchemeStyle, useResizeObserver } from '@wordpress/compose';
 
 /**
  * Internal dependencies
  */
-import { ALLOWED_MEDIA_TYPES } from './constants';
+import { ALLOWED_MEDIA_TYPES, LAYOUT_CIRCLE, LAYOUT_STYLES } from './constants';
+import { getActiveStyleName } from '../../shared/block-styles';
+import LayoutPicker from './layout-picker.native';
 import { icon } from '.';
 import styles from './styles.scss';
-import TiledGallerySettings, { dynamicColumnsNumber, MAX_COLUMNS } from './settings';
 
 const TILE_SPACING = 8;
+const DEFAULT_COLUMNS_PORTRAIT = 2;
+const DEFAULT_COLUMNS_LANDSCAPE = 4;
+const MIN_COLUMNS = 1;
+const MAX_COLUMNS = 8;
+const MIN_ROUNDED_CORNERS = 0;
+const MAX_ROUNDED_CORNERS = 20;
+const DEFAULT_ROUNDED_CORNERS = 2;
 
 export function defaultColumnsNumber( images ) {
 	return Math.min( MAX_COLUMNS, images.length );
 }
+
+function defaultColumnsSetting( window ) {
+	return window.width >= window.height ? DEFAULT_COLUMNS_LANDSCAPE : DEFAULT_COLUMNS_PORTRAIT;
+}
+
+function shownColumns( window, numImages ) {
+	return Math.min( numImages, defaultColumnsSetting( window ) );
+}
+
+const TiledGallerySettings = props => {
+	const horizontalSettingsDivider = usePreferredColorSchemeStyle(
+		styles.horizontalBorder,
+		styles.horizontalBorderDark
+	);
+
+	const { setAttributes, numImages, linkTo, columns, roundedCorners, clientId, className } = props;
+
+	const [ columnNumber, setColumnNumber ] = useState( columns );
+
+	const [ roundedCornerRadius, setRoundedCornerRadius ] = useState(
+		roundedCorners ?? DEFAULT_ROUNDED_CORNERS
+	);
+	const [ linkToURL, setLinkToURL ] = useState( linkTo ?? '' );
+
+	const linkSettingsOptions = {
+		url: {
+			label: __( 'Link URL', 'jetpack' ),
+			placeholder: __( 'Add URL', 'jetpack' ),
+			autoFocus: true,
+			autoFill: true,
+		},
+	};
+
+	const layoutStyle = getActiveStyleName( LAYOUT_STYLES, className );
+
+	return (
+		<InspectorControls>
+			<PanelBody title={ __( 'Tiled gallery settings', 'jetpack' ) } />
+
+			<PanelBody>
+				<LayoutPicker clientId={ clientId } className={ className } />
+			</PanelBody>
+			<PanelBody>
+				<UnitControl
+					label={ __( 'Columns', 'jetpack' ) }
+					min={ MIN_COLUMNS }
+					max={ numImages }
+					value={ Math.min( columnNumber, numImages ) }
+					onChange={ value => {
+						setColumnNumber( value );
+						setAttributes( { columns: value } );
+					} }
+				/>
+			</PanelBody>
+			{ layoutStyle !== LAYOUT_CIRCLE && (
+				<PanelBody style={ horizontalSettingsDivider }>
+					<RangeControl
+						label={ __( 'Rounded corners', 'jetpack' ) }
+						minimumValue={ MIN_ROUNDED_CORNERS }
+						maximumValue={ MAX_ROUNDED_CORNERS }
+						value={ roundedCornerRadius }
+						onChange={ value => {
+							setRoundedCornerRadius( value );
+							setAttributes( { roundedCorners: value } );
+						} }
+					/>
+				</PanelBody>
+			) }
+			<PanelBody>
+				<LinkSettingsNavigation
+					url={ linkToURL }
+					setAttributes={ value => {
+						setLinkToURL( value.url );
+					} }
+					withBottomSheet={ false }
+					hasPicker
+					options={ linkSettingsOptions }
+					showIcon={ false }
+				/>
+			</PanelBody>
+		</InspectorControls>
+	);
+};
 
 const TiledGalleryEdit = props => {
 	const [ resizeObserver, sizes ] = useResizeObserver();
@@ -46,7 +144,7 @@ const TiledGalleryEdit = props => {
 		noticeUI,
 		onFocus,
 		setAttributes,
-		attributes: { columns, images: attributeImages, linkTo, roundedCorners },
+		attributes: { columns: columnsSetting, images: attributeImages, linkTo, roundedCorners },
 	} = props;
 
 	const { replaceInnerBlocks, updateBlockAttributes } = useDispatch( blockEditorStore );
@@ -55,13 +153,10 @@ const TiledGalleryEdit = props => {
 		const { width } = sizes || {};
 		if ( width ) {
 			setMaxWidth( width );
-
-			if ( columns ) {
-				const columnWidths = new Array( columns ).fill( Math.floor( width / columns ) );
-				setAttributes( { columnWidths: [ columnWidths ] } );
-			}
 		}
-	}, [ sizes, columns, setAttributes ] );
+
+		console.log( '****** COUNT : %d', columnsSetting );
+	}, [ sizes ] );
 
 	const innerBlockImages = useSelect(
 		select => {
@@ -83,6 +178,8 @@ const TiledGalleryEdit = props => {
 	);
 
 	useEffect( () => {
+		console.log( '****** COUNT : %d', columnsSetting );
+
 		images?.forEach( newImage => {
 			updateBlockAttributes( newImage.clientId, {
 				...newImage.attributes,
@@ -94,14 +191,9 @@ const TiledGalleryEdit = props => {
 		setAttributes( { ids: newIds } );
 	}, [ images, setAttributes, updateBlockAttributes ] );
 
-	useEffect( () => {
-		if ( ! columns ) {
-			const col = Math.min( images.length, dynamicColumnsNumber( window ) );
-			setAttributes( { columns: Math.max( col, 1 ) } );
-		}
-	}, [ columns, images, setAttributes ] );
-
 	const populateInnerBlocksWithImages = ( imgs, replace = false ) => {
+		console.log( '***', Math.min( columnsSetting, shownColumns( window, images.length ) ) );
+
 		const newBlocks = imgs.map( image => {
 			return createBlock( 'core/image', {
 				id: image.id,
@@ -115,16 +207,10 @@ const TiledGalleryEdit = props => {
 		replaceInnerBlocks( clientId, replace ? newBlocks : concat( innerBlockImages, newBlocks ) );
 	};
 
-	useEffect( () => {
-		if ( ! columns ) {
-			setAttributes( { columns: dynamicColumnsNumber( window ) } );
-		}
-	}, [ columns, setAttributes ] );
-
 	if ( attributeImages.length && ! images.length ) {
 		populateInnerBlocksWithImages( attributeImages, true );
 	}
-
+	// Math.min( columnsSetting, shownColumns( window, images.length ) )
 	const innerBlocksProps = useInnerBlocksProps(
 		{},
 		{
@@ -132,12 +218,12 @@ const TiledGalleryEdit = props => {
 			allowedBlocks: [ 'core/image' ],
 			orientation: 'horizontal',
 			renderAppender: false,
-			numColumns: columns,
+			numColumns: shownColumns( window, images.length ),
 			marginHorizontal: TILE_SPACING,
 			marginVertical: TILE_SPACING,
 			__experimentalLayout: { type: 'default', alignments: [] },
 			gridProperties: {
-				numColumns: columns,
+				numColumns: shownColumns( window, images.length ),
 			},
 			parentWidth: maxWidth + 2 * TILE_SPACING,
 		}
@@ -171,13 +257,12 @@ const TiledGalleryEdit = props => {
 		<View blockProps={ blockProps }>
 			{ resizeObserver }
 			<TiledGallerySettings
-				setAttributes={ props.setAttributes }
+				setAttributes={ setAttributes }
 				linkTo={ linkTo }
-				columns={ columns }
+				columns={ columnsSetting }
 				roundedCorners={ roundedCorners }
 				clientId={ clientId }
 				className={ props.attributes.className }
-				window={ window }
 				numImages={ images.length }
 			/>
 			<View { ...innerBlocksProps } />
