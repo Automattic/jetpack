10730058510d74102430c42cbf23a8e74414d79a
leogermani <leogermani@gmail.com>
2021-12-06 20:35:29 -0300

    basic implementation of Grid

--- projects/js-packages/components/components/layout/grid/index.jsx
+++ projects/js-packages/components/components/layout/grid/index.jsx
@@ -3,6 +3,7 @@
  */
 import React from 'react';
 import classnames from 'classnames';
+import PropTypes from 'prop-types';
 
 /**
  * Internal dependencies
@@ -10,19 +11,29 @@ import classnames from 'classnames';
 import styles from './style.module.scss';
 
 /**
- * This is the wrapper component to build sections within your admin page.
+ * The basic Grid component.
  *
  * @param {object} props - Component properties.
  * @returns {React.Component} Grid component.
  */
 const Grid = props => {
 	const { children, sm, md, lg } = props;
+	const minimum = [ sm, md, lg ].reduce( ( prev, curr ) => ( curr < prev ? curr : prev ) );
 	const className = classnames(
-		styles[ 'sm-col-span-' + sm ],
-		styles[ 'md-col-span-' + md ],
-		styles[ 'lg-col-span-' + lg ]
+		typeof sm !== 'undefined' ? styles[ 'sm-col-span-' + sm ] : styles[ 'sm-col-span-' + minimum ],
+		typeof md !== 'undefined' ? styles[ 'md-col-span-' + md ] : styles[ 'md-col-span-' + minimum ],
+		typeof lg !== 'undefined' ? styles[ 'lg-col-span-' + lg ] : styles[ 'lg-col-span-' + minimum ]
 	);
 	return <div className={ className }>{ children }</div>;
+};
+
+Grid.proptypes = {
+	/** Colspan for small viewport. Defaults to the smallest colspan informed. */
+	sm: PropTypes.number,
+	/** Colspan for medium viewport. Defaults to the smallest colspan informed. */
+	md: PropTypes.number,
+	/** Colspan for large viewport. Defaults to the smallest colspan informed. */
+	lg: PropTypes.number,
 };
 
 export default Grid;
