2943401a1b451d69eb914161b9e10bed19be6699
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2021-12-07 11:05:00 +0100

    Move the CSS enqueue to the `widget()` function
    This change will make sure the CSS is enqueued only when the widget is added to the site.

--- projects/plugins/jetpack/modules/widgets/milestone/class-milestone-widget.php
+++ projects/plugins/jetpack/modules/widgets/milestone/class-milestone-widget.php
@@ -49,7 +49,6 @@ class Milestone_Widget extends WP_Widget {
 		);
 
 		add_action( 'wp_enqueue_scripts', array( __class__, 'enqueue_template' ) );
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 		add_action( 'admin_enqueue_scripts', array( __class__, 'enqueue_admin' ) );
 		add_action( 'wp_footer', array( $this, 'localize_script' ) );
 
@@ -205,6 +204,8 @@ class Milestone_Widget extends WP_Widget {
 	 */
 	public function widget( $args, $instance ) {
 		$instance = wp_parse_args( $instance, $this->defaults() );
+
+		$this->enqueue_scripts();
 
 		echo $args['before_widget']; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 
