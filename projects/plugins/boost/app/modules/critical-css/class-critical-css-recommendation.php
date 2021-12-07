4df09b64865d23c4bb053783352d6075867f309d
Karthik Bhat <karthik.ax.ax@gmail.com>
2021-12-07 17:13:02 +0530

    Move Critical CSS Recommendation to own class

---
+++ projects/plugins/boost/app/modules/critical-css/class-critical-css-recommendation.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Critical CSS recommendations.
+ *
+ * @link       https://automattic.com
+ * @since      1.0.0
+ * @package    automattic/jetpack-boost
+ */
+
+namespace Automattic\Jetpack_Boost\Modules\Critical_CSS;
+
+/**
+ * Critical CSS Recommendation class
+ */
+class Critical_CSS_Recommendation {
+	const DISMISSED_RECOMMENDATIONS_STORAGE_KEY = 'jb-critical-css-dismissed-recommendations';
+	const DISMISS_CSS_RECOMMENDATIONS_NONCE     = 'dismiss_notice';
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		add_filter( 'jetpack_boost_js_constants', array( $this, 'always_add_critical_css_constants' ) );
+	}
+
+	/**
+	 * Constructor.
+	 */
+	public function init() {
+
+		add_action( 'wp_ajax_dismiss_recommendations', array( $this, 'dismiss_recommendations' ) );
+		add_action( 'wp_ajax_reset_dismissed_recommendations', array( $this, 'reset_dismissed_recommendations' ) );
+	}
+
+	/**
+	 * Run on plugin uninstall
+	 */
+	public static function on_uninstall() {
+		self::clear_dismissed_recommendations();
+	}
+
+	/**
+	 * Add Critical CSS related constants to be passed to JavaScript whether or not the module is enabled.
+	 *
+	 * @param array $constants Constants to be passed to JavaScript.
+	 *
+	 * @return array
+	 */
+	public function always_add_critical_css_constants( $constants ) {
+		$constants['criticalCssDismissRecommendationsNonce'] = wp_create_nonce( self::DISMISS_CSS_RECOMMENDATIONS_NONCE );
+
+		return $constants;
+	}
+
+	/**
+	 * Clear Critical CSS dismissed recommendations option.
+	 */
+	public static function clear_dismissed_recommendations() {
+		\delete_option( self::DISMISSED_RECOMMENDATIONS_STORAGE_KEY );
+	}
+
+	/**
+	 * Dismiss Critical CSS recommendations.
+	 */
+	public function dismiss_recommendations() {
+		if ( check_ajax_referer( self::DISMISS_CSS_RECOMMENDATIONS_NONCE, 'nonce' ) && current_user_can( 'manage_options' ) ) {
+			$response = array(
+				'status' => 'ok',
+			);
+		} else {
+			$error = new \WP_Error( 'authorization', __( 'You do not have permission to take this action.', 'jetpack-boost' ) );
+			wp_send_json_error( $error, 403 );
+		}
+
+		$provider_key = $_POST['providerKey'] ? filter_var( $_POST['providerKey'], FILTER_SANITIZE_STRING ) : '';
+		if ( empty( $provider_key ) ) {
+			$response['status'] = 'error';
+			echo wp_json_encode( $response );
+			wp_die();
+		}
+		$dismissed_recommendations = \get_option( self::DISMISSED_RECOMMENDATIONS_STORAGE_KEY, array() );
+
+		if ( ! in_array( $provider_key, $dismissed_recommendations, true ) ) {
+			$dismissed_recommendations[] = $provider_key;
+			\update_option( self::DISMISSED_RECOMMENDATIONS_STORAGE_KEY, $dismissed_recommendations );
+		}
+
+		echo wp_json_encode( $response );
+		wp_die();
+	}
+
+	/**
+	 * Reset dismissed Critical CSS recommendations.
+	 */
+	public function reset_dismissed_recommendations() {
+		if ( check_ajax_referer( self::DISMISS_CSS_RECOMMENDATIONS_NONCE, 'nonce' ) && current_user_can( 'manage_options' ) ) {
+			$response = array(
+				'status' => 'ok',
+			);
+		} else {
+			$error = new \WP_Error( 'authorization', __( 'You do not have permission to take this action.', 'jetpack-boost' ) );
+			wp_send_json_error( $error, 403 );
+		}
+
+		self::clear_dismissed_recommendations();
+
+		echo wp_json_encode( $response );
+		wp_die();
+	}
+}
--- projects/plugins/boost/app/modules/critical-css/class-critical-css.php
+++ projects/plugins/boost/app/modules/critical-css/class-critical-css.php
@@ -23,13 +23,11 @@ use Automattic\Jetpack_Boost\Modules\Module;
  */
 class Critical_CSS extends Module {
 
-	const MODULE_SLUG                           = 'critical-css';
-	const GENERATE_QUERY_ACTION                 = 'jb-generate-critical-css';
-	const GENERATE_PROXY_NONCE                  = 'jb-generate-proxy-nonce';
-	const CSS_CALLBACK_ACTION                   = 'jb-critical-css-callback';
-	const RESET_REASON_STORAGE_KEY              = 'jb-generate-critical-css-reset-reason';
-	const DISMISSED_RECOMMENDATIONS_STORAGE_KEY = 'jb-critical-css-dismissed-recommendations';
-	const DISMISS_CSS_RECOMMENDATIONS_NONCE     = 'dismiss_notice';
+	const MODULE_SLUG              = 'critical-css';
+	const GENERATE_QUERY_ACTION    = 'jb-generate-critical-css';
+	const GENERATE_PROXY_NONCE     = 'jb-generate-proxy-nonce';
+	const CSS_CALLBACK_ACTION      = 'jb-critical-css-callback';
+	const RESET_REASON_STORAGE_KEY = 'jb-generate-critical-css-reset-reason';
 
 	/**
 	 * Viewport sizes for this module.
@@ -110,12 +108,20 @@ class Critical_CSS extends Module {
 	protected $state;
 
 	/**
+	 * Critical CSS Recommendation class instance.
+	 *
+	 * @var Critical_CSS_Recommendation
+	 */
+
+	protected $recommendation;
+
+	/**
 	 * Constructor.
 	 */
 	public function __construct() {
 		parent::__construct();
 
-		add_filter( 'jetpack_boost_js_constants', array( $this, 'always_add_critical_css_constants' ) );
+		$this->recommendation = new Critical_CSS_Recommendation();
 	}
 
 	/**
@@ -155,8 +161,7 @@ class Critical_CSS extends Module {
 		add_filter( 'jetpack_boost_js_constants', array( $this, 'add_critical_css_constants' ) );
 
 		if ( is_admin() ) {
-			add_action( 'wp_ajax_dismiss_recommendations', array( $this, 'dismiss_recommendations' ) );
-			add_action( 'wp_ajax_reset_dismissed_recommendations', array( $this, 'reset_dismissed_recommendations' ) );
+			$this->recommendation->init();
 			add_action( 'wp_ajax_boost_proxy_css', array( $this, 'handle_css_proxy' ) );
 		}
 
@@ -168,7 +173,7 @@ class Critical_CSS extends Module {
 	 */
 	public function on_uninstall() {
 		self::clear_reset_reason();
-		self::clear_dismissed_recommendations();
+		Critical_CSS_Recommendation::on_uninstall();
 	}
 
 	/**
@@ -476,19 +481,6 @@ class Critical_CSS extends Module {
 	// phpcs:enable WordPress.Security.NonceVerification.Recommended
 
 	/**
-	 * Add Critical CSS related constants to be passed to JavaScript whether or not the module is enabled.
-	 *
-	 * @param array $constants Constants to be passed to JavaScript.
-	 *
-	 * @return array
-	 */
-	public function always_add_critical_css_constants( $constants ) {
-		$constants['criticalCssDismissRecommendationsNonce'] = wp_create_nonce( self::DISMISS_CSS_RECOMMENDATIONS_NONCE );
-
-		return $constants;
-	}
-
-	/**
 	 * Add Critical CSS related constants to be passed to JavaScript only if the module is enabled.
 	 *
 	 * @param array $constants Constants to be passed to JavaScript.
@@ -498,7 +490,7 @@ class Critical_CSS extends Module {
 	public function add_critical_css_constants( $constants ) {
 		// Information about the current status of Critical CSS / generation.
 		$constants['criticalCssStatus']                   = $this->get_local_critical_css_generation_info();
-		$constants['criticalCssDismissedRecommendations'] = \get_option( self::DISMISSED_RECOMMENDATIONS_STORAGE_KEY, array() );
+		$constants['criticalCssDismissedRecommendations'] = \get_option( Critical_CSS_Recommendation::DISMISSED_RECOMMENDATIONS_STORAGE_KEY, array() );
 
 		return $constants;
 	}
@@ -597,7 +589,7 @@ class Critical_CSS extends Module {
 			$this->storage->clear();
 			$this->state->create_request( $this->providers );
 			self::clear_reset_reason();
-			self::clear_dismissed_recommendations();
+			Critical_CSS_Recommendation::clear_dismissed_recommendations();
 		}
 
 		return rest_ensure_response(
@@ -900,13 +892,6 @@ class Critical_CSS extends Module {
 	}
 
 	/**
-	 * Clear Critical CSS dismissed recommendations option.
-	 */
-	public static function clear_dismissed_recommendations() {
-		\delete_option( self::DISMISSED_RECOMMENDATIONS_STORAGE_KEY );
-	}
-
-	/**
 	 * Given a provider key, find the provider which owns the key. Returns false
 	 * if no Provider is found.
 	 *
@@ -944,54 +929,5 @@ class Critical_CSS extends Module {
 		 * @param string $provider_key
 		 */
 		return $provider::describe_key( $provider_key );
-	}
-
-	/**
-	 * Dismiss Critical CSS recommendations.
-	 */
-	public function dismiss_recommendations() {
-		if ( check_ajax_referer( self::DISMISS_CSS_RECOMMENDATIONS_NONCE, 'nonce' ) && current_user_can( 'manage_options' ) ) {
-			$response = array(
-				'status' => 'ok',
-			);
-		} else {
-			$error = new \WP_Error( 'authorization', __( 'You do not have permission to take this action.', 'jetpack-boost' ) );
-			wp_send_json_error( $error, 403 );
-		}
-
-		$provider_key = $_POST['providerKey'] ? filter_var( $_POST['providerKey'], FILTER_SANITIZE_STRING ) : '';
-		if ( empty( $provider_key ) ) {
-			$response['status'] = 'error';
-			echo wp_json_encode( $response );
-			wp_die();
-		}
-		$dismissed_recommendations = \get_option( self::DISMISSED_RECOMMENDATIONS_STORAGE_KEY, array() );
-
-		if ( ! in_array( $provider_key, $dismissed_recommendations, true ) ) {
-			$dismissed_recommendations[] = $provider_key;
-			\update_option( self::DISMISSED_RECOMMENDATIONS_STORAGE_KEY, $dismissed_recommendations );
-		}
-
-		echo wp_json_encode( $response );
-		wp_die();
-	}
-
-	/**
-	 * Reset dismissed Critical CSS recommendations.
-	 */
-	public function reset_dismissed_recommendations() {
-		if ( check_ajax_referer( self::DISMISS_CSS_RECOMMENDATIONS_NONCE, 'nonce' ) && current_user_can( 'manage_options' ) ) {
-			$response = array(
-				'status' => 'ok',
-			);
-		} else {
-			$error = new \WP_Error( 'authorization', __( 'You do not have permission to take this action.', 'jetpack-boost' ) );
-			wp_send_json_error( $error, 403 );
-		}
-
-		self::clear_dismissed_recommendations();
-
-		echo wp_json_encode( $response );
-		wp_die();
 	}
 }
