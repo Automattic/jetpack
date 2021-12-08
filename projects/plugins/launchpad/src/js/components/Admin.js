850f2a96038aabb8c34c55078103cbb8a78231b5
Daniel Post <daniel.post@automattic.com>
2021-12-08 12:41:17 +0100

    Extract Connect components from hook

--- projects/plugins/launchpad/src/js/components/Admin.js
+++ projects/plugins/launchpad/src/js/components/Admin.js
@@ -1,19 +1,37 @@
 /**
  * External dependencies
  */
+import { AdminPage, AdminSection, AdminSectionHero } from '@automattic/jetpack-components';
+import {
+	ConnectScreen,
+	ConnectionStatusCard,
+	CONNECTION_STORE_ID,
+} from '@automattic/jetpack-connection';
+
+/**
+ * WordPress dependencies
+ */
+import { useSelect } from '@wordpress/data';
 import { useState, useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-import { AdminPage, AdminSection, AdminSectionHero } from '@automattic/jetpack-components';
 
 /**
  * Internal dependencies
  */
-import useConnection from '../hooks/useConnection';
+import { STORE_ID } from '../store';
+import ConnectRight from './assets/connect-right.png';
 import './admin-style.scss';
 
-/* eslint react/react-in-jsx-scope: 0 */
 const Admin = () => {
-	const [ connectionStatus, renderConnectScreen, renderConnectionStatusCard ] = useConnection();
+	const APINonce = useSelect( select => select( STORE_ID ).getAPINonce(), [] );
+	const APIRoot = useSelect( select => select( STORE_ID ).getAPIRoot(), [] );
+	const connectedPlugins = useSelect( select => select( STORE_ID ).getConnectedPlugins(), [] );
+	const connectedSiteData = useSelect( select => select( STORE_ID ).getSiteData(), [] );
+	const registrationNonce = useSelect( select => select( STORE_ID ).getRegistrationNonce(), [] );
+	const connectionStatus = useSelect(
+		select => select( CONNECTION_STORE_ID ).getConnectionStatus(),
+		[]
+	);
 	const [ connectionLoaded, setConnectionLoaded ] = useState( false );
 
 	useEffect( () => {
@@ -28,7 +46,23 @@ const Admin = () => {
 		return (
 			<div className="jp-wrap">
 				<div className="jp-row">
-					<div class="lg-col-span-12 md-col-span-8 sm-col-span-4">{ renderConnectScreen() }</div>
+					<div class="lg-col-span-12 md-col-span-8 sm-col-span-4">
+						<ConnectScreen
+							apiRoot={ APIRoot }
+							apiNonce={ APINonce }
+							registrationNonce={ registrationNonce }
+							from="jetpack-launchpad"
+							redirectUri="admin.php?page=jetpack-launchpad"
+							images={ [ ConnectRight ] }
+						>
+							<p>
+								{ __(
+									'Jetpack Launchpad requires a user connection to WordPress.com.',
+									'jetpack-launchpad'
+								) }
+							</p>
+						</ConnectScreen>
+					</div>
 				</div>
 			</div>
 		);
@@ -41,7 +75,16 @@ const Admin = () => {
 					<h1>{ __( 'Jetpack Launchpad', 'jetpack-launchpad' ) }</h1>
 				</AdminSectionHero>
 				<AdminSection>
-					{ renderConnectionStatusCard() }
+					<ConnectionStatusCard
+						isRegistered={ connectionStatus.isRegistered }
+						isUserConnected={ connectionStatus.isUserConnected }
+						apiRoot={ APIRoot }
+						apiNonce={ APINonce }
+						connectedPlugins={ connectedPlugins }
+						connectedSiteId={ connectedSiteData ? connectedSiteData.id : null }
+						redirectUri="admin.php?page=jetpack-launchpad"
+						context="jetpack-launchpad-page"
+					/>
 				</AdminSection>
 			</AdminPage>
 		</div>
