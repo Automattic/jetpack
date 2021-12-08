641e48d1cbe1bd4da3d33adc0235a57c812ba3ab
Samiff <samiff@users.noreply.github.com>
2021-12-08 12:20:52 -0700

    Update PHP 7.4 to 8.0 in our tools (#22046)
    * Update versions.sh PHP version from 7.4 to 8.0
    
    * Docker: remove php-json ext. included with PHP 8.0
    
    * Remove unused files
    
    Versions are now referenced in .github/versions.sh
    
    * Update phpunit workaround
    
    See: Automattic/jetpack/pull/22046#issuecomment-988887739

--- .github/files/setup-wordpress-env.sh
+++ .github/files/setup-wordpress-env.sh
@@ -35,10 +35,11 @@ case "$WP_BRANCH" in
 		;;
 	latest)
 		LATEST=$(php ./tools/get-wp-version.php)
-		# 5.8.1 does not have the polyfill stuff backported.
-		# @todo: Remove this once 5.8.1 is no longer "latest".
-		if [[ "$LATEST" == "5.8.1" ]]; then
-			LATEST=5.8
+		# 5.8.x still requires a monkey-patched obsolete version of phpunit.
+		# If that's latest, run the coverage test with the upcoming 5.9 instead.
+		# @todo: Remove this once WordPress 5.9 is "latest".
+		if [[ "$LATEST" == 5.8.* && "$TEST_SCRIPT" == "test-coverage" ]]; then
+			LATEST=master
 		fi
 		git clone --depth=1 --branch "$LATEST" git://develop.git.wordpress.org/ /tmp/wordpress-latest
 		;;
