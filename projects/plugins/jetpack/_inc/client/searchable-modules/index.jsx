Automattic/jetpack

commit 42d5646d5db7e36f393d41adbbbdf878154dfbbc

Userless Connection: connection flow (#18837)
- Modify connection screen appearance
- Redirect "userless" users to the "Plans" page after they choose to proceed without connecting to WP.com
- `ConnectUserBar` React component
- New module setting: `requires_user_connection`, and new reducers to pull this data
- New property for the `SettingsGroup` component: `disableInUserlessMode`, which checks if the site is working in userless mode, and makes the `SettingsGroup` inactive
- Adjusting the modules that require user connection accordingly

projects/plugins/jetpack/_inc/client/searchable-modules/index.jsx
@@ -17,7 +17,12 @@ import { isModuleFound } from 'state/search';
 import SettingsCard from 'components/settings-card';
 import SettingsGroup from 'components/settings-group';
 import { userCanManageModules } from 'state/initial-state';
-import { isOfflineMode, isUnavailableInOfflineMode } from 'state/connection';
+import {
+	isOfflineMode,
+	isUnavailableInOfflineMode,
+	isUnavailableInUserlessMode,
+} from 'state/connection';
+import ConnectUserBar from 'components/connect-user-bar';
 
 export const SearchableModules = withModuleSettingsFormHelpers(
 	class extends Component {
@@ -44,13 +49,21 @@ export const SearchableModules = withModuleSettingsFormHelpers(
 				results = [];
 			forEach( allModules, ( moduleData, slug ) => {
 				if ( this.props.isModuleFound( slug ) && includes( safelist, slug ) ) {
-					// Not available in offline mode.
-					if (
-						this.props.isOfflineMode &&
-						this.props.isUnavailableInOfflineMode( moduleData.module )
-					) {
+					const isModuleUnavailableInOfflineMode =
+						this.props.isOfflineMode && this.props.isUnavailableInOfflineMode( moduleData.module );
+					const isModuleUnavailableInUserlessMode =
+						! this.props.hasConnectedOwner &&
+						this.props.isUnavailableInUserlessMode( moduleData.module );
+
+					// Not available in offline or userless mode.
+					if ( isUnavailableInOfflineMode || isUnavailableInUserlessMode ) {
 						return results.push(
-							<ActiveCard key={ slug } moduleData={ moduleData } offlineMode={ true } />
+							<ActiveCard
+								key={ slug }
+								moduleData={ moduleData }
+								offlineMode={ isModuleUnavailableInOfflineMode }
+								userlessMode={ isModuleUnavailableInUserlessMode }
+							/>
 						);
 					}
 
@@ -89,17 +102,29 @@ SearchableModules.defaultProps = {
 class ActiveCard extends Component {
 	render() {
 		const m = this.props.moduleData,
-			offlineMode = this.props.offlineMode;
+			offlineMode = this.props.offlineMode,
+			userlessMode = this.props.userlessMode;
 
 		return (
 			<SettingsCard module={ m.module } header={ m.name } action={ m.module } hideButton>
 				<SettingsGroup
 					disableInOfflineMode={ offlineMode }
+					disableInUserlessMode={ userlessMode }
 					module={ { module: m.module } }
 					support={ { link: m.learn_more_button } }
 				>
 					{ m.description }
 				</SettingsGroup>
+
+				{ userlessMode && (
+					<ConnectUserBar
+						feature={ m.module }
+						text={ __(
+							'The feature is provided by the WordPress.com cloud. Sign in to configure it.',
+							'jetpack'
+						) }
+					/>
+				) }
 			</SettingsCard>
 		);
 	}
@@ -112,5 +137,6 @@ export default connect( state => {
 		canManageModules: userCanManageModules( state ),
 		isUnavailableInOfflineMode: module_name => isUnavailableInOfflineMode( state, module_name ),
 		isOfflineMode: isOfflineMode( state ),
+		isUnavailableInUserlessMode: module_name => isUnavailableInUserlessMode( state, module_name ),
 	};
 } )( SearchableModules );

projects/plugins/jetpack/_inc/client/components/auth-iframe/index.jsx
@@ -31,6 +31,7 @@ export class AuthIframe extends React.Component {
 		scrollToIframe: PropTypes.bool,
 		onAuthorized: PropTypes.func,
 		hasConnectedOwner: PropTypes.bool,
+		source: PropTypes.string,
 	};
 
 	static defaultProps = {
@@ -85,9 +86,15 @@ export class AuthIframe extends React.Component {
 			height = ( parseInt( height ) + 50 ).toString();
 		}
 
+		src += '&iframe_height=' + parseInt( height );
+
+		if ( this.props.source ) {
+			src += '&iframe_source=' + this.props.source;
+		}
+
 		return (
 			<div ref="iframeWrap" className="dops-card fade-in jp-iframe-wrap">
-				<h1>{ this.props.title }</h1>
+				{ this.props.title && <h1>{ this.props.title }</h1> }
 				{ this.props.fetchingConnectUrl ? (
 					<p>{ __( 'Loading…', 'jetpack' ) }</p>
 				) : (

projects/plugins/jetpack/_inc/client/components/connect-button/index.jsx
@@ -44,6 +44,7 @@ export class ConnectButton extends React.Component {
 		asLink: PropTypes.bool,
 		connectLegend: PropTypes.string,
 		connectInPlace: PropTypes.bool,
+		customConnect: PropTypes.func,
 	};
 
 	static defaultProps = {
@@ -78,10 +79,16 @@ export class ConnectButton extends React.Component {
 		if ( this.props.isAuthorizing || this.props.fetchingConnectUrl ) {
 			return;
 		}
+
 		// Track click
 		analytics.tracks.recordJetpackClick( 'link_account_in_place' );
-		// Dispatch user in place authorization.
-		this.props.authorizeUserInPlace();
+
+		if ( this.props.customConnect ) {
+			this.props.customConnect();
+		} else {
+			// Dispatch user in place authorization.
+			this.props.authorizeUserInPlace();
+		}
 	};
 
 	renderUserButton = () => {

projects/plugins/jetpack/_inc/client/components/connect-user-bar/index.jsx
@@ -1,0 +1,54 @@
+/**
+ * External dependencies
+ */
+import React, { useState, useCallback } from 'react';
+import PropTypes from 'prop-types';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import ConnectButton from 'components/connect-button';
+import ConnectUserFrame from 'components/connect-user-frame';
+import Card from 'components/card';
+import analytics from 'lib/analytics';
+import './style.scss';
+
+const ConnectUserBar = props => {
+	const [ showConnect, setShowConnect ] = useState( false );
+
+	const customConnect = useCallback( () => {
+		analytics.tracks.recordJetpackClick( {
+			target: 'connection-bar-click',
+			feature: props.feature,
+			is_user_wpcom_connected: 'no',
+			is_connection_owner: 'no',
+		} );
+
+		setShowConnect( true );
+	}, [ setShowConnect, props.feature ] );
+
+	return (
+		<Card compact className="jp-connect-user-bar__card">
+			{ ! showConnect && <div className="jp-connect-user-bar__text">{ props.text }</div> }
+			{ ! showConnect && (
+				<div className="jp-connect-user-bar__button">
+					<ConnectButton
+						connectUser={ true }
+						from="unlinked-user-connect"
+						connectLegend={ __( 'Connect my user account', 'jetpack' ) }
+						customConnect={ customConnect }
+					/>
+				</div>
+			) }
+			{ showConnect && <ConnectUserFrame source="connect-user-bar" /> }
+		</Card>
+	);
+};
+
+ConnectUserBar.propTypes = {
+	text: PropTypes.string.isRequired,
+	feature: PropTypes.string,
+};
+
+export default ConnectUserBar;

projects/plugins/jetpack/_inc/client/components/connect-user-frame/index.jsx
@@ -1,0 +1,78 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import AuthIframe from 'components/auth-iframe';
+import Gridicon from 'components/gridicon';
+import './style.scss';
+
+const ConnectUserFrame = props => {
+	return (
+		<div className="jp-connect-user-frame">
+			<div className="jp-connect-user-frame__left">
+				<h2>{ __( 'Unlock Monitoring and more amazing features', 'jetpack' ) }</h2>
+
+				<ul>
+					<li>
+						<Gridicon icon="checkmark-circle" />{ ' ' }
+						{ __( 'Receive instant downtime alerts', 'jetpack' ) }
+					</li>
+					<li>
+						<Gridicon icon="checkmark-circle" />{ ' ' }
+						{ __( 'Automatically share your content on social media', 'jetpack' ) }
+					</li>
+					<li>
+						<Gridicon icon="checkmark-circle" />{ ' ' }
+						{ __( 'Let your subscribers know when you post', 'jetpack' ) }
+					</li>
+					<li>
+						<Gridicon icon="checkmark-circle" />{ ' ' }
+						{ __( 'Receive notifications about new likes and comments', 'jetpack' ) }
+					</li>
+					<li>
+						<Gridicon icon="checkmark-circle" />{ ' ' }
+						{ __( 'Let visitors share your content on social media', 'jetpack' ) }
+					</li>
+				</ul>
+
+				<p>
+					<a
+						className="jp-connect-user-frame__features-link"
+						target="_blank"
+						rel="noreferrer"
+						href="https://jetpack.com/support/features/"
+					>
+						{ __( 'See all Jetpack features', 'jetpack' ) }
+						<svg width="16" height="16" viewBox="0 0 24 24" className="gridicon gridicons-external">
+							<g>
+								<path d="M19 13v6c0 1.105-.895 2-2 2H5c-1.105 0-2-.895-2-2V7c0-1.105.895-2 2-2h6v2H5v12h12v-6h2zM13 3v2h4.586l-7.793 7.793 1.414 1.414L19 6.414V11h2V3h-8z"></path>
+							</g>
+						</svg>
+					</a>
+				</p>
+			</div>
+
+			<div className="jp-connect-user-frame__right">
+				<AuthIframe
+					scrollToIframe={ false }
+					title=""
+					width="347"
+					height="353"
+					source={ props.source }
+				/>
+			</div>
+		</div>
+	);
+};
+
+ConnectUserFrame.propTypes = {
+	source: PropTypes.string,
+};
+
+export default ConnectUserFrame;

projects/plugins/jetpack/_inc/client/components/jetpack-notices/index.jsx
@@ -25,6 +25,7 @@ import {
 	isCurrentUserLinked,
 	isReconnectingSite,
 	getConnectUrl as _getConnectUrl,
+	hasConnectedOwner,
 } from 'state/connection';
 import {
 	isDevVersion,
@@ -244,6 +245,7 @@ class JetpackNotices extends React.Component {
 				<DismissableNotices />
 				{ ! this.props.isReconnectingSite &&
 					this.props.userCanConnectAccount &&
+					this.props.hasConnectedOwner &&
 					! siteDataErrors.length &&
 					! this.props.connectionErrors.length && (
 						<UserUnlinked
@@ -292,6 +294,7 @@ export default connect(
 			siteDataErrors: getSiteDataErrors( state ),
 			isReconnectingSite: isReconnectingSite( state ),
 			licensingError: getLicensingError( state ),
+			hasConnectedOwner: hasConnectedOwner( state ),
 		};
 	},
 	dispatch => {

projects/plugins/jetpack/_inc/client/components/settings-group/index.jsx
@@ -12,7 +12,12 @@ import { includes, noop } from 'lodash';
  * Internal dependencies
  */
 import SupportInfo from 'components/support-info';
-import { isOfflineMode, isUnavailableInOfflineMode, isCurrentUserLinked } from 'state/connection';
+import {
+	isOfflineMode,
+	isUnavailableInOfflineMode,
+	isUnavailableInUserlessMode,
+	isCurrentUserLinked,
+} from 'state/connection';
 import { userCanManageModules, isSitePublic, userCanEditPosts } from 'state/initial-state';
 import { isModuleActivated } from 'state/modules';
 
@@ -31,7 +36,11 @@ export const SettingsGroup = props => {
 
 	const disableInOfflineMode =
 		props.disableInOfflineMode && props.isUnavailableInOfflineMode( module.module );
-	let displayFadeBlock = disableInOfflineMode;
+
+	const disableInUserlessMode =
+		props.disableInUserlessMode && props.isUnavailableInUserlessMode( module.module );
+
+	let displayFadeBlock = disableInOfflineMode || disableInUserlessMode;
 
 	if ( 'post-by-email' === module.module && ! props.isLinked ) {
 		displayFadeBlock = true;
@@ -42,7 +51,7 @@ export const SettingsGroup = props => {
 			<Card
 				className={ classNames( {
 					'jp-form-has-child': props.hasChild,
-					'jp-form-settings-disable': disableInOfflineMode,
+					'jp-form-settings-disable': disableInOfflineMode || disableInUserlessMode,
 				} ) }
 			>
 				{ displayFadeBlock && <div className="jp-form-block-fade" /> }
@@ -57,6 +66,7 @@ SettingsGroup.propTypes = {
 	support: PropTypes.object,
 	module: PropTypes.object,
 	disableInOfflineMode: PropTypes.bool.isRequired,
+	disableInUserlessMode: PropTypes.bool,
 	isOfflineMode: PropTypes.bool.isRequired,
 	isSitePublic: PropTypes.bool.isRequired,
 	userCanManageModules: PropTypes.bool.isRequired,
@@ -69,6 +79,7 @@ SettingsGroup.defaultProps = {
 	support: { text: '', link: '' },
 	module: {},
 	disableInOfflineMode: false,
+	disableInUserlessMode: false,
 	isOfflineMode: false,
 	isSitePublic: true,
 	userCanManageModules: false,
@@ -86,5 +97,6 @@ export default connect( state => {
 		isLinked: isCurrentUserLinked( state ),
 		isModuleActivated: module => isModuleActivated( state, module ),
 		isUnavailableInOfflineMode: module_name => isUnavailableInOfflineMode( state, module_name ),
+		isUnavailableInUserlessMode: module_name => isUnavailableInUserlessMode( state, module_name ),
 	};
 } )( SettingsGroup );

projects/plugins/jetpack/_inc/client/security/index.jsx
@@ -12,7 +12,7 @@ import { __ } from '@wordpress/i18n';
 import Card from 'components/card';
 import { getModule } from 'state/modules';
 import { getSettings } from 'state/settings';
-import { isOfflineMode, isUnavailableInOfflineMode } from 'state/connection';
+import { isOfflineMode, isUnavailableInOfflineMode, hasConnectedOwner } from 'state/connection';
 import { getVaultPressData } from 'state/at-a-glance';
 import { isModuleFound } from 'state/search';
 import { isPluginActive, isPluginInstalled } from 'state/site/plugins';
@@ -68,6 +68,7 @@ export class Security extends Component {
 			isUnavailableInOfflineMode: this.props.isUnavailableInOfflineMode,
 			rewindStatus: this.props.rewindStatus,
 			siteRawUrl: this.props.siteRawUrl,
+			hasConnectedOwner: this.props.hasConnectedOwner,
 		};
 
 		const foundProtect = this.props.isModuleFound( 'protect' ),
@@ -146,5 +147,6 @@ export default connect( state => {
 		isPluginActive: plugin_slug => isPluginActive( state, plugin_slug ),
 		isPluginInstalled: plugin_slug => isPluginInstalled( state, plugin_slug ),
 		vaultPressData: getVaultPressData( state ),
+		hasConnectedOwner: hasConnectedOwner( state ),
 	};
 } )( Security );

commit 766a8036b2532fb7b935d9e9a0769b9a30065f3b

Dash: allow turning on searchable modules again
This bug was introduced in #18837, and stopped one from enabling searchable modules from the Jetpack dashboard, because the logic was not looking for the right module data.

projects/plugins/jetpack/_inc/client/searchable-modules/index.jsx
@@ -56,7 +56,7 @@ export const SearchableModules = withModuleSettingsFormHelpers(
 						this.props.isUnavailableInSiteConnectionMode( moduleData.module );
 
 					// Not available in offline or SiteConnection mode.
-					if ( isUnavailableInOfflineMode || isUnavailableInSiteConnectionMode ) {
+					if ( isModuleUnavailableInOfflineMode || isModuleUnavailableInSiteConnectionMode ) {
 						return results.push(
 							<ActiveCard
 								key={ slug }