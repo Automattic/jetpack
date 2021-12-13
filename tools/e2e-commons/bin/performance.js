09ef5f4bc2a07da45947e14cb04f981fb54558b0
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2021-12-13 22:22:15 +0800

    Run tests for multiple attempts

--- tools/e2e-commons/bin/performance.js
+++ tools/e2e-commons/bin/performance.js
@@ -1,5 +1,9 @@
+import { readFileSync, writeFileSync } from 'fs';
 import { prerequisitesBuilder } from '../env/prerequisites.js';
 import { execSyncShellCommand, execWpCommand, resolveSiteUrl } from '../helpers/utils-helper.cjs';
+import _ from 'lodash';
+
+const numAttempts = 3;
 
 function envReset() {
 	console.log( execSyncShellCommand( 'pwd' ) );
@@ -20,20 +24,49 @@ async function envSetup( type ) {
 	);
 }
 
-function runTests( type ) {
+function runTests( type, id ) {
 	const siteUrl = resolveSiteUrl();
 
 	execSyncShellCommand( `export WP_BASE_URL=${ siteUrl } &&
 	cd ../../gutenberg &&
 	echo $WP_BASE_URL &&
 	WP_BASE_URL=${ siteUrl } npm run test-performance packages/e2e-tests/specs/performance/post-editor.test.js &&
-	mv packages/e2e-tests/specs/performance/post-editor.test.results.json ../tools/e2e-commons/results/${ type }.test.results.json` );
+	mv packages/e2e-tests/specs/performance/post-editor.test.results.json ../tools/e2e-commons/results/${ type }.${ id }.test.results.json` );
+}
+
+async function testRun( type, id ) {
+	console.log( `Starting test run #${ id } for ${ type }` );
+	envReset();
+	await envSetup( type );
+	runTests( type, id );
+	console.log( `Done with #${ id } for ${ type }` );
 }
 
 async function main() {
-	envReset();
-	await envSetup( process.argv[ 2 ] );
-	runTests( process.argv[ 2 ] );
+	for ( let i = 0; i < numAttempts; i++ ) {
+		await testRun( 'base', i );
+		await testRun( 'jetpack', i );
+	}
 }
 
-main().then( () => console.log( 'Done!' ) );
+function mergeResults( type ) {
+	const objs = [];
+	for ( let i = 0; i < numAttempts; i++ ) {
+		const file = `results/${ type }.${ i }.test.results.json`;
+		objs.push( JSON.parse( readFileSync( file ) ) );
+	}
+
+	const out = _.mergeWith( {}, ...objs, ( objValue, srcValue ) => {
+		if ( _.isArray( objValue ) ) {
+			return objValue.concat( srcValue );
+		}
+	} );
+
+	writeFileSync( `results/${ type }.test.results.json`, JSON.stringify( out ) );
+}
+
+main().then( () => {
+	mergeResults( 'base' );
+	mergeResults( 'jetpack' );
+	console.log( 'Done!' );
+} );
