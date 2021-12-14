d482edfacf728d1aac387c1435d609fe97af0c4b
Matthew Denton <matt@mdbitz.com>
2021-12-14 08:56:54 -0500

    Test simplification.

--- projects/plugins/jetpack/tests/php/media/test-class.jetpack-media-extractor.php
+++ projects/plugins/jetpack/tests/php/media/test-class.jetpack-media-extractor.php
@@ -579,7 +579,7 @@ EOT;
 	}
 
 	/**
-	 * Verify alt_text is extracted
+	 * Verify alt_text is extracted.
 	 *
 	 * @author mdbitz
 	 * @covers Jetpack_Media_Meta_Extractor::extract
@@ -588,31 +588,7 @@ EOT;
 	public function test_mediaextractor_alt_text() {
 		$post_id = $this->factory->post->create(
 			array(
-				'post_author'           => '23314024',
-				'post_date'             => '2013-10-25 16:43:34',
-				'post_date_gmt'         => '2013-10-25 16:43:34',
-				'post_content'          => 'Sed dapibus ut mauris imperdiet volutpat. <img src="https://example.org/assets/test.jpg" alt="red green" /><img src="https://example.org/assets/test2.jpg" />Nullam in dolor vel nulla pulvinar accumsan facilisis quis lorem.
-			',
-				'post_title'            => 'Sed dapibus ut mauris imperdiet volutpat http vimeo...',
-				'post_excerpt'          => '',
-				'post_status'           => 'publish',
-				'comment_status'        => 'open',
-				'ping_status'           => 'open',
-				'post_password'         => '',
-				'post_name'             => 'sed-dapibus-ut-mauris-imperdiet-volutpat-http-vimeo',
-				'to_ping'               => '',
-				'pinged'                => '
-			http://vimeo.com/77120044/',
-				'post_modified'         => '2013-10-28 22:54:50',
-				'post_modified_gmt'     => '2013-10-28 22:54:50',
-				'post_content_filtered' => '',
-				'post_parent'           => 0,
-				'guid'                  => 'http://breakmyblog.wordpress.com/2013/10/25/sed-dapibus-ut-mauris-imperdiet-volutpat-http-vimeo/',
-				'menu_order'            => 0,
-				'post_type'             => 'post',
-				'post_mime_type'        => '',
-				'comment_count'         => '0',
-				'filter'                => 'raw',
+				'post_content' => 'Sed dapibus ut mauris imperdiet volutpat. <img src="https://example.org/assets/test.jpg" alt="red green" /> yellow <img src="https://example.org/assets/test2.jpg" />Nullam in dolor vel nulla pulvinar accumsan facilisis quis lorem.',
 			)
 		);
 
