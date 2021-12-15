7590dd84943482cb40d6c8398e83b4d885a826f1
Caleb Bauermeister <cbauerman@gmail.com>
2021-12-15 13:24:03 -0800

    [not verified] refine plugin dash items

--- projects/plugins/jetpack/_inc/client/components/plugin-dash-item/index.jsx
+++ projects/plugins/jetpack/_inc/client/components/plugin-dash-item/index.jsx
@@ -4,20 +4,28 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 import apiFetch from '@wordpress/api-fetch';
+import { Spinner } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
-import DashItem from 'components/dash-item';
+import Card from 'components/card';
 import {
+	fetchPluginsData as dispatchFetchPluginsData,
 	isPluginActive,
 	isPluginInstalled,
 	isFetchingPluginsData as getIsFetchingPluginsData,
 } from 'state/site/plugins';
 import JetpackBanner from 'components/jetpack-banner';
+import SectionHeader from 'components/section-header';
 import { getSiteAdminUrl } from 'state/initial-state';
+
+/**
+ * Style dependencies
+ */
+import './style.scss';
 
 class PluginDashItem extends Component {
 	static propTypes = {
@@ -33,71 +41,134 @@ class PluginDashItem extends Component {
 		siteAdminUrl: PropTypes.string,
 	};
 
+	state = {
+		isActivating: false,
+		isInstalling: false,
+	};
+
 	activateOrInstallPlugin = () => {
-		apiFetch( {
+		const { fetchPluginsData, pluginIsActive, pluginIsInstalled, pluginSlug } = this.props;
+
+		if ( ! pluginIsInstalled ) {
+			this.setState( { isInstalling: true } );
+		} else if ( ! pluginIsActive ) {
+			this.setState( { isActivating: true } );
+		} else if ( pluginIsInstalled && pluginIsActive ) {
+			// do not try to do anything to an installed, active plugin
+			return Promise.resolve();
+		}
+
+		return apiFetch( {
 			path: '/jetpack/v4/plugins',
 			method: 'POST',
 			data: {
-				slug: this.props.pluginSlug,
+				slug: pluginSlug,
 				status: 'active',
 			},
-		} ).then( () => {} );
+		} )
+			.then( () => {
+				return fetchPluginsData();
+			} )
+			.finally( () => {
+				this.setState( {
+					isActivating: false,
+					isInstalling: false,
+				} );
+			} );
 	};
 
-
-
-	renderNotInstalled() {
-		const { installOrActivatePrompt, pluginName } = this.props;
-
-		return (
-			<JetpackBanner
-				callToAction={ 
-					/* translators: "%s" is the name of the plugin. i.e. Boost, CRM, etc. */
-					sprintf( __( 'Install %s', 'jetpack' ), pluginName )
-				}
-				title={ installOrActivatePrompt }
-				onClick={ this.activateOrInstallPlugin }
-			/>
-		);
-	}
-
-	renderNotActivated() {
-		const { installOrActivatePrompt, pluginName } = this.props;
-
-		return (
-			<JetpackBanner
-				callToAction={ 
-					/* translators: "%s" is the name of the plugin. i.e. Boost, CRM, etc. */
-					sprintf( __( 'Activate %s', 'jetpack' ), pluginName )
-				}
-				title={ installOrActivatePrompt }
-				onClick={ this.activateOrInstallPlugin }
-			/>
-		);
-	}
-
 	renderContent() {
-		const { pluginIsActive, pluginIsInstalled } = this.props;
+		const {
+			isFetchingPluginsData,
+			pluginName,
+			pluginIsActive,
+			pluginIsInstalled,
+			installOrActivatePrompt,
+		} = this.props;
+		const { isInstalling, isActivating } = this.state;
 
-		if ( ! pluginIsInstalled ) {
-			return this.renderNotInstalled();
+		if ( isFetchingPluginsData ) {
+			return (
+				<Card className="plugin-dash-item__content">
+					<p>{ 'Loading...' }</p>
+				</Card>
+			);
+		} else if ( isInstalling ) {
+			return (
+				<Card className="plugin-dash-item__content">
+					<Spinner />
+					<p>
+						{ sprintf(
+							/* translators: "%s" is the name of the plugin. i.e. Boost, CRM, etc. */
+							__( 'Installing %s', 'jetpack' ),
+							pluginName
+						) }
+					</p>
+				</Card>
+			);
+		} else if ( isActivating ) {
+			return (
+				<Card className="plugin-dash-item__content">
+					<Spinner />
+					<p>
+						{ sprintf(
+							/* translators: "%s" is the name of the plugin. i.e. Boost, CRM, etc. */
+							__( 'Activating %s', 'jetpack' ),
+							pluginName
+						) }
+					</p>
+				</Card>
+			);
+		} else if ( ! pluginIsInstalled ) {
+			return (
+				<JetpackBanner
+					callToAction={ sprintf(
+						/* translators: "%s" is the name of the plugin. i.e. Boost, CRM, etc. */
+						__( 'Install %s', 'jetpack' ),
+						pluginName
+					) }
+					title={ installOrActivatePrompt }
+					onClick={ this.activateOrInstallPlugin }
+				/>
+			);
 		} else if ( ! pluginIsActive ) {
-			return this.renderNotActivated();
-		} else {
-			return <div></div>;
+			return (
+				<JetpackBanner
+					callToAction={ sprintf(
+						/* translators: "%s" is the name of the plugin. i.e. Boost, CRM, etc. */
+						__( 'Activate %s', 'jetpack' ),
+						pluginName
+					) }
+					title={ installOrActivatePrompt }
+					onClick={ this.activateOrInstallPlugin }
+				/>
+			);
 		}
+		return (
+			<Card className="plugin-dash-item__content">
+				<p>{ 'Plugin is installed & active.' }</p>
+			</Card>
+		);
 	}
 
 	render() {
 		const { pluginName } = this.props;
 
-		return <DashItem label={ pluginName } isModule={ false } overrideContent={ this.renderContent() } />;
+		return (
+			<div className="plugin-dash-item">
+				<SectionHeader className="plugin-dash-item__section-header" label={ pluginName } />
+				{ this.renderContent() }
+			</div>
+		);
 	}
 }
 
-export default connect( ( state, ownProps ) => ( {
-	isFetchingPluginsData: getIsFetchingPluginsData( state ),
-	pluginIsInstalled: isPluginInstalled( state, ownProps.pluginFile ),
-	pluginIsActive: isPluginActive( state, ownProps.pluginFile ),
-	siteAdminUrl: getSiteAdminUrl( state ),
-} ) )( PluginDashItem );
+export default connect(
+	( state, ownProps ) => ( {
+		isFetchingPluginsData: getIsFetchingPluginsData( state ),
+		pluginIsInstalled: isPluginInstalled( state, ownProps.pluginFile ),
+		pluginIsActive: isPluginActive( state, ownProps.pluginFile ),
+		siteAdminUrl: getSiteAdminUrl( state ),
+	} ),
+	dispatch => ( { fetchPluginsData: () => dispatch( dispatchFetchPluginsData() ) } )
+)( PluginDashItem );
