e6c7d5dcada1d67f66e4a4edfe0f8588046851cb
Brad Jorsch <brad.jorsch@automattic.com>
2021-12-16 15:55:36 -0500

    Fix for php 5.6

--- projects/packages/phpcs-filter/stdin-bootstrap.php
+++ projects/packages/phpcs-filter/stdin-bootstrap.php
@@ -8,38 +8,40 @@
 
 use PHP_CodeSniffer\Autoload;
 
-( function () {
-	global $runner;
+call_user_func(
+	function () {
+		global $runner;
 
-	$config = $runner->config;
-	if ( ! $config->stdin || ! $config->stdinPath || ! $config->filter ) {
-		return;
-	}
-	$path = realpath( $config->stdinPath );
-	if ( ! $path ) {
-		return;
-	}
-
-	// This should match the logic in phpcs's FileList::getFilterClass().
-	if ( strpos( $config->filter, '.' ) !== false ) {
-		// This is a path to a custom filter class.
-		$filename = realpath( $config->filter );
-		if ( false === $filename ) {
+		$config = $runner->config;
+		if ( ! $config->stdin || ! $config->stdinPath || ! $config->filter ) {
 			return;
 		}
-		$filterClass = Autoload::loadFile( $filename );
-	} else {
-		$filterClass = '\\PHP_CodeSniffer\\Filters\\' . $config->filter;
-	}
+		$path = realpath( $config->stdinPath );
+		if ( ! $path ) {
+			return;
+		}
 
-	// Ask the filter which config and ruleset it should use for the path.
-	$iter   = new RecursiveArrayIterator( array( $path ) );
-	$filter = new $filterClass( $iter, '/', $config, $runner->ruleset );
-	if ( is_callable( array( $filter, 'getConfigAndRuleset' ) ) ) {
-		try {
-			list( $runner->config, $runner->ruleset ) = $filter->getConfigAndRuleset( $path );
-		} catch ( Exception $ex ) { // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedCatch
-			// Don't care.
+		// This should match the logic in phpcs's FileList::getFilterClass().
+		if ( strpos( $config->filter, '.' ) !== false ) {
+			// This is a path to a custom filter class.
+			$filename = realpath( $config->filter );
+			if ( false === $filename ) {
+				return;
+			}
+			$filterClass = Autoload::loadFile( $filename );
+		} else {
+			$filterClass = '\\PHP_CodeSniffer\\Filters\\' . $config->filter;
+		}
+
+		// Ask the filter which config and ruleset it should use for the path.
+		$iter   = new RecursiveArrayIterator( array( $path ) );
+		$filter = new $filterClass( $iter, '/', $config, $runner->ruleset );
+		if ( is_callable( array( $filter, 'getConfigAndRuleset' ) ) ) {
+			try {
+				list( $runner->config, $runner->ruleset ) = $filter->getConfigAndRuleset( $path );
+			} catch ( Exception $ex ) { // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedCatch
+				// Don't care.
+			}
 		}
 	}
-} )();
+);
