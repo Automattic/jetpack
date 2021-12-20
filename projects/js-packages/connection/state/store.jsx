e76a5bce41d78fd47711eec21251e73054a90f9f
leogermani <leogermani@gmail.com>
2021-12-20 11:48:07 -0300

    add resolvers and controls back after merge

--- projects/js-packages/connection/state/store.jsx
+++ projects/js-packages/connection/state/store.jsx
@@ -7,6 +7,8 @@ import reducer from './reducers';
 import actions from './actions';
 import selectors from './selectors';
 import storeHolder from './store-holder';
+import resolvers from './resolvers';
+import controls from './controls';
 
 const STORE_ID = 'jetpack-connection';
 
@@ -22,6 +24,8 @@ storeHolder.mayBeInit( STORE_ID, {
 	reducer,
 	actions,
 	selectors,
+	resolvers,
+	controls,
 	initialState: initialState || {},
 } );
 
