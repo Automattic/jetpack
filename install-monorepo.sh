759b2a1ff9edd97e33df02454c1422c4857894e3
sdixon194 <steve.dixon@automattic.com>
2021-12-23 15:21:23 -0500

    add checks and installation for brew and nvm

--- install-monorepo.sh
+++ install-monorepo.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 set -eo pipefail
+#set -x
+
 
 cd $(dirname "${BASH_SOURCE[0]}")/..
 BASE=$PWD
-. "$BASE/jetpack/tools/includes/check-osx-bash-version.sh"
 . "$BASE/jetpack/tools/includes/chalk-lite.sh"
 . "$BASE/jetpack/tools/includes/plugin-functions.sh"
 
@@ -21,3 +22,39 @@ function usage {
 if [[ $1 ]]; then
 	usage
 fi
+
+# Check if we're on a Mac or Linux, bail if we're not.
+# First check OS.
+OS="$(uname)"
+if [[ "$OS" == "Linux" ]]; then
+	HOMEBREW_ON_LINUX=1
+elif [[ "$OS" != "Darwin" ]]; then
+	abort "Homebrew is only supported on macOS and Linux."
+fi
+
+# Check of Homebrew and nvm are installed
+echo "Checking if Homebrew is installed..."
+if ! command -v brew &> /dev/null; then
+    echo "Installing Homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+	echo "Updating brew"
+    brew update
+fi
+
+echo "Checking if NVM is installed..."
+if ! command -v nvm &> /dev/null ; then
+    echo "Installing nvm"
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+else
+	echo "Updating nvm"
+    nvm update
+fi
+
+#install Bash
+brew install bash
+
+# Instsall JQ
+brew install jq 
+
+# Install node inside the Jetpack monorepo to pickup the .nvmrc 
