678ddff7fd9fdca4302547a763757ccd998fa0a0
Renzo Canepa <rcanepag@gmail.com>
2022-01-04 12:57:49 -0300

    Compute label on return statement

--- projects/js-packages/licensing/components/activation-screen-success-info/primary-link/index.jsx
+++ projects/js-packages/licensing/components/activation-screen-success-info/primary-link/index.jsx
@@ -14,25 +14,24 @@ import './style.scss';
 const PrimaryLink = props => {
 	const { currentRecommendationsStep, siteAdminUrl } = props;
 
-	const buttonLabel =
-		currentRecommendationsStep !== 'not-started'
-			? __( 'View my plans', 'jetpack' )
-			: __( 'Configure my site', 'jetpack' );
+	const linkToMyPlanSection = currentRecommendationsStep !== 'not-started';
 
-	const buttonLink =
-		currentRecommendationsStep !== 'not-started'
+	const buttonLink = linkToMyPlanSection
 			? siteAdminUrl + 'admin.php?page=jetpack#/my-plan'
 			: siteAdminUrl + 'admin.php?page=jetpack#/recommendations';
 
 	return (
 		<Button className="jp-license-activation-screen-success-info--button" href={ buttonLink }>
-			{ buttonLabel }
+			{ linkToMyPlanSection
+				? __( 'View my plans', 'jetpack' )
+				: __( 'Configure my site', 'jetpack' )
+			}
 		</Button>
 	);
 };
 
 PrimaryLink.propTypes = {
-	siteAdminUrl: PropTypes.string,
+	siteAdminUrl: PropTypes.string.isRequired,
 	currentRecommendationsStep: PropTypes.string,
 };
 
