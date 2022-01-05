18ae23787771857d969b885f049a6ae44e266f79
Siobhan <siobhan@automattic.com>
2022-01-05 21:59:17 +0000

    Formatting corrections

--- projects/plugins/jetpack/extensions/blocks/tiled-gallery/edit.native.js
+++ projects/plugins/jetpack/extensions/blocks/tiled-gallery/edit.native.js
@@ -52,10 +52,7 @@ const TiledGalleryEdit = props => {
 
 	const { replaceInnerBlocks, updateBlockAttributes } = useDispatch( blockEditorStore );
 
-	const displayedColumns = Math.min(
-		columns,
-		dynamicColumnsNumber(attributeImages.length)
-	);
+	const displayedColumns = Math.min( columns, dynamicColumnsNumber( attributeImages.length ) );
 
 	useEffect( () => {
 		const { width } = sizes || {};
