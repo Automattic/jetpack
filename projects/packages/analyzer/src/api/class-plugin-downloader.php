142d9d0b9330ccca5c3681e798d9262fe071d8c2
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2022-01-06 20:41:11 +0800

    update error message

--- projects/packages/analyzer/src/api/class-plugin-downloader.php
+++ projects/packages/analyzer/src/api/class-plugin-downloader.php
@@ -56,7 +56,7 @@ class PluginDownloader {
 		$zip = new \ZipArchive;
 
 		if ($zip->open($file_path) !== true) {
-			throw new \Exception("OH, NO!");
+			throw new \Exception("Failed to open plugin's zip file.");
 		}
 		for($i = 0; $i < $zip->numFiles; $i++) {
 			$filename = $target_path . '/' . str_replace($zip->getNameIndex(0), '', $zip->getNameIndex($i));
