a0b6cc4c1187949a86154404c68cdf5d4306de93
Steve D <33553323+sdixon194@users.noreply.github.com>
2022-01-10 15:23:58 -0500

    Update install-monorepo.sh
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>

--- install-monorepo.sh
+++ install-monorepo.sh
@@ -33,7 +33,7 @@ if [[ -z "$(command -v brew)" ]]; then
     echo "Installing Homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	if [[ ON_LINUX ]]; then # Add homebrew to PATH
-		echo 'eval "$(/home/ubuntu/.linuxbrew/bin/brew shellenv)"' >> /home/ubuntu/.profile
+		echo 'eval "$("$HOME/.linuxbrew/bin/brew" shellenv)"' >> "$HOME/.profile"
 		eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 		PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
 		hash -r 
