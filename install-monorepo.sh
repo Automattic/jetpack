35b8f32a4bc692252c5b7f52682c37728119ccfd
Steve D <33553323+sdixon194@users.noreply.github.com>
2022-01-11 11:27:58 -0500

    Update install-monorepo.sh
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>

--- install-monorepo.sh
+++ install-monorepo.sh
@@ -85,7 +85,7 @@ brew link php@$PHP_VERSION
 
 echo "Checking composer..."
 if ! command -v composer &>/dev/null; then
-echo "Installing Composer"
+	echo "Installing Composer"
 	EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
 	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 	ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
