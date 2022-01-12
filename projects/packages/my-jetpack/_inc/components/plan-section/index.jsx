0b9068a5692a6d23097bfb8a984d243ab0f058d0
retrofox <rdsuarez@gmail.com>
2022-01-12 11:20:27 -0300

    [not verified] my-jetpack: consume usePlan hook in PlanSection

--- projects/packages/my-jetpack/_inc/components/plan-section/index.jsx
+++ projects/packages/my-jetpack/_inc/components/plan-section/index.jsx
@@ -9,10 +9,22 @@ import React from 'react';
 import { __ } from '@wordpress/i18n';
 
 /**
+ * Internal dependencies
+ */
+import usePlan from '../../hooks/use-plan';
+
+/**
  * Plan section component.
  *
  * @returns {object} PlanSection React component.
  */
 export default function PlanSection() {
-	return <h1>{ __( 'My Plan', 'jetpack-my-jetpack' ) }</h1>;
+	const { name } = usePlan();
+	return (
+		<div>
+			<h1>{ __( 'Your plan', 'jetpack-my-jetpack' ) }</h1>
+			<p>{ __( 'The extra power you added to your Jetpack.', 'jetpack-my-jetpack' ) }</p>
+			{ name }
+		</div>
+	);
 }
