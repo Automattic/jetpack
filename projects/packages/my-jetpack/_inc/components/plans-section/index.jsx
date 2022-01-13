09cb081c5266f1e5fad6dfe8ca0adc8f8a4ab848
oskosk <oskosk@gmail.com>
2022-01-13 09:27:08 -0300

    [not verified] Update title copy

--- projects/packages/my-jetpack/_inc/components/plans-section/index.jsx
+++ projects/packages/my-jetpack/_inc/components/plans-section/index.jsx
@@ -22,7 +22,7 @@ export default function PlansSection() {
 	const { name, billingPeriod } = usePlan();
 	return (
 		<div>
-			<h1>{ __( 'Your plan', 'jetpack-my-jetpack' ) }</h1>
+			<h1>{ __( 'My Plan', 'jetpack-my-jetpack' ) }</h1>
 			<p>{ __( 'The extra power you added to your Jetpack.', 'jetpack-my-jetpack' ) }</p>
 
 			<h2>{ name }</h2>
