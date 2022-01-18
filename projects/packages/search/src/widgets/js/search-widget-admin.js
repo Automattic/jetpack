3ad390a0ea1b2ea51e701cfa80f64410feb3d610
Jasper Kang <jasper.kang@automattic.com>
2022-01-19 10:52:22 +1300

    revert auto changes

--- projects/packages/search/src/widgets/js/search-widget-admin.js
+++ projects/packages/search/src/widgets/js/search-widget-admin.js
@@ -1,10 +1,11 @@
 /* globals jetpack_search_filter_admin, jQuery, analytics */
 
-( function ( $, args ) {
-	var defaultFilterCount =
-		'undefined' !== typeof args && args.defaultFilterCount ? args.defaultFilterCount : 5; // Just in case we couldn't find the defaultFiltercount arg
+( function( $, args ) {
+	var defaultFilterCount = ( 'undefined' !== typeof args && args.defaultFilterCount ) ?
+		args.defaultFilterCount :
+		5; // Just in case we couldn't find the defaultFiltercount arg
 
-	$( document ).ready( function () {
+	$( document ).ready( function() {
 		setListeners();
 
 		window.JetpackSearch = window.JetpackSearch || {};
@@ -23,26 +24,28 @@
 			type = container.find( '.filter-select' ).val();
 
 		if ( 'taxonomy' === type ) {
-			placeholder = container.find( '.taxonomy-select option:selected' ).text().trim();
+			placeholder = container.find('.taxonomy-select option:selected').text().trim();
 		} else if ( 'date_histogram' === type && args && args.i18n ) {
-			isModified = -1 !== container.find( '.date-field-select' ).val().indexOf( 'modified' );
-			isMonth = 'month' === container.find( '.date-interval-select' ).val();
+			isModified = ( -1 !== container.find( '.date-field-select' ).val().indexOf( 'modified' ) );
+			isMonth = ( 'month' === container.find( '.date-interval-select' ).val() );
 
 			if ( isMonth ) {
-				placeholder = isModified ? args.i18n.monthUpdated : args.i18n.month;
+				placeholder = isModified ?
+					args.i18n.monthUpdated :
+					args.i18n.month;
 			} else {
-				placeholder = isModified ? args.i18n.yearUpdated : args.i18n.year;
+				placeholder = isModified ?
+					args.i18n.yearUpdated :
+					args.i18n.year;
 			}
 		} else {
-			placeholder = container.find( '.filter-select option:selected' ).text().trim();
+			placeholder = container.find('.filter-select option:selected').text().trim();
 		}
 
-		$( container )
-			.find( '.jetpack-search-filters-widget__title input' )
-			.prop( 'placeholder', placeholder );
+		$( container ).find('.jetpack-search-filters-widget__title input').prop( 'placeholder', placeholder );
 	}
 
-	var addFilter = function ( filtersContainer, args ) {
+	var addFilter = function( filtersContainer, args ) {
 		var template = _.template(
 			filtersContainer
 				.closest( '.jetpack-search-filters-widget' )
@@ -52,18 +55,20 @@
 		generateFilterTitlePlaceholder( filtersContainer.append( template( args ) ) );
 	};
 
-	var setListeners = function ( widget ) {
-		widget = 'undefined' === typeof widget ? $( '.jetpack-search-filters-widget' ) : widget;
+	var setListeners = function( widget ) {
+		widget = ( 'undefined' === typeof widget ) ?
+			$( '.jetpack-search-filters-widget' ):
+			widget;
 
-		var getContainer = function ( el ) {
-			return $( el ).closest( '.jetpack-search-filters-widget__filter' );
+		var getContainer = function( el ) {
+			return $( el ).closest('.jetpack-search-filters-widget__filter');
 		};
 
-		widget.on( 'change', '.filter-select', function () {
+		widget.on( 'change', '.filter-select', function() {
 			var select = $( this ),
 				selectVal = select.val(),
 				eventArgs = {
-					is_customizer: args.tracksEventData.is_customizer,
+					is_customizer: args.tracksEventData.is_customizer
 				};
 
 			eventArgs.type = selectVal;
@@ -79,9 +84,9 @@
 		} );
 
 		// enable showing sort controls only if showing search box is enabled
-		widget.on( 'change', '.jetpack-search-filters-widget__search-box-enabled', function () {
+		widget.on( 'change', '.jetpack-search-filters-widget__search-box-enabled', function() {
 			var checkbox = $( this ),
-				checkboxVal = checkbox.is( ':checked' ),
+				checkboxVal = checkbox.is(':checked'),
 				filterParent = checkbox.closest( '.jetpack-search-filters-widget' ),
 				sortControl = filterParent.find( '.jetpack-search-filters-widget__sort-controls-enabled' );
 
@@ -97,7 +102,7 @@
 			}
 		} );
 
-		widget.on( 'change', '.jetpack-search-filters-widget__sort-controls-enabled', function () {
+		widget.on( 'change', '.jetpack-search-filters-widget__sort-controls-enabled', function() {
 			if ( $( this ).is( ':checked' ) ) {
 				trackAndBumpMCStats( 'enabled_sort_controls', args.tracksEventData );
 			} else {
@@ -105,51 +110,40 @@
 			}
 		} );
 
-		widget.on(
-			'click',
-			'.jetpack-search-filters-widget__post-types-select input[type="checkbox"]',
-			function ( e ) {
-				var t = $( this );
-				var siblingsChecked = t
-					.closest( '.jetpack-search-filters-widget' )
-					.find(
-						'.jetpack-search-filters-widget__post-types-select input[type="checkbox"]:checked'
-					);
+		widget.on( 'click', '.jetpack-search-filters-widget__post-types-select input[type="checkbox"]', function( e ) {
+			var t = $( this );
+			var siblingsChecked = t.closest( '.jetpack-search-filters-widget' )
+				.find( '.jetpack-search-filters-widget__post-types-select input[type="checkbox"]:checked' );
 
-				if ( 0 === siblingsChecked.length ) {
-					e.preventDefault();
-					e.stopPropagation();
+			if ( 0 === siblingsChecked.length ) {
+				e.preventDefault();
+				e.stopPropagation();
 
-					trackAndBumpMCStats( 'attempted_no_post_types', args.tracksEventData );
-				}
+				trackAndBumpMCStats( 'attempted_no_post_types', args.tracksEventData );
 			}
-		);
+		} );
 
-		widget.on(
-			'change',
-			'.jetpack-search-filters-widget__post-types-select input[type="checkbox"]',
-			function () {
-				var t = $( this );
-				var eventArgs = {
-					is_customizer: args.tracksEventData.is_customizer,
-					post_type: t.val(),
-				};
-
-				if ( wp && wp.customize ) {
-					wp.customize.state( 'saved' ).set( false );
-				}
-
-				if ( t.is( ':checked' ) ) {
-					trackAndBumpMCStats( 'added_post_type', eventArgs );
-				} else {
-					trackAndBumpMCStats( 'removed_post_type', eventArgs );
-				}
-			}
-		);
-
-		widget.on( 'change', '.jetpack-search-filters-widget__sort-order', function () {
+		widget.on( 'change', '.jetpack-search-filters-widget__post-types-select input[type="checkbox"]', function() {
+			var t = $( this );
 			var eventArgs = {
 				is_customizer: args.tracksEventData.is_customizer,
+				post_type:  t.val()
+			};
+
+			if ( wp && wp.customize ) {
+				wp.customize.state( 'saved' ).set( false );
+			}
+
+			if ( t.is( ':checked' ) ) {
+				trackAndBumpMCStats( 'added_post_type', eventArgs );
+			} else {
+				trackAndBumpMCStats( 'removed_post_type', eventArgs );
+			}
+		} );
+
+		widget.on( 'change', '.jetpack-search-filters-widget__sort-order', function() {
+			var eventArgs = {
+				is_customizer: args.tracksEventData.is_customizer
 			};
 
 			eventArgs.order = $( this ).val();
@@ -161,9 +155,9 @@
 			trackAndBumpMCStats( 'changed_sort_order', eventArgs );
 		} );
 
-		widget.on( 'change', '.jetpack-search-filters-widget__taxonomy-select select', function () {
+		widget.on( 'change', '.jetpack-search-filters-widget__taxonomy-select select', function() {
 			var eventArgs = {
-				is_customizer: args.tracksEventData.is_customizer,
+				is_customizer: args.tracksEventData.is_customizer
 			};
 
 			eventArgs.taxonomy = $( this ).val();
@@ -177,9 +171,9 @@
 			trackAndBumpMCStats( 'changed_taxonomy', eventArgs );
 		} );
 
-		widget.on( 'change', 'select.date-field-select', function () {
+		widget.on( 'change', 'select.date-field-select', function() {
 			var eventArgs = {
-				is_customizer: args.tracksEventData.is_customizer,
+				is_customizer: args.tracksEventData.is_customizer
 			};
 
 			eventArgs.field = $( this ).val();
@@ -193,9 +187,9 @@
 			trackAndBumpMCStats( 'changed_date_field', eventArgs );
 		} );
 
-		widget.on( 'change', 'select.date-interval-select', function () {
+		widget.on( 'change', 'select.date-interval-select', function() {
 			var eventArgs = {
-				is_customizer: args.tracksEventData.is_customizer,
+				is_customizer: args.tracksEventData.is_customizer
 			};
 
 			eventArgs.interval = $( this ).val();
@@ -209,9 +203,9 @@
 			trackAndBumpMCStats( 'changed_date_interval', eventArgs );
 		} );
 
-		widget.on( 'change', 'input.filter-count', function () {
+		widget.on( 'change', 'input.filter-count', function() {
 			var eventArgs = {
-				is_customizer: args.tracksEventData.is_customizer,
+				is_customizer: args.tracksEventData.is_customizer
 			};
 
 			eventArgs.count = $( this ).val();
@@ -224,7 +218,7 @@
 		} );
 
 		// add filter button
-		widget.on( 'click', '.jetpack-search-filters-widget__add-filter', function ( e ) {
+		widget.on( 'click', '.jetpack-search-filters-widget__add-filter', function( e ) {
 			e.preventDefault();
 
 			var filtersContainer = $( this )
@@ -239,7 +233,7 @@
 				interval: '',
 				count: defaultFilterCount,
 				name_placeholder: '',
-				name: '',
+				name: ''
 			} );
 
 			if ( wp && wp.customize ) {
@@ -255,28 +249,22 @@
 			trackAndBumpMCStats( 'added_filter', args.tracksEventData );
 		} );
 
-		widget.on( 'click', '.jetpack-search-filters-widget__controls .delete', function ( e ) {
+		widget.on( 'click', '.jetpack-search-filters-widget__controls .delete', function( e ) {
 			e.preventDefault();
 			var filter = $( this ).closest( '.jetpack-search-filters-widget__filter' ),
 				eventArgs = {
-					is_customizer: args.tracksEventData.is_customizer,
+					is_customizer: args.tracksEventData.is_customizer
 				};
 
 			eventArgs.type = filter.find( '.filter-select' ).val();
 
 			switch ( eventArgs.type ) {
 				case 'taxonomy':
-					eventArgs.taxonomy = filter
-						.find( '.jetpack-search-filters-widget__taxonomy-select select' )
-						.val();
+					eventArgs.taxonomy = filter.find( '.jetpack-search-filters-widget__taxonomy-select select' ).val();
 					break;
 				case 'date_histogram':
-					eventArgs.dateField = filter
-						.find( '.jetpack-search-filters-widget__date-histogram-select:first select' )
-						.val();
-					eventArgs.dateInterval = filter
-						.find( '.jetpack-search-filters-widget__date-histogram-select:nth-child( 2 ) select' )
-						.val();
+					eventArgs.dateField = filter.find( '.jetpack-search-filters-widget__date-histogram-select:first select' ).val();
+					eventArgs.dateInterval = filter.find( '.jetpack-search-filters-widget__date-histogram-select:nth-child( 2 ) select' ).val();
 					break;
 			}
 
@@ -293,31 +281,30 @@
 		} );
 
 		// make the filters sortable
-		$( '.jetpack-search-filters-widget__filters' )
-			.sortable( {
-				placeholder: 'jetpack-search-filters-widget__filter-placeholder',
-				axis: 'y',
-				revert: true,
-				cancel: 'input,textarea,button,select,option,.jetpack-search-filters-widget__controls a',
-				change: function () {
-					if ( wp && wp.customize ) {
-						wp.customize.state( 'saved' ).set( false );
-					}
-				},
-				update: function ( e, ui ) {
-					$( ui.item ).find( 'input, textarea, select' ).change();
-				},
-			} )
-			.disableSelection();
+		$( '.jetpack-search-filters-widget__filters' ).sortable( {
+			placeholder: 'jetpack-search-filters-widget__filter-placeholder',
+			axis: 'y',
+			revert: true,
+			cancel: 'input,textarea,button,select,option,.jetpack-search-filters-widget__controls a',
+			change: function() {
+				if ( wp && wp.customize ) {
+					wp.customize.state( 'saved' ).set( false );
+				}
+			},
+			update: function( e, ui ) {
+				$( ui.item ).find( 'input, textarea, select' ).change();
+			}
+		} )
+		.disableSelection();
 	};
 
 	// When widgets are updated, remove and re-add listeners
-	$( document ).on( 'widget-updated widget-added', function ( e, widget ) {
-		var idBase = $( widget ).find( '.id_base' ).val(),
-			isJetpackSearch = idBase && 'jetpack-search-filters' === idBase;
+	$( document ).on( 'widget-updated widget-added', function( e, widget ) {
+		var idBase = $( widget ).find('.id_base').val(),
+			isJetpackSearch = ( idBase && ( 'jetpack-search-filters' === idBase ) );
 
 		if ( ! isJetpackSearch ) {
-			return;
+			 return;
 		}
 
 		// Intentionally not tracking widget additions and updates here as these events
@@ -334,11 +321,8 @@
 		widget.off( 'change', '.jetpack-search-filters-widget__taxonomy-select' );
 		widget.off( 'change', '.jetpack-search-filters-widget__date-histogram-select:first select' );
 		widget.off( 'change', '.jetpack-search-filters-widget__date-histogram-select:eq(1) select' );
-		widget.off(
-			'click',
-			'.jetpack-search-filters-widget__post-types-select input[type="checkbox"]'
-		);
-		widget.off( 'click', '.jetpack-search-filters-widget__add-filter' );
+		widget.off( 'click', '.jetpack-search-filters-widget__post-types-select input[type="checkbox"]' );
+		widget.off( 'click', '.jetpack-search-filters-widget__add-filter');
 
 		setListeners( widget );
 	} );
@@ -359,19 +343,10 @@
 			extraProps = {};
 		}
 
-		if (
-			eventName &&
-			eventName.length &&
-			'undefined' !== typeof analytics &&
-			analytics.tracks &&
-			analytics.mc
-		) {
+		if ( eventName && eventName.length && 'undefined' !== typeof analytics && analytics.tracks && analytics.mc ) {
 			// Format for Tracks
 			eventName = eventName.replace( /-/g, '_' );
-			eventName =
-				eventName.indexOf( 'jetpack_widget_search_' ) !== 0
-					? 'jetpack_widget_search_' + eventName
-					: eventName;
+			eventName = eventName.indexOf( 'jetpack_widget_search_' ) !== 0 ? 'jetpack_widget_search_' + eventName : eventName;
 			analytics.tracks.recordEvent( eventName, extraProps );
 
 			// Now format for MC stats
