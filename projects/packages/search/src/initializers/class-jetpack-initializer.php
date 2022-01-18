8bf2c21ef76d1d0a02b488cab8dffd305fb32c8a
Jasper Kang <jasper.kang@automattic.com>
2022-01-19 10:56:42 +1300

    we do not need widget register code in Jetpack

--- projects/packages/search/src/initializers/class-jetpack-initializer.php
+++ projects/packages/search/src/initializers/class-jetpack-initializer.php
@@ -34,7 +34,8 @@ class Jetpack_Initializer extends Initializer {
 			return;
 		}
 
-		require_once JETPACK__PLUGIN_DIR . 'modules/widgets/jetpack-search.php';
+		// registers Jetpack Search widget.
+		add_action( 'widgets_init', array( 'Automattic\Jetpack\Search\Jetpack_Initializer', 'jetpack_search_widget_init' ) );
 
 		/**
 		 * Location of built Jetpack Search assets, does not include trailing slash.
@@ -75,5 +76,22 @@ class Jetpack_Initializer extends Initializer {
 	 */
 	public static function is_search_supported() {
 		return ( new Plan() )->supports_search();
+	}
+
+	/**
+	 * Register the widget if Jetpack Search is available and enabled.
+	 */
+	public static function jetpack_search_widget_init() {
+		if (
+		! Jetpack::is_connection_ready()
+		|| ( method_exists( 'Jetpack_Plan', 'supports' ) && ! Jetpack_Plan::supports( 'search' ) )
+		|| ! Jetpack::is_module_active( 'search' )
+		) {
+			return;
+		}
+
+		// There won't be multiple widgets registered when Search stand alone plugin registers it again.
+		// Because the function tests the hash of the class, if they are the same, just register again.
+		register_widget( 'Automattic\Jetpack\Search\Search_Widget' );
 	}
 }
