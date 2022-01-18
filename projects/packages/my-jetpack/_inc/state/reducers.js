fec7266a5881c731ee26b76ef8b0d2035848d2bc
Damián Suárez <rdsuarez@gmail.com>
2022-01-17 21:18:02 -0300

    My Jetpack: reduxify purchases data (#22368)
    Co-authored-by: oskosk <oskosk@gmail.com>

--- projects/packages/my-jetpack/_inc/state/reducers.js
+++ projects/packages/my-jetpack/_inc/state/reducers.js
@@ -3,12 +3,37 @@
  */
 import { combineReducers } from '@wordpress/data';
 
+/**
+ * Internal dependencies
+ */
+import { SET_PURCHASES, SET_PURCHASES_IS_FETCHING } from './actions';
+
 const products = ( state = {} ) => {
 	return state;
 };
 
+const purchases = ( state = {}, action ) => {
+	switch ( action.type ) {
+		case SET_PURCHASES_IS_FETCHING:
+			return {
+				...state,
+				isFetching: action.isFetching,
+			};
+
+		case SET_PURCHASES:
+			return {
+				...state,
+				items: action?.purchases || [],
+			};
+
+		default:
+			return state;
+	}
+};
+
 const reducers = combineReducers( {
 	products,
+	purchases,
 } );
 
 export default reducers;
