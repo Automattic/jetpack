60af10d223dcd840ea687f7e77a8a64648d63c14
retrofox <rdsuarez@gmail.com>
2022-01-18 16:22:03 -0300

    my-jetpack: add /site/products/$product endpoint

--- projects/packages/my-jetpack/src/class-rest-products.php
+++ projects/packages/my-jetpack/src/class-rest-products.php
@@ -26,6 +26,16 @@ class REST_Products {
 				'permission_callback' => __CLASS__ . '::permissions_callback',
 			)
 		);
+
+		register_rest_route(
+			'my-jetpack/v1/',
+			'/site/products/(?P<product>[a-z\-]+)',
+			array(
+				'methods'             => \WP_REST_Server::READABLE,
+				'callback'            => __CLASS__ . '::get_product',
+				'permission_callback' => __CLASS__ . '::permissions_callback',
+			)
+		);
 	}
 
 	/**
@@ -48,5 +58,35 @@ class REST_Products {
 	public static function get_products() {
 		$products = Products::get_products();
 		return rest_ensure_response( $products, 200 );
+	}
+
+	/**
+	 * Site products endpoint.
+	 *
+	 * @param \WP_REST_Request $request The request object.
+	 * @return array of site products list.
+	 */
+	public static function get_product( $request ) {
+		$params = $request->get_params();
+
+		if ( ! isset( $params['product'] ) ) {
+			return new \WP_Error(
+				'my_jetpack_missing_product',
+				__( 'Missing product param', 'jetpack-my-jetpack' ),
+				array( 'status' => 404 )
+			);
+		}
+
+		$products     = Products::get_products();
+		$product_slug = $params['product'];
+		if ( ! array_key_exists( $product_slug, $products ) ) {
+			return new \WP_Error(
+				'my_jetpack_product_not_found',
+				__( 'Product not found', 'jetpack-my-jetpack' ),
+				array( 'status' => 404 )
+			);
+		}
+
+		return rest_ensure_response( $products[ $product_slug ], 200 );
 	}
 }
