9bc0445cca144f1e73d257cbe693a9683ca46aba
Brad Jorsch <brad.jorsch@automattic.com>
2022-01-20 15:17:00 -0500

    Add a `--git-changed` option so we can replace list-changed-projects.php too

--- tools/cli/commands/dependencies.js
+++ tools/cli/commands/dependencies.js
@@ -1,7 +1,30 @@
 /**
+ * External dependencies
+ */
+import chalk from 'chalk';
+import { spawn } from 'child_process';
+import ignore from 'ignore';
+
+/**
  * Internal dependencies
  */
 import { getDependencies, filterDeps, getBuildOrder } from '../helpers/dependencyAnalysis.js';
+
+// Files that mean --git-changed should report all projects as changed.
+const infrastructureFiles = new Set( [
+	'tools/cli/commands/dependencies.js',
+	'tools/cli/helpers/dependencyAnalysis.js',
+	'.github/actions/tool-setup/action.yml',
+	'.github/files/list-changed-projects.sh',
+	'.github/files/generate-ci-matrix.php',
+	'.github/files/process-coverage.sh',
+	'.github/files/setup-wordpress-env.sh',
+	'.github/versions.sh',
+	'.github/workflows/tests.yml',
+] );
+
+// Files to ignore for --git-changed.
+const ignoreFiles = [ '**/*.md', '**/*.txt' ];
 
 export const command = 'dependencies <subcommand> [projects...]';
 export const describe = 'Report monorepo project dependencies';
@@ -22,6 +45,10 @@ export function builder( yargs ) {
 		} )
 		.positional( 'projects', {
 			describe: 'Only include dependencies relevant to these projects.',
+			type: 'string',
+		} )
+		.option( 'git-changed', {
+			describe: 'Include projects changed in git. Value is as appropriate for `git diff`.',
 			type: 'string',
 		} )
 		.option( 'add-dependencies', {
@@ -59,6 +86,62 @@ export async function handler( argv ) {
 		deps.delete( 'monorepo' );
 	}
 
+	if ( argv.gitChanged ) {
+		const stdout = await new Promise( ( resolve, reject ) => {
+			// eslint-disable-next-line no-shadow
+			let stdout = '';
+			const p = spawn(
+				'git',
+				[
+					'-c',
+					'core.quotepath=off',
+					'diff',
+					'--no-renames',
+					'--name-only',
+					argv.gitChanged,
+					'--',
+				],
+				{
+					cwd: process.cwd(),
+					stdio: [ 'ignore', 'pipe', 'inherit' ],
+				}
+			);
+			p.stdout.on( 'data', data => {
+				stdout += data;
+			} );
+			p.on( 'error', reject );
+			p.on( 'close', code =>
+				code === 0 ? resolve( stdout ) : reject( new Error( `Git exited with code ${ code }` ) )
+			);
+		} );
+
+		const projset = new Set( argv.projects );
+		const ig = ignore().add( ignoreFiles );
+		const debug = argv.v ? m => console.error( chalk.stderr.blue( m ) ) : () => {};
+		for ( const file of stdout.split( '\n' ).filter( v => v.length ) ) {
+			if ( infrastructureFiles.has( file ) ) {
+				debug( `Diff touches infrastructure file ${ file }, considering all projects as changed.` );
+				deps.keys().forEach( k => projset.add( k ) );
+				break;
+			}
+			const slug = file.match( /^projects\/([^/]+\/[^/]+)\// )?.[ 1 ] || 'monorepo';
+			if ( ! projset.has( slug ) ) {
+				if ( ig.ignores( file ) ) {
+					debug( `Diff touches ${ file }, which is on the ignore list.` );
+				} else {
+					debug( `Diff touches ${ file }, marking ${ slug } as changed.` );
+					projset.add( slug );
+				}
+			}
+		}
+		argv.projects = [ ...projset ];
+
+		// If the diff touched nothing, we output nothing.
+		if ( projset.size === 0 ) {
+			deps.clear();
+		}
+	}
+
 	if ( argv.projects.length ) {
 		deps = filterDeps( deps, argv.projects, {
 			dependencies: argv.addDependencies,
@@ -86,7 +169,9 @@ export async function handler( argv ) {
 	}
 
 	if ( argv.subcommand === 'list' ) {
-		console.log( Array.from( deps.keys() ).join( '\n' ) );
+		if ( deps.size ) {
+			console.log( Array.from( deps.keys() ).join( '\n' ) );
+		}
 		return;
 	}
 
