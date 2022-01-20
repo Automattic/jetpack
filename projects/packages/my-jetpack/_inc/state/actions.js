f91a659d9817228ba677c68a294543bc91213596
retrofox <rdsuarez@gmail.com>
2022-01-20 14:18:06 -0300

    [not verified] my-jetpack: add actions to active product

--- projects/packages/my-jetpack/_inc/state/actions.js
+++ projects/packages/my-jetpack/_inc/state/actions.js
@@ -1,7 +1,9 @@
 const SET_PURCHASES_IS_FETCHING = 'SET_PURCHASES_IS_FETCHING';
 const FETCH_PURCHASES = 'FETCH_PURCHASES';
 const SET_PURCHASES = 'SET_PURCHASES';
+const SET_PRODUCT_ACTION_ERROR = 'SET_PRODUCT_ACTION_ERROR';
 const ACTIVATE_PRODUCT = 'ACTIVATE_PRODUCT';
+const SET_PRODUCT_ACTIVATED = 'SET_PRODUCT_ACTIVATED';
 
 const setPurchasesIsFetching = isFetching => {
 	return { type: SET_PURCHASES_IS_FETCHING, isFetching };
@@ -19,8 +21,18 @@ const activateProduct = productId => {
 	return { type: ACTIVATE_PRODUCT, productId };
 };
 
+export const setProductActivated = productId => {
+	return { type: SET_PRODUCT_ACTIVATED, productId };
+};
+
+export const setProductActionError = error => {
+	return { type: SET_PRODUCT_ACTION_ERROR, error };
+};
+
 const productActions = {
 	activateProduct,
+	setProductActivated,
+	setProductActionError,
 };
 
 const actions = {
@@ -34,6 +46,8 @@ export {
 	SET_PURCHASES_IS_FETCHING,
 	FETCH_PURCHASES,
 	SET_PURCHASES,
+	SET_PRODUCT_ACTION_ERROR,
 	ACTIVATE_PRODUCT,
+	SET_PRODUCT_ACTIVATED,
 	actions as default,
 };
