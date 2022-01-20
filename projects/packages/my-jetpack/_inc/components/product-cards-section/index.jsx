34ccc9e5851eaa00068db3b0babe93b5791a76aa
oskosk <oskosk@gmail.com>
2022-01-20 11:13:37 -0300

    Add Products Section component to My Jetpack

---
+++ projects/packages/my-jetpack/_inc/components/product-cards-section/index.jsx
@@ -0,0 +1,18 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { Row } from '@automattic/jetpack-components';
+
+/**
+ * Product cards section component.
+ *
+ * @returns {object} ProductCardsSection React component.
+ */
+export default function ProductCardsSection() {
+	return (
+		<div>
+			<Row></Row>
+		</div>
+	);
+}
