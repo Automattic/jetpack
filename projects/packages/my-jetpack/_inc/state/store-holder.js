26568123e64fa232d10311654e4d6bc38e0c0b48
retrofox <rdsuarez@gmail.com>
2022-01-20 11:47:48 -0300

    [not verified] my-jetpack: apply middlewares to store

--- projects/packages/my-jetpack/_inc/state/store-holder.js
+++ projects/packages/my-jetpack/_inc/state/store-holder.js
@@ -1,15 +1,25 @@
 /**
  * External dependencies
  */
-import { createReduxStore, register } from '@wordpress/data';
+import { registerStore } from '@wordpress/data';
+import applyMiddlewares from './middlewares';
 
+/**
+ * Internal dependencies
+ */
+import effects from './effects';
 class storeHolder {
 	static store = null;
 
 	static mayBeInit( storeId, storeConfig ) {
 		if ( null === storeHolder.store ) {
-			storeHolder.store = createReduxStore( storeId, storeConfig );
-			register( storeHolder.store );
+			// Register `my-jetpack` store.
+			const store = registerStore( storeId, storeConfig );
+
+			// Apply side effects to the store.
+			const storeWithEffects = applyMiddlewares( store, effects );
+
+			storeHolder.store = storeWithEffects;
 		}
 	}
 }
