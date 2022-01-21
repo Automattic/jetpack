fad0a6d6c4edcdfbce4d9f15ba038cb5839aefd0
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2022-01-21 17:51:50 -0300

    My Jetpack: Fix BoostCard icon

--- projects/packages/my-jetpack/_inc/components/product-cards-section/boost-card.jsx
+++ projects/packages/my-jetpack/_inc/components/product-cards-section/boost-card.jsx
@@ -10,7 +10,7 @@ import PropTypes from 'prop-types';
 import ProductCard, { PRODUCT_STATUSES } from '../product-card';
 
 const BoostIcon = () => (
-	<svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
 		<path d="M7 1.5L12 7L7 12.5M1 1.5L6 7L1 12.5" stroke="#1E1E1E" stroke-width="1.5" />
 	</svg>
 );
