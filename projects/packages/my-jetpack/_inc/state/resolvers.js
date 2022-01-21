14b7cbb1ce4770c48d1111b6ea2fcd942bd8e877
retrofox <rdsuarez@gmail.com>
2022-01-21 16:54:46 -0300

    [not verified] my-jetpack: swap actions to fetch product

--- projects/packages/my-jetpack/_inc/state/resolvers.js
+++ projects/packages/my-jetpack/_inc/state/resolvers.js
@@ -13,8 +13,8 @@ const myJetpackResolvers = {
 		dispatch.setPurchasesIsFetching( true );
 
 		try {
-			dispatch.setPurchasesIsFetching( false );
 			dispatch.setPurchases( await apiFetch( { path: REST_API_SITE_PURCHASES_ENDPOINT } ) );
+			dispatch.setPurchasesIsFetching( false );
 		} catch ( error ) {
 			dispatch.setPurchasesIsFetching( false );
 			throw error;
