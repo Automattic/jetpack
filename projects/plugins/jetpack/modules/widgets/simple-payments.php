b6a4234f7982475fec6be5f31bda2e069eff37c4
sdixon194 <steve.dixon@automattic.com>
2022-01-21 14:23:39 -0500

    [not verified] yoda-ing

--- projects/plugins/jetpack/modules/widgets/simple-payments.php
+++ projects/plugins/jetpack/modules/widgets/simple-payments.php
@@ -530,11 +530,11 @@ if ( ! class_exists( 'Jetpack_Simple_Payments_Widget' ) ) {
 			);
 
 			if ( strcmp( $new_instance['form_action'], $old_instance['form_action'] ) !== 0 ) {
-				if ( $new_instance['form_action'] == 'edit' ) {
+				if ( 'edit' == $new_instance['form_action'] ) {
 					return array_merge( $this->get_product_from_post( (int) $old_instance['product_post_id'] ), $required_widget_props );
 				}
 
-				if ( $new_instance['form_action'] == 'clear' ) {
+				if ( 'clear' === $new_instance['form_action'] ) {
 					return array_merge( $this->defaults(), $required_widget_props );
 				}
 			}
