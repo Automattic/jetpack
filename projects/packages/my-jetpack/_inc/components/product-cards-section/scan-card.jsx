4aa114b069793d0e41a4abea7ea2f242ef9c6999
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2022-01-21 17:49:46 -0300

    My Jetpack: Fix ScanCard icon

--- projects/packages/my-jetpack/_inc/components/product-cards-section/scan-card.jsx
+++ projects/packages/my-jetpack/_inc/components/product-cards-section/scan-card.jsx
@@ -10,7 +10,7 @@ import PropTypes from 'prop-types';
 import ProductCard, { PRODUCT_STATUSES } from '../product-card';
 
 const ScanIcon = () => (
-	<svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<svg width="14" height="17" viewBox="0 0 14 17" fill="none" xmlns="http://www.w3.org/2000/svg">
 		<path
 			fillRule="evenodd"
 			clipRule="evenodd"
