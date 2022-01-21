c5acdd64ff3e64ab6d20ac749122fd8e7a3932f5
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2022-01-21 17:04:18 -0300

    My Jetpack: Add initial structure for ProductCardsSection

--- projects/packages/my-jetpack/_inc/components/product-cards-section/index.jsx
+++ projects/packages/my-jetpack/_inc/components/product-cards-section/index.jsx
@@ -2,7 +2,12 @@
  * External dependencies
  */
 import React from 'react';
-import { Row } from '@automattic/jetpack-components';
+import { Container, Row, Col } from '@automattic/jetpack-components';
+
+/**
+ * Internal dependencies
+ */
+import ProductCard, { PRODUCT_STATUSES } from '../product-card';
 
 /**
  * Product cards section component.
@@ -11,8 +16,65 @@ import { Row } from '@automattic/jetpack-components';
  */
 export default function ProductCardsSection() {
 	return (
-		<div>
-			<Row></Row>
-		</div>
+		<Container>
+			<Row>
+				<Col sm={ 3 }>
+					<ProductCard
+						name="Backup"
+						description="Save every change"
+						status={ PRODUCT_STATUSES.ERROR }
+					/>
+				</Col>
+				<Col sm={ 3 }>
+					<ProductCard
+						name="Scan"
+						description="Stay one step ahead of threats"
+						status={ PRODUCT_STATUSES.ERROR }
+					/>
+				</Col>
+				<Col sm={ 3 }>
+					<ProductCard
+						name="Anti-spam"
+						description="Stop comment and form spam"
+						status={ PRODUCT_STATUSES.ERROR }
+					/>
+				</Col>
+				<Col sm={ 3 }>
+					<ProductCard
+						name="Boost"
+						description="Instant speed and SEO"
+						status={ PRODUCT_STATUSES.ERROR }
+					/>
+				</Col>
+				<Col sm={ 3 }>
+					<ProductCard
+						name="Search"
+						description="Help them find what they need"
+						status={ PRODUCT_STATUSES.ERROR }
+					/>
+				</Col>
+				<Col sm={ 3 }>
+					<ProductCard
+						name="VideoPress"
+						description="High-quality, ad-free video"
+						status={ PRODUCT_STATUSES.ERROR }
+					/>
+				</Col>
+				<Col sm={ 3 }>
+					<ProductCard
+						name="CRM"
+						description="Connect with your people"
+						status={ PRODUCT_STATUSES.ERROR }
+					/>
+				</Col>
+				<Col sm={ 3 }>
+					<ProductCard
+						name="Extras"
+						description="Basic tools for a successful site"
+						status={ PRODUCT_STATUSES.ERROR }
+					/>
+				</Col>
+			</Row>
+		</Container>
 	);
 }
