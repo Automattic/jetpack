501e07901584d04ffb8b173a09c72c1a33dd0b2c
Brad Jorsch <brad.jorsch@automattic.com>
2022-01-24 15:57:19 -0500

    Avoid local eslint errors from e2e dirs
    We have the e2e test directories in separate pnpm workspaces because
    they require large downloads for playwright that are not normally needed
    in local development. But that can cause problems when trying to run
    eslint as the plugins referenced from eslintrc are not available.
    
    To avoid that, let's adjust the eslint configurations in the e2e dirs to
    detect this situation and avoid trying to lint at all.

--- projects/plugins/boost/tests/e2e/.eslintrc.cjs
+++ projects/plugins/boost/tests/e2e/.eslintrc.cjs
@@ -1,6 +1,32 @@
 const loadIgnorePatterns = require( '../../../../../tools/js-tools/load-eslint-ignore.js' );
 
-module.exports = {
-	extends: [ require.resolve( 'jetpack-e2e-commons/.eslintrc.cjs' ) ],
-	ignorePatterns: loadIgnorePatterns( __dirname ),
-};
+// For local development, detect when `pnpm install` has not been run in the E2E workspace and
+// return a "disabled" config to avoid errors.
+let disable = false;
+if ( ! process.env.CI ) {
+	try {
+		require.resolve( 'jetpack-e2e-commons/.eslintrc.cjs' );
+	} catch ( e ) {
+		disable = true;
+	}
+}
+if ( disable ) {
+	const s = `Disabling eslint in ${ __dirname } because \`pnpm install\` has not been run there.`;
+	console.warn( '-'.repeat( s.length ) );
+	console.warn( s );
+	console.warn( '-'.repeat( s.length ) );
+	module.exports = {
+		root: true,
+		parser: '@babel/eslint-parser',
+		parserOptions: {
+			requireConfigFile: false,
+		},
+		ignorePatterns: loadIgnorePatterns( __dirname ),
+		rules: {},
+	};
+} else {
+	module.exports = {
+		extends: [ require.resolve( 'jetpack-e2e-commons/.eslintrc.cjs' ) ],
+		ignorePatterns: loadIgnorePatterns( __dirname ),
+	};
+}
