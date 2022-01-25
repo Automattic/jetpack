9e1d00288ca69b242b790eea7b2d15e15b1831df
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2022-01-25 19:46:00 -0300

    RNA: Remove the usage of base-styles and refactor cols handling

--- projects/js-packages/components/components/layout/col/index.jsx
+++ projects/js-packages/components/components/layout/col/index.jsx
@@ -17,29 +17,32 @@ import styles from './style.module.scss';
  * @returns {React.Component} Col component.
  */
 const Col = props => {
-	const { children, sm, md, lg } = props;
-	const small = Number.isInteger( sm ) ? sm : 0;
-	const medium = Number.isInteger( md ) ? md : 0;
-	const large = Number.isInteger( lg ) ? lg : 0;
-	const minimum = [ small, medium, large ].reduce( ( prev, curr ) =>
-		curr > 0 && curr < prev ? curr : prev
-	);
+	const { children, className, xs, sm, md, lg, xl } = props;
 
-	const className = classnames(
-		small > 0 ? styles[ 'sm-col-span-' + small ] : styles[ 'sm-col-span-' + minimum ],
-		medium > 0 ? styles[ 'md-col-span-' + medium ] : styles[ 'md-col-span-' + minimum ],
-		large > 0 ? styles[ 'lg-col-span-' + large ] : styles[ 'lg-col-span-' + minimum ]
-	);
-	return <div className={ className }>{ children }</div>;
+	const colClassName = classnames( className, {
+		[ styles[ `col-xs-${ xs }` ] ]: Number.isInteger( xs ),
+		[ styles[ `col-sm-${ sm }` ] ]: Number.isInteger( sm ),
+		[ styles[ `col-md-${ md }` ] ]: Number.isInteger( md ),
+		[ styles[ `col-lg-${ lg }` ] ]: Number.isInteger( lg ),
+		[ styles[ `col-xl-${ xl }` ] ]: Number.isInteger( xl ),
+	} );
+
+	return <div className={ colClassName }>{ children }</div>;
 };
 
 Col.proptypes = {
-	/** Colspan for small viewport. Needs to be an integer. Defaults to the smallest colspan informed. */
+	/** Custom className to be inserted. */
+	className: PropTypes.string,
+	/** Colspan for extra small viewport. Needs to be an integer. */
+	xs: PropTypes.number.isRequired,
+	/** Colspan for small viewport. Needs to be an integer. */
 	sm: PropTypes.number,
-	/** Colspan for medium viewport. Needs to be an integer. Defaults to the smallest colspan informed. */
+	/** Colspan for medium viewport. Needs to be an integer. */
 	md: PropTypes.number,
-	/** Colspan for large viewport. Needs to be an integer. Defaults to the smallest colspan informed. */
+	/** Colspan for large viewport. Needs to be an integer. */
 	lg: PropTypes.number,
+	/** Colspan for extra large viewport. Needs to be an integer. */
+	xl: PropTypes.number,
 };
 
 export default Col;
