5c8777731a8acfd3be03c0dece19e0a8caab0feb
retrofox <rdsuarez@gmail.com>
2022-01-25 18:40:45 -0300

    my-jetpack: expose admin url in boost product info

--- projects/packages/my-jetpack/src/products/class-boost.php
+++ projects/packages/my-jetpack/src/products/class-boost.php
@@ -33,7 +33,7 @@ class Boost {
 			'name'        => __( 'Boost', 'jetpack-my-jetpack' ),
 			'status'      => $status,
 			'class'       => __CLASS__,
-			'admin-url'   => admin_url( '?page=jetpack-boost' ),
+			'admin-url'   => admin_url( 'admin.php?page=jetpack-boost' ),
 		);
 	}
 
