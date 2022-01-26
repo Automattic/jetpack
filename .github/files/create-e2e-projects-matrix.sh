f9fa9cd8d9e0eb28cd872ed2dfc1faa8eb726018
Adrian Moldovan <adim.moldovan@gmail.com>
2022-01-26 12:04:49 +0200

    Use quotes

--- .github/files/create-e2e-projects-matrix.sh
+++ .github/files/create-e2e-projects-matrix.sh
@@ -7,8 +7,6 @@ CHANGED_PROJECTS="$(.github/files/list-changed-projects.php --debug)"
 for PROJECT in "${PROJECTS[@]}"; do
 	PROJECT_NAME=$(jq -r ".project" <<<"$PROJECT")
 	PROJECT_PATH=$(jq -r ".path" <<<"$PROJECT")
-	echo "$PROJECT_NAME"
-
 	TARGET_PROJECTS=$(jq -r -e ".ci.projects[]" "$PROJECT_PATH/package.json")
 
 	if [ "$TARGET_PROJECTS" == "" ]; then
@@ -18,15 +16,14 @@ for PROJECT in "${PROJECTS[@]}"; do
 		# iterate over defined target plugins/projects and see if they are changed
 		for TESTED_PROJECT in $TARGET_PROJECTS; do
 			RESULT=$(jq --arg prj "$TESTED_PROJECT" '.[$prj]' <<<"$CHANGED_PROJECTS")
-			printf "\t%s: %s\n" "$TESTED_PROJECT" "$RESULT"
+			printf "%s: %s" "$TESTED_PROJECT" "$RESULT"
 			if [[ "$RESULT" == true ]]; then
 				PROJECTS_MATRIX+=("$PROJECT")
+				printf " ==> %s tests should run\n" "$PROJECT_NAME"
 				break
 			fi
 		done
 	fi
-
-	echo
 done
 
 echo "$(jq -s -c -r '.' <<<"${PROJECTS_MATRIX[@]}")"
