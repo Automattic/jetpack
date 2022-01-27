b5a3d6cc87d03b3f60e483a4770c61552bb67f5f
Dean Sas <dean.sas@automattic.com>
2022-01-27 12:20:33 +0000

    [not verified] Payment blocks Remove hard-coded icon dimensions
    The recurring-payments block had an svg icon with hard-coded width and
    heights, which meant it didn't scale the same as other icons. Removing
    this means it looks the same in existing contexts but also scales to
    match other blocks in the unified payments block picker.

--- projects/plugins/jetpack/extensions/blocks/recurring-payments/index.js
+++ projects/plugins/jetpack/extensions/blocks/recurring-payments/index.js
@@ -22,7 +22,7 @@ import './editor.scss';
 export const name = 'recurring-payments';
 
 export const icon = (
-	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 		<Rect x="0" fill="none" width="24" height="24" />
 		<G>
 			<Path d="M20 4H4c-1.105 0-2 .895-2 2v12c0 1.105.895 2 2 2h16c1.105 0 2-.895 2-2V6c0-1.105-.895-2-2-2zm0 2v2H4V6h16zM4 18v-6h16v6H4zm2-4h7v2H6v-2zm9 0h3v2h-3v-2z" />
