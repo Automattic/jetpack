20f52c926ea361915025520152fcbd7a4b02e725
retrofox <rdsuarez@gmail.com>
2022-01-27 12:05:52 -0300

    [not verified] my-jetpack: when the site is not connected, redirect to the Jetpack dashboard.

--- projects/packages/my-jetpack/_inc/hooks/use-my-jetpack-connection/index.js
+++ projects/packages/my-jetpack/_inc/hooks/use-my-jetpack-connection/index.js
@@ -1,8 +1,10 @@
 /* global myJetpackInitialState */
+
 /**
  * WordPress dependencies
  */
 import { useConnection } from '@automattic/jetpack-connection';
+import { useEffect } from 'react';
 
 /**
  * React custom hook to get the site purchases data.
@@ -11,5 +13,24 @@ import { useConnection } from '@automattic/jetpack-connection';
  */
 export default function useMyJetpackConnection() {
 	const { apiRoot, apiNonce } = myJetpackInitialState;
-	return useConnection( { apiRoot, apiNonce } );
+	const connectionData = useConnection( { apiRoot, apiNonce } );
+
+	// Alias: https://github.com/Automattic/jetpack/blob/master/projects/packages/connection/src/class-rest-connector.php/#L315
+	const isSiteConnected = connectionData.isRegistered;
+
+	/*
+	 * When the site is not connect,
+	 * redirect to the Jetpack dashboard.
+	 */
+	useEffect( () => {
+		if ( isSiteConnected ) {
+			return;
+		}
+		window.location = myJetpackInitialState.topJetpackMenuItemUrl;
+	}, [ isSiteConnected ] );
+
+	return {
+		...connectionData,
+		isSiteConnected,
+	};
 }
