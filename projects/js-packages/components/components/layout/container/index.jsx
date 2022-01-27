98b2b011c98369ec9f5ea4edd31824c04f36c8d5
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2022-01-27 13:25:21 -0300

    RNA: Update Container to support horizontal spacing and gap values

--- projects/js-packages/components/components/layout/container/index.jsx
+++ projects/js-packages/components/components/layout/container/index.jsx
@@ -18,20 +18,38 @@ import styles from './style.module.scss';
  */
 const Container = props => {
 	const { children, fluid, className } = props;
+
+	const horizontalSpacing = `calc( var(--horizontal-spacing) * ${ props.horizontalSpacing } )`;
+	const horizontalGap = `calc( var(--horizontal-spacing) * ${ props.horizontalGap } )`;
+
+	const containerStyle = {
+		paddingTop: horizontalSpacing,
+		paddingBottom: horizontalSpacing,
+		rowGap: horizontalGap,
+	};
+
 	const containerClassName = classNames( className, styles.container, {
 		[ styles.fluid ]: fluid,
 	} );
 
-	return <div className={ containerClassName }>{ children }</div>;
+	return (
+		<div className={ containerClassName } style={ containerStyle }>
+			{ children }
+		</div>
+	);
 };
 
 Container.propTypes = {
-	className: PropTypes.string,
 	fluid: PropTypes.bool,
+	className: PropTypes.string,
+	horizontalSpacing: PropTypes.number,
+	horizontalGap: PropTypes.number,
 };
 
 Container.defaultProps = {
 	fluid: false,
+	horizontalGap: 1,
+	horizontalSpacing: 1,
 };
 
 export default Container;
