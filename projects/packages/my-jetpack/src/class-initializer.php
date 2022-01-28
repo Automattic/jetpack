539ff68d381476cd66ba04f7d29e57cf2e99d80c
oskosk <oskosk@gmail.com>
2022-01-28 13:23:48 -0300

    [not verified] Update the way we check if My jetpack should be loaded

--- projects/packages/my-jetpack/src/class-initializer.php
+++ projects/packages/my-jetpack/src/class-initializer.php
@@ -28,17 +28,7 @@ class Initializer {
 	 * @return void
 	 */
 	public static function init() {
-		if ( did_action( 'my_jetpack_init' ) ) {
-			return;
-		}
-
-		// Feature flag while we are developing it.
-		if ( ! defined( 'JETPACK_ENABLE_MY_JETPACK' ) || ! JETPACK_ENABLE_MY_JETPACK ) {
-			return;
-		}
-
-		// Do not initialize My Jetpack if site is not connected.
-		if ( ! ( new Connection_Manager() )->is_connected() ) {
+		if ( ! self::should_initialize() ) {
 			return;
 		}
 
@@ -176,6 +166,28 @@ class Initializer {
 	 */
 	public static function permissions_callback() {
 		return current_user_can( 'manage_options' );
+	}
+
+	/**
+	 * Return true if we should initialize the My Jetpack
+	 */
+	public static function should_initialize() {
+		if ( did_action( 'my_jetpack_init' ) ) {
+			return false;
+		}
+		$should = apply_filters( 'jetpack_my_jetpck_should_initialize', true );
+
+		// Feature flag while we are developing it.
+		if ( ! defined( 'JETPACK_ENABLE_MY_JETPACK' ) || ! JETPACK_ENABLE_MY_JETPACK ) {
+			return false;
+		}
+
+		// Do not initialize My Jetpack if site is not connected.
+		if ( ! ( new Connection_Manager() )->is_connected() ) {
+			return false;
+		}
+
+		return $should;
 	}
 
 	/**
