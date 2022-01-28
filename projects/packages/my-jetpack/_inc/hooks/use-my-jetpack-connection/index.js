991696144adbb4dfb038cd9e25dc9c6469aa62eb
retrofox <rdsuarez@gmail.com>
2022-01-28 11:27:40 -0300

    [not verified] my-jetpack: add options to useMyJetpackConnection() hook

--- projects/packages/my-jetpack/_inc/hooks/use-my-jetpack-connection/index.js
+++ projects/packages/my-jetpack/_inc/hooks/use-my-jetpack-connection/index.js
@@ -10,10 +10,13 @@ import { useEffect } from 'react';
 /**
  * React custom hook to get the site purchases data.
  *
+ * @param   {object} options           - Options to pass to the hook.
+ * @param   {boolean} options.reditect - Perform a redirect when no connection is found.
  * @returns {object} site purchases data
  */
-export default function useMyJetpackConnection() {
+export default function useMyJetpackConnection( options = { redirect: false } ) {
 	const { apiRoot, apiNonce } = myJetpackRest;
+	const { redirect } = options;
 	const connectionData = useConnection( { apiRoot, apiNonce } );
 
 	// Alias: https://github.com/Automattic/jetpack/blob/master/projects/packages/connection/src/class-rest-connector.php/#L315
@@ -21,14 +24,22 @@ export default function useMyJetpackConnection() {
 
 	/*
 	 * When the site is not connect,
+	 * and the `redirect` option is set to `true`,
 	 * redirect to the Jetpack dashboard.
 	 */
 	useEffect( () => {
+		// Bail early when redirect mode is disabled.
+		if ( ! redirect ) {
+			return;
+		}
+
+		// When site is connected, bail early.
 		if ( isSiteConnected ) {
 			return;
 		}
+
 		window.location = myJetpackInitialState.topJetpackMenuItemUrl;
-	}, [ isSiteConnected ] );
+	}, [ isSiteConnected, redirect ] );
 
 	return {
 		...connectionData,
