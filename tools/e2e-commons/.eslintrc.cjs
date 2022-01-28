3300998c322bf38753a225b46d35368499468c44
Brad Jorsch <brad.jorsch@automattic.com>
2022-01-28 15:16:19 -0500

    We can use it there too

--- tools/e2e-commons/.eslintrc.cjs
+++ tools/e2e-commons/.eslintrc.cjs
@@ -1,46 +1,21 @@
 const loadIgnorePatterns = require( '../../tools/js-tools/load-eslint-ignore.js' );
+const eslintCheckPnpmInstall = require( '../../tools/js-tools/eslint-check-pnpm-install.js' );
 
-// For local development, detect when `pnpm install` has not been run in the E2E workspace and
-// return a "disabled" config to avoid errors.
-let disable = false;
-if ( ! process.env.CI ) {
-	try {
-		require.resolve( 'eslint-plugin-playwright' );
-	} catch ( e ) {
-		disable = true;
-	}
-}
-if ( disable ) {
-	const s = `Disabling eslint in ${ __dirname } because \`pnpm install\` has not been run there.`;
-	console.warn( '-'.repeat( s.length ) );
-	console.warn( s );
-	console.warn( '-'.repeat( s.length ) );
-	module.exports = {
-		root: true,
-		parser: '@babel/eslint-parser',
-		parserOptions: {
-			requireConfigFile: false,
-		},
-		ignorePatterns: loadIgnorePatterns( __dirname ),
-		rules: {},
-	};
-} else {
-	module.exports = {
-		root: true,
-		extends: [ 'plugin:@wordpress/eslint-plugin/recommended', 'plugin:playwright/playwright-test' ],
-		ignorePatterns: loadIgnorePatterns( __dirname ),
-		parserOptions: {
-			requireConfigFile: false,
-		},
-		globals: {
-			wp: true,
-			jpConnect: true,
-		},
-		rules: {
-			'arrow-parens': [ 0, 'as-needed' ],
-			'no-console': 0,
-			'jest/no-done-callback': 0,
-			'jest/no-disabled-tests': 0,
-		},
-	};
-}
+module.exports = eslintCheckPnpmInstall( __dirname, require, 'eslint-plugin-playwright' ) || {
+	root: true,
+	extends: [ 'plugin:@wordpress/eslint-plugin/recommended', 'plugin:playwright/playwright-test' ],
+	ignorePatterns: loadIgnorePatterns( __dirname ),
+	parserOptions: {
+		requireConfigFile: false,
+	},
+	globals: {
+		wp: true,
+		jpConnect: true,
+	},
+	rules: {
+		'arrow-parens': [ 0, 'as-needed' ],
+		'no-console': 0,
+		'jest/no-done-callback': 0,
+		'jest/no-disabled-tests': 0,
+	},
+};
