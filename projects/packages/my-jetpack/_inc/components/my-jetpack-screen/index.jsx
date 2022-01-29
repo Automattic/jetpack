1bcdaca9b495570d955c51e1832550df26e52184
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2022-01-29 12:16:42 -0300

    RNA: New layout structure for Container and Col (#22496)
    Co-authored-by: oskosk <oskosk@gmail.com>

--- projects/packages/my-jetpack/_inc/components/my-jetpack-screen/index.jsx
+++ projects/packages/my-jetpack/_inc/components/my-jetpack-screen/index.jsx
@@ -9,7 +9,7 @@ import {
 	AdminSection,
 	AdminSectionHero,
 	AdminPage,
-	Row,
+	Container,
 	Col,
 } from '@automattic/jetpack-components';
 
@@ -22,17 +22,9 @@ import ProductCardsSection from '../product-cards-section';
 import useAnalytics from '../../hooks/use-analytics';
 import useNoticeWatcher, { useGlobalNotice } from '../../hooks/use-notice';
 import useMyJetpackConnection from '../../hooks/use-my-jetpack-connection';
-import './style.scss';
+import styles from './styles.module.scss';
 
-/**
- * Component that renders the My Jetpack global notices.
- *
- * @returns {object} The GlobalNotice component.
- */
-function GlobalNotice() {
-	// Watch global events.
-	useNoticeWatcher();
-
+const GlobalNotice = ( { message, options, clean } ) => {
 	/*
 	 * Map Notice statuses with Icons.
 	 * `success`, `info`, `warning`, `error`
@@ -42,18 +34,13 @@ function GlobalNotice() {
 		info,
 	};
 
-	const { message, options, clean } = useGlobalNotice();
-	if ( ! message ) {
-		return null;
-	}
-
 	return (
 		<Notice { ...options } onRemove={ clean }>
 			{ iconMap?.[ options.status ] && <Icon icon={ iconMap[ options.status ] } /> }
 			<div className="components-notice__message-content">{ message }</div>
 		</Notice>
 	);
-}
+};
 
 /**
  * The My Jetpack App Main Screen.
@@ -61,52 +48,57 @@ function GlobalNotice() {
  * @returns {object} The MyJetpackScreen component.
  */
 export default function MyJetpackScreen() {
+	useNoticeWatcher();
+	const { message, options, clean } = useGlobalNotice();
+
 	const {
 		tracks: { recordEvent },
 	} = useAnalytics();
+
 	useEffect( () => {
 		recordEvent( 'jetpack_myjetpack_page_view' );
 	}, [ recordEvent ] );
 
 	// No render when site is not connected.
 	const { isSiteConnected } = useMyJetpackConnection( { redirect: true } );
+
 	if ( ! isSiteConnected ) {
 		return null;
 	}
 
 	return (
-		<div className="jp-my-jetpack-screen">
-			<AdminPage>
-				<AdminSectionHero>
-					<Row>
-						<Col lg={ 12 } md={ 8 } sm={ 4 }>
-							<h1>
-								{ __(
-									'Manage your Jetpack plan and products all in one place',
-									'jetpack-my-jetpack'
-								) }
-							</h1>
-							<GlobalNotice />
+		<AdminPage>
+			<AdminSectionHero>
+				<Container horizontalSpacing={ 5 } horizontalGap={ message ? 3 : 6 }>
+					<Col sm={ 4 } md={ 7 } lg={ 6 }>
+						<h1 className={ styles.heading }>
+							{ __(
+								'Manage your Jetpack plan and products all in one place',
+								'jetpack-my-jetpack'
+							) }
+						</h1>
+					</Col>
+					{ message && (
+						<Col>
+							<GlobalNotice message={ message } options={ options } clean={ clean } />
 						</Col>
-					</Row>
-					<Row>
-						<Col lg={ 12 } md={ 8 } sm={ 4 }>
-							<ProductCardsSection />
-						</Col>
-					</Row>
-				</AdminSectionHero>
+					) }
+					<Col>
+						<ProductCardsSection />
+					</Col>
+				</Container>
+			</AdminSectionHero>
 
-				<AdminSection>
-					<Row>
-						<Col lg={ 6 } sm={ 4 }>
-							<PlansSection />
-						</Col>
-						<Col lg={ 6 } sm={ 4 }>
-							<ConnectionsSection />
-						</Col>
-					</Row>
-				</AdminSection>
-			</AdminPage>
-		</div>
+			<AdminSection>
+				<Container horizontalSpacing={ 8 }>
+					<Col sm={ 2 } md={ 4 } lg={ 6 }>
+						<PlansSection />
+					</Col>
+					<Col sm={ 2 } md={ 4 } lg={ 6 }>
+						<ConnectionsSection />
+					</Col>
+				</Container>
+			</AdminSection>
+		</AdminPage>
 	);
 }
