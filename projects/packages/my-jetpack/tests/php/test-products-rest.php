760829fec10cd92f093b7a62392ab63ceaa489fc
leogermani <leogermani@gmail.com>
2022-01-31 18:10:36 -0300

    skip tests in php 5.6

--- projects/packages/my-jetpack/tests/php/test-products-rest.php
+++ projects/packages/my-jetpack/tests/php/test-products-rest.php
@@ -60,6 +60,10 @@ class Test_Products_Rest extends TestCase {
 	 */
 	public function set_up() {
 
+		if ( version_compare( phpversion(), '5.7', '<=' ) ) {
+			$this->markTestSkipped( 'avoid bug in PHP 5.6 that throws strict mode warnings for abstract static methods.' );
+		}
+
 		$this->install_mock_plugin();
 		wp_cache_delete( 'plugins', 'plugins' );
 
