0bf19248fb8e62a5c8d93ccf29e4a5cdebc270e9
Dean Sas <dean.sas@automattic.com>
2022-01-31 14:46:22 +0000

    [not verified] Generate the variations definitions

--- projects/plugins/jetpack/extensions/blocks/one-payment/variations.js
+++ projects/plugins/jetpack/extensions/blocks/one-payment/variations.js
@@ -14,32 +14,28 @@ import { getBlockType } from '@wordpress/blocks';
 import { settings as DonationSettings } from '../donations';
 import { settings as RecurringPaymentSettings } from '../recurring-payments';
 
-const variations = [];
+const variations = [
+	[ 'donations', DonationSettings ],
+	[ 'recurring-payments', RecurringPaymentSettings ],
+];
 
-if ( getBlockType( 'jetpack/donations' ) ) {
-	variations.push( {
-		name: 'onepayment-donations',
-		title: DonationSettings.title,
-		description: DonationSettings.description,
-		icon: DonationSettings.icon.src,
-		innerBlocks: [ [ 'jetpack/donations', {} ] ],
-		// The inner block itself is already listed in the inserter in its own right, so just include in this blocks
-		// unified intro.
-		scope: [ 'block' ],
-	} );
-}
+const variationDefinitions = variations
+	.map( blockNameSettings => {
+		const blockDefinition = {};
+		const [ blockName, settings ] = blockNameSettings;
+		if ( getBlockType( 'jetpack/' + blockName ) ) {
+			blockDefinition.name = 'jetpack/onepayment-' + blockName;
+			blockDefinition.title = settings.title;
+			blockDefinition.description = settings.desccription;
+			blockDefinition.icon = settings.icon.src;
+			blockDefinition.innerBlocks = [ [ 'jetpack/' + blockName, {} ] ];
+			// The inner block itself is already listed in the inserter in its own right, so just include in this blocks
+			// unified intro.
+			blockDefinition.scope = [ 'block' ];
+		}
 
-if ( getBlockType( 'jetpack/recurring-payments' ) ) {
-	variations.push( {
-		name: 'onepayment-recurring-payments',
-		title: RecurringPaymentSettings.title,
-		description: RecurringPaymentSettings.description,
-		icon: RecurringPaymentSettings.icon.src,
-		innerBlocks: [ [ 'jetpack/recurring-payments', {} ] ],
-		// The inner block itself is already listed in the inserter in its own right, so just include in this blocks
-		// unified intro.
-		scope: [ 'block' ],
-	} );
-}
+		return blockDefinition;
+	} )
+	.filter( blockDefinition => Object.entries( blockDefinition ).length > 0 );
 
-export default variations;
+export default variationDefinitions;
