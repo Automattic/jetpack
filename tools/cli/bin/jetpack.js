Automattic/jetpack

commit 470b89a44c0c3b7c41492f425d267893b7d8ee6f

cli: Use native ESM (#22395)
Node has supported ESM natively since version 14. Let's take advantage
of that in the CLI, instead of the `esm` thing that has caused us other
problems in the past (and makes local testing a bit of a pain).

The main changes needed are:

* Have to use `import.meta.url` instead of `__dirname`.
* `import` statements have to specify the extension.
* No `require()`.

tools/cli/bin/jetpack.js
@@ -1,0 +1,33 @@
+#!/usr/bin/env node
+
+/**
+ * External dependencies
+ */
+import process from 'process';
+import { fileURLToPath } from 'url';
+
+/**
+ * Internal dependencies
+ */
+import * as cliRouter from '../cliRouter.js';
+
+/**
+ * Standardizes the cwd for the process. Allows `jetpack` cli to run correctly from any location in the repo.
+ */
+
+process.chdir( fileURLToPath( new URL( '../../..', import.meta.url ) ) );
+
+try {
+	cliRouter.cli();
+} catch ( error ) {
+	if ( error.code === 'MODULE_NOT_FOUND' ) {
+		// if pnpm install hasn't been run, esm require will fail here.
+		console.error(
+			'Something is missing from your install. Please run `pnpm install` and try again.'
+		);
+	} else {
+		console.error( error );
+		console.error( 'Something unexpected happened. See error above.' );
+	}
+	process.exit( 1 );
+}

pnpm-lock.yaml
@@ -1198,8 +1198,8 @@ importers:
       chalk: 4.1.2
       configstore: 5.0.1
       envfile: 6.17.0
-      esm: 3.2.25
       execa: 5.0.0
+      glob: 7.1.6
       inquirer: 7.3.3
       js-yaml: 4.1.0
       listr: 0.14.3
@@ -1210,6 +1210,7 @@ importers:
       pluralize: 8.0.0
       process: 0.11.10
       simple-git: 2.37.0
+      sinon: 9.2.4
       yargs: 16.2.0
       yargs-parser: 20.2.4
     dependencies:
@@ -1218,8 +1219,8 @@ importers:
       chalk: 4.1.2
       configstore: 5.0.1
       envfile: 6.17.0
-      esm: 3.2.25
       execa: 5.0.0
+      glob: 7.1.6
       inquirer: 7.3.3
       js-yaml: 4.1.0
       listr: 0.14.3
@@ -1233,6 +1234,7 @@ importers:
     devDependencies:
       chai: 4.3.4
       mocha: 8.4.0
+      sinon: 9.2.4
       yargs-parser: 20.2.4
 
   tools/js-test-runner:
@@ -12677,11 +12679,6 @@ packages:
     transitivePeerDependencies:
       - supports-color
     dev: true
-
-  /esm/3.2.25:
-    resolution: {integrity: sha512-U1suiZ2oDVWv4zPO56S0NcR5QriEahGtdN2OR6FiOG4WJvcjBVFB0qI4+eKoWFH483PKGuLuu6V8Z4T5g63UVA==}
-    engines: {node: '>=6'}
-    dev: false
 
   /espree/7.3.1:
     resolution: {integrity: sha512-v3JCNCE64umkFpmkFGqzVKsOT0tN1Zr+ueqLZfpV1Ob8e+CEgPWa+OxCoGH3tnhimMKIaBm4m/vaRpJ/krRz2g==}

tools/cli/bin/jetpack
@@ -1,23 +1,0 @@
-#!/usr/bin/env node
-
-/**
- * Standardizes the cwd for the process. Allows `jetpack` cli to run correctly from any location in the repo.
- */
-const process = require( 'process' );
-const path = require( 'path' );
-
-process.chdir( path.join( __dirname, '../../..' ) );
-
-try {
-	// eslint-disable-next-line no-global-assign
-	require = require( 'esm' )( module /*, options*/ );
-	require( '../cliRouter' ).cli();
-} catch ( error ) {
-	if ( error.code === "MODULE_NOT_FOUND" ) { // if pnpm install hasn't been run, esm require will fail here.
-		console.error( 'Something is missing from your install. Please run `pnpm install` and try again.' );
-	} else {
-		console.error( error );
-		console.error( 'Something unexpected happened. See error above.' );
-	}
-	process.exit( 1 );
-}

tools/cli/cliRouter.js
@@ -7,17 +7,17 @@ import { hideBin } from 'yargs/helpers';
 /**
  * Internal dependencies
  */
-import { buildDefine } from './commands/build';
-import { watchDefine } from './commands/watch';
-import { installDefine } from './commands/install';
-import { cleanDefine } from './commands/clean';
-import { cliDefine } from './commands/cli';
-import { generateDefine } from './commands/generate';
-import { draftDefine } from './commands/draft';
-import { changelogDefine } from './commands/changelog';
-import { dockerDefine } from './commands/docker';
-import { testDefine } from './commands/test';
-import { releaseDefine } from './commands/release';
+import { buildDefine } from './commands/build.js';
+import { watchDefine } from './commands/watch.js';
+import { installDefine } from './commands/install.js';
+import { cleanDefine } from './commands/clean.js';
+import { cliDefine } from './commands/cli.js';
+import { generateDefine } from './commands/generate.js';
+import { draftDefine } from './commands/draft.js';
+import { changelogDefine } from './commands/changelog.js';
+import { dockerDefine } from './commands/docker.js';
+import { testDefine } from './commands/test.js';
+import { releaseDefine } from './commands/release.js';
 
 /**
  * The main CLI router function.
@@ -28,6 +28,8 @@ import { releaseDefine } from './commands/release';
 export async function cli() {
 	// Sets up the yargs instance.
 	let argv = yargs( hideBin( process.argv ) );
+
+	argv.scriptName( 'jetpack' );
 
 	/*
 	 * Adds the commands to the yargs instance. Help text will list commands in the order they are included here.

tools/cli/commands/build.js
@@ -11,13 +11,13 @@ import UpdateRenderer from 'listr-update-renderer';
  */
 import { chalkJetpackGreen } from '../helpers/styling.js';
 import promptForProject from '../helpers/promptForProject.js';
-import { readComposerJson } from '../helpers/json';
-import installProjectTask from '../helpers/tasks/installProjectTask';
-import { allProjectsByType } from '../helpers/projectHelpers';
-import { normalizeBuildArgv, normalizeProject } from '../helpers/normalizeArgv';
-import buildProjectTask from '../helpers/tasks/buildProjectTask';
-import projectBuildCommand from '../helpers/projectBuildCommand';
-import listrOpts from '../helpers/tasks/listrOpts';
+import { readComposerJson } from '../helpers/json.js';
+import installProjectTask from '../helpers/tasks/installProjectTask.js';
+import { allProjectsByType } from '../helpers/projectHelpers.js';
+import { normalizeBuildArgv, normalizeProject } from '../helpers/normalizeArgv.js';
+import buildProjectTask from '../helpers/tasks/buildProjectTask.js';
+import projectBuildCommand from '../helpers/projectBuildCommand.js';
+import listrOpts from '../helpers/tasks/listrOpts.js';
 
 /**
  * Relays build commands to a particular project.

tools/cli/commands/changelog.js
@@ -12,11 +12,11 @@ import simpleGit from 'simple-git';
 /**
  * Internal dependencies
  */
-import promptForProject from '../helpers/promptForProject';
-import { chalkJetpackGreen } from '../helpers/styling';
-import { normalizeProject } from '../helpers/normalizeArgv';
-import { allProjects, projectTypes } from '../helpers/projectHelpers';
-import { readComposerJson } from '../helpers/json';
+import promptForProject from '../helpers/promptForProject.js';
+import { chalkJetpackGreen } from '../helpers/styling.js';
+import { normalizeProject } from '../helpers/normalizeArgv.js';
+import { allProjects, projectTypes } from '../helpers/projectHelpers.js';
+import { readComposerJson } from '../helpers/json.js';
 
 /**
  * Comand definition for changelog subcommand.
@@ -468,7 +468,10 @@ async function changedProjects() {
 function doesFilenameExist( fileName, needChangelog ) {
 	let fileExists = false;
 	for ( const proj of needChangelog ) {
-		const projPath = path.join( __dirname, '../../..', 'projects', proj, 'changelog', fileName );
+		const projPath = new URL(
+			`../../../projects/${ proj }/changelog/${ fileName }`,
+			import.meta.url
+		);
 		try {
 			if ( fs.existsSync( projPath ) ) {
 				console.log(

tools/cli/commands/clean.js
@@ -8,9 +8,9 @@ import child_process from 'child_process';
 /**
  * Internal dependencies
  */
-import promptForProject, { promptForType } from '../helpers/promptForProject';
-import { normalizeCleanArgv } from '../helpers/normalizeArgv';
-import { allProjects } from '../helpers/projectHelpers';
+import promptForProject, { promptForType } from '../helpers/promptForProject.js';
+import { normalizeCleanArgv } from '../helpers/normalizeArgv.js';
+import { allProjects } from '../helpers/projectHelpers.js';
 import fs from 'fs';
 
 /**

commit d21f2a4dec8fcc1199cef2184ecba7e233bc8186

cli: Fix friendly error for missing modules
The CLI used to print a friendly error message when modules were
missing, but that was broken in #22395. This restores it.

Also, this adds output of the original error message for cases where the
problem isn't fixed by `pnpm install`. And since I'm doing it anyway, it
also tries to bold the final line on other errors.

tools/cli/bin/jetpack.js
@@ -7,27 +7,36 @@ import process from 'process';
 import { fileURLToPath } from 'url';
 
 /**
- * Internal dependencies
- */
-import * as cliRouter from '../cliRouter.js';
-
-/**
  * Standardizes the cwd for the process. Allows `jetpack` cli to run correctly from any location in the repo.
  */
 
 process.chdir( fileURLToPath( new URL( '../../..', import.meta.url ) ) );
 
 try {
+	const cliRouter = await import( '../cliRouter.js' );
 	cliRouter.cli();
 } catch ( error ) {
-	if ( error.code === 'MODULE_NOT_FOUND' ) {
-		// if pnpm install hasn't been run, esm require will fail here.
+	const identity = v => v;
+	const chalk = await import( 'chalk' ).then(
+		m => m.default,
+		() => ( {} )
+	);
+
+	if ( error.code === 'ERR_MODULE_NOT_FOUND' ) {
+		// if pnpm install hasn't been run, the import() will fail here.
 		console.error(
 			'Something is missing from your install. Please run `pnpm install` and try again.'
 		);
+
+		// Print the original error's message too, as sometimes the error isn't fixed by `pnpm install`.
+		console.error(
+			( chalk.grey || identity )( `The original error message was ${ error.message }` )
+		);
 	} else {
 		console.error( error );
-		console.error( 'Something unexpected happened. See error above.' );
+		console.error(
+			( chalk.bold || identity )( 'Something unexpected happened. See error above.' )
+		);
 	}
 	process.exit( 1 );
 }