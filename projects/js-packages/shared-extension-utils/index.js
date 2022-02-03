5bf7be87840946d7ff8d379ede5b7f82162168ea
Manish Mishra <manishmishra@Manishs-MacBook-Pro.local>
2022-02-03 18:47:40 +0530

    [not verified] removed getJetpackAvailabilityExtension file from the PR

--- projects/js-packages/shared-extension-utils/index.js
+++ projects/js-packages/shared-extension-utils/index.js
@@ -1,2 +1,1 @@
 export { default as getJetpackData, JETPACK_DATA_PATH } from './src/get-jetpack-data';
-export { default as getJetpackExtensionAvailability } from './src/get-jetpack-extension-availability';
