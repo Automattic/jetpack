506f25b571f31f899b7ad480fd1e6c5ec90bdf15
Manish Mishra <manishmishra@Manishs-MacBook-Pro.local>
2022-02-03 17:17:14 +0530

    [not verified] added returns type to getJetpackData method

--- projects/js-packages/shared-extension-utils/src/get-jetpack-data.js
+++ projects/js-packages/shared-extension-utils/src/get-jetpack-data.js
@@ -5,6 +5,11 @@ import { get } from 'lodash';
 
 export const JETPACK_DATA_PATH = 'Jetpack_Editor_Initial_State';
 
+/**
+ * Retrieves Jetpack editor state
+ *
+ * @returns {object|null}Object The Jetpack Editor State.
+ */
 export default function getJetpackData() {
 	return get( 'object' === typeof window ? window : null, [ JETPACK_DATA_PATH ], null );
 }
