53d899b943de94386a85150c2b781001bbc6cc7a
leogermani <leogermani@gmail.com>
2022-02-04 17:39:11 -0300

    Add has_required_plan to product info and implement it in Search (#22682)
    * Add has_required_plan info and implement in Search
    
    * updating lock files
    
    * monorepo gardening
    
    * create needs_purchase status

--- projects/packages/my-jetpack/src/products/class-product.php
+++ projects/packages/my-jetpack/src/products/class-product.php
@@ -95,6 +95,7 @@ abstract class Product {
 			'status'                   => static::get_status(),
 			'pricing_for_ui'           => static::get_pricing_for_ui(),
 			'requires_user_connection' => static::$requires_user_connection,
+			'has_required_plan'        => static::has_required_plan(),
 			'class'                    => get_called_class(),
 		);
 	}
@@ -142,6 +143,19 @@ abstract class Product {
 	abstract public static function get_pricing_for_ui();
 
 	/**
+	 * Checks whether the current plan (or purchases) of the site already supports the product
+	 *
+	 * Returns true if it supports. Return false if a purchase is still required.
+	 *
+	 * Free products will always return true.
+	 *
+	 * @return boolean
+	 */
+	public static function has_required_plan() {
+		return true;
+	}
+
+	/**
 	 * Undocumented function
 	 *
 	 * @return string
@@ -149,6 +163,9 @@ abstract class Product {
 	public static function get_status() {
 		if ( static::is_active() ) {
 			$status = 'active';
+			if ( ! static::has_required_plan() ) {
+				$status = 'needs_purchase';
+			}
 		} elseif ( ! self::is_plugin_installed() ) {
 			$status = 'plugin_absent';
 		} else {
