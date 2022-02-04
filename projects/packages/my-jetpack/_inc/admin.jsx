82cd3fdcb5a5cf344ec1629c901d78ebf6fc7ea5
retrofox <rdsuarez@gmail.com>
2022-02-04 07:31:23 -0300

    [not verified] my-jetpack: introduce Layout component

--- projects/packages/my-jetpack/_inc/admin.jsx
+++ projects/packages/my-jetpack/_inc/admin.jsx
@@ -4,6 +4,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Container, Col } from '@automattic/jetpack-components';
 
 /**
  * Internal dependencies
@@ -12,15 +13,46 @@ import MyJetpackScreen from './components/my-jetpack-screen';
 import ConnectionScreen from './components/connection-screen';
 import { initStore } from './state/store';
 import { BoostInterstitial } from './components/product-interstitial';
+import GoBackLink from './components/go-back-link';
 
 initStore();
+
+/**
+ * Main layout component.
+ * Takes it as an initil basic approach that
+ * could get more complex in the future.
+ *
+ * @param {object} props          - Component props.
+ * @param {object} props.nav      - Header navigation.
+ * @param {object} props.children - Child components.
+ * @returns {object}                Layout react component.
+ */
+function Layout( { nav, children } ) {
+	if ( ! nav ) {
+		return children;
+	}
+
+	const headerNav = nav.back ? (
+		<Container horizontalSpacing={ 3 } horizontalGap={ 3 }>
+			<Col>
+				<GoBackLink />
+			</Col>
+		</Container>
+	) : null;
+
+	return (
+		<>
+			{ headerNav }
+			{ children }
+		</>
+	);
+}
 
 /**
  * The initial renderer function.
  */
 function render() {
 	const container = document.getElementById( 'my-jetpack-container' );
-
 	if ( null === container ) {
 		return;
 	}
@@ -29,8 +61,14 @@ function render() {
 		<HashRouter>
 			<Routes>
 				<Route path="/" element={ <MyJetpackScreen /> } />
-				<Route path="/connection" element={ <ConnectionScreen /> } />
-				<Route path="/add-boost" element={ <BoostInterstitial /> } />
+				<Route
+					path="/connection"
+					element={ <Layout nav={ { back: true } } children={ <ConnectionScreen /> } /> }
+				/>
+				<Route
+					path="/add-boost"
+					element={ <Layout nav={ { back: true } } children={ <BoostInterstitial /> } /> }
+				/>
 			</Routes>
 		</HashRouter>,
 		container
