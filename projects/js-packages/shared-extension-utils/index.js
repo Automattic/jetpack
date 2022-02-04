b49a9c114d315fe3197fb013e6de79deff72e88d
Manish Mishra <manishmishra@Manishs-MacBook-Pro.local>
2022-02-04 19:41:44 +0530

    [not verified] Moved register-jetpack-plugin file to shared-extension-utils package
    Updated imports in jetpack plugin

--- projects/js-packages/shared-extension-utils/index.js
+++ projects/js-packages/shared-extension-utils/index.js
@@ -1,2 +1,3 @@
 export { default as getJetpackData, JETPACK_DATA_PATH } from './src/get-jetpack-data';
 export { default as getJetpackExtensionAvailability } from './src/get-jetpack-extension-availability';
+export { default as registerJetpackPlugin } from './src/register-jetpack-plugin';
