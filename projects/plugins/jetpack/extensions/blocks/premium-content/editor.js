cdbc81260c3572d1f05fb2afbaf656f9173d9938
Jeremy Herve <jeremy@jeremy.hu>
2022-02-07 18:52:37 +0100

    Premium Content Block: restrict registration to wpcom
    Fixes #18521
    
    We already do the same on the PHP side; let's also limit the use of the block to WordPress.com sites (simple and atomic).

--- projects/plugins/jetpack/extensions/blocks/premium-content/editor.js
+++ projects/plugins/jetpack/extensions/blocks/premium-content/editor.js
@@ -2,29 +2,33 @@
  * Internal dependencies
  */
 import registerJetpackBlock from '../../shared/register-jetpack-block';
+import { isAtomicSite, isSimpleSite } from '../../shared/site-type-utils';
 import { name, settings } from '.';
 import {
 	name as loggedOutViewBlockName,
-	settings as loggedOutViewBlockSettings
+	settings as loggedOutViewBlockSettings,
 } from './logged-out-view/.';
 import {
 	name as subscriberViewBlockName,
-	settings as subscriberViewBlockSettings
+	settings as subscriberViewBlockSettings,
 } from './subscriber-view/.';
-import {
-	name as buttonsBlockName,
-	settings as buttonsBlockSettings
-} from './buttons/.';
+import { name as buttonsBlockName, settings as buttonsBlockSettings } from './buttons/.';
 import {
 	name as loginButtonBlockName,
-	settings as loginButtonBlockSettings
+	settings as loginButtonBlockSettings,
 } from './login-button/.';
 
 const prefix = false;
-registerJetpackBlock( name, settings, [
-	{ name: loggedOutViewBlockName, settings: loggedOutViewBlockSettings },
-	{ name: subscriberViewBlockName, settings: subscriberViewBlockSettings },
-	{ name: buttonsBlockName, settings: buttonsBlockSettings },
-	{ name: loginButtonBlockName, settings: loginButtonBlockSettings },
-], prefix );
-
+if ( isSimpleSite() || isAtomicSite() ) {
+	registerJetpackBlock(
+		name,
+		settings,
+		[
+			{ name: loggedOutViewBlockName, settings: loggedOutViewBlockSettings },
+			{ name: subscriberViewBlockName, settings: subscriberViewBlockSettings },
+			{ name: buttonsBlockName, settings: buttonsBlockSettings },
+			{ name: loginButtonBlockName, settings: loginButtonBlockSettings },
+		],
+		prefix
+	);
+}
