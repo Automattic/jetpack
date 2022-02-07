e3129d5bcee6cafc57ce2040f9c2d63126837e79
Adnan Haque <adnan007.id@gmail.com>
2022-02-07 14:27:01 +0600

    Conditionally load critical CSS module based on const

--- projects/plugins/boost/app/assets/src/js/pages/settings/elements/Module.svelte
+++ projects/plugins/boost/app/assets/src/js/pages/settings/elements/Module.svelte
@@ -22,6 +22,7 @@
 	const dispatch = createEventDispatcher();
 
 	const isEnabled = derived( modules, $modules => $modules[ slug ] && $modules[ slug ].enabled );
+	const isAvailable = derived( modules, $modules => typeof $modules[ slug ] !== 'undefined' );
 
 	let error = null;
 	let isLoading = false;
@@ -52,32 +53,34 @@
 	} );
 </script>
 
-<div class="jb-feature-toggle">
-	<div class="jb-feature-toggle__toggle">
-		<Toggle
-			id={`jb-feature-toggle-${ slug }`}
-			checked={$isEnabled}
-			bind:disabled={isLoading}
-			on:click={handleToggle}
-		/>
-	</div>
-	<div class="jb-feature-toggle__content">
-		<slot name="title" />
-
-		<div class="jb-feature-toggle__text">
-			<slot name="description" />
+{#if $isAvailable}
+	<div class="jb-feature-toggle">
+		<div class="jb-feature-toggle__toggle">
+			<Toggle
+				id={`jb-feature-toggle-${ slug }`}
+				checked={$isEnabled}
+				bind:disabled={isLoading}
+				on:click={handleToggle}
+			/>
 		</div>
-
 		<div class="jb-feature-toggle__content">
-			<slot />
+			<slot name="title" />
 
-			{#if error}
-				<ErrorNotice title={__( 'Failed to toggle feature', 'jetpack-boost' )} {error} />
-			{/if}
+			<div class="jb-feature-toggle__text">
+				<slot name="description" />
+			</div>
 
-			{#if $isEnabled}
-				<slot name="meta" />
-			{/if}
+			<div class="jb-feature-toggle__content">
+				<slot />
+
+				{#if error}
+					<ErrorNotice title={__( 'Failed to toggle feature', 'jetpack-boost' )} {error} />
+				{/if}
+
+				{#if $isEnabled}
+					<slot name="meta" />
+				{/if}
+			</div>
 		</div>
 	</div>
-</div>
+{/if}
