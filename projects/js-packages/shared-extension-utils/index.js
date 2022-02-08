8aca540f7adecaf04644a9d8d9ec478a410d385b
mkm119 <95693514+mkm119@users.noreply.github.com>
2022-02-08 20:12:42 +0530

    Moved get-jetpack-extension-availability file to shared-extension-utils (#22642)
    Co-authored-by: Manish Mishra <manishmishra@Manishs-MacBook-Pro.local>
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>

--- projects/js-packages/shared-extension-utils/index.js
+++ projects/js-packages/shared-extension-utils/index.js
@@ -1,1 +1,2 @@
 export { default as getJetpackData, JETPACK_DATA_PATH } from './src/get-jetpack-data';
+export { default as getJetpackExtensionAvailability } from './src/get-jetpack-extension-availability';
