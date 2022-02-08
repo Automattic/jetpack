3836c45f7ee8876d9f4f419286b55986fdcc3af9
mkm119 <95693514+mkm119@users.noreply.github.com>
2022-02-08 20:57:21 +0530

    Moved register-jetpack-plugin file to shared-extension-utils package (#22674)
    Co-authored-by: Manish Mishra <manishmishra@Manishs-MacBook-Pro.local>
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>

--- projects/plugins/jetpack/extensions/blocks/social-previews/editor.js
+++ projects/plugins/jetpack/extensions/blocks/social-previews/editor.js
@@ -1,15 +1,17 @@
 /**
- * WordPress dependencies
+ * External dependencies
  */
 import { registerPlugin } from '@wordpress/plugins';
-import { getJetpackExtensionAvailability } from '@automattic/jetpack-shared-extension-utils';
+import {
+	getJetpackExtensionAvailability,
+	registerJetpackPlugin,
+} from '@automattic/jetpack-shared-extension-utils';
 
 /**
  * Internal dependencies
  */
 import { name, settings, SocialPreviews } from '.';
 import { isSimpleSite } from '../../shared/site-type-utils';
-import registerJetpackPlugin from '../../shared/register-jetpack-plugin';
 
 /*
  * Register the main "social-previews" extension if the feature is available
