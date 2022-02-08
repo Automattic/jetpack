c2b9fe26034fddace1571db250aa11058721c114
retrofox <rdsuarez@gmail.com>
2022-02-08 12:30:49 -0300

    [not verified] my-jetpack: add unlink to checkout link when no user connected

--- projects/packages/my-jetpack/_inc/utils/get-product-checkout-url.js
+++ projects/packages/my-jetpack/_inc/utils/get-product-checkout-url.js
@@ -15,16 +15,18 @@ import { MY_JETPACK_PRODUCT_CHECKOUT } from '../constants';
  * Return the product redirect URL, according to
  * the Jetpack redirect source, site, path, and redirect_to params.
  *
- * @param {string} product - Checkout product name
+ * @param {string} product          - Checkout product name
+ * @param {boolean} isUserConnected - True when the user is connected Jetpack
  * @returns {string} the redirect URL
  */
-export default function getProductCheckoutUrl( product ) {
+export default function getProductCheckoutUrl( product, isUserConnected ) {
 	const { siteSuffix: site, redirectUrl } = window?.myJetpackInitialState || {};
 	const redirect_to = `${ redirectUrl }&product=${ product }`;
+	const unlinked = ! isUserConnected ? '&unlinked=1' : '';
 
 	return getRedirectUrl( MY_JETPACK_PRODUCT_CHECKOUT, {
 		site,
 		path: 'jetpack_search',
-		query: `redirect_to=${ redirect_to }`,
+		query: `redirect_to=${ redirect_to }${ unlinked }`,
 	} );
 }
