400292181804288ed0a15786f422cabe278b0841
Adnan Haque <adnan007.id@gmail.com>
2022-02-09 14:07:33 +0600

    Moved critical css Generator to Critical_CSS feature root

--- projects/plugins/boost/app/features/optimizations/cloud-css/Cloud_CSS.php
+++ projects/plugins/boost/app/features/optimizations/cloud-css/Cloud_CSS.php
@@ -2,7 +2,7 @@
 namespace Automattic\Jetpack_Boost\Features\Optimizations\Cloud_CSS;
 
 use Automattic\Jetpack_Boost\Contracts\Feature;
-use Automattic\Jetpack_Boost\Features\Optimizations\Critical_CSS\Generate\Generator;
+use Automattic\Jetpack_Boost\Features\Optimizations\Critical_CSS\Generator;
 use Automattic\Jetpack_Boost\Lib\Critical_CSS\Critical_CSS_Storage;
 use Automattic\Jetpack_Boost\Lib\Critical_CSS\Display_Critical_CSS;
 use Automattic\Jetpack_Boost\Lib\Critical_CSS\Source_Providers\Source_Providers;
--- projects/plugins/boost/app/features/optimizations/critical-css/Critical_CSS.php
+++ projects/plugins/boost/app/features/optimizations/critical-css/Critical_CSS.php
@@ -3,7 +3,6 @@
 namespace Automattic\Jetpack_Boost\Features\Optimizations\Critical_CSS;
 
 use Automattic\Jetpack_Boost\Contracts\Feature;
-use Automattic\Jetpack_Boost\Features\Optimizations\Critical_CSS\Generate\Generator;
 use Automattic\Jetpack_Boost\Lib\Critical_CSS\Critical_CSS_State;
 use Automattic\Jetpack_Boost\Lib\Critical_CSS\Critical_CSS_Storage;
 use Automattic\Jetpack_Boost\Lib\Critical_CSS\Display_Critical_CSS;
