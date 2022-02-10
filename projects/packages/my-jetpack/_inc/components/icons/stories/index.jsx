a277a232ce676c9fb7dacf73ca8370f0c27079dc
retrofox <rdsuarez@gmail.com>
2022-02-10 16:23:11 -0300

    [not verified] my-jetpack: add Backup icons component. story

---
+++ projects/packages/my-jetpack/_inc/components/icons/stories/index.jsx
@@ -0,0 +1,64 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import * as allIcons from '../index.jsx';
+import styles from './style.module.scss';
+
+export default {
+	title: 'Packages/My Jetpack/Icons',
+	component: allIcons,
+	parameters: {},
+};
+
+const sizes = [
+	{
+		label: 'small',
+		value: '24',
+	},
+	{
+		label: 'medium',
+		value: '48',
+	},
+	{
+		label: 'large',
+		value: '72',
+	},
+];
+
+/**
+ * Icons story components.
+ *
+ * @returns {object} - story component
+ */
+function IconsStory() {
+	return (
+		<div>
+			{ sizes.map( size => (
+				<div key={ size.label }>
+					<h3>{ size.label }</h3>
+					<div className={ styles[ 'icons-container' ] }>
+						{ Object.keys( allIcons ).map( key => {
+							const Icon = allIcons[ key ];
+							return (
+								<div className={ styles[ 'icon-wrapper' ] } key={ key }>
+									<Icon size={ size.value } />
+								</div>
+							);
+						} ) }
+					</div>
+				</div>
+			) ) }
+		</div>
+	);
+}
+
+const Template = args => <IconsStory { ...args } />;
+
+const DefaultArgs = {};
+export const Default = Template.bind( {} );
+Default.args = DefaultArgs;
