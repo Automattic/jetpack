d7b3523361d4e529f64b3f79cd0739943c417316
Jasper Kang <jasper.kang@automattic.com>
2022-02-11 13:10:20 +1300

    change init option values to null

--- projects/packages/search/tests/php/bootstrap.php
+++ projects/packages/search/tests/php/bootstrap.php
@@ -27,9 +27,9 @@ Constants::$set_constants['JETPACK__API_BASE'] = 'https://jetpack.wordpress.com/
  */
 function dbless_default_options() {
 	return array(
-		'sidebars_widgets'                       => false,
-		Helper::get_widget_option_name()         => false,
-		Options::OPTION_PREFIX . 'result_format' => false,
+		'sidebars_widgets'                       => null,
+		Helper::get_widget_option_name()         => null,
+		Options::OPTION_PREFIX . 'result_format' => null,
 	);
 }
 
