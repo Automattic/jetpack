a6bf408f32fc18488751133f2421a5342babb215
Copons <Copons@users.noreply.github.com>
2022-02-11 21:31:22 +0000

    [not verified] Remove console log

--- projects/plugins/jetpack/extensions/blocks/premium-content/deprecated/v1/index.js
+++ projects/plugins/jetpack/extensions/blocks/premium-content/deprecated/v1/index.js
@@ -9,14 +9,10 @@ import save from '../../save';
 
 export default {
 	isEligible: ( attributes, innerBlocks ) => {
-		console.log( { type: 'copons', innerBlocks } );
-		if ( 'premium-content/logged-out-view' === innerBlocks?.[0]?.name ) {
-			return true
+		if ( 'premium-content/logged-out-view' === innerBlocks?.[ 0 ]?.name ) {
+			return true;
 		}
 	},
-	migrate: ( attributes, innerBlocks ) => [
-		attributes,
-		[ innerBlocks[1], innerBlocks[0] ],
-	],
+	migrate: ( attributes, innerBlocks ) => [ attributes, [ innerBlocks[ 1 ], innerBlocks[ 0 ] ] ],
 	save,
 };
