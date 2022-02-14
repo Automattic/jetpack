c88d23646a85fbad1ce0a0adbbe56313a9562a39
Jason Moon <jason.moon@automattic.com>
2022-02-14 16:36:38 -0700

    Restore review feedback changes

--- projects/packages/search/src/class-helper.php
+++ projects/packages/search/src/class-helper.php
@@ -932,10 +932,10 @@ class Helper {
 	public static function get_wpcom_site_id() {
 		// Returns local blog ID for a multi-site network.
 		if ( defined( 'IS_WPCOM' ) && constant( 'IS_WPCOM' ) ) {
-			return get_current_blog_id();
+			return \get_current_blog_id();
 		}
 
 		// Returns cache site ID.
-		return Jetpack_Options::get_option( 'id' );
+		return \Jetpack_Options::get_option( 'id' );
 	}
 }
