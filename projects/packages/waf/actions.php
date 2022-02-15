72c2872330350cccd048d9b81e2de7216b6b492b
Kolja Zuelsdorf <kolja.zuelsdorf@automattic.com>
2022-02-15 17:12:24 +0100

    Ensured that running the Jetpack WAF still works when the JETPACK_WAF_MODE constant is already defined before the plugin_loaded action is executed.

--- projects/packages/waf/actions.php
+++ projects/packages/waf/actions.php
@@ -13,6 +13,21 @@ if ( ! function_exists( 'add_action' ) ) {
 }
 
 /**
+ * Determines if the passed $option is one of the allowed WAF operation modes.
+ *
+ * @param  string $option The mode option.
+ * @return bool
+ */
+function jpwaf_is_allowed_mode( $option ) {
+	$allowed_modes = array(
+		'normal',
+		'silent',
+	);
+
+	return in_array( $option, $allowed_modes, true );
+}
+
+/**
  * Runs the WAF in the WP context.
  *
  * @return void
@@ -20,18 +35,20 @@ if ( ! function_exists( 'add_action' ) ) {
 add_action(
 	'plugin_loaded',
 	function () {
-		$allowed_modes = array(
-			'normal',
-			'silent',
-		);
 
-		$mode_option = get_option( 'jetpack_waf_mode' );
+		if ( ! defined( 'JETPACK_WAF_MODE' ) ) {
+			$mode_option = get_option( 'jetpack_waf_mode' );
 
-		if ( ! in_array( $mode_option, $allowed_modes, true ) ) {
-			return;
+			if ( ! jpwaf_is_allowed_mode( $mode_option ) ) {
+				return;
+			}
+
+			define( 'JETPACK_WAF_MODE', $mode_option );
 		}
 
-		define( 'JETPACK_WAF_MODE', $mode_option );
+		if ( ! jpwaf_is_allowed_mode( JETPACK_WAF_MODE ) ) {
+			return;
+		}
 
 		if ( ! WafRunner::did_run() ) {
 			WafRunner::run();
