b4c6554714d38396c3c20407ec8bb21cac1f9e97
Samiff <samiff@users.noreply.github.com>
2022-02-15 11:54:51 -0700

    [not verified] enhanced-distribution.php: doc comments

--- projects/plugins/jetpack/modules/enhanced-distribution.php
+++ projects/plugins/jetpack/modules/enhanced-distribution.php
@@ -9,9 +9,13 @@
  * Module Tags: Writing
  * Feature: Engagement
  * Additional Search Queries: google, seo, firehose, search, broadcast, broadcasting
+ *
+ * @package automattic/jetpack
  */
 
-// In case it's active prior to upgrading to 1.9
+/**
+ * In case it's active prior to upgrading to '1.9'.
+ */
 function jetpack_enhanced_distribution_before_activate_default_modules() {
 	$old_version         = Jetpack_Options::get_option( 'old_version' );
 	list( $old_version ) = explode( ':', $old_version );
@@ -25,12 +29,11 @@ function jetpack_enhanced_distribution_before_activate_default_modules() {
 
 add_action( 'jetpack_before_activate_default_modules', 'jetpack_enhanced_distribution_before_activate_default_modules' );
 
-/**
- * If a request has ?get_freshly_pressed_data=true appended
- * to the end, then let's provide the necessary data back via JSON.
- */
 if ( isset( $_GET['get_freshly_pressed_data'] ) ) {
-	add_action( 'template_redirect', 'jetpack_get_freshly_pressed_data' );
+	/**
+	 * If a request has ?get_freshly_pressed_data=true appended
+	 * to the end, then let's provide the necessary data back via JSON.
+	 */
 	function jetpack_get_freshly_pressed_data() {
 		if ( is_single() ) {
 			wp_send_json_success(
@@ -47,6 +50,7 @@ if ( isset( $_GET['get_freshly_pressed_data'] ) ) {
 			);
 		}
 	}
+	add_action( 'template_redirect', 'jetpack_get_freshly_pressed_data' );
 }
 
 add_action( 'rss_head', 'jetpack_enhanced_distribution_feed_id' );
@@ -54,6 +58,9 @@ add_action( 'rss_item', 'jetpack_enhanced_distribution_post_id' );
 add_action( 'rss2_head', 'jetpack_enhanced_distribution_feed_id' );
 add_action( 'rss2_item', 'jetpack_enhanced_distribution_post_id' );
 
+/**
+ * Output feed identifier based on blog ID.
+ */
 function jetpack_enhanced_distribution_feed_id() {
 	(int) $id = Jetpack_Options::get_option( 'id' );
 	if ( $id > 0 ) {
@@ -62,6 +69,9 @@ function jetpack_enhanced_distribution_feed_id() {
 	}
 }
 
+/**
+ * Output feed item identifier based on current post ID.
+ */
 function jetpack_enhanced_distribution_post_id() {
 	$id = get_the_ID();
 	if ( $id ) {
