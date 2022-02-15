c8912d1da3eadb7e3f8da33d313932662b11df34
Foteini Giannaropoulou <giannaropoulou.foteini@gmail.com>
2022-02-15 16:37:26 +0200

    e2e tests: Update Sync helper functions

--- projects/plugins/jetpack/tests/e2e/helpers/sync-helper.js
+++ projects/plugins/jetpack/tests/e2e/helpers/sync-helper.js
@@ -2,52 +2,60 @@ import { execWpCommand } from 'jetpack-e2e-commons/helpers/utils-helper.cjs';
 import logger from 'jetpack-e2e-commons/logger.cjs';
 
 export async function enableSync() {
-	return await execWpCommand( 'jetpack sync enable' );
+	return execWpCommand( 'jetpack sync enable' );
 }
 
 export async function disableSync() {
-	return await execWpCommand( 'jetpack sync disable' );
+	return execWpCommand( 'jetpack sync disable' );
 }
 
 export async function getSyncStatus() {
-	return await execWpCommand( 'jetpack sync status' );
+	return execWpCommand( 'jetpack sync status' );
 }
 
 export async function enableDedicatedSync() {
-	return await execWpCommand( 'option update jetpack_sync_settings_dedicated_sync_enabled 1' );
+	return execWpCommand( 'option update jetpack_sync_settings_dedicated_sync_enabled 1' );
 }
 
 export async function disableDedicatedSync() {
-	return await execWpCommand( 'option update jetpack_sync_settings_dedicated_sync_enabled 0' );
+	return execWpCommand( 'option update jetpack_sync_settings_dedicated_sync_enabled 0' );
 }
 
 export async function isSyncQueueEmpty() {
-	const status = await getSyncStatus();
+	try {
+		const status = await getSyncStatus();
 
-	return status.includes( 'queue_size' ) && status.includes( 'queue_size	0' );
+		return status.includes( 'queue_size' ) && status.includes( 'queue_size	0' );
+	} catch ( e ) {
+		logger.error( `isSyncQueueEmpty: ${ e }` );
+		return false;
+	}
 }
 
 /**
- * Wait till the Sync Queue is empty or a certain timeout has been reached.
+ * Wait till the Sync Queue is empty.
  *
- * @param {number} [timeoutMs=10000] - The timeout to stop checking if the Sync Queue is empty.
+ * @param {number} [interval=1000]  - The time we want to wait between
+ *                                  checking if the Sync Queue is empty.
+ * @param {number} [maxAttempts=10] - An upper bound for the number of
+ *                                  attempts to check if the Sync Queue is empty.
  */
-export async function waitTillSyncQueueisEmpty( timeoutMs = 10000 ) {
-	return new Promise( ( resolve, reject ) => {
-		const timeWas = new Date();
-		const wait = setInterval( function () {
-			if ( isSyncQueueEmpty() ) {
-				logger.action(
-					`waitTillSyncQueueisEmpty: Sync Queue is empty after ${ new Date() - timeWas } ms`
-				);
-				clearInterval( wait );
-				resolve();
-			} else if ( new Date() - timeWas > timeoutMs ) {
-				// Timeout
-				logger.warn( `waitTillSyncQueueisEmpty: Timeout after ${ new Date() - timeWas } ms` );
-				clearInterval( wait );
-				reject();
-			}
-		}, 20 );
-	} );
+export async function waitTillSyncQueueisEmpty( interval = 1000, maxAttempts = 10 ) {
+	let attempts = 0;
+
+	const executeWait = async ( resolve, reject ) => {
+		const isEmpty = await isSyncQueueEmpty();
+		attempts++;
+
+		if ( true === isEmpty ) {
+			logger.action( `waitTillSyncQueueisEmpty: Sync Queue is empty after ${ attempts } attempts` );
+			return resolve();
+		} else if ( maxAttempts && attempts === maxAttempts ) {
+			logger.error( `waitTillSyncQueueisEmpty: Exceeded max attempts ( ${ maxAttempts } )` );
+			return reject();
+		}
+		setTimeout( executeWait, interval, resolve, reject );
+	};
+
+	return new Promise( executeWait );
 }
