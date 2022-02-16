08329b604c950882623bba7c6c8c975e88609a30
Samiff <samiff@users.noreply.github.com>
2022-02-16 10:42:30 -0700

    blog-stats.php: function vis

--- projects/plugins/jetpack/modules/widgets/blog-stats.php
+++ projects/plugins/jetpack/modules/widgets/blog-stats.php
@@ -1,4 +1,4 @@
-<?php
+<?php // phpcs:ignore WordPress.Files.FileName.InvalidClassFileNam
 /**
  * Blog Stats Widget.
  *
@@ -26,7 +26,7 @@ class Jetpack_Blog_Stats_Widget extends WP_Widget {
 	/**
 	 * Constructor
 	 */
-	function __construct() {
+	public function __construct() {
 		$widget_ops = array(
 			'classname'                   => 'blog-stats',
 			'description'                 => esc_html__( 'Show a hit counter for your blog.', 'jetpack' ),
@@ -85,7 +85,7 @@ class Jetpack_Blog_Stats_Widget extends WP_Widget {
 	 *
 	 * @return void
 	 */
-	function form( $instance ) {
+	public function form( $instance ) {
 		$instance = wp_parse_args( $instance, $this->defaults() );
 		?>
 
@@ -112,7 +112,7 @@ class Jetpack_Blog_Stats_Widget extends WP_Widget {
 	 *
 	 * @return array Updated safe values to be saved.
 	 */
-	function update( $new_instance, $old_instance ) {
+	public function update( $new_instance, $old_instance ) {
 		$instance          = array();
 		$instance['title'] = wp_kses( $new_instance['title'], array() );
 		$instance['hits']  = wp_kses( $new_instance['hits'], array() );
@@ -128,7 +128,7 @@ class Jetpack_Blog_Stats_Widget extends WP_Widget {
 	 * @param array $args     Widget arguments.
 	 * @param array $instance Saved values from database.
 	 */
-	function widget( $args, $instance ) {
+	public function widget( $args, $instance ) {
 		$instance = wp_parse_args( $instance, $this->defaults() );
 
 		/** This filter is documented in wp-includes/widgets/class-wp-widget-pages.php */
