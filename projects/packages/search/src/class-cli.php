3e87dffc138473e206d65517b5250ea60ff81956
Jasper Kang <jasper.kang@automattic.com>
2022-02-17 09:48:18 +1300

    ask user to pass in user id or username

--- projects/packages/search/src/class-cli.php
+++ projects/packages/search/src/class-cli.php
@@ -9,6 +9,7 @@ namespace Automattic\Jetpack\Search;
 
 use \WP_CLI;
 use \WP_CLI_Command;
+use \WP_Error;
 
 /**
  * Provide functionality by WPCLI.
@@ -19,12 +20,22 @@ class CLI extends WP_CLI_Command {
 	 *
 	 * ## EXAMPLES
 	 *
-	 * wp jetpack-search auto_config
+	 * wp jetpack-search auto_config user_login
+	 *
+	 * wp jetpack-search auto_config user_id
+	 *
+	 * @param array $args - Args passsed in.
 	 */
-	public function auto_config() {
+	public function auto_config( $args ) {
 		try {
+			if ( empty( $args ) ) {
+				WP_CLI::error( 'Jetpack Search: a user_login or user_id is required' );
+			}
 			// Some functions requires admin capabilities to run.
-			$this->set_admin_user();
+			$ret = $this->set_user( $args[0] );
+			if ( is_wp_error( $ret ) ) {
+				WP_CLI::error( $ret->get_error_message() );
+			}
 			$blog_id = Helper::get_wpcom_site_id();
 			Instant_Search::instance( $blog_id )->auto_config_search();
 			WP_CLI::line( 'Jetpack Search: auto config success!' );
@@ -34,21 +45,19 @@ class CLI extends WP_CLI_Command {
 	}
 
 	/**
-	 * Choose a admin user and set it.
+	 * Set current user by ID or username
+	 *
+	 * @param string|int $user User ID or username.
 	 */
-	protected function set_admin_user() {
-		$user = get_user_by( 'ID', 1 );
-		if ( user_can( $user, 'administrator' ) ) {
-			wp_set_current_user( 1 );
-			return;
+	protected function set_user( $user ) {
+		$get_user_by = 'user_login';
+		if ( filter_var( $user, FILTER_VALIDATE_INT ) > 0 ) {
+			$get_user_by = 'ID';
 		}
-
-		$users = get_users();
-		foreach ( $users as $user ) {
-			if ( user_can( $user, 'administrator' ) ) {
-				wp_set_current_user( $user->ID );
-				return;
-			}
+		$user = get_user_by( $get_user_by, (string) $user );
+		if ( ! $user ) {
+			return new WP_Error( "Could not find user ${user} by ${get_user_by}." );
 		}
+		return wp_set_current_user( $user->ID );
 	}
 }
