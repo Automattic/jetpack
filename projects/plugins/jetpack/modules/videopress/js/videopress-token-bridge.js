2757b6ff1a1d4b8ed09f93f3bfaede6fe6c42bdd
John Caruso <johncaruso@gmail.com>
2022-02-16 11:13:55 -0500

    [not verified] remove unnecessary indentation

--- projects/plugins/jetpack/modules/videopress/js/videopress-token-bridge.js
+++ projects/plugins/jetpack/modules/videopress/js/videopress-token-bridge.js
@@ -5,39 +5,41 @@
 			return;
 		}
 
-		if ( event.data.event === 'videopress_token_request' ) {
-			if ( ! window.videopressAjax ) {
-				return;
-			}
-
-			var fetchData = {
-				action: 'videopress-get-playback-jwt',
-				guid: event.data.guid,
-			};
-
-			fetch( window.videopressAjax, {
-				method: 'POST',
-				credentials: 'same-origin',
-				body: new URLSearchParams( fetchData ),
-			} )
-				.then( function ( response ) {
-					if ( response.ok ) {
-						return response.json();
-					}
-					throw Error( 'Response is not ok' );
-				} )
-				.then( function ( jsonResponse ) {
-					if ( !! jsonResponse.success && jsonResponse.data ) {
-						event.source.postMessage(
-							{
-								event: 'videopress_token_received',
-								guid: fetchData.guid,
-								jwt: jsonResponse.data.jwt,
-							},
-							'*'
-						);
-					}
-				} );
+		if ( event.data.event !== 'videopress_token_request' ) {
+			return;
 		}
+
+		if ( ! window.videopressAjax ) {
+			return;
+		}
+
+		var fetchData = {
+			action: 'videopress-get-playback-jwt',
+			guid: event.data.guid,
+		};
+
+		fetch( window.videopressAjax, {
+			method: 'POST',
+			credentials: 'same-origin',
+			body: new URLSearchParams( fetchData ),
+		} )
+			.then( function ( response ) {
+				if ( response.ok ) {
+					return response.json();
+				}
+				throw Error( 'Response is not ok' );
+			} )
+			.then( function ( jsonResponse ) {
+				if ( !! jsonResponse.success && jsonResponse.data ) {
+					event.source.postMessage(
+						{
+							event: 'videopress_token_received',
+							guid: fetchData.guid,
+							jwt: jsonResponse.data.jwt,
+						},
+						'*'
+					);
+				}
+			} );
 	} );
 } )();
