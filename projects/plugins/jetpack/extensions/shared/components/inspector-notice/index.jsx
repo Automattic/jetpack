eb3687d5c3dbf0fefb35a0bca7217d44b8e49a1d
Yan Sern <yansern@yan.sr>
2022-02-16 10:26:47 +0200

    Use className, move style.css under internal dependencies.
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>

--- projects/plugins/jetpack/extensions/shared/components/inspector-notice/index.jsx
+++ projects/plugins/jetpack/extensions/shared/components/inspector-notice/index.jsx
@@ -2,11 +2,15 @@
  * External dependencies
  */
 import { JetpackLogo } from '@automattic/jetpack-components';
+
+/**
+ * Internal dependencies
+ */
 import './style.scss';
 
 export default function InspectorNotice( { children } ) {
 	return (
-		<div class="jetpack-inspector-notice">
+		<div className="jetpack-inspector-notice">
 			<span>{ children }</span>
 			<JetpackLogo height="16" logoColor="#1a1a1a" showText={ false } />
 		</div>
