deaec3bcef61ade5bedff1c75a12e9f27c7f9cfe
Jasper Kang <jasper.kang@automattic.com>
2022-02-17 09:59:26 +1300

    ask user to pass in user id or username

--- projects/packages/search/src/class-cli.php
+++ projects/packages/search/src/class-cli.php
@@ -29,35 +29,37 @@ class CLI extends WP_CLI_Command {
 	public function auto_config( $args ) {
 		try {
 			if ( empty( $args ) ) {
-				WP_CLI::error( 'Jetpack Search: a user_login or user_id is required' );
+				WP_CLI::error( 'Jetpack Search: a user login or ID is required.' );
 			}
-			// Some functions requires admin capabilities to run.
+
+			// Some functions may require admin capabilities to run.
 			$ret = $this->set_user( $args[0] );
 			if ( is_wp_error( $ret ) ) {
 				WP_CLI::error( $ret->get_error_message() );
 			}
+
 			$blog_id = Helper::get_wpcom_site_id();
 			Instant_Search::instance( $blog_id )->auto_config_search();
-			WP_CLI::line( 'Jetpack Search: auto config success!' );
+			WP_CLI::success( 'Jetpack Search: auto config success!' );
 		} catch ( \Exception $e ) {
 			WP_CLI::error( $e->getMessage() );
 		}
 	}
 
 	/**
-	 * Set current user by ID or username
+	 * Set current user by ID or login
 	 *
-	 * @param string|int $user User ID or username.
+	 * @param string|int $user User ID or login.
 	 */
 	protected function set_user( $user ) {
-		$get_user_by = 'user_login';
+		$get_user_by = 'login';
 		if ( filter_var( $user, FILTER_VALIDATE_INT ) > 0 ) {
 			$get_user_by = 'ID';
 		}
-		$user = get_user_by( $get_user_by, (string) $user );
-		if ( ! $user ) {
-			return new WP_Error( "Could not find user ${user} by ${get_user_by}." );
+		$user_info = get_user_by( $get_user_by, (string) $user );
+		if ( ! $user_info ) {
+			return new WP_Error( 'user_not_found', "Could not find user '${user}' by ${get_user_by}." );
 		}
-		return wp_set_current_user( $user->ID );
+		return wp_set_current_user( $user_info->ID );
 	}
 }
