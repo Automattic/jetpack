e7df8d79a648430a0e8f1910ab65238fb28807fd
Adnan Haque <adnan007.id@gmail.com>
2022-02-17 13:31:46 +0600

    Add helpful css comment to indicate critical css is generating

--- projects/plugins/boost/app/features/optimizations/cloud-css/Cloud_CSS_Request.php
+++ projects/plugins/boost/app/features/optimizations/cloud-css/Cloud_CSS_Request.php
@@ -11,6 +11,7 @@ namespace Automattic\Jetpack_Boost\Features\Optimizations\Cloud_CSS;
 
 use Automattic\Jetpack_Boost\Lib\Boost_API;
 use Automattic\Jetpack_Boost\Lib\Critical_CSS\Critical_CSS_State;
+use Automattic\Jetpack_Boost\Lib\Critical_CSS\Critical_CSS_Storage;
 
 /**
  * Cloud CSS State
@@ -33,6 +34,15 @@ class Cloud_CSS_Request {
 		$payload              = array( 'providers' => $sources );
 		$payload['requestId'] = md5( wp_json_encode( $payload ) );
 
+		$this->reset_existing_css();
 		return $client->post( 'cloud-css', $payload );
+	}
+
+	private function reset_existing_css() {
+		$storage = new Critical_CSS_Storage();
+
+		foreach ( $this->state->get_provider_urls() as $provider => $urls ) {
+			$storage->store_css( $provider, '/* ' . __( 'Jetpack Boost is currently generating critical css for this page', 'jetpack-boost' ) . ' */' );
+		}
 	}
 }
