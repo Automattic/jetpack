2f52b4d8fdb6ed55d938686a999dc2ee4f771bcd
Matias Ventura <matias@automattic.com>
2022-02-18 12:38:04 +0100

    Update projects/plugins/jetpack/extensions/shared/components/inspector-notice/index.jsx
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>

--- projects/plugins/jetpack/extensions/shared/components/inspector-notice/index.jsx
+++ projects/plugins/jetpack/extensions/shared/components/inspector-notice/index.jsx
@@ -12,7 +12,7 @@ export default function InspectorNotice( { children } ) {
 	return (
 		<div className="jetpack-inspector-notice">
 			<span>{ children }</span>
-			<JetpackLogo height="16" logoColor="#1a1a1a" showText={ false } />
+			<JetpackLogo height={ 16 } logoColor="#1a1a1a" showText={ false } />
 		</div>
 	);
 }
