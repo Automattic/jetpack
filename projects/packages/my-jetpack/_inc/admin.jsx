77cda5be38459dd81b1b900bb0ab4fdc27ca9069
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2022-02-18 13:54:27 -0300

    My Jetpack: Implements CloseLink type of navigation for layout

--- projects/packages/my-jetpack/_inc/admin.jsx
+++ projects/packages/my-jetpack/_inc/admin.jsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 import React, { useCallback } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Container, Col, JetpackFooter } from '@automattic/jetpack-components';
+import classNames from 'classnames';
 
 /**
  * Internal dependencies
@@ -22,10 +23,16 @@ import {
 	VideoPressInterstitial,
 } from './components/product-interstitial';
 import GoBackLink from './components/go-back-link';
+import CloseLink from './components/close-link';
 import styles from './style.module.scss';
 import useAnalytics from './hooks/use-analytics';
 
 initStore();
+
+const NAV_TYPES = {
+	LINK: 'link',
+	CLOSE: 'close',
+};
 
 /**
  * Main layout component.
@@ -33,31 +40,51 @@ initStore();
  * could get more complex in the future.
  *
  * @param {object} props          - Component props.
- * @param {boolean} props.nav     - Header navigation.
  * @param {object} props.children - Child components.
  * @param {string} props.slug     - A product slug or undefined. Will Fire Tracks event with product:slug if not undefined
+ * @param {string} props.navType  - Type of nav that will be used
  * @returns {object}                Layout react component.
  */
-function Layout( { nav = false, children, slug } ) {
+function Layout( { children, slug, navType = NAV_TYPES.LINK } ) {
 	const {
 		tracks: { recordEvent },
 	} = useAnalytics();
+
 	const onClick = useCallback( () => {
 		if ( slug ) {
 			recordEvent( 'jetpack_myjetpack_product_interstitial_back_link_click', { product: slug } );
 		}
 	}, [ recordEvent, slug ] );
 
-	if ( ! nav ) {
-		return children;
+	const navColClassName = classNames( {
+		[ styles[ 'relative-col' ] ]: navType === NAV_TYPES.CLOSE,
+	} );
+
+	let containerProps = {};
+	let navComponent = null;
+
+	switch ( navType ) {
+		case NAV_TYPES.CLOSE:
+			navComponent = <CloseLink className={ styles[ 'close-link' ] } />;
+			containerProps = {
+				horizontalGap: 0,
+				horizontalSpacing: 8,
+			};
+			break;
+		case NAV_TYPES.LINK:
+		default:
+			navComponent = <GoBackLink onClick={ onClick } />;
+			containerProps = {
+				horizontalSpacing: 3,
+				horizontalGap: 3,
+			};
+			break;
 	}
 
 	return (
 		<div className={ styles.layout }>
-			<Container horizontalSpacing={ 3 } horizontalGap={ 3 }>
-				<Col>
-					<GoBackLink onClick={ onClick } />
-				</Col>
+			<Container { ...containerProps }>
+				<Col className={ navColClassName }>{ navComponent }</Col>
 				<Col>{ children }</Col>
 			</Container>
 			<Container horizontalSpacing={ 5 }>
@@ -75,37 +102,32 @@ const MyJetpack = () => (
 			<Route path="/" element={ <MyJetpackScreen /> } />
 			<Route
 				path="/connection"
-				element={ <Layout nav={ true } children={ <ConnectionScreen /> } /> }
+				element={ <Layout navType="close" children={ <ConnectionScreen /> } /> }
 			/>
 			<Route
 				path="/add-anti-spam"
-				element={ <Layout nav={ true } children={ <AntiSpamInterstitial /> } slug="anti-spam" /> }
+				element={ <Layout children={ <AntiSpamInterstitial /> } slug="anti-spam" /> }
 			/>
 			<Route
 				path="/add-backup"
-				element={ <Layout nav={ true } children={ <BackupInterstitial /> } slug="backup" /> }
+				element={ <Layout children={ <BackupInterstitial /> } slug="backup" /> }
 			/>
 			<Route
 				path="/add-boost"
-				element={ <Layout nav={ true } children={ <BoostInterstitial /> } slug="boost" /> }
+				element={ <Layout children={ <BoostInterstitial /> } slug="boost" /> }
 			/>
-			<Route
-				path="/add-crm"
-				element={ <Layout nav={ true } children={ <CRMInterstitial /> } slug="crm" /> }
-			/>
+			<Route path="/add-crm" element={ <Layout children={ <CRMInterstitial /> } slug="crm" /> } />
 			<Route
 				path="/add-scan"
-				element={ <Layout nav={ true } children={ <ScanInterstitial /> } slug="scan" /> }
+				element={ <Layout children={ <ScanInterstitial /> } slug="scan" /> }
 			/>
 			<Route
 				path="/add-search"
-				element={ <Layout nav={ true } children={ <SearchInterstitial /> } slug="search" /> }
+				element={ <Layout children={ <SearchInterstitial /> } slug="search" /> }
 			/>
 			<Route
 				path="/add-videopress"
-				element={
-					<Layout nav={ true } children={ <VideoPressInterstitial /> } slug="videopress" />
-				}
+				element={ <Layout children={ <VideoPressInterstitial /> } slug="videopress" /> }
 			/>
 		</Routes>
 	</HashRouter>
