a0495d398e56c915ea2fbed98cfee7bc2157f563
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2022-02-21 17:47:11 -0300

    My Jetpack: Implement navigation structure direct on ProductInterstitial

--- projects/packages/my-jetpack/_inc/components/product-interstitial/index.jsx
+++ projects/packages/my-jetpack/_inc/components/product-interstitial/index.jsx
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import React, { useCallback, useEffect } from 'react';
-import { Container, Col } from '@automattic/jetpack-components';
+import { Container, Col, AdminPage } from '@automattic/jetpack-components';
 
 /**
  * Internal dependencies
@@ -18,6 +18,7 @@ import { useProduct } from '../../hooks/use-product';
 import useMyJetpackNavigate from '../../hooks/use-my-jetpack-navigate';
 import getProductCheckoutUrl from '../../utils/get-product-checkout-url';
 import useMyJetpackConnection from '../../hooks/use-my-jetpack-connection';
+import GoBackLink from '../go-back-link';
 
 /**
  * Product Interstitial component.
@@ -85,24 +86,40 @@ export default function ProductInterstitial( { installsPlugin = false, slug, chi
 				} );
 		}
 	}, [ activate, isFree, installsPlugin, afterInstallation ] );
+
+	const onClickGoBack = useCallback( () => {
+		if ( slug ) {
+			recordEvent( 'jetpack_myjetpack_product_interstitial_back_link_click', { product: slug } );
+		}
+	}, [ recordEvent, slug ] );
+
 	return (
-		<Container
-			className={ ! isUpgradableByBundle ? styles.container : null }
-			horizontalSpacing={ 0 }
-			horizontalGap={ 0 }
-			fluid
-		>
-			<Col sm={ 4 } md={ 4 } lg={ 7 }>
-				<Product
-					slug={ slug }
-					trackButtonClick={ trackProductClick }
-					onClick={ installsPlugin ? clickHandler : undefined }
-				/>
-			</Col>
-			<Col sm={ 4 } md={ 4 } lg={ 5 } className={ styles.imageContainer }>
-				{ children }
-			</Col>
-		</Container>
+		<AdminPage showHeader={ false } showBackground={ false }>
+			<Container horizontalSpacing={ 3 } horizontalGap={ 3 }>
+				<Col>
+					<GoBackLink onClick={ onClickGoBack } />
+				</Col>
+				<Col>
+					<Container
+						className={ ! isUpgradableByBundle ? styles.container : null }
+						horizontalSpacing={ 0 }
+						horizontalGap={ 0 }
+						fluid
+					>
+						<Col sm={ 4 } md={ 4 } lg={ 7 }>
+							<Product
+								slug={ slug }
+								trackButtonClick={ trackProductClick }
+								onClick={ installsPlugin ? clickHandler : undefined }
+							/>
+						</Col>
+						<Col sm={ 4 } md={ 4 } lg={ 5 } className={ styles.imageContainer }>
+							{ children }
+						</Col>
+					</Container>
+				</Col>
+			</Container>
+		</AdminPage>
 	);
 }
 
