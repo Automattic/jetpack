6986d984959c80351fdd69e967571ffde07f2175
Dan Roundhill <dan@automattic.com>
2022-02-22 14:37:51 -0700

    Add Resumable Upload Support to the VideoPress Block (#22584)
    * UI stub with styles and VideoPress logo.
    
    * WIP Tus uploader for collaboration with teammate.
    
    * Adding use-uploader.js
    
    * Get upload jwt.
    
    * Bug fixes to get it kinda working!
    
    * Handle upload errors and upload completed events
    
    * Set up resumable block to be removable by using `useBlockProps`. Stop the Tus upload if the block is removed.
    
    * Added an error state and retry UI. Still needs testing :D
    
    * Added error state w/ try again and cancel button. Cleaned up some `console.log` debug lines.
    
    * Fixed issue where drag and drop would still use the VP block even if it was disabled.
    
    * Fixed error when refreshing the page while the video was uploading.
    
    * Grab the guid!
    
    * Fixed media library selection.
    
    * Added is_automattician() check for enabling resumable uploads.
    
    * Force-enable resumable uploads for now
    
    * Removed a few console logs.
    
    * Adding changelog file
    
    * Linting fixes
    
    * PHP linting
    
    * Updating package.json and pnpm-lock
    
    * Fixing variable name to fix resumable upload token append.
    
    * Fixes for handling completed upload.
    
    * Moving enqueue of js that enables resumable uploads so it works on wp.com
    
    * Set the background color of the resumable uploader to always be white.
    
    * Changing endpoint to a wpcom/v2 which works better on wpcom and Jetpack
    
    * Disable drop area for the resumable upload placeholder
    
    * Get a fresh JWT when doing HEAD requests to determine if an upload can
    be resumed.
    
    * PHP lint fix
    
    * Code cleanup based on feedback.
    
    * Update error string to differentiate better
    
    Co-authored-by: John Caruso <johncaruso@gmail.com>
    
    * Removing unused header value
    
    * Remove `resume` option that is now deprecated.
    
    * Use camelCase for a few svg args
    
    * Create a new props object to pass to the original placeholder component.
    
    Co-authored-by: Panos Kountanis <panosktn@gmail.com>
    Co-authored-by: John Caruso <johncaruso@gmail.com>
    Co-authored-by: Samiff <samiff@users.noreply.github.com>

---
+++ projects/plugins/jetpack/extensions/blocks/videopress/components.js
@@ -0,0 +1,34 @@
+/**
+ * WordPress dependencies
+ */
+import { createContext } from '@wordpress/element';
+
+/**
+ * VideoPress Block context
+ * Used to pass data between components of a VideoPress Block.
+ */
+export const VideoPressBlockContext = createContext();
+
+/**
+ * VideoPress Block Provider
+ *
+ * @param {object}  props - Provider properties.
+ * @param {Function}  props.onFilesSelected - Callback when video files have been selected
+ * @param {Function}  props.onMediaItemSelected - Callback when video files have been selected
+ * @param {Function}  props.onUploadFinished - Callback when video file upload finished
+ * @param {boolean} props.children - Provider Children.
+ * @returns {*} Provider component.
+ */
+export const VideoPressBlockProvider = ( {
+	onFilesSelected,
+	onMediaItemSelected,
+	onUploadFinished,
+	children,
+} ) => {
+	return (
+		<VideoPressBlockContext.Provider
+			value={ { onFilesSelected, onMediaItemSelected, onUploadFinished } }
+			children={ children }
+		/>
+	);
+};
