31ba0de42fab1aa5adce6801ee4780ad1fcc4bf5
Dean Sas <dean.sas@automattic.com>
2022-02-23 12:25:14 +0000

    Payments blocks: Add more robust set of keywords
    Add a more extensive set of keywords to some of the payments blocks, to
    make it easier for users to find what they are looking for.
    
    Fixes #22898

--- projects/plugins/jetpack/extensions/blocks/donations/index.js
+++ projects/plugins/jetpack/extensions/blocks/donations/index.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
+import { __, _x } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
@@ -24,7 +24,51 @@ export const settings = {
 	description: __( 'Collect one-time, monthly, or annually recurring donations.', 'jetpack' ),
 	icon: DonationsIcon,
 	category: 'earn',
-	keywords: [ __( 'Donations', 'jetpack' ) ],
+	keywords: [
+		_x( 'accept', 'block search term', 'jetpack' ),
+		_x( 'charity', 'block search term', 'jetpack' ),
+		_x( 'contribution', 'block search term', 'jetpack' ),
+		_x( 'commerce', 'block search term', 'jetpack' ),
+		_x( 'credit card', 'block search term', 'jetpack' ),
+		_x( 'debit card', 'block search term', 'jetpack' ),
+		_x( 'donate', 'block search term', 'jetpack' ),
+		_x( 'Donations', 'block search term', 'jetpack' ),
+		_x( 'earn', 'block search term', 'jetpack' ),
+		_x( 'ecommerce', 'block search term', 'jetpack' ),
+		_x( 'form', 'block search term', 'jetpack' ),
+		_x( 'fundraising', 'block search term', 'jetpack' ),
+		_x( 'fundraiser', 'block search term', 'jetpack' ),
+		_x( 'give', 'block search term', 'jetpack' ),
+		_x( 'giving', 'block search term', 'jetpack' ),
+		'gofundme',
+		'go fund me',
+		_x( 'gratuity', 'block search term', 'jetpack' ),
+		_x( 'memberships', 'block search term', 'jetpack' ),
+		_x( 'money', 'block search term', 'jetpack' ),
+		_x( 'monthly', 'block search term', 'jetpack' ),
+		_x( 'nonprofit', 'block search term', 'jetpack' ),
+		_x( 'non-profit', 'block search term', 'jetpack' ),
+		_x( 'onetime', 'block search term', 'jetpack' ),
+		_x( 'one time', 'block search term', 'jetpack' ),
+		_x( 'paid', 'block search term', 'jetpack' ),
+		'patreon',
+		_x( 'pay', 'block search term', 'jetpack' ),
+		_x( 'payments', 'block search term', 'jetpack' ),
+		_x( 'processing', 'block search term', 'jetpack' ),
+		_x( 'processor', 'block search term', 'jetpack' ),
+		_x( 'recurring', 'block search term', 'jetpack' ),
+		_x( 'repeat', 'block search term', 'jetpack' ),
+		'stripe',
+		_x( 'subscribe', 'block search term', 'jetpack' ),
+		_x( 'subscriptions', 'block search term', 'jetpack' ),
+		_x( 'support', 'block search term', 'jetpack' ),
+		_x( 'sponsor', 'block search term', 'jetpack' ),
+		'square',
+		_x( 'tipping', 'block search term', 'jetpack' ),
+		'toast',
+		'venmo',
+		_x( 'yearly', 'block search term', 'jetpack' ),
+	],
 	supports: {
 		html: false,
 	},
