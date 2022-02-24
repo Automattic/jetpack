1e88722c9721d2ceb58b79c04349f7288e5ff3d9
Jasper Kang <jasper.kang@automattic.com>
2022-02-25 10:27:11 +1300

    conditions could be removed

--- projects/packages/search/src/initializers/class-jetpack-initializer.php
+++ projects/packages/search/src/initializers/class-jetpack-initializer.php
@@ -82,7 +82,7 @@ class Jetpack_Initializer extends Initializer {
 		}
 
 		// registers Jetpack Search widget.
-		add_action( 'widgets_init', array( 'Automattic\Jetpack\Search\Jetpack_Initializer', 'jetpack_search_widget_init' ) );
+		register_widget( 'Automattic\Jetpack\Search\Search_Widget' );
 
 		// Fired when plugin ready.
 		do_action( 'jetpack_search_loaded' );
@@ -101,23 +101,6 @@ class Jetpack_Initializer extends Initializer {
 	 */
 	public static function is_search_supported() {
 		return ( new Plan() )->supports_search();
-	}
-
-	/**
-	 * Register the widget if Jetpack Search is available and enabled.
-	 */
-	public static function jetpack_search_widget_init() {
-		if (
-		! self::is_connected()
-		|| ! self::is_search_supported()
-		|| ! ( new Module_Control() )->is_active()
-		) {
-			return;
-		}
-
-		// There won't be multiple widgets registered when Search stand alone plugin registers it again.
-		// Because the function tests the hash of the class, if they are the same, just register again.
-		register_widget( 'Automattic\Jetpack\Search\Search_Widget' );
 	}
 
 	/**
