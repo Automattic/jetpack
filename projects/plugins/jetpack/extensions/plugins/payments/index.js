982227536a77f95dc38de47c475258505d38b656
Jeremy Herve <jeremy@jeremy.hu>
2022-02-25 15:55:05 +0100

    Add tracking to the link
    This involves moving away from the native ExternalLink to a custom link. I'm not a big fan of that but that works.

--- projects/plugins/jetpack/extensions/plugins/payments/index.js
+++ projects/plugins/jetpack/extensions/plugins/payments/index.js
@@ -1,13 +1,28 @@
 /**
  * External dependencies
  */
-import { ExternalLink } from '@wordpress/components';
+import { Button } from '@wordpress/components';
 import { PluginPostPublishPanel } from '@wordpress/edit-post';
+import { useCallback } from '@wordpress/element';
+import { external, Icon } from '@wordpress/icons';
 import { __ } from '@wordpress/i18n';
 
-export const name = 'payments';
-export const settings = {
-	render: () => (
+/**
+ * Internal dependencies
+ */
+import analytics from '../../../_inc/client/lib/analytics';
+
+/**
+ * Style dependencies
+ */
+import './editor.scss';
+
+const PaymentsPostPublish = () => {
+	const trackClick = useCallback(
+		() => analytics.tracks.recordEvent( 'jetpack_editor_payments_post_publish_click' ),
+		[]
+	);
+	return (
 		<PluginPostPublishPanel className="jetpack-payments-post-publish-panel" initialOpen>
 			<p className="post-publish-panel__postpublish-subheader">
 				<strong>{ __( 'Start accepting payments', 'jetpack' ) }</strong>
@@ -18,11 +33,23 @@ export const settings = {
 					'jetpack'
 				) }
 			</p>
-			<p>
-				<ExternalLink href="https://wordpress.com/payments-donations/" target="_blank">
-					{ __( 'Learn more about these blocks', 'jetpack' ) }
-				</ExternalLink>
-			</p>
+			<div
+				role="link"
+				className="post-publish-panel__postpublish-buttons"
+				tabIndex={ 0 }
+				onClick={ trackClick }
+				onKeyDown={ trackClick }
+			>
+				<Button isPrimary href="https://wordpress.com/payments-donations/" target="_top">
+					{ __( 'Learn more about these blocks', 'jetpack' ) }{ ' ' }
+					<Icon icon={ external } className="payments-post-publish-outbound-link__external_icon" />
+				</Button>
+			</div>
 		</PluginPostPublishPanel>
-	),
+	);
+};
+
+export const name = 'payments';
+export const settings = {
+	render: PaymentsPostPublish,
 };
