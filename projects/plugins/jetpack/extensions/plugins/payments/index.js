c2149b69288c369b1f1981dfa9b05d5fc09c95b0
Jeremy Herve <jeremy@jeremy.hu>
2022-02-28 19:30:54 +0100

    Switch back to core's ExternalLink instead of custom solution
    This reverts part of the previous commit, 982227536a77f95dc38de47c475258505d38b656, since we can rely on `ExternalLink` to do most of the heavy-lifting.
    
    This also switches to using an existing component to save and redirect, as discussed here:
    https://github.com/Automattic/jetpack/pull/23118#discussion_r814974117
    
    I've also taken the opportunity to use `PluginPostPublishPanel's `title` instead of a custom title, as suggested here:
    https://github.com/Automattic/jetpack/pull/23118#discussion_r814982385
    
    Co-authored-by: Jacopo Tomasone <Copons@users.noreply.github.com>

--- projects/plugins/jetpack/extensions/plugins/payments/index.js
+++ projects/plugins/jetpack/extensions/plugins/payments/index.js
@@ -1,50 +1,46 @@
 /**
  * External dependencies
  */
-import { Button } from '@wordpress/components';
+import { ExternalLink } from '@wordpress/components';
 import { PluginPostPublishPanel } from '@wordpress/edit-post';
-import { useCallback } from '@wordpress/element';
-import { external, Icon } from '@wordpress/icons';
 import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
 import analytics from '../../../_inc/client/lib/analytics';
-
-/**
- * Style dependencies
- */
-import './editor.scss';
+import useAutosaveAndRedirect from '../../shared/use-autosave-and-redirect/index';
 
 const PaymentsPostPublish = () => {
-	const trackClick = useCallback(
-		() => analytics.tracks.recordEvent( 'jetpack_editor_payments_post_publish_click' ),
-		[]
-	);
+	const paymentInfoUrl = 'https://wordpress.com/payments-donations/';
+	const { autosaveAndRedirect } = useAutosaveAndRedirect( paymentInfoUrl );
+	const trackClick = event => {
+		event.preventDefault();
+		analytics.tracks.recordEvent( 'jetpack_editor_payments_post_publish_click' );
+		autosaveAndRedirect( event );
+	};
+
 	return (
-		<PluginPostPublishPanel className="jetpack-payments-post-publish-panel" initialOpen>
-			<p className="post-publish-panel__postpublish-subheader">
-				<strong>{ __( 'Start accepting payments', 'jetpack' ) }</strong>
-			</p>
+		<PluginPostPublishPanel
+			className="jetpack-payments-post-publish-panel"
+			title={ __( 'Start accepting payments', 'jetpack' ) }
+			initialOpen
+		>
 			<p>
 				{ __(
 					'Insert the Payments block button or the Donations block form -- no plugin required.',
 					'jetpack'
 				) }
 			</p>
-			<div
-				role="link"
-				className="post-publish-panel__postpublish-buttons"
-				tabIndex={ 0 }
-				onClick={ trackClick }
-				onKeyDown={ trackClick }
-			>
-				<Button isPrimary href="https://wordpress.com/payments-donations/" target="_top">
-					{ __( 'Learn more about these blocks', 'jetpack' ) }{ ' ' }
-					<Icon icon={ external } className="payments-post-publish-outbound-link__external_icon" />
-				</Button>
-			</div>
+			<p>
+				<ExternalLink
+					href="https://wordpress.com/payments-donations/"
+					target="_blank"
+					onClick={ trackClick }
+				>
+					{ __( 'Learn more about these blocks', 'jetpack' ) }
+				</ExternalLink>
+			</p>
 		</PluginPostPublishPanel>
 	);
 };
