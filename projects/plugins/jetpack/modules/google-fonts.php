d6bf8d2be1786502512d167558bc37588e8e9fe8
Grant Kinney <hi@grant.mk>
2022-02-28 16:41:59 -0600

    [not verified] Add filter fonts list

--- projects/plugins/jetpack/modules/google-fonts.php
+++ projects/plugins/jetpack/modules/google-fonts.php
@@ -6,7 +6,7 @@
  * Recommendation Order: 2
  * First Introduced: $$next-version$$
  * Requires Connection: No
- * Auto Activate: Yes
+ * Auto Activate: No
  * Module Tags: Fonts, Recommended
  * Feature: Writing
  * Additional Search Queries: fonts, webfonts, typography
@@ -55,18 +55,20 @@ const JETPACK_GOOGLE_FONTS_LIST = array(
  *
  * @return void
  */
-function jetpack_add_google_font_provider() {
-	wp_register_webfont_provider( 'google', '\Automattic\Jetpack\Fonts\Google_Fonts_Provider' );
+function jetpack_add_google_fonts_provider() {
+	wp_register_webfont_provider( 'google-fonts', '\Automattic\Jetpack\Fonts\Google_Fonts_Provider' );
 
-	foreach ( JETPACK_GOOGLE_FONTS_LIST as $font_family ) {
+	$fonts_to_register = apply_filters( 'jetpack_google_fonts_list', JETPACK_GOOGLE_FONTS_LIST );
+
+	foreach ( $fonts_to_register as $font_family ) {
 		wp_register_webfonts(
 			array(
 				array(
 					'font-family' => $font_family,
-					'provider'    => 'google',
+					'provider'    => 'google-fonts',
 				),
 			)
 		);
 	}
 }
-add_action( 'after_setup_theme', 'jetpack_add_google_font_provider' );
+add_action( 'after_setup_theme', 'jetpack_add_google_fonts_provider' );
