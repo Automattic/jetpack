ac77960073b73eacb2c97c2c49d26275436d1595
Mustaque Ahmed <amustaque97@gmail.com>
2022-03-01 00:42:10 +0530

    fix import error `ExternalLink`
    this commit is done to fix test case failure `Recommendations – Feature Prompt`

--- projects/plugins/jetpack/_inc/client/recommendations/prompts/feature-prompt/index.jsx
+++ projects/plugins/jetpack/_inc/client/recommendations/prompts/feature-prompt/index.jsx
@@ -6,7 +6,7 @@ import { createInterpolateElement } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import React, { useCallback, useEffect } from 'react';
 import { connect } from 'react-redux';
-import ExternalLink from '@wordpress/components';
+import { ExternalLink } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -69,15 +69,7 @@ const FeaturePromptComponent = props => {
 			question={ question }
 			description={ createInterpolateElement( description, {
 				strong: <strong />,
-				ExternalLink: (
-					<ExternalLink
-						href={ descriptionLink }
-						target="_blank"
-						icon={ true }
-						iconSize={ 16 }
-						onClick={ onExternalLinkClick }
-					/>
-				),
+				ExternalLink: <ExternalLink href={ descriptionLink } onClick={ onExternalLinkClick } />,
 			} ) }
 			answer={
 				<div className="jp-recommendations-question__install-section">
