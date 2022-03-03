43ccdc300e1788dc8018d03cec9a9511465d2737
Samiff <samiff@users.noreply.github.com>
2022-03-03 11:50:27 -0700

    class.wpcom-json-api-update-post-endpoint.php: strict comparison

--- projects/plugins/jetpack/json-endpoints/class.wpcom-json-api-update-post-endpoint.php
+++ projects/plugins/jetpack/json-endpoints/class.wpcom-json-api-update-post-endpoint.php
@@ -759,7 +759,7 @@ class WPCOM_JSON_API_Update_Post_Endpoint extends WPCOM_JSON_API_Post_Endpoint {
 
 				$meta = (object) $meta;
 
-				if ( Jetpack_SEO_Posts::DESCRIPTION_META_KEY == $meta->key && ! Jetpack_SEO_Utils::is_enabled_jetpack_seo() ) { // phpcs:ignore Universal.Operators.StrictComparisons.LooseEqual
+				if ( Jetpack_SEO_Posts::DESCRIPTION_META_KEY === $meta->key && ! Jetpack_SEO_Utils::is_enabled_jetpack_seo() ) {
 					return new WP_Error( 'unauthorized', __( 'SEO tools are not enabled for this site.', 'jetpack' ), 403 );
 				}
 
