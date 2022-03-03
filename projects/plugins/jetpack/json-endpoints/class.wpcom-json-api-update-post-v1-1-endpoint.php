78d9e09af0c5137aed77ee02f83643c601e87b4f
Samiff <samiff@users.noreply.github.com>
2022-03-03 13:25:26 -0700

    class.wpcom-json-api-update-post-v1-1-endpoint.php: fix DisallowMultipleAssignments

--- projects/plugins/jetpack/json-endpoints/class.wpcom-json-api-update-post-v1-1-endpoint.php
+++ projects/plugins/jetpack/json-endpoints/class.wpcom-json-api-update-post-v1-1-endpoint.php
@@ -592,17 +592,19 @@ class WPCOM_JSON_API_Update_Post_v1_1_Endpoint extends WPCOM_JSON_API_Post_v1_1_
 						break;
 					case 1:
 						// 1 image - make it big
-						$insert['post_content'] = $input['content'] = sprintf(
+						$input['content']       = sprintf(
 							"[gallery size=full ids='%s' columns=1]\n\n",
 							$media_id_string
 						) . $input['content'];
+						$insert['post_content'] = $input['content'];
 						break;
 					default:
 						// Several images - 3 column gallery.
-						$insert['post_content'] = $input['content'] = sprintf(
+						$input['content']       = sprintf(
 							"[gallery ids='%s']\n\n",
 							$media_id_string
 						) . $input['content'];
+						$insert['post_content'] = $input['content'];
 						break;
 				}
 			}
