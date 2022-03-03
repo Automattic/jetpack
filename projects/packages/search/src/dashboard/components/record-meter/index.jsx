8e36d9d954fa3af51117b09a326e9555f14c26cd
Anna <anna.mcphee@gmail.com>
2022-03-02 19:57:56 -0600

    [not verified] tidy up comments & logging

--- projects/packages/search/src/dashboard/components/record-meter/index.jsx
+++ projects/packages/search/src/dashboard/components/record-meter/index.jsx
@@ -16,10 +16,6 @@ import './style.scss';
 /**
  * Generate Record Meter showing how many records the user has indexed
  *
- * @param {object} props - Props
- * @param {number} props.postCount - Post count
- * @param {object} props.postTypeBreakdown - Post type breakdown (post type => number of posts)
- * @param {number} props.tierMaximumRecords - Max number of records allowed in user's current tier
  * @returns {React.Component} RecordMeter React component
  */
 export default function RecordMeter( { postCount, postTypeBreakdown, tierMaximumRecords } ) {
@@ -47,11 +43,6 @@ export default function RecordMeter( { postCount, postTypeBreakdown, tierMaximum
 								postTypeBreakdown={ postTypeBreakdown }
 							/>
 						</div>
-					) }
-					{ postCount && (
-						<p>
-							Post count: <strong>{ postCount }</strong>
-						</p>
 					) }
 				</div>
 				<div className="lg-col-span-2 md-col-span-1 sm-col-span-0"></div>
