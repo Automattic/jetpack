fcfc8178b6c4b638217634d692c76cb30daa97e5
Anna <anna.mcphee@gmail.com>
2022-03-02 19:44:00 -0600

    add basic file structures for chart

--- projects/packages/search/src/dashboard/components/record-meter/index.jsx
+++ projects/packages/search/src/dashboard/components/record-meter/index.jsx
@@ -1,8 +1,20 @@
-/**
- * External dependencies
+/* * External dependencies
  */
-import React from 'react';
+import React, { useState } from 'react';
 import { __ } from '@wordpress/i18n';
+
+/**
+ * WordPress dependencies
+ */
+import { useSelect } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import { STORE_ID } from 'store';
+import { BarChart } from './barchart';
+import getRecordInfo from './lib/recordInfo';
+import createData from './lib/createData';
 
 import './style.scss';
 
@@ -15,7 +27,17 @@ import './style.scss';
  * @param {number} props.tierMaximumRecords - Max number of records allowed in user's current tier
  * @returns {React.Component} RecordMeter React component
  */
-export default function RecordMeter( { postCount, postTypeBreakdown, tierMaximumRecords } ) {
+export default function RecordMeter() {
+	const tierMaximumRecords = useSelect( select => select( STORE_ID ).getTierMaximumRecords() );
+
+	const [ recordInfo, setRecordInfo ] = useState(
+		getRecordInfo( createData().data, createData().planInfo )
+	);
+
+	// quick console log for the moment, just to check the correct data & correct form is coming through the state
+	console.log( recordInfo.data );
+	console.log( recordInfo.isValid );
+
 	return (
 		<div className="jp-search-record-meter jp-search-dashboard-wrap">
 			<div className="jp-search-dashboard-row">
@@ -25,6 +47,7 @@ export default function RecordMeter( { postCount, postTypeBreakdown, tierMaximum
 					{ tierMaximumRecords && (
 						<p>
 							Tier maximum records: <strong>{ tierMaximumRecords }</strong>
+							<BarChart data={ recordInfo.data } isValid={ recordInfo.isValid } />
 						</p>
 					) }
 					{ postCount && (
