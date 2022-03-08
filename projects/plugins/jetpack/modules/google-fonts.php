ba49c7ff806ee68a7eb07931d48de6cb99971ca3
Jeremy Herve <jeremy@jeremy.hu>
2022-03-08 15:17:32 +0100

    Document new filter

--- projects/plugins/jetpack/modules/google-fonts.php
+++ projects/plugins/jetpack/modules/google-fonts.php
@@ -62,6 +62,15 @@ function jetpack_add_google_fonts_provider() {
 
 	wp_register_webfont_provider( 'jetpack-google-fonts', '\Automattic\Jetpack\Fonts\Google_Fonts_Provider' );
 
+	/**
+	 * Curated list of Google Fonts.
+	 *
+	 * @module google-fonts
+	 *
+	 * @since $$next-version$$
+	 *
+	 * @param array $fonts_to_register Array of Google Font names to register.
+	 */
 	$fonts_to_register = apply_filters( 'jetpack_google_fonts_list', JETPACK_GOOGLE_FONTS_LIST );
 
 	foreach ( $fonts_to_register as $font_family ) {
