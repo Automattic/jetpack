d8011214bbf0a5a7abb94f1841e0406470c05233
Kevin Zoschke <kevin.zoschke@automattic.com>
2022-03-08 15:00:10 -0500

    Complete discount card styling

--- projects/plugins/jetpack/_inc/client/recommendations/sidebar/discount-card/index.jsx
+++ projects/plugins/jetpack/_inc/client/recommendations/sidebar/discount-card/index.jsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 import React, { useCallback } from 'react';
 
 /**
@@ -25,28 +25,45 @@ const DiscountCard = () => {
 
 	return (
 		<div className="jp-recommendations-discount-card">
-			<div className="jp-recommendations-discount-card__card-container">
-				<div className="jp-recommendations-discount-card__card-header">
-					<img
-						src={ imagePath + 'recommendations/cloud-icon.png' }
-						alt={ __( 'An icon representing the cloud.', 'jetpack' ) }
-					/>
+			<div className="jp-recommendations-discount-card__container">
+				<div className="jp-recommendations-discount-card__card">
+					<div className="jp-recommendations-discount-card__card-header">
+						<img
+							className="jp-recommendations-discount-card__header-icon"
+							src={ imagePath + 'recommendations/cloud-icon.svg' }
+							alt=""
+						/>
+						<div className="jp-recommendations-discount-card__discount">
+							{ sprintf(
+								// translators: %s is a percentage
+								__( '%s off', 'jetpack' ),
+								'68%'
+							) }
+						</div>
+					</div>
+					<div className="jp-recommendations-discount-card__card-body">
+						<h3 className="jp-recommendations-discount-card__heading">
+							{ __( 'Increase your site security!', 'jetpack' ) }
+						</h3>
+						<ul className="jp-recommendations-discount-card__feature-list">
+							<li>{ __( 'Real-time cloud backups', 'jetpack' ) }</li>
+							<li>{ __( 'One-click restores', 'jetpack' ) }</li>
+							<li>{ __( 'Real-time malware scanning', 'jetpack' ) }</li>
+							<li>{ __( 'Comments and form spam protection', 'jetpack' ) }</li>
+						</ul>
+						<Button
+							className="jp-recommendations-discount-card__button"
+							rna
+							href={ '#/recommendations/product-suggestions' }
+							onClick={ onViewDiscountClick }
+						>
+							{ __( 'View discounted plans', 'jetpack' ) }
+						</Button>
+					</div>
 				</div>
-				<div className="jp-recommendations-discount-card__card-body">
-					<h3>{ __( 'Increase your site security!', 'jetpack' ) }</h3>
-					<ul className="jp-recommendations-discount-card__feature-list">
-						<li>{ __( 'Real-time cloud backups', 'jetpack' ) }</li>
-						<li>{ __( 'One-click restores', 'jetpack' ) }</li>
-						<li>{ __( 'Real-time malware scanning', 'jetpack' ) }</li>
-						<li>{ __( 'Comments and form spam protection', 'jetpack' ) }</li>
-					</ul>
-					<Button
-						className="jp-recommendations-discount-card__button"
-						href={ '#/recommendations/product-suggestions' }
-						onClick={ onViewDiscountClick }
-					>
-						{ __( 'View discounted plans', 'jetpack' ) }
-					</Button>
+				<div className="jp-recommendations-discount-card__timer">
+					{ __( 'Discounts ends in:', 'jetpack' ) }
+					<span className="jp-recommendations-discount-card__time">4d 5h 25m 36s</span>
 				</div>
 			</div>
 		</div>
