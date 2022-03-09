47dde95dfaa361adc07f5aa30c286a7743249a11
Jeremy Herve <jeremy@jeremy.hu>
2022-03-09 10:05:39 +0100

    Only add resource hint if the site supports the Webfonts API
    See #23306

--- projects/packages/google-fonts-provider/src/class-google-fonts-provider.php
+++ projects/packages/google-fonts-provider/src/class-google-fonts-provider.php
@@ -49,14 +49,17 @@ class Google_Fonts_Provider extends \WP_Webfonts_Provider {
 	}
 
 	/**
-	 * Adds a preconnect link for improving performance when downloading
-	 * Google Font files.
+	 * Adds a preconnect link for improving performance when downloading Google Font files.
+	 * Only do so if the site supports the Webfonts API.
 	 *
 	 * @param array  $urls          Array of resources and their attributes, or URLs to print for resource hints.
 	 * @param string $relation_type The relation type the URLs are printed for, e.g. 'preconnect' or 'prerender'.
 	 */
 	public static function font_source_resource_hint( $urls, $relation_type ) {
-		if ( 'preconnect' === $relation_type ) {
+		if (
+			'preconnect' === $relation_type
+			&& class_exists( 'WP_Webfonts_Provider' )
+		) {
 			$urls[] = array(
 				'href' => 'https://fonts.gstatic.com',
 				'crossorigin',
