5e5cdd7408bd44f8b66709bf725db13bd3f3bd05
retrofox <rdsuarez@gmail.com>
2022-03-09 17:33:08 -0300

    [not verified] jetpack: qr-post: add Jetpack logo as fallback

--- projects/plugins/jetpack/extensions/plugins/post-publish-qr-post-panel/components/qr-post.js
+++ projects/plugins/jetpack/extensions/plugins/post-publish-qr-post-panel/components/qr-post.js
@@ -4,7 +4,8 @@
 import { useSelect } from '@wordpress/data';
 import { store as editorStore } from '@wordpress/editor';
 import { Component } from '@wordpress/components';
-import { QRCode } from '@automattic/jetpack-components';
+import { JetpackLogo, QRCode } from '@automattic/jetpack-components';
+import { useRef, useEffect, useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -18,6 +19,25 @@ import useSiteLogo from '../hooks/use-site-logo.js';
  * @returns {Component}   The react component.
  */
 export default function QRPost() {
+	const wrapperElementRef = useRef();
+
+	// Pick and convert Jetpack logo to data image.
+	const [ jetpackLogoUrl, setJetpackLogo ] = useState();
+	useEffect( () => {
+		if ( ! wrapperElementRef?.current ) {
+			return;
+		}
+
+		const svgJetpackLogo = wrapperElementRef.current.querySelector( 'svg' );
+		if ( ! svgJetpackLogo ) {
+			return;
+		}
+
+		const serializedSVG = new XMLSerializer().serializeToString( svgJetpackLogo );
+		setJetpackLogo( `data:image/svg+xml;base64,${ window.btoa( serializedSVG ) }` );
+	}, [ wrapperElementRef ] );
+
+	// Pick title and permalink post.
 	const {
 		post: { title },
 		permalink,
@@ -29,17 +49,29 @@ export default function QRPost() {
 		[]
 	);
 
+	const { url: siteLogoUrl } = useSiteLogo();
+	const codeLogo = siteLogoUrl || jetpackLogoUrl;
+	
 	const codeContent = `${ title } ${ permalink }`;
-	const data = useSiteLogo( { generateDataUrl: true } );
-	const { dataUrl: src } = data;
 
 	return (
-		<QRCode
-			value={ codeContent }
-			size={ 238 }
-			imageSettings={ src && { src, width: 48, height: 48, excavate: true } }
-			renderAs="canvas"
-			level="H"
-		/>
+		<div ref={ wrapperElementRef }>
+			<QRCode
+				value={ codeContent }
+				size={ 248 }
+				imageSettings={
+					codeLogo && {
+						src: codeLogo,
+						width: 48,
+						height: 48,
+						excavate: true,
+					}
+				}
+				renderAs="canvas"
+				level="H"
+			/>
+
+			<JetpackLogo className="qr-post-jetpack-logo" width={ 48 } height={ 48 } showText={ false } />
+		</div>
 	);
 }
