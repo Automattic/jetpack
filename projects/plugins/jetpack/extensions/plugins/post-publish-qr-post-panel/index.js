437f32b626ba56e49ca943d75b223a8c3aee8679
retrofox <rdsuarez@gmail.com>
2022-03-09 18:05:55 -0300

    [not verified] jetpack: render QRPost in Jetpack sidebar

--- projects/plugins/jetpack/extensions/plugins/post-publish-qr-post-panel/index.js
+++ projects/plugins/jetpack/extensions/plugins/post-publish-qr-post-panel/index.js
@@ -1,9 +1,15 @@
 /**
  * External dependencies
  */
+import { PanelBody } from '@wordpress/components';
 import { PluginPostPublishPanel } from '@wordpress/edit-post';
 import { __ } from '@wordpress/i18n';
 import { useRef } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import JetpackPluginSidebar from '../../shared/jetpack-plugin-sidebar.js';
 
 /**
  * Internal dependencies
@@ -18,20 +24,39 @@ export const name = 'post-publish-qr-post-panel';
 export const settings = {
 	render: function PluginPostPublishPanelQRPost() {
 		const qrCodeRef = useRef();
-		return (
-			<PluginPostPublishPanel
-				name="post-publish-qr-post-panel"
-				title={ __( 'QR Code', 'jetpack' ) }
-				className="post-publish-qr-post-panel"
-				icon={ <QRIcon /> }
-				initialOpen={ true }
-			>
-				<div className="post-publish-qr-post-panel__container" ref={ qrCodeRef }>
-					<QRPost />
-				</div>
 
-				<ImageActionsPanelRow qrCodeRef={ qrCodeRef } />
-			</PluginPostPublishPanel>
+		const panelBodyProps = {
+			name: 'post-publish-qr-post-panel',
+			title: __( 'QR Code', 'jetpack' ),
+			className: 'post-publish-qr-post-panel',
+			icon: <QRIcon />,
+			initialOpen: true,
+		};
+
+		function QRPostPanelBodyContent() {
+			return (
+				<>
+					<div className="post-publish-qr-post-panel__container" ref={ qrCodeRef }>
+						<QRPost />
+					</div>
+
+					<ImageActionsPanelRow qrCodeRef={ qrCodeRef } />
+				</>
+			);
+		}
+
+		return (
+			<>
+				<PluginPostPublishPanel { ...panelBodyProps }>
+					<QRPostPanelBodyContent />
+				</PluginPostPublishPanel>
+
+				<JetpackPluginSidebar>
+					<PanelBody { ...panelBodyProps }>
+						<QRPostPanelBodyContent />
+					</PanelBody>
+				</JetpackPluginSidebar>
+			</>
 		);
 	},
 };
