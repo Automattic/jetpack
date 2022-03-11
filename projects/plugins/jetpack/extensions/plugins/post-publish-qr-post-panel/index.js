6e76c94baf7b9afc2fbcd67c51e6c8b4d6aaeda5
retrofox <rdsuarez@gmail.com>
2022-03-11 15:51:26 -0300

    [not verified] jetpack: add view and download buttons to QRPost modal

--- projects/plugins/jetpack/extensions/plugins/post-publish-qr-post-panel/index.js
+++ projects/plugins/jetpack/extensions/plugins/post-publish-qr-post-panel/index.js
@@ -4,7 +4,6 @@
 import { PanelBody, PanelRow } from '@wordpress/components';
 import { PluginPostPublishPanel } from '@wordpress/edit-post';
 import { __ } from '@wordpress/i18n';
-import { useRef } from '@wordpress/element';
 import { useSelect } from '@wordpress/data';
 import { store as editorStore } from '@wordpress/editor';
 
@@ -23,8 +22,6 @@ export const name = 'post-publish-qr-post-panel';
 
 export const settings = {
 	render: function PluginPostPublishPanelQRPost() {
-		const qrCodeRef = useRef();
-
 		const panelBodyProps = {
 			name: 'post-publish-qr-post-panel',
 			title: __( 'QR Code', 'jetpack' ),
@@ -40,7 +37,7 @@ export const settings = {
 
 		function QRPostPanelBodyContent() {
 			return (
-				<div className="post-publish-qr-post-panel__container" ref={ qrCodeRef }>
+				<>
 					<PanelRow>
 						<p>
 							{ __(
@@ -50,7 +47,7 @@ export const settings = {
 						</p>
 					</PanelRow>
 					<QRPostButton />
-				</div>
+				</>
 			);
 		}
 
