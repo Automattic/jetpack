9866f57434bcc40c81f20d6d1e3570cba7456f57
retrofox <rdsuarez@gmail.com>
2022-03-11 15:51:26 -0300

    [not verified] jetpack: add view and download buttons to QRPost modal

--- projects/plugins/jetpack/extensions/plugins/post-publish-qr-post-panel/components/qr-post.js
+++ projects/plugins/jetpack/extensions/plugins/post-publish-qr-post-panel/components/qr-post.js
@@ -12,12 +12,13 @@ import { JetpackLogo, QRCode } from '@automattic/jetpack-components';
  * Internal dependencies
  */
 import useSiteLogo from '../hooks/use-site-logo.js';
+import { handleDownloadQRCode } from '../utils/handle-download-qr-code.js';
 
 /**
  * React component that renders a QR code for the post,
  * pulling the post data from the editor store.
  *
- * @returns {Component}   The react component.
+ * @returns {Component} The react component.
  */
 export function QRPost() {
 	const wrapperElementRef = useRef();
@@ -66,30 +67,46 @@ export function QRPost() {
 }
 
 export function QRPostButton() {
+	const qrCodeRef = useRef();
+	const slug = useSelect( select => select( editorStore ).getEditedPostSlug(), [] );
 	const [ isModalOpen, setIsModalOpen ] = useState( false );
 	const switchModal = () => setIsModalOpen( v => ! v );
 	const closeModal = () => setIsModalOpen( false );
 
 	return (
 		<div className="qr-post-button">
-			<Button
-				isSecondary
-				onClick={ switchModal }
-				help={ __(
-					'Get advantage of the QR code to open the post in different devices, on the fly.',
-					'jetpack'
-				) }
-			>
+			<Button isSecondary onClick={ switchModal }>
 				{ __( 'Get QR code', 'jetpack' ) }
 			</Button>
 
 			{ isModalOpen && (
-				<Modal title={ __( 'QR Post code', 'jetpack' ) } onRequestClose={ closeModal }>
-					<QRPost />
+				<Modal
+					title={ __( 'QR Post code', 'jetpack' ) }
+					onRequestClose={ closeModal }
+					className="qr-post-modal"
+					ref={ qrCodeRef }
+				>
+					<div className="qr-post-modal__qr-code">
+						<QRPost />
+					</div>
 
-					<Button variant="secondary" isSmall onClick={ closeModal }>
-						{ __( 'Close', 'jetpack' ) }
-					</Button>
+					<div className="qr-post-modal__actions_buttons">
+						<Button
+							isSmall
+							isSecondary
+							onClick={ () => handleDownloadQRCode( slug, qrCodeRef, false ) }
+						>
+							{ __( 'View', 'jetpack' ) }
+						</Button>
+
+						<Button isSmall isSecondary onClick={ () => handleDownloadQRCode( slug, qrCodeRef ) }>
+							{ __( 'Download', 'jetpack' ) }
+						</Button>
+
+						<Button isSmall isSecondary onClick={ closeModal }>
+							{ __( 'Close', 'jetpack' ) }
+						</Button>
+					</div>
 				</Modal>
 			) }
 		</div>
