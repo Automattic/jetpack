daf8b3466e2a1253a95c942909ab86f33c1e3578
retrofox <rdsuarez@gmail.com>
2022-03-14 11:48:48 -0300

    jetpack: test

--- projects/plugins/jetpack/extensions/plugins/post-publish-qr-post-panel/utils/handle-download-qr-code.js
+++ projects/plugins/jetpack/extensions/plugins/post-publish-qr-post-panel/utils/handle-download-qr-code.js
@@ -7,21 +7,25 @@
  * @returns {void}           - Nothing when the image is not created.
  */
 export function handleDownloadQRCode( slug, ref, download = true ) {
+	console.log( 'slug: ', slug );
 	if ( ! slug ) {
 		return;
 	}
+	console.log( 'ref: ', ref );
 
 	if ( ! ref?.current ) {
 		return;
 	}
 
 	const canvasElement = ref.current.querySelector( 'canvas' );
+	console.log( 'canvasElement: ', canvasElement );
 	if ( ! canvasElement ) {
 		return;
 	}
 
 	// Convert to canvas element to data URL image.
 	canvasElement.toBlob( imageBlob => {
+		console.log( 'imageBlob: ', imageBlob );
 		const imageURL = URL.createObjectURL( imageBlob );
 		const tempLink = document.createElement( 'a' );
 		tempLink.href = imageURL;
