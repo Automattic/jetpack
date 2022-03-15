adf9c114ddb2001324acf70328dbcc71894a60b2
sdixon194 <steve.dixon@automattic.com>
2022-03-15 14:44:51 -0400

    undo change

--- tools/js-tools/git-hooks/pre-push-hook.js
+++ tools/js-tools/git-hooks/pre-push-hook.js
@@ -78,6 +78,7 @@ async function checkChangelogFiles() {
 						] );
 						if ( commitFiles.status === 0 ) {
 							checkChangelogFiles();
+							await spawnSync( 'git', [ 'push' ] );
 						}
 					}
 				}
