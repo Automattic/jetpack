cd7b3c28d87f350abf497db90ce597e9f474b1e1
Igor Zinovyev <zinigor@gmail.com>
2022-03-15 15:41:45 +0300

    [not verified] Added a stub file to load the new Tracking class.

---
+++ projects/packages/tracking/src/class-tracking.php
@@ -0,0 +1,10 @@
+<?php // phpcs:ignore
+/**
+ * Nosara Tracks for Jetpack
+ *
+ * @package automattic/jetpack-tracking
+ * @deprecated $$next-version$$
+ * @see automattic/jetpack-connection
+ */
+
+require_once __DIR__ . '/../../jetpack-connection/src/class-tracking.php';
