6744cd2cd737ece66ca4def82a47ff25506a8318
Manish Mishra <manishmishra@Manishs-MacBook-Pro.local>
2022-03-17 01:49:35 +0530

    [not verified] copied useAutosaveAndRedirect to shared-extension-utils

--- projects/js-packages/shared-extension-utils/index.js
+++ projects/js-packages/shared-extension-utils/index.js
@@ -4,3 +4,4 @@ export { isSimpleSite, isAtomicSite, isPrivateSite } from './src/site-type-utils
 export { default as getJetpackExtensionAvailability } from './src/get-jetpack-extension-availability';
 export { default as registerJetpackPlugin } from './src/register-jetpack-plugin';
 export { default as withHasWarningIsInteractiveClassNames } from './src/with-has-warning-is-interactive-class-names';
+export { default as useAutosaveAndRedirect } from './src/use-autosave-and-redirect';
