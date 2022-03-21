0e2df5b61b69f685a315924683e1593fe07f62fb
oskosk <oskosk@gmail.com>
2022-03-21 12:00:03 -0300

    Add Monitor product card

--- projects/packages/my-jetpack/_inc/components/product-cards-section/index.jsx
+++ projects/packages/my-jetpack/_inc/components/product-cards-section/index.jsx
@@ -15,6 +15,7 @@ import SearchCard from './search-card';
 import VideopressCard from './videopress-card';
 import CrmCard from './crm-card';
 import ExtrasCard from './extras-card';
+import MonitorCard from './monitor-card';
 
 /**
  * Product cards section component.
@@ -47,6 +48,9 @@ const ProductCardsSection = () => {
 			</Col>
 			<Col sm={ 4 } md={ 4 } lg={ 3 }>
 				<ExtrasCard admin={ true } />
+			</Col>
+			<Col sm={ 4 } md={ 4 } lg={ 3 }>
+				<MonitorCard admin={ true } />
 			</Col>
 		</Container>
 	);
