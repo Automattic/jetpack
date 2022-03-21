eba0d88bfeb37926b16ed4c3db3b817bfe1e0296
Gergely Csécsey <gcsecsey@gmail.com>
2022-03-21 12:41:38 +0100

    Replace the Yoast error notice when the site is in Coming Soon mode https://github.com/Automattic/wpcomsh/pull/846
    * replace the Yoast error notice when the site is in Coming Soon mode
    
    * update notice text
    
    * Update feature-plugins/full-site-editing.php
    
    Co-authored-by: Harris Papazoglou <hrrsppzgl@gmail.com>
    
    * only display SEO notice on certain pages
    
    * update conditions
    
    * update notice text
    
    * rename variable
    
    * also show notice when indexing is discouraged
    
    * add comments
    
    * fix condition
    
    * bump version
    
    Co-authored-by: Harris Papazoglou <hrrsppzgl@gmail.com>

--- projects/plugins/wpcomsh/feature-plugins/full-site-editing.php
+++ projects/plugins/wpcomsh/feature-plugins/full-site-editing.php
@@ -45,6 +45,79 @@ function wpcomsh_coming_soon_get_atomic_persistent_data( $wpcom_public_coming_so
 add_filter( 'option_wpcom_public_coming_soon', 'wpcomsh_coming_soon_get_atomic_persistent_data' );
 
 /**
+ * Replaces the Yoast SEO error notice from that warns of SEO issues
+ * when the site is in Coming Soon mode,
+ * or when search engines are discouraged from indexing the site.
+ *
+ * @return void
+ */
+function wpcom_public_coming_soon_replace_yoast_seo_notice() {
+	$wpseo_options = get_option('wpseo', false );	
+	if ( ! $wpseo_options ) {
+		// Do nothing if Yoast is not installed
+		return;
+	}
+	
+	$is_wpcom_public_coming_soon_enabled = 1 === (int) get_option('wpcom_public_coming_soon', 0);
+	$are_search_engines_discouraged = 0 === (int) get_option( 'blog_public' );
+	
+	// The Yoast SEO notice should be replaced
+	// either if the site is in Coming Soon mode,
+	// or if indexing is discouraged on the site
+	$should_replace_yoast_notice = $are_search_engines_discouraged || $is_wpcom_public_coming_soon_enabled;
+
+	if ( $should_replace_yoast_notice && ! $wpseo_options['ignore_search_engines_discouraged_notice'] ) {
+		// Hide the Yoast SEO notice if it's not hidden and the site is set to Coming Soon mode
+		$wpseo_options['ignore_search_engines_discouraged_notice'] = true;
+		update_option( 'wpseo', $wpseo_options );
+	}
+
+	if ( ! $should_replace_yoast_notice && $wpseo_options['ignore_search_engines_discouraged_notice'] ) {
+		// Restore the default setting if Coming Soon mode is disabled
+		$wpseo_options['ignore_search_engines_discouraged_notice'] = false;
+		update_option( 'wpseo', $wpseo_options );
+	}
+
+	$pagenow = $GLOBALS['pagenow'];
+	$on_wpseo_admin_page = $pagenow === 'admin.php' && strpos( filter_input( INPUT_GET, 'page' ), 'wpseo' ) === 0;
+	$notice_pages = [
+		'index.php',
+		'plugins.php',
+		'update-core.php',
+	];
+
+	// Only show the notice on certain admin pages and Yoast SEO admin pages
+	if (
+		$should_replace_yoast_notice
+		&& $wpseo_options['ignore_search_engines_discouraged_notice'] == true
+		&& ( $on_wpseo_admin_page || in_array( $pagenow, $notice_pages, true ) )
+	) {
+		// Get site slug & settings url
+		$blog_id    = get_current_blog_id();
+		$site_url   = get_home_url( $blog_id );
+		$site_slug  = wp_parse_url( $site_url, PHP_URL_HOST );
+		$settings_url = 'https://wordpress.com/settings/general/' . $site_slug . '#site-privacy-settings';
+		
+		if ( $is_wpcom_public_coming_soon_enabled ) {
+			$text = __(
+				'Heads up! Your site is currently in Coming Soon mode. If you want search engines to show this site in their search results you have to launch your site. %1$sClick here%2$s to change your site\'s privacy settings.'
+			);
+		} else {
+			$text = __(
+				'Heads up! Search engines are currently discouraged from indexing your site. If you want search engines to show this site in their search results you have to launch your site. %1$sClick here%2$s to change your site\'s privacy settings.'
+			);
+		}
+
+		printf(
+			'<div class="notice notice-info is-dismissible"><p><strong>' . esc_html( $text ) . '</strong></p></div>',
+			'<a href="' . esc_url( $settings_url ) . '">',
+			'</a>'
+		);
+	}
+}
+add_action( 'admin_notices', 'wpcom_public_coming_soon_replace_yoast_seo_notice' );
+
+/**
  * Adds the tracking identity to config that is passed to Starter Page Template frontend.
  * That way we can publish our code to plugin directory sans tracking code.
  *
