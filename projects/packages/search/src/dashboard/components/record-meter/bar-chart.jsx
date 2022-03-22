Automattic/jetpack

commit fe03aa16d9dbc99b3d4c12bef4709ed8aaf625e5

Use accessor function for legend items

projects/packages/search/src/dashboard/components/record-meter/bar-chart.jsx
@@ -71,12 +71,10 @@ export class BarChart extends React.Component {
 				datasets: this.props.data.map( d => d.data ),
 			},
 		} );
+	}
 
-		if ( this.props.isValid === true ) {
-			// TODO: move setState out of componentDidMount
-			// eslint-disable-next-line react/no-did-mount-set-state
-			this.setState( { legendItems: this.myChart.legend.legendItems } );
-		}
+	getLegendItems() {
+		return this.myChart?.legend.legendItems ?? [];
 	}
 
 	render() {
@@ -104,17 +102,15 @@ export class BarChart extends React.Component {
 							<tbody>
 								<tr>
 									<th scope="col">{ __( 'Post type', 'jetpack-search-pkg' ) }</th>
-									{ this.state?.legendItems.length &&
-										this.state.legendItems.map( item => {
-											return <th scope="col">{ item.text }</th>;
-										} ) }
+									{ this.getLegendItems().map( item => {
+										return <th scope="col">{ item.text }</th>;
+									} ) }
 								</tr>
 								<tr>
 									<th scope="row">{ __( 'Record count', 'jetpack-search-pkg' ) }</th>
-									{ this.state?.legendItems.length &&
-										this.state.legendItems.map( item => {
-											return <td>{ this.props.data[ item.datasetIndex ].data.data }</td>;
-										} ) }
+									{ this.getLegendItems().map( item => {
+										return <td>{ this.props.data[ item.datasetIndex ].data.data }</td>;
+									} ) }
 								</tr>
 							</tbody>
 						</table>
@@ -122,23 +118,22 @@ export class BarChart extends React.Component {
 				</div>
 				<div className="jp-search-chart-legend__container">
 					<ul className="jp-search-chart-legend">
-						{ this.state?.legendItems.length > 0 &&
-							this.state.legendItems.map( item => {
-								return (
-									<li key={ item.text }>
-										<div
-											className="jp-search-chart-legend__box"
-											style={ {
-												backgroundColor: item.fillStyle,
-											} }
-										/>
-										<span className="jp-search-chart-legend__label" children={ item.text } />
-										<span className="jp-search-chart-legend__count">
-											({ this.props.data[ item.datasetIndex ].data.data })
-										</span>
-									</li>
-								);
-							} ) }
+						{ this.getLegendItems().map( item => {
+							return (
+								<li key={ item.text }>
+									<div
+										className="jp-search-chart-legend__box"
+										style={ {
+											backgroundColor: item.fillStyle,
+										} }
+									/>
+									<span className="jp-search-chart-legend__label" children={ item.text } />
+									<span className="jp-search-chart-legend__count">
+										({ this.props.data[ item.datasetIndex ].data.data })
+									</span>
+								</li>
+							);
+						} ) }
 					</ul>
 				</div>
 			</div>

commit 5880260f01298515ae4bdf4d994ba7a11c255880

Fix regression introduced by fe03aa1

projects/packages/search/src/dashboard/components/record-meter/bar-chart.jsx
@@ -74,6 +74,9 @@ export class BarChart extends React.Component {
 				datasets: this.props.data.map( d => d.data ),
 			},
 		} );
+
+		// Force re-render after mounting chart to ensure correct value for this.getLegendItems().
+		this.forceUpdate();
 	}
 
 	getLegendItems() {