af0e773b55b24c2b24b3fb64e9f4c3b0079ac049
Adnan Haque <adnan007.id@gmail.com>
2022-03-22 16:50:51 +0600

    Add PricingCard component to benefits interstitial

--- projects/plugins/boost/app/assets/src/js/pages/settings/Settings.svelte
+++ projects/plugins/boost/app/assets/src/js/pages/settings/Settings.svelte
@@ -6,7 +6,7 @@
 	import Score from './sections/Score.svelte';
 	import AdvancedCriticalCss from './sections/AdvancedCriticalCss.svelte';
 	import Modules from './sections/Modules.svelte';
-	import { Router, Route } from 'svelte-navigator';
+	import { Router, Route } from '../../utils/router';
 </script>
 
 <div class="jb-section--alt jb-section--scores">
