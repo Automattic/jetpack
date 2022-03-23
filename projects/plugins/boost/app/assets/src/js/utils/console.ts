648b4abc2c7d241fd1615f35f1c33d1bbde6dc95
Karthik Bhat <karthik.ax.ax@gmail.com>
2022-03-24 03:38:17 +0530

    Update documentation page link

--- projects/plugins/boost/app/assets/src/js/utils/console.ts
+++ projects/plugins/boost/app/assets/src/js/utils/console.ts
@@ -3,10 +3,9 @@
  */
 export function logPreCriticalCSSGeneration(): void {
 	const styles = 'font-size: 1rem;line-height:1.6;';
-	// Todo: Update the documentation page link.
 	// eslint-disable-next-line no-console
 	console.log(
-		'%cGenerating Critical CSS will often leave errors in your console. Most of them are nothing to worry about - just the sounds of your browser complaining about our strict security settings. Check out the following page for more information: https://jetpack.com/support/performance/jetpack-boost/{console-error-docs-page}',
+		'%cGenerating Critical CSS will often leave errors in your console. Most of them are nothing to worry about - just the sounds of your browser complaining about our strict security settings. Check out the following page for more information: https://jetpack.com/support/performance/jetpack-boost/jetpack-boost-console-error/',
 		styles
 	);
 }
