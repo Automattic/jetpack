df903b6945c2acc4f7c4cf88865a96666f373901
retrofox <rdsuarez@gmail.com>
2022-03-23 16:12:39 -0300

    [not verified] jetpack-components: add fonts and colors stories

--- projects/js-packages/components/components/theme-provider/stories/index.jsx
+++ projects/js-packages/components/components/theme-provider/stories/index.jsx
@@ -11,6 +11,42 @@ import Container from '../../layout/container';
 import ThemeProvider, { typography, colors, borders, spacing } from '../index.jsx';
 import styles from './style.module.scss';
 
+/**
+ * Get the contrasting color for any hex color
+ * (c) 2019 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * Derived from work by Brian Suda, https://24ways.org/2010/calculating-color-contrast/
+ *
+ * @param   {string} hexcolor - hexcolor value
+ * @returns {string}            The contrasting color (black or white)
+ */
+function getContrast( hexcolor ) {
+	// If a leading # is provided, remove it
+	if ( hexcolor.slice( 0, 1 ) === '#' ) {
+		hexcolor = hexcolor.slice( 1 );
+	}
+
+	// If a three-character hexcode, make six-character
+	if ( hexcolor.length === 3 ) {
+		hexcolor = hexcolor
+			.split( '' )
+			.map( function ( hex ) {
+				return hex + hex;
+			} )
+			.join( '' );
+	}
+
+	// Convert to RGB value
+	const r = parseInt( hexcolor.substring( 0, 2 ), 16 );
+	const g = parseInt( hexcolor.substring( 2, 4 ), 16 );
+	const b = parseInt( hexcolor.substring( 4, 6 ), 16 );
+
+	// Get YIQ ratio
+	const yiq = ( r * 299 + g * 587 + b * 114 ) / 1000;
+
+	// Check contrast
+	return yiq >= 128 ? 'black' : 'white';
+}
+
 export default {
 	title: 'JS Packages/Components/Theme Provider',
 	decorators: [
@@ -60,4 +96,27 @@ export const Tokens = () => (
 		<Section title="Borders" data={ borders } />
 		<Section title="Spacing" data={ spacing } />
 	</>
+);
+
+export const Typographies = () => (
+	<div className={ styles[ 'font-instances-wrapper' ] }>
+		{ Object.keys( typography ).map( key => (
+			<div className={ styles[ 'font-instance' ] } style={ { fontSize: typography[ key ] } }>
+				{ `${ key } (${ typography[ key ] } )` }
+			</div>
+		) ) }
+	</div>
+);
+
+export const Colors = () => (
+	<div className={ styles[ 'instances-wrapper' ] }>
+		{ Object.keys( colors ).map( key => (
+			<div
+				className={ styles[ 'color-instance' ] }
+				style={ { backgroundColor: colors[ key ], color: getContrast( colors[ key ] ) } }
+			>
+				{ `${ key } (${ colors[ key ].toUpperCase() } )` }
+			</div>
+		) ) }
+	</div>
 );
