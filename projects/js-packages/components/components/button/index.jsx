ce8820eb5585f640b658b8d4b5bf4b02c2811fd3
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2022-03-23 18:56:17 -0300

    Components: Add initial button component

---
+++ projects/js-packages/components/components/button/index.jsx
@@ -0,0 +1,38 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { Button as WPButton } from '@wordpress/components';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import styles from './style.module.scss';
+
+export const BUTTON_SIZES = {
+	NORMAL: 'normal',
+	SMALL: 'small',
+};
+
+const Button = ( { children, variant, size, icon, disabled } ) => {
+	const className = classNames( styles.button, {
+		[ styles.normal ]: size === BUTTON_SIZES.NORMAL,
+		[ styles.small ]: size === BUTTON_SIZES.SMALL,
+		[ styles.icon ]: Boolean( icon ),
+	} );
+
+	return (
+		<WPButton variant={ variant } className={ className } icon={ icon } disabled={ disabled }>
+			{ children }
+		</WPButton>
+	);
+};
+
+Button.propTypes = {
+	variant: PropTypes.oneOf( [ 'primary', 'secondary', 'link' ] ),
+	size: PropTypes.oneOf( [ BUTTON_SIZES.NORMAL, BUTTON_SIZES.SMALL ] ),
+};
+
+Button.defaultProps = {
+	size: BUTTON_SIZES.NORMAL,
+};
+
+export default Button;
---
+++ projects/js-packages/components/components/button/stories/index.jsx
@@ -0,0 +1,73 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import Button from '../index.jsx';
+
+export default {
+	title: 'JS Packages/Components/Button',
+	component: Button,
+	argTypes: {
+		icon: {
+			control: false,
+		},
+	},
+	parameters: {
+		backgrounds: {
+			default: 'Light',
+		},
+	},
+};
+
+const Template = args => <Button { ...args } />;
+
+export const ButtonPrimary = Template.bind( {} );
+ButtonPrimary.args = {
+	size: 'normal',
+	children: 'Jetpack Button',
+	variant: 'primary',
+};
+
+export const ButtonSecondary = Template.bind( {} );
+ButtonSecondary.args = {
+	size: 'normal',
+	children: 'Jetpack Button',
+	variant: 'secondary',
+};
+
+export const ButtonLink = Template.bind( {} );
+ButtonLink.args = {
+	size: 'normal',
+	children: 'Jetpack Button',
+	variant: 'link',
+};
+
+const DownIcon = () => (
+	<svg width="15" height="9" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="10 9 4 7">
+		<path
+			fillRule="evenodd"
+			clipRule="evenodd"
+			d="m18.004 10.555-6.005 5.459-6.004-5.459 1.009-1.11 4.995 4.542 4.996-4.542 1.009 1.11Z"
+		/>
+	</svg>
+);
+
+export const Icon = Template.bind( {} );
+Icon.args = {
+	size: 'normal',
+	children: 'Jetpack Button',
+	icon: <DownIcon />,
+	variant: 'primary',
+};
+
+export const Disabled = Template.bind( {} );
+Disabled.args = {
+	size: 'normal',
+	children: 'Jetpack Button',
+	variant: 'primary',
+	disabled: true,
+};
