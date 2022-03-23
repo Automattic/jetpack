9890b3af12f69c1eba1dcd540021712ae8ab4e9d
Chris R <chris.rosser@automattic.com>
2022-03-23 15:02:25 +0000

    [not verified] Use partial text matching in test

--- projects/packages/search/src/dashboard/components/record-meter/test/record-count.test.jsx
+++ projects/packages/search/src/dashboard/components/record-meter/test/record-count.test.jsx
@@ -15,12 +15,12 @@ import '@testing-library/jest-dom/extend-expect';
 import { RecordCount } from 'components/record-meter/record-count';
 
 describe( 'record count', () => {
-	test( 'outputs correct text', () => {
+	test( 'outputs correct record counts', () => {
 		render( <RecordCount recordCount={ 20 } planRecordLimit={ 100 } /> );
 
-		expect(
-			screen.getByText( '20 records indexed out of the 100 allotted for your current plan' )
-		).toBeInTheDocument();
+		expect( screen.getByText( /20 records indexed/i ) ).toBeInTheDocument();
+
+		expect( screen.getByText( /100 allotted/i ) ).toBeInTheDocument();
 	} );
 
 	test( "doesn't output if there are no records", () => {
