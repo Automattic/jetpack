ef3e6b493931e818e806ba1bc81a9e91694a3384
Jasper Kang <jasper.kang@automattic.com>
2022-03-25 15:42:44 +1300

    sync constants to wpcom

--- projects/plugins/search/src/class-jetpack-search-plugin.php
+++ projects/plugins/search/src/class-jetpack-search-plugin.php
@@ -8,6 +8,7 @@
 namespace Automattic\Jetpack\Search_Plugin;
 
 use Automattic\Jetpack\Config;
+use Automattic\Jetpack\Connection\Rest_Authentication as Connection_Rest_Authentication;
 use Automattic\Jetpack\My_Jetpack\Initializer as My_Jetpack_Initializer;
 
 /**
@@ -16,6 +17,14 @@ use Automattic\Jetpack\My_Jetpack\Initializer as My_Jetpack_Initializer;
  * @package automattic/jetpack-search
  */
 class Jetpack_Search_Plugin {
+	/**
+	 * List of Constants to sync
+	 *
+	 * @var array
+	 */
+	protected static $constants_to_sync = array(
+		'JETPACK_SEARCH_PLUGIN__VERSION',
+	);
 	/**
 	 * Register hooks to initialize the plugin
 	 */
@@ -50,6 +59,25 @@ class Jetpack_Search_Plugin {
 	 * Initialize the plugin
 	 */
 	public static function initialize() {
+		// Set up the REST authentication hooks.
+		Connection_Rest_Authentication::init();
+		// Ininitialize My Jetpack.
 		My_Jetpack_Initializer::init();
+		// Merge a list of constant we want to sync to shadow DB.
+		add_filter( 'jetpack_sync_constants_whitelist', array( self::class, 'sync_constants' ) );
+	}
+
+	/**
+	 * Merge a list of constant we want to sync to shadow DB.
+	 *
+	 * @param array $constants_whitelist Array of constant names.
+	 *
+	 * @return array
+	 */
+	public static function sync_constants( $constants_whitelist ) {
+		if ( is_array( $constants_whitelist ) ) {
+			return array_merge( $constants_whitelist, self::$constants_to_sync );
+		}
+		return $constants_whitelist;
 	}
 }
