Automattic/jetpack

commit 549a2d73505eb312fefa9b56f800abf16f407e5e

cli: Add dependency analysis helper and command (#22433)
This is the existing logic from tools/find-project-deps.php,
tools/get-build-order.php, and .github/files/list-changed-projects.php,
rewritten in JS. It also has a little more flexibility as far as
processing only a subset of projects.

One difference is in the unused composer.json `.extra.dependencies`
array: it will want that to instead be an object holding
`.extra.dependencies.test` and `.extra.dependencies.build`.

Then remove the old scripts in favor of the new command.

.github/files/list-changed-projects.sh
@@ -1,0 +1,35 @@
+#!/bin/bash
+
+set -eo pipefail
+
+cd $(dirname "${BASH_SOURCE[0]}")/../..
+
+. tools/includes/chalk-lite.sh
+
+function debug {
+	blue "$@" >&2
+}
+
+function die {
+	echo "::error::$*" >&2
+	exit 1
+}
+
+ARGS=( --add-dependents )
+
+# Code coverage probably needs to run all tests to avoid confusing metrics.
+# @todo Switch it to use codecov.io's carryforward: https://about.codecov.io/blog/new-product-test-only-what-you-change-with-carryforward-flags/
+if [[ "$TEST_SCRIPT" == "test-coverage" ]]; then
+	debug "TEST_SCRIPT is test-coverage, considering all projects changed."
+elif [[ "${GITHUB_EVENT_NAME:?}" == "pull_request" ]]; then
+	[[ -f "${GITHUB_EVENT_PATH:?}" ]] || die "GITHUB_EVENT_PATH file $GITHUB_EVENT_PATH does not exist"
+	DIFF="$(jq -r '"\( .pull_request.base.sha )..\( .pull_request.head.sha )"' "$GITHUB_EVENT_PATH")"
+	debug "GITHUB_EVENT_NAME is pull_request, checking diff from $DIFF"
+	ARGS+=( --verbose "--git-changed=$DIFF" )
+elif [[ "${GITHUB_EVENT_NAME:?}" == "push" ]]; then
+	debug "GITHUB_EVENT_NAME is pull_request, considering all projects changed."
+else
+	die "Unsupported GITHUB_EVENT_NAME \"$GITHUB_EVENT_NAME\""
+fi
+
+pnpx jetpack dependencies list "${ARGS[@]}" | jq -cR 'reduce inputs as $i ({}; .[$i] |= true)'

.github/workflows/linting.yml
@@ -484,10 +484,10 @@ jobs:
     timeout-minutes: 1  # 2021-01-18: Successful runs take just a few seconds
     steps:
       - uses: actions/checkout@v2
-      - name: Tool versions
-        run: |
-          which jq
-          jq --version
+      - name: Setup tools
+        uses: ./.github/actions/tool-setup
+        with:
+          php: false
       - run: tools/check-intra-monorepo-deps.sh -v
       - run: .github/files/check-monorepo-package-repos.sh
 

tools/check-intra-monorepo-deps.sh
@@ -93,9 +93,8 @@ DO_PNPM_LOCK=true
 SLUGS=()
 if [[ $# -le 0 ]]; then
 	# Use a temp variable so pipefail works
-	TMP="$(tools/get-build-order.php 2>/dev/null)"
+	TMP="$(pnpx jetpack dependencies build-order --pretty)"
 	mapfile -t SLUGS <<<"$TMP"
-	SLUGS+=( 'monorepo' )
 	TMP="$(git ls-files '**/composer.json' '**/package.json' | sed -E -n -e '\!^projects/[^/]*/[^/]*/(composer|package)\.json$! d' -e 's!/(composer|package)\.json$!!' -e 's/^/nonproject:/p' | sort -u)"
 	mapfile -t -O ${#SLUGS[@]} SLUGS <<<"$TMP"
 else

.github/files/build-all-projects.sh
@@ -30,7 +30,7 @@ echo "::group::Changelogger setup"
 echo "::endgroup::"
 
 echo "::group::Determining build order"
-TMP="$(tools/get-build-order.php)"
+TMP="$(pnpx jetpack dependencies build-order --pretty --ignore-root)"
 SLUGS=()
 mapfile -t SLUGS <<<"$TMP"
 echo "::endgroup::"

.github/files/list-changed-projects.php
@@ -1,216 +1,0 @@
-#!/usr/bin/env php
-<?php
-/**
- * Tool to list whether projects have changed.
- *
- * @package automattic/jetpack
- */
-
-// phpcs:disable WordPress.WP.AlternativeFunctions, WordPress.PHP.DiscouragedPHPFunctions, WordPress.Security.EscapeOutput.OutputNotEscaped
-
-ob_start();
-require_once __DIR__ . '/../../tools/find-project-deps.php';
-ob_end_clean();
-
-// Files that mean all tests should be run.
-// phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
-$infrastructure_files = array(
-	'.github/actions/tool-setup/action.yml',
-	'.github/files/list-changed-projects.php',
-	'.github/files/generate-ci-matrix.php',
-	'.github/files/process-coverage.sh',
-	'.github/files/setup-wordpress-env.sh',
-	'.github/versions.sh',
-	'.github/workflows/tests.yml',
-);
-
-// Globs that should be ignored. Supports `*`, `**`, and `?`.
-// phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
-$ignore_globs = array(
-	'**.md',
-	'**.txt',
-);
-
-chdir( __DIR__ . '/../../' );
-
-if ( array_search( '--debug', $argv, true ) !== false ) {
-	/**
-	 * Output debug info.
-	 *
-	 * @param array ...$args Arguments to printf. A newline is automatically appended.
-	 */
-	function debug( ...$args ) {
-		if ( getenv( 'CI' ) ) {
-			$args[0] = "\e[34m${args[0]}\e[0m\n";
-		} else {
-			$args[0] = "\e[1;30m${args[0]}\e[0m\n";
-		}
-		fprintf( STDERR, ...$args );
-	}
-} else {
-	/**
-	 * Do not output debug info.
-	 */
-	function debug() {
-	}
-}
-
-/**
- * Fetch the list of all projects.
- *
- * @return string[]
- */
-function get_all_projects() {
-	static $cache = null;
-
-	if ( null === $cache ) {
-		$cache = array( 'monorepo' );
-		foreach ( glob( 'projects/*/*/composer.json' ) as $file ) {
-			$cache[] = substr( $file, 9, -14 );
-		}
-		ksort( $cache );
-	}
-
-	return $cache;
-}
-
-/**
- * Fetch the list of changed projects.
- *
- * @return string[]
- */
-function get_changed_projects() {
-	global $infrastructure_files, $ignore_globs;
-
-	// Code coverage probably needs to run all tests to avoid confusing metrics.
-	// @todo Determine if that's actually true.
-	if ( 'test-coverage' === getenv( 'TEST_SCRIPT' ) ) {
-		debug( 'TEST_SCRIPT is test-coverage, considering all projects changed.' );
-		return get_all_projects();
-	}
-
-	$event = getenv( 'GITHUB_EVENT_NAME' );
-	if ( 'pull_request' === $event ) {
-		$event_path = getenv( 'GITHUB_EVENT_PATH' );
-		if ( ! $event_path || ! file_exists( $event_path ) ) {
-			fprintf( STDERR, "Missing GITHUB_EVENT_PATH for pull_request event\n" );
-			exit( 1 );
-		}
-		$event = json_decode( file_get_contents( $event_path ) );
-		if ( ! isset( $event->pull_request->base->sha ) || ! isset( $event->pull_request->head->sha ) ) {
-			fprintf( STDERR, "Missing pull_request data in GITHUB_EVENT_PATH file\n" );
-			exit( 1 );
-		}
-		$base = $event->pull_request->base->sha;
-		$head = $event->pull_request->head->sha;
-		debug( 'GITHUB_EVENT_NAME is pull_request, checking diff from %s...%s.', $base, $head );
-	} elseif ( 'push' === $event ) {
-		debug( 'GITHUB_EVENT_NAME is push, considering all projects changed.' );
-		return get_all_projects();
-	} else {
-		fprintf( STDERR, "Unsupported GITHUB_EVENT_NAME \"%s\"\n", $event );
-		exit( 1 );
-	}
-
-	$pipes = null;
-	$p     = proc_open(
-		sprintf( 'git -c core.quotepath=off diff --no-renames --name-only %s...%s', escapeshellarg( $base ), escapeshellarg( $head ) ),
-		array( array( 'pipe', 'r' ), array( 'pipe', 'w' ), STDERR ),
-		$pipes
-	);
-	if ( ! $p ) {
-		exit( 1 );
-	}
-	fclose( $pipes[0] );
-
-	// PHP doesn't seem to have something that'll correctly handle `**`, so munge to a regex.
-	$ignore_regexes = array();
-	foreach ( $ignore_globs as $glob ) {
-		$re = '!^';
-		for ( $i = 0, $l = strlen( $glob ); $i < $l; $i++ ) {
-			$c = $glob[ $i ];
-			switch ( $c ) {
-				case '\\':
-					$re .= preg_quote( $glob[ ++$i ], '!' );
-					break;
-				case '*':
-					if ( '*' === $glob[ $i + 1 ] ) {
-						$i++;
-						$re .= '.*';
-					} else {
-						$re .= '[^/]*';
-					}
-					break;
-				case '?':
-					$re .= '[^/]';
-					break;
-				default:
-					$re .= preg_quote( $c, '!' );
-					break;
-			}
-		}
-		$re                     .= '$!';
-		$ignore_regexes[ $glob ] = $re;
-	}
-
-	$projects = array();
-	// phpcs:ignore WordPress.CodeAnalysis.AssignmentInCondition.FoundInWhileCondition
-	while ( ( $line = fgets( $pipes[1] ) ) ) {
-		$line = trim( $line );
-		if ( in_array( $line, $infrastructure_files, true ) ) {
-			debug( 'PR touches infrastructure file %s, considering all projects changed.', $line );
-			return get_all_projects();
-		}
-		foreach ( $ignore_regexes as $glob => $re ) {
-			if ( preg_match( $re, $line ) ) {
-				debug( 'PR touches file %s, but ignoring due to match with `%s`.', $line, $glob );
-				continue 2;
-			}
-		}
-		$parts = explode( '/', $line, 4 );
-		if ( count( $parts ) === 4 && 'projects' === $parts[0] ) {
-			$slug = "{$parts[1]}/{$parts[2]}";
-		} else {
-			$slug = 'monorepo';
-		}
-		if ( empty( $projects[ $slug ] ) ) {
-			debug( 'PR touches file %s, marking %s as changed.', $line, $slug );
-			$projects[ $slug ] = true;
-		}
-	}
-	fclose( $pipes[1] );
-
-	$status = proc_close( $p );
-	if ( $status ) {
-		exit( $status );
-	}
-
-	ksort( $projects );
-	return array_keys( $projects );
-}
-
-// Get a list of projects indicating which are changed.
-$projects = array_fill_keys( get_changed_projects(), true ) + array_fill_keys( get_all_projects(), false );
-
-// Figure out if any projects depend on a changed project. Repeat to propagate until none are found.
-$deps = get_dependencies();
-do {
-	$any = false;
-	foreach ( $projects as $project => $changed ) {
-		if ( $changed || ! isset( $deps[ $project ] ) ) {
-			continue;
-		}
-		foreach ( $deps[ $project ] as $slug ) {
-			if ( ! empty( $projects[ $slug ] ) ) {
-				debug( 'Project %s depends on %s, marking it as changed.', $project, $slug );
-				$projects[ $project ] = true;
-				$any                  = true;
-				break;
-			}
-		}
-	}
-} while ( $any );
-
-// Output.
-ksort( $projects );
-echo json_encode( $projects, JSON_FORCE_OBJECT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE ) . "\n";

.github/workflows/e2e-tests-boost.yml
@@ -12,18 +12,21 @@ jobs:
   check:
     name: "Check if Boost E2E tests should run"
     runs-on: ubuntu-latest
-    timeout-minutes: 1  # 2021-11-12: Should only take a second.
+    timeout-minutes: 3  # 2022-01-20: Takes a little time now for the tools and pnpm install.
     outputs:
       boost: ${{ steps.check.outputs.boost }}
     steps:
       - uses: actions/checkout@v2
-      # For pull requests, list-changed-projects.php needs the merge base.
+      # For pull requests, list-changed-projects.sh needs the merge base.
       - name: Deepen to merge base
         if: github.event_name == 'pull_request'
         uses: ./.github/actions/deepen-to-merge-base
+      - name: Setup tools
+        uses: ./.github/actions/tool-setup
       - id: check
         run: |
-          CHANGED="$(.github/files/list-changed-projects.php --debug)"
+          pnpm install
+          CHANGED="$(.github/files/list-changed-projects.sh)"
           BOOST="$(jq '.["plugins/boost"]' <<<"$CHANGED")"
           echo "::set-output name=boost::${BOOST}"
 

.github/workflows/tests.yml
@@ -56,7 +56,7 @@ jobs:
           # codecov.io requires a fetch depth > 1.
           fetch-depth: 2
 
-      # For pull requests, list-changed-projects.php needs the merge base.
+      # For pull requests, list-changed-projects.sh needs the merge base.
       # But it doesn't have to be checked out, and codecov.io requires it not be.
       - name: Deepen to merge base
         if: github.event_name == 'pull_request'
@@ -71,10 +71,16 @@ jobs:
           coverage: ${{ matrix.script == 'test-coverage' && 'pcov' || 'none' }}
           node: ${{ matrix.node }}
 
+      - name: Monorepo install
+        run: |
+          echo "::group::Pnpm"
+          pnpm install
+          echo "::endgroup::"
+
       - name: Detect changed projects
         id: changed
         run: |
-          CHANGED="$(.github/files/list-changed-projects.php --debug)"
+          CHANGED="$(.github/files/list-changed-projects.sh)"
           ANY_PLUGINS="$(jq --argjson changed "$CHANGED" -n '$changed | with_entries( select( .key | startswith( "plugins/" ) ) ) | any')"
           echo "::set-output name=projects::${CHANGED}"
           echo "::set-output name=any-plugins::${ANY_PLUGINS}"
@@ -82,12 +88,6 @@ jobs:
       - name: Setup WordPress environment for plugin tests
         if: steps.changed.outputs.any-plugins == 'true' && matrix.wp != 'none'
         run: .github/files/setup-wordpress-env.sh
-
-      - name: Monorepo install
-        run: |
-          echo "::group::Pnpm"
-          pnpm install
-          echo "::endgroup::"
 
       - name: Run project tests
         env:

pnpm-lock.yaml
@@ -463,7 +463,7 @@ importers:
     specifiers:
       '@automattic/babel-plugin-preserve-i18n': 1.0.0
       '@automattic/babel-plugin-replace-textdomain': workspace:^1.0.2
-      '@automattic/i18n-check-webpack-plugin': workspace:^1.0.3
+      '@automattic/i18n-check-webpack-plugin': workspace:^1.0.4-alpha
       '@automattic/i18n-loader-webpack-plugin': workspace:^2.0.0
       '@automattic/webpack-rtl-plugin': 5.1.0
       '@babel/compat-data': 7.16.4
@@ -523,7 +523,7 @@ importers:
 
   projects/packages/assets:
     specifiers:
-      '@automattic/jetpack-webpack-config': workspace:^1.1.0
+      '@automattic/jetpack-webpack-config': workspace:^1.1.1-alpha
       jest: 27.3.1
       md5-es: 1.8.2
       webpack: 5.65.0
@@ -537,7 +537,7 @@ importers:
     specifiers:
       '@automattic/jetpack-api': workspace:^0.8.3
       '@automattic/jetpack-connection': workspace:^0.14.1-alpha
-      '@automattic/jetpack-webpack-config': workspace:^1.1.0
+      '@automattic/jetpack-webpack-config': workspace:^1.1.1-alpha
       '@babel/core': 7.16.0
       '@babel/preset-env': 7.16.4
       '@babel/register': 7.16.0
@@ -577,7 +577,7 @@ importers:
   projects/packages/identity-crisis:
     specifiers:
       '@automattic/jetpack-idc': workspace:^0.9.0
-      '@automattic/jetpack-webpack-config': workspace:^1.1.0
+      '@automattic/jetpack-webpack-config': workspace:^1.1.1-alpha
       '@babel/core': 7.16.0
       '@babel/preset-env': 7.16.4
       '@babel/register': 7.16.0
@@ -609,7 +609,7 @@ importers:
 
   projects/packages/jitm:
     specifiers:
-      '@automattic/jetpack-webpack-config': workspace:^1.1.0
+      '@automattic/jetpack-webpack-config': workspace:^1.1.1-alpha
       '@wordpress/browserslist-config': 4.1.0
       sass: 1.43.3
       sass-loader: 12.4.0
@@ -625,7 +625,7 @@ importers:
 
   projects/packages/lazy-images:
     specifiers:
-      '@automattic/jetpack-webpack-config': workspace:^1.1.0
+      '@automattic/jetpack-webpack-config': workspace:^1.1.1-alpha
       copy-webpack-plugin: 10.2.0
       intersection-observer: 0.12.0
       webpack: 5.65.0
@@ -641,7 +641,7 @@ importers:
       '@automattic/jetpack-base-styles': workspace:^0.1.7
       '@automattic/jetpack-components': workspace:^0.10.3
       '@automattic/jetpack-connection': workspace:^0.14.1-alpha
-      '@automattic/jetpack-webpack-config': workspace:^1.1.0
+      '@automattic/jetpack-webpack-config': workspace:^1.1.1-alpha
       '@babel/core': 7.16.0
       '@babel/preset-env': 7.16.4
       '@babel/register': 7.16.0
@@ -707,7 +707,7 @@ importers:
       '@automattic/jetpack-analytics': workspace:^0.1.7
       '@automattic/jetpack-api': workspace:^0.8.3
       '@automattic/jetpack-components': workspace:^0.10.3
-      '@automattic/jetpack-webpack-config': workspace:^1.1.0
+      '@automattic/jetpack-webpack-config': workspace:^1.1.1-alpha
       '@babel/core': 7.16.0
       '@babel/plugin-proposal-nullish-coalescing-operator': 7.16.0
       '@babel/preset-env': 7.16.4
@@ -805,7 +805,7 @@ importers:
       '@automattic/jetpack-base-styles': workspace:^0.1.7
       '@automattic/jetpack-components': workspace:^0.10.3
       '@automattic/jetpack-connection': workspace:^0.14.1-alpha
-      '@automattic/jetpack-webpack-config': workspace:^1.1.0
+      '@automattic/jetpack-webpack-config': workspace:^1.1.1-alpha
       '@babel/core': 7.16.0
       '@babel/preset-env': 7.16.4
       '@babel/register': 7.16.0
@@ -930,7 +930,7 @@ importers:
       '@automattic/jetpack-connection': workspace:^0.14.1-alpha
       '@automattic/jetpack-licensing': workspace:^0.4.4
       '@automattic/jetpack-partner-coupon': workspace:^0.1.7-alpha
-      '@automattic/jetpack-webpack-config': workspace:^1.1.0
+      '@automattic/jetpack-webpack-config': workspace:^1.1.1-alpha
       '@automattic/popup-monitor': 1.0.0
       '@automattic/remove-asset-webpack-plugin': workspace:^1.0.3
       '@automattic/request-external-access': 1.0.0
@@ -1230,6 +1230,7 @@ importers:
       envfile: 6.17.0
       execa: 5.0.0
       glob: 7.1.6
+      ignore: 5.1.8
       inquirer: 7.3.3
       js-yaml: 4.1.0
       listr: 0.14.3
@@ -1252,6 +1253,7 @@ importers:
       envfile: 6.17.0
       execa: 5.0.0
       glob: 7.1.6
+      ignore: 5.1.8
       inquirer: 7.3.3
       js-yaml: 4.1.0
       listr: 0.14.3
@@ -14733,7 +14735,6 @@ packages:
   /ignore/5.1.8:
     resolution: {integrity: sha512-BMpfD7PpiETpBl/A6S498BaIJ6Y/ABT93ETbby2fP00v4EbvPBXWEoaR1UBPKs3iR53pJY7EtZk5KACI57i1Uw==}
     engines: {node: '>= 4'}
-    dev: true
 
   /ignore/5.2.0:
     resolution: {integrity: sha512-CmxgYGiEPCLhfLnpPp1MoRmifwEIOgjcHXxOBjv7mY96c+eWScsOP9c112ZyLdWHi0FxHjI+4uVhKYp/gcdRmQ==}

commit 0efbbed61f54395577b82282b7e9722b4edd9f7b

linting: Fix monorepo version refs lint
It seems likely to have been broken since #22433 added a call to the
Jetpack CLI without there being a `pnpm install` first. Add that to the
action.

Also update the script to detect this sort of error and handle it
better, instead of trying to lint a project with the improbably name of
"? Install the following package: jetpack@latest? (Y/n) ‣ ", and pass
the `--no-install` flag to `pnpx` in a few more places for good measure.

.github/files/lint-project-structure.sh
@@ -301,13 +301,13 @@ fi
 # - package.json engines should be satisfied by .github/versions.sh.
 debug "Checking .github/versions.sh vs package.json engines"
 RANGE="$(jq -r '.engines.node' package.json)"
-if ! pnpx semver --range "$RANGE" "$NODE_VERSION" &>/dev/null; then
+if ! pnpx --no-install semver --range "$RANGE" "$NODE_VERSION" &>/dev/null; then
 	EXIT=1
 	LINE=$(jq --stream 'if length == 1 then .[0][:-1] else .[0] end | if . == ["engines","node"] then input_line_number - 1 else empty end' package.json)
 	echo "::error file=package.json,line=$LINE::Node version $NODE_VERSION in .github/versions.sh does not satisfy requirement $RANGE from package.json"
 fi
 RANGE="$(jq -r '.engines.pnpm' package.json)"
-if ! pnpx semver --range "$RANGE" "$PNPM_VERSION" &>/dev/null; then
+if ! pnpx --no-install semver --range "$RANGE" "$PNPM_VERSION" &>/dev/null; then
 	EXIT=1
 	LINE=$(jq --stream 'if length == 1 then .[0][:-1] else .[0] end | if . == ["engines","pnpm"] then input_line_number - 1 else empty end' package.json)
 	echo "::error file=package.json,line=$LINE::Pnpm version $PNPM_VERSION in .github/versions.sh does not satisfy requirement $RANGE from package.json"

.github/files/list-changed-projects.sh
@@ -39,4 +39,4 @@ if [[ -n "$EXTRA" ]]; then
 	fi
 fi
 
-pnpx jetpack dependencies list "${ARGS[@]}" | jq -ncR 'reduce inputs as $i ({}; .[$i] |= true)'
+pnpx --no-install jetpack dependencies list "${ARGS[@]}" | jq -ncR 'reduce inputs as $i ({}; .[$i] |= true)'

.github/workflows/linting.yml
@@ -463,13 +463,14 @@ jobs:
     runs-on: ubuntu-latest
     needs: changed_files
     if: needs.changed_files.outputs.lockfiles == 'true' || needs.changed_files.outputs.misc == 'true'
-    timeout-minutes: 1  # 2021-01-18: Successful runs take just a few seconds
+    timeout-minutes: 5  # 2022-03-25: The pnpm install will probably take a minute or so.
     steps:
       - uses: actions/checkout@v3
       - name: Setup tools
         uses: ./.github/actions/tool-setup
         with:
           php: false
+      - run: pnpm install
       - run: tools/check-intra-monorepo-deps.sh -v
       - run: .github/files/check-monorepo-package-repos.sh
 

tools/check-intra-monorepo-deps.sh
@@ -98,7 +98,7 @@ DO_PNPM_LOCK=true
 SLUGS=()
 if [[ $# -le 0 ]]; then
 	# Use a temp variable so pipefail works
-	TMP="$(pnpx jetpack dependencies build-order --pretty)"
+	TMP="$(pnpx --no-install jetpack dependencies build-order --pretty)" || { echo "$TMP"; exit 1; }
 	mapfile -t SLUGS <<<"$TMP"
 	TMP="$(git ls-files '**/composer.json' '**/package.json' | sed -E -n -e '\!^projects/[^/]*/[^/]*/(composer|package)\.json$! d' -e 's!/(composer|package)\.json$!!' -e 's/^/nonproject:/p' | sort -u)"
 	mapfile -t -O ${#SLUGS[@]} SLUGS <<<"$TMP"
@@ -130,9 +130,9 @@ if $UPDATE; then
 			ARGS+=( "--type=$CLTYPE" )
 		fi
 
-                if [[ -n "$CL_FILENAME" ]]; then
-                    ARGS+=( --filename="$CL_FILENAME" )
-                fi
+		if [[ -n "$CL_FILENAME" ]]; then
+			ARGS+=( --filename="$CL_FILENAME" )
+		fi
 		if $AUTO_SUFFIX; then
 			ARGS+=( --filename-auto-suffix )
 		fi
@@ -184,6 +184,15 @@ for SLUG in "${SLUGS[@]}"; do
 		fi
 		DOCL=$DOCL_EVER
 		DIR="projects/$SLUG"
+	fi
+	if [[ ! -d "$DIR" ]]; then
+		EXIT=1
+		if [[ -n "$CI" ]]; then
+			echo "::error::Cannot check $SLUG, as $DIR does not exist."
+		else
+			error "Cannot check $SLUG, as $DIR does not exist."
+		fi
+		continue
 	fi
 	PHPFILE="$DIR/composer.json"
 	JSFILE="$DIR/package.json"

tools/version-packages.sh
@@ -104,7 +104,7 @@ EXIT=0
 while IFS=$'\t' read -r PKG OLDVER NEWVER; do
 	OV=$(sed -e 's/\.x-dev$/.0-alpha/' <<<"$OLDVER")
 	NV=$(sed -e 's/^\^//' -e 's/\.x-dev$/.0-alpha/' <<<"$NEWVER")
-	if ! pnpx semver -c -r ">$OV" "$NV" >/dev/null; then
+	if ! pnpx --no-install semver -c -r ">$OV" "$NV" >/dev/null; then
 		EXIT=1
 		error "$PKG was not upgraded ($NEWVER <= $OLDVER)"
 	fi