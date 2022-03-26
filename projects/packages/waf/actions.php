6cca94375ae601da68008177164bf0ffbfeab0f3
Jared <jwidavid72@gmail.com>
2022-03-25 21:27:01 -0400

    Actions now create and/or update the rules.php file when Jetpack is activated or the WAF has a new version

--- projects/packages/waf/actions.php
+++ projects/packages/waf/actions.php
@@ -12,14 +12,56 @@ if ( ! function_exists( 'add_action' ) ) {
 	return;
 }
 
+define( 'JETPACK_WAF_VERSION', '1.0.3' );
+define( 'JETPACK_WAF_DIR', __DIR__ );
+const RULES_FILE = JETPACK_WAF_DIR . '/rules/rules.php';
+
+/**
+ * Triggers when the Jetpack plugin is activated
+ */
+register_activation_hook(
+	JETPACK__PLUGIN_FILE,
+	// Another way array( __NAMESPACE__ . '\Waf', 'activate' ).
+	function () {
+		global $wp_filesystem;
+
+		if ( ! function_exists( '\\WP_Filesystem' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/file.php';
+		}
+
+		if ( ! \WP_Filesystem() ) {
+			throw new \Exception( 'No filesystem available' );
+		}
+
+		return $wp_filesystem->put_contents( RULES_FILE, '<?php' );
+	}
+);
+
 /**
  * Runs the WAF in the WP context.
  *
  * @return void
  */
 add_action(
-	'plugin_loaded',
+	'admin_init',
 	function () {
+		$version = get_option( 'jetpack_waf_version' );
+		if ( ! $version ) {
+			add_option( 'jetpack_waf_version', JETPACK_WAF_VERSION );
+		}
+		if ( JETPACK_WAF_VERSION !== $version ) {
+			global $wp_filesystem;
+
+			if ( ! function_exists( '\\WP_Filesystem' ) ) {
+				require_once ABSPATH . 'wp-admin/includes/file.php';
+			}
+
+			if ( ! \WP_Filesystem() ) {
+				throw new \Exception( 'No filesystem available' );
+			}
+
+			$wp_filesystem->put_contents( RULES_FILE, '<?php ' . JETPACK_WAF_VERSION );
+		}
 		require_once __DIR__ . '/run.php';
 	}
 );
