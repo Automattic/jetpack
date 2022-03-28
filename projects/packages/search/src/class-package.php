8f78d584060786ec6a308dc76b7636b3be0cc43d
Jasper Kang <jasper.kang@automattic.com>
2022-03-29 01:37:31 +1300

    Search: Add connection screen for search dashboard (#23477)

--- projects/packages/search/src/class-package.php
+++ projects/packages/search/src/class-package.php
@@ -11,7 +11,7 @@ namespace Automattic\Jetpack\Search;
  * Search package general information
  */
 class Package {
-	const VERSION = '0.11.4-alpha';
+	const VERSION = '0.12.0-alpha';
 	const SLUG    = 'search';
 
 	/**
