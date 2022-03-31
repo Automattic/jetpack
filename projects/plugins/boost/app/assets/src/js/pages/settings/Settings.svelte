33c75235e9ea30a7d02ffb20c680580d948fb003
Adnan Haque <adnan007.id@gmail.com>
2022-03-31 14:07:26 +0600

    Add support section

--- projects/plugins/boost/app/assets/src/js/pages/settings/Settings.svelte
+++ projects/plugins/boost/app/assets/src/js/pages/settings/Settings.svelte
@@ -7,9 +7,10 @@
 	import AdvancedCriticalCss from './sections/AdvancedCriticalCss.svelte';
 	import Modules from './sections/Modules.svelte';
 	import { Router, Route } from '../../utils/router';
+	import Support from './sections/Support.svelte';
 </script>
 
-<div class="jb-section--alt jb-section--scores">
+<div class="jb-section jb-section--alt jb-section--scores">
 	<Score />
 </div>
 
@@ -20,6 +21,10 @@
 	</div>
 </Router>
 
-<div class="jb-section--alt">
+<div class="jb-section jb-section--alt">
 	<Tips />
+</div>
+
+<div class="jb-section">
+	<Support />
 </div>
---
+++ projects/plugins/boost/app/assets/src/js/pages/settings/sections/Support.svelte
@@ -0,0 +1,43 @@
+<script>
+	/**
+	 * Internal dependencies
+	 */
+	import ReactComponent from '../../../elements/ReactComponent.svelte';
+
+	/**
+	 * WordPress dependencies
+	 */
+	import { __ } from '@wordpress/i18n';
+	import { Button } from '@wordpress/components';
+
+	const openSupport = () => {
+		const supportUrl = 'https://jetpackme.wordpress.com/contact-support/';
+
+		window.open( supportUrl, '_blank' );
+	};
+</script>
+
+<div class="jb-container--narrow">
+	<div class="jb-support">
+		<div class="jb-support__content">
+			<h3 class="jb-support__title">
+				{__( "We're here to help", 'jetpack-boost' )}
+			</h3>
+			<p class="jb-support__description">
+				{__(
+					'Your paid plan gives you access to prioritized Jetpack Boost support',
+					'jetpack-boost'
+				)}
+			</p>
+		</div>
+		<div class="jb-support__cta">
+			<ReactComponent
+				this={Button}
+				label={__( 'Continue', 'jetpack-boost' )}
+				onClick={openSupport}
+				className="jb-button jb-button--outline"
+				children={__( 'Contact Us', 'jetpack-boost' )}
+			/>
+		</div>
+	</div>
+</div>
