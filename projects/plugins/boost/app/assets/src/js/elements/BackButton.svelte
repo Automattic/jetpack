5c53e8466d007699f758211f6efefb0dee35aa58
Adnan Haque <eAdnan007@users.noreply.github.com>
2022-04-01 17:22:40 +0600

    Boost: Add post purchase page (#23617)
    * Add svelt-navigator
    
    * Use svelte-navigator package
    
    * Add changelog
    
    * Add content and style for Benefits interstitial
    
    * Fix rollup warning
    
    * [not verified] Add PricingCard component in benefits interstitial
    
    This commit is a bit messy as it is actually a squashed merge of a newer branch.
    
    https://github.com/Automattic/jetpack/pull/23537
    
    * Revert "[not verified] Add PricingCard component in benefits interstitial"
    
    This reverts commit 38f889993068472b04014484c0d399ea1d0ae37c.
    
    * Add PricingCard component to benefits interstitial
    
    * Remove unused dependencies
    
    * Add styles to boost benefits interstitial
    
    * Improve spacing in upgrade page
    
    * Imlement purchase success page and improve upgrade page
    
    * Improve upgrade and post-purchase page
    
    * Improve post-purchase navigation
    
    * Replace boost.svg with boost.png for purchase success page
    
    * changelog
    
    * Improve whitespacing
    
    * Remove unused variable
    
    * Fix asset path
    
    * Take router props in PurchaseSuccess.svelte
    
    * Update lock-file
    
    * Add links to pricing card info text

--- projects/plugins/boost/app/assets/src/js/elements/BackButton.svelte
+++ projects/plugins/boost/app/assets/src/js/elements/BackButton.svelte
@@ -15,11 +15,13 @@
 	import { __ } from '@wordpress/i18n';
 
 	const { navigate } = routerHistory;
+
+	export let route = -1;
 </script>
 
 <button
 	class="components-button components-button--back is-link close"
-	on:click={() => navigate( -1 )}
+	on:click={() => navigate( route )}
 >
 	<LeftArrow />
 	{__( 'Go back', 'jetpack-boost' )}
