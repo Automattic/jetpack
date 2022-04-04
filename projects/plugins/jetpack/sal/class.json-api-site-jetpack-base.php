21d40b9ed0c9e6770c07ec6b9973f0587d93113d
Karen Attfield <karenlattfield@gmail.com>
2022-04-04 12:27:42 +0100

    [not verified] class.json-api-site-jetpack-base.php: adding comments where needed

--- projects/plugins/jetpack/sal/class.json-api-site-jetpack-base.php
+++ projects/plugins/jetpack/sal/class.json-api-site-jetpack-base.php
@@ -1,29 +1,117 @@
-<?php
+<?php  // phpcs:ignore WordPress.Files.FileName.InvalidClassFileName
+/**
+ * This class extends the SAL_Site class, providing the implementation for
+ * functions that were declared in that SAL_Site class as well as defining
+ * base functions to be implemented in class Jetpack_Site.
+ *
+ * @see class.json-api-site-jetpack.php for more context on
+ * the functions extended here.
+ *
+ * @package automattic/jetpack
+ */
 require_once dirname( __FILE__ ) . '/class.json-api-site-base.php';
 
+/**
+ * Base class for Abstract_Jetpack_Site.
+ */
 abstract class Abstract_Jetpack_Site extends SAL_Site {
+
+	/**
+	 * Defining a base get_constant() function to be extended in the Jetpack_Site class.
+	 *
+	 * If a Jetpack constant name has been defined, this will return the value of the constant.
+	 *
+	 * @param string $name the name of the Jetpack constant to check.
+	 */
 	abstract protected function get_constant( $name );
 
+	/**
+	 * Defining a base current_theme_supports() function to be extended in the Jetpack_Site class.
+	 *
+	 * Returns true if the current theme supports the $feature_name, false otherwise.
+	 *
+	 * @param string $feature_name the name of the Jetpack feature.
+	 */
 	abstract protected function current_theme_supports( $feature_name );
 
+	/**
+	 * Defining a base get_theme_support() function to be extended in the Jetpack_Site class.
+	 *
+	 * Gets theme support arguments to be checked against the specific Jetpack feature.
+	 *
+	 * @param string $feature_name the name of the Jetpack feature to check against.
+	 */
 	abstract protected function get_theme_support( $feature_name );
 
+	/**
+	 * Defining a base get_mock_option() function to be extended in the Jetpack_Site class.
+	 *
+	 * Retrieves a Jetpack option's value, given the option name.
+	 *
+	 * @param string $name the name of the Jetpack option, without the 'jetpack' prefix (eg. 'log' for 'jetpack_log').
+	 */
 	abstract protected function get_mock_option( $name );
 
+	/**
+	 * Defining a base get_jetpack_version() function to be extended in the Jetpack_Site class.
+	 *
+	 * Returns the current Jetpack version number.
+	 */
 	abstract public function get_jetpack_version();
 
+	/**
+	 * Defining a base get_updates() function to be extended in the Jetpack_Site class.
+	 *
+	 * Gets updates and then stores them in the jetpack_updates option, returning an array with the option schema.
+	 */
 	abstract public function get_updates();
 
+	/**
+	 * Defining a base main_network_site() function to be extended in the Jetpack_Site class.
+	 *
+	 * Returns the site URL for the current network.
+	 */
 	abstract protected function main_network_site();
 
+	/**
+	 * Defining a base wp_version() function to be extended in the Jetpack_Site class.
+	 *
+	 * Returns the WordPress version for the current site.
+	 */
 	abstract protected function wp_version();
 
+	/**
+	 * Defining a base max_upload_size() function to be extended in the Jetpack_Site class.
+	 *
+	 * Returns the maximum upload size allowed in php.ini.
+	 */
 	abstract protected function max_upload_size();
 
+	/**
+	 * Defining a base is_main_network() function to be extended in the Jetpack_Site class.
+	 *
+	 * Returns true if the site is within a system with a multiple networks, false otherwise.
+	 *
+	 * @see /projects/packages/status/src/class-status.php.
+	 */
 	abstract protected function is_main_network();
 
+	/**
+	 * Defining a base is_version_controlled() function to be extended in the Jetpack_Site class.
+	 *
+	 * Returns true if is_vcs_checkout discovers a version control checkout, false otherwise.
+	 *
+	 * @see projects/packages/sync/src/class-functions.php.
+	 */
 	abstract protected function is_version_controlled();
 
+	/**
+	 * Defining a base file_system_write_access() function to be extended in the Jetpack_Site class.
+	 *
+	 * Returns true if the site has file write access false otherwise.
+	 *
+	 * @see projects/packages/sync/src/class-functions.php.
+	 */
 	abstract protected function file_system_write_access();
 
 	/**
@@ -31,26 +119,46 @@ abstract class Abstract_Jetpack_Site extends SAL_Site {
 	 */
 	abstract protected function get_connection_active_plugins();
 
+	/**
+	 * This function is implemented on WPCom sites, where a filter is removed which forces the URL to http.
+	 *
+	 * @see /wpcom/public.api/rest/sal/class.json-api-site-jetpack-shadow.php.
+	 */
 	function before_render() {
 	}
 
+	/**
+	 * This function returns the value of the 'WP_MEMORY_LIMIT' constant.
+	 *
+	 * @return int|string
+	 */
 	protected function wp_memory_limit() {
 		return $this->get_constant( 'WP_MEMORY_LIMIT' );
 	}
 
+	/**
+	 * This function returns the value of the 'WP_MAX_MEMORY_LIMIT' constant.
+	 *
+	 * @return int|string
+	 */
 	protected function wp_max_memory_limit() {
 		return $this->get_constant( 'WP_MAX_MEMORY_LIMIT' );
 	}
 
-
+	/**
+	 * If a user has manage options permissions and the site is the main site of the network, make updates visible.
+	 *
+	 * Called after response_keys have been rendered, which itself is used to return all the necessary information for a site’s response.
+	 *
+	 * @param array $response an array of the response keys.
+	 */
 	function after_render( &$response ) {
-		// Add the updates only make them visible if the user has manage options permission and the site is the main site of the network
 		if ( current_user_can( 'manage_options' ) && $this->is_main_site( $response ) ) {
 			$jetpack_update = $this->get_updates();
 			if ( ! empty( $jetpack_update ) ) {
-				// In previous version of Jetpack 3.4, 3.5, 3.6 we synced the wp_version into to jetpack_updates
+				// In previous version of Jetpack 3.4, 3.5, 3.6 we synced the wp_version into to jetpack_updates.
 				unset( $jetpack_update['wp_version'] );
-				// In previous version of Jetpack 3.4, 3.5, 3.6 we synced the site_is_version_controlled into to jetpack_updates
+				// In previous version of Jetpack 3.4, 3.5, 3.6 we synced the site_is_version_controlled into to jetpack_updates.
 				unset( $jetpack_update['site_is_version_controlled'] );
 
 				$response['updates'] = $jetpack_update;
@@ -58,8 +166,12 @@ abstract class Abstract_Jetpack_Site extends SAL_Site {
 		}
 	}
 
+	/**
+	 * Extends the Jetpack options array with details including site constraints, WordPress and Jetpack versions, and plugins using the Jetpack connection.
+	 *
+	 * @param array $options an array of the Jetpack options.
+	 */
 	function after_render_options( &$options ) {
-
 		$options['jetpack_version'] = $this->get_jetpack_version();
 
 		if ( $main_network_site = $this->main_network_site() ) {
@@ -76,7 +188,7 @@ abstract class Abstract_Jetpack_Site extends SAL_Site {
 		$options['wp_max_memory_limit']  = $this->wp_max_memory_limit();
 
 		// Sites have to prove that they are not main_network site.
-		// If the sync happends right then we should be able to see that we are not dealing with a network site
+		// If the sync happends right then we should be able to see that we are not dealing with a network site.
 		$options['is_multi_network'] = (bool) $this->is_main_network();
 		$options['is_multi_site']    = (bool) $this->is_multisite();
 
@@ -95,24 +207,51 @@ abstract class Abstract_Jetpack_Site extends SAL_Site {
 		$options['jetpack_connection_active_plugins'] = $this->get_connection_active_plugins();
 	}
 
+	/**
+	 * This function returns the values of any active Jetpack modules.
+	 *
+	 * @return array
+	 */
 	function get_jetpack_modules() {
 		return array_values( Jetpack_Options::get_option( 'active_modules', array() ) );
 	}
 
+	/**
+	 * This function returns true if a specified Jetpack module is active, false otherwise.
+	 *
+	 * @param string $module The Jetpack module name to check.
+	 *
+	 * @return bool
+	 */
 	function is_module_active( $module ) {
 		return in_array ( $module, Jetpack_Options::get_option( 'active_modules', array() ), true );
 	}
 
+	/**
+	 * This function returns false for a check as to whether a site is a VIP site or not.
+	 *
+	 * @return bool Always returns false.
+	 */
 	function is_vip() {
-		return false; // this may change for VIP Go sites, which sync using Jetpack
+		return false; // this may change for VIP Go sites, which sync using Jetpack.
 	}
 
+	/**
+	 * If the site's current theme supports post thumbnails, return true (otherwise return false).
+	 *
+	 * @return bool
+	 */
 	function featured_images_enabled() {
 		return $this->current_theme_supports( 'post-thumbnails' );
 	}
 
+	/**
+	 * Returns an array of supported post formats.
+	 *
+	 * @return array
+	 */
 	function get_post_formats() {
-		// deprecated - see separate endpoint. get a list of supported post formats
+		// deprecated - see separate endpoint. get a list of supported post formats.
 		$all_formats = get_post_format_strings();
 		$supported   = $this->get_theme_support( 'post-formats' );
 
@@ -127,6 +266,11 @@ abstract class Abstract_Jetpack_Site extends SAL_Site {
 		return $supported_formats;
 	}
 
+	/**
+	 * Returns an array with site icon details.
+	 *
+	 * @return array
+	 */
 	function get_icon() {
 		$icon_id = get_option( 'site_icon' );
 		if ( empty( $icon_id ) ) {
@@ -157,6 +301,13 @@ abstract class Abstract_Jetpack_Site extends SAL_Site {
 	 * Private methods
 	 **/
 
+	/**
+	 * This function returns true if the current site is the main network site, false otherwise.
+	 *
+	 * @param array $response The array of Jetpack response keys.
+	 *
+	 * @return bool
+	 */
 	private function is_main_site( $response ) {
 		if ( isset( $response['options']->main_network_site, $response['options']->unmapped_url ) ) {
 			$main_network_site_url = set_url_scheme( $response['options']->main_network_site, 'http' );
@@ -169,7 +320,15 @@ abstract class Abstract_Jetpack_Site extends SAL_Site {
 		return false;
 	}
 
-	// For Jetpack sites this will always return false
+	/**
+	 * For Jetpack sites this will always return false.
+	 *
+	 * This is extended for WordPress.com sites in wpcom/public.api/rest/sal/trait.json-api-site-wpcom.php.
+	 *
+	 * @param int $post_id The post id.
+	 *
+	 * @return bool
+	 */
 	protected function is_a8c_publication( $post_id ) {
 		return false;
 	}
