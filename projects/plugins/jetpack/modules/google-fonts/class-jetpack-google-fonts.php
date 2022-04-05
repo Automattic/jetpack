279c27579e6d8c1b2ac4863ef61a465e5cff8f09
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2022-04-05 18:07:06 -0500

    Instropect blocks and enqueue used Google fonts

--- projects/plugins/jetpack/modules/google-fonts/class-jetpack-google-fonts.php
+++ projects/plugins/jetpack/modules/google-fonts/class-jetpack-google-fonts.php
@@ -10,6 +10,14 @@
  */
 class Jetpack_Google_Fonts {
 	/**
+	 * The Google fonts that we're registered after
+	 * running the `jetpack_google_fonts_list`.
+	 *
+	 * @var array
+	 */
+	private $registered_google_fonts = array();
+
+	/**
 	 * Hook into WordPress to register
 	 * and enqueue webfonts.
 	 */
@@ -17,6 +25,14 @@ class Jetpack_Google_Fonts {
 		$this->register_google_fonts_provider();
 
 		add_action( 'after_setup_theme', array( $this, 'register_google_fonts' ) );
+
+		/**
+		 * We are already enqueueing all registered fonts by default when loading the block editor,
+		 * so we only need to scan for webfonts when browsing as a guest.
+		 */
+		if ( ! is_admin() ) {
+			add_filter( 'pre_render_block', array( $this, 'scan_block_for_google_fonts' ), 10, 2 );
+		}
 	}
 
 	/**
@@ -68,6 +84,46 @@ class Jetpack_Google_Fonts {
 					),
 				)
 			);
+
+			$slug = wp_webfonts()->get_font_slug( $font_family );
+
+			/**
+			 * When introspecting, all we have is the slug,
+			 * so we need to keep track of the registered families
+			 * to check whether the font was filtered after
+			 * running the `jetpack_google_fonts_list` hook.
+			 */
+			$this->registered_google_fonts[ $slug ] = $font_family;
 		}
+	}
+
+	/**
+	 * Scan block for Google fonts.
+	 *
+	 * @param string $content The block content.
+	 * @param array  $parsed_block The parsed block attributes.
+	 *
+	 * @return string The block content.
+	 */
+	public function scan_block_for_google_fonts( $content, $parsed_block ) {
+		if ( isset( $parsed_block['attrs']['fontFamily'] ) ) {
+			$this->maybe_enqueue_font_family( $parsed_block['attrs']['fontFamily'] );
+		}
+
+		return $content;
+	}
+
+	/**
+	 * Enqueue a font family if it was not removed from the curated list.
+	 *
+	 * @param string $font_family_slug The block content.
+	 */
+	private function maybe_enqueue_font_family( $font_family_slug ) {
+		if ( ! isset( $this->registered_google_fonts[ $font_family_slug ] ) ) {
+			// Font not allow-listed.
+			return;
+		}
+
+		wp_enqueue_webfont( $font_family_slug );
 	}
 }
