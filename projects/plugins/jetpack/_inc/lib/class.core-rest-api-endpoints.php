8dd610b835bcf703bf69ca6faacc8a34de9a9c33
Kevin Zoschke <kevin.zoschke@automattic.com>
2022-04-05 14:00:21 -0400

    Remove unnecessary args

--- projects/plugins/jetpack/_inc/lib/class.core-rest-api-endpoints.php
+++ projects/plugins/jetpack/_inc/lib/class.core-rest-api-endpoints.php
@@ -2027,9 +2027,7 @@ class Jetpack_Core_Json_Api_Endpoints {
 				'headers' => array(
 					'X-Forwarded-For' => ( new Visitor() )->get_ip( true ),
 				),
-			),
-			null,
-			'wpcom'
+			)
 		);
 
 		$response_code = wp_remote_retrieve_response_code( $response );
@@ -2085,9 +2083,7 @@ class Jetpack_Core_Json_Api_Endpoints {
 				'headers' => array(
 					'X-Forwarded-For' => ( new Visitor() )->get_ip( true ),
 				),
-			),
-			null,
-			'wpcom'
+			)
 		);
 
 		$response_code = wp_remote_retrieve_response_code( $response );
