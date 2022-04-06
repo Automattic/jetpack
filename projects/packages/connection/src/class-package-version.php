ff2c7f1adf15061e6af50cb088e0430d861307c9
Karen Attfield <karenlattfield@gmail.com>
2022-04-06 14:41:27 +0100

    10.9-a.1 changelog

--- projects/packages/connection/src/class-package-version.php
+++ projects/packages/connection/src/class-package-version.php
@@ -12,7 +12,7 @@ namespace Automattic\Jetpack\Connection;
  */
 class Package_Version {
 
-	const PACKAGE_VERSION = '1.37.5-alpha';
+	const PACKAGE_VERSION = '1.37.5';
 
 	const PACKAGE_SLUG = 'connection';
 
