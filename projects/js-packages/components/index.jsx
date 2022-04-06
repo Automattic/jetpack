2b218224b60df524347a492476205cd5ba458c76
retrofox <rdsuarez@gmail.com>
2022-04-06 13:14:26 -0300

    [not verified] expose ProductPrice component

--- projects/js-packages/components/index.jsx
+++ projects/js-packages/components/index.jsx
@@ -34,4 +34,5 @@ export { default as Text, H2, H3, Title } from './components/text';
 export { default as numberFormat } from './components/number-format';
 export { default as QRCode } from './components/qr-code';
 export { default as Button } from './components/button';
+export { default as ProductPrice } from './components/product-price';
 export { getUserLocale, cleanLocale } from './lib/locale';
