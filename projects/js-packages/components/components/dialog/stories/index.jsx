0e769cf1207a5c9125c57d2cbe58b8161f6599d1
retrofox <rdsuarez@gmail.com>
2022-04-06 19:09:04 -0300

    [not verified] js-components: add Dialog component

---
+++ projects/js-packages/components/components/dialog/stories/index.jsx
@@ -0,0 +1,45 @@
+/* eslint-disable react/react-in-jsx-scope */
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import ProductDetailCard from '../../product-offer';
+import Dialog from '../';
+import BoostImage from '../boost.png';
+
+export default {
+	title: 'JS Packages/Components/Dialog',
+	component: Dialog,
+	parameters: {
+		actions: { argTypesRegex: '^on.*' },
+		layout: 'centered',
+	},
+};
+
+const Template = args => <Dialog { ...args } />;
+
+export const InterstitialJetpackBoost = Template.bind( {} );
+InterstitialJetpackBoost.parameters = {};
+InterstitialJetpackBoost.args = {
+	primary: (
+		<ProductDetailCard
+			slug="boost"
+			name="Boost"
+			title="Jepack Boost"
+			description="Jetpack Boost gives your site the same performance advantages as the world’s leading websites, no developer required."
+			features={ [
+				'Check your site performance',
+				'Enable improvements in one click',
+				'Standalone free plugin for those focused on speed',
+			] }
+			pricing={ {
+				isFree: true,
+			} }
+		/>
+	),
+	secondary: <img src={ BoostImage } alt="Boost" />,
+};
