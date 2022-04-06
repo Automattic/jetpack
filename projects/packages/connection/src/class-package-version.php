0fceaff4d1b8937a84d667e21025c053ba8a0664
jboland88 <18016357+jboland88@users.noreply.github.com>
2022-04-06 11:29:17 -0400

    Add conditional recommendations to the Jetpack assistant (#23362)
    * [not verified] Initial commit, add conditional recommendation for publicize
    
    * [not verified] Changelogs and version management
    
    * [not verified] Fix composer lock deps/ ref numbers
    
    * [not verified] recgonize and label new recommendations when they show for the first time
    
    * [not verified] Update composer lock files
    
    * [not verified] Update logic for showing new recommendations
    
    * [not verified] Add isset check when checking viewed recommendations
    
    * [not verified] Fix JS unit tests
    
    * [not verified] Address feedback, add new image for recommendation
    
    * [not verified] Update package versions
    
    * [not verified] Renamed image asset
    
    * [not verified] Stagger updating recommendations data on step render
    
    * [not verified] Version management
    
    * [not verified] Copy updates
    
    * [not verified] Fix bug when checking for new recommendations if there is more than one enabled
    
    * [not verified] Fix unit tests
    
    * [not verified] Update comments
    
    * [not verified] Remove erroneous line from json file, version management
    
    * Update pnpm-lock, version management
    
    * Small adjustments to markup and comments
    
    * Version management
    
    * Add recommendation for VideoPress (#23678)
    
    * [not verified] Initial commit for videopress recommendation
    
    * Update CTA link on summary screen
    
    * Add changelog file
    
    * Version management
    
    Co-authored-by: Brandon Kraft <public@brandonkraft.com>

--- projects/packages/connection/src/class-package-version.php
+++ projects/packages/connection/src/class-package-version.php
@@ -12,7 +12,7 @@ namespace Automattic\Jetpack\Connection;
  */
 class Package_Version {
 
-	const PACKAGE_VERSION = '1.37.5';
+	const PACKAGE_VERSION = '1.37.6-alpha';
 
 	const PACKAGE_SLUG = 'connection';
 
--- projects/packages/identity-crisis/src/class-identity-crisis.php
+++ projects/packages/identity-crisis/src/class-identity-crisis.php
@@ -28,7 +28,7 @@ class Identity_Crisis {
 	/**
 	 * Package Version
 	 */
-	const PACKAGE_VERSION = '0.8.5';
+	const PACKAGE_VERSION = '0.8.6-alpha';
 
 	/**
 	 * Instance of the object.
