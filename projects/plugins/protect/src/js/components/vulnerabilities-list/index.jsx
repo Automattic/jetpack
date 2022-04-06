279d7377aac66602584c4e676f645fc02ee7fce0
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2022-04-06 13:30:50 -0300

    Protect: Remove `severity` from `VulnerabilitiesList` and include `Fixed in` (#23822)

--- projects/plugins/protect/src/js/components/vulnerabilities-list/index.jsx
+++ projects/plugins/protect/src/js/components/vulnerabilities-list/index.jsx
@@ -2,52 +2,36 @@
  * External dependencies
  */
 import React from 'react';
+import { __, sprintf } from '@wordpress/i18n';
 import { Container, Col, Text, H3 } from '@automattic/jetpack-components';
-import classNames from 'classnames';
 
 /**
  * Internal dependencies
  */
 import styles from './styles.module.scss';
 
-const RISKS_LEVELS = {
-	LOW: 'low',
-	MEDIUM: 'medium',
-	HIGH: 'high',
-};
-
-const RISKS_LABELS = {
-	[ RISKS_LEVELS.LOW ]: 'Low',
-	[ RISKS_LEVELS.MEDIUM ]: 'Medium',
-	[ RISKS_LEVELS.HIGH ]: 'High',
-};
-
-const VulnerabilityRisk = ( { risk } ) => {
-	const className = classNames( styles.risk, {
-		[ styles.low ]: risk === RISKS_LEVELS.LOW,
-		[ styles.medium ]: risk === RISKS_LEVELS.MEDIUM,
-		[ styles.high ]: risk === RISKS_LEVELS.HIGH,
-	} );
-
-	return (
-		<Text component="div" className={ className }>
-			{ RISKS_LABELS[ risk ] }
-		</Text>
-	);
-};
-
 const VulnerabilityItem = ( { name, version, vulnerabilities } ) => {
 	return (
 		<Container fluid className={ styles.item }>
 			<Col lg={ 4 } className={ styles.name }>
 				<Text variant="title-small">{ name }</Text>
-				<Text variant="body-small">Version { version }</Text>
+				<Text variant="body-small">
+					{
+						/* translators: placeholder is version. */
+						sprintf( __( 'Version %s', 'jetpack-protect' ), version )
+					}
+				</Text>
 			</Col>
 			<Col lg={ 8 }>
 				{ vulnerabilities.map( vulnerability => (
 					<div className={ styles.vulnerability }>
-						<VulnerabilityRisk risk={ vulnerability.risk } />
 						<Text>{ vulnerability.description }</Text>
+						<Text variant="body-extra-small">
+							{
+								/* translators: placeholder is version. */
+								sprintf( __( 'Fixed in %s', 'jetpack-protect' ), vulnerability.fixedIn )
+							}
+						</Text>
 					</div>
 				) ) }
 			</Col>
