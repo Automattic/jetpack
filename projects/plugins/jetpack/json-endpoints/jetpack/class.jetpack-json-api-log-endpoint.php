1efd3d1febbc4419242274e6fe920f0d5a3c2306
sdixon194 <steve.dixon@automattic.com>
2022-04-07 16:24:36 -0400

    json-epi-log-endpoint.php

--- projects/plugins/jetpack/json-endpoints/jetpack/class.jetpack-json-api-log-endpoint.php
+++ projects/plugins/jetpack/json-endpoints/jetpack/class.jetpack-json-api-log-endpoint.php
@@ -1,16 +1,28 @@
-<?php
+<?php // phpcs:ignore WordPress.Files.FileName.InvalidClassFileName
 
+/**
+ * Jetpack log endpoint class.
+ *
+ * GET /sites/%s/jetpack-log
+ */
 class Jetpack_JSON_API_Jetpack_Log_Endpoint extends Jetpack_JSON_API_Endpoint {
-	// GET /sites/%s/jetpack-log
+	/**
+	 * Needed capabilities.
+	 *
+	 * @var array
+	 */
 	protected $needed_capabilities = 'manage_options';
 
+	/**
+	 * The result.
+	 */
 	protected function result() {
-		$args = $this->input();
-		$event = ( isset( $args['event'] ) && is_string( $args['event'] ) ) ? $code : false;
-		$num  = ( isset( $args['num'] ) ) ? (int) $num : false;
+		$args  = $this->input();
+		$event = ( isset( $args['event'] ) && is_string( $args['event'] ) ) ? $code : false; // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable
+		$num   = ( isset( $args['num'] ) ) ? (int) $num : false;
 
 		return array(
-			'log' => Jetpack::get_log( $event, $num )
+			'log' => Jetpack::get_log( $event, $num ),
 		);
 	}
 }
