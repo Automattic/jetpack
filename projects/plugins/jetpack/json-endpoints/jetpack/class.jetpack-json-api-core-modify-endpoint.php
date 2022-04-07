77d989920fca96581961f7545fc918250aac711f
sdixon194 <steve.dixon@automattic.com>
2022-04-07 12:59:56 -0400

    core-modify-endpoint.php

--- projects/plugins/jetpack/json-endpoints/jetpack/class.jetpack-json-api-core-modify-endpoint.php
+++ projects/plugins/jetpack/json-endpoints/jetpack/class.jetpack-json-api-core-modify-endpoint.php
@@ -1,13 +1,44 @@
-<?php
+<?php // phpcs:ignore WordPress.Files.FileName.InvalidClassFileName
 
+/**
+ * Core modify endpoint class.
+ *
+ * POST /sites/%s/core
+ * POST /sites/%s/core/update
+ */
 class Jetpack_JSON_API_Core_Modify_Endpoint extends Jetpack_JSON_API_Core_Endpoint {
-	// POST /sites/%s/core
-	// POST /sites/%s/core/update
+
+	/**
+	 * Needed capabilities.
+	 *
+	 * @var string
+	 */
 	protected $needed_capabilities = 'update_core';
-	protected $action              = 'default_action';
+
+	/**
+	 * Action.
+	 *
+	 * @var string
+	 */
+	protected $action = 'default_action';
+
+	/**
+	 * New version.
+	 *
+	 * @var string
+	 */
 	protected $new_version;
+
+	/**
+	 *  An array of log strings.
+	 *
+	 * @var array
+	 */
 	protected $log;
 
+	/**
+	 * The default action.
+	 */
 	public function default_action() {
 		$args = $this->input();
 
@@ -18,10 +49,13 @@ class Jetpack_JSON_API_Core_Modify_Endpoint extends Jetpack_JSON_API_Core_Endpoi
 		return true;
 	}
 
+	/**
+	 * Update the version.
+	 */
 	protected function update() {
-		$args = $this->input();
-		$version    = isset( $args['version'] ) ? $args['version'] : false;
-		$locale     = isset( $args['locale'] ) ? $args['locale'] : get_locale();
+		$args    = $this->input();
+		$version = isset( $args['version'] ) ? $args['version'] : false;
+		$locale  = isset( $args['locale'] ) ? $args['locale'] : get_locale();
 
 		include_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
 
@@ -36,12 +70,12 @@ class Jetpack_JSON_API_Core_Modify_Endpoint extends Jetpack_JSON_API_Core_Endpoi
 
 		/**
 		 * Pre-upgrade action
-		 * 
+		 *
 		 * @since 3.9.3
-		 * 
+		 *
 		 * @param object|array $update as returned by find_core_update() or find_core_auto_update()
 		 */
-		do_action('jetpack_pre_core_upgrade', $update);
+		do_action( 'jetpack_pre_core_upgrade', $update );
 
 		$skin     = new Automatic_Upgrader_Skin();
 		$upgrader = new Core_Upgrader( $skin );
@@ -57,17 +91,21 @@ class Jetpack_JSON_API_Core_Modify_Endpoint extends Jetpack_JSON_API_Core_Endpoi
 		return $this->new_version;
 	}
 
+	/**
+	 * Select the latest update.
+	 * Remove filters to bypass automattic updates.
+	 */
 	protected function find_latest_update_offer() {
 		// Select the latest update.
 		// Remove filters to bypass automattic updates.
-		add_filter( 'request_filesystem_credentials',      '__return_true'  );
-		add_filter( 'automatic_updates_is_vcs_checkout',   '__return_false' );
-		add_filter( 'allow_major_auto_core_updates',       '__return_true'  );
+		add_filter( 'request_filesystem_credentials', '__return_true' );
+		add_filter( 'automatic_updates_is_vcs_checkout', '__return_false' );
+		add_filter( 'allow_major_auto_core_updates', '__return_true' );
 		add_filter( 'send_core_update_notification_email', '__return_false' );
 		$update = find_core_auto_update();
-		remove_filter( 'request_filesystem_credentials',      '__return_true'  );
-		remove_filter( 'automatic_updates_is_vcs_checkout',   '__return_false' );
-		remove_filter( 'allow_major_auto_core_updates',       '__return_true'  );
+		remove_filter( 'request_filesystem_credentials', '__return_true' );
+		remove_filter( 'automatic_updates_is_vcs_checkout', '__return_false' );
+		remove_filter( 'allow_major_auto_core_updates', '__return_true' );
 		remove_filter( 'send_core_update_notification_email', '__return_false' );
 		return $update;
 	}
