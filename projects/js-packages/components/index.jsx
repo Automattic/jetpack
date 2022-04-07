758ba81801962f7b8dd56a93574a3f919ec78a07
retrofox <rdsuarez@gmail.com>
2022-04-07 03:30:26 -0300

    expose Dialog component

--- projects/js-packages/components/index.jsx
+++ projects/js-packages/components/index.jsx
@@ -35,5 +35,6 @@ export { default as numberFormat } from './components/number-format';
 export { default as QRCode } from './components/qr-code';
 export { default as Button } from './components/button';
 export { default as ProductPrice } from './components/product-price';
+export { default as Dialog } from './components/dialog';
 export { default as ProductDetail } from './components/product-detail';
 export { getUserLocale, cleanLocale } from './lib/locale';
