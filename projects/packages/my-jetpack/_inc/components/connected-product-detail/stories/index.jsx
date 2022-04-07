b1f72ceaad959a04c474965ef8ebe2d83362f3af
retrofox <rdsuarez@gmail.com>
2022-04-07 15:07:05 -0300

    [not verified] my-jetpack: introduce ConnectedProductDetail cpm

---
+++ projects/packages/my-jetpack/_inc/components/connected-product-detail/stories/index.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/react-in-jsx-scope */
+/**
+ * External dependencies
+ */
+import React from 'react';
+import withMock from 'storybook-addon-mock';
+
+/**
+ * Internal dependencies
+ */
+import ProductDetailCard from '../index.jsx';
+import { getAllMockData, getProductSlugs } from './utils.js';
+
+export default {
+	title: 'Packages/My Jetpack/Connected Product Detail',
+	component: ProductDetailCard,
+	decorators: [ withMock ],
+	parameters: {
+		layout: 'centered',
+	},
+	argTypes: {
+		slug: {
+			control: { type: 'select', options: getProductSlugs( true ) },
+		},
+		isCard: {
+			control: { type: 'boolean' },
+		},
+	},
+};
+
+const mockData = getAllMockData();
+
+const DefaultProductDetailCard = args => <ProductDetailCard { ...args } />;
+
+export const Default = DefaultProductDetailCard.bind( {} );
+Default.parameters = { mockData };
+Default.args = {
+	slug: 'backup',
+	isCard: false,
+};
