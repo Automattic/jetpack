1de043bc25f2c4f5f55dab0f4350514d3ecc7585
Damián Suárez <rdsuarez@gmail.com>
2022-04-08 09:36:28 -0300

    JS Components: Add ProductOffer component (#23842)
    * [not verified] js-components. Add initial ProductDetail implementation
    
    * [not verified] js-components: render icons for bundle product
    
    * [not verified] js-components: add title and description to Product detail card
    
    * [not verified] js-components: add Product features
    
    * [not verified] js-coponents: minor code improvement
    
    * [not verified] show Product price section
    
    * [not verified] add Free section
    
    * [not verified] add has-required-plan-state
    
    * [not verified] js-component: finish Product Detail card component
    
    * [not verified] js-components: change hasRequiredPlan prop in story
    
    * [not verified] make product icons component powerful
    
    * [not verified] change story name
    
    * [not verified] tweak the disabled stae of the add button
    
    * [not verified] add addProductUrl prop
    
    * [not verified] set name prop as nor required
    
    * [not verified] define properly icons when list is empty
    
    * [not verified] move to product-detail
    
    * [not verified] add isCard property to ProductDetail
    
    * [not verified] add Jetpack Backup story
    
    * js-components: fix prices type story issue
    
    * changelog
    
    * set header height based on line-height and padding
    
    * rename to ProductOffer
    
    * fix exposing ProductOffer component

--- projects/js-packages/components/index.jsx
+++ projects/js-packages/components/index.jsx
@@ -35,4 +35,5 @@ export { default as numberFormat } from './components/number-format';
 export { default as QRCode } from './components/qr-code';
 export { default as Button } from './components/button';
 export { default as ProductPrice } from './components/product-price';
+export { default as ProductOffer } from './components/product-offer';
 export { getUserLocale, cleanLocale } from './lib/locale';
