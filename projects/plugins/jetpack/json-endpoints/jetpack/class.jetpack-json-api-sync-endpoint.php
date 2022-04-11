09ed4bcf2d0f737119c3accfec4423af93fd8b3d
Steve D <33553323+sdixon194@users.noreply.github.com>
2022-04-11 13:02:18 -0400

    Update projects/plugins/jetpack/json-endpoints/jetpack/class.jetpack-json-api-sync-endpoint.php
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>

--- projects/plugins/jetpack/json-endpoints/jetpack/class.jetpack-json-api-sync-endpoint.php
+++ projects/plugins/jetpack/json-endpoints/jetpack/class.jetpack-json-api-sync-endpoint.php
@@ -474,7 +474,7 @@ class Jetpack_JSON_API_Sync_Close_Endpoint extends Jetpack_JSON_API_Sync_Endpoin
 		}
 
 		// Limit to A-Z,a-z,0-9,_,-
-		$request_body ['buffer_id'] = preg_replace( '/[^A-Za-z0-9]/', '', $request_body['buffer_id'] );
+		$request_body['buffer_id'] = preg_replace( '/[^A-Za-z0-9]/', '', $request_body['buffer_id'] );
 		$request_body['item_ids']   = array_filter( array_map( array( 'Jetpack_JSON_API_Sync_Close_Endpoint', 'sanitize_item_ids' ), $request_body['item_ids'] ) );
 
 		$queue = new Queue( $queue_name );
