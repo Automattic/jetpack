633fe98d778b89c2944513cdfd79749d27a03e2d
spsiddarthan <siddarthan@outlook.com>
2022-04-11 15:05:11 +0530

    Move ensure call into the loop/

--- projects/plugins/jetpack/class.jetpack.php
+++ projects/plugins/jetpack/class.jetpack.php
@@ -824,6 +824,7 @@ class Jetpack {
 			array(
 				'sync',
 				'jitm',
+				'publicize',
 			)
 			as $feature
 		) {
@@ -849,7 +850,6 @@ class Jetpack {
 
 		$config->ensure( 'search' );
 		$config->ensure( 'wordads' );
-		$config->ensure( 'publicize' );
 
 		if ( ! $this->connection_manager ) {
 			$this->connection_manager = new Connection_Manager( 'jetpack' );
