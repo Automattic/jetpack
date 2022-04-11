2532ffcd2283c20e15100f820ee86c41f426bb7c
Steve D <33553323+sdixon194@users.noreply.github.com>
2022-04-11 15:14:56 -0400

    Update projects/plugins/jetpack/json-endpoints/jetpack/class.jetpack-json-api-cron-endpoint.php
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>

--- projects/plugins/jetpack/json-endpoints/jetpack/class.jetpack-json-api-cron-endpoint.php
+++ projects/plugins/jetpack/json-endpoints/jetpack/class.jetpack-json-api-cron-endpoint.php
@@ -291,7 +291,7 @@ class Jetpack_JSON_API_Cron_Unschedule_Endpoint extends Jetpack_JSON_API_Cron_En
 	/**
 	 * The result.
 	 *
-	 * @return array
+	 * @return array|WP_Error
 	 */
 	protected function result() {
 		$args = $this->input();
