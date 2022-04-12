cc2aa9b51198d1f16d3013b79c82e4c6d28b9f49
retrofox <rdsuarez@gmail.com>
2022-04-12 10:40:36 -0300

    [not verified] js/connection: Add connected-button story

---
+++ projects/js-packages/connection/components/connect-button/stories/index.jsx
@@ -0,0 +1,23 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import ConnectedButton from '../index.jsx';
+
+export default {
+	title: 'JS Packages/Connection/Connected Button',
+	component: ConnectedButton,
+};
+
+const Template = args => <ConnectedButton { ...args } />;
+
+export const Default = Template.bind( {} );
+Default.args = {
+	size: 'normal',
+	children: 'Jetpack Button',
+	variant: 'primary',
+};
