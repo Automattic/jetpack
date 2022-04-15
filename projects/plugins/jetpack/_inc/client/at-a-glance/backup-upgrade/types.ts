2994324cf2c8db03a3e75719ec3176264a8cc33d
Manzoor Wani <manzoorwani.jk@gmail.com>
2022-04-15 13:55:08 +0530

    [not verified] Revert "[not verified] Convert BackupUpgrade and BarChart to typescript"
    This reverts commit a895001796c378553b5817ae73cc7fc185ff3fc7.

--- projects/plugins/jetpack/_inc/client/at-a-glance/backup-upgrade/types.ts
+++
@@ -1,8 +0,0 @@
-export interface BackupUpgradeBaseProps {
-	comments: number;
-	plugins: number;
-	posts: number;
-}
-export interface BarChartProps extends BackupUpgradeBaseProps {
-	onClosePopup: () => void;
-}
