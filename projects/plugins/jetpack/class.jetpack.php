ab26309b1ec9b7667819c70c8cbdfdd619aff4d6
leogermani <leogermani@gmail.com>
2022-04-15 10:17:04 -0300

    Do not hard disconnect Jetpack on deactivation (#23957)
    * Do not hard disconnect Jetpack on deactivation
    
    Co-authored-by: Samiff <samiff@users.noreply.github.com>

--- projects/plugins/jetpack/class.jetpack.php
+++ projects/plugins/jetpack/class.jetpack.php
@@ -2973,7 +2973,10 @@ p {
 
 		// If the site is in an IDC because sync is not allowed,
 		// let's make sure to not disconnect the production site.
-		$connection->disconnect_site( ! Identity_Crisis::validate_sync_error_idc_option() );
+		if ( ! Identity_Crisis::validate_sync_error_idc_option() ) {
+			$connection->disconnect_site_wpcom();
+		}
+		$connection->delete_all_connection_tokens();
 	}
 
 	/**
