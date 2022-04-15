61e94775c3bd0195329a654f76895f0e2c906939
Leo Germani <leogermani@automattic.com>
2022-04-15 15:19:26 -0300

    add get themes to synced callables in Protect

--- projects/plugins/protect/src/class-jetpack-protect.php
+++ projects/plugins/protect/src/class-jetpack-protect.php
@@ -65,6 +65,7 @@ class Jetpack_Protect {
 						),
 						'jetpack_sync_callable_whitelist'  => array(
 							'get_plugins' => array( 'Automattic\\Jetpack\\Sync\\Functions', 'get_plugins' ),
+							'get_themes'  => array( 'Automattic\\Jetpack\\Sync\\Functions', 'get_themes' ),
 							'wp_version'  => array( 'Automattic\\Jetpack\\Sync\\Functions', 'wp_version' ),
 						),
 						'jetpack_sync_options_contentless' => array(),
