11264f092cb5399318a16a2c9422be16daeaa4e8
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2022-04-15 12:08:25 -0700

    fix and test apiFetch middleware assuming file property in all request bodies

---
+++ projects/plugins/jetpack/modules/videopress/js/test/.eslintrc.js
@@ -0,0 +1,22 @@
+/**
+ * @type {import("eslint").Linter.Config}
+ */
+module.exports = {
+	extends: [ '../../../../../../../tools/js-tools/eslintrc/base.js' ],
+	parserOptions: {
+		babelOptions: {
+			configFile: require.resolve( '../../../../babel.config.js' ),
+		},
+	},
+	/*
+	rules: {
+		// Enforce use of the correct textdomain.
+		'@wordpress/i18n-text-domain': [
+			'error',
+			{
+				allowedTextDomain: 'jetpack',
+			},
+		],
+	},
+	*/
+};
