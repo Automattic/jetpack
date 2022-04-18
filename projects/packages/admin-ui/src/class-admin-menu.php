6e5c93ab7391c47ba6e2a69684ad7538e676f82c
Brad Jorsch <brad.jorsch@automattic.com>
2022-04-18 17:22:31 -0400

    Fix most remaining errors

--- projects/packages/admin-ui/src/class-admin-menu.php
+++ projects/packages/admin-ui/src/class-admin-menu.php
@@ -13,7 +13,7 @@ namespace Automattic\Jetpack\Admin_UI;
  */
 class Admin_Menu {
 
-	const PACKAGE_VERSION = '0.2.6';
+	const PACKAGE_VERSION = '0.2.7-alpha';
 
 	/**
 	 * Whether this class has been initialized
