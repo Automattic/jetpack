fbeafeaffddf9a81278f9e8c4ab630bf6491d8c5
Brad Jorsch <brad.jorsch@automattic.com>
2022-04-21 17:27:57 -0400

    jetpack: Remove create-react-class and react-pure-render deps
    These are back-compat packages for code from before React 16. Let's fix
    the uses to use modern syntax instead.
    
    This also deletes mixins/url-search. Modern React doesn't use mixins,
    and this was only used in one place so it's easier to just inline it
    rather than converting it into a wrapper component or something.

--- projects/plugins/jetpack/_inc/client/components/section-nav/item.jsx
+++ projects/plugins/jetpack/_inc/client/components/section-nav/item.jsx
@@ -6,9 +6,7 @@
 import PropTypes from 'prop-types';
 
 import React from 'react';
-import PureRenderMixin from 'react-pure-render/mixin';
 import classNames from 'classnames';
-import createReactClass from 'create-react-class';
 
 /**
  * Internal Dependencies
@@ -18,11 +16,10 @@ import Count from 'components/count';
 /**
  * Main
  */
-const NavItem = createReactClass( {
-	displayName: 'NavItem',
-	mixins: [ PureRenderMixin ],
+class NavItem extends React.PureComponent {
+	static displayName = 'NavItem';
 
-	propTypes: {
+	static propTypes = {
 		itemType: PropTypes.string,
 		path: PropTypes.string,
 		selected: PropTypes.bool,
@@ -31,9 +28,9 @@ const NavItem = createReactClass( {
 		isExternalLink: PropTypes.bool,
 		disabled: PropTypes.bool,
 		count: PropTypes.number,
-	},
+	};
 
-	render: function () {
+	render() {
 		const itemClassPrefix = this.props.itemType ? this.props.itemType : 'tab';
 		const itemClasses = {
 			'is-selected': this.props.selected,
@@ -70,7 +67,7 @@ const NavItem = createReactClass( {
 				</a>
 			</li>
 		);
-	},
-} );
+	}
+}
 
 export default NavItem;
