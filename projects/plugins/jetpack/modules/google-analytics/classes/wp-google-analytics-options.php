3feae76c70699463c7b19cc924b95f74a687db4e
Samiff <samiff@users.noreply.github.com>
2022-04-21 16:05:43 -0600

    [not verified] wp-google-analytics-options.php: doc comments

--- projects/plugins/jetpack/modules/google-analytics/classes/wp-google-analytics-options.php
+++ projects/plugins/jetpack/modules/google-analytics/classes/wp-google-analytics-options.php
@@ -1,5 +1,4 @@
-<?php
-
+<?php // phpcs:ignore WordPress.Files.FileName.InvalidClassFileNames
 /**
  * Jetpack_Google_Analytics_Options provides a single interface to module options
  *
@@ -13,58 +12,98 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Jetpack_Google_Analytics_Options main class.
+ */
 class Jetpack_Google_Analytics_Options {
+	/**
+	 * Get the Google Analytics tracking ID.
+	 *
+	 * @param string $option_name Nested 'jetpack_wga' option value to retrieve.
+	 * @param mixed  $default Default value if $option is not set.
+	 */
 	public static function get_option( $option_name, $default = false ) {
 		$o = get_option( 'jetpack_wga' );
 		return isset( $o[ $option_name ] ) ? $o[ $option_name ] : $default;
 	}
 
+	/**
+	 * Get the analytics tracking code.
+	 */
 	public static function get_tracking_code() {
 		return self::get_option( 'code', '' );
 	}
 
+	/**
+	 * Check if the tracking code is set.
+	 *
+	 * @return bool
+	 */
 	public static function has_tracking_code() {
 		$code = self::get_tracking_code();
 		return ! empty( $code );
 	}
 
-	// Options used by both legacy and universal analytics
+	/**
+	 * Get the 'anonymize_ip' option used by both legacy and universal analytics.
+	 */
 	public static function anonymize_ip_is_enabled() {
 		return self::get_option( 'anonymize_ip' );
 	}
 
-	// eCommerce options used by both legacy and universal analytics
+	/**
+	 * Get the 'ec_track_purchases' eCommerce option used by both legacy and universal analytics
+	 */
 	public static function track_purchases_is_enabled() {
 		return self::get_option( 'ec_track_purchases' );
 	}
 
+	/**
+	 * Get the 'ec_track_add_to_cart' analytics option.
+	 */
 	public static function track_add_to_cart_is_enabled() {
 		return self::get_option( 'ec_track_add_to_cart' );
 	}
 
-	// Enhanced eCommerce options
+	/**
+	 * Get the 'enh_ec_tracking' analytics option.
+	 */
 	public static function enhanced_ecommerce_tracking_is_enabled() {
 		return self::get_option( 'enh_ec_tracking' );
 	}
 
+	/**
+	 * Get the 'enh_ec_track_remove_from_cart' analytics option.
+	 */
 	public static function track_remove_from_cart_is_enabled() {
 		return self::get_option( 'enh_ec_track_remove_from_cart' );
 	}
 
+	/**
+	 * Get the 'enh_ec_track_prod_impression' analytics option.
+	 */
 	public static function track_product_impressions_is_enabled() {
 		return self::get_option( 'enh_ec_track_prod_impression' );
 	}
 
+	/**
+	 * Get the 'enh_ec_track_prod_click' analytics option.
+	 */
 	public static function track_product_clicks_is_enabled() {
 		return self::get_option( 'enh_ec_track_prod_click' );
 	}
 
+	/**
+	 * Get the 'enh_ec_track_prod_detail_view' analytics option.
+	 */
 	public static function track_product_detail_view_is_enabled() {
 		return self::get_option( 'enh_ec_track_prod_detail_view' );
 	}
 
+	/**
+	 * Get the 'enh_ec_track_checkout_started' analytics option.
+	 */
 	public static function track_checkout_started_is_enabled() {
 		return self::get_option( 'enh_ec_track_checkout_started' );
 	}
 }
-
