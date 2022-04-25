c8a29ac3ee0bbe2c63459a43dca654ba6bc7da95
Samiff <samiff@users.noreply.github.com>
2022-04-25 11:18:43 -0600

    Update projects/packages/changelogger/src/Utils.php
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>

--- projects/packages/changelogger/src/Utils.php
+++ projects/packages/changelogger/src/Utils.php
@@ -167,7 +167,7 @@ class Utils {
 		try {
 			$process = self::runCommand( array( 'git', 'log', '-1', "--format=%cI\n%s", $file ), $output, $formatter );
 			if ( $process->isSuccessful() ) {
-				$cmd_output = explode( '\n', trim( $process->getOutput() ) );
+				$cmd_output = explode( "\n", trim( $process->getOutput() ) );
 
 				// Timestamp.
 				if ( isset( $cmd_output[0] ) && preg_match( '/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:Z|[+-]\d{2}:\d{2})$/', $cmd_output[0] ) ) {
