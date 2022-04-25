c064a07ac737e1132f5b9164d14844988711bf34
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2022-04-25 17:14:17 -0300

    [not verified] Protect: Move vul navigation to own file

--- projects/plugins/protect/src/js/components/vulnerabilities-list/index.jsx
+++ projects/plugins/protect/src/js/components/vulnerabilities-list/index.jsx
@@ -4,60 +4,14 @@
 import React, { useState } from 'react';
 import { __ } from '@wordpress/i18n';
 import { Container, Col, Text, Button } from '@automattic/jetpack-components';
-import { wordpress, plugins as pluginsIcon, warning, color } from '@wordpress/icons';
+import { plugins as pluginsIcon, color } from '@wordpress/icons';
 
 /**
  * Internal dependencies
  */
-// import styles from './styles.module.scss';
-import Navigation, { NavigationItem, NavigationGroup } from '../navigation';
 import Accordion, { AccordionItem } from '../accordion';
 import useProtectData from '../../hooks/use-protect-data';
-
-const VulnerabilitiesNavigation = ( { selected, onSelect } ) => {
-	const { plugins, themes, numVulnerabilities, numCoreVulnerabilities } = useProtectData();
-	return (
-		<Navigation selected={ selected } onSelect={ onSelect }>
-			<NavigationItem
-				initial
-				id="all"
-				label={ __( 'All vulnerabilities', 'jetpack-protect' ) }
-				icon={ warning }
-				badge={ numVulnerabilities }
-				disabled={ numVulnerabilities <= 0 }
-			/>
-			<NavigationItem
-				id="wordpress"
-				label={ __( 'WordPress', 'jetpack-protect' ) }
-				icon={ wordpress }
-				badge={ numCoreVulnerabilities }
-				disabled={ numCoreVulnerabilities <= 0 }
-			/>
-			<NavigationGroup label={ __( 'Plugins', 'jetpack-protect' ) } icon={ pluginsIcon }>
-				{ plugins.map( ( { name, vulnerabilities } ) => (
-					<NavigationItem
-						key={ name }
-						id={ name }
-						label={ name }
-						badge={ vulnerabilities?.length }
-						disabled={ vulnerabilities?.length <= 0 }
-					/>
-				) ) }
-			</NavigationGroup>
-			<NavigationGroup label={ __( 'Themes', 'jetpack-protect' ) } icon={ color }>
-				{ themes.map( ( { name, vulnerabilities } ) => (
-					<NavigationItem
-						key={ name }
-						id={ name }
-						label={ name }
-						badge={ vulnerabilities?.length }
-						disabled={ vulnerabilities?.length <= 0 }
-					/>
-				) ) }
-			</NavigationGroup>
-		</Navigation>
-	);
-};
+import VulnerabilitiesNavigation from './navigation';
 
 const VulAccordionItem = ( { id, name, version, title, icon } ) => {
 	return (
