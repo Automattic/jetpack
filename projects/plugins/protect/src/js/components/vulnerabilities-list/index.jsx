b5dac0703e80d8d14ae16214e927d786a9e12ba2
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2022-04-25 16:24:12 -0300

    [not verified] Protect: Create internal VulnerabilitiesNavigation component

--- projects/plugins/protect/src/js/components/vulnerabilities-list/index.jsx
+++ projects/plugins/protect/src/js/components/vulnerabilities-list/index.jsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { useState } from 'react';
 import { __ } from '@wordpress/i18n';
 import { Container, Col, Text } from '@automattic/jetpack-components';
 import { wordpress, plugins as pluginsIcon, warning, color } from '@wordpress/icons';
@@ -14,49 +14,56 @@ import Navigation, { NavigationItem, NavigationGroup } from '../navigation';
 import Accordion, { AccordionItem } from '../accordion';
 import useProtectData from '../../hooks/use-protect-data';
 
-const VulnerabilitiesList = () => {
+const VulnerabilitiesNavigation = ( { selected, onSelect } ) => {
 	const { plugins, themes, numVulnerabilities, numCoreVulnerabilities } = useProtectData();
+	return (
+		<Navigation selected={ selected } onSelect={ onSelect }>
+			<NavigationItem
+				initial
+				id="all"
+				label={ __( 'All vulnerabilities', 'jetpack-protect' ) }
+				icon={ warning }
+				badge={ numVulnerabilities }
+				disabled={ numVulnerabilities <= 0 }
+			/>
+			<NavigationItem
+				id="wordpress"
+				label={ __( 'WordPress', 'jetpack-protect' ) }
+				icon={ wordpress }
+				badge={ numCoreVulnerabilities }
+				disabled={ numCoreVulnerabilities <= 0 }
+			/>
+			<NavigationGroup label={ __( 'Plugins', 'jetpack-protect' ) } icon={ pluginsIcon }>
+				{ plugins.map( ( { name, vulnerabilities } ) => (
+					<NavigationItem
+						id={ name }
+						label={ name }
+						badge={ vulnerabilities?.length }
+						disabled={ vulnerabilities?.length <= 0 }
+					/>
+				) ) }
+			</NavigationGroup>
+			<NavigationGroup label={ __( 'Themes', 'jetpack-protect' ) } icon={ color }>
+				{ themes.map( ( { name, vulnerabilities } ) => (
+					<NavigationItem
+						id={ name }
+						label={ name }
+						badge={ vulnerabilities?.length }
+						disabled={ vulnerabilities?.length <= 0 }
+					/>
+				) ) }
+			</NavigationGroup>
+		</Navigation>
+	);
+};
+
+const VulnerabilitiesList = () => {
+	const [ selected, setSelected ] = useState( 'all' );
 
 	return (
 		<Container fluid>
 			<Col lg={ 4 }>
-				<Navigation>
-					<NavigationItem
-						initial
-						id="all"
-						label={ __( 'All vulnerabilities', 'jetpack-protect' ) }
-						icon={ warning }
-						badge={ numVulnerabilities }
-						disabled={ numVulnerabilities <= 0 }
-					/>
-					<NavigationItem
-						id="wordpress"
-						label={ __( 'WordPress', 'jetpack-protect' ) }
-						icon={ wordpress }
-						badge={ numCoreVulnerabilities }
-						disabled={ numCoreVulnerabilities <= 0 }
-					/>
-					<NavigationGroup label={ __( 'Plugins', 'jetpack-protect' ) } icon={ pluginsIcon }>
-						{ plugins.map( ( { name, vulnerabilities } ) => (
-							<NavigationItem
-								id={ name }
-								label={ name }
-								badge={ vulnerabilities?.length }
-								disabled={ vulnerabilities?.length <= 0 }
-							/>
-						) ) }
-					</NavigationGroup>
-					<NavigationGroup label={ __( 'Themes', 'jetpack-protect' ) } icon={ color }>
-						{ themes.map( ( { name, vulnerabilities } ) => (
-							<NavigationItem
-								id={ name }
-								label={ name }
-								badge={ vulnerabilities?.length }
-								disabled={ vulnerabilities?.length <= 0 }
-							/>
-						) ) }
-					</NavigationGroup>
-				</Navigation>
+				<VulnerabilitiesNavigation selected={ selected } onSelect={ setSelected } />
 			</Col>
 			<Col lg={ 8 }>
 				<Accordion>
