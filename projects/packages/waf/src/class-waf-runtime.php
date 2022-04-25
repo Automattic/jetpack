9ace957394353de30a4209bdac1d365dae536944
Miguel Xavier Penha Neto <miguelxpn2@gmail.com>
2022-04-24 21:10:48 -0300

    [not verified] WAF: Save log info in the database

--- projects/packages/waf/src/class-waf-runtime.php
+++ projects/packages/waf/src/class-waf-runtime.php
@@ -301,6 +301,9 @@ class Waf_Runtime {
 		$statement = $conn->prepare( "INSERT INTO {$table_prefix}jetpack_waf_blocklog(reason,rule_id, timestamp) VALUES (?, ?, ?)" );
 		if ( false !== $statement ) {
 			$statement->bind_param( 'sis', $log_data['reason'], $log_data['rule_id'], $log_data['timestamp'] );
+			$statement->execute();
+
+			var_dump( $conn );
 
 			if ( $conn->insert_id > 100 ) {
 				$conn->query( "DELETE FROM {$table_prefix}jetpack_waf_blocklog ORDER BY log_id LIMIT 1" );
