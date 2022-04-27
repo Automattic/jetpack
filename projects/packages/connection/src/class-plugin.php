59b83c90d918c04a17e09e40e5ccc3d0a219bbc6
Karen Attfield <karenlattfield@gmail.com>
2022-04-27 11:21:24 +0100

    Reverting changes from 7cee72a

--- projects/packages/connection/src/class-plugin.php
+++ projects/packages/connection/src/class-plugin.php
@@ -91,19 +91,25 @@ class Plugin {
 	/**
 	 * Add the plugin to the set of disconnected ones.
 	 *
+	 * @deprecated since 1.39.0.
+	 *
 	 * @return bool
 	 */
 	public function disable() {
-		return Plugin_Storage::disable_plugin( $this->slug );
+		_deprecated_function( __METHOD__, '1.39.0' );
+		return true;
 	}
 
 	/**
 	 * Remove the plugin from the set of disconnected ones.
 	 *
+	 * @deprecated since 1.39.0.
+	 *
 	 * @return bool
 	 */
 	public function enable() {
-		return Plugin_Storage::enable_plugin( $this->slug );
+		_deprecated_function( __METHOD__, '1.39.0' );
+		return true;
 	}
 
 	/**
