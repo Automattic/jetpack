62a78a6afb54e75e9b1b816598442846e21fc450
Miguel Xavier Penha Neto <miguelxpn2@gmail.com>
2022-04-28 14:30:49 -0300

    WAF: Add activation/deactivation hooks for proper setup

--- projects/plugins/jetpack/modules/waf.php
+++ projects/plugins/jetpack/modules/waf.php
@@ -11,3 +11,57 @@
  *
  * @package automattic/jetpack
  */
+
+use Automattic\Jetpack\Waf\Waf_Runner;
+use Automattic\Jetpack\Waf\Waf_Standalone_Bootstrap;
+
+/**
+ * Jetpack waf module class.
+ */
+class Jetpack_Waf_Module {
+
+	/**
+	 * Instance of the class.
+	 *
+	 * @var Jetpack_Waf_Module()
+	 */
+	private static $instance = null;
+
+	/**
+	 * Singleton implementation
+	 *
+	 * @return object
+	 */
+	public static function instance() {
+		if ( ! is_a( self::$instance, 'Jetpack_Waf_Module' ) ) {
+			self::$instance = new Jetpack_Waf_Module();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Registers actions
+	 */
+	private function __construct() {
+		add_action( 'jetpack_activate_module_waf', array( $this, 'on_activation' ) );
+		add_action( 'jetpack_deactivate_module_waf', array( $this, 'on_deactivation' ) );
+	}
+
+	/**
+	 * On module activation set up waf mode
+	 */
+	public function on_activation() {
+		update_option( Waf_Runner::MODE_OPTION_NAME, 'normal' );
+		( new Waf_Standalone_Bootstrap() )->generate();
+	}
+
+	/**
+	 * On module deactivation, unset waf mode
+	 */
+	public function on_deactivation() {
+		delete_option( Waf_Runner::MODE_OPTION_NAME );
+	}
+}
+
+Jetpack_Waf_Module::instance();
