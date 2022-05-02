a39fd1e90ffa39787c89531888fe5ed470c3140a
Rahul <rahul.gavande@automattic.com>
2022-05-02 22:00:32 +0530

    [not verified] Replace plan check with hasActiveSiteFeature check

--- projects/plugins/jetpack/_inc/client/at-a-glance/akismet.jsx
+++ projects/plugins/jetpack/_inc/client/at-a-glance/akismet.jsx
@@ -4,7 +4,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { get, noop } from 'lodash';
+import { noop } from 'lodash';
 
 /**
  * WordPress dependencies
@@ -20,7 +20,7 @@ import Card from 'components/card';
 import { createNotice, removeNotice } from 'components/global-notices/state/notices/actions';
 import DashItem from 'components/dash-item';
 import { getAkismetData } from 'state/at-a-glance';
-import { getSitePlan } from 'state/site';
+import { getSitePlan, hasActiveSiteFeature } from 'state/site';
 import { getApiNonce } from 'state/initial-state';
 import { getProductDescriptionUrl } from 'product-descriptions/utils';
 import { getJetpackProductUpsellByFeature, FEATURE_SPAM_AKISMET_PLUS } from 'lib/plans/constants';
@@ -96,8 +96,6 @@ class DashAkismet extends Component {
 	getContent() {
 		const akismetData = this.props.akismetData;
 		const labelName = __( 'Anti-spam', 'jetpack' );
-		const isSiteOnFreePlan =
-			'jetpack_free' === get( this.props.sitePlan, 'product_slug', 'jetpack_free' );
 
 		const support = {
 			text: __(
@@ -202,7 +200,7 @@ class DashAkismet extends Component {
 
 		const hasSitePlan = false !== this.props.sitePlan;
 
-		if ( isSiteOnFreePlan ) {
+		if ( ! this.props.hasAkismet ) {
 			if ( 'not_installed' === akismetData ) {
 				return (
 					<DashItem
@@ -306,6 +304,7 @@ export default connect(
 			upgradeUrl: getProductDescriptionUrl( state, 'akismet' ),
 			nonce: getApiNonce( state ),
 			hasConnectedOwner: hasConnectedOwner( state ),
+			hasAkismet: hasActiveSiteFeature( state, 'akismet' ),
 		};
 	},
 	dispatch => ( {
