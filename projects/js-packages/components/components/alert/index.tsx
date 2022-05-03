e77aa5fe1379d23a686b940999f4b58640933fd6
retrofox <rdsuarez@gmail.com>
2022-05-03 08:43:10 -0300

    [not verified] re-defin alert level prop type

--- projects/js-packages/components/components/alert/index.tsx
+++ projects/js-packages/components/components/alert/index.tsx
@@ -18,12 +18,9 @@ export const LEVEL_WARNING = 'warning';
 export const LEVEL_INFO = 'info';
 export const LEVEL_SUCCESS = 'success';
 
-export const ALERT_LEVELS = [ LEVEL_ERROR, LEVEL_WARNING, LEVEL_INFO, LEVEL_SUCCESS ] as const;
-export type alertLevelProp = typeof ALERT_LEVELS[ number ];
-
 type AlertProps = {
 	/** The severity of the alert. */
-	level: alertLevelProp;
+	level: typeof LEVEL_ERROR | typeof LEVEL_WARNING | typeof LEVEL_INFO | typeof LEVEL_SUCCESS;
 
 	/** Children to be rendered inside the alert. */
 	children: React.ReactNode;
@@ -32,7 +29,7 @@ type AlertProps = {
 	showIcon?: boolean;
 };
 
-const getIconByLevel = ( level: alertLevelProp ) => {
+const getIconByLevel = ( level: AlertProps[ 'level' ] ) => {
 	switch ( level ) {
 		case LEVEL_ERROR:
 			return warning;
@@ -54,9 +51,13 @@ const getIconByLevel = ( level: alertLevelProp ) => {
  * @param {string} props.level             - The alert level: error, warning, info, success.
  * @param {boolean} props.showIcon         - Whether to show the alert icon.
  * @param {React.Component} props.children - The alert content.
- * @returns {React.Component}                The `Alert` component.
+ * @returns {React.ReactElement}             The `Alert` component.
  */
-function Alert( { level, children, showIcon }: AlertProps ): React.ReactNode {
+const Alert: React.FC< React.PropsWithChildren< AlertProps > > = ( {
+	level,
+	children,
+	showIcon,
+}: AlertProps ): React.ReactElement => {
 	const classes = classNames( styles.container, styles[ `is-${ level }` ] );
 
 	return (
@@ -65,10 +66,10 @@ function Alert( { level, children, showIcon }: AlertProps ): React.ReactNode {
 			{ children }
 		</div>
 	);
-}
+};
 
 Alert.defaultProps = {
-	level: LEVEL_WARNING,
+	level: 'warning',
 	showIcon: true,
 };
 
