0a458439e78dea103c059e15cc865399aef06b02
Steve D <33553323+sdixon194@users.noreply.github.com>
2022-05-04 11:23:47 -0400

    Update tools/version-packages.sh
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>

--- tools/version-packages.sh
+++ tools/version-packages.sh
@@ -101,10 +101,10 @@ fi
 
 # Compare new versus old versions to check for downgrades.
 EXIT=0
-while IFS=$'\t' read --range PKG OLDVER NEWVER; do
+while IFS=$'\t' read -r PKG OLDVER NEWVER; do
 	OV=$(sed -e 's/\.x-dev$/.0-alpha/' <<<"$OLDVER")
 	NV=$(sed -e 's/^\^//' -e 's/\.x-dev$/.0-alpha/' <<<"$NEWVER")
-	if ! pnpm semver -c -r ">$OV" "$NV" >/dev/null; then
+	if ! pnpm semver -c --range ">$OV" "$NV" >/dev/null; then
 		EXIT=1
 		error "$PKG was not upgraded ($NEWVER <= $OLDVER)"
 	fi
