02b04f6de1b0b6402d3a3b45c729c9da418b16bc
Daniel Post <daniel.post@automattic.com>
2022-05-05 16:18:19 +0200

    [not verified] Add Jetpack Social sidebar

---
+++ projects/plugins/social/src/js/editor.js
@@ -0,0 +1,52 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import {
+	PluginSidebar,
+	PluginSidebarMoreMenuItem,
+	PluginPrePublishPanel,
+} from '@wordpress/edit-post';
+import { PostTypeSupportCheck } from '@wordpress/editor';
+import { registerPlugin } from '@wordpress/plugins';
+import { dispatch } from '@wordpress/data';
+import { getQueryArg } from '@wordpress/url';
+import domReady from '@wordpress/dom-ready';
+
+/**
+ * Open Jetpack Spcoal; sidebar by default when URL includes jetpackSidebarIsOpen=true.
+ */
+domReady( () => {
+	if ( getQueryArg( window.location.search, 'jetpackSidebarIsOpen' ) === 'true' ) {
+		dispatch( 'core/interface' ).enableComplementaryArea(
+			'core/edit-post',
+			'jetpack-social-sidebar/jetpack-social'
+		);
+	}
+} );
+
+const PublicizePanel = () => <span>This is a placeholder panel</span>;
+
+registerPlugin( 'jetpack-social', {
+	render: () => (
+		<PostTypeSupportCheck supportKeys="publicize">
+			<PluginSidebarMoreMenuItem target="jetpack-social">Jetpack Social</PluginSidebarMoreMenuItem>
+
+			<PluginSidebar name="jetpack-social" title="Jetpack Social">
+				<PublicizePanel />
+			</PluginSidebar>
+
+			<PluginPrePublishPanel
+				initialOpen
+				id="publicize-title"
+				title={
+					<span id="publicize-defaults" key="publicize-title-span">
+						{ __( 'Share this post', 'jetpack-social' ) }
+					</span>
+				}
+			>
+				<PublicizePanel prePublish={ true } />
+			</PluginPrePublishPanel>
+		</PostTypeSupportCheck>
+	),
+} );
