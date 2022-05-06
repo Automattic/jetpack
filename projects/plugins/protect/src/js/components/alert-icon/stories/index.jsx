0570f6acf2f6f95e2b673a58a8a4bc69f9f3c5ff
retrofox <rdsuarez@gmail.com>
2022-05-06 10:57:39 -0300

    [not verified] improve Alert icon story

--- projects/plugins/protect/src/js/components/alert-icon/stories/index.jsx
+++ projects/plugins/protect/src/js/components/alert-icon/stories/index.jsx
@@ -12,6 +12,13 @@ import AlertIcon from '../index.jsx';
 export default {
 	title: 'Plugins/Protect/Alert Icon',
 	component: AlertIcon,
+	argTypes: {
+		color: {
+			control: {
+				type: 'color',
+			},
+		},
+	},
 };
 
 const FooterTemplate = args => <AlertIcon { ...args } />;
