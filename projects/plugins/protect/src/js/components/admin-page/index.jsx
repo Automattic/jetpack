bd7b58457cb7a801cfedd5b35253bea158dd163a
Damián Suárez <rdsuarez@gmail.com>
2022-05-06 14:33:26 -0300

    Protect: Add alert icon when having problem scanning the site (#24273)
    * [not verified] protect: add AlertIcon component
    
    * show Alert icon when errors
    
    * [not verified] tweak vertical position of alert
    
    * [not verified] remove margin to to error title
    
    * changelog
    
    * [not verified] improve Alert icon story

--- projects/plugins/protect/src/js/components/admin-page/index.jsx
+++ projects/plugins/protect/src/js/components/admin-page/index.jsx
@@ -27,6 +27,8 @@ import useProtectData from '../../hooks/use-protect-data';
 import inProgressImage from './in-progress.png';
 import useAnalyticsTracks from '../../hooks/use-analytics-tracks';
 import Logo from '../logo';
+import AlertSVGIcon from '../alert-icon';
+import styles from './styles.module.scss';
 
 export const SECURITY_BUNDLE = 'jetpack_security_t1_yearly';
 
@@ -63,10 +65,8 @@ const ProtectAdminPage = () => {
 				<AdminSectionHero>
 					<Container horizontalSpacing={ 3 } horizontalGap={ 7 }>
 						<Col sm={ 4 } md={ 4 } lg={ 6 }>
-							ALERT ICON GOES HERE
-							<H3 mt={ 8 }>
-								{ __( 'We’re having problems scanning your site', 'jetpack-protect' ) }
-							</H3>
+							<AlertSVGIcon className={ styles[ 'alert-icon-wrapper' ] } />
+							<H3>{ __( 'We’re having problems scanning your site', 'jetpack-protect' ) }</H3>
 							<Text>{ displayErrorMessage }</Text>
 						</Col>
 						<Col sm={ 0 } md={ 0 } lg={ 1 }></Col>
