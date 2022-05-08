04583a75ed3ff289df5f7b4a0164fe3ced5f423e
Eric Binnion <ericbinnion@gmail.com>
2022-05-07 21:05:30 -0500

    My Jetpack: Licensing: Update "View my plans" button to go to Calypso Green

--- projects/js-packages/licensing/components/activation-screen-success-info/primary-link/index.jsx
+++ projects/js-packages/licensing/components/activation-screen-success-info/primary-link/index.jsx
@@ -5,6 +5,7 @@ import { __ } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
 import PropTypes from 'prop-types';
 import React from 'react';
+import { getRedirectUrl } from '@automattic/jetpack-components';
 
 /**
  * Style dependencies
@@ -12,7 +13,7 @@ import React from 'react';
 import './style.scss';
 
 const PrimaryLink = props => {
-	const { currentRecommendationsStep, siteAdminUrl } = props;
+	const { currentRecommendationsStep, siteAdminUrl, siteRawUrl } = props;
 
 	// If the user has not completed the first step of the Assistant, make the primary button link to it.
 	if ( currentRecommendationsStep === 'not-started' ) {
@@ -29,7 +30,7 @@ const PrimaryLink = props => {
 	return (
 		<Button
 			className="jp-license-activation-screen-success-info--button"
-			href={ siteAdminUrl + 'admin.php?page=jetpack#/my-plan' }
+			href={ getRedirectUrl( 'license-activation-view-my-plans', { site: siteRawUrl } ) }
 		>
 			{ __( 'View my plans', 'jetpack' ) }
 		</Button>
@@ -38,6 +39,7 @@ const PrimaryLink = props => {
 
 PrimaryLink.propTypes = {
 	siteAdminUrl: PropTypes.string.isRequired,
+	siteRawUrl: PropTypes.string.isRequired,
 	currentRecommendationsStep: PropTypes.string,
 };
 
