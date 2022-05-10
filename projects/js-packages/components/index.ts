0ae7bf9333e7da5b4856530ff88c2e45ca1ce771
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2022-05-10 18:48:26 -0300

    Components: Only export Button

--- projects/js-packages/components/index.ts
+++ projects/js-packages/components/index.ts
@@ -35,7 +35,7 @@ export { default as ThemeProvider } from './components/theme-provider';
 export { default as Text, H2, H3, Title } from './components/text';
 export { default as numberFormat } from './components/number-format';
 export { default as QRCode } from './components/qr-code';
-export * from './components/button';
+export { default as Button } from './components/button';
 export { default as ProductPrice } from './components/product-price';
 export { default as ProductOffer, IconsCard } from './components/product-offer';
 export { default as Dialog } from './components/dialog';
