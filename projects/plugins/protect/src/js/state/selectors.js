d8fbf5d794ce5bf49522db77a3a212901f5bcccf
retrofox <rdsuarez@gmail.com>
2022-05-12 18:34:08 -0400

    [not verified] reduxify product data

--- projects/plugins/protect/src/js/state/selectors.js
+++ projects/plugins/protect/src/js/state/selectors.js
@@ -5,6 +5,7 @@ const selectors = {
 	getStatusIsFetching: state => state.statusIsFetching || false,
 	getWpVersion: state => state.wpVersion || '',
 	getSecurityBundle: state => state.securityBundle || {},
+	getProduct: state => state.productData || {},
 };
 
 export default selectors;
