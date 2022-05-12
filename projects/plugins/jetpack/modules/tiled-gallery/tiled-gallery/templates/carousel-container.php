957c0b219b3abb3b8e818fde1d2fd0e579cb191d
sdixon194 <steve.dixon@automattic.com>
2022-05-12 13:37:01 -0400

    carousel-container.php

--- projects/plugins/jetpack/modules/tiled-gallery/tiled-gallery/templates/carousel-container.php
+++ projects/plugins/jetpack/modules/tiled-gallery/tiled-gallery/templates/carousel-container.php
@@ -1,18 +1,25 @@
 <?php
+/**
+ * Encode extra carousel container data.
+ *
+ * @package jetpack
+ */
+
 if ( defined( 'JSON_HEX_AMP' ) ) {
 	// see shortcodes/slideshow.php
 	// This is nice to have, but not strictly necessary since we use _wp_specialchars() below
 	// phpcs:ignore PHPCompatibility
-	$extra = json_encode( $this->get_container_extra_data(), JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT );
+	$extra = wp_json_encode( $this->get_container_extra_data(), JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT ); // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable
 } else {
-	$extra = json_encode( $this->get_container_extra_data() );
+	$extra = wp_json_encode( $this->get_container_extra_data() ); // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable
 }
+// phpcs:disable
 ?>
 <div
 	class="tiled-gallery type-<?php echo $this->type; ?> tiled-gallery-unresized"
 	data-original-width="<?php echo esc_attr( Jetpack_Tiled_Gallery::get_content_width() ); ?>"
 	<?php if ( isset( $extra ) ) : ?>
-		data-carousel-extra='<?php echo _wp_specialchars( wp_check_invalid_utf8( $extra ), ENT_QUOTES, false, true ); ?>'
+		data-carousel-extra='<?php echo esc_attr( _wp_specialchars( wp_check_invalid_utf8( $extra ), ENT_QUOTES, false, true ) ); ?>'
 	<?php endif; ?>
 	itemscope itemtype="http://schema.org/ImageGallery"
 	>
