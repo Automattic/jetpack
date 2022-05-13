b4bc6320fea9bde72b519132bb95e8fe1e2905bd
Brad Jorsch <anomiex@users.noreply.github.com>
2022-05-13 11:06:09 -0400

    update carousel-container.php

--- projects/plugins/jetpack/modules/tiled-gallery/tiled-gallery/templates/carousel-container.php
+++ projects/plugins/jetpack/modules/tiled-gallery/tiled-gallery/templates/carousel-container.php
@@ -5,23 +5,16 @@
  * @package jetpack
  */
 
-if ( defined( 'JSON_HEX_AMP' ) ) {
-	// see shortcodes/slideshow.php
-	// This is nice to have, but not strictly necessary since we use _wp_specialchars() below
-	// phpcs:ignore PHPCompatibility
-	$extra = wp_json_encode( $this->get_container_extra_data(), JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT ); // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable
-} else {
-	$extra = wp_json_encode( $this->get_container_extra_data() ); // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable
-}
-// phpcs:disable
+// Using JSON_HEX_AMP avoids breakage due to `esc_attr()` refusing to double-encode.
+$extra = wp_json_encode( $this->get_container_extra_data(), JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT ); // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable
 ?>
 <div
-	class="tiled-gallery type-<?php echo $this->type; ?> tiled-gallery-unresized"
+	class="tiled-gallery type-<?php echo esc_html( $this->type ); // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable ?> tiled-gallery-unresized"
 	data-original-width="<?php echo esc_attr( Jetpack_Tiled_Gallery::get_content_width() ); ?>"
 	<?php if ( isset( $extra ) ) : ?>
-		data-carousel-extra='<?php echo esc_attr( _wp_specialchars( wp_check_invalid_utf8( $extra ), ENT_QUOTES, false, true ) ); ?>'
+data-carousel-extra='<?php echo esc_attr( $extra ); ?>'
 	<?php endif; ?>
 	itemscope itemtype="http://schema.org/ImageGallery"
 	>
-	<?php $this->template( "$this->type-layout", $context ); ?>
+	<?php $this->template( "$this->type-layout", $context ); // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable ?>
 </div>
