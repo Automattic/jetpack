53ffb7d6da4537eb984ff51e3422516d41b5607d
Karen Attfield <karenlattfield@gmail.com>
2022-05-16 14:26:06 +0100

    class.json-api-token.php: Adding function visibility.

--- projects/plugins/jetpack/sal/class.json-api-token.php
+++ projects/plugins/jetpack/sal/class.json-api-token.php
@@ -70,7 +70,7 @@ class SAL_Token {
 	 * @param string $external_user_code Used for tokens created by Oauth clients.
 	 * @param string $auth_type The type of authorization based on where the Jetpack connection is made (eg. calypso).
 	 */
-	function __construct( $blog_id, $user_id, $scope, $client_id, $external_user_id, $external_user_code, $auth_type ) {
+	public function __construct( $blog_id, $user_id, $scope, $client_id, $external_user_id, $external_user_code, $auth_type ) {
 		$this->blog_id = $blog_id; // if blog_id is set and scope is not global, limit to that blog.
 		$this->user_id = $user_id;
 		$this->client_id = $client_id;
@@ -94,7 +94,7 @@ class SAL_Token {
 	 *
 	 * @return SAL_Token
 	 */
-	static function for_anonymous_user() {
+	public static function for_anonymous_user() {
 		return new SAL_Token( 
 			null, 
 			get_current_user_id(), 
@@ -113,7 +113,7 @@ class SAL_Token {
 	 *
 	 * @return SAL_Token
 	 */
-	static function from_rest_token( $token ) {
+	public static function from_rest_token( $token ) {
 		$user_id = isset( $token['user_id'] ) ? $token['user_id'] : get_current_user_id();
 		$scope = isset( $token['scope'] ) ? $token['scope'][0] : null;
 		$client_id = isset( $token['client_id'] ) ? $token['client_id'] : null;
