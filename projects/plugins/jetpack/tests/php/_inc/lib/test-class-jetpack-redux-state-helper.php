5576b431f4006a68e87cd12c11edabaa998ceb7b
Wojtek Naruniec <wojtek@naruniec.me>
2022-05-17 20:05:59 +0200

    Add teardown blocks to restore the theme features for better test isolation
    The theme features global variable is preserved in set_up() and then
    restored in tear_down() to ensure that tests do not modify the global state.

--- projects/plugins/jetpack/tests/php/_inc/lib/test-class-jetpack-redux-state-helper.php
+++ projects/plugins/jetpack/tests/php/_inc/lib/test-class-jetpack-redux-state-helper.php
@@ -14,6 +14,33 @@ jetpack_require_lib( 'admin-pages/class-jetpack-redux-state-helper' );
  */
 class WP_Test_Jetpack_Redux_State_Helper extends WP_UnitTestCase {
 	/**
+	 * Theme features.
+	 *
+	 * @var array
+	 */
+	private $theme_features;
+
+	/**
+	 * Saving the original theme features.
+	 */
+	public function set_up() {
+		parent::set_up();
+
+		global $_wp_theme_features;
+		$this->theme_features = $_wp_theme_features;
+	}
+
+	/**
+	 * Restoring the original theme features.
+	 */
+	public function tear_down() {
+		global $_wp_theme_features;
+
+		$_wp_theme_features = $this->theme_features;
+		parent::tear_down();
+	}
+
+	/**
 	 * Tests whether get_initial_state() signals that the theme supports widgets.
 	 *
 	 * @covers ::get_initial_state
