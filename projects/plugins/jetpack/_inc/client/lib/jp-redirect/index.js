109d3b1f445ad5bd1f6f94d88e4ffc0178f4e456
Manzoor Wani <manzoorwani.jk@gmail.com>
2022-05-18 10:59:33 +0530

    Remove unused module that results in TS error

--- projects/plugins/jetpack/_inc/client/lib/jp-redirect/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * @since 8.5.0
- * @deprecated
- * @see @automattic/jetpack-components
- * @returns {string} The redirect URL
- */
-export { getRedirectUrl as default } from '@automattic/jetpack-components';
