3ca39165b17b4c2878689348a8a96a30c91e930a
Chris R <chris.rosser@automattic.com>
2022-05-19 10:27:54 +1200

    Fixup project versions

--- projects/packages/search/src/class-package.php
+++ projects/packages/search/src/class-package.php
@@ -11,7 +11,7 @@ namespace Automattic\Jetpack\Search;
  * Search package general information
  */
 class Package {
-	const VERSION = '0.13.4';
+	const VERSION = '0.14.0-alpha';
 	const SLUG    = 'search';
 
 	/**
