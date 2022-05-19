3e2132af5a520f8b2298e33728dc750bd790971a
bindlegirl <1242807+bindlegirl@users.noreply.github.com>
2022-05-19 12:23:17 +0200

    Changelog updates

--- projects/packages/connection/src/class-package-version.php
+++ projects/packages/connection/src/class-package-version.php
@@ -12,7 +12,7 @@ namespace Automattic\Jetpack\Connection;
  */
 class Package_Version {
 
-	const PACKAGE_VERSION = '1.40.3-alpha';
+	const PACKAGE_VERSION = '1.40.3';
 
 	const PACKAGE_SLUG = 'connection';
 
