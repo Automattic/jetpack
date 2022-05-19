ce85409d5b4d1a6cf878c13fe04ef1934aea1c6e
Derek Smart <smart@automattic.com>
2022-05-19 15:56:21 -0400

    Modules: Make activate() method Jetpack-the-plugin agnostic (#24385)
    * Make activate() method jetpack-agnostic
    
    * Do the actual updating if no Jetpack.
    
    * Search: Use Module activate() and deactivate() methods
    
    * changelog
    
    * Search: Bail on activation if plan does not support
    
    * Move defensive is_module check to Jetpack context.
    
    * Fix versions

--- projects/packages/search/src/class-package.php
+++ projects/packages/search/src/class-package.php
@@ -11,7 +11,7 @@ namespace Automattic\Jetpack\Search;
  * Search package general information
  */
 class Package {
-	const VERSION = '0.14.0';
+	const VERSION = '0.14.1-alpha';
 	const SLUG    = 'search';
 
 	/**
