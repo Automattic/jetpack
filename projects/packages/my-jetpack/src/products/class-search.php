69aff8d4a81979a8bee944acbf9c9bc3d868b965
Leo Germani <leogermani@automattic.com>
2022-05-20 15:30:45 -0300

    enable instant search on search activation

--- projects/packages/my-jetpack/src/products/class-search.php
+++ projects/packages/my-jetpack/src/products/class-search.php
@@ -10,6 +10,7 @@ namespace Automattic\Jetpack\My_Jetpack\Products;
 use Automattic\Jetpack\Connection\Client;
 use Automattic\Jetpack\My_Jetpack\Hybrid_Product;
 use Automattic\Jetpack\My_Jetpack\Wpcom_Products;
+use Automattic\Jetpack\Search\Module_Control as Search_Module_Control;
 use Jetpack_Options;
 use WP_Error;
 
@@ -207,6 +208,26 @@ class Search extends Hybrid_Product {
 	public static function has_required_plan() {
 		$search_state = static::get_state_from_wpcom();
 		return ! empty( $search_state->supports_search ) || ! empty( $search_state->supports_instant_search );
+	}
+
+	/**
+	 * Activates the product. Try to enable instant search after the Search module was enabled.
+	 *
+	 * @param bool|WP_Error $product_activation Is the result of the top level activation actions. You probably won't do anything if it is an WP_Error.
+	 * @return bool|WP_Error
+	 */
+	public static function do_product_specific_activation( $product_activation ) {
+		$product_activation = parent::do_product_specific_activation( $product_activation );
+		if ( is_wp_error( $product_activation ) ) {
+			return $product_activation;
+		}
+
+		if ( class_exists( 'Automattic\Jetpack\Search\Module_Control' ) ) {
+			( new Search_Module_Control() )->enable_instant_search();
+		}
+
+		// we don't want to change the success of the activation if we fail to activate instant search. That's not mandatory.
+		return $product_activation;
 	}
 
 	/**
