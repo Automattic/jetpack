94ae5c64b9f864cdc9f2eed89b282ae97faa27f0
retrofox <rdsuarez@gmail.com>
2022-05-23 17:17:38 -0300

    [not verified] improve jsdoc

--- projects/plugins/protect/src/js/components/admin-page/index.jsx
+++ projects/plugins/protect/src/js/components/admin-page/index.jsx
@@ -34,6 +34,20 @@ import styles from './styles.module.scss';
 
 export const SECURITY_BUNDLE = 'jetpack_security_t1_yearly';
 
+/**
+ * SeventyFive layout meta component
+ * The component name references to
+ * the sections disposition of the layout.
+ * FiftyFifty, 75, thus 7|5 means the cols numbers
+ * for main and secondary sections respectively,
+ * in large lg viewport size.
+ *
+ * @param {object} props                            - Component props
+ * @param {React.ReactNode} props.main              - Main section component
+ * @param {React.ReactNode} props.secondary         - Secondary section component
+ * @param {boolean} props.preserveSecondaryOnMobile - Whether to show secondary section on mobile
+ * @returns {React.ReactNode} 					    - React meta-component
+ */
 export const SeventyFiveLayout = ( { main, secondary, preserveSecondaryOnMobile = false } ) => {
 	const [ isSmall ] = useBreakpointMatch( [ 'sm', 'lg' ], [ null, '<' ] );
 
