fbe594f70955a284776ace718960e4b712b5718b
Samiff <samiff@users.noreply.github.com>
2022-05-24 11:35:25 -0600

    Bump package version

--- projects/packages/search/src/class-package.php
+++ projects/packages/search/src/class-package.php
@@ -11,7 +11,7 @@ namespace Automattic\Jetpack\Search;
  * Search package general information
  */
 class Package {
-	const VERSION = '0.14.1';
+	const VERSION = '0.14.2-alpha';
 	const SLUG    = 'search';
 
 	/**
