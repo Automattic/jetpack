1875e47383a033305de0dfaa4258b1dc664f22a8
Anna McPhee <anna.mcphee@automattic.com>
2022-05-25 15:05:57 +0100

    add additional context for translators

--- projects/packages/search/src/dashboard/components/record-meter/index.jsx
+++ projects/packages/search/src/dashboard/components/record-meter/index.jsx
@@ -40,6 +40,8 @@ export default function RecordMeter( {
 			<div className="jp-search-dashboard-row">
 				<div className="lg-col-span-2 md-col-span-1 sm-col-span-0"></div>
 				<div className="jp-search-record-meter__title lg-col-span-8 md-col-span-6 sm-col-span-4">
+					/* translators: Your Search Index is a breakdown of the site's indexed post type content,
+					such as the number of indexed posts, pages etc. */
 					<h2>{ __( 'Your Search Index', 'jetpack-search-pkg' ) }</h2>
 					<div>
 						<RecordCount
