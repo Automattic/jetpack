1f14fd4bf28268743e7ff70dac94c8ff6c115ffd
Paul Bunkham <paul@dobit.co.uk>
2022-05-25 12:15:20 +0100

    Publicize Components: Move remaining components to the package. (#24464)

--- projects/js-packages/publicize-components/src/hooks/use-social-media-message/index.js
+++ projects/js-packages/publicize-components/src/hooks/use-social-media-message/index.js
@@ -4,6 +4,18 @@
 import { useSelect, useDispatch } from '@wordpress/data';
 import { store as editorStore } from '@wordpress/editor';
 
+/**
+ * @typedef {Object} MessageHook
+ * @property {string} message - The text of the message.
+ * @property {number} maxLength - The maximum length of the message.
+ * @property {Function} updateMessage - Callback used to update the message.
+ */
+
+/**
+ * Hook to handle storing the the current custom message.
+ *
+ * @returns {MessageHook} - An object with the message hook properties set.
+ */
 export default function useSocialMediaMessage() {
 	const { editPost } = useDispatch( editorStore );
 
