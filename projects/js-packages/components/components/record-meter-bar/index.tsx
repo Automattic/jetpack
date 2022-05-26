Automattic/jetpack

commit 178f257c30ef7b9e34daaf336342c37abfd5aedd

RecordMeterBar Component: Adds a prop to display legend count after label (#24474)
Co-authored-by: Chris R <chris.rosser@automattic.com>

projects/js-packages/components/components/record-meter-bar/index.tsx
@@ -29,6 +29,10 @@ export type RecordMeterBarProps = {
 	 * The items to display in Record meter.
 	 */
 	items: Array< RecordMeterBarItem >;
+	/**
+	 * The formatting style for legend item display. If not provided, it defaults to showing legend label after count
+	 */
+	showLegendLabelBeforeCount: boolean;
 };
 
 /**
@@ -37,7 +41,11 @@ export type RecordMeterBarProps = {
  * @param {RecordMeterBarProps} props - Props
  * @returns {React.ReactElement} - JSX element
  */
-const RecordMeterBar: React.FC< RecordMeterBarProps > = ( { totalCount, items = [] } ) => {
+const RecordMeterBar: React.FC< RecordMeterBarProps > = ( {
+	totalCount,
+	items = [],
+	showLegendLabelBeforeCount = false,
+} ) => {
 	const total = useMemo( () => {
 		// If total count is not given, then compute it from items' count
 		return (
@@ -67,8 +75,20 @@ const RecordMeterBar: React.FC< RecordMeterBarProps > = ( { totalCount, items = 
 									className="record-meter-bar__legend--item-circle"
 									style={ { backgroundColor } }
 								/>
-								<span className="record-meter-bar__legend--item-count">{ count }</span>
-								<span className="record-meter-bar__legend--item-label">{ label }</span>
+								{ ! showLegendLabelBeforeCount && (
+									<span>
+										<span className="record-meter-bar__legend--item-count">{ count }</span>
+										<span className="record-meter-bar__legend--item-label">{ label }</span>
+									</span>
+								) }
+								{ showLegendLabelBeforeCount && (
+									<span>
+										<span className="record-meter-bar__legend--item-label record-meter-bar__legend--item-label-first">
+											{ label }
+										</span>
+										<span className="record-meter-bar__legend--item-count">({ count })</span>
+									</span>
+								) }
 							</li>
 						);
 					} ) }

projects/js-packages/components/components/record-meter-bar/stories/index.tsx
@@ -28,3 +28,9 @@ WithTotalCount.args = {
 	...DefaultArgs,
 	totalCount: 200,
 };
+
+export const LabelBeforeCount = Template.bind( {} );
+LabelBeforeCount.args = {
+	...DefaultArgs,
+	showLegendLabelBeforeCount: true,
+};

projects/js-packages/components/components/record-meter-bar/style.scss
@@ -1,35 +1,38 @@
 .record-meter-bar {
-    padding-block: 1em;
+	padding-block: 1em;
 
-    &__items {
-        height: 2rem;
-        display: flex;
-        border-radius: 1rem;
-        overflow: hidden;
-        margin-bottom: 2em;
-        background-color: var(--jp-gray-off);
-    }
+	&__items {
+		height: 2rem;
+		display: flex;
+		border-radius: 1rem;
+		overflow: hidden;
+		margin-bottom: 2em;
+		background-color: var(--jp-gray-off);
+	}
 
-    &__legend {
-        &--items {
-            display: flex;
-            margin: 0px;
-        }
-        &--item {
-            display: flex;
-            margin: 0px;
-            margin-inline-end: 1em;
-            align-items: center;
-        }
-        &--item-circle {
-            width: 1rem;
-            height: 1rem;
-            display: inline-block;
-            margin-inline-end: 0.4em;
-            border-radius: 100%;
-        }
-        &--item-count {
-            margin-inline-end: 0.4em;
-        }
-    }
+	&__legend {
+		&--items {
+			display: flex;
+			margin: 0;
+		}
+		&--item {
+			display: flex;
+			margin: 0;
+			margin-inline-end: 1em;
+			align-items: center;
+		}
+		&--item-circle {
+			width: 1rem;
+			height: 1rem;
+			display: inline-block;
+			margin-inline-end: 0.4em;
+			border-radius: 100%;
+		}
+		&--item-count {
+			margin-inline-end: 0.4em;
+		}
+		&--item-label-first {
+			margin-inline-end: 0.4em;
+		}
+	}
 }

commit addd965c72a8012d3a10c521a0b2efa1af18a0bb

Fix the types broken in #24474

projects/js-packages/components/components/record-meter-bar/index.tsx
@@ -32,7 +32,7 @@ export type RecordMeterBarProps = {
 	/**
 	 * The formatting style for legend item display. If not provided, it defaults to showing legend label after count
 	 */
-	showLegendLabelBeforeCount: boolean;
+	showLegendLabelBeforeCount?: boolean;
 };
 
 /**