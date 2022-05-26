Automattic/jetpack

commit 178f257c30ef7b9e34daaf336342c37abfd5aedd

RecordMeterBar Component: Adds a prop to display legend count after label (#24474)
Co-authored-by: Chris R <chris.rosser@automattic.com>

projects/js-packages/components/components/record-meter-bar/index.tsx
@@ -29,6 +29,10 @@ export type RecordMeterBarProps = {
 	 * The items to display in Record meter.
 	 */
 	items: Array< RecordMeterBarItem >;
+	/**
+	 * The formatting style for legend item display. If not provided, it defaults to showing legend label after count
+	 */
+	showLegendLabelBeforeCount: boolean;
 };
 
 /**
@@ -37,7 +41,11 @@ export type RecordMeterBarProps = {
  * @param {RecordMeterBarProps} props - Props
  * @returns {React.ReactElement} - JSX element
  */
-const RecordMeterBar: React.FC< RecordMeterBarProps > = ( { totalCount, items = [] } ) => {
+const RecordMeterBar: React.FC< RecordMeterBarProps > = ( {
+	totalCount,
+	items = [],
+	showLegendLabelBeforeCount = false,
+} ) => {
 	const total = useMemo( () => {
 		// If total count is not given, then compute it from items' count
 		return (
@@ -67,8 +75,20 @@ const RecordMeterBar: React.FC< RecordMeterBarProps > = ( { totalCount, items = 
 									className="record-meter-bar__legend--item-circle"
 									style={ { backgroundColor } }
 								/>
-								<span className="record-meter-bar__legend--item-count">{ count }</span>
-								<span className="record-meter-bar__legend--item-label">{ label }</span>
+								{ ! showLegendLabelBeforeCount && (
+									<span>
+										<span className="record-meter-bar__legend--item-count">{ count }</span>
+										<span className="record-meter-bar__legend--item-label">{ label }</span>
+									</span>
+								) }
+								{ showLegendLabelBeforeCount && (
+									<span>
+										<span className="record-meter-bar__legend--item-label record-meter-bar__legend--item-label-first">
+											{ label }
+										</span>
+										<span className="record-meter-bar__legend--item-count">({ count })</span>
+									</span>
+								) }
 							</li>
 						);
 					} ) }

projects/js-packages/components/components/record-meter-bar/stories/index.tsx
@@ -28,3 +28,9 @@ WithTotalCount.args = {
 	...DefaultArgs,
 	totalCount: 200,
 };
+
+export const LabelBeforeCount = Template.bind( {} );
+LabelBeforeCount.args = {
+	...DefaultArgs,
+	showLegendLabelBeforeCount: true,
+};

projects/js-packages/components/components/record-meter-bar/style.scss
@@ -1,35 +1,38 @@
 .record-meter-bar {
-    padding-block: 1em;
+	padding-block: 1em;
 
-    &__items {
-        height: 2rem;
-        display: flex;
-        border-radius: 1rem;
-        overflow: hidden;
-        margin-bottom: 2em;
-        background-color: var(--jp-gray-off);
-    }
+	&__items {
+		height: 2rem;
+		display: flex;
+		border-radius: 1rem;
+		overflow: hidden;
+		margin-bottom: 2em;
+		background-color: var(--jp-gray-off);
+	}
 
-    &__legend {
-        &--items {
-            display: flex;
-            margin: 0px;
-        }
-        &--item {
-            display: flex;
-            margin: 0px;
-            margin-inline-end: 1em;
-            align-items: center;
-        }
-        &--item-circle {
-            width: 1rem;
-            height: 1rem;
-            display: inline-block;
-            margin-inline-end: 0.4em;
-            border-radius: 100%;
-        }
-        &--item-count {
-            margin-inline-end: 0.4em;
-        }
-    }
+	&__legend {
+		&--items {
+			display: flex;
+			margin: 0;
+		}
+		&--item {
+			display: flex;
+			margin: 0;
+			margin-inline-end: 1em;
+			align-items: center;
+		}
+		&--item-circle {
+			width: 1rem;
+			height: 1rem;
+			display: inline-block;
+			margin-inline-end: 0.4em;
+			border-radius: 100%;
+		}
+		&--item-count {
+			margin-inline-end: 0.4em;
+		}
+		&--item-label-first {
+			margin-inline-end: 0.4em;
+		}
+	}
 }

commit eb821d513a903378cd52941a1ba9635c249a62fe

Add TS check to build process (#24329)
* Add TS check to build step for js-packages/components

* Use tsconfig in order to use tsc

* Add TS check to build step for plugins/jetpack

* Fix TS errors

* Add changelog files

* Tell TS that we are not passing 'external-link' as variant to WPButton

* [not verified] Revert "Tell TS that we are not passing 'external-link' as variant to WPButton"

This reverts commit 36b7295350bffa7b4df063ea43288bc8050f84a5.

* Add global declaration file to avoid adding it at too many places

* Remove unused module that results in TS error

* Add TS check to build step for plugins/boost

* Fix versions

* Fix the types broken in #24474

pnpm-lock.yaml
@@ -1200,6 +1200,7 @@ importers:
       '@wordpress/components': 19.10.0
       '@wordpress/element': 4.6.0
       '@wordpress/i18n': 4.8.0
+      concurrently: 6.0.2
       history: 5.3.0
       jetpack-boost-critical-css-gen: github:automattic/jetpack-boost-critical-css-gen#release-0.0.4
       node-wp-i18n: 1.2.6
@@ -1243,6 +1244,7 @@ importers:
       '@rollup/plugin-replace': 4.0.0_rollup@2.56.3
       '@rollup/plugin-typescript': 8.2.5_d5b4c212067eb4489bd764c22d3b50b7
       '@wordpress/i18n': 4.8.0
+      concurrently: 6.0.2
       node-wp-i18n: 1.2.6
       npm-run-all: 4.1.5
       postcss: 8.4.13

projects/js-packages/components/components/alert/declaration.d.ts
@@ -1,1 +1,0 @@
-declare module '*.module.scss';

projects/js-packages/components/components/dialog/declaration.d.ts
@@ -1,1 +1,0 @@
-declare module '*.module.scss';

projects/js-packages/components/components/record-meter-bar/index.tsx
@@ -32,7 +32,7 @@ export type RecordMeterBarProps = {
 	/**
 	 * The formatting style for legend item display. If not provided, it defaults to showing legend label after count
 	 */
-	showLegendLabelBeforeCount: boolean;
+	showLegendLabelBeforeCount?: boolean;
 };
 
 /**

projects/plugins/jetpack/_inc/client/components/section-nav/docs/example.jsx
@@ -167,7 +167,7 @@ class SectionNavigation extends React.PureComponent {
 		}.bind( this );
 	}
 
-	demoSearch: () => {};
+	demoSearch() {}
 }
 
 export default SectionNavigation;

projects/plugins/jetpack/_inc/client/components/select-dropdown/docs/example.jsx
@@ -123,7 +123,7 @@ class SelectDropdownDemo extends React.PureComponent {
 		} );
 	}
 
-	onDropdownSelect: () => {};
+	onDropdownSelect() {}
 }
 
 export default SelectDropdownDemo;

projects/plugins/jetpack/_inc/client/lib/jp-redirect/index.js
@@ -1,7 +1,0 @@
-/**
- * @since 8.5.0
- * @deprecated
- * @see @automattic/jetpack-components
- * @returns {string} The redirect URL
- */
-export { getRedirectUrl as default } from '@automattic/jetpack-components';