924e313be448343a3cbb600cf97d759b2fa9763c
Manzoor Wani <manzoorwani.jk@gmail.com>
2022-05-30 15:51:30 +0530

    Convert JetpackLogo tests to TypeScript and get rid of enzyme

---
+++ projects/js-packages/components/components/jetpack-logo/test/component.tsx
@@ -0,0 +1,32 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+import { render } from '@testing-library/react';
+
+/**
+ * Internal dependencies
+ */
+import JetpackLogo from '../index';
+
+describe( 'JetpackLogo', () => {
+	const testProps = {
+		className: 'sample-classname',
+	};
+
+	describe( 'Render the JetpackLogo component', () => {
+		it( 'component exists', () => {
+			const { container } = render( <JetpackLogo { ...testProps } /> );
+
+			expect( container.firstElementChild ).to.be.an.instanceof( SVGSVGElement );
+		} );
+
+		it( 'validate the class name', () => {
+			const { container } = render( <JetpackLogo { ...testProps } /> );
+
+			expect( container.firstElementChild.getAttribute( 'class' ) ).to.include(
+				testProps.className
+			);
+		} );
+	} );
+} );
