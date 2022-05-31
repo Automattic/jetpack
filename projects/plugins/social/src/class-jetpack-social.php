119a5d3bf3ee85640abf17ad5fcd1e75efcae57b
Daniel Post <daniel.post@automattic.com>
2022-05-31 16:00:52 +0200

    [not verified] Add missing exit

--- projects/plugins/social/src/class-jetpack-social.php
+++ projects/plugins/social/src/class-jetpack-social.php
@@ -353,6 +353,7 @@ class Jetpack_Social {
 		delete_option( self::JETPACK_SOCIAL_ACTIVATION_OPTION );
 		( new Modules() )->activate( self::JETPACK_PUBLICIZE_MODULE_SLUG, false, false );
 		wp_safe_redirect( esc_url( admin_url( 'admin.php?page=' . JETPACK_SOCIAL_PLUGIN_SLUG ) ) );
+			exit;
 	}
 
 	/**
