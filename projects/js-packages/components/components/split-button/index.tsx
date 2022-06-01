ff307c0bd705feba87245f7fc119bad91c16ec4d
Manzoor Wani <manzoorwani.jk@gmail.com>
2022-06-01 21:46:11 +0530

    Fix lint

--- projects/js-packages/components/components/split-button/index.tsx
+++ projects/js-packages/components/components/split-button/index.tsx
@@ -1,7 +1,7 @@
-import type React from 'react';
 import { ButtonGroup, Button, DropdownMenu } from '@wordpress/components';
 import styles from './style.module.scss';
 import { SplitButtonProps } from './types';
+import type React from 'react';
 
 const DownIcon = () => (
 	<svg width="15" height="9" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="10 9 4 7">
