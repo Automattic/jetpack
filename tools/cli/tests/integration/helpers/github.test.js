4099b0a0ede0919c1a63f6173a89a8384dcf9cfa
Brad Jorsch <anomiex@users.noreply.github.com>
2022-06-06 09:28:32 -0400

    Apply suggestions from code review
    Co-authored-by: Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>

--- tools/cli/tests/integration/helpers/github.test.js
+++ tools/cli/tests/integration/helpers/github.test.js
@@ -5,10 +5,10 @@ describe.skip( 'doesRepoExist Integration Tests', function () {
 	test( 'checks for an existing mirror repo', async () => {
 		await expect( doesRepoExist( 'jetpack' ) ).resolves.toBe( true );
 	}, 60000 );
-	it( 'checks for an non-existent repo', async () => {
+	test( 'checks for an non-existent repo', async () => {
 		await expect( doesRepoExist( 'jetpack-zzz-test-not-exist' ) ).resolves.toBe( false );
 	}, 60000 );
-	it( 'checks for an existent private repo', async () => {
+	test( 'checks for an existent private repo', async () => {
 		await expect( doesRepoExist( 'jpop-issues' ) ).resolves.toBe( true );
 	}, 60000 );
 } );
