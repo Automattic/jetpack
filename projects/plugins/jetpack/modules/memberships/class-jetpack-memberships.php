Automattic/jetpack

commit 61a7b7b81834321d48a060f4e72f786e694775a1

Payments Block: Default URL opens the corresponding form (#23487)

projects/plugins/jetpack/modules/memberships/class-jetpack-memberships.php
@@ -312,7 +312,7 @@ class Jetpack_Memberships {
 			$content       = str_replace( 'recurring-payments-id', $block_id, $content );
 			$content       = str_replace( 'wp-block-jetpack-recurring-payments', 'wp-block-jetpack-recurring-payments wp-block-button', $content );
 			$subscribe_url = $this->get_subscription_url( $plan_id );
-			return str_replace( 'href="#"', 'href="' . $subscribe_url . '"', $content );
+			return preg_replace( '/(href=".*")/', 'href="' . $subscribe_url . '"', $content );
 		}
 
 		return $this->deprecated_render_button_v1( $attributes, $plan_id );

projects/plugins/jetpack/extensions/blocks/recurring-payments/edit.jsx
@@ -338,7 +338,14 @@ export class MembershipsButtonEdit extends Component {
 			} );
 		}
 
-		return setAttributes( { planId: parseInt( id ) } );
+		const planId = parseInt( id );
+		this.props.postLink.searchParams.set( 'recurring_payments', planId.toString() );
+		return setAttributes( {
+			planId,
+			// We need to use an absolute URL otherwise the reader won't be able to open the post.
+			url: this.props.postLink.toString(),
+			uniqueId: `recurring-payments-${ planId }`,
+		} );
 	};
 
 	renderMembershipAmounts = () => (
@@ -499,7 +506,10 @@ export class MembershipsButtonEdit extends Component {
 							'jetpack/button',
 							{
 								element: 'a',
-								uniqueId: 'recurring-payments-id',
+								passthroughAttributes: {
+									uniqueId: 'uniqueId',
+									url: 'url',
+								},
 							},
 						],
 					] }
@@ -515,6 +525,7 @@ export class MembershipsButtonEdit extends Component {
 export default compose( [
 	withSelect( select => ( {
 		postId: select( 'core/editor' ).getCurrentPostId(),
+		postLink: new URL( select( 'core/editor' ).getCurrentPost().link ),
 	} ) ),
 	withDispatch( dispatch => {
 		const { updateBlockAttributes } = dispatch( 'core/editor' );

projects/plugins/jetpack/extensions/blocks/recurring-payments/index.js
@@ -88,6 +88,16 @@ export const settings = {
 		align: {
 			type: 'string',
 		},
+		url: {
+			type: 'string',
+			// Used for blocks created without the payment form auto open feature.
+			default: '#',
+		},
+		uniqueId: {
+			type: 'string',
+			// Used for blocks created without the payment form auto open feature.
+			default: 'id',
+		},
 	},
 	edit,
 	save: ( { className } ) => (

projects/plugins/jetpack/extensions/blocks/recurring-payments/test/edit.js
@@ -47,6 +47,7 @@ describe( 'MembershipsButtonEdit', () => {
 		autosaveAndRedirect,
 		clientId: 1,
 		postId: 1,
+		postLink: new URL( 'https://anyposturl.com/' ),
 		noticeList: [],
 		isSelected: true,
 		context: {},
@@ -123,14 +124,28 @@ describe( 'MembershipsButtonEdit', () => {
 
 	describe( 'when a plan has been selected', () => {
 		test( 'the Payment Plan form does not display', async () => {
-			const props = { ...defaultProps, attributes: { planId: 1 } };
+			const props = {
+				...defaultProps,
+				attributes: {
+					planId: 1,
+					uniqueId: 'recurring-payments-1',
+					url: 'https://anyposturl.com/?recurring_payments=1',
+				},
+			};
 			render( <MembershipsButtonEdit { ...props } /> );
 
 			await waitFor( () => expect( screen.queryByText( 'Payments' ) ).not.toBeInTheDocument() );
 		} );
 
 		test( 'the Upgrade nudge does not display', async () => {
-			const props = { ...defaultProps, attributes: { planId: 1 } };
+			const props = {
+				...defaultProps,
+				attributes: {
+					planId: 1,
+					uniqueId: 'recurring-payments-1',
+					url: 'https://anyposturl.com/?recurring_payments=1',
+				},
+			};
 			render( <MembershipsButtonEdit { ...props } /> );
 
 			await waitFor( () =>
@@ -222,9 +237,21 @@ describe( 'MembershipsButtonEdit', () => {
 			const { rerender } = render( <MembershipsButtonEdit { ...defaultProps } /> );
 
 			await waitFor( () => userEvent.click( screen.getByText( '$10.00 / month' ) ) );
-			expect( setAttributes ).toHaveBeenCalledWith( { planId: 1 } );
+			expect( setAttributes ).toHaveBeenCalledWith( {
+				planId: 1,
+				uniqueId: 'recurring-payments-1',
+				url: 'https://anyposturl.com/?recurring_payments=1',
+			} );
 
-			const props = { ...defaultProps, attributes: { ...defaultAttributes, planId: 1 } };
+			const props = {
+				...defaultProps,
+				attributes: {
+					...defaultAttributes,
+					planId: 1,
+					uniqueId: 'recurring-payments-1',
+					url: 'https://anyposturl.com/?recurring_payments=1',
+				},
+			};
 			rerender( <MembershipsButtonEdit { ...props } /> );
 
 			expect( screen.queryByText( 'Payments' ) ).not.toBeInTheDocument();
@@ -325,9 +352,23 @@ describe( 'MembershipsButtonEdit', () => {
 
 				userEvent.click( screen.getByText( 'Add this payment plan' ) );
 
-				await waitFor( () => expect( setAttributes ).toHaveBeenCalledWith( { planId: 1 } ) );
+				await waitFor( () =>
+					expect( setAttributes ).toHaveBeenCalledWith( {
+						planId: 1,
+						uniqueId: 'recurring-payments-1',
+						url: 'https://anyposturl.com/?recurring_payments=1',
+					} )
+				);
 
-				const props = { ...defaultProps, attributes: { ...defaultAttributes, planId: 1 } };
+				const props = {
+					...defaultProps,
+					attributes: {
+						...defaultAttributes,
+						planId: 1,
+						uniqueId: 'recurring-payments-1',
+						url: 'https://anyposturl.com/?recurring_payments=1',
+					},
+				};
 				rerender( <MembershipsButtonEdit { ...props } /> );
 
 				expect( screen.queryByText( 'Payments' ) ).not.toBeInTheDocument();

projects/plugins/jetpack/extensions/blocks/recurring-payments/test/fixtures/jetpack__recurring-payments--with-no-url.html
@@ -1,0 +1,3 @@
+<!-- wp:jetpack/recurring-payments {"planId":12,"align":"right"} -->
+<div class="wp-block-jetpack-recurring-payments"><!-- wp:jetpack/button {"element":"a","uniqueId":"recurring-payments-id","text":"Payment","borderRadius":0} /--></div>
+<!-- /wp:jetpack/recurring-payments -->

projects/plugins/jetpack/extensions/blocks/recurring-payments/test/fixtures/jetpack__recurring-payments--with-no-url.serialized.html
@@ -1,0 +1,3 @@
+<!-- wp:jetpack/recurring-payments {"planId":12,"align":"right"} -->
+<div class="wp-block-jetpack-recurring-payments"><!-- wp:jetpack/button {"element":"a","uniqueId":"recurring-payments-id","text":"Payment","borderRadius":0} /--></div>
+<!-- /wp:jetpack/recurring-payments -->

projects/plugins/jetpack/extensions/blocks/recurring-payments/test/fixtures/jetpack__recurring-payments.html
@@ -1,3 +1,3 @@
-<!-- wp:jetpack/recurring-payments {"planId":12,"align":"right"} -->
-<div class="wp-block-jetpack-recurring-payments"><!-- wp:jetpack/button {"element":"a","uniqueId":"recurring-payments-id","text":"Payment","borderRadius":0} /--></div>
+<!-- wp:jetpack/recurring-payments {"planId":12,"align":"right","url":"?open_payment_plan=12","uniqueId":"recurring-payments-12"} -->
+<div class="wp-block-jetpack-recurring-payments"><!-- wp:jetpack/button {"element":"a","uniqueId":"recurring-payments-12","text":"Payment","url":"?open_payment_plan=12","borderRadius":0} /--></div>
 <!-- /wp:jetpack/recurring-payments -->

projects/plugins/jetpack/extensions/blocks/recurring-payments/test/fixtures/jetpack__recurring-payments.serialized.html
@@ -1,3 +1,3 @@
-<!-- wp:jetpack/recurring-payments {"planId":12,"align":"right"} -->
-<div class="wp-block-jetpack-recurring-payments"><!-- wp:jetpack/button {"element":"a","uniqueId":"recurring-payments-id","text":"Payment","borderRadius":0} /--></div>
+<!-- wp:jetpack/recurring-payments {"planId":12,"align":"right","url":"?open_payment_plan=12","uniqueId":"recurring-payments-12"} -->
+<div class="wp-block-jetpack-recurring-payments"><!-- wp:jetpack/button {"element":"a","uniqueId":"recurring-payments-12","text":"Payment","url":"?open_payment_plan=12","borderRadius":0} /--></div>
 <!-- /wp:jetpack/recurring-payments -->

commit 40e6c543ca579eea5c1d39ab0cbd97af230d6100

Fix bug preventing images in payment buttons
Inline images didn't work in payment buttons - the image wasn't rendered
at all, and neither was any accompanying text.

This was happening due to a small bug introduced in #23487 where the
regular expression wasn't just replacing the href attribute and the "
surrounding it's values, but all of the content between the " opening
the href value and the last " in the user-supplied content.

This commit fixes it by using an ungreedy regex, which should mean just
the href attribute and it's delimiters are matched.

Fixes #24662

projects/plugins/jetpack/modules/memberships/class-jetpack-memberships.php
@@ -312,7 +312,7 @@ class Jetpack_Memberships {
 			$content       = str_replace( 'recurring-payments-id', $block_id, $content );
 			$content       = str_replace( 'wp-block-jetpack-recurring-payments', 'wp-block-jetpack-recurring-payments wp-block-button', $content );
 			$subscribe_url = $this->get_subscription_url( $plan_id );
-			return preg_replace( '/(href=".*")/', 'href="' . $subscribe_url . '"', $content );
+			return preg_replace( '/(href=".*")/U', 'href="' . $subscribe_url . '"', $content );
 		}
 
 		return $this->deprecated_render_button_v1( $attributes, $plan_id );