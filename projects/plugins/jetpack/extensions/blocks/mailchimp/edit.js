Automattic/jetpack

commit 9d290b438a1f7dac31c9d9414c66dcfc075260df

Mailchimp block - improve loading state (#24694)
* Mailchimp block: center spinner

* changelog

* Add Mailchimp label

projects/plugins/jetpack/extensions/blocks/mailchimp/edit.js
@@ -118,8 +118,15 @@ class MailchimpSubscribeEdit extends Component {
 		} = attributes;
 		const classPrefix = 'wp-block-jetpack-mailchimp';
 		const waiting = (
-			<Placeholder icon={ icon } notices={ notices }>
-				<Spinner />
+			<Placeholder
+				icon={ icon }
+				notices={ notices }
+				className="wp-block-jetpack-mailchimp"
+				label={ __( 'Mailchimp', 'jetpack' ) }
+			>
+				<div class="align-center">
+					<Spinner />
+				</div>
 			</Placeholder>
 		);
 		const placeholder = (

projects/plugins/jetpack/extensions/blocks/mailchimp/editor.scss
@@ -42,6 +42,9 @@
 		.components-button {
 			margin-bottom: 0;
 		}
-	}
 
+		.align-center {
+			text-align: center;
+		}
+	}
 }

commit bc48cf741334cd3f4b5cc004ae2fd335fa282257

Fix new error introduced in #24694

projects/plugins/jetpack/extensions/blocks/mailchimp/edit.js
@@ -124,7 +124,7 @@ class MailchimpSubscribeEdit extends Component {
 				className="wp-block-jetpack-mailchimp"
 				label={ __( 'Mailchimp', 'jetpack' ) }
 			>
-				<div class="align-center">
+				<div className="align-center">
 					<Spinner />
 				</div>
 			</Placeholder>