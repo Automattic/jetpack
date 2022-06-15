54ef81e353f7ed91e812f10fb71d6a07a5b30301
Brad Jorsch <brad.jorsch@automattic.com>
2022-06-15 14:45:14 -0400

    Simplify jest config
    Apparently no need to explicitly disable the transform

--- projects/js-packages/analytics/jest.config.cjs
+++ projects/js-packages/analytics/jest.config.cjs
@@ -2,11 +2,5 @@ const baseConfig = require( 'jetpack-js-tools/jest/config.base.js' );
 
 module.exports = {
 	...baseConfig,
-	transform: {
-		...baseConfig.transform,
-	},
 	extensionsToTreatAsEsm: [ '.jsx' ],
 };
-
-// Disable default esm transform.
-delete module.exports.transform[ '\\.[jt]sx?$' ];
