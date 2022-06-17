1b2e2623e85da02172c99a89311b18a1317802d4
Leo Germani <leogermani@automattic.com>
2022-06-17 18:20:36 -0300

    display alert when we cant install the plugin

--- projects/packages/my-jetpack/_inc/components/product-detail-button/index.js
+++ projects/packages/my-jetpack/_inc/components/product-detail-button/index.js
@@ -2,14 +2,22 @@ import { Button, Spinner } from '@wordpress/components';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const ProductDetailButton = ( { children, className, href, isLoading, onClick, isPrimary } ) => {
+const ProductDetailButton = ( {
+	children,
+	className,
+	href,
+	isLoading,
+	onClick,
+	isPrimary,
+	disabled,
+} ) => {
 	return (
 		<Button
 			onClick={ onClick }
 			className={ className }
 			href={ href }
 			variant={ isPrimary ? 'primary' : 'secondary' }
-			disabled={ isLoading }
+			disabled={ isLoading || disabled }
 		>
 			{ isLoading ? <Spinner /> : children }
 		</Button>
@@ -20,11 +28,13 @@ ProductDetailButton.propTypes = {
 	className: PropTypes.string,
 	isLoading: PropTypes.bool,
 	isPrimary: PropTypes.bool,
+	disabled: PropTypes.bool,
 };
 
 ProductDetailButton.defaultProps = {
 	isLoading: false,
 	isPrimary: true,
+	disabled: false,
 };
 
 export default ProductDetailButton;
