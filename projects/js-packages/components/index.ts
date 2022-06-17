4bb25d1c895f4a5255b7bdef00c8b684279c5475
Leo Germani <leogermani@automattic.com>
2022-06-17 18:18:31 -0300

    Export Alert component

--- projects/js-packages/components/index.ts
+++ projects/js-packages/components/index.ts
@@ -41,4 +41,5 @@ export { default as ProductPrice } from './components/product-price';
 export { default as ProductOffer, IconsCard } from './components/product-offer';
 export { default as Dialog } from './components/dialog';
 export { default as RecordMeterBar } from './components/record-meter-bar';
+export { default as Alert } from './components/alert';
 export { getUserLocale, cleanLocale } from './lib/locale';
