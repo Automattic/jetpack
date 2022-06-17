d75885832ade72cc42a6489356a5559221e5a415
Robert Sreberski <robertsreberski@gmail.com>
2022-06-17 14:32:22 +0200

    Add video to one-click-restores recommendation

--- projects/plugins/jetpack/_inc/client/recommendations/sidebar/one-click-restores/index.jsx
+++ projects/plugins/jetpack/_inc/client/recommendations/sidebar/one-click-restores/index.jsx
@@ -1,4 +1,4 @@
-import { imagePath } from 'constants/urls';
+import { imagePath, GETTING_STARTED_WITH_JETPACK_BACKUP_VIDEO_URL } from 'constants/urls';
 import { getRedirectUrl } from '@automattic/jetpack-components';
 import { __, sprintf } from '@wordpress/i18n';
 import Button from 'components/button';
@@ -26,6 +26,12 @@ const OneClickRestoresComponent = props => {
 		} );
 	}, [] );
 
+	const trackOpenVideo = useCallback( () => {
+		analytics.tracks.recordEvent( 'jetpack_backup_getting_started_video_click', {
+			position: 'recommendations/one_click_restores',
+		} );
+	}, [] );
+
 	/* Avoid ternary as code minification will break translation function. :( */
 	let backupsName = __( 'Daily Backups', 'jetpack' );
 	if ( hasRealTimeBackups ) {
@@ -33,39 +39,67 @@ const OneClickRestoresComponent = props => {
 	}
 
 	return (
-		<Layout
-			illustrationPath={ imagePath + '/recommendations/one-click-restores.svg' }
-			content={
-				<div className="jp-recommendations-one-click-restores">
-					<h2>{ __( 'Enable one-click restores', 'jetpack' ) }</h2>
-					<p>
-						{ sprintf(
-							/* translators: placeholder is the name of a backups plan: Daily Backups or Real-time Backups */
-							__(
-								'Get the most out of your %s. One-click restores ensure you’ll be able to easily restore your site, if anything goes wrong.',
+		<>
+			<Layout
+				illustrationPath={ imagePath + '/recommendations/one-click-restores.svg' }
+				content={
+					<div className="jp-recommendations-one-click-restores">
+						<h2>{ __( 'Enable one-click restores', 'jetpack' ) }</h2>
+						<p>
+							{ sprintf(
+								/* translators: placeholder is the name of a backups plan: Daily Backups or Real-time Backups */
+								__(
+									'Get the most out of your %s. One-click restores ensure you’ll be able to easily restore your site, if anything goes wrong.',
+									'jetpack'
+								),
+								backupsName
+							) }
+						</p>
+						<p>
+							{ __(
+								'Enter your server credentials to enable one-click restores included in your plan.',
 								'jetpack'
-							),
-							backupsName
-						) }
-					</p>
-					<p>
-						{ __(
-							'Enter your server credentials to enable one-click restores included in your plan.',
-							'jetpack'
-						) }
-					</p>
-					<div className="jp-recommendations-one-click-restores__cta">
-						<Button
-							rna
-							href={ getRedirectUrl( 'jetpack-backup-dash-credentials', { site: siteRawUrl } ) }
-							onClick={ onCtaClick }
-						>
-							{ __( 'Enable one-click restores', 'jetpack' ) }
-						</Button>
+							) }
+						</p>
+						<div className="jp-recommendations-one-click-restores__cta">
+							<Button
+								rna
+								href={ getRedirectUrl( 'jetpack-backup-dash-credentials', { site: siteRawUrl } ) }
+								onClick={ onCtaClick }
+							>
+								{ __( 'Enable one-click restores', 'jetpack' ) }
+							</Button>
+						</div>
 					</div>
-				</div>
-			}
-		/>
+				}
+			/>
+			<Layout
+				compact
+				content={
+					<div className="jp-recommendations-getting-started-compact">
+						<a
+							href={ GETTING_STARTED_WITH_JETPACK_BACKUP_VIDEO_URL }
+							onClick={ trackOpenVideo }
+							target="_blank"
+							rel="noreferrer"
+						>
+							<img
+								className="jp-recommendations-getting-started-compact__thumbnail"
+								src={ imagePath + 'backup-getting-started-thumbnail.png' }
+								srcSet={ `${ imagePath + 'backup-getting-started-thumbnail.png' } 1x, ${
+									imagePath + 'backup-getting-started-thumbnail-2x.png'
+								} 2x` }
+								alt={ __( 'Getting started with Jetpack Backup video thumbnail', 'jetpack' ) }
+							/>
+						</a>
+						<div>
+							<h2>{ __( 'Getting started with Jetpack Backup', 'jetpack' ) }</h2>
+							<p>{ __( 'A short video guide on how to back up your website', 'jetpack' ) }</p>
+						</div>
+					</div>
+				}
+			/>
+		</>
 	);
 };
 
