967fd4d1fdfa80d2da25cbdafdab946e4f876346
spsiddarthan <siddarthan@outlook.com>
2022-06-22 12:51:35 +0530

    Change the UI to get rid of the meter

--- projects/plugins/social/src/js/components/header/index.js
+++ projects/plugins/social/src/js/components/header/index.js
@@ -1,12 +1,9 @@
 import { Container, Col, H3, Text } from '@automattic/jetpack-components';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { createInterpolateElement } from '@wordpress/element';
-import { __ } from '@wordpress/i18n';
-import { Icon, edit, lifesaver } from '@wordpress/icons';
-import classnames from 'classnames';
+import { __, sprintf } from '@wordpress/i18n';
+import { Icon, edit } from '@wordpress/icons';
 import { STORE_ID } from '../../store';
-import ShareCounter from '../share-counter';
-import illustration from './illustration.svg';
 import styles from './styles.module.scss';
 
 const Actions = ( { actions } ) => (
@@ -21,45 +18,31 @@ const Actions = ( { actions } ) => (
 		) ) }
 	</div>
 );
-
-const SideColumn = ( { hasConnections, sharesCount } ) => {
-	if ( ! hasConnections ) {
-		return <img src={ illustration } alt="" />;
-	}
-
-	return (
-		<div className={ styles.column }>
-			<ShareCounter sharesCount={ sharesCount } />
-		</div>
-	);
-};
-
 const Header = () => {
 	useDispatch( STORE_ID ).getSharesCount();
-	const { hasConnections, hasPaidUpgrade, sharesCountResponse } = useSelect( select => {
+	const { sharesCountResponse } = useSelect( select => {
 		const store = select( STORE_ID );
 		return {
-			hasConnections: store.hasConnections(),
-			hasPaidUpgrade: store.hasPaidUpgrade(),
 			sharesCountResponse: store.getSharesCount(),
 		};
 	} );
 	const sharesCountLoaded = sharesCountResponse && sharesCountResponse.results;
 	const sharesCount = sharesCountLoaded ? sharesCountResponse.results.total : 0;
-	const columnClassname = classnames( {
-		[ styles.illustration ]: ! hasConnections,
-	} );
-
-	const text = createInterpolateElement(
-		__(
-			'Share your WordPress posts up to 30 times in your social media for free each month. <link>Learn more</link>',
-			'jetpack-social'
-		),
-		{
-			// TODO: Add proper link (with Jetpack Redirect)
-			link: <a href="" />,
-		}
-	);
+	const text = sharesCountLoaded
+		? createInterpolateElement(
+				sprintf(
+					// translators: %1$d is the number of shares used.
+					__(
+						'You’ve made <boldText>%1$d</boldText> shares over the past 30 days.',
+						'jetpack-social'
+					),
+					sharesCount
+				),
+				{
+					boldText: <strong />,
+				}
+		  )
+		: __( 'Computing your shares', 'jetpack-social' );
 
 	// TODO: Add real links
 	const actions = [
@@ -69,32 +52,15 @@ const Header = () => {
 			icon: edit,
 		},
 	];
-
-	if ( hasPaidUpgrade ) {
-		actions.push( { link: '', label: __( 'Need help?', 'jetpack-social' ), icon: lifesaver } );
-	}
-
-	return sharesCountLoaded ? (
+	return (
 		<Container horizontalSpacing={ 3 } horizontalGap={ 7 } className={ styles.container }>
 			<Col sm={ 4 } md={ 4 } lg={ 5 }>
-				<H3 mt={ 2 }>
-					{ hasPaidUpgrade
-						? __( 'Post everywhere at any time', 'jetpack-social' )
-						: __( 'Post everywhere', 'jetpack-social' ) }
-				</H3>
-				{ ! hasPaidUpgrade && <Text className={ styles.title }>{ text }</Text> }
+				<H3 mt={ 2 }>{ __( 'Post everywhere', 'jetpack-social' ) }</H3>
+				{ <Text className={ styles.title }>{ text }</Text> }
 				<Actions actions={ actions } />
 			</Col>
-			<Col
-				sm={ 4 }
-				md={ 4 }
-				lg={ { start: hasPaidUpgrade ? 7 : 6, end: 12 } }
-				className={ columnClassname }
-			>
-				<SideColumn hasConnections={ hasConnections } sharesCount={ sharesCount } />
-			</Col>
 		</Container>
-	) : null;
+	);
 };
 
 export default Header;
