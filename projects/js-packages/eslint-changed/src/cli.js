Automattic/jetpack

commit 66c64222964f8d59bb2da554f0ed02967867503f

eslint-changed: Change to esm and use jest for tests (#24626)
The ESM switch was very straightforward, all that was needed was to swap
`require` for `import` and replace some uses of `__dirname` in the tests.

The switch to `jest` was a little more complicated:

* The first part was simple enough, once `--experimental-vm-modules` was
  added then it worked without even disabling code transforms.
* But jest's resolver choked on a fancy feature in the new version of
  chalk, so a workaround had to be added for that. Sigh.
* And then getting coverage working required a restructure of all the
  code, because jest won't record coverage for code in child processes.

And then when I was getting all the tests to work with the restructured
code, I noticed that "Filters by filename" was testing for incorrect
behavior (it wasn't actually filtering). So I had to fix that too.

projects/js-packages/eslint-changed/src/cli.js
@@ -1,0 +1,362 @@
+import { spawnSync } from 'child_process';
+import fs from 'fs';
+import path from 'path';
+import chalk from 'chalk';
+import { Command } from 'commander';
+import { ESLint } from 'eslint';
+import parseDiff from 'parse-diff';
+
+const APP_VERSION = '2.0.3-alpha';
+
+/**
+ * Create a Commander instance.
+ *
+ * Call `.parseAsync()` to run.
+ *
+ * @param {object} [process] - Process object. Needs at least a `cwd()` method and an `env` property.
+ * @returns {Command} Commander instance.
+ */
+export function createProgram( process = global.process ) {
+	const program = new Command();
+	program
+		.usage(
+			'Run ESLint on files and only report new warnings/errors compared to the previous version.'
+		)
+
+		.option( '--diff <file>', 'A file containing a unified diff of the changes.' )
+		.option(
+			'--diff-base <dir>',
+			'Base directory the diff is relative to. Defaults to the current directory.',
+			v => path.resolve( process.cwd(), v )
+		)
+		.option(
+			'--eslint-orig <file>',
+			'A file containing the JSON output of ESLint on the unchanged files.'
+		)
+		.option(
+			'--eslint-new <file>',
+			'A file containing the JSON output of ESLint on the changed files.'
+		)
+
+		.option(
+			'--git',
+			"Assume git-versioned files. Set environment variables GIT and ESLINT to point to appropriate commands if they're not already in the path."
+		)
+		.option(
+			'--git-staged',
+			'Compare the staged version to the HEAD version (this is the default).'
+		)
+		.option( '--git-unstaged', 'Compare the working copy version to the staged (or HEAD) version.' )
+		.option(
+			'--git-base <ref>',
+			'Compare the HEAD version to the HEAD of a different base (e.g. branch).'
+		)
+
+		.option( '--debug', 'Enable debug output.' )
+		.option(
+			'--ext <list>',
+			'Comma-separated list of JavaScript file extensions. Ignored if files are listed.',
+			'.js'
+		)
+		.option(
+			'--in-diff-only',
+			'Only include messages on lines changed in the diff. This may miss things like deleting a `var` that leads to a new `no-undef` elsewhere.'
+		)
+		.option( '--format <name>', 'ESLint format to use for output.', 'stylish' )
+		.version( APP_VERSION )
+		.action( main.bind( program, process ) );
+
+	return program;
+}
+
+/**
+ * Main method.
+ *
+ * @param {object} process - Process object.
+ * @param {object} argv - Command line options.
+ * @param {Command} program - Commander instance.
+ */
+async function main( process, argv, program ) {
+	if ( argv.diff !== undefined && argv.git ) {
+		program.error( 'error: options `--diff` and `--git` are mutually exclusive', { exitCode: 1 } );
+	}
+
+	[
+		[ 'diff', 'eslint-orig' ],
+		[ 'diff', 'eslint-new' ],
+		[ 'diff-base', 'diff' ],
+		[ 'eslint-orig', 'diff' ],
+		[ 'eslint-new', 'diff' ],
+		[ 'git-staged', 'git' ],
+		[ 'git-unstaged', 'git' ],
+		[ 'git-base', 'git' ],
+	].forEach( x => {
+		const [ arg1, arg2 ] = x;
+		const prop1 = arg1.replace( /-[a-z]/g, v => v[ 1 ].toUpperCase() );
+		const prop2 = arg2.replace( /-[a-z]/g, v => v[ 1 ].toUpperCase() );
+		if ( argv[ prop1 ] !== undefined && argv[ prop2 ] === undefined ) {
+			program.error( `error: option \`--${ arg1 }\` requires option \`--${ arg2 }\``, {
+				exitCode: 1,
+			} );
+		}
+	} );
+
+	const writeOut = program.configureOutput().writeOut;
+	const debug = argv.debug ? ( ...m ) => writeOut( chalk.grey( ...m ) ) : () => {};
+
+	/**
+	 * Get files from a diff.
+	 *
+	 * @param {Array} diff - Diff array from `parse-diff`.
+	 * @returns {string[]} File name strings.
+	 */
+	function getFilesFromDiff( diff ) {
+		let files = diff.map( x => x.to );
+		if ( program.args.length === 0 ) {
+			const exts = argv.ext.split( ',' );
+			files = files.filter( file => exts.some( ext => file.endsWith( ext ) ) );
+		}
+		return files;
+	}
+
+	const spawnOpt = {
+		stdio: [ null, 'pipe', 'inherit' ],
+		maxBuffer: Infinity,
+		encoding: 'utf8',
+	};
+
+	/**
+	 * Spawn a command, exiting if it fails.
+	 *
+	 * @param {string} cmd - Command to execute.
+	 * @param {string[]} cmdArgs - Arguments to the command.
+	 * @returns {string} Command output.
+	 */
+	function doCmd( cmd, cmdArgs ) {
+		const res = spawnSync( cmd, cmdArgs, spawnOpt );
+		if ( res.status ) {
+			program.error( `error: ${ cmd } exited with status ${ res.status }`, { exitCode: 1 } );
+		}
+		return res.stdout;
+	}
+
+	const eslint = new ESLint();
+	debug( 'Using ESLint version', eslint.version );
+	const formatter = await eslint.loadFormatter( argv.format );
+
+	let diff, diffBase, files, eslintOrig, eslintNew;
+	if ( argv.git ) {
+		const git = process.env.GIT || 'git';
+		let origRef, newRef, args, ret;
+
+		ret = spawnSync( git, [ '--version' ], spawnOpt );
+		if ( ret.error ) {
+			program.error(
+				`error: failed to execute git as \`${ git }\`. Use environment variable \`GIT\` to override.`,
+				1
+			);
+		}
+		debug( 'Using git version', ret.stdout.trim() );
+
+		args = [ 'rev-parse', '--show-toplevel' ];
+		debug( 'Getting git top level:', git, args.join( ' ' ) );
+		diffBase = doCmd( git, args ).trim();
+
+		args = [ 'diff' ];
+		if ( argv.gitBase !== undefined ) {
+			const args2 = [ 'merge-base', argv.gitBase, 'HEAD' ];
+			debug( 'Running git merge-base command:', git, args2.join( ' ' ) );
+			origRef = doCmd( git, args2 ).trim();
+			debug( 'Merge base is:', origRef );
+			newRef = 'HEAD';
+			args.push( `${ origRef }...HEAD` );
+		} else if ( argv.gitUnstaged ) {
+			origRef = ':0';
+			newRef = null;
+		} else {
+			origRef = 'HEAD';
+			newRef = ':0';
+			args.push( '--staged' );
+		}
+		args = args.concat( program.args );
+
+		debug( 'Running git diff command:', git, args.join( ' ' ) );
+		diff = parseDiff( doCmd( git, args ) );
+		if ( ! argv.inDiffOnly && program.args.length ) {
+			files = program.args;
+			debug( 'Determined files from command line:', files );
+		} else {
+			files = getFilesFromDiff( diff );
+			debug( 'Determined files from diff:', files );
+			if ( program.args.length ) {
+				const cmdLineFiles = new Set( program.args );
+				files = files.filter( file => cmdLineFiles.has( file ) );
+				debug( 'Intersected files with those from the command line:', files );
+			}
+		}
+
+		eslintOrig = [];
+		eslintNew = [];
+		for ( const file of files ) {
+			let content;
+
+			args = [ 'cat-file', '-e', origRef + ':' + file ];
+			debug( 'Testing if file is new:', git, args.join( ' ' ) );
+			if ( spawnSync( git, args, { stdio: 'ignore' } ).status ) {
+				debug( "It's new, so no orig ESLint data." );
+			} else {
+				args = [ 'show', origRef + ':' + file ];
+				debug( 'Fetching orig file contents:', git, args.join( ' ' ) );
+				content = doCmd( git, args );
+				debug( 'Executing ESLint for orig file' );
+				ret = await eslint.lintText( content, {
+					filePath: file,
+				} );
+				eslintOrig = eslintOrig.concat( ret );
+			}
+
+			if ( newRef === null ) {
+				content = fs.readFileSync( file, 'utf8' );
+			} else {
+				args = [ 'show', newRef + ':' + file ];
+				debug( 'Fetching new file contents:', git, args.join( ' ' ) );
+				content = doCmd( git, args );
+			}
+			debug( 'Executing ESLint for new file' );
+			ret = await eslint.lintText( content, {
+				filePath: file,
+			} );
+			eslintNew = eslintNew.concat( ret );
+		}
+	} else if ( argv.diff ) {
+		diff = parseDiff( fs.readFileSync( argv.diff, 'utf8' ) );
+		diffBase = argv.diffBase || process.cwd();
+		if ( argv.inDiffOnly ) {
+			files = getFilesFromDiff( diff );
+			debug( 'Determined files from diff:', files );
+			if ( program.args.length ) {
+				const cmdLineFiles = new Set( program.args );
+				files = files.filter( file => cmdLineFiles.has( file ) );
+				debug( 'Intersected files with those from the command line:', files );
+			}
+		} else if ( program.args.length ) {
+			files = program.args;
+			debug( 'Determined files from command line:', files );
+		}
+		eslintOrig = JSON.parse( fs.readFileSync( argv.eslintOrig, 'utf8' ) );
+		eslintNew = JSON.parse( fs.readFileSync( argv.eslintNew, 'utf8' ) );
+	} else {
+		program.help( { error: true } );
+	}
+
+	// oldLines maps line numbers in the old version to the new.
+	// newLines just lists lines present in the diff.
+	const oldLines = {};
+	const newLines = {};
+	diff.forEach( file => {
+		const fileName = path.resolve( diffBase, file.to );
+		const ol = {},
+			nl = {};
+		file.chunks.forEach( chunk => {
+			let p = 0;
+			chunk.changes.forEach( c => {
+				switch ( c.type ) {
+					case 'add':
+						p = c.ln;
+						nl[ c.ln ] = true;
+						break;
+					case 'del':
+						ol[ c.ln ] = ++p;
+						break;
+					case 'normal':
+						p = c.ln2;
+						ol[ c.ln1 ] = c.ln2;
+						nl[ c.ln2 ] = true;
+						break;
+				}
+			} );
+		} );
+		oldLines[ fileName ] = ol;
+		newLines[ fileName ] = nl;
+	} );
+
+	if ( files ) {
+		files = new Set( files.map( file => path.resolve( diffBase, file ) ) );
+		if ( argv.inDiffOnly ) {
+			eslintOrig = eslintOrig.filter( x => files.has( x.filePath ) && oldLines[ x.filePath ] );
+			eslintNew = eslintNew.filter( x => files.has( x.filePath ) && newLines[ x.filePath ] );
+		} else {
+			eslintOrig = eslintOrig.filter( x => files.has( x.filePath ) );
+			eslintNew = eslintNew.filter( x => files.has( x.filePath ) );
+		}
+	}
+
+	const origMsgs = {};
+	eslintOrig.forEach( file => {
+		const lines = {};
+		const oldL = oldLines[ file.filePath ] || {};
+		file.messages.forEach( msg => {
+			let line = msg.line;
+			for ( let i = msg.line; i > 0; i-- ) {
+				if ( oldL[ i ] ) {
+					line = msg.line + oldL[ i ] - i;
+					break;
+				}
+			}
+
+			debug( `Orig ${ file.filePath }: Found ${ msg.ruleId } on line ${ msg.line } => ${ line }` );
+			if ( ! lines[ line ] ) {
+				lines[ line ] = {};
+			}
+			lines[ line ][ msg.ruleId ] = msg.line;
+		} );
+		origMsgs[ file.filePath ] = lines;
+	} );
+
+	process.exitCode = 0;
+	eslintNew.forEach( file => {
+		const newL = newLines[ file.filePath ] || {};
+		const messages = file.messages;
+
+		file.messages = [];
+		file.errorCount = 0;
+		file.fatalErrorCount = 0;
+		file.warningCount = 0;
+		file.fixableErrorCount = 0;
+		file.fixableWarningCount = 0;
+
+		messages.forEach( msg => {
+			if ( argv.inDiffOnly && ! newL[ msg.line ] ) {
+				debug(
+					`New ${ file.filePath }: Ignoring ${ msg.ruleId } on line ${ msg.line }, not in diff`
+				);
+				return;
+			}
+
+			const l =
+				origMsgs[ file.filePath ] &&
+				origMsgs[ file.filePath ][ msg.line ] &&
+				origMsgs[ file.filePath ][ msg.line ][ msg.ruleId ];
+			if ( l ) {
+				debug(
+					`New ${ file.filePath }: Ignoring ${ msg.ruleId } on line ${ msg.line }, found in orig at line ${ l }`
+				);
+				return;
+			}
+
+			debug( `New ${ file.filePath }: Found ${ msg.ruleId } on line ${ msg.line }` );
+			file.messages.push( msg );
+			if ( msg.severity === 1 ) {
+				file.warningCount++;
+				file.fixableWarningCount += msg.fix ? 1 : 0;
+			} else {
+				file.errorCount++;
+				file.fatalErrorCount += msg.fatal ? 1 : 0;
+				file.fixableErrorCount += msg.fix ? 1 : 0;
+			}
+			process.exitCode = 1;
+		} );
+	} );
+
+	writeOut( formatter.format( eslintNew ) );
+}

pnpm-lock.yaml
@@ -225,18 +225,16 @@ importers:
 
   projects/js-packages/eslint-changed:
     specifiers:
-      chalk: 4.1.2
-      commander: 7.2.0
-      jetpack-js-test-runner: workspace:*
-      nyc: 15.1.0
+      chalk: 5.0.1
+      commander: 9.3.0
+      jest: 28.1.0
       parse-diff: 0.8.1
     dependencies:
-      chalk: 4.1.2
-      commander: 7.2.0
+      chalk: 5.0.1
+      commander: 9.3.0
       parse-diff: 0.8.1
     devDependencies:
-      jetpack-js-test-runner: link:../../../tools/js-test-runner
-      nyc: 15.1.0
+      jest: 28.1.0
 
   projects/js-packages/eslint-config-target-es:
     specifiers:
@@ -9973,7 +9971,6 @@ packages:
   /chalk/5.0.1:
     resolution: {integrity: sha512-Fo07WOYGqMfCWHOzSXOt2CxDbC6skS/jO9ynEcmpANMoPrD+W1r1K6Vx7iNm+AQmETU1Xr2t+n8nzkV9t6xh3w==}
     engines: {node: ^12.17.0 || ^14.13 || >=16.0.0}
-    dev: true
 
   /char-regex/1.0.2:
     resolution: {integrity: sha512-kWWXztvZ5SBQV+eRgKFeh8q5sLuZY2+8WUIzlxWVTg+oGwY14qylx1KbKzHd8P6ZYkAg0xyIDU9JMHhyJMZ1jw==}
@@ -10337,6 +10334,11 @@ packages:
     resolution: {integrity: sha512-e2i4wANQiSXgnrBlIatyHtP1odfUp0BbV5Y5nEGbxtIrStkEOAAzCUirvLBNXHLr7kwLvJl6V+4V3XV9x7Wd9w==}
     engines: {node: ^12.20.0 || >=14}
     dev: true
+
+  /commander/9.3.0:
+    resolution: {integrity: sha512-hv95iU5uXPbK83mjrJKuZyFM/LBAoCV/XhVGkS5Je6tl7sxr6A0ITMw5WoRV46/UaJ46Nllm3Xt7IaJhXTIkzw==}
+    engines: {node: ^12.20.0 || >=14}
+    dev: false
 
   /comment-parser/1.3.0:
     resolution: {integrity: sha512-hRpmWIKgzd81vn0ydoWoyPoALEOnF4wt8yKD35Ib1D6XC2siLiYaiqfGkYrunuKdsXGwpBpHU3+9r+RVw2NZfA==}

projects/js-packages/eslint-changed/.eslintrc.cjs
@@ -1,0 +1,5 @@
+module.exports = {
+	env: {
+		node: true,
+	},
+};

projects/js-packages/eslint-changed/.gitattributes
@@ -1,5 +1,6 @@
 /changelog/**    production-exclude
+/.eslintrc.cjs   production-exclude
 /.gitattributes  production-exclude
 /.gitignore      production-exclude
-/.nycrc          production-exclude
+/jest.config.js  production-exclude
 /tests/**        production-exclude

projects/js-packages/eslint-changed/.nycrc
@@ -1,5 +1,0 @@
-{
-	"extensions": [".js"],
-	"exclude": "tests/",
-	"reporter": "clover"
-}

projects/js-packages/eslint-changed/bin/eslint-changed.js
@@ -1,345 +1,10 @@
 #!/usr/bin/env node
 
-/* eslint-env node */
-/* eslint-disable no-console, no-process-exit */
+import { createProgram } from '../src/cli.js';
 
-const spawnSync = require( 'child_process' ).spawnSync;
-const fs = require( 'fs' );
-const path = require( 'path' );
-const chalk = require( 'chalk' );
-const { program } = require( 'commander' );
-const { ESLint } = require( 'eslint' );
-const parseDiff = require( 'parse-diff' );
-
-const APP_VERSION = '2.0.3-alpha';
-
-program
-	.usage(
-		'Run ESLint on files and only report new warnings/errors compared to the previous version.'
-	)
-
-	.option( '--diff <file>', 'A file containing a unified diff of the changes.' )
-	.option(
-		'--diff-base <dir>',
-		'Base directory the diff is relative to. Defaults to the current directory.',
-		v => path.resolve( process.cwd(), v )
-	)
-	.option(
-		'--eslint-orig <file>',
-		'A file containing the JSON output of ESLint on the unchanged files.'
-	)
-	.option(
-		'--eslint-new <file>',
-		'A file containing the JSON output of ESLint on the changed files.'
-	)
-
-	.option(
-		'--git',
-		"Assume git-versioned files. Set environment variables GIT and ESLINT to point to appropriate commands if they're not already in the path."
-	)
-	.option( '--git-staged', 'Compare the staged version to the HEAD version (this is the default).' )
-	.option( '--git-unstaged', 'Compare the working copy version to the staged (or HEAD) version.' )
-	.option(
-		'--git-base <ref>',
-		'Compare the HEAD version to the HEAD of a different base (e.g. branch).'
-	)
-
-	.option( '--debug', 'Enable debug output.' )
-	.option(
-		'--ext <list>',
-		'Comma-separated list of JavaScript file extensions. Ignored if files are listed.',
-		'.js'
-	)
-	.option(
-		'--in-diff-only',
-		'Only include messages on lines changed in the diff. This may miss things like deleting a `var` that leads to a new `no-undef` elsewhere.'
-	)
-	.option( '--format <name>', 'ESLint format to use for output.', 'stylish' )
-	.version( APP_VERSION );
-
-program.parse();
-const argv = program.opts();
-
-if ( argv.diff !== undefined && argv.git ) {
-	console.error( 'error: options `--diff` and `--git` are mutually exclusive' );
-	process.exit( 1 );
+try {
+	await createProgram().parseAsync();
+} catch ( e ) {
+	console.error( e ); // eslint-disable-line no-console
+	process.exitCode = 1;
 }
-
-[
-	[ 'diff', 'eslint-orig' ],
-	[ 'diff', 'eslint-new' ],
-	[ 'diff-base', 'diff' ],
-	[ 'eslint-orig', 'diff' ],
-	[ 'eslint-new', 'diff' ],
-	[ 'git-staged', 'git' ],
-	[ 'git-unstaged', 'git' ],
-	[ 'git-base', 'git' ],
-].forEach( x => {
-	const [ arg1, arg2 ] = x;
-	const prop1 = arg1.replace( /-[a-z]/g, v => v[ 1 ].toUpperCase() );
-	const prop2 = arg2.replace( /-[a-z]/g, v => v[ 1 ].toUpperCase() );
-	if ( argv[ prop1 ] !== undefined && argv[ prop2 ] === undefined ) {
-		console.error( `error: option \`--${ arg1 }\` requires option \`--${ arg2 }\`` );
-		process.exit( 1 );
-	}
-} );
-
-const debug = argv.debug ? ( ...m ) => console.debug( chalk.grey( ...m ) ) : () => {};
-
-/**
- * Get files from a diff.
- *
- * @param {Array} diff - Diff array from `parse-diff`.
- * @returns {string[]} File name strings.
- */
-function getFilesFromDiff( diff ) {
-	let files = diff.map( x => x.to );
-	if ( program.args.length === 0 ) {
-		const exts = argv.ext.split( ',' );
-		files = files.filter( file => exts.some( ext => file.endsWith( ext ) ) );
-	}
-	return files;
-}
-
-const spawnOpt = {
-	stdio: [ null, 'pipe', 'inherit' ],
-	maxBuffer: Infinity,
-	encoding: 'utf8',
-};
-
-/**
- * Spawn a command, exiting if it fails.
- *
- * @param {string} cmd - Command to execute.
- * @param {string[]} cmdArgs - Arguments to the command.
- * @returns {string} Command output.
- */
-function doCmd( cmd, cmdArgs ) {
-	const res = spawnSync( cmd, cmdArgs, spawnOpt );
-	if ( res.status ) {
-		process.exit( res.status );
-	}
-	return res.stdout;
-}
-
-/**
- * Main method.
- */
-async function main() {
-	const eslint = new ESLint();
-	debug( 'Using ESLint version', eslint.version );
-	const formatter = await eslint.loadFormatter( argv.format );
-
-	let diff, diffBase, files, eslintOrig, eslintNew;
-	if ( argv.git ) {
-		const git = process.env.GIT || 'git';
-		let origRef, newRef, args, ret;
-
-		ret = spawnSync( git, [ '--version' ], spawnOpt );
-		if ( ret.error ) {
-			console.error(
-				`error: failed to execute git as \`${ git }\`. Use environment variable \`GIT\` to override.`
-			);
-			process.exit( 1 );
-		}
-		debug( 'Using git version', ret.stdout.trim() );
-
-		args = [ 'rev-parse', '--show-toplevel' ];
-		debug( 'Getting git top level:', git, args.join( ' ' ) );
-		diffBase = doCmd( git, args ).trim();
-
-		args = [ 'diff' ];
-		if ( argv.gitBase !== undefined ) {
-			const args2 = [ 'merge-base', argv.gitBase, 'HEAD' ];
-			debug( 'Running git merge-base command:', git, args2.join( ' ' ) );
-			origRef = doCmd( git, args2 ).trim();
-			debug( 'Merge base is:', origRef );
-			newRef = 'HEAD';
-			args.push( `${ origRef }...HEAD` );
-		} else if ( argv.gitUnstaged ) {
-			origRef = ':0';
-			newRef = null;
-		} else {
-			origRef = 'HEAD';
-			newRef = ':0';
-			args.push( '--staged' );
-		}
-		args = args.concat( program.args );
-
-		debug( 'Running git diff command:', git, args.join( ' ' ) );
-		diff = parseDiff( doCmd( git, args ) );
-		if ( ! argv.inDiffOnly && program.args.length ) {
-			files = program.args;
-			debug( 'Determined files from command line:', files );
-		} else {
-			files = getFilesFromDiff( diff );
-			debug( 'Determined files from diff:', files );
-		}
-
-		eslintOrig = [];
-		eslintNew = [];
-		for ( const file of files ) {
-			let content;
-
-			args = [ 'cat-file', '-e', origRef + ':' + file ];
-			debug( 'Testing if file is new:', git, args.join( ' ' ) );
-			if ( spawnSync( git, args, { stdio: 'ignore' } ).status ) {
-				debug( "It's new, so no orig ESLint data." );
-			} else {
-				args = [ 'show', origRef + ':' + file ];
-				debug( 'Fetching orig file contents:', git, args.join( ' ' ) );
-				content = doCmd( git, args );
-				debug( 'Executing ESLint for orig file' );
-				ret = await eslint.lintText( content, {
-					filePath: file,
-				} );
-				eslintOrig = eslintOrig.concat( ret );
-			}
-
-			if ( newRef === null ) {
-				content = fs.readFileSync( file, 'utf8' );
-			} else {
-				args = [ 'show', newRef + ':' + file ];
-				debug( 'Fetching new file contents:', git, args.join( ' ' ) );
-				content = doCmd( git, args );
-			}
-			debug( 'Executing ESLint for new file' );
-			ret = await eslint.lintText( content, {
-				filePath: file,
-			} );
-			eslintNew = eslintNew.concat( ret );
-		}
-	} else if ( argv.diff ) {
-		diff = parseDiff( fs.readFileSync( argv.diff, 'utf8' ) );
-		diffBase = argv.diffBase || process.cwd();
-		if ( ! argv.inDiffOnly && program.args.length ) {
-			files = program.args;
-			debug( 'Determined files from command line:', files );
-		} else {
-			files = getFilesFromDiff( diff );
-			debug( 'Determined files from diff:', files );
-			if ( program.args.length ) {
-				const cmdLineFiles = new Set( program.args );
-				files = files.filter( file => cmdLineFiles.has( file ) );
-				debug( 'Intersected files with those from the command line:', files );
-			}
-		}
-		eslintOrig = JSON.parse( fs.readFileSync( argv.eslintOrig, 'utf8' ) );
-		eslintNew = JSON.parse( fs.readFileSync( argv.eslintNew, 'utf8' ) );
-	} else {
-		program.outputHelp();
-		process.exit( 1 );
-	}
-
-	// oldLines maps line numbers in the old version to the new.
-	// newLines just lists lines present in the diff.
-	const oldLines = {};
-	const newLines = {};
-	diff.forEach( file => {
-		const fileName = path.resolve( diffBase, file.to );
-		const ol = {},
-			nl = {};
-		file.chunks.forEach( chunk => {
-			let p = 0;
-			chunk.changes.forEach( c => {
-				switch ( c.type ) {
-					case 'add':
-						p = c.ln;
-						nl[ c.ln ] = true;
-						break;
-					case 'del':
-						ol[ c.ln ] = ++p;
-						break;
-					case 'normal':
-						p = c.ln2;
-						ol[ c.ln1 ] = c.ln2;
-						nl[ c.ln2 ] = true;
-						break;
-				}
-			} );
-		} );
-		oldLines[ fileName ] = ol;
-		newLines[ fileName ] = nl;
-	} );
-
-	if ( argv.inDiffOnly ) {
-		files = new Set( files.map( file => path.resolve( diffBase, file ) ) );
-		eslintOrig = eslintOrig.filter( x => files.has( x.filePath ) && oldLines[ x.filePath ] );
-		eslintNew = eslintNew.filter( x => files.has( x.filePath ) && newLines[ x.filePath ] );
-	}
-
-	const origMsgs = {};
-	eslintOrig.forEach( file => {
-		const lines = {};
-		const oldL = oldLines[ file.filePath ] || {};
-		file.messages.forEach( msg => {
-			let line = msg.line;
-			for ( let i = msg.line; i > 0; i-- ) {
-				if ( oldL[ i ] ) {
-					line = msg.line + oldL[ i ] - i;
-					break;
-				}
-			}
-
-			debug( `Orig ${ file.filePath }: Found ${ msg.ruleId } on line ${ msg.line } => ${ line }` );
-			if ( ! lines[ line ] ) {
-				lines[ line ] = {};
-			}
-			lines[ line ][ msg.ruleId ] = msg.line;
-		} );
-		origMsgs[ file.filePath ] = lines;
-	} );
-
-	let exitCode = 0;
-	eslintNew.forEach( file => {
-		const newL = newLines[ file.filePath ] || {};
-		const messages = file.messages;
-
-		file.messages = [];
-		file.errorCount = 0;
-		file.fatalErrorCount = 0;
-		file.warningCount = 0;
-		file.fixableErrorCount = 0;
-		file.fixableWarningCount = 0;
-
-		messages.forEach( msg => {
-			if ( argv.inDiffOnly && ! newL[ msg.line ] ) {
-				debug(
-					`New ${ file.filePath }: Ignoring ${ msg.ruleId } on line ${ msg.line }, not in diff`
-				);
-				return;
-			}
-
-			const l =
-				origMsgs[ file.filePath ] &&
-				origMsgs[ file.filePath ][ msg.line ] &&
-				origMsgs[ file.filePath ][ msg.line ][ msg.ruleId ];
-			if ( l ) {
-				debug(
-					`New ${ file.filePath }: Ignoring ${ msg.ruleId } on line ${ msg.line }, found in orig at line ${ l }`
-				);
-				return;
-			}
-
-			debug( `New ${ file.filePath }: Found ${ msg.ruleId } on line ${ msg.line }` );
-			file.messages.push( msg );
-			if ( msg.severity === 1 ) {
-				file.warningCount++;
-				file.fixableWarningCount += msg.fix ? 1 : 0;
-			} else {
-				file.errorCount++;
-				file.fatalErrorCount += msg.fatal ? 1 : 0;
-				file.fixableErrorCount += msg.fix ? 1 : 0;
-			}
-			exitCode = 1;
-		} );
-	} );
-
-	console.log( formatter.format( eslintNew ) );
-	process.exit( exitCode );
-}
-
-main().catch( e => {
-	console.error( e );
-	process.exit( 1 );
-} );

projects/js-packages/eslint-changed/jest.config.js
@@ -1,0 +1,14 @@
+import { createRequire } from 'module';
+import path from 'path';
+
+const chalk = createRequire( import.meta.url )
+	.resolve( 'chalk' )
+	.replace( /\/chalk\/.*?$/, '/chalk/' );
+
+export default {
+	// https://github.com/facebook/jest/issues/12270
+	moduleNameMapper: {
+		'#ansi-styles': path.join( chalk, 'source/vendor/ansi-styles/index.js' ),
+		'#supports-color': path.join( chalk, 'source/vendor/supports-color/index.js' ),
+	},
+};

projects/js-packages/eslint-changed/tests/.eslintrc.cjs
@@ -1,0 +1,3 @@
+module.exports = {
+	extends: [ require.resolve( 'jetpack-js-tools/eslintrc/jest' ) ],
+};

commit bb716734bc735943dad0373431d386eac973dd38

eslint-changed: Fix debug output
* Print newlines after each line (broken in #24626)
* Fix fetching of eslint version (broken in #21606)
* Strip "git version" from `git version` output.

projects/js-packages/eslint-changed/src/cli.js
@@ -102,7 +102,7 @@ async function main( process, argv, program ) {
 	} );
 
 	const writeOut = program.configureOutput().writeOut;
-	const debug = argv.debug ? ( ...m ) => writeOut( chalk.grey( ...m ) ) : () => {};
+	const debug = argv.debug ? ( ...m ) => writeOut( chalk.grey( ...m ) + '\n' ) : () => {};
 
 	/**
 	 * Get files from a diff.
@@ -141,7 +141,7 @@ async function main( process, argv, program ) {
 	}
 
 	const eslint = new ESLint();
-	debug( 'Using ESLint version', eslint.version );
+	debug( 'Using ESLint version', ESLint.version );
 	const formatter = await eslint.loadFormatter( argv.format );
 
 	let diff, diffBase, files, eslintOrig, eslintNew;
@@ -156,7 +156,7 @@ async function main( process, argv, program ) {
 				1
 			);
 		}
-		debug( 'Using git version', ret.stdout.trim() );
+		debug( 'Using git version', ret.stdout.trim().replace( /^git version /, '' ) );
 
 		args = [ 'rev-parse', '--show-toplevel' ];
 		debug( 'Getting git top level:', git, args.join( ' ' ) );