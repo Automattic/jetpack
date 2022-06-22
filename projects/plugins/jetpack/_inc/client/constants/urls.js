3e4fd6e09cca69a5894c7167fe4b5089f9b949fa
Robert Sreberski <robertsreberski@gmail.com>
2022-06-22 14:50:47 +0200

    Replace link to the video

--- projects/plugins/jetpack/_inc/client/constants/urls.js
+++ projects/plugins/jetpack/_inc/client/constants/urls.js
@@ -5,4 +5,4 @@ export const JETPACK_CONTACT_SUPPORT = getRedirectUrl( 'jetpack-contact-support'
 export const JETPACK_CONTACT_BETA_SUPPORT = getRedirectUrl( 'jetpack-contact-support-beta-group' );
 
 export const GETTING_STARTED_WITH_JETPACK_BACKUP_VIDEO_URL =
-	'https://www.youtube.com/watch?v=pS5-i5KjKTw';
+	'https://jetpack.com/support/backup/the-jetpack-backup-plugin/getting-started-with-the-jetpack-backup-plugin/#starting-your-first-backup';
