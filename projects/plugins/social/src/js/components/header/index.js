37a8a74d247eb5d7b841ad6fa77d0ca8b56b5c2d
spsiddarthan <siddarthan@outlook.com>
2022-06-23 11:46:01 +0530

    Use the paid version UI to display share counter.

--- projects/plugins/social/src/js/components/header/index.js
+++ projects/plugins/social/src/js/components/header/index.js
@@ -1,8 +1,7 @@
-import { Container, Col, H3, Text, Spinner } from '@automattic/jetpack-components';
+import { Container, Col, H3, Text, Spinner, SocialIcon } from '@automattic/jetpack-components';
 import { useSelect, useDispatch } from '@wordpress/data';
-import { createInterpolateElement } from '@wordpress/element';
-import { __, sprintf } from '@wordpress/i18n';
-import { Icon, edit } from '@wordpress/icons';
+import { __ } from '@wordpress/i18n';
+import { Icon, edit, lifesaver } from '@wordpress/icons';
 import { STORE_ID } from '../../store';
 import styles from './styles.module.scss';
 
@@ -28,35 +27,33 @@ const Header = () => {
 	} );
 	const sharesCountLoaded = sharesCountResponse && sharesCountResponse.results;
 	const sharesCount = sharesCountLoaded ? sharesCountResponse.results.total : 0;
-	const text = createInterpolateElement(
-		sprintf(
-			// translators: %1$d is the number of shares used.
-			__( 'You’ve made <boldText>%1$d</boldText> shares over the past 30 days.', 'jetpack-social' ),
-			sharesCount
-		),
-		{
-			boldText: <strong />,
-		}
-	);
-
 	const actions = [
 		{
 			link: '/wp-admin/post-new.php',
 			label: __( 'Write a post', 'jetpack-social' ),
 			icon: edit,
 		},
+		{
+			link: '/wp-admin/post-new.php',
+			label: __( 'Need help?', 'jetpack-social' ),
+			icon: lifesaver,
+		},
 	];
 	return (
 		<Container horizontalSpacing={ 3 } horizontalGap={ 7 } className={ styles.container }>
 			<Col sm={ 4 } md={ 4 } lg={ 5 }>
-				<H3 mt={ 2 }>{ __( 'Post everywhere', 'jetpack-social' ) }</H3>
-				{ sharesCountLoaded ? (
-					<Text className={ styles.title }>{ text }</Text>
-				) : (
-					<Spinner color="#000" size={ 32 } />
-				) }
+				<H3 mt={ 2 }>{ __( 'Post everywhere at any time', 'jetpack-social' ) }</H3>
 				<Actions actions={ actions } />
 			</Col>
+			<div className={ styles.sharesCard }>
+				<div className={ styles.socialIcon }>
+					<SocialIcon />
+				</div>
+				<div className={ styles.sharesCardHeading }>Total shares this month</div>
+				<div className={ styles.sharesCount }>
+					{ sharesCountLoaded ? sharesCount : <Spinner color="#000" size={ 24 } /> }
+				</div>
+			</div>
 		</Container>
 	);
 };
