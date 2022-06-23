c5c373ea15f68de0596e7b0bcfc04cf1df05079c
spsiddarthan <siddarthan@outlook.com>
2022-06-23 16:18:27 +0530

    Removed unwanted change.

--- projects/plugins/social/src/js/store/selectors/jetpack-settings.js
+++ projects/plugins/social/src/js/store/selectors/jetpack-settings.js
@@ -2,8 +2,6 @@ const jetpackSettingSelectors = {
 	getPublicizeModuleStatus: state => state.jetpackSettings,
 	isModuleEnabled: state => state.jetpackSettings.publicize_active,
 	isUpdatingJetpackSettings: state => state.jetpackSettings.is_updating,
-	// TODO: Replace this with a real endpoint
-	hasPaidUpgrade: () => false,
 };
 
 export default jetpackSettingSelectors;
