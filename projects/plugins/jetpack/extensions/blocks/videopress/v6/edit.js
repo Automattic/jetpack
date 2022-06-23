a427ce9dc4e0b7212a923923426106514ab74e42
retrofox <rdsuarez@gmail.com>
2022-06-23 15:35:03 -0300

    add super basic styles in both sides

--- projects/plugins/jetpack/extensions/blocks/videopress/v6/edit.js
+++ projects/plugins/jetpack/extensions/blocks/videopress/v6/edit.js
@@ -4,6 +4,11 @@
 import { useBlockProps } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 
+/**
+ * Internal dependencies
+ */
+import './editor.scss';
+
 export default function VideoPressEdit() {
 	const blockProps = useBlockProps( {
 		className: 'wp-block-jetpack-videopress',
