3556355a237a494513ba02130f29bd5850758d8d
retrofox <rdsuarez@gmail.com>
2022-06-23 16:28:55 -0300

    [not verified] vpblock: show mediaplaceholder when no src

--- projects/plugins/jetpack/extensions/blocks/videopress/v6/edit.js
+++ projects/plugins/jetpack/extensions/blocks/videopress/v6/edit.js
@@ -1,20 +1,27 @@
 /**
  * WordPress dependencies
  */
-import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
+import {
+	InspectorControls,
+	useBlockProps,
+	BlockIcon,
+	MediaPlaceholder,
+} from '@wordpress/block-editor';
 import { PanelBody, ToggleControl, Tooltip } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-
 /**
  * Internal dependencies
  */
-import './editor.scss';
+import { VideoPressIcon as icon } from '../../../shared/icons';
+
+const ALLOWED_MEDIA_TYPES = [ 'video' ];
+
+// @Todo: replace with uploading implementation.
+const noop = () => {};
 
 export default function VideoPressEdit( props ) {
 	const { attributes, setAttributes } = props;
-
-	const { controls } = attributes;
-
+	const { controls, src } = attributes;
 	const blockProps = useBlockProps( {
 		className: 'wp-block-jetpack-videopress',
 	} );
@@ -51,10 +58,27 @@ export default function VideoPressEdit( props ) {
 		</>
 	);
 
-	return (
-		<>
-			{ blockSettings }
-			<div { ...blockProps }>{ __( 'VideoPress', 'jetpack' ) }</div>
-		</>
-	);
+	if ( ! src ) {
+		return (
+			<>
+				{ blockSettings }
+				<div { ...blockProps }>
+					<MediaPlaceholder
+						icon={ <BlockIcon icon={ icon } /> }
+						labels={ {
+							title: __( 'VideoPress', 'jetpack' ),
+						} }
+						onSelect={ noop }
+						onSelectURL={ noop }
+						accept="video/*"
+						allowedTypes={ ALLOWED_MEDIA_TYPES }
+						value={ attributes }
+						onError={ noop }
+					/>
+				</div>
+			</>
+		);
+	}
+
+	return <div { ...blockProps }>{ __( 'VideoPress', 'jetpack' ) }</div>;
 }
