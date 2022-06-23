21b29278b0295267a2554e0d425012b19ef5b05a
retrofox <rdsuarez@gmail.com>
2022-06-23 12:59:23 -0300

    [not verified] expose properly assets path

--- projects/plugins/jetpack/extensions/blocks/videopress/videopress.php
+++ projects/plugins/jetpack/extensions/blocks/videopress/videopress.php
@@ -12,8 +12,9 @@ namespace Automattic\Jetpack\Extensions\VideoPress;
 use Automattic\Jetpack\Blocks;
 use Jetpack_Gutenberg;
 
-const FEATURE_NAME = 'videopress-block';
-const BLOCK_NAME   = 'jetpack/' . FEATURE_NAME;
+const FEATURE_NAME   = 'videopress-block';
+const FEATURE_FOLDER = 'videopress';
+const BLOCK_NAME     = 'jetpack/' . FEATURE_NAME;
 
 /**
  * Registers the block for use in Gutenberg
@@ -37,6 +38,6 @@ add_action( 'init', __NAMESPACE__ . '\register_block' );
  * @return string
  */
 function load_assets( $attrs, $content ) {
-	Jetpack_Gutenberg::load_assets_as_required( FEATURE_NAME );
+	Jetpack_Gutenberg::load_assets_as_required( FEATURE_FOLDER );
 	return $content;
 }
