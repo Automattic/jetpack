ff5ff05ab9b3d6783a4d322765003b703143b17f
leogermani <leogermani@gmail.com>
2022-06-28 17:48:15 -0300

    Update/videopress add inspector controls (#24861)
    * move inspector control to new file
    
    * changelog
    
    * add new options to sidebar
    
    * only show sidebar when video is present
    
    * add memoization via usecallback

--- projects/plugins/jetpack/extensions/blocks/videopress/v6/edit.js
+++ projects/plugins/jetpack/extensions/blocks/videopress/v6/edit.js
@@ -3,13 +3,8 @@
  */
 
 import { getBlobByURL, isBlobURL } from '@wordpress/blob';
-import {
-	InspectorControls,
-	useBlockProps,
-	BlockIcon,
-	MediaPlaceholder,
-} from '@wordpress/block-editor';
-import { Button, PanelBody, ToggleControl, Tooltip } from '@wordpress/components';
+import { useBlockProps, BlockIcon, MediaPlaceholder } from '@wordpress/block-editor';
+import { Button } from '@wordpress/components';
 import { usePrevious } from '@wordpress/compose';
 import { store as coreStore } from '@wordpress/core-data';
 import { useSelect, useDispatch } from '@wordpress/data';
@@ -23,6 +18,7 @@ import { VpBlock } from '../edit';
 import Loading from '../loading';
 // import { getJWT, useResumableUploader } from '../resumable-upload/use-uploader';
 import { getVideoPressUrl } from '../url';
+import VideoPressInspectorControls from './components/inspector-controls';
 import { useResumableUploader } from './hooks/use-uploader.js';
 import './editor.scss';
 
@@ -102,20 +98,6 @@ export default function VideoPressEdit( { attributes, setAttributes } ) {
 		className: 'wp-block-jetpack-videopress is-placeholder-container',
 	} );
 
-	const renderControlLabelWithTooltip = ( label, tooltipText ) => {
-		return (
-			<Tooltip text={ tooltipText } position="top">
-				<span>{ label }</span>
-			</Tooltip>
-		);
-	};
-
-	const handleAttributeChange = attributeName => {
-		return newValue => {
-			setAttributes( { [ attributeName ]: newValue } );
-		};
-	};
-
 	// Helper instance to upload the video to the VideoPress infrastructure.
 	const [ videoPressUploader ] = useResumableUploader( {
 		onProgress: setUploadingProgress,
@@ -156,24 +138,6 @@ export default function VideoPressEdit( { attributes, setAttributes } ) {
 		videoPressUploader( file );
 	}
 
-	const blockSettings = (
-		<>
-			<InspectorControls>
-				<PanelBody title={ __( 'Video Settings', 'jetpack' ) }>
-					<ToggleControl
-						label={ renderControlLabelWithTooltip(
-							__( 'Playback Controls', 'jetpack' ),
-							/* translators: Tooltip describing the "controls" option for the VideoPress player */
-							__( 'Display the video playback controls', 'jetpack' )
-						) }
-						onChange={ handleAttributeChange( 'controls' ) }
-						checked={ controls }
-					/>
-				</PanelBody>
-			</InspectorControls>
-		</>
-	);
-
 	/*
 	 * 1 - Initial block state. Show MediaPlaceholder when:
 	 *     - no src attribute,
@@ -201,7 +165,6 @@ export default function VideoPressEdit( { attributes, setAttributes } ) {
 	if ( isUploadingFile ) {
 		return (
 			<>
-				{ blockSettings }
 				<div { ...blockProps }>
 					<Loading text={ __( '(2) Uploading file to backend…', 'jetpack' ) } />;
 				</div>
@@ -213,7 +176,6 @@ export default function VideoPressEdit( { attributes, setAttributes } ) {
 	if ( fileHasBeenUploaded && ! isRequestingEmbedPreview && ! videoPressUrl ) {
 		return (
 			<>
-				{ blockSettings }
 				<div { ...blockProps }>
 					<Loading text={ __( '(3) Uploading file to VideoPress…', 'jetpack' ) } />;
 				</div>
@@ -225,7 +187,6 @@ export default function VideoPressEdit( { attributes, setAttributes } ) {
 	if ( isRequestingEmbedPreview && ! preview ) {
 		return (
 			<>
-				{ blockSettings }
 				<div { ...blockProps }>
 					<Loading text={ __( '(4) Generating preview…', 'jetpack' ) } />
 				</div>
@@ -237,7 +198,6 @@ export default function VideoPressEdit( { attributes, setAttributes } ) {
 	if ( fileHasBeenUploaded && ! isRequestingEmbedPreview && ! preview ) {
 		return (
 			<>
-				{ blockSettings }
 				<div { ...blockProps }>
 					<p>
 						{ __( "The video is still being processed. It'll take a little bit more…", 'jetpack' ) }
@@ -254,7 +214,6 @@ export default function VideoPressEdit( { attributes, setAttributes } ) {
 	if ( ! preview ) {
 		return (
 			<>
-				{ blockSettings }
 				<div { ...blockProps }>
 					<Loading text={ __( '(6) Generating preview…', 'jetpack' ) } />
 				</div>
@@ -265,7 +224,7 @@ export default function VideoPressEdit( { attributes, setAttributes } ) {
 	// X - Show VideoPress player. @todo: finish
 	return (
 		<>
-			{ blockSettings }
+			<VideoPressInspectorControls attributes={ attributes } setAttributes={ setAttributes } />
 			<VpBlock
 				html={ html }
 				scripts={ scripts }
