Automattic/jetpack

commit 41b1919fee7b88c9718e137ca7a8b2afe361d43b

PHPCS: Fix WordPress.Security.ValidatedSanitizedInput outside of Jetpack and WAF (#23942)
* codesniffer: Prepare for adding missing WordPress rules

We're currently pulling in rules from WordPress-Core, WordPress-Docs,
and WordPress-Extra. It turns out that that misses four rules from
"WordPress":

- WordPress.DB.DirectDatabaseQuery
- WordPress.DB.SlowDBQuery
- WordPress.Security.ValidatedSanitizedInput
- WordPress.Utils.I18nTextDomainFixer

The last of those is no problem, it's redundant to WordPress.WP.I18n
(for some reason they put the fixing logic into a separate rule) and
doesn't report anything unless configured anyway.

For the other three we'll have to clean up our existing code to fix at
least the 224, 35, and 808 instances in the non-excluded files before we
can enable them. So for now we'll explicitly disable them, then enable
them as we fix them.

* Drop WordPress.DB.SlowDBQuery

* PHPCS: Fix WordPress.Security.ValidatedSanitizedInput outside of Jetpack and WAF

Mostly this means adding `wp_unslash()` and `filter_var()` calls, and
sometimes `isset()` checks too. Occasionally it means ignoring the sniff
in code that's already doing better than `filter_var()` would do.

Jetpack is skipped for now because it's big. WAF is skipped for now
because it's not immediately clear to me whether WordPress's
`wp_unslash()` is loaded at the time it runs.

* That wasn't supposed to be here

* device-detection: Work around possible non-presence of `wp_unslash`

* Add stubs for wp_unslash etc in packages/status, plugins/boost

* Add one more ignore

projects/packages/search/src/classic-search/class-classic-search.php
@@ -309,7 +309,7 @@ class Classic_Search {
 	 * @param WP_Query $query A WP_Query instance.
 	 */
 	public function maybe_add_post_type_as_var( WP_Query $query ) {
-		$post_type = ( ! empty( $_GET['post_type'] ) ) ? $_GET['post_type'] : false; // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		$post_type = ( ! empty( $_GET['post_type'] ) ) ? sanitize_key( $_GET['post_type'] ) : false; // phpcs:ignore WordPress.Security.NonceVerification.Recommended
 		if ( $this->should_handle_query( $query ) && $post_type ) {
 			$post_types = ( is_string( $post_type ) && false !== strpos( $post_type, ',' ) )
 				? explode( ',', $post_type )

commit 9ca6c02dd4ccdac6c7aa56409f0bd06a70477017

Jetpack Search Widget: Restore support for `&post_type=post%2Cpage`
Prevent `sanitize_key` from stripping out commas before we process
them. Bug introduced in 41b1919fee7b88c9718e137ca7a8b2afe361d43b

projects/packages/search/src/classic-search/class-classic-search.php
@@ -309,12 +309,20 @@ class Classic_Search {
 	 * @param WP_Query $query A WP_Query instance.
 	 */
 	public function maybe_add_post_type_as_var( WP_Query $query ) {
-		$post_type = ( ! empty( $_GET['post_type'] ) ) ? sanitize_key( $_GET['post_type'] ) : false; // phpcs:ignore WordPress.Security.NonceVerification.Recommended
-		if ( $this->should_handle_query( $query ) && $post_type ) {
-			$post_types = ( is_string( $post_type ) && false !== strpos( $post_type, ',' ) )
-				? explode( ',', $post_type )
-				: (array) $post_type;
-			$post_types = array_map( 'sanitize_key', $post_types );
+		// phpcs:disable WordPress.Security.NonceVerification.Recommended
+		if ( $this->should_handle_query( $query ) && ! empty( $_GET['post_type'] ) ) {
+			if ( is_array( $_GET['post_type'] ) ) {
+				$post_types = array_map( 'sanitize_key', $_GET['post_type'] );
+			} else {
+				$post_types = array_map(
+					'sanitize_key',
+					explode(
+						',',
+						sanitize_text_field( wp_unslash( $_GET['post_type'] ) )
+					)
+				);
+			}
+			// phpcs:enable WordPress.Security.NonceVerification.Recommended
 			$query->set( 'post_type', $post_types );
 		}
 	}