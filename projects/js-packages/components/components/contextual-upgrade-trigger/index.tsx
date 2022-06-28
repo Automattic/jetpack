926300bebd44a862defa294d27d3c1ecd2a3b0ed
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2022-06-28 12:51:50 -0300

    Components: Improve CUT with hover actions

--- projects/js-packages/components/components/contextual-upgrade-trigger/index.tsx
+++ projects/js-packages/components/components/contextual-upgrade-trigger/index.tsx
@@ -1,5 +1,4 @@
 import { Icon, arrowRight } from '@wordpress/icons';
-import Button from '../button';
 import Text from '../text';
 import styles from './style.module.scss';
 
@@ -8,7 +7,7 @@ const ContextualUpgradeTrigger = () => {
 		<div className={ styles.cut }>
 			<div>
 				<Text>Your site is updated with new content several times a day</Text>
-				<Button variant="link">Consider upgrading to real-time protection</Button>
+				<Text className={ styles.cta }>Consider upgrading to real-time protection</Text>
 			</div>
 			<Icon icon={ arrowRight } className={ styles.icon } size={ 30 } />
 		</div>
