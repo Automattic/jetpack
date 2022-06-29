99d982ab124be20e16fb84c2dc64d808de2bb826
Dean Sas <dean.sas@automattic.com>
2022-06-29 11:08:18 +0100

    [not verified] Add pattern chooser to the PaymentsIntro block
    The pattern chooser has been added to the Payments Intro block so that
    people have an easier route to getting started with nicely laid out
    Payment Blocks.
    
    Currently just the Single Product pattern is available.
    
    Fixes #23422

--- projects/plugins/jetpack/extensions/blocks/payments-intro/edit.js
+++ projects/plugins/jetpack/extensions/blocks/payments-intro/edit.js
@@ -1,12 +1,48 @@
-import { InnerBlocks, store as blockEditorStore } from '@wordpress/block-editor';
-import { createBlock, getBlockType, registerBlockVariation } from '@wordpress/blocks';
-import { Placeholder } from '@wordpress/components';
+import {
+	InnerBlocks,
+	__experimentalBlockPatternSetup as BlockPatternSetup, // eslint-disable-line wpcalypso/no-unsafe-wp-apis
+	store as blockEditorStore,
+} from '@wordpress/block-editor';
+import { cloneBlock, createBlock, getBlockType, registerBlockVariation } from '@wordpress/blocks';
+import { Button, Modal, Placeholder } from '@wordpress/components';
 import { useDispatch, useSelect } from '@wordpress/data';
-import { useEffect } from '@wordpress/element';
+import { useEffect, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { get } from 'lodash';
 import PaymentsIntroBlockPicker from './block-picker';
 import defaultVariations from './variations';
+
+const JetpackPatternPicker = function ( { onBlockPatternSelect } ) {
+	const [ isPatternSelectionModalOpen, setIsPatternSelectionModalOpen ] = useState( false );
+
+	const patternFilter = pattern => {
+		return pattern.categories.includes( 'earn' );
+	};
+
+	return (
+		<>
+			<Button
+				variant="primary"
+				onClick={ () => setIsPatternSelectionModalOpen( true ) }
+				className="wp-payments-intro-pattern-picker__opener"
+			>
+				{ __( 'Choose a pattern', 'jetpack' ) }
+			</Button>
+			{ isPatternSelectionModalOpen && (
+				<Modal
+					title={ __( 'Choose a pattern', 'jetpack' ) }
+					closeLabel={ __( 'Cancel', 'jetpack' ) }
+					onRequestClose={ () => setIsPatternSelectionModalOpen( false ) }
+				>
+					<BlockPatternSetup
+						onBlockPatternSelect={ onBlockPatternSelect }
+						filterPatternsFn={ patternFilter }
+					/>
+				</Modal>
+			) }
+		</>
+	);
+};
 
 export default function JetpackPaymentsIntroEdit( { name, clientId, className } ) {
 	const { blockType, hasInnerBlocks } = useSelect( select => {
@@ -34,14 +70,21 @@ export default function JetpackPaymentsIntroEdit( { name, clientId, className } 
 		}
 	} );
 
-	const renderVariationPicker = () => {
+	if ( ! hasInnerBlocks && registerBlockVariation ) {
 		return (
 			<Placeholder
 				icon={ get( blockType, [ 'icon', 'src' ] ) }
 				label={ get( blockType, [ 'title' ] ) }
-				instructions={ __( "Please select which kind of payment you'd like to add.", 'jetpack' ) }
+				instructions={ __( 'Start by choosing one of our suggested layout patterns', 'jetpack' ) }
 				className={ className }
 			>
+				<JetpackPatternPicker
+					onBlockPatternSelect={ blocks => {
+						const clonedBlocks = blocks.map( block => cloneBlock( block ) );
+						replaceBlock( clientId, clonedBlocks );
+					} }
+				/>
+				<p>{ __( 'Or use one of our blocks to create your own', 'jetpack' ) }</p>
 				<PaymentsIntroBlockPicker
 					label={ __( 'Payment Block list', 'jetpack' ) }
 					variations={ usableVariations }
@@ -51,10 +94,6 @@ export default function JetpackPaymentsIntroEdit( { name, clientId, className } 
 				/>
 			</Placeholder>
 		);
-	};
-
-	if ( ! hasInnerBlocks && registerBlockVariation ) {
-		return renderVariationPicker();
 	}
 
 	return <InnerBlocks />;
