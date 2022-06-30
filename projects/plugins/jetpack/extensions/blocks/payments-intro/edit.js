01c41c063dadd90e76c65e508df4f48d7183e71d
Dean Sas <dean.sas@automattic.com>
2022-06-30 12:10:24 +0100

    [not verified] Inline BlockVariationPicker in PaymentsIntro
    The PaymentsIntro block used the experimental BlockVariationPicker from
    Core Gutenberg [0] in order to let people select a payment block.
    
    This change copies some of the existing BlockVariationPicker logic from
    core Gutenberg into the PaymentsIntro block itself. The main reason for
    this is to allow greater UI flexibility, specifically integrating block
    patterns into the PaymentsIntro block (see #23422).
    
    Additionally, the BlockVariationPicker is experimental and subject to
    drastic and breaking change, it was arguably a misuse of the component -
     the blocks presented are not variations of each other, and are
    technically not really related at all.
    
    [0]
    https://github.com/WordPress/gutenberg/blob/93ab227ef76a1a73720582b6958cf1a079a91aa8/packages/block-editor/src/components/block-variation-picker/README.md

--- projects/plugins/jetpack/extensions/blocks/payments-intro/edit.js
+++ projects/plugins/jetpack/extensions/blocks/payments-intro/edit.js
@@ -1,14 +1,36 @@
-import {
-	InnerBlocks,
-	__experimentalBlockVariationPicker as BlockVariationPicker, // eslint-disable-line wpcalypso/no-unsafe-wp-apis
-	store as blockEditorStore,
-} from '@wordpress/block-editor';
+import { InnerBlocks, store as blockEditorStore } from '@wordpress/block-editor';
 import { createBlock, getBlockType, registerBlockVariation } from '@wordpress/blocks';
+import { Button, Placeholder } from '@wordpress/components';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { get } from 'lodash';
 import defaultVariations from './variations';
+
+const JetpackBlockVariationPicker = function ( { variations, onSelect } ) {
+	return (
+		<ul
+			aria-label={ __( 'Payment Block list', 'jetpack' ) }
+			className="wp-payments-intro-variation-picker"
+		>
+			{ variations.map( variation => (
+				<li key={ variation.name }>
+					<Button
+						variant="secondary"
+						icon={ variation.icon }
+						iconSize={ 48 }
+						onClick={ () => onSelect( variation ) }
+						className="wp-payments-intro-variation-picker__variation"
+						label={ variation.description || variation.title }
+					/>
+					<span className="wp-payments-intro-variation-picker__variation-label" role="presentation">
+						{ variation.title }
+					</span>
+				</li>
+			) ) }
+		</ul>
+	);
+};
 
 export default function JetpackPaymentsIntroEdit( { name, clientId } ) {
 	const { blockType, hasInnerBlocks } = useSelect( select => {
@@ -38,15 +60,20 @@ export default function JetpackPaymentsIntroEdit( { name, clientId } ) {
 
 	const renderVariationPicker = () => {
 		return (
-			<BlockVariationPicker
+			<Placeholder
 				icon={ get( blockType, [ 'icon', 'src' ] ) }
 				label={ get( blockType, [ 'title' ] ) }
 				instructions={ __( "Please select which kind of payment you'd like to add.", 'jetpack' ) }
-				variations={ usableVariations }
-				onSelect={ ( nextVariation = usableVariations[ 0 ] ) => {
-					setVariation( nextVariation );
-				} }
-			/>
+				//className="block-editor-block-variation-picker"
+				className="wp-payments-intro-wrapper"
+			>
+				<JetpackBlockVariationPicker
+					variations={ usableVariations }
+					onSelect={ ( nextVariation = usableVariations[ 0 ] ) => {
+						setVariation( nextVariation );
+					} }
+				/>
+			</Placeholder>
 		);
 	};
 
