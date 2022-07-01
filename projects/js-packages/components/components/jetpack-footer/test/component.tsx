330ab9843b8206eacce41052b8fb76be0edf0735
Manzoor Wani <manzoorwani.jk@gmail.com>
2022-07-01 18:50:21 +0530

    [not verified] Add more test cases

--- projects/js-packages/components/components/jetpack-footer/test/component.tsx
+++ projects/js-packages/components/components/jetpack-footer/test/component.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import JetpackFooter from '../index';
 
 describe( 'JetpackFooter', () => {
@@ -11,6 +11,35 @@ describe( 'JetpackFooter', () => {
 			const { container } = render( <JetpackFooter { ...testProps } /> );
 			// eslint-disable-next-line testing-library/no-node-access
 			expect( container.firstChild ).toHaveClass( 'sample-classname' );
+		} );
+
+		it( 'validates Jetpack logo', () => {
+			render( <JetpackFooter /> );
+
+			expect( screen.getByLabelText( 'Jetpack logo' ) ).toBeInTheDocument();
+		} );
+
+		it( 'tests for module name and link', () => {
+			render(
+				<JetpackFooter
+					moduleName="Test module"
+					moduleNameHref="https://jetpack.com/path/to-some-page"
+				/>
+			);
+
+			const element = screen.getByLabelText( 'Test module' );
+
+			expect( element ).toBeInTheDocument();
+			expect( element ).toBeInstanceOf( HTMLAnchorElement );
+			expect( element ).toHaveAttribute( 'href', 'https://jetpack.com/path/to-some-page' );
+		} );
+
+		it( 'validates the a8c label', () => {
+			render( <JetpackFooter /> );
+
+			for ( const element of screen.getAllByLabelText( 'An Automattic Airline' ) ) {
+				expect( element ).toBeInTheDocument();
+			}
 		} );
 	} );
 } );
