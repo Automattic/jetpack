35309aa23bf699057842979bf806a12a91afcdfc
Jasper Kang <jasper.kang@automattic.com>
2022-07-05 13:33:28 +1200

    [not verified] convert old jetpack sidebar if exists

--- projects/packages/search/compatibility/search-0.15.2.php
+++ projects/packages/search/compatibility/search-0.15.2.php
@@ -4,3 +4,22 @@
  *
  * @package automattic/jetpack-search
  */
+
+namespace Automattic\Jetpack\Search\Compatibility\Jetpack;
+
+use Automattic\Jetpack\Search\Instant_Search;
+
+add_filter( 'option_sidbars_widgets', __NAMESPACE__ . '\convert_old_jetpack_search_sidebar', 10, 2 );
+
+/**
+ * Map the old Jetpack Search sidebar to the new Instant Search sidebar.
+ *
+ * @param array $sidebars_widgets Value of `sidebars_widgets` option.
+ */
+function convert_old_jetpack_search_sidebar( $sidebars_widgets ) {
+	if ( ! is_array( $sidebars_widgets ) || ! isset( $sidebars_widgets[ Instant_Search::OLD_INSTANT_SEARCH_SIDEBAR ] ) ) {
+		return $sidebars_widgets;
+	}
+	$sidebars_widgets[ Instant_Search::INSTANT_SEARCH_SIDEBAR ] = $sidebars_widgets[ Instant_Search::OLD_INSTANT_SEARCH_SIDEBAR ];
+	return $sidebars_widgets;
+}
