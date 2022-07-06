e6131b6d3deca39907741657d775837291059424
retrofox <rdsuarez@gmail.com>
2022-07-06 11:25:36 -0300

    [not verified] update VideoPressIcon dependency for v6

--- projects/plugins/jetpack/extensions/blocks/videopress/v6/components/videopress-uploader/index.js
+++ projects/plugins/jetpack/extensions/blocks/videopress/v6/components/videopress-uploader/index.js
@@ -11,6 +11,7 @@ import { __ } from '@wordpress/i18n';
  */
 import { VideoPressIcon as icon } from '../../../../../shared/icons';
 import { useResumableUploader } from '../../hooks/use-uploader.js';
+import { VideoPressIcon as icon } from '../icons';
 
 const ALLOWED_MEDIA_TYPES = [ 'video' ];
 
