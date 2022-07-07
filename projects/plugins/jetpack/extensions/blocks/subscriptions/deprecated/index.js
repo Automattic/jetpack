89ff1050d9af65757d5a3a539afd567cf8db9d73
Maciej Grabowski <littlesoldiergraba@gmail.com>
2022-07-07 10:35:38 +0200

    [not verified] !WIP: creating deprecated version for subscribe block

--- projects/plugins/jetpack/extensions/blocks/subscriptions/deprecated/index.js
+++ projects/plugins/jetpack/extensions/blocks/subscriptions/deprecated/index.js
@@ -4,11 +4,13 @@ import deprecatedV3 from './v3';
 import deprecatedV4 from './v4';
 import deprecatedV5 from './v5';
 import deprecatedV6 from './v6';
+import deprecatedV7 from './v7';
 
 // Deprecations should run in reverse chronological order. Most probable
 // deprecations to run are the most recent. This ordering makes the process
 // a little more performant.
 export default [
+	deprecatedV7,
 	deprecatedV6,
 	deprecatedV5,
 	deprecatedV4,
---
+++ projects/plugins/jetpack/extensions/blocks/subscriptions/deprecated/v7/index.js
@@ -0,0 +1,14 @@
+/**
+ * Internal dependencies
+ */
+import save from './save';
+
+/**
+ * Deprecation reason
+ *
+ * Adding wrapper div so that saved block class styles are not
+ * overriden by user added styles (via Block settings, e.g. extra padding)
+ */
+export default {
+	save,
+};
