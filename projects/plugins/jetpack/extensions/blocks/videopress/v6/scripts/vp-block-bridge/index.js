af265e0f77c320bf8414488c491f75ccd1c97b4f
retrofox <rdsuarez@gmail.com>
2022-07-07 08:32:50 -0300

    [not verified] rebase: amit events to player

--- projects/plugins/jetpack/extensions/blocks/videopress/v6/scripts/vp-block-bridge/index.js
+++ projects/plugins/jetpack/extensions/blocks/videopress/v6/scripts/vp-block-bridge/index.js
@@ -2,14 +2,42 @@ const rawScript = `
 	function initWPBlockBridge() {
 		// Allowed events emitted by the videopress API.
 		const videoPressEventsMap = {
-			'playing': 'onVideoPressPlaying',
-			'pause': 'onVideoPressPause',
-			'seeking': 'onVideoPressSeeking',
-			'resize': 'onVideoPressResize',
-			'volumechange': 'onVideoPressVolumeChange',
-			'ended': 'onVideoPressEnded',
-			'videopress_progress': 'onVideoPressProgress',
-			'videopress_loading_state': 'onVideoPressLoadingState',
+			'playing': {
+				name: 'onVideoPressPlaying',
+				type: 'event',
+			},
+			'pause': {
+				name: 'onVideoPressPause',
+				type: 'event',
+			},
+			'seeking': {
+				name: 'onVideoPressSeeking',
+				type: 'event',
+			},
+			'resize': {
+				name: 'onVideoPressResize',
+				type: 'event',
+			},
+			'volumechange': {
+				name: 'onVideoPressVolumeChange',
+				type: 'event',
+			},
+			'ended': {
+				name: 'onVideoPressEnded',
+				type: 'event',
+			},
+			'videopress_progress': {
+				name: 'onVideoPressProgress',
+				type: 'event',
+			},
+			'videopress_loading_state': {
+				name: 'onVideoPressLoadingState',
+				type: 'event',
+			},
+			'videopress_action_play': {
+				name: 'onVideoPressActionPlay',
+				type: 'action',
+			},
 		};
 
 		const allowedVideoPressEvents = Object.keys( videoPressEventsMap );
@@ -22,26 +50,38 @@ const rawScript = `
 			}
 			
 			// Rename event with the 'onVideoPress' prefix.
-			const vpEventName = videoPressEventsMap[ eventName ];
+			const vpEvent = videoPressEventsMap[ eventName ];
+			const { name: vpEventName, type: vpEventType } = vpEvent;
 
-			// It preferrs to use the guid instead of the id.
-			const guid = data.id;
-			const originalEventName = data.event;
+			console.log( 'vpEventName: ', vpEventName );
 
-			// clean event data object
-			delete data.event;
-			delete data.id;
+			// Dispatch event to top when it's an event
+			if ( vpEventType === 'event' ) {
+				// It preferrs to use the guid instead of the id.
+				const guid = data.id;
+				const originalEventName = data.event;
+	
+				// clean event data object
+				delete data.event;
+				delete data.id;
+	
+				// Emite custom event with the event data.
+				const videoPressBlockEvent = new CustomEvent( vpEventName, {
+					detail: {
+						...data,
+						originalEventName,
+						guid,
+					},
+				} );
+	
+				top.dispatchEvent( videoPressBlockEvent );
+			}
 
-			// Emite custom event with the event data.
-			const videoPressBlockEvent = new CustomEvent( vpEventName, {
-				detail: {
-					...data,
-					originalEventName,
-					guid,
-				},
-			} );
-
-			top.dispatchEvent( videoPressBlockEvent );
+			// if ( vpEventType === 'action' ) {
+			// 	window.postMessage( {
+			// 		event: eventName,
+			// 	} );
+			// }
 		} );
 	}
 
