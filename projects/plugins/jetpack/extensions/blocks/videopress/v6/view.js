5837b902f2a22ec0d6460c1c0783992bc701c54b
retrofox <rdsuarez@gmail.com>
2022-07-08 16:13:14 -0300

    [not verified] implement autoplay hover feature

--- projects/plugins/jetpack/extensions/blocks/videopress/v6/view.js
+++ projects/plugins/jetpack/extensions/blocks/videopress/v6/view.js
@@ -11,11 +11,55 @@ import './style.scss';
 
 const BLOCK_CLASSNAME = 'wp-block-jetpack-videopress';
 
+function dispatchPlayerAction( ref, event, data = {} ) {
+	debug( 'dispatching %o action [%o]', event, data );
+	ref.contentWindow.postMessage(
+		{
+			event,
+			...data,
+		},
+		'*'
+	);
+}
+
 function initVideoPressBlocks( blockDOMReference ) {
 	debug( 'initializing block instance', blockDOMReference );
 
 	// Block initialized flag.
 	blockDOMReference.setAttribute( 'data-jetpack-block-initialized', 'true' );
+
+	let features;
+	try {
+		features = JSON.parse( blockDOMReference.dataset.features );
+		debug( 'features', features );
+	} catch ( e ) {
+		debug( 'error parsing json', e );
+	}
+
+	if ( ! features ) {
+		return;
+	}
+
+	const { autoplayHovering, autoplayHoveringStart, autoplayHoveringDuration } = features;
+
+	// Autoplay hover feature.
+	const playerIFrame = blockDOMReference.querySelector( 'iframe' );
+	if ( autoplayHovering && playerIFrame ) {
+		debug( 'adding autoplay hover feature' );
+
+		// Add the autoplay hover feature.
+		blockDOMReference.addEventListener( 'mouseenter', () => {
+			const currentTime = autoplayHoveringStart + autoplayHoveringDuration;
+			dispatchPlayerAction( playerIFrame, 'videopress_action_set_currenttime', {
+				currentTime,
+			} );
+			dispatchPlayerAction( playerIFrame, 'videopress_action_play' );
+		} );
+
+		blockDOMReference.addEventListener( 'mouseleave', () => {
+			dispatchPlayerAction( playerIFrame, 'videopress_action_pause' );
+		} );
+	}
 }
 
 document.addEventListener( 'DOMContentLoaded', function () {
