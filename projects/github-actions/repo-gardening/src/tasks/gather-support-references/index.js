7d8127cd7d521b33207dcb71c0cc8c59a5abdb40
Jeremy Herve <jeremy@jeremy.hu>
2022-07-12 18:51:15 +0200

    Only get all comments on issue once
    See https://github.com/Automattic/jetpack/pull/25066/files#r919122536

--- projects/github-actions/repo-gardening/src/tasks/gather-support-references/index.js
+++ projects/github-actions/repo-gardening/src/tasks/gather-support-references/index.js
@@ -6,18 +6,14 @@ const getComments = require( '../../get-comments' );
 /**
  * Search for a previous comment from this task in our issue.
  *
- * @param {GitHub} octokit - Initialized Octokit REST client.
- * @param {string} owner   - Repository owner.
- * @param {string} repo    - Repository name.
- * @param {string} number  - Issue number.
+ * @param {Array} issueComments - Array of all comments on that issue.
  * @returns {Promise<number>} Promise resolving to a number.
  */
-async function getListComment( octokit, owner, repo, number ) {
+async function getListComment( issueComments ) {
 	let commentID = 0;
 
 	debug( `gather-support-references: Looking for a previous comment from this task in our issue.` );
 
-	const issueComments = await getComments( octokit, owner, repo, number );
 	issueComments.map( comment => {
 		if (
 			comment.user.login === 'github-actions[bot]' &&
@@ -33,18 +29,18 @@ async function getListComment( octokit, owner, repo, number ) {
 /**
  * Scan the contents of the issue as well as all its comments, get all support references, and add them to an array of references.
  *
- * @param {GitHub} octokit - Initialized Octokit REST client.
- * @param {string} owner   - Repository owner.
- * @param {string} repo    - Repository name.
- * @param {string} number  - Issue number.
+ * @param {GitHub} octokit      - Initialized Octokit REST client.
+ * @param {string} owner        - Repository owner.
+ * @param {string} repo         - Repository name.
+ * @param {string} number       - Issue number.
+ * @param {Array} issueComments - Array of all comments on that issue.
  * @returns {Promise<Array>} Promise resolving to an array.
  */
-async function getIssueReferences( octokit, owner, repo, number ) {
+async function getIssueReferences( octokit, owner, repo, number, issueComments ) {
 	const ticketReferences = [];
 	const supportIds = [];
 	const referencesRegexP = /[0-9]*-(?:chat|hc|zen|zd)/gim;
 
-	const issueComments = await getComments( octokit, owner.login, repo, number );
 	debug( `gather-support-references: Getting references from comments.` );
 	issueComments.map( comment => {
 		ticketReferences.push( ...comment.body.matchAll( referencesRegexP ) );
@@ -89,10 +85,11 @@ async function getIssueReferences( octokit, owner, repo, number ) {
  * Creates or updates a comment on issue.
  *
  * @param {WebhookPayloadIssue} payload - Issue event payload.
- * @param {GitHub} octokit - Initialized Octokit REST client.
- * @param {Array} issueReferences - Array of support references.
+ * @param {GitHub} octokit              - Initialized Octokit REST client.
+ * @param {Array} issueReferences       - Array of support references.
+ * @param {Array} issueComments         - Array of all comments on that issue.
  */
-async function createOrUpdateComment( payload, octokit, issueReferences ) {
+async function createOrUpdateComment( payload, octokit, issueReferences, issueComments ) {
 	const { issue, repository } = payload;
 	const { number } = issue;
 	const { name: repo, owner } = repository;
@@ -114,7 +111,7 @@ async function createOrUpdateComment( payload, octokit, issueReferences ) {
 		body: comment,
 	};
 
-	const existingComment = await getListComment( octokit, ownerLogin, repo, number );
+	const existingComment = await getListComment( issueComments );
 
 	// If there is a comment already, update it.
 	if ( existingComment !== 0 ) {
@@ -146,10 +143,11 @@ async function gatherSupportReferences( payload, octokit ) {
 	const { number } = issue;
 	const { name: repo, owner } = repository;
 
-	const issueReferences = await getIssueReferences( octokit, owner, repo, number );
+	const issueComments = await getComments( octokit, owner.login, repo, number );
+	const issueReferences = await getIssueReferences( octokit, owner, repo, number, issueComments );
 	if ( issueReferences.length > 0 ) {
 		debug( `gather-support-references: Found ${ issueReferences.length } references.` );
-		await createOrUpdateComment( payload, octokit, issueReferences );
+		await createOrUpdateComment( payload, octokit, issueReferences, issueComments );
 	}
 }
 
