36c1f6fe2558d032496b87d17c12de663f536901
Manzoor Wani <manzoorwani.jk@gmail.com>
2022-07-12 16:35:49 +0530

    Convert AdminSection hero to TypeScript

--- projects/js-packages/components/components/admin-section/hero/index.tsx
+++ projects/js-packages/components/components/admin-section/hero/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './style.module.scss';
+import type { AdminSectionBaseProps } from '../types';
 
 /**
  * The wrapper component for a Hero Section to be used in admin pages.
  *
- * @param {object} props - Component properties.
+ * @param {AdminSectionBaseProps} props - Component properties.
  * @returns {React.Component} AdminSectionHero component.
  */
-const AdminSectionHero = props => {
-	const { children } = props;
+const AdminSectionHero: React.FC< AdminSectionBaseProps > = ( { children } ) => {
 	return <div className={ styles[ 'section-hero' ] }>{ children }</div>;
 };
 
