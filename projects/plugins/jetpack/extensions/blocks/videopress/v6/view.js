2c42f22c5f94e1416cec17c49ec7311eca739816
retrofox <rdsuarez@gmail.com>
2022-07-14 15:08:22 -0300

    [not verified] implement autoplay hovering in the front-end

--- projects/plugins/jetpack/extensions/blocks/videopress/v6/view.js
+++ projects/plugins/jetpack/extensions/blocks/videopress/v6/view.js
@@ -1,15 +1,64 @@
 /**
  * WordPress dependencies
  */
-import domReady from '@wordpress/dom-ready';
-import { __ } from '@wordpress/i18n';
-
+import debugFactory from 'debug';
 /**
  * Internal dependencies
  */
+import dispatchPlayerAction from './utils/dispatcher';
 import './style.scss';
 
-domReady( function () {
+const debug = debugFactory( 'jetpack:vpblock' );
+const BLOCK_CLASSNAME = 'wp-block-jetpack-videopress';
+
+function initVideoPressBlocks( blockDOMReference ) {
+	debug( 'initializing block instance', blockDOMReference );
+
+	// Block initialized flag.
+	blockDOMReference.setAttribute( 'data-jetpack-block-initialized', 'true' );
+
+	let features;
+	try {
+		features = JSON.parse( blockDOMReference.dataset.features );
+		debug( 'features', features );
+	} catch ( e ) {
+		debug( 'error parsing json', e );
+	}
+
+	if ( ! features ) {
+		return;
+	}
+
+	const { autoplayHovering } = features;
+
+	// Autoplay hover feature.
+	const playerIFrame = blockDOMReference.querySelector( 'iframe' );
+	if ( autoplayHovering && playerIFrame ) {
+		debug( 'adding autoplay hover feature' );
+
+		// Add the autoplay hover feature.
+		blockDOMReference.addEventListener( 'mouseenter', () => {
+			dispatchPlayerAction( playerIFrame, 'videopress_action_set_currenttime', {
+				currentTime: 0,
+			} );
+			dispatchPlayerAction( playerIFrame, 'videopress_action_play' );
+		} );
+
+		blockDOMReference.addEventListener( 'mouseleave', () => {
+			dispatchPlayerAction( playerIFrame, 'videopress_action_pause' );
+		} );
+	}
+}
+
+document.addEventListener( 'DOMContentLoaded', function () {
 	// eslint-disable-next-line no-console
-	console.log( __( 'VideoPress init', 'jetpack' ) );
+	debug( 'init' );
+
+	const instances = document.querySelectorAll(
+		`.${ BLOCK_CLASSNAME }:not([data-jetpack-block-initialized])`
+	);
+
+	if ( instances.length ) {
+		instances.forEach( initVideoPressBlocks );
+	}
 } );
