d8d1f65ef387d7e79c0ed4393e131c2a21fb03dc
retrofox <rdsuarez@gmail.com>
2022-07-18 16:30:43 -0300

    [not verified] implement autoplay hovering play/pause in the front-end

--- projects/plugins/jetpack/extensions/blocks/videopress/v6/view.js
+++ projects/plugins/jetpack/extensions/blocks/videopress/v6/view.js
@@ -6,93 +6,137 @@ import debugFactory from 'debug';
  * Internal dependencies
  */
 import { VIDEO_AUTOPLAY_DURATION } from './constants';
+import { rawBridgeScript } from './scripts/vp-block-bridge';
 import dispatchPlayerAction from './utils/dispatcher';
 import './style.scss';
 
 const debug = debugFactory( 'jetpack:vpblock' );
+
+// Define a debug instance for block bridge.
+window.debugBridgeInstance = debugFactory( 'jetpack:vp-block:bridge' );
+
 const BLOCK_CLASSNAME = 'wp-block-jetpack-videopress';
 
-const _video_instances = {
-	items: {},
-	current: null,
-};
+function isFrameAccessible( iFrameDomRef ) {
+	try {
+		return !! iFrameDomRef.contentDocument.body;
+	} catch ( e ) {
+		return false;
+	}
+}
 
 function tryToGetFeatures( domElement ) {
 	try {
 		return JSON.parse( domElement.dataset.features );
 	} catch ( e ) {
-		debug( 'error parsing json', e );
+		return debug( 'Error parsing json: ', e );
+	}
+}
+
+function tryToGetHTML( domElement ) {
+	try {
+		return JSON.parse( domElement.dataset.html );
+	} catch ( e ) {
+		debug( 'error parsing json: ', e );
 		return;
 	}
 }
 
-function initVideoPressBlocks( blockDOMReference ) {
-	debug( 'initializing block instance' );
-	const playerIFrame = blockDOMReference.querySelector( 'iframe' );
-	if ( ! playerIFrame ) {
-		return;
-	}
+function initVideoPressBlocks( blockDOMReference, clientId ) {
+	debug( 'initializing block player %o', clientId );
 
 	const features = tryToGetFeatures( blockDOMReference );
 	if ( ! features ) {
 		return;
 	}
 
-	const { autoplayHovering, autoplayHoveringStart, guid } = features;
-	const instanceId = `vpblock-${ Object.keys( _video_instances.items ).length }-${ guid }`;
-	_video_instances.items = {
-		..._video_instances.items,
-		[ instanceId ]: features,
-	};
+	const html = tryToGetHTML( blockDOMReference );
+	if ( ! html ) {
+		return debug( 'ERROR No html provided' );
+	}
 
-	blockDOMReference.setAttribute( 'data-jetpack-block-initialized', instanceId );
-	debug( '%o initialized', instanceId );
+	const playerIFrame = blockDOMReference.querySelector( 'iframe' );
+	if ( ! isFrameAccessible( playerIFrame ) ) {
+		return debug( 'iframe is not accessible' );
+	}
+
+	const { contentDocument, ownerDocument, contentWindow } = playerIFrame;
+	const __html = '<div class="videopress-player-container">' + html + '</div>';
+
+	const htmlDoc = `
+		<html lang=${ ownerDocument.documentElement.lang }>
+			<head>
+				<title>${ ownerDocument.documentElement.title }</title>
+				<style>body { margin: 0 padding: 0; border: 0; overflow: hidden;}</style>
+			</head>
+			<body>
+				${ __html }
+				<script type="text/javascript">
+					${ rawBridgeScript }
+				</script>
+			</body>
+		</html>
+	`;
+
+	contentDocument.open();
+	contentDocument.write( '<!DOCTYPE html>' + htmlDoc );
+	contentDocument.close();
+
+	// Hack to set current time on the video.
+	function positionatePlayer() {
+		dispatchPlayerAction( playerIFrame, 'vpBlockActionSetCurrentTime', {
+			currentTime: autoplayHoveringStart,
+		} );
+		dispatchPlayerAction( playerIFrame, 'vpBlockActionPause' );
+		setTimeout( () => {
+			// Auto clean up the listener.
+			contentWindow.removeEventListener( 'onVideoPressPlaying', positionatePlayer );
+		}, 0 );
+	}
+
+	function setInitialTime() {
+		contentWindow.addEventListener( 'onVideoPressPlaying', positionatePlayer );
+
+		// Hack: Play video to be able to set the current time.
+		dispatchPlayerAction( playerIFrame, 'vpBlockActionPlay' );
+
+		setTimeout( () => {
+			// Once the video current time is set, clean up the listener.
+			contentWindow.removeEventListener( 'onVideoPressLoadingState', setInitialTime );
+		}, 0 );
+	}
+	// End of hack.
+
+	const { autoplayHovering, autoplayHoveringStart } = features;
+	blockDOMReference.setAttribute( 'data-jetpack-block-initialized', true );
 
 	// Autoplay hover feature.
 	if ( autoplayHovering && playerIFrame ) {
-		debug( 'adding autoplay hover feature' );
+		debug( 'adding autoplay hovering feature' );
 
-		window.addEventListener( 'message', event => {
-			if ( ! _video_instances.current ) {
+		// When video is ready, set initial time position.
+		contentWindow.addEventListener( 'onVideoPressLoadingState', setInitialTime );
+
+		// Stop autoplay hovering after VIDEO_AUTOPLAY_DURATION seconds.
+		contentWindow.addEventListener( 'onVideoPressTimeUpdate', event => {
+			const { currentTime } = event.detail;
+			if ( currentTime <= autoplayHoveringStart + VIDEO_AUTOPLAY_DURATION ) {
 				return;
 			}
 
-			const { data } = event;
-			if ( ! [ 'videopress_timeupdate' ].includes( data?.event ) ) {
-				return;
-			}
-
-			const currentFeatures = _video_instances.items[ _video_instances.current ];
-			if ( ! currentFeatures ) {
-				return;
-			}
-
-			const { autoplayHoveringStart: currentStartTime } = currentFeatures;
-			const countDown =
-				data?.currentTime && currentStartTime + VIDEO_AUTOPLAY_DURATION - data.currentTime;
-
-			if ( data.event === 'videopress_timeupdate' && ! countDown ) {
-				return dispatchPlayerAction( playerIFrame, 'videopress_action_pause' );
-			}
+			dispatchPlayerAction( playerIFrame, 'vpBlockActionPause' );
 		} );
 
-		// Add the autoplay hover feature.
-		blockDOMReference.addEventListener( 'mouseenter', event => {
-			_video_instances.current = event.target.dataset.jetpackBlockInitialized;
-
-			dispatchPlayerAction( playerIFrame, 'videopress_action_set_currenttime', {
-				currentTime: autoplayHoveringStart,
-			} );
-			dispatchPlayerAction( playerIFrame, 'videopress_action_play' );
+		blockDOMReference.addEventListener( 'mouseenter', () => {
+			dispatchPlayerAction( playerIFrame, 'vpBlockActionPlay' );
 		} );
 
 		blockDOMReference.addEventListener( 'mouseleave', () => {
-			_video_instances.current = null;
-
-			dispatchPlayerAction( playerIFrame, 'videopress_action_set_currenttime', {
+			dispatchPlayerAction( playerIFrame, 'vpBlockActionSetCurrentTime', {
 				currentTime: autoplayHoveringStart,
 			} );
-			dispatchPlayerAction( playerIFrame, 'videopress_action_pause' );
+
+			dispatchPlayerAction( playerIFrame, 'vpBlockActionPause' );
 		} );
 	}
 }
@@ -102,21 +146,11 @@ document.onreadystatechange = function () {
 		return;
 	}
 
-	debug( 'init' );
-
-	const instances = document.querySelectorAll(
+	const wpPlayerElements = document.querySelectorAll(
 		`.${ BLOCK_CLASSNAME }:not([data-jetpack-block-initialized])`
 	);
 
-	if ( instances.length ) {
-		instances.forEach( initVideoPressBlocks );
-	}
-
-	const instancesDos = document.querySelectorAll(
-		`.${ BLOCK_CLASSNAME }:not([data-jetpack-block-initialized])`
-	);
-
-	if ( instancesDos.length ) {
-		instancesDos.forEach( initVideoPressBlocks );
+	if ( wpPlayerElements.length ) {
+		wpPlayerElements.forEach( initVideoPressBlocks );
 	}
 };
