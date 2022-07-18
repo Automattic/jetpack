03e5293fce112fa6691a205669655e66e287aee3
Adrian Moldovan <adim.moldovan@gmail.com>
2022-07-18 16:54:21 +0300

    Remove unnecessary call to `page.waitForTimeout()`

--- tools/e2e-commons/helpers/plan-helper.js
+++ tools/e2e-commons/helpers/plan-helper.js
@@ -537,10 +537,7 @@ export async function syncPlanData( page ) {
 		const planJson = await execWpCommand( 'option get jetpack_active_plan --format=json' );
 		bkPlan = JSON.parse( planJson );
 
-		logger.info( `PLANS: frontend: ${ frPlan }, backend: ${ bkPlan.product_slug }` );
+		logger.debug( `PLANS: frontend: ${ frPlan }, backend: ${ bkPlan.product_slug }` );
 		isSame = frPlan.trim() === bkPlan.product_slug.trim();
 	} while ( ! isSame );
-
-	// eslint-disable-next-line playwright/no-wait-for-timeout
-	await page.waitForTimeout( 1000 );
 }
