f849f69fd448b0418aa0825d482831d0bdefb55b
Daniel Post <daniel.post@automattic.com>
2022-07-22 13:28:23 +0200

    Add posts box

--- projects/plugins/social/src/js/store/selectors/shares-data.js
+++ projects/plugins/social/src/js/store/selectors/shares-data.js
@@ -1,13 +1,8 @@
 const sharesDataSelectors = {
-	getSharesCount: state => {
-		return (
-			( state.sharesData?.publicized_count || 0 ) +
-			( state.sharesData?.to_be_publicized_count || 0 )
-		);
-	},
-	isShareLimitEnabled: state => {
-		return state.sharesData?.is_share_limit_enabled || false;
-	},
+	getSharesCount: state =>
+		( state.sharesData?.publicized_count || 0 ) + ( state.sharesData?.to_be_publicized_count || 0 ),
+	getPostsCount: state => state.sharesData?.shared_posts_count || 0,
+	isShareLimitEnabled: state => state.sharesData?.is_share_limit_enabled || false,
 };
 
 export default sharesDataSelectors;
