a8ef71836eb9da9857d70b07943322ed4401f902
Dale du Preez <dale@automattic.com>
2022-07-22 12:45:33 +0200

    Remove minimum cache timeout and improve documentation

--- projects/plugins/jetpack/_inc/lib/class-jetpack-podcast-helper.php
+++ projects/plugins/jetpack/_inc/lib/class-jetpack-podcast-helper.php
@@ -9,8 +9,6 @@
  * Class Jetpack_Podcast_Helper
  */
 class Jetpack_Podcast_Helper {
-	const MIN_PODCAST_CACHE_TIMEOUT = 10 * MINUTE_IN_SECONDS;
-
 	/**
 	 * The RSS feed of the podcast.
 	 *
@@ -22,11 +20,13 @@ class Jetpack_Podcast_Helper {
 	 * The number of seconds to cache the podcast feed data.
 	 * This value defaults to 12 hours, which is the default for RSS feeds in WordPress.
 	 * The value can be overridden specifically for podcasts using the
-	 * `jetpack_podcast_feed_cache_timeout` filter, in which case the value
-	 * must be greater than {@see self::MIN_PODCAST_CACHE_TIMEOUT}, or the value
+	 * `jetpack_podcast_feed_cache_timeout` filter. Note that the cache timeout value
 	 * for all RSS feeds can be modified using the `wp_feed_cache_transient_lifetime`
 	 * filter from WordPress core.
+	 * Note that this code depends on the site using the default `WP_Feed_Cache_Transient`
+	 * cache implementation for RSS feeds.
 	 *
+	 * @see https://developer.wordpress.org/reference/hooks/wp_feed_cache_transient_lifetime/
 	 * @see WP_Feed_Cache_Transient
 	 *
 	 * @var int|null
@@ -42,9 +42,12 @@ class Jetpack_Podcast_Helper {
 		$this->feed = esc_url_raw( $feed );
 
 		/**
-		 * Filter the number of seconds to cache a specific podcast URL for. The returned value will be ignored if it is not a valid integer.
+		 * Filter the number of seconds to cache a specific podcast URL for. The returned value will be ignored if it is null or not a valid integer.
+		 * Note that we will only use this value if it is less than the RSS feed cache timeout returned from the `wp_feed_cache_transient_lifetime`
+		 * filter in WordPress core.
 		 *
 		 * @since $$next-version$$
+		 * @see https://developer.wordpress.org/reference/hooks/wp_feed_cache_transient_lifetime/
 		 *
 		 * @param int|null $cache_timeout The number of seconds to cache the podcast data. Default value is 12 hours in seconds.
 		 * @param string   $podcast_url   The URL of the podcast feed.
@@ -52,9 +55,8 @@ class Jetpack_Podcast_Helper {
 		$podcast_cache_timeout = apply_filters( 'jetpack_podcast_feed_cache_timeout', $this->cache_timeout, $this->feed );
 
 		// Make sure we force $this->cache_timeout to be int|null.
-		// Also ensure we enforce our minimum cache timeout.
 		if ( is_int( $$podcast_cache_timeout ) ) {
-			$this->cache_timeout = max( (int) $podcast_cache_timeout, self::MIN_PODCAST_CACHE_TIMEOUT );
+			$this->cache_timeout = (int) $podcast_cache_timeout;
 		} else {
 			$this->cache_timeout = null;
 		}
