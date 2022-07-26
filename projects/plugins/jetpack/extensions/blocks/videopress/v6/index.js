e18f9b214e2a7ab04bd6993bd2067d201c9b9711
retrofox <rdsuarez@gmail.com>
2022-07-26 07:22:30 -0300

    [not verified] export blockType const

--- projects/plugins/jetpack/extensions/blocks/videopress/v6/index.js
+++ projects/plugins/jetpack/extensions/blocks/videopress/v6/index.js
@@ -14,6 +14,7 @@ import './style.scss';
 
 export const name = 'videopress-block';
 export const namespace = 'jetpack';
+export const blockType = `${ namespace }/${ name }`;
 
 export const title = __( 'VideoPress', 'jetpack' );
 export const description = __(
