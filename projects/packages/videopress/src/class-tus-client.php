b18f6a0556ec4101fe33b4635fa8c1ea3d7e8b53
Leo Germani <leogermani@automattic.com>
2022-07-27 18:08:07 -0300

    Introduces the Uploader tool

--- projects/packages/videopress/src/class-tus-client.php
+++ projects/packages/videopress/src/class-tus-client.php
@@ -5,16 +5,18 @@
  * @package automattic/jetpack-videopress
  */
 
+// phpcs:disable
+
 namespace Automattic\Jetpack\VideoPress;
 
 use Carbon\Carbon;
-use TusPhp\Exception\TusException;
-use TusPhp\Exception\FileException;
 use GuzzleHttp\Exception\ClientException;
-use TusPhp\Exception\ConnectionException;
-use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\GuzzleException;
 use Symfony\Component\HttpFoundation\Response as HttpResponse;
+use TusPhp\Exception\ConnectionException;
+use TusPhp\Exception\FileException;
+use TusPhp\Exception\TusException;
 
 /**
  * VideoPress Tus Client class.
@@ -24,6 +26,40 @@ use Symfony\Component\HttpFoundation\Response as HttpResponse;
  * * Store the key specific token our server sends after we create the upload and use it in patch requests.
  */
 class Tus_Client extends \TusPhp\Tus\Client {
+
+	/**
+	 * The details of the server response about the uploaded file
+	 *
+	 * @var array
+	 */
+	protected $uploaded_video_details = null;
+
+	/**
+	 * Sets the uploaded video details
+	 *
+	 * Values come wrapped in arrays because are result of a getHeader() call
+	 *
+	 * @param array $guid The guid of the created video.
+	 * @param array $media_id The ID of the attachment created.
+	 * @param array $upload_src The video URL.
+	 * @return void
+	 */
+	protected function set_uploaded_video_details( $guid, $media_id, $upload_src ) {
+		$this->uploaded_video_details = array(
+			'guid'       => $guid[0],
+			'media_id'   => $media_id[0],
+			'upload_src' => $upload_src[0],
+		);
+	}
+
+	/**
+	 * Gets the details of the uploaded video
+	 *
+	 * @return array
+	 */
+	public function get_uploaded_video_details() {
+		return $this->uploaded_video_details;
+	}
 
 	/**
 	 * Create resource with POST request and upload data using the creation-with-upload extension.
@@ -36,63 +72,69 @@ class Tus_Client extends \TusPhp\Tus\Client {
 	 * @throws GuzzleException
 	 *
 	 * @return array [
-	 *	 'location' => string,
-	 *	 'offset' => int
+	 *   'location' => string,
+	 *   'offset' => int
 	 * ]
 	 */
-	public function createWithUpload(string $key, int $bytes = -1): array {
+	public function createWithUpload( string $key, int $bytes = -1 ): array {
 		$bytes = $bytes < 0 ? $this->fileSize : $bytes;
 
-		$headers = $this->headers + [
-			'Upload-Length' => $this->fileSize,
-			'Upload-Key' => $key,
+		$headers = $this->headers + array(
+			'Upload-Length'   => $this->fileSize,
+			'Upload-Key'      => $key,
 			'Upload-Checksum' => $this->getUploadChecksumHeader(),
 			'Upload-Metadata' => $this->getUploadMetadataHeader(),
-		];
+		);
 
 		$data = '';
-		if ($bytes > 0) {
-			$data = $this->getData(0, $bytes);
+		if ( $bytes > 0 ) {
+			$data = $this->getData( 0, $bytes );
 
-			$headers += [
-				'Content-Type' => self::HEADER_CONTENT_TYPE,
-				'Content-Length' => \strlen($data),
-			];
+			$headers += array(
+				'Content-Type'   => self::HEADER_CONTENT_TYPE,
+				'Content-Length' => \strlen( $data ),
+			);
 		}
 
-		if ($this->isPartial()) {
-			$headers += ['Upload-Concat' => 'partial'];
+		if ( $this->isPartial() ) {
+			$headers += array( 'Upload-Concat' => 'partial' );
 		}
 
 		try {
-			$response = $this->getClient()->post($this->apiPath, [
-				'body' => $data,
-				'headers' => $headers,
-			]);
-		} catch (ClientException $e) {
+			$response = $this->getClient()->post(
+				$this->apiPath,
+				array(
+					'body'    => $data,
+					'headers' => $headers,
+				)
+			);
+		} catch ( ClientException $e ) {
 			$response = $e->getResponse();
 		}
 
 		$statusCode = $response->getStatusCode();
 
-		if (HttpResponse::HTTP_CREATED !== $statusCode) {
-			throw new FileException('Unable to create resource.');
+		if ( HttpResponse::HTTP_CREATED !== $statusCode ) {
+			throw new FileException( 'Unable to create resource.' );
 		}
 
-		$uploadOffset   = $bytes > 0 ? current($response->getHeader('upload-offset')) : 0;
-		$uploadLocation = current($response->getHeader('location'));
+		$uploadOffset   = $bytes > 0 ? current( $response->getHeader( 'upload-offset' ) ) : 0;
+		$uploadLocation = current( $response->getHeader( 'location' ) );
 
-		$this->getCache()->set($this->getKey(), [
-			'location' => $uploadLocation,
-			'expires_at' => Carbon::now()->addSeconds($this->getCache()->getTtl())->format($this->getCache()::RFC_7231),
-			// VideoPress mod: Store key specific token for future usage
-			'token_for_key' => $response->getHeader('x-videopress-upload-key-token'),
-		]);
+		$this->getCache()->set(
+			$this->getKey(),
+			array(
+				'location'      => $uploadLocation,
+				'expires_at'    => Carbon::now()->addSeconds( $this->getCache()->getTtl() )->format( $this->getCache()::RFC_7231 ),
+				// VideoPress mod: Store key specific token for future usage
+				'token_for_key' => $response->getHeader( 'x-videopress-upload-key-token' ),
+			)
+		);
 
-		return [
+		return array(
 			'location' => $uploadLocation,
-			'offset' => $uploadOffset,
-		];
+			'offset'   => $uploadOffset,
+		);
 	}
 
 	/**
@@ -104,16 +146,16 @@ class Tus_Client extends \TusPhp\Tus\Client {
 	 * @return void
 	 */
 	public function delete() {
-		$headers = $this->headers + [
+		$headers = $this->headers + array(
 			'X-HTTP-Method-Override' => 'DELETE', // VideoPress mod: add method override header.
-		];
+		);
 		try {
-			$this->getClient()->post($this->getUrl()); // VideoPress mod: use post() instead of delete()
-		} catch (ClientException $e) {
+			$this->getClient()->post( $this->getUrl() ); // VideoPress mod: use post() instead of delete()
+		} catch ( ClientException $e ) {
 			$statusCode = $e->getResponse()->getStatusCode();
 
-			if (HttpResponse::HTTP_NOT_FOUND === $statusCode || HttpResponse::HTTP_GONE === $statusCode) {
-				throw new FileException('File not found.');
+			if ( HttpResponse::HTTP_NOT_FOUND === $statusCode || HttpResponse::HTTP_GONE === $statusCode ) {
+				throw new FileException( 'File not found.' );
 			}
 		}
 	}
@@ -127,17 +169,17 @@ class Tus_Client extends \TusPhp\Tus\Client {
 	 * @return int
 	 */
 	protected function sendHeadRequest(): int {
-		$headers = $this->headers + [
+		$headers    = $this->headers + array(
 			'X-HTTP-Method-Override' => 'HEAD', // VideoPress mod: add method override header.
-		];
-		$response   = $this->getClient()->get($this->getUrl(), [ 'headers' => $headers ]); // VideoPress mod: use get() instead of head()
+		);
+		$response   = $this->getClient()->get( $this->getUrl(), array( 'headers' => $headers ) ); // VideoPress mod: use get() instead of head()
 		$statusCode = $response->getStatusCode();
 
-		if (HttpResponse::HTTP_OK !== $statusCode) {
-			throw new FileException('File not found.');
+		if ( HttpResponse::HTTP_OK !== $statusCode ) {
+			throw new FileException( 'File not found.' );
 		}
 
-		return (int) current($response->getHeader('upload-offset'));
+		return (int) current( $response->getHeader( 'upload-offset' ) );
 	}
 
 	/**
@@ -153,37 +195,49 @@ class Tus_Client extends \TusPhp\Tus\Client {
 	 *
 	 * @return int
 	 */
-	protected function sendPatchRequest(int $bytes, int $offset): int {
-		$data	= $this->getData($offset, $bytes);
-		$headers = $this->headers + [
-			'Content-Type' => self::HEADER_CONTENT_TYPE,
-			'Content-Length' => \strlen($data),
-			'Upload-Checksum' => $this->getUploadChecksumHeader(),
+	protected function sendPatchRequest( int $bytes, int $offset ): int {
+		$data    = $this->getData( $offset, $bytes );
+		$headers = $this->headers + array(
+			'Content-Type'           => self::HEADER_CONTENT_TYPE,
+			'Content-Length'         => \strlen( $data ),
+			'Upload-Checksum'        => $this->getUploadChecksumHeader(),
 			'X-HTTP-Method-Override' => 'PATCH', // VideoPress mod: add method override header.
 
-		];
+		);
 
 		// VideoPress mod: override token with key specific token.
-		$token = $this->getCache()->get($this->getKey())['token_for_key'] ?? null;
+		$token                                = $this->getCache()->get( $this->getKey() )['token_for_key'] ?? null;
 		$headers['x-videopress-upload-token'] = $token;
 
-		if ($this->isPartial()) {
-			$headers += ['Upload-Concat' => self::UPLOAD_TYPE_PARTIAL];
+		if ( $this->isPartial() ) {
+			$headers += array( 'Upload-Concat' => self::UPLOAD_TYPE_PARTIAL );
 		} else {
-			$headers += ['Upload-Offset' => $offset];
+			$headers += array( 'Upload-Offset' => $offset );
 		}
 
 		try {
-			$response = $this->getClient()->post($this->getUrl(), [ // VideoPress mod: use post instead of patch.
-				'body' => $data,
-				'headers' => $headers,
-			]);
+			$response = $this->getClient()->post( // VideoPress mod: use post instead of patch.
+				$this->getUrl(),
+				array(
+					'body'    => $data,
+					'headers' => $headers,
+				)
+			);
 
-			return (int) current($response->getHeader('upload-offset'));
-		} catch (ClientException $e) {
-			throw $this->handleClientException($e);
-		} catch (ConnectException $e) {
-			throw new ConnectionException("Couldn't connect to server.");
+			// VideoPress mod: Check for headers that indicate the end of the process.
+			$guid       = $response->getHeader( 'x-videopress-upload-guid' );
+			$media_id   = $response->getHeader( 'x-videopress-upload-media-id' );
+			$upload_src = $response->getHeader( 'x-videopress-upload-src-url' );
+
+			if ( $guid && $media_id && $upload_src ) {
+				$this->set_uploaded_video_details( $guid, $media_id, $upload_src );
+			}
+
+			return (int) current( $response->getHeader( 'upload-offset' ) );
+		} catch ( ClientException $e ) {
+			throw $this->handleClientException( $e );
+		} catch ( ConnectException $e ) {
+			throw new ConnectionException( "Couldn't connect to server." );
 		}
 	}
 
--- projects/packages/videopress/src/class-uploader.php
+++ projects/packages/videopress/src/class-uploader.php
@@ -7,9 +7,11 @@
 
 namespace Automattic\Jetpack\VideoPress;
 
-use TusPhp\Exception\TusException;
-use TusPhp\Exception\FileException;
+use Automattic\Jetpack\Connection\Client;
+use Jetpack_Options;
 use TusPhp\Exception\ConnectionException;
+use TusPhp\Exception\FileException;
+use TusPhp\Exception\TusException;
 
 /**
  * VideoPress Uploader class
@@ -25,98 +27,245 @@ class Uploader {
 	 */
 	const UPLOADED_KEY = '_videopress_uploaded_id';
 
+	/**
+	 * The chunk size of each upload step
+	 *
+	 * @var int
+	 */
 	const CHUNK_SIZE = 1000000;
 
+	/**
+	 * The Tus Client instance
+	 *
+	 * @var Tus_Client
+	 */
 	protected $client = null;
 
+	/**
+	 * The attachment ID
+	 *
+	 * @var int
+	 */
+	protected $attachment_id;
+
+	/**
+	 * Checks whether this feature is supported by the server
+	 *
+	 * @return boolean
+	 */
 	public static function is_supported() {
-		return true;
+		return version_compare( phpversion(), '7', '>=' );
 	}
 
+	/**
+	 * Constructs the object
+	 *
+	 * @throws Upload_Exception If attachment is invalid or server does not support it.
+	 * @param int $attachment_id The ID of the video attachment we want to upload to VideoPress.
+	 */
 	public function __construct( $attachment_id ) {
+		if ( ! $this->is_supported() ) {
+			throw new Upload_Exception( __( 'VideoPress uploader requires PHP 7 or higher', 'jetpack-videopress' ) );
+		}
 		$this->attachment_id = $attachment_id;
+		if ( ! $this->get_file_path() ) {
+			throw new Upload_Exception( __( 'Invalid attachment ID', 'jetpack-videopress' ) );
+		}
+		if ( ! is_readable( $this->get_file_path() ) ) {
+			throw new Upload_Exception( __( 'File not found', 'jetpack-videopress' ) );
+		}
+		if ( ! $this->file_has_supported_mime_type() ) {
+			throw new Upload_Exception( __( 'Mime type not supported', 'jetpack-videopress' ) );
+		}
 	}
 
-	public function get_filename( $id ) {
+	/**
+	 * Gets the path of the video file
+	 *
+	 * @return string
+	 */
+	public function get_file_path() {
 		return get_attached_file( $this->attachment_id );
 	}
 
+	/**
+	 * Gets the mime type of the attachment
+	 *
+	 * @return string
+	 */
+	public function get_file_mime_type() {
+		return get_post_mime_type( $this->attachment_id );
+	}
+
+	/**
+	 * Gets the name of the video file
+	 *
+	 * @return string
+	 */
+	public function get_file_name() {
+		return basename( $this->get_file_path() );
+	}
+
+	/**
+	 * Gets the size of the video file
+	 *
+	 * @return int
+	 */
+	public function get_file_size() {
+		return filesize( $this->get_file_path() );
+	}
+
+	/**
+	 * Checks if the mime type of the attachment is supported to be uploaded
+	 *
+	 * @return boolean
+	 */
+	public function file_has_supported_mime_type() {
+		return 0 === strpos( $this->get_file_mime_type(), 'video/' );
+	}
+
+	/**
+	 * Gets the VideoPress upload token
+	 *
+	 * @throws Upload_Exception If it fails to fetch the token.
+	 *
+	 * @return string
+	 */
 	public function get_upload_token() {
-		$blog_id  = Jetpack_Options::get_option('id');
+		$blog_id  = Jetpack_Options::get_option( 'id' );
 		$endpoint = "sites/{$blog_id}/media/videopress-upload-jwt";
 		$args     = array( 'method' => 'POST' );
 		$result   = Client::wpcom_json_api_request_as_blog( $endpoint, 'v2', $args, null, 'wpcom' );
 		if ( is_wp_error( $result ) ) {
-			wp_send_json_error( array( 'message' => __( 'Could not obtain a VideoPress upload JWT. Please try again later.', 'jetpack' ) ) );
-			return;
+			throw new Upload_Exception(
+				__( 'Could not obtain a VideoPress upload JWT. Please try again later.', 'jetpack-videopress' ) .
+				'(' . $result->get_error_message() . ')'
+			);
 		}
 
 		$response = json_decode( $result['body'], true );
 
 		if ( empty( $response['upload_token'] ) ) {
-			wp_send_json_error( array( 'message' => __( 'Could not obtain a VideoPress upload JWT. Please try again later. (empty upload token)', 'jetpack' ) ) );
-			return;
+			throw new Upload_Exception( __( 'Could not obtain a VideoPress upload JWT. Please try again later. (empty upload token)', 'jetpack-videopress' ) );
 		}
 
 		return $response['upload_token'];
 
 	}
 
+	/**
+	 * Gets a unique upload key for this attachment
+	 *
+	 * @return string
+	 */
 	public function get_key() {
-		return sprintf( 's-%d-v-%d', Jetpack_Options::get_option('id'), $this->attachment_id );
+		return sprintf( 's-%d-v-%d', Jetpack_Options::get_option( 'id' ), $this->attachment_id );
 	}
 
-	public function mark_as_uploaded( $new_attachment_id ) {
+	/**
+	 * Sets the current attachment as uploaded and stores the ID of the VideoPress video attachment ID
+	 *
+	 * @param int $new_attachment_id The ID of the new attachment created to hold the VideoPress video.
+	 * @return void
+	 */
+	protected function mark_as_uploaded( $new_attachment_id ) {
 		update_post_meta( $this->attachment_id, self::UPLOADED_KEY, $new_attachment_id );
 	}
 
+	/**
+	 * Checks whether this attachment was uploaded before
+	 *
+	 * @return boolean
+	 */
 	public function is_uploaded() {
 		return false !== get_post_meta( $this->attachment_id, self::UPLOADED_KEY, true );
 	}
 
+	/**
+	 * Gets the ID of the VideoPress video attachment in case this attachment was uploaded before
+	 *
+	 * @return boolean|string False if value is absent. Post ID on success.
+	 */
 	public function get_uploaded_attachment_id() {
 		return get_post_meta( $this->attachment_id, self::UPLOADED_KEY, true );
 	}
 
+	/**
+	 * Retrieves the instance of the Tus_Client
+	 *
+	 * @return Tus_Client
+	 */
 	public function get_client() {
-		if ( ! is_null( $this->client ) ) {
+		if ( $this->client !== null ) {
 			return $this->client;
 		}
-		$options = array(
+		$options      = array(
 			'headers' => array(
 				'x-videopress-upload-token' => $this->get_upload_token(),
 			),
 		);
-		$endpoint = sprintf( 'https://public-api.wordpress.com/rest/v1.1/video-uploads/%d', Jetpack_Options::get_option('id') );
-		$client = new \Automattic\Jetpack\VideoPress\Tus_Client( $endpoint, $options );
-		$this->client = $client->setApiPath('')->setKey( $this->get_key() );
+		$endpoint     = sprintf( 'https://public-api.wordpress.com/rest/v1.1/video-uploads/%d', Jetpack_Options::get_option( 'id' ) );
+		$client       = new \Automattic\Jetpack\VideoPress\Tus_Client( $endpoint, $options );
+		$this->client = $client->setApiPath( '' )->setKey( $this->get_key() );
 
-		$this->client->getCache()->setCacheDir( trailingslashit( sys_get_temp_dir() ) )->setCacheFile( $key . '.tus.cache' );
+		$this->client->getCache()->setCacheDir( trailingslashit( sys_get_temp_dir() ) )->setCacheFile( $this->get_key() . '.tus.cache' );
+
 		return $this->client;
 	}
 
+	/**
+	 * Uploads a chunk of the file
+	 *
+	 * @return array With the status of the upload
+	 */
 	public function upload() {
+		if ( $this->is_uploaded() ) {
+			return $this->check_status();
+		}
 		try {
-			$this->get_client()->file( $this->get_file_path(), $this->get_filename() );
+			$this->get_client()->file( $this->get_file_path(), $this->get_file_name() );
 
-			$bytesUploaded = $client->upload( self::CHUNK_SIZE);
+			$bytes_uploaded = $this->get_client()->upload( self::CHUNK_SIZE );
+
+			if ( $bytes_uploaded === $this->get_file_size() ) {
+				$this->mark_as_uploaded( $this->get_client()->get_uploaded_video_details()['media_id'] );
+				return array(
+					'status'           => 'complete',
+					'bytes_uploaded'   => $bytes_uploaded,
+					'upload_key'       => $this->get_key(),
+					'uploaded_details' => $this->get_client()->get_uploaded_video_details(),
+				);
+			}
 
 			return array(
-				'status' => 'uploading',
-				'bytes_uploaded' => $bytesUploaded,
-				'upload_key' => $uploadKey
+				'status'         => 'uploading',
+				'bytes_uploaded' => $bytes_uploaded,
+				'upload_key'     => $this->get_key(),
 			);
-		} catch (ConnectionException | FileException | TusException $e) {
+		} catch ( Upload_Exception | ConnectionException | FileException | TusException $e ) { // phpcs:ignore PHPCompatibility.ControlStructures.NewMultiCatch.Found
 			return array(
-				'status' => 'error',
+				'status'         => 'error',
 				'bytes_uploaded' => -1,
-				'upload_key' => '',
-				'error' => $e->getMessage(),
+				'upload_key'     => $this->get_key(),
+				'error'          => $e->getMessage(),
 			);
 		}
 	}
 
+	/**
+	 * Checks the status of the upload of this attachment
+	 *
+	 * @return array
+	 */
 	public function check_status() {
+
+		if ( $this->is_uploaded() ) {
+			return array(
+				'status'           => 'uploaded',
+				'upload_key'       => $this->get_key(),
+				'uploaded_post_id' => $this->get_uploaded_attachment_id(),
+			);
+		}
 
 		try {
 			$offset = $this->get_client()->getOffset();
@@ -124,20 +273,20 @@ class Uploader {
 			$offset = false === $offset ? 0 : $offset;
 
 			return array(
-				'status' => $status,
+				'status'         => $status,
 				'bytes_uploaded' => $offset,
-				'upload_key' => $uploadKey,
+				'upload_key'     => $this->get_key(),
 			);
-		} catch (ConnectException $e) {
+		} catch ( Upload_Exception | ConnectException $e ) { // phpcs:ignore PHPCompatibility.ControlStructures.NewMultiCatch.Found
 			return array(
-				'status' => 'error',
+				'status'         => 'error',
 				'bytes_uploaded' => -1,
 			);
-		} catch (FileException $e) {
+		} catch ( FileException $e ) {
 			return array(
-				'status' => 'resume',
+				'status'         => 'resume',
 				'bytes_uploaded' => 0,
-				'upload_key' => '',
+				'upload_key'     => '',
 			);
 		}
 	}
