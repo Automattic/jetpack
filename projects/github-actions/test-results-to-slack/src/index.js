e2601ef75937e9a1002834b4aa16e08da2aafb64
Adrian Moldovan <adim.moldovan@gmail.com>
2022-07-28 17:36:21 +0300

    Send test message

--- projects/github-actions/test-results-to-slack/src/index.js
+++ projects/github-actions/test-results-to-slack/src/index.js
@@ -1,14 +1,39 @@
 const { setFailed, getInput } = require( '@actions/core' );
 const { context } = require( '@actions/github' );
+const { WebClient, retryPolicies, LogLevel } = require( '@slack/web-api' );
 
 ( async function main() {
-	const token = getInput( 'github_token' );
-	if ( ! token ) {
-		setFailed( 'main: Input `github_token` is required' );
+	const ghToken = getInput( 'github_token' );
+	if ( ! ghToken ) {
+		setFailed( 'Input `github_token` is required' );
+		return;
+	}
+
+	const slackToken = getInput( 'slack_token' );
+	if ( ! slackToken ) {
+		setFailed( 'Input `slack_token` is required' );
+		return;
+	}
+
+	const slackChannel = getInput( 'slack_channel' );
+	if ( ! slackChannel ) {
+		setFailed( 'Input `slack_channel` is required' );
 		return;
 	}
 
 	process.stdout.write(
 		`Received event = '${ context.eventName }', action = '${ context.payload.action }'`
 	);
+
+	const slackClient = new WebClient( slackToken, {
+		retryConfig: retryPolicies.rapidRetryPolicy,
+		logLevel: LogLevel.ERROR,
+	} );
+
+	await slackClient.chat.postMessage( {
+		text: `Received event = '${ context.eventName }', action = '${ context.payload.action }'`,
+		slackChannel,
+		username: 'Tests reporter',
+		icon_emoji: ':jetpack:',
+	} );
 } )();
