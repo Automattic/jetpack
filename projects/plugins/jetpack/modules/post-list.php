cc6b474b4e83cf294974eb1d36f084c87c0cc900
Steve D <33553323+sdixon194@users.noreply.github.com>
2022-07-29 09:39:42 -0400

    Update projects/plugins/jetpack/modules/post-list.php
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>

--- projects/plugins/jetpack/modules/post-list.php
+++ projects/plugins/jetpack/modules/post-list.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Module Name: Post List
- * Module Description: Enhance the classic view of the Admin section of your WordPress site.
+ * Module Description: Display extra information alongside each post in your dashboard's Posts screen.
  * Sort Order: 31
  * Recommendation Order: 12
  * First Introduced: 11.3
