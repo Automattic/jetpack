ab770dcabd37ae261ad0a4c3d49454838c6e3ad3
Chris R <chris.rosser@automattic.com>
2022-07-29 14:42:35 +1200

    Search: changelog and README for v1.1.0-beta plugin release (#25319)

--- projects/packages/connection/src/class-package-version.php
+++ projects/packages/connection/src/class-package-version.php
@@ -12,7 +12,7 @@ namespace Automattic\Jetpack\Connection;
  */
 class Package_Version {
 
-	const PACKAGE_VERSION = '1.41.8-alpha';
+	const PACKAGE_VERSION = '1.41.8';
 
 	const PACKAGE_SLUG = 'connection';
 
