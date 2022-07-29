457c34d6116355afc27210fbb7ca0461ab57b702
Adrian Moldovan <adim.moldovan@gmail.com>
2022-07-29 15:14:42 +0300

    Get jobs conclusions

--- projects/github-actions/test-results-to-slack/src/index.js
+++ projects/github-actions/test-results-to-slack/src/index.js
@@ -1,39 +1,48 @@
 const { setFailed, getInput } = require( '@actions/core' );
-const { context } = require( '@actions/github' );
+const { context, getOctokit } = require( '@actions/github' );
 const { WebClient, retryPolicies, LogLevel } = require( '@slack/web-api' );
 
 ( async function main() {
 	const ghToken = getInput( 'github_token' );
 	if ( ! ghToken ) {
-		setFailed( 'Input `github_token` is required' );
+		setFailed( 'main: Input `github_token` is required' );
 		return;
 	}
 
-	const slackToken = getInput( 'slack_token' );
-	if ( ! slackToken ) {
+	const token = getInput( 'slack_token' );
+	if ( ! token ) {
 		setFailed( 'Input `slack_token` is required' );
 		return;
 	}
 
-	const slackChannel = getInput( 'slack_channel' );
-	if ( ! slackChannel ) {
+	const channel = getInput( 'slack_channel' );
+	if ( ! channel ) {
 		setFailed( 'Input `slack_channel` is required' );
 		return;
 	}
 
-	process.stdout.write(
-		`Received event = '${ context.eventName }', action = '${ context.payload.action }'`
-	);
+	// eslint-disable-next-line new-cap
+	const octokit = new getOctokit( ghToken );
 
-	const slackClient = new WebClient( slackToken, {
+	const response = await octokit.rest.actions.listJobsForWorkflowRun( {
+		owner: context.payload.repository.owner.login,
+		repo: context.payload.repository.name,
+		run_id: context.runId,
+	} );
+
+	process.stdout.write( JSON.stringify( response ) );
+
+	const conclusions = [ ...new Set( response.data.jobs.map( job => job.conclusion ) ) ];
+
+	const client = new WebClient( token, {
 		retryConfig: retryPolicies.rapidRetryPolicy,
 		logLevel: LogLevel.ERROR,
 	} );
 
-	await slackClient.chat.postMessage( {
-		text: `Received event = '${ context.eventName }', action = '${ context.payload.action }'`,
-		channel: slackChannel,
+	await client.chat.postMessage( {
+		text: `Received event: '${ context.eventName }', action: '${ context.payload.action }', conclusions: ${ conclusions }`,
+		channel,
 		username: 'Tests reporter',
-		icon_emoji: ':jetpack:',
+		icon_emoji: ':bot:',
 	} );
 } )();
