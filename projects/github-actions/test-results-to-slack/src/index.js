37779998d96ac99140c96b6b28615afc5fd00ae4
Adrian Moldovan <adim.moldovan@gmail.com>
2022-08-01 12:58:38 +0300

    Format messages

--- projects/github-actions/test-results-to-slack/src/index.js
+++ projects/github-actions/test-results-to-slack/src/index.js
@@ -40,13 +40,13 @@ const { WebClient, retryPolicies, LogLevel } = require( '@slack/web-api' );
 
 	if ( context.eventName === 'pull_request' ) {
 		const { pull_request } = context.payload;
-		event = `PR ${ pull_request.number }: \`[${ pull_request.title }](${ pull_request.html_url })\``;
+		event = `PR \`${ pull_request.number }: ${ pull_request.title }\` (${ pull_request.html_url })`;
 	}
 
 	if ( context.eventName === 'push' ) {
-		event = `commit id \`[${ context.sha }](${
+		event = `commit \`${ context.sha } \` on branch \`${ context.ref.substring( 11 ) }\` ${
 			context.payload.head_commit.url
-		})\` on branch \`${ context.ref.substring( 11 ) }\``;
+		}`;
 	}
 
 	const text = `Tests ${ status } for ${ event }`;
