c75411d0b240ea78222782937bdc794715ca7752
dkmyta <dkmyta10@gmail.com>
2022-08-03 14:30:40 -0700

    Register endpoints and clean any expired helper scripts

--- projects/plugins/protect/jetpack-protect.php
+++ projects/plugins/protect/jetpack-protect.php
@@ -114,6 +114,29 @@ add_filter(
 	}
 );
 
+// If WordPress's plugin API is available already, use it. If not,
+// drop data into `$wp_filter` for `WP_Hook::build_preinitialized_hooks()`.
+if ( function_exists( 'add_filter' ) ) {
+	$add_filter = 'add_filter';
+	$add_action = 'add_action';
+} else {
+	$add_filter = function ( $name, $cb, $priority = 10, $accepted_args = 1 ) {
+		global $wp_filter;
+		// phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+		$wp_filter[ $name ][ $priority ][] = array(
+			'accepted_args' => $accepted_args,
+			'function'      => $cb,
+		);
+	};
+	$add_action = $add_filter;
+}
+
+// Clean up expired Helper Scripts from a scheduled event.
+$add_action( 'jetpack_protect_cleanup_helper_scripts', array( 'Automattic\\Jetpack\\Protect\\Helper_Script_Manager', 'cleanup_expired_helper_scripts' ) );
+
+// Register REST routes.
+$add_action( 'rest_api_init', array( 'Automattic\\Jetpack\\Protect\\REST_Controller', 'register_rest_routes' ) );
+
 register_deactivation_hook( __FILE__, array( 'Jetpack_Protect', 'plugin_deactivation' ) );
 
 // Main plugin class.
