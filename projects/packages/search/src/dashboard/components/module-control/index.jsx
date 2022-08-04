f3a09dec2ad29a6e18a487d7668bd2ef2adde442
Chris R <chris.rosser@automattic.com>
2022-08-05 11:43:08 +1200

    Update copy based on review feedback

--- projects/packages/search/src/dashboard/components/module-control/index.jsx
+++ projects/packages/search/src/dashboard/components/module-control/index.jsx
@@ -17,7 +17,7 @@ const SEARCH_DESCRIPTION = __(
 	'jetpack-search-pkg'
 );
 const INSTANT_SEARCH_DESCRIPTION = __(
-	'Instant search will allow your visitors to get our speediest search experience with lightning-fast sorting and filtering.',
+	'Instant search uses a dynamic overlay for lightning-fast results, sorting, and filtering without reloading the page.',
 	'jetpack-search-pkg'
 );
 const RETURN_PATH = 'admin.php?page=jetpack-search';
