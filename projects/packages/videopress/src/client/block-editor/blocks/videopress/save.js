b3e99f61211227f8a396d31a8a7f545a04c04bba
retrofox <rdsuarez@gmail.com>
2022-08-04 13:40:19 -0300

    [not verified] migrate current VideoPress beta block

--- projects/packages/videopress/src/client/block-editor/blocks/videopress/save.js
+++ projects/packages/videopress/src/client/block-editor/blocks/videopress/save.js
@@ -1,21 +1,75 @@
 /**
  * External dependencies
  */
-import { useBlockProps } from '@wordpress/block-editor';
-import { __ } from '@wordpress/i18n';
+import { RichText, useBlockProps } from '@wordpress/block-editor';
+import classnames from 'classnames';
+/**
+ * Internal dependencies
+ */
+import { getVideoPressUrl } from './utils/url';
 
 /**
- * VideoPress blok save function
+ * VideoPress block save function
  *
- * @returns {object} Element to render.
+ * @param {object} props             - Component props.
+ * @param {object} props.attributes  - Block attributes.
+ * @returns {object}                 - React component.
  */
-export default function save() {
+export default function save( { attributes } ) {
+	const {
+		align,
+		autoplay,
+		caption,
+		loop,
+		muted,
+		controls,
+		playsinline,
+		preload,
+		useAverageColor,
+		seekbarColor,
+		seekbarLoadingColor,
+		seekbarPlayedColor,
+		guid,
+		maxWidth,
+		poster,
+	} = attributes;
+
+	const blockProps = useBlockProps.save( {
+		className: classnames( 'wp-block-jetpack-videopress', 'jetpack-videopress-player', {
+			[ `align${ align }` ]: align,
+		} ),
+	} );
+
+	const videoPressUrl = getVideoPressUrl( guid, {
+		autoplay,
+		controls,
+		loop,
+		muted,
+		playsinline,
+		preload,
+		seekbarColor,
+		seekbarLoadingColor,
+		seekbarPlayedColor,
+		useAverageColor,
+		poster,
+	} );
+
+	// Adjust block with based on custom maxWidth.
+	const style = {};
+	if ( maxWidth && maxWidth.length > 0 && '100%' !== maxWidth ) {
+		style.maxWidth = maxWidth;
+		style.margin = 'auto';
+	}
+
 	return (
-		<p { ...useBlockProps.save() }>
-			{ __(
-				'VideoPress – hello from the saved content!',
-				"no text domain is set in this in this project's .eslintrc.js"
+		<figure { ...blockProps } style={ style }>
+			<div className="jetpack-videopress-player__wrapper">
+				{ `\n${ videoPressUrl }\n` /* URL needs to be on its own line. */ }
+			</div>
+
+			{ ! RichText.isEmpty( caption ) && (
+				<RichText.Content tagName="figcaption" value={ caption } />
 			) }
-		</p>
+		</figure>
 	);
 }
