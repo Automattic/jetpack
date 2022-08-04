5fa05ba8166f1213e0a4a3e0ca846dfeb17dbabf
Adrian Moldovan <adim.moldovan@gmail.com>
2022-08-04 20:40:41 +0300

    * Add more tests

--- projects/github-actions/test-results-to-slack/tests/index.test.js
+++ projects/github-actions/test-results-to-slack/tests/index.test.js
@@ -1,4 +1,4 @@
-const path = require( 'path' );
+const github = require( '@actions/github' );
 const nock = require( 'nock' );
 
 const channel = '123abc';
@@ -9,6 +9,7 @@ const runId = '12345';
 const branch = 'trunk';
 const repo = 'foo/bar';
 const ghToken = 'token';
+const repository = { owner: { login: 'foo' }, name: 'bar' };
 
 process.env.INPUT_GITHUB_TOKEN = ghToken;
 process.env.INPUT_SLACK_TOKEN = 'token';
@@ -16,9 +17,37 @@ process.env.INPUT_SLACK_CHANNEL = channel;
 process.env.INPUT_SLACK_USERNAME = username;
 process.env.INPUT_SLACK_ICON_EMOJI = emoji;
 process.env.GITHUB_REPOSITORY = repo;
-process.env.GITHUB_SHA = testSHA;
-process.env.GITHUB_REF = `refs/heads/${ branch }`;
-process.env.GITHUB_RUN_ID = runId;
+
+const defaultGitHubContext = { ...github.context };
+afterEach( () => {
+	// Reset GitHub context to default
+	mockGitHubContext( defaultGitHubContext );
+} );
+
+describe( 'Notification text', () => {
+	const commitURL = 'https://github.com/commit/123';
+	const commitId = '123';
+
+	test.each`
+		event       | isFailure  | expected
+		${ 'push' } | ${ false } | ${ `Tests passed for commit <${ commitURL }|${ commitId }> on branch *${ branch }*` }
+		${ 'push' } | ${ true }  | ${ `Tests failed for commit <${ commitURL }|${ commitId }> on branch *${ branch }*` }
+	`( `$isFailure for $event`, async ( { event, isFailure, expected } ) => {
+		// Mock GitHub context
+		mockGitHubContext( {
+			payload: { head_commit: { url: commitURL, id: 123 } },
+			ref: `refs/heads/${ branch }`,
+			sha: testSHA,
+			eventName: event,
+		} );
+
+		// Mock workflow conclusion
+		const utils = require( '../src/utils' );
+		jest.spyOn( utils, 'isWorkflowFailed' ).mockImplementation().mockReturnValueOnce( isFailure );
+
+		await await expect( utils.getNotificationText( isFailure ) ).resolves.toBe( expected );
+	} );
+} );
 
 describe( 'Workflow conclusion', () => {
 	test.each`
@@ -28,7 +57,10 @@ describe( 'Workflow conclusion', () => {
 		${ false } | ${ 'workflow is successful for 2 uncompleted jobs' }          | ${ [ { conclusion: 'failed' }, { status: 'should-not-matter', conclusion: 'failed' } ] }
 		${ true }  | ${ 'workflow is failed for one failed job' }                  | ${ [ { status: 'completed', conclusion: 'success' }, { status: 'completed', conclusion: 'failed' } ] }
 	`( '$description', async ( { expected, jobs } ) => {
-		process.env.GITHUB_EVENT_PATH = path.join( __dirname, 'fixtures/payload-push.json' );
+		// Mock GitHub context
+		mockGitHubContext( { payload: { repository }, runId } );
+
+		// Intercept request to GitHub Api and mock response
 		nock( 'https://api.github.com' )
 			.get( `/repos/${ repo }/actions/runs/${ runId }/jobs` )
 			.reply( 200, {
@@ -36,7 +68,17 @@ describe( 'Workflow conclusion', () => {
 			} );
 
 		const utils = require( '../src/utils' );
-
-		await expect( utils.isWorkflowFailed( ghToken ) ).resolves.toBe( expected );
+		await await expect( utils.isWorkflowFailed( ghToken ) ).resolves.toBe( expected );
 	} );
 } );
+
+/**
+ * Mocks the GitHub context
+ *
+ * @param {object} value - context object
+ */
+function mockGitHubContext( value ) {
+	Object.defineProperty( github, 'context', {
+		value,
+	} );
+}
