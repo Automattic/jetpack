57490963e6192f254ff05c46aff4971a3d815126
Adrian Moldovan <adim.moldovan@gmail.com>
2022-08-05 15:32:05 +0300

    Add tests for pull_request and schedule events

--- projects/github-actions/test-results-to-slack/src/utils.js
+++ projects/github-actions/test-results-to-slack/src/utils.js
@@ -1,4 +1,4 @@
-const { context, getOctokit } = require( '@actions/github' );
+const github = require( '@actions/github' );
 
 /**
  * Decides if the current workflow failed
@@ -7,13 +7,13 @@ const { context, getOctokit } = require( '@actions/github' );
  */
 async function isWorkflowFailed( token ) {
 	// eslint-disable-next-line new-cap
-	const octokit = new getOctokit( token );
+	const octokit = new github.getOctokit( token );
 
 	// Get the list of jobs for the current workflow run
 	const response = await octokit.rest.actions.listJobsForWorkflowRun( {
-		owner: context.payload.repository.owner.login,
-		repo: context.payload.repository.name,
-		run_id: context.runId,
+		owner: github.context.payload.repository.owner.login,
+		repo: github.context.payload.repository.name,
+		run_id: github.context.runId,
 	} );
 
 	// Get unique list of conclusions of completed jobs
@@ -33,15 +33,15 @@ async function isWorkflowFailed( token ) {
  * @param {boolean} isFailure - whether the workflow is failed or not
  */
 async function getNotificationText( isFailure ) {
-	let event = context.sha;
+	let event = github.context.sha;
 
-	if ( context.eventName === 'pull_request' ) {
-		const { html_url, number, title } = context.payload.pull_request;
+	if ( github.context.eventName === 'pull_request' ) {
+		const { html_url, number, title } = github.context.payload.pull_request;
 		event = `PR <${ html_url }|${ number }: ${ title }>`;
 	}
-	if ( context.eventName === 'push' ) {
-		const { url, id } = context.payload.head_commit;
-		event = `commit <${ url }|${ id }> on branch *${ context.ref.substring( 11 ) }*`;
+	if ( github.context.eventName === 'push' || github.context.eventName === 'schedule' ) {
+		const { url, id } = github.context.payload.head_commit;
+		event = `commit <${ url }|${ id }> on branch *${ github.context.ref.substring( 11 ) }*`;
 	}
 	return `Tests ${ isFailure ? 'failed' : 'passed' } for ${ event }`;
 }
--- projects/github-actions/test-results-to-slack/tests/content.test.js
+++ projects/github-actions/test-results-to-slack/tests/content.test.js
@@ -5,6 +5,9 @@ const branch = 'trunk';
 const repo = 'foo/bar';
 const commitURL = 'https://github.com/commit/123';
 const commitId = '123';
+const prUrl = 'https://github.com/foo/bar/pull/123';
+const prNumber = '123';
+const prTitle = 'Pull request title';
 
 beforeAll( () => {
 	setInputData( { repo } );
@@ -12,15 +15,22 @@ beforeAll( () => {
 
 describe( 'Notification text', () => {
 	test.each`
-		event       | isFailure  | expected
-		${ 'push' } | ${ false } | ${ `Tests passed for commit <${ commitURL }|${ commitId }> on branch *${ branch }*` }
-		${ 'push' } | ${ true }  | ${ `Tests failed for commit <${ commitURL }|${ commitId }> on branch *${ branch }*` }
+		event               | isFailure  | expected
+		${ 'push' }         | ${ false } | ${ `Tests passed for commit <${ commitURL }|${ commitId }> on branch *${ branch }*` }
+		${ 'push' }         | ${ true }  | ${ `Tests failed for commit <${ commitURL }|${ commitId }> on branch *${ branch }*` }
+		${ 'schedule' }     | ${ false } | ${ `Tests passed for commit <${ commitURL }|${ commitId }> on branch *${ branch }*` }
+		${ 'schedule' }     | ${ true }  | ${ `Tests failed for commit <${ commitURL }|${ commitId }> on branch *${ branch }*` }
+		${ 'pull_request' } | ${ false } | ${ `Tests passed for PR <${ prUrl }|${ prNumber }: ${ prTitle }>` }
+		${ 'pull_request' } | ${ true }  | ${ `Tests failed for PR <${ prUrl }|${ prNumber }: ${ prTitle }>` }
 	`(
 		`Message text is correct for $event event and workflow failed $isFailure`,
 		async ( { event, isFailure, expected } ) => {
 			// Mock GitHub context
-			mockGitHubContext( {
-				payload: { head_commit: { url: commitURL, id: 123 } },
+			await mockGitHubContext( {
+				payload: {
+					head_commit: { url: commitURL, id: 123 },
+					pull_request: { html_url: prUrl, number: prNumber, title: prTitle },
+				},
 				ref: `refs/heads/${ branch }`,
 				sha: testSHA,
 				eventName: event,
@@ -28,7 +38,10 @@ describe( 'Notification text', () => {
 
 			// Mock workflow conclusion
 			const utils = require( '../src/utils' );
-			jest.spyOn( utils, 'isWorkflowFailed' ).mockReturnValueOnce( isFailure );
+			const isWorkflowFailed = jest
+				.spyOn( utils, 'isWorkflowFailed' )
+				.mockReturnValueOnce( isFailure );
+			await isWorkflowFailed();
 
 			await expect( utils.getNotificationText( isFailure ) ).resolves.toBe( expected );
 		}
