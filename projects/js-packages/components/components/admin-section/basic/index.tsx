fa5eaaf451be40cb7149be268cc41e30849fbadf
Manzoor Wani <manzoorwani.jk@gmail.com>
2022-08-06 00:07:14 +0530

    JS Components: Convert admin section (AdminSection, AdminSectionHero) components to TypeScript (#25360)
    * Create base shared types
    
    * Convert basic AdminSection to TypeScript
    
    * Convert AdminSection hero to TypeScript
    
    * Convert AdminSection stories to TypeScript
    
    * Fix story source not loading
    
    * Add changelog
    
    * Fix version

--- projects/js-packages/components/components/admin-section/basic/index.tsx
+++ projects/js-packages/components/components/admin-section/basic/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './style.module.scss';
+import type { AdminSectionBaseProps } from '../types';
 
 /**
  * This is the wrapper component to build sections within your admin page.
  *
- * @param {object} props - Component properties.
- * @returns {React.Component} AdminSection component.
+ * @param {AdminSectionBaseProps} props - Component properties.
+ * @returns {React.ReactNode} AdminSection component.
  */
-const AdminSection = props => {
-	const { children } = props;
+const AdminSection: React.FC< AdminSectionBaseProps > = ( { children } ) => {
 	return <div className={ styles.section }>{ children }</div>;
 };
 
