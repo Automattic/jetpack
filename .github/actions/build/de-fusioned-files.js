81a630e47a57cbf53ac838e6ee7c1d6aa0c46c2d
Jeremy Herve <jeremy@jeremy.hu>
2022-08-10 14:44:19 +0200

    Add new file for our list of De-Fusioned files so far
    We'll keep that list up to date as we move forward with the project.

---
+++ .github/actions/build/de-fusioned-files.js
@@ -0,0 +1,11 @@
+/**
+ * List of all files and directories
+ * that we do not need to keep in sync between Jetpack and WordPress.com anymore.
+ *
+ * Notes:
+ * Make sure to keep this list up to date as you remove files from build-plugin-files.php on WordPress.com.
+ * Keep this list in alphabetical order.
+ */
+const deFusionedFiles = [];
+
+module.exports = deFusionedFiles;
