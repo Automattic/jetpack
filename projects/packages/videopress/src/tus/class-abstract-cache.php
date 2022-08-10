2c2fc6232876d2083e753dc9e4bc7ef8f4b24d3f
Leo Germani <leogermani@automattic.com>
2022-08-10 11:11:25 -0300

    Lint abstract cache

--- projects/packages/videopress/src/tus/class-abstract-cache.php
+++ projects/packages/videopress/src/tus/class-abstract-cache.php
@@ -1,88 +1,100 @@
 <?php
+/**
+ * Abstract Cache
+ *
+ * @package jetpack-videopress
+ */
 
+// phpcs:disable Squiz.Commenting.FunctionComment.MissingParamComment
 namespace Automattic\Jetpack\VideoPress\Tus;
 
 use InvalidArgumentException;
 
-abstract class Abstract_Cache implements Cacheable
-{
-    /** @var int TTL in secs (default 1 day) */
-    protected $ttl = 86400;
+/**
+ * Abstract_Cache class
+ */
+abstract class Abstract_Cache implements Cacheable {
 
-    /** @var string Prefix for cache keys */
-    protected $prefix = 'tus:';
+	/**
+	 * TTL in secs (default 1 day)
+	 *
+	 * @var int */
+	protected $ttl = 86400;
 
-    /**
-     * Set time to live.
-     *
-     * @param int $secs
-     *
-     * @return self
-     */
-    public function setTtl($secs)
-    {
-        if ( ! is_int( $secs ) ) {
-			throw new InvalidArgumentException('$secs needs to be an integer');
+	/**
+	 * Prefix for cache keys
+	 *
+	 * @var string */
+	protected $prefix = 'tus:';
+
+	/**
+	 * Set time to live.
+	 *
+	 * @param int $secs
+	 *
+	 * @throws InvalidArgumentException If type is not respected.
+	 * @return self
+	 */
+	public function setTtl( $secs ) {
+		if ( ! is_int( $secs ) ) {
+			throw new InvalidArgumentException( '$secs needs to be an integer' );
 		}
 		$this->ttl = $secs;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public function getTtl()
-    {
-        return $this->ttl;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getTtl() {
+		return $this->ttl;
+	}
 
-    /**
-     * Set cache prefix.
-     *
-     * @param string $prefix
-     *
-     * @return Cacheable
-     */
-    public function setPrefix($prefix)
-    {
-        if ( ! is_string( $prefix ) ) {
-			throw new InvalidArgumentException('$prefix needs to be a string');
+	/**
+	 * Set cache prefix.
+	 *
+	 * @param string $prefix
+	 *
+	 * @throws InvalidArgumentException If type is not respected.
+	 * @return Cacheable
+	 */
+	public function setPrefix( $prefix ) {
+		if ( ! is_string( $prefix ) ) {
+			throw new InvalidArgumentException( '$prefix needs to be a string' );
 		}
 		$this->prefix = $prefix;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get cache prefix.
-     *
-     * @return string
-     */
-    public function getPrefix()
-    {
-        return $this->prefix;
-    }
+	/**
+	 * Get cache prefix.
+	 *
+	 * @return string
+	 */
+	public function getPrefix() {
+		return $this->prefix;
+	}
 
-    /**
-     * Delete all keys.
-     *
-     * @param array $keys
-     *
-     * @return bool
-     */
-    public function deleteAll(array $keys)
-    {
-        if (empty($keys)) {
-            return false;
-        }
+	/**
+	 * Delete all keys.
+	 *
+	 * @param array $keys
+	 *
+	 * @return bool
+	 */
+	public function deleteAll( array $keys ) {
+		if ( empty( $keys ) ) {
+			return false;
+		}
 
-        $status = true;
+		$status = true;
 
-        foreach ($keys as $key) {
-            $status = $status && $this->delete($key);
-        }
+		foreach ( $keys as $key ) {
+			$status = $status && $this->delete( $key );
+		}
 
-        return $status;
-    }
+		return $status;
+	}
 }
