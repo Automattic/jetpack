b558d94bc38962f9b013db57c4d0d5b3e04db967
Grant Kinney <hi@grant.mk>
2022-08-11 17:06:26 -0500

    [not verified] Scaffold basic UI for Action Bar

--- projects/packages/action-bar/src/class-action-bar.php
+++ projects/packages/action-bar/src/class-action-bar.php
@@ -4,10 +4,21 @@
  *
  * @package automattic/jetpack-action-bar
  */
+
 namespace Automattic\Jetpack;
 
+/**
+ * Action_Bar class.
+ */
 class Action_Bar {
+	/**
+	 * Enqueue scripts for rendering Action Bar client.
+	 */
 	public function enqueue_scripts() {
+		if ( is_admin() || ! is_single() ) {
+			return;
+		}
+
 		Assets::register_script(
 			'jetpack-action-bar',
 			'build/action-bar.js',
@@ -21,10 +32,16 @@ class Action_Bar {
 		Assets::enqueue_script( 'jetpack-action-bar' );
 	}
 
+	/**
+	 * Render app container html.
+	 */
 	public function print_html() {
-		echo '<div id="jetpack-action-bar"></div>';
+		echo '<div id="jetpack-action-bar" class="jetpack-action-bar"></div>';
 	}
 
+	/**
+	 * Initialize Action Bar.
+	 */
 	public function init() {
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 		add_action( 'wp_footer', array( $this, 'print_html' ) );
