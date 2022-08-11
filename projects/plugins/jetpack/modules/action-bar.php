570b0a9df9c2d9135883bb945bebfa04d1cccf29
Grant Kinney <hi@grant.mk>
2022-08-11 17:18:04 -0500

    [not verified] Updates Action Bar module header

--- projects/plugins/jetpack/modules/action-bar.php
+++ projects/plugins/jetpack/modules/action-bar.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Module Name: Action bar
+ * Module Name: Action Bar
  * Module Description: An easy to use way for visits to follow, like, and comment on your site.
- * Sort Order: 38
- * Recommendation Order: 16
+ * Sort Order: 40
+ * Recommendation Order: 18
  * First Introduced: $$next-version$$
  * Requires Connection: Yes
  * Auto Activate: No
- * Module Tags: Sharing
- * Feature: Sharing
- * Additional Search Queries: adminbar, actionbar
+ * Module Tags: Social
+ * Feature: Engagement
+ * Additional Search Queries: adminbar, actionbar, comments, likes, follow, sharing
  *
  * @package automattic/jetpack
  */
