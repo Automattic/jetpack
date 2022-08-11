d3960685bbf6005c8bbcf7dfec748e4774a8e618
Jasper Kang <kangzjnet@gmail.com>
2022-08-12 11:42:22 +1200

    re arrange errors

--- projects/packages/search/src/instant-search/lib/errors.js
+++ projects/packages/search/src/instant-search/lib/errors.js
@@ -8,31 +8,32 @@ import { __, sprintf } from '@wordpress/i18n';
  */
 export function getErrorMessage( error ) {
 	switch ( error?.message ) {
-		case 'unknown_blog':
-		case 'unauthorized':
-		case 'bad_request':
-		case 'payload_too_large':
-		case 'not_supported':
+		case 'service_unavailable':
 			return sprintf(
 				// translators: %s: Error code.
 				__(
-					'Jetpack Search has encountered an error. Please contact the site administrator if the issue persists. [%s]',
+					'Jetpack Search is currently unavailable. Please try again later. [%s]',
 					'jetpack-search-pkg'
 				),
-				error.message
+				error?.message
 			);
+
 		case 'offline':
 			return __(
 				"It looks like you're offline. Please reconnect to load the latest results.",
 				'jetpack-search-pkg'
 			);
 
-		case 'service_unavailable':
+		case 'unknown_blog':
+		case 'unauthorized':
+		case 'bad_request':
+		case 'payload_too_large':
+		case 'not_supported':
 		default:
 			return sprintf(
 				// translators: %s: Error code.
 				__(
-					'Jetpack Search is currently unavailable. Please try again later. [%s]',
+					'Jetpack Search has encountered an error. Please contact the site administrator if the issue persists. [%s]',
 					'jetpack-search-pkg'
 				),
 				error?.message ?? 'unknown'
