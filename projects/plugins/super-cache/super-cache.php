01aff8e44e45b28f6d443f96456ccd25df076a6a
Samiff <samiff@users.noreply.github.com>
2022-08-12 11:03:04 -0600

    [not verified] WPSC: get changelog passing for JP monorepo

--- projects/plugins/super-cache/super-cache.php
+++ projects/plugins/super-cache/super-cache.php
@@ -4,7 +4,7 @@
  * Plugin Name: Jetpack super-cache
  * Plugin URI: TBD
  * Description: A very fast caching engine for WordPress that produces static html files.
- * Version: 1.7.9
+ * Version: 1.7.10-alpha
  * Author: Automattic
  * Author URI: https://jetpack.com/
  * License: GPLv2 or later
