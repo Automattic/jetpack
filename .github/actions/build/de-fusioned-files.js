59cf1faccb498a4a20f2d2c19da09aee238e3097
Jeremy Herve <jeremy@jeremy.hu>
2022-08-12 13:00:48 +0200

    Add more de-fusioned files

--- .github/actions/build/de-fusioned-files.js
+++ .github/actions/build/de-fusioned-files.js
@@ -9,9 +9,13 @@
 const deFusionedFiles = [
 	'projects/plugins/jetpack/_inc/genericons.php',
 	'projects/plugins/jetpack/_inc/genericons/',
+	'projects/plugins/jetpack/class.jetpack-post-images.php',
+	'projects/plugins/jetpack/class.jetpack-twitter-cards.php',
+	'projects/plugins/jetpack/modules/seo-tools/',
 	'projects/plugins/jetpack/modules/widget-visibility/widget-conditions.php',
 	'projects/plugins/jetpack/modules/widget-visibility/widget-conditions/',
 	'projects/plugins/jetpack/modules/widget-visibility/editor/',
+	'projects/plugins/jetpack/modules/widgets/',
 	'projects/plugins/jetpack/modules/memberships/',
 	'projects/plugins/jetpack/modules/cloudflare-analytics/',
 	'projects/plugins/jetpack/src/',
