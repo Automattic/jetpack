56711276890ff2cc1b50d35a15c1526698dedb99
Kolja Zuelsdorf <kolja.zuelsdorf@automattic.com>
2022-08-15 16:26:54 -0500

    Added jetpack connection prerequisites.

--- projects/plugins/jetpack/tests/e2e/specs/waf/blocking.test.js
+++ projects/plugins/jetpack/tests/e2e/specs/waf/blocking.test.js
@@ -1,19 +1,27 @@
 import { test } from 'jetpack-e2e-commons/fixtures/base-test.js';
 import { WpPage } from 'jetpack-e2e-commons/pages/index.js';
 import playwrightConfig from '../../playwright.config.cjs';
-import { prerequisitesBuilder } from 'jetpack-e2e-commons/env/index.js';
+import { Plans, prerequisitesBuilder } from 'jetpack-e2e-commons/env/index.js';
 import { resolveSiteUrl } from 'jetpack-e2e-commons/helpers/utils-helper.cjs';
 
 test.describe.parallel( 'WAF Blocking', () => {
 	test.beforeAll( async ( { browser } ) => {
 		const page = await browser.newPage( playwrightConfig.use );
-		await prerequisitesBuilder( page ).withCleanEnv().build();
+		await prerequisitesBuilder( page )
+			.withCleanEnv()
+			.withWpComLoggedIn( true )
+			.withLoggedIn( true )
+			.withConnection( true )
+			.withPlan( Plans.Complete )
+			.withActiveModules( [ 'waf' ] )
+			.build();
 		await page.close();
 	} );
 
 	test( 'Block a simple request', async ( { page } ) => {
 		await test.step( 'Block it', async () => {
 			const ourPage = new WpPage( page, { pageName: 'our page' } );
+
 			ourPage.goto( `${ resolveSiteUrl() }/?blubb=<script` );
 		} );
 	} );
