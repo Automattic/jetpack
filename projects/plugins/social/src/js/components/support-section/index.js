37bd01d70738a5db706b3183233be9343528dd39
Gergely Juhasz <gergely.juhasz@automattic.com>
2022-08-15 17:38:34 +0200

    Fix review comments

--- projects/plugins/social/src/js/components/support-section/index.js
+++ projects/plugins/social/src/js/components/support-section/index.js
@@ -1,33 +1,39 @@
-import { Text } from '@automattic/jetpack-components';
+import { Text, Container, getRedirectUrl } from '@automattic/jetpack-components';
 import { ExternalLink } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 import { Icon, lifesaver } from '@wordpress/icons';
 import { STORE_ID } from '../../store';
-import IconTextSection from '../icon-text-section';
+import IconText from '../icon-text';
 import styles from './styles.module.scss';
 
 const SupportSection = () => {
-	const isPremiumPlan = useSelect( select => select( STORE_ID ).isPremiumPlan() );
+	const hasPaidPlan = useSelect( select => select( STORE_ID ).hasPaidPlan() );
 
-	return isPremiumPlan ? (
-		<IconTextSection
-			icon={ <Icon icon={ lifesaver } size={ 30 }></Icon> }
-			title={ __( 'World-class support', 'jetpack-social' ) }
-		>
-			<Text>
-				{ __(
-					'Do you need any help? Get in touch with our world-class support with a high-priority support ticket and get a solution faster.',
-					'jetpack-social'
-				) }
-			</Text>
-			<Text className={ styles.link }>
-				<ExternalLink href="https://wordpress.com/support">
-					{ __( 'Contact Support', 'jetpack-social' ) }
-				</ExternalLink>
-			</Text>
-		</IconTextSection>
-	) : null;
+	if ( ! hasPaidPlan ) {
+		return null;
+	}
+
+	return (
+		<Container horizontalSpacing={ 7 } horizontalGap={ 3 }>
+			<IconText
+				icon={ <Icon icon={ lifesaver } size={ 30 }></Icon> }
+				title={ __( 'World-class support', 'jetpack-social' ) }
+			>
+				<Text>
+					{ __(
+						'Do you need any help? Get in touch with our world-class support with a high-priority support ticket and get a solution faster.',
+						'jetpack-social'
+					) }
+				</Text>
+				<Text className={ styles.link }>
+					<ExternalLink href={ getRedirectUrl( 'https://wordpress.com/support' ) }>
+						{ __( 'Contact Support', 'jetpack-social' ) }
+					</ExternalLink>
+				</Text>
+			</IconText>
+		</Container>
+	);
 };
 
 export default SupportSection;
--- projects/plugins/social/src/js/store/selectors/jetpack-settings.js
+++ projects/plugins/social/src/js/store/selectors/jetpack-settings.js
@@ -2,7 +2,7 @@ const jetpackSettingSelectors = {
 	getPublicizeModuleStatus: state => state.jetpackSettings,
 	isModuleEnabled: state => state.jetpackSettings.publicize_active,
 	isUpdatingJetpackSettings: state => state.jetpackSettings.is_updating,
-	isPremiumPlan: state => ! state.showNudge,
+	hasPaidPlan: state => ! state.showNudge,
 };
 
 export default jetpackSettingSelectors;
