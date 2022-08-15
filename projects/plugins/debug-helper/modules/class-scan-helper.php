1d3b7a72783d12ff12f4073b49c32dbfc2eaeb7d
Nate Weller <nate.weller@automattic.com>
2022-08-15 11:35:38 -0500

    Wireframe scan debug helper module

---
+++ projects/plugins/debug-helper/modules/class-scan-helper.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Jetpack Scan helper class.
+ *
+ * @package automattic/jetpack-debug-helper
+ */
+
+/**
+ * Helps debug Scan
+ */
+class Scan_Helper {
+
+	/**
+	 * Construction.
+	 */
+	public function __construct() {
+		add_action( 'admin_menu', array( $this, 'register_submenu_page' ), 1000 );
+
+	}
+
+	/**
+	 * Add submenu item.
+	 */
+	public function register_submenu_page() {
+		add_submenu_page(
+			'jetpack-debug-tools',
+			'Scan Helper',
+			'Scan Helper',
+			'manage_options',
+			'scan-helper',
+			array( $this, 'render_ui' ),
+			99
+		);
+	}
+
+	/**
+	 * Renders the UI.
+	 */
+	public function render_ui() {
+		echo 'Hello, World!';
+	}
+
+}
