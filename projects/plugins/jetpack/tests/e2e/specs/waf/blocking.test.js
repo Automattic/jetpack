441fd7cd91eb5fad47002e9af2b6a86ef93f2ec5
Kolja Zuelsdorf <kolja.zuelsdorf@automattic.com>
2022-08-15 14:58:03 -0500

    Added very basic version of opening a site in the waf blocking test.

--- projects/plugins/jetpack/tests/e2e/specs/waf/blocking.test.js
+++ projects/plugins/jetpack/tests/e2e/specs/waf/blocking.test.js
@@ -1,6 +1,5 @@
-import { test, expect } from 'jetpack-e2e-commons/fixtures/base-test.js';
+import { test } from 'jetpack-e2e-commons/fixtures/base-test.js';
 import { WpPage } from 'jetpack-e2e-commons/pages/index.js';
-import config from 'config';
 import playwrightConfig from '../../playwright.config.cjs';
 import { prerequisitesBuilder } from 'jetpack-e2e-commons/env/index.js';
 import { resolveSiteUrl } from 'jetpack-e2e-commons/helpers/utils-helper.cjs';
