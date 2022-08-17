8b08d661a3bfca6e63786dd4cf1096c9998e0475
Jeremy Herve <jeremy@jeremy.hu>
2022-08-17 10:05:03 +0200

    My Jetpack: do not enable Licensing UI if My Jetpack is disabled
    Fixes #25666
    
    This is true for Multisite environments, for example.

--- projects/packages/my-jetpack/src/class-initializer.php
+++ projects/packages/my-jetpack/src/class-initializer.php
@@ -28,7 +28,7 @@ class Initializer {
 	 *
 	 * @var string
 	 */
-	const PACKAGE_VERSION = '2.0.0';
+	const PACKAGE_VERSION = '2.0.1-alpha';
 
 	/**
 	 * Initialize My Jetapack
@@ -80,6 +80,17 @@ class Initializer {
 	 * @return boolean
 	 */
 	public static function is_licensing_ui_enabled() {
+		// Default changed to true in 1.5.0.
+		$is_enabled = true;
+
+		/*
+		 * Bail if My Jetpack is not enabled,
+		 * and thus the licensing UI shouldn't be enabled either.
+		 */
+		if ( ! self::should_initialize() ) {
+			$is_enabled = false;
+		}
+
 		/**
 		 * Acts as a feature flag, returning a boolean for whether we should show the licensing UI.
 		 *
@@ -90,7 +101,7 @@ class Initializer {
 		 */
 		return apply_filters(
 			'jetpack_my_jetpack_should_enable_add_license_screen',
-			true
+			$is_enabled
 		);
 	}
 
