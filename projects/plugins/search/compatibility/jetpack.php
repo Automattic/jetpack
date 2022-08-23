4e4a18e18481c464ab74b26329656d2c012a9324
Jasper Kang <kangzjnet@gmail.com>
2022-08-23 13:25:16 +1200

    [not verified] always show search submenu when plugin is installed

--- projects/plugins/search/compatibility/jetpack.php
+++ projects/plugins/search/compatibility/jetpack.php
@@ -7,22 +7,11 @@
 
 namespace Automattic\Jetpack\Search_Plugin\Compatibility\Jetpack;
 
-use Jetpack;
-
 /**
  * Override the condition to show Search submenu when Jetpack plugin exists.
  */
 function should_show_jetpack_search_submenu() {
-	if ( ! current_user_can( 'manage_options' ) ) {
-		return false;
-	}
-
-	// If site is in Offline Mode or not connected yet.
-	if ( ! Jetpack::is_active_and_not_offline_mode() ) {
-		return false;
-	}
-
-	return true;
+	return current_user_can( 'manage_options' );
 }
 
 // Search package uses priority 10 to override Search submenu visibility for Jetpack.
