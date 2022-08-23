d8181e78b97fdf5cc30b677c3391ac9de3096247
retrofox <rdsuarez@gmail.com>
2022-08-23 16:43:31 -0300

    register VideoPress Video only when its extension is available

--- projects/plugins/jetpack/extensions/extended-blocks/videopress-video/videopress-video.php
+++ projects/plugins/jetpack/extensions/extended-blocks/videopress-video/videopress-video.php
@@ -12,6 +12,11 @@ use Automattic\Jetpack\VideoPress\Initializer as VideoPress_Pkg_Initializer;
 add_action(
 	'init',
 	function () {
+		$is_extension_available = \Jetpack_Gutenberg::is_extension_available( 'videopress/video' );
+		if ( ! $is_extension_available ) {
+			return;
+		}
+
 		VideoPress_Pkg_Initializer::register_videopress_block();
 	}
 );
