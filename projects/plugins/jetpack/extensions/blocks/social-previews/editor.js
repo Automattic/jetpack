61bd1459d854a81bba0a2144e81f1a1079676070
spsiddarthan <siddarthan@outlook.com>
2022-08-23 11:28:51 +0530

    Update social previews plan checks.

--- projects/plugins/jetpack/extensions/blocks/social-previews/editor.js
+++ projects/plugins/jetpack/extensions/blocks/social-previews/editor.js
@@ -1,30 +1,8 @@
-import {
-	isSimpleSite,
-	getJetpackExtensionAvailability,
-	registerJetpackPlugin,
-} from '@automattic/jetpack-shared-extension-utils';
-import { registerPlugin } from '@wordpress/plugins';
-import { name, settings, SocialPreviews } from '.';
+import { registerJetpackPlugin } from '@automattic/jetpack-shared-extension-utils';
+import { name, settings } from '.';
 
 /*
  * Register the main "social-previews" extension if the feature is available
  * on the current plan.
  */
 registerJetpackPlugin( name, settings );
-
-/*
- * If the social previews extension is **not** available on this plan (WP.com only)
- * then manually register a near identical Plugin which shows the upgrade nudge.
- * Note this is necessary because the official `registerJetpackPlugin` checks the
- * extension availability so will not render the Plugin if the extension is not
- * availabile.
- */
-const extensionAvailableOnPlan = getJetpackExtensionAvailability( 'social-previews' )?.available;
-
-if ( ! extensionAvailableOnPlan && isSimpleSite() ) {
-	registerPlugin( `jetpack-${ name }-upgrade-nudge`, {
-		render: () => {
-			return <SocialPreviews showUpgradeNudge={ true } />;
-		},
-	} );
-}
