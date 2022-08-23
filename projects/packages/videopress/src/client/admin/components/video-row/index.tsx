797b95dc6fc3ebd4de0cc8721242cdf745ee953d
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2022-08-23 16:08:14 -0300

    VideoPress: Implement ellpsis at Title

--- projects/packages/videopress/src/client/admin/components/video-row/index.tsx
+++ projects/packages/videopress/src/client/admin/components/video-row/index.tsx
@@ -1,16 +1,16 @@
-import { Text, Button } from '@automattic/jetpack-components';
+import { Text, Button, useBreakpointMatch } from '@automattic/jetpack-components';
 import { Popover } from '@wordpress/components';
 import { dateI18n } from '@wordpress/date';
 import { sprintf, __ } from '@wordpress/i18n';
 import { Icon, image, trash } from '@wordpress/icons';
 import classNames from 'classnames';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import privacy from './privacy-icon';
 import styles from './style.module.scss';
 
 // Hiding it based on Design request:
 // https://github.com/Automattic/jetpack/issues/25742#issuecomment-1223123815
-const HIDE_QUICK_ACTIONS = true;
+const HIDE_QUICK_ACTIONS = false;
 
 const millisecondsToMinutesAndSeconds = milliseconds => {
 	const minutes = Math.floor( milliseconds / 60000 );
@@ -68,13 +68,16 @@ const VideoRow = ( {
 	isPrivate: boolean;
 	onClickEdit?: () => void;
 } ) => {
-	const [ showActions, setShowActions ] = useState( false );
+	const [ isSmall ] = useBreakpointMatch( 'sm' );
+	const [ showActions, setShowActions ] = useState( true );
 	const [ keyPressed, setKeyDown ] = useState( false );
+	const textRef = useRef( null );
 
 	const editVideoLabel = __( 'Edit video details', 'jetpack-videopress-pkg' );
 	const durationInMinutesAndSeconds = millisecondsToMinutesAndSeconds( duration );
 	const uploadDateFormatted = dateI18n( 'M j, Y', uploadDate, null );
 	const isSpaceOrEnter = code => code === 'Space' || code === 'Enter';
+	const isEllipsisActive = textRef?.current?.offsetWidth < textRef?.current?.scrollWidth;
 
 	const wrapperAriaLabel = sprintf(
 		/* translators: 1 Video title, 2 Video duration, 3 Video upload date */
@@ -110,7 +113,10 @@ const VideoRow = ( {
 
 	return (
 		<div
-			className={ classNames( styles[ 'video-row' ], { [ styles.pressed ]: keyPressed } ) }
+			className={ classNames( styles[ 'video-row' ], {
+				[ styles.pressed ]: keyPressed,
+				[ styles.small ]: isSmall,
+			} ) }
 			onMouseOver={ handleOverOrFocus }
 			onMouseLeave={ handleLeave }
 			onFocus={ handleOverOrFocus }
@@ -122,7 +128,14 @@ const VideoRow = ( {
 		>
 			<div className={ styles[ 'info-wrapper' ] }>
 				<img className={ styles.poster } alt="" src={ posterImage } />
-				<Text variant="title-small">{ videoTitle }</Text>
+				<Text variant="title-small" className={ styles.title } ref={ textRef }>
+					{ ! isSmall && isEllipsisActive && (
+						<Text variant="body-extra-small" className={ styles.label } component="span">
+							{ videoTitle }
+						</Text>
+					) }
+					{ videoTitle }
+				</Text>
 			</div>
 			<div className={ styles[ 'meta-wrapper' ] }>
 				{ showActions ? (
