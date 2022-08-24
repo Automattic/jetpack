1bac003393d0e2913cf0b853bdf66b21a722d219
retrofox <rdsuarez@gmail.com>
2022-08-24 09:12:12 -0300

    [not verified] rename extension name to videopress/video

--- projects/plugins/jetpack/extensions/extended-blocks/videopress-video/videopress-video.php
+++ projects/plugins/jetpack/extensions/extended-blocks/videopress-video/videopress-video.php
@@ -12,7 +12,7 @@ use Automattic\Jetpack\VideoPress\Initializer as VideoPress_Pkg_Initializer;
 add_action(
 	'init',
 	function () {
-		$is_extension_available = in_array( 'jetpack/videopress', \Jetpack_Gutenberg::get_available_extensions(), true );
+		$is_extension_available = in_array( 'videopress/video', \Jetpack_Gutenberg::get_available_extensions(), true );
 		if ( ! $is_extension_available ) {
 			return;
 		}
