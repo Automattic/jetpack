afdf2fe8c705c48871184f0b60510ae02f707663
retrofox <rdsuarez@gmail.com>
2022-08-25 18:26:40 -0300

    Add a Beta label to the blocks in the editor canvas

--- projects/plugins/jetpack/extensions/extended-blocks/index.js
+++ projects/plugins/jetpack/extensions/extended-blocks/index.js
@@ -1,11 +1,13 @@
 /**
  * External dependencies
  */
+import { createHigherOrderComponent } from '@wordpress/compose';
 import { addFilter } from '@wordpress/hooks';
 /**
  * Internal dependencies
  */
 import extensionList from '../index.json';
+import './index.scss';
 
 function isBetaExtension( name ) {
 	if ( ! extensionList ) {
@@ -13,9 +15,11 @@ function isBetaExtension( name ) {
 	}
 
 	const betaExtensions = extensionList.beta || [];
+
 	/*
-	 * Some extensions are defined without the jetpack/ prefix,
-	 * so we need to check for both :-/
+	 * Some extensions are defined without the `jetpack/` prefix,
+	 * so we need to check for both since, for instance,
+	 * the jetpack blocks are prefixed with `jetpack/`.
 	 */
 	const cleanName = name.replace( /jetpack\//, '' );
 
@@ -35,3 +39,16 @@ function labelBlocksTitle( settings, name ) {
 }
 
 addFilter( 'blocks.registerBlockType', 'jetpack/label-blocks-title', labelBlocksTitle );
+
+const withBetaClassName = createHigherOrderComponent( BlockListBlock => {
+	return props => {
+		const { name } = props;
+		if ( ! isBetaExtension( name ) ) {
+			return <BlockListBlock { ...props } />;
+		}
+
+		return <BlockListBlock { ...props } className="is-beta-extension" />;
+	};
+}, 'withBetaClassName' );
+
+addFilter( 'editor.BlockListBlock', 'my-plugin/with-client-id-class-name', withBetaClassName );
