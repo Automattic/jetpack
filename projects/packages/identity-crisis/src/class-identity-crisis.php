3de1425989b4396fdd1706d541c965ce96e77abb
Jasper Kang <kangzjnet@gmail.com>
2022-08-26 09:32:11 +1200

    [not verified] generated changelog

--- projects/packages/identity-crisis/src/class-identity-crisis.php
+++ projects/packages/identity-crisis/src/class-identity-crisis.php
@@ -28,7 +28,7 @@ class Identity_Crisis {
 	/**
 	 * Package Version
 	 */
-	const PACKAGE_VERSION = '0.8.21-alpha';
+	const PACKAGE_VERSION = '0.8.21';
 
 	/**
 	 * Instance of the object.
--- projects/packages/search/src/class-package.php
+++ projects/packages/search/src/class-package.php
@@ -11,7 +11,7 @@ namespace Automattic\Jetpack\Search;
  * Search package general information
  */
 class Package {
-	const VERSION = '0.21.0-alpha';
+	const VERSION = '0.21.0';
 	const SLUG    = 'search';
 
 	/**
