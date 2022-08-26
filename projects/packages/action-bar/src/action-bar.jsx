df0a2c3b04213b2b1ef147b553a0e7987e5e55ca
Jeremy Herve <jeremy@jeremy.hu>
2022-08-26 09:30:08 +0200

    Switch to using wp.element

--- projects/packages/action-bar/src/action-bar.jsx
+++ projects/packages/action-bar/src/action-bar.jsx
@@ -1,8 +1,4 @@
-import { Button } from '@wordpress/components';
-import { render } from 'react-dom';
-// This must be loaded otherwise the production build fails with a cryptic error message.
-// eslint-disable-next-line no-unused-vars
-// import * as Redux from 'react-redux';
+import { render } from '@wordpress/element';
 import { CommentIcon, EllipsisIcon, FollowIcon, StarIcon } from './icons';
 import './action-bar.scss';
 
@@ -10,24 +6,24 @@ const ActionBar = () => {
 	return (
 		<ul className="jetpack-action-bar__action-list">
 			<li>
-				<Button className="jetpack-action-bar__action-button">
+				<button className="jetpack-action-bar__action-button">
 					<EllipsisIcon />
-				</Button>
+				</button>
 			</li>
 			<li>
-				<Button className="jetpack-action-bar__action-button">
+				<button className="jetpack-action-bar__action-button">
 					<CommentIcon />
-				</Button>
+				</button>
 			</li>
 			<li>
-				<Button className="jetpack-action-bar__action-button">
+				<button className="jetpack-action-bar__action-button">
 					<StarIcon />
-				</Button>
+				</button>
 			</li>
 			<li>
-				<Button className="jetpack-action-bar__action-button">
+				<button className="jetpack-action-bar__action-button">
 					<FollowIcon />
-				</Button>
+				</button>
 			</li>
 		</ul>
 	);
