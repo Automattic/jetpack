33097e448403cab6a50b0050b5892b66b63cdb6e
Paul Bunkham <paul@dobit.co.uk>
2022-08-27 19:46:39 +0100

    Add a test to check the default filter results in skip meta being added.

--- projects/packages/publicize/tests/php/test-connections-post-field.php
+++ projects/packages/publicize/tests/php/test-connections-post-field.php
@@ -336,4 +336,24 @@ class Test_Connections_Post_Field  extends TestCase {
 			$this->assertSame( 'facebook' !== $connection->service_name, $connection->enabled );
 		}
 	}
+
+	/**
+	 * Test that connections are skipped when the publicize_checkbox_default filter is used.
+	 */
+	public function test_default_checkbox_filter() {
+		$filter_func = function ( $default ) {
+			return false;
+		};
+
+		add_filter( 'publicize_checkbox_default', $filter_func );
+		$request = new WP_REST_Request( 'POST', sprintf( '/wp/v2/posts/%d', $this->draft_id ) );
+		$this->server->dispatch( $request );
+
+		foreach ( self::$connection_ids as $unique_id ) {
+			$skip_key = $this->publicize->POST_SKIP . $unique_id;
+			$this->assertNotEmpty( get_post_meta( $this->draft_id, $skip_key, true ) );
+		}
+
+		remove_filter( 'publicize_checkbox_default', $filter_func );
+	}
 }
