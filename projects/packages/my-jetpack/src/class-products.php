0b4fe6b10225bce4849b5a67b36965fe8aa6bcb9
leogermani <leogermani@gmail.com>
2022-08-30 13:25:15 -0300

    Allow product classes to be overwitten (#25891)
    * Allow product classes to be overwitten
    
    * improive docblock
    
    * bumping versions
    
    * fix test on php 5.6
    
    * php 56 compat

--- projects/packages/my-jetpack/src/class-products.php
+++ projects/packages/my-jetpack/src/class-products.php
@@ -17,22 +17,49 @@ class Products {
 	 *
 	 * Here's where all the existing Products are registered
 	 *
+	 * @throws \Exception If the result of a filter has invalid classes.
 	 * @return array List of class names
 	 */
 	public static function get_products_classes() {
-		return array(
-			Products\Anti_Spam::class,
-			Products\Backup::class,
-			Products\Boost::class,
-			Products\Crm::class,
-			Products\Extras::class,
-			Products\Scan::class,
-			Products\Search::class,
-			Products\Social::class,
-			Products\Security::class,
-			Products\Protect::class,
-			Products\Videopress::class,
+		$classes = array(
+			'anti-spam'  => Products\Anti_Spam::class,
+			'backup'     => Products\Backup::class,
+			'boost'      => Products\Boost::class,
+			'crm'        => Products\Crm::class,
+			'extras'     => Products\Extras::class,
+			'scan'       => Products\Scan::class,
+			'search'     => Products\Search::class,
+			'social'     => Products\Social::class,
+			'security'   => Products\Security::class,
+			'protect'    => Products\Protect::class,
+			'videopress' => Products\Videopress::class,
 		);
+
+		/**
+		 * This filter allows plugin to override the Product class of a given product. The new class must be a child class of the default one declared in My Jetpack
+		 *
+		 * For example, a stand-alone plugin could overwrite its product class to control specific behavior of the product in the My Jetpack page after it is active without having to commit changes to the My Jetpack package:
+		 *
+		 * add_filter( 'my_jetpack_products_classes', function( $classes ) {
+		 *  $classes['my_plugin'] = 'My_Plugin'; // a class that extends the original one declared in the My Jetpack package.
+		 *  return $classes
+		 * } );
+		 *
+		 * @param array $classes An array where the keys are the product slugs and the values are the class names.
+		 */
+		$final_classes = apply_filters( 'my_jetpack_products_classes', $classes );
+
+		// Check that the classes are still child of the same original classes.
+		foreach ( (array) $final_classes as $slug => $final_class ) {
+			if ( $final_class === $classes[ $slug ] ) {
+				continue;
+			}
+			if ( ! class_exists( $final_class ) || ! is_subclass_of( $final_class, $classes[ $slug ] ) ) {
+				throw new \Exception( 'You can only overwrite a Product class with a child of the original class.' );
+			}
+		}
+
+		return $final_classes;
 	}
 
 	/**
@@ -57,13 +84,24 @@ class Products {
 	 * @return ?array
 	 */
 	public static function get_product( $product_slug ) {
-		foreach ( self::get_products_classes() as $class ) {
-			$p_slug = $class::$slug;
-			if ( $p_slug === $product_slug ) {
-				return $class::get_info();
-			}
+		$classes = self::get_products_classes();
+		if ( isset( $classes[ $product_slug ] ) ) {
+			return $classes[ $product_slug ]::get_info();
 		}
-		return null;
+	}
+
+	/**
+	 * Get one product Class name
+	 *
+	 * @param string $product_slug The product slug.
+	 *
+	 * @return ?string
+	 */
+	public static function get_product_class( $product_slug ) {
+		$classes = self::get_products_classes();
+		if ( isset( $classes[ $product_slug ] ) ) {
+			return $classes[ $product_slug ];
+		}
 	}
 
 	/**
@@ -72,11 +110,7 @@ class Products {
 	 * @return array Product slugs array.
 	 */
 	public static function get_products_slugs() {
-		$slugs = array();
-		foreach ( self::get_products_classes() as $class ) {
-			$slugs[] = $class::$slug;
-		}
-		return $slugs;
+		return array_keys( self::get_products_classes() );
 	}
 
 	/**
@@ -133,12 +167,16 @@ class Products {
 	 * tied to the Products.
 	 */
 	public static function extend_plugins_action_links() {
-		Products\Backup::extend_plugin_action_links();
-		Products\Boost::extend_plugin_action_links();
-		Products\Crm::extend_plugin_action_links();
-
-		// Extend Jetpack plugin using Videopress instance.
-		Products\Videopress::extend_plugin_action_links();
+		$products = array(
+			'backup',
+			'boost',
+			'crm',
+			'videopress', // we use videopress here to add the plugin action to the Jetpack plugin itself
+		);
+		foreach ( $products as $product ) {
+			$class_name = self::get_product_class( $product );
+			$class_name::extend_plugin_action_links();
+		}
 	}
 
 }
