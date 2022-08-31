e2535955db16b386b054fd09d5d3b2dfbaf01fae
CodeyGuyDylan <dylan.munson@automattic.com>
2022-08-31 11:10:35 -0600

    [not verified] Add dynamic body text and update layout of sidebar

--- projects/plugins/jetpack/_inc/client/recommendations/sidebar/mobile-app/index.tsx
+++ projects/plugins/jetpack/_inc/client/recommendations/sidebar/mobile-app/index.tsx
@@ -1,5 +1,5 @@
 import { imagePath } from 'constants/urls';
-import { createInterpolateElement } from '@wordpress/element';
+//import { createInterpolateElement } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import AppsBadge from 'components/apps-badge';
 import analytics from 'lib/analytics';
@@ -14,14 +14,6 @@ type Props = {
 };
 
 const MobileApp: FC< Props > = ( { slug } ) => {
-	const features = [
-		__( 'Refined post and page editor', 'jetpack' ),
-		__( 'Manage multiple sites from one dashboard', 'jetpack' ),
-		__( 'Multi-site plugin management', 'jetpack' ),
-		__( 'Free stock photo library', 'jetpack' ),
-		__( 'Update your site from any device', 'jetpack' ),
-	];
-
 	const getHeading = () => {
 		switch ( slug ) {
 			case 'download-app':
@@ -31,15 +23,26 @@ const MobileApp: FC< Props > = ( { slug } ) => {
 		}
 	};
 
+	const getBody = () => {
+		switch ( slug ) {
+			case 'download-app':
+				return __(
+					'Never miss an important event with realtime notifications and your activity log just a tap away.',
+					'jetpack'
+				);
+			default:
+				return __(
+					'Never miss an important event with realtime notifications and your activity log just a tap away.',
+					'jetpack'
+				);
+		}
+	};
+
+	//const QRCode = <div></div>;
+
 	useEffect( () => {
 		analytics.tracks.recordEvent( 'jetpack_recommendations_summary_sidebar_display', {
 			type: 'mobile_app',
-		} );
-	}, [] );
-
-	const onWpcomClick = useCallback( () => {
-		analytics.tracks.recordEvent( 'jetpack_recommendations_summary_sidebar_click', {
-			type: 'mobile_app_wpcom',
 		} );
 	}, [] );
 
@@ -58,6 +61,8 @@ const MobileApp: FC< Props > = ( { slug } ) => {
 				height={ 25 }
 				alt="wordpress icon"
 			/>
+			{ /* Extra 2 pixels on width and height are to account for the white padding on this SVG, so the
+			actual image will look the same size as the wordpress one */ }
 			<img
 				className="jp-recommendation-sidebar-mobile__jetpack-icon"
 				src={ imagePath + '/recommendations/jetpack-icon.svg' }
@@ -70,44 +75,19 @@ const MobileApp: FC< Props > = ( { slug } ) => {
 
 	return (
 		<SidebarCard header={ header }>
-			<div>
-				<h2 className="jp-recommendation-sidebar-mobile__heading">{ getHeading() }</h2>
-				<p>
-					{ createInterpolateElement(
-						__(
-							'With Jetpack, you have <strong>free access</strong> to managing your site with <a>WordPress.com</a> and the Android and iOS WordPress apps.',
-							'jetpack'
-						),
-						{
-							strong: <strong />,
-							a: (
-								<a
-									href="https://wordpress.com"
-									target="_blank"
-									rel="noreferrer"
-									onClick={ onWpcomClick }
-								/>
-							),
-						}
-					) }
-				</p>
-				<ul className="jp-recommendations-sidebar-card__features">
-					{ features.map( ( feature: string, index: number ) => (
-						<li key={ index }>{ feature }</li>
-					) ) }
-				</ul>
-				<div className="jp-recommendations-sidebar-mobile__apps-badge">
-					<AppsBadge
-						onBadgeClick={ onAppBadgeClick }
-						storeName={ 'ios' }
-						utm_source={ 'jetpack-plugin-recommendations' }
-					/>
-					<AppsBadge
-						onBadgeClick={ onAppBadgeClick }
-						storeName={ 'android' }
-						utm_source={ 'jetpack-plugin-recommendations' }
-					/>
-				</div>
+			<h2 className="jp-recommendation-sidebar-mobile__heading">{ getHeading() }</h2>
+			<p>{ getBody() }</p>
+			<div className="jp-recommendations-sidebar-mobile__apps-badge">
+				<AppsBadge
+					onBadgeClick={ onAppBadgeClick }
+					storeName={ 'ios' }
+					utm_source={ 'jetpack-plugin-recommendations' }
+				/>
+				<AppsBadge
+					onBadgeClick={ onAppBadgeClick }
+					storeName={ 'android' }
+					utm_source={ 'jetpack-plugin-recommendations' }
+				/>
 			</div>
 		</SidebarCard>
 	);
