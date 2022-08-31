ad19d91ff0532cf862d885819a2bb31c96f74212
Grant Kinney <hi@grant.mk>
2022-08-31 17:32:39 -0500

    [not verified] Fix build error

--- projects/plugins/jetpack/modules/action-bar.php
+++ projects/plugins/jetpack/modules/action-bar.php
@@ -4,7 +4,7 @@
  * Module Description: An easy to use way for visitors to follow, like, and comment on your site.
  * Sort Order: 40
  * Recommendation Order: 18
- * First Introduced: $$next-version$$
+ * First Introduced: 11.4
  * Requires Connection: Yes
  * Auto Activate: No
  * Module Tags: Social
