a4613f2a48bae56c3993a7a95fb286348a112626
Kolja Zuelsdorf <kolja.zuelsdorf@automattic.com>
2022-09-01 14:09:05 +0200

    Add basic E2E WAF tests (#25643)
    * Added very basic version of a test that blocks a request based on firewall (aka WAF) rules.
    
    * Added jetpack connection prerequisites.
    
    * Added comment to make it more clear why we are not expecting a HTML tag on the blocked request.
    
    * Moved waf data interceptor to its own plugin file.
    
    Co-authored-by: Benedict Singer <singerb@mac.com>
    Co-authored-by: Jared <jwidavid72@gmail.com>
    Co-authored-by: Yaroslav Kukharuk <i.kukharuk@gmail.com>
    Co-authored-by: Brandon Kraft <public@brandonkraft.com>

---
+++ projects/plugins/jetpack/tests/e2e/specs/post-connection/waf-blocking.test.js
@@ -0,0 +1,39 @@
+import { test, expect } from 'jetpack-e2e-commons/fixtures/base-test.js';
+import { WpPage } from 'jetpack-e2e-commons/pages/index.js';
+import playwrightConfig from '../../playwright.config.cjs';
+import { Plans, prerequisitesBuilder } from 'jetpack-e2e-commons/env/index.js';
+import { resolveSiteUrl } from 'jetpack-e2e-commons/helpers/utils-helper.cjs';
+
+test.describe.parallel( 'WAF Blocking', () => {
+	test.beforeAll( async ( { browser } ) => {
+		const page = await browser.newPage( playwrightConfig.use );
+		/* Note that .withPlan( Plans.Complete ) does not really apply yet because we are mocking the data returned from
+		 * the API for now. See tools/e2e-commons/plugins/e2e-waf-data-interceptor.php for details.
+		 */
+		await prerequisitesBuilder( page )
+			.withCleanEnv()
+			.withWpComLoggedIn( true )
+			.withLoggedIn( true )
+			.withConnection( true )
+			.withPlan( Plans.Complete )
+			.withActiveModules( [ 'waf' ] )
+			.build();
+		await page.close();
+	} );
+
+	test( 'Block a simple request', async ( { page } ) => {
+		await test.step( 'Block it', async () => {
+			const blockedPage = new WpPage( page, { pageName: 'Blocked request' } );
+
+			const response = await blockedPage.goto( `${ resolveSiteUrl() }/?blubb=<script>` );
+			expect( response.status() ).toStrictEqual( 403 );
+
+			/*
+			The job of the WAF is to block certain requests, and that is what we are testing here.
+			Given that when a request is blocked, the code does die() with a specific message, we never render the page.
+			The assertion is just to ensure that we indeed do not see a page rendered in the output.
+			 */
+			await expect( response.body() ).not.toContain( '<html>' );
+		} );
+	} );
+} );
