1b299ae882ca8fe5d12f16e65d2a1bdfd2f9c8f5
Olaseni Oluwunmi <olaseni.oluwunmi@automattic.com>
2022-09-01 11:19:24 +0000

    [not verified] This commit was generated from D84975-code.

--- projects/plugins/jetpack/modules/masterbar/admin-menu/class-domain-only-admin-menu.php
+++ projects/plugins/jetpack/modules/masterbar/admin-menu/class-domain-only-admin-menu.php
@@ -23,6 +23,13 @@ class Domain_Only_Admin_Menu extends Base_Admin_Menu {
 		$menu    = array(); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
 		$submenu = array(); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
 
-		add_menu_page( esc_attr__( 'Settings', 'jetpack' ), __( 'Settings', 'jetpack' ), 'manage_options', 'https://wordpress.com/domains/manage/' . $this->domain, null, 'dashicons-admin-settings' );
+		add_menu_page( esc_attr__( 'Manage Domain', 'jetpack' ), __( 'Manage Domain', 'jetpack' ), 'manage_options', 'https://wordpress.com/domains/manage/' . $this->domain . '/edit/' . $this->domain, null, 'dashicons-admin-settings' );
+
+		if ( function_exists( 'wpcom_site_has_feature' ) && wpcom_site_has_feature( \WPCOM_Features::EMAIL_SUBSCRIPTION ) ) {
+			add_menu_page( esc_attr__( 'Manage Email', 'jetpack' ), __( 'Manage Email', 'jetpack' ), 'manage_options', 'https://wordpress.com/email/' . $this->domain . '/manage/' . $this->domain, null, 'dashicons-admin-settings' );
+		}
+
+		add_menu_page( esc_attr__( 'Manage Purchases', 'jetpack' ), __( 'Manage Purchases', 'jetpack' ), 'manage_options', 'https://wordpress.com/purchases/subscriptions/' . $this->domain, null, 'dashicons-cart' );
+		add_menu_page( esc_attr__( 'Inbox', 'jetpack' ), __( 'Inbox', 'jetpack' ), 'manage_options', 'https://wordpress.com/inbox/' . $this->domain, null, 'dashicons-email' );
 	}
 }
--- projects/plugins/jetpack/tests/php/modules/masterbar/test-class-domain-only-admin-menu.php
+++ projects/plugins/jetpack/tests/php/modules/masterbar/test-class-domain-only-admin-menu.php
@@ -52,8 +52,8 @@ class Test_Domain_Only_Admin_Menu extends WP_UnitTestCase {
 	 * @param WP_UnitTest_Factory $factory Fixture factory.
 	 */
 	public static function wpSetUpBeforeClass( $factory ) {
-		static::$domain    = ( new Status() )->get_site_suffix();
-		static::$user_id   = $factory->user->create( array( 'role' => 'administrator' ) );
+		static::$domain = ( new Status() )->get_site_suffix();
+		static::$user_id = $factory->user->create( [ 'role' => 'administrator' ] );
 		static::$menu_data = get_menu_fixture();
 	}
 
@@ -73,16 +73,84 @@ class Test_Domain_Only_Admin_Menu extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Tests reregister_menu_items.
+	 * Tests reregister_menu_items when email subscriptions don't exist.
 	 *
 	 * @covers ::reregister_menu_items
 	 */
-	public function test_reregister_menu_items() {
+	public function test_reregister_menu_items_without_email_subscriptions() {
 		global $menu;
 
 		static::$admin_menu->reregister_menu_items();
 
-		$this->assertCount( 1, $menu );
-		$this->assertEquals( 'https://wordpress.com/domains/manage/' . static::$domain, $menu[0][2] );
+		$this->assertCount( 3, $menu );
+
+		$this->assertEquals( 'https://wordpress.com/domains/manage/' . static::$domain . '/edit/' . static::$domain, $menu[0][2] );
+		$this->assertEquals( 'https://wordpress.com/purchases/subscriptions/' . static::$domain, $menu[1][2] );
+		$this->assertEquals( 'https://wordpress.com/inbox/' . static::$domain, $menu[2][2] );
+	}
+
+	/**
+	 * Tests reregister_menu_items with email subscriptions .
+	 *
+	 * @covers ::reregister_menu_items
+	 */
+	public function test_reregister_menu_items_with_email_subscriptions() {
+		self::createTestEmailSubscription();
+
+		global $menu;
+
+		static::$admin_menu->reregister_menu_items();
+
+		$this->assertCount( 4, $menu );
+
+		$this->assertEquals( 'https://wordpress.com/domains/manage/' . static::$domain . '/edit/' . static::$domain, $menu[0][2] );
+		$this->assertEquals( 'https://wordpress.com/email/' . static::$domain . '/manage/' . static::$domain, $menu[1][2] );
+		$this->assertEquals( 'https://wordpress.com/purchases/subscriptions/' . static::$domain, $menu[2][2] );
+		$this->assertEquals( 'https://wordpress.com/inbox/' . static::$domain, $menu[3][2] );
+
+		self::removeTestEmailSubscription();
+	}
+
+	private static function createTestEmailSubscription() {
+		global $wpdb;
+
+		$product_id = WPCOM_TITAN_MAIL_YEARLY;
+		$meta = 'example.com';
+		$user_id = get_current_user_id();
+		$expiry = gmdate( 'Y-m-d', strtotime( '+1 day' ) );
+
+		\store_logger()
+			->set_authorized_user_id( (int) get_current_user_id() )
+			->set_request_source( \A8C\Billingdaddy\Logger\Request_Source::TESTS() )
+			->set_request_path( sprintf( 'product_id=%d;user_id=%d', $product_id, $user_id ) )
+			->set_request_endpoint( 'Test_Domain_Only_Admin_Menu::createTestSubscription' )
+			->set_request_host( 'test.wordpress.com' );
+
+		$subscription_data = [
+			'user_id' => $user_id,
+			'blog_id' => get_current_blog_id(),
+			'product_id' => $product_id,
+			'meta' => $meta,
+			'expiry' => $expiry,
+			'ownership_id' => wp_rand(),
+		];
+
+		\WPCOM\Store\insert_with_history(
+			$wpdb,
+			$wpdb->store_subscriptions,
+			$subscription_data,
+			Store_Configuration::FEATURE_STORE_SUBSCRIPTIONS_HISTORY
+		);
+
+		update_option( 'bundle_upgrade', 1 );
+
+		\A8C\Billingdaddy\Container::get_purchases_api()->clear_cache_for_site_purchases( get_current_blog_id() );
+	}
+
+	private static function removeTestEmailSubscription(): void {
+		$subscription = \get_subscription( get_current_blog_id(), get_current_user_id(), WPCOM_TITAN_MAIL_YEARLY, 'example.com' );
+		if ( $subscription ) {
+			$subscription->remove();
+		}
 	}
 }
