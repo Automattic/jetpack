ee97d4782fddaab39b124c4eb57274b1e474b78c
Douglas Henri <douglas.henri@automattic.com>
2022-09-05 11:49:44 -0300

    add StatCard component

---
+++ projects/js-packages/components/components/stat-card/index.tsx
@@ -0,0 +1,33 @@
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+/**
+ * Internal dependencies
+ */
+import Text from '../text';
+import styles from './style.module.scss';
+import { StatCardProps } from './types';
+import type React from 'react';
+
+/**
+ * StatCard component
+ *
+ * @param {StatCardProps} props - Component props.
+ * @returns {React.ReactNode} - StatCard react component.
+ */
+const StatCard = ( { className, icon, label, value, variant = 'square' }: StatCardProps ) => {
+	const valueTextVariant = variant === 'square' ? 'headline-small' : 'title-medium-semi-bold';
+
+	return (
+		<div className={ classnames( className, styles.wrapper, styles[ variant ] ) }>
+			<div className={ classnames( styles.icon ) }>{ icon }</div>
+			<div className={ classnames( styles.info ) }>
+				<Text className={ styles.label }>{ label }</Text>
+				<Text variant={ valueTextVariant }>{ value }</Text>
+			</div>
+		</div>
+	);
+};
+
+export default StatCard;
