cc0554f615922d63d78c797061d035b06bde6084
Douglas Henri <douglas.henri@automattic.com>
2022-09-05 15:00:52 -0300

    add number format for stat value

--- projects/js-packages/components/components/stat-card/index.tsx
+++ projects/js-packages/components/components/stat-card/index.tsx
@@ -5,6 +5,7 @@ import classnames from 'classnames';
 /**
  * Internal dependencies
  */
+import numberFormat from '../number-format';
 import Text from '../text';
 import styles from './style.module.scss';
 import { StatCardProps } from './types';
@@ -18,13 +19,14 @@ import type React from 'react';
  */
 const StatCard = ( { className, icon, label, value, variant = 'square' }: StatCardProps ) => {
 	const valueTextVariant = variant === 'square' ? 'headline-small' : 'title-medium-semi-bold';
+	const formattedValue = numberFormat( value );
 
 	return (
 		<div className={ classnames( className, styles.wrapper, styles[ variant ] ) }>
 			<div className={ classnames( styles.icon ) }>{ icon }</div>
 			<div className={ classnames( styles.info ) }>
 				<Text className={ styles.label }>{ label }</Text>
-				<Text variant={ valueTextVariant }>{ value }</Text>
+				<Text variant={ valueTextVariant }>{ formattedValue }</Text>
 			</div>
 		</div>
 	);
