0c6dfea23631693855f91c800f9f43fdbd7c72d2
Karen Attfield <karenlattfield@gmail.com>
2022-09-07 15:44:26 +0100

    Properly alphabetizing the entries

--- .github/files/build-reminder-comment/de-fusioned-files.js
+++ .github/files/build-reminder-comment/de-fusioned-files.js
@@ -7,8 +7,6 @@
  * Keep this list in alphabetical order.
  */
 const deFusionedFiles = [
-	'projects/plugins/jetpack/functions.opengraph.php',
-	'projects/plugins/jetpack/enhanced-open-graph.php',
 	'projects/plugins/jetpack/_inc/crowdsignal-shortcode.js',
 	'projects/plugins/jetpack/_inc/crowdsignal-survey.js',
 	'projects/plugins/jetpack/_inc/polldaddy-shortcode.js',
@@ -17,6 +15,8 @@ const deFusionedFiles = [
 	'projects/plugins/jetpack/_inc/lib/class-jetpack-mapbox-helper.php',
 	'projects/plugins/jetpack/class.jetpack-post-images.php',
 	'projects/plugins/jetpack/class.jetpack-twitter-cards.php',
+	'projects/plugins/jetpack/enhanced-open-graph.php',
+	'projects/plugins/jetpack/functions.opengraph.php',
 	'projects/plugins/jetpack/modules/carousel.php',
 	'projects/plugins/jetpack/modules/carousel/',
 	'projects/plugins/jetpack/functions.photon.php',
