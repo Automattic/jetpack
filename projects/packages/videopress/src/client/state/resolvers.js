bed40786ebd2f4b870dab86b07528bd6b32b4bd3
Damián Suárez <rdsuarez@gmail.com>
2022-09-07 11:30:05 -0300

    VideoPress: add query object to the getVideos() selector (#26074)
    Co-authored-by: Nauris Pūķis <hi@pyronaur.com>
    Co-authored-by: Jasper Kang <jasper.kang@automattic.com>

--- projects/packages/videopress/src/client/state/resolvers.js
+++ projects/packages/videopress/src/client/state/resolvers.js
@@ -1,26 +1,56 @@
 /**
  * Internal dependencies
  */
-import { REST_API_SITE_PURCHASES_ENDPOINT } from './constants';
+import { REST_API_SITE_PURCHASES_ENDPOINT, SET_IS_FETCHING_VIDEOS } from './constants';
+import { stateDebug } from '.';
+
+/**
+ * Return a full request query object,
+ * including the default query parameters.
+ *
+ * @param {object} query - Query object.
+ * @returns {object}       Full query object.
+ */
+function getFullRequestQuery( query ) {
+	return {
+		orderBy: 'date',
+		order: 'DESC',
+		itemsPerPage: 6,
+		page: 1,
+		type: 'video/videopress',
+		...query,
+	};
+}
 
 const getVideos = {
-	isFulfilled: state => {
-		return !! state?.videos?.items;
+	isFulfilled: ( state, recentQuery ) => {
+		const query = state?.videos.query;
+		recentQuery = getFullRequestQuery( recentQuery );
+		const isSameQueryRequest = JSON.stringify( query ) === JSON.stringify( recentQuery );
+		if ( ! isSameQueryRequest ) {
+			stateDebug( 'getVideos: isFulfilled: query mismatch', query, recentQuery );
+			return false;
+		}
+
+		return true;
 	},
-	fulfill: () => async ( { dispatch } ) => {
+
+	fulfill: ( query = {} ) => async ( { dispatch } ) => {
 		const payload = new FormData();
 		payload.set( 'action', 'query-attachments' );
 		payload.set( 'post_id', 0 );
 
-		payload.set( 'query[orderby]', 'date' );
-		payload.set( 'query[order]', 'DESC' );
-		payload.set( 'query[posts_per_page]', 6 );
-		payload.set( 'query[paged]', 1 );
-		payload.set( 'query[mime_type]', 'video/' );
+		const freshQuery = getFullRequestQuery( query );
+
+		payload.set( 'query[orderby]', freshQuery.orderBy );
+		payload.set( 'query[order]', freshQuery.order );
+		payload.set( 'query[posts_per_page]', freshQuery.itemsPerPage );
+		payload.set( 'query[paged]', freshQuery.page );
+		payload.set( 'query[post_mime_type]', freshQuery.type );
+
+		dispatch.setIsFetchingVideos( true, freshQuery );
 
 		try {
-			dispatch.setIsFetchingVideos();
-
 			const response = await fetch( REST_API_SITE_PURCHASES_ENDPOINT, {
 				method: 'POST',
 				body: payload,
@@ -32,11 +62,13 @@ const getVideos = {
 			}
 
 			dispatch.setVideos( body.data );
-			return Promise.resolve();
+			return body.data;
 		} catch ( error ) {
 			dispatch.setFetchVideosError( error );
-			return Promise.resolve();
 		}
+	},
+	shouldInvalidate: action => {
+		return action.type === SET_IS_FETCHING_VIDEOS;
 	},
 };
 
