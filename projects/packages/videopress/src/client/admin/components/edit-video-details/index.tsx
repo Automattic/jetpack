11a262a7f44993339f39674b46e078846c8ff971
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2022-09-08 14:37:31 -0300

    [not verified] VideoPress: Introduce initial version of EditVideoDetails

---
+++ projects/packages/videopress/src/client/admin/components/edit-video-details/index.tsx
@@ -0,0 +1,41 @@
+import { Text, AdminPage, AdminSection, Container, Col } from '@automattic/jetpack-components';
+import { __ } from '@wordpress/i18n';
+import { Icon, chevronRightSmall } from '@wordpress/icons';
+import { useParams } from 'react-router-dom';
+import Logo from '../logo';
+import styles from './style.module.scss';
+
+const Header = () => {
+	return (
+		<div>
+			<div className={ styles.breadcrumb }>
+				<Logo />
+				<Icon icon={ chevronRightSmall } />
+				<Text>{ __( 'Edit video details', 'jetpack-videopress-pkg' ) }</Text>
+			</div>
+		</div>
+	);
+};
+
+const EditVideoDetails = () => {
+	const { videoId } = useParams();
+	return (
+		<AdminPage
+			moduleName={ __( 'Jetpack VideoPress', 'jetpack-videopress-pkg' ) }
+			header={ <Header /> }
+		>
+			<AdminSection>
+				<Container horizontalSpacing={ 6 } horizontalGap={ 10 }>
+					<Col sm={ 4 } md={ 6 } lg={ 12 }>
+						<div>EditVideoDetails { videoId }</div>
+					</Col>
+					<Col sm={ 4 } md={ 6 } lg={ 12 }>
+						<div>EditVideoDetails</div>
+					</Col>
+				</Container>
+			</AdminSection>
+		</AdminPage>
+	);
+};
+
+export default EditVideoDetails;
