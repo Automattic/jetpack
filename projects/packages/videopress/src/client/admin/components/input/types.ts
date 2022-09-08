4100ebe303a8672b0fa165878cc1ce456ce24943
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2022-09-08 16:55:49 -0300

    [not verified] VideoPress: Support size, label and type in Input

--- projects/packages/videopress/src/client/admin/components/input/types.ts
+++ projects/packages/videopress/src/client/admin/components/input/types.ts
@@ -1,30 +1,20 @@
 import type React from 'react';
 
-export type InputProps = {
+type InputBaseProps = {
 	/**
 	 * Optional classname to apply to the root element.
 	 */
 	className?: string;
 
 	/**
-	 * The input value.
+	 * Input size.
 	 */
-	value?: string;
+	size?: 'small' | 'large';
 
 	/**
-	 * The input placeholder.
+	 * Input label.
 	 */
-	placeholder?: string;
-
-	/**
-	 * Whether the input is disabled.
-	 */
-	disabled?: boolean;
-
-	/**
-	 * Optional icon.
-	 */
-	icon?: React.ReactNode;
+	label?: React.ReactNode;
 
 	/**
 	 * Callback to be invoked when the input value changes.
@@ -37,4 +27,28 @@ export type InputProps = {
 	onEnter: ( value: string ) => unknown;
 };
 
-export type SearchInputProps = Omit< InputProps, 'icon' >;
+type Input = Omit< React.InputHTMLAttributes< HTMLInputElement >, 'size' > & {
+	/**
+	 * Optional icon.
+	 */
+	icon?: React.ReactNode;
+	/**
+	 * Input types.
+	 */
+	type?: 'text' | 'password' | 'email' | 'number' | 'search' | 'tel' | 'url';
+} & InputBaseProps;
+
+type TextArea = React.TextareaHTMLAttributes< HTMLTextAreaElement > & {
+	/**
+	 * No support for icon when using textarea.
+	 */
+	icon?: undefined;
+	/**
+	 * Only use these types if is textarea.
+	 */
+	type: 'textarea';
+} & InputBaseProps;
+
+export type InputProps = Input | TextArea;
+
+export type SearchInputProps = InputBaseProps & Omit< Input, 'icon' | 'type' >;
