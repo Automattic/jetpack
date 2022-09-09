dff7dc79cb2a4ef2bbdf73643cf4b5bbc3ec10b1
retrofox <rdsuarez@gmail.com>
2022-09-09 18:35:19 -0300

    [not verified] add doc blocks

--- projects/packages/videopress/src/client/admin/components/checkbox/index.tsx
+++ projects/packages/videopress/src/client/admin/components/checkbox/index.tsx
@@ -1,5 +1,14 @@
+/**
+ * External dependencies
+ */
 import { forwardRef } from 'react';
+/**
+ * Internal dependencies
+ */
 import styles from './style.module.scss';
+/**
+ * Types
+ */
 import { CheckboxProps } from './types';
 
 const Checkbox = forwardRef< HTMLInputElement, CheckboxProps >(
