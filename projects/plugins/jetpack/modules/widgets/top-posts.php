efa283b5b1a6aef83c5150003214d2aabed4d68a
Sagar Nasit <sagarnasit@gmail.com>
2022-09-12 22:02:16 +0530

    Update image check

--- projects/plugins/jetpack/modules/widgets/top-posts.php
+++ projects/plugins/jetpack/modules/widgets/top-posts.php
@@ -420,7 +420,7 @@ class Jetpack_Top_Posts_Widget extends WP_Widget {
 						)
 					);
 
-					if ( ! empty( $image ) ) {
+					if ( $image ) {
 
 						$post['image'] = $image['src'];
 						if ( 'blavatar' !== $image['from'] && 'gravatar' !== $image['from'] ) {
@@ -458,7 +458,7 @@ class Jetpack_Top_Posts_Widget extends WP_Widget {
 						 */
 						$filtered_permalink = apply_filters( 'jetpack_top_posts_widget_permalink', $post['permalink'], $post );
 
-						if ( ! empty( $image ) ) {
+						if ( $image ) {
 							printf(
 								'<a href="%1$s" title="%2$s" class="bump-view" data-bump-view="tp"%3$s><img width="%4$d" height="%5$d" src="%6$s" alt="%2$s" data-pin-nopin="true"/></a>',
 								esc_url( $filtered_permalink ),
@@ -495,7 +495,7 @@ class Jetpack_Top_Posts_Widget extends WP_Widget {
 						/** This filter is documented in modules/widgets/top-posts.php */
 						$filtered_permalink = apply_filters( 'jetpack_top_posts_widget_permalink', $post['permalink'], $post );
 
-						if ( ! empty( $image ) ) {
+						if ( $image ) {
 							printf(
 								'<a href="%1$s" title="%2$s" class="bump-view" data-bump-view="tp"%3$s><img width="%4$d" height="%5$d" src="%6$s" alt="%2$s" data-pin-nopin="true" class="widgets-list-layout-blavatar" /></a>',
 								esc_url( $filtered_permalink ),
