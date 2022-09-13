f186b68d72895788d2f55256e6858c25fed8ba16
retrofox <rdsuarez@gmail.com>
2022-09-13 15:44:18 -0300

    [not verified] propagate values from request response to block attributes

--- projects/plugins/jetpack/extensions/blocks/videopress/video-chapters/edit.js
+++ projects/plugins/jetpack/extensions/blocks/videopress/video-chapters/edit.js
@@ -3,6 +3,7 @@
  */
 import { InspectorControls } from '@wordpress/block-editor';
 import { createHigherOrderComponent } from '@wordpress/compose';
+import { useEffect } from '@wordpress/element';
 /**
  * Internal dependencies
  */
@@ -11,7 +12,23 @@ import useVideoItem from './hooks/use-video-item';
 import { isVideoChaptersEnabled } from '.';
 
 const withVideoChaptersEdit = createHigherOrderComponent( BlockEdit => props => {
-	const [ isRequestingVideoItem ] = useVideoItem( props?.attributes?.id );
+	const [ videoItem, isRequestingVideoItem ] = useVideoItem( props?.attributes?.id );
+	const { setAttributes } = props;
+
+	/*
+	 * Propagate title and description from the video item
+	 * to the block attributes.
+	 */
+	useEffect( () => {
+		if ( ! videoItem ) {
+			return;
+		}
+
+		setAttributes( {
+			title: videoItem?.title,
+			description: videoItem?.description,
+		} );
+	}, [ videoItem, setAttributes ] );
 
 	if ( ! isVideoChaptersEnabled ) {
 		return <BlockEdit { ...props } />;
