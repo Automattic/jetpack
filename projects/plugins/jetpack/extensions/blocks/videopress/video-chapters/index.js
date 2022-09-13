a52aed4611e38e58fc1140901df4e517a67160a7
retrofox <rdsuarez@gmail.com>
2022-09-13 13:10:59 -0300

    [not verified] extend VideoPress block with title and descr attrs

--- projects/plugins/jetpack/extensions/blocks/videopress/video-chapters/index.js
+++ projects/plugins/jetpack/extensions/blocks/videopress/video-chapters/index.js
@@ -3,3 +3,26 @@ const VIDEOPRESS_VIDEO_CHAPTERS_FEATURE = 'videopress/video-chapters';
 export const isVideoChaptersEnabled = !! window?.Jetpack_Editor_Initial_State?.available_blocks[
 	VIDEOPRESS_VIDEO_CHAPTERS_FEATURE
 ];
+
+export default function addVideoPressVideoChaptersSupport( settings, name ) {
+	if ( isVideoChaptersEnabled ) {
+		return settings;
+	}
+
+	if ( name !== 'core/video' ) {
+		return settings;
+	}
+
+	return {
+		...settings,
+		attributes: {
+			...settings.attributes,
+			title: {
+				type: 'string',
+			},
+			description: {
+				type: 'string',
+			},
+		},
+	};
+}
