f4203c7570cd8d662241c9cffcb3bea74972a990
Aneesh Devasthale <aneeshd16@users.noreply.github.com>
2022-09-14 16:40:15 +0530

    Add difm_lite_site_options to get site endpoint

--- projects/plugins/jetpack/json-endpoints/class.wpcom-json-api-get-site-endpoint.php
+++ projects/plugins/jetpack/json-endpoints/class.wpcom-json-api-get-site-endpoint.php
@@ -180,7 +180,14 @@ class WPCOM_JSON_API_GET_Site_Endpoint extends WPCOM_JSON_API_Endpoint {
 		'selected_features',
 		'anchor_podcast',
 		'is_difm_lite_in_progress',
+		'difm_lite_site_options',
 		'site_intent',
+	);
+
+	protected static $displayed_difm_lite_site_options = array(
+		'site_category',
+		'is_website_content_submitted',
+		'selected_page_titles',
 	);
 
 	/**
@@ -726,9 +733,6 @@ class WPCOM_JSON_API_GET_Site_Endpoint extends WPCOM_JSON_API_Endpoint {
 				case 'design_type':
 					$options[ $key ] = $site->get_design_type();
 					break;
-				case 'site_goals':
-					$options[ $key ] = $site->get_site_goals();
-					break;
 				case 'site_segment':
 					$options[ $key ] = $site->get_site_segment();
 					break;
@@ -763,6 +767,21 @@ class WPCOM_JSON_API_GET_Site_Endpoint extends WPCOM_JSON_API_Endpoint {
 					break;
 				case 'is_difm_lite_in_progress':
 					$options[ $key ] = $site->is_difm_lite_in_progress();
+					break;
+				case 'difm_lite_site_options':
+					$difm_lite_options  = $site->get_difm_lite_site_options();
+					$visible_options = self::$displayed_difm_lite_site_options;
+					$filtered_difm_lite_options = new stdClass();
+					if ( $difm_lite_options ) {
+						$filtered_difm_lite_options = array_filter(
+							$difm_lite_options,
+							function ( $key ) use ( $visible_options ) {
+								return in_array( $key, $visible_options );
+							},
+							ARRAY_FILTER_USE_KEY
+						);
+					}
+					$options[ $key ] = $filtered_difm_lite_options;
 					break;
 				case 'site_intent':
 					$options[ $key ] = $site->get_site_intent();
