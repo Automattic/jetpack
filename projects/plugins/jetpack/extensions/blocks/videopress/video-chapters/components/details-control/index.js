98c63b7d6478c1265c693e89d15ad55c748d3e16
Douglas Henri <douglas.henri@automattic.com>
2022-09-16 18:29:50 -0300

    add dynamic rows number to details description on videopress block

--- projects/plugins/jetpack/extensions/blocks/videopress/video-chapters/components/details-control/index.js
+++ projects/plugins/jetpack/extensions/blocks/videopress/video-chapters/components/details-control/index.js
@@ -15,10 +15,21 @@ const isVideoChaptersEnabled = !! window?.Jetpack_Editor_Initial_State?.availabl
 	VIDEOPRESS_VIDEO_CHAPTERS_FEATURE
 ];
 
+const CHARACTERS_PER_LINE = 31;
+
 export default function DetailsControl( { isRequestingVideoItem } ) {
 	const { attributes, setAttributes } = useBlockAttributes();
 	const { title, description } = attributes;
 	const isBeta = isBetaExtension( VIDEOPRESS_VIDEO_CHAPTERS_FEATURE );
+
+	// Expands the description textarea to accommodate the description
+	const rows = description
+		.split( '\n' )
+		.map( line => Math.ceil( line.length / CHARACTERS_PER_LINE ) || 1 )
+		.reduce( ( sum, current ) => sum + current, 0 );
+	const maxRows = 20;
+	const minRows = 4;
+	const descriptionControlRows = Math.min( maxRows, Math.max( rows, minRows ) );
 
 	if ( ! isVideoChaptersEnabled ) {
 		return null;
@@ -48,6 +59,7 @@ export default function DetailsControl( { isRequestingVideoItem } ) {
 				placeholder={ __( 'Video description', 'jetpack' ) }
 				onChange={ setDescriptionAttribute }
 				disabled={ isRequestingVideoItem }
+				rows={ descriptionControlRows }
 			/>
 		</PanelBody>
 	);
