Automattic/jetpack

commit 80d13f14a68db5a5afbc6b383fb9f90dc9df65b0

Paid blocks: Improve rendering of the upgrade nudge (#25812)
Co-authored-by: sdixon194 <steve.dixon@automattic.com>

projects/plugins/jetpack/extensions/extended-blocks/paid-blocks/editor.scss
@@ -1,29 +1,28 @@
 @import '@automattic/jetpack-base-styles/gutenberg-base-styles';
 @import "@automattic/color-studio/dist/color-variables";
 
-$banner-height: 48px;
 $icon-size: 10px;
 $icon-background-color: #fff;
 
 // Upgrade Banner.
 .jetpack-upgrade-plan-banner {
-	$banner-height: 48px;
-
 	.jetpack-upgrade-plan-banner__wrapper {
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
 		font-size: 14px;
-		height: $banner-height;
 		background: black;
 		padding: 0 20px;
 		border-radius: 2px;
 		box-shadow: 0 0 1px inset $studio-white;
+		flex-wrap: wrap;
 
 		.banner-title,
 		.banner-description {
 			color: $studio-white;
 			margin-right: 10px;
+			margin-top: 10px;
+			margin-bottom: 10px;
 		}
 
 		.jetpack-upgrade-plan-banner__title,
@@ -36,6 +35,8 @@ $icon-background-color: #fff;
 			line-height: 1;
 			margin-left: auto;
 			height: 28px;
+			margin-top: 10px;
+			margin-bottom: 10px;
 
 			&.is-primary {
 				text-decoration: none;
@@ -56,12 +57,11 @@ $icon-background-color: #fff;
 	// Banner in the editor-canvas context.
 	&.block-editor-block-list__block {
 		margin-top: 0;
-		margin-bottom: 0;
+		margin-bottom: 12px;
 	}
 
 	&.wp-block[data-align=right],
 	&.wp-block[data-align=left] {
-		height: $banner-height;
 		.jetpack-upgrade-plan-banner__wrapper {
 			max-width: $content-width;
 			width: 100%;
@@ -71,35 +71,6 @@ $icon-background-color: #fff;
 
 .jetpack-upgrade-plan__hidden {
 	display: none;
-}
-
-// Tweak Banner depending on the context.
-// Tweak Banner: Inspector Controls.
-.editor-styles-wrapper [data-block].is-upgradable,
-.block-editor-block-list__block.is-upgradable {
-	margin-top: 0;
-	padding-top: $banner-height;
-}
-
-// Tweak Banner: More specific selector needed for Inspector Controls.
-.editor-styles-wrapper .block-editor-block-list__layout.is-root-container .is-upgradable {
-	margin-top: 0;
-	padding-top: $banner-height;
-}
-
-// Tweak Banner: Editor Canvas.
-// Banner height minus half the toolbar padding to center it.
-.block-editor-block-list__layout .jetpack-upgrade-plan-banner {
-	position: relative;
-	top: $banner-height - ( $grid-unit-15 * 0.5 );
-	z-index: 10;
-}
-
-.block-editor-block-inspector {
-	.jetpack-upgrade-plan-banner {
-		border-radius: 0;
-		margin: 0 20px 20px;
-	}
 }
 
 // Paid Icons: Hide by default.
@@ -142,10 +113,6 @@ $icon-background-color: #fff;
 .block-editor-block-list__block.is-upgradable.is-selected.is-placeholder {
 	padding-top: 0;
 	background-color: transparent;
-
-	.paid-block-media-placeholder {
-		margin-top: $banner-height;
-	}
 }
 
 .block-editor-block-list__layout .block-editor-block-list__block.is-upgradable:focus:after {

projects/plugins/jetpack/extensions/extended-blocks/paid-blocks/with-upgrade-banner.jsx
@@ -5,21 +5,20 @@ import {
 } from '@automattic/jetpack-shared-extension-utils';
 import { createHigherOrderComponent } from '@wordpress/compose';
 import { useSelect } from '@wordpress/data';
-import { useState, useEffect, useMemo, useContext } from '@wordpress/element';
-import classNames from 'classnames';
+import { render, useState, useEffect, useMemo, useContext } from '@wordpress/element';
 import { PaidBlockContext, PaidBlockProvider } from './components';
 import UpgradePlanBanner from './upgrade-plan-banner';
 import { trackUpgradeBannerImpression, trackUpgradeClickEvent } from './utils';
 
 export default createHigherOrderComponent(
-	BlockListBlock => props => {
-		const { name, className, clientId, isSelected, attributes, setAttributes } = props || {};
-		const { onChildBannerVisibilityChange, hasParentBanner } = useContext( PaidBlockContext ) || {};
+	BlockEdit => props => {
+		const { name, clientId, isSelected, attributes, setAttributes } = props || {};
+		const { hasParentBanner } = useContext( PaidBlockContext ) || {};
 
 		const requiredPlan = getRequiredPlan( name );
 
 		if ( ! requiredPlan ) {
-			return <BlockListBlock { ...props } />;
+			return <BlockEdit { ...props } />;
 		}
 
 		const isDualMode = isStillUsableWithFreePlan( name );
@@ -27,7 +26,6 @@ export default createHigherOrderComponent(
 
 		const [ isVisible, setIsVisible ] = useState( ! isDualMode );
 		const [ hasBannerAlreadyShown, setBannerAlreadyShown ] = useState( false );
-		const [ isChildBannerVisible, setIsChildBannerVisible ] = useState( false );
 
 		const bannerContext = 'editor-canvas';
 		const hasChildrenSelected = useSelect(
@@ -35,9 +33,8 @@ export default createHigherOrderComponent(
 			[]
 		);
 
-		// Banner should be not be displayed if one of its children is already displaying a banner.
-		const isBannerVisible =
-			( isSelected || hasChildrenSelected ) && isVisible && ! isChildBannerVisible;
+		// Banner should be not be displayed if one of its parents is already displaying a banner.
+		const isBannerVisible = ( isSelected || hasChildrenSelected ) && isVisible && ! hasParentBanner;
 
 		const trackEventData = useMemo(
 			() => ( {
@@ -73,38 +70,42 @@ export default createHigherOrderComponent(
 			setAttributes( { shouldDisplayFrontendBanner: ! hasParentBanner } );
 		}, [ setAttributes, hasParentBanner ] );
 
-		// Set isChildBannerVisible for parent block.
 		useEffect( () => {
-			// onChildBannerVisibilityChange sets isChildBannerVisible for our parent paid block.
-			// It is undefined if this block has no parent paid block.
-			if ( onChildBannerVisibilityChange ) {
-				onChildBannerVisibilityChange( isBannerVisible || isChildBannerVisible );
+			let upgradeBannerContainer = document.querySelector(
+				`[data-block="${ clientId }"] > .jetpack-block-upgrade-banner-container`
+			);
+			if ( ! upgradeBannerContainer ) {
+				upgradeBannerContainer = document.createElement( 'div' );
+				upgradeBannerContainer.classList.add( 'jetpack-block-upgrade-banner-container' );
+				document.querySelector( `[data-block="${ clientId }"]` ).prepend( upgradeBannerContainer );
 			}
-		}, [ isBannerVisible, isChildBannerVisible, onChildBannerVisibilityChange ] );
 
-		// Set banner CSS classes depending on its visibility.
-		const listBlockCSSClass = classNames( className, {
-			'is-upgradable': isBannerVisible,
-		} );
-
-		return (
-			<PaidBlockProvider
-				onBannerVisibilityChange={ setIsVisible }
-				onChildBannerVisibilityChange={ setIsChildBannerVisible }
-				hasParentBanner
-			>
+			render(
 				<UpgradePlanBanner
-					className={ `is-${ props.name.replace( /\//, '-' ) }-paid-block` }
+					className={ `is-${ name.replace( /\//, '-' ) }-paid-block` }
 					title={ null }
-					align={ props?.attributes?.align }
+					align={ attributes?.align }
 					visible={ isBannerVisible }
 					description={ usableBlocksProps?.description }
 					requiredPlan={ requiredPlan }
 					context={ bannerContext }
 					onRedirect={ () => trackUpgradeClickEvent( trackEventData ) }
-				/>
+				/>,
+				upgradeBannerContainer
+			);
+		}, [
+			attributes?.align,
+			clientId,
+			isBannerVisible,
+			name,
+			requiredPlan,
+			trackEventData,
+			usableBlocksProps?.description,
+		] );
 
-				<BlockListBlock { ...props } className={ listBlockCSSClass } />
+		return (
+			<PaidBlockProvider onBannerVisibilityChange={ setIsVisible } hasParentBanner>
+				<BlockEdit { ...props } />
 			</PaidBlockProvider>
 		);
 	},

projects/plugins/jetpack/extensions/extended-blocks/paid-blocks/components.js
@@ -10,20 +10,14 @@ export const PaidBlockContext = createContext();
  * Paid Block Provider
  *
  * @param {object}  props - Provider properties.
- * @param {Function}  props.onBannerVisibilityChange - Callback to set banner visibility.
- * @param {Function} props.onChildBannerVisibilityChange - Callback to set child banner visibility.
+ * @param {Function} props.onBannerVisibilityChange - Callback to set banner visibility.
  * @param {boolean} props.hasParentBanner - True if a parent of this block has a banner, which may or may not be visible.
  * @param {boolean} props.children - Provider Children.
  * @returns {*} Provider component.
  */
-export const PaidBlockProvider = ( {
-	onBannerVisibilityChange,
-	onChildBannerVisibilityChange,
-	hasParentBanner,
-	children,
-} ) => (
+export const PaidBlockProvider = ( { onBannerVisibilityChange, hasParentBanner, children } ) => (
 	<PaidBlockContext.Provider
-		value={ { onBannerVisibilityChange, onChildBannerVisibilityChange, hasParentBanner } }
+		value={ { onBannerVisibilityChange, hasParentBanner } }
 		children={ children }
 	/>
 );

projects/plugins/jetpack/extensions/extended-blocks/paid-blocks/index.js
@@ -37,6 +37,12 @@ const jetpackPaidBlock = ( settings, name ) => {
 				default: true,
 			};
 		}
+
+		// Ensure that the toolbar of the inner blocks doesn't overlap the upgrade banner.
+		settings.supports = {
+			...settings.supports,
+			__experimentalExposeControlsToChildren: true,
+		};
 	}
 
 	return settings;
@@ -46,7 +52,7 @@ const jetpackPaidBlock = ( settings, name ) => {
 addFilter( 'blocks.registerBlockType', 'jetpack/paid-block', jetpackPaidBlock );
 
 // Extend BlockListBlock.
-addFilter( 'editor.BlockListBlock', 'jetpack/paid-block-with-warning', withUpgradeBanner );
+addFilter( 'editor.BlockEdit', 'jetpack/paid-block-with-warning', withUpgradeBanner );
 
 // Take the control of the MediaPlaceholder.
 addFilter(

projects/plugins/jetpack/extensions/shared/components/upgrade-nudge/style.scss
@@ -6,7 +6,6 @@
 	justify-content: space-between;
 	align-items: center;
 	font-size: 14px;
-	height: 48px;
 	background: $studio-black;
 	padding: 0 20px;
 	border-radius: 2px;
@@ -33,10 +32,11 @@
 
 .jetpack-upgrade-plan-banner.block-editor-block-list__block {
 	margin-top: 0;
-	margin-bottom: 0; }
+	margin-bottom: 0;
+}
 
-.jetpack-upgrade-plan-banner.wp-block[data-align=right], .jetpack-upgrade-plan-banner.wp-block[data-align=left] {
-	height: 48px; }
-	.jetpack-upgrade-plan-banner.wp-block[data-align=right] .jetpack-upgrade-plan-banner__wrapper, .jetpack-upgrade-plan-banner.wp-block[data-align=left] .jetpack-upgrade-plan-banner__wrapper {
-		max-width: 580px;
-		width: 100%; }
+.jetpack-upgrade-plan-banner.wp-block[data-align=right] .jetpack-upgrade-plan-banner__wrapper,
+.jetpack-upgrade-plan-banner.wp-block[data-align=left] .jetpack-upgrade-plan-banner__wrapper {
+	max-width: 580px;
+	width: 100%;
+}

commit 87399db16c7417e9c9dfa55aa3b67659812532c1

Paid blocks: Show upgrade nudge for paid blocks nested inside a cover block (#26234)
Fixes a regression introduced by #25812 that prevented the upgrade nudges of paid blocks nested inside a cover block from being visible.

projects/plugins/jetpack/extensions/extended-blocks/paid-blocks/editor.scss
@@ -93,6 +93,11 @@ $icon-background-color: #fff;
 	}
 }
 
+// Cover block: Override flexbox so we can place the banner above the block.
+.wp-block-cover.is-placeholder {
+	display: block;
+}
+
 // Media Placeholder
 .paid-block-media-placeholder {
 	width: 100%;

projects/plugins/jetpack/extensions/extended-blocks/paid-blocks/with-upgrade-banner.jsx
@@ -109,7 +109,10 @@ export default createHigherOrderComponent(
 		] );
 
 		return (
-			<PaidBlockProvider onBannerVisibilityChange={ setIsVisible } hasParentBanner>
+			<PaidBlockProvider
+				onBannerVisibilityChange={ setIsVisible }
+				hasParentBanner={ isBannerVisible }
+			>
 				<BlockEdit { ...props } />
 			</PaidBlockProvider>
 		);