9ecb9183a299f1c5939e3eee3be98e462306def1
Nate Weller <nate.weller@automattic.com>
2022-09-19 11:34:07 -0600

    Fix implementation of Jetpack_Plan class

--- projects/plugins/protect/src/class-status.php
+++ projects/plugins/protect/src/class-status.php
@@ -7,6 +7,8 @@
 
 namespace Automattic\Jetpack\Protect;
 
+use Jetpack_Plan;
+
 /**
  * Class that handles fetching and caching the Status of vulnerabilities check from the WPCOM servers
  */
@@ -52,7 +54,7 @@ class Status {
 	 * @return Status_Model
 	 */
 	public static function get_status() {
-		$use_scan_status = \Jetpack_Plan::supports( 'scan' );
+		$use_scan_status = Jetpack_Plan::supports( 'scan' );
 
 		if ( defined( 'JETPACK_PROTECT_DEV__DATA_SOURCE' ) ) {
 			if ( 'scan_api' === JETPACK_PROTECT_DEV__DATA_SOURCE ) {
