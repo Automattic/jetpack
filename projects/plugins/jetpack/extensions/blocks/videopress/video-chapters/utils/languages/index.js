02a855720125ff788b18a94720605c15a12d2333
retrofox <rdsuarez@gmail.com>
2022-09-19 11:16:46 -0300

    [not verified] add helper to get languages list

---
+++ projects/plugins/jetpack/extensions/blocks/videopress/video-chapters/utils/languages/index.js
@@ -0,0 +1,922 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+export function getIsoLangs() {
+	return [
+		{
+			label: __( 'Abkhaz', 'jetpack' ),
+			value: 'ab',
+			nativeName: 'аҧсуа',
+		},
+		{
+			label: __( 'Afar', 'jetpack' ),
+			value: 'aa',
+			nativeName: 'Afaraf',
+		},
+		{
+			label: __( 'Afrikaans', 'jetpack' ),
+			value: 'af',
+			nativeName: 'Afrikaans',
+		},
+		{
+			label: __( 'Akan', 'jetpack' ),
+			value: 'ak',
+			nativeName: 'Akan',
+		},
+		{
+			label: __( 'Albanian', 'jetpack' ),
+			value: 'sq',
+			nativeName: 'Shqip',
+		},
+		{
+			label: __( 'Amharic', 'jetpack' ),
+			value: 'am',
+			nativeName: 'አማርኛ',
+		},
+		{
+			label: __( 'Arabic', 'jetpack' ),
+			value: 'ar',
+			nativeName: 'العربية',
+		},
+		{
+			label: __( 'Aragonese', 'jetpack' ),
+			value: 'an',
+			nativeName: 'Aragonés',
+		},
+		{
+			label: __( 'Armenian', 'jetpack' ),
+			value: 'hy',
+			nativeName: 'Հայերեն',
+		},
+		{
+			label: __( 'Assamese', 'jetpack' ),
+			value: 'as',
+			nativeName: 'অসমীয়া',
+		},
+		{
+			label: __( 'Avaric', 'jetpack' ),
+			value: 'av',
+			nativeName: 'авар мацӀ, магӀарул мацӀ',
+		},
+		{
+			label: __( 'Avestan', 'jetpack' ),
+			value: 'ae',
+			nativeName: 'avesta',
+		},
+		{
+			label: __( 'Aymara', 'jetpack' ),
+			value: 'ay',
+			nativeName: 'aymar aru',
+		},
+		{
+			label: __( 'Azerbaijani', 'jetpack' ),
+			value: 'az',
+			nativeName: 'azərbaycan dili',
+		},
+		{
+			label: __( 'Bambara', 'jetpack' ),
+			value: 'bm',
+			nativeName: 'bamanankan',
+		},
+		{
+			label: __( 'Bashkir', 'jetpack' ),
+			value: 'ba',
+			nativeName: 'башҡорт теле',
+		},
+		{
+			label: __( 'Basque', 'jetpack' ),
+			value: 'eu',
+			nativeName: 'euskara, euskera',
+		},
+		{
+			label: __( 'Belarusian', 'jetpack' ),
+			value: 'be',
+			nativeName: 'Беларуская',
+		},
+		{
+			label: __( 'Bengali', 'jetpack' ),
+			value: 'bn',
+			nativeName: 'বাংলা',
+		},
+		{
+			label: __( 'Bihari', 'jetpack' ),
+			value: 'bh',
+			nativeName: 'भोजपुरी',
+		},
+		{
+			label: __( 'Bislama', 'jetpack' ),
+			value: 'bi',
+			nativeName: 'Bislama',
+		},
+		{
+			label: __( 'Bosnian', 'jetpack' ),
+			value: 'bs',
+			nativeName: 'bosanski jezik',
+		},
+		{
+			label: __( 'Breton', 'jetpack' ),
+			value: 'br',
+			nativeName: 'brezhoneg',
+		},
+		{
+			label: __( 'Bulgarian', 'jetpack' ),
+			value: 'bg',
+			nativeName: 'български език',
+		},
+		{
+			label: __( 'Burmese', 'jetpack' ),
+			value: 'my',
+			nativeName: 'ဗမာစာ',
+		},
+		{
+			label: __( 'Catalan; Valencian', 'jetpack' ),
+			value: 'ca',
+			nativeName: 'Català',
+		},
+		{
+			label: __( 'Chamorro', 'jetpack' ),
+			value: 'ch',
+			nativeName: 'Chamoru',
+		},
+		{
+			label: __( 'Chechen', 'jetpack' ),
+			value: 'ce',
+			nativeName: 'нохчийн мотт',
+		},
+		{
+			label: __( 'Chichewa; Chewa; Nyanja', 'jetpack' ),
+			value: 'ny',
+			nativeName: 'chiCheŵa, chinyanja',
+		},
+		{
+			label: __( 'Chinese', 'jetpack' ),
+			value: 'zh',
+			nativeName: '中文 (Zhōngwén), 汉语, 漢語',
+		},
+		{
+			label: __( 'Chuvash', 'jetpack' ),
+			value: 'cv',
+			nativeName: 'чӑваш чӗлхи',
+		},
+		{
+			label: __( 'Cornish', 'jetpack' ),
+			value: 'kw',
+			nativeName: 'Kernewek',
+		},
+		{
+			label: __( 'Corsican', 'jetpack' ),
+			value: 'co',
+			nativeName: 'corsu, lingua corsa',
+		},
+		{
+			label: __( 'Cree', 'jetpack' ),
+			value: 'cr',
+			nativeName: 'ᓀᐦᐃᔭᐍᐏᐣ',
+		},
+		{
+			label: __( 'Croatian', 'jetpack' ),
+			value: 'hr',
+			nativeName: 'hrvatski',
+		},
+		{
+			label: __( 'Czech', 'jetpack' ),
+			value: 'cs',
+			nativeName: 'česky, čeština',
+		},
+		{
+			label: __( 'Danish', 'jetpack' ),
+			value: 'da',
+			nativeName: 'dansk',
+		},
+		{
+			label: __( 'Divehi; Dhivehi; Maldivian;', 'jetpack' ),
+			value: 'dv',
+			nativeName: 'ދިވެހި',
+		},
+		{
+			label: __( 'Dutch', 'jetpack' ),
+			value: 'nl',
+			nativeName: 'Nederlands, Vlaams',
+		},
+		{
+			label: __( 'English', 'jetpack' ),
+			value: 'en',
+			nativeName: 'English',
+		},
+		{
+			label: __( 'Esperanto', 'jetpack' ),
+			value: 'eo',
+			nativeName: 'Esperanto',
+		},
+		{
+			label: __( 'Estonian', 'jetpack' ),
+			value: 'et',
+			nativeName: 'eesti, eesti keel',
+		},
+		{
+			label: __( 'Ewe', 'jetpack' ),
+			value: 'ee',
+			nativeName: 'Eʋegbe',
+		},
+		{
+			label: __( 'Faroese', 'jetpack' ),
+			value: 'fo',
+			nativeName: 'føroyskt',
+		},
+		{
+			label: __( 'Fijian', 'jetpack' ),
+			value: 'fj',
+			nativeName: 'vosa Vakaviti',
+		},
+		{
+			label: __( 'Finnish', 'jetpack' ),
+			value: 'fi',
+			nativeName: 'suomi, suomen kieli',
+		},
+		{
+			label: __( 'French', 'jetpack' ),
+			value: 'fr',
+			nativeName: 'français, langue française',
+		},
+		{
+			label: __( 'Fula; Fulah; Pulaar; Pular', 'jetpack' ),
+			value: 'ff',
+			nativeName: 'Fulfulde, Pulaar, Pular',
+		},
+		{
+			label: __( 'Galician', 'jetpack' ),
+			value: 'gl',
+			nativeName: 'Galego',
+		},
+		{
+			label: __( 'Georgian', 'jetpack' ),
+			value: 'ka',
+			nativeName: 'ქართული',
+		},
+		{
+			label: __( 'German', 'jetpack' ),
+			value: 'de',
+			nativeName: 'Deutsch',
+		},
+		{
+			label: __( 'Greek, Modern', 'jetpack' ),
+			value: 'el',
+			nativeName: 'Ελληνικά',
+		},
+		{
+			label: __( 'Guaraní', 'jetpack' ),
+			value: 'gn',
+			nativeName: 'Avañeẽ',
+		},
+		{
+			label: __( 'Gujarati', 'jetpack' ),
+			value: 'gu',
+			nativeName: 'ગુજરાતી',
+		},
+		{
+			label: __( 'Haitian; Haitian Creole', 'jetpack' ),
+			value: 'ht',
+			nativeName: 'Kreyòl ayisyen',
+		},
+		{
+			label: __( 'Hausa', 'jetpack' ),
+			value: 'ha',
+			nativeName: 'Hausa, هَوُسَ',
+		},
+		{
+			label: __( 'Hebrew (modern)', 'jetpack' ),
+			value: 'he',
+			nativeName: 'עברית',
+		},
+		{
+			label: __( 'Herero', 'jetpack' ),
+			value: 'hz',
+			nativeName: 'Otjiherero',
+		},
+		{
+			label: __( 'Hindi', 'jetpack' ),
+			value: 'hi',
+			nativeName: 'हिन्दी, हिंदी',
+		},
+		{
+			label: __( 'Hiri Motu', 'jetpack' ),
+			value: 'ho',
+			nativeName: 'Hiri Motu',
+		},
+		{
+			label: __( 'Hungarian', 'jetpack' ),
+			value: 'hu',
+			nativeName: 'Magyar',
+		},
+		{
+			label: __( 'Interlingua', 'jetpack' ),
+			value: 'ia',
+			nativeName: 'Interlingua',
+		},
+		{
+			label: __( 'Indonesian', 'jetpack' ),
+			value: 'id',
+			nativeName: 'Bahasa Indonesia',
+		},
+		{
+			label: __( 'Interlingue', 'jetpack' ),
+			value: 'ie',
+			nativeName: 'Originally called Occidental; then Interlingue after WWII',
+		},
+		{
+			label: __( 'Irish', 'jetpack' ),
+			value: 'ga',
+			nativeName: 'Gaeilge',
+		},
+		{
+			label: __( 'Igbo', 'jetpack' ),
+			value: 'ig',
+			nativeName: 'Asụsụ Igbo',
+		},
+		{
+			label: __( 'Inupiaq', 'jetpack' ),
+			value: 'ik',
+			nativeName: 'Iñupiaq, Iñupiatun',
+		},
+		{
+			label: __( 'Ido', 'jetpack' ),
+			value: 'io',
+			nativeName: 'Ido',
+		},
+		{
+			label: __( 'Icelandic', 'jetpack' ),
+			value: 'is',
+			nativeName: 'Íslenska',
+		},
+		{
+			label: __( 'Italian', 'jetpack' ),
+			value: 'it',
+			nativeName: 'Italiano',
+		},
+		{
+			label: __( 'Inuktitut', 'jetpack' ),
+			value: 'iu',
+			nativeName: 'ᐃᓄᒃᑎᑐᑦ',
+		},
+		{
+			label: __( 'Japanese', 'jetpack' ),
+			value: 'ja',
+			nativeName: '日本語 (にほんご／にっぽんご)',
+		},
+		{
+			label: __( 'Javanese', 'jetpack' ),
+			value: 'jv',
+			nativeName: 'basa Jawa',
+		},
+		{
+			label: __( 'Kalaallisut, Greenlandic', 'jetpack' ),
+			value: 'kl',
+			nativeName: 'kalaallisut, kalaallit oqaasii',
+		},
+		{
+			label: __( 'Kannada', 'jetpack' ),
+			value: 'kn',
+			nativeName: 'ಕನ್ನಡ',
+		},
+		{
+			label: __( 'Kanuri', 'jetpack' ),
+			value: 'kr',
+			nativeName: 'Kanuri',
+		},
+		{
+			label: __( 'Kashmiri', 'jetpack' ),
+			value: 'ks',
+			nativeName: 'कश्मीरी, كشميري',
+		},
+		{
+			label: __( 'Kazakh', 'jetpack' ),
+			value: 'kk',
+			nativeName: 'Қазақ тілі',
+		},
+		{
+			label: __( 'Khmer', 'jetpack' ),
+			value: 'km',
+			nativeName: 'ភាសាខ្មែរ',
+		},
+		{
+			label: __( 'Kikuyu, Gikuyu', 'jetpack' ),
+			value: 'ki',
+			nativeName: 'Gĩkũyũ',
+		},
+		{
+			label: __( 'Kinyarwanda', 'jetpack' ),
+			value: 'rw',
+			nativeName: 'Ikinyarwanda',
+		},
+		{
+			label: __( 'Kirghiz, Kyrgyz', 'jetpack' ),
+			value: 'ky',
+			nativeName: 'кыргыз тили',
+		},
+		{
+			label: __( 'Komi', 'jetpack' ),
+			value: 'kv',
+			nativeName: 'коми кыв',
+		},
+		{
+			label: __( 'Kongo', 'jetpack' ),
+			value: 'kg',
+			nativeName: 'KiKongo',
+		},
+		{
+			label: __( 'Korean', 'jetpack' ),
+			value: 'ko',
+			nativeName: '한국어 (韓國語), 조선말 (朝鮮語)',
+		},
+		{
+			label: __( 'Kurdish', 'jetpack' ),
+			value: 'ku',
+			nativeName: 'Kurdî, كوردی',
+		},
+		{
+			label: __( 'Kwanyama, Kuanyama', 'jetpack' ),
+			value: 'kj',
+			nativeName: 'Kuanyama',
+		},
+		{
+			label: __( 'Latin', 'jetpack' ),
+			value: 'la',
+			nativeName: 'latine, lingua latina',
+		},
+		{
+			label: __( 'Luxembourgish, Letzeburgesch', 'jetpack' ),
+			value: 'lb',
+			nativeName: 'Lëtzebuergesch',
+		},
+		{
+			label: __( 'Luganda', 'jetpack' ),
+			value: 'lg',
+			nativeName: 'Luganda',
+		},
+		{
+			label: __( 'Limburgish, Limburgan, Limburger', 'jetpack' ),
+			value: 'li',
+			nativeName: 'Limburgs',
+		},
+		{
+			label: __( 'Lingala', 'jetpack' ),
+			value: 'ln',
+			nativeName: 'Lingála',
+		},
+		{
+			label: __( 'Lao', 'jetpack' ),
+			value: 'lo',
+			nativeName: 'ພາສາລາວ',
+		},
+		{
+			label: __( 'Lithuanian', 'jetpack' ),
+			value: 'lt',
+			nativeName: 'lietuvių kalba',
+		},
+		{
+			label: __( 'Luba-Katanga', 'jetpack' ),
+			value: 'lu',
+			nativeName: '',
+		},
+		{
+			label: __( 'Latvian', 'jetpack' ),
+			value: 'lv',
+			nativeName: 'latviešu valoda',
+		},
+		{
+			label: __( 'Manx', 'jetpack' ),
+			value: 'gv',
+			nativeName: 'Gaelg, Gailck',
+		},
+		{
+			label: __( 'Macedonian', 'jetpack' ),
+			value: 'mk',
+			nativeName: 'македонски јазик',
+		},
+		{
+			label: __( 'Malagasy', 'jetpack' ),
+			value: 'mg',
+			nativeName: 'Malagasy fiteny',
+		},
+		{
+			label: __( 'Malay', 'jetpack' ),
+			value: 'ms',
+			nativeName: 'bahasa Melayu, بهاس ملايو',
+		},
+		{
+			label: __( 'Malayalam', 'jetpack' ),
+			value: 'ml',
+			nativeName: 'മലയാളം',
+		},
+		{
+			label: __( 'Maltese', 'jetpack' ),
+			value: 'mt',
+			nativeName: 'Malti',
+		},
+		{
+			label: __( 'Māori', 'jetpack' ),
+			value: 'mi',
+			nativeName: 'te reo Māori',
+		},
+		{
+			label: __( 'Marathi (Marāṭhī)', 'jetpack' ),
+			value: 'mr',
+			nativeName: 'मराठी',
+		},
+		{
+			label: __( 'Marshallese', 'jetpack' ),
+			value: 'mh',
+			nativeName: 'Kajin M̧ajeļ',
+		},
+		{
+			label: __( 'Mongolian', 'jetpack' ),
+			value: 'mn',
+			nativeName: 'монгол',
+		},
+		{
+			label: __( 'Nauru', 'jetpack' ),
+			value: 'na',
+			nativeName: 'Ekakairũ Naoero',
+		},
+		{
+			label: __( 'Navajo, Navaho', 'jetpack' ),
+			value: 'nv',
+			nativeName: 'Diné bizaad, Dinékʼehǰí',
+		},
+		{
+			label: __( 'Norwegian Bokmål', 'jetpack' ),
+			value: 'nb',
+			nativeName: 'Norsk bokmål',
+		},
+		{
+			label: __( 'North Ndebele', 'jetpack' ),
+			value: 'nd',
+			nativeName: 'isiNdebele',
+		},
+		{
+			label: __( 'Nepali', 'jetpack' ),
+			value: 'ne',
+			nativeName: 'नेपाली',
+		},
+		{
+			label: __( 'Ndonga', 'jetpack' ),
+			value: 'ng',
+			nativeName: 'Owambo',
+		},
+		{
+			label: __( 'Norwegian Nynorsk', 'jetpack' ),
+			value: 'nn',
+			nativeName: 'Norsk nynorsk',
+		},
+		{
+			label: __( 'Norwegian', 'jetpack' ),
+			value: 'no',
+			nativeName: 'Norsk',
+		},
+		{
+			label: __( 'Nuosu', 'jetpack' ),
+			value: 'ii',
+			nativeName: 'ꆈꌠ꒿ Nuosuhxop',
+		},
+		{
+			label: __( 'South Ndebele', 'jetpack' ),
+			value: 'nr',
+			nativeName: 'isiNdebele',
+		},
+		{
+			label: __( 'Occitan', 'jetpack' ),
+			value: 'oc',
+			nativeName: 'Occitan',
+		},
+		{
+			label: __( 'Ojibwe, Ojibwa', 'jetpack' ),
+			value: 'oj',
+			nativeName: 'ᐊᓂᔑᓈᐯᒧᐎᓐ',
+		},
+		{
+			label: __(
+				'Old Church Slavonic, Church Slavic, Church Slavonic, Old Bulgarian, Old Slavonic',
+				'jetpack'
+			),
+			value: 'cu',
+			nativeName: 'ѩзыкъ словѣньскъ',
+		},
+		{
+			label: __( 'Oromo', 'jetpack' ),
+			value: 'om',
+			nativeName: 'Afaan Oromoo',
+		},
+		{
+			label: __( 'Oriya', 'jetpack' ),
+			value: 'or',
+			nativeName: 'ଓଡ଼ିଆ',
+		},
+		{
+			label: __( 'Ossetian, Ossetic', 'jetpack' ),
+			value: 'os',
+			nativeName: 'ирон æвзаг',
+		},
+		{
+			label: __( 'Panjabi, Punjabi', 'jetpack' ),
+			value: 'pa',
+			nativeName: 'ਪੰਜਾਬੀ, پنجابی',
+		},
+		{
+			label: __( 'Pāli', 'jetpack' ),
+			value: 'pi',
+			nativeName: 'पाऴि',
+		},
+		{
+			label: __( 'Persian', 'jetpack' ),
+			value: 'fa',
+			nativeName: 'فارسی',
+		},
+		{
+			label: __( 'Polish', 'jetpack' ),
+			value: 'pl',
+			nativeName: 'polski',
+		},
+		{
+			label: __( 'Pashto, Pushto', 'jetpack' ),
+			value: 'ps',
+			nativeName: 'پښتو',
+		},
+		{
+			label: __( 'Portuguese', 'jetpack' ),
+			value: 'pt',
+			nativeName: 'Português',
+		},
+		{
+			label: __( 'Quechua', 'jetpack' ),
+			value: 'qu',
+			nativeName: 'Runa Simi, Kichwa',
+		},
+		{
+			label: __( 'Romansh', 'jetpack' ),
+			value: 'rm',
+			nativeName: 'rumantsch grischun',
+		},
+		{
+			label: __( 'Kirundi', 'jetpack' ),
+			value: 'rn',
+			nativeName: 'kiRundi',
+		},
+		{
+			label: __( 'Romanian, Moldavian, Moldovan', 'jetpack' ),
+			value: 'ro',
+			nativeName: 'română',
+		},
+		{
+			label: __( 'Russian', 'jetpack' ),
+			value: 'ru',
+			nativeName: 'русский язык',
+		},
+		{
+			label: __( 'Sanskrit (Saṁskṛta)', 'jetpack' ),
+			value: 'sa',
+			nativeName: 'संस्कृतम्',
+		},
+		{
+			label: __( 'Sardinian', 'jetpack' ),
+			value: 'sc',
+			nativeName: 'sardu',
+		},
+		{
+			label: __( 'Sindhi', 'jetpack' ),
+			value: 'sd',
+			nativeName: 'सिन्धी, سنڌي، سندھی',
+		},
+		{
+			label: __( 'Northern Sami', 'jetpack' ),
+			value: 'se',
+			nativeName: 'Davvisámegiella',
+		},
+		{
+			label: __( 'Samoan', 'jetpack' ),
+			value: 'sm',
+			nativeName: 'gagana faa Samoa',
+		},
+		{
+			label: __( 'Sango', 'jetpack' ),
+			value: 'sg',
+			nativeName: 'yângâ tî sängö',
+		},
+		{
+			label: __( 'Serbian', 'jetpack' ),
+			value: 'sr',
+			nativeName: 'српски језик',
+		},
+		{
+			label: __( 'Scottish Gaelic; Gaelic', 'jetpack' ),
+			value: 'gd',
+			nativeName: 'Gàidhlig',
+		},
+		{
+			label: __( 'Shona', 'jetpack' ),
+			value: 'sn',
+			nativeName: 'chiShona',
+		},
+		{
+			label: __( 'Sinhala, Sinhalese', 'jetpack' ),
+			value: 'si',
+			nativeName: 'සිංහල',
+		},
+		{
+			label: __( 'Slovak', 'jetpack' ),
+			value: 'sk',
+			nativeName: 'slovenčina',
+		},
+		{
+			label: __( 'Slovene', 'jetpack' ),
+			value: 'sl',
+			nativeName: 'slovenščina',
+		},
+		{
+			label: __( 'Somali', 'jetpack' ),
+			value: 'so',
+			nativeName: 'Soomaaliga, af Soomaali',
+		},
+		{
+			label: __( 'Southern Sotho', 'jetpack' ),
+			value: 'st',
+			nativeName: 'Sesotho',
+		},
+		{
+			label: __( 'Spanish; Castilian', 'jetpack' ),
+			value: 'es',
+			nativeName: 'español, castellano',
+		},
+		{
+			label: __( 'Sundanese', 'jetpack' ),
+			value: 'su',
+			nativeName: 'Basa Sunda',
+		},
+		{
+			label: __( 'Swahili', 'jetpack' ),
+			value: 'sw',
+			nativeName: 'Kiswahili',
+		},
+		{
+			label: __( 'Swati', 'jetpack' ),
+			value: 'ss',
+			nativeName: 'SiSwati',
+		},
+		{
+			label: __( 'Swedish', 'jetpack' ),
+			value: 'sv',
+			nativeName: 'svenska',
+		},
+		{
+			label: __( 'Tamil', 'jetpack' ),
+			value: 'ta',
+			nativeName: 'தமிழ்',
+		},
+		{
+			label: __( 'Telugu', 'jetpack' ),
+			value: 'te',
+			nativeName: 'తెలుగు',
+		},
+		{
+			label: __( 'Tajik', 'jetpack' ),
+			value: 'tg',
+			nativeName: 'тоҷикӣ, toğikī, تاجیکی',
+		},
+		{
+			label: __( 'Thai', 'jetpack' ),
+			value: 'th',
+			nativeName: 'ไทย',
+		},
+		{
+			label: __( 'Tigrinya', 'jetpack' ),
+			value: 'ti',
+			nativeName: 'ትግርኛ',
+		},
+		{
+			label: __( 'Tibetan Standard, Tibetan, Central', 'jetpack' ),
+			value: 'bo',
+			nativeName: 'བོད་ཡིག',
+		},
+		{
+			label: __( 'Turkmen', 'jetpack' ),
+			value: 'tk',
+			nativeName: 'Türkmen, Түркмен',
+		},
+		{
+			label: __( 'Tagalog', 'jetpack' ),
+			value: 'tl',
+			nativeName: 'Wikang Tagalog, ᜏᜒᜃᜅ᜔ ᜆᜄᜎᜓᜄ᜔',
+		},
+		{
+			label: __( 'Tswana', 'jetpack' ),
+			value: 'tn',
+			nativeName: 'Setswana',
+		},
+		{
+			label: __( 'Tonga (Tonga Islands)', 'jetpack' ),
+			value: 'to',
+			nativeName: 'faka Tonga',
+		},
+		{
+			label: __( 'Turkish', 'jetpack' ),
+			value: 'tr',
+			nativeName: 'Türkçe',
+		},
+		{
+			label: __( 'Tsonga', 'jetpack' ),
+			value: 'ts',
+			nativeName: 'Xitsonga',
+		},
+		{
+			label: __( 'Tatar', 'jetpack' ),
+			value: 'tt',
+			nativeName: 'татарча, tatarça, تاتارچا',
+		},
+		{
+			label: __( 'Twi', 'jetpack' ),
+			value: 'tw',
+			nativeName: 'Twi',
+		},
+		{
+			label: __( 'Tahitian', 'jetpack' ),
+			value: 'ty',
+			nativeName: 'Reo Tahiti',
+		},
+		{
+			label: __( 'Uighur, Uyghur', 'jetpack' ),
+			value: 'ug',
+			nativeName: 'Uyƣurqə, ئۇيغۇرچە',
+		},
+		{
+			label: __( 'Ukrainian', 'jetpack' ),
+			value: 'uk',
+			nativeName: 'українська',
+		},
+		{
+			label: __( 'Urdu', 'jetpack' ),
+			value: 'ur',
+			nativeName: 'اردو',
+		},
+		{
+			label: __( 'Uzbek', 'jetpack' ),
+			value: 'uz',
+			nativeName: 'zbek, Ўзбек, أۇزبېك',
+		},
+		{
+			label: __( 'Venda', 'jetpack' ),
+			value: 've',
+			nativeName: 'Tshivenḓa',
+		},
+		{
+			label: __( 'Vietnamese', 'jetpack' ),
+			value: 'vi',
+			nativeName: 'Tiếng Việt',
+		},
+		{
+			label: __( 'Volapük', 'jetpack' ),
+			value: 'vo',
+			nativeName: 'Volapük',
+		},
+		{
+			label: __( 'Walloon', 'jetpack' ),
+			value: 'wa',
+			nativeName: 'Walon',
+		},
+		{
+			label: __( 'Welsh', 'jetpack' ),
+			value: 'cy',
+			nativeName: 'Cymraeg',
+		},
+		{
+			label: __( 'Wolof', 'jetpack' ),
+			value: 'wo',
+			nativeName: 'Wollof',
+		},
+		{
+			label: __( 'Western Frisian', 'jetpack' ),
+			value: 'fy',
+			nativeName: 'Frysk',
+		},
+		{
+			label: __( 'Xhosa', 'jetpack' ),
+			value: 'xh',
+			nativeName: 'isiXhosa',
+		},
+		{
+			label: __( 'Yiddish', 'jetpack' ),
+			value: 'yi',
+			nativeName: 'ייִדיש',
+		},
+		{
+			label: __( 'Yoruba', 'jetpack' ),
+			value: 'yo',
+			nativeName: 'Yorùbá',
+		},
+		{
+			label: __( 'Zhuang, Chuang', 'jetpack' ),
+			value: 'za',
+			nativeName: 'Saɯ cueŋƅ, Saw cuengh',
+		},
+	];
+}
