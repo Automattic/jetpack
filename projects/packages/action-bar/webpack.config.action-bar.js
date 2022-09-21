bf19b548dfa230244d857eab238900e1a50d416a
Eru Penkman <roo2@protonmail.com>
2022-09-21 14:38:08 +1000

    [not verified] convert to scss

--- projects/packages/action-bar/webpack.config.action-bar.js
+++ projects/packages/action-bar/webpack.config.action-bar.js
@@ -41,6 +41,7 @@ module.exports = {
 			// Handle CSS.
 			jetpackWebpackConfig.CssRule( {
 				extensions: [ 'css', 'sass', 'scss' ],
+				extraLoaders: [ 'sass-loader' ],
 			} ),
 
 			// Handle images.
