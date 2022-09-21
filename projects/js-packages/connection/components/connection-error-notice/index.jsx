bfc71856cf1a16840fffd991a189ab22a5614299
Chris Jean <gaarai@gaarai.com>
2022-09-21 16:22:33 -0500

    Add: Readding ConnectionErrorNotice modifications
    An earlier commit accidentally removed the key changes to the
    ConnectionErrorNotice React component. This commit readds the changes.

--- projects/js-packages/connection/components/connection-error-notice/index.jsx
+++ projects/js-packages/connection/components/connection-error-notice/index.jsx
@@ -1,4 +1,3 @@
-import { Button } from '@automattic/jetpack-components';
 import { Notice } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { Icon, warning } from '@wordpress/icons';
@@ -15,22 +14,13 @@ import styles from './styles.module.scss';
 const ConnectionErrorNotice = props => {
 	const { message } = props;
 
-	if ( Object.values( connectionErrors ).length ) {
-		const errors = Object.values( connectionErrors ).shift();
-
-		if ( errors.length && errors[ 0 ].error_message ) {
-			return (
-				<Notice status={ 'error' } isDismissible={ false } className={ styles.notice }>
-					<Icon icon={ warning } />
-					<div className={ styles.message }>{ errors[ 0 ].error_message }</div>
-					<Button variant="link" className={ styles.link } href="#">
-						{ __( 'Restore Connection', 'jetpack' ) }
-					</Button>
-				</Notice>
-			);
-		}
-	}
-	return null;
+	return (
+		<Notice status={ 'error' } isDismissible={ false } className={ styles.notice }>
+			<Icon icon={ warning } />
+			<div className={ styles.message }>{ message }</div>
+			<a className={ styles.link }>{ __( 'Restore Connection', 'jetpack' ) }</a>
+		</Notice>
+	);
 };
 
 ConnectionErrorNotice.propTypes = {
