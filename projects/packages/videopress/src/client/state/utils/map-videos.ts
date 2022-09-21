c09f18b6a0ccf1aa7acd1e0cae8c4a0267c37c73
retrofox <rdsuarez@gmail.com>
2022-09-20 21:40:07 -0300

    [not verified] add helper to map and adjust wp/v2/media response body

--- projects/packages/videopress/src/client/state/utils/map-videos.ts
+++ projects/packages/videopress/src/client/state/utils/map-videos.ts
@@ -1,3 +1,10 @@
+/**
+ * External dependencies
+ */
+import { gmdateI18n } from '@wordpress/date';
+/**
+ * Internal dependencies
+ */
 import { OriginalVideoPressVideo, VideoPressVideo } from '../types';
 
 export const mapVideo = ( video: OriginalVideoPressVideo ): VideoPressVideo => {
@@ -11,4 +18,53 @@ export const mapVideo = ( video: OriginalVideoPressVideo ): VideoPressVideo => {
 
 export const mapVideos = ( videos: OriginalVideoPressVideo[] ): VideoPressVideo[] => {
 	return videos.map( mapVideo );
+};
+
+export const mapVideoFromWPV2MediaEndpoint = (
+	video: OriginalVideoPressVideo
+): VideoPressVideo => {
+	const { media_details: mediaDetails, id, caption } = video;
+
+	const { videopress: videoPressMediaDetails, width, height } = mediaDetails;
+
+	const {
+		title,
+		description,
+		original: url,
+		poster,
+		upload_date: date,
+		duration,
+		is_private: isPrivate,
+		file_url_base: fileURLBase,
+		files,
+	} = videoPressMediaDetails;
+
+	const { dvd } = files;
+
+	// Pick poster image from dvd file type.
+	const dvdImage = `${ fileURLBase.https }${ dvd.original_img }`;
+
+	return {
+		id,
+		title,
+		description,
+		caption,
+		url,
+		posterImage: poster,
+		date,
+		duration,
+		isPrivate,
+		dateFormatted: gmdateI18n( 'F j, Y', date ),
+		image: {
+			src: dvdImage,
+			width,
+			height,
+		},
+	};
+};
+
+export const mapVideosFromWPV2MediaEndpoint = (
+	videos: OriginalVideoPressVideo[]
+): VideoPressVideo => {
+	return videos.map( mapVideoFromWPV2MediaEndpoint );
 };
