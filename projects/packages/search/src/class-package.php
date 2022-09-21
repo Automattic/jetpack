36428f5b4eee22683325353938ce8a9c89198689
Dognose <dognose24@users.noreply.github.com>
2022-09-22 05:40:14 +0800

    Search: Move JetpackColophon and enforce display on the free tier (#26320)
    * Move JetpackColophon to bottom of SearchResults
    * Modify JetpackColophon link and text
    * Determine showPoweredBy by URL flag temporarily
    * Refactor SearchWrapper styles with classnames
    * Add changelog
    * Update versions
    
    Co-authored-by: oskosk <oskosk@gmail.com>

--- projects/packages/search/src/class-package.php
+++ projects/packages/search/src/class-package.php
@@ -11,7 +11,7 @@ namespace Automattic\Jetpack\Search;
  * Search package general information
  */
 class Package {
-	const VERSION = '0.22.3-alpha';
+	const VERSION = '0.23.0-alpha';
 	const SLUG    = 'search';
 
 	/**
