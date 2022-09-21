66015b573c4c9599918ae0ebcd1b30f7eee2f5bc
oskosk <oskosk@gmail.com>
2022-09-21 12:40:55 -0300

    Update versions

--- projects/packages/search/src/class-package.php
+++ projects/packages/search/src/class-package.php
@@ -11,7 +11,7 @@ namespace Automattic\Jetpack\Search;
  * Search package general information
  */
 class Package {
-	const VERSION = '0.22.2';
+	const VERSION = '0.22.3-alpha';
 	const SLUG    = 'search';
 
 	/**
