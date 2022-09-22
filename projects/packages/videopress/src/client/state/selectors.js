1843bd7b7f6702450e6b75e869e67ebea7fef63f
Damián Suárez <rdsuarez@gmail.com>
2022-09-22 07:27:58 -0300

    VideoPress: make Pagination work with true data (#26326)
    * [not verified] replace apiFetch() by native fetch() function
    
    * [not verified] reduxify pagination
    
    * [not verified] cast pagination data to Number
    
    * [not verified] Build paginatin with trully data
    
    * [not verified] reset pagination page when searching
    
    * [not verified] set the initial value of the Search input cmp
    
    * changelog

--- projects/packages/videopress/src/client/state/selectors.js
+++ projects/packages/videopress/src/client/state/selectors.js
@@ -6,6 +6,10 @@ export const getVideosQuery = state => {
 	return state?.videos?.query;
 };
 
+export const getPagination = state => {
+	return state?.videos?.pagination;
+};
+
 export const getVideo = ( state, id ) => {
 	const videos = getVideos( state );
 	const video = videos.find( ( { id: videoId } ) => videoId === id );
@@ -15,6 +19,7 @@ export const getVideo = ( state, id ) => {
 const selectors = {
 	getVideos,
 	getVideosQuery,
+	getPagination,
 	getVideo,
 };
 
