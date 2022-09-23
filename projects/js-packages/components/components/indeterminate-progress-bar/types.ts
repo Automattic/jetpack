94873998f88d5c81c272973f2417742836ce4aff
Jasper Kang <kangzjnet@gmail.com>
2022-09-23 17:03:58 +1200

    [not verified] remove unused param declaration

--- projects/js-packages/components/components/indeterminate-progress-bar/types.ts
+++ projects/js-packages/components/components/indeterminate-progress-bar/types.ts
@@ -3,14 +3,4 @@ export type IndeterminateProgressBarProps = {
 	 * Optional classname to apply to the root element.
 	 */
 	className?: string;
-
-	/**
-	 * Light color for the bar.
-	 */
-	lightColor?: string;
-
-	/**
-	 * Dark color for the bar.
-	 */
-	darkColor?: string;
 };
