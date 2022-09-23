f71b030e35f04c73f66ea73c22564a0814ccfc9e
Kevin L <40267301+a8ck3n@users.noreply.github.com>
2022-09-23 20:20:21 +0800

    Build out InfoSecondary component.
    Added props for the message string and the callback function to be called when the user clicks the link.

--- projects/packages/search/src/dashboard/components/donut-meter-container/index.jsx
+++ projects/packages/search/src/dashboard/components/donut-meter-container/index.jsx
@@ -9,6 +9,11 @@ import './style.scss';
  * @returns {React.Component} DonutMeterContainer component.
  */
 function DonutMeterContainer() {
+	// TODO: Remove local callback in favour of props.
+	const onLinkClicked = () => {
+		// eslint-disable-next-line no-console
+		console.log( 'higher level callback...' );
+	};
 	return (
 		<div className="donut-meter-container">
 			<div className="donut-meter-wrapper">
@@ -16,7 +21,7 @@ function DonutMeterContainer() {
 			</div>
 			<div className="donut-info-wrapper">
 				<InfoPrimary />
-				<InfoSecondary />
+				<InfoSecondary localizedMessage={ 'message' } linkClickedCallback={ onLinkClicked } />
 			</div>
 		</div>
 	);
@@ -33,10 +38,27 @@ const InfoPrimary = () => {
 	);
 };
 
-const InfoSecondary = () => {
+const InfoSecondary = ( { localizedMessage, linkClickedCallback } ) => {
+	// TODO: Localize linkText.
+	const linkText = 'Show details';
+	// Verify callback before usage.
+	const haveCallback = typeof linkClickedCallback === 'function';
+	// Our local callback to prevent refresh.
+	const onLinkClicked = e => {
+		e.preventDefault();
+		// TODO: Remove logging.
+		// eslint-disable-next-line no-console
+		console.log( 'link clicked...' );
+		linkClickedCallback();
+	};
 	return (
 		<p className="donut-info-secondary">
-			212/500 <a href="#">Show details</a>
+			{ localizedMessage }{ ' ' }
+			{ haveCallback && (
+				<a href="#" onClick={ onLinkClicked }>
+					{ linkText }
+				</a>
+			) }
 		</p>
 	);
 };
