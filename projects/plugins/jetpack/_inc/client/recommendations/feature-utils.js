cf55177cf38130f404cc1a61dc4b40aa4f94fe51
CodeyGuyDylan <dylan.munson@automattic.com>
2022-09-23 09:29:33 -0600

    [not verified] Update discount percentage

--- projects/plugins/jetpack/_inc/client/recommendations/feature-utils.js
+++ projects/plugins/jetpack/_inc/client/recommendations/feature-utils.js
@@ -194,7 +194,7 @@ export const getStepContent = stepSlug => {
 				question: __( 'Manage your clients’ sites with ease', 'jetpack' ),
 				// eslint-disable-next-line @wordpress/i18n-translator-comments
 				description: __(
-					'Jetpack’s world-class security features are now easier to manage for anyone with at least five WordPress websites.<br/><br/>Purchase and manage licenses, and get a 25% discount with our licensing platform.<br/><br/><ExternalLink>Learn More</ExternalLink>',
+					'Jetpack’s world-class security features are now easier to manage for anyone with at least five WordPress websites.<br/><br/>Purchase and manage licenses, and get a 60% discount with our licensing platform.<br/><br/><ExternalLink>Learn More</ExternalLink>',
 					'jetpack'
 				),
 				descriptionLink: getRedirectUrl( 'jetpack-for-agencies-assistant-recommendation' ),
