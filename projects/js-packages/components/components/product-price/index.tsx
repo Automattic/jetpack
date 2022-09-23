540808923a89e40b3600d3a7d0f0d0946dbe96ec
Dognose <dognose24@gmail.com>
2022-09-24 03:35:57 +0800

    Add children prop to support HTML for leyend

--- projects/js-packages/components/components/product-price/index.tsx
+++ projects/js-packages/components/components/product-price/index.tsx
@@ -20,6 +20,7 @@ const ProductPrice: React.FC< ProductPriceProps > = ( {
 	leyend = __( '/month, paid yearly', 'jetpack' ),
 	isNotConvenientPrice = false,
 	hidePriceFraction = false,
+	children,
 } ) => {
 	if ( ( price == null && offPrice == null ) || ! currency ) {
 		return null;
@@ -46,7 +47,8 @@ const ProductPrice: React.FC< ProductPriceProps > = ( {
 				) }
 				{ promoLabel && <Text className={ styles[ 'promo-label' ] }>{ promoLabel }</Text> }
 			</div>
-			{ <Text className={ styles.leyend }>{ leyend }</Text> }
+			{ leyend && <Text className={ styles.leyend }>{ leyend }</Text> }
+			{ children }
 		</>
 	);
 };
