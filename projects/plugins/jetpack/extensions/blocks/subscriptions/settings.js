18a6cca44a1a49fb6bd073cf4bc6ab2f8d536263
Romario <romarioraff@gmail.com>
2022-09-24 20:45:00 +0200

    Refactor to make code more readable

--- projects/plugins/jetpack/extensions/blocks/subscriptions/settings.js
+++ projects/plugins/jetpack/extensions/blocks/subscriptions/settings.js
@@ -3,12 +3,10 @@ import { __experimentalInspectorPopoverHeader as InspectorPopoverHeader } from '
 import { Button, PanelRow, Dropdown, VisuallyHidden } from '@wordpress/components';
 import { compose, useInstanceId } from '@wordpress/compose';
 import { withSelect, withDispatch } from '@wordpress/data';
+import { PluginPrePublishPanel } from '@wordpress/edit-post';
 import { PostVisibilityCheck } from '@wordpress/editor';
 import { __, sprintf } from '@wordpress/i18n';
 
-/**
- * Visibility options for Newsletter settings
- */
 const visibilityOptions = {
 	public: {
 		label: __( 'Public', 'jetpack' ),
@@ -24,110 +22,104 @@ const visibilityOptions = {
 	},
 };
 
-function SubscriptionPostSettings( { postMeta, setPostMeta } ) {
+function NewsletterSettings( { postMeta, setPostMeta } ) {
 	const visibilityKey = postMeta._newsletter_visibility ?? 'public';
 	const visibilityLabel = visibilityOptions[ visibilityKey ]?.label ?? 'Public';
 
 	return (
 		<>
 			<PanelRow>
-				<PostVisibilityCheck
-					render={ ( { canEdit } ) => (
-						<PanelRow className="edit-post-post-visibility">
-							<span>{ __( 'Audience', 'jetpack' ) }</span>
-							{ ! canEdit && <span>{ visibilityLabel }</span> }
-							{ canEdit && (
-								<Dropdown
-									position="bottom left"
-									contentClassName="edit-post-post-visibility__dialog"
-									focusOnMount
-									renderToggle={ ( { isOpen, onToggle } ) => (
-										<Button
-											isTertiary
-											onClick={ onToggle }
-											aria-expanded={ isOpen }
-											aria-label={ sprintf(
-												// translators: %s: Current newsletter post visibility.
-												__( 'Select auudience: %s', 'jetpack' ),
-												visibilityLabel
-											) }
-										>
-											{ visibilityLabel }
-										</Button>
-									) }
-									renderContent={ ( { onClose } ) => (
-										<PostVisibility
-											onClose={ onClose }
-											visibility={ visibilityKey }
-											setPostMeta={ setPostMeta }
-										/>
-									) }
-								/>
-							) }
-						</PanelRow>
-					) }
+				<NewsletterAudience
+					setPostMeta={ setPostMeta }
+					visibilityKey={ visibilityKey }
+					visibilityLabel={ visibilityLabel }
 				/>
 			</PanelRow>
+
+			<PluginPrePublishPanel title={ __( 'Newsletter settings', 'jetpack' ) }>
+				<NewsletterAudience
+					setPostMeta={ setPostMeta }
+					visibilityKey={ visibilityKey }
+					visibilityLabel={ visibilityLabel }
+				/>
+			</PluginPrePublishPanel>
 		</>
 	);
 }
 
-function PostVisibility( { visibility, setPostMeta, onClose } ) {
-	const instanceId = useInstanceId( PostVisibility );
-	const hanldeOnClick = event => {
-		setPostMeta( { _newsletter_visibility: event?.target?.value } );
-	};
+function NewsletterAudience( { visibilityLabel, visibilityKey, setPostMeta } ) {
+	const instanceId = useInstanceId( NewsletterAudience );
 
 	return (
-		<div className="editor-post-visibility">
-			<InspectorPopoverHeader
-				title={ __( 'Audience', 'jetpack' ) }
-				help={ __( 'Control how this newsletter is viewed.', 'jetpack' ) }
-				onClose={ onClose }
-			/>
-			<fieldset className="editor-post-visibility__fieldset">
-				<VisuallyHidden as="legend">{ __( 'Audience', 'jetpack' ) } </VisuallyHidden>
-				{ Object.keys( visibilityOptions ).map( key => (
-					<PostVisibilityChoice
-						instanceId={ instanceId }
-						key={ key }
-						value={ key }
-						label={ visibilityOptions[ key ].label }
-						info={ visibilityOptions[ key ].info }
-						checked={ key === visibility }
-						onChange={ hanldeOnClick }
-					/>
-				) ) }
-			</fieldset>
-		</div>
-	);
-}
-
-function PostVisibilityChoice( { instanceId, value, label, info, ...props } ) {
-	return (
-		<div className="editor-post-visibility__choice">
-			<input
-				type="radio"
-				name={ `editor-post-visibility__setting-${ instanceId }` }
-				value={ value }
-				id={ `editor-post-${ value }-${ instanceId }` }
-				aria-describedby={ `editor-post-${ value }-${ instanceId }-description` }
-				className="editor-post-visibility__radio"
-				{ ...props }
-			/>
-			<label
-				htmlFor={ `editor-post-${ value }-${ instanceId }` }
-				className="editor-post-visibility__label"
-			>
-				{ label }
-			</label>
-			<p
-				id={ `editor-post-${ value }-${ instanceId }-description` }
-				className="editor-post-visibility__info"
-			>
-				{ info }
-			</p>
-		</div>
+		<PostVisibilityCheck
+			render={ ( { canEdit } ) => (
+				<PanelRow className="edit-post-post-visibility">
+					<span>{ __( 'Audience', 'jetpack' ) }</span>
+					{ ! canEdit && <span>{ visibilityLabel }</span> }
+					{ canEdit && (
+						<Dropdown
+							position="bottom left"
+							contentClassName="edit-post-post-visibility__dialog"
+							focusOnMount
+							renderToggle={ ( { isOpen, onToggle } ) => (
+								<Button
+									isTertiary
+									onClick={ onToggle }
+									aria-expanded={ isOpen }
+									aria-label={ sprintf(
+										// translators: %s: Current newsletter post visibility.
+										__( 'Select audience: %s', 'jetpack' ),
+										visibilityLabel
+									) }
+								>
+									{ visibilityLabel }
+								</Button>
+							) }
+							renderContent={ ( { onClose } ) => (
+								<div className="editor-post-visibility">
+									<InspectorPopoverHeader
+										title={ __( 'Audience', 'jetpack' ) }
+										help={ __( 'Control how this newsletter is viewed.', 'jetpack' ) }
+										onClose={ onClose }
+									/>
+									<fieldset className="editor-post-visibility__fieldset">
+										<VisuallyHidden as="legend">{ __( 'Audience', 'jetpack' ) } </VisuallyHidden>
+										{ Object.keys( visibilityOptions ).map( key => (
+											<div className="editor-post-visibility__choice">
+												<input
+													type="radio"
+													checked={ key === visibilityKey }
+													name={ `editor-post-visibility__setting-${ instanceId }` }
+													value={ key }
+													id={ `editor-post-${ key }-${ instanceId }` }
+													aria-describedby={ `editor-post-${ key }-${ instanceId }-description` }
+													className="editor-post-visibility__radio"
+													onChange={ event =>
+														setPostMeta( { _newsletter_visibility: event?.target?.value } )
+													}
+												/>
+												<label
+													htmlFor={ `editor-post-${ key }-${ instanceId }` }
+													className="editor-post-visibility__label"
+												>
+													{ visibilityOptions[ key ].label }
+												</label>
+												<p
+													id={ `editor-post-${ key }-${ instanceId }-description` }
+													className="editor-post-visibility__info"
+												>
+													{ visibilityOptions[ key ].info }
+												</p>
+											</div>
+										) ) }
+									</fieldset>
+								</div>
+							) }
+						/>
+					) }
+				</PanelRow>
+			) }
+		/>
 	);
 }
 
@@ -140,4 +132,4 @@ export default compose( [
 			dispatch( 'core/editor' ).editPost( { meta: newMeta } );
 		},
 	} ) ),
-] )( SubscriptionPostSettings );
+] )( NewsletterSettings );
