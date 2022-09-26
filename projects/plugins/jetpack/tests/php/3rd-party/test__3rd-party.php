6159634aa83f88ac13e1c75ef3dd5906450cbf9e
Brandon Kraft <public@brandonkraft.com>
2022-09-26 14:47:53 -0500

    DRY

--- projects/plugins/jetpack/tests/php/3rd-party/test__3rd-party.php
+++ projects/plugins/jetpack/tests/php/3rd-party/test__3rd-party.php
@@ -22,12 +22,19 @@ class WP_Test_Third_Party_Support extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Test that Development Versions are suppressed on Atomic.
+	 * Helper to setup the Atomic constants as needed.
 	 */
-	public function test_atomic_returns_false_on_dev_version() {
+	public function helper__set_atomic_constants() {
 		Constants::set_constant( 'ATOMIC_CLIENT_ID', 999 );
 		Constants::set_constant( 'ATOMIC_SITE_ID', 999 );
 		Constants::set_constant( 'JETPACK__VERSION', '10.3-a.1' );
+	}
+
+	/**
+	 * Test that Development Versions are suppressed on Atomic.
+	 */
+	public function test_atomic_returns_false_on_dev_version() {
+		$this->helper__set_atomic_constants();
 		$this->assertFalse( Jetpack::is_development_version() );
 	}
 
@@ -35,9 +42,7 @@ class WP_Test_Third_Party_Support extends WP_UnitTestCase {
 	 * Test that Development Versions via the Beta plugin are still considered as Development versions.
 	 */
 	public function test_atomic_returns_true_on_beta_plugin_version() {
-		Constants::set_constant( 'ATOMIC_CLIENT_ID', 999 );
-		Constants::set_constant( 'ATOMIC_SITE_ID', 999 );
-		Constants::set_constant( 'JETPACK__VERSION', '10.3-a.1' );
+		$this->helper__set_atomic_constants();
 		Constants::set_constant( 'JETPACK__PLUGIN_DIR', '/srv/www/public/wp-content/plugins/jetpack-dev/' );
 		$this->assertTrue( Jetpack::is_development_version() );
 		Constants::clear_single_constant( 'JETPACK__PLUGIN_DIR' );
@@ -47,8 +52,7 @@ class WP_Test_Third_Party_Support extends WP_UnitTestCase {
 	 * Test that Development Versions via the Beta plugin are still considered as Development versions.
 	 */
 	public function test_atomic_returns_expected_if_not_on_atomic() {
-		Constants::set_constant( 'ATOMIC_CLIENT_ID', false );
-		Constants::set_constant( 'ATOMIC_SITE_ID', false );
+		Constants::set_constant( 'JETPACK__VERSION', '10.3-a.1' );
 		$this->assertTrue( Jetpack::is_development_version() );
 
 		Constants::set_constant( 'JETPACK__VERSION', '10.3.0' );
@@ -59,8 +63,7 @@ class WP_Test_Third_Party_Support extends WP_UnitTestCase {
 	 * Test that the WAF is not available on Atomic.
 	 */
 	public function test_atomic_no_waf() {
-			Constants::set_constant( 'ATOMIC_CLIENT_ID', 999 );
-			Constants::set_constant( 'ATOMIC_SITE_ID', 999 );
+			$this->helper__set_atomic_constants();
 			$this->assertNotContains( 'waf', ( new Modules() )->get_available() );
 	}
 }
