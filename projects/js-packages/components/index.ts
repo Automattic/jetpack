7c04d95c24cdf56d07a90eacffbfca89f224c2ed
oskosk <oskosk@gmail.com>
2022-09-26 13:41:04 -0300

    .

--- projects/js-packages/components/index.ts
+++ projects/js-packages/components/index.ts
@@ -49,7 +49,6 @@ export { default as ProductOffer, IconsCard } from './components/product-offer';
 export { default as Dialog } from './components/dialog';
 export { default as DonutMeter } from './components/donut-meter';
 export { default as RecordMeterBar } from './components/record-meter-bar';
-export { default as DonutMeter } from './components/record-meter-donut';
 export { default as ContextualUpgradeTrigger } from './components/contextual-upgrade-trigger';
 export { default as Alert } from './components/alert';
 export { getUserLocale, cleanLocale } from './lib/locale';
