f0f5d3b868755b48524ccdaa38c9a3d7c45e817e
Brad Jorsch <brad.jorsch@automattic.com>
2022-09-26 09:48:39 -0400

    Fix autoloader test

--- projects/packages/autoloader/tests/php/lib/class-test-plugin-factory.php
+++ projects/packages/autoloader/tests/php/lib/class-test-plugin-factory.php
@@ -452,6 +452,7 @@ class Test_Plugin_Factory {
 
 		// We can finally execute Composer now that we're ready.
 		putenv( 'COMPOSER_HOME=' . TEST_TEMP_BIN_DIR ); // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.runtime_configuration_putenv
+		putenv( 'COMPOSER_AUTH' ); // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.runtime_configuration_putenv
 		exec( 'php ' . escapeshellarg( $composer_bin ) . ' install -q -d ' . escapeshellarg( $plugin_dir ) );
 		if ( ! is_file( $plugin_dir . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php' ) ) {
 			throw new \RuntimeException( 'Unable to execute the `' . $composer_bin . '` archive for tests.' );
