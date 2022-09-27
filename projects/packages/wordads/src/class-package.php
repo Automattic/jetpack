238fb4a21baf27fe450293bb75e927e763fa5617
Jason Moon <jason.moon@automattic.com>
2022-09-27 12:32:26 -0700

    Bump versions

--- projects/packages/wordads/src/class-package.php
+++ projects/packages/wordads/src/class-package.php
@@ -11,7 +11,7 @@ namespace Automattic\Jetpack\WordAds;
  * WordAds package general information
  */
 class Package {
-	const VERSION = '0.2.16';
+	const VERSION = '0.2.17-alpha';
 	const SLUG    = 'wordads';
 
 	/**
