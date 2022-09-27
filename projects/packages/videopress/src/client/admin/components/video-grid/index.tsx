397e6251fd814de8388238bc1f0d57cf9164ec81
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2022-09-26 23:06:23 -0300

    VideoPress: Return id at VideoGrid

--- projects/packages/videopress/src/client/admin/components/video-grid/index.tsx
+++ projects/packages/videopress/src/client/admin/components/video-grid/index.tsx
@@ -30,6 +30,7 @@ const VideoGrid = ( { videos, count = 6, onVideoDetailsClick }: VideoGridProps )
 					return (
 						<Col key={ index } sm={ 4 } md={ 4 } lg={ 4 }>
 							<VideoCard
+								id={ video?.id }
 								title={ video.title }
 								thumbnail={ video?.uploading ? <div>Uploading</div> : video?.posterImage } // TODO: we should use thumbnail when the API is ready https://github.com/Automattic/jetpack/issues/26319
 								duration={ video.duration }
