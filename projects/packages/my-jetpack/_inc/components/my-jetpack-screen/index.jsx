ebc5ee12b372eb8e245999385284c0028e96e05b
Sergey Mitroshin <sergeymitr@gmail.com>
2022-09-28 17:32:40 -0400

    Bump package versions.

--- projects/packages/my-jetpack/_inc/components/my-jetpack-screen/index.jsx
+++ projects/packages/my-jetpack/_inc/components/my-jetpack-screen/index.jsx
@@ -6,7 +6,7 @@ import {
 	Col,
 	Text,
 } from '@automattic/jetpack-components';
-import { ConnectionErrorNotice } from '@automattic/jetpack-connection';
+import { useConnectionErrorNotice, ConnectionError } from '@automattic/jetpack-connection';
 import { Notice } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { Icon, warning, info } from '@wordpress/icons';
@@ -46,6 +46,7 @@ const GlobalNotice = ( { message, options, clean } ) => {
 export default function MyJetpackScreen() {
 	useConnectionWatcher();
 	const { message, options, clean } = useGlobalNotice();
+	const { hasConnectionError } = useConnectionErrorNotice();
 
 	const { recordEvent } = useAnalytics();
 
@@ -74,9 +75,11 @@ export default function MyJetpackScreen() {
 							{ __( 'Manage your Jetpack products', 'jetpack-my-jetpack' ) }
 						</Text>
 					</Col>
-					<Col>
-						<ConnectionErrorNotice />
-					</Col>
+					{ hasConnectionError && (
+						<Col>
+							<ConnectionError />
+						</Col>
+					) }
 					{ message && (
 						<Col>
 							<GlobalNotice message={ message } options={ options } clean={ clean } />
