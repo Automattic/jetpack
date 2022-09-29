3b7a952ea46658f8de710827374f88b467c1cfbf
Mark George <thingalon@gmail.com>
2022-09-29 19:55:19 +1000

    Clean up lint whinges

--- projects/plugins/super-cache/tests/e2e/lib/system-tools.ts
+++ projects/plugins/super-cache/tests/e2e/lib/system-tools.ts
@@ -1,14 +1,14 @@
-import child_process from 'node:child_process';
+import childProcess from 'node:child_process';
 import util from 'util';
 import shellEscape from 'shell-escape';
 
-const execPromise = util.promisify( child_process.exec );
+const execPromise = util.promisify( childProcess.exec );
 
 /**
  * Execute a command in the local shell. Returns its stdout.
  *
  * @param {...string} command - Command to run. Each string will be escaped.
- * @returns {string} stdout contents.
+ * @return {string} stdout contents.
  */
 export function exec( ...command: string[] ) {
 	return execPromise( shellEscape( command ) );
--- projects/plugins/super-cache/tests/e2e/lib/test-tools.ts
+++ projects/plugins/super-cache/tests/e2e/lib/test-tools.ts
@@ -5,8 +5,8 @@ import { getSiteUrl } from './plugin-tools';
 /**
  * Loads the specified page and returns the response body. Expects a 200 response so tests fail otherwise.
  *
- * @param {string} path - The path within the test site to load.
- * @param {Object} params - GET parameters to add to the URL.
+ * @param {string} path   The path within the test site to load.
+ * @param {Object} params GET parameters to add to the URL.
  */
 export async function loadPage( path = '/', params = {} ): Promise< string > {
 	const response = await axios.get( getSiteUrl( path, params ) );
