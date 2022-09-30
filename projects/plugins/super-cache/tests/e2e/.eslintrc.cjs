8338a57eca205bdb2441f4a2ecae2023c9e5e77b
Mark George <thingalon@gmail.com>
2022-09-30 12:30:30 +1000

    fix linting errors

--- projects/plugins/super-cache/tests/e2e/.eslintrc.cjs
+++ projects/plugins/super-cache/tests/e2e/.eslintrc.cjs
@@ -1,15 +1,18 @@
 module.exports = {
 	root: true,
-	extends: [ require.resolve( 'jetpack-js-tools/eslintrc/wp-eslint-plugin/recommended' ) ],
-	plugins: [ 'jest', '@typescript-eslint' ],
-	parser: '@typescript-eslint/parser',
+	extends: [
+		require.resolve( 'jetpack-js-tools/eslintrc/base' ),
+		require.resolve( 'jetpack-js-tools/eslintrc/wp-eslint-plugin/recommended' ),
+	],
 	parserOptions: {
 		sourceType: 'module',
 		tsconfigRootDir: __dirname,
+		project: [ './tsconfig.json' ],
 	},
 	rules: {
 		'comma-dangle': 0,
 		'prettier/prettier': 0,
 		'@typescript-eslint/no-unused-vars': 0,
+		'import/no-unresolved': 0,
 	},
 };
