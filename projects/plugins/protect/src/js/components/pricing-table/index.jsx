ab0c96eff55cd1519cec440bde20e20f00ecd07d
dkmyta <dkmyta10@gmail.com>
2022-10-01 13:54:31 -0700

    Add PricingTable component

---
+++ projects/plugins/protect/src/js/components/pricing-table/index.jsx
@@ -0,0 +1,151 @@
+/**
+ * External dependencies
+ */
+import {
+	Button,
+	ProductPrice,
+	PricingTable,
+	PricingTableColumn,
+	PricingTableHeader,
+	PricingTableItem,
+} from '@automattic/jetpack-components';
+import { useConnection } from '@automattic/jetpack-connection';
+import { __ } from '@wordpress/i18n';
+import React from 'react';
+import useAnalyticsTracks from '../../hooks/use-analytics-tracks';
+import useProtectData from '../../hooks/use-protect-data';
+
+/**
+ * Product Detail component.
+ *
+ * @param {object} props                     - Component props
+ * @param {Function} props.onSecurityAdd     - Callback when adding paid protect product successfully
+ * @param {Function} props.securityJustAdded - Callback when adding paid protect product was recently added
+ * @returns {object}                ConnectedPricingTable react component.
+ */
+const ConnectedPricingTable = ( { onSecurityAdd, securityJustAdded } ) => {
+	const { siteIsRegistering, handleRegisterSite, registrationError } = useConnection( {
+		skipUserConnection: true,
+	} );
+
+	// Access paid protect product data
+	const { securityBundle } = useProtectData();
+	const { pricingForUi } = securityBundle;
+
+	// Compute the price per month.
+	const price = pricingForUi.fullPrice
+		? Math.ceil( ( pricingForUi.fullPrice / 12 ) * 100 ) / 100
+		: null;
+	const offPrice = pricingForUi.discountPrice
+		? Math.ceil( ( pricingForUi.discountPrice / 12 ) * 100 ) / 100
+		: null;
+	const { currencyCode: currency = 'USD' } = pricingForUi;
+
+	// Track free and paid click events
+	const { recordEventHandler } = useAnalyticsTracks();
+	const getSecurityBundle = recordEventHandler(
+		'jetpack_protect_pricing_table_get_security_link_click',
+		onSecurityAdd
+	);
+	const getProtectFree = recordEventHandler(
+		'jetpack_protect_connected_product_activated',
+		handleRegisterSite
+	);
+
+	const args = {
+		title: __( 'Stay one step ahead of threats', 'jetpack-protect' ),
+		items: [
+			{
+				name: __( 'Scan for threats and vulnerabilities', 'jetpack-protect' ),
+				tooltipInfo: __( 'Default info for Feature A', 'jetpack-protect' ),
+			},
+			{
+				name: __( 'Daily automated scans', 'jetpack-protect' ),
+				tooltipInfo: __( 'Default info for Feature B', 'jetpack-protect' ),
+			},
+			{
+				name: __( 'Access to scan on Cloud', 'jetpack-protect' ),
+				tooltipInfo: __( 'Default info for Feature C', 'jetpack-protect' ),
+			},
+			{
+				name: __( 'One-click auto fixes', 'jetpack-protect' ),
+				tooltipInfo: __( 'Default info for Feature D', 'jetpack-protect' ),
+			},
+			{
+				name: __( 'Notifications', 'jetpack-protect' ),
+				tooltipInfo: __( 'Default info for Feature E', 'jetpack-protect' ),
+			},
+			{
+				name: __( 'Severity labels', 'jetpack-protect' ),
+				tooltipInfo: __( 'Default info for Feature F', 'jetpack-protect' ),
+			},
+		],
+	};
+
+	return (
+		<>
+			<PricingTable { ...args }>
+				<PricingTableColumn primary>
+					<PricingTableHeader>
+						<ProductPrice
+							price={ price }
+							offPrice={ offPrice }
+							leyend={ __( '/month, billed yearly', 'jetpack-protect' ) }
+							currency={ currency }
+							hideDiscountLabel={ false }
+						/>
+						<Button fullWidth onClick={ getSecurityBundle } isLoading={ securityJustAdded }>
+							{ __( 'Get Jetpack Protect', 'jetpack-protect' ) }
+						</Button>
+					</PricingTableHeader>
+					<PricingTableItem
+						isIncluded={ true }
+						label={ <strong>{ __( 'Line by line malware scanning', 'jetpack-protect' ) }</strong> }
+					/>
+					<PricingTableItem
+						isIncluded={ true }
+						label={ <strong>{ __( 'Plus on-demand manual scans', 'jetpack-protect' ) }</strong> }
+					/>
+					<PricingTableItem isIncluded={ true } />
+					<PricingTableItem isIncluded={ true } />
+					<PricingTableItem isIncluded={ true } />
+					<PricingTableItem isIncluded={ true } />
+				</PricingTableColumn>
+				<PricingTableColumn>
+					<PricingTableHeader>
+						<ProductPrice
+							price={ 0 }
+							leyend={ __( 'Free forever', 'jetpack-protect' ) }
+							currency={ currency }
+							hidePriceFraction
+						/>
+						<Button
+							fullWidth
+							variant="secondary"
+							onClick={ getProtectFree }
+							isLoading={ siteIsRegistering }
+							error={
+								registrationError
+									? __( 'An error occurred. Please try again.', 'jetpack-protect' )
+									: null
+							}
+						>
+							{ __( 'Start for free', 'jetpack-protect' ) }
+						</Button>
+					</PricingTableHeader>
+					<PricingTableItem
+						isIncluded={ true }
+						label={ __( 'Check items against database', 'jetpack-protect' ) }
+					/>
+					<PricingTableItem isIncluded={ true } />
+					<PricingTableItem isIncluded={ false } />
+					<PricingTableItem isIncluded={ false } />
+					<PricingTableItem isIncluded={ false } />
+					<PricingTableItem isIncluded={ false } />
+				</PricingTableColumn>
+			</PricingTable>
+		</>
+	);
+};
+
+export default ConnectedPricingTable;
