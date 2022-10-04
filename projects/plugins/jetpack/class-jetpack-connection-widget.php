da59501e97e50e45e1f77ab0ecd59c4a4edb488c
Heyde Moura <moura.heyde@gmail.com>
2022-10-04 15:12:34 -0300

    Jetpack: Add bare template for connection widget

--- projects/plugins/jetpack/class-jetpack-connection-widget.php
+++ projects/plugins/jetpack/class-jetpack-connection-widget.php
@@ -28,15 +28,17 @@ class Jetpack_Connection_Widget {
 	 * Sets up the Jetpack Connection Widget in the WordPress admin dashboard.
 	 */
 	public static function wp_dashboard_setup() {
-		$widget_title = sprintf(
-			__( 'Jetpack Connection Widget', 'jetpack' )
-		);
+		if ( Jetpack::is_connection_ready() ) {
+			$widget_title = sprintf(
+				__( 'Jetpack - Security, Backup, Speed & Growth', 'jetpack' )
+			);
 
-		wp_add_dashboard_widget(
-			'jetpack_connection_widget',
-			$widget_title,
-			array( __CLASS__, 'connection_widget' )
-		);
+			wp_add_dashboard_widget(
+				'jetpack_connection_widget',
+				$widget_title,
+				array( __CLASS__, 'connection_widget' )
+			);
+		}
 	}
 
 	/**
@@ -44,8 +46,15 @@ class Jetpack_Connection_Widget {
 	 */
 	public static function connection_widget() {
 		?>
-			<div class="jp-connection-banner">
-				<p><?php esc_html_e( 'Placeholder connection widget', 'jetpack' ); ?></p>
+			<div class="jp-connection-widget">
+				<img
+					width="100%"
+					src="<?php echo esc_url( plugins_url( 'images/dashboard-connection-widget-hero.png', JETPACK__PLUGIN_FILE ) ); ?>" />
+				<h3><?php esc_html_e( 'Finish setting up your site', 'jetpack' ); ?></h3>
+				<p>
+					<?php esc_html_e( 'You’re missing out on great Jetpack features bundled by your host, sign up to set up.', 'jetpack' ); ?>
+				</p>
+				<a><?php esc_html_e( 'Set up Jetpack for free', 'jetpack' ); ?></a>
 			</div>
 		<?php
 	}
