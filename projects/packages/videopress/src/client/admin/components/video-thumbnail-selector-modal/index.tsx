8677af8b869825bc2adf39257fcf96b72d43a270
Douglas <douglas.henri@automattic.com>
2022-10-04 12:00:25 -0300

    extract VideoThumbnailSelectorModal component

---
+++ projects/packages/videopress/src/client/admin/components/video-thumbnail-selector-modal/index.tsx
@@ -0,0 +1,58 @@
+/**
+ * External dependencies
+ */
+import { Button, ThemeProvider } from '@automattic/jetpack-components';
+import { Modal } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { useState } from 'react';
+import { ReactNode } from 'react';
+/**
+ * Internal dependencies
+ */
+import VideoFrameSelector from '../../../components/video-frame-selector';
+import styles from './style.module.scss';
+import { VideoThumbnailSelectorModalProps } from './types';
+
+/**
+ * Video Thumbnail Selector component
+ *
+ * @param {VideoThumbnailSelectorModalProps} props - Component props.
+ * @returns {ReactNode} - VideoThumbnailSelector react component.
+ */
+const VideoThumbnailSelectorModal = ( {
+	url,
+	selectedTime,
+	handleCloseSelectFrame,
+	handleVideoFrameSelected,
+	handleConfirmFrame,
+}: VideoThumbnailSelectorModalProps ) => {
+	const [ modalRef, setModalRef ] = useState< HTMLDivElement | null >( null );
+
+	return (
+		<Modal
+			title={ __( 'Select thumbnail from video', 'jetpack-videopress-pkg' ) }
+			onRequestClose={ handleCloseSelectFrame }
+			isDismissible={ false }
+		>
+			<ThemeProvider targetDom={ modalRef }>
+				<div ref={ setModalRef } className={ styles.selector }>
+					<VideoFrameSelector
+						src={ url }
+						onVideoFrameSelected={ handleVideoFrameSelected }
+						initialCurrentTime={ selectedTime }
+					/>
+					<div className={ styles.actions }>
+						<Button variant="secondary" onClick={ handleCloseSelectFrame }>
+							{ __( 'Close', 'jetpack-videopress-pkg' ) }
+						</Button>
+						<Button variant="primary" onClick={ handleConfirmFrame }>
+							{ __( 'Select this frame', 'jetpack-videopress-pkg' ) }
+						</Button>
+					</div>
+				</div>
+			</ThemeProvider>
+		</Modal>
+	);
+};
+
+export default VideoThumbnailSelectorModal;
