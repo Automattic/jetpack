54e119d54afb431704e7c743e1979969d166fdf3
Kirah Sapong <kirah.sapong@automattic.com>
2022-10-05 12:31:50 -0400

    [not verified] Client: Update Section Header component to Typescript file
    Why: To reap the benefits of TypeScript (such as added safety and automation) for this file.
    Also, a good way to practice creating a PR for Jetpack.

---
+++ projects/plugins/jetpack/_inc/client/components/section-header/index.tsx
@@ -0,0 +1,26 @@
+import classNames from 'classnames';
+import Card from 'components/card';
+import React from 'react';
+
+import './style.scss';
+
+interface SectionHeaderProps {
+	label?: string;
+	children?: React.ReactNode;
+	className?: string;
+	cardBadge?: string | React.ReactNode;
+}
+
+const SectionHeader = ( { label, children, className }: SectionHeaderProps ) => {
+	const classes = classNames( className, 'dops-section-header' );
+
+	return (
+		<Card compact className={ classes }>
+			<div className="dops-section-header__label">
+				<span className="dops-section-header__label-text">{ label }</span>
+			</div>
+			<div className="dops-section-header__actions">{ children }</div>
+		</Card>
+	);
+};
+export default SectionHeader;
