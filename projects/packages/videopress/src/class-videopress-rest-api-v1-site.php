639fb87fb2a353e1c29df12c5bff211d8c42635d
Luiz Kowalski <lhkowalski@gmail.com>
2022-10-05 20:03:59 -0300

    Refactor the rest endpoint class to use the new Site class
    Moved all the logic related to requesting site data to the new Site class, so the VideoPress_Rest_Api_V1_Site can simply use it to fetch the data and return it as a response.

--- projects/packages/videopress/src/class-videopress-rest-api-v1-site.php
+++ projects/packages/videopress/src/class-videopress-rest-api-v1-site.php
@@ -7,10 +7,6 @@
 
 namespace Automattic\Jetpack\VideoPress;
 
-use Automattic\Jetpack\Connection\Client;
-use WP_Error;
-use WP_REST_Response;
-
 /**
  * VideoPress rest api class for fetching site information
  */
@@ -56,25 +52,7 @@ class VideoPress_Rest_Api_V1_Site {
 	 * @return WP_Rest_Response The response object.
 	 */
 	public static function get_site_info() {
-		$error = new WP_Error(
-			'videopress_site_error',
-			__( 'Could not fetch site information from the service', 'jetpack-videopress-pkg' )
-		);
-
-		$blog_id      = VideoPressToken::blog_id();
-		$request_path = sprintf( 'sites/%d?force=wpcom', $blog_id );
-		$response     = Client::wpcom_json_api_request_as_blog( $request_path, '1.1', array(), null, 'rest' );
-
-		if ( is_wp_error( $response ) ) {
-			return $error;
-		}
-
-		$response_code = wp_remote_retrieve_response_code( $response );
-		if ( 200 !== $response_code ) {
-			return $error;
-		}
-
-		$body = wp_remote_retrieve_body( $response );
-		return rest_ensure_response( json_decode( $body, true ) );
+		$data = Site::get_site_info();
+		return rest_ensure_response( $data );
 	}
 }
