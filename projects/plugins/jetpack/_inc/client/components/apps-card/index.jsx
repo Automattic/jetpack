b5215d6b25dc6e6af60ac0806fb37c3c6e465439
Siobhan <siobhan@automattic.com>
2022-10-06 14:31:24 +0100

    Add 'getRedirectUrl' function to link's URL
    This function is used throughout the Jetpack plugin for links, as a best practice way to ensure backwards compatibility. See here for more: https://github.com/Automattic/jetpack/tree/trunk/projects/js-packages/components/tools/jp-redirect

--- projects/plugins/jetpack/_inc/client/components/apps-card/index.jsx
+++ projects/plugins/jetpack/_inc/client/components/apps-card/index.jsx
@@ -1,4 +1,5 @@
 import { imagePath } from 'constants/urls';
+import { getRedirectUrl } from '@automattic/jetpack-components';
 import { createInterpolateElement } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import classNames from 'classnames';
@@ -71,7 +72,12 @@ class AppsCard extends React.Component {
 						'jetpack'
 					),
 					{
-						a: <a className="jp-apps-card__link" href="https://jetpack.com/app" />,
+						a: (
+							<a
+								className="jp-apps-card__link"
+								href={ getRedirectUrl( 'jetpack-plugin-dashboard-apps-card' ) }
+							/>
+						),
 					}
 				) }
 			</p>
