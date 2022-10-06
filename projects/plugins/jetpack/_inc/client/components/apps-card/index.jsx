ebb67685852418fb171dac12795f99a504a577d0
Siobhan <siobhan@automattic.com>
2022-10-06 16:04:35 +0100

    Remove unused functions now that Redux isn't used
    As we're no longer using states to determine whether AppsCard displays, it's no longer necessary to explore any functions along with the component. We can therefore remove the 'connect' and 'withRouter' functions.

--- projects/plugins/jetpack/_inc/client/components/apps-card/index.jsx
+++ projects/plugins/jetpack/_inc/client/components/apps-card/index.jsx
@@ -8,8 +8,6 @@ import analytics from 'lib/analytics';
 import detectMobileDevice from 'lib/device-detector';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 
 class AppsCard extends React.Component {
 	static displayName = 'AppsCard';
@@ -109,4 +107,4 @@ AppsCard.propTypes = {
 	className: PropTypes.string,
 };
 
-export default connect( withRouter( AppsCard ) );
+export default AppsCard;
