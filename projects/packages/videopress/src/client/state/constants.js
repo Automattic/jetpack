b1fdbc34e47aeb470949d9bca7285a693c925414
Luiz Kowalski <lhkowalski@gmail.com>
2022-10-07 05:45:27 -0300

    VideoPress: Use new site info endpoint on client (#26677)
    * Add constant to reference the new VideoPress site info endpoint
    
    * Move site data request from the Jetpack API to the new VideoPress one
    
    * Round the number of bytes to the nearest integer value
    
    * Add changelog file

--- projects/packages/videopress/src/client/state/constants.js
+++ projects/packages/videopress/src/client/state/constants.js
@@ -10,6 +10,7 @@ export const WP_ADMIN_AJAX_API_URL = '/wp-admin/admin-ajax.php';
 export const WP_REST_API_MEDIA_ENDPOINT = 'wp/v2/media';
 export const WP_REST_API_VIDEOPRESS_META_ENDPOINT = 'wpcom/v2/videopress/meta';
 export const REST_API_SITE_PURCHASES_ENDPOINT = 'my-jetpack/v1/site/purchases';
+export const REST_API_SITE_INFO_ENDPOINT = 'videopress/v1/site';
 
 /*
  * Actions
