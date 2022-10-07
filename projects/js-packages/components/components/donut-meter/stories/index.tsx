9ca3adf2cf63e21bdf6066589073f4b9de92b679
Jason Moon <jason.moon@automattic.com>
2022-10-06 17:16:04 -0700

    Improve donut meter stories

--- projects/js-packages/components/components/donut-meter/stories/index.tsx
+++ projects/js-packages/components/components/donut-meter/stories/index.tsx
@@ -1,22 +1,97 @@
-import DonutMeter, { DonutMeterProps } from '../index';
+import DonutMeter from '../index';
 
 export default {
 	title: 'JS Packages/Components/DonutMeter',
 	component: DonutMeter,
+	parameters: {
+		layout: 'centered',
+	},
 };
 
-const Template = args => <DonutMeter { ...args } />;
+export const Default = () => (
+	<DonutMeter
+		segmentCount={ 16 }
+		totalCount={ 100 }
+		thickness="3.5"
+		donutWidth="64px"
+		title="Meter title goes here"
+		description="Meter description goes here"
+	/>
+);
 
-const DefaultArgs: DonutMeterProps = {
-	segmentCount: 18,
-	totalCount: 100,
-	backgroundColor: '#00BA37',
-	thickness: '3.5',
-	donutWidth: '64px',
-	title: 'Meter title goes here',
-	description: 'Meter description goes here',
-};
+export const Warning = () => (
+	<DonutMeter
+		segmentCount={ 16 }
+		totalCount={ 100 }
+		thickness="3.5"
+		donutWidth="64px"
+		title="Meter title goes here"
+		description="Meter description goes here"
+		type="warning"
+	/>
+);
 
-// Export Default story
-export const _default = Template.bind( {} );
-_default.args = DefaultArgs;
+export const Danger = () => (
+	<DonutMeter
+		segmentCount={ 16 }
+		totalCount={ 100 }
+		thickness="3.5"
+		donutWidth="64px"
+		title="Meter title goes here"
+		description="Meter description goes here"
+		type="danger"
+	/>
+);
+
+export const AdaptiveColors = () => (
+	<div>
+		<div style={ { display: 'flex', flexFlow: 'row' } }>
+			<DonutMeter
+				segmentCount={ 20 }
+				totalCount={ 100 }
+				thickness="3.5"
+				donutWidth="64px"
+				title="Meter title goes here"
+				description="Meter description goes here"
+				useAdaptiveColors
+			/>
+			<DonutMeter
+				segmentCount={ 40 }
+				totalCount={ 100 }
+				thickness="3.5"
+				donutWidth="64px"
+				title="Meter title goes here"
+				description="Meter description goes here"
+				useAdaptiveColors
+			/>
+			<DonutMeter
+				segmentCount={ 60 }
+				totalCount={ 100 }
+				thickness="3.5"
+				donutWidth="64px"
+				title="Meter title goes here"
+				description="Meter description goes here"
+				useAdaptiveColors
+			/>
+			<DonutMeter
+				segmentCount={ 80 }
+				totalCount={ 100 }
+				thickness="3.5"
+				donutWidth="64px"
+				title="Meter title goes here"
+				description="Meter description goes here"
+				useAdaptiveColors
+			/>
+			<DonutMeter
+				segmentCount={ 100 }
+				totalCount={ 100 }
+				thickness="3.5"
+				donutWidth="64px"
+				title="Meter title goes here"
+				description="Meter description goes here"
+				useAdaptiveColors
+			/>
+		</div>
+		<p>Color changing according to the fullness of the meter.</p>
+	</div>
+);
