bc1d2a49bc8173fc25eca8c755f76cfb5496d618
Adnan Haque <adnan007.id@gmail.com>
2022-10-08 03:13:04 +0600

    Fix critical-css-advanced path

--- projects/plugins/boost/app/assets/src/js/Main.svelte
+++ projects/plugins/boost/app/assets/src/js/Main.svelte
@@ -1,8 +1,13 @@
 <script>
 	import BenefitsInterstitial from './pages/benefits/BenefitsInterstitial.svelte';
+	import Connection from './pages/connection/Connection.svelte';
 	import GettingStarted from './pages/getting-started/GettingStarted.svelte';
-	import Home from './pages/home/Home.svelte';
 	import PurchaseSuccess from './pages/purchase-success/PurchaseSuccess.svelte';
+	import Settings from './pages/settings/Settings.svelte';
+	import Footer from './sections/Footer.svelte';
+	import Header from './sections/Header.svelte';
+	import config from './stores/config';
+	import { connection } from './stores/connection';
 	import { Router, Route } from './utils/router';
 	import routerHistory from './utils/router-history';
 </script>
@@ -11,5 +16,19 @@
 	<Route path="upgrade" component={BenefitsInterstitial} />
 	<Route path="purchase-successful" component={PurchaseSuccess} />
 	<Route path="getting-started" component={GettingStarted} />
-	<Route path="/" component={Home} />
+	<Route path="/*" let:location let:navigate>
+		<div id="jb-settings" class="jb-settings jb-settings--main">
+			<div class="jb-container">
+				<Header />
+			</div>
+
+			{#if $connection.connected || ! $config.site.online}
+				<Settings {location} {navigate} />
+			{:else}
+				<Connection />
+			{/if}
+
+			<Footer />
+		</div>
+	</Route>
 </Router>
