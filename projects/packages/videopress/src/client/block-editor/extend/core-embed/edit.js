c8b36c9935c425cf06afd301ec617ec2a51b9ae2
retrofox <rdsuarez@gmail.com>
2022-10-10 16:56:03 -0300

    add warning to core/oembed videopress block variation

--- projects/packages/videopress/src/client/block-editor/extend/core-embed/edit.js
+++ projects/packages/videopress/src/client/block-editor/extend/core-embed/edit.js
@@ -1,7 +1,14 @@
 /**
  * External dependencies
  */
+import { Warning } from '@wordpress/block-editor';
+import { Button } from '@wordpress/components';
 import { createHigherOrderComponent } from '@wordpress/compose';
+import { __ } from '@wordpress/i18n';
+/**
+ * Internal dependencies
+ */
+import './editor.scss';
 
 const withCoreEmbedVideoPressBlock = createHigherOrderComponent( CoreEmbedBlockEdit => {
 	return props => {
@@ -15,9 +22,29 @@ const withCoreEmbedVideoPressBlock = createHigherOrderComponent( CoreEmbedBlockE
 		}
 
 		return (
-			<>
-				<CoreEmbedBlockEdit { ...props } />
-			</>
+			<div>
+				<Warning
+					className="videopress-embed-warning"
+					actions={ [
+						<Button key="convert" variant="primary">
+							{ __( 'Use the new VideoPress Video block', 'jetpack-videopress-pkg' ) }
+						</Button>,
+						<Button key="convert" variant="tertiary">
+							{ __( 'Keep using the VideoPress Embed block variation', 'jetpack-videopress-pkg' ) }
+						</Button>,
+					] }
+				>
+					{ __(
+						'Your site currently supports the VideoPress Video block.',
+						'jetpack-videopress-pkg'
+					) }
+				</Warning>
+
+				<div className="wp-block-core-embed-wrapper is-disabled">
+					<div className="core-embed-videopress-player__overlay" />
+					<CoreEmbedBlockEdit { ...props } />
+				</div>
+			</div>
 		);
 	};
 }, 'withCoreEmbedVideoPressBlock' );
