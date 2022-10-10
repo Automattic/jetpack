7e1782908cd6bd89068ee4134d3aabec980ec5e9
retrofox <rdsuarez@gmail.com>
2022-10-10 17:44:21 -0300

    [not verified] convert core/embed to v6 by clicking on the button

--- projects/packages/videopress/src/client/block-editor/extend/core-embed/edit.js
+++ projects/packages/videopress/src/client/block-editor/extend/core-embed/edit.js
@@ -1,27 +1,58 @@
 /**
  * External dependencies
  */
-import { Warning } from '@wordpress/block-editor';
+import { Warning, store as blockEditorStore } from '@wordpress/block-editor';
+import { createBlock } from '@wordpress/blocks';
 import { Button } from '@wordpress/components';
 import { createHigherOrderComponent } from '@wordpress/compose';
+import { useDispatch } from '@wordpress/data';
+import { useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
 import './editor.scss';
+import { pickGUIDFromUrl } from '../../blocks/video/utils/url';
 
 const withCoreEmbedVideoPressBlock = createHigherOrderComponent( CoreEmbedBlockEdit => {
 	return props => {
+		const { clientId } = props;
+		const { attributes, setAttributes } = props;
+		const { url, keepUsingOEmbedVariation } = attributes;
+		const { replaceBlock } = useDispatch( blockEditorStore );
+
+		const guid = pickGUIDFromUrl( url );
+
+		useEffect( () => {
+			if ( keepUsingOEmbedVariation !== false ) {
+				return;
+			}
+
+			replaceBlock(
+				clientId,
+				createBlock( 'videopress/video', {
+					...attributes,
+					guid,
+				} )
+			);
+		}, [ keepUsingOEmbedVariation, guid ] );
+
+		// Check if the block is a core/embed block.
 		if ( props.name !== 'core/embed' ) {
 			return <CoreEmbedBlockEdit { ...props } />;
 		}
 
-		const { attributes } = props;
+		// ...and if it's a `videopress` variation.
 		if ( ! attributes?.providerNameSlug || attributes.providerNameSlug !== 'videopress' ) {
 			return <CoreEmbedBlockEdit { ...props } />;
 		}
 
-		const { keepUsingOEmbedVariation } = attributes;
+		// ... and if was possible to pick the GUID from the URL.
+		if ( ! guid ) {
+			return <CoreEmbedBlockEdit { ...props } />;
+		}
+
+		// ... and if the user has already decided to keep using the oEmbed variation.
 		if ( keepUsingOEmbedVariation === true ) {
 			return <CoreEmbedBlockEdit { ...props } />;
 		}
@@ -31,10 +62,18 @@ const withCoreEmbedVideoPressBlock = createHigherOrderComponent( CoreEmbedBlockE
 				<Warning
 					className="videopress-embed-warning"
 					actions={ [
-						<Button key="convert" variant="primary">
+						<Button
+							key="convert"
+							variant="primary"
+							onClick={ () => setAttributes( { keepUsingOEmbedVariation: false } ) }
+						>
 							{ __( 'Use the new VideoPress Video block', 'jetpack-videopress-pkg' ) }
 						</Button>,
-						<Button key="convert" variant="tertiary">
+						<Button
+							key="convert"
+							variant="tertiary"
+							onClick={ () => setAttributes( { keepUsingOEmbedVariation: true } ) }
+						>
 							{ __( 'Keep using the VideoPress Embed block variation', 'jetpack-videopress-pkg' ) }
 						</Button>,
 					] }
