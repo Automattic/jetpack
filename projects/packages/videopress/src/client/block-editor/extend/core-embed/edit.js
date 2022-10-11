bff00853c05c5f90b39ef809bb467cdf0f5bb966
retrofox <rdsuarez@gmail.com>
2022-10-11 16:10:03 -0300

    rollback testing change

--- projects/packages/videopress/src/client/block-editor/extend/core-embed/edit.js
+++ projects/packages/videopress/src/client/block-editor/extend/core-embed/edit.js
@@ -25,7 +25,7 @@ const withCoreEmbedVideoPressBlock = createHigherOrderComponent( CoreEmbedBlockE
 		const guid = pickGUIDFromUrl( url );
 
 		useEffect( () => {
-			if ( ! guid ) {
+			if ( keepUsingOEmbedVariation !== false ) {
 				return;
 			}
 
