f0375b529be2233610fed6a7217acabbaa99a531
dognose <dognose24@gmail.com>
2022-10-13 00:16:22 +0800

    Add IconTooltipProps annotations

--- projects/js-packages/components/components/icon-tooltip/types.ts
+++ projects/js-packages/components/components/icon-tooltip/types.ts
@@ -9,16 +9,63 @@ export type Position =
 	| 'bottom right';
 
 export type IconTooltipProps = {
+	/**
+	 * The wrapper class name of this IconTooltip component.
+	 */
 	className?: string;
+
+	/**
+	 * The class name applied to Gridicon.
+	 */
 	iconClassName?: string;
+
+	/**
+	 * The deprecated position of Popover.
+	 */
 	position?: Position;
+
+	/**
+	 * The placement of Popover.
+	 */
 	placement?: Placement;
+
+	/**
+	 * The animation for Popover appears.
+	 */
 	animate?: boolean;
+
+	/**
+	 * The icon code for Gridicon.
+	 */
 	iconCode?: string;
+
+	/**
+	 * The title of Popover.
+	 */
 	title?: string;
+
+	/**
+	 * The main body content of Popover.
+	 */
 	children?: React.ReactNode;
+
+	/**
+	 * The icon square width and height size (in px) also calculate the Popover shift.
+	 */
 	iconSize?: number;
+
+	/**
+	 * The distance (in px) between the anchor and the Popover.
+	 */
 	offset?: number;
+
+	/**
+	 * Keep the Popover placement aligned with its parent wrapper rather than the icon.
+	 */
 	shadowAnchor?: boolean;
+
+	/**
+	 * Force the Popover to show without event triggering.
+	 */
 	forceShow?: boolean;
 };
