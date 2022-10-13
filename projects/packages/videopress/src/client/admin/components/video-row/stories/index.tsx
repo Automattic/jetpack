ebdee6e8ce45ad07f0b4f9edd8e2cee1ad2323fd
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2022-10-13 19:09:22 -0300

    VideoPress: Loading mode for `VideoRow`  (#26834)
    * VideoPress: Add disabled style for checkbox
    
    * VideoPress: Support blankIconSize in VideoThumbnail
    
    * VideoPress: Avoid rest props and use correct types in VideoList
    
    * VideoPress: Add ConnectedVideoRow and support loading mode with thumbnail
    
    * VideoPress: Use correct color at Checkbox disabled mode
    
    * VideoPress: Implement loading in Stats at VideoRow
    
    * VideoPress: Pass loading mode down to VideoRow
    
    * VideoPress: Update logic to not bug when loading
    
    * Changelog
    
    * VideoPress: Improve loading for small screens
    
    * VideoPress: Do not expand while loading on small screen

--- projects/packages/videopress/src/client/admin/components/video-row/stories/index.tsx
+++ projects/packages/videopress/src/client/admin/components/video-row/stories/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import VideoRow from '..';
+import { VideoRow } from '..';
+import { postersArray, randomPoster } from '../../../mock';
 import styles from '../style.module.scss';
 import type { ComponentStory, ComponentMeta } from '@storybook/react';
 
@@ -16,9 +17,43 @@ export default {
 			</div>
 		),
 	],
+	argTypes: {
+		id: {
+			table: {
+				disable: true,
+			},
+		},
+		onSelect: {
+			table: {
+				disable: true,
+			},
+		},
+		onVideoDetailsClick: {
+			table: {
+				disable: true,
+			},
+		},
+		className: {
+			table: {
+				disable: true,
+			},
+		},
+		checked: {
+			table: {
+				disable: true,
+			},
+		},
+		thumbnail: {
+			control: { type: 'select', options: [ ...postersArray, 'none' ] },
+		},
+	},
 } as ComponentMeta< typeof VideoRow >;
 
 const Template: ComponentStory< typeof VideoRow > = args => {
+	if ( args.thumbnail === 'none' ) {
+		args.thumbnail = null;
+	}
+
 	const [ checked, setChecked ] = useState( false );
 	const onSelect = current => setChecked( current );
 	return <VideoRow { ...args } checked={ checked } onSelect={ onSelect } />;
@@ -26,13 +61,14 @@ const Template: ComponentStory< typeof VideoRow > = args => {
 
 export const _default = Template.bind( {} );
 _default.args = {
-	posterImage:
-		'https://videos.files.wordpress.com/PnQvSqdF/videopress-upload-demo-7_mp4_hd_1080p.original.jpg',
+	id: 1,
+	thumbnail: randomPoster(),
 	title: 'videopress-upload-demo-7-mp4',
 	duration: 158633,
 	plays: 200,
 	uploadDate: '2022-08-15T21:16:59+0000',
 	isPrivate: true,
-	hideEditButton: false,
-	hideQuickActions: false,
+	showEditButton: true,
+	showQuickActions: true,
+	loading: false,
 };
