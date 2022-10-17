8411011516fd7a0dd65f9996f83b40c713da0318
Jason Moon <jason.moon@automattic.com>
2022-10-17 16:21:44 -0700

    Fix selector errors

--- projects/packages/search/src/dashboard/store/selectors/search-pricing.js
+++ projects/packages/search/src/dashboard/store/selectors/search-pricing.js
@@ -1,20 +1,24 @@
+const getPriceBefore = state => state.searchPricing.full_price ?? 0;
+const getPriceAfter = state =>
+	// If discounted price is unavailable, use the full price.
+	Number.isFinite( state.searchPricing.discount_price )
+		? state.searchPricing.discount_price
+		: getPriceBefore( state );
+const getPricingDiscountPercentage = state => {
+	const before = getPriceBefore( state );
+	const after = getPriceAfter( state );
+	// Original price is less than or greater than the sale price. No discount!
+	if ( before <= after ) {
+		return 0;
+	}
+	return Math.round( ( ( before - after ) / before ) * 100 );
+};
+
 const searchPricingSelectors = {
 	getSearchPricing: state => state.searchPricing,
-	getPriceBefore: state => state.searchPricing.full_price ?? 0,
-	getPriceAfter: state =>
-		// If discounted price is unavailable, use the full price.
-		Number.isFinite( state.searchPricing.discount_price )
-			? state.searchPricing.discount_price
-			: this.getPriceBefore( state ),
-	getPricingDiscountPercentage: state => {
-		const before = this.getPriceBefore( state );
-		const after = this.getPriceAfter( state );
-		// Original price is less than or greater than the sale price. No discount!
-		if ( before <= after ) {
-			return 0;
-		}
-		return Math.round( ( ( before - after ) / before ) * 100 );
-	},
+	getPriceBefore,
+	getPriceAfter,
+	getPricingDiscountPercentage,
 	getPriceCurrencyCode: state => state.searchPricing.currency_code ?? 'USD',
 	isNewPricing202208: state => state.searchPricing.pricing_version >= '202208',
 	getPaidRequestsLimit: state => state.searchPricing.monthly_search_request_limit ?? 10000,
