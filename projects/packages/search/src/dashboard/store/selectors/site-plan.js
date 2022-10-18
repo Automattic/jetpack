ef8a70a40d0c80f65d15c7f8b1b612bd3ce604b3
Jasper Kang <kangzjnet@gmail.com>
2022-10-18 14:05:27 +1300

    revert change

--- projects/packages/search/src/dashboard/store/selectors/site-plan.js
+++ projects/packages/search/src/dashboard/store/selectors/site-plan.js
@@ -1,3 +1,5 @@
+const productSlugFree = 'jetpack_search_free';
+
 const sitePlanSelectors = {
 	getSearchPlanInfo: state => state.sitePlan,
 	hasBusinessPlan: state => state.sitePlan.supports_only_classic_search,
@@ -8,7 +10,7 @@ const sitePlanSelectors = {
 	supportsSearch: state =>
 		state.sitePlan.supports_instant_search || state.sitePlan.supports_only_classic_search,
 	getTierMaximumRecords: state => state.sitePlan.tier_maximum_records,
-	isFreePlan: state => !! state.isFreePlan,
+	isFreePlan: state => state.sitePlan.effective_subscription?.product_slug === productSlugFree,
 	getLatestMonthRequests: state => state.sitePlan.plan_usage.num_requests_3m[ 0 ],
 	getCurrentPlan: state => state.sitePlan.plan_current,
 	getCurrentUsage: state => state.sitePlan.plan_usage,
