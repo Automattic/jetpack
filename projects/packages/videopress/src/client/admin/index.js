711c4dec1b218b1746dfb0851e330fc57768cabe
Douglas <douglas.henri@automattic.com>
2022-10-18 15:25:33 -0300

    use useUploadUnloadCheck hook on main component

--- projects/packages/videopress/src/client/admin/index.js
+++ projects/packages/videopress/src/client/admin/index.js
@@ -1,25 +1,36 @@
+/**
+ * External dependencies
+ */
 import { ThemeProvider } from '@automattic/jetpack-components';
 import ReactDOM from 'react-dom';
 import { HashRouter, Routes, Route } from 'react-router-dom';
+/**
+ * Internal dependencies
+ */
 import { initStore } from '../state';
 import AdminPage from './components/admin-page';
 import EditVideoDetails from './components/edit-video-details';
+import useUploadUnloadCheck from './hooks/use-upload-unload-check';
 import './style.module.scss';
 
 initStore();
 
-const VideoPress = () => (
-	<ThemeProvider>
-		<HashRouter>
-			<Routes>
-				<Route path="/" element={ <AdminPage /> } />
-				<Route path="video">
-					<Route path=":videoId/edit" element={ <EditVideoDetails /> } />
-				</Route>
-			</Routes>
-		</HashRouter>
-	</ThemeProvider>
-);
+const VideoPress = () => {
+	useUploadUnloadCheck();
+
+	return (
+		<ThemeProvider>
+			<HashRouter>
+				<Routes>
+					<Route path="/" element={ <AdminPage /> } />
+					<Route path="video">
+						<Route path=":videoId/edit" element={ <EditVideoDetails /> } />
+					</Route>
+				</Routes>
+			</HashRouter>
+		</ThemeProvider>
+	);
+};
 
 /**
  * Initial render function.
