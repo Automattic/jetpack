dc81dfe448db50f4b4e47e79bd2a02443bc2de01
Nate Weller <nate.weller@automattic.com>
2022-10-18 10:35:43 -0600

    Move useCredentialState() hook into the AdminPage so it doesn't run when disconnected

--- projects/plugins/protect/src/js/components/admin-page/index.jsx
+++ projects/plugins/protect/src/js/components/admin-page/index.jsx
@@ -91,9 +91,21 @@ const InterstitialPage = ( { run, hasCheckoutStarted } ) => {
 	);
 };
 
+const useCredentialState = () => {
+	const { checkCredentialsState } = useDispatch( STORE_ID );
+	const credentialState = useSelect( select => select( STORE_ID ).getCredentialState() );
+
+	useEffect( () => {
+		if ( ! credentialState.state ) {
+			checkCredentialsState();
+		}
+	}, [ checkCredentialsState, credentialState.state ] );
+};
+
 const ProtectAdminPage = () => {
 	const { lastChecked, currentStatus, errorCode, errorMessage } = useProtectData();
 	const { hasConnectionError } = useConnectionErrorNotice();
+	useCredentialState();
 
 	let currentScanStatus;
 	if ( 'error' === currentStatus ) {
@@ -234,17 +246,6 @@ const ProtectAdminPage = () => {
 	);
 };
 
-const useCredentialState = () => {
-	const { checkCredentialsState } = useDispatch( STORE_ID );
-	const credentialState = useSelect( select => select( STORE_ID ).getCredentialState() );
-
-	useEffect( () => {
-		if ( ! credentialState.state ) {
-			checkCredentialsState();
-		}
-	}, [ checkCredentialsState, credentialState.state ] );
-};
-
 const useRegistrationWatcher = () => {
 	const { isRegistered } = useConnection();
 	const { refreshStatus } = useDispatch( STORE_ID );
@@ -297,7 +298,6 @@ const useStatusPolling = () => {
 
 const Admin = () => {
 	useRegistrationWatcher();
-	useCredentialState();
 	useStatusPolling();
 	const { adminUrl } = window.jetpackProtectInitialState || {};
 	const { run, isRegistered, hasCheckoutStarted } = useProductCheckoutWorkflow( {
