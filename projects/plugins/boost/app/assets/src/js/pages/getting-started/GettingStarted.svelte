23cedb36437a443519c6ffd547e1bf509ceb3d4e
Adnan Haque <adnan007.id@gmail.com>
2022-10-18 12:01:57 +0600

    [not verified] Switch getting started page to use react component

--- projects/plugins/boost/app/assets/src/js/pages/getting-started/GettingStarted.svelte
+++ projects/plugins/boost/app/assets/src/js/pages/getting-started/GettingStarted.svelte
@@ -1,40 +1,13 @@
 <script>
-	import { getRedirectUrl } from '@automattic/jetpack-components';
-	import { __ } from '@wordpress/i18n';
-	import ContextInfo from '../../elements/ContextInfo.svelte';
-	import ProductPrice from '../../elements/ProductPrice.svelte';
-	import TemplatedString from '../../elements/TemplatedString.svelte';
+	import ReactComponent from '../../elements/ReactComponent.svelte';
+	import { BoostPricingTable } from '../../react-components/BoostPricingTable.tsx';
 	import Header from '../../sections/Header.svelte';
 	import config, { markGetStartedComplete } from '../../stores/config';
 	import { updateModuleState } from '../../stores/modules';
 	import { recordBoostEvent } from '../../utils/analytics';
-	import externalLinkTemplateVar from '../../utils/external-link-template-var';
 	import { getUpgradeURL } from '../../utils/upgrade';
 
 	$: pricing = $config.pricing;
-
-	const cssOptimizationContext = __(
-		'Move important styling information to the start of the page, which helps pages display your content sooner, so your users don’t have to wait for the entire page to load. Commonly referred to as Critical CSS.',
-		'jetpack-boost'
-	);
-
-	const deferJSContext = __(
-		'Run non-essential JavaScript after the page has loaded so that styles and images can load more quickly. Read more on <link>web.dev</link>.',
-		'jetpack-boost'
-	);
-
-	const lazyLoadingContext = __(
-		'Improve page loading speed by only loading images when they are required. Read more on <link>web.dev</link>.',
-		'jetpack-boost'
-	);
-
-	const supportContext = __(
-		`<p>Paid customers get dedicated email support from our world-class Happiness Engineers to help with any issue.</p><p>All other questions are handled by our team as quickly as we are able to go through the WordPress support forum.</p>`,
-		'jetpack-boost'
-	);
-
-	const deferJsLink = getRedirectUrl( 'jetpack-boost-defer-js' );
-	const lazyLoadlink = getRedirectUrl( 'jetpack-boost-lazy-load' );
 
 	// svelte-ignore unused-export-let - Ignored values supplied by svelte-navigator.
 	export let navigate, location;
@@ -59,178 +32,14 @@
 		<Header />
 	</div>
 
-	<div class="jb-section jb-section--alt jb-section--scores">
+	<div class="jb-section jb-section--alt">
 		<div class="jb-container">
-			<div class="comparison-table" style="--rows: 11; --columns: 3">
-				<div class="comparison-table__main">
-					<h1>{__( 'The easiest speed optimization plugin for WordPress', 'jetpack-boost' )}</h1>
-
-					<p class="visible-md comparison-table__feature-row">
-						Optimize CSS Loading
-						<ContextInfo placement="left">{cssOptimizationContext}</ContextInfo>
-					</p>
-					<hr class="visible-md" />
-					<p class="visible-md comparison-table__feature-row">
-						Defer non-essential JavaScript
-						<ContextInfo placement="left">
-							<TemplatedString
-								template={deferJSContext}
-								vars={{
-									...externalLinkTemplateVar( deferJsLink ),
-								}}
-							/>
-						</ContextInfo>
-					</p>
-					<hr class="visible-md" />
-					<p class="visible-md comparison-table__feature-row">
-						Lazy image loading
-						<ContextInfo placement="left">
-							<TemplatedString
-								template={lazyLoadingContext}
-								vars={{
-									...externalLinkTemplateVar( lazyLoadlink ),
-								}}
-							/>
-						</ContextInfo>
-					</p>
-					<hr class="visible-md" />
-					<p class="visible-md comparison-table__feature-row">
-						Dedicated email support
-						<ContextInfo placement="left">
-							{@html supportContext}
-						</ContextInfo>
-					</p>
-					<hr class="visible-md" />
-
-					<footer class="comparison-table__spacing" />
-				</div>
-				<div class="comparison-table__tier comparison-table__tier--card">
-					<header>
-						<ProductPrice
-							currency={pricing.yearly.currencyCode}
-							price={pricing.yearly.priceBefore / 12}
-							offPrice={pricing.yearly.priceAfter / 12}
-							priceDetails={__( '/month, paid yearly', 'jetpack-boost' )}
-							showNotOffPrice={true}
-						/>
-					</header>
-					<div class="comparison-table__cta">
-						<button
-							type="button"
-							class="components-button jb-button jb-button--primary"
-							on:click={choosePaidPlan}
-						>
-							Get Boost
-						</button>
-					</div>
-					<p class="comparison-table__feature-row">
-						<span class="jb-icon jb-icon--check" />
-						<strong class="visible-md">Automatically kept up to date</strong>
-						<strong class="comparison-table__feature-elaborate hidden-md"
-							>CSS optimization automatically kept up to date</strong
-						>
-						<span class="hidden-md"
-							><ContextInfo placement="left">{cssOptimizationContext}</ContextInfo></span
-						>
-					</p>
-					<hr />
-					<p class="comparison-table__feature-row">
-						<span class="jb-icon jb-icon--check" />
-						<span class="visible-md">Included</span>
-						<span class="comparison-table__feature-elaborate hidden-md"
-							>Defer non-essential JavaScript</span
-						>
-						<span class="hidden-md">
-							<ContextInfo placement="left">
-								<TemplatedString
-									template={deferJSContext}
-									vars={{
-										...externalLinkTemplateVar( deferJsLink ),
-									}}
-								/>
-							</ContextInfo>
-						</span>
-					</p>
-					<hr />
-					<p class="comparison-table__feature-row">
-						<span class="jb-icon jb-icon--check" />
-						<span class="visible-md">Included</span>
-						<span class="comparison-table__feature-elaborate hidden-md">Lazy image loading</span>
-						<span class="hidden-md">
-							<ContextInfo placement="left">
-								<TemplatedString
-									template={lazyLoadingContext}
-									vars={{
-										...externalLinkTemplateVar( lazyLoadlink ),
-									}}
-								/>
-							</ContextInfo>
-						</span>
-					</p>
-					<hr />
-					<p class="comparison-table__feature-row">
-						<span class="jb-icon jb-icon--check" />
-						<span class="visible-md">Included</span>
-						<span class="comparison-table__feature-elaborate hidden-md"
-							>Dedicated email support</span
-						>
-						<span class="hidden-md">
-							<ContextInfo placement="left">
-								{@html supportContext}
-							</ContextInfo>
-						</span>
-					</p>
-					<hr />
-
-					<footer class="comparison-table__spacing" />
-				</div>
-				<div class="comparison-table__tier">
-					<header>
-						<ProductPrice
-							price={0}
-							currency={pricing.yearly.currencyCode}
-							hidePriceFraction={true}
-							showNotOffPrice={false}
-							leyend=""
-						/>
-					</header>
-					<div class="comparison-table__cta">
-						<button
-							type="button"
-							class="components-button jb-button jb-button--outline"
-							on:click={chooseFreePlan}
-						>
-							Start for free
-						</button>
-					</div>
-					<p class="comparison-table__feature-row">
-						<span class="jb-icon jb-icon--check" />
-						<span class="visible-md">Manual generation</span>
-						<strong class="hidden-md">Optimize CSS loading manually</strong>
-					</p>
-					<hr />
-					<p class="comparison-table__feature-row">
-						<span class="jb-icon jb-icon--check" />
-						<span class="visible-md">Included</span>
-						<span class="hidden-md">Defer non-essential JavaScript</span>
-					</p>
-					<hr />
-					<p class="comparison-table__feature-row">
-						<span class="jb-icon jb-icon--check" />
-						<span class="visible-md">Included</span>
-						<span class="hidden-md">Lazy image loading</span>
-					</p>
-					<hr />
-					<p class="comparison-table__feature-row">
-						<span class="jb-icon jb-icon--cross" />
-						<span class="visible-md">Not Included</span>
-						<span class="hidden-md">Dedicated email support</span>
-					</p>
-					<hr />
-
-					<footer class="comparison-table__spacing" />
-				</div>
-			</div>
+			<ReactComponent
+				this={BoostPricingTable}
+				{pricing}
+				onPremiumCTA={choosePaidPlan}
+				onFreeCTA={chooseFreePlan}
+			/>
 		</div>
 	</div>
 </div>
