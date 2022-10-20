0060817b6264cc87d69af200e80d2c7aa1a27976
dkmyta <dkmyta10@gmail.com>
2022-10-20 14:41:59 -0700

    Fix regression in wording

--- projects/plugins/protect/src/js/components/credentials-needed-modal/index.jsx
+++ projects/plugins/protect/src/js/components/credentials-needed-modal/index.jsx
@@ -59,7 +59,7 @@ const CredentialsNeededModal = () => {
 
 			<Text mb={ 3 }>
 				{ __(
-					'Once you’ve entered server credentials, Jetpack Protect will be fixing the three selected threats.',
+					'Once you’ve entered server credentials, Jetpack Protect will be fixing the selected threats.',
 					'jetpack-protect'
 				) }
 			</Text>
