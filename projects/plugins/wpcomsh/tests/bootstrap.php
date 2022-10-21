c71be495792bffc75774493a6682561453d8b43a
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2022-10-21 05:01:43 -0700

    Tests: Manually load plugin to avoid local build + symlink business https://github.com/Automattic/wpcomsh/pull/1114

--- projects/plugins/wpcomsh/tests/bootstrap.php
+++ projects/plugins/wpcomsh/tests/bootstrap.php
@@ -33,5 +33,16 @@ foreach ( new RegexIterator( $lib, '/^.*\.php$/', RegexIterator::GET_MATCH ) as 
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
+	if ( file_exists( WPMU_PLUGIN_DIR . '/wpcomsh-loader.php' ) ) {
+		return;
+	}
+	require_once dirname( dirname( __FILE__ ) ) . '/wpcomsh.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
 // Start up the WP testing environment.
-require $_tests_dir . '/includes/bootstrap.php';
+require_once $_tests_dir . '/includes/bootstrap.php';
