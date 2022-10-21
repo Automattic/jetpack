48a5a14981b6c5daadc3a47638b0985b7359ee2c
Luiz Kowalski <lhkowalski@gmail.com>
2022-10-21 10:41:05 -0300

    VideoPress: Expose playback token generation endpoint (#26974)
    * Register route to generate VideoPress playback tokens
    
    This route will be consumed mostly when the client needs to show a video or a thumbnail for a private video. In this case, if we do not provide a playback token, the request fails as a 403 response.
    
    * Add changelog file
    
    Co-authored-by: Damián Suárez <rdsuarez@gmail.com>

--- projects/packages/videopress/src/class-wpcom-rest-api-v2-endpoint-videopress.php
+++ projects/packages/videopress/src/class-wpcom-rest-api-v2-endpoint-videopress.php
@@ -159,6 +159,19 @@ class WPCOM_REST_API_V2_Endpoint_VideoPress extends WP_REST_Controller {
 				},
 			)
 		);
+
+		// Playback Token Route
+		register_rest_route(
+			$this->namespace,
+			$this->rest_base . '/playback-jwt/(?P<video_guid>\w+)',
+			array(
+				'methods'             => \WP_REST_Server::EDITABLE,
+				'callback'            => array( $this, 'videopress_playback_jwt' ),
+				'permission_callback' => function () {
+					return current_user_can( 'read' );
+				},
+			)
+		);
 	}
 
 	/**
@@ -291,6 +304,54 @@ class WPCOM_REST_API_V2_Endpoint_VideoPress extends WP_REST_Controller {
 				'upload_token'   => $token,
 				'upload_url'     => videopress_make_resumable_upload_path( $blog_id ),
 				'upload_blog_id' => $blog_id,
+			);
+		} catch ( \Exception $e ) {
+			$status = 500;
+			$data   = array(
+				'error' => $e->getMessage(),
+			);
+
+		}
+
+		return rest_ensure_response(
+			new WP_REST_Response( $data, $status )
+		);
+	}
+
+	/**
+	 * Endpoint for generating a VideoPress Playback JWT
+	 *
+	 * @param WP_REST_Request $request the request object.
+	 * @return WP_Rest_Response - The response object.
+	 */
+	public static function videopress_playback_jwt( $request ) {
+		$has_connected_owner = Data::has_connected_owner();
+		if ( ! $has_connected_owner ) {
+			return rest_ensure_response(
+				new WP_Error(
+					'owner_not_connected',
+					'User not connected.',
+					array(
+						'code'        => 503,
+						'connect_url' => Admin_UI::get_admin_page_url(),
+					)
+				)
+			);
+		}
+
+		$blog_id = Data::get_blog_id();
+		if ( ! $blog_id ) {
+			return rest_ensure_response(
+				new WP_Error( 'site_not_registered', 'Site not registered.', 503 )
+			);
+		}
+
+		try {
+			$video_guid = $request->get_param( 'video_guid' );
+			$token      = VideoPressToken::videopress_playback_jwt( $video_guid );
+			$status     = 200;
+			$data       = array(
+				'playback_token' => $token,
 			);
 		} catch ( \Exception $e ) {
 			$status = 500;
