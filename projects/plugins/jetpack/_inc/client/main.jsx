302d48b6c9d50d440006d03632620271330e4b76
Robert Sreberski <robertsreberski@gmail.com>
2022-10-24 08:13:43 -0500

    Add post-purchase onboardings to Recommendation flows (#26484)
    Co-authored-by: Karen Attfield <karenlattfield@gmail.com>

--- projects/plugins/jetpack/_inc/client/main.jsx
+++ projects/plugins/jetpack/_inc/client/main.jsx
@@ -502,6 +502,19 @@ class Main extends React.Component {
 			case '/recommendations/backup-plan':
 			case '/recommendations/boost':
 			case '/recommendations/summary':
+			case '/recommendations/welcome-backup':
+			case '/recommendations/welcome-complete':
+			case '/recommendations/welcome-security':
+			case '/recommendations/welcome-antispam':
+			case '/recommendations/welcome-videopress':
+			case '/recommendations/welcome-search':
+			case '/recommendations/welcome-scan':
+			case '/recommendations/backup-activated':
+			case '/recommendations/scan-activated':
+			case '/recommendations/antispam-activated':
+			case '/recommendations/videopress-activated':
+			case '/recommendations/search-activated':
+			case '/recommendations/server-credentials':
 				if ( this.props.showRecommendations ) {
 					pageComponent = <Recommendations />;
 				} else {
