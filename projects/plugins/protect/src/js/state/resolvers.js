e89dd55eb319d86da58441a67ffcd250ecf6f4f5
Nate Weller <nate.weller@automattic.com>
2022-10-25 14:02:58 -0600

    Re-add resolvers

--- projects/plugins/protect/src/js/state/resolvers.js
+++ projects/plugins/protect/src/js/state/resolvers.js
@@ -1,3 +1,21 @@
-const resolvers = {};
+import apiFetch from '@wordpress/api-fetch';
+import actions from './actions';
+
+const resolvers = {
+	getJetpackScan: {
+		isFulfilled: state => {
+			return Object.keys( state?.jetpackScan ).length > 0;
+		},
+
+		fulfill: () => async ( { dispatch } ) => {
+			const response = await apiFetch( {
+				path: '/my-jetpack/v1/site/products/scan',
+				method: 'GET',
+			} );
+
+			dispatch( actions.setJetpackScan( response ) );
+		},
+	},
+};
 
 export default resolvers;
