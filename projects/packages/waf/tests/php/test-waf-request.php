893f6e711d305bbb5d7a4f9721afd841dfb74a1e
bhoop <briley.hooper@automattic.com>
2022-10-26 10:29:17 -0400

    [not verified] waf rules: allow regex in targets and add tests

--- projects/packages/waf/tests/php/test-waf-request.php
+++ projects/packages/waf/tests/php/test-waf-request.php
@@ -11,7 +11,6 @@ use Automattic\Jetpack\Waf\Waf_Request;
  * Request test suite.
  */
 class WafRequestTest extends PHPUnit\Framework\TestCase {
-
 	/**
 	 * Test for null on CLI.
 	 */
@@ -113,5 +112,236 @@ class WafRequestTest extends PHPUnit\Framework\TestCase {
 		$request->set_trusted_proxies( array( '10.0.0.23' ) );
 
 		$this->assertSame( '192.168.2.2', $request->get_real_user_ip_address(), 'Trusted headers were not ignored from untrusted proxy.' );
+	}
+
+	/**
+	 * Test Waf_Request::normalize_header_name()
+	 *
+	 * @testWith [ "CONTENTTYPE",    "contenttype" ]
+	 *           [ "T E  S T",       "t-e--s-t" ]
+	 *           [ "T_E__S___T",     "t-e--s---t" ]
+	 *           [ "My_Header Name", "my-header-name" ]
+	 * @param string $input     The un-normalized header name.
+	 * @param string $expected  The expected normalized header name.
+	 */
+	public function testNormalizeHeaderName( $input, $expected ) {
+		$request = new Waf_Request();
+		$this->assertSame( $expected, $request->normalize_header_name( $input ) );
+	}
+
+	/**
+	 * Test Waf_Request::get_headers()
+	 */
+	public function testGetHeaders() {
+		// ensure entries from $_SERVER are interpreted correctly
+		$_SERVER['notaheader']      = 'wrong';
+		$_SERVER['http_headername'] = 'test';
+		$_SERVER['CONTENT_TYPE']    = 'mocked/content-type';
+		$_SERVER['CONTENT_LENGTH']  = '1234567890';
+		$request                    = new Waf_Request();
+		$headers                    = $request->get_headers();
+		$this->assertNotContainsEquals( array( 'notaheader', 'wrong' ), $headers );
+		$this->assertContainsEquals( array( 'headername', 'test' ), $headers );
+		$this->assertContainsEquals( array( 'content-type', 'mocked/content-type' ), $headers );
+		$this->assertContainsEquals( array( 'content-length', '1234567890' ), $headers );
+		// ensure defaults for Content-Type and Content-Length are used if not found
+		unset( $_SERVER['CONTENT_TYPE'] );
+		unset( $_SERVER['CONTENT_LENGTH'] );
+		$request = new Waf_Request();
+		$headers = $request->get_headers();
+		$this->assertContainsEquals( array( 'content-type', 'application/octet-stream' ), $headers );
+		$this->assertContainsEquals( array( 'content-length', '0' ), $headers );
+	}
+
+	/**
+	 * Test Waf_Request::get_method()
+	 */
+	public function testGetMethod() {
+		$_SERVER['REQUEST_METHOD'] = 'OPTIONS';
+		$request                   = new Waf_Request();
+		$this->assertSame( 'OPTIONS', $request->get_method() );
+	}
+
+	/**
+	 * Test Waf_Request::get_protocol()
+	 */
+	public function testGetProtocol() {
+		// test default
+		$_SERVER['SERVER_PROTOCOL'] = 'TEST';
+		$request                    = new Waf_Request();
+		$this->assertSame( 'TEST', $request->get_protocol() );
+	}
+
+	/**
+	 * Test Waf_Request::get_uri()
+	 */
+	public function testGetUri() {
+		// test with a "full" URI in REQUEST_URI
+		$_SERVER['REQUEST_URI'] = 'https://wordpress.com/index.php';
+		$request                = new Waf_Request();
+		$this->assertSame( 'https://wordpress.com/index.php', $request->get_uri( true ) );
+		// test with a "relative" URI in REQUEST_URI
+		$_SERVER['REQUEST_URI'] = '/index.php';
+		$_SERVER['HTTPS']       = 'on';
+		$_SERVER['HTTP_HOST']   = 'wordpress.com';
+		$request                = new Waf_Request();
+		$this->assertSame( 'https://wordpress.com/index.php', $request->get_uri( true ) );
+		// test with a query string
+		$_SERVER['QUERY_STRING'] = 'red=1&orange=2';
+		$_SERVER['REQUEST_URI']  = 'https://wordpress.com/index.php?incorrect=bad';
+		$request                 = new Waf_Request();
+		$this->assertSame( '/index.php?red=1&orange=2', $request->get_uri( false ) );
+	}
+
+	/**
+	 * Test Waf_Request::get_filename()
+	 */
+	public function testGetFilename() {
+		$_SERVER['REQUEST_URI'] = 'https://wordpress.com/some/file?test';
+		$request                = new Waf_Request();
+		$this->assertSame( '/some/file', $request->get_filename() );
+		// test with a root path request
+		$_SERVER['REQUEST_URI'] = 'https://wordpress.com/';
+		$request                = new Waf_Request();
+		$this->assertSame( '/', $request->get_filename() );
+		// test with a relative root path request
+		$_SERVER['REQUEST_URI'] = '/';
+		$request                = new Waf_Request();
+		$this->assertSame( '/', $request->get_filename() );
+	}
+
+	/**
+	 * Test Waf_Request::get_remote_addr()
+	 */
+	public function testGetRemoteAddr() {
+		// reset the state
+		unset( $_SERVER['HTTP_CLIENT_IP'] );
+		unset( $_SERVER['HTTP_X_FORWARDED_FOR'] );
+		// test the REMOTE_ADDR fallback
+		$_SERVER['REMOTE_ADDR'] = 'test_remote_addr';
+		$request                = new Waf_Request();
+		$this->assertSame( 'test_remote_addr', $request->get_remote_addr() );
+		// HTTP_X_FORWARDED_FOR takes priority over REMOTE_ADDR
+		$_SERVER['HTTP_X_FORWARDED_FOR'] = 'test_x';
+		$request                         = new Waf_Request();
+		$this->assertSame( 'test_x', $request->get_remote_addr() );
+		// HTTP_CLIENT_IP takes precendence over everything
+		$_SERVER['HTTP_CLIENT_IP'] = 'test_ip';
+		$request                   = new Waf_Request();
+		$this->assertSame( 'test_ip', $request->get_remote_addr() );
+	}
+
+	/**
+	 * Test Waf_Request::get_query_string()
+	 */
+	public function testGetQueryString() {
+		$_SERVER['QUERY_STRING'] = 'this=is&a=test';
+		$_SERVER['REQUEST_URI']  = 'https://wordpress.com/';
+		$request                 = new Waf_Request();
+		$this->assertSame( '?this=is&a=test', $request->get_query_string() );
+	}
+
+	/**
+	 * Test that the Waf_Request class returns $_COOKIE data correctly via Waf_Request::get_cookies().
+	 */
+	public function testGetCookies() {
+		$_COOKIE['test_cookie'] = 'test_value';
+		$request                = new Waf_Request();
+		$value                  = $request->get_cookies();
+		$this->assertIsArray( $value );
+		$this->assertContainsEquals( array( 'test_cookie', 'test_value' ), $value );
+	}
+
+	/**
+	 * Test that the Waf_Request class returns $_GET data correctly via Waf_Request::get_get_vars().
+	 */
+	public function testGetVarsGet() {
+		$_GET['get_var'] = 'test_value';
+		$_GET['get_num'] = array( 'value1' );
+		$_GET['get_2']   = array( 'child' => 'value' );
+		$request         = new Waf_Request();
+		$value           = $request->get_get_vars();
+		$this->assertIsArray( $value );
+		$this->assertContainsEquals( array( 'get_var', 'test_value' ), $value );
+		$this->assertContainsEquals( array( 'get_2[child]', 'value' ), $value );
+		$this->assertContainsEquals( array( 'get_num[0]', 'value1' ), $value );
+	}
+
+	/**
+	 * Test that the Waf_Request class returns $_POST data correctly via Waf_Request::get_post_vars().
+	 */
+	public function testGetVarsPost() {
+		$_POST['test_var'] = 'test_value';
+		$_POST['test_num'] = array( 'value1' );
+		$_POST['test_2']   = array( 'child' => 'value' );
+		$request           = new Waf_Request();
+		$value             = $request->get_post_vars();
+		$this->assertIsArray( $value );
+		$this->assertContainsEquals( array( 'test_var', 'test_value' ), $value );
+		$this->assertContainsEquals( array( 'test_2[child]', 'value' ), $value );
+		$this->assertContainsEquals( array( 'test_num[0]', 'value1' ), $value );
+	}
+
+	/**
+	 * Test that the Waf_Request class transforms and returns $_FILES data correctly via Waf_Request::get_files().
+	 */
+	public function testGetFiles() {
+		// <input type="file" name="single"/>
+		$_FILES['single'] = array( 'name' => 'single_file.pdf' );
+		// <input type="file" name="double[]"/>
+		// <input type="file" name="double[]"/>
+		$_FILES['double'] = array( 'name' => array( 'double_1.pdf', 'double_2.pdf' ) );
+		// <input type="file" name="nested[a][b]"/>
+		// <input type="file" name="nested[a][c]"/>
+		$_FILES['nested'] = array(
+			'name' => array(
+				'a' => array(
+					'b' => 'nested_ab.pdf',
+					'c' => 'nested_ac.pdf',
+				),
+			),
+		);
+		$request          = new Waf_Request();
+		$values           = $request->get_files();
+		$this->assertIsArray( $values );
+		$this->assertCount( 5, $values );
+		// $_FILES['single'] should turn into a single output value:
+		$this->assertContainsEquals(
+			array(
+				'name'     => 'single',
+				'filename' => 'single_file.pdf',
+			),
+			$values
+		);
+		// $_FILES['double'] should turn into two output values:
+		$this->assertContainsEquals(
+			array(
+				'name'     => 'double[0]',
+				'filename' => 'double_1.pdf',
+			),
+			$values
+		);
+		$this->assertContainsEquals(
+			array(
+				'name'     => 'double[1]',
+				'filename' => 'double_2.pdf',
+			),
+			$values
+		);
+		// $_FILES['nested'] should turn into two output values:
+		$this->assertContainsEquals(
+			array(
+				'name'     => 'nested[a][b]',
+				'filename' => 'nested_ab.pdf',
+			),
+			$values
+		);
+		$this->assertContainsEquals(
+			array(
+				'name'     => 'nested[a][c]',
+				'filename' => 'nested_ac.pdf',
+			),
+			$values
+		);
 	}
 }
---
+++ projects/packages/waf/tests/php/test-waf-runtime-targets.php
@@ -0,0 +1,514 @@
+<?php
+/**
+ * Test WAF runtime target matching
+ *
+ * @package automattic/jetpack-waf
+ */
+
+use Automattic\Jetpack\Waf\Waf_Operators;
+use Automattic\Jetpack\Waf\Waf_Request;
+use Automattic\Jetpack\Waf\Waf_Runtime;
+use Automattic\Jetpack\Waf\Waf_Transforms;
+
+/**
+ * Runtime test suite.
+ */
+final class WafRuntimeTargetsTest extends PHPUnit\Framework\TestCase {
+
+	/**
+	 * Test key/value targets  REQUEST_HEADERS, TX, IP, REQUEST_COOKIES, ARGS, ARGS_POST, ARGS_GET, FILES
+	 *
+	 * @dataProvider provideArrayTargets
+	 *
+	 * @param Waf_Runtime                     $runtime                    The Waf_Runtime instance to use for the test (pre-loaded with items of mocked data).
+	 * @param string                          $target_name                The modsecurity target name being tested, lowercase (examples: 'request_headers', 'tx', 'args', etc).
+	 * @param array{ 0: string, 1: scalar }[] $expected_names_and_values  Array of key/value tuples, where `key` is the name of one of the three mocked items, and `value` is its value.
+	 * @param string                          $second_name_regex          RegEx pattern that will match only the second item in the list.
+	 */
+	public function testArrayTargets( $runtime, $target_name, $expected_names_and_values, $second_name_regex ) {
+		$expected_count = count( $expected_names_and_values );
+		$expected       = array();
+		foreach ( $expected_names_and_values as list( $exp_name, $exp_value ) ) {
+			$expected[] = array(
+				'name'   => $exp_name,
+				'value'  => $exp_value,
+				'source' => "$target_name:$exp_name",
+			);
+		}
+
+		// test getting all values
+		$values = $runtime->normalize_targets( array( $target_name => array() ) );
+		$this->assertCount( $expected_count, $values, "$target_name 'all' test returned incorrect count" );
+		foreach ( $expected as $exp ) {
+			$this->assertContainsEquals( $exp, $values, "$target_name 'all' test did not include value for '{$exp['name']}'" );
+		}
+		// test "only" filter
+		$values = $runtime->normalize_targets( array( $target_name => array( 'only' => array( $expected[1]['name'] ) ) ) );
+		$this->assertCount( 1, $values, "$target_name 'only' test returned incorrect number of values" );
+		$this->assertContainsEquals( $expected[1], $values, "$target_name 'only' test did not include value for '{$expected[1]['name']}'" );
+
+		// test "only" filter with regex pattern
+		$values = $runtime->normalize_targets( array( $target_name => array( 'only' => array( $second_name_regex ) ) ) );
+		$this->assertCount( 1, $values, "$target_name 'only regex' test returned incorrect number of values" );
+		$this->assertContainsEquals( $expected[1], $values, "$target_name 'only regex' test did not include value for '{$expected[1]['name']}'" );
+
+		// test "except" filter
+		$values = $runtime->normalize_targets( array( $target_name => array( 'except' => array( $expected[0]['name'] ) ) ) );
+		$this->assertCount( $expected_count - 1, $values, "$target_name 'except' test returned incorrect number of values" );
+		foreach ( $expected as $i => $exp ) {
+			if ( 0 === $i ) {
+				$this->assertNotContainsEquals( $exp, $values );
+			} else {
+				$this->assertContainsEquals( $exp, $values );
+			}
+		}
+
+		// test "except" filter with regex pattern
+		$values = $runtime->normalize_targets( array( $target_name => array( 'except' => array( $second_name_regex ) ) ) );
+		$this->assertCount( $expected_count - 1, $values, "$target_name 'except regex' test returned incorrect number of values" );
+		foreach ( $expected as $i => $exp ) {
+			if ( 1 === $i ) {
+				$this->assertNotContainsEquals( $exp, $values );
+			} else {
+				$this->assertContainsEquals( $exp, $values );
+			}
+		}
+
+		// test that "except" has higher priority than "only"
+		$values = $runtime->normalize_targets(
+			array(
+				$target_name => array(
+					'only'   => array( $expected[0]['name'] ),
+					'except' => array( $expected[0]['name'] ),
+				),
+			)
+		);
+		$this->assertCount( 0, $values, "$target_name 'only and except' test return more than zero values" );
+
+		// test that "count" works
+		$values = $runtime->normalize_targets( array( $target_name => array( 'count' => true ) ) );
+		$this->assertCount( 1, $values, "$target_name 'count' test returned more than 1 value" );
+		$this->assertContainsEquals(
+			array(
+				'name'   => $target_name,
+				'value'  => $expected_count,
+				'source' => "&$target_name",
+			),
+			$values,
+			"$target_name 'count' test returned wrong count"
+		);
+	}
+
+	/**
+	 * Test all of the *_NAMES targets for the targets tested by testArrayTargets() above
+	 *
+	 * @dataProvider provideArrayTargets
+	 *
+	 * @param Waf_Runtime                     $runtime                    The Waf_Runtime instance to use for the test (pre-loaded with items of mocked data).
+	 * @param string                          $target_name                The modsecurity target name being tested (without the _NAMES), lowercase (examples: 'request_headers', 'tx', 'args', etc).
+	 * @param array{ 0: string, 1: scalar }[] $expected_names_and_values  Array of key/value tuples, where `key` is the name of one of the three mocked items, and `value` is its value.
+	 * @param string                          $second_name_regex          RegEx pattern that will match only the second item in the list.
+	 */
+	public function testArrayTargetsNames( $runtime, $target_name, $expected_names_and_values, $second_name_regex ) {
+		if ( $target_name === 'tx' || $target_name === 'ip' ) {
+			// TX_NAMES and IP_NAMES aren't targets
+			$this->expectNotToPerformAssertions();
+			return;
+		}
+
+		$target_name   .= '_names';
+		$expected_count = count( $expected_names_and_values );
+		$expected       = array();
+		foreach ( $expected_names_and_values as $i => list( $exp_name ) ) {
+			$expected[] = array(
+				'name'   => "$i",
+				'value'  => $exp_name,
+				'source' => "$target_name:$i",
+			);
+		}
+
+		// test getting all values
+		$values = $runtime->normalize_targets( array( $target_name => array() ) );
+		$this->assertCount( $expected_count, $values, "$target_name 'all' test returned incorrect count" );
+		foreach ( $expected as $exp ) {
+			$this->assertContainsEquals( $exp, $values );
+		}
+
+		// test "only" filter
+		$values = $runtime->normalize_targets( array( $target_name => array( 'only' => array( $expected[1]['value'] ) ) ) );
+		$this->assertCount( 1, $values );
+		$this->assertContainsEquals( $expected[1], $values );
+
+		// test "only" filter with regex pattern
+		$values = $runtime->normalize_targets( array( $target_name => array( 'only' => array( $second_name_regex ) ) ) );
+		$this->assertCount( 1, $values );
+		$this->assertContainsEquals( $expected[1], $values );
+
+		// test "except" filter
+		$values = $runtime->normalize_targets( array( $target_name => array( 'except' => array( $expected[0]['value'] ) ) ) );
+		$this->assertCount( $expected_count - 1, $values );
+		foreach ( $expected as $i => $exp ) {
+			if ( 0 === $i ) {
+				$this->assertNotContainsEquals( $exp, $values );
+			} else {
+				$this->assertContainsEquals( $exp, $values );
+			}
+		}
+
+		// test "except" filter with regex pattern
+		$values = $runtime->normalize_targets( array( $target_name => array( 'except' => array( $second_name_regex ) ) ) );
+		$this->assertCount( $expected_count - 1, $values );
+		foreach ( $expected as $i => $exp ) {
+			if ( 1 === $i ) {
+				$this->assertNotContainsEquals( $exp, $values );
+			} else {
+				$this->assertContainsEquals( $exp, $values );
+			}
+		}
+
+		// test that "except" has higher priority than "only"
+		$values = $runtime->normalize_targets(
+			array(
+				$target_name => array(
+					'only'   => array( $expected[0]['value'] ),
+					'except' => array( $expected[0]['value'] ),
+				),
+			)
+		);
+		$this->assertCount( 0, $values );
+
+		// test that "count" works
+		$values = $runtime->normalize_targets( array( $target_name => array( 'count' => true ) ) );
+		$this->assertCount( 1, $values );
+		$this->assertContainsEquals(
+			array(
+				'name'   => $target_name,
+				'value'  => $expected_count,
+				'source' => "&$target_name",
+			),
+			$values
+		);
+	}
+
+	/**
+	 * Provide data to test key/value targets such as REQUEST_HEADERS, ARGS, TX, etc. (as well as their associated *_NAMES targets)
+	 */
+	public function provideArrayTargets() {
+		// REQUEST_HEADERS
+		$expected = array(
+			array( 'header-a', 'testa' ),
+			array( 'header-b', 'testb' ),
+			array( 'header-c', 'testc' ),
+		);
+		$request  = $this->mock_request(
+			array(
+				'headers' => $expected,
+			)
+		);
+		$runtime  = new Waf_Runtime( new Waf_Transforms(), new Waf_Operators(), $request );
+		yield 'REQUEST_HEADERS' => array( $runtime, 'request_headers', $expected, '/-b$/' );
+
+		// TX
+		$runtime  = new Waf_Runtime( new Waf_Transforms(), new Waf_Operators() );
+		$expected = array(
+			array( 'test_a', 'val_a' ),
+			array( 'test_b', 'val_b' ),
+			array( 'test_c', 'val_c' ),
+		);
+		foreach ( $expected as list ( $name, $value ) ) {
+			$runtime->set_var( "tx.$name", $value );
+		}
+		yield 'TX' => array( $runtime, 'tx', $expected, '/_b$/' );
+
+		// IP
+		$runtime  = new Waf_Runtime( new Waf_Transforms(), new Waf_Operators() );
+		$expected = array(
+			array( 'test_a', 'val_a' ),
+			array( 'test_b', 'val_b' ),
+			array( 'test_c', 'val_c' ),
+		);
+		foreach ( $expected as list ( $name, $value ) ) {
+			$runtime->set_var( "ip.$name", $value );
+		}
+		yield 'IP' => array( $runtime, 'ip', $expected, '/_b$/' );
+
+		// REQUEST_COOKIES
+		$expected = array(
+			array( 'test_a', 'cookie_a' ),
+			array( 'test_b', 'cookie_b' ),
+			array( 'test_c', 'cookie_c' ),
+		);
+		$request  = $this->mock_request(
+			array(
+				'cookies' => $expected,
+			)
+		);
+		$runtime  = new Waf_Runtime( new Waf_Transforms(), new Waf_Operators(), $request );
+		yield 'REQUEST_COOKIES' => array( $runtime, 'request_cookies', $expected, '/_b$/' );
+
+		// ARGS
+		$request  = $this->mock_request(
+			array(
+				'get_vars'  => array(
+					array( 'get_var', 'get_val' ),
+				),
+				'post_vars' => array(
+					array( 'post_var', 'post_val' ),
+				),
+			)
+		);
+		$runtime  = new Waf_Runtime( new Waf_Transforms(), new Waf_Operators(), $request );
+		$expected = array(
+			array( 'get_var', 'get_val' ),
+			array( 'post_var', 'post_val' ),
+		);
+		yield 'ARGS' => array( $runtime, 'args', $expected, '/^post_/' );
+
+		// ARGS_GET
+		$request  = $this->mock_request(
+			array(
+				'get_vars' => array(
+					array( 'scalar', 'scalar_val' ),
+					array( 'array[0]', 'array_val_0' ),
+					array( 'array[1]', 'array_val_1' ),
+					array( 'assoc[key0]', 'value0' ),
+					array( 'assoc[key1][key1a]', 'val1a' ),
+				),
+			)
+		);
+		$runtime  = new Waf_Runtime( new Waf_Transforms(), new Waf_Operators(), $request );
+		$expected = array(
+			array( 'scalar', 'scalar_val' ),
+			array( 'array[0]', 'array_val_0' ),
+			array( 'array[1]', 'array_val_1' ),
+			array( 'assoc[key0]', 'value0' ),
+			array( 'assoc[key1][key1a]', 'val1a' ),
+		);
+		yield 'ARGS_GET' => array( $runtime, 'args_get', $expected, '/\[0\]$/' );
+
+		// ARGS_POST
+		$request  = $this->mock_request(
+			array(
+				'post_vars' => array(
+					array( 'scalar', 'scalar_val' ),
+					array( 'array[0]', 'array_val_0' ),
+					array( 'array[1]', 'array_val_1' ),
+					array( 'assoc[key0]', 'value0' ),
+					array( 'assoc[key1][key1a]', 'val1a' ),
+				),
+			)
+		);
+		$runtime  = new Waf_Runtime( new Waf_Transforms(), new Waf_Operators(), $request );
+		$expected = array(
+			array( 'scalar', 'scalar_val' ),
+			array( 'array[0]', 'array_val_0' ),
+			array( 'array[1]', 'array_val_1' ),
+			array( 'assoc[key0]', 'value0' ),
+			array( 'assoc[key1][key1a]', 'val1a' ),
+		);
+		yield 'ARGS_POST' => array( $runtime, 'args_post', $expected, '/\[0\]$/' );
+
+		// FILES
+		$expected   = array(
+			array( 'fileAA', 'file1' ),
+			array( 'fileBB', 'file2' ),
+			array( 'file[]', 'file3' ),
+			array( 'file[]', 'file4' ),
+		);
+		$mock_files = array_map(
+			function ( $item ) {
+				return array(
+					'name'     => $item[0],
+					'filename' => $item[1],
+				);
+			},
+			$expected
+		);
+		$request    = $this->mock_request(
+			array(
+				'files' => $mock_files,
+			)
+		);
+		$runtime    = new Waf_Runtime( new Waf_Transforms(), new Waf_Operators(), $request );
+		yield 'FILES' => array( $runtime, 'files', $expected, '/^fileBB$/' );
+	}
+
+	/**
+	 * Test using REQUEST_METHOD target
+	 */
+	public function testNormalizeRequestMethod() {
+		$request = $this->mock_request( array( 'method' => 'DELETE' ) );
+		$runtime = new Waf_Runtime( new Waf_Transforms(), new Waf_Operators(), $request );
+		$values  = $runtime->normalize_targets( array( 'request_method' => array() ) );
+		$this->assertCount( 1, $values );
+		$this->assertContainsEquals(
+			array(
+				'name'   => 'request_method',
+				'value'  => 'DELETE',
+				'source' => 'request_method',
+			),
+			$values
+		);
+	}
+
+	/**
+	 * Test using REQUEST_PROTOCOL target
+	 */
+	public function testNormalizeRequestProtocol() {
+		$request = $this->mock_request( array( 'protocol' => 'TEST' ) );
+		$runtime = new Waf_Runtime( new Waf_Transforms(), new Waf_Operators(), $request );
+		$values  = $runtime->normalize_targets( array( 'request_protocol' => array() ) );
+		$this->assertCount( 1, $values );
+		$this->assertContainsEquals(
+			array(
+				'name'   => 'request_protocol',
+				'value'  => 'TEST',
+				'source' => 'request_protocol',
+			),
+			$values
+		);
+	}
+
+	/**
+	 * Test using REQUEST_URI, REQUEST_URI_RAW, REQUEST_FILENAME, REQUEST_BASENAME, and QUERY_STRING targets
+	 */
+	public function testNormalizeRequestUri() {
+		$request = $this->mock_request( array( 'url' => array( 'https://wordpress.com', '/index.php', '?test=test' ) ) );
+		$runtime = new Waf_Runtime( new Waf_Transforms(), new Waf_Operators(), $request );
+		// test request_uri
+		$values = $runtime->normalize_targets( array( 'request_uri' => array() ) );
+		$this->assertCount( 1, $values );
+		$this->assertContainsEquals(
+			array(
+				'name'   => 'request_uri',
+				'value'  => '/index.php?test=test',
+				'source' => 'request_uri',
+			),
+			$values
+		);
+		// test request_uri_raw
+		$values = $runtime->normalize_targets( array( 'request_uri_raw' => array() ) );
+		$this->assertCount( 1, $values );
+		$this->assertContainsEquals(
+			array(
+				'name'   => 'request_uri_raw',
+				'value'  => 'https://wordpress.com/index.php?test=test',
+				'source' => 'request_uri_raw',
+			),
+			$values
+		);
+		// test request_filename
+		$values = $runtime->normalize_targets( array( 'request_filename' => array() ) );
+		$this->assertCount( 1, $values );
+		$this->assertContainsEquals(
+			array(
+				'name'   => 'request_filename',
+				'value'  => '/index.php',
+				'source' => 'request_filename',
+			),
+			$values
+		);
+		// test request_basename
+		$values = $runtime->normalize_targets( array( 'request_basename' => array() ) );
+		$this->assertCount( 1, $values );
+		$this->assertContainsEquals(
+			array(
+				'name'   => 'request_basename',
+				'value'  => 'index.php',
+				'source' => 'request_basename',
+			),
+			$values
+		);
+		// test query_string
+		$values = $runtime->normalize_targets( array( 'query_string' => array() ) );
+		$this->assertCount( 1, $values );
+		$this->assertContainsEquals(
+			array(
+				'name'   => 'query_string',
+				'value'  => '?test=test',
+				'source' => 'query_string',
+			),
+			$values
+		);
+	}
+
+	/**
+	 * Test using REMOTE_ADDR target
+	 */
+	public function testNormalizeRemoteAddr() {
+		$request = $this->mock_request( array( 'remote_addr' => 'test_remote_addr' ) );
+		$runtime = new Waf_Runtime( new Waf_Transforms(), new Waf_Operators(), $request );
+		$values  = $runtime->normalize_targets( array( 'remote_addr' => array() ) );
+		$this->assertCount( 1, $values );
+		$this->assertContainsEquals(
+			array(
+				'name'   => 'remote_addr',
+				'value'  => 'test_remote_addr',
+				'source' => 'remote_addr',
+			),
+			$values
+		);
+	}
+
+	/**
+	 * Test using REQUEST_BODY target
+	 */
+	public function testNormalizeRequestBody() {
+		$request = $this->mock_request( array( 'body' => 'test request body' ) );
+		$runtime = new Waf_Runtime( new Waf_Transforms(), new Waf_Operators(), $request );
+		$values  = $runtime->normalize_targets( array( 'request_body' => array() ) );
+		$this->assertCount( 1, $values );
+		$this->assertContainsEquals(
+			array(
+				'name'   => 'request_body',
+				'value'  => 'test request body',
+				'source' => 'request_body',
+			),
+			$values
+		);
+	}
+
+	/**
+	 * Test using REQUEST_LINE target
+	 */
+	public function testNormalizeRequestLine() {
+		$request = $this->mock_request(
+			array(
+				'method'   => 'GET',
+				'url'      => array( 'https://wordpress.com', '/index.php', '?test=test' ),
+				'protocol' => 'HTTP/1.2',
+			)
+		);
+		$runtime = new Waf_Runtime( new Waf_Transforms(), new Waf_Operators(), $request );
+		$values  = $runtime->normalize_targets( array( 'request_line' => array() ) );
+		$this->assertCount( 1, $values );
+		$this->assertContainsEquals(
+			array(
+				'name'   => 'request_line',
+				'value'  => 'GET /index.php?test=test HTTP/1.2',
+				'source' => 'request_line',
+			),
+			$values
+		);
+	}
+
+	/**
+	 * Returned a Waf_Request instance with mocked data.
+	 *
+	 * @param array $data Key/value assoc. array of mocked data to pre-fill the request with.
+	 * @return Waf_Request
+	 */
+	protected function mock_request( $data ) {
+		$method_names = array_map(
+			function ( $k ) {
+				return "get_$k";
+			},
+			array_keys( $data )
+		);
+		$mock         = $this->createPartialMock( Waf_Request::class, $method_names );
+		foreach ( $data as $k => $v ) {
+			$mock->method( "get_$k" )->willReturn( $v );
+		}
+		return $mock;
+	}
+}
