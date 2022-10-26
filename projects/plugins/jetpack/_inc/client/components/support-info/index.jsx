42bde67d0bf1b487cb5c762b380c9a0577ae0c81
Alameen688 <ogundiran12@gmail.com>
2022-10-26 04:52:12 +0100

    [not verified] Refactor support info to allow displaying info text without links

--- projects/plugins/jetpack/_inc/client/components/support-info/index.jsx
+++ projects/plugins/jetpack/_inc/client/components/support-info/index.jsx
@@ -72,24 +72,28 @@ export default class SupportInfo extends Component {
 					screenReaderText={ __( 'Learn more', 'jetpack' ) }
 				>
 					{ text + ' ' }
-					<span className="jp-support-info__learn-more">
-						<ExternalLink
-							href={ link }
-							onClick={ this.trackLearnMoreClick }
-							rel="noopener noreferrer"
-						>
-							{ __( 'Learn more', 'jetpack' ) }
-						</ExternalLink>
-					</span>
-					<span className="jp-support-info__privacy">
-						<ExternalLink
-							href={ privacyLink }
-							onClick={ this.trackPrivacyInfoClick }
-							rel="noopener noreferrer"
-						>
-							{ __( 'Privacy information', 'jetpack' ) }
-						</ExternalLink>
-					</span>
+					{ link && (
+						<span className="jp-support-info__learn-more">
+							<ExternalLink
+								href={ link }
+								onClick={ this.trackLearnMoreClick }
+								rel="noopener noreferrer"
+							>
+								{ __( 'Learn more', 'jetpack' ) }
+							</ExternalLink>
+						</span>
+					) }
+					{ privacyLink && (
+						<span className="jp-support-info__privacy">
+							<ExternalLink
+								href={ privacyLink }
+								onClick={ this.trackPrivacyInfoClick }
+								rel="noopener noreferrer"
+							>
+								{ __( 'Privacy information', 'jetpack' ) }
+							</ExternalLink>
+						</span>
+					) }
 				</InfoPopover>
 			</div>
 		);
