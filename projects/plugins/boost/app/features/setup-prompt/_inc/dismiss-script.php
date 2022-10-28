caf41f4d253b191e6209df082dc1e61eb37b223e
Adnan Haque <adnan007.id@gmail.com>
2022-10-28 13:37:52 +0600

    Minor improvement for text in js

--- projects/plugins/boost/app/features/setup-prompt/_inc/dismiss-script.php
+++ projects/plugins/boost/app/features/setup-prompt/_inc/dismiss-script.php
@@ -2,8 +2,8 @@
 	jQuery( '.boost-dismiss' ).on( 'click', function() {
 		jQuery( '.boost-banner' ).fadeOut( 'slow' );
 		jQuery.post( ajaxurl, {
-			action: '<?php echo esc_js( Automattic\Jetpack_Boost\Features\Setup_Prompt\Setup_Prompt::AJAX_ACTION ); ?>',
-			nonce: '<?php echo esc_js( wp_create_nonce( Automattic\Jetpack_Boost\Features\Setup_Prompt\Setup_Prompt::NONCE_ACTION ) ); ?>',
+			action: <?php echo wp_json_encode( Automattic\Jetpack_Boost\Features\Setup_Prompt\Setup_Prompt::AJAX_ACTION ); ?>,
+			nonce: <?php echo wp_json_encode( wp_create_nonce( Automattic\Jetpack_Boost\Features\Setup_Prompt\Setup_Prompt::NONCE_ACTION ) ); ?>,
 		} );
 	} );
 </script>
