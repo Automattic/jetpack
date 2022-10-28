1ce8234149bba5f166f170f42b5529fff484fb24
retrofox <rdsuarez@gmail.com>
2022-10-28 13:21:10 -0400

    [not verified] add isEstensionEnabled helper()

--- projects/packages/videopress/src/client/block-editor/extensions/index.ts
+++ projects/packages/videopress/src/client/block-editor/extensions/index.ts
@@ -2,9 +2,21 @@
  * External dependencies
  */
 import debugFactory from 'debug';
+import { VideoPressExtensionProp } from './global';
 
 const debug = debugFactory( 'videopress:extensions' );
 
 const extensions = window?.videoPressExtensions || {};
 
 debug( 'Extensions: %o', extensions );
+
+/**
+ * Helper function to check if a given extension is enabled.
+ *
+ * @param {string} extension - The extension to check.
+ * @returns {boolean} - Whether the extension is enabled.
+ */
+export function isEstensionEnabled( extension: string ) {
+	const vpExtension = extensions.find( ( ext: VideoPressExtensionProp ) => ext.name === extension );
+	return !! vpExtension?.isEnabled;
+}
