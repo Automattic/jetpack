5bbcd0663f50ab30aa952a2afb0a2cd1250c9c7b
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2022-10-31 07:51:02 -0700

    Atomic Revival: Restore display of frontend notice https://github.com/Automattic/wpcomsh/pull/1129

--- projects/plugins/wpcomsh/frontend-notices/wpcomsh-frontend-notices.php
+++ projects/plugins/wpcomsh/frontend-notices/wpcomsh-frontend-notices.php
@@ -11,5 +11,5 @@
 require_once __DIR__ . '/class-wpcomsh-frontend-notices.php';
 
 // Disable the notice entirely.
-// add_action( 'wp_enqueue_scripts', array( 'WPCOMSH_Frontend_Notices', 'action_wp_enqueue_script' ) );
+add_action( 'wp_enqueue_scripts', array( 'WPCOMSH_Frontend_Notices', 'action_wp_enqueue_script' ) );
 
