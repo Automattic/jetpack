16962cf20e5309f0293ef89a372880f0516c4fa1
Romario <romarioraff@gmail.com>
2022-10-31 10:50:19 -0500

    [not verified] Fix translation error when builing for production

--- projects/plugins/jetpack/extensions/blocks/subscriptions/paid-plan.js
+++ projects/plugins/jetpack/extensions/blocks/subscriptions/paid-plan.js
@@ -1,14 +1,18 @@
 import { PanelBody, PanelRow } from '@wordpress/components';
 import { compose } from '@wordpress/compose';
 import { withSelect } from '@wordpress/data';
-import { __ } from '@wordpress/i18n';
+import { __, _x } from '@wordpress/i18n';
 import GetAddPaidPlanButton from './utils';
 
 function PaidPlanPanel( { hasNewsletterPlans } ) {
 	const title = __( 'Paid Newsletter', 'jetpack' );
 	const text = hasNewsletterPlans
-		? __( 'Manage paid plan for readers to access your content.', 'jetpack' )
-		: __( 'Set up paid plan for readers to access your content.', 'jetpack' );
+		? _x(
+				'Manage paid plan for readers to access your content.',
+				'unused context to distinguish translations',
+				'jetpack'
+		  )
+		: _x( 'Set up paid plan for readers to access your content.', '', 'jetpack' );
 
 	return (
 		<PanelBody title={ title } initialOpen={ true }>
