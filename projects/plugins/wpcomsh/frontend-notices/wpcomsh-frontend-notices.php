032276ff243e77293006fba405af0816c520da62
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2022-11-01 05:48:13 -0700

    Atomic Revival: Remove erroneous comment https://github.com/Automattic/wpcomsh/pull/1132

--- projects/plugins/wpcomsh/frontend-notices/wpcomsh-frontend-notices.php
+++ projects/plugins/wpcomsh/frontend-notices/wpcomsh-frontend-notices.php
@@ -10,6 +10,5 @@
  */
 require_once __DIR__ . '/class-wpcomsh-frontend-notices.php';
 
-// Disable the notice entirely.
 add_action( 'wp_enqueue_scripts', array( 'WPCOMSH_Frontend_Notices', 'action_wp_enqueue_script' ) );
 
