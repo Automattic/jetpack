9aebd2e3a4d2eb8c1cb3c1963715707ba5458b87
Chris Jean <gaarai@gaarai.com>
2022-11-02 16:39:56 -0600

    VideoPress: New Control in VideoPress Block to Add VideoChapters Block (#27240)
    * Add: Video chapter block prompt when VTT present
    
    Updated the VideoPress editor block to show a prompt to guide the user
    to add the new Video Chapters block after the user adds chapter links to
    the block description.
    
    * changelog
    
    * Modify: VideoChapter prompt styling in VideoPress
    
    Improved the styling of the prompt to add a VideoChapter block when
    editing a VideoPress block.
    
    * Fix: VideoChapters prompt dismiss
    
    The prompt in the VideoPress block to add a VideoChapters block is now
    dismissable.
    
    * tools/fixup-project-versions.sh

--- projects/packages/videopress/src/client/block-editor/plugins/video-chapters/components/details-control/index.js
+++ projects/packages/videopress/src/client/block-editor/plugins/video-chapters/components/details-control/index.js
@@ -1,8 +1,9 @@
 /**
  * External dependencies
  */
-import { PanelBody, TextareaControl, TextControl } from '@wordpress/components';
+import { Button, Notice, PanelBody, TextareaControl, TextControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+import { useState } from 'react';
 /**
  * Internal dependencies
  */
@@ -22,6 +23,11 @@ const CHARACTERS_PER_LINE = 31;
 export default function DetailsControl( { attributes, setAttributes, isRequestingVideoData } ) {
 	const { title, description } = attributes;
 	const isBeta = true;
+	const [ dismiss, setDismiss ] = useState( false );
+
+	const onRemove = () => {
+		setDismiss( true );
+	};
 
 	// Expands the description textarea to accommodate the description
 	const minRows = 4;
@@ -60,10 +66,31 @@ export default function DetailsControl( { attributes, setAttributes, isRequestin
 				label={ __( 'Description', 'jetpack-videopress-pkg' ) }
 				value={ description }
 				placeholder={ __( 'Video description', 'jetpack-videopress-pkg' ) }
+				help={ __(
+					'These details are reflected wherever the video is shown.',
+					'jetpack-videopress-pkg'
+				) }
 				onChange={ setDescriptionAttribute }
 				rows={ descriptionControlRows }
 				disabled={ isRequestingVideoData }
 			/>
+
+			{ ! dismiss && !! attributes.tracks.length && (
+				<Notice
+					className={ 'jetpack-videopress-videochapters-prompt' }
+					status={ 'success' }
+					isDismissable={ true }
+					onRemove={ onRemove }
+				>
+					<p>
+						{ __( 'We detected chapters in your video Description', 'jetpack-videopress-pkg' ) }
+					</p>
+
+					<Button variant="primary">
+						{ __( 'Add chapters list to post', 'jetpack-videopress-pkg' ) }
+					</Button>
+				</Notice>
+			) }
 		</PanelBody>
 	);
 }
