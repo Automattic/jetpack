e51ee9d53218971150385ea199afb94e5eb5fdf8
Wilmerson da Silva <wilmersondasilva@gmail.com>
2022-11-02 16:29:42 -0600

    VideoPress: add a modal to teach users how to add chapters to a video

--- projects/packages/videopress/src/client/block-editor/plugins/video-chapters/components/learn-how-notice/index.js
+++ projects/packages/videopress/src/client/block-editor/plugins/video-chapters/components/learn-how-notice/index.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { Notice, Button } from '@wordpress/components';
+import { Notice, Button, Modal } from '@wordpress/components';
 import { createInterpolateElement } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { useState } from 'react';
@@ -17,9 +17,18 @@ import './index.scss';
  */
 export default function LearnHowNotice() {
 	const [ dismiss, setDismiss ] = useState( false );
+	const [ showModal, setShowModal ] = useState( false );
 
 	const onRemove = () => {
 		setDismiss( true );
+	};
+
+	const openModal = () => {
+		setShowModal( true );
+	};
+
+	const closeModal = () => {
+		setShowModal( false );
 	};
 
 	const message = createInterpolateElement(
@@ -28,7 +37,7 @@ export default function LearnHowNotice() {
 			'jetpack-videopress-pkg'
 		),
 		{
-			addChapters: <span className="add-chapters-message" />,
+			addChapters: <span className="learn-how-notice__add-chapters-message" />,
 		}
 	);
 
@@ -37,11 +46,54 @@ export default function LearnHowNotice() {
 	}
 
 	return (
-		<Notice status="info" className="learn-how-notice" onRemove={ onRemove }>
-			<p className="message">{ message }</p>
-			<Button href="true" className="learn-how-button" onClick={ () => {} }>
-				{ __( 'Learn how', 'jetpack-videopress-pkg' ) }
-			</Button>
-		</Notice>
+		<>
+			<Notice status="info" className="learn-how-notice" onRemove={ onRemove }>
+				<p className="learn-how-notice__message">{ message }</p>
+				<Button className="learn-how-notice__button" onClick={ openModal } variant="link">
+					{ __( 'Learn how', 'jetpack-videopress-pkg' ) }
+				</Button>
+			</Notice>
+			{ showModal && (
+				<Modal
+					title={ __( 'Chapters in VideoPress', 'jetpack-videopress-pkg' ) }
+					isDismissible={ false }
+					className="learn-how-modal"
+				>
+					<p className="learn-how-modal__heading">
+						{ __( 'How to add Chapters to your VideoPress videos', 'jetpack-videopress-pkg' ) }
+					</p>
+					<p>
+						{ __(
+							'1. In the Description, add a list of timestamps and titles.',
+							'jetpack-videopress-pkg'
+						) }
+					</p>
+					<p>
+						{ __(
+							'2. Make sure that the first timestamp you list starts with 00:00.',
+							'jetpack-videopress-pkg'
+						) }
+					</p>
+					<p>
+						{ __(
+							'3. Your video should have at least three timestamps listed in ascending order.',
+							'jetpack-videopress-pkg'
+						) }
+					</p>
+
+					<p className="learn-how-modal__heading">
+						{ __( 'How to add Chapters to your VideoPress videos', 'jetpack-videopress-pkg' ) }
+					</p>
+					<p>{ __( '00:00 Intro', 'jetpack-videopress-pkg' ) }</p>
+					<p>{ __( '00:24 Mountains arise', 'jetpack-videopress-pkg' ) }</p>
+					<p>{ __( '02:38 Coming back home', 'jetpack-videopress-pkg' ) }</p>
+					<p>{ __( '03:04 Credits', 'jetpack-videopress-pkg' ) }</p>
+
+					<Button className="learn-how-modal__button" onClick={ closeModal } variant="primary">
+						{ __( 'Got it, thanks', 'jetpack-videopress-pkg' ) }
+					</Button>
+				</Modal>
+			) }
+		</>
 	);
 }
