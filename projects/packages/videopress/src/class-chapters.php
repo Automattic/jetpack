96f589af5da8ee742ffe0ea5340f4e006f3a1bf4
Piotr Stankowski <piotr.stankowski@automattic.com>
2022-11-03 09:08:30 -0600

    Change rendering vtt time, add data-time attribute

--- projects/packages/videopress/src/class-chapters.php
+++ projects/packages/videopress/src/class-chapters.php
@@ -53,16 +53,62 @@ class Chapters {
 			return '';
 		}
 
-		$html = '<div class="video-chapters_list" data-guid="' . $video_guid .'"><ul>';
+		$html = '<div class="video-chapters_list" data-guid="' . $video_guid . '"><ul>';
 		foreach ( $chapters['chapters']['en'] as $chapter ) {
 			$html .= sprintf(
-				'<li><div class="video-chapters__item"><a class="video-chapters__text" href="#">%s</a> %s</div></li>',
+				'<li><div class="video-chapters__item"><a class="video-chapters__text" href="#" data-time="%s">%s</a> %s</div></li>',
+				esc_attr( self::vtt_time_to_ms( $chapter['start'] ) ),
 				esc_html( $chapter['description'] ),
-				esc_html( $chapter['start'] )
+				esc_html( self::vtt_time_to_mm_ss( $chapter['start'] ) )
 			);
 		}
 		$html .= '</ul></div>';
 
 		return $html;
+	}
+
+	/**
+	 * Vtt time to milliseconds integer.
+	 *
+	 * @param string $vtt_time_format Timestamp in VTT format.
+	 *
+	 * @return int Number of milliseconds.
+	 */
+	protected static function vtt_time_to_ms( $vtt_time_format ) {
+		$date_time_string = '1970-01-01 ' . $vtt_time_format;
+		$date             = \DateTime::createFromFormat( 'Y-m-d H:i:s.u', $date_time_string );
+		if ( ! $date ) {
+			$date = \DateTime::createFromFormat( 'Y-m-d i:s.u', $date_time_string );
+			if ( ! $date ) {
+				return 0;
+			}
+		}
+
+		$secs       = $date->getTimestamp();
+		$millisecs  = $secs * 1000;
+		$millisecs += $date->format( 'u' ) / 1000;
+
+		return (int) $millisecs;
+	}
+
+	/**
+	 * Convert vtt timestamp to mm:ss format.
+	 *
+	 * @param string $vtt_time_format Timestamp in VTT format.
+	 *
+	 * @return string Time in mm:ss
+	 */
+	protected static function vtt_time_to_mm_ss( $vtt_time_format ) {
+		$date_time_string = '1970-01-01 ' . $vtt_time_format;
+		$date             = \DateTime::createFromFormat( 'Y-m-d H:i:s.u', $date_time_string );
+		if ( ! $date ) {
+			$date = \DateTime::createFromFormat( 'Y-m-d i:s.u', $date_time_string );
+			if ( ! $date ) {
+				return '00:00';
+			}
+		}
+
+		// @todo add hours if longer than one hour
+		return $date->format( 'i:s' );
 	}
 }
