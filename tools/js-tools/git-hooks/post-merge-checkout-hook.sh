5e3310b08d17ca482129e840176c9b518ba23a42
Brad Jorsch <brad.jorsch@automattic.com>
2022-11-03 16:08:20 -0400

    git hooks: Improve post-checkout/post-merge hook
    * Use correct previous ref for post-checkout, so it doesn't give
      spurious notifications comparing to whatever the last merge was.
    * Remove "Files within the packages directory have changed" entirely.
      That probably made sense back in 2020 when we first added packages,
      but it doesn't now.
    * When a monorepo root lock file changes, say that instead of just "A
      lockfile".
    * Monorepo root message should not be printed when plugin composer.lock
      changes.
    * List the monorepo root last, it's most important an doing it first
      means it tends to get lost.

--- tools/js-tools/git-hooks/post-merge-checkout-hook.sh
+++ tools/js-tools/git-hooks/post-merge-checkout-hook.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-changedFiles="$(git -c core.quotepath=off diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)"
+changedFiles="$(git -c core.quotepath=off diff-tree -r --name-only --no-commit-id "$1" HEAD)"
 
 runOnChange() {
-	echo "$changedFiles" | grep -q "^$1" && echo -e "$2"
+	echo "$changedFiles" | grep -q "^\($1\)" && echo -e "$2"
 }
 
-runOnChange 'pnpm-lock.yaml\|composer.lock' "A lock file has changed. Consider updating your working copy by running: jetpack install -r"
 for f in $(git -c core.quotepath=off ls-files '**/composer.lock'); do
 	slug="${f#projects/}"
 	slug="${slug%/composer.lock}"
 	runOnChange "$f" "$f has changed. Consider updating your working copy by running: jetpack install $slug"
 done
-runOnChange projects/packages/ "Files within the packages directory have changed. Consider running: jetpack install --all"
+runOnChange 'pnpm-lock.yaml\|composer.lock' "A monorepo root lock file has changed. Consider updating your working copy by running: jetpack install -r"
 
 exit 0
