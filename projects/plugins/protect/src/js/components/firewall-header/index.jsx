1a74a8f955aefbaf68cbe515e017f0b928ca2aae
dkmyta <dkmyta10@gmail.com>
2022-11-04 15:29:27 -0700

    [not verified] Remove unneeded comments

--- projects/plugins/protect/src/js/components/firewall-header/index.jsx
+++ projects/plugins/protect/src/js/components/firewall-header/index.jsx
@@ -1,48 +1,14 @@
-import {
-	AdminSectionHero,
-	Container,
-	Col,
-	Text,
-	H3,
-	Button,
-	// useBreakpointMatch,
-} from '@automattic/jetpack-components';
+import { AdminSectionHero, Container, Col, Text, H3, Button } from '@automattic/jetpack-components';
 import { Spinner } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import {
-	Icon,
-	// shield,
-	// chartBar,
-	help,
-} from '@wordpress/icons';
+import { Icon, help } from '@wordpress/icons';
 import classnames from 'classnames';
-// import StatCard from '../stat-card';
 import styles from './styles.module.scss';
 
 const FirewallHeader = () => {
 	// TODO: Update placeholder with actual WAF data
 	const status = 'off';
 	const hasRequiredPlan = false;
-
-	// const [ isSmall ] = useBreakpointMatch( [ 'sm', 'lg' ], [ null, '<' ] );
-
-	// const lastThirtyArgs = {
-	// 	icon: <Icon icon={ shield } />,
-	// 	label: __( 'Blocked requests', 'jetpack-protect' ),
-	// 	period: __( 'Last 30 days', 'jetpack-protect' ),
-	// 	value: 0, // TODO: Update with actual WAF data when on
-	// 	variant: isSmall ? 'horizontal' : 'square',
-	// 	disabled: hasRequiredPlan ? false : true,
-	// };
-
-	// const allTimeArgs = {
-	// 	icon: <Icon icon={ chartBar } />,
-	// 	label: __( 'Blocked requests', 'jetpack-protect' ),
-	// 	period: __( 'All time', 'jetpack-protect' ),
-	// 	value: 0, // TODO: Update with actual WAF data when on
-	// 	variant: isSmall ? 'horizontal' : 'square',
-	// 	disabled: hasRequiredPlan ? false : true,
-	// };
 
 	if ( 'on' === status ) {
 		return (
@@ -61,10 +27,7 @@ const FirewallHeader = () => {
 						</H3>
 					</Col>
 					<Col>
-						<div className={ styles[ 'stat-card-wrapper' ] }>
-							{ /* <StatCard { ...lastThirtyArgs } /> */ }
-							{ /* <StatCard { ...allTimeArgs } /> */ }
-						</div>
+						<div className={ styles[ 'stat-card-wrapper' ] }></div>
 					</Col>
 				</Container>
 			</AdminSectionHero>
@@ -99,10 +62,7 @@ const FirewallHeader = () => {
 						) }
 					</Col>
 					<Col>
-						<div className={ styles[ 'stat-card-wrapper' ] }>
-							{ /* <StatCard { ...lastThirtyArgs } /> */ }
-							{ /* <StatCard { ...allTimeArgs } /> */ }
-						</div>
+						<div className={ styles[ 'stat-card-wrapper' ] }></div>
 					</Col>
 				</Container>
 			</AdminSectionHero>
@@ -126,10 +86,7 @@ const FirewallHeader = () => {
 					</Text>
 				</Col>
 				<Col>
-					<div className={ styles[ 'stat-card-wrapper' ] }>
-						{ /* <StatCard { ...lastThirtyArgs } /> */ }
-						{ /* <StatCard { ...allTimeArgs } /> */ }
-					</div>
+					<div className={ styles[ 'stat-card-wrapper' ] }></div>
 				</Col>
 			</Container>
 		</AdminSectionHero>
