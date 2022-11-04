799b3645aa18e54ad7eec2b97600463bcbc6c056
Nauris Pūķis <hi@pyronaur.com>
2022-11-04 16:55:44 +0200

    Compare image scale

--- projects/plugins/boost/app/features/guide/src/Measurements.ts
+++ projects/plugins/boost/app/features/guide/src/Measurements.ts
@@ -7,16 +7,39 @@ export interface MeasuredImage extends Image {
 	};
 }
 
-export const measure = (images: Image[]) => {
+export interface ComparedImage extends MeasuredImage {
+	scaling: {
+		width: number;
+		height: number;
+		pixels: number;
+	};
+}
+
+function compareDimensions(image: MeasuredImage) {
+	const onScreen = image.onScreen;
+
+	return {
+		width: image.width / onScreen.width,
+		height: image.height / onScreen.height,
+		pixels: (image.width * image.height) / (onScreen.width * onScreen.height),
+	};
+}
+
+export function measure(images: Image[]): ComparedImage[] {
 	return images.map(image => {
 		const { width, height } = image.node.getBoundingClientRect();
 
-		return {
+		const measuredImage = {
 			...image,
 			onScreen: {
 				width,
 				height,
 			},
 		};
+
+		return {
+			scaling: compareDimensions(measuredImage),
+			...measuredImage,
+		};
 	});
-};
+}
