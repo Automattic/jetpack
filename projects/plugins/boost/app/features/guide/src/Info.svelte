f9b8b475723599133dd3597a46635320d9acf3e9
Nauris Pūķis <hi@pyronaur.com>
2022-11-08 13:40:57 +0200

    Us real data in the bubbles

--- projects/plugins/boost/app/features/guide/src/Info.svelte
+++ projects/plugins/boost/app/features/guide/src/Info.svelte
@@ -1,24 +1,18 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
 	import ImageGuide from './ImageGuide.svelte';
 	import type { ComparedImage } from './Measurements';
+	import Bubble from './ui/Bubble.svelte';
 	export let images: ComparedImage[];
-	const insertNodes = images.some( image => image.type === "background" );
+	const insertNodes = images.some(image => image.type === 'background');
 
-	// const ratio = image.scaling.pixels.toFixed(2);
-	// const severity = *image.scaling.pixels > 4 ? 'high' : image.scaling.pixels > 2 ? 'medium' : 'normal';
-	const ratio = 2;
-	const severity = 'medium';
-
-	let show: number|false = false;
+	let show: number | false = false;
 </script>
 
-<div class="jb-guide" class:bg={!insertNodes} on:mouseleave={() => show = false}>
+
+<div class="jb-guide" class:bg={!insertNodes} on:mouseleave={() => (show = false)}>
 	<div class="jb-guide-previews">
-		{#each images as _, index}
-		<div class="jb-guide-preview {severity}" on:mouseenter={() => show = index}>
-			<div class="jb-guide-preview__ratio">{ratio}</div>
-		</div>
+		{#each images as image, index}
+			<Bubble ratio={image.scaling.pixels} on:mouseenter={() => (show = index)} />
 		{/each}
 	</div>
 	{#if show !== false}
---
+++ projects/plugins/boost/app/features/guide/src/ui/Bubble.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let ratio: number;
+	const severity = ratio > 4 ? 'high' : ratio > 2 ? 'medium' : 'normal';
+</script>
+
+<div class="jb-guide-preview {severity}" on:mouseenter>
+	<div class="jb-guide-preview__ratio">{ratio.toFixed(2)}</div>
+</div>
