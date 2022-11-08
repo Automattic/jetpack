f7400a87fb7f2d86d47020f2efd03a17a1b3522c
Damián Suárez <rdsuarez@gmail.com>
2022-11-08 13:47:09 -0700

    VideoPress: introduce PlaybackControl component (#27303)
    * [not verified] add types for Video block
    
    * Add PlaybackControl component
    
    * Add PlaybackControl component
    
    * Always show autoplay help
    
    * rename block attributes type name
    
    * changelog
    
    * reword 'controls' control

---
+++ projects/packages/videopress/src/client/block-editor/blocks/video/types.ts
@@ -0,0 +1,40 @@
+type Track = {
+	items: object;
+};
+
+export type VideoBlockAttributes = {
+	id?: number;
+	guid?: string;
+	src?: string;
+	poster?: string;
+	videoRatio?: number;
+	tracks?: Track[];
+
+	// Playback types
+	autoplay?: boolean;
+	caption?: string;
+	controls?: boolean;
+	loop?: boolean;
+	muted?: boolean;
+	playsinline?: boolean;
+	preload?: string;
+
+	// Rendering types
+	cacheHtml?: string;
+	maxWidth?: string;
+
+	// Colors types
+	seekbarPlayedColor?: string;
+	seekbarLoadingColor?: string;
+	seekbarColor?: string;
+	useAverageColor?: boolean;
+};
+
+export type PlaybackControlProps = {
+	/**
+	 * Block Attributes object.
+	 */
+	attributes: VideoBlockAttributes;
+
+	setAttributes: ( attributes: VideoBlockAttributes ) => void;
+};
