360b7a6c62215c792dc6cb45663c3daded68b2f2
Douglas <douglas.henri@automattic.com>
2022-11-08 16:11:26 -0300

    fix hover flickering

--- projects/packages/videopress/src/client/admin/hooks/use-drop-files/index.ts
+++ projects/packages/videopress/src/client/admin/hooks/use-drop-files/index.ts
@@ -13,27 +13,55 @@ const useDropFiles = ( {
 }: { canDrop?: boolean; onSelectFiles?: ( files: File[] ) => void } = {} ) => {
 	const [ isDraggingOver, setIsDraggingOver ] = useState( false );
 	const inputRef = useRef( null );
+	// Chrome and Firefox diverge on the order of events, with Chrome firing two leave events in a row, hence the array
+	let dragTimeouts = [];
 
 	const handleFileInputChangeEvent = useCallback(
-		( e: ChangeEvent< HTMLInputElement > ) => {
+		( e: Pick< ChangeEvent< HTMLInputElement >, 'currentTarget' > ) => {
 			onSelectFiles( Array.from( e.currentTarget.files ) );
 		},
 		[ onSelectFiles ]
 	);
 
-	const handleDragOverEvent = ( event: DragEvent< HTMLElement > ) => {
+	const handleDragOverEvent = (
+		event: Pick< DragEvent< HTMLElement >, 'stopPropagation' | 'preventDefault' >
+	) => {
 		event.preventDefault();
+		event.stopPropagation();
+
+		if ( dragTimeouts.length > 0 ) {
+			dragTimeouts.forEach( timeout => clearTimeout( timeout ) );
+			dragTimeouts = [];
+		}
+
 		setIsDraggingOver( true );
 	};
 
 	const handleDragLeaveEvent = () => {
-		setIsDraggingOver( false );
+		// Timeout to avoid flickering as the cursor changes which element it is hovering
+		const timeout = setTimeout( () => {
+			setIsDraggingOver( false );
+
+			const timeoutIndex = dragTimeouts.findIndex( dragTimeout => dragTimeout === timeout );
+			if ( timeoutIndex > -1 ) {
+				dragTimeouts.splice( timeoutIndex, 1 );
+			}
+		}, 100 );
+
+		dragTimeouts.push( timeout );
 	};
 
 	const handleDropEvent = useCallback(
-		( event: DragEvent< HTMLElement > ) => {
-			event.preventDefault();
+		(
+			event: Pick< DragEvent< HTMLElement >, 'preventDefault' | 'stopPropagation' | 'dataTransfer' >
+		) => {
 			setIsDraggingOver( false );
+
+			event.preventDefault();
+			event.stopPropagation();
+
+			dragTimeouts.forEach( timeout => clearTimeout( timeout ) );
+			dragTimeouts = [];
 
 			if ( ! canDrop ) {
 				return;
