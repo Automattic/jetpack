3b98be214fee6748b025de0893c78ac10d28d50a
Jasper Kang <kangzjnet@gmail.com>
2022-11-15 09:09:43 +1300

    port search plugin 1.3.1 changelog

--- projects/plugins/search/jetpack-search.php
+++ projects/plugins/search/jetpack-search.php
@@ -3,9 +3,9 @@
  *
  * Plugin Name: Jetpack Search
  * Plugin URI: https://jetpack.com/search/
- * Description: A cloud-powered replacement for WordPress' search.
- * Version: 1.3.1-alpha
- * Author: Automattic
+ * Description: Easily add cloud-powered instant search and filters to your website or WooCommerce store with advanced algorithms that boost your search results based on traffic to your site.
+ * Version: 1.3.1
+ * Author: Automattic - Jetpack Search team
  * Author URI: https://jetpack.com/
  * License: GPLv2 or later
  * Text Domain: jetpack-search
