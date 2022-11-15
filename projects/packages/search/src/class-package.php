f600069acea9800397f1aa7d30f270a8270f401f
Piotr Stankowski <piotr.stankowski@automattic.com>
2022-11-15 22:37:29 +0100

    Fixup versions

--- projects/packages/search/src/class-package.php
+++ projects/packages/search/src/class-package.php
@@ -11,7 +11,7 @@ namespace Automattic\Jetpack\Search;
  * Search package general information
  */
 class Package {
-	const VERSION = '0.30.3-alpha';
+	const VERSION = '0.31.0-alpha';
 	const SLUG    = 'search';
 
 	/**
