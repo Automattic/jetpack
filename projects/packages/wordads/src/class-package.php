9293fdc33d97ac0e011bbe6c8baef4c05b2cc829
thingalon <mark@thingalon.com>
2022-11-15 11:50:06 +1100

    Boost: Add a gadget for measuring the performance improvement Super Cache brings (#26736)
    * Creating basics for super cache speed measurement
    
    * Basic demonstration of the idea working
    
    * changelog
    
    * Remove unused hook
    
    * Calm changelogger's farm
    
    * Allow camelcase in JS, as it's commonly used when passing stuff from PHP
    
    * Re-style Super Cache info to match installer designs
    
    * Add a new notice component
    
    * changelog
    
    * Switch to using a the new storybook component for the Super Cache info box
    
    * Make notices dismissable
    
    * Add tracks events for measuring super cache speed
    
    * fix versions
    
    * Remove duplicate def of is-primary
    
    * Remove superfluous quotes
    
    * Update projects/plugins/boost/app/assets/src/js/stores/config.ts
    
    Co-authored-by: Adnan Haque <eAdnan007@users.noreply.github.com>
    
    * Update lockfile
    
    * update versions
    
    * Fix versioning whatsits
    
    * Fixing yet more version issues. whee
    
    * I am having so much fun spending my morning fixing versioning issues
    
    Co-authored-by: Mark George <thingalon@gmail.com>
    Co-authored-by: Karen Attfield <karenlattfield@gmail.com>
    Co-authored-by: oskosk <oskosk@gmail.com>
    Co-authored-by: Adnan Haque <eAdnan007@users.noreply.github.com>

--- projects/packages/wordads/src/class-package.php
+++ projects/packages/wordads/src/class-package.php
@@ -11,7 +11,7 @@ namespace Automattic\Jetpack\WordAds;
  * WordAds package general information
  */
 class Package {
-	const VERSION = '0.2.24';
+	const VERSION = '0.2.25-alpha';
 	const SLUG    = 'wordads';
 
 	/**
