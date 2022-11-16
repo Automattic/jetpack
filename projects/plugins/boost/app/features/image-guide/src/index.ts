c0eebb93dc77d1bb9cf733a1ee7436eff371183f
Nauris Pūķis <hi@pyronaur.com>
2022-11-16 17:44:21 +0200

    Initialize when the plugin state changes after window load

--- projects/plugins/boost/app/features/image-guide/src/index.ts
+++ projects/plugins/boost/app/features/image-guide/src/index.ts
@@ -2,31 +2,39 @@ import { attachGuides } from './Attach';
 import { load } from './Images';
 import { measure } from './Measurements';
 import AdminBarToggle from './ui/AdminBarToggle.svelte';
+import state from './ui/StateStore';
 
 /**
  * Initialize the admin bar toggle.
  */
-document.addEventListener( 'DOMContentLoaded', () => {
-	const adminBarToggle = document.getElementById( 'wp-admin-bar-jetpack-boost-image-guide' );
-	const link = adminBarToggle?.querySelector( 'a' );
-	if ( adminBarToggle && link ) {
-		const href = link.getAttribute( 'href' );
+document.addEventListener('DOMContentLoaded', () => {
+	const adminBarToggle = document.getElementById('wp-admin-bar-jetpack-boost-image-guide');
+	const link = adminBarToggle?.querySelector('a');
+	if (adminBarToggle && link) {
+		const href = link.getAttribute('href');
 		link.remove();
-		new AdminBarToggle( {
+		new AdminBarToggle({
 			target: adminBarToggle,
 			props: {
 				href,
 			},
-		} );
+		});
 	}
-} );
+});
 
 /**
  * Initialize the guides when window is loaded.
  */
-window.addEventListener( 'load', async () => {
-	const nodes = document.querySelectorAll( 'body *' );
-	const images = await load( Array.from( nodes ) );
-	const measuredImages = measure( images );
-	attachGuides( measuredImages );
-} );
+window.addEventListener('load', () => {
+	state.subscribe(async state => {
+		if (state === 'Paused') {
+			return;
+		}
+		const nodes = document.querySelectorAll(
+			'body *:not(.jetpack-boost-guide > *):not(.jetpack-boost-guide)'
+		);
+		let images = await load(Array.from(nodes));
+		const measuredImages = measure(images);
+		attachGuides(measuredImages);
+	});
+});
