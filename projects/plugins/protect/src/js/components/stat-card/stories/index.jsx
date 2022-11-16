59922d18593d6e026c28b4a7bee7029cf79f4df1
dkmyta <dkmyta10@gmail.com>
2022-11-16 11:48:00 -0800

    [not verified] Add StatCard component stories

---
+++ projects/plugins/protect/src/js/components/stat-card/stories/index.jsx
@@ -0,0 +1,54 @@
+import { Icon, shield, chartBar } from '@wordpress/icons';
+import React from 'react';
+import StatCard from '../index.jsx';
+
+export default {
+	title: 'Plugins/Protect/Stat Card',
+	component: StatCard,
+	decorators: [
+		Story => (
+			<div style={ { display: 'inline-flex' } }>
+				<Story />
+			</div>
+		),
+	],
+};
+
+export const Square = () => {
+	const lastThirtyArgs = {
+		icon: <Icon icon={ shield } />,
+		label: 'Blocked requests',
+		period: 'Last 30 days',
+		value: 15,
+		variant: 'square',
+		disabled: false,
+	};
+
+	return <StatCard { ...lastThirtyArgs } />;
+};
+
+export const Horizontal = () => {
+	const allTimeArgs = {
+		icon: <Icon icon={ chartBar } />,
+		label: 'Blocked requests',
+		period: 'All time',
+		value: 1000,
+		variant: 'horizontal',
+		disabled: false,
+	};
+
+	return <StatCard { ...allTimeArgs } />;
+};
+
+export const Disabled = () => {
+	const lastThirtyArgs = {
+		icon: <Icon icon={ shield } />,
+		label: 'Blocked requests',
+		period: 'Last 30 days',
+		value: 10,
+		variant: 'square',
+		disabled: true,
+	};
+
+	return <StatCard { ...lastThirtyArgs } />;
+};
