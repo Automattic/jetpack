4155bb285aa582e91151331a1982c7bdf9574393
Adrian Moldovan <adim.moldovan@gmail.com>
2022-11-17 16:07:31 +0200

    Rename var

--- .github/files/e2e-tests/report-metadata.sh
+++ .github/files/e2e-tests/report-metadata.sh
@@ -1,5 +1,5 @@
-if [[ -z "$RUN_ID" ]]; then
-	echo "RUN_ID is not defined, using pull request number or branch"
+if [[ -z "$SUITE" ]]; then
+	echo "SUITE is not defined, using pull request number or branch"
 
 	if [[ -z "$PR_NUMBER" ]]; then
 		echo "PR_NUMBER is not defined, using branch"
@@ -8,11 +8,11 @@ if [[ -z "$RUN_ID" ]]; then
     else
         BRANCH=${GITHUB_REF:11}
     fi
-    RUN_ID=$BRANCH
+    SUITE=$BRANCH
   else
-  	RUN_ID=$PR_NUMBER
+  	SUITE=$PR_NUMBER
 	fi
 fi
 
-jq -n --arg runId "$RUN_ID" '{runId:$runId}' > "$OUTPUT_PATH/output/report-metadata.json"
+jq -n --arg runId "$SUITE" '{suite:$suite}' > "$OUTPUT_PATH/output/report-metadata.json"
 cat report-metadata.json
