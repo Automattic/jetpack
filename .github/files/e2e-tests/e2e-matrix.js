35d75409ffc11ecb0efe160ab76cec7801ecfc91
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2022-11-17 19:48:00 +0200

    E2E tests: update runs for jetpack-production and add report metadata (#27466)

--- .github/files/e2e-tests/e2e-matrix.js
+++ .github/files/e2e-tests/e2e-matrix.js
@@ -6,34 +6,44 @@ const projects = [
 		project: 'Jetpack connection',
 		path: 'projects/plugins/jetpack/tests/e2e',
 		testArgs: [ 'specs/connection', '--retries=1' ],
+		suite: '',
 	},
 	{
 		project: 'Jetpack pre-connection',
 		path: 'projects/plugins/jetpack/tests/e2e',
 		testArgs: [ 'specs/pre-connection', '--retries=1' ],
+		suite: '',
 	},
 	{
 		project: 'Jetpack post-connection',
 		path: 'projects/plugins/jetpack/tests/e2e',
 		testArgs: [ 'specs/post-connection', '--retries=1' ],
+		suite: '',
 	},
 	{
 		project: 'Jetpack sync',
 		path: 'projects/plugins/jetpack/tests/e2e',
 		testArgs: [ 'specs/sync', '--retries=1' ],
+		suite: '',
 	},
 	{
 		project: 'Jetpack blocks',
 		path: 'projects/plugins/jetpack/tests/e2e',
 		testArgs: [ 'specs/blocks', '--retries=1' ],
+		suite: '',
 	},
-	{ project: 'Boost', path: 'projects/plugins/boost/tests/e2e', testArgs: [] },
-	{ project: 'Search', path: 'projects/plugins/search/tests/e2e', testArgs: [] },
-	{ project: 'VideoPress', path: 'projects/plugins/videopress/tests/e2e', testArgs: [] },
-	{ project: 'Social', path: 'projects/plugins/social/tests/e2e', testArgs: [] },
+	{ project: 'Boost', path: 'projects/plugins/boost/tests/e2e', testArgs: [], suite: '' },
+	{ project: 'Search', path: 'projects/plugins/search/tests/e2e', testArgs: [], suite: '' },
+	{
+		project: 'VideoPress',
+		path: 'projects/plugins/videopress/tests/e2e',
+		testArgs: [],
+		suite: '',
+	},
+	{ project: 'Social', path: 'projects/plugins/social/tests/e2e', testArgs: [], suite: '' },
 ];
 
-const runConfig = { run: '', matrix: [] };
+const matrix = [];
 
 switch ( process.env.GITHUB_EVENT_NAME ) {
 	case 'pull_request':
@@ -49,34 +59,43 @@ switch ( process.env.GITHUB_EVENT_NAME ) {
 				// iterate over defined target plugins/projects and see if they are changed
 				for ( const target of packageJson.ci.targets ) {
 					if ( Object.keys( changedProjects ).includes( target ) ) {
-						runConfig.matrix.push( project );
+						matrix.push( project );
 						break;
 					}
 				}
 			} else {
 				// if no targets are defined, run the tests
-				runConfig.matrix.push( project );
+				matrix.push( project );
 			}
 		}
 		break;
 	}
 	case 'workflow_run': {
-		runConfig.matrix.push( ...projects );
+		matrix.push( ...projects );
 		break;
 	}
 	case 'repository_dispatch':
 		if ( process.env.DISPATCH_REPO ) {
 			const repoName = process.env.DISPATCH_REPO.split( '/' )[ 1 ];
-			runConfig.run = repoName;
 
 			for ( const project of projects ) {
 				const packageJson = JSON.parse(
 					fs.readFileSync( `${ project.path }/package.json`, 'utf8' )
 				);
 
+				project.suite = repoName;
 				if ( packageJson?.ci?.mirrorName === repoName ) {
-					runConfig.matrix.push( project );
+					matrix.push( project );
 				}
+			}
+
+			if ( repoName === 'jetpack-production' ) {
+				matrix.push( {
+					project: 'Jetpack on Atomic',
+					path: 'projects/plugins/jetpack/tests/e2e',
+					testArgs: [ 'blocks', '--retries=1' ],
+					suite: 'atomic',
+				} );
 			}
 		} else {
 			// eslint-disable-next-line no-console
@@ -86,19 +105,11 @@ switch ( process.env.GITHUB_EVENT_NAME ) {
 	case 'schedule':
 		// gutenberg scheduled run
 		if ( process.env.CRON === '0 */12 * * *' ) {
-			runConfig.matrix.push( {
+			matrix.push( {
 				project: 'Jetpack with Gutenberg',
 				path: 'projects/plugins/jetpack/tests/e2e',
 				testArgs: [ 'blocks', '--retries=1' ],
-			} );
-		}
-
-		// atomic scheduled run
-		if ( process.env.CRON === '30 */4 * * *' ) {
-			runConfig.matrix.push( {
-				project: 'Jetpack on Atomic',
-				path: 'projects/plugins/jetpack/tests/e2e',
-				testArgs: [ 'blocks', '--retries=1' ],
+				suite: 'gutenberg',
 			} );
 		}
 		break;
@@ -108,4 +119,4 @@ switch ( process.env.GITHUB_EVENT_NAME ) {
 }
 
 // eslint-disable-next-line no-console
-console.log( JSON.stringify( runConfig ) );
+console.log( JSON.stringify( matrix ) );
