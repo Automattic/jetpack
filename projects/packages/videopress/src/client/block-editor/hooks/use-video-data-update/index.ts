59c5b1971950cc6827ffc8a9daecd92ee50dc75f
Damián Suárez <rdsuarez@gmail.com>
2022-11-17 11:57:20 -0300

    [not verified] not need to cast allowDownload attr

--- projects/packages/videopress/src/client/block-editor/hooks/use-video-data-update/index.ts
+++ projects/packages/videopress/src/client/block-editor/hooks/use-video-data-update/index.ts
@@ -131,11 +131,6 @@ export function useSyncMedia(
 		// ...and udpate the block attributes with fresh data.
 		const initialAttributesValues = mapObjectKeysToCamel( initialVideoData, true );
 
-		// Cast/tweak response body => block attributes.
-		if ( typeof initialAttributesValues.allowDownload !== 'undefined' ) {
-			initialAttributesValues.allowDownload = !! initialAttributesValues.allowDownload;
-		}
-
 		setAttributes( initialAttributesValues );
 	}, [ videoData, isRequestingVideoData ] );
 
