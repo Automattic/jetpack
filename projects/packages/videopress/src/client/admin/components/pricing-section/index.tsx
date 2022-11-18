58a34a90d6f0386aab140ddc71dcdf2685ef370e
Damián Suárez <rdsuarez@gmail.com>
2022-11-18 14:40:54 -0300

    do not show discount price

--- projects/packages/videopress/src/client/admin/components/pricing-section/index.tsx
+++ projects/packages/videopress/src/client/admin/components/pricing-section/index.tsx
@@ -20,8 +20,8 @@ import { usePlan } from '../../hooks/use-plan';
 const PricingPage = ( { onRedirecting } ) => {
 	const { siteSuffix, adminUri, registrationNonce } = window.jetpackVideoPressInitialState;
 	const { siteProduct, product } = usePlan();
-	const { productPrice } = product;
 	const { pricingForUi } = siteProduct;
+	const { productPrice } = product;
 	const { handleRegisterSite, userIsConnecting } = useConnection( {
 		redirectUri: adminUri,
 		from: 'jetpack-videopress',
@@ -42,16 +42,14 @@ const PricingPage = ( { onRedirecting } ) => {
 	 * This can happen when the site is not connected yet.
 	 */
 	if ( ! pricingForUi?.fullPrice ) {
+		pricingForUi.currencyCode = product.currencyCode;
+		pricingForUi.discountPrice = null;
+
 		if ( productPrice ) {
-			pricingForUi.fullPrice = productPrice.monthly.price;
-			pricingForUi.discountPrice = productPrice.yearly.priceByMonth;
-			pricingForUi.currencyCode = product.currencyCode;
+			pricingForUi.fullPrice = productPrice.yearly.priceByMonth;
 		} else {
 			// Let's hard code these values for now,
-			// in case the data is still cached.
-			pricingForUi.fullPrice = 20;
-			pricingForUi.discountPrice = 10;
-			pricingForUi.currencyCode = product.currencyCode;
+			pricingForUi.fullPrice = 10;
 		}
 	}
 
@@ -62,6 +60,9 @@ const PricingPage = ( { onRedirecting } ) => {
 					<ProductPrice
 						price={ pricingForUi.fullPrice }
 						offPrice={ pricingForUi.discountPrice }
+						promoLabel={
+							pricingForUi.discountPrice ? __( '50% off', 'jetpack-videopress-pkg' ) : null
+						}
 						legend={ __( '/month, billed yearly', 'jetpack-videopress-pkg' ) }
 						currency={ pricingForUi.currencyCode }
 					/>
