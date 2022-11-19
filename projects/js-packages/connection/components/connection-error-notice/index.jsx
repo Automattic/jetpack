1874cda593a7587c7273bfa538554cf74dc7c461
oskosk <oskosk@gmail.com>
2022-11-19 09:35:34 -0300

    Use SVG and Path instead of svg and path

--- projects/js-packages/connection/components/connection-error-notice/index.jsx
+++ projects/js-packages/connection/components/connection-error-notice/index.jsx
@@ -1,5 +1,5 @@
 import { Spinner } from '@automattic/jetpack-components';
-import { Icon, Notice } from '@wordpress/components';
+import { Icon, Notice, Path, SVG } from '@wordpress/components';
 import { __, sprintf } from '@wordpress/i18n';
 import PropTypes from 'prop-types';
 import React from 'react';
@@ -22,21 +22,21 @@ const ConnectionErrorNotice = props => {
 	const icon = (
 		<Icon
 			icon={
-				<svg
+				<SVG
 					width="24"
 					height="24"
 					viewBox="0 0 24 24"
 					fill="none"
 					xmlns="http://www.w3.org/2000/svg"
 				>
-					<path
+					<Path
 						d="M11.7815 4.93772C11.8767 4.76626 12.1233 4.76626 12.2185 4.93772L20.519 19.8786C20.6116 20.0452 20.4911 20.25 20.3005 20.25H3.69951C3.50889 20.25 3.3884 20.0452 3.48098 19.8786L11.7815 4.93772Z"
 						stroke="#D63638"
 						strokeWidth="1.5"
 					/>
-					<path d="M13 10H11V15H13V10Z" fill="#D63638" />
-					<path d="M13 16H11V18H13V16Z" fill="#D63638" />
-				</svg>
+					<Path d="M13 10H11V15H13V10Z" fill="#D63638" />
+					<Path d="M13 16H11V18H13V16Z" fill="#D63638" />
+				</SVG>
 			}
 		/>
 	);
