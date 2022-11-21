5152f1220c63c86ead90ec007a180ab2bc73318f
Nate Weller <nate.weller@automattic.com>
2022-11-21 12:25:24 -0700

    Add 'new' badge for users who have never loaded the WAF screen

--- projects/plugins/protect/src/js/components/firewall-page/index.jsx
+++ projects/plugins/protect/src/js/components/firewall-page/index.jsx
@@ -1,9 +1,29 @@
-import { AdminPage } from '@automattic/jetpack-components';
-import { __ } from '@wordpress/i18n';
-import Logo from '../logo';
+import apiFetch from '@wordpress/api-fetch';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { useEffect } from 'react';
+import { STORE_ID } from '../../state/store';
+import AdminPage from '../admin-page';
 
-const FirewallPage = () => (
-	<AdminPage moduleName={ __( 'Jetpack Protect', 'jetpack-protect' ) } header={ <Logo /> } />
-);
+const FirewallPage = () => {
+	const wafSeen = useSelect( select => select( STORE_ID ).getWafSeen() );
+	const { setWafSeen } = useDispatch( STORE_ID );
+
+	useEffect( () => {
+		if ( wafSeen ) {
+			return;
+		}
+
+		// remove the "new" badge immediately
+		setWafSeen( true );
+
+		// update the meta value in the background
+		apiFetch( {
+			path: 'jetpack-protect/v1/waf-seen',
+			method: 'POST',
+		} );
+	}, [ wafSeen, setWafSeen ] );
+
+	return <AdminPage />;
+};
 
 export default FirewallPage;
