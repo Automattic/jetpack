d441341fa5c25f6fb3edda38d8947872ef30ed1e
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2022-11-21 20:07:21 -0300

    My Jetpack: If needs purchase, request connection first

--- projects/packages/my-jetpack/_inc/components/connected-product-card/index.jsx
+++ projects/packages/my-jetpack/_inc/components/connected-product-card/index.jsx
@@ -1,15 +1,18 @@
 import { getIconBySlug } from '@automattic/jetpack-components';
+import { useConnection } from '@automattic/jetpack-connection';
 import PropTypes from 'prop-types';
 import React, { useCallback } from 'react';
 import useMyJetpackNavigate from '../../hooks/use-my-jetpack-navigate';
 import { useProduct } from '../../hooks/use-product';
-import ProductCard from '../product-card';
+import ProductCard, { PRODUCT_STATUSES } from '../product-card';
 
 const ConnectedProductCard = ( { admin, slug } ) => {
+	const { isRegistered, isUserConnected } = useConnection();
 	const { detail, status, activate, deactivate, isFetching } = useProduct( slug );
 	const { name, description, manageUrl } = detail;
 
 	const navigateToConnectionPage = useMyJetpackNavigate( '/connection' );
+
 	const navigateToAddProductPage = useMyJetpackNavigate( `add-${ slug }` );
 
 	/*
@@ -18,6 +21,26 @@ const ConnectedProductCard = ( { admin, slug } ) => {
 	const onManage = useCallback( () => {
 		window.location = manageUrl;
 	}, [ manageUrl ] );
+
+	/*
+	 * Redirect only if connected
+	 */
+	const callOnlyIfAllowed = callback => () => {
+		if (
+			status !== PRODUCT_STATUSES.NEEDS_PURCHASE &&
+			status !== PRODUCT_STATUSES.NEEDS_PURCHASE_OR_FREE
+		) {
+			callback();
+			return;
+		}
+
+		if ( isRegistered && isUserConnected ) {
+			callback();
+			return;
+		}
+
+		navigateToConnectionPage();
+	};
 
 	const Icon = getIconBySlug( slug );
 
@@ -32,9 +55,9 @@ const ConnectedProductCard = ( { admin, slug } ) => {
 			onDeactivate={ deactivate }
 			slug={ slug }
 			onActivate={ activate }
-			onAdd={ navigateToAddProductPage }
-			onFixConnection={ navigateToConnectionPage }
+			onAdd={ callOnlyIfAllowed( navigateToAddProductPage ) }
 			onManage={ onManage }
+			onFixConnection={ navigateToConnectionPage }
 		/>
 	);
 };
