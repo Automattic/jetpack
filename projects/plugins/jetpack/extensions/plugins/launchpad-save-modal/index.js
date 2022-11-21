df951d8db1b719af74d7385eaabf93e0d22c1b75
Sharpirate <stoynov.bogomil@gmail.com>
2022-11-21 15:04:57 -0700

    Blocks: add new Launchpad modal
    - Initial plugin setup and iterations based on feedback on see #27538

---
+++ projects/plugins/jetpack/extensions/plugins/launchpad-save-modal/index.js
@@ -0,0 +1,94 @@
+/* global launchpadModalOptions */
+
+import { Modal, Button, CheckboxControl } from '@wordpress/components';
+import { usePrevious } from '@wordpress/compose';
+import { useSelect } from '@wordpress/data';
+import { store as editorStore } from '@wordpress/editor';
+import { __ } from '@wordpress/i18n';
+import { useEffect, useState } from 'react';
+import './editor.scss';
+
+export const name = 'launchpad-save-modal';
+
+export const settings = {
+	render: function LaunchpadSaveModal() {
+		const isSaving = useSelect(
+			select => select( editorStore ).isSavingNonPostEntityChanges(),
+			[]
+		);
+		const prevIsSaving = usePrevious( isSaving );
+		const [ isModalOpen, setIsModalOpen ] = useState( false );
+		const [ dontShowAgain, setDontShowAgain ] = useState( false );
+		const [ isChecked, setIsChecked ] = useState( false );
+		const [ , siteSlug ] = launchpadModalOptions.siteUrlOption.split( '//' );
+		const isInsideSiteEditor = window.location.href.includes( 'site-editor' );
+
+		useEffect( () => {
+			if ( prevIsSaving === true && isSaving === false ) {
+				setIsModalOpen( true );
+			}
+		}, [ isSaving, prevIsSaving ] );
+
+		const showModal =
+			launchpadModalOptions.launchpadScreenOption === 'full' &&
+			launchpadModalOptions.siteIntentOption === 'link-in-bio' &&
+			isInsideSiteEditor &&
+			! dontShowAgain &&
+			isModalOpen;
+
+		return (
+			showModal && (
+				<Modal
+					isDismissible={ true }
+					className="launchpad__save-modal"
+					onRequestClose={ () => {
+						setIsModalOpen( false );
+						setDontShowAgain( isChecked );
+					} }
+				>
+					<div className="launchpad__save-modal-body">
+						<div className="launchpad__save-modal-text">
+							<h1 className="launchpad__save-modal-heading">
+								{ __( 'Your site is ready to launch!', 'jetpack' ) }
+							</h1>
+							<p className="launchpad__save-modal-message">
+								{ __(
+									'Launching your Link in Bio will allow you to share a link with others and promote your site.',
+									'jetpack'
+								) }
+							</p>
+						</div>
+						<div className="launchpad__save-modal-controls">
+							<CheckboxControl
+								label={ __( "Don't show this again.", 'jetpack' ) }
+								checked={ isChecked }
+								onChange={ () => setIsChecked( ! isChecked ) }
+							/>
+							<div className="launchpad__save-modal-buttons">
+								<Button
+									size="normal"
+									variant="secondary"
+									onClick={ () => {
+										setDontShowAgain( isChecked );
+										setIsModalOpen( false );
+									} }
+								>
+									{ __( 'Back to Edit', 'jetpack' ) }
+								</Button>
+								<Button
+									size="normal"
+									variant="primary"
+									onClick={ () => {
+										window.top.location.href = `https://www.wordpress.com/setup/link-in-bio/launchpad?siteSlug=${ siteSlug }`;
+									} }
+								>
+									{ __( 'Next Steps', 'jetpack' ) }
+								</Button>
+							</div>
+						</div>
+					</div>
+				</Modal>
+			)
+		);
+	},
+};
---
+++ projects/plugins/jetpack/extensions/plugins/launchpad-save-modal/launchpad-save-modal.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Launchpad Save Modal
+ *
+ * @package automattic/jetpack
+ */
+
+// Feature name.
+const FEATURE_NAME = 'launchpad-save-modal';
+
+function enque_script() {
+	wp_enqueue_script(
+		'launchpad-save-modal',
+		plugins_url( 'index.js', __FILE__ ),
+		array(),
+		JETPACK__VERSION,
+		true
+	);
+
+	wp_add_inline_script(
+		'launchpad-save-modal',
+		'const launchpadModalOptions = ' . wp_json_encode(
+			array(
+				'launchpadScreenOption' => get_option( 'launchpad_screen' ),
+				'siteUrlOption'         => get_option( 'siteurl' ),
+				'siteIntentOption'      => get_option( 'site_intent' ),
+			)
+		),
+		'before'
+	);
+}
+
+add_action( 'enqueue_block_editor_assets', 'enque_script' );
+
+// Populate the available extensions with launchpad-save-modal.
+add_filter(
+	'jetpack_set_available_extensions',
+	function ( $extensions ) {
+		return array_merge(
+			$extensions,
+			array(
+				FEATURE_NAME,
+			)
+		);
+	}
+);
+
+// Set the launchpad-save-modal availability, depending on the site plan.
+add_action(
+	'jetpack_register_gutenberg_extensions',
+	function () {
+		\Jetpack_Gutenberg::set_extension_available( FEATURE_NAME );
+	}
+);
