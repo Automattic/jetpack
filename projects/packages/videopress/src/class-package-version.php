70faf29d2ce6d1f66952d0e2f69bb3e4028dbff5
Douglas <douglas.henri@automattic.com>
2022-11-22 18:30:27 -0300

    bump version

--- projects/packages/videopress/src/class-package-version.php
+++ projects/packages/videopress/src/class-package-version.php
@@ -11,7 +11,7 @@ namespace Automattic\Jetpack\VideoPress;
  * The Package_Version class.
  */
 class Package_Version {
-	const PACKAGE_VERSION = '0.8.1';
+	const PACKAGE_VERSION = '0.8.2-alpha';
 
 	const PACKAGE_SLUG = 'videopress';
 
