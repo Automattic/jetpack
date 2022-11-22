23dd49286100dbed47476aab444951ee7895fe63
bindlegirl <1242807+bindlegirl@users.noreply.github.com>
2022-11-22 15:15:53 +0100

    Update plugin-deactivation package

--- projects/packages/plugin-deactivation/src/class-deactivation-handler.php
+++ projects/packages/plugin-deactivation/src/class-deactivation-handler.php
@@ -21,7 +21,7 @@ class Deactivation_Handler {
 	 *
 	 * @var string
 	 */
-	const PACKAGE_VERSION = '0.1.0';
+	const PACKAGE_VERSION = '0.1.1';
 
 	/**
 	 * Slug of the plugin to intercept deactivation for.
