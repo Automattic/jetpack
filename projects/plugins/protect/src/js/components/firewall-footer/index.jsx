dc04321e0ffffacde1c3775b31c902e043645f68
dkmyta <dkmyta10@gmail.com>
2022-11-21 19:14:17 -0800

    [not verified] Update FirewallFooter to use SeventyFiveLayout

--- projects/plugins/protect/src/js/components/firewall-footer/index.jsx
+++ projects/plugins/protect/src/js/components/firewall-footer/index.jsx
@@ -1,11 +1,12 @@
-import { AdminSectionHero, Container, Col, Text, Button } from '@automattic/jetpack-components';
+import { AdminSectionHero, Title, Text, Button } from '@automattic/jetpack-components';
 import { CheckboxControl } from '@wordpress/components';
 import { useDispatch } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 import { STORE_ID } from '../../state/store';
+import SeventyFiveLayout from '../seventy-five-layout';
 import styles from './styles.module.scss';
 
-const FirewallFooter = () => {
+const StandaloneMode = () => {
 	const { setModal } = useDispatch( STORE_ID );
 
 	const handleClick = () => {
@@ -18,46 +19,51 @@ const FirewallFooter = () => {
 	};
 
 	return (
-		<AdminSectionHero>
-			<Container
-				className={ styles[ 'firewall-footer' ] }
-				horizontalSpacing={ 6 }
-				horizontalGap={ 0 }
+		<div className={ styles[ 'standalone-mode-section' ] }>
+			<Title>{ __( 'Standalone mode', 'jetpack-protect' ) }</Title>
+			<Text mb={ 3 }>
+				{ __(
+					'Learn how you can execute the firewall before WordPress initializes. This mode offers the most protection.',
+					'jetpack-protect'
+				) }
+			</Text>
+			<Button
+				variant={ 'link' }
+				isExternalLink={ true }
+				weight={ 'regular' }
+				onClick={ handleClick() }
 			>
-				<Col>
-					<Text variant={ 'title-medium-semi-bold' } mb={ 2 }>
-						{ __( 'Standalone mode', 'jetpack-protect' ) }
-					</Text>
-					<Text mb={ 2 }>
-						{ __(
-							'Learn how you can execute the firewall before WordPress initializes. This mode offers the most protection.',
-							'jetpack-protect'
-						) }
-					</Text>
-					<Button
-						variant={ 'link' }
-						isExternalLink={ true }
-						weight={ 'regular' }
-						onClick={ handleClick() }
-					>
-						{ __( 'Learn more', 'jetpack-protect' ) }
-					</Button>
-				</Col>
-				<Col>
-					<Text variant={ 'title-medium-semi-bold' } mb={ 2 }>
-						{ __( ' Share data with Jetpack', 'jetpack-protect' ) }
-					</Text>
-					<div className={ styles[ 'footer-checkbox' ] }>
-						<CheckboxControl checked={ true } />
-						<Text>
-							{ __(
-								'Allow Jetpack to collect data to improve firewall protection and rules. Collected data is also used to display advanced usage metrics.',
-								'jetpack-protect'
-							) }
-						</Text>
-					</div>
-				</Col>
-			</Container>
+				{ __( 'Learn more', 'jetpack-protect' ) }
+			</Button>
+		</div>
+	);
+};
+
+const ShareData = () => {
+	return (
+		<div className={ styles[ 'share-data-section' ] }>
+			<Title>{ __( ' Share data with Jetpack', 'jetpack-protect' ) }</Title>
+			<div className={ styles[ 'footer-checkbox' ] }>
+				<CheckboxControl checked={ true } />
+				<Text>
+					{ __(
+						'Allow Jetpack to collect data to improve firewall protection and rules. Collected data is also used to display advanced usage metrics.',
+						'jetpack-protect'
+					) }
+				</Text>
+			</div>
+		</div>
+	);
+};
+
+const FirewallFooter = () => {
+	return (
+		<AdminSectionHero>
+			<SeventyFiveLayout
+				main={ <StandaloneMode /> }
+				secondary={ <ShareData /> }
+				preserveSecondaryOnMobile={ true }
+			/>
 		</AdminSectionHero>
 	);
 };
