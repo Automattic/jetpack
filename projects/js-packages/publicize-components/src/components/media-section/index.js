2bdc5c20a6f9e3ab9b63609c341dcf5428c6c262
Gergely Juhasz <gergely.juhasz@automattic.com>
2022-11-22 15:20:12 +0100

    Create POC for image restrictions

--- projects/js-packages/publicize-components/src/components/media-section/index.js
+++ projects/js-packages/publicize-components/src/components/media-section/index.js
@@ -1,10 +1,22 @@
 import { MediaUpload, MediaUploadCheck } from '@wordpress/block-editor';
-import { Button, ResponsiveWrapper, Spinner } from '@wordpress/components';
+import { Button, ResponsiveWrapper, Spinner, Notice } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { useCallback } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import { useEffect, useState } from 'react';
 import useAttachedMedia from '../../hooks/use-attached-media';
+import useSocialMediaConnections from '../../hooks/use-social-media-connections';
 import styles from './styles.module.scss';
+
+// These will come from the server
+const RESTRICTIONS = {
+	twitter: {
+		maxImageSize: 2000000,
+	},
+	facebook: {
+		maxImageSize: 1000000,
+	},
+};
 
 /**
  * Get relevant details from a WordPress media object.
@@ -42,17 +54,44 @@ const REMOVE_MEDIA_LABEL = __( 'Remove Social Image', 'jetpack' );
 
 const ALLOWED_MEDIA_TYPES = [ 'image/jpeg', 'image/png' ];
 
+const getMaxImageSize = ( restrictions, connections ) => {
+	return Math.min(
+		...connections.map( connection => restrictions[ connection.service_name ].maxImageSize )
+	);
+};
+
+const isMediaObjectValid = ( mediaObject, maxImageSize ) => {
+	if ( mediaObject?.media_details?.filesize <= maxImageSize ) {
+		return true;
+	}
+	return false;
+};
+
+const formatMaxSize = maxImageSize => ` (${ maxImageSize / Math.pow( 10, 6 ) } Mb)`;
+
 /**
  * Wrapper that handles media-related functionality.
  *
  * @returns {object} The media section.
  */
 export default function MediaSection() {
+	const [ showNotice, setShotNotice ] = useState( false );
 	const { attachedMedia, updateAttachedMedia } = useAttachedMedia();
+	const { connections } = useSocialMediaConnections();
+	const enabledConnections = connections.filter( connection => connection.enabled );
+	const maxImageSize = getMaxImageSize( RESTRICTIONS, enabledConnections );
 
 	const mediaObject = useSelect( select =>
 		select( 'core' ).getMedia( attachedMedia[ 0 ] || null, { context: 'view' } )
 	);
+
+	useEffect( () => {
+		// Removes selected media if connection change results in invalid image
+		if ( mediaObject && ! isMediaObjectValid( mediaObject, maxImageSize ) ) {
+			updateAttachedMedia( [] );
+			setShotNotice( true );
+		}
+	}, [ updateAttachedMedia, maxImageSize, mediaObject ] );
 
 	const { mediaWidth, mediaHeight, mediaSourceUrl } = getMediaDetails( mediaObject );
 
@@ -65,9 +104,17 @@ export default function MediaSection() {
 				return;
 			}
 
+			// Do not select media if criteria is not met
+			if ( media.filesizeInBytes > getMaxImageSize( RESTRICTIONS, enabledConnections ) ) {
+				updateAttachedMedia( [] );
+				setShotNotice( true );
+				return;
+			}
+
 			updateAttachedMedia( [ media.id ] );
+			setShotNotice( false );
 		},
-		[ updateAttachedMedia ]
+		[ updateAttachedMedia, enabledConnections ]
 	);
 
 	const setMediaRender = useCallback(
@@ -79,11 +126,16 @@ export default function MediaSection() {
 							<img src={ mediaSourceUrl } alt="" />
 						</ResponsiveWrapper>
 					) }
-					{ ! mediaObject && ( attachedMedia.length ? <Spinner /> : ADD_MEDIA_LABEL ) }
+					{ ! mediaObject &&
+						( attachedMedia.length ? (
+							<Spinner />
+						) : (
+							ADD_MEDIA_LABEL + formatMaxSize( maxImageSize )
+						) ) }
 				</Button>
 			</div>
 		),
-		[ mediaHeight, mediaObject, mediaSourceUrl, mediaWidth, attachedMedia ]
+		[ mediaHeight, mediaObject, mediaSourceUrl, mediaWidth, attachedMedia, maxImageSize ]
 	);
 
 	const replaceMediaRender = useCallback(
@@ -95,11 +147,13 @@ export default function MediaSection() {
 		[]
 	);
 
+	const onDismissClick = useCallback( () => setShotNotice( false ), [] );
+
 	return (
 		<div className={ styles.wrapper }>
 			<MediaUploadCheck>
 				<MediaUpload
-					title={ ADD_MEDIA_LABEL }
+					title={ ADD_MEDIA_LABEL + formatMaxSize( maxImageSize ) }
 					onSelect={ onUpdateMedia }
 					allowedTypes={ ALLOWED_MEDIA_TYPES }
 					render={ setMediaRender }
@@ -117,6 +171,16 @@ export default function MediaSection() {
 							{ REMOVE_MEDIA_LABEL }
 						</Button>
 					</>
+				) }
+				{ showNotice && (
+					<Notice
+						className={ styles.notice }
+						isDismissible={ true }
+						onDismiss={ onDismissClick }
+						status="warning"
+					>
+						<p>{ 'The selected media it is too big for one of the selected platforms.' }</p>
+					</Notice>
 				) }
 			</MediaUploadCheck>
 		</div>
