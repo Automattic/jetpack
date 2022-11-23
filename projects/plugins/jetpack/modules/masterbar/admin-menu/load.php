3fb8df7cdcd9561159c7ec6d9985233bb94114e0
Dehan <dehandecroos@gmail.com>
2022-11-23 14:51:27 +0400

    Add class exist check

--- projects/plugins/jetpack/modules/masterbar/admin-menu/load.php
+++ projects/plugins/jetpack/modules/masterbar/admin-menu/load.php
@@ -50,7 +50,7 @@ function get_admin_menu_class() {
 		// DIFM Lite In Progress Atomic Sites. Uses the same menu used for domain-only sites.
 		// Ignore this check if we are in a support session.
 		$is_difm_lite_in_progress = wpcomsh_is_site_sticker_active( 'difm-lite-in-progress' );
-		$is_support_session       = WPCOMSH_Support_Session_Detect::is_probably_support_session();
+		$is_support_session       = class_exists( 'WPCOMSH_Support_Session_Detect' ) && WPCOMSH_Support_Session_Detect::is_probably_support_session();
 		if ( $is_difm_lite_in_progress && ! $is_support_session ) {
 			require_once __DIR__ . '/class-domain-only-admin-menu.php';
 			return Domain_Only_Admin_Menu::class;
