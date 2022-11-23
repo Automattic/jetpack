d7d0024244ccd2781cf16ba9d84a71b1057a63d9
dkmyta <dkmyta10@gmail.com>
2022-11-23 05:41:27 -0800

    Update StatCard stories

--- projects/plugins/protect/src/js/components/stat-card/stories/index.jsx
+++ projects/plugins/protect/src/js/components/stat-card/stories/index.jsx
@@ -15,39 +15,48 @@ export default {
 };
 
 export const Square = () => {
+	const isSmall = false;
+	const hasRequiredPlan = true;
+
 	const lastThirtyArgs = {
 		icon: <Icon icon={ shield } />,
 		label: 'Blocked requests',
 		period: 'Last 30 days',
 		value: 15,
-		variant: 'square',
-		disabled: false,
+		variant: isSmall ? 'horizontal' : 'square',
+		disabled: hasRequiredPlan ? false : true,
 	};
 
 	return <StatCard { ...lastThirtyArgs } />;
 };
 
 export const Horizontal = () => {
+	const isSmall = true;
+	const hasRequiredPlan = true;
+
 	const allTimeArgs = {
 		icon: <Icon icon={ chartBar } />,
 		label: 'Blocked requests',
 		period: 'All time',
 		value: 1000,
-		variant: 'horizontal',
-		disabled: false,
+		variant: isSmall ? 'horizontal' : 'square',
+		disabled: hasRequiredPlan ? false : true,
 	};
 
 	return <StatCard { ...allTimeArgs } />;
 };
 
 export const Disabled = () => {
+	const isSmall = false;
+	const hasRequiredPlan = false;
+
 	const lastThirtyArgs = {
 		icon: <Icon icon={ shield } />,
 		label: 'Blocked requests',
 		period: 'Last 30 days',
 		value: 0,
-		variant: 'square',
-		disabled: true,
+		variant: isSmall ? 'horizontal' : 'square',
+		disabled: hasRequiredPlan ? false : true,
 	};
 
 	return <StatCard { ...lastThirtyArgs } />;
