6db12552e5c0532541fe49095104199e18777bc6
Tim Broddin <tim@wannabes.be>
2022-11-24 13:53:43 +0100

    Remove the friendly formatting (1000 -> 1K)

--- projects/js-packages/publicize-components/src/components/connection-count/index.js
+++ projects/js-packages/publicize-components/src/components/connection-count/index.js
@@ -1,24 +1,12 @@
 import PropTypes from 'prop-types';
-
 import './style.scss';
-
-// convert to friendly number
-const friendlyNumber = number => {
-	if ( number >= 1000000 ) {
-		return `${ ( number / 1000000 ).toFixed( 1 ) }M`;
-	}
-	if ( number >= 1000 ) {
-		return `${ ( number / 1000 ).toFixed( 1 ) }K`;
-	}
-	return number;
-};
 
 const ConnectionCount = props => {
 	const { followerCount } = props;
 
 	return (
 		<span className="jetpack-publicize-connection-label-follower-count">
-			{ followerCount ? friendlyNumber( followerCount ) : null }
+			{ followerCount ? followerCount.toLocaleString() : null }
 		</span>
 	);
 };
