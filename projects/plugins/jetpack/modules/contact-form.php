32c504e36f4a5d2d3bf07ef4cf86478ab393c679
João Heringer <joao.heringer@automattic.com>
2022-11-25 18:27:49 -0300

    Change Newsletter pattern background color

--- projects/plugins/jetpack/modules/contact-form.php
+++ projects/plugins/jetpack/modules/contact-form.php
@@ -145,9 +145,9 @@ function jetpack_form_register_pattern() {
 					<span aria-hidden="true" class="wp-block-cover__background has-secondary-background-color has-background-dim-0 has-background-dim"></span>
 					<img class="wp-block-cover__image-background wp-image-616" alt="contemporary art" src="https://atomicsitecrowdsignal.wpcomstaging.com/wp-content/uploads/2022/11/pexels-photo-2838545.jpeg" data-object-fit="cover"/>
 					<div class="wp-block-cover__inner-container">
-						<!-- wp:cover {"overlayColor":"background","minHeight":318,"minHeightUnit":"px","isDark":false,"align":"center","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}}}} -->
+						<!-- wp:cover {"customOverlayColor":"#FFF","minHeight":318,"minHeightUnit":"px","isDark":false,"align":"center","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}}}} -->
 						<div class="wp-block-cover aligncenter is-light" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50);min-height:318px">
-							<span aria-hidden="true" class="wp-block-cover__background has-background-background-color has-background-dim-100 has-background-dim"></span>
+							<span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim" style="background-color:#FFF"></span>
 							<div class="wp-block-cover__inner-container">
 								<!-- wp:columns {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
 								<div class="wp-block-columns" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
