7a13555dd50d4e99ad214bdaac65063a179a62ba
Samiff <samiff@users.noreply.github.com>
2022-11-28 09:47:36 -0700

    Refactor SeoNoindexPanel to use withSeoHelper

--- projects/plugins/jetpack/extensions/blocks/seo/noindex-panel.js
+++ projects/plugins/jetpack/extensions/blocks/seo/noindex-panel.js
@@ -1,15 +1,12 @@
 import { CheckboxControl } from '@wordpress/components';
-import { useDispatch, useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 import React from 'react';
+import { withSeoHelper } from './with-seo-helper';
 
-const SeoNoindexPanel = () => {
-	const { postMeta } = useSelect( select => {
-		return {
-			postMeta: select( 'core/editor' ).getEditedPostAttribute( 'meta' ),
-		};
-	} );
-	const { editPost } = useDispatch( 'core/editor', [ postMeta.jetpack_seo_noindex ] );
+const SeoNoindexPanel = ( { metaValue, updateMetaValue } ) => {
+	const onCheckboxChange = value => {
+		updateMetaValue( value );
+	};
 
 	return (
 		<CheckboxControl
@@ -18,10 +15,10 @@ const SeoNoindexPanel = () => {
 				"If selected, a 'noindex' tag will help instruct search engines to not include this page in search results. This page will also be removed from the Jetpack sitemap.",
 				'jetpack'
 			) }
-			checked={ !! postMeta.jetpack_seo_noindex }
-			onChange={ value => editPost( { meta: { jetpack_seo_noindex: value } } ) }
+			checked={ !! metaValue }
+			onChange={ onCheckboxChange }
 		/>
 	);
 };
 
-export default SeoNoindexPanel;
+export default withSeoHelper( 'jetpack_seo_noindex' )( SeoNoindexPanel );
