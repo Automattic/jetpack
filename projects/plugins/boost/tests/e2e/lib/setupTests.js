ec68787ddc7a8a6fa5ded8e1f8cf2a47fb3eb5b5
Mark George <thingalon@gmail.com>
2022-11-29 14:54:50 +1100

    Remove Jetpack before running Boost e2e

--- projects/plugins/boost/tests/e2e/lib/setupTests.js
+++ projects/plugins/boost/tests/e2e/lib/setupTests.js
@@ -5,7 +5,11 @@ import { boostPrerequisitesBuilder } from './env/prerequisites.js';
 export default async function () {
 	const browser = await chromium.launch();
 	const page = await browser.newPage();
-	await prerequisitesBuilder( page ).withLoggedIn( true ).withActivePlugins( [ 'boost' ] ).build();
+	await prerequisitesBuilder( page )
+		.withLoggedIn( true )
+		.withActivePlugins( [ 'boost' ] )
+		.withInactivePlugins( [ 'jetpack' ] )
+		.build();
 	await boostPrerequisitesBuilder( page )
 		.withCleanEnv( true )
 		.withSpeedScoreMocked( true )
