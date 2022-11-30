ce0d7ba63b36019e1284bae1b7d9e744fe776adf
Dusty Reagan <dusty@dustyreagan.com>
2022-11-30 12:13:09 -0600

    Update variable name and comment https://github.com/Automattic/wpcomsh/pull/1170
    * Update variable name and comment
    
    * Bump version number

--- projects/plugins/wpcomsh/frontend-notices/gifting-banner/gifting-banner.php
+++ projects/plugins/wpcomsh/frontend-notices/gifting-banner/gifting-banner.php
@@ -26,16 +26,16 @@ class Gifting_Banner {
 	 * Maybe show the gifting banner for the current site.
 	 */
 	public function maybe_show_gifting_banner() {
-		$blog_id         = null;
+		$_blog_id        = null;
 		$jetpack_options = get_option( 'jetpack_options' );
 		if ( $jetpack_options ) {
-			$blog_id = (int) $jetpack_options['id'];
+			$_blog_id = (int) $jetpack_options['id'];
 		}
 
-		// Hide the gifting banner behind a8c proxy.
+		// Expose the gifting banner to 10% of users or if behind the a8c proxy.
 		if ( ( defined( 'AT_PROXIED_REQUEST' ) && AT_PROXIED_REQUEST ) ||
 			( defined( 'A8C_PROXIED_REQUEST' ) && A8C_PROXIED_REQUEST ) ||
-			( ! is_null( $blog_id ) && 0 === $blog_id % 10 ) ) {
+			( ! is_null( $_blog_id ) && 0 === $_blog_id % 10 ) ) {
 
 			if ( $this->should_display_expiring_plan_notice() ) {
 				// Inject the gifting banner after the launch banner.
