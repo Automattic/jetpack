6033339ba56bf106b2a3e53c813cb3da911a3f99
Jasper Kang <kangzjnet@gmail.com>
2022-12-01 11:10:23 +1300

    [not verified] remove unnecessary endpoints

--- projects/packages/stats-admin/src/class-rest-controller.php
+++ projects/packages/stats-admin/src/class-rest-controller.php
@@ -1,13 +1,14 @@
 <?php
 /**
  * The Stats Rest Controller class.
- * Registers the REST routes for Stats.
+ * Registers the REST routes for Calypso Stats.
  *
  * @package automattic/jetpack-stats-admin
  */
 
 namespace Automattic\Jetpack\Stats_Admin;
 
+use Automattic\Jetpack\Connection\Client;
 use Automattic\Jetpack\Stats\WPCOM_Stats;
 use Jetpack_Options;
 use WP_Error;
@@ -53,7 +54,7 @@ class REST_Controller {
 			sprintf( '/sites/%d/stats/(?P<resource>[\-\w]+)/(?P<resource_id>[\d]+)', Jetpack_Options::get_option( 'id' ) ),
 			array(
 				'methods'             => WP_REST_Server::READABLE,
-				'callback'            => array( $this, 'get_stats_single_resource_from_wpcom' ),
+				'callback'            => array( $this, 'get_single_resource_stats' ),
 				'permission_callback' => array( $this, 'check_user_privileges_callback' ),
 			)
 		);
@@ -91,35 +92,24 @@ class REST_Controller {
 			)
 		);
 
-		// Required site endpoints for the Stats app.
+		// General stats for the site.
 		register_rest_route(
 			static::$namespace,
-			sprintf( '/sites/%d/(?P<resource>[\-\w]+)', Jetpack_Options::get_option( 'id' ) ),
+			sprintf( '/sites/%d/stats', Jetpack_Options::get_option( 'id' ) ),
 			array(
 				'methods'             => WP_REST_Server::READABLE,
-				'callback'            => array( $this, 'get_site_resource_from_wpcom' ),
+				'callback'            => array( $this, 'get_site_stats' ),
 				'permission_callback' => array( $this, 'check_user_privileges_callback' ),
 			)
 		);
 
-		// TODO remove call to the endpoint from the frontend.
+		// Whether site has never published post / page.
 		register_rest_route(
 			static::$namespace,
-			sprintf( '/jetpack-blogs/%d/rest-api/', Jetpack_Options::get_option( 'id' ) ),
+			sprintf( '/sites/%d/site-has-never-published-post', Jetpack_Options::get_option( 'id' ) ),
 			array(
 				'methods'             => WP_REST_Server::READABLE,
-				'callback'            => array( $this, 'empty_result' ),
-				'permission_callback' => array( $this, 'check_user_privileges_callback' ),
-			)
-		);
-
-		// TODO remove call to the endpoint from the frontend.
-		register_rest_route(
-			static::$namespace,
-			'/me/connections',
-			array(
-				'methods'             => WP_REST_Server::READABLE,
-				'callback'            => array( $this, 'empty_result' ),
+				'callback'            => array( $this, 'site_has_never_published_post' ),
 				'permission_callback' => array( $this, 'check_user_privileges_callback' ),
 			)
 		);
@@ -242,7 +232,7 @@ class REST_Controller {
 	 * @param WP_REST_Request $req The request object.
 	 * @return array
 	 */
-	public function get_stats_single_resource_from_wpcom( $req ) {
+	public function get_single_resource_stats( $req ) {
 		switch ( $req->get_param( 'resource' ) ) {
 			case 'post':
 				return $this->wpcom_stats->get_post_views(
@@ -262,7 +252,7 @@ class REST_Controller {
 	}
 
 	/**
-	 * Site Resource endpoint.
+	 * Get brief information for a single post.
 	 *
 	 * @param WP_REST_Request $req The request object.
 	 * @return array
@@ -282,77 +272,85 @@ class REST_Controller {
 	}
 
 	/**
-	 * Returns an empty object.
-	 *
-	 * @return object
-	 */
-	public function empty_result() {
-		return json_decode( '{}' );
-	}
-
-	/**
-	 * Get a resource under site.
+	 * Get site stats.
 	 *
 	 * @param WP_REST_Request $req The request object.
 	 * @return array
 	 */
-	public function get_site_resource_from_wpcom( $req ) {
-		$resource = $req->get_param( 'resource' );
-		switch ( $resource ) {
-			case 'site-has-never-published-post':
-				return $this->get_has_never_published_post( $req );
-
-			// TODO: remove the following calls from the frontend.
-			case 'posts':
-			case 'sharing-buttons':
-			case 'plugins':
-			case 'keyrings':
-			case 'rewind':
-				return $this->empty_result();
-
-			// General stats endpiont.
-			case 'stats':
-				return $this->wpcom_stats->get_stats( $req->get_params() );
-
-			default:
-				return $this->get_forbidden_error();
-		}
+	public function get_site_stats( $req ) {
+		return $this->wpcom_stats->get_stats( $req->get_params() );
 	}
 
 	/**
-	 * Stolen from `wp-content/rest-api-plugins/endpoints/sites-has-never-published-post.php`
+	 * Whether site has never published post.
 	 *
 	 * @param WP_REST_Request $req The request object.
-	 *
-	 * @return bool the value of has ever published post
+	 * @return array
 	 */
-	protected function get_has_never_published_post( $req ) {
-		$has_never_published_post = (bool) get_option( 'has_never_published_post', false );
+	public function site_has_never_published_post( $req ) {
+		return $this->request_as_blog_cached(
+			sprintf(
+				'/sites/%d/site-has-never-published-post?%s',
+				Jetpack_Options::get_option( 'id' ),
+				http_build_query(
+					$req->get_params()
+				)
+			),
+			'v2',
+			array( 'timeout' => 5 ),
+			null,
+			'wpcom'
+		);
+	}
 
-		if ( ! $has_never_published_post ) {
-			return false;
-		}
+	/**
+	 * Query the WordPress.com REST API using the blog token
+	 *
+	 * @param String $path The API endpoint relative path.
+	 * @param String $version The API version.
+	 * @param array  $args Request arguments.
+	 * @param String $body Request body.
+	 * @param String $base_api_path (optional) the API base path override, defaults to 'rest'.
+	 * @param bool   $use_cache (optional) default to true.
+	 * @return array|WP_Error $response Data.
+	 */
+	protected function request_as_blog_cached( $path, $version = '1.1', $args = array(), $body = null, $base_api_path = 'rest', $use_cache = true ) {
+		// Arrays are serialized without considering the order of objects, but it's okay atm.
+		$cache_key = 'STATS_REST_RESP_' . md5( implode( '|', array( $path, $version, wp_json_encode( $args ), wp_json_encode( $body ), $base_api_path ) ) );
 
-		$include_pages = $req->get_param( 'include_pages' );
-		if ( $include_pages ) {
-			$has_never_published_page = true;
-			$pages                    = get_pages();
-			// 20 is a threshold, We are assuming that there won't be more than 20 head start pages.
-			if ( count( $pages ) <= 20 ) {
-				foreach ( $pages as $page ) {
-					$is_headstart_post = ! empty( get_post_meta( $page->ID, '_headstart_post' ) );
-					if ( ! $is_headstart_post ) {
-						$has_never_published_page = false;
-						break;
-					}
-				}
-			} else {
-				$has_never_published_page = false;
+		if ( $use_cache ) {
+			$response_body = get_transient( $cache_key );
+			if ( false !== $response_body ) {
+				return json_decode( $response_body, true );
 			}
-			return rest_ensure_response( $has_never_published_post && $has_never_published_page );
 		}
 
-		return rest_ensure_response( $has_never_published_post );
+		$response              = Client::wpcom_json_api_request_as_blog(
+			$path,
+			$version,
+			$args,
+			$body,
+			$base_api_path
+		);
+		$response_code         = wp_remote_retrieve_response_code( $response );
+		$response_body_content = wp_remote_retrieve_body( $response );
+		$response_body         = json_decode( $response_body, true );
+
+		if ( is_wp_error( $response ) ) {
+			return $response;
+		}
+
+		if ( 200 !== $response_code ) {
+			return new WP_Error(
+				isset( $response_body['error'] ) ? 'remote-error-' . $response_body['error'] : 'remote-error',
+				isset( $response_body['message'] ) ? $response_body['message'] : 'unknown remote error',
+				array( 'status' => $response_code )
+			);
+		}
+
+		// Cache the successful JSON response for 5 minutes.
+		set_transient( $cache_key, $response_body_content, 5 * MINUTE_IN_SECONDS );
+		return $response_body;
 	}
 
 	/**
