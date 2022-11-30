0b6321c9b730593b3ea8315d7127116abe7af875
Luiz Kowalski <lhkowalski@gmail.com>
2022-11-30 18:59:30 -0300

    Set redirect_to query parameter on the Purchase a Plan link

--- projects/packages/my-jetpack/_inc/utils/get-purchase-plan-url.js
+++ projects/packages/my-jetpack/_inc/utils/get-purchase-plan-url.js
@@ -11,5 +11,8 @@ import { MY_JETPACK_MY_PLANS_PURCHASE_SOURCE } from '../constants';
  */
 export default function () {
 	const site = window?.myJetpackInitialState?.siteSuffix;
-	return getRedirectUrl( MY_JETPACK_MY_PLANS_PURCHASE_SOURCE, { site } );
+	const query = window?.myJetpackInitialState?.topJetpackMenuItemUrl
+		? `redirect_to=${ window?.myJetpackInitialState?.topJetpackMenuItemUrl }`
+		: null;
+	return getRedirectUrl( MY_JETPACK_MY_PLANS_PURCHASE_SOURCE, { site, query } );
 }
