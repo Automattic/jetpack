a141e566a602dd21e509ed7264e726bb88feaf2b
Konstantin Obenland <obenland@gmx.de>
2022-11-30 16:18:51 -0700

    Version bump v3.4.27 https://github.com/Automattic/wpcomsh/pull/1171

--- projects/plugins/wpcomsh/wpcomsh.php
+++ projects/plugins/wpcomsh/wpcomsh.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WordPress.com Site Helper
  * Description: A helper for connecting WordPress.com sites to external host infrastructure.
- * Version: 3.4.26
+ * Version: 3.4.27
  * Author: Automattic
  * Author URI: http://automattic.com/
  *
@@ -10,7 +10,7 @@
  */
 
 // Increase version number if you change something in wpcomsh.
-define( 'WPCOMSH_VERSION', '3.4.26' );
+define( 'WPCOMSH_VERSION', '3.4.27' );
 
 // If true, Typekit fonts will be available in addition to Google fonts
 add_filter( 'jetpack_fonts_enable_typekit', '__return_true' );
