26d1eaaa7ecf2680080cc24d68a454c82c8b5f2a
Vassilis Dimitrakis <vassilis.dimitrakis@automattic.com>
2022-11-30 21:23:12 +0200

    Test commit

--- projects/plugins/jetpack/extensions/plugins/post-publish-promote-post-panel/post-publish-promote-post-panel.php
+++ projects/plugins/jetpack/extensions/plugins/post-publish-promote-post-panel/post-publish-promote-post-panel.php
@@ -10,38 +10,25 @@ namespace Automattic\Jetpack\Extensions\PromotePosts;
 // Feature name.
 const FEATURE_NAME = 'post-publish-promote-post-panel';
 
-use Automattic\Jetpack\Connection\Client;
-
-$response = Client::wpcom_json_api_request_as_blog( '/me', '1.1' );
-// if ( is_wp_error( $response ) ) {
-// return $response;
-// }
-//
-// $body = json_decode( wp_remote_retrieve_body( $response ) );
-//
-echo '<pre>';
-var_dump( $response );
-echo '</pre>';
-
-// return;
-
 // Populate the available extensions with post-publish-promote-post-panel.
-add_filter(
-	'jetpack_set_available_extensions',
-	function ( $extensions ) {
-		return array_merge(
-			$extensions,
-			array(
-				FEATURE_NAME,
-			)
-		);
-	}
-);
+if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
+	add_filter(
+		'jetpack_set_available_extensions',
+		function ( $extensions ) {
+			return array_merge(
+				$extensions,
+				array(
+					FEATURE_NAME,
+				)
+			);
+		}
+	);
 
-// Set the  post-publish-promote-post-panel availability, depending on the site plan.
-add_action(
-	'jetpack_register_gutenberg_extensions',
-	function () {
-		\Jetpack_Gutenberg::set_extension_available( FEATURE_NAME );
-	}
-);
+	// Set the  post-publish-promote-post-panel availability, depending on the site plan.
+	add_action(
+		'jetpack_register_gutenberg_extensions',
+		function () {
+			\Jetpack_Gutenberg::set_extension_available( FEATURE_NAME );
+		}
+	);
+}
