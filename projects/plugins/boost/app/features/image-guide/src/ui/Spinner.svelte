938283e0321ba31e2e8a0177820cc2d66c79614d
Nauris Pūķis <hi@pyronaur.com>
2022-11-30 17:47:37 +0200

    [not verified] Add a loading spinner

---
+++ projects/plugins/boost/app/features/image-guide/src/ui/Spinner.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import JetpackLogo from './JetpackLogo.svelte';
+	import { fade } from 'svelte/transition';
+</script>
+
+<div
+	class="spinner"
+	out:fade={{ duration: 300 }}
+>
+	<JetpackLogo size={12} bg="transparent" />
+</div>
+
+<style lang="scss">
+	.spinner {
+		animation: ease-in-out 1.25s infinite reverse pulse;
+		@keyframes pulse {
+			0% {
+				transform: scale(1) rotate(0deg);
+			}
+			70% {
+				transform: scale(1.4) rotate(30deg);
+			}
+
+			100% {
+				transform: scale(0.9) rotate(-190deg);
+			}
+		}
+	}
+	.spinner.paused {
+		animation-play-state: paused;
+	}
+</style>
