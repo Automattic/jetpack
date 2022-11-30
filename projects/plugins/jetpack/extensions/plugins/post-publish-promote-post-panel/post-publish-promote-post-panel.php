cf0c15795af4b1f21dae1a3c4803ae0cb4b2fd55
Vassilis Dimitrakis <vassilis.dimitrakis@automattic.com>
2022-11-30 20:45:25 +0200

    Test commit

--- projects/plugins/jetpack/extensions/plugins/post-publish-promote-post-panel/post-publish-promote-post-panel.php
+++ projects/plugins/jetpack/extensions/plugins/post-publish-promote-post-panel/post-publish-promote-post-panel.php
@@ -10,6 +10,21 @@ namespace Automattic\Jetpack\Extensions\PromotePosts;
 // Feature name.
 const FEATURE_NAME = 'post-publish-promote-post-panel';
 
+use Automattic\Jetpack\Connection\Client;
+
+$response = Client::wpcom_json_api_request_as_blog( '/me', '1.1' );
+// if ( is_wp_error( $response ) ) {
+// return $response;
+// }
+//
+// $body = json_decode( wp_remote_retrieve_body( $response ) );
+//
+echo '<pre>';
+var_dump( $response );
+echo '</pre>';
+
+// return;
+
 // Populate the available extensions with post-publish-promote-post-panel.
 add_filter(
 	'jetpack_set_available_extensions',
