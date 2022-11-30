43f57b1112aa459052ed0ee280e1b03de2428fde
Sharpirate <stoynov.bogomil@gmail.com>
2022-11-30 12:44:52 -0800

    [not verified] finalize modal functionality

--- projects/plugins/jetpack/extensions/plugins/launchpad-post-save-modal/index.js
+++ projects/plugins/jetpack/extensions/plugins/launchpad-post-save-modal/index.js
@@ -2,26 +2,30 @@
  * External dependencies
  */
 import { Modal, Button } from '@wordpress/components';
-import { useSelect, useDispatch } from '@wordpress/data';
-import { PluginPostPublishPanel } from '@wordpress/edit-post';
+import { usePrevious } from '@wordpress/compose';
+import { useSelect } from '@wordpress/data';
 import { store as editorStore } from '@wordpress/editor';
 import { __ } from '@wordpress/i18n';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './editor.scss';
 
 export const name = 'launchpad-post-save-modal';
 
 export const settings = {
 	render: function LaunchpadPostSaveModal() {
-		const isPostPublished = useSelect(
-			select => select( editorStore ).isCurrentPostPublished(),
+		const isSaving = useSelect(
+			select => select( editorStore ).isSavingNonPostEntityChanges(),
 			[]
 		);
+		const prevIsSaving = usePrevious( isSaving );
 		const [ showModal, setShowModal ] = useState( false );
+		const siteSlug = window.location.hostname;
 
 		useEffect( () => {
-			setShowModal( true );
-		}, [ isPostPublished ] );
+			if ( prevIsSaving === true && isSaving === false ) {
+				setShowModal( true );
+			}
+		}, [ isSaving, prevIsSaving ] );
 
 		return (
 			showModal && (
@@ -43,11 +47,17 @@ export const settings = {
 							</p>
 						</div>
 						<div className="launchpad__post-save-modal-buttons">
-							<Button size="normal" variant="secondary" onClick={ () => {} }>
+							<Button size="normal" variant="secondary" onClick={ () => setShowModal( false ) }>
 								{ ' ' }
 								Back to Edit{ ' ' }
 							</Button>
-							<Button size="normal" variant="primary" onClick={ () => {} }>
+							<Button
+								size="normal"
+								variant="primary"
+								onClick={ () => {
+									window.top.location.href = `https://www.wordpress.com/setup/link-in-bio/launchpad?siteSlug=${ siteSlug }`;
+								} }
+							>
 								{ ' ' }
 								Launch Site{ ' ' }
 							</Button>
--- projects/plugins/jetpack/extensions/plugins/launchpad-post-save-modal/launchpad-post-save-modal.php
+++ projects/plugins/jetpack/extensions/plugins/launchpad-post-save-modal/launchpad-post-save-modal.php
@@ -1,28 +1,30 @@
 <?php
 /**
- * Block Editor - Republicize feature.
+ * Block Editor - QR Post feature.
  *
  * @package automattic/jetpack
- **/
+ */
 
+// Feature name.
+const FEATURE_NAME = 'launchpad-post-save-modal';
 
-	// Populate the available extensions with launchpad-post-save-modal.
-	add_filter(
-		'jetpack_set_available_extensions',
-		function ( $extensions ) {
-			return array_merge(
-				$extensions,
-				array(
-					'launchpad-post-save-modal',
-				)
-			);
-		}
-	);
+// Populate the available extensions with launchpad-post-save-modal.
+add_filter(
+	'jetpack_set_available_extensions',
+	function ( $extensions ) {
+		return array_merge(
+			$extensions,
+			array(
+				FEATURE_NAME,
+			)
+		);
+	}
+);
 
-	// Set the launchpad-post-save-modal availability, depending on the site plan.
-	add_action(
-		'jetpack_register_gutenberg_extensions',
-		function () {
-			\Jetpack_Gutenberg::set_availability_for_plan( 'launchpad-post-save-modal' );
-		}
-	);
+// Set the launchpad-post-save-modal availability, depending on the site plan.
+add_action(
+	'jetpack_register_gutenberg_extensions',
+	function () {
+		\Jetpack_Gutenberg::set_extension_available( FEATURE_NAME );
+	}
+);
