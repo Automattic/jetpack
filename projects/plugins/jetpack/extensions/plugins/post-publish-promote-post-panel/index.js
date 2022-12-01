98e4635633856f2724c8ff7a4a39b25db0476831
Vassilis Dimitrakis <vassilis.dimitrakis@automattic.com>
2022-12-01 14:48:58 +0200

    Minor fixes

--- projects/plugins/jetpack/extensions/plugins/post-publish-promote-post-panel/index.js
+++ projects/plugins/jetpack/extensions/plugins/post-publish-promote-post-panel/index.js
@@ -1,17 +1,16 @@
 import { JetpackLogo } from '@automattic/jetpack-components';
 import {
-	// getSiteFragment,
+	getSiteFragment,
 	isAtomicSite,
 	isPrivateSite,
 	isSimpleSite,
 } from '@automattic/jetpack-shared-extension-utils';
-import { PanelBody, PanelRow } from '@wordpress/components';
+import { Button, PanelBody, PanelRow } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { PluginPostPublishPanel } from '@wordpress/edit-post';
 import { store as editorStore } from '@wordpress/editor';
 import { __ } from '@wordpress/i18n';
 import JetpackPluginSidebar from '../../shared/jetpack-plugin-sidebar.js';
-import { PromotePostButton } from './components/promote-post.js';
 import './editor.scss';
 
 export const name = 'post-publish-promote-post-panel';
@@ -30,42 +29,37 @@ export const settings = {
 			return select( editorStore ).isCurrentPostPublished();
 		}, [] );
 
-		// const currentPostType = useSelect( select => {
-		// 	return select( editorStore ).getCurrentPostType();
-		// }, [] );
-		//
-		// const currentPost = useSelect( select => {
-		// 	console.log( select( editorStore ) );
-		// 	return select( editorStore ).getCurrentPost();
-		// }, [] );
+		const currentPostType = useSelect( select => {
+			return select( editorStore ).getCurrentPostType();
+		}, [] );
 
-		// const { getMedia, getUser } = select( 'core' );
-		// const { getCurrentPost, getEditedPostAttribute } = select( 'core/editor' );
+		function promoted_content_enabled( type ) {
+			return type === 'post' || type === 'page' || type === 'product';
+		}
 
-		// const authorId = useSelect(
-		// 	theSelect => theSelect( 'core/editor' ).getEditedPostAttribute( 'author' ),
-		// 	[]
-		// );
+		const currentPost = useSelect( select => {
+			return select( editorStore ).getCurrentPost();
+		}, [] );
 
-		// const site = useSelect( theSelect => theSelect( 'core' ).getSite(), [] );
-		// const user = useSelect( theSelect => theSelect( 'core' ).getUser( authorId ), [] ); // not valid, we still need to call wpcomapi
-		// const post = useSelect( theSelect => theSelect( 'core/editor' ).getCurrentPost(), [] );
-		// console.log( authorId );
-		// console.log( site );
-		// console.log( post );
-		// console.log( user );
-		// if ( user ) {
-		// 	debugger;
-		// }
+		const currentPostId = currentPost?.id;
 
-		// console.log( 'postTYpe', currentPostType );
-		// console.log( 'getCurrentPost', currentPost );
-		// // console.log( 'woSite', woSite );
-		// // console.log( 'test', test );
+		const siteFragment = getSiteFragment();
 
+		const targetLink = `https://wordpress.com/advertising/${ siteFragment }?blazepress-widget=post-${ currentPostId }`;
+
+		const authorId = useSelect( select => {
+			return select( 'core/editor' ).getEditedPostAttribute( 'author' );
+		}, [] );
+
+		const user = useSelect( select => {
+			return select( 'core' ).getUser( authorId );
+		}, [] );
+
+		const isUserAdmin = user?.capabilities.administrator;
 		const isWPCOMSite = isSimpleSite() || isAtomicSite();
+		const isContentAllowed = promoted_content_enabled( currentPostType );
 
-		const hasPromotedPosts = isWPCOMSite && ! isPrivateSite();
+		const hasPromotedPosts = isUserAdmin && isContentAllowed && isWPCOMSite && ! isPrivateSite();
 
 		function PromotePostPanelBodyContent() {
 			return (
@@ -78,7 +72,11 @@ export const settings = {
 							) }
 						</p>
 					</PanelRow>
-					<PromotePostButton />
+					<div className="qr-post-button">
+						<Button isSecondary href={ targetLink }>
+							{ __( 'Promote Post', 'jetpack' ) }
+						</Button>
+					</div>
 				</>
 			);
 		}
