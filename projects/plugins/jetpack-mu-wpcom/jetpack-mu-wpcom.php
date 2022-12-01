d7c7b86794de1c08454097112bd89bba8d12fa41
Samiff <samiff@users.noreply.github.com>
2022-12-01 10:58:04 -0700

    Initial plugin skeleton

---
+++ projects/plugins/jetpack-mu-wpcom/jetpack-mu-wpcom.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *
+ * Plugin Name: WordPress.com Features
+ * Plugin URI: TBD
+ * Description: Enhances your site with features powered by WordPress.com
+ * Version: 0.1.0-alpha
+ * Author: Automattic
+ * Author URI: https://jetpack.com/
+ * License: GPLv2 or later
+ * Text Domain: jetpack
+ *
+ * @package automattic/jetpack-mu-wpcom
+ */
+
+// Code some good stuff!
