949cbf9752717b626e31afee685f7be65ff75779
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2022-12-02 16:49:00 +0700

    Add simple JS test

---
+++ projects/plugins/starter-plugin/src/js/components/admin-page/test/index.test.js
@@ -0,0 +1,30 @@
+import { CONNECTION_STORE_ID } from '@automattic/jetpack-connection';
+import { render, screen } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
+import { useSelect } from '@wordpress/data';
+import React from 'react';
+import Admin from '../index';
+
+describe( 'Admin', () => {
+	beforeAll( () => {
+		window.jetpackStarterPluginInitialState = {
+			apiNonce: '',
+			apiRoot: '',
+			registrationNonce: '',
+		};
+	} );
+
+	test( 'Renders the component', () => {
+		let storeSelect;
+
+		renderHook( () => useSelect( select => ( storeSelect = select( CONNECTION_STORE_ID ) ) ) );
+		jest
+			.spyOn( storeSelect, 'getConnectionStatus' )
+			.mockReset()
+			.mockReturnValue( { isRegistered: false, isUserConnected: false } );
+
+		render( <Admin /> );
+		expect( screen.getByLabelText( 'Jetpack Starter Plugin' ) ).toBeInTheDocument();
+		expect( screen.getByText( 'Amazing feature 1', { selector: 'li' } ) ).toBeInTheDocument();
+	} );
+} );
