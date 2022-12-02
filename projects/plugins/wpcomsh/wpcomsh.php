58e60f765bf966938e97add7b89ce54a073fac80
dlind1 <dlind1@users.noreply.github.com>
2022-12-02 17:03:04 +0100

    Update language files https://github.com/Automattic/wpcomsh/pull/1176
    * Update language files
    
    * Increased release version to include translations
    
    Co-authored-by: Paulo Trentin <paulo@paulotrentin.com.br>

--- projects/plugins/wpcomsh/wpcomsh.php
+++ projects/plugins/wpcomsh/wpcomsh.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WordPress.com Site Helper
  * Description: A helper for connecting WordPress.com sites to external host infrastructure.
- * Version: 3.4.30
+ * Version: 3.4.31
  * Author: Automattic
  * Author URI: http://automattic.com/
  *
@@ -10,7 +10,7 @@
  */
 
 // Increase version number if you change something in wpcomsh.
-define( 'WPCOMSH_VERSION', '3.4.30' );
+define( 'WPCOMSH_VERSION', '3.4.31' );
 
 // If true, Typekit fonts will be available in addition to Google fonts
 add_filter( 'jetpack_fonts_enable_typekit', '__return_true' );
