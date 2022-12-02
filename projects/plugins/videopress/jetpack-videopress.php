8976eb4af81909d7159413539e765f49c320b1b1
Alvaro Vega <alvaro.vega@automattic.com>
2022-12-02 21:10:20 +0100

    Fix project versions

--- projects/plugins/videopress/jetpack-videopress.php
+++ projects/plugins/videopress/jetpack-videopress.php
@@ -4,7 +4,7 @@
  * Plugin Name: Jetpack VideoPress
  * Plugin URI: https://wordpress.org/plugins/jetpack-videopress
  * Description: High quality, ad-free video.
- * Version: 1.2.0-beta
+ * Version: 1.2.1-alpha
  * Author: Automattic - Jetpack Video team
  * Author URI: https://jetpack.com/videopress/
  * License: GPLv2 or later
