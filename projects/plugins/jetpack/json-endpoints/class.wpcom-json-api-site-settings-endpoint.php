e64deb972fd17af566afb304ed4a8eeae2a692ef
Grant Kinney <hi@grant.mk>
2022-12-04 15:07:29 -0600

    Adds blogging prompt settings to other site settings endpoint

--- projects/plugins/jetpack/json-endpoints/class.wpcom-json-api-site-settings-endpoint.php
+++ projects/plugins/jetpack/json-endpoints/class.wpcom-json-api-site-settings-endpoint.php
@@ -437,6 +437,7 @@ class WPCOM_JSON_API_Site_Settings_Endpoint extends WPCOM_JSON_API_Endpoint {
 						'launchpad_screen'                 => (string) get_option( 'launchpad_screen' ),
 						'featured_image_email_enabled'     => (bool) get_option( 'featured_image_email_enabled' ),
 						'wpcom_gifting_subscription'       => (bool) get_option( 'wpcom_gifting_subscription', $this->get_wpcom_gifting_subscription_default() ),
+						'jetpack_blogging_prompts_enabled' => (bool) get_option( 'jetpack_blogging_prompts_enabled' ),
 					);
 
 					if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
