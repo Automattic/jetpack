829e0833da73d9bdd3e47ce1566f210889e84672
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2022-12-05 22:17:01 +0700

    Update projects/plugins/starter-plugin/jest.config.js

--- projects/plugins/starter-plugin/jest.config.js
+++ projects/plugins/starter-plugin/jest.config.js
@@ -3,6 +3,5 @@ const baseConfig = require( 'jetpack-js-tools/jest/config.base.js' );
 module.exports = {
 	...baseConfig,
 	roots: [ '<rootDir>/src' ],
-	moduleDirectories: [ 'node_modules', '<rootDir>/src' ],
 	setupFilesAfterEnv: [ ...baseConfig.setupFilesAfterEnv, '<rootDir>/jest.setup.js' ],
 };
