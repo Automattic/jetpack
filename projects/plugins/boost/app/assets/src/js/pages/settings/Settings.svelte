b5bf4fa93a136e03bfecb198afc5131c91791ec0
Mark George <thingalon@gmail.com>
2022-12-05 15:53:05 +1100

    Add a re-reouter to gracefully reroute without displaying child routes

--- projects/plugins/boost/app/assets/src/js/pages/settings/Settings.svelte
+++ projects/plugins/boost/app/assets/src/js/pages/settings/Settings.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import ReRouter from '../../elements/ReRouter.svelte';
 	import Footer from '../../sections/Footer.svelte';
 	import Header from '../../sections/Header.svelte';
 	import config from '../../stores/config';
@@ -9,39 +9,29 @@
 	import Score from './sections/Score.svelte';
 	import Support from './sections/Support.svelte';
 	import Tips from './sections/Tips.svelte';
-
-	// svelte-ignore unused-export-let - Ignored values supplied by svelte-navigator.
-	export let location, navigate;
-
-	onMount( () => {
-		if ( $config.site.getStarted ) {
-			// Use a timeout, because it's not ok to navigate during page setup. Svelte-navigator doesn't like that.
-			setTimeout( () => {
-				navigate( '/getting-started' );
-			}, 0 );
-		}
-	} );
 </script>
 
-<div id="jb-settings" class="jb-settings jb-settings--main">
-	<div class="jb-container">
-		<Header />
-	</div>
-
-	<div class="jb-section jb-section--alt jb-section--scores">
-		<Score />
-	</div>
-
-	<Router>
-		<div class="jb-section jb-section--main">
-			<Route path="critical-css-advanced" component={AdvancedCriticalCss} />
-			<Route path="/" component={Modules} />
+<ReRouter to="/getting-started" when={$config.site.getStarted}>
+	<div id="jb-settings" class="jb-settings jb-settings--main">
+		<div class="jb-container">
+			<Header />
 		</div>
-	</Router>
 
-	<Tips />
+		<div class="jb-section jb-section--alt jb-section--scores">
+			<Score />
+		</div>
 
-	<Support />
+		<Router>
+			<div class="jb-section jb-section--main">
+				<Route path="critical-css-advanced" component={AdvancedCriticalCss} />
+				<Route path="/" component={Modules} />
+			</div>
+		</Router>
 
-	<Footer />
-</div>
+		<Tips />
+
+		<Support />
+
+		<Footer />
+	</div>
+</ReRouter>
