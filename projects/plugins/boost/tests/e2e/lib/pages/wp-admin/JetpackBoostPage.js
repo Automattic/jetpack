cdba95dc1209d8c6aa2a439269a92a4b38b33b3a
Mark George <thingalon@gmail.com>
2022-12-06 14:08:50 +1100

    [not verified] Increase timeout when waiting on speed score results

--- projects/plugins/boost/tests/e2e/lib/pages/wp-admin/JetpackBoostPage.js
+++ projects/plugins/boost/tests/e2e/lib/pages/wp-admin/JetpackBoostPage.js
@@ -96,7 +96,7 @@ export default class JetpackBoostPage extends WpPage {
 		);
 		await this.page.waitForSelector( '.jb-score-bar__score', {
 			state: 'visible',
-			timeout: 40 * 1000,
+			timeout: 120 * 1000,
 		} );
 		return Number( await speedBar.$eval( '.jb-score-bar__score', e => e.textContent ) );
 	}
