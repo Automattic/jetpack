dd0df41be69e5ecb6860ba3e709543a07fb5cf73
dkmyta <dkmyta10@gmail.com>
2022-12-07 06:15:28 -0800

    Remove auto vs manual toggle separation and upgrade messaging

--- projects/plugins/protect/src/js/components/firewall-page/index.jsx
+++ projects/plugins/protect/src/js/components/firewall-page/index.jsx
@@ -1,16 +1,9 @@
-import {
-	Button,
-	Col,
-	Container,
-	Text,
-	ContextualUpgradeTrigger,
-} from '@automattic/jetpack-components';
-import { useProductCheckoutWorkflow } from '@automattic/jetpack-connection';
-import { ExternalLink, Popover } from '@wordpress/components';
+import { Button, Col, Container, Text } from '@automattic/jetpack-components';
+import { ExternalLink } from '@wordpress/components';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { createInterpolateElement } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-import { Icon, arrowLeft, closeSmall } from '@wordpress/icons';
+import { arrowLeft } from '@wordpress/icons';
 import { useCallback, useEffect, useState } from 'react';
 import API from '../../api';
 import { PLUGIN_SUPPORT_URL } from '../../constants';
@@ -18,7 +11,7 @@ import useAnalyticsTracks from '../../hooks/use-analytics-tracks';
 import useProtectData from '../../hooks/use-protect-data';
 import useWafData from '../../hooks/use-waf-data';
 import { STORE_ID } from '../../state/store';
-import AdminPage, { JETPACK_SCAN } from '../admin-page';
+import AdminPage from '../admin-page';
 import FirewallFooter from '../firewall-footer';
 import ConnectedFirewallHeader from '../firewall-header';
 import FormToggle from '../form-toggle';
@@ -28,18 +21,10 @@ import styles from './styles.module.scss';
 
 const FirewallPage = () => {
 	const notice = useSelect( select => select( STORE_ID ).getNotice() );
-	const {
-		config,
-		isSeen,
-		upgradeIsSeen,
-		isEnabled,
-		toggleWaf,
-		toggleManualRules,
-		updateConfig,
-	} = useWafData();
+	const { config, isSeen, isEnabled, toggleWaf, toggleManualRules, updateConfig } = useWafData();
 	const { hasRequiredPlan } = useProtectData();
 	const { jetpackWafIpList, jetpackWafIpBlockList, jetpackWafIpAllowList } = config || {};
-	const { setWafIsSeen, setWafUpgradeIsSeen, setNotice } = useDispatch( STORE_ID );
+	const { setWafIsSeen, setNotice } = useDispatch( STORE_ID );
 
 	const [ settings, setSettings ] = useState( {
 		module_enabled: isEnabled,
@@ -114,12 +99,6 @@ const FirewallPage = () => {
 						  ),
 				} )
 			)
-			.then( () => {
-				if ( ! upgradeIsSeen ) {
-					setWafUpgradeIsSeen( true );
-					API.wafUpgradeSeen();
-				}
-			} )
 			.catch( () => {
 				setNotice( {
 					type: 'error',
@@ -127,7 +106,7 @@ const FirewallPage = () => {
 				} );
 			} )
 			.finally( () => setSettingsIsUpdating( false ) );
-	}, [ settings, toggleWaf, setNotice, errorMessage, upgradeIsSeen, setWafUpgradeIsSeen ] );
+	}, [ settings, toggleWaf, setNotice, errorMessage ] );
 
 	const handleManualRulesChange = useCallback( () => {
 		const newManualRulesStatus = ! settings.jetpack_waf_ip_list;
@@ -192,25 +171,6 @@ const FirewallPage = () => {
 		setShowManualRules( true );
 	}, [ showManualRules, setShowManualRules ] );
 
-	const { adminUrl } = window.jetpackProtectInitialState || {};
-	const firewallUrl = adminUrl + '#/firewall';
-
-	const { run } = useProductCheckoutWorkflow( {
-		productSlug: JETPACK_SCAN,
-		redirectUrl: firewallUrl,
-	} );
-
-	const { recordEventHandler } = useAnalyticsTracks();
-	const getScan = recordEventHandler( 'jetpack_protect_waf_page_get_scan_link_click', run );
-
-	const handleClosePopoverClick = useCallback( () => {
-		setWafUpgradeIsSeen( true );
-		API.wafUpgradeSeen();
-	}, [ setWafUpgradeIsSeen ] );
-
-	const { waf } = window.jetpackProtectInitialState;
-	const { displayUpgradeBadge } = waf;
-
 	return (
 		<AdminPage>
 			{ notice.message && <Notice floating={ true } dismissable={ true } { ...notice } /> }
@@ -224,111 +184,65 @@ const FirewallPage = () => {
 									<FormToggle
 										checked={ settings.module_enabled }
 										onChange={ handleEnabledChange }
-										disabled={ ! hasRequiredPlan || settingsIsUpdating }
-									/>
-									{ hasRequiredPlan && upgradeIsSeen === false && (
-										<Popover noArrow={ false } offset={ 8 } position={ 'top right' }>
-											<div className={ styles.popover }>
-												<div className={ styles[ 'popover-header' ] }>
-													<Text className={ styles[ 'popover-title' ] } variant={ 'title-small' }>
-														{ __( 'Thanks for upgrading!', 'jetpack-protect' ) }
-													</Text>
-													<Button variant={ 'icon' }>
-														<Icon
-															onClick={ handleClosePopoverClick }
-															icon={ closeSmall }
-															size={ 24 }
-															aria-label={ __( 'Close Window', 'jetpack-protect' ) }
-														/>
-													</Button>
-												</div>
-												<Text
-													className={ styles[ 'popover-description' ] }
-													variant={ 'body' }
-													mt={ 2 }
-													mb={ 3 }
-												>
-													{ __(
-														'Turn on Jetpack Firewall to automatically protect your site with the latest security rules.',
-														'jetpack-protect'
-													) }
-												</Text>
-												<div className={ styles[ 'popover-footer' ] }>
-													<Button onClick={ handleClosePopoverClick }>
-														{ __( 'Got it', 'jetpack-protect' ) }
-													</Button>
-												</div>
-											</div>
-										</Popover>
-									) }
-								</div>
-								<div>
-									<div className={ styles[ 'toggle-section-title' ] }>
-										<Text
-											className={ ! hasRequiredPlan ? styles.disabled : null }
-											variant="title-medium"
-										>
-											{ __( 'Enable automatic rules', 'jetpack-protect' ) }
-										</Text>
-										{ hasRequiredPlan && displayUpgradeBadge && (
-											<span className={ styles.badge }>
-												{ __( 'NOW AVAILABLE', 'jetpack-protect' ) }
-											</span>
-										) }
-									</div>
-									<Text className={ ! hasRequiredPlan ? styles.disabled : null }>
-										{ __(
-											'Protect your site against untrusted traffic sources with automatic security rules.',
-											'jetpack-protect'
-										) }
-									</Text>
-								</div>
-							</div>
-							<div className={ styles[ 'upgrade-trigger-section' ] }>
-								{ ! hasRequiredPlan && (
-									<ContextualUpgradeTrigger
-										className={ styles[ 'upgrade-trigger' ] }
-										description={ __( 'Setup automatic rules with one click', 'jetpack-protect' ) }
-										cta={ __( 'Upgrade to enable automatic rules', 'jetpack-protect' ) }
-										onClick={ getScan }
-									/>
-								) }
-							</div>
-							<div className={ styles[ 'toggle-section' ] }>
-								<div>
-									<FormToggle
-										id="jetpack_waf_ip_list"
-										checked={ Boolean( settings.jetpack_waf_ip_list ) }
-										onChange={ handleManualRulesChange }
 										disabled={ settingsIsUpdating }
 									/>
 								</div>
 								<div>
-									<Text variant="title-medium" mb={ 2 }>
-										{ __( 'Enable manual rules', 'jetpack-protect' ) }
-									</Text>
+									<div className={ styles[ 'toggle-section-title' ] }>
+										<Text variant="title-medium">
+											{ __(
+												"Protect your site with Jetpack's Web Application Firewall",
+												'jetpack-protect'
+											) }
+										</Text>
+									</div>
 									<Text>
 										{ __(
-											'Allows you to add manual rules to block or allow traffic from specific IPs.',
+											'The Jetpack Firewall is a web application firewall designed to protect your WordPress site from malicious requests.',
 											'jetpack-protect'
 										) }
 									</Text>
 								</div>
 							</div>
-							{ jetpackWafIpList && (
-								<div className={ styles[ 'edit-manual-rules-section' ] }>
-									<Text variant={ 'body-small' } mt={ 2 }>
-										{ /* // TO DO: Update or remove until ready for this */ }
-										{ '' === jetpackWafIpAllowList &&
-											'' === jetpackWafIpBlockList &&
-											__( 'No manual rules are being applied.', 'jetpack-protect' ) }
-									</Text>
-									<Button variant={ 'link' }>
-										<Text variant={ 'body-small' } onClick={ handleShowManualRulesClick }>
-											{ __( 'Edit manual rules', 'jetpack-protect' ) }
-										</Text>
-									</Button>
-								</div>
+							{ isEnabled && (
+								<>
+									<div className={ styles[ 'toggle-section' ] }>
+										<div>
+											<FormToggle
+												id="jetpack_waf_ip_list"
+												checked={ Boolean( settings.jetpack_waf_ip_list ) }
+												onChange={ handleManualRulesChange }
+												disabled={ settingsIsUpdating }
+											/>
+										</div>
+										<div>
+											<Text variant="title-medium" mb={ 2 }>
+												{ __( 'Enable manual rules', 'jetpack-protect' ) }
+											</Text>
+											<Text>
+												{ __(
+													'Allows you to add manual rules to block or allow traffic from specific IPs.',
+													'jetpack-protect'
+												) }
+											</Text>
+										</div>
+									</div>
+									{ jetpackWafIpList && (
+										<div className={ styles[ 'edit-manual-rules-section' ] }>
+											<Text variant={ 'body-small' } mt={ 2 }>
+												{ /* // TO DO: Update or remove until ready for this */ }
+												{ '' === jetpackWafIpAllowList &&
+													'' === jetpackWafIpBlockList &&
+													__( 'No manual rules are being applied.', 'jetpack-protect' ) }
+											</Text>
+											<Button variant={ 'link' }>
+												<Text variant={ 'body-small' } onClick={ handleShowManualRulesClick }>
+													{ __( 'Edit manual rules', 'jetpack-protect' ) }
+												</Text>
+											</Button>
+										</div>
+									) }
+								</>
 							) }
 						</div>
 					) : (
