28a239d706f76df74d6ff9b462af41b06314fe21
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2022-12-08 09:08:40 +0700

    Revert "Fix/error on getting started (#27728)"
    This reverts commit d028563541d35140f3548e9836a6c035f77aaa45.

--- projects/plugins/boost/app/assets/src/js/pages/settings/Settings.svelte
+++ projects/plugins/boost/app/assets/src/js/pages/settings/Settings.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import ReRouter from '../../elements/ReRouter.svelte';
 	import Footer from '../../sections/Footer.svelte';
 	import Header from '../../sections/Header.svelte';
 	import config from '../../stores/config';
@@ -9,29 +8,37 @@
 	import Score from './sections/Score.svelte';
 	import Support from './sections/Support.svelte';
 	import Tips from './sections/Tips.svelte';
+
+	// svelte-ignore unused-export-let - Ignored values supplied by svelte-navigator.
+	export let location, navigate;
+
+	$: {
+		// If the user has Cloud CSS, assume they already got started.
+		if ( $config.site.getStarted ) {
+			navigate( '/getting-started' );
+		}
+	}
 </script>
 
-<ReRouter to="/getting-started" when={$config.site.getStarted}>
-	<div id="jb-settings" class="jb-settings jb-settings--main">
-		<div class="jb-container">
-			<Header />
-		</div>
-
-		<div class="jb-section jb-section--alt jb-section--scores">
-			<Score />
-		</div>
-
-		<Router>
-			<div class="jb-section jb-section--main">
-				<Route path="critical-css-advanced" component={AdvancedCriticalCss} />
-				<Route path="/" component={Modules} />
-			</div>
-		</Router>
-
-		<Tips />
-
-		<Support />
-
-		<Footer />
+<div id="jb-settings" class="jb-settings jb-settings--main">
+	<div class="jb-container">
+		<Header />
 	</div>
-</ReRouter>
+
+	<div class="jb-section jb-section--alt jb-section--scores">
+		<Score />
+	</div>
+
+	<Router>
+		<div class="jb-section jb-section--main">
+			<Route path="critical-css-advanced" component={AdvancedCriticalCss} />
+			<Route path="/" component={Modules} />
+		</div>
+	</Router>
+
+	<Tips />
+
+	<Support />
+
+	<Footer />
+</div>
