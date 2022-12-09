61ce6be6966ebf3b2452389683ec3f49f3c0af63
Adnan Haque <adnan007.id@gmail.com>
2022-12-09 11:25:31 +0600

    Wrap Settings page in ReRouter again

--- projects/plugins/boost/app/assets/src/js/pages/settings/Settings.svelte
+++ projects/plugins/boost/app/assets/src/js/pages/settings/Settings.svelte
@@ -11,26 +11,27 @@
 	import Tips from './sections/Tips.svelte';
 </script>
 
-<ReRouter to="/getting-started" when={$config.site.getStarted} />
-<div id="jb-settings" class="jb-settings jb-settings--main">
-	<div class="jb-container">
-		<Header />
-	</div>
-
-	<div class="jb-section jb-section--alt jb-section--scores">
-		<Score />
-	</div>
-
-	<Router>
-		<div class="jb-section jb-section--main">
-			<Route path="critical-css-advanced" component={AdvancedCriticalCss} />
-			<Route path="/" component={Modules} />
+<ReRouter to="/getting-started" when={$config.site.getStarted}>
+	<div id="jb-settings" class="jb-settings jb-settings--main">
+		<div class="jb-container">
+			<Header />
 		</div>
-	</Router>
 
-	<Tips />
+		<div class="jb-section jb-section--alt jb-section--scores">
+			<Score />
+		</div>
 
-	<Support />
+		<Router>
+			<div class="jb-section jb-section--main">
+				<Route path="critical-css-advanced" component={AdvancedCriticalCss} />
+				<Route path="/" component={Modules} />
+			</div>
+		</Router>
 
-	<Footer />
-</div>
+		<Tips />
+
+		<Support />
+
+		<Footer />
+	</div>
+</ReRouter>
