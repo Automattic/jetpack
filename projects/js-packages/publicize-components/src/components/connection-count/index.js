0865e3d8b9ca4a2c381d6eb934f86c5907060573
Tim Broddin <tim@wannabes.be>
2022-12-09 13:29:00 +0100

    Link to connections admin

--- projects/js-packages/publicize-components/src/components/connection-count/index.js
+++ projects/js-packages/publicize-components/src/components/connection-count/index.js
@@ -1,12 +1,28 @@
+import { ExternalLink, Tooltip } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 import PropTypes from 'prop-types';
+import usePublicizeConfig from '../../hooks/use-publicize-config';
+
 import './style.scss';
 
 const ConnectionCount = props => {
 	const { followerCount } = props;
+	const { connectionsAdminUrl } = usePublicizeConfig();
 
 	return (
 		<span className="jetpack-publicize-connection-label-follower-count">
-			{ followerCount ? followerCount.toLocaleString() : null }
+			{ followerCount ? (
+				followerCount.toLocaleString()
+			) : (
+				<Tooltip
+					text={ __(
+						'You need to disconnect and reconnect this service to see the follower count.',
+						'jetpack'
+					) }
+				>
+					<ExternalLink href={ connectionsAdminUrl }>{ __( 'Fix', 'jetpack' ) }</ExternalLink>
+				</Tooltip>
+			) }
 		</span>
 	);
 };
