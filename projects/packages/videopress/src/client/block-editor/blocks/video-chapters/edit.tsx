397f534b26337fd220c416dde965ec569b2af732
Damián Suárez <rdsuarez@gmail.com>
2022-12-11 15:28:26 +0000

    [not verified] move inspector controls stuff straight to edit

--- projects/packages/videopress/src/client/block-editor/blocks/video-chapters/edit.tsx
+++ projects/packages/videopress/src/client/block-editor/blocks/video-chapters/edit.tsx
@@ -2,11 +2,15 @@
  * WordPress dependencies
  */
 import { useBlockProps } from '@wordpress/block-editor';
+import { InspectorControls } from '@wordpress/block-editor';
+import { PanelBody } from '@wordpress/components';
+import { useCallback } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
 import classNames from 'classnames';
-import VideoPressChaptersInspectorControls from './components/inspector-controls';
+import VideoBlockSelectControl from './components/video-block-select-control';
 import useChapters from './hooks/use-chapters';
 import './editor.scss';
 /**
@@ -25,35 +29,51 @@ export default function VideoPressChaptersEdit( {
 	attributes,
 	setAttributes,
 }: VideoChaptersBlockPropertiesProps ) {
+	const { persistentBlockLinkId } = attributes;
 	const chapters = useChapters();
+
+	const handleAttributeChange = useCallback(
+		attributeName => {
+			return newValue => {
+				setAttributes( { [ attributeName ]: newValue } );
+			};
+		},
+		[ setAttributes ]
+	);
 
 	const blockProps = useBlockProps( {
 		className: 'wp-block-jetpack-video-chapters',
 	} );
 
 	return (
-		<div { ...blockProps }>
-			<VideoPressChaptersInspectorControls
-				attributes={ attributes }
-				setAttributes={ setAttributes }
-			/>
+		<>
+			<InspectorControls>
+				<PanelBody title={ __( 'Details', 'jetpack-videopress-pkg' ) }>
+					<VideoBlockSelectControl
+						value={ persistentBlockLinkId }
+						onChange={ handleAttributeChange( 'videoPressBlockId' ) }
+					/>
+				</PanelBody>
+			</InspectorControls>
 
-			<ul className="video-chapters_list">
-				{ chapters.map( ( { chapter, time }, index ) => (
-					<li
-						className={ classNames( 'video-chapters__item', {
-							// At block we just provide an way of user see the three states, not interact with them.
-							// - Not selected
-							// - Selected
-							// - Hover
-							selected: 0 === index,
-						} ) }
-					>
-						<span>{ chapter }</span>
-						<span>{ time }</span>
-					</li>
-				) ) }
-			</ul>
-		</div>
+			<div { ...blockProps }>
+				<ul className="video-chapters_list">
+					{ chapters.map( ( { chapter, time }, index ) => (
+						<li
+							className={ classNames( 'video-chapters__item', {
+								// At block we just provide an way of user see the three states, not interact with them.
+								// - Not selected
+								// - Selected
+								// - Hover
+								selected: 0 === index,
+							} ) }
+						>
+							<span>{ chapter }</span>
+							<span>{ time }</span>
+						</li>
+					) ) }
+				</ul>
+			</div>
+		</>
 	);
 }
