02025cac64e4a4af9aefb6698df7e4bfbb60d78b
Mark George <thingalon@gmail.com>
2022-12-12 19:22:41 +1100

    Store boost banner dismissal in user options

--- projects/plugins/super-cache/wp-cache.php
+++ projects/plugins/super-cache/wp-cache.php
@@ -321,7 +321,7 @@ function wpsc_is_boost_active() {
  */
 function wpsc_hide_boost_banner() {
 	check_ajax_referer( 'wpsc_dismiss_boost_banner', 'nonce' );
-	update_option( 'wpsc_2022_boost_banner', 0, 'no' );
+	update_user_option( get_current_user_id(), 'wpsc_dismissed_boost_banner', '1' );
 
 	wp_die();
 }
@@ -347,7 +347,8 @@ add_action( 'wp_ajax_wpsc_activate_boost', 'wpsc_ajax_activate_boost' );
  */
 function wpsc_jetpack_boost_install_banner() {
 	// Don't show the banner if Boost is installed, or the banner has been dismissed.
-	if ( wpsc_is_boost_active() || ! get_option( 'wpsc_2022_boost_banner', true ) ) {
+	$is_dismissed = '1' === get_user_option( 'wpsc_dismissed_boost_banner' );
+	if ( wpsc_is_boost_active() || $is_dismissed ) {
 		return;
 	}
 
