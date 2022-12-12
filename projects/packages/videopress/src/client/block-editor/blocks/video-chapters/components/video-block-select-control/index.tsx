1c0d160a906e3233783880e1fce82d22b7935a8b
Damián Suárez <rdsuarez@gmail.com>
2022-12-12 12:54:11 +0000

    change the rule to link automatically

--- projects/packages/videopress/src/client/block-editor/blocks/video-chapters/components/video-block-select-control/index.tsx
+++ projects/packages/videopress/src/client/block-editor/blocks/video-chapters/components/video-block-select-control/index.tsx
@@ -3,32 +3,34 @@
  */
 import { PanelRow, SelectControl } from '@wordpress/components';
 import { select } from '@wordpress/data';
+import { useCallback, useEffect } from '@wordpress/element';
 import { sprintf, __ } from '@wordpress/i18n';
-import { useEffect } from 'react';
 import { PersistentBlockLinkIdProp } from '../../types';
 
 type VideoBlockSelectControlProps = {
-	value: string;
+	linkId: string;
 	onChange: ( newvalue: PersistentBlockLinkIdProp ) => void;
 };
 
 type VideoBlocksSelectControlItemProps = {
 	value: string;
 	label: string;
+	persistentBlockLinkId: PersistentBlockLinkIdProp;
 };
 
 type VideoBlocksSelectControlDataProps = VideoBlocksSelectControlItemProps[];
 
-const VideoBlockSelectControl = ( { value, onChange }: VideoBlockSelectControlProps ) => {
+const VideoBlockSelectControl = ( { linkId, onChange }: VideoBlockSelectControlProps ) => {
 	const blocks = select( 'core/block-editor' )
 		.getBlocks()
 		.filter( block => block.name === 'videopress/video' );
 
-	useEffect( () => {
-		if ( ! value && blocks.length > 0 ) {
-			onChange( blocks[ 0 ].clientId );
-		}
-	}, [] );
+	const onChangeHandler = useCallback(
+		( newValue: string ) => {
+			onChange( newValue );
+		},
+		[ onChange ]
+	);
 
 	const availableBlocksToLink: VideoBlocksSelectControlDataProps = blocks
 		.map( block => ( {
@@ -36,36 +38,59 @@ const VideoBlockSelectControl = ( { value, onChange }: VideoBlockSelectControlPr
 			label: block.attributes.title,
 			persistentBlockLinkId: block.attributes.persistentBlockLinkId,
 		} ) )
-		.filter( data => data.label );
+		.filter( data => data.value && ! data.persistentBlockLinkId );
+
+	/*
+	 * Link the chapters block automatically when:
+	 * - The chapters block is created (mounted)
+	 * - The chapters block is not already linked to a video block
+	 * - There is only one video block in the post to link to.
+	 */
+	useEffect( () => {
+		if ( linkId ) {
+			return;
+		}
+
+		if ( ! availableBlocksToLink || availableBlocksToLink.length !== 1 ) {
+			return;
+		}
+
+		onChangeHandler( availableBlocksToLink[ 0 ].value );
+	}, [ linkId, availableBlocksToLink ] );
 
 	if ( ! availableBlocksToLink.length ) {
 		return <PanelRow>{ __( 'No video blocks found.', 'jetpack-videopress-pkg' ) }</PanelRow>;
 	}
 
-	const blockToLinkName = availableBlocksToLink.find( data => data.value === value )?.label;
+	/*
+	 * Remove the current linked video block
+	 * from the available blocks list.
+	 */
+	if ( linkId ) {
+		availableBlocksToLink.splice(
+			availableBlocksToLink.findIndex( data => data.value === linkId ),
+			1
+		);
+	}
 
-	const linkedToMessage = sprintf(
-		/* translators: %s: The block name to link to. */
-		__( 'Linked to %s video', 'jetpack-videopress-pkg' ),
-		blockToLinkName
-	);
-
-	// Remove the current block from the list of blocks to link to
-	availableBlocksToLink.splice(
-		availableBlocksToLink.findIndex( data => data.value === value ),
-		1
-	);
+	const linkedToMessage = linkId
+		? sprintf(
+				/* translators: %s: The block name to link to. */
+				__( 'Linked to %s video', 'jetpack-videopress-pkg' ),
+				availableBlocksToLink.find( data => data.value === linkId )?.label
+		  )
+		: '';
 
 	return (
 		<>
 			<SelectControl
 				label={ __( 'Change video link', 'jetpack-videopress-pkg' ) }
-				value={ value }
-				onChange={ onChange }
+				value={ linkId }
+				onChange={ onChangeHandler }
 				options={ availableBlocksToLink }
 			/>
 
-			<PanelRow>{ linkedToMessage }</PanelRow>
+			{ linkId && <PanelRow>{ linkedToMessage }</PanelRow> }
 		</>
 	);
 };
