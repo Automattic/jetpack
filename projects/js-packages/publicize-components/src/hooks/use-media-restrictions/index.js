e5cec7b9bcd8fb126c0a8b1253c9121ae2264637
Gergely Juhasz <gergely.juhasz@automattic.com>
2022-12-12 13:26:21 +0100

    Update FB restrictions

--- projects/js-packages/publicize-components/src/hooks/use-media-restrictions/index.js
+++ projects/js-packages/publicize-components/src/hooks/use-media-restrictions/index.js
@@ -1,62 +1,9 @@
 import { useCallback } from 'react';
+import { RESTRICTIONS } from './restrictions';
 
 export const FILE_TYPE_ERROR = 'FILE_TYPE_ERROR';
 export const FILE_SIZE_ERROR = 'FILE_SIZE_ERROR';
 export const VIDEO_LENGTH_ERROR = 'VIDEO_LENGTH_ERROR';
-
-const MP4 = 'video/mp4';
-const MOV = 'video/mov';
-/**
- * These restrictions were updated on: November 18, 2022.
- *
- * Image and video size is in MB.
- * Video length is in seconds.
- */
-const allowedImageTypes = [ 'image/jpeg', 'image/jpg', 'image/png' ];
-const RESTRICTIONS = {
-	twitter: {
-		allowedMediaTypes: allowedImageTypes.concat( [ MP4 ] ),
-		image: {
-			maxSize: 5,
-		},
-		video: {
-			maxSize: 512,
-			maxLength: 140,
-		},
-	},
-	facebook: {
-		allowedMediaTypes: allowedImageTypes.concat( [ MP4 ] ),
-		image: {
-			maxSize: 4,
-		},
-		video: {
-			maxSize: 10000,
-			maxLength: 14400,
-		},
-	},
-	tumblr: {
-		allowedMediaTypes: allowedImageTypes.concat( [ MP4, MOV ] ),
-		image: {
-			maxSize: 20,
-		},
-		video: {
-			maxSize: 500,
-			maxLength: 600,
-		},
-	},
-	linkedin: {
-		allowedMediaTypes: allowedImageTypes.concat( [ MP4 ] ),
-		image: {
-			maxSize: 20,
-		},
-		video: {
-			minSize: 0.075,
-			maxSize: 200,
-			maxLength: 600,
-			minLength: 3,
-		},
-	},
-};
 
 /**
  * Checks whether a media is a video.
