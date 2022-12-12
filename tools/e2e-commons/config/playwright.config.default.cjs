d13358752919ca7fc34c43acea8d6f346fa851bf
Mark George <thingalon@gmail.com>
2022-12-12 19:53:03 +1100

    Embiggen the test browsr for visibility

--- tools/e2e-commons/config/playwright.config.default.cjs
+++ tools/e2e-commons/config/playwright.config.default.cjs
@@ -39,7 +39,7 @@ const playwrightConfig = {
 		browserName: 'chromium',
 		channel: '',
 		headless: true,
-		viewport: { width: 1280, height: 720 },
+		viewport: { width: 1280, height: 1720 },
 		ignoreHTTPSErrors: true,
 		actionTimeout: 30000,
 		screenshot: 'only-on-failure',
