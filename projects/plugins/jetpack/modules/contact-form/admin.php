c84cc93fa66eda48ba48390af3c9335f2cfb849d
Christian Gastrell <cgastrell@gmail.com>
2022-12-12 18:32:35 -0300

    lint whitespaces

--- projects/plugins/jetpack/modules/contact-form/admin.php
+++ projects/plugins/jetpack/modules/contact-form/admin.php
@@ -1287,29 +1287,27 @@ class Grunion_Admin {
 			array( 'data-nonce-name' => $this->export_nonce_field )
 		);
 		?>
-
-			<div class="export-card">
-				<div class="export-card__header">
-					<svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path fill-rule="evenodd" clip-rule="evenodd" d="M11.2309 5.04199L10.0797 2.73945C9.98086 2.54183 9.77887 2.41699 9.55792 2.41699H2.83333C2.51117 2.41699 2.25 2.67816 2.25 3.00033V16.7087C2.25 17.0308 2.51117 17.292 2.83333 17.292H19.1667C19.4888 17.292 19.75 17.0308 19.75 16.7087V5.62533C19.75 5.30316 19.4888 5.04199 19.1667 5.04199H11.2309ZM12.3125 3.29199L11.6449 1.95683C11.2497 1.16633 10.4417 0.666992 9.55792 0.666992H2.83333C1.54467 0.666992 0.5 1.71166 0.5 3.00033V16.7087C0.5 17.9973 1.54467 19.042 2.83333 19.042H19.1667C20.4553 19.042 21.5 17.9973 21.5 16.7087V5.62533C21.5 4.33666 20.4553 3.29199 19.1667 3.29199H12.3125Z" fill="#008710"/>
-					</svg>
-					<div class="export-card__header-title">CSV File</div>
+		<div class="export-card">
+			<div class="export-card__header">
+				<svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path fill-rule="evenodd" clip-rule="evenodd" d="M11.2309 5.04199L10.0797 2.73945C9.98086 2.54183 9.77887 2.41699 9.55792 2.41699H2.83333C2.51117 2.41699 2.25 2.67816 2.25 3.00033V16.7087C2.25 17.0308 2.51117 17.292 2.83333 17.292H19.1667C19.4888 17.292 19.75 17.0308 19.75 16.7087V5.62533C19.75 5.30316 19.4888 5.04199 19.1667 5.04199H11.2309ZM12.3125 3.29199L11.6449 1.95683C11.2497 1.16633 10.4417 0.666992 9.55792 0.666992H2.83333C1.54467 0.666992 0.5 1.71166 0.5 3.00033V16.7087C0.5 17.9973 1.54467 19.042 2.83333 19.042H19.1667C20.4553 19.042 21.5 17.9973 21.5 16.7087V5.62533C21.5 4.33666 20.4553 3.29199 19.1667 3.29199H12.3125Z" fill="#008710"/>
+				</svg>
+				<div class="export-card__header-title"><?php echo esc_html( __( 'CSV File', 'jetpack' ) ); ?></div>
+			</div>
+			<div class="export-card__body">
+				<div class="export-card__body-description">
+					<?php echo esc_html( __( 'Download your response form data via CSV file.', 'jetpack' ) ); ?>
 				</div>
-				<div class="export-card__body">
-					<div class="export-card__body-description">
-						Download your response form data via CSV file.
-					</div>
-					<div class="export-card__body-cta">
-						<?php
-						// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- we're literally building all this html to output it
-						echo $button_csv_html;
-						// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- we're literally building all this html to output it
-						echo wp_nonce_field( 'feedback_export', $this->export_nonce_field, false, false );
-						?>
-					</div>
+				<div class="export-card__body-cta">
+					<?php
+					// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- we're literally building all this html to output it
+					echo $button_csv_html;
+					// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- we're literally building all this html to output it
+					echo wp_nonce_field( 'feedback_export', $this->export_nonce_field, false, false );
+					?>
 				</div>
 			</div>
-
+		</div>
 		<?php
 	}
 
