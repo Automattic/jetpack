c6a97098ca1688c88efd992a10f837c73c3123d0
Sharpirate <stoynov.bogomil@gmail.com>
2022-12-13 15:53:32 -0700

    [not verified] enque scripts

--- projects/plugins/jetpack/extensions/plugins/launchpad-save-modal/launchpad-save-modal.php
+++ projects/plugins/jetpack/extensions/plugins/launchpad-save-modal/launchpad-save-modal.php
@@ -8,25 +8,29 @@
 // Feature name.
 const FEATURE_NAME = 'launchpad-save-modal';
 
-wp_enqueue_script(
-	'launchpad-save-modal',
-	plugins_url( 'index.js', __FILE__ ),
-	array(),
-	JETPACK__VERSION,
-	true
-);
+function enque_script() {
+	wp_enqueue_script(
+		'launchpad-save-modal',
+		plugins_url( 'index.js', __FILE__ ),
+		array(),
+		JETPACK__VERSION,
+		true
+	);
 
-wp_add_inline_script(
-	'launchpad-save-modal',
-	'const launchpadModalOptions = ' . wp_json_encode(
-		array(
-			'launchpadScreenOption' => get_option( 'launchpad_screen' ),
-			'siteUrlOption'         => get_option( 'siteurl' ),
-			'siteIntentOption'      => get_option( 'site_intent' ),
-		)
-	),
-	'before'
-);
+	wp_add_inline_script(
+		'launchpad-save-modal',
+		'const launchpadModalOptions = ' . wp_json_encode(
+			array(
+				'launchpadScreenOption' => get_option( 'launchpad_screen' ),
+				'siteUrlOption'         => get_option( 'siteurl' ),
+				'siteIntentOption'      => get_option( 'site_intent' ),
+			)
+		),
+		'before'
+	);
+}
+
+add_action( 'enqueue_block_editor_assets', 'enque_script' );
 
 // Populate the available extensions with launchpad-save-modal.
 add_filter(
