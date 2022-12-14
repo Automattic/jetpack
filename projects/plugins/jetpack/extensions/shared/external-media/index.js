8cbc6d79b15d59cb4ea4a7562d4e615d58091906
Jeremy Herve <jeremy@jeremy.hu>
2022-12-14 10:12:16 +0100

    Extensions: update usage of isCurrentUserConnected from package
    Now that we've extracted this function into our shared package, let's import it from there.

--- projects/plugins/jetpack/extensions/shared/external-media/index.js
+++ projects/plugins/jetpack/extensions/shared/external-media/index.js
@@ -1,6 +1,6 @@
+import isCurrentUserConnected from '@automattic/jetpack-shared-extension-utils';
 import { useBlockEditContext } from '@wordpress/block-editor';
 import { addFilter } from '@wordpress/hooks';
-import isCurrentUserConnected from '../is-current-user-connected';
 import MediaButton from './media-button';
 import { mediaSources } from './sources';
 import './editor.scss';
