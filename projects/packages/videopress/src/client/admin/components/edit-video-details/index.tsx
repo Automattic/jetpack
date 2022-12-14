b77dcbd6714e0235315697b98288d29940e5f1ea
Kev <kevin.zoschke@automattic.com>
2022-12-14 16:06:29 -0500

    Jetpack VideoPress: update logos (#27807)
    * Jetpack VideoPress: update logo
    
    * Fix invalid React prop

--- projects/packages/videopress/src/client/admin/components/edit-video-details/index.tsx
+++ projects/packages/videopress/src/client/admin/components/edit-video-details/index.tsx
@@ -9,6 +9,7 @@ import {
 	Container,
 	Col,
 	useBreakpointMatch,
+	JetpackVideoPressLogo,
 } from '@automattic/jetpack-components';
 import { __ } from '@wordpress/i18n';
 import { Icon, chevronRightSmall, arrowLeft } from '@wordpress/icons';
@@ -24,7 +25,6 @@ import { usePermission } from '../../hooks/use-permission';
 import useUnloadPrevent from '../../hooks/use-unload-prevent';
 import { useVideosQuery } from '../../hooks/use-videos';
 import Input from '../input';
-import Logo from '../logo';
 import Placeholder from '../placeholder';
 import VideoDetails from '../video-details';
 import VideoThumbnail from '../video-thumbnail';
@@ -51,7 +51,7 @@ const Header = ( {
 	return (
 		<div className={ classnames( styles[ 'header-wrapper' ], { [ styles.small ]: isSm } ) }>
 			<button onClick={ () => history.push( '/' ) } className={ styles[ 'logo-button' ] }>
-				<Logo />
+				<JetpackVideoPressLogo />
 			</button>
 			<div className={ styles[ 'header-content' ] }>
 				<div className={ styles.breadcrumb }>
