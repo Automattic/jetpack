320f9e40c9887755d2f4dd8d604d84b488521bd1
Mikael Korpela <mikael@ihminen.org>
2022-12-15 13:36:26 +0200

    [not verified] Simplify lists and renaming

--- projects/plugins/jetpack/modules/google-fonts.php
+++ projects/plugins/jetpack/modules/google-fonts.php
@@ -15,91 +15,76 @@
  */
 
 /**
- * Return a curated selection of Google Fonts.
+ * Curated list of Google Fonts
  * See p9Jlb4-22P, p9Jlb4-44k-p2 and p9Jlb4-5Dj-p2
- *
- * @return array List of Google Fonts.
  */
-function jetpack_get_google_fonts_list() {
-	$fonts = array(
-		'Albert Sans',
-		'Alegreya',
-		'Arvo',
-		'Bodoni Moda',
-		'Cabin',
-		'Chivo',
-		'Commissioner',
-		'Cormorant',
-		'Courier Prime',
-		'Crimson Pro',
-		'DM Mono',
-		'DM Sans',
-		'Domine',
-		'EB Garamond',
-		'Epilogue',
-		'Figtree',
-		'Fira Sans',
-		'Fraunces',
-		'IBM Plex Mono',
-		'IBM Plex Sans',
-		'Inter',
-		'Josefin Sans',
-		'Jost',
-		'Libre Baskerville',
-		'Libre Franklin',
-		'Literata',
-		'Lora',
-		'Merriweather',
-		'Montserrat',
-		'Newsreader',
-		'Nunito',
-		'Open Sans',
-		'Overpass',
-		'Petrona',
-		'Piazzolla',
-		'Playfair Display',
-		'Plus Jakarta Sans',
-		'Poppins',
-		'Raleway',
-		'Roboto Slab',
-		'Roboto',
-		'Rubik',
-		'Sora',
-		'Source Sans Pro',
-		'Source Serif Pro',
-		'Space Mono',
-		'Texturina',
-		'Work Sans',
+const JETPACK_GOOGLE_FONTS_LIST = array(
+	'Albert Sans',
+	'Alegreya',
+	'Arvo',
+	'Bodoni Moda',
+	'Cabin',
+	'Chivo',
+	'Commissioner',
+	'Cormorant',
+	'Courier Prime',
+	'Crimson Pro',
+	'DM Mono',
+	'DM Sans',
+	'Domine',
+	'EB Garamond',
+	'Epilogue',
+	'Figtree',
+	'Fira Sans',
+	'Fraunces',
+	'IBM Plex Mono',
+	'IBM Plex Sans',
+	'Inter',
+	'Josefin Sans',
+	'Jost',
+	'Libre Baskerville',
+	'Libre Franklin',
+	'Literata',
+	'Lora',
+	'Merriweather',
+	'Montserrat',
+	'Newsreader',
+	'Nunito',
+	'Open Sans',
+	'Overpass',
+	'Petrona',
+	'Piazzolla',
+	'Playfair Display',
+	'Plus Jakarta Sans',
+	'Poppins',
+	'Raleway',
+	'Roboto Slab',
+	'Roboto',
+	'Rubik',
+	'Sora',
+	'Source Sans Pro',
+	'Source Serif Pro',
+	'Space Mono',
+	'Texturina',
+	'Work Sans',
 
-		// Keep i18n fonts at the end of the fonts list
-		'Alexandria'           => _x( 'Alexandria (Arabic)', 'Font name', 'jetpack' ),
-		'IBM Plex Sans Arabic' => _x( 'IBM Plex Sans (Arabic)', 'Font name', 'jetpack' ),
-		'Noto Sans Hebrew'     => _x( 'Noto Sans (Hebrew)', 'Font name', 'jetpack' ),
-		'Noto Sans HK'         => _x( 'Noto Sans (Hong Kong)', 'Font name', 'jetpack' ),
-		'Noto Sans JP'         => _x( 'Noto Sans (Japanese)', 'Font name', 'jetpack' ),
-		'Noto Sans KR'         => _x( 'Noto Sans (Korean)', 'Font name', 'jetpack' ),
-		'Noto Sans SC'         => _x( 'Noto Sans (Simplified Chinese)', 'Font name', 'jetpack' ),
-		'Noto Sans TC'         => _x( 'Noto Sans (Traditional Chinese)', 'Font name', 'jetpack' ),
-		'Noto Sans Telugu'     => _x( 'Noto Sans (Telugu)', 'Font name', 'jetpack' ),
-		'Noto Serif Hebrew'    => _x( 'Noto Serif (Hebrew)', 'Font name', 'jetpack' ),
-		'Noto Serif HK'        => _x( 'Noto Serif (Hong Kong)', 'Font name', 'jetpack' ),
-		'Noto Serif JP'        => _x( 'Noto Serif (Japanese)', 'Font name', 'jetpack' ),
-		'Noto Serif KR'        => _x( 'Noto Serif (Korean)', 'Font name', 'jetpack' ),
-		'Noto Serif SC'        => _x( 'Noto Serif (Simplified Chinese)', 'Font name', 'jetpack' ),
-		'Noto Serif TC'        => _x( 'Noto Serif (Traditional Chinese)', 'Font name', 'jetpack' ),
-	);
-
-	/**
-	 * Curated list of Google Fonts.
-	 *
-	 * @module google-fonts
-	 *
-	 * @since 10.8
-	 *
-	 * @param array $fonts Array of Google Font names to register.
-	 */
-	return apply_filters( 'jetpack_google_fonts_list', array_keys( $fonts ) );
-}
+	// Keep i18n fonts at the end of the fonts list. These are provided different names at `jetpack_rename_google_font_names()` function.
+	'Alexandria',
+	'IBM Plex Sans Arabic',
+	'Noto Sans Hebrew',
+	'Noto Sans HK',
+	'Noto Sans JP',
+	'Noto Sans KR',
+	'Noto Sans SC',
+	'Noto Sans TC',
+	'Noto Sans Telugu',
+	'Noto Serif Hebrew',
+	'Noto Serif HK',
+	'Noto Serif JP',
+	'Noto Serif KR',
+	'Noto Serif SC',
+	'Noto Serif TC',
+);
 
 /**
  * Register a curated selection of Google Fonts.
@@ -113,7 +98,7 @@ function jetpack_add_google_fonts_provider() {
 
 	wp_register_webfont_provider( 'jetpack-google-fonts', '\Automattic\Jetpack\Fonts\Google_Fonts_Provider' );
 
-	$fonts_to_register = jetpack_get_google_fonts_list();
+	$fonts_to_register = apply_filters( 'jetpack_google_fonts_list', JETPACK_GOOGLE_FONTS_LIST );
 
 	foreach ( $fonts_to_register as $font_family ) {
 		wp_register_webfonts(
@@ -154,13 +139,30 @@ add_action( 'after_setup_theme', 'jetpack_add_google_fonts_provider' );
 function jetpack_rename_google_font_names( $theme_json ) {
 	$raw_data      = $theme_json->get_data();
 	$font_families = $raw_data['settings']['typography']['fontFamilies']['theme'];
-	$jetpack_fonts = jetpack_get_google_fonts_list();
+
+	$renamed_fonts = array(
+		'Alexandria'           => _x( 'Alexandria (Arabic)', 'Font name', 'jetpack' ),
+		'IBM Plex Sans Arabic' => _x( 'IBM Plex Sans (Arabic)', 'Font name', 'jetpack' ),
+		'Noto Sans Hebrew'     => _x( 'Noto Sans (Hebrew)', 'Font name', 'jetpack' ),
+		'Noto Sans HK'         => _x( 'Noto Sans (Hong Kong)', 'Font name', 'jetpack' ),
+		'Noto Sans JP'         => _x( 'Noto Sans (Japanese)', 'Font name', 'jetpack' ),
+		'Noto Sans KR'         => _x( 'Noto Sans (Korean)', 'Font name', 'jetpack' ),
+		'Noto Sans SC'         => _x( 'Noto Sans (Simplified Chinese)', 'Font name', 'jetpack' ),
+		'Noto Sans TC'         => _x( 'Noto Sans (Traditional Chinese)', 'Font name', 'jetpack' ),
+		'Noto Sans Telugu'     => _x( 'Noto Sans (Telugu)', 'Font name', 'jetpack' ),
+		'Noto Serif Hebrew'    => _x( 'Noto Serif (Hebrew)', 'Font name', 'jetpack' ),
+		'Noto Serif HK'        => _x( 'Noto Serif (Hong Kong)', 'Font name', 'jetpack' ),
+		'Noto Serif JP'        => _x( 'Noto Serif (Japanese)', 'Font name', 'jetpack' ),
+		'Noto Serif KR'        => _x( 'Noto Serif (Korean)', 'Font name', 'jetpack' ),
+		'Noto Serif SC'        => _x( 'Noto Serif (Simplified Chinese)', 'Font name', 'jetpack' ),
+		'Noto Serif TC'        => _x( 'Noto Serif (Traditional Chinese)', 'Font name', 'jetpack' ),
+	);
 
 	foreach ( $font_families as $key => $font_family ) {
 		$font_name = $font_family['name'];
 
-		if ( array_key_exists( $font_name, $jetpack_fonts ) ) {
-			$font_families[ $key ]['name'] = $jetpack_fonts[ $font_name ];
+		if ( array_key_exists( $font_name, $renamed_fonts ) ) {
+			$font_families[ $key ]['name'] = $renamed_fonts[ $font_name ];
 		}
 	}
 
