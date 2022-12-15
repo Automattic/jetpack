6ca248aa3025a7a405bb62536940851866eb59ea
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2022-12-15 17:04:43 +0200

    E2E tests: retry navigation failed (#27945)

--- tools/e2e-commons/pages/page-actions.js
+++ tools/e2e-commons/pages/page-actions.js
@@ -28,8 +28,18 @@ export default class PageActions {
 		if ( ! url ) {
 			throw new Error( 'Cannot navigate! Page URL is not set' );
 		}
+
 		logger.action( `Navigating to ${ url }` );
-		return await this.page.goto( url, options );
+
+		let response;
+		try {
+			response = await this.page.goto( url, options );
+		} catch ( e ) {
+			logger.error( `Error navigating to ${ url } (1). Retrying once.\n${ e }` );
+			response = await this.page.goto( url, options );
+		}
+
+		return response;
 	}
 
 	/**
