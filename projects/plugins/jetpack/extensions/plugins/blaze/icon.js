452483758082c322a71bc0bb47303e0f15291b84
Jeremy Herve <jeremy@jeremy.hu>
2022-12-15 15:36:45 +0100

    Blocks: update Promote button in post-publish panel
    Let's turn the simple link into a button so it is more visible. This also follows the pattern already in use in the Anchor post-publish panel.
    
    This also updates tracking for this button click to use the new tool introduced in #27937

---
+++ projects/plugins/jetpack/extensions/plugins/blaze/icon.js
@@ -0,0 +1,39 @@
+import { SVG, Path, Rect, G } from '@wordpress/components';
+
+export default function BlazeIcon() {
+	return (
+		<SVG width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/SVG">
+			<G clipPath="url(#clip0_309_5794)">
+				<Rect width="30" height="30" rx="15" fill="white" />
+				<Path
+					fillRule="evenodd"
+					clipRule="evenodd"
+					d="M18.1324 29.6725C24.913 28.2319 30 22.21 30 15C30 6.71573 23.2843 0 15 0C6.71573 0 0 6.71573 0 15C0 22.7364 5.85685 29.1049 13.3785 29.9134C4.82434 22.1809 10.2158 15.7808 13.8235 13.2C11.2887 16.7618 12.3885 22.2287 15.6475 22.2287C19.9929 22.2287 17.8202 15.4288 17.8202 15.4288C17.8202 15.4288 25.2121 22.0386 18.1324 29.6725Z"
+					fill="#EB4A35"
+				/>
+				<Path
+					fillRule="evenodd"
+					clipRule="evenodd"
+					d="M13.4623 29.9888H9.20543C5.50337 26.5131 3.38679 20.9478 5.88305 16.9183C6.73663 15.5404 7.96514 14.6337 9.21236 13.7132C11.6126 11.9417 14.0821 10.119 14.0821 4.78876C19.6517 7.48876 23.313 12.9087 24.0962 14.7474C25.2214 17.3888 26.7945 23.0691 21.273 29.9888H17.8313C25.4173 22.2222 17.8203 15.4288 17.8203 15.4288C17.8203 15.4288 19.9929 22.2287 15.6476 22.2287C12.3885 22.2287 11.2887 16.7619 13.8235 13.2C10.2041 15.7892 4.78932 22.2225 13.4623 29.9888Z"
+					fill="url(#paint0_linear_309_5794)"
+				/>
+			</G>
+			<defs>
+				<linearGradient
+					id="paint0_linear_309_5794"
+					x1="15"
+					y1="4.78876"
+					x2="15"
+					y2="29.9888"
+					gradientUnits="userSpaceOnUse"
+				>
+					<stop stopColor="#FEB104" />
+					<stop offset="1" stopColor="#FFB403" />
+				</linearGradient>
+				<clipPath id="clip0_309_5794">
+					<Rect width="30" height="30" rx="15" fill="white" />
+				</clipPath>
+			</defs>
+		</SVG>
+	);
+}
