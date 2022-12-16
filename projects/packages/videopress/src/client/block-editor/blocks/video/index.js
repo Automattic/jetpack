716d762b0a87b117953e4143818abf5073c46a36
Damián Suárez <rdsuarez@gmail.com>
2022-12-16 14:09:13 +0000

    [not verified] add core/embed to/from transform

--- projects/packages/videopress/src/client/block-editor/blocks/video/index.js
+++ projects/packages/videopress/src/client/block-editor/blocks/video/index.js
@@ -1,10 +1,11 @@
 /**
  * WordPress dependencies
  */
-import { registerBlockType } from '@wordpress/blocks';
+import { registerBlockType, createBlock } from '@wordpress/blocks';
 /**
  * Internal dependencies
  */
+import { getVideoPressUrl, pickGUIDFromUrl } from '../../../lib/url';
 import metadata from './block.json';
 import { VideoPressIcon as icon } from './components/icons';
 import Edit from './edit';
@@ -23,5 +24,47 @@ registerBlockType( name, {
 			src: videoPressBlockExampleImage,
 			isExample: true,
 		},
+	},
+	transforms: {
+		from: [
+			{
+				type: 'block',
+				blocks: [ 'core/embed' ],
+				isMatch: attrs => attrs.providerNameSlug !== 'videopress' && pickGUIDFromUrl( attrs?.url ),
+				transform: attrs => {
+					const { url, providerNameSlug } = attrs;
+					const guid = pickGUIDFromUrl( url );
+					const isCoreEmbedVideoPressVariation = providerNameSlug === 'videopress' && !! guid;
+
+					/*
+					 * Do not add transform when the block
+					 * is not a core/embed VideoPress block variation
+					 */
+					if ( ! isCoreEmbedVideoPressVariation ) {
+						return createBlock( 'core/embed', attrs );
+					}
+
+					return createBlock( 'videopress/video', { ...attrs, guid, src: url } );
+				},
+			},
+		],
+		to: [
+			{
+				type: 'block',
+				blocks: [ 'core/embed' ],
+				isMatch: attrs => attrs?.src || getVideoPressUrl( attrs?.guid, attrs ),
+				transform: attrs => {
+					const { guid, src } = attrs;
+
+					// Build the source (URL) in case it isn't defined.
+					const url = src || getVideoPressUrl( guid, attrs );
+					if ( ! url ) {
+						return createBlock( 'core/embed' );
+					}
+
+					return createBlock( 'core/embed', { ...attrs, url } );
+				},
+			},
+		],
 	},
 } );
