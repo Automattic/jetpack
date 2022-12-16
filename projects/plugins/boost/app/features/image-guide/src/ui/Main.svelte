d88cdf8bea563833a4045ac6d8b725bd4ebf771a
Nauris Pūķis <hi@pyronaur.com>
2022-12-16 16:12:53 +0200

    Main IG container should not break layouts (#27956)
    This fixes a problem where .jetpack-boost-guide element disrupted the layout in Gutenberg galleries.

--- projects/plugins/boost/app/features/image-guide/src/ui/Main.svelte
+++ projects/plugins/boost/app/features/image-guide/src/ui/Main.svelte
@@ -66,12 +66,24 @@
 {/if}
 
 <style lang="scss">
+	:global(.jetpack-boost-guide) {
+		&:not(.relative) {
+			position: absolute;
+			top: 0;
+			left: 0;
+			right: 0;
+			bottom: 0;
+			width: 100%;
+			height: 100%;
+		}
+	}
+	:global( .jetpack-boost-guide.relative ) {
+		position: relative;
+	}
 	.guide {
 		position: absolute;
 		top: 0;
 		left: 0;
-		right: 0;
-		bottom: 0;
 		width: 100%;
 		height: 100%;
 		z-index: 8000;
