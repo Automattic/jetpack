823886c5c5ea471ea25e3c33163640acda9960df
James Kenneth Guidaven <jkguidaven@gmail.com>
2022-12-19 18:48:56 +0800

    Jetpack plugin: Activate license key dropdown selection in Jetpack plugin's activation page. (#27974)
    Co-authored-by: sdixon194 <steve.dixon@automattic.com>

--- projects/plugins/jetpack/_inc/client/main.jsx
+++ projects/plugins/jetpack/_inc/client/main.jsx
@@ -68,7 +68,10 @@ import {
 	isWooCommerceActive,
 } from 'state/initial-state';
 import {
+	getDetachedLicenses as getAvailableLicenses,
+	getDetachedLicensesLoadingInfo as getFetchingAvailableLicense,
 	updateLicensingActivationNoticeDismiss as updateLicensingActivationNoticeDismissAction,
+	updateUserLicenses as updateUserLicensesAction,
 	updateUserLicensesCounts as updateUserLicensesCountsAction,
 } from 'state/licensing';
 import { fetchModules as fetchModulesAction } from 'state/modules';
@@ -166,6 +169,9 @@ class Main extends React.Component {
 				state: { previousPath: this.props.location.pathname },
 			} );
 		}
+
+		// Loads current user licenses
+		this.props.updateUserLicenses();
 	}
 
 	/*
@@ -475,10 +481,12 @@ class Main extends React.Component {
 					navComponent = null;
 					pageComponent = (
 						<ActivationScreen
+							availableLicenses={ this.props.availableLicenses }
+							currentRecommendationsStep={ this.props.currentRecommendationsStep }
+							fetchingAvailableLicenses={ this.props.fetchingAvailableLicenses }
 							siteRawUrl={ this.props.siteRawUrl }
 							onActivationSuccess={ this.onLicenseActivationSuccess }
 							siteAdminUrl={ this.props.siteAdminUrl }
-							currentRecommendationsStep={ this.props.currentRecommendationsStep }
 						/>
 					);
 				} else {
@@ -784,6 +792,8 @@ export default connect(
 			hasSeenWCConnectionModal: getHasSeenWCConnectionModal( state ),
 			partnerCoupon: getPartnerCoupon( state ),
 			currentRecommendationsStep: getInitialRecommendationsStep( state ),
+			availableLicenses: getAvailableLicenses( state ),
+			fetchingAvailableLicenses: getFetchingAvailableLicense( state ),
 		};
 	},
 	dispatch => ( {
@@ -798,6 +808,9 @@ export default connect(
 		},
 		resetConnectUser: () => {
 			return dispatch( resetConnectUser() );
+		},
+		updateUserLicenses: () => {
+			return dispatch( updateUserLicensesAction() );
 		},
 		updateLicensingActivationNoticeDismiss: () => {
 			return dispatch( updateLicensingActivationNoticeDismissAction() );
