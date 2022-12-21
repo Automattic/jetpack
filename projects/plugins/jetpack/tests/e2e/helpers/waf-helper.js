3f1111819dceb1ae70522801498024dfd4f58443
Nate Weller <nate.weller@automattic.com>
2022-12-21 10:04:14 -0700

    Update tests

--- projects/plugins/jetpack/tests/e2e/helpers/waf-helper.js
+++ projects/plugins/jetpack/tests/e2e/helpers/waf-helper.js
@@ -3,7 +3,10 @@ import logger from 'jetpack-e2e-commons/logger.cjs';
 
 export async function enableAutomaticRules() {
 	logger.sync( 'Enabling automatic firewall rules' );
-	const optionUpdated = execWpCommand( 'option update jetpack_waf_automatic_rules 1' );
-	const rulesGenerated = execWpCommand( 'jetpack-waf generate_rules' );
-	return optionUpdated && rulesGenerated;
+	return execWpCommand( 'option update jetpack_waf_automatic_rules 1' );
+}
+
+export async function generateRules() {
+	logger.sync( 'Generating firewall rules' );
+	return execWpCommand( 'jetpack-waf generate_rules' );
 }
