eecb80fd3d7f72594180b7deb5ffaa5c7adab648
Nate Weller <nate.weller@automattic.com>
2022-12-23 09:50:30 -0700

    WAF: Check if rule files exist before requiring them (#28050)

--- projects/packages/waf/src/class-waf-runner.php
+++ projects/packages/waf/src/class-waf-runner.php
@@ -212,8 +212,10 @@ class Waf_Runner {
 			$waf = new Waf_Runtime( new Waf_Transforms(), new Waf_Operators() );
 
 			// execute waf rules.
-			// phpcs:ignore
-			include self::RULES_FILE;
+			if ( file_exists( self::RULES_FILE ) ) {
+				// phpcs:ignore
+				include self::RULES_FILE;
+			}
 		} catch ( \Exception $err ) { // phpcs:ignore
 			// Intentionally doing nothing.
 		}
@@ -466,8 +468,8 @@ class Waf_Runner {
 		$ip_allow_rules = self::ALLOW_IP_FILE;
 		$ip_block_rules = self::BLOCK_IP_FILE;
 
-		$ip_list_code = "if ( require('$ip_allow_rules') ) { return; }\n" .
-			"if ( require('$ip_block_rules') ) { return \$waf->block('block', -1, 'ip block list'); }\n";
+		$ip_list_code = "if ( file_exists( '$ip_allow_rules' ) ) { if ( require( '$ip_allow_rules' ) ) { return; } }\n" .
+			"if ( file_exists( '$ip_block_rules' ) ) { if ( require( '$ip_block_rules' ) ) { return \$waf->block('block', -1, 'ip block list'); } }\n";
 
 		$rules_divided_by_line = explode( "\n", $rules );
 		array_splice( $rules_divided_by_line, 1, 0, $ip_list_code );
