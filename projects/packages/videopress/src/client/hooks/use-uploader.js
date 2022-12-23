adcc1ab28befd71fa34be2fb9d25ccec77f9c047
Damián Suárez <rdsuarez@gmail.com>
2022-12-23 13:56:13 +0000

    useResumableUploader() hook

--- projects/packages/videopress/src/client/hooks/use-uploader.js
+++ projects/packages/videopress/src/client/hooks/use-uploader.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import apiFetch from '@wordpress/api-fetch';
-import { useEffect, useState } from '@wordpress/element';
+import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import * as tus from 'tus-js-client';
 
@@ -27,114 +27,124 @@ export const getJWT = function () {
 	} );
 };
 
-export const uploadVideo = ( { file, onProgress, onSuccess, onError, data } ) => {
-	const upload = new tus.Upload( file, {
-		onError: onError,
-		onProgress: onProgress,
-		endpoint: data.url,
-		removeFingerprintOnSuccess: true,
-		withCredentials: false,
-		autoRetry: true,
-		overridePatchMethod: false,
-		chunkSize: 10000000, // 10 Mb.
-		metadata: {
-			filename: file.name,
-			filetype: file.type,
-		},
-		retryDelays: [ 0, 1000, 3000, 5000, 10000 ],
-		onAfterResponse: function ( req, res ) {
-			// Why is this not showing the x-headers?
-			if ( res.getStatus() >= 400 ) {
-				return;
-			}
+export const uploadVideo = ( { file, onProgress, onSuccess, onError, onGetJWT } ) => {
+	let upload;
 
-			const GUID_HEADER = 'x-videopress-upload-guid';
-			const MEDIA_ID_HEADER = 'x-videopress-upload-media-id';
-			const SRC_URL_HEADER = 'x-videopress-upload-src-url';
+	getJWT()
+		.then( data => {
+			onGetJWT && onGetJWT( data );
+			upload = new tus.Upload( file, {
+				onError: onError,
+				onProgress: onProgress,
+				endpoint: data.url,
+				removeFingerprintOnSuccess: true,
+				withCredentials: false,
+				autoRetry: true,
+				overridePatchMethod: false,
+				chunkSize: 10000000, // 10 Mb.
+				metadata: {
+					filename: file.name,
+					filetype: file.type,
+				},
+				retryDelays: [ 0, 1000, 3000, 5000, 10000 ],
+				onAfterResponse: function ( req, res ) {
+					// Why is this not showing the x-headers?
+					if ( res.getStatus() >= 400 ) {
+						return;
+					}
 
-			const guid = res.getHeader( GUID_HEADER );
-			const mediaId = res.getHeader( MEDIA_ID_HEADER );
-			const src = res.getHeader( SRC_URL_HEADER );
+					const GUID_HEADER = 'x-videopress-upload-guid';
+					const MEDIA_ID_HEADER = 'x-videopress-upload-media-id';
+					const SRC_URL_HEADER = 'x-videopress-upload-src-url';
 
-			if ( guid && mediaId && src ) {
-				onSuccess && onSuccess( { id: Number( mediaId ), guid, src }, file );
-				return;
-			}
+					const guid = res.getHeader( GUID_HEADER );
+					const mediaId = res.getHeader( MEDIA_ID_HEADER );
+					const src = res.getHeader( SRC_URL_HEADER );
 
-			const headerMap = {
-				'x-videopress-upload-key-token': 'token',
-				'x-videopress-upload-key': 'key',
-			};
+					if ( guid && mediaId && src ) {
+						onSuccess && onSuccess( { id: Number( mediaId ), guid, src }, file );
+						return;
+					}
 
-			const tokenData = {};
-			Object.keys( headerMap ).forEach( function ( header ) {
-				const value = res.getHeader( header );
-				if ( ! value ) {
-					return;
-				}
+					const headerMap = {
+						'x-videopress-upload-key-token': 'token',
+						'x-videopress-upload-key': 'key',
+					};
 
-				tokenData[ headerMap[ header ] ] = value;
-			} );
+					const tokenData = {};
+					Object.keys( headerMap ).forEach( function ( header ) {
+						const value = res.getHeader( header );
+						if ( ! value ) {
+							return;
+						}
 
-			if ( tokenData.key && tokenData.token ) {
-				jwtsForKeys[ tokenData.key ] = tokenData.token;
-			}
-		},
-		onBeforeRequest: function ( req ) {
-			// make ALL requests be either POST or GET to honor the public-api.wordpress.com "contract".
-			const method = req._method;
-			if ( [ 'HEAD', 'OPTIONS' ].indexOf( method ) >= 0 ) {
-				req._method = 'GET';
-				req.setHeader( 'X-HTTP-Method-Override', method );
-			}
-
-			if ( [ 'DELETE', 'PUT', 'PATCH' ].indexOf( method ) >= 0 ) {
-				req._method = 'POST';
-				req.setHeader( 'X-HTTP-Method-Override', method );
-			}
-
-			req._xhr.open( req._method, req._url, true );
-			// Set the headers again, reopening the xhr resets them.
-			Object.keys( req._headers ).map( function ( headerName ) {
-				req.setHeader( headerName, req._headers[ headerName ] );
-			} );
-
-			if ( 'POST' === method ) {
-				const hasJWT = !! data.token;
-				if ( hasJWT ) {
-					req.setHeader( 'x-videopress-upload-token', data.token );
-				} else {
-					throw 'should never happen';
-				}
-			}
-
-			if ( [ 'OPTIONS', 'GET', 'HEAD', 'DELETE', 'PUT', 'PATCH' ].indexOf( method ) >= 0 ) {
-				const url = new URL( req._url );
-				const path = url.pathname;
-				const parts = path.split( '/' );
-				const maybeUploadkey = parts[ parts.length - 1 ];
-				if ( jwtsForKeys[ maybeUploadkey ] ) {
-					req.setHeader( 'x-videopress-upload-token', jwtsForKeys[ maybeUploadkey ] );
-				} else if ( 'HEAD' === method ) {
-					return getJWT( maybeUploadkey ).then( responseData => {
-						jwtsForKeys[ maybeUploadkey ] = responseData.token;
-						req.setHeader( 'x-videopress-upload-token', responseData.token );
-						return req;
+						tokenData[ headerMap[ header ] ] = value;
 					} );
+
+					if ( tokenData.key && tokenData.token ) {
+						jwtsForKeys[ tokenData.key ] = tokenData.token;
+					}
+				},
+				onBeforeRequest: function ( req ) {
+					// make ALL requests be either POST or GET to honor the public-api.wordpress.com "contract".
+					const method = req._method;
+					if ( [ 'HEAD', 'OPTIONS' ].indexOf( method ) >= 0 ) {
+						req._method = 'GET';
+						req.setHeader( 'X-HTTP-Method-Override', method );
+					}
+
+					if ( [ 'DELETE', 'PUT', 'PATCH' ].indexOf( method ) >= 0 ) {
+						req._method = 'POST';
+						req.setHeader( 'X-HTTP-Method-Override', method );
+					}
+
+					req._xhr.open( req._method, req._url, true );
+					// Set the headers again, reopening the xhr resets them.
+					Object.keys( req._headers ).map( function ( headerName ) {
+						req.setHeader( headerName, req._headers[ headerName ] );
+					} );
+
+					if ( 'POST' === method ) {
+						const hasJWT = !! data.token;
+						if ( hasJWT ) {
+							req.setHeader( 'x-videopress-upload-token', data.token );
+						} else {
+							throw 'should never happen';
+						}
+					}
+
+					if ( [ 'OPTIONS', 'GET', 'HEAD', 'DELETE', 'PUT', 'PATCH' ].indexOf( method ) >= 0 ) {
+						const url = new URL( req._url );
+						const path = url.pathname;
+						const parts = path.split( '/' );
+						const maybeUploadkey = parts[ parts.length - 1 ];
+						if ( jwtsForKeys[ maybeUploadkey ] ) {
+							req.setHeader( 'x-videopress-upload-token', jwtsForKeys[ maybeUploadkey ] );
+						} else if ( 'HEAD' === method ) {
+							return getJWT( maybeUploadkey ).then( responseData => {
+								jwtsForKeys[ maybeUploadkey ] = responseData.token;
+								req.setHeader( 'x-videopress-upload-token', responseData.token );
+								return req;
+							} );
+						}
+					}
+
+					return Promise.resolve( req );
+				},
+			} );
+
+			upload.findPreviousUploads().then( function ( previousUploads ) {
+				if ( previousUploads.length ) {
+					upload.resumeFromPreviousUpload( previousUploads[ 0 ] );
 				}
-			}
 
-			return Promise.resolve( req );
-		},
-	} );
-
-	upload.findPreviousUploads().then( function ( previousUploads ) {
-		if ( previousUploads.length ) {
-			upload.resumeFromPreviousUpload( previousUploads[ 0 ] );
-		}
-
-		upload.start();
-	} );
+				upload.start();
+			} );
+		} )
+		.catch( jwtError => {
+			// setError( jwtError );
+			onError?.( jwtError );
+		} );
 
 	return upload;
 };
@@ -176,18 +186,8 @@ export const uploadFromLibrary = attachmentId => {
 };
 
 export const useResumableUploader = ( { onError, onProgress, onSuccess } ) => {
-	const [ data, setData ] = useState( {} );
-	const [ error, setError ] = useState( null );
-
-	// collect the jwt for the key
-	useEffect( () => {
-		getJWT()
-			.then( setData )
-			.catch( jwtError => {
-				setError( jwtError );
-				onError?.( jwtError );
-			} );
-	}, [] );
+	const [ data ] = useState( {} );
+	const [ error ] = useState( null );
 
 	const uploaded = file =>
 		uploadVideo( {
@@ -195,7 +195,6 @@ export const useResumableUploader = ( { onError, onProgress, onSuccess } ) => {
 			onProgress,
 			onSuccess,
 			onError,
-			data,
 		} );
 
 	return [ uploaded, data, error ];
