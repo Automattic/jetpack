1aecb29da63ab7414d976014ac2f0fb338a86ff1
Damián Suárez <rdsuarez@gmail.com>
2023-01-02 11:36:24 +0000

    move and create resumableFileUploader() helper

---
+++ projects/packages/videopress/src/client/lib/resumable-file-uploader/types.ts
@@ -0,0 +1,6 @@
+/**
+ * Internal dependencies
+ */
+import { VideoGuid, VideoId } from '../../block-editor/blocks/video/types';
+
+export type VideoMediaProps = { id: VideoId; guid: VideoGuid; src: string };
