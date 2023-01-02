52c00506497d35d993a0ec15870586d7009b8f1b
Maciej Grabowski <littlesoldiergraba@gmail.com>
2023-01-02 14:02:21 +0100

    [not verified] For each page return 'title' instead of 'post_title' prop.

--- projects/plugins/jetpack/json-endpoints/class.wpcom-json-api-list-dropdown-pages-endpoint.php
+++ projects/plugins/jetpack/json-endpoints/class.wpcom-json-api-list-dropdown-pages-endpoint.php
@@ -1,17 +1,17 @@
 <?php // phpcs:ignore WordPress.Files.FileName.InvalidClassFileName
 
 /**
- * List pages endpoint.
+ * List dropdown pages endpoint.
  */
 new WPCOM_JSON_API_List_Dropdown_Pages_Endpoint(
 	array(
 
-		'description'                          => 'Get a list of dropdown pages.',
+		'description'                          => 'Get a list of pages to be displayed as options in a select-a-page-dropdown.',
 		'min_version'                          => '1.1',
 		'max_version'                          => '1.1',
 
 		'group'                                => 'posts',
-		'stat'                                 => 'posts',
+		'stat'                                 => 'posts:dropdown-pages',
 
 		'method'                               => 'GET',
 		'path'                                 => '/sites/%s/dropdown-pages/',
@@ -26,9 +26,9 @@ new WPCOM_JSON_API_List_Dropdown_Pages_Endpoint(
 );
 
 /**
- * List pages endpoint class.
+ * Endpoint class responsible for listing pages to be displayed as options in a select-a-page-dropdown.
  *
- * /sites/%s/page-options/ -> $blog_id
+ * /sites/%s/dropdown-pages/ -> $blog_id
  */
 class WPCOM_JSON_API_List_Dropdown_Pages_Endpoint extends WPCOM_JSON_API_Post_Endpoint {
 	/**
@@ -44,7 +44,8 @@ class WPCOM_JSON_API_List_Dropdown_Pages_Endpoint extends WPCOM_JSON_API_Post_En
 	 * API callback.
 	 *
 	 * @param string $path - the path.
-	 * @param string $blog_id - the blog ID.
+	 * @param string $blog_id - the blog id.
+	 * @return stdClass[] $pages - array of objects containing only the ID and title fields. An empty array if no pages are found.
 	 */
 	public function callback( $path = '', $blog_id = 0 ) {
 		$blog_id = $this->api->switch_to_blog_and_validate_user( $this->api->get_blog_id( $blog_id ) );
@@ -57,9 +58,9 @@ class WPCOM_JSON_API_List_Dropdown_Pages_Endpoint extends WPCOM_JSON_API_Post_En
 	}
 
 	/**
-	 * Get pages.
+	 * Get all pages for the current site. The results are cached.
 	 *
-	 * @return stdClass[] $pages       Array of objects containing only the ID, post_parent, post_title, and post_status fields.
+	 * @return stdClass[] $pages - array of objects containing only the ID and title fields.
 	 */
 	protected static function get_pages() {
 		global $wpdb;
@@ -68,7 +69,7 @@ class WPCOM_JSON_API_List_Dropdown_Pages_Endpoint extends WPCOM_JSON_API_Post_En
 		$cache_key    = "get_pages:$last_changed";
 		$pages        = wp_cache_get( $cache_key, 'dropdown_pages' );
 		if ( false === $pages ) {
-			$pages = $wpdb->get_results( "SELECT {$wpdb->posts}.ID, {$wpdb->posts}.post_title FROM {$wpdb->posts} WHERE {$wpdb->posts}.post_type = 'page' AND {$wpdb->posts}.post_status = 'publish' ORDER BY {$wpdb->posts}.post_title ASC" );
+			$pages = $wpdb->get_results( "SELECT {$wpdb->posts}.ID, {$wpdb->posts}.post_title as title FROM {$wpdb->posts} WHERE {$wpdb->posts}.post_type = 'page' AND {$wpdb->posts}.post_status = 'publish' ORDER BY {$wpdb->posts}.post_title ASC" );
 			wp_cache_set( $cache_key, $pages, 'dropdown_pages' );
 		}
 
