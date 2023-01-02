53124e13fef419e7a556a00c482b888afc044bb7
Derek Smart <dsmart@dsmart-mbp.lan>
2023-01-02 14:47:58 -0500

    Blaze: Only show promote post row on published posts.

--- projects/packages/blaze/src/class-blaze.php
+++ projects/packages/blaze/src/class-blaze.php
@@ -95,6 +95,10 @@ class Blaze {
 	public function jetpack_blaze_row_action( $post_actions, $post ) {
 		$post_id = $post->ID;
 
+		if ( $post->post_status !== 'publish' ) {
+			return $post_actions;
+		}
+
 		// Might be useful to wrap in a method call for general use without post_id.
 		$blaze_url = Redirect::get_url(
 			'jetpack-blaze',
