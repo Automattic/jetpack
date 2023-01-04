960db7c47d6822cd5fc510cb4bc99c4e42ff9eff
Karen Attfield <karenlattfield@gmail.com>
2023-01-04 11:40:39 +0000

    Updating blaze package versions after new PR merge

--- projects/packages/blaze/src/class-blaze.php
+++ projects/packages/blaze/src/class-blaze.php
@@ -16,7 +16,7 @@ use Automattic\Jetpack\Sync\Settings as Sync_Settings;
  */
 class Blaze {
 
-	const PACKAGE_VERSION = '0.3.2-alpha';
+	const PACKAGE_VERSION = '0.3.2';
 
 	/**
 	 * The configuration method that is called from the jetpack-config package.
