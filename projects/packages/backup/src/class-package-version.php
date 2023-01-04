412489388dbd0c68666ac8717a006fec574d4107
Rafael Agostini <rafael.agostini@automattic.com>
2023-01-04 11:40:38 -0500

    Update backup package version to 1.10.7-alpha

--- projects/packages/backup/src/class-package-version.php
+++ projects/packages/backup/src/class-package-version.php
@@ -12,7 +12,7 @@ namespace Automattic\Jetpack\Backup;
  */
 class Package_Version {
 
-	const PACKAGE_VERSION = '1.10.6';
+	const PACKAGE_VERSION = '1.10.7-alpha';
 
 	const PACKAGE_SLUG = 'backup';
 
