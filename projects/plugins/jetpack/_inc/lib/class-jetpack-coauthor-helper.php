07ec87f573f2138a685cbc1400fa100dfbb3e398
artpi <artur.piszek@gmail.com>
2023-01-05 17:26:43 +0100

    caching

--- projects/plugins/jetpack/_inc/lib/class-jetpack-coauthor-helper.php
+++ projects/plugins/jetpack/_inc/lib/class-jetpack-coauthor-helper.php
@@ -73,12 +73,35 @@ class Jetpack_Coauthor_Helper {
 	}
 
 	/**
+	 * Get the name of the transient for image generation. Unique per prompt and allows for reuse of results for the same prompt across entire WPCOM.
+	 * I expext "puppy" to always be from cache.
+	 *
+	 * @param  string $prompt - Supplied prompt.
+	 */
+	public static function transient_name_for_image_generation( $prompt ) {
+		return 'openai-image-' . md5( $prompt );
+	}
+
+	/**
+	 * Get the name of the transient for text completion. Unique per user, but not per text. Serves more as a cooldown.
+	 */
+	public static function transient_name_for_completion() {
+		return 'openai-completion-' . get_current_user_id(); // Cache for each user, so that other users dont get weird cached version from somebody else.
+	}
+
+	/**
 	 * Get text back from WordPress.com based off a starting text.
 	 *
 	 * @param  string $content The content that's already been typed in the block.
 	 * @return mixed
 	 */
 	public static function get_gpt_completion( $content ) {
+		$content = wp_strip_all_tags( $content );
+		$cache   = get_transient( self::transient_name_for_completion() );
+		if ( $cache ) {
+			return array( 'prompts' => array( $cache ) );
+		}
+
 		if ( ( new Status() )->is_offline_mode() ) {
 			return new WP_Error(
 				'dev_mode',
@@ -86,7 +109,6 @@ class Jetpack_Coauthor_Helper {
 			);
 		}
 
-		$content = strip_tags( $content );
 		$site_id = self::get_site_id();
 		if ( is_wp_error( $site_id ) ) {
 			return $site_id;
@@ -97,7 +119,13 @@ class Jetpack_Coauthor_Helper {
 				\require_lib( 'openai' );
 			}
 
-			return ( new OpenAI( 'openai' ) )->request_gpt_completion( $content );
+			$result = ( new OpenAI( 'openai' ) )->request_gpt_completion( $content );
+			if ( is_wp_error( $result ) ) {
+				return $result;
+			}
+			// In case of Jetpack we are setting a transient on the WPCOM and not the remote site. I think the 'get_current_user_id' may default for the connection owner at this point but we'll deal with this later.
+			set_transient( self::transient_name_for_completion(), $result->choices[0], self::text_completion_cooldown_seconds );
+			return array( 'prompts' => $result->choices );
 		}
 
 		$response = Client::wpcom_json_api_request_as_blog(
@@ -116,6 +144,7 @@ class Jetpack_Coauthor_Helper {
 		if ( wp_remote_retrieve_response_code( $response ) >= 400 ) {
 			return new WP_Error( $data->code, $data->message, $data->data );
 		}
+		set_transient( self::transient_name_for_completion(), $data->choices[0], self::text_completion_cooldown_seconds );
 
 		return $data;
 	}
@@ -127,6 +156,11 @@ class Jetpack_Coauthor_Helper {
 	 * @return mixed
 	 */
 	public static function get_dalle_generation( $prompt ) {
+		$cache = get_transient( self::transient_name_for_image_generation( $prompt ) );
+		if ( $cache ) {
+			return $cache;
+		}
+
 		if ( ( new Status() )->is_offline_mode() ) {
 			return new WP_Error(
 				'dev_mode',
@@ -144,7 +178,12 @@ class Jetpack_Coauthor_Helper {
 				\require_lib( 'openai' );
 			}
 
-			return ( new OpenAI( 'openai' ) )->request_dalle_generation( $prompt );
+			$result = ( new OpenAI( 'openai' ) )->request_dalle_generation( $prompt );
+			if ( is_wp_error( $result ) ) {
+				return $result;
+			}
+			set_transient( self::transient_name_for_image_generation( $prompt ), $result, self::$image_generation_cache_timeout );
+			return $result;
 		}
 
 		$response = Client::wpcom_json_api_request_as_blog(
@@ -163,6 +202,7 @@ class Jetpack_Coauthor_Helper {
 		if ( wp_remote_retrieve_response_code( $response ) >= 400 ) {
 			return new WP_Error( $data->code, $data->message, $data->data );
 		}
+		set_transient( self::transient_name_for_image_generation( $prompt ), $data, self::$image_generation_cache_timeout );
 
 		return $data;
 	}
