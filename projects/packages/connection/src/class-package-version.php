55e8466625f9059a764610e2af19dff9cb989962
Jeremy Herve <jeremy@jeremy.hu>
2023-01-06 17:47:25 +0100

    Bump package version

--- projects/packages/connection/src/class-package-version.php
+++ projects/packages/connection/src/class-package-version.php
@@ -12,7 +12,7 @@ namespace Automattic\Jetpack\Connection;
  */
 class Package_Version {
 
-	const PACKAGE_VERSION = '1.48.2-alpha';
+	const PACKAGE_VERSION = '1.49.0-alpha';
 
 	const PACKAGE_SLUG = 'connection';
 
