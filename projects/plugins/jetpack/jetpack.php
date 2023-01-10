03cc8d8e5346d8ee5a589e5b8453bcc1cef2ec63
Karen Attfield <karenlattfield@gmail.com>
2023-01-10 11:30:25 +0000

    Updating version numbers to 11.7

--- projects/plugins/jetpack/jetpack.php
+++ projects/plugins/jetpack/jetpack.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://jetpack.com
  * Description: Security, performance, and marketing tools made by WordPress experts. Jetpack keeps your site protected so you can focus on more important things.
  * Author: Automattic
- * Version: 11.7-beta3
+ * Version: 11.7
  * Author URI: https://jetpack.com
  * License: GPL2+
  * Text Domain: jetpack
@@ -32,7 +32,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 define( 'JETPACK__MINIMUM_WP_VERSION', '6.0' );
 define( 'JETPACK__MINIMUM_PHP_VERSION', '5.6' );
-define( 'JETPACK__VERSION', '11.7-beta3' );
+define( 'JETPACK__VERSION', '11.7' );
 
 /**
  * Constant used to fetch the connection owner token
