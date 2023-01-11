f1956117ad06828d236fdf412ee29cd21948ced6
Kuba Birecki <kuba.birecki@crystalplanet.io>
2023-01-11 15:59:59 +0100

    Add forms package

---
+++ projects/packages/forms/src/class-jetpack-forms.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Package description here
+ *
+ * @package automattic/jetpack-forms
+ */
+
+namespace Automattic\Jetpack;
+
+/**
+ * Class description.
+ */
+class Jetpack_Forms {
+
+	const PACKAGE_VERSION = '1.0.0-alpha';
+
+}
