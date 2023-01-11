b1ec973d6b9ec1d2da943a82eaf6feb53498d546
Rafael Agostini <rafael.agostini@automattic.com>
2023-01-11 00:41:31 -0500

    Add BackupStorageSection component for Storage UI

---
+++ projects/packages/backup/src/js/components/backup-storage-section/use-usage-storage-text.ts
@@ -0,0 +1,52 @@
+import { useMemo } from '@wordpress/element';
+import { __, sprintf } from '@wordpress/i18n';
+
+const StorageUnits = {
+	Gigabyte: 2 ** 30,
+	Terabyte: 2 ** 40,
+};
+
+const getAppropriateStorageUnit = bytes => {
+	if ( bytes < StorageUnits.Terabyte ) {
+		return StorageUnits.Gigabyte;
+	}
+
+	return StorageUnits.Terabyte;
+};
+
+const bytesToUnit = ( bytes, unit ) => bytes / unit;
+
+export const useStorageUsageText = ( bytesUsed, bytesAvailable ) => {
+	return useMemo( () => {
+		if ( bytesUsed === undefined ) {
+			return null;
+		}
+
+		const usedGigabytes = bytesToUnit( bytesUsed, StorageUnits.Gigabyte );
+
+		if ( bytesAvailable === undefined ) {
+			// translators: Must use unit abbreviation; describes an amount of storage space in gigabytes (e.g., 15.4GB used)
+			return sprintf( __( '%1$dGB used', 'jetpack-backup-pkg' ), usedGigabytes );
+		}
+
+		const availableUnit = getAppropriateStorageUnit( bytesAvailable );
+		const availableUnitAmount = bytesToUnit( bytesAvailable, availableUnit );
+
+		if ( availableUnit === StorageUnits.Gigabyte ) {
+			// eslint-disable-next-line @wordpress/valid-sprintf
+			return sprintf(
+				// translators: Must use unit abbreviation; describes used vs available storage amounts (e.g., 20.0GB of 30GB used, 0.5GB of 20GB used)
+				__( '%1.1fGB used of %2.1fGB', 'jetpack-backup-pkg' ),
+				usedGigabytes,
+				availableUnitAmount
+			);
+		}
+
+		return sprintf(
+			// translators: Must use unit abbreviation; describes used vs available storage amounts (e.g., 20.0GB of 1TB used, 0.5GB of 2TB used)
+			__( '%1$dGB used of %2$dTB', 'jetpack-backup-pkg' ),
+			usedGigabytes,
+			availableUnitAmount
+		);
+	}, [ bytesUsed, bytesAvailable ] );
+};
