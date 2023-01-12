7ba69c08a4851acab4fc7ac115a5931c0b775054
Erick Danzer <edanzer@gmail.com>
2023-01-12 15:35:57 -0700

    Launchpad: Add launchpad feature plugin https://github.com/Automattic/wpcomsh/pull/1223
    Co-authored-by: Matt Wiebe <wiebe@automattic.com>

--- projects/plugins/wpcomsh/wpcomsh.php
+++ projects/plugins/wpcomsh/wpcomsh.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WordPress.com Site Helper
  * Description: A helper for connecting WordPress.com sites to external host infrastructure.
- * Version: 3.5.28
+ * Version: 3.5.29
  * Author: Automattic
  * Author URI: http://automattic.com/
  *
@@ -10,7 +10,7 @@
  */
 
 // Increase version number if you change something in wpcomsh.
-define( 'WPCOMSH_VERSION', '3.5.28' );
+define( 'WPCOMSH_VERSION', '3.5.29' );
 
 // If true, Typekit fonts will be available in addition to Google fonts
 add_filter( 'jetpack_fonts_enable_typekit', '__return_true' );
@@ -80,6 +80,7 @@ require_once __DIR__ . '/endpoints/rest-api.php';
 require_once __DIR__ . '/feature-plugins/additional-css.php';
 require_once __DIR__ . '/feature-plugins/autoload-homepage-replacement.php';
 require_once __DIR__ . '/feature-plugins/autosave-revision.php';
+require_once __DIR__ . '/feature-plugins/class-wpcom-launchpad.php';
 require_once __DIR__ . '/feature-plugins/coblocks-mods.php';
 require_once __DIR__ . '/feature-plugins/full-site-editing.php';
 require_once __DIR__ . '/feature-plugins/google-fonts.php';
