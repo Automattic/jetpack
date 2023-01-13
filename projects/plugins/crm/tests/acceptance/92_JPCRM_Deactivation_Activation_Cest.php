f55694c40b5703be8ac1150ebea1e81d94a5d5fe
Brad Jorsch <brad.jorsch@automattic.com>
2023-01-13 12:12:39 -0500

    crm: Set up infrastructure to run tests in CI
    Import the tests from the old private repo, then set up the
    infrastructure to run them within our CI.
    
    This doesn't make them actually pass, it seems there are a few things
    still broken, e.g.
    
    * It expects to be in `wp-content/plugins/zero-bs-crm` but the CI puts
      it at `wp-content/plugins/crm`.
    * `includes/lib/dompdf-2/vendor/` is missing.
    * A few tests fail after fixing that too.

---
+++ projects/plugins/crm/tests/acceptance/92_JPCRM_Deactivation_Activation_Cest.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Tests to check the deactivation, activation the core plugin and the extension plugins
+ */
+class JPCRM_Deactivation_Activation_Cest {
+
+	private $plugin_slug = 'zero-bs-crm';
+
+	public function _before( AcceptanceTester $I ) {
+		$I->loginAsAdmin();
+	}
+
+	public function jpcrm_deactivation( AcceptanceTester $I ) {
+		$I->amOnPluginsPage();
+
+		$I->seePluginActivated( $this->plugin_slug );
+		$I->deactivatePlugin( $this->plugin_slug );
+
+		$I->see( 'Before you go' );
+		$I->seeInSource( 'https://forms.gle/q5KjMBytni3kfFco7' );
+		$I->see( 'Not right now' );
+
+		$I->click( 'Not right now' );
+		$I->seeInCurrentUrl( '/plugins.php' );
+		$I->see( 'Plugins' );
+	}
+
+	public function jpcrm_force_wizard( AcceptanceTester $I ) {
+		$I->amOnPluginsPage();
+		// Plugin should be deactivated
+		$I->seePluginDeactivated( $this->plugin_slug );
+		$I->activatePlugin( $this->plugin_slug );
+
+		$I->gotoAdminPage( 'dashboard', '&jpcrm_force_wizard=1' );
+		$I->see( 'Essential Details' );
+		$I->see( 'Essentials' );
+		$I->see( 'Your Customers' );
+		$I->see( 'Which Extensions?' );
+	}
+
+	public function jpcrm_is_activated( AcceptanceTester $I ) {
+		$I->amOnPluginsPage();
+		$I->seePluginActivated( $this->plugin_slug );
+
+		// Check that Jetpack CRM menu is there
+		$I->amOnPage( 'wp-admin' );
+		$I->see( 'Jetpack CRM', '.wp-menu-name' );
+	}
+
+	public function jpcrm_activate_invoicing_pro_extension( AcceptanceTester $I ) {
+		$I->amOnPluginsPage();
+		$I->seePluginInstalled( 'jetpack-crm-extension-invoicing-pro' );
+		$I->activatePlugin( 'jetpack-crm-extension-invoicing-pro' );
+
+		$I->goToPageViaSlug( 'settings', '&tab=invpro' );
+		$I->see( 'Payments', '#zbs-settings-menu .item' );
+		$I->see( 'Invoicing Pro Payments' );
+	}
+
+	public function jpcrm_deactivate_core_before_extension( AcceptanceTester $I ) {
+		$I->amOnPluginsPage();
+
+		$I->deactivatePlugin( $this->plugin_slug );
+
+		$I->see( 'Before you go' );
+	}
+}
