6689e5c717fb29b817ca7fe8a3f301e927dae394
Fabien MILLERAND <fab@millerf.com>
2023-01-13 17:44:29 +0100

    Push examples to a separate file

---
+++ projects/plugins/jetpack/extensions/blocks/ai-image/dalle-example-prompts.js
@@ -0,0 +1,12 @@
+export const dalleExamplePrompts = [
+	'A digital Illustration of the a purely mechanical television, 4k, detailed, fantasy vivid colors',
+	'3D render of a floating futuristic castle in a clear sky, digital art',
+	'hedgehog smelling a flower | clear blue sky | intricate artwork by Beatrix Potter | cottagecore aesthetic | 8K | highly detailed | wide angle |',
+	'hyper realistic photo of a high end futuristic single-level house where walls are made of windows, light coming through the window, mid century modern style, cinematic lighting',
+	'botticelli’s simonetta vespucci young portrait photography hyperrealistic modern dressed, futuristic',
+	'pink ape Astronaut in space holding a claymate in a photorealistic style, digital art',
+	'studio photography set of high detail irregular marble stones with gold lines stacked in impossible balance, perfect composition, cinematic light photo studio, beige color scheme, indirect lighting, 8k, elegant and luxury style',
+	'Ultrawide, highway,  with golden ratio style, 4K , sides  are prairies, light is Golden hour, sky with red Clouds, ultrarealistic, CinémaScope, ultra wide format, ratio 16/9, 1/1000 sec, maximum resolution, Sharp details',
+	'a photo of cat flying out to space as an astronaut, digital art',
+	'a hyper realistic rilakkuma enjoying an icecream in the snow',
+];
