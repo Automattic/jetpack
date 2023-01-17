2ad651c879d8161cbe6bf7bd4fad9e378f8c6312
Kuba Birecki <kuba.birecki@crystalplanet.io>
2023-01-17 13:39:47 +0100

    Update version and namespace on the plugin class

--- projects/packages/forms/src/class-jetpack-forms.php
+++ projects/packages/forms/src/class-jetpack-forms.php
@@ -5,13 +5,13 @@
  * @package automattic/jetpack-forms
  */
 
-namespace Automattic\Jetpack;
+namespace Automattic\Jetpack\Forms;
 
 /**
  * Class description.
  */
 class Jetpack_Forms {
 
-	const PACKAGE_VERSION = '1.0.0-alpha';
+	const PACKAGE_VERSION = '0.1.0';
 
 }
