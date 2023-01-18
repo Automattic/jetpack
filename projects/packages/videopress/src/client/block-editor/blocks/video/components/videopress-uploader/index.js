18aaf14fd421d33f3f84e704b6cced023201d92a
Damián Suárez <rdsuarez@gmail.com>
2023-01-18 12:13:26 +0000

    clean

--- projects/packages/videopress/src/client/block-editor/blocks/video/components/videopress-uploader/index.js
+++ projects/packages/videopress/src/client/block-editor/blocks/video/components/videopress-uploader/index.js
@@ -193,45 +193,6 @@ const VideoPressUploader = ( {
 
 		// Handle selection of Media Library regular attachment
 		if ( media.id ) {
-			/*
-			 * Check if the selected media is hosted in VideoPress,
-			 * based on the media URL.
-			 * Dotcom Media Library doesn't provide the guid.
-			 */
-			const isVideoPressUrl = media.url?.includes( 'files.wordpress.com' );
-			if ( isVideoPressUrl ) {
-				apiFetch( {
-					path: `wp/v2/media/${ media.id }`,
-				} )
-					.then( ( mediaItem = {} ) => {
-						const { jetpack_videopress: videopressDetails } = mediaItem;
-						if ( ! videopressDetails || Object.keys( videopressDetails ).length === 0 ) {
-							return;
-						}
-
-						const blockAttributes = {
-							id: media.id,
-							guid: videopressDetails.guid,
-							allowDownload: !! videopressDetails.allow_download,
-							title: videopressDetails.title,
-							description: videopressDetails.description,
-							displayEmbed: !! videopressDetails.display_embed,
-							privacySetting: videopressDetails.privacy_setting,
-							rating: videopressDetails.rating,
-						};
-
-						setAttributes( blockAttributes );
-
-						const { url } = buildVideoPressURL( videopressDetails.guid, blockAttributes );
-						onSelectURL( url, media.id );
-					} )
-					.catch( error => {
-						// ToDo" Better error handling
-						console.error( error ); // eslint-disable-line no-console
-					} );
-				return;
-			}
-
 			if ( isSimpleSite() ) {
 				// It should not try to move the file in Simple sites.
 				return;
