3959548f14b3dc16743359525a4c940f56cb7745
Samiff <samiff@users.noreply.github.com>
2023-01-19 15:24:46 -0700

    Add jetpack-mu-wpcom-plugin skeleton

---
+++ projects/plugins/jetpack-mu-wpcom-plugin/jetpack-mu-wpcom-plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *
+ * Plugin Name: WordPress.com Features
+ * Plugin URI: TBD
+ * Description: A testing plugin for the jetpack-mu-wpcom package
+ * Version: 0.1.0-alpha
+ * Author: Automattic
+ * Author URI: https://jetpack.com/
+ * License: GPLv2 or later
+ * Text Domain: jetpack
+ *
+ * @package automattic/jetpack-mu-wpcom-plugin
+ */
+
+/**
+ * Include the composer autoloader.
+ */
+require_once __DIR__ . '/vendor/autoload.php';
