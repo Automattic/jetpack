84782a056f8949944f6f6e325fc039f2559b5d6d
Nauris Pūķis <hi@pyronaur.com>
2023-01-20 17:59:58 +0200

    Setup Svelte & TypeScript

---
+++ projects/js-packages/svelte-data-sync-client/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'd63a0f5e3a1435688e3a');
---
+++ projects/js-packages/svelte-data-sync-client/build/index.d.ts
@@ -0,0 +1,2 @@
+declare const store: import("svelte/store").Writable<number>;
+export default store;
