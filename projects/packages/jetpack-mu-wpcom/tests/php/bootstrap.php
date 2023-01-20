2e9271cbe267fdc7af0824a79380695ac3efe0f9
Samiff <samiff@users.noreply.github.com>
2023-01-20 14:38:35 -0700

    Init main class in test bootstrap

--- projects/packages/jetpack-mu-wpcom/tests/php/bootstrap.php
+++ projects/packages/jetpack-mu-wpcom/tests/php/bootstrap.php
@@ -11,4 +11,6 @@
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 \WorDBless\Load::load();
+
 require_once __DIR__ . '/../../src/class-jetpack-mu-wpcom.php';
+Automattic\Jetpack\Jetpack_Mu_Wpcom::init();
