605dc6c39249a0523dd6d65e70249a7f4adaa654
Brad Jorsch <anomiex@users.noreply.github.com>
2023-01-23 17:28:11 -0500

    Update node to 18.13.0 (#28450)

--- projects/packages/wordads/src/class-package.php
+++ projects/packages/wordads/src/class-package.php
@@ -11,7 +11,7 @@ namespace Automattic\Jetpack\WordAds;
  * WordAds package general information
  */
 class Package {
-	const VERSION = '0.2.28';
+	const VERSION = '0.2.29-alpha';
 	const SLUG    = 'wordads';
 
 	/**
