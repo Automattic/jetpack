9327233bb76110052a81f7f6c07a02dfeeaeb04f
Kuba Birecki <kuba.birecki@crystalplanet.io>
2023-01-24 12:05:44 +0100

    [not verified] Move contact form PHP files over to the new package

--- projects/packages/forms/src/class-jetpack-forms.php
+++ projects/packages/forms/src/class-jetpack-forms.php
@@ -7,8 +7,10 @@
 
 namespace Automattic\Jetpack\Forms;
 
+use Automattic\Jetpack\Forms\ContactForm\Util;
+
 /**
- * Class description.
+ * Understands the Jetpack Forms package.
  */
 class Jetpack_Forms {
 
@@ -18,5 +20,19 @@ class Jetpack_Forms {
 	 * Load the contact form module.
 	 */
 	public static function load_contact_form() {
+		Util::init();
+
+		if ( is_admin() && apply_filters( 'tmp_grunion_allow_editor_view', true ) ) {
+			add_action( 'current_screen', '\Automattic\Jetpack\Forms\ContactForm\Editor_View::add_hooks' );
+		}
+
+		add_action( 'init', '\Automattic\Jetpack\Forms\ContactForm\Util::register_pattern' );
+	}
+
+	/**
+	 * Get the plugin URL.
+	 */
+	public static function plugin_url() {
+		return plugin_dir_url( __FILE__ );
 	}
 }
