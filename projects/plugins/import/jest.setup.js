b0353f00f8dc919d700042501e1be6ce744a2b94
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2023-01-26 08:19:37 +0100

    Remove unnecessary files.

--- projects/plugins/import/jest.setup.js
+++
@@ -1,7 +0,0 @@
-window.JP_CONNECTION_INITIAL_STATE = {
-	userConnectionData: {
-		currentUser: {
-			wpcomUser: { Id: 99999, login: 'bobsacramento', display_name: 'Bob Sacrmaneto' },
-		},
-	},
-};
--- projects/plugins/import/src/class-jetpack-import.php
+++ projects/plugins/import/src/class-jetpack-import.php
@@ -9,9 +9,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-use Automattic\Jetpack\Admin_UI\Admin_Menu;
-use Automattic\Jetpack\Assets;
-use Automattic\Jetpack\Connection\Initial_State as Connection_Initial_State;
 use Automattic\Jetpack\Connection\Manager as Connection_Manager;
 use Automattic\Jetpack\Connection\Rest_Authentication as Connection_Rest_Authentication;
 use Automattic\Jetpack\My_Jetpack\Initializer as My_Jetpack_Initializer;
@@ -35,16 +32,6 @@ class Jetpack_Import {
 	public function __construct() {
 		// Set up the REST authentication hooks.
 		Connection_Rest_Authentication::init();
-
-		$page_suffix = Admin_Menu::add_menu(
-			__( 'Jetpack Import', 'jetpack-import' ),
-			_x( 'Import', 'The Jetpack Import product name, without the Jetpack prefix', 'jetpack-import' ),
-			'manage_options',
-			'jetpack-import',
-			array( $this, 'plugin_settings_page' ),
-			99
-		);
-		add_action( 'load-' . $page_suffix, array( $this, 'admin_init' ) );
 
 		// Init Jetpack packages
 		add_action(
@@ -85,64 +72,6 @@ class Jetpack_Import {
 		);
 
 		$this->rest_api = REST_API::register( $routes );
-	}
-
-	/**
-	 * Initialize the admin resources.
-	 */
-	public function admin_init() {
-		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
-	}
-
-	/**
-	 * Enqueue plugin admin scripts and styles.
-	 */
-	public function enqueue_admin_scripts() {
-
-		Assets::register_script(
-			'jetpack-import',
-			'build/index.js',
-			JETPACK_IMPORT_ROOT_FILE,
-			array(
-				'in_footer'  => true,
-				'textdomain' => 'jetpack-import',
-			)
-		);
-		Assets::enqueue_script( 'jetpack-import' );
-		// Initial JS state including JP Connection data.
-		wp_add_inline_script( 'jetpack-import', Connection_Initial_State::render(), 'before' );
-		wp_add_inline_script( 'jetpack-import', $this->render_initial_state(), 'before' );
-	}
-
-	/**
-	 * Render the initial state into a JavaScript variable.
-	 *
-	 * @return string
-	 */
-	public function render_initial_state() {
-		return 'var jetpackImportInitialState=JSON.parse(decodeURIComponent("' . rawurlencode( wp_json_encode( $this->initial_state() ) ) . '"));';
-	}
-
-	/**
-	 * Get the initial state data for hydrating the React UI.
-	 *
-	 * @return array
-	 */
-	public function initial_state() {
-		return array(
-			'apiRoot'           => esc_url_raw( rest_url() ),
-			'apiNonce'          => wp_create_nonce( 'wp_rest' ),
-			'registrationNonce' => wp_create_nonce( 'jetpack-registration-nonce' ),
-		);
-	}
-
-	/**
-	 * Main plugin settings page.
-	 */
-	public function plugin_settings_page() {
-		?>
-			<div id="jetpack-import-root"></div>
-		<?php
 	}
 
 	/**
