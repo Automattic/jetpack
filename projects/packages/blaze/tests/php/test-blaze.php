b814d73953fd69da6c95edf41667aee94cb74bfd
Jeremy Herve <jeremy@jeremy.hu>
2023-01-26 15:44:02 +0100

    Add basic test for init method

--- projects/packages/blaze/tests/php/test-blaze.php
+++ projects/packages/blaze/tests/php/test-blaze.php
@@ -15,6 +15,16 @@ use WorDBless\BaseTestCase;
  */
 class Test_Blaze extends BaseTestCase {
 	/**
+	 * Test that Blaze::init() does not run everything by default.
+	 *
+	 * @covers Automattic\Jetpack\Blaze::init
+	 */
+	public function test_should_initialize() {
+		$this->assertFalse( has_action( 'post_row_actions', 'Blaze::jetpack_blaze_row_action' ), 'post_row_actions' );
+		$this->assertFalse( has_action( 'admin_enqueue_scripts', 'Blaze::enqueue_block_editor_assets' ), 'admin_enqueue_scripts' );
+	}
+
+	/**
 	 * Test that the jetpack_blaze_enabled filter overwrites eligibility.
 	 *
 	 * @covers Automattic\Jetpack\Blaze::should_initialize
@@ -23,5 +33,6 @@ class Test_Blaze extends BaseTestCase {
 		$this->assertFalse( Blaze::should_initialize() );
 		add_filter( 'jetpack_blaze_enabled', '__return_true' );
 		$this->assertTrue( Blaze::should_initialize() );
+		add_filter( 'jetpack_blaze_enabled', '__return_false' );
 	}
 }
