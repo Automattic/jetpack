4d2e83b72ebb8eeba8ed2f9d713c0ed7cc5058e3
Donncha Ó Caoimh <5656673+donnchawp@users.noreply.github.com>
2023-01-27 17:23:39 +0000

    Form Block: Add styling for input fields (#27837)
    Co-authored-by: Kuba Birecki <kuba.birecki@crystalplanet.io>
    Co-authored-by: João Heringer <joao.heringer@automattic.com>

--- projects/plugins/jetpack/extensions/blocks/contact-form/components/jetpack-field-label.js
+++ projects/plugins/jetpack/extensions/blocks/contact-form/components/jetpack-field-label.js
@@ -2,6 +2,7 @@ import { RichText } from '@wordpress/block-editor';
 import { useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { isNil } from 'lodash';
+import { useJetpackFieldStyles } from './use-jetpack-field-styles';
 
 const JetpackFieldLabel = ( {
 	setAttributes,
@@ -11,6 +12,7 @@ const JetpackFieldLabel = ( {
 	resetFocus,
 	required,
 	requiredText,
+	attributes,
 } ) => {
 	useEffect( () => {
 		if ( isNil( requiredText ) ) {
@@ -19,36 +21,40 @@ const JetpackFieldLabel = ( {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [] );
 
+	const { labelStyle } = useJetpackFieldStyles( attributes );
+
 	return (
 		<div className="jetpack-field-label">
-			<RichText
-				tagName="label"
-				value={ label }
-				className="jetpack-field-label__input"
-				onChange={ value => {
-					resetFocus && resetFocus();
-					if ( labelFieldName ) {
-						setAttributes( { [ labelFieldName ]: value } );
-						return;
-					}
-					setAttributes( { label: value } );
-				} }
-				placeholder={ placeholder ?? __( 'Add label…', 'jetpack' ) }
-				withoutInteractiveFormatting
-				allowedFormats={ [ 'core/bold', 'core/italic' ] }
-			/>
-			{ required && (
+			<div style={ labelStyle }>
 				<RichText
-					tagName="span"
-					value={ requiredText }
-					className="required"
+					tagName="label"
+					value={ label }
+					className="jetpack-field-label__input"
 					onChange={ value => {
-						setAttributes( { requiredText: value } );
+						resetFocus && resetFocus();
+						if ( labelFieldName ) {
+							setAttributes( { [ labelFieldName ]: value } );
+							return;
+						}
+						setAttributes( { label: value } );
 					} }
+					placeholder={ placeholder ?? __( 'Add label…', 'jetpack' ) }
 					withoutInteractiveFormatting
 					allowedFormats={ [ 'core/bold', 'core/italic' ] }
 				/>
-			) }
+				{ required && (
+					<RichText
+						tagName="span"
+						value={ requiredText }
+						className="required"
+						onChange={ value => {
+							setAttributes( { requiredText: value } );
+						} }
+						withoutInteractiveFormatting
+						allowedFormats={ [ 'core/bold', 'core/italic' ] }
+					/>
+				) }
+			</div>
 		</div>
 	);
 };
