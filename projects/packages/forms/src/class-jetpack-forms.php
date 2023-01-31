f1bb0d48bc08c1efc828b6109895f62b7344fab2
Jeremy Herve <jeremy@jeremy.hu>
2023-01-31 12:17:59 +0100

    Update versions

--- projects/packages/forms/src/class-jetpack-forms.php
+++ projects/packages/forms/src/class-jetpack-forms.php
@@ -14,7 +14,7 @@ use Automattic\Jetpack\Forms\ContactForm\Util;
  */
 class Jetpack_Forms {
 
-	const PACKAGE_VERSION = '0.2.0';
+	const PACKAGE_VERSION = '0.3.0-alpha';
 
 	/**
 	 * Load the contact form module.
