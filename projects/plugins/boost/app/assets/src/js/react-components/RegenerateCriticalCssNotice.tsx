f64827351c08c338a5649642b1b0211020bb5eb7
Adnan Haque <adnan007.id@gmail.com>
2023-02-01 15:51:33 +0600

    Make boost settings regenerate suggestion dismissable

--- projects/plugins/boost/app/assets/src/js/react-components/RegenerateCriticalCssNotice.tsx
+++ projects/plugins/boost/app/assets/src/js/react-components/RegenerateCriticalCssNotice.tsx
@@ -1,9 +1,22 @@
 import { Notice } from '@automattic/jetpack-components';
+import { useState } from 'react';
 import { __ } from '@wordpress/i18n';
 
 export const RegenerateCriticalCssNotice = () => {
+	const [ isDismissed, setIsDismissed ] = useState( false );
+
+	if ( isDismissed ) {
+		return null;
+	}
+
 	return (
-		<Notice level="info" title={ __( 'Regenerate Critical CSS', 'jetpack-boost' ) }>
+		<Notice
+			level="info"
+			title={ __( 'Regenerate Critical CSS', 'jetpack-boost' ) }
+			onClose={ () => {
+				setIsDismissed( true );
+			} }
+		>
 			<p>
 				{ __(
 					'We noticed some updates to your site that may have changed your HTML/CSS structure.',
