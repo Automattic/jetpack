5a4ec6c3f4809ddffc711d64e96f6d1f61ebe2fb
Damián Suárez <rdsuarez@gmail.com>
2023-02-02 12:25:50 +0000

    add is_registrant_plugin_active() method

--- projects/packages/videopress/src/class-status.php
+++ projects/packages/videopress/src/class-status.php
@@ -21,16 +21,23 @@ class Status {
 	 * @return boolean
 	 */
 	public static function is_active() {
-		return self::is_jetpack_plugin_and_module_active() || self::is_standalone_plugin_active();
+		return self::is_jetpack_plugin_and_videooress_module_active() || self::is_standalone_plugin_active();
 	}
 
 	/**
-	 * Returns whether the Jetpack plugin
+	 * Checks whether the Jetpack plugin is active
+	 */
+	public static function is_jetpack_plugin_active() {
+		return class_exists( 'Jetpack' );
+	}
+
+	/**
+	 * Checks whether the Jetpack plugin
 	 * and its VideoPress module are active.
 	 *
 	 * @return boolean
 	 */
-	public static function is_jetpack_plugin_and_module_active() {
+	public static function is_jetpack_plugin_and_videooress_module_active() {
 		return class_exists( 'Jetpack' ) && Jetpack::is_module_active( 'videopress' );
 	}
 
@@ -41,5 +48,16 @@ class Status {
 	 */
 	public static function is_standalone_plugin_active() {
 		return class_exists( 'Jetpack_VideoPress_Plugin' );
+	}
+
+	/**
+	 * Checks whether the registrant plugin is active
+	 * either as a Jetpack module (via Jetpack plugin)
+	 * or as a stand-alone plugin.
+	 *
+	 * @return boolean True if the register plugin is active.
+	 */
+	public static function is_registrant_plugin_active() {
+		return self::is_jetpack_plugin_active() || self::is_standalone_plugin_active();
 	}
 }
