80acbae94f5dd47aabeb7f650313522bc597c53c
jcheringer <jcheringer@users.noreply.github.com>
2023-02-03 14:58:07 -0300

    Move Forms blocks to the Forms package (#28630)
    * [not verified] Move Forms blocks to Forms package
    
    * [not verified] changelog
    
    * Load editor scripts
    
    * [not verified] Update Form blocks with commit 4d2e83b72ebb8eeba8ed2f9d713c0ed7cc5058e3
    
    * Adjust linter
    
    * Update pnpm-lock file
    
    * Temporarily remove tests
    
    * Code clean up
    
    * Put 'legacy' blocks registration behind a flag
    
    * changelog
    
    * Fix allowed text domain
    
    * Improve scripts enqueueing
    
    * Fix build commnad
    
    * Fix allowed text domain
    
    * Adjust eslint file
    
    * Exclude raw block files from final build
    
    * Improve composer.json file structure
    
    * Update composer.lock file

---
+++ projects/packages/forms/src/blocks/contact-form/components/help-message/index.js
@@ -0,0 +1,20 @@
+import classNames from 'classnames';
+import GridiconNoticeOutline from 'gridicons/dist/notice-outline';
+import './style.scss';
+
+export default ( { children = null, isError = false, ...props } ) => {
+	const classes = classNames( 'help-message', {
+		'help-message-is-error': isError,
+	} );
+
+	return (
+		children && (
+			<div className={ classes } { ...props }>
+				{ isError && (
+					<GridiconNoticeOutline size="24" aria-hidden="true" role="img" focusable="false" />
+				) }
+				<span>{ children }</span>
+			</div>
+		)
+	);
+};
