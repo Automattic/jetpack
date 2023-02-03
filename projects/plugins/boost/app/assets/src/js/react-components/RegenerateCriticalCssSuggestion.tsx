ea480fca1b82a597d540b08abae6bc03ae430ef1
Adnan Haque <adnan007.id@gmail.com>
2023-02-03 17:29:58 +0600

    Hide close button on regenerate notice on boost dashboard

--- projects/plugins/boost/app/assets/src/js/react-components/RegenerateCriticalCssSuggestion.tsx
+++ projects/plugins/boost/app/assets/src/js/react-components/RegenerateCriticalCssSuggestion.tsx
@@ -14,6 +14,7 @@ export const RegenerateCriticalCssSuggestion = ( { show } ) => {
 			onClose={ () => {
 				hideRegenerateCriticalCssSuggestion();
 			} }
+			hideCloseButton={ true }
 		>
 			<p>
 				{ __(
