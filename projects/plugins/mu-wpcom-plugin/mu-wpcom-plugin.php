c9af508f878d2703f916621357854dfd3d4e2050
Samiff <samiff@users.noreply.github.com>
2023-02-06 12:17:52 -0700

    jetpack-mu-wpcom: migrate coming-soon to package (#28498)
    * Add jetpack-mu-wpcom-plugin skeleton
    
    * Require the jetpack-mu-wpcom package
    
    * Add test bootstrap
    
    * Add some initialization handling
    
    * Migrate code from Automattic/jetpack/pull/27815
    
    Copy/paste of the code, will refactor in following commits.
    
    * changelog
    
    * Require wordbless
    
    * Update paths and namespaces
    
    * Add wordbless to correct composer file
    
    * Init main class in test bootstrap
    
    * changelogs and validity
    
    * Remove plugins/jetpack-mu-wpcom-plugin dir
    
    We are using plugins/mu-wpcom-plugin instead.
    
    * Backport v1.0.0 version release and init next
    
    * Add init for Jetpack_Mu_Wpcom and coming-soon

--- projects/plugins/mu-wpcom-plugin/mu-wpcom-plugin.php
+++ projects/plugins/mu-wpcom-plugin/mu-wpcom-plugin.php
@@ -3,7 +3,7 @@
  *
  * Plugin Name: WordPress.com Features
  * Description: Test plugin for the jetpack-mu-wpcom package
- * Version: 0.1.0-alpha
+ * Version: 1.0.1-alpha
  * Author: Automattic
  * License: GPLv2 or later
  * Text Domain: jetpack-mu-wpcom-plugin
@@ -17,5 +17,8 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 /**
- * Todo: init/config for the package class Jetpack_Mu_Wpcom
+ * Jetpack_Mu_Wpcom initialization.
  */
+if ( class_exists( 'Automattic\Jetpack\Jetpack_Mu_Wpcom' ) ) {
+	Automattic\Jetpack\Jetpack_Mu_Wpcom::init();
+}
