10dd417bb0e712e7f5a44fe73d00bcdcce4304d6
Nate Weller <nate.weller@automattic.com>
2023-02-07 11:34:16 -0700

    Generate packages/ip

---
+++ projects/packages/ip/src/class-ip.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Package description here
+ *
+ * @package automattic/jetpack-ip
+ */
+
+namespace Automattic\Jetpack;
+
+/**
+ * Class description.
+ */
+class IP {
+
+	const PACKAGE_VERSION = '1.0.0-alpha';
+
+}
