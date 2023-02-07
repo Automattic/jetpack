2e3abb8a0ee736d2456693e38186e648966fb751
Steve D <33553323+sdixon194@users.noreply.github.com>
2023-02-07 14:05:26 -0500

    Release Script: Check for remote release branch first (#28730)
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>

--- tools/release-plugin.sh
+++ tools/release-plugin.sh
@@ -119,6 +119,21 @@ fi
 
 proceed_p "Releasing $PROJECT $VERSION" "Proceed?"
 
+# Check if a remote branch for the release branch exits and ask to delete it if it does.
+PREFIX=$(jq -r '.extra["release-branch-prefix"] // ""' "$BASE"/projects/"$PROJECT"/composer.json)
+RELEASE_BRANCH=
+if [[ -n "$PREFIX" ]]; then
+	RELEASE_BRANCH="$PREFIX/branch-${VERSION%%-*}"
+else
+	die "No release branch prefix found for $PROJECT, aborting."
+fi
+
+REMOTE_BRANCH="$(git ls-remote origin "$RELEASE_BRANCH")"
+if [[ -n "$REMOTE_BRANCH" ]]; then
+	proceed_p "Existing release branch $RELEASE_BRANCH found." "Delete it before continuing?"
+	git push origin --delete "$RELEASE_BRANCH"
+fi
+
 # Make sure we're standing on trunk and working directory is clean
 CURRENT_BRANCH="$( git rev-parse --abbrev-ref HEAD )"
 if [[ "$CURRENT_BRANCH" != "trunk" ]]; then
