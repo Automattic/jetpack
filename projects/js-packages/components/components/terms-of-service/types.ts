055998dced537ac1630feba366fae23f13d23e63
Matt Gawarecki <matt.gawarecki@automattic.com>
2023-02-09 10:23:40 -0600

    [not verified] Refine TermsOfServiceProps to show mutual exclusivity

--- projects/js-packages/components/components/terms-of-service/types.ts
+++ projects/js-packages/components/components/terms-of-service/types.ts
@@ -1,4 +1,25 @@
-export type TermsOfServiceProps = {
-	multipleButtons?: boolean;
-	agreeButtonLabel?: string;
+type MultipleButtonsProps = {
+	/**
+	 * Indicates whether there are multiple buttons present that would imply agreement if clicked.
+	 */
+	multipleButtons: true;
+
+	/**
+	 * The text label of the button someone would click to agree to the terms.
+	 */
+	agreeButtonLabel?: undefined;
 };
+
+type SingleButtonProps = {
+	/**
+	 * Indicates whether there are multiple buttons present that would imply agreement if clicked.
+	 */
+	multipleButtons?: false;
+
+	/**
+	 * The text label of the button someone would click to agree to the terms.
+	 */
+	agreeButtonLabel: string;
+};
+
+export type TermsOfServiceProps = MultipleButtonsProps | SingleButtonProps;
