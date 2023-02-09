0ab48d58d0a8360f799c61708cce70614157726e
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2023-02-09 15:53:58 +0100

    [not verified] Add MigrationStatus type

---
+++ projects/plugins/migration/src/js/components/migration/types.d.ts
@@ -0,0 +1,9 @@
+export type MigrationStatus = {
+	status: 'inactive' | 'backing-up' | string;
+	source_blog_id?: string;
+	target_blog_id?: string;
+	percent?: number;
+	created?: string;
+	last_modified?: string;
+	is_atomic?: boolean;
+};
