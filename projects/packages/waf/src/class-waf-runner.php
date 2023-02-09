787bed67d47dbca1e40679a345a542127292474e
Nate Weller <nate.weller@automattic.com>
2023-02-09 13:09:46 -0700

    WAF: Ensure the jetpack-waf directory exists as part of filesystem initialization (#28760)

--- projects/packages/waf/src/class-waf-runner.php
+++ projects/packages/waf/src/class-waf-runner.php
@@ -251,7 +251,7 @@ class Waf_Runner {
 	}
 
 	/**
-	 * Initializes the WP filesystem.
+	 * Initializes the WP filesystem and WAF directory structure.
 	 *
 	 * @return void
 	 * @throws \Exception If filesystem is unavailable.
@@ -264,6 +264,8 @@ class Waf_Runner {
 		if ( ! \WP_Filesystem() ) {
 			throw new \Exception( 'No filesystem available.' );
 		}
+
+		self::initialize_waf_directory();
 	}
 
 	/**
@@ -298,12 +300,12 @@ class Waf_Runner {
 	}
 
 	/**
-	 * Created the waf directory on activation.
+	 * Ensures that the waf directory is created.
 	 *
 	 * @return void
 	 * @throws \Exception In case there's a problem when creating the directory.
 	 */
-	public static function create_waf_directory() {
+	public static function initialize_waf_directory() {
 		WP_Filesystem();
 		Waf_Constants::define_waf_directory();
 
@@ -314,7 +316,7 @@ class Waf_Runner {
 
 		if ( ! $wp_filesystem->is_dir( JETPACK_WAF_DIR ) ) {
 			if ( ! $wp_filesystem->mkdir( JETPACK_WAF_DIR ) ) {
-				throw new \Exception( 'Failed creating WAF standalone bootstrap file directory: ' . JETPACK_WAF_DIR );
+				throw new \Exception( 'Failed creating WAF file directory: ' . JETPACK_WAF_DIR );
 			}
 		}
 	}
