4bd1662bdfd864465462d40cbf2965791848ad20
Brienna Ransom <cometgrrl@gmail.com>
2023-02-10 12:10:40 -0800

    adds a notification task to notify KitKat for high and blocker bugs

---
+++ projects/github-actions/repo-gardening/src/tasks/notify-kitkat/index.js
@@ -0,0 +1,94 @@
+const { getInput, setFailed } = require( '@actions/core' );
+const debug = require( '../../utils/debug' );
+const getLabels = require( '../../utils/get-labels' );
+const sendSlackMessage = require( '../../utils/send-slack-message' );
+
+/* global GitHub, WebhookPayloadIssue */
+
+/**
+ * Check for a high priority label on an issue.
+ *
+ * @param {GitHub} octokit - Initialized Octokit REST client.
+ * @param {string} owner   - Repository owner.
+ * @param {string} repo    - Repository name.
+ * @param {string} number  - Issue number.
+ * @returns {Promise<boolean>} Promise resolving to boolean.
+ */
+async function hasHighPrioLabel( octokit, owner, repo, number ) {
+	const labels = await getLabels( octokit, owner, repo, number );
+	// We're only interested in the [Prio] High label.
+	return labels.includes( '[Prio] High' );
+}
+
+/**
+ * Check for a BLOCKER priority label on an issue.
+ *
+ * @param {GitHub} octokit - Initialized Octokit REST client.
+ * @param {string} owner   - Repository owner.
+ * @param {string} repo    - Repository name.
+ * @param {string} number  - Issue number.
+ * @returns {Promise<boolean>} Promise resolving to boolean.
+ */
+async function hasBlockerPrioLabel( octokit, owner, repo, number ) {
+	const labels = await getLabels( octokit, owner, repo, number );
+	// We're only interested in the [Prio] BLOCKER label.
+	return labels.includes( '[Prio] BLOCKER' );
+}
+
+
+/**
+ * Send a Slack notification about a label to Team KitKat.
+ *
+ * @param {WebhookPayloadIssue} payload - Issue event payload.
+ * @param {GitHub}                    octokit - Initialized Octokit REST client.
+ */
+async function notifyKitKat( payload, octokit ) {
+	const { number, repository } = payload;
+	const { owner, name: repo } = repository;
+	const ownerLogin = owner.login;
+
+	const slackToken = getInput( 'slack_token' );
+	if ( ! slackToken ) {
+		setFailed( `notify-kitkat: Input slack_token is required but missing. Aborting.` );
+		return;
+	}
+
+	const channel = getInput( 'slack_kitkat_channel' );
+	if ( ! channel ) {
+		setFailed(
+			`notify-editorial: Input slack_kitkat_channel is required but missing. Aborting.`
+		);
+		return;
+	}
+
+
+	// Check for a [Prio] High label.
+	const isLabeledHighPriority = await hasHighPrioLabel( octokit, ownerLogin, repo, number );
+	if ( isLabeledHighPriority ) {
+		debug(
+			`notify-kitkat: Found a [Prio] High label on issue #${ number }. Sending in Slack message.`
+		);
+		await sendSlackMessage(
+			`New High priority bug.`,
+			channel,
+			slackToken,
+			payload
+		);
+	}
+
+	// Check for a BLOCKER priority label.
+	const isLabeledBlocker = await hasLabel( octokit, ownerLogin, repo, number );
+	if ( isLabeledBlocker ) {
+		debug(
+			`notify-editorial: Found a [Pri] BLOCKER label on issue #${ number }. Sending in Slack message.`
+		);
+		await sendSlackMessage(
+			`New Blocker bug!.`,
+			channel,
+			slackToken,
+			payload
+		);
+	}
+}
+
+module.exports = notifyKitKat;
---
+++ projects/github-actions/repo-gardening/src/tasks/notify-kitkat/readme.md
@@ -0,0 +1,14 @@
+# Notify KitKat
+
+Send a Slack notification to KitKat when there is a high or blocker priority bug, or a bug with no priority label.
+
+This task responds to the following labels:
+
+- "[Pri] BLOCKER"
+- "[Pri] High"
+
+When one of the labels above is added to an issue, a Slack message will be sent to channel of your choice.
+
+## Rationale
+
+This alert will help improve triage times which should then improve MTTR.
