64040b3da71c6f81b3cd52f08ac330179708d392
jhnstn <jhnstn@pm.me>
2023-02-14 16:50:21 -0500

    Add use media token hook

---
+++ projects/packages/videopress/src/client/block-editor/hooks/use-media-token.ts
@@ -0,0 +1,39 @@
+/**
+ * WordPress dependencies
+ */
+import { useEffect, useState } from '@wordpress/element';
+/**
+ * Internal dependencies
+ */
+import getMediaToken from '../../lib/get-media-token';
+import { MediaTokenScopeProps } from '../../lib/get-media-token/types';
+import { VideoGUID, VideoId } from '../blocks/video/types';
+
+export type UseMediaTokenArgs = {
+	id?: VideoId;
+	guid?: VideoGUID;
+	isPrivate: boolean;
+	tokenType: MediaTokenScopeProps;
+};
+/**
+ * Hook to get a media token.
+ *
+ * @param {UseMediaTokenArgs} args - Hook arguments object.
+ * @returns {string} The media token.
+ */
+const useMediaToken = ( { id, guid, isPrivate, tokenType }: UseMediaTokenArgs ): string | null => {
+	const [ token, setToken ] = useState< string | null >( null );
+
+	useEffect( () => {
+		if ( ! isPrivate ) {
+			return setToken( null );
+		}
+		getMediaToken( tokenType, { id, guid } ).then( tokenData => {
+			setToken( tokenData?.token );
+		} );
+	}, [ isPrivate ] );
+
+	return token;
+};
+
+export default useMediaToken;
