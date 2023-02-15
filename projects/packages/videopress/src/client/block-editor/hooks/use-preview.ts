eca45becd242c216f2ab167aa7c4f0eff251857f
jhnstn <jhnstn@pm.me>
2023-02-15 15:02:19 -0500

    [not verified] Add usePreview hook

---
+++ projects/packages/videopress/src/client/block-editor/hooks/use-preview.ts
@@ -0,0 +1,32 @@
+/*
+ * WordPress dependencies
+ */
+import { store as coreStore } from '@wordpress/core-data';
+import { useSelect } from '@wordpress/data';
+/*
+ * Internal dependencies
+ */
+import { VideoPreview } from '../blocks/video/types';
+
+export type UsePreviewResult = {
+	preview: VideoPreview;
+	isRequestingEmbedPreview: boolean;
+};
+
+const defaultPreview: VideoPreview = { html: null, scripts: [], width: null, height: null };
+
+export const usePreview = ( videoPressUrl?: string ): UsePreviewResult => {
+	return useSelect(
+		select => {
+			if ( ! videoPressUrl ) {
+				return { preview: defaultPreview, isRequestingEmbedPreview: false };
+			}
+			return {
+				preview: select( coreStore ).getEmbedPreview( videoPressUrl ) || defaultPreview,
+				isRequestingEmbedPreview:
+					select( coreStore ).isRequestingEmbedPreview( videoPressUrl ) || false,
+			};
+		},
+		[ videoPressUrl ]
+	);
+};
