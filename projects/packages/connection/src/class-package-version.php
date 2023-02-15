4ea3a751123cb6998c892e0f925f0020e42defe3
Sergey Mitroshin <sergeymitr@gmail.com>
2023-02-15 12:57:39 -0500

    Connection: prevent potential version compatibility issues with the Licensing package (#28809)
    PR #28509 introduced a change that might cause compatibility issues if the site runs the latest Connection package and an outdated Licensing package.
    
    This PR introduces additional check to make sure the method is there before running it.

--- projects/packages/connection/src/class-package-version.php
+++ projects/packages/connection/src/class-package-version.php
@@ -12,7 +12,7 @@ namespace Automattic\Jetpack\Connection;
  */
 class Package_Version {
 
-	const PACKAGE_VERSION = '1.50.1';
+	const PACKAGE_VERSION = '1.50.2-alpha';
 
 	const PACKAGE_SLUG = 'connection';
 
