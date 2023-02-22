6a2d7f941428c72046032be339fda3dd5477fc69
Kuba Birecki <kuba.birecki@crystalplanet.io>
2023-02-22 23:30:17 +0100

    Update versions

--- projects/packages/forms/src/class-jetpack-forms.php
+++ projects/packages/forms/src/class-jetpack-forms.php
@@ -14,7 +14,7 @@ use Automattic\Jetpack\Forms\Dashboard\Dashboard;
  */
 class Jetpack_Forms {
 
-	const PACKAGE_VERSION = '0.5.1';
+	const PACKAGE_VERSION = '0.5.2-alpha';
 
 	/**
 	 * Load the contact form module.
