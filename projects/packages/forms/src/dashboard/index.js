9e4aded146bcc0a7d60a238fc9c597602eb66b74
Christian Gastrell <cgastrell@gmail.com>
2023-02-23 19:31:50 -0300

    [not verified] add state and provider wrapping

--- projects/packages/forms/src/dashboard/index.js
+++ projects/packages/forms/src/dashboard/index.js
@@ -1,8 +1,15 @@
 import { render } from '@wordpress/element';
+import { Provider } from 'react-redux';
 import Inbox from './inbox';
+import store from './state';
 
 window.addEventListener( 'load', () => {
 	const container = document.getElementById( 'jp-forms-dashboard' );
 
-	render( <Inbox />, container );
+	render(
+		<Provider store={ store }>
+			<Inbox />
+		</Provider>,
+		container
+	);
 } );
