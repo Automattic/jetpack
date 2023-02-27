006c89a39cc1fa37aa0ea74ea8d5b6cb8f71d6d8
Brandon Kraft <public@brandonkraft.com>
2023-02-27 10:14:17 -0600

    rector.php: make 5.6 compat for the linter. Doesn't matter either way functionally

--- rector.php
+++ rector.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 
-return static function ( RectorConfig $rectorConfig ): void {
+return static function ( RectorConfig $rectorConfig ) {
 	$rectorConfig->paths(
 		array(
 			__DIR__ . '/projects',
