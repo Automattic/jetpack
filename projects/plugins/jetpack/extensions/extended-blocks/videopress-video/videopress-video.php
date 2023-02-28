0e7668b4137db899781d29034fa540986dce0bce
Damián Suárez <rdsuarez@gmail.com>
2023-02-28 07:56:14 -0300

    switch code blocks

--- projects/plugins/jetpack/extensions/extended-blocks/videopress-video/videopress-video.php
+++ projects/plugins/jetpack/extensions/extended-blocks/videopress-video/videopress-video.php
@@ -9,14 +9,6 @@ namespace Automattic\Jetpack\Extensions\VideoPress_Video;
 
 use Automattic\Jetpack\VideoPress\Initializer as VideoPress_Pkg_Initializer;
 
-// Set the videopress/video block availability, depending on the site plan.
-add_action(
-	'jetpack_register_gutenberg_extensions',
-	function () {
-		\Jetpack_Gutenberg::set_availability_for_plan( 'videopress/video' );
-	}
-);
-
 // Register the videopress/video block.
 add_action(
 	'init',
@@ -30,5 +22,13 @@ add_action(
 		) {
 			VideoPress_Pkg_Initializer::register_videopress_video_block();
 		}
+	}
+);
+
+// Set the videopress/video block availability, depending on the site plan.
+add_action(
+	'jetpack_register_gutenberg_extensions',
+	function () {
+		\Jetpack_Gutenberg::set_availability_for_plan( 'videopress/video' );
 	}
 );
