db37a6f5ad58e846406cffbc3c93e080967ec0ff
Damián Suárez <rdsuarez@gmail.com>
2023-02-28 09:45:11 -0300

    do not call get_availability() method

--- projects/plugins/jetpack/extensions/extended-blocks/videopress-video/videopress-video.php
+++ projects/plugins/jetpack/extensions/extended-blocks/videopress-video/videopress-video.php
@@ -6,3 +6,30 @@
  **/
 
 namespace Automattic\Jetpack\Extensions\VideoPress_Video;
+
+use Automattic\Jetpack\VideoPress\Initializer as VideoPress_Pkg_Initializer;
+
+// Register the videopress/video block.
+add_action(
+	'init',
+	function () {
+		// $availability                  = \Jetpack_Gutenberg::get_availability();
+		// $is_videopress_video_available = isset( $availability['videopress/video']['available'] ) && $availability['videopress/video']['available'] === true;
+		$is_videopress_video_available = true;
+
+		if (
+			$is_videopress_video_available &&
+			method_exists( 'Automattic\Jetpack\VideoPress\Initializer', 'register_videopress_video_block' )
+		) {
+			VideoPress_Pkg_Initializer::register_videopress_video_block();
+		}
+	}
+);
+
+// Set the videopress/video block availability, depending on the site plan.
+add_action(
+	'jetpack_register_gutenberg_extensions',
+	function () {
+		\Jetpack_Gutenberg::set_availability_for_plan( 'videopress/video' );
+	}
+);
