fb0bf02df97d2341334a76289c387d55e3e0ae01
Omar Alshaker <omar@omaralshaker.com>
2023-02-28 16:24:06 +0100

    Remove ads conditions

--- projects/plugins/jetpack/extensions/blocks/cookie-consent/cookie-consent.php
+++ projects/plugins/jetpack/extensions/blocks/cookie-consent/cookie-consent.php
@@ -17,23 +17,7 @@ const BLOCK_NAME   = 'jetpack/' . FEATURE_NAME;
 const COOKIE_NAME  = 'eucookielaw';
 
 /**
- * If the site has ads, it will have its own cookie widget. This function checks if the site has ads.
- *
- * @since $$next-version$$
- *
- * @return bool
- */
-function should_use_ads_cookie_widget_instead() {
-	return ! ( ( defined( 'NOADVERTS' )
-	|| defined( 'NOADSUPGRADE' )
-	|| 1 == get_option( 'permanent_noadverts' ) // phpcs:ignore Universal.Operators.StrictComparisons.LooseEqual,WordPress.PHP.StrictComparisons.LooseComparison -- This can be stored as string as well.
-	|| ( function_exists( 'has_blog_sticker' ) && has_blog_sticker( 'wordads' ) ) ) ); // Enable widget for WordAds users (regardless of plan).
-}
-
-/**
  * Should the block be registered?
- * We do not want to register the block when the site has ads,
- * or when we're not on the frontend.
  * In wp-admin, we only want to show the block in the site editor.
  *
  * @since $$next-version$$
@@ -41,21 +25,16 @@ function should_use_ads_cookie_widget_instead() {
  * @return bool
  */
 function should_register_block() {
-	return true;
 	global $pagenow;
 
-	if ( should_use_ads_cookie_widget_instead() ) {
-		return false;
+	// Always register the widget if we're on the front end
+	if ( ! is_admin() ) {
+		return true;
 	}
 
-	if (
-		is_admin()
-		&& $pagenow !== 'site-editor.php'
-	) {
-		return false;
+	if ( is_admin() && $pagenow === 'site-editor.php' ) {
+		return true;
 	}
-
-	return true;
 }
 
 /**
