85a3c57d4bb224d47cc304425662d577fd2634fc
Omar Alshaker <omar@omaralshaker.com>
2023-02-28 15:45:18 +0100

    Add Icon

--- projects/plugins/jetpack/extensions/blocks/cookie-consent/icon.js
+++ projects/plugins/jetpack/extensions/blocks/cookie-consent/icon.js
@@ -4,8 +4,14 @@
 import { SVG, Path } from '@wordpress/components';
 
 export default (
-	/* @TODO Add the icon. You can use one of these https://material.io/tools/icons/?style=outline */
-	<SVG height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-		<Path d="M9 15h2V9H9v6zm1-10c-.5 0-1 .5-1 1s.5 1 1 1 1-.5 1-1-.5-1-1-1zm0-4c-5 0-9 4-9 9s4 9 9 9 9-4 9-9-4-9-9-9zm0 16c-3.9 0-7-3.1-7-7s3.1-7 7-7 7 3.1 7 7-3.1 7-7 7z" />
+	<SVG height="24" viewBox="0 0 25 25" width="24" xmlns="http://www.w3.org/2000/svg">
+		<Path
+			stroke="currentColor"
+			fill="white"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth="2"
+			d="M4.419 12c0-6.4 5.358-8 8.037-8h1.005v4h3.014v3h4.019v1c0 6.4-5.024 8-8.038 8-3.014 0-8.037-1.6-8.037-8zm5.023-4h.001m-2.01 4h0m5.023 1h.001m-2.01 3h0m6.028-1h0"
+		/>
 	</SVG>
 );
