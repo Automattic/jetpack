24415087991d394fc403a87113d847431ea3e665
Grant Kinney <creativecoder@users.noreply.github.com>
2023-03-02 11:03:51 -0600

    Writing prompts: adds basic UI to blogging prompts block (#29189)

--- projects/plugins/jetpack/extensions/blocks/blogging-prompt/attributes.js
+++ projects/plugins/jetpack/extensions/blocks/blogging-prompt/attributes.js
@@ -1,3 +1,30 @@
 export default {
-	// @TODO - Add block attributes here
+	gravatars: {
+		type: 'array',
+		source: 'query',
+		selector: '.jetpack-blogging-prompts__answers-gravatar',
+		query: {
+			url: {
+				type: 'string',
+				source: 'attribute',
+				attribute: 'src',
+			},
+		},
+	},
+	prompt: {
+		type: 'text',
+		source: 'html',
+		selector: '.jetpack-blogging-prompts__prompt',
+	},
+	promptId: {
+		type: 'number',
+	},
+	showResponses: {
+		type: 'boolean',
+		default: true,
+	},
+	showLabel: {
+		type: 'boolean',
+		default: true,
+	},
 };
