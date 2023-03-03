7240b2d96d666721dfb2b7e5a08b4e6c2e6bfa2f
Dave Martin <davemart-in@users.noreply.github.com>
2023-03-03 08:25:33 -0500

    Fix JS Error and adjust marginRight of Pinterest official button instead of width

--- projects/plugins/jetpack/modules/sharedaddy/sharing-sources.php
+++ projects/plugins/jetpack/modules/sharedaddy/sharing-sources.php
@@ -2572,8 +2572,10 @@ class Share_Pinterest extends Sharing_Source {
 						var shares = document.querySelectorAll( 'li.share-pinterest' );
 						for ( var i = 0; i < shares.length; i++ ) {
 							var share = shares[ i ];
-							if ( share.querySelector( 'a span:visible' ) ) {
-								share.style.width = '80px';
+							var countComputedStyle = window.getComputedStyle(share.querySelector( 'a span' ));
+							if ( countComputedStyle.display === 'block' ) {
+								var countWidth = parseInt( countComputedStyle.width, 10 );
+								share.style.marginRight = countWidth + 11 + 'px';
 							}
 						}
 					}
