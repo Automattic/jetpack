e73d812933456734fd026c8c09ffc1755a70ce9a
Antony Agrios <antonyagrios@gmail.com>
2023-03-03 09:00:10 +0200

    [not verified] Update: Use loading placeholder instead of local component

--- projects/packages/videopress/src/client/admin/components/video-details/index.tsx
+++ projects/packages/videopress/src/client/admin/components/video-details/index.tsx
@@ -1,14 +1,13 @@
 /**
  * External dependencies
  */
-import { Text } from '@automattic/jetpack-components';
+import { Text, LoadingPlaceholder } from '@automattic/jetpack-components';
 import { gmdateI18n } from '@wordpress/date';
 import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
 import ClipboardButtonInput from '../clipboard-button-input';
-import Placeholder from '../placeholder';
 import styles from './style.module.scss';
 import { VideoDetailsProps } from './types';
 
@@ -19,22 +18,26 @@ const VideoDetails = ( { filename, src, uploadDate, loading, shortcode }: VideoD
 		<div className={ styles.details }>
 			<div>
 				<Text variant="body-small">{ __( 'Link to video', 'jetpack-videopress-pkg' ) }</Text>
-				{ loading ? <Placeholder height={ 36 } /> : <ClipboardButtonInput value={ src } /> }
+				{ loading ? <LoadingPlaceholder height={ 36 } /> : <ClipboardButtonInput value={ src } /> }
 			</div>
 
 			<div>
 				<Text variant="body-small">{ __( 'WordPress shortcode', 'jetpack-videopress-pkg' ) }</Text>
-				{ loading ? <Placeholder height={ 36 } /> : <ClipboardButtonInput value={ shortcode } /> }
+				{ loading ? (
+					<LoadingPlaceholder height={ 36 } />
+				) : (
+					<ClipboardButtonInput value={ shortcode } />
+				) }
 			</div>
 
 			<div>
 				<Text variant="body-small">{ __( 'File name', 'jetpack-videopress-pkg' ) }</Text>
-				{ loading ? <Placeholder height={ 24 } /> : <Text>{ filename }</Text> }
+				{ loading ? <LoadingPlaceholder height={ 24 } /> : <Text>{ filename }</Text> }
 			</div>
 
 			<div>
 				<Text variant="body-small">{ __( 'Upload date', 'jetpack-videopress-pkg' ) }</Text>
-				{ loading ? <Placeholder height={ 24 } /> : <Text>{ formattedDate }</Text> }
+				{ loading ? <LoadingPlaceholder height={ 24 } /> : <Text>{ formattedDate }</Text> }
 			</div>
 		</div>
 	);
