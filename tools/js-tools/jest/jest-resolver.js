13d080802be8655df67aeee2409feb2fdea2e980
Brad Jorsch <brad.jorsch@automattic.com>
2023-03-03 12:08:54 -0500

    `react-colorful` has been fixed. `uuid` too, but our deps haven't picked up the new version of that yet.

--- tools/js-tools/jest/jest-resolver.js
+++ tools/js-tools/jest/jest-resolver.js
@@ -1,7 +1,10 @@
 // Some packages assume that a "browser" environment is esm or otherwise break in node.
 // List them here and the resolver will adjust the conditions to resolve them as "node" instead.
 // cf. https://github.com/microsoft/accessibility-insights-web/pull/5421#issuecomment-1109168149
-const badBrowserPackages = new Set( [ 'uuid', 'react-colorful' ] );
+const badBrowserPackages = new Set( [
+	// Supposedly fixed in v9: https://github.com/uuidjs/uuid/pull/616#issuecomment-1237428554
+	'uuid',
+] );
 
 module.exports = ( path, options ) => {
 	const basedir = options.basedir;
