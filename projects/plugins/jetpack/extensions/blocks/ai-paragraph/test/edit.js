817fd9309d433cd8d954d9e931baf9404e0c8e66
Fabien MILLERAND <fab@millerf.com>
2023-03-07 23:56:01 +0700

    Update edit.js

--- projects/plugins/jetpack/extensions/blocks/ai-paragraph/test/edit.js
+++ projects/plugins/jetpack/extensions/blocks/ai-paragraph/test/edit.js
@@ -17,38 +17,42 @@ describe( 'AIParagraphEdit', () => {
 		expect( createPrompt( '', [], '', '' ) ).toBeFalsy();
 
 		// Test contents
-		expect( createPrompt( 'title', [], '', '' ) ).toBe( "This is a post titled 'title'" );
+		expect( createPrompt( 'title', [], '', '' ) ).toBe(
+			"Give me content for a blog post titled 'title' . Do not not include any smalltalk, just the content of a post."
+		);
 		expect(
 			createPrompt( 'title', [ fakeBlock, { attributes: { whatever: 'content' } } ], '', '' )
-		).toBe( "Give me content for a blog post titled 'title' :\n\n … content" );
+		).toBe(
+			"Give me content for a blog post titled 'title' :\n\n … content. Do not not include any smalltalk, just the content of a post."
+		);
 
 		// Test that <BR/> are being translated. And content trimmed
 		expect( createPrompt( 'title', [ fakeBlockWithBr ], '', '' ) ).toBe(
-			"Give me content for a blog post titled 'title' :\n\n … content\ncontent2"
+			"Give me content for a blog post titled 'title' :\n\n … content\ncontent2. Do not not include any smalltalk, just the content of a post."
 		);
 
 		expect( createPrompt( 'title', [ fakeBlock, fakeBlock ], 'cat 1', '' ) ).toBe(
-			"Give me content for a blog post titled 'title' , published in categories 'cat 1':\n\n … content\ncontent"
+			"Give me content for a blog post titled 'title' , published in categories 'cat 1':\n\n … content\ncontent. Do not not include any smalltalk, just the content of a post."
 		);
 
 		// Test MAX content length
 		expect( createPrompt( 'title', [ fakeBlockWithVeryLongContent ] ) ).toBe(
-			"Give me content for a blog post titled 'title' :\n\n … " +
+			"Give me content for a blog post titled 'title' :\n\n … . Do not not include any smalltalk, just the content of a post." +
 				longContent.slice( -MAXIMUM_NUMBER_OF_CHARACTERS_SENT_FROM_CONTENT )
 		);
 
 		// Test only cats
 		expect( createPrompt( '', [ fakeBlock ], 'cat1', 'tag1' ) ).toBe(
-			"Give me content for a blog post, published in categories 'cat1' and tagged 'tag1':\n\n … content"
+			"Give me content for a blog post, published in categories 'cat1' and tagged 'tag1':\n\n … content. Do not not include any smalltalk, just the content of a post."
 		);
 		expect( createPrompt( '', [ fakeBlock ], 'cat1, cat2', 'tag1' ) ).toBe(
-			"Give me content for a blog post, published in categories 'cat1, cat2' and tagged 'tag1':\n\n … content"
+			"Give me content for a blog post, published in categories 'cat1, cat2' and tagged 'tag1':\n\n … content. Do not not include any smalltalk, just the content of a post."
 		);
 		expect( createPrompt( '', [], 'cat1, cat2', 'tag1' ) ).toBe(
-			"Give me content for a blog post, published in categories 'cat1, cat2' and tagged 'tag1'"
+			"Give me content for a blog post, published in categories 'cat1, cat2' and tagged 'tag1'. Do not not include any smalltalk, just the content of a post."
 		);
 		expect( createPrompt( 'title', [], 'cat1, cat2', 'tag1' ) ).toBe(
-			"Give me content for a blog post titled 'title' , published in categories 'cat1, cat2' and tagged 'tag1'"
+			"Give me content for a blog post titled 'title' , published in categories 'cat1, cat2' and tagged 'tag1'. Do not not include any smalltalk, just the content of a post."
 		);
 	} );
 } );
