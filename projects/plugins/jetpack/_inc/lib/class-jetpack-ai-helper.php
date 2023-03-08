2d4f894085f74f192846ef306cb53fd27e967dc9
Fabien MILLERAND <fab@millerf.com>
2023-03-08 20:07:45 +0700

    Force max tokens to 140

--- projects/plugins/jetpack/_inc/lib/class-jetpack-ai-helper.php
+++ projects/plugins/jetpack/_inc/lib/class-jetpack-ai-helper.php
@@ -162,8 +162,9 @@ class Jetpack_AI_Helper {
 				),
 			);
 
-			$result = ( new OpenAI( 'openai', array( 'post_id' => $post_id ) ) )->request_chat_completion(
-				$data
+			$result         = ( new OpenAI( 'openai', array( 'post_id' => $post_id ) ) )->request_chat_completion(
+				$data,
+				$max_tokens = 140
 			);
 
 			if ( is_wp_error( $result ) ) {
