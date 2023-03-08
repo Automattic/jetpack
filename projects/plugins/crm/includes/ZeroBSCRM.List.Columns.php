2eb9802eb65d60315a7332fcbd5a3048e4e2fd64
Karen Attfield <karenlattfield@gmail.com>
2023-03-08 08:16:50 +0000

    Update projects/plugins/crm/includes/ZeroBSCRM.List.Columns.php
    Co-authored-by: gogdzl <37049295+gogdzl@users.noreply.github.com>

--- projects/plugins/crm/includes/ZeroBSCRM.List.Columns.php
+++ projects/plugins/crm/includes/ZeroBSCRM.List.Columns.php
@@ -471,7 +471,7 @@
       $zeroBSCRM_filterbuttons_company['default'] = array(
 
                                   'lead' => array(__('Lead',"zero-bs-crm")),
-									'customer' => array( __( 'Contact', 'zero-bs-crm' ) ),  // phpcs:ignore WordPress.Arrays.ArrayIndentation.ItemNotAligned -- impossible to fix without fixing everything else.
+									'customer' => array( __( 'Customer', 'zero-bs-crm' ) ),  // phpcs:ignore WordPress.Arrays.ArrayIndentation.ItemNotAligned -- impossible to fix without fixing everything else.
                             );
       $zeroBSCRM_filterbuttons_company['all'] = array(
 
