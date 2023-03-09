eb003d2a6b2545c511ca367a037939bd31fd3025
Steve D <33553323+sdixon194@users.noreply.github.com>
2023-03-09 15:19:03 -0500

    Jetpack-mu-wpcom: Release 1.0.8 (#29405)

--- projects/plugins/mu-wpcom-plugin/mu-wpcom-plugin.php
+++ projects/plugins/mu-wpcom-plugin/mu-wpcom-plugin.php
@@ -3,7 +3,7 @@
  *
  * Plugin Name: WordPress.com Features
  * Description: Test plugin for the jetpack-mu-wpcom package
- * Version: 1.0.8-alpha
+ * Version: 1.0.8
  * Author: Automattic
  * License: GPLv2 or later
  * Text Domain: jetpack-mu-wpcom-plugin
