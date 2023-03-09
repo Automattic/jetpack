5641d8786e4ecf6cfbcbc75b1b3dc48bfab9c97d
jcheringer <jcheringer@users.noreply.github.com>
2023-03-09 14:27:53 -0300

    Forms: Multiple Choice and Single Choice fields redesign (#29290)
    * Multiple Choice and Single Choice fields redesign
    
    * changelog
    
    * MC/SC fields - editor adjustments
    
    * Fields style adjustments
    
    * Adjust MC/SC fields to handle ids properly
    
    * Code clean up
    
    * Add safety checks
    
    * Update files on Jetpack plugin
    
    * Fix Forms submission
    
    * Update Forms package
    
    * Increase Field Option line-height
    
    ---------
    
    Co-authored-by: Brandon Kraft <public@brandonkraft.com>

--- projects/packages/forms/src/contact-form/class-contact-form-plugin.php
+++ projects/packages/forms/src/contact-form/class-contact-form-plugin.php
@@ -368,9 +368,21 @@ class Contact_Form_Plugin {
 			)
 		);
 		Blocks::jetpack_register_block(
+			'jetpack/field-option-checkbox',
+			array(
+				'render_callback' => array( __CLASS__, 'gutenblock_render_field_option' ),
+			)
+		);
+		Blocks::jetpack_register_block(
 			'jetpack/field-radio',
 			array(
 				'render_callback' => array( __CLASS__, 'gutenblock_render_field_radio' ),
+			)
+		);
+		Blocks::jetpack_register_block(
+			'jetpack/field-option-radio',
+			array(
+				'render_callback' => array( __CLASS__, 'gutenblock_render_field_option' ),
 			)
 		);
 		Blocks::jetpack_register_block(
@@ -547,6 +559,19 @@ class Contact_Form_Plugin {
 	 */
 	public static function gutenblock_render_field_checkbox_multiple( $atts, $content ) {
 		$atts = self::block_attributes_to_shortcode_attributes( $atts, 'checkbox-multiple' );
+		return Contact_Form::parse_contact_field( $atts, $content );
+	}
+
+	/**
+	 * Render the multiple choice field option.
+	 *
+	 * @param array  $atts - the block attributes.
+	 * @param string $content - html content.
+	 *
+	 * @return string HTML for the contact form field.
+	 */
+	public static function gutenblock_render_field_option( $atts, $content ) {
+		$atts = self::block_attributes_to_shortcode_attributes( $atts, 'field-option' );
 		return Contact_Form::parse_contact_field( $atts, $content );
 	}
 
@@ -891,6 +916,10 @@ class Contact_Form_Plugin {
 	public function add_shortcode() {
 		add_shortcode( 'contact-form', array( '\Automattic\Jetpack\Forms\ContactForm\Contact_Form', 'parse' ) );
 		add_shortcode( 'contact-field', array( '\Automattic\Jetpack\Forms\ContactForm\Contact_Form', 'parse_contact_field' ) );
+
+		// We need 'contact-field-option' to be registered, so it's included to the get_shortcode_regex() method
+		// But we don't need a callback because we're handling contact-field-option manually
+		add_shortcode( 'contact-field-option', '__return_null' );
 	}
 
 	/**
