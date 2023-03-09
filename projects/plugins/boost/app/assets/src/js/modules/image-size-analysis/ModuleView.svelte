75b58896df70f389a9316a634648bb1d01390b90
Nauris Pūķis <hi@pyronaur.com>
2023-03-09 16:28:35 +0200

    Add a button with a spacer

--- projects/plugins/boost/app/assets/src/js/modules/image-size-analysis/ModuleView.svelte
+++ projects/plugins/boost/app/assets/src/js/modules/image-size-analysis/ModuleView.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
 	import { __ } from '@wordpress/i18n';
+	import Button from '../../elements/Button.svelte';
 	import ProgressBar from '../../elements/ProgressBar.svelte';
 </script>
 
 <ProgressBar progress={50}>
 	{__( 'Give us a few minutes while we go through all your content.', 'jetpack-boost' )}
 </ProgressBar>
+<div class="jb-spacer" />
+<Button>See report in progress</Button>
+
+<style lang="scss">
+	.jb-spacer {
+		height: 3.5rem;
+	}
+</style>
