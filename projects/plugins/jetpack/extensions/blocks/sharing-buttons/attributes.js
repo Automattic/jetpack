afd252e6f511ee51348d8c945ba57e3f586f4808
Heyde Moura <moura.heyde@gmail.com>
2023-03-10 14:53:03 -0300

    [not verified] Add available services module

--- projects/plugins/jetpack/extensions/blocks/sharing-buttons/attributes.js
+++ projects/plugins/jetpack/extensions/blocks/sharing-buttons/attributes.js
@@ -1,3 +1,3 @@
 export default {
-	// @TODO - Add block attributes here
+	services: [],
 };
---
+++ projects/plugins/jetpack/extensions/blocks/sharing-buttons/available-services.js
@@ -0,0 +1,14 @@
+export default {
+	print: 'Print',
+	email: 'Email',
+	facebook: 'Facebook',
+	linkedin: 'LinkedIn',
+	reddit: 'Reddit',
+	twitter: 'Twitter',
+	tumblr: 'Tumblr',
+	pinterest: 'Pinterest',
+	pocket: 'Pocket',
+	telegram: 'Telegram',
+	'jetpack-whatsapp': 'Jetpack_WhatsApp',
+	skype: 'Skype',
+};
