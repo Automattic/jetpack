452f234c08002c5b26acf0fbde3edfb658264ec0
Heyde Moura <moura.heyde@gmail.com>
2023-03-10 11:19:38 -0300

    [not verified] Fix import paths

--- projects/plugins/jetpack/extensions/blocks/sharing-buttons/components/SocialButton.js
+++ projects/plugins/jetpack/extensions/blocks/sharing-buttons/components/SocialButton.js
@@ -1,22 +1,22 @@
 import { __ } from '@wordpress/i18n';
-import SharingButtonContext from './context';
+import SharingButtonContext from '../context';
 
 const services = {
-	twitter: __( 'Twitter', 'jetpack' ),
-	facebook: __( 'Facebook', 'jetpack' ),
+	twitter: __('Twitter', 'jetpack'),
+	facebook: __('Facebook', 'jetpack'),
 };
 
-const SocialButton = ( { service } ) => {
+const SocialButton = ({ service }) => {
 	return (
 		<a
 			rel="nofollow noopener noreferrer"
-			shared={ `sharing-${ service }-1` }
-			href={ `https://www.${ service }.com` }
-			className={ `share-${ service } sd-button share-icon` }
+			shared={`sharing-${service}-1`}
+			href={`https://www.${service}.com`}
+			className={`share-${service} sd-button share-icon`}
 			target="_blank"
-			title={ `Click to share on ${ services[ service ] }` }
+			title={`Click to share on ${services[service]}`}
 		>
-			<span>{ services[ service ] }</span>
+			<span>{services[service]}</span>
 		</a>
 	);
 };
