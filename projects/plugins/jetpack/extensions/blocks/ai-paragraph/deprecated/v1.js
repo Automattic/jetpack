a00c99f5452bcc646177b6e954e4399e5bbb67c3
neffff <nef@a8c.com>
2023-03-09 18:05:40 -0800

    [not verified] Rebase & review fixes.

--- projects/plugins/jetpack/extensions/blocks/ai-paragraph/deprecated/v1.js
+++ projects/plugins/jetpack/extensions/blocks/ai-paragraph/deprecated/v1.js
@@ -1,18 +1,6 @@
 import { useBlockProps } from '@wordpress/block-editor';
 import { pasteHandler } from '@wordpress/blocks';
 
-/**
- * Example data to migrate:
- * ```
- * <!-- wp:paragraph -->
- * <p>GPT generates a block for testing.  This block should be an interesting but somewhat obscure fact regarding pirates and their constant battles with ninjas.</p>
- * <!-- /wp:paragraph -->
- *
- * <!-- wp:jetpack/ai-paragraph {"animationDone":true} -->
- * <div class="wp-block-jetpack-ai-paragraph">Pirates and ninjas shared a common enemy during the Age of Exploration: samurai. Both pirates and ninjas were constantly engaged in a battle of wits to outsmart the samurai and escape their grip. The pirates and ninjas often ended up teaming up in order to evade the samurai, swapping knowledge and resources and even joining forces to achieve greater strength. This was a highly effective strategy, as the samurai were no match for the combined strength of the two groups.</div>
- * <!-- /wp:jetpack/ai-paragraph -->
- * ```
- */
 export default {
 	attributes: {
 		content: {
