fc430ab0a66d26496dec919ac8c0c5dce2f1c5fe
Heyde Moura <moura.heyde@gmail.com>
2023-03-10 21:23:19 -0300

    [not verified] Add specific rendering for email sharing button

--- projects/plugins/jetpack/extensions/blocks/sharing-buttons/components/SocialButton.js
+++ projects/plugins/jetpack/extensions/blocks/sharing-buttons/components/SocialButton.js
@@ -1,12 +1,33 @@
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 import availableServices from '../available-services';
+import { addQueryArgs } from '@wordpress/url';
 
-const SocialButton = ({ service, link }) => {
+const SocialButton = ({ service, post }) => {
+	if ('email' === service) {
+		const href = addQueryArgs('mailto:', {
+			subject: __(sprintf('Shared post: %s', post.title), 'my-plugin'),
+			body: post.link,
+		});
+
+		return (
+			<a
+				rel="nofollow noopener noreferrer"
+				shared={`sharing-${service}-1`}
+				href={href}
+				className={`share-${service} sd-button share-icon`}
+				target="_blank"
+				title={`Click to share on ${availableServices[service]}`}
+			>
+				<span>{availableServices[service]}</span>
+			</a>
+		);
+	}
+
 	return (
 		<a
 			rel="nofollow noopener noreferrer"
 			shared={`sharing-${service}-1`}
-			href={`${link}?share=${service}&nb=1`}
+			href={`${post.link}?share=${service}&nb=1`}
 			className={`share-${service} sd-button share-icon`}
 			target="_blank"
 			title={`Click to share on ${availableServices[service]}`}
@@ -14,6 +35,11 @@ const SocialButton = ({ service, link }) => {
 			<span>{availableServices[service]}</span>
 		</a>
 	);
+};
+
+SocialButton.defaultProps = {
+	service: '',
+	post: {},
 };
 
 export default SocialButton;
