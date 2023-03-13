7a6ceb248d0e148e9b04ae8693a11ab5a436fe1a
Matt Gawarecki <matt.gawarecki@automattic.com>
2023-03-13 15:00:43 -0500

    [not verified] Boost: Remove terms-of-service text to use new TermsOfService component

--- projects/plugins/boost/app/assets/src/js/pages/benefits/BenefitsInterstitial.svelte
+++ projects/plugins/boost/app/assets/src/js/pages/benefits/BenefitsInterstitial.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
 	import { PricingCard } from '@automattic/jetpack-components';
-	import React from 'react';
 	import { derived } from 'svelte/store';
-	import { createInterpolateElement } from '@wordpress/element';
-	import { __, sprintf } from '@wordpress/i18n';
+	import { __ } from '@wordpress/i18n';
 	import BackButton from '../../elements/BackButton.svelte';
 	import ReactComponent from '../../elements/ReactComponent.svelte';
 	import config from '../../stores/config';
 	import Logo from '../../svg/jetpack-green.svg';
 	import { recordBoostEvent } from '../../utils/analytics';
-	import { jetpackURL } from '../../utils/jetpack-url';
 	import { getUpgradeURL } from '../../utils/upgrade';
 
 	async function goToCheckout() {
@@ -23,31 +20,6 @@
 
 	const ctaText = __( 'Upgrade Jetpack Boost', 'jetpack-boost' );
 
-	const tosText = createInterpolateElement(
-		sprintf(
-			/* translators: placeholder is a button label */
-			__(
-				'By clicking the <strong>%s</strong> button, you agree to our <tosLink>Terms of Service</tosLink> and to <shareDetailsLink>share details</shareDetailsLink> with WordPress.com.',
-				'jetpack-boost'
-			),
-			ctaText
-		),
-
-		{
-			strong: React.createElement( 'strong' ),
-			tosLink: React.createElement( 'a', {
-				href: jetpackURL( 'https://jetpack.com/redirect/?source=wpcom-tos' ),
-				target: '_blank',
-			} ),
-			shareDetailsLink: React.createElement( 'a', {
-				href: jetpackURL(
-					'https://jetpack.com/redirect/?source=jetpack-support-what-data-does-jetpack-sync'
-				),
-				target: '_blank',
-			} ),
-		}
-	);
-
 	const pricing = derived( config, $config => $config.pricing );
 
 	if ( ! ( 'yearly' in $pricing ) ) {
@@ -56,48 +28,47 @@
 </script>
 
 <div id="jb-settings" class="jb-settings">
-	<div class="jb-container jb-container--fixed mt-2">
-		<BackButton />
-		<div class="jb-card">
-			<div class="jb-card__content">
-				<Logo class="my-2" />
-				<h1 class="my-2">{__( "Optimize your website's performance", 'jetpack-boost' )}</h1>
-				<p class="jb-card__summary my-2">
-					{__(
-						'Remove the need to manually regenerate critical CSS after site changes with automated critical CSS.',
-						'jetpack-boost'
-					)}
-				</p>
-				<ul class="jb-checklist my-2">
-					<li>{__( 'Automatic critical CSS regeneration', 'jetpack-boost' )}</li>
-					<li>{__( 'Performance scores are recalculated after each change', 'jetpack-boost' )}</li>
-					<li>{__( 'Dedicated email support', 'jetpack-boost' )}</li>
-				</ul>
-			</div>
+		<div class="jb-container jb-container--fixed mt-2">
+			<BackButton />
+			<div class="jb-card">
+				<div class="jb-card__content">
+					<Logo class="my-2" />
+					<h1 class="my-2">{__( "Optimize your website's performance", 'jetpack-boost' )}</h1>
+					<p class="jb-card__summary my-2">
+						{__(
+							'Remove the need to manually regenerate critical CSS after site changes with automated critical CSS.',
+							'jetpack-boost'
+						)}
+					</p>
+					<ul class="jb-checklist my-2">
+						<li>{__( 'Automatic critical CSS regeneration', 'jetpack-boost' )}</li>
+						<li>{__( 'Performance scores are recalculated after each change', 'jetpack-boost' )}</li>
+						<li>{__( 'Dedicated email support', 'jetpack-boost' )}</li>
+					</ul>
+				</div>
 
-			<div class="jb-card__cta px-2 my-4">
-				{#if 'yearly' in $pricing}
-					<!-- svelte-ignore missing-declaration Jetpack_Boost -->
-					<ReactComponent
-						this={PricingCard}
-						title={__( 'Jetpack Boost', 'jetpack-boost' )}
-						icon={`${ Jetpack_Boost.site.assetPath }../static/images/forward.svg`}
-						priceBefore={$pricing.yearly.priceBefore / 12}
-						priceAfter={$pricing.yearly.priceAfter / 12}
-						priceDetails={__( '/month, paid yearly', 'jetpack-boost' )}
-						currencyCode={$pricing.yearly.currencyCode}
-						{ctaText}
-						onCtaClick={goToCheckout}
-						{tosText}
-					/>
-				{/if}
+				<div class="jb-card__cta px-2 my-4">
+					{#if 'yearly' in $pricing}
+						<!-- svelte-ignore missing-declaration Jetpack_Boost -->
+						<ReactComponent
+							this={PricingCard}
+							title={__( 'Jetpack Boost', 'jetpack-boost' )}
+							icon={`${ Jetpack_Boost.site.assetPath }../static/images/forward.svg`}
+							priceBefore={$pricing.yearly.priceBefore / 12}
+							priceAfter={$pricing.yearly.priceAfter / 12}
+							priceDetails={__( '/month, paid yearly', 'jetpack-boost' )}
+							currencyCode={$pricing.yearly.currencyCode}
+							{ctaText}
+							onCtaClick={goToCheckout}
+						/>
+					{/if}
+				</div>
 			</div>
-		</div>
-		<footer class="jb-footer-note">
-			{__(
-				'Special introductory pricing, all renewals are at full price. 14 day money back guarantee.',
-				'jetpack-boost'
-			)}
-		</footer>
+			<footer class="jb-footer-note">
+				{__(
+					'Special introductory pricing, all renewals are at full price. 14 day money back guarantee.',
+					'jetpack-boost'
+				)}
+			</footer>
 	</div>
 </div>
