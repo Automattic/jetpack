f3a6b6b7d3f18b344f9f37867a55bb30add5a23c
Adnan Haque <adnan007.id@gmail.com>
2023-03-14 13:33:12 +0600

    Return real values for available modules via read-only data-store

--- projects/plugins/boost/app/data-sync/read-only/Available_Modules.php
+++ projects/plugins/boost/app/data-sync/read-only/Available_Modules.php
@@ -2,11 +2,18 @@
 
 namespace Automattic\Jetpack_Boost\Data_Sync\Read_Only;
 
+use Automattic\Jetpack_Boost\Modules\Modules;
+
 class Available_Modules extends Storage {
 	public function get( $_key ) {
-		return array(
-			'cloud_css',
-			'lazy_images',
+		$modules = new Modules();
+		$keys    = array_keys( $modules->available_modules() );
+
+		return array_map(
+			function ( $key ) {
+				return str_replace( '-', '_', $key );
+			},
+			$keys
 		);
 	}
 }
