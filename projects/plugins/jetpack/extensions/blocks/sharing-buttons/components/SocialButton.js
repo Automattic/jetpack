ffeb73565c562263cfb88daefb595ebe8d9974ef
Heyde Moura <moura.heyde@gmail.com>
2023-03-13 21:56:30 -0300

    [not verified] Refactor to rely on SocialIcons

--- projects/plugins/jetpack/extensions/blocks/sharing-buttons/components/SocialButton.js
+++ projects/plugins/jetpack/extensions/blocks/sharing-buttons/components/SocialButton.js
@@ -1,38 +1,48 @@
 import { __, sprintf } from '@wordpress/i18n';
-import availableServices from '../available-services';
 import { addQueryArgs } from '@wordpress/url';
+import SocialIcon from 'social-logos';
+import availableServices from '../available-services';
 
-const SocialButton = ({ service, post }) => {
+const mountLink = (service, post) => {
 	if ('email' === service) {
-		const href = addQueryArgs('mailto:', {
-			subject: __(sprintf('Shared post: %s', post.title), 'my-plugin'),
+		return addQueryArgs('mailto:', {
+			subject: __(sprintf('Shared post: %s', post.title), 'jetpack'),
 			body: post.link,
 		});
-
-		return (
-			<a
-				rel="nofollow noopener noreferrer"
-				shared={`sharing-${service}-1`}
-				href={href}
-				className={`share-${service} sd-button share-icon`}
-				target="_blank"
-				title={`Click to share on ${availableServices[service]?.label}`}
-			>
-				<span>{availableServices[service].label}</span>
-			</a>
-		);
+	} else {
+		return addQueryArgs(post.link, {
+			share: service,
+			nb: 1,
+		});
 	}
+};
 
+// const SocialButton = ({ service, post }) => {
+// 	return (
+// 		<a
+// 			className={`sd-button share-icon share-${service}`}
+// 			rel="nofollow noopener noreferrer"
+// 			shared={`sharing-${service}-1`}
+// 			target="_blank"
+// 			title={__(sprintf('Click to share on %s', availableServices[service].label), 'jetpack')}
+// 			href={mountLink(service, post)}
+// 			primary
+// 		>
+// 			{availableServices[service].label}
+// 		</a>
+// 	);
+// };
+
+const SocialButton = ({ service, post }) => {
 	return (
 		<a
-			rel="nofollow noopener noreferrer"
-			shared={`sharing-${service}-1`}
-			href={`${post.link}?share=${service}&nb=1`}
-			className={`share-${service} sd-button share-icon`}
-			target="_blank"
-			title={`Click to share on ${availableServices[service]?.label}`}
+			className={`jetpack-sharing-buttons__share-button share-${service}`}
+			href={mountLink(service, post)}
 		>
-			<span>{availableServices[service].label}</span>
+			<SocialIcon icon={availableServices[service].icon} size={24} />
+			<span className="jetpack-sharing-buttons__service-label">
+				{availableServices[service].label}
+			</span>
 		</a>
 	);
 };
