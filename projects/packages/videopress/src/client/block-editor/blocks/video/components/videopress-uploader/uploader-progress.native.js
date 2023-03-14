d38d70fb5118d8a2ce68de59b09549c8d1999ff5
Carlos Garcia <fluiddot@gmail.com>
2023-03-14 16:31:25 +0100

    [not verified] Add native version of `UploaderProgress` component

---
+++ projects/packages/videopress/src/client/block-editor/blocks/video/components/videopress-uploader/uploader-progress.native.js
@@ -0,0 +1,43 @@
+/**
+ * External dependencies
+ */
+import { MediaUploadProgress, VIDEO_ASPECT_RATIO } from '@wordpress/block-editor';
+import { Icon } from '@wordpress/components';
+import { usePreferredColorSchemeStyle } from '@wordpress/compose';
+import { View } from 'react-native';
+/**
+ * Internal dependencies
+ */
+import { VideoPressIcon } from '../icons';
+import style from './style.scss';
+
+const UploaderProgress = ( { file, onDone } ) => {
+	const containerStyle = usePreferredColorSchemeStyle(
+		style[ 'videopress-uploader__container' ],
+		style[ 'videopress-uploader__container--dark' ]
+	);
+	const iconStyle = usePreferredColorSchemeStyle(
+		style[ 'videopress-uploader__icon' ],
+		style[ 'videopress-uploader__icon--dark' ]
+	);
+
+	return (
+		<MediaUploadProgress
+			mediaId={ file?.id }
+			onFinishMediaUploadWithSuccess={ payload => {
+				const { metadata, mediaServerId } = payload;
+				onDone( { id: mediaServerId, guid: metadata?.videopressGUID } );
+			} }
+			renderContent={ ( { isUploadFailed, retryMessage } ) => {
+				return (
+					<View style={ [ containerStyle, { aspectRatio: VIDEO_ASPECT_RATIO } ] }>
+						<Icon icon={ VideoPressIcon } { ...iconStyle } />
+						{ isUploadFailed && <Text style={ style.uploadFailedText }>{ retryMessage }</Text> }
+					</View>
+				);
+			} }
+		/>
+	);
+};
+
+export default UploaderProgress;
