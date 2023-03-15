c3e1c795cd08a343334dc478269fa35b918f769f
Adnan Haque <adnan007.id@gmail.com>
2023-03-15 18:07:24 +0600

    Fix pending in SyncedStore

--- projects/js-packages/svelte-data-sync-client/src/SyncedStore.ts
+++ projects/js-packages/svelte-data-sync-client/src/SyncedStore.ts
@@ -118,7 +118,9 @@ export class SyncedStore< T > {
 		if ( signal.aborted ) {
 			return;
 		}
+		this.pending.start();
 		const result = await this.synchronize( value );
+		this.pending.stop();
 		if ( signal.aborted ) {
 			return;
 		}
