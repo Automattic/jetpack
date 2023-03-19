569f3b95fc230a4e482aaec67de3e253e89df5e0
Nauris Pūķis <hi@pyronaur.com>
2023-03-19 14:41:36 +0100

    Rename Critical CSS Sync handler

--- projects/plugins/boost/app/data-sync/critical-css-state-handler.php
+++ projects/plugins/boost/app/data-sync/critical-css-state-handler.php
@@ -4,7 +4,7 @@ namespace Automattic\Jetpack_Boost\Data_Sync;
 
 use Automattic\Jetpack\WP_JS_Data_Sync\Data_Sync_Entry_Handler;
 
-final class Critical_CSS_Sync extends Data_Sync_Entry_Handler {
+final class Critical_CSS_State_Handler extends Data_Sync_Entry_Handler {
 
 	public function parse( $value ) {
 		return $value;
