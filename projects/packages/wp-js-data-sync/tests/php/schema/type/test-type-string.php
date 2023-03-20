3f9f5cf5c17538b2d8f304a808a1d2adacff4672
Nauris Pūķis <hi@pyronaur.com>
2023-03-20 11:40:01 +0100

    Rename test methods

--- projects/packages/wp-js-data-sync/tests/php/schema/type/test-type-string.php
+++ projects/packages/wp-js-data-sync/tests/php/schema/type/test-type-string.php
@@ -5,24 +5,29 @@ use PHPUnit\Framework\TestCase;
 
 class Test_Type_String extends TestCase {
 
-	public function test_validate_with_valid_data() {
+	public function test_ok_validate_string() {
 		$validator = new Type_String();
 		$this->assertTrue( $validator->validate( 'hello world' ) );
 	}
 
-	public function test_validate_with_invalid_data() {
+	public function test_fail_validate_number() {
 		$validator = new Type_String();
 		$this->assertFalse( $validator->validate( 123 ) );
 	}
 
-	public function test_sanitize_with_valid_data() {
+	public function test_ok_sanitize_string() {
 		$validator = new Type_String();
 		$this->assertSame( 'hello world', $validator->sanitize( 'hello world' ) );
 	}
 
-	public function test_sanitize_with_invalid_data() {
+	public function test_fail_sanitize_number() {
 		$validator = new Type_String();
 		$this->assertSame( '', $validator->sanitize( 123 ) );
+	}
+
+	public function test_fail_sanitize_array() {
+		$validator = new Type_String();
+		$this->assertSame( '', $validator->sanitize( array( "hello world" ) ) );
 	}
 
 }
