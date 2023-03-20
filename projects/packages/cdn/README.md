6305f0752547e3df8100814a7ce4fc7e8ef6f3c1
Peter Petrov <peter.petrov89@gmail.com>
2023-03-20 10:22:23 +0100

    Add image CDN package

---
+++ projects/packages/cdn/README.md
@@ -0,0 +1,20 @@
+# cdn
+
+Serve static assets through CDN.
+
+## How to install cdn
+
+### Installation From Git Repo
+
+## Contribute
+
+## Get Help
+
+## Security
+
+Need to report a security vulnerability? Go to [https://automattic.com/security/](https://automattic.com/security/) or directly to our security bug bounty site [https://hackerone.com/automattic](https://hackerone.com/automattic).
+
+## License
+
+cdn is licensed under [GNU General Public License v2 (or later)](./LICENSE.txt)
+
---
+++ projects/packages/cdn/src/class-image-cdn-setup.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Package description here
+ *
+ * @package automattic/jetpack-image-cdn
+ */
+
+namespace Automattic\Jetpack;
+
+/**
+ * Class description.
+ */
+class Image_CDN_Setup {
+
+	const PACKAGE_VERSION = '1.0.0-alpha';
+
+	/**
+	 * Initialize Image CDN.
+	 */
+	public static function load() {
+		\Automattic\Jetpack\Assets::add_resource_hint(
+			array(
+				'//i0.wp.com',
+			),
+			'dns-prefetch'
+		);
+
+		require_once __DIR__ . '/compatibility/jetpack.php';
+		require_once __DIR__ . '/functions.photon.php';
+		require_once __DIR__ . '/class-image-cdn.php';
+		require_once __DIR__ . '/class-image-cdn-image.php';
+		require_once __DIR__ . '/class-image-cdn-image-sizes.php';
+
+		Image_Cdn::instance();
+	}
+}
