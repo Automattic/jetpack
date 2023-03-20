2139df562235bb0cdfdbac9e65544ca777526dac
João Heringer <joao.heringer@automattic.com>
2023-03-20 17:02:30 +0100

    Scroll behavior adjustments

--- projects/packages/forms/src/dashboard/inbox/response.js
+++ projects/packages/forms/src/dashboard/inbox/response.js
@@ -1,24 +1,11 @@
 import { dateI18n, getSettings as getDateSettings } from '@wordpress/date';
-import { Fragment, useEffect, useState } from '@wordpress/element';
+import { Fragment } from '@wordpress/element';
 import { __, sprintf } from '@wordpress/i18n';
 import classnames from 'classnames';
 import { map } from 'lodash';
 import { formatFieldName, getDisplayName, getPath } from './util';
 
 const InboxResponse = ( { loading, response } ) => {
-	const [ responseStyle, setResponseStyle ] = useState( {} );
-
-	useEffect( () => {
-		const content = document.querySelector( '#wpbody-content' );
-		const topOffset = content?.getBoundingClientRect()?.top;
-
-		if ( ! topOffset ) {
-			return;
-		}
-
-		setResponseStyle( { '--wp-content-offset-top': `${ topOffset }px` } );
-	}, [] );
-
 	const classes = classnames( 'jp-forms__inbox-response', {
 		'has-email': true,
 	} );
@@ -33,7 +20,7 @@ const InboxResponse = ( { loading, response } ) => {
 	}
 
 	return (
-		<div className={ classes } style={ responseStyle }>
+		<div className={ classes }>
 			<div className="jp-forms__inbox-response-avatar">
 				<img
 					src="https://gravatar.com/avatar/6e998f49bfee1a92cfe639eabb350bc5?size=68&default=identicon"
