39c1da60cbeaab7ae2676807db116cbe889fba75
Siobhan <siobhan@automattic.com>
2023-03-20 17:25:07 +0000

    refactor: Use native functionality and components

--- projects/packages/videopress/src/client/block-editor/blocks/video/components/color-panel/index.native.tsx
+++ projects/packages/videopress/src/client/block-editor/blocks/video/components/color-panel/index.native.tsx
@@ -1,35 +1,42 @@
 /**
- *External dependencies
+ * External dependencies
  */
+import { useNavigation } from '@react-navigation/native';
 import { PanelColorSettings } from '@wordpress/block-editor';
-import {
-	PanelRow,
-	ToggleControl,
-	// eslint-disable-next-line wpcalypso/no-unsafe-wp-apis
-	__experimentalToolsPanelItem as ToolsPanelItem,
-} from '@wordpress/components';
+import { PanelBody, ToggleControl, BottomSheet } from '@wordpress/components';
 import { useDebounce } from '@wordpress/compose';
 import { useCallback, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import { Icon, chevronRight } from '@wordpress/icons';
 /**
  * Internal dependencies
  */
-import editorImageURL from '../../../../utils/editor-image-url';
 import { VideoBlockColorAttributesProps, VideoControlProps } from '../../types';
-import dynamicColorsImage from './dynamic-colors.png';
-import './style.scss';
 /**
  * Types
  */
 import type React from 'react';
 
 /**
- * Sidebar Control component.
+ * React component that renders the playback bar color settings panel.
  *
- * @param {VideoControlProps} props - Component props.
- * @returns {React.ReactElement}    Component template
+ * @param {VideoControlProps} props - Component properties.
+ * @returns {React.ReactElement}    - Playback bar color settings panel.
  */
-export default function ColorPanel( { clientId, attributes, setAttributes }: VideoControlProps ) {
+export default function ColorPanel( { attributes, setAttributes }: VideoControlProps ) {
+	const [ showSubSheet, setShowSubSheet ] = useState( false );
+	const navigation = useNavigation();
+
+	const goBack = useCallback( () => {
+		setShowSubSheet( false );
+		navigation.goBack();
+	}, [] );
+
+	const openSubSheet = useCallback( () => {
+		navigation.navigate( BottomSheet.SubSheet.screenName );
+		setShowSubSheet( true );
+	}, [] );
+
 	const { useAverageColor, seekbarColor, seekbarLoadingColor, seekbarPlayedColor } = attributes;
 
 	const initialColorState: VideoBlockColorAttributesProps = {
@@ -49,79 +56,74 @@ export default function ColorPanel( { clientId, attributes, setAttributes }: Vid
 		debouncedSetColors( colorToUpdate );
 	}, [] );
 
-	const resetStaticColors = useCallback( () => {
-		setColorsState( {} );
-
-		setAttributes( {
-			useAverageColor: true,
-			seekbarColor: '',
-			seekbarLoadingColor: '',
-			seekbarPlayedColor: '',
-		} );
-	}, [] );
-
 	return (
-		<ToolsPanelItem
-			className="videopress-playback-bar-colors-panel-item"
-			hasValue={ () => ! useAverageColor }
-			label={ __( 'Dynamic color', 'jetpack-videopress-pkg' ) }
-			resetAllFilter={ resetStaticColors }
-			isShownByDefault
-			panelId={ clientId }
-			onDeselect={ resetStaticColors }
+		<BottomSheet.SubSheet
+			navigationButton={
+				<BottomSheet.Cell
+					label={ __( 'Dynamic Color', 'jetpack-videopress-pkg' ) }
+					onPress={ openSubSheet }
+					leftAlign
+				>
+					<Icon icon={ chevronRight }></Icon>
+				</BottomSheet.Cell>
+			}
+			showSheet={ showSubSheet }
 		>
-			<PanelRow className="videopress-color-panel__title">
-				{ __( 'Playback bar colors', 'jetpack-videopress-pkg' ) }
-			</PanelRow>
+			<>
+				<BottomSheet.NavBar>
+					<BottomSheet.NavBar.BackButton onPress={ goBack } />
+					<BottomSheet.NavBar.Heading>
+						{ __( 'Dynamic Color', 'jetpack-videopress-pkg' ) }
+					</BottomSheet.NavBar.Heading>
+				</BottomSheet.NavBar>
+				<PanelBody>
+					<ToggleControl
+						label={ __( 'Dynamic color', 'jetpack-videopress-pkg' ) }
+						help={
+							<>
+								{ __(
+									'Playback bar colors adapt to the video as it plays.',
+									'jetpack-videopress-pkg'
+								) }
+							</>
+						}
+						onChange={ newUseAverageColor =>
+							setAttributes( { useAverageColor: newUseAverageColor } )
+						}
+						checked={ useAverageColor }
+					/>
+				</PanelBody>
 
-			<ToggleControl
-				label={ __( 'Dynamic color', 'jetpack-videopress-pkg' ) }
-				help={
-					<>
-						{ __(
-							'Playback bar colors adapt to the video as it plays.',
-							'jetpack-videopress-pkg'
-						) }
-						<img
-							className="videopress-dynamic-color-example"
-							src={ editorImageURL( dynamicColorsImage ) }
-							alt={ __( 'Dynamic colors example', 'jetpack-videopress-pkg' ) }
-						></img>
-					</>
-				}
-				onChange={ newUseAverageColor => setAttributes( { useAverageColor: newUseAverageColor } ) }
-				checked={ useAverageColor }
-			/>
-
-			{ ! useAverageColor && (
-				<PanelColorSettings
-					className="videopress-color-panel"
-					opened={ ! useAverageColor }
-					showTitle={ false }
-					colorSettings={ [
-						{
-							label: __( 'Main', 'jetpack-videopress-pkg' ),
-							showTitle: true,
-							value: colors.seekbarColor,
-							onChange: newSeekbarColor => setColor( { seekbarColor: newSeekbarColor } ),
-						},
-						{
-							label: __( 'Loaded', 'jetpack-videopress-pkg' ),
-							showTitle: true,
-							value: colors.seekbarLoadingColor,
-							onChange: newSeekbarLoadingColor =>
-								setColor( { seekbarLoadingColor: newSeekbarLoadingColor } ),
-						},
-						{
-							label: __( 'Progress', 'jetpack-videopress-pkg' ),
-							showTitle: true,
-							value: colors.seekbarPlayedColor,
-							onChange: newSeekbarPlayedColor =>
-								setColor( { seekbarPlayedColor: newSeekbarPlayedColor } ),
-						},
-					] }
-				/>
-			) }
-		</ToolsPanelItem>
+				{ ! useAverageColor && (
+					<PanelColorSettings
+						className="videopress-color-panel"
+						opened={ ! useAverageColor }
+						showTitle={ false }
+						colorSettings={ [
+							{
+								label: __( 'Main', 'jetpack-videopress-pkg' ),
+								showTitle: true,
+								value: colors.seekbarColor,
+								onChange: newSeekbarColor => setColor( { seekbarColor: newSeekbarColor } ),
+							},
+							{
+								label: __( 'Loaded', 'jetpack-videopress-pkg' ),
+								showTitle: true,
+								value: colors.seekbarLoadingColor,
+								onChange: newSeekbarLoadingColor =>
+									setColor( { seekbarLoadingColor: newSeekbarLoadingColor } ),
+							},
+							{
+								label: __( 'Progress', 'jetpack-videopress-pkg' ),
+								showTitle: true,
+								value: colors.seekbarPlayedColor,
+								onChange: newSeekbarPlayedColor =>
+									setColor( { seekbarPlayedColor: newSeekbarPlayedColor } ),
+							},
+						] }
+					/>
+				) }
+			</>
+		</BottomSheet.SubSheet>
 	);
 }
