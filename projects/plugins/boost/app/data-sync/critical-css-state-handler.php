b5bc3b3a870992a35e2a552b3d36d16f9855eb50
Nauris Pūķis <hi@pyronaur.com>
2023-03-20 12:03:04 +0100

    Implement Schema in state handler parser

--- projects/plugins/boost/app/data-sync/critical-css-state-handler.php
+++ projects/plugins/boost/app/data-sync/critical-css-state-handler.php
@@ -3,28 +3,41 @@
 namespace Automattic\Jetpack_Boost\Data_Sync;
 
 use Automattic\Jetpack\WP_JS_Data_Sync\Data_Sync_Entry_Handler;
+use Automattic\Jetpack\WP_JS_Data_Sync\Schema\Schema;
 
 final class Critical_CSS_State_Handler extends Data_Sync_Entry_Handler {
 
-	public function parse( $value ) {
-		return $value;
+	/**
+	 * @var Schema;
+	 */
+	private $schema;
+
+	public function __construct() {
+		$this->schema = Schema::as_assoc_array(
+			[
+				'status'    => Schema::enum( [ 'pending', 'success', 'error', 'validation-error' ] ),
+				'providers' => Schema::as_array(
+					[
+						'key'           => Schema::as_string(),
+						'label'         => Schema::as_string(),
+						'urls'          => Schema::as_array( [ Schema::as_string() ] ),
+						'success_ratio' => Schema::as_number(),
+						'status'        => Schema::enum( [ 'pending', 'success', 'error', 'validation-error' ] ),
+					]
+				),
+				'created'   => Schema::as_number(),
+				'updated'   => Schema::as_number(),
+			]
+		);
 	}
 
-	public function validate( $_value ) {
-		return true;
+
+	public function validate( $value ) {
+		return $this->schema->validate( $value );
 	}
 
 	public function sanitize( $value ) {
-		return $value;
+		return $this->schema->sanitize( $value );
 	}
 
-	public function transform( $value ) {
-		return $value;
-	}
-
-	public function get_default_value() {
-		return array(
-			'status' => 'not_generated',
-		);
-	}
 }
