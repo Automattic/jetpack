5c23f2d25560ce332dc20b77003eb371b31b8ba2
Nauris Pūķis <hi@pyronaur.com>
2023-03-21 11:08:16 +0100

    Document the schema

--- projects/packages/wp-js-data-sync/src/schema/class-schema.php
+++ projects/packages/wp-js-data-sync/src/schema/class-schema.php
@@ -9,7 +9,55 @@ use Automattic\Jetpack\WP_JS_Data_Sync\Schema\Types\Type_Enum;
 use Automattic\Jetpack\WP_JS_Data_Sync\Schema\Types\Type_Float;
 use Automattic\Jetpack\WP_JS_Data_Sync\Schema\Types\Type_Number;
 use Automattic\Jetpack\WP_JS_Data_Sync\Schema\Types\Type_String;
-
+/**
+ * The Schema class is a factory for creating validation rules based on specific
+ * schema types. It is a central point for defining the structure of your data
+ * and ensuring that it conforms to the expected format. The class provides
+ * static methods for creating Validation_Rule instances with different data types,
+ * making it easy to build complex validation rules with a clean and readable syntax.
+ *
+ * The purpose of the Schema class is to provide a consistent way parse structured data.
+ * By using this class, you can ensure that your data adheres to the defined schema and avoid issues
+ * caused by incorrect data types or missing properties.
+ *
+ * How to use the Schema class:
+ *
+ * 1. Define your schema structure using the static methods provided by the Schema
+ *    class, such as `as_string()`, `as_array()`, `as_boolean()`, `as_number()`,
+ *    `as_float()`, and `enum()`. These methods return Validation_Rule instances
+ *    for the respective data types.
+ *
+ * 2. Optionally, you can chain additional methods on the Validation_Rule instances
+ *    to further customize the validation behavior. For example, you can use
+ *    `fallback()` to specify a default value when the input data is invalid or
+ *    `nullable()` to allow null values.
+ *
+ * 3. Validate and sanitize your data using the Validation_Rule instances. Call the
+ *    `parse()` method on the Validation_Rule instance, passing in the input data
+ *    to be parsed. The method will return the sanitized data or
+ *    a default value (if specified) when the input data is invalid.
+ *
+ * Example:
+ *
+ * $my_schema = Schema::as_array(
+ *     [
+ *         'name' => Schema::as_string(),
+ *         'age' => Schema::as_number()->fallback(0),
+ *         'is_active' => Schema::as_boolean()->nullable(),
+ *         'tags' => Schema::as_array(Schema::as_string())
+ *     ]
+ * );
+ *
+ * $input_data = [
+ *     'name' => 'John Doe',
+ *     'age' => 30,
+ *     'is_active' => null,
+ *     'tags' => ['tag1', 'tag2']
+ * ];
+ *
+ * $sanitized_data = $my_schema->parse($input_data);
+ *
+ */
 class Schema {
 	public static function as_string() {
 		return new Validation_Rule( new Type_String() );
--- projects/packages/wp-js-data-sync/src/schema/modifiers/class-decorate-with-default.php
+++ projects/packages/wp-js-data-sync/src/schema/modifiers/class-decorate-with-default.php
@@ -4,7 +4,12 @@ namespace Automattic\Jetpack\WP_JS_Data_Sync\Schema\Modifiers;
 
 use Automattic\Jetpack\WP_JS_Data_Sync\Schema\Schema_Type;
 
-class Decorate_Default implements Schema_Type {
+/**
+ * This takes in a Schema Type (like a String/Number/Boolean) and a default value.
+ * If the schema type parsing throws an error, this will catch it
+ * and return the default value instead.
+ */
+class Decorate_With_Default implements Schema_Type {
 	private $schema_type;
 	private $default_value;
 
