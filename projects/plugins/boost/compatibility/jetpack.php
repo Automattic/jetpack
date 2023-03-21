1d27be82d64097a0305988c504ae290974a4c512
Adnan Haque <adnan007.id@gmail.com>
2023-03-21 15:00:04 +0100

    Disable photon functionality in Jetpack

--- projects/plugins/boost/compatibility/jetpack.php
+++ projects/plugins/boost/compatibility/jetpack.php
@@ -86,3 +86,27 @@ add_action( 'jetpack_activate_module_lazy-images', __NAMESPACE__ . '\lazy_images
  * in case the options are out of sync when the page is loaded.
  */
 add_action( 'load-jetpack_page_jetpack-boost', __NAMESPACE__ . '\lazy_images_sync_with_jetpack' );
+
+/**
+ * Disable the unpackaged photon version living in Jetpack.
+ *
+ * We added image CDN on boost by copying the photon functionality in Jetpack to a new package in monorepo.
+ * At the time of writing, Jetpack still has the photon functionality in the Jetpack plugin. To avoid double
+ * activation, we need to disable the photon functionality in Jetpack. If boost is installed, Image CDN should
+ * be served instead.
+ */
+function jetpack_photon_compat() {
+	remove_filter( 'jetpack_photon_url', 'jetpack_photon_url', 10, 3 );
+	remove_filter( 'jetpack_photon_pre_args', 'jetpack_photon_parse_wpcom_query_args', 10, 2 );
+	remove_filter( 'jetpack_photon_skip_for_url', 'jetpack_photon_banned_domains', 9, 2 );
+	remove_filter( 'widget_text', 'jetpack_photon_support_text_widgets' );
+
+	// If photon is active, fake loading the module in Jetpack itself. This will prevent jetpack from loading
+	// the photon module, and we will use the image CDN package instead.
+	if ( \Jetpack::is_module_active( 'photon' ) ) {
+		do_action( 'jetpack_module_loaded_photon' );
+	}
+}
+
+// This compatibility file loads on plugins loaded. So, this is the right time to disable photon.
+jetpack_photon_compat();
