c8d719ed670a54e3c55a9c744c4c959755e2a56b
Robert Sreberski <robertsreberski@gmail.com>
2023-03-22 11:07:47 -0600

    Generate yoast promo package

---
+++ projects/packages/yoast-promo/src/class-yoast-promo.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Package description here
+ *
+ * @package automattic/jetpack-yoast-promo
+ */
+
+namespace Automattic\Jetpack;
+
+/**
+ * Class description.
+ */
+class Yoast_Promo {
+
+	const PACKAGE_VERSION = '1.0.0-alpha';
+
+}
