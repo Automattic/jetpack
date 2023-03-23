3f9dc994ef6f175b425b3b289042d20e4d92e1ce
Mark George <thingalon@gmail.com>
2023-03-23 08:23:29 +0100

    Make meta json nullable.

--- projects/plugins/boost/app/data-sync/critical-css-state-handler.php
+++ projects/plugins/boost/app/data-sync/critical-css-state-handler.php
@@ -33,7 +33,7 @@ final class Critical_CSS_State_Handler extends Data_Sync_Entry_Handler {
 										'url'     => Schema::as_string(),
 										'message' => Schema::as_string(),
 										'type'    => Schema::as_string(),
-										'meta'    => Schema::_any_json_data(),
+										'meta'    => Schema::_any_json_data()->nullable(),
 									)
 								)->fallback( array() )
 							),
