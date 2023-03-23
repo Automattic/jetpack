752908d13fe6481cc9f0b15f725c3391044f2ff6
Daniel Post <daniel.post@automattic.com>
2023-03-23 17:35:04 +0100

    [not verified] Improve setting check

--- projects/packages/publicize/src/social-image-generator/class-setup.php
+++ projects/packages/publicize/src/social-image-generator/class-setup.php
@@ -18,7 +18,7 @@ class Setup {
 		// Be wary of any code that you add to this file, since this function is called on plugin load.
 		// We're using the `wp_after_insert_post` hook because we need access to the updated post meta. By using the default priority
 		// of 10 we make sure that our code runs before Sync processes the post.
-		add_action( 'wp_after_insert_post', array( $this, 'generate_token_on_save' ), 10, 4 );
+		add_action( 'wp_after_insert_post', array( $this, 'generate_token_on_save' ), 10, 3 );
 	}
 
 	/**
@@ -49,34 +49,32 @@ class Setup {
 	/**
 	 * Trigger token generation for a post if SIG is enabled.
 	 *
-	 * @param int          $post_id     Post ID.
-	 * @param \WP_Post     $post        Post that's being updated.
-	 * @param bool         $update      Whether this is an existing post being updated.
-	 * @param null|WP_Post $post_before Null for new posts, the WP_Post object prior
-	 *                                  to the update for updated posts.
+	 * @param int      $post_id Post ID.
+	 * @param \WP_Post $post    Post that's being updated.
+	 * @param bool     $update  Whether this is an existing post being updated.
 	 */
-	public function generate_token_on_save( $post_id, $post, $update, $post_before ) {
+	public function generate_token_on_save( $post_id, $post, $update ) {
+		// If we're not using the block editor for this post, do not continue.
+		if ( ! use_block_editor_for_post( $post ) ) {
+			return;
+		}
+
+		global $publicize;
+
+		$post_settings = new Post_Settings( $post_id );
+		$settings      = $post_settings->get_settings();
+
+		// This is a new post. Set SIG to enabled if it wasn't set specifically.
+		if ( ! $update && $publicize->post_type_is_publicizeable( $post->post_type ) && ! isset( $settings['enabled'] ) ) {
+			$post_settings->update_setting( 'enabled', true );
+		}
+
 		if ( wp_is_post_autosave( $post ) || $post->post_status === 'auto-draft' ) {
 			return;
 		}
 
 		if ( wp_is_post_revision( $post_id ) ) {
 			return;
-		}
-
-		// If we're not using the block editor for this post, do not continue.
-		if ( ! use_block_editor_for_post( $post ) ) {
-			return;
-		}
-
-		$post_settings = new Post_Settings( $post_id );
-		$settings      = $post_settings->get_settings();
-
-		if ( ! $post_before || $post_before->post_status === 'auto-draft' ) {
-			// This is a new post. Set SIG to enabled if it wasn't set specifically.
-			if ( ! isset( $settings['enabled'] ) ) {
-				$post_settings->update_setting( 'enabled', true );
-			}
 		}
 
 		if ( ! $post_settings->is_enabled() ) {
