5919f6ea9a2706d563dbd36dc399f56f15f99e2d
João Heringer <joao.heringer@automattic.com>
2023-03-23 11:40:16 +0100

    Add Tabs to Forms dashboard inbox view

--- projects/packages/forms/src/dashboard/state/resolvers.js
+++ projects/packages/forms/src/dashboard/state/resolvers.js
@@ -10,15 +10,17 @@ import {
  * Fetches responses from backend (API)
  *
  * @param {string} search - Some search term
+ * @param {string }status - The response status (spam|trash)
  * @param {number} limit  - Maximum results to get from backend
  * @param {number} offset - The offset for the results (paging)
  * @returns {Promise}     - The fetch promise
  */
-const fetchResponsesFromApi = ( search, limit = 20, offset = 0 ) => {
+const fetchResponsesFromApi = ( search, status, limit = 20, offset = 0 ) => {
 	const queryString = new URLSearchParams( {
 		search,
 		limit,
 		offset,
+		status,
 	} ).toString();
 
 	return apiFetch( { path: `/wpcom/v2/forms/responses?${ queryString }` } );
@@ -28,14 +30,20 @@ const fetchResponsesFromApi = ( search, limit = 20, offset = 0 ) => {
  * getResponses resolver will trigger a backend request
  *
  * @param {string} search - Some search term
+ * @param {string} status - The response status (spam|trash)
  * @param {number} limit  - Maximum results to get from backend
  * @param {number} offset - The offset for the results (paging)
  * @yields
  */
-function* getResponses( search, limit = 20, offset = 0 ) {
+function* getResponses( search, status, limit = 20, offset = 0 ) {
 	try {
 		yield fetchResponses();
-		const response = yield dispatchAsync( fetchResponsesFromApi, [ search, limit, offset ] );
+		const response = yield dispatchAsync( fetchResponsesFromApi, [
+			search,
+			status,
+			limit,
+			offset,
+		] );
 		yield receiveResponsesFetch( response );
 	} catch ( error ) {
 		yield failResponsesFetch( error );
