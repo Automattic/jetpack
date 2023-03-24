665cff874552e344f415461f4e5ed41914f6c6d4
Rafael Agostini <rafael.agostini@automattic.com>
2023-03-24 17:42:30 -0500

    Validate if jetpack and jetpack-dev plugin is active

--- projects/packages/my-jetpack/src/class-initializer.php
+++ projects/packages/my-jetpack/src/class-initializer.php
@@ -168,7 +168,7 @@ class Initializer {
 				'fileSystemWriteAccess' => self::has_file_system_write_access(),
 				'loadAddLicenseScreen'  => self::is_licensing_ui_enabled(),
 				'adminUrl'              => esc_url( admin_url() ),
-				'isJetpackPluginActive' => Plugins_Installer::is_plugin_active( Plugins_Installer::get_plugin_id_by_slug( 'jetpack' ) ),
+				'isJetpackPluginActive' => Plugins_Installer::is_plugin_active( 'jetpack/jetpack.php' ) || Plugins_Installer::is_plugin_active( 'jetpack-dev/jetpack.php' ),
 			)
 		);
 
