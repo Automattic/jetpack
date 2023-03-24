8669f4db3fe2ab4865c943d50dcd81639fbd09eb
André Kallehauge <3846700+kallehauge@users.noreply.github.com>
2023-03-24 01:44:11 +0100

    Remove redundant container

--- projects/packages/my-jetpack/_inc/components/golden-token/index.jsx
+++ projects/packages/my-jetpack/_inc/components/golden-token/index.jsx
@@ -37,81 +37,79 @@ function GoldenToken( { redeemClick, hasGoldenToken } ) {
 	} );
 
 	return (
-		<div className={ styles.container }>
-			<div className={ modalClassName }>
-				<JetpackLogo className={ styles[ 'jetpack-logo' ] } />
-				<div className={ styles[ 'video-wrap' ] }>
-					{ /* eslint-disable-next-line jsx-a11y/media-has-caption */ }
-					<video
-						ref={ videoRef }
-						src="https://videos.files.wordpress.com/oSlNIBQO/jetpack-golden-token.mp4"
-					/>
+		<div className={ modalClassName }>
+			<JetpackLogo className={ styles[ 'jetpack-logo' ] } />
+			<div className={ styles[ 'video-wrap' ] }>
+				{ /* eslint-disable-next-line jsx-a11y/media-has-caption */ }
+				<video
+					ref={ videoRef }
+					src="https://videos.files.wordpress.com/oSlNIBQO/jetpack-golden-token.mp4"
+				/>
+			</div>
+
+			<div className={ styles[ 'content-wrap' ] }>
+				<div className={ styles[ 'content-wrap-text' ] }>
+					<p className={ styles[ 'hi-user' ] }>Hey, Jetpack Friend</p>
+					<h2 className={ styles.headline }>
+						{ __( 'Your exclusive Jetpack Experience awaits.', 'jetpack-my-jetpack' ) }
+					</h2>
+					<p>
+						{ __(
+							'You have been gifted a Jetpack Gold Token. This unlocks a lifetime of Jetpack powers for your website.',
+							'jetpack-my-jetpack'
+						) }
+					</p>
+				</div>
+				<Button variant="primary" weight="regular" onClick={ redeemClickHandler }>
+					{ __( 'Redeem your token', 'jetpack-my-jetpack' ) }
+				</Button>
+			</div>
+
+			<div className={ `${ styles[ 'powers-wrap' ] } ${ styles[ 'content-wrap' ] }` }>
+				<div className={ styles[ 'content-wrap-text' ] }>
+					<h2 className={ styles.headline }>
+						{ __( 'Super powers are ready!', 'jetpack-my-jetpack' ) }
+					</h2>
+					<p className={ styles.paragraph }>
+						{ __(
+							'Your Jetpack Gold Token provides a lifetime license for this website and includes the following products:',
+							'jetpack-my-jetpack'
+						) }
+					</p>
 				</div>
 
-				<div className={ styles[ 'content-wrap' ] }>
-					<div className={ styles[ 'content-wrap-text' ] }>
-						<p className={ styles[ 'hi-user' ] }>Hey, Jetpack Friend</p>
-						<h2 className={ styles.headline }>
-							{ __( 'Your exclusive Jetpack Experience awaits.', 'jetpack-my-jetpack' ) }
-						</h2>
+				<div className={ styles[ 'jetpack-products' ] }>
+					<div>
+						<JetpackBackupLogo />
+
+						<h3>{ __( 'VaultPress Backup', 'jetpack-my-jetpack' ) }</h3>
 						<p>
 							{ __(
-								'You have been gifted a Jetpack Gold Token. This unlocks a lifetime of Jetpack powers for your website.',
+								'Save every change and get back online quickly with one‑click restores.',
 								'jetpack-my-jetpack'
 							) }
 						</p>
 					</div>
-					<Button variant="primary" weight="regular" onClick={ redeemClickHandler }>
-						{ __( 'Redeem your token', 'jetpack-my-jetpack' ) }
-					</Button>
-				</div>
+					<div>
+						<JetpackScanLogo />
 
-				<div className={ `${ styles[ 'powers-wrap' ] } ${ styles[ 'content-wrap' ] }` }>
-					<div className={ styles[ 'content-wrap-text' ] }>
-						<h2 className={ styles.headline }>
-							{ __( 'Super powers are ready!', 'jetpack-my-jetpack' ) }
-						</h2>
-						<p className={ styles.paragraph }>
+						<h3>{ __( 'Scan', 'jetpack-my-jetpack' ) }</h3>
+						<p>
 							{ __(
-								'Your Jetpack Gold Token provides a lifetime license for this website and includes the following products:',
+								'Automated scanning and one‑click fixes to keep your site ahead of security threats.',
 								'jetpack-my-jetpack'
 							) }
 						</p>
 					</div>
-
-					<div className={ styles[ 'jetpack-products' ] }>
-						<div>
-							<JetpackBackupLogo />
-
-							<h3>{ __( 'VaultPress Backup', 'jetpack-my-jetpack' ) }</h3>
-							<p>
-								{ __(
-									'Save every change and get back online quickly with one‑click restores.',
-									'jetpack-my-jetpack'
-								) }
-							</p>
-						</div>
-						<div>
-							<JetpackScanLogo />
-
-							<h3>{ __( 'Scan', 'jetpack-my-jetpack' ) }</h3>
-							<p>
-								{ __(
-									'Automated scanning and one‑click fixes to keep your site ahead of security threats.',
-									'jetpack-my-jetpack'
-								) }
-							</p>
-						</div>
-					</div>
-
-					<Button
-						variant="primary"
-						weight="regular"
-						href={ window?.myJetpackInitialState?.myJetpackUrl }
-					>
-						{ __( 'Explore your new powers', 'jetpack-my-jetpack' ) }
-					</Button>
 				</div>
+
+				<Button
+					variant="primary"
+					weight="regular"
+					href={ window?.myJetpackInitialState?.myJetpackUrl }
+				>
+					{ __( 'Explore your new powers', 'jetpack-my-jetpack' ) }
+				</Button>
 			</div>
 		</div>
 	);
