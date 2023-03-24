54aaee424ccd0ba5f4a718d81d2406a9f1d311d7
dereksmart <smart@automattic.com>
2023-03-24 15:24:51 -0400

    Remove tracks. We'll do it separate.

--- projects/packages/my-jetpack/_inc/components/redeem-token-screen/index.jsx
+++ projects/packages/my-jetpack/_inc/components/redeem-token-screen/index.jsx
@@ -1,6 +1,5 @@
 import { useConnection } from '@automattic/jetpack-connection';
 import React, { useCallback } from 'react';
-import useAnalytics from '../../hooks/use-analytics';
 import usePurchases from '../../hooks/use-purchases';
 import GoldenTokenModal from '../golden-token';
 
@@ -10,23 +9,17 @@ import GoldenTokenModal from '../golden-token';
  * @returns {object} The RedeemTokenScreen component.
  */
 export default function RedeemTokenScreen() {
-	const { recordEvent } = useAnalytics();
-
-	const onModalClose = useCallback(
-		event => {
-			recordEvent( 'jetpack_myjetpack_redeem_token_back_link_click' );
-			if ( document.referrer.includes( window.location.host ) ) {
-				// Prevent default here to minimize page change within the My Jetpack app.
-				event.preventDefault();
-				history.back();
-			} else {
-				// If noreferrer, redirect to the My Jetpack dashboard.
-				event.preventDefault();
-				window.location.href = window?.myJetpackInitialState?.myJetpackUrl;
-			}
-		},
-		[ recordEvent ]
-	);
+	const onModalClose = useCallback( event => {
+		if ( document.referrer.includes( window.location.host ) ) {
+			// Prevent default here to minimize page change within the My Jetpack app.
+			event.preventDefault();
+			history.back();
+		} else {
+			// If noreferrer, redirect to the My Jetpack dashboard.
+			event.preventDefault();
+			window.location.href = window?.myJetpackInitialState?.myJetpackUrl;
+		}
+	}, [] );
 
 	const { isFetchingPurchases, purchases } = usePurchases();
 	const { userConnectionData } = useConnection();
