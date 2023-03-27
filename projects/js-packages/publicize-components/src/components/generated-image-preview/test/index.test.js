9d7dc582001910ff005d035c096be0e1be6d7a87
Gergely Juhasz <gergely.juhasz@automattic.com>
2023-03-27 12:12:14 +0200

    Add + tests for POST body check

--- projects/js-packages/publicize-components/src/components/generated-image-preview/test/index.test.js
+++ projects/js-packages/publicize-components/src/components/generated-image-preview/test/index.test.js
@@ -2,31 +2,45 @@ import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import apiFetch from '@wordpress/api-fetch';
 import { useSelect } from '@wordpress/data';
 import GeneratedImagePreview from '..';
+import useImageGeneratorConfig from '../../../hooks/use-image-generator-config';
 
 jest.mock( '@wordpress/data/build/components/use-select', () => jest.fn() );
 jest.mock( '@wordpress/api-fetch' );
 
-jest.mock( '../../../hooks/use-image-generator-config', () => {
-	return jest.fn( () => ( {
-		customText: 'Custom text',
-		imageTyp: 'Featured',
-		imageId: 1,
-		template: 'dois',
-	} ) );
-} );
+jest.mock( '../../../hooks/use-image-generator-config', () => jest.fn() );
 
 jest.mock( '../styles.module.scss', () => ( { hidden: 'hidden' } ) );
 
 const TEST_TOKEN = 'test_token_123';
 
-describe( 'GeneratedImagePreview', () => {
+const setupMocks = ( title = 'Title', customText = 'Custom text' ) => {
 	useSelect.mockImplementation( () => {
 		return {
-			title: 'Title',
+			title,
 			featuredImage: 1,
 		};
 	} );
 	apiFetch.mockReturnValue( Promise.resolve( TEST_TOKEN ) );
+	useImageGeneratorConfig.mockReturnValue( {
+		customText,
+		imageTyp: 'Featured',
+		imageId: 1,
+		template: 'dois',
+	} );
+};
+
+const getPostBody = ( text, template ) => ( {
+	path: '/jetpack/v4/social-image-generator/generate-preview-token',
+	method: 'POST',
+	data: {
+		text: text,
+		image_url: null,
+		template: template,
+	},
+} );
+
+describe( 'GeneratedImagePreview', () => {
+	setupMocks();
 
 	it( 'should define the component', () => {
 		expect( 'GeneratedImagePreview' ).toBeDefined();
@@ -38,6 +52,23 @@ describe( 'GeneratedImagePreview', () => {
 		const image = screen.queryByRole( 'img' );
 		expect( image ).toHaveClass( 'hidden' );
 		await expect( screen.findByTestId( 'spinner' ) ).resolves.toBeInTheDocument();
+	} );
+
+	it( 'should use custom text over title', async () => {
+		render( <GeneratedImagePreview /> );
+
+		await waitFor( () => {
+			expect( apiFetch ).toHaveBeenCalledWith( getPostBody( 'Custom text', 'dois' ) );
+		} );
+	} );
+
+	it( 'should use title if there is no custom text', async () => {
+		setupMocks( 'Title', '' );
+		render( <GeneratedImagePreview /> );
+
+		await waitFor( () => {
+			expect( apiFetch ).toHaveBeenCalledWith( getPostBody( 'Title', 'dois' ) );
+		} );
 	} );
 
 	it( 'should have the correct image source', async () => {
