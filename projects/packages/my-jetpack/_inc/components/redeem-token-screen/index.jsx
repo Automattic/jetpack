59121efbaf39b95c8a5caa51d4f8673e06e4dced
Derek Smart <smart@automattic.com>
2023-03-28 14:10:47 -0400

    Licensing: Add GoldenTokenModal on manual enter of key (#29748)
    * Update props. Modal now behaves differently whether the token has been redeemed already.
    
    * Move GoldenTokenModal to licensing package. Trigger it if a valid golden token is accepted.
    
    * changelog
    
    * Add handling for displayName
    
    * try to fix e2e tests

--- projects/packages/my-jetpack/_inc/components/redeem-token-screen/index.jsx
+++ projects/packages/my-jetpack/_inc/components/redeem-token-screen/index.jsx
@@ -1,7 +1,9 @@
 import { useConnection } from '@automattic/jetpack-connection';
-import React, { useCallback } from 'react';
+import { GoldenTokenModal } from '@automattic/jetpack-licensing';
+import { __ } from '@wordpress/i18n';
+import React from 'react';
 import usePurchases from '../../hooks/use-purchases';
-import GoldenTokenModal from '../golden-token';
+import { includesLifetimePurchase } from '../../utils/is-lifetime-purchase';
 
 /**
  * The RedeemToken component of the My Jetpack app.
@@ -9,27 +11,22 @@ import GoldenTokenModal from '../golden-token';
  * @returns {object} The RedeemTokenScreen component.
  */
 export default function RedeemTokenScreen() {
-	const onModalClose = useCallback( event => {
-		if ( document.referrer.includes( window.location.host ) ) {
-			// Prevent default here to minimize page change within the My Jetpack app.
-			event.preventDefault();
-			history.back();
-		} else {
-			// If noreferrer, redirect to the My Jetpack dashboard.
-			event.preventDefault();
-			window.location.href = window?.myJetpackInitialState?.myJetpackUrl;
-		}
-	}, [] );
-
-	const { isFetchingPurchases, purchases } = usePurchases();
 	const { userConnectionData } = useConnection();
+	// They might not have a display name set in wpcom, so fall back to wpcom login or local username.
+	const displayName =
+		userConnectionData?.currentUser?.wpcomUser?.display_name ||
+		userConnectionData?.currentUser?.wpcomUser?.login ||
+		userConnectionData?.currentUser?.username;
+	const { isFetchingPurchases, purchases } = usePurchases();
+	const tokenRedeemed = includesLifetimePurchase( purchases );
+
+	if ( isFetchingPurchases ) {
+		return <>{ __( 'Checking gold status…', 'jetpack-my-jetpack' ) }</>;
+	}
 
 	return (
-		<GoldenTokenModal
-			purchases={ purchases }
-			fetchingPurchases={ isFetchingPurchases }
-			userConnectionData={ userConnectionData }
-			onModalClose={ onModalClose }
-		/>
+		<>
+			<GoldenTokenModal tokenRedeemed={ tokenRedeemed } displayName={ displayName } />
+		</>
 	);
 }
