5057a5141efbbe4e3d697477f0e06d669c46b398
Paul Bunkham <paul@dobit.co.uk>
2023-03-29 11:14:01 +0100

    Features: Sync class-wpcom-features.php with D106219-code https://github.com/Automattic/wpcomsh/pull/1324
    * Features: Sync class-wpcom-features.php with D106219-code
    
    This PR is to add the `social-image-generator` feature back to the
    FEATURE_MAP and keep it in sync with the change in D106219-code.
    
    * Fix PHPCS error.

--- projects/plugins/wpcomsh/wpcom-features/class-wpcom-features.php
+++ projects/plugins/wpcomsh/wpcom-features/class-wpcom-features.php
@@ -803,6 +803,15 @@ class WPCOM_Features {
 			self::JETPACK_COMPLETE_PLANS,
 			self::BUNDLE_ENTERPRISE,
 		),
+		self::SOCIAL_IMAGE_GENERATOR         => array(
+			array(
+				// This feature isn't launched yet, so we're ensuring that it's not available on any plans.
+				'before' => '1900-01-01',
+				self::JETPACK_SOCIAL_ADVANCED_PLANS,
+				self::JETPACK_COMPLETE_PLANS,
+				self::BUNDLE_ENTERPRISE,
+			),
+		),
 
 		self::SPACE                          => array(
 			self::WPCOM_ALL_SITES,
