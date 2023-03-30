d5fd6b73a5a82e6700916d99763673cd93389907
Nate Weller <nate.weller@automattic.com>
2023-03-30 10:02:12 -0600

    [not verified] Restore the Jetpack_Protect_Module class and deprecate it

--- projects/plugins/jetpack/modules/protect.php
+++ projects/plugins/jetpack/modules/protect.php
@@ -12,3 +12,40 @@
  * Feature: Security
  * Additional Search Queries: security, jetpack protect, secure, protection, botnet, brute force, protect, login, bot, password, passwords, strong passwords, strong password, wp-login.php,  protect admin
  */
+
+use Automattic\Jetpack\Waf\Brute_Force_Protection\Brute_Force_Protection;
+
+/**
+ * Jetpack project module class.
+ *
+ * @deprecated $$next_version$$ - Use Automattic\Jetpack\Waf\Brute_Force_Protection\Brute_Force_Protection instead.
+ */
+class Jetpack_Protect_Module {
+	/**
+	 * Instance of the class.
+	 *
+	 * @var Jetpack_Protect_Module()
+	 */
+	private static $instance = null;
+
+	/**
+	 * Singleton implementation
+	 *
+	 * @return object
+	 */
+	public static function instance() {
+		if ( ! is_a( self::$instance, 'Jetpack_Protect_Module' ) ) {
+			self::$instance = new Jetpack_Protect_Module();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Check if someone is able to login based on IP.
+	 */
+	public function has_login_ability() {
+		$brute_force_protection = Brute_Force_Protection::instance();
+		return $brute_force_protection->has_login_ability();
+	}
+}
