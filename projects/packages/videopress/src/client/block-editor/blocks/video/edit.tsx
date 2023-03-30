4eb6b19504c6b226b4d9fc0ef413690293742e19
Damián Suárez <rdsuarez@gmail.com>
2023-03-30 11:54:41 -0300

    [not verified] propagate isGeneratingPoster state to Poster Panel

--- projects/packages/videopress/src/client/block-editor/blocks/video/edit.tsx
+++ projects/packages/videopress/src/client/block-editor/blocks/video/edit.tsx
@@ -40,7 +40,7 @@ import { description, title } from '.';
 /**
  * Types
  */
-import type { VideoBlockAttributes, VideoBlockEditProps } from './types';
+import type { VideoBlockAttributes } from './types';
 import type React from 'react';
 
 import './editor.scss';
@@ -101,7 +101,7 @@ export default function VideoPressEdit( {
 	setAttributes,
 	isSelected,
 	clientId,
-}: VideoBlockEditProps ): React.ReactNode {
+} ): React.ReactNode {
 	const {
 		autoplay,
 		loop,
@@ -151,6 +151,7 @@ export default function VideoPressEdit( {
 		isRequestingVideoData,
 		error: syncError,
 		isOverwriteChapterAllowed,
+		isGeneratingPoster,
 	} = useSyncMedia( attributes, setAttributes );
 
 	const { filename, private_enabled_for_site: privateEnabledForSite } = videoData;
@@ -524,6 +525,7 @@ export default function VideoPressEdit( {
 					clientId={ clientId }
 					attributes={ attributes }
 					setAttributes={ setAttributes }
+					isGeneratingPoster={ isGeneratingPoster }
 				/>
 
 				<PrivacyAndRatingPanel
