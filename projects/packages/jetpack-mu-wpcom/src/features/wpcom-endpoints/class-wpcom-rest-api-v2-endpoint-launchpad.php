f61815444388e67ec103609b346b5d39aaa45c1d
Anthony Grullon <agrullon95@gmail.com>
2023-04-03 10:08:24 -0400

    Update/launchpad checklist tasks statuses site settings (#29841)
    * Add update launchpad site settings endpoint
    
    * changelog
    
    * Updated update site options function name and comment
    
    * Update launchpad update_site_options logic
    
    * Update version
    
    * Update composer lock
    
    * changelog
    
    * Update composer lock
    
    * Update project versions

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-launchpad.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-launchpad.php
@@ -33,9 +33,19 @@ class WPCOM_REST_API_V2_Endpoint_Launchpad extends WP_REST_Controller {
 			$this->namespace,
 			$this->rest_base,
 			array(
-				'methods'             => WP_REST_Server::READABLE,
-				'callback'            => array( $this, 'get_data' ),
-				'permission_callback' => array( $this, 'can_read' ),
+				array(
+					'methods'             => WP_REST_Server::READABLE,
+					'callback'            => array( $this, 'get_data' ),
+					'permission_callback' => array( $this, 'can_access' ),
+				),
+				array(
+					'methods'             => WP_REST_Server::EDITABLE,
+					'callback'            => array( $this, 'update_site_options' ),
+					'permission_callback' => array( $this, 'can_access' ),
+					'request_format'      => array(
+						'checklist_statuses' => '(array) Array of launchpad checklist tasks completion status',
+					),
+				),
 			)
 		);
 	}
@@ -45,7 +55,7 @@ class WPCOM_REST_API_V2_Endpoint_Launchpad extends WP_REST_Controller {
 	 *
 	 * @return boolean
 	 */
-	public function can_read() {
+	public function can_access() {
 		return current_user_can( 'manage_options' );
 	}
 
@@ -62,6 +72,53 @@ class WPCOM_REST_API_V2_Endpoint_Launchpad extends WP_REST_Controller {
 			'checklist_statuses' => get_option( 'launchpad_checklist_tasks_statuses', array() ),
 		);
 	}
+
+	/**
+	 * Updates Launchpad-related options and returns the result
+	 *
+	 * @param array $request The unsanitized request values.
+	 *
+	 * @return array Associative array with updated site options
+	 */
+	public function update_site_options( $request ) {
+		$updated = array();
+		$input   = $request->get_json_params();
+
+		foreach ( $input as $key => $value ) {
+			if ( ! is_array( $value ) ) {
+				$value = trim( $value );
+			}
+
+			switch ( $key ) {
+				case 'checklist_statuses':
+					$launchpad_checklist_tasks_statuses_option = get_option( 'launchpad_checklist_tasks_statuses' );
+
+					$filtered_input_array = array_filter(
+						(array) $value,
+						function ( $array_value ) {
+							return is_bool( $array_value );
+						}
+					);
+
+					if ( ! is_array( $launchpad_checklist_tasks_statuses_option ) ) {
+						$launchpad_checklist_tasks_statuses_option = array();
+					}
+					$launchpad_checklist_tasks_statuses_option = array_merge( $launchpad_checklist_tasks_statuses_option, $filtered_input_array );
+
+					if ( update_option( 'launchpad_checklist_tasks_statuses', $launchpad_checklist_tasks_statuses_option ) ) {
+						$updated[ $key ] = $filtered_input_array;
+					}
+					break;
+				default:
+					break;
+			}
+
+			return array(
+				'updated' => $updated,
+			);
+		}
+	}
+
 }
 
 wpcom_rest_api_v2_load_plugin( 'WPCOM_REST_API_V2_Endpoint_Launchpad' );
