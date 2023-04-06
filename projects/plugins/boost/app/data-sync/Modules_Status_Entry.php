0f69bbe054dad4012a1a7368fef21eb15a447511
Adnan Haque <adnan007.id@gmail.com>
2023-04-06 12:32:54 +0300

    Store module status individually

--- projects/plugins/boost/app/data-sync/Modules_Status_Entry.php
+++ projects/plugins/boost/app/data-sync/Modules_Status_Entry.php
@@ -7,56 +7,41 @@ use Automattic\Jetpack\WP_JS_Data_Sync\Contracts\Entry_Can_Merge;
 use Automattic\Jetpack_Boost\Modules\Modules_Index;
 
 class Modules_Status_Entry implements Entry_Can_Get, Entry_Can_Merge {
-	private $key;
-
-	private $option_key;
-
-	private $namespace;
-
-	public function __construct( $namespace, $key ) {
-		$this->namespace  = $namespace;
-		$this->key        = $key;
-		$this->option_key = $this->namespace . '_' . $this->key;
-	}
-
 	public function get() {
-		$option_value = get_option( $this->option_key );
+		$modules = Modules_Index::MODULES;
+
+		$modules_state     = array();
+		$available_modules = ( new Modules_Index() )->available_modules();
 
 		/*
-		 * Module statuses are stored as a boolean record of whether the module is active.
-		 * Example: [ 'lazy_images' => true, 'critical_css' => false ]
-		 *
-		 * While reading the value we are also attaching the availability of the module.
-		 * Example: [ 'lazy_images' => [ 'active' => true, 'available' => true ], 'critical_css' => [ 'active' => false, 'available' => false ] ]
-		 *
-		 * So, we are adding the availability property to the value.
+		 * Module states are stored in their individual wp_options records.
+		 * We combining the states of all modules into a single record and attaching the availability of the module.
 		 */
-		$available_modules = ( new Modules_Index() )->available_modules();
-		foreach ( $option_value as $module_slug => $status ) {
-			$option_value[ $module_slug ] = array(
-				'active'    => $status,
-				'available' => isset( $available_modules[ $module_slug ] ),
+		foreach ( $modules as $module ) {
+			$slug        = $module::get_slug();
+			$option_name = $this->get_module_option_name( $slug );
+
+			$modules_state[ $slug ] = array(
+				'active'    => get_option( $option_name, false ),
+				'available' => isset( $available_modules[ $slug ] ),
 			);
 		}
 
-		return $option_value;
+		return $modules_state;
 	}
 
 	public function set( $value ) {
-		/*
-		 * Module statuses have two properties: active and available. But, we only want to store the active status.
-		 * Example:
-		 * The value received by svelte client: [ 'lazy_images' => [ 'active' => true, 'available' => true ], 'critical_css' => [ 'active' => false, 'available' => false ] ]
-		 * The value stored in the database: [ 'lazy_images' => true, 'critical_css' => false ]
-		 *
-		 * So, we are stripping the availability property from the value before saving it.
-		 */
-		$value = wp_list_pluck( $value, 'active' );
-
-		update_option( $this->option_key, $value );
+		foreach ( $value as $module_slug => $module_state ) {
+			$option_name = $this->get_module_option_name( $module_slug );
+			update_option( $option_name, $module_state['active'] );
+		}
 	}
 
 	public function merge( $value, $partial_value ) {
 		return array_merge( $value, $partial_value );
+	}
+
+	private function get_module_option_name( $module_slug ) {
+		return 'jetpack_boost_status_' . str_replace( '_', '-', $module_slug );
 	}
 }
