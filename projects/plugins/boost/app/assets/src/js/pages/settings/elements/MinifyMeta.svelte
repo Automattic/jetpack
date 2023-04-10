f8506dabd172ecc06f4953052404fc411b5ae238
Peter Petrov <peter.petrov89@gmail.com>
2023-04-10 12:53:03 +0300

    Add styles for minify UI 'exclude' elements

--- projects/plugins/boost/app/assets/src/js/pages/settings/elements/MinifyMeta.svelte
+++ projects/plugins/boost/app/assets/src/js/pages/settings/elements/MinifyMeta.svelte
@@ -14,6 +14,7 @@
 
 	let isEditing = true;
 	const editingJs = 'js' === type;
+	const excludesHtmlId = `${ type }-excludes-list`;
 
 	$: value = editingJs ? $minifyJsExcludesState : $minifyCssExcludesState;
 
@@ -28,19 +29,22 @@
 
 <div class="jb-critical-css__meta">
 	{#if isEditing}
-		<p>{inputLabel}</p>
-		<input
-			type="text"
-			bind:value
-			placeholder={__( 'Use comma to separate them', 'jetpack-boost' )}
-		/>
-		<div class="button-container">
-			<button on:click={handleUpdate}>Save</button>
-			<button
-				on:click={() => {
-					isEditing = false;
-				}}>Cancel</button
-			>
+		<div class="manage-excludes">
+			<label for={excludesHtmlId}>{inputLabel}</label>
+			<input
+				type="text"
+				bind:value
+				placeholder={__( 'Use comma to separate them', 'jetpack-boost' )}
+				id={excludesHtmlId}
+			/>
+			<div class="buttons-container">
+				<button on:click={handleUpdate}>Save</button>
+				<button
+					on:click={() => {
+						isEditing = false;
+					}}>Cancel</button
+				>
+			</div>
 		</div>
 	{:else}
 		<div class="summary">
@@ -65,3 +69,56 @@
 		</button>
 	{/if}
 </div>
+
+<style lang="scss">
+	@use '../../../../css/main/variables.scss' as *;
+
+	.manage-excludes {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 100%;
+	}
+
+	.manage-excludes label {
+		display: block;
+		text-align: left;
+		margin-bottom: 16px;
+		font-weight: bold;
+		width: 100%;
+	}
+
+	.manage-excludes input[type='text'] {
+		width: 100%;
+		padding: 10px;
+		border-radius: 4px;
+		border: 1px solid #ccc;
+		margin-bottom: 16px;
+	}
+
+	.buttons-container {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		width: 100%;
+	}
+
+	.buttons-container button {
+		margin-right: 10px;
+		padding: 8px 24px;
+		border: 1px solid $primary-black;
+		border-radius: 4px;
+		cursor: pointer;
+		color: $primary-black;
+	}
+
+	.buttons-container button:first-child {
+		background-color: black;
+		color: $primary-white;
+	}
+
+	.buttons-container button:last-child {
+		margin-right: 0;
+		background-color: transparent;
+	}
+</style>
