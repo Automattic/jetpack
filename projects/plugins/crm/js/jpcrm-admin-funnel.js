7f0627077567624758ac00311fd6a80aed96e103
tbradsha <32492176+tbradsha@users.noreply.github.com>
2023-04-10 09:35:09 -0600

    Change param name

--- projects/plugins/crm/js/jpcrm-admin-funnel.js
+++ projects/plugins/crm/js/jpcrm-admin-funnel.js
@@ -7,16 +7,16 @@
  * @element HTML element to turn into a funnel
  **/
 
-function jpcrm_build_funnel(data, funnel_element) {
+function jpcrm_build_funnel(funnel_data, funnel_element) {
 	// something's wrong, so stop
-	if (!element || !data) return;
+	if (!element || !funnel_data) return;
 
 	const COLORS = [ '#000', '#222', '#333', '#035d88', '#0073aa', '#00a0d2' ];
 
 	// show legend if desired
 	const SHOW_FUNNEL_LEGEND = false;
 
-	num_of_funnel_sections = data.length;
+	num_of_funnel_sections = funnel_data.length;
 	funnel_height = num_of_funnel_sections * 50;
 	let funnel_html = `<div class="jpcrm_funnel" style="height: ${funnel_height}px">`;
 	let legend_html = '';
@@ -27,16 +27,16 @@ function jpcrm_build_funnel(data, funnel_element) {
 	for (var i=0; i<num_of_funnel_sections; i++) {
 		let section_color = COLORS[i%COLORS.length];
 		let section_html = `<a
-			${data[i]['link']?'href='+data[i]['link']:''}
+			${funnel_data[i]['link']?'href='+funnel_data[i]['link']:''}
 			class="funnel_section"
-			data-hover="${data[i]['contact_status'] + ': \u00a0'}"
-			alt="${data[i]['contact_status']}: ${data[i]['backfill_count']}"
+			data-hover="${funnel_data[i]['contact_status'] + ': \u00a0'}"
+			alt="${funnel_data[i]['contact_status']}: ${funnel_data[i]['backfill_count']}"
 			style="background-color: ${section_color};"
-			>${data[i]['backfill_count']}</a>`;
+			>${funnel_data[i]['backfill_count']}</a>`;
 		funnel_html += section_html;
 
 		if (SHOW_FUNNEL_LEGEND) {
-			legend_html += `<div class="legend-color" style="background-color: ${section_color}"></div><div class="legend-label">${data[i]['contact_status']}</div>`;
+			legend_html += `<div class="legend-color" style="background-color: ${section_color}"></div><div class="legend-label">${funnel_data[i]['contact_status']}</div>`;
 		}
 	}
 	funnel_html += '</div>';
