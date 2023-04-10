e5185d41bdca35bf4bcf6dc1149eae5ca7213875
tbradsha <32492176+tbradsha@users.noreply.github.com>
2023-04-10 10:26:28 -0600

    Adjust funnel color logic

--- projects/plugins/crm/js/jpcrm-admin-funnel.js
+++ projects/plugins/crm/js/jpcrm-admin-funnel.js
@@ -1,4 +1,31 @@
 /**
+ * Gets an array of funnel segment colors
+ * 
+ * @param num_segments the number of segments to generate colors for
+ * 
+ * @returns an array of colors
+ **/
+function jpcrm_get_segment_colors(num_segments) {
+	let colors = [ '#68b3e8', '#399ce3', '#1689db', '#0675c4', '#055d9c', '#044b7a', '#02395c', '#01283d', '#001621', '#000000' ];
+
+	if (num_segments === colors.length) {
+		// use full set of colors
+		return colors;
+	} else if (num_segments < colors.length) {
+		// use a middle subset of colors
+		let mid_point = Math.floor(colors.length/2);
+		let start_index = Math.floor(mid_point - num_segments/2);
+		return colors.slice(start_index,start_index+num_segments);
+	}
+
+	// use full set of colors, repeated
+	while (num_segments > colors.length) {
+		colors = colors.concat(colors);
+	}
+	return colors.slice(0,num_segments);
+}
+
+/**
  * Draws a simple funnel
  * 
  * Inspired in part by https://codepen.io/tylerlwsmith/pen/RwNZKgp
@@ -11,8 +38,6 @@ function jpcrm_build_funnel(funnel_data, funnel_element) {
 	// something's wrong, so stop
 	if (!funnel_element || !funnel_data) return;
 
-	const COLORS = [ '#000', '#222', '#333', '#035d88', '#0073aa', '#00a0d2' ];
-
 	// show legend if desired
 	const SHOW_FUNNEL_LEGEND = false;
 
@@ -22,6 +47,9 @@ function jpcrm_build_funnel(funnel_data, funnel_element) {
 	// height of each section
 	const SECTION_HEIGHT = 50;
 
+	// colors to use
+	const SEGMENT_COLORS = jpcrm_get_segment_colors(NUM_FUNNEL_SECTIONS);
+
 	let funnel_html = `<div class="jpcrm_funnel" style="height: ${SECTION_HEIGHT * NUM_FUNNEL_SECTIONS}px">`;
 	let legend_html = '';
 	
@@ -29,7 +57,7 @@ function jpcrm_build_funnel(funnel_data, funnel_element) {
 		legend_html = '<div class="jpcrm_funnel_legend">';
 	}
 	for (var i=0; i<NUM_FUNNEL_SECTIONS; i++) {
-		let section_color = COLORS[i%COLORS.length];
+		let section_color = SEGMENT_COLORS[i];
 		funnel_html += `<a
 			${funnel_data[i]['link']?'href='+funnel_data[i]['link']:''}
 			class="funnel_section"
@@ -52,7 +80,6 @@ function jpcrm_build_funnel(funnel_data, funnel_element) {
 		funnel_html += '<br>' + legend_html;
 	}
 	funnel_element.innerHTML = funnel_html + '<br>';
-
 }
 
 if ( typeof module !== 'undefined' ) {
