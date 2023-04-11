43e6a761b38715e8a2d47c75e88d5d48d6c9bc45
Manzoor Wani <manzoorwani.jk@gmail.com>
2023-04-11 16:07:30 +0530

    Create Twitter preview component wrapper

--- projects/js-packages/publicize-components/src/components/social-previews/utils.js
+++ projects/js-packages/publicize-components/src/components/social-previews/utils.js
@@ -12,3 +12,29 @@ export function getMediaSourceUrl( media ) {
 	// Try getting the large size (1024px width) and fallback to the full size.
 	return media.media_details?.sizes?.large?.source_url || media.source_url;
 }
+
+/**
+ * Get the text for the auto-shared tweet.
+ *
+ * @param {object} params - The params.
+ * @param {string} params.title - The title.
+ * @param {string} params.url 	- The url.
+ * @param {string} params.text 	- The text.
+ * @returns {string} The text for the auto-shared tweet.
+ */
+export function getAutoSharedTweetText( { title, url, text } ) {
+	let content = ( text || title || '' ).trim();
+
+	// 24 is the length of the URL and 2 is for the space and the ellipsis.
+	const maxTweetLength = 280 - ( 24 + 2 );
+
+	if ( content.length > maxTweetLength ) {
+		// Get the limited number of characters without breaking words.
+		content =
+			content.match( new RegExp( '.{1,' + maxTweetLength + '}(?:\\s|$)', 'u' ) )?.[ 0 ]?.trim() ||
+			'';
+		content += '…';
+	}
+
+	return `${ content } ${ url }`;
+}
