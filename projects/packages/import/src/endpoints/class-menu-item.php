34ddf4067cf7e54e695675cfde0cff15c745f610
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2023-04-11 12:09:23 +0200

    [not verified] Add UIMP start endpoint

--- projects/packages/import/src/endpoints/class-menu-item.php
+++ projects/packages/import/src/endpoints/class-menu-item.php
@@ -13,9 +13,14 @@ namespace Automattic\Jetpack\Import\Endpoints;
 class Menu_Item extends \WP_REST_Menu_Items_Controller {
 
 	/**
-	 * The Import ID add a new item to the schema.
+	 * Base class
 	 */
 	use Import;
+
+	/**
+	 * The Import ID add a new item to the schema.
+	 */
+	use Import_ID;
 
 	/**
 	 * Whether the controller supports batching. Default true.
