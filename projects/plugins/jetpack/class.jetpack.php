8e100e26874e24b41b38dc9d025148d92ee77d60
Brad Jorsch <brad.jorsch@automattic.com>
2023-04-12 14:43:56 -0400

    jetpack: Only check `is_connection_ready()` once in `Jetpack::configure()`
    It's not likely to change in the middle of the function, save a few
    microseconds by only checking the status once.

--- projects/plugins/jetpack/class.jetpack.php
+++ projects/plugins/jetpack/class.jetpack.php
@@ -901,7 +901,9 @@ class Jetpack {
 			$network->set_connection( $this->connection_manager );
 		}
 
-		if ( self::is_connection_ready() ) {
+		$is_connection_ready = self::is_connection_ready();
+
+		if ( $is_connection_ready ) {
 			add_action( 'login_form_jetpack_json_api_authorization', array( $this, 'login_form_json_api_authorization' ) );
 
 			Jetpack_Heartbeat::init();
@@ -917,7 +919,7 @@ class Jetpack {
 		/*
 		 * Enable enhanced handling of previewing sites in Calypso
 		 */
-		if ( self::is_connection_ready() ) {
+		if ( $is_connection_ready ) {
 			require_once JETPACK__PLUGIN_DIR . '_inc/lib/class.jetpack-iframe-embed.php';
 			add_action( 'init', array( 'Jetpack_Iframe_Embed', 'init' ), 9, 0 );
 			require_once JETPACK__PLUGIN_DIR . '_inc/lib/class.jetpack-keyring-service-helper.php';
