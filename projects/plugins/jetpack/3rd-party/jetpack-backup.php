88345c9322e21c30a87ec82368f81f1d1ab07bca
Brandon Kraft <public@brandonkraft.com>
2023-04-14 15:24:26 -0500

    Remove unneeded check

--- projects/plugins/jetpack/3rd-party/jetpack-backup.php
+++ projects/plugins/jetpack/3rd-party/jetpack-backup.php
@@ -33,10 +33,6 @@ if ( isset( $_GET['jetpack-backup-action'] ) ) { // phpcs:ignore WordPress.Secur
  * This works in tandem with a JITM set up in the JITM package.
  */
 function try_install() {
-	if ( ! isset( $_GET['jetpack-backup-action'] ) ) {
-		return;
-	}
-
 	check_admin_referer( 'jetpack-backup-install' );
 
 	$result = false;
@@ -45,7 +41,7 @@ function try_install() {
 
 	// Attempt to install and activate the plugin.
 	if ( current_user_can( 'activate_plugins' ) ) {
-		switch ( $_GET['jetpack-backup-action'] ) {
+		switch ( $_GET['jetpack-backup-action'] ) { // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotValidated -- Function only hooked if set.
 			case 'install':
 				$result = install_and_activate();
 				break;
@@ -99,10 +95,6 @@ function activate() {
  * Notify the user that the installation of Jetpack Backup failed.
  */
 function error_notice() {
-	if ( empty( $_GET['jetpack-backup-install-error'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
-		return;
-	}
-
 	?>
 	<div class="notice notice-error is-dismissible">
 		<p><?php esc_html_e( 'There was an error installing Jetpack Backup. Please try again.', 'jetpack' ); ?></p>
