d407f95a17d28d9eb406f5ec1bcf8d3ed7215294
Carlos Garcia <fluiddot@gmail.com>
2023-04-18 14:11:36 +0200

    Bump version to 0.13.9-alpha

--- projects/packages/videopress/src/class-package-version.php
+++ projects/packages/videopress/src/class-package-version.php
@@ -11,7 +11,7 @@ namespace Automattic\Jetpack\VideoPress;
  * The Package_Version class.
  */
 class Package_Version {
-	const PACKAGE_VERSION = '0.13.8';
+	const PACKAGE_VERSION = '0.13.9-alpha';
 
 	const PACKAGE_SLUG = 'videopress';
 
