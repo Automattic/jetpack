e3b0c7666fd0164c3db015d21cad026c61f6c418
Christian Gastrell <cgastrell@gmail.com>
2023-04-19 13:02:04 -0300

    resolve changelogger conflicts

--- projects/packages/forms/src/class-jetpack-forms.php
+++ projects/packages/forms/src/class-jetpack-forms.php
@@ -15,7 +15,7 @@ use Automattic\Jetpack\Forms\Dashboard\Dashboard_View_Switch;
  */
 class Jetpack_Forms {
 
-	const PACKAGE_VERSION = '0.13.1-alpha';
+	const PACKAGE_VERSION = '0.14.0-alpha';
 
 	/**
 	 * Load the contact form module.
