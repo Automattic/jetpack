5db69a04a3f38081d7b8c9860b5876ee63eb5c13
Peter Petrov <peter.petrov89@gmail.com>
2023-04-19 15:51:51 +0300

    Add migration for page optimize options

--- projects/plugins/boost/compatibility/page-optimize.php
+++ projects/plugins/boost/compatibility/page-optimize.php
@@ -1,28 +1,34 @@
 <?php
+/**
+ * Compatibility file for Page Optimize.
+ *
+ * This will synchronize the settings from Page Optimize to Jetpack Boost.
+ *
+ * @package automattic/jetpack-boost
+ */
 
-function jetpack_boost_page_optimize_js_excludes( $default ) {
-	$exclude_list = get_option( 'page_optimize-js-exclude' );
-	if ( ! $exclude_list ) {
-		return $default;
-	}
-
-	delete_option( 'page_optimize-js-exclude' );
-	add_option( 'jetpack_boost_ds_minify_js_excludes', $exclude_list );
-
-	return $exclude_list;
+$js_concatenate_enabled  = (bool) get_option( 'page_optimize-js', page_optimize_js_default() );
+$boost_js_concat_enabled = get_option( 'jetpack_boost_status_minify-js' );
+if ( $js_concatenate_enabled && false === $boost_js_concat_enabled ) {
+	add_option( 'jetpack_boost_status_minify-js', true );
 }
 
-function jetpack_boost_page_optimize_css_excludes( $default ) {
-	$exclude_list = get_option( 'page_optimize-css-exclude' );
-	if ( ! $exclude_list ) {
-		return $default;
-	}
+$boost_js_excludes = get_option( 'jetpack_boost_ds_minify_js_excludes' );
+if ( false === $boost_js_excludes ) {
+	$js_excludes = implode( ',', page_optimize_js_exclude_list() );
 
-	delete_option( 'page_optimize-css-exclude' );
-	add_option( 'jetpack_boost_ds_minify_css_excludes', $exclude_list );
-
-	return $exclude_list;
+	add_option( 'jetpack_boost_ds_minify_js_excludes', $js_excludes );
 }
 
-add_filter( 'default_option_jetpack_boost_ds_minify_js_excludes', 'jetpack_boost_page_optimize_js_excludes' );
-add_filter( 'default_option_jetpack_boost_ds_minify_css_excludes', 'jetpack_boost_page_optimize_css_excludes' );
+$css_concatenate_enabled  = (bool) get_option( 'page_optimize-css', page_optimize_css_default() );
+$boost_css_concat_enabled = get_option( 'jetpack_boost_status_minify-css' );
+if ( $css_concatenate_enabled && false === $boost_css_concat_enabled ) {
+	add_option( 'jetpack_boost_status_minify-css', true );
+}
+
+$boost_css_excludes = get_option( 'jetpack_boost_ds_minify_css_excludes' );
+if ( false === $boost_css_excludes ) {
+	$css_excludes = implode( ',', page_optimize_css_exclude_list() );
+
+	add_option( 'jetpack_boost_ds_minify_css_excludes', $css_excludes );
+}
