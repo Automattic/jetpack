8b2e34549142e8f45e7da607916998175b8440ba
Nate Weller <nate.weller@automattic.com>
2023-04-19 16:33:35 -0600

    Add Status component

---
+++ projects/js-packages/components/components/status/index.tsx
@@ -0,0 +1,39 @@
+import { __ } from '@wordpress/i18n';
+import classNames from 'classnames';
+import styles from './style.module.scss';
+
+interface StatusProps {
+	status?: 'active' | 'error' | 'inactive';
+	label?: string;
+	className?: string;
+}
+
+const Status = ( { className, label, status = 'inactive', ...rest }: StatusProps ): JSX.Element => {
+	const defaultLabels: Record< string, string > = {
+		active: __( 'Active', 'jetpack' ),
+		error: __( 'Error', 'jetpack' ),
+		inactive: __( 'Inactive', 'jetpack' ),
+	};
+
+	return (
+		<span
+			className={ classNames(
+				styles.status,
+				{
+					[ styles[ `status--${ status }` ] ]: status,
+				},
+				className
+			) }
+			{ ...rest }
+		>
+			<span className={ styles.status__indicator } />
+			{ label || label === '' ? (
+				<span className={ styles.status__label }>{ label }</span>
+			) : (
+				<span className={ styles.status__label }>{ defaultLabels[ status ] }</span>
+			) }
+		</span>
+	);
+};
+
+export default Status;
---
+++ projects/js-packages/components/components/status/stories/index.stories.tsx
@@ -0,0 +1,34 @@
+import Status from '..';
+
+export default {
+	title: 'JS Packages/Components/Status',
+	component: Status,
+	parameters: {
+		layout: 'centered',
+	},
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const Active = args => {
+	return <Status status="active" />;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const Error = args => {
+	return <Status status="error" />;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const Inactive = args => {
+	return <Status status="inactive" />;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const CustomLabel = args => {
+	return <Status label="Code is poetry" />;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const NoLabel = args => {
+	return <Status status="active" label="" />;
+};
