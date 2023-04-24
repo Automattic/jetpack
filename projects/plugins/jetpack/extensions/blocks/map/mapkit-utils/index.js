ed37efa87a2b29ac9cbc2a2441c243c340c8dc72
Tim Broddin <tim@wannabes.be>
2023-04-24 15:56:50 +0200

    Remove helper class & separate endpoint for now

--- projects/plugins/jetpack/extensions/blocks/map/mapkit-utils/index.js
+++ projects/plugins/jetpack/extensions/blocks/map/mapkit-utils/index.js
@@ -1,4 +1,3 @@
-import apiFetch from '@wordpress/api-fetch';
 import { waitForObject } from '../../../shared/block-editor-asset-loader';
 
 const earthRadius = 6.371e6;
@@ -113,9 +112,9 @@ function fetchMapkitKey( mapkitObj, blogId, currentWindow ) {
 			mapkitObj.init( {
 				authorizationCallback: async done => {
 					try {
-						const data = await apiFetch( { path: `/wpcom/v2/mapkit-token` } );
-
-						if ( data ) {
+						const response = await fetch( `https://public-api.wordpress.com/wpcom/v2/mapkit` );
+						if ( response.status === 200 ) {
+							const data = await response.json();
 							done( data.wpcom_mapkit_access_token );
 						} else {
 							reject();
