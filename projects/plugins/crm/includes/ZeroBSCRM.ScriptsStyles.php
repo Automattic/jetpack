60a9b22f6baec88490e321b835070728e2baacc9
tbradsha <32492176+tbradsha@users.noreply.github.com>
2023-04-25 09:25:10 -0600

    Update enqueue handles

--- projects/plugins/crm/includes/ZeroBSCRM.ScriptsStyles.php
+++ projects/plugins/crm/includes/ZeroBSCRM.ScriptsStyles.php
@@ -376,7 +376,7 @@ function zeroBSCRM_global_admin_styles(){
 		wp_enqueue_script( 'zerobsjsmodal');
 
 		// moment everywhere (from 2.98)
-		wp_enqueue_script('wh-moment-v2-8-1-js', untrailingslashit(ZEROBSCRM_URL) .'/js/lib/moment-with-locales.min.js', array('jquery'), $zbs->version );
+		wp_enqueue_script( 'jpcrm-moment-v2-29-4', untrailingslashit( ZEROBSCRM_URL ) . '/js/lib/moment-with-locales.min.js', array( 'jquery' ), $zbs->version, false );
 
 		// semantic everywhere (on our pages)
 		wp_enqueue_style( 'zbs-wp-semanticui' );  
@@ -587,10 +587,10 @@ function zeroBSCRM_calendar_admin_styles(){
 
 	wp_register_style('zerobscrm-events', ZEROBSCRM_URL .'css/ZeroBSCRM.admin.events'.wp_scripts_get_suffix().'.css', array(), $zbs->version );
 
-	wp_register_script('zerobscrm-calendar-js' , ZEROBSCRM_URL .'js/lib/fullcalendar.mod'.wp_scripts_get_suffix().'.js', array('jquery','wh-moment-v2-8-1-js'), $zbs->version );//2.97.9 upgraded moment array('jquery','wh-moment-v2-1-3-js'));
+	wp_register_script( 'zerobscrm-calendar-js', ZEROBSCRM_URL . 'js/lib/fullcalendar.mod' . wp_scripts_get_suffix() . '.js', array( 'jquery', 'jpcrm-moment-v2-29-4' ), $zbs->version, false );
 	wp_register_style('zerobscrm-calendar', ZEROBSCRM_URL .'css/lib/fullcalendar.min.css', array(), $zbs->version );
 	wp_register_style('zerobscrm-calendar-print', ZEROBSCRM_URL .'css/lib/fullcalendar.print.min.css', array(), $zbs->version );
-	wp_register_script('zerobscrm-events-js' , ZEROBSCRM_URL .'js/ZeroBSCRM.admin.task'.wp_scripts_get_suffix().'.js', array('jquery','wh-moment-v2-8-1-js','zerobscrm-calendar-js'), $zbs->version );
+	wp_register_script( 'zerobscrm-events-js', ZEROBSCRM_URL . 'js/ZeroBSCRM.admin.task' . wp_scripts_get_suffix() . '.js', array( 'jquery', 'jpcrm-moment-v2-29-4', 'zerobscrm-calendar-js' ), $zbs->version, false );
 	
 
 	// LOCALE Specific
