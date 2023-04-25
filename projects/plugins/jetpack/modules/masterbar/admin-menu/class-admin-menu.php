354289961fba2cd11f6000816c62ed59e8da836d
Wojtek Naruniec <wojtek@naruniec.me>
2023-04-25 16:45:24 +0200

    Hide Backup link for WPCOM staging sites

--- projects/plugins/jetpack/modules/masterbar/admin-menu/class-admin-menu.php
+++ projects/plugins/jetpack/modules/masterbar/admin-menu/class-admin-menu.php
@@ -412,7 +412,10 @@ class Admin_Menu extends Base_Admin_Menu {
 		}
 
 		add_submenu_page( 'jetpack', esc_attr__( 'Activity Log', 'jetpack' ), __( 'Activity Log', 'jetpack' ), 'manage_options', 'https://wordpress.com/activity-log/' . $this->domain, null, 2 );
-		add_submenu_page( 'jetpack', esc_attr__( 'Backup', 'jetpack' ), __( 'Backup', 'jetpack' ), 'manage_options', 'https://wordpress.com/backup/' . $this->domain, null, 3 );
+
+		if ( ! get_option( 'wpcom_is_staging_site' ) ) {
+			add_submenu_page( 'jetpack', esc_attr__( 'Backup', 'jetpack' ), __( 'Backup', 'jetpack' ), 'manage_options', 'https://wordpress.com/backup/' . $this->domain, null, 3 );
+		}
 
 		$this->hide_submenu_page( 'jetpack', 'jetpack#/settings' );
 		$this->hide_submenu_page( 'jetpack', 'stats' );
