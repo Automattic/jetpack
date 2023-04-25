224fa03439116b70867b752221672f94e8eb35c3
Nauris Pūķis <hi@pyronaur.com>
2023-04-25 14:55:26 +0300

    Get more sample images

--- projects/plugins/boost/app/data-sync/init-image-size-analysis.php
+++ projects/plugins/boost/app/data-sync/init-image-size-analysis.php
@@ -3,62 +3,78 @@
 use Automattic\Jetpack\WP_JS_Data_Sync\Schema\Schema;
 
 function jetpack_boost_mock_api( $count ) {
-	$args = array(
-		'post_type'      => 'post',
-		'post_status'    => 'publish',
-		'posts_per_page' => $count,
-		'orderby'        => 'date',
-		'order'          => 'DESC',
-		'meta_query'     => array(
-			array(
-				'key'     => '_thumbnail_id',
-				'compare' => 'EXISTS',
-			),
-		),
-	);
+	$image_posts    = array();
+	$offset         = 0;
+	$posts_per_page = 10;
 
-	$posts       = get_posts( $args );
-	$image_posts = array();
-
-	foreach ( $posts as $post ) {
-		$image_meta = array();
-
-		// Get the featured image.
-		$image_url                  = get_the_post_thumbnail_url( $post->ID );
-		$image_meta['thumbnail']    = $image_url;
-		$image_meta['image']['url'] = $image_url;
-
-		// Get image dimensions.
-		list( $width, $height )                        = getimagesize( $image_url );
-		$random                                        = mt_rand( 50, 90 ) / 100;
-		$image_meta['image']['dimensions']['file']     = array(
-			'width'  => $width,
-			'height' => $height,
-		);
-		$image_meta['image']['dimensions']['expected'] = array(
-			'width'  => $width * $random,
-			'height' => $height * $random,
-		);
-		$image_meta['image']['dimensions']['size_on_screen'] = array(
-			'width'  => ( $width * $random ) / 2,
-			'height' => ( $width * $random ) / 2,
+	while ( count( $image_posts ) < $count ) {
+		$args = array(
+			'post_type'      => 'post',
+			'post_status'    => 'publish',
+			'posts_per_page' => $posts_per_page,
+			'offset'         => $offset,
+			'orderby'        => 'date',
+			'order'          => 'DESC',
 		);
 
-		// Get image weight.
-		$weight                                     = filesize( get_attached_file( get_post_thumbnail_id( $post->ID ) ) ) / 1024;
-		$image_meta['image']['weight']['current']   = $weight;
-		$image_meta['image']['weight']['potential'] = $weight * 0.5;
+		$posts = get_posts( $args );
 
-		$image_meta['page']['id']    = $post->ID;
-		$permalink                   = home_url() . '?p=' . $post->ID; // Fallback permalink with query parameter
-		$image_meta['page']['url']   = $permalink;
-		$image_meta['page']['title'] = get_the_title( $post->ID );
+		// Break the loop if no more posts are found.
+		if ( empty( $posts ) ) {
+			break;
+		}
 
-		$image_meta['device_type'] = mt_rand( 1, 2 ) === 1 ? 'phone' : 'desktop';
+		foreach ( $posts as $post ) {
+			// Get all attached images.
+			$attached_images = get_attached_media( 'image', $post->ID );
 
-		$image_meta['instructions'] = 'Resize the image to the expected dimensions and compress it.';
+			foreach ( $attached_images as $attachment ) {
+				if ( count( $image_posts ) >= $count ) {
+					break 2; // Break out of both loops.
+				}
 
-		$image_posts[] = $image_meta;
+				$image_meta = array();
+
+				// Get the image URL.
+				$image_url                  = wp_get_attachment_url( $attachment->ID );
+				$image_meta['thumbnail']    = $image_url;
+				$image_meta['image']['url'] = $image_url;
+
+				// Get image dimensions.
+				list( $width, $height )                        = getimagesize( $image_url );
+				$random                                        = mt_rand( 50, 90 ) / 100;
+				$image_meta['image']['dimensions']['file']     = array(
+					'width'  => $width,
+					'height' => $height,
+				);
+				$image_meta['image']['dimensions']['expected'] = array(
+					'width'  => $width * $random,
+					'height' => $height * $random,
+				);
+				$image_meta['image']['dimensions']['size_on_screen'] = array(
+					'width'  => ( $width * $random ) / 2,
+					'height' => ( $width * $random ) / 2,
+				);
+
+				// Get image weight.
+				$weight                                     = filesize( get_attached_file( get_post_thumbnail_id( $post->ID ) ) ) / 1024;
+				$image_meta['image']['weight']['current']   = $weight;
+				$image_meta['image']['weight']['potential'] = $weight * 0.5;
+
+				$image_meta['page']['id']    = $post->ID;
+				$permalink                   = home_url() . '?p=' . $post->ID; // Fallback permalink with query parameter
+				$image_meta['page']['url']   = $permalink;
+				$image_meta['page']['title'] = get_the_title( $post->ID );
+
+				$image_meta['device_type'] = mt_rand( 1, 2 ) === 1 ? 'phone' : 'desktop';
+
+				$image_meta['instructions'] = 'Resize the image to the expected dimensions and compress it.';
+
+				$image_posts[] = $image_meta;
+			}
+		}
+
+		$offset += $posts_per_page;
 	}
 
 	return array(
@@ -120,6 +136,6 @@ $image_size_analysis = Schema::as_assoc_array(
 			)
 		),
 	)
-)->fallback( jetpack_boost_mock_api( 10 ) );
+)->fallback( jetpack_boost_mock_api( 21 ) );
 
 jetpack_boost_register_option( 'image_size_analysis', $image_size_analysis );
