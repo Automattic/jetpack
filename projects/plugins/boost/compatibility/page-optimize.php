b35035327bd8384b4d1af5c36c61913c6727e952
Peter Petrov <peter.petrov89@gmail.com>
2023-04-25 12:44:35 +0300

    [not verified] Improve code readability of Page Optimize compatibility

--- projects/plugins/boost/compatibility/page-optimize.php
+++ projects/plugins/boost/compatibility/page-optimize.php
@@ -9,36 +9,42 @@
  */
 
 if ( function_exists( 'page_optimize_js_default' ) ) {
-	$js_concatenate_enabled  = (bool) get_option( 'page_optimize-js', page_optimize_js_default() );
-	$boost_js_concat_enabled = get_option( 'jetpack_boost_status_minify-js' );
-	if ( $js_concatenate_enabled && false === $boost_js_concat_enabled ) {
+	$page_optimize_js_concatenate = (bool) get_option( 'page_optimize-js', page_optimize_js_default() );
+	$boost_js_concatenate         = get_option( 'jetpack_boost_status_minify-js' );
+	// Only migrate JS Concatenation if Page Optimize has it enabled
+	// and if Boost's equivalent hasn't been used at all.
+	if ( $page_optimize_js_concatenate && false === $boost_js_concatenate ) {
 		add_option( 'jetpack_boost_status_minify-js', true );
 	}
 }
 
 if ( function_exists( 'page_optimize_js_exclude_list' ) ) {
 	$boost_js_excludes = get_option( 'jetpack_boost_ds_minify_js_excludes' );
+	// Only migrate this setting if Boost's equivalent hasn't been used.
 	if ( false === $boost_js_excludes ) {
-		$js_excludes = implode( ',', page_optimize_js_exclude_list() );
+		$page_optimize_js_excludes = implode( ',', page_optimize_js_exclude_list() );
 
-		add_option( 'jetpack_boost_ds_minify_js_excludes', $js_excludes );
+		add_option( 'jetpack_boost_ds_minify_js_excludes', $page_optimize_js_excludes );
 	}
 }
 
 if ( function_exists( 'page_optimize_css_default' ) ) {
-	$css_concatenate_enabled  = (bool) get_option( 'page_optimize-css', page_optimize_css_default() );
-	$boost_css_concat_enabled = get_option( 'jetpack_boost_status_minify-css' );
-	if ( $css_concatenate_enabled && false === $boost_css_concat_enabled ) {
+	$css_concatenate       = (bool) get_option( 'page_optimize-css', page_optimize_css_default() );
+	$boost_css_concatenate = get_option( 'jetpack_boost_status_minify-css' );
+	// Only migrate CSS Concatenation if Page Optimize has it enabled
+	// and if Boost's equivalent hasn't been used at all.
+	if ( $css_concatenate && false === $boost_css_concatenate ) {
 		add_option( 'jetpack_boost_status_minify-css', true );
 	}
 }
 
 if ( function_exists( 'page_optimize_css_exclude_list' ) ) {
 	$boost_css_excludes = get_option( 'jetpack_boost_ds_minify_css_excludes' );
+	// Only migrate this setting if Boost's equivalent hasn't been used.
 	if ( false === $boost_css_excludes ) {
-		$css_excludes = implode( ',', page_optimize_css_exclude_list() );
+		$page_optimize_css_excludes = implode( ',', page_optimize_css_exclude_list() );
 
-		add_option( 'jetpack_boost_ds_minify_css_excludes', $css_excludes );
+		add_option( 'jetpack_boost_ds_minify_css_excludes', $page_optimize_css_excludes );
 	}
 }
 
