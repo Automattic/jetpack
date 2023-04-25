fd23b112ea2954fe79bd66308f0530946f438d45
Nauris Pūķis <hi@pyronaur.com>
2023-04-25 13:17:45 +0300

    Navigate back to the dashboard when clicking the logo

--- projects/plugins/boost/app/assets/src/js/sections/Header.svelte
+++ projects/plugins/boost/app/assets/src/js/sections/Header.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
+	import { useNavigate } from 'svelte-navigator';
 	import Logo from '../svg/logo.svg';
+	const navigate = useNavigate();
 </script>
 
 <div class="jb-dashboard-header">
-	<div class="jb-dashboard-header__logo">
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<div class="jb-dashboard-header__logo" on:click={() => navigate( '/' )}>
 		<Logo />
 	</div>
 </div>
+
+<style lang="scss">
+	.jb-dashboard-header__logo {
+		cursor: pointer;
+	}
+</style>
