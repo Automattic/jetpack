8673a58952ebc78ff7e10cfafc5100f9294310a1
Renan <renansscarvalho@gmail.com>
2023-04-26 15:10:22 +0200

    Add initial server side php blogroll content

--- projects/plugins/jetpack/extensions/blocks/blogroll/blogroll.php
+++ projects/plugins/jetpack/extensions/blocks/blogroll/blogroll.php
@@ -24,11 +24,54 @@ function register_block() {
 	Blocks::jetpack_register_block(
 		BLOCK_NAME,
 		array(
+			'attributes'      => array(
+				'blogroll_title' => array(
+					'type' => 'string',
+				),
+				'align'          => array(
+					'type' => 'string',
+				),
+			),
 			'render_callback' => __NAMESPACE__ . '\render',
 		)
 	);
 }
 add_action( 'init', __NAMESPACE__ . '\register_block' );
+
+/**
+ * Return markup bookmark content.
+ *
+ * @param array $attributes Array containing the Blogroll block attributes.
+ *
+ * @return string block markup.
+ */
+function get_bookmark_content( $attributes ) {
+	$blogroll_title = $attributes['blogroll_title'];
+	$bookmars       = get_bookmarks(
+		array(
+			'orderby'       => 'name',
+			'order'         => 'ASC',
+			'category_name' => '',
+			'limit'         => -1,
+		)
+	);
+
+	$bookmars = array_map(
+		function ( $bookmark ) {
+			return sprintf(
+				'<li><a href="%1$s">%2$s</a></li>',
+				$bookmark->link_url,
+				$bookmark->link_name
+			);
+		},
+		$bookmars
+	);
+
+	return sprintf(
+		'<ul>%s</ul>',
+		implode( $bookmars )
+	);
+}
 
 /**
  * Blogroll block registration/dependency declaration.
@@ -43,7 +86,13 @@ function render( $attributes, $content ) {
 	 * Enqueue necessary scripts and styles.
 	 */
 	Jetpack_Gutenberg::load_assets_as_required( FEATURE_NAME );
-	return '<div>Blogroll</div>';
+	$content = get_bookmark_content( $attributes );
+
+	return sprintf(
+		'<div">%1$s</div>',
+		$content
+	);
 }
 
-
+// Aid users in enabling the links menu without having to install other plugins or search how to.
+add_filter( 'pre_option_link_manager_enabled', '__return_true' );
--- projects/plugins/jetpack/extensions/blocks/blogroll/edit.js
+++ projects/plugins/jetpack/extensions/blocks/blogroll/edit.js
@@ -1,4 +1,4 @@
-import { useBlockProps, InspectorControls, InnerBlocks } from '@wordpress/block-editor';
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
 import { PanelBody, TextControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import ServerSideRender from '@wordpress/server-side-render';
@@ -6,12 +6,18 @@ import './editor.scss';
 
 export default function Edit( props ) {
 	const { attributes, setAttributes, className } = props;
-	const { blogroll_title } = attributes;
+	const { blogroll_title, align } = attributes;
 
 	const blockProps = useBlockProps();
 	return (
 		<div className={ className } { ...blockProps }>
-			<ServerSideRender block="jetpack/blogroll" />
+			{ blogroll_title.trim() !== '' && (
+				<div>
+					<h1>{ blogroll_title }</h1>
+				</div>
+			) }
+			<ServerSideRender block="jetpack/blogroll" attributes={ { blogroll_title, align } } />
+
 			<InspectorControls>
 				<PanelBody title={ 'Blogroll Title' }>
 					<TextControl
@@ -22,9 +28,6 @@ export default function Edit( props ) {
 					/>
 				</PanelBody>
 			</InspectorControls>
-			<div>
-				<InnerBlocks allowedBlocks={ [ 'jetpack/blogroll-item' ] } orientation="vertical" />
-			</div>
 		</div>
 	);
 }
