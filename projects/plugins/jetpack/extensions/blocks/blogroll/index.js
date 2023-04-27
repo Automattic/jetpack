003648fee17c10f246166a50914c56b2bcaccbd6
heavyweight <kpapazov@gmail.com>
2023-04-27 12:13:55 +0200

    Add icon, keywords, description

--- projects/plugins/jetpack/extensions/blocks/blogroll/index.js
+++ projects/plugins/jetpack/extensions/blocks/blogroll/index.js
@@ -1,11 +1,24 @@
-import { __ } from '@wordpress/i18n';
+import { __, _x } from '@wordpress/i18n';
+import { getIconColor } from '../../shared/block-icons';
 import edit from './edit';
 
 export const name = 'blogroll';
 export const title = __( 'Blogroll', 'jetpack' );
 export const settings = {
 	title,
-	description: __( 'The blogroll.', 'jetpack' ),
+	icon: {
+		src: 'admin-links',
+		foreground: getIconColor(),
+	},
+	category: 'text',
+	keywords: [
+		_x( 'blog roll', 'block search term', 'jetpack' ),
+		_x( 'links', 'block search term', 'jetpack' ),
+	],
+	description: __(
+		'The blogroll block allows you to display your links added from the link manager.',
+		'jetpack'
+	),
 	supports: {
 		align: [ 'left', 'right', 'wide', 'full' ],
 		color: {
