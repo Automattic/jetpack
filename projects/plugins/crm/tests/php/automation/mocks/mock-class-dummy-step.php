d34a7c06cb9f8e412beeb52098cc30b1a4c6f4e2
cleacos <javacodia@gmail.com>
2023-04-29 09:50:07 +0100

    [not verified] Add Dummy_Step

---
+++ projects/plugins/crm/tests/php/automation/mocks/mock-class-dummy-step.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Automattic\Jetpack\CRM\Automation\Tests\Mocks;
+
+use Automattic\Jetpack\CRM\Automation\Automation_Logger;
+use Automattic\Jetpack\CRM\Automation\Base_Step;
+
+class Dummy_Step extends Base_Step {
+	
+	public function __construct( array $step_data )
+	{
+	 	parent::__construct( $step_data );
+		 
+		$this->name = 'dummy_step';
+		$this->title = 'Dummy Step';
+		$this->description = 'Dummy step for testing purposes';
+		$this->type = 'dummy';
+		$this->category = 'testing';
+		
+		$this->data = [
+			'name' => 'dummy_step',
+			'title' => 'Dummy Step',
+			'description' => 'Dummy step for testing purposes',
+			'type' => 'dummy',
+			'category' => 'testing',
+		];
+	}
+
+	/**
+	 * Execute the step
+	 * 
+	 * @param array $data
+	 * @return void
+	 */
+	public function execute( array $data ) {
+		Automation_Logger::instance()->log( 'Dummy step executed' );
+	}
+}
