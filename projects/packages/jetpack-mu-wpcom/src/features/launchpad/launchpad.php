c45b3bdfb0e6da43f6f929623e59fc6e2d81c3a5
Matt Wiebe <wiebe@automattic.com>
2023-05-02 14:17:08 -0500

    Fix domain check

--- projects/packages/jetpack-mu-wpcom/src/features/launchpad/launchpad.php
+++ projects/packages/jetpack-mu-wpcom/src/features/launchpad/launchpad.php
@@ -338,16 +338,9 @@ function is_design_step_enabled() {
  * @return boolean True if domain upsell task is completed
  */
 function is_domain_upsell_completed() {
-	// Adding a 'false' bypass to this for now to unblock Jetpack release.
-	// This was resulting in queries being run on too many requests to wpcom.
-	// Slack context - p1682634633096559/1682634536.650749-slack-C0299DMPG
-	if ( false && defined( 'IS_WPCOM' ) && IS_WPCOM ) {
-		if ( class_exists( '\WPCOM_Store_API' ) ) {
-			$plan = \WPCOM_Store_API::get_current_plan( \get_current_blog_id() );
-			return ! $plan['is_free'] || get_checklist_task( 'domain_upsell_deferred' );
-		}
+	if ( wpcom_site_has_feature( 'custom-domain' ) ) {
+		return true;
 	}
-
 	return get_checklist_task( 'domain_upsell_deferred' );
 }
 
