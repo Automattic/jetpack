b1ff39f2c8874bbab2842bdefdce50a2a187fafd
Peter Petrov <peter.petrov89@gmail.com>
2023-05-02 15:23:07 +0300

    Fix minify store referencing old DS client

--- projects/plugins/boost/app/assets/src/js/stores/minify.ts
+++ projects/plugins/boost/app/assets/src/js/stores/minify.ts
@@ -1,13 +1,13 @@
 import { type Writable, get, writable } from 'svelte/store';
 import { z } from 'zod';
-import { client } from './data-sync-client';
+import { jetpack_boost_ds } from './data-sync-client';
 
-export const minifyJsExcludesClient = client.createAsyncStore(
+export const minifyJsExcludesClient = jetpack_boost_ds.createAsyncStore(
 	'minify_js_excludes',
 	z.array( z.string() )
 );
 
-export const minifyCssExcludesClient = client.createAsyncStore(
+export const minifyCssExcludesClient = jetpack_boost_ds.createAsyncStore(
 	'minify_css_excludes',
 	z.array( z.string() )
 );
