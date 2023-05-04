db2a8e3c7ae792294b749a412bde8e90e39273cb
cleacos <javacodia@gmail.com>
2023-05-04 12:27:10 +0100

    [not verified] Update Automation_Exception codes

--- projects/plugins/crm/src/automation/class-automation-exception.php
+++ projects/plugins/crm/src/automation/class-automation-exception.php
@@ -3,10 +3,8 @@
 namespace Automattic\Jetpack\CRM\Automation;
 
 class Automation_Exception extends \Exception {
-	const TRIGGER_NOT_EXIST       = 10;
-	const TRIGGER_CLASS_NOT_FOUND = 11;
-	const CONDITION_NOT_EXIST     = 20;
-	const ACTION_NOT_EXIST        = 30;
+	const STEP_CLASS_NOT_FOUND    = 10;
+	const TRIGGER_CLASS_NOT_FOUND = 20;
 	const GENERAL_ERROR           = 999;
 
 	public function __construct( $message = 'Automation Exception', $code = self::GENERAL_ERROR ) {
