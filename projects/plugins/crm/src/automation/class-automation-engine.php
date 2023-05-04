2e804bbfb58ed40063ca034556abd7956b91fe34
cleacos <javacodia@gmail.com>
2023-05-04 12:26:45 +0100

    [not verified] Use only two mapping: triggers and steps map

--- projects/plugins/crm/src/automation/class-automation-engine.php
+++ projects/plugins/crm/src/automation/class-automation-engine.php
@@ -12,14 +12,11 @@ class Automation_Engine {
 	/** @var Automation_Engine Instance singleton */
 	private static $instance = null;
 
-	/** @var array Triggers map name => classname */
+	/** @var array triggers map name => classname */
 	private $triggers_map = array();
-
-	/** @var array Actions map name => classname */
-	private $actions_map = array();
-
-	/** @var array Conditions map name => classname */
-	private $conditions_map = array();
+	
+	/** @var array steps map name => classname */
+	private $steps_map = array();
 
 	/** @var Automation_Logger Automation logger */
 	private $automation_logger;
@@ -67,119 +64,31 @@ class Automation_Engine {
 	}
 
 	/**
-	 * Register an action
-	 *
-	 * @param string $action_name
+	 * Register a step in the automation engine
+	 * 
+	 * @param string $step_name
 	 * @param string $class_name
 	 * @throws Automation_Exception
 	 */
-	public function register_action( string $action_name, string $class_name ) {
+	public function register_step( string $step_name, string $class_name ) {
 		if ( ! class_exists( $class_name ) ) {
-			throw new Automation_Exception( sprintf( __( 'Action class %s does not exist', 'zero-bs-crm' ), $class_name ) );
+			throw new Automation_Exception( sprintf( __( 'Step class %s does not exist', 'zero-bs-crm' ), $class_name ), Automation_Exception::STEP_CLASS_NOT_FOUND );
 		}
-		$this->actions_map[ $action_name ] = $class_name;
-	}
-
-	/**
-	 * Register a condition
-	 *
-	 * @param string $condition_name
-	 * @param string $class_name
-	 * @throws Automation_Exception
-	 */
-	public function register_condition( string $condition_name, string $class_name ) {
-		if ( ! class_exists( $class_name ) ) {
-			throw new Automation_Exception( sprintf( __( 'Condition class %s does not exist', 'zero-bs-crm' ), $class_name ) );
-		}
-		$this->conditions_map[ $condition_name ] = $class_name;
-	}
-
-	/**
-	 * Get a trigger class by name
-	 *
-	 * @param string $trigger_name
-	 * @return string
-	 * @throws Automation_Exception
-	 */
-	public function get_trigger_class( string $trigger_name ): string {
-		if ( ! isset( $this->triggers_map[ $trigger_name ] ) ) {
-			throw new Automation_Exception( sprintf( __( 'Trigger %s does not exist', 'zero-bs-crm' ), $trigger_name ) );
-		}
-		return $this->triggers_map[ $trigger_name ];
-	}
-
-	/**
-	 * Get an action class by name
-	 *
-	 * @param string $action_name
-	 * @return string
-	 * @throws Automation_Exception
-	 */
-	public function get_action_class( string $action_name ): string {
-		if ( ! isset( $this->actions_map[ $action_name ] ) ) {
-			throw new Automation_Exception( sprintf( __( 'Action %s does not exist', 'zero-bs-crm' ), $action_name ) );
-		}
-		return $this->actions_map[ $action_name ];
-	}
-
-	/**
-	 * Get a condition class by name
-	 *
-	 * @param string $condition_name
-	 * @return string
-	 * @throws Automation_Exception
-	 */
-	public function get_condition_class( string $condition_name ): string {
-		if ( ! isset( $this->conditions_map[ $condition_name ] ) ) {
-			throw new Automation_Exception( sprintf( __( 'Condition %s does not exist', 'zero-bs-crm' ), $condition_name ) );
-		}
-		return $this->conditions_map[ $condition_name ];
+		$this->steps_map[ $step_name ] = $class_name;
 	}
 
 	/**
 	 * Get a step class by name
 	 *
 	 * @param string $step_name
-	 * @param string $step_type
 	 * @return string
 	 * @throws Automation_Exception
 	 */
-	public function get_step_class( string $step_name, string $step_type ): string {
-		switch ( $step_type ) {
-			case 'action':
-				return $this->get_action_class( $step_name );
-			case 'condition':
-				return $this->get_condition_class( $step_name );
-			default:
-				throw new Automation_Exception( sprintf( __( 'Step type %s does not exist', 'zero-bs-crm' ), $step_type ) );
+	public function get_step_class( string $step_name ): string {
+		if ( ! isset( $this->steps_map[ $step_name ] ) ) {
+			throw new Automation_Exception( sprintf( __( 'Step %s does not exist', 'zero-bs-crm' ), $step_name ), Automation_Exception::STEP_CLASS_NOT_FOUND );
 		}
-	}
-
-	/**
-	 * Get registered triggers
-	 *
-	 * @return array
-	 */
-	public function get_registered_triggers(): array {
-		return $this->triggers_map;
-	}
-
-	/**
-	 * Get registered actions
-	 *
-	 * @return array
-	 */
-	public function get_registered_actions(): array {
-		return $this->actions_map;
-	}
-
-	/**
-	 * Get registered conditions
-	 *
-	 * @return array
-	 */
-	public function get_registered_conditions(): array {
-		return $this->conditions_map;
+		return $this->steps_map[ $step_name ];
 	}
 
 	/**
@@ -201,8 +110,8 @@ class Automation_Engine {
 	/**
 	 * Build and add a recipe
 	 *
-	 * @param array       $recipe_data
-	 * @param $init_recipe
+	 * @param array $recipe_data
+	 * @param bool $init_recipe
 	 * @return Automation_Recipe
 	 * @throws Recipe_Exception
 	 */
@@ -222,5 +131,43 @@ class Automation_Engine {
 		foreach ( $this->recipes as $recipe ) {
 			$recipe->init_triggers();
 		}
+	}
+
+	/**
+	 * Get step instance
+	 * 
+	 * @param $step_name
+	 * @param array $step_data
+	 * @return Step
+	 * @throws Automation_Exception
+	 */
+	public function get_registered_step( $step_name, array $step_data = array() ): Step {
+		
+		$step_class = $this->get_step_class( $step_name );
+		
+		if ( ! class_exists( $step_class ) ) {
+			throw new Automation_Exception( sprintf( __( 'Step class %s does not exist', 'zero-bs-crm' ), $step_class ), Automation_Exception::STEP_CLASS_NOT_FOUND );
+		}
+		
+		return new $step_class( $step_data );
+	}
+
+	/**
+	 * Get registered steps
+	 * 
+	 * @return array
+	 */
+	public function get_registered_steps():array {
+		return $this->steps_map;
+	}
+
+	/**
+	 * Get trigger instance
+	 *
+	 * @param string $trigger_name
+	 * @return string
+	 */
+	public function get_trigger_class( string $trigger_name ): string {
+		return $this->triggers_map[ $trigger_name ];
 	}
 }
