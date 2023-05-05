e3d0818465ff12a6224ffa45b8c33bae496c8739
cleacos <javacodia@gmail.com>
2023-05-05 12:07:07 +0100

    [not verified] Update Base_Condition

--- projects/plugins/crm/src/automation/class-base-condition.php
+++ projects/plugins/crm/src/automation/class-base-condition.php
@@ -1,13 +1,26 @@
 <?php
+/**
+ * Base Condition implementation
+ *
+ * @package automattic/jetpack-crm
+ */
 
 namespace Automattic\Jetpack\CRM\Automation;
 
+/**
+ * Base Condition Step
+ *
+ * @inheritDoc
+ */
 abstract class Base_Condition extends Base_Step implements Condition {
-	
-	protected $next_step_true  = null;
+
+	/** @var array|null Next step data if it meets the condition  */
+	protected $next_step_true = null;
+	/** @var array|null Next step data if it does not meet the condition */
 	protected $next_step_false = null;
-	protected $condition_met   = false;
-	
+	/** @var bool If the condition is met or not */
+	protected $condition_met = false;
+
 	/**
 	 * Base_Condition constructor.
 	 *
@@ -15,7 +28,7 @@ abstract class Base_Condition extends Base_Step implements Condition {
 	 */
 	public function __construct( array $step_data ) {
 		parent::__construct( $step_data );
-		
+
 		$this->next_step_true  = $step_data['next_step_true'] ?? null;
 		$this->next_step_false = $step_data['next_step_false'] ?? null;
 	}
@@ -26,12 +39,12 @@ abstract class Base_Condition extends Base_Step implements Condition {
 	 * @return array|null
 	 */
 	public function get_next_step(): ?array {
-		return $this->condition_met ? $this->next_step_true : $this->next_step_false;
+		return ( $this->condition_met ? $this->next_step_true : $this->next_step_false );
 	}
-	
+
 	/**
 	 *  Met the condition?
-	 * 
+	 *
 	 * @return bool
 	 */
 	public function condition_met(): bool {
