bc21c5f2c4b31860efffba1f4c6195074fe55fbb
cleacos <javacodia@gmail.com>
2023-05-05 11:49:10 +0100

    [not verified] Update Base_Action implementing Action interface

--- projects/plugins/crm/src/automation/class-base-action.php
+++ projects/plugins/crm/src/automation/class-base-action.php
@@ -1,7 +1,17 @@
 <?php
+/**
+ * Base Action implementation
+ *
+ * @package automattic/jetpack-crm
+ */
 
 namespace Automattic\Jetpack\CRM\Automation;
 
-abstract class Base_Action extends Base_Step {
+/**
+ * Base Action Step
+ *
+ * @inheritDoc
+ */
+abstract class Base_Action extends Base_Step implements Action {
 
 }
