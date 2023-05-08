151a389ec3b887f5807e82d0fe0030dd9cee9ca5
Nauris Pūķis <hi@pyronaur.com>
2023-05-08 10:14:23 +0300

    Boost/update/igp phase 4 (#30435)
    * Tab UI
    
    * Refactor the image size analysis schema
    
    * Wire up Tabs
    
    * Add an inline TODO
    
    * Split Stores from the mock file
    
    * Add a todo
    
    * Do not autoload Data Sync Options
    
    * Add a fallback to the todo code
    
    * Wire up DataSync issue reporting
    
    * Move stores
    
    * Create a prefix for Image Size Analysis (ISA)
    
    * Simplistic ignore implementation
    
    * Add changelog entry.
    
    * Update changelogger
    
    * Linting fixes
    
    * Fix fatal dashboard error when flag is off

--- projects/plugins/boost/app/data-sync/init-image-size-analysis.php
+++ projects/plugins/boost/app/data-sync/init-image-size-analysis.php
@@ -1,148 +1,98 @@
 <?php
-// phpcs:ignoreFile
-// This is a temporary file only active when the flag is enabled.
 
 use Automattic\Jetpack\WP_JS_Data_Sync\Schema\Schema;
 use Automattic\Jetpack_Boost\Data_Sync\Image_Size_Analysis_Entry;
+use Automattic\Jetpack_Boost\Data_Sync\Image_Size_Analysis_Groups;
 
-function jetpack_boost_mock_api( $count, $paged = 1 ) {
-	$image_posts    = array();
-	$posts_per_page = 10;
-
-
-	while ( count( $image_posts ) < $count ) {
-		$args = array(
-			'post_type'      => 'post',
-			'post_status'    => 'publish',
-			'posts_per_page' => $posts_per_page,
-			'paged'          => $paged,
-			'orderby'        => 'date',
-			'order'          => 'DESC',
-		);
-
-		$posts = get_posts( $args );
-
-		// Break the loop if no more posts are found.
-		if ( empty( $posts ) ) {
-			break;
-		}
-
-		foreach ( $posts as $post ) {
-			// Get all attached images.
-			$attached_images = get_attached_media( 'image', $post->ID );
-
-			foreach ( $attached_images as $attachment ) {
-				if ( count( $image_posts ) >= $count ) {
-					break 2; // Break out of both loops.
-				}
-
-				$image_meta = array();
-
-				// Get the image URL.
-				$image_url                  = wp_get_attachment_url( $attachment->ID );
-				$image_meta['thumbnail']    = $image_url;
-				$image_meta['image']['url'] = $image_url;
-
-				// Get image dimensions.
-				list( $width, $height ) = getimagesize( $image_url );
-				$random                                              = random_int( 50, 90 ) / 100;
-				$image_meta['image']['dimensions']['file']           = array(
-					'width'  => $width,
-					'height' => $height,
-				);
-				$image_meta['image']['dimensions']['expected']       = array(
-					'width'  => $width * $random,
-					'height' => $height * $random,
-				);
-				$image_meta['image']['dimensions']['size_on_screen'] = array(
-					'width'  => ( $width * $random ) / 2,
-					'height' => ( $width * $random ) / 2,
-				);
-
-				// Get image weight.
-				$weight                                     = filesize( get_attached_file( get_post_thumbnail_id( $post->ID ) ) ) / 1024;
-				$image_meta['image']['weight']['current']   = $weight;
-				$image_meta['image']['weight']['potential'] = $weight * 0.5;
-
-				$image_meta['page']['id']    = $post->ID;
-				$permalink                   = home_url() . '?p=' . $post->ID; // Fallback permalink with query parameter
-				$image_meta['page']['url']   = $permalink;
-				$image_meta['page']['title'] = get_the_title( $post->ID );
-
-				$image_meta['device_type'] = random_int( 1, 2 ) === 1 ? 'phone' : 'desktop';
-
-				$image_meta['instructions'] = 'Resize the image to the expected dimensions and compress it.';
-
-				$image_posts[] = $image_meta;
-			}
-		}
-
-	}
-
-	return $image_posts;
-}
+$image_data = Schema::as_assoc_array(
+	array(
+		'thumbnail'    => Schema::as_string(),
+		'image'        => Schema::as_assoc_array(
+			array(
+				'url'        => Schema::as_string(),
+				'dimensions' => Schema::as_assoc_array(
+					array(
+						'file'           => Schema::as_assoc_array(
+							array(
+								'width'  => Schema::as_number(),
+								'height' => Schema::as_number(),
+							)
+						),
+						'expected'       => Schema::as_assoc_array(
+							array(
+								'width'  => Schema::as_number(),
+								'height' => Schema::as_number(),
+							)
+						),
+						'size_on_screen' => Schema::as_assoc_array(
+							array(
+								'width'  => Schema::as_number(),
+								'height' => Schema::as_number(),
+							)
+						),
+					)
+				),
+				'weight'     => Schema::as_assoc_array(
+					array(
+						'current'   => Schema::as_number(),
+						'potential' => Schema::as_number(),
+					)
+				),
+			)
+		),
+		'page'         => Schema::as_assoc_array(
+			array(
+				'id'    => Schema::as_number(),
+				'url'   => Schema::as_string(),
+				'title' => Schema::as_string(),
+			)
+		),
+		'device_type'  => Schema::enum( array( 'phone', 'desktop' ) ),
+		'instructions' => Schema::as_string(),
+	)
+);
 
 $image_size_analysis = Schema::as_assoc_array(
 	array(
-		'last_updated' => Schema::as_number(),
-		'pagination'   => Schema::as_assoc_array(
-			[
-				'current' => Schema::as_number(),
-				'total'   => Schema::as_number(),
-			]
+		'query' => Schema::as_assoc_array(
+			array(
+				'page'   => Schema::as_number(),
+				'group'  => Schema::as_string(),
+				'search' => Schema::as_string(),
+			)
 		),
-		'images'       => Schema::as_array(
-			Schema::as_assoc_array(
-				array(
-					'thumbnail'    => Schema::as_string(),
-					'image'        => Schema::as_assoc_array(
-						array(
-							'url'        => Schema::as_string(),
-							'dimensions' => Schema::as_assoc_array(
-								array(
-									'file'           => Schema::as_assoc_array(
-										array(
-											'width'  => Schema::as_number(),
-											'height' => Schema::as_number(),
-										)
-									),
-									'expected'       => Schema::as_assoc_array(
-										array(
-											'width'  => Schema::as_number(),
-											'height' => Schema::as_number(),
-										)
-									),
-									'size_on_screen' => Schema::as_assoc_array(
-										array(
-											'width'  => Schema::as_number(),
-											'height' => Schema::as_number(),
-										)
-									),
-								)
-							),
-							'weight'     => Schema::as_assoc_array(
-								array(
-									'current'   => Schema::as_number(),
-									'potential' => Schema::as_number(),
-								)
-							),
-						)
-					),
-					'page'         => Schema::as_assoc_array(
-						array(
-							'id'    => Schema::as_number(),
-							'url'   => Schema::as_string(),
-							'title' => Schema::as_string(),
-						)
-					),
-					'device_type'  => Schema::enum( array( 'phone', 'desktop' ) ),
-					'instructions' => Schema::as_string(),
-				)
+		'data'  => Schema::as_assoc_array(
+			array(
+				'last_updated' => Schema::as_number(),
+				'total_pages'  => Schema::as_number(),
+				'images'       => Schema::as_array( $image_data ),
 			)
 		),
 	)
 );
 
-
 $entry = new Image_Size_Analysis_Entry();
 jetpack_boost_register_option( 'image_size_analysis', $image_size_analysis, $entry );
+
+$group_schema = Schema::as_assoc_array(
+	array(
+		'name'     => Schema::as_string(),
+		'progress' => Schema::as_number(),
+		'issues'   => Schema::as_number(),
+		'done'     => Schema::as_boolean(),
+	)
+);
+jetpack_boost_register_option(
+	'image_size_analysis_groups',
+	Schema::as_array( $group_schema ),
+	new Image_Size_Analysis_Groups()
+);
+
+// @TODO
+// Implement using class Storage_Post_Type
+jetpack_boost_register_option(
+	'image_size_analysis_ignored_images',
+	Schema::as_array(
+		$image_data
+	)->fallback( array() )
+);
