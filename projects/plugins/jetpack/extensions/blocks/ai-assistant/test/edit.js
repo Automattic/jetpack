046a356321cd7fc7d8eb1047ca51baf8c90e8c73
Damián Suárez <rdsuarez@gmail.com>
2023-05-09 08:56:25 -0300

    [not verified] add createPrompt tests

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/test/edit.js
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/test/edit.js
@@ -1,57 +1,66 @@
+/**
+ * External dependencies
+ */
 import {
-	createPrompt,
-	// MAXIMUM_NUMBER_OF_CHARACTERS_SENT_FROM_CONTENT,
+	MAXIMUM_NUMBER_OF_CHARACTERS_SENT_FROM_CONTENT,
 	PROMPT_SUFFIX,
+	createPrompt,
 } from '../create-prompt';
 
 describe( 'AIParagraphEdit', () => {
-	test.skip( 'createPrompt', () => {
-		const fakeBlock = { attributes: { content: 'content' } };
-		const fakeBlockWithBr = { attributes: { content: 'content<br/>content2' } };
-		// const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-		// const charactersLength = characters.length;
-		// let longContent = '';
-		// for ( let i = 0; i < MAXIMUM_NUMBER_OF_CHARACTERS_SENT_FROM_CONTENT + 10; i++ ) {
-		// 	longContent += characters.charAt( Math.floor( Math.random() * charactersLength ) );
-		// }
-		// const fakeBlockWithVeryLongContent = { attributes: { content: longContent } };
+	test( 'createPrompt', () => {
+		const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+		const charactersLength = characters.length;
+		let longContent = '';
+		for ( let i = 0; i < MAXIMUM_NUMBER_OF_CHARACTERS_SENT_FROM_CONTENT + 10; i++ ) {
+			longContent += characters.charAt( Math.floor( Math.random() * charactersLength ) );
+		}
 
 		// Test empty posts get falsy
 		expect( createPrompt() ).toBeFalsy();
 		expect( createPrompt( '', [], '', '' ) ).toBeFalsy();
 
-		// Test Title summary - no blocks
-		expect( createPrompt( 'title', [], '', '', '', 'titleSummary' ) ).toBe(
-			"Please help me write a short piece of a blog post titled 'title'" + PROMPT_SUFFIX
+		// Test Title summary - no content
+		expect( createPrompt( 'The story of my life', '', '', '', '', 'titleSummary' ) ).toBe(
+			"Please help me write a short piece of a blog post titled 'The story of my life'" +
+				PROMPT_SUFFIX
 		);
 
-		// Test Expand preceding - with blocks
+		// Test `continue` preceding - with content
 		expect(
-			createPrompt(
-				'title',
-				[ fakeBlock, { attributes: { whatever: 'content' } } ],
-				'',
-				'',
-				'',
-				'continue'
-			)
-		).toBe( ' Please continue from here:\n\n … content' + PROMPT_SUFFIX );
+			createPrompt( 'The story of my life', 'whatver the post content is', '', '', '', 'continue' )
+		).toBe( ' Please continue from here:\n\n … whatver the post content is' + PROMPT_SUFFIX );
 
 		// Test that <BR/> are being translated. And content trimmed
-		expect( createPrompt( 'title', [ fakeBlockWithBr ], '', '', '', 'continue' ) ).toBe(
-			' Please continue from here:\n\n … content\ncontent2' + PROMPT_SUFFIX
+		expect(
+			createPrompt( 'The story of my life', 'content<br/>content2', '', '', '', 'continue' )
+		).toBe( ' Please continue from here:\n\n … content\ncontent2' + PROMPT_SUFFIX );
+
+		// Generated based on `title` and `content`.
+		expect( createPrompt( 'The story of my life', 'This story is obout hate and love...' ) ).toBe(
+			"Please help me write a short piece of a blog post titled 'The story of my life'. Please continue from here:\n\n … " +
+				'This story is obout hate and love...' +
+				PROMPT_SUFFIX
 		);
 
-		// Test MAX content length @todo: fix it!
-		// expect( createPrompt( 'title', [ fakeBlockWithVeryLongContent ] ) ).toBe(
-		// 	"Please help me write a short piece of a blog post titled 'title'. Please only output generated content ready for publishing. Please continue from here:\n\n … " +
-		// 		longContent.slice( -MAXIMUM_NUMBER_OF_CHARACTERS_SENT_FROM_CONTENT )
-		// );
+		// Generated based on `title` and (long) `content`.
+		expect( createPrompt( 'The story of my life', longContent ) ).toBe(
+			"Please help me write a short piece of a blog post titled 'The story of my life'. Please continue from here:\n\n … " +
+				longContent.slice( -MAXIMUM_NUMBER_OF_CHARACTERS_SENT_FROM_CONTENT ) +
+				PROMPT_SUFFIX
+		);
+
+		// Summarize
+		expect( createPrompt( '', longContent, '', '', '', 'summarize' ) ).toBe(
+			'Summarize this:\n\n … ' +
+				longContent.slice( -MAXIMUM_NUMBER_OF_CHARACTERS_SENT_FROM_CONTENT ) +
+				PROMPT_SUFFIX
+		);
 
 		/* @todo: Uncommment once tags and categories are supported
 
-		// expect( createPrompt( 'title', [ fakeBlock, fakeBlock ], 'cat 1', '' ) ).toBe(
-		// 	"Please help me write a short piece of a blog post titled 'title', published in categories 'cat 1'. Please only output generated content ready for publishing. Please continue from here:\n\n … content\ncontent"
+		// expect( createPrompt( 'The story of my life', [ fakeBlock, fakeBlock ], 'cat 1', '' ) ).toBe(
+		// 	"Please help me write a short piece of a blog post titled 'The story of my life', published in categories 'cat 1'. Please only output generated content ready for publishing. Please continue from here:\n\n … content\ncontent"
 		// );
 
 		// Test only cats
@@ -64,8 +73,8 @@ describe( 'AIParagraphEdit', () => {
 		expect( createPrompt( '', [], 'cat1, cat2', 'tag1' ) ).toBe(
 			"Please help me write a short piece of a blog post, published in categories 'cat1, cat2' and tagged 'tag1'. Please only output generated content ready for publishing."
 		);
-		expect( createPrompt( 'title', [], 'cat1, cat2', 'tag1' ) ).toBe(
-			"Please help me write a short piece of a blog post titled 'title', published in categories 'cat1, cat2' and tagged 'tag1'. Please only output generated content ready for publishing."
+		expect( createPrompt( 'The story of my life', [], 'cat1, cat2', 'tag1' ) ).toBe(
+			"Please help me write a short piece of a blog post titled 'The story of my life', published in categories 'cat1, cat2' and tagged 'tag1'. Please only output generated content ready for publishing."
 		);
 		*/
 	} );
