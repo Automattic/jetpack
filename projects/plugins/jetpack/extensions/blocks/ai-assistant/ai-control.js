9eee1de993d675d13d055d58bcd13d47baaf8a49
oskosk <oskosk@gmail.com>
2023-05-10 08:38:12 -0300

    Change toolbar icon for dropdownmenu

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/ai-control.js
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/ai-control.js
@@ -7,7 +7,16 @@ import {
 	ToolbarGroup,
 } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { arrowRight, check, image, pencil, update, title, undo } from '@wordpress/icons';
+import {
+	arrowRight,
+	check,
+	chevronDown,
+	image,
+	pencil,
+	update,
+	title,
+	undo,
+} from '@wordpress/icons';
 import Loading from './loading';
 
 const AIControl = ( {
@@ -132,6 +141,7 @@ const ToolbarControls = ( {
 
 					{ ! showRetry && ! contentIsLoaded && (
 						<ToolbarDropdownMenu
+							icon={ chevronDown }
 							label="More"
 							controls={ [
 								{
