f8df749d8268f78ff4e97f6508dda2dedb6ffc35
spsiddarthan <siddarthan@outlook.com>
2023-05-11 22:14:47 +0530

    [not verified] Revert "Fix prev connection look-up"
    This reverts commit 98a0949caa358b384927e646ebb13af86168d2a2.

--- projects/js-packages/publicize-components/src/store/effects.js
+++ projects/js-packages/publicize-components/src/store/effects.js
@@ -33,9 +33,7 @@ export async function refreshConnectionTestResults() {
 		 * in order to refresh or update current connections.
 		 */
 		for ( const freshConnection of freshConnections ) {
-			const prevConnection = prevConnections.find(
-				conn => conn.connection_id === freshConnection.connection_id
-			);
+			const prevConnection = prevConnections.find( conn => conn.id === freshConnection.id );
 			const { done, enabled, toggleable } = prevConnection ?? defaults;
 			const connection = {
 				display_name: freshConnection.display_name,
