fba17f4d9a921837e3706794eb8cb51a8b83d981
Damián Suárez <rdsuarez@gmail.com>
2023-05-11 16:13:38 -0300

    [not verified] fix handling changeTone prompt type

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/use-suggestions-from-openai.js
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/use-suggestions-from-openai.js
@@ -163,7 +163,7 @@ const useSuggestionsFromOpenAI = ( {
 		if ( ! options.retryRequest ) {
 			// If there is a content already, let's iterate over it.
 			switch ( type ) {
-				case 'titleSummary':
+				case 'changeTone':
 					prompt = buildPromptTemplate( {
 						content,
 						rules: [ `Please, do this with a ${ options.tone } tone` ],
