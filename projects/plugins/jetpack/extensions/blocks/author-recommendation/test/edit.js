1ca3a19f263be55c5bd9f835ef301e6d49564300
heavyweight <kpapazov@gmail.com>
2023-05-12 16:49:14 +0200

    [not verified] AuthorRecommendations: update tests

--- projects/plugins/jetpack/extensions/blocks/author-recommendation/test/edit.js
+++ projects/plugins/jetpack/extensions/blocks/author-recommendation/test/edit.js
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import apiFetchMock from '@wordpress/api-fetch';
 import { AuthorRecommendationEdit } from '../edit';
 
 jest.mock( '@wordpress/block-editor', () => ( {
@@ -6,6 +7,22 @@ jest.mock( '@wordpress/block-editor', () => ( {
 	useBlockProps: jest.fn(),
 } ) );
 
+jest.mock( '@wordpress/api-fetch' );
+
+const mockResponse = [
+	{
+		blog_id: '1',
+		URL: 'https:///test.com',
+		name: 'test1',
+		site_icon: 'https://en.wordpress.com/i/void.gif',
+	},
+	{
+		blog_id: '2',
+		URL: 'https:///test2.com',
+		name: 'test2',
+		site_icon: 'https://en.wordpress.com/i/void.gif',
+	},
+];
 describe( 'AuthorRecommendationEdit Edit', () => {
 	const defaultAttributes = {
 		recommendations: [],
@@ -20,11 +37,32 @@ describe( 'AuthorRecommendationEdit Edit', () => {
 		setAttributes.mockClear();
 	} );
 
-	test( 'Displays empty subscriptions', () => {
+	afterEach( () => {
+		jest.resetAllMocks();
+	} );
+
+	test( 'Displays empty subscriptions', async () => {
+		apiFetchMock.mockResolvedValueOnce( [] );
+
 		render( <AuthorRecommendationEdit { ...defaultProps } /> );
 
-		expect(
-			screen.getByText( 'No subscriptions to display', { exact: false } )
-		).toBeInTheDocument();
+		await waitFor( () => {
+			expect(
+				screen.getByText( 'No subscriptions to display', { exact: false } )
+			).toBeInTheDocument();
+		} );
+	} );
+
+	test( 'Displays users subscriptions', async () => {
+		apiFetchMock.mockResolvedValueOnce( mockResponse );
+
+		render( <AuthorRecommendationEdit { ...defaultProps } /> );
+
+		await waitFor( () => {
+			expect( screen.getAllByRole( 'img' ) ).toHaveLength( 2 );
+		} );
+
+		expect( screen.getByText( 'test1', { exact: false } ) ).toBeInTheDocument();
+		expect( screen.getByText( 'test2', { exact: false } ) ).toBeInTheDocument();
 	} );
 } );
