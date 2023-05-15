cafefdaa514a9c01d4a02729cdc4f0011c0f7659
CodeyGuyDylan <dylan.munson@automattic.com>
2023-05-15 15:35:38 -0600

    [not verified] Fix storybook component

--- projects/js-packages/components/components/boost-score-bar/stories/index.stories.tsx
+++ projects/js-packages/components/components/boost-score-bar/stories/index.stories.tsx
@@ -1,17 +1,17 @@
-import BoostScoreBar from '..';
-import type { ComponentMeta, ComponentStory } from '@storybook/react';
+import BoostScoreBar from '../index';
+import type { Meta } from '@storybook/react';
 
-export default {
+const meta: Meta< typeof BoostScoreBar > = {
 	title: 'JS Packages/Components/Boost Score Bar',
 	component: BoostScoreBar,
 	argTypes: {
-		score: { control: 'number', defaultValue: 80, min: 0, max: 100 },
-		isLoading: { control: 'boolean', defaultValue: false },
-		showPrevScores: { control: 'boolean', defaultValue: false },
-		active: { control: 'boolean', defaultValue: true },
-		prevScore: { control: 'number', defaultValue: 70, min: 0, max: 100 },
-		scoreBarType: { control: 'text', options: [ 'desktop', 'mobile' ], defaultValue: 'desktop' },
-		noBoostScoreTooltip: { control: 'text', defaultValue: false },
+		score: { control: 'range', min: 0, max: 100, defaultValue: 80 },
+		prevScore: { control: 'range', min: 0, max: 100 },
+		isLoading: { control: 'boolean' },
+		showPrevScores: { control: 'boolean' },
+		active: { control: 'boolean' },
+		scoreBarType: { control: 'radio', options: [ 'desktop', 'mobile' ] },
+		noBoostScoreTooltip: { control: 'text' },
 	},
 	decorators: [
 		Story => (
@@ -20,8 +20,20 @@ export default {
 			</div>
 		),
 	],
-} as ComponentMeta< typeof BoostScoreBar >;
+};
 
-const Template: ComponentStory< typeof BoostScoreBar > = args => <BoostScoreBar { ...args } />;
+const defaultValues = {
+	score: 80,
+	prevScore: 70,
+	isLoading: false,
+	showPrevScores: true,
+	active: true,
+	scoreBarType: 'desktop',
+	noBoostScoreTooltip: 'No boost score',
+};
 
+export default meta;
+
+const Template = args => <BoostScoreBar { ...args } />;
 export const _default = Template.bind( {} );
+_default.args = defaultValues;
