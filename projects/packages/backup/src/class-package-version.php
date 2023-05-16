c926308ab30b168ed996c0a137c67fd8a43b4b72
Rustam Shaikh <rustam.shaikh@automattic.com>
2023-05-16 15:51:29 +0530

    Package version fix

--- projects/packages/backup/src/class-package-version.php
+++ projects/packages/backup/src/class-package-version.php
@@ -12,7 +12,7 @@ namespace Automattic\Jetpack\Backup;
  */
 class Package_Version {
 
-	const PACKAGE_VERSION = '1.12.16';
+	const PACKAGE_VERSION = '1.12.17-alpha';
 
 	const PACKAGE_SLUG = 'backup';
 
