413fd1921c4040f0f98760b091f625dfaa4faad3
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2023-05-17 17:37:56 -0300

    Jetpack AI: Add ai.php module

---
+++ projects/plugins/jetpack/modules/ai.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Module Name: Jetpack AI
+ * Module Description: Allow AI assistant to help you write your posts for you or selecting images.
+ * First Introduced: 12.2
+ * Sort Order: 20
+ * Requires Connection: Yes
+ * Auto Activate: No
+ * Module Tags: AI, Recommended
+ * Feature: Writing
+ * Additional Search Queries: ai, openai
+ *
+ * @package automattic/jetpack
+ */
