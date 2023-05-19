d642f15f0bb4f7b1b7889093feaaefef40a42674
Sergey Mitroshin <sergeymitr@gmail.com>
2023-05-20 00:09:00 +0300

    Connection: improve Offline Mode handling (#30570)
    * Add Offline Mode flag into the Site Health debug info
    * Action Button: Add "Disabled" prop
    * Connect Screen: Offline Mode for Required Plan component

--- projects/packages/connection/src/class-package-version.php
+++ projects/packages/connection/src/class-package-version.php
@@ -12,7 +12,7 @@ namespace Automattic\Jetpack\Connection;
  */
 class Package_Version {
 
-	const PACKAGE_VERSION = '1.51.10';
+	const PACKAGE_VERSION = '1.52.0-alpha';
 
 	const PACKAGE_SLUG = 'connection';
 
