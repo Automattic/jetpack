68e81c0639846f2b6e73d6861b4c9a7ba0a630b1
Peter Petrov <peter.petrov89@gmail.com>
2023-05-22 16:28:26 +0300

    Clean up old file system references

--- projects/plugins/boost/app/lib/minify/Utils.php
+++ projects/plugins/boost/app/lib/minify/Utils.php
@@ -5,21 +5,15 @@ namespace Automattic\Jetpack_Boost\Lib\Minify;
 class Utils {
 
 	private $use_wp;
-	private $fs;
 
 	public function __construct( $use_wp = true ) {
 		$this->use_wp = $use_wp;
-
-		$this->init_fs();
 	}
 
 	public function init_fs() {
 		if ( ! $this->use_wp ) {
 			return;
 		}
-
-		// Todo: check if falsy?
-		$this->fs = jetpack_boost_init_filesystem();
 	}
 
 	public function json_encode( $value ) {
@@ -44,37 +38,5 @@ class Utils {
 		}
 
 		return parse_url( $url, $component );
-	}
-
-	public function mkdir( $path, $mode, $recursive ) {
-		if ( $this->use_wp ) {
-			return $this->fs->mkdir( $path, $mode, $recursive );
-		}
-
-		return mkdir( $path, $mode, $recursive );
-	}
-
-	public function is_writable( $path ) {
-		if ( $this->use_wp ) {
-			return $this->fs->is_writable( $path );
-		}
-
-		return is_writable( $path );
-	}
-
-	public function get_contents( $path ) {
-		if ( $this->use_wp ) {
-			return $this->fs->get_contents( $path );
-		}
-
-		return file_get_contents( $path );
-	}
-
-	public function put_contents( $path, $content ) {
-		if ( $this->use_wp ) {
-			return $this->fs->put_contents( $path, $content );
-		}
-
-		return file_put_contents( $path, $content );
 	}
 }
