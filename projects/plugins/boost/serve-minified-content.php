90a1a3d34294f572cf16a75c0cc5cc5f42511086
Peter Petrov <peter.petrov89@gmail.com>
2023-05-22 15:06:34 +0300

    Add missing cssmin library

--- projects/plugins/boost/serve-minified-content.php
+++ projects/plugins/boost/serve-minified-content.php
@@ -1,15 +1,14 @@
 <?php
 
-// how do we know if called directly?
 if ( ! defined( 'JETPACK_BOOST_CONCAT_USE_WP' ) ) {
 	define( 'JETPACK_BOOST_CONCAT_USE_WP', false );
 }
-// define( 'ABSPATH', '../../../' );
-// define( 'WP_CONTENT_DIR', '../../cache/page_optimize' );
-// define( 'WP_CONTENT_URL', 'http://jetpack-boost.test/wp-content/' );
-// define( 'WP_PLUGIN_URL', 'http://jetpack-boost.test/wp-content/plugins/boost/' );
-// define( 'WP_PLUGIN_DIR', __DIR__ );
 
+// Load CSSmin.
+require_once __DIR__ . '/vendor/tubalmartin/cssmin/src/Colors.php';
+require_once __DIR__ . '/vendor/tubalmartin/cssmin/src/Utils.php';
+require_once __DIR__ . '/vendor/tubalmartin/cssmin/src/Minifier.php';
+// Load minify library code.
 require_once __DIR__ . '/app/lib/minify/Utils.php';
 require_once __DIR__ . '/app/lib/minify/Config.php';
 require_once __DIR__ . '/app/lib/minify/Dependency_Path_Mapping.php';
@@ -19,6 +18,5 @@ require_once __DIR__ . '/app/lib/minify/functions-service.php';
 jetpack_boost_page_optimize_service_request();
 
 // Todo:
-// - move cssmin outside vendor, or load directly from vendor
 // replace all wp_filesystem calls with own
 // replace all wp_ calls with own
