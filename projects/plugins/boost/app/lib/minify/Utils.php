8364cc3bfeb7beec25d3ae7278a1fc82d1daec75
Peter Petrov <peter.petrov89@gmail.com>
2023-05-23 13:01:45 +0300

    Improve code readability

--- projects/plugins/boost/app/lib/minify/Utils.php
+++ projects/plugins/boost/app/lib/minify/Utils.php
@@ -4,18 +4,29 @@ namespace Automattic\Jetpack_Boost\Lib\Minify;
 
 class Utils {
 
+	/**
+	 * Indicates whether to use the WordPress functions.
+	 *
+	 * @var bool $use_wp Whether to use WordPress functions.
+	 */
 	private $use_wp;
 
+	/**
+	 * Utils constructor.
+	 *
+	 * @param bool $use_wp Whether to use WordPress functions. Default is true.
+	 */
 	public function __construct( $use_wp = true ) {
 		$this->use_wp = $use_wp;
 	}
 
-	public function init_fs() {
-		if ( ! $this->use_wp ) {
-			return;
-		}
-	}
-
+	/**
+	 * Encodes a value to JSON.
+	 *
+	 * @param mixed $value The value to encode.
+	 *
+	 * @return string The JSON-encoded string.
+	 */
 	public function json_encode( $value ) {
 		if ( $this->use_wp ) {
 			return wp_json_encode( $value );
@@ -24,6 +35,13 @@ class Utils {
 		return json_encode( $value );
 	}
 
+	/**
+	 * Removes slashes from a string or an array of strings.
+	 *
+	 * @param mixed $value The string or array of strings to remove slashes from.
+	 *
+	 * @return mixed The string or array of strings with slashes removed.
+	 */
 	public function unslash( $value ) {
 		if ( $this->use_wp ) {
 			return wp_unslash( $value );
@@ -32,6 +50,15 @@ class Utils {
 		return is_string( $value ) ? stripslashes( $value ) : $value;
 	}
 
+	/**
+	 * Parses a URL and returns its components.
+	 *
+	 * @param string $url       The URL to parse.
+	 * @param int    $component Optional. The specific component to retrieve.
+	 *                          Use one of the PHP_URL_* constants. Default is -1 (all components).
+	 *
+	 * @return mixed|array|string|null The parsed URL component(s), or the entire URL string if $component is -1.
+	 */
 	public function parse_url( $url, $component = -1 ) {
 		if ( $this->use_wp ) {
 			return wp_parse_url( $url, $component );
