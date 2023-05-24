06153365dc8b2b946dc17626cb504335ed55b305
Ian Ramos <5714212+IanRamosC@users.noreply.github.com>
2023-05-24 17:01:31 -0300

    [not verified] Disable PHPCS rule for WPDB

--- projects/packages/boost-speed-score/src/lib/class-transient.php
+++ projects/packages/boost-speed-score/src/lib/class-transient.php
@@ -90,6 +90,7 @@ class Transient {
 		 */
 		$prefix_search_pattern = $wpdb->esc_like( $option_prefix ) . '%';
 
+		//phpcs:disable WordPress.DB.DirectDatabaseQuery.DirectQuery,WordPress.DB.DirectDatabaseQuery.NoCaching
 		$option_names = $wpdb->get_col(
 			$wpdb->prepare(
 				"
@@ -100,6 +101,7 @@ class Transient {
 				$prefix_search_pattern
 			)
 		);
+		// phpcs:enable
 
 		// Go through each option individually to ensure caches are handled properly.
 		foreach ( $option_names as $option_name ) {
