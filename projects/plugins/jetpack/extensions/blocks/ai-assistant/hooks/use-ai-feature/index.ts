e4a4fd95aa51db7d00c069994b657482c4241f32
Damián Suárez <rdsuarez@gmail.com>
2023-05-25 08:18:57 -0300

    [not verified] expose requireUpgrade boolean

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/hooks/use-ai-feature/index.ts
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/hooks/use-ai-feature/index.ts
@@ -19,6 +19,7 @@ export default function useAIFeature() {
 		isOverLimit: false,
 		requestsCount: 0,
 		requestsLimit: NUM_FREE_REQUESTS_LIMIT,
+		requireUpgrade: false,
 	} );
 
 	useEffect( () => {
@@ -27,12 +28,15 @@ export default function useAIFeature() {
 				path: '/wpcom/v2/jetpack-ai/ai-assistant-feature',
 			} );
 
+			const requireUpgrade = ! response[ 'has-feature' ] && response[ 'is-over-limit' ];
+
 			try {
 				setData( {
 					hasFeature: !! response[ 'has-feature' ],
 					isOverLimit: !! response[ 'is-over-limit' ],
 					requestsCount: response[ 'requests-count' ],
 					requestsLimit: response[ 'requests-limit' ],
+					requireUpgrade,
 				} );
 			} catch ( error ) {
 				console.error( error ); // eslint-disable-line no-console
