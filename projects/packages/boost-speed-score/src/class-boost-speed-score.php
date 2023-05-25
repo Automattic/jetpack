3f731f23c14364b862e379b30aee28a1afdd1020
Ian Ramos <5714212+IanRamosC@users.noreply.github.com>
2023-05-25 14:25:12 -0300

    [not verified] Add Boost Speed Score package skeleton (#30950)
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>

---
+++ projects/packages/boost-speed-score/src/class-boost-speed-score.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Package description here
+ *
+ * @package automattic/jetpack-boost-speed-score
+ */
+
+namespace Automattic\Jetpack;
+
+/**
+ * Class description.
+ */
+class Boost_Speed_Score {
+
+	const PACKAGE_VERSION = '0.1.0-alpha';
+
+}
