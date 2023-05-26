9070a44dd395f1245e55f5c9d3d3aa5608a06ce0
Douglas <douglas.henri@automattic.com>
2023-05-26 16:05:17 -0300

    disable multi block panel if user is not connected

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/multiple-blocks-edition/generate-panel.tsx
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/multiple-blocks-edition/generate-panel.tsx
@@ -22,6 +22,7 @@ import { buildPromptTemplate } from '../../create-prompt';
 import { askQuestion } from '../../get-suggestion-with-stream';
 import { LANGUAGE_MAP } from '../../i18n-dropdown-control';
 import Icon from '../../icons/ai-assistant';
+import { isUserConnected } from '../../lib/connection';
 import { PROMPT_TONES_MAP } from '../../tone-dropdown-control';
 
 // Create a Tone Array of objects with `key` and `name` keys
@@ -48,6 +49,7 @@ export const langOptions = Object.keys( LANGUAGE_MAP ).map( key => {
  * @returns {React.ReactElement}       The component's elements.
  */
 export default function GenerateContentPanel( { blocksIds } ) {
+	const connected = isUserConnected();
 	const [ tone, setTone ] = useState( {
 		key: '',
 		name: '',
@@ -167,6 +169,10 @@ export default function GenerateContentPanel( { blocksIds } ) {
 		lastBlockIndex,
 		updateBlockAttributes,
 	] );
+
+	if ( ! connected ) {
+		return null;
+	}
 
 	return (
 		<PanelBody
