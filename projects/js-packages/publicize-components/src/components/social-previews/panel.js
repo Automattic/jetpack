c18d5cb3f91fc4926655c6eb1efac5f0ed8caa45
Manzoor Wani <manzoorwani.jk@gmail.com>
2023-05-26 20:27:43 +0530

    [not verified] Social Previews | Add Instagram preview (#30929)
    * Create `getInstagramDetails` selector
    
    * Create instagram preview component
    
    * Unleash the beast
    
    * Use the latest beta of social-previews
    
    * Make changelogger happy
    
    * Add supported check for instagram
    
    * Update social-previews to the latest beta
    
    * Use username instead of display_name
    
    * Update pnpm-lock.yaml
    
    * Update @automattic/social-previews to the latest beta

--- projects/js-packages/publicize-components/src/components/social-previews/panel.js
+++ projects/js-packages/publicize-components/src/components/social-previews/panel.js
@@ -6,29 +6,36 @@
 
 import { Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { AVAILABLE_SERVICES } from './constants';
+import { useAvailableSerivces } from './useAvailableServices';
 import './panel.scss';
 
-const SocialPreviewsPanel = ( { openModal } ) => (
-	<div className="jetpack-social-previews__panel">
-		<p>
-			{ __( 'Preview what this will look like on social networks and Google search.', 'jetpack' ) }
-		</p>
+const SocialPreviewsPanel = ( { openModal } ) => {
+	const availableServices = useAvailableSerivces();
 
-		<div className="jetpack-gutenberg-social-icons">
-			{ AVAILABLE_SERVICES.map( service => (
-				<service.icon key={ service.name } className="jetpack-social-previews__icon" />
-			) ) }
+	return (
+		<div className="jetpack-social-previews__panel">
+			<p>
+				{ __(
+					'Preview what this will look like on social networks and Google search.',
+					'jetpack'
+				) }
+			</p>
+
+			<div className="jetpack-gutenberg-social-icons">
+				{ availableServices.map( service => (
+					<service.icon key={ service.name } className="jetpack-social-previews__icon" />
+				) ) }
+			</div>
+
+			<Button
+				variant="secondary"
+				onClick={ openModal }
+				label={ __( 'Open Social Previews', 'jetpack' ) }
+			>
+				{ __( 'Preview', 'jetpack' ) }
+			</Button>
 		</div>
-
-		<Button
-			variant="secondary"
-			onClick={ openModal }
-			label={ __( 'Open Social Previews', 'jetpack' ) }
-		>
-			{ __( 'Preview', 'jetpack' ) }
-		</Button>
-	</div>
-);
+	);
+};
 
 export default SocialPreviewsPanel;
