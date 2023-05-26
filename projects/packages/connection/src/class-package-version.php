b60341969b30411066291da6f2815bd50a8911f9
Jeremy Herve <jeremy@jeremy.hu>
2023-05-26 17:26:54 +0200

    [not verified] Block Editor: add connected user's email address to editor context
    This data may prove useful to display the current user's email address in the editor to demo subscription-related features.
    
    Related:
    
    - D110864-code
    - p1685112173899369-slack-CBG1CP4EN

--- projects/packages/connection/src/class-package-version.php
+++ projects/packages/connection/src/class-package-version.php
@@ -12,7 +12,7 @@ namespace Automattic\Jetpack\Connection;
  */
 class Package_Version {
 
-	const PACKAGE_VERSION = '1.52.0';
+	const PACKAGE_VERSION = '1.52.1-alpha';
 
 	const PACKAGE_SLUG = 'connection';
 
