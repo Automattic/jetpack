966a86efd084c4aec7f6a7d83874f2c8cd13ba80
Miguel Lezama <miguel@automattic.com>
2023-05-28 12:19:18 -0300

    Use the endpoint

--- projects/plugins/jetpack/extensions/blocks/subscriptions/preview-email.js
+++ projects/plugins/jetpack/extensions/blocks/subscriptions/preview-email.js
@@ -1,11 +1,14 @@
+import apiFetch from '@wordpress/api-fetch';
 import { Button, Modal, TextControl } from '@wordpress/components';
+import { compose } from '@wordpress/compose';
 import { withSelect } from '@wordpress/data';
 import { PluginMoreMenuItem } from '@wordpress/edit-post';
 import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
-const PreviewEmail = ( { userEmail } ) => {
+const PreviewEmail = ( { postId } ) => {
 	const [ isModalOpen, setIsModalOpen ] = useState( false );
+	const [ emailSent, setEmailSent ] = useState( false );
 
 	return (
 		<>
@@ -17,27 +20,47 @@ const PreviewEmail = ( { userEmail } ) => {
 					title={ __( 'Send a test email', 'jetpack' ) }
 					onRequestClose={ () => setIsModalOpen( false ) }
 				>
-					<TextControl value={ userEmail } disabled={ true } />
-					<Button
-						isPrimary
-						onClick={ () => {
-							// Call the wpcom endpoint here.
-							setIsModalOpen( false );
-						} }
-					>
-						Call WPCOM Endpoint
-					</Button>
+					{ emailSent ? (
+						<p>{ __( 'Email sent successfully', 'jetpack' ) }</p>
+					) : (
+						<>
+							<TextControl
+								value={ window?.Jetpack_Editor_Initial_State?.tracksUserData?.email }
+								disabled={ true }
+							/>
+							<Button
+								isPrimary
+								onClick={ () => {
+									apiFetch( {
+										path: '/wpcom/v2/send-email-preview/',
+										method: 'POST',
+										data: {
+											id: postId,
+										},
+									} )
+										.then( () => {
+											// Handle response here
+											setEmailSent( true );
+										} )
+										.catch( () => {
+											// Handle error here
+										} );
+								} }
+							>
+								{ __( 'Send', 'jetpack' ) }
+							</Button>
+						</>
+					) }
 				</Modal>
 			) }
 		</>
 	);
 };
 
-export default withSelect( select => {
-	const { getCurrentUser } = select( 'core' );
-	const currentUser = getCurrentUser();
-
-	return {
-		userEmail: currentUser ? currentUser.email : '',
-	};
-} )( PreviewEmail );
+export default compose( [
+	withSelect( select => {
+		return {
+			postId: select( 'core/editor' ).getCurrentPostId(),
+		};
+	} ),
+] )( PreviewEmail );
