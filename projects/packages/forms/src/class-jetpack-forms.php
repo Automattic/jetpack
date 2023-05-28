8d52ce15e9da8ea0080237b031926a605e1a05ee
donncha <5656673+donnchawp@users.noreply.github.com>
2023-05-28 11:17:39 +0100

    Update versions

--- projects/packages/forms/src/class-jetpack-forms.php
+++ projects/packages/forms/src/class-jetpack-forms.php
@@ -15,7 +15,7 @@ use Automattic\Jetpack\Forms\Dashboard\Dashboard_View_Switch;
  */
 class Jetpack_Forms {
 
-	const PACKAGE_VERSION = '0.19.0';
+	const PACKAGE_VERSION = '0.19.1-alpha';
 
 	/**
 	 * Load the contact form module.
