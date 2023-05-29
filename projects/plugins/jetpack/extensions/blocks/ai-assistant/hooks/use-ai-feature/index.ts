6b11f26e0e10acf97e1764d80f8a20f9f61da05c
Damián Suárez <rdsuarez@gmail.com>
2023-05-29 11:10:39 -0300

    [not verified] setting initial state in the useAIFeature() hook

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/hooks/use-ai-feature/index.ts
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/hooks/use-ai-feature/index.ts
@@ -4,7 +4,7 @@
 import apiFetch from '@wordpress/api-fetch';
 import { useEffect, useState } from 'react';
 
-type SiteAIAssistantFeatureEndpointResponseProps = {
+export type SiteAIAssistantFeatureEndpointResponseProps = {
 	'has-feature': boolean;
 	'is-over-limit': boolean;
 	'requests-count': number;
@@ -12,35 +12,52 @@ type SiteAIAssistantFeatureEndpointResponseProps = {
 	'site-require-upgrade': boolean;
 };
 
+type AIFeatureProps = {
+	hasFeature: boolean;
+	isOverLimit: boolean;
+	requestsCount: number;
+	requestsLimit: number;
+	requireUpgrade: boolean;
+};
+
 const NUM_FREE_REQUESTS_LIMIT = 20;
 
-export default function useAIFeature() {
-	const [ data, setData ] = useState( {
-		hasFeature: true,
-		isOverLimit: false,
-		requestsCount: 0,
-		requestsLimit: NUM_FREE_REQUESTS_LIMIT,
-		requireUpgrade: false,
+export const AI_Assistant_Initial_State = {
+	hasFeature: window?.Jetpack_Editor_Initial_State?.[ 'ai-assistant' ]?.[ 'has-feature' ] || true,
+	isOverLimit:
+		window?.Jetpack_Editor_Initial_State?.[ 'ai-assistant' ]?.[ 'is-over-limit' ] || false,
+	requestsCount:
+		window?.Jetpack_Editor_Initial_State?.[ 'ai-assistant' ]?.[ 'requests-count' ] || 0,
+	requestsLimit:
+		window?.Jetpack_Editor_Initial_State?.[ 'ai-assistant' ]?.[ 'requests-limit' ] ||
+		NUM_FREE_REQUESTS_LIMIT,
+	requireUpgrade:
+		window?.Jetpack_Editor_Initial_State?.[ 'ai-assistant' ]?.[ 'site-require-upgrade' ] || false,
+};
+
+export async function getAIFeatures(): Promise< AIFeatureProps > {
+	const response: SiteAIAssistantFeatureEndpointResponseProps = await apiFetch( {
+		path: '/wpcom/v2/jetpack-ai/ai-assistant-feature',
 	} );
 
-	useEffect( () => {
-		( async () => {
-			const response: SiteAIAssistantFeatureEndpointResponseProps = await apiFetch( {
-				path: '/wpcom/v2/jetpack-ai/ai-assistant-feature',
-			} );
+	try {
+		return {
+			hasFeature: !! response[ 'has-feature' ],
+			isOverLimit: !! response[ 'is-over-limit' ],
+			requestsCount: response[ 'requests-count' ],
+			requestsLimit: response[ 'requests-limit' ],
+			requireUpgrade: !! response[ 'site-require-upgrade' ],
+		};
+	} catch ( error ) {
+		console.error( error ); // eslint-disable-line no-console
+	}
+}
 
-			try {
-				setData( {
-					hasFeature: !! response[ 'has-feature' ],
-					isOverLimit: !! response[ 'is-over-limit' ],
-					requestsCount: response[ 'requests-count' ],
-					requestsLimit: response[ 'requests-limit' ],
-					requireUpgrade: !! response[ 'site-require-upgrade' ],
-				} );
-			} catch ( error ) {
-				console.error( error ); // eslint-disable-line no-console
-			}
-		} )();
+export default function useAIFeature() {
+	const [ data, setData ] = useState< AIFeatureProps >( AI_Assistant_Initial_State );
+
+	useEffect( () => {
+		getAIFeatures().then( setData );
 	}, [] );
 
 	return data;
