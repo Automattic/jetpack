4f5c91f05d5076e83a0f3c39d28e29d0b0f2b5a7
cleacos <javacodia@gmail.com>
2023-05-31 17:07:32 +0100

    [not verified] Rename class-recipe-exception by class-workflow-exception

--- projects/plugins/crm/src/automation/class-recipe-exception.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Automattic\Jetpack\CRM\Automation;
-
-class Workflow_Exception extends \Exception {
-    const INVALID_RECIPE                = 10;
-	const RECIPE_REQUIRE_A_TRIGGER      = 11;
-	const RECIPE_REQUIRE_A_INITIAL_STEP = 12;
-}
