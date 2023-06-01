a59977810e954885ce23e30cf2e720fb274892e4
Peter Petrov <peter.petrov89@gmail.com>
2023-06-01 20:29:33 +0300

    Update Boost menu item to include a notification dot if C.CSS regen is needed

--- projects/plugins/boost/app/lib/critical-css/Critical_CSS_Invalidator.php
+++ projects/plugins/boost/app/lib/critical-css/Critical_CSS_Invalidator.php
@@ -6,6 +6,7 @@
  */
 namespace Automattic\Jetpack_Boost\Lib\Critical_CSS;
 
+use Automattic\Jetpack_Boost\Lib\Environment_Change_Detector;
 use Automattic\Jetpack_Boost\Modules\Optimizations\Cloud_CSS\Cloud_CSS_Followup;
 
 /**
@@ -21,6 +22,7 @@ class Critical_CSS_Invalidator {
 	public static function init() {
 		add_action( 'jetpack_boost_deactivate', array( __CLASS__, 'clear_data' ) );
 		add_action( 'handle_environment_change', array( __CLASS__, 'handle_environment_change' ) );
+		add_filter( 'jetpack_boost_total_problem_count', array( __CLASS__, 'update_boost_problem_count' ) );
 	}
 
 	/**
@@ -45,6 +47,15 @@ class Critical_CSS_Invalidator {
 
 			do_action( 'critical_css_invalidated' );
 		}
+	}
+
+	public static function update_boost_problem_count( $count ) {
+		$suggest_regenerate = jetpack_boost_ds_get( 'critical_css_suggest_regenerate' );
+		if ( in_array( $suggest_regenerate, Environment_Change_Detector::get_available_env_change_statuses() ) ) {
+			++$count;
+		}
+
+		return $count;
 	}
 
 }
