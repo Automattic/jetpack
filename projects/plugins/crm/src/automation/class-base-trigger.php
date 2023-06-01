db6b9d193e95fb55b94fdb25b93ca53407c40f49
cleacos <javacodia@gmail.com>
2023-06-01 17:18:22 +0100

    [not verified] Update Base_Trigger using static methods to get the info

--- projects/plugins/crm/src/automation/class-base-trigger.php
+++ projects/plugins/crm/src/automation/class-base-trigger.php
@@ -14,67 +14,70 @@ namespace Automattic\Jetpack\CRM\Automation;
  */
 abstract class Base_Trigger implements Trigger {
 
-	/** @var mixed|string The trigger slug name */
-	protected $slug = '';
-	/** @var string The trigger title */
-	protected $title = '';
-	/** @var string The trigger description */
-	protected $description = '';
-	/** @var string The trigger category */
-	protected $category = '';
 
+	/** @var Automation_Workflow The workflow to execute by this trigger */
+	protected $workflow = null;
+	
 	/**
-	 * Base_Trigger constructor.
+	 * Set the workflow to execute by this trigger
 	 *
-	 * @param array $trigger_data The trigger data.
+	 * @param Automation_Workflow $workflow The workflow to execute by this trigger
 	 */
-	public function __construct( array $trigger_data ) {
-		$this->name        = $trigger_data['name'];
-		$this->title       = $trigger_data['title'] ?? '';
-		$this->description = $trigger_data['description'] ?? '';
-		$this->category    = $trigger_data['category'] ?? '';
+	public function set_workflow( Automation_Workflow $workflow ) {
+		$this->workflow = $workflow;
 	}
 
 	/**
-	 * Get the trigger name
+	 * Execute the workflow
+	 *
+	 * @param array $data The data to pass to the workflow.
+	 * @throws Automation_Exception
+	 */
+	public function execute_workflow( array $data = null ) {
+		if ( $this->workflow ) {
+			$this->workflow->execute( $this, $data );
+		}
+	}
+
+	/**
+	 * Initialize the trigger to listen to the desired event
+	 */
+	public function init( Automation_Workflow $workflow ) {
+		$this->workflow = $workflow;
+		$this->listen_to_event();
+	}
+
+	/**
+	 * Get the trigger slug
 	 *
 	 * @return string
 	 */
-	public function get_slug(): string {
-		return $this->slug;
-	}
+	abstract public static function get_slug(): string;
 
 	/**
 	 * Get the trigger title
 	 *
 	 * @return string
 	 */
-	public function get_title(): string {
-		return $this->title;
-	}
+	abstract public static function get_title(): ?string;
 
 	/**
 	 * Get the trigger description
 	 *
 	 * @return string|null
 	 */
-	public function get_description(): ?string {
-		return $this->description;
-	}
+	abstract public static function get_description(): ?string;
 
 	/**
 	 * Get the trigger category
 	 *
 	 * @return string
 	 */
-	public function get_category(): string {
-		return $this->category;
-	}
+	abstract public static function get_category(): ?string;
 
 	/**
-	 * Initialize the trigger
-	 *
-	 * @param Automation_Workflow $workflow The workflow to execute.
+	 * Listen to the desired event. It will be called by init(), it should
+	 * call the execute_workflow method when the event happens.
 	 */
-	abstract public function init( Automation_Workflow $workflow );
+	abstract protected function listen_to_event();
 }
