377fea430843cf576f18a50239f4aa39e6b63cc3
Adnan Haque <adnan007.id@gmail.com>
2023-06-02 17:45:15 +0600

    Project init

---
+++ projects/packages/boost-core/src/class-boost-core.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Package description here
+ *
+ * @package automattic/jetpack-boost-core
+ */
+
+namespace Automattic\Jetpack;
+
+/**
+ * Class description.
+ */
+class Boost_Core {
+
+	const PACKAGE_VERSION = '1.0.0-alpha';
+
+}
