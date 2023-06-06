d93d9d6a8098400fdb68ad76d06c12ff28547207
bindlegirl <1242807+bindlegirl@users.noreply.github.com>
2023-06-06 18:32:40 +0200

    Redirecting users to dashboard if they click back button before approving coonection

--- projects/plugins/jetpack/_inc/client/main.jsx
+++ projects/plugins/jetpack/_inc/client/main.jsx
@@ -504,7 +504,6 @@ class Main extends React.Component {
 			case '/connect-user':
 			case '/connect-user-setup':
 			case '/woo-setup':
-			case '/setup':
 				pageComponent = (
 					<AtAGlance
 						siteRawUrl={ this.props.siteRawUrl }
@@ -512,6 +511,12 @@ class Main extends React.Component {
 						rewindStatus={ this.props.rewindStatus }
 					/>
 				);
+				break;
+			case '/setup':
+				if ( this.props.isSiteConnected ) {
+					this.props.history.replace( '/dashboard' );
+					pageComponent = this.getAtAGlance();
+				}
 				break;
 			case '/my-plan':
 				pageComponent = (
