d80efa378e80b86b4d3b67aee4014c5ff65dc1aa
Mark George <thingalon@gmail.com>
2023-06-06 15:42:56 +1000

    add gating by feature

--- projects/plugins/boost/app/modules/image-size-analysis/Image_Size_Analysis.php
+++ projects/plugins/boost/app/modules/image-size-analysis/Image_Size_Analysis.php
@@ -3,6 +3,7 @@
 namespace Automattic\Jetpack_Boost\Modules\Image_Size_Analysis;
 
 use Automattic\Jetpack_Boost\Contracts\Pluggable;
+use Automattic\Jetpack_Boost\Lib\Premium_Features;
 use Automattic\Jetpack_Boost\REST_API\Contracts\Has_Endpoints;
 use Automattic\Jetpack_Boost\REST_API\Endpoints\Image_Analysis_Start;
 
@@ -13,7 +14,11 @@ class Image_Size_Analysis implements Pluggable, Has_Endpoints {
 	}
 
 	public static function is_available() {
-		return defined( 'JETPACK_BOOST_IMAGE_SIZE_ANALYSIS' ) && true === JETPACK_BOOST_IMAGE_SIZE_ANALYSIS;
+		if ( ! defined( 'JETPACK_BOOST_IMAGE_SIZE_ANALYSIS' ) || ! JETPACK_BOOST_IMAGE_SIZE_ANALYSIS ) {
+			return false;
+		}
+
+		return Premium_Features::has_feature( Premium_Features::IMAGE_SIZE_ANALYSIS );
 	}
 
 	public static function get_slug() {
