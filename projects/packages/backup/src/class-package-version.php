f6eaf4fa624a6fd5c40f14667fe591c6b29c4a87
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2023-06-06 09:20:05 -0600

    Update CTA section on backup connection page (#31201)
    * [not verified] Add tests to boost-score-api package
    
    * [not verified] Update tests to not reassign values to global mockData
    
    * [not verified] Update logo to show VaultPress and Jetpack logo when showText is false
    
    * [not verified] Update content in connection module
    
    * [not verified] Update spacing and margins and positioning for rna marked modules
    
    * [not verified] Add color blobs
    
    * [not verified] changelog
    
    * [not verified] Remove bad merge
    
    * [not verified] Remove old code
    
    * [not verified] Remove space
    
    * [not verified] Add missing feature line
    
    * [not verified] Update package versions
    
    * [not verified] Update lockfiles
    
    * [not verified] changelog

--- projects/packages/backup/src/class-package-version.php
+++ projects/packages/backup/src/class-package-version.php
@@ -12,7 +12,7 @@ namespace Automattic\Jetpack\Backup;
  */
 class Package_Version {
 
-	const PACKAGE_VERSION = '1.13.1-alpha';
+	const PACKAGE_VERSION = '1.14.0-alpha';
 
 	const PACKAGE_SLUG = 'backup';
 
