aa73a1d28c6b2af1ac8c252b19470e6992ef4db6
Karen Attfield <karenlattfield@gmail.com>
2023-06-07 08:22:43 +0100

    [not verified] CRM: Adding Automation Contact Actions (#31109)
    * [not verified] Adding an initial contact action and associated tests
    
    * [not verified] Adding update contact actions and associated tests
    
    * [not verified] Adding delete contact actions and their associated tests
    
    * Adding function comments
    
    * [not verified] Add translations to Delete_Contact action
    
    ---------
    
    Co-authored-by: cleacos <javacodia@gmail.com>

---
+++ projects/plugins/crm/src/automation/commons/actions/contacts/class-update-contact.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Jetpack CRM Automation Update_Contact action.
+ *
+ * @package automattic/jetpack-crm
+ */
+
+namespace Automattic\Jetpack\CRM\Automation\Actions;
+
+use Automattic\Jetpack\CRM\Automation\Base_Action;
+
+/**
+ * Adds the Update_Contact class.
+ */
+class Update_Contact extends Base_Action {
+
+	/**
+	 * @var object The action data.
+	 */
+	protected $action_data;
+
+	/**
+	 * @var array Step data.
+	 */
+	protected $attributes;
+
+	/**
+	 * Update_Contact constructor.
+	 *
+	 * @param array $action_data An array of the action data.
+	 *
+	 */
+	public function __construct( $action_data ) {
+		Base_Action::__construct( $action_data );
+
+		$this->action_data = $action_data;
+		$this->attributes  = $action_data['attributes'];
+	}
+
+	/**
+	 * Get the slug name of the step
+	 *
+	 * @return string
+	 */
+	public static function get_slug(): string {
+		return 'jpcrm/update_contact';
+	}
+
+	/**
+	 * Get the title of the step
+	 *
+	 * @return string
+	 */
+	public static function get_title(): ?string {
+		return 'Update Contact Action';
+	}
+
+	/**
+	 * Get the description of the step
+	 *
+	 * @return string
+	 */
+	public static function get_description(): ?string {
+		return 'Action to update the contact';
+	}
+
+	/**
+	 * Get the type of the step
+	 *
+	 * @return string
+	 */
+	public static function get_type(): string {
+		return 'contacts';
+	}
+
+	/**
+	 * Get the category of the step
+	 *
+	 * @return string
+	 */
+	public static function get_category(): ?string {
+		return 'actions';
+	}
+
+	/**
+	 * Get the allowed triggers
+	 *
+	 * @return array
+	 */
+	public static function get_allowed_triggers(): ?array {
+		return array();
+	}
+
+	/**
+	 * Update the DAL with the new contact status.
+	 *
+	 * @param array $contact_data The contact data to be updated.
+	 */
+	public function execute( array $contact_data ) {
+		global $zbs;
+
+		$contact_data['data']['status'] = $this->attributes['new_status'];
+		$zbs->DAL->contacts->addUpdateContact( $contact_data ); // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+	}
+
+}
