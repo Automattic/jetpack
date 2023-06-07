fdbb6c6a835953a6287301118e6edd45bae30274
Mark Biek <mark.biek@automattic.com>
2023-06-07 13:24:58 -0400

    [not verified] Remove extra parameter

--- projects/plugins/jetpack/_inc/lib/core-api/class.jetpack-core-api-module-endpoints.php
+++ projects/plugins/jetpack/_inc/lib/core-api/class.jetpack-core-api-module-endpoints.php
@@ -196,7 +196,7 @@ class Jetpack_Core_API_Module_List_Endpoint {
 	 */
 	public function process( $request ) {
 		if ( 'GET' === $request->get_method() ) {
-			return $this->get_modules( $request );
+			return $this->get_modules();
 		} else {
 			return $this->activate_modules( $request );
 		}
