a557545c9460ebe4d5bfdf9ef2b4ea991747cc1f
Valter Lorran <valter.lorran@automattic.com>
2023-06-07 09:24:16 +0300

    Support task list filtering of visible task

--- projects/packages/jetpack-mu-wpcom/tests/php/features/launchpad/class-launchpad-task-lists-test.php
+++ projects/packages/jetpack-mu-wpcom/tests/php/features/launchpad/class-launchpad-task-lists-test.php
@@ -22,4 +22,49 @@ class Launchpad_Task_Lists_Test extends \WorDBless\BaseTestCase {
 		$this->assertIsArray( $result );
 		$this->assertEmpty( $result );
 	}
+
+	/**
+	 * Filter out tasks with the task list callback.
+	 *
+	 * @covers ::build
+	 */
+	public function test_task_list_is_visible_callback() {
+		wpcom_register_launchpad_task(
+			array(
+				'id'    => 'task_0',
+				'title' => 'task_0',
+			)
+		);
+
+		wpcom_register_launchpad_task(
+			array(
+				'id'    => 'task_1',
+				'title' => 'task_1',
+			)
+		);
+
+		wpcom_register_launchpad_task_list(
+			array(
+				'id'                     => 'test-task-list',
+				'title'                  => 'test-task-list',
+				'task_ids'               => array(
+					'task_0',
+					'task_1',
+				),
+				'visible_tasks_callback' => function ( $task_list, $task_ids ) {
+					return array_filter(
+						$task_ids,
+						function ( $task_id ) {
+							return 'task_1' === $task_id;
+						}
+					);
+				},
+			)
+		);
+
+		$result = Launchpad_Task_Lists::get_instance()->build( 'test-task-list' );
+
+		$this->assertCount( 1, $result );
+		$this->assertEquals( 'task_1', $result[0]['id'] );
+	}
 }
