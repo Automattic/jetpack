928ba9a2cff71a39f43e8fc6086ab57b7b81d7fe
Donncha O Caoimh <5656673+donnchawp@users.noreply.github.com>
2023-06-08 13:46:56 +0100

    Remove links in the response emails

--- projects/packages/forms/src/contact-form/templates/email-response.php
+++ projects/packages/forms/src/contact-form/templates/email-response.php
@@ -22,11 +22,9 @@ $template = '
 <!-- response -->
 <p>%2$s</p>
 
-<!-- link to responses page -->
-<p><a href="%3$s"> ' . esc_html__( 'View Responses', 'jetpack-forms' ) . '</a></p>
+<!-- %3$s -->
 
-<!-- link to edit form -->
-<p><a href="%4$s">' . esc_html__( 'Edit', 'jetpack-forms' ) . '</a></p>
+<!-- %4$s -->
 
 <!-- footer -->
 <p>%5$s</p>
