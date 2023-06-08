3b86fd0bc8b864a3ad3474185484bf4ea624dc17
Donncha Ó Caoimh <5656673+donnchawp@users.noreply.github.com>
2023-06-08 17:20:44 +0100

    Jetpack Forms: remove links from the response emails sent to site owners (#31270)
    * Remove links in the response emails
    
    * changelog
    
    * Simplify this as they're blank anyway.
    
    * Remove links from the email tests too
    
    * Update versions

--- projects/packages/forms/src/class-jetpack-forms.php
+++ projects/packages/forms/src/class-jetpack-forms.php
@@ -15,7 +15,7 @@ use Automattic\Jetpack\Forms\Dashboard\Dashboard_View_Switch;
  */
 class Jetpack_Forms {
 
-	const PACKAGE_VERSION = '0.19.3';
+	const PACKAGE_VERSION = '0.19.4-alpha';
 
 	/**
 	 * Load the contact form module.
