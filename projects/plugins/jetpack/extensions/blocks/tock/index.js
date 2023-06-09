eea2506d6840965a77306b2965f6721ccba247b6
Jeremy Herve <jeremy@jeremy.hu>
2023-06-09 16:43:29 +0200

    Add block description

--- projects/plugins/jetpack/extensions/blocks/tock/index.js
+++ projects/plugins/jetpack/extensions/blocks/tock/index.js
@@ -1,4 +1,4 @@
-import { __ } from '@wordpress/i18n';
+import { __, _x } from '@wordpress/i18n';
 import { getIconColor } from '../../shared/block-icons';
 import attributes from './attributes';
 import edit from './edit';
@@ -13,33 +13,23 @@ export const name = 'tock';
 export const title = __( 'Tock', 'jetpack' );
 export const settings = {
 	title,
-	description: __( 'Tock', 'jetpack' ),
+	description: __( 'Allow visitors to book a reservation with Tock', 'jetpack' ),
 	icon: {
 		src: icon,
 		foreground: getIconColor(),
 	},
 	category: 'earn',
-	keywords: [],
+	keywords: [
+		_x( 'booking', 'block search term', 'jetpack' ),
+		_x( 'reservation', 'block search term', 'jetpack' ),
+		_x( 'restaurant', 'block search term', 'jetpack' ),
+	],
 	supports: {
-		// Support for block's alignment (left, center, right, wide, full). When true, it adds block controls to change block’s alignment.
-		align: false /* if set to true, the 'align' option below can be used*/,
-		// Pick which alignment options to display.
-		/*align: [ 'left', 'right', 'full' ],*/
-		// Support for wide alignment, that requires additional support in themes.
-		alignWide: true,
-		// When true, a new field in the block sidebar allows to define an id for the block and a button to copy the direct link.
-		anchor: false,
-		// When true, a new field in the block sidebar allows to define a custom className for the block’s wrapper.
+		align: [ 'left', 'right', 'full' ],
 		customClassName: true,
-		// When false, Gutenberg won't add a class like .wp-block-your-block-name to the root element of your saved markup
 		className: true,
-		// Setting this to false suppress the ability to edit a block’s markup individually. We often set this to false in Jetpack blocks.
 		html: false,
-		// Passing false hides this block in Gutenberg's visual inserter.
-		/*inserter: true,*/
-		// When false, user will only be able to insert the block once per post.
 		multiple: true,
-		// When false, the block won't be available to be converted into a reusable block.
 		reusable: true,
 	},
 	edit,
