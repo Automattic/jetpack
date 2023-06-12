1dde535c85c6ecc83069286d514aefb38a5d524a
CodeyGuyDylan <dylan.munson@automattic.com>
2023-06-12 16:13:11 -0600

    [not verified] Add zE function type to global window type

--- projects/js-packages/components/components/zendesk-chat/types.ts
+++ projects/js-packages/components/components/zendesk-chat/types.ts
@@ -1,5 +1,12 @@
 import type { FC } from 'react';
 
+declare global {
+	interface Window {
+		// eslint-disable-next-line @typescript-eslint/ban-types
+		zE: Function;
+	}
+}
+
 interface ZendeskChatProps {
 	jwt_token: string;
 }
