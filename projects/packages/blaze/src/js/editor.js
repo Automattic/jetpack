72eb462ac4211ccf8d5df343e38b74f05951d473
Romario Raffington <romarioraff@gmail.com>
2023-06-14 12:25:15 -0500

    Remove margin from top from paragraphs in Panel

--- projects/packages/blaze/src/js/editor.js
+++ projects/packages/blaze/src/js/editor.js
@@ -84,7 +84,7 @@ const BlazePostPublishPanel = () => {
 	return (
 		<PluginPostPublishPanel { ...panelBodyProps }>
 			<PanelRow>
-				<p>
+				<p className="blaze-panel__paragraph">
 					{ __(
 						'Reach a larger audience boosting the content to the WordPress.com community of blogs and sites.',
 						'jetpack-blaze'
