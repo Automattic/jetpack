406ea75214445fbc2dde2c0b69baaaa6d3533224
Mark George <thingalon@gmail.com>
2023-06-15 13:29:10 +1000

    Update preload every 3 seconds when something is happening, every 30 seconds otherwise

--- projects/plugins/super-cache/js/preload-notification.js
+++ projects/plugins/super-cache/js/preload-notification.js
@@ -1,8 +1,11 @@
 jQuery( document ).ready( function () {
 	const { __, sprintf } = window.wp.i18n;
 
-	// Set how often to check.
-	const CHECK_INTERVAL = 3000;
+	// Set how often to check when a preload job is running
+	const ACTIVE_INTERVAL = 3000;
+
+	// Set how often to check when no preload job is running
+	const INACTIVE_INTERVAL = 30000;
 
 	// Get a reference to the log element and the previous log entry
 	const preloadInfoPanel = jQuery( '#wpsc-preload-status' );
@@ -13,12 +16,13 @@ jQuery( document ).ready( function () {
 	}
 
 	update_preload_status( window.wpsc_preload_ajax.preload_status );
-	run_preload_interval();
 
 	/**
-	 * Regularly check the preload status.
+	 * Schedule the next preload status update.
+	 *
+	 * @param {number} time - time in milliseconds to wait before updating.
 	 */
-	function run_preload_interval() {
+	function schedule_preload_update( time ) {
 		setTimeout( () => {
 			jQuery.post(
 				window.wpsc_preload_ajax.ajax_url,
@@ -31,10 +35,9 @@ jQuery( document ).ready( function () {
 					}
 
 					update_preload_status( json.data );
-					run_preload_interval();
 				}
 			);
-		}, CHECK_INTERVAL );
+		}, time );
 	}
 
 	/**
@@ -43,12 +46,14 @@ jQuery( document ).ready( function () {
 	 * @param {object} data - description of the preload status.
 	 */
 	function update_preload_status( data ) {
-		// Bail early if no data is available.
+		// Bail early if no data is available. But try again in a few seconds.
 		if ( ! data || ( ! data.running && ! data.next && ! data.previous ) ) {
+			schedule_preload_update( ACTIVE_INTERVAL );
 			return;
 		}
 
 		preloadInfoPanel.empty();
+		let nextPreloadTime = data.running ? ACTIVE_INTERVAL : INACTIVE_INTERVAL;
 
 		if ( data.running ) {
 			const panel = jQuery( '<div class="notice notice-warning">' );
@@ -72,6 +77,11 @@ jQuery( document ).ready( function () {
 				const seconds = diff % 60;
 				const minutes = Math.floor( diff / 60 ) % 60;
 				const hours = Math.floor( diff / 3600 ) % 24;
+
+				// If we're preloading within the next minute, start loading faster.
+				if ( minutes + hours === 0 ) {
+					nextPreloadTime = ACTIVE_INTERVAL;
+				}
 
 				const p = jQuery( '<p>' );
 				p.append(
@@ -101,5 +111,7 @@ jQuery( document ).ready( function () {
 
 			preloadInfoPanel.append( panel );
 		}
+
+		schedule_preload_update( nextPreloadTime );
 	}
 } );
