671fe1ee59da4b4d4152082612f47904b6605f00
thingalon <mark@thingalon.com>
2023-06-16 11:42:13 +1000

    [Boost] Connect image guide ui to back-end (#31234)
    * Detect 'error' or 'message' keys in Api Errors
    
    * wip
    
    * Fix imports
    
    * use speed-score api from package
    
    * add gating by feature
    
    * Allow schemas to automatically convert stdClass objects (fresh out of json_decode) to assoc array
    
    * Return reports properly
    
    * changelog
    
    * small ui fix
    
    * Add logic for polling for updates as the image guide runs
    
    * Make isa summary load lazily
    
    * Bugfixes
    
    * Hook the tabs up to real data
    
    * Attach hero header to real data
    
    * [not verified] Remove set action from summary endpoint
    
    * Load data from real API with some placeholders
    
    * Slot metadata into the right spot, move responsibility for status to wpcom side
    
    * silence warnings
    
    * Add proper page titles
    
    * Add edit urls
    
    * connect up potential savings
    
    * Add translations, incluge group in query params, clean out leftover status stuff
    
    * fix package versions
    
    * Remove redundant code
    
    ---------
    
    Co-authored-by: Mark George <thingalon@gmail.com>
    Co-authored-by: Adnan Haque <adnan007.id@gmail.com>
    Co-authored-by: Adnan Haque <3737780+haqadn@users.noreply.github.com>

--- projects/plugins/boost/app/modules/image-size-analysis/data-sync/Image_Size_Analysis_Entry.php
+++ projects/plugins/boost/app/modules/image-size-analysis/data-sync/Image_Size_Analysis_Entry.php
@@ -2,10 +2,11 @@
 
 namespace Automattic\Jetpack_Boost\Modules\Image_Size_Analysis\Data_Sync;
 
+use Automattic\Jetpack\Boost_Speed_Score\Lib\Boost_API;
 use Automattic\Jetpack\WP_JS_Data_Sync\Contracts\Entry_Can_Get;
 use Automattic\Jetpack\WP_JS_Data_Sync\Contracts\Entry_Can_Set;
 use Automattic\Jetpack\WP_JS_Data_Sync\Contracts\Lazy_Entry;
-use function Automattic\Jetpack_Boost\Modules\Image_Size_Analysis\jetpack_boost_mock_api;
+use Automattic\Jetpack_Boost\Lib\Critical_CSS\Source_Providers\Source_Providers;
 
 require_once dirname( __DIR__ ) . '/jetpack-boost-mock-api.php';
 
@@ -16,6 +17,27 @@ class Image_Size_Analysis_Entry implements Lazy_Entry, Entry_Can_Get, Entry_Can_
 	private $search_query = '';
 
 	public function get() {
+		$data = Boost_API::get(
+			'image-guide/reports/latest/issues',
+			array(
+				'page'     => $this->page,
+				'group'    => $this->group,
+				'per_page' => 20,
+			)
+		);
+
+		$issues = array();
+		foreach ( $data->issues as $issue ) {
+			$issues[] = array(
+				'id'           => $issue->id,
+				'thumbnail'    => $issue->url,
+				'device_type'  => $issue->device,
+				'status'       => $issue->status,
+				'instructions' => $this->get_instructions( $issue ),
+				'page'         => $this->get_page( $issue ),
+				'image'        => $this->get_image_info( $issue ),
+			);
+		}
 
 		$results = array(
 			'query' => array(
@@ -24,11 +46,9 @@ class Image_Size_Analysis_Entry implements Lazy_Entry, Entry_Can_Get, Entry_Can_
 				'search' => $this->search_query,
 			),
 			'data'  => array(
-				'last_updated' => 1682419855474,
-				// This is fine 🔥- while in development only
-				// phpcs:ignore
-				'total_pages'  => random_int( 1, 15 ),
-				'images'       => jetpack_boost_mock_api( 10, $this->group, $this->page ),
+				'last_updated' => 1682419855474, // @todo: Update
+				'total_pages'  => $data->pagination->total_pages,
+				'images'       => $issues,
 			),
 		);
 
@@ -39,10 +59,81 @@ class Image_Size_Analysis_Entry implements Lazy_Entry, Entry_Can_Get, Entry_Can_
 		return $results;
 	}
 
+	/**
+	 * Get the page info for a given key
+	 *
+	 * @todo: Implement
+	 */
+	private function get_page( $issue ) {
+		$key      = $issue->page_provider;
+		$provider = $this->get_provider( $key );
+		$title    = empty( $provider ) ? $key : $provider::describe_key( $key );
+		$edit_url = empty( $provider ) ? null : $provider::get_edit_url( $key );
+
+		if ( empty( $title ) ) {
+			$title = $issue->page_provider;
+		}
+
+		return array(
+			'id'       => $issue->page_id,
+			'url'      => $issue->page_url,
+			'edit_url' => $edit_url,
+			'title'    => $title,
+		);
+	}
+
+	/**
+	 * Generate instructions for an issue.
+	 */
+	private function get_instructions( $_issue ) {
+		return __( 'Resize the image to the expected dimensions and compress it.', 'jetpack-boost' );
+	}
+
+	/**
+	 * Get the image info for a given issue
+	 *
+	 * @todo: Implement
+	 */
+	private function get_image_info( $issue ) {
+		return array(
+			'url'        => $issue->url,
+			'dimensions' => array(
+				'file'           => array(
+					'width'  => $issue->meta->fileSize_width,
+					'height' => $issue->meta->fileSize_height,
+				),
+				'expected'       => array(
+					'width'  => $issue->meta->expectedSize_width,
+					'height' => $issue->meta->expectedSize_height,
+				),
+				'size_on_screen' => array(
+					'width'  => $issue->meta->sizeOnPage_width,
+					'height' => $issue->meta->sizeOnPage_height,
+				),
+			),
+			'weight'     => array(
+				'current'   => $issue->meta->fileSize_weight,
+				'potential' => (int) $issue->meta->potentialSavings,
+			),
+		);
+	}
+
 	public function set( $value ) {
 		$this->page         = $value['query']['page'];
 		$this->group        = $value['query']['group'];
 		$this->search_query = $value['query']['search'];
+	}
+
+	/**
+	 * Get a provider for a given key.
+	 */
+	private function get_provider( $key ) {
+		static $providers = null;
+		if ( null === $providers ) {
+			$providers = new Source_Providers();
+		}
+
+		return $providers->get_provider_for_key( $key );
 	}
 }
 
