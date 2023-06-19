7dff7668285e08794ab8ac4ce13e67244588baf7
Damián Suárez <rdsuarez@gmail.com>
2023-06-19 14:41:34 -0300

    [not verified] allow transform only when possible to extend

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/transforms/index.tsx
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/transforms/index.tsx
@@ -6,11 +6,12 @@ import { createBlock } from '@wordpress/blocks';
  * Internal dependencies
  */
 import { blockName } from '..';
-import { EXTENDED_BLOCKS } from '../extensions/ai-assistant';
+import { EXTENDED_BLOCKS, isPossibleToExtendBlock } from '../extensions/ai-assistant';
 
 const transformFromCoreEmbed = {
 	type: 'block',
 	blocks: EXTENDED_BLOCKS,
+	isMatch: () => isPossibleToExtendBlock(),
 	transform: ( { content } ) => {
 		return createBlock( blockName, { content } );
 	},
