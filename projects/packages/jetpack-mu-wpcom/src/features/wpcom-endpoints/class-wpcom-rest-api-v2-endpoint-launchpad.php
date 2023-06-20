1cf2813726b53db44850a195404a5b4aa2e8a800
sixhours <calobee@gmail.com>
2023-06-20 13:07:23 -0400

    Update endpoint logic to avoid changing the output

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-launchpad.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-launchpad.php
@@ -141,13 +141,15 @@ class WPCOM_REST_API_V2_Endpoint_Launchpad extends WP_REST_Controller {
 				case 'checklist_statuses':
 					$launchpad_checklist_tasks_statuses_option = (array) get_option( 'launchpad_checklist_tasks_statuses', array() );
 					$launchpad_checklist_tasks_statuses_option = array_merge( $launchpad_checklist_tasks_statuses_option, $value );
-					if ( update_option( 'launchpad_checklist_tasks_statuses', $launchpad_checklist_tasks_statuses_option ) ) {
-						foreach ( $value as $key => $val ) {
-							if ( $val ) {
-								// If we're marking a task as complete, the value should be truthy, so fire the Tracks event.
-								wpcom_launchpad_track_completed_task( $key );
-							}
+
+					foreach ( $value as $task => $task_value ) {
+						if ( $task_value ) {
+							// If we're marking a task as complete, the value should be truthy, so fire the Tracks event.
+							wpcom_launchpad_track_completed_task( $task );
 						}
+					}
+
+					if ( update_option( 'launchpad_checklist_tasks_statuses', $launchpad_checklist_tasks_statuses_option ) ) {
 						$updated[ $key ] = $value;
 					}
 					// This will check if we have completed all the tasks and disable Launchpad if so.
