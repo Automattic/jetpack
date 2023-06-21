266d8bf9ba9330db0cbed9ab161f23ad952591dd
Jeremy Herve <jeremy@jeremy.hu>
2023-06-21 11:48:00 +0200

    [not verified] Blaze: introduce Blaze module
    fixes #29294
    
    Instead of loading Blaze automatically and only providing a code snippet to disable it, let's make it a module (automatically enabled upon connection to WordPress.com) that can be disabled via the modules page.

---
+++ projects/plugins/jetpack/modules/blaze.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Module Name: Blaze
+ * Module Description: Attract high-quality traffic to your site using Blaze.
+ * Sort Order: 22
+ * Recommendation Order: 12
+ * First Introduced: 12.3
+ * Requires Connection: Yes
+ * Auto Activate: Yes
+ * Module Tags: Traffic
+ * Additional Search Queries: advertising, ads
+ *
+ * @package automattic/jetpack
+ */
+
+use Automattic\Jetpack\Blaze;
+
+Blaze::init();
