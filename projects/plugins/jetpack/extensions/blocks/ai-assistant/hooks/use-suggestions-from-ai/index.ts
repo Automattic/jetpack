568e1dd20dc67f72e61fd403afbbaa70de8184a9
Luiz Kowalski <lhkowalski@gmail.com>
2023-06-21 18:20:57 -0300

    Add support for handling EventSource errors and dispatching it if possible

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/hooks/use-suggestions-from-ai/index.ts
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/hooks/use-suggestions-from-ai/index.ts
@@ -4,6 +4,7 @@
 import { useSelect } from '@wordpress/data';
 import { store as editorStore } from '@wordpress/editor';
 import { useCallback, useEffect, useRef } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
 import debugFactory from 'debug';
 /**
  * Types
@@ -12,6 +13,23 @@ import { SuggestionsEventSource, askQuestion } from '../../lib/suggestions';
 import type { PromptItemProps } from '../../lib/prompt';
 
 const debug = debugFactory( 'jetpack-ai-assistant:prompt' );
+
+export type SuggestionError = {
+	/*
+	 * A string code to refer to the error.
+	 */
+	code: string;
+
+	/*
+	 * The user-friendly error message.
+	 */
+	message: string;
+
+	/*
+	 * The type of the error.
+	 */
+	status: 'info' | 'error';
+};
 
 type UseSuggestionsFromAIOptions = {
 	/*
@@ -33,6 +51,11 @@ type UseSuggestionsFromAIOptions = {
 	 * onDone callback.
 	 */
 	onDone?: ( content: string ) => void;
+
+	/*
+	 * onError callback.
+	 */
+	onError?: ( error: SuggestionError ) => void;
 };
 
 type useSuggestionsFromAIProps = {
@@ -69,6 +92,7 @@ export default function useSuggestionsFromAI( {
 	autoRequest = true,
 	onSuggestion,
 	onDone,
+	onError,
 }: UseSuggestionsFromAIOptions ): useSuggestionsFromAIProps {
 	// Collect data
 	const { postId, postTitle } = useSelect( select => {
@@ -125,12 +149,68 @@ export default function useSuggestionsFromAI( {
 				if ( onDone ) {
 					source?.current?.addEventListener( 'done', handleDone );
 				}
+
+				if ( onError ) {
+					source?.current?.addEventListener( 'error_quota_exceeded', () => {
+						source?.current?.close();
+						onError( {
+							code: 'error_quota_exceeded',
+							message: __( 'You have reached the limit of requests for this site.', 'jetpack' ),
+							status: 'info',
+						} );
+					} );
+
+					source?.current?.addEventListener( 'error_unclear_prompt', () => {
+						source?.current?.close();
+						onError( {
+							code: 'error_unclear_prompt',
+							message: __( 'Your request was unclear. Mind trying again?', 'jetpack' ),
+							status: 'info',
+						} );
+					} );
+
+					source?.current?.addEventListener( 'error_service_unavailable', () => {
+						source?.current?.close();
+						onError( {
+							code: 'error_service_unavailable',
+							message: __(
+								'Jetpack AI services are currently unavailable. Sorry for the inconvenience.',
+								'jetpack'
+							),
+							status: 'info',
+						} );
+					} );
+
+					source?.current?.addEventListener( 'error_moderation', () => {
+						source?.current?.close();
+						onError( {
+							code: 'error_moderation',
+							message: __(
+								'This request has been flagged by our moderation system. Please try to rephrase it and try again.',
+								'jetpack'
+							),
+							status: 'info',
+						} );
+					} );
+
+					source?.current?.addEventListener( 'error_network', () => {
+						source?.current?.close();
+						onError( {
+							code: 'error_network',
+							message: __(
+								'It was not possible to process your request. Mind trying again?',
+								'jetpack'
+							),
+							status: 'info',
+						} );
+					} );
+				}
 			} catch ( e ) {
 				// eslint-disable-next-line no-console
 				console.error( e );
 			}
 		},
-		[ postId, onSuggestion, onDone, handleSuggestion, handleDone ]
+		[ postId, onSuggestion, onDone, onError, handleSuggestion, handleDone ]
 	);
 
 	// Request suggestions automatically when ready.
