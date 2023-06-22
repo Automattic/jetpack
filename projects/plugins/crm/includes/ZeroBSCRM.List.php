Automattic/jetpack

commit 7c2bf46e5b3eb64e36ce1455d1f070532ba57b60

CRM: Resolves 3129 - catch PHP errors (#31032)
* [not verified] Catch PHP errors if tags or quickfilters are not arrays

* [not verified] Only check slug if it exists

* [not verified] Add changelog

projects/plugins/crm/includes/ZeroBSCRM.List.php
@@ -782,8 +782,8 @@ class zeroBSCRM_list{
 	public function draw_listview_header( $listview_filters ) {
 		global $zbs;
 
-		$filter_var       = 'zeroBSCRM_filterbuttons_' . $this->objType; // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
-		$all_quickfilters = $GLOBALS[ $filter_var ]['all'];
+		$filter_var       = 'zeroBSCRM_filterbuttons_a' . $this->objType; // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+		$all_quickfilters = ( empty( $GLOBALS[ $filter_var ]['all'] ) ? array() : $GLOBALS[ $filter_var ]['all'] );
 		$all_tags         = $zbs->DAL->getTagsForObjType( // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
 			array(
 				'objtypeid'    => $this->objTypeID, // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
@@ -822,7 +822,7 @@ class zeroBSCRM_list{
 				}
 
 				// add tags filter if current object has tags
-				if ( count( $all_tags ) > 0 ) {
+				if ( is_array( $all_tags ) && count( $all_tags ) > 0 ) {
 					echo '<select class="filter-dropdown' . ( ! empty( $current_tag ) ? ' hidden' : '' ) . '" data-filtertype="tags">';
 					echo '<option disabled selected>' . esc_html__( 'Select tag', 'zero-bs-crm' ) . '</option>';
 					foreach ( $all_tags as $tag ) {

projects/plugins/crm/includes/ZeroBSCRM.AdminPages.php
@@ -2167,8 +2167,7 @@ function zeroBSCRM_html_extensions() {
 
 			// We want to prioritize the top 5 Woo modules in the list if 'woosync' is active, but otherwise alphabetize everything.
 			foreach ( $extensions->paid as $extension ) {
-
-				if ( $has_woosync && in_array( $extension->slug, $top_woo_extension_slugs, true ) ) {
+				if ( $has_woosync && ! empty( $extension->slug ) && in_array( $extension->slug, $top_woo_extension_slugs, true ) ) {
 					$top_woo_extensions[] = $extension;
 					continue;
 				}

commit f8b4d70007191c2989ed10538cf72aaca9b9c7a2

Fix bug introduced in #31032 (probably a typo)

projects/plugins/crm/includes/ZeroBSCRM.List.php
@@ -766,7 +766,7 @@ class zeroBSCRM_list{
 	public function draw_listview_header( $listview_filters ) {
 		global $zbs;
 
-		$filter_var       = 'zeroBSCRM_filterbuttons_a' . $this->objType; // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+		$filter_var       = 'zeroBSCRM_filterbuttons_' . $this->objType; // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
 		$all_quickfilters = ( empty( $GLOBALS[ $filter_var ]['all'] ) ? array() : $GLOBALS[ $filter_var ]['all'] );
 		$all_tags         = $zbs->DAL->getTagsForObjType( // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
 			array(