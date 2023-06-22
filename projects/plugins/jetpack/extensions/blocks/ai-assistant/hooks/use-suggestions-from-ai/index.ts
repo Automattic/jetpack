50b6f9485bafea37d929d0b246a3b3a4a9b1d20b
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2023-06-22 16:34:11 -0300

    AI Assistant: Support multiple messages request

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/hooks/use-suggestions-from-ai/index.ts
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/hooks/use-suggestions-from-ai/index.ts
@@ -15,7 +15,16 @@ const debug = debugFactory( 'jetpack-ai-assistant:prompt' );
 
 type SourceCallback = ( event: CustomEvent ) => void;
 
-type ExtraArgs = object | undefined;
+type PromptMessages = Array< PromptItemProps >;
+
+type PromptArg = PromptMessages[] | PromptMessages;
+
+type ExtraArgs =
+	| {
+			[ key: string ]: object | string | number | boolean;
+			index?: number;
+	  }
+	| undefined;
 
 type UseSuggestionsFromAIOptions = {
 	/*
@@ -58,7 +67,7 @@ type useSuggestionsFromAIProps = {
 	/*
 	 * The request handler.
 	 */
-	request: ( prompt: Array< PromptItemProps >, extraArgs?: ExtraArgs ) => Promise< void >;
+	request: ( prompt: PromptArg, extraArgs?: ExtraArgs ) => Promise< void >;
 };
 
 /**
@@ -95,29 +104,43 @@ export default function useSuggestionsFromAI( {
 	 * @returns {Promise<void>} The promise.
 	 */
 	const request = useCallback(
-		async ( promptArg: Array< PromptItemProps >, extraArgs?: ExtraArgs ) => {
-			promptArg.forEach( ( { role, content: promptContent }, i ) =>
-				debug( '(%s/%s) %o\n%s', i + 1, promptArg.length, `[${ role }]`, promptContent )
-			);
+		async ( promptArg: PromptArg, extraArgs?: ExtraArgs ) => {
+			const currentIndex = extraArgs?.index ?? 0;
+			const isMultiple = Array.isArray( promptArg[ 0 ] );
+			const hasNext = isMultiple && promptArg?.[ currentIndex + 1 ] !== undefined;
+			const nextExtraArgs = { ...( extraArgs || {} ), index: currentIndex + 1 };
+			const currentPrompt = (
+				isMultiple ? promptArg[ currentIndex ] : promptArg
+			) as PromptMessages;
+
+			const log = ( messages: PromptMessages ) => {
+				messages.forEach( ( { role, content: promptContent }, i ) =>
+					debug( '(%s/%s) %o\n%s', i + 1, promptArg.length, `[${ role }]`, promptContent )
+				);
+			};
 
 			try {
-				source.current = await askQuestion( promptArg, {
+				log( currentPrompt );
+
+				source.current = await askQuestion( currentPrompt, {
 					postId,
 					requireUpgrade: false, // It shouldn't be part of the askQuestion API.
 					fromCache: false,
 				} );
 
-				if ( onSuggestion ) {
-					handleSuggestion.current = ( event: CustomEvent ) =>
-						onSuggestion( event?.detail, extraArgs );
+				handleSuggestion.current = ( event: CustomEvent ) =>
+					onSuggestion?.( event?.detail, extraArgs );
 
-					source?.current?.addEventListener( 'suggestion', handleSuggestion.current );
-				}
+				source?.current?.addEventListener( 'suggestion', handleSuggestion.current );
 
-				if ( onDone ) {
-					handleDone.current = ( event: CustomEvent ) => onDone( event?.detail, extraArgs );
-					source?.current?.addEventListener( 'done', handleDone.current );
-				}
+				handleDone.current = ( event: CustomEvent ) => {
+					onDone?.( event?.detail, extraArgs );
+					if ( hasNext ) {
+						request( promptArg, nextExtraArgs );
+					}
+				};
+
+				source?.current?.addEventListener( 'done', handleDone.current );
 			} catch ( e ) {
 				// eslint-disable-next-line no-console
 				console.error( e );
