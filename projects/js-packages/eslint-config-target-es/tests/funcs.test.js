8dfdc50a4799cccdef542545d90c91c4251f188d
Brad Jorsch <anomiex@users.noreply.github.com>
2023-06-23 16:39:46 -0400

    Replace eslint-plugin-es with eslint-plugin-es-x (#31556)
    It appears that eslint-plugin-es has been abandoned. A fork at
    eslint-plugin-es-x is currently maintained, and has a bunch of updates.
    Let's use it.

--- projects/js-packages/eslint-config-target-es/tests/funcs.test.js
+++ projects/js-packages/eslint-config-target-es/tests/funcs.test.js
@@ -17,60 +17,60 @@ describe( 'getRules', () => {
 		const rules = funcs.getRules( { query: 'ie 11' } );
 		expect( rules ).toMatchObject( {
 			// A small selection of rules to test against.
-			'es/no-array-from': 2,
-			'es/no-block-scoped-variables': 2,
-			'es/no-nullish-coalescing-operators': 2,
-			'es/no-promise-any': 2,
-			'es/no-regexp-unicode-property-escapes': 2,
-			'es/no-regexp-unicode-property-escapes-2019': 0,
-			'es/no-set': 0,
-			'es/no-symbol': 2,
-			'es/no-weak-map': 0,
+			'es-x/no-array-from': 2,
+			'es-x/no-block-scoped-variables': 2,
+			'es-x/no-nullish-coalescing-operators': 2,
+			'es-x/no-promise-any': 2,
+			'es-x/no-regexp-unicode-property-escapes': 2,
+			'es-x/no-regexp-unicode-property-escapes-2019': 0,
+			'es-x/no-set': 0,
+			'es-x/no-symbol': 2,
+			'es-x/no-weak-map': 0,
 		} );
 	} );
 
 	test( 'With FF 76', () => {
 		const rules = funcs.getRules( { query: 'ff 76' } );
 		expect( rules ).toMatchObject( {
-			'es/no-array-from': 0,
-			'es/no-block-scoped-variables': 0,
-			'es/no-nullish-coalescing-operators': 0,
-			'es/no-promise-any': 2,
-			'es/no-regexp-unicode-property-escapes': 2,
-			'es/no-regexp-unicode-property-escapes-2019': 0,
-			'es/no-set': 0,
-			'es/no-symbol': 0,
-			'es/no-weak-map': 0,
+			'es-x/no-array-from': 0,
+			'es-x/no-block-scoped-variables': 0,
+			'es-x/no-nullish-coalescing-operators': 0,
+			'es-x/no-promise-any': 2,
+			'es-x/no-regexp-unicode-property-escapes': 2,
+			'es-x/no-regexp-unicode-property-escapes-2019': 0,
+			'es-x/no-set': 0,
+			'es-x/no-symbol': 0,
+			'es-x/no-weak-map': 0,
 		} );
 	} );
 
 	test( 'With Chrome 79', () => {
 		const rules = funcs.getRules( { query: 'chrome 79' } );
 		expect( rules ).toMatchObject( {
-			'es/no-array-from': 0,
-			'es/no-block-scoped-variables': 0,
-			'es/no-nullish-coalescing-operators': 2,
-			'es/no-promise-any': 2,
-			'es/no-regexp-unicode-property-escapes': 0,
-			'es/no-regexp-unicode-property-escapes-2019': 0,
-			'es/no-set': 0,
-			'es/no-symbol': 0,
-			'es/no-weak-map': 0,
+			'es-x/no-array-from': 0,
+			'es-x/no-block-scoped-variables': 0,
+			'es-x/no-nullish-coalescing-operators': 2,
+			'es-x/no-promise-any': 2,
+			'es-x/no-regexp-unicode-property-escapes': 0,
+			'es-x/no-regexp-unicode-property-escapes-2019': 0,
+			'es-x/no-set': 0,
+			'es-x/no-symbol': 0,
+			'es-x/no-weak-map': 0,
 		} );
 	} );
 
 	test( 'With FF 76 and Chrome 79', () => {
 		const rules = funcs.getRules( { query: 'ff 76, chrome 79' } );
 		expect( rules ).toMatchObject( {
-			'es/no-array-from': 0,
-			'es/no-block-scoped-variables': 0,
-			'es/no-nullish-coalescing-operators': 2,
-			'es/no-promise-any': 2,
-			'es/no-regexp-unicode-property-escapes': 2,
-			'es/no-regexp-unicode-property-escapes-2019': 0,
-			'es/no-set': 0,
-			'es/no-symbol': 0,
-			'es/no-weak-map': 0,
+			'es-x/no-array-from': 0,
+			'es-x/no-block-scoped-variables': 0,
+			'es-x/no-nullish-coalescing-operators': 2,
+			'es-x/no-promise-any': 2,
+			'es-x/no-regexp-unicode-property-escapes': 2,
+			'es-x/no-regexp-unicode-property-escapes-2019': 0,
+			'es-x/no-set': 0,
+			'es-x/no-symbol': 0,
+			'es-x/no-weak-map': 0,
 		} );
 	} );
 
@@ -78,15 +78,15 @@ describe( 'getRules', () => {
 		const rules = funcs.getRules( { query: 'ie 11', builtins: true } );
 		expect( rules ).toMatchObject( {
 			// A small selection of rules to test against.
-			'es/no-array-from': 2,
-			'es/no-block-scoped-variables': 0,
-			'es/no-nullish-coalescing-operators': 0,
-			'es/no-promise-any': 2,
-			'es/no-regexp-unicode-property-escapes': 0,
-			'es/no-regexp-unicode-property-escapes-2019': 0,
-			'es/no-set': 0,
-			'es/no-symbol': 2,
-			'es/no-weak-map': 0,
+			'es-x/no-array-from': 2,
+			'es-x/no-block-scoped-variables': 0,
+			'es-x/no-nullish-coalescing-operators': 0,
+			'es-x/no-promise-any': 2,
+			'es-x/no-regexp-unicode-property-escapes': 0,
+			'es-x/no-regexp-unicode-property-escapes-2019': 0,
+			'es-x/no-set': 0,
+			'es-x/no-symbol': 2,
+			'es-x/no-weak-map': 0,
 		} );
 	} );
 
@@ -94,15 +94,15 @@ describe( 'getRules', () => {
 		const rules = funcs.getRules( { query: 'ie 11', builtins: false } );
 		expect( rules ).toMatchObject( {
 			// A small selection of rules to test against.
-			'es/no-array-from': 0,
-			'es/no-block-scoped-variables': 2,
-			'es/no-nullish-coalescing-operators': 2,
-			'es/no-promise-any': 0,
-			'es/no-regexp-unicode-property-escapes': 2,
-			'es/no-regexp-unicode-property-escapes-2019': 0,
-			'es/no-set': 0,
-			'es/no-symbol': 0,
-			'es/no-weak-map': 0,
+			'es-x/no-array-from': 0,
+			'es-x/no-block-scoped-variables': 2,
+			'es-x/no-nullish-coalescing-operators': 2,
+			'es-x/no-promise-any': 0,
+			'es-x/no-regexp-unicode-property-escapes': 2,
+			'es-x/no-regexp-unicode-property-escapes-2019': 0,
+			'es-x/no-set': 0,
+			'es-x/no-symbol': 0,
+			'es-x/no-weak-map': 0,
 		} );
 	} );
 } );
