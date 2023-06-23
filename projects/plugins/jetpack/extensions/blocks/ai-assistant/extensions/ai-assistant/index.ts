5657e69a32794f1596fc97710bccf5d6b700611c
Damián Suárez <rdsuarez@gmail.com>
2023-06-23 18:52:10 -0300

    extend when the edit-post store is undefined

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/ai-assistant/index.ts
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/ai-assistant/index.ts
@@ -67,9 +67,13 @@ export function isPossibleToExtendBlock(): boolean {
 		return false;
 	}
 
-	// Do not extend if the AI Assistant block is hidden
-	const { getHiddenBlockTypes } = select( editPostStore );
-	const hiddenBlocks = getHiddenBlockTypes();
+	/*
+	 * Do not extend if the AI Assistant block is hidden
+	 * ToDo: the `editPostStore` is undefined for P2 sites.
+	 * Let's find a way to check if the block is hidden.
+	 */
+	const { getHiddenBlockTypes } = select( editPostStore ) || {};
+	const hiddenBlocks = getHiddenBlockTypes?.() || []; // It will extend the block if the function is undefined.
 	if ( hiddenBlocks.includes( blockName ) ) {
 		return false;
 	}
