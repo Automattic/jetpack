c094b3615a59a45ac4148508f8639cc74ae8d247
sixhours <calobee@gmail.com>
2023-06-28 11:39:11 -0400

    [not verified] Update to not hide the task upon completion

--- projects/packages/jetpack-mu-wpcom/src/features/launchpad/launchpad.php
+++ projects/packages/jetpack-mu-wpcom/src/features/launchpad/launchpad.php
@@ -139,7 +139,6 @@ function wpcom_launchpad_get_task_list_definitions() {
 				'drive_traffic',
 			),
 			'is_enabled_callback'    => 'wpcom_launchpad_is_keep_building_enabled',
-			'visible_tasks_callback' => 'wpcom_launchpad_keep_building_visible_tasks',
 		),
 	);
 
@@ -570,33 +569,6 @@ function wpcom_launchpad_is_keep_building_enabled() {
 	}
 
 	return false;
-}
-
-/**
- * Filter task visibility for the Keep building task list.
- *
- * @param array $task_list The task array.
- *
- * @return array The filtered array of task IDs.
- */
-function wpcom_launchpad_keep_building_visible_tasks( $task_list ) {
-	$task_ids = $task_list['task_ids'];
-
-	if ( ! $task_ids ) {
-		return array();
-	}
-
-	return array_filter(
-		$task_ids,
-		function ( $task_id ) {
-			// Only show design_edited/site_edited if it hasn't been marked as complete.
-			if ( in_array( $task_id, array( 'design_edited', 'site_edited' ), true ) ) {
-				return ! wpcom_is_checklist_task_complete( $task_id );
-			}
-
-			return true;
-		}
-	);
 }
 
 // Unhook our old mu-plugin - this current file is being loaded on 0 priority for `plugins_loaded`.
