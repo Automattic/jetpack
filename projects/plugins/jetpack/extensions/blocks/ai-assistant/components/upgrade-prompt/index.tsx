ac251d4b126f9738b24554a63fbce9d9032f8a78
Damián Suárez <rdsuarez@gmail.com>
2023-06-29 20:40:26 -0300

    simplify

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/components/upgrade-prompt/index.tsx
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/components/upgrade-prompt/index.tsx
@@ -1,7 +1,12 @@
 /*
  * External dependencies
  */
-import { isAtomicSite, isSimpleSite } from '@automattic/jetpack-shared-extension-utils';
+import { getRedirectUrl } from '@automattic/jetpack-components';
+import {
+	isAtomicSite,
+	isSimpleSite,
+	getSiteFragment,
+} from '@automattic/jetpack-shared-extension-utils';
 import { createInterpolateElement } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import React from 'react';
@@ -10,31 +15,18 @@ import React from 'react';
  */
 import { Nudge } from '../../../../shared/components/upgrade-nudge';
 import useAutosaveAndRedirect from '../../../../shared/use-autosave-and-redirect';
-import useUpgradeFlow from '../../../../shared/use-upgrade-flow';
-
-const useAIAssistantUpgradeFlow = () => {
-	// Direct checkout flow for atomic and simple sites, as they do not have My Jetpack.
-	const [ directCheckoutUrl, goToCheckoutPage, isRedirecting ] =
-		useUpgradeFlow( 'jetpack_ai_monthly' );
-
-	// My Jetpack checkout flow for Jetpack sites.
-	const myJetpackCheckoutUrl = `${ window?.Jetpack_Editor_Initial_State?.adminUrl }admin.php?page=my-jetpack#/add-jetpack-ai`;
-	const { autosaveAndRedirect, isRedirecting: isRedirectingMyJetpack } =
-		useAutosaveAndRedirect( myJetpackCheckoutUrl );
-
-	if ( isAtomicSite() || isSimpleSite() ) {
-		return { checkoutUrl: directCheckoutUrl, goToCheckoutPage, isRedirecting };
-	}
-
-	return {
-		checkoutUrl: myJetpackCheckoutUrl,
-		goToCheckoutPage: autosaveAndRedirect,
-		isRedirecting: isRedirectingMyJetpack,
-	};
-};
 
 const UpgradePrompt = () => {
-	const { checkoutUrl, goToCheckoutPage, isRedirecting } = useAIAssistantUpgradeFlow();
+	const wpcomCheckoutUrl = getRedirectUrl( 'jetpack-ai-monthly-plan-ai-assistant-block-banner', {
+		site: getSiteFragment(),
+	} );
+
+	const checkoutUrl =
+		isAtomicSite() || isSimpleSite()
+			? wpcomCheckoutUrl
+			: `${ window?.Jetpack_Editor_Initial_State?.adminUrl }admin.php?page=my-jetpack#/add-jetpack-ai`;
+
+	const { autosaveAndRedirect, isRedirecting } = useAutosaveAndRedirect( checkoutUrl );
 
 	return (
 		<Nudge
@@ -50,7 +42,7 @@ const UpgradePrompt = () => {
 					br: <br />,
 				}
 			) }
-			goToCheckoutPage={ goToCheckoutPage }
+			goToCheckoutPage={ autosaveAndRedirect }
 			isRedirecting={ isRedirecting }
 			visible={ true }
 			align={ null }
