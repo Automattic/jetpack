637c4a84187af85833245e422a30351a1710f7c1
Caroline Moore <calobee@gmail.com>
2023-07-03 15:00:54 -0400

    Launchpad: Fix edit site design visibility (#31620)
    * [not verified] Update to not hide the task upon completion
    
    * changelog
    
    * [not verified] Align double arrows

--- projects/packages/jetpack-mu-wpcom/src/features/launchpad/launchpad.php
+++ projects/packages/jetpack-mu-wpcom/src/features/launchpad/launchpad.php
@@ -129,8 +129,8 @@ function wpcom_launchpad_get_task_list_definitions() {
 			'is_enabled_callback' => 'wpcom_get_launchpad_is_enabled',
 		),
 		'keep-building'   => array(
-			'title'                  => 'Keep Building',
-			'task_ids'               => array(
+			'title'               => 'Keep Building',
+			'task_ids'            => array(
 				'site_title',
 				'design_edited',
 				'domain_claim',
@@ -138,8 +138,7 @@ function wpcom_launchpad_get_task_list_definitions() {
 				'domain_customize',
 				'drive_traffic',
 			),
-			'is_enabled_callback'    => 'wpcom_launchpad_is_keep_building_enabled',
-			'visible_tasks_callback' => 'wpcom_launchpad_keep_building_visible_tasks',
+			'is_enabled_callback' => 'wpcom_launchpad_is_keep_building_enabled',
 		),
 	);
 
@@ -574,33 +573,6 @@ function wpcom_launchpad_is_keep_building_enabled() {
 	}
 
 	return false;
-}
-
-/**
- * Filter task visibility for the Keep building task list.
- *
- * @param array $task_list The task array.
- *
- * @return array The filtered array of task IDs.
- */
-function wpcom_launchpad_keep_building_visible_tasks( $task_list ) {
-	$task_ids = $task_list['task_ids'];
-
-	if ( ! $task_ids ) {
-		return array();
-	}
-
-	return array_filter(
-		$task_ids,
-		function ( $task_id ) {
-			// Only show design_edited/site_edited if it hasn't been marked as complete.
-			if ( in_array( $task_id, array( 'design_edited', 'site_edited' ), true ) ) {
-				return ! wpcom_is_checklist_task_complete( $task_id );
-			}
-
-			return true;
-		}
-	);
 }
 
 // Unhook our old mu-plugin - this current file is being loaded on 0 priority for `plugins_loaded`.
