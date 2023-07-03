2285799c815fb8461a5a714cb252c1a95ad59948
Caroline Moore <calobee@gmail.com>
2023-07-03 14:46:51 -0400

    Launchpad: Update is_enabled_callback for Keep Building checklist (#31623)
    * [not verified] Only enable Keep Building if the site has launched.
    
    * changelog
    
    * Update with a comment to explain why we're not checking the other launch statuses

--- projects/packages/jetpack-mu-wpcom/src/features/launchpad/launchpad.php
+++ projects/packages/jetpack-mu-wpcom/src/features/launchpad/launchpad.php
@@ -562,10 +562,14 @@ function wpcom_get_launchpad_task_list_is_enabled( $checklist_slug ) {
  * @return bool True if the task list is enabled, false otherwise.
  */
 function wpcom_launchpad_is_keep_building_enabled() {
-	$intent  = get_option( 'site_intent', false );
-	$blog_id = get_current_blog_id();
+	$intent                  = get_option( 'site_intent', false );
+	$launchpad_task_statuses = get_option( 'launchpad_checklist_tasks_statuses', array() );
 
-	if ( 'build' === $intent && $blog_id > 220443356 ) {
+	// We don't care about the other *_launched tasks, since this is specific to the Build flow.
+	$launched = isset( $launchpad_task_statuses['site_launched'] ) && $launchpad_task_statuses['site_launched'];
+	$blog_id  = get_current_blog_id();
+
+	if ( 'build' === $intent && $blog_id > 220443356 && $launched ) {
 		return true;
 	}
 
