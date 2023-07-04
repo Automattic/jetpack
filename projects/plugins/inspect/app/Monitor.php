6ed0f5aa8d3cfa0eeb92de1b9bd1b2b49a33ba1f
Igor Zinovyev <zinigor@gmail.com>
2023-07-04 22:43:01 +0300

    Prettified and PHP 5.6-ified.

--- projects/plugins/inspect/app/Monitor.php
+++ projects/plugins/inspect/app/Monitor.php
@@ -1,27 +1,61 @@
-<?php
+<?php // phpcs:ignore WordPress.Files.FileName.InvalidClassFileName
 
 namespace Automattic\Jetpack_Inspect;
 
 use Automattic\Jetpack\Packages\Async_Option\Async_Option;
 use Automattic\Jetpack_Inspect\Monitor\Observable;
 
+/**
+ * The Monitor class.
+ */
 class Monitor {
 
-	protected Observable $observer;
-	protected string     $name;
-	protected bool       $bypass_filter = false;
+	/**
+	 * The observable object.
+	 *
+	 * @var Observable
+	 */
+	protected $observer;
 
-	protected Async_Option $option;
+	/**
+	 * The option name.
+	 *
+	 * @var String
+	 */
+	protected $name;
 
-	public function __construct( string $name, Observable $observable ) {
+	/**
+	 * Whether to bypass the filter.
+	 *
+	 * @var Boolean
+	 * */
+	protected $bypass_filter = false;
+
+	/**
+	 * The async option object.
+	 *
+	 * @var Async_Option
+	 */
+	protected $option;
+
+	/**
+	 * Creates a Monitor object.
+	 *
+	 * @param String     $name the option name.
+	 * @param Observable $observable the object to attach hooks to.
+	 */
+	public function __construct( $name, $observable ) {
 		$this->name     = $name;
 		$this->observer = $observable;
 		$this->option   = jetpack_inspect_option( $name );
 	}
 
+	/**
+	 * Initializes the object.
+	 */
 	public function initialize() {
 
-		if ( defined( "DOING_CRON" ) && DOING_CRON ) {
+		if ( defined( 'DOING_CRON' ) && DOING_CRON ) {
 			return false;
 		}
 
@@ -29,18 +63,26 @@ class Monitor {
 			$this->observer->attach_hooks();
 		}
 
-		add_action( 'shutdown', [ $this, 'save' ] );
+		add_action( 'shutdown', array( $this, 'save' ) );
 	}
 
+	/**
+	 * Ensures that the hooks are attached.
+	 */
 	public function ensure_enabled() {
 		if ( $this->is_enabled() ) {
 			return;
 		}
 		$this->observer->attach_hooks();
-		add_action( 'shutdown', [ $this, 'log' ] );
+		add_action( 'shutdown', array( $this, 'log' ) );
 	}
 
-	protected function match_request_filter( $url ): bool {
+	/**
+	 * Returns true whether the request url matches the filter.
+	 *
+	 * @param String $url the request URL.
+	 */
+	protected function match_request_filter( $url ) {
 		if ( $this->bypass_filter ) {
 			return true;
 		}
@@ -50,21 +92,24 @@ class Monitor {
 			return true;
 		}
 
-		// https://example.com/?foo=bar will match "*example[s].com*
+		// https://example.com/?foo=bar will match "*example[s].com*.
 		if ( str_contains( $filter, '*' ) || ( str_contains( $filter, '[' ) && str_contains( $filter, ']' ) ) ) {
 			return fnmatch( $filter, $url );
 		}
 
-		// https://example.com/?foo=bar will match "https://example.com/?foo=bar"
+		// https://example.com/?foo=bar will match "https://example.com/?foo=bar".
 		if ( $filter[0] === $filter[ strlen( $filter ) - 1 ] && $filter[0] === '"' ) {
 			$filter = substr( $filter, 1, - 1 );
 			return $filter === $url;
 		}
 
-		// https://example.com/?foo=bar will match example.com
+		// https://example.com/?foo=bar will match example.com.
 		return str_contains( $url, $filter );
 	}
 
+	/**
+	 * Saves the log data.
+	 */
 	public function save() {
 
 		$log_data = $this->observer->get();
@@ -87,16 +132,13 @@ class Monitor {
 				$log_name = 'wp_error';
 			}
 
-			$log = [
+			$log = array(
 				'url'     => $url,
 				$log_name => $data,
-			];
-
+			);
 
 			Log::insert( $url, $log );
 		}
-
-
 	}
 
 	/**
@@ -104,19 +146,25 @@ class Monitor {
 	 *   Example keys:
 	 *      * observer_incoming
 	 *      * observer_outgoing
+	 *
+	 * @param String $name option name.
 	 */
 	private function key( $name ) {
 		return "{$this->name}_{$name}";
 	}
 
+	/**
+	 * Returns the Monitor status.
+	 */
 	public function is_enabled() {
 		return jetpack_inspect_get_option( 'monitor_status' ) && $this->option->get()['enabled'];
 	}
 
+	/**
+	 * Returns the currently set filter.
+	 */
 	public function get_filter() {
 		return $this->option->get()['filter'];
 	}
-
-
 
 }
