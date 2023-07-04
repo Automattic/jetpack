610eaf6571f9efbce5418e39812413c393a1f451
Donncha O Caoimh <5656673+donnchawp@users.noreply.github.com>
2023-07-04 13:41:24 +0100

    Define the length in the truncateString parameter

--- projects/plugins/boost/app/assets/src/js/utils/truncate-string.ts
+++ projects/plugins/boost/app/assets/src/js/utils/truncate-string.ts
@@ -5,10 +5,6 @@
  * @param {string} str       string to truncate
  * @param {number} maxLength maximum length of the string
  */
-export function truncateString( str: string, maxLength?: number ): string {
-	if ( ! maxLength ) {
-		maxLength = 20;
-	}
-
+export function truncateString( str: string, maxLength = 20 ): string {
 	return str.length > maxLength ? str.slice( 0, maxLength ) + '…' : str;
 }
