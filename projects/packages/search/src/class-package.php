c7145d2a36886e4f398bed2b1afd25959a0a23f3
Jasper Kang <kangzjnet@gmail.com>
2023-07-07 10:28:00 +1200

    fix up project versions

--- projects/packages/search/src/class-package.php
+++ projects/packages/search/src/class-package.php
@@ -11,7 +11,7 @@ namespace Automattic\Jetpack\Search;
  * Search package general information
  */
 class Package {
-	const VERSION = '0.37.4';
+	const VERSION = '0.38.0-alpha';
 	const SLUG    = 'search';
 
 	/**
