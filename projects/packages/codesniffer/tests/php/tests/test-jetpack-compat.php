753dcbdc910a7791f9128bb8e028add4c9242630
Brad Jorsch <brad.jorsch@automattic.com>
2023-07-05 21:04:23 -0400

    More tests to verify that the exclusions don't strip codes to 3 parts

--- projects/packages/codesniffer/tests/php/tests/test-jetpack-compat.php
+++ projects/packages/codesniffer/tests/php/tests/test-jetpack-compat.php
@@ -19,7 +19,7 @@ use PHPUnit\Framework\TestCase;
 class JetpackCompatTest extends TestCase {
 	use \Yoast\PHPUnitPolyfills\Polyfills\AssertIsType;
 
-	const SRC = <<<'EOF'
+	const SRC1 = <<<'EOF'
 <?php
 function bad_in_56( bool $b ) {}
 try {} catch ( Bad | In70 $e ) {}
@@ -29,14 +29,26 @@ $bad_in_73 ??= true;
 // TODO: More stuff for 8.0+ once PHPCompatibility 10 finally releases.
 EOF;
 
+	const SRC2 = <<<'EOF'
+<?php
+ini_get( 'assert.exception' );
+ini_get( 'hard_timeout' );
+ini_get( 'imap.enable_insecure_rsh' );
+ini_get( 'session.cookie_samesite' );
+ini_get( 'opcache.cache_id' );
+ini_get( 'zend.exception_string_param_max_len' );
+ini_get( 'fiber.stack_size' );
+EOF;
+
 	/**
 	 * Test the sniffs by running phpcs or phpcbf against a file.
 	 *
 	 * @dataProvider provide_standards
 	 * @param string $standard Standard to run with.
+	 * @param string $src Source to check.
 	 * @param string $expect Expected output.
 	 */
-	public function test_phpcs( $standard, $expect ) {
+	public function test_phpcs( $standard, $src, $expect ) {
 		$config = new Config();
 
 		$config->standards   = array( $standard );
@@ -63,7 +75,7 @@ EOF;
 		Config::setConfigData( 'testVersion', '5.6-' );
 
 		$ruleset = new Ruleset( $config );
-		$dummy   = new DummyFile( self::SRC, $ruleset, $config );
+		$dummy   = new DummyFile( $src, $ruleset, $config );
 		$dummy->process();
 
 		$reporter = new Reporter( $config );
@@ -87,7 +99,7 @@ EOF;
 	 * @return array
 	 */
 	public function provide_standards() {
-		$lines = array(
+		$lines1 = array(
 			' 2 | ERROR | \'bool\' type declaration is not present in PHP version 5.6 or earlier (PHPCompatibility.FunctionDeclarations.NewParamTypeDeclarations.boolFound)',
 			' 3 | ERROR | Catching multiple exceptions within one statement is not supported in PHP 7.0 or earlier. (PHPCompatibility.ControlStructures.NewMultiCatch.Found)',
 			' 4 | ERROR | \'object\' type declaration is not present in PHP version 7.1 or earlier (PHPCompatibility.FunctionDeclarations.NewParamTypeDeclarations.objectFound)',
@@ -95,15 +107,34 @@ EOF;
 			' 6 | ERROR | null coalesce equal operator (??=) is not present in PHP version 7.3 or earlier (PHPCompatibility.Operators.NewOperators.t_coalesce_equalFound)',
 		);
 
-		yield array( 'Jetpack', $lines );
-		yield array( 'Jetpack-Compat-70', array_slice( $lines, 1 ) );
-		yield array( 'Jetpack-Compat-71', array_slice( $lines, 2 ) );
-		yield array( 'Jetpack-Compat-72', array_slice( $lines, 3 ) );
-		yield array( 'Jetpack-Compat-73', array_slice( $lines, 4 ) );
-		yield array( 'Jetpack-Compat-74', array_slice( $lines, 5 ) );
-		yield array( 'Jetpack-Compat-80', array_slice( $lines, 5 ) );
-		yield array( 'Jetpack-Compat-81', array_slice( $lines, 5 ) );
-		yield array( 'Jetpack-Compat-82', array_slice( $lines, 5 ) );
+		yield array( 'Jetpack', self::SRC1, $lines1 );
+		yield array( 'Jetpack-Compat-70', self::SRC1, array_slice( $lines1, 1 ) );
+		yield array( 'Jetpack-Compat-71', self::SRC1, array_slice( $lines1, 2 ) );
+		yield array( 'Jetpack-Compat-72', self::SRC1, array_slice( $lines1, 3 ) );
+		yield array( 'Jetpack-Compat-73', self::SRC1, array_slice( $lines1, 4 ) );
+		yield array( 'Jetpack-Compat-74', self::SRC1, array_slice( $lines1, 5 ) );
+		yield array( 'Jetpack-Compat-80', self::SRC1, array_slice( $lines1, 6 ) );
+		yield array( 'Jetpack-Compat-81', self::SRC1, array_slice( $lines1, 7 ) );
+		yield array( 'Jetpack-Compat-82', self::SRC1, array_slice( $lines1, 8 ) );
+
+		$lines2 = array(
+			' 2 | WARNING | INI directive \'assert.exception\' is not present in PHP version 5.6 or earlier (PHPCompatibility.IniDirectives.NewIniDirectives.assert_exceptionFound)',
+			' 3 | WARNING | INI directive \'hard_timeout\' is not present in PHP version 7.0 or earlier (PHPCompatibility.IniDirectives.NewIniDirectives.hard_timeoutFound)',
+			' 4 | WARNING | INI directive \'imap.enable_insecure_rsh\' is not present in PHP version 7.1.24 or earlier (PHPCompatibility.IniDirectives.NewIniDirectives.imap_enable_insecure_rshFound)',
+			' 5 | WARNING | INI directive \'session.cookie_samesite\' is not present in PHP version 7.2 or earlier (PHPCompatibility.IniDirectives.NewIniDirectives.session_cookie_samesiteFound)',
+			' 6 | WARNING | INI directive \'opcache.cache_id\' is not present in PHP version 7.3 or earlier (PHPCompatibility.IniDirectives.NewIniDirectives.opcache_cache_idFound)',
+			// TODO: More lines for 8.0+ once PHPCompatibility 10 finally releases.
+		);
+
+		yield array( 'Jetpack', self::SRC2, $lines2 );
+		yield array( 'Jetpack-Compat-70', self::SRC2, array_slice( $lines2, 1 ) );
+		yield array( 'Jetpack-Compat-71', self::SRC2, array_slice( $lines2, 2 ) );
+		yield array( 'Jetpack-Compat-72', self::SRC2, array_slice( $lines2, 3 ) );
+		yield array( 'Jetpack-Compat-73', self::SRC2, array_slice( $lines2, 4 ) );
+		yield array( 'Jetpack-Compat-74', self::SRC2, array_slice( $lines2, 5 ) );
+		yield array( 'Jetpack-Compat-80', self::SRC2, array_slice( $lines2, 6 ) );
+		yield array( 'Jetpack-Compat-81', self::SRC2, array_slice( $lines2, 7 ) );
+		yield array( 'Jetpack-Compat-82', self::SRC2, array_slice( $lines2, 8 ) );
 	}
 
 }
