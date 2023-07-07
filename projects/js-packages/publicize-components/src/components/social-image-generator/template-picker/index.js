c8a0300657bb60b7ed37e1f341c7cb9b03d82ab9
Gergely Juhasz <gergely.juhasz@automattic.com>
2023-07-07 12:20:41 +0200

    Refactor template picker

--- projects/js-packages/publicize-components/src/components/social-image-generator/template-picker/index.js
+++ projects/js-packages/publicize-components/src/components/social-image-generator/template-picker/index.js
@@ -1,6 +1,4 @@
-import { ThemeProvider } from '@automattic/jetpack-components';
-import { Button, Modal } from '@wordpress/components';
-import { useState, useCallback, useMemo } from '@wordpress/element';
+import { useState, useMemo } from '@wordpress/element';
 import { sprintf, __ } from '@wordpress/i18n';
 import classnames from 'classnames';
 import React from 'react';
@@ -8,73 +6,53 @@ import styles from './styles.module.scss';
 import TEMPLATES_DATA from './templates.js';
 
 /**
- * A component that displays a modal with a list of templates to choose from.
  *
- * @param {object} props - The component props.
- * @param {Function} props.onSelect - A function that will be called when a template is selected. Receives the name of the selected template as an argument.
- * @param {Function} props.render - A function that will be called with an object containing an "open" function, which can be called to open the template picker.
- * @param {string|null} [props.value=null] - The name of the currently selected template.
+ * The pure template picker component. Does not save the template changes, just sends it back to the parent component,
+ * with the onTemplateSelected callback.
+ *
+ * @param {{value: string|null, onTemplateSelected: Function}} props - The component props:
+ * Value is the name of the currently selected template, onTemplateSelected is a function that
+ * will be called when a template is selected. Receives the name of the selected template as an argument.
  * @returns {React.ReactNode} - The component's rendered output.
  */
-const TemplatePicker = ( { onSelect, render, value = null } ) => {
-	const [ isOpen, setIsOpen ] = useState( false );
+const TemplatePicker = ( { value = null, onTemplateSelected = null } ) => {
 	const [ selectedTemplate, setSelectedTemplate ] = useState( value );
-
-	const openPicker = useCallback( () => setIsOpen( true ), [ setIsOpen ] );
-	const closePicker = useCallback( () => {
-		setSelectedTemplate( value );
-		setIsOpen( false );
-	}, [ value, setSelectedTemplate, setIsOpen ] );
-	const saveAndClosePicker = useCallback( () => {
-		onSelect( selectedTemplate );
-		setIsOpen( false );
-	}, [ onSelect, setIsOpen, selectedTemplate ] );
 
 	// Add memoized callback function for each template.
 	const TEMPLATES = useMemo(
 		() =>
 			TEMPLATES_DATA.map( template => ( {
 				...template,
-				onSelect: () => setSelectedTemplate( template.name ),
+				onSelect: () => {
+					setSelectedTemplate( template.name );
+					if ( onTemplateSelected ) {
+						onTemplateSelected( template.name );
+					}
+				},
 			} ) ),
-		[ setSelectedTemplate ]
+		[ setSelectedTemplate, onTemplateSelected ]
 	);
 
 	return (
-		<ThemeProvider targetDom={ document.body }>
-			{ render( { open: openPicker } ) }
-			{ isOpen && (
-				<Modal onRequestClose={ closePicker } title={ __( 'Pick a Template', 'jetpack' ) }>
-					<div className={ styles.templates }>
-						{ TEMPLATES.map( template => (
-							<button
-								onClick={ template.onSelect }
-								key={ template.name }
-								className={ classnames( styles.template, {
-									[ styles[ 'template--active' ] ]: template.name === selectedTemplate,
-								} ) }
-							>
-								<img src={ template.image } alt={ template.label } />
-								<span className="screen-reader-text">
-									{
-										/* translators: %s is the name of the template */
-										sprintf( __( 'Pick the %s template', 'jetpack' ), template.label )
-									}
-								</span>
-							</button>
-						) ) }
-					</div>
-					<div className={ styles.footer }>
-						<Button variant="tertiary" onClick={ closePicker }>
-							{ __( 'Cancel', 'jetpack' ) }
-						</Button>
-						<Button variant="primary" onClick={ saveAndClosePicker }>
-							{ __( 'Save', 'jetpack' ) }
-						</Button>
-					</div>
-				</Modal>
-			) }
-		</ThemeProvider>
+		<div className={ styles.templates }>
+			{ TEMPLATES.map( template => (
+				<button
+					onClick={ template.onSelect }
+					key={ template.name }
+					className={ classnames( styles.template, {
+						[ styles[ 'template--active' ] ]: template.name === selectedTemplate,
+					} ) }
+				>
+					<img src={ template.image } alt={ template.label } />
+					<span className="screen-reader-text">
+						{
+							/* translators: %s is the name of the template */
+							sprintf( __( 'Pick the %s template', 'jetpack' ), template.label )
+						}
+					</span>
+				</button>
+			) ) }
+		</div>
 	);
 };
 
