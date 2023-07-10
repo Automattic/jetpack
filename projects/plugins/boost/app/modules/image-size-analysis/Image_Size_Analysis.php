73273bbffb8abcf0e52049678d6716e1469e6411
thingalon <mark@thingalon.com>
2023-07-10 20:13:45 +1000

    [Boost] Remove Image Analyzer toggle (#31770)
    * Always turn IG Analyzer on
    
    * cleanup
    
    * changelog
    
    * Move the Image Analysis module up the page, remove its toggle
    
    ---------
    
    Co-authored-by: Mark George <thingalon@gmail.com>

--- projects/plugins/boost/app/modules/image-size-analysis/Image_Size_Analysis.php
+++ projects/plugins/boost/app/modules/image-size-analysis/Image_Size_Analysis.php
@@ -2,12 +2,13 @@
 
 namespace Automattic\Jetpack_Boost\Modules\Image_Size_Analysis;
 
+use Automattic\Jetpack_Boost\Contracts\Is_Always_On;
 use Automattic\Jetpack_Boost\Contracts\Pluggable;
 use Automattic\Jetpack_Boost\Lib\Premium_Features;
 use Automattic\Jetpack_Boost\REST_API\Contracts\Has_Endpoints;
 use Automattic\Jetpack_Boost\REST_API\Endpoints\Image_Analysis_Start;
 
-class Image_Size_Analysis implements Pluggable, Has_Endpoints {
+class Image_Size_Analysis implements Pluggable, Has_Endpoints, Is_Always_On {
 
 	public function setup() {
 		// noop
