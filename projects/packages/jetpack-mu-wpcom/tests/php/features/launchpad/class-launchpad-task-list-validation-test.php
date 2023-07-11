ef7c3972ae89c7475ddee5c5b91fcd75d480c8d4
Mark Biek <mark.biek@automattic.com>
2023-07-11 15:53:55 -0400

    [not verified] Update validate_task_list to return WP_Error on failure

--- projects/packages/jetpack-mu-wpcom/tests/php/features/launchpad/class-launchpad-task-list-validation-test.php
+++ projects/packages/jetpack-mu-wpcom/tests/php/features/launchpad/class-launchpad-task-list-validation-test.php
@@ -14,6 +14,14 @@ class Launchpad_Task_List_Validation_Test extends \WorDBless\BaseTestCase {
 	/**
 	 * Data provider for test_validate_task_list.
 	 *
+	 * The test data is in the format of:
+	 *
+	 *   'test key' => array(
+	 *     (array) $task_list,
+	 *     (null | WP_Error) $expected_result,
+	 *     (bool) $expected_is_wp_error,
+	 *   )
+	 *
 	 * @return array
 	 */
 	public function provide_validate_task_list_test_cases() {
@@ -25,7 +33,8 @@ class Launchpad_Task_List_Validation_Test extends \WorDBless\BaseTestCase {
 					'required_task_ids'            => array( 'task_1' ),
 					'require_last_task_completion' => true,
 				),
-				true,
+				null,
+				false,
 			),
 			'Valid task list with last task completion'   => array(
 				array(
@@ -33,7 +42,8 @@ class Launchpad_Task_List_Validation_Test extends \WorDBless\BaseTestCase {
 					'task_ids'                     => array( 'task_1', 'task_2' ),
 					'require_last_task_completion' => true,
 				),
-				true,
+				null,
+				false,
 			),
 			'Valid task list with only required task IDs' => array(
 				array(
@@ -41,14 +51,16 @@ class Launchpad_Task_List_Validation_Test extends \WorDBless\BaseTestCase {
 					'task_ids'          => array( 'task_1', 'task_2' ),
 					'required_task_ids' => array( 'task_1' ),
 				),
-				true,
+				null,
+				false,
 			),
 			'Valid task list with minimal validation'     => array(
 				array(
 					'id'       => 'task_list_1',
 					'task_ids' => array( 'task_1', 'task_2' ),
 				),
-				true,
+				null,
+				false,
 			),
 			'Invalid task list with no id'                => array(
 				array(
@@ -56,7 +68,8 @@ class Launchpad_Task_List_Validation_Test extends \WorDBless\BaseTestCase {
 					'required_task_ids'            => array( 'task_1' ),
 					'require_last_task_completion' => true,
 				),
-				false,
+				new WP_Error( 'invalid-task-list', 'Task list is missing an ID.' ),
+				true,
 			),
 			'Invalid task list with no task_ids'          => array(
 				array(
@@ -64,7 +77,8 @@ class Launchpad_Task_List_Validation_Test extends \WorDBless\BaseTestCase {
 					'required_task_ids'            => array( 'task_1' ),
 					'require_last_task_completion' => true,
 				),
-				false,
+				new WP_Error( 'invalid-task-list', 'Task list is missing task IDs.' ),
+				true,
 			),
 			'Invalid task list with invalid required_task_ids' => array(
 				array(
@@ -72,7 +86,8 @@ class Launchpad_Task_List_Validation_Test extends \WorDBless\BaseTestCase {
 					'task_ids'          => array( 'task_1', 'task_2' ),
 					'required_task_ids' => 'task_1',
 				),
-				false,
+				new WP_Error( 'invalid-task-list', 'Task list required task IDs must be an array.' ),
+				true,
 			),
 			'Invalid task list with invalid require_last_task_completion' => array(
 				array(
@@ -81,7 +96,8 @@ class Launchpad_Task_List_Validation_Test extends \WorDBless\BaseTestCase {
 					'required_task_ids'            => array( 'task_1' ),
 					'require_last_task_completion' => 'true',
 				),
-				false,
+				new WP_Error( 'invalid-task-list', 'Task list require last task completion must be a boolean.' ),
+				true,
 			),
 		);
 	}
@@ -91,10 +107,16 @@ class Launchpad_Task_List_Validation_Test extends \WorDBless\BaseTestCase {
 	 *
 	 * @param array $task_list       Task list to validate.
 	 * @param bool  $expected_result Expected validation result.
+	 * @param bool  $expected_is_wp_error Flag to signal if the expected result is a WP_Error.
 	 * @dataProvider provide_validate_task_list_test_cases
 	 */
-	public function test_validate_task_list( $task_list, $expected_result ) {
+	public function test_validate_task_list( $task_list, $expected_result, $expected_is_wp_error ) {
 		$result = Launchpad_Task_Lists::validate_task_list( $task_list );
+
+		if ( $expected_is_wp_error ) {
+			$this->assertTrue( is_wp_error( $result ) );
+			return;
+		}
 
 		$this->assertSame( $expected_result, $result );
 	}
