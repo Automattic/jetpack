33c48a9984765301842f4f17749b001084ef3c31
Mikael Korpela <mikael@ihminen.org>
2023-07-13 01:31:43 +0300

    [not verified] Restructure JS better

--- projects/plugins/jetpack/extensions/blocks/subscriptions/view.js
+++ projects/plugins/jetpack/extensions/blocks/subscriptions/view.js
@@ -1,15 +1,64 @@
 /* global tb_show, tb_remove */
 
+import domReady from '@wordpress/dom-ready';
 import './view.scss';
 import '../../shared/memberships.scss';
-
-import domReady from '@wordpress/dom-ready';
 import {
 	setPurchaseResultCookie,
 	reloadPageWithPremiumContentQueryString,
 } from '../../../extensions/shared/memberships';
 
-domReady( function () {
+//TODO: Unify this code across premium content, subscribe and payment button.
+const handleIframeResult = function ( eventFromIframe ) {
+	if ( eventFromIframe.origin === 'https://subscribe.wordpress.com' && eventFromIframe.data ) {
+		let premiumContentJWTToken = '';
+
+		const data = JSON.parse( eventFromIframe.data );
+		if ( data && data.result && data.result.jwt_token ) {
+			// We save the token for now, doing nothing.
+			premiumContentJWTToken = data.result.jwt_token;
+			setPurchaseResultCookie( premiumContentJWTToken );
+		} else if ( data && data.action === 'close' && premiumContentJWTToken ) {
+			// The token was set during the purchase flow, we want to relead the whole page with token in query string so it recognizes that the user is logged in.
+			reloadPageWithPremiumContentQueryString( premiumContentJWTToken, {
+				subscribe: 'success',
+			} );
+		} else if ( data && data.action === 'close' ) {
+			// User just aborted.
+			window.removeEventListener( 'message', handleIframeResult );
+			tb_remove();
+		}
+	}
+};
+
+function handleShowingFormModal( form, email ) {
+	if ( form.resubmitted || ! email ) {
+		return;
+	}
+
+	const url =
+		'https://subscribe.wordpress.com/memberships/?' +
+		'blog=' +
+		form.dataset.blog +
+		'&plan=newsletter' +
+		'&source=jetpack_subscribe' +
+		'&post_access_level=' +
+		form.dataset.post_access_level +
+		'&display=alternate&' +
+		'email=' +
+		encodeURIComponent( email );
+	window.scrollTo( 0, 0 );
+	tb_show( null, url + '&TB_iframe=true', null );
+
+	window.addEventListener( 'message', handleIframeResult, false );
+	const tbWindow = document.querySelector( '#TB_window' );
+	tbWindow.classList.add( 'jetpack-memberships-modal' );
+
+	// This line has to come after the Thickbox has opened otherwise Firefox doesnt scroll to the top.
+	window.scrollTo( 0, 0 );
+}
+
+function initializeSubscriberPayWallLoginLink() {
 	const loginLink = document.querySelector( '.jetpack-subscriber-paywall-login' );
 	if ( ! loginLink ) {
 		return;
@@ -24,107 +73,31 @@ domReady( function () {
 
 		const email = prompt( 'Please enter your email' );
 
-		let premiumContentJWTToken = '';
-		const url =
-			'https://subscribe.wordpress.com/memberships/?' +
-			'blog=' +
-			form.dataset.blog +
-			'&plan=newsletter' +
-			'&source=jetpack_subscribe' +
-			'&post_access_level=' +
-			form.dataset.post_access_level +
-			'&display=alternate&' +
-			'email=' +
-			encodeURIComponent( email );
-		window.scrollTo( 0, 0 );
-		tb_show( null, url + '&TB_iframe=true', null );
+		if ( ! email ) {
+			return;
+		}
 
-		//TODO: Unify this code across premium content, subscribe and payment button.
-		const handleIframeResult = function ( eventFromIframe ) {
-			if ( eventFromIframe.origin === 'https://subscribe.wordpress.com' && eventFromIframe.data ) {
-				const data = JSON.parse( eventFromIframe.data );
-				if ( data && data.result && data.result.jwt_token ) {
-					// We save the token for now, doing nothing.
-					premiumContentJWTToken = data.result.jwt_token;
-					setPurchaseResultCookie( premiumContentJWTToken );
-				} else if ( data && data.action === 'close' && premiumContentJWTToken ) {
-					// The token was set during the purchase flow, we want to relead the whole page with token in query string so it recognizes that the user is logged in.
-					reloadPageWithPremiumContentQueryString( premiumContentJWTToken, {
-						subscribe: 'success',
-					} );
-				} else if ( data && data.action === 'close' ) {
-					// User just aborted.
-					window.removeEventListener( 'message', handleIframeResult );
-					tb_remove();
-				}
-			}
-		};
-		window.addEventListener( 'message', handleIframeResult, false );
-		const tbWindow = document.querySelector( '#TB_window' );
-		tbWindow.classList.add( 'jetpack-memberships-modal' );
-
-		// This line has to come after the Thickbox has opened otherwise Firefox doesnt scroll to the top.
-		window.scrollTo( 0, 0 );
+		handleShowingFormModal( form, email );
 	} );
-} );
+}
 
-domReady( function () {
+function initializeSubscriberForm() {
 	const form = document.querySelector( '.wp-block-jetpack-subscriptions__container form' );
 	if ( ! form ) {
 		return;
 	}
-	let premiumContentJWTToken = '';
+
 	if ( ! form.payments_attached ) {
 		form.payments_attached = true;
 		form.addEventListener( 'submit', function ( event ) {
-			const email = form.querySelector( 'input[type=email]' ).value;
-			if ( form.resubmitted || ! email ) {
-				return;
-			}
 			event.preventDefault();
-			const url =
-				'https://subscribe.wordpress.com/memberships/?' +
-				'blog=' +
-				form.dataset.blog +
-				'&plan=newsletter' +
-				'&source=jetpack_subscribe' +
-				'&post_access_level=' +
-				form.dataset.post_access_level +
-				'&display=alternate&' +
-				'email=' +
-				encodeURIComponent( email );
-			window.scrollTo( 0, 0 );
-			tb_show( null, url + '&TB_iframe=true', null );
-
-			//TODO: Unify this code across premium content, subscribe and payment button.
-			const handleIframeResult = function ( eventFromIframe ) {
-				if (
-					eventFromIframe.origin === 'https://subscribe.wordpress.com' &&
-					eventFromIframe.data
-				) {
-					const data = JSON.parse( eventFromIframe.data );
-					if ( data && data.result && data.result.jwt_token ) {
-						// We save the token for now, doing nothing.
-						premiumContentJWTToken = data.result.jwt_token;
-						setPurchaseResultCookie( premiumContentJWTToken );
-					} else if ( data && data.action === 'close' && premiumContentJWTToken ) {
-						// The token was set during the purchase flow, we want to relead the whole page with token in query string so it recognizes that the user is logged in.
-						reloadPageWithPremiumContentQueryString( premiumContentJWTToken, {
-							subscribe: 'success',
-						} );
-					} else if ( data && data.action === 'close' ) {
-						// User just aborted.
-						window.removeEventListener( 'message', handleIframeResult );
-						tb_remove();
-					}
-				}
-			};
-			window.addEventListener( 'message', handleIframeResult, false );
-			const tbWindow = document.querySelector( '#TB_window' );
-			tbWindow.classList.add( 'jetpack-memberships-modal' );
-
-			// This line has to come after the Thickbox has opened otherwise Firefox doesnt scroll to the top.
-			window.scrollTo( 0, 0 );
+			const email = form.querySelector( 'input[type=email]' ).value;
+			handleShowingFormModal( form, email );
 		} );
 	}
+}
+
+domReady( function () {
+	initializeSubscriberPayWallLoginLink();
+	initializeSubscriberForm();
 } );
