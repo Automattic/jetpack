9ae71b0934faea19bcfc33e606b39a4378d6a73d
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2023-07-14 00:01:25 -0700

    Add home redirect

--- projects/plugins/crm/src/js/components/automations-admin/index.tsx
+++ projects/plugins/crm/src/js/components/automations-admin/index.tsx
@@ -1,5 +1,6 @@
 import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { AutomationsAdminContainer } from './components/automations-admin-container';
+import { RedirectHome } from './components/redirect-home';
 
 export const AutomationsAdmin = () => {
 	const router = createHashRouter( [
@@ -7,6 +8,8 @@ export const AutomationsAdmin = () => {
 			path: '/automations',
 			element: (
 				<AutomationsAdminContainer>
+					<div>This is the home page</div>
+					<br />
 					<a href="#/automations/edit">Go to edit</a>
 					<br />
 					<a href="#/automations/add">Go to add</a>
@@ -17,6 +20,10 @@ export const AutomationsAdmin = () => {
 			path: '/automations/edit',
 			element: (
 				<AutomationsAdminContainer>
+					<div>This is the edit page</div>
+					<br />
+					<a href="#/automations/add">Go to add</a>
+					<br />
 					<a href="#/automations">Go back to home</a>
 				</AutomationsAdminContainer>
 			),
@@ -25,9 +32,17 @@ export const AutomationsAdmin = () => {
 			path: '/automations/add',
 			element: (
 				<AutomationsAdminContainer>
+					<div>This is the add page</div>
+					<br />
+					<a href="#/automations/edit">Go to edit</a>
+					<br />
 					<a href="#/automations">Go back to home</a>
 				</AutomationsAdminContainer>
 			),
+		},
+		{
+			path: '*',
+			element: <RedirectHome />,
 		},
 	] );
 
