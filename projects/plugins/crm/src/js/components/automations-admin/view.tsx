3806516a754757d027a921e8cb44ef6b0367c0f8
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2023-07-13 19:39:00 -0700

    Use ThemeProvider

--- projects/plugins/crm/src/js/components/automations-admin/view.tsx
+++ projects/plugins/crm/src/js/components/automations-admin/view.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from '@automattic/jetpack-components';
 import * as WPElement from '@wordpress/element';
 import { AutomationsAdmin } from '.';
 
@@ -11,7 +12,12 @@ const render = () => {
 		return;
 	}
 
-	const component = <AutomationsAdmin />;
+	// @todo: Remove fallback when we drop support for WP 6.1
+	const component = (
+		<ThemeProvider>
+			<AutomationsAdmin />
+		</ThemeProvider>
+	);
 
 	if ( WPElement.createRoot ) {
 		WPElement.createRoot( container ).render( component );
