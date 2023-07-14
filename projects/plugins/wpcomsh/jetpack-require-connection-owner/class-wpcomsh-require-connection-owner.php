2d52e450849d99c8b2f2064712a7eaed2795c3f8
Sergey Mitroshin <sergeymitr@gmail.com>
2023-07-14 12:40:49 -0400

    Jetpack Connection: ignore invalid connection owner https://github.com/Automattic/wpcomsh/pull/1422
    Make Jetpack Dashboard show up even if connection owner is invalid.

--- projects/plugins/wpcomsh/jetpack-require-connection-owner/class-wpcomsh-require-connection-owner.php
+++ projects/plugins/wpcomsh/jetpack-require-connection-owner/class-wpcomsh-require-connection-owner.php
@@ -17,12 +17,12 @@ class WPCOMSH_Require_Connection_Owner {
 	/**
 	 * Filters the Jetpack::is_connection_ready to ensure a connectino owner is always needed
 	 *
-	 * @param bool                                  $is_conection_ready Is the connection ready?
+	 * @param bool                                  $is_connection_ready Is the connection ready?
 	 * @param Automattic\Jetpack\Connection\Manager $connection_manager Instance of the Manager class, can be used to check the connection status.
 	 * @return bool
 	 */
-	public static function filter_is_connection_ready( $is_conection_ready, $connection_manager ) { // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
-		return $connection_manager->has_connected_owner();
+	public static function filter_is_connection_ready( $is_connection_ready, $connection_manager ) { // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
+		return $connection_manager->has_connected_owner() || $connection_manager->is_user_connected();
 	}
 }
 add_filter( 'jetpack_is_connection_ready', array( 'WPCOMSH_Require_Connection_Owner', 'filter_is_connection_ready' ), 1000, 2 );
