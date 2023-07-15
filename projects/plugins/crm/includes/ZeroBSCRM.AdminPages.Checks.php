297f0c6d24116be5b51f0ce9f9a5379362884e6a
gogdzl <diego.rodrigues@automattic.com>
2023-07-15 14:11:22 -0300

    Add function to check if the page should be displayed in full width or not

--- projects/plugins/crm/includes/ZeroBSCRM.AdminPages.Checks.php
+++ projects/plugins/crm/includes/ZeroBSCRM.AdminPages.Checks.php
@@ -1024,3 +1024,35 @@ function jpcrm_is_settings_page() {
 	global $zbs;
 	return zeroBS_isPage( array( 'admin.php' ), false, array( $zbs->slugs['settings'] ) );
 }
+
+/**
+ * Checks if a page is designated as a full-width page in Jetpack CRM.
+ *
+ * @param string $page_name The name of the page to check (usually this is the http `page` GET param).
+ *
+ * @return bool Whether the page should be displayed in full width.
+ */
+function jpcrm_is_full_width_page( $page_name ) {
+	global $zbs;
+
+	if ( $zbs->settings->get( 'showfullwidthforlisting' ) !== 1 ) {
+		return false;
+	}
+
+	$full_width_pages = array(
+		'tag-manager',
+		'manage-customers',
+		'manage-companies',
+		'manage-segments',
+		'manage-quotes',
+		'manage-invoices',
+		'manage-quote-templates',
+		'manage-transactions',
+		'manage-events',
+		'manage-forms',
+	);
+
+	$full_width_pages = apply_filters( 'jetpack_crm_full_width_pages', $full_width_pages );
+
+	return in_array( $page_name, $full_width_pages, true );
+}
