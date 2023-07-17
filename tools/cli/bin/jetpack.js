afba341f4973c2e5b72da5fd4c546539e8796b3b
Brad Jorsch <brad.jorsch@automattic.com>
2023-07-17 12:20:32 -0400

    cli: Check node, php, and pnpm versions too
    And also better handle the case where the program is not installed.
    
    Also fix the handler for "forgot to `pnpm install`", it wasn't working
    since #23966 added some unguarded `import`s in the `bin/jetpack.js`
    entry point.

--- tools/cli/bin/jetpack.js
+++ tools/cli/bin/jetpack.js
@@ -2,7 +2,46 @@
 
 import process from 'process';
 import { fileURLToPath } from 'url';
-import { checkCliLocation, compareComposerVersion } from '../helpers/checkEnvironment.js';
+
+/**
+ * Async import with better error handling.
+ *
+ * @param {string} path - Path to import from.
+ * @returns {*} - Return from `import()`.
+ */
+async function guardedImport( path ) {
+	try {
+		return await import( path );
+	} catch ( error ) {
+		const identity = v => v;
+		const chalk = await import( 'chalk' ).then(
+			m => m.default,
+			() => ( {} )
+		);
+
+		if ( error.code === 'ERR_MODULE_NOT_FOUND' ) {
+			// if pnpm install hasn't been run, the import() will fail here.
+			console.error(
+				'Something is missing from your install. Please run `pnpm install` and try again.'
+			);
+
+			// Print the original error's message too, as sometimes the error isn't fixed by `pnpm install`.
+			console.error(
+				( chalk.grey || identity )( `The original error message was ${ error.message }` )
+			);
+		} else {
+			console.error( error );
+			console.error(
+				( chalk.bold || identity )( 'Something unexpected happened. See error above.' )
+			);
+		}
+		process.exit( 1 );
+	}
+}
+
+const { checkCliLocation, compareToolVersions } = await guardedImport(
+	'../helpers/checkEnvironment.js'
+);
 
 /**
  * Checks for executing the CLI within a different monorepo checkout.
@@ -16,10 +55,13 @@ try {
 }
 
 /**
- * Checks to make sure we're on the right version of composer.
+ * Checks to make sure we're on the right version of various tools.
  */
 try {
-	compareComposerVersion();
+	if ( ! ( await compareToolVersions() ) && ! process.env.JETPACK_CLI_IGNORE_VERSION_CHECKS ) {
+		console.error( '(Ignore the above errors by setting JETPACK_CLI_IGNORE_VERSION_CHECKS=1)' );
+		process.exit( 1 );
+	}
 } catch ( error ) {
 	console.error( error );
 	console.error( 'Something unexpected happened. See error above.' );
@@ -32,31 +74,5 @@ try {
 
 process.chdir( fileURLToPath( new URL( '../../..', import.meta.url ) ) );
 
-try {
-	const cliRouter = await import( '../cliRouter.js' );
-	cliRouter.cli();
-} catch ( error ) {
-	const identity = v => v;
-	const chalk = await import( 'chalk' ).then(
-		m => m.default,
-		() => ( {} )
-	);
-
-	if ( error.code === 'ERR_MODULE_NOT_FOUND' ) {
-		// if pnpm install hasn't been run, the import() will fail here.
-		console.error(
-			'Something is missing from your install. Please run `pnpm install` and try again.'
-		);
-
-		// Print the original error's message too, as sometimes the error isn't fixed by `pnpm install`.
-		console.error(
-			( chalk.grey || identity )( `The original error message was ${ error.message }` )
-		);
-	} else {
-		console.error( error );
-		console.error(
-			( chalk.bold || identity )( 'Something unexpected happened. See error above.' )
-		);
-	}
-	process.exit( 1 );
-}
+const cliRouter = await guardedImport( '../cliRouter.js' );
+cliRouter.cli();
