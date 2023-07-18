5dc32dec60dba36f880cd00ea61a203799c00b8c
Peter Petrov <peter.petrov89@gmail.com>
2023-07-18 14:24:29 +0300

    Remove critical css regen code, as it happens in the back-end now

--- projects/plugins/boost/app/assets/src/js/pages/purchase-success/PurchaseSuccess.svelte
+++ projects/plugins/boost/app/assets/src/js/pages/purchase-success/PurchaseSuccess.svelte
@@ -6,7 +6,6 @@
 	import ReactComponent from '../../elements/ReactComponent.svelte';
 	import TemplatedString from '../../elements/TemplatedString.svelte';
 	import { requestImageAnalysis } from '../../modules/image-size-analysis/store/isa-summary';
-	import { regenerateCriticalCss } from '../../stores/critical-css-state';
 	import { modulesState } from '../../stores/modules';
 	import Logo from '../../svg/jetpack-green.svg';
 	import externalLinkTemplateVar from '../../utils/external-link-template-var';
@@ -19,7 +18,6 @@
 	onMount( async () => {
 		// Enable cloud css module on upgrade.
 		$modulesState.cloud_css.active = true;
-		await regenerateCriticalCss();
 
 		// If image guide is enabled, request a new ISA report.
 		if ( $modulesState.image_guide.active ) {
