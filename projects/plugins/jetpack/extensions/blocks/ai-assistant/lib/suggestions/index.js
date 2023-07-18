35404a4918850798578607fadd6cb1156b7ac893
Damián Suárez <rdsuarez@gmail.com>
2023-07-18 14:07:50 -0300

    [not verified] remove askJetpack() lib

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/lib/suggestions/index.js
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/lib/suggestions/index.js
@@ -2,34 +2,6 @@
  * External dependencies
  */
 import { requestJwt, SuggestionsEventSource } from '@automattic/jetpack-ai-client';
-import debugFactory from 'debug';
-
-const debug = debugFactory( 'jetpack-ai-assistant' );
-
-/**
- *
- * askJetpack exists just for debugging purposes
- *
- * @param {string} question - The query to send to the API
- * @returns {string} The event source
- */
-export async function askJetpack( question ) {
-	let source;
-	try {
-		source = await askQuestion( question );
-	} catch ( err ) {
-		debug( 'Error', err );
-		return source;
-	}
-	source.addEventListener( 'error', err => {
-		debug( 'Error', err );
-	} );
-
-	source.addEventListener( 'suggestion', e => {
-		debug( 'fullMessage', e );
-	} );
-	return source;
-}
 
 /**
  * Leaving this here to make it easier to debug the streaming API calls for now
