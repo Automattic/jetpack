a1fc684bf69a35783e246afd4ef040557b72c67a
Mark George <thingalon@gmail.com>
2023-07-18 11:53:30 +1000

    Soften the language a bit

--- projects/plugins/boost/app/assets/src/js/elements/ImageCDNRecommendation.svelte
+++ projects/plugins/boost/app/assets/src/js/elements/ImageCDNRecommendation.svelte
@@ -1,24 +1,21 @@
 <script lang="ts">
-	import { __, sprintf } from '@wordpress/i18n';
+	import { __ } from '@wordpress/i18n';
 	import TemplatedString from './TemplatedString.svelte';
 </script>
 
 <p>
 	<TemplatedString
-		template={sprintf(
-			// translators: %s is Image CDN.
-			__(
-				'A lot of the reported issues can be easily fixed by enabling the %s feature.',
-				'jetpack-boost'
-			),
-			'<b>' + __( 'Image CDN', 'jetpack-boost' ) + '</b>'
-		)}
-		vars={{
-			b: [ 'strong', {}, '' ],
-		}}
+		template={__( 'We recommend enabling the <b>Image CDN</b>.', 'jetpack-boost' )}
 	/>
 </p>
-<p><small>{__( 'You`ll need to analyze again if you enable it.', 'jetpack-boost' )}</small></p>
+<p>
+	<small>
+		{__(
+			"Jetpack Boost's Image CDN can automatically resize many images to the correct size for you.",
+			'jetpack-boost'
+		)}
+	</small>
+</p>
 
 <style lang="scss">
 	p {
