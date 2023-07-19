4a75d2b7ec58a70f47aa49af060577fa996d1590
André Kallehauge <3846700+kallehauge@users.noreply.github.com>
2023-07-19 15:04:55 +0200

    Trying to match external "non-prettified" formatting
    Technically, third attempt.

--- projects/plugins/jetpack/modules/contact-form/js/dropdown.js
+++ projects/plugins/jetpack/modules/contact-form/js/dropdown.js
@@ -14,12 +14,12 @@ jQuery( function ( $ ) {
 
 	function initializeSelectMenu() {
 		$( '.contact-form .contact-form-dropdown' ).selectmenu( {
-				classes: {
-					'ui-selectmenu-button': 'contact-form-dropdown__button',
-					'ui-selectmenu-menu': 'contact-form-dropdown__menu',
-				},
-			} )
-			.attr( 'aria-hidden', true )
-			.prop( 'tabindex', -1 );
+			classes: {
+				'ui-selectmenu-button': 'contact-form-dropdown__button',
+				'ui-selectmenu-menu': 'contact-form-dropdown__menu',
+			},
+		} )
+		.attr( 'aria-hidden', true )
+		.prop( 'tabindex', -1 );
 	}
 } );
