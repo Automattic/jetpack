0956202def0f06ea3e03f31f67055070851926bd
André Kallehauge <3846700+kallehauge@users.noreply.github.com>
2023-07-19 14:42:35 +0200

    Move React Router to when we render the app itself
    The main reason we do this is to we can wrap our tests with <MemoryRouter> to gain more control in Jest.
    
    Resource: https://v5.reactrouter.com/web/guides/testing (it's v5 and not v6, but the same principles seems to apply)

--- projects/plugins/crm/src/js/components/automations-admin/index.tsx
+++ projects/plugins/crm/src/js/components/automations-admin/index.tsx
@@ -1,80 +1,77 @@
 import { AdminSection, Col, Container } from '@automattic/jetpack-components';
 import { __ } from '@wordpress/i18n';
 import React from 'react';
-import { createHashRouter, RouterProvider } from 'react-router-dom';
+import { Link, Routes, Route } from 'react-router-dom';
 import AdminPage from '../admin-page';
 import { RedirectHome } from './components/redirect-home';
 
 export const AutomationsAdmin = () => {
-	const router = createHashRouter( [
-		{
-			path: '/automations',
-			element: (
-				<AdminPage
-					headline={ __( 'Automations', 'zero-bs-crm' ) }
-					subHeadline={ __( 'Streamline your workflows with CRM Automations', 'zero-bs-crm' ) }
-				>
-					<AdminSection>
-						<Container horizontalSpacing={ 8 }>
-							<Col>
-								<div>This is the home page</div>
-								<br />
-								<a href="#/automations/edit">Go to edit</a>
-								<br />
-								<a href="#/automations/add">Go to add</a>
-							</Col>
-						</Container>
-					</AdminSection>
-				</AdminPage>
-			),
-		},
-		{
-			path: '/automations/add',
-			element: (
-				<AdminPage
-					headline={ __( 'Add Automation', 'zero-bs-crm' ) }
-					subHeadline={ __( 'Streamline your workflows with CRM Automations', 'zero-bs-crm' ) }
-				>
-					<AdminSection>
-						<Container horizontalSpacing={ 8 }>
-							<Col>
-								<div>This is the add page</div>
-								<br />
-								<a href="#/automations/edit">Go to edit</a>
-								<br />
-								<a href="#/automations">Go back to home</a>
-							</Col>
-						</Container>
-					</AdminSection>
-				</AdminPage>
-			),
-		},
-		{
-			path: '/automations/edit',
-			element: (
-				<AdminPage
-					headline={ __( 'Edit Automation', 'zero-bs-crm' ) }
-					subHeadline={ __( 'Streamline your workflows with CRM Automations', 'zero-bs-crm' ) }
-				>
-					<AdminSection>
-						<Container horizontalSpacing={ 8 }>
-							<Col>
-								<div>This is the edit page</div>
-								<br />
-								<a href="#/automations/add">Go to add</a>
-								<br />
-								<a href="#/automations">Go back to home</a>
-							</Col>
-						</Container>
-					</AdminSection>
-				</AdminPage>
-			),
-		},
-		{
-			path: '*',
-			element: <RedirectHome />,
-		},
-	] );
-
-	return <RouterProvider router={ router } />;
+	return (
+		<Routes>
+			<Route
+				path="/automations"
+				element={
+					<AdminPage
+						headline={ __( 'Automations', 'zero-bs-crm' ) }
+						subHeadline={ __( 'Streamline your workflows with CRM Automations', 'zero-bs-crm' ) }
+					>
+						<AdminSection>
+							<Container horizontalSpacing={ 8 }>
+								<Col>
+									<div>This is the home page</div>
+									<br />
+									<Link to={ '/automations/edit' }>Go to edit</Link>
+									<br />
+									<Link to={ '/automations/add' }>Go to add</Link>
+								</Col>
+							</Container>
+						</AdminSection>
+					</AdminPage>
+				}
+			/>
+			<Route
+				path="/automations/add"
+				element={
+					<AdminPage
+						headline={ __( 'Add Automation', 'zero-bs-crm' ) }
+						subHeadline={ __( 'Streamline your workflows with CRM Automations', 'zero-bs-crm' ) }
+					>
+						<AdminSection>
+							<Container horizontalSpacing={ 8 }>
+								<Col>
+									<div>This is the add page</div>
+									<br />
+									<Link to={ '/automations/edit' }>Go to edit</Link>
+									<br />
+									<Link to={ '/automations' }>Go back to home</Link>
+								</Col>
+							</Container>
+						</AdminSection>
+					</AdminPage>
+				}
+			/>
+			<Route
+				path="/automations/edit"
+				element={
+					<AdminPage
+						headline={ __( 'Edit Automation', 'zero-bs-crm' ) }
+						subHeadline={ __( 'Streamline your workflows with CRM Automations', 'zero-bs-crm' ) }
+					>
+						<AdminSection>
+							<Container horizontalSpacing={ 8 }>
+								<Col>
+									<div>This is the edit page</div>
+									<br />
+									<Link to={ '/automations/add' }>Go to add</Link>
+									<br />
+									<Link to={ '/automations' }>Go back to home</Link>
+								</Col>
+							</Container>
+						</AdminSection>
+					</AdminPage>
+				}
+			/>
+			<Route path="*" element={ <RedirectHome /> } />
+		</Routes>
+	);
 };
