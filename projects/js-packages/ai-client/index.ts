ce664919ed549be77f540778c684201a9bbb5efc
Damián Suárez <rdsuarez@gmail.com>
2023-07-19 14:44:05 -0300

    [not verified] expose useSuggestions() react custom hook

--- projects/js-packages/ai-client/index.ts
+++ projects/js-packages/ai-client/index.ts
@@ -1,4 +1,8 @@
 export { default as requestJwt } from './src/jwt';
 export { default as SuggestionsEventSource } from './src/suggestions-event-source';
 export { default as askQuestion } from './src/ask-question';
+
+// Hooks
+export { default as useSuggestions } from './src/hooks/use-suggestions';
+
 export * from './src/index.js';
