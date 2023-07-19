aef2f4e69a5b5c17745ece88a50a215f6460aa89
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2023-07-19 16:28:39 -0300

    AI Assistant: Add proofread component (#31963)
    * AI Assistant: Add flag for plugin sidebar
    
    * AI Assistant: Add new plugin sidebar component
    
    * AI Assistant: Register jetpack plugin
    
    * changelog
    
    * AI Assistant: Create ai-assistant plugin folder
    
    * [not verified] AI Assistant: Add proofread component
    
    * [not verified] AI Assistant: Insert Proofread component in sidebar
    
    * [not verified] Changelog
    
    * AI Assistant: Move component to plugin folder
    
    * AI Assistant: Fix mispelling toggle

--- projects/plugins/jetpack/extensions/plugins/ai-assistant/components/ai-assistant-plugin-sidebar/index.tsx
+++ projects/plugins/jetpack/extensions/plugins/ai-assistant/components/ai-assistant-plugin-sidebar/index.tsx
@@ -2,13 +2,14 @@
  * External dependencies
  */
 import { JetpackLogo } from '@automattic/jetpack-components';
-import { PanelBody } from '@wordpress/components';
+import { PanelBody, PanelRow } from '@wordpress/components';
 import { PluginPrePublishPanel } from '@wordpress/edit-post';
 import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
 import JetpackPluginSidebar from '../../../../shared/jetpack-plugin-sidebar';
+import Proofread from '../proofread';
 
 const isPluginSidebarVisible =
 	window?.Jetpack_Editor_Initial_State?.[ 'ai-assistant' ]?.[ 'is-plugin-sidebar-visible' ];
@@ -24,7 +25,9 @@ export default function AiAssistantPluginSidebar() {
 		<>
 			<JetpackPluginSidebar>
 				<PanelBody title={ title } initialOpen={ false }>
-					<p>AI Assistant is a tool that helps you write better content.</p>
+					<PanelRow>
+						<Proofread />
+					</PanelRow>
 				</PanelBody>
 			</JetpackPluginSidebar>
 			<PluginPrePublishPanel
@@ -32,7 +35,7 @@ export default function AiAssistantPluginSidebar() {
 				icon={ <JetpackLogo showText={ false } height={ 16 } logoColor="#1E1E1E" /> }
 				initialOpen={ false }
 			>
-				<p>AI Assistant is a tool that helps you write better content.</p>
+				<Proofread />
 			</PluginPrePublishPanel>
 		</>
 	);
