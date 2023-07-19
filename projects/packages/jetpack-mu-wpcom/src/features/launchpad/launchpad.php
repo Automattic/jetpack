99161ff589208ac44fb24a4218b808d8093f9596
sixhours <calobee@gmail.com>
2023-07-19 17:14:00 -0400

    [not verified] Don't rename the wpcom_launchpad_is_keep_building_enabled function yet; we use this on WP.com

--- projects/packages/jetpack-mu-wpcom/src/features/launchpad/launchpad.php
+++ projects/packages/jetpack-mu-wpcom/src/features/launchpad/launchpad.php
@@ -141,7 +141,7 @@ function wpcom_launchpad_get_task_list_definitions() {
 				'edit_page',
 				'share_site',
 			),
-			'is_enabled_callback' => 'wpcom_launchpad_is_intent_build_enabled',
+			'is_enabled_callback' => 'wpcom_launchpad_is_keep_building_enabled',
 		),
 		'intent-build'    => array(
 			'title'               => 'Keep Building',
@@ -155,7 +155,7 @@ function wpcom_launchpad_get_task_list_definitions() {
 				'edit_page',
 				'share_site',
 			),
-			'is_enabled_callback' => 'wpcom_launchpad_is_intent_build_enabled',
+			'is_enabled_callback' => 'wpcom_launchpad_is_keep_building_enabled',
 		),
 		'intent-write'    => array(
 			'title'               => 'Blog',
@@ -589,7 +589,7 @@ function wpcom_get_launchpad_task_list_is_enabled( $checklist_slug ) {
  *
  * @return bool True if the task list is enabled, false otherwise.
  */
-function wpcom_launchpad_is_intent_build_enabled() {
+function wpcom_launchpad_is_keep_building_enabled() {
 	$intent                  = get_option( 'site_intent', false );
 	$launchpad_task_statuses = get_option( 'launchpad_checklist_tasks_statuses', array() );
 
