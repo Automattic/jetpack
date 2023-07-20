39e186249b854449eeb8325da94b70476a54ee8d
Erick Danzer <edanzer@gmail.com>
2023-07-20 10:29:11 -0600

    Launchpad: Update initial state of subscribers task (#31947)

--- projects/packages/jetpack-mu-wpcom/src/features/launchpad/launchpad-task-definitions.php
+++ projects/packages/jetpack-mu-wpcom/src/features/launchpad/launchpad-task-definitions.php
@@ -129,7 +129,7 @@ function wpcom_launchpad_get_task_definitions() {
 			'get_title'            => function () {
 				return __( 'Add subscribers', 'jetpack-mu-wpcom' );
 			},
-			'is_complete_callback' => '__return_true',
+			'is_complete_callback' => 'wpcom_is_task_option_completed',
 			'is_visible_callback'  => 'wpcom_has_goal_import_subscribers',
 		),
 
