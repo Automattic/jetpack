2894283be3e767cff5655f89ad6bc610665119b2
kthai15 <kavin.keratikasama@automattic.com>
2023-07-19 17:12:56 -0700

    [not verified] add functionality to send and receive messages

--- projects/plugins/chat/src/js/components/chatform.jsx
+++ projects/plugins/chat/src/js/components/chatform.jsx
@@ -6,33 +6,37 @@ import {
 	MessageInput,
 } from '@chatscope/chat-ui-kit-react';
 import chatStyles from '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
-import { client } from '@xmpp/client';
+import { client, xml } from '@xmpp/client';
 import React, { useState, useEffect } from 'react';
 
 const xmpp = client( {
-	service: 'wss://localhost:5443/ws', // TODO: Replace with actual XMPP server (use ws instead of wss for local testing if your server doesn't have TLS enabled)
+	service: 'ws://localhost:5443/ws', // TODO: Replace with actual XMPP server (use ws instead of wss for local testing if your server doesn't have TLS enabled)
 	domain: 'localhost',
-	username: 'admin', // TODO: Replace with actual username
+	username: 'kthai15@localhost', // TODO: Replace with actual username
 	password: 'password',
 } );
 
 const ChatForm = () => {
 	const [ messages, setMessages ] = useState( [] );
-	const [ connected, setConnected ] = useState( false );
+	const [ botJid, setBotJid ] = useState( '' );
 
-	const handleSend = text => {
-		console.log( 'Sending message', text );
+	const handleSend = async text => {
 		// TODO: Send message to XMPP server and update messages state
-		const response = `You said: ${ text }`;
-		setMessages( [ ...messages, { text, isOwn: true }, { text: response, isOwn: false } ] );
+		const message = xml( 'message', { type: 'chat', to: botJid }, xml( 'body', {}, text ) );
+		await xmpp.send( message );
+
+		setMessages( prevMessages => [ ...prevMessages, { text, isOwn: true } ] );
 	};
 
-	const handleClear = () => {
-		setMessages( [] );
-	};
-
-	const handleConnect = () => {
-		xmpp.start().catch( err => console.error( err ) );
+	useEffect( () => {
+		/**
+		 *
+		 */
+		function disconnectXMPP() {
+			xmpp.send( xml( 'presence', { type: 'unavailable' } ) );
+			xmpp.stop().catch( console.error );
+		}
+		window.addEventListener( 'beforeunload', disconnectXMPP );
 
 		xmpp.on( 'error', err => {
 			console.error( err );
@@ -40,41 +44,37 @@ const ChatForm = () => {
 		xmpp.on( 'online', async jid => {
 			console.log( 'online as', jid.toString() );
 
+			// Makes itself available
+			await xmpp.send( xml( 'presence' ) );
+
 			const chat_request = { jid: jid.toString() };
-			const response = await fetch(
-				'https://public-api.wordpress.com/wpcom/v2/odysseus/start_chat',
-				{
-					method: 'POST',
-					body: JSON.stringify( chat_request ),
-					headers: {
-						'Content-Type': 'application/json',
-					},
-				}
-			);
+			const response = await fetch( 'https://public-api.wordpress.com/wpcom/v2/odie/start_chat', {
+				method: 'POST',
+				body: JSON.stringify( chat_request ),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			} );
 			console.log( 'response', response );
 		} );
 		xmpp.on( 'stanza', stanza => {
 			if ( stanza.is( 'message' ) && stanza.getChild( 'body' ) ) {
+				setBotJid( stanza.attrs.from );
+
 				const message = stanza.getChild( 'body' ).getText();
-				setMessages( [ ...messages, { text: message, isOwn: false } ] );
+				setMessages( prevMessages => [ ...prevMessages, { text: message, isOwn: false } ] );
 			}
 		} );
 		xmpp.on( 'offline', () => {
 			console.log( 'offline' );
 		} );
-	};
 
-	useEffect( () => {
-		if ( xmpp.status === 'online' ) {
-			setConnected( true );
-		} else {
-			setConnected( false );
-		}
-	}, [ xmpp.status ] );
+		// Start the XMPP client
+		xmpp.start().catch( console.error );
+	}, [] );
 
 	return (
 		<div>
-			{ ! connected && <button onClick={ handleConnect }>Connect to XMPP Server</button> }
 			<h3>Chat:</h3>
 			<MainContainer>
 				<ChatContainer>
@@ -100,7 +100,6 @@ const ChatForm = () => {
 					/>
 				</ChatContainer>
 			</MainContainer>
-			<button onClick={ handleClear }>Clear Chat</button>
 		</div>
 	);
 };
