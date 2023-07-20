d97a4df211024387a534ac21214afb9c3bf13ef3
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2023-07-20 17:41:58 -0300

    AI Assistant: Add it as experimental

--- projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/index.js
+++ projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/index.js
@@ -1,11 +1,10 @@
 /**
  * Internal dependencies
  */
-import { name } from '../../blocks/ai-assistant';
 import AiAssistantPluginSidebar from './components/ai-assistant-plugin-sidebar';
 
 // plugin settings
-export { name };
+export const name = 'ai-assistant-plugin';
 export const settings = {
 	render: AiAssistantPluginSidebar,
 };
