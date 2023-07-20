005f769c22ebcaf8005992d2c4fd86d5b7857aba
okmttdhr <okmttdhr@users.noreply.github.com>
2023-07-20 14:45:18 +0900

    Run load_block_theme_previews

--- projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
+++ projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
@@ -32,8 +32,10 @@ class Jetpack_Mu_Wpcom {
 
 		// Coming Soon feature.
 		add_action( 'plugins_loaded', array( __CLASS__, 'load_coming_soon' ) );
+
 		add_action( 'plugins_loaded', array( __CLASS__, 'load_wpcom_rest_api_endpoints' ) );
 		add_action( 'plugins_loaded', array( __CLASS__, 'load_launchpad' ), 0 );
+		add_action( 'plugins_loaded', array( __CLASS__, 'load_block_theme_previews' ) );
 
 		// Unified navigation fix for changes in WordPress 6.2.
 		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'unbind_focusout_on_wp_admin_bar_menu_toggle' ) );
