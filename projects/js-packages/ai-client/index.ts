01203717bc59a8febe4e64304b292275d3c8cf3a
Damián Suárez <rdsuarez@gmail.com>
2023-07-25 18:31:54 -0300

    AI Client: add useSuggestions() hook (#32022)
    * [not verified] define const and types for response errors
    
    * [not verified] introduce useSuggestions react custom hook
    
    * [not verified] expose useSuggestions() react custom hook
    
    * [not verified] add .eslintrc.cjs file
    
    * [not verified] organize errors stuff
    
    * [not verified] use alias to simplify the code
    
    * [not verified] add askQuestionOptions to hook
    
    * [not verified] rename hook name
    
    * [not verified] add hook readme file
    
    * [not verified] changelog
    
    * [not verified] add @wordpress deps
    
    * [not verified] set `autoRequest` as false by default
    
    * [not verified] use conts to register/unregisters events
    
    * update deps
    
    * update pnpm deps
    
    * bump new ai-client version
    
    * add react deps to ai client pkg
    
    * Update projects/js-packages/ai-client/src/hooks/use-ai-suggestions/index.ts
    
    Co-authored-by: Luiz Kowalski <lhkowalski@gmail.com>
    
    * Update projects/js-packages/ai-client/src/hooks/use-ai-suggestions/Readme.md
    
    Co-authored-by: Luiz Kowalski <lhkowalski@gmail.com>
    
    * set postId an optional option for the hook
    
    * add debug() line to check postId param
    
    * Fix typo on parameter comment
    
    ---------
    
    Co-authored-by: Luiz Kowalski <lhkowalski@gmail.com>

--- projects/js-packages/ai-client/index.ts
+++ projects/js-packages/ai-client/index.ts
@@ -1,4 +1,8 @@
 export { default as requestJwt } from './src/jwt';
 export { default as SuggestionsEventSource } from './src/suggestions-event-source';
 export { default as askQuestion } from './src/ask-question';
+
+// Hooks
+export { default as useAiSuggestions } from './src/hooks/use-ai-suggestions';
+
 export * from './src/index.js';
