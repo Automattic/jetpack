1b1699ea5ec94055c0cc39413eddecfe21332f15
Miguel Lezama <lezama@gmail.com>
2023-07-26 22:39:47 -0300

    copy "supports" from "More" block

--- projects/plugins/jetpack/extensions/blocks/paywall/index.js
+++ projects/plugins/jetpack/extensions/blocks/paywall/index.js
@@ -21,26 +21,9 @@ export const settings = {
 	category: 'grow',
 	keywords: [],
 	supports: {
-		// Support for block's alignment (left, center, right, wide, full). When true, it adds block controls to change block’s alignment.
-		align: false /* if set to true, the 'align' option below can be used*/,
-		// Pick which alignment options to display.
-		/*align: [ 'left', 'right', 'full' ],*/
-		// Support for wide alignment, that requires additional support in themes.
-		alignWide: true,
-		// When true, a new field in the block sidebar allows to define an id for the block and a button to copy the direct link.
-		anchor: false,
-		// When true, a new field in the block sidebar allows to define a custom className for the block’s wrapper.
-		customClassName: true,
-		// When false, Gutenberg won't add a class like .wp-block-your-block-name to the root element of your saved markup
-		className: true,
-		// Setting this to false suppress the ability to edit a block’s markup individually. We often set this to false in Jetpack blocks.
+		customClassName: false,
 		html: false,
-		// Passing false hides this block in Gutenberg's visual inserter.
-		/*inserter: true,*/
-		// When false, user will only be able to insert the block once per post.
-		multiple: true,
-		// When false, the block won't be available to be converted into a reusable block.
-		reusable: true,
+		multiple: false,
 	},
 	edit,
 	/* @TODO Write the block editor output */
