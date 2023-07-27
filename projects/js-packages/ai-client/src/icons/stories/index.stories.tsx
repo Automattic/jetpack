3e4ab13076b52c0aa76db501af3dc907b822799b
Damián Suárez <rdsuarez@gmail.com>
2023-07-27 18:07:20 +0100

    [not verified] update story importing path

--- projects/js-packages/ai-client/src/icons/stories/index.stories.tsx
+++ projects/js-packages/ai-client/src/icons/stories/index.stories.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 /**
  * Internal dependencies
  */
-import * as allIcons from '../index.ts';
+import * as allIcons from '../index';
 import styles from './style.module.scss';
 /**
  * Types
