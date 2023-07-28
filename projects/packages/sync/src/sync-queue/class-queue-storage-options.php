52090c0ddb7f6a0d89fab67ed3644cf0a042cf7a
Biser Perchinkov <biser@bisko.be>
2023-07-28 14:00:42 +0300

    Check for empty input parameters before trying to work with them.

--- projects/packages/sync/src/sync-queue/class-queue-storage-options.php
+++ projects/packages/sync/src/sync-queue/class-queue-storage-options.php
@@ -267,6 +267,11 @@ class Queue_Storage_Options {
 	 */
 	public function delete_items_by_ids( $ids ) {
 		global $wpdb;
+
+		if ( ! is_array( $ids ) || empty( $ids ) ) {
+			return false;
+		}
+
 		// TODO check if it's working properly - no need to delete all options in the table if the params are not right
 		$ids_placeholders = implode( ', ', array_fill( 0, count( $ids ), '%s' ) );
 
