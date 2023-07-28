855d73caa8805d9791d95906314f26109851528b
cleacos <javacodia@gmail.com>
2023-07-28 18:01:14 +0100

    [not verified] Add some contact Event Manager unit tests
    Signed-off-by: cleacos <javacodia@gmail.com>

---
+++ projects/plugins/crm/tests/php/event-manager/class-event-manager-faker.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Automattic\Jetpack\CRM\Event_Manager\Tests;
+
+
+class Event_Manager_Faker {
+
+	private static $instance;
+
+	public static function instance(): Event_Manager_Faker {
+		if ( ! self::$instance ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	public function contact_data() {
+		return array(
+			'id'          => 1,
+			'status'      => 'Lead',
+			'name'        => 'John Doe',
+			'email'       => 'johndoe@example.com',
+			'lastupdated' => 1690385165,
+		);
+	}
+
+	public function invoice_data() {
+		return array(
+			'id'   => 1,
+			'data' => array(
+				'id_override' => '1',
+				'parent'      => '',
+				'status'      => 'Unpaid',
+				'hash'        => 'ISSQndSUjlhJ8feWj2v',
+				'lineitems'   => array(
+					array(
+						'net'      => 3.75,
+						'desc'     => 'Dummy product',
+						'quantity' => '3',
+						'price'    => '1.25',
+						'total'    => 3.75,
+					),
+				),
+				'contacts'    => array( 1 ),
+				'created'     => -1,
+			),
+		);
+	}
+
+	/**
+	 * Return data for a dummy company
+	 * @return array
+	 */
+	public function company_data() {
+		return array(
+			'id'     => 1,
+			'name'   => 'Dummy Company',
+			'email'  => 'johndoe@dummycompany.com',
+			'status' => 'lead',
+		);
+	}
+}
---
+++ projects/plugins/crm/tests/php/event-manager/class-event-manager-test.php
@@ -0,0 +1,75 @@
+<?php // phpcs:ignore WordPress.Files.FileName.InvalidClassFileName
+
+namespace Automattic\Jetpack\CRM\Event_Manager\Tests;
+
+use Automattic\Jetpack\CRM\Event_Manager\Contact_Event;
+use WorDBless\BaseTestCase;
+
+require_once __DIR__ . '/class-event-manager-faker.php';
+
+/**
+ * Test Event Manager system
+ *
+ * @covers Automattic\Jetpack\CRM\Event_Manager
+ */
+class Event_Manager_Test extends BaseTestCase {
+
+	public function setUp(): void {
+		parent::setUp();
+	}
+
+	/**
+	 * @testdox Test that contact created event is fired
+	 */
+	public function test_notify_on_contact_created() {
+		
+		$contact_data = Event_Manager_Faker::instance()->contact_data();
+		
+		add_action( 'jpcrm_contact_created', function( $contact ) use ( $contact_data ) {
+			$this->assertEquals( $contact, $contact_data );
+		}, 10, 1 );
+
+		$contact_event = new Contact_Event();
+		
+		$contact_event->created( $contact_data );
+	}
+
+	/**
+	 * @testdox Test that contact status updated event is fired
+	 */
+	public function test_notify_on_contact_status_updated() {
+
+		$contact_data = Event_Manager_Faker::instance()->contact_data();
+		
+		$contact_updated           = $contact_data;
+		$contact_updated['status'] = 'Customer';
+
+		add_action( 'jpcrm_contact_status_updated', function( $contact, $old_status_value ) {
+			$this->assertEquals( $contact['status'], 'Customer' );
+			$this->assertEquals( $old_status_value, 'Lead' );
+		}, 10, 2 );
+
+		$contact_event = new Contact_Event();
+
+		$contact_event->updated( $contact_updated, $contact_data );
+	}
+
+	/**
+	 * @testdox Test that contact updated event is fired
+	 */
+	public function test_notify_on_contact_updated() {
+
+		$contact_data = Event_Manager_Faker::instance()->contact_data();
+
+		$contact_updated           = $contact_data;
+		$contact_updated['status'] = 'Customer';
+
+		add_action( 'jpcrm_contact_updated', function( $contact ) {
+			$this->assertEquals( $contact['status'], 'Customer' );
+		} );
+
+		$contact_event = new Contact_Event();
+
+		$contact_event->updated( $contact_updated, $contact_data );
+	}
+}
