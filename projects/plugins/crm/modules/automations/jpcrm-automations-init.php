30b2826cbbdce4a6e5503bb766f9da9a94a34d3c
André Kallehauge <3846700+kallehauge@users.noreply.github.com>
2023-07-28 16:19:54 +0200

    Only load automations in the backend

--- projects/plugins/crm/modules/automations/jpcrm-automations-init.php
+++ projects/plugins/crm/modules/automations/jpcrm-automations-init.php
@@ -49,8 +49,11 @@ function load_module() {
 	require_once JPCRM_AUTOMATIONS_MODULE_PATH . '/admin/admin-page-init.php';
 	initialize_admin_page();
 
-	$bootstrap = new Automation_Boostrap();
-	$bootstrap->init();
+	/* @todo Refactor this to be part of a separate initialisation process. */
+	if ( is_admin() ) {
+		$bootstrap = new Automation_Boostrap();
+		$bootstrap->init();
+	}
 }
 
 add_action( 'jpcrm_load_modules', __NAMESPACE__ . '\load_module' );
