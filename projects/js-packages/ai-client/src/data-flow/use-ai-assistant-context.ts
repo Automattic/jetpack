0cafee0afa46e249192d0cf1744e4ab940ee7838
Damián Suárez <rdsuarez@gmail.com>
2023-07-31 07:13:54 +0100

    [not verified] first implementation of the useAiDataContext() hook

---
+++ projects/js-packages/ai-client/src/data-flow/use-ai-assistant-context.ts
@@ -0,0 +1,72 @@
+/**
+ * External dependencies
+ */
+import { useCallback, useContext, useEffect } from '@wordpress/element';
+/**
+ * Internal dependencies
+ */
+import { AiDataContext } from '.';
+/**
+ * Types & constants
+ */
+import type { AiDataContextProps } from './context';
+import type { AskQuestionOptionsArgProps } from '../ask-question';
+
+type UseAiAssistantContextOptions = {
+	/*
+	 * Ask question options.
+	 */
+	askQuestionOptions?: AskQuestionOptionsArgProps;
+	/*
+	 * onDone callback.
+	 */
+	onDone?: ( content: string ) => void;
+
+	/*
+	 * onSuggestion callback.
+	 */
+	onSuggestion?: ( suggestion: string ) => void;
+};
+
+/**
+ * useAiDataContext hook to provide access to
+ * the AI Assistant data (from context),
+ * and to subscribe to the request events (onDone, onSuggestion).
+ *
+ * @param {UseAiAssistantContextOptions} options - the hook options.
+ * @returns {AiDataContextProps}          the AI Assistant data context.
+ */
+export default function useAiDataContext( {
+	onDone,
+	onSuggestion,
+}: UseAiAssistantContextOptions = {} ): AiDataContextProps {
+	const context = useContext( AiDataContext );
+	const { eventSource } = context;
+
+	const done = useCallback( ( event: CustomEvent ) => onDone?.( event?.detail ), [ onDone ] );
+	const suggestion = useCallback(
+		( event: CustomEvent ) => onSuggestion?.( event?.detail ),
+		[ onSuggestion ]
+	);
+
+	useEffect( () => {
+		if ( ! eventSource ) {
+			return;
+		}
+
+		if ( onDone ) {
+			eventSource.addEventListener( 'done', done );
+		}
+
+		if ( onSuggestion ) {
+			eventSource.addEventListener( 'suggestion', suggestion );
+		}
+
+		return () => {
+			eventSource.removeEventListener( 'done', done );
+			eventSource.removeEventListener( 'suggestion', suggestion );
+		};
+	}, [ eventSource ] );
+
+	return context;
+}
