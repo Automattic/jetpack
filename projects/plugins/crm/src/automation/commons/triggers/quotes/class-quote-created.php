0deacb40a81ab6524d0e5f186d26a99ba942441c
Clemen <javacodia@gmail.com>
2023-07-31 23:26:15 +0100

    Update/crm/3266 rename trigger hooks and classes (#32158)
    * Change _new by _created in hooks and class names
    
    Signed-off-by: cleacos <javacodia@gmail.com>
    
    * changelog
    
    ---------
    
    Signed-off-by: cleacos <javacodia@gmail.com>

--- projects/plugins/crm/src/automation/commons/triggers/quotes/class-quote-created.php
+++ projects/plugins/crm/src/automation/commons/triggers/quotes/class-quote-created.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Jetpack CRM Automation Company_New trigger.
+ * Jetpack CRM Automation Quote_Created trigger.
  *
  * @package automattic/jetpack-crm
  */
@@ -10,44 +10,48 @@ namespace Automattic\Jetpack\CRM\Automation\Triggers;
 use Automattic\Jetpack\CRM\Automation\Base_Trigger;
 
 /**
- * Adds the Company_New class.
+ * Adds the Quote_Created class.
  */
-class Company_New extends Base_Trigger {
+class Quote_Created extends Base_Trigger {
 
-	/** Get the slug name of the trigger
+	/**
+	 * Get the slug name of the trigger.
 	 * @return string
 	 */
 	public static function get_slug(): string {
-		return 'jpcrm/company_new';
-	}
-
-	/** Get the title of the trigger
-	 * @return string
-	 */
-	public static function get_title(): ?string {
-		return __( 'New Company', 'zero-bs-crm' );
-	}
-
-	/** Get the description of the trigger
-	 * @return string
-	 */
-	public static function get_description(): ?string {
-		return __( 'Triggered when a CRM company is added', 'zero-bs-crm' );
-	}
-
-	/** Get the category of the trigger
-	 * @return string
-	 */
-	public static function get_category(): ?string {
-		return __( 'company', 'zero-bs-crm' );
+		return 'jpcrm/quote_created';
 	}
 
 	/**
-	 * Listen to the desired event
+	 * Get the title of the trigger.
+	 * @return string
+	 */
+	public static function get_title(): ?string {
+		return __( 'New Quote', 'zero-bs-crm' );
+	}
+
+	/**
+	 * Get the description of the trigger.
+	 * @return string
+	 */
+	public static function get_description(): ?string {
+		return __( 'Triggered when a new quote status is added', 'zero-bs-crm' );
+	}
+
+	/**
+	 * Get the category of the trigger.
+	 * @return string
+	 */
+	public static function get_category(): ?string {
+		return __( 'quote', 'zero-bs-crm' );
+	}
+
+	/**
+	 * Listen to this trigger's target event.
 	 */
 	protected function listen_to_event() {
 		add_action(
-			'jpcrm_automation_company_new',
+			'jpcrm_quote_created',
 			array( $this, 'execute_workflow' )
 		);
 	}
