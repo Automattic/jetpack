626e9f87bf44fd7d64844cd19436eb5bcc9ff69a
Miguel Lezama <lezama@gmail.com>
2023-08-01 11:32:33 -0300

    [not verified] paywall only for posts

--- projects/plugins/jetpack/extensions/blocks/paywall/editor.js
+++ projects/plugins/jetpack/extensions/blocks/paywall/editor.js
@@ -1,4 +1,19 @@
 import registerJetpackBlock from '../../shared/register-jetpack-block';
 import { name, settings } from '.';
 
+/* global wp */
+const unsubscribe = wp.data.subscribe( () => {
+	const postType = wp.data.select( 'core/editor' ).getCurrentPostType();
+
+	// If postType is still not available, simply return and wait for the next call.
+	if ( postType === null ) {
+		return;
+	}
+	unsubscribe();
+	// If postType is defined and not 'post', unregister the block.
+	if ( postType && postType !== 'post' ) {
+		wp.blocks.unregisterBlockType( 'jetpack/' + name );
+	}
+} );
+
 registerJetpackBlock( name, settings );
