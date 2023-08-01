3965db1a29945fb68757a126128e2c503ffb9053
Luiz Kowalski <lhkowalski@gmail.com>
2023-08-01 18:19:03 -0300

    [not verified] Add initial version of AIControl to form extension

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/jetpack-contact-form/components/ai-assistant-popover/index.tsx
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/jetpack-contact-form/components/ai-assistant-popover/index.tsx
@@ -1,8 +1,8 @@
 /**
  * External dependencies
  */
-import { useAiContext } from '@automattic/jetpack-ai-client';
-import { Button, KeyboardShortcuts, Popover, TextControl } from '@wordpress/components';
+import { useAiContext, AIControl } from '@automattic/jetpack-ai-client';
+import { KeyboardShortcuts, Popover } from '@wordpress/components';
 import { useContext } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 /**
@@ -29,11 +29,28 @@ export const AiAssistantPopover = () => {
 
 	const { requestSuggestion, requestingState } = useAiContext();
 
-	const isDisabled = requestingState === 'requesting' || requestingState === 'suggesting';
+	const isLoading = requestingState === 'requesting' || requestingState === 'suggesting';
 
 	if ( ! isVisible ) {
 		return null;
 	}
+
+	const onStop = () => {
+		alert( 'onStop' );
+	};
+
+	const onAccept = () => {
+		alert( 'onAccept' );
+	};
+
+	const onSend = () => {
+		const prompt = getPrompt( PROMPT_TYPE_JETPACK_FORM_CUSTOM_PROMPT, {
+			request: inputValue,
+			content: '',
+		} );
+
+		requestSuggestion( prompt );
+	};
 
 	return (
 		<Popover { ...popoverProps } className="jetpack-ai-assistant__popover">
@@ -44,22 +61,15 @@ export const AiAssistantPopover = () => {
 				} }
 			>
 				<div className="jetpack-ai-assistant__popover-container">
-					<TextControl onChange={ setInputValue } value={ inputValue } disabled={ isDisabled } />
-
-					<Button
-						variant="primary"
-						onClick={ () => {
-							const prompt = getPrompt( PROMPT_TYPE_JETPACK_FORM_CUSTOM_PROMPT, {
-								request: inputValue,
-								content: '',
-							} );
-
-							requestSuggestion( prompt );
-						} }
-						disabled={ isDisabled }
-					>
-						{ __( 'Ask', 'jetpack' ) }
-					</Button>
+					<AIControl
+						loading={ isLoading }
+						value={ inputValue }
+						placeholder={ __( 'Which form do you need?', 'jetpack' ) }
+						onChange={ setInputValue }
+						onSend={ onSend }
+						onStop={ onStop }
+						onAccept={ onAccept }
+					/>
 				</div>
 			</KeyboardShortcuts>
 		</Popover>
