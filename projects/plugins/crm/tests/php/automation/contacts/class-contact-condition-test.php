4c0afccfa6425db80f13ced5622d3a02ec17e763
gogdzl <diego.rodrigues@automattic.com>
2023-08-01 16:20:57 -0300

    Add test cases for Contact_Status_Changed condition

---
+++ projects/plugins/crm/tests/php/automation/contacts/class-contact-condition-test.php
@@ -0,0 +1,87 @@
+<?php // phpcs:ignore WordPress.Files.FileName.InvalidClassFileName
+
+namespace Automattic\Jetpack\CRM\Automation\Tests;
+
+use Automattic\Jetpack\CRM\Automation\Automation_Exception;
+use Automattic\Jetpack\CRM\Automation\Conditions\Contact_Status_Changed;
+use WorDBless\BaseTestCase;
+
+require_once __DIR__ . '../../tools/class-automation-faker.php';
+
+/**
+ * Test Automation Workflow functionalities
+ *
+ * @covers Automattic\Jetpack\CRM\Automation
+ */
+class Contact_Condition_Test extends BaseTestCase {
+
+	private $automation_faker;
+
+	public function setUp(): void {
+		parent::setUp();
+		$this->automation_faker = Automation_Faker::instance();
+		$this->automation_faker->reset_all();
+	}
+
+	private function get_contact_status_changed_condition( $operator, $expected_value ) {
+		$condition_data = array(
+			'slug'       => 'jpcrm/condition/contact_status_changed',
+			'attributes' => array(
+				'field'    => 'status',
+				'operator' => $operator,
+				'value'    => $expected_value,
+			),
+		);
+		return new Contact_Status_Changed( $condition_data );
+	}
+
+	/**
+	 * @testdox Test the update contact status condition for the is operator.
+	 */
+	public function test_status_changed_is_operator() {
+		$contact_status_changed_condition = $this->get_contact_status_changed_condition( 'is', 'customer' );
+		$contact_data                     = $this->automation_faker->contact_data();
+
+		// Testing when the condition has been met.
+		$contact_data['data']['status'] = 'customer';
+		$contact_status_changed_condition->execute( $contact_data );
+		$this->assertTrue( $contact_status_changed_condition->condition_met() );
+
+		// Testing when the condition has not been met.
+		$contact_data['data']['status'] = 'lead';
+		$contact_status_changed_condition->execute( $contact_data );
+		$this->assertFalse( $contact_status_changed_condition->condition_met() );
+	}
+
+	/**
+	 * @testdox Test the update contact status condition for the is_not operator.
+	 */
+	public function test_status_changed_is_not_operator() {
+		$contact_status_changed_condition = $this->get_contact_status_changed_condition( 'is_not', 'customer' );
+		$contact_data                     = $this->automation_faker->contact_data();
+
+		// Testing when the condition has been met.
+		$contact_data['data']['status'] = 'lead';
+		$contact_status_changed_condition->execute( $contact_data );
+		$this->assertTrue( $contact_status_changed_condition->condition_met() );
+
+		// Testing when the condition has not been met.
+		$contact_data['data']['status'] = 'customer';
+		$contact_status_changed_condition->execute( $contact_data );
+		$this->assertFalse( $contact_status_changed_condition->condition_met() );
+	}
+
+	/**
+	 * @testdox Test if an exception is being correctly thrown for wrong operators.
+	 */
+	public function test_status_changed_invalid_operator_throws_exception() {
+		$contact_status_changed_condition = $this->get_contact_status_changed_condition( 'wrong_operator', 'customer' );
+		$contact_data                     = $this->automation_faker->contact_data();
+
+		$this->expectException( Automation_Exception::class );
+		$this->expectExceptionMessage( 'Invalid operator: wrong_operator' );
+
+		$contact_status_changed_condition->execute( $contact_data );
+	}
+
+}
