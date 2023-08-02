6d035325cd1faf007cc7a60cb4493ce809e103e8
dereksmart <smart@automattic.com>
2023-08-02 17:26:29 -0400

    Add new chat plugin that uses the chatbot package.

---
+++ projects/plugins/chat/jetpack-chat.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ *
+ * Plugin Name: Jetpack Chat
+ * Plugin URI: https://wordpress.org/plugins/jetpack-chat
+ * Description: Plugin example for Chatbot functionality.
+ * Version: 0.3.1-alpha
+ * Author: Automattic
+ * Author URI: https://jetpack.com/
+ * License: GPLv2 or later
+ * Text Domain: jetpack-chat
+ *
+ * @package automattic/jetpack-chat
+ */
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+define( 'JETPACK_CHAT_DIR', plugin_dir_path( __FILE__ ) );
+define( 'JETPACK_CHAT_ROOT_FILE', __FILE__ );
+define( 'JETPACK_CHAT_ROOT_FILE_RELATIVE_PATH', plugin_basename( __FILE__ ) );
+define( 'JETPACK_CHAT_SLUG', 'jetpack-chat' );
+define( 'JETPACK_CHAT_NAME', 'Jetpack Chat' );
+define( 'JETPACK_CHAT_URI', 'https://jetpack.com/jetpack-chat' );
+define( 'JETPACK_CHAT_FOLDER', dirname( plugin_basename( __FILE__ ) ) );
+
+// Jetpack Autoloader.
+$jetpack_autoloader = JETPACK_CHAT_DIR . 'vendor/autoload_packages.php';
+if ( is_readable( $jetpack_autoloader ) ) {
+	require_once $jetpack_autoloader;
+	if ( method_exists( \Automattic\Jetpack\Assets::class, 'alias_textdomains_from_file' ) ) {
+		\Automattic\Jetpack\Assets::alias_textdomains_from_file( JETPACK_CHAT_DIR . 'jetpack_vendor/i18n-map.php' );
+	}
+} else { // Something very unexpected. Error out gently with an admin_notice and exit loading.
+	if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+		error_log( // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+			__( 'Error loading autoloader file for Jetpack Chat plugin', 'jetpack-chat' )
+		);
+	}
+
+	add_action(
+		'admin_notices',
+		function () {
+			?>
+		<div class="notice notice-error is-dismissible">
+			<p>
+				<?php
+				printf(
+					wp_kses(
+						/* translators: Placeholder is a link to a support document. */
+						__( 'Your installation of Jetpack Chat is incomplete. If you installed Jetpack Chat from GitHub, please refer to <a href="%1$s" target="_blank" rel="noopener noreferrer">this document</a> to set up your development environment. Jetpack Chat must have Composer dependencies installed and built via the build command.', 'jetpack-chat' ),
+						array(
+							'a' => array(
+								'href'   => array(),
+								'target' => array(),
+								'rel'    => array(),
+							),
+						)
+					),
+					'https://github.com/Automattic/jetpack/blob/trunk/docs/development-environment.md#building-your-project'
+				);
+				?>
+			</p>
+		</div>
+			<?php
+		}
+	);
+
+	return;
+}
+
+// Redirect to plugin page when the plugin is activated.
+add_action( 'activated_plugin', 'jetpack_chat_activation' );
+
+/**
+ * Redirects to plugin page when the plugin is activated
+ *
+ * @param string $plugin Path to the plugin file relative to the plugins directory.
+ */
+function jetpack_chat_activation( $plugin ) {
+	if (
+		JETPACK_CHAT_ROOT_FILE_RELATIVE_PATH === $plugin &&
+		\Automattic\Jetpack\Plugins_Installer::is_current_request_activating_plugin_from_plugins_screen( JETPACK_CHAT_ROOT_FILE_RELATIVE_PATH )
+	) {
+		wp_safe_redirect( esc_url( admin_url( 'admin.php?page=jetpack-chat' ) ) );
+		exit;
+	}
+}
+
+// Add "Settings" link to plugins page.
+add_filter(
+	'plugin_action_links_' . JETPACK_CHAT_FOLDER . '/jetpack-chat.php',
+	function ( $actions ) {
+		$settings_link = '<a href="' . esc_url( admin_url( 'admin.php?page=jetpack-chat' ) ) . '">' . __( 'Settings', 'jetpack-chat' ) . '</a>';
+		array_unshift( $actions, $settings_link );
+
+		return $actions;
+	}
+);
+
+register_deactivation_hook( __FILE__, array( 'Jetpack_Chat', 'plugin_deactivation' ) );
+
+// Main plugin class.
+new Jetpack_Chat();
---
+++ projects/plugins/chat/src/js/components/admin-page/index.jsx
@@ -0,0 +1,71 @@
+import { AdminPage, AdminSectionHero, Container, Col } from '@automattic/jetpack-components';
+import { CONNECTION_STORE_ID, ConnectScreen } from '@automattic/jetpack-connection';
+import { useSelect } from '@wordpress/data';
+import { __ } from '@wordpress/i18n';
+import React from 'react';
+import styles from './styles.module.scss';
+
+const Admin = () => {
+	const connectionStatus = useSelect(
+		select => select( CONNECTION_STORE_ID ).getConnectionStatus(),
+		[]
+	);
+	const { isUserConnected, isRegistered } = connectionStatus;
+	const showConnectionCard = ! isRegistered || ! isUserConnected;
+	return (
+		<AdminPage moduleName={ __( 'Jetpack Chat', 'jetpack-chat' ) }>
+			<AdminSectionHero>
+				{ showConnectionCard ? (
+					<Container horizontalSpacing={ 3 } horizontalGap={ 3 }>
+						<Col sm={ 4 } md={ 8 } lg={ 12 }>
+							<ConnectionSection />
+						</Col>
+					</Container>
+				) : (
+					<Container horizontalSpacing={ 3 } horizontalGap={ 3 }>
+						<Col>
+							<div id="jp-admin-notices" className="jetpack-chat-jitm-card" />
+						</Col>
+						<Col sm={ 4 } md={ 6 } lg={ 6 }>
+							<h1 className={ styles.heading }>{ __( 'Jetpack Chat', 'jetpack-chat' ) }</h1>
+							<h3>{ __( "Don't waste your time chatting with humans.", 'jetpack-chat' ) }</h3>
+							<ul className={ styles[ 'jp-product-promote' ] }>
+								<li>{ __( 'Chat with your site.', 'jetpack-chat' ) }</li>
+								<li>{ __( 'Chat with a bot.', 'jetpack-chat' ) }</li>
+								<li>{ __( 'Chat chat chat.', 'jetpack-chat' ) }</li>
+							</ul>
+						</Col>
+						<Col lg={ 1 } md={ 1 } sm={ 0 } />
+						<Col sm={ 4 } md={ 5 } lg={ 5 }>
+							<div id="jetpack-chatbot-root"></div>
+						</Col>
+					</Container>
+				) }
+			</AdminSectionHero>
+		</AdminPage>
+	);
+};
+
+export default Admin;
+
+const ConnectionSection = () => {
+	const { apiNonce, apiRoot, registrationNonce } = window.jetpackChatInitialState;
+	return (
+		<ConnectScreen
+			buttonLabel={ __( 'Connect to start chatting', 'jetpack-chat' ) }
+			title={ __( 'Jetpack Odie!', 'jetpack-chat' ) }
+			apiRoot={ apiRoot }
+			apiNonce={ apiNonce }
+			registrationNonce={ registrationNonce }
+			from="jetpack-chat"
+			redirectUri="admin.php?page=jetpack-chat"
+		>
+			<h3>{ __( "Don't waste your time chatting with humans.", 'jetpack-chat' ) }</h3>
+			<ul>
+				<li>{ __( 'Chat with your site.', 'jetpack-chat' ) }</li>
+				<li>{ __( 'Chat with a bot.', 'jetpack-chat' ) }</li>
+				<li>{ __( 'Chat chat chat.', 'jetpack-chat' ) }</li>
+			</ul>
+		</ConnectScreen>
+	);
+};
