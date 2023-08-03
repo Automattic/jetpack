5eedc6f2badda6a902fe63e2241b182e2a39fba9
cleacos <javacodia@gmail.com>
2023-08-03 09:46:55 +0100

    Removed unused fake data
    Signed-off-by: cleacos <javacodia@gmail.com>

--- projects/plugins/crm/tests/php/event-manager/class-event-manager-faker.php
+++ projects/plugins/crm/tests/php/event-manager/class-event-manager-faker.php
@@ -23,40 +23,4 @@ class Event_Manager_Faker {
 			'lastupdated' => 1690385165,
 		);
 	}
-
-	public function invoice_data() {
-		return array(
-			'id'   => 1,
-			'data' => array(
-				'id_override' => '1',
-				'parent'      => '',
-				'status'      => 'Unpaid',
-				'hash'        => 'ISSQndSUjlhJ8feWj2v',
-				'lineitems'   => array(
-					array(
-						'net'      => 3.75,
-						'desc'     => 'Dummy product',
-						'quantity' => '3',
-						'price'    => '1.25',
-						'total'    => 3.75,
-					),
-				),
-				'contacts'    => array( 1 ),
-				'created'     => -1,
-			),
-		);
-	}
-
-	/**
-	 * Return data for a dummy company
-	 * @return array
-	 */
-	public function company_data() {
-		return array(
-			'id'     => 1,
-			'name'   => 'Dummy Company',
-			'email'  => 'johndoe@dummycompany.com',
-			'status' => 'lead',
-		);
-	}
 }
