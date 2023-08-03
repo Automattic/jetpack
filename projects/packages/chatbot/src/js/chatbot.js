30021953c3d26c25cba887db6b80c67b0df7ac33
dereksmart <smart@automattic.com>
2023-08-03 19:04:05 -0400

    Use correct object...

--- projects/packages/chatbot/src/js/chatbot.js
+++ projects/packages/chatbot/src/js/chatbot.js
@@ -5,7 +5,7 @@ document.addEventListener( 'DOMContentLoaded', function () {
 	button.addEventListener( 'click', function () {
 		if ( window.Odie && window.Odie.render ) {
 			window.Odie.render( {
-				...window.chatbotData,
+				...window.JetpackXhrParams,
 				domNode: document.getElementById( 'jetpack-chatbot-root' ),
 				// eslint-disable-next-line no-console
 				onLoaded: () => console.log( 'Chat is loaded.' ),
