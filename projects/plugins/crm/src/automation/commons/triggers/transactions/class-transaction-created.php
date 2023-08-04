58479e352991cdc8a148c258bac931017f8591f1
gogdzl <diego.rodrigues@automattic.com>
2023-08-04 08:52:09 -0300

    Add the trigger for transaction created

---
+++ projects/plugins/crm/src/automation/commons/triggers/transactions/class-transaction-created.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Jetpack CRM Automation Transaction_Created trigger.
+ *
+ * @package automattic/jetpack-crm
+ */
+
+namespace Automattic\Jetpack\CRM\Automation\Triggers;
+
+use Automattic\Jetpack\CRM\Automation\Base_Trigger;
+
+/**
+ * Adds the Transaction_Created class.
+ *
+ * @since $$next-version$$
+ */
+class Transaction_Created extends Base_Trigger {
+
+	/**
+	 * Get the slug name of the trigger.
+	 *
+	 * @return string
+	 */
+	public static function get_slug(): string {
+		return 'jpcrm/transaction_created';
+	}
+
+	/**
+	 * Get the title of the trigger.
+	 *
+	 * @return string
+	 */
+	public static function get_title(): string {
+		return __( 'New Transaction', 'zero-bs-crm' );
+	}
+
+	/**
+	 * Get the description of the trigger.
+	 *
+	 * @return string
+	 */
+	public static function get_description(): string {
+		return __( 'Triggered when a transaction is created', 'zero-bs-crm' );
+	}
+
+	/**
+	 * Get the category of the trigger.
+	 *
+	 * @return string
+	 */
+	public static function get_category(): string {
+		return 'transaction';
+	}
+
+	/**
+	 * Listen to this trigger's target event.
+	 */
+	protected function listen_to_event(): void {
+		add_action(
+			'jpcrm_transaction_created',
+			array( $this, 'execute_workflow' )
+		);
+	}
+}
