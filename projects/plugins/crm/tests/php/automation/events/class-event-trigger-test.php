8477ac2c11ec0dda90117755dbebd0b44cba2f18
gogdzl <diego.rodrigues@automattic.com>
2023-08-04 09:06:08 -0300

    Add tests for event updated trigger

--- projects/plugins/crm/tests/php/automation/events/class-event-trigger-test.php
+++ projects/plugins/crm/tests/php/automation/events/class-event-trigger-test.php
@@ -6,6 +6,7 @@ use Automattic\Jetpack\CRM\Automation\Automation_Engine;
 use Automattic\Jetpack\CRM\Automation\Automation_Workflow;
 use Automattic\Jetpack\CRM\Automation\Triggers\Event_Created;
 use Automattic\Jetpack\CRM\Automation\Triggers\Event_Deleted;
+use Automattic\Jetpack\CRM\Automation\Triggers\Event_Updated;
 use WorDBless\BaseTestCase;
 
 require_once __DIR__ . '../../tools/class-automation-faker.php';
@@ -86,5 +87,37 @@ class Event_Trigger_Test extends BaseTestCase {
 
 		// Run the event_deleted action.
 		do_action( 'jpcrm_event_delete', $event_data );
+	}
+
+	/**
+	 * @testdox Test the event updated trigger executes the workflow with an action
+	 */
+	public function test_event_updated_trigger() {
+		$workflow_data = $this->automation_faker->workflow_without_initial_step_customize_trigger( 'jpcrm/event_updated' );
+
+		$trigger = new Event_Updated();
+
+		// Build a PHPUnit mock Automation_Workflow
+		$workflow = $this->getMockBuilder( Automation_Workflow::class )
+			->setConstructorArgs( array( $workflow_data, new Automation_Engine() ) )
+			->onlyMethods( array( 'execute' ) )
+			->getMock();
+
+		// Init the Event_Updated trigger.
+		$trigger->init( $workflow );
+
+		// Fake event data.
+		$event_data = $this->automation_faker->event_data();
+
+		// We expect the workflow to be executed on event_updated event with the event data.
+		$workflow->expects( $this->once() )
+		->method( 'execute' )
+		->with(
+			$this->equalTo( $trigger ),
+			$this->equalTo( $event_data )
+		);
+
+		// Run the event_updated action.
+		do_action( 'jpcrm_event_update', $event_data );
 	}
 }
