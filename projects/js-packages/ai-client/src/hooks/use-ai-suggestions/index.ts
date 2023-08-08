142ac88dafd59d7652e5ebf5f3175daf3d23cf4c
Luiz Kowalski <lhkowalski@gmail.com>
2023-08-08 15:25:06 -0300

    Handle errors outside the event source object

--- projects/js-packages/ai-client/src/hooks/use-ai-suggestions/index.ts
+++ projects/js-packages/ai-client/src/hooks/use-ai-suggestions/index.ts
@@ -282,8 +282,8 @@ export default function useAiSuggestions( {
 
 				eventSource.addEventListener( 'done', handleDone );
 			} catch ( e ) {
-				// eslint-disable-next-line no-console
-				console.error( e );
+				debug( 'Error outside the event source object: %o', e );
+				handleNetwotkError();
 			}
 		},
 		[
