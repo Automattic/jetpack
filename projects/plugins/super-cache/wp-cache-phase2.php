fda3a6c097fce05ed95ddeb7c6e4d4e93737f48c
Mark George <thingalon@gmail.com>
2023-08-08 14:06:33 +1000

    Only attempt gunzipping content if cache compression is on - but we have a reason to serve uncompressed

--- projects/plugins/super-cache/wp-cache-phase2.php
+++ projects/plugins/super-cache/wp-cache-phase2.php
@@ -366,9 +366,9 @@ function wp_cache_serve_cache_file() {
 			wp_cache_debug( 'Sending Header: Content-Length: ' . $size );
 			header( 'Content-Length: ' . $size );
 		}
-	} elseif ( $do_not_serve_gzip_data ) {
+	} elseif ( $cache_compression && $do_not_serve_gzip_data ) {
 		$cache        = wp_cache_get_legacy_cache( $cache_file );
-		$uncompressed = gzuncompress( $cache );
+		$uncompressed = @gzuncompress( $cache ); // phpcs:ignore WordPress.PHP.NoSilencedErrors.Discouraged -- there is a small chance the cache isn't gzipped despite being configured to be.
 		if ( $uncompressed ) {
 			$cache = $uncompressed;
 			unset( $uncompressed );
