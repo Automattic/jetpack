03d91040f45ef625ff4bb13e72fdd8c893c8e140
Jeremy Herve <jeremy@jeremy.hu>
2023-08-10 11:11:47 +0200

    e2e tests: change the Monitoring test
    Internal reference: p1691598965621699-slack-C03QNBQKG73

--- projects/plugins/jetpack/tests/e2e/specs/post-connection/recommendations.test.js
+++ projects/plugins/jetpack/tests/e2e/specs/post-connection/recommendations.test.js
@@ -47,7 +47,7 @@ test( 'Recommendations (Jetpack Assistant)', async ( { page } ) => {
 	await test.step( 'Enable Monitoring and continue to Related Post step', async () => {
 		await recommendationsPage.saveSiteTypeAndContinue();
 		await recommendationsPage.reload();
-		await recommendationsPage.waitForNetworkIdle();
+		//await recommendationsPage.waitForNetworkIdle();
 		await recommendationsPage.enableMonitoringAndContinue();
 		await recommendationsPage.reload();
 		await recommendationsPage.waitForNetworkIdle();
