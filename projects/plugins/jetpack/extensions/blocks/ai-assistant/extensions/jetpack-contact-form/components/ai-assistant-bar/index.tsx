f44b75e58c8d112d98716e56855a9d105f6f6afa
Damián Suárez <rdsuarez@gmail.com>
2023-08-11 08:32:44 -0300

    AI Extension: improve show/hide assistant bar when fixed (#32416)
    * [not verified] fix visual issue when hidding fixed toolbar
    
    * [not verified] move party of the UI logic at BlockEdit fn level
    
    * [not verified] first approach of auto-switch
    
    * [not verified] replace debound by a toggle approach
    
    * [not verified] add a ref to the main assistant wrapper DOM ref
    
    * changelog
    
    * remove code about debounceUpdateIsMobileMode

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/jetpack-contact-form/components/ai-assistant-bar/index.tsx
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/jetpack-contact-form/components/ai-assistant-bar/index.tsx
@@ -5,7 +5,7 @@ import { useAiContext, AIControl } from '@automattic/jetpack-ai-client';
 import { serialize } from '@wordpress/blocks';
 import { select } from '@wordpress/data';
 import { useDispatch } from '@wordpress/data';
-import { useContext, useCallback, useRef } from '@wordpress/element';
+import { useContext, useCallback, useRef, useState, useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { store as noticesStore } from '@wordpress/notices';
 /**
@@ -17,6 +17,19 @@ import useAIFeature from '../../../../hooks/use-ai-feature';
 import { PROMPT_TYPE_JETPACK_FORM_CUSTOM_PROMPT, getPrompt } from '../../../../lib/prompt';
 import { AiAssistantUiContext } from '../../ui-handler/context';
 import { AI_ASSISTANT_JETPACK_FORM_NOTICE_ID } from '../../ui-handler/with-ui-handler-data-provider';
+
+/*
+ * Core viewport breakpoints.
+ * @see https://github.com/WordPress/gutenberg/blob/d5d8533cf2cc04bb005bda147114cf00782d6c38/packages/base-styles/_breakpoints.scss#L5-L14
+ */
+const BREAKPOINTS = {
+	huge: 1440,
+	wide: 1280,
+	large: 960,
+	medium: 782,
+	small: 600,
+	mobile: 480,
+};
 
 /**
  * Return the serialized content from the childrens block.
@@ -51,6 +64,7 @@ export default function AiAssistantBar( {
 	clientId: string;
 	className?: string;
 } ) {
+	const wrapperRef = useRef< HTMLDivElement >( null );
 	const inputRef = useRef< HTMLInputElement >( null );
 
 	const { requireUpgrade } = useAIFeature();
@@ -85,8 +99,52 @@ export default function AiAssistantBar( {
 		requestSuggestion( prompt, { feature: 'jetpack-form-ai-extension' } );
 	}, [ clientId, inputValue, removeNotice, requestSuggestion ] );
 
+	/*
+	 * Auto-resize mode.
+	 * Update the bar layout depending on the component width.
+	 */
+	const isMobileModeRef = useRef( isFixed );
+	const [ isMobileMode, setMobileMode ] = useState( isFixed );
+
+	const observerRef = useRef( null );
+
+	useEffect( () => {
+		// Get the Assistant bar DOM element.
+		const barElement = wrapperRef?.current;
+		if ( ! barElement ) {
+			return;
+		}
+
+		// Only create a new observer if there isn't one already
+		if ( ! observerRef.current ) {
+			observerRef.current = new ResizeObserver( entries => {
+				// Bail erly if the bar is already fixed.
+				if ( isFixed ) {
+					return setMobileMode( true );
+				}
+
+				const barWidth = entries[ 0 ].contentRect.width;
+				const isMobileModeBasedOnWidth = barWidth < BREAKPOINTS.mobile;
+
+				// Only update the state if the mode has changed.
+				if ( isMobileModeBasedOnWidth !== isMobileModeRef.current ) {
+					isMobileModeRef.current = isMobileModeBasedOnWidth; // Update the ref to be able to compare later.
+					setMobileMode( isMobileModeBasedOnWidth ); // Update the state (and re-render)
+				}
+			} );
+		}
+
+		// Start observing the Assistant bar element.
+		observerRef.current.observe( barElement );
+
+		return () => {
+			// Disconnect the observer when the component is unmounted.
+			observerRef.current?.disconnect();
+		};
+	}, [ isFixed ] );
+
 	return (
-		<div className={ classNames( 'jetpack-ai-assistant__bar', className ) }>
+		<div ref={ wrapperRef } className={ classNames( 'jetpack-ai-assistant__bar', className ) }>
 			{ requireUpgrade && <UpgradePrompt /> }
 			<AIControl
 				ref={ inputRef }
@@ -98,7 +156,7 @@ export default function AiAssistantBar( {
 				onStop={ stopSuggestion }
 				state={ requestingState }
 				isOpaque={ requireUpgrade }
-				showButtonsLabel={ ! isFixed }
+				showButtonsLabel={ ! isMobileMode }
 			/>
 		</div>
 	);
