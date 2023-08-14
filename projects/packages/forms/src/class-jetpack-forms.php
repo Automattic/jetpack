d4d9d98243af1c4d94a23cf183eea46f5701a21d
sdixon194 <steve.dixon@automattic.com>
2023-08-14 11:56:31 -0400

    Changelog edits.

--- projects/packages/forms/src/class-jetpack-forms.php
+++ projects/packages/forms/src/class-jetpack-forms.php
@@ -15,7 +15,7 @@ use Automattic\Jetpack\Forms\Dashboard\Dashboard_View_Switch;
  */
 class Jetpack_Forms {
 
-	const PACKAGE_VERSION = '0.19.11-alpha';
+	const PACKAGE_VERSION = '0.19.11';
 
 	/**
 	 * Load the contact form module.
