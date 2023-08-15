3b11f43be70045d691071212d99a075a154b3a50
Mikael Korpela <mikael@ihminen.org>
2023-08-15 16:08:04 +0300

    Mailchimp block: allow transform to subscribe block

--- projects/plugins/jetpack/extensions/blocks/mailchimp/index.js
+++ projects/plugins/jetpack/extensions/blocks/mailchimp/index.js
@@ -4,6 +4,7 @@ import { __, _x } from '@wordpress/i18n';
 import { getIconColor } from '../../shared/block-icons';
 import deprecatedV1 from './deprecated/v1';
 import edit from './edit';
+import transforms from './transforms';
 import './editor.scss';
 
 export const name = 'mailchimp';
@@ -107,4 +108,5 @@ export const settings = {
 		innerBlocks: [ innerButtonBlock ],
 	},
 	deprecated: [ deprecatedV1 ],
+	transforms,
 };
