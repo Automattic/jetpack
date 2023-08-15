eff6fd3ebc232db49236819adc6d8fdcd8546971
Damián Suárez <rdsuarez@gmail.com>
2023-08-15 14:20:58 +0100

    [not verified] improve parsing blocks process

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/jetpack-contact-form/ui-handler/with-ui-handler-data-provider.tsx
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/jetpack-contact-form/ui-handler/with-ui-handler-data-provider.tsx
@@ -18,16 +18,42 @@ import { AiAssistantUiContextProvider } from './context';
  * Types
  */
 import type { RequestingErrorProps } from '@automattic/jetpack-ai-client';
-type BlockData = [ string, Record< string, unknown >, Array< BlockData > | undefined ];
+type blockName = string;
+type BlockData = [ blockName, Record< string, unknown >, Array< BlockData > | undefined ];
 
 // An identifier to use on the extension error notices,
 export const AI_ASSISTANT_JETPACK_FORM_NOTICE_ID = 'ai-assistant';
 
-function parseBlocksFromJson( jsonContent: Array< BlockData > ) {
+function parseBlocksFromJson( jsonContent: Array< BlockData > | BlockData ): Array< BlockData > {
+	/*
+	 * JsonContent can be an array of blocks or a single block (array)
+	 * If it's a single block, we need to wrap it in an array.
+	 *
+	 * Array of blocks:
+	 * [
+	 * 		[ 'core/paragraph', { content: 'Hello World!' } ],
+	 * 		[ 'core/paragraph', { content: 'Hello World!' } ],...
+	 * ]
+	 *
+	 * Single block:
+	 * [ 'core/paragraph', { content: 'Hello World!' } ]
+	 *
+	 * When it's a single block,
+	 * the first eement of the array is the block name (string),
+	 * so let's check if the first element is a string.
+	 * Is so, wrap the array in another array.
+	 */
+	if ( typeof jsonContent[ 0 ] === 'string' ) {
+		console.warn( 'Single block detected. Wrapping in an array.' ); // eslint-disable-line no-console
+		jsonContent = [ jsonContent ];
+		console.warn( 'jsonContent: ', jsonContent ); // eslint-disable-line no-console
+	}
+
 	const blocks = [];
 	for ( let i = 0; i < jsonContent.length; i++ ) {
 		const blockData = jsonContent[ i ];
 		const [ name, attributes, innerBlocks ] = blockData;
+
 		let block;
 
 		try {
@@ -39,7 +65,10 @@ function parseBlocksFromJson( jsonContent: Array< BlockData > ) {
 			}
 
 			blocks.push( block );
-		} catch ( e ) {}
+		} catch ( e ) {
+			console.error( 'Create block error: ', e ); // eslint-disable-line no-console
+			console.warn( 'Block data: ', blockData ); // eslint-disable-line no-console
+		}
 	}
 
 	return blocks;
@@ -51,11 +80,19 @@ const withUiHandlerDataProvider = createHigherOrderComponent( BlockListBlock => 
 
 		// AI Assistant input value
 		const [ inputValue, setInputValue ] = useState(
-			'In three columns, put in the first one an extensive text about Jorge Luis Borge. In next one, about Mariana Enriquez. And in the third one, a form to buy books. Please add all fields you consider appropriate.'
+			'In three columns, put in the first one a large article about Jorge Luis Borge. In next one, about Mariana Enriquez. And in the third one, a form to buy books. Please add all fields you consider appropriate.'
 		);
 
 		// const [ inputValue, setInputValue ] = useState(
 		// 	'Write a complete article about the CZSpectrum, split in three columns. In the first one, create an introductory content. In the second one, talk about the most important companies around it. In the third one, create a list with the best ten games of it.'
+		// );
+
+		// const [ inputValue, setInputValue ] = useState(
+		// 	'Write a large article about Johannes Gutenberg. Please organize the content in three columns. Feel free to add Headers, Lists, quotes, etc.'
+		// );
+
+		// const [ inputValue, setInputValue ] = useState(
+		// 	'Create a content about Mariana Enriquez. Please add a background image to the composition. And please, in Spanish.'
 		// );
 
 		// AI Assistant component visibility
@@ -181,13 +218,14 @@ const withUiHandlerDataProvider = createHigherOrderComponent( BlockListBlock => 
 					insertBlocks( containerBlock, clientId );
 					containerBlockWasInserted.current = true;
 				}
-				// newContent = decompressContent( newContent );
 
 				let jsonContent = [];
 				try {
 					const repairedContent = jsonrepair( newContent );
 					jsonContent = JSON.parse( repairedContent );
-				} catch ( e ) {}
+				} catch ( e ) {
+					console.error( 'Error parsing JSON: ', e ); // eslint-disable-line no-console
+				}
 
 				let parsedBlocks = [];
 				try {
@@ -198,7 +236,9 @@ const withUiHandlerDataProvider = createHigherOrderComponent( BlockListBlock => 
 								return block.isValid && block.name !== 'core/freeform';
 						  } )
 						: [];
-				} catch ( e ) {}
+				} catch ( e ) {
+					console.error( 'Error parsing blocks: ', e ); // eslint-disable-line no-console
+				}
 
 				// console.log( { parsedBlocks } );
 				if ( parsedBlocks?.length ) {
