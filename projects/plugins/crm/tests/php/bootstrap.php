86ea6338317751436c80721bf4bb389bec11fa25
André Kallehauge <3846700+kallehauge@users.noreply.github.com>
2023-08-16 19:25:38 +0200

    Make test root logic a 1:1 copy of the Jetpack plugin

--- projects/plugins/crm/tests/php/bootstrap.php
+++ projects/plugins/crm/tests/php/bootstrap.php
@@ -14,41 +14,43 @@ $_plugin_root = dirname( dirname( __DIR__ ) );
  * Locate WordPress or wordpress-develop. We look in several places.
  */
 if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-	// Jetpack Monorepo environment variable.
-	$_tests_dir = getenv( 'WP_DEVELOP_DIR' );
-	if ( file_exists( "$_tests_dir/tests/phpunit/" ) ) {
-		$_tests_dir .= '/tests/phpunit/';
+	// Jetpack Monorepo environment variable defined on command line.
+	$test_root = getenv( 'WP_DEVELOP_DIR' );
+	if ( file_exists( "$test_root/tests/phpunit/" ) ) {
+		$test_root .= '/tests/phpunit/';
 	}
-} elseif ( false !== getenv( 'WP_TESTS_DIR' ) ) {
-	// WordPress core environment variable.
-	$_tests_dir = getenv( 'WP_TESTS_DIR' );
-} elseif ( file_exists( dirname( dirname( dirname( dirname( $_plugin_root ) ) ) ) . '/tests/phpunit/includes/bootstrap.php' ) ) {
+} elseif ( file_exists( '../../../../tests/phpunit/includes/bootstrap.php' ) ) {
 	// Installed inside wordpress-develop.
-	$_tests_dir = dirname( dirname( $_plugin_root ) ) . '/tests/phpunit/includes/bootstrap.php';
+	$test_root = '../../../../tests/phpunit';
 } elseif ( file_exists( '/vagrant/www/wordpress-develop/public_html/tests/phpunit/includes/bootstrap.php' ) ) {
 	// VVV.
-	$_tests_dir = '/vagrant/www/wordpress-develop/public_html/tests/phpunit';
+	$test_root = '/vagrant/www/wordpress-develop/public_html/tests/phpunit';
 } elseif ( file_exists( '/srv/www/wordpress-trunk/public_html/tests/phpunit/includes/bootstrap.php' ) ) {
 	// VVV 3.0.
-	$_tests_dir = '/srv/www/wordpress-trunk/public_html/tests/phpunit';
+	$test_root = '/srv/www/wordpress-trunk/public_html/tests/phpunit';
 } elseif ( file_exists( '/tmp/wordpress-develop/tests/phpunit/includes/bootstrap.php' ) ) {
 	// Manual checkout & Jetpack's docker environment.
-	$_tests_dir = '/tmp/wordpress-develop/tests/phpunit';
-} elseif ( file_exists( rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib/includes/bootstrap.php' ) ) {
+	$test_root = '/tmp/wordpress-develop/tests/phpunit';
+} elseif ( file_exists( '/tmp/wordpress-tests-lib/includes/bootstrap.php' ) ) {
 	// Legacy tests.
-	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+	$test_root = '/tmp/wordpress-tests-lib';
 }
 
-if ( ! isset( $_tests_dir ) || ! file_exists( $_tests_dir . '/includes/bootstrap.php' ) ) {
-	echo 'Failed to automatically locate WordPress or wordpress-develop to run tests.' . PHP_EOL;
-	echo PHP_EOL;
-	echo 'Set the WP_DEVELOP_DIR environment variable to point to a copy of WordPress' . PHP_EOL;
-	echo 'or wordpress-develop.' . PHP_EOL;
+if ( ! isset( $test_root ) || ! file_exists( $test_root . '/includes/bootstrap.php' ) ) {
+	fprintf(
+		STDERR,
+		<<<'EOF'
+Failed to automatically locate WordPress or wordpress-develop to run tests.
+
+Set the WP_DEVELOP_DIR environment variable to point to a copy of WordPress
+or wordpress-develop.
+EOF
+	);
 	exit( 1 );
 }
 
 // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-echo "Using test root $_tests_dir\n";
+echo "Using test root $test_root\n";
 
 if ( ! is_readable( $_plugin_root . '/vendor/autoload.php' ) ) {
 	echo 'The plugin is not ready for testing.' . PHP_EOL;
@@ -60,7 +62,7 @@ if ( ! is_readable( $_plugin_root . '/vendor/autoload.php' ) ) {
 /**
  * Give access to tests_add_filter() function.
  */
-require_once $_tests_dir . '/includes/functions.php';
+require $test_root . '/includes/functions.php';
 
 /**
  * Load Jetpack CRM.
@@ -87,7 +89,7 @@ tests_add_filter( 'muplugins_loaded', '_jpcrm_manually_load_plugin' );
 /**
  * Start up the WP testing environment.
  */
-require $_tests_dir . '/includes/bootstrap.php';
+require $test_root . '/includes/bootstrap.php';
 
 /**
  * Make Jetpack CRM test case available for all tests.
