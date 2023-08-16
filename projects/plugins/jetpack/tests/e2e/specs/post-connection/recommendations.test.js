568d60dd4aefb618fcecce517908d9d09986a435
Adrian Moldovan <adim.moldovan@gmail.com>
2023-08-16 13:11:01 +0300

    Try without waitForNetworkIdle

--- projects/plugins/jetpack/tests/e2e/specs/post-connection/recommendations.test.js
+++ projects/plugins/jetpack/tests/e2e/specs/post-connection/recommendations.test.js
@@ -46,8 +46,8 @@ test( 'Recommendations (Jetpack Assistant)', async ( { page } ) => {
 
 	await test.step( 'Enable Monitoring and continue to Related Post step', async () => {
 		await recommendationsPage.saveSiteTypeAndContinue();
-		// await recommendationsPage.reload();
-		await recommendationsPage.waitForNetworkIdle();
+		await recommendationsPage.reload();
+		// await recommendationsPage.waitForNetworkIdle();
 		const isMonitorStep = await recommendationsPage.isEnableMonitoringButtonVisible();
 		expect( isMonitorStep, 'Monitor step should be visible' ).toBeTruthy();
 		expect(
@@ -56,7 +56,7 @@ test( 'Recommendations (Jetpack Assistant)', async ( { page } ) => {
 		).toBeTruthy();
 		await recommendationsPage.enableMonitoringAndContinue();
 		await recommendationsPage.reload();
-		await recommendationsPage.waitForNetworkIdle();
+		// await recommendationsPage.waitForNetworkIdle();
 		const isRelatedPostsStep = await recommendationsPage.isEnableRelatedPostsButtonVisible();
 		expect( isRelatedPostsStep, 'Related posts step should be visible' ).toBeTruthy();
 		expect(
@@ -67,8 +67,8 @@ test( 'Recommendations (Jetpack Assistant)', async ( { page } ) => {
 
 	await test.step( 'Enable Related Posts and continue to Newsletter step', async () => {
 		await recommendationsPage.enableRelatedPostsAndContinue();
-		// await recommendationsPage.reload();
-		await recommendationsPage.waitForNetworkIdle();
+		await recommendationsPage.reload();
+		// await recommendationsPage.waitForNetworkIdle();
 		const isNewsletterStep = await recommendationsPage.isEnableNewsletterButtonVisible();
 		expect( isNewsletterStep, 'Newsletter step should be visible' ).toBeTruthy();
 		expect(
@@ -79,8 +79,8 @@ test( 'Recommendations (Jetpack Assistant)', async ( { page } ) => {
 
 	await test.step( 'Enable Newsletter and continue to Site Accelerator', async () => {
 		await recommendationsPage.enableNewsletterAndContinue();
-		// await recommendationsPage.reload();
-		await recommendationsPage.waitForNetworkIdle();
+		await recommendationsPage.reload();
+		// await recommendationsPage.waitForNetworkIdle();
 		const isSiteAcceleratorStep = await recommendationsPage.isEnableSiteAcceleratorButtonVisible();
 		expect( isSiteAcceleratorStep, 'Site Accelerator step should be visible' ).toBeTruthy();
 		expect(
@@ -91,8 +91,8 @@ test( 'Recommendations (Jetpack Assistant)', async ( { page } ) => {
 
 	await test.step( 'Skip Site Accelerator and continue to VaultPress Backup card', async () => {
 		await recommendationsPage.skipSiteAcceleratorAndContinue();
-		// await recommendationsPage.reload();
-		await recommendationsPage.waitForNetworkIdle();
+		await recommendationsPage.reload();
+		// await recommendationsPage.waitForNetworkIdle();
 		const isVaultPressBackupStep = await recommendationsPage.isTryVaultPressBackupButtonVisible();
 		expect( isVaultPressBackupStep, 'VaultPress Backup step should be visible' ).toBeTruthy();
 		expect(
@@ -103,8 +103,8 @@ test( 'Recommendations (Jetpack Assistant)', async ( { page } ) => {
 
 	await test.step( 'Skip VaultPress Backup card and continue to Summary', async () => {
 		await recommendationsPage.skipVaultPressBackupAndContinue();
-		// await recommendationsPage.reload();
-		await recommendationsPage.waitForNetworkIdle();
+		await recommendationsPage.reload();
+		// await recommendationsPage.waitForNetworkIdle();
 		const isSummaryContent = await recommendationsPage.isSummaryContentVisible();
 		const isSummarySidebar = await recommendationsPage.isSummarySidebarVisible();
 		expect(
