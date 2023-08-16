5d43e57f705dc2675796eadc72af9717a89ced54
David Calhoun <github@davidcalhoun.me>
2023-08-15 22:26:06 -0400

    [not verified] wip: Testing native Paywall block logic

--- projects/plugins/jetpack/extensions/blocks/paywall/edit.native.js
+++ projects/plugins/jetpack/extensions/blocks/paywall/edit.native.js
@@ -1,9 +1,46 @@
 import { HorizontalRule } from '@wordpress/components';
 import { usePreferredColorSchemeStyle } from '@wordpress/compose';
+import { useEntityProp } from '@wordpress/core-data';
+import { useSelect } from '@wordpress/data';
+import { store as editorStore } from '@wordpress/editor';
 import { __ } from '@wordpress/i18n';
+import { accessOptions } from '../../shared/memberships/constants';
+import { useAccessLevel } from '../../shared/memberships/edit';
+import { store as membershipProductsStore } from '../../store/membership-products';
 import styles from './editor.scss';
 
 export default function PaywallEdit() {
+	const postType = useSelect( select => select( editorStore ).getCurrentPostType(), [] );
+	const accessLevel = useAccessLevel( postType );
+	const [ , setPostMeta ] = useEntityProp( 'postType', postType, 'meta' );
+	const getText = key => {
+		switch ( key ) {
+			case accessOptions.everybody.key:
+				return __( 'Change visibility to enable paywall', 'jetpack' );
+			case accessOptions.subscribers.key:
+				return __( 'Subscriber-only content below', 'jetpack' );
+			case accessOptions.paid_subscribers.key:
+				return __( 'Paid content below this line', 'jetpack' );
+			default:
+				return __( 'Paywall', 'jetpack' );
+		}
+	};
+
+	const text = getText( accessLevel );
+
+	const { hasNewsletterPlans, stripeConnectUrl, isLoading } = useSelect( select => {
+		const { getNewsletterProducts, getConnectUrl, isApiStateLoading } =
+			select( membershipProductsStore );
+
+		return {
+			isLoading: isApiStateLoading(),
+			stripeConnectUrl: getConnectUrl(),
+			hasNewsletterPlans: getNewsletterProducts()?.length !== 0,
+		};
+	} );
+
+	console.log( '>>>', { hasNewsletterPlans, stripeConnectUrl, isLoading } );
+
 	const textStyle = usePreferredColorSchemeStyle(
 		styles[ 'paywall--text' ],
 		styles[ 'paywall--text__dark' ]
@@ -15,7 +52,7 @@ export default function PaywallEdit() {
 
 	return (
 		<HorizontalRule
-			text={ __( 'Exclusive content below this line', 'jetpack' ) }
+			text={ text }
 			marginLeft={ 0 }
 			marginRight={ 0 }
 			textStyle={ textStyle }
