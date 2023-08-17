0643a9313179f3f85a908ea6c983e8242c720605
André Kallehauge <3846700+kallehauge@users.noreply.github.com>
2023-08-17 12:24:08 +0200

    Add comments to data transformer exception constants

--- projects/plugins/crm/src/automation/class-data-transformer-exception.php
+++ projects/plugins/crm/src/automation/class-data-transformer-exception.php
@@ -13,9 +13,50 @@ namespace Automattic\Jetpack\CRM\Automation;
  * @since $$next-version$$
  */
 class Data_Transformer_Exception extends \Exception {
-	const CLASS_NOT_FOUND            = 10;
-	const DO_NOT_EXTEND_BASE         = 11;
-	const SLUG_EXISTS                = 12;
-	const MISSING_LINK               = 20;
+
+	/**
+	 * Error code for when the class doesn't exist.
+	 *
+	 * @since $$next-version$$
+	 *
+	 * @var int
+	 */
+	const CLASS_NOT_FOUND = 10;
+
+	/**
+	 * Error code for when a transformer is passed, but doesn't extend the base class.
+	 *
+	 * @since $$next-version$$
+	 *
+	 * @var int
+	 */
+	const DO_NOT_EXTEND_BASE = 11;
+
+	/**
+	 * Error code for when a slug is already being used.
+	 *
+	 * @since $$next-version$$
+	 *
+	 * @var int
+	 */
+	const SLUG_EXISTS = 12;
+
+	/**
+	 * Error code for when an object doesn't have a related ID to map to.
+	 *
+	 * @since $$next-version$$
+	 *
+	 * @var int
+	 */
+	const MISSING_LINK = 20;
+
+	/**
+	 * Error code for when two objects cannot be mixed by the system (yet).
+	 *
+	 * @since $$next-version$$
+	 *
+	 * @var int
+	 */
 	const TRANSFORM_IS_NOT_SUPPORTED = 30;
+
 }
