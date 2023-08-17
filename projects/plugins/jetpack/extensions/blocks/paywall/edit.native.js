eaf9c8dfb55e7ccd2aab29b74ed8f9553c635599
David Calhoun <github@davidcalhoun.me>
2023-08-17 15:57:11 -0400

    feat: Jetpack block displays dynamic labels
    A WIP exploration of modifying the Paywall block label based upon the
    newsletter access level. Unfortunately, the native editor does not have
    access to post meta data, it is not provided by the host app currently.

--- projects/plugins/jetpack/extensions/blocks/paywall/edit.native.js
+++ projects/plugins/jetpack/extensions/blocks/paywall/edit.native.js
@@ -1,6 +1,10 @@
 import { HorizontalRule } from '@wordpress/components';
 import { usePreferredColorSchemeStyle } from '@wordpress/compose';
+import { useSelect } from '@wordpress/data';
+import { store as editorStore } from '@wordpress/editor';
 import { __ } from '@wordpress/i18n';
+import { accessOptions } from '../../shared/memberships/constants';
+import { useAccessLevel } from '../../shared/memberships/edit';
 import styles from './editor.scss';
 
 export default function PaywallEdit() {
@@ -13,11 +17,22 @@ export default function PaywallEdit() {
 		styles[ 'paywall--line__dark' ]
 	);
 
-	return (
-		<HorizontalRule
-			text={ __( 'Exclusive content below this line', 'jetpack' ) }
-			textStyle={ textStyle }
-			lineStyle={ lineStyle }
-		/>
-	);
+	const postType = useSelect( select => select( editorStore ).getCurrentPostType(), [] );
+	const accessLevel = useAccessLevel( postType );
+	const getText = key => {
+		switch ( key ) {
+			case accessOptions.everybody.key:
+				return __( 'Change visibility to enable paywall', 'jetpack' );
+			case accessOptions.subscribers.key:
+				return __( 'Subscriber-only content below', 'jetpack' );
+			case accessOptions.paid_subscribers.key:
+				return __( 'Paid content below this line', 'jetpack' );
+			default:
+				return __( 'Paywall', 'jetpack' );
+		}
+	};
+
+	const text = getText( accessLevel );
+
+	return <HorizontalRule text={ text } textStyle={ textStyle } lineStyle={ lineStyle } />;
 }
