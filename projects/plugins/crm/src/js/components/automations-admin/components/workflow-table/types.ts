82d91a2762a81c14abec517a6923ba2837be8439
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2023-08-17 09:12:22 -0500

    Remove unused type

--- projects/plugins/crm/src/js/components/automations-admin/components/workflow-table/types.ts
+++ projects/plugins/crm/src/js/components/automations-admin/components/workflow-table/types.ts
@@ -1,8 +1,4 @@
-import { Workflow } from '../../types';
-
 export type WorkflowTableColumn = 'checkbox' | 'name' | 'status' | 'added' | 'trigger' | 'edit';
 export type SortableWorkflowTableColumn = Exclude< WorkflowTableColumn, 'checkbox' | 'edit' >;
 
 export type SortDirection = 'ascending' | 'descending';
-
-export type WorkflowSortFunction = ( a: Workflow, b: Workflow ) => number;
