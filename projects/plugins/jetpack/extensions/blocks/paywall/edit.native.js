6d9928427efa20857a8784f9c90f74fe6356f310
David Calhoun <github@davidcalhoun.me>
2023-08-24 11:24:48 -0400

    feat: Redesign mobile editor Jetpack Paywall block visuals
    Provide additional context via visual separation and label text.

--- projects/plugins/jetpack/extensions/blocks/paywall/edit.native.js
+++ projects/plugins/jetpack/extensions/blocks/paywall/edit.native.js
@@ -1,12 +1,21 @@
+import { View, Text } from 'react-native';
 import { HorizontalRule } from '@wordpress/components';
 import { usePreferredColorSchemeStyle } from '@wordpress/compose';
 import { __ } from '@wordpress/i18n';
 import styles from './editor.scss';
 
 export default function PaywallEdit() {
+	const paywallStyle = usePreferredColorSchemeStyle(
+		styles[ 'paywall' ],
+		styles[ 'paywall__dark' ]
+	);
 	const textStyle = usePreferredColorSchemeStyle(
 		styles[ 'paywall--text' ],
 		styles[ 'paywall--text__dark' ]
+	);
+	const subtextStyle = usePreferredColorSchemeStyle(
+		styles[ 'paywall--subtext' ],
+		styles[ 'paywall--subtext__dark' ]
 	);
 	const lineStyle = usePreferredColorSchemeStyle(
 		styles[ 'paywall--line' ],
@@ -14,10 +23,16 @@ export default function PaywallEdit() {
 	);
 
 	return (
-		<HorizontalRule
-			text={ __( 'Exclusive content below this line', 'jetpack' ) }
-			textStyle={ textStyle }
-			lineStyle={ lineStyle }
-		/>
+		<View style={ paywallStyle }>
+			<HorizontalRule
+				lineStyle={ lineStyle }
+				style={ styles[ 'paywall--horizontalRule' ] }
+				text={ __( 'Subscriber-only content below', 'jetpack' ) }
+				textStyle={ textStyle }
+			/>
+			<Text style={ subtextStyle }>
+				{ __( 'Access this Paywall block on your web browser for advanced settings.', 'jetpack' ) }
+			</Text>
+		</View>
 	);
 }
