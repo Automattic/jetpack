041bf349aa58f82e57676e59d7fc2b373ae94fd1
Peter Petrov <peter.petrov89@gmail.com>
2023-08-24 07:50:40 +0300

    [not verified] Boost: Prevent SEO plugins from breaking Critical CSS generation (#32627)
    * Add Critical CSS compatibility with Yoast
    
    * Add Critical CSS compatibility with AIOSEO
    
    * Improve code readability
    
    * add changelog
    
    * Simplify changelog description
    
    * Add Critical CSS gen token to query vars list
    
    * Move some code around

--- projects/plugins/boost/jetpack-boost.php
+++ projects/plugins/boost/jetpack-boost.php
@@ -209,6 +209,14 @@ function include_compatibility_files() {
 	if ( function_exists( 'wp_cache_is_enabled' ) ) {
 		require_once __DIR__ . '/compatibility/wp-super-cache.php';
 	}
+
+	if ( class_exists( '\Yoast\WP\SEO\Main' ) ) {
+		require_once __DIR__ . '/compatibility/yoast.php';
+	}
+
+	if ( function_exists( 'aioseo' ) ) {
+		require_once __DIR__ . '/compatibility/aioseo.php';
+	}
 }
 
 add_action( 'plugins_loaded', __NAMESPACE__ . '\include_compatibility_files' );
