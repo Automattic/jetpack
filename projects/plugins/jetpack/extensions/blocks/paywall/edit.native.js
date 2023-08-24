f7e58cc197125cdf73ddeab974e322d2bf9c213e
David Calhoun <github@davidcalhoun.me>
2023-08-24 11:42:51 -0400

    refactor: Address linter errors

--- projects/plugins/jetpack/extensions/blocks/paywall/edit.native.js
+++ projects/plugins/jetpack/extensions/blocks/paywall/edit.native.js
@@ -1,14 +1,11 @@
-import { View, Text } from 'react-native';
 import { HorizontalRule } from '@wordpress/components';
 import { usePreferredColorSchemeStyle } from '@wordpress/compose';
 import { __ } from '@wordpress/i18n';
+import { View, Text } from 'react-native';
 import styles from './editor.scss';
 
 export default function PaywallEdit() {
-	const paywallStyle = usePreferredColorSchemeStyle(
-		styles[ 'paywall' ],
-		styles[ 'paywall__dark' ]
-	);
+	const paywallStyle = usePreferredColorSchemeStyle( styles.paywall, styles.paywall__dark );
 	const textStyle = usePreferredColorSchemeStyle(
 		styles[ 'paywall--text' ],
 		styles[ 'paywall--text__dark' ]
