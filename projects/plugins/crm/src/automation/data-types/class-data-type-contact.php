b90ab34079984707d4afc8bec9a715bb888d0ae8
cleacos <javacodia@gmail.com>
2023-08-28 16:52:23 +0100

    [not verified] Update Data_Type_Contact to use Contact
    Signed-off-by: cleacos <javacodia@gmail.com>

--- projects/plugins/crm/src/automation/data-types/class-data-type-contact.php
+++ projects/plugins/crm/src/automation/data-types/class-data-type-contact.php
@@ -8,6 +8,8 @@
 
 namespace Automattic\Jetpack\CRM\Automation\Data_Types;
 
+use Automattic\Jetpack\CRM\Entities\Contact;
+
 /**
  * Contact Data Type.
  *
@@ -19,7 +21,7 @@ class Data_Type_Contact extends Data_Type_Base {
 	 * {@inheritDoc}
 	 */
 	public static function get_slug(): string {
-		return 'contact';
+		return Contact::class;
 	}
 
 	/**
@@ -36,8 +38,10 @@ class Data_Type_Contact extends Data_Type_Base {
 	 * @throws \Automattic\Jetpack\CRM\Automation\Data_Type_Exception If the entity is not valid.
 	 */
 	public function __construct( $entity ) {
-		$entity = $this->unify_data( $entity );
 
+		if ( ! $entity instanceof Contact ) {
+			$entity = new Contact( $entity );
+		}
 		parent::__construct( $entity );
 	}
 
@@ -57,57 +61,11 @@ class Data_Type_Contact extends Data_Type_Base {
 	 * @return bool Whether the entity is valid or not.
 	 */
 	public function validate_entity( $entity ): bool {
-		// We do not have a model/object class for contacts, so as the very
-		// minimum we need to make sure we received an array since that's
-		// the response the CRM DAL returns.
-		if ( ! is_array( $entity ) ) {
-			return false;
+
+		if ( $entity instanceof Contact ) {
+			return true;
 		}
 
-		// We could look for even more fields, but this should ensure we have
-		// received a data array that looks valid enough.
-		$required_fields = array(
-			'id',
-			'email',
-			'status',
-			'fname',
-			'lname',
-		);
-
-		foreach ( $required_fields as $field ) {
-			if ( ! isset( $entity[ $field ] ) ) {
-				return false;
-			}
-		}
-
-		return true;
+		return false;
 	}
-
-	/**
-	 * Unify how CRM contact data is formatted.
-	 *
-	 * zbsDAL_contacts::getContact() formats the data when it returns results
-	 * but some hooks like "contact.new" passes an older, raw, formatting which
-	 * means we have two different types of formats being passed around.
-	 * This method attempts to unify the formatting, so we only have to work
-	 * with a single version of the formatting (the most recent one).
-	 *
-	 * @since $$next-version$$
-	 *
-	 * @param mixed $entity The data we want to potentially prepare.
-	 * @return array|mixed The unified data.
-	 */
-	public function unify_data( $entity ) {
-		if ( ! is_array( $entity ) ) {
-			return $entity;
-		}
-
-		if ( ! isset( $entity['zbsc_status'] ) ) {
-			return $entity;
-		}
-
-		$contacts_dal = new \zbsDAL_contacts();
-		return $contacts_dal->tidy_contact( (object) $entity );
-	}
-
 }
