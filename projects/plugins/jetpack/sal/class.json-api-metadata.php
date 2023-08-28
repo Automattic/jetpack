f729b8eab542da1674f0f8596e5f841ef4752922
Brent Nef <nef@a8c.com>
2023-08-28 05:46:35 -0700

    [not verified] Earn/tier selector (#32683)
    * [not verified] Create a TierSelector component for paid newsletters.
    
    * [not verified] changelog
    
    * Change from range to radio. Get the tier from post meta, and set it as well.
    
    * Apply review fixes.
    
    * Fix sort.
    
    * Fix lint
    
    * Review comments.

--- projects/plugins/jetpack/sal/class.json-api-metadata.php
+++ projects/plugins/jetpack/sal/class.json-api-metadata.php
@@ -4,6 +4,9 @@
  *
  * @package automattic/jetpack
  */
+
+use const Automattic\Jetpack\Extensions\Subscriptions\META_NAME_FOR_POST_TIER_ID_SETTINGS;
+
 /**
  * Base class for WPCOM_JSON_API_Metadata
  */
@@ -53,8 +56,14 @@ class WPCOM_JSON_API_Metadata {
 			return false;
 		}
 
-		// We want to always return the `_jetpack_newsletter_access` key to display the correct newsletter access in Calypso.
-		if ( $key === '_jetpack_newsletter_access' ) {
+		// We want to always return the `_jetpack_newsletter_access` key to
+		// display the correct newsletter access in Calypso.
+		$whitelist = array(
+			'_jetpack_newsletter_access',
+			META_NAME_FOR_POST_TIER_ID_SETTINGS,
+		);
+
+		if ( in_array( $key, $whitelist, true ) ) {
 			return false;
 		}
 
