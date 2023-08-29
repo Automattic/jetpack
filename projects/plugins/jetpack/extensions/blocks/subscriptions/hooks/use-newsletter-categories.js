d8f09c215c9453420e8c6a7006810cc8ea67cab1
Tim Broddin <tim@wannabes.be>
2023-08-29 09:04:50 +0200

    [not verified] Add newsletter categories REST API endpoint and adjust client-side fetch
    This commit introduces a new REST API endpoint for retrieving the newsletter categories. The new endpoint will enable a more seamless and effective integration of these categories in the client-side.
    
    Moreover, modifications were also made to the 'trait-wpcom-rest-api-proxy-request-trait.php' file. Now the body for Client::wpcom_json_api_request_as_user is checked if not null. This was done to prevent an error when the body is null.
    
    Lastly, the fetch request at 'use-newsletter-categories.js' has been updated to use the new /newsletter-categories endpoint within the local server environment instead of making a direct request to the public API.

--- projects/plugins/jetpack/extensions/blocks/subscriptions/hooks/use-newsletter-categories.js
+++ projects/plugins/jetpack/extensions/blocks/subscriptions/hooks/use-newsletter-categories.js
@@ -1,7 +1,5 @@
 import apiFetch from '@wordpress/api-fetch';
-import { select } from '@wordpress/data';
 import { useEffect, useState } from '@wordpress/element';
-import { CONNECTION_STORE_ID } from '../../../../../../js-packages/connection';
 
 const useNewsletterCategories = () => {
 	const [ data, setData ] = useState( [] );
@@ -10,12 +8,10 @@ const useNewsletterCategories = () => {
 	const [ loading, setLoading ] = useState( true );
 
 	useEffect( () => {
-		const blogId = select( CONNECTION_STORE_ID ).getBlogId();
-
 		const fetchData = async () => {
 			try {
 				const newsLetterCategories = await apiFetch( {
-					url: `https://public-api.wordpress.com/wpcom/v2/sites/${ blogId }/newsletter-categories`,
+					path: `/wpcom/v2/newsletter-categories`,
 				} );
 				setData( newsLetterCategories?.newsletter_categories );
 				setEnabled( newsLetterCategories?.enabled ?? false );
