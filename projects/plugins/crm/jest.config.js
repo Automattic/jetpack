2f28bf0bc149c97e27e9e0b9293cc96a33ca895b
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2023-08-29 16:32:12 -0500

    Add Jetpack Connection test globals

--- projects/plugins/crm/jest.config.js
+++ projects/plugins/crm/jest.config.js
@@ -3,5 +3,5 @@ const baseConfig = require( 'jetpack-js-tools/jest/config.base.js' );
 module.exports = {
 	...baseConfig,
 	roots: [ '<rootDir>/src' ],
-	setupFilesAfterEnv: [ ...baseConfig.setupFilesAfterEnv ],
+	setupFilesAfterEnv: [ ...baseConfig.setupFilesAfterEnv, "<rootDir>/tests/jest-globals.gui.js" ],
 };
