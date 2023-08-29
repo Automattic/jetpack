16ad6ddb1a7705a2e48f82b0ad155af33b781b43
Paulo Cruz <paulo.cruz@automattic.com>
2023-08-29 04:48:42 -0300

    Options: Add wpcom_newsletter_categories_enabled site option #32569 (#32569)
    * Add wpcom_newsletter_categories_enabled site option
    
    * changelog
    
    * Update package version
    
    ---------
    
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>

--- projects/packages/sync/src/class-defaults.php
+++ projects/packages/sync/src/class-defaults.php
@@ -176,6 +176,7 @@ class Defaults {
 		'wp_mobile_featured_images',
 		'wp_page_for_privacy_policy',
 		'wpcom_featured_image_in_email',
+		'wpcom_newsletter_categories_enabled',
 		'wpcom_is_fse_activated',
 		'wpcom_publish_comments_with_markdown',
 		'wpcom_publish_posts_with_markdown',
