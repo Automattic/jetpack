2726c5b9bc145a822d1ba27e1f7012ee574119d1
Douglas <douglas.henri@automattic.com>
2023-08-31 16:11:13 -0300

    fix typescript issues

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/jetpack-contact-form/ui-handler/with-ui-handler-data-provider.tsx
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/jetpack-contact-form/ui-handler/with-ui-handler-data-provider.tsx
@@ -10,6 +10,7 @@ import { useDispatch, useSelect, dispatch } from '@wordpress/data';
 import { useState, useMemo, useCallback, useEffect, useRef } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { store as noticesStore } from '@wordpress/notices';
+import React from 'react';
 /**
  * Internal dependencies
  */
@@ -20,6 +21,7 @@ import { AiAssistantUiContextProvider } from './context';
 /**
  * Types
  */
+import type { Block } from '../../../lib/utils/compare-blocks';
 import type { RequestingErrorProps } from '@automattic/jetpack-ai-client';
 
 // An identifier to use on the extension error notices,
@@ -54,7 +56,7 @@ const withUiHandlerDataProvider = createHigherOrderComponent( BlockListBlock => 
 		const [ assistantAnchor, setAssistantAnchor ] = useState< HTMLElement | null >( null );
 
 		// Keep track of the current list of valid blocks between renders.
-		const currentListOfValidBlocks = useRef( [] );
+		const currentListOfValidBlocks = useRef< Array< Block > >( [] );
 
 		const { replaceInnerBlocks } = useDispatch( 'core/block-editor' );
 		const coreEditorSelect = useSelect( select => select( 'core/editor' ), [] ) as {
