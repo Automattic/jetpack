fdf7a0d744dda5c078034db43360122173408f0d
Adnan Haque <adnan007.id@gmail.com>
2023-08-31 11:22:54 +0600

    Apply quality settings

--- projects/plugins/boost/app/modules/optimizations/image-cdn/class-image-cdn.php
+++ projects/plugins/boost/app/modules/optimizations/image-cdn/class-image-cdn.php
@@ -9,6 +9,8 @@ class Image_CDN implements Pluggable {
 
 	public function setup() {
 		Image_CDN_Setup::load();
+
+		add_filter( 'jetpack_photon_pre_args', array( $this, 'add_quality_args' ) );
 	}
 
 	public static function get_slug() {
@@ -17,5 +19,17 @@ class Image_CDN implements Pluggable {
 
 	public static function is_available() {
 		return true;
+	}
+
+	/**
+	 * Add quality arg to existing photon args.
+	 *
+	 * @param $args array - Existing photon args.
+	 *
+	 * @return mixed
+	 */
+	public function add_quality_args( $args ) {
+		$args['quality'] = jetpack_boost_ds_get( 'image_cdn_quality' );
+		return $args;
 	}
 }
