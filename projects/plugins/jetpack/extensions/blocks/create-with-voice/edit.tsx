cc41f95af753967b921a0cfe7eb4402b321b7af9
Damián Suárez <rdsuarez@gmail.com>
2023-08-31 16:42:50 +0100

    [not verified] introduce "Done" button

--- projects/plugins/jetpack/extensions/blocks/create-with-voice/edit.tsx
+++ projects/plugins/jetpack/extensions/blocks/create-with-voice/edit.tsx
@@ -1,14 +1,18 @@
 /**
  * External dependencies
  */
-import { micIcon, playerStopIcon, useMediaRecording } from '@automattic/jetpack-ai-client';
+import { micIcon, playerPauseIcon, useMediaRecording } from '@automattic/jetpack-ai-client';
 import { useBlockProps } from '@wordpress/block-editor';
 import { Placeholder, Button } from '@wordpress/components';
 import { useCallback } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 export default function CreateWithVoiceEdit() {
-	const { state, start, pause, resume } = useMediaRecording();
+	const { state, start, pause, stop, resume } = useMediaRecording( {
+		onDone: blob => {
+			console.log( 'Blob created: ', blob ); // eslint-disable-line no-console
+		},
+	} );
 
 	const recordingHandler = useCallback( () => {
 		if ( state === 'inactive' ) {
@@ -20,7 +24,7 @@ export default function CreateWithVoiceEdit() {
 		}
 	}, [ state, start, pause, resume ] );
 
-	let buttonLabel = __( 'Start recording', 'jetpack' );
+	let buttonLabel = __( 'Begin recording', 'jetpack' );
 	if ( state === 'recording' ) {
 		buttonLabel = __( 'Pause recording', 'jetpack' );
 	} else if ( state === 'paused' ) {
@@ -39,14 +43,26 @@ export default function CreateWithVoiceEdit() {
 					'jetpack'
 				) }
 			>
-				<Button
-					className="jetpack-ai-create-with-voice__record-button"
-					icon={ state === 'recording' ? playerStopIcon : micIcon }
-					variant="primary"
-					onClick={ recordingHandler }
-				>
-					{ buttonLabel }
-				</Button>
+				<div className="jetpack-ai-create-with-voice__recorder">
+					<Button
+						className="jetpack-ai-create-with-voice__record-button"
+						icon={ state === 'recording' ? playerPauseIcon : micIcon }
+						iconPosition="right"
+						variant="primary"
+						onClick={ recordingHandler }
+					>
+						{ buttonLabel }
+					</Button>
+
+					<Button
+						className="jetpack-ai-create-with-voice__done-button"
+						variant="primary"
+						onClick={ stop }
+						disabled={ state === 'inactive' }
+					>
+						{ __( 'Done', 'jetpack' ) }
+					</Button>
+				</div>
 			</Placeholder>
 		</div>
 	);
