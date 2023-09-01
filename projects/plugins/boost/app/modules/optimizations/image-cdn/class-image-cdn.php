aeaccbbab0b6fa5b17add319aa47645515d473ab
Adnan Haque <adnan007.id@gmail.com>
2023-09-01 11:37:55 +0600

    Apply quality settings based on image type

--- projects/plugins/boost/app/modules/optimizations/image-cdn/class-image-cdn.php
+++ projects/plugins/boost/app/modules/optimizations/image-cdn/class-image-cdn.php
@@ -11,7 +11,7 @@ class Image_CDN implements Pluggable {
 	public function setup() {
 		Image_CDN_Setup::load();
 
-		add_filter( 'jetpack_photon_pre_args', array( $this, 'add_quality_args' ) );
+		add_filter( 'jetpack_photon_pre_args', array( $this, 'add_quality_args' ), 10, 2 );
 	}
 
 	public static function get_slug() {
@@ -29,11 +29,49 @@ class Image_CDN implements Pluggable {
 	 *
 	 * @return mixed
 	 */
-	public function add_quality_args( $args ) {
-		if ( Premium_Features::has_feature( Premium_Features::IMAGE_CDN_QUALITY ) ) {
-			$args['quality'] = jetpack_boost_ds_get( 'image_cdn_quality' );
+	public function add_quality_args( $args, $image_url ) {
+		$quality = $this->get_quality_for_image( $image_url );
+		if ( Premium_Features::has_feature( Premium_Features::IMAGE_CDN_QUALITY ) && $quality !== null ) {
+			$args['quality'] = $quality;
 		}
 
 		return $args;
+	}
+
+	/**
+	 * Get the quality for an image based on the extension.
+	 */
+	private function get_quality_for_image( $image_url ) {
+		// Define an associative array to map extensions to image types
+		$extension_to_quality = array(
+			'jpg'  => $this->get_quality_for_type( 'jpg' ),
+			'jpeg' => $this->get_quality_for_type( 'jpg' ),
+			'webp' => $this->get_quality_for_type( 'webp' ),
+			'png'  => $this->get_quality_for_type( 'png' ),
+		);
+
+		// Extract the file extension from the URL
+		$file_extension = pathinfo( $image_url, PATHINFO_EXTENSION );
+
+		// Convert the extension to lowercase for case-insensitive comparison
+		$file_extension = strtolower( $file_extension );
+
+		// Determine the image type based on the extension
+		if ( isset( $extension_to_quality[ $file_extension ] ) ) {
+			return $extension_to_quality[ $file_extension ];
+		}
+
+		return null;
+	}
+
+	private function get_quality_for_type( $image_type ) {
+		$quality_settings = jetpack_boost_ds_get( 'image_cdn_quality' );
+
+		if ( ! isset( $quality_settings[ $image_type ] ) ) {
+			return null;
+		}
+
+		// Passing 100 to photon will result in a lossless image
+		return $quality_settings[ $image_type ]['lossless'] ? 100 : $quality_settings[ $image_type ]['quality'];
 	}
 }
