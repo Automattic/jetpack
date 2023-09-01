d57e95f1265235cc0a7ca17dc31831c393d3d4b7
Adnan Haque <adnan007.id@gmail.com>
2023-09-01 12:22:17 +0600

    Make image quality settings collapsible

---
+++ projects/plugins/boost/app/assets/src/js/pages/settings/elements/CollapsibleMeta.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import PencilIcon from '../../../svg/pencil.svg';
+
+	export let editText: string;
+	export let closeEditText: string;
+
+	let isEditing = false;
+</script>
+
+<div class="jb-collapsible-meta">
+	<header class="jb-collapsible-meta__header">
+		<button
+			type="button"
+			class="jb-collapsible-meta__edit-button components-button is-link"
+			on:click={() => {
+				isEditing = ! isEditing;
+			}}
+		>
+			<PencilIcon class="edit-icon" />
+			{isEditing ? closeEditText : editText}
+		</button>
+
+		<slot name="header" />
+	</header>
+
+	{#if isEditing}
+		<slot />
+	{:else}
+		<div class="summary">
+			<slot name="summary" />
+		</div>
+	{/if}
+</div>
+
+<style lang="scss">
+	@use '../../../../css/main/variables.scss' as *;
+
+	.jb-collapsible-meta {
+		&__edit-button {
+			float: right;
+		}
+
+		.summary {
+			color: $gray-40;
+			font-size: 14px;
+			line-height: 22px;
+		}
+	}
+
+	:global( .jb-collapsible-meta__edit-button .edit-icon ) {
+		fill: $jetpack-green-50;
+		margin: 4px 4px 2px 0;
+	}
+</style>
--- projects/plugins/boost/app/assets/src/js/pages/settings/elements/ImageCdnQualitySettings.svelte
+++ projects/plugins/boost/app/assets/src/js/pages/settings/elements/ImageCdnQualitySettings.svelte
@@ -1,41 +1,64 @@
 <script lang="ts">
-	import { __ } from '@wordpress/i18n';
+	import { __, sprintf } from '@wordpress/i18n';
 	import Tooltip from '../../../elements/Tooltip.svelte';
 	import { imageCdnQuality } from '../../../stores/image-cdn';
 	import { premiumFeatures } from '../../../stores/premium-features';
+	import CollapsibleMeta from './CollapsibleMeta.svelte';
 	import ImageCdnQualityControl from './ImageCdnQualityControl.svelte';
 </script>
 
 {#if $premiumFeatures.includes( 'image-cdn-quality' )}
-	<div class="jb-image-cdn-quality__section-title">
-		{__( 'Image Quality', 'jetpack-boost' )}
-		<Tooltip>
-			<div slot="title">
-				{__( 'Image Quality', 'jetpack-boost' )}
-			</div>
-			{__(
-				'Select the quality for images served by the CDN. Choosing a lower quality will compress your images and load them faster. If you choose lossless, we will not compress your images.',
-				'jetpack-boost'
-			)}
-		</Tooltip>
+	<CollapsibleMeta
+		editText={__( 'Change Image Quality', 'jetpack-boost' )}
+		closeEditText={__( 'Close', 'jetpack-boost' )}
+	>
+		<div slot="header" class="jb-image-cdn-quality__section-title">
+			{__( 'Image Quality', 'jetpack-boost' )}
+			<Tooltip>
+				<div slot="title">
+					{__( 'Image Quality', 'jetpack-boost' )}
+				</div>
+				{__(
+					'Select the quality for images served by the CDN. Choosing a lower quality will compress your images and load them faster. If you choose lossless, we will not compress your images.',
+					'jetpack-boost'
+				)}
+			</Tooltip>
 
-		<span class="jb-badge">{__( 'Upgraded', 'jetpack-boost' )}</span>
-	</div>
-	<ImageCdnQualityControl
-		label={__( 'JPEG Quality', 'jetpack-boost' )}
-		bind:config={$imageCdnQuality.jpg}
-		maxValue={89}
-	/>
-	<ImageCdnQualityControl
-		label={__( 'PNG Quality', 'jetpack-boost' )}
-		bind:config={$imageCdnQuality.png}
-		maxValue={80}
-	/>
-	<ImageCdnQualityControl
-		label={__( 'WEBP Quality', 'jetpack-boost' )}
-		bind:config={$imageCdnQuality.webp}
-		maxValue={80}
-	/>
+			<span class="jb-badge">{__( 'Upgraded', 'jetpack-boost' )}</span>
+		</div>
+
+		<div slot="summary">
+			{sprintf(
+				/* translators: %1$s is the JPEG quality value, %2$s is PNG quality value, and %3$s is WEBP quality value. Each value may also say 'lossless' */
+				__( 'JPEG Quality: %1$s, PNG Quality: %2$s, WEBP Quality: %3$s', 'jetpack-boost' ),
+				$imageCdnQuality.jpg.lossless
+					? __( 'lossless', 'jetpack-boost' )
+					: $imageCdnQuality.jpg.quality.toString(),
+				$imageCdnQuality.png.lossless
+					? __( 'lossless', 'jetpack-boost' )
+					: $imageCdnQuality.png.quality.toString(),
+				$imageCdnQuality.webp.lossless
+					? __( 'lossless', 'jetpack-boost' )
+					: $imageCdnQuality.webp.quality.toString()
+			)}
+		</div>
+
+		<ImageCdnQualityControl
+			label={__( 'JPEG Quality', 'jetpack-boost' )}
+			bind:config={$imageCdnQuality.jpg}
+			maxValue={89}
+		/>
+		<ImageCdnQualityControl
+			label={__( 'PNG Quality', 'jetpack-boost' )}
+			bind:config={$imageCdnQuality.png}
+			maxValue={80}
+		/>
+		<ImageCdnQualityControl
+			label={__( 'WEBP Quality', 'jetpack-boost' )}
+			bind:config={$imageCdnQuality.webp}
+			maxValue={80}
+		/>
+	</CollapsibleMeta>
 {/if}
 
 <style lang="scss">
