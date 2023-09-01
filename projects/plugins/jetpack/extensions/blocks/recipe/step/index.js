487d6eba447e5b4c21df1409401eecf42afd4e4a
Kevin Zoschke <kevin.zoschke@automattic.com>
2023-09-01 14:12:50 -0400

    [not verified] Fix missing imports in Recipe child blocks

--- projects/plugins/jetpack/extensions/blocks/recipe/step/index.js
+++ projects/plugins/jetpack/extensions/blocks/recipe/step/index.js
@@ -1,7 +1,7 @@
 import { Fragment } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-import { getIconColor } from '../../../shared/block-icons';
-import icon from '../icon';
+import { getClientBlockIconProp } from '../../../shared/get-block-icon-from-metadata';
+import metadata from '../block.json';
 import edit from './edit';
 import save from './save';
 
@@ -15,10 +15,7 @@ export const settings = {
 		</Fragment>
 	),
 	keywords: [],
-	icon: {
-		src: icon,
-		foreground: getIconColor(),
-	},
+	icon: getClientBlockIconProp( metadata ),
 	category: 'widgets',
 	edit,
 	save,
