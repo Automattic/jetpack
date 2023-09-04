3c6cd66913f5c5e67e8294f2a0902d2b056d94d2
Damián Suárez <rdsuarez@gmail.com>
2023-09-04 11:00:41 -0300

    AI Client: introduce audio duration display component (#32825)
    * [not verified] add super basic cmp
    
    * [not verified] Add getDuration() helper fn
    
    * [not verified] format time in mm:ss
    
    * [not verified] remove audio duration dusplay story
    
    * [not verified] useMediaRecording() returns media url
    
    * [not verified] show audio duration in hook story
    
    * [not verified] changelog
    
    * [not verified] export AudioDurationDisplay component
    
    * bump pkg versions
    
    * changelog

--- projects/js-packages/ai-client/src/components/index.ts
+++ projects/js-packages/ai-client/src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as AIControl } from './ai-control';
 export { default as AiStatusIndicator } from './ai-status-indicator';
+export { default as AudioDurationDisplay } from './audio-duration-display';
