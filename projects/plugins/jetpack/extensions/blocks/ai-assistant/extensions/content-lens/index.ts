1792e94ce3096febdd671a5ae4d4fdb88abf0b0a
Damián Suárez <rdsuarez@gmail.com>
2023-09-05 16:16:26 +0100

    [not verified] extend only if beta extension is enabled

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/content-lens/index.ts
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/content-lens/index.ts
@@ -5,9 +5,17 @@ import { registerPlugin } from '@wordpress/plugins';
 /**
  * Internal dependencies
  */
+import { isPossibleToExtendBlock } from '../ai-assistant';
 import { PluginDocumentSettingPanelAiExcerpt } from './plugins/ai-post-excerpt';
 
-// AI-driven Post Excerpt
-registerPlugin( 'jetpack-ai-post-excerpt-panel', {
-	render: PluginDocumentSettingPanelAiExcerpt,
-} );
+export const AI_CONTENT_LENS = 'ai-content-lens' as const;
+
+const isAiAssistantSupportExtensionEnabled =
+	window?.Jetpack_Editor_Initial_State.available_blocks[ 'ai-content-lens' ];
+
+// AI Content Lens features
+if ( isPossibleToExtendBlock() && isAiAssistantSupportExtensionEnabled.available ) {
+	registerPlugin( 'jetpack-ai-post-excerpt-panel', {
+		render: PluginDocumentSettingPanelAiExcerpt,
+	} );
+}
