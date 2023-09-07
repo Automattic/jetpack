9c50cbec689fe5af1226b04b87c097a27b9b9feb
Kevin Zoschke <kevin.zoschke@automattic.com>
2023-09-07 17:55:38 -0400

    [not verified] Update path passed to jetpack_register_block

--- projects/plugins/jetpack/extensions/blocks/recipe/class-jetpack-recipe-block.php
+++ projects/plugins/jetpack/extensions/blocks/recipe/class-jetpack-recipe-block.php
@@ -7,7 +7,6 @@
 
 namespace Automattic\Jetpack\Extensions\Recipe;
 
-use Automattic\Jetpack\Blocks;
 use Jetpack_Gutenberg;
 
 /**
@@ -28,7 +27,7 @@ class Jetpack_Recipe_Block {
 	 * @return string
 	 */
 	public static function render( $attr, $content ) {
-		Jetpack_Gutenberg::load_assets_as_required( Blocks::get_path_to_block_metadata( __DIR__ ) );
+		Jetpack_Gutenberg::load_assets_as_required( __DIR__ );
 
 		$find    = array(
 			'/(class="wp-block-jetpack-recipe(\s|"))/',
