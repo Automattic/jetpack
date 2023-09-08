3240af337d69f0e058e794ccfe882f124a632d21
Anthony Grullon <agrullon95@gmail.com>
2023-09-08 14:08:29 -0400

    Fix image attributes and unlock template

--- projects/plugins/jetpack/extensions/blocks/blogroll/blogroll-item/edit.js
+++ projects/plugins/jetpack/extensions/blocks/blogroll/blogroll-item/edit.js
@@ -3,45 +3,45 @@ import { Button } from '@wordpress/components';
 import './editor.scss';
 import { __ } from '@wordpress/i18n';
 
-function BlogrollItemEdit( { className, attributes, setAttributes } ) {
+function BlogrollItemEdit({ className, attributes, setAttributes }) {
 	const { icon, name, description } = attributes;
-	const blockProps = useBlockProps( { className } );
+	const blockProps = useBlockProps({ className });
 
 	return (
-		<div { ...blockProps }>
+		<div {...blockProps}>
 			<MediaUpload
-				multiple={ false }
-				onSelect={ media => {
-					setAttributes( { icon: media.url } );
-				} }
-				render={ ( { open } ) => (
-					<Button variant="link" onClick={ open } style={ { padding: 0 } }>
+				multiple={false}
+				onSelect={media => {
+					setAttributes({ icon: media.url });
+				}}
+				render={({ open }) => (
+					<Button variant="link" onClick={open} style={{ padding: 0 }}>
 						<figure>
-							<img src={ icon } alt={ name } />
+							<img src={icon} alt={name} />
 						</figure>
 					</Button>
-				) }
+				)}
 			/>
 			<div>
 				<a>
 					<RichText
 						className="jetpack-blogroll-item-title"
-						value={ name }
-						tagName={ 'h3' }
-						allowedFormats={ [ 'core/bold', 'core/italic' ] }
-						onChange={ value => {
-							setAttributes( { name: value } );
-						} }
-						placeholder={ __( 'Enter site title', 'jetpack' ) }
+						value={name}
+						tagName={'h3'}
+						allowedFormats={['core/bold', 'core/italic']}
+						onChange={value => {
+							setAttributes({ name: value });
+						}}
+						placeholder={__('Enter site title', 'jetpack')}
 					/>
 				</a>
 				<RichText
 					className="jetpack-blogroll-item-description"
-					value={ description }
-					onChange={ value => {
-						setAttributes( { description: value } );
-					} }
-					placeholder={ __( 'Enter site description', 'jetpack' ) }
+					value={description}
+					onChange={value => {
+						setAttributes({ description: value });
+					}}
+					placeholder={__('Enter site description', 'jetpack')}
 				/>
 			</div>
 		</div>
