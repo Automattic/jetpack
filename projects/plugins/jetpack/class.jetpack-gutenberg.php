c0f3324db3ccc09224038b5eda53df22e63fbd92
Gergely Márk Juhász <36671565+gmjuhasz@users.noreply.github.com>
2023-09-08 10:13:34 +0200

    Social: Add the change setting logic to the auto-conversion feature (#32712)
    * Add new notice type
    
    * Connect isAutoConversionAvailable to the store
    
    * Update restriction handler hook
    
    * Update the notice in the editor
    
    * changelog
    
    * Fixup versions
    
    * Add usePageHasFocus hook and new store action
    
    * Add logic to be able to change the setting and see it refresh
    
    * Remove unwanted linter change
    
    * Remove unwanted linter change
    
    * changelog
    
    * Use package for visibility check, refresh only after clicked change settings
    
    * Fixup versions
    
    * Init autoConversionSettings for the store from wpcom
    
    * Refresh if notice available and only once on focus
    
    * Fix button layout
    
    * Move logic to hook
    
    * Update projects/js-packages/publicize-components/src/components/form/styles.module.scss
    
    Good one thanks!
    
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    
    ---------
    
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>

--- projects/plugins/jetpack/class.jetpack-gutenberg.php
+++ projects/plugins/jetpack/class.jetpack-gutenberg.php
@@ -744,8 +744,11 @@ class Jetpack_Gutenberg {
 				'dismissedNotices'                => $publicize->get_dismissed_notices(),
 				'isInstagramConnectionSupported'  => $publicize->has_instagram_connection_feature(),
 				'isMastodonConnectionSupported'   => $publicize->has_mastodon_connection_feature(),
-				'isAutoConversionAvailable'       => $auto_conversion_settings->is_available( 'image' ),
-				'isAutoConversionEnabled'         => $auto_conversion_settings->is_enabled( 'image' ),
+				'autoConversionSettings'          => array(
+					'available' => $auto_conversion_settings->is_available( 'image' ),
+					'image'     => $auto_conversion_settings->is_enabled( 'image' ),
+				),
+				'jetpackSharingSettingsUrl'       => esc_url_raw( admin_url( 'admin.php?page=jetpack#/sharing' ) ),
 			);
 		}
 
