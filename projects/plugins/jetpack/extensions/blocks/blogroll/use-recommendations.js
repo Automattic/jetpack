8b334e7d49e6ee4eb3a234fad8a04d3b1cee043a
Anthony Grullon <agrullon95@gmail.com>
2023-09-08 14:09:10 -0400

    Allow disabling of useRecommendation hook

--- projects/plugins/jetpack/extensions/blocks/blogroll/use-recommendations.js
+++ projects/plugins/jetpack/extensions/blocks/blogroll/use-recommendations.js
@@ -2,36 +2,36 @@ import apiFetch from '@wordpress/api-fetch';
 import { useEffect, useRef, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
-export default function useRecommendations( enabled = false ) {
-	const [ isLoading, setIsLoading ] = useState( false );
-	const [ errorMessage, setErrorMessage ] = useState( null );
-	const [ recommendations, setRecommendations ] = useState( [] );
+export default function useRecommendations(enabled = false) {
+	const [isLoading, setIsLoading] = useState(false);
+	const [errorMessage, setErrorMessage] = useState(null);
+	const [recommendations, setRecommendations] = useState([]);
 	const abortControllerRef = useRef();
 
-	useEffect( () => {
-		if ( ! enabled ) {
-			setIsLoading( false );
+	useEffect(() => {
+		if (!enabled) {
+			setIsLoading(false);
 			return;
 		}
 
-		setIsLoading( true );
-		setErrorMessage( null );
+		setIsLoading(true);
+		setErrorMessage(null);
 
-		if ( abortControllerRef.current ) {
+		if (abortControllerRef.current) {
 			abortControllerRef.current.abort();
 		}
 
 		abortControllerRef.current =
 			typeof AbortController === 'undefined' ? undefined : new AbortController();
 
-		apiFetch( {
+		apiFetch({
 			path: '/wpcom/v2/following/recommendations',
 			global: true,
 			method: 'GET',
 			signal: abortControllerRef.current?.signal,
-		} )
-			.then( response => {
-				if ( ! response.length ) {
+		})
+			.then(response => {
+				if (!response.length) {
 					setErrorMessage(
 						__(
 							'No subscriptions found. You need to follow some sites in order to see results.',
@@ -39,26 +39,26 @@ export default function useRecommendations( enabled = false ) {
 						)
 					);
 				}
-				setRecommendations( response );
-			} )
-			.catch( error => {
-				if ( error.name === 'AbortError' ) {
+				setRecommendations(response);
+			})
+			.catch(error => {
+				if (error.name === 'AbortError') {
 					return;
 				}
 
-				if ( error.message ) {
-					setErrorMessage( error.message ); // Message was already translated by the backend
+				if (error.message) {
+					setErrorMessage(error.message); // Message was already translated by the backend
 				} else {
 					setErrorMessage(
-						__( 'Whoops, we have encountered an error. Please try again later.', 'jetpack' )
+						__('Whoops, we have encountered an error. Please try again later.', 'jetpack')
 					);
 				}
-			} )
-			.finally( () => {
+			})
+			.finally(() => {
 				abortControllerRef.current = null;
-				setIsLoading( false );
-			} );
-	}, [ enabled ] );
+				setIsLoading(false);
+			});
+	}, [enabled]);
 
 	return { isLoading, errorMessage, recommendations };
 }
