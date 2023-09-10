8ceaa58723a9e38d19d59ab81e7c24d7dedbab76
Andrés Blanco <andres.blanco@automattic.com>
2023-09-10 17:20:32 -0500

    Update projects/packages/jetpack-mu-wpcom/src/features/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-launchpad-navigator.php
    Co-authored-by: daledupreez <dale@automattic.com>

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-launchpad-navigator.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-launchpad-navigator.php
@@ -9,7 +9,7 @@
 /**
  * Fetches Launchpad Navigator-related data for the site.
  *
- * @since 1.1.0
+ * @since 4.9.0
  */
 class WPCOM_REST_API_V2_Endpoint_Launchpad_Navigator extends WP_REST_Controller {
 
