3f1e2f5cb5cd52693f1e472b60a4a62c06164acc
Andrés Blanco <email@gmail.com>
2023-09-10 16:45:23 -0500

    [not verified] update current active list

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-launchpad-navigator.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-launchpad-navigator.php
@@ -37,7 +37,49 @@ class WPCOM_REST_API_V2_Endpoint_Launchpad_Navigator extends WP_REST_Controller 
 					'callback'            => array( $this, 'get_navigator_data' ),
 					'permission_callback' => array( $this, 'can_access' ),
 				),
+				array(
+					'methods'             => WP_REST_Server::EDITABLE,
+					'callback'            => array( $this, 'update_navigator_options' ),
+					'permission_callback' => array( $this, 'can_access' ),
+					'args'                => array(
+						'active_checklist_slug' => array(
+							'description' => 'The slug of the checklist to set as active.',
+							'type'        => 'string',
+							'enum'        => $this->get_checklist_slug_enums(),
+						),
+					),
+				),
 			)
+		);
+	}
+
+	/**
+	 * Returns all available checklist slugs.
+	 * TODO: This function is used by both endpoints, we should move it somewhere common.
+	 *
+	 * @return array Array of checklist slugs.
+	 */
+	public function get_checklist_slug_enums() {
+		$checklists = wpcom_launchpad_checklists()->get_all_task_lists();
+		return array_keys( $checklists );
+	}
+
+	/**
+	 * Updates Launchpad navigator-related options and returns the result
+	 *
+	 * @param WP_REST_Request $request Request object.
+	 */
+	public function update_navigator_options( $request ) {
+		$input = $request->get_json_params();
+
+		if ( ! isset( $input['active_checklist_slug'] ) ) {
+			return;
+		}
+
+		$updated = wpcom_launchpad_set_current_active_checklist( $input['active_checklist_slug'] );
+
+		return array(
+			'updated' => $updated,
 		);
 	}
 
