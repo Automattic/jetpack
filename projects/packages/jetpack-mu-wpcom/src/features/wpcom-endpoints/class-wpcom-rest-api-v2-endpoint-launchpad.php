8aa3d855ee3f8fb9c2c0de4afa2ab0adf99a17ab
Andrés Blanco <email@gmail.com>
2023-09-10 16:00:50 -0500

    [not verified] updates

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-launchpad.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-launchpad.php
@@ -113,8 +113,8 @@ class WPCOM_REST_API_V2_Endpoint_Launchpad extends WP_REST_Controller {
 		}
 
 		return array(
-			'checklists'       => $checklists,
-			'active_checklist' => 'build-intent',
+			'available_checklists' => $checklists,
+			'current_checklist'    => 'intent-build',
 		);
 	}
 
