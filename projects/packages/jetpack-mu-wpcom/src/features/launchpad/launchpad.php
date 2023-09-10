d210a6568e2197f1f55fdc054b73a137c4f4c0fa
Andrés Blanco <email@gmail.com>
2023-09-10 16:53:05 -0500

    [not verified] update option name

--- projects/packages/jetpack-mu-wpcom/src/features/launchpad/launchpad.php
+++ projects/packages/jetpack-mu-wpcom/src/features/launchpad/launchpad.php
@@ -719,11 +719,11 @@ function wpcom_launchpad_set_current_active_checklist( $checklist_slug ) {
 		$wpcom_launchpad_config = array();
 	}
 
-	if ( isset( $wpcom_launchpad_config['current_checklist_slug'] ) && $checklist_slug === $wpcom_launchpad_config['current_checklist_slug'] ) {
+	if ( isset( $wpcom_launchpad_config['active_checklist_slug'] ) && $checklist_slug === $wpcom_launchpad_config['active_checklist_slug'] ) {
 		return true;
 	}
 
-	$wpcom_launchpad_config['current_checklist_slug'] = $checklist_slug;
+	$wpcom_launchpad_config['active_checklist_slug'] = $checklist_slug;
 
 	return update_option( 'wpcom_launchpad_config', $wpcom_launchpad_config );
 }
