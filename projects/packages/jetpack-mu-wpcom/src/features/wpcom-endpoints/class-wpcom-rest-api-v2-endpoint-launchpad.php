2e3ca69fbc660b94f9ebd76bedc7a8df022282ee
Andrés Blanco <email@gmail.com>
2023-09-10 16:07:03 -0500

    [not verified] separate file

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-launchpad.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-launchpad.php
@@ -82,40 +82,6 @@ class WPCOM_REST_API_V2_Endpoint_Launchpad extends WP_REST_Controller {
 				),
 			)
 		);
-
-		// Register rest route for getting a list of available checklists and the currently active checklist.
-		register_rest_route(
-			$this->namespace,
-			$this->rest_base . '/navigator',
-			array(
-				array(
-					'methods'             => WP_REST_Server::READABLE,
-					'callback'            => array( $this, 'get_navigator_data' ),
-					'permission_callback' => array( $this, 'can_access' ),
-				),
-			)
-		);
-	}
-
-	/**
-	 * Returns a list of available checklists and the currently active checklist.
-	 *
-	 * @return array Array with two keys: `checklists` and `active_checklist`.
-	 */
-	public function get_navigator_data() {
-		$raw_checklists = wpcom_launchpad_checklists()->get_all_task_lists();
-		$checklists     = array();
-		foreach ( $raw_checklists as $slug => $checklist ) {
-			$checklists[] = array(
-				'slug'  => $slug,
-				'title' => $checklist['title'],
-			);
-		}
-
-		return array(
-			'available_checklists' => $checklists,
-			'current_checklist'    => 'intent-build',
-		);
 	}
 
 	/**
