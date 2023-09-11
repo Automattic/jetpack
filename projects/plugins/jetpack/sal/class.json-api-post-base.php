210e43bb50e81206846403fe0b43c55a407dec64
Brandon Kraft <public@brandonkraft.com>
2023-09-11 02:15:08 -0500

    SAL: Defenseive coding for optionally returned item (#32961)

--- projects/plugins/jetpack/sal/class.json-api-post-base.php
+++ projects/plugins/jetpack/sal/class.json-api-post-base.php
@@ -915,7 +915,7 @@ abstract class SAL_Post {
 
 		$file      = basename( wp_get_attachment_url( $media_item->ID ) );
 		$file_info = pathinfo( $file );
-		$ext       = $file_info['extension'];
+		$ext       = isset( $file_info['extension'] ) ? $file_info['extension'] : '';
 
 		$response = array(
 			'ID'          => $media_item->ID,
