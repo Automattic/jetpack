8a9b0dd5ef83a9ed69d675f0d00e059f7bebbca9
Igor Zinovyev <zinigor@gmail.com>
2023-09-11 19:12:43 +0300

    Backport social 2.3.0-beta, backup 2.2-beta, mu-wpcom-plugin 1.6.19, jetpack 12.6-a.5 Changes (#32978)
    * Changelog edits.
    
    * Init new cycle

--- projects/packages/my-jetpack/src/class-initializer.php
+++ projects/packages/my-jetpack/src/class-initializer.php
@@ -31,7 +31,7 @@ class Initializer {
 	 *
 	 * @var string
 	 */
-	const PACKAGE_VERSION = '3.4.4-alpha';
+	const PACKAGE_VERSION = '3.4.4';
 
 	/**
 	 * HTML container ID for the IDC screen on My Jetpack page.
