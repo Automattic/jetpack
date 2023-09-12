61420132cd20aaa2f047814506d6bc2dba566adc
Kevin Zoschke <kevin.zoschke@automattic.com>
2023-09-12 14:37:02 -0400

    Refactor Eventbrite block registration

--- projects/plugins/jetpack/extensions/blocks/eventbrite/test/validate.js
+++ projects/plugins/jetpack/extensions/blocks/eventbrite/test/validate.js
@@ -1,10 +1,10 @@
-import { name, settings } from '../';
 import runBlockFixtureTests from '../../../shared/test/block-fixtures';
-// The Eventbrite block uses the Button block in Innerblocks so we have to import the button block type definitions.
 import { settings as buttonSettings } from '../../button';
+import metadata from '../block.json';
 
 const blocks = [
-	{ name: `jetpack/${ name }`, settings },
+	{ name, settings: metadata },
 	{ name: 'jetpack/button', settings: buttonSettings },
 ];
-runBlockFixtureTests( `jetpack/${ name }`, blocks, __dirname );
+
+runBlockFixtureTests( name, blocks, __dirname );
