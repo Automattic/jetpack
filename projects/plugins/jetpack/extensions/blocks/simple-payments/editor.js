9c40e4c3e14ecdeaa3fde9eb587c7f9508742bb4
Kevin Zoschke <kevin.zoschke@automattic.com>
2023-09-13 09:23:05 -0400

    Refactor Simple Payments block registration

--- projects/plugins/jetpack/extensions/blocks/simple-payments/editor.js
+++ projects/plugins/jetpack/extensions/blocks/simple-payments/editor.js
@@ -1,4 +1,50 @@
-import registerJetpackBlock from '../../shared/register-jetpack-block';
-import { name, settings } from '.';
+import { __ } from '@wordpress/i18n';
+import { registerJetpackBlockFromMetadata } from '../../shared/register-jetpack-block';
+import metadata from './block.json';
+import deprecatedV1 from './deprecated/v1';
+import deprecatedV2 from './deprecated/v2';
+import edit from './edit';
+import save from './save';
+import simplePaymentsExample1 from './simple-payments_example-1.jpg';
+import './editor.scss';
 
-registerJetpackBlock( name, settings );
+registerJetpackBlockFromMetadata( metadata, {
+	edit,
+	save,
+	example: {
+		attributes: {
+			price: 25.0,
+			title: __( 'Jetpack t-shirt', 'jetpack' ),
+			content: __(
+				'Take flight in ultimate comfort with this stylish t-shirt featuring the Jetpack logo.',
+				'jetpack'
+			),
+			email: 'jetpack@jetpack.com',
+			featuredMediaUrl: simplePaymentsExample1,
+		},
+	},
+	transforms: {
+		from: [
+			{
+				type: 'shortcode',
+				tag: 'simple-payment',
+				attributes: {
+					productId: {
+						type: 'number',
+						shortcode: ( { named: { id } } ) => {
+							if ( ! id ) {
+								return;
+							}
+
+							const result = parseInt( id, 10 );
+							if ( result ) {
+								return result;
+							}
+						},
+					},
+				},
+			},
+		],
+	},
+	deprecated: [ deprecatedV1, deprecatedV2 ],
+} );
--- projects/plugins/jetpack/extensions/blocks/simple-payments/test/validate.js
+++ projects/plugins/jetpack/extensions/blocks/simple-payments/test/validate.js
@@ -1,15 +1,14 @@
-import { settings } from '../';
 import runBlockFixtureTests from '../../../shared/test/block-fixtures';
+import metadata from '../block.json';
 
 const intlNumberFormatSpy = jest.spyOn( Intl, 'NumberFormat' );
+
 beforeEach( () => {
 	intlNumberFormatSpy
 		.mockReset()
 		.mockImplementation( () => ( { format: value => `A$${ value.toString() }.00` } ) );
 } );
+const { name } = metadata;
+const blocks = [ { name, settings: metadata } ];
 
-// Need to include all the blocks involved in rendering this block.
-// The main block should be the first in the array.
-const blocks = [ { name: 'jetpack/simple-payments', settings } ];
-
-runBlockFixtureTests( 'jetpack/simple-payments', blocks, __dirname );
+runBlockFixtureTests( name, blocks, __dirname );
