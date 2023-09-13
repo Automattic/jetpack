fc5772710d81e57bce233f816432b171708799e5
Kevin Zoschke <kevin.zoschke@automattic.com>
2023-09-12 20:11:54 -0400

    Refactor Pinterest block registration

--- projects/plugins/jetpack/extensions/blocks/pinterest/utils.js
+++ projects/plugins/jetpack/extensions/blocks/pinterest/utils.js
@@ -1,10 +1,10 @@
 import { getPath } from '@wordpress/url';
-import { URL_REGEX } from '.';
+import { URL_REGEX } from './constants';
 
 /**
  * Determines the Pinterest embed type from the URL.
  *
- * @param {string} url The URL to check.
+ * @param {string} url - The URL to check.
  * @returns {string} The pin type. Empty string if it isn't a valid Pinterest URL.
  */
 export function pinType( url ) {
