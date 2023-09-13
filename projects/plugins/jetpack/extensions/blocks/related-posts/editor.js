258eeff5c6218a3b6a414a6d1393b16688cf9dea
Kevin Zoschke <kevin.zoschke@automattic.com>
2023-09-13 08:33:17 -0400

    Refactor Related Post block registration

--- projects/plugins/jetpack/extensions/blocks/related-posts/editor.js
+++ projects/plugins/jetpack/extensions/blocks/related-posts/editor.js
@@ -1,4 +1,18 @@
-import registerJetpackBlock from '../../shared/register-jetpack-block';
-import { name, settings } from '.';
+import { registerJetpackBlockFromMetadata } from '../../shared/register-jetpack-block';
+import metadata from './block.json';
+import edit from './edit';
 
-registerJetpackBlock( name, settings );
+import './style.scss';
+
+registerJetpackBlockFromMetadata( metadata, {
+	edit,
+	save: () => null,
+	transforms: {
+		from: [
+			{
+				type: 'shortcode',
+				tag: 'jetpack-related-posts',
+			},
+		],
+	},
+} );
