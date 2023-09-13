54921ca28cc70074d0ef8a3ea677f753dfd055b3
Kevin Zoschke <kevin.zoschke@automattic.com>
2023-09-13 08:58:40 -0400

    Refactor Send a Message block registration

--- projects/plugins/jetpack/extensions/blocks/send-a-message/editor.js
+++ projects/plugins/jetpack/extensions/blocks/send-a-message/editor.js
@@ -1,13 +1,32 @@
-import registerJetpackBlock from '../../shared/register-jetpack-block';
+import { InnerBlocks } from '@wordpress/block-editor';
+import { registerJetpackBlockFromMetadata } from '../../shared/register-jetpack-block';
+import metadata from './block.json';
+import edit from './edit';
+import variations from './variations';
 import {
 	name as whatsAppButtonBlockName,
 	settings as whatsAppButtonBlockSettings,
 } from './whatsapp-button';
-import { name, settings } from '.';
 
-registerJetpackBlock( name, settings, [
+import './editor.scss';
+
+registerJetpackBlockFromMetadata(
+	metadata,
 	{
-		name: whatsAppButtonBlockName,
-		settings: whatsAppButtonBlockSettings,
+		edit,
+		save: props => {
+			return (
+				<div className={ props.className }>
+					<InnerBlocks.Content />
+				</div>
+			);
+		},
+		variations,
 	},
-] );
+	[
+		{
+			name: whatsAppButtonBlockName,
+			settings: whatsAppButtonBlockSettings,
+		},
+	]
+);
