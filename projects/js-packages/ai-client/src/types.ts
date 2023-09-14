4f1c27abbdfe6f2afc00cb85788e66e8ae4460ca
Damián Suárez <rdsuarez@gmail.com>
2023-09-14 16:42:29 +0100

    register model consts and types

--- projects/js-packages/ai-client/src/types.ts
+++ projects/js-packages/ai-client/src/types.ts
@@ -35,7 +35,6 @@ export type { RequestingErrorProps } from './hooks/use-ai-suggestions';
 /*
  * Requests types
  */
-
 const REQUESTING_STATE_INIT = 'init' as const;
 const REQUESTING_STATE_REQUESTING = 'requesting' as const;
 const REQUESTING_STATE_SUGGESTING = 'suggesting' as const;
@@ -51,3 +50,11 @@ export const REQUESTING_STATES = [
 ] as const;
 
 export type RequestingStateProp = ( typeof REQUESTING_STATES )[ number ];
+
+/*
+ * Model types and constants
+ */
+export const AI_MODEL_GPT_3_5_Turbo_16K = 'gpt-3.5-turbo-16k' as const;
+export const AI_MODEL_GPT_4 = 'gpt-4' as const;
+
+export type AiModelTypeProp = typeof AI_MODEL_GPT_3_5_Turbo_16K | typeof AI_MODEL_GPT_4;
