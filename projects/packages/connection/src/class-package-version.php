8698f61457ad3ce058fa855bfc08c9ff779777be
Brad Jorsch <brad.jorsch@automattic.com>
2023-09-14 11:56:58 -0400

    Change entries and version bumps

--- projects/packages/connection/src/class-package-version.php
+++ projects/packages/connection/src/class-package-version.php
@@ -12,7 +12,7 @@ namespace Automattic\Jetpack\Connection;
  */
 class Package_Version {
 
-	const PACKAGE_VERSION = '1.57.4';
+	const PACKAGE_VERSION = '1.57.5-alpha';
 
 	const PACKAGE_SLUG = 'connection';
 
