97b57320cef6ccf43207f3d8664ece8addbcc2d8
gogdzl <diego.rodrigues@automattic.com>
2023-09-14 23:15:35 -0300

    Fix function zeroBS_getCompanyIDWithName

--- projects/plugins/crm/includes/ZeroBSCRM.DAL3.Obj.Companies.php
+++ projects/plugins/crm/includes/ZeroBSCRM.DAL3.Obj.Companies.php
@@ -334,6 +334,33 @@ class zbsDAL_companies extends zbsDAL_ObjectLayer {
 
     }
 
+	/**
+	 * Retrieves the company ID by its name.
+	 *
+	 * @param  string $name  The name of the company whose ID is to be retrieved.
+	 * @return int|bool      Returns the ID of the company if found, false otherwise.
+	 *
+	 * @throws Exception     Catches and handles exceptions, logging SQL errors.
+	 *
+	 * @since  $$next-version$$
+	 */
+	public function get_company_id_by_name( $name ) {
+		global $ZBSCRM_t; // phpcs:ignore WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase
+		global $wpdb;
+
+		try {
+			$query  = $this->prepare( 'SELECT ID FROM ' . $ZBSCRM_t['companies'] . ' WHERE zbsco_name LIKE %s', $name ); // phpcs:ignore
+			$result = $wpdb->get_row( $query, OBJECT ); // phpcs:ignore
+
+			if ( isset( $result ) && ( $result->ID ) ) {
+				return $result->ID;
+			}
+		} catch ( Exception $e ) {
+			$this->catchSQLError( $e );
+		}
+		return false;
+	}
+
     /**
      * returns full company line +- details
      *
