deeb6fc22bca9cccb188bd9c0d8015282bcaa811
Konstantin Obenland <obenland@gmx.de>
2023-09-15 09:24:49 -0500

    Add new options to sync allow list.
    I took inspiration from #31662, please let me know if this needs more work!

--- projects/plugins/jetpack/tests/php/sync/test_class.jetpack-sync-options.php
+++ projects/plugins/jetpack/tests/php/sync/test_class.jetpack-sync-options.php
@@ -237,6 +237,8 @@ class WP_Test_Jetpack_Sync_Options extends WP_Test_Jetpack_Sync_Base {
 			'wpcom_subscription_emails_use_excerpt'        => false,
 			'launchpad_checklist_tasks_statuses'           => array(),
 			'launchpad_screen'                             => 'full',
+			'wpcom_legacy_contact'                         => '',
+			'wpcom_locked_mode'                            => false,
 			'wpcom_reader_views_enabled'                   => true,
 			'wpcom_site_setup'                             => '',
 		);
