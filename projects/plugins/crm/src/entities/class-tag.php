c6c23209cbf8137a01a24ccf24b492460ac7c009
tbradsha <32492176+tbradsha@users.noreply.github.com>
2023-09-18 14:18:09 -0600

    Define Tag entity

---
+++ projects/plugins/crm/src/entities/class-tag.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Tag Entity.
+ *
+ * @package automattic/jetpack-crm
+ */
+
+namespace Automattic\Jetpack\CRM\Entities;
+
+/**
+ * Tag class.
+ */
+class Tag {
+	/**
+	 * The ID of the tag as found in the database.
+	 *
+	 * @var int
+	 */
+	protected $id = -1;
+
+	/**
+	 * The object type of the tag.
+	 *
+	 * @var int
+	 */
+	protected $obj_type_id = -1;
+
+	/**
+	 * The user-friendly label of the tag.
+	 *
+	 * @var string
+	 */
+	public $label = '';
+
+	/**
+	 * The tag slug.
+	 *
+	 * @var string
+	 */
+	public $slug = '';
+}
