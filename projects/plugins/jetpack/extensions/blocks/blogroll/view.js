36f756df7b9ca26a9145efd537e87f6989417c15
Anthony Grullon <agrullon95@gmail.com>
2023-09-18 02:15:26 -0400

    Update projects/plugins/jetpack/extensions/blocks/blogroll/view.js
    Co-authored-by: Omar Alshaker <omar@omaralshaker.com>

--- projects/plugins/jetpack/extensions/blocks/blogroll/view.js
+++ projects/plugins/jetpack/extensions/blocks/blogroll/view.js
@@ -7,7 +7,7 @@ function toggleSubscribeForm( event ) {
 		parent.querySelector( '.jetpack-blogroll-item-submit' ).removeAttribute( 'disabled' );
 	} else {
 		// Disable the submission field-group. This has two benefits:
-		// 1. Prevents the hidden fields of an active item from being submitted.
+		// 1. Prevents the hidden fields of an inactive item from being submitted.
 		// 2. Prevents the hidden items from stealing focus when tab-navigating.
 		parent.querySelector( '.jetpack-blogroll-item-submit' ).setAttribute( 'disabled', 'disabled' );
 	}
