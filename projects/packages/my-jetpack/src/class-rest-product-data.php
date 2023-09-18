fb419bd16d99b3c24c5c0e21e7f095a82f3489a8
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2023-09-18 12:35:13 -0600

    Update version number

--- projects/packages/my-jetpack/src/class-rest-product-data.php
+++ projects/packages/my-jetpack/src/class-rest-product-data.php
@@ -45,12 +45,12 @@ class REST_Product_Data {
 	 * @return array|WP_Error
 	 */
 	public static function get_all_product_data() {
-		$site_id           = \Jetpack_Options::get_option( 'id' );
-		$wpcom_endpoint    = sprintf( 'sites/%d/jetpack-product-data?locale=%2$s', $site_id, get_user_locale() );
-		$wpcom_api_version = '1.1';
-		$response          = Client::wpcom_json_api_request_as_blog( $wpcom_endpoint, $wpcom_api_version );
-		$response_code     = wp_remote_retrieve_response_code( $response );
-		$body              = json_decode( wp_remote_retrieve_body( $response ) );
+		$site_id        = \Jetpack_Options::get_option( 'id' );
+		$wpcom_endpoint = sprintf( 'sites/%d/jetpack-product-data?locale=%2$s', $site_id, get_user_locale() );
+		$api_version    = '1.2';
+		$response       = Client::wpcom_json_api_request_as_blog( $wpcom_endpoint, $api_version );
+		$response_code  = wp_remote_retrieve_response_code( $response );
+		$body           = json_decode( wp_remote_retrieve_body( $response ) );
 
 		l( $response );
 		if ( is_wp_error( $response ) || empty( $response['body'] ) || 200 !== $response_code ) {
