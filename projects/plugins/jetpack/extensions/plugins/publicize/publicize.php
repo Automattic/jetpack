1eae2828d57edcf039eaa4fa840a771dafdd2ffc
Jeremy Herve <jeremy@jeremy.hu>
2023-09-18 16:56:20 +0200

    Publicize: display the editor modal regardless of module status
    Fixes #12367
    
    - Always register the Publicize editor extension.
    - Add a new placeholder when the panel is originally loading.
    - Add a placeholder with a button to enable the module when it is disabled.
    - Track views of that placeholder and clicks to enable the module.

--- projects/plugins/jetpack/extensions/plugins/publicize/publicize.php
+++ projects/plugins/jetpack/extensions/plugins/publicize/publicize.php
@@ -1,9 +1,47 @@
 <?php
 /**
- * Block Editor - Republicize feature.
+ * Block Editor - Publicize and Republicize features.
  *
  * @package automattic/jetpack
- **/
+ */
+
+namespace Automattic\Jetpack\Extensions\Publicize;
+
+use Automattic\Jetpack\Connection\Manager as Connection_Manager;
+use Automattic\Jetpack\Status;
+use Automattic\Jetpack\Status\Host;
+use Jetpack_Gutenberg;
+
+/**
+ * Register both Publicize and Republicize plugins.
+ *
+ * @return void
+ */
+function register_plugins() {
+	global $publicize;
+
+	// Capability check.
+	if (
+		! $publicize
+		|| ! $publicize->current_user_can_access_publicize_data()
+	) {
+		Jetpack_Gutenberg::set_extension_unavailable( 'jetpack/publicize', 'unauthorized' );
+		return;
+	}
+
+	// Connection check.
+	if (
+		( new Host() )->is_wpcom_simple()
+		|| ( ( new Connection_Manager( 'jetpack' ) )->has_connected_owner() && ! ( new Status() )->is_offline_mode() )
+	) {
+		// Register Publicize.
+		Jetpack_Gutenberg::set_extension_available( 'jetpack/publicize' );
+
+		// Set the republicize availability, depending on the site plan.
+		Jetpack_Gutenberg::set_availability_for_plan( 'republicize' );
+	}
+}
+add_action( 'jetpack_register_gutenberg_extensions', __NAMESPACE__ . '\register_plugins' );
 
 // Populate the available extensions with republicize.
 add_filter(
@@ -15,13 +53,5 @@ add_filter(
 				'republicize',
 			)
 		);
-	}
-);
-
-// Set the republicize availability, depending on the site plan.
-add_action(
-	'jetpack_register_gutenberg_extensions',
-	function () {
-		\Jetpack_Gutenberg::set_availability_for_plan( 'republicize' );
 	}
 );
