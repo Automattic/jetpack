833257fbdf99c5d6d55bbfc74a7e705b05c7911a
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2023-09-18 11:17:59 -0600

    Update api_version

--- projects/packages/my-jetpack/src/class-rest-product-data.php
+++ projects/packages/my-jetpack/src/class-rest-product-data.php
@@ -47,11 +47,12 @@ class REST_Product_Data {
 	public static function get_all_product_data() {
 		$site_id           = \Jetpack_Options::get_option( 'id' );
 		$wpcom_endpoint    = sprintf( 'sites/%d/jetpack-product-data?locale=%2$s', $site_id, get_user_locale() );
-		$wpcom_api_version = '2';
+		$wpcom_api_version = '1.1';
 		$response          = Client::wpcom_json_api_request_as_blog( $wpcom_endpoint, $wpcom_api_version );
 		$response_code     = wp_remote_retrieve_response_code( $response );
 		$body              = json_decode( wp_remote_retrieve_body( $response ) );
 
+		l( $response );
 		if ( is_wp_error( $response ) || empty( $response['body'] ) || 200 !== $response_code ) {
 			return new WP_Error( 'site_products_data_fetch_failed', 'Site products data fetch failed', array( 'status' => $response_code ? $response_code : 400 ) );
 		}
