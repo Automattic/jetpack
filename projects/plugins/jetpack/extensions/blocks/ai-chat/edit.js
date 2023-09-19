49ce199caaffc9a2d5f8011bd0e9d2b4e545dccb
artpi <artur.piszek@gmail.com>
2023-09-19 16:37:38 +0200

    remove functionality from GB, styling

--- projects/plugins/jetpack/extensions/blocks/ai-chat/edit.js
+++ projects/plugins/jetpack/extensions/blocks/ai-chat/edit.js
@@ -1,7 +1,8 @@
 /**
  * WordPress dependencies
  */
-import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
+import { InspectorControls, useBlockProps, RichText } from '@wordpress/block-editor';
+import { TextControl } from '@wordpress/components';
 /**
  * Internal dependencies
  */
@@ -9,7 +10,6 @@ import './editor.scss';
 import ConnectPrompt from './components/nudge-connect';
 import EnableJetpackSearchPrompt from './components/nudge-enable-search';
 import { AiChatControls } from './controls';
-import QuestionAnswer from './question-answer';
 
 export default function Edit( { attributes, setAttributes } ) {
 	const blockProps = useBlockProps();
@@ -17,10 +17,21 @@ export default function Edit( { attributes, setAttributes } ) {
 		<div { ...blockProps }>
 			<ConnectPrompt />
 			<EnableJetpackSearchPrompt />
-			<QuestionAnswer
-				askButtonLabel={ attributes.askButtonLabel }
-				placeholder={ attributes.placeholder }
-			/>
+			<div className="jetpack-ai-chat-question-wrapper">
+				<TextControl
+					className="jetpack-ai-chat-question-input"
+					placeholder={ attributes.placeholder }
+					size={ 50 }
+					disabled={ true }
+				/>
+				<RichText
+					className="wp-block-button__link jetpack-ai-chat-question-button"
+					onChange={ value => setAttributes( { askButtonLabel: value } ) }
+					value={ attributes.askButtonLabel }
+					withoutInteractiveFormatting
+					allowedFormats={ [ 'core/bold', 'core/italic', 'core/strikethrough' ] }
+				/>
+			</div>
 			<InspectorControls>
 				<AiChatControls
 					askButtonLabel={ attributes.askButtonLabel }
