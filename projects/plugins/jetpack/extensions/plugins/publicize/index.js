13262b644b6d7f90e6da48e752fcaa0e512327d5
Jeremy Herve <jeremy@jeremy.hu>
2023-09-19 12:38:26 +0200

    Publicize: display the editor modal regardless of module status (#33149)

--- projects/plugins/jetpack/extensions/plugins/publicize/index.js
+++ projects/plugins/jetpack/extensions/plugins/publicize/index.js
@@ -7,7 +7,6 @@
  * It also hooks into our dedicated Jetpack plugin sidebar and
  * displays the Publicize UI there.
  */
-
 import {
 	TwitterThreadListener,
 	PublicizePanel,
@@ -16,11 +15,16 @@ import {
 	SocialImageGeneratorPanel,
 	PostPublishReviewPrompt,
 } from '@automattic/jetpack-publicize-components';
-import { JetpackEditorPanelLogo } from '@automattic/jetpack-shared-extension-utils';
+import {
+	JetpackEditorPanelLogo,
+	useModuleStatus,
+} from '@automattic/jetpack-shared-extension-utils';
 import { PluginPrePublishPanel } from '@wordpress/edit-post';
 import { PostTypeSupportCheck } from '@wordpress/editor';
 import { __ } from '@wordpress/i18n';
 import JetpackPluginSidebar from '../../shared/jetpack-plugin-sidebar';
+import { PublicizePlaceholder } from './components/placeholder';
+import PublicizeSkeletonLoader from './components/skeleton-loader';
 import UpsellNotice from './components/upsell';
 
 import './editor.scss';
@@ -30,6 +34,32 @@ export const name = 'publicize';
 const PublicizeSettings = () => {
 	const { hasEnabledConnections } = useSocialMediaConnections();
 	const { isSocialImageGeneratorAvailable } = usePublicizeConfig();
+	const { isLoadingModules, isChangingStatus, isModuleActive, changeStatus } =
+		useModuleStatus( name );
+
+	if ( isLoadingModules ) {
+		return (
+			<PostTypeSupportCheck supportKeys="publicize">
+				<JetpackPluginSidebar>
+					<PublicizeSkeletonLoader />
+				</JetpackPluginSidebar>
+			</PostTypeSupportCheck>
+		);
+	}
+
+	if ( ! isModuleActive ) {
+		return (
+			<PostTypeSupportCheck supportKeys="publicize">
+				<JetpackPluginSidebar>
+					<PublicizePlaceholder
+						changeStatus={ changeStatus }
+						isModuleActive={ isModuleActive }
+						isLoading={ isChangingStatus }
+					/>
+				</JetpackPluginSidebar>
+			</PostTypeSupportCheck>
+		);
+	}
 
 	return (
 		<PostTypeSupportCheck supportKeys="publicize">
