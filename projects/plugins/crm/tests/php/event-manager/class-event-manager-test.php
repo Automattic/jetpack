98d7562e3436b2bbad6841930f879df9fa474624
gogdzl <diego.rodrigues@automattic.com>
2023-09-19 22:48:34 -0300

    Add Invoice Event Manager

--- projects/plugins/crm/tests/php/event-manager/class-event-manager-test.php
+++ projects/plugins/crm/tests/php/event-manager/class-event-manager-test.php
@@ -3,6 +3,7 @@
 namespace Automattic\Jetpack\CRM\Event_Manager\Tests;
 
 use Automattic\Jetpack\CRM\Event_Manager\Contact_Event;
+use Automattic\Jetpack\CRM\Event_Manager\Invoice_Event;
 use Automattic\Jetpack\CRM\Tests\JPCRM_Base_Test_Case;
 
 require_once __DIR__ . '/class-event-manager-faker.php';
@@ -160,5 +161,45 @@ class Event_Manager_Test extends JPCRM_Base_Test_Case {
 		$contact_event = new Contact_Event();
 
 		$contact_event->before_delete( $contact_data['id'] );
+	}
+
+	/**
+	 * @testdox Test that invoice created event is fired
+	 */
+	public function test_notify_on_invoice_created() {
+
+		$invoice_data = Event_Manager_Faker::instance()->invoice_data();
+
+		add_action(
+			'jpcrm_invoice_created',
+			function ( $invoice ) use ( $invoice_data ) {
+				$this->assertEquals( $invoice, $invoice_data );
+			},
+			10,
+			1
+		);
+
+		$invoice_event = new Invoice_Event();
+		$invoice_event->created( $invoice_data );
+	}
+
+	/**
+	 * @testdox Test that invoice updated event is fired
+	 */
+	public function test_notify_on_invoice_updated() {
+
+		$invoice_data = Event_Manager_Faker::instance()->invoice_data();
+
+		add_action(
+			'jpcrm_invoice_updated',
+			function ( $invoice ) use ( $invoice_data ) {
+				$this->assertEquals( $invoice, $invoice_data );
+			},
+			10,
+			1
+		);
+
+		$invoice_event = new Invoice_Event();
+		$invoice_event->updated( $invoice_data );
 	}
 }
