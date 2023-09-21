e30109f0c7c2e423b5ce1fe68543b53df4be5b99
Gergely Juhasz <gergely.juhasz@automattic.com>
2023-09-21 14:01:50 +0200

    Use the info modal

--- projects/js-packages/publicize-components/src/components/post-publish-one-click-sharing/index.js
+++ projects/js-packages/publicize-components/src/components/post-publish-one-click-sharing/index.js
@@ -1,30 +1,43 @@
 import { Text, ThemeProvider } from '@automattic/jetpack-components';
 import { JetpackEditorPanelLogo } from '@automattic/jetpack-shared-extension-utils';
+import { Button } from '@wordpress/components';
 import { PluginPostPublishPanel } from '@wordpress/edit-post';
+import { useState, useCallback } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import OneClickSharingModal from '../one-click-sharing-modal';
 import { ShareButtons } from '../share-buttons/share-buttons';
 import styles from './styles.module.scss';
 
 const PostPublishOneClickSharing = () => {
+	const [ isModalOpened, setIsModalOpened ] = useState( false );
+
+	const openModal = useCallback( () => setIsModalOpened( true ), [] );
+	const closeModal = useCallback( () => setIsModalOpened( false ), [] );
+
 	return (
-		<>
-			<PluginPostPublishPanel
-				initialOpen
-				title={ __( 'One-Click sharing', 'jetpack' ) }
-				id="publicize-manual-sharing"
-				icon={ <JetpackEditorPanelLogo /> }
-			>
-				<ThemeProvider>
-					<Text className={ styles.description } variant="body-small">
-						{ __(
-							"Share with a Single Click! ✨ Just tap the Twitter or WhatsApp icon, and we'll prep your content for sharing.",
+		<PluginPostPublishPanel
+			initialOpen
+			title={ __( 'One-Click sharing', 'jetpack' ) }
+			id="publicize-manual-sharing"
+			icon={ <JetpackEditorPanelLogo /> }
+		>
+			<ThemeProvider>
+				<Text className={ styles.description } variant="body-small">
+					{ __( 'Share with a Single Click!', 'jetpack' ) +
+						'✨ ' +
+						__(
+							"Just tap on any of the icons below, and we'll prep your content for sharing.",
 							'jetpack'
 						) }
-					</Text>
-					<ShareButtons />
-				</ThemeProvider>
-			</PluginPostPublishPanel>
-		</>
+					&nbsp;
+					<Button variant="link" onClick={ openModal }>
+						{ __( 'Learn more..', 'jetpack' ) }
+					</Button>
+				</Text>
+				<ShareButtons />
+			</ThemeProvider>
+			{ isModalOpened && <OneClickSharingModal onClose={ closeModal } /> }
+		</PluginPostPublishPanel>
 	);
 };
 
