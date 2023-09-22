6a309234e6b03f038ba99429b6156d4944ad499d
Renan <renansscarvalho@gmail.com>
2023-09-22 16:51:30 +0200

    Improve list rendering

--- projects/plugins/jetpack/extensions/blocks/blogroll/components/blogroll-appender-results/index.js
+++ projects/plugins/jetpack/extensions/blocks/blogroll/components/blogroll-appender-results/index.js
@@ -5,31 +5,34 @@ export default function BlogrollAppenderResults( { results, showPlaceholder, onS
 	return (
 		<div className="jetpack-blogroll__appender-results">
 			{ showPlaceholder && <div aria-autocomplete="list">{ __( 'Suggestions', 'jetpack' ) }</div> }
+
 			{ results.length === 0 && ! showPlaceholder && (
 				<div aria-autocomplete="none">{ __( 'No websites found.', 'jetpack' ) }</div>
 			) }
-			<ul aria-live="polite">
-				{ results.map( result => (
-					<li key={ result.blog_id }>
-						<a
-							className="jetpack-blogroll__appender-result-title"
-							href="#"
-							onClick={ () => onSelect( result ) }
-						>
-							<div className="jetpack-blogroll__appender-result-image">
-								{ result.site_icon && <img src={ result.site_icon } alt={ result.name } /> }
-							</div>
+			{ results.length > 0 && (
+				<ul aria-live="polite">
+					{ results.map( result => (
+						<li key={ result.blog_id }>
+							<a
+								className="jetpack-blogroll__appender-result-title"
+								href="#"
+								onClick={ () => onSelect( result ) }
+							>
+								<div className="jetpack-blogroll__appender-result-image">
+									{ result.site_icon && <img src={ result.site_icon } alt={ result.name } /> }
+								</div>
 
-							<div className="jetpack-blogroll__appender-result-text">
-								<span className="jetpack-blogroll__appender-result-title">{ result.name }</span>
-								<span className="jetpack-blogroll__appender-result-description">
-									{ result.description }
-								</span>
-							</div>
-						</a>
-					</li>
-				) ) }
-			</ul>
+								<div className="jetpack-blogroll__appender-result-text">
+									<span className="jetpack-blogroll__appender-result-title">{ result.name }</span>
+									<span className="jetpack-blogroll__appender-result-description">
+										{ result.description }
+									</span>
+								</div>
+							</a>
+						</li>
+					) ) }
+				</ul>
+			) }
 		</div>
 	);
 }
