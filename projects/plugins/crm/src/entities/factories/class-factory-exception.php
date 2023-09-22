5c831c097c9b765792543a918956fc59ef6988ba
cleacos <javacodia@gmail.com>
2023-09-22 13:25:58 +0100

    Add the general Factory Exception class
    Signed-off-by: cleacos <javacodia@gmail.com>

---
+++ projects/plugins/crm/src/entities/factories/class-factory-exception.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Factory Exception.
+ *
+ * @package automattic/jetpack-crm
+ */
+
+namespace Automattic\Jetpack\CRM\Entities\Factories;
+
+/**
+ * Factory Exception.
+ *
+ * @since $$next-version$$
+ */
+class Factory_Exception extends \Exception {
+
+	/**
+	 * The error code for invalid data.
+	 *
+	 * @since $$next-version$$
+	 * @var int
+	 */
+	const INVALID_DATA = 1;
+}
