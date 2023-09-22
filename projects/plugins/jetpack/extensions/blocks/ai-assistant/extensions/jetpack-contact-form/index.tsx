eb0370ffefa7ddf32d201466ffb4a660b77298a5
Douglas Henri <dhasilva@users.noreply.github.com>
2023-09-22 13:54:27 -0300

    AI Extension: Show AI Form extension with connection nudge for disconnected users (#33186)
    * remove conditions for not extending the form block when user is disconnected
    
    * add connection nudge on AI input for form extension
    
    * changelog

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/jetpack-contact-form/index.tsx
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/jetpack-contact-form/index.tsx
@@ -11,8 +11,6 @@ import { addFilter } from '@wordpress/hooks';
 /*
  * Internal dependencies
  */
-import { AI_Assistant_Initial_State } from '../../hooks/use-ai-feature';
-import { isUserConnected } from '../../lib/connection';
 import AiAssistantBar from './components/ai-assistant-bar';
 import AiAssistantToolbarButton from './components/ai-assistant-toolbar-button';
 import { isJetpackFromBlockAiCompositionAvailable } from './constants';
@@ -68,17 +66,6 @@ export function isPossibleToExtendJetpackFormBlock(
 		}
 	} else if ( blockName !== 'jetpack/contact-form' ) {
 		// If it is not a child block, check if it is the Jetpack Form block.
-		return false;
-	}
-
-	// Do not extend the block if the site is not connected.
-	const connected = isUserConnected();
-	if ( ! connected ) {
-		return false;
-	}
-
-	// Do not extend if there is an error getting the feature.
-	if ( AI_Assistant_Initial_State.errorCode ) {
 		return false;
 	}
 
