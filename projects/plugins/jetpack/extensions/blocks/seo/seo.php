e42550c2e2e531dae4350e6fc764a9280f5d59f7
Andrii Lysenko <lysenkoa.work@gmail.com>
2023-09-25 10:02:06 -0700

    make block accessible only to connected users

--- projects/plugins/jetpack/extensions/blocks/seo/seo.php
+++ projects/plugins/jetpack/extensions/blocks/seo/seo.php
@@ -7,6 +7,9 @@
 
 namespace Automattic\Jetpack\Extensions\Seo;
 
+use Automattic\Jetpack\Connection\Manager as Connection_Manager;
+use Automattic\Jetpack\Status;
+use Automattic\Jetpack\Status\Host;
 use Jetpack_Gutenberg;
 
 /**
@@ -16,7 +19,12 @@ use Jetpack_Gutenberg;
  */
 function register_plugins() {
 	// Register SEO.
-	Jetpack_Gutenberg::set_extension_available( 'jetpack-seo' );
+	if (
+		( new Host() )->is_wpcom_simple()
+		|| ( ( new Connection_Manager( 'jetpack' ) )->has_connected_owner() && ! ( new Status() )->is_offline_mode() )
+	) {
+		Jetpack_Gutenberg::set_extension_available( 'jetpack-seo' );
+	}
 }
 
 add_action( 'jetpack_register_gutenberg_extensions', __NAMESPACE__ . '\register_plugins' );
