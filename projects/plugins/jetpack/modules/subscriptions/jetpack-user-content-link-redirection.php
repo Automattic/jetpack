608215c8c5f4f0cb263ff6b2d9b705f39472ae7c
Yan Sern <yansern@yan.sr>
2023-09-26 10:39:32 +0300

    Bring the updated version over from WPCOM.

--- projects/plugins/jetpack/modules/subscriptions/jetpack-user-content-link-redirection.php
+++ projects/plugins/jetpack/modules/subscriptions/jetpack-user-content-link-redirection.php
@@ -12,33 +12,38 @@
  * Generate the iframe to track and redirect the user generated link clicks.
  */
 function jetpack_user_content_link_redirection() {
-	// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-	if ( ! isset( $_SERVER['QUERY_STRING'] ) || ( isset( $_GET['action'] ) && 'user_content_redirect' !== $_GET['action'] ) ) {
+	if ( empty( $_SERVER['QUERY_STRING'] ) ) {
 		return;
 	}
+	$query_params = sanitize_text_field( wp_unslash( $_SERVER['QUERY_STRING'] ) );
+	$iframe_url   = "https://subscribe.wordpress.com/?$query_params";
 
-	$query_params = sanitize_text_field( wp_unslash( $_SERVER['QUERY_STRING'] ) ); // sanitization
-	?>
-	<iframe id="trackingIframe" src='<?php echo esc_url( "https://subscribe.wordpress.com/?$query_params" ); ?>'></iframe>
-
-	<script>
-		window.addEventListener("message", function(event) {
-			if (event.origin !== "https://subscribe.wordpress.com") {
-				return;
-			}
-
-			if (event.data.redirectUrl) {
-				window.location.href = event.data.redirectUrl;
-			}
-		});
-	</script>
-	<?php
+	echo <<<EOF
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+	window.addEventListener( 'message', function(event) {
+		if ( event.origin !== 'https://subscribe.wordpress.com' ) {
+			return;
+		}
+		if ( event.data.redirectUrl ) {
+			window.location.href = event.data.redirectUrl;
+		}
+	} );
+</script>
+</head>
+<body>
+EOF;
+	echo '<iframe id="user-content-link-redirection" hidden aria-hidden="true" tabindex="-1" width="0" height="0" style="display: none" src="' . esc_url( $iframe_url ) . '"></iframe>';
+	echo <<<EOF
+</body>
+</html>
+EOF;
 	exit;
 }
 
-// If this was loaded by Jetpack's mu-wpcom-plugin, execute right away.
-if ( defined( 'WPCOM_USER_CONTENT_LINK_REDIRECTION' ) ) {
-	jetpack_user_content_link_redirection();
-} else {
+// phpcs:ignore WordPress.Security.NonceVerification.Recommended
+if ( isset( $_GET['action'] ) && $_GET['action'] === 'user_content_redirect' ) {
 	add_action( 'init', 'jetpack_user_content_link_redirection' );
 }
