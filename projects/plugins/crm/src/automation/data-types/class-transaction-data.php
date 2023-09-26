7a083890adfd47e79101398d1c59610f04882433
André Kallehauge <3846700+kallehauge@users.noreply.github.com>
2023-09-27 00:44:52 +0200

    Code standards

--- projects/plugins/crm/src/automation/data-types/class-transaction-data.php
+++ projects/plugins/crm/src/automation/data-types/class-transaction-data.php
@@ -18,7 +18,15 @@ use Automattic\Jetpack\CRM\Entities\Transaction;
 class Transaction_Data extends Data_Type_Base {
 
 	/**
-	 * {@inheritDoc}
+	 * Validate the data.
+	 *
+	 * This method is meant to validate if the data has the expected inheritance
+	 * or structure and will be used to throw a fatal error if not.
+	 *
+	 * @since $$next-version$$
+	 *
+	 * @param mixed $data The data to validate.
+	 * @return bool Whether the data is valid.
 	 */
 	public function validate_data( $data ): bool {
 		return $data instanceof Transaction;
