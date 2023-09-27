ab46928f0d34270cd4738e46441221b91d69dbd6
Anthony Grullon <agrullon95@gmail.com>
2023-09-27 12:40:25 -0400

    Simplify logic

--- projects/plugins/jetpack/extensions/blocks/blogroll/components/blogroll-appender-results/index.js
+++ projects/plugins/jetpack/extensions/blocks/blogroll/components/blogroll-appender-results/index.js
@@ -10,55 +10,48 @@ export default function BlogrollAppenderResults( {
 	isLoading,
 } ) {
 	const [ siteRecommendations ] = useEntityProp( 'root', 'site', 'Blogroll Recommendations' );
-
-	// Check if site is already appended to the blogroll
-	// If it is, add a duplicateRecommendation flag to the site object
-	const updatedResults = results?.map( result => {
-		const itemBlogrollItem = siteRecommendations.find( siteRecommendation => {
-			return siteRecommendation?.id === result?.blog_id;
-		} );
-		if ( itemBlogrollItem ) {
-			return { ...result, duplicateRecommendation: true };
-		}
-		return result;
-	} );
-
 	return (
 		<div className="jetpack-blogroll__appender-results">
 			{ showPlaceholder && <div aria-autocomplete="list">{ __( 'Suggestions', 'jetpack' ) }</div> }
 
 			{ isLoading && <div role="status">{ __( 'Loading…', 'jetpack' ) }</div> }
 
-			{ ! isLoading && updatedResults.length === 0 && ! showPlaceholder && (
+			{ ! isLoading && results.length === 0 && ! showPlaceholder && (
 				<div role="status">{ __( 'No websites found.', 'jetpack' ) }</div>
 			) }
-			{ updatedResults.length > 0 && (
+			{ results.length > 0 && (
 				<ul aria-live="polite">
-					{ updatedResults.map( result => (
-						<li
-							key={ result.blog_id }
-							className={ classNames( 'jetpack-blogroll__appender-result-container', {
-								'is-disabled-result': result?.duplicateRecommendation,
-							} ) }
-						>
-							<button
-								className="jetpack-blogroll__appender-result-title"
-								disabled={ result?.duplicateRecommendation }
-								href="#"
-								onClick={ () => onSelect( result ) }
+					{ results.map( result => {
+						const isDuplicate = siteRecommendations.some( siteRecommendation => {
+							return siteRecommendation?.id === result?.blog_id;
+						} );
+
+						return (
+							<li
+								key={ result.blog_id }
+								className={ classNames( 'jetpack-blogroll__appender-result-container', {
+									'is-disabled-result': isDuplicate,
+								} ) }
 							>
-								<div className="jetpack-blogroll__appender-result-image">
-									{ result.site_icon && <img src={ result.site_icon } alt={ result.name } /> }
-								</div>
-								<div className="jetpack-blogroll__appender-result-text">
-									<span className="jetpack-blogroll__appender-result-title">{ result.name }</span>
-									<span className="jetpack-blogroll__appender-result-description">
-										{ result.description }
-									</span>
-								</div>
-							</button>
-						</li>
-					) ) }
+								<button
+									className="jetpack-blogroll__appender-result-title"
+									disabled={ isDuplicate }
+									href="#"
+									onClick={ () => onSelect( result ) }
+								>
+									<div className="jetpack-blogroll__appender-result-image">
+										{ result.site_icon && <img src={ result.site_icon } alt={ result.name } /> }
+									</div>
+									<div className="jetpack-blogroll__appender-result-text">
+										<span className="jetpack-blogroll__appender-result-title">{ result.name }</span>
+										<span className="jetpack-blogroll__appender-result-description">
+											{ result.description }
+										</span>
+									</div>
+								</button>
+							</li>
+						);
+					} ) }
 				</ul>
 			) }
 		</div>
