6c1b2cb03e43aa39e2b1393c26b54fc9a2d5a7bf
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2023-09-27 16:37:50 -0500

    Fix tests

--- projects/plugins/crm/jest.config.js
+++ projects/plugins/crm/jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
 	...baseConfig,
 	roots: [ '<rootDir>/src' ],
 	setupFilesAfterEnv: [ ...baseConfig.setupFilesAfterEnv, '<rootDir>/tests/jest-globals.gui.js' ],
+	moduleNameMapper: { '^crm/(.*)': '<rootDir>/src/js/$1' }
 };
--- projects/plugins/crm/src/js/components/automations-admin/test/index.test.tsx
+++ projects/plugins/crm/src/js/components/automations-admin/test/index.test.tsx
@@ -1,43 +1,29 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render, screen } from '@testing-library/react';
-import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { AutomationsAdmin } from '../index';
 
 describe( 'AutomationsAdmin', () => {
+	const queryClient = new QueryClient();
+
 	test( 'Render the main page', () => {
 		render(
 			<MemoryRouter initialEntries={ [ '/automations' ] }>
-				<AutomationsAdmin />
+				<QueryClientProvider client={ queryClient }>
+					<AutomationsAdmin />
+				</QueryClientProvider>
 			</MemoryRouter>
 		);
 
 		expect( screen.getByRole( 'heading', { name: 'Automations' } ) ).toBeInTheDocument();
 	} );
 
-	test( 'Render the add page', () => {
-		render(
-			<MemoryRouter initialEntries={ [ '/automations/add' ] }>
-				<AutomationsAdmin />
-			</MemoryRouter>
-		);
-
-		expect( screen.getByRole( 'heading', { name: 'Add Automation' } ) ).toBeInTheDocument();
-	} );
-
-	test( 'Render the edit page', () => {
-		render(
-			<MemoryRouter initialEntries={ [ '/automations/edit' ] }>
-				<AutomationsAdmin />
-			</MemoryRouter>
-		);
-
-		expect( screen.getByRole( 'heading', { name: 'Edit Automation' } ) ).toBeInTheDocument();
-	} );
-
 	test( 'Fallback to main page', () => {
 		render(
 			<MemoryRouter initialEntries={ [ '/some-path' ] }>
-				<AutomationsAdmin />
+				<QueryClientProvider client={ queryClient }>
+					<AutomationsAdmin />
+				</QueryClientProvider>
 			</MemoryRouter>
 		);
 
