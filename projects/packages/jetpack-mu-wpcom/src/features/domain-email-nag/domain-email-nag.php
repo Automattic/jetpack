6d3397d06fab4e27dbed7e2aaefed4bc318085b6
vykes-mac <knrymcleish@gmail.com>
2023-09-28 14:02:46 -0500

    show frontend domain email nag

--- projects/packages/jetpack-mu-wpcom/src/features/domain-email-nag/domain-email-nag.php
+++ projects/packages/jetpack-mu-wpcom/src/features/domain-email-nag/domain-email-nag.php
@@ -7,29 +7,61 @@
 
 namespace A8C\Domain\Frontend_Email_Nag;
 
+use Automattic\Jetpack\Jetpack_Mu_Wpcom;
+
 /**
  * Determines whether the email nag should be shown.
  *
  * @return boolean
  */
 function should_show_domain_frontend_email_nag() {
-	if ( ! is_front_page() && ! is_user_logged_in() && ! current_user_can( 'edit_post' ) ) {
+	if ( ! is_front_page() || ( is_front_page() && ! is_user_logged_in() ) ) {
 		return false;
 	}
 
-	$should_show = ( (int) get_user_attribute( get_current_user_id(), 'is_email_unverified' ) === 1 );
+	if ( ! class_exists( 'Email_Verification' ) ) {
+		return false;
+	}
+
+	$should_show = Email_Verification::is_domain_email_unverified();
 
 	return apply_filters( 'a8c_show_domain_frontend_email_nag', $should_show );
 }
 
 /**
- * Decides whether to render to the email nag.
+ * Returns account url for fixing email issue
  *
- * @param string $template The template to render.
+ * @return string account url
  */
-function domain_email_nag( $template ) {
-	if ( ! should_show_domain_frontend_email_nag() ) {
-		return $template;
-	}
+function get_account_url() {
+	return 'https://wordpress.com/me/account';
 }
-add_filter( 'template_include', __NAMESPACE__ . '\domain_email_nag' );
+
+/**
+ * Decides whether to render to the email nag.
+ */
+function domain_email_nag() {
+	if ( ! should_show_domain_frontend_email_nag() ) {
+		return;
+	}
+
+	wp_enqueue_style( 'wpcom-domain-email-nag-style', plugins_url( 'domain-nag.style.css', __FILE__ ), array(), Jetpack_Mu_Wpcom::PACKAGE_VERSION );
+
+	$notice = sprintf(
+	/* translators: %1 User's email address, %2 current domain */
+		__( 'You need to confirm your email address <strong>%1$s</strong> to avoid having your domain <strong>%2$s</strong> suspended. Please check your inbox.', 'jetpack-mu-wpcom' ),
+		wp_get_current_user()->user_email,
+		wp_parse_url( site_url(), PHP_URL_HOST )
+	);
+
+	?>
+	<div class="wp-domain-nag-sticky-message">
+		<div class="wp-domain-nag-inner">
+			<p class="wp-domain-nag-text"><?php echo wp_kses( $notice, array( 'strong' => array() ) ); ?></p>
+			<a class="button" href="<?php echo esc_url( get_account_url() ); ?>"><?php esc_html_e( 'Fix', 'jetpack-mu-wpcom' ); ?></a>
+		</div>
+	</div>
+	<?php
+}
+add_action( 'wp_footer', __NAMESPACE__ . '\domain_email_nag' );
+
