e781f405b4eb142b34953b403815c025b0069245
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2023-09-27 23:55:39 -0500

    Remove old testing routes

--- projects/plugins/crm/src/js/components/automations-admin/index.tsx
+++ projects/plugins/crm/src/js/components/automations-admin/index.tsx
@@ -1,13 +1,11 @@
-import { Button, AdminSection, Col, Container } from '@automattic/jetpack-components';
+import { Button } from '@automattic/jetpack-components';
 import { useQuery } from '@tanstack/react-query';
 import { dispatch, useSelect } from '@wordpress/data';
-import { __ } from '@wordpress/i18n';
 import axios from 'axios';
 import { Workflow } from 'crm/state/automations-admin/types';
 import { store } from 'crm/state/store';
 import { useCallback, useState } from 'react';
-import { Link, Routes, Route } from 'react-router-dom';
-import AdminPage from '../admin-page';
+import { Routes, Route } from 'react-router-dom';
 import { BulkWorkflowActions } from './components/bulk-workflow-actions';
 import { EditModal } from './components/edit-modal';
 import { RedirectHome } from './components/redirect-home';
@@ -50,69 +48,6 @@ export const AutomationsAdmin = () => {
 
 	return (
 		<Routes>
-			<Route
-				path="/automations"
-				element={
-					<AdminPage
-						headline={ __( 'Automations', 'zero-bs-crm' ) }
-						subHeadline={ __( 'Streamline your workflows with CRM Automations', 'zero-bs-crm' ) }
-					>
-						<AdminSection>
-							<Container horizontalSpacing={ 8 }>
-								<Col>
-									<div>This is the home page</div>
-									<br />
-									<Link to={ '/automations/edit' }>Go to edit</Link>
-									<br />
-									<Link to={ '/automations/add' }>Go to add</Link>
-								</Col>
-							</Container>
-						</AdminSection>
-					</AdminPage>
-				}
-			/>
-			<Route
-				path="/automations/add"
-				element={
-					<AdminPage
-						headline={ __( 'Add Automation', 'zero-bs-crm' ) }
-						subHeadline={ __( 'Streamline your workflows with CRM Automations', 'zero-bs-crm' ) }
-					>
-						<AdminSection>
-							<Container horizontalSpacing={ 8 }>
-								<Col>
-									<div>This is the add page</div>
-									<br />
-									<Link to={ '/automations/edit' }>Go to edit</Link>
-									<br />
-									<Link to={ '/automations' }>Go back to home</Link>
-								</Col>
-							</Container>
-						</AdminSection>
-					</AdminPage>
-				}
-			/>
-			<Route
-				path="/automations/edit"
-				element={
-					<AdminPage
-						headline={ __( 'Edit Automation', 'zero-bs-crm' ) }
-						subHeadline={ __( 'Streamline your workflows with CRM Automations', 'zero-bs-crm' ) }
-					>
-						<AdminSection>
-							<Container horizontalSpacing={ 8 }>
-								<Col>
-									<div>This is the edit page</div>
-									<br />
-									<Link to={ '/automations/add' }>Go to add</Link>
-									<br />
-									<Link to={ '/automations' }>Go back to home</Link>
-								</Col>
-							</Container>
-						</AdminSection>
-					</AdminPage>
-				}
-			/>
 			<Route
 				path="/automations/test-workflow-row"
 				element={
