2a69fe6290aa3f11e4c23557045c220221477f5e
Anthony Grullon <agrullon95@gmail.com>
2023-09-28 10:31:55 -0400

    Blogroll: Disable blog appender sites that have been added to blog roll (#33327)
    Co-authored-by: Kosta <heavyweight@users.noreply.github.com>
    Co-authored-by: heavyweight <kpapazov@gmail.com>

--- projects/plugins/jetpack/extensions/blocks/blogroll/components/blogroll-appender/index.js
+++ projects/plugins/jetpack/extensions/blocks/blogroll/components/blogroll-appender/index.js
@@ -19,6 +19,7 @@ export default function BlogrollAppender( { subscriptions, clientId } ) {
 		subscriptions,
 		enabled: searchInput,
 	} );
+
 	const toggleVisible = () => {
 		setIsVisible( state => ! state );
 	};
