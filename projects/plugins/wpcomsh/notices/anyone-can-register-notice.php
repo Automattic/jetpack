236e022d194e02f59424b16c352c69d146f0f35c
Nic-Sevic <47717887+Nic-Sevic@users.noreply.github.com>
2023-09-28 13:27:39 +0300

    Allow "Anyone can register" notice to be dismissed https://github.com/Automattic/wpcomsh/pull/1404
    * fix: add self contained handling of notice dismiss
    - sets/updates usermeta for persistent dismissal by user
    
    * fix: handling of notice dismissal
    - added handling of notice dismissal
    - added metadata update when option disabled in order to notify again if re-enabled in the future
    - clarified Subscriber case to mention membership sites
    - clarified general warning to make it seem less alarming
    
    * fix: linting issues and worked on adding nonce
    - accounted for undefined superglobals with isset
    - removed unncessary prepare
    - escaped ajax url
    - fixed spacing
    
    TODO:
    - account for caching if needed
    - find alternative for direct DB call?
    - figure out why nonce in jquery not working
    
    * fix: handling of dismissal notice pt 2
    - addressed previous linting errors/warnings
    -- verified superglobals set
    -- added nonce and verification
    -- switched direct DB call to WP functions which also handles caching
    - for clarity, switched instances of ACR to anyone_can_register
    
    * fix: linting errors
    
    * fix: comment lint issue
    
    * fix: again, fix function comment linting issue
    
    * fix: review suggestions
    - changed exit to return on line 16
    - removed unused `global $wpdb`
    - removed meta_key query line 39
    - moved assignment of $dismissed to line 48
    
    * fix: responding to feedback
    - bolded last line of notice text to call attention to it
    - made sure functions prefixed correctly
    - removed invalid nonce handling and duplication of method checks
    - changed metadata update query to improve performance
    - changed escaping to use wp_json_encode
    
    TODO: look into tying metadata deletion to option chage hook
    
    * fix: tied metadata deletion to option update
    
    * fix: remove redundant $_POST['action'] check
    
    * fix: def constant for and prefaced user meta key
    
    * fix: corrected PHPDoc comment for main function
    
    * fix: add globals and periods to PHPDoc comments
    
    * fix: remove missed blank line in doc comment
    
    * add: unit tests
    
    * fix: unit test issues
    - updated metadata constant to match change in notice: WPCOMSH_ANYONE_CAN_REGISTER_DISMISSED_METADATA -> WPCOMSH_ACR_DISMISSED_METADATA
    - added foreach loop to loop through user ids for tests on 114 and 128
    - added prepare back into queries for metadata in 114 and 128
    - removed pointless test `test_wpcomsh_users_can_register_option_change_not_run_when_option_not_updated`
    
    * fix: shorten metadata constant name
    
    * fix: various test issues
    - added setting of current user as admin on tests missing it: 70, 87, 157
    - in test on line 104, change assert to actual value passed
    - test 157 corrected assert statement
    - test 115, 136 corrected use of metadata constant
    - added line to first set option as true for tests of metadata deletion
    -- needed to trigger hook
    -- applies to tests 104, 115, 136
    - fixed creation of '_ajax_nonce' in test 70
    
    * fix: fix various unit tests
    - set users_can_register to true where missed
    - adjust $POST to $REQUEST for nonces in fn 78 & 95
    - correct query/prepare use and escape $metakey in fn 123 & 145
    - correct assert statement and string match for fn on 16
    
    * add: added coment to ignore lint error about query
    
    * Mark tests as skipped on multisite
    
    * Fix linting issue
    
    * Prevent error notice from Atomic MU plugin.
    
    * Mock another value
    
    * Mock another value
    
    * Mock another value
    
    * Mock another value
    
    * Mock another value
    
    * Version bump to 3.13.26
    
    ---------
    
    Co-authored-by: Daniel Bachhuber <daniel.bachhuber@automattic.com>

--- projects/plugins/wpcomsh/notices/anyone-can-register-notice.php
+++ projects/plugins/wpcomsh/notices/anyone-can-register-notice.php
@@ -6,17 +6,58 @@
  */
 
 /**
- * Adds an admin notice if the Anyone can register option is active.
- * Default roles of Admin and Shop Manager are persistent while lower role notices are dismissable.
+ * Adds a dismissible admin notice to warn about the risks when users_can_register option is active.
+ * The purpose is to reduce unnecessary activation of this option and to reduce chances that malicious admins can register to the site.
+ */
+const WPCOMSH_ACR_DISMISSED_METADATA = 'wpcomsh_anyone_can_register_dismissed_notice';
+
+/** Handle AJAX request to dismiss notice **/
+function wpcomsh_ajax_anyone_can_register_handle_dismissal() {
+	if ( isset( $_SERVER['REQUEST_METHOD'] ) && isset( $_POST['action'] ) ) {
+		if ( 'anyone_can_register_dismiss_notice' === $_POST['action'] && check_ajax_referer( 'anyone_can_register_ajax_nonce', '_ajax_nonce', false ) ) {
+			update_user_meta( get_current_user_id(), WPCOMSH_ACR_DISMISSED_METADATA, '1' );
+		}
+	}
+}
+add_action( 'wp_ajax_anyone_can_register_dismiss_notice', 'wpcomsh_ajax_anyone_can_register_handle_dismissal' );
+
+/**
+ * Clear metadata when option disabled.
+ *
+ * @param number $old_value of users_can_register option.
+ * @param number $new_value of users_can_register option.
+ */
+function wpcomsh_users_can_register_option_change( $old_value, $new_value ) {
+	if ( ! $new_value ) {
+		delete_metadata( 'user', 0, WPCOMSH_ACR_DISMISSED_METADATA, 1, true );
+		return;
+	}
+}
+add_action( 'update_option_users_can_register', 'wpcomsh_users_can_register_option_change', 10, 2 );
+
+/**
+ * Adds a dismissible notice to wp-admin pages for all administrators if users_can_register option is active.
+ * Default roles of Admin and Shop Manager warn red while all other roles warn orange.
+ * Dismissal metadata is cleared for all admins when option is disabled.
+ *
+ * @global WP_Roles $wp_roles Available roles.
+ * @global string $pagenow Current page being viewed.
  */
 function wpcomsh_anyone_register_warning() {
 	global $wp_roles;
 	global $pagenow;
+
+	if ( ! current_user_can( 'manage_options' ) ) {
+		return;
+	}
+
 	if ( ! get_option( 'users_can_register' ) ) {
 		return;
 	}
 
-	if ( ! current_user_can( 'manage_options' ) ) {
+	$dismissed = get_user_meta( get_current_user_id(), WPCOMSH_ACR_DISMISSED_METADATA, true );
+
+	if ( $dismissed ) {
 		return;
 	}
 
@@ -25,15 +66,15 @@ function wpcomsh_anyone_register_warning() {
 	$warning_text_main = ( $pagenow !== 'options-general.php' ) ?
 	/* translators: %1$s default role, %2$s support doc URL, %3$s site options URL, %4$s string highlighting risks*/
 	__(
-		'The <a href="%2$s">"Anyone can register" option</a> is currently active. The current default role is %1$s. %4$s <a href="%3$s">Please consider disabling this option</a>.'
+		'The <a href="%2$s">"Anyone can register" option</a> is currently active. The current default role is %1$s. %4$s <a href="%3$s"><strong>Please consider disabling this option if open registration is not needed.</strong></a>.'
 	) :
 	/* translators: %1$s default role, %2$s support doc URL, %3$s site options URL, %4$s string highlighting risks*/
 	__(
-		'The <a href="%2$s">"Anyone can register" option</a> is currently active. The current default role is %1$s. %4$s Please consider disabling this option.'
+		'The <a href="%2$s">"Anyone can register" option</a> is currently active. The current default role is %1$s. %4$s <strong>Please consider disabling this option if open registration is not needed.<strong>'
 	);
 
 	$warning_text_role = '';
-	// using switch instead of array to account for unknown $default_role
+	// using switch instead of array to account for custom $default_role
 	switch ( $default_role ) {
 		case 'administrator':
 			$warning_text_role = __(
@@ -57,12 +98,12 @@ function wpcomsh_anyone_register_warning() {
 			break;
 		case 'contributor':
 			$warning_text_role = __(
-				'It allows a user to create but not post content.'
+				'It allows a user to create but not to post content.'
 			);
 			break;
 		case 'subscriber':
 			$warning_text_role = __(
-				'It could result in spam users.'
+				'It could result in spam users but is commonly used in membership sites.'
 			);
 			break;
 		default:
@@ -81,7 +122,7 @@ function wpcomsh_anyone_register_warning() {
 
 	$notice_style = in_array( $default_role, array( 'administrator', 'shop_manager' ), true ) ? 'notice__icon-wrapper-red' : 'notice__icon-wrapper-orange';
 	printf(
-		'<div class="notice wpcomsh-notice">
+		'<div class="notice wpcomsh-notice is-dismissible anyone-can-register-notice">
 			<span class="notice__icon-wrapper %1$s">
 				<span class="dashicons dashicons-info"></span>
 			</span>
@@ -92,7 +133,29 @@ function wpcomsh_anyone_register_warning() {
 		esc_attr( $notice_style ),
 		wp_kses_post( $message )
 	);
+
+	$nonce = wp_create_nonce( 'anyone_can_register_ajax_nonce' );
+
+	// admin-ajax call to add metadata for persistent dismissal
+	echo '<script id="anyone-can-register-notice" type="text/javascript">
+		jQuery( function( $ ) {
+			$( document ).ready( function() {
+				$( ".anyone-can-register-notice .notice-dismiss" ).on( "click", function() {
+					$.ajax( {
+						url: ' . wp_json_encode( esc_url_raw( admin_url( 'admin-ajax.php' ) ) ) . ',
+						type: "POST",
+						data: {
+							action: "anyone_can_register_dismiss_notice",
+							_ajax_nonce: ' . wp_json_encode( $nonce ) . '
+						},
+						error: function( xhr, status, error ) {
+							alert( error );
+						}
+					});
+				});
+			});
+		});
+	</script>';
 }
 
 add_action( 'admin_notices', 'wpcomsh_anyone_register_warning' );
-
