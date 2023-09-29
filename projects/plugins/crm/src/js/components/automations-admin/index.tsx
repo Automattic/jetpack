6d524e9fbc05f991ab7bb349a3feb4fa9098bb2c
André Kallehauge <3846700+kallehauge@users.noreply.github.com>
2023-09-29 23:01:51 +0200

    [CRM] Automation UI: Remove test pages (#33418)
    * [CRM] Automation UI: Remove test pages
    
    * Changelog

--- projects/plugins/crm/src/js/components/automations-admin/index.tsx
+++ projects/plugins/crm/src/js/components/automations-admin/index.tsx
@@ -1,5 +1,4 @@
 import { Col, Container } from '@automattic/jetpack-components';
-import { Button } from '@automattic/jetpack-components';
 import { useQuery } from '@tanstack/react-query';
 import { dispatch, useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
@@ -7,26 +6,14 @@ import axios from 'axios';
 import AdminPage from 'crm/components/admin-page';
 import { Workflow } from 'crm/state/automations-admin/types';
 import { store } from 'crm/state/store';
-import { useCallback, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { BulkWorkflowActions } from './components/bulk-workflow-actions';
-import { EditModal } from './components/edit-modal';
 import { RedirectHome } from './components/redirect-home';
-import { WorkflowRow } from './components/workflow-row';
-import { WorkflowTable } from './components/workflow-table';
 import { WorkflowsHome } from './components/workflows-home';
-import { workflowOne, workflowTwo } from './test/util/data';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare let jpcrmAutomationsInitialState: any;
 
 export const AutomationsAdmin = () => {
-	const [ isModalOpen, setIsModalOpen ] = useState( false );
-
-	const closeModal = useCallback( () => {
-		setIsModalOpen( false );
-	}, [ setIsModalOpen ] );
-
 	useQuery( {
 		queryKey: [ 'automations', 'workflows' ],
 		queryFn: async () => {
@@ -47,56 +34,9 @@ export const AutomationsAdmin = () => {
 	} );
 
 	const workflows = useSelect( select => select( store ).getWorkflows(), [] );
-	const workflow = Object.values( workflows )[ 0 ];
 
 	return (
 		<Routes>
-			<Route
-				path="/automations/test-workflow-row"
-				element={
-					<table
-						id="workflow-test"
-						style={ { padding: '20px', background: 'white', width: '100%' } }
-					>
-						<tr style={ { padding: '5px 10px' } }>
-							<th style={ { padding: '5px 10px' } }>check</th>
-							<th style={ { padding: '5px 10px' } }>Name</th>
-							<th style={ { padding: '5px 10px' } }>Status</th>
-							<th style={ { padding: '5px 10px' } }>Added</th>
-							<th style={ { padding: '5px 10px' } }>Trigger</th>
-							<th style={ { padding: '5px 10px' } }>Edit</th>
-						</tr>
-						<WorkflowRow workflow={ workflowOne } />
-						<WorkflowRow workflow={ workflowTwo } />
-					</table>
-				}
-			/>
-			<Route
-				path="/automations/test-workflow-table"
-				element={ <WorkflowTable workflows={ Object.values( workflows ) } /> }
-			/>
-			<Route
-				path="/automations/test-workflow-bulk-actions"
-				element={
-					<div style={ { display: 'flex', flexDirection: 'column', background: 'white' } }>
-						<BulkWorkflowActions />
-						<WorkflowTable workflows={ Object.values( workflows ) } />
-					</div>
-				}
-			/>
-			<Route
-				path="/automations/test-edit-modal"
-				element={
-					workflow && (
-						<div style={ { margin: '24px' } }>
-							<Button onClick={ () => setIsModalOpen( true ) }>
-								Click here to open the modal.
-							</Button>
-							<EditModal isOpen={ isModalOpen } onClose={ closeModal } workflow={ workflow } />
-						</div>
-					)
-				}
-			/>
 			<Route
 				path="/automations/:id?"
 				element={
