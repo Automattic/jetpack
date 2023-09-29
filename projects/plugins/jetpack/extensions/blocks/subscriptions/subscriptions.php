8c11f63c42cfdfb7d739fd8fcee553edaff68953
Miguel Lezama <miguel@automattic.com>
2023-09-29 15:52:23 -0300

    Update/paywall use magic link again (#33407)

--- projects/plugins/jetpack/extensions/blocks/subscriptions/subscriptions.php
+++ projects/plugins/jetpack/extensions/blocks/subscriptions/subscriptions.php
@@ -1140,7 +1140,7 @@ function get_paywall_blocks( $newsletter_access_level ) {
 
 	$sign_in = '';
 	if ( ! is_user_logged_in() && ( new Host() )->is_wpcom_simple() ) {
-		$sign_in_link = wpcom_logmein_redirect_url( get_current_url() );
+		$sign_in_link = wpcom_logmein_redirect_url( get_current_url(), false, null, 'link' );
 		$sign_in      = '<!-- wp:paragraph {"align":"center", "typography":{"fontSize":"14px","fontWeight":"400";"color":"#646970"}} -->
 <p class="has-text-align-center" style="font-size:14px;font-weight:400;color:#646970">' . esc_html( $access_question ) . ' <a href="' . $sign_in_link . '" class="jetpack-subscriber-paywall-login" style="font-size:14px;font-weight:400;color:#646970">' . esc_html__( 'Log in', 'jetpack' ) . '</a></p>
 <!-- /wp:paragraph -->';
