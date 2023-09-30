b43f8cb0a98ad6bf8b27807712a48feeb14d1932
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2023-09-30 00:57:55 -0500

    Make it easier to search for URLs

--- projects/plugins/crm/src/js/data/query-functions.ts
+++ projects/plugins/crm/src/js/data/query-functions.ts
@@ -1,27 +1,30 @@
 import axios from 'axios';
-import { urls } from 'crm/data/urls';
 import { Workflow, ServerPreparedWorkflow } from 'crm/state/automations-admin/types';
 import { getServerPreparedWorkflow } from 'crm/state/automations-admin/util';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare let jpcrmAutomationsInitialState: any;
 
+const v4ApiRoot = `${ jpcrmAutomationsInitialState.apiRoot }jetpack-crm/v4`;
+
 const api = axios.create( {
-	baseURL: `${ jpcrmAutomationsInitialState.apiRoot }jetpack-crm/v4`,
+	baseURL: v4ApiRoot,
 	headers: { 'X-WP-Nonce': jpcrmAutomationsInitialState.apiNonce },
 } );
 
+// /wp-json/jetpack-crm/v4/automation/workflows
 export const getAutomationWorkflows =
 	( hydrate: ( workflows: Workflow[] ) => void ) => async () => {
-		const result = await api.get< Workflow[] >( urls.automation.workflows() );
+		const result = await api.get< Workflow[] >( '/automation/workflows' );
 		result.data && hydrate( result.data );
 		return result;
 	};
 
+// /wp-json/jetpack-crm/v4/automation/workflows/:id
 export const postAutomationWorkflow = async ( workflow: Workflow ) => {
 	const serverPreparedWorkflow = getServerPreparedWorkflow( workflow );
 	return await api.post< ServerPreparedWorkflow >(
-		urls.automation.workflows( workflow.id ),
+		`/automation/workflows/${ workflow.id }`,
 		serverPreparedWorkflow
 	);
 };
