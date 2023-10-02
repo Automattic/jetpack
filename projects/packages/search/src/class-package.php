13cce2d9b112b121645020c49e43a85dc593dd71
Artur Piszek <artur.piszek@automattic.com>
2023-10-02 18:40:35 +0200

    Jetpack AI Search: Release (#33432)
    * Release Jetpack AI Search
    
    * changelog
    
    * change the location of the setting for the test processs
    
    * changelog
    
    * tools/project-version.sh
    
    ---------
    
    Co-authored-by: dereksmart <smart@automattic.com>

--- projects/packages/search/src/class-package.php
+++ projects/packages/search/src/class-package.php
@@ -11,7 +11,7 @@ namespace Automattic\Jetpack\Search;
  * Search package general information
  */
 class Package {
-	const VERSION = '0.38.8';
+	const VERSION = '0.39.0-alpha';
 	const SLUG    = 'search';
 
 	/**
