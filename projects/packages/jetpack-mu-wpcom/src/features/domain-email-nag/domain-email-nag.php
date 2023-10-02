93612d4d403b79ac31f04ea918ba548874d23972
mpkelly <kikemelly@gmail.com>
2023-10-02 12:26:50 +0700

    Rework logic to use Domain_Management class; change backround to red

--- projects/packages/jetpack-mu-wpcom/src/features/domain-email-nag/domain-email-nag.php
+++ projects/packages/jetpack-mu-wpcom/src/features/domain-email-nag/domain-email-nag.php
@@ -18,23 +18,36 @@ function should_show_domain_frontend_email_nag() {
 	if ( ! is_front_page() || ( is_front_page() && ! is_user_logged_in() ) ) {
 		return false;
 	}
-
-	if ( ! class_exists( 'Email_Verification' ) ) {
-		return false;
-	}
-
-	$should_show = \Email_Verification::is_domain_email_unverified();
-
-	return apply_filters( 'a8c_show_domain_frontend_email_nag', $should_show );
+	return true;
 }
 
 /**
  * Returns account url for fixing email issue
  *
+ * @param string $domain the domain that is unverified.
+ *
  * @return string account url
  */
-function get_account_url() {
-	return 'https://wordpress.com/me/account';
+function get_account_url( $domain ) {
+	return 'https://wordpress.com/domains/manage/' . $domain;
+}
+
+/**
+ * Find the domain, if any, that has an unverified email address.
+ */
+function get_domain_with_unverified_email() {
+	if ( ! class_exists( 'Domain_Management' ) ) {
+		return false;
+	}
+
+	$domains = \Domain_Management::get_paid_domains_with_icann_verification_status();
+
+	foreach ( $domains as $domain ) {
+		if ( $domain['is_pending_icann_verification'] === true ) {
+			return $domain['domain'];
+		}
+	}
+	return false;
 }
 
 /**
@@ -45,20 +58,25 @@ function domain_email_nag() {
 		return;
 	}
 
+	$domain = get_domain_with_unverified_email();
+
+	if ( ! $domain ) {
+		return;
+	}
+
 	wp_enqueue_style( 'wpcom-domain-email-nag-style', plugins_url( 'domain-nag.style.css', __FILE__ ), array(), Jetpack_Mu_Wpcom::PACKAGE_VERSION );
 
 	$notice = sprintf(
 	/* translators: %1 User's email address, %2 current domain */
-		__( 'You need to confirm your email address <strong>%1$s</strong> to avoid having your domain <strong>%2$s</strong> suspended. Please check your inbox.', 'jetpack-mu-wpcom' ),
-		wp_get_current_user()->user_email,
-		wp_parse_url( site_url(), PHP_URL_HOST )
+		__( 'You need to confirm your domain email address to avoid having your domain <strong>%1$s</strong> suspended. Please check your inbox.', 'jetpack-mu-wpcom' ),
+		$domain
 	);
 
 	?>
 	<div class="wp-domain-nag-sticky-message">
 		<div class="wp-domain-nag-inner">
 			<p class="wp-domain-nag-text"><?php echo wp_kses( $notice, array( 'strong' => array() ) ); ?></p>
-			<a class="button" href="<?php echo esc_url( get_account_url() ); ?>"><?php esc_html_e( 'Fix', 'jetpack-mu-wpcom' ); ?></a>
+			<a class="button" href="<?php echo esc_url( get_account_url( $domain ) ); ?>"><?php esc_html_e( 'Fix', 'jetpack-mu-wpcom' ); ?></a>
 		</div>
 	</div>
 	<?php
