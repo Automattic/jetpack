84a02895467516805d3e4607d06fd3c2f69f3ca9
Kevin Zoschke <kevin.zoschke@automattic.com>
2023-10-11 10:00:33 -0400

    Fix failing Calendly tests

--- projects/plugins/jetpack/extensions/blocks/calendly/test/validate.js
+++ projects/plugins/jetpack/extensions/blocks/calendly/test/validate.js
@@ -1,9 +1,18 @@
 import runBlockFixtureTests from '../../../shared/test/block-fixtures';
 import { settings as buttonSettings } from '../../button';
 import metadata from '../block.json';
+import deprecatedV1 from '../deprecated/v1';
+import save from '../save';
 
 const { name } = metadata;
-const primaryBlock = { name, settings: metadata };
+const primaryBlock = {
+	name,
+	settings: {
+		...metadata,
+		save,
+		deprecated: [ deprecatedV1 ],
+	},
+};
 const innerBlocks = [ { name: 'jetpack/button', settings: buttonSettings } ];
 const blocks = [ primaryBlock, ...innerBlocks ];
 
