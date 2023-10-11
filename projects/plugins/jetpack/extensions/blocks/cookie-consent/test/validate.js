eb7eb9a8f5ce8325fbae09ef5ebf3b8a31e429e5
Kevin Zoschke <kevin.zoschke@automattic.com>
2023-10-11 09:55:17 -0400

    Fix Cookie consent failing tests

--- projects/plugins/jetpack/extensions/blocks/cookie-consent/test/validate.js
+++ projects/plugins/jetpack/extensions/blocks/cookie-consent/test/validate.js
@@ -1,7 +1,16 @@
 import runBlockFixtureTests from '../../../shared/test/block-fixtures';
 import metadata from '../block.json';
+import save from '../save';
 
 const { name } = metadata;
-const blocks = [ { name, settings: metadata } ];
+const blocks = [
+	{
+		name,
+		settings: {
+			...metadata,
+			save,
+		},
+	},
+];
 
 runBlockFixtureTests( name, blocks, __dirname );
