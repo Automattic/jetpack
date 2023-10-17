171d232954c22e4c1006dea8e73f233b56b75866
Nauris Pūķis <hi@pyronaur.com>
2023-10-17 15:19:36 +0300

    phpcs ignore

--- projects/js-packages/react-data-sync-client/build/index.asset.php
+++ projects/js-packages/react-data-sync-client/build/index.asset.php
@@ -1,1 +1,3 @@
-<?php return array('dependencies' => array(), 'version' => '3bb429ba5740b888a10e');
+<?php
+// phpcs:ignoreFile
+return array('dependencies' => array(), 'version' => '3bb429ba5740b888a10e');
