99a50bf6b70538e8333edd700acd2fb1054e4cfe
Nauris Pūķis <hi@pyronaur.com>
2023-10-17 08:42:24 +0300

    Initialize react-data-sync-client

---
+++ projects/js-packages/react-data-sync-client/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '3bb429ba5740b888a10e');
---
+++ projects/js-packages/react-data-sync-client/build/index.js
@@ -0,0 +1,22 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["ReactDataSyncClient"] = factory();
+	else
+		root["ReactDataSyncClient"] = factory();
+})(self, () => {
+return /******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+console.log("Hello World!");
+
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
+//# sourceMappingURL=index.js.map
