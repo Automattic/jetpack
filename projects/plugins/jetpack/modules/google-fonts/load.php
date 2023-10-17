39fe30c462e1646dffc50dc3bda2d71e4bf12659
arthur <arthur.chu@automattic.com>
2023-10-17 18:28:29 +0800

    Address feedback

--- projects/plugins/jetpack/modules/google-fonts/load.php
+++ projects/plugins/jetpack/modules/google-fonts/load.php
@@ -5,10 +5,10 @@
  * @package automattic/jetpack
  */
 
-if ( class_exists( 'WP_Font_Library' ) && class_exists( 'WP_Font_Face' ) && class_exists( 'WP_Font_Collection' ) ) {
-	// WordPress 6.4 or above with the new Font Library.
+if ( class_exists( 'WP_Font_Face' ) ) {
+	// WordPress 6.5 or above with the new Font Library.
 	require_once __DIR__ . '/current/load-google-fonts.php';
 } elseif ( class_exists( 'WP_Fonts' ) || class_exists( 'WP_Webfonts' ) ) {
-	// WordPress 6.3 compat.
+	// Gutenberg Fonts API compatible.
 	require_once __DIR__ . '/wordpress-6.3/load-google-fonts.php';
 }
