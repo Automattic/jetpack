e50473e8c23043ac785aa1c1564eb2dec0b1a00e
cleacos <javacodia@gmail.com>
2023-10-18 13:23:03 +0100

    Improve the $transaction_fields loop. We don't have to declare optional fields. +Linter issues.
    Signed-off-by: cleacos <javacodia@gmail.com>

--- projects/plugins/crm/api/create_transaction.php
+++ projects/plugins/crm/api/create_transaction.php
@@ -10,22 +10,16 @@
  * Date: 04/06/2019
  */
 
-	// V3.0 version of API
+// V3.0 version of API
 
-/*
-======================================================
-	Breaking Checks ( stops direct access )
-	====================================================== */
+// Breaking Checks ( stops direct access )
 if ( ! defined( 'ZEROBSCRM_PATH' ) ) {
 	exit;
 }
-/*
-======================================================
-	/ Breaking Checks
-	====================================================== */
 
-		$json_params = file_get_contents( 'php://input' );
-		$new_trans   = json_decode( $json_params, true );
+// Breaking Checks
+$json_params = file_get_contents( 'php://input' );
+$new_trans   = json_decode( $json_params, true );
 
 // REQUIRED FIELDS
 $required_fields = array(
@@ -91,10 +85,10 @@ if ( empty( $customer ) ) {
 		);
 	}
 				// } Actual log var passed- SHOULD NEVER HAPPEN HERE... this is new cust
-				$fallBackLog = array();
+				$fall_back_log = array();
 
 				// } Internal automator overrides - here we pass a "customer.create" note override (so we can pass it a custom str, else we let it fall back to "created by form")
-				$internalAutomatorOverride = array(
+				$internal_automator_override = array(
 
 					'note_override' => array(
 
@@ -118,141 +112,135 @@ if ( empty( $customer ) ) {
 					),
 					'', // ) Customer date (auto)
 					// } Fallback log (for customers who already exist)
-					$fallBackLog,
+					$fall_back_log,
 					false, // } Extra meta
 					// } Internal automator overrides - here we pass a "customer.create" note override (so we can pass it a custom str, else we let it fall back to "created by API")
-					$internalAutomatorOverride
+					$internal_automator_override
 				);
 
 }
-		$transaction_fields = array(
-			'orderid'  => $orderid,
-			'customer' => $customer,
-			'status'   => '',
-			'total'    => '',
-			'date'     => '',
-			'item'     => '',
-			'net'      => '',
-			'tax'      => '',
-			'fee'      => '',
-			'discount' => '',
-			'tax_rate' => '',
+
+$transaction_fields = array();
+
+foreach ( $new_trans as $field => $value ) {
+
+	// These fields are not part of the transaction object.
+	if ( $field === 'email' || $field === 'fname' || $field === 'orderid' ) {
+		continue;
+	}
+
+	if ( $field === 'date' ) {
+		$transaction_fields[ $field ] = gmdate( 'Y-m-d H:i:s', (int) $value );
+		continue;
+	}
+
+	$transaction_fields[ $field ] = sanitize_text_field( $value );
+}
+
+$transaction_fields['customer'] = $customer;
+$transaction_fields['ref']      = $orderid;
+
+// } We can only add a trans if it has a unique id ($orderid)
+// } This isn't even a check that it's unique, if it exists, it'll update...
+if ( ! empty( $orderid ) ) {
+
+	// } Build pretty log msgs :)
+
+		// } DEFAULTS
+			// } Existing user updated by API
+			$existing_transaction_short_log = __( 'Transaction Updated by API Action', 'zero-bs-crm' ) . ' <i class="fa fa-random"></i>';
+			$existing_transaction_desc_log  = __( 'API Action fired to update a transaction', 'zero-bs-crm' ) . ': #' . $orderid . ' for ' . zeroBSCRM_getCurrencyChr() . $transaction_fields['total'] . ' (Status: ' . $transaction_fields['status'] . ')';
+
+			// } New Transaction from API
+			$new_transaction_short_log = __( 'Transaction Created from API Action', 'zero-bs-crm' ) . ' <i class="fa fa-random"></i>';
+			$new_transaction_desc_log  = __( 'API Action fired to create a transaction', 'zero-bs-crm' ) . ': #' . $orderid . ' for ' . zeroBSCRM_getCurrencyChr() . $transaction_fields['total'] . ' (Status: ' . $transaction_fields['status'] . ')';
+
+	$external_api_name = jpcrm_api_process_external_api_name();
+	if ( $external_api_name !== false ) {
+		$existing_transaction_short_log = sprintf( // phpcs:ignore WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase
+			// Translators: %s is a dynamic service name invoking the API.
+			__( 'Transaction Updated by %s (API)', 'zero-bs-crm' ) . ' <i class="fa fa-random"></i>',
+			$external_api_name
 		);
+		$existing_transaction_desc_log = sprintf( // phpcs:ignore WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase
+			// Translators: %1$s is a dynamic service name invoking the API, %2$s is the transaction ID, %3$s is the currency/value, %4$s is the status string.
+			__( '%1$s fired an API Action to update a transaction: #%2$s for %3$s (Status: %4$s)', 'zero-bs-crm' ),
+			$external_api_name,
+			$orderid,
+			zeroBSCRM_getCurrencyChr() . $transaction_fields['total'],
+			$transaction_fields['status']
+		);
+		$new_transaction_short_log = sprintf( // phpcs:ignore WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase
+			// Translators: %s is a dynamic service name invoking the API.
+			__( 'Transaction Added by %s (API)', 'zero-bs-crm' ) . ' <i class="fa fa-random"></i>',
+			$external_api_name
+		);
+		$new_transaction_desc_log = sprintf( // phpcs:ignore WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase
+			// Translators: %1$s is a dynamic service name invoking the API, %2$s is the transaction ID, %3$s is the currency/value, %4$s is the status string.
+			__( '%1$s fired an API Action to add a transaction: #%2$s for %3$s (Status: %4$s)', 'zero-bs-crm' ),
+			$external_api_name,
+			$orderid,
+			zeroBSCRM_getCurrencyChr() . $transaction_fields['total'],
+			$transaction_fields['status']
+		);
+	}
+			// } Actual log var passed
+			$fall_back_log = array(
+				'type'      => 'API Action',
+				'shortdesc' => $existing_transaction_short_log,
+				'longdesc'  => $existing_transaction_desc_log,
+			);
 
-		foreach ( $new_trans as $field => $value ) {
+			// } Internal automator overrides - here we pass a "transaction.create" note override (so we can pass it a custom str, else we let it fall back to "created by api")
+			$internal_automator_override = array(
 
-			if ( $field === 'orderid' || $field === 'email' || $field === 'fname' ) {
-				continue;
-			}
-			if ( $field === 'date' ) {
-				$transaction_fields[ $field ] = gmdate( 'Y-m-d H:i:s', (int) $value );
-				continue;
-			}
+				'note_override' => array(
 
-			$transaction_fields[ $field ] = sanitize_text_field( $value );
+					'type'      => 'API Action',
+					'shortdesc' => $new_transaction_short_log,
+					'longdesc'  => $new_transaction_desc_log,
+
+				),
+
+			);
+
+			$trans = zeroBS_integrations_addOrUpdateTransaction(
+				'api',
+				$orderid,
+				$transaction_fields,
+				array(), // } TAGS
+				'', // ) Trans date (auto)
+				$fall_back_log, // } Fallback log (for Trans who already exist)
+				false, // } Extra meta
+				$internal_automator_override, // } Internal automator overrides - here we pass a "transaction.create" note override (so we can pass it a custom str, else we let it fall back to "created by API")
+				'' // field prefix, e.g. zbst_
+			);
+	// ^^ this'll be either: ID if added, no of rows if updated, or FALSE if failed to insert/update
+
+	// old way just returned what was sent...
+	// wp_send_json($json_params);
+
+	// thorough much? lol.
+	if ( ! empty( $trans ) && $trans !== false && $trans !== -1 ) {
+
+		// return what was passed...
+		// this is legacy funk.. not ideal at all, should probs reload.
+		$return_params = $new_trans;
+
+		// add the id if it's new
+		if ( $trans > 0 ) {
+			$return_params['id'] = $trans;
 		}
 
-		// } We can only add a trans if it has a unique id ($orderid)
-		// } This isn't even a check that it's unique, if it exists, it'll update...
-		if ( ! empty( $orderid ) ) {
+		// return
+		wp_send_json( $return_params );
 
-			// } Build pretty log msgs :)
+	} else {
 
-				// } DEFAULTS
-					// } Existing user updated by API
-					$existing_transaction_short_log = __( 'Transaction Updated by API Action', 'zero-bs-crm' ) . ' <i class="fa fa-random"></i>';
-					$existing_transaction_desc_log  = __( 'API Action fired to update a transaction', 'zero-bs-crm' ) . ': #' . $orderid . ' for ' . zeroBSCRM_getCurrencyChr() . $transaction_fields['total'] . ' (Status: ' . $transaction_fields['status'] . ')';
+		// fail.
+		wp_send_json( array( 'error' => 100 ) );
 
-					// } New Transaction from API
-					$new_transaction_short_log = __( 'Transaction Created from API Action', 'zero-bs-crm' ) . ' <i class="fa fa-random"></i>';
-					$new_transaction_desc_log  = __( 'API Action fired to create a transaction', 'zero-bs-crm' ) . ': #' . $orderid . ' for ' . zeroBSCRM_getCurrencyChr() . $transaction_fields['total'] . ' (Status: ' . $transaction_fields['status'] . ')';
+	}
+}
 
-			$external_api_name = jpcrm_api_process_external_api_name();
-			if ( $external_api_name !== false ) {
-				$existing_transaction_short_log = sprintf( // phpcs:ignore WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase
-					// Translators: %s is a dynamic service name invoking the API.
-					__( 'Transaction Updated by %s (API)', 'zero-bs-crm' ) . ' <i class="fa fa-random"></i>',
-					$external_api_name
-				);
-				$existing_transaction_desc_log = sprintf( // phpcs:ignore WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase
-					// Translators: %1$s is a dynamic service name invoking the API, %2$s is the transaction ID, %3$s is the currency/value, %4$s is the status string.
-					__( '%1$s fired an API Action to update a transaction: #%2$s for %3$s (Status: %4$s)', 'zero-bs-crm' ),
-					$external_api_name,
-					$orderid,
-					zeroBSCRM_getCurrencyChr() . $transaction_fields['total'],
-					$transaction_fields['status']
-				);
-				$new_transaction_short_log = sprintf( // phpcs:ignore WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase
-					// Translators: %s is a dynamic service name invoking the API.
-					__( 'Transaction Added by %s (API)', 'zero-bs-crm' ) . ' <i class="fa fa-random"></i>',
-					$external_api_name
-				);
-				$new_transaction_desc_log = sprintf( // phpcs:ignore WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase
-					// Translators: %1$s is a dynamic service name invoking the API, %2$s is the transaction ID, %3$s is the currency/value, %4$s is the status string.
-					__( '%1$s fired an API Action to add a transaction: #%2$s for %3$s (Status: %4$s)', 'zero-bs-crm' ),
-					$external_api_name,
-					$orderid,
-					zeroBSCRM_getCurrencyChr() . $transaction_fields['total'],
-					$transaction_fields['status']
-				);
-			}
-					// } Actual log var passed
-					$fallBackLog = array(
-						'type'      => 'API Action',
-						'shortdesc' => $existing_transaction_short_log,
-						'longdesc'  => $existing_transaction_desc_log,
-					);
-
-					// } Internal automator overrides - here we pass a "transaction.create" note override (so we can pass it a custom str, else we let it fall back to "created by api")
-					$internalAutomatorOverride = array(
-
-						'note_override' => array(
-
-							'type'      => 'API Action',
-							'shortdesc' => $new_transaction_short_log,
-							'longdesc'  => $new_transaction_desc_log,
-
-						),
-
-					);
-
-					$trans = zeroBS_integrations_addOrUpdateTransaction(
-						'api',
-						$orderid,
-						$transaction_fields,
-						array(), // } TAGS
-						'', // ) Trans date (auto)
-						$fallBackLog, // } Fallback log (for Trans who already exist)
-						false, // } Extra meta
-						$internalAutomatorOverride, // } Internal automator overrides - here we pass a "transaction.create" note override (so we can pass it a custom str, else we let it fall back to "created by API")
-						'' // field prefix, e.g. zbst_
-					);
-			// ^^ this'll be either: ID if added, no of rows if updated, or FALSE if failed to insert/update
-
-			// old way just returned what was sent...
-			// wp_send_json($json_params);
-
-			// thorough much? lol.
-			if ( ! empty( $trans ) && $trans !== false && $trans !== -1 ) {
-
-				// return what was passed...
-				// this is legacy funk.. not ideal at all, should probs reload.
-				$return_params = $new_trans;
-
-				// add id (if new)
-				if ( $trans > 0 ) {
-					$return_params['id'] = $trans;
-				}
-
-				// return
-				wp_send_json( $return_params );
-
-			} else {
-
-				// fail.
-				wp_send_json( array( 'error' => 100 ) );
-
-			}
-		}
-
-		exit();
+exit();
