3e275bbe7a0a0194f6ffad7c01e6da8713041d2a
Foteini Giannaropoulou <giannaropoulou.foteini@gmail.com>
2023-10-18 16:56:25 +0300

    CI: Add WC tests in matrix

--- projects/plugins/jetpack/tests/action-test-php.sh
+++ projects/plugins/jetpack/tests/action-test-php.sh
@@ -4,22 +4,25 @@ set -eo pipefail
 
 # Conditionally allow WooCommerce/Jetpack integration tests to run.
 # Unlike WP_MULTISITE or LEGACY_FULL_SYNC, this only enables some tests, it doesn't change the behavior of any.
-if [[ -d "/tmp/wordpress-$WP_BRANCH/src/wp-content/plugins/woocommerce/tests" ]]; then
+if [[ "$WITH_WOOCOMMERCE" == true ]]; then
 	export JETPACK_TEST_WOOCOMMERCE=1
-fi
-
-echo "::group::Jetpack tests"
-phpunit
-echo "::endgroup::"
-
-if [[ "$WP_BRANCH" == "trunk" ]]; then
-	echo "::group::Jetpack multisite tests"
-	WP_MULTISITE=1 phpunit -c tests/php.multisite.xml
+	echo "::group::Jetpack tests"
+	phpunit --group=woocommerce
 	echo "::endgroup::"
-fi
-
-if [[ "$WP_BRANCH" == "latest" && "$PHP_VERSION" == "7.0" ]]; then
-	echo "::group::Jetpack Legacy Full Sync tests"
-	LEGACY_FULL_SYNC=1 phpunit --group=legacy-full-sync
+else
+	echo "::group::Jetpack tests"
+	phpunit
 	echo "::endgroup::"
+
+	if [[ "$WP_BRANCH" == "trunk" ]]; then
+		echo "::group::Jetpack multisite tests"
+		WP_MULTISITE=1 phpunit -c tests/php.multisite.xml
+		echo "::endgroup::"
+	fi
+
+	if [[ "$WP_BRANCH" == "latest" && "$PHP_VERSION" == "7.0" ]]; then
+		echo "::group::Jetpack Legacy Full Sync tests"
+		LEGACY_FULL_SYNC=1 phpunit --group=legacy-full-sync
+		echo "::endgroup::"
+	fi
 fi
