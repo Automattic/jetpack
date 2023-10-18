e50eb0a4754a142b9a30283a52f4cf942d20cb3c
Kevin Zoschke <kevin.zoschke@automattic.com>
2023-10-18 10:01:06 -0400

    Make AI Assistant description string only

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/editor.js
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/editor.js
@@ -1,7 +1,3 @@
-import { getRedirectUrl } from '@automattic/jetpack-components';
-import { ExternalLink } from '@wordpress/components';
-import { Fragment } from '@wordpress/element';
-import { __ } from '@wordpress/i18n';
 import { registerJetpackBlockFromMetadata } from '../../shared/register-jetpack-block';
 import metadata from './block.json';
 import edit from './edit';
@@ -19,21 +15,5 @@ import './extensions/jetpack-contact-form';
 registerJetpackBlockFromMetadata( metadata, {
 	edit,
 	save: () => null,
-	// The `description` property in `block.json` doesn't support formatting or markup.
-	description: (
-		<Fragment>
-			<p style={ { whiteSpace: 'pre-wrap' } }>{ metadata.description }</p>
-			<p>
-				<ExternalLink href="https://automattic.com/ai-guidelines">
-					{ __( 'AI guidelines.', 'jetpack' ) }
-				</ExternalLink>
-			</p>
-			<p>
-				<ExternalLink href={ getRedirectUrl( 'jetpack_ai_feedback' ) }>
-					{ __( 'Share your feedback.', 'jetpack' ) }
-				</ExternalLink>
-			</p>
-		</Fragment>
-	),
 	transforms,
 } );
