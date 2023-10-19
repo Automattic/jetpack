a7ba9ed1d195a958a468b73870d8c7b74b01a3df
Nauris Pūķis <hi@pyronaur.com>
2023-10-19 10:39:46 +0300

    Extract tabs.scss

--- projects/plugins/boost/app/assets/src/js/modules/image-size-analysis/recommendations/Tabs.svelte
+++ projects/plugins/boost/app/assets/src/js/modules/image-size-analysis/recommendations/Tabs.svelte
@@ -24,20 +24,20 @@
 	$: currentTab = imageDataGroupTabs[ activeGroup ];
 </script>
 
-<div class="dropdown">
+<div class="jb-dropdown">
 	<!-- svelte-ignore a11y-click-events-have-key-events -->
-	<div class="head-bar" on:click={onClickDropdown}>
+	<div class="jb-dropdown__head-bar" on:click={onClickDropdown}>
 		{isaGroupLabels[ activeGroup ]}
 		{#if currentTab?.issue_count > 0}
-			<span class="issues">{currentTab.issue_count}</span>
+			<span class="jb-dropdown__issues">{currentTab.issue_count}</span>
 		{/if}
-		<span class="dashicons dashicons-arrow-down-alt2" />
+		<span class="jb-dropdown__dashicons dashicons-arrow-down-alt2" />
 	</div>
 
 	{#if dropdownOpen}
-		<ul class="options">
+		<ul class="jb-dropdown__options">
 			{#each Object.entries( imageDataGroupTabs ) as [group, details]}
-				{@const  issues = details.issue_count }
+				{@const ( issues = details.issue_count )}
 
 				<!-- svelte-ignore a11y-click-events-have-key-events -->
 				<li
@@ -46,7 +46,7 @@
 					on:click={() => issues > 0 && selectGroup( group )}
 				>
 					{isaGroupLabels[ group ]}
-					<span class="issues">{issues}</span>
+					<span class="jb-dropdown__issues">{issues}</span>
 				</li>
 			{/each}
 		</ul>
@@ -55,8 +55,8 @@
 
 <div class="jb-tabs">
 	{#each Object.entries( imageDataGroupTabs ) as [group, details]}
-		{@const  label = isaGroupLabels[ group ] }
-		{@const  issues = details.issue_count }
+		{@const ( label = isaGroupLabels[ group ] )}
+		{@const ( issues = details.issue_count )}
 
 		<div class="jb-tab jb-tab--{group}" class:active={activeGroup === group}>
 			<div class="jb-tab__header">
@@ -79,99 +79,3 @@
 		</div>
 	{/each}
 </div>
-
-<style lang="scss">
-	.dropdown {
-		/* Hide on wide screens */
-		@media ( min-width: 783px ) {
-			display: none;
-		}
-
-		margin-bottom: 32px;
-
-		.head-bar {
-			background-color: #fff;
-			border: 1px solid #000;
-			border-radius: 1px;
-			padding: 8px 12px;
-
-			.dashicons {
-				float: right;
-			}
-		}
-
-		ul {
-			margin: 0px;
-			border: 1px solid #000;
-			background-color: #fff;
-			padding: 8px 12px;
-			color: var( --gray-10 );
-
-			li.active {
-				color: black;
-			}
-
-			li.selected {
-				background-color: var( --gray-0 );
-			}
-		}
-	}
-
-	.jb-tabs {
-		display: flex;
-		border-bottom: 1px solid var( --gray-5 );
-		margin-bottom: 32px;
-		gap: var( --expanded-gap );
-
-		/* Hide on narrow screens. */
-		@media ( max-width: 782px ) {
-			display: none;
-		}
-	}
-
-	.jb-tab {
-		min-width: 90px;
-		display: flex;
-		justify-content: center;
-		margin-bottom: -1px; // offset border
-		border-bottom: 1px solid transparent;
-		&.active {
-			border-bottom: 1px solid black;
-		}
-	}
-
-	.jb-tab--ignored {
-		margin-left: auto;
-	}
-
-	.jb-tab:hover,
-	.jb-tab:focus-within {
-		border-bottom: 1px solid var( --gray-40 );
-	}
-
-	.jb-tabs :global( .jb-navigator-link ) {
-		background: none;
-		padding: 8px 16px;
-		border: 0;
-		cursor: pointer;
-		text-decoration: none;
-
-		display: flex;
-		gap: 10px;
-		align-items: center;
-		justify-content: center;
-		outline: 0;
-		box-shadow: none;
-	}
-	.jb-navigator-link--inactive {
-		opacity: 0.5;
-		cursor: default;
-	}
-	.jb-tab span,
-	.dropdown span.issues {
-		padding: 2px 8px;
-		font-size: 0.75rem;
-		border-radius: var( --border-radius );
-		background-color: var( --gray-5 );
-	}
-</style>
