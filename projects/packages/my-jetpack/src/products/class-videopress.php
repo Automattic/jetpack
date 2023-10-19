a94517b6eee932d10fce9a3689f01a375dde7084
Christian Gastrell <cgastrell@gmail.com>
2023-10-19 17:58:08 -0300

    just return true as a way to revert 33410 as the lib dependency is not properly imported

--- projects/packages/my-jetpack/src/products/class-videopress.php
+++ projects/packages/my-jetpack/src/products/class-videopress.php
@@ -7,7 +7,6 @@
 
 namespace Automattic\Jetpack\My_Jetpack\Products;
 
-use Automattic\Jetpack\Current_Plan;
 use Automattic\Jetpack\My_Jetpack\Hybrid_Product;
 use Automattic\Jetpack\My_Jetpack\Wpcom_Products;
 
@@ -164,7 +163,6 @@ class Videopress extends Hybrid_Product {
 	 * @return boolean
 	 */
 	public static function has_required_plan() {
-		// using second argument `true` to force fetching from wpcom
-		return Current_Plan::supports( 'videopress-1tb-storage', true );
+		return true;
 	}
 }
