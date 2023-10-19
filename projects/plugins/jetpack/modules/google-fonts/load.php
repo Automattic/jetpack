9eadf36dff682e96198c73b5c24f86c48a740325
arthur <arthur.chu@automattic.com>
2023-10-19 13:39:53 +0800

    Load google fonts after plugins are loaded

--- projects/plugins/jetpack/modules/google-fonts/load.php
+++ projects/plugins/jetpack/modules/google-fonts/load.php
@@ -5,10 +5,15 @@
  * @package automattic/jetpack
  */
 
-if ( class_exists( 'WP_Font_Face' ) ) {
-	// WordPress 6.5 or above with the new Font Library.
-	require_once __DIR__ . '/current/load-google-fonts.php';
-} elseif ( class_exists( 'WP_Fonts' ) || class_exists( 'WP_Webfonts' ) ) {
-	// Gutenberg Fonts API compatible.
-	require_once __DIR__ . '/wordpress-6.3/load-google-fonts.php';
-}
+add_action(
+	'plugins_loaded',
+	function () {
+		if ( class_exists( 'WP_Font_Face' ) ) {
+			// WordPress 6.5 or above with the new Font Library.
+			require_once __DIR__ . '/current/load-google-fonts.php';
+		} elseif ( class_exists( 'WP_Fonts' ) || class_exists( 'WP_Webfonts' ) ) {
+			// Gutenberg Fonts API compatible.
+			require_once __DIR__ . '/wordpress-6.3/load-google-fonts.php';
+		}
+	}
+);
