f1bbab2ce288221d050bca4a8502be020f75d83d
Kevin Zoschke <kevin.zoschke@automattic.com>
2023-10-19 12:13:58 -0400

    Refactor Payments block registration

--- projects/plugins/jetpack/extensions/blocks/payments-intro/editor.js
+++ projects/plugins/jetpack/extensions/blocks/payments-intro/editor.js
@@ -1,5 +1,25 @@
-import registerJetpackBlock from '../../shared/register-jetpack-block';
-import { name, settings } from '.';
+import { InnerBlocks } from '@wordpress/block-editor';
+import { registerJetpackBlockFromMetadata } from '../../shared/register-jetpack-block';
+import { settings as donationSettings } from '../donations';
+import { settings as premiumContentSettings } from '../premium-content';
+import { settings as recurringPaymentSettings } from '../recurring-payments';
+import metadata from './block.json';
+import edit from './edit';
+
 import './editor.scss';
 
-registerJetpackBlock( name, settings );
+registerJetpackBlockFromMetadata( metadata, {
+	edit,
+	save: () => <InnerBlocks.Content />,
+	keywords: [
+		...new Set( [
+			...metadata.keywords,
+			donationSettings.title,
+			...donationSettings.keywords,
+			recurringPaymentSettings.title,
+			...recurringPaymentSettings.keywords,
+			premiumContentSettings.title,
+			...premiumContentSettings.keywords,
+		] ),
+	],
+} );
