a5aa5d0007b4f8a9e90f2a6297ed2e5c6a2d7da0
Damián Suárez <rdsuarez@gmail.com>
2023-10-19 16:55:09 +0200

    clean

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/ai-assistant/with-ai-assistant.tsx
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/ai-assistant/with-ai-assistant.tsx
@@ -7,22 +7,12 @@ import React from 'react';
 /**
  * Internal dependencies
  */
-import AiAssistantDropdown, { KEY_ASK_AI_ASSISTANT } from '../../components/ai-assistant-controls';
-import { getBlockTextContent } from '../../lib/utils/block-content';
-/*
- * Types
- */
+import AiAssistantDropdown from '../../components/ai-assistant-controls';
 
 export function getStoreBlockId( clientId ) {
 	return `ai-assistant-block-${ clientId }`;
 }
 
-export function getBlocksContent( blocks ) {
-	return blocks
-		.filter( block => block != null ) // Safeguard against null or undefined blocks
-		.map( block => getBlockTextContent( block.clientId ) )
-		.join( '\n\n' );
-}
 /*
  * Extend the withAIAssistant function of the block
  * to implement multiple blocks edition
@@ -30,15 +20,6 @@ export function getBlocksContent( blocks ) {
 export const withAIAssistant = createHigherOrderComponent(
 	BlockEdit => props => {
 		const { name: blockType } = props;
-
-		/*
-		 * Set exclude dropdown options.
-		 * - Exclude "Ask AI Assistant" for core/list-item block.
-		 */
-		const exclude = [];
-		if ( blockType === 'core/list-item' ) {
-			exclude.push( KEY_ASK_AI_ASSISTANT );
-		}
 
 		const blockControlProps = {
 			group: 'block',
@@ -49,7 +30,7 @@ export const withAIAssistant = createHigherOrderComponent(
 				<BlockEdit { ...props } />
 
 				<BlockControls { ...blockControlProps }>
-					<AiAssistantDropdown blockType={ blockType } disabled={ false } exclude={ exclude } />
+					<AiAssistantDropdown blockType={ blockType } disabled={ false } />
 				</BlockControls>
 			</>
 		);
