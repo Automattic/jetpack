09514a72bb6a21dc92242ac470e169309708bf58
Douglas <douglas.henri@automattic.com>
2023-10-20 14:06:26 -0300

    add disabled state to toolbar dropdown menus

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/components/tone-dropdown-control/index.tsx
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/components/tone-dropdown-control/index.tsx
@@ -8,6 +8,8 @@ import {
 	ToolbarDropdownMenu,
 	DropdownMenu,
 	Icon,
+	Button,
+	Tooltip,
 } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { chevronRight } from '@wordpress/icons';
@@ -131,6 +133,7 @@ type ToneToolbarDropdownMenuProps = {
 	value?: ToneProp;
 	onChange: ( value: ToneProp ) => void;
 	label?: string;
+	disabled?: boolean;
 };
 
 const ToneMenuGroup = ( { value, onChange }: ToneToolbarDropdownMenuProps ) => (
@@ -187,11 +190,20 @@ export function ToneDropdownMenu( {
 export default function ToneToolbarDropdownMenu( {
 	value = DEFAULT_PROMPT_TONE,
 	onChange,
+	disabled = false,
 }: ToneToolbarDropdownMenuProps ) {
-	return (
+	const label = __( 'Change tone', 'jetpack' );
+
+	return disabled ? (
+		<Tooltip text={ label }>
+			<Button disabled>
+				<Icon icon={ speakToneIcon } />
+			</Button>
+		</Tooltip>
+	) : (
 		<ToolbarDropdownMenu
 			icon={ speakToneIcon }
-			label={ __( 'Change tone', 'jetpack' ) }
+			label={ label }
 			popoverProps={ {
 				variant: 'toolbar',
 			} }
