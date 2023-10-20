a8520b3bc4dda8e5453abbbdedb31c902e382780
Luiz Kowalski <lhkowalski@gmail.com>
2023-10-20 18:51:59 -0300

    Only show the upgrade button when the user role allows plan purchase

--- projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/usage-panel/index.tsx
+++ projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/usage-panel/index.tsx
@@ -9,10 +9,12 @@ import { __, sprintf } from '@wordpress/i18n';
 import './style.scss';
 import useAICheckout from '../../../../blocks/ai-assistant/hooks/use-ai-checkout';
 import useAIFeature from '../../../../blocks/ai-assistant/hooks/use-ai-feature';
+import { canUserPurchasePlan } from '../../../../blocks/ai-assistant/lib/connection';
 import UsageBar from '../usage-bar';
 
 export default function UsagePanel() {
 	const { checkoutUrl, autosaveAndRedirect, isRedirecting } = useAICheckout();
+	const canUpgrade = canUserPurchasePlan();
 
 	// fetch usage data
 	const { hasFeature, requestsCount, requestsLimit } = useAIFeature();
@@ -52,7 +54,7 @@ export default function UsagePanel() {
 				</p>
 			) }
 
-			{ ! hasFeature && (
+			{ ! hasFeature && canUpgrade && (
 				<Button
 					variant="primary"
 					label="Upgrade your Jetpack AI plan"
