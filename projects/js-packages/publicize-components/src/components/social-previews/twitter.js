e35b11f717af15de09aaf0bdb30f48d9c43a7b9f
Paul Bunkham <paul@dobit.co.uk>
2023-10-23 08:05:29 +0100

    Fix the twitter preview and swap to useSelect.

--- projects/js-packages/publicize-components/src/components/social-previews/twitter.js
+++ projects/js-packages/publicize-components/src/components/social-previews/twitter.js
@@ -1,32 +1,39 @@
 import { TwitterPreviews } from '@automattic/social-previews';
-import { withSelect } from '@wordpress/data';
+import { useSelect } from '@wordpress/data';
 import React from 'react';
 
 /**
  * The twitter tab component.
  *
  * @param {object} props - The props.
- * @param {object[]} props.tweets - The tweets.
+ * @param {string} props.title - The post title
+ * @param {string} props.description - The post description/excerpt
+ * @param {object} props.image - The post featured image
+ * @param {string} props.url - The URL of the post
+ * @param {object[]} props.media - Array of attached media
  * @returns {React.ReactNode} The twitter tab component.
  */
-function Twitter( { tweets } ) {
+function Twitter( { title, description, image, url, media } ) {
+	const tweets = useSelect(
+		select => {
+			const { getTweetTemplate, getShareMessage } = select( 'jetpack/publicize' );
+			return [
+				{
+					...getTweetTemplate(),
+					text: getShareMessage() + ( media.length ? ` ${ url }` : '' ),
+					cardType: image ? 'summary_large_image' : 'summary',
+					title,
+					description,
+					image,
+					media,
+					url,
+				},
+			];
+		},
+		[ title, image, description, media, url ]
+	);
+
 	return <TwitterPreviews tweets={ tweets } hidePostPreview />;
 }
 
-export default withSelect( ( select, { title, description, image, url, media } ) => {
-	const { getTweetTemplate, getShareMessage } = select( 'jetpack/publicize' );
-
-	const tweets = [
-		{
-			...getTweetTemplate(),
-			text: getShareMessage() + ( media.length ? ` ${ url }` : '' ),
-			cardType: image ? 'summary_large_image' : 'summary',
-			title,
-			description,
-			image,
-			media,
-			url,
-		},
-	];
-	return { tweets };
-} )( Twitter );
+export default Twitter;
--- projects/js-packages/publicize-components/src/store/selectors.js
+++ projects/js-packages/publicize-components/src/store/selectors.js
@@ -78,6 +78,32 @@ export function getInstagramDetails() {
 }
 
 /**
+ * Returns a template for tweet data, based on the first Twitter account found.
+ *
+ * @param {object} state - State object.
+ * @returns {object} The Twitter account data.
+ */
+export function getTweetTemplate( state ) {
+	/*
+	 * state.connections is not used anymore,
+	 * since they are stored into the post meta.
+	 * This is kept for backward compatibility,
+	 * especially for the selector tests.
+	 * it should be removed in the future.
+	 * Take a look at the getTweetstormHelper
+	 * helper for more details,
+	 */
+	const connections = state.connections || getConnections();
+	const twitterAccount = connections?.find( connection => 'twitter' === connection.service_name );
+
+	return {
+		name: twitterAccount?.profile_display_name,
+		profileImage: twitterAccount?.profile_picture,
+		screenName: twitterAccount?.display_name,
+	};
+}
+
+/**
  * Gets the message that will be used hen sharing this post.
  *
  * @returns {string} The share message.
