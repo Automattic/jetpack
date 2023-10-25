cbb16bb5e0c34abff9f57e7494e8e0bcbf69d1bd
Kateryna Kodonenko <kateryna@automattic.com>
2023-10-25 10:55:25 +0200

    Add explanation comment

--- projects/plugins/jetpack/modules/masterbar/admin-menu/class-atomic-admin-menu.php
+++ projects/plugins/jetpack/modules/masterbar/admin-menu/class-atomic-admin-menu.php
@@ -135,6 +135,7 @@ class Atomic_Admin_Menu extends Admin_Menu {
 		// Calypso plugins screens link.
 		$plugins_slug = 'https://wordpress.com/plugins/' . $this->domain;
 
+		// Link to the Marketplace from Plugins > Add New on Atomic sites where the wpcom_admin_interface option is set to wp-admin.
 		if ( self::CLASSIC_VIEW === $this->get_preferred_view( 'plugins.php' ) ) {
 			$submenus_to_update = array( 'plugin-install.php' => $plugins_slug );
 			$this->update_submenus( 'plugins.php', $submenus_to_update );
