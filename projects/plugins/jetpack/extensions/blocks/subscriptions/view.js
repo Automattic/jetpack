981065af989d60a61f0658917f3173293206f52e
Fabien MILLERAND <millerf@automattic.com>
2023-10-26 06:04:07 +0200

    Move iframe stuff to shared lib to use in paid-content
    Change iframe wording to modal

--- projects/plugins/jetpack/extensions/blocks/subscriptions/view.js
+++ projects/plugins/jetpack/extensions/blocks/subscriptions/view.js
@@ -1,50 +1,8 @@
-/* global tb_show */
-
 import './view.scss';
 import '../../shared/memberships.scss';
 
 import domReady from '@wordpress/dom-ready';
-import { handleIframeResult } from '../../shared/memberships';
-
-// @ts-ignore
-function show_iframe_retrieve_subscriptions_from_email() {
-	const form = document.querySelector( '.wp-block-jetpack-subscriptions__container form' );
-	if ( ! form ) {
-		return;
-	}
-
-	if ( ! form.checkValidity() ) {
-		form.reportValidity();
-		return;
-	}
-
-	const email = form.querySelector( 'input[type=email]' ).value;
-
-	show_iframe( {
-		email,
-		blog: form.dataset.blog,
-		plan: 'newsletter',
-		source: 'jetpack_retrieve_subscriptions',
-		post_access_level: form.dataset.post_access_level,
-		display: 'alternate',
-	} );
-}
-
-function show_iframe( data ) {
-	const params = new URLSearchParams( data );
-
-	const url = 'https://subscribe.wordpress.com/memberships/?' + params.toString();
-
-	window.scrollTo( 0, 0 );
-	tb_show( null, url + '&TB_iframe=true', null );
-
-	window.addEventListener( 'message', handleIframeResult, false );
-	const tbWindow = document.querySelector( '#TB_window' );
-	tbWindow.classList.add( 'jetpack-memberships-modal' );
-
-	// This line has to come after the Thickbox has opened otherwise Firefox doesnt scroll to the top.
-	window.scrollTo( 0, 0 );
-}
+import { show_modal_retrieve_subscriptions_from_email, show_modal } from '../../shared/memberships';
 
 domReady( function () {
 	const form = document.querySelector( '.wp-block-jetpack-subscriptions__container form' );
@@ -57,7 +15,7 @@ domReady( function () {
 	if ( link ) {
 		link.addEventListener( 'click', function ( event ) {
 			event.preventDefault();
-			show_iframe_retrieve_subscriptions_from_email();
+			show_modal_retrieve_subscriptions_from_email();
 		} );
 	}
 
@@ -79,7 +37,7 @@ domReady( function () {
 
 			const tier_id = form.querySelector( 'input[name=tier_id]' )?.value ?? '';
 
-			show_iframe( {
+			show_modal( {
 				email,
 				post_id,
 				tier_id,
--- projects/plugins/jetpack/extensions/shared/memberships.js
+++ projects/plugins/jetpack/extensions/shared/memberships.js
@@ -2,30 +2,6 @@
 
 let premiumContentJWTTokenForCookie = '';
 
-/**
- * @typedef globalThis
- * @param {globalThis.Event} eventFromIframe - message event that gets emitted in the checkout iframe.
- * @listens window#message
- */
-export function handleIframeResult( eventFromIframe ) {
-	if ( eventFromIframe.origin === 'https://subscribe.wordpress.com' && eventFromIframe.data ) {
-		const data = JSON.parse( eventFromIframe.data );
-		if ( data && data.result && data.result.jwt_token ) {
-			// We save the token for now, doing nothing.
-			premiumContentJWTTokenForCookie = data.result.jwt_token;
-			setPurchaseResultCookie( premiumContentJWTTokenForCookie );
-		}
-		if ( data && data.action === 'close' && premiumContentJWTTokenForCookie ) {
-			// The token was set during the purchase flow, we want to reload the whole page so the content is displayed
-			window.location.reload();
-		} else if ( data && data.action === 'close' ) {
-			// User just aborted.
-			window.removeEventListener( 'message', handleIframeResult );
-			tb_remove && tb_remove();
-		}
-	}
-}
-
 function setUpThickbox( button ) {
 	button.addEventListener( 'click', event => {
 		event.preventDefault();
@@ -98,3 +74,66 @@ export const reloadPageWithPremiumContentQueryString = function (
 	}
 	document.location.href = newQueryString;
 };
+
+export function show_modal( data ) {
+	const params = new URLSearchParams( data );
+
+	const url = 'https://subscribe.wordpress.com/memberships/?' + params.toString();
+
+	window.scrollTo( 0, 0 );
+	tb_show( null, url + '&TB_iframe=true', null );
+
+	window.addEventListener( 'message', handleIframeResult, false );
+	const tbWindow = document.querySelector( '#TB_window' );
+	tbWindow.classList.add( 'jetpack-memberships-modal' );
+
+	// This line has to come after the Thickbox has opened otherwise Firefox doesnt scroll to the top.
+	window.scrollTo( 0, 0 );
+}
+
+export function show_modal_retrieve_subscriptions_from_email() {
+	const form = document.querySelector( '.wp-block-jetpack-subscriptions__container form' );
+	if ( ! form ) {
+		return;
+	}
+
+	if ( ! form.checkValidity() ) {
+		form.reportValidity();
+		return;
+	}
+
+	const email = form.querySelector( 'input[type=email]' ).value;
+
+	show_modal( {
+		email,
+		blog: form.dataset.blog,
+		plan: 'newsletter',
+		source: 'jetpack_retrieve_subscriptions',
+		post_access_level: form.dataset.post_access_level,
+		display: 'alternate',
+	} );
+}
+
+/**
+ * @typedef globalThis
+ * @param {globalThis.Event} eventFromIframe - message event that gets emitted in the checkout iframe.
+ * @listens window#message
+ */
+export function handleIframeResult( eventFromIframe ) {
+	if ( eventFromIframe.origin === 'https://subscribe.wordpress.com' && eventFromIframe.data ) {
+		const data = JSON.parse( eventFromIframe.data );
+		if ( data && data.result && data.result.jwt_token ) {
+			// We save the token for now, doing nothing.
+			premiumContentJWTTokenForCookie = data.result.jwt_token;
+			setPurchaseResultCookie( premiumContentJWTTokenForCookie );
+		}
+		if ( data && data.action === 'close' && premiumContentJWTTokenForCookie ) {
+			// The token was set during the purchase flow, we want to reload the whole page so the content is displayed
+			window.location.reload();
+		} else if ( data && data.action === 'close' ) {
+			// User just aborted.
+			window.removeEventListener( 'message', handleIframeResult );
+			tb_remove && tb_remove();
+		}
+	}
+}
