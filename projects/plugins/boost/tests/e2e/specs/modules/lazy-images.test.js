41faa4d9e8ce3505384fd4985b6ac8047c0f7066
Peter Petrov <peter.petrov89@gmail.com>
2023-10-27 15:37:37 +0300

    Remove unneeded e2e test

--- projects/plugins/boost/tests/e2e/specs/modules/lazy-images.test.js
+++ projects/plugins/boost/tests/e2e/specs/modules/lazy-images.test.js
@@ -26,14 +26,4 @@ test.describe( 'Lazy Images module', () => {
 			'No images should be lazy loaded'
 		).toBe( 0 );
 	} );
-
-	test( 'Images on a post should be lazy loaded when the module is active', async () => {
-		await boostPrerequisitesBuilder( page ).withActiveModules( [ 'lazy_images' ] ).build();
-		const frontend = await PostFrontendPage.visit( page );
-		await frontend.click( `text=${ testPostTitle }` );
-		expect(
-			await page.locator( '.jetpack-lazy-image' ).count(),
-			'Images should be lazy loaded'
-		).toBeGreaterThan( 0 );
-	} );
 } );
