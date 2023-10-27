69606ebb98904f0ce1ca808cab04f75196e84bfa
Kevin Zoschke <kevin.zoschke@automattic.com>
2023-10-27 15:51:25 -0400

    Revert "Refactor Payments block registration"
    This reverts commit 77d2c8bda608bdf31b5dd0d52616695bacd98893.

--- projects/plugins/jetpack/extensions/blocks/payments-intro/editor.js
+++ projects/plugins/jetpack/extensions/blocks/payments-intro/editor.js
@@ -1,25 +1,5 @@
-import { InnerBlocks } from '@wordpress/block-editor';
-import { registerJetpackBlockFromMetadata } from '../../shared/register-jetpack-block';
-import { settings as donationSettings } from '../donations';
-import { settings as premiumContentSettings } from '../premium-content';
-import { settings as recurringPaymentSettings } from '../recurring-payments';
-import metadata from './block.json';
-import edit from './edit';
-
+import registerJetpackBlock from '../../shared/register-jetpack-block';
+import { name, settings } from '.';
 import './editor.scss';
 
-registerJetpackBlockFromMetadata( metadata, {
-	edit,
-	save: () => <InnerBlocks.Content />,
-	keywords: [
-		...new Set( [
-			...metadata.keywords,
-			donationSettings.title,
-			...donationSettings.keywords,
-			recurringPaymentSettings.title,
-			...recurringPaymentSettings.keywords,
-			premiumContentSettings.title,
-			...premiumContentSettings.keywords,
-		] ),
-	],
-} );
+registerJetpackBlock( name, settings );
