1fc75562a916dcd6b6b63e232769ce13fb856a86
Jeremy Herve <jeremy@jeremy.hu>
2023-10-30 18:06:59 +0100

    Mark Partner package as deprecated

---
+++ projects/packages/partner/src/class-partner-coupon.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Class for the Jetpack partner coupon logic.
+ *
+ * @deprecated $$next-version$$
+ * @see automattic/jetpack-connection
+ *
+ * @package automattic/jetpack-partner
+ */
