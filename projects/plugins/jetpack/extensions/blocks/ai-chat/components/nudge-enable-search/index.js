e6b75674a63092155425eae4c65fb90f472f2332
dereksmart <smart@automattic.com>
2023-10-31 17:05:34 -0400

    Refactor into separate nudge components

--- projects/plugins/jetpack/extensions/blocks/ai-chat/components/nudge-enable-search/index.js
+++ projects/plugins/jetpack/extensions/blocks/ai-chat/components/nudge-enable-search/index.js
@@ -9,55 +9,63 @@ import { __ } from '@wordpress/i18n';
 import { Nudge } from '../../../../shared/components/upgrade-nudge';
 import useAutosaveAndRedirect from '../../../../shared/use-autosave-and-redirect';
 
+const UpgradePrompt = () => {
+	const calypsoSlug = window?.Jetpack_AIChatBlock?.jetpackSettings?.calypsoSlug;
+	const redirectUrl = window.location;
+	const upgradeUrl = getProductCheckoutUrl( 'jetpack_search', calypsoSlug, redirectUrl, true );
+	const { autosaveAndRedirect, isRedirecting } = useAutosaveAndRedirect( upgradeUrl );
+	const goToCheckoutPage = event => {
+		autosaveAndRedirect( event );
+	};
+	return (
+		<Nudge
+			buttonText={ __( 'Upgrade', 'jetpack' ) }
+			checkoutUrl={ upgradeUrl }
+			className={ 'jetpack-ai-connect-banner' }
+			description={ __( 'Upgrade to enable Jetpack Search', 'jetpack' ) }
+			goToCheckoutPage={ goToCheckoutPage }
+			isRedirecting={ isRedirecting }
+			visible={ true }
+		/>
+	);
+};
+
+const ActivatePrompt = () => {
+	let wpAdminUrl = window?.Jetpack_Editor_Initial_State?.adminUrl || '';
+	// Remove frame-nonce in case we are doing calypsoify
+	wpAdminUrl = wpAdminUrl.replace( /wp-admin\/\?frame-nonce=[a-z0-9]+/, 'wp-admin/' );
+	const activateUrl = `${ wpAdminUrl }admin.php?page=jetpack-search`;
+	const { autosaveAndRedirect, isRedirecting } = useAutosaveAndRedirect( activateUrl );
+	const goToActivatePage = event => {
+		autosaveAndRedirect( event );
+	};
+	return (
+		<Nudge
+			buttonText={ __( 'Enable', 'jetpack' ) }
+			checkoutUrl={ activateUrl }
+			className={ 'jetpack-ai-connect-banner' }
+			description={ __(
+				'Turn on the Instant Search feature so Jetpack AI can index your site.',
+				'jetpack'
+			) }
+			goToCheckoutPage={ goToActivatePage }
+			isRedirecting={ isRedirecting }
+			visible={ true }
+		/>
+	);
+};
+
 const EnableJetpackSearchPrompt = () => {
 	const instantSearchEnabled = window?.Jetpack_AIChatBlock?.jetpackSettings?.instant_search_enabled;
 	const isFreePlan = window?.Jetpack_AIChatBlock?.jetpackSettings?.is_free_plan;
 	const planSupportsSearch = window?.Jetpack_AIChatBlock?.jetpackSettings?.plan_supports_search;
 	const requiresUpgrade = isFreePlan || ! planSupportsSearch;
 
-	// Build the upgrade or activate URLs and flows.
-	const calypsoSlug = window?.Jetpack_AIChatBlock?.jetpackSettings?.calypsoSlug;
-	const redirectUrl = window.location;
-	const upgradeUrl = getProductCheckoutUrl( 'jetpack_search', calypsoSlug, redirectUrl, true );
-
-	let wpAdminUrl = window?.Jetpack_Editor_Initial_State?.adminUrl || '';
-	// We have to remove frame-nonce in case we are doing calypsoify.
-	wpAdminUrl = wpAdminUrl.replace( /wp-admin\/\?frame-nonce=[a-z0-9]+/, 'wp-admin/' );
-	const activateUrl = `${ wpAdminUrl }admin.php?page=jetpack-search`;
-	const checkoutUrl = requiresUpgrade ? upgradeUrl : activateUrl;
-	const { autosaveAndRedirect, isRedirecting } = useAutosaveAndRedirect( checkoutUrl );
-
-	const goToCheckoutPage = event => {
-		autosaveAndRedirect( event );
-	};
-
 	if ( instantSearchEnabled && ! requiresUpgrade ) {
 		return null;
 	}
 
-	return (
-		<Nudge
-			buttonText={
-				requiresUpgrade ? __( 'Upgrade', 'jetpack' ) : __( 'Enable Instant Search', 'jetpack' )
-			}
-			checkoutUrl={ checkoutUrl }
-			className={ 'jetpack-ai-connect-banner' }
-			description={
-				requiresUpgrade
-					? __( 'Upgrade to enable Jetpack Search', 'jetpack' )
-					: __(
-							'You need to enable the Instant Search feature so that Jetpack AI can index your site.',
-							'jetpack'
-					  )
-			}
-			goToCheckoutPage={ goToCheckoutPage }
-			isRedirecting={ isRedirecting }
-			visible={ true }
-			align={ null }
-			title={ null }
-			context={ null }
-		/>
-	);
+	return requiresUpgrade ? <UpgradePrompt /> : <ActivatePrompt />;
 };
 
 export default EnableJetpackSearchPrompt;
