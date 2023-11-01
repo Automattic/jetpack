6eaec59d6fb657b8782bc086b3ad3a79d0ca3649
Mikael Korpela <mikael@ihminen.org>
2023-11-01 14:38:28 +0200

    Subscribe modal: enable for Jetpack sites

--- projects/plugins/jetpack/tests/php/modules/subscriptions/test_class.jetpack-subscribe-modal.php
+++ projects/plugins/jetpack/tests/php/modules/subscriptions/test_class.jetpack-subscribe-modal.php
@@ -37,28 +37,6 @@ class WP_Test_Jetpack_Subscribe_Modal extends WP_UnitTestCase {
 	 */
 	public function tear_down() {
 		remove_all_filters( 'jetpack_is_connection_ready' );
-		remove_all_filters( 'jetpack_subscriptions_modal_enabled' );
 		parent::tear_down();
-	}
-
-	/**
-	 * Test that the Jetpack Subscribe Modal does not load by default.
-	 * We are removing a feature flag, but adding a check to load this
-	 * feature only on WordPress.com sites.
-	 */
-	public function test_subscriber_modal_enabled_under_correct_conditions() {
-		// Test that the modal is not enabled by default.
-		$this->assertFalse( apply_filters( 'jetpack_subscriptions_modal_enabled', false ) );
-
-		// Test that the modal is not enabled even when
-		// other needed conditions are met.
-		switch_theme( 'block-theme' );
-		update_option( 'site_intent', 'newsletter' );
-		$this->assertFalse( apply_filters( 'jetpack_subscriptions_modal_enabled', false ) );
-
-		// Test that feature can be enabled by manually adding
-		// feature flag filter.
-		add_filter( 'jetpack_subscriptions_modal_enabled', '__return_true', 11 );
-		$this->assertTrue( apply_filters( 'jetpack_subscriptions_modal_enabled', false ) );
 	}
 }
