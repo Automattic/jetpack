b2c3975e568c110383cccc4ceb2aaee66a7faba4
Jasper Kang <kangzjnet@gmail.com>
2023-11-02 14:29:01 +1300

    rename file

--- projects/packages/search/tests/php/test-get-ios-version.php
+++ projects/packages/search/tests/php/test-get-ios-version.php
@@ -1,7 +1,7 @@
 <?php // phpcs:ignore WordPress.Files.FileName.InvalidClassFileName
 namespace Automattic\Jetpack\Search;
 
-require_once __DIR__ . '/../../compatibility/ios-lt-16.php';
+require_once __DIR__ . '/../../compatibility/ios-15.php';
 
 use Automattic\Jetpack\Search\Test_Case as Search_Test_Case;
 
