c15beae4fcbc46f259e85569a137794ecd0f696c
Adnan Haque <adnan007.id@gmail.com>
2023-11-03 20:09:06 +0600

    Implement back button in react

---
+++ projects/plugins/boost/app/assets/src/js/react-components/common/back-button/BackButton.tsx
@@ -0,0 +1,24 @@
+import { __ } from '@wordpress/i18n';
+import LeftArrow from '../../svg/left-arrow.svg';
+
+type BackButtonProps = {
+	route: string;
+};
+
+const BackButton: React.FC< BackButtonProps > = ( { route = '/' } ) => {
+	const handleBack = () => {
+		window.location.hash = route;
+	};
+
+	return (
+		<button
+			className="components-button aaa components-button--back is-link close"
+			onClick={ handleBack }
+		>
+			<LeftArrow />
+			{ __( 'Go back', 'jetpack-boost' ) }
+		</button>
+	);
+};
+
+export default BackButton;
