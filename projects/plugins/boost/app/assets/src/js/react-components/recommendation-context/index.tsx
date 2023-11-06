24fcb10bc0455729fbf449827f65982f426cd2ed
Peter Petrov <peter.petrov89@gmail.com>
2023-11-06 14:36:34 +0200

    Update recommendation context component to use IconTooltip

--- projects/plugins/boost/app/assets/src/js/react-components/recommendation-context/index.tsx
+++ projects/plugins/boost/app/assets/src/js/react-components/recommendation-context/index.tsx
@@ -1,15 +1,18 @@
-import classNames from 'classnames';
+import { IconTooltip } from '@automattic/jetpack-components';
 import { ImageCdnRecommendation } from '../image-cdn-recommendation';
 import styles from './styles.module.scss';
 
 export const RecommendationContext = () => {
 	return (
-		<div className={ classNames( styles.wrapper, 'jb-score-context' ) }>
-			<span className={ classNames( styles.icon, 'jb-score-context__info-icon' ) }>i</span>
-			<div className={ classNames( styles.container, 'jb-score-context__info-container' ) }>
-				<ImageCdnRecommendation />
-				<i />
-			</div>
-		</div>
+		<IconTooltip
+			title=""
+			placement={ 'bottom-end' }
+			className={ styles.tooltip }
+			iconSize={ 22 }
+			offset={ 20 }
+			wide={ true }
+		>
+			<ImageCdnRecommendation />
+		</IconTooltip>
 	);
 };
