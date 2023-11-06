4a01eb105047a42fa3f23f9a369f57dc1dca0961
Siobhan <siobhan@automattic.com>
2023-11-06 16:12:46 +0000

    Revert "Pass icon as string when registering native block"
    This reverts commit 1fdc69a8c3d93a1fdd04eb09415bc3e3279138d2.

--- projects/js-packages/shared-extension-utils/src/get-block-icon-from-metadata.native.js
+++ projects/js-packages/shared-extension-utils/src/get-block-icon-from-metadata.native.js
@@ -1,5 +1,5 @@
 /* eslint-disable jsdoc/no-undefined-types */
-import { createElement } from '@wordpress/element';
+import { SvgXml } from 'react-native-svg';
 import getIconColor from './get-icon-color';
 
 /**
@@ -10,33 +10,10 @@ import getIconColor from './get-icon-color';
  * dynamically set the icon color later on.
  *
  * @param {object} metadata - Block.json content
- * @returns {React.Component} Icon component
+ * @returns {JSX.Element} Icon component
  */
 export function getBlockIconComponent( metadata ) {
-	// Set default values
-	const attrs = {};
-	let tagName = 'span';
-	let markup = metadata.icon;
-
-	// Convert SVG from string to HTML element
-	const placeholder = document.createElement( 'div' );
-	placeholder.innerHTML = metadata.icon;
-	const svg = placeholder.querySelector( 'svg' );
-
-	// Get SVG attributes and content
-	if ( svg ) {
-		tagName = 'svg';
-		markup = svg.innerHTML;
-
-		Array.from( svg.attributes ).forEach(
-			( { nodeName, nodeValue } ) => ( attrs[ nodeName ] = nodeValue )
-		);
-	}
-
-	return createElement( tagName, {
-		...attrs,
-		dangerouslySetInnerHTML: { __html: markup || '' },
-	} );
+	return <SvgXml xml={ metadata.icon } />;
 }
 
 /**
