ad941767bcd2f62f2ab6e39b0aa3e32bdc086ff4
Damián Suárez <rdsuarez@gmail.com>
2023-11-07 15:19:40 -0600

    clean obsolte code from custom hook

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/hooks/use-ai-feature/index.ts
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/hooks/use-ai-feature/index.ts
@@ -1,14 +1,8 @@
 /**
  * External dependencies
  */
-import apiFetch from '@wordpress/api-fetch';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { useEffect, useState } from '@wordpress/element';
-/**
- * Types & constants
- */
-import { AiFeatureProps } from '../../../../store/wordpress-com/types';
-import type { SiteAIAssistantFeatureEndpointResponseProps } from '../../../../types';
 
 const NUM_FREE_REQUESTS_LIMIT = 20;
 
@@ -32,31 +26,6 @@ export const AI_Assistant_Initial_State = {
 		value: aiAssistantFeature?.[ 'current-tier' ]?.value || 1,
 	},
 };
-
-export async function getAIFeatures(): Promise< AiFeatureProps > {
-	const response: SiteAIAssistantFeatureEndpointResponseProps = await apiFetch( {
-		path: '/wpcom/v2/jetpack-ai/ai-assistant-feature',
-	} );
-
-	return {
-		hasFeature: !! response[ 'has-feature' ],
-		isOverLimit: !! response[ 'is-over-limit' ],
-		requestsCount: response[ 'requests-count' ],
-		requestsLimit: response[ 'requests-limit' ],
-		requireUpgrade: !! response[ 'site-require-upgrade' ],
-		errorMessage: response[ 'error-message' ],
-		errorCode: response[ 'error-code' ],
-		upgradeType: response[ 'upgrade-type' ],
-		usagePeriod: {
-			currentStart: response[ 'usage-period' ]?.[ 'current-start' ],
-			nextStart: response[ 'usage-period' ]?.[ 'next-start' ],
-			requestsCount: response[ 'usage-period' ]?.[ 'requests-count' ] || 0,
-		},
-		currentTier: {
-			value: response[ 'current-tier' ]?.value || 1,
-		},
-	};
-}
 
 export default function useAIFeature() {
 	const [ error ] = useState< Error >( null );
