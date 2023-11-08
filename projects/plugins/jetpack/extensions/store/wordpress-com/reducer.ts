435393d37bb9107f8270b99545ef326de030658c
Damián Suárez <rdsuarez@gmail.com>
2023-11-08 12:12:31 -0600

    AI Assistant: introduce action to increase requests counter (#34019)
    * introduce constants file
    
    * introduce ACTION_INCREASE_AI_ASSISTANT_REQUESTS_COUNT
    
    * introduce a reducer file
    
    * reduce the encrease counter action
    
    * changelog

---
+++ projects/plugins/jetpack/extensions/store/wordpress-com/reducer.ts
@@ -0,0 +1,93 @@
+/**
+ * Types
+ */
+import {
+	ACTION_INCREASE_AI_ASSISTANT_REQUESTS_COUNT,
+	ACTION_REQUEST_AI_ASSISTANT_FEATURE,
+	ACTION_SET_PLANS,
+	ACTION_STORE_AI_ASSISTANT_FEATURE,
+} from './constants';
+import type { PlanStateProps } from './types';
+
+const INITIAL_STATE: PlanStateProps = {
+	plans: [],
+	features: {
+		aiAssistant: {
+			hasFeature: false,
+			isOverLimit: false,
+			requestsCount: 0,
+			requestsLimit: 0,
+			requireUpgrade: false,
+			errorMessage: '',
+			errorCode: '',
+			upgradeType: 'default',
+			currentTier: {
+				value: 1,
+			},
+			usagePeriod: {
+				currentStart: '',
+				nextStart: '',
+				requestsCount: 0,
+			},
+			_meta: {
+				isRequesting: false,
+			},
+		},
+	},
+};
+
+export default function reducer( state = INITIAL_STATE, action ) {
+	switch ( action.type ) {
+		case ACTION_SET_PLANS:
+			return {
+				...state,
+				plans: action.plans,
+			};
+
+		case ACTION_REQUEST_AI_ASSISTANT_FEATURE:
+			return {
+				...state,
+				features: {
+					...state.features,
+					aiAssistant: {
+						...state.features.aiAssistant,
+						_meta: {
+							...state.features.aiAssistant._meta,
+							isRequesting: true,
+						},
+					},
+				},
+			};
+
+		case ACTION_STORE_AI_ASSISTANT_FEATURE: {
+			return {
+				...state,
+				features: {
+					...state.features,
+					aiAssistant: {
+						...action.feature,
+						_meta: {
+							...state.features.aiAssistant._meta,
+							isRequesting: false,
+						},
+					},
+				},
+			};
+		}
+
+		case ACTION_INCREASE_AI_ASSISTANT_REQUESTS_COUNT: {
+			return {
+				...state,
+				features: {
+					...state.features,
+					aiAssistant: {
+						...state.features.aiAssistant,
+						requestsCount: state.features.aiAssistant.requestsCount + action.count,
+					},
+				},
+			};
+		}
+	}
+
+	return state;
+}
