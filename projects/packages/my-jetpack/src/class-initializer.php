5fe117984e71bbcc0890a7e212b64a7e79e34816
Luiz Kowalski <lhkowalski@gmail.com>
2023-11-13 16:48:17 -0300

    Version bump

--- projects/packages/my-jetpack/src/class-initializer.php
+++ projects/packages/my-jetpack/src/class-initializer.php
@@ -32,7 +32,7 @@ class Initializer {
 	 *
 	 * @var string
 	 */
-	const PACKAGE_VERSION = '3.12.1';
+	const PACKAGE_VERSION = '3.12.2-alpha';
 
 	/**
 	 * HTML container ID for the IDC screen on My Jetpack page.
