9a7c412e5860d009663985b0701d2619c4f65ecf
Renan <renansscarvalho@gmail.com>
2023-11-14 15:08:38 +0100

    Allow non-wpcom sites to be suggested

--- projects/plugins/jetpack/extensions/blocks/blogroll/use-get-site-details.js
+++ projects/plugins/jetpack/extensions/blocks/blogroll/use-get-site-details.js
@@ -12,11 +12,20 @@ export default function useGetSiteDetails( { siteURL, subscriptions, enabled = f
 
 	if ( wpcomSite ) {
 		results.unshift( {
-			id: wpcomSite?.ID,
+			blog_id: wpcomSite?.ID,
 			description: wpcomSite?.description,
 			URL: wpcomSite?.URL,
 			site_icon: getSiteIcon( wpcomSite?.logo?.url ),
 			name: wpcomSite?.name,
+		} );
+	} else if ( validDomain && ! isLoading ) {
+		results.unshift( {
+			blog_id: validDomain,
+			description: '',
+			URL: validDomain,
+			site_icon: getSiteIcon( null ),
+			name: validDomain,
+			is_non_wpcom_site: true,
 		} );
 	}
 	return { isLoading, siteDetails: results };
