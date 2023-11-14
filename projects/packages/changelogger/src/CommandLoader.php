13612b1618f4620854d75b7c642df8947b696817
Jeremy Herve <jeremy@jeremy.hu>
2023-11-14 19:35:16 +0100

    General: update our PHP version requirements to PHP 7.0
    Fixes #31714

--- projects/packages/changelogger/src/CommandLoader.php
+++ projects/packages/changelogger/src/CommandLoader.php
@@ -2,7 +2,7 @@
 /**
  * Compatibility stub for Symfony 6 changes.
  *
- * Symfony 6 (for PHP 8.0+) added return type hints to its interface. But we still support PHP 5.6, which doesn't recognize that syntax.
+ * Symfony 6 (for PHP 8.0+) added return type hints to its interface. But we still support PHP 7.0, which doesn't recognize that syntax.
  * Since specifying a return type when the interface doesn't is ok, use the version that always does that for PHP 7+ instead of figuring
  * out how to check the actual symfony version.
  *
@@ -11,8 +11,4 @@
 
 namespace Automattic\Jetpack\Changelogger;
 
-if ( PHP_VERSION_ID >= 70000 ) {
-	class_alias( php7\CommandLoader::class, CommandLoader::class );
-} else {
-	class_alias( php5\CommandLoader::class, CommandLoader::class );
-}
+class_alias( php7\CommandLoader::class, CommandLoader::class );
