bb1b509ebab88c7f355d8ec8611b699b5e171c04
Manzoor Wani <manzoorwani.jk@gmail.com>
2023-11-14 17:23:47 +0530

    Fix post editor dirty state caused by publicize store (#34064)
    * Move the connections state and actions from publicize to social store
    
    * Move selectors from publicize to social store
    
    * Update consumers to use the new store
    
    * Add changelog
    
    * Fix-up versions
    
    * Fix unit tests
    
    * Remove unnecessary async keyword
    
    * Remove unnecessary return statement
    
    * Unify connection profile details selector
    
    * Use constants for connection strings
    
    * Move constants to index file
    
    * Social Store | Add connections unit tests (#34104)
    
    * Add dependency
    
    * Add unit tests for connection selectors
    
    * Add unit tests for connection actions
    
    * Add changelog

--- projects/js-packages/publicize-components/src/social-store/index.js
+++ projects/js-packages/publicize-components/src/social-store/index.js
@@ -19,5 +19,13 @@ export const SOCIAL_STORE_CONFIG = {
 		{},
 };
 
-const store = createReduxStore( SOCIAL_STORE_ID, SOCIAL_STORE_CONFIG );
+export const CONNECTION_SERVICE_FACEBOOK = 'facebook';
+export const CONNECTION_SERVICE_INSTAGRAM_BUSINESS = 'instagram-business';
+export const CONNECTION_SERVICE_LINKEDIN = 'linkedin';
+export const CONNECTION_SERVICE_MASTODON = 'mastodon';
+export const CONNECTION_SERVICE_NEXTDOOR = 'nextdoor';
+export const CONNECTION_SERVICE_TUMBLR = 'tumblr';
+export const CONNECTION_SERVICE_TWITTER = 'twitter';
+
+export const store = createReduxStore( SOCIAL_STORE_ID, SOCIAL_STORE_CONFIG );
 register( store );
