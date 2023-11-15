d9cad0eafe7b4c9e6a13602c015805c8daed4c1f
Jeremy Herve <jeremy@jeremy.hu>
2023-11-15 17:40:40 +0100

    General: update WP version requirements to WordPress 6.3. (#34127)

--- projects/plugins/jetpack/jetpack.php
+++ projects/plugins/jetpack/jetpack.php
@@ -8,7 +8,7 @@
  * Author URI: https://jetpack.com
  * License: GPL2+
  * Text Domain: jetpack
- * Requires at least: 6.2
+ * Requires at least: 6.3
  * Requires PHP: 5.6
  *
  * @package automattic/jetpack
@@ -32,7 +32,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-define( 'JETPACK__MINIMUM_WP_VERSION', '6.2' );
+define( 'JETPACK__MINIMUM_WP_VERSION', '6.3' );
 define( 'JETPACK__MINIMUM_PHP_VERSION', '5.6' );
 define( 'JETPACK__VERSION', '12.9-a.4' );
 
