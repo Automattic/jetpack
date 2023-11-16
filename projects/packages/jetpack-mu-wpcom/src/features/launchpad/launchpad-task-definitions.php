0a6e095e21c55c5665db278ae6cf02a07ca4f9ba
Caroline Moore <calobee@gmail.com>
2023-11-16 14:58:17 +0100

    use add_query_arg

--- projects/packages/jetpack-mu-wpcom/src/features/launchpad/launchpad-task-definitions.php
+++ projects/packages/jetpack-mu-wpcom/src/features/launchpad/launchpad-task-definitions.php
@@ -84,7 +84,7 @@ function wpcom_launchpad_get_task_definitions() {
 
 				// Add a new_prompt query param for Write sites.
 				if ( 'write' === get_option( 'site_intent' ) ) {
-					return $base_path . '/?new_prompt=true';
+					return add_query_arg( 'new_prompt', true, $base_path );
 				}
 
 				return $base_path;
