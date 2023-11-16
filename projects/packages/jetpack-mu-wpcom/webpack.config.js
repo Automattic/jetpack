5900f65894ea89ec5a4dc35dad4bbfeadf144d3f
Dale du Preez <dale@automattic.com>
2023-11-16 11:00:16 +0200

    Add initial JS build code for jetpack-mu-wpcom

---
+++ projects/packages/jetpack-mu-wpcom/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require( 'path' );
+const jetpackConfig = require( '@automattic/jetpack-webpack-config/webpack' );
+
+module.exports = {
+	entry: {
+		'error-reporting': './src/features/error-reporting/index.js',
+	},
+	mode: jetpackConfig.mode,
+	devtool: jetpackConfig.devtool,
+	output: {
+		...jetpackConfig.output,
+		path: path.resolve( __dirname, 'dist' ),
+	},
+	optimization: {
+		...jetpackConfig.optimization,
+	},
+	resolve: {
+		...jetpackConfig.resolve,
+	},
+	node: false,
+	plugins: [ ...jetpackConfig.StandardPlugins() ],
+	module: {
+		strictExportPresence: true,
+		rules: [
+			// Transpile JavaScript.
+			jetpackConfig.TranspileRule( {
+				exclude: /node_modules\//,
+			} ),
+
+			// Transpile @automattic/jetpack-* in node_modules too.
+			jetpackConfig.TranspileRule( {
+				includeNodeModules: [ '@automattic/jetpack-' ],
+			} ),
+
+			// Handle CSS.
+			jetpackConfig.CssRule(),
+
+			// Handle images.
+			jetpackConfig.FileRule(),
+		],
+	},
+};
