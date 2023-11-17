e65b4016e01b41a3793fbd2ae438533adadea087
Luiz Kowalski <lhkowalski@gmail.com>
2023-11-17 18:29:34 -0300

    Change usage panel to use the new usage control interface

--- projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/usage-panel/index.tsx
+++ projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/usage-panel/index.tsx
@@ -14,21 +14,68 @@ import { canUserPurchasePlan } from '../../../../blocks/ai-assistant/lib/connect
 import UsageControl from '../usage-bar';
 import './style.scss';
 
+const useAiFeaturePlanType = currentTier => {
+	if ( currentTier?.value === 0 ) {
+		return 'free';
+	}
+	if ( currentTier?.value === 1 ) {
+		return 'unlimited';
+	}
+
+	return 'tiered';
+};
+
+const useAiFeatureDaysUntilReset = nextStartDate => {
+	// Bail early if we don't have a next start date
+	if ( ! nextStartDate ) {
+		return null;
+	}
+
+	// Build an object from the next start date
+	const parsedNextStartDate = new Date( nextStartDate );
+
+	/*
+	 * Get the miliseconds difference between now and the next start date
+	 * nextStartDate is in UTC, so we need to use UTC methods.
+	 */
+	const differenceInMiliseconds =
+		Date.UTC(
+			parsedNextStartDate.getFullYear(),
+			parsedNextStartDate.getMonth(),
+			parsedNextStartDate.getDate()
+		) - Date.now();
+
+	return Math.floor( differenceInMiliseconds / ( 1000 * 60 * 60 * 24 ) );
+};
+
 export default function UsagePanel() {
 	const { checkoutUrl, autosaveAndRedirect, isRedirecting } = useAICheckout();
 	const canUpgrade = canUserPurchasePlan();
 
 	// fetch usage data
-	const { hasFeature, requestsCount, requestsLimit, isOverLimit } = useAiFeature();
+	const {
+		hasFeature,
+		requestsCount: allTimeRequestsCount,
+		requestsLimit: freeRequestsLimit,
+		isOverLimit,
+		usagePeriod,
+		currentTier,
+	} = useAiFeature();
+	const planType = useAiFeaturePlanType( currentTier );
+	const daysUntilReset = useAiFeatureDaysUntilReset( usagePeriod?.nextStart );
+
+	const requestsCount = planType === 'tiered' ? usagePeriod?.requestsCount : allTimeRequestsCount;
+	const requestsLimit = planType === 'free' ? freeRequestsLimit : currentTier?.limit;
 
 	return (
 		<div className="jetpack-ai-usage-panel">
 			<>
 				<UsageControl
 					isOverLimit={ isOverLimit }
-					hasFeature={ hasFeature }
 					requestsCount={ requestsCount }
 					requestsLimit={ requestsLimit }
+					daysUntilReset={ daysUntilReset }
+					planType={ planType }
 				/>
 
 				{ false && (
