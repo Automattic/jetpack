3b303a4d1a686acbaa4435473fe66b1a4c61292a
arthur791004 <arthur.chu@automattic.com>
2023-11-17 16:15:47 +0900

    Revert Disabling Google Fonts for incompatible themes https://github.com/Automattic/wpcomsh/pull/1568

--- projects/plugins/wpcomsh/feature-plugins/google-fonts.php
+++ projects/plugins/wpcomsh/feature-plugins/google-fonts.php
@@ -27,20 +27,6 @@ if ( ! defined( 'FONT_LIBRARY_DISABLED' ) ) {
 }
 
 /**
- * Jetpack adds a redirect on Jetpack::deactivate_module() to the current page,
- * by adding a filter on wp_redirect which changes all redirect destinations.
- *
- * If we're calling Jetpack::deactivate_module() directly ourselves, we end up
- * breaking future redirects in the request.
- * This function is meant to unhook the filter that JP adds.
- */
-function wpcomsh_jp_module_fix_redirect() {
-	if ( has_filter( 'wp_redirect', 'wp_get_referer' ) ) {
-		remove_filter( 'wp_redirect', 'wp_get_referer' );
-	}
-}
-
-/**
  * Force-enable the Google fonts module
  * If you use a version of Jetpack that supports it,
  * and if it is not already enabled.
@@ -55,33 +41,8 @@ function wpcomsh_activate_google_fonts_module() {
 		return;
 	}
 
-	// Hotfix to google fonts causing issues with Gutenberg 16.9 and specific themes
-	// https://github.com/Automattic/wp-calypso/issues/83986
-	$problematic_themes = array(
-		'course',
-		'pendant',
-		'twentytwentytwo',
-		'smithland',
-		'awburn',
-		'nokul',
-		'ueno',
-	);
-	$incompatible_theme = false;
-	$stylesheet         = get_option( 'stylesheet' );
-	foreach ( $problematic_themes as $problematic_theme ) {
-		if ( strpos( $stylesheet, $problematic_theme ) === 0 ) {
-			$incompatible_theme = true;
-			break;
-		}
-	}
-
-	// Force-activate for safe themes and deactivate for the rest
-	if ( ! Jetpack::is_module_active( 'google-fonts' ) && ! $incompatible_theme ) {
+	if ( ! Jetpack::is_module_active( 'google-fonts' ) ) {
 		Jetpack::activate_module( 'google-fonts', false, false );
-	} elseif ( $incompatible_theme ) {
-		add_action( 'jetpack_pre_deactivate_module', 'wpcomsh_jp_module_fix_redirect', 20, 0 );
-		Jetpack::deactivate_module( 'google-fonts' );
-		remove_action( 'jetpack_pre_deactivate_module', 'wpcomsh_jp_module_fix_redirect', 20, 0 );
 	}
 }
 add_action( 'setup_theme', 'wpcomsh_activate_google_fonts_module' );
