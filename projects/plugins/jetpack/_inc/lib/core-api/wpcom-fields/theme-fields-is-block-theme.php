2bacab152e3443c18ecd12fca6ac2a0527eb93da
Jeremy Herve <jeremy@jeremy.hu>
2023-11-21 10:58:50 +0100

    Remove deprecated file
    See https://github.com/Automattic/jetpack/pull/34210/files#r1399644010

--- projects/plugins/jetpack/_inc/lib/core-api/wpcom-fields/theme-fields-is-block-theme.php
+++
@@ -1,9 +0,0 @@
-<?php // phpcs:ignore WordPress.Files.FileName.InvalidClassFileName
-/**
- * Themes API: add is_block_theme field to the themes endpoint.
- * This is not useful anymore. It is now handled by WP 6.3 directly.
- *
- * @deprecated $$next-version$$
- *
- * @package automattic/jetpack
- */
