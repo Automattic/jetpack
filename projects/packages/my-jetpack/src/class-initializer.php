c4bc9d5d8128864051afed8b0e24b97c4abc2dc3
Kuba Birecki <ice9js@users.noreply.github.com>
2023-11-22 17:24:54 +0100

    Backport changes for VideoPress 1.7 (#34234)

--- projects/packages/my-jetpack/src/class-initializer.php
+++ projects/packages/my-jetpack/src/class-initializer.php
@@ -32,7 +32,7 @@ class Initializer {
 	 *
 	 * @var string
 	 */
-	const PACKAGE_VERSION = '4.0.2-alpha';
+	const PACKAGE_VERSION = '4.0.3-alpha';
 
 	/**
 	 * HTML container ID for the IDC screen on My Jetpack page.
