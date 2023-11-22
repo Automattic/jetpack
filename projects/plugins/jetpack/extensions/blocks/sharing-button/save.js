dd1f1c0e7baa208e0a3436e9202bf0f64f2fbe60
Andrii Lysenko <lysenkoa.work@gmail.com>
2023-11-21 21:40:01 -0800

    update view styles with temporary code

--- projects/plugins/jetpack/extensions/blocks/sharing-button/save.js
+++ projects/plugins/jetpack/extensions/blocks/sharing-button/save.js
@@ -1,11 +1,30 @@
-import ViewSocialButton from './components/view-button';
+import classNames from 'classnames';
+import SocialIcon from 'social-logos';
+
 import './style.scss';
 
 const SharingButtonsView = ( { attributes } ) => {
-	const { service, url } = attributes;
+	const { service } = attributes;
+	const sharingLinkClass = classNames(
+		'jetpack-sharing-button__button',
+		'style_button_replace_at_runtime',
+		'share-' + service
+	);
 	return (
 		<li className="jetpack-sharing-button__list-item">
-			<ViewSocialButton service={ service } url={ url } />
+			<a
+				rel="nofollow noopener noreferrer"
+				className={ sharingLinkClass }
+				href={ 'url_replaced_in_runtime' }
+				target="_blank"
+				data-shared={ 'data-shared_replaced_in_runtime' }
+				primary
+			>
+				<SocialIcon icon={ service } size={ 24 } />
+				<span className="style_button_replace_at_runtime jetpack-sharing-buttons__service-label">
+					{ service }
+				</span>
+			</a>
 		</li>
 	);
 };
--- projects/plugins/jetpack/extensions/blocks/sharing-button/sharing-button.php
+++ projects/plugins/jetpack/extensions/blocks/sharing-button/sharing-button.php
@@ -12,6 +12,8 @@ namespace Automattic\Jetpack\Extensions\Sharing_Button;
 use Automattic\Jetpack\Blocks;
 use Jetpack_Gutenberg;
 
+require_once JETPACK__PLUGIN_DIR . 'modules/sharedaddy/sharing-service.php';
+
 /**
  * Registers the block for use in Gutenberg
  * This is done via an action so that we can disable
@@ -20,7 +22,7 @@ use Jetpack_Gutenberg;
 function register_block() {
 	Blocks::jetpack_register_block(
 		__DIR__,
-		array( 'render_callback' => __NAMESPACE__ . '\load_assets' )
+		array( 'render_callback' => __NAMESPACE__ . '\render_block' )
 	);
 }
 add_action( 'init', __NAMESPACE__ . '\register_block' );
@@ -30,10 +32,19 @@ add_action( 'init', __NAMESPACE__ . '\register_block' );
  *
  * @param array  $attr    Array containing the Sharing Buttons block attributes.
  * @param string $content String containing the Sharing Buttons block content.
+ * @param array  $block Array containing block data.
  *
  * @return string
  */
-function load_assets( $attr, $content ) {
+function render_block( $attr, $content, $block ) {
+	$post_id     = $block->context['postId'];
+	$style_type  = $block->context['styleType'];
+	$style       = 'style-' . $style_type;
+	$data_shared = 'sharing-' . $attr['service'] . '-' . $post_id;
+	$link_url    = get_permalink( $post_id ) . '?share=' . $attr['service'] . '&nb=1';
+	$content     = str_replace( 'url_replaced_in_runtime', $link_url, $content );
+	$content     = str_replace( 'style_button_replace_at_runtime', $style, $content );
+	$content     = str_replace( 'data-shared_replaced_in_runtime', $data_shared, $content );
 	Jetpack_Gutenberg::load_assets_as_required( __DIR__ );
 	return $content;
 }
