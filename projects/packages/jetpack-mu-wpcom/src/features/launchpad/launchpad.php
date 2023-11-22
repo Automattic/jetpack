6bb2505d75fbde880e117df853674e51e7edd399
Valter Lorran <valter.lorran@automattic.com>
2023-11-22 09:34:01 -0300

    Add dynamic titles to task lists

--- projects/packages/jetpack-mu-wpcom/src/features/launchpad/launchpad.php
+++ projects/packages/jetpack-mu-wpcom/src/features/launchpad/launchpad.php
@@ -27,6 +27,9 @@ function wpcom_launchpad_get_task_list_definitions() {
 	$core_task_list_definitions = array(
 		'build'                  => array(
 			'title'               => 'Build',
+			'get_title'           => function () {
+				return __( 'Next steps for your site', 'jetpack-mu-wpcom' );
+			},
 			'task_ids'            => array(
 				'verify_domain_email',
 				'setup_general',
@@ -40,6 +43,9 @@ function wpcom_launchpad_get_task_list_definitions() {
 		),
 		'free'                   => array(
 			'title'               => 'Free',
+			'get_title'           => function () {
+				return __( 'Next steps for your site', 'jetpack-mu-wpcom' );
+			},
 			'task_ids'            => array(
 				'verify_domain_email',
 				'plan_selected',
@@ -54,6 +60,9 @@ function wpcom_launchpad_get_task_list_definitions() {
 		),
 		'link-in-bio'            => array(
 			'title'               => 'Link In Bio',
+			'get_title'           => function () {
+				return __( 'Next steps for your site', 'jetpack-mu-wpcom' );
+			},
 			'task_ids'            => array(
 				'verify_domain_email',
 				'design_selected',
@@ -66,6 +75,9 @@ function wpcom_launchpad_get_task_list_definitions() {
 		),
 		'link-in-bio-tld'        => array(
 			'title'               => 'Link In Bio',
+			'get_title'           => function () {
+				return __( 'Next steps for your site', 'jetpack-mu-wpcom' );
+			},
 			'task_ids'            => array(
 				'verify_domain_email',
 				'design_selected',
@@ -78,6 +90,9 @@ function wpcom_launchpad_get_task_list_definitions() {
 		),
 		'newsletter'             => array(
 			'title'               => 'Newsletter',
+			'get_title'           => function () {
+				return __( 'Next steps for your site', 'jetpack-mu-wpcom' );
+			},
 			'task_ids'            => array(
 				'setup_newsletter',
 				'plan_selected',
@@ -92,6 +107,9 @@ function wpcom_launchpad_get_task_list_definitions() {
 		),
 		'videopress'             => array(
 			'title'               => 'Videopress',
+			'get_title'           => function () {
+				return __( 'Next steps for your site', 'jetpack-mu-wpcom' );
+			},
 			'task_ids'            => array(
 				'verify_domain_email',
 				'videopress_setup',
@@ -103,6 +121,9 @@ function wpcom_launchpad_get_task_list_definitions() {
 		),
 		'write'                  => array(
 			'title'               => 'Write',
+			'get_title'           => function () {
+				return __( 'Next steps for your site', 'jetpack-mu-wpcom' );
+			},
 			'task_ids'            => array(
 				'verify_domain_email',
 				'setup_write',
@@ -115,6 +136,9 @@ function wpcom_launchpad_get_task_list_definitions() {
 		),
 		'start-writing'          => array(
 			'title'               => 'Start Writing',
+			'get_title'           => function () {
+				return __( 'Next steps for your site', 'jetpack-mu-wpcom' );
+			},
 			'task_ids'            => array(
 				'verify_domain_email',
 				'first_post_published',
@@ -127,6 +151,9 @@ function wpcom_launchpad_get_task_list_definitions() {
 		),
 		'design-first'           => array(
 			'title'               => 'Pick a Design',
+			'get_title'           => function () {
+				return __( 'Next steps for your site', 'jetpack-mu-wpcom' );
+			},
 			'task_ids'            => array(
 				'verify_domain_email',
 				'design_completed',
@@ -140,6 +167,9 @@ function wpcom_launchpad_get_task_list_definitions() {
 		),
 		'intent-build'           => array(
 			'title'               => 'Keep Building',
+			'get_title'           => function () {
+				return __( 'Next steps for your site', 'jetpack-mu-wpcom' );
+			},
 			'task_ids'            => array(
 				'site_title',
 				'domain_claim',
@@ -155,6 +185,9 @@ function wpcom_launchpad_get_task_list_definitions() {
 		),
 		'intent-write'           => array(
 			'title'               => 'Blog',
+			'get_title'           => function () {
+				return __( 'Next steps for your site', 'jetpack-mu-wpcom' );
+			},
 			'task_ids'            => array(
 				'site_title',
 				'domain_claim',
@@ -167,6 +200,9 @@ function wpcom_launchpad_get_task_list_definitions() {
 		),
 		'intent-free-newsletter' => array(
 			'title'               => 'Free Newsletter',
+			'get_title'           => function () {
+				return __( 'Next steps for your site', 'jetpack-mu-wpcom' );
+			},
 			'task_ids'            => array(
 				'verify_email',
 				'share_site',
@@ -179,6 +215,9 @@ function wpcom_launchpad_get_task_list_definitions() {
 		),
 		'intent-paid-newsletter' => array(
 			'title'               => 'Paid Newsletter',
+			'get_title'           => function () {
+				return __( 'Next steps for your site', 'jetpack-mu-wpcom' );
+			},
 			'task_ids'            => array(
 				'verify_email',
 				'share_site',
@@ -192,7 +231,6 @@ function wpcom_launchpad_get_task_list_definitions() {
 			'is_enabled_callback' => 'wpcom_launchpad_is_paid_newsletter_enabled',
 		),
 		'earn'                   => array(
-			'title'               => 'Earn',
 			'task_ids'            => array(
 				'stripe_connected',
 				'paid_offer_created',
@@ -200,7 +238,6 @@ function wpcom_launchpad_get_task_list_definitions() {
 			'is_enabled_callback' => '__return_true',
 		),
 		'host-site'              => array(
-			'title'               => 'Hosting Flow',
 			'task_ids'            => array(
 				'site_theme_selected',
 				'install_custom_plugin',
@@ -212,7 +249,6 @@ function wpcom_launchpad_get_task_list_definitions() {
 			'is_enabled_callback' => 'wpcom_launchpad_is_hosting_flow_enabled',
 		),
 		'subscribers'            => array(
-			'title'    => 'Subscribers',
 			'task_ids' => array(
 				'import_subscribers',
 				'add_subscribe_block',
@@ -986,3 +1022,17 @@ function add_launchpad_options_to_jetpack_sync( $allowed_options ) {
 	return array_merge( $allowed_options, $launchpad_options );
 }
 add_filter( 'jetpack_sync_options_whitelist', 'add_launchpad_options_to_jetpack_sync', 10, 1 );
+
+/**
+ * Get the title of a checklist by its slug.
+ *
+ * @param string $checklist_slug The slug of the checklist.
+ * @return string The title of the checklist.
+ */
+function wpcom_get_launchpad_checklist_title_by_checklist_slug( $checklist_slug ) {
+	if ( ! $checklist_slug ) {
+		return array();
+	}
+
+	return wpcom_launchpad_checklists()->get_task_list_title( $checklist_slug );
+}
