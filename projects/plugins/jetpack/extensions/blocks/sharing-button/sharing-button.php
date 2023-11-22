6f87d9484849d4155f5bdb6f939f441711e7fe00
Andrii Lysenko <lysenkoa.work@gmail.com>
2023-11-22 10:59:41 -0800

    improve matching styles

--- projects/plugins/jetpack/extensions/blocks/sharing-button/sharing-button.php
+++ projects/plugins/jetpack/extensions/blocks/sharing-button/sharing-button.php
@@ -12,7 +12,7 @@ namespace Automattic\Jetpack\Extensions\Sharing_Button;
 use Automattic\Jetpack\Blocks;
 use Jetpack_Gutenberg;
 
-require_once JETPACK__PLUGIN_DIR . 'modules/sharedaddy/sharing-service.php';
+require_once JETPACK__PLUGIN_DIR . 'modules/sharedaddy/sharing-sources.php';
 
 /**
  * Registers the block for use in Gutenberg
@@ -37,14 +37,16 @@ add_action( 'init', __NAMESPACE__ . '\register_block' );
  * @return string
  */
 function render_block( $attr, $content, $block ) {
-	$post_id     = $block->context['postId'];
+	$post_id = $block->context['postId'];
+
 	$style_type  = $block->context['styleType'];
 	$style       = 'style-' . $style_type;
-	$data_shared = 'sharing-' . $attr['service'] . '-' . $post_id;
+	$data_shared = 'sharing-' . $attr['service'] . '-' . $post_id . $attr['service'];
 	$link_url    = get_permalink( $post_id ) . '?share=' . $attr['service'] . '&nb=1';
-	$content     = str_replace( 'url_replaced_in_runtime', $link_url, $content );
-	$content     = str_replace( 'style_button_replace_at_runtime', $style, $content );
-	$content     = str_replace( 'data-shared_replaced_in_runtime', $data_shared, $content );
+
+	$content = str_replace( 'url_replaced_in_runtime', $link_url, $content );
+	$content = str_replace( 'style_button_replace_at_runtime', $style, $content );
+	$content = str_replace( 'data-shared_replaced_in_runtime', $data_shared, $content );
 	Jetpack_Gutenberg::load_assets_as_required( __DIR__ );
 	return $content;
 }
