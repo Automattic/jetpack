040ca55c9740381f4ed6ae3edea4385982d9d798
Brad Jorsch <anomiex@users.noreply.github.com>
2023-11-22 13:05:55 -0500

    Clean up output from various tests (#34256)
    Tests shouldn't produce extraneous output to the console, but some of
    ours were.
    
    * packages/blaze: Prints URLs due to not passing `false` for the
      `$display` parameter to `menu_page_url()`.
    * packages/connection: `error_log()` output, which we can redirect to
      /dev/null.
    * packages/forms: In CI it prints a bunch of "sh: 1: /usr/sbin/sendmail:
      not found", while locally it doesn't because that exists and it
      **actually sends mail** (to an example.com address, at least, which
      has no MX and so is undeliverable). The 'pre_wp_mail' filter easily
      prevents the sending of mail.
    * plugins/jetpack: Output from sync, which we can expect as part of the
      test.

--- projects/packages/connection/src/class-package-version.php
+++ projects/packages/connection/src/class-package-version.php
@@ -12,7 +12,7 @@ namespace Automattic\Jetpack\Connection;
  */
 class Package_Version {
 
-	const PACKAGE_VERSION = '2.0.2';
+	const PACKAGE_VERSION = '2.0.3-alpha';
 
 	const PACKAGE_SLUG = 'connection';
 
