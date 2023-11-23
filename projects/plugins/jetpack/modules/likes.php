6e5f218dfd101898577a01cd2ee3de943fdf1253
Tim Broddin <tim@wannabes.be>
2023-11-23 10:01:19 +0100

    Update likes.php
    Change n=1 to ver=2

--- projects/plugins/jetpack/modules/likes.php
+++ projects/plugins/jetpack/modules/likes.php
@@ -433,7 +433,7 @@ class Jetpack_Likes {
 		* we need a slightly more unique id / name for the widget wrapper.
 		*/
 		$uniqid     = uniqid();
-		$new_layout = apply_filters( 'likes_new_layout', false ) ? '&amp;n=1' : '';
+		$new_layout = apply_filters( 'likes_new_layout', false ) ? '&amp;ver=2' : '';
 
 		$src      = sprintf( 'https://widgets.wp.com/likes/#blog_id=%1$d&amp;post_id=%2$d&amp;origin=%3$s&amp;obj_id=%1$d-%2$d-%4$s%5$s', $blog_id, $post_id, $domain, $uniqid, $new_layout );
 		$name     = sprintf( 'like-post-frame-%1$d-%2$d-%3$s', $blog_id, $post_id, $uniqid );
