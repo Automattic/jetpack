044f74a8a5daef2a8aada1874041c9dab99d6d51
Sergey Mitroshin <sergeymitr@gmail.com>
2023-11-22 19:58:39 -0500

    Add changelog files and version bumps.

--- projects/packages/identity-crisis/src/class-identity-crisis.php
+++ projects/packages/identity-crisis/src/class-identity-crisis.php
@@ -27,7 +27,7 @@ class Identity_Crisis {
 	/**
 	 * Package Version
 	 */
-	const PACKAGE_VERSION = '0.12.1-alpha';
+	const PACKAGE_VERSION = '0.13.0-alpha';
 
 	/**
 	 * Persistent WPCOM blog ID that stays in the options after disconnect.
