3af9df457b0e95d7facce42d58c7a5e4936786f8
Adnan Haque <adnan007.id@gmail.com>
2023-11-23 11:24:01 +0600

    Move state to a separate file

---
+++ projects/plugins/boost/app/assets/src/js/features/performance-history/lib/stores/store.ts
@@ -0,0 +1,46 @@
+import { z } from 'zod';
+import { useDataSync } from '@automattic/jetpack-react-data-sync-client';
+
+const performanceHistoryDataSchema = z.object( {
+	periods: z.array(
+		z.object( {
+			timestamp: z.number(),
+			dimensions: z.object( {
+				desktop_overall_score: z.number(),
+				mobile_overall_score: z.number(),
+				desktop_cls: z.number(),
+				desktop_lcp: z.number(),
+				desktop_tbt: z.number(),
+				mobile_cls: z.number(),
+				mobile_lcp: z.number(),
+				mobile_tbt: z.number(),
+			} ),
+		} )
+	),
+	startDate: z.number(),
+	endDate: z.number(),
+} );
+
+export const usePerformanceHistoryQuery = () => {
+	const { useQuery } = useDataSync(
+		'jetpack_boost_ds',
+		'performance_history',
+		performanceHistoryDataSchema
+	);
+	return useQuery();
+};
+
+/**
+ * A custom hook to handle performance history panel being open or closed.
+ */
+export const usePerformanceHistoryPanelQuery = () => {
+	const { useQuery, useMutation } = useDataSync(
+		'jetpack_boost_ds',
+		'performance_history_toggle',
+		z.boolean()
+	);
+	const { data } = useQuery();
+	const { mutate } = useMutation();
+
+	return [ data, mutate ] as const;
+};
--- projects/plugins/boost/app/assets/src/js/features/performance-history/performance-history.tsx
+++ projects/plugins/boost/app/assets/src/js/features/performance-history/performance-history.tsx
@@ -1,5 +1,5 @@
-import { DataSyncProvider, useDataSync } from '@automattic/jetpack-react-data-sync-client';
-import { z } from 'zod';
+import { DataSyncProvider } from '@automattic/jetpack-react-data-sync-client';
+import { usePerformanceHistoryPanelQuery, usePerformanceHistoryQuery } from './lib/stores/store';
 import GraphComponent from './graph-component/graph-component';
 import classNames from 'classnames';
 import ErrorNotice from '$features/error-notice/error-notice';
@@ -8,50 +8,6 @@ import { Panel, PanelBody, PanelRow } from '@wordpress/components';
 import { navigate } from '$lib/utils/navigate';
 
 import styles from './performance-history.module.scss';
-
-const performanceHistoryDataSchema = z.object( {
-	periods: z.array(
-		z.object( {
-			timestamp: z.number(),
-			dimensions: z.object( {
-				desktop_overall_score: z.number(),
-				mobile_overall_score: z.number(),
-				desktop_cls: z.number(),
-				desktop_lcp: z.number(),
-				desktop_tbt: z.number(),
-				mobile_cls: z.number(),
-				mobile_lcp: z.number(),
-				mobile_tbt: z.number(),
-			} ),
-		} )
-	),
-	startDate: z.number(),
-	endDate: z.number(),
-} );
-
-const usePerformanceHistoryQuery = () => {
-	const { useQuery } = useDataSync(
-		'jetpack_boost_ds',
-		'performance_history',
-		performanceHistoryDataSchema
-	);
-	return useQuery();
-};
-
-/**
- * A custom hook to check if performance history needs upgrade.
- */
-const usePerformanceHistoryPanelQuery = () => {
-	const { useQuery, useMutation } = useDataSync(
-		'jetpack_boost_ds',
-		'performance_history_toggle',
-		z.boolean()
-	);
-	const { data } = useQuery();
-	const { mutate } = useMutation();
-
-	return [ data, mutate ] as const;
-};
 
 const PerformanceHistory = ( { needsUpgrade, isFreshStart, onDismissFreshStart } ) => {
 	const {
