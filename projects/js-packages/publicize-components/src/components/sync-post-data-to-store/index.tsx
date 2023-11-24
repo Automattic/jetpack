0e69194cf792a950aaf73d961d3b107a7e6bfd6b
Manzoor Wani <manzoorwani.jk@gmail.com>
2023-11-24 16:00:52 +0530

    Create SyncPostDataToStore to render the hook

---
+++ projects/js-packages/publicize-components/src/components/sync-post-data-to-store/index.tsx
@@ -0,0 +1,8 @@
+import { useSyncPostDataToStore } from './use-sync-post-data-to-store';
+
+/**
+ * Renders the hooks to sync post data to the social store.
+ */
+export function SyncPostDataToStore() {
+	useSyncPostDataToStore();
+}
