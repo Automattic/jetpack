6850e6aa1a2e7821979a281856752ed5f1710f99
Manzoor Wani <manzoorwani.jk@gmail.com>
2023-11-24 14:17:53 +0530

    Create useSyncPostDataToStore hook

--- projects/js-packages/publicize-components/src/utils/index.js
+++ projects/js-packages/publicize-components/src/utils/index.js
@@ -2,3 +2,4 @@ export * from './get-share-message-max-length';
 export * from './get-supported-additional-connections';
 export * from './types';
 export * from './use-post-meta';
+export * from './use-sync-post-data-to-store';
