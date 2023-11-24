658be6c47d30b74ef217c20f253bde5814218790
Manzoor Wani <manzoorwani.jk@gmail.com>
2023-11-24 18:00:38 +0530

    Replace core select with useSelect

--- projects/js-packages/publicize-components/src/components/sync-post-data-to-store/use-sync-post-data-to-store.js
+++ projects/js-packages/publicize-components/src/components/sync-post-data-to-store/use-sync-post-data-to-store.js
@@ -1,4 +1,4 @@
-import { select as coreSelect, useDispatch } from '@wordpress/data';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { store as editorStore } from '@wordpress/editor';
 import { usePostJustPublished } from '../../hooks/use-saving-post';
 import { store as socialStore } from '../../social-store';
@@ -9,24 +9,24 @@ import { store as socialStore } from '../../social-store';
 export function useSyncPostDataToStore() {
 	const { mergeConnections } = useDispatch( socialStore );
 
+	const { didPostSaveRequestSucceed, isCurrentPostPublished, getEditedPostAttribute } = useSelect(
+		select => select( editorStore ),
+		[]
+	);
+
 	usePostJustPublished( () => {
 		/**
 		 * We need to update the connections only when the post is published
 		 * and the save request is successful
 		 * This is because the connections are updated only when the post is published
 		 */
-		if (
-			coreSelect( editorStore ).didPostSaveRequestSucceed() &&
-			coreSelect( editorStore ).isCurrentPostPublished()
-		) {
+		if ( didPostSaveRequestSucceed() && isCurrentPostPublished() ) {
 			// get the fresh connections from the store
-			const freshConnections = coreSelect( editorStore ).getEditedPostAttribute(
-				'jetpack_publicize_connections'
-			);
+			const freshConnections = getEditedPostAttribute( 'jetpack_publicize_connections' );
 			// if the connections have changed, merge them into the social store
 			if ( freshConnections?.length ) {
 				mergeConnections( freshConnections );
 			}
 		}
-	} );
+	}, [ didPostSaveRequestSucceed, isCurrentPostPublished, getEditedPostAttribute ] );
 }
