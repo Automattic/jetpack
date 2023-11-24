0142818ec1fdd56096fcc85f8faa0171b841f121
Linas Valiukas <linas.valiukas@automattic.com>
2023-11-24 18:29:56 +0200

    Fix import

--- projects/packages/transport-helper/actions.php
+++ projects/packages/transport-helper/actions.php
@@ -5,10 +5,10 @@
  * @package automattic/jetpack-transport-helper
  */
 
-use \Automattic\Jetpack\Transport_Helper;
-
 // If WordPress's plugin API is available already, use it. If not,
 // drop data into `$wp_filter` for `WP_Hook::build_preinitialized_hooks()`.
+use Automattic\Jetpack\Transport_Helper\Helper_Script_Manager;
+
 if ( function_exists( 'add_filter' ) ) {
 	$add_filter = 'add_filter';
 	$add_action = 'add_action';
