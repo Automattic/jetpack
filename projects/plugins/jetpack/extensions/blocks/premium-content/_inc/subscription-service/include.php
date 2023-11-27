ee8558391394e719537b1f9582b34f4c040f818a
Chris McCluskey <chris.mccluskey@automattic.com>
2023-11-27 13:21:13 -0800

    Remove deleted file include

--- projects/plugins/jetpack/extensions/blocks/premium-content/_inc/subscription-service/include.php
+++ projects/plugins/jetpack/extensions/blocks/premium-content/_inc/subscription-service/include.php
@@ -11,10 +11,9 @@ require_once __DIR__ . '/class-jwt.php';
 require_once __DIR__ . '/class-subscription-service.php';
 require_once __DIR__ . '/class-token-subscription.php';
 require_once __DIR__ . '/class-token-subscription-service.php';
-require_once __DIR__ . '/class-wpcom-token-subscription-service.php';
+require_once __DIR__ . '/class-jetpack-token-subscription-service.php';
 require_once __DIR__ . '/class-wpcom-online-subscription-service.php';
 require_once __DIR__ . '/class-wpcom-offline-subscription-service.php';
-require_once __DIR__ . '/class-jetpack-token-subscription-service.php';
 require_once __DIR__ . '/class-unconfigured-subscription-service.php';
 
 use Automattic\Jetpack\Extensions\Premium_Content\Subscription_Service\Jetpack_Token_Subscription_Service;
