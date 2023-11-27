cf6d2bee6f48f16a8b2c59ff1f3c7f2e476dab55
Jeremy Herve <jeremy@jeremy.hu>
2023-11-27 20:03:27 +0100

    Remove existing functions now extracted

--- projects/github-actions/repo-gardening/src/tasks/reply-to-customers-reminder/index.js
+++ projects/github-actions/repo-gardening/src/tasks/reply-to-customers-reminder/index.js
@@ -23,35 +23,6 @@ async function hasHighPriorityLabel( octokit, owner, repo, number ) {
 }
 
 /**
- * Check if the issue has a comment with a list of support references,
- * and at least x support references listed there.
- * (x is specified with reply_to_customers_threshold input, default to 10).
- * We only count the number of unanswered support references, since they're the ones we'll need to contact.
- *
- * @param {Array} issueComments - Array of all comments on that issue.
- * @returns {Promise<boolean>} Promise resolving to boolean.
- */
-async function hasManySupportReferences( issueComments ) {
-	const referencesThreshhold = getInput( 'reply_to_customers_threshold' );
-
-	let isWidelySpreadIssue = false;
-	issueComments.map( comment => {
-		if (
-			comment.user.login === 'github-actions[bot]' &&
-			comment.body.includes( '**Support References**' )
-		) {
-			// Count the number of to-do items in the comment.
-			const countReferences = comment.body.split( '- [ ] ' ).length - 1;
-			if ( countReferences >= parseInt( referencesThreshhold ) ) {
-				isWidelySpreadIssue = true;
-			}
-		}
-	} );
-
-	return isWidelySpreadIssue;
-}
-
-/**
  * Build an object containing the slack message and its formatting to send to Slack.
  *
  * @param {WebhookPayloadIssue} payload - Issue event payload.
--- projects/github-actions/repo-gardening/src/tasks/update-board/index.js
+++ projects/github-actions/repo-gardening/src/tasks/update-board/index.js
@@ -9,28 +9,6 @@ const { automatticAssignments } = require( './automattic-label-team-assignments'
 /* global GitHub, WebhookPayloadIssue */
 
 /**
- * Check for Priority labels on an issue.
- * It could be existing labels,
- * or it could be that it's being added as part of the event that triggers this action.
- *
- * @param {GitHub} octokit    - Initialized Octokit REST client.
- * @param {string} owner      - Repository owner.
- * @param {string} repo       - Repository name.
- * @param {string} number     - Issue number.
- * @param {string} action     - Action that triggered the event ('opened', 'reopened', 'labeled').
- * @param {object} eventLabel - Label that was added to the issue.
- * @returns {Promise<Array>} Promise resolving to an array of Priority labels.
- */
-async function hasPriorityLabels( octokit, owner, repo, number, action, eventLabel ) {
-	const labels = await getLabels( octokit, owner, repo, number );
-	if ( 'labeled' === action && eventLabel.name && eventLabel.name.match( /^\[Pri\].*$/ ) ) {
-		labels.push( eventLabel.name );
-	}
-
-	return labels.filter( label => label.match( /^\[Pri\].*$/ ) && label !== '[Pri] TBD' );
-}
-
-/**
  * Check if an issue has a "Triaged" label.
  * It could be an existing label,
  * or it could be that it's being added as part of the event that triggers this action.
@@ -76,32 +54,6 @@ async function needsThirdPartyFix( octokit, owner, repo, number, action, eventLa
 	}
 
 	return labels.some( label => label.match( /^\[Status\] Needs (3rd Party|Core) Fix$/ ) );
-}
-
-/**
- * Ensure the issue is a bug, by looking for a "[Type] Bug" label.
- * It could be an existing label,
- * or it could be that it's being added as part of the event that triggers this action.
- *
- * @param {GitHub} octokit    - Initialized Octokit REST client.
- * @param {string} owner      - Repository owner.
- * @param {string} repo       - Repository name.
- * @param {string} number     - Issue number.
- * @param {string} action     - Action that triggered the event ('opened', 'reopened', 'labeled').
- * @param {object} eventLabel - Label that was added to the issue.
- * @returns {Promise<boolean>} Promise resolving to boolean.
- */
-async function isBug( octokit, owner, repo, number, action, eventLabel ) {
-	// If the issue has a "[Type] Bug" label, it's a bug.
-	const labels = await getLabels( octokit, owner, repo, number );
-	if ( labels.includes( '[Type] Bug' ) ) {
-		return true;
-	}
-
-	// Next, check if the current event was a [Type] Bug label being added.
-	if ( 'labeled' === action && eventLabel.name && '[Type] Bug' === eventLabel.name ) {
-		return true;
-	}
 }
 
 /**
