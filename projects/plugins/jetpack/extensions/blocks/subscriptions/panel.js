e595415f2aee537f81545dd94469f92591ae4850
Mikael Korpela <mikael@ihminen.org>
2023-11-28 09:59:35 +0200

    Correct tenses

--- projects/plugins/jetpack/extensions/blocks/subscriptions/panel.js
+++ projects/plugins/jetpack/extensions/blocks/subscriptions/panel.js
@@ -197,7 +197,7 @@ function getNumberOfSubscribersText( {
 			return sprintf(
 				// translators: %1s is the post name, %2s is the list of categories with subscriptions count
 				__(
-					'<postPublishedLink>%1$s</postPublishedLink> was sent to everyone subscribed to %2$s.',
+					'<postPublishedLink>%1$s</postPublishedLink> will be sent to everyone subscribed to %2$s.',
 					'jetpack'
 				),
 				postName,
@@ -207,7 +207,7 @@ function getNumberOfSubscribersText( {
 			return sprintf(
 				// translators: %1s is the post name, %2s is the list of categories with subscriptions count
 				__(
-					'<postPublishedLink>%1$s</postPublishedLink> will be sent to everyone subscribed to %2$s.',
+					'<postPublishedLink>%1$s</postPublishedLink>was sent to everyone subscribed to %2$s.',
 					'jetpack'
 				),
 				postName,
