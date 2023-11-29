aadd10dc1f6e1e7f9e971b66925742a9cd2f8edf
Mikael Korpela <mikael@ihminen.org>
2023-11-29 17:53:56 +0100

    Simplify toggles
    See https://github.com/Automattic/jetpack/pull/34358#issuecomment-1831937883

--- projects/plugins/jetpack/_inc/client/newsletter/subscriptions-settings.jsx
+++ projects/plugins/jetpack/_inc/client/newsletter/subscriptions-settings.jsx
@@ -91,6 +91,9 @@ function SubscriptionsSettings( props ) {
 		);
 	};
 
+	const isDisabled =
+		! isSubscriptionsActive || unavailableInOfflineMode || isSavingAnyOption( [ 'subscriptions' ] );
+
 	return (
 		<SettingsCard { ...props } hideButton module="subscriptions">
 			<SettingsGroup
@@ -119,11 +122,7 @@ function SubscriptionsSettings( props ) {
 					<FormFieldset>
 						<ToggleControl
 							checked={ isSubscriptionsActive && isStbEnabled }
-							disabled={
-								! isSubscriptionsActive ||
-								unavailableInOfflineMode ||
-								isSavingAnyOption( [ 'subscriptions' ] )
-							}
+							disabled={ isDisabled }
 							toggling={ isSavingAnyOption( [ 'stb_enabled' ] ) }
 							onChange={ handleSubscribeToBlogToggleChange }
 							label={ __(
@@ -133,11 +132,7 @@ function SubscriptionsSettings( props ) {
 						/>
 						<ToggleControl
 							checked={ isSubscriptionsActive && isStcEnabled }
-							disabled={
-								! isSubscriptionsActive ||
-								unavailableInOfflineMode ||
-								isSavingAnyOption( [ 'subscriptions' ] )
-							}
+							disabled={ isDisabled }
 							toggling={ isSavingAnyOption( [ 'stc_enabled' ] ) }
 							onChange={ handleSubscribeToCommentToggleChange }
 							label={ __(
@@ -147,11 +142,7 @@ function SubscriptionsSettings( props ) {
 						/>
 						<ToggleControl
 							checked={ isSubscriptionsActive && isSmEnabled }
-							disabled={
-								! isSubscriptionsActive ||
-								unavailableInOfflineMode ||
-								isSavingAnyOption( [ 'subscriptions' ] )
-							}
+							disabled={ isDisabled }
 							toggling={ isSavingAnyOption( [ 'sm_enabled' ] ) }
 							onChange={ handleSubscribeModalToggleChange }
 							label={ __( 'Enable subscriber pop-up', 'jetpack' ) }
