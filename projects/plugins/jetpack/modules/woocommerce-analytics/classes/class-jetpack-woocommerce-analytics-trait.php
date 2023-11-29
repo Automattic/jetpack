efe72766eed0a23939330a8af187c4bd844d21b8
Thomas Roberts <thomas.roberts@automattic.com>
2023-11-29 12:13:23 +0000

    Add cart_content_source class prop

--- projects/plugins/jetpack/modules/woocommerce-analytics/classes/class-jetpack-woocommerce-analytics-trait.php
+++ projects/plugins/jetpack/modules/woocommerce-analytics/classes/class-jetpack-woocommerce-analytics-trait.php
@@ -27,6 +27,13 @@ trait Jetpack_WooCommerce_Analytics_Trait {
 	protected $cart_checkout_templates_in_use;
 
 	/**
+	 * The content of the cart page or where the cart page is ultimately derived from if using a template.
+	 *
+	 * @var string
+	 */
+	protected $cart_content_source;
+
+	/**
 	 * Default event properties which should be included with all events.
 	 *
 	 * @return array Array of standard event props.
