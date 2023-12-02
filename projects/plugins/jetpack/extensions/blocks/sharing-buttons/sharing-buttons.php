3cd30856266c8b99959b74e15448533a493b70e5
Andrii Lysenko <lysenkoa.work@gmail.com>
2023-12-01 17:23:36 -0800

    update: move open window script to the view.js file

--- projects/plugins/jetpack/extensions/blocks/sharing-buttons/sharing-buttons.php
+++ projects/plugins/jetpack/extensions/blocks/sharing-buttons/sharing-buttons.php
@@ -10,81 +10,12 @@
 namespace Automattic\Jetpack\Extensions\Sharing_Buttons;
 
 use Automattic\Jetpack\Blocks;
-use Jetpack_Gutenberg;
 /**
  * Registers the block for use in Gutenberg
  * This is done via an action so that we can disable
  * registration if we need to.
  */
 function register_block() {
-	Blocks::jetpack_register_block(
-		__DIR__,
-		array( 'render_callback' => __NAMESPACE__ . '\load_assets' )
-	);
+	Blocks::jetpack_register_block( __DIR__ );
 }
 add_action( 'init', __NAMESPACE__ . '\register_block' );
-
-/**
- * Sharing Buttons block registration/dependency declaration.
- *
- * @param array  $attr    Array containing the Sharing Buttons block attributes.
- * @param string $content String containing the Sharing Buttons block content.
- *
- * @return string
- */
-function load_assets( $attr, $content ) {
-	/*
-	 * Enqueue necessary scripts and styles.
-	 */
-	Jetpack_Gutenberg::load_assets_as_required( __DIR__ );
-	return $content;
-}
-
-/**
- * Add sharing JavaScript to the footer of a page.
- *
- * @return void
- */
-function sharing_add_footer() {
-	?>
-	<script>
-	const variations = ['print', 'facebook', 'linkedin', 'mail', 'mastodon', 'patreon', 'pinterest', 'pocket', 'reddit', 'skype', 'telegram', 'tumblr', 'twitch', 'whatsapp', 'x', 'nextdoor']
-	let windowOpen;
-
-	( function () {
-		function matches( el, sel ) {
-			if ( ! el ) {
-				return false;
-			}
-			return !! (
-				( el.matches && el.matches( sel ) ) ||
-				( el.msMatchesSelector && el.msMatchesSelector( sel ) )
-			);
-		}
-		variations.forEach(variation => {
-			document.querySelectorAll(`a.share-${variation}`).forEach(link => {
-				link.addEventListener('click', event => {
-					event.preventDefault();
-
-					const el = event.target.closest(`a.share-${variation}`);
-					if (!el) return;
-
-					if (windowOpen !== undefined) {
-						windowOpen.close();
-					}
-
-					const options = 'enubar=1,resizable=1,width=600,height=400';
-					windowOpen = window.open(el.getAttribute('href'), `wpcom${variation}`, options);
-					if (windowOpen) {
-						windowOpen.focus();
-					}
-				});
-			});
-		});
-	} )();
-
-	</script>
-	<?php
-}
-
-add_action( 'wp_footer', __NAMESPACE__ . '\sharing_add_footer' );
--- projects/plugins/jetpack/extensions/blocks/sharing-buttons/view.js
+++ projects/plugins/jetpack/extensions/blocks/sharing-buttons/view.js
@@ -1,1 +1,30 @@
-import './style.scss';
+//import './style.scss';
+import variations from '../sharing-button/variations.js';
+
+const services = variations.map( variation => variation.name );
+let windowOpen;
+
+( function () {
+	services.forEach( service => {
+		document.querySelectorAll( `a.share-${ service }` ).forEach( link => {
+			link.addEventListener( 'click', event => {
+				event.preventDefault();
+
+				const el = event.target.closest( `a.share-${ service }` );
+				if ( ! el ) {
+					return;
+				}
+
+				if ( windowOpen !== undefined ) {
+					windowOpen.close();
+				}
+
+				const options = 'enubar=1,resizable=1,width=600,height=400';
+				windowOpen = window.open( el.getAttribute( 'href' ), `wpcom${ service }`, options );
+				if ( windowOpen ) {
+					windowOpen.focus();
+				}
+			} );
+		} );
+	} );
+} )();
