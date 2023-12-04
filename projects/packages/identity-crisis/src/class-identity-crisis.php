3045abd1bade575f0b2d1952dc1ec07c122e1839
bindlegirl <1242807+bindlegirl@users.noreply.github.com>
2023-12-04 19:59:19 +0100

    Fixing versions

--- projects/packages/identity-crisis/src/class-identity-crisis.php
+++ projects/packages/identity-crisis/src/class-identity-crisis.php
@@ -27,7 +27,7 @@ class Identity_Crisis {
 	/**
 	 * Package Version
 	 */
-	const PACKAGE_VERSION = '0.13.1-alpha';
+	const PACKAGE_VERSION = '0.14.0-alpha';
 
 	/**
 	 * Persistent WPCOM blog ID that stays in the options after disconnect.
