bd4f27cba0f0e67e8bae797dedf9b9412130d535
Igor Zinovyev <zinigor@gmail.com>
2023-12-05 11:26:00 +0300

    Generated project skeleton.

---
+++ projects/packages/doc-parser/src/class-doc-parser.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Package description here
+ *
+ * @package automattic/jetpack-doc-parser
+ */
+
+namespace Automattic\Jetpack;
+
+/**
+ * Class description.
+ */
+class Doc_Parser {
+
+	const PACKAGE_VERSION = '1.0.0-alpha';
+}
