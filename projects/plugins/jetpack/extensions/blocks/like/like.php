ca18c7d2d7e7fef74e25f9a13a14a138b69d7270
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2023-12-05 15:21:31 +0100

    Add Simple sites support

--- projects/plugins/jetpack/extensions/blocks/like/like.php
+++ projects/plugins/jetpack/extensions/blocks/like/like.php
@@ -45,18 +45,10 @@ function render_block( $attr, $content, $block ) {
 
 	$html = '';
 
-	if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
-		$blog_id = get_current_blog_id();
-	} else {
-		$blog_id   = \Jetpack_Options::get_option( 'id' );
-		$url       = home_url();
-		$url_parts = wp_parse_url( $url );
-		$domain    = $url_parts['host'];
-	}
-
+	$uniqid  = uniqid();
 	$post_id = $block->context['postId'];
+	$title   = esc_html__( 'Like or Reblog', 'jetpack' );
 
-	$uniqid = uniqid();
 	/**
 	 * Enable an alternate Likes layout.
 	 *
@@ -68,16 +60,33 @@ function render_block( $attr, $content, $block ) {
 	 */
 	$new_layout = apply_filters( 'likes_new_layout', true ) ? '&amp;n=1' : '';
 
-	$src      = sprintf( 'https://widgets.wp.com/likes/#blog_id=%1$d&amp;post_id=%2$d&amp;origin=%3$s&amp;obj_id=%1$d-%2$d-%4$s%5$s', $blog_id, $post_id, $domain, $uniqid, $new_layout );
-	$name     = sprintf( 'like-post-frame-%1$d-%2$d-%3$s', $blog_id, $post_id, $uniqid );
-	$wrapper  = sprintf( 'like-post-wrapper-%1$d-%2$d-%3$s', $blog_id, $post_id, $uniqid );
-	$headline = sprintf(
-		/** This filter is already documented in modules/sharedaddy/sharing-service.php */
-		apply_filters( 'jetpack_sharing_headline_html', '<h3 class="sd-title">%s</h3>', esc_html__( 'Like this:', 'jetpack' ), 'likes' ),
-		esc_html__( 'Like this:', 'jetpack' )
-	);
+	if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
+		$blog_id  = get_current_blog_id();
+		$bloginfo = get_blog_details( (int) $blog_id );
+		$domain   = $bloginfo->domain;
+		$version  = '20231201';
+		$src      = sprintf( '//widgets.wp.com/likes/index.html?ver=%1$d#blog_id=%2$d&amp;post_id=%3$d&amp;origin=%4$s&amp;obj_id=%2$d-%3$d-%5$s%6$s', $version, $blog_id, $post_id, $domain, $uniqid, $new_layout );
 
-	$title = esc_html__( 'Like or Reblog', 'jetpack' );
+		// provide the mapped domain when needed
+		if ( isset( $_SERVER['HTTP_HOST'] ) && strpos( sanitize_text_field( wp_unslash( $_SERVER['HTTP_HOST'] ) ), '.wordpress.com' ) === false ) {
+			$sanitized_host = filter_var( wp_unslash( $_SERVER['HTTP_HOST'] ), FILTER_SANITIZE_URL );
+			$src           .= '&amp;domain=' . rawurlencode( $sanitized_host );
+		}
+	} else {
+		$blog_id   = \Jetpack_Options::get_option( 'id' );
+		$url       = home_url();
+		$url_parts = wp_parse_url( $url );
+		$domain    = $url_parts['host'];
+		$src       = sprintf( 'https://widgets.wp.com/likes/#blog_id=%1$d&amp;post_id=%2$d&amp;origin=%3$s&amp;obj_id=%1$d-%2$d-%4$s%5$s', $blog_id, $post_id, $domain, $uniqid, $new_layout );
+		$headline  = sprintf(
+			/** This filter is already documented in modules/sharedaddy/sharing-service.php */
+			apply_filters( 'jetpack_sharing_headline_html', '<h3 class="sd-title">%s</h3>', esc_html__( 'Like this:', 'jetpack' ), 'likes' ),
+			esc_html__( 'Like this:', 'jetpack' )
+		);
+	}
+
+	$name    = sprintf( 'like-post-frame-%1$d-%2$d-%3$s', $blog_id, $post_id, $uniqid );
+	$wrapper = sprintf( 'like-post-wrapper-%1$d-%2$d-%3$s', $blog_id, $post_id, $uniqid );
 
 	$html  = "<div class='sharedaddy sd-block sd-like jetpack-likes-widget-wrapper jetpack-likes-widget-unloaded' id='$wrapper' data-src='$src' data-name='$name' data-title='$title'>";
 	$html .= $headline;
