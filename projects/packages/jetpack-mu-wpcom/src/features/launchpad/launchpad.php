bb6c71e02995ecc8fa5f0dd4ef246555e776b2b2
Juan Lucha <juanlucha@gmail.com>
2023-12-05 17:59:18 +0100

    Fix/css changes like avatar (#34440)
    * Removed like avatar border inside popup
    
    * changelog

--- projects/packages/jetpack-mu-wpcom/src/features/launchpad/launchpad.php
+++ projects/packages/jetpack-mu-wpcom/src/features/launchpad/launchpad.php
@@ -258,6 +258,18 @@ function wpcom_launchpad_get_task_list_definitions() {
 			),
 			'is_enabled_callback' => 'wpcom_launchpad_get_fullscreen_enabled',
 		),
+		'legacy-site-setup'      => array(
+			'task_ids' => array(
+				'blogname_set',
+				'front_page_updated',
+				'verify_domain_email',
+				'verify_email',
+				'mobile_app_installed',
+				'setup_professional_email',
+				'post_sharing_enabled',
+				'site_launched',
+			),
+		),
 	);
 
 	$extended_task_list_definitions = apply_filters( 'wpcom_launchpad_extended_task_list_definitions', array() );
