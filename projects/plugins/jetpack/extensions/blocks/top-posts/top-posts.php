87a13ebb5e030837d7fc89afe3a3f817a9cf3ca8
Aurorum <43215253+Aurorum@users.noreply.github.com>
2023-12-06 21:46:43 +0000

    Fix fatal

--- projects/plugins/jetpack/extensions/blocks/top-posts/top-posts.php
+++ projects/plugins/jetpack/extensions/blocks/top-posts/top-posts.php
@@ -101,7 +101,7 @@ function load_assets( $attributes ) {
 
 		if ( $attributes['displayContext'] && ! empty( $item['context'] ) && is_array( $item['context'] ) ) {
 			$context = reset( $item['context'] );
-			$output .= '<a class="jetpack-top-posts-context has-small-font-size" href="' . esc_url( get_category_link( $context['term_id'] ) ) . '">' . esc_html( $context['name'] ) . '</a>';
+			$output .= '<a class="jetpack-top-posts-context has-small-font-size" href="' . esc_url( get_category_link( $context->term_id ) ) . '">' . esc_html( $context->name ) . '</a>';
 		}
 
 		$output .= '</div>';
