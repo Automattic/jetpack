f1c3980b4363a3ad0cb0a26fc9d320b6ba1dcf39
Andrii Lysenko <60262784+andrii-lysenko@users.noreply.github.com>
2023-12-07 13:11:32 -0800

    Update sharing block controls (#34414)

--- projects/plugins/jetpack/extensions/blocks/sharing-buttons/view.js
+++ projects/plugins/jetpack/extensions/blocks/sharing-buttons/view.js
@@ -1,1 +1,38 @@
 import './style.scss';
+
+const services = window?.jetpack_sharing_buttons_services || [];
+let windowOpen;
+
+( function () {
+	services.forEach( service => {
+		document.querySelectorAll( `a.share-${ service }` ).forEach( link => {
+			link.addEventListener( 'click', event => {
+				if ( service === 'mail' ) {
+					return;
+				}
+				event.preventDefault();
+				event.stopPropagation();
+
+				if ( service === 'print' ) {
+					window.print();
+					return;
+				}
+
+				const el = event.target.closest( `a.share-${ service }` );
+				if ( ! el ) {
+					return;
+				}
+
+				if ( windowOpen !== undefined ) {
+					windowOpen.close();
+				}
+
+				const options = 'menubar=1,resizable=1,width=600,height=400';
+				windowOpen = window.open( el.getAttribute( 'href' ), `wpcom${ service }`, options );
+				if ( windowOpen ) {
+					windowOpen.focus();
+				}
+			} );
+		} );
+	} );
+} )();
