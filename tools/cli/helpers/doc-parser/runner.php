cfa1d12d1157726ebd771a761e85f49b353f5c38
Igor Zinovyev <zinigor@gmail.com>
2023-12-08 21:50:40 +0300

    Added parsing and exporting of Markdown files.

--- tools/cli/helpers/doc-parser/runner.php
+++ tools/cli/helpers/doc-parser/runner.php
@@ -5,6 +5,8 @@
  * @package automattic/jetpack-doc-parser
  */
 
+use Michelf\Markdown;
+
 /**
  * Loading the autoloader and starting the process.
  */
@@ -13,3 +15,35 @@ require __DIR__ . '/vendor/autoload.php';
 $args   = array_slice( $argv, 1 );
 $parser = new \Automattic\Jetpack\Doc_Parser();
 $parser->generate( array( $args, 'phpdoc.json' ) );
+
+$markdown  = new Markdown();
+$docs_json = json_decode( file_get_contents( __DIR__ . '/docs.json' ), true );
+
+$result = array();
+// Each parent file has to be present in the import.
+foreach ( $docs_json['parents'] as $parent => $child_docs ) {
+
+	// We're assuming files are in the Monorepo root.
+	$contents = $markdown->defaultTransform(
+		file_get_contents(
+			dirname( __DIR__, 4 ) . DIRECTORY_SEPARATOR . $parent
+		)
+	);
+
+	$document = new DOMDocument();
+	$document->loadHTML( $contents );
+
+	$doc_title = $parent;
+
+	$headers = $document->getElementsByTagName( 'h1' );
+	if ( count( $headers ) ) {
+		$doc_title = $headers[0]->textContent;
+	}
+
+	$result[] = array(
+		'title'   => $doc_title,
+		'content' => $document->saveHTML(),
+	);
+}
+
+file_put_contents( './markdown.json', json_encode( $result ) );
