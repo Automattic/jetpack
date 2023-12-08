65399a8164cc819655c106b8797e05ce72776222
Mark George <thingalon@gmail.com>
2023-12-08 14:36:29 +1100

    Remove unused file

--- projects/plugins/boost/app/assets/src/js/lib/stores/prompt.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-import { jetpack_boost_ds } from './data-sync-client';
-
-export const dismissedScorePrompt = jetpack_boost_ds.createAsyncStore(
-	'dismissed_score_prompt',
-	z.array( z.string() ).catch( [] )
-);
-
-export const dismissedScorePromptStore = dismissedScorePrompt.store;
