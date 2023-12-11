57bc3ab6705574f1f6301aab148db91e06f5a3a6
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2023-12-11 16:25:45 +0100

    Add handle for the Reblog toggle change

---
+++ projects/plugins/jetpack/extensions/blocks/like/use-set-reblog-setting.js
@@ -0,0 +1,47 @@
+/**
+ * WordPress dependencies
+ */
+import apiFetch from '@wordpress/api-fetch';
+import { useState, useCallback } from '@wordpress/element';
+
+export default function useSetReblogSetting( blogId ) {
+	const [ isLoading, setIsLoading ] = useState( false );
+	const [ success, setSuccess ] = useState( null );
+	const [ error, setError ] = useState( null );
+
+	const setReblogSetting = useCallback(
+		async reblogSetting => {
+			// console.log( 'disabled_reblogs is going to be set to: ', ! reblogSetting );
+			const path = `https://public-api.wordpress.com/rest/v1.3/sites/${ blogId }/settings/`;
+			const data = {
+				disabled_reblogs: ! reblogSetting,
+			};
+
+			setIsLoading( true );
+			await apiFetch( {
+				url: path,
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				data: data,
+			} )
+				.then( () => {
+					setSuccess( true );
+					setError( null );
+				} )
+				.catch( err => {
+					setError( err );
+				} )
+				.finally( () => {
+					setIsLoading( false );
+				} );
+		},
+		[ blogId ]
+	);
+
+	return {
+		isLoading,
+		success,
+		error,
+		setReblogSetting,
+	};
+}
