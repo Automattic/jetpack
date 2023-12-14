053a6bfe40dec943bab031a88fc7422d42d60259
dkmyta <dkmyta10@gmail.com>
2023-12-14 09:11:12 -0800

    First take at implementing onboarding flows

--- projects/plugins/protect/src/js/components/summary/index.jsx
+++ projects/plugins/protect/src/js/components/summary/index.jsx
@@ -1,17 +1,44 @@
-import { Container, Col, Text, Title, getIconBySlug, Button } from '@automattic/jetpack-components';
+import {
+	Container,
+	Col,
+	Text,
+	Title,
+	getIconBySlug,
+	Button,
+	ActionPopover,
+} from '@automattic/jetpack-components';
+import { useProductCheckoutWorkflow } from '@automattic/jetpack-connection';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { dateI18n } from '@wordpress/date';
+import { createInterpolateElement } from '@wordpress/element';
 import { __, sprintf } from '@wordpress/i18n';
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import { JETPACK_SCAN_SLUG } from '../../constants';
+import useAnalyticsTracks from '../../hooks/use-analytics-tracks';
 import useProtectData from '../../hooks/use-protect-data';
 import { STORE_ID } from '../../state/store';
 import styles from './styles.module.scss';
 
-const Summary = () => {
+const Summary = ( {
+	anchors,
+	setAnchors,
+	onboardingStep,
+	incrementOnboardingStep,
+	closeOnboarding,
+} ) => {
 	const { numThreats, lastChecked, hasRequiredPlan } = useProtectData();
 	const scanIsEnqueuing = useSelect( select => select( STORE_ID ).getScanIsEnqueuing() );
 	const { scan } = useDispatch( STORE_ID );
 	const Icon = getIconBySlug( 'protect' );
+
+	const { adminUrl } = window.jetpackProtectInitialState || {};
+	const { run } = useProductCheckoutWorkflow( {
+		productSlug: JETPACK_SCAN_SLUG,
+		redirectUrl: adminUrl,
+	} );
+
+	const { recordEventHandler } = useAnalyticsTracks();
+	const getScan = recordEventHandler( 'jetpack_protect_threat_list_get_scan_link_click', run );
 
 	const handleScanClick = () => {
 		return event => {
@@ -20,18 +47,58 @@ const Summary = () => {
 		};
 	};
 
+	const anchor2aRef = useRef( null );
+	const anchor2bRef = useRef( null );
+
+	useEffect( () => {
+		setAnchors( prevAnchors => ( {
+			...prevAnchors, // Spread the existing anchors
+			anchor2a: anchor2aRef.current,
+			anchor2b: anchor2bRef.current,
+		} ) );
+	}, [ anchor2aRef, anchor2bRef, setAnchors ] );
+
 	return (
 		<Container fluid>
 			<Col>
 				<div className={ styles.summary }>
 					<div>
+						{ ! hasRequiredPlan && onboardingStep === 2 && (
+							<ActionPopover
+								title={ __( 'Daily automated scans', 'jetpack-protect' ) }
+								buttonContent={ __( 'Finish', 'jetpack-protect' ) }
+								anchor={ anchors.anchor2a }
+								onClose={ closeOnboarding }
+								onClick={ incrementOnboardingStep }
+								noArrow={ false }
+								className={ styles[ 'action-popover' ] }
+								position={ 'middle right' }
+								offset={ 15 }
+								step={ 2 }
+								totalSteps={ 2 }
+							>
+								<Text>
+									{ createInterpolateElement(
+										__(
+											'We run daily automated scans. Do you want ot be able to scan manually? <upgradeLink>Upgrade</upgradeLink>',
+											'jetpack-protect'
+										),
+										{
+											upgradeLink: <Button variant="link" weight="regular" onClick={ getScan } />,
+										}
+									) }
+								</Text>
+							</ActionPopover>
+						) }
 						<Title size="small" className={ styles.summary__title }>
 							<Icon size={ 32 } className={ styles.summary__icon } />
-							{ sprintf(
-								/* translators: %s: Latest check date  */
-								__( 'Latest results as of %s', 'jetpack-protect' ),
-								dateI18n( 'F jS', lastChecked )
-							) }
+							<span ref={ ! hasRequiredPlan ? anchor2aRef : null }>
+								{ sprintf(
+									/* translators: %s: Latest check date  */
+									__( 'Latest results as of %s', 'jetpack-protect' ),
+									dateI18n( 'F jS', lastChecked )
+								) }
+							</span>
 						</Title>
 						{ numThreats > 0 && (
 							<Text variant="headline-small" component="h1">
@@ -45,14 +112,39 @@ const Summary = () => {
 						) }
 					</div>
 					{ hasRequiredPlan && numThreats === 0 && (
-						<Button
-							variant="secondary"
-							className={ styles[ 'summary__scan-button' ] }
-							isLoading={ scanIsEnqueuing }
-							onClick={ handleScanClick() }
-						>
-							{ __( 'Scan now', 'jetpack-protect' ) }
-						</Button>
+						<>
+							{ onboardingStep === 2 && (
+								<ActionPopover
+									title={ __( 'Daily & manual scanning', 'jetpack-protect' ) }
+									buttonContent={ __( 'Finish', 'jetpack-protect' ) }
+									anchor={ anchors.anchor2b }
+									onClose={ closeOnboarding }
+									onClick={ closeOnboarding }
+									noArrow={ false }
+									className={ styles[ 'action-popover' ] }
+									position={ 'middle left' }
+									offset={ 15 }
+									step={ 2 }
+									totalSteps={ 2 }
+								>
+									<Text>
+										{ __(
+											'We run daily automated scans but you can also run on-demand scans if you want to check the latest status.',
+											'jetpack-protect'
+										) }
+									</Text>
+								</ActionPopover>
+							) }
+							<Button
+								ref={ anchor2bRef }
+								variant="secondary"
+								className={ styles[ 'summary__scan-button' ] }
+								isLoading={ scanIsEnqueuing }
+								onClick={ handleScanClick() }
+							>
+								{ __( 'Scan now', 'jetpack-protect' ) }
+							</Button>
+						</>
 					) }
 				</div>
 			</Col>
--- projects/plugins/protect/src/js/components/threats-list/index.jsx
+++ projects/plugins/protect/src/js/components/threats-list/index.jsx
@@ -1,7 +1,17 @@
-import { Container, Col, Title, Button } from '@automattic/jetpack-components';
+import {
+	Container,
+	Col,
+	Title,
+	Button,
+	Text,
+	ActionPopover,
+	getRedirectUrl,
+	useBreakpointMatch,
+} from '@automattic/jetpack-components';
 import { useDispatch, useSelect } from '@wordpress/data';
+import { createInterpolateElement } from '@wordpress/element';
 import { __, sprintf } from '@wordpress/i18n';
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo, useRef, useEffect } from 'react';
 import useProtectData from '../../hooks/use-protect-data';
 import { STORE_ID } from '../../state/store';
 import EmptyList from './empty';
@@ -11,19 +21,29 @@ import PaidList from './paid-list';
 import styles from './styles.module.scss';
 import useThreatsList from './use-threats-list';
 
-const ThreatsList = () => {
+const ThreatsList = ( {
+	anchors,
+	setAnchors,
+	onboardingStep,
+	incrementOnboardingStep,
+	closeOnboarding,
+} ) => {
+	const { siteSuffix } = window.jetpackProtectInitialState;
+
+	const [ isSm ] = useBreakpointMatch( 'sm' );
+
 	const { hasRequiredPlan } = useProtectData();
 	const { item, list, selected, setSelected } = useThreatsList();
 	const fixableList = list.filter( obj => obj.fixable );
 
 	const { setModal } = useDispatch( STORE_ID );
-
 	const { scan } = useDispatch( STORE_ID );
 	const scanIsEnqueuing = useSelect( select => select( STORE_ID ).getScanIsEnqueuing() );
 
 	const handleFixAllThreatsClick = threatList => {
 		return event => {
 			event.preventDefault();
+			closeOnboarding();
 			setModal( {
 				type: 'FIX_ALL_THREATS',
 				props: { threatList },
@@ -37,6 +57,31 @@ const ThreatsList = () => {
 			scan();
 		};
 	};
+
+	const totalSteps = useMemo( () => {
+		if ( ! hasRequiredPlan || list.length === 0 ) {
+			return 2;
+		} else if ( fixableList.length === 0 ) {
+			return 3;
+		}
+
+		return 4;
+	}, [ hasRequiredPlan, list, fixableList ] );
+
+	const anchor1Ref = useRef( null );
+	const anchor2Ref = useRef( null );
+	const anchor3Ref = useRef( null );
+	const anchor4Ref = useRef( null );
+
+	useEffect( () => {
+		setAnchors( prevAnchors => ( {
+			...prevAnchors, // Spread the existing anchors
+			anchor1: anchor1Ref.current,
+			anchor2: anchor2Ref.current,
+			anchor3: anchor3Ref.current,
+			anchor4: anchor4Ref.current,
+		} ) );
+	}, [ anchor1Ref, anchor2Ref, anchor3Ref, anchor4Ref, setAnchors ] );
 
 	const getTitle = useCallback( () => {
 		switch ( selected ) {
@@ -78,7 +123,31 @@ const ThreatsList = () => {
 	return (
 		<Container fluid horizontalSpacing={ 0 } horizontalGap={ 3 }>
 			<Col lg={ 4 }>
-				<ThreatsNavigation selected={ selected } onSelect={ setSelected } />
+				{ onboardingStep === 1 && (
+					<ActionPopover
+						title={ __( 'Your scan results', 'jetpack-protect' ) }
+						buttonContent={ __( 'Next', 'jetpack-protect' ) }
+						anchor={ anchors.anchor1 }
+						onClose={ closeOnboarding }
+						onClick={ incrementOnboardingStep }
+						noArrow={ false }
+						className={ styles[ 'action-popover' ] }
+						position={ 'middle top' }
+						offset={ 15 }
+						step={ 1 }
+						totalSteps={ totalSteps }
+					>
+						<Text>
+							{ __(
+								'Navigate through the results of the scan on your WordPress installation, plugins, themes and other files',
+								'jetpack-protect'
+							) }
+						</Text>
+					</ActionPopover>
+				) }
+				<div ref={ anchor1Ref }>
+					<ThreatsNavigation selected={ selected } onSelect={ setSelected } />
+				</div>
 			</Col>
 			<Col lg={ 8 }>
 				{ list?.length > 0 ? (
@@ -88,18 +157,88 @@ const ThreatsList = () => {
 							{ hasRequiredPlan && (
 								<>
 									{ fixableList.length > 0 && (
-										<Button
-											variant="primary"
-											className={ styles[ 'list-header-button' ] }
-											onClick={ handleFixAllThreatsClick( fixableList ) }
+										<>
+											{ onboardingStep === 2 && (
+												<ActionPopover
+													title={ __( 'Auto-fix with one click', 'jetpack-protect' ) }
+													buttonContent={ __( 'Next', 'jetpack-protect' ) }
+													anchor={ anchors.anchor2 }
+													onClose={ closeOnboarding }
+													onClick={ incrementOnboardingStep }
+													noArrow={ false }
+													className={ styles[ 'action-popover' ] }
+													position={ isSm ? 'bottom right' : 'middle left' }
+													offset={ 15 }
+													step={ 2 }
+													totalSteps={ 4 }
+												>
+													<Text>
+														{ __(
+															'Jetpack Protect offers one-click fixes for most threats. Press this button and be safe again.',
+															'jetpack-protect'
+														) }
+													</Text>
+													{ <br /> }
+													<Text>
+														{ createInterpolateElement(
+															__(
+																"Note that you'll have to <credentialsLink>input your server credentials</credentialsLink> first.",
+																'jetpack-protect'
+															),
+															{
+																credentialsLink: (
+																	<Button
+																		variant="link"
+																		weight="regular"
+																		href={ getRedirectUrl(
+																			'jetpack-settings-security-credentials',
+																			{ site: siteSuffix }
+																		) }
+																	/>
+																),
+															}
+														) }
+													</Text>
+												</ActionPopover>
+											) }
+											<Button
+												ref={ anchor2Ref }
+												variant="primary"
+												className={ styles[ 'list-header-button' ] }
+												onClick={ handleFixAllThreatsClick( fixableList ) }
+											>
+												{ sprintf(
+													/* translators: Translates to Auto fix all. $s: Number of fixable threats. */
+													__( 'Auto fix all (%s)', 'jetpack-protect' ),
+													fixableList.length
+												) }
+											</Button>
+										</>
+									) }
+									{ hasRequiredPlan && onboardingStep === totalSteps && (
+										<ActionPopover
+											title={ __( 'Daily & manual scanning', 'jetpack-protect' ) }
+											buttonContent={ __( 'Finish', 'jetpack-protect' ) }
+											anchor={ anchors.anchor4 }
+											onClose={ closeOnboarding }
+											onClick={ closeOnboarding }
+											noArrow={ false }
+											className={ styles[ 'action-popover' ] }
+											position={ isSm ? 'bottom left' : 'middle left' }
+											offset={ 15 }
+											step={ totalSteps }
+											totalSteps={ totalSteps }
 										>
-											{
-												/* translators: Translates to Auto fix all. $s: Number of fixable threats. */
-												sprintf( __( 'Auto fix all (%s)', 'jetpack-protect' ), fixableList.length )
-											}
-										</Button>
+											<Text>
+												{ __(
+													'We run daily automated scans but you can also run on-demand scans if you want to check the latest status.',
+													'jetpack-protect'
+												) }
+											</Text>
+										</ActionPopover>
 									) }
 									<Button
+										ref={ anchor4Ref }
 										variant="secondary"
 										className={ styles[ 'list-header-button' ] }
 										isLoading={ scanIsEnqueuing }
@@ -110,7 +249,37 @@ const ThreatsList = () => {
 								</>
 							) }
 						</div>
-						{ hasRequiredPlan ? <PaidList list={ list } /> : <FreeList list={ list } /> }
+						<>
+							{ hasRequiredPlan && onboardingStep === totalSteps - 1 && (
+								<ActionPopover
+									title={ __( 'Understand severity', 'jetpack-protect' ) }
+									buttonContent={ __( 'Next', 'jetpack-protect' ) }
+									anchor={ anchors.anchor3 }
+									onClose={ closeOnboarding }
+									onClick={ incrementOnboardingStep }
+									noArrow={ false }
+									className={ styles[ 'action-popover' ] }
+									position={ 'top middle' }
+									offset={ 15 }
+									step={ totalSteps - 1 }
+									totalSteps={ totalSteps }
+								>
+									<Text>
+										{ __(
+											'Learn how critical these threats are for the security of your site by glancing at the severity labels.',
+											'jetpack-protect'
+										) }
+									</Text>
+								</ActionPopover>
+							) }
+							{ hasRequiredPlan ? (
+								<div ref={ anchor3Ref }>
+									<PaidList list={ list } />
+								</div>
+							) : (
+								<FreeList list={ list } />
+							) }
+						</>
 					</>
 				) : (
 					<EmptyList />
