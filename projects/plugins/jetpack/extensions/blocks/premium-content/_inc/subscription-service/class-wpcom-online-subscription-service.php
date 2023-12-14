62af8fd1d76fdf48d5b9d0fd78991aacfb7787cc
Fabien MILLERAND <millerf@automattic.com>
2023-12-14 13:52:36 +0000

    Update class-wpcom-online-subscription-service.php

--- projects/plugins/jetpack/extensions/blocks/premium-content/_inc/subscription-service/class-wpcom-online-subscription-service.php
+++ projects/plugins/jetpack/extensions/blocks/premium-content/_inc/subscription-service/class-wpcom-online-subscription-service.php
@@ -85,9 +85,9 @@ class WPCOM_Online_Subscription_Service extends Jetpack_Token_Subscription_Servi
 	/**
 	 * Returns true if the current authenticated user has a pending subscription to the current site.
 	 *
-	 * @return boolean
+	 * @return bool
 	 */
-	public function is_current_user_pending_subscriber() {
+	public function is_current_user_pending_subscriber(): bool {
 		include_once WP_CONTENT_DIR . '/mu-plugins/email-subscriptions/subscriptions.php';
 		$email             = wp_get_current_user()->user_email;
 		$subscriber_object = \Blog_Subscriber::get( $email );
