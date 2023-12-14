290a259e2e5ab2afa97c04572ac779538c64fec1
Adnan Haque <3737780+haqadn@users.noreply.github.com>
2023-12-14 04:58:09 +0000

    Boost: Refactor speed scores into react (#34610)
    * Refactor speed scores section into react
    
    * Move performance history fresh start state management to performance-history
    
    * Cleanup
    
    * Move speed score styles to component
    
    * Fix type
    
    * Rename a variable
    
    * Don't use sass:math
    
    * Fix error notice
    
    * changelog
    
    * Fix e2e tests

--- projects/plugins/boost/app/assets/src/js/layout/SettingsPage/SettingsPage.svelte
+++ projects/plugins/boost/app/assets/src/js/layout/SettingsPage/SettingsPage.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Score from '../../features/speed-score/Score.svelte';
+	import SpeedScore from '../../features/speed-score/speed-score';
 	import Support from './support/support';
 	import Tips from './Tips.svelte';
 	import ReactComponent from '$features/ReactComponent.svelte';
@@ -11,19 +11,26 @@
 
 	$: criticalCssCreated = $criticalCssState.created;
 	$: criticalCssIsGenerating = $isGenerating;
-	$: activeModules = Object.entries( $modulesState ).reduce( ( acc, [ key, value ] ) => {
+	$: moduleStates = Object.entries( $modulesState ).reduce( ( acc, [ key, value ] ) => {
 		if ( key !== 'image_guide' && key !== 'image_size_analysis' ) {
 			acc.push( value.active );
 		}
 		return acc;
 	}, [] );
+	$: performanceHistoryNeedsUpgrade = ! $modulesState.performance_history.available;
 </script>
 
 <div id="jb-dashboard" class="jb-dashboard jb-dashboard--main">
 	<ReactComponent this={Header} />
 
-	<div class="jb-section jb-section--alt jb-section--scores">
-		<Score {activeModules} {criticalCssCreated} {criticalCssIsGenerating} />
+	<div class="jb-section jb-section--alt">
+		<ReactComponent
+			this={SpeedScore}
+			{moduleStates}
+			{criticalCssCreated}
+			{criticalCssIsGenerating}
+			{performanceHistoryNeedsUpgrade}
+		/>
 	</div>
 
 	<div class="jb-section jb-section--main">
