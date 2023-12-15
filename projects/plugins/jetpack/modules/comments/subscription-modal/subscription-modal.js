0bbf7141b92b5f3f96acd3650fbe098930fe147e
Gabriel Demichelis <gabrieldemichelis@gmail.com>
2023-12-15 01:18:06 -0300

    Promote subscription: Add subscription modal on comment

---
+++ projects/plugins/jetpack/modules/comments/subscription-modal/subscription-modal.js
@@ -0,0 +1,57 @@
+const { domReady } = wp;
+
+domReady( function () {
+	const modal = document.getElementsByClassName( 'jetpack-subscription-modal' )[ 0 ];
+
+	if ( ! modal ) {
+		return;
+	}
+
+	const close = document.getElementsByClassName( 'jetpack-subscription-modal__close' )[ 0 ];
+	let redirectUrl = '';
+	let hasLoaded = false;
+
+	function JetpackSubscriptionModalOnCommentMessageListener( event ) {
+		let message = event && event.data;
+		if ( typeof message === 'string' ) {
+			try {
+				message = JSON.parse( message );
+			} catch ( err ) {
+				return;
+			}
+		}
+
+		const type = message && message.type;
+		const data = message && message.data;
+
+		if ( type !== 'subscriptionModalShow' || typeof data.url === 'undefined' ) {
+			return;
+		}
+
+		// TODO: add a check for origin
+
+		// TODO: Add a check for Times the user has seen the modal
+		if ( ! hasLoaded ) {
+			modal.classList.toggle( 'open' );
+			hasLoaded = true;
+			redirectUrl = data.url;
+			return;
+		}
+	}
+
+	window.addEventListener( 'message', JetpackSubscriptionModalOnCommentMessageListener );
+
+	if ( close ) {
+		close.onclick = function () {
+			modal.classList.toggle( 'open' );
+			window.location.href = redirectUrl;
+		};
+	}
+
+	window.onclick = function ( event ) {
+		if ( event.target === modal ) {
+			modal.style.display = 'none';
+			window.location.href = redirectUrl;
+		}
+	};
+} );
