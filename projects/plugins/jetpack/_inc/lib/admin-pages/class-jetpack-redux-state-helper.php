9b32abccc49fa096a2dde564c6c084c296c78c35
Gergely Juhasz <gergely.juhasz@automattic.com>
2023-12-18 10:00:27 +0100

    Remove change for Jetpack notices

--- projects/plugins/jetpack/_inc/lib/admin-pages/class-jetpack-redux-state-helper.php
+++ projects/plugins/jetpack/_inc/lib/admin-pages/class-jetpack-redux-state-helper.php
@@ -23,7 +23,6 @@ use Automattic\Jetpack\Licensing\Endpoints as Licensing_Endpoints;
 use Automattic\Jetpack\My_Jetpack\Initializer as My_Jetpack_Initializer;
 use Automattic\Jetpack\Partner;
 use Automattic\Jetpack\Partner_Coupon as Jetpack_Partner_Coupon;
-use Automattic\Jetpack\Publicize\Jetpack_Social_Settings\Dismissed_Notices;
 use Automattic\Jetpack\Stats\Options as Stats_Options;
 use Automattic\Jetpack\Status;
 use Automattic\Jetpack\Status\Host;
@@ -148,7 +147,7 @@ class Jetpack_Redux_State_Helper {
 			'connectUrl'                  => false == $current_user_data['isConnected'] // phpcs:ignore Universal.Operators.StrictComparisons.LooseEqual
 				? Jetpack::init()->build_connect_url( true, false, false )
 				: '',
-			'dismissedNotices'            => Dismissed_Notices::get_dismissed_notices(),
+			'dismissedNotices'            => self::get_dismissed_jetpack_notices(),
 			'isDevVersion'                => Jetpack::is_development_version(),
 			'currentVersion'              => JETPACK__VERSION,
 			'is_gutenberg_available'      => true,
@@ -303,7 +302,6 @@ class Jetpack_Redux_State_Helper {
 	/**
 	 * Gets array of any Jetpack notices that have been dismissed.
 	 *
-	 * @deprecated $$next_version$$
 	 * @return mixed|void
 	 */
 	public static function get_dismissed_jetpack_notices() {
