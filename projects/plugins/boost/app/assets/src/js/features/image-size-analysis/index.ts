deda788a505df4fe414eaa78a41a34dc6c1dc8f1
Nauris Pūķis <hi@pyronaur.com>
2023-12-19 14:04:30 +0200

    Only export types as public interafce from types file

--- projects/plugins/boost/app/assets/src/js/features/image-size-analysis/index.ts
+++ projects/plugins/boost/app/assets/src/js/features/image-size-analysis/index.ts
@@ -1,3 +1,4 @@
-export * from './lib/stores/types';
+export type * from './lib/stores/types';
+export { ISAStatus } from './lib/stores/types';
 export * from './lib/stores/isa-data';
 export * from './lib/stores/isa-report';
