0c13b9ce3ced0d0fedb66e2372a14f3162233209
Nauris Pūķis <hi@pyronaur.com>
2023-12-19 20:25:39 +0200

    TODO: Figure out navigation with DataSync

--- projects/plugins/boost/app/assets/src/js/pages/image-size-analysis/image-size-analysis.tsx
+++ projects/plugins/boost/app/assets/src/js/pages/image-size-analysis/image-size-analysis.tsx
@@ -11,15 +11,31 @@ import {
 import Footer from '../../layout/footer/footer';
 import Header from '../../layout/header/header';
 import { DataSyncProvider } from '@automattic/jetpack-react-data-sync-client';
+import { useEffect } from 'react';
 
 const _ImageSizeAnalysis = ( { isImageCdnModuleActive }: { isImageCdnModuleActive: boolean } ) => {
-	const [ isaData ] = useIsaData();
+	const [ isaData, updateIsaData ] = useIsaData();
 	const [ isaReport ] = useIsaReport();
 	const dataGroupTabs = isaReport.data ? getGroupedReports( isaReport.data ) : undefined;
 	const activeGroupName = isaData.data?.query.group as keyof typeof dataGroupTabs;
 	const activeReport =
 		dataGroupTabs && activeGroupName ? dataGroupTabs[ activeGroupName ] : undefined;
 
+	// @TODO:
+	// Figure out a way to navigate using DataSync.
+	useEffect( () => {
+		const timer = setTimeout( () => {
+			const data = {
+				data: isaData.data?.data,
+				query: {
+					...isaData.data?.query,
+					group: 'pages',
+				},
+			};
+			updateIsaData.mutate( data );
+		}, 3000 );
+		return () => clearTimeout( timer );
+	}, [] );
 	return (
 		<div id="jb-dashboard" className="jb-dashboard">
 			<Header subPageTitle={ __( 'Image analysis report', 'jetpack-boost' ) } />
