1a647fe97ce705eed0814ce1755c888715a6184e
Nauris Pūķis <hi@pyronaur.com>
2023-12-19 14:11:40 +0200

    Add comments

--- projects/plugins/boost/app/assets/src/js/features/image-size-analysis/index.ts
+++ projects/plugins/boost/app/assets/src/js/features/image-size-analysis/index.ts
@@ -1,4 +1,13 @@
+/**
+ * The types.ts file contains both Zod schemas and TypeScript types.
+ * Intentionally exposing only the TypeScript types"to the public"
+ * to discourage Zod schema usage outside of this module.
+ */
 export type * from './lib/stores/types';
 export { ISAStatus } from './lib/stores/types';
+
+/**
+ * Export stores and utility functions from libs.
+ */
 export * from './lib/stores/isa-data';
 export * from './lib/stores/isa-report';
