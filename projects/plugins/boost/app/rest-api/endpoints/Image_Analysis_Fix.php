2f35df397848668f961e64a6444d05841a9b6684
Nauris Pūķis <hi@pyronaur.com>
2023-12-20 20:14:12 +0200

    TEMPORARY HACK: Manually register DS Action Endpoint

--- projects/plugins/boost/app/rest-api/endpoints/Image_Analysis_Fix.php
+++ projects/plugins/boost/app/rest-api/endpoints/Image_Analysis_Fix.php
@@ -10,7 +10,7 @@ use Automattic\Jetpack_Boost\REST_API\Permissions\Current_User_Admin;
 
 /**
  * API Endpoint for starting an Image Size Analysis run.
- * Lives at POST jetpack-boost/v1/image-size-analysis/fix
+ * Lives at POST jetpack-boost/v1/image-size-analysis/action/fix
  */
 class Image_Analysis_Fix implements Endpoint {
 
@@ -21,18 +21,25 @@ class Image_Analysis_Fix implements Endpoint {
 	}
 
 	public function response( $request ) {
+
 		// @TODO: Add a proper feature flag for this instead of just checking if priority support available.
 		if ( ! Premium_Features::has_feature( Premium_Features::IMAGE_SIZE_ANALYSIS ) ) {
 			return new \WP_Error( 'not-allowed', 'Feature not enabled' );
 		}
+		$json_params = $request->get_json_params();
+		$data   = isset( $json_params['JSON'] ) ? $json_params['JSON'] : null;
 
-		if ( empty( $request->get_param( 'nonce' ) ) || ! wp_verify_nonce( sanitize_key( $request->get_param( 'nonce' ) ), self::NONCE_ACTION ) ) {
+		if ( empty( $data['nonce'] ) || ! wp_verify_nonce( sanitize_key( $data['nonce'] ), self::NONCE_ACTION ) ) {
 			return new \WP_Error( 'not-allowed', 'Bad Nonce' );
 		}
 
-		$params        = Image_Size_Analysis_Fixer::sanitize_params( $request->get_params() );
-		$fixes         = Image_Size_Analysis_Fixer::get_fixes( $request->get_param( 'post_id' ) );
-		$image_url     = Image_Size_Analysis_Fixer::fix_url( $request->get_param( 'image_url' ) );
+		try {
+			$params = Image_Size_Analysis_Fixer::sanitize_params( $data );
+		} catch ( \Exception $e ) {
+			return new \WP_Error( 'bad-params', $e->getMessage() );
+		}
+		$fixes         = Image_Size_Analysis_Fixer::get_fixes( $params['post_id'] );
+		$image_url     = Image_Size_Analysis_Fixer::fix_url( $params[ 'image_url' ] );
 		$attachment_id = attachment_url_to_postid( esc_url( $image_url ) );
 		$changed       = false;
 
@@ -52,25 +59,32 @@ class Image_Analysis_Fix implements Endpoint {
 		}
 
 		if ( $changed ) {
-			$status = update_post_meta( $request->get_param( 'post_id' ), '_jb_image_fixes', $fixes );
+			$status = update_post_meta( $data['post_id'], '_jb_image_fixes', $fixes );
 		}
 
 		if ( ! $status ) {
 			return rest_ensure_response(
 				array(
 					'status' => 'error',
-					'code'   => 'failed-to-save-fixes',
-				)
-			);
-		} else {
-			return rest_ensure_response(
-				array(
-					'status'  => 'success',
-					'code'    => 'fixes-saved',
-					'changed' => $changed,
+					'JSON'   => array(
+						'status' => 'error',
+						'code'   => 'failed-to-save-fixes',
+					),
 				)
 			);
 		}
+
+		return rest_ensure_response(
+			array(
+				'status' => 'success',
+				'JSON'   => array(
+					'status'  => 'success',
+					'code'    => 'fixes-saved',
+					'changed' => $changed,
+					'image_id' => $data['image_id'],
+				),
+			)
+		);
 	}
 
 	public function permissions() {
@@ -80,6 +94,48 @@ class Image_Analysis_Fix implements Endpoint {
 	}
 
 	public function name() {
-		return 'image-size-analysis/fix';
+
+		//// TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		//// TEMPORARY HACK DO NOT SHIP ///////
+		/// vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+		register_rest_route(
+			'jetpack-boost-ds',
+			'image-size-analysis/action/fix',
+			array(
+				'methods'             => $this->request_methods(),
+				'callback'            => array( $this, 'response' ),
+				'permission_callback' => '__return_true',
+			)
+		);
+		// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+		//// TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		///  TEMPORARY HACK DO NOT SHIP ///////
+		//// TEMPORARY HACK DO NOT SHIP ///////
+		return 'jetpack-boost-ds/image-size-analysis/action/fix';
 	}
 }
