8a88d7b68002a71c2a5adc8499e0a8d61d81acfd
Kevin Zoschke <kevin.zoschke@automattic.com>
2023-12-20 16:41:20 -0500

    Contact Form: add date format to date picker

--- projects/packages/forms/src/contact-form/js/grunion-frontend.js
+++ projects/packages/forms/src/contact-form/js/grunion-frontend.js
@@ -1,3 +1,9 @@
 jQuery( function ( $ ) {
-	$( '.contact-form input.jp-contact-form-date' ).datepicker();
+	const $input = $( '.contact-form input.jp-contact-form-date' );
+	const dateFormat = $input.attr( 'data-format' ) || 'yy-mm-dd';
+
+	$input.datepicker( {
+		dateFormat,
+		constrainInput: false,
+	} );
 } );
