27e1fc516ba64cb743eb2092d0a71ec68b97ed99
Andrii Lysenko <lysenkoa.work@gmail.com>
2023-12-22 07:04:34 -0800

    refactor seo registration

--- projects/plugins/jetpack/extensions/plugins/seo/seo.php
+++ projects/plugins/jetpack/extensions/plugins/seo/seo.php
@@ -18,11 +18,14 @@ use Jetpack_Gutenberg;
  * @return void
  */
 function register_plugins() {
-	// Register SEO.
-	// On Simple sites we handle plan with Upsell message if needed
-	$is_wpcom_simple = ( new Host() )->is_wpcom_simple();
+	// Setting availability for Advanced SEO plan.
+	Jetpack_Gutenberg::set_availability_for_plan( 'advanced-seo' );
+
+	// On WPCOM sites we handle plan with Upsell message if needed.
+	// On Self-Hosted we check if there's an active connection and if the site is not in offline mode.
+	$is_wpcom_platform = ( new Host() )->is_wpcom_platform();
 	if (
-		$is_wpcom_simple
+		$is_wpcom_platform
 		|| ( ( new Connection_Manager( 'jetpack' ) )->has_connected_owner() && ! ( new Status() )->is_offline_mode() )
 	) {
 		Jetpack_Gutenberg::set_extension_available( 'jetpack-seo' );
@@ -31,7 +34,6 @@ function register_plugins() {
 
 add_action( 'jetpack_register_gutenberg_extensions', __NAMESPACE__ . '\register_plugins' );
 
-// Populate the available extensions with Advanced SEO.
 add_filter(
 	'jetpack_set_available_extensions',
 	function ( $extensions ) {
@@ -39,12 +41,5 @@ add_filter(
 			$extensions,
 			array( 'advanced-seo' )
 		);
-	}
-);
-
-add_action(
-	'jetpack_register_gutenberg_extensions',
-	function () {
-		\Jetpack_Gutenberg::set_availability_for_plan( 'advanced-seo' );
 	}
 );
