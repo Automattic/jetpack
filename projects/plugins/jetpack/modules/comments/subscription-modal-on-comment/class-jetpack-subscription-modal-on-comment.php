372b5fb91dfc8add814543f8946e8346d980ff0b
Gabriel Demichelis <gabrieldemichelis@gmail.com>
2023-12-27 19:56:55 -0300

    Remove unused code for Jetpack/not-enabled

--- projects/plugins/jetpack/modules/comments/subscription-modal-on-comment/class-jetpack-subscription-modal-on-comment.php
+++ projects/plugins/jetpack/modules/comments/subscription-modal-on-comment/class-jetpack-subscription-modal-on-comment.php
@@ -54,8 +54,8 @@ class Jetpack_Subscription_Modal_On_Comment {
 			if ( ( new Host() )->is_woa_site() && get_option( 'jetpack_verbum_subscription_modal', true ) ) {
 				add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_assets' ) );
 				add_action( 'wp_footer', array( $this, 'add_subscription_modal_to_frontend' ) );
+				add_filter( 'get_block_template', array( $this, 'get_block_template_filter' ), 10, 3 );
 			}
-			add_filter( 'get_block_template', array( $this, 'get_block_template_filter' ), 10, 3 );
 		}
 	}
 
