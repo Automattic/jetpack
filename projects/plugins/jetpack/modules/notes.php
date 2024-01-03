fb71cf18d6624fe1dabfe356d347e040114d9585
Paulo Cruz <paulo.cruz@automattic.com>
2024-01-03 12:59:15 -0300

    Notification Accessibility: Turn notification icon into a link (#34810)
    * Turn notification icon into a link
    
    * changelog
    
    * Update notification link on the default toolbar
    
    * Add screen reader text

--- projects/plugins/jetpack/modules/notes.php
+++ projects/plugins/jetpack/modules/notes.php
@@ -192,6 +192,7 @@ class Jetpack_Notifications {
 			array(
 				'id'     => 'notes',
 				'title'  => '<span id="wpnt-notes-unread-count" class="' . esc_attr( $classes ) . '">
+					<span class="screen-reader-text">' . esc_html__( 'Notifications', 'jetpack' ) . '</span>
 					<span class="noticon noticon-notification"></span>
 					</span>',
 				'meta'   => array(
@@ -199,6 +200,7 @@ class Jetpack_Notifications {
 					'class' => 'menupop',
 				),
 				'parent' => 'top-secondary',
+				'href'   => 'https://wordpress.com/notifications',
 			)
 		);
 	}
