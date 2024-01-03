6ba1128b06b6e0b6f1e2fa76ee4a8a4ec2f56715
Linas Valiukas <linas.valiukas@automattic.com>
2024-01-03 15:46:20 +0200

    Revert "Remove unused imports"
    This reverts commit 8e4d8f4ba09083dfe4cccdaaab204d3d76727910.

--- projects/plugins/jetpack/tests/e2e/specs/post-connection/waf-blocking.test.js
+++ projects/plugins/jetpack/tests/e2e/specs/post-connection/waf-blocking.test.js
@@ -1,7 +1,7 @@
 import { test, expect } from 'jetpack-e2e-commons/fixtures/base-test.js';
 import { WpPage } from 'jetpack-e2e-commons/pages/index.js';
 import playwrightConfig from '../../playwright.config.mjs';
-import { prerequisitesBuilder } from 'jetpack-e2e-commons/env/index.js';
+import { Plans, prerequisitesBuilder } from 'jetpack-e2e-commons/env/index.js';
 import { resolveSiteUrl } from 'jetpack-e2e-commons/helpers/utils-helper.js';
 import { enableAutomaticRules, generateRules } from '../../helpers/waf-helper.js';
 
