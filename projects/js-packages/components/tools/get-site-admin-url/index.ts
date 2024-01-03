e1b9c84679ea8942eb3846cd102038926f807346
Juanma Rodriguez Escriche <juanma.rodriguez.escriche@automattic.com>
2024-01-03 17:08:12 +0100

    Moved  global declaration to a separate file.

--- projects/js-packages/components/tools/get-site-admin-url/index.ts
+++ projects/js-packages/components/tools/get-site-admin-url/index.ts
@@ -1,16 +1,3 @@
-declare global {
-	interface Window {
-		Initial_State?: {
-			adminUrl?: string;
-		};
-		Jetpack_Editor_Initial_State?: {
-			adminUrl?: string;
-		};
-		myJetpackInitialState?: {
-			adminUrl?: string;
-		};
-	}
-}
 /**
  * Returns the site admin URL.
  *
