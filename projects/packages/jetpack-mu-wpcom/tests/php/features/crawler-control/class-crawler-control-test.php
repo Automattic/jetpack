998e07f6c528d29e4e611f6da267f2a5e56abe9c
artpi <artur.piszek@gmail.com>
2024-01-10 18:16:45 +0100

    lint

--- projects/packages/jetpack-mu-wpcom/tests/php/features/crawler-control/class-crawler-control-test.php
+++ projects/packages/jetpack-mu-wpcom/tests/php/features/crawler-control/class-crawler-control-test.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * Coming Soon Tests File
+ * Tests for Crawler Control
+ *
+ * @package automattic/jetpack-mu-wpcom
  */
 
 use Automattic\Jetpack\Jetpack_Mu_Wpcom;
@@ -12,21 +14,39 @@ require_once Jetpack_Mu_Wpcom::PKG_DIR . 'src/features/crawler-control/class-cra
  */
 class Crawler_Control_Test extends \WorDBless\BaseTestCase {
 
-	public function setUp(): void {
+	/**
+	 * Yes, comment is needed here.
+	 */
+	public function setUp(): void { //phpcs:ignore
 		parent::setUp();
 		add_filter( 'wp_die_handler', array( $this, 'wp_die_handler_filter' ) );
 	}
 
-	public function tearDown(): void {
+	/**
+	 * Yes, comment is needed here.
+	 */
+	public function tearDown(): void { //phpcs:ignore
 		parent::tearDown();
 		delete_option( Crawler_Control::OPTION_NAME );
 		remove_filter( 'wp_die_handler', array( $this, 'wp_die_handler_filter' ) );
 	}
 
-	public function wp_die_handler_filter( $handler ) {
+	/**
+	 * This overrides the default wp_die_handler with the returned function name.
+	 * We will use this to handle wp_die in test environment.
+	 *
+	 * @return string
+	 */
+	public function wp_die_handler_filter() {
 		return 'wp_die_halt_handler';
 	}
 
+	/**
+	 * Returns a mock of Crawler_Control.
+	 *
+	 * @param string $user_agent The user agent.
+	 * @param bool   $is_frontend Whether the request is frontend or not.
+	 */
 	private function get_crawler_control( $user_agent = 'sentibot', $is_frontend = true ) {
 		$cc = $this->getMockBuilder( Crawler_Control::class )
 		->disableOriginalConstructor()
@@ -40,17 +60,26 @@ class Crawler_Control_Test extends \WorDBless\BaseTestCase {
 		return $cc;
 	}
 
+	/**
+	 * What it says
+	 */
 	public function test_default_crawler_state() {
 		$cc = $this->get_crawler_control();
 		$this->assertFalse( $cc->is_crawlable() );
 	}
 
+	/**
+	 * What it says
+	 */
 	public function test_option_changes_crawler_state() {
 		add_option( Crawler_Control::OPTION_NAME, 1 );
 		$cc = $this->get_crawler_control();
 		$this->assertTrue( (bool) $cc->is_crawlable() );
 	}
 
+	/**
+	 * What it says
+	 */
 	public function test_crawler_disables_GPTBot_by_default() {
 		$cc = $this->get_crawler_control( 'GPTBot', true );
 
@@ -58,6 +87,9 @@ class Crawler_Control_Test extends \WorDBless\BaseTestCase {
 		$cc->exit_for_bots_unless_permitted();
 	}
 
+	/**
+	 * What it says
+	 */
 	public function test_crawler_disables_sentibot_by_default() {
 		$cc = $this->get_crawler_control( 'sentibot', true );
 
@@ -65,6 +97,9 @@ class Crawler_Control_Test extends \WorDBless\BaseTestCase {
 		$cc->exit_for_bots_unless_permitted();
 	}
 
+	/**
+	 * What it says
+	 */
 	public function test_crawler_enables_sentibot_if_permitted() {
 		$this->expectNotToPerformAssertions();
 		add_option( Crawler_Control::OPTION_NAME, 1 );
@@ -73,6 +108,9 @@ class Crawler_Control_Test extends \WorDBless\BaseTestCase {
 		$cc->exit_for_bots_unless_permitted();
 	}
 
+	/**
+	 * What it says
+	 */
 	public function test_crawler_enables_chrome() {
 		$this->expectNotToPerformAssertions();
 		$cc = $this->get_crawler_control( 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36', true );
@@ -81,9 +119,9 @@ class Crawler_Control_Test extends \WorDBless\BaseTestCase {
 	}
 }
 
-class CrawlerControlDieException extends \Exception {}
+class CrawlerControlDieException extends \Exception {} //phpcs:ignore
 
-function wp_die_halt_handler( $message, $title, $args ) {
+function wp_die_halt_handler( $message, $title, $args ) { //phpcs:ignore
 	throw new \CrawlerControlDieException(
 		wp_json_encode(
 			array(
