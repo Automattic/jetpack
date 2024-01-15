c7cb06358cfc9f49d5caa45726463d49e078035c
Donncha O Caoimh <5656673+donnchawp@users.noreply.github.com>
2024-01-15 17:42:18 +0000

    Caching engine for Boost with debug logs

---
+++ projects/plugins/boost/app/modules/cache/cache.php
@@ -0,0 +1,81 @@
+<?php
+
+class Boost_Cache {
+	public $key         = false;
+	public $request_uri = false;
+
+	public function __construct() {
+		$this->key();
+		$this->request_uri = isset( $_SERVER['REQUEST_URI'] )
+			? $_SERVER['REQUEST_URI'] // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.ValidatedSanitizedInput.MissingUnslash
+			: false;
+
+		add_action( 'wp', array( $this, 'sanitize' ), 0 );
+	}
+
+	public function sanitize() {
+		if ( ! $this->request_uri ) {
+			return;
+		}
+
+		$this->request_uri = esc_url_raw( wp_unslash( $this->request_uri ) );
+	}
+
+	public function is_ok_to_cache() {
+		if ( $this->is_backend() ) {
+			error_log( "not caching a backend request: {$this->request_uri}" ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+			return false;
+		}
+
+		if ( ( isset( $_SERVER['REQUEST_METHOD'] ) && $_SERVER['REQUEST_METHOD'] === 'POST' ) ) {
+			error_log( "not caching a POST request: {$this->request_uri}" ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+			return false;
+		}
+
+		return apply_filters( 'boost_cache_cacheable', $this->request_uri );
+	}
+
+	public function key() {
+		if ( ! $this->key ) {
+			$cookies   = isset( $_COOKIE ) ? $_COOKIE : array();
+			$this->key = md5( json_encode( $cookies ) ); // phpcs:ignore WordPress.WP.AlternativeFunctions.json_encode_json_encode
+		}
+		return $this->key;
+	}
+
+	public function get() {
+		return false;
+	}
+
+	public function set( $data ) {
+		echo esc_html( $data );
+		return false;
+	}
+
+	public function ob_start() {
+		return false;
+	}
+
+	public function is_backend() {
+
+		$is_backend = is_admin();
+		if ( $is_backend ) {
+			return $is_backend;
+		}
+
+		$script = isset( $_SERVER['PHP_SELF'] ) ? basename( $_SERVER['PHP_SELF'] ) : ''; // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.ValidatedSanitizedInput.MissingUnslash
+		if ( $script !== 'index.php' ) {
+			if ( in_array( $script, array( 'wp-login.php', 'xmlrpc.php', 'wp-cron.php' ), true ) ) {
+				$is_backend = true;
+			} elseif ( defined( 'DOING_CRON' ) && DOING_CRON ) {
+				$is_backend = true;
+			} elseif ( PHP_SAPI === 'cli' || ( defined( 'WP_CLI' ) && WP_CLI ) ) {
+				$is_backend = true;
+			}
+		} elseif ( defined( 'REST_REQUEST' ) ) {
+			$is_backend = true;
+		}
+
+		return $is_backend;
+	}
+}
---
+++ projects/plugins/boost/app/modules/cache/file_cache.php
@@ -0,0 +1,71 @@
+<?php
+
+require_once __DIR__ . '/cache.php';
+
+class Boost_File_Cache extends Boost_Cache {
+	private $path;
+	private $cache_file;
+
+	public function __construct() {
+		parent::__construct();
+		error_log( 'request_uri: ' . $this->request_uri ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+		error_log( 'sanitize_path: ' . $this->sanitize_path( $this->request_uri ) ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+		$this->path       = WP_CONTENT_DIR . '/boost-cache/cache' . parse_url( $this->sanitize_path( $this->request_uri ), PHP_URL_PATH ); // phpcs:ignore WordPress.WP.AlternativeFunctions.parse_url_parse_url
+		$this->cache_file = $this->path . $this->key() . '.php';
+	}
+
+	public function sanitize_path( $path ) {
+		$path = str_replace( '/', DIRECTORY_SEPARATOR, $path );
+		$path = preg_replace( '/[ <>\'\"\r\n\t\(\)]/', '', $path );
+		$path = str_replace( array( '//', '..' ), array( '/', '' ), $path );
+		return $path;
+	}
+
+	public function get() {
+		if ( ! $this->is_cacheable() ) {
+			return false;
+		}
+
+		if ( file_exists( $this->cache_file ) ) {
+			error_log( 'got cache from ' . $this->path . ' for ' . $this->request_uri ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+			// phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents, WordPress.Security.EscapeOutput.OutputNotEscaped
+			echo file_get_contents( $this->cache_file ) . '<!-- cached -->';
+			die();
+		}
+		return false;
+	}
+
+	private function createCacheDirectory( $path ) {
+		if ( ! is_dir( $path ) ) {
+			// phpcs:ignore WordPress.WP.AlternativeFunctions.dir_mkdir_dirname, WordPress.WP.AlternativeFunctions.file_system_operations_mkdir
+			mkdir( $path, 0755, true );
+		}
+	}
+
+	public function set( $data ) {
+		if ( ! $this->is_cacheable() ) {
+			return false;
+		}
+
+		error_log( 'saving: ' . $this->path . ' for ' . $this->request_uri ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+		$this->createCacheDirectory( dirname( $this->cache_file ) );
+		// phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_operations_file_put_contents
+		file_put_contents( $this->cache_file, $data );
+		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- cached page that has already been escaped
+		echo $data;
+		die();
+	}
+
+	public function ob_start() {
+		if ( ! $this->is_cacheable() ) {
+			return false;
+		}
+
+		ob_start( array( $this, 'set' ) );
+	}
+}
+
+$boost_cache = new Boost_File_Cache();
+if ( ! $boost_cache->get() ) {
+	$boost_cache->ob_start();
+}
