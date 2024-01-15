f729e00769e16c047f5e3bdb5db0f58c130d5749
pkuliga <pkuliga@gmail.com>
2024-01-15 13:42:12 +0100

    link URLs

--- projects/plugins/jetpack/extensions/blocks/subscriber-login/subscriber-login.php
+++ projects/plugins/jetpack/extensions/blocks/subscriber-login/subscriber-login.php
@@ -21,19 +21,30 @@ use Jetpack_Memberships;
 function register_block() {
 	Blocks::jetpack_register_block(
 		__DIR__,
-		array( 'render_callback' => __NAMESPACE__ . '\load_assets' )
+		array( 'render_callback' => __NAMESPACE__ . '\render_block' )
 	);
 }
 add_action( 'init', __NAMESPACE__ . '\register_block' );
 
 /**
- * Subscriber Login block registration/dependency declaration.
+ * Returns current URL
+ */
+function get_current_url() {
+	if ( ! isset( $_SERVER['HTTP_HOST'] ) || ! isset( $_SERVER['REQUEST_URI'] ) ) {
+		return '';
+	}
+
+	return ( is_ssl() ? 'https://' : 'http://' ) . wp_unslash( $_SERVER['HTTP_HOST'] ) . wp_unslash( $_SERVER['REQUEST_URI'] ); // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+}
+
+/**
+ * Renders Subscriber Login block
  *
  * @param array $attr    Array containing the Subscriber Login block attributes.
  *
  * @return string
  */
-function load_assets( $attr ) {
+function render_block( $attr ) {
 	/*
 	 * Enqueue necessary scripts and styles.
 	 */
@@ -41,23 +52,26 @@ function load_assets( $attr ) {
 
 	if ( ! is_user_logged_in() ) {
 		return sprintf(
-			'<div class="%1$s"><a href="#">%2$s</a></div>',
+			'<div class="%1$s"><a href="%2$s">%3$s</a></div>',
 			esc_attr( Blocks::classes( Blocks::get_block_feature( __DIR__ ), $attr ) ),
+			wp_loginout( get_current_url(), false ),
 			__( 'Log in', 'jetpack' )
 		);
 	}
 
 	if ( Jetpack_Memberships::is_current_user_subscribed() ) {
 		return sprintf(
-			'<div class="%1$s"><a href="#">%2$s</a></div>',
+			'<div class="%1$s"><a href="%2$s">%3$s</a></div>',
 			esc_attr( Blocks::classes( Blocks::get_block_feature( __DIR__ ), $attr ) ),
+			'https://wordpress.com/read/subscriptions',
 			__( 'Manage subscriptions', 'jetpack' )
 		);
 	}
 
 	return sprintf(
-		'<div class="%1$s"><a href="#">%2$s</a></div>',
+		'<div class="%1$s"><a href="%2$s">%3$s</a></div>',
 		esc_attr( Blocks::classes( Blocks::get_block_feature( __DIR__ ), $attr ) ),
+		wp_loginout( get_current_url(), false ),
 		__( 'Log out', 'jetpack' )
 	);
 }
