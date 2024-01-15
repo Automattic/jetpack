977cd9f7aac938773e551da21bfdd2ca0cf72f18
pkuliga <pkuliga@gmail.com>
2024-01-15 11:57:39 +0100

    remove not used stuff

--- projects/plugins/jetpack/extensions/blocks/subscriber-login/edit.js
+++ projects/plugins/jetpack/extensions/blocks/subscriber-login/edit.js
@@ -1,33 +1,12 @@
-import { getBlockIconComponent } from '@automattic/jetpack-shared-extension-utils';
-import { BlockIcon } from '@wordpress/block-editor';
-import { Placeholder, withNotices } from '@wordpress/components';
-import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-import metadata from './block.json';
 import './editor.scss';
 
-const icon = getBlockIconComponent( metadata );
-
-function SubscriberLoginEdit( {
-	attributes,
-	className,
-	noticeOperations,
-	noticeUI,
-	setAttributes,
-} ) {
+function SubscriberLoginEdit() {
 	/**
 	 * Write the block editor UI.
 	 *
 	 * @returns {object} The UI displayed when user edits this block.
 	 */
-	const [ notice, setNotice ] = useState();
-
-	/* Call this function when you want to show an error in the placeholder. */
-	const setErrorNotice = () => {
-		noticeOperations.removeAllNotices();
-		noticeOperations.createErrorNotice( __( 'Put error message here.', 'jetpack' ) );
-	};
-
 	return (
 		<div>
 			<a href="#">{ __( 'Log out', 'jetpack' ) }</a>
