be38e14097b355b8f488eb2542aa575172152154
Gabriel Caires <gabriel.caires@automattic.com>
2024-01-16 11:08:46 +0000

    Launchpad: Enable to temporary dismiss a dismissible launchpad (#34889)
    * Add the is_dismissible prop to the Launchpad definition
    
    * changelog
    
    * Check for empty checklist_slug
    
    * Add dismissed_until field
    
    * changelog
    
    * Bump version
    
    * Changelog
    
    * Add is_temporally_dismissed method
    
    * Add description to dimissed_until field
    
    * Fix dismiss logic
    
    * Enable to temporally dismiss a single checklist
    
    * Bump version
    
    * Remove is_dismissed_until field from the endpoint
    
    * Use relative dismiss values
    
    * Improve test names
    
    ---------
    
    Co-authored-by: Valter Lorran <valter.lorran@automattic.com>

--- projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
+++ projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
@@ -13,7 +13,6 @@ namespace Automattic\Jetpack;
  * Jetpack_Mu_Wpcom main class.
  */
 class Jetpack_Mu_Wpcom {
-
 	const PACKAGE_VERSION = '5.9.0-alpha';
 	const PKG_DIR         = __DIR__ . '/../';
 	const BASE_DIR        = __DIR__ . '/';
