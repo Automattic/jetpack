2f5bf512edf299323b2be3afafa8b369dee0d520
cleacos <javacodia@gmail.com>
2024-01-16 22:50:43 +0000

    Create the JetpackManageBanner component and add it to My Jetpack

---
+++ projects/packages/my-jetpack/_inc/components/upsell-banner/jetpack-manage-banner/index.jsx
@@ -0,0 +1,47 @@
+import { getRedirectUrl } from '@automattic/jetpack-components';
+import { __ } from '@wordpress/i18n';
+import UpsellBanner from '../';
+import jetpackManageIcon from './jetpack-manage.svg';
+
+/**
+ * Jetpack Manager Banner component that renders a banner with CTAs.
+ *
+ * @param {object} props - Component props.
+ * @returns {object} The JetpackManageBanner component.
+ */
+const JetpackManageBanner = props => {
+	const { isAgencyAccount } = props;
+
+	// Set up the first CTA
+	const cta1Label = __( 'Learn more', 'jetpack-my-jetpack' );
+	const cta1WithIcon = true;
+	const cta1URL = getRedirectUrl( 'my-jetpack-jetpack-manage-learn-more' );
+
+	// Set up the second CTA
+	const cta2Label = isAgencyAccount
+		? __( 'Manage sites', 'jetpack-my-jetpack' )
+		: __( 'Sign up for free', 'jetpack-my-jetpack' );
+	const cta2WithIcon = true;
+	const cta2URL = isAgencyAccount
+		? getRedirectUrl( 'my-jetpack-jetpack-manage-dashboard' )
+		: getRedirectUrl( 'my-jetpack-jetpack-manage-sign-up' );
+
+	return (
+		<UpsellBanner
+			icon={ jetpackManageIcon }
+			title={ __( 'Jetpack Manage', 'jetpack-my-jetpack' ) }
+			description={ __(
+				'Jetpack Manage has the tools you need to manage multiple WordPress sites. Monitor site security, performance, and traffic, and get alerted if a site needs attention.',
+				'jetpack-my-jetpack'
+			) }
+			cta1Label={ cta1Label }
+			cta1WithIcon={ cta1WithIcon }
+			cta1URL={ cta1URL }
+			cta2Label={ cta2Label }
+			cta2WithIcon={ cta2WithIcon }
+			cta2URL={ cta2URL }
+		/>
+	);
+};
+
+export default JetpackManageBanner;
