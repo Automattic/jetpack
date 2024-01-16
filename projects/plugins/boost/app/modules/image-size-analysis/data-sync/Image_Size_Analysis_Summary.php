66317b5070ebb45ee94ba406970163392db102f4
Nauris Pūķis <hi@pyronaur.com>
2024-01-16 17:34:08 +0200

    Throw an exception when a report is not found

--- projects/plugins/boost/app/modules/image-size-analysis/data-sync/Image_Size_Analysis_Summary.php
+++ projects/plugins/boost/app/modules/image-size-analysis/data-sync/Image_Size_Analysis_Summary.php
@@ -17,9 +17,7 @@ class Image_Size_Analysis_Summary implements Lazy_Entry, Entry_Can_Get {
 		if ( is_wp_error( $report ) ) {
 			// If no report is found, return it as a status.
 			if ( $report->get_error_code() === 'report-not-found' ) {
-				return array(
-					'status' => 'not-found',
-				);
+				throw new \RuntimeException( 'Report not found' );
 			}
 
 			// Other kinds of errors are a problem.
