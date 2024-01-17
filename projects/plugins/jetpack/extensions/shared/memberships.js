f55ef19d42d8c5f9ecfbb79a7b39438f277bbdbe
Christopher Smith <chris@chriswsmith.info>
2024-01-17 12:10:19 -0600

    Alter modal URL to use language

--- projects/plugins/jetpack/extensions/shared/memberships.js
+++ projects/plugins/jetpack/extensions/shared/memberships.js
@@ -41,10 +41,12 @@ export function showModal( url ) {
 	dialog.setAttribute( 'id', 'memberships-modal-window' );
 
 	const iframe = document.createElement( 'iframe' );
+	const siteLanguage = document.documentElement.lang;
 	iframe.setAttribute( 'id', 'memberships-modal-iframe' );
 	iframe.innerText =
 		'This feature requires inline frames. You have iframes disabled or your browser does not support them.';
-	iframe.src = url + '&display=alternate&jwt_token=' + getTokenFromCookie();
+	iframe.src =
+		url + '&display=alternate&language=' + siteLanguage + '&jwt_token=' + getTokenFromCookie();
 	iframe.setAttribute( 'frameborder', '0' );
 	iframe.setAttribute( 'allowtransparency', 'true' );
 	iframe.setAttribute( 'allowfullscreen', 'true' );
