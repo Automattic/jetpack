9bb34a683b2fc08bd45889c0da61b21a6e9f54ca
cleacos <javacodia@gmail.com>
2024-01-17 09:27:53 +0000

    Update JetpackManageBanner with a better variable names.

--- projects/packages/my-jetpack/_inc/components/upsell-banner/jetpack-manage-banner/index.jsx
+++ projects/packages/my-jetpack/_inc/components/upsell-banner/jetpack-manage-banner/index.jsx
@@ -13,19 +13,15 @@ const JetpackManageBanner = props => {
 	const { isAgencyAccount } = props;
 
 	// Set up the first CTA
-	const cta1Label = __( 'Learn more', 'jetpack-my-jetpack' );
-	const cta1WithIcon = true;
-	const cta1URL = getRedirectUrl( 'my-jetpack-jetpack-manage-learn-more' );
-
-	const manageSitesBtnLabel = __( 'Manage sites', 'jetpack-my-jetpack' );
-	const signUpForFreeBtnLabel = __( 'Sign up for free', 'jetpack-my-jetpack' );
+	const ctaLearnMoreLabel = __( 'Learn more', 'jetpack-my-jetpack' );
+	const ctaLearnMoreUrl = getRedirectUrl( 'my-jetpack-jetpack-manage-learn-more' );
 
 	// Set up the second CTA
-	const cta2Label = isAgencyAccount ? manageSitesBtnLabel : signUpForFreeBtnLabel;
-	const cta2WithIcon = true;
-	const cta2URL = isAgencyAccount
-		? getRedirectUrl( 'my-jetpack-jetpack-manage-dashboard' )
-		: getRedirectUrl( 'my-jetpack-jetpack-manage-sign-up' );
+	const ctaManageSitesLabel = __( 'Manage sites', 'jetpack-my-jetpack' );
+	const ctaManageSitesUrl = getRedirectUrl( 'my-jetpack-jetpack-manage-dashboard' );
+
+	const ctaSignUpForFreeLabel = __( 'Sign up for free', 'jetpack-my-jetpack' );
+	const ctaSignUpForFreeUrl = getRedirectUrl( 'my-jetpack-jetpack-manage-sign-up' );
 
 	return (
 		<UpsellBanner
@@ -35,12 +31,12 @@ const JetpackManageBanner = props => {
 				'Jetpack Manage has the tools you need to manage multiple WordPress sites. Monitor site security, performance, and traffic, and get alerted if a site needs attention.',
 				'jetpack-my-jetpack'
 			) }
-			cta1Label={ cta1Label }
-			cta1WithIcon={ cta1WithIcon }
-			cta1URL={ cta1URL }
-			cta2Label={ cta2Label }
-			cta2WithIcon={ cta2WithIcon }
-			cta2URL={ cta2URL }
+			cta1Label={ ctaLearnMoreLabel }
+			cta1URL={ ctaLearnMoreUrl }
+			cta1IsExternalLink={ true }
+			cta2Label={ isAgencyAccount ? ctaManageSitesLabel : ctaSignUpForFreeLabel }
+			cta2URL={ isAgencyAccount ? ctaManageSitesUrl : ctaSignUpForFreeUrl }
+			cta2IsExternalLink={ true }
 		/>
 	);
 };
