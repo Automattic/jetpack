56145a661424164f99c09fb8374f1c348699177d
Adnan Haque <adnan007.id@gmail.com>
2024-01-17 10:49:38 +0600

    Update compatibility

--- projects/plugins/boost/compatibility/score-prompt.php
+++ projects/plugins/boost/compatibility/score-prompt.php
@@ -4,11 +4,19 @@
  *
  * @package automattic/jetpack-boost
  */
-
-$old_value = get_option( 'jb_show_score_prompt' );
+// Old value is the previous DS key, fallback to even older non-ds value.
+$old_value = get_option( 'jetpack_boost_ds_dismissed_score_prompt', get_option( 'jb_show_score_prompt' ) );
 if ( false !== $old_value ) {
-	$ds_value = get_option( 'jetpack_boost_ds_dismissed_score_prompt' );
-	if ( false === $ds_value ) {
-		add_option( 'jetpack_boost_ds_dismissed_score_prompt', $old_value );
+	$new_value = get_option( 'jetpack_boost_ds_dismissed_alerts' );
+
+	if ( in_array( 'score-increase', $old_value, true ) ) {
+		$new_value['score_increase'] = true;
 	}
+	if ( in_array( 'score-decrease', $old_value, true ) ) {
+		$new_value['score_decrease'] = true;
+	}
+
+	update_option( 'jetpack_boost_ds_dismissed_alerts', $new_value, false );
+	delete_option( 'jetpack_boost_ds_dismissed_score_prompt' );
+	delete_option( 'jb_show_score_prompt' );
 }
