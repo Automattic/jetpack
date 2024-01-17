46f0754a1935bcce62a5bfe7b37a199eeecde915
Nauris Pūķis <hi@pyronaur.com>
2024-01-17 19:44:17 +0200

    Improve test syntax

--- projects/packages/wp-js-data-sync/tests/php/schema/integration/test-integration-fallback-values.php
+++ projects/packages/wp-js-data-sync/tests/php/schema/integration/test-integration-fallback-values.php
@@ -97,18 +97,19 @@ class Test_Integration_Fallback_Values extends TestCase {
 		$entry  = new Data_Sync_Entry_Adapter( new Data_Sync_Option( $key ), $schema );
 
 		// Test with a valid value
-		$this->assertSame( true, $entry->set( true ) );
-		$this->assertSame( true, $entry->set( 1 ) );
-		$this->assertSame( true, $entry->set( '1' ) );
-		$this->assertSame( false, $entry->set( false ) );
-		$this->assertSame( false, $entry->set( '' ) );
-		$this->assertSame( false, $entry->set( '0' ) );
-		$this->assertSame( false, $entry->set( 0 ) );
+		$this->assertTrue( $entry->set( true ) );
+		$this->assertTrue( $entry->set( 1 ) );
+		$this->assertTrue( $entry->set( '1' ) );
+		$this->assertFalse( $entry->set( false ) );
+		$this->assertFalse( $entry->set( '' ) );
+		$this->assertFalse( $entry->set( '0' ) );
+		$this->assertFalse( $entry->set( 0 ) );
 
 		// Entry should default to true
 		// because the fallback is set.
 		$entry->delete();
 		$this->assertSame( true, $entry->get() );
+
 	}
 
 	public function test_wordpress_option_no_fallback() {
