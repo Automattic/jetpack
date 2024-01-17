5330d3ba446016243a1f4ada608fc5332b1cd505
pkuliga <pkuliga@gmail.com>
2024-01-17 11:19:03 +0100

    add margin/padding support

--- projects/plugins/jetpack/extensions/blocks/subscriber-login/subscriber-login.php
+++ projects/plugins/jetpack/extensions/blocks/subscriber-login/subscriber-login.php
@@ -85,17 +85,16 @@ function get_subscriber_login_url() {
 /**
  * Renders Subscriber Login block.
  *
- * @param array $attr    Array containing the Subscriber Login block attributes.
- *
  * @return string
  */
-function render_block( $attr ) {
+function render_block() {
 	Jetpack_Gutenberg::load_assets_as_required( __DIR__ );
+
+	$block_template = '<div ' . get_block_wrapper_attributes() . '><a href="%1$s">%2$s</a></div>';
 
 	if ( ! is_user_logged_in() ) {
 		return sprintf(
-			'<div class="%1$s"><a href="%2$s">%3$s</a></div>',
-			esc_attr( Blocks::classes( Blocks::get_block_feature( __DIR__ ), $attr ) ),
+			$block_template,
 			get_subscriber_login_url(),
 			__( 'Log in', 'jetpack' )
 		);
@@ -103,16 +102,14 @@ function render_block( $attr ) {
 
 	if ( Jetpack_Memberships::is_current_user_subscribed() ) {
 		return sprintf(
-			'<div class="%1$s"><a href="%2$s">%3$s</a></div>',
-			esc_attr( Blocks::classes( Blocks::get_block_feature( __DIR__ ), $attr ) ),
+			$block_template,
 			'https://wordpress.com/read/subscriptions',
 			__( 'Manage subscriptions', 'jetpack' )
 		);
 	}
 
 	return sprintf(
-		'<div class="%1$s"><a href="%2$s">%3$s</a></div>',
-		esc_attr( Blocks::classes( Blocks::get_block_feature( __DIR__ ), $attr ) ),
+		$block_template,
 		wp_logout_url( get_current_url() ),
 		__( 'Log out', 'jetpack' )
 	);
