4eb31c2aa748729ac1dda457d97ece098fcb06a5
Nauris Pūķis <hi@pyronaur.com>
2024-01-18 16:14:50 +0200

    TMP: Create validation meta from within Decorate_With_Default

--- projects/packages/wp-js-data-sync/src/schema/modifiers/class-decorate-with-default.php
+++ projects/packages/wp-js-data-sync/src/schema/modifiers/class-decorate-with-default.php
@@ -5,6 +5,7 @@ namespace Automattic\Jetpack\WP_JS_Data_Sync\Schema\Modifiers;
 use Automattic\Jetpack\WP_JS_Data_Sync\Schema\Parser;
 use Automattic\Jetpack\WP_JS_Data_Sync\Schema\Schema_Internal_Error;
 use Automattic\Jetpack\WP_JS_Data_Sync\Schema\Schema_Parsing_Error;
+use Automattic\Jetpack\WP_JS_Data_Sync\Schema\Schema_Validation_Meta;
 
 /**
  * This takes in a Schema Type (like a String/Number/Boolean) and a default value.
@@ -21,11 +22,27 @@ class Decorate_With_Default implements Parser {
 	}
 
 	public function parse( $value, $meta = null ) {
+		// @TODO: This shouldn't actually be this way. WIP.
+		$meta = $meta ?? new Schema_Validation_Meta('decorated_with_default');
 		try {
 			return $this->parser->parse( $value, $meta );
 		} catch ( Schema_Internal_Error $e ) {
+			$meta->log(
+				'Returning default value',
+				array(
+					'parser'        => $this->parser->__toString(),
+					'default_value' => $this->default_value,
+				)
+			);
 			return $this->default_value;
 		} catch ( Schema_Parsing_Error $e ) {
+			$meta->log(
+				'Returning default value',
+				array(
+					'parser'        => $this->parser->__toString(),
+					'default_value' => $this->default_value,
+				)
+			);
 			return $this->default_value;
 		}
 	}
