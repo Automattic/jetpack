8fd3f76d14f27675db7226fdab4526c9f0e1beb3
pkuliga <pkuliga@gmail.com>
2024-01-22 22:56:12 +0100

    add settings

--- projects/plugins/jetpack/extensions/blocks/subscriber-login/edit.js
+++ projects/plugins/jetpack/extensions/blocks/subscriber-login/edit.js
@@ -1,9 +1,9 @@
 import { InspectorControls } from '@wordpress/block-editor';
-import { PanelBody, ToggleControl } from '@wordpress/components';
+import { PanelBody, ToggleControl, BaseControl, TextControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 function SubscriberLoginEdit( { attributes, setAttributes, className } ) {
-	const { redirectToCurrent } = attributes;
+	const { redirectToCurrent, logInLabel, logOutLabel, manageSubscriptionsLabel } = attributes;
 
 	return (
 		<>
@@ -14,6 +14,27 @@ function SubscriberLoginEdit( { attributes, setAttributes, className } ) {
 						checked={ redirectToCurrent }
 						onChange={ () => setAttributes( { redirectToCurrent: ! redirectToCurrent } ) }
 					/>
+					<BaseControl label={ __( 'Log in label', 'jetpack' ) }>
+						<TextControl
+							placeholder={ __( 'Log in link label', 'jetpack' ) }
+							onChange={ value => setAttributes( { logInLabel: value } ) }
+							value={ logInLabel }
+						/>
+					</BaseControl>
+					<BaseControl label={ __( 'Log out label', 'jetpack' ) }>
+						<TextControl
+							placeholder={ __( 'Log out link label', 'jetpack' ) }
+							onChange={ value => setAttributes( { logOutLabel: value } ) }
+							value={ logOutLabel }
+						/>
+					</BaseControl>
+					<BaseControl label={ __( 'Manage subscriptions label', 'jetpack' ) }>
+						<TextControl
+							placeholder={ __( 'Manage subscriptions link label', 'jetpack' ) }
+							onChange={ value => setAttributes( { manageSubscriptionsLabel: value } ) }
+							value={ manageSubscriptionsLabel }
+						/>
+					</BaseControl>
 				</PanelBody>
 			</InspectorControls>
 			<div className={ className }>
