489a5e19955cb2707d221176d4440964e008463c
pkuliga <pkuliga@gmail.com>
2024-01-22 12:35:55 +0100

    basic transforms

--- projects/plugins/jetpack/extensions/blocks/subscriber-login/editor.js
+++ projects/plugins/jetpack/extensions/blocks/subscriber-login/editor.js
@@ -1,8 +1,28 @@
+import { createBlock } from '@wordpress/blocks';
 import { registerJetpackBlockFromMetadata } from '../../shared/register-jetpack-block';
 import metadata from './block.json';
 import edit from './edit';
 
 registerJetpackBlockFromMetadata( metadata, {
 	edit,
-	save: () => null,
+	transforms: {
+		from: [
+			{
+				type: 'block',
+				blocks: [ 'core/loginout' ],
+				transform: () => {
+					return createBlock( 'jetpack/subscriber-login' );
+				},
+			},
+		],
+		to: [
+			{
+				type: 'block',
+				blocks: [ 'core/loginout' ],
+				transform: () => {
+					return createBlock( 'core/loginout' );
+				},
+			},
+		],
+	},
 } );
