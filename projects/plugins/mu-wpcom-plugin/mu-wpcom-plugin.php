81feb1bf310eb05b1b3d15513284a625ff9a09dc
Brad Jorsch <anomiex@users.noreply.github.com>
2024-01-23 09:26:16 -0500

    Revert "Transfer Verbum app into into jetpack-mu-wpcom"

--- projects/plugins/mu-wpcom-plugin/mu-wpcom-plugin.php
+++ projects/plugins/mu-wpcom-plugin/mu-wpcom-plugin.php
@@ -3,7 +3,7 @@
  *
  * Plugin Name: WordPress.com Features
  * Description: Test plugin for the jetpack-mu-wpcom package
- * Version: 2.0.15-alpha
+ * Version: 2.0.14
  * Author: Automattic
  * License: GPLv2 or later
  * Text Domain: jetpack-mu-wpcom-plugin
