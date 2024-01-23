fe8587f337438daa64147a41e88c5d1aab741dd6
Donncha O Caoimh <5656673+donnchawp@users.noreply.github.com>
2024-01-23 14:22:26 +0000

    Add 404 and feed exclusions to caching

--- projects/plugins/boost/app/modules/cache/Boost_Cache.php
+++ projects/plugins/boost/app/modules/cache/Boost_Cache.php
@@ -42,6 +42,14 @@ abstract class Boost_Cache {
 			return false;
 		}
 
+		if ( function_exists( 'is_404' ) && is_404() ) {
+			return false;
+		}
+
+		if ( function_exists( 'is_feed' ) && is_feed() ) {
+			return false;
+		}
+
 		if ( $this->is_backend() ) {
 			return false;
 		}
