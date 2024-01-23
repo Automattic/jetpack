69dd8420e3f239d34e0aca84e5d8f2f622c73308
Omar Alshaker <omar@omaralshaker.com>
2024-01-23 12:09:23 +0100

    Dont touch babel and webpack config for other projects

--- projects/packages/jetpack-mu-wpcom/babel.config.js
+++ projects/packages/jetpack-mu-wpcom/babel.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-	plugins: [
-		[
-			'@babel/plugin-transform-react-jsx',
-			{
-				pragma: 'h',
-				pragmaFrag: 'Fragment',
-			},
-		],
-	],
 	presets: [ [ '@automattic/jetpack-webpack-config/babel/preset' ] ],
 };
--- projects/packages/jetpack-mu-wpcom/webpack.config.js
+++ projects/packages/jetpack-mu-wpcom/webpack.config.js
@@ -2,86 +2,56 @@
 const path = require( 'path' );
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const jetpackConfig = require( '@automattic/jetpack-webpack-config/webpack' );
-const CopyPlugin = require( 'copy-webpack-plugin' );
-const webpack = require( 'webpack' );
+const verbumConfig = require( './verbum.webpack.config.js' );
 
-module.exports = {
-	entry: {
-		'error-reporting': './src/features/error-reporting/index.js',
-		'verbum-comments': './src/features/verbum-comments/src/index.tsx',
-		'block-theme-previews': './src/features/block-theme-previews/index.js',
-	},
-	mode: jetpackConfig.mode,
-	devtool: jetpackConfig.devtool,
-	output: {
-		...jetpackConfig.output,
-		filename: '[name]/[name].js',
-		path: path.resolve( __dirname, 'src/build' ),
-		environment: {
-			module: true,
-			dynamicImport: true,
+module.exports = [
+	verbumConfig,
+	{
+		entry: {
+			'error-reporting': './src/features/error-reporting/index.js',
+			'block-theme-previews': './src/features/block-theme-previews/index.js',
+		},
+		mode: jetpackConfig.mode,
+		devtool: jetpackConfig.devtool,
+		output: {
+			...jetpackConfig.output,
+			filename: '[name]/[name].js',
+			path: path.resolve( __dirname, 'src/build' ),
+		},
+		optimization: {
+			...jetpackConfig.optimization,
+		},
+		resolve: {
+			...jetpackConfig.resolve,
+		},
+		node: false,
+		plugins: [
+			...jetpackConfig.StandardPlugins( {
+				MiniCssExtractPlugin: { filename: '[name]/[name].css' },
+			} ),
+		],
+		module: {
+			strictExportPresence: true,
+			rules: [
+				// Transpile JavaScript.
+				jetpackConfig.TranspileRule( {
+					exclude: /node_modules\//,
+				} ),
+
+				// Transpile @automattic/jetpack-* in node_modules too.
+				jetpackConfig.TranspileRule( {
+					includeNodeModules: [ '@automattic/jetpack-' ],
+				} ),
+
+				// Handle CSS.
+				jetpackConfig.CssRule( {
+					extensions: [ 'css', 'scss' ],
+					extraLoaders: [ 'sass-loader' ],
+				} ),
+
+				// Handle images.
+				jetpackConfig.FileRule(),
+			],
 		},
 	},
-	optimization: {
-		...jetpackConfig.optimization,
-	},
-	resolve: {
-		...jetpackConfig.resolve,
-	},
-	node: false,
-	plugins: [
-		...jetpackConfig.StandardPlugins( {
-			DependencyExtractionPlugin: { injectPolyfill: true },
-			MiniCssExtractPlugin: { filename: '[name]/[name].css' },
-		} ),
-		new webpack.ProvidePlugin( {
-			h: [ 'preact', 'h' ],
-			Fragment: [ 'preact', 'Fragment' ],
-		} ),
-		new CopyPlugin( {
-			patterns: [
-				{
-					from: './src/features/verbum-comments/class-verbum-comments.php',
-					to: './verbum-comments',
-				},
-				{
-					from: './src/features/verbum-comments/assets',
-					to: './verbum-comments/assets',
-				},
-			],
-		} ),
-	],
-	module: {
-		strictExportPresence: true,
-		rules: [
-			// Transpile JavaScript.
-			jetpackConfig.TranspileRule( {
-				exclude: /node_modules\//,
-			} ),
-
-			// Transpile @automattic/jetpack-* in node_modules too.
-			jetpackConfig.TranspileRule( {
-				includeNodeModules: [ '@automattic/jetpack-' ],
-			} ),
-
-			// preact has some `__` internal methods, which confuse i18n-check-webpack-plugin. Hack around that.
-			jetpackConfig.TranspileRule( {
-				includeNodeModules: [ 'preact' ],
-				babelOpts: {
-					configFile: false,
-					plugins: [ [ 'babel-plugin-transform-rename-properties', { rename: { __: '__ǃ' } } ] ],
-					presets: [],
-				},
-			} ),
-
-			// Handle CSS.
-			jetpackConfig.CssRule( {
-				extensions: [ 'css', 'scss' ],
-				extraLoaders: [ 'sass-loader' ],
-			} ),
-
-			// Handle images.
-			jetpackConfig.FileRule(),
-		],
-	},
-};
+];
