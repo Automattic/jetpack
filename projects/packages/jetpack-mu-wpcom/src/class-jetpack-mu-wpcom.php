e89db3566ffcea92c99d357749e73baa1f4b9aa8
heavyweight <kpapazov@gmail.com>
2024-01-23 16:32:24 +0100

    Check before including wpforteams lib

--- projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
+++ projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
@@ -42,7 +42,10 @@ class Jetpack_Mu_Wpcom {
 
 		add_action( 'plugins_loaded', array( __CLASS__, 'load_first_posts_stream_helpers' ) );
 
-		add_action( 'plugins_loaded', array( __CLASS__, 'load_verbum_comments' ) );
+		// This feature runs only on simple sites
+		if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
+			add_action( 'plugins_loaded', array( __CLASS__, 'load_verbum_comments' ) );
+		}
 
 		// Unified navigation fix for changes in WordPress 6.2.
 		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'unbind_focusout_on_wp_admin_bar_menu_toggle' ) );
@@ -227,8 +230,13 @@ class Jetpack_Mu_Wpcom {
 	 * @param int $blog_id The blog ID.
 	 * @return boolean
 	 */
-	private function should_disable_comment_experience( $blog_id ) {
-		require_once WP_CONTENT_DIR . '/lib/wpforteams/functions.php';
+	private static function should_disable_comment_experience( $blog_id ) {
+		$path_wp_for_teams = WP_CONTENT_DIR . '/lib/wpforteams/functions.php';
+
+		if ( file_exists( $path_wp_for_teams ) ) {
+			require_once $path_wp_for_teams;
+		}
+
 		// This covers both P2 and P2020 themes.
 		$is_p2     = str_contains( get_stylesheet(), 'pub/p2' ) || function_exists( '\WPForTeams\is_wpforteams_site' ) && is_wpforteams_site( $blog_id );
 		$is_forums = str_contains( get_stylesheet(), 'a8c/supportforums' ); // Not in /forums
@@ -251,7 +259,7 @@ class Jetpack_Mu_Wpcom {
 			if ( isset( $_GET['blogid'] ) ) {
 				$blog_id = intval( $_GET['blogid'] ); // phpcs:ignore WordPress.Security.NonceVerification.Recommended
 			}
-			if ( should_disable_comment_experience( $blog_id ) ) {
+			if ( self::should_disable_comment_experience( $blog_id ) ) {
 				return false;
 			}
 			require_once __DIR__ . '/build/verbum-comments/class-verbum-comments.php';
