d5ff1420ee72ee3517376bf0b95f7f0ea4e59a9d
Brad Jorsch <anomiex@users.noreply.github.com>
2024-01-24 15:20:50 -0500

    webpack-config: Auto-set textdomain for I18nLoaderPlugin (#35231)
    We already pull the expected text domain for I18nCheckPlugin from
    composer.json in the usual case where it's not already set. Let's do the
    same for I18nLoaderPlugin.

--- projects/packages/search/src/class-package.php
+++ projects/packages/search/src/class-package.php
@@ -11,7 +11,7 @@ namespace Automattic\Jetpack\Search;
  * Search package general information
  */
 class Package {
-	const VERSION = '0.41.1';
+	const VERSION = '0.41.2-alpha';
 	const SLUG    = 'search';
 
 	/**
