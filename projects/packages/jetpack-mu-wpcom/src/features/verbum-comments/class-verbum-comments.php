cdc49b999198b8d2a01ace8106873a42415cc2c2
heavyweight <kpapazov@gmail.com>
2024-01-25 13:07:00 +0100

    Ensure block comments are always sanitized on save and render

--- projects/packages/jetpack-mu-wpcom/src/features/verbum-comments/class-verbum-comments.php
+++ projects/packages/jetpack-mu-wpcom/src/features/verbum-comments/class-verbum-comments.php
@@ -64,6 +64,8 @@ class Verbum_Comments {
 
 		// After the comment is saved, we add meta data to the comment.
 		add_action( 'comment_post', array( $this, 'add_verbum_meta_data' ) );
+		add_filter( 'pre_comment_content', array( $this, 'remove_blocks' ) );
+		add_filter( 'comment_text', array( $this, 'render_verbum_blocks' ) );
 
 		// Load the Gutenberg editor for comments.
 		if (
@@ -71,6 +73,118 @@ class Verbum_Comments {
 		) {
 			new \Verbum_Gutenberg_Editor();
 		}
+	}
+
+	/**
+	 * Helper function to filter blocks that are not allowed recursively.
+	 *
+	 * @param array[] $blocks the blocks to be filtered.
+	 * @return array[] the filtered blocks.
+	 */
+	private function filter_blocks_recursive( $blocks ) {
+		$filtered_blocks = array();
+
+		$allowed_blocks = self::get_allowed_blocks();
+		foreach ( $blocks as $block ) {
+			if ( in_array( $block['blockName'], $allowed_blocks, true ) ) {
+				$filtered_block = $block;
+
+				// Filter possible NULL values from innerContent to avoid parsing issues
+				if ( isset( $block['innerContent'] ) && is_array( $block['innerContent'] ) ) {
+					$filtered_block['innerContent'] = array_filter(
+						$filtered_block['innerContent'],
+						function ( $value ) {
+							return $value !== null;
+						}
+					);
+				}
+
+				// Recursively apply the filtering to innerBlocks
+				if ( isset( $block['innerBlocks'] ) && is_array( $block['innerBlocks'] ) ) {
+					$filtered_block['innerBlocks'] = $this->filter_blocks_recursive( $block['innerBlocks'] );
+				}
+
+				$filtered_blocks[] = $filtered_block;
+			}
+		}
+		return $filtered_blocks;
+	}
+
+	/**
+	 * Render blocks in the comment content
+	 * Filters blocks that aren't allowed
+	 *
+	 * @param string $comment_content - Text of the comment.
+	 * @return string
+	 */
+	public function render_verbum_blocks( $comment_content ) {
+		if ( ! has_blocks( $comment_content ) ) {
+			return $comment_content;
+		}
+
+		$blocks          = parse_blocks( $comment_content );
+		$filtered_blocks = $this->filter_blocks_recursive( $blocks );
+
+		$comment_content = '';
+
+		foreach ( $filtered_blocks as $block ) {
+			$comment_content .= render_block( $block );
+		}
+		return $comment_content;
+	}
+
+	/**
+	 * Remove blocks that aren't allowed
+	 *
+	 * @param string $content - Text of the comment.
+	 * @return string
+	 */
+	public function remove_blocks( $content ) {
+		if ( ! has_blocks( $content ) ) {
+			return $content;
+		}
+
+		// The block attributes come slashed and `parse_blocks` won't be able to parse them.
+		$content         = wp_unslash( $content );
+		$blocks          = parse_blocks( $content );
+		$filtered_blocks = $this->filter_blocks_recursive( $blocks );
+
+		$output = '';
+
+		foreach ( $filtered_blocks as $block ) {
+			$output .= serialize_block( $block );
+		}
+
+		return ltrim( $output );
+	}
+
+	/**
+	 * Get a list of allowed blocks by looking at the allowed comment tags
+	 *
+	 * @return string[]
+	 */
+	public static function get_allowed_blocks() {
+		global $allowedtags;
+
+		$allowed_blocks = array( 'core/paragraph', 'core/list', 'core/code', 'core/list-item', 'core/quote', 'core/image', 'core/embed' );
+		$convert        = array(
+			'blockquote' => 'core/quote',
+			'h1'         => 'core/heading',
+			'h2'         => 'core/heading',
+			'h3'         => 'core/heading',
+			'img'        => 'core/image',
+			'ul'         => 'core/list',
+			'ol'         => 'core/list',
+			'pre'        => 'core/code',
+		);
+
+		foreach ( array_keys( $allowedtags ) as $tag ) {
+			if ( isset( $convert[ $tag ] ) ) {
+				$allowed_blocks[] = $convert[ $tag ];
+			}
+		}
+
+		return $allowed_blocks;
 	}
 
 	/**
@@ -236,7 +350,7 @@ class Verbum_Comments {
 					'enableSubscriptionModal'           => boolval( $this->should_show_subscription_modal() ),
 					'currentLocale'                     => $locale,
 					'isJetpackComments'                 => is_jetpack_comments(),
-					'allowedBlocks'                     => \Verbum_Gutenberg_Editor::get_allowed_blocks(),
+					'allowedBlocks'                     => self::get_allowed_blocks(),
 					'embedNonce'                        => wp_create_nonce( 'embed_nonce' ),
 					'verbumBundleUrl'                   => plugins_url( 'dist/index.js', __FILE__ ),
 					'isRTL'                             => is_rtl( $locale ),
