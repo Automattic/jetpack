98de13261f169ff4f51b80a5e4c0d234fcaca06c
Omar Alshaker <omar@omaralshaker.com>
2024-01-25 12:29:20 +0100

    Fix Verbum cache buster

--- projects/packages/jetpack-mu-wpcom/src/features/verbum-comments/class-verbum-comments.php
+++ projects/packages/jetpack-mu-wpcom/src/features/verbum-comments/class-verbum-comments.php
@@ -174,7 +174,9 @@ class Verbum_Comments {
 		$post_id = isset( $_GET['postid'] ) ? intval( $_GET['postid'] ) : get_queried_object_id();
 		$locale  = get_locale();
 
-		$vbe_cache_buster = filemtime( ABSPATH . '/widgets.wp.com/verbum-block-editor/build_meta.json' );
+		$js_mtime         = filemtime( ABSPATH . '/widgets.wp.com/verbum-block-editor/block-editor.css' );
+		$css_mtime        = filemtime( ABSPATH . '/widgets.wp.com/verbum-block-editor/block-editor.min.js' );
+		$vbe_cache_buster = max( $js_mtime, $css_mtime );
 
 		wp_add_inline_script(
 			'verbum-settings',
