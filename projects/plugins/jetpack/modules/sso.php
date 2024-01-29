58b0a7c8ceeac54cc27bcd7072071365be3a5e06
Renan Carvalho <renansscarvalho@gmail.com>
2024-01-29 17:01:30 +0100

    Invite user message: Fix translation (#35304)
    * Fix translation
    
    * Add changelog
    
    * Add wp_kses

--- projects/plugins/jetpack/modules/sso.php
+++ projects/plugins/jetpack/modules/sso.php
@@ -219,13 +219,18 @@ class Jetpack_SSO {
 		// Enqueue the CSS for the admin create user page.
 		wp_enqueue_style( 'jetpack-sso-admin-create-user', plugins_url( 'modules/sso/jetpack-sso-admin-create-user.css', JETPACK__PLUGIN_FILE ), array(), time() );
 
-		$message = sprintf(
-			// translators: %s is a link to jetpack support site.
-			__( 'New users will receive an invite to join WordPress.com, so they can log in securely using %s', 'jetpack' ),
-			sprintf(
-				'<a class="jetpack-sso-admin-create-user-invite-message-link-sso" rel="noopener noreferrer" target="_blank" href="%s">%s</a>',
-				'https://jetpack.com/support/sso/',
-				__( 'Secure Sign On.', 'jetpack' )
+		$message = wp_kses(
+			__(
+				'New users will receive an invite to join WordPress.com, so they can log in securely using <a class="jetpack-sso-admin-create-user-invite-message-link-sso" rel="noopener noreferrer" target="_blank" href="https://jetpack.com/support/sso/">Secure Sign On</a>.',
+				'jetpack'
+			),
+			array(
+				'a' => array(
+					'class'  => array(),
+					'href'   => array(),
+					'rel'    => array(),
+					'target' => array(),
+				),
 			)
 		);
 		wp_admin_notice(
