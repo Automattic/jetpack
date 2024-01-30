d306b9414af1fb26d897aa9b7a8e958b21cc5a58
Adnan Haque <adnan007.id@gmail.com>
2024-01-30 15:26:50 +0600

    Fix function call

--- projects/packages/boost-speed-score/src/class-speed-score.php
+++ projects/packages/boost-speed-score/src/class-speed-score.php
@@ -320,7 +320,7 @@ class Speed_Score {
 			);
 
 			// Only include noBoost scores if at least one module is enabled.
-			if ( $score_request->active_modules ) {
+			if ( ! empty( $score_request->get_active_modules() ) ) {
 				$response['scores']['noBoost'] = $history_no_boost->latest_scores();
 			}
 
