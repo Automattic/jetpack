95be7305703d7e5180ff3680fae6b2b825bfcd21
DustyReagan <dusty@dustyreagan.com>
2024-01-30 18:15:05 -0600

    Add import_page_customizations_init

--- projects/packages/jetpack-mu-wpcom/src/features/import-customizations/import-customizations.php
+++ projects/packages/jetpack-mu-wpcom/src/features/import-customizations/import-customizations.php
@@ -5,30 +5,38 @@
  * @package automattic/jetpack-mu-wpcom
  */
 
-require_once __DIR__ . '/../../utils.php';
+/**
+ * Only add_action if the current screen is the wp-admin/import.php page.
+ */
+function import_page_customizations_init() {
+	$screen = get_current_screen();
+	if ( $screen->id === 'import' ) {
+		add_action( 'admin_notices', 'import_admin_banner' );
+		add_action( 'admin_enqueue_scripts', 'import_admin_banner_css' );
+	}
+}
+add_action( 'current_screen', 'import_page_customizations_init' );
 
 /**
  * Displays a banner on the wp-admin/import.php page that links to the Calypso importer.
  */
 function import_admin_banner() {
-	if ( $GLOBALS['pagenow'] === 'import.php' ) {
-		$import_url = '/setup/import-focused/import?siteSlug=' . wpcom_get_site_slug();
-		echo '<div id="wpcom-import-banner" class="notice">';
-		echo '<p>Import your content with WordPress.com’s guided importer. Designed for seamless integration from multiple platforms.</p>';
-		echo '<a href="' . esc_url( $import_url ) . '" class="button">Start Importing</a>';
-		echo '</div>';
-	}
+	require_once __DIR__ . '/../../utils.php';
+	$import_url = '/setup/import-focused/import?siteSlug=' . wpcom_get_site_slug();
+	echo '<div id="wpcom-import-banner" class="notice">';
+	echo '<p>Import your content with WordPress.com’s guided importer. Designed for seamless integration from multiple platforms.</p>';
+	echo '<a href="' . esc_url( $import_url ) . '" class="button">Start Importing</a>';
+	echo '</div>';
 }
-add_action( 'admin_notices', 'import_admin_banner' );
 
 /**
  * Enqueues CSS for the wp-admin/import.php Calypso import banner.
  */
 function import_admin_banner_css() {
-	if ( $GLOBALS['pagenow'] === 'import.php' ) {
-		// Cache-bust the CSS file using the file modification time.
-		$version = filemtime( plugin_dir_path( __FILE__ ) . 'css/import-customizations.css' );
+	$css_file_path = plugin_dir_path( __FILE__ ) . 'css/import-customizations.css';
+
+	if ( file_exists( $css_file_path ) ) {
+		$version = filemtime( $css_file_path );
 		wp_enqueue_style( 'import_admin_banner_css', plugin_dir_url( __FILE__ ) . 'css/import-customizations.css', array(), $version );
 	}
 }
-add_action( 'admin_enqueue_scripts', 'import_admin_banner_css' );
