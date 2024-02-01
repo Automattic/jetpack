df9879477884ea6f2cad131c7622146263ea42b1
vykes-mac <47489215+vykes-mac@users.noreply.github.com>
2024-01-31 20:07:24 -0500

    Github Deployments i1: Add filter to show Github deployments menu for specified developers https://github.com/Automattic/wpcomsh/pull/1672
    * add filter to show gihub deployments menu for developers
    
    * fix php lint

--- projects/plugins/wpcomsh/feature-plugins/github-deploy.php
+++ projects/plugins/wpcomsh/feature-plugins/github-deploy.php
@@ -38,3 +38,32 @@ function wpcomsh_bump_github_deploy_stats( $bin ) {
 		error_log( 'WPComSH: ' . $result->get_error_message() ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions
 	}
 }
+
+/**
+ * Jetpack adds the github deployments menu to WoA sites depending on the result of the
+ * jetpack_show_wpcom_github_deployments_menu filter. During development only particular
+ * users can see this menu. At launch time this filter will return true for
+ * all users.
+ *
+ * @return bool true if the current user should see the github deployments menu
+ */
+function wpcomsh_should_show_wpcom_github_deployments_menu() {
+	if ( defined( 'WPCOMSH_SHOW_WPCOM_github_deployments_MENU' ) ) {
+		return boolval( WPCOMSH_SHOW_WPCOM_GITHUB_DEPLOYMENTS_MENU );
+	}
+
+	// Using user_login rather than ID here because user IDs on Atomic sites don't
+	// always match WPCOM user IDs. Logins might not be fullproof either, but
+	// they're probably better.
+	$allowed_users = array(
+		'mk9287',
+		'philipmjackson',
+		'vykesmac',
+		'zaguiini',
+		'paulopmt1',
+		'jeroenpfeil',
+	);
+
+	return in_array( wp_get_current_user()->get( 'user_login' ), $allowed_users, true );
+}
+add_filter( 'jetpack_show_wpcom_github_deployments_menu', 'wpcomsh_should_show_wpcom_github_deployments_menu' );
