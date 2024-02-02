cb849193c823b0f31535881a9eb142a408178f73
Addison-Stavlo <stavz01@gmail.com>
2024-02-02 10:53:10 -0500

    add comments

--- projects/packages/jetpack-mu-wpcom/src/features/global-nav-bar/class-wpcom-global-nav.php
+++ projects/packages/jetpack-mu-wpcom/src/features/global-nav-bar/class-wpcom-global-nav.php
@@ -1,16 +1,33 @@
 <?php
+/**
+ * WPCOM Global Navbar Class
+ *
+ * @package automattic/jetpack-mu-wpcom
+ */
 
+/**
+ * Provides custom admin bar instead of the default WordPress admin bar.
+ */
 class WPcom_Global_Nav {
 
+	/**
+	 * Constructor
+	 */
 	public function __construct() {
 		add_action( 'admin_bar_init', array( $this, 'init' ) );
 	}
 
+	/**
+	 * Initialize our masterbar.
+	 */
 	public function init() {
-		add_action( 'wp_before_admin_bar_render', array( $this, 'replace_core_masterbar' ), 99999 );
+		add_action( 'wp_before_admin_bar_render', array( $this, 'update_core_masterbar' ), 99999 );
 	}
 
-	public function replace_core_masterbar() {
+	/**
+	 * Update and add items on the core masterbar.
+	 */
+	public function update_core_masterbar() {
 		global $wp_admin_bar;
 
 		if ( ! is_object( $wp_admin_bar ) ) {
@@ -31,15 +48,5 @@ class WPcom_Global_Nav {
 		foreach ( $nodes as $id => $node ) {
 			$wp_admin_bar->add_node( $node );
 		}
-	}
-
-	// temporary because sometimes console goo is easier to read than an error log.
-	public function console_log( $output, $with_script_tags = true ) {
-		$js_code = 'console.log(' . json_encode( $output, JSON_HEX_TAG ) .
-		');';
-		if ( $with_script_tags ) {
-			$js_code = '<script>' . $js_code . '</script>';
-		}
-		echo $js_code;
 	}
 }
--- projects/packages/jetpack-mu-wpcom/src/features/global-nav-bar/global-nav.php
+++ projects/packages/jetpack-mu-wpcom/src/features/global-nav-bar/global-nav.php
@@ -1,8 +1,18 @@
 <?php
+/**
+ * WPCOM Global Navbar Loader
+ *
+ * @package automattic/jetpack-mu-wpcom
+ */
 
 require __DIR__ . '/class-wpcom-global-nav.php';
 
+/**
+ * Determine if new global nav should be loaded.
+ */
 function should_use_new_global_nav() {
+	// True for the sake of simplicity in testing this right now. We may want to check a user
+	// setting or meta here.
 	return true;
 }
 add_filter( 'wpcom_global_nav_enabled', 'should_use_new_global_nav' );
