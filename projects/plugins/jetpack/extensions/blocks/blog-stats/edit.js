924d9bead4bc06b53461f874bef2cd95eb235d0e
Aurorum <43215253+Aurorum@users.noreply.github.com>
2024-02-04 21:49:22 +0000

    Try fix tests

--- projects/plugins/jetpack/extensions/blocks/blog-stats/edit.js
+++ projects/plugins/jetpack/extensions/blocks/blog-stats/edit.js
@@ -55,10 +55,20 @@ function BlogStatsEdit( { attributes, className, setAttributes } ) {
 		);
 	}
 
-	const visitorsPlaceholder = _n( 'visitor', 'visitors', parseInt( stats ), 'jetpack' );
-
-	/* Translators: Number of views */
-	const viewsPlaceholder = _n( 'hit', 'hits', parseInt( stats ), 'jetpack' );
+	const visitorsPlaceholder = _n(
+		/* Translators: Number of visitors */
+		'visitor',
+		'visitors',
+		parseInt( stats ),
+		'jetpack'
+	);
+	const viewsPlaceholder = _n(
+		/* Translators: Number of views */
+		'hit',
+		'hits',
+		parseInt( stats ),
+		'jetpack'
+	);
 
 	return (
 		<>
