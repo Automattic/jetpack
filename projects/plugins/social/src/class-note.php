546d25c246ef4cb07c499849e8481070856cdaea
spsiddarthan <siddarthan@outlook.com>
2024-02-05 18:11:21 +0530

    Social Note: Add feature support for activity pub.

--- projects/plugins/social/src/class-note.php
+++ projects/plugins/social/src/class-note.php
@@ -44,7 +44,7 @@ class Note {
 				'items_list'            => esc_html__( 'Notes list', 'jetpack-social' ),
 			),
 			'show_in_rest' => true,
-			'supports'     => array( 'editor', 'thumbnail', 'publicize' ),
+			'supports'     => array( 'editor', 'thumbnail', 'publicize', 'activitypub' ),
 			'menu_icon'    => 'dashicons-welcome-write-blog',
 
 		);
