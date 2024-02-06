b3f29adb71f35806e154414a2cf3f7d1f7daaf06
Gergely Juhasz <gergely.juhasz@automattic.com>
2024-02-06 13:31:38 +0100

    Refactor functions

--- projects/packages/publicize/src/class-publicize-base.php
+++ projects/packages/publicize/src/class-publicize-base.php
@@ -236,7 +236,7 @@ abstract class Publicize_Base {
 
 		// Default checkbox state for each Connection.
 		add_filter( 'publicize_checkbox_default', array( $this, 'publicize_checkbox_default' ), 10, 2 );
-		add_filter( 'jetpack_open_graph_tags', array( $this, 'add_jetpack_social_og_images' ), 12, 1 ); // $priority = 12, to run after Jetpack adds the tags in the Jetpack_Twitter_Cards class.
+		add_filter( 'jetpack_open_graph_tags', array( $this, 'jetpack_social_open_graph_filter' ), 12, 1 ); // $priority = 12, to run after Jetpack adds the tags in the Jetpack_Twitter_Cards class.
 
 		// Alter the "Post Publish" admin notice to mention the Connections we Publicized to.
 		add_filter( 'post_updated_messages', array( $this, 'update_published_message' ), 20, 1 );
@@ -1714,44 +1714,55 @@ abstract class Publicize_Base {
 	}
 
 	/**
-	 * Add the Jetpack Social images (attached media, SIG image) to the OpenGraph tags.
+	 * Hooks into jetpack_open_graph_tags to add the Jetpack Social images to the OpenGraph tags,
+	 * or to make the Jetpack open graph images pass restrictions.
 	 *
 	 * @param array $tags Current tags.
 	 */
-	public function add_jetpack_social_og_images( $tags ) {
-		$opengraph_image = $this->get_social_opengraph_image( get_the_ID() );
+	public function jetpack_social_open_graph_filter( $tags ) {
+		$social_opengraph_image = $this->get_social_opengraph_image( get_the_ID() );
 
-		if ( empty( $opengraph_image ) ) {
-			// If we do not have a SIG or attached image, but we have an image in post body
-			// we need to check that the image is not too large for the social sites.
-			if ( ! empty( $tags['og:image'] ) && ! empty( $tags['og:image:width'] ) && ! empty( $tags['og:image:height'] ) ) {
-				$filesize = $this->get_remote_filesize( $tags['og:image'] );
-				// If the image is small enough, we do not need to do anything.
-				if ( empty( $filesize ) || $filesize <= self::OG_IMAGE_MAX_FILESIZE ) {
-					return $tags;
-				}
-
-				$compressed_image = $this->compress_and_scale_og_image( $tags['og:image'], $tags['og:image:width'], $tags['og:image:height'] );
-				$filesize         = $this->get_remote_filesize( $compressed_image['url'] );
-				// If the compressed image is small enough, we use it.
-				if ( $filesize <= self::OG_IMAGE_MAX_FILESIZE ) {
-					$tags['og:image']        = $compressed_image['url'];
-					$tags['og:image:width']  = $compressed_image['width'];
-					$tags['og:image:height'] = $compressed_image['height'];
-					return $tags;
-				}
-
-				$reduced_image = $this->reduce_file_size( $compressed_image['url'], $compressed_image['width'], $compressed_image['height'] );
-				if ( ! empty( $reduced_image ) ) {
-					$tags['og:image']        = $reduced_image['url'];
-					$tags['og:image:width']  = $reduced_image['width'];
-					$tags['og:image:height'] = $reduced_image['height'];
-				}
-			}
-
-			return $tags;
+		if ( ! empty( $social_opengraph_image ) ) {
+			return $this->add_jetpack_social_og_image( $tags, $social_opengraph_image );
 		}
 
+		// If we do not have a SIG or attached image, but we have an image in post body
+		// we need to check that the image is not too large for the social sites.
+		if ( ! empty( $tags['og:image'] ) && ! empty( $tags['og:image:width'] ) && ! empty( $tags['og:image:height'] ) ) {
+			$filesize = $this->get_remote_filesize( $tags['og:image'] );
+			// If the image is small enough, we do not need to do anything.
+			if ( empty( $filesize ) || $filesize <= self::OG_IMAGE_MAX_FILESIZE ) {
+				return $tags;
+			}
+
+			$compressed_image = $this->compress_and_scale_og_image( $tags['og:image'], $tags['og:image:width'], $tags['og:image:height'] );
+			$filesize         = $this->get_remote_filesize( $compressed_image['url'] );
+			// If the compressed image is small enough, we use it.
+			if ( $filesize <= self::OG_IMAGE_MAX_FILESIZE ) {
+				$tags['og:image']        = $compressed_image['url'];
+				$tags['og:image:width']  = $compressed_image['width'];
+				$tags['og:image:height'] = $compressed_image['height'];
+				return $tags;
+			}
+
+			$reduced_image = $this->reduce_file_size( $compressed_image['url'], $compressed_image['width'], $compressed_image['height'] );
+			if ( ! empty( $reduced_image ) ) {
+				$tags['og:image']        = $reduced_image['url'];
+				$tags['og:image:width']  = $reduced_image['width'];
+				$tags['og:image:height'] = $reduced_image['height'];
+			}
+		}
+
+		return $tags;
+	}
+
+	/**
+	 * Add the Jetpack Social images (attached media, SIG image) to the OpenGraph tags.
+	 *
+	 * @param array $tags Current tags.
+	 * @param array $opengraph_image The Jetpack Social image data.
+	 */
+	public function add_jetpack_social_og_image( $tags, $opengraph_image ) {
 		// If this code is running in Jetpack, we need to add Twitter cards.
 		// Some active plugins disable Jetpack's Twitter Cards, so we need
 		// to check if the class was instantiated before adding the cards.
