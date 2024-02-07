34652dfb76bf8eb70417a26bc92e8d00396cac08
Linas Valiukas <linas.valiukas@automattic.com>
2024-02-07 16:02:57 -0500

    Revert "Add proxy `Package_Version` classes with different namespaces"
    This reverts commit abd558f3cb0d8145c2091840343a17b0d080ee1c.

--- projects/packages/backup/src/class-package-version.php
+++ projects/packages/backup/src/class-package-version.php
@@ -5,9 +5,6 @@
  * @package automattic/jetpack-backup
  */
 
-// phpcs:disable Generic.Files.OneObjectStructurePerFile.MultipleFound
-// phpcs:disable Universal.Namespaces.OneDeclarationPerFile.MultipleFound
-
 // After changing this file, consider increasing the version number ("VXXX") in all the files using this namespace, in
 // order to ensure that the specific version of this file always get loaded. Otherwise, Jetpack autoloader might decide
 // to load an older/newer version of the class (if, for example, both the standalone and bundled versions of the plugin
@@ -16,10 +13,6 @@ namespace Automattic\Jetpack\Backup\V0002;
 
 /**
  * The Package_Version class.
- *
- * Used as a "jetpack_package_versions" filter, so whenever you bump the old "VXXXX" version number to a new "VYYYY"
- * version, make sure to add a proxy class with the old "VXXXX" version number that would point to a new "VYYYY"
- * version of the class, otherwise upgrades will attempt to load the old "VXXX" version of the class and fail to do so.
  */
 class Package_Version {
 
@@ -36,31 +29,6 @@ class Package_Version {
 	 */
 	public static function send_package_version_to_tracker( $package_versions ) {
 		$package_versions[ self::PACKAGE_SLUG ] = self::PACKAGE_VERSION;
-
 		return $package_versions;
-	}
-}
-
-namespace Automattic\Jetpack\Backup;
-
-/**
- * Filter namespace + class for upgrades from pre-2.4 versions of the plugin.
- */
-class Package_Version {
-	// phpcs:ignore Squiz.Commenting.FunctionComment.Missing
-	public static function send_package_version_to_tracker( $package_versions ) {
-		return V0002\Package_Version::send_package_version_to_tracker( $package_versions );
-	}
-}
-
-namespace Automattic\Jetpack\Backup\V0001;
-
-/**
- * Filter namespace + class for upgrades from version 2.4 of the plugin.
- */
-class Package_Version {
-	// phpcs:ignore Squiz.Commenting.FunctionComment.Missing
-	public static function send_package_version_to_tracker( $package_versions ) {
-		return \Automattic\Jetpack\Backup\V0002\Package_Version::send_package_version_to_tracker( $package_versions );
 	}
 }
