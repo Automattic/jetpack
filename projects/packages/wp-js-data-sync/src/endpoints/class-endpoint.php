63172887f1835ac290c0b8540ee090a2dd2ef0d7
Nauris Pūķis <hi@pyronaur.com>
2024-02-08 13:04:28 +0200

    Allow disabling action routes

--- projects/packages/wp-js-data-sync/src/endpoints/class-endpoint.php
+++ projects/packages/wp-js-data-sync/src/endpoints/class-endpoint.php
@@ -160,11 +160,11 @@ class Endpoint {
 				'status' => 'success',
 				'JSON'   => $result,
 			);
-			if ( DS_Utils::is_debug() ) {
+			if ( true === DS_Utils::is_debug() ) {
 				$response['log'] = $this->entry->get_parser()->get_log();
 			}
 
-			if ( DS_Utils::debug_disable( $this->route_base ) ) {
+			if ( true === DS_Utils::debug_disable( $this->route_base ) ) {
 				// Return 418 I'm a teapot if this is a debug request to the endpoint.
 				return rest_ensure_response( new \WP_Error( 'teapot', "I'm a teapot.", array( 'status' => 418 ) ) );
 			}
