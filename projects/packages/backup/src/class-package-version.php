7eba97cf922d8a4c9f7d2c65f5c05018579b7931
Linas Valiukas <linas.valiukas@automattic.com>
2024-02-07 20:26:09 -0500

    Backup: Write helper script to ABSPATH by default, just like we did before (#35508)
    * Write helper script to ABSPATH, just like we did before
    
    Writing to "wp-content/" has caused some issues, as some hosts don't allow PHP code from "wp-content/" to be accessed directly.
    
    * Bump class namespace version
    
    https://github.com/Automattic/jetpack/pull/34739
    
    * changelog
    
    * Make the test expect the right order of install locations
    
    * Fix up project versions
    
    * Add proxy `Package_Version` classes with different namespaces
    
    * Revert "Add proxy `Package_Version` classes with different namespaces"
    
    This reverts commit abd558f3cb0d8145c2091840343a17b0d080ee1c.
    
    * Don't use namespace versioning in Package_Version, add <...>\V0001\Package_Version proxy
    
    * Use correct path to Package_Version in the test
    
    * Fix package name in a test
    
    * Move test to Automattic\Jetpack\Backup namespace
    
    * Move Package_Version's compatibility class to separate file
    
    * Add (possibly) missing import
    
    * Apply the same behavior to Automattic\Jetpack\Transport_Helper\Package_Version too
    
    * Fix up project/package versions again
    
    * Fix transport-helper's actions.php to point to the right Package_Version

--- projects/packages/backup/src/class-package-version.php
+++ projects/packages/backup/src/class-package-version.php
@@ -5,18 +5,18 @@
  * @package automattic/jetpack-backup
  */
 
-// After changing this file, consider increasing the version number ("VXXX") in all the files using this namespace, in
-// order to ensure that the specific version of this file always get loaded. Otherwise, Jetpack autoloader might decide
-// to load an older/newer version of the class (if, for example, both the standalone and bundled versions of the plugin
-// are installed, or in some other cases).
-namespace Automattic\Jetpack\Backup\V0001;
+namespace Automattic\Jetpack\Backup;
 
 /**
  * The Package_Version class.
+ *
+ * Does *not* use namespaced versioning ("VXXXX") because send_package_version_to_tracker() is used as a
+ * "jetpack_package_versions" filter, and said filter gets run during a plugin upgrade, so it always expects to
+ * find the "Package_Version" class with the same namespace, name, and interface.
  */
 class Package_Version {
 
-	const PACKAGE_VERSION = '3.1.3';
+	const PACKAGE_VERSION = '3.1.4-alpha';
 
 	const PACKAGE_SLUG = 'backup';
 
@@ -29,6 +29,7 @@ class Package_Version {
 	 */
 	public static function send_package_version_to_tracker( $package_versions ) {
 		$package_versions[ self::PACKAGE_SLUG ] = self::PACKAGE_VERSION;
+
 		return $package_versions;
 	}
 }
