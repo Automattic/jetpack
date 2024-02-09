e0462a5cfb78e8a36bb299f5a73a3109feb306c2
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2024-02-09 09:27:33 -0700

    Add/my jetpack link to all jetpack plugins on plugins page (#35523)
    * Launchpad: Make the 'Install the mobile app' task visible to Simple and Atomic sites (#35465)
    
    * Make the Install the mobile app task visible
    
    * changelog
    
    * Bump version
    
    ---------
    
    Co-authored-by: Valter Lorran <valter.lorran@automattic.com>
    
    * changelog
    
    * Add comments
    
    * update project version
    
    * fix test
    
    * fix test
    
    * Try to fix tests again
    
    * Add My Jetpack back to tests
    
    * Add multisite condition to test
    
    * Shorten multisite check
    
    * Show My Jetpack link even if site is not connected
    
    * changelog
    
    * Add search
    
    ---------
    
    Co-authored-by: valterlorran <valterlorran@hotmail.com>
    Co-authored-by: Valter Lorran <valter.lorran@automattic.com>

--- projects/packages/my-jetpack/src/class-initializer.php
+++ projects/packages/my-jetpack/src/class-initializer.php
@@ -34,7 +34,7 @@ class Initializer {
 	 *
 	 * @var string
 	 */
-	const PACKAGE_VERSION = '4.9.0';
+	const PACKAGE_VERSION = '4.9.1-alpha';
 
 	/**
 	 * HTML container ID for the IDC screen on My Jetpack page.
