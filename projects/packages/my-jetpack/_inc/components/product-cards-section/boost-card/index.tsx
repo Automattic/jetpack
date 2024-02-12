7d923695a5691c52ac15454b96b23ff176e05028
elliottprogrammer <bryan@elliottprogrammer.com>
2024-02-12 15:55:11 -0500

    Change CTA to "Boost your site" instead of "Learn more".

--- projects/packages/my-jetpack/_inc/components/product-cards-section/boost-card/index.tsx
+++ projects/packages/my-jetpack/_inc/components/product-cards-section/boost-card/index.tsx
@@ -1,11 +1,21 @@
+import { __ } from '@wordpress/i18n';
 import PropTypes from 'prop-types';
 import React from 'react';
 import ProductCard from '../../connected-product-card';
+import { PRODUCT_STATUSES } from '../../product-card/action-button';
 import BoostSpeedScore from './boost-speed-score';
 
 const BoostCard = ( { admin }: { admin: boolean } ) => {
+	// Override the primary action button to read "Boost your site" instead
+	// of the default text, "Lern more".
+	const primaryActionOverride = {
+		[ PRODUCT_STATUSES.ABSENT_WITH_PLAN ]: {
+			label: __( 'Boost your site', 'jetpack-my-jetpack' ),
+		},
+	};
+
 	return (
-		<ProductCard admin={ admin } slug="boost">
+		<ProductCard admin={ admin } slug="boost" primaryActionOverride={ primaryActionOverride }>
 			<BoostSpeedScore />
 		</ProductCard>
 	);
