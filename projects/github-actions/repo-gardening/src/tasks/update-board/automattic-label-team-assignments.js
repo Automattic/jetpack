fabc602644372d2c681a1ab752d89b21ac551fd9
Jeremy Herve <jeremy@jeremy.hu>
2024-02-12 18:45:09 +0100

    Board triage: update Slack channel ID for Vulcan-related issues

--- projects/github-actions/repo-gardening/src/tasks/update-board/automattic-label-team-assignments.js
+++ projects/github-actions/repo-gardening/src/tasks/update-board/automattic-label-team-assignments.js
@@ -68,13 +68,13 @@ export const automatticAssignments = {
 	'Blocks infrastructure': {
 		team: 'Vulcan',
 		labels: [ '[Package] Blocks', '[Focus] FSE', '[Focus] Blocks' ],
-		slack_id: 'CBG1CP4EN',
+		slack_id: 'C05PV073SG3',
 		board_id: 'https://github.com/orgs/Automattic/projects/778',
 	},
 	Connection: {
 		team: 'Vulcan',
 		labels: [ '[Package] Connection', '[Package] Identity Crisis', '[Package] Sync' ],
-		slack_id: 'CBG1CP4EN',
+		slack_id: 'C05PV073SG3',
 		board_id: 'https://github.com/orgs/Automattic/projects/778',
 	},
 	CRM: {
@@ -110,7 +110,7 @@ export const automatticAssignments = {
 	'React Dashboard': {
 		team: 'Vulcan',
 		labels: [ 'Admin Page' ],
-		slack_id: 'CBG1CP4EN',
+		slack_id: 'C05PV073SG3',
 		board_id: 'https://github.com/orgs/Automattic/projects/778',
 	},
 	Search: {
