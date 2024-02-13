150e4908fb00121b69e87457d052a488da27351a
Andrii <lysenkoa.work@gmail.com>
2024-02-13 14:27:07 -0800

    My Jeptack Connection: Make footer logo a list

--- projects/packages/my-jetpack/_inc/components/connection-screen/index.jsx
+++ projects/packages/my-jetpack/_inc/components/connection-screen/index.jsx
@@ -23,23 +23,29 @@ const ConnectionScreenFooter = () => {
 					'jetpack-my-jetpack'
 				) }
 			</div>
-			<span className={ styles[ 'account-images' ] }>
-				<img
-					src={ wordpressLogo }
-					className={ styles.wordpress }
-					alt={ __( 'WordPress Logo', 'jetpack-my-jetpack' ) }
-				/>
-				<img
-					src={ googleLogo }
-					className={ styles.google }
-					alt={ __( 'Google Logo', 'jetpack-my-jetpack' ) }
-				/>
-				<img
-					src={ appleLogo }
-					className={ styles.apple }
-					alt={ __( 'Apple Logo', 'jetpack-my-jetpack' ) }
-				/>
-			</span>
+			<ul className={ styles[ 'account-images' ] }>
+				<li>
+					<img
+						src={ wordpressLogo }
+						className={ styles.wordpress }
+						alt={ __( 'WordPress Logo', 'jetpack-my-jetpack' ) }
+					/>
+				</li>
+				<li>
+					<img
+						src={ googleLogo }
+						className={ styles.google }
+						alt={ __( 'Google Logo', 'jetpack-my-jetpack' ) }
+					/>
+				</li>
+				<li>
+					<img
+						src={ appleLogo }
+						className={ styles.apple }
+						alt={ __( 'Apple Logo', 'jetpack-my-jetpack' ) }
+					/>
+				</li>
+			</ul>
 		</>
 	);
 };
