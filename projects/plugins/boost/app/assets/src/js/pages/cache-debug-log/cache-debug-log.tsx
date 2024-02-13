0543e736bc155a55b1c557c230dd4e7b52976f9d
Adnan Haque <adnan007.id@gmail.com>
2024-02-13 15:48:48 +0600

    Add a page to show debug logs

--- projects/plugins/boost/app/assets/src/js/pages/cache-debug-log/cache-debug-log.tsx
+++ projects/plugins/boost/app/assets/src/js/pages/cache-debug-log/cache-debug-log.tsx
@@ -1,7 +1,5 @@
 import { __ } from '@wordpress/i18n';
 import Header from '$layout/header/header';
-import styles from './cache-debug-log.module.scss';
-import classNames from 'classnames';
 import { useDataSync } from '@automattic/jetpack-react-data-sync-client';
 import { z } from 'zod';
 import Footer from '$layout/footer/footer';
@@ -11,7 +9,7 @@ const CacheDebugLog = () => {
 	return (
 		<div id="jb-dashboard" className="jb-dashboard">
 			<Header subPageTitle={ __( 'Cache Debug Log', 'jetpack-boost' ) } />
-			<div className={ classNames( styles.page, 'jb-section--alt' ) }>
+			<div className="jb-section--alt">
 				<div className="jb-container">
 					<pre>{ debugLog }</pre>
 				</div>
