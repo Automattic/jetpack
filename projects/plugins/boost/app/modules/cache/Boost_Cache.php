286a18b03751599e4b4343e1864ebbbb3010b677
Mark George <thingalon@gmail.com>
2024-02-13 13:37:08 +1100

    Do not cache empty buffer

--- projects/plugins/boost/app/modules/cache/Boost_Cache.php
+++ projects/plugins/boost/app/modules/cache/Boost_Cache.php
@@ -206,7 +206,7 @@ class Boost_Cache {
 	 * @return string - The output buffer.
 	 */
 	public function ob_callback( $buffer ) {
-		if ( $this->is_cacheable() ) {
+		if ( strlen( $buffer ) > 0 && $this->is_cacheable() ) {
 			$result = $this->storage->write( $this->request_uri, $this->request_parameters, $buffer );
 
 			if ( is_wp_error( $result ) ) { // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedIf
