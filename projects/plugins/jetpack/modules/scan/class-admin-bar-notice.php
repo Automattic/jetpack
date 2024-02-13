Automattic/jetpack

commit ca5ff93ae6d54977a25434dd8afb2f62ab9a45ed

Add support for script strategy (#34072)
* Add support for script strategy

* Script strategy was introduced in 6.3

* Update projects/packages/assets/src/class-assets.php

Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>

* Update projects/packages/assets/src/class-assets.php

Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>

* Switch version check to use `$wp_versoin` global

* Add strategy option

* Enable async support for strategy option

* Update projects/packages/assets/src/class-assets.php

Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>

* Simplify logic as per PR suggestion

* Support deprecated `async` attribute using new strategy=‘defer’

* Deprecate add_async_script

* Adjust use of add_async_script in class-tracking-pixel.php

* Deprecate script_add_async

* Hook `script_add_async` conditionally

* Deprecate `enqueue_async_script` (and restore pre-PR code)

* Update uses of enqueue_async_script to use register_script instead

* Deprecate the async option, improve doc block

* restore `enqueue_async_script`

* Try: add tests for strategy

* Remove pre-6.3 code from Stats

This gives us the opportunity to remove the need for the Assets package entirely.

* Update tests following feedback

See https://github.com/Automattic/jetpack/pull/34072/files#r1396404112

Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>

* Remove pre-6.3 code

Jetpack now requires WordPress 6.3

* Address feedback from code review

See:

- https://github.com/Automattic/jetpack/pull/34072/files#r1396389177
- https://github.com/Automattic/jetpack/pull/34072/files#r1396389463
- https://github.com/Automattic/jetpack/pull/34072/files#r1396359251
- https://github.com/Automattic/jetpack/pull/34072/files#r1396389743
- https://github.com/Automattic/jetpack/pull/34072/files#r1396362871
- https://github.com/Automattic/jetpack/pull/34072/files#r1396363681

projects/plugins/jetpack/modules/scan/class-admin-bar-notice.php
@@ -105,8 +105,19 @@ class Admin_Bar_Notice {
 		}
 
 		// We don't know about threats in the cache lets load the JS that fetches the info and updates the admin bar.
-		Assets::enqueue_async_script( self::SCRIPT_NAME, '_inc/build/scan/admin-bar-notice.min.js', 'modules/scan/admin-bar-notice.js', array( 'admin-bar' ), self::SCRIPT_VERSION, true );
-
+		Assets::register_script(
+			self::SCRIPT_NAME,
+			'_inc/build/scan/admin-bar-notice.min.js',
+			__FILE__,
+			array(
+				'in_footer'    => true,
+				'strategy'     => 'defer',
+				'nonmin_path'  => '_inc/build/scan/admin-bar-notice.js',
+				'dependencies' => array( 'admin-bar' ),
+				'version'      => self::SCRIPT_VERSION,
+				'enqueue'      => true,
+			)
+		);
 		$script_data = array(
 			'nonce'              => wp_create_nonce( 'wp_rest' ),
 			'scan_endpoint'      => get_rest_url( null, 'jetpack/v4/scan' ),

projects/packages/assets/src/class-assets.php
@@ -54,40 +54,35 @@ class Assets {
 	public static function instance() {
 		if ( ! isset( self::$instance ) ) {
 			self::$instance = new Assets();
-			self::$instance->init_hooks();
 		}
 
 		return self::$instance;
 	}
 
 	/**
-	 * Initalize the hooks as needed.
-	 */
-	private function init_hooks() {
-		/*
-		 * Load some scripts asynchronously.
-		 */
-		add_filter( 'script_loader_tag', array( $this, 'script_add_async' ), 10, 2 );
-	}
-
-	/**
 	 * A public method for adding the async script.
+	 *
+	 * @deprecated Since $$next-version$$, the `strategy` feature should be used instead, with the "defer" setting.
 	 *
 	 * @param string $script_handle Script handle.
 	 */
 	public static function add_async_script( $script_handle ) {
-		$assets_instance                         = self::instance();
-		$assets_instance->defer_script_handles[] = $script_handle;
+		_deprecated_function( __METHOD__, '$$next-version$$' );
+
+		wp_script_add_data( $script_handle, 'strategy', 'defer' );
 	}
 
 	/**
 	 * Add an async attribute to scripts that can be loaded deferred.
 	 * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script
 	 *
+	 * @deprecated Since $$next-version$$, the `strategy` feature should be used instead.
+	 *
 	 * @param string $tag    The <script> tag for the enqueued script.
 	 * @param string $handle The script's registered handle.
 	 */
 	public function script_add_async( $tag, $handle ) {
+		_deprecated_function( __METHOD__, '$$next-version$$' );
 		if ( empty( $this->defer_script_handles ) ) {
 			return $tag;
 		}
@@ -103,6 +98,8 @@ class Assets {
 	/**
 	 * A helper function that lets you enqueue scripts in an async fashion.
 	 *
+	 * @deprecated Since $$next-version$$ - use the strategy feature instead.
+	 *
 	 * @param string $handle        Name of the script. Should be unique.
 	 * @param string $min_path      Minimized script path.
 	 * @param string $non_min_path  Full Script path.
@@ -111,6 +108,7 @@ class Assets {
 	 * @param bool   $in_footer       Should the script be included in the footer.
 	 */
 	public static function enqueue_async_script( $handle, $min_path, $non_min_path, $deps = array(), $ver = false, $in_footer = true ) {
+		_deprecated_function( __METHOD__, '$$next-version$$' );
 		$assets_instance = self::instance();
 		$assets_instance->add_async_script( $handle );
 		wp_enqueue_script( $handle, self::get_file_url_for_environment( $min_path, $non_min_path ), $deps, $ver, $in_footer );
@@ -307,12 +305,13 @@ class Assets {
 	 * This wrapper handles all of that.
 	 *
 	 * @since 1.12.0
+	 * @since $$next-version$$ Add a new `strategy` option to leverage WP >= 6.3 script strategy feature. The `async` option is deprecated.
 	 * @param string $handle      Name of the script. Should be unique across both scripts and styles.
 	 * @param string $path        Minimized script path.
 	 * @param string $relative_to File that `$path` is relative to. Pass `__FILE__`.
 	 * @param array  $options     Additional options:
 	 *  - `asset_path`:       (string|null) `.asset.php` to load. Default is to base it on `$path`.
-	 *  - `async`:            (bool) Set true to register the script as async, like `Assets::enqueue_async_script()`
+	 *  - `async`:            (bool) Set true to register the script as deferred, like `Assets::enqueue_async_script()`. Deprecated in favor of `strategy`.
 	 *  - `css_dependencies`: (string[]) Additional style dependencies to queue.
 	 *  - `css_path`:         (string|null) `.css` to load. Default is to base it on `$path`.
 	 *  - `dependencies`:     (string[]) Additional script dependencies to queue.
@@ -321,6 +320,7 @@ class Assets {
 	 *  - `media`:            (string) Media for the css file. Default 'all'.
 	 *  - `minify`:           (bool|null) Set true to pass `minify=true` in the query string, or `null` to suppress the normal `minify=false`.
 	 *  - `nonmin_path`:      (string) Non-minified script path.
+	 *  - `strategy`:         (string) Specify a script strategy to use, eg. `defer` or `async`. Default is `""`.
 	 *  - `textdomain`:       (string) Text domain for the script. Required if the script depends on wp-i18n.
 	 *  - `version`:          (string) Override the version from the `asset_path` file.
 	 * @throws \InvalidArgumentException If arguments are invalid.
@@ -328,6 +328,10 @@ class Assets {
 	public static function register_script( $handle, $path, $relative_to, array $options = array() ) {
 		if ( substr( $path, -3 ) !== '.js' ) {
 			throw new \InvalidArgumentException( '$path must end in ".js"' );
+		}
+
+		if ( isset( $options['async'] ) ) {
+			_deprecated_argument( __METHOD__, '$$next-version$$', 'The `async` option is deprecated in favor of `strategy`' );
 		}
 
 		$dir      = dirname( $relative_to );
@@ -342,6 +346,7 @@ class Assets {
 			'in_footer'        => false,
 			'media'            => 'all',
 			'minify'           => false,
+			'strategy'         => '',
 			'textdomain'       => null,
 		);
 
@@ -376,10 +381,20 @@ class Assets {
 			$ver = isset( $options['version'] ) ? $options['version'] : filemtime( "$dir/$path" );
 		}
 
-		wp_register_script( $handle, $url, $options['dependencies'], $ver, $options['in_footer'] );
-		if ( $options['async'] ) {
-			self::instance()->add_async_script( $handle );
+		if ( $options['async'] && '' === $options['strategy'] ) { // Handle the deprecated `async` option
+			$options['strategy'] = 'defer';
 		}
+		wp_register_script(
+			$handle,
+			$url,
+			$options['dependencies'],
+			$ver,
+			array(
+				'in_footer' => $options['in_footer'],
+				'strategy'  => $options['strategy'],
+			)
+		);
+
 		if ( $options['textdomain'] ) {
 			// phpcs:ignore Jetpack.Functions.I18n.DomainNotLiteral
 			wp_set_script_translations( $handle, $options['textdomain'] );

projects/packages/assets/tests/php/test-assets.php
@@ -12,6 +12,7 @@ use Brain\Monkey;
 use Brain\Monkey\Filters;
 use Brain\Monkey\Functions;
 use InvalidArgumentException;
+use Mockery;
 use PHPUnit\Framework\TestCase;
 use Wikimedia\TestingAccessWrapper;
 
@@ -204,83 +205,6 @@ class AssetsTest extends TestCase {
 	}
 
 	/**
-	 * Test that enqueue_async_script calls wp_enqueue_script
-	 */
-	public function test_enqueue_async_script_calls_wp_enqueue_script() {
-		Functions\expect( 'wp_enqueue_script' )
-			->once()
-			->with( 'handle', Assets::get_file_url_for_environment( '/minpath.js', '/path.js' ), array(), '123', true );
-		Assets::enqueue_async_script( 'handle', '/minpath.js', '/path.js', array(), '123', true );
-		$asset_instance = Assets::instance();
-		$this->assertEquals( 10, (int) has_filter( 'script_loader_tag', array( $asset_instance, 'script_add_async' ) ) );
-	}
-
-	/**
-	 * Test that the `defer` attribute is properly added to the script tags for async scripts.
-	 */
-	public function test_defer_attribute_properly_added() {
-		Functions\expect( 'wp_enqueue_script' )
-			->once()
-			->with( 'handle', Assets::get_file_url_for_environment( '/minpath.js', '/path.js' ), array(), '123', true );
-		Assets::enqueue_async_script( 'handle', '/minpath.js', '/path.js', array(), '123', true );
-
-		$asset_instance = Assets::instance();
-
-		// phpcs:ignore WordPress.WP.EnqueuedResources.NonEnqueuedScript
-		$tag    = '<script src="/minpath.js" id="handle"></script>';
-		$actual = $asset_instance->script_add_async( $tag, 'handle' );
-		// phpcs:ignore WordPress.WP.EnqueuedResources.NonEnqueuedScript
-		$expected = '<script defer src="/minpath.js" id="handle"></script>';
-		$this->assertEquals( $expected, $actual );
-	}
-
-	/**
-	 * Test that the `defer` attribute is properly added to the script tags for async scripts with translations.
-	 */
-	public function test_defer_attribute_properly_added_with_translations() {
-		Functions\expect( 'wp_enqueue_script' )
-			->once()
-			->with( 'handle', Assets::get_file_url_for_environment( '/minpath.js', '/path.js' ), array(), '123', true );
-		Assets::enqueue_async_script( 'handle', '/minpath.js', '/path.js', array(), '123', true );
-
-		$asset_instance = Assets::instance();
-
-		$translations =
-			// phpcs:ignore WordPress.WP.EnqueuedResources.NonEnqueuedScript
-			'<script id="handle-js-translations">
-			( function( domain, translations ) {
-				var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
-				localeData[""].domain = domain;
-				wp.i18n.setLocaleData( localeData, domain );
-			} )( "default", { "locale_data": { "messages": { "": {} } } } );
-			</script>';
-		// phpcs:ignore WordPress.WP.EnqueuedResources.NonEnqueuedScript
-		$tag    = $translations . '<script src="/minpath.js" id="handle"></script>';
-		$actual = $asset_instance->script_add_async( $tag, 'handle' );
-		// phpcs:ignore WordPress.WP.EnqueuedResources.NonEnqueuedScript
-		$expected = $translations . '<script defer src="/minpath.js" id="handle"></script>';
-		$this->assertEquals( $expected, $actual );
-	}
-
-	/**
-	 * Test that the `defer` attribute is not added to incorrect script tags.
-	 */
-	public function test_defer_attribute_with_incorrect_tag() {
-		Functions\expect( 'wp_enqueue_script' )
-			->once()
-			->with( 'handle', Assets::get_file_url_for_environment( '/minpath.js', '/path.js' ), array(), '123', true );
-		Assets::enqueue_async_script( 'handle', '/minpath.js', '/path.js', array(), '123', true );
-
-		$asset_instance = Assets::instance();
-		// phpcs:ignore WordPress.WP.EnqueuedResources.NonEnqueuedScript
-		$tag    = '<scriptfoo src="/minpath.js" id="handle"></scriptfoo>';
-		$actual = $asset_instance->script_add_async( $tag, 'handle' );
-		// phpcs:ignore WordPress.WP.EnqueuedResources.NonEnqueuedScript
-		$expected = '<scriptfoo src="/minpath.js" id="handle"></scriptfoo>';
-		$this->assertEquals( $expected, $actual );
-	}
-
-	/**
 	 * Test whether static resources are properly updated to use a WordPress.com static domain.
 	 *
 	 * @covers Automattic\Jetpack\Assets::staticize_subdomain
@@ -426,12 +350,6 @@ class AssetsTest extends TestCase {
 		}
 
 		Assets::register_script( ...$args );
-
-		// Check whether $options['async'] was honored.
-		$this->assertSame(
-			isset( $extra['async'] ) ? $extra['async'] : array(),
-			TestingAccessWrapper::newFromObject( Assets::instance() )->defer_script_handles
-		);
 	}
 
 	/** Data provider for `test_register_script` */
@@ -446,7 +364,10 @@ class AssetsTest extends TestCase {
 						"$url_base/single-js-file.js?minify=false",
 						array(),
 						2883865438,
-						false,
+						array(
+							'in_footer' => false,
+							'strategy'  => '',
+						),
 					),
 					'wp_script_add_data' => array( 'single-file', 'Jetpack::Assets::hascss', false ),
 				),
@@ -468,7 +389,10 @@ class AssetsTest extends TestCase {
 						"$url_base/single-js-file.js?minify=false",
 						array( 'xyz' ),
 						'foobar',
-						false,
+						array(
+							'in_footer' => false,
+							'strategy'  => '',
+						),
 					),
 					'wp_set_script_translations' => array( 'single-file', 'foobaz' ),
 					'wp_script_add_data'         => array( 'single-file', 'Jetpack::Assets::hascss', false ),
@@ -483,7 +407,10 @@ class AssetsTest extends TestCase {
 						"$url_base/js-and-css.js?minify=false",
 						array( 'wp-polyfill' ),
 						'ver-from-js-and-css',
-						false,
+						array(
+							'in_footer' => false,
+							'strategy'  => '',
+						),
 					),
 					'wp_register_style'  => array( 'handle', "$url_base/js-and-css.css?minify=false", array(), 'ver-from-js-and-css', 'all' ),
 					'wp_script_add_data' => array( 'handle', 'Jetpack::Assets::hascss', true ),
@@ -510,7 +437,10 @@ class AssetsTest extends TestCase {
 						"$url_base/js-and-css.js",
 						array( 'wp-polyfill', 'qwerty', 'uiop' ),
 						'foobaz',
-						true,
+						array(
+							'in_footer' => true,
+							'strategy'  => '',
+						),
 					),
 					'wp_register_style'  => array( 'handle', "$url_base/js-and-css.css", array( 'asdf' ), 'foobaz', 'screen' ),
 					'wp_script_add_data' => array( 'handle', 'Jetpack::Assets::hascss', true ),
@@ -534,7 +464,10 @@ class AssetsTest extends TestCase {
 						"$url_base/everything.js?minify=false",
 						array( 'wp-polyfill', 'wp-components', 'wp-i18n' ),
 						'ver-from-everything',
-						false,
+						array(
+							'in_footer' => false,
+							'strategy'  => '',
+						),
 					),
 					'wp_set_script_translations' => array( 'handle', 'foobar' ),
 					'wp_register_style'          => array( 'handle', "$url_base/everything.css?minify=false", array( 'wp-components' ), 'ver-from-everything', 'all' ),
@@ -564,7 +497,10 @@ class AssetsTest extends TestCase {
 						"$url_base/everything.src.js?minify=true",
 						array( 'wp-polyfill', 'wp-components', 'wp-i18n', 'qwerty', 'uiop' ),
 						'foobaz',
-						true,
+						array(
+							'in_footer' => true,
+							'strategy'  => '',
+						),
 					),
 					'wp_set_script_translations' => array( 'handle', 'foobar' ),
 					'wp_register_style'          => array( 'handle', "$url_base/everything.rtl.css?minify=true", array( 'wp-components', 'asdf' ), 'foobaz', 'screen' ),
@@ -592,7 +528,10 @@ class AssetsTest extends TestCase {
 						"$url_base/single-js-file.js?minify=false",
 						array( 'wp-polyfill' ),
 						'ver-from-js-and-css',
-						false,
+						array(
+							'in_footer' => false,
+							'strategy'  => '',
+						),
 					),
 					'wp_register_style'  => array( 'single-file', "$url_base/everything.rtl.css?minify=false", array(), 'ver-from-js-and-css', 'all' ),
 					'wp_script_add_data' => array( 'single-file', 'Jetpack::Assets::hascss', true ),
@@ -603,14 +542,18 @@ class AssetsTest extends TestCase {
 			'Async'                                     => array(
 				array( 'single-file', 'test-assets-files/single-js-file.js', __FILE__, array( 'async' => true ) ),
 				array(
-					'wp_register_script' => array(
+					'wp_register_script'   => array(
 						'single-file',
 						"$url_base/single-js-file.js?minify=false",
 						array(),
 						2883865438,
-						false,
+						array(
+							'in_footer' => false,
+							'strategy'  => 'defer',
+						),
 					),
-					'wp_script_add_data' => array( 'single-file', 'Jetpack::Assets::hascss', false ),
+					'wp_script_add_data'   => array( 'single-file', 'Jetpack::Assets::hascss', false ),
+					'_deprecated_argument' => array( 'Automattic\Jetpack\Assets::register_script', Mockery::type( 'string' ), 'The `async` option is deprecated in favor of `strategy`' ),
 				),
 				array( 'async' => array( 'single-file' ) ),
 			),
@@ -623,7 +566,10 @@ class AssetsTest extends TestCase {
 						"$url_base/single-js-file.js?minify=false",
 						array(),
 						2883865438,
-						false,
+						array(
+							'in_footer' => false,
+							'strategy'  => '',
+						),
 					),
 					'wp_script_add_data' => array( 'single-file', 'Jetpack::Assets::hascss', false ),
 					'wp_enqueue_script'  => array( 'single-file' ),
@@ -646,7 +592,10 @@ class AssetsTest extends TestCase {
 						"$url_base/everything.js?minify=false",
 						array( 'wp-polyfill', 'wp-components', 'wp-i18n' ),
 						'ver-from-everything',
-						false,
+						array(
+							'in_footer' => false,
+							'strategy'  => '',
+						),
 					),
 					'wp_set_script_translations' => array( 'everything', 'foobar' ),
 					'wp_register_style'          => array( 'everything', "$url_base/everything.css?minify=false", array( 'wp-components' ), 'ver-from-everything', 'all' ),
@@ -678,7 +627,10 @@ class AssetsTest extends TestCase {
 						"$url_base/everything.js?minify=false",
 						array( 'wp-polyfill', 'wp-components', 'wp-i18n' ),
 						'ver-from-everything',
-						false,
+						array(
+							'in_footer' => false,
+							'strategy'  => '',
+						),
 					),
 					'_doing_it_wrong'    => array( Assets::class . '::register_script', 'Script &quot;everything&quot; depends on wp-i18n but does not specify &quot;textdomain&quot;', '' ),
 					'wp_register_style'  => array( 'everything', "$url_base/everything.css?minify=false", array( 'wp-components' ), 'ver-from-everything', 'all' ),
@@ -687,6 +639,40 @@ class AssetsTest extends TestCase {
 				array(
 					'is_rtl' => false,
 				),
+			),
+			'Strategy Defer'                            => array(
+				array( 'single-file', 'test-assets-files/single-js-file.js', __FILE__, array( 'strategy' => 'defer' ) ),
+				array(
+					'wp_register_script' => array(
+						'single-file',
+						"$url_base/single-js-file.js?minify=false",
+						array(),
+						2883865438,
+						array(
+							'in_footer' => false,
+							'strategy'  => 'defer',
+						),
+					),
+					'wp_script_add_data' => array( 'single-file', 'Jetpack::Assets::hascss', false ),
+				),
+				array(),
+			),
+			'Strategy Async'                            => array(
+				array( 'single-file', 'test-assets-files/single-js-file.js', __FILE__, array( 'strategy' => 'async' ) ),
+				array(
+					'wp_register_script' => array(
+						'single-file',
+						"$url_base/single-js-file.js?minify=false",
+						array(),
+						2883865438,
+						array(
+							'in_footer' => false,
+							'strategy'  => 'async',
+						),
+					),
+					'wp_script_add_data' => array( 'single-file', 'Jetpack::Assets::hascss', false ),
+				),
+				array(),
 			),
 		);
 	}

projects/packages/forms/src/contact-form/class-contact-form-plugin.php
@@ -259,7 +259,7 @@ class Contact_Form_Plugin {
 			'../../dist/contact-form/js/accessible-form.js',
 			__FILE__,
 			array(
-				'async'        => true,
+				'strategy'     => 'defer',
 				'textdomain'   => 'jetpack-forms',
 				'version'      => \JETPACK__VERSION,
 				'dependencies' => array( 'wp-i18n' ),

projects/packages/stats/src/class-tracking-pixel.php
@@ -7,7 +7,6 @@
 
 namespace Automattic\Jetpack\Stats;
 
-use Automattic\Jetpack\Assets;
 use Jetpack_Options;
 use WP_Post;
 
@@ -128,11 +127,11 @@ _stq.push([ "clickTrackerInit", "%2$s", "%3$s" ]);',
 			'https://stats.wp.com/e-' . gmdate( 'YW' ) . '.js',
 			array(),
 			null, // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion -- The version is set in the URL.
-			true
+			array(
+				'in_footer' => true,
+				'strategy'  => 'defer',
+			)
 		);
-
-		// Make sure the script loads asynchronously (add a defer attribute).
-		Assets::instance()->add_async_script( 'jetpack-stats' );
 
 		$data = self::build_view_data();
 

projects/plugins/jetpack/modules/woocommerce-analytics/class-jetpack-woocommerce-analytics.php
@@ -119,7 +119,16 @@ class Jetpack_WooCommerce_Analytics {
 			gmdate( 'YW' )
 		);
 
-		Assets::enqueue_async_script( 'woocommerce-analytics', esc_url( $filename ), esc_url( $filename ), array(), null, false );
+		Assets::register_script(
+			'woocommerce-analytics',
+			esc_url( $filename ),
+			__FILE__,
+			array(
+				'in_footer' => false,
+				'strategy'  => 'defer',
+				'enqueue'   => true,
+			)
+		);
 	}
 
 	/**

commit 63f39b656d31524e0bb63a0be5817457af0f0fc9

Scan notices: fix the location of the enqueued js files
This fixes an issue introduced in #34072.

- The non-minified file (accessible when `SCRIPT_DEBUG` is set to true) should load from inside the modules directory.
- The minified file should load from _inc/build, located at the root of the Jetpack plugin.

projects/plugins/jetpack/modules/scan/class-admin-bar-notice.php
@@ -108,11 +108,11 @@ class Admin_Bar_Notice {
 		Assets::register_script(
 			self::SCRIPT_NAME,
 			'_inc/build/scan/admin-bar-notice.min.js',
-			__FILE__,
+			JETPACK__PLUGIN_FILE,
 			array(
 				'in_footer'    => true,
 				'strategy'     => 'defer',
-				'nonmin_path'  => '_inc/build/scan/admin-bar-notice.js',
+				'nonmin_path'  => 'modules/scan/admin-bar-notice.js',
 				'dependencies' => array( 'admin-bar' ),
 				'version'      => self::SCRIPT_VERSION,
 				'enqueue'      => true,