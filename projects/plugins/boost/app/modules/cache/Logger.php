afb697dcf90487677915ccd30e0e5dadff0733e2
Adnan Haque <adnan007.id@gmail.com>
2024-02-14 10:41:22 +0600

    Use tabs in log line

--- projects/plugins/boost/app/modules/cache/Logger.php
+++ projects/plugins/boost/app/modules/cache/Logger.php
@@ -80,7 +80,7 @@ class Logger {
 	public function log( $message ) {
 		$request     = new Request();
 		$request_uri = htmlspecialchars( $request->get_uri(), ENT_QUOTES, 'UTF-8' );
-		$line        = gmdate( 'H:i:s' ) . ' ' . getmypid() . " {$request_uri} {$message}" . PHP_EOL;
+		$line        = gmdate( 'H:i:s' ) . ' ' . getmypid() . "\t{$request_uri}\t\t{$message}" . PHP_EOL;
 		// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
 		error_log( $line, 3, $this->get_log_file() );
 	}
