5421cfd54df4e4cb53f228398a78582036ff6f24
Peter Petrov <peter.petrov89@gmail.com>
2024-02-14 18:37:59 +0200

    Add exceptions and logging UI elements

--- projects/plugins/boost/app/assets/src/js/features/page-cache/meta/meta.tsx
+++ projects/plugins/boost/app/assets/src/js/features/page-cache/meta/meta.tsx
@@ -11,22 +11,55 @@ const Meta = () => {
 
 	return (
 		<div className={ styles.wrapper }>
-			<div className={ styles.summary }>{ __( 'No exceptions or logging.', 'jetpack-boost' ) }</div>
-			<div className={ styles.actions }>
-				<Button variant="link" size="small" weight="regular" iconSize={ 16 } icon={ <Lightning /> }>
-					{ __( 'Clear Cache', 'jetpack-boost' ) }
-				</Button>{ ' ' }
-				<Button
-					variant="link"
-					size="small"
-					weight="regular"
-					iconSize={ 16 }
-					icon={ isExpanded ? <ChevronUp /> : <ChevronDown /> }
-					onClick={ () => setIsExpanded( ! isExpanded ) }
-				>
-					{ __( 'Show Options', 'jetpack-boost' ) }
-				</Button>
+			<div className={ styles.head }>
+				<div className={ styles.summary }>
+					{ __( 'No exceptions or logging.', 'jetpack-boost' ) }
+				</div>
+				<div className={ styles.actions }>
+					<Button
+						variant="link"
+						size="small"
+						weight="regular"
+						iconSize={ 16 }
+						icon={ <Lightning /> }
+					>
+						{ __( 'Clear Cache', 'jetpack-boost' ) }
+					</Button>{ ' ' }
+					<Button
+						variant="link"
+						size="small"
+						weight="regular"
+						iconSize={ 16 }
+						icon={ isExpanded ? <ChevronUp /> : <ChevronDown /> }
+						onClick={ () => setIsExpanded( ! isExpanded ) }
+					>
+						{ __( 'Show Options', 'jetpack-boost' ) }
+					</Button>
+				</div>
 			</div>
+			{ isExpanded && (
+				<div className={ styles.body }>
+					<div className={ styles.section }>
+						<div className={ styles.title }>{ __( 'Exceptions', 'jetpack-boost' ) }</div>
+						<p>{ __( 'URLs of pages and posts that will never be cached:', 'jetpack-boost' ) }</p>
+						<textarea rows={ 3 }></textarea>
+						<p className={ styles.description }>
+							{ __(
+								'Use (*) to address multiple URLs under a given path. Be sure each URL path is in its own line. See an example or learn more.',
+								'jetpack-boost'
+							) }
+						</p>
+						<Button>{ __( 'Save', 'jetpack-boost' ) }</Button>
+					</div>
+					<div className={ styles.section }>
+						<div className={ styles.title }>{ __( 'Logging', 'jetpack-boost' ) }</div>
+						<label htmlFor="cache-logging">
+							<input type="checkbox" id="cache-logging" />{ ' ' }
+							{ __( 'Activate logging to track all your cache events.', 'jetpack-boost' ) }
+						</label>
+					</div>
+				</div>
+			) }
 		</div>
 	);
 };
