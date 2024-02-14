b31c6f09b7bae5619d27a3da1e2eaaaa08b9561d
Luiz Kowalski <lhkowalski@gmail.com>
2024-02-14 12:37:43 -0300

    Create function to process transcriptions

---
+++ projects/js-packages/ai-client/src/audio-transcription/index.ts
@@ -0,0 +1,67 @@
+/**
+ * External dependencies
+ */
+import apiFetch from '@wordpress/api-fetch';
+import debugFactory from 'debug';
+/**
+ * Internal dependencies
+ */
+import requestJwt from '../jwt';
+
+const debug = debugFactory( 'jetpack-ai-client:audio-transcription' );
+
+/**
+ * The response from the audio transcription service.
+ */
+type AudioTranscriptionResponse = {
+	/**
+	 * The transcribed text.
+	 */
+	text: string;
+};
+
+/**
+ * A function that takes an audio blob and transcribes it.
+ *
+ * @param {Blob} audio - The audio to be transcribed, from a recording or from a file.
+ * @param {string} feature - The feature name that is calling the transcription.
+ * @returns {Promise<string>} - The promise of a string containing the transcribed audio.
+ */
+export default async function transcribeAudio( audio: Blob, feature?: string ): Promise< string > {
+	debug( 'Transcribing audio: %o. Feature: %o', audio, feature );
+
+	// Get a token to use the transcription service
+	let token = '';
+	try {
+		token = ( await requestJwt() ).token;
+	} catch ( error ) {
+		debug( 'Error getting token: %o', error );
+		return Promise.reject( error );
+	}
+
+	// Build a FormData object to hold the audio file
+	const formData = new FormData();
+	formData.append( 'audio_file', audio );
+
+	try {
+		const headers = {
+			Authorization: `Bearer ${ token }`,
+		};
+
+		const response: AudioTranscriptionResponse = await apiFetch( {
+			url: `https://public-api.wordpress.com/wpcom/v2/jetpack-ai-transcription${
+				feature ? `?feature=${ feature }` : ''
+			}`,
+			method: 'POST',
+			body: formData,
+			headers,
+		} );
+
+		debug( 'Transcription response: %o', response );
+
+		return response.text;
+	} catch ( error ) {
+		debug( 'Transcription error response: %o', error );
+		return Promise.reject( error );
+	}
+}
