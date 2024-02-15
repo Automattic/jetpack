916ea8d9b9a3f42f3411479e277d8709d1f54aad
Donncha Ó Caoimh <5656673+donnchawp@users.noreply.github.com>
2024-02-15 12:39:51 +0000

    Boost: handle post edits and comments and delete cache files (#35397)
    * WIP in progress on handling edits in case anyone wants to see
    
    * changelog
    
    * Major cleanup of edit handling
    
    Can delete single directories now, so the homepage can be deleted.
    Also, single posts can be deleted when a comment status changes.
    If someone leaves an moderated comment, only their cache file will
    be deleted.
    
    * Updated docblock
    
    * Removed error_log debugging
    
    * Add switch_theme support, delete the entire cache
    
    * Only delete the post itself when comments are modified
    
    * Add delete_cache_for_post_only() to delete the post, not homepage
    
    * Use delete_cache_for_post_only() on comment post
    
    Fix checking the comment_approved status
    
    * Add invalidate() function to delete a path and subdirectories
    
    * Added invalidate() functions and some logging
    
    * Add some logging
    
    * Added back in deletion functions.
    
    * Delete the cache for the right URL, the post URL, not comment php file
    
    * Add $recurse to logging to make testing easier
    
    * Delete /page/ when deleting the homepage.
    
    * Handle editing comments
    
    * Simplify homepage maintenance as paged archives deleted.
    
    There's not much point in checking if the the edited post is on the
    front page if it's on the second page of the archives. So we should
    delete the homepage and page archives.
    
    * Updates to doc blocks
    
    * Add delete_cache function to delete everything.
    
    Used when switching themes
    
    * Update doc block
    
    * Simplify the post status transition action.
    
    Only delete the cache if the old or new status was published in some
    way.
    
    * Remove this code as we'll rely on post transition status
    
    * Check that $post is a WP_Post explicitely.
    
    * Fixed docblock on delete_front_page_cache()
    
    * Rename for consistency with other similar functions
    
    * Fix up docblocks in these two files
    
    * More docblock fixes
    
    * All these are basically empty()
    
    * Remove outdated comment about $recurse
    
    * These warnings should be translatable
    
    * Fix docblocks
    
    * Move is_visible_post_type() to Utils class
    
    * Remove unused param from comment
    
    * Fix docblock param
    
    * Don't delete on comments unless they're approved.
    
    Delete only: 1 unapproved -> approved
                 2 approved -> unapproved/trash/spam
    Don't delete when unapproved -> trash or spam
    
    ---------
    
    Co-authored-by: Mark George <thingalon@gmail.com>

--- projects/plugins/boost/app/modules/cache/Boost_Cache.php
+++ projects/plugins/boost/app/modules/cache/Boost_Cache.php
@@ -1,16 +1,16 @@
 <?php
+/**
+ * This file is loaded by advanced-cache.php, and so cannot rely on autoloading.
+ */
 
 namespace Automattic\Jetpack_Boost\Modules\Page_Cache;
 
-/*
- * This file is loaded by advanced-cache.php, and so cannot rely on autoloading.
- */
 require_once __DIR__ . '/Boost_Cache_Settings.php';
 require_once __DIR__ . '/Boost_Cache_Utils.php';
 require_once __DIR__ . '/storage/File_Storage.php';
 
 class Boost_Cache {
-	/*
+	/**
 	 * @var Boost_Cache_Settings - The settings for the page cache.
 	 */
 	private $settings;
@@ -20,12 +20,12 @@ class Boost_Cache {
 	 */
 	private $storage;
 
-	/*
+	/**
 	 * @var string - The normalized path for the current request. This is not sanitized. Only to be used for comparison purposes.
 	 */
 	private $request_uri = false;
 
-	/*
+	/**
 	 * @var array - The GET parameters and cookies for the current request. Everything considered in the cache key.
 	 */
 	private $request_parameters;
@@ -42,7 +42,7 @@ class Boost_Cache {
 			? $this->normalize_request_uri( $_SERVER['REQUEST_URI'] ) // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.ValidatedSanitizedInput.MissingUnslash
 			: false;
 
-		/*
+		/**
 		 * Set the cookies and get parameters for the current request.
 		 * Sometimes these arrays are modified by WordPress or other plugins.
 		 * We need to cache them here so they can be used for the cache key later.
@@ -52,9 +52,22 @@ class Boost_Cache {
 			'cookies' => $_COOKIE, // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
 			'get'     => $_GET,   // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.NonceVerification.Recommended
 		);
+
+		$this->init_actions();
 	}
 
-	/*
+	/**
+	 * Initialize the actions for the cache.
+	 */
+	protected function init_actions() {
+		add_action( 'transition_post_status', array( $this, 'delete_on_post_transition' ), 10, 3 );
+		add_action( 'transition_comment_status', array( $this, 'delete_on_comment_transition' ), 10, 3 );
+		add_action( 'comment_post', array( $this, 'delete_on_comment_post' ), 10, 3 );
+		add_action( 'edit_comment', array( $this, 'delete_on_comment_edit' ), 10, 2 );
+		add_action( 'switch_theme', array( $this, 'delete_cache' ) );
+	}
+
+	/**
 	 * Serve the cached page if it exists, otherwise start output buffering.
 	 */
 	public function serve() {
@@ -63,7 +76,7 @@ class Boost_Cache {
 		}
 	}
 
-	/*
+	/**
 	 * Returns true if the current request has a fatal error.
 	 *
 	 * @return bool
@@ -85,7 +98,7 @@ class Boost_Cache {
 		return in_array( $error['type'], $fatal_errors, true );
 	}
 
-	/*
+	/**
 	 * Returns true if the request is cacheable.
 	 *
 	 * If a request is in the backend, or is a POST request, or is not an
@@ -100,6 +113,11 @@ class Boost_Cache {
 		}
 
 		if ( defined( 'DONOTCACHEPAGE' ) ) {
+			return false;
+		}
+
+		// do not cache post previews or customizer previews
+		if ( ! empty( $_GET ) && ( isset( $_GET['preview'] ) || isset( $_GET['customize_changeset_uuid'] ) ) ) { // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.NonceVerification.Recommended
 			return false;
 		}
 
@@ -144,7 +162,7 @@ class Boost_Cache {
 		return true;
 	}
 
-	/*
+	/**
 	 * Normalize the request uri so it can be used for caching purposes.
 	 * It removes the query string and the trailing slash, and characters
 	 * that might cause problems with the filesystem.
@@ -158,7 +176,7 @@ class Boost_Cache {
 	protected function normalize_request_uri( $request_uri ) {
 		// get path from request uri
 		$request_uri = parse_url( $request_uri, PHP_URL_PATH ); // phpcs:ignore WordPress.WP.AlternativeFunctions.parse_url_parse_url
-		if ( $request_uri === '' ) {
+		if ( empty( $request_uri ) ) {
 			$request_uri = '/';
 		} elseif ( substr( $request_uri, -1 ) !== '/' ) {
 			$request_uri .= '/';
@@ -185,7 +203,7 @@ class Boost_Cache {
 		return false;
 	}
 
-	/*
+	/**
 	 * Starts output buffering and sets the callback to save the cache file.
 	 *
 	 * @return bool - false if page is not cacheable.
@@ -198,7 +216,7 @@ class Boost_Cache {
 		ob_start( array( $this, 'ob_callback' ) );
 	}
 
-	/*
+	/**
 	 * Callback function from output buffer. This function saves the output
 	 * buffer to a cache file and then returns the buffer so PHP will send it
 	 * to the browser.
@@ -218,7 +236,7 @@ class Boost_Cache {
 		return $buffer;
 	}
 
-	/*
+	/**
 	 * Returns true if the current request is one of the following:
 	 * 1. wp-admin
 	 * 2. wp-login.php, xmlrpc.php or wp-cron.php/cron request
@@ -254,5 +272,206 @@ class Boost_Cache {
 		}
 
 		return $is_backend;
+	}
+
+	/**
+	 * Delete the cache for the front page and paged archives.
+	 * This is called when a post is edited, deleted, or published.
+	 *
+	 * @param WP_Post $post - The post that should be deleted.
+	 */
+	protected function delete_cache_for_front_page() {
+		if ( get_option( 'show_on_front' ) === 'page' ) {
+			$front_page_id = get_option( 'page_on_front' ); // static page
+			if ( $front_page_id ) {
+				error_log( 'delete_cache_for_front_page: deleting front page cache' ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+				$this->delete_cache_for_post( get_post( $front_page_id ) );
+			}
+			$posts_page_id = get_option( 'page_for_posts' ); // posts page
+			if ( $posts_page_id ) {
+				error_log( 'delete_cache_for_front_page: deleting posts page cache' ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+				$this->delete_cache_for_post( get_post( $posts_page_id ) );
+			}
+		} else {
+			$this->storage->invalidate_home_page( $this->normalize_request_uri( home_url() ) );
+			error_log( 'delete front page cache ' . $this->normalize_request_uri( home_url() ) ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+		}
+	}
+
+	/**
+	 * Delete the cache for the post if the comment transitioned from one state to another.
+	 *
+	 * @param string $new_status - The new status of the comment.
+	 * @param string $old_status - The old status of the comment.
+	 * @param WP_Comment $comment - The comment that transitioned.
+	 */
+	public function delete_on_comment_transition( $new_status, $old_status, $comment ) {
+		if ( $new_status === $old_status ) {
+			return;
+		}
+		error_log( "delete_on_comment_transition: $new_status, $old_status" ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+
+		if ( $new_status !== 'approved' && $old_status !== 'approved' ) {
+			error_log( 'delete_on_comment_transition: comment not approved' ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+			return;
+		}
+
+		$post = get_post( $comment->comment_post_ID );
+		$this->delete_cache_for_post( $post );
+	}
+
+	/**
+	 * After editing a comment, delete the cache for the post if the comment is approved.
+	 * If changing state and editing, both actions will be called, but the cache will only be deleted once.
+	 *
+	 * @param int $comment_id - The id of the comment.
+	 * @param array $commentdata - The comment data.
+	 */
+	public function delete_on_comment_edit( $comment_id, $commentdata ) {
+		$post = get_post( $commentdata['comment_post_ID'] );
+
+		if ( (int) $commentdata['comment_approved'] === 1 ) {
+			$this->delete_cache_for_post( $post );
+		}
+	}
+
+	/**
+	 * After a comment is posted, delete the cache for the post if the comment is approved.
+	 * If the comment is not approved, only delete the cache for this post for this visitor.
+	 *
+	 * @param int $comment_id - The id of the comment.
+	 * @param int $comment_approved - The approval status of the comment.
+	 * @param array $commentdata - The comment data.
+	 */
+	public function delete_on_comment_post( $comment_id, $comment_approved, $commentdata ) {
+		$post = get_post( $commentdata['comment_post_ID'] );
+
+		/**
+		 * If a comment is not approved, we only need to delete the cache for
+		 * this post for this visitor so the unmoderated comment is shown to them.
+		 */
+		if ( $comment_approved !== 1 ) {
+			$this->storage->invalidate_single_visitor( $this->normalize_request_uri( get_permalink( $post->ID ) ), $this->request_parameters );
+			return;
+		}
+
+		$this->delete_cache_for_post( $post );
+	}
+
+	/**
+	 * Returns true if the post is published or private.
+	 *
+	 * @param string $status - The status of the post.
+	 * @return bool
+	 */
+	private function is_published( $status ) {
+		return $status === 'publish' || $status === 'private';
+	}
+
+	/**
+	 * Delete the cached post if it transitioned from one state to another.
+	 *
+	 * @param string $new_status - The new status of the post.
+	 * @param string $old_status - The old status of the post.
+	 * @param WP_Post $post - The post that transitioned.
+	 */
+	public function delete_on_post_transition( $new_status, $old_status, $post ) {
+		if ( ! Boost_Cache_Utils::is_visible_post_type( $post ) ) {
+			return;
+		}
+
+		error_log( "delete_on_post_transition: $new_status, $old_status, {$post->ID}" ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+
+		// Don't delete the cache for posts that weren't published and aren't published now
+		if ( ! $this->is_published( $new_status ) && ! $this->is_published( $old_status ) ) {
+			error_log( 'delete_on_post_transition: not published' ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+			return;
+		}
+
+		error_log( "delete_on_post_transition: deleting post {$post->ID}" ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+
+		$this->delete_cache_for_post( $post );
+		$this->delete_cache_for_post_terms( $post );
+		$this->delete_cache_for_front_page();
+	}
+
+	/**
+	 * Deletes cache files for the given post.
+	 *
+	 * @param WP_Post $post - The post to delete the cache file for.
+	 */
+	public function delete_cache_for_post( $post ) {
+		static $already_deleted = -1;
+		if ( $already_deleted === $post->ID ) {
+			return;
+		}
+
+		/**
+		 * Don't delete the cache for post types that are not public.
+		 */
+		if ( ! Boost_Cache_Utils::is_visible_post_type( $post ) ) {
+			return;
+		}
+
+		$already_deleted = $post->ID;
+
+		/**
+		 * If a post is unpublished, the permalink will be deleted. In that case,
+		 * get_sample_permalink() will return a permalink with ?p=123 instead of
+		 * the post name. We need to get the post name from the post object.
+		 */
+		$permalink = get_permalink( $post->ID );
+		if ( strpos( $permalink, '?p=' ) !== false ) {
+			if ( ! function_exists( 'get_sample_permalink' ) ) {
+				require_once ABSPATH . 'wp-admin/includes/post.php';
+			}
+			list( $permalink, $post_name ) = get_sample_permalink( $post );
+			$permalink                     = str_replace( array( '%postname%', '%pagename%' ), $post_name, $permalink );
+		}
+
+		error_log( "delete_cache_for_post: $permalink" ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+		$this->delete_cache_for_url( $permalink );
+	}
+
+	/**
+	 * Delete the cache for terms associated with this post.
+	 *
+	 * @param WP_Post $post - The post to delete the cache for.
+	 */
+	public function delete_cache_for_post_terms( $post ) {
+		$categories = get_the_category( $post->ID );
+		if ( is_array( $categories ) ) {
+			foreach ( $categories as $category ) {
+				$link = trailingslashit( get_category_link( $category->term_id ) );
+				$this->delete_cache_for_url( $link );
+			}
+		}
+
+		$tags = get_the_tags( $post->ID );
+		if ( is_array( $tags ) ) {
+			foreach ( $tags as $tag ) {
+				$link = trailingslashit( get_tag_link( $tag->term_id ) );
+				$this->delete_cache_for_url( $link );
+			}
+		}
+	}
+
+	/**
+	 * Delete the cache for the given url.
+	 *
+	 * @param string $url - The url to delete the cache for.
+	 */
+	public function delete_cache_for_url( $url ) {
+		error_log( 'delete_cache_for_url: ' . $url ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+		$path = $this->normalize_request_uri( $url );
+
+		return $this->storage->invalidate( $path );
+	}
+
+	/**
+	 * Delete the entire cache.
+	 */
+	public function delete_cache() {
+		return $this->delete_cache_for_url( home_url() );
 	}
 }
