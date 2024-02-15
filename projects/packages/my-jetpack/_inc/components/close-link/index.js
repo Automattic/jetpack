2fbf7fddef43739985491a68ff925facaf79379c
Kevin Zoschke <kevin.zoschke@automattic.com>
2024-02-15 10:44:46 -0500

    My Jetpack: add label for screen readers to connect page close button

--- projects/packages/my-jetpack/_inc/components/close-link/index.js
+++ projects/packages/my-jetpack/_inc/components/close-link/index.js
@@ -4,10 +4,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './styles.module.scss';
 
-const CloseLink = ( { className } ) => {
+const CloseLink = ( { className, accessibleName } ) => {
 	return (
 		<Link to="/" className={ classNames( styles.link, className ) }>
 			<Icon icon={ close } className={ styles.icon } />
+			{ /* Screen reader users require a textual information of what the button does. */ }
+			<span className={ styles[ 'visually-hidden' ] }>{ accessibleName }</span>
 		</Link>
 	);
 };
