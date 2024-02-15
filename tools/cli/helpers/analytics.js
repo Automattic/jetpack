d12c34628b1f22fb5d2bcd8c864745b783e853c0
Samiff <samiff@users.noreply.github.com>
2024-02-14 23:14:35 -0700

    Refactor analytics.js
    - Store a uuid for the user with Configstore
    - Send the Tracks POST request

--- tools/cli/helpers/analytics.js
+++ tools/cli/helpers/analytics.js
@@ -1,16 +1,20 @@
+import crypto from 'crypto';
+import axios from 'axios';
 import Configstore from 'configstore';
 import enquirer from 'enquirer';
 
 const configStore = new Configstore( 'automattic/jetpack-cli/cli' );
+const baseEventName = 'jetpack_test_cli_'; // Todo: remove 'test' prefix before merging and after testing.
 
 /**
  * Checks if:
  * - the user has been asked to enable analytics tracking.
  * - the user has enabled or disabled analytics tracking.
+ * - the user has a UUID set.
  *
  * @returns {boolean} Whether analytics tracking is enabled.
  */
-async function analyticsEnabled() {
+async function checkAnalyticsEnabled() {
 	if ( configStore.get( 'askedToEnableAnalytics' ) === undefined ) {
 		const prompt = await enquirer.prompt( [
 			{
@@ -29,19 +33,47 @@ async function analyticsEnabled() {
 				: "We appreciate your privacy. If you'd like to enable analytics tracking in the future, run: jetpack cli analytics"
 		);
 	}
+
+	if ( ! configStore.get( 'uuid' ) ) {
+		configStore.set( 'uuid', crypto.randomUUID() );
+	}
+
 	return configStore.get( 'analyticsEnabled' );
 }
 
 /**
  * Record a Tracks event, see: PCYsg-4KT-p2
  *
- * @param {string} eventName - The name of the event to track.
+ * @param {string} eventName - Name to track in format <action>_<optional qualifier> (e.g. 'rsync_watch').
  */
-export async function tracks( eventName ) {
-	if ( ! ( await analyticsEnabled() ) ) {
+export async function tracks( eventName = 'uncategorized' ) {
+	if ( ! ( await checkAnalyticsEnabled() ) ) {
 		// Bail if the user has disabled analytics tracking.
 		return;
 	}
 
-	console.log( `Todo: testing tracks(). Event name: ${ eventName }` );
+	try {
+		await axios.post(
+			'https://public-api.wordpress.com/rest/v1.1/tracks/record?http_envelope=1',
+			{
+				commonProps: {
+					_ul: 'jetpackisbestpack',
+					uuid: configStore.get( 'uuid' ),
+				},
+				events: [
+					{
+						_en: baseEventName + eventName,
+					},
+				],
+			},
+			{
+				headers: {
+					'User-Agent': 'Node.js Jetpack CLI',
+					'Content-Type': 'application/json',
+				},
+			}
+		);
+	} catch ( error ) {
+		console.error( error );
+	}
 }
