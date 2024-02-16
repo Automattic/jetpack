4f66af1a13d89003021538543a7395ee27f27a68
dkmyta <dkmyta10@gmail.com>
2024-02-16 13:58:43 -0800

    Remove FirewallHeader stories edits

--- projects/plugins/protect/src/js/components/firewall-header/stories/index.stories.jsx
+++ projects/plugins/protect/src/js/components/firewall-header/stories/index.stories.jsx
@@ -12,38 +12,28 @@ export const FirewallOn = Template.bind( {} );
 FirewallOn.args = {
 	status: 'on',
 	hasRequiredPlan: false,
-	oneDayStats: 25,
-	thirtyDayStats: 250,
 };
 
 export const FirewallOnPaid = Template.bind( {} );
 FirewallOnPaid.args = {
 	status: 'on',
 	hasRequiredPlan: true,
-	oneDayStats: 25,
-	thirtyDayStats: 250,
 };
 
 export const FirewallOff = Template.bind( {} );
 FirewallOff.args = {
 	status: 'off',
 	hasRequiredPlan: false,
-	oneDayStats: 25,
-	thirtyDayStats: 250,
 };
 
 export const FirewallOffPaid = Template.bind( {} );
 FirewallOffPaid.args = {
 	status: 'off',
 	hasRequiredPlan: true,
-	oneDayStats: 25,
-	thirtyDayStats: 250,
 };
 
 export const FirewallLoading = Template.bind( {} );
 FirewallLoading.args = {
 	status: 'loading',
 	hasRequiredPlan: true,
-	oneDayStats: 25,
-	thirtyDayStats: 250,
 };
