33bbfac2ba10c8693e1e3f0eda1ab5533b46790b
Jeremy Herve <jeremy@jeremy.hu>
2024-02-19 17:10:59 +0100

    WooCommerce Analytics: introduce new PHP package
    Fixes #35629
    
    It will be home to the WooComerce Analytics codebase currently living under modules/woocommerce-analytics in the Jetpack plugin.

---
+++ projects/packages/woocommerce-analytics/src/class-woocommerce-analytics.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Package description here
+ *
+ * @package automattic/woocommerce-analytics
+ */
+
+namespace Automattic\Jetpack;
+
+/**
+ * Class description.
+ */
+class Woocommerce_Analytics {
+
+	const PACKAGE_VERSION = '1.0.0-alpha';
+}
