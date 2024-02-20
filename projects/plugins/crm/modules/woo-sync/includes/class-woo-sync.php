c1d1b78a37c5643d128d8fef389729469989e593
tbradsha <32492176+tbradsha@users.noreply.github.com>
2024-02-20 12:45:15 -0700

    More robust HPOS check (supports older Woo versions)

--- projects/plugins/crm/modules/woo-sync/includes/class-woo-sync.php
+++ projects/plugins/crm/modules/woo-sync/includes/class-woo-sync.php
@@ -469,9 +469,7 @@ class Woo_Sync {
 	 * @return bool Defaults to false.
 	 */
 	public function is_hpos_enabled() {
-		global $zbs;
-
-		if ( $zbs->woocommerce_is_active() ) {
+		if ( class_exists('Automattic\WooCommerce\Utilities\OrderUtil') ) {
 			return \Automattic\WooCommerce\Utilities\OrderUtil::custom_orders_table_usage_is_enabled();
 		}
 		return false;
