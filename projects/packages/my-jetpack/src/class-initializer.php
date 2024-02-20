50a2639c9778fcf50b906af2fc3ac1f9c3078c83
Andrii <lysenkoa.work@gmail.com>
2024-02-20 10:25:08 -0800

    updating versions

--- projects/packages/my-jetpack/src/class-initializer.php
+++ projects/packages/my-jetpack/src/class-initializer.php
@@ -35,7 +35,7 @@ class Initializer {
 	 *
 	 * @var string
 	 */
-	const PACKAGE_VERSION = '4.10.0';
+	const PACKAGE_VERSION = '4.10.1-alpha';
 
 	/**
 	 * HTML container ID for the IDC screen on My Jetpack page.
