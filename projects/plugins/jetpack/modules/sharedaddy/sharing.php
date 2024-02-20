38abc51fd4ebb422a85920a5880e5b19106671d0
Griffith Chen <griffith.chen@automattic.com>
2024-02-20 21:54:05 +0800

    Remove Sharedaddy action subscription_menu

--- projects/plugins/jetpack/modules/sharedaddy/sharing.php
+++ projects/plugins/jetpack/modules/sharedaddy/sharing.php
@@ -29,7 +29,15 @@ class Sharing_Admin {
 		require_once WP_SHARING_PLUGIN_DIR . 'sharing-service.php';
 
 		add_action( 'admin_init', array( $this, 'admin_init' ) );
-		add_action( 'admin_menu', array( $this, 'subscription_menu' ) );
+
+		/**
+		 * The following action is commented out since it creates an entry to a legacy page.
+		 *
+		 * This menu item was previously hidden via https://github.com/Automattic/jetpack/pull/1960
+		 * but now we are preventing its registration altogether.
+		 *
+		 * add_action( 'admin_menu', array( $this, 'subscription_menu' ) );
+		 */
 
 		// Insert our CSS and JS
 		add_action( 'load-settings_page_sharing', array( $this, 'sharing_head' ) );
