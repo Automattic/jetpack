fe174890289c8ae7bf8a84aa47a183b0c5f83841
Ian Ramos <5714212+IanRamosC@users.noreply.github.com>
2024-02-20 17:47:08 -0300

    Fix changelog

--- projects/packages/my-jetpack/src/class-initializer.php
+++ projects/packages/my-jetpack/src/class-initializer.php
@@ -35,7 +35,7 @@ class Initializer {
 	 *
 	 * @var string
 	 */
-	const PACKAGE_VERSION = '4.10.0';
+	const PACKAGE_VERSION = '4.11.0-alpha';
 
 	/**
 	 * HTML container ID for the IDC screen on My Jetpack page.
