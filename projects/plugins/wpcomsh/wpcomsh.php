547830ec2e80fa805a45a538809fd1b47a258bfb
Igor Zinovyev <zinigor@gmail.com>
2024-02-20 21:17:16 +0300

    Generated package skeleton.

---
+++ projects/plugins/wpcomsh/wpcomsh.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *
+ * Plugin Name: Jetpack wpcomsh
+ * Plugin URI: TBD
+ * Description: A helper for connecting WordPress.com sites to external host infrastructure.
+ * Version: 3.14.40
+ * Author: Automattic
+ * Author URI: https://jetpack.com/
+ * License: GPLv2 or later
+ * Text Domain: jetpack
+ *
+ * @package automattic/jetpack-wpcomsh
+ */
+
+// Code some good stuff!
