a7f5c64f9a1494efdad99be5716a29158a67e0e0
Peter Petrov <peter.petrov89@gmail.com>
2024-02-20 13:43:19 +0200

    Add error boundary for PageCache component

--- projects/plugins/boost/app/assets/src/js/features/page-cache/page-cache.tsx
+++ projects/plugins/boost/app/assets/src/js/features/page-cache/page-cache.tsx
@@ -1,6 +1,9 @@
+import { __ } from '@wordpress/i18n';
 import Meta from '$features/page-cache/meta/meta';
 import Health from '$features/page-cache/health/health';
 import { usePageCacheErrorDS } from '$lib/stores/page-cache';
+import ErrorBoundary from '$features/error-boundary/error-boundary';
+import ErrorNotice from '$features/error-notice/error-notice';
 
 const PageCache = () => {
 	const pageCacheError = usePageCacheErrorDS();
@@ -8,4 +11,17 @@ const PageCache = () => {
 	return <>{ pageCacheError ? <Health error={ pageCacheError } /> : <Meta /> }</>;
 };
 
-export default PageCache;
+export default () => {
+	return (
+		<ErrorBoundary
+			fallback={
+				<ErrorNotice
+					title={ __( 'Error', 'jetpack-boost' ) }
+					error={ new Error( __( 'Unable to load Cache settings.', 'jetpack-boost' ) ) }
+				/>
+			}
+		>
+			<PageCache />
+		</ErrorBoundary>
+	);
+};
