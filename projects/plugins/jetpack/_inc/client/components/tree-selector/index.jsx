4d2eff19fe96fc9b2f375e93fdaf0d3472d631f9
Paulo Cruz <paulo.cruz@automattic.com>
2024-02-21 18:17:08 -0300

    Display no results message

--- projects/plugins/jetpack/_inc/client/components/tree-selector/index.jsx
+++ projects/plugins/jetpack/_inc/client/components/tree-selector/index.jsx
@@ -1,5 +1,7 @@
 import { CheckboxControl } from '@wordpress/components';
-import { useCallback } from 'react';
+import { createInterpolateElement } from '@wordpress/element';
+import { __, sprintf } from '@wordpress/i18n';
+import { useCallback, useMemo } from 'react';
 import './style.scss';
 import { createFlatTreeItems } from './utils';
 
@@ -46,7 +48,30 @@ const TreeSelector = props => {
 		</li>
 	) );
 
-	return <ul className="jp-tree-items">{ treeElements }</ul>;
+	const noResultsMessage = useMemo(
+		() => (
+			<span>
+				{ createInterpolateElement(
+					// translators: %s is the keyword being searched
+					sprintf( __( 'No results found for <b>%s</b>.', 'jetpack' ), keyword ),
+					{
+						b: <b />,
+					}
+				) }
+			</span>
+		),
+		[ keyword ]
+	);
+
+	return (
+		<ul className="jp-tree-items">
+			{ isSearching && filteredTreeItems.length === 0 ? (
+				<li className="jp-tree-item jp-tree-item__no-results">{ noResultsMessage }</li>
+			) : (
+				treeElements
+			) }
+		</ul>
+	);
 };
 
 export default TreeSelector;
