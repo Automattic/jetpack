5a5f96ac3b2c3ba669261fe12ecd7db97cfa60e8
Ian Ramos <5714212+IanRamosC@users.noreply.github.com>
2024-02-21 10:20:13 -0300

    Update My Jetpack interstitial click events (#35740)
    * Add productSlug to Tracks event on interstitials
    
    * changelog
    
    * Refactor logic to get the product slug
    
    * Fix changelog

--- projects/packages/my-jetpack/src/class-initializer.php
+++ projects/packages/my-jetpack/src/class-initializer.php
@@ -35,7 +35,7 @@ class Initializer {
 	 *
 	 * @var string
 	 */
-	const PACKAGE_VERSION = '4.10.1-alpha';
+	const PACKAGE_VERSION = '4.11.0-alpha';
 
 	/**
 	 * HTML container ID for the IDC screen on My Jetpack page.
