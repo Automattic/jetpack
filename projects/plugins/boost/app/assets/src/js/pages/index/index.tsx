15423e6ed8c482b8eafeb19ca74689d94d60239c
Peter Petrov <peter.petrov89@gmail.com>
2024-02-22 17:22:20 +0200

    Hide Super Cache notice if page cache feature is active

--- projects/plugins/boost/app/assets/src/js/pages/index/index.tsx
+++ projects/plugins/boost/app/assets/src/js/pages/index/index.tsx
@@ -24,6 +24,7 @@ const Index = () => {
 
 	const [ isaState ] = useSingleModuleState( 'image_size_analysis' );
 	const [ imageCdn ] = useSingleModuleState( 'image_cdn' );
+	const [ pageCache ] = useSingleModuleState( 'page_cache' );
 
 	const regenerateCssAction = useRegenerateCriticalCssAction();
 	const requestRegenerateCriticalCss = () => {
@@ -286,7 +287,7 @@ const Index = () => {
 				</Module>
 			</div>
 
-			<SuperCacheInfo />
+			{ ! pageCache?.active && <SuperCacheInfo /> }
 		</div>
 	);
 };
