9abd5e3a8f906847a20c20711a7ced1c09f99f7b
Ashar Fuadi <ashar.fuadi@automattic.com>
2024-02-22 12:53:13 +0700

    Untangle: Temporarily point WordPress.com menu to wpcalypso https://github.com/Automattic/wpcomsh/pull/1711

--- projects/plugins/wpcomsh/feature-plugins/nav-redesign.php
+++ projects/plugins/wpcomsh/feature-plugins/nav-redesign.php
@@ -19,3 +19,23 @@ function wpcom_is_nav_redesign_enabled() {
 
 	return apply_filters( 'is_nav_redesign_enabled', $is_proxied && $uses_wp_admin_interface );
 }
+
+/**
+ * Temporarily override the `< WordPress.com` menu to link wpcalypso.wordpress.com,
+ * which enables the nav redesign by default.
+ */
+function override_wpcom_menu_to_wpcalypso() {
+	global $menu;
+
+	if ( ! wpcom_is_nav_redesign_enabled() ) {
+		return;
+	}
+
+	$wpcom_menu_position = 0;
+	if ( isset( $menu[ $wpcom_menu_position ] ) ) {
+		$wpcom_menu                   = $menu[ $wpcom_menu_position ];
+		$wpcom_menu[2]                = str_replace( 'https://wordpress.com/', 'https://wpcalypso.wordpress.com/', $wpcom_menu[2] );
+		$menu[ $wpcom_menu_position ] = $wpcom_menu; // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+	}
+}
+add_action( 'admin_menu', 'override_wpcom_menu_to_wpcalypso', 99999 );
