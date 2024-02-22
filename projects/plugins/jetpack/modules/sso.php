5e10731ac2d0cbd8b1591e1e3612c6458971f861
Anthony Grullon <agrullon95@gmail.com>
2024-02-21 21:12:16 -0500

    Disable SSO user admin changes

--- projects/plugins/jetpack/modules/sso.php
+++ projects/plugins/jetpack/modules/sso.php
@@ -42,7 +42,6 @@ class Jetpack_SSO {
 	private function __construct() {
 
 		self::$instance = $this;
-		new Jetpack_SSO_User_Admin();
 
 		add_action( 'admin_init', array( $this, 'maybe_authorize_user_after_sso' ), 1 );
 		add_action( 'admin_init', array( $this, 'register_settings' ) );
