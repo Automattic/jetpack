77ce0a8c29db8873488d93aef49742a95eb1b81b
jboland88 <18016357+jboland88@users.noreply.github.com>
2024-02-22 10:28:06 -0500

    Remove translation of product names in My Jetpack (#35830)
    * Remove translation of product names
    
    * changelog
    
    * Version management

--- projects/packages/my-jetpack/src/class-initializer.php
+++ projects/packages/my-jetpack/src/class-initializer.php
@@ -35,7 +35,7 @@ class Initializer {
 	 *
 	 * @var string
 	 */
-	const PACKAGE_VERSION = '4.11.0';
+	const PACKAGE_VERSION = '4.11.1-alpha';
 
 	/**
 	 * HTML container ID for the IDC screen on My Jetpack page.
