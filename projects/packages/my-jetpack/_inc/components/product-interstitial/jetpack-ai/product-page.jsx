6d485bea8158cc9e68ab04de8527e93c5939748c
Christian Gastrell <cgastrell@gmail.com>
2024-02-23 15:04:55 -0300

    refine some styles across pricing table and product page

--- projects/packages/my-jetpack/_inc/components/product-interstitial/jetpack-ai/product-page.jsx
+++ projects/packages/my-jetpack/_inc/components/product-interstitial/jetpack-ai/product-page.jsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { AdminPage, Col, Container /*, JetpackLogo */ } from '@automattic/jetpack-components';
+import { AdminPage, Col, Container, JetpackLogo } from '@automattic/jetpack-components';
 import { Card } from '@wordpress/components';
 import classnames from 'classnames';
 /**
@@ -30,14 +30,12 @@ export default function () {
 				className={ styles[ 'product-interstitial__container' ] }
 			>
 				<Col className={ classnames( styles[ 'product-interstitial__section' ] ) }>
-					<div className={ styles[ 'product-interstitial__header' ] }>
+					<div className={ styles[ 'product-interstitial__section-wrapper-wide' ] }>
 						<GoBackLink onClick={ onClickGoBack } />
 					</div>
-				</Col>
-				{ /* <Col className={ styles[ 'product-interstitial__section' ] }>
 					<div
 						className={ classnames(
-							styles[ 'product-interstitial__section-body' ],
+							styles[ 'product-interstitial__section-wrapper-wide' ],
 							styles[ 'product-interstitial__product-header' ]
 						) }
 					>
@@ -46,7 +44,7 @@ export default function () {
 							AI Assistant
 						</div>
 					</div>
-				</Col> */ }
+				</Col>
 				<Col
 					className={ classnames(
 						styles[ 'product-interstitial__hero-section' ],
@@ -54,8 +52,8 @@ export default function () {
 					) }
 				>
 					<div className={ styles[ 'product-interstitial__hero-content' ] }>
-						<h1 className={ styles[ 'product-interstitial__hero-title' ] }>{ description }</h1>
-						<div className={ styles[ 'product-interstitial__hero-text' ] }>
+						<h1 className={ styles[ 'product-interstitial__hero-heading' ] }>{ description }</h1>
+						<div className={ styles[ 'product-interstitial__hero-sub-heading' ] }>
 							Draft, transform, translate, and alter both new and existing content leveraging the
 							capabilities of AI, inside the block editor.
 						</div>
@@ -66,18 +64,22 @@ export default function () {
 					</div>
 				</Col>
 				<Col className={ styles[ 'product-interstitial__section' ] }>
-					<div className={ styles[ 'product-interstitial__section-body' ] }>
-						<h2>AI Features</h2>
-						<p>Discover all the Jetpack features powered by AI</p>
+					<div className={ styles[ 'product-interstitial__section-wrapper' ] }>
+						<h2 className={ styles[ 'product-interstitial__section-heading' ] }>AI Features</h2>
+						<p className={ styles[ 'product-interstitial__section-sub-heading' ] }>
+							Discover all the Jetpack features powered by AI
+						</p>
 						<div>presentational card with video</div>
 						<div>presentational card with video</div>
 						<div>presentational card with video</div>
 					</div>
 				</Col>
 				<Col className={ styles[ 'product-interstitial__section' ] }>
-					<div className={ styles[ 'product-interstitial__section-body' ] }>
-						<h2>Do you have any feedback?</h2>
-						<p>
+					<div className={ styles[ 'product-interstitial__section-wrapper' ] }>
+						<h2 className={ styles[ 'product-interstitial__section-heading' ] }>
+							Do you have any feedback?
+						</h2>
+						<p className={ styles[ 'product-interstitial__section-sub-heading' ] }>
 							Help us improving the accuracy of our results and feel free to give us ideas for
 							future implementations and improvements. Share your feedback!
 						</p>
