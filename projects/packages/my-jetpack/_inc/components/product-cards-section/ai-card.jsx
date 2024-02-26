7d6c3046a2c68b62d4eacdd48ec33ea298520f23
Christian Gastrell <cgastrell@gmail.com>
2024-02-26 11:48:07 -0300

    override card case can_upgrade to point to product page (#35915)

--- projects/packages/my-jetpack/_inc/components/product-cards-section/ai-card.jsx
+++ projects/packages/my-jetpack/_inc/components/product-cards-section/ai-card.jsx
@@ -3,7 +3,19 @@ import React from 'react';
 import ProductCard from '../connected-product-card';
 
 const AiCard = ( { admin } ) => {
-	return <ProductCard admin={ admin } slug="jetpack-ai" upgradeInInterstitial={ true } />;
+	const overrides = {
+		can_upgrade: {
+			href: '#/jetpack-ai',
+		},
+	};
+	return (
+		<ProductCard
+			admin={ admin }
+			slug="jetpack-ai"
+			upgradeInInterstitial={ true }
+			primaryActionOverride={ overrides }
+		/>
+	);
 };
 
 AiCard.propTypes = {
