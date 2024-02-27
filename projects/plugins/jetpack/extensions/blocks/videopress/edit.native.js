7194ccbae51ac79ffc2e73fdbe172f12b7820f97
Siobhan Bamber <siobhan@automattic.com>
2024-02-27 10:20:33 +0000

    RNMobile: Detect and avoid invalid media URLs in VideoPress v5 block (#35961)

--- projects/plugins/jetpack/extensions/blocks/videopress/edit.native.js
+++ projects/plugins/jetpack/extensions/blocks/videopress/edit.native.js
@@ -206,7 +206,7 @@ class VideoPressEdit extends Component {
 	onSelectURL( url ) {
 		const { createErrorNotice, setAttributes, onReplace } = this.props;
 
-		if ( isURL( url ) ) {
+		if ( isURL( url ) && /^https?:/.test( getProtocol( url ) ) ) {
 			// Check if there's an embed block that handles this URL.
 			const embedBlock = createVideoPressEmbedBlock( {
 				attributes: { url },
