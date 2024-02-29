19ac65137294e6c955dd2345fcb6f4772cddc457
Adnan Haque <3737780+haqadn@users.noreply.github.com>
2024-02-29 18:51:54 +0600

    Boost: Fix cache translations (#36062)
    * Remove translations from pre-wordpress files
    
    * Remove messages from WP_Errors that aren't used
    
    * changelog

--- projects/plugins/boost/app/modules/optimizations/page-cache/Page_Cache_Setup.php
+++ projects/plugins/boost/app/modules/optimizations/page-cache/Page_Cache_Setup.php
@@ -55,7 +55,7 @@ class Page_Cache_Setup {
 		wp_delete_file( $filename );
 
 		if ( $result === false ) {
-			return new \WP_Error( 'wp-content-not-writable', 'wp-content directory is not writeable' );
+			return new \WP_Error( 'wp-content-not-writable' );
 		}
 
 		return true;
@@ -68,7 +68,7 @@ class Page_Cache_Setup {
 		global $wp_rewrite;
 
 		if ( ! $wp_rewrite || ! $wp_rewrite->using_permalinks() ) {
-			return new \WP_Error( 'not-using-permalinks', 'This site does not appear to use permalinks' );
+			return new \WP_Error( 'not-using-permalinks' );
 		}
 	}
 
@@ -96,11 +96,11 @@ class Page_Cache_Setup {
 			$content = file_get_contents( $advanced_cache_filename ); // phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
 
 			if ( strpos( $content, 'WP SUPER CACHE' ) !== false ) {
-				return new \WP_Error( 'advanced-cache-for-super-cache', 'advanced-cache.php exists, but belongs to WP Super Cache.' );
+				return new \WP_Error( 'advanced-cache-for-super-cache' );
 			}
 
 			if ( strpos( $content, Page_Cache::ADVANCED_CACHE_SIGNATURE ) === false ) {
-				return new \WP_Error( 'advanced-cache-incompatible', 'advanced-cache.php exists, but belongs to another plugin/system.' );
+				return new \WP_Error( 'advanced-cache-incompatible' );
 			}
 
 			if ( strpos( $content, Page_Cache::ADVANCED_CACHE_VERSION ) !== false ) {
@@ -112,7 +112,7 @@ class Page_Cache_Setup {
 		$plugin_dir_name      = untrailingslashit( str_replace( JETPACK_BOOST_PLUGIN_FILENAME, '', JETPACK_BOOST_PLUGIN_BASE ) );
 		$boost_cache_filename = WP_CONTENT_DIR . '/plugins/' . $plugin_dir_name . '/app/modules/optimizations/page-cache/pre-wordpress/Boost_Cache.php';
 		if ( ! file_exists( $boost_cache_filename ) ) {
-			return new \WP_Error( 'boost-cache-file-not-found', 'Boost_Cache.php not found' );
+			return new \WP_Error( 'boost-cache-file-not-found' );
 		}
 		$contents = '<?php
 // ' . Page_Cache::ADVANCED_CACHE_SIGNATURE . ' - ' . Page_Cache::ADVANCED_CACHE_VERSION . '
@@ -127,7 +127,7 @@ $boost_cache->serve();
 
 		$write_advanced_cache = Filesystem_Utils::write_to_file( $advanced_cache_filename, $contents );
 		if ( $write_advanced_cache instanceof Boost_Cache_Error ) {
-			return new \WP_Error( 'unable-to-write-to-advanced-cache', $write_advanced_cache->get_error_message() );
+			return new \WP_Error( 'unable-to-write-to-advanced-cache', $write_advanced_cache->get_error_code() );
 		}
 
 		if ( function_exists( 'opcache_invalidate' ) ) {
@@ -151,7 +151,7 @@ $boost_cache->serve();
 			 * in wp-config.php.
 			 */
 			if ( defined( 'WP_CACHE' ) && ! WP_CACHE ) {
-				return new \WP_Error( 'wp-cache-defined-not-true', 'WP_CACHE is defined but not true' );
+				return new \WP_Error( 'wp-cache-defined-not-true' );
 			}
 
 			return true; // WP_CACHE already added.
@@ -165,7 +165,7 @@ define( \'WP_CACHE\', true ); // ' . Page_Cache::ADVANCED_CACHE_SIGNATURE,
 
 		$result = file_put_contents( ABSPATH . 'wp-config.php', $content ); // phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_operations_file_put_contents
 		if ( $result === false ) {
-			return new \WP_Error( 'wp-config-not-writable', 'Could not write to wp-config.php' );
+			return new \WP_Error( 'wp-config-not-writable' );
 		}
 
 		if ( function_exists( 'opcache_invalidate' ) ) {
