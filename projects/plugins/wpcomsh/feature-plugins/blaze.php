4c5da0ba999326ea238aef02e641d269957fb4cd
arthur791004 <arthur.chu@automattic.com>
2024-02-29 09:37:46 +0800

    Blaze: Activate the moule when the site is launching https://github.com/Automattic/wpcomsh/pull/1720

--- projects/plugins/wpcomsh/feature-plugins/blaze.php
+++ projects/plugins/wpcomsh/feature-plugins/blaze.php
@@ -7,16 +7,12 @@
  */
 
 /**
- * Force-enable the Blaze module
+ * Activate the Blaze module
  * If you use a version of Jetpack that supports it,
  * and if it is not already enabled.
  */
 function wpcomsh_activate_blaze_module() {
-	if (
-		! defined( 'JETPACK__VERSION' )
-		|| ! class_exists( 'Jetpack' )
-		|| 'wp-admin' === get_option( 'wpcom_admin_interface' )
-	) {
+	if ( ! defined( 'JETPACK__VERSION' ) || ! class_exists( 'Jetpack' ) ) {
 		return;
 	}
 
@@ -29,7 +25,18 @@ function wpcomsh_activate_blaze_module() {
 		Jetpack::activate_module( 'blaze', false, false );
 	}
 }
-add_action( 'init', 'wpcomsh_activate_blaze_module', 0, 0 );
+
+/**
+ * Force-enable the Blaze module
+ */
+function wpcomsh_force_activate_blaze_module() {
+	if ( 'wp-admin' === get_option( 'wpcom_admin_interface' ) ) {
+		return;
+	}
+
+	wpcomsh_activate_blaze_module();
+}
+add_action( 'init', 'wpcomsh_force_activate_blaze_module', 0, 0 );
 
 /**
  * Remove Blaze from the old Module list.
@@ -45,3 +52,19 @@ function wpcomsh_rm_blaze_module_list( $items ) {
 	return $items;
 }
 add_filter( 'jetpack_modules_list_table_items', 'wpcomsh_rm_blaze_module_list' );
+
+/**
+ * The Blaze module may not be auto-activated when the site is not public,
+ * so we have to activate the module when it's public manually.
+ *
+ * @param number $old_value of blog_public option.
+ * @param number $new_value of blog_public option.
+ */
+function wpcomsh_activate_blaze_module_on_launching( $old_value, $new_value ) {
+	$blog_public = (int) $new_value;
+	// 'blog_public' is set to '1' when a site is launched.
+	if ( $blog_public === 1 ) {
+		wpcomsh_activate_blaze_module();
+	}
+}
+add_filter( 'update_option_blog_public', 'wpcomsh_activate_blaze_module_on_launching', 10, 2 );
