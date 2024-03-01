ff706107e00bb1213e5d25cfcaa8e659fad2006f
Nauris Pūķis <hi@pyronaur.com>
2024-03-01 05:29:15 +0200

    Jetpack Boost: Improved page cache and error handling (#36022)
    * Restore error formatting
    
    * Add plugins/boost changelog entry.
    
    * Fix modulestate type
    
    * Auto-disble page cache module on error
    
    * Prevent excessive flickering
    
    * Dedupe requests, more responsive UI
    
    * Add plugins/boost changelog entry.
    
    * unused async
    
    * monolintfix
    
    * Remove outdated comment
    
    * Remove extra pagecache active checks for now
    
    * Make linter happy
    
    * Show notice when enabling Page Cache module (before setup is ran)
    
    * Remove page cache setup notice before enabling page cache
    
    * Add `useMutationNotice` hook
    
    * Remove unused meta.tsx
    
    * Show "setting up cache" notice earlier
    
    * Also add idle and reset to MutationSubset
    
    * Flip the useMutationNotice args for better devex
    
    * Add js-packages/react-data-sync-client changelog entry.
    
    * Clean up
    
    * // todo
    
    * Update hook docblock comment
    
    * Move auto-dismissal to manager.tsx
    
    * Remove `hasNotice`
    
    * removePageCacheNotice
    
    * `isPending` > `isSuccess` - even if isSuccess exists, use `isPending`
    
    So if a mutation happens over and over again, isPending is going to be re-triggered and notice is going to flip to isPending state again, even if `isSuccess` is stuck for some reason.
    
    * Only auto-dismiss if a notice isn't pending
    
    * `setNotice` is a dependency
    
    Not sure if this is necessary, but looks like it doesn't hurt
    
    * Clear the notice before turning the module off
    
    * Set the timeout back to 5s
    
    ---------
    
    Co-authored-by: Peter Petrov <peter.petrov89@gmail.com>
    Co-authored-by: Mark George <thingalon@gmail.com>

--- projects/plugins/boost/app/assets/src/js/features/ui/mutation-notice/mutation-notice.tsx
+++ projects/plugins/boost/app/assets/src/js/features/ui/mutation-notice/mutation-notice.tsx
@@ -1,34 +1,35 @@
-import React, { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { __ } from '@wordpress/i18n';
 import { useNotices } from '$features/notice/context';
 
-type MutationNoticeProps = {
-	mutationId: string;
+export type MutationNoticeState = {
 	isSuccess: boolean;
 	isError: boolean;
 	isPending: boolean;
+	isIdle: boolean;
+	reset?: () => void;
+};
+
+type MutationNoticeMessages = {
 	savingMessage?: string;
 	errorMessage?: string;
 	successMessage?: string;
 };
 
 /**
- * Mutation Notice: A component that shows a notice when a mutation is pending, successful or failed.
+ * Mutation Notice: A hook for showing a notice when a mutation is pending, successful or failed.
  *
  * Usage:
  * ```tsx
- *	 <MutationNotice
- *		 mutationId="unique-mutation-id"
- *		 isSuccess={ mutation.isSuccess }
- *		 isError={ mutation.isError }
- *		 isPending={ mutation.isPending }
- *		 savingMessage={ __( 'Saving…', 'jetpack-boost' ) }
- *		 errorMessage={ __(
- *		 'An error occurred while saving changes. Please, try again.',
- *		 'jetpack-boost'
- *		 ) }
- *		 successMessage={ __( 'Changes saved.', 'jetpack-boost' ) }
- *		 />
+ *	useMutationNotice( {
+ *		"unique-mutation-id",
+ *		mutationState,
+ *		{
+ *			savingMessage: 'Saving…',
+ *			errorMessage: 'An error occurred while saving changes. Please, try again.',
+ *			successMessage: 'Changes saved.',
+ *		}
+ *	} );
  * ```
  *
  * Usage when you don't need to customize the messages:
@@ -37,58 +38,66 @@ type MutationNoticeProps = {
  * 	const [ data, mutation ] = useDataSync(...);
  * 	<MutationNotice { ...mutation } />
  * ```
- * @param props
- * @param props.mutationId     The unique identifier for the mutation.
- * @param props.isSuccess      Whether the mutation was successful.
- * @param props.isError        Whether the mutation failed.
- * @param props.isPending      Whether the mutation is pending.
- * @param props.savingMessage  The message to show when the mutation is pending.
- * @param props.errorMessage   The message to show when the mutation failed.
- * @param props.successMessage The message to show when the mutation was successful.
+ * @param mutationId    A unique identifier for the mutation notice.
+ * @param mutationState An object representing the current state of the mutation.
+ * @param messages      An object containing custom messages for different states of the mutation.
  */
-export const MutationNotice = ( {
-	mutationId,
-	isSuccess,
-	isError,
-	isPending,
-	savingMessage = __( 'Saving…', 'jetpack-boost' ),
-	errorMessage = __( 'An error occurred while saving changes.', 'jetpack-boost' ),
-	successMessage = __( 'Changes saved.', 'jetpack-boost' ),
-}: MutationNoticeProps ) => {
+export const useMutationNotice = (
+	mutationId: string,
+	mutationState: MutationNoticeState | null = null,
+	messages: MutationNoticeMessages = {}
+) => {
 	const { setNotice, removeNotice } = useNotices();
 
+	const defaultMessages = {
+		savingMessage: __( 'Saving…', 'jetpack-boost' ),
+		errorMessage: __(
+			'An error occurred while saving changes. Please, try again.',
+			'jetpack-boost'
+		),
+		successMessage: __( 'Changes saved.', 'jetpack-boost' ),
+	};
+
+	const { savingMessage, errorMessage, successMessage } = { ...defaultMessages, ...messages };
+
+	const { isSuccess, isError, isPending, isIdle, reset } = useMemo( () => {
+		const mutationStateDefaults = {
+			isSuccess: false,
+			isError: false,
+			isPending: false,
+			isIdle: false,
+			reset: () => {},
+		};
+		return {
+			...mutationStateDefaults,
+			...mutationState,
+		};
+	}, [ mutationState ] );
+
 	useEffect( () => {
-		let timeout: number;
-		if ( isPending && ! isSuccess ) {
+		if ( isPending ) {
 			setNotice( { id: mutationId, type: 'pending', message: savingMessage } );
 		} else if ( isSuccess ) {
 			setNotice( { id: mutationId, type: 'success', message: successMessage } );
-			timeout = setTimeout( () => {
-				removeNotice( mutationId );
-			}, 5000 );
 		} else if ( isError ) {
 			setNotice( { id: mutationId, type: 'error', message: errorMessage } );
 		}
-
-		// Cleanup function to remove notice when the component unmounts or if the mutationId changes
-		return () => {
-			clearTimeout( timeout );
-			removeNotice( mutationId );
-		};
 	}, [
+		mutationId,
 		setNotice,
 		removeNotice,
-		isSuccess,
-		isError,
-		isPending,
 		savingMessage,
 		errorMessage,
 		successMessage,
-		mutationId,
+		isSuccess,
+		isError,
+		isPending,
+		isIdle,
+		reset,
 	] );
 
-	return null;
+	return [ () => removeNotice( mutationId ) ];
 };
 
 // This is a pure component, so we can use React.memo to avoid unnecessary re-renders.
-export default React.memo( MutationNotice );
+export default useMutationNotice;
