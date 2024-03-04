6d68e59e67a10894e91ef71cfda1c784e3a38203
Anthony Grullon <agrullon95@gmail.com>
2024-03-04 11:55:17 -0500

    Uncheck Send user invitation checkbox if User invite checkbox is checked

--- projects/plugins/jetpack/modules/sso/jetpack-sso-admin-create-user.js
+++ projects/plugins/jetpack/modules/sso/jetpack-sso-admin-create-user.js
@@ -6,11 +6,15 @@ jQuery( document ).ready( function ( $ ) {
 		// Toggle Send User Notification checkbox enabled/disabled based on Invite User checkbox
 		inviteUserWpcomCheckbox.on( 'change', function () {
 			sendUserNotificationCheckbox.prop( 'disabled', inviteUserWpcomCheckbox.prop( 'checked' ) );
+			if ( inviteUserWpcomCheckbox.prop( 'checked' ) ) {
+				sendUserNotificationCheckbox.prop( 'checked', false );
+			}
 		} );
 
 		// On load, disable Send User Notification checkbox if Invite User checkbox is checked
 		if ( inviteUserWpcomCheckbox.prop( 'checked' ) ) {
 			sendUserNotificationCheckbox.prop( 'disabled', true );
+			sendUserNotificationCheckbox.prop( 'checked', false );
 		}
 	}
 } );
