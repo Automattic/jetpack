9307206573ec1e525793320e9186f9d14f8af440
Juanma Rodriguez Escriche <juanma.rodriguez.escriche@automattic.com>
2024-03-04 15:35:46 +0100

    Update projects/packages/sync/src/class-settings.php
    Co-authored-by: Foteini Giannaropoulou <giannaropoulou.foteini@gmail.com>

--- projects/packages/sync/src/class-settings.php
+++ projects/packages/sync/src/class-settings.php
@@ -61,7 +61,7 @@ class Settings {
 		'checksum_disable'                       => true,
 		'dedicated_sync_enabled'                 => true,
 		'custom_queue_table_enabled'             => true,
-		'use_rest_api_for_sending'               => true,
+		'wpcom_rest_api_enabled'               => true,
 	);
 
 	/**
