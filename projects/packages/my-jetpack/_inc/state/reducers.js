ce15ad16fda995fddaf29564c8a57febc2787a67
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2024-03-06 11:25:06 -0700

    Update/migrate dismiss welcome banner to tanstack (#36199)
    * Migrate welcome banner dismissal to tanstack
    
    * changelog
    
    * changelog
    
    * Undo accidental change

--- projects/packages/my-jetpack/_inc/state/reducers.js
+++ projects/packages/my-jetpack/_inc/state/reducers.js
@@ -1,10 +1,5 @@
 import { combineReducers } from '@wordpress/data';
-import {
-	SET_STATS_COUNTS_IS_FETCHING,
-	SET_STATS_COUNTS,
-	SET_DISMISSED_WELCOME_BANNER_IS_FETCHING,
-	SET_DISMISSED_WELCOME_BANNER,
-} from './actions';
+import { SET_STATS_COUNTS_IS_FETCHING, SET_STATS_COUNTS } from './actions';
 
 const plugins = ( state = {} ) => {
 	return state;
@@ -29,29 +24,9 @@ const statsCounts = ( state = {}, action ) => {
 	}
 };
 
-const welcomeBanner = ( state = {}, action ) => {
-	switch ( action.type ) {
-		case SET_DISMISSED_WELCOME_BANNER_IS_FETCHING:
-			return {
-				...state,
-				isFetching: action.isFetching,
-			};
-
-		case SET_DISMISSED_WELCOME_BANNER:
-			return {
-				...state,
-				hasBeenDismissed: action.hasBeenDismissed,
-			};
-
-		default:
-			return state;
-	}
-};
-
 const reducers = combineReducers( {
 	plugins,
 	statsCounts,
-	welcomeBanner,
 } );
 
 export default reducers;
