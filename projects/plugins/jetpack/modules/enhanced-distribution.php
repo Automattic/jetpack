77a9307de1f35874558e019587bc7c4ccba680fe
Brandon Kraft <public@brandonkraft.com>
2024-03-07 07:31:49 +0800

    Update projects/plugins/jetpack/modules/enhanced-distribution.php
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>

--- projects/plugins/jetpack/modules/enhanced-distribution.php
+++ projects/plugins/jetpack/modules/enhanced-distribution.php
@@ -7,4 +7,5 @@
  * @package automattic/jetpack
  */
 
+_deprecated_file( basename( __FILE__ ), 'jetpack-$$next-version$$' );
 // Silence is golden. Left here to ensure no fatals on some seemingly misconfigured opcache setups.
