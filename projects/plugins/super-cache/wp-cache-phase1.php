329bbc9b2664935c8f9c6fe6845f79e10ace350b
Donncha O Caoimh <5656673+donnchawp@users.noreply.github.com>
2024-03-06 11:25:15 +0000

    Separate out this check to make logging better.
    * Define DONOTCACHEPAGE so postload function doesn't serve a cached
      page.

--- projects/plugins/super-cache/wp-cache-phase1.php
+++ projects/plugins/super-cache/wp-cache-phase1.php
@@ -135,9 +135,19 @@ if ( isset( $wp_cache_make_known_anon ) && $wp_cache_make_known_anon ) {
 // an init action wpsc plugins can hook on to.
 do_cacheaction( 'cache_init' );
 
+if ( ! $cache_enabled ) {
+	return true;
+}
+
 // don't cache or serve cached files for various URLs, including the Customizer.
-if ( ! $cache_enabled || ( isset( $_SERVER['REQUEST_METHOD'] ) && in_array( $_SERVER['REQUEST_METHOD'], array( 'POST', 'PUT', 'DELETE' ) ) ) || isset( $_GET['customize_changeset_uuid'] ) ) {
-	wp_cache_debug( 'Caching disabled.' );
+if ( isset( $_SERVER['REQUEST_METHOD'] ) && in_array( $_SERVER['REQUEST_METHOD'], array( 'POST', 'PUT', 'DELETE' ), true ) ) {
+	wp_cache_debug( 'Caching disabled for non GET request.' );
+	define( 'DONOTCACHEPAGE', 1 );
+	return true;
+}
+
+if ( isset( $_GET['customize_changeset_uuid'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+	wp_cache_debug( 'Caching disabled for customizer.' );
 	define( 'DONOTCACHEPAGE', 1 );
 	return true;
 }
