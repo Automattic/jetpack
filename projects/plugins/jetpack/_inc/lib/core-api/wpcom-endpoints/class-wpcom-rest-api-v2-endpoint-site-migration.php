3a084523a15ddd3797cbb6286ef5b3e19d9ac990
Caroline Moore <calobee@gmail.com>
2024-03-07 11:07:46 -0500

    Add site migration endpoint on jetpack side

---
+++ projects/plugins/jetpack/_inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-site-migration.php
@@ -0,0 +1,78 @@
+<?php
+declare( strict_types = 1 );
+
+/**
+ * WARNING: This file is distributed verbatim in Jetpack.
+ * There should be nothing WordPress.com specific in this file.
+ *
+ * @hide-in-jetpack
+ */
+
+class WPCOM_REST_API_V2_Endpoint_Site_Migration {
+
+	/**
+	 * Class constructor
+	 */
+	public function __construct() {
+		$this->namespace = 'wpcom/v2';
+		$this->rest_base = 'site-migration';
+
+		add_action( 'rest_api_init', array( $this, 'register_routes' ) );
+	}
+
+	/**
+	 * Register our routes.
+	 */
+	public function register_routes() {
+		register_rest_route(
+			$this->namespace,
+			$this->rest_base,
+			array(
+				array(
+					'methods'             => WP_REST_Server::READABLE,
+					'callback'            => array( $this, 'get_data' ),
+					'permission_callback' => array( $this, 'can_access' ),
+				),
+			)
+		);
+	}
+
+	/**
+	 * Permission callback for the REST route.
+	 *
+	 * @return boolean
+	 */
+	public function can_access() {
+		return current_user_can( 'manage_options' );
+	}
+
+	/**
+	 * Returns the migration key.
+	 *
+	 * @return string
+	 */
+	private function get_migration_key() {
+		// Only fetch the key if we have the classes.
+		if ( class_exists( 'MGWPSettings' ) && class_exists( 'MGInfo' ) ) {
+			$migrate_guru_settings = new MGWPSettings();
+			$migrate_guru_info     = new MGInfo( $migrate_guru_settings );
+
+			return $migrate_guru_info->getConnectionKey();
+		}
+
+		return '';
+	}
+
+	/**
+	 * Returns Launchpad-related options.
+	 *
+	 * @return array Associative array with `migration_key`.
+	 */
+	public function get_data() {
+		return array(
+			'migration_key' => $this->get_migration_key(),
+		);
+	}
+}
+
+wpcom_rest_api_v2_load_plugin( 'WPCOM_REST_API_V2_Endpoint_Site_Migration' );
