e83c76ff9dad733eaca595b7a3ff85faef47c54f
Christian Gastrell <cgastrell@gmail.com>
2024-03-08 13:42:03 -0300

    My Jetpack: direct straight into AI product page once opted for the free version (#36265)
    * add ctaCallback prop on ProductInterstitial, optionally triggered on the products clickHandler
    
    * add ctaCallback on Jetpack AI interstitial
    
    * allow AI card to detect if user has already opted for free version
    
    * changelog entry
    
    * remove unused commented code

--- projects/packages/my-jetpack/_inc/components/product-cards-section/ai-card.jsx
+++ projects/packages/my-jetpack/_inc/components/product-cards-section/ai-card.jsx
@@ -1,22 +1,36 @@
+import { useConnection } from '@automattic/jetpack-connection';
 import { __ } from '@wordpress/i18n';
 import PropTypes from 'prop-types';
-import React from 'react';
+import { useRef } from 'react';
 import ProductCard from '../connected-product-card';
 import { PRODUCT_STATUSES } from '../product-card/action-button';
 
 const AiCard = ( { admin } ) => {
-	const overrides = {
+	const { userConnectionData } = useConnection();
+	const { currentUser } = userConnectionData;
+	const { wpcomUser } = currentUser;
+	const userId = currentUser?.id || 0;
+	const blogId = currentUser?.blogId || 0;
+	const wpcomUserId = wpcomUser?.ID || 0;
+	const userOptKey = `jetpack_ai_optfree_${ userId }_${ blogId }_${ wpcomUserId }`;
+	const userOptFree = useRef( localStorage.getItem( userOptKey ) );
+
+	const userOverrides = {
 		[ PRODUCT_STATUSES.CAN_UPGRADE ]: {
 			href: '#/jetpack-ai',
 			label: __( 'View', 'jetpack-my-jetpack' ),
 		},
+		[ PRODUCT_STATUSES.NEEDS_PURCHASE ]: {
+			href: userOptFree.current ? '#/jetpack-ai' : '#/add-jetpack-ai',
+		},
 	};
+
 	return (
 		<ProductCard
 			admin={ admin }
 			slug="jetpack-ai"
 			upgradeInInterstitial={ true }
-			primaryActionOverride={ overrides }
+			primaryActionOverride={ userOverrides }
 		/>
 	);
 };
