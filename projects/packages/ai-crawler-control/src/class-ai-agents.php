1b34cac1d3599ef3801e61e1d28c826b4d6b5966
Jeremy Herve <jeremy@jeremy.hu>
2024-03-14 12:36:30 +0100

    Add first version of the package

---
+++ projects/packages/ai-crawler-control/src/class-ai-agents.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * AI User Agents Utilities.
+ *
+ * @package automattic/jetpack-ai-crawler-control
+ */
+
+namespace Automattic\Jetpack;
+
+/**
+ * AI User Agents Utilities.
+ */
+class AI_Agents {
+	/**
+	 * Get a list of AI User Agents.
+	 *
+	 * @since $$next-version$$
+	 *
+	 * @return array
+	 */
+	public static function agent_list() {
+		// Try to keep this list ordered alphabetically.
+		$user_agents = array(
+			'Amazonbot', // https://developer.amazon.com/support/amazonbot
+			'anthropic-ai', // https://www.anthropic.com/
+			'Bytespider', // https://www.bytedance.com/
+			'CCBot', // https://commoncrawl.org/ccbot
+			'ClaudeBot', // https://claude.ai/
+			'ChatGPT-User', // https://platform.openai.com/docs/plugins/bot/chatgpt-user
+			'cohere-ai', // https://cohere.com/
+			'Google-Extended', // https://blog.google/technology/ai/an-update-on-web-publisher-controls/
+			'FacebookBot', // https://developers.facebook.com/docs/sharing/bot
+			'GPTBot', // https://platform.openai.com/docs/gptbot
+			'omgili', // https://webz.io/blog/web-data/what-is-the-omgili-bot-and-why-is-it-crawling-your-website/
+			'omgilibot', // https://webz.io/blog/web-data/what-is-the-omgili-bot-and-why-is-it-crawling-your-website/
+			'SentiBot', // https://sentione.com/
+			'sentibot', // https://sentione.com/
+		);
+
+		/**
+		 * Filter the list of AI User Agents.
+		 *
+		 * @since $$next-version$$
+		 *
+		 * @param array $user_agents List of AI User Agents.
+		 */
+		return apply_filters( 'jetpack_ai_control_agents_list', $user_agents );
+	}
+}
