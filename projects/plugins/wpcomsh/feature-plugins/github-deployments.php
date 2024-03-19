5cd2f992f13e688b8937c6ec640dfe7efe6cfcf8
Luis Felipe Zaguini <26530524+zaguiini@users.noreply.github.com>
2024-03-19 19:07:50 -0300

    GitHub Deployments: Remove feature flag call https://github.com/Automattic/wpcomsh/pull/1746
    * GitHub Deployments: Remove feature flag call
    
    * Bump WPCOMSH version to 3.17.4

--- projects/plugins/wpcomsh/feature-plugins/github-deployments.php
+++ projects/plugins/wpcomsh/feature-plugins/github-deployments.php
@@ -5,39 +5,4 @@
  * @package wpcomsh
  */
 
-/**
- * Jetpack adds the github deployments menu to WoA sites depending on the result of the
- * jetpack_show_wpcom_github_deployments_menu filter. During development only particular
- * users can see this menu. At launch time this filter will return true for
- * all users.
- *
- * @return bool true if the current user should see the github deployments menu
- */
-function wpcomsh_should_show_wpcom_github_deployments_menu() {
-	return request_github_deployments_available();
-}
-
-add_filter( 'jetpack_show_wpcom_github_deployments_menu', 'wpcomsh_should_show_wpcom_github_deployments_menu' );
-
-/**
- * Check if the GitHub Deployments feature is enabled for the given site.
- * This is for testing purposes only.
- */
-function request_github_deployments_available() {
-	$wpcom_blog_id = Jetpack_Options::get_option( 'id' );
-	$endpoint      = '/hosting/github/available';
-	$response      = Jetpack_Client::wpcom_json_api_request_as_blog(
-		$endpoint,
-		'v2',
-		array( 'method' => 'GET' ),
-		array( 'blog_id' => $wpcom_blog_id ),
-		'wpcom'
-	);
-
-	if ( is_wp_error( $response ) || 200 !== $response['response']['code'] || empty( $response['body'] ) ) {
-		return false;
-	}
-
-	$response = json_decode( wp_remote_retrieve_body( $response ), true );
-	return $response['available'];
-}
+add_filter( 'jetpack_show_wpcom_github_deployments_menu', '__return_true' );
