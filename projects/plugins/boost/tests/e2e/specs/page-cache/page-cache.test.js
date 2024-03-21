e73627424d5b19d2effc0767a6a214a46790d6f3
Peter Petrov <peter.petrov89@gmail.com>
2024-03-21 12:55:18 +0200

    Add test for Page Cache header when module is inactive

--- projects/plugins/boost/tests/e2e/specs/page-cache/page-cache.test.js
+++ projects/plugins/boost/tests/e2e/specs/page-cache/page-cache.test.js
@@ -1,6 +1,7 @@
 import { test, expect } from 'jetpack-e2e-commons/fixtures/base-test.js';
 import { boostPrerequisitesBuilder } from '../../lib/env/prerequisites.js';
 import { JetpackBoostPage } from '../../lib/pages/index.js';
+import { PostFrontendPage } from 'jetpack-e2e-commons/pages/index.js';
 import playwrightConfig from 'jetpack-e2e-commons/playwright.config.mjs';
 
 test.describe( 'Cache module', () => {
@@ -19,5 +20,18 @@ test.describe( 'Cache module', () => {
 			await jetpackBoostPage.isThePageCacheMetaInformationVisible(),
 			'Page Cache meta information should not be visible'
 		).toBeFalsy();
+	} );
+
+	// Make sure there's no cache header when module is disabled.
+	test( 'Page Cache should not be present when Page Cache module is inactive', async () => {
+		await boostPrerequisitesBuilder( page ).withInactiveModules( [ 'page_cache' ] ).build();
+		await PostFrontendPage.visit( page );
+
+		page.on( 'response', response => {
+			expect(
+				response.headers().hasOwnProperty( 'X-Jetpack-Boost-Cache' ),
+				'Page Cache header should not be present'
+			).toBeFalsy();
+		} );
 	} );
 } );
