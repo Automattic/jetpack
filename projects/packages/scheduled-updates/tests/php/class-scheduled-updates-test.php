efe24ab277b48bc17a4338e7070f0c4eddd9b376
Konstantin Obenland <obenland@gmx.de>
2024-03-22 12:28:11 +0100

    Add unit test.

--- projects/packages/scheduled-updates/tests/php/class-scheduled-updates-test.php
+++ projects/packages/scheduled-updates/tests/php/class-scheduled-updates-test.php
@@ -555,6 +555,50 @@ class Scheduled_Updates_Test extends \WorDBless\BaseTestCase {
 	}
 
 	/**
+	 * Test get_scheduled_update_text.
+	 *
+	 * @dataProvider update_text_provider
+	 * @covers ::get_scheduled_update_text
+	 *
+	 * @param object $schedule The schedule object.
+	 * @param string $expected The expected text.
+	 */
+	public function test_get_scheduled_update_text( $schedule, $expected ) {
+		$this->assertSame( $expected, Scheduled_Updates::get_scheduled_update_text( $schedule ) );
+	}
+
+	/**
+	 * Data provider for test_get_scheduled_update_text.
+	 *
+	 * @return array[]
+	 */
+	private function update_text_provider() {
+		return array(
+			array(
+				(object) array(
+					'timestamp' => strtotime( 'next Monday 00:00' ),
+					'interval'  => WEEK_IN_SECONDS,
+				),
+				sprintf( 'Mondays at %s.', gmdate( get_option( 'time_format' ), strtotime( 'next Monday 8:00' ) ) ),
+			),
+			array(
+				(object) array(
+					'timestamp' => strtotime( 'next Tuesday 00:00' ),
+					'interval'  => DAY_IN_SECONDS,
+				),
+				sprintf( 'Daily at %s.', gmdate( get_option( 'time_format' ), strtotime( 'next Tuesday 8:00' ) ) ),
+			),
+			array(
+				(object) array(
+					'timestamp' => strtotime( 'next Sunday 00:00' ),
+					'interval'  => WEEK_IN_SECONDS,
+				),
+				sprintf( 'Sundays at %s.', gmdate( get_option( 'time_format' ), strtotime( 'next Sunday 8:00' ) ) ),
+			),
+		);
+	}
+
+	/**
 	 * Create a list of plugins to be deleted.
 	 *
 	 * @param int $count The number of plugins to create.
