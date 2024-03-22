49856061e147eeced606ea492f3de7d283155cb0
Konstantin Obenland <obenland@gmx.de>
2024-03-22 14:02:30 +0100

    Use new wp_date for improved compat
    Props @TimBroddin

--- projects/packages/scheduled-updates/src/class-scheduled-updates.php
+++ projects/packages/scheduled-updates/src/class-scheduled-updates.php
@@ -221,7 +221,7 @@ class Scheduled_Updates {
 			$html = sprintf(
 				/* translators: %s is the time of day. Daily at 10 am. */
 				esc_html__( 'Daily at %s.', 'jetpack-scheduled-updates' ),
-				get_date_from_gmt( gmdate( 'Y-m-d H:i:s', $schedule->timestamp ), get_option( 'time_format' ) )
+				wp_date( get_option( 'time_format' ), $schedule->timestamp )
 			);
 		} else {
 			// Not getting smart about passing in weekdays makes it easier to translate.
@@ -243,8 +243,8 @@ class Scheduled_Updates {
 			);
 
 			$html = sprintf(
-				$weekdays[ date_i18n( 'N', $schedule->timestamp ) ],
-				get_date_from_gmt( gmdate( 'Y-m-d H:i:s', $schedule->timestamp ), get_option( 'time_format' ) )
+				$weekdays[ wp_date( 'N', $schedule->timestamp ) ],
+				wp_date( get_option( 'time_format' ), $schedule->timestamp )
 			);
 		}
 
