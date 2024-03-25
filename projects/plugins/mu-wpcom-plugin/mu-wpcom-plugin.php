b3f1066e8601a21c9afae50f4d41942b70bd8405
Dusty Reagan <dusty@dustyreagan.com>
2024-03-25 11:32:51 -0500

    Remove "Subscribers" from the "Hosting" menu (#36513)
    * Remove Subscribers from Hosting menu
    
    * changelog
    
    * Run tools/fixup-project-versions.sh to update version numbers

--- projects/plugins/mu-wpcom-plugin/mu-wpcom-plugin.php
+++ projects/plugins/mu-wpcom-plugin/mu-wpcom-plugin.php
@@ -3,7 +3,7 @@
  *
  * Plugin Name: WordPress.com Features
  * Description: Test plugin for the jetpack-mu-wpcom package
- * Version: 2.1.8
+ * Version: 2.1.9-alpha
  * Author: Automattic
  * License: GPLv2 or later
  * Text Domain: jetpack-mu-wpcom-plugin
