8d57435f97d74b8643de77bfcd92090b61ec7883
Jeremy Herve <jeremy@jeremy.hu>
2024-03-26 15:05:52 +0100

    SSO: add package skeleton
    Add the necessary skeleton for a new SSO package.

---
+++ projects/packages/sso/src/class-sso.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Package description here
+ *
+ * @package automattic/jetpack-sso
+ */
+
+namespace Automattic\Jetpack;
+
+/**
+ * Class description.
+ */
+class Sso {
+
+	const PACKAGE_VERSION = '1.0.0-alpha';
+}
