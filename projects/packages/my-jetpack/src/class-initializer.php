8d7171a45125bae4ad800fefac16377e0eb6c129
jboland88 <18016357+jboland88@users.noreply.github.com>
2024-03-27 17:28:13 -0400

    Better handling on My Jetpack product interstitials if site already has prodcut  (#36570)
    * Update product interstitials to use paid plan check, better handling on table interstitial if site already has a plan
    
    * changelog
    
    * version management
    
    * More version fixing

--- projects/packages/my-jetpack/src/class-initializer.php
+++ projects/packages/my-jetpack/src/class-initializer.php
@@ -36,7 +36,7 @@ class Initializer {
 	 *
 	 * @var string
 	 */
-	const PACKAGE_VERSION = '4.19.1-alpha';
+	const PACKAGE_VERSION = '4.20.0-alpha';
 
 	/**
 	 * HTML container ID for the IDC screen on My Jetpack page.
