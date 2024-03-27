715e31a21b6e61db876d0c5f7342f164ca6839ca
Adnan Haque <adnan007.id@gmail.com>
2024-03-27 10:05:35 +0700

    Remove `Jetpack_Boost_Modules` placeholder class and pass an array of module slugs directly

--- projects/packages/my-jetpack/src/class-initializer.php
+++ projects/packages/my-jetpack/src/class-initializer.php
@@ -9,7 +9,6 @@ namespace Automattic\Jetpack\My_Jetpack;
 
 use Automattic\Jetpack\Admin_UI\Admin_Menu;
 use Automattic\Jetpack\Assets;
-use Automattic\Jetpack\Boost_Speed_Score\Jetpack_Boost_Modules;
 use Automattic\Jetpack\Boost_Speed_Score\Speed_Score;
 use Automattic\Jetpack\Boost_Speed_Score\Speed_Score_History;
 use Automattic\Jetpack\Connection\Client;
@@ -87,8 +86,7 @@ class Initializer {
 		}
 
 		// Initialize Boost Speed Score
-		$boost_modules = Jetpack_Boost_Modules::init();
-		new Speed_Score( $boost_modules, 'jetpack-my-jetpack' );
+		new Speed_Score( array(), 'jetpack-my-jetpack' );
 
 		// Add custom WP REST API endoints.
 		add_action( 'rest_api_init', array( __CLASS__, 'register_rest_endpoints' ) );
