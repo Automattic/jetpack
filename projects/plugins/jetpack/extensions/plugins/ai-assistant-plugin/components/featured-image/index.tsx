3ea3075e8ca3f6471136e443cfc36841118c2ca6
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2024-03-27 12:31:25 -0300

    AI Featured Image: Add modal and content (#36613)
    * AI Plugin: Add new modal component
    
    * AI Plugin: Reuse modal component in proofread
    
    * AI Plugin: Use new modal in featured image
    
    * changelog
    
    * AI Plugin: Add correct text for accept
    
    * AI Plugin: Remove checks from featured image

--- projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/featured-image/index.tsx
+++ projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/featured-image/index.tsx
@@ -1,23 +1,39 @@
 /**
  * External dependencies
  */
-import { Button } from '@wordpress/components';
+import { Button, Spinner } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
 import './style.scss';
+import { useState } from 'react';
+import AiAssistantModal from '../modal';
 
-export default function FeaturedImage( {
-	disabled = false,
-	busy = false,
-}: {
-	disabled?: boolean;
-	busy?: boolean;
-} ) {
-	const postContent = '';
+export default function FeaturedImage() {
+	const [ isFeaturedImageModalVisible, setIsFeaturedImageModalVisible ] = useState( false );
+	const [ generating, setGenerating ] = useState( false );
 
-	const handleRequest = () => {};
+	// TODO: Implement post content retrieval and block button click handling
+	// const postContent = '';
+
+	// TODO: Implement real generation
+	const fakeGeneration = () => {
+		setGenerating( true );
+		setTimeout( () => {
+			setGenerating( false );
+		}, 3000 );
+	};
+
+	const toggleFeaturedImageModal = () => {
+		setIsFeaturedImageModalVisible( ! isFeaturedImageModalVisible );
+	};
+
+	const handleRequest = () => {
+		toggleFeaturedImageModal();
+		//TODO: Implement image generation
+		fakeGeneration();
+	};
 
 	return (
 		<div>
@@ -27,14 +43,39 @@ export default function FeaturedImage( {
 					'jetpack'
 				) }
 			</p>
-			<Button
-				onClick={ handleRequest }
-				variant="secondary"
-				disabled={ ! postContent || disabled }
-				isBusy={ busy }
-			>
+			<Button onClick={ handleRequest } variant="secondary">
 				{ __( 'Generate image', 'jetpack' ) }
 			</Button>
+			{ isFeaturedImageModalVisible && (
+				<AiAssistantModal handleClose={ toggleFeaturedImageModal }>
+					{ generating ? (
+						<div className="ai-assistant-featured-image__loading">
+							<Spinner
+								style={ {
+									width: '50px',
+									height: '50px',
+								} }
+							/>
+						</div>
+					) : (
+						<div className="ai-assistant-featured-image__content">
+							<img
+								className="ai-assistant-featured-image__image"
+								src="https://images.pexels.com/photos/346529/pexels-photo-346529.jpeg"
+								alt={ __( 'Beatiful landscape', 'jetpack' ) }
+							/>
+							<div className="ai-assistant-featured-image__actions">
+								<Button onClick={ toggleFeaturedImageModal } variant="secondary">
+									{ __( 'Accept image', 'jetpack' ) }
+								</Button>
+								<Button onClick={ fakeGeneration } variant="secondary">
+									{ __( 'Generate another image', 'jetpack' ) }
+								</Button>
+							</div>
+						</div>
+					) }
+				</AiAssistantModal>
+			) }
 		</div>
 	);
 }
