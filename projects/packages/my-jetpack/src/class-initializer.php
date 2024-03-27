9d884bca5952c836b535e59c8c14ebcbc5b7a1df
sergeymitr <sergeymitr@gmail.com>
2024-03-27 09:11:29 -0400

    Bump versions.

--- projects/packages/my-jetpack/src/class-initializer.php
+++ projects/packages/my-jetpack/src/class-initializer.php
@@ -36,7 +36,7 @@ class Initializer {
 	 *
 	 * @var string
 	 */
-	const PACKAGE_VERSION = '4.19.0';
+	const PACKAGE_VERSION = '4.19.1-alpha';
 
 	/**
 	 * HTML container ID for the IDC screen on My Jetpack page.
