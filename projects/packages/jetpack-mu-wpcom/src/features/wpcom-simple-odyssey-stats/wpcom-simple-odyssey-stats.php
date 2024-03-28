0d86b04b4a626d523623766a6c9f3998cdc61a95
Candy Tsai <candy02058912@gmail.com>
2024-03-28 15:37:21 +0800

    Load Odyssey Stats for simple sites

---
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-simple-odyssey-stats/wpcom-simple-odyssey-stats.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Load the Odyssey stats feature on WordPress.com Simple Site.
+ * See https://github.com/Automattic/jetpack/tree/trunk/projects/packages/stats-admin
+ *
+ * @package automattic/jetpack-mu-wpcom
+ */
+
+use Automattic\Jetpack\Stats_Admin\Dashboard as OdysseyStats;
+OdysseyStats::init();
+
+/**
+ * Load the Odyssey stats widget in the Dashboard.
+ */
+require_once JETPACK_PLUGIN_LOADER_PATH . '/class-jetpack-stats-dashboard-widget.php';
+add_action( 'wp_dashboard_setup', array( new Jetpack_Stats_Dashboard_Widget(), 'init' ) );
