7005ee02c585dd0f5fb326ab4c7f2c8e9a10d305
Miguel <miguel.san.segundo@automattic.com>
2024-03-29 15:26:26 +0100

    Add param to stub

--- .phan/stubs/wpcom-functions.php
+++ .phan/stubs/wpcom-functions.php
@@ -15,6 +15,7 @@ function wpcom_is_nav_redesign_enabled(): bool {}
 /**
  * Whether the user is an Automattician
  *
+ * @param int|null $user_id The user id.
  * @phan-return bool Returns true if the user is Automattician, false otherwise.
  */
-function is_automattician(): bool {}
+function is_automattician( $user_id = null ): bool {} // phpcs:ignore
