0463973c6122cdfaf41789e5770ce8fc845e00e4
Miguel <miguel.san.segundo@automattic.com>
2024-03-29 16:00:52 +0100

    More specific phpcs:ignore

--- .phan/stubs/wpcom-functions.php
+++ .phan/stubs/wpcom-functions.php
@@ -18,4 +18,4 @@ function wpcom_is_nav_redesign_enabled(): bool {}
  * @param int|null $user_id The user id.
  * @phan-return bool Returns true if the user is Automattician, false otherwise.
  */
-function is_automattician( $user_id = null ): bool {} // phpcs:ignore
+function is_automattician( $user_id = null ): bool {} // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
