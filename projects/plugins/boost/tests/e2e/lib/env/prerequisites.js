cff585edd0df38f97aaa169a59599d2224fa3479
Peter Petrov <peter.petrov89@gmail.com>
2024-04-01 14:25:36 +0300

    Add e2e 'concatenate assets' helper plugin

--- projects/plugins/boost/tests/e2e/lib/env/prerequisites.js
+++ projects/plugins/boost/tests/e2e/lib/env/prerequisites.js
@@ -12,6 +12,7 @@ export function boostPrerequisitesBuilder( page ) {
 		connected: undefined,
 		jetpackDeactivated: undefined,
 		mockSpeedScore: undefined,
+		enqueuedAssets: undefined,
 	};
 
 	return {
@@ -35,6 +36,10 @@ export function boostPrerequisitesBuilder( page ) {
 			state.mockSpeedScore = shouldMockSpeedScore;
 			return this;
 		},
+		withEnqueuedAssets( shouldEnqueueAssets ) {
+			state.enqueuedAssets = shouldEnqueueAssets;
+			return this;
+		},
 		withCleanEnv() {
 			state.clean = true;
 			return this;
@@ -52,6 +57,7 @@ async function buildPrerequisites( state, page ) {
 		testPostTitles: () => ensureTestPosts( state.testPostTitles ),
 		clean: () => ensureCleanState( state.clean ),
 		mockSpeedScore: () => ensureMockSpeedScoreState( state.mockSpeedScore ),
+		enqueuedAssets: () => ensureEnqueuedAssets( state.enqueuedAssets ),
 	};
 
 	logger.prerequisites( JSON.stringify( state, null, 2 ) );
@@ -90,6 +96,16 @@ export async function ensureMockSpeedScoreState( mockSpeedScore ) {
 	} else {
 		logger.prerequisites( 'Unmocking Speed Score' );
 		await execWpCommand( 'plugin deactivate e2e-mock-speed-score-api' );
+	}
+}
+
+export async function ensureEnqueuedAssets( enqueue ) {
+	if ( enqueue ) {
+		logger.prerequisites( 'Enqueuing assets' );
+		await execWpCommand( 'plugin activate e2e-concatenate-enqueue' );
+	} else {
+		logger.prerequisites( 'Deactivating assets' );
+		await execWpCommand( 'plugin deactivate e2e-concatenate-enqueue' );
 	}
 }
 
---
+++ projects/plugins/boost/tests/e2e/plugins/e2e-concatenate-enqueue.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Boost E2E Enqueue Assets
+ * Description: Enqueue assets for testing Concatenate JS and CSS.
+ * Plugin URI: https://github.com/automattic/jetpack
+ * Author: Heart of Gold
+ * Version: 1.0.0
+ * Text Domain: jetpack
+ *
+ * @package automattic/jetpack
+ */
+
+add_action( 'wp_enqueue_scripts', 'e2e_enqueue_assets' );
+
+function e2e_enqueue_assets() {
+	wp_enqueue_script( 'wp-tinymce' );
+}
