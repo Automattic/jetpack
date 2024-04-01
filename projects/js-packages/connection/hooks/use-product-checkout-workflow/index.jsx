5f0081fbff265493540158e2740ec5f898f359b8
jboland88 <18016357+jboland88@users.noreply.github.com>
2024-04-01 19:21:08 -0400

    set checkout started to false when the usecheckout hook first runs

--- projects/js-packages/connection/hooks/use-product-checkout-workflow/index.jsx
+++ projects/js-packages/connection/hooks/use-product-checkout-workflow/index.jsx
@@ -177,6 +177,7 @@ export default function useProductCheckoutWorkflow( {
 	useEffect( () => {
 		restApi.setApiRoot( apiRoot );
 		restApi.setApiNonce( apiNonce );
+		setCheckoutStarted( false );
 	}, [] );
 
 	return {
