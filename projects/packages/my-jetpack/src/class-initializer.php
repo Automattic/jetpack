66643dab19e1190d2c522664aba8ab3517f931ec
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2024-04-02 09:38:11 -0600

    Fixup project versions

--- projects/packages/my-jetpack/src/class-initializer.php
+++ projects/packages/my-jetpack/src/class-initializer.php
@@ -36,7 +36,7 @@ class Initializer {
 	 *
 	 * @var string
 	 */
-	const PACKAGE_VERSION = '4.20.2';
+	const PACKAGE_VERSION = '4.20.3-alpha';
 
 	/**
 	 * HTML container ID for the IDC screen on My Jetpack page.
