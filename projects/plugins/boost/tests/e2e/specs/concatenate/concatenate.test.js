d0359b306add421283d15f5f3ff2cf7ea6d17b2d
Peter Petrov <peter.petrov89@gmail.com>
2024-04-03 16:48:01 +0300

    Combine concatenate tests into one

--- projects/plugins/boost/tests/e2e/specs/concatenate/concatenate.test.js
+++ projects/plugins/boost/tests/e2e/specs/concatenate/concatenate.test.js
@@ -4,7 +4,7 @@ import { JetpackBoostPage } from '../../lib/pages/index.js';
 import { PostFrontendPage } from 'jetpack-e2e-commons/pages/index.js';
 import playwrightConfig from 'jetpack-e2e-commons/playwright.config.mjs';
 
-test.describe( 'Concatenate JS', () => {
+test.describe( 'Concatenate JS/CSS', () => {
 	let page;
 
 	test.beforeAll( async ( { browser } ) => {
@@ -14,19 +14,25 @@ test.describe( 'Concatenate JS', () => {
 
 	test.afterAll( async () => {} );
 
-	test( 'No Concatenate JS meta information should show on the admin when the module is inactive', async () => {
-		await boostPrerequisitesBuilder( page ).withInactiveModules( [ 'minify_js' ] ).build();
+	test( 'No Concatenate meta information should show on the admin when the modules are inactive', async () => {
+		await boostPrerequisitesBuilder( page )
+			.withInactiveModules( [ 'minify_js', 'minify_css' ] )
+			.build();
 		const jetpackBoostPage = await JetpackBoostPage.visit( page );
 
 		expect(
 			await jetpackBoostPage.isConcatenateJsMetaVisible(),
 			'Concatenate JS meta information should not be visible'
 		).toBeFalsy();
+		expect(
+			await jetpackBoostPage.isConcatenateCssMetaVisible(),
+			'Concatenate CSS meta information should not be visible'
+		).toBeFalsy();
 	} );
 
-	test( 'JS concatenation shouldn`t occur when the module is inactive', async () => {
+	test( 'Concatenation shouldn`t occur when the modules are inactive', async () => {
 		await boostPrerequisitesBuilder( page )
-			.withInactiveModules( [ 'minify_js' ] )
+			.withInactiveModules( [ 'minify_js', 'minify_css' ] )
 			.withEnqueuedAssets( true )
 			.build();
 		const postFrontPage = await PostFrontendPage.visit( page );
@@ -36,21 +42,32 @@ test.describe( 'Concatenate JS', () => {
 			( await postFrontPage.page.locator( '[id="e2e-script-one-js"]' ).count() ) > 0,
 			'JS concatenation shouldn`t occur when the module is inactive'
 		).toBeTruthy();
+		expect(
+			// This style is enqueued via a helper plugin.
+			( await postFrontPage.page.locator( '[id="e2e-style-one-css"]' ).count() ) > 0,
+			'CSS concatenation shouldn`t occur when the module is inactive'
+		).toBeTruthy();
 	} );
 
-	test( 'Meta information should be visible when the module is active', async () => {
-		await boostPrerequisitesBuilder( page ).withActiveModules( [ 'minify_js' ] ).build();
+	test( 'Meta information should be visible when the modules are active', async () => {
+		await boostPrerequisitesBuilder( page )
+			.withActiveModules( [ 'minify_js', 'minify_css' ] )
+			.build();
 		const jetpackBoostPage = await JetpackBoostPage.visit( page );
 
 		expect(
 			await jetpackBoostPage.isConcatenateJsMetaVisible(),
 			'Concatenate JS meta information should be visible'
 		).toBeTruthy();
+		expect(
+			await jetpackBoostPage.isConcatenateCssMetaVisible(),
+			'Concatenate CSS meta information should be visible'
+		).toBeTruthy();
 	} );
 
-	test( 'JS Concatenation occurs when module is active', async () => {
+	test( 'Concatenation occurs when modules are active', async () => {
 		await boostPrerequisitesBuilder( page )
-			.withActiveModules( [ 'minify_js' ] )
+			.withActiveModules( [ 'minify_js', 'minify_css' ] )
 			.withEnqueuedAssets( true )
 			.build();
 		const postFrontPage = await PostFrontendPage.visit( page );
@@ -63,11 +80,19 @@ test.describe( 'Concatenate JS', () => {
 				.count() ) > 0,
 			'JS Concatenation occurs when module is active'
 		).toBeTruthy();
+		expect(
+			// e2e-style-one and e2e-style-two are enqueued by a helper plugin. When concatenation is enabled,
+			// they should be concatenated into a single style.
+			( await postFrontPage.page
+				.locator( '[data-handles*="e2e-style-one"][data-handles*="e2e-style-two"]' )
+				.count() ) > 0,
+			'CSS Concatenation occurs when module is active'
+		).toBeTruthy();
 	} );
 
-	test( 'jQuery should not be concatenated as it is excluded by default', async () => {
+	test( 'Assets that are excluded by default shouldn`t be concatenated', async () => {
 		await boostPrerequisitesBuilder( page )
-			.withActiveModules( [ 'minify_js' ] )
+			.withActiveModules( [ 'minify_js', 'minify_css' ] )
 			.withEnqueuedAssets( true )
 			.build();
 		const postFrontPage = await PostFrontendPage.visit( page );
@@ -76,6 +101,11 @@ test.describe( 'Concatenate JS', () => {
 			// jQuery is enqueued by a helper plugin.
 			( await postFrontPage.page.locator( '[id="jquery-core-js"]' ).count() ) > 0,
 			'jQuery should not be concatenated'
+		).toBeTruthy();
+		expect(
+			// Admin bar stylesheet is enqueued by default when logged-in.
+			( await postFrontPage.page.locator( '[id="admin-bar-css"]' ).count() ) > 0,
+			'Admin bar stylesheet should not be concatenated'
 		).toBeTruthy();
 	} );
 } );
