bd0535918e518eeb53ea01668d9b25cfe79bceab
Douglas <douglas.henri@automattic.com>
2024-04-04 19:12:17 -0300

    use scrollPadding and scrollMarging to keep the scroll aligned to end

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/hooks/use-auto-scroll/index.ts
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/hooks/use-auto-scroll/index.ts
@@ -11,11 +11,16 @@ const useAutoScroll = (
 	blockRef: React.MutableRefObject< HTMLDivElement >,
 	contentRef: React.MutableRefObject< HTMLDivElement >
 ) => {
-	const scrollElementRef = useRef( null );
+	const scrollElementRef = useRef< HTMLElement | Document | null >( null );
+	const styledScrollElementRef = useRef< HTMLElement | null >( null );
 	const autoScrollEnabled = useRef( false );
 	const ignoreScroll = useRef( false );
 	const startedAutoScroll = useRef( false );
 	const doingAutoScroll = useRef( false );
+	const scrollElementOriginalStyle = useRef< { scrollPadding: string; scrollMargin: string } >( {
+		scrollPadding: '',
+		scrollMargin: '',
+	} );
 
 	const enableIgnoreScroll = useCallback( () => {
 		debug( 'enabling ignore scroll' );
@@ -43,6 +48,15 @@ const useAutoScroll = (
 		startedAutoScroll.current = false;
 		doingAutoScroll.current = false;
 		scrollElementRef.current?.removeEventListener( 'scroll', userScrollHandler );
+
+		// Reset scroll padding and margin
+		if ( styledScrollElementRef.current?.style ) {
+			styledScrollElementRef.current.style.scrollPadding =
+				scrollElementOriginalStyle.current.scrollPadding;
+			styledScrollElementRef.current.style.scrollMargin =
+				scrollElementOriginalStyle.current.scrollMargin;
+		}
+
 		scrollElementRef.current = null;
 		debug( 'disabling auto scroll' );
 	}, [ userScrollHandler ] );
@@ -59,7 +73,7 @@ const useAutoScroll = (
 			doingAutoScroll.current = true;
 
 			scrollElementRef?.current?.removeEventListener?.( 'scroll', userScrollHandler );
-			lastParagraph?.scrollIntoView( { block: 'center', inline: 'center' } );
+			lastParagraph?.scrollIntoView( { block: 'end', inline: 'end' } );
 
 			setTimeout( () => {
 				doingAutoScroll.current = false;
@@ -68,38 +82,54 @@ const useAutoScroll = (
 		}
 	}, [ contentRef, userScrollHandler ] );
 
-	const getScrollParent = useCallback( el => {
-		if ( el == null ) {
-			return null;
-		}
+	const getScrollParent = useCallback(
+		( el: HTMLElement | null ): HTMLElement | Document | null => {
+			if ( el == null ) {
+				return null;
+			}
 
-		// If we arrived to the body, we should stop since it's the last event handler
-		if ( el?.nodeName === 'BODY' ) {
-			return el;
-		}
+			// If we arrived to the body, we should stop since it's the last event handler
+			if ( el?.nodeName === 'BODY' ) {
+				return el;
+			}
 
-		// Gutenberg on newest version run inside iframe, on that case the scroll parent is the iframe
-		if ( el?.ownerDocument !== document ) {
-			return el.ownerDocument;
-		}
+			// Gutenberg on newest version run inside iframe, on that case the scroll parent is the iframe
+			if ( el?.ownerDocument !== document ) {
+				return el.ownerDocument;
+			}
 
-		const { overflow } = window.getComputedStyle( el );
+			const { overflow } = window.getComputedStyle( el );
 
-		if ( overflow.split( ' ' ).every( o => o === 'auto' || o === 'scroll' ) ) {
-			return el;
-		}
+			if ( overflow.split( ' ' ).every( o => o === 'auto' || o === 'scroll' ) ) {
+				return el;
+			}
 
-		if ( ! el?.parentElement ) {
-			return el;
-		}
+			if ( ! el?.parentElement ) {
+				return el;
+			}
 
-		return getScrollParent( el?.parentElement );
-	}, [] );
+			return getScrollParent( el?.parentElement );
+		},
+		[]
+	);
 
 	useEffect( () => {
 		const parent = getScrollParent( blockRef?.current?.parentElement );
-		if ( ! scrollElementRef.current ) {
+
+		if ( ! scrollElementRef.current && parent ) {
 			scrollElementRef.current = parent;
+			styledScrollElementRef.current =
+				parent instanceof HTMLElement ? parent : parent.documentElement;
+
+			// Save the original scroll padding and margin
+			scrollElementOriginalStyle.current = {
+				scrollPadding: styledScrollElementRef.current.style.scrollPadding,
+				scrollMargin: styledScrollElementRef.current.style.scrollMargin,
+			};
+
+			// Add scroll padding and margin to avoid the content to be hidden by the fixed input bar
+			styledScrollElementRef.current.style.scrollPadding = '80px';
+			styledScrollElementRef.current.style.scrollMargin = '10px';
 		}
 	}, [ blockRef, getScrollParent ] );
 
