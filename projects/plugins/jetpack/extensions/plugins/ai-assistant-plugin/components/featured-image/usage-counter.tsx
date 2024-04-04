6c18908be1d3493b6a7cf20f226e75faf302180b
Luiz Kowalski <lhkowalski@gmail.com>
2024-04-04 16:28:35 -0300

    Add small component to handle usage display

---
+++ projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/featured-image/usage-counter.tsx
@@ -0,0 +1,43 @@
+/**
+ * External dependencies
+ */
+import { Tooltip } from '@wordpress/components';
+import { __, sprintf } from '@wordpress/i18n';
+import { Icon, info } from '@wordpress/icons';
+/**
+ * Internal dependencies
+ */
+import useAiFeature from '../../../../blocks/ai-assistant/hooks/use-ai-feature';
+import './usage-counter.scss';
+
+export default function UsageCounter() {
+	const featureData = useAiFeature();
+
+	const currentLimit = featureData?.currentTier?.value || 0;
+	const currentUsage = featureData?.usagePeriod?.requestsCount || 0;
+	const isUnlimited = currentLimit === 1;
+
+	const loadingLabel = __( '…', 'jetpack' );
+	const requestsCountLabel = sprintf(
+		// Translators: %1$d is the number of requests used, %d is the limit of requests.
+		__( 'Usage: %1$d / %2$d requests', 'jetpack' ),
+		currentUsage,
+		currentLimit
+	);
+
+	const pricingLabel = __( 'Featured image generation costs 10 requests per image', 'jetpack' );
+
+	// No usage counter if the plan is unlimited.
+	if ( isUnlimited ) {
+		return null;
+	}
+
+	return (
+		<div className="ai-assistant-featured-image__usage-counter">
+			{ featureData?.loading ? loadingLabel : requestsCountLabel }
+			<Tooltip text={ pricingLabel } placement="bottom">
+				<Icon className="usage-counter__icon" icon={ info } />
+			</Tooltip>
+		</div>
+	);
+}
