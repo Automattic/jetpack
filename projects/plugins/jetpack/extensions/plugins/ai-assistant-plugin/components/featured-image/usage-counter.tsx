40989b4746377bd74143d3fd3c43c4adbf877d19
Luiz Kowalski <lhkowalski@gmail.com>
2024-04-05 16:42:00 -0300

    Show a different tooltip message when the requests balance is not enough for an image

--- projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/featured-image/usage-counter.tsx
+++ projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/featured-image/usage-counter.tsx
@@ -16,6 +16,9 @@ type UsageCounterProps = {
 };
 
 export default function UsageCounter( { currentLimit, currentUsage, cost }: UsageCounterProps ) {
+	const requestsBalance = currentLimit - currentUsage;
+	const notEnoughRequests = requestsBalance < cost;
+
 	const requestsCountLabel = sprintf(
 		// Translators: %1$d is the number of requests used, %d is the limit of requests.
 		__( 'Usage: %1$d / %2$d requests', 'jetpack' ),
@@ -25,14 +28,26 @@ export default function UsageCounter( { currentLimit, currentUsage, cost }: Usag
 
 	const pricingLabel = sprintf(
 		// Translators: %d is the cost of generating a featured image.
-		__( 'Featured image generation costs %d requests per image', 'jetpack' ),
+		__( 'Featured image generation costs %d requests per image.', 'jetpack' ),
+		cost
+	);
+
+	const pricingLabelNotEnoughRequests = sprintf(
+		// Translators: %d is the cost of generating a featured image.
+		__(
+			"Featured image generation costs %d requests per image. You don't have enough requests to generate another image.",
+			'jetpack'
+		),
 		cost
 	);
 
 	return (
 		<div className="ai-assistant-featured-image__usage-counter">
 			{ requestsCountLabel }
-			<Tooltip text={ pricingLabel } placement="bottom">
+			<Tooltip
+				text={ notEnoughRequests ? pricingLabelNotEnoughRequests : pricingLabel }
+				placement="bottom"
+			>
 				<Icon className="usage-counter__icon" icon={ info } />
 			</Tooltip>
 		</div>
