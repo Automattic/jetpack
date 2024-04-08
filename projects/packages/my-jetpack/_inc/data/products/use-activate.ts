e0b8fc38993053aac303541e9256b2ec315f3b81
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2024-04-08 12:53:10 -0600

    Manually update window state when product is successfully activated

--- projects/packages/my-jetpack/_inc/data/products/use-activate.ts
+++ projects/packages/my-jetpack/_inc/data/products/use-activate.ts
@@ -3,8 +3,21 @@ import useAnalytics from '../../hooks/use-analytics';
 import { REST_API_SITE_PRODUCTS_ENDPOINT } from '../constants';
 import { QUERY_ACTIVATE_PRODUCT_KEY } from '../constants';
 import useSimpleMutation from '../use-simple-mutation';
+import { getMyJetpackWindowInitialState } from '../utils/get-my-jetpack-window-state';
 import useProduct from './use-product';
 import type { ProductCamelCase } from '../types';
+
+const setPluginActiveState = ( productId: string ) => {
+	const { items } = getMyJetpackWindowInitialState( 'products' );
+	if ( items[ productId ]?.standalone_plugin_info.has_standalone_plugin ) {
+		window.myJetpackInitialState.products.items[
+			productId
+		].standalone_plugin_info.is_standalone_active = true;
+		window.myJetpackInitialState.products.items[
+			productId
+		].standalone_plugin_info.is_standalone_installed = true;
+	}
+};
 
 const getIsPluginAlreadyActive = ( detail: ProductCamelCase ) => {
 	const { standalonePluginInfo, isPluginActive } = detail;
@@ -32,6 +45,11 @@ const useActivate = ( productId: string ) => {
 					recordEvent( 'jetpack_myjetpack_product_activated', {
 						product: productId,
 					} );
+
+					// This is to handle an edge case where a user is redirected somewhere after activation
+					// and goes back in the browser and "activates" the product again. This will manually update
+					// the window state so that the tracking is not recorded twice for one activation.
+					setPluginActiveState( productId );
 				}
 				refetch();
 			},
