801a724e4a0ed0e11146b201386986f2187694fd
Tim Broddin <tim@wannabes.be>
2024-04-15 09:19:52 +0200

    Adds the ability to filter users in the WPCOM_JSON_API_List_Users_Endpoint endpoint by capability

--- projects/plugins/jetpack/json-endpoints/class.wpcom-json-api-list-users-endpoint.php
+++ projects/plugins/jetpack/json-endpoints/class.wpcom-json-api-list-users-endpoint.php
@@ -38,6 +38,7 @@ new WPCOM_JSON_API_List_Users_Endpoint(
 			'search'          => '(string) Find matching users.',
 			'search_columns'  => "(array) Specify which columns to check for matching users. Can be any of 'ID', 'user_login', 'user_email', 'user_url', 'user_nicename', and 'display_name'. Only works when combined with `search` parameter.",
 			'role'            => '(string) Specify a specific user role to fetch.',
+			'capabilities'    => '(array) Specify a specific capability to fetch.',
 		),
 
 		'response_format'      => array(
@@ -157,6 +158,10 @@ class WPCOM_JSON_API_List_Users_Endpoint extends WPCOM_JSON_API_Endpoint {
 
 		if ( ! empty( $args['role'] ) ) {
 			$query['role'] = $args['role'];
+		}
+
+		if ( ! empty( $args['capabilities'] ) ) {
+			$query['capability__in'] = $args['capabilities'];
 		}
 
 		$user_query = new WP_User_Query( $query );
