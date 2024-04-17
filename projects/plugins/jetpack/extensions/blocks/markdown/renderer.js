8bf2b80633eebc2927eab73f3d6857723d24b8e6
Jeremy Herve <jeremy@jeremy.hu>
2024-04-17 17:42:13 +0200

    Markdown Block: add support for task lists
    Fixes #36891
    
    This introduces a new Markdown-it plugin to render to-do lists in the Markdown block.
    
    Since to-do lists typically do not include any list style (no dots before each item), I've opted to modify the rendered html to add inline styles to remove those dots. This saves us from enqueuing a CSS file just to render content.

--- projects/plugins/jetpack/extensions/blocks/markdown/renderer.js
+++ projects/plugins/jetpack/extensions/blocks/markdown/renderer.js
@@ -3,13 +3,17 @@ import { RawHTML } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import MarkdownIt from 'markdown-it';
 import footnote_plugin from 'markdown-it-footnote';
+import taskLists from 'markdown-it-task-lists';
 
 /**
  * Module variables
  */
 const markdownConverter = new MarkdownIt( {
 	typographer: true,
-} ).use( footnote_plugin );
+} )
+	.use( footnote_plugin )
+	.use( taskLists );
+
 const handleLinkClick = event => {
 	if ( event.target.nodeName === 'A' ) {
 		const hasConfirmed = window.confirm(
@@ -27,8 +31,24 @@ const getStyles = ( attributes = {} ) => {
 	return spacingProps?.style ? spacingProps.style : {};
 };
 
+const renderMarkdown = source => {
+	if ( ! source.length ) {
+		return '';
+	}
+
+	let html = markdownConverter.render( source );
+
+	// Add inline styles to any to-do lists, found in the html as unordered lists with the .contains-task-list class.
+	// This is done to allow for custom styling of the to-do list items.
+	html = html.replace(
+		/<ul class="contains-task-list">/g,
+		'<ul class="contains-task-list" style="list-style: none;">'
+	);
+
+	return html;
+};
 export default ( { className, source = '', attributes } ) => (
 	<RawHTML className={ className } onClick={ handleLinkClick } style={ getStyles( attributes ) }>
-		{ source.length ? markdownConverter.render( source ) : '' }
+		{ renderMarkdown( source ) }
 	</RawHTML>
 );
