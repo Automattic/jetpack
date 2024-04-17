8b5391da8609716c7fa444befc2f4b4570ca07c3
sergeymitr <sergeymitr@gmail.com>
2024-04-17 19:19:14 -0400

    Connection: hoist Identity Crisis package back into Connection.

--- projects/packages/connection/webpack.config.js
+++ projects/packages/connection/webpack.config.js
@@ -12,6 +12,7 @@ module.exports = [
 					type: 'window',
 				},
 			},
+			'identity-crisis': './src/identity-crisis/_inc/admin.jsx',
 		},
 		mode: jetpackWebpackConfig.mode,
 		devtool: jetpackWebpackConfig.devtool,
@@ -32,7 +33,24 @@ module.exports = [
 			rules: [
 				// Transpile JavaScript, including node_modules.
 				jetpackWebpackConfig.TranspileRule(),
+
+				// Transpile @automattic/jetpack-* in node_modules too.
+				jetpackWebpackConfig.TranspileRule( {
+					includeNodeModules: [ '@automattic/jetpack-' ],
+				} ),
+
+				// Handle CSS.
+				jetpackWebpackConfig.CssRule( {
+					extensions: [ 'css', 'sass', 'scss' ],
+					extraLoaders: [ 'sass-loader' ],
+				} ),
 			],
+		},
+		externals: {
+			...jetpackWebpackConfig.externals,
+			jetpackConfig: JSON.stringify( {
+				consumer_slug: 'identity_crisis',
+			} ),
 		},
 	},
 ];
