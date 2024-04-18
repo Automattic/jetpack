21f90bd7dc1b16bbce5f10e98cd0bb103a4cb57f
Nauris Pūķis <hi@pyronaur.com>
2024-04-18 16:48:57 +0300

    Refactor

--- projects/plugins/boost/app/modules/optimizations/image-cdn/inline-script.js
+++ projects/plugins/boost/app/modules/optimizations/image-cdn/inline-script.js
@@ -1,59 +1,68 @@
+/* eslint-disable @wordpress/no-unused-vars-before-return */
+
 ( function () {
-	const lazyImages = document.querySelectorAll( 'img' );
+	const lazyImages = document.querySelectorAll( 'img[loading=lazy]' );
 	lazyImages.forEach( function ( img ) {
-		const currentSrc = img.currentSrc;
-		const src = img.dataset.src || img.src;
 
-		if ( ! src && ! src.includes( '.wp.com' ) ) {
-			return;
-		}
-		const dpr = window.devicePixelRatio || 1;
-		const clientSizes = [ img.clientWidth * dpr, img.clientHeight * dpr ];
-		const attrSizes = [
-			parseInt( img.attributes.width.value ) * dpr,
-			parseInt( img.attributes.height.value ) * dpr,
-		];
-		let sizes = clientSizes;
-
+		// Require srcset and width/height attributes
 		if (
-			attrSizes[ 0 ] &&
-			attrSizes[ 1 ] &&
-			attrSizes[ 0 ] > 0 &&
-			attrSizes[ 1 ] > 0 &&
-			attrSizes[ 0 ] < clientSizes[ 0 ]
+			! img.attributes.width ||
+			! img.attributes.height ||
+			! img.srcset ||
+			! img.srcset.includes( '.wp.com' )
 		) {
-			sizes = [ attrSizes[ 0 ], attrSizes[ 1 ] ];
-		}
-
-		// Adjust width and height based on devicePixelRatio without exceeding original dimensions
-		const width = sizes[ 0 ];
-		const height = sizes[ 1 ];
-
-		const url = new URL( src );
-		const existingSize = url.searchParams.get( 'resize' );
-		url.searchParams.set( 'resize', `${ width },${ height }` );
-		url.searchParams.set( 'jb-lazy', existingSize || '1' );
-
-		if ( img.srcset?.includes( ` ${ width }w,` ) ) {
 			return;
 		}
+
+		const dpr = window.devicePixelRatio || 1;
+		const rect = img.getBoundingClientRect();
+		const elWidth = rect.width;
+		const ratio = rect.width / rect.height;
+		const srcsetEntries = img.srcset.split( ',' );
+
+		let targetIndex = 0;
+		let closestWidth = -1;
+		let comparisonSrc = img.src;
+		let comparisonWidth = elWidth;
+
+		for ( let i = 0; i < srcsetEntries.length; i++ ) {
+			const src = srcsetEntries[ i ].trim();
+			const [ url, w ] = src.split( ' ' );
+			if ( ! w.trim().endsWith( 'w' ) ) {
+				continue;
+			}
+
+			const width = parseInt( w.trim().slice( 0, -1 ), 10 );
+			if ( elWidth > width || ! comparisonWidth || comparisonWidth > width ) {
+				comparisonWidth = width;
+				comparisonSrc = url;
+				targetIndex = i;
+				closestWidth = comparisonWidth;
+			}
+		}
+
+		const url = new URL( comparisonSrc );
+		const existingResize = url.searchParams.get( 'resize' );
+		if ( ! existingResize ) {
+			return;
+		}
+
+		// Only resize if the difference is more than 10% (min 50px)
+		const diff = elWidth - closestWidth;
+		if ( closestWidth > 0 && ( diff < 50 || diff / closestWidth < 0.1 ) ) {
+			return;
+		}
+
+		// Ceil the width to the nearest 10 to optimize CDN caching
+		const width = Math.ceil( elWidth / 10 ) * 10;
+		const height = Math.ceil( width / ratio );
+
+		url.searchParams.set( 'resize', `${ width },${ height }` );
+		url.searchParams.set( 'jb-lazy', existingResize || '1' );
 
 		const newSrc = url.toString();
-		if ( img.srcset ) {
-			img.srcset = `${ newSrc } ${ dpr * window.innerWidth }w, ${ img.srcset }`;
-		} else {
-			img.srcset = `${ newSrc } ${ dpr * window.innerWidth }w`;
-		}
+		srcsetEntries.splice( targetIndex, 0, `${ newSrc } ${ dpr * window.innerWidth }w` );
+		img.srcset = srcsetEntries.join( ',' );
 		img.sizes = 'auto';
-
-		console.log( 'Adding', newSrc, {
-			width,
-			height,
-			attrSizes,
-			clientSizes,
-			sizes,
-			complete: img.complete,
-			currentSrc,
-		} );
 	} );
 } )();
