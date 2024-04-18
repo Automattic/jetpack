a231bd462dab0958ee3585f510101506b3b2a9a1
pkuliga <pkuliga@gmail.com>
2024-04-18 12:56:25 +0100

    init

---
+++ projects/plugins/jetpack/_inc/client/newsletter/paid-newsletter.jsx
@@ -0,0 +1,70 @@
+import { __ } from '@wordpress/i18n';
+import Button from 'components/button';
+import { withModuleSettingsFormHelpers } from 'components/module-settings/with-module-settings-form-helpers';
+import SettingsCard from 'components/settings-card';
+import SettingsGroup from 'components/settings-group';
+import analytics from 'lib/analytics';
+import React from 'react';
+import { connect } from 'react-redux';
+import { isUnavailableInOfflineMode, isUnavailableInSiteConnectionMode } from 'state/connection';
+import { getJetpackCloudUrl } from 'state/initial-state';
+import { SUBSCRIPTIONS_MODULE_NAME } from './constants';
+
+const trackSetupPaymentPlansButtonClick = () => {
+	analytics.tracks.recordJetpackClick( 'setup_payment_plans_button_click' );
+};
+
+/**
+ * Paid Newsletter component.
+ *
+ * @param {object} props - Component props.
+ * @returns {React.Component} Subscription settings component.
+ */
+function PaidNewsletter( props ) {
+	const {
+		unavailableInOfflineMode,
+		unavailableInSiteConnectionMode,
+		isSubscriptionsActive,
+		setupPaymentPlansUrl,
+	} = props;
+
+	const setupPaymentPlansButtonDisabled =
+		unavailableInOfflineMode || unavailableInSiteConnectionMode || ! isSubscriptionsActive;
+
+	return (
+		<SettingsCard header={ __( 'Paid Newsletter', 'jetpack' ) } hideButton>
+			<SettingsGroup>
+				<p className="jp-settings-card__email-settings">
+					{ __(
+						'Earn money through your Newsletter. Reward your most loyal subscribers with exclusive content or add a paywall to monetize content.',
+						'jetpack'
+					) }
+				</p>
+
+				<Button
+					href={ ! setupPaymentPlansButtonDisabled ? setupPaymentPlansUrl : undefined }
+					onClick={ trackSetupPaymentPlansButtonClick }
+					disabled={ setupPaymentPlansButtonDisabled }
+					primary
+					rna
+				>
+					{ __( 'Set up', 'jetpack' ) }
+				</Button>
+			</SettingsGroup>
+		</SettingsCard>
+	);
+}
+
+export default withModuleSettingsFormHelpers(
+	connect( ( state, ownProps ) => {
+		return {
+			unavailableInOfflineMode: isUnavailableInOfflineMode( state, SUBSCRIPTIONS_MODULE_NAME ),
+			unavailableInSiteConnectionMode: isUnavailableInSiteConnectionMode(
+				state,
+				SUBSCRIPTIONS_MODULE_NAME
+			),
+			isSubscriptionsActive: ownProps.getOptionValue( SUBSCRIPTIONS_MODULE_NAME ),
+			setupPaymentPlansUrl: getJetpackCloudUrl( state, 'monetize/payments' ),
+		};
+	} )( PaidNewsletter )
+);
