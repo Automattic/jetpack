bf0319ab5928a426b5068b16f78344a4f572f339
matticbot <matticbot@users.noreply.github.com>
2024-04-23 00:34:39 +0000

    phan: Update custom stubs

--- .phan/stubs/full-site-editing-stubs.php
+++ .phan/stubs/full-site-editing-stubs.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Stubs automatically generated from WordPress.com Editing Toolkit 4.19624
+ * Stubs automatically generated from WordPress.com Editing Toolkit 4.19964
  * using the definition file `tools/stubs/full-site-editing-stub-defs.php` in the Jetpack monorepo.
  *
  * Do not edit this directly! Run tools/stubs/update-stubs.sh to regenerate it.
