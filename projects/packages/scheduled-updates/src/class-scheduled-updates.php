908087142c492b9e3bfa116faa4c8fca0caef7e0
Kuba Birecki <ice9js@users.noreply.github.com>
2024-04-23 17:17:18 +0200

    Backport mu-wpcom-plugin 2.1.16, jetpack 13.4-a.5 Changes (#37006)
    Changelog and readme.txt edits.

--- projects/packages/scheduled-updates/src/class-scheduled-updates.php
+++ projects/packages/scheduled-updates/src/class-scheduled-updates.php
@@ -20,7 +20,7 @@ class Scheduled_Updates {
 	 *
 	 * @var string
 	 */
-	const PACKAGE_VERSION = '0.8.0-alpha';
+	const PACKAGE_VERSION = '0.9.0-alpha';
 
 	/**
 	 * The cron event hook for the scheduled plugins update.
