8eb85b4d005d26e630889edc16f527a40f4e995e
Kev <kevin.zoschke@automattic.com>
2024-04-23 10:34:32 -0400

    Cookie Consent: localize default text (#37012)

--- projects/plugins/jetpack/extensions/blocks/cookie-consent/constants.js
+++ projects/plugins/jetpack/extensions/blocks/cookie-consent/constants.js
@@ -1,6 +1,12 @@
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 
-export const DEFAULT_TEXT = __(
-	'Privacy &amp; Cookies: This site uses cookies. By continuing to use this website, you agree to their use. To find out more, including how to control cookies, see here: <a href="https://automattic.com/cookies/">Cookie Policy</a>.',
-	'jetpack'
+export const COOKIE_POLICY_URL = 'https://automattic.com/cookies/';
+
+export const DEFAULT_TEXT = sprintf(
+	// translators: %s is a link to the cookie policy.
+	__(
+		'Privacy & Cookies: This site uses cookies. By continuing to use this website, you agree to their use. To find out more, including how to control cookies, see here: %s.',
+		'jetpack'
+	),
+	`<a href="${ COOKIE_POLICY_URL }">${ __( 'Cookie Policy', 'jetpack' ) }</a>`
 );
