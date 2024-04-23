13b2a2e450b090caec3631aeff413005628efed5
Nate Weller <hello@nateweller.com>
2024-04-23 13:51:33 -0600

    Update connection flow to handle connection errors and reconnect flow

--- projects/packages/connection/src/class-rest-connector.php
+++ projects/packages/connection/src/class-rest-connector.php
@@ -995,7 +995,7 @@ class REST_Connector {
 	 *
 	 * @return mixed|null
 	 */
-	public function connection_check() {
+	public static function connection_check() {
 		/**
 		 * Filters the successful response of the REST API test_connection method
 		 *
