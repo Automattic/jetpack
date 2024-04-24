01d6d6d27ccd5543ceadc1cfeca4aa18106dbefe
Brad Jorsch <anomiex@users.noreply.github.com>
2024-04-24 14:57:39 -0400

    connection: Fix Phan issues and improve phpdocs in Client class (#37056)
    * Declare the shape of returned response arrays for better static
      analysis.
    * Fix a bunch of other phpdocs to be more correct too.
    * Remove some unnecessary boolean checks, that were confusing Phan into
      marking all the keys in arrays as being possibly-missing (cf. phan/phan#4846)
      * At least they're unnecessary based on the declared return values of
        stuff and a quick look through the code. If we really need them, we
        should be more specific about what we're checking for and we should
        always return the documented WP_Error instead of passing the
        weird-falsey-value along.
    
    The most important fix here is getting the type of the `$body`
    parameter to `wpcom_json_api_request_as_blog` right, which fixes
    PhanTypeMismatchArgumentProbablyReal issues in a lot of callers.

--- projects/packages/search/.phan/baseline.php
+++ projects/packages/search/.phan/baseline.php
@@ -11,10 +11,10 @@ return [
     // # Issue statistics:
     // PhanUnextractableAnnotation : 45+ occurrences
     // PhanPluginDuplicateConditionalNullCoalescing : 10+ occurrences
-    // PhanTypeMismatchArgumentProbablyReal : 10+ occurrences
     // PhanTypeMismatchReturnProbablyReal : 9 occurrences
     // PhanUndeclaredClassMethod : 8 occurrences
     // PhanTypeMismatchArgument : 7 occurrences
+    // PhanTypeMismatchArgumentProbablyReal : 7 occurrences
     // PhanUndeclaredTypeProperty : 7 occurrences
     // PhanRedundantCondition : 6 occurrences
     // PhanTypeMismatchProperty : 6 occurrences
@@ -46,9 +46,8 @@ return [
         'src/class-cli.php' => ['PhanTypeMismatchArgument'],
         'src/class-helper.php' => ['PhanDeprecatedPartiallySupportedCallable', 'PhanImpossibleCondition', 'PhanPluginDuplicateConditionalNullCoalescing', 'PhanRedundantCondition', 'PhanTypeMismatchArgument', 'PhanTypeMismatchDefault', 'PhanTypeMismatchReturn', 'PhanUndeclaredClassMethod', 'PhanUndeclaredClassReference', 'PhanUndeclaredTypeParameter'],
         'src/class-options.php' => ['PhanPluginSimplifyExpressionBool'],
-        'src/class-plan.php' => ['PhanDeprecatedFunction', 'PhanTypeMismatchArgumentProbablyReal', 'PhanUnextractableAnnotation'],
-        'src/class-rest-controller.php' => ['PhanPluginDuplicateConditionalNullCoalescing', 'PhanTypeMismatchArgumentProbablyReal'],
-        'src/class-stats.php' => ['PhanTypeMismatchArgumentProbablyReal'],
+        'src/class-plan.php' => ['PhanDeprecatedFunction', 'PhanUnextractableAnnotation'],
+        'src/class-rest-controller.php' => ['PhanPluginDuplicateConditionalNullCoalescing'],
         'src/class-template-tags.php' => ['PhanTypeMismatchArgument', 'PhanTypeMismatchArgumentProbablyReal'],
         'src/classic-search/class-classic-search.php' => ['PhanImpossibleCondition', 'PhanPluginRedundantAssignment', 'PhanRedundantCondition', 'PhanTypeComparisonToArray', 'PhanTypeInvalidDimOffset', 'PhanTypeMismatchDeclaredParamNullable', 'PhanTypeMismatchProperty', 'PhanTypeMismatchReturn', 'PhanTypePossiblyInvalidDimOffset', 'PhanUndeclaredClassMethod', 'PhanUndeclaredClassProperty'],
         'src/customizer/customize-controls/class-excluded-post-types-control.php' => ['PhanRedundantCondition', 'PhanTypeMismatchReturnProbablyReal'],
