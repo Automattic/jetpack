83c2b2721902ca2f0478cb53af4d5e65cde3ddba
Enej Bajgoric <enej.bajgoric@automattic.com>
2024-04-26 15:27:06 -0700

    Sync jetpack_subscriptions_reply_to_email option

--- projects/packages/sync/src/class-defaults.php
+++ projects/packages/sync/src/class-defaults.php
@@ -197,6 +197,7 @@ class Defaults {
 		'wpcom_site_setup',
 		'wpcom_subscription_emails_use_excerpt',
 		'jetpack_subscriptions_reply_to',
+		'jetpack_subscriptions_reply_to_email',
 		'jetpack_verbum_subscription_modal',
 		'jetpack_blocks_disabled',
 		'jetpack_package_versions',
