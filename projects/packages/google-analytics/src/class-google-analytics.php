850236edc43d0400fd13568fdc35d9083dc1c545
sergeymitr <sergeymitr@gmail.com>
2024-04-30 11:22:01 -0400

    Google Analytics: introduce the empty package.

---
+++ projects/packages/google-analytics/src/class-google-analytics.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Package description here
+ *
+ * @package automattic/jetpack-google-analytics
+ */
+
+namespace Automattic\Jetpack;
+
+/**
+ * Class description.
+ */
+class Google_Analytics {
+
+	const PACKAGE_VERSION = '1.0.0-alpha';
+}
