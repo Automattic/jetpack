f4b068ef81cea13d750b66f5083719707933d64c
Luiz Kowalski <lhkowalski@gmail.com>
2024-04-30 18:12:09 -0300

    Add AI generated image button on external media dropdown

---
+++ projects/plugins/jetpack/extensions/shared/external-media/sources/jetpack-ai-featured-image.js
@@ -0,0 +1,7 @@
+import FeaturedImage from '../../../plugins/ai-assistant-plugin/components/featured-image';
+
+function JetpackAIFeaturedImage() {
+	return <FeaturedImage />;
+}
+
+export default JetpackAIFeaturedImage;
