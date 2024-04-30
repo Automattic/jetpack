afd1e4d63c8db2824d9d8815bead1f001e01dfa5
Anthony Grullon <agrullon95@gmail.com>
2024-04-30 17:40:23 -0400

    Log error message from user invite response

--- projects/plugins/jetpack/modules/sso/class.jetpack-sso-user-admin.php
+++ projects/plugins/jetpack/modules/sso/class.jetpack-sso-user-admin.php
@@ -252,8 +252,9 @@ if ( ! class_exists( 'Jetpack_SSO_User_Admin' ) ) :
 				);
 
 				if ( 200 !== wp_remote_retrieve_response_code( $response ) ) {
-					$error        = 'invalid-invite-api-error';
-					$query_params = array(
+					$error_message = $response->get_error_message();
+					$error         = 'invalid-invite-api-error';
+					$query_params  = array(
 						'jetpack-sso-invite-user'  => 'failed',
 						'jetpack-sso-invite-error' => $error,
 						'_wpnonce'                 => $nonce,
@@ -263,7 +264,7 @@ if ( ! class_exists( 'Jetpack_SSO_User_Admin' ) ) :
 						$event,
 						array(
 							'success'       => 'false',
-							'error_message' => $error,
+							'error_message' => $error_message,
 						)
 					);
 					return self::create_error_notice_and_redirect( $query_params );
