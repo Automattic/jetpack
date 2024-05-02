49f6911dd40da640e4689a12ef2059c6cd81d518
Paul Bunkham <paul@dobit.co.uk>
2024-05-02 13:09:26 +0100

    Revert previous change and update wpcom login step

--- projects/plugins/social/tests/e2e/specs/social-sidebar.test.js
+++ projects/plugins/social/tests/e2e/specs/social-sidebar.test.js
@@ -7,10 +7,10 @@ import { connect } from '../flows/index.js';
 test.beforeEach( async ( { page } ) => {
 	await prerequisitesBuilder( page )
 		.withCleanEnv()
-		.withLoggedIn( true )
-		.withWpComLoggedIn( true )
 		.withActivePlugins( [ 'social' ] )
 		.withInactivePlugins( [ 'jetpack' ] )
+		.withLoggedIn( true )
+		.withWpComLoggedIn( true )
 		.build();
 } );
 
