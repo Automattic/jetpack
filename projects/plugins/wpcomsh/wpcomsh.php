5418e4208eb5064316765970f32cb4967c34e583
Dean Sas <dean.sas@automattic.com>
2024-05-02 12:30:08 +0100

    Add an upsell to plugin-install.php https://github.com/Automattic/wpcomsh/pull/1839
    /wp-admin/plugin-install.php will now show a large banner promoting the
    WordPress.com plugin marketplace except when browsing favourite plugins
    or when performing a search that gets search results.
    
    It's hoped that this will help prevent the loss of plugin subscription
    revenue as the marketplace is otherwise de-emphasised.
    
    Co-authored-by: Miguel Torres <1233880+mmtr@users.noreply.github.com>

--- projects/plugins/wpcomsh/wpcomsh.php
+++ projects/plugins/wpcomsh/wpcomsh.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WordPress.com Site Helper
  * Description: A helper for connecting WordPress.com sites to external host infrastructure.
- * Version: 3.20.5
+ * Version: 3.20.6
  * Author: Automattic
  * Author URI: http://automattic.com/
  *
@@ -10,7 +10,7 @@
  */
 
 // Increase version number if you change something in wpcomsh.
-define( 'WPCOMSH_VERSION', '3.20.5' );
+define( 'WPCOMSH_VERSION', '3.20.6' );
 
 // If true, Typekit fonts will be available in addition to Google fonts
 add_filter( 'jetpack_fonts_enable_typekit', '__return_true' );
@@ -140,6 +140,7 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 
 require_once __DIR__ . '/wpcom-migration-helpers/site-migration-helpers.php';
 
+require_once __DIR__ . '/wpcom-plugins/plugins.php';
 require_once __DIR__ . '/wpcom-themes/themes.php';
 
 require_once __DIR__ . '/class-jetpack-plugin-compatibility.php';
