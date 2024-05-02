34e229de487b0dc0741e35b3695228b0ad0ab33c
bindlegirl <1242807+bindlegirl@users.noreply.github.com>
2024-05-02 12:28:28 +0200

    Packages: creating classic theme helper package (#37175)

---
+++ projects/packages/classic-theme-helper/src/class-classic-theme-helper.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Package description here
+ *
+ * @package automattic/jetpack-classic-theme-helper
+ */
+
+namespace Automattic\Jetpack;
+
+/**
+ * Class description.
+ */
+class Classic_Theme_Helper {
+
+	const PACKAGE_VERSION = '0.1.0-alpha';
+}
