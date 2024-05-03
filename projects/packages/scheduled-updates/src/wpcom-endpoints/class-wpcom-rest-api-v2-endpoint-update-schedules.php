5f4389984e3a651ccab832b662322bb04fcf8ff8
Konstantin Obenland <obenland@gmx.de>
2024-05-03 14:37:33 -0500

    Bring back health_check_paths item in public schema

--- projects/packages/scheduled-updates/src/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-update-schedules.php
+++ projects/packages/scheduled-updates/src/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-update-schedules.php
@@ -499,6 +499,10 @@ class WPCOM_REST_API_V2_Endpoint_Update_Schedules extends WP_REST_Controller {
 					'type'        => 'string',
 					'enum'        => array( 'success', 'failure-and-rollback', 'failure-and-rollback-fail' ),
 				),
+				'health_check_paths' => array(
+					'description' => 'Paths to check for site health.',
+					'type'        => 'array',
+				),
 			),
 		);
 
@@ -554,8 +558,9 @@ class WPCOM_REST_API_V2_Endpoint_Update_Schedules extends WP_REST_Controller {
 							'required'    => true,
 						),
 						'health_check_paths' => array(
-							'description' => 'Paths to check for site health.',
+							'description' => 'List of paths to check for site health after the update.',
 							'type'        => 'array',
+							'maxItems'    => 5,
 							'items'       => array(
 								'type'        => 'string',
 								'arg_options' => array(
