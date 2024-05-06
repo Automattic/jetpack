7840533abc17386ba1c0bc0de6bdb33d7a9ddb30
bindlegirl <1242807+bindlegirl@users.noreply.github.com>
2024-05-06 14:07:12 +0200

    Init plugin

---
+++ projects/plugins/classic-theme-plugin/classic-theme-plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *
+ * Plugin Name: Classic-theme-plugin
+ * Plugin URI: TBD
+ * Description: Features for classic themes.
+ * Version: 0.0-alpha
+ * Author: Automattic
+ * Author URI: https://jetpack.com/
+ * License: GPLv2 or later
+ * Text Domain: jetpack
+ *
+ * @package automattic/classic-theme-plugin
+ */
+
+// Code some good stuff!
