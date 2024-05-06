7c4922296d20dcbdb76ae313c15015ceb6f2d1b9
mmtr <1233880+mmtr@users.noreply.github.com>
2024-05-06 15:03:08 +0200

    Untangling: Only show WP.com themes banner on Popular, Latest, and Block themes

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-themes/js/banner.js
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-themes/js/banner.js
@@ -14,14 +14,32 @@ document.addEventListener( 'DOMContentLoaded', () => {
 	themeBrowser.insertAdjacentHTML(
 		'beforebegin',
 		`
-		<div class="wpcom-themes-banner">
-			<div class="wpcom-themes-banner__content">
-				<img src="${ wpcomThemesBanner.logo }" alt="WordPress.com">
-				<h3>${ wpcomThemesBanner.title }</h3>
-				<p>${ wpcomThemesBanner.description }</p>
-				<a href="${ wpcomThemesBanner.actionUrl }">${ wpcomThemesBanner.actionText }</a>
+			<div class="wpcom-themes-banner hidden">
+				<div class="wpcom-themes-banner__content">
+					<img src="${ wpcomThemesBanner.logo }" alt="WordPress.com">
+					<h3>${ wpcomThemesBanner.title }</h3>
+					<p>${ wpcomThemesBanner.description }</p>
+					<a href="${ wpcomThemesBanner.actionUrl }">${ wpcomThemesBanner.actionText }</a>
+				</div>
 			</div>
-		</div>
-	`
+		`
 	);
+
+	const themesBanner = document.querySelector( '.wpcom-themes-banner' );
+
+	const wpcomThemesObserver = new MutationObserver( () => {
+		if (
+			document.querySelector(
+				'[data-sort="popular"].current, [data-sort="new"].current, [data-sort="block-themes"].current'
+			) &&
+			! document.querySelector( '.no-results p.no-themes' ) &&
+			! document.querySelector( '.loading-content .spinner, .spinner.is-active' )
+		) {
+			themesBanner.classList.remove( 'hidden' );
+		} else {
+			themesBanner.classList.add( 'hidden' );
+		}
+	} );
+	wpcomThemesObserver.observe( themeBrowser, { childList: true, subtree: true } );
+	wpcomThemesObserver.observe( document.body, { attributes: true } );
 } );
