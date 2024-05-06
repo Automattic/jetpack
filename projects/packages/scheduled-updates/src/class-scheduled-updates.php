48c20181df2d512083b0e2d1ba3e511bc6d2ffda
Konstantin Obenland <obenland@gmx.de>
2024-05-06 15:03:19 -0500

    Scheduled Updates: Make sure sync options maintains all required data (#37240)

--- projects/packages/scheduled-updates/src/class-scheduled-updates.php
+++ projects/packages/scheduled-updates/src/class-scheduled-updates.php
@@ -193,6 +193,10 @@ class Scheduled_Updates {
 	 * Save the schedules for sync after cron option saving.
 	 */
 	public static function update_option_cron() {
+		Scheduled_Updates_Logs::add_log_fields();
+		Scheduled_Updates_Active::add_active_field();
+		Scheduled_Updates_Health_Paths::add_health_check_paths_field();
+
 		$endpoint = new \WPCOM_REST_API_V2_Endpoint_Update_Schedules();
 		$events   = $endpoint->get_items( new \WP_REST_Request() );
 
