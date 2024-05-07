8b18cf2511f01e74f6c28c33426f6608f3bac1e3
Kev <kevin.zoschke@automattic.com>
2024-05-07 09:51:27 -0400

    Tiled Gallery: fix view rendering issues (#37213)
    * Tiled Gallery: fix view rendering issues
    
    * Update saved block
    
    * Ensure gallery is rendered correctly in exising posts
    
    ---------
    
    Co-authored-by: Karen Attfield <karenlattfield@gmail.com>

--- projects/plugins/jetpack/extensions/blocks/tiled-gallery/deprecated/index.js
+++ projects/plugins/jetpack/extensions/blocks/tiled-gallery/deprecated/index.js
@@ -2,5 +2,6 @@ import * as deprecatedV1 from './v1';
 import * as deprecatedV2 from './v2';
 import * as deprecatedV3 from './v3';
 import * as deprecatedV4 from './v4';
+import * as deprecatedV5 from './v5';
 
-export default [ deprecatedV4, deprecatedV3, deprecatedV2, deprecatedV1 ];
+export default [ deprecatedV5, deprecatedV4, deprecatedV3, deprecatedV2, deprecatedV1 ];
