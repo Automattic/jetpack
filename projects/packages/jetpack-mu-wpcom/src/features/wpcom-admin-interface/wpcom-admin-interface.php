bc8d1f6fa9584a1460f6a094956626f2b9f5a27f
arthur <arthur.chu@automattic.com>
2024-05-08 17:32:46 +0800

    Save the wpcom_admin_interface option

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-admin-interface/wpcom-admin-interface.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-admin-interface/wpcom-admin-interface.php
@@ -12,10 +12,6 @@
  * The setting is displayed only if the has the wp-admin interface selected.
  */
 function wpcomsh_wpcom_admin_interface_settings_field() {
-	if ( get_option( 'wpcom_admin_interface' ) !== 'wp-admin' ) {
-		return;
-	}
-
 	add_settings_field( 'wpcom_admin_interface', null, 'wpcom_admin_interface_display', 'general', 'default' );
 
 	register_setting( 'general', 'wpcom_admin_interface', 'esc_attr' );
@@ -34,6 +30,41 @@ function wpcom_admin_interface_display() {
 	echo '</fieldset>';
 }
 
-if ( function_exists( 'wpcom_is_nav_redesign_enabled' ) && wpcom_is_nav_redesign_enabled() ) {
+if ( ! empty( get_option( 'wpcom_classic_early_release' ) ) ) {
 	add_action( 'admin_init', 'wpcomsh_wpcom_admin_interface_settings_field' );
 }
+
+/**
+ * Update the wpcom_admin_interface option on wpcom as it's the persistent data.
+ *
+ * @access private
+ * @since 4.20.0
+ *
+ * @param array $new_value The new settings value.
+ * @param array $old_value The old settings value.
+ * @return array The value to update.
+ */
+function wpcom_admin_interface_pre_update_option( $new_value, $old_value ) {
+	if ( $new_value === $old_value ) {
+		return $new_value;
+	}
+
+	if ( ! class_exists( 'Jetpack_Options' ) || ! class_exists( 'Automattic\Jetpack\Connection\Client' ) || ! class_exists( 'Automattic\Jetpack\Status\Host' ) ) {
+		return $new_value;
+	}
+
+	if ( ( new Automattic\Jetpack\Status\Host() )->is_wpcom_simple() ) {
+		return $new_value;
+	}
+
+	$blog_id = Jetpack_Options::get_option( 'id' );
+	Automattic\Jetpack\Connection\Client::wpcom_json_api_request_as_user(
+		"/sites/$blog_id/hosting/admin-interface",
+		'v2',
+		array( 'method' => 'POST' ),
+		array( 'interface' => $new_value )
+	);
+
+	return $new_value;
+}
+add_filter( 'pre_update_option_wpcom_admin_interface', 'wpcom_admin_interface_pre_update_option', 10, 2 );
