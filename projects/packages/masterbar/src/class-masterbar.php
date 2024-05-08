1ba7bad905e0f4338ca58c2a358ee2ad90105040
Foteini Giannaropoulou <giannaropoulou.foteini@gmail.com>
2024-05-08 12:17:04 +0300

    Masterbar: Introduce new package

---
+++ projects/packages/masterbar/src/class-masterbar.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Package description here
+ *
+ * @package automattic/jetpack-masterbar
+ */
+
+namespace Automattic\Jetpack;
+
+/**
+ * Class description.
+ */
+class Masterbar {
+
+	const PACKAGE_VERSION = '1.0.0-alpha';
+}
