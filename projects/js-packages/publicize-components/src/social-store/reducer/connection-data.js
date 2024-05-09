4ccd0f643de7c53d50e0e622d997e7d01f2a47b2
Manzoor Wani <manzoorwani.jk@gmail.com>
2024-05-09 15:30:45 +0530

    Social | Add disconnect/reconnect UI and logic (#37237)
    * Create selectors, actions and update reducer for disconnection
    
    * Create and wire up Disconnect component
    
    * Create and render Snackbars component for notices
    
    * Fix permission for disconnection
    
    * Fix minification mess up
    
    * Simplify Disconnect component
    
    * Create ConnectionStatus and Info components
    
    * Use the new Connection Info component
    
    * Add changelog
    
    * Fix JSX error
    
    * Fix up versions
    
    * Create GlobalNotices component
    
    * Render global notices in Jetpack and Social settings pages
    
    * Remove snackbars component in favour of the new GlobalNotices component
    
    * Wire up reconnect with modal
    
    * Add changelog
    
    * Make modal CSS class stronger than jp
    
    * Fix import
    
    * Hide broken notice if status is undefined
    
    * Revert "Create GlobalNotices component"
    
    This reverts commit d2de40d6d1ab67d888c210bbe9ae6923b055d2be.
    
    * Use globalNoticesStore
    
    * Remove unnecessary changelog
    
    * Make disconnection isolated and don't disable all disconnect buttons
    
    * Fix table shift on disconnect
    
    ---------
    
    Co-authored-by: Gergely Juhasz <gergely.juhasz@automattic.com>

--- projects/js-packages/publicize-components/src/social-store/reducer/connection-data.js
+++ projects/js-packages/publicize-components/src/social-store/reducer/connection-data.js
@@ -1,4 +1,9 @@
-import { SET_CONNECTIONS, TOGGLE_CONNECTION } from '../actions/constants';
+import {
+	DELETE_CONNECTION,
+	DELETING_CONNECTION,
+	SET_CONNECTIONS,
+	TOGGLE_CONNECTION,
+} from '../actions/constants';
 
 const connectionData = ( state = {}, action ) => {
 	switch ( action.type ) {
@@ -7,6 +12,32 @@ const connectionData = ( state = {}, action ) => {
 				...state,
 				connections: action.connections,
 			};
+
+		case DELETE_CONNECTION:
+			return {
+				...state,
+				connections: state.connections.filter( connection => {
+					// If the connection has a connection_id, then give it priority.
+					// Otherwise, use the id.
+					const isTargetConnection = connection.connection_id
+						? connection.connection_id === action.connectionId
+						: connection.id === action.connectionId;
+
+					return ! isTargetConnection;
+				} ),
+			};
+
+		case DELETING_CONNECTION: {
+			const deleting = new Set( state.deletingConnections );
+			action.deleting
+				? deleting.add( action.connectionId )
+				: deleting.delete( action.connectionId );
+
+			return {
+				...state,
+				deletingConnections: [ ...deleting ],
+			};
+		}
 
 		case TOGGLE_CONNECTION:
 			return {
