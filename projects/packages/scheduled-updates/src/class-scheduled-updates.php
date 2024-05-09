d46ac01a37d019ae8e868ab116e79bac8f162032
ouikhuan <wayne.wang@automattic.com>
2024-05-09 15:40:11 +0800

    Scheduled Updates: Remove unused status API call (#37299)
    * Remove unused status api call
    
    * changelog

--- projects/packages/scheduled-updates/src/class-scheduled-updates.php
+++ projects/packages/scheduled-updates/src/class-scheduled-updates.php
@@ -113,7 +113,6 @@ class Scheduled_Updates {
 		wpcom_rest_api_v2_load_plugin( 'WPCOM_REST_API_V2_Endpoint_Update_Schedules' );
 		wpcom_rest_api_v2_load_plugin( 'WPCOM_REST_API_V2_Endpoint_Update_Schedules_Capabilities' );
 		wpcom_rest_api_v2_load_plugin( 'WPCOM_REST_API_V2_Endpoint_Update_Schedules_Logs' );
-		wpcom_rest_api_v2_load_plugin( 'WPCOM_REST_API_V2_Endpoint_Update_Schedules_Status' );
 		wpcom_rest_api_v2_load_plugin( 'WPCOM_REST_API_V2_Endpoint_Update_Schedules_Active' );
 	}
 
