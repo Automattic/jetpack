e26f72d6df142399a3af87733b75806a0ee60222
Richard Ortiz <rcrd.ortiz@gmail.com>
2024-05-10 16:42:59 +0200

    Moved the fix to its own function so that it is semantically correct

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-themes/wpcom-themes.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-themes/wpcom-themes.php
@@ -45,12 +45,6 @@ function wpcom_themes_show_banner() {
 		array(),
 		Jetpack_Mu_Wpcom::PACKAGE_VERSION
 	);
-	wp_enqueue_style(
-		'wpcom-themes-ui-fixes',
-		plugins_url( 'css/ui-fixes.css', __FILE__ ),
-		array(),
-		Jetpack_Mu_Wpcom::PACKAGE_VERSION
-	);
 }
 add_action( 'load-theme-install.php', 'wpcom_themes_show_banner' );
 
@@ -91,6 +85,13 @@ function wpcom_themes_remove_wpcom_actions() {
 			'in_footer' => true,
 		)
 	);
+}
+add_action( 'load-themes.php', 'wpcom_themes_remove_wpcom_actions' );
+
+/**
+ * Adds a CSS file to fix UI issues in the theme browser.
+ */
+function wpcom_themes_load_ui_fixes() {
 	wp_enqueue_style(
 		'wpcom-themes-ui-fixes',
 		plugins_url( 'css/ui-fixes.css', __FILE__ ),
@@ -98,4 +99,4 @@ function wpcom_themes_remove_wpcom_actions() {
 		Jetpack_Mu_Wpcom::PACKAGE_VERSION
 	);
 }
-add_action( 'load-themes.php', 'wpcom_themes_remove_wpcom_actions' );
+add_action( 'load-themes.php', 'wpcom_themes_load_ui_fixes' );
