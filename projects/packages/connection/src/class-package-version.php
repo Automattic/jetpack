83fcea883f693eb82d5d6b5b274b6073db72e4bf
Anthony Grullon <agrullon95@gmail.com>
2024-05-10 05:58:10 -0400

    SSO: Log error message from user invite response (#37144)
    * Log error message from user invite response
    
    * changelog
    
    * Check for wp_error
    
    * Retrieve error message
    
    * Copy changes to connection package
    
    * changelog
    
    * Connection package version update
    
    * Display user invite error message
    
    * update query param key
    
    * Update logic to display error message on users screen
    
    * Sync latest sso user admin changes in Connection package
    
    * Update connection package version
    
    * Remove duplicate changelogs
    
    * Update projects/packages/connection/src/sso/class-user-admin.php
    
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    * Update projects/packages/connection/src/sso/class-user-admin.php
    
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    * Update projects/plugins/jetpack/modules/sso/class.jetpack-sso-user-admin.php
    
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    * Update projects/plugins/jetpack/modules/sso/class.jetpack-sso-user-admin.php
    
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    * Display api error response for invite revoke
    
    * Update revoke user invite error tracking
    
    * Bump version
    
    ---------
    
    Co-authored-by: mindbuc <immanuelkanterino@gmail.com>
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>

--- projects/packages/connection/src/class-package-version.php
+++ projects/packages/connection/src/class-package-version.php
@@ -12,7 +12,7 @@ namespace Automattic\Jetpack\Connection;
  */
 class Package_Version {
 
-	const PACKAGE_VERSION = '2.7.7';
+	const PACKAGE_VERSION = '2.8.0-alpha';
 
 	const PACKAGE_SLUG = 'connection';
 
--- projects/packages/connection/src/sso/class-user-admin.php
+++ projects/packages/connection/src/sso/class-user-admin.php
@@ -112,12 +112,18 @@ class User_Admin {
 				$event    = 'sso_user_invite_revoked';
 
 				if ( 200 !== wp_remote_retrieve_response_code( $response ) ) {
+					$body                = json_decode( wp_remote_retrieve_body( $response ) );
+					$tracking_event_data = array(
+						'success'    => 'false',
+						'error_code' => 'invalid-revoke-api-error',
+					);
+
+					if ( ! empty( $body ) && ! empty( $body->message ) ) {
+						$tracking_event_data['error_message'] = $body->message;
+					}
 					self::$tracking->record_user_event(
 						$event,
-						array(
-							'success'       => 'false',
-							'error_message' => 'invalid-revoke-api-error',
-						)
+						$tracking_event_data
 					);
 					return $response;
 				}
@@ -169,6 +175,10 @@ class User_Admin {
 
 		if ( $_GET['jetpack-sso-invite-user'] === 'successful-revoke' ) {
 			return wp_admin_notice( __( 'User invite revoked successfully.', 'jetpack-connection' ), array( 'type' => 'success' ) );
+		}
+
+		if ( $_GET['jetpack-sso-invite-user'] === 'failed' && isset( $_GET['jetpack-sso-api-error-message'] ) ) {
+			return wp_admin_notice( wp_kses( wp_unslash( $_GET['jetpack-sso-api-error-message'] ), array() ), array( 'type' => 'error' ) );
 		}
 
 		if ( $_GET['jetpack-sso-invite-user'] === 'failed' && isset( $_GET['jetpack-sso-invite-error'] ) ) {
@@ -261,19 +271,27 @@ class User_Admin {
 			);
 
 			if ( 200 !== wp_remote_retrieve_response_code( $response ) ) {
-				$error        = 'invalid-invite-api-error';
+				$error_code   = 'invalid-invite-api-error';
 				$query_params = array(
 					'jetpack-sso-invite-user'  => 'failed',
-					'jetpack-sso-invite-error' => $error,
+					'jetpack-sso-invite-error' => $error_code,
 					'_wpnonce'                 => $nonce,
 				);
 
+				$tracking_event_data = array(
+					'success'    => 'false',
+					'error_code' => $error_code,
+				);
+
+				$body = json_decode( wp_remote_retrieve_body( $response ) );
+				if ( ! empty( $body ) && ! empty( $body->message ) ) {
+					$query_params['jetpack-sso-api-error-message'] = $body->message;
+					$tracking_event_data['error_message']          = $body->message;
+				}
+
 				self::$tracking->record_user_event(
 					$event,
-					array(
-						'success'       => 'false',
-						'error_message' => $error,
-					)
+					$tracking_event_data
 				);
 				return self::create_error_notice_and_redirect( $query_params );
 			}
@@ -410,12 +428,21 @@ class User_Admin {
 					'jetpack-sso-invite-error' => $error, // general error message
 					'_wpnonce'                 => $nonce,
 				);
+
+				$tracking_event_data = array(
+					'success'    => 'false',
+					'error_code' => $error,
+				);
+
+				$body = json_decode( wp_remote_retrieve_body( $response ) );
+				if ( ! empty( $body ) && ! empty( $body->message ) ) {
+					$query_params['jetpack-sso-api-error-message'] = $body->message;
+					$tracking_event_data['error_message']          = $body->message;
+				}
+
 				self::$tracking->record_user_event(
 					$event,
-					array(
-						'success'       => 'false',
-						'error_message' => $error,
-					)
+					$tracking_event_data
 				);
 				return self::create_error_notice_and_redirect( $query_params );
 			}
