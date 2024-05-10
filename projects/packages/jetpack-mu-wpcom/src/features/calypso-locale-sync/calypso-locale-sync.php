db5291675cb87b77879e749197176806d83017c4
Miguel <miguel.san.segundo@automattic.com>
2024-05-10 19:06:50 +0700

    Use /me/language endpoint and support site default option

--- projects/packages/jetpack-mu-wpcom/src/features/calypso-locale-sync/calypso-locale-sync.php
+++ projects/packages/jetpack-mu-wpcom/src/features/calypso-locale-sync/calypso-locale-sync.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * Sync Calypso locale when Admin locale is updated
- * only for wpcom_admin_interface = wp-admin and wpcom_classic_early_release = 1
+ * Sync Calypso locale when wp-admin locale is updated via /wp-admin/profile.php.
  *
  * @package automattic/jetpack-mu-wpcom
  */
@@ -11,26 +10,23 @@ use Automattic\Jetpack\Connection\Client;
 if ( function_exists( 'wpcom_is_nav_redesign_enabled' ) && wpcom_is_nav_redesign_enabled() ) {
 	add_filter(
 		'profile_update',
-		function ( $user_id, $old_user_data, $userdata ) {
-			if ( ! $userdata['locale'] ) {
-				// error_log( '>>> No locale means "Site defaults"' );
-				// TODO: get the site locale (I think that's the option lang_id)
-				return;
+		function ( $user_id, $old_user_data, $user_data ) {
+			$locale = $user_data['locale'];
+
+			if ( ! $locale ) {
+				// No locale means the "Site Default" option which is the Site language (WPLANG).
+				$locale = get_option( 'WPLANG', '' );
 			}
-			// Tried also with wpcom_json_api_request_as_user
-			$response = Client::wpcom_json_api_request_as_blog(
-				'/me/settings?meta=locale&force=wpcom',
-				'1.1',
+
+			Client::wpcom_json_api_request_as_user(
+				'/me/language',
+				'2',
 				array(
-					'method'  => 'POST',
-					'headers' => array( 'Content-Type' => 'application/json; charset=utf-8' ),
+					'method' => 'POST',
 				),
-				// TODO: locale needs to be validated because only mag-16 languages are supported in Calypso
-				wp_json_encode( array( 'language' => $userdata['locale'] ) ),
-				'rest'
+				(object) array( 'language' => $locale ),
+				'wpcom'
 			);
-			// The requests returns {"error":"authorization_required","message":"An active access token must be used to query information about the current user."}
-			// error_log( print_r( $response, true ) );
 		},
 		8,
 		3
