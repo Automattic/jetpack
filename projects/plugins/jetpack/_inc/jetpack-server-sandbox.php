4eee3938f33c6bb276794a31cd4ec59bed7625c6
Brad Jorsch <brad.jorsch@automattic.com>
2024-05-10 16:07:44 -0400

    Just remove projects/plugins/jetpack/_inc/jetpack-server-sandbox.php entirely, it hasn't been loaded since 11.4.

--- projects/plugins/jetpack/_inc/jetpack-server-sandbox.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * The feature formerly here is now found in `Automattic\Jetpack\Connection\Server_Sandbox`
- * in the jetpack-connection package.
- *
- * @package automattic/jetpack
- */
