bb44cfd149efe84a3bcb00846de92216e9500561
Miguel <miguel.san.segundo@automattic.com>
2024-05-13 16:54:17 +0700

    WordPress.com Features: Add wp-admin sync with Calypso locale

---
+++ projects/packages/jetpack-mu-wpcom/src/features/wp-admin-locale-sync/wp-admin-locale-sync.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Sync wp-admin locale with Calypso locale.
+ *
+ * @package automattic/jetpack-mu-wpcom
+ */
+
+use Automattic\Jetpack\Connection\Manager as Connection_Manager;
+
+if ( function_exists( 'wpcom_is_nav_redesign_enabled' ) && wpcom_is_nav_redesign_enabled() ) {
+	add_filter(
+		'admin_init',
+		function () {
+			// Get user connection
+			$connection_manager = new Connection_Manager( 'jetpack' );
+			if ( ! $connection_manager->is_user_connected( get_current_user_id() ) ) {
+				return;
+			}
+			// Get user locale
+			$user_data = $connection_manager->get_connected_user_data( get_current_user_id() );
+			$locale    = get_jetpack_locale( $user_data['user_locale'] );
+
+			// Check for changes
+			if ( $locale && $locale !== get_user_option( 'locale' ) ) {
+				// Install
+				install_locale( $locale );
+
+				// Update user meta
+				update_user_option( get_current_user_id(), 'locale', $locale, true );
+
+				// @TODO: Apply the new locale to the sidebar
+				// Other wise, it needs a browser refresh.
+			}
+		}
+	);
+}
+
+/**
+ * Get Jetpack locale name.
+ *
+ * @param  string $slug Locale slug.
+ * @return string Jetpack locale.
+ */
+function get_jetpack_locale( $slug = '' ) {
+	if ( ! class_exists( 'GP_Locales' ) ) {
+		if ( defined( 'JETPACK__GLOTPRESS_LOCALES_PATH' ) && file_exists( JETPACK__GLOTPRESS_LOCALES_PATH ) ) {
+			require JETPACK__GLOTPRESS_LOCALES_PATH;
+		}
+	}
+
+	if ( class_exists( 'GP_Locales' ) ) {
+		$jetpack_locale_object = GP_Locales::by_field( 'slug', $slug );
+		if ( $jetpack_locale_object instanceof GP_Locale ) {
+			$jetpack_locale = $jetpack_locale_object->wp_locale ? $jetpack_locale_object->wp_locale : 'en_US';
+		}
+	}
+
+	return $jetpack_locale;
+}
+
+/**
+ * Install locale if not yet available.
+ *
+ * @param string $locale The new locale slug.
+ */
+function install_locale( $locale = '' ) {
+	if ( ! in_array( $locale, get_available_languages(), true )
+	&& ! empty( $locale ) && current_user_can( 'install_languages' ) ) {
+		if ( ! function_exists( 'wp_download_language_pack' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/translation-install.php';
+		}
+
+		if ( ! function_exists( 'request_filesystem_credentials' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/file.php';
+		}
+
+		if ( wp_can_install_language_pack() ) {
+			wp_download_language_pack( $locale );
+			load_default_textdomain( $locale );
+		}
+	}
+}
