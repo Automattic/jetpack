bf2a700ceafcbe1ccfb699c902bb4d5caa89b7e3
Manzoor Wani <manzoorwani.jk@gmail.com>
2024-05-13 18:49:38 +0530

    Render available and connected accounts separately

--- projects/js-packages/publicize-components/src/components/add-connection-modal/confirmation-form/index.tsx
+++ projects/js-packages/publicize-components/src/components/add-connection-modal/confirmation-form/index.tsx
@@ -18,6 +18,31 @@ type ConfirmationFormProps = {
 	onComplete: VoidFunction;
 };
 
+type AccountOption = { label: string; value: string; profile_picture?: string };
+
+type AccountInfoProps = {
+	label: string;
+	profile_picture?: string;
+};
+
+/**
+ * Account info
+ *
+ * @param {AccountInfoProps} props - Component props
+ *
+ * @returns {import('react').ReactNode} Account info component
+ */
+function AccountInfo( { label, profile_picture }: AccountInfoProps ) {
+	return (
+		<div className={ styles[ 'account-info' ] }>
+			{ profile_picture ? (
+				<img className={ styles[ 'propfile-pic' ] } src={ profile_picture } alt={ label } />
+			) : null }
+			{ label }&nbsp;
+		</div>
+	);
+}
+
 /**
  * Connection confirmation component
  *
@@ -51,38 +76,38 @@ export function ConfirmationForm( { keyringResult, onComplete }: ConfirmationFor
 		[ existingConnections, service.ID ]
 	);
 
-	const accountOptions = useMemo( () => {
-		const options: Array< { label: string; value: string; profile_picture?: string } > = [];
+	const { connected, not_connected } = useMemo( () => {
+		const options: Array< AccountOption > = [];
 
 		// Better safe than sorry
-		if ( ! service ) {
-			return options;
-		}
-
-		// If user account is supported, add it to the list
-		if ( ! service.external_users_only ) {
-			options.push( {
-				label: keyringResult.external_display,
-				value: keyringResult.external_ID,
-				profile_picture: keyringResult.external_profile_picture,
-			} );
-		}
-
-		if (
-			service.multiple_external_user_ID_support &&
-			keyringResult.additional_external_users?.length
-		) {
-			for ( const user of keyringResult.additional_external_users ) {
+		if ( service ) {
+			// If user account is supported, add it to the list
+			if ( ! service.external_users_only ) {
 				options.push( {
-					label: user.external_name,
-					value: user.external_ID,
-					profile_picture: user.external_profile_picture,
+					label: keyringResult.external_display,
+					value: keyringResult.external_ID,
+					profile_picture: keyringResult.external_profile_picture,
 				} );
+			}
+
+			if (
+				service.multiple_external_user_ID_support &&
+				keyringResult.additional_external_users?.length
+			) {
+				for ( const user of keyringResult.additional_external_users ) {
+					options.push( {
+						label: user.external_name,
+						value: user.external_ID,
+						profile_picture: user.external_profile_picture,
+					} );
+				}
 			}
 		}
 
-		return options;
-	}, [ keyringResult, service ] );
+		return Object.groupBy( options, ( { value } ) => {
+			return isAlreadyConnected( value ) ? 'connected' : 'not_connected';
+		} );
+	}, [ isAlreadyConnected, keyringResult, service ] );
 
 	const { createConnection } = useDispatch( socialStore );
 
@@ -127,105 +152,114 @@ export function ConfirmationForm( { keyringResult, onComplete }: ConfirmationFor
 		]
 	);
 
-	if ( ! accountOptions.length ) {
-		// TODO Make this more useful. For example, in case of Instagram, we could show a message that only Instagra business accounts are supported.
-		return <div>{ __( 'No accounts/pages found.', 'jetpack' ) }</div>;
-	}
-
-	let defaultSelected = false;
-
 	return (
 		<section className={ styles.confirmation }>
-			<p>
-				{ __(
-					`Select the account you'd like to connect. All your new blog posts will be automatically shared to this account. You'll be able to change this option in the editor sidebar when you're writing a post.`,
-					'jetpack'
-				) }
-			</p>
-			<form className={ styles.form } onSubmit={ onConfirm }>
-				{
-					//
-					/**
-					 * It is such a shame that we can't use any of the form components from @wordpress/components here.
-					 * Because of the way the components are designed, we can't use them in an uncontrolled way.
-					 * Every component is forced be used only in controlled mode.
-					 *
-					 * @see https://github.com/WordPress/gutenberg/issues/57004
-					 */
-				 }
-				<div className={ styles[ 'accounts-list' ] }>
-					{ accountOptions.map( option => {
-						const alreadyConnected = isAlreadyConnected( option.value );
-						const defaultChecked = ! alreadyConnected && ! defaultSelected;
+			{ ! not_connected?.length ? (
+				<div>
+					{
+						// TODO Make this more useful. For example, in case of Instagram, we could show a message that only Instagra business accounts are supported.
+						connected?.length
+							? _x(
+									'No more accounts/pages found.',
+									'Message shown when there are no connections found to connect',
+									'jetpack'
+							  )
+							: __( 'No accounts/pages found.', 'jetpack' )
+					}
+				</div>
+			) : (
+				<div>
+					<p>
+						{ __(
+							`Select the account you'd like to connect. All your new blog posts will be automatically shared to this account. You'll be able to change this option in the editor sidebar when you're writing a post.`,
+							'jetpack'
+						) }
+					</p>
+					<form className={ styles.form } onSubmit={ onConfirm } id="connection-confirmation-form">
+						{
+							//
+							/**
+							 * It is such a shame that we can't use any of the form components from @wordpress/components here.
+							 * Because of the way the components are designed, we can't use them in an uncontrolled way.
+							 * Every component is forced be used only in controlled mode.
+							 *
+							 * @see https://github.com/WordPress/gutenberg/issues/57004
+							 */
+						 }
+						<div className={ styles[ 'accounts-list' ] }>
+							{ not_connected.map( ( option, index ) => {
+								return (
+									<label key={ option.value } className={ styles[ 'account-label' ] } aria-required>
+										<input
+											type="radio"
+											name="external_user_ID"
+											value={ option.value }
+											defaultChecked={ index === 0 }
+											className={ styles[ 'account-input' ] }
+											required
+										/>
+										<AccountInfo
+											label={ option.label }
+											profile_picture={ option.profile_picture }
+										/>
+									</label>
+								);
+							} ) }
+						</div>
 
-						if ( defaultChecked ) {
-							defaultSelected = true;
-						}
+						<BaseControl
+							id="mark-connection-as-shared"
+							help={ __(
+								'If enabled, the connection will be available to all administrators, editors, and authors.',
+								'jetpack'
+							) }
+						>
+							<HStack justify="flex-start" spacing={ 3 }>
+								<span>
+									<input type="checkbox" id="mark-connection-as-shared" name="shared" value="1" />
+								</span>
+								<FlexBlock as="label" htmlFor="mark-connection-as-shared">
+									{ __( 'Mark the connection as shared', 'jetpack' ) }
+								</FlexBlock>
+							</HStack>
+						</BaseControl>
 
-						return (
-							<label
-								key={ option.value }
-								className={ styles[ 'account-label' ] }
-								aria-disabled={ alreadyConnected }
-								aria-required
-							>
-								<input
-									type="radio"
-									name="external_user_ID"
-									value={ option.value }
-									defaultChecked={ defaultChecked }
-									className={ styles[ 'account-input' ] }
-									disabled={ alreadyConnected }
-									required
+						<input type="hidden" name="keyring_connection_ID" value={ keyringResult.ID } />
+					</form>
+				</div>
+			) }
+
+			{ connected?.length ? (
+				<section>
+					<h3>{ __( 'Already connected', 'jetpack' ) }</h3>
+					<ul>
+						{ connected.map( ( connection, i ) => (
+							<li key={ connection.label + i }>
+								<AccountInfo
+									label={ connection.label }
+									profile_picture={ connection.profile_picture }
 								/>
-								{ option.profile_picture ? (
-									<img
-										className={ styles[ 'propfile-pic' ] }
-										src={ option.profile_picture }
-										alt={ option.label }
-									/>
-								) : null }
-								{ option.label }&nbsp;
-								{ alreadyConnected ? __( '(Already connected)', 'jetpack' ) : '' }
-							</label>
-						);
-					} ) }
-				</div>
+							</li>
+						) ) }
+					</ul>
+				</section>
+			) : null }
 
-				<BaseControl
-					id="mark-connection-as-shared"
-					help={ __(
-						'If enabled, the connection will be available to all administrators, editors, and authors.',
-						'jetpack'
-					) }
+			<div className={ styles[ 'submit-wrap' ] }>
+				<Button variant="secondary" onClick={ onComplete }>
+					{ __( 'Cancel', 'jetpack' ) }
+				</Button>
+				<Button
+					form="connection-confirmation-form"
+					type="submit"
+					disabled={ isCreatingConnection }
+					isLoading={ isCreatingConnection }
 				>
-					<HStack justify="flex-start" spacing={ 3 }>
-						<span>
-							<input type="checkbox" id="mark-connection-as-shared" name="shared" value="1" />
-						</span>
-						<FlexBlock as="label" htmlFor="mark-connection-as-shared">
-							{ __( 'Mark the connection as shared', 'jetpack' ) }
-						</FlexBlock>
-					</HStack>
-				</BaseControl>
-
-				<input type="hidden" name="keyring_connection_ID" value={ keyringResult.ID } />
-
-				<div className={ styles[ 'submit-wrap' ] }>
-					<Button variant="secondary" onClick={ onComplete }>
-						{ __( 'Cancel', 'jetpack' ) }
-					</Button>
-					<Button
-						type="submit"
-						disabled={ isCreatingConnection }
-						isLoading={ isCreatingConnection }
-					>
-						{ isCreatingConnection
-							? _x( 'Connecting…', 'Connecting a social media account', 'jetpack' )
-							: __( 'Confirm', 'jetpack' ) }
-					</Button>
-				</div>
-			</form>
+					{ isCreatingConnection
+						? _x( 'Connecting…', 'Connecting a social media account', 'jetpack' )
+						: __( 'Confirm', 'jetpack' ) }
+				</Button>
+			</div>
 		</section>
 	);
 }
