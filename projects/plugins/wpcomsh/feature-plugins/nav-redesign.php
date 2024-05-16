33ea9665de0450188e16bfa0e8c6e26bbe03cc27
Ashar Fuadi <ashar.fuadi@automattic.com>
2024-05-16 11:32:23 +0700

    Nav redesign: remove Horizon link overrides https://github.com/Automattic/wpcomsh/pull/1872
    * Nav redesign: remove Horizon link overrides
    
    * Bump version to 3.20.20

--- projects/plugins/wpcomsh/feature-plugins/nav-redesign.php
+++ projects/plugins/wpcomsh/feature-plugins/nav-redesign.php
@@ -51,29 +51,6 @@ function add_hosting_overview_menu() {
 add_action( 'admin_menu', 'add_hosting_overview_menu', 999999 );
 
 /**
- * Temporarily override the All Sites menu to point to horizon.wordpress.com,
- * which enables the nav redesign by default.
- */
-function temporarily_override_all_sites_menus_to_horizon() {
-	global $wp_admin_bar;
-
-	if ( ! wpcom_is_nav_redesign_enabled() || ! is_proxied() ) {
-		return;
-	}
-
-	$node = $wp_admin_bar->get_node( 'all-sites' );
-	if ( $node ) {
-		$wp_admin_bar->remove_node( $node->id );
-
-		$args         = (array) $node;
-		$args['href'] = str_replace( 'https://wordpress.com/', 'https://horizon.wordpress.com/', $args['href'] );
-
-		$wp_admin_bar->add_node( $args );
-	}
-}
-add_action( 'admin_bar_menu', 'temporarily_override_all_sites_menus_to_horizon', 16 );
-
-/**
  * Temporarily hides the Hosting menus that are already in GSV.
  */
 function temporarily_hide_hosting_menus_already_in_global_site_view() {
@@ -95,20 +72,3 @@ function temporarily_hide_hosting_menus_already_in_global_site_view() {
 	}
 }
 add_action( 'admin_menu', 'temporarily_hide_hosting_menus_already_in_global_site_view', PHP_INT_MAX );
-
-/**
- * Temporarily override the Hosting menus to point to horizon.wordpress.com,
- * which enables the nav redesign by default.
- */
-function temporarily_override_hosting_menus_to_horizon() {
-	global $submenu;
-
-	if ( ! wpcom_is_nav_redesign_enabled() || ! is_proxied() ) {
-		return;
-	}
-
-	foreach ( $submenu['wpcom-hosting-menu'] as &$menu_item ) {
-		$menu_item[2] = str_replace( 'https://wordpress.com/', 'https://horizon.wordpress.com/', $menu_item[2] );
-	}
-}
-add_action( 'admin_menu', 'temporarily_override_hosting_menus_to_horizon', PHP_INT_MAX );
