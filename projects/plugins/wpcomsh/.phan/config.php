c055644495c00bc0c57150d5be5fbc545128c2f7
Igor Zinovyev <zinigor@gmail.com>
2024-05-17 16:48:49 +0300

    Updated phan config.

--- projects/plugins/wpcomsh/.phan/config.php
+++ projects/plugins/wpcomsh/.phan/config.php
@@ -10,4 +10,14 @@
 // Require base config.
 require __DIR__ . '/../../../../.phan/config.base.php';
 
-return make_phan_config( dirname( __DIR__ ) );
+$root = dirname( __DIR__, 4 );
+
+return make_phan_config(
+	dirname( __DIR__ ),
+	array(
+		'parse_file_list' => array(
+			"$root/projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php",
+			"$root/projects/packages/connection/src/class-tracking.php",
+		),
+	)
+);
