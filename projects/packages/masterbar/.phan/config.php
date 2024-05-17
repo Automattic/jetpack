4b63fb7a2f3447ad6ba17a20a292575437a1924f
Foteini Giannaropoulou <giannaropoulou.foteini@gmail.com>
2024-05-17 17:20:34 +0300

    Update phan config with Jetpack-the-plugin dependencies

--- projects/packages/masterbar/.phan/config.php
+++ projects/packages/masterbar/.phan/config.php
@@ -11,4 +11,22 @@
 require __DIR__ . '/../../../../.phan/config.base.php';
 
 // We need to load the wpcom and amp stubs too.
-return make_phan_config( dirname( __DIR__ ), array( '+stubs' => array( 'wpcom', 'amp' ) ) );
+return make_phan_config(
+	dirname( __DIR__ ),
+	array(
+		'+stubs'          => array( 'wpcom', 'amp' ),
+		'parse_file_list' => array(
+
+			/*
+			Reference files to handle code checking for stuff from Jetpack-the-plugin or other in-monorepo plugins.
+			 * Wherever feasible we should really clean up this sort of thing instead of adding stuff here.
+			 *
+			 * DO NOT add references to files in other packages like this! Generally packages should be listed in composer.json 'require'.
+			 * If there are truly optional dependencies or circular dependencies that can't be cleaned up, one package may list the
+			 * other in 'require-dev' and `extra.dependencies.test-only' instead. See packages/config for an example.
+			 */
+			__DIR__ . '/../../../plugins/jetpack/3rd-party/class.jetpack-amp-support.php',
+			__DIR__ . '/../../../plugins/jetpack/modules/custom-css/custom-css.php',
+		),
+	)
+);
