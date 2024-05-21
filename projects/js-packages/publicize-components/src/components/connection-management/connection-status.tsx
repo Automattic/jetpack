d26d36e47ae0842dfc5131c29e7c1109c3faee63
Manzoor Wani <manzoorwani.jk@gmail.com>
2024-05-21 19:03:51 +0530

    Update UI components to fire reconnect window

--- projects/js-packages/publicize-components/src/components/connection-management/connection-status.tsx
+++ projects/js-packages/publicize-components/src/components/connection-management/connection-status.tsx
@@ -1,10 +1,12 @@
 import { __ } from '@wordpress/i18n';
 import { Connection } from '../../social-store/types';
-import { Disconnect } from './disconnect';
+import { SupportedService } from '../services/use-supported-services';
+import { Reconnect } from './reconnect';
 
 export type ConnectionStatusProps = {
 	connection: Connection;
-	onReconnect?: VoidFunction;
+	onConfirmReconnect?: VoidFunction;
+	service: SupportedService;
 };
 
 /**
@@ -14,7 +16,11 @@ export type ConnectionStatusProps = {
  *
  * @returns {import('react').ReactNode} - React element
  */
-export function ConnectionStatus( { connection, onReconnect }: ConnectionStatusProps ) {
+export function ConnectionStatus( {
+	connection,
+	service,
+	onConfirmReconnect,
+}: ConnectionStatusProps ) {
 	if ( connection.status === undefined || connection.status === 'ok' ) {
 		return null;
 	}
@@ -29,14 +35,10 @@ export function ConnectionStatus( { connection, onReconnect }: ConnectionStatusP
 		<div>
 			<span className="description">{ notice }</span>
 			&nbsp;
-			<Disconnect
+			<Reconnect
 				connection={ connection }
-				label={ __( 'Reconnect', 'jetpack' ) }
-				showSuccessNotice={ false }
-				onDisconnect={ onReconnect }
-				variant="link"
-				isDestructive={ false }
-				showConfirmation={ false }
+				service={ service }
+				onConfirmReconnect={ onConfirmReconnect }
 			/>
 		</div>
 	);
