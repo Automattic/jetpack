2e6741a5239033b1ed49817d3269c122f597fefd
Luiz Kowalski <lhkowalski@gmail.com>
2024-05-21 17:00:00 -0300

    Drop the apiFetch call since the issue is on the backend

--- projects/js-packages/ai-client/src/hooks/use-image-generator/index.ts
+++ projects/js-packages/ai-client/src/hooks/use-image-generator/index.ts
@@ -1,13 +1,11 @@
 /**
  * External dependencies
  */
-import { isSimpleSite } from '@automattic/jetpack-shared-extension-utils';
 import { __ } from '@wordpress/i18n';
 import debugFactory from 'debug';
 /**
  * Internal dependencies
  */
-import apiFetch from '../../api-fetch/index.js';
 import askQuestionSync from '../../ask-question/sync.js';
 import requestJwt from '../../jwt/index.js';
 
@@ -159,7 +157,6 @@ const useImageGenerator = () => {
 		postContent: string;
 		userPrompt?: string;
 	} ): Promise< { data: Array< { [ key: string ]: string } > } > {
-		const isSimple = isSimpleSite();
 		let token = null;
 
 		try {
@@ -186,28 +183,16 @@ const useImageGenerator = () => {
 				height: 768,
 			};
 
-			let response: Response = null;
-			if ( isSimple ) {
-				response = await apiFetch( {
-					path: `/wpcom/v2/sites/${ token.blogId }/ai-image`,
+			const response: Response = await fetch(
+				`https://public-api.wordpress.com/wpcom/v2/sites/${ token.blogId }/ai-image`,
+				{
 					method: 'POST',
 					headers: {
 						'Content-Type': 'application/json',
 					},
 					body: JSON.stringify( data ),
-				} );
-			} else {
-				response = await fetch(
-					`https://public-api.wordpress.com/wpcom/v2/sites/${ token.blogId }/ai-image`,
-					{
-						method: 'POST',
-						headers: {
-							'Content-Type': 'application/json',
-						},
-						body: JSON.stringify( data ),
-					}
-				);
-			}
+				}
+			);
 
 			if ( ! response?.ok ) {
 				debug( 'Error generating image: %o', response );
