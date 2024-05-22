4d766ca98d53b12b45bac0ca5bcb34d0b96be7df
Peter Petrov <peter.petrov89@gmail.com>
2024-05-22 13:32:13 +0300

    Update elementor compatibility checks

--- projects/plugins/boost/compatibility/elementor.php
+++ projects/plugins/boost/compatibility/elementor.php
@@ -7,22 +7,19 @@
 
 namespace Automattic\Jetpack_Boost\Compatibility\Elementor;
 
-use Elementor\Modules\LandingPages\Module;
-use Elementor\TemplateLibrary\Source_Local;
-
 /**
  * Exclude Elementor Library custom post type from the list of post types to get urls from.
  *
  * @param array $post_types Post types.
  */
 function exclude_elementor_library_custom_post_type( $post_types ) {
-	if ( isset( $post_types[ Source_Local::CPT ] ) ) {
-		unset( $post_types[ Source_Local::CPT ] );
+	if ( defined( '\Elementor\TemplateLibrary\Source_Local::CPT' ) ) {
+		unset( $post_types[ \Elementor\TemplateLibrary\Source_Local::CPT ] );
 	}
 
 	// Elementor's landing pages are broken. See https://github.com/elementor/elementor/issues/16244
-	if ( isset( $post_types[ Module::CPT ] ) ) {
-		unset( $post_types[ Module::CPT ] );
+	if ( defined( '\Elementor\Modules\LandingPages\Module::CPT' ) ) {
+		unset( $post_types[ \Elementor\Modules\LandingPages\Module::CPT ] );
 	}
 
 	if ( isset( $post_types['elementor-hf'] ) ) {
