8fae9513aafb72958c10814adfe20a661be53caf
Manzoor Wani <manzoorwani.jk@gmail.com>
2024-05-24 22:39:59 +0530

    Sort generated icons list

--- projects/js-packages/social-logos/tools/svg-to-react-data.js
+++ projects/js-packages/social-logos/tools/svg-to-react-data.js
@@ -36,6 +36,8 @@ export const SocialLogoData = [`;
 
 const files = glob.sync( svgDir + '/*.svg' );
 
+files.sort( ( a, z ) => path.basename( a, '.svg' ).localeCompare( path.basename( z, '.svg' ) ) );
+
 files.forEach( file => {
 	// Get logo name from SVG file
 	const logoName = path.basename( file, '.svg' );
