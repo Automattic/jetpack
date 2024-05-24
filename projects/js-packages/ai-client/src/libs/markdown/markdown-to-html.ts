75e0965e0e2e13c2e2a3776056b98d3a5fc0be6e
Douglas <douglas.henri@automattic.com>
2024-05-24 18:36:04 -0300

    update list markdown to html fix

--- projects/js-packages/ai-client/src/libs/markdown/markdown-to-html.ts
+++ projects/js-packages/ai-client/src/libs/markdown/markdown-to-html.ts
@@ -8,30 +8,38 @@ import MarkdownIt from 'markdown-it';
 import type { Options } from 'markdown-it';
 
 export type Fix = 'list' | 'paragraph' | 'listItem';
+
+const addListComments = ( content: string ) => {
+	return (
+		content
+			// Add Gutenberg comments to <li> tags
+			.replaceAll( '<li>', '<!-- wp:list-item --><li >' )
+			.replaceAll( '</li>', '</li ><!-- /wp:list-item -->' )
+			// Add Gutenberg comments to <ol> tags
+			.replaceAll( '<ol>', '<!-- wp:list {"ordered":true} --><ol >' )
+			.replaceAll( '</ol>', '</ol ><!-- /wp:list -->' )
+			// Add Gutenberg comments to <ul> tags
+			.replaceAll( '<ul>', '<!-- wp:list --><ul >' )
+			.replaceAll( '</ul>', '</ul ><!-- /wp:list -->' )
+	);
+};
+
 type Fixes = {
 	[ key in Fix ]: ( content: string ) => string;
 };
-
 const fixes: Fixes = {
 	list: ( content: string ) => {
-		// Fix list indentation
-		return content.replace( /<li>\s+<p>/g, '<li>' ).replace( /<\/p>\s+<\/li>/g, '</li>' );
+		return addListComments(
+			// Fix list indentation
+			content.replace( /<li>\s+<p>/g, '<li>' ).replace( /<\/p>\s+<\/li>/g, '</li>' )
+		);
 	},
 	listItem: ( content: string ) => {
-		return (
+		return addListComments(
 			content
 				// Remove wrapping <ul> or <ol> tag
 				.replace( /^<[ou]l>\s*/g, '' )
 				.replace( /\s*<\/[ou]l>\s*$/g, '' )
-				// Add Gutenberg comments to <li> tags
-				.replaceAll( '<li>', '<!-- wp:list-item --><li>' )
-				.replaceAll( '</li>', '</li><!-- /wp:list-item -->' )
-				// Add Gutenberg comments to <ol> tags
-				.replaceAll( '<ol>', '<!-- wp:list {"ordered":true} --><ol>' )
-				.replaceAll( '</ol>', '</ol><!-- /wp:list -->' )
-				// Add Gutenberg comments to <ul> tags
-				.replaceAll( '<ul>', '<!-- wp:list --><ul>' )
-				.replaceAll( '</ul>', '</ul><!-- /wp:list -->' )
 		);
 	},
 	paragraph: ( content: string ) => {
