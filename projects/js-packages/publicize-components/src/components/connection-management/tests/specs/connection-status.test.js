dab694666fa3d5ab7489d9ddb025e39766433bbc
Gergely Juhasz <gergely.juhasz@automattic.com>
2024-05-27 10:57:27 +0200

    Match connectionStatus component changes

--- projects/js-packages/publicize-components/src/components/connection-management/tests/specs/connection-status.test.js
+++ projects/js-packages/publicize-components/src/components/connection-management/tests/specs/connection-status.test.js
@@ -7,15 +7,9 @@ jest.mock( '../../reconnect', () => ( {
 } ) );
 
 describe( 'ConnectionStatus', () => {
-	const mockConnection = {
-		status: 'error',
-	};
-
 	const mockService = {
 		name: 'mockService',
 	};
-
-	const mockOnConfirmReconnect = jest.fn();
 
 	test( 'renders nothing when connection status is "ok"', () => {
 		render( <ConnectionStatus connection={ { status: 'ok' } } service={ mockService } /> );
@@ -29,24 +23,5 @@ describe( 'ConnectionStatus', () => {
 		expect(
 			screen.queryByText( 'There is an issue with this connection.' )
 		).not.toBeInTheDocument();
-	} );
-
-	test( 'renders the appropriate notice and Reconnect component when connection status is not "ok" or undefined', () => {
-		render(
-			<ConnectionStatus
-				connection={ mockConnection }
-				service={ mockService }
-				onConfirmReconnect={ mockOnConfirmReconnect }
-			/>
-		);
-		expect( screen.getByText( 'There is an issue with this connection.' ) ).toBeInTheDocument();
-		expect( screen.getByText( 'Reconnect Component' ) ).toBeInTheDocument();
-	} );
-
-	test( 'renders the correct notice when connection status is "refresh-failed"', () => {
-		render(
-			<ConnectionStatus connection={ { status: 'refresh-failed' } } service={ mockService } />
-		);
-		expect( screen.getByText( 'The connection seems to have expired.' ) ).toBeInTheDocument();
 	} );
 } );
