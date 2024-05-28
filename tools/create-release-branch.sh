7777429347b029a84b525de23d434355c9976ae3
Brad Jorsch <brad.jorsch@automattic.com>
2024-05-28 11:02:08 -0400

    tools: Default some prompts to "yes"
    Some of the prompts, such as the one about pushing the release branch
    after creating it, really should default to "yes" so if someone is just
    hitting enter they do the thing that's right most of the time.
    
    So, add an option to the `proceed_p` shell function to specify that the
    default should be "yes", and use that in various places where it seems
    to make sense.

--- tools/create-release-branch.sh
+++ tools/create-release-branch.sh
@@ -89,7 +89,7 @@ else
 	if [[ "${PREFIXES[$PREFIX]}" == *$'\n'* ]]; then
 		info "Plugin $PLUGIN_NAME uses prefix $PREFIX, which is used in multiple plugins:"
 		echo "   ${PREFIXES[$PREFIX]//$'\n'/$'\n   '}"
-		proceed_p '' 'Release all these plugins?'
+		proceed_p '' 'Release all these plugins?' true
 		mapfile -t DIRS <<<"${PREFIXES[$PREFIX]}"
 	fi
 fi
@@ -116,7 +116,7 @@ git fetch
 TMP=$(git rev-parse --abbrev-ref HEAD)
 if [[ "$TMP" != "prerelease" ]]; then
 	# shellcheck disable=SC2310
-	if proceed_p "Current branch is $TMP." "Check out prerelease branch?"; then
+	if proceed_p "Current branch is $TMP." "Check out prerelease branch?" true; then
 		git checkout prerelease
 	else
 		proceed_p " " "Continue anyway?"
@@ -128,7 +128,7 @@ if [[ -n "$COMMITS" ]]; then
 	info "The current branch is behind $TMP."
 	echo "$COMMITS"
 	# shellcheck disable=SC2310
-	if proceed_p "" "Pull?"; then
+	if proceed_p "" "Pull?" true; then
 		git pull
 	else
 		proceed_p " " "Continue anyway?"
@@ -225,7 +225,7 @@ fi
 if isnotempty git ls-remote --heads origin "$BRANCH"; then
 	die "Release branch $BRANCH has already been pushed. Aborting."
 elif isnotempty git branch --list "$BRANCH"; then
-	proceed_p "Release branch $BRANCH already exists locally, but has not been pushed." "Delete it?"
+	proceed_p "Release branch $BRANCH already exists locally, but has not been pushed." "Delete it?" true
 	git branch -D "$BRANCH"
 fi
 
@@ -263,10 +263,10 @@ EOM
 PUSH_MSG=" after you push"
 if $INTERACTIVE; then
 	# shellcheck disable=SC2310
-	if proceed_p "" "Check changes and push?"; then
+	if proceed_p "" "Check changes and push?" true; then
 		git log -p "$BASE_REF".."$BRANCH" || true
 		# shellcheck disable=SC2310
-		if proceed_p "" "Push it now?"; then
+		if proceed_p "" "Push it now?" true; then
 			git push -u origin "$BRANCH"
 			PUSH_MSG=
 		fi
--- tools/includes/proceed_p.sh
+++ tools/includes/proceed_p.sh
@@ -12,26 +12,36 @@ fi
 # Args:
 #  1: The situation that requires prompting.
 #  2: Optional text to replace "Proceed?" as the question.
+#  3: true or false (default false); set true to default to "Y".
 #
 # Returns success if "yes", failure if "no" or non-interactive.
+# Note non-interactive always aborts even if the default is "Y".
 function proceed_p {
 	if ! $INTERACTIVE; then
 		error "$1 Aborting"
 		return 42
 	fi
-	local OK
+	local OK YN DEF
+
+	if ${3:-false}; then
+		YN="Y/n"
+		DEF=Y
+	else
+		YN="y/N"
+		DEF=N
+	fi
 
 	# Clear input before prompting
 	while read -r -t 0 OK; do read -r OK; done
 
 	local PROMPT
 	[[ -n "$1" ]] && PROMPT="$1 "
-	PROMPT="${PROMPT}${2:-Proceed?} [y/N] "
+	PROMPT="${PROMPT}${2:-Proceed?} [$YN] "
 	if color_supported; then
 		PROMPT=$(FORCE_COLOR=1 prompt "$PROMPT")
 	fi
 
-	read -n 1 -p "$PROMPT" OK
-	echo ""
-	[[ "$OK" == "y" || "$OK" == "Y" ]]
+	read -s -n 1 -p "$PROMPT" OK
+	echo "${OK:-$DEF}"
+	[[ "${OK:-$DEF}" == "y" || "${OK:-$DEF}" == "Y" ]]
 }
