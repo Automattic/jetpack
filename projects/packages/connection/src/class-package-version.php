1e6fac8269af84a77e4f5521a1f933aae2aa29a8
Jeremy Herve <jeremy@jeremy.hu>
2024-05-28 14:15:08 +0200

    Changelog and readme.txt edits.

--- projects/packages/connection/src/class-package-version.php
+++ projects/packages/connection/src/class-package-version.php
@@ -12,7 +12,7 @@ namespace Automattic\Jetpack\Connection;
  */
 class Package_Version {
 
-	const PACKAGE_VERSION = '2.8.6-alpha';
+	const PACKAGE_VERSION = '2.8.6';
 
 	const PACKAGE_SLUG = 'connection';
 
