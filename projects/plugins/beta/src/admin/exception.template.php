1b8c20ed1bb505ee802b9da1723d4539aed0ad0e
Brad Jorsch <brad.jorsch@automattic.com>
2024-05-29 11:45:48 -0400

    Clarify "--" (U+002D, ASCII hyphen-minus) vs "−−" (U+2212, minus sign) in comment

--- projects/plugins/beta/src/admin/exception.template.php
+++ projects/plugins/beta/src/admin/exception.template.php
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<p><?php echo esc_html( $exception->getMessage() ); ?></p>
 	<!--
 <?php
-// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- We're printing into a comment, and escaping any "--" so it should be impossible to end the comment early.
+// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- We're printing into a comment, and escaping any "--" (U+002D, ASCII hyphen-minus) to "−−" (U+2212, minus sign) to prevent ending the comment early.
 echo str_replace( '--', '−−', $exception->__toString() );
 ?>
 	-->
