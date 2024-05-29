846d66b1447e89d8782554ef9e1bb907234b5242
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2024-05-29 18:09:45 -0300

    AI Inline Extensions: Handle Jetpack Children Form

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/inline-extensions/jetpack-form/children.tsx
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/inline-extensions/jetpack-form/children.tsx
@@ -1,7 +1,25 @@
+import { select, dispatch } from '@wordpress/data';
 import { BlockHandler } from '../block-handler';
-
+import { BlockEditorDispatch } from '../types';
 export class JetpackChildrenFormHandler extends BlockHandler {
 	constructor( clientId: string ) {
-		super( clientId, [], 'action' );
+		super( clientId, [], null );
+		this.behavior = this.handleBehavior;
 	}
+
+	handleBehavior = ( { onAskAiAssistant } ) => {
+		const blockEditorDispatch = dispatch( 'core/block-editor' ) as BlockEditorDispatch;
+
+		const { getBlockParentsByBlockName } = select( 'core/block-editor' ) as unknown as {
+			getBlockParentsByBlockName: ( clientId: string, blockName: string ) => string[];
+		};
+
+		const jetpackFormClientId = getBlockParentsByBlockName(
+			this.clientId,
+			'jetpack/contact-form'
+		)?.[ 0 ];
+
+		blockEditorDispatch.selectBlock( jetpackFormClientId );
+		onAskAiAssistant();
+	};
 }
