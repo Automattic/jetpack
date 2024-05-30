e91180138ee5ea1459c5959e4fdff5ceab6d874b
Gergely Juhasz <gergely.juhasz@automattic.com>
2024-05-30 14:08:19 +0200

    Update the editor nudges

--- projects/js-packages/publicize-components/src/components/form/share-count-info.tsx
+++ projects/js-packages/publicize-components/src/components/form/share-count-info.tsx
@@ -12,11 +12,18 @@ import styles from './styles.module.scss';
 import { useAutoSaveAndRedirect } from './use-auto-save-and-redirect';
 
 export const ShareCountInfo: React.FC = () => {
-	const showShareLimits = useSelect( select => select( socialStore ).showShareLimits(), [] );
+	const { showShareLimits, hasPaidFeatures } = useSelect( select => {
+		const store = select( socialStore );
+		return {
+			showShareLimits: store.showShareLimits(),
+			hasPaidFeatures: store.hasPaidFeatures(),
+		};
+	}, [] );
+
 	const { noticeType, usedCount, scheduledCount, remainingCount } = useShareLimits();
 	const autosaveAndRedirect = useAutoSaveAndRedirect();
 
-	if ( ! showShareLimits ) {
+	if ( ! showShareLimits && hasPaidFeatures ) {
 		return null;
 	}
 
@@ -24,28 +31,33 @@ export const ShareCountInfo: React.FC = () => {
 		<PanelRow>
 			<div className={ styles[ 'share-count-info' ] }>
 				<ThemeProvider>
-					<div className={ styles[ 'title-container' ] }>
-						<Text variant="body-extra-small" className={ styles[ 'auto-share-title' ] }>
-							{ __( 'Auto-shares this cycle', 'jetpack' ) }
-						</Text>
-						<IconTooltip inline={ false } shift iconSize={ 16 } placement="top-end">
-							<Text variant="body-small">
-								{ __(
-									'As a free Jetpack Social user, you get 30 shares within every rolling 30-day window.',
-									'jetpack'
-								) }
-							</Text>
-						</IconTooltip>
-					</div>
-					<ShareCountNotice />
-					<ShareLimitsBar
-						usedCount={ usedCount }
-						scheduledCount={ scheduledCount }
-						remainingCount={ remainingCount }
-						className={ styles[ 'bar-wrapper' ] }
-						noticeType={ noticeType }
-					/>
-					{ noticeType === 'default' ? (
+					{ showShareLimits ? (
+						<>
+							<div className={ styles[ 'title-container' ] }>
+								<Text variant="body-extra-small" className={ styles[ 'auto-share-title' ] }>
+									{ __( 'Auto-shares this cycle', 'jetpack' ) }
+								</Text>
+								<IconTooltip inline={ false } shift iconSize={ 16 } placement="top-end">
+									<Text variant="body-small">
+										{ __(
+											'As a free Jetpack Social user, you get 30 shares within every rolling 30-day window.',
+											'jetpack'
+										) }
+									</Text>
+								</IconTooltip>
+							</div>
+
+							<ShareCountNotice />
+							<ShareLimitsBar
+								usedCount={ usedCount }
+								scheduledCount={ scheduledCount }
+								remainingCount={ remainingCount }
+								className={ styles[ 'bar-wrapper' ] }
+								noticeType={ noticeType }
+							/>
+						</>
+					) : null }
+					{ noticeType === 'default' && ! hasPaidFeatures ? (
 						<Button
 							key="upgrade"
 							variant="link"
@@ -55,7 +67,11 @@ export const ShareCountInfo: React.FC = () => {
 								query: 'redirect_to=' + encodeURIComponent( window.location.href ),
 							} ) }
 						>
-							{ _x( 'Upgrade to share more.', 'Call to action to buy a new plan', 'jetpack' ) }
+							{ _x(
+								'Upgrade for advanced sharing.',
+								'Call to action to buy a new plan',
+								'jetpack'
+							) }
 						</Button>
 					) : null }
 				</ThemeProvider>
