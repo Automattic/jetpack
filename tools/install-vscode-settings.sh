9b53d3be9af252c8c77ee20749ad253c5c848c7d
tbradsha <32492176+tbradsha@users.noreply.github.com>
2024-06-03 12:05:11 -0600

    Add simple VS Code symlink script

---
+++ tools/install-vscode-settings.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# This script allows a user to install repo-recommended VS Code settings.
+# See: p1HpG7-sQE-p2
+#
+# Exit codes:
+#   0: all is well
+#   1: Repo settings template is missing
+#   2: Symlink creation failed
+
+# Go to monorepo root.
+cd $(dirname "${BASH_SOURCE[0]}")/..
+
+# Abort if settings file already exists.
+if [[ -f .vscode/settings.json ]]; then
+	echo "Settings file exists; aborting."
+	exit
+fi
+
+# Abort if repo settings file is missing.
+if [[ ! -f .vscode/settings.dist.json ]]; then
+	echo "Repo settings template is missing; aborting."
+	exit 1
+fi
+
+# Create symlink.
+ln -s settings.dist.json .vscode/settings.json
+
+# Verify success.
+if [[ ! -f .vscode/settings.json ]]; then
+	echo "Failed to create symlink!"
+	exit 2
+fi
+
+echo "Created VS Code settings symlink: .vscode/settings.json"
