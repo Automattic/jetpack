5f0f770103c3ebf67d7bc05862604e1109ed9b88
Peter Petrov <peter.petrov89@gmail.com>
2024-06-04 15:45:18 +0300

    Only init available sub modules

--- projects/plugins/boost/app/modules/Modules_Setup.php
+++ projects/plugins/boost/app/modules/Modules_Setup.php
@@ -98,6 +98,10 @@ class Modules_Setup implements Has_Setup {
 	}
 
 	private function init_modules( $modules ) {
+		if ( empty( $modules ) ) {
+			return;
+		}
+
 		foreach ( $modules as $slug => $module ) {
 
 			$this->register_always_available_endpoints( $module->feature );
@@ -112,7 +116,9 @@ class Modules_Setup implements Has_Setup {
 			if ( $submodules ) {
 				$submodules_instances = array();
 				foreach ( $submodules as $sub_module ) {
-					$submodules_instances[] = new Module( new $sub_module() );
+					if ( $sub_module::is_available() ) {
+						$submodules_instances[] = new Module( new $sub_module() );
+					}
 				}
 				$this->init_modules( $submodules_instances );
 			}
