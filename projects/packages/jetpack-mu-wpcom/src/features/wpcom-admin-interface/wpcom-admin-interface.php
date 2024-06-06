7bcc6798adfb8c8a9b348c866697fc58782a3748
Miguel <miguel.san.segundo@automattic.com>
2024-06-06 18:49:57 +0700

    Hide the Hosting menus hosting-config and site-monitoring

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-admin-interface/wpcom-admin-interface.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-admin-interface/wpcom-admin-interface.php
@@ -254,3 +254,26 @@ function wpcom_show_admin_interface_notice() {
 	);
 }
 add_action( 'admin_notices', 'wpcom_show_admin_interface_notice' );
+
+/**
+ * Hides the Hosting menus hosting-config and site-monitoring because they already exist in the site management panel.
+ */
+function hide_hosting_menus_already_in_global_site_view() {
+	global $submenu;
+
+	if ( ! wpcom_is_nav_redesign_enabled() ) {
+		return;
+	}
+
+	$items_to_hide = array( 'hosting-config', 'site-monitoring' );
+
+	foreach ( $submenu['wpcom-hosting-menu'] as &$menu_item ) {
+		foreach ( $items_to_hide  as $item_to_hide ) {
+			if ( str_starts_with( $menu_item[2], 'https://wordpress.com/' . $item_to_hide ) ) {
+				remove_submenu_page( 'wpcom-hosting-menu', $menu_item[2] );
+				break;
+			}
+		}
+	}
+}
+add_action( 'admin_menu', 'hide_hosting_menus_already_in_global_site_view', PHP_INT_MAX );
