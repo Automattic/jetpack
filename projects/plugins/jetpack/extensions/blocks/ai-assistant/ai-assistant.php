e00257684f471bb6e5e86f81013adc855e4a9406
Luiz Kowalski <lhkowalski@gmail.com>
2024-06-06 17:05:24 -0300

    Register the ai-general-purpose-image-generator flag and set it as beta

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/ai-assistant.php
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/ai-assistant.php
@@ -173,3 +173,15 @@ add_action(
 		}
 	}
 );
+
+/**
+ * Register the `ai-general-purpose-image-generator` extension.
+ */
+add_action(
+	'jetpack_register_gutenberg_extensions',
+	function () {
+		if ( apply_filters( 'jetpack_ai_enabled', true ) ) {
+			\Jetpack_Gutenberg::set_extension_available( 'ai-general-purpose-image-generator' );
+		}
+	}
+);
