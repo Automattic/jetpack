4ae116c1966d80db2db15fc93b431b0d3b876c27
Miguel San Segundo <1881481+miksansegundo@users.noreply.github.com>
2024-06-07 09:56:23 +0700

    Fix changing to English

--- projects/packages/jetpack-mu-wpcom/src/features/calypso-locale-sync/sync-wp-admin-to-calypso.php
+++ projects/packages/jetpack-mu-wpcom/src/features/calypso-locale-sync/sync-wp-admin-to-calypso.php
@@ -35,7 +35,7 @@ function sync_wp_admin_locale_on_profile_update() {
 	$user_id = get_current_user_id();
 	// phpcs:ignore WordPress.Security.NonceVerification.Missing
 	$locale = isset( $_POST['locale'] ) ? sanitize_text_field( wp_unslash( empty( $_POST['locale'] ) ? 'en' : $_POST['locale'] ) ) : null;
-	if ( ! $user_id || empty( $locale ) ) {
+	if ( ! $user_id || ! isset( $locale ) ) {
 		return;
 	}
 
