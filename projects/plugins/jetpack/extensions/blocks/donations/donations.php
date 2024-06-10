53ec46b80e59449119a054572364c762372363b4
Mikael Korpela <mikael@ihminen.org>
2024-06-10 11:23:34 +0300

    Add spinning animation to donate button

--- projects/plugins/jetpack/extensions/blocks/donations/donations.php
+++ projects/plugins/jetpack/extensions/blocks/donations/donations.php
@@ -130,17 +130,31 @@ function render_block( $attr, $content ) {
 				esc_html( \Jetpack_Currencies::format_price( $amount, $currency ) )
 			);
 		}
-		$amounts    .= '</div>';
-		$extra_text .= sprintf(
+		$amounts        .= '</div>';
+		$extra_text     .= sprintf(
 			'<p class="%1$s">%2$s</p>',
 			esc_attr( $donation['class'] ),
 			wp_kses_post( $donation['extraText'] ?? $default_texts['extraText'] )
 		);
-		$buttons    .= sprintf(
+		$loading_spinner = '<span class="jetpack-donations-spinner">
+			<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+				<path
+					d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
+					opacity=".25"
+					fill="currentColor"
+				/>
+				<path
+					d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
+					class="jetpack-donations-spinner-rotating"
+					fill="currentColor"
+				/>
+			</svg>
+		</span>';
+		$buttons        .= sprintf(
 			'<a class="wp-block-button__link donations__donate-button %1$s" href="%2$s">%3$s</a>',
 			esc_attr( $donation['class'] ),
 			esc_url( \Jetpack_Memberships::get_instance()->get_subscription_url( $plan_id ) ),
-			wp_kses_post( $donation['buttonText'] )
+			wp_kses_post( $donation['buttonText'] ) . $loading_spinner
 		);
 	}
 	if ( $nav ) {
