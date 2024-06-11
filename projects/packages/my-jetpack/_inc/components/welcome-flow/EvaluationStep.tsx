79450564222e9741a687a735fcebfec0d143b4fc
robertsreberski <robert.sreberski@automattic.com>
2024-06-11 11:55:19 +0200

    Add evaluation form step

---
+++ projects/packages/my-jetpack/_inc/components/welcome-flow/EvaluationStep.tsx
@@ -0,0 +1,93 @@
+import { Col, Button, Text } from '@automattic/jetpack-components';
+import { CheckboxControl } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import classNames from 'classnames';
+import React, { useCallback, useState } from 'react';
+import styles from './style.module.scss';
+
+export type EvaluationAreas = 'protect' | 'performance' | 'audience' | 'content' | 'unsure';
+
+const questions: { [ key in EvaluationAreas ]: string } = {
+	protect: __( 'Protect my site', 'jetpack-my-jetpack' ),
+	performance: __( "Improve my site's performance", 'jetpack-my-jetpack' ),
+	audience: __( 'Grow my audience', 'jetpack-my-jetpack' ),
+	content: __( 'Create quality content', 'jetpack-my-jetpack' ),
+	unsure: __( "I don't know", 'jetpack-my-jetpack' ),
+};
+
+const defaultValues: { [ key in EvaluationAreas ]: boolean } = {
+	protect: false,
+	performance: false,
+	audience: false,
+	content: false,
+	unsure: false,
+};
+
+type EvaluationStepProps = {
+	onSkipOnboarding: () => void;
+	onSubmitEvaluation: ( values: { [ key in EvaluationAreas ]: boolean } ) => void;
+};
+
+const EvaluationStep = ( {
+	onSkipOnboarding,
+	onSubmitEvaluation: onSubmitAnswers,
+}: EvaluationStepProps ) => {
+	const [ values, setValues ] = useState( defaultValues );
+	const setChecked = useCallback(
+		( key: EvaluationAreas ) => ( value: boolean ) => {
+			setValues( prevValues => ( { ...prevValues, [ key ]: value } ) );
+		},
+		[ setValues ]
+	);
+
+	const handleSubmit = useCallback( () => onSubmitAnswers( values ), [ onSubmitAnswers, values ] );
+
+	const isSelected = Object.values( values ).some( v => v );
+
+	return (
+		<>
+			<Col sm={ 6 } md={ 8 } lg={ 6 } className={ styles[ 'banner-description' ] }>
+				<Text variant="headline-small" mb={ 3 }>
+					{ __( 'What is your goal?', 'jetpack-my-jetpack' ) }
+				</Text>
+				<Text variant="body" mb={ 2 }>
+					{ __( 'Let us customise your Jetpack experience', 'jetpack-my-jetpack' ) }
+				</Text>
+				<form className={ styles.form }>
+					{ Object.entries( questions ).map( ( [ key, question ]: [ EvaluationAreas, string ] ) => (
+						<CheckboxControl
+							className={ styles[ 'form-checkbox' ] }
+							label={ question }
+							checked={ values[ key ] }
+							onChange={ setChecked( key ) }
+						/>
+					) ) }
+					<div className={ styles[ 'form-actions' ] }>
+						<Button
+							variant="primary"
+							text={ __( 'See solutions', 'jetpack-my-jetpack' ) }
+							onClick={ handleSubmit }
+							disabled={ ! isSelected }
+						/>
+						<Button
+							variant="link"
+							className={ styles[ 'form-link-action' ] }
+							text={ __( 'Skip onboarding', 'jetpack-my-jetpack' ) }
+							onClick={ onSkipOnboarding }
+						/>
+					</div>
+				</form>
+			</Col>
+			<Col
+				sm={ 6 }
+				md={ 8 }
+				lg={ 6 }
+				className={ classNames( styles[ 'banner-image-evaluation' ], {
+					[ styles[ 'is-selected' ] ]: isSelected,
+				} ) }
+			></Col>
+		</>
+	);
+};
+
+export default EvaluationStep;
