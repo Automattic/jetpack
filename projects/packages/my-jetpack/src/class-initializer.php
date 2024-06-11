27f19fca5cbd1ee97f208afaa89d89deb93c30d7
Igor Zinovyev <zinigor@gmail.com>
2024-06-11 15:41:18 +0300

    Backport mu-wpcom-plugin 2.1.31, jetpack 13.6-a.1 Changes (#37778)
    * Changelog and readme.txt edits.
    
    * Init new cycle

--- projects/packages/my-jetpack/src/class-initializer.php
+++ projects/packages/my-jetpack/src/class-initializer.php
@@ -37,7 +37,7 @@ class Initializer {
 	 *
 	 * @var string
 	 */
-	const PACKAGE_VERSION = '4.24.5-alpha';
+	const PACKAGE_VERSION = '4.24.6-alpha';
 
 	/**
 	 * HTML container ID for the IDC screen on My Jetpack page.
