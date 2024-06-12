11de350a9156b4e6a488a4d03f03e83e0d6f9b57
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2024-06-12 11:56:49 -0600

    Update tests for Connection Status Card

--- projects/packages/my-jetpack/_inc/components/connection-status-card/test/component.jsx
+++ projects/packages/my-jetpack/_inc/components/connection-status-card/test/component.jsx
@@ -2,8 +2,48 @@ import { CONNECTION_STORE_ID } from '@automattic/jetpack-connection';
 import { jest } from '@jest/globals';
 import { render, renderHook, screen } from '@testing-library/react';
 import { useSelect } from '@wordpress/data';
-import React from 'react';
 import ConnectionStatusCard from '../index';
+
+const resetInitialState = () => {
+	global.window.myJetpackInitialState = {
+		lifecycleStats: {
+			historicallyActiveModules: [],
+			brokenModules: {
+				needs_site_connection: [],
+				needs_user_connection: [],
+			},
+		},
+		products: {
+			items: {
+				'anti-spam': {
+					requires_user_connection: false,
+					status: 'inactive',
+					pricingForUi: {
+						productTerm: 'year',
+					},
+				},
+			},
+		},
+	};
+};
+
+const setConnectionStore = ( {
+	isRegistered = false,
+	isUserConnected = false,
+	hasConnectedOwner = false,
+} = {} ) => {
+	let storeSelect;
+	renderHook( () => useSelect( select => ( storeSelect = select( CONNECTION_STORE_ID ) ) ) );
+	jest
+		.spyOn( storeSelect, 'getConnectionStatus' )
+		.mockReset()
+		.mockReturnValue( { isRegistered, isUserConnected, hasConnectedOwner } );
+};
+
+beforeEach( () => {
+	resetInitialState();
+	setConnectionStore();
+} );
 
 // TODO Mock requests with dummy data.
 describe( 'ConnectionStatusCard', () => {
@@ -13,108 +53,135 @@ describe( 'ConnectionStatusCard', () => {
 		redirectUri: 'https://example.org',
 	};
 
-	describe( 'When the user has not connected their WordPress.com account', () => {
+	describe( 'When the site is not registered and has no broken modules', () => {
 		const setup = () => {
-			let storeSelect;
-			renderHook( () => useSelect( select => ( storeSelect = select( CONNECTION_STORE_ID ) ) ) );
-			jest
-				.spyOn( storeSelect, 'getConnectionStatus' )
-				.mockReset()
-				.mockReturnValue( { isRegistered: true, isUserConnected: false } );
 			return render( <ConnectionStatusCard { ...testProps } /> );
 		};
 
-		it( 'renders the "Site connected" success list item', () => {
+		it( 'renders the correct copy for the site connection line item', () => {
 			setup();
-			expect( screen.getByText( 'Site connected.' ) ).toBeInTheDocument();
+			expect( screen.getByText( 'Start with Jetpack.' ) ).toBeInTheDocument();
+			expect(
+				screen.getByRole( 'button', { name: 'Connect your site with one click' } )
+			).toBeInTheDocument();
 		} );
 
-		it( 'Does not render the "Manage" button', () => {
+		it( 'does not render the user connection line item', () => {
 			setup();
-			expect( screen.queryByRole( 'button', { name: 'Manage' } ) ).not.toBeInTheDocument();
-		} );
-
-		it( 'renders the "User account not connected" error list item', () => {
-			setup();
-			expect( screen.getByText( 'User account not connected.' ) ).toBeInTheDocument();
-		} );
-
-		it( 'renders the "Connect your user account" button', () => {
-			setup();
-			expect( screen.getByRole( 'button', { name: 'Connect' } ) ).toBeInTheDocument();
+			expect( screen.queryByText( 'Unlock more of Jetpack' ) ).not.toBeInTheDocument();
+			expect( screen.queryByRole( 'button', { name: 'Sign in' } ) ).not.toBeInTheDocument();
 		} );
 	} );
 
-	describe( "When the user has not connected their WordPress.com account but the site has an owner and we don't need a user connection", () => {
+	describe( 'When the site is not registered and has broken modules', () => {
 		const setup = () => {
-			let storeSelect;
-			renderHook( () => useSelect( select => ( storeSelect = select( CONNECTION_STORE_ID ) ) ) );
-			jest.spyOn( storeSelect, 'getConnectionStatus' ).mockReset().mockReturnValue( {
-				isRegistered: true,
-				isUserConnected: false,
-				hasConnectedOwner: true,
-			} );
-			return render( <ConnectionStatusCard { ...testProps } requiresUserConnection={ false } /> );
+			global.window.myJetpackInitialState.lifecycleStats.brokenModules.needs_site_connection = [
+				'module1',
+			];
+			return render( <ConnectionStatusCard { ...testProps } /> );
 		};
 
-		it( 'renders the "Site connected" success list item', () => {
+		it( 'renders the correct copy for the site connection line item', () => {
 			setup();
-			expect( screen.getByText( 'Site connected.' ) ).toBeInTheDocument();
-		} );
-
-		it( 'Does not render the "Manage" button', () => {
-			setup();
-			expect( screen.queryByRole( 'button', { name: 'Manage' } ) ).not.toBeInTheDocument();
-		} );
-
-		it( 'Render the "User account not connected" error list item', () => {
-			setup();
-			expect( screen.getByText( 'User account not connected.' ) ).toBeInTheDocument();
-		} );
-
-		it( 'renders the "Connect" button', () => {
-			setup();
+			expect(
+				screen.getByText( 'Missing site connection to enable some features.' )
+			).toBeInTheDocument();
 			expect( screen.getByRole( 'button', { name: 'Connect' } ) ).toBeInTheDocument();
 		} );
 
-		it.todo( 'Render the "Also connected:" error list item' );
+		it( 'does not render the user connection line item', () => {
+			setup();
+			expect( screen.queryByText( 'Unlock more of Jetpack' ) ).not.toBeInTheDocument();
+			expect( screen.queryByRole( 'button', { name: 'Sign in' } ) ).not.toBeInTheDocument();
+		} );
+	} );
+
+	describe( 'When the user has not connected their WordPress.com account and there are no broken modules', () => {
+		describe( 'There are no products that require user connection', () => {
+			const setup = () => {
+				setConnectionStore( { isRegistered: true } );
+				return render( <ConnectionStatusCard { ...testProps } /> );
+			};
+
+			it( 'renders the correct site connection line item', () => {
+				setup();
+				expect( screen.getByText( 'Site connected.' ) ).toBeInTheDocument();
+				expect( screen.getByRole( 'button', { name: 'Manage' } ) ).toBeInTheDocument();
+			} );
+
+			it( 'renders the correct user connection line item', () => {
+				setup();
+				expect( screen.getByText( 'Unlock more of Jetpack' ) ).toBeInTheDocument();
+				expect( screen.getByRole( 'button', { name: 'Sign in' } ) ).toBeInTheDocument();
+			} );
+		} );
+
+		describe( 'There are products that require user connection', () => {
+			const setup = () => {
+				setConnectionStore( { isRegistered: true } );
+				global.window.myJetpackInitialState.products.items[
+					'anti-spam'
+				].requires_user_connection = true;
+				return render( <ConnectionStatusCard { ...testProps } /> );
+			};
+
+			it( 'renders the correct site connection line item', () => {
+				setup();
+				expect( screen.getByText( 'Site connected.' ) ).toBeInTheDocument();
+				expect( screen.getByRole( 'button', { name: 'Manage' } ) ).toBeInTheDocument();
+			} );
+
+			it( 'renders the correct user connection line item', () => {
+				setup();
+				expect( screen.getByText( 'Some features require authentication.' ) ).toBeInTheDocument();
+				expect( screen.getByRole( 'button', { name: 'Sign in' } ) ).toBeInTheDocument();
+			} );
+		} );
+	} );
+
+	describe( 'When the user has not connected their WordPress.com account and there are broken modules', () => {
+		const setup = () => {
+			setConnectionStore( { isRegistered: true } );
+			global.window.myJetpackInitialState.lifecycleStats.brokenModules.needs_user_connection = [
+				'module1',
+			];
+			return render( <ConnectionStatusCard { ...testProps } /> );
+		};
+
+		it( 'renders the correct site connection line item', () => {
+			setup();
+			expect( screen.getByText( 'Site connected.' ) ).toBeInTheDocument();
+			expect( screen.getByRole( 'button', { name: 'Manage' } ) ).toBeInTheDocument();
+		} );
+
+		it( 'renders the correct user connection line item', () => {
+			setup();
+			expect(
+				screen.getByText( 'Missing authentication to enable all features.' )
+			).toBeInTheDocument();
+			expect( screen.getByRole( 'button', { name: 'Sign in' } ) ).toBeInTheDocument();
+		} );
 	} );
 
 	describe( 'When the user has connected their WordPress.com account', () => {
 		const setup = () => {
-			let storeSelect;
-			renderHook( () => useSelect( select => ( storeSelect = select( CONNECTION_STORE_ID ) ) ) );
-			jest.spyOn( storeSelect, 'getConnectionStatus' ).mockReset().mockReturnValue( {
-				isRegistered: true,
-				isUserConnected: true,
-				hasConnectedOwner: true,
-			} );
+			setConnectionStore( { isRegistered: true, isUserConnected: true, hasConnectedOwner: true } );
 			return render( <ConnectionStatusCard { ...testProps } /> );
 		};
 
-		it( 'renders the "Site connected" success list item', () => {
+		it( 'renders the correct site connection line item', () => {
 			setup();
 			expect( screen.getByText( 'Site connected.' ) ).toBeInTheDocument();
 		} );
 
-		it( 'renders the "Manage" buttons', () => {
-			setup();
-			expect( screen.getByRole( 'button', { name: 'Manage' } ) ).toBeInTheDocument();
-		} );
-
-		it( 'renders the "Logged in as" success list item', () => {
+		it( 'renders the correct user connection line item', () => {
 			setup();
 			expect( screen.getByText( /Connected as/ ) ).toBeInTheDocument();
 		} );
 
-		it( 'Doesn\'t render the "Requires user connection" error list item', () => {
+		it( 'renders two manage buttons', () => {
 			setup();
-			expect( screen.queryByText( 'Requires user connection.' ) ).not.toBeInTheDocument();
-		} );
-
-		it( 'doesn\'t render the "Connect your WordPress.com account" button', () => {
-			setup();
-			expect( screen.queryByRole( 'button', { name: 'Connect' } ) ).not.toBeInTheDocument();
+			expect( screen.getAllByRole( 'button', { name: 'Manage' } ) ).toHaveLength( 2 );
 		} );
 	} );
 } );
