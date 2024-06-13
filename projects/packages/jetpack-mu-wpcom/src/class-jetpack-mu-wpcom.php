07102ab3401aec93345352c9879ffa0bb57ebc53
DustyReagan <dusty@dustyreagan.com>
2024-06-13 17:58:09 -0500

    Run tools/fixup-project-versions.sh

--- projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
+++ projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
@@ -13,7 +13,7 @@ namespace Automattic\Jetpack;
  * Jetpack_Mu_Wpcom main class.
  */
 class Jetpack_Mu_Wpcom {
-	const PACKAGE_VERSION = '5.35.1';
+	const PACKAGE_VERSION = '5.35.2-alpha';
 	const PKG_DIR         = __DIR__ . '/../';
 	const BASE_DIR        = __DIR__ . '/';
 	const BASE_FILE       = __FILE__;
