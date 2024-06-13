42960aa032f1b2b059f553a95e6a877e2b5fe713
Peter Petrov <peter.petrov89@gmail.com>
2024-06-13 13:53:04 +0300

    Add LIAR to pricing table

--- projects/plugins/boost/app/assets/src/js/features/boost-pricing-table/lib/features.tsx
+++ projects/plugins/boost/app/assets/src/js/features/boost-pricing-table/lib/features.tsx
@@ -184,6 +184,18 @@ export const boostFeatureList: FeatureItem[] = [
 	},
 	{
 		description: {
+			name: __( 'Image CDN Auto-Resize Lazy Images', 'jetpack-boost' ),
+			tooltipInfo: __(
+				'Optimizes lazy-loaded images by dynamically serving perfectly sized images for each device.',
+				'jetpack-boost'
+			),
+			tooltipPlacement: 'bottom-start',
+		},
+		free: <PricingTableItem isIncluded={ false } />,
+		premium: <PricingTableItem isIncluded={ true } label={ __( 'Included', 'jetpack-boost' ) } />,
+	},
+	{
+		description: {
 			name: __( 'Image CDN', 'jetpack-boost' ),
 			tooltipInfo: imageCdnContext,
 			tooltipPlacement: 'bottom-start',
