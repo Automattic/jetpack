bd72090fcfe698eade79203615e48e632a9bb663
tbradsha <32492176+tbradsha@users.noreply.github.com>
2024-06-13 11:14:34 -0600

    Use svgo plugin to add <g> instead of regex

--- projects/js-packages/social-logos/tools/svg-optimize.js
+++ projects/js-packages/social-logos/tools/svg-optimize.js
@@ -31,6 +31,41 @@ const svgo_config = {
 				attributes: [ { viewBox: '0 0 24 24' }, { xmlns: 'http://www.w3.org/2000/svg' } ],
 			},
 		},
+		{
+			name: 'addWrappingGroup',
+			description: 'We want each svg to have a `<g>` around its content.',
+			fn: () => ( {
+				element: {
+					enter( node, parentNode ) {
+						// Look for root-level `<svg>` that doesn't already have a single `<g>` as its child.
+						if (
+							node.name !== 'svg' ||
+							parentNode.type !== 'root' ||
+							( node.children.length === 1 &&
+								node.children[ 0 ].type === 'element' &&
+								node.children[ 0 ].name === 'g' )
+						) {
+							return;
+						}
+
+						// Apparently this is how you create a new node.
+						const g = {
+							type: 'element',
+							name: 'g',
+							attributes: {},
+							children: node.children,
+						};
+						// TODO remove legacy parentNode in v4
+						Object.defineProperty( g, 'parentNode', {
+							writable: true,
+							value: node,
+						} );
+
+						node.children = [ g ];
+					},
+				},
+			} ),
+		},
 	],
 };
 
@@ -44,7 +79,7 @@ fs.mkdirSync( destSvgDir, { recursive: true } );
 const srcFiles = glob.sync( srcSvgDir + '/*.svg' ).sort();
 for ( const srcFile of srcFiles ) {
 	const data = fs.readFileSync( srcFile, 'utf8' );
-	let optimizedData = optimize( data, {
+	const optimizedData = optimize( data, {
 		path: srcFile, // recommended by library for use by its plugins
 		...svgo_config,
 	} )?.data;
@@ -54,8 +89,6 @@ for ( const srcFile of srcFiles ) {
 	}
 	const destFile = destSvgDir + '/' + path.basename( srcFile );
 
-	// Add <g> to SVG innards.
-	optimizedData = optimizedData.replace( /(>)(.*)(<\/svg>)/, '$1<g>$2</g>$3' );
 	fs.writeFileSync( destFile, optimizedData );
 }
 
