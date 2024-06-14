4f7bae937ca948b5953b46313a8d220dbd0addfc
Dusty Reagan <dusty@dustyreagan.com>
2024-06-14 06:52:34 -0500

    Profile Behavior: Remove bi-directional syncing (#37862)
    * Remove bi-directional syncing file loading
    
    * changelog
    
    * Delete syncing files and folder
    
    * Run tools/fixup-project-versions.sh
    
    * Revert "Delete syncing files and folder"
    
    This reverts commit 3b070918605065231c9284d3521f03ab19f22fcf.
    
    * Bump version to 5.35.3-alpha
    
    ---------
    
    Co-authored-by: okmttdhr <okmttdhr@users.noreply.github.com>

--- projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
+++ projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
@@ -13,7 +13,7 @@ namespace Automattic\Jetpack;
  * Jetpack_Mu_Wpcom main class.
  */
 class Jetpack_Mu_Wpcom {
-	const PACKAGE_VERSION = '5.35.2';
+	const PACKAGE_VERSION = '5.35.3-alpha';
 	const PKG_DIR         = __DIR__ . '/../';
 	const BASE_DIR        = __DIR__ . '/';
 	const BASE_FILE       = __FILE__;
@@ -91,8 +91,6 @@ class Jetpack_Mu_Wpcom {
 		require_once __DIR__ . '/features/wpcom-block-editor/functions.editor-type.php';
 		require_once __DIR__ . '/features/wpcom-site-menu/wpcom-site-menu.php';
 		require_once __DIR__ . '/features/wpcom-themes/wpcom-themes.php';
-		require_once __DIR__ . '/features/calypso-locale-sync/sync-wp-admin-to-calypso.php';
-		require_once __DIR__ . '/features/calypso-locale-sync/sync-calypso-to-wp-admin.php';
 
 		// Initializers, if needed.
 		\Marketplace_Products_Updater::init();
