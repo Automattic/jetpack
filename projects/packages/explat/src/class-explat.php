109431d4b5fae5918515a36a15a305ff3854f822
Ian Ramos <5714212+IanRamosC@users.noreply.github.com>
2024-06-17 10:34:31 -0300

    Add ExPlat package code

---
+++ projects/packages/explat/src/class-explat.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Package description here
+ *
+ * @package automattic/jetpack-explat
+ */
+
+namespace Automattic\Jetpack;
+
+/**
+ * Class description.
+ */
+class Explat {
+
+	const PACKAGE_VERSION = '1.0.0-alpha';
+}
