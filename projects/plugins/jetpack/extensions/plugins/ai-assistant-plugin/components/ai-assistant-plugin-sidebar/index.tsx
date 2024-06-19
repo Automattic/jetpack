b2c3ca65a57a6beca2b340ca3f70d0835bea74a5
Jeremy Herve <jeremy@jeremy.hu>
2024-06-19 14:28:36 +0200

    Site Editor: do not display Jetpack sidebar panels when not needed
    Related:
    
    - #37713 https://make.wordpress.org/core/2024/06/18/editor-unified-extensibility-apis-in-6-6/
    -

--- projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/ai-assistant-plugin-sidebar/index.tsx
+++ projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/ai-assistant-plugin-sidebar/index.tsx
@@ -4,7 +4,10 @@
 import { JetpackEditorPanelLogo } from '@automattic/jetpack-shared-extension-utils';
 import { useAnalytics } from '@automattic/jetpack-shared-extension-utils';
 import { PanelBody, PanelRow, BaseControl } from '@wordpress/components';
+import { store as coreStore } from '@wordpress/core-data';
+import { useSelect } from '@wordpress/data';
 import { PluginPrePublishPanel, PluginDocumentSettingPanel } from '@wordpress/edit-post';
+import { store as editorStore } from '@wordpress/editor';
 import { __ } from '@wordpress/i18n';
 import debugFactory from 'debug';
 import React from 'react';
@@ -25,6 +28,7 @@ import {
 	PLACEMENT_PRE_PUBLISH,
 } from './types';
 import Upgrade from './upgrade';
+import type * as EditorSelectors from '@wordpress/editor/store/selectors';
 
 import './style.scss';
 
@@ -89,6 +93,18 @@ export default function AiAssistantPluginSidebar() {
 	const { checkoutUrl } = useAICheckout();
 
 	const { tracks } = useAnalytics();
+
+	const isViewable = useSelect( select => {
+		const postTypeName = ( select( editorStore ) as typeof EditorSelectors ).getCurrentPostType();
+		const postTypeObject = select( coreStore ).getPostType( postTypeName );
+
+		return postTypeObject?.viewable;
+	}, [] );
+	// If the post type is not viewable, do not render my plugin.
+	if ( ! isViewable ) {
+		return null;
+	}
+
 	const title = __( 'AI Assistant', 'jetpack' );
 
 	const panelToggleTracker = placement => {
