7ac664ce9882d6ceae06713426b06a730ef504e9
Diogo Alves <diogoca@gmail.com>
2024-06-19 10:21:16 -0300

    Return a custom error when the posts are not synced (#37865)
    * Check if the Jetpack is sync when showing the posts
    
    * Add a changelog
    
    * Check if the post is synced instead of if the Full Sync is running
    
    * Minor fixes
    
    * Rename is_posts_synced to are_posts_ready to avoid detailing the context
    
    * Rely on Health status instead of checking the Full_Sync module
    
    * Update the error message

--- projects/packages/blaze/src/class-dashboard.php
+++ projects/packages/blaze/src/class-dashboard.php
@@ -21,7 +21,7 @@ class Dashboard {
 	 *
 	 * @var string
 	 */
-	const PACKAGE_VERSION = '0.21.7';
+	const PACKAGE_VERSION = '0.21.8-alpha';
 
 	/**
 	 * List of dependencies needed to render the dashboard in wp-admin.
