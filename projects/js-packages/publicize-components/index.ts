7c69616ad2eee85318813a6a05b7c3865117fbc3
Manzoor Wani <manzoorwani.jk@gmail.com>
2024-06-19 17:58:11 +0530

    Clean up unused code

--- projects/js-packages/publicize-components/index.ts
+++ projects/js-packages/publicize-components/index.ts
@@ -3,7 +3,6 @@
 //store for the components, but at the moment they're tied.
 import './src/social-store';
 
-export { default as Connection } from './src/components/connection';
 export { default as ConnectionVerify } from './src/components/connection-verify';
 export { default as Form } from './src/components/form';
 export { default as SocialPreviewsModal } from './src/components/social-previews/modal';
