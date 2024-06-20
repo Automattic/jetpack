870a5a1479f40a74ea7d04dfe5a73b24fdcc15a4
Manzoor Wani <manzoorwani.jk@gmail.com>
2024-06-20 12:08:28 +0530

    Add Manage Connections button

--- projects/js-packages/publicize-components/src/components/form/connections-list.tsx
+++ projects/js-packages/publicize-components/src/components/form/connections-list.tsx
@@ -1,7 +1,7 @@
 import usePublicizeConfig from '../../hooks/use-publicize-config';
 import useSocialMediaConnections from '../../hooks/use-social-media-connections';
 import PublicizeConnection from '../connection';
-import PublicizeSettingsButton from '../settings-button';
+import { SettingsButton } from './settings-button';
 import styles from './styles.module.scss';
 import { useConnectionState } from './use-connection-state';
 
@@ -13,29 +13,27 @@ export const ConnectionsList: React.FC = () => {
 	const { needsUserConnection } = usePublicizeConfig();
 
 	return (
-		<ul className={ styles[ 'connections-list' ] }>
-			{ connections.map( conn => {
-				const { display_name, id, service_name, profile_picture, connection_id } = conn;
-				const currentId = connection_id ? connection_id : id;
+		<div>
+			<ul className={ styles[ 'connections-list' ] }>
+				{ connections.map( conn => {
+					const { display_name, id, service_name, profile_picture, connection_id } = conn;
+					const currentId = connection_id ? connection_id : id;
 
-				return (
-					<PublicizeConnection
-						disabled={ shouldBeDisabled( conn ) }
-						enabled={ canBeTurnedOn( conn ) && conn.enabled }
-						key={ currentId }
-						id={ currentId }
-						label={ display_name }
-						name={ service_name }
-						toggleConnection={ toggleById }
-						profilePicture={ profile_picture }
-					/>
-				);
-			} ) }
-			{ ! needsUserConnection ? (
-				<li>
-					<PublicizeSettingsButton />
-				</li>
-			) : null }
-		</ul>
+					return (
+						<PublicizeConnection
+							disabled={ shouldBeDisabled( conn ) }
+							enabled={ canBeTurnedOn( conn ) && conn.enabled }
+							key={ currentId }
+							id={ currentId }
+							label={ display_name }
+							name={ service_name }
+							toggleConnection={ toggleById }
+							profilePicture={ profile_picture }
+						/>
+					);
+				} ) }
+			</ul>
+			{ ! needsUserConnection ? <SettingsButton /> : null }
+		</div>
 	);
 };
