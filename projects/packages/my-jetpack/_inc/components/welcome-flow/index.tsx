5fc5605bdd8ee23f992ae30a7b13138ae8888040
robertsreberski <robert.sreberski@automattic.com>
2024-06-21 10:15:33 +0200

    Code improvements

--- projects/packages/my-jetpack/_inc/components/welcome-flow/index.tsx
+++ projects/packages/my-jetpack/_inc/components/welcome-flow/index.tsx
@@ -1,21 +1,22 @@
 import { Container, Col, Button } from '@automattic/jetpack-components';
 import { __ } from '@wordpress/i18n';
 import { close } from '@wordpress/icons';
-import React, { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import useWelcomeBanner from '../../data/welcome-banner/use-welcome-banner';
 import useAnalytics from '../../hooks/use-analytics';
 import useMyJetpackConnection from '../../hooks/use-my-jetpack-connection';
 import { CardWrapper } from '../card';
 import ConnectionStep from './ConnectionStep';
 import styles from './style.module.scss';
+import type { FC } from 'react';
 
-const WelcomeFlow: React.FC = () => {
+const WelcomeFlow: FC = () => {
 	const { recordEvent } = useAnalytics();
 	const { isWelcomeBannerVisible, dismissWelcomeBanner } = useWelcomeBanner();
 	const { siteIsRegistered, siteIsRegistering, handleRegisterSite } = useMyJetpackConnection( {
 		skipUserConnection: true,
 	} );
-	const [ visible, setVisible ] = React.useState( isWelcomeBannerVisible );
+	const [ visible, setVisible ] = useState( isWelcomeBannerVisible );
 
 	const onDismissClick = useCallback( () => {
 		recordEvent( 'jetpack_myjetpack_welcome_banner_dismiss_click' );
@@ -56,6 +57,7 @@ const WelcomeFlow: React.FC = () => {
 					aria-label={ __( 'Don’t show the welcome message again', 'jetpack-my-jetpack' ) }
 					size="small"
 					icon={ close }
+					disabled={ siteIsRegistering }
 					onClick={ onDismissClick }
 				/>
 			</Col>
