51d8b5d2b8db8585589912b1131cbc0e4c549f52
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2024-06-22 09:32:39 -0600

    Add/default tracks to my jetpack events (#37974)

--- projects/packages/my-jetpack/_inc/hooks/use-analytics/index.ts
+++ projects/packages/my-jetpack/_inc/hooks/use-analytics/index.ts
@@ -9,7 +9,12 @@ type TracksRecordEvent = (
 ) => void;
 
 const useAnalytics = () => {
-	const { isUserConnected, connectedPlugins, userConnectionData = {} } = useMyJetpackConnection();
+	const {
+		isUserConnected,
+		isSiteConnected,
+		connectedPlugins,
+		userConnectionData = {},
+	} = useMyJetpackConnection();
 	const { login, ID } = userConnectionData.currentUser?.wpcomUser || {};
 	const { myJetpackVersion = '' } = getMyJetpackWindowInitialState();
 
@@ -39,6 +44,8 @@ const useAnalytics = () => {
 		jetpackAnalytics.tracks.recordEvent( event, {
 			...properties,
 			version: myJetpackVersion,
+			isSiteConnected,
+			isUserConnected,
 			referring_plugins: connectedPluginsSlugs,
 		} );
 		// eslint-disable-next-line react-hooks/exhaustive-deps
