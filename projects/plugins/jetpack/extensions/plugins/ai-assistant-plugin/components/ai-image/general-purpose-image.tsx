d20048577839c0b491dd6956f5f83dae373a962c
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2024-06-24 15:16:54 -0300

    AI Image: Add specific placeholder instructions for `Featured` and `General` image (#38008)
    * AI Image: Use specific placeholder instruction to general and featured image
    
    * changelog
    
    * AI Image: Update general image instruction following Figma

--- projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/ai-image/general-purpose-image.tsx
+++ projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/ai-image/general-purpose-image.tsx
@@ -251,6 +251,7 @@ export default function GeneralPurposeImage( {
 			handleNextImage={ handleNextImage }
 			acceptButton={ acceptButton }
 			generateButtonLabel={ pointer?.current > 0 ? generateAgainText : generateText }
+			instructionsPlaceholder={ __( "Describe the image you'd like to create.", 'jetpack' ) }
 		/>
 	);
 }
