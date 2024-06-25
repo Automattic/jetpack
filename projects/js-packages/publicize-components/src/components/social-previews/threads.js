Automattic/jetpack

commit bc30c21a6d0a205f52ad711d577accc7920eb3e0

Social: Add Threads connection UI (#37977)
* Update threads icon color

* Add Threads to supported services for social connections

* Add badges support for services list in the modal

* Add changelogs

* Fix up versions

* Fix up versions

* Ensure threads is loaded in the initial post meta

* Add changelog

* Update screenshot and description

* Fix icon backdrop radius on Jetpack settings page

* Add specificity to facebook icon style

projects/js-packages/components/components/icons/style.module.scss
@@ -15,8 +15,11 @@
 		fill: var( --color-bluesky );
 	}
 	&.facebook {
-		fill: var( --color-facebook );
-		border-radius: 50% !important;
+		// Add some specificity to override the border-radius on Jetpack settings page
+		&:global(.social-logo) {
+			fill: var( --color-facebook );
+			border-radius: 50%;
+		}
 	}
 	&.instagram {
 		fill: var( --color-instagram );
@@ -37,14 +40,23 @@
 		fill: var( --color-mastodon );
 	}
 	&.nextdoor {
-		fill: var( --color-nextdoor );
-		border-radius: 50%;
+		// Add some specificity to override the border-radius on Jetpack settings page
+		&:global(.social-logo) {
+			fill: var( --color-nextdoor );
+			border-radius: 50%;
+		}
 	}
 	&.instagram {
 		fill: var( --color-instagram );
 	}
 	&.whatsapp {
 		fill: var( --color-whatsapp );
+	}
+	&.threads {
+		&:global(.social-logo) {
+			fill: var( --color-threads );
+			border-radius: 40%;
+		}
 	}
 }
 

projects/js-packages/publicize-components/src/components/services/style.module.scss
@@ -35,6 +35,24 @@
 		gap: 0.25rem;
 	}
 
+	.heading {
+		display: flex;
+		gap: 1rem;
+		align-items: center;
+	}
+
+	.badges {
+		display: flex;
+		gap: 0.5rem;
+	}
+
+	.badge {
+		border-radius: 4px;
+		font-size: .75rem;
+		font-weight: 600;
+		padding: 2px 8px;
+	}
+
 	.title {
 		font-size: 16px;
 		line-height: 24px;

projects/js-packages/publicize-components/src/components/services/service-item.tsx
@@ -30,7 +30,18 @@ export function ServiceItem( { service, serviceConnections }: ServicesItemProps 
 					<service.icon iconSize={ isSmall ? 36 : 48 } />
 				</div>
 				<div className={ styles[ 'service-basics' ] }>
-					<span className={ styles.title }>{ service.label }</span>
+					<div className={ styles.heading }>
+						<span className={ styles.title }>{ service.label }</span>
+						{ service.badges?.length ? (
+							<div className={ styles.badges }>
+								{ service.badges.map( ( { text, style }, index ) => (
+									<span key={ index } className={ styles.badge } style={ style }>
+										{ text }
+									</span>
+								) ) }
+							</div>
+						) : null }
+					</div>
 					{ ! isSmall && ! serviceConnections.length ? (
 						<span className={ styles.description }>{ service.description }</span>
 					) : null }

projects/js-packages/publicize-components/src/components/services/use-supported-services.tsx
@@ -7,14 +7,21 @@ import connectionsFacebook from '../../assets/connections-facebook.png';
 import connectionsInstagramBusiness from '../../assets/connections-instagram-business.png';
 import connectionsLinkedin from '../../assets/connections-linkedin.png';
 import connectionsNextdoor from '../../assets/connections-nextdoor.png';
+import connectionsThreads from '../../assets/connections-threads.png';
 import connectionsTumblr from '../../assets/connections-tumblr.png';
 import { store } from '../../social-store';
 import { ConnectionService } from '../../social-store/types';
+
+export type Badge = {
+	text: string;
+	style?: React.CSSProperties;
+};
 
 export interface SupportedService extends ConnectionService {
 	icon: React.ComponentType< { iconSize: number } >;
 	examples?: Array< React.ComponentType >;
 	needsCustomInputs?: boolean;
+	badges?: Array< Badge >;
 }
 
 /**
@@ -34,6 +41,11 @@ export function useSupportedServices(): Array< SupportedService > {
 				{}
 			);
 	}, [] );
+
+	const badgeNew: Badge = {
+		text: __( 'New', 'jetpack' ),
+		style: { background: '#e9eff5', color: '#0675C4' },
+	};
 
 	const supportedServices: Array< SupportedService > = [
 		{
@@ -103,6 +115,23 @@ export function useSupportedServices(): Array< SupportedService > {
 						alt={ __( 'Add Instagram photo', 'jetpack' ) }
 					/>
 				),
+			],
+		},
+		{
+			...availableServices.threads,
+			icon: props => <SocialServiceIcon serviceName="threads" { ...props } />,
+			description: __( 'Share posts to your Threads feed.', 'jetpack' ),
+			badges: [ badgeNew ],
+			examples: [
+				() => (
+					<>
+						{ __(
+							'Increase your presence in social media by sharing your posts automatically to Threads.',
+							'jetpack'
+						) }
+					</>
+				),
+				() => <img src={ connectionsThreads } alt={ __( 'Add Threads connection', 'jetpack' ) } />,
 			],
 		},
 		{

projects/js-packages/social-logos/src/social-logo-colors.css
@@ -14,12 +14,13 @@
 	--color-print: #f8f8f8;
 	--color-reddit: #5f99cf;
 	--color-skype: #00aff0;
+	--color-squarespace: #222;
 	--color-stumbleupon: #eb4924;
 	--color-substack: #ff6719;
-	--color-squarespace: #222;
 	--color-telegram: #08c;
 	--color-tumblr: #35465c;
 	--color-twitter: #55acee;
+	--color-threads: #000;
 	--color-whatsapp: #43d854;
 	--color-wix: #faad4d;
 	--color-x: #000;

projects/packages/publicize/src/class-publicize.php
@@ -546,6 +546,7 @@ class Publicize extends Publicize_Base {
 			'mastodon'           => array(),
 			'instagram-business' => array(),
 			'nextdoor'           => array(),
+			'threads'            => array(),
 		);
 
 		if ( 'all' === $filter ) {

commit 64517a4d93245a6492c5f65a270c627f1068fdf3

Social: Add Threads to social previews (#38003)
* Update dependencies

* Wire up Threads preview

* Fix color override for facebook icon to fix a regression introduced in #37977

* Add changelog

* Add context to Threads i18n

* Update dependencies to fix JSX attributes issue

* Fix up versions

* Fix title not displayed in caption

pnpm-lock.yaml
@@ -897,8 +897,8 @@ importers:
         specifier: 1.0.2
         version: 1.0.2
       '@automattic/social-previews':
-        specifier: 2.0.1
-        version: 2.0.1(@babel/runtime@7.24.7)(@types/react@18.3.1)(react-dom@18.3.1(react@18.3.1))(react@18.3.1)
+        specifier: 2.1.0-beta.3
+        version: 2.1.0-beta.3(@babel/runtime@7.24.7)(@types/react@18.3.1)(react-dom@18.3.1(react@18.3.1))(react@18.3.1)
       '@wordpress/annotations':
         specifier: 3.0.0
         version: 3.0.0(react@18.3.1)
@@ -3586,8 +3586,8 @@ importers:
         specifier: 1.0.0
         version: 1.0.0
       '@automattic/social-previews':
-        specifier: 2.0.1
-        version: 2.0.1(@babel/runtime@7.24.7)(@types/react@18.3.1)(react-dom@18.3.1(react@18.3.1))(react@18.3.1)
+        specifier: 2.1.0-beta.3
+        version: 2.1.0-beta.3(@babel/runtime@7.24.7)(@types/react@18.3.1)(react-dom@18.3.1(react@18.3.1))(react@18.3.1)
       '@automattic/viewport':
         specifier: 1.0.0
         version: 1.0.0
@@ -4785,8 +4785,8 @@ packages:
   '@automattic/request-external-access@1.0.0':
     resolution: {integrity: sha512-vhN72lwPFzhCVMP1l2ODBqt7fI5jfeJz1JyBnq/AUCg9PpsJfdk4vZxhSOLhSSds8VMkU5WaNnaztkYfkkYOiA==}
 
-  '@automattic/social-previews@2.0.1':
-    resolution: {integrity: sha512-diFHzjWOp5mOp+wP8zqDsRz0uQwlmxsJj8zHjzlyoT6nMhrKjTae0bbffyFqp51zRN6HNdfgTSrji3hjcxyh6Q==}
+  '@automattic/social-previews@2.1.0-beta.3':
+    resolution: {integrity: sha512-SwnTZhaC/LGQQ/XW7uxZdTVfaejs0mlDResR249dg4eX0y3//iErxhGHGc5PUpgvnLPbBbKfQqbCxnOh9nnHRA==}
     peerDependencies:
       '@babel/runtime': ^7.24.5
       react: ^18.2.0
@@ -14476,7 +14476,7 @@ snapshots:
     dependencies:
       '@automattic/popup-monitor': 1.0.2
 
-  '@automattic/social-previews@2.0.1(@babel/runtime@7.24.7)(@types/react@18.3.1)(react-dom@18.3.1(react@18.3.1))(react@18.3.1)':
+  '@automattic/social-previews@2.1.0-beta.3(@babel/runtime@7.24.7)(@types/react@18.3.1)(react-dom@18.3.1(react@18.3.1))(react@18.3.1)':
     dependencies:
       '@emotion/react': 11.11.4(@types/react@18.3.1)(react@18.3.1)
       '@wordpress/components': 28.0.0(@types/react@18.3.1)(react-dom@18.3.1(react@18.3.1))(react@18.3.1)

projects/js-packages/components/components/icons/style.module.scss
@@ -15,9 +15,9 @@
 		fill: var( --color-bluesky );
 	}
 	&.facebook {
+		fill: var( --color-facebook );
 		// Add some specificity to override the border-radius on Jetpack settings page
 		&:global(.social-logo) {
-			fill: var( --color-facebook );
 			border-radius: 50%;
 		}
 	}
@@ -40,9 +40,9 @@
 		fill: var( --color-mastodon );
 	}
 	&.nextdoor {
+		fill: var( --color-nextdoor );
 		// Add some specificity to override the border-radius on Jetpack settings page
 		&:global(.social-logo) {
-			fill: var( --color-nextdoor );
 			border-radius: 50%;
 		}
 	}
@@ -53,8 +53,8 @@
 		fill: var( --color-whatsapp );
 	}
 	&.threads {
+		fill: var( --color-threads );
 		&:global(.social-logo) {
-			fill: var( --color-threads );
 			border-radius: 40%;
 		}
 	}

projects/js-packages/publicize-components/src/components/social-previews/threads.js
@@ -1,0 +1,49 @@
+import { ThreadsPreviews } from '@automattic/social-previews';
+import { useSelect } from '@wordpress/data';
+import React from 'react';
+import { usePostMeta } from '../../hooks/use-post-meta';
+import { CONNECTION_SERVICE_THREADS, store } from '../../social-store';
+
+/**
+ * The threads tab component.
+ *
+ * @param {object} props - The props.
+ * @param {string} props.title - The post title
+ * @param {string} props.description - The post description/excerpt
+ * @param {object} props.image - The post featured image
+ * @param {string} props.url - The URL of the post
+ * @param {object[]} props.media - Array of attached media
+ * @returns {React.ReactNode} The threads tab component.
+ */
+export function Threads( { title, description, image, url, media } ) {
+	const { shareMessage: text } = usePostMeta();
+
+	const posts = useSelect(
+		select => {
+			const { displayName: name, profileImage } = select( store ).getConnectionProfileDetails(
+				CONNECTION_SERVICE_THREADS
+			);
+
+			return [
+				{
+					name,
+					profileImage,
+					text,
+					title,
+					description,
+					image,
+					media,
+					url,
+				},
+			];
+		},
+		[ title, image, description, media, url, text ]
+	);
+
+	const threadsConnections = useSelect(
+		select => select( store ).getConnectionsByService( CONNECTION_SERVICE_THREADS ),
+		[]
+	);
+
+	return <ThreadsPreviews posts={ posts } hidePostPreview={ ! threadsConnections.length } />;
+}

projects/js-packages/publicize-components/src/components/social-previews/use-available-services.js
@@ -1,10 +1,11 @@
 import { SocialServiceIcon } from '@automattic/jetpack-components';
-import { __ } from '@wordpress/i18n';
+import { __, _x } from '@wordpress/i18n';
 import React, { useMemo } from 'react';
 import {
 	CONNECTION_SERVICE_INSTAGRAM_BUSINESS,
 	CONNECTION_SERVICE_MASTODON,
 	CONNECTION_SERVICE_NEXTDOOR,
+	CONNECTION_SERVICE_THREADS,
 } from '../../social-store';
 import { getSupportedAdditionalConnections } from '../../utils';
 import FacebookPreview from './facebook';
@@ -13,6 +14,7 @@ import { Instagram } from './instagram';
 import { LinkedIn } from './linkedin';
 import MastodonPreview from './mastodon';
 import { Nextdoor } from './nextdoor';
+import { Threads } from './threads';
 import TumblrPreview from './tumblr';
 import Twitter from './twitter';
 
@@ -28,6 +30,7 @@ export function useAvailableSerivces() {
 	);
 	const isMastodonSupported = additionalConnections.includes( CONNECTION_SERVICE_MASTODON );
 	const isNextdoorSupported = additionalConnections.includes( CONNECTION_SERVICE_NEXTDOOR );
+	const isThreadsSupported = additionalConnections.includes( CONNECTION_SERVICE_THREADS );
 
 	return useMemo(
 		() =>
@@ -56,6 +59,18 @@ export function useAvailableSerivces() {
 							icon: props => <SocialServiceIcon serviceName="instagram" { ...props } />,
 							name: 'instagram',
 							preview: Instagram,
+					  }
+					: null,
+				isThreadsSupported
+					? {
+							title: _x(
+								'Threads',
+								'The name of the social media network - threads.net',
+								'jetpack'
+							),
+							icon: props => <SocialServiceIcon serviceName="threads" { ...props } />,
+							name: 'threads',
+							preview: Threads,
 					  }
 					: null,
 				{
@@ -87,6 +102,6 @@ export function useAvailableSerivces() {
 					  }
 					: null,
 			].filter( Boolean ),
-		[ isInstagramSupported, isMastodonSupported, isNextdoorSupported ]
+		[ isInstagramSupported, isMastodonSupported, isNextdoorSupported, isThreadsSupported ]
 	);
 }

projects/js-packages/publicize-components/src/social-store/index.js
@@ -26,6 +26,7 @@ export const CONNECTION_SERVICE_MASTODON = 'mastodon';
 export const CONNECTION_SERVICE_NEXTDOOR = 'nextdoor';
 export const CONNECTION_SERVICE_TUMBLR = 'tumblr';
 export const CONNECTION_SERVICE_TWITTER = 'twitter';
+export const CONNECTION_SERVICE_THREADS = 'threads';
 
 export const store = createReduxStore( SOCIAL_STORE_ID, SOCIAL_STORE_CONFIG );
 register( store );

projects/js-packages/publicize-components/src/social-store/selectors/connection-data.js
@@ -120,9 +120,9 @@ export function getConnectionProfileDetails( state, service, { forceDefaults = f
 		);
 
 		if ( connection ) {
-			const { display_name, profile_display_name, profile_picture } = connection;
+			const { display_name, profile_display_name, profile_picture, external_display } = connection;
 
-			displayName = 'twitter' === service ? profile_display_name : display_name;
+			displayName = 'twitter' === service ? profile_display_name : display_name || external_display;
 			username = 'twitter' === service ? display_name : connection.username;
 			profileImage = profile_picture;
 		}

projects/packages/publicize/src/class-publicize-base.php
@@ -2051,6 +2051,10 @@ abstract class Publicize_Base {
 			$additional_connections[] = 'nextdoor';
 		}
 
+		if ( $this->has_connection_feature( 'threads' ) ) {
+			$additional_connections[] = 'threads';
+		}
+
 		return $additional_connections;
 	}
 