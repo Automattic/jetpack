725f29347eda36bde8d0357da6edfb0ce354d20a
sergeymitr <sergeymitr@gmail.com>
2024-06-26 17:44:53 -0400

    Only show notice and load JS when necessary.

---
+++ projects/plugins/jetpack/src/class-deprecate.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Place to properly deprecate Jetpack features.
+ *
+ * @package automattic/jetpack
+ */
+
+namespace Automattic\Jetpack\Plugin;
+
+use Automattic\Jetpack\Assets;
+use Automattic\Jetpack\Modules;
+
+/**
+ * Place to properly deprecate Jetpack features.
+ */
+class Deprecate {
+
+	/**
+	 * The singleton instance.
+	 *
+	 * @var Deprecate
+	 */
+	private static $instance;
+
+	/**
+	 * Initialize the class.
+	 */
+	private function __construct() {
+		add_action( 'admin_notices', array( $this, 'render_ga_admin_notice' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
+	}
+
+	/**
+	 * Create/get the singleton instance.
+	 *
+	 * @return static
+	 */
+	public static function instance() {
+		if ( null === static::$instance ) {
+			static::$instance = new static();
+		}
+
+		return static::$instance;
+	}
+
+	/**
+	 * Enqueue the scripts.
+	 *
+	 * @return void
+	 */
+	public function enqueue_admin_scripts() {
+		if ( ! $this->has_notices() ) {
+			return;
+		}
+
+		if ( ! wp_script_is( 'jetpack-deprecate', 'registered' ) ) {
+			wp_register_script(
+				'jetpack-deprecate',
+				Assets::get_file_url_for_environment( '_inc/build/deprecate.min.js', '_inc/deprecate.js' ),
+				array(),
+				JETPACK__VERSION,
+				true
+			);
+		}
+
+		wp_enqueue_script( 'jetpack-deprecate' );
+	}
+
+	/**
+	 * Render Google Analytics deprecation notice.
+	 *
+	 * @return void
+	 */
+	public function render_ga_admin_notice() {
+		if ( ! $this->has_ga_notice() ) {
+			return;
+		}
+
+		$this->render_notice(
+			'jetpack-ga-admin-notice',
+			esc_html__( 'Jetpack Google Analytics will no longer be supported after August 6, 2024.', 'jetpack' )
+			. ' <a href="https://jetpack.com/support/google-analytics/" target="_blank">' . esc_html__( 'See how to keep your site intact.', 'jetpack' ) . '</a>'
+		);
+	}
+
+	/**
+	 * Render the notice.
+	 *
+	 * @param string $id The notice ID.
+	 * @param string $text The notice text.
+	 * @param array  $params Additional notice params.
+	 *
+	 * @return void
+	 */
+	private function render_notice( $id, $text, $params = array() ) {
+		if ( ! empty( $_COOKIE['jetpack_deprecate_dismissed'][ $id ] ) ) {
+			return;
+		}
+
+		$params['id'] = $id;
+
+		if ( empty( $params['type'] ) ) {
+			$params['type'] = 'warning';
+		}
+
+		if ( empty( $params['dismissible'] ) ) {
+			$params['dismissible'] = true;
+		}
+
+		if ( $params['dismissible'] ) {
+			if ( empty( $params['additional_classes'] ) ) {
+				$params['additional_classes'] = array();
+			}
+
+			$params['additional_classes'][] = 'jetpack-deprecate-dismissible';
+		}
+
+		wp_admin_notice( $text, $params );
+	}
+
+	/**
+	 * Check if there are any notices to be displayed, so we wouldn't load unnecessary JS.
+	 *
+	 * @return bool
+	 */
+	private function has_notices() {
+		return $this->has_ga_notice();
+	}
+
+	/**
+	 * Check if Google Analytics notice should show up.
+	 *
+	 * @return bool
+	 */
+	private function has_ga_notice() {
+		return ( new Modules() )->is_active( 'google-analytics', false )
+			&& ! is_plugin_active( 'jetpack-legacy-google-analytics/jetpack-legacy-google-analytics.php' );
+	}
+}
