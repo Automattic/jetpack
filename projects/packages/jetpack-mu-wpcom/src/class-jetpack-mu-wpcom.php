88f774e81ba3eb1e47ea3eb1c598a73261508002
Omar Alshaker <omar@omaralshaker.com>
2024-06-27 19:26:14 +0200

    Add Help Center

--- projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
+++ projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
@@ -85,6 +85,7 @@ class Jetpack_Mu_Wpcom {
 		require_once __DIR__ . '/features/cloudflare-analytics/cloudflare-analytics.php';
 		require_once __DIR__ . '/features/error-reporting/error-reporting.php';
 		require_once __DIR__ . '/features/first-posts-stream/first-posts-stream-helpers.php';
+		require_once __DIR__ . '/features/help-center/class-help-center.php';
 		require_once __DIR__ . '/features/import-customizations/import-customizations.php';
 		require_once __DIR__ . '/features/marketplace-products-updater/class-marketplace-products-updater.php';
 		require_once __DIR__ . '/features/media/heif-support.php';
