1b79414a9d40f5b6b17e95a028fc0e26d0380f8d
Luiz Kowalski <lhkowalski@gmail.com>
2024-06-28 16:40:01 -0300

    Fix issue when the selection should be multiple

--- projects/plugins/jetpack/extensions/shared/external-media/sources/jetpack-ai-general-purpose-image.js
+++ projects/plugins/jetpack/extensions/shared/external-media/sources/jetpack-ai-general-purpose-image.js
@@ -3,12 +3,12 @@ import {
 	PLACEMENT_MEDIA_SOURCE_DROPDOWN,
 } from '../../../plugins/ai-assistant-plugin/components/ai-image';
 
-function JetpackAIGeneralPurposeImage( { onClose = () => {}, onSelect } ) {
+function JetpackAIGeneralPurposeImage( { onClose = () => {}, onSelect, multiple = false } ) {
 	return (
 		<GeneralPurposeImage
 			placement={ PLACEMENT_MEDIA_SOURCE_DROPDOWN }
 			onClose={ onClose }
-			onSetImage={ onSelect }
+			onSetImage={ image => onSelect( multiple ? [ image ] : image ) }
 		/>
 	);
 }
