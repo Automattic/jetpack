1ff3e58e2e5b29311d77f9e55a96cb0afb2e1ab6
Omar Alshaker <omar@omaralshaker.com>
2024-06-28 19:15:39 +0200

    Add translations

---
+++ projects/packages/jetpack-mu-wpcom/bin/download-help-center-languages.js
@@ -0,0 +1,51 @@
+const https = require( 'https' );
+
+[
+	'ar',
+	'de-ch',
+	'de',
+	'de_formal',
+	'el-po',
+	'es-cl',
+	'es-mx',
+	'es',
+	'fr-be',
+	'fr-ca',
+	'fr-ch',
+	'fr',
+	'he',
+	'id',
+	'it',
+	'ja',
+	'ko',
+	'nl',
+	'pt-br',
+	'ro',
+	'ru',
+	'sv',
+	'tr',
+	'zh-cn',
+	'zh-sg',
+	'zh-tw',
+].forEach( lang => {
+	const url = `https://widgets.wp.com/help-center/languages/${ lang }-v1.1.json`;
+	const dest = require( 'path' ).resolve(
+		'src',
+		'features',
+		'help-center',
+		'languages',
+		`${ lang }.json`
+	);
+
+	https.get( url, response => {
+		let data = '';
+
+		response.on( 'data', chunk => {
+			data += chunk;
+		} );
+
+		response.on( 'end', () => {
+			require( 'fs' ).writeFileSync( dest, data );
+		} );
+	} );
+} );
