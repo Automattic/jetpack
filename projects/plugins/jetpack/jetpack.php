2945393df3262b366f3a6d1a0f3f3f905c15b8bf
LuPuS2k <gabrieldemichelis@gmail.com>
2024-07-01 16:15:55 -0300

    Test plugin update 1

--- projects/plugins/jetpack/jetpack.php
+++ projects/plugins/jetpack/jetpack.php
@@ -22,6 +22,8 @@ modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
 of the License, or (at your option) any later version.
 
+*** This text change is just to test the plugin update workflow ***
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
