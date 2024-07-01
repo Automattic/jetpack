5522891d5e403343afe38001639d6fa6733415f9
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2024-07-01 16:44:53 +0200

    Remove `is_profile_page` check

--- projects/packages/jetpack-mu-wpcom/src/features/classic-profile-settings-notice/classic-profile-settings-notice.php
+++ projects/packages/jetpack-mu-wpcom/src/features/classic-profile-settings-notice/classic-profile-settings-notice.php
@@ -6,22 +6,9 @@
  */
 
 /**
- * Check whether the current page is the profile page
- */
-function is_profile_page() {
-	$screen = get_current_screen();
-
-	return 'profile' === $screen->id;
-}
-
-/**
  * Show a notice informing about profile settings on "Classic" admin-interface sites being "untangled" from `/me` and `/me/account`
  */
 function show_classic_admin_interface_profile_settings_notice() {
-	if ( ! is_profile_page() ) {
-		return;
-	}
-
 	$message = sprintf(
 		/* translators: 1: Learn more URL */
 		__( 'These settings are applied to sites using the Classic admin interface style. <a href="%1$s">Learn more</a>.', 'jetpack-mu-wpcom' ),
