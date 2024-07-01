b556b8fc2fe9bc277d8e7ba82d4b5c18ba82eb31
Luiz Kowalski <lhkowalski@gmail.com>
2024-07-01 15:39:07 -0300

    Jetpack AI Image: add button entrypoint on the image block (#38123)
    * Add button to trigger general purpose image generation
    
    * Fix button positioning
    
    * Let media button follow the same margin/padding rule of other buttons
    
    * Fix issue when the selection should be multiple
    
    * Move the AI button to it's own component file
    
    * Do not add the button when it's a featured image generation
    
    * Only support AI button on specific blocks
    
    * Add new placement const for the new button
    
    * Add media source specific to render general purpose image for blocks
    
    * Trigger the general image for block when the button is clicked
    
    * Rename original component to follow pattern and refer to media source placement
    
    * changelog
    
    * Only show the button when the beta extensions are enabled :|
    
    ---------
    
    Co-authored-by: Douglas <douglas.henri@automattic.com>

--- projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/ai-image/index.ts
+++ projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/ai-image/index.ts
@@ -1,5 +1,10 @@
 import FeaturedImage from './featured-image';
 import GeneralPurposeImage from './general-purpose-image';
-import { PLACEMENT_MEDIA_SOURCE_DROPDOWN } from './types';
+import { PLACEMENT_MEDIA_SOURCE_DROPDOWN, PLACEMENT_BLOCK_PLACEHOLDER_BUTTON } from './types';
 
-export { FeaturedImage, PLACEMENT_MEDIA_SOURCE_DROPDOWN, GeneralPurposeImage };
+export {
+	FeaturedImage,
+	PLACEMENT_MEDIA_SOURCE_DROPDOWN,
+	PLACEMENT_BLOCK_PLACEHOLDER_BUTTON,
+	GeneralPurposeImage,
+};
