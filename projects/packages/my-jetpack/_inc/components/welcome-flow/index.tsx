e2b0e2dba872b2066d3e9b31562a770644f97942
Robert Sreberski <robertsreberski@gmail.com>
2024-07-03 15:00:31 +0200

    Add evaluation form to Welcome Flow (#37790)
    * Enhance TermsOfService component
    
    * Update MyJetpack connection hook and its usage
    
    * Update WelcomeBanner text and add async site registration on click
    
    * changelog
    
    * Add events and banner dismissal upon connection success
    
    * Update versions
    
    * Add WelcomeFlow (separate code) structure
    
    * Revert WelcomeBanner code
    
    * Update banner
    
    * Update versions
    
    * Add evaluation form step
    
    * Add evaluation processing step and styles
    
    * Add evaluation steps to welcome flow
    
    * Replace classnames with clsx
    
    * Update image filename
    
    * Hide notices when welcome banner is active
    
    * Code improvements
    
    * Prevent reloading on site connection
    
    * Introduce Notice templating (unify Notices)
    
    * Add 'currentStep' prop to dismiss Tracks event
    
    * Add more info to dismiss Tracks event
    
    * Code improvements

--- projects/packages/my-jetpack/_inc/components/welcome-flow/index.tsx
+++ projects/packages/my-jetpack/_inc/components/welcome-flow/index.tsx
@@ -1,36 +1,63 @@
 import { Container, Col, Button } from '@automattic/jetpack-components';
 import { __ } from '@wordpress/i18n';
 import { close } from '@wordpress/icons';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import useWelcomeBanner from '../../data/welcome-banner/use-welcome-banner';
 import useAnalytics from '../../hooks/use-analytics';
 import useMyJetpackConnection from '../../hooks/use-my-jetpack-connection';
 import { CardWrapper } from '../card';
 import ConnectionStep from './ConnectionStep';
+import EvaluationProcessingStep from './EvaluationProcessingStep';
+import EvaluationStep, { EvaluationAreas } from './EvaluationStep';
 import styles from './style.module.scss';
 import type { FC } from 'react';
 
 const WelcomeFlow: FC = () => {
 	const { recordEvent } = useAnalytics();
 	const { isWelcomeBannerVisible, dismissWelcomeBanner } = useWelcomeBanner();
-	const { siteIsRegistered, siteIsRegistering, handleRegisterSite } = useMyJetpackConnection( {
+	const {
+		siteIsRegistered,
+		siteIsRegistering,
+		isUserConnected,
+		isSiteConnected,
+		handleRegisterSite,
+	} = useMyJetpackConnection( {
 		skipUserConnection: true,
 	} );
 	const [ visible, setVisible ] = useState( isWelcomeBannerVisible );
+	const [ isProcessingEvaluation, setIsProcessingEvaluation ] = useState( false );
+
+	const currentStep = useMemo( () => {
+		if ( ! siteIsRegistered ) {
+			return 'connection';
+		} else if ( ! isProcessingEvaluation ) {
+			return 'evaluation';
+		}
+
+		return 'evaluation-processing';
+	}, [ isProcessingEvaluation, siteIsRegistered ] );
 
 	const onDismissClick = useCallback( () => {
-		recordEvent( 'jetpack_myjetpack_welcome_banner_dismiss_click' );
+		recordEvent( 'jetpack_myjetpack_welcome_banner_dismiss_click', {
+			currentStep,
+			isUserConnected,
+			isSiteConnected,
+		} );
 		setVisible( false );
 		dismissWelcomeBanner();
-	}, [ recordEvent, dismissWelcomeBanner ] );
+	}, [ recordEvent, currentStep, isUserConnected, isSiteConnected, dismissWelcomeBanner ] );
 
-	useEffect( () => {
-		// TODO: It's a temporary code instead of Survey step
-		if ( siteIsRegistered && visible ) {
-			setVisible( false );
-			dismissWelcomeBanner();
-		}
-	}, [ visible, siteIsRegistered, dismissWelcomeBanner ] );
+	const handleEvaluation = useCallback(
+		( _values: { [ key in EvaluationAreas ]: boolean } ) => {
+			setIsProcessingEvaluation( true );
+			setTimeout( () => {
+				// TODO: Mock "evaluation": Implement the evaluation endpoint
+				setVisible( false );
+				dismissWelcomeBanner();
+			}, 3_000 );
+		},
+		[ dismissWelcomeBanner ]
+	);
 
 	if ( ! visible ) {
 		return null;
@@ -45,10 +72,19 @@ const WelcomeFlow: FC = () => {
 						horizontalGap={ 0 }
 						className={ styles[ 'banner-content' ] }
 					>
-						<ConnectionStep
-							onActivateSite={ handleRegisterSite }
-							isActivating={ siteIsRegistering }
-						/>
+						{ 'connection' === currentStep && (
+							<ConnectionStep
+								onActivateSite={ handleRegisterSite }
+								isActivating={ siteIsRegistering }
+							/>
+						) }
+						{ 'evaluation' === currentStep && (
+							<EvaluationStep
+								onSkipOnboarding={ onDismissClick }
+								onSubmitEvaluation={ handleEvaluation }
+							/>
+						) }
+						{ 'evaluation-processing' === currentStep && <EvaluationProcessingStep /> }
 					</Container>
 				</CardWrapper>
 				<Button
