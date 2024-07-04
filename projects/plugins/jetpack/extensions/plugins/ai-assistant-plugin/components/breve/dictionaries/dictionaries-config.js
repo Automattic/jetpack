a086cb48d03cce67f3dbe91e1f834bdd52efb353
Douglas Henri <dhasilva@users.noreply.github.com>
2024-07-04 12:25:03 -0300

    AI Proofread: Move prompts to the backend (#38182)
    * rename variables and change request message to the jetpack-ai role
    
    * remove the prompts from the dictionaries config file
    
    * changelog

--- projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/breve/dictionaries/dictionaries-config.js
+++ projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/breve/dictionaries/dictionaries-config.js
@@ -11,10 +11,6 @@ const config = {
 			type: 'key-value',
 			tooltip: "Consider replacing with '{value}'.",
 			label: 'Complex words',
-			apiRequest: {
-				systemMessage:
-					"Replace the word '{text}' in the sentence with '{value}'. FIX the grammar after replacing the word. REFINE grammar if needed. THEN insert the sentence back into the paragraph. ONLY respond with the updated paragraph, nothing else.",
-			},
 		},
 		'long-sentence': {
 			type: 'function',
@@ -30,40 +26,24 @@ const config = {
 			},
 			tooltip: 'Break this long sentence into shorter ones.',
 			label: 'Long sentences',
-			apiRequest: {
-				systemMessage:
-					"Break this exact sentence only, into shorter sentences: '{text}'. DO NOT touch any other sentence in the paragraph. Make sure the new, shorter sentences are grammatically correct. If any of the sentences are in the passive voice, change them to active voice. THEN insert the new sentences back into the paragraph in place of the original long sentence. ONLY respond with the updated paragraph, nothing else.",
-			},
 		},
 		weasel: {
 			dictionary: weaselWords,
 			type: 'list',
 			tooltip: "Remove 'Weasel' words to add confidence.",
 			label: 'Weasel words',
-			apiRequest: {
-				systemMessage:
-					"Take out the word '{text}' out from the sentence. FIX the grammar after taking the word out. REFINE grammar if needed. THEN insert the sentence back into the paragraph. ONLY respond with the updated paragraph, nothing else.",
-			},
 		},
 		adverb: {
 			dictionary: adverbs,
 			type: 'list',
 			tooltip: 'Adverbs make your writing less concise.',
 			label: 'Adverbs',
-			apiRequest: {
-				systemMessage:
-					"Take out the word '{text}' out from the sentence. FIX the grammar after taking the word out. REFINE grammar if needed. THEN insert the sentence back into the paragraph. ONLY respond with the updated paragraph, nothing else.",
-			},
 		},
 		adjective: {
 			dictionary: adjectives,
 			type: 'list',
 			tooltip: 'Replace adjectives with data or remove them.',
 			label: 'Adjectives',
-			apiRequest: {
-				systemMessage:
-					"Take out the word '{text}' out from the sentence. FIX the grammar after taking the word out. REFINE grammar if needed. THEN insert the sentence back into the paragraph. ONLY respond with the updated paragraph, nothing else.",
-			},
 		},
 	},
 };
