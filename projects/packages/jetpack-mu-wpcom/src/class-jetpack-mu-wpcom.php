f725afa45afdab6f9cf3bdbefa69e9b20371f1f9
arthur791004 <arthur.chu@automattic.com>
2024-07-05 11:15:26 +0900

    MU WPCOM: Port font-smoothing-antialiased feature from ETK (#38195)
    * MU WPCOM: Port font-smoothing-antialiased feature from ETK
    
    * changelog

--- projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
+++ projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
@@ -90,6 +90,7 @@ class Jetpack_Mu_Wpcom {
 		require_once __DIR__ . '/features/cloudflare-analytics/cloudflare-analytics.php';
 		require_once __DIR__ . '/features/error-reporting/error-reporting.php';
 		require_once __DIR__ . '/features/first-posts-stream/first-posts-stream-helpers.php';
+		require_once __DIR__ . '/features/font-smoothing-antialiased/font-smoothing-antialiased.php';
 		// To avoid potential collisions with ETK.
 		if ( ! class_exists( 'A8C\FSE\Help_Center' ) ) {
 			require_once __DIR__ . '/features/help-center/class-help-center.php';
