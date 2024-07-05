c7778c73da01b4f758317b80743b7d3b2880e52f
arthur <arthur.chu@automattic.com>
2024-07-05 10:52:57 +0800

    Load feature only on simple sites

--- projects/packages/jetpack-mu-wpcom/src/features/override-preview-button-url/override-preview-button-url.php
+++ projects/packages/jetpack-mu-wpcom/src/features/override-preview-button-url/override-preview-button-url.php
@@ -11,9 +11,36 @@ use Automattic\Jetpack\Jetpack_Mu_Wpcom;
 define( 'MU_WPCOM_OVERRIDE_PREVIEW_BUTTON_URL', true );
 
 /**
+ * Check if the feature should be loaded.
+ *
+ * @return bool
+ */
+function should_load_override_preview_button_url() {
+	if ( ! class_exists( 'Automattic\Jetpack\Status\Host' ) ) {
+		return false;
+	}
+
+	$host = new Automattic\Jetpack\Status\Host();
+	if ( ! $host->is_wpcom_simple() ) {
+		return false;
+	}
+
+	global $pagenow;
+	$allowed_pages = array(
+		'post.php',
+		'post-new.php',
+	);
+	return isset( $pagenow ) && in_array( $pagenow, $allowed_pages, true );
+}
+
+/**
  * Enqueue assets
  */
 function wpcom_enqueue_override_preview_button_url_assets() {
+	if ( ! should_load_override_preview_button_url() ) {
+		return;
+	}
+
 	$asset_file          = include Jetpack_Mu_Wpcom::BASE_DIR . 'build/override-preview-button-url/override-preview-button-url.asset.php';
 	$script_dependencies = $asset_file['dependencies'] ?? array();
 	$script_version      = $asset_file['version'] ?? filemtime( Jetpack_Mu_Wpcom::BASE_DIR . 'build/override-preview-button-url/override-preview-button-url.js' );
