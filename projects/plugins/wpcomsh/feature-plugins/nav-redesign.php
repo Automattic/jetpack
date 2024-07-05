c058882957585040c8bfd31475171f4e96cb9797
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2024-07-05 12:03:31 +0200

    Revert "Move `uses_wp_admin_interface` to new feature"
    This reverts commit 0d6af43c621f893a3f6eb77473f2e2e472f2a1ae.

--- projects/plugins/wpcomsh/feature-plugins/nav-redesign.php
+++ projects/plugins/wpcomsh/feature-plugins/nav-redesign.php
@@ -6,6 +6,15 @@
  */
 
 /**
+ * Whether the current blog uses wp-admin as the admin interface style.
+ *
+ * @return bool Returns true if the blog uses wp-admin as the admin interface style, false otherwise.
+ */
+function uses_wp_admin_interface() {
+	return get_option( 'wpcom_admin_interface' ) === 'wp-admin';
+}
+
+/**
  * Returns whether the current request is coming from the a8c proxy.
  */
 function is_proxied() {
