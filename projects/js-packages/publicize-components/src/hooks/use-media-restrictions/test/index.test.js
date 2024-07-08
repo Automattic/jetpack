ca1d15f2099caf8ee79e2607175cf377e07976fc
Gergely Juhasz <gergely.juhasz@automattic.com>
2024-07-08 15:22:07 +0200

    Add test to cover this case

--- projects/js-packages/publicize-components/src/hooks/use-media-restrictions/test/index.test.js
+++ projects/js-packages/publicize-components/src/hooks/use-media-restrictions/test/index.test.js
@@ -8,8 +8,6 @@ import useMediaRestrictions, {
 } from '../index';
 
 jest.mock( '../../use-attached-media', () => jest.fn() );
-// Mocking attached media to allow validation
-useAttachedMedia.mockReturnValue( [ 1 ] );
 
 const DUMMY_CONNECTIONS = [
 	{
@@ -63,6 +61,33 @@ const getHookProps = (
 ];
 
 describe( 'useMediaRestrictions hook', () => {
+	beforeEach( () => {
+		// Mocking attached media to allow validation
+		useAttachedMedia.mockReturnValue( [ 1 ] );
+	} );
+
+	test( 'Should not run validation if a connection does not require media or if attachedMedia is empty', () => {
+		// Mocking useAttachedMedia to return an empty array to simulate no attached media
+		useAttachedMedia.mockReturnValue( [] );
+
+		// Define media that would normally fail validation
+		const INVALID_MEDIA = {
+			metaData: { mime: 'image/jpg', fileSize: 100000000 }, // Large file size
+			mediaData: { width: 400, height: 500 },
+		};
+
+		const { result } = renderHook( () =>
+			useMediaRestrictions(
+				...getHookProps( { media: INVALID_MEDIA, connections: DUMMY_CONNECTIONS } )
+			)
+		);
+
+		// Expect only the Instagram connection to have an error since it requires media
+		// and we validate it even if there is no attached media
+		expect( result.current.validationErrors ).toEqual( {
+			'instagram-business': 'FILE_SIZE_ERROR',
+		} );
+	} );
 	test( 'should not get any errors for image that accepted by all platforms', () => {
 		VALID_MEDIA_ALL.forEach( media => {
 			const { result } = renderHook( () => useMediaRestrictions( ...getHookProps( { media } ) ) );
