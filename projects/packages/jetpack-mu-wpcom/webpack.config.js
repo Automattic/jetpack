d7ee9212a0cf7573445911387c019632caf0ea4e
Griffith Chen <griffith.chen@automattic.com>
2024-07-09 18:13:50 +0800

    Fix feature build

--- projects/packages/jetpack-mu-wpcom/webpack.config.js
+++ projects/packages/jetpack-mu-wpcom/webpack.config.js
@@ -21,6 +21,7 @@ module.exports = [
 				'./src/features/paragraph-block-placeholder/paragraph-block-placeholder.js',
 			'tags-education': './src/features/tags-education/tags-education.js',
 			'wpcom-admin-bar': './src/features/wpcom-admin-bar/wpcom-admin-bar.scss',
+			'wpcom-block-description-links': './src/features/wpcom-block-description-links/index.tsx',
 			'wpcom-sidebar-notice': './src/features/wpcom-sidebar-notice/wpcom-sidebar-notice.scss',
 		},
 		mode: jetpackWebpackConfig.mode,
