61d22dad965c04aefa278df327d6b52e37e6333e
okmttdhr <okmttdhr@users.noreply.github.com>
2024-07-09 17:13:05 +0900

    Remove wpcom-site-menu.scss import from webpack.config.js

--- projects/packages/jetpack-mu-wpcom/webpack.config.js
+++ projects/packages/jetpack-mu-wpcom/webpack.config.js
@@ -25,7 +25,6 @@ module.exports = [
 			'tags-education': './src/features/tags-education/tags-education.js',
 			'wpcom-admin-bar': './src/features/wpcom-admin-bar/wpcom-admin-bar.scss',
 			'wpcom-sidebar-notice': './src/features/wpcom-sidebar-notice/wpcom-sidebar-notice.scss',
-			'wpcom-site-menu': './src/features/wpcom-site-menu/wpcom-site-menu.scss',
 		},
 		mode: jetpackWebpackConfig.mode,
 		devtool: jetpackWebpackConfig.devtool,
