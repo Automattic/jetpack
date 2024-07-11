b38310c6797fb0c87e5010beee653e7a0ab6f028
Bryan Elliott <bryan@elliottprogrammer.com>
2024-07-10 23:17:44 -0400

    Show "Upgrade" button & secondary "View" button when no paid plan.

--- projects/packages/my-jetpack/_inc/components/product-cards-section/protect-card/index.tsx
+++ projects/packages/my-jetpack/_inc/components/product-cards-section/protect-card/index.tsx
@@ -1,16 +1,35 @@
 import { __ } from '@wordpress/i18n';
 import { useCallback, type FC } from 'react';
-import { PRODUCT_STATUSES } from '../../../constants';
+import useProduct from '../../../data/products/use-product';
+import useAnalytics from '../../../hooks/use-analytics';
 import ProductCard from '../../connected-product-card';
 import ProtectValueSection from './protect-value-section';
 
 const ProtectCard: FC< { admin: boolean } > = ( { admin } ) => {
-	// Override the primary action button to read "Protect your site" instead
-	// of the default text, "Learn more".
-	const primaryActionOverride = {
-		[ PRODUCT_STATUSES.ABSENT ]: {
-			label: __( 'Protect your site', 'jetpack-my-jetpack' ),
-		},
+	const { recordEvent } = useAnalytics();
+	const slug = 'protect';
+	const { detail } = useProduct( slug );
+	const { hasPaidPlanForProduct: hasProtectPaidPlan } = detail;
+
+	/**
+	 * Called when secondary "View" button is clicked.
+	 */
+	const onViewButtonClick = useCallback( () => {
+		recordEvent( 'jetpack_myjetpack_product_card_manage_click', {
+			product: slug,
+		} );
+	}, [ recordEvent ] );
+
+	const shouldShowSecondaryButton = useCallback(
+		() => ! hasProtectPaidPlan,
+		[ hasProtectPaidPlan ]
+	);
+
+	const viewButton = {
+		href: 'admin.php?page=jetpack-protect',
+		label: __( 'View', 'jetpack-my-jetpack' ),
+		onClick: onViewButtonClick,
+		shouldShowButton: shouldShowSecondaryButton,
 	};
 
 	const noDescription = useCallback( () => null, [] );
@@ -18,8 +37,9 @@ const ProtectCard: FC< { admin: boolean } > = ( { admin } ) => {
 	return (
 		<ProductCard
 			admin={ admin }
-			slug="protect"
-			primaryActionOverride={ primaryActionOverride }
+			slug={ slug }
+			upgradeInInterstitial={ true }
+			secondaryAction={ viewButton }
 			Description={ noDescription }
 		>
 			<ProtectValueSection />
