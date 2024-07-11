788b84c44911a0fbac718eb1066500832c0bdf34
Griffith Chen <griffith.chen@automattic.com>
2024-07-11 09:12:14 +0800

    Typo

--- projects/packages/jetpack-mu-wpcom/src/features/block-inserter-modifications/block-inserter-modifications.php
+++ projects/packages/jetpack-mu-wpcom/src/features/block-inserter-modifications/block-inserter-modifications.php
@@ -13,26 +13,26 @@ define( 'MU_WPCOM_BLOCK_INSERTER_MODIFICATIONS', true );
  * Enqueue script for the Block Inserter modifications.
  */
 function wpcom_enqueue_block_inserter_modifications_assets() {
-	$asset_file = include Jetpack_Mu_Wpcom::BASE_DIR . 'build/block-inserter-modications/block-inserter-modications.asset.php';
+	$asset_file = include Jetpack_Mu_Wpcom::BASE_DIR . 'build/block-inserter-modifications/block-inserter-modifications.asset.php';
 
 	wp_enqueue_script(
-		'block-inserter-modications-script',
-		plugins_url( 'build/block-inserter-modications/block-inserter-modications.js', Jetpack_Mu_Wpcom::BASE_FILE ),
+		'block-inserter-modifications-script',
+		plugins_url( 'build/block-inserter-modifications/block-inserter-modifications.js', Jetpack_Mu_Wpcom::BASE_FILE ),
 		$asset_file['dependencies'] ?? array(),
-		$asset_file['version'] ?? filemtime( Jetpack_Mu_Wpcom::BASE_DIR . 'build/block-inserter-modications/block-inserter-modications.js' ),
+		$asset_file['version'] ?? filemtime( Jetpack_Mu_Wpcom::BASE_DIR . 'build/block-inserter-modifications/block-inserter-modifications.js' ),
 		true
 	);
 
-	wp_set_script_translations( 'block-inserter-modications-script', 'jetpack-mu-wpcom' );
+	wp_set_script_translations( 'block-inserter-modifications-script', 'jetpack-mu-wpcom' );
 
-	$style_name = 'block-inserter-modications';
+	$style_name = 'block-inserter-modifications';
 	$style_file = is_rtl() ? $style_name . '.rtl.css' : $style_name . '.css';
 
 	wp_enqueue_style(
-		'block-inserter-modications-styles',
-		plugins_url( 'build/block-inserter-modications/' . $style_file, Jetpack_Mu_Wpcom::BASE_FILE ),
+		'block-inserter-modifications-styles',
+		plugins_url( 'build/block-inserter-modifications/' . $style_file, Jetpack_Mu_Wpcom::BASE_FILE ),
 		array(),
-		filemtime( Jetpack_Mu_Wpcom::BASE_DIR . 'build/wpcom-documentation-links/' . $style_file . '.css' )
+		filemtime( Jetpack_Mu_Wpcom::BASE_DIR . 'build/block-inserter-modifications/' . $style_file )
 	);
 }
 
