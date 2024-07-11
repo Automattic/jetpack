8ff0828c6311c5a0179415882c61264b22d76b50
Douglas Henri <dhasilva@users.noreply.github.com>
2024-07-11 11:54:23 -0300

    AI Assistant: Add ambiguous words to proofread features list (#38292)
    * fix ts issues and add types
    
    * add ambiguous words feature
    
    * fix menu spacing
    
    * changelog

--- projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/breve/features/events.ts
+++ projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/breve/features/events.ts
@@ -7,23 +7,24 @@ import { dispatch } from '@wordpress/data';
  */
 import getContainer from './container';
 import features from './index';
+/**
+ * Types
+ */
+import type { BreveDispatch } from '../types';
 
-let timeout;
+let timeout: number;
 
-function handleMouseEnter( e ) {
+function handleMouseEnter( e: React.MouseEvent ) {
 	e.stopPropagation();
 	clearTimeout( timeout );
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	( dispatch( 'jetpack/ai-breve' ) as any ).setHighlightHover( true );
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	( dispatch( 'jetpack/ai-breve' ) as any ).setPopoverAnchor( e.target );
+	( dispatch( 'jetpack/ai-breve' ) as BreveDispatch ).setHighlightHover( true );
+	( dispatch( 'jetpack/ai-breve' ) as BreveDispatch ).setPopoverAnchor( e.target );
 }
 
-function handleMouseLeave( e ) {
+function handleMouseLeave( e: React.MouseEvent ) {
 	e.stopPropagation();
 	timeout = setTimeout( () => {
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		( dispatch( 'jetpack/ai-breve' ) as any ).setHighlightHover( false );
+		( dispatch( 'jetpack/ai-breve' ) as BreveDispatch ).setHighlightHover( false );
 	}, 100 );
 }
 
@@ -33,7 +34,8 @@ export default function registerEvents( clientId: string ) {
 	const block = container?.querySelector?.( `#${ id }` );
 
 	features.forEach( ( { config } ) => {
-		const items = block?.querySelectorAll?.( `[data-type='${ config.name }']` );
+		const items = block?.querySelectorAll?.( `[data-type='${ config.name }']` ) || [];
+
 		if ( items?.length > 0 ) {
 			items.forEach( highlightEl => {
 				highlightEl?.removeEventListener?.( 'mouseenter', handleMouseEnter );
