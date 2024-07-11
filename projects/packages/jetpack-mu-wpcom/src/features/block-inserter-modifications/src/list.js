bbc847b58d9bb9bc75bc8a93e3794fde1ea4f6c3
Griffith Chen <griffith.chen@automattic.com>
2024-07-11 09:44:43 +0800

    Remove outdated tips

--- projects/packages/jetpack-mu-wpcom/src/features/block-inserter-modifications/src/list.js
+++ projects/packages/jetpack-mu-wpcom/src/features/block-inserter-modifications/src/list.js
@@ -37,26 +37,6 @@ const tips = [
 		permission: 'settings',
 	},
 	{
-		context: 'css',
-		keywords: [
-			'css',
-			__( 'css', 'jetpack-mu-wpcom' ),
-			'style',
-			__( 'style', 'jetpack-mu-wpcom' ),
-		],
-		description: getTipDescription(
-			__(
-				'You can visit the the <a>Customizer</a> to edit the CSS on your site.',
-				'jetpack-mu-wpcom'
-			),
-			{
-				a: <TipLink section="customizer" subsection="custom_css" />,
-			},
-			__( 'You can visit the the Customizer to edit the CSS on your site.', 'jetpack-mu-wpcom' )
-		),
-		permission: 'settings',
-	},
-	{
 		context: 'plugin',
 		keywords: [ 'plugin', __( 'plugin', 'jetpack-mu-wpcom' ) ],
 		description: getTipDescription(
@@ -71,36 +51,6 @@ const tips = [
 				'You can visit the plugin directory to get started with installing new plugins.',
 				'jetpack-mu-wpcom'
 			)
-		),
-		permission: 'settings',
-	},
-	{
-		context: 'header',
-		keywords: [ 'header', __( 'header', 'jetpack-mu-wpcom' ) ],
-		description: getTipDescription(
-			__(
-				'You can visit the the <a>Customizer</a> to edit your logo and site title.',
-				'jetpack-mu-wpcom'
-			),
-			{
-				a: <TipLink section="customizer" subsection="title_tagline" />,
-			},
-			__( 'You can visit the the Customizer to edit your logo and site title.', 'jetpack-mu-wpcom' )
-		),
-		permission: 'settings',
-	},
-	{
-		context: 'color',
-		keywords: [ 'color', __( 'color', 'jetpack-mu-wpcom' ) ],
-		description: getTipDescription(
-			__(
-				'You can visit the the <a>Customizer</a> to edit the colors on your site.',
-				'jetpack-mu-wpcom'
-			),
-			{
-				a: <TipLink section="customizer" subsection="colors" />,
-			},
-			__( 'You can visit the the Customizer to edit the colors on your site.', 'jetpack-mu-wpcom' )
 		),
 		permission: 'settings',
 	},
