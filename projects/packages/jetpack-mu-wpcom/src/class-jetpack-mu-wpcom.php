3fc8c3c3a01e9eacc2b76d21dd4993de885dc6e5
Sergey Mitroshin <sergeymitr@gmail.com>
2024-07-12 14:46:10 -0400

    Google Analytics: initialize via MU-WPCOM plugin (#37622)
    Initialize Google Analytics package via MU-WPCOM plugin.

--- projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
+++ projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
@@ -109,6 +109,7 @@ class Jetpack_Mu_Wpcom {
 		require_once __DIR__ . '/features/wpcom-block-editor/functions.editor-type.php';
 		require_once __DIR__ . '/features/wpcom-sidebar-notice/wpcom-sidebar-notice.php';
 		require_once __DIR__ . '/features/wpcom-themes/wpcom-themes.php';
+		require_once __DIR__ . '/features/google-analytics/google-analytics.php';
 
 		// Initializers, if needed.
 		\Marketplace_Products_Updater::init();
