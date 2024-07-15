7b6c448f3eb89abe12bba2bc14fbdeb426b9ff71
Filipe Varela <keoshi@keoshi.com>
2024-07-15 14:53:04 +0100

    Connection screen: update the social icons area

--- projects/packages/my-jetpack/_inc/components/connection-screen/footer.tsx
+++ projects/packages/my-jetpack/_inc/components/connection-screen/footer.tsx
@@ -1,5 +1,6 @@
 import { __ } from '@wordpress/i18n';
 import appleLogo from './apple.svg';
+import githubLogo from './github.svg';
 import googleLogo from './google.svg';
 import styles from './styles.module.scss';
 import wordpressLogo from './wordpress.svg';
@@ -9,7 +10,10 @@ const ConnectionScreenFooter: React.FC = () => (
 		{ /* not using p here since connect screen apply styles for all p down the tree */ }
 		{ /* https://github.com/Automattic/jetpack/blob/trunk/projects/js-packages/connection/components/connect-screen/layout/style.scss#L49-L54 */ }
 		<div className={ styles[ 'account-description' ] }>
-			{ __( 'You can use your existing account on any of these services:', 'jetpack-my-jetpack' ) }
+			{ __(
+				'On the next screen, you can connect with an existing account from any of these services or create a new one.',
+				'jetpack-my-jetpack'
+			) }
 		</div>
 		{ /*
 				Since the list style type is set to none, `role=list` is required for VoiceOver (on Safari) to announce the list.
@@ -24,6 +28,9 @@ const ConnectionScreenFooter: React.FC = () => (
 			</li>
 			<li>
 				<img src={ appleLogo } className={ styles.apple } alt="Apple" />
+			</li>
+			<li>
+				<img src={ githubLogo } className={ styles.github } alt="GitHub" />
 			</li>
 		</ul>
 	</>
