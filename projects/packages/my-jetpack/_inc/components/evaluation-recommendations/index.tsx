27f82db7e37ca27a40adddf713d33df883a0de78
robertsreberski <robert.sreberski@automattic.com>
2024-07-17 17:56:38 +0200

    Add context menu to view layer

--- projects/packages/my-jetpack/_inc/components/evaluation-recommendations/index.tsx
+++ projects/packages/my-jetpack/_inc/components/evaluation-recommendations/index.tsx
@@ -1,7 +1,12 @@
 import { Container, Col, Text } from '@automattic/jetpack-components';
+import { DropdownMenu } from '@wordpress/components';
+import { Flex } from '@wordpress/components';
+import { FlexItem } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+import { moreHorizontalMobile } from '@wordpress/icons';
 import useEvaluationRecommendations from '../../data/evaluation-recommendations/use-evaluation-recommendations';
 import { JetpackModuleToProductCard } from '../product-cards-section/all';
+import styles from './style.module.scss';
 
 const EvaluationRecommendations: React.FC = () => {
 	const { isSectionVisible, recommendedModules, redoEvaluation, removeEvaluationResult } =
@@ -12,19 +17,36 @@ const EvaluationRecommendations: React.FC = () => {
 	}
 
 	return (
-		<Container horizontalGap={ 2 } horizontalSpacing={ 8 }>
+		<Container horizontalGap={ 2 } horizontalSpacing={ 6 }>
 			<Col>
-				<Text variant="headline-small">
-					{ __( 'Our recommendations for you', 'jetpack-my-jetpack' ) }
-				</Text>
-			</Col>
-			<Col>
-				<Text>
-					{ __(
-						'Here are the features that will best help you protect your site:',
-						'jetpack-my-jetpack'
-					) }
-				</Text>
+				<Flex>
+					<FlexItem>
+						<Text variant="headline-small" className={ styles.title }>
+							{ __( 'Our recommendations for you', 'jetpack-my-jetpack' ) }
+						</Text>
+						<Text>
+							{ __(
+								'Here are the features that will best help you protect your site:',
+								'jetpack-my-jetpack'
+							) }
+						</Text>
+					</FlexItem>
+					<FlexItem>
+						<DropdownMenu
+							menuProps={ { className: styles.dropdownMenu } }
+							popoverProps={ { position: 'bottom left' } }
+							icon={ moreHorizontalMobile }
+							label="Recommendations menu"
+							controls={ [
+								{
+									title: 'Redo',
+									onClick: redoEvaluation,
+								},
+								{ title: 'Hide', onClick: removeEvaluationResult },
+							] }
+						/>
+					</FlexItem>
+				</Flex>
 			</Col>
 			<Col>
 				<Container horizontalGap={ 4 } horizontalSpacing={ 2 } fluid>
