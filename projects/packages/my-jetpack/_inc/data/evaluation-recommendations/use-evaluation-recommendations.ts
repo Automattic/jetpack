63b0a3bf2ef47c7385c71e13dd222847c7671a21
robertsreberski <robert.sreberski@automattic.com>
2024-07-17 17:54:59 +0200

    Add delete endpoint, unify with others

--- projects/packages/my-jetpack/_inc/data/evaluation-recommendations/use-evaluation-recommendations.ts
+++ projects/packages/my-jetpack/_inc/data/evaluation-recommendations/use-evaluation-recommendations.ts
@@ -5,7 +5,7 @@ import {
 	QUERY_EVALUATE_KEY,
 	QUERY_SAVE_EVALUATION_KEY,
 	REST_API_EVALUATE_SITE_RECOMMENDATIONS,
-	REST_API_SAVE_EVALUATION_RECOMMENDATIONS,
+	REST_API_SITE_EVALUATION_RESULT,
 } from '../constants';
 import useSimpleMutation from '../use-simple-mutation';
 import { getMyJetpackWindowInitialState } from '../utils/get-my-jetpack-window-state';
@@ -32,10 +32,10 @@ const useEvaluationRecommendations = () => {
 	const { mutate: handleSaveEvaluationResult } = useSimpleMutation< JetpackModule[] >( {
 		name: QUERY_SAVE_EVALUATION_KEY,
 		query: {
-			path: REST_API_SAVE_EVALUATION_RECOMMENDATIONS,
+			path: REST_API_SITE_EVALUATION_RESULT,
 			method: 'POST',
 		},
-		errorMessage: __( 'Failed to save the evaluation. Please try again', 'jetpack-my-jetpack' ),
+		errorMessage: __( 'Failed to save evaluation results. Please try again', 'jetpack-my-jetpack' ),
 	} );
 
 	const submitEvaluation = useCallback(
