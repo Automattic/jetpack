d88fc410325e13ed3e164f20661bc83594d05609
Gabriel Demichelis <gabrieldemichelis@gmail.com>
2024-07-17 17:00:32 -0300

    Revert "Admin Bar: Make it consistent between Calypso and WP Admin regardless…" (#38389)
    This reverts commit ebfa7d323cd6e440401488641b7830c5ac545cf3.

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-admin-bar/wpcom-admin-bar.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-admin-bar/wpcom-admin-bar.php
@@ -9,7 +9,9 @@
 
 use Automattic\Jetpack\Jetpack_Mu_Wpcom;
 
-define( 'WPCOM_ADMIN_BAR_UNIFICATION', true );
+if ( get_option( 'wpcom_admin_interface' ) !== 'wp-admin' ) {
+	return;
+}
 
 /**
  * Enqueue assets needed by the WordPress.com admin bar.
