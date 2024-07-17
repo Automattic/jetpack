a7b59d6eb254e23f99aee94425143eab3017f407
Kevin Zoschke <kevin.zoschke@automattic.com>
2024-07-17 17:13:57 -0400

    Fix paths

--- projects/packages/classic-theme-helper/src/class-main.php
+++ projects/packages/classic-theme-helper/src/class-main.php
@@ -66,7 +66,7 @@ class Main {
 	 */
 	public static function jetpack_load_theme_tools() {
 		if ( current_theme_supports( 'tonesque' ) ) {
-			require_once __DIR__ . '../_inc/lib/tonesque.php';
+			require_once __DIR__ . '/../_inc/lib/tonesque.php';
 		}
 	}
 
--- projects/packages/classic-theme-helper/src/jetpack-color.php
+++ projects/packages/classic-theme-helper/src/jetpack-color.php
@@ -5,4 +5,4 @@
  * @package automattic/jetpack-classic-theme-helper
  */
 
-require_once __DIR__ . '/_inc/lib/class.color.php';
+require_once __DIR__ . '/../_inc/lib/class.color.php';
