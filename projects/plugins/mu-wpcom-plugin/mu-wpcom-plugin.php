cecbf3a1ea9aedc4dafc91589feb9a03da81bddf
Karen Attfield <karenlattfield@gmail.com>
2024-07-18 09:53:21 +0100

    Update proejct versions

--- projects/plugins/mu-wpcom-plugin/mu-wpcom-plugin.php
+++ projects/plugins/mu-wpcom-plugin/mu-wpcom-plugin.php
@@ -3,7 +3,7 @@
  *
  * Plugin Name: WordPress.com Features
  * Description: Test plugin for the jetpack-mu-wpcom package
- * Version: 2.4.1
+ * Version: 2.4.2-alpha
  * Author: Automattic
  * License: GPLv2 or later
  * Text Domain: jetpack-mu-wpcom-plugin
