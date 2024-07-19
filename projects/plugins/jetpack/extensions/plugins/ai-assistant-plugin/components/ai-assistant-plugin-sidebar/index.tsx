748356972cd3d56367561d855499c3a26acf40f6
Luiz Kowalski <lhkowalski@gmail.com>
2024-07-19 07:09:57 -0300

    Add experimental shortcut for the logo generator so we can test the package

--- projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/ai-assistant-plugin-sidebar/index.tsx
+++ projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/ai-assistant-plugin-sidebar/index.tsx
@@ -1,13 +1,15 @@
 /**
  * External dependencies
  */
+import { GeneratorModal } from '@automattic/jetpack-ai-client';
 import { JetpackEditorPanelLogo } from '@automattic/jetpack-shared-extension-utils';
 import { useAnalytics } from '@automattic/jetpack-shared-extension-utils';
-import { PanelBody, PanelRow, BaseControl } from '@wordpress/components';
+import { PanelBody, PanelRow, BaseControl, Button } from '@wordpress/components';
 import { store as coreStore } from '@wordpress/core-data';
 import { useSelect } from '@wordpress/data';
 import { PluginPrePublishPanel, PluginDocumentSettingPanel } from '@wordpress/edit-post';
 import { store as editorStore } from '@wordpress/editor';
+import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import debugFactory from 'debug';
 import React from 'react';
@@ -50,13 +52,23 @@ const isAITitleOptimizationAvailable =
 	window?.Jetpack_Editor_Initial_State?.available_blocks?.[ 'ai-title-optimization' ]?.available ||
 	false;
 
+const siteDetails = {
+	ID: parseInt( window?.Jetpack_Editor_Initial_State?.wpcomBlogId ),
+	URL: window?.Jetpack_Editor_Initial_State?.siteFragment,
+	domain: window?.Jetpack_Editor_Initial_State?.siteFragment,
+	name: '',
+	description: '',
+};
+
 const JetpackAndSettingsContent = ( {
 	placement,
 	requireUpgrade,
 	upgradeType,
 }: JetpackSettingsContentProps ) => {
 	const isBreveAvailable = getFeatureAvailability( 'ai-proofread-breve' );
+	const isLogoGeneratorAvailable = getFeatureAvailability( 'ai-assistant-site-logo-support' );
 	const { checkoutUrl } = useAICheckout();
+	const [ showLogoGeneratorModal, setShowLogoGeneratorModal ] = useState( false );
 
 	return (
 		<>
@@ -66,6 +78,29 @@ const JetpackAndSettingsContent = ( {
 					<Proofread placement={ placement } busy={ false } disabled={ requireUpgrade } />
 				</BaseControl>
 			</PanelRow>
+
+			{ isLogoGeneratorAvailable && (
+				<PanelRow className="jetpack-ai-logo-generator-control__header">
+					<BaseControl label={ __( 'AI Logo Generator', 'jetpack' ) }>
+						<p>
+							{ __(
+								'Experimental panel to trigger the logo generator modal. Will be dropped after the extension is ready.',
+								'jetpack'
+							) }
+						</p>
+						<Button variant="secondary" onClick={ () => setShowLogoGeneratorModal( true ) }>
+							{ __( 'Generate Logo', 'jetpack' ) }
+						</Button>
+						<GeneratorModal
+							isOpen={ showLogoGeneratorModal }
+							onClose={ () => setShowLogoGeneratorModal( false ) }
+							context="jetpack-ai-sidebar"
+							siteDetails={ siteDetails }
+						/>
+					</BaseControl>
+				</PanelRow>
+			) }
+
 			{ isAITitleOptimizationAvailable && (
 				<PanelRow className="jetpack-ai-title-optimization__header">
 					<BaseControl label={ __( 'Optimize Publishing', 'jetpack' ) }>
