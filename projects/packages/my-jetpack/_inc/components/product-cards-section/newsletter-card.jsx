f702051e9c767a6a3a49be8f9b1734e4f7e4abb3
Ian Ramos <5714212+IanRamosC@users.noreply.github.com>
2024-07-19 19:59:21 -0300

    Add override for other common Newsletter statuses

--- projects/packages/my-jetpack/_inc/components/product-cards-section/newsletter-card.jsx
+++ projects/packages/my-jetpack/_inc/components/product-cards-section/newsletter-card.jsx
@@ -1,15 +1,28 @@
+import { __ } from '@wordpress/i18n';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { PRODUCT_STATUSES } from '../../constants';
 import ProductCard from '../connected-product-card';
 
+const NEWSLETTER_SETTINGS_PAGE_URL = 'admin.php?page=jetpack#/newsletter';
+
 const NewsletterCard = ( { admin } ) => {
 	const actionOverride = {
 		[ PRODUCT_STATUSES.NEEDS_PURCHASE_OR_FREE ]: {
-			href: 'admin.php?page=jetpack#/newsletter',
+			href: NEWSLETTER_SETTINGS_PAGE_URL,
 		},
 		[ PRODUCT_STATUSES.NEEDS_FIRST_SITE_CONNECTION ]: {
-			href: 'admin.php?page=jetpack#/newsletter',
+			href: NEWSLETTER_SETTINGS_PAGE_URL,
+		},
+		[ PRODUCT_STATUSES.INACTIVE ]: {
+			href: NEWSLETTER_SETTINGS_PAGE_URL,
+			label: __( 'View', 'jetpack-my-jetpack' ),
+			onClick: () => {},
+		},
+		[ PRODUCT_STATUSES.MODULE_DISABLED ]: {
+			href: NEWSLETTER_SETTINGS_PAGE_URL,
+			label: __( 'View', 'jetpack-my-jetpack' ),
+			onClick: () => {},
 		},
 	};
 
