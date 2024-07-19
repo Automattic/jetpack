5449debf252655e4a24c7591546b6c1715fdc761
Peter Petrov <peter.petrov89@gmail.com>
2024-07-19 15:55:51 +0300

    Add rollup for browser version (not working properly)

---
+++ projects/js-packages/critical-css-gen/rollup.config.js
@@ -0,0 +1,50 @@
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+import { terser } from 'rollup-plugin-terser';
+
+const sharedPlugins = [
+	resolve( { browser: true, preferBuiltins: false } ),
+	typescript( {
+		sourceMap: true,
+		inlineSources: false,
+		declaration: false,
+	} ),
+	commonjs( {
+		transformMixedEsModules: true,
+	} ),
+	nodePolyfills(),
+	json(),
+];
+
+export default {
+	input: 'src/index.ts',
+	output: [
+		{
+			sourcemap: true,
+			format: 'iife',
+			name: 'CriticalCSSGenerator',
+			file: 'dist/bundle.full.js',
+			globals: {
+				'node-fetch': 'fetch',
+			},
+		},
+		{
+			sourcemap: true,
+			format: 'iife',
+			name: 'CriticalCSSGenerator',
+			file: 'dist/bundle.js',
+			plugins: [ terser() ],
+			globals: {
+				'node-fetch': 'fetch',
+			},
+		},
+	],
+	external: [ 'node-fetch' ],
+	plugins: sharedPlugins,
+	watch: {
+		clearScreen: false,
+	},
+};
