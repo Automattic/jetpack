3e89b77553e082176b0204978dac004f358a0adc
Ian Ramos <5714212+IanRamosC@users.noreply.github.com>
2024-07-22 16:06:20 -0300

    My Jetpack: add A/A experiment (#38327)
    * My Jetpack: add ExPlat package
    
    * My Jetpack: initialize ExPlat package
    
    * changelog
    
    * Fix project versions
    
    * Add A/A experiment
    
    * Fix project versions
    
    * Prevent fetching experiment if Tracks is disabled
    
    * changelog

--- projects/packages/my-jetpack/src/class-initializer.php
+++ projects/packages/my-jetpack/src/class-initializer.php
@@ -16,6 +16,7 @@ use Automattic\Jetpack\Connection\Initial_State as Connection_Initial_State;
 use Automattic\Jetpack\Connection\Manager as Connection_Manager;
 use Automattic\Jetpack\Connection\Rest_Authentication as Connection_Rest_Authentication;
 use Automattic\Jetpack\Constants as Jetpack_Constants;
+use Automattic\Jetpack\ExPlat;
 use Automattic\Jetpack\JITMS\JITM;
 use Automattic\Jetpack\Licensing;
 use Automattic\Jetpack\Modules;
@@ -110,6 +111,9 @@ class Initializer {
 		add_action( 'admin_init', array( __CLASS__, 'setup_historically_active_jetpack_modules_sync' ) );
 		// This is later than the admin-ui package, which runs on 1000
 		add_action( 'admin_init', array( __CLASS__, 'maybe_show_red_bubble' ), 1001 );
+
+		//  Set up the ExPlat package endpoints
+		ExPlat::init();
 
 		// Sets up JITMS.
 		JITM::configure();
