baf43c21a22e60b0a0283f27b45fce0dc7cbe46e
dkmyta <dkmyta10@gmail.com>
2024-07-22 13:41:29 -0700

    Use supported phpunit method

--- projects/packages/waf/tests/php/unit/test-waf-blocklog-manager.php
+++ projects/packages/waf/tests/php/unit/test-waf-blocklog-manager.php
@@ -28,7 +28,7 @@ final class WafBlocklogManagerTest extends PHPUnit\Framework\TestCase {
 		$file_content = file_get_contents( $waf_log_path );
 
 		$this->assertTrue( file_exists( $waf_log_path ) );
-		$this->assertStringContainsString( '{"rule_id":"1337","reason":"test block"', $file_content );
+		$this->assertFalse( strpos( $file_content, '{"rule_id":"1337","reason":"test block"' ) === false );
 
 		unlink( $waf_log_path );
 	}
