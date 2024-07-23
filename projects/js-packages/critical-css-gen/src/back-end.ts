1389dcf36310a82a5fdfba8c5d5fea25d57e5184
Peter Petrov <peter.petrov89@gmail.com>
2024-07-23 17:05:50 +0300

    Add back-end/node build

---
+++ projects/js-packages/critical-css-gen/src/back-end.ts
@@ -0,0 +1,9 @@
+export { BrowserInterfacePuppeteer } from './browser-interface-puppeteer.js';
+export { BrowserInterfacePlaywright } from './browser-interface-playwright.js';
+export { BrowserInterfaceIframe } from './browser-interface-iframe.js';
+export { BrowserInterface } from './browser-interface.js';
+export { generateCriticalCSS } from './generate-critical-css.js';
+
+export * from './errors.js';
+
+export const version = '0.0.11';
--- projects/js-packages/critical-css-gen/src/index.ts
+++
@@ -1,9 +0,0 @@
-export { BrowserInterfacePuppeteer } from './browser-interface-puppeteer';
-export { BrowserInterfacePlaywright } from './browser-interface-playwright';
-export { BrowserInterfaceIframe } from './browser-interface-iframe';
-export { BrowserInterface } from './browser-interface';
-export { generateCriticalCSS } from './generate-critical-css';
-
-export * from './errors';
-
-export const version = '0.0.11';
