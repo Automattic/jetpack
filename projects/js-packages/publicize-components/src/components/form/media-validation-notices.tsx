7490e321adea7e5c5b16312033bc4ddb3fe48ced
Manzoor Wani <manzoorwani.jk@gmail.com>
2024-07-24 17:50:00 +0530

    Create media requirements notice

--- projects/js-packages/publicize-components/src/components/form/media-validation-notices.tsx
+++ projects/js-packages/publicize-components/src/components/form/media-validation-notices.tsx
@@ -2,9 +2,11 @@ import useAttachedMedia from '../../hooks/use-attached-media';
 import useFeaturedImage from '../../hooks/use-featured-image';
 import useImageGeneratorConfig from '../../hooks/use-image-generator-config';
 import useMediaDetails from '../../hooks/use-media-details';
-import useMediaRestrictions, { NO_MEDIA_ERROR } from '../../hooks/use-media-restrictions';
+import useMediaRestrictions from '../../hooks/use-media-restrictions';
+import { NO_MEDIA_ERROR } from '../../hooks/use-media-restrictions/constants';
 import useSocialMediaConnections from '../../hooks/use-social-media-connections';
 import { InstagramNoMediaNotice } from './instagram-no-media-notice';
+import { MediaRequirementsNotice } from './media-requirements-notice';
 
 export const MediaValidationNotices: React.FC = () => {
 	const { connections } = useSocialMediaConnections();
@@ -13,9 +15,13 @@ export const MediaValidationNotices: React.FC = () => {
 	const featuredImageId = useFeaturedImage();
 
 	const mediaId = attachedMedia[ 0 ]?.id || featuredImageId;
-	const { validationErrors } = useMediaRestrictions( connections, useMediaDetails( mediaId )[ 0 ], {
-		isSocialImageGeneratorEnabledForPost,
-	} );
+	const { validationErrors, isConvertible } = useMediaRestrictions(
+		connections,
+		useMediaDetails( mediaId )[ 0 ],
+		{
+			isSocialImageGeneratorEnabledForPost,
+		}
+	);
 
 	const invalidIds = Object.keys( validationErrors );
 
@@ -25,6 +31,10 @@ export const MediaValidationNotices: React.FC = () => {
 
 	if ( Object.values( validationErrors ).includes( NO_MEDIA_ERROR ) ) {
 		return <InstagramNoMediaNotice />;
+	}
+
+	if ( ! isConvertible ) {
+		return <MediaRequirementsNotice validationErrors={ validationErrors } />;
 	}
 
 	return null;
