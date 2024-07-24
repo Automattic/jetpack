3eac4784517928ed28267362c994b6f69d04b2e8
Miguel <miguel.san.segundo@automattic.com>
2024-07-24 13:20:08 +0700

    Remove typing to avoid adding peer dependency

--- projects/packages/jetpack-mu-wpcom/src/features/starter-page-templates/store.ts
+++ projects/packages/jetpack-mu-wpcom/src/features/starter-page-templates/store.ts
@@ -1,10 +1,8 @@
 import { register, createReduxStore } from '@wordpress/data';
-import type { Reducer } from 'redux';
 
 type OpenState = 'CLOSED' | 'OPEN_FROM_ADD_PAGE' | 'OPEN_FOR_BLANK_CANVAS';
-type Action = ReturnType< typeof actions.setOpenState >;
 
-const reducer: Reducer< OpenState, Action > = ( state = 'CLOSED', { type, ...action } ) =>
+const reducer = ( state = 'CLOSED', { type, ...action } ) =>
 	'SET_IS_OPEN' === type ? action.openState : state;
 
 const actions = {
