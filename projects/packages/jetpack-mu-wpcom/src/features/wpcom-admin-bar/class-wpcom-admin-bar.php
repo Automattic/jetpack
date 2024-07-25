1b4f6c1663fd32a07e0a48499b564d037f192c53
DustyReagan <dusty@dustyreagan.com>
2024-07-24 19:04:23 -0500

    Bail if not an array

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-admin-bar/class-wpcom-admin-bar.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-admin-bar/class-wpcom-admin-bar.php
@@ -60,12 +60,7 @@ class WPCOM_Admin_Bar extends \WP_Admin_Bar {
 	 * }
 	 */
 	public function add_node( $args ) {
-		// Ensure $args is an array.
-		if ( is_object( $args ) ) {
-			$args = (array) $args;
-		}
-
-		if ( empty( $args['href'] ) ) {
+		if ( ! is_array( $args ) || empty( $args['href'] ) ) {
 			parent::add_node( $args );
 			return;
 		}
