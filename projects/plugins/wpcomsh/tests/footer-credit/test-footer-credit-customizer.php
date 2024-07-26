6d2994d4fa6359deb2e70f383e07095f7fa649fc
mmtr <1233880+mmtr@users.noreply.github.com>
2024-07-26 17:14:39 +0200

    Validate that action remains registered after switching theme

--- projects/plugins/wpcomsh/tests/footer-credit/test-footer-credit-customizer.php
+++ projects/plugins/wpcomsh/tests/footer-credit/test-footer-credit-customizer.php
@@ -17,7 +17,9 @@ class FooterCreditCustomizerTest extends WP_UnitTestCase {
 	 */
 	public function test_wpcomsh_footer_credit_customizer_child_block_theme() {
 		$this->assertFalse( wp_is_block_theme() );
+		$this->assertSame( 99, has_action( 'customize_register', 'footercredits_register' ) );
 		switch_theme( 'block-theme' );
 		$this->assertTrue( wp_is_block_theme() );
+		$this->assertSame( 99, has_action( 'customize_register', 'footercredits_register' ) );
 	}
 }
