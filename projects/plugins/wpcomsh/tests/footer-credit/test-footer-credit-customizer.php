57b3decbd9f54d1b3195ee0fde1f888914ddee52
mmtr <1233880+mmtr@users.noreply.github.com>
2024-07-26 17:04:54 +0200

    Remove core test setup

--- projects/plugins/wpcomsh/tests/footer-credit/test-footer-credit-customizer.php
+++ projects/plugins/wpcomsh/tests/footer-credit/test-footer-credit-customizer.php
@@ -9,57 +9,9 @@
  * Class FooterCreditCustomizerTest.
  */
 class FooterCreditCustomizerTest extends WP_UnitTestCase {
-
-	/**
-	 * Theme root directory.
-	 *
-	 * @var string
-	 */
-	const THEME_ROOT = DIR_TESTDATA . '/themedir1';
-
-	/**
-	 * Original theme directory.
-	 *
-	 * @var string
-	 */
-	private $orig_theme_dir;
-
 	public function set_up() {
 		parent::set_up();
-
-		$this->orig_theme_dir            = $GLOBALS['wp_theme_directories'];
-		$GLOBALS['wp_theme_directories'] = array( WP_CONTENT_DIR . '/themes', self::THEME_ROOT );
-
-		add_filter( 'theme_root', array( $this, 'filter_theme_root' ) );
-		add_filter( 'stylesheet_root', array( $this, 'filter_theme_root' ) );
-		add_filter( 'template_root', array( $this, 'filter_theme_root' ) );
-
-		// Clear caches.
-		wp_clean_themes_cache();
-		unset( $GLOBALS['wp_themes'] );
-
-		// Remove Footer Credit action in case it was previously registered.
 		switch_theme( 'block-theme' );
-	}
-
-	public function tear_down() {
-		$GLOBALS['wp_theme_directories'] = $this->orig_theme_dir;
-		remove_filter( 'theme_root', array( $this, 'filter_theme_root' ) );
-		remove_filter( 'stylesheet_root', array( $this, 'filter_theme_root' ) );
-		remove_filter( 'template_root', array( $this, 'filter_theme_root' ) );
-
-		wp_clean_themes_cache();
-		unset( $GLOBALS['wp_themes'] );
-		parent::tear_down();
-	}
-
-	/**
-	 * Replace the normal theme root directory with our premade test directory.
-	 *
-	 * @return string Replaced theme root.
-	 */
-	public function filter_theme_root() {
-		return self::THEME_ROOT;
 	}
 
 	/**
