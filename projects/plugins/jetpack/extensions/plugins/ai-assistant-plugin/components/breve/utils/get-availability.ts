276e169702d18ebb0bf54e926b844525ad539b7d
Douglas <douglas.henri@automattic.com>
2024-07-26 16:29:37 -0300

    update comment

--- projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/breve/utils/get-availability.ts
+++ projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/breve/utils/get-availability.ts
@@ -2,7 +2,7 @@ import { getFeatureAvailability } from '../../../../../blocks/ai-assistant/lib/u
 
 const blogId = parseInt( window?.Jetpack_Editor_Initial_State?.wpcomBlogId );
 
-// Enable backend prompts for beta sites + 10% of production sites.
+// Enable backend prompts for beta sites + 20% of production sites.
 const isBreveAvailable =
 	getFeatureAvailability( 'ai-proofread-breve' ) || [ 0, 6 ].includes( blogId % 10 );
 
