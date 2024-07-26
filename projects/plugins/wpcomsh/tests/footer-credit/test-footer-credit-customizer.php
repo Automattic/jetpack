f6f33d2da302c59d436baa18c9f368d16bffbd87
mmtr <1233880+mmtr@users.noreply.github.com>
2024-07-26 15:56:45 +0200

    Footer credit: Add unit tests

---
+++ projects/plugins/wpcomsh/tests/footer-credit/test-footer-credit-customizer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Footer Credit Customizer Test file.
+ *
+ * @package wpcomsh
+ */
+
+/**
+ * Class FooterCreditCustomizerTest.
+ */
+class FooterCreditCustomizerTest extends WP_UnitTestCase {
+
+	/**
+	 * Checks that the Footer Credit customizer settings and controls are not registered
+	 * for block themes.
+	 */
+	public function test_wpcomsh_footer_credit_customizer_block_theme() {
+		$this->assertTrue( true );
+	}
+}
