ed49ad80164e4976a5a9c4c39d3c42681dc9a88f
mmtr <1233880+mmtr@users.noreply.github.com>
2024-07-26 16:19:09 +0200

    Basic test

--- projects/plugins/wpcomsh/tests/footer-credit/test-footer-credit-customizer.php
+++ projects/plugins/wpcomsh/tests/footer-credit/test-footer-credit-customizer.php
@@ -11,10 +11,21 @@
 class FooterCreditCustomizerTest extends WP_UnitTestCase {
 
 	/**
-	 * Checks that the Footer Credit customizer settings and controls are not registered
-	 * for block themes.
+	 * Set up.
 	 */
-	public function test_wpcomsh_footer_credit_customizer_block_theme() {
-		$this->assertTrue( false );
+	public function set_up() {
+		parent::set_up();
+		switch_theme( 'block-theme-child' );
+		do_action( 'setup_theme' );
+	}
+
+	/**
+	 * Checks that the Footer Credit customizer settings and controls are not registered
+	 * for child block themes without causing a fatal error.
+	 *
+	 * @see p1721946083481019-slack-C02FMH4G8
+	 */
+	public function test_wpcomsh_footer_credit_customizer_child_block_theme() {
+		$this->assertFalse( has_action( 'customize_register', 'footercredits_register' ) );
 	}
 }
