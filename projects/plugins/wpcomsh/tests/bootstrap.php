fd373c2dd52f376ea408bf06f3f7f0b54f09c545
mmtr <1233880+mmtr@users.noreply.github.com>
2024-07-29 13:19:26 +0200

    Fix patchwork path

--- projects/plugins/wpcomsh/tests/bootstrap.php
+++ projects/plugins/wpcomsh/tests/bootstrap.php
@@ -11,7 +11,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // Preloading the file to reconcile Brain\Monkey with WordPress Core.
-require_once __DIR__ . '/../../vendor/antecedent/patchwork/Patchwork.php';
+require_once __DIR__ . '/../vendor/antecedent/patchwork/Patchwork.php';
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 $_core_dir  = getenv( 'WP_CORE_DIR' );
