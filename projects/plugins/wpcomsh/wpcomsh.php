5e7e4e97555d195ae8ad1793e3e3894e8da8855d
Nate Weller <hello@nateweller.com>
2024-07-30 13:12:42 -0600

    Restore project versions

--- projects/plugins/wpcomsh/wpcomsh.php
+++ projects/plugins/wpcomsh/wpcomsh.php
@@ -2,14 +2,14 @@
 /**
  * Plugin Name: WordPress.com Site Helper
  * Description: A helper for connecting WordPress.com sites to external host infrastructure.
- * Version: 5.1.2-alpha
+ * Version: 5.1.1
  * Author: Automattic
  * Author URI: http://automattic.com/
  *
  * @package wpcomsh
  */
 
-define( 'WPCOMSH_VERSION', '5.1.2-alpha' );
+define( 'WPCOMSH_VERSION', '5.1.1' );
 
 // If true, Typekit fonts will be available in addition to Google fonts
 add_filter( 'jetpack_fonts_enable_typekit', '__return_true' );
