f4ec24e7db11f3807be7f0c30abf200139084a66
Miguel Lezama <lezama@gmail.com>
2024-08-02 15:38:13 -0300

    improvements to icon and buttons

--- projects/plugins/jetpack/extensions/blocks/subscriptions/menu.js
+++ projects/plugins/jetpack/extensions/blocks/subscriptions/menu.js
@@ -1,10 +1,21 @@
 import apiFetch from '@wordpress/api-fetch';
-import { Button, Modal } from '@wordpress/components';
+import { Button, ButtonGroup, Modal, PanelBody, Path, SVG } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { PluginSidebar } from '@wordpress/edit-post';
 import { __ } from '@wordpress/i18n';
-import { atSymbol } from '@wordpress/icons';
+import { send as sendIcon } from '@wordpress/icons';
 import { useState, useCallback, useEffect } from 'react';
+
+// Fallback SVG for the send icon that will be released with Gutenberg 19.0
+const sendIconSvg = (
+	<SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+		<Path
+			fillRule="evenodd"
+			clipRule="evenodd"
+			d="M6.332 5.748c-1.03-.426-2.06.607-1.632 1.636l1.702 3.93 7.481.575c.123.01.123.19 0 .2l-7.483.575-1.7 3.909c-.429 1.029.602 2.062 1.632 1.636l12.265-5.076c1.03-.426 1.03-1.884 0-2.31L6.332 5.748Z"
+		/>
+	</SVG>
+);
 
 const NewsletterMenu = () => {
 	const [ isModalOpen, setIsModalOpen ] = useState( false );
@@ -75,15 +86,34 @@ const NewsletterMenu = () => {
 			<PluginSidebar
 				name="newsletter-settings-sidebar"
 				title={ __( 'Newsletter', 'jetpack' ) }
-				icon={ atSymbol }
+				icon={ sendIcon || sendIconSvg }
 			>
-				<Button onClick={ openModal } isPrimary>
-					{ __( 'Email Preview', 'jetpack' ) }
-				</Button>
+				<PanelBody>
+					<p>
+						{ __(
+							'Ensure your email looks perfect. Use the buttons below to view a preview or send a test email.',
+							'jetpack'
+						) }
+					</p>
+					<ButtonGroup>
+						<Button
+							onClick={ openModal }
+							style={ {
+								marginRight: '18px',
+							} }
+							variant="secondary"
+						>
+							{ __( 'Preview email', 'jetpack' ) }
+						</Button>
+						<Button onClick={ openModal } variant="secondary">
+							{ __( 'Send a test', 'jetpack' ) }
+						</Button>
+					</ButtonGroup>
+				</PanelBody>
 			</PluginSidebar>
 			{ isModalOpen && (
 				<Modal
-					title={ __( 'Email Preview', 'jetpack' ) }
+					title={ __( 'Preview email', 'jetpack' ) }
 					onRequestClose={ closeModal }
 					isFullScreen={ true }
 				>
