2c5616baacfd82538db53fd179891acc8fe76ca1
arthur791004 <arthur.chu@automattic.com>
2024-08-02 09:49:27 +0800

    MU WPCOM: Port Dotcom FSE feature from ETK (#38212)
    * MU WPCOM: Port FSE feature from ETK
    
    * changelog
    
    * Fix some errors
    
    * Use functions from Wpcom_Legacy_FSE
    
    * changelog
    
    * Fix lint
    
    * Clean up site-credit block as it's not in use
    
    * Track whether the legacy fse blocks are rendered
    
    * Fix lint

--- projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
+++ projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
@@ -165,6 +165,7 @@ class Jetpack_Mu_Wpcom {
 		require_once __DIR__ . '/features/wpcom-blocks/timeline/timeline.php';
 		require_once __DIR__ . '/features/wpcom-documentation-links/wpcom-documentation-links.php';
 		require_once __DIR__ . '/features/wpcom-global-styles/index.php';
+		require_once __DIR__ . '/features/wpcom-legacy-fse/wpcom-legacy-fse.php';
 		require_once __DIR__ . '/features/wpcom-whats-new/wpcom-whats-new.php';
 		require_once __DIR__ . '/features/starter-page-templates/class-starter-page-templates.php';
 	}
