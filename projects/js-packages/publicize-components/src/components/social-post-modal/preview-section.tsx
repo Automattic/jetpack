7a253a0a5138009ecceb711f33e8a924fc610e36
Manzoor Wani <manzoorwani.jk@gmail.com>
2024-08-02 14:14:17 +0530

    A little bit of readability improvement

--- projects/js-packages/publicize-components/src/components/social-post-modal/preview-section.tsx
+++ projects/js-packages/publicize-components/src/components/social-post-modal/preview-section.tsx
@@ -1,7 +1,6 @@
 import { TabPanel } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { store as socialStore } from '../../social-store';
-import { Connection } from '../../social-store/types';
 import ConnectionIcon from '../connection-icon';
 import styles from './styles.module.scss';
 
@@ -11,36 +10,35 @@ import styles from './styles.module.scss';
  * @returns {import('react').ReactNode} - Preview section of the social post modal.
  */
 export function PreviewSection() {
-	const connections = useSelect(
-		select =>
-			select( socialStore )
-				.getConnections()
-				.map( connection => {
-					const title = connection.display_name || connection.external_display;
+	const connections = useSelect( select => {
+		const store = select( socialStore );
 
-					return {
-						...connection,
-						name: `${ connection.service_name }-${ connection.connection_id }`,
-						title,
-						icon: (
-							<ConnectionIcon
-								label={ title }
-								serviceName={ connection.service_name }
-								profilePicture={ connection.profile_picture }
-							/>
-						),
-					};
-				} ),
-		[]
-	);
+		return store.getConnections().map( connection => {
+			const title = connection.display_name || connection.external_display;
+			const name = `${ connection.service_name }-${ connection.connection_id }`;
+			const icon = (
+				<ConnectionIcon
+					label={ title }
+					serviceName={ connection.service_name }
+					profilePicture={ connection.profile_picture }
+				/>
+			);
+
+			return {
+				...connection,
+				// Add the props needed for the TabPanel component
+				name,
+				title,
+				icon,
+			};
+		} );
+	}, [] );
 
 	return (
 		<div className={ styles[ 'preview-section' ] }>
 			<TabPanel tabs={ connections }>
-				{ ( connection: Connection ) => (
-					<div className={ styles[ 'preview-content' ] }>
-						Content for { connection.display_name || connection.external_display }
-					</div>
+				{ ( tab: ( typeof connections )[ number ] ) => (
+					<div className={ styles[ 'preview-content' ] }>Content for { tab.title }</div>
 				) }
 			</TabPanel>
 		</div>
