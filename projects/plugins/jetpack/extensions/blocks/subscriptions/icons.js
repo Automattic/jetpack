8d2c70e0bf1295ff836f78dcc698b3c4ebedce33
Miguel Lezama <lezama@gmail.com>
2024-08-06 15:22:02 -0300

    fix svg

--- projects/plugins/jetpack/extensions/blocks/subscriptions/icons.js
+++ projects/plugins/jetpack/extensions/blocks/subscriptions/icons.js
@@ -1,7 +1,7 @@
 import { Path, SVG } from '@wordpress/components';
 
 export const SendIcon = () => (
-	<SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+	<SVG width="24" height="24" viewBox="0 0 24 24">
 		<Path
 			fillRule="evenodd"
 			clipRule="evenodd"
