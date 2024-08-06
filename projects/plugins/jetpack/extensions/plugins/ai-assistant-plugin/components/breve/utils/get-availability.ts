36373babe0ec2eadfa0e0ca13bdb18340bf0ca1e
Douglas <douglas.henri@automattic.com>
2024-08-06 14:43:01 -0300

    disable Breve for free plans and when block is disabled

--- projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/breve/utils/get-availability.ts
+++ projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/breve/utils/get-availability.ts
@@ -1,5 +1,28 @@
+/**
+ * External dependencies
+ */
+import { select } from '@wordpress/data';
+/**
+ * Internal dependencies
+ */
 import { getFeatureAvailability } from '../../../../../blocks/ai-assistant/lib/utils/get-feature-availability';
 
-const isBreveAvailable = getFeatureAvailability( 'ai-proofread-breve' );
+export function getBreveAvailability( isFreePlan: boolean ) {
+	// Breve is not available for free plans.
+	if ( isFreePlan ) {
+		return false;
+	}
 
-export default isBreveAvailable;
+	const { getHiddenBlockTypes } = select( 'core/edit-post' ) || {};
+	const hiddenBlocks = getHiddenBlockTypes?.() || []; // It will assume the block is not hidden if the function is undefined.
+
+	// Not enabled if the AI Assistant block is hidden.
+	if ( hiddenBlocks.includes( 'jetpack/ai-assistant' ) ) {
+		return false;
+	}
+
+	// Not enabled if the feature flag is intentionally disabled.
+	return getFeatureAvailability( 'ai-proofread-breve' );
+}
+
+export default getBreveAvailability;
