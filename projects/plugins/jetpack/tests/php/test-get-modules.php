5f5bab0d91f95bf411091f5c90e1039911880e90
Igor Zinovyev <zinigor@gmail.com>
2024-08-07 18:04:35 +0300

    Added connection filter to satisfy subscriptions.

--- projects/plugins/jetpack/tests/php/test-get-modules.php
+++ projects/plugins/jetpack/tests/php/test-get-modules.php
@@ -188,7 +188,9 @@ class WP_Test_Get_Modules extends WP_UnitTestCase {
 		Jetpack_Options::update_option( 'id', '123' );
 
 		add_filter( 'jetpack_no_user_testing_mode', '__return_true' );
+		add_filter( 'jetpack_is_connection_ready', '__return_true', 1000 );
 		$this->assertSame( 'Requires a connected WordPress.com account', Jetpack_Admin::get_module_unavailable_reason( $dummy_module ) );
+		remove_filter( 'jetpack_is_connection_ready', '__return_true', 1000 );
 		remove_filter( 'jetpack_no_user_testing_mode', '__return_true' );
 		// Mock a user connection.
 		$user = self::factory()->user->create_and_get(
