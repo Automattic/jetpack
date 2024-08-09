987ada54fe01f8a0b8acc332367a6263ef592ad1
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2024-08-09 14:35:46 -0600

    Update time format to accomodate years

--- projects/packages/my-jetpack/_inc/utils/format-time.ts
+++ projects/packages/my-jetpack/_inc/utils/format-time.ts
@@ -4,6 +4,11 @@ const formatTime: FormatTimeFunction = ( seconds: number ) => {
 	const minutes = Math.floor( seconds / 60 );
 	const hours = Math.floor( minutes / 60 );
 	const days = Math.floor( hours / 24 );
+	const years = Math.floor( days / 365 );
+
+	if ( years > 0 ) {
+		return `${ years }y ${ days % 365 }d`;
+	}
 
 	if ( days > 0 ) {
 		return `${ days }d ${ hours % 24 }h`;
