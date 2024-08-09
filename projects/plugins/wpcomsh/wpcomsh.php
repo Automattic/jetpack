ed391beeea3526b472cde3c318fc682230c4bd21
Miguel Torres <1233880+mmtr@users.noreply.github.com>
2024-08-09 15:15:25 +0200

    Changelog and readme.txt edits. (#38803)

--- projects/plugins/wpcomsh/wpcomsh.php
+++ projects/plugins/wpcomsh/wpcomsh.php
@@ -2,14 +2,14 @@
 /**
  * Plugin Name: WordPress.com Site Helper
  * Description: A helper for connecting WordPress.com sites to external host infrastructure.
- * Version: 5.3.1-alpha
+ * Version: 5.3.1
  * Author: Automattic
  * Author URI: http://automattic.com/
  *
  * @package wpcomsh
  */
 
-define( 'WPCOMSH_VERSION', '5.3.1-alpha' );
+define( 'WPCOMSH_VERSION', '5.3.1' );
 
 // If true, Typekit fonts will be available in addition to Google fonts
 add_filter( 'jetpack_fonts_enable_typekit', '__return_true' );
