1f5bef01199868bc6017c4d9cdae05500b1c1264
Gergely Juhasz <gergely.juhasz@automattic.com>
2024-08-12 11:37:34 +0200

    Extract to component

--- projects/js-packages/publicize-components/src/components/form/index.tsx
+++ projects/js-packages/publicize-components/src/components/form/index.tsx
@@ -9,18 +9,16 @@
 import { Disabled, PanelRow } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { Fragment } from '@wordpress/element';
-import { __ } from '@wordpress/i18n';
 import { usePublicizeConfig } from '../../..';
 import useSocialMediaConnections from '../../hooks/use-social-media-connections';
 import { store as socialStore } from '../../social-store';
 import { ThemedConnectionsModal as ManageConnectionsModal } from '../manage-connections-modal';
 import { SocialPostModal } from '../social-post-modal/modal';
 import { AdvancedPlanNudge } from './advanced-plan-nudge';
+import { ConnectionNotice } from './connection-notice';
 import { ConnectionsList } from './connections-list';
-import { SettingsButton } from './settings-button';
 import { ShareCountInfo } from './share-count-info';
 import { SharePostForm } from './share-post-form';
-import styles from './styles.module.scss';
 
 /**
  * The Publicize form component. It contains the connection list, and the message box.
@@ -29,12 +27,7 @@ import styles from './styles.module.scss';
  */
 export default function PublicizeForm() {
 	const { hasConnections, hasEnabledConnections } = useSocialMediaConnections();
-	const {
-		isPublicizeEnabled,
-		isPublicizeDisabledBySitePlan,
-		needsUserConnection,
-		userConnectionUrl,
-	} = usePublicizeConfig();
+	const { isPublicizeEnabled, isPublicizeDisabledBySitePlan } = usePublicizeConfig();
 
 	const { useAdminUiV1, featureFlags } = useSelect( select => {
 		const store = select( socialStore );
@@ -61,43 +54,7 @@ export default function PublicizeForm() {
 					<ShareCountInfo />
 				</>
 			) : null }
-			{
-				// Use IIFE make it more readable and avoid nested ternary operators.
-				( () => {
-					if ( needsUserConnection ) {
-						return (
-							<PanelRow>
-								<p>
-									{ __(
-										'You must connect your WordPress.com account to be able to add social media connections.',
-										'jetpack'
-									) }
-									&nbsp;
-									<a href={ userConnectionUrl }>{ __( 'Connect now', 'jetpack' ) }</a>
-								</p>
-							</PanelRow>
-						);
-					}
-
-					if ( ! hasConnections ) {
-						return (
-							<PanelRow>
-								<p>
-									<span className={ styles[ 'no-connections-text' ] }>
-										{ __(
-											'Sharing is disabled because there are no social media accounts connected.',
-											'jetpack'
-										) }
-									</span>
-									<SettingsButton label={ __( 'Connect an account', 'jetpack' ) } />
-								</p>
-							</PanelRow>
-						);
-					}
-
-					return null;
-				} )()
-			}
+			<ConnectionNotice />
 
 			{ ! isPublicizeDisabledBySitePlan && (
 				<Fragment>
