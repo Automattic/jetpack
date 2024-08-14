74c2580eabcbfa1d925dfd0d74f14f956ac7d305
Manzoor Wani <manzoorwani.jk@gmail.com>
2024-08-14 15:53:17 +0530

    Register connect bundle as a script handle for dependents to use

--- projects/packages/connection/src/class-manager.php
+++ projects/packages/connection/src/class-manager.php
@@ -156,7 +156,7 @@ class Manager {
 		// Initial Partner management.
 		Partner::init();
 
-		add_filter( 'jetpack_admin_js_script_data', array( Initial_State::class, 'set_connection_script_data' ), 10, 1 );
+		Connection_Assets::configure();
 	}
 
 	/**
