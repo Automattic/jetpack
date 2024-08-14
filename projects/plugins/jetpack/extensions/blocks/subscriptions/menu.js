b3fa66510899b16e9dafbf3719b576eff59ab709
Miguel Lezama <lezama@gmail.com>
2024-08-14 12:11:40 -0300

    Connect to WordPress.com

--- projects/plugins/jetpack/extensions/blocks/subscriptions/menu.js
+++ projects/plugins/jetpack/extensions/blocks/subscriptions/menu.js
@@ -1,3 +1,5 @@
+import { Text } from '@automattic/jetpack-components';
+import { useConnection } from '@automattic/jetpack-connection';
 import { Button, PanelBody } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { PluginSidebar } from '@wordpress/edit-post';
@@ -23,40 +25,61 @@ const NewsletterMenu = () => {
 	const accessLevel = useAccessLevel( postType );
 	const isPublished = postStatus === 'publish';
 
+	const { isUserConnected } = useConnection();
+	const connectUrl = `${ window?.Jetpack_Editor_Initial_State?.adminUrl }admin.php?page=my-jetpack#/connection`;
+
 	const openModal = () => setIsModalOpen( true );
 	const closeModal = () => setIsModalOpen( false );
 
 	return (
-		<>
-			<PluginSidebar
-				name="newsletter-settings-sidebar"
-				title={ __( 'Newsletter', 'jetpack' ) }
-				icon={ <SendIcon /> }
-			>
-				<PanelBody>
-					<SubscribersAffirmation accessLevel={ accessLevel } prePublish={ ! isPublished } />
-					{ ! isPublished && (
-						<p>
+		<PluginSidebar
+			name="newsletter-settings-sidebar"
+			title={ __( 'Newsletter', 'jetpack' ) }
+			icon={ <SendIcon /> }
+		>
+			<PanelBody>
+				{ isUserConnected ? (
+					<>
+						<SubscribersAffirmation accessLevel={ accessLevel } />
+						{ ! isPublished && (
+							<Text>
+								{ __(
+									'Ensure your email looks perfect. Use the buttons below to view a preview or send a test email.',
+									'jetpack'
+								) }
+							</Text>
+						) }
+						<Button
+							onClick={ openModal }
+							style={ {
+								marginRight: '18px',
+								marginTop: '10px',
+							} }
+							variant="secondary"
+							disabled={ isPublished }
+						>
+							{ __( 'Preview email', 'jetpack' ) }
+						</Button>
+						<PreviewModal isOpen={ isModalOpen } onClose={ closeModal } postId={ postId } />
+					</>
+				) : (
+					<>
+						<Text variant="title-small" mb={ 2 }>
+							{ __( 'Newsletter', 'jetpack' ) }
+						</Text>
+						<Text mb={ 3 }>
 							{ __(
-								'Ensure your email looks perfect. Use the buttons below to view a preview or send a test email.',
+								'To email your posts, build an audience, and use features like preview and test, connect to WordPress.com cloud.',
 								'jetpack'
 							) }
-						</p>
-					) }
-					<Button
-						onClick={ openModal }
-						style={ {
-							marginRight: '18px',
-						} }
-						variant="secondary"
-						disabled={ isPublished }
-					>
-						{ __( 'Preview email', 'jetpack' ) }
-					</Button>
-				</PanelBody>
-			</PluginSidebar>
-			<PreviewModal isOpen={ isModalOpen } onClose={ closeModal } postId={ postId } />
-		</>
+						</Text>
+						<Button variant="primary" href={ connectUrl } style={ { marginTop: '10px' } }>
+							{ __( 'Connect WordPress.com account', 'jetpack' ) }
+						</Button>
+					</>
+				) }
+			</PanelBody>
+		</PluginSidebar>
 	);
 };
 
