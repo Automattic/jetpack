eeb2993f21f8047c103d4ac35b09b2de5a34ed68
Kuba Birecki <kuba.birecki@crystalplanet.io>
2024-08-19 22:13:56 +0200

    Changelog and readme.txt edits.

--- projects/plugins/mu-wpcom-plugin/mu-wpcom-plugin.php
+++ projects/plugins/mu-wpcom-plugin/mu-wpcom-plugin.php
@@ -3,7 +3,7 @@
  *
  * Plugin Name: WordPress.com Features
  * Description: Test plugin for the jetpack-mu-wpcom package
- * Version: 2.5.7-alpha
+ * Version: 2.5.7
  * Author: Automattic
  * License: GPLv2 or later
  * Text Domain: jetpack-mu-wpcom-plugin
