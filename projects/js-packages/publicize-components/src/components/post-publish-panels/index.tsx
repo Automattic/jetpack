710678dc45019785d9acce4e0722282cc8c86e8d
Gergely Juhasz <gergely.juhasz@automattic.com>
2024-08-23 09:58:09 +0200

    Wrap with ThemeProvider

--- projects/js-packages/publicize-components/src/components/post-publish-panels/index.tsx
+++ projects/js-packages/publicize-components/src/components/post-publish-panels/index.tsx
@@ -1,12 +1,13 @@
+import { ThemeProvider } from '@automattic/jetpack-components';
 import PostPublishManualSharing from '../post-publish-manual-sharing';
 import PostPublishReviewPrompt from '../post-publish-review-prompt';
 
 const PostPublishPanels = () => {
 	return (
-		<>
+		<ThemeProvider>
 			<PostPublishManualSharing />
 			<PostPublishReviewPrompt />
-		</>
+		</ThemeProvider>
 	);
 };
 
