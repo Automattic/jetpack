0199cbdec17e579c1e96339ffa205c390669a2ac
Igor Zinovyev <zinigor@gmail.com>
2024-08-23 14:57:28 +0300

    Added an autorelease flow for rolling releases.

--- .github/files/gh-autorelease/files/autorelease.sh
+++ .github/files/gh-autorelease/files/autorelease.sh
@@ -8,26 +8,35 @@ set -eo pipefail
 : "${GITHUB_API_URL:?Build argument needs to be set and non-empty.}"
 : "${GITHUB_REPOSITORY:?Build argument needs to be set and non-empty.}"
 
-## Determine tag
-if [[ ! "$GITHUB_REF" =~ ^refs/tags/v?[0-9]+(\.[0-9]+)+(-[a-z0-9._-]+)?$ ]]; then
-	echo "::error::Expected GITHUB_REF like \`refs/tags/v1.2.3\` or \`refs/tags/1.2.3\`, got \`$GITHUB_REF\`"
+if [[ ! -f composer.json ]]; then
+	echo '::error::No composer.json. Did it get excluded from the mirror?'
 	exit 1
 fi
-TAG="${GITHUB_REF#refs/tags/}"
+
+## Determine tag
+ROLLING_MODE=
+if [[ "$GITHUB_REF" =~ ^refs/tags/v?[0-9]+(\.[0-9]+)+(-[a-z0-9._-]+)?$ ]]; then
+	TAG="${GITHUB_REF#refs/tags/}"
+elif [[ "$GITHUB_REF" == "refs/heads/trunk" ]]; then
+	ROLLING_MODE="$(jq -r '.extra.autorelease.rolling-release' composer.json)"
+	if [[ -z "$ROLLING_MODE" ]]; then
+		echo "::notice::Skipping trunk release because autorelease rolling mode is not enabled."
+		exit 0
+	fi
+	TAG="rolling-release"
+else
+	echo "::error::Expected GITHUB_REF like \`refs/tags/v1.2.3\` or \`refs/tags/1.2.3\` or \`trunk\` for rolling releases, got \`$GITHUB_REF\`"
+	exit 1
+fi
 
 ## Check for alphas
-if [[ "$TAG" =~ -(alpha|a\.[0-9]*[02468])$ ]]; then
+if [[ "$TAG" =~ -(alpha|a\.[0-9]*[02468])$ && -z "$ROLLING_MODE" ]]; then
 	echo "Not creating a release for alpha version $TAG"
 	exit 0
 fi
 echo "Creating release for $TAG"
 
 ## Determine slug and title format.
-if [[ ! -f composer.json ]]; then
-	echo '::error::No composer.json. Did it get excluded from the mirror?'
-	exit 1
-fi
-
 SLUG="$(jq -r '.extra.autorelease.slug? // .extra["wp-plugin-slug"] // .extra["beta-plugin-slug"] // ( .name | sub( "^.*/"; "" ) )' composer.json)"
 if [[ -z "$SLUG" ]]; then
 	echo '::error::Failed to get slug from composer.json.'
@@ -48,53 +57,58 @@ echo "::group::Creating $SLUG.zip"
 git archive -v --output="$SLUG.zip" --prefix="$SLUG/" HEAD 2>&1
 echo "::endgroup::"
 
-## Create the release note.
-# Extract the changelog section.
-echo "::group::Extracting release notes"
-if [[ ! -f CHANGELOG.md ]]; then
-	echo '::endgroup::'
-	echo '::error::No CHANGELOG.md for release notes.'
-	exit 1
-fi
-SCRIPT="
-	/^## \\[?$(sed 's/[.\[\]\\*^$\/()+?{}|]/\\&/g' <<<"${TAG#v}")\\]? - / {
-		bc
-		:a
-		n
-		/^## / {
-			q
+if [[ -z "$ROLLING_MODE" ]]; then
+	## Create the release note.
+	# Extract the changelog section.
+	echo "::group::Extracting release notes"
+	if [[ ! -f CHANGELOG.md ]]; then
+		echo '::endgroup::'
+		echo '::error::No CHANGELOG.md for release notes.'
+		exit 1
+	fi
+	SCRIPT="
+		/^## \\[?$(sed 's/[.\[\]\\*^$\/()+?{}|]/\\&/g' <<<"${TAG#v}")\\]? - / {
+			bc
+			:a
+			n
+			/^## / {
+				q
+			}
+			:c
+			s/^## \[([^]]+)\]/## \1/
+			p
+			ba
 		}
-		:c
-		s/^## \[([^]]+)\]/## \1/
+	"
+	ENTRY=$(sed -n -E -e "$SCRIPT" CHANGELOG.md)
+	if [[ -z "$ENTRY" ]]; then
+		echo '::endgroup::'
+		echo "::error::Failed to find section for ${TAG#v} in CHANGELOG.md"
+		exit 1
+	fi
+
+	# Strip unwanted sections.
+	SCRIPT="
+		:a
+		/^### .* This section will not be copied to readme\.txt/ {
+			:b
+			n
+			/^#/ ba
+			bb
+		}
 		p
-		ba
-	}
-"
-ENTRY=$(sed -n -E -e "$SCRIPT" CHANGELOG.md)
-if [[ -z "$ENTRY" ]]; then
-	echo '::endgroup::'
-	echo "::error::Failed to find section for ${TAG#v} in CHANGELOG.md"
-	exit 1
+	"
+	ENTRY=$(sed -n -E -e "$SCRIPT" <<<"$ENTRY")
+
+	echo "Release notes:"
+	echo "-----"
+	echo "$ENTRY"
+	echo "-----"
+	echo "::endgroup::"
+else
+	## Using a brief explanation for the rolling release note.
+	ENTRY="### Rolling release based on the trunk branch."
 fi
-
-# Strip unwanted sections.
-SCRIPT="
-	:a
-	/^### .* This section will not be copied to readme\.txt/ {
-		:b
-		n
-		/^#/ ba
-		bb
-	}
-	p
-"
-ENTRY=$(sed -n -E -e "$SCRIPT" <<<"$ENTRY")
-
-echo "Release notes:"
-echo "-----"
-echo "$ENTRY"
-echo "-----"
-echo "::endgroup::"
 
 echo "::group::Creating release"
 curl -v -L \
