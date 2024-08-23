e2a64d7b9c91d0a9888fd2eb22a341666d443bcb
Gergely Juhasz <gergely.juhasz@automattic.com>
2024-08-23 10:28:07 +0200

    Add feature flag to postpublishShareStatus

--- projects/js-packages/publicize-components/src/components/post-publish-share-status/index.tsx
+++ projects/js-packages/publicize-components/src/components/post-publish-share-status/index.tsx
@@ -1,4 +1,6 @@
+import { useSelect } from '@wordpress/data';
 import { PluginPostPublishPanel } from '@wordpress/edit-post';
+import { store as socialStore } from '../../social-store';
 import { ShareStatusModal } from '../share-status-modal';
 
 /**
@@ -7,6 +9,17 @@ import { ShareStatusModal } from '../share-status-modal';
  * @return {import('react').ReactNode} - Post publish share status component.
  */
 export function PostPublishShareStatus() {
+	const { featureFlags } = useSelect( select => {
+		const store = select( socialStore );
+		return {
+			featureFlags: store.featureFlags(),
+		};
+	}, [] );
+
+	if ( ! featureFlags.useShareStatus ) {
+		return null;
+	}
+
 	return (
 		<PluginPostPublishPanel id="publicize-share-status">
 			Your post was successfully shared in 4 connections.
