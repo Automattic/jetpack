4ac6e93f3ef25cd80b29cf4ff1ea23fd0c43b7a0
Manzoor Wani <manzoorwani.jk@gmail.com>
2024-08-26 19:37:32 +0530

    Show share status

--- projects/js-packages/publicize-components/src/components/post-publish-share-status/index.tsx
+++ projects/js-packages/publicize-components/src/components/post-publish-share-status/index.tsx
@@ -1,7 +1,8 @@
 import { useSelect } from '@wordpress/data';
 import { PluginPostPublishPanel } from '@wordpress/edit-post';
+import { store as editorStore } from '@wordpress/editor';
 import { store as socialStore } from '../../social-store';
-import { ShareStatusModal } from '../share-status-modal';
+import { ShareStatus } from './share-status';
 
 /**
  * Post publish share status component.
@@ -9,21 +10,26 @@ import { ShareStatusModal } from '../share-status-modal';
  * @return {import('react').ReactNode} - Post publish share status component.
  */
 export function PostPublishShareStatus() {
-	const { featureFlags } = useSelect( select => {
+	const { featureFlags, postId, isPostPublised, willPostBeShared } = useSelect( select => {
 		const store = select( socialStore );
+
+		const meta = select( editorStore ).getEditedPostAttribute( 'meta' );
+
 		return {
 			featureFlags: store.featureFlags(),
+			postId: select( editorStore ).getCurrentPostId(),
+			isPostPublised: select( editorStore ).isCurrentPostPublished(),
+			willPostBeShared: meta.jetpack_publicize_feature_enabled,
 		};
 	}, [] );
 
-	if ( ! featureFlags.useShareStatus ) {
+	if ( ! featureFlags.useShareStatus || ! willPostBeShared || ! isPostPublised ) {
 		return null;
 	}
 
 	return (
 		<PluginPostPublishPanel id="publicize-share-status">
-			Your post was successfully shared in 4 connections.
-			<ShareStatusModal />
+			<ShareStatus postId={ postId } />
 		</PluginPostPublishPanel>
 	);
 }
