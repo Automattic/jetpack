c446c1fd67b033bd6c58528ff9f45fb6bf5a88ce
Manzoor Wani <manzoorwani.jk@gmail.com>
2024-08-27 14:41:32 +0530

    Make use of usePostMeta

--- projects/js-packages/publicize-components/src/components/post-publish-share-status/index.tsx
+++ projects/js-packages/publicize-components/src/components/post-publish-share-status/index.tsx
@@ -1,6 +1,7 @@
 import { useSelect } from '@wordpress/data';
 import { PluginPostPublishPanel } from '@wordpress/edit-post';
 import { store as editorStore } from '@wordpress/editor';
+import { usePostMeta } from '../../hooks/use-post-meta';
 import { store as socialStore } from '../../social-store';
 import { ShareStatus } from './share-status';
 
@@ -10,19 +11,17 @@ import { ShareStatus } from './share-status';
  * @return {import('react').ReactNode} - Post publish share status component.
  */
 export function PostPublishShareStatus() {
-	const { featureFlags, postId, isPostPublised, willPostBeShared } = useSelect( select => {
+	const { isPublicizeEnabled: willPostBeShared } = usePostMeta();
+	const { featureFlags, postId, isPostPublised } = useSelect( select => {
 		const store = select( socialStore );
 
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any -- `@wordpress/editor` is a nightmare to work with TypeScript
 		const _editorStore = select( editorStore ) as any;
 
-		const meta = _editorStore.getEditedPostAttribute( 'meta' );
-
 		return {
 			featureFlags: store.featureFlags(),
 			postId: _editorStore.getCurrentPostId(),
 			isPostPublised: _editorStore.isCurrentPostPublished(),
-			willPostBeShared: meta.jetpack_publicize_feature_enabled,
 		};
 	}, [] );
 
