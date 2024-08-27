a2f1119f94545a20e060fc7e066a119ab0213b8b
Karen Attfield <karenlattfield@gmail.com>
2024-08-27 11:41:33 +0100

    Fixup project versions

--- projects/packages/search/src/class-package.php
+++ projects/packages/search/src/class-package.php
@@ -11,7 +11,7 @@ namespace Automattic\Jetpack\Search;
  * Search package general information
  */
 class Package {
-	const VERSION = '0.44.17';
+	const VERSION = '0.44.18-alpha';
 	const SLUG    = 'search';
 
 	/**
