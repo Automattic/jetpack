723059da5479b981ce96ee8ac3129300c96f5f9a
Luiz Kowalski <lhkowalski@gmail.com>
2024-08-27 16:22:08 -0300

    Use warning severity for the fair usage limit message

--- projects/js-packages/ai-client/src/components/message/index.tsx
+++ projects/js-packages/ai-client/src/components/message/index.tsx
@@ -131,7 +131,7 @@ export function FairUsageLimitMessage(): React.ReactElement {
 		),
 	} );
 
-	return <Message>{ element }</Message>;
+	return <Message severity={ MESSAGE_SEVERITY_WARNING }>{ element }</Message>;
 }
 
 /**
