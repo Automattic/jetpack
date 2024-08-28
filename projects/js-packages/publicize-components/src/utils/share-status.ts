82f9e7d08925be4202f2bce7fe47669d05389f88
Gergely Juhasz <gergely.juhasz@automattic.com>
2024-08-28 12:41:44 +0200

    Only order the list

--- projects/js-packages/publicize-components/src/utils/share-status.ts
+++ projects/js-packages/publicize-components/src/utils/share-status.ts
@@ -1,6 +1,5 @@
 /**
  * Normalizes the share status object.
- * TODO: Optimize this function to avoid unnecessary iterations.
  *
  * @param {import( './types' ).PostShareStatus} shareStatus - Share status object.
  * @return {import( './types' ).PostShareStatus | undefined} - Normalized share status object.
@@ -11,24 +10,6 @@ export function normalizeShareStatus( shareStatus ) {
 	}
 	// Sort shares to show the latest shares on the top.
 	shareStatus.shares.sort( ( a, b ) => b.timestamp - a.timestamp );
-
-	// Remove failed shares that have a successful share later.
-	shareStatus.shares = shareStatus.shares.filter( share => {
-		const hasSuccessfulShareLater = shareStatus.shares.some( otherShare => {
-			return (
-				otherShare.timestamp > share.timestamp &&
-				'success' === otherShare.status &&
-				otherShare.external_id === share.external_id &&
-				share.external_id // We added external_id later to the object
-			);
-		} );
-
-		if ( 'failure' === share.status ) {
-			return ! hasSuccessfulShareLater;
-		}
-
-		return true;
-	} );
 
 	return shareStatus;
 }
