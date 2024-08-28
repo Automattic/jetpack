d7efe41744f3877592f436686171d582e527268c
Manzoor Wani <manzoorwani.jk@gmail.com>
2024-08-28 18:43:52 +0530

    Social: Hide share status when no connection is enabled

--- projects/js-packages/publicize-components/src/components/post-publish-share-status/index.tsx
+++ projects/js-packages/publicize-components/src/components/post-publish-share-status/index.tsx
@@ -2,6 +2,7 @@ import { useSelect } from '@wordpress/data';
 import { PluginPostPublishPanel } from '@wordpress/edit-post';
 import { store as editorStore } from '@wordpress/editor';
 import { usePostMeta } from '../../hooks/use-post-meta';
+import { usePostPrePublishValue } from '../../hooks/use-post-pre-publish-value';
 import { store as socialStore } from '../../social-store';
 import { ShareStatus } from './share-status';
 
@@ -11,7 +12,7 @@ import { ShareStatus } from './share-status';
  * @return {import('react').ReactNode} - Post publish share status component.
  */
 export function PostPublishShareStatus() {
-	const { isPublicizeEnabled: willPostBeShared } = usePostMeta();
+	const { isPublicizeEnabled } = usePostMeta();
 	const { featureFlags, postId, isPostPublised } = useSelect( select => {
 		const store = select( socialStore );
 
@@ -24,6 +25,12 @@ export function PostPublishShareStatus() {
 			isPostPublised: _editorStore.isCurrentPostPublished(),
 		};
 	}, [] );
+
+	const enabledConnections = usePostPrePublishValue(
+		useSelect( select => select( socialStore ).getEnabledConnections(), [] )
+	);
+
+	const willPostBeShared = isPublicizeEnabled && enabledConnections.length > 0;
 
 	if ( ! featureFlags.useShareStatus || ! willPostBeShared || ! isPostPublised ) {
 		return null;
