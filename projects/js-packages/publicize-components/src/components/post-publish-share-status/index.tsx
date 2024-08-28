5db5551cf0780cb392eaab976d57a08b290d1136
Gergely Juhasz <gergely.juhasz@automattic.com>
2024-08-28 14:34:28 +0200

    revert bad change

--- projects/js-packages/publicize-components/src/components/post-publish-share-status/index.tsx
+++ projects/js-packages/publicize-components/src/components/post-publish-share-status/index.tsx
@@ -3,7 +3,7 @@ import { PluginPostPublishPanel } from '@wordpress/edit-post';
 import { store as editorStore } from '@wordpress/editor';
 import { usePostMeta } from '../../hooks/use-post-meta';
 import { store as socialStore } from '../../social-store';
-import { ShareStatusModalTrigger } from '../share-status';
+import { ShareStatus } from './share-status';
 
 /**
  * Post publish share status component.
@@ -12,7 +12,7 @@ import { ShareStatusModalTrigger } from '../share-status';
  */
 export function PostPublishShareStatus() {
 	const { isPublicizeEnabled: willPostBeShared } = usePostMeta();
-	const { featureFlags, isPostPublised } = useSelect( select => {
+	const { featureFlags, postId, isPostPublised } = useSelect( select => {
 		const store = select( socialStore );
 
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any -- `@wordpress/editor` is a nightmare to work with TypeScript
@@ -31,7 +31,7 @@ export function PostPublishShareStatus() {
 
 	return (
 		<PluginPostPublishPanel id="publicize-share-status">
-			<ShareStatusModalTrigger withWrapper />
+			<ShareStatus postId={ postId } />
 		</PluginPostPublishPanel>
 	);
 }
