efd0f15b56f25d06312ddc26dc3437e1046cd291
Gergely Juhasz <gergely.juhasz@automattic.com>
2024-08-29 12:51:10 +0200

    Only show modal trigger if there are shares for the post

--- projects/js-packages/publicize-components/src/components/share-status/modal-trigger.tsx
+++ projects/js-packages/publicize-components/src/components/share-status/modal-trigger.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@wordpress/components';
 import { useDispatch, useSelect } from '@wordpress/data';
+import { store as editorStore } from '@wordpress/editor';
 import { __ } from '@wordpress/i18n';
 import { forwardRef } from 'react';
 import { store as socialStore } from '../../social-store';
@@ -16,8 +17,21 @@ type ModalTriggerProps = ButtonProps & {
 export const ModalTrigger = forwardRef(
 	( { withWrapper = false, ...props }: ModalTriggerProps, ref: unknown ) => {
 		const { openShareStatusModal } = useDispatch( socialStore );
-
 		const featureFlags = useSelect( select => select( socialStore ).featureFlags(), [] );
+		const { shareStatus } = useSelect( select => {
+			const store = select( socialStore );
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any -- `@wordpress/editor` is a nightmare to work with TypeScript
+			const _editorStore = select( editorStore ) as any;
+
+			return {
+				shareStatus: store.getPostShareStatus( _editorStore.getCurrentPostId() ),
+			};
+		}, [] );
+
+		// If the post is not shared anywhere, thus there is no share status or no shares, we don't need to show the trigger.
+		if ( ! shareStatus || ! shareStatus.shares || shareStatus.shares.length === 0 ) {
+			return null;
+		}
 
 		if ( ! featureFlags.useShareStatus ) {
 			return null;
