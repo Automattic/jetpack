a173eedd2acabab32493faee6e86e99d425b8960
Karen Attfield <karenlattfield@gmail.com>
2024-08-29 10:30:04 +0100

    Add Portfolios CPT to CTH package

--- projects/packages/classic-theme-helper/src/custom-content-types.php
+++ projects/packages/classic-theme-helper/src/custom-content-types.php
@@ -10,7 +10,7 @@
  * Feature: Writing
  * Additional Search Queries: cpt, custom post types, portfolio, portfolios, testimonial, testimonials
  *
- * @package automattic/jetpack
+ * @package automattic/jetpack-classic-theme-helper
  */
 
 use Automattic\Jetpack\Redirect;
@@ -20,8 +20,12 @@ if ( ! function_exists( 'jetpack_load_custom_post_types' ) ) {
 	 * Load Portfolio CPT.
 	 */
 	function jetpack_load_custom_post_types() {
-		include __DIR__ . '/custom-post-types/portfolios.php';
+		include __DIR__ . '/custom-post-types/class-jetpack-portfolio.php';
 	}
+	add_action( 'init', array( '\Automattic\Jetpack\Classic_Theme_Helper\Jetpack_Portfolio', 'init' ) );
+	register_activation_hook( __FILE__, array( '\Automattic\Jetpack\Classic_Theme_Helper\Jetpack_Portfolio', 'activation_post_type_support' ) );
+	add_action( 'jetpack_activate_module_custom-content-types', array( '\Automattic\Jetpack\Classic_Theme_Helper\Jetpack_Portfolio', 'activation_post_type_support' ) );
+
 }
 
 if ( ! function_exists( 'jetpack_custom_post_types_loaded' ) ) {
@@ -41,7 +45,7 @@ if ( ! function_exists( 'jetpack_cpt_settings_api_init' ) ) {
 	function jetpack_cpt_settings_api_init() {
 		add_settings_section(
 			'jetpack_cpt_section',
-			'<span id="cpt-options">' . __( 'Your Custom Content Types', 'jetpack' ) . '</span>',
+			'<span id="cpt-options">' . __( 'Your Custom Content Types', 'jetpack-classic-theme-helper' ) . '</span>',
 			'jetpack_cpt_section_callback',
 			'writing'
 		);
@@ -56,13 +60,15 @@ if ( ! function_exists( 'jetpack_cpt_section_callback' ) ) {
 	function jetpack_cpt_section_callback() {
 		?>
 		<p>
-			<?php esc_html_e( 'Use these settings to display different types of content on your site.', 'jetpack' ); ?>
-			<a target="_blank" rel="noopener noreferrer" href="<?php echo esc_url( Redirect::get_url( 'jetpack-support-custom-content-types' ) ); ?>"><?php esc_html_e( 'Learn More', 'jetpack' ); ?></a>
+			<?php esc_html_e( 'Use these settings to display different types of content on your site.', 'jetpack-classic-theme-helper' ); ?>
+			<a target="_blank" rel="noopener noreferrer" href="<?php echo esc_url( Redirect::get_url( 'jetpack-support-custom-content-types' ) ); ?>"><?php esc_html_e( 'Learn More', 'jetpack-classic-theme-helper' ); ?></a>
 		</p>
 		<?php
 	}
 }
 
 if ( function_exists( 'jetpack_load_custom_post_types' ) ) {
+
 	jetpack_load_custom_post_types();
+
 }
