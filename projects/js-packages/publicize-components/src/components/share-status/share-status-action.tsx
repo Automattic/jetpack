76942fce6bdd7c9c66d254d3326bcd4400e949ae
Gergely Juhasz <gergely.juhasz@automattic.com>
2024-09-02 11:07:10 +0200

    Show connection as removed if the connection_id is missing

--- projects/js-packages/publicize-components/src/components/share-status/share-status-action.tsx
+++ projects/js-packages/publicize-components/src/components/share-status/share-status-action.tsx
@@ -1,23 +1,27 @@
-import { Button, ExternalLink } from '@wordpress/components';
+import { Button, ExternalLink, Tooltip } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { store as editorStore } from '@wordpress/editor';
 import { __ } from '@wordpress/i18n';
+import { Icon, linkOff } from '@wordpress/icons';
 import { useCallback } from 'react';
 import useSharePost from '../../hooks/use-share-post';
 import { store as socialStore } from '../../social-store';
 import styles from './styles.module.scss';
 
+type ShareStatusActionProps = {
+	status: string;
+	shareLink: string;
+	connectionId: number;
+};
+
 /**
  *
  * Share status action component.
  *
- * @param {object}  props              - component props
- * @param {boolean} props.status       - status of the share
- * @param {string}  props.shareLink    - link to the share
- * @param {number}  props.connectionId - connection id
+ * @param {ShareStatusActionProps} props - component props
  * @return {import('react').ReactNode} - React element
  */
-export function ShareStatusAction( { connectionId, status, shareLink } ) {
+export function ShareStatusAction( { connectionId, status, shareLink }: ShareStatusActionProps ) {
 	const postId = useSelect( select => select( editorStore ).getCurrentPostId(), [] );
 	const connections = useSelect( select => select( socialStore ).getConnections(), [] );
 
@@ -36,17 +40,31 @@ export function ShareStatusAction( { connectionId, status, shareLink } ) {
 		doPublicize( skippedConnections.map( connection => connection.id ) );
 	}, [ connectionId, connections, doPublicize ] );
 
-	return (
-		<div className={ styles[ 'share-status-action-wrapper' ] }>
-			{ 'success' !== status ? (
-				<Button variant="link" onClick={ onRetry }>
-					{ __( 'Retry', 'jetpack' ) }
-				</Button>
-			) : (
+	const renderActions = () => {
+		if ( 'success' === status ) {
+			return (
 				<ExternalLink className={ styles[ 'profile-link' ] } href={ shareLink }>
 					{ __( 'View', 'jetpack' ) }
 				</ExternalLink>
-			) }
-		</div>
-	);
+			);
+		}
+
+		if (
+			! connections.find( connection => connection.connection_id === connectionId.toString() )
+		) {
+			return (
+				<Tooltip text={ __( 'This connection has been removed.', 'jetpack' ) }>
+					<Icon icon={ linkOff } size={ 20 } className={ styles[ 'disconnected-icon' ] } />
+				</Tooltip>
+			);
+		}
+
+		return (
+			<Button variant="link" onClick={ onRetry }>
+				{ __( 'Retry', 'jetpack' ) }
+			</Button>
+		);
+	};
+
+	return <div className={ styles[ 'share-status-action-wrapper' ] }>{ renderActions() }</div>;
 }
