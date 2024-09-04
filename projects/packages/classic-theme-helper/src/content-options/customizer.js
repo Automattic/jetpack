3f80a4ac6b7e5b3c6fa0f72b4ab4021b42f13190
Karen Attfield <karenlattfield@gmail.com>
2024-09-04 14:10:24 +0100

    Rewriting customizer.js, primarily due to issues with only the first selectors being selected in many cases

--- projects/packages/classic-theme-helper/src/content-options/customizer.js
+++ projects/packages/classic-theme-helper/src/content-options/customizer.js
@@ -8,6 +8,19 @@
  * Contains handlers to make Theme Customizer preview reload changes asynchronously.
  */
 
+/**
+ * Function to apply styles to elements based on the display type
+ * @param {object} selectors - HTML selectors which styles will apply to.
+ * @param {object} styles    - Styles to be applied to selectors.
+ */
+function applyStyles( selectors, styles ) {
+	document.querySelectorAll( selectors ).forEach( el => {
+		for ( const [ key, value ] of Object.entries( styles ) ) {
+			el.style[ key ] = value;
+		}
+	} );
+}
+
 // Blog Display
 wp.customize( 'jetpack_content_blog_display', function ( value ) {
 	/**
@@ -15,52 +28,50 @@ wp.customize( 'jetpack_content_blog_display', function ( value ) {
 	 * @param {string} to - Content display option.
 	 */
 	function updateBlogDisplay( to ) {
-		const contentElements = document.querySelectorAll(
-			'.jetpack-blog-display.jetpack-the-content'
-		);
-		const excerptElements = document.querySelectorAll(
-			'.jetpack-blog-display.jetpack-the-excerpt'
-		);
+		const contentSelectors = '.jetpack-blog-display.jetpack-the-content';
+		const excerptSelectors = '.jetpack-blog-display.jetpack-the-excerpt';
+		const featuredContentSelectors = '.featured-content .jetpack-blog-display';
 
 		if ( to === 'content' ) {
-			excerptElements.forEach( el => {
-				el.style.clip = 'rect(1px, 1px, 1px, 1px)';
-				el.style.position = 'absolute';
+			applyStyles( `${ excerptSelectors }, ${ featuredContentSelectors }.jetpack-the-excerpt`, {
+				clip: 'rect(1px, 1px, 1px, 1px)',
+				position: 'absolute',
 			} );
-			contentElements.forEach( el => {
-				el.style.clip = 'auto';
-				el.style.position = 'relative';
+			applyStyles( `${ contentSelectors }, ${ featuredContentSelectors }.jetpack-the-content`, {
+				clip: 'auto',
+				position: 'relative',
 			} );
 		} else if ( to === 'excerpt' ) {
-			contentElements.forEach( el => {
-				el.style.clip = 'rect(1px, 1px, 1px, 1px)';
-				el.style.position = 'absolute';
+			applyStyles( `${ contentSelectors }, ${ featuredContentSelectors }.jetpack-the-content`, {
+				clip: 'rect(1px, 1px, 1px, 1px)',
+				position: 'absolute',
 			} );
-			excerptElements.forEach( el => {
-				el.style.clip = 'auto';
-				el.style.position = 'relative';
+			applyStyles( `${ excerptSelectors }, ${ featuredContentSelectors }.jetpack-the-excerpt`, {
+				clip: 'auto',
+				position: 'relative',
 			} );
 		} else if ( to === 'mixed' ) {
-			document
-				.querySelectorAll( '.jetpack-blog-display.jetpack-the-content.output-the-content' )
-				.forEach( el => {
-					el.style.clip = 'auto';
-					el.style.position = 'relative';
-				} );
-			document
-				.querySelectorAll(
-					'.jetpack-blog-display.jetpack-the-excerpt.output-the-content, .jetpack-blog-display.jetpack-the-content.output-the-excerpt'
-				)
-				.forEach( el => {
-					el.style.clip = 'rect(1px, 1px, 1px, 1px)';
-					el.style.position = 'absolute';
-				} );
-			document
-				.querySelectorAll( '.jetpack-blog-display.jetpack-the-excerpt.output-the-excerpt' )
-				.forEach( el => {
-					el.style.clip = 'auto';
-					el.style.position = 'relative';
-				} );
+			applyStyles(
+				`${ contentSelectors }.output-the-content, ${ featuredContentSelectors }.jetpack-the-content.output-the-content`,
+				{
+					clip: 'auto',
+					position: 'relative',
+				}
+			);
+			applyStyles(
+				`${ excerptSelectors }.output-the-content, ${ contentSelectors }.output-the-excerpt, ${ featuredContentSelectors }.jetpack-the-excerpt.output-the-content, ${ featuredContentSelectors }.jetpack-the-content.output-the-excerpt`,
+				{
+					clip: 'rect(1px, 1px, 1px, 1px)',
+					position: 'absolute',
+				}
+			);
+			applyStyles(
+				`${ excerptSelectors }.output-the-excerpt, ${ featuredContentSelectors }.jetpack-the-excerpt.output-the-excerpt`,
+				{
+					clip: 'auto',
+					position: 'relative',
+				}
+			);
 		}
 
 		if ( blogDisplay.masonry ) {
@@ -75,112 +86,63 @@ wp.customize( 'jetpack_content_blog_display', function ( value ) {
 	value.bind( updateBlogDisplay );
 } );
 
+/**
+ * Function to update post details visibility
+ * @param {object} selectors   - HTML selectors which styles will apply to.
+ * @param {string} to          - Content display option.
+ * @param {string} hiddenClass - Class to be added to the body when the post details are hidden.
+ */
+function updatePostDetails( selectors, to, hiddenClass ) {
+	document.querySelectorAll( selectors ).forEach( element => {
+		if ( to === false ) {
+			element.style.clip = 'rect(1px, 1px, 1px, 1px)';
+			element.style.height = '1px';
+			element.style.overflow = 'hidden';
+			element.style.position = 'absolute';
+			element.style.width = '1px';
+			document.body.classList.add( hiddenClass );
+		} else {
+			element.style.clip = 'auto';
+			element.style.height = 'auto';
+			element.style.overflow = 'auto';
+			element.style.position = 'relative';
+			element.style.width = 'auto';
+			document.body.classList.remove( hiddenClass );
+		}
+	} );
+}
+
 // Post Details: Date
 wp.customize( 'jetpack_content_post_details_date', function ( value ) {
 	value.bind( function ( to ) {
-		const dateElement = document.querySelector( postDetails.date );
-		if ( to === false ) {
-			dateElement.style.clip = 'rect(1px, 1px, 1px, 1px)';
-			dateElement.style.height = '1px';
-			dateElement.style.overflow = 'hidden';
-			dateElement.style.position = 'absolute';
-			dateElement.style.width = '1px';
-			document.body.classList.add( 'date-hidden' );
-		} else {
-			dateElement.style.clip = 'auto';
-			dateElement.style.height = 'auto';
-			dateElement.style.overflow = 'auto';
-			dateElement.style.position = 'relative';
-			dateElement.style.width = 'auto';
-			document.body.classList.remove( 'date-hidden' );
-		}
+		updatePostDetails( postDetails.date, to, 'date-hidden' );
 	} );
 } );
 
 // Post Details: Categories
 wp.customize( 'jetpack_content_post_details_categories', function ( value ) {
 	value.bind( function ( to ) {
-		const categoriesElement = document.querySelector( postDetails.categories );
-		if ( to === false ) {
-			categoriesElement.style.clip = 'rect(1px, 1px, 1px, 1px)';
-			categoriesElement.style.height = '1px';
-			categoriesElement.style.overflow = 'hidden';
-			categoriesElement.style.position = 'absolute';
-			categoriesElement.style.width = '1px';
-			document.body.classList.add( 'categories-hidden' );
-		} else {
-			categoriesElement.style.clip = 'auto';
-			categoriesElement.style.height = 'auto';
-			categoriesElement.style.overflow = 'auto';
-			categoriesElement.style.position = 'relative';
-			categoriesElement.style.width = 'auto';
-			document.body.classList.remove( 'categories-hidden' );
-		}
+		updatePostDetails( postDetails.categories, to, 'categories-hidden' );
 	} );
 } );
 
 // Post Details: Tags
 wp.customize( 'jetpack_content_post_details_tags', function ( value ) {
 	value.bind( function ( to ) {
-		const tagsElement = document.querySelector( postDetails.tags );
-		if ( to === false ) {
-			tagsElement.style.clip = 'rect(1px, 1px, 1px, 1px)';
-			tagsElement.style.height = '1px';
-			tagsElement.style.overflow = 'hidden';
-			tagsElement.style.position = 'absolute';
-			tagsElement.style.width = '1px';
-			document.body.classList.add( 'tags-hidden' );
-		} else {
-			tagsElement.style.clip = 'auto';
-			tagsElement.style.height = 'auto';
-			tagsElement.style.overflow = 'auto';
-			tagsElement.style.position = 'relative';
-			tagsElement.style.width = 'auto';
-			document.body.classList.remove( 'tags-hidden' );
-		}
+		updatePostDetails( postDetails.tags, to, 'tags-hidden' );
 	} );
 } );
 
 // Post Details: Author
 wp.customize( 'jetpack_content_post_details_author', function ( value ) {
 	value.bind( function ( to ) {
-		const authorElement = document.querySelector( postDetails.author );
-		if ( to === false ) {
-			authorElement.style.clip = 'rect(1px, 1px, 1px, 1px)';
-			authorElement.style.height = '1px';
-			authorElement.style.overflow = 'hidden';
-			authorElement.style.position = 'absolute';
-			authorElement.style.width = '1px';
-			document.body.classList.add( 'author-hidden' );
-		} else {
-			authorElement.style.clip = 'auto';
-			authorElement.style.height = 'auto';
-			authorElement.style.overflow = 'auto';
-			authorElement.style.position = 'relative';
-			authorElement.style.width = 'auto';
-			document.body.classList.remove( 'author-hidden' );
-		}
+		updatePostDetails( postDetails.author, to, 'author-hidden' );
 	} );
 } );
 
 // Post Details: Comment link
 wp.customize( 'jetpack_content_post_details_comment', function ( value ) {
 	value.bind( function ( to ) {
-		const commentElement = document.querySelector( postDetails.comment );
-		if ( to === false ) {
-			commentElement.style.clip = 'rect(1px, 1px, 1px, 1px)';
-			commentElement.style.height = '1px';
-			commentElement.style.overflow = 'hidden';
-			commentElement.style.position = 'absolute';
-			commentElement.style.width = '1px';
-			document.body.classList.add( 'comment-hidden' );
-		} else {
-			commentElement.style.clip = 'auto';
-			commentElement.style.height = 'auto';
-			commentElement.style.overflow = 'auto';
-			commentElement.style.position = 'relative';
-			commentElement.style.width = 'auto';
-			document.body.classList.remove( 'comment-hidden' );
-		}
+		updatePostDetails( postDetails.comment, to, 'comment-hidden' );
 	} );
 } );
