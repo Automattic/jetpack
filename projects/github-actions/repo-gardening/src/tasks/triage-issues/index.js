2a47fc317d817c28e935cbd206968d6f13a97fe7
Jeremy Herve <jeremy@jeremy.hu>
2024-09-05 10:15:19 +0200

    Add a label marking processed entries
    This is necessary during the experiment, since we trigger the process on multiple actions (opened, labeled, reopened). We don't want the labeling to be duplicated if an issue is opened AND labeled.

--- projects/github-actions/repo-gardening/src/tasks/triage-issues/index.js
+++ projects/github-actions/repo-gardening/src/tasks/triage-issues/index.js
@@ -130,6 +130,37 @@ async function hasTestLabel( octokit, owner, repo, number, action, eventLabel ) 
 }
 
 /**
+ * Check if the issue has the "Experiment] AI labels added" label.
+ *
+ * @param {GitHub} octokit    - Initialized Octokit REST client.
+ * @param {string} owner      - Repository owner.
+ * @param {string} repo       - Repository name.
+ * @param {string} number     - Issue number.
+ * @param {string} action     - Action that triggered the event ('opened', 'reopened', 'labeled').
+ * @param {object} eventLabel - Label that was added to the issue.
+ *
+ * @return {Promise<boolean>} Promise resolving to boolean.
+ */
+async function hasProcessedLabel( octokit, owner, repo, number, action, eventLabel ) {
+	// Check existing labels.
+	const labels = await getLabels( octokit, owner, repo, number );
+	if ( labels.includes( '[Experiment] AI labels added' ) ) {
+		return true;
+	}
+
+	// Next, check if the current event was a [Experiment] AI labels added label being added.
+	if (
+		'labeled' === action &&
+		eventLabel.name &&
+		'[Experiment] AI labels added' === eventLabel.name
+	) {
+		return true;
+	}
+
+	return false;
+}
+
+/**
  * Automatically add labels to issues, and send Slack notifications.
  *
  * This task can send 2 different types of Slack notifications:
@@ -236,9 +267,18 @@ async function triageIssues( payload, octokit ) {
 	// and add labels if any matching labels can be found.
 	// During testing, we'll run it for any issues, not just opened,
 	// but only on issues with the "[Experiment] Automated labeling" label.
+	// In that situation, we'll add a label to note that the issue was processed.
 	const isTestIssue = await hasTestLabel( octokit, ownerLogin, name, number, action, label );
+	const alreadyProcessed = await hasProcessedLabel(
+		octokit,
+		ownerLogin,
+		name,
+		number,
+		action,
+		label
+	);
 	const apiKey = getInput( 'openai_api_key' );
-	if ( isTestIssue && apiKey ) {
+	if ( isTestIssue && ! alreadyProcessed && apiKey ) {
 		debug( `triage-issues: Fetching labels suggested by OpenAI for issue #${ number }` );
 		const { labels, explanations } = await fetchOpenAiLabelsSuggestions(
 			octokit,
@@ -275,6 +315,14 @@ ${ Object.entries( explanations )
 				repo: name,
 				issue_number: number,
 				body: explanationComment,
+			} );
+
+			// Add a label to note that the issue was processed.
+			await octokit.rest.issues.addLabels( {
+				owner: ownerLogin,
+				repo: name,
+				issue_number: number,
+				labels: [ '[Experiment] AI labels added' ],
 			} );
 		}
 	}
