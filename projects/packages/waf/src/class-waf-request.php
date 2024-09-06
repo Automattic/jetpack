508c406c2d29d778777db3a05e40d2596e6be995
Briley Hooper <briley.hooper@automattic.com>
2024-09-06 00:09:28 -0400

    Protect WAF: Ensure request body is parsed correctly (#39262)
    ---------
    
    Co-authored-by: Nate Weller <nate.weller@automattic.com>

--- projects/packages/waf/src/class-waf-request.php
+++ projects/packages/waf/src/class-waf-request.php
@@ -334,17 +334,21 @@ class Waf_Request {
 	 * @return array{string, scalar}[]
 	 */
 	public function get_post_vars() {
+		$content_type = $this->get_header( 'content-type' );
 		if ( ! empty( $_POST ) ) {
+			// If $_POST is populated, use it.
 			return flatten_array( $_POST );
-		} elseif ( strpos( $this->get_header( 'content-type' ), 'application/json' ) !== false ) {
+		} elseif ( strpos( $content_type, 'application/json' ) !== false ) {
 			// Attempt to decode JSON requests.
 			$decoded_json = json_decode( $this->get_body(), true ) ?? array();
 			return flatten_array( $decoded_json, 'json', true );
-		} else {
-			// Attempt to retrieve all parameters when method used isn't POST
-			$body = $this->get_body();
-			parse_str( $body, $params );
+		} elseif ( strpos( $content_type, 'application/x-www-form-urlencoded' ) !== false ) {
+			// Attempt to decode url-encoded data
+			parse_str( $this->get_body(), $params );
 			return flatten_array( $params );
+		} else {
+			// Don't try to parse any other content types
+			return array();
 		}
 	}
 
