ca6df5de26c7c09cea91a737eda18e0121d0a69d
arthur791004 <arthur.chu@automattic.com>
2024-09-10 00:56:00 +0800

    AI Content Lens: Fix the AI Excerpt is missing from the Post Editor (#39292)
    * AI Content Lens: Fix the feature of the AI Content Lens is gone
    
    * changelog
    
    * add canAIAssistantBeEnabled function and change excerpt check
    
    ---------
    
    Co-authored-by: Douglas <douglas.henri@automattic.com>

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/lib/is-possible-to-extend-block.ts
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/lib/is-possible-to-extend-block.ts
@@ -1,51 +1,24 @@
 /*
- * External dependencies
- */
-import { getBlockType } from '@wordpress/blocks';
-import { select } from '@wordpress/data';
-/*
  * Internal dependencies
  */
-import { getFeatureAvailability } from '../../lib/utils/get-feature-availability';
 import { EXTENDED_BLOCKS } from '../constants';
-
-export const AI_ASSISTANT_SUPPORT_NAME = 'ai-assistant-support';
-
-// Check if the AI Assistant support is enabled.
-export const isAiAssistantSupportEnabled = getFeatureAvailability( AI_ASSISTANT_SUPPORT_NAME );
+import { canAIAssistantBeEnabled } from './can-ai-assistant-be-enabled';
 
 /**
- * Check if it is possible to extend the block as an inline extension.
+ * Check if it is possible to extend a block with AI Assistant capabilities.
  * @param {string} blockName - The block name.
  * @return {boolean}           Whether it is possible to extend the block.
  */
 export function isPossibleToExtendBlock( blockName: string ): boolean {
-	// Check if the AI Assistant block is registered. If not, we understand that Jetpack AI is not active.
-	const isBlockRegistered = getBlockType( 'jetpack/ai-assistant' );
+	const canEnableAIAssistant = canAIAssistantBeEnabled();
 
-	if ( ! isBlockRegistered ) {
-		return false;
-	}
-
-	// Check if AI Assistant support is enabled
-	if ( ! isAiAssistantSupportEnabled ) {
+	// Do not extend the block if AI Assistant cannot be enabled.
+	if ( ! canEnableAIAssistant ) {
 		return false;
 	}
 
 	// Only extend the blocks in the inline blocks list
 	if ( ! EXTENDED_BLOCKS.includes( blockName ) ) {
-		return false;
-	}
-
-	/*
-	 * Do not extend if the AI Assistant block is hidden
-	 * ToDo: the `editPostStore` is undefined for P2 sites.
-	 * Let's find a way to check if the block is hidden.
-	 */
-	const { getHiddenBlockTypes } = select( 'core/edit-post' ) || {};
-	const hiddenBlocks = getHiddenBlockTypes?.() || []; // It will extend the block if the function is undefined
-
-	if ( hiddenBlocks.includes( 'jetpack/ai-assistant' ) ) {
 		return false;
 	}
 
