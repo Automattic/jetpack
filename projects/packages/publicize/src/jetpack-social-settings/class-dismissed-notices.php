aeb84610ff8e1a602dd6fe61967232b2c76acfcc
Manzoor Wani <manzoorwani.jk@gmail.com>
2024-09-11 08:09:43 +0530

    Social: Clean up media auto-conversion backend logic (#38587)
    * Social: Clean up media auto-conversion backend logic
    
    * Add changelog
    
    * Fix uncached call via delete_option
    
    * Fix unit tests
    
    * Fix up versions
    
    * Update class-publicize-base.php
    
    * Do what CI should be doing - fix up versions
    
    * Add unnecessary changelogs
    
    * Remove unnecessary changelog
    
    * Fix up versions
    
    * Fix up versions
    
    * Fix up versions
    
    * Fix up versions
    
    * Fix up versions
    
    * Fix up versions
    
    * Fix up versions
    
    * Fix up versions
    
    * Remove unnecessary changelogs
    
    * Delete unnecessary changelog

--- projects/packages/publicize/src/jetpack-social-settings/class-dismissed-notices.php
+++ projects/packages/publicize/src/jetpack-social-settings/class-dismissed-notices.php
@@ -28,7 +28,6 @@ class Dismissed_Notices {
 					'schema' => array(
 						'type'       => 'object',
 						'properties' => array(
-							'auto-conversion-editor-notice' => array( 'type' => 'number' ),
 							'advanced-upgrade-nudge-admin' => array( 'type' => 'number' ),
 							'advanced-upgrade-nudge-editor' => array( 'type' => 'number' ),
 						),
