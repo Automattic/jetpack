96237e8b956baac5aceffdd6e501179c4b6037c2
Louis Laugesen <louis.laugesen@gmail.com>
2024-09-11 23:39:52 +1000

    Add new sharing networks to match post-published sharing modal

--- projects/js-packages/publicize-components/src/components/share-buttons/available-networks.tsx
+++ projects/js-packages/publicize-components/src/components/share-buttons/available-networks.tsx
@@ -2,7 +2,7 @@ import { __ } from '@wordpress/i18n';
 
 type Network = {
 	label: string;
-	networkName: 'x' | 'whatsapp' | 'facebook';
+	networkName: 'x' | 'whatsapp' | 'facebook' | 'linkedin' | 'tumblr' | 'pinterest' | 'mail';
 	url: string;
 };
 
@@ -21,5 +21,25 @@ export const availableNetworks: Array< Network > = [
 		label: __( 'Facebook', 'jetpack' ),
 		networkName: 'facebook',
 		url: 'https://www.facebook.com/sharer/sharer.php?u={{url}}',
+	},
+	{
+		label: __( 'Tumblr', 'jetpack' ),
+		networkName: 'tumblr',
+		url: 'https://www.tumblr.com/widgets/share/tool?canonicalUrl={{url}}&title={{text}}',
+	},
+	{
+		label: __( 'LinkedIn', 'jetpack' ),
+		networkName: 'linkedin',
+		url: 'https://www.linkedin.com/shareArticle?mini=true&url={{url}}&title={{text}}',
+	},
+	{
+		label: __( 'Pinterest', 'jetpack' ),
+		networkName: 'pinterest',
+		url: 'https://pinterest.com/pin/create/button/?url={{url}}&description={{text}}',
+	},
+	{
+		label: __( 'Email', 'jetpack' ),
+		networkName: 'mail',
+		url: 'mailto:?subject={{text}}&body={{url}}',
 	},
 ];
