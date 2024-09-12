6b7fcd6a9910efa1a0877cae3e4a92ef4233b3e7
Eoin Gallagher <eoin@automattic.com>
2024-09-12 21:51:53 +0100

    SEO Upsell: update link to open in help center (#39339)
    * SEO Upsell: update link to open in help center
    
    * remove debug
    
    * add changelog
    
    * try without localizeUrl
    
    * add localiseUrl to jetpack packages
    
    * revert change to file
    
    * needs yaml file
    
    * remove css changes - not needed
    
    * remove dependency on localiseUrl
    
    * update to use getRedirectUrl
    
    * remove the use of Jetpack redirects - not going to work with help center
    
    * postId not needed

--- projects/plugins/jetpack/extensions/plugins/seo/components/upsell.js
+++ projects/plugins/jetpack/extensions/plugins/seo/components/upsell.js
@@ -1,5 +1,6 @@
 import { useAnalytics } from '@automattic/jetpack-shared-extension-utils';
 import { Button, ExternalLink } from '@wordpress/components';
+import { useDispatch } from '@wordpress/data';
 import { __, sprintf } from '@wordpress/i18n';
 import { external } from '@wordpress/icons';
 import clsx from 'clsx';
@@ -15,12 +16,17 @@ const UpsellNotice = ( { requiredPlan } ) => {
 
 	const buttonText = __( 'Upgrade now', 'jetpack' );
 	const supportUrl = 'https://wordpress.com/support/seo-tools/';
+	const supportLinkTitle = __( 'Learn more about the SEO feature.', 'jetpack' );
 
 	const onClickHandler = event => {
 		event.preventDefault();
 		tracks.recordEvent( 'jetpack_seo_tools_upsell_click' );
 		goToCheckoutPage( event );
 	};
+
+	const helpCenterDispatch = useDispatch( 'automattic/help-center' );
+	const setShowHelpCenter = helpCenterDispatch?.setShowHelpCenter;
+	const setShowSupportDoc = helpCenterDispatch?.setShowSupportDoc;
 
 	return (
 		<>
@@ -33,9 +39,20 @@ const UpsellNotice = ( { requiredPlan } ) => {
 			</div>
 
 			<div className="components-seo-upsell__learn-more">
-				<ExternalLink href={ supportUrl }>
-					{ __( 'Learn more about SEO feature.', 'jetpack' ) }
-				</ExternalLink>
+				{ setShowHelpCenter ? (
+					<Button
+						onClick={ () => {
+							setShowHelpCenter( true );
+							setShowSupportDoc( supportUrl );
+						} }
+						className="components-seo-upsell__learn-more-link"
+						variant="link"
+					>
+						{ supportLinkTitle }
+					</Button>
+				) : (
+					<ExternalLink href={ supportUrl }>{ supportLinkTitle }</ExternalLink>
+				) }
 			</div>
 
 			<Button
