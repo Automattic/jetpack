162993ba3a98ec8d25e71116d09219ec2a306bc7
Jason Moon <noreply@jsnmoon.com>
2024-09-12 15:45:59 -0700

    Search: Add cached getter for search options

--- projects/packages/search/src/class-options.php
+++ projects/packages/search/src/class-options.php
@@ -92,4 +92,40 @@ class Options {
 		 */
 		return apply_filters( 'jetpack_search_has_vip_index', $has_vip_index );
 	}
+
+	/**
+	 * Returns the cache key for an option if persisted with get_cached_option method below.
+	 *
+	 * @return bool
+	 */
+	public static function get_cached_option_key( $option_name ) {
+		return 'jetpack-search-' . Package::version . '-' . $option_name;
+	}
+
+	/**
+	 * Checks the cache for the option. If it doesn't exist, fetch from DB and persist in the cache.
+	 * Designed to be used for "pre_option_${option_name}" filters.
+	 *
+	 * @return bool
+	 */
+	public static function get_cached_option( $option_name, $default_value = false, $cache_timeout = 3600 ) {
+		$cache_key = self::get_cached_option_key( $option_name );
+		$value = wp_cache_get( $cache_key );
+		if ( false === $value ) {
+			$value = get_option( $option_name, $default );
+			wp_cache_set( $cache_key, $value, '', $cache_timeout );
+		}
+		return $value;
+	}
+
+	/**
+	 * Updates the cache for the option.
+	 * Designed to be used for "updated_option" actions.
+	 *
+	 * @return bool
+	 */
+	public static function update_cached_option( $option_name, $value ) {
+		$option_key = self::get_cached_option_key( $option_name );
+		wp_cache_set( $option_key, $value );
+	}
 }
