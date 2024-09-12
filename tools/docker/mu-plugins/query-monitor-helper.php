8062f8e2facc5859fd3dd64e8437daa83c8c1efc
Jeremy Herve <jeremy@jeremy.hu>
2024-09-12 12:12:21 +0200

    Apply indenting / spacing / phpcs fixes to avoid linting errors

--- tools/docker/mu-plugins/query-monitor-helper.php
+++ tools/docker/mu-plugins/query-monitor-helper.php
@@ -29,34 +29,47 @@
  */
 
 
-add_filter('qm/component_type/unknown', function($type, $file, $name, $context) {
-    if (strpos($file, 'projects/plugins') !== false) {
-        return 'plugin';
-    }
-    if (strpos($file, 'projects/packages') !== false) {
-        return 'other';
-    }
-    return $type;
-}, 10, 4);
+add_filter(
+	'qm/component_type/unknown',
+	function ( $type, $file, $name, $context ) { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
+		if ( strpos( $file, 'projects/plugins' ) !== false ) {
+			return 'plugin';
+		}
+		if ( strpos( $file, 'projects/packages' ) !== false ) {
+			return 'other';
+		}
+		return $type;
+	},
+	10,
+	4
+);
 
-// Hook into qm/component_name/plugin
-add_filter('qm/component_name/plugin', function($name, $file) {
-    if (strpos($file, 'projects/plugins/') !== false) {
-        $parts = explode('/', $file);
-        $plugin_index = array_search('plugins', $parts);
-        if ($plugin_index !== false && isset($parts[$plugin_index + 1])) {
-            return 'Plugin: ' . $parts[$plugin_index + 1];
-        }
-    }
-    return $name;
-}, 10, 2);
+add_filter(
+	'qm/component_name/plugin',
+	function ( $name, $file ) {
+		if ( strpos( $file, 'projects/plugins/' ) !== false ) {
+			$parts        = explode( '/', $file );
+			$plugin_index = array_search( 'plugins', $parts, true );
+			if ( $plugin_index !== false && isset( $parts[ $plugin_index + 1 ] ) ) {
+				return 'Plugin: ' . $parts[ $plugin_index + 1 ];
+			}
+		}
+		return $name;
+	},
+	10,
+	2
+);
 
-// Hook into qm/component_name/package
-add_filter('qm/component_name/other', function($name, $file) {
-    if (strpos($file, 'projects/packages/') !== false) {
-        $parts = explode('/', $file);
-        $package_name = $parts[array_search('packages', $parts) + 1] ?? '';
-        return 'Jetpack Package: ' . $package_name;
-    }
-    return $name;
-}, 10, 2);
+add_filter(
+	'qm/component_name/other',
+	function ( $name, $file ) {
+		if ( strpos( $file, 'projects/packages/' ) !== false ) {
+			$parts        = explode( '/', $file );
+			$package_name = $parts[ array_search( 'packages', $parts, true ) + 1 ] ?? '';
+			return 'Jetpack Package: ' . $package_name;
+		}
+		return $name;
+	},
+	10,
+	2
+);
