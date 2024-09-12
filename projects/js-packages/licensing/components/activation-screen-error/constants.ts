2deccb089c5376e8d8edc6b8faee46022539baff
Bryan Elliott <bryan@elliottprogrammer.com>
2024-09-12 14:02:56 -0400

    License activation: Add helpful error messages & display help steps on error response. (#39333)

---
+++ projects/js-packages/licensing/components/activation-screen-error/constants.ts
@@ -0,0 +1,9 @@
+export const LICENSE_ERRORS = {
+	NOT_SAME_OWNER: '[not_same_owner]',
+	ACTIVE_ON_SAME_SITE: '[active_on_same_site]',
+	ATTACHED_LICENSE: '[attached_license]',
+	PRODUCT_INCOMPATIBILITY: '[product_incompatibility]',
+	REVOKED_LICENSE: '[revoked_license]',
+	INVALID_INPUT: '[invalid_input]',
+	MULTISITE_INCOMPATIBILITY: '[multisite_incompatibility]',
+} as const;
