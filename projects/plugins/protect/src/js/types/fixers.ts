c0b098c4aa1be0f53707515dd6e36f2986ca6f0b
dkmyta <dkmyta10@gmail.com>
2024-09-12 08:39:00 -0700

    Ensure FixerStatus type matches response structure

--- projects/plugins/protect/src/js/types/fixers.ts
+++ projects/plugins/protect/src/js/types/fixers.ts
@@ -1,6 +1,7 @@
 export type FixerStatus = 'not_started' | 'in_progress' | 'fixed' | 'not_fixed';
 
 export type FixersStatus = {
+	ok: boolean;
 	threats: {
 		[ key: number ]: {
 			status: FixerStatus;
