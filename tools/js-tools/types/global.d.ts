f4230d9416fcb590cddfd5f506cb7ebbfc771fe0
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-09-13 14:12:57 +0200

    LaunchpadSaveModal: Update action href based on experiment variation (#39237)
    * Get the experiment variant name
    
    * Add missing dependency
    
    * Add testing logic
    
    * Update primary action href based on the experiment name
    
    * Introduce session storage item supporting experiment testing
    
    * Add node process type declaration
    
    * Add changelog
    
    * Rename the session item name

--- tools/js-tools/types/global.d.ts
+++ tools/js-tools/types/global.d.ts
@@ -4,6 +4,13 @@ declare module '*.module.scss' {
 	export default classes;
 }
 
+// Add the process declaration
+declare const process: {
+	env: {
+		NODE_ENV: 'development' | 'production' | 'test';
+	};
+};
+
 type AvailableBlockProps =
 	| {
 			available?: boolean;
