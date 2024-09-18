168d3d1820c0f14a631c490436808edde017d72a
Brandon Kraft <public@brandonkraft.com>
2024-09-18 16:38:50 -0500

    Blocks: convert to a module

---
+++ projects/plugins/jetpack/modules/blocks.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Module Name: Blocks
+ * Module Description: Add additional blocks to your site and post editors.
+ * Sort Order: 5
+ * First Introduced: 13.9
+ * Requires Connection: Yes
+ * Auto Activate: Yes
+ * Module Tags: blocks
+ * Feature: Writing
+ *
+ * @package automattic/jetpack
+ */
