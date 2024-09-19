7b88491851da4104095631d680ad2933cb717576
Brandon Kraft <public@brandonkraft.com>
2024-09-19 10:21:23 -0500

    Remove connection requirement

--- projects/plugins/jetpack/modules/blocks.php
+++ projects/plugins/jetpack/modules/blocks.php
@@ -4,7 +4,7 @@
  * Module Description: Add additional blocks to your site and post editors.
  * Sort Order: 5
  * First Introduced: 13.9
- * Requires Connection: Yes
+ * Requires Connection: No
  * Auto Activate: Yes
  * Module Tags: blocks
  * Feature: Writing
