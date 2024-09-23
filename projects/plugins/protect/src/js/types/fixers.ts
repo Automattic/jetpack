c2ff8a849956cad8443c6832241390415542a1d5
dkmyta <dkmyta10@gmail.com>
2024-09-23 15:33:28 -0700

    Update FixerStatus type to account for potential error props

--- projects/plugins/protect/src/js/types/fixers.ts
+++ projects/plugins/protect/src/js/types/fixers.ts
@@ -2,12 +2,14 @@ export type FixerStatus = 'not_started' | 'in_progress' | 'fixed' | 'not_fixed';
 
 export type FixersStatus = {
 	ok: boolean;
-	threats: {
+	error?: string;
+	threats?: {
 		[ key: number ]: ThreatFixStatus;
 	};
 };
 
 export type ThreatFixStatus = {
-	status: FixerStatus;
-	last_updated: string;
+	error?: string;
+	status?: FixerStatus;
+	last_updated?: string;
 };
