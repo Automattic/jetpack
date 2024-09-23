3bc35a753ccb4289097659664caf66a1f296b351
Foteini Giannaropoulou <giannaropoulou.foteini@gmail.com>
2024-09-23 11:51:33 +0300

    Deprecate masterbar.php instead of removing it

---
+++ projects/plugins/jetpack/modules/masterbar.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The WordPress.com Toolbar and Dashboard customizations Module has been deprecated and this file
+ * will be removed in an upcoming Jetpack release.
+ *
+ * @deprecated $$$next-version$
+ *
+ * @package automattic/jetpack
+ */
+
+_deprecated_file( __FILE__, 'jetpack-$$next-version$$' );
