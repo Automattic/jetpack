eaa996ed95380303562abf4aa057735608ee41b7
Richard Ortiz <rcrd.ortiz@gmail.com>
2024-09-26 14:24:47 +0200

    My Jetpack: fix highlights and purchases API calls when not connected (#39522)
    * We now disable the API call if Jetpack is not connect
    
    * changelog
    
    * Removed placeholder data
    
    * Removed enabled check from purchases
    
    * Returning null path when no userId for highlights endpoint
    
    * Updated implementation to rely oon not using a path instead of disabling
    
    * Changed from using the siteID as discriminator to using isSiteConnected
    
    * Changed to use enabled property again

--- projects/packages/my-jetpack/_inc/components/redeem-token-screen/index.jsx
+++ projects/packages/my-jetpack/_inc/components/redeem-token-screen/index.jsx
@@ -11,7 +11,7 @@ import { includesLifetimePurchase } from '../../utils/is-lifetime-purchase';
  * @return {object} The RedeemTokenScreen component.
  */
 export default function RedeemTokenScreen() {
-	const { userConnectionData } = useMyJetpackConnection();
+	const { userConnectionData, isSiteConnected } = useMyJetpackConnection();
 	// They might not have a display name set in wpcom, so fall back to wpcom login or local username.
 	const displayName =
 		userConnectionData?.currentUser?.wpcomUser?.display_name ||
@@ -19,9 +19,8 @@ export default function RedeemTokenScreen() {
 		userConnectionData?.currentUser?.username;
 	const { isLoading, data: purchases } = useSimpleQuery( {
 		name: QUERY_PURCHASES_KEY,
-		query: {
-			path: REST_API_SITE_PURCHASES_ENDPOINT,
-		},
+		query: { path: REST_API_SITE_PURCHASES_ENDPOINT },
+		options: { enabled: isSiteConnected },
 	} );
 
 	if ( isLoading ) {
