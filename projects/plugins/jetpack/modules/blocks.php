3472baf42d0e422cda4b287078cc79158560c53c
Brandon Kraft <public@brandonkraft.com>
2024-09-26 11:37:35 -0500

    Move module activation code to the module itself

--- projects/plugins/jetpack/modules/blocks.php
+++ projects/plugins/jetpack/modules/blocks.php
@@ -11,3 +11,20 @@
  *
  * @package automattic/jetpack
  */
+
+add_action( 'jetpack_activate_module_blocks', 'jetpack_blocks_activate_module' );
+
+/**
+ * Actions needed upon activating the blocks module.
+ *
+ * There is a legacy option to disable Jetpack blocks that we'll delete when this module is activated.
+ * Via jetpack_get_default_modules filter, we remove blocks from the default if the option is true.
+ * We'll leave that in place so _until the module is activated_ we will be sure to respect the previous
+ * setting.
+ *
+ * @since $$next-version$$
+ * @return void
+ */
+function jetpack_blocks_activate_module() {
+	delete_option( 'jetpack_blocks_disabled' ); // The function will check and return early if not present.
+}
