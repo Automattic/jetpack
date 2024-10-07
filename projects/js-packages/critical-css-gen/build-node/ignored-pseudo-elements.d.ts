fd282f61ba9735d43c74f25b1e4e1f63f579d8be
Nate Weller <hello@nateweller.com>
2024-10-07 10:43:08 -0600

    Remove built files from critical-css-gen js-package

--- projects/js-packages/critical-css-gen/build-node/ignored-pseudo-elements.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Remove pseudo elements that are ignored while matching elements above the fold.
- *
- * @param {string} selector - selector to filter.
- *
- * @return {string} selector with ignored pseudo elements removed.
- */
-export declare function removeIgnoredPseudoElements(selector: string): string;
--- projects/js-packages/critical-css-gen/build-node/node.d.ts
+++
@@ -1,5 +0,0 @@
-export { BrowserInterfacePlaywright } from './browser-interface-playwright.js';
-export { BrowserInterface } from './browser-interface.js';
-export { generateCriticalCSS } from './generate-critical-css.js';
-export * from './errors.js';
-export declare const version = "0.0.11";
