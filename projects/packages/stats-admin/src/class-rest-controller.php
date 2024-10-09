79d2478c9770a8d6c511b80be86eb69db48154f8
Jasper Kang <jasper.kang@automattic.com>
2024-10-09 13:23:29 +1300

    comment

--- projects/packages/stats-admin/src/class-rest-controller.php
+++ projects/packages/stats-admin/src/class-rest-controller.php
@@ -604,7 +604,9 @@ class REST_Controller {
 			return $post;
 		}
 
-		// It shouldn't be a problem because only title and ID are exposed.
+		// The endpoint should be as compatible as possible with `/sites/$site_id/posts/$post_id`.
+		// The reason we are not forwarding the request is that `/sites/$site_id/posts/$post_id` might require user tokens for private posts/sites, which is not possible for users without a WordPress.com account.
+		// 'like_count' is not included in the response because it's available through another endpoint `/sites/$site_id/posts/$post_id/likes`.
 		return array(
 			'ID'             => $post->ID,
 			'site_ID'        => Jetpack_Options::get_option( 'id' ),
