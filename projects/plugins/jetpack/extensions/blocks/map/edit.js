b28314e7951c8137d33c9492472a679416e528a0
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2024-10-15 17:46:58 -0300

    Default to mapHeight and fall back to 400px

--- projects/plugins/jetpack/extensions/blocks/map/edit.js
+++ projects/plugins/jetpack/extensions/blocks/map/edit.js
@@ -243,7 +243,7 @@ const MapEdit = ( {
 				<img
 					alt={ __( 'Map Preview', 'jetpack' ) }
 					src={ mapStyleObject ? mapStyleObject.preview : previewPlaceholder }
-					style={ { width: '100%', maxHeight: '400px', objectFit: 'cover' } }
+					style={ { width: '100%', height: mapHeight || '400px', objectFit: 'cover' } }
 				/>
 			</div>
 		);
