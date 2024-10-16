479612f3da471d40cadb92e0e9f8528e9cd4aa3c
Miguel Lezama <lezama@gmail.com>
2024-10-15 23:03:23 -0300

    working version

--- projects/plugins/jetpack/extensions/blocks/subscriptions/editor.js
+++ projects/plugins/jetpack/extensions/blocks/subscriptions/editor.js
@@ -1,13 +1,17 @@
-import { registerJetpackPlugin } from '@automattic/jetpack-shared-extension-utils';
+import { registerJetpackPlugin, useAnalytics } from '@automattic/jetpack-shared-extension-utils';
 import { createBlock } from '@wordpress/blocks';
 import { select } from '@wordpress/data';
+import { PluginPreviewMenuItem } from '@wordpress/editor';
+import { useState } from '@wordpress/element';
 import { addFilter } from '@wordpress/hooks';
-import { atSymbol } from '@wordpress/icons';
+import { __ } from '@wordpress/i18n';
+import { atSymbol, send } from '@wordpress/icons';
 import { registerJetpackBlockFromMetadata } from '../../shared/register-jetpack-block';
 import metadata from './block.json';
 import CommandPalette from './command-palette';
 import deprecated from './deprecated';
 import edit from './edit';
+import { NewsletterPreviewModal } from './email-preview';
 import NewsletterMenu from './menu';
 import SubscribePanels from './panel';
 
@@ -71,13 +75,44 @@ const shouldShowNewsletterMenu = () => {
 	return isPost;
 };
 
+const SubscriptionsEditorAdditions = () => {
+	const [ isPreviewModalOpen, setIsPreviewModalOpen ] = useState( false );
+	const { tracks } = useAnalytics();
+
+	const postId = select( 'core/editor' ).getCurrentPostId();
+
+	return (
+		<>
+			<PluginPreviewMenuItem
+				onClick={ () => {
+					tracks.recordEvent( 'jetpack_newsletter_preview_menu_item_click' );
+					setIsPreviewModalOpen( true );
+				} }
+				icon={ send }
+			>
+				{ __( 'Email Preview', 'jetpack' ) }
+			</PluginPreviewMenuItem>
+			<NewsletterPreviewModal
+				isOpen={ isPreviewModalOpen }
+				onClose={ () => setIsPreviewModalOpen( false ) }
+				postId={ postId }
+			/>
+		</>
+	);
+};
+
 // Registers slot/fill panels defined via settings.render and command palette commands
 registerJetpackPlugin( blockName, {
 	render: () => {
 		return (
 			<>
 				<SubscribePanels />
-				{ shouldShowNewsletterMenu() && <NewsletterMenu /> }
+				{ shouldShowNewsletterMenu() && (
+					<>
+						<SubscriptionsEditorAdditions />
+						<NewsletterMenu />
+					</>
+				) }
 				<CommandPalette />
 			</>
 		);
