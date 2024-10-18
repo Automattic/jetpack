76c0fa366a08888271e95e047aa1dfa63ae6eef4
Jeremy Herve <jeremy@jeremy.hu>
2024-10-18 19:20:46 +0200

    Avoid PHP notices when widget is first added

--- projects/packages/search/src/widgets/class-search-widget.php
+++ projects/packages/search/src/widgets/class-search-widget.php
@@ -637,8 +637,8 @@ class Search_Widget extends \WP_Widget {
 		// Keep `search_box_enabled` and `user_sort_enabled` settings when updating widget on Instant Search
 		// Set `search_box_enabled` and `user_sort_enabled` default to '1' when createing a NEW widget
 		if ( Options::is_instant_enabled() ) {
-			$instance['search_box_enabled'] = empty( $old_instance ) ? '1' : $old_instance['search_box_enabled'];
-			$instance['user_sort_enabled']  = empty( $old_instance ) ? '1' : $old_instance['user_sort_enabled'];
+			$instance['search_box_enabled'] = empty( $old_instance ) || empty( $old_instance['search_box_enabled'] ) ? '1' : $old_instance['search_box_enabled'];
+			$instance['user_sort_enabled']  = empty( $old_instance ) || empty( $old_instance['user_sort_enabled'] ) ? '1' : $old_instance['user_sort_enabled'];
 		} else {
 			$instance['search_box_enabled'] = empty( $new_instance['search_box_enabled'] ) ? '0' : '1';
 			$instance['user_sort_enabled']  = empty( $new_instance['user_sort_enabled'] ) ? '0' : '1';
