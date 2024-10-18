a4d1801e6507c41719ce776d2c4ed33137a1a648
dkmyta <43220201+dkmyta@users.noreply.github.com>
2024-10-18 12:35:34 -0700

    Protect: Add/apply shared AdminSectionHero component (#39702)

--- projects/plugins/protect/src/js/components/seventy-five-layout/index.tsx
+++ projects/plugins/protect/src/js/components/seventy-five-layout/index.tsx
@@ -1,6 +1,18 @@
 import { Container, Col, useBreakpointMatch } from '@automattic/jetpack-components';
 import React from 'react';
 
+// Define the props interface for the SeventyFiveLayout component
+interface SeventyFiveLayoutProps {
+	spacing?: number;
+	gap?: number;
+	main: React.ReactNode;
+	mainClassName?: string;
+	secondary: React.ReactNode;
+	secondaryClassName?: string;
+	preserveSecondaryOnMobile?: boolean;
+	fluid?: boolean;
+}
+
 /**
  * SeventyFive layout meta component
  * The component name references to
@@ -10,13 +22,27 @@ import React from 'react';
  * in large lg viewport size.
  *
  * @param {object}          props                           - Component props
+ * @param {number}          props.spacing                   - Horizontal spacing
+ * @param {number}          props.gap                       - Horizontal gap
  * @param {React.ReactNode} props.main                      - Main section component
+ * @param {string}          props.mainClassName             - Main section class name
  * @param {React.ReactNode} props.secondary                 - Secondary section component
+ * @param {string}          props.secondaryClassName        - Secondary section class name
  * @param {boolean}         props.preserveSecondaryOnMobile - Whether to show secondary section on mobile
  * @param {boolean}         props.fluid                     - Whether to use fluid layout
  * @return {React.ReactNode} - React meta-component
  */
-const SeventyFiveLayout = ( { main, secondary, preserveSecondaryOnMobile = false, fluid } ) => {
+const SeventyFiveLayout: React.FC< SeventyFiveLayoutProps > = ( {
+	spacing = 0,
+	gap = 0,
+	main,
+	mainClassName,
+	secondary,
+	secondaryClassName,
+	preserveSecondaryOnMobile = false,
+	fluid,
+} ) => {
+	// Ensure the correct typing for useBreakpointMatch
 	const [ isSmall, isLarge ] = useBreakpointMatch( [ 'sm', 'lg' ] );
 
 	/*
@@ -27,14 +53,14 @@ const SeventyFiveLayout = ( { main, secondary, preserveSecondaryOnMobile = false
 	const hideSecondarySection = ! preserveSecondaryOnMobile && isSmall;
 
 	return (
-		<Container horizontalSpacing={ 0 } horizontalGap={ 0 } fluid={ fluid }>
+		<Container horizontalSpacing={ spacing } horizontalGap={ gap } fluid={ fluid }>
 			{ ! hideSecondarySection && (
 				<>
-					<Col sm={ 12 } md={ 4 } lg={ 6 }>
+					<Col className={ mainClassName } sm={ 12 } md={ 4 } lg={ 6 }>
 						{ main }
 					</Col>
 					{ isLarge && <Col lg={ 1 } /> }
-					<Col sm={ 12 } md={ 4 } lg={ 5 }>
+					<Col className={ secondaryClassName } sm={ 12 } md={ 4 } lg={ 5 }>
 						{ secondary }
 					</Col>
 				</>
