0f4dadc2d6cb80a721980c7154a46db2b9f8ccf2
Manzoor Wani <manzoorwani.jk@gmail.com>
2024-10-22 04:35:25 -0700

    Social | Fix reconnection for broken Bluesky connections (#39844)

--- projects/js-packages/publicize-components/src/components/services/custom-inputs.tsx
+++ projects/js-packages/publicize-components/src/components/services/custom-inputs.tsx
@@ -1,6 +1,9 @@
+import { Alert } from '@automattic/jetpack-components';
 import { ExternalLink } from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
 import { createInterpolateElement, useId } from '@wordpress/element';
 import { __, _x } from '@wordpress/i18n';
+import { store } from '../../social-store';
 import { SupportedService } from '../services/use-supported-services';
 import styles from './style.module.scss';
 
@@ -16,6 +19,8 @@ type CustomInputsProps = {
  */
 export function CustomInputs( { service }: CustomInputsProps ) {
 	const id = useId();
+
+	const reconnectingAccount = useSelect( select => select( store ).getReconnectingAccount(), [] );
 
 	if ( 'mastodon' === service.ID ) {
 		return (
@@ -58,6 +63,11 @@ export function CustomInputs( { service }: CustomInputsProps ) {
 						required
 						type="text"
 						name="handle"
+						defaultValue={
+							reconnectingAccount?.service_name === 'bluesky'
+								? reconnectingAccount?.external_name
+								: undefined
+						}
 						autoComplete="off"
 						autoCapitalize="off"
 						autoCorrect="off"
@@ -93,6 +103,11 @@ export function CustomInputs( { service }: CustomInputsProps ) {
 						aria-describedby={ `${ id }-password-description` }
 						placeholder={ 'xxxx-xxxx-xxxx-xxxx' }
 					/>
+					{ reconnectingAccount?.service_name === 'bluesky' && (
+						<Alert level="error" showIcon={ false }>
+							{ __( 'Please provide an app password to fix the connection.', 'jetpack' ) }
+						</Alert>
+					) }
 				</div>
 			</>
 		);
