6dcbdf2a97f91b0ebdfa9c2ecc92b0b080924ae4
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2024-10-23 10:05:33 -0600

    Bypass My Jetpack connection page

--- projects/plugins/jetpack/extensions/shared/components/connect-banner/index.tsx
+++ projects/plugins/jetpack/extensions/shared/components/connect-banner/index.tsx
@@ -4,6 +4,7 @@
 import { useConnection } from '@automattic/jetpack-connection';
 import { useAnalytics } from '@automattic/jetpack-shared-extension-utils';
 import { __ } from '@wordpress/i18n';
+import { useState, useEffect } from 'react';
 /*
  * Internal dependencies
  */
@@ -26,20 +27,34 @@ const getRedirectUri = () => {
 };
 
 const ConnectBanner: FC< ConnectBannerProps > = ( { block, explanation = null } ) => {
+	const [ isRedirecting, setIsRedirecting ] = useState( false );
+	const [ isSaving, setIsSaving ] = useState( false );
 	const { handleConnectUser } = useConnection( {
 		from: 'editor',
 		redirectUri: getRedirectUri(),
 		autoTrigger: false,
 		skipPricingPage: true,
 	} );
-	const { autosaveAndRedirect, isRedirecting } = useAutosaveAndRedirect();
+	const { autosave } = useAutosaveAndRedirect();
 	const { tracks } = useAnalytics();
 
 	const goToCheckoutPage = ( event: MouseEvent< HTMLButtonElement > ) => {
-		handleConnectUser();
-		tracks.recordEvent( 'jetpack_editor_connect_banner_click', { block } );
-		autosaveAndRedirect( event );
+		setIsSaving( true );
+		autosave( event ).then( () => {
+			tracks.recordEvent( 'jetpack_editor_connect_banner_click', { block } );
+			setIsRedirecting( true );
+			setIsSaving( false );
+		} );
 	};
+
+	useEffect( () => {
+		// The redirection is handled this way to ensure we get the right redirectUri
+		// In the case that the post is new and unsaved, the component requires a re-render
+		// in order to get the correct URI when the handler is called.
+		if ( isRedirecting ) {
+			handleConnectUser();
+		}
+	}, [ isRedirecting, handleConnectUser ] );
 
 	return (
 		<div>
@@ -49,7 +64,7 @@ const ConnectBanner: FC< ConnectBannerProps > = ( { block, explanation = null } 
 				description={ __( 'Your account is not connected to Jetpack at the moment.', 'jetpack' ) }
 				goToCheckoutPage={ goToCheckoutPage }
 				checkoutUrl={ '#' }
-				isRedirecting={ isRedirecting }
+				isRedirecting={ isRedirecting || isSaving }
 			/>
 			<div className="jetpack-connect-banner">
 				{ explanation && (
