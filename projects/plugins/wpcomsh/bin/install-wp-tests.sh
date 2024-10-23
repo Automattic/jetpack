b10273825c93a58e3e5d6fbd1218e48b0e13a4bc
Igor Zinovyev <zinigor@gmail.com>
2024-10-23 20:31:32 +0300

    Added DB password slash escaping.

--- projects/plugins/wpcomsh/bin/install-wp-tests.sh
+++ projects/plugins/wpcomsh/bin/install-wp-tests.sh
@@ -118,7 +118,7 @@ install_test_suite() {
 		sed $ioption "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR/':" "$WP_TESTS_DIR"/wp-tests-config.php
 		sed $ioption "s/youremptytestdbnamehere/$DB_NAME/" "$WP_TESTS_DIR"/wp-tests-config.php
 		sed $ioption "s/yourusernamehere/$DB_USER/" "$WP_TESTS_DIR"/wp-tests-config.php
-		sed $ioption "s/yourpasswordhere/$DB_PASS/" "$WP_TESTS_DIR"/wp-tests-config.php
+		sed $ioption "s/yourpasswordhere/${DB_PASS//\//\\/}/" "$WP_TESTS_DIR"/wp-tests-config.php
 		sed $ioption "s|localhost|${DB_HOST}|" "$WP_TESTS_DIR"/wp-tests-config.php
 	fi
 
