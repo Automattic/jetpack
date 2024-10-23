66c11b37ee315f193b8ad69510c332384983e85d
Adnan Haque <adnan007.id@gmail.com>
2024-10-23 21:33:34 +0300

    Add default values

--- projects/plugins/boost/app/lib/Foundation_Pages.php
+++ projects/plugins/boost/app/lib/Foundation_Pages.php
@@ -2,9 +2,14 @@
 
 namespace Automattic\Jetpack_Boost\Lib;
 
+use Automattic\Jetpack\Schema\Schema;
 use Automattic\Jetpack_Boost\Contracts\Has_Setup;
+use Automattic\Jetpack_Boost\Data_Sync\Foundation_Pages_Entry;
 
 class Foundation_Pages implements Has_Setup {
+
+	const PREMIUM_MAX_PAGES = 10;
+	const FREE_MAX_PAGES    = 1;
 
 	public function setup() {
 		if ( ! $this->is_development_features_enabled() ) {
@@ -13,6 +18,13 @@ class Foundation_Pages implements Has_Setup {
 
 		add_filter( 'jetpack_boost_critical_css_providers', array( $this, 'remove_ccss_front_page_provider' ), 10, 2 );
 		add_filter( 'display_post_states', array( $this, 'add_display_post_states' ), 10, 2 );
+		add_action( 'init', array( $this, 'register_ds_stores' ) );
+	}
+
+	public function register_ds_stores() {
+		$schema = Schema::as_array( Schema::as_string() )->fallback( self::default_pages() );
+		jetpack_boost_register_option( 'foundation_pages_list', $schema, new Foundation_Pages_Entry( 'foundation_pages_list' ) );
+		jetpack_boost_register_readonly_option( 'foundation_pages_properties', array( $this, 'get_properties' ) );
 	}
 
 	public function remove_ccss_front_page_provider( $providers ) {
@@ -25,6 +37,70 @@ class Foundation_Pages implements Has_Setup {
 		}
 
 		return $filtered_providers;
+	}
+
+	private function default_pages() {
+		if ( $this->get_max_pages() === static::FREE_MAX_PAGES ) {
+			return array( '/' );
+		}
+
+		$max_pages               = $this->get_max_pages();
+		$yoast_cornerstone_pages = $this->get_yoast_cornerstone_pages();
+		$woocommerce_pages       = $this->get_woocommerce_pages();
+
+		$homepage = array( '/' );
+
+		$urls = array_unique( array_merge( $homepage, $woocommerce_pages, $yoast_cornerstone_pages ) );
+
+		return array_slice( $urls, 0, $max_pages );
+	}
+
+	private function get_yoast_cornerstone_pages() {
+		$max_pages                 = $this->get_max_pages();
+		$yoast_cornerstone_content = get_posts(
+			array(
+				'meta_key'       => '_yoast_wpseo_is_cornerstone',
+				'meta_value'     => '1',
+				'post_type'      => 'any',
+				'posts_per_page' => $max_pages,
+			)
+		);
+
+		$urls = array();
+		foreach ( $yoast_cornerstone_content as $post ) {
+			$permalink = get_permalink( $post->ID );
+			$permalink = $this->make_relative_url( $permalink );
+			$urls[]    = $permalink;
+		}
+
+		return $urls;
+	}
+
+	private function get_woocommerce_pages() {
+		$urls = array();
+		if ( ! function_exists( 'wc_get_page_id' ) ) {
+			return $urls;
+		}
+
+		$shop_page_id = wc_get_page_id( 'shop' );
+		if ( $shop_page_id ) {
+			$url = get_permalink( $shop_page_id );
+			$url = $this->make_relative_url( $url );
+
+			if ( $url ) {
+				$urls[] = $url;
+			}
+		}
+
+		return $urls;
+	}
+
+	private function make_relative_url( $url ) {
+		if ( is_string( $url ) && strpos( $url, home_url() ) === 0 ) {
+			$url = substr( $url, strlen( home_url() ) );
+		}
+
+		return $url;
 	}
 
 	public function get_pages() {
@@ -57,7 +133,7 @@ class Foundation_Pages implements Has_Setup {
 	}
 
 	private function get_max_pages() {
-		return Premium_Features::has_any() ? 10 : 1;
+		return Premium_Features::has_any() ? static::PREMIUM_MAX_PAGES : static::FREE_MAX_PAGES;
 	}
 
 	private function get_blog_url() {
