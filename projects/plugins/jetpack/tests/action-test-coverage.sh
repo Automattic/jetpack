8d8da9c53e6a4d0f74fb1d53ca631503d09f1235
Brad Jorsch <brad.jorsch@automattic.com>
2024-10-31 13:00:28 +0000

    ci: Run tests in parallel
    Mostly for the coverage tests, let's see if we can speed things up a
    bit this way.

--- projects/plugins/jetpack/tests/action-test-coverage.sh
+++ projects/plugins/jetpack/tests/action-test-coverage.sh
@@ -2,23 +2,16 @@
 
 set -eo pipefail
 
-echo "::group::Jetpack Admimnpage coverage"
-pnpm run test-client --coverage --collectCoverageFrom='_inc/client/state/**/*.js' --coverageDirectory="$COVERAGE_DIR/client" --coverageReporters=clover
-pnpm run test-gui --coverage --collectCoverageFrom='_inc/client/**/*.js' --coverageDirectory="$COVERAGE_DIR/gui" --coverageReporters=clover
-echo "::endgroup::"
+declare -A TESTS
+TESTS=()
+# Can't run PHP tests in parallel, it runs into DB deadlocks.
+TESTS[php]="
+	php -dpcov.directory=. \"$(command -v phpunit)\" --coverage-clover \"$COVERAGE_DIR/backend/clover.xml\" &&
+	LEGACY_FULL_SYNC=1 php -dpcov.directory=. \"$(command -v phpunit)\" --group=legacy-full-sync --coverage-clover \"$COVERAGE_DIR/legacy-sync/clover.xml\" &&
+	WP_MULTISITE=1 php -dpcov.directory=. \"$(command -v phpunit)\" -c tests/php.multisite.xml --coverage-clover \"$COVERAGE_DIR/multisite/clover.xml\"
+"
+TESTS[client]="pnpm run test-client --coverage --collectCoverageFrom='_inc/client/state/**/*.js' --coverageDirectory=\"$COVERAGE_DIR/client\" --coverageReporters=clover"
+TESTS[gui]="pnpm run test-gui --coverage --collectCoverageFrom='_inc/client/state/**/*.js' --coverageDirectory=\"$COVERAGE_DIR/client\" --coverageReporters=clover"
+TESTS[extensions]="pnpm run test-extensions --coverage --collectCoverageFrom='_inc/client/state/**/*.js' --coverageDirectory=\"$COVERAGE_DIR/client\" --coverageReporters=clover"
 
-echo "::group::Jetpack Extensions coverage"
-pnpm run test-extensions --coverage --collectCoverageFrom='extensions/**/*.js' --coverageDirectory="$COVERAGE_DIR/extensions" --coverageReporters=clover
-echo "::endgroup::"
-
-echo "::group::Jetpack Backend coverage"
-php -dpcov.directory=. "$(command -v phpunit)" --coverage-clover "$COVERAGE_DIR/backend/clover.xml"
-echo "::endgroup::"
-
-echo "::group::Jetpack Legacy full sync coverage"
-LEGACY_FULL_SYNC=1 php -dpcov.directory=. "$(command -v phpunit)" --group=legacy-full-sync --coverage-clover "$COVERAGE_DIR/legacy-sync/clover.xml"
-echo "::endgroup::"
-
-echo "::group::Jetpack Multisite coverage"
-WP_MULTISITE=1 php -dpcov.directory=. "$(command -v phpunit)" -c tests/php.multisite.xml --coverage-clover "$COVERAGE_DIR/multisite/clover.xml"
-echo "::endgroup::"
+pnpm exec concurrently --kill-others-on-fail --max-processes '100%' --names "$( IFS=,; echo "${!TESTS[*]}" )" "${TESTS[@]}"
