f7d7a52aede41251d6447890307269e9057b5faa
bindlegirl <1242807+bindlegirl@users.noreply.github.com>
2024-11-05 08:56:59 +0100

    Updates based on review

--- projects/plugins/jetpack/enhanced-open-graph.php
+++ projects/plugins/jetpack/enhanced-open-graph.php
@@ -139,7 +139,7 @@ function enhanced_og_video( $tags ) {
 			$secure_video_url = 'https://www.youtube.com/embed/' . $id;
 		} elseif ( strstr( $video_url, 'vimeo' ) ) {
 			preg_match( '|vimeo\.com/(\d+)/?$|i', $video_url, $match );
-			$id               = isset( $match[1] ) ? (int) $match[1] : null;
+			$id               = (int) $match[1];
 			$video_url        = 'http://vimeo.com/moogaloop.swf?clip_id=' . $id;
 			$secure_video_url = 'https://vimeo.com/moogaloop.swf?clip_id=' . $id;
 		}
