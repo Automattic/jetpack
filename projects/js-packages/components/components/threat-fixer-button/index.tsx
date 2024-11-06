95c43e15be9ecd910fd8b1c17308887ac0711408
dkmyta <dkmyta10@gmail.com>
2024-11-06 15:46:36 -0800

    Optimize and improve

--- projects/js-packages/components/components/threat-fixer-button/index.tsx
+++ projects/js-packages/components/components/threat-fixer-button/index.tsx
@@ -1,5 +1,11 @@
 import { Button, IconTooltip } from '@automattic/jetpack-components';
-import { CONTACT_SUPPORT_URL, type Threat, fixerStatusIsStale } from '@automattic/jetpack-scan';
+import {
+	CONTACT_SUPPORT_URL,
+	type Threat,
+	fixerIsInError,
+	fixerIsInProgress,
+	fixerStatusIsStale,
+} from '@automattic/jetpack-scan';
 import { ExternalLink } from '@wordpress/components';
 import { createInterpolateElement, useCallback, useMemo, useState } from '@wordpress/element';
 import { __, sprintf } from '@wordpress/i18n';
@@ -24,106 +30,117 @@ export default function ThreatFixerButton( {
 	onClick: ( items: Threat[] ) => void;
 	className?: string;
 } ): JSX.Element {
+	const fixerInProgress = useMemo( () => {
+		return threat.fixer && fixerIsInProgress( threat.fixer );
+	}, [ threat.fixer ] );
+
+	const fixerError = useMemo( () => {
+		return threat.fixer && fixerIsInError( threat.fixer );
+	}, [ threat.fixer ] );
+
+	const fixerIsStale = useMemo( () => {
+		return threat.fixer && fixerStatusIsStale( threat.fixer );
+	}, [ threat.fixer ] );
+
 	const errorMessage = useMemo( () => {
-		if ( threat.fixer && fixerStatusIsStale( threat.fixer ) ) {
+		if ( fixerIsStale ) {
 			return __( 'The auto-fixer is taking longer than expected.', 'jetpack' );
 		}
 
-		if ( threat.fixer && 'error' in threat.fixer && threat.fixer.error ) {
+		if ( fixerError ) {
 			return __( 'An error occurred auto-fixing this threat.', 'jetpack' );
 		}
 
 		return null;
-	}, [ threat.fixer ] );
+	}, [ fixerIsStale, fixerError ] );
 
 	const popoverText = useMemo( () => {
 		if ( ! threat.fixable ) {
 			return null;
 		}
 
-		if ( threat.fixer && 'status' in threat.fixer && threat.fixer.status === 'in_progress' ) {
+		if ( fixerInProgress ) {
 			return __(
 				'An auto-fixer is in progress. This may take a moment, please check back shortly.',
 				'jetpack'
 			);
 		}
 
-		if ( threat.fixable.fixer === 'delete' ) {
-			if ( threat.filename ) {
-				if ( threat.filename.endsWith( '/' ) ) {
-					return __( 'Deletes the directory that the infected file is in.', 'jetpack' );
+		switch ( threat.fixable.fixer ) {
+			case 'delete':
+				if ( threat.filename ) {
+					if ( threat.filename.endsWith( '/' ) ) {
+						return __( 'Deletes the directory that the infected file is in.', 'jetpack' );
+					}
+
+					if ( threat.signature === 'Core.File.Modification' ) {
+						return __( 'Deletes the unexpected file in a core WordPress directory.', 'jetpack' );
+					}
+
+					return __( 'Deletes the infected file.', 'jetpack' );
 				}
 
-				if ( threat.signature === 'Core.File.Modification' ) {
-					return __( 'Deletes the unexpected file in a core WordPress directory.', 'jetpack' );
+				if ( threat.extension?.type === 'plugin' ) {
+					return __( 'Deletes the plugin directory to fix the threat.', 'jetpack' );
 				}
 
-				return __( 'Deletes the infected file.', 'jetpack' );
-			}
+				if ( threat.extension?.type === 'theme' ) {
+					return __( 'Deletes the theme directory to fix the threat.', 'jetpack' );
+				}
+				break;
+			case 'update':
+				return __( 'Upgrades the plugin or theme to a newer version.', 'jetpack' );
+			case 'replace':
+			case 'rollback':
+				if ( threat.filename ) {
+					return threat.signature === 'Core.File.Modification'
+						? __(
+								'Replaces the modified core WordPress file with the original clean version from the WordPress source code.',
+								'jetpack'
+						  )
+						: __(
+								'Replaces the infected file with a previously backed up version that is clean.',
+								'jetpack'
+						  );
+				}
 
-			if ( threat.extension?.type === 'plugin' ) {
-				return __( 'Deletes the plugin directory to fix the threat.', 'jetpack' );
-			}
-
-			if ( threat.extension?.type === 'theme' ) {
-				return __( 'Deletes the theme directory to fix the threat.', 'jetpack' );
-			}
-		}
-
-		if ( threat.fixable.fixer === 'update' ) {
-			return __( 'Upgrades the plugin or theme to a newer version.', 'jetpack' );
-		}
-
-		if ( [ 'replace', 'rollback' ].includes( threat.fixable.fixer ) ) {
-			if ( threat.filename ) {
-				if ( threat.signature === 'Core.File.Modification' ) {
+				if ( threat.signature === 'php_hardening_WP_Config_NoSalts_001' ) {
 					return __(
-						'Replaces the modified core WordPress file with the original clean version from the WordPress source code.',
+						'Replaces the default salt keys in wp-config.php with unique ones.',
 						'jetpack'
 					);
 				}
-
-				return __(
-					'Replaces the infected file with a previously backed up version that is clean.',
-					'jetpack'
-				);
-			}
-
-			if ( threat.signature === 'php_hardening_WP_Config_NoSalts_001' ) {
-				return __( 'Replaces the default salt keys in wp-config.php with unique ones.', 'jetpack' );
-			}
+				break;
+			default:
+				return __( 'An auto-fixer is available.', 'jetpack' );
 		}
-
-		return __( 'An auto-fixer is available.', 'jetpack' );
-	}, [ threat ] );
+	}, [ threat, fixerInProgress ] );
 
 	const buttonText = useMemo( () => {
 		if ( ! threat.fixable ) {
 			return null;
 		}
 
-		if ( threat.fixer && 'error' in threat.fixer && threat.fixer.error ) {
+		if ( fixerError ) {
 			return __( 'Error', 'jetpack' );
 		}
 
-		if ( threat.fixer && 'status' in threat.fixer && threat.fixer.status === 'in_progress' ) {
+		if ( fixerInProgress ) {
 			return __( 'Fixing…', 'jetpack' );
 		}
 
-		if ( threat.fixable.fixer === 'delete' ) {
-			return __( 'Delete', 'jetpack' );
+		switch ( threat.fixable.fixer ) {
+			case 'delete':
+				return __( 'Delete', 'jetpack' );
+			case 'update':
+				return __( 'Update', 'jetpack' );
+			case 'replace':
+			case 'rollback':
+				return __( 'Replace', 'jetpack' );
+			default:
+				return __( 'Fix', 'jetpack' );
 		}
-
-		if ( threat.fixable.fixer === 'update' ) {
-			return __( 'Update', 'jetpack' );
-		}
-
-		if ( [ 'replace', 'rollback' ].includes( threat.fixable.fixer ) ) {
-			return __( 'Replace', 'jetpack' );
-		}
-
-		return __( 'Fix', 'jetpack' );
-	}, [ threat.fixable, threat.fixer ] );
+	}, [ threat.fixable, fixerError, fixerInProgress ] );
 
 	const [ showTooltip, setShowTooltip ] = useState( false );
 
@@ -156,19 +173,10 @@ export default function ThreatFixerButton( {
 				onClick={ errorMessage ? handleErrorClick : handleClick }
 				children={ buttonText }
 				className={ className }
-				disabled={
-					threat.fixer &&
-					'status' in threat.fixer &&
-					threat.fixer.status === 'in_progress' &&
-					! errorMessage
-				}
-				isLoading={
-					threat.fixer && 'status' in threat.fixer && threat.fixer.status === 'in_progress'
-				}
+				disabled={ fixerInProgress && ! errorMessage }
+				isLoading={ fixerInProgress }
 				isDestructive={
-					( threat.fixable && threat.fixable.fixer === 'delete' ) ||
-					( threat.fixer && 'error' in threat.fixer && threat.fixer.error ) ||
-					( threat.fixer && fixerStatusIsStale( threat.fixer ) )
+					( threat.fixable && threat.fixable.fixer === 'delete' ) || fixerError || fixerIsStale
 				}
 				style={ { minWidth: '72px' } }
 			/>
