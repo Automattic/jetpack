2fb79828df8d31132f16b9e840715599e273a2d4
Karen Attfield <karenlattfield@gmail.com>
2024-11-07 08:13:34 +0000

    Separate all properties but supports from Payments Intro deprecation export default object for consistency

--- projects/plugins/jetpack/extensions/blocks/payments-intro/deprecated/v1/index.js
+++ projects/plugins/jetpack/extensions/blocks/payments-intro/deprecated/v1/index.js
@@ -1,9 +1,10 @@
 import { InnerBlocks } from '@wordpress/block-editor';
-import attributes from './attributes';
 import supports from './supports';
 
+export { default as attributes } from './attributes';
+
+export const save = () => <InnerBlocks.Content />;
+
 export default {
-	attributes,
 	supports,
-	save: () => <InnerBlocks.Content />,
 };
