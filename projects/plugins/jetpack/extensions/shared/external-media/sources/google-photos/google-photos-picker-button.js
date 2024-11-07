2eec15a51de1f96ec2a50c12316c8ca80843e045
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-11-07 20:11:06 +0100

    Add session creation logic

--- projects/plugins/jetpack/extensions/shared/external-media/sources/google-photos/google-photos-picker-button.js
+++ projects/plugins/jetpack/extensions/shared/external-media/sources/google-photos/google-photos-picker-button.js
@@ -3,7 +3,9 @@ import { __ } from '@wordpress/i18n';
 import { Icon, external } from '@wordpress/icons';
 import mediaImage from '../../../../../images/media.svg';
 
-export default function GooglePhotosPickerButton() {
+export default function GooglePhotosPickerButton( props ) {
+	const { pickerSession } = props;
+
 	return (
 		<div className="jetpack-external-media__google-photos-picker">
 			<img src={ mediaImage } width="150" alt={ __( 'Google Photos', 'jetpack' ) } />
@@ -11,7 +13,7 @@ export default function GooglePhotosPickerButton() {
 			<h1>{ __( 'Google Photos', 'jetpack' ) }</h1>
 			<p>{ __( 'Select photos directly from your Google Photos library.', 'jetpack' ) }</p>
 
-			<Button variant="primary">
+			<Button variant="primary" isBusy={ ! pickerSession }>
 				{ __( 'Open Google Photos Picker', 'jetpack' ) }
 				&nbsp;
 				<Icon icon={ external } size={ 18 } />
