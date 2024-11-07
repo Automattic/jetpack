5e9dba39f53ab1c0f47aa3115ade7743c45df84b
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-11-07 14:35:05 +0100

    Fix linting issues

--- projects/plugins/jetpack/extensions/shared/external-media/sources/google-photos/google-photos-picker-button.js
+++ projects/plugins/jetpack/extensions/shared/external-media/sources/google-photos/google-photos-picker-button.js
@@ -3,14 +3,14 @@ import { __ } from '@wordpress/i18n';
 import { Icon, external } from '@wordpress/icons';
 import mediaImage from '../../../../../images/media.svg';
 
-export default function GooglePhotosPickerButton( props ) {
+export default function GooglePhotosPickerButton() {
 	return (
 		<div className="jetpack-external-media__google-photos-picker">
 			<img src={ mediaImage } width="150" alt={ __( 'Google Photos', 'jetpack' ) } />
-			
+
 			<h1>{ __( 'Google Photos', 'jetpack' ) }</h1>
 			<p>{ __( 'Select photos directly from your Google Photos library.', 'jetpack' ) }</p>
-			
+
 			<Button variant="primary">
 				{ __( 'Open Google Photos Picker', 'jetpack' ) }
 				&nbsp;
