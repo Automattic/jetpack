558ad0930367fb4297a07ecaa3efa01d6b12b75d
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-11-07 12:43:41 +0100

    Photos Picker: Create Google Photos Picker component

---
+++ projects/plugins/jetpack/extensions/shared/external-media/sources/google-photos/google-photos-picker-button.js
@@ -0,0 +1,21 @@
+import { Button } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { Icon, external } from '@wordpress/icons';
+import mediaImage from '../../../../../images/media.svg';
+
+export default function GooglePhotosPickerButton( props ) {
+	return (
+		<div className="jetpack-external-media__google-photos-picker">
+			<img src={ mediaImage } width="150" alt={ __( 'Google Photos', 'jetpack' ) } />
+			
+			<h1>{ __( 'Google Photos', 'jetpack' ) }</h1>
+			<p>{ __( 'Select photos directly from your Google Photos library.', 'jetpack' ) }</p>
+			
+			<Button variant="primary">
+				{ __( 'Open Google Photos Picker', 'jetpack' ) }
+				&nbsp;
+				<Icon icon={ external } size={ 18 } />
+			</Button>
+		</div>
+	);
+}
