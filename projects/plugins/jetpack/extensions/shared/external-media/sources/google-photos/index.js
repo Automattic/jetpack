cb58ae9af96776dd7d8d4e34e146771ef74d77cf
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-11-07 20:11:06 +0100

    Add session creation logic

--- projects/plugins/jetpack/extensions/shared/external-media/sources/google-photos/index.js
+++ projects/plugins/jetpack/extensions/shared/external-media/sources/google-photos/index.js
@@ -1,11 +1,25 @@
+import { useEffect } from 'react';
 import { MediaSource } from '../../media-service/types';
 import withMedia from '../with-media';
 import GooglePhotosAuth from './google-photos-auth';
 import GooglePhotosMedia from './google-photos-media';
+import GooglePhotosPickerButton from './google-photos-picker-button';
 
 function GooglePhotos( props ) {
+	const { pickerSession, createPickerSession } = props;
+
+	useEffect( () => {
+		if ( ! pickerSession ) {
+			createPickerSession();
+		}
+	}, [ pickerSession, createPickerSession ] );
+
 	if ( ! props.isAuthenticated ) {
 		return <GooglePhotosAuth { ...props } />;
+	}
+
+	if ( ! props.pickerSession?.mediaItemsSet ) {
+		return <GooglePhotosPickerButton { ...props } />;
 	}
 
 	return <GooglePhotosMedia { ...props } />;
