1a314394c83ca179d83cf6e9bbc9c50983cb435f
Bogdan Ungureanu <bogdanungureanu21@gmail.com>
2024-11-08 19:17:16 +0200

    Fix broken test

--- projects/packages/masterbar/src/nudges/additional-css/class-wpcom-additional-css-manager.php
+++ projects/packages/masterbar/src/nudges/additional-css/class-wpcom-additional-css-manager.php
@@ -66,7 +66,7 @@ class WPCOM_Additional_CSS_Manager {
 	 * @return mixed
 	 */
 	private function get_plan() {
-		$plan_slug = \WPCOM_Feature_Flags::is_enabled( \WPCOM_Feature_Flags::GLOBAL_STYLES_ON_PERSONAL_PLAN ) ? 'personal-bundle' : 'value_bundle';
+		$plan_slug = class_exists( 'WPCOM_Feature_Flags' ) && \WPCOM_Feature_Flags::is_enabled( \WPCOM_Feature_Flags::GLOBAL_STYLES_ON_PERSONAL_PLAN ) ? 'personal-bundle' : 'value_bundle';
 
 		return \Automattic\Jetpack\Plans::get_plan( $plan_slug );
 	}
