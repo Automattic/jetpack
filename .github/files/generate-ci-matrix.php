b131376fe185fbd5569ccf9ccd8bd5846ac787d6
tbradsha <32492176+tbradsha@users.noreply.github.com>
2024-11-12 09:51:18 -0700

    Remove 7.0 tests

--- .github/files/generate-ci-matrix.php
+++ .github/files/generate-ci-matrix.php
@@ -69,17 +69,6 @@ foreach ( array( '7.2', '7.3', '7.4', '8.0', '8.1', '8.2', '8.3' ) as $php ) {
 	);
 }
 
-// TODO: When WordPress 6.5 is no longer supported, this can be removed. Runs too slow on ubuntu-24.04 (ubuntu-latest).
-$matrix[] = array(
-	'name'                => 'PHP tests: PHP 7.0 WP previous',
-	'runner'              => 'ubuntu-22.04',
-	'script'              => 'test-php',
-	'php'                 => '7.0',
-	'wp'                  => 'previous',
-	'timeout'             => 20, // 2023-08-17: Successful runs seem to take up to ~12 minutes.
-	'force-package-tests' => true,
-);
-
 foreach ( array( 'previous', 'trunk' ) as $wp ) {
 	$phpver   = $versions['PHP_VERSION'];
 	$matrix[] = array(
