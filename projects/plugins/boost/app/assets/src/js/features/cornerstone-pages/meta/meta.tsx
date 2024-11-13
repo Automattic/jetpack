1c34621da737165ba923a8e2f7e8f6395468c37c
Peter Petrov <peter.petrov89@gmail.com>
2024-11-13 17:13:06 +0200

    Improve domain check

--- projects/plugins/boost/app/assets/src/js/features/cornerstone-pages/meta/meta.tsx
+++ projects/plugins/boost/app/assets/src/js/features/cornerstone-pages/meta/meta.tsx
@@ -16,6 +16,7 @@ import { useRegenerationReason } from '$features/critical-css/lib/stores/suggest
 import { usePremiumFeatures } from '$lib/stores/premium-features';
 import { useNavigate } from 'react-router-dom';
 import { useRegenerateCriticalCssAction } from '$features/critical-css/lib/stores/critical-css-state';
+import { isSameSiteUrl } from '$lib/utils/is-same-site-url';
 
 const Meta = () => {
 	const [ isExpanded, setIsExpanded ] = useState( false );
@@ -233,7 +234,7 @@ const List: React.FC< ListProps > = ( {
 			} catch ( e ) {
 				// If the URL is invalid, they have provided a relative URL, which we will allow.
 			}
-			if ( url && url.origin !== siteUrl.origin ) {
+			if ( url && ! isSameSiteUrl( url, siteUrl ) ) {
 				throw new Error(
 					/* translators: %s is the URL that didn't match the site URL */
 					sprintf( __( 'The URL seems to be a different site: %s', 'jetpack-boost' ), line )
