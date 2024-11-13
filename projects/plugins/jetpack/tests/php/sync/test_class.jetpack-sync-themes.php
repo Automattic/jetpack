a7f23028a0cad0c2af12c7edcae0e2950e02e36f
tbradsha <32492176+tbradsha@users.noreply.github.com>
2024-11-12 19:15:52 -0700

    Use a more recent theme

--- projects/plugins/jetpack/tests/php/sync/test_class.jetpack-sync-themes.php
+++ projects/plugins/jetpack/tests/php/sync/test_class.jetpack-sync-themes.php
@@ -250,8 +250,8 @@ class WP_Test_Jetpack_Sync_Themes extends WP_Test_Jetpack_Sync_Base {
 	}
 
 	public function test_install_edit_delete_theme_sync() {
-		$theme_slug = 'itek';
-		$theme_name = 'iTek';
+		$theme_slug = 'dossier';
+		$theme_name = 'Dossier';
 
 		delete_theme( $theme_slug ); // Ensure theme is not lingering on file system
 		$this->server_event_storage->reset();
@@ -295,7 +295,7 @@ class WP_Test_Jetpack_Sync_Themes extends WP_Test_Jetpack_Sync_Base {
 
 		$event_data = $this->server_event_storage->get_most_recent_event( 'jetpack_deleted_theme' );
 
-		$this->assertEquals( 'itek', $event_data->args[0] );
+		$this->assertEquals( 'dossier', $event_data->args[0] );
 	}
 
 	public function test_update_themes_sync() {
