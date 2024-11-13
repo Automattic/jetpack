1aaa625e9208aa6f737962e4c0b05cb6e9291a57
Jeremy Herve <jeremy@jeremy.hu>
2024-11-13 19:21:50 +0100

    Add some basic files back

---
+++ projects/plugins/migration/wpcom-migration.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: Move to WordPress.com
+ * This plugin is now developed in another GitHub repository. This repository is now archived and no longer maintained.
+ *
+ * @package automattic/wpcom-migration
+ */
