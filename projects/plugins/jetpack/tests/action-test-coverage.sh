8e8f98b375462deaa7ab0dbcfb62e201579f1cdb
Brad Jorsch <brad.jorsch@automattic.com>
2024-11-13 13:15:23 -0500

    coverage: Always process coverage, even if tests fail
    First, for projects that have both JS and PHP tests or multiple suites
    of tests, run all the tests for coverage even if one fails. Mostly this
    means using the `concurrently` package, since that's a very easy way to
    run both and collect the exit statuses.
    
    Then, in the workflow itself, add `always()` to the `if` on the step for
    processing coverage reports so it runs even if the tests failed to
    process whatever data is available.

--- projects/plugins/jetpack/tests/action-test-coverage.sh
+++ projects/plugins/jetpack/tests/action-test-coverage.sh
@@ -20,4 +20,4 @@ TESTS[client]="pnpm run test-client --coverage"
 TESTS[gui]="pnpm run test-gui --coverage"
 TESTS[extensions]="pnpm run test-extensions --coverage"
 
-pnpm exec concurrently --kill-others-on-fail --max-processes '100%' --names "$( IFS=,; echo "${!TESTS[*]}" )" "${TESTS[@]}"
+pnpm exec concurrently --max-processes '100%' --names "$( IFS=,; echo "${!TESTS[*]}" )" "${TESTS[@]}"
