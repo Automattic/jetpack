d75afd8d5838cb27aa850f9ba9ddd53d7f35b687
Caroline Moore <calobee@gmail.com>
2024-11-13 10:06:17 -0500

    Rename to use existing wpcom_import_* events

--- projects/plugins/wpcomsh/wpcom-migration-helpers/site-migration-helpers.php
+++ projects/plugins/wpcomsh/wpcom-migration-helpers/site-migration-helpers.php
@@ -74,8 +74,9 @@ function aiowp_migration_logging_helper() {
 		'ai1wm_import',
 		function ( $params = array() ) use ( $target_blog_id ) {
 			wpcomsh_record_tracks_event(
-				'wpcomsh_aiowp_import_start',
+				'wpcom_import_start',
 				array(
+					'migration_tool' => 'aiowp',
 					'target_blog_id' => $target_blog_id,
 				)
 			);
@@ -89,8 +90,9 @@ function aiowp_migration_logging_helper() {
 		'ai1wm_import',
 		function ( $params = array() ) use ( $target_blog_id ) {
 			wpcomsh_record_tracks_event(
-				'wpcomsh_aiowp_import_done',
+				'wpcom_import_done',
 				array(
+					'migration_tool' => 'aiowp',
 					'target_blog_id' => $target_blog_id,
 				)
 			);
