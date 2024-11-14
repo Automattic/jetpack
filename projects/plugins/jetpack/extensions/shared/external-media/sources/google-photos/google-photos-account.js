c8f0f6d9d80c942a4fe8bf901ff1261974dd706a
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-11-14 18:51:42 +0100

    Extend GooglePhotosAccount with option to hide account info

--- projects/plugins/jetpack/extensions/shared/external-media/sources/google-photos/google-photos-account.js
+++ projects/plugins/jetpack/extensions/shared/external-media/sources/google-photos/google-photos-account.js
@@ -1,22 +1,29 @@
 import GooglePhotosDisconnect from './google-photos-disconnect';
 
-const GooglePhotosAccount = ( { account, setAuthenticated, disconnectBtnVariant } ) => {
+const GooglePhotosAccount = ( {
+	account,
+	setAuthenticated,
+	disconnectBtnVariant,
+	showAccountInfo = true,
+} ) => {
 	const { image, name } = account || {};
 
 	return (
 		<div className="jetpack-external-media-header__account">
-			<div className="jetpack-external-media-header__account-info">
-				{ image && (
-					<img
-						className="jetpack-external-media-header__account-image"
-						src={ image }
-						alt=""
-						height="18"
-						width="18"
-					/>
-				) }
-				{ name && <div className="jetpack-external-media-header__account-name">{ name }</div> }
-			</div>
+			{ showAccountInfo && (
+				<div className="jetpack-external-media-header__account-info">
+					{ image && (
+						<img
+							className="jetpack-external-media-header__account-image"
+							src={ image }
+							alt=""
+							height="18"
+							width="18"
+						/>
+					) }
+					{ name && <div className="jetpack-external-media-header__account-name">{ name }</div> }
+				</div>
+			) }
 
 			<GooglePhotosDisconnect
 				setAuthenticated={ setAuthenticated }
