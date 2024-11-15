0dd6d173e719eb1c18b68fd144306afc67df434f
tbradsha <32492176+tbradsha@users.noreply.github.com>
2024-11-15 09:45:15 -0700

    Jetpack::is_connection_ready has been around in its current state since JP 9.7

--- projects/packages/jetpack-mu-wpcom/src/features/replace-site-visibility/replace-site-visibility.php
+++ projects/packages/jetpack-mu-wpcom/src/features/replace-site-visibility/replace-site-visibility.php
@@ -12,7 +12,7 @@
  */
 function is_jetpack_connected() {
 	// @phan-suppress-next-line PhanUndeclaredClassMethod, PhanUndeclaredClassInCallable
-	return class_exists( 'Jetpack' ) && is_callable( 'Jetpack::is_connection_ready' ) && Jetpack::is_connection_ready();
+	return class_exists( 'Jetpack' ) && Jetpack::is_connection_ready();
 }
 
 /**
