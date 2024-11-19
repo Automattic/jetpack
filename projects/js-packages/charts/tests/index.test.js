ac5f96ed62bd73231fd1e87b806e7097c4c78c14
Jeremy Herve <jeremy@jeremy.hu>
2024-11-19 16:38:31 +0100

    Add a placeholder test, or the tests will fail

--- projects/js-packages/charts/tests/index.test.js
+++ projects/js-packages/charts/tests/index.test.js
@@ -5,3 +5,14 @@
 //
 // The default setup is to have files named like "name.test.js" (or .jsx, .ts, or .tsx) in this `tests/` directory.
 // But you could instead put them in `src/`, or put files like "name.js" (or .jsx, .ts, or .tsx) in `test` or `__tests__` directories somewhere.
+
+// This is a placeholder test, new tests will be added soon
+const placeholderFunction = () => {
+	return true;
+};
+
+describe( 'placeholderTest', () => {
+	it( 'should be a function', () => {
+		expect( typeof placeholderFunction ).toBe( 'function' );
+	} );
+} );
