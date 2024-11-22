46837658afdea5926ba64acf6df3907408b439f3
Miguel Lezama <lezama@gmail.com>
2024-11-22 11:42:08 -0300

    linter

--- projects/plugins/jetpack/extensions/blocks/subscriptions/test/edit.js
+++ projects/plugins/jetpack/extensions/blocks/subscriptions/test/edit.js
@@ -1,5 +1,5 @@
 import { useModuleStatus } from '@automattic/jetpack-shared-extension-utils';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { SubscriptionEdit } from '../edit';
 
