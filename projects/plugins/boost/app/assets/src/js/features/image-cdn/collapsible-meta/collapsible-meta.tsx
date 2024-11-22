6a0ba148c4cedb159b6f27ee740098bf7fb60412
Donncha O Caoimh <5656673+donnchawp@users.noreply.github.com>
2024-11-22 16:24:22 +0000

    Update so it works with other modules.
    * Allow it to use Chevrons as well as pencil
    * External state for enclosing module to track expanded status.
    * Set a Tracks event when it's toggled open/closed.

--- projects/plugins/boost/app/assets/src/js/features/image-cdn/collapsible-meta/collapsible-meta.tsx
+++ projects/plugins/boost/app/assets/src/js/features/image-cdn/collapsible-meta/collapsible-meta.tsx
@@ -3,16 +3,58 @@ import { Button } from '@automattic/jetpack-components';
 import React, { useState } from 'react';
 import CloseIcon from '$svg/close';
 import PencilIcon from '$svg/pencil';
+import ChevronDown from '$svg/chevron-down';
+import ChevronUp from '$svg/chevron-up';
+import { recordBoostEvent } from '$lib/utils/analytics';
 
 type CollapsibleMetaProps = {
 	children: React.ReactNode;
 	header: React.ReactNode;
 	summary: React.ReactNode;
 	editText: string;
+	useChevron?: boolean;
+	isExpandedExternal?: boolean;
+	setIsExpandedExternal?: ( value: boolean ) => void;
+	tracksEvent?: string;
 };
 
-const CollapsibleMeta = ( { children, header, summary, editText }: CollapsibleMetaProps ) => {
-	const [ isEditing, setIsEditing ] = useState( false );
+const CollapsibleMeta = ( {
+	children,
+	header,
+	summary,
+	editText,
+	isExpandedExternal,
+	setIsExpandedExternal,
+	useChevron = true,
+	tracksEvent = '',
+}: CollapsibleMetaProps ) => {
+	const [ isExpandedInternal, setIsExpandedInternal ] = useState( false );
+
+	// Use external state if provided, otherwise use internal state
+	const isExpanded = isExpandedExternal !== undefined ? isExpandedExternal : isExpandedInternal;
+	const setIsExpanded = setIsExpandedExternal || setIsExpandedInternal;
+
+	const togglePanel = () => {
+		setIsExpanded( ! isExpanded );
+
+		if ( tracksEvent !== '' ) {
+			recordBoostEvent( tracksEvent, {
+				status: ! isExpanded ? 'open' : 'close',
+			} );
+		}
+	};
+
+	const getIcon = () => {
+		if ( useChevron ) {
+			return isExpanded ? <ChevronUp /> : <ChevronDown />;
+		}
+		return isExpanded ? (
+			<CloseIcon className={ styles[ 'edit-icon' ] } />
+		) : (
+			<PencilIcon className={ styles[ 'edit-icon' ] } />
+		);
+	};
+
 	return (
 		<div className={ styles[ 'collapsible-meta' ] }>
 			<header className={ styles.header }>
@@ -21,23 +63,15 @@ const CollapsibleMeta = ( { children, header, summary, editText }: CollapsibleMe
 					variant="link"
 					size="small"
 					weight="regular"
-					icon={
-						isEditing ? (
-							<CloseIcon className={ styles[ 'edit-icon' ] } />
-						) : (
-							<PencilIcon className={ styles[ 'edit-icon' ] } />
-						)
-					}
+					icon={ getIcon() }
 					className={ styles[ 'edit-button' ] }
-					onClick={ () => {
-						setIsEditing( ! isEditing );
-					} }
+					onClick={ togglePanel }
 				>
 					{ editText }
 				</Button>
 			</header>
 
-			{ isEditing ? children : <div className={ styles.summary }>{ summary }</div> }
+			{ isExpanded ? children : <div className={ styles.summary }>{ summary }</div> }
 		</div>
 	);
 };
