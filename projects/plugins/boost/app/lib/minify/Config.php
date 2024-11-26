a7bb1c7c1d3add9ea1af1eafa1195707082b6a89
Adnan Haque <adnan007.id@gmail.com>
2024-11-26 16:32:05 +0600

    Use path hash in the URL

--- projects/plugins/boost/app/lib/minify/Config.php
+++ projects/plugins/boost/app/lib/minify/Config.php
@@ -27,4 +27,43 @@ class Config {
 
 		return $path;
 	}
+
+	public static function can_use_cache() {
+		$cache_dir = static::get_cache_dir_path();
+		$use_cache = ! empty( $cache_dir );
+
+		// Ensure the cache directory exists.
+		// phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_operations_mkdir
+		if ( $use_cache && ! is_dir( $cache_dir ) && ! mkdir( $cache_dir, 0775, true ) ) {
+			$use_cache = false;
+			if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+				// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+				error_log(
+					sprintf(
+					/* translators: a filesystem path to a directory */
+						__( "Disabling page-optimize cache. Unable to create cache directory '%s'.", 'jetpack-boost' ),
+						$cache_dir
+					)
+				);
+			}
+		}
+
+		// Ensure the cache directory is writable.
+		// phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_operations_is_writable
+		if ( $use_cache && ( ! is_dir( $cache_dir ) || ! is_writable( $cache_dir ) || ! is_executable( $cache_dir ) ) ) {
+			$use_cache = false;
+			if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+				// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+				error_log(
+					sprintf(
+					/* translators: a filesystem path to a directory */
+						__( "Disabling page-optimize cache. Unable to write to cache directory '%s'.", 'jetpack-boost' ),
+						$cache_dir
+					)
+				);
+			}
+		}
+
+		return $use_cache;
+	}
 }
