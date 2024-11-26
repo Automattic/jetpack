9b386e9d49cd97122cf43a356748b3b2d22699e0
Bryan Elliott <bryan@elliottprogrammer.com>
2024-11-26 10:25:46 -0500

    Clean up, refactor, & optimize code in use-get-expiring-notice-content.tsx hook.

--- projects/packages/my-jetpack/_inc/hooks/use-notification-watcher/use-get-expiring-notice-content.tsx
+++ projects/packages/my-jetpack/_inc/hooks/use-notification-watcher/use-get-expiring-notice-content.tsx
@@ -26,140 +26,66 @@ export const useGetExpiringNoticeContent = ( {
 	const product = productSlug.replace( /(?:_t1|_t2|_bi)?(?:_yearly|_monthly)/, '' );
 	const expiryDateFormatted = gmdateI18n( 'M j, Y', expiryDate );
 
-	// EXPIRED
-	if ( expiredAlertType === 'expired' ) {
-		switch ( productSlug ) {
-			case 'jetpack_videopress':
-				return {
-					noticeTitle: sprintf(
-						// translators: %s is the Jetpack product name, i.e.- Jetpack Backup, Jetpack Security, etc..
-						__( 'Your %1$s plan has expired', 'jetpack-my-jetpack' ),
-						productName
-					),
-					noticeMessage: (
-						<Col>
-							<Text mt={ 2 } mb={ 2 }>
-								{ __(
-									'Your videos are no longer visible to your viewers, and you won’t be able to upload new content.',
-									'jetpack-my-jetpack'
-								) }
-							</Text>
-							<Text mb={ 2 }>
-								{ __(
-									'Don’t worry—you can resume your plan anytime to restore your video library and continue uploading.',
-									'jetpack-my-jetpack'
-								) }
-							</Text>
-						</Col>
-					),
-					learnMoreUrl:
-						'https://jetpack.com/support/jetpack-videopress/#canceled-or-expired-videopress-plan',
-				};
-			case 'jetpack_complete':
-			case 'jetpack_security':
-			case 'jetpack_growth':
-				return {
-					noticeTitle: sprintf(
-						// translators: %1$s is the Jetpack product name, i.e.- Jetpack Backup, Jetpack Security, etc..
-						__( 'Your %1$s plan has expired', 'jetpack-my-jetpack' ),
-						productName
-					),
-					noticeMessage: (
-						<Col>
-							<Text mt={ 2 }>
-								{ sprintf(
-									// translators: %1$s is the Jetpack product name, i.e.- Jetpack Backup, Jetpack Security, etc..
-									__(
-										'%1$s provides paid features for the following products:',
-										'jetpack-my-jetpack'
-									),
-									productName
-								) }
-							</Text>
-
-							<ProductsList products={ productsEffected } />
-
-							<Text mb={ 2 }>
-								{ sprintf(
-									// translators: %1$s is the Jetpack product name, i.e.- Jetpack Backup, Jetpack Security, etc..
-									__(
-										'Since your %1$s plan is expired, the paid features for these products are no longer available for your site.',
-										'jetpack-my-jetpack'
-									),
-									productName
-								) }
-							</Text>
-							<Text mb={ 2 }>
-								{ __(
-									'But don’t worry—you can resume your plan to restore your access to your plan’s paid features.',
-									'jetpack-my-jetpack'
-								) }
-							</Text>
-						</Col>
-					),
-					learnMoreUrl: 'https://jetpack.com/support/jetpack-billing-payments/',
-				};
-			default:
-				return {
-					noticeTitle: sprintf(
-						// translators: %1$s is the Jetpack product name, i.e.- Jetpack Backup, Jetpack Security, etc..
-						__( 'Your %1$s plan has expired', 'jetpack-my-jetpack' ),
-						productName
-					),
-					noticeMessage: (
-						<Col>
-							<Text mt={ 2 } mb={ 2 }>
-								{ sprintf(
-									// translators: %1$s is the Jetpack product name, i.e.- Jetpack Backup, Jetpack Security, etc..
-									__(
-										'Your %1$s subcription has expired. The paid features are no longer available.',
-										'jetpack-my-jetpack'
-									),
-									productName
-								) }
-							</Text>
-							<Text mb={ 2 }>
-								{ __(
-									'Don’t worry—you can resume your plan anytime to restore your access to the upgraded features.',
-									'jetpack-my-jetpack'
-								) }
-							</Text>
-						</Col>
-					),
-					learnMoreUrl: 'https://jetpack.com/support/jetpack-billing-payments/',
-				};
-		}
-	}
-
-	// EXPIRING-SOON
-	switch ( product ) {
-		case 'jetpack_videopress':
-			return {
-				noticeTitle: sprintf(
+	const noticeHeading =
+		expiredAlertType === 'expired'
+			? sprintf(
+					// translators: %s is the Jetpack product name, i.e.- Jetpack Backup, Jetpack Security, etc..
+					__( 'Your %1$s plan has expired', 'jetpack-my-jetpack' ),
+					productName
+			  )
+			: sprintf(
 					// translators: %1$s is the Jetpack product name, i.e.- Jetpack Backup, Jetpack Security, etc..
 					__( 'Heads up! Your %1$s plan is about to expire', 'jetpack-my-jetpack' ),
 					productName
-				),
-				noticeMessage: (
-					<Col>
-						<Text mt={ 2 } mb={ 2 }>
-							{ __(
-								'Your videos will soon stop showing for your viewers, and you won’t be able to upload new content.',
-								'jetpack-my-jetpack'
-							) }
-						</Text>
-						<Text mb={ 2 }>
-							{ sprintf(
-								// translators: %1$s is the product's expiration date, i.e.- "Nov 11, 2024"
-								__(
-									'To keep everything running smoothly, renew your plan by %1$s!',
-									'jetpack-my-jetpack'
-								),
-								expiryDateFormatted
-							) }
-						</Text>
-					</Col>
-				),
+			  );
+
+	// Notice Content
+	switch ( product ) {
+		case 'jetpack_videopress':
+			return {
+				noticeTitle: noticeHeading,
+				noticeMessage:
+					expiredAlertType === 'expired' ? (
+						<>
+							{ /* EXPIRED */ }
+							<Col>
+								<Text mt={ 2 } mb={ 2 }>
+									{ __(
+										'Your videos are no longer visible to your viewers, and you won’t be able to upload new content.',
+										'jetpack-my-jetpack'
+									) }
+								</Text>
+								<Text mb={ 2 }>
+									{ __(
+										'Don’t worry—you can resume your plan anytime to restore your video library and continue uploading.',
+										'jetpack-my-jetpack'
+									) }
+								</Text>
+							</Col>
+						</>
+					) : (
+						<>
+							{ /* EXPIRING-SOON */ }
+							<Col>
+								<Text mt={ 2 } mb={ 2 }>
+									{ __(
+										'Your videos will soon stop showing for your viewers, and you won’t be able to upload new content.',
+										'jetpack-my-jetpack'
+									) }
+								</Text>
+								<Text mb={ 2 }>
+									{ sprintf(
+										// translators: %1$s is the product's expiration date, i.e.- "Nov 11, 2024"
+										__(
+											'To keep everything running smoothly, renew your plan by %1$s!',
+											'jetpack-my-jetpack'
+										),
+										expiryDateFormatted
+									) }
+								</Text>
+							</Col>
+						</>
+					),
 				learnMoreUrl:
 					'https://jetpack.com/support/jetpack-videopress/#canceled-or-expired-videopress-plan',
 			};
@@ -167,68 +93,125 @@ export const useGetExpiringNoticeContent = ( {
 		case 'jetpack_security':
 		case 'jetpack_growth':
 			return {
-				noticeTitle: sprintf(
-					// translators: %1$s is the Jetpack product name, i.e.- Jetpack Backup, Jetpack Security, etc..
-					__( 'Heads up! Your %1$s plan is about to expire', 'jetpack-my-jetpack' ),
-					productName
-				),
-				noticeMessage: (
-					<Col>
-						<Text mt={ 2 }>
-							{ sprintf(
-								// translators: %1$s is the Jetpack product name, i.e.- Jetpack Backup, Jetpack Security, etc..
-								__(
-									'%1$s provides paid features for the following products:',
-									'jetpack-my-jetpack'
-								),
-								productName
-							) }
-						</Text>
+				noticeTitle: noticeHeading,
+				noticeMessage:
+					expiredAlertType === 'expired' ? (
+						<>
+							{ /* EXPIRED */ }
+							<Col>
+								<Text mt={ 2 }>
+									{ sprintf(
+										// translators: %1$s is the Jetpack product name, i.e.- Jetpack Backup, Jetpack Security, etc..
+										__(
+											'%1$s provides paid features for the following products:',
+											'jetpack-my-jetpack'
+										),
+										productName
+									) }
+								</Text>
 
-						<ProductsList products={ productsEffected } />
+								<ProductsList products={ productsEffected } />
 
-						<Text mb={ 2 }>
-							{ sprintf(
-								// translators: %1$s is the product's expiration date, i.e.- "Nov 11, 2024"
-								__(
-									'Renew your plan by %1$s to continue to have access to the paid features of these products.',
-									'jetpack-my-jetpack'
-								),
-								expiryDateFormatted
-							) }
-						</Text>
-					</Col>
-				),
+								<Text mb={ 2 }>
+									{ sprintf(
+										// translators: %1$s is the Jetpack product name, i.e.- Jetpack Backup, Jetpack Security, etc..
+										__(
+											'Since your %1$s plan is expired, the paid features for these products are no longer available for your site.',
+											'jetpack-my-jetpack'
+										),
+										productName
+									) }
+								</Text>
+								<Text mb={ 2 }>
+									{ __(
+										'But don’t worry—you can resume your plan to restore your access to your plan’s paid features.',
+										'jetpack-my-jetpack'
+									) }
+								</Text>
+							</Col>
+						</>
+					) : (
+						<>
+							{ /* EXPIRING-SOON */ }
+							<Col>
+								<Text mt={ 2 }>
+									{ sprintf(
+										// translators: %1$s is the Jetpack product name, i.e.- Jetpack Backup, Jetpack Security, etc..
+										__(
+											'%1$s provides paid features for the following products:',
+											'jetpack-my-jetpack'
+										),
+										productName
+									) }
+								</Text>
+
+								<ProductsList products={ productsEffected } />
+
+								<Text mb={ 2 }>
+									{ sprintf(
+										// translators: %1$s is the product's expiration date, i.e.- "Nov 11, 2024"
+										__(
+											'Renew your plan by %1$s to continue to have access to the paid features of these products.',
+											'jetpack-my-jetpack'
+										),
+										expiryDateFormatted
+									) }
+								</Text>
+							</Col>
+						</>
+					),
 				learnMoreUrl: 'https://jetpack.com/support/jetpack-billing-payments/',
 			};
 		default:
 			return {
-				noticeTitle: sprintf(
-					// translators: %1$s is the Jetpack product name, i.e.- Jetpack Backup, Jetpack Security, etc..
-					__( 'Heads up! Your %1$s plan is about to expire', 'jetpack-my-jetpack' ),
-					productName
-				),
-				noticeMessage: (
-					<Col>
-						<Text mt={ 2 } mb={ 2 }>
-							{ sprintf(
-								// translators: %1$s is the Jetpack product name, i.e.- Jetpack Backup, Jetpack Security, etc.., %2$s is the product's expiration date, i.e.- "Nov 11, 2024".
-								__(
-									'Your %1$s subcription will be expiring soon on %2$s, and the paid features for this product will no longer be available.',
-									'jetpack-my-jetpack'
-								),
-								productName,
-								expiryDateFormatted
-							) }
-						</Text>
-						<Text mb={ 2 }>
-							{ __(
-								'To keep everything running smoothly, renew your plan today!',
-								'jetpack-my-jetpack'
-							) }
-						</Text>
-					</Col>
-				),
+				noticeTitle: noticeHeading,
+				noticeMessage:
+					expiredAlertType === 'expired' ? (
+						<>
+							{ /* EXPIRED */ }
+							<Col>
+								<Text mt={ 2 } mb={ 2 }>
+									{ sprintf(
+										// translators: %1$s is the Jetpack product name, i.e.- Jetpack Backup, Jetpack Security, etc..
+										__(
+											'Your %1$s subcription has expired. The paid features are no longer available.',
+											'jetpack-my-jetpack'
+										),
+										productName
+									) }
+								</Text>
+								<Text mb={ 2 }>
+									{ __(
+										'Don’t worry—you can resume your plan anytime to restore your access to the upgraded features.',
+										'jetpack-my-jetpack'
+									) }
+								</Text>
+							</Col>
+						</>
+					) : (
+						<>
+							{ /* EXPIRING-SOON */ }
+							<Col>
+								<Text mt={ 2 } mb={ 2 }>
+									{ sprintf(
+										// translators: %1$s is the Jetpack product name, i.e.- Jetpack Backup, Jetpack Security, etc.., %2$s is the product's expiration date, i.e.- "Nov 11, 2024".
+										__(
+											'Your %1$s subcription will be expiring soon on %2$s, and the paid features for this product will no longer be available.',
+											'jetpack-my-jetpack'
+										),
+										productName,
+										expiryDateFormatted
+									) }
+								</Text>
+								<Text mb={ 2 }>
+									{ __(
+										'To keep everything running smoothly, renew your plan today!',
+										'jetpack-my-jetpack'
+									) }
+								</Text>
+							</Col>
+						</>
+					),
 				learnMoreUrl: 'https://jetpack.com/support/jetpack-billing-payments/',
 			};
 	}
