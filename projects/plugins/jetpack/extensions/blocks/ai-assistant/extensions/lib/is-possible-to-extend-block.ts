3c1e11ef77824ab95acafd58decc70df1d985bdb
Christian Gastrell <cgastrell@gmail.com>
2024-11-25 21:47:45 -0300

    add block-to-module minimap, get module active status from store

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/lib/is-possible-to-extend-block.ts
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/extensions/lib/is-possible-to-extend-block.ts
@@ -1,8 +1,17 @@
 /*
+ * External dependencies
+ */
+import { JETPACK_MODULES_STORE_ID } from '@automattic/jetpack-shared-extension-utils';
+import { select } from '@wordpress/data';
+/*
  * Internal dependencies
  */
 import { EXTENDED_BLOCKS } from '../constants';
 import { canAIAssistantBeEnabled } from './can-ai-assistant-be-enabled';
+
+const BLOCK_TO_MODULE_MAP = {
+	'jetpack/contact-form': 'contact-form',
+};
 
 /**
  * Check if it is possible to extend a block with AI Assistant capabilities.
@@ -20,6 +29,19 @@ export function isPossibleToExtendBlock( blockName: string ): boolean {
 	// Only extend the blocks in the inline blocks list
 	if ( ! EXTENDED_BLOCKS.includes( blockName ) ) {
 		return false;
+	}
+
+	const blockRequiredModule = BLOCK_TO_MODULE_MAP[ blockName ];
+	if ( blockRequiredModule ) {
+		// This call is the same as useModuleStatus( blockRequiredModule ).isModuleActive,
+		// yet we can't use a hook outside a component.
+		// See: js-packages/shared-extension-utils/src/hooks/use-module-status/index.js
+		const blockModuleIsEnabled =
+			select( JETPACK_MODULES_STORE_ID )?.isModuleActive?.( blockRequiredModule ) || false;
+
+		if ( ! blockModuleIsEnabled ) {
+			return false;
+		}
 	}
 
 	return true;
