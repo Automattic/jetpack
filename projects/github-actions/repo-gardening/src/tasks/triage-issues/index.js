f0313ebd181bd3723668e2f6cf3b102aa51745b5
Brad Jorsch <anomiex@users.noreply.github.com>
2024-12-02 19:34:54 +0100

    Avoid catching [Type] Feature request
    See https://github.com/Automattic/jetpack/pull/40396#discussion_r1866270129

--- projects/github-actions/repo-gardening/src/tasks/triage-issues/index.js
+++ projects/github-actions/repo-gardening/src/tasks/triage-issues/index.js
@@ -152,7 +152,7 @@ async function triageIssues( payload, octokit ) {
 		const issueLabels = await aiLabeling( payload, octokit );
 
 		// At this point, if we still miss a [Type] label, a [Feature] label, or a [Pri] label, ask the author to add it.
-		const requiredLabelTypes = [ /^\[Type\]/, /^\[Pri/, /^\[.*?Feature/ ];
+		const requiredLabelTypes = [ /^\[Type\]/, /^\[Pri/, /^\[[^\]]*Feature/ ];
 		const missingLabelTypes = requiredLabelTypes.filter(
 			requiredLabelType => ! issueLabels.some( label => requiredLabelType.test( label ) )
 		);
