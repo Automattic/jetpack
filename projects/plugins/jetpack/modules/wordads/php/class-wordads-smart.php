e085e59f1fbf6219b538a40629d671da7462c1bc
Diogo Alves <diogoca@gmail.com>
2024-12-04 09:41:55 -0300

    [WordAds]: Update Smart class to handle new formats (#40431)
    * Update Smart class to handle new formats
    
    * Fix sas_fallback variable and add sidebar tag
    
    * Add sidebar on Smart formats to support the legacy sidebar widget
    
    * Add resource hints
    
    * Reorder methods
    
    * Fix enabled check for top format
    
    * Undo the support for sidebar format and minor fixes

--- projects/plugins/jetpack/modules/wordads/php/class-wordads-smart.php
+++ projects/plugins/jetpack/modules/wordads/php/class-wordads-smart.php
@@ -24,18 +24,11 @@ class WordAds_Smart {
 	protected static $instance = null;
 
 	/**
-	 * Is this an AMP request?
+	 * The parameters for WordAds.
 	 *
-	 * @var bool
+	 * @var WordAds_Params
 	 */
-	private $is_amp;
-
-	/**
-	 * Current blog theme from get_stylesheet().
-	 *
-	 * @var string
-	 */
-	private $theme;
+	private $params;
 
 	/**
 	 * Has Smart asset been enqueued?
@@ -43,13 +36,6 @@ class WordAds_Smart {
 	 * @var bool True if Smart asset has been enqueued.
 	 */
 	private $is_asset_enqueued = false;
-
-	/**
-	 * Toggle for inline ads.
-	 *
-	 * @var bool True if inline ads are enabled.
-	 */
-	private $is_inline_enabled;
 
 	/**
 	 * Supported formats.
@@ -99,8 +85,8 @@ class WordAds_Smart {
 	 *
 	 * @return WordAds_Smart
 	 */
-	public static function instance() {
-		if ( self::$instance === null ) {
+	public static function instance(): self {
+		if ( null === self::$instance ) {
 			self::$instance = new self();
 		}
 		return self::$instance;
@@ -114,19 +100,12 @@ class WordAds_Smart {
 	 * @return void
 	 */
 	public function init( WordAds_Params $params ) {
-		$this->is_amp            = function_exists( 'amp_is_request' ) && amp_is_request();
-		$this->theme             = get_stylesheet();
-		$this->is_inline_enabled = is_singular( 'post' ) && $params->options['wordads_inline_enabled'];
+		$this->params = $params;
 
-		// Allow enabled format override by query string.
+		$this->enable_formats();
 		$this->override_formats_from_query_string();
-		// TODO: refactor to remove the need to do this
-		$this->is_inline_enabled = $this->formats['inline']['enabled'];
 
-		// TODO: Is this necessary? if we want to always run Smart insert ads logic after IPW migrations
-		$has_any_format_enabled = in_array( true, array_column( $this->formats, 'enabled' ), true );
-		if ( $has_any_format_enabled ) {
-			// Insert ads.
+		if ( $this->has_any_format_enabled() ) {
 			$this->insert_ads();
 		}
 	}
@@ -173,7 +152,7 @@ class WordAds_Smart {
 	 * @return void
 	 */
 	private function insert_ads() {
-		if ( $this->is_amp ) {
+		if ( $this->params->is_amp ) {
 			return;
 		}
 
@@ -182,13 +161,16 @@ class WordAds_Smart {
 			return;
 		}
 
+		// Add the resource hints.
+		add_filter( 'wp_resource_hints', array( $this, 'resource_hints' ), 10, 2 );
+
 		// Enqueue JS assets.
 		$this->enqueue_assets();
 
 		$is_static_front_page = is_front_page() && 'page' === get_option( 'show_on_front' );
 
 		if ( ! ( $is_static_front_page || is_home() ) ) {
-			if ( $this->is_inline_enabled ) {
+			if ( $this->formats['inline']['enabled'] ) {
 				add_filter(
 					'the_content',
 					array( $this, 'insert_inline_marker' ),
@@ -217,14 +199,9 @@ class WordAds_Smart {
 		global $post;
 
 		$config = array(
-			'blog_id' => $this->get_blog_id(),
 			'post_id' => ( $post instanceof WP_Post ) && is_singular( 'post' ) ? $post->ID : null,
-			'theme'   => $this->theme,
+			'theme'   => get_stylesheet(),
 			'target'  => $this->target_keywords(),
-			'_'       => array(
-				'title'            => __( 'Advertisement', 'jetpack' ),
-				'privacy_settings' => __( 'Privacy Settings', 'jetpack' ),
-			),
 		) + $this->formats;
 
 		// Do conversion.
@@ -235,6 +212,22 @@ class WordAds_Smart {
 	}
 
 	/**
+	 * Add the Smart resource hints.
+	 *
+	 * @param array  $hints Domains for hinting.
+	 * @param string $relation_type Resource type.
+	 *
+	 * @return array Domains for hinting.
+	 */
+	public function resource_hints( $hints, $relation_type ) {
+		if ( 'dns-prefetch' === $relation_type ) {
+			$hints[] = '//af.pubmine.com';
+		}
+
+		return $hints;
+	}
+
+	/**
 	 * Gets the URL to a JSONP endpoint with configuration data.
 	 *
 	 * @return string The URL.
@@ -242,22 +235,8 @@ class WordAds_Smart {
 	private function get_config_url(): string {
 		return sprintf(
 			'https://public-api.wordpress.com/wpcom/v2/sites/%1$d/adflow/conf/?_jsonp=a8c_adflow_callback',
-			$this->get_blog_id()
+			$this->params->blog_id
 		);
-	}
-
-	/**
-	 * Allow format enabled override from query string, eg. ?inline=true.
-	 *
-	 * @return void
-	 */
-	private function override_formats_from_query_string(): void {
-		foreach ( $this->formats as $format_type => $_ ) {
-			// phpcs:disable WordPress.Security.NonceVerification.Recommended
-			if ( isset( $_GET[ $format_type ] ) && 'true' === $_GET[ $format_type ] ) {
-				$this->formats[ $format_type ]['enabled'] = true;
-			}
-		}
 	}
 
 	/**
@@ -266,9 +245,9 @@ class WordAds_Smart {
 	 * @param string|null $content The post content.
 	 * @return string|null The post content with the marker appended.
 	 */
-	public function insert_inline_marker( $content ) {
-		if ( $content === null ) {
-			return $content;
+	public function insert_inline_marker( ?string $content ): ?string {
+		if ( null === $content ) {
+			return null;
 		}
 		$inline_ad_marker = '<span id="wordads-inline-marker" style="display: none;"></span>';
 
@@ -285,7 +264,6 @@ class WordAds_Smart {
 		$target_keywords = array_merge(
 			$this->get_blog_keywords(),
 			$this->get_language_keywords()
-			// TODO: Include categorization.
 		);
 
 		return implode( ';', $target_keywords );
@@ -297,7 +275,7 @@ class WordAds_Smart {
 	 * @return array The list of blog keywords.
 	 */
 	private function get_blog_keywords(): array {
-		return array( 'wp_blog_id=' . $this->get_blog_id() );
+		return array( 'wp_blog_id=' . $this->params->blog_id );
 	}
 
 	/**
@@ -310,11 +288,43 @@ class WordAds_Smart {
 	}
 
 	/**
-	 * Gets the blog's ID.
+	 * Enable formats by post types and the display options.
 	 *
-	 * @return int The blog's ID.
+	 * @return void
 	 */
-	private function get_blog_id(): int {
-		return Jetpack::get_option( 'id', 0 );
+	private function enable_formats(): void {
+		$this->formats['top']['enabled']                  = $this->params->options['enable_header_ad'];
+		$this->formats['inline']['enabled']               = is_singular( 'post' ) && $this->params->options['wordads_inline_enabled'];
+		$this->formats['belowpost']['enabled']            = $this->params->should_show();
+		$this->formats['bottom_sticky']['enabled']        = $this->params->options['wordads_bottom_sticky_enabled'];
+		$this->formats['sidebar_sticky_right']['enabled'] = $this->params->options['wordads_sidebar_sticky_right_enabled'];
+	}
+
+	/**
+	 * Allow format enabled override from query string, eg. ?inline=true.
+	 *
+	 * @return void
+	 */
+	private function override_formats_from_query_string(): void {
+		// phpcs:disable WordPress.Security.NonceVerification.Recommended
+		if ( ! isset( $_GET['wordads-logging'] ) ) {
+			return;
+		}
+
+		foreach ( $this->formats as $format_type => $_ ) {
+			// phpcs:disable WordPress.Security.NonceVerification.Recommended
+			if ( isset( $_GET[ $format_type ] ) && 'true' === $_GET[ $format_type ] ) {
+				$this->formats[ $format_type ]['enabled'] = true;
+			}
+		}
+	}
+
+	/**
+	 * Check if has any format enabled.
+	 *
+	 * @return bool True if enabled, false otherwise.
+	 */
+	private function has_any_format_enabled(): bool {
+		return in_array( true, array_column( $this->formats, 'enabled' ), true );
 	}
 }
