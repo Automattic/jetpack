cdd94b96e67f1de18a3e5303559aef7d4df7485e
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2024-12-05 16:47:48 +0200

    Introduce component usage stats

---
+++ react-scanner.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	// Crawl the entire repo
+	crawlFrom: './',
+	// Needed for properly reporting components with dot notation
+	includeSubComponents: true,
+	// Exclude usage in tests and stories.
+	globs: [ '**/!(test|stories)/!(*stories).@(js|ts)?(x)' ],
+	// Exclude any vendor or docs directories
+	exclude: [
+		'docs',
+		'node_modules',
+		'tools',
+	],
+	// Consider only imports of `@wordpress/components`
+	importedFrom: '@wordpress/components',
+	// Full usage report
+	processors: [ [ 'raw-report', { outputTo: './results/jetpack.json' } ] ],
+};
