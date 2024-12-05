77fc70d67214a953473e25409c81f0ed08abef18
dkmyta <dkmyta10@gmail.com>
2024-12-05 13:35:10 -0800

    Add dummy arg

--- projects/js-packages/components/components/threat-modal/threat-technical-details.tsx
+++ projects/js-packages/components/components/threat-modal/threat-technical-details.tsx
@@ -39,7 +39,11 @@ const ThreatTechnicalDetails = (): JSX.Element => {
 						<Text variant="title-small" mb={ 0 }>
 							{ open
 								? __( 'Hide the technical details', 'jetpack-components' )
-								: __( 'Show the technical details', 'jetpack-components' ) }
+								: __(
+										'Show the technical details',
+										'jetpack-components',
+										/* dummy arg to avoid bad minification */ 0
+								  ) }
 						</Text>
 						<Icon icon={ open ? chevronUp : chevronDown } size={ 24 } />
 					</div>
