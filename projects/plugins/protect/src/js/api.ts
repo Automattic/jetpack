075bc51d518e31ee58abd5d6088cc6c2fa329062
Nate Weller <nate.weller@automattic.com>
2024-12-05 10:50:27 -0700

    Protect: Integrate ThreatsDataViews Component (#40076)

--- projects/plugins/protect/src/js/api.ts
+++ projects/plugins/protect/src/js/api.ts
@@ -1,6 +1,7 @@
-import { type FixersStatus, type ScanStatus, type WafStatus } from '@automattic/jetpack-scan';
+import { type FixersStatus, type ScanStatus } from '@automattic/jetpack-scan';
 import apiFetch from '@wordpress/api-fetch';
 import camelize from 'camelize';
+import { WafStatus } from './types/waf';
 
 const API = {
 	getWaf: (): Promise< WafStatus > =>
