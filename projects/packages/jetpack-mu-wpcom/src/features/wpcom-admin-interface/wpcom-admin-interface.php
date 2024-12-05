c990445361bc061d1fde5b6e369597c7d94c29eb
mmtr <1233880+mmtr@users.noreply.github.com>
2024-12-05 11:40:40 +0100

    Duplicate views: Add util to fetch experiment assignment

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-admin-interface/wpcom-admin-interface.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-admin-interface/wpcom-admin-interface.php
@@ -5,7 +5,10 @@
  * @package automattic/jetpack-mu-wpcom
  */
 
+use Automattic\Jetpack\Connection\Client;
+use Automattic\Jetpack\Connection\Manager as Jetpack_Connection;
 use Automattic\Jetpack\Jetpack_Mu_Wpcom;
+use Automattic\Jetpack\Status\Host;
 
 /**
  * Add the Admin Interface Style setting on the General settings page.
@@ -273,3 +276,47 @@ function wpcom_show_admin_interface_notice() {
 	);
 }
 add_action( 'admin_notices', 'wpcom_show_admin_interface_notice' );
+
+/**
+ * Fetches the current assignment to the duplicate views experiment.
+ *
+ * @return string
+ */
+function wpcom_fetch_duplicate_views_assignment() {
+	// TODO: We don't know yet the experiment name.
+	$experiment_platform = 'calypso';
+	$experiment_name     = "{$experiment_platform}_duplicate_views_placeholder";
+
+	if ( ( new Host() )->is_wpcom_simple() ) {
+		return \ExPlat\assign_current_user( $experiment_name );
+	} else {
+		// If there isn't a WP.com connection, we should keep users in WP Admin.
+		if ( ! ( new Jetpack_Connection() )->is_user_connected() ) {
+			return 'treatment';
+		}
+
+		$request_path = add_query_arg(
+			array( 'experiment_name' => $experiment_name ),
+			"/experiments/0.1.0/assignments/{$experiment_platform}"
+		);
+		$response     = Client::wpcom_json_api_request_as_user( $request_path, 'v2' );
+
+		if ( is_wp_error( $response ) ) {
+			return 'treatment';
+		}
+
+		$response_code = wp_remote_retrieve_response_code( $response );
+
+		if ( 200 !== $response_code ) {
+			return 'treatment';
+		}
+
+		$data = json_decode( wp_remote_retrieve_body( $response ), true );
+
+		if ( isset( $data['variations'] ) && isset( $data['variations'][ $experiment_name ] ) ) {
+			return $data['variations'][ $experiment_name ];
+		} else {
+			return 'treatment';
+		}
+	}
+}
