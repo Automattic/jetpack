e0bc71610c0842375e32f5887d1dfafd3655e183
mmtr <1233880+mmtr@users.noreply.github.com>
2024-12-06 09:57:37 +0100

    Fix unit tests

--- projects/packages/masterbar/src/admin-menu/class-base-admin-menu.php
+++ projects/packages/masterbar/src/admin-menu/class-base-admin-menu.php
@@ -759,7 +759,9 @@ abstract class Base_Admin_Menu {
 	public function is_using_default_admin_menu() {
 		remove_filter( 'pre_option_wpcom_admin_interface', 'wpcom_admin_interface_pre_get_option' );
 		$option = get_option( 'wpcom_admin_interface' ) !== 'wp-admin';
-		add_filter( 'pre_option_wpcom_admin_interface', 'wpcom_admin_interface_pre_get_option', 10 );
+		if ( function_exists( 'wpcom_admin_interface_pre_get_option' ) ) {
+			add_filter( 'pre_option_wpcom_admin_interface', 'wpcom_admin_interface_pre_get_option', 10 );
+		}
 
 		return $option;
 	}
