e0dca84225b95cfef98e331ef45ae0aa18b3f7dc
bogdanungureanu <bogdanungureanu21@gmail.com>
2024-12-06 15:52:18 +0200

    Check if the function exists, just to be on the safe side.

--- projects/packages/masterbar/src/class-main.php
+++ projects/packages/masterbar/src/class-main.php
@@ -31,7 +31,10 @@ class Main {
 
 		remove_filter( 'pre_option_wpcom_admin_interface', 'wpcom_admin_interface_pre_get_option' );
 		$is_wp_admin_interface = get_option( 'wpcom_admin_interface' ) === 'wp-admin';
-		add_filter( 'pre_option_wpcom_admin_interface', 'wpcom_admin_interface_pre_get_option', 10 );
+		if ( function_exists( 'wpcom_admin_interface_pre_get_option' ) ) {
+			add_filter( 'pre_option_wpcom_admin_interface', 'wpcom_admin_interface_pre_get_option', 10 );
+		}
+
 		if ( $is_wp_admin_interface ) {
 			return;
 		}
