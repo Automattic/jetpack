Automattic/jetpack

commit 7d1307ed4f6844972ecf00fb2f214736f5e99e5e

Update projects/packages/woocommerce-analytics/src/class-checkout-flow.php
Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>

projects/packages/woocommerce-analytics/src/class-checkout-flow.php
@@ -142,7 +142,7 @@ class Checkout_Flow {
 	public function capture_checkout_view() {
 		global $post;
 
-		if ( ! $post ) {
+		if ( ! $post instanceof WP_Post ) {
 			return;
 		}
 

commit 513b0cd73c9cbd647ff783302b8870556d5b7845

Fix namespace issue introduced in 7d1307ed4f6844972ecf00fb2f214736f5e99e5e

projects/packages/woocommerce-analytics/src/class-checkout-flow.php
@@ -9,6 +9,7 @@ namespace Automattic\Woocommerce_Analytics;
 
 use Automattic\Jetpack\Constants;
 use WC_Product;
+use WP_Post;
 
 /**
  * Class that handles all page view events for the checkout flow (from product view to order confirmation view)