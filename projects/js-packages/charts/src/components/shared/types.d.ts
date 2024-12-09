2c41acd96f91cbba354a779ba554baeffb24f441
Grzegorz Chudzinski-Pawlowski <112354940+grzegorz-cp@users.noreply.github.com>
2024-12-09 17:38:05 +1100

    Charts - Adding baaic pie semi circle chart

--- projects/js-packages/charts/src/components/shared/types.d.ts
+++ projects/js-packages/charts/src/components/shared/types.d.ts
@@ -7,3 +7,26 @@ export type DataPointDate = {
 	date: Date;
 	value: number;
 };
+
+export type DataPointPercentage = {
+	/**
+	 * Label for the data point
+	 */
+	label: string;
+	/**
+	 * Numerical value
+	 */
+	value: number;
+	/**
+	 * Formatted value for display
+	 */
+	valueDisplay?: string;
+	/**
+	 * Percentage value
+	 */
+	percentage: number;
+	/**
+	 * Color code for the segment
+	 */
+	color?: string;
+};
