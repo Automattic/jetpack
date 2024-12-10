a4ac2a9f1754d3f10cd312bd7272dc15988a97ad
annacmc <anna.mcphee@automattic.com>
2024-12-11 10:03:59 +1100

    create the base tooltip component

---
+++ projects/js-packages/charts/src/components/tooltip/base-tooltip.tsx
@@ -0,0 +1,48 @@
+import styles from './base-tooltip.module.css';
+import type { TooltipProps } from './types';
+import type { CSSProperties, ComponentType } from 'react';
+
+interface TooltipComponentProps {
+	data: TooltipProps[ 'data' ];
+	className?: string;
+}
+
+const DefaultTooltipContent = ( { data }: TooltipComponentProps ) => (
+	<>
+		{ data.label }: { data.value }
+	</>
+);
+
+type BaseTooltipProps = {
+	data: TooltipProps[ 'data' ];
+	top: number;
+	left: number;
+	style?: CSSProperties;
+	component?: ComponentType< TooltipComponentProps >;
+	className?: string;
+};
+
+export const BaseTooltip = ( {
+	data,
+	top,
+	left,
+	style = {},
+	component: Component = DefaultTooltipContent,
+	className,
+}: BaseTooltipProps ) => {
+	return (
+		<div
+			className={ styles.tooltip }
+			style={ {
+				top,
+				left,
+				...style,
+			} }
+			role="tooltip"
+		>
+			<Component data={ data } className={ className } />
+		</div>
+	);
+};
+
+export type { BaseTooltipProps };
