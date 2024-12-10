17d3f33f4a78d0c0573bd27ee99d11c089e0b94d
Karen Attfield <karenlattfield@gmail.com>
2024-12-10 18:56:11 +0000

    Ensure we consider all iframes on a page - there may be more than one

--- projects/plugins/jetpack/extensions/blocks/slideshow/utils.js
+++ projects/plugins/jetpack/extensions/blocks/slideshow/utils.js
@@ -1,20 +1,14 @@
 export default function applyPaddingForStackBlock() {
-	let parentElement = document.querySelector( '.wp-site-blocks' );
-	// In the template editor, the slideshow block is inside an iframe
-	const iframeDocument = document.querySelector( 'iframe' )?.contentDocument || null;
+	// Helper function to apply styles
+	const applyPaddingStyles = ( doc, isIframe ) => {
+		const parentElement = doc.querySelector( '.wp-site-blocks' );
+		if ( ! parentElement ) return;
 
-	if ( iframeDocument ) {
-		if ( ! parentElement ) {
-			parentElement = iframeDocument.querySelector( '.wp-site-blocks' );
-		}
-	}
-
-	if ( parentElement ) {
 		const { paddingLeft, paddingRight } = window.getComputedStyle( parentElement );
 		const totalPadding = parseFloat( paddingLeft ) + parseFloat( paddingRight );
 
-		const targetElements = ( iframeDocument || document ).querySelectorAll(
-			iframeDocument
+		const targetElements = doc.querySelectorAll(
+			isIframe
 				? '.wp-block-group.is-vertical:not(.is-layout-constrained) .wp-block-jetpack-slideshow'
 				: '.wp-block-group.is-vertical:not(.is-layout-constrained) .wp-block-jetpack-slideshow:not(.entry-content .wp-block-jetpack-slideshow)'
 		);
@@ -22,5 +16,17 @@ export default function applyPaddingForStackBlock() {
 		targetElements.forEach( element => {
 			element.style.maxWidth = `calc(100vw - ${ totalPadding }px)`;
 		} );
-	}
+	};
+
+	// Apply styles to the main document
+	applyPaddingStyles( document, false );
+
+	// Find all iframes and apply styles to their content
+	const iframes = document.querySelectorAll( 'iframe' );
+	iframes.forEach( iframe => {
+		const iframeDoc = iframe.contentDocument;
+		if ( iframeDoc ) {
+			applyPaddingStyles( iframeDoc, true );
+		}
+	} );
 }
