7d27c513b8b89aba7457ca845d20858286bc108f
annacmc <anna.mcphee@automattic.com>
2024-12-11 14:39:58 +1100

    Update base-tooltip.tsx with consolidated types and removed inline styles:

--- projects/js-packages/charts/src/components/tooltip/base-tooltip.tsx
+++ projects/js-packages/charts/src/components/tooltip/base-tooltip.tsx
@@ -1,20 +1,18 @@
 import styles from './base-tooltip.module.scss';
-import type { TooltipProps } from './types';
 import type { CSSProperties, ComponentType } from 'react';
 
-interface TooltipComponentProps {
-	data: TooltipProps[ 'data' ];
-	className?: string;
-}
+type TooltipData = {
+	label: string;
+	value: number;
+};
 
-const DefaultTooltipContent = ( { data }: TooltipComponentProps ) => (
-	<>
-		{ data.label }: { data.value }
-	</>
-);
+type TooltipComponentProps = {
+	data: TooltipData;
+	className?: string;
+};
 
 type BaseTooltipProps = {
-	data: TooltipProps[ 'data' ];
+	data: TooltipData;
 	top: number;
 	left: number;
 	style?: CSSProperties;
@@ -22,27 +20,24 @@ type BaseTooltipProps = {
 	className?: string;
 };
 
+const DefaultTooltipContent = ( { data }: TooltipComponentProps ) => (
+	<>
+		{ data.label }: { data.value }
+	</>
+);
+
 export const BaseTooltip = ( {
 	data,
 	top,
 	left,
-	style = {},
 	component: Component = DefaultTooltipContent,
 	className,
 }: BaseTooltipProps ) => {
 	return (
-		<div
-			className={ styles.tooltip }
-			style={ {
-				top,
-				left,
-				...style,
-			} }
-			role="tooltip"
-		>
+		<div className={ styles.tooltip } style={ { top, left } } role="tooltip">
 			<Component data={ data } className={ className } />
 		</div>
 	);
 };
 
-export type { BaseTooltipProps };
+export type { BaseTooltipProps, TooltipData };
