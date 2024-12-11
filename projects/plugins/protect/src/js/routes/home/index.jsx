f1bc5f7b2f85053971875c7316c1365ef59d7944
dkmyta <43220201+dkmyta@users.noreply.github.com>
2024-12-11 10:17:14 -0800

    Protect: Integrate ScanReport (#40420)

--- projects/plugins/protect/src/js/routes/home/index.jsx
+++ projects/plugins/protect/src/js/routes/home/index.jsx
@@ -1,6 +1,8 @@
-import { AdminSection, Container, Col } from '@automattic/jetpack-components';
+import { AdminSection, Container, Col, ScanReport } from '@automattic/jetpack-components';
 import AdminPage from '../../components/admin-page';
+import useScanStatusQuery from '../../data/scan/use-scan-status-query';
 import HomeAdminSectionHero from './home-admin-section-hero';
+import styles from './styles.module.scss';
 
 /**
  * Home Page
@@ -10,12 +12,30 @@ import HomeAdminSectionHero from './home-admin-section-hero';
  * @return {Component} The root component for the scan page.
  */
 const HomePage = () => {
+	const { data: status } = useScanStatusQuery( { usePolling: true } );
+	const { core, plugins, themes, files } = status;
+
+	const data = [
+		core,
+		...plugins,
+		...themes,
+		{ checked: true, threats: files, type: 'files' },
+	].map( ( item, index ) => {
+		return { id: index + 1, ...item };
+	} );
+
 	return (
 		<AdminPage>
 			<HomeAdminSectionHero />
 			<AdminSection>
-				<Container horizontalSpacing={ 7 } horizontalGap={ 4 }>
-					<Col>{ /* TODO: Add ScanReport component here */ }</Col>
+				<Container
+					className={ styles[ 'scan-report-container' ] }
+					horizontalSpacing={ 5 }
+					horizontalGap={ 4 }
+				>
+					<Col>
+						<ScanReport dataSource={ status.dataSource } data={ data } />
+					</Col>
 				</Container>
 			</AdminSection>
 		</AdminPage>
