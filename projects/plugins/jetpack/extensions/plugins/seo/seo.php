e5cd1b1fb8b5aa0396037f1e5b7f8963facd8b33
Karen Attfield <karenlattfield@gmail.com>
2024-12-11 14:25:14 +0000

    Ensure we only enable the SEO extension if the jetpack_disable_seo_tools is not set to false

--- projects/plugins/jetpack/extensions/plugins/seo/seo.php
+++ projects/plugins/jetpack/extensions/plugins/seo/seo.php
@@ -11,7 +11,17 @@ add_action(
 	'jetpack_register_gutenberg_extensions',
 	function () {
 		\Jetpack_Gutenberg::set_availability_for_plan( 'advanced-seo' );
-		\Jetpack_Gutenberg::set_extension_available( 'jetpack-seo' );
+	}
+);
+
+add_action(
+	'after_setup_theme',
+	function () {
+		$disable_seo_tools = apply_filters( 'jetpack_disable_seo_tools', false );
+		// We only want to enable the SEO extension (and display SEO settings) if the 'jetpack_disable_seo_tools' filter is not set to false.
+		if ( ! $disable_seo_tools ) {
+			\Jetpack_Gutenberg::set_extension_available( 'jetpack-seo' );
+		}
 	}
 );
 
