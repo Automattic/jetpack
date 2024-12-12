636383f15a87c55a05138af911579a79165eeaed
Richard Ortiz <rcrd.ortiz@gmail.com>
2024-12-12 12:43:51 +0100

    We now trigger an error in cases where the media property does not exist so that the editor does not crash under some circumstances

--- projects/plugins/jetpack/extensions/shared/external-media/sources/with-media.js
+++ projects/plugins/jetpack/extensions/shared/external-media/sources/with-media.js
@@ -169,6 +169,11 @@ export default function withMedia( mediaSource = MediaSource.Unknown ) {
 					parse: window.wpcomFetch === undefined,
 				} )
 					.then( result => {
+						// If we don't have media available, we should show an error instead of crashing the editor.
+						if ( result.media === undefined ) {
+							result.code = 'internal_server_error';
+							throw result;
+						}
 						this.setState( {
 							account: result.meta.account,
 							media: this.mergeMedia( media, result.media ),
