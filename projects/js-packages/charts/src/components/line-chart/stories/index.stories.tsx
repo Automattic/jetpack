14ca95e74d6b423d8abeb8b1a42eac24e8c5933c
Grzegorz Chudzinski-Pawlowski <112354940+grzegorz-cp@users.noreply.github.com>
2024-12-13 11:56:52 +1100

    Charts - Updating examples

--- projects/js-packages/charts/src/components/line-chart/stories/index.stories.tsx
+++ projects/js-packages/charts/src/components/line-chart/stories/index.stories.tsx
@@ -19,10 +19,39 @@ export default {
 
 const Template = args => <LineChart { ...args } />;
 
+// Default story with multiple series
 export const Default = Template.bind( {} );
 Default.args = {
 	width: 500,
 	height: 300,
+	margin: { top: 20, right: 20, bottom: 30, left: 40 },
+	data: sampleData,
+};
+
+// Story with single data series
+export const SingleSeries = Template.bind( {} );
+SingleSeries.args = {
+	width: 500,
+	height: 300,
+	margin: { top: 20, right: 20, bottom: 30, left: 40 },
+	data: [ sampleData[ 0 ] ], // Only London temperature data
+};
+
+// Story without tooltip
+export const WithoutTooltip = Template.bind( {} );
+WithoutTooltip.args = {
+	width: 500,
+	height: 300,
+	margin: { top: 20, right: 20, bottom: 30, left: 40 },
+	data: sampleData,
+	withTooltips: false,
+};
+
+// Story with custom dimensions
+export const CustomDimensions = Template.bind( {} );
+CustomDimensions.args = {
+	width: 800,
+	height: 400,
 	margin: { top: 20, right: 20, bottom: 30, left: 40 },
 	data: sampleData,
 };
