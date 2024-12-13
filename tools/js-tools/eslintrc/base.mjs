933be9cc9c9a988ffae00f834ccce66a9fc154be
Manzoor Wani <manzoorwani.jk@gmail.com>
2024-12-13 20:32:49 +0530

    Turn back jsdoc/no-undefined-types ON with disabled reporting

--- tools/js-tools/eslintrc/base.mjs
+++ tools/js-tools/eslintrc/base.mjs
@@ -327,8 +327,8 @@ export default function makeBaseConfig( configurl, opts = {} ) {
 				],
 				// Disable param name check for destructured params.
 				'jsdoc/check-param-names': [ 'error', { checkDestructured: false } ],
-				// TS should be enough to catch undefined types.
-				'jsdoc/no-undefined-types': 'off',
+				// Mark types in jsdoc as used without reporting about any that are undefined.
+				'jsdoc/no-undefined-types': [ 'warn', { disableReporting: true } ],
 				// Disable param check except when destructured.
 				'jsdoc/require-param': [ 'error', { checkDestructured: false } ],
 				// TS should mostly have the type set.
