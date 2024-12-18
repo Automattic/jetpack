e34dc193b5e40f77076516e91ab2a34f9a522071
Ashar Fuadi <ashar.fuadi@automattic.com>
2024-12-18 20:54:55 +0700

    Gutenberg 19.8.0 hotfix: don't show the template-locked rendering mode for pages

--- projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
+++ projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
@@ -125,6 +125,20 @@ class Jetpack_Mu_Wpcom {
 		if ( class_exists( 'Automattic\Jetpack\Scheduled_Updates' ) ) {
 			Scheduled_Updates::init();
 		}
+
+		/**
+		 * Hotfix for a Gutenberg 19.8.0 bug preventing lower-capability users from editing pages.
+		 * See: p1734525664059729-slack-C02FMH4G8
+		 * See: https://github.com/WordPress/gutenberg/issues/68053#issuecomment-2550730705
+		 */
+		add_filter(
+			'register_post_type_args',
+			function ( $args ) {
+				unset( $args['default_rendering_mode'] );
+				return $args;
+			},
+			20
+		);
 	}
 
 	/**
