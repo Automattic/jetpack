e3efa9ba114927b2928a172b0748f4b73c20d908
Manzoor Wani <manzoorwani.jk@gmail.com>
2024-12-19 17:48:32 +0530

    Restore deprecated connection fields for time being

--- projects/packages/publicize/src/class-connections.php
+++ projects/packages/publicize/src/class-connections.php
@@ -7,6 +7,7 @@
 
 namespace Automattic\Jetpack\Publicize;
 
+use Automattic\Jetpack\Connection;
 use Automattic\Jetpack\Publicize\REST_API\Connections_Controller;
 use Automattic\Jetpack\Status\Host;
 
@@ -33,23 +34,60 @@ class Connections {
 
 		if ( $is_wpcom ) {
 			// We don't need to cache connections for simple sites.
-			return Connections_Controller::get_connections( $run_tests );
+			$connections = Connections_Controller::get_connections( $run_tests );
+		} else {
+
+			$clear_cache = $args['clear_cache'] ?? false;
+
+			if ( $clear_cache || $run_tests ) {
+				self::clear_transient();
+			}
+
+			$connections = get_transient( self::CONNECTIONS_TRANSIENT );
+
+			// This can be an empty array, so we need to check for false.
+			if ( false === $connections ) {
+				$connections = self::fetch_and_cache_connections( $run_tests );
+			}
 		}
 
-		$clear_cache = $args['clear_cache'] ?? false;
-
-		if ( $clear_cache || $run_tests ) {
-			self::clear_transient();
-		}
-
-		$connections = get_transient( self::CONNECTIONS_TRANSIENT );
-
-		// This can be an empty array, so we need to check for false.
-		if ( false === $connections ) {
-			$connections = self::fetch_and_cache_connections( $run_tests );
-		}
+		// Let us add the deprecated fields for now.
+		// TODO: Remove this after https://github.com/Automattic/jetpack/pull/40539 is merged.
+		$connections = self::retain_deprecated_fields( $connections );
 
 		return $connections;
+	}
+
+	/**
+	 * Retain deprecated fields.
+	 *
+	 * @param array $connections Connections.
+	 * @return array
+	 */
+	private static function retain_deprecated_fields( $connections ) {
+		return array_map(
+			function ( $connection ) {
+				$wpcom_user_data = ( new Connection\Manager() )->get_connected_user_data();
+
+				$owns_connection = ! empty( $wpcom_user_data['ID'] ) && $wpcom_user_data['ID'] === $connection['user_id'];
+
+				$connection = array_merge(
+					$connection,
+					array(
+						'external_display' => $connection['display_name'],
+						'can_disconnect'   => current_user_can( 'edit_others_posts' ) || $owns_connection,
+						'label'            => $connection['service_label'],
+					)
+				);
+
+				if ( 'bluesky' === $connection['service_name'] ) {
+					$connection['external_name'] = $connection['external_handle'];
+				}
+
+				return $connection;
+			},
+			$connections
+		);
 	}
 
 	/**
