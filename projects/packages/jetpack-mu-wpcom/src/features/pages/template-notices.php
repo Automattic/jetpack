74a1639a85ff0892eb3dbf88723e337678472d05
mmtr <1233880+mmtr@users.noreply.github.com>
2024-12-20 17:18:31 +0100

    Get data from template and add actions

--- projects/packages/jetpack-mu-wpcom/src/features/pages/template-notices.php
+++ projects/packages/jetpack-mu-wpcom/src/features/pages/template-notices.php
@@ -42,8 +42,8 @@ function wpcom_show_homepage_notice() {
 	}
 
 	$show_on_front  = get_option( 'show_on_front' );
-	$page_on_front  = get_option( 'page_on_front' );
-	$posts_on_front = $show_on_front === 'posts' || ( $show_on_front === 'page' && ! $page_on_front );
+	$front_page_id  = get_option( 'page_on_front' );
+	$posts_on_front = $show_on_front === 'posts' || ( $show_on_front === 'page' && ! $front_page_id );
 	if ( ! $posts_on_front ) {
 		return;
 	}
@@ -63,17 +63,67 @@ function wpcom_show_homepage_notice() {
 		}
 	}
 	// phpcs:enable WordPress.Security.NonceVerification.Recommended
+
+	$template = resolve_block_template( 'home', array( 'front-page', 'home', 'index' ), '' );
+	if ( ! $template ) {
+		return;
+	}
+
+	if ( $template->wp_id ) {
+		$template_title = apply_filters( 'the_title', $template->title, $template->wp_id );
+	} else {
+		$template_title = $template->title;
+	}
+
+	$can_edit  = current_user_can( 'edit_theme_options' );
+	$edit_link = '/wp-admin/site-editor.php';
+	$view_link = home_url();
+	// Link to the wp-admin stats page.
+
+	if ( 'wp-admin' === get_option( 'wpcom_admin_interface' ) ) {
+		$stats_link = admin_url( sprintf( 'admin.php?page=stats#!/stats/post/0/%d', Jetpack_Options::get_option( 'id', 0 ) ) );
+	} else {
+		$domain     = wp_parse_url( home_url(), PHP_URL_HOST );
+		$stats_link = sprintf( 'https://wordpress.com/stats/post/0/%s', $domain );
+	}
+
+	if ( $can_edit ) {
+		$main_action = __( 'Edit homepage', 'jetpack-mu-wpcom' );
+		$main_link   = '/wp-admin/site-editor.php';
+	} else {
+		$main_action = __( 'View homepage', 'jetpack-mu-wpcom' );
+		$main_link   = home_url();
+	}
+
+	/* translators: template title */
+	$description = sprintf( __( 'Your homepage uses the %s template.', 'jetpack-mu-wpcom' ), $template_title );
 	?>
 	<template id="wpcom-homepate-notice-template">
 		<div class="wpcom-homepage-notice card">
-			<div class="icon">
+			<div class="wpcom-homepage-notice-icon">
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 					<path d="M12 4L4 7.9V20h16V7.9L12 4zm6.5 14.5H14V13h-4v5.5H5.5V8.8L12 5.7l6.5 3.1v9.7z" />
 				</svg>
 			</div>
 			<div>
-				<h2><?php esc_html_e( 'Homepage', 'jetpack-mu-wpcom' ); ?></h2>
-				<p><?php esc_html_e( 'Your homepage uses the Blog Home template', 'jetpack-mu-wpcom' ); ?></p>
+				<h2 class="wpcom-homepage-notice-title">
+					<a
+						href="<?php echo esc_attr( $main_link ); ?>"
+						title="<?php echo esc_attr( $main_action ); ?>"
+					>
+						<?php esc_html_e( 'Homepage', 'jetpack-mu-wpcom' ); ?>
+					</a>
+				</h2>
+				<p class="wpcom-homepage-notice-description">
+					<?php echo esc_attr( $description ); ?>
+				</p>
+				<div class="wpcom-homepage-notice-actions">
+					<?php if ( $can_edit ) { ?>
+						<a href="<?php echo esc_attr( $edit_link ); ?>"><?php esc_html_e( 'Edit', 'jetpack-mu-wpcom' ); ?></a>
+					<?php } ?>
+					<a href="<?php echo esc_attr( $view_link ); ?>"><?php esc_html_e( 'View', 'jetpack-mu-wpcom' ); ?></a>
+					<a href="<?php echo esc_attr( $stats_link ); ?>"><?php esc_html_e( 'Stats', 'jetpack-mu-wpcom' ); ?></a>
+				</div>
 			</div>
 		</div>
 	</template>
@@ -88,18 +138,41 @@ function wpcom_show_homepage_notice() {
 			gap: 16px;
 		}
 
-		.wpcom-homepage-notice h2,
-		.wpcom-homepage-notice p {
-			margin: 0;
-		}
-
-		.wpcom-homepage-notice h2 {
-			margin-bottom: 0.25em;
-		}
-
-		.wpcom-homepage-notice svg {
+		.wpcom-homepage-notice-icon svg {
 			width: 24px;
 			height: 24px;
+		}
+
+		.wpcom-homepage-notice-title,
+		.wpcom-homepage-notice-description {
+			margin: 0 0 0.25em;
+		}
+
+		.wpcom-homepage-notice-title {
+			display: flex;
+			gap: 2px;
+		}
+
+		.wpcom-homepage-notice a {
+			text-decoration: none;
+		}
+
+		.wpcom-homepage-notice-actions {
+			display: flex;
+		}
+
+		.wpcom-homepage-notice-actions a {
+			border-left: 1px solid #a7aaad;
+			padding: 0 4px;
+		}
+
+		.wpcom-homepage-notice-actions a:first-child {
+			border-left: none;
+			padding-left: 0;
+		}
+
+		.wpcom-homepage-notice-actions a:last-child {
+			padding-right: 0;
 		}
 	</style>
 	<script type="text/javascript">
