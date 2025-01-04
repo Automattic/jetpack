fb418db00a1e9295b4b21b4add2214af748f5f37
dkmyta <43220201+dkmyta@users.noreply.github.com>
2025-01-03 16:53:21 -0800

    Protect: Restore HistoryAdminSectionHero (#40551)
    * Init project branch
    
    * Protect: Add Go to Cloud and Scan now button to Protect primary header (#40057)
    
    Co-authored-by: Nate Weller <hello@nateweller.com>
    
    * Protect: Update Scan and History headers (#40058)
    
    * Update Scan and History section header structure/content
    
    * changelog
    
    * Update projects/plugins/protect/src/js/routes/scan/scan-admin-section-hero.tsx
    
    Co-authored-by: Nate Weller <nate.weller@automattic.com>
    
    ---------
    
    Co-authored-by: Nate Weller <nate.weller@automattic.com>
    
    * Protect: de-emphasize cloud link by using link variant (#40211)
    
    * Protect: add ShieldIcon component
    
    * Protect: Add ShieldIcon Component (#40402)
    
    * Protect: Integrate ThreatsDataViews Component (#40076)
    
    * Components: Add ScanReport (#40419)
    
    * Fix type errors
    
    Protect: add HMR support
    
    Revert "Protect: add HMR support"
    
    This reverts commit 06497a05bb050c86e097b36038c8742af427388d.
    
    * Protect: Refactor AdminSectionHero (#40516)
    
    * Restore history header
    
    * Pass status filter presets to consumer
    
    * Restore early return
    
    * Add plan level restrictions
    
    * Remove unneeded filter handling
    
    * Revert unnecessary threats data views updates
    
    * Fix import
    
    * Add size prop
    
    ---------
    
    Co-authored-by: Nate Weller <hello@nateweller.com>
    Co-authored-by: Nate Weller <nate.weller@automattic.com>

---
+++ projects/plugins/protect/src/js/routes/scan/history-admin-section-hero.tsx
@@ -0,0 +1,78 @@
+import { Text } from '@automattic/jetpack-components';
+import { dateI18n } from '@wordpress/date';
+import { __, sprintf } from '@wordpress/i18n';
+import clsx from 'clsx';
+import { useMemo } from 'react';
+import AdminSectionHero from '../../components/admin-section-hero';
+import ErrorAdminSectionHero from '../../components/error-admin-section-hero';
+import useHistoryQuery from '../../data/scan/use-history-query';
+import styles from './styles.module.scss';
+
+const HistoryAdminSectionHero: React.FC = ( {
+	size = 'normal',
+}: {
+	size?: 'normal' | 'large';
+} ) => {
+	const { data: history } = useHistoryQuery();
+	const numThreats = history ? history.threats.length : 0;
+
+	const oldestFirstDetected = useMemo( () => {
+		if ( ! history ) {
+			return null;
+		}
+
+		return history.threats.reduce( ( oldest, current ) => {
+			return new Date( current.firstDetected ) < new Date( oldest.firstDetected )
+				? current
+				: oldest;
+		} ).firstDetected;
+	}, [ history ] );
+
+	if ( history && history.error ) {
+		return (
+			<ErrorAdminSectionHero
+				baseErrorMessage={ __( 'We are having problems loading your history.', 'jetpack-protect' ) }
+				errorMessage={ history.errorMessage }
+				errorCode={ history.errorMessage }
+			/>
+		);
+	}
+
+	return (
+		<AdminSectionHero>
+			<AdminSectionHero.Main
+				className={ clsx( styles[ 'hero-main' ], {
+					[ styles[ 'hero-main--large' ] ]: size === 'large',
+				} ) }
+			>
+				{ ' ' }
+				<Text mb={ 2 }>
+					{ oldestFirstDetected ? (
+						<span className={ styles[ 'subheading-content' ] }>
+							{ sprintf(
+								/* translators: %s: Oldest first detected date */
+								__( '%s - Today', 'jetpack-protect' ),
+								dateI18n( 'F jS g:i A', oldestFirstDetected, false )
+							) }
+						</span>
+					) : (
+						__( 'Most recent results', 'jetpack-protect' )
+					) }
+				</Text>
+				<AdminSectionHero.Heading icon={ numThreats > 0 ? 'error' : 'success' }>
+					{ numThreats > 0
+						? sprintf(
+								/* translators: %s: Total number of threats  */
+								__( '%1$s previously active %2$s', 'jetpack-protect' ),
+								numThreats,
+								numThreats === 1 ? 'threat' : 'threats'
+						  )
+						: __( 'No previously active threats', 'jetpack-protect' ) }
+				</AdminSectionHero.Heading>
+				<Text>{ __( 'Here you can view all of your threats to date.', 'jetpack-protect' ) }</Text>
+			</AdminSectionHero.Main>
+		</AdminSectionHero>
+	);
+};
+
+export default HistoryAdminSectionHero;
