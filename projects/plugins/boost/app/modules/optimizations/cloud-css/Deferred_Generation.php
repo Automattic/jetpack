a8d438ddfaa60cc9f243d717f65bb7894aed2c6a
Peter Petrov <peter.petrov89@gmail.com>
2025-01-08 16:06:20 +0200

    Add comments

--- projects/plugins/boost/app/modules/optimizations/cloud-css/Deferred_Generation.php
+++ projects/plugins/boost/app/modules/optimizations/cloud-css/Deferred_Generation.php
@@ -2,19 +2,31 @@
 
 namespace Automattic\Jetpack_Boost\Modules\Optimizations\Cloud_CSS;
 
+/**
+ * Handles deferring the Cloud CSS generation.
+ */
 class Deferred_Generation {
 
 	const SCHEDULER_HOOK = 'jetpack_boost_cloud_css_deferred_generation';
 
+	/**
+	 * Initialize the deferred generation.
+	 */
 	public static function init() {
 		add_action( self::SCHEDULER_HOOK, array( self::class, 'run' ) );
 	}
 
+	/**
+	 * Run the deferred generation.
+	 */
 	public static function run() {
 		$cloud_css = new Cloud_CSS();
 		$cloud_css->regenerate_cloud_css( Cloud_CSS::REGENERATE_REASON_SAVE_POST, $cloud_css->get_all_providers() );
 	}
 
+	/**
+	 * Schedule the deferred generation.
+	 */
 	public static function schedule() {
 		$scheduled = wp_next_scheduled( self::SCHEDULER_HOOK );
 		if ( $scheduled ) {
@@ -27,6 +39,9 @@ class Deferred_Generation {
 		);
 	}
 
+	/**
+	 * Clear the deferred generation.
+	 */
 	public static function clear() {
 		wp_clear_scheduled_hook( self::SCHEDULER_HOOK );
 	}
