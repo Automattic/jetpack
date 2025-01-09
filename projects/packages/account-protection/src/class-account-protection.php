d20732c63aee9d716d1b614d029ae744cf6cf3d4
dkmyta <dkmyta10@gmail.com>
2025-01-09 05:33:04 -0800

    Add account protection package

---
+++ projects/packages/account-protection/src/class-account-protection.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Package description here
+ *
+ * @package automattic/jetpack-account-protection
+ */
+
+namespace Automattic\Jetpack;
+
+/**
+ * Class description.
+ */
+class Account_Protection {
+
+	const PACKAGE_VERSION = '1.0.0-alpha';
+}
