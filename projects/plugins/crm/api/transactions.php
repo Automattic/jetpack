cab54a1300c0e5539285fbfaee2cd77f6a3040d1
gogdzl <diego.rodrigues@automattic.com>
2025-01-10 15:37:04 -0300

    Update code to use DAL call

--- projects/plugins/crm/api/transactions.php
+++ projects/plugins/crm/api/transactions.php
@@ -27,9 +27,15 @@ jpcrm_api_check_http_method( array( 'GET' ) );
 
 // Process the pagination parameters from the query
 $pagination = jpcrm_api_process_pagination();
+$args       = array(
+	'withAssigned' => false,
+	'withTags'     => false,
+	'page'         => $pagination['page'],
+	'perPage'      => $pagination['per_page'],
+	'sortOrder'    => $pagination['order'],
+);
 
-// needs moving to the $args version
-// v3.0 needs these objects refined, including textify for html
-$transactions = zeroBS_getTransactions( true, $pagination['per_page'], $pagination['page'], false, '', array(), array(), 'ID', $pagination['order'] );
+global $zbs;
+$transactions = $zbs->DAL->transactions->getTransactions( $args ); // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
 
 wp_send_json( $transactions );
