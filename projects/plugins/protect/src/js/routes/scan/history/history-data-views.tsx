5ccae122e36df0ded3f8555b7d1c555eb3fc7a4b
Nate Weller <hello@nateweller.com>
2025-01-12 14:45:27 -0800

    Fix ts warning

--- projects/plugins/protect/src/js/routes/scan/history/history-data-views.tsx
+++ projects/plugins/protect/src/js/routes/scan/history/history-data-views.tsx
@@ -20,9 +20,9 @@ export default function HistoryDataViews() {
 		if ( filter ) {
 			return [
 				{
-					field: 'status',
+					field: 'status' as const,
 					value: filter,
-					operator: 'isAny',
+					operator: 'isAny' as const,
 				},
 			];
 		}
