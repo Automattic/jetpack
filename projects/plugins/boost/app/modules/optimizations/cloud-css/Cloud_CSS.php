15f998c14a646aa5ccc55fd4a06674b070aa0f7c
Peter Petrov <peter.petrov89@gmail.com>
2025-01-14 11:58:12 +0200

    Revert requestId update

--- projects/plugins/boost/app/modules/optimizations/cloud-css/Cloud_CSS.php
+++ projects/plugins/boost/app/modules/optimizations/cloud-css/Cloud_CSS.php
@@ -146,7 +146,7 @@ class Cloud_CSS implements Pluggable, Has_Always_Available_Endpoints, Changes_Pa
 
 		// Send the request to the Cloud.
 		$payload              = array( 'providers' => $grouped_urls );
-		$payload['requestId'] = md5( wp_json_encode( $payload ) );
+		$payload['requestId'] = md5( wp_json_encode( $payload ) . time() );
 		$payload['reason']    = $reason;
 		return Boost_API::post( 'cloud-css', $payload );
 	}
