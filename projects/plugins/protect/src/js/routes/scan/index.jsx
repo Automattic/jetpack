5eda38edd52db184147ee4f719e7a7f785f3f8de
Nate Weller <nate.weller@automattic.com>
2025-01-14 11:40:13 -0700

    Protect Meets Core: Fix "0" rendered by conditional render chain (#40882)

--- projects/plugins/protect/src/js/routes/scan/index.jsx
+++ projects/plugins/protect/src/js/routes/scan/index.jsx
@@ -35,8 +35,8 @@ const ScanPage = () => {
 		currentScanStatus = 'active';
 	}
 
-	const hasActiveThreats = status && status.threats.length;
-	const hasHistory = history && history.threats.length;
+	const hasActiveThreats = status && !! status.threats.length;
+	const hasHistory = history && !! history.threats.length;
 	const showResults = hasActiveThreats || hasHistory;
 
 	// Track view for Protect admin page.
