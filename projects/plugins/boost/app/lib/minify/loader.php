8373fa796941a5138684a0732337b097521f59ed
Donncha O Caoimh <5656673+donnchawp@users.noreply.github.com>
2025-01-16 14:20:33 +0000

    Always load minification classes, for maintenance tasks

--- projects/plugins/boost/app/lib/minify/loader.php
+++ projects/plugins/boost/app/lib/minify/loader.php
@@ -1,4 +1,13 @@
 <?php
+
+// Load minify library code.
+require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/Utils.php';
+require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/Config.php';
+require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/Dependency_Path_Mapping.php';
+require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/functions-helpers.php';
+require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/functions-service.php';
+require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/functions-service-new.php';
+
 // Only intercept 404 requests.
 if ( ! is_404() ) {
 	return;
@@ -9,14 +18,6 @@ $request_uri = remove_query_arg( 'JB_NONEXISTENTQUERY_ARG' );
 if ( stripos( $request_uri, '/wp-content/boost-cache/static/' ) === false ) {
 	return;
 }
-
-// Load minify library code.
-require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/Utils.php';
-require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/Config.php';
-require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/Dependency_Path_Mapping.php';
-require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/functions-helpers.php';
-require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/functions-service.php';
-require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/functions-service-new.php';
 
 // Send a status 200 header, otherwise the browser will return a 404.
 status_header( 200 );
