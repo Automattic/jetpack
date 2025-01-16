f3960840bbc25bde6d1834e3dacd6acd565915ba
Donncha O Caoimh <5656673+donnchawp@users.noreply.github.com>
2025-01-16 14:00:47 +0000

    Load the minification loader

--- projects/plugins/boost/jetpack-boost.php
+++ projects/plugins/boost/jetpack-boost.php
@@ -141,33 +141,7 @@ if ( is_readable( $boost_packages_path ) ) {
 add_action(
 	'template_redirect',
 	function () {
-		// We only want to hijack the 404 page.
-		if ( ! is_404() ) {
-			return;
-		}
-
-		// Get the path for the current request.
-		$request_uri = remove_query_arg( 'JB_NONEXISTENTQUERY_ARG' );
-		if ( stripos( $request_uri, '/wp-content/boost-cache/static/' ) === false ) {
-			return;
-		}
-
-		// @todo - Get hash and check database for files related to it.
-		// If not found, return a 404.
-		// If found, get their content, minify it, and print.
-
-		// Load minify library code.
-		require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/Utils.php';
-		require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/Config.php';
-		require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/Dependency_Path_Mapping.php';
-		require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/functions-helpers.php';
-		require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/functions-service.php';
-		require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/functions-service-new.php';
-
-		// Send a status 200 header, otherwise the browser will return a 404.
-		status_header( 200 );
-		jetpack_boost_handle_minify_request( $request_uri );
-		exit;
+		require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/loader.php';
 	}
 );
 
