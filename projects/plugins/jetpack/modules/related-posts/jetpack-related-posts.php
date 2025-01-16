4b606339142d8dd48b12e25c902a52e1709f0a73
okmttdhr <okmttdhr@users.noreply.github.com>
2025-01-16 15:34:49 +0900

    Hide settings on Simple

--- projects/plugins/jetpack/modules/related-posts/jetpack-related-posts.php
+++ projects/plugins/jetpack/modules/related-posts/jetpack-related-posts.php
@@ -727,24 +727,6 @@ EOT;
 			esc_html__( 'Preview:', 'jetpack' )
 		);
 
-		// The above settings does not take effect on block themes.
-		if ( wp_is_block_theme() ) {
-			$ui_settings = sprintf(
-				'<p><a href="%s" target="_blank" rel="noopener noreferrer">%s</a> %s</p>',
-				esc_url(
-					Redirect::get_url(
-						'jetpack-support-related-posts',
-						array(
-							'anchor' => 'adding-related-posts-block-theme',
-							'site'   => $this->get_blog_id(),
-						)
-					)
-				),
-				esc_html__( 'Add a Related Posts Block to your site’s template in the site editor', 'jetpack' ),
-				esc_html__( 'to keep your visitors engaged with related content at the bottom of each post.', 'jetpack' )
-			);
-		}
-
 		if ( ! $this->allow_feature_toggle() ) {
 			$template = <<<EOT
 <input type="hidden" name="jetpack_relatedposts[enabled]" value="1" />
@@ -755,6 +737,28 @@ EOT;
 				$ui_settings // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- data is escaped when variable is set.
 			);
 		} else {
+			// Hide settings that does not take effect on block themes.
+			if ( wp_is_block_theme() ) {
+				$ui_settings = sprintf(
+					'<p><a href="%s" target="_blank" rel="noopener noreferrer">%s</a> %s</p>',
+					esc_url(
+						Redirect::get_url(
+							'jetpack-support-related-posts',
+							array(
+								'anchor' => 'adding-related-posts-block-theme',
+								'site'   => $this->get_blog_id(),
+							)
+						)
+					),
+					esc_html__( 'Add a Related Posts Block to your site’s template in the site editor', 'jetpack' ),
+					esc_html__( 'to keep your visitors engaged with related content at the bottom of each post.', 'jetpack' )
+				);
+				printf(
+					$ui_settings // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- data is escaped when variable is set.
+				);
+				return;
+			}
+
 			$template = <<<EOT
 <ul id="settings-reading-relatedposts">
 	<li>
