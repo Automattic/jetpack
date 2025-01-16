450fd9b76342ba7d0a04ab5633391bbccacf93a3
Jasper Kang <jasper.kang@automattic.com>
2025-01-17 11:34:46 +1300

    fix sample data

--- projects/js-packages/charts/src/components/line-chart/stories/site-traffic-sample.ts
+++ projects/js-packages/charts/src/components/line-chart/stories/site-traffic-sample.ts
@@ -7,51 +7,51 @@ export default [
 		data: [
 			{
 				date: new Date( '2024-01-01' ),
-				value: 2412.12,
+				value: 2412,
 			},
 			{
 				date: new Date( '2024-01-02' ),
-				value: 1899.42,
+				value: 1899,
 			},
 			{
 				date: new Date( '2024-01-03' ),
-				value: 2061.46,
+				value: 2061,
 			},
 			{
 				date: new Date( '2024-01-04' ),
-				value: 1939.93,
+				value: 1939,
 			},
 			{
 				date: new Date( '2024-01-05' ),
-				value: 1986.85,
+				value: 1986,
 			},
 			{
 				date: new Date( '2024-01-06' ),
-				value: 1560.45,
+				value: 1560,
 			},
 			{
 				date: new Date( '2024-01-07' ),
-				value: 1741.28,
+				value: 1741,
 			},
 			{
 				date: new Date( '2024-01-08' ),
-				value: 2120.05,
+				value: 2120,
 			},
 			{
 				date: new Date( '2024-01-09' ),
-				value: 1889.73,
+				value: 1889,
 			},
 			{
 				date: new Date( '2024-01-10' ),
-				value: 1666.45,
+				value: 1666,
 			},
 			{
 				date: new Date( '2024-01-11' ),
-				value: 2396.03,
+				value: 2396,
 			},
 			{
 				date: new Date( '2024-01-12' ),
-				value: 2276.12,
+				value: 2276,
 			},
 			{
 				date: new Date( '2024-01-13' ),
@@ -59,71 +59,71 @@ export default [
 			},
 			{
 				date: new Date( '2024-01-14' ),
-				value: 1228.54,
+				value: 1228,
 			},
 			{
 				date: new Date( '2024-01-15' ),
-				value: 1600.33,
+				value: 1600,
 			},
 			{
 				date: new Date( '2024-01-16' ),
-				value: 1814.22,
+				value: 1814,
 			},
 			{
 				date: new Date( '2024-01-17' ),
-				value: 1701.17,
+				value: 1701,
 			},
 			{
 				date: new Date( '2024-01-18' ),
-				value: 1507.86,
+				value: 1507,
 			},
 			{
 				date: new Date( '2024-01-19' ),
-				value: 1833.56,
+				value: 1833,
 			},
 			{
 				date: new Date( '2024-01-20' ),
-				value: 1407.48,
+				value: 1407,
 			},
 			{
 				date: new Date( '2024-01-21' ),
-				value: 965.93,
+				value: 965,
 			},
 			{
 				date: new Date( '2024-01-22' ),
-				value: 2288.24,
+				value: 2288,
 			},
 			{
 				date: new Date( '2024-01-23' ),
-				value: 2135.49,
+				value: 2135,
 			},
 			{
 				date: new Date( '2024-01-24' ),
-				value: 1824.84,
+				value: 1824,
 			},
 			{
 				date: new Date( '2024-01-25' ),
-				value: 2219.89,
+				value: 2219,
 			},
 			{
 				date: new Date( '2024-01-26' ),
-				value: 1918.42,
+				value: 1918,
 			},
 			{
 				date: new Date( '2024-01-27' ),
-				value: 1101.28,
+				value: 1101,
 			},
 			{
 				date: new Date( '2024-01-28' ),
-				value: 1695.33,
+				value: 1695,
 			},
 			{
 				date: new Date( '2024-01-29' ),
-				value: 1615.09,
+				value: 1615,
 			},
 			{
 				date: new Date( '2024-01-30' ),
-				value: 2056.87,
+				value: 2056,
 			},
 		],
 	},
@@ -135,27 +135,27 @@ export default [
 		data: [
 			{
 				date: new Date( '2024-01-01' ),
-				value: 2558.63,
+				value: 2558,
 			},
 			{
 				date: new Date( '2024-01-02' ),
-				value: 3399.23,
+				value: 3399,
 			},
 			{
 				date: new Date( '2024-01-03' ),
-				value: 2260.18,
+				value: 2260,
 			},
 			{
 				date: new Date( '2024-01-04' ),
-				value: 2331.22,
+				value: 2331,
 			},
 			{
 				date: new Date( '2024-01-05' ),
-				value: 3302.97,
+				value: 3302,
 			},
 			{
 				date: new Date( '2024-01-06' ),
-				value: 1481.83,
+				value: 1852,
 			},
 			{
 				date: new Date( '2024-01-07' ),
@@ -163,95 +163,95 @@ export default [
 			},
 			{
 				date: new Date( '2024-01-08' ),
-				value: 2804.36,
+				value: 2804,
 			},
 			{
 				date: new Date( '2024-01-09' ),
-				value: 3260.53,
+				value: 3260,
 			},
 			{
 				date: new Date( '2024-01-10' ),
-				value: 2941.85,
+				value: 2941,
 			},
 			{
 				date: new Date( '2024-01-11' ),
-				value: 2857.21,
+				value: 2857,
 			},
 			{
 				date: new Date( '2024-01-12' ),
-				value: 3461.18,
+				value: 3461,
 			},
 			{
 				date: new Date( '2024-01-13' ),
-				value: 1548.95,
+				value: 1548,
 			},
 			{
 				date: new Date( '2024-01-14' ),
-				value: 2739.51,
+				value: 2739,
 			},
 			{
 				date: new Date( '2024-01-15' ),
-				value: 3288.45,
+				value: 3288,
 			},
 			{
 				date: new Date( '2024-01-16' ),
-				value: 2869.58,
+				value: 2869,
 			},
 			{
 				date: new Date( '2024-01-17' ),
-				value: 2590.08,
+				value: 2590,
 			},
 			{
 				date: new Date( '2024-01-18' ),
-				value: 2609.35,
+				value: 2609,
 			},
 			{
 				date: new Date( '2024-01-19' ),
-				value: 2648.22,
+				value: 2648,
 			},
 			{
 				date: new Date( '2024-01-20' ),
-				value: 1805.72,
+				value: 1805,
 			},
 			{
 				date: new Date( '2024-01-21' ),
-				value: 2531.49,
+				value: 2531,
 			},
 			{
 				date: new Date( '2024-01-22' ),
-				value: 3605.86,
+				value: 3605,
 			},
 			{
 				date: new Date( '2024-01-23' ),
-				value: 2366.38,
+				value: 2366,
 			},
 			{
 				date: new Date( '2024-01-24' ),
-				value: 2782.46,
+				value: 2782,
 			},
 			{
 				date: new Date( '2024-01-25' ),
-				value: 2885.44,
+				value: 2885,
 			},
 			{
 				date: new Date( '2024-01-26' ),
-				value: 2918.29,
+				value: 2918,
 			},
 			{
 				date: new Date( '2024-01-27' ),
-				value: 2518.05,
+				value: 2518,
 			},
 			{
 				date: new Date( '2024-01-28' ),
-				value: 2378.37,
+				value: 2378,
 			},
 			{
 				date: new Date( '2024-01-29' ),
-				value: 2714.76,
+				value: 2714,
 			},
 			{
 				date: new Date( '2024-01-30' ),
-				value: 3279.33,
+				value: 3279,
 			},
 		],
 	},
