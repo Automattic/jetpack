f3025fc91cb50741585aa79b7fde899637726540
Matias Benedetto <matias.benedetto@gmail.com>
2025-01-17 17:10:05 -0300

    set priority to 10

--- projects/plugins/jetpack/modules/google-fonts/current/class-jetpack-google-font-face.php
+++ projects/plugins/jetpack/modules/google-fonts/current/class-jetpack-google-font-face.php
@@ -27,7 +27,7 @@ class Jetpack_Google_Font_Face {
 		add_action( 'current_screen', array( $this, 'current_screen' ), 10 );
 
 		// Collect and print fonts in use
-		add_action( 'wp_head', array( $this, 'print_font_faces' ), 20 );
+		add_action( 'wp_head', array( $this, 'print_font_faces' ), 10 );
 		add_filter( 'pre_render_block', array( $this, 'collect_block_fonts' ), 10, 2 );
 	}
 
