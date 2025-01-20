a80c024442d3620045afb7e33df70901886b9a6d
dkmyta <dkmyta10@gmail.com>
2025-01-20 15:39:40 -0800

    Revert tests dir changes in favour of a dedicated task

--- projects/packages/account-protection/tests/action-test-coverage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -veo pipefail
-
-EXIT=0
-php -dpcov.directory=. ./vendor/bin/phpunit --coverage-php "$COVERAGE_DIR/integration/php.cov" --configuration tests/php/integration/phpunit.xml.dist || EXIT=1
-php -dpcov.directory=. ./vendor/bin/phpunit --coverage-php "$COVERAGE_DIR/unit/php.cov" --configuration tests/php/unit/phpunit.xml.dist || EXIT=1
-
-exit $EXIT
