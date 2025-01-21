11a62d022fa0dc6cebc57596cc1a1b12cb4831eb
Gergely Juhasz <gergely.juhasz@automattic.com>
2025-01-21 09:49:21 +0100

    Fix TS errors

--- projects/js-packages/publicize-components/src/social-store/selectors/pricing-page.ts
+++ projects/js-packages/publicize-components/src/social-store/selectors/pricing-page.ts
@@ -7,10 +7,9 @@ import { SHOW_PRICING_PAGE_KEY } from '../constants';
  * Returns the UTM state.
  */
 export const getShouldShowPricingPage = createRegistrySelector( select => () => {
-	// @ts-expect-error TS2339 - https://github.com/WordPress/gutenberg/issues/67847
 	const { getSite } = select( coreStore );
 
-	const settings = getSite( undefined, { _fields: SHOW_PRICING_PAGE_KEY } ) as boolean;
+	const settings = getSite( undefined, { _fields: SHOW_PRICING_PAGE_KEY } );
 
 	// If the settings are not available in the store yet, use the default settings.
 	return (
--- projects/js-packages/publicize-components/src/social-store/selectors/social-notes.ts
+++ projects/js-packages/publicize-components/src/social-store/selectors/social-notes.ts
@@ -2,19 +2,16 @@ import { store as coreStore } from '@wordpress/core-data';
 import { createRegistrySelector } from '@wordpress/data';
 import { getSocialScriptData } from '../../utils';
 import { SOCIAL_NOTES_CONFIG_KEY, SOCIAL_NOTES_ENABLED_KEY } from '../constants';
-import { SocialNotesConfig } from '../types';
 
 /**
  * Returns if Social Notes are enabled for the current site.
  */
 export const getSocialNotesEnabled = createRegistrySelector( select => () => {
-	// @ts-expect-error TS2339 - https://github.com/WordPress/gutenberg/issues/67847
 	const { getSite } = select( coreStore );
 
 	const settings = getSite( undefined, {
 		_fields: SOCIAL_NOTES_ENABLED_KEY,
-	} ) as boolean;
-
+	} );
 	// If the settings are not available in the store yet, use the default settings.
 	return (
 		settings?.[ SOCIAL_NOTES_ENABLED_KEY ] ??
@@ -26,12 +23,11 @@ export const getSocialNotesEnabled = createRegistrySelector( select => () => {
  * Returns the Social Notes Config for the current site.
  */
 export const getSocialNotesConfig = createRegistrySelector( select => () => {
-	// @ts-expect-error TS2339 - https://github.com/WordPress/gutenberg/issues/67847
 	const { getSite } = select( coreStore );
 
 	const settings = getSite( undefined, {
 		_fields: SOCIAL_NOTES_CONFIG_KEY,
-	} ) as SocialNotesConfig;
+	} );
 
 	// If the settings are not available in the store yet, use the default settings.
 	return (
