946bbaa5249cf9db28d8ca23cbc355a14d89f50d
DustyReagan <dusty@dustyreagan.com>
2025-01-21 10:18:42 -0600

    Remove test code

--- projects/plugins/jetpack/json-endpoints/class.wpcom-json-api-list-comments-endpoint.php
+++ projects/plugins/jetpack/json-endpoints/class.wpcom-json-api-list-comments-endpoint.php
@@ -200,7 +200,6 @@ class WPCOM_JSON_API_List_Comments_Endpoint extends WPCOM_JSON_API_Comment_Endpo
 	 * @param int    $object_id - the object ID.
 	 */
 	public function callback( $path = '', $blog_id = 0, $object_id = 0 ) {
-		return array( 'test' => 'test' );
 		$blog_id = $this->api->switch_to_blog_and_validate_user( $this->api->get_blog_id( $blog_id ) );
 		if ( is_wp_error( $blog_id ) ) {
 			return $blog_id;
