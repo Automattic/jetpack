d059c8006d5450623081f2d7e774bcd0e0d28811
arthur <arthur.chu@automattic.com>
2025-01-21 13:49:41 +0800

    Introduce @automattic/jetpack-external-media

---
+++ projects/packages/external-media/src/class-external-media.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Package description here
+ *
+ * @package automattic/jetpack-external-media
+ */
+
+namespace Automattic\Jetpack;
+
+/**
+ * Class description.
+ */
+class External_Media {
+
+	const PACKAGE_VERSION = '0.1.0-alpha';
+}
