b06e784feb67868f022c7e6ba075db5c5e46e7b8
Griffith Chen <griffith.chen@automattic.com>
2025-01-23 15:03:35 +0800

    Reorder checks

--- projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
+++ projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
@@ -265,9 +265,9 @@ class Jetpack_Mu_Wpcom {
 		$fse_plugin                 = 'full-site-editing/full-site-editing-plugin.php';
 		$fse_plugin_path            = WP_PLUGIN_DIR . '/' . $fse_plugin;
 		$invalid_fse_version_active =
-			is_plugin_active( $fse_plugin ) &&
 			file_exists( $fse_plugin_path ) &&
 			is_file( $fse_plugin_path ) &&
+			is_plugin_active( $fse_plugin ) &&
 			version_compare( get_plugin_data( $fse_plugin_path, false )['Version'], '3.56084', '<' );
 
 		if ( $invalid_fse_version_active ) {
