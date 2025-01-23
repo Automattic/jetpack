d6a40cbd927acf1ba7edbfafc7dfbcff4c450b41
arthur <arthur.chu@automattic.com>
2025-01-23 10:18:35 +0800

    Load external-media assets

--- projects/packages/external-media/src/class-external-media.php
+++ projects/packages/external-media/src/class-external-media.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Package description here
+ * Register the external media to both WP Admin and Editor.
  *
  * @package automattic/jetpack-external-media
  */
@@ -8,9 +8,35 @@
 namespace Automattic\Jetpack;
 
 /**
- * Class description.
+ * Class External_Media
  */
 class External_Media {
-
 	const PACKAGE_VERSION = '0.1.0-alpha';
+	const BASE_DIR        = __DIR__ . '/';
+	const BASE_FILE       = __FILE__;
+
+	/**
+	 * Add hooks and filters.
+	 */
+	public static function init() {
+		add_action( 'enqueue_block_editor_assets', array( __CLASS__, 'enqueue_block_editor_assets' ) );
+	}
+
+	/**
+	 * Enqueue block editor assets.
+	 */
+	public static function enqueue_block_editor_assets() {
+		$assets_base_path = 'build/';
+		$asset_name       = 'jetpack-external-media-editor';
+
+		Assets::register_script(
+			$asset_name,
+			$assets_base_path . "$asset_name/$asset_name.js",
+			__FILE__,
+			array(
+				'enqueue'  => true,
+				'css_path' => $assets_base_path . "$asset_name/$asset_name" . is_rtl() ? '.rtl.css' : '.css',
+			)
+		);
+	}
 }
