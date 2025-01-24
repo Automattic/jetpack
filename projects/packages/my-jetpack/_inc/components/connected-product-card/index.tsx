479e2d597b203605bb2a018da3d9a1ae02fd6029
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2025-01-24 12:31:49 -0700

    First commit

--- projects/packages/my-jetpack/_inc/components/connected-product-card/index.tsx
+++ projects/packages/my-jetpack/_inc/components/connected-product-card/index.tsx
@@ -2,21 +2,15 @@
  * External dependencies
  */
 import { Text } from '@automattic/jetpack-components';
-import { __ } from '@wordpress/i18n';
-import { useCallback, useEffect } from 'react';
+import { useEffect } from 'react';
 /**
  * Internal dependencies
  */
-import { MyJetpackRoutes, PRODUCT_STATUSES } from '../../constants';
-import useActivatePlugins from '../../data/products/use-activate-plugins';
-import useInstallPlugins from '../../data/products/use-install-plugins';
 import useProduct from '../../data/products/use-product';
-import useAnalytics from '../../hooks/use-analytics';
 import useMyJetpackConnection from '../../hooks/use-my-jetpack-connection';
-import useMyJetpackNavigate from '../../hooks/use-my-jetpack-navigate';
 import preventWidows from '../../utils/prevent-widows';
 import ProductCard from '../product-card';
-import type { AdditionalAction, SecondaryAction } from '../product-card/types';
+import type { AdditionalAction, SecondaryAction } from '../action-button/types';
 import type { FC, ReactNode } from 'react';
 
 interface ConnectedProductCardProps {
@@ -51,41 +45,9 @@ const ConnectedProductCard: FC< ConnectedProductCardProps > = ( {
 	onMouseLeave,
 	customLoadTracks,
 } ) => {
-	const { isRegistered, isUserConnected } = useMyJetpackConnection();
-	const { recordEvent } = useAnalytics();
-
-	const { install: installStandalonePlugin, isPending: isInstalling } = useInstallPlugins( [
-		slug,
-	] );
-	const { activate, isPending: isActivating } = useActivatePlugins( [ slug ] );
+	const { isRegistered } = useMyJetpackConnection();
 	const { detail, refetch, isLoading: isProductDataLoading } = useProduct( slug );
-	const {
-		name,
-		description: defaultDescription,
-		requiresUserConnection,
-		status,
-		manageUrl,
-	} = detail;
-
-	const navigateToConnectionPage = useMyJetpackNavigate( MyJetpackRoutes.ConnectionSkipPricing );
-
-	/*
-	 * Redirect only if connected
-	 */
-	const handleActivate = useCallback( () => {
-		if ( ( ! isRegistered || ! isUserConnected ) && requiresUserConnection ) {
-			navigateToConnectionPage();
-			return;
-		}
-
-		activate();
-	}, [
-		activate,
-		isRegistered,
-		isUserConnected,
-		requiresUserConnection,
-		navigateToConnectionPage,
-	] );
+	const { name, description: defaultDescription, status, manageUrl } = detail;
 
 	const DefaultDescription = () => {
 		// Replace the last space with a non-breaking space to prevent widows
@@ -104,23 +66,6 @@ const ConnectedProductCard: FC< ConnectedProductCardProps > = ( {
 		}
 	}, [ isRegistered, status, refetch ] );
 
-	/**
-	 * Calls the passed function onManage after firing Tracks event
-	 */
-	const manageHandler = useCallback( () => {
-		recordEvent( 'jetpack_myjetpack_product_card_manage_click', {
-			product: slug,
-		} );
-	}, [ slug, recordEvent ] );
-
-	if ( ! secondaryAction && status === PRODUCT_STATUSES.CAN_UPGRADE ) {
-		secondaryAction = {
-			href: manageUrl,
-			label: __( 'View', 'jetpack-my-jetpack' ),
-			onClick: manageHandler,
-		};
-	}
-
 	return (
 		<ProductCard
 			name={ name }
@@ -128,19 +73,17 @@ const ConnectedProductCard: FC< ConnectedProductCardProps > = ( {
 			status={ status }
 			admin={ admin }
 			recommendation={ recommendation }
-			isFetching={ isActivating || isInstalling || isProductDataLoading }
+			isFetching={ isProductDataLoading }
 			isDataLoading={ isDataLoading }
-			isInstallingStandalone={ isInstalling }
 			additionalActions={ additionalActions }
 			primaryActionOverride={ primaryActionOverride }
 			secondaryAction={ secondaryAction }
 			slug={ slug }
-			onActivate={ handleActivate }
-			onInstallStandalone={ installStandalonePlugin }
 			upgradeInInterstitial={ upgradeInInterstitial }
 			onMouseEnter={ onMouseEnter }
 			onMouseLeave={ onMouseLeave }
 			customLoadTracks={ customLoadTracks }
+			manageUrl={ manageUrl }
 		>
 			{ children }
 		</ProductCard>
