fba2928e9b791ba48dfdcbf7aa843f35f1d40c12
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-01-27 17:48:18 +0530

    Update required social version following a release

--- projects/packages/publicize/src/class-social-admin-page.php
+++ projects/packages/publicize/src/class-social-admin-page.php
@@ -60,8 +60,7 @@ class Social_Admin_Page {
 			$plugin_version = $plugin_data['Version'];
 
 			// If it's the old social version, remove the submenu page.
-			// TODO Update the version and operator before next Social release.
-			if ( version_compare( $plugin_version, '6.0.0', '<' ) ) {
+			if ( version_compare( $plugin_version, '6.1.0', '<' ) ) {
 				/**
 				 * `add_submenu_page` allows multiple submenus with the same slug,
 				 * but `remove_submenu_page` only removes the first one it finds with the given slug.
