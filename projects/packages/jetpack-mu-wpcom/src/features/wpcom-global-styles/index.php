a2afd4b0d00724232cd1b775f6ab487c7a323166
Richard Ortiz <rcrd.ortiz@gmail.com>
2025-01-28 09:57:51 +0100

    Updated experiment key

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-global-styles/index.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-global-styles/index.php
@@ -808,7 +808,7 @@ function wpcom_site_has_global_styles_in_personal_plan( $blog_id = 0 ) {
 	}
 
 	// Placeholder experiment key, we need to update this to the new experiment key once it's created.
-	$experiment_assignment              = \ExPlat\assign_given_user( 'calypso_post_onboarding_holdout_160125', $owner );
+	$experiment_assignment              = \ExPlat\assign_given_user( 'calypso_plans_global_styles_personal_20240127', $owner );
 	$has_global_styles_in_personal_plan = 'treatment' === $experiment_assignment;
 	// Cache the experiment assignment to prevent duplicate DB queries in the frontend.
 	wp_cache_set( $cache_key, $has_global_styles_in_personal_plan, 'a8c_experiments', MONTH_IN_SECONDS );
