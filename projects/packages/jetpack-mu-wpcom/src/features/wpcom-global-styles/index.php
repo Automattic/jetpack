163d924289b3bcbb67e615607189479bc815953e
Richard Ortiz <rcrd.ortiz@gmail.com>
2025-01-28 17:58:25 +0100

    Removed comment

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-global-styles/index.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-global-styles/index.php
@@ -807,7 +807,6 @@ function wpcom_site_has_global_styles_in_personal_plan( $blog_id = 0 ) {
 		return false;
 	}
 
-	// Placeholder experiment key, we need to update this to the new experiment key once it's created.
 	$experiment_assignment              = \ExPlat\assign_given_user( 'calypso_plans_global_styles_personal_20240127', $owner );
 	$has_global_styles_in_personal_plan = null !== $experiment_assignment;
 	// Cache the experiment assignment to prevent duplicate DB queries in the frontend.
