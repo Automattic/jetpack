4339dc41ae0a787cb1f1c27b46ab1cc2d3c2a3a2
Bryan Elliott <bryan@elliottprogrammer.com>
2025-01-28 10:10:51 -0500

    Remove use of 'wp_timezone()'.

--- projects/packages/jitm/src/class-jitm.php
+++ projects/packages/jitm/src/class-jitm.php
@@ -255,11 +255,8 @@ class JITM {
 	 * @param bool   $full_jp_logo_exists Is there a big JP logo already displayed on this screen.
 	 */
 	public function generate_icon( $content_icon, $full_jp_logo_exists ) {
-		$timezone = wp_timezone();
-		$date_now = new \DateTime();
-		$date_now->setTimezone( $timezone );
+		$date_now   = new \DateTime();
 		$feb_4_date = new \DateTime( '02-04-2025' );
-		$feb_4_date->setTimezone( $timezone );
 		// Don't show the new Woo svg logo until after Feb 4th, 2025
 		$show_new_logo = $date_now >= $feb_4_date;
 
