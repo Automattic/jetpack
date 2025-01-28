9e4345171bf20ca9897c8734f406f5ed060cba42
Adnan Haque <adnan007.id@gmail.com>
2025-01-28 14:13:20 -0500

    Add a story for score popout component

--- projects/plugins/boost/app/assets/src/js/features/speed-score/pop-out/pop-out.tsx
+++ projects/plugins/boost/app/assets/src/js/features/speed-score/pop-out/pop-out.tsx
@@ -54,6 +54,66 @@ const slowerMessage: ScoreChangeMessage = {
 	ctaLink: getRedirectUrl( 'boost-improve-site-speed-score' ),
 };
 
+type VanillaPopOutProps = {
+	message: ScoreChangeMessage;
+	onClose: () => void;
+	onDismiss: () => void;
+	isVisible: boolean;
+};
+
+/**
+ * The basic pop out excluding all external dependencies.
+ *
+ * @param {VanillaPopOutProps} props
+ * @return {React.ReactNode} Vanilla PopOut component.
+ */
+export const VanillaPopOut = ( { message, onClose, onDismiss, isVisible }: VanillaPopOutProps ) => {
+	const animationStyles = useSpring( {
+		from: {
+			right: '-100%',
+		},
+		to: {
+			right: isVisible ? '0%' : '-100%',
+		},
+	} );
+
+	return (
+		<div id="parent" className={ styles.wrapper }>
+			<animated.div
+				className={ styles.card }
+				style={ {
+					...animationStyles,
+				} }
+			>
+				<CloseButton onClick={ onClose } />
+
+				<h3 className={ styles.headline }>{ message.title }</h3>
+
+				<>{ message.body }</>
+
+				<Button
+					variant="primary"
+					href={ message?.ctaLink }
+					target="_blank"
+					rel="noreferrer"
+					onClick={ onDismiss }
+				>
+					{ message.cta }
+				</Button>
+
+				<Button
+					variant="link"
+					size="small"
+					className={ styles[ 'dismiss-button' ] }
+					onClick={ onDismiss }
+				>
+					{ __( 'Do not show me again', 'jetpack-boost' ) }
+				</Button>
+			</animated.div>
+		</div>
+	);
+};
+
 function PopOut( { scoreChange }: Props ) {
 	/*
 	 * Determine if the score has changed enough to show the alert.
@@ -85,49 +145,13 @@ function PopOut( { scoreChange }: Props ) {
 		}
 	}, [ hasScoreChanged, scoreChange, isDismissed, isClosed ] );
 
-	const animationStyles = useSpring( {
-		from: {
-			right: '-100%',
-		},
-		to: {
-			right: hasScoreChanged && ! isDismissed && ! isClosed ? '0%' : '-100%',
-		},
-	} );
-
 	return (
-		<div id="parent" className={ styles.wrapper }>
-			<animated.div
-				className={ styles.card }
-				style={ {
-					...animationStyles,
-				} }
-			>
-				<CloseButton onClick={ hideAlert } />
-
-				<h3 className={ styles.headline }>{ message.title }</h3>
-
-				<>{ message.body }</>
-
-				<a
-					className="jb-button--primary"
-					href={ message?.ctaLink }
-					target="_blank"
-					rel="noreferrer"
-					onClick={ dismissAlert }
-				>
-					{ message.cta }
-				</a>
-
-				<Button
-					variant="link"
-					size="small"
-					className={ styles[ 'dismiss-button' ] }
-					onClick={ dismissAlert }
-				>
-					{ __( 'Do not show me again', 'jetpack-boost' ) }
-				</Button>
-			</animated.div>
-		</div>
+		<VanillaPopOut
+			message={ message }
+			onClose={ hideAlert }
+			onDismiss={ dismissAlert }
+			isVisible={ hasScoreChanged && ! isDismissed && ! isClosed }
+		/>
 	);
 }
 
