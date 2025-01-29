1917198e12fdd64e934f4128175192f91fb63777
Ella <4710635+ellatrix@users.noreply.github.com>
2025-01-29 21:49:53 +0100

    Revert "MU WPCOM: dashboard: add launchpad (#41138)"
    This reverts commit 923e295263d2acf30669de3006663b46209135c9.

--- projects/packages/jetpack-mu-wpcom/webpack.config.js
+++ projects/packages/jetpack-mu-wpcom/webpack.config.js
@@ -79,10 +79,6 @@ module.exports = [
 				/** Replace the classnames used by @automattic/newspack-blocks with clsx because we changed to use clsx */
 				classnames: findPackage( 'clsx' ),
 			},
-			fallback: {
-				...jetpackWebpackConfig.resolve.fallback,
-				events: require.resolve( 'events/' ),
-			},
 		},
 		node: false,
 		plugins: [
@@ -103,9 +99,9 @@ module.exports = [
 					exclude: /node_modules\//,
 				} ),
 
-				// Transpile @automattic/* in node_modules too.
+				// Transpile @automattic/jetpack-* in node_modules too.
 				jetpackWebpackConfig.TranspileRule( {
-					includeNodeModules: [ '@automattic/' ],
+					includeNodeModules: [ '@automattic/jetpack-' ],
 				} ),
 
 				// Handle CSS.
