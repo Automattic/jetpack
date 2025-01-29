288ceb3ae605eb9868cbe71d1668a716f645159b
Grzegorz Chudzinski-Pawlowski <112354940+grzegorz-cp@users.noreply.github.com>
2025-01-29 16:40:53 +1100

    Cleanup

--- projects/packages/my-jetpack/_inc/components/product-interstitial-modal/product-interstitial-plugin.tsx
+++ projects/packages/my-jetpack/_inc/components/product-interstitial-modal/product-interstitial-plugin.tsx
@@ -50,7 +50,11 @@ const ProductInterstitialPlugin: FC< ProductInterstitialPluginProps > = ( {
 	} else if ( productTerm === 'year' ) {
 		priceDescription = __( '/month, paid yearly', 'jetpack-my-jetpack' );
 	} else {
-		priceDescription = __( '/month', 'jetpack-my-jetpack' );
+		priceDescription = __(
+			'/month',
+			'jetpack-my-jetpack',
+			/* dummy arg to avoid bad minification */ 0
+		);
 	}
 
 	const productPrice = introductoryOffer?.reason ? price : discountPrice;
