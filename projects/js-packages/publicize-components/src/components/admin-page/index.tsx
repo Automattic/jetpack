c88feecbe69c6310b4a1ebc8caa514c86c7cec8f
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-01-29 19:09:17 +0530

    Hide UTM toggle for now on WPCOM

--- projects/js-packages/publicize-components/src/components/admin-page/index.tsx
+++ projects/js-packages/publicize-components/src/components/admin-page/index.tsx
@@ -80,7 +80,7 @@ export const SocialAdminPage = () => {
 					</AdminSectionHero>
 					<AdminSection>
 						<SocialModuleToggle />
-						{ isModuleEnabled && <UtmToggle /> }
+						{ isModuleEnabled && ! is_wpcom && <UtmToggle /> }
 						{
 							// Only show the Social Notes toggle if Social plugin is active
 							socialPluginVersion && isModuleEnabled && (
