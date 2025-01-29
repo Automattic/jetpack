887ba8081570775837d73fa5f85dbffb5e568a15
Kelly Dwan <ryelle@users.noreply.github.com>
2025-01-29 13:56:31 -0600

    Use the page load hook to enqueue script

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-themes/wpcom-themes.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-themes/wpcom-themes.php
@@ -94,14 +94,6 @@ add_action( 'load-theme-install.php', 'wpcom_auto_open_upload_theme' );
  * Renders a theme install page.
  */
 function render_theme_install() {
-	wp_enqueue_script(
-		'wpcom-theme-install',
-		plugin_dir_url( __FILE__ ) . '/js/wpcom-theme-install.js',
-		array(),
-		filemtime( __DIR__ . '/js/wpcom-theme-install.js' ),
-		true
-	);
-
 	require_once __DIR__ . '/theme-install.php';
 }
 
@@ -120,15 +112,22 @@ function wpcom_add_theme_install_menu() {
 }
 add_action( 'admin_menu', 'wpcom_add_theme_install_menu' );
 
-
+/**
+ * Enqueue the theme install script on the custom page.
+ */
 function wpcom_themes_enqueue_theme_install_script() {
+	wp_enqueue_script(
+		'wpcom-theme-install',
+		plugin_dir_url( __FILE__ ) . '/js/wpcom-theme-install.js',
+		array(),
+		filemtime( __DIR__ . '/js/wpcom-theme-install.js' ),
+		true
+	);
 }
 
 add_action(
-	'load-wpcom-install-theme.php',
+	'load-appearance_page_wpcom-install-theme',
 	function () {
-		die('greenz');
 		add_action( 'admin_enqueue_scripts', 'wpcom_themes_enqueue_theme_install_script' );
-
 	}
 );
