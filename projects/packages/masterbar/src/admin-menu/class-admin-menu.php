c0e3adadf3c34fb7212ba459936105abca1b5a47
Dave Smith <getdavemail@gmail.com>
2025-01-29 00:07:02 +0000

    Update projects/packages/masterbar/src/admin-menu/class-admin-menu.php

--- projects/packages/masterbar/src/admin-menu/class-admin-menu.php
+++ projects/packages/masterbar/src/admin-menu/class-admin-menu.php
@@ -22,7 +22,6 @@ class Admin_Menu extends Base_Admin_Menu {
 	 * Create the desired menu output.
 	 */
 	public function reregister_menu_items() {
-
 		// Remove separators.
 		remove_menu_page( 'separator1' );
 		$this->add_stats_menu();
