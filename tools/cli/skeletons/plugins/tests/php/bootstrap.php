071523d912e09e300aae536ef9691f77c060187b
Brandon Kraft <public@brandonkraft.com>
2025-01-29 13:38:07 -0600

    Update cli generate command to use test package instead of wordbless

--- tools/cli/skeletons/plugins/tests/php/bootstrap.php
+++ tools/cli/skeletons/plugins/tests/php/bootstrap.php
@@ -9,3 +9,10 @@
  * Include the composer autoloader.
  */
 require_once __DIR__ . '/../../vendor/autoload.php';
+
+// The `@automattic/jetpack-test-environment` package is required for this.
+// This, and @automattic/jetpack-test-environment in composer.json, can be removed if WordPress is not needed for the unit tests.
+if ( class_exists( '\Automattic\Jetpack\Test_Environment' ) ) {
+	// Initialize WordPress test environment
+	\Automattic\Jetpack\Test_Environment::init();
+}
