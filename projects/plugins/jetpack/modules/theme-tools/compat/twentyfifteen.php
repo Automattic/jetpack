1c375e679e150bc7f8e146ab5bd4ee159c8bb768
Karen Attfield <karenlattfield@gmail.com>
2025-01-29 16:21:19 +0000

    Removing function_exists check for TwentyFifteen init function as it isn't duplicated

--- projects/plugins/jetpack/modules/theme-tools/compat/twentyfifteen.php
+++ projects/plugins/jetpack/modules/theme-tools/compat/twentyfifteen.php
@@ -22,24 +22,22 @@ function twentyfifteen_jetpack_setup() {
 }
 add_action( 'after_setup_theme', 'twentyfifteen_jetpack_setup' );
 
-if ( ! function_exists( 'twentyfifteen_init_jetpack' ) ) {
+/**
+ * Enqueue Jetpack compat styles for Twenty Fifteen.
+ */
+function twentyfifteen_init_jetpack() {
 	/**
-	 * Enqueue Jetpack compat styles for Twenty Fifteen.
+	 * Add our compat CSS file for custom widget stylings and such.
+	 * Set the version equal to filemtime for development builds, and the JETPACK__VERSION for production
+	 * or skip it entirely for wpcom.
 	 */
-	function twentyfifteen_init_jetpack() {
-		/**
-		 * Add our compat CSS file for custom widget stylings and such.
-		 * Set the version equal to filemtime for development builds, and the JETPACK__VERSION for production
-		 * or skip it entirely for wpcom.
-		 */
-		if ( ! is_admin() ) {
-			$version = false;
-			if ( method_exists( 'Jetpack', 'is_development_version' ) ) {
-				$version = Jetpack::is_development_version() ? filemtime( plugin_dir_path( __FILE__ ) . 'twentyfifteen.css' ) : JETPACK__VERSION;
-			}
-			wp_enqueue_style( 'twentyfifteen-jetpack', plugins_url( 'twentyfifteen.css', __FILE__ ), array(), $version );
-			wp_style_add_data( 'twentyfifteen-jetpack', 'rtl', 'replace' );
+	if ( ! is_admin() ) {
+		$version = false;
+		if ( method_exists( 'Jetpack', 'is_development_version' ) ) {
+			$version = Jetpack::is_development_version() ? filemtime( plugin_dir_path( __FILE__ ) . 'twentyfifteen.css' ) : JETPACK__VERSION;
 		}
+		wp_enqueue_style( 'twentyfifteen-jetpack', plugins_url( 'twentyfifteen.css', __FILE__ ), array(), $version );
+		wp_style_add_data( 'twentyfifteen-jetpack', 'rtl', 'replace' );
 	}
-	add_action( 'init', 'twentyfifteen_init_jetpack' );
 }
+add_action( 'init', 'twentyfifteen_init_jetpack' );
