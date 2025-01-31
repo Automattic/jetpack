4315b1d2eff064d184a9f81016eb4d646774032c
Nate Weller <hello@nateweller.com>
2025-01-30 20:09:46 -0700

    Remove changes to badge components, moved to #41450

--- projects/js-packages/components/components/badge/index.tsx
+++ projects/js-packages/components/components/badge/index.tsx
@@ -5,7 +5,7 @@ import styles from './style.module.scss';
 type BadgeProps = {
 	children?: React.ReactNode;
 	className?: string;
-	variant?: 'info' | 'success' | 'warning' | 'danger';
+	variant?: 'success' | 'warning' | 'danger';
 	[ key: string ]: unknown;
 };
 
