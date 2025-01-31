6906f662bcc91cd1cefcbc68e42afb9f684ef0d2
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-01-31 09:55:41 -0700

    Inform Phan of type rather than suppressing error

--- projects/plugins/jetpack/tests/php/sync/test_class.jetpack-sync-meta.php
+++ projects/plugins/jetpack/tests/php/sync/test_class.jetpack-sync-meta.php
@@ -343,7 +343,7 @@ class WP_Test_Jetpack_Sync_Meta extends WP_Test_Jetpack_Sync_Base {
 		update_post_meta( $this->post_id, $this->whitelisted_post_meta, $meta_test_value );
 
 		$module = Modules::get_module( 'meta' );
-		// @phan-suppress-next-line PhanParamTooMany -- Phan looks at the Module abstract class instead of the Meta class that extends it.
+		'@phan-var \Automattic\Jetpack\Sync\Modules\Meta $module';
 		$metas = $module->get_object_by_id( 'post', $this->post_id, $this->whitelisted_post_meta );
 		$this->assertSame( '', $metas[0]['meta_value'] );
 	}
@@ -356,7 +356,7 @@ class WP_Test_Jetpack_Sync_Meta extends WP_Test_Jetpack_Sync_Base {
 		update_post_meta( $this->post_id, $this->whitelisted_post_meta, $meta_test_value );
 
 		$module = Modules::get_module( 'meta' );
-		// @phan-suppress-next-line PhanParamTooMany -- Phan looks at the Module abstract class instead of the Meta class that extends it.
+		'@phan-var \Automattic\Jetpack\Sync\Modules\Meta $module';
 		$metas = $module->get_object_by_id( 'post', $this->post_id, $this->whitelisted_post_meta );
 		$this->assertEquals( $meta_test_value, $metas[0]['meta_value'] );
 	}
