7fe0f875615e4bd9d5b9f917ccad172f2cbb98c0
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-01-31 13:19:46 -0700

    Update newspack-blocks to 4.5.7

--- projects/packages/jetpack-mu-wpcom/src/features/newspack-blocks/synced-newspack-blocks/types/index.d.ts
+++ projects/packages/jetpack-mu-wpcom/src/features/newspack-blocks/synced-newspack-blocks/types/index.d.ts
@@ -11,6 +11,7 @@ declare global {
 			post_subtitle: boolean;
 			can_use_name_your_price: boolean;
 			tier_amounts_template: string;
+			currency: string;
 		};
 		grecaptcha: any;
 		newspackReaderActivation: {
