8966d745811c3b57f6c0b8e93063b6a15941fe95
Jasper Kang <jasper.kang@automattic.com>
2025-02-04 11:32:49 +1300

    Apply suggestions from code review

--- projects/js-packages/charts/src/components/line-chart/line-chart.tsx
+++ projects/js-packages/charts/src/components/line-chart/line-chart.tsx
@@ -1,4 +1,4 @@
-import { curveNatural, curveLinear } from '@visx/curve';
+import { curveCatmullRom, curveLinear } from '@visx/curve';
 import { LinearGradient } from '@visx/gradient';
 import {
 	XYChart,
@@ -193,7 +193,7 @@ const LineChart: FC< LineChartProps > = ( {
 								{ ...accessors }
 								fill={ withGradientFill ? `url(#area-gradient-${ index + 1 })` : undefined }
 								renderLine={ true }
-								curve={ smoothing ? curveNatural : curveLinear }
+								curve={ smoothing ? curveCatmullRom : curveLinear }
 							/>
 						</g>
 					);
