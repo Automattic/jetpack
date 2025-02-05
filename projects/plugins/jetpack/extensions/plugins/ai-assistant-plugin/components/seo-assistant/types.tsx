e6e133a9851d316d798df3a92baf06b876e3003a
Christian Gastrell <cgastrell@gmail.com>
2025-02-05 19:37:06 -0300

    update onStart signature and add includeResults on steps

--- projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/seo-assistant/types.tsx
+++ projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/seo-assistant/types.tsx
@@ -11,13 +11,21 @@ export interface Message {
 
 export type OptionMessage = Pick< Message, 'id' | 'content' >;
 
+export interface Results {
+	[ key: string ]: {
+		value: string;
+		type: string;
+		label: string;
+	};
+}
+
 export interface Step {
 	id: string;
 	title: string;
 	label?: string;
 	messages: Message[];
 	type: StepType;
-	onStart?: ( options?: { fromSkip: boolean; stepValue: string } ) => void;
+	onStart?: ( options?: { fromSkip: boolean; stepValue: string; results: Results } ) => void;
 	onSubmit?: () => Promise< string >;
 	onSkip?: () => void;
 	value?: string;
@@ -27,6 +35,7 @@ export interface Step {
 	setCompleted?: React.Dispatch< React.SetStateAction< boolean > >;
 	completed?: boolean;
 	autoAdvance?: number;
+	includeInResults?: boolean;
 
 	// Input step properties
 	placeholder?: string;
