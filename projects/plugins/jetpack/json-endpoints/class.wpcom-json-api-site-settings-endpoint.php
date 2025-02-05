1e24f66433aeb643961fa361269e62c5970d9f64
Eoin Gallagher <eoin@automattic.com>
2025-02-05 11:26:25 +0000

    fix misnamed option

--- projects/plugins/jetpack/json-endpoints/class.wpcom-json-api-site-settings-endpoint.php
+++ projects/plugins/jetpack/json-endpoints/class.wpcom-json-api-site-settings-endpoint.php
@@ -468,7 +468,7 @@ class WPCOM_JSON_API_Site_Settings_Endpoint extends WPCOM_JSON_API_Endpoint {
 						'jetpack_post_date_in_email'       => (bool) get_option( 'jetpack_post_date_in_email', true ),
 						'wpcom_newsletter_categories'      => $newsletter_category_ids,
 						'wpcom_newsletter_categories_enabled' => (bool) get_option( 'wpcom_newsletter_categories_enabled' ),
-						'wpcom_newsletter_categories_modal_hidden_enabled' => (bool) get_option( 'wpcom_newsletter_categories_modal_hidden_enabled', false ),
+						'wpcom_newsletter_categories_modal_hidden' => (bool) get_option( 'wpcom_newsletter_categories_modal_hidden', false ),
 						'sm_enabled'                       => (bool) get_option( 'sm_enabled' ),
 						'jetpack_subscribe_overlay_enabled' => (bool) get_option( 'jetpack_subscribe_overlay_enabled' ),
 						'jetpack_subscribe_floating_button_enabled' => (bool) get_option( 'jetpack_subscribe_floating_button_enabled' ),
