abf27519f710754976efd411fcee3db551fb093c
Nate Weller <hello@nateweller.com>
2025-02-05 19:13:18 -0700

    Use .jsx import to fix plugin builds

--- projects/js-packages/components/index.ts
+++ projects/js-packages/components/index.ts
@@ -49,7 +49,7 @@ export { default as ThreatSeverityBadge } from './components/threat-severity-bad
 export { default as ThreatsDataViews } from './components/threats-data-views/index.js';
 export { default as Text, H2, H3, Title } from './components/text/index.js';
 export { default as ToggleControl } from './components/toggle-control/index.js';
-export { default as NumberControl } from './components/number-control/index.js';
+export { default as NumberControl } from './components/number-control/index.jsx';
 export { default as numberFormat } from './components/number-format/index.js';
 export { default as QRCode } from './components/qr-code/index.js';
 export { default as Button } from './components/button/index.js';
