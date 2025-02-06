928e25e4db32099f590a13ecc2d2ed547194d692
Ashar Fuadi <ashar.fuadi@automattic.com>
2025-02-06 11:06:12 +0700

    Stop the event propagation

--- projects/packages/external-media/src/features/admin/external-media-import-button.js
+++ projects/packages/external-media/src/features/admin/external-media-import-button.js
@@ -4,16 +4,11 @@ document.addEventListener( 'DOMContentLoaded', function () {
 	const addNewButton = document.querySelector( 'a.page-title-action' );
 	if ( addNewButton ) {
 		const importButton = document.createElement( 'a' );
-		importButton.className = 'button';
+		importButton.className = 'page-title-action';
 		importButton.role = 'button';
 		importButton.innerHTML = __( 'Import Media', 'jetpack-external-media' );
 		importButton.href = window.JETPACK_EXTERNAL_MEDIA_IMPORT_BUTTON?.href;
-		importButton.style = `
-			position: relative;
-			top: -3px;
-			margin-left: 3px;
-			vertical-align: baseline;
-		`;
+		importButton.onclick = event => event.stopImmediatePropagation();
 
 		addNewButton.parentNode.insertBefore( importButton, addNewButton.nextSibling );
 	}
