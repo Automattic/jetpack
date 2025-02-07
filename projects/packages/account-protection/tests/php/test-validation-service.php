c55e5d238d5e4cf5c05e773f70334678400882cb
dkmyta <dkmyta10@gmail.com>
2025-02-07 13:07:55 -0800

    Fix test

--- projects/packages/account-protection/tests/php/test-validation-service.php
+++ projects/packages/account-protection/tests/php/test-validation-service.php
@@ -197,13 +197,14 @@ class Validation_Service_Test extends BaseTestCase {
 	}
 
 	public function test_returns_false_if_password_was_not_recently_used() {
-		$user_id       = 1;
-		$password_hash = wp_hash_password( 'somepassword' );
+		$user            = new \WP_User();
+		$user->user_pass = wp_hash_password( 'somepassword' );
+		$user->ID        = 1;
 
-		update_user_meta( $user_id, Config::PASSWORD_MANAGER_RECENT_PASSWORD_HASHES_USER_META_KEY, array( $password_hash ) );
+		update_user_meta( $user->ID, Config::PASSWORD_MANAGER_RECENT_PASSWORD_HASHES_USER_META_KEY, array( $user->user_pass ) );
 
 		$validation_service = new Validation_Service( $this->get_connection_manager() );
-		$this->assertFalse( $validation_service->is_recent_password( $user_id, 'anotherpassword' ) );
+		$this->assertFalse( $validation_service->is_recent_password( $user, 'anotherpassword' ) );
 	}
 
 	public function test_returns_true_if_password_matches_user_data() {
