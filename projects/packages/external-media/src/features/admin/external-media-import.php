7b7f26099863cf42e1225121d048100f89b9e5d1
arthur <arthur.chu@automattic.com>
2025-02-07 17:41:20 +0800

    Untangle Calypso Media Page: Enable the feature

--- projects/packages/external-media/src/features/admin/external-media-import.php
+++ projects/packages/external-media/src/features/admin/external-media-import.php
@@ -27,10 +27,6 @@ function is_current_user_connected() {
  * Register the Jetpack external media page to Media > Import.
  */
 function add_jetpack_external_media_import_page() {
-	if ( empty( $_GET['jetpack_external_media_import_page'] ) && empty( $_GET['untangling-media'] ) ) { // phpcs:disable WordPress.Security.NonceVerification.Recommended
-		return;
-	}
-
 	/**
 	 * The feature is enabled only when the current user is connected to WordPress.com.
 	 */
