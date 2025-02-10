f911dbffd19a21837730f7edf865f24370d09074
arthur <arthur.chu@automattic.com>
2025-02-10 11:45:54 +0800

    Remove the feature flag from the Import link

--- projects/packages/external-media/src/features/admin/external-media-import.php
+++ projects/packages/external-media/src/features/admin/external-media-import.php
@@ -71,7 +71,7 @@ function enqueue_jetpack_external_media_import_button() {
 		$asset_name,
 		'JETPACK_EXTERNAL_MEDIA_IMPORT_BUTTON',
 		array(
-			'href' => admin_url( 'upload.php?page=jetpack_external_media_import_page&untangling-media=true' ),
+			'href' => admin_url( 'upload.php?page=jetpack_external_media_import_page' ),
 		)
 	);
 }
