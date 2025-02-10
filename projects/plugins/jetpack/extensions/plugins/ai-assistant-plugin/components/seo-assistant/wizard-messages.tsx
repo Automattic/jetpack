1e8ee54ff59c146a6d0d9815c348a0fadd435a7a
Christian Gastrell <cgastrell@gmail.com>
2025-02-10 14:59:00 -0300

    allow editLastMessage to append content instead of replacing

--- projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/seo-assistant/wizard-messages.tsx
+++ projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/seo-assistant/wizard-messages.tsx
@@ -35,13 +35,27 @@ export const useMessages = () => {
 	};
 
 	/* Edits content of last message */
-	const editLastMessage = ( content: Message[ 'content' ] ) => {
+	const editLastMessage = ( content: Message[ 'content' ], append = false ) => {
 		setMessages( prev => {
 			const prevMessages = [ ...prev ];
 			if ( prevMessages.length > 0 ) {
+				const lastMessageContent = prevMessages[ prevMessages.length - 1 ].content;
+				let newContent = content;
+				if ( append ) {
+					if ( typeof lastMessageContent === 'string' ) {
+						newContent = lastMessageContent + content;
+					} else if ( typeof lastMessageContent === 'object' ) {
+						newContent = (
+							<>
+								{ lastMessageContent }
+								{ content }
+							</>
+						);
+					}
+				}
 				prevMessages[ prevMessages.length - 1 ] = {
 					...prevMessages[ prevMessages.length - 1 ],
-					content,
+					content: newContent,
 				};
 			}
 			return prevMessages;
