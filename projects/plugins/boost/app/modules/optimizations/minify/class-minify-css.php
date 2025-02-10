75306333b916fe5fc3b40f1e7a3a5540bee37106
Donncha O Caoimh <5656673+donnchawp@users.noreply.github.com>
2025-02-10 14:10:11 +0000

    Schedule the daily jobs from the setup, rather than activation
    If the plugin is deactivated with the concatenation modules enabled
    then the activate function isn't called when the plugin is activated
    again.
    The setup function runs on every pageload, but it does an is_admin()
    check to reduce DB queries.

--- projects/plugins/boost/app/modules/optimizations/minify/class-minify-css.php
+++ projects/plugins/boost/app/modules/optimizations/minify/class-minify-css.php
@@ -13,10 +13,13 @@ class Minify_CSS implements Pluggable, Changes_Page_Output, Optimization, Has_Ac
 
 	public static $default_excludes = array( 'admin-bar', 'dashicons', 'elementor-app' );
 
+	/**
+	 * Setup the module. This runs on every page load.
+	 */
 	public function setup() {
 		require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/functions-helpers.php';
 
-		jetpack_boost_minify_init();
+		jetpack_boost_minify_setup();
 
 		if ( jetpack_boost_page_optimize_bail() ) {
 			return;
@@ -50,11 +53,15 @@ class Minify_CSS implements Pluggable, Changes_Page_Output, Optimization, Has_Ac
 		$wp_styles->allow_gzip_compression = true; // @todo - used constant ALLOW_GZIP_COMPRESSION = true if not defined.
 	}
 
+	/**
+	 * This is called only when the module is activated.
+	 */
 	public static function activate() {
-		jetpack_boost_minify_activation();
-		jetpack_boost_404_tester();
 	}
 
+	/**
+	 * This is called only when the module is deactivated.
+	 */
 	public static function deactivate() {
 		jetpack_boost_page_optimize_cleanup_cache( 'css' );
 		jetpack_boost_minify_deactivation();
--- projects/plugins/boost/app/modules/optimizations/minify/class-minify-js.php
+++ projects/plugins/boost/app/modules/optimizations/minify/class-minify-js.php
@@ -13,10 +13,13 @@ class Minify_JS implements Pluggable, Changes_Page_Output, Optimization, Has_Act
 
 	public static $default_excludes = array( 'jquery', 'jquery-core', 'underscore', 'backbone' );
 
+	/**
+	 * Setup the module. This runs on every page load.
+	 */
 	public function setup() {
 		require_once JETPACK_BOOST_DIR_PATH . '/app/lib/minify/functions-helpers.php';
 
-		jetpack_boost_minify_init();
+		jetpack_boost_minify_setup();
 
 		if ( jetpack_boost_page_optimize_bail() ) {
 			return;
@@ -50,11 +53,15 @@ class Minify_JS implements Pluggable, Changes_Page_Output, Optimization, Has_Act
 		$wp_scripts->allow_gzip_compression = true; // @todo - used constant ALLOW_GZIP_COMPRESSION = true if not defined.
 	}
 
+	/**
+	 * This is called only when the module is activated.
+	 */
 	public static function activate() {
-		jetpack_boost_minify_activation();
-		jetpack_boost_404_tester();
 	}
 
+	/**
+	 * This is called only when the module is deactivated.
+	 */
 	public static function deactivate() {
 		jetpack_boost_page_optimize_cleanup_cache( 'js' );
 		jetpack_boost_minify_deactivation();
