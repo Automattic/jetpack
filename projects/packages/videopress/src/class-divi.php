d68cbae1bec5df1afcc3da8173cd70ce636026ed
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-02-11 15:12:30 -0700

    Fix annotation

--- projects/packages/videopress/src/class-divi.php
+++ projects/packages/videopress/src/class-divi.php
@@ -28,7 +28,7 @@ class Divi {
 	/**
 	 * VideoPress Divi Extension object.
 	 *
-	 * @var ?VideoPress_Divi_Extension
+	 * @var ?\Automattic\Jetpack\VideoPress\VideoPress_Divi_Extension
 	 **/
 	private $vidi_extension;
 
