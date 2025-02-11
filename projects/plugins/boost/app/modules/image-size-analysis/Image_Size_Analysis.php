f806adb4262c1df986f2eedb6962d8e3db4e66d1
Liam Sarsfield <43409125+LiamSarsfield@users.noreply.github.com>
2025-02-11 14:52:11 +0000

    Ensure ISA has Has_Data_Sync interface

--- projects/plugins/boost/app/modules/image-size-analysis/Image_Size_Analysis.php
+++ projects/plugins/boost/app/modules/image-size-analysis/Image_Size_Analysis.php
@@ -3,12 +3,13 @@
 namespace Automattic\Jetpack_Boost\Modules\Image_Size_Analysis;
 
 use Automattic\Jetpack\WP_JS_Data_Sync\Data_Sync;
+use Automattic\Jetpack_Boost\Contracts\Has_Data_Sync;
 use Automattic\Jetpack_Boost\Contracts\Is_Always_On;
 use Automattic\Jetpack_Boost\Contracts\Pluggable;
 use Automattic\Jetpack_Boost\Lib\Premium_Features;
 use Automattic\Jetpack_Boost\Modules\Image_Size_Analysis\Data_Sync\Data_Sync_Setup;
 
-class Image_Size_Analysis implements Pluggable, Is_Always_On {
+class Image_Size_Analysis implements Pluggable, Is_Always_On, Has_Data_Sync {
 
 	public function setup() {
 		Image_Size_Analysis_Fixer::setup();
