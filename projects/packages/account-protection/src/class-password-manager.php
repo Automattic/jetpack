7c5733c43717075b619fcc34c49a6858d5b01986
dkmyta <dkmyta10@gmail.com>
2025-02-12 10:49:34 -0800

    Update methods, removes nonce checks, fix tests

--- projects/packages/account-protection/src/class-password-manager.php
+++ projects/packages/account-protection/src/class-password-manager.php
@@ -28,32 +28,6 @@ class Password_Manager {
 	}
 
 	/**
-	 * Verify the nonce for password update.
-	 *
-	 * @param string $key The nonce key.
-	 *
-	 * @return bool True if the nonce is valid, false otherwise.
-	 */
-	private function verify_password_update_nonce( $key ) {
-		if ( ! isset( $_POST['_wpnonce'] ) || ! wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['_wpnonce'] ) ), $key ) ) {
-			return false;
-		}
-
-		return true;
-	}
-
-	/**
-	 * Verify the nonce for profile update.
-	 *
-	 * @param int $user_id The user ID.
-	 *
-	 * @return bool True if the nonce is valid, false otherwise.
-	 */
-	public function verify_profile_update_nonce( $user_id ) {
-		return $this->verify_password_update_nonce( 'update-user_' . $user_id );
-	}
-
-	/**
 	 * Validate the profile update.
 	 *
 	 * @param \WP_Error $errors The error object.
@@ -63,49 +37,30 @@ class Password_Manager {
 	 * @return void
 	 */
 	public function validate_profile_update( \WP_Error $errors, bool $update, \stdClass $user ): void {
-		if ( empty( $_POST['pass1'] ) ) {
+		if ( empty( $user->user_pass ) ) {
 			return;
 		}
 
 		// If bypass is enabled, do not validate the password
+		// phpcs:ignore WordPress.Security.NonceVerification
 		if ( isset( $_POST['pw_weak'] ) && 'on' === $_POST['pw_weak'] ) {
 			return;
 		}
 
-		if ( ( ! $update && ( ! isset( $_POST['_wpnonce_create-user'] ) || ! wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['_wpnonce_create-user'] ) ), 'create-user' ) ) )
-			|| ( $update && ! $this->verify_profile_update_nonce( $user->ID ) ) ) {
-			$errors->add( 'nonce_error', __( '<strong>Error:</strong> Nonce verification failed.', 'jetpack-account-protection' ) );
-			return;
-		}
-
-		$password = sanitize_text_field( wp_unslash( $_POST['pass1'] ) );
-
 		if ( $update ) {
-			$old_user_data = $this->get_old_user_data( $user->ID );
-			if ( $this->validation_service->is_current_password( $old_user_data, $password ) ) {
+			if ( $this->validation_service->is_current_password( $user->ID, $user->user_pass ) ) {
 				$errors->add( 'password_error', __( '<strong>Error:</strong> The password was used recently.', 'jetpack-account-protection' ) );
 				return;
 			}
 		}
 
 		$context = $update ? 'update' : 'create-user';
-		$error   = $this->validation_service->return_first_validation_error( $user, $password, $context );
+		$error   = $this->validation_service->return_first_validation_error( $user, $user->user_pass, $context );
 
 		if ( ! empty( $error ) ) {
 			$errors->add( 'password_error', $error );
 			return;
 		}
-	}
-
-	/**
-	 * Get the old user data.
-	 *
-	 * @param int $user_id The user ID.
-	 *
-	 * @return \WP_User|false The old user data, or false if the user does not exist.
-	 */
-	public function get_old_user_data( $user_id ) {
-		return get_userdata( $user_id );
 	}
 
 	/**
@@ -123,19 +78,20 @@ class Password_Manager {
 			return;
 		}
 
-		// phpcs:disable WordPress.Security.NonceVerification
+		// phpcs:ignore WordPress.Security.NonceVerification
 		if ( empty( $_POST['pass1'] ) ) {
 			return;
 		}
 
 		// If bypass is enabled, do not validate the password
-		// phpcs:disable WordPress.Security.NonceVerification
+		// phpcs:ignore WordPress.Security.NonceVerification
 		if ( isset( $_POST['pw_weak'] ) && 'on' === $_POST['pw_weak'] ) {
 			return;
 		}
 
+		// phpcs:ignore WordPress.Security.NonceVerification
 		$password = sanitize_text_field( wp_unslash( $_POST['pass1'] ) );
-		if ( $this->validation_service->is_current_password( $user, $password ) ) {
+		if ( $this->validation_service->is_current_password( $user->ID, $password ) ) {
 			$errors->add( 'password_error', __( '<strong>Error:</strong> The password was used recently.', 'jetpack-account-protection' ) );
 			return;
 		}
@@ -151,18 +107,14 @@ class Password_Manager {
 	 * Handle the profile update.
 	 *
 	 * @param int      $user_id The user ID.
-	 * @param \WP_User $old_user_data The old user data.
-	 * @param array    $userdata The user data.
+	 * @param \WP_User $old_user_data Object containing user data prior to update.
 	 *
 	 * @return void
 	 */
-	public function on_profile_update( int $user_id, \WP_User $old_user_data, array $userdata ): void { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
+	public function on_profile_update( int $user_id, \WP_User $old_user_data ): void {
+		// phpcs:ignore WordPress.Security.NonceVerification
 		if ( isset( $_POST['action'] ) && $_POST['action'] === 'update' ) {
-			if ( isset( $_POST['pass1'] ) && ! empty( $_POST['pass1'] ) ) {
-				if ( $this->verify_profile_update_nonce( $user_id ) ) {
-						$this->save_recent_password( $user_id, $old_user_data->user_pass );
-				}
-			}
+			$this->save_recent_password( $user_id, $old_user_data->user_pass );
 		}
 	}
 
@@ -170,11 +122,10 @@ class Password_Manager {
 	 * Handle the password reset.
 	 *
 	 * @param \WP_User $user The user.
-	 * @param string   $new_password The new password.
 	 *
 	 * @return void
 	 */
-	public function on_password_reset( $user, $new_password ): void { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
+	public function on_password_reset( $user ): void {
 		$this->save_recent_password( $user->ID, $user->user_pass );
 	}
 
--- projects/packages/account-protection/tests/php/test-password-manager.php
+++ projects/packages/account-protection/tests/php/test-password-manager.php
@@ -8,51 +8,28 @@ use WorDBless\BaseTestCase;
  * Tests for the Password_Manager class.
  */
 class Password_Manager_Test extends BaseTestCase {
-	public function test_validate_profile_update_nonce_failure() {
-		$_POST['_wpnonce'] = 'invalid_nonce';
-		$_POST['pass1']    = 'newpassword';
-
-		$errors = new \WP_Error();
-		$user   = (object) array( 'ID' => 1 );
-
-		$validation_service_mock = $this->createMock( Validation_Service::class );
-		$password_manager_mock   = new Password_Manager( $validation_service_mock );
-
-		$password_manager_mock->validate_profile_update( $errors, true, $user );
-
-		$this->assertTrue( $errors->has_errors() );
-		$this->assertArrayHasKey( 'nonce_error', $errors->errors );
-	}
-
 	public function test_validate_profile_update_success() {
-		$_POST['_wpnonce'] = 'update-user_1';
-		$_POST['pass1']    = 'newpassword';
+		$user_id = wp_insert_user(
+			array(
+				'user_login' => 'admin',
+				'user_pass'  => wp_hash_password( 'oldpassword' ),
+				'user_email' => 'admin@admin.com',
+				'role'       => 'administrator',
+			)
+		);
 
 		$errors = new \WP_Error();
-		$user   = (object) array( 'ID' => 1 );
-
-		$fake_user            = new \WP_User();
-		$fake_user->ID        = 1;
-		$fake_user->user_pass = wp_hash_password( 'oldpassword' );
+		$user   = (object) array(
+			'ID'        => $user_id,
+			'user_pass' => wp_hash_password( 'newpassword' ),
+		);
 
 		$validation_service_mock = $this->createMock( Validation_Service::class );
 		$validation_service_mock->expects( $this->once() )
 			->method( 'return_first_validation_error' )
 			->willReturn( '' );
 
-		$password_manager_mock = $this->getMockBuilder( Password_Manager::class )
-			->setConstructorArgs( array( $validation_service_mock ) )
-			->onlyMethods( array( 'verify_profile_update_nonce', 'get_old_user_data' ) )
-			->getMock();
-
-		$password_manager_mock->expects( $this->once() )
-			->method( 'verify_profile_update_nonce' )
-			->willReturn( true );
-
-		$password_manager_mock->expects( $this->once() )
-			->method( 'get_old_user_data' )
-			->willReturn( $fake_user );
-
+		$password_manager_mock = new Password_Manager( $validation_service_mock );
 		$password_manager_mock->validate_profile_update( $errors, true, $user );
 
 		$this->assertFalse( $errors->has_errors() );
@@ -89,10 +66,6 @@ class Password_Manager_Test extends BaseTestCase {
 	}
 
 	public function test_on_profile_update_with_valid_nonce() {
-		$_POST['action']   = 'update';
-		$_POST['_wpnonce'] = 'valid_nonce';
-		$_POST['pass1']    = 'newpassword';
-
 		$user_id                  = 1;
 		$old_user_data            = new \WP_User();
 		$old_user_data->user_pass = 'oldhashedpassword';
@@ -100,18 +73,21 @@ class Password_Manager_Test extends BaseTestCase {
 		$validation_service_mock = $this->createMock( Validation_Service::class );
 		$password_manager_mock   = $this->getMockBuilder( Password_Manager::class )
 			->setConstructorArgs( array( $validation_service_mock ) )
-			->onlyMethods( array( 'save_recent_password', 'verify_profile_update_nonce' ) )
+			->onlyMethods( array( 'save_recent_password' ) )
 			->getMock();
-
-		$password_manager_mock->expects( $this->once() )
-			->method( 'verify_profile_update_nonce' )
-			->willReturn( true );
 
 		$password_manager_mock->expects( $this->once() )
 			->method( 'save_recent_password' )
 			->with( $user_id, 'oldhashedpassword' );
 
-		$password_manager_mock->on_profile_update( $user_id, $old_user_data, array() );
+		$password_manager_mock->on_profile_update(
+			$user_id,
+			$old_user_data,
+			array(
+				'ID'        => 1,
+				'user_pass' => 'newpassword',
+			)
+		);
 	}
 
 	public function test_on_password_reset_saves_recent_password() {
@@ -129,7 +105,7 @@ class Password_Manager_Test extends BaseTestCase {
 			->method( 'save_recent_password' )
 			->with( $user->ID, 'hashedpassword' );
 
-		$password_manager_mock->on_password_reset( $user, 'newpassword' );
+		$password_manager_mock->on_password_reset( $user );
 	}
 
 	public function test_save_recent_password_stores_last_10_passwords() {
