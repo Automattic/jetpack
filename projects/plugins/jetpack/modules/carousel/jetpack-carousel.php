c670f5dd3e4fdaf84b95cabd832aca221e179628
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-02-12 10:51:39 -0700

    More tweaks

--- projects/plugins/jetpack/modules/carousel/jetpack-carousel.php
+++ projects/plugins/jetpack/modules/carousel/jetpack-carousel.php
@@ -817,8 +817,8 @@ class Jetpack_Carousel {
 	 * @param string $content HTML content of the post.
 	 * @return string
 	 */
-	public function add_data_img_tags_and_enqueue_assets( $content = '' ) {
-		if ( ! is_string( $content ) ) {
+	public function add_data_img_tags_and_enqueue_assets( $content ) {
+		if ( ! is_string( $content ) || $content === '' ) {
 			return '';
 		}
 		if (
