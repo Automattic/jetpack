1106e3083a511806006dcb36c45c7e878a1303fc
Daniel Richards <daniel.richards@automattic.com>
2025-02-13 15:29:33 +0800

    Hide Jetpack field width controls when a flex or grid layout is in operation

--- projects/packages/forms/src/blocks/contact-form/components/jetpack-field-width.js
+++ projects/packages/forms/src/blocks/contact-form/components/jetpack-field-width.js
@@ -1,20 +1,36 @@
+import { useBlockEditContext } from '@wordpress/block-editor';
 import {
 	BaseControl,
 	__experimentalToggleGroupControl as ToggleGroupControl, // eslint-disable-line @wordpress/no-unsafe-wp-apis
 	__experimentalToggleGroupControlOption as ToggleGroupControlOption, // eslint-disable-line @wordpress/no-unsafe-wp-apis
 } from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 
 const PERCENTAGE_WIDTHS = [ 25, 50, 75, 100 ];
 
 export default function JetpackFieldWidth( { setAttributes, width } ) {
+	const { clientId } = useBlockEditContext();
+	const parentLayoutType = useSelect(
+		select => {
+			const { getBlockRootClientId, getBlockAttributes } = select( 'core/block-editor' );
+			const rootClientId = getBlockRootClientId( clientId );
+			return getBlockAttributes( rootClientId )?.layout?.type;
+		},
+		[ clientId ]
+	);
+
+	if ( parentLayoutType === 'flex' || parentLayoutType === 'grid' ) {
+		return null;
+	}
+
 	return (
 		<BaseControl
 			help={ __(
 				'Adjust the width of the field to include multiple fields on a single line.',
 				'jetpack-forms'
 			) }
-			__nextHasNoMarginBottom={ true }
+			__nextHasNoMarginBottom
 		>
 			<ToggleGroupControl
 				__next40pxDefaultSize
