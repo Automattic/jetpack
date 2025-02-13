48d11130beba1045c6c2f1c680814e0f52ae95db
Ben Dwyer <ben@scruffian.com>
2025-02-13 08:52:03 +0000

    move the wpcomsh theme files into wpcom-themes

--- projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
+++ projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
@@ -190,7 +190,12 @@ class Jetpack_Mu_Wpcom {
 		require_once __DIR__ . '/features/wpcom-profile-settings/profile-settings-notices.php';
 		require_once __DIR__ . '/features/wpcom-sidebar-notice/wpcom-sidebar-notice.php';
 		require_once __DIR__ . '/features/wpcom-themes/wpcom-themes.php';
-		require_once __DIR__ . '/features/wpcom-themes-api/themes.php';
+		require_once __DIR__ . '/features/wpcom-themes/wpcomsh-themes.php';
+
+		// We include WPCom Themes results and installation on WoA sites only and non-WP_CLI context.
+		if ( ( defined( 'IS_ATOMIC' ) && IS_ATOMIC ) && ( ! defined( 'WP_CLI' ) || ! WP_CLI ) ) {
+			require_once __DIR__ . '/features/wpcom-themes/wpcomsh-themes.php';
+		}
 
 		// Only load the Calypsoify and Masterbar features on WoA sites.
 		if ( class_exists( '\Automattic\Jetpack\Status\Host' ) && ( new \Automattic\Jetpack\Status\Host() )->is_woa_site() ) {
