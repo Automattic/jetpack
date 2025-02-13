b666b8530668af70d62933c3a7bbf9fe76c12bc5
spsiddarthan <siddarthan@outlook.com>
2025-02-13 20:01:13 +0530

    Newsletter: Added a new Jetpack package.

---
+++ projects/packages/newsletter-widget/src/class-newsletter-widget.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Package description here
+ *
+ * @package automattic/jetpack-newsletter-widget
+ */
+
+namespace Automattic\Jetpack;
+
+/**
+ * Class description.
+ */
+class Newsletter_Widget {
+
+	const PACKAGE_VERSION = '0.1.0-alpha';
+}
