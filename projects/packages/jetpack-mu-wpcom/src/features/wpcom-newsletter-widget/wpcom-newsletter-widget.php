dfcd414de2e173e9df5b981cb46e89bf92d693c6
spsiddarthan <siddarthan@outlook.com>
2025-02-14 13:23:37 +0530

    Initiate the right function.

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-newsletter-widget/wpcom-newsletter-widget.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-newsletter-widget/wpcom-newsletter-widget.php
@@ -11,5 +11,5 @@
 if ( defined( 'JETPACK_PLUGIN_LOADER_PATH' ) ) {
 	// TODO: Add the newsletter dashboard widget feature checks.
 	require_once JETPACK_PLUGIN_LOADER_PATH . '/class-jetpack-newsletter-widget.php';
-	add_action( 'wp_dashboard_setup', array( new Jetpack_Newsletter_Widget(), 'init' ) );
+	add_action( 'wp_dashboard_setup', array( new Jetpack_Newsletter_Dashboard_Widget(), 'init' ) );
 }
