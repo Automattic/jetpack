b3f612694fe05f78839b6d1b39b96e1c1b217c92
spsiddarthan <siddarthan@outlook.com>
2025-02-14 14:09:34 +0530

    Update path name.

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-newsletter-widget/wpcom-newsletter-widget.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-newsletter-widget/wpcom-newsletter-widget.php
@@ -10,6 +10,6 @@
  */
 if ( defined( 'JETPACK_PLUGIN_LOADER_PATH' ) ) {
 	// TODO: Add the newsletter dashboard widget feature checks.
-	require_once JETPACK_PLUGIN_LOADER_PATH . '/class-jetpack-newsletter-widget.php';
+	require_once JETPACK_PLUGIN_LOADER_PATH . '/class-jetpack-newsletter-dashboard-widget.php';
 	add_action( 'wp_dashboard_setup', array( new Jetpack_Newsletter_Dashboard_Widget(), 'init' ) );
 }
