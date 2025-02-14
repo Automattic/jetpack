cae17a82565c9a46f6ae9348e32f5a0501aa6ce6
spsiddarthan <siddarthan@outlook.com>
2025-02-14 12:39:44 +0530

    Newsletter widget: init.

--- projects/plugins/jetpack/class-jetpack-newsletter-dashboard-widget.php
+++ projects/plugins/jetpack/class-jetpack-newsletter-dashboard-widget.php
@@ -49,69 +49,11 @@ class Jetpack_Newsletter_Dashboard_Widget {
 	}
 
 	/**
-	 * JavaScript and CSS for dashboard widget.
-	 *
-	 * @access public
-	 * @return void
-	 */
-	public static function admin_head() {
-		?>
-			<script type="text/javascript">
-				/* <![CDATA[ */
-				jQuery( function($) {
-					var dashStats = jQuery( '#dashboard_stats div.inside' );
-
-					if ( dashStats.find( '.dashboard-widget-control-form' ).length ) {
-						return;
-					}
-
-					if ( ! dashStats.length ) {
-						dashStats = jQuery( '#dashboard_stats div.dashboard-widget-content' );
-						var h = parseInt( dashStats.parent().height() ) - parseInt( dashStats.prev().height() );
-						var args = 'width=' + dashStats.width() + '&height=' + h.toString();
-					} else {
-						if ( jQuery('#dashboard_stats' ).hasClass('postbox') ) {
-							var args = 'width=' + ( dashStats.prev().width() * 2 ).toString();
-						} else {
-							var args = 'width=' + ( dashStats.width() * 2 ).toString();
-						}
-					}
-
-					dashStats
-						.not( '.dashboard-widget-control' )
-						.load( 'admin.php?page=stats&noheader&dashboard&' + args, function() {
-							jQuery( '#dashboard_stats' ).removeClass( 'is-loading' );
-							jQuery( '#stat-chart' ).css( 'width', 'auto' );
-						} );
-
-					// Widget settings toggle container.
-					var toggle = $( '.js-toggle-stats_dashboard_widget_control' );
-
-					// Move the toggle in the widget header.
-					toggle.appendTo( '#jetpack_summary_widget .handle-actions' );
-
-					// Toggle settings when clicking on it.
-					toggle.show().click( function( e ) {
-						e.preventDefault();
-						e.stopImmediatePropagation();
-						$( this ).parent().toggleClass( 'controlVisible' );
-						$( '#stats_dashboard_widget_control' ).slideToggle();
-					} );
-				} );
-				/* ]]> */
-			</script>
-		<?php
-	}
-
-	/**
 	 * Sets up the Jetpack Newsletter widget in the WordPress admin dashboard.
 	 */
 	public static function wp_dashboard_setup() {
-
+		static::load_admin_scripts( 'jp-newsletter-widget', 'app.min', array( 'config_variable_name' => 'jetpackNewsletterWidgetConfigData' ) );
 		if ( Jetpack::is_connection_ready() ) {
-			add_action( 'admin_head', array( static::class, 'admin_head' ) );
-			add_action( 'admin_init', array( static::class, 'admin_init' ) );
-
 			$widget_title = sprintf(
 				__( 'Newsletter', 'jetpack' )
 			);
