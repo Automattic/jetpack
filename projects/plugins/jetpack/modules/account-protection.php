fd2cb1cb00ae27046cacb9ee743ff0b3768b69b5
dkmyta <43220201+dkmyta@users.noreply.github.com>
2025-02-16 04:23:23 -0800

    Update projects/plugins/jetpack/modules/account-protection.php

--- projects/plugins/jetpack/modules/account-protection.php
+++ projects/plugins/jetpack/modules/account-protection.php
@@ -3,7 +3,7 @@
  * Module Name: Account protection
  * Module Description: When enabled, users can only set passwords that meet strong security standards, helping protect their accounts and your site.
  * Sort Order: 4
- * First Introduced: $$next-version$$
+ * First Introduced: 14.4
  * Requires Connection: Yes
  * Requires User Connection: No
  * Auto Activate: Yes
