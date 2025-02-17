92054df9c72e8d89ecc01082974b63517598d18d
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-02-17 13:20:27 +0530

    Do not render the panels if publicize is OFF

--- projects/js-packages/publicize-components/src/components/block-editor/components/social-panels.tsx
+++ projects/js-packages/publicize-components/src/components/block-editor/components/social-panels.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from '@automattic/jetpack-components';
 import { PostTypeSupportCheck } from '@wordpress/editor';
+import { getSocialScriptData } from '../../../utils';
 import { GlobalModals } from '../../global-modals';
 import PostPublishPanels from './post-publish-panels';
 import PrePublishPanels from './pre-publish-panels';
@@ -10,6 +11,9 @@ import PrePublishPanels from './pre-publish-panels';
  * @return The social panels
  */
 export function SocialPanels() {
+	if ( ! getSocialScriptData().is_publicize_enabled ) {
+		return null;
+	}
 	return (
 		<PostTypeSupportCheck supportKeys="publicize">
 			<ThemeProvider targetDom={ document.body }>
