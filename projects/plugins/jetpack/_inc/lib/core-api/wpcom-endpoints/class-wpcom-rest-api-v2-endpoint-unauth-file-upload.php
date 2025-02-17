730e9d2bb10ed3352e7d9d7dc9f5624d9dbe843e
Miguel Lezama <lezama@gmail.com>
2025-02-17 17:23:29 -0300

    make the endpoint generic

--- projects/plugins/jetpack/_inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-unauth-file-upload.php
+++ projects/plugins/jetpack/_inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-unauth-file-upload.php
@@ -2,8 +2,8 @@
 /**
  * Unauthenticated File Upload endpoint for the WordPress.com REST API.
  *
- * This endpoint is primarily used by the File Upload field in Jetpack Forms,
- * allowing unauthenticated users to securely upload files through WordPress.com.
+ * A generic endpoint that allows unauthenticated users to securely upload files through WordPress.com.
+ * The endpoint requires a nonce and context to validate and organize uploads.
  *
  * @package automattic/jetpack
  */
@@ -38,34 +38,6 @@ class WPCOM_REST_API_V2_Endpoint_Unauth_File_Upload extends WP_REST_Controller {
 	 * Registers the routes for file upload.
 	 */
 	public function register_routes() {
-
-		register_rest_route(
-			$this->namespace,
-			$this->rest_base . '/nonce',
-			array(
-				'methods'             => 'GET',
-				'callback'            => array( $this, 'get_nonce' ),
-				'permission_callback' => array( $this, 'permissions_check_nonce' ),
-				'args'                => array(
-					'hash'    => array(
-						'description' => __( 'Pass the hash', 'jetpack' ), // This should contain info about the form. allowed types, form id, etc.
-						'type'        => 'string',
-						'required'    => true,
-					),
-					'id'      => array(
-						'description' => __( 'Pass the field id', 'jetpack' ),
-						'type'        => 'string',
-						'required'    => true,
-					),
-					'form_id' => array(
-						'description' => __( 'Pass the form id', 'jetpack' ),
-						'type'        => 'string',
-						'required'    => true,
-					),
-				),
-			)
-		);
-
 		register_rest_route(
 			$this->namespace,
 			$this->rest_base,
@@ -73,24 +45,15 @@ class WPCOM_REST_API_V2_Endpoint_Unauth_File_Upload extends WP_REST_Controller {
 				'methods'             => 'POST',
 				'callback'            => array( $this, 'handle_upload' ),
 				'permission_callback' => array( $this, 'permissions_check' ),
+				'authentication'      => array(),  // Disable all authentication methods
 				'args'                => array(
-					'_nonce'  => array(
+					'_wpnonce' => array(
 						'description' => __( 'Nonce', 'jetpack' ),
 						'type'        => 'string',
 						'required'    => true,
 					),
-					'hash'    => array(
-						'description' => __( 'Pass the hash', 'jetpack' ), // This should contain info about the form. allowed types, form id, etc.
-						'type'        => 'string',
-						'required'    => true,
-					),
-					'id'      => array(
-						'description' => __( 'Pass the field id', 'jetpack' ),
-						'type'        => 'string',
-						'required'    => true,
-					),
-					'form_id' => array(
-						'description' => __( 'Pass the form id', 'jetpack' ),
+					'context'  => array(
+						'description' => __( 'Context identifier for the upload', 'jetpack' ),
 						'type'        => 'string',
 						'required'    => true,
 					),
@@ -99,7 +62,13 @@ class WPCOM_REST_API_V2_Endpoint_Unauth_File_Upload extends WP_REST_Controller {
 		);
 	}
 
-	public function permissions_check_nonce( $request ) {
+	/**
+	 * Checks if the request has permission to upload files
+	 *
+	 * @param WP_REST_Request $request Full data about the request.
+	 * @return bool|WP_Error True if the request has permission, WP_Error object otherwise.
+	 */
+	public function permissions_check( $request ) {
 		// For non-WPCOM sites, require Jetpack connection
 		if ( ! ( new Host() )->is_wpcom_simple() && ! ( new Manager() )->is_connected() ) {
 			return new WP_Error(
@@ -109,47 +78,19 @@ class WPCOM_REST_API_V2_Endpoint_Unauth_File_Upload extends WP_REST_Controller {
 			);
 		}
 
-		$id       = $request->get_param( 'id' );
-		$hash     = $request->get_param( 'hash' );
-		$form_id  = $request->get_param( 'form_id' );
-		$new_hash = wp_hash( $id . $form_id ); // in the future this could contain other info.
-		return hash_equals( $new_hash, $hash );
-	}
+		$context = $request->get_param( 'context' );
+		$nonce   = $request->get_param( '_wpnonce' );
 
-	public function get_nonce( $request ) {
-		return wp_create_nonce( 'file_field' . $request->get_param( 'id' ) );
-	}
-
-	/**
-	 * Checks if the site has the required Jetpack connection
-	 *
-	 * @pram WP_REST_Request $request Full data about the request.
-	 *
-	 * @return true|WP_Error True if the request has permission, WP_Error object otherwise.
-	 */
-	public function permissions_check( $request ) {
-		$nonce_permission_check = $this->permissions_check_nonce( $request );
-		if ( is_wp_error( $nonce_permission_check ) ) {
-			return $nonce_permission_check;
-		}
-
-		if ( ! $nonce_permission_check ) {
-			return false;
-		}
-
-		$id       = $request->get_param( 'id' );
-		$nonce    = $request->get_param( '_nonce' );
-		$verified = wp_verify_nonce( $nonce, 'file_field' . $id );
-
-		if ( ! $verified ) {
+		// Verify the form upload nonce
+		if ( ! wp_verify_nonce( $nonce, 'jetpack_upload_' . $context ) ) {
 			return new WP_Error(
 				'rest_forbidden',
-				__( 'Nonce verification failed', 'jetpack' ),
+				__( 'Invalid upload token.', 'jetpack' ),
 				array( 'status' => rest_authorization_required_code() )
 			);
 		}
 
-		return $verified;
+		return true;
 	}
 
 	/**
@@ -159,7 +100,6 @@ class WPCOM_REST_API_V2_Endpoint_Unauth_File_Upload extends WP_REST_Controller {
 	 * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
 	 */
 	public function handle_upload( $request ) {
-
 		// Get the uploaded file
 		$files = $request->get_file_params();
 
@@ -171,17 +111,34 @@ class WPCOM_REST_API_V2_Endpoint_Unauth_File_Upload extends WP_REST_Controller {
 			);
 		}
 
-		// we should probably check that the file has all the correct things.
+		$context = $request->get_param( 'context' );
 
+		// Allow filtering of upload handling based on context
+		$upload_handler = apply_filters( 'jetpack_unauth_file_upload_handler', array( $this, 'handle_default_upload' ), $context );
+
+		if ( is_callable( $upload_handler ) ) {
+			return call_user_func( $upload_handler, $request, $files['file'] );
+		}
+
+		return new WP_Error(
+			'rest_invalid_handler',
+			__( 'Invalid upload handler.', 'jetpack' ),
+			array( 'status' => 500 )
+		);
+	}
+
+	/**
+	 * Default handler for file uploads when no specific handler is registered
+	 *
+	 * @param WP_REST_Request $request Full data about the request.
+	 * @param array           $file The uploaded file data.
+	 * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
+	 */
+	public function handle_default_upload( $request, $file ) {
 		if ( ( new Host() )->is_wpcom_simple() ) {
 			// Cloud handling.
 			return apply_filters( 'jetpack_unauth_file_upload_response', new WP_REST_Response( array() ), $request );
 		} else {
-
-			$file = $files['file'];
-			l( 'FILE SEND OVER!' );
-			l( $file );
-
 			if ( UPLOAD_ERR_OK !== $file['error'] ) {
 				return $this->get_upload_error_message( $file['error'] );
 			}
@@ -197,19 +154,47 @@ class WPCOM_REST_API_V2_Endpoint_Unauth_File_Upload extends WP_REST_Controller {
 				return $this->proxy_request_to_wpcom( $request );
 			}
 
-			// Handle file upload for Jetpack Forms
-			return $this->handle_single_file_upload( $request, $file );
+			// Handle file upload locally
+			return $this->handle_local_file_upload( $request, $file );
 		}
 	}
 
-	private function get_upload_error_message( $file_error ) {
+	/**
+	 * Gets an error message for file upload errors
+	 *
+	 * @param int $error_code The error code from the file upload.
+	 * @return WP_Error Error object with the error message.
+	 */
+	private function get_upload_error_message( $error_code ) {
+		$error_message = __( 'Failed to upload file.', 'jetpack' );
+
+		// Add specific error messages based on the error code if needed
+		switch ( $error_code ) {
+			case UPLOAD_ERR_INI_SIZE:
+			case UPLOAD_ERR_FORM_SIZE:
+				$error_message = __( 'The uploaded file exceeds the maximum allowed size.', 'jetpack' );
+				break;
+			case UPLOAD_ERR_PARTIAL:
+				$error_message = __( 'The file was only partially uploaded.', 'jetpack' );
+				break;
+			case UPLOAD_ERR_NO_FILE:
+				$error_message = __( 'No file was uploaded.', 'jetpack' );
+				break;
+		}
+
 		return new WP_Error(
 			'rest_upload_error',
-			__( 'Failed to upload file.', 'jetpack' ),
+			$error_message,
 			array( 'status' => 400 )
 		);
 	}
 
+	/**
+	 * Checks if the file type is allowed
+	 *
+	 * @param string $file_name The name of the file to check.
+	 * @return true|WP_Error True if the file type is allowed, WP_Error object otherwise.
+	 */
 	private function check_file_type( $file_name ) {
 		// Define allowed mime types
 		$allowed_mime_types = get_allowed_mime_types();
@@ -223,64 +208,62 @@ class WPCOM_REST_API_V2_Endpoint_Unauth_File_Upload extends WP_REST_Controller {
 			);
 		}
 	}
+
 	/**
-	 *
-	 * todo: This should be its own class.
+	 * Gets a secure upload directory for temporary files
 	 */
 	private function get_secret_directory() {
-		$jetpack_forms_dir = get_option( 'jetpack_forms_dir', false );
-		if ( ! $jetpack_forms_dir ) {
-			$jetpack_forms_dir = wp_generate_password( 64, false );
-			update_option( 'jetpack_forms_dir', $jetpack_forms_dir );
+		$secret_dir = get_option( 'jetpack_upload_temp_dir', false );
+		if ( ! $secret_dir ) {
+			$secret_dir = wp_generate_password( 64, false );
+			update_option( 'jetpack_upload_temp_dir', $secret_dir );
 		}
-		return $jetpack_forms_dir;
+		return $secret_dir;
 	}
 
-	private function handle_single_file_upload( $request, $file ) {
-		// only one request per fle
-		// Initialize an empty array for storing uploaded file paths
-		$file_name  = $file_name = sanitize_file_name( wp_unslash( $file['name'] ) );
+	/**
+	 * Handles local file upload processing
+	 *
+	 * @param WP_REST_Request $request The request object.
+	 * @param array           $file The uploaded file data.
+	 * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
+	 */
+	private function handle_local_file_upload( $request, $file ) {
 		$upload_dir = wp_upload_dir();
 		if ( ! empty( $upload_dir['error'] ) ) {
-			// The 302 is to that we can help debug this error.
-			return new WP_Error( 'dir_error', __( 'Unable to process file upload.', 'jetpack-forms' ) . ' [301] ', array( 'status' => 400 ) );
+			return new WP_Error( 'dir_error', __( 'Unable to process file upload.', 'jetpack' ) . ' [301] ', array( 'status' => 400 ) );
 		}
 
-		$jetpack_forms_dir = $upload_dir['basedir'] . '/jetpack-forms/' . $this->get_secret_directory() . '/temp';
-		if ( ! file_exists( $jetpack_forms_dir ) ) {
-			$create_dir = wp_mkdir_p( $jetpack_forms_dir );
+		$temp_dir = $upload_dir['basedir'] . '/jetpack-upload/' . $this->get_secret_directory() . '/temp';
+		if ( ! file_exists( $temp_dir ) ) {
+			$create_dir = wp_mkdir_p( $temp_dir );
 			if ( ! $create_dir ) {
-				// The 301 is to that we can help debug this error.
-				return new WP_Error( 'dir_create', __( 'Unable to process file upload.', 'jetpack-forms' ) . ' [302] ', array( 'status' => 400 ) );
+				return new WP_Error( 'dir_create', __( 'Unable to process file upload.', 'jetpack' ) . ' [302] ', array( 'status' => 400 ) );
 			}
 		}
 
 		$file_name = sanitize_file_name( wp_unslash( $file['name'] ) );
-		$hash      = $request->get_param( 'hash' );
 		// This is a temporary secret that will be used to move the file to the final location.
-		$temporary_secret      = wp_hash( wp_rand( 100000, 999999 ) . microtime() );
-		$secret_file_name_hash = wp_hash( $temporary_secret . $hash );
-		$secret_file_name      = $secret_file_name_hash . '-' . $file_name;
+		$temporary_secret = wp_hash( wp_rand( 100000, 999999 ) . microtime() );
+		$secret_file_name = $temporary_secret . '-' . $file_name;
 
-		$new_secret__filename = wp_unique_filename( $jetpack_forms_dir, $secret_file_name );
+		$new_secret__filename = wp_unique_filename( $temp_dir, $secret_file_name );
 
 		if ( $secret_file_name !== $new_secret__filename ) {
-			// The 304 is to that we can help debug this error.
-			return new WP_Error( 'file_exists', __( 'Unable to process file upload.', 'jetpack-forms' ) . ' [304] ', array( 'status' => 400 ) );
+			return new WP_Error( 'file_exists', __( 'Unable to process file upload.', 'jetpack' ) . ' [304] ', array( 'status' => 400 ) );
 		}
 
 		// Move uploaded file
-		$move_result = move_uploaded_file( $file['tmp_name'], $jetpack_forms_dir . '/' . $new_secret__filename );
+		$move_result = move_uploaded_file( $file['tmp_name'], $temp_dir . '/' . $new_secret__filename );
 		if ( ! $move_result ) {
-			// The 303 is to that we can help debug this error.
-			return new WP_Error( 'file_move', __( 'Unable to process file upload.', 'jetpack-forms' ) . ' [303] ' . array( 'status' => 400 ) );
+			return new WP_Error( 'file_move', __( 'Unable to process file upload.', 'jetpack' ) . ' [303] ', array( 'status' => 400 ) );
 		}
 
 		return rest_ensure_response(
 			array(
 				'success' => true,
 				'data'    => array(
-					'filename' => ltrim( $new_secret__filename, $secret_file_name_hash . '-' ),
+					'filename' => ltrim( $new_secret__filename, $temporary_secret . '-' ),
 					'temp'     => $temporary_secret,
 				),
 			)
