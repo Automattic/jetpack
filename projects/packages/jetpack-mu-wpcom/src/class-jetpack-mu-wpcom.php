6fc6f338ff2350c3a2216dd3d9bfd6f4fbfbe97a
arthur <arthur.chu@automattic.com>
2025-02-17 17:15:16 +0900

    MU WPCOM: Filter out the full-site-editing plugin from the active_plugins list

--- projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
+++ projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
@@ -257,6 +257,7 @@ class Jetpack_Mu_Wpcom {
 
 		require_once __DIR__ . '/features/jetpack-global-styles/class-global-styles.php';
 		require_once __DIR__ . '/features/mailerlite/subscriber-popup.php';
+		require_once __DIR__ . '/features/wpcom-fse/wpcom-fse.php';
 
 		/**
 		 * Load features for the editor and the frontend pages.
