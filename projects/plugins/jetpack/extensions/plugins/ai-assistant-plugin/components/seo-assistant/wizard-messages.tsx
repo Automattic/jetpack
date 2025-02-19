7de8c90c06e151d4c74e1045186da186d512ecc2
Douglas <douglas.henri@automattic.com>
2025-02-19 15:22:18 -0300

    add useArrayState

--- projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/seo-assistant/wizard-messages.tsx
+++ projects/plugins/jetpack/extensions/plugins/ai-assistant-plugin/components/seo-assistant/wizard-messages.tsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { useCallback, useState } from '@wordpress/element';
+import { useCallback } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import clsx from 'clsx';
 /**
@@ -9,6 +9,7 @@ import clsx from 'clsx';
  */
 import bigSkyIcon from './big-sky-icon.svg';
 import TypingMessage from './typing-message';
+import { useArrayState } from './use-array-state';
 /**
  * Types
  */
@@ -22,20 +23,22 @@ const randomId = () => Math.random().toString( 32 ).substring( 2, 8 );
  * @return {object} An object containing methods to manage messages
  */
 export const useMessages = ( count = 1 ) => {
-	const [ messagesArray, setMessagesArray ] = useState< Message[][] >(
+	const [ messagesArray, setMessages ] = useArrayState< Message[] >(
 		Array.from( { length: count }, () => [] )
 	);
 
-	const wrapMessagesWithId = useCallback( ( rawMessages: Message[], id = 0 ) => {
-		setMessagesArray( prev => {
-			const next = [ ...prev ];
-			next[ id ] = rawMessages.map( rawMessage => ( {
-				...rawMessage,
-				id: rawMessage.id || randomId(),
-			} ) );
-			return next;
-		} );
-	}, [] );
+	const wrapMessagesWithId = useCallback(
+		( rawMessages: Message[], id = 0 ) => {
+			setMessages(
+				rawMessages.map( rawMessage => ( {
+					...rawMessage,
+					id: rawMessage.id || randomId(),
+				} ) ),
+				id
+			);
+		},
+		[ setMessages ]
+	);
 
 	const addMessage = async ( message: Message, id = 0 ) => {
 		const newMessage = {
@@ -44,30 +47,23 @@ export const useMessages = ( count = 1 ) => {
 			id: message.id || randomId(),
 		} as Message;
 
-		setMessagesArray( prev => {
-			const next = [ ...prev ];
-			next[ id ] = [ ...next[ id ], newMessage ];
-			return next;
-		} );
+		setMessages( previousMessages => [ ...previousMessages, newMessage ], id );
 	};
 
 	/* Removes last message */
 	const removeLastMessage = ( id = 0 ) => {
-		setMessagesArray( prev => {
-			const next = [ ...prev ];
-			next[ id ] = next[ id ].slice( 0, -1 );
-			return next;
-		} );
+		setMessages( previousMessages => previousMessages.slice( 0, -1 ), id );
 	};
 
 	/* Edits content of last message */
 	const editLastMessage = ( content: Message[ 'content' ], append = false, id = 0 ) => {
-		setMessagesArray( prev => {
-			const next = [ ...prev ];
-			const prevMessages = [ ...next[ id ] ];
-			if ( prevMessages.length > 0 ) {
-				const lastMessageContent = prevMessages[ prevMessages.length - 1 ].content;
+		setMessages( previousMessages => {
+			const next = [ ...previousMessages ];
+
+			if ( next.length > 0 ) {
+				const lastMessageContent = next[ next.length - 1 ].content;
 				let newContent = content;
+
 				if ( append ) {
 					if ( typeof lastMessageContent === 'object' || typeof newContent === 'object' ) {
 						newContent = (
@@ -80,29 +76,31 @@ export const useMessages = ( count = 1 ) => {
 						newContent = `${ lastMessageContent } + ${ newContent }`;
 					}
 				}
-				prevMessages[ prevMessages.length - 1 ] = {
-					...prevMessages[ prevMessages.length - 1 ],
+
+				next[ next.length - 1 ] = {
+					...next[ next.length - 1 ],
 					content: newContent,
 				};
 			}
+
 			return next;
-		} );
+		}, id );
 	};
 
 	const setSelectedMessage = ( message: Message, id = 0 ) => {
-		setMessagesArray( prev => {
-			const next = [ ...prev ];
-			next[ id ] = next[ id ].map( prevMessage => ( {
-				...prevMessage,
-				selected: message.id === prevMessage.id,
+		setMessages( previousMessages => {
+			const next = previousMessages.map( previousMessage => ( {
+				...previousMessage,
+				selected: message.id === previousMessage.id,
 			} ) );
+
 			return next;
-		} );
+		}, id );
 	};
 
 	return {
 		getMessages: ( id = 0 ) => messagesArray[ id ],
-		setMessages: ( messages: Message[], id = 0 ) => wrapMessagesWithId( messages, id ),
+		setMessages: wrapMessagesWithId,
 		addMessage,
 		removeLastMessage,
 		editLastMessage,
