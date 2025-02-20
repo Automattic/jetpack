78dea267e545c26882d349be8f6a0b6943707f5b
cpapazoglou <hrrsppzgl@gmail.com>
2025-02-20 14:33:37 +0200

    Removes not required files.

--- projects/packages/videopress/jest.config.cjs
+++ projects/packages/videopress/jest.config.cjs
@@ -2,14 +2,4 @@ const baseConfig = require( 'jetpack-js-tools/jest/config.base.js' );
 
 module.exports = {
 	...baseConfig,
-	testEnvironment: 'jsdom',
-	setupFilesAfterEnv: [ ...( baseConfig.setupFilesAfterEnv || [] ) ],
-	moduleNameMapper: {
-		...( baseConfig.moduleNameMapper || {} ),
-		'\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
-	},
-	transform: {
-		...( baseConfig.transform || {} ),
-		'^.+\\.(ts|tsx)$': 'babel-jest',
-	},
 };
