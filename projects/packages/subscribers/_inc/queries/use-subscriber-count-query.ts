4cb3463e031a674c44d6ef10e710fd8fed15a480
Jorge Costa <jorge.costa@developer.pt>
2025-02-20 13:44:58 +0000

    addition of subscribers code

---
+++ projects/packages/subscribers/_inc/queries/use-subscriber-count-query.ts
@@ -0,0 +1,38 @@
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import wpcom from 'calypso/lib/wp';
+
+export interface SubscribersTotals {
+	email_subscribers: number;
+	paid_subscribers: number;
+	social_followers: number;
+}
+
+export const defaultSubscribersTotals = {
+	email_subscribers: 0,
+	paid_subscribers: 0,
+	social_followers: 0,
+};
+
+const getSubscriberCount = ( siteId: number | null ): Promise< any > => {
+	return wpcom.req.get( {
+		apiNamespace: 'wpcom/v2',
+		path: `/sites/${ siteId }/subscribers/counts`,
+	} );
+};
+
+/**
+ *
+ * @param siteId
+ */
+export default function useSubscriberCountQuery( siteId: number | null ) {
+	return useQuery< SubscribersTotals >( {
+		queryKey: [ 'subscribers', 'count', siteId ],
+		queryFn: () => {
+			return getSubscriberCount( siteId ).then( response => {
+				return response.counts || defaultSubscribersTotals;
+			} );
+		},
+		enabled: !! siteId,
+		placeholderData: keepPreviousData,
+	} );
+}
