5a409fe2ea1970432a89a4e7d28d777ce3e82f92
Liam Sarsfield <43409125+LiamSarsfield@users.noreply.github.com>
2025-02-21 16:13:09 +0000

    Boost: Fix WordPress Action Handling for Empty Arguments
    - Add workaround to handle WordPress converting empty arrays to strings for actions
    - Ensure actions with no arguments are called correctly using `do_action()`
    - Preserve original action call behavior for non-empty argument lists

--- projects/plugins/boost/app/lib/Scheduled_Event.php
+++ projects/plugins/boost/app/lib/Scheduled_Event.php
@@ -71,6 +71,12 @@ class Scheduled_Event implements Has_Setup {
 
 		update_site_option( "{$action}_network_cron_ran", $current_time );
 
-		do_action( $action, ...$args );
+		// This is required as WordPress converts empty arrays to strings for actions during spreading.
+		// This is a workaround to ensure the action is called with no arguments, so default arguments can be used within the action.
+		if ( empty( $args ) ) {
+			do_action( $action );
+		} else {
+			do_action( $action, ...$args );
+		}
 	}
 }
