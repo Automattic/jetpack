8c13ceed0efb00100b4e04b13d841f09d8005f9b
Gergely Juhasz <gergely.juhasz@automattic.com>
2025-02-24 13:01:41 +0100

    Move Mastodon input form to the left

--- projects/js-packages/publicize-components/src/components/services/connect-form.tsx
+++ projects/js-packages/publicize-components/src/components/services/connect-form.tsx
@@ -76,7 +76,7 @@ export function ConnectForm( {
 			onSubmit={ onSubmitForm }
 		>
 			{ displayInputs ? (
-				<div className={ styles[ 'fields-wrapper' ] }>
+				<div className={ clsx( styles[ 'fields-wrapper' ], styles.input ) }>
 					<CustomInputs service={ service } />
 				</div>
 			) : null }
