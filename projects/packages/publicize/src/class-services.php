5f4aec2cd28f5bd04fe284135f449a3e65a8114d
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-02-24 13:05:01 +0530

    Move the publicize/services endpoint to publicize package
    This commit:
    - Moves the `wpcom/v2/publicize/services` endpoint from Jetpack plugin to publicize package
    - Adds Services:: wpcom_get_all() method
    - Updates the schema, using 'id' instead of 'name' because that helps us use the core getEntityRecords selectors
    - Initializes the endpoint in Publicize_Setup

--- projects/packages/publicize/src/class-services.php
+++ projects/packages/publicize/src/class-services.php
@@ -18,6 +18,41 @@ class Services {
 	const SERVICES_TRANSIENT = 'jetpack_social_available_services_list';
 
 	/**
+	 * Get the available publicize services. Meant to be called directly only on WPCOM.
+	 *
+	 * @return array
+	 */
+	public static function wpcom_get_all() {
+		// Ensure that we are on WPCOM.
+		Publicize_Utils::assert_is_wpcom( __METHOD__ );
+
+		require_lib( 'external-connections' );
+
+		$external_connections = \WPCOM_External_Connections::init();
+
+		$services = $external_connections->get_external_services_list( 'publicize', get_current_blog_id() );
+
+		$items = array();
+
+		foreach ( $services as $service ) {
+			// Set the fields as per the schema in Services_Controller.
+			$items[] = array(
+				'id'          => $service['ID'],
+				'description' => $service['description'],
+				'label'       => $service['label'],
+				'status'      => $service['status'] ?? 'ok',
+				'supports'    => array(
+					'additional_users'      => $service['multiple_external_user_ID_support'],
+					'additional_users_only' => $service['external_users_only'],
+				),
+				'url'         => $service['connect_URL'],
+			);
+		}
+
+		return $items;
+	}
+
+	/**
 	 * Get all services.
 	 *
 	 * @param bool $force_refresh Whether to force a refresh of the services.
