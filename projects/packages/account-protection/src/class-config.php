aa854fea447fa7e730103561a0dfa9af816ac591
dkmyta <43220201+dkmyta@users.noreply.github.com>
2025-02-24 13:21:25 -0800

    Account Protection: Update auth code email handling (#41801)
    * Improve the display of error notices on the password detection screen
    
    * Improve success flow, add success notices
    
    * Fix phan errors
    
    * Fix tests
    
    * Fix notice responsiveness
    
    * Use API errors and error object for setting error transients
    
    * Use correct transient prefix constant
    
    * Improve rate limiting
    
    * Enforce request limit per token
    
    * Update account recovery message
    
    * Fix constant
    
    * General constants
    
    * Remove unneeded comments
    
    * Update email request limit message
    
    * Show recovery message on email_send_error also
    
    * Fix phan errors
    
    * Update recovery message to recommend a password reset
    
    * Fix typo
    
    * Remove unrelated changes
    
    * Fix expected transient prefix in tests
    
    ---------
    
    Co-authored-by: Nate Weller <nate.weller@automattic.com>

--- projects/packages/account-protection/src/class-config.php
+++ projects/packages/account-protection/src/class-config.php
@@ -11,17 +11,17 @@ namespace Automattic\Jetpack\Account_Protection;
  * Class Config
  */
 class Config {
-	public const SUPPORT_LINK = 'https://jetpack.com/?post_type=jetpack_support&p=324199';
+	public const SUPPORT_LINK                         = 'https://jetpack.com/?post_type=jetpack_support&p=324199';
+	public const TRANSIENT_PREFIX                     = 'jetpack_account_protection';
+	public const RECENT_PASSWORD_HASHES_USER_META_KEY = 'jetpack_account_protection_recent_password_hashes';
 
 	// Password Detection Constants
-	public const PASSWORD_DETECTION_TRANSIENT_PREFIX      = 'password_detection';
 	public const PASSWORD_DETECTION_ERROR_CODE            = 'password_detection_validation_error';
 	public const PASSWORD_DETECTION_EMAIL_SENT_EXPIRATION = 600; // 10 minutes
-	public const PASSWORD_DETECTION_MAX_RESEND_ATTEMPTS   = 3;
+	public const PASSWORD_DETECTION_EMAIL_REQUEST_LIMIT   = 4;
 
 	// Password Manager Constants
-	public const PASSWORD_MANAGER_RECENT_PASSWORD_HASHES_USER_META_KEY = 'jetpack_account_protection_recent_password_hashes';
-	public const PASSWORD_MANAGER_RECENT_PASSWORDS_LIMIT               = 10;
+	public const PASSWORD_MANAGER_RECENT_PASSWORDS_LIMIT = 10;
 
 	// Validation Service Constants
 	public const VALIDATION_SERVICE_MIN_LENGTH = 6;
