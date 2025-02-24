c3e2bd4612947ce701d27de01f9b45f5f0e838c1
Matias Benedetto <matias.benedetto@gmail.com>
2025-02-24 10:06:36 -0800

    Hide customizer link from admin bar and admin menu when a block theme is set.

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-admin-menu/wpcom-admin-menu.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-admin-menu/wpcom-admin-menu.php
@@ -291,6 +291,25 @@ remove_action( 'admin_menu', 'add_jetpack_submenu', 999999 );
  */
 function wpcom_hide_customizer_submenu_on_block_theme() {
 	if ( wp_is_block_theme() && ! is_customize_preview() ) {
+
+		// Remove the customize item from admin bar
+		add_action(
+			'admin_bar_menu',
+			function ( $wp_admin_bar ) {
+				$wp_admin_bar->remove_node( 'customize' );
+			},
+			50
+		);
+
+		// Remove the customize submenu from the admin menu
+		add_action(
+			'admin_menu',
+			function () {
+				remove_submenu_page( 'themes.php', 'customize.php?return=%2Fwp-admin%2Foptions-general.php' );
+			},
+			50
+		);
+
 		remove_action( 'customize_register', 'add_logotool_button', 20 );
 		remove_action( 'customize_register', 'footercredits_register', 99 );
 		remove_action( 'customize_register', 'wpcom_disable_customizer_site_icon', 20 );
