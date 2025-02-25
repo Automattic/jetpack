ffdbf5f0ea4af15b398b4287b050cfacc00723fd
Erick Danzer <edanzer@gmail.com>
2025-02-25 10:57:25 -0700

    Update AkismetPanel based on PR feedback

--- projects/packages/forms/src/blocks/contact-form/components/akismet-panel/index.js
+++ projects/packages/forms/src/blocks/contact-form/components/akismet-panel/index.js
@@ -1,54 +1,62 @@
+import { getSiteAdminUrl } from '@automattic/jetpack-components';
 import { Button } from '@wordpress/components';
-import { store as coreStore } from '@wordpress/core-data';
-import { useSelect } from '@wordpress/data';
+import { createInterpolateElement } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import PluginIntegrationPanel from '../shared/plugin-integration-panel';
 import './styles.css';
 
 const AkismetPanel = () => {
-	const { adminUrl } = useSelect( select => {
-		const site = select( coreStore ).getEntityRecord( 'root', 'site' );
-		return {
-			adminUrl: site?.url ? `${ site.url }/wp-admin/` : null,
-		};
-	}, [] );
-
 	return (
 		<PluginIntegrationPanel
-			title={ __( 'Spam Protection', 'jetpack-forms' ) }
+			title={ __( 'Spam protection', 'jetpack-forms' ) }
 			pluginSlug="akismet"
 			pluginPath="akismet/akismet"
-			installText={ __( 'Install Akismet', 'jetpack-forms' ) }
-			activateText={ __( 'Activate Akismet', 'jetpack-forms' ) }
-			description={ __(
-				"Add one-click spam protection for your forms with Akismet. Simply install the plugin and you're set.",
-				'jetpack-forms'
+			installText={ __( 'Install akismet', 'jetpack-forms' ) }
+			activateText={ __( 'Activate akismet', 'jetpack-forms' ) }
+			description={ createInterpolateElement(
+				__(
+					"Add one-click spam protection for your forms with <a>Akismet</a>. Simply install the plugin and you're set.",
+					'jetpack-forms'
+				),
+				{
+					a: (
+						<a
+							href="https://wordpress.org/plugins/akismet/"
+							target="_blank"
+							rel="noopener noreferrer"
+						/>
+					),
+				}
 			) }
 			tracksEventName="jetpack_forms_upsell_akismet_click"
 			initialOpen={ false }
 		>
 			<p className="jetpack-akismet-panel__text">
-				{ __( 'Your forms are protected with Akismet!', 'jetpack-forms' ) }
+				{ createInterpolateElement(
+					__( 'Your forms are protected from spam with <a>Akismet</a>!', 'jetpack-forms' ),
+					{
+						a: (
+							<a
+								href="https://akismet.com/support/getting-started/using-akismet-with-your-contact-forms/"
+								target="_blank"
+								rel="noopener noreferrer"
+							/>
+						),
+					}
+				) }
 			</p>
 			<div className="jetpack-akismet-panel__buttons">
-				{ adminUrl && (
+				{ getSiteAdminUrl() && (
 					<Button
 						variant="secondary"
-						href={ `${ adminUrl }admin.php?page=jetpack-forms#/responses?status=spam` }
+						href={ `${ getSiteAdminUrl() }admin.php?page=jetpack-forms#/responses?status=spam` }
 						target="_blank"
 						rel="noopener noreferrer"
+						__next40pxDefaultSize={ true }
 					>
-						{ __( 'Review Spam', 'jetpack-forms' ) }
+						{ __( 'Review spam', 'jetpack-forms' ) }
 					</Button>
 				) }
-				<Button
-					variant="secondary"
-					href="https://akismet.com"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					{ __( 'Learn More', 'jetpack-forms' ) }
-				</Button>
 			</div>
 		</PluginIntegrationPanel>
 	);
