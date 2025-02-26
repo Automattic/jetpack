f6237dbd853df7946abf955dc6bade3122c1aecb
Jeremy Herve <jeremy@jeremy.hu>
2025-02-26 12:57:59 +0100

    Revert "Admin menu: do not display dashboard switcher twice (#42053)"
    This reverts commit 69492b15f9e7c8eea36a6324c935f31c6fc0147f.

--- projects/packages/masterbar/src/admin-menu/class-base-admin-menu.php
+++ projects/packages/masterbar/src/admin-menu/class-base-admin-menu.php
@@ -540,11 +540,6 @@ abstract class Base_Admin_Menu {
 	 * Adds a dashboard switcher to the list of screen meta links of the current page.
 	 */
 	public function add_dashboard_switcher() {
-		static $is_added = false;
-		if ( $is_added ) {
-			return;
-		}
-
 		$menu_mappings = require __DIR__ . '/menu-mappings.php';
 		$screen        = $this->get_current_screen();
 
@@ -571,8 +566,6 @@ abstract class Base_Admin_Menu {
 			</div>
 		</div>
 		<?php
-
-		$is_added = true;
 	}
 
 	/**
