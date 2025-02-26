741055bf860fa2187dd845f214a305f7267671e5
Donncha O Caoimh <5656673+donnchawp@users.noreply.github.com>
2025-02-26 13:29:17 +0000

    Remove this as it's using jetpack_boost_ds_get

--- projects/plugins/boost/app/modules/optimizations/speculation-rules/Speculation_Rules.php
+++ projects/plugins/boost/app/modules/optimizations/speculation-rules/Speculation_Rules.php
@@ -10,13 +10,6 @@ use Automattic\Jetpack_Boost\Contracts\Optimization;
 use Automattic\Jetpack_Boost\Contracts\Pluggable;
 
 class Speculation_Rules implements Pluggable, Optimization, Changes_Page_Output, Has_Data_Sync {
-	/**
-	 * The data sync instance.
-	 *
-	 * @var Data_Sync
-	 */
-	private $data_sync;
-
 	public static function is_available() {
 		if ( defined( 'JETPACK_BOOST_ALPHA_FEATURES' ) ) {
 			return \JETPACK_BOOST_ALPHA_FEATURES === true;
@@ -31,7 +24,6 @@ class Speculation_Rules implements Pluggable, Optimization, Changes_Page_Output,
 	 * @return void
 	 */
 	public function setup() {
-		$this->data_sync = Data_Sync::get_instance( JETPACK_BOOST_DATASYNC_NAMESPACE );
 		add_action( 'wp_footer', array( $this, 'inject_speculation_rules' ) );
 	}
 
