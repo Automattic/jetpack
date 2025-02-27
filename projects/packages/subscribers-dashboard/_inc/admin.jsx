17f30a252dafef116a02a813f7297f5af98aea3e
Jorge Costa <jorge.costa@developer.pt>
2025-02-27 16:17:56 +0000

    add missing build scripts

--- projects/packages/subscribers-dashboard/_inc/admin.jsx
+++ projects/packages/subscribers-dashboard/_inc/admin.jsx
@@ -8,14 +8,7 @@ import { __ } from '@wordpress/i18n';
 import './style.module.scss';
 
 const JetpackSubscribers = () => {
-	return (
-		<Text>
-			{ __(
-				'Hello world!',
-				"no text domain is set in this in this project's eslint.config.mjs or composer.json"
-			) }
-		</Text>
-	);
+	return <Text>{ __( 'Hello world!', 'jetpack-subscribers-dashboard' ) }</Text>;
 };
 
 /**
