1d3dd529eaaae2f1a34f1583ddf985c513abae12
Aneesh Devasthale <aneesh.devasthale@automattic.com>
2025-02-26 18:18:28 -0800

    revert options changes

--- projects/packages/sync/src/class-defaults.php
+++ projects/packages/sync/src/class-defaults.php
@@ -54,7 +54,6 @@ class Defaults {
 		'default_pingback_flag',
 		'default_post_format',
 		'default_role',
-		'difm_lite_site_options',
 		'disabled_likes',
 		'disabled_reblogs',
 		'disallowed_keys',
