7b519e6a8939b6af9c15cf029cd8f9a4c046c274
sergeymitr <sergeymitr@gmail.com>
2025-02-28 09:15:25 -0500

    Hardcode the next Jetpack version.

--- projects/plugins/jetpack/json-endpoints/jetpack/class.jetpack-json-api-plugins-list-endpoint.php
+++ projects/plugins/jetpack/json-endpoints/jetpack/class.jetpack-json-api-plugins-list-endpoint.php
@@ -6,7 +6,7 @@ new Jetpack_JSON_API_Plugins_List_Endpoint(
 		'method'                  => 'GET',
 		'path'                    => '/sites/%s/plugins',
 		'rest_route'              => '/plugins',
-		'rest_min_jp_version'     => '$$next-version$$',
+		'rest_min_jp_version'     => '14.4',
 		'stat'                    => 'plugins',
 		'min_version'             => '1',
 		'max_version'             => '1.1',
