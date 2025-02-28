228aa4a39547fed7bcbcbb1fffaa2c37f09ca33d
Anton Vlasenko <vlasenko.anton@gmail.com>
2025-02-28 01:33:09 +0100

    As $menu_item can be null due to combination of plugins/themes, its value must be checked before passing it to the ::prepare_submenu_item method

--- projects/plugins/jetpack/_inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-admin-menu.php
+++ projects/plugins/jetpack/_inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-admin-menu.php
@@ -123,6 +123,11 @@ class WPCOM_REST_API_V2_Endpoint_Admin_Menu extends WP_REST_Controller {
 
 				// Add submenu items.
 				foreach ( $submenu_items as $submenu_item ) {
+					// As $menu_item can be null due to combination of plugins/themes,
+					// its value must be checked before passing it to the ::prepare_submenu_item method.
+					if ( ! is_array( $submenu_item ) ) {
+						continue;
+					}
 					$submenu_item = $this->prepare_submenu_item( $submenu_item, $menu_item );
 					if ( ! empty( $submenu_item ) ) {
 						$item['children'][] = $submenu_item;
