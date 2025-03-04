758c6f8162135b218b8d46d534605a5d464a8dc4
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2025-03-04 10:45:54 -0700

    Remove unnecessary class initiation

--- projects/packages/my-jetpack/src/class-initializer.php
+++ projects/packages/my-jetpack/src/class-initializer.php
@@ -519,7 +519,6 @@ class Initializer {
 		new REST_Zendesk_Chat();
 		new REST_AI();
 		new REST_Recommendations_Evaluation();
-		new Historically_Active_Modules();
 
 		Products::register_product_endpoints();
 		Historically_Active_Modules::register_rest_endpoints();
