960be2093e75b60394a60ac39149a6e851bb5862
Enej Bajgoric <enej.bajgoric@automattic.com>
2025-03-05 11:11:24 -0800

    Add endpoint example tests

---
+++ projects/packages/forms/tests/php/contact-form/Contact_Form_Endpoint_Test.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Automattic\Jetpack\Forms\ContactForm;
+
+use PHPUnit\Framework\TestCase;
+use WorDBless\Options as WorDBless_Options;
+use WorDBless\Users as WorDBless_Users;
+use WP_REST_Request;
+use WP_REST_Server;
+
+/**
+ * Unit tests for the REST API endpoints.
+ *
+ * To run this test, you can use the following command: (from the projects/packages/forms directory)
+ *
+ * composer test-php tests/php/contact-form/Contact_Form_Endpoint_Test.php
+ */
+class Contact_Form_Endpoint_Test extends TestCase {
+
+	/**
+	 * REST Server object.
+	 *
+	 * @var WP_REST_Server
+	 */
+	private $server;
+
+	/**
+	 * The current user id.
+	 *
+	 * @var int
+	 */
+	private static $user_id;
+
+	private $plugin;
+
+	/**
+	 * Setting up the test.
+	 *
+	 * @before
+	 */
+	public function set_up() {
+		global $wp_rest_server;
+
+		$this->plugin = Contact_Form_Plugin::init();
+
+		$wp_rest_server = new WP_REST_Server();
+		$this->server   = $wp_rest_server;
+
+		do_action( 'rest_api_init' );
+
+		self::$user_id = wp_insert_user(
+			array(
+				'user_login' => 'test_admin',
+				'user_pass'  => '123',
+				'role'       => 'administrator',
+			)
+		);
+		wp_set_current_user( self::$user_id );
+	}
+
+	/**
+	 * Returning the environment into its initial state.
+	 *
+	 * @after
+	 */
+	public function tear_down() {
+		WorDBless_Options::init()->clear_options();
+		WorDBless_Users::init()->clear_all_users();
+
+		unset( $_SERVER['REQUEST_METHOD'] );
+		$_GET = array();
+	}
+
+	/**
+	 * Test GET feedback/filters
+	 */
+	public function test_get_feedbacks_filters_returns_200() {
+		$request  = new WP_REST_Request( 'GET', '/wp/v2/feedback/filters' );
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( 200, $response->get_status() );
+	}
+
+	/**
+	 * Test GET feedback/filters unautorized.
+	 */
+	public function test_get_feedbacks_filters_returns_401() {
+		wp_set_current_user( 0 );
+		$request  = new WP_REST_Request( 'GET', '/wp/v2/feedback/filters' );
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( 401, $response->get_status() );
+	}
+}
