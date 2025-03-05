234ac7b13918970175af668298ddd0d2034c515f
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-03-05 12:06:38 +0530

    Wire up jetpackSidebarIsOpen for social sidebar

--- projects/js-packages/publicize-components/src/components/block-editor/social-sidebar.tsx
+++ projects/js-packages/publicize-components/src/components/block-editor/social-sidebar.tsx
@@ -1,8 +1,21 @@
 import { SocialIcon } from '@automattic/jetpack-components';
+import { dispatch } from '@wordpress/data';
+import domReady from '@wordpress/dom-ready';
 import { PluginSidebar } from '@wordpress/editor';
+import { store as interfaceStore } from '@wordpress/interface';
 import { registerPlugin } from '@wordpress/plugins';
+import { getQueryArg } from '@wordpress/url';
 import { SocialPanels } from './components/social-panels';
 import { SocialSettings } from './components/social-settings';
+
+/**
+ * Open Jetpack Social sidebar by default when URL includes jetpackSidebarIsOpen=true.
+ */
+domReady( () => {
+	if ( getQueryArg( window.location.search, 'jetpackSidebarIsOpen' ) === 'true' ) {
+		dispatch( interfaceStore ).enableComplementaryArea( 'core', 'jetpack-social/jetpack-social' );
+	}
+} );
 
 /**
  * Social sidebar for the social plugin
