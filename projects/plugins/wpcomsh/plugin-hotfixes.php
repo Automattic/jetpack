8be3bc864c61401df1d37bff6c7708e96e23c300
Thom Stackhouse <81396999+hideandgeek404@users.noreply.github.com>
2025-03-05 12:08:34 +0000

    Update plugin-hotfixes.php
    Moving out of single-purpose file and into existing hotfix file

--- projects/plugins/wpcomsh/plugin-hotfixes.php
+++ projects/plugins/wpcomsh/plugin-hotfixes.php
@@ -138,3 +138,26 @@ if ( ! function_exists( 'get_magic_quotes_gpc' ) ) {
 		return false;
 	}
 }
+
+/**
+ * Use filter to remove the Elementor Developer Notice.
+ *
+ * @param array $notices The array of admin notices from Elementor.
+ * @return array Modified array without the Elementor Developer Notice.
+ */
+function wpcomsh_remove_elementor_dev_notice( $notices ) {
+
+    if ( is_array( $notices ) ) {
+        if ( class_exists( 'Elementor\Core\Admin\Notices\Elementor_Dev_Notice' ) ) {
+            foreach ( $notices as $key => $notice ) {
+                if ( $notice instanceof Elementor\Core\Admin\Notices\Elementor_Dev_Notice ) {
+                    unset( $notices[ $key ] );
+                }
+            }
+        }
+    }
+
+    return $notices;
+}
+
+add_filter( 'elementor/core/admin/notices', 'wpcomsh_remove_elementor_dev_notice' );
