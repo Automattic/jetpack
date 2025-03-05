21662bbbe1f8a6611edf7a3d0edcb9c6ffd167fe
Grzegorz Chudzinski-Pawlowski <112354940+grzegorz-cp@users.noreply.github.com>
2025-03-05 15:10:59 +1100

    Boost: updating modal with Boost specific price structure

--- projects/plugins/boost/app/assets/src/js/pages/index/index.tsx
+++ projects/plugins/boost/app/assets/src/js/pages/index/index.tsx
@@ -19,6 +19,7 @@ import { recordBoostEvent } from '$lib/utils/analytics';
 import { Notice, getRedirectUrl } from '@automattic/jetpack-components';
 import { createInterpolateElement } from '@wordpress/element';
 import { ProductInterstitialMyJetpack } from '@automattic/jetpack-my-jetpack/components/product-interstitial-modal';
+import boostImage from '@automattic/jetpack-my-jetpack/components/product-interstitial/boost.png';
 import { __ } from '@wordpress/i18n';
 import styles from './index.module.scss';
 
