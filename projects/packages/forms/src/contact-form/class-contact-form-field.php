6a8b6d6c718c47bbf1ddfbc3d38af74ede2506a8
Enej Bajgoric <enej.bajgoric@automattic.com>
2025-03-05 16:12:10 -0800

    Make the request to .com

--- projects/packages/forms/src/contact-form/class-contact-form-field.php
+++ projects/packages/forms/src/contact-form/class-contact-form-field.php
@@ -902,11 +902,22 @@ class Contact_Form_Field extends Contact_Form_Shortcode {
 					'invalidType'        => __( 'This file type is not allowed', 'jetpack-forms' ),
 				),
 				'maxUploadSize'  => $max_file_size,
-				'uploadEndpoint' => rest_url( 'wpcom/v2/unauth-file-upload' ),
+				'uploadEndpoint' => $this->get_unauth_endpoint_url(),
 				'wp_nonce'       => wp_create_nonce( 'wp_rest' ),
 				'jp_nonce'       => wp_create_nonce( 'jetpack_file_upload_jetpack-form' ),
 			)
 		);
+	}
+	/**
+	 * Returns the URL for the unauthenticated file upload endpoint.
+	 *
+	 * @return string
+	 */
+	private function get_unauth_endpoint_url() {
+		if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
+			return sprintf( 'https://public-api.wordpress.com/wpcom/v2/sites/%d/unauth-file-upload', get_current_blog_id() );
+		}
+		return rest_url( 'wpcom/v2/unauth-file-upload' );
 	}
 
 	/**
