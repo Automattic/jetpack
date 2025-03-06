a7a46817e387e171b668a366eb9e847e282e9858
ntsekouras <ntsekouras@outlook.com>
2025-03-06 10:32:17 +0200

    update deps

--- projects/packages/forms/src/dashboard/components/layout/index.js
+++ projects/packages/forms/src/dashboard/components/layout/index.js
@@ -1,13 +1,10 @@
 import { JetpackFooter } from '@automattic/jetpack-components';
-import // __experimentalHStack as HStack, // eslint-disable-line @wordpress/no-unsafe-wp-apis
-'@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import clsx from 'clsx';
 import JetpackFormsLogo from '../logo';
 
 import './style.scss';
 
-// TODO: cleanup. It might not be needed at all..
 const Layout = ( { children, className, showFooter } ) => {
 	return (
 		<div className={ clsx( 'jp-forms__layout', className ) }>
