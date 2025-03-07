6a62248e9c0946d4a3a9a3f107b925a1f3fcb042
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2025-03-07 11:01:57 -0700

    Remove unused extra optin in useSimpleQuery

--- projects/packages/my-jetpack/_inc/data/use-simple-query.ts
+++ projects/packages/my-jetpack/_inc/data/use-simple-query.ts
@@ -23,7 +23,7 @@ import type { APIFetchOptions } from '@wordpress/api-fetch';
 type QueryParams = {
 	name: string;
 	query: APIFetchOptions;
-	options?: Pick< UseQueryOptions, 'enabled' | 'gcTime' >;
+	options?: Pick< UseQueryOptions, 'enabled' >;
 	errorMessage?: string;
 };
 const useSimpleQuery = < T >( { name, query, options, errorMessage }: QueryParams ) => {
