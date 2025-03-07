f06655c24aafd0be3bab39fcd205ebd9f111ef7a
Chris Holder <chris.holder@automattic.com>
2025-03-07 11:41:16 -0600

    Change args so Phan is happy

--- projects/plugins/jetpack/class-jetpack-newsletter-dashboard-widget.php
+++ projects/plugins/jetpack/class-jetpack-newsletter-dashboard-widget.php
@@ -108,8 +108,9 @@ class Jetpack_Newsletter_Dashboard_Widget {
 				self::$widget_id,
 				$widget_title,
 				array( static::class, 'render' ),
+				// @phan-suppress-next-line PhanTypeMismatchArgumentProbablyReal -- Core should ideally document null for no-callback arg. https://core.trac.wordpress.org/ticket/52539.
 				null,
-				null,
+				array(),
 				'side',
 				'high'
 			);
