7b82e889d049f929792e6aafcc92337270cec9f8
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2025-03-11 09:05:56 -0600

    Check for loading/error status instead of the presence of data in use product refetch

--- projects/packages/my-jetpack/_inc/data/products/use-products.ts
+++ projects/packages/my-jetpack/_inc/data/products/use-products.ts
@@ -26,10 +26,10 @@ const refetchProducts = async (
 		options?: RefetchOptions
 	) => Promise< QueryObserverResult< { [ key: string ]: ProductSnakeCase }, WP_Error > >
 ) => {
-	const { data: refetchedProducts } = await refetch();
+	const { data: refetchedProducts, isError, isLoading } = await refetch();
 	const prevProducts = window.myJetpackInitialState.products.items;
 
-	if ( ! refetchedProducts ) {
+	if ( isError || isLoading ) {
 		return;
 	}
 
