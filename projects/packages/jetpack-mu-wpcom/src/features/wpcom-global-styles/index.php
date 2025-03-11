d8a3b20ce84f219e377f490531964669d872c856
ramon <ramonjd@gmail.com>
2025-03-11 14:18:43 +1100

    increase size of toggle and reduce delete styles button size

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-global-styles/index.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-global-styles/index.php
@@ -642,17 +642,20 @@ function wpcom_display_global_styles_launch_bar() {
 							>
 								<?php echo esc_html__( 'Upgrade and keep premium styles', 'jetpack-mu-wpcom' ); ?>
 							</a>
-								<?php
+							<?php
 						} else {
 							?>
 							<a
 								class="launch-bar-global-styles__button--upgrade"
 								href="<?php echo esc_url( $upgrade_url ); ?>"
 							>
-								<?php echo esc_html__( 'Upgrade and keep premium styles', 'jetpack-mu-wpcom' ); ?>
+								<?php echo esc_html__( 'Upgrade to unlock premium styles', 'jetpack-mu-wpcom' ); ?>
 							</a>
-							<a
-								class="launch-bar-global-styles__button--reset"
+							<?php
+						}
+						?>
+						<a
+							class="launch-bar-global-styles__button--reset"
 								href="https://wordpress.com/support/using-styles/#reset-all-styles"
 								target="_blank"
 								data-blog-id="<?php echo esc_attr( (string) $blog_id ); ?>"
@@ -661,9 +664,6 @@ function wpcom_display_global_styles_launch_bar() {
 								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" aria-hidden="true" focusable="false"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 5.5A2.25 2.25 0 0 0 9.878 7h4.244A2.251 2.251 0 0 0 12 5.5ZM12 4a3.751 3.751 0 0 0-3.675 3H5v1.5h1.27l.818 8.997a2.75 2.75 0 0 0 2.739 2.501h4.347a2.75 2.75 0 0 0 2.738-2.5L17.73 8.5H19V7h-3.325A3.751 3.751 0 0 0 12 4Zm4.224 4.5H7.776l.806 8.861a1.25 1.25 0 0 0 1.245 1.137h4.347a1.25 1.25 0 0 0 1.245-1.137l.805-8.861Z"></path></svg>
 								<?php echo esc_html__( 'Remove premium styles', 'jetpack-mu-wpcom' ); ?>
 							</a>
-								<?php
-						}
-						?>
 						</div>
 					</div>
 					<div class="launch-bar-global-styles-toggle">
