c3154c2ddfd98d20685f7081c11b3b19ecfd24df
Gergely Juhasz <gergely.juhasz@automattic.com>
2025-03-11 18:57:19 +0100

    Update SharePostButton props + add common hook for isResharingPossible

---
+++ projects/js-packages/publicize-components/src/hooks/use-is-resharing-possible/index.ts
@@ -0,0 +1,35 @@
+import { useSelect } from '@wordpress/data';
+import { store as editorStore } from '@wordpress/editor';
+import { useIsSharingPossible } from '../use-is-sharing-possible';
+import usePublicizeConfig from '../use-publicize-config';
+import useSharePost from '../use-share-post';
+
+/**
+ * Returns whether re-sharing is possible currently.
+ *
+ * False when
+ * - sharing is disabled
+ * - no enabled connections
+ * - post is not published
+ * - is sharing post
+ *
+ * @return {boolean} Whether re-sharing is possible.
+ */
+export function useIsReSharingPossible() {
+	const { isPublicizeEnabled } = usePublicizeConfig();
+	const isSharingPossible = useIsSharingPossible();
+	const { isFetching } = useSharePost();
+
+	const { isPostPublished, isSavingPost } = useSelect( select => {
+		const editorSelector = select( editorStore );
+
+		return {
+			isPostPublished: editorSelector.isCurrentPostPublished(),
+			isSavingPost: editorSelector.isSavingPost(),
+		};
+	}, [] );
+
+	return (
+		isPublicizeEnabled && isSharingPossible && isPostPublished && ! isFetching && ! isSavingPost
+	);
+}
