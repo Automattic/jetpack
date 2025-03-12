94698b5814b1bf809cf590154056e6e368ae0d38
Gergely Juhasz <gergely.juhasz@automattic.com>
2025-03-12 13:15:54 +0100

    Move notice handling to the action

--- projects/js-packages/publicize-components/src/hooks/use-schedule-post/index.tsx
+++ projects/js-packages/publicize-components/src/hooks/use-schedule-post/index.tsx
@@ -2,8 +2,6 @@ import { siteHasFeature } from '@automattic/jetpack-script-data';
 import { dispatch, useDispatch, useSelect } from '@wordpress/data';
 import { store as editorStore } from '@wordpress/editor';
 import { useCallback, useState } from '@wordpress/element';
-import { __ } from '@wordpress/i18n';
-import { store as noticesStore } from '@wordpress/notices';
 import { store as socialStore } from '../../social-store';
 import { features } from '../../utils';
 import useSocialMediaMessage from '../use-social-media-message';
@@ -36,7 +34,6 @@ export function useSchedulePost() {
 	}, [] );
 
 	const { createScheduledShare } = useDispatch( socialStore );
-	const { createSuccessNotice, createErrorNotice } = useDispatch( noticesStore );
 	const { message } = useSocialMediaMessage();
 	const hasMediaFeatures =
 		siteHasFeature( features.IMAGE_GENERATOR ) || siteHasFeature( features.ENHANCED_PUBLISHING );
@@ -61,29 +58,19 @@ export function useSchedulePost() {
 			}
 
 			try {
-				// Process each connection separately, one at a time
-				for ( const connectionId of connectionIds ) {
-					await createScheduledShare( {
-						post_id: postId,
-						connection_id: connectionId,
-						message,
-						timestamp,
-					} );
-				}
-
-				createSuccessNotice(
-					__( 'Social post scheduled successfully.', 'jetpack-publicize-components' ),
-					{ type: 'snackbar' }
+				await Promise.all(
+					connectionIds.map( connection_id => {
+						return createScheduledShare( {
+							post_id: postId,
+							connection_id,
+							message,
+							timestamp,
+						} );
+					} )
 				);
-
 				setIsScheduling( false );
 				return true;
 			} catch {
-				createErrorNotice(
-					__( 'Failed to schedule social post.', 'jetpack-publicize-components' ),
-					{ type: 'snackbar' }
-				);
-
 				setIsScheduling( false );
 				return false;
 			}
@@ -93,11 +80,9 @@ export function useSchedulePost() {
 			isAutosaveablePost,
 			hasMediaFeatures,
 			savePost,
-			createSuccessNotice,
 			createScheduledShare,
 			postId,
 			message,
-			createErrorNotice,
 		]
 	);
 
