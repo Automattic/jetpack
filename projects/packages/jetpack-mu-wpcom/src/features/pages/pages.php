ce2340e087b62c7bc5d6bb515b43452e91d0c1eb
Antonio Botic <abotic00@gmail.com>
2025-03-13 00:50:05 +0100

    final fixes

--- projects/packages/jetpack-mu-wpcom/src/features/pages/pages.php
+++ projects/packages/jetpack-mu-wpcom/src/features/pages/pages.php
@@ -1,4 +1,4 @@
 <?php // phpcs:ignore Squiz.Commenting.FileComment.Missing
 
 require_once __DIR__ . '/quick-actions.php';
-require_once __DIR__ . '/homepage-notice.php';
+require_once __DIR__ . '/pages-homepage-connection-banner.php';
