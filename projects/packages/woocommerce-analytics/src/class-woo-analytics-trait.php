639875d9fc4d80670c901f428bb99a99ba466528
Miguel Pérez Pellicer <5908855+puntope@users.noreply.github.com>
2025-03-13 13:27:42 +0400

    [WooCommerce Analytics] Fix update cart not being triggered (#42369)
    * Fix Update Cart not triggering

--- projects/packages/woocommerce-analytics/src/class-woo-analytics-trait.php
+++ projects/packages/woocommerce-analytics/src/class-woo-analytics-trait.php
@@ -67,6 +67,14 @@ trait Woo_Analytics_Trait {
 	protected $landing_page = '';
 
 	/**
+	 *  Locks Add to Cart Events Tracking in the current request avoiding duplications.
+	 *  i.e If update_cart and add_to_cart actions happens in the same request.
+	 *
+	 *  @var bool If true. Cart events are locked for the current request.
+	 */
+	protected $lock_add_to_cart_events = false;
+
+	/**
 	 * Format Cart Items or Order Items to an array
 	 *
 	 * @param array|WC_Order_Item[] $items Cart Items or Order Items.
