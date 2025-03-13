77492ac375c43ed5616dd6be6fe0fd4a7a514d3a
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2025-03-13 14:15:08 -0600

    Remove waf package and ignore phan errors

--- projects/packages/my-jetpack/src/products/class-protect.php
+++ projects/packages/my-jetpack/src/products/class-protect.php
@@ -418,9 +418,12 @@ class Protect extends Hybrid_Product {
 		$is_waf_enabled = false;
 
 		if ( class_exists( 'Automattic\Jetpack\Waf\Waf_Runner' ) ) {
-			$waf_config     = Waf_Runner::get_config();
+			// @phan-suppress-next-line PhanUndeclaredClassMethod
+			$waf_config = Waf_Runner::get_config();
+			// @phan-suppress-next-line PhanUndeclaredClassMethod
 			$is_waf_enabled = Waf_Runner::is_enabled();
-			$waf_supported  = Waf_Runner::is_supported_environment();
+			// @phan-suppress-next-line PhanUndeclaredClassMethod
+			$waf_supported = Waf_Runner::is_supported_environment();
 		}
 
 		return rest_ensure_response(
