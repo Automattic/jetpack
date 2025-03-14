21691bdf9386ef42bb33647859246b7ad743ffc3
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-03-14 16:48:22 +0530

    Init endpoint class

---
+++ projects/packages/publicize/src/rest-api/class-social-image-generator-controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Publicize: Social Image Generator Controller
+ *
+ * @package automattic/jetpack-publicize
+ */
+
+namespace Automattic\Jetpack\Publicize\REST_API;
+
+/**
+ * Publicize: Social Image Generator Controller class.
+ */
+class Social_Image_Generator_Controller extends Base_Controller {
+}
