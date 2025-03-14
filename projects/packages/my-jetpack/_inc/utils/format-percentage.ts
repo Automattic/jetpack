1155060a94a153c9182586b819268a769e3a3b3d
Kevin Zoschke <kevin.zoschke@automattic.com>
2025-03-14 15:28:54 -0400

    Document formatPercentage

--- projects/packages/my-jetpack/_inc/utils/format-percentage.ts
+++ projects/packages/my-jetpack/_inc/utils/format-percentage.ts
@@ -5,13 +5,20 @@ const defaultConfig: Intl.NumberFormatOptions = {
 	style: 'percent',
 };
 
+/**
+ * Formats a number as a percentage string.
+ *
+ * @param {number}                   number   - The number to format (e.g. 0.5 for 50%)
+ * @param {Intl.NumberFormatOptions} [config] - Optional formatting configuration
+ * @return {string} The formatted percentage string (e.g. "50%")
+ */
 const formatPercentage: FormatNumberFunction = ( number, config = defaultConfig ) => {
-	// Force percentage
-	config.style = 'percent';
-
 	if ( number === null || ! Number.isFinite( number ) ) {
 		return '-';
 	}
+
+	// Force percentage
+	config.style = 'percent';
 
 	return numberFormat( number, config );
 };
