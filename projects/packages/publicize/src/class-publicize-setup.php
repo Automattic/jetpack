2f9267396d0d68c91a6c9da7c4d358b7cd9358f0
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-03-14 17:33:55 +0530

    POC: SIG on WPCOM

--- projects/packages/publicize/src/class-publicize-setup.php
+++ projects/packages/publicize/src/class-publicize-setup.php
@@ -43,6 +43,7 @@ class Publicize_Setup {
 			REST_API\Scheduled_Actions_Controller::class,
 			REST_API\Services_Controller::class,
 			REST_API\Share_Post_Controller::class,
+			REST_API\Social_Image_Generator_Controller::class,
 		);
 
 		// Load the REST controllers.
