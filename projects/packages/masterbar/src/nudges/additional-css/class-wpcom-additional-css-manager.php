5f13679b007a965ea22e15ecf8cc49b0f3270e2e
Richard Ortiz <rcrd.ortiz@gmail.com>
2025-03-14 16:58:20 +0100

    Global Styles: Fix Additional CSS not upselling to correct plan

--- projects/packages/masterbar/src/nudges/additional-css/class-wpcom-additional-css-manager.php
+++ projects/packages/masterbar/src/nudges/additional-css/class-wpcom-additional-css-manager.php
@@ -66,7 +66,8 @@ class WPCOM_Additional_CSS_Manager {
 	 * @return mixed
 	 */
 	protected function get_plan() {
-		$plan_slug = class_exists( 'WPCOM_Feature_Flags' ) && \WPCOM_Feature_Flags::is_enabled( \WPCOM_Feature_Flags::GLOBAL_STYLES_ON_PERSONAL_PLAN ) ? 'personal-bundle' : 'value_bundle';
+
+		$plan_slug = wpcom_site_has_global_styles_in_personal_plan() ? 'personal-bundle' : 'value_bundle';
 
 		return \Automattic\Jetpack\Plans::get_plan( $plan_slug );
 	}
