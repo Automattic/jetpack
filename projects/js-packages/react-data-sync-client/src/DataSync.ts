fe3d44725ec611ed2b57c685ad43f2158afb0d66
Peter Petrov <peter.petrov89@gmail.com>
2025-03-17 10:18:35 +0200

    Update error message
    Co-authored-by: Adnan Haque <3737780+haqadn@users.noreply.github.com>

--- projects/js-packages/react-data-sync-client/src/DataSync.ts
+++ projects/js-packages/react-data-sync-client/src/DataSync.ts
@@ -345,7 +345,11 @@ export class DataSync< Schema extends z.ZodSchema, Value extends z.infer< Schema
 			url = this.maybeRequestDisabled( url );
 			const result = await fetch( url, args );
 			if ( ! result.ok ) {
-				throw new DataSyncError( `DS request failed: ${ result.status } - ${ result.statusText }`, {
+				let errorText = result.status;
+				if( result.statusText ) {
+					errorText = `${errorText} - ${result.statusText}`;
+				}
+				throw new DataSyncError( `DS request failed: ${errorText}`, {
 					...this.describeSelf(),
 					method: args.method,
 					location: url,
