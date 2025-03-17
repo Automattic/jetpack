db2557d2ce073f9a8c6ee255e02e14c4ef9249cf
Karen Attfield <karenlattfield@gmail.com>
2025-03-17 11:26:46 +0000

    Change admin_menu pri to prevent issues with Akismet menu item loading

--- projects/packages/backup/src/class-jetpack-backup.php
+++ projects/packages/backup/src/class-jetpack-backup.php
@@ -119,7 +119,8 @@ class Jetpack_Backup {
 
 		add_action( 'rest_api_init', array( __CLASS__, 'register_rest_routes' ) );
 
-		add_action( 'admin_menu', array( __CLASS__, 'add_wp_admin_submenu' ), 999 ); // Jetpack uses 998 and 'Admin_Menu' uses 1000, so we need to use 999.
+		// Register menu on init to ensure textdomain is properly loaded
+		add_action( 'admin_menu', array( __CLASS__, 'add_wp_admin_submenu' ), 1 ); // Akismet uses 4, so we need to use 1 to ensure both menus are added when only they exist.
 
 		// Init Jetpack packages.
 		add_action(
