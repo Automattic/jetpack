71f2f50ebf744d87dbc7ad497e82d24e96a1b765
dkmyta <dkmyta10@gmail.com>
2025-03-18 11:20:12 -0700

    Fix logo rendering

--- projects/packages/account-protection/src/class-password-detection.php
+++ projects/packages/account-protection/src/class-password-detection.php
@@ -302,7 +302,10 @@ class Password_Detection {
 			</head>
 			<body class="<?php echo esc_attr( $body_classes ); ?>">
 				<div class="password-detection-content">
-					<?php echo esc_html( ( new Jetpack_Logo() )->get_jp_emblem( true ) ); ?>
+					<?php
+						// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+						echo ( new Jetpack_Logo() )->get_jp_emblem( true );
+					?>
 					<p class="password-detection-title"><?php echo $success_data['code'] === 'auth_code_success' ? esc_html__( 'Take action to stay secure', 'jetpack-account-protection' ) : esc_html__( 'Verify your identity', 'jetpack-account-protection' ); ?></p>
 					<?php if ( $error_data['message'] ) : ?>
 						<div class="error notice">
