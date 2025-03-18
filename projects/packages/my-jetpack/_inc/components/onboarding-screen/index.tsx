b15ce8f8577d40742fe108a8c4e9b2895fee859c
Ian Ramos <5714212+IanRamosC@users.noreply.github.com>
2025-03-17 23:12:34 -0300

    My Jetpack: add dummy content to onboarding screen

--- projects/packages/my-jetpack/_inc/components/onboarding-screen/index.tsx
+++ projects/packages/my-jetpack/_inc/components/onboarding-screen/index.tsx
@@ -1,18 +1,25 @@
 import { Container, Col, JetpackLogo } from '@automattic/jetpack-components';
 import { useFullScreen } from '../../hooks/use-fullscreen';
+import ConnectionsSection from '../connections-section';
+import exampleImage from './grow-your-audience.png';
 import styles from './styles.module.scss';
 import type { FC } from 'react';
+
 const OnboardingScreen: FC = () => {
 	useFullScreen( [] );
 
 	return (
-		<Container horizontalSpacing={ 3 } horizontalGap={ 0 }>
+		<Container
+			horizontalSpacing={ 3 }
+			horizontalGap={ 0 }
+			className={ styles[ 'onboarding-screen' ] }
+		>
 			<JetpackLogo height={ 24 } showText={ false } className={ styles[ 'jetpack-logo' ] } />
-			<Col sm={ 4 } md={ 4 } lg={ 6 }>
-				<h1>Column 1</h1>
+			<Col sm={ 4 } md={ 4 } lg={ 6 } className={ styles[ 'primary-column' ] }>
+				<ConnectionsSection />
 			</Col>
-			<Col sm={ 4 } md={ 4 } lg={ 6 }>
-				<h1>Column 2</h1>
+			<Col sm={ 4 } md={ 4 } lg={ 6 } className={ styles[ 'seconday-column' ] }>
+				<img src={ exampleImage } alt="Onboarding Screen" />
 			</Col>
 		</Container>
 	);
