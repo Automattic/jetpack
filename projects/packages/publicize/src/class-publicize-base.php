f4d5400d304a72fd4a82fdc756930d03b60b873e
Gergely Juhasz <gergely.juhasz@automattic.com>
2025-03-19 19:04:57 +0100

    Move get_post_share_status to Publicize Base

--- projects/packages/publicize/src/class-publicize-base.php
+++ projects/packages/publicize/src/class-publicize-base.php
@@ -2100,6 +2100,48 @@ abstract class Publicize_Base {
 	public static function can_manage_connection( $connection_data ) {
 		return current_user_can( 'edit_others_posts' ) || get_current_user_id() === (int) $connection_data['user_id'];
 	}
+
+	/**
+	 * Gets the share status for a post.
+	 *
+	 * @param int $post_id The post ID.
+	 */
+	public function get_post_share_status( $post_id ) {
+		$shares = get_post_meta( $post_id, REST_Controller::SOCIAL_SHARES_POST_META_KEY, true );
+
+		// If the data is in an associative array format, we fetch it without true to get all the shares.
+		// This is needed to support the old WPCOM format.
+		if ( isset( $shares ) && is_array( $shares ) && ! array_is_list( $shares ) ) {
+			$shares = get_post_meta( $post_id, REST_CONTROLLER::SOCIAL_SHARES_POST_META_KEY );
+		}
+
+		// If the data is not an array, it means that sharing is not done yet.
+		$done = is_array( $shares );
+
+		if ( $done ) {
+			// The site could have multiple admins, editors and authors connected. Load shares information that only the current user has access to.
+			$connection_ids = wp_list_pluck( Connections::get_all_for_user(), 'connection_id', 'connection_id' );
+
+			$shares = array_filter(
+				$shares,
+				function ( $share ) use ( $connection_ids ) {
+					return isset( $connection_ids[ $share['connection_id'] ] );
+				}
+			);
+
+			usort(
+				$shares,
+				function ( $a, $b ) {
+					return $b['timestamp'] - $a['timestamp'];
+				}
+			);
+		}
+
+		return array(
+			'shares' => $done ? $shares : array(),
+			'done'   => $done,
+		);
+	}
 }
 
 // phpcs:disable Universal.Files.SeparateFunctionsFromOO.Mixed -- TODO: Move these functions to some other file.
--- projects/packages/publicize/src/class-publicize.php
+++ projects/packages/publicize/src/class-publicize.php
@@ -736,40 +736,4 @@ class Publicize extends Publicize_Base {
 
 		return $flags;
 	}
-
-	/**
-	 * Gets the share status for a post.
-	 *
-	 * @param int $post_id The post ID.
-	 */
-	public function get_post_share_status( $post_id ) {
-		$shares = get_post_meta( $post_id, REST_Controller::SOCIAL_SHARES_POST_META_KEY, true );
-
-		// If the data is not an array, it means that sharing is not done yet.
-		$done = is_array( $shares );
-
-		if ( $done ) {
-			// The site could have multiple admins, editors and authors connected. Load shares information that only the current user has access to.
-			$connection_ids = wp_list_pluck( Connections::get_all_for_user(), 'connection_id', 'connection_id' );
-
-			$shares = array_filter(
-				$shares,
-				function ( $share ) use ( $connection_ids ) {
-					return isset( $connection_ids[ $share['connection_id'] ] );
-				}
-			);
-
-			usort(
-				$shares,
-				function ( $a, $b ) {
-					return $b['timestamp'] - $a['timestamp'];
-				}
-			);
-		}
-
-		return array(
-			'shares' => $done ? $shares : array(),
-			'done'   => $done,
-		);
-	}
 }
