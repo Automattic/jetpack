7b10b099df5847d1d24686e4f57c8e202106de57
Jerry Jones <jones.jeremydavid@gmail.com>
2025-03-19 15:56:58 -0500

    Consolodate fiverr logo tool upsell from wpcom and wpcomsh into one location

--- projects/packages/jetpack-mu-wpcom/src/features/logo-tool/js/customizer.js
+++ projects/packages/jetpack-mu-wpcom/src/features/logo-tool/js/customizer.js
@@ -1,24 +1,12 @@
-/**
- * IMPORTANT: All changes in this plugin should be synced between wpcom (Simple Sites) and wpcomsh (Atomic Sites).
- */
-
-/* global wp, _LogoTool_ */
-/**
- * Logo tool customizer object.
- *
- * @param {object} $        - jQuery.
- * @param {object} wp       - WordPress.
- * @param {object} LogoTool - LogoTool.
- */
 ( function ( $, wp, LogoTool ) {
 	wp.customize.bind( 'ready', function () {
-		let logoThumbnail;
-		const logoControlId = '#customize-control-' + LogoTool.controlId;
+		let logoThumbnail,
+			logoControlId = '#customize-control-' + LogoTool.controlId;
 
 		// Could be a Core custom-logo, Jetpack site-logo, or a theme specific logo that uses the same image control.
 		if ( wp.customize( LogoTool.settingId ) ) {
 			logoThumbnail = $( logoControlId + ' .thumbnail' );
-			wp.customize( LogoTool.settingId ).bind( 'change', function ( to ) {
+			wp.customize( LogoTool.settingId ).bind( 'change', function ( to, from ) {
 				if ( ! to ) {
 					insertLogoButton( logoControlId );
 					showLogoDescription( logoControlId );
@@ -39,15 +27,16 @@
 	} );
 
 	/**
-	 * Inserts the logo button.
 	 *
-	 * @param {string} id - the ID of the logo element.
+	 * @param id
 	 */
 	function insertLogoButton( id ) {
+		const externalIcon =
+			'<svg class="external-link-icon" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.97949 2.91699H4.91699C3.81242 2.91699 2.91699 3.81242 2.91699 4.91699V9.08366C2.91699 10.1882 3.81242 11.0837 4.91699 11.0837H9.08366C10.1882 11.0837 11.0837 10.1882 11.0837 9.08366V7.79431" stroke="#6BA774"/><path d="M8.68922 2.20873L11.9253 2.19411M11.9253 2.19411L11.8976 5.41706M11.9253 2.19411C10.5504 3.56904 8.76465 5.35476 7.44471 6.67469" stroke="#6BA774"/></svg>';
 		const button = $(
-			'<a class="button create-logo-button" target="_blank" href="https://wp.me/logo-maker" />'
+			'<a class="button create-logo-button" target="_blank" href="https://wp.me/logo-maker/?utm_campaign=customizer" />'
 		).text( LogoTool.l10n.create );
-
+		button.prepend( externalIcon );
 		// Timeout lets us render after the core control finishes.
 		setTimeout( function () {
 			$( id + ' .actions' ).prepend( button );
@@ -55,16 +44,24 @@
 	}
 
 	/**
-	 * Shows logo description.
-	 * @param {string} id - description element ID.
+	 *
+	 * @param id
+	 */
+	function removeLogoButton( id ) {
+		$( id + ' .create-logo-button' ).remove();
+	}
+
+	/**
+	 *
+	 * @param id
 	 */
 	function showLogoDescription( id ) {
 		$( id + ' .description' ).show();
 	}
 
 	/**
-	 * Hides logo description.
-	 * @param {string} id - description element ID.
+	 *
+	 * @param id
 	 */
 	function hideLogoDescription( id ) {
 		$( id + ' .description' ).hide();
---
+++ projects/packages/jetpack-mu-wpcom/src/features/logo-tool/logo-tool.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Add a 'Create Logo' button to the Customizer when the theme supports a logo . The button directs customers to Fiverr .
+ *
+ * @package automattic/jetpack-mu-wpcom
+ */
+
+if ( ! function_exists( 'add_logotool_button' ) ) {
+	/**
+	 * Activate the Site Logo plugin.
+	 *
+	 * @uses current_theme_supports()
+	 *
+	 * @param WP_Customize_Manager $wp_customize Control manager for the Customizer.
+	 */
+	function add_logotool_button( $wp_customize ) {
+		if ( ! is_admin() ) {
+			return;
+		}
+
+		$logo_control = null;
+
+		if ( current_theme_supports( 'custom-logo' ) ) {
+			// WP Core logo integration.
+			$logo_control = $wp_customize->get_control( 'custom_logo' );
+		} elseif ( current_theme_supports( 'site-logo' ) ) {
+			// Jetpack logo integration.
+			$logo_control = $wp_customize->get_control( 'site_logo' );
+		} else {
+			// Other custom logo integrations, for example in the dotorg versions of Lovecraft or Radcliffe themes.
+			foreach ( $wp_customize->controls() as $control ) {
+				if (
+				// Control has the name logo in it.
+				false !== strpos( $control->id, 'logo' ) &&
+				// Control is not a `site_logo` or `custom_logo` (those are handled above).
+				! in_array( $control->id, array( 'custom_logo', 'site_logo' ), true ) &&
+				// Control is an instance of `WP_Customize_Image_Control` so we know how the UI is rendered to add the button.
+				is_a( $control, 'WP_Customize_Image_Control' )
+				) {
+					$logo_control = $control;
+					break;
+				}
+			}
+		}
+
+		// Make sure we have a valid Customize Control.
+		if ( ! is_a( $logo_control, 'WP_Customize_Control' ) ) {
+			return;
+		}
+
+		// And we have a valid setting attached to the control.
+		if ( ! is_a( $logo_control->setting, 'WP_Customize_Setting' ) ) {
+			return;
+		}
+
+		$logo_control->description = fixme__( 'Add a logo to display on your site. No logo? Buy a pro one today, powered by Fiverr — Click “Create logo” to start.', __( 'Add a logo to display on your site. No logo? Buy a pro one today — Click “Create logo” to start.', 'jetpack-mu-wpcom' ) );
+		// Adding it back just overwrites the previous control instance.
+		$wp_customize->add_control( $logo_control );
+
+		add_action(
+			'customize_controls_enqueue_scripts',
+			function () use ( $logo_control ) {
+				wp_enqueue_script( 'wpcom-logo-tool', plugins_url( 'js/customizer.js', __FILE__ ), array( 'customize-controls' ), '20210706', true );
+				wp_enqueue_style( 'wpcom-logo-tool', plugins_url( 'css/logo-tool.css', __FILE__ ), null, '20220108' );
+				wp_localize_script(
+					'wpcom-logo-tool',
+					'_LogoTool_',
+					array(
+						'l10n'      => array( 'create' => fixme__( 'Create logo in minutes', __( 'Create logo', 'jetpack-mu-wpcom' ) ) ),
+						'controlId' => $logo_control->id,
+						'settingId' => $logo_control->setting->id,
+					)
+				);
+			}
+		);
+	}
+	add_action( 'customize_register', 'add_logotool_button', 20 );
+}
