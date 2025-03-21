3a656da7993ebbe3b09ef38fa27f1e4c7dd0d26a
dkmyta <43220201+dkmyta@users.noreply.github.com>
2025-03-21 10:13:31 -0700

    Account Protection: Address security review items (#41960)
    * Improve method naming
    
    * Do not sanitize_text_field before password validation
    
    * Fix static analysis error
    
    * Fix tests
    
    * Update module first introduced version
    
    * Fix test method name

--- projects/packages/account-protection/src/class-password-detection.php
+++ projects/packages/account-protection/src/class-password-detection.php
@@ -54,7 +54,7 @@ class Password_Detection {
 			return $user;
 		}
 
-		if ( ! $this->validation_service->is_weak_password( $password ) ) {
+		if ( ! $this->validation_service->is_leaked_password( $password ) ) {
 			return $user;
 		}
 
