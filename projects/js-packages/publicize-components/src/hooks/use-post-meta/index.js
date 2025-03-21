cc87c8d2d901775b7a36acec9b3f2bb74b9a2a37
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-03-21 13:03:59 +0530

    Fix the warning for attachedMedia

--- projects/js-packages/publicize-components/src/hooks/use-post-meta/index.js
+++ projects/js-packages/publicize-components/src/hooks/use-post-meta/index.js
@@ -4,6 +4,11 @@ import { useCallback, useMemo } from '@wordpress/element';
 import { useShareMessageMaxLength } from '../../utils';
 
 /**
+ * This is to avoid creating a new empty array each time the value is requested.
+ */
+const DEFAULT_ATTACHED_MEDIA = [];
+
+/**
  * Returns the post meta values.
  *
  * @return {import('../../utils/types').UsePostMeta} The post meta values.
@@ -18,7 +23,7 @@ export function usePostMeta() {
 
 			const isPublicizeEnabled = meta.jetpack_publicize_feature_enabled ?? true;
 			const jetpackSocialOptions = meta.jetpack_social_options || {};
-			const attachedMedia = jetpackSocialOptions.attached_media || [];
+			const attachedMedia = jetpackSocialOptions.attached_media || DEFAULT_ATTACHED_MEDIA;
 			const imageGeneratorSettings = jetpackSocialOptions.image_generator_settings ?? {
 				enabled: false,
 			};
