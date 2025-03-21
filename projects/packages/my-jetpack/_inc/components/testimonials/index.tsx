db1a6116a5a838a3a02657296f7e6bc66fe7d329
Marcelo Vega <mavegaf@gmail.com>
2025-03-21 12:45:23 -0300

    Add some testimonials for testing purposes

---
+++ projects/packages/my-jetpack/_inc/components/testimonials/index.tsx
@@ -0,0 +1,48 @@
+import { DotPager } from '@automattic/jetpack-components';
+
+import './testimonial-style.scss';
+
+const Testimonials: React.FC = () => {
+	return (
+		<>
+			<DotPager>
+				<div className="testimonial1">
+					<div className="testimonial-content">
+						<p className="header">
+							<div>Grow your audience with Jetpack.</div>
+							<div>Speed up your site.</div>
+							<div>Keep it secure.</div>
+						</p>
+					</div>
+				</div>
+				<div className="testimonial2">
+					<div className="testimonial-content">
+						<p className="quote">
+							Jetpack&apos;s performance features are no-brainers for the sites I build. With
+							one-click CDN, there&apos;s no need to sacrifice performance for style. I know that it
+							just automagically works once I toggle that button.
+						</p>
+						<p className="author">
+							<strong>Sasha Endoh</strong>
+						</p>
+						<p className="title">Multidisciplinary Designed</p>
+					</div>
+				</div>
+				<div className="testimonial3">
+					<div className="testimonial-content">
+						<p className="quote">
+							Millions of people depend on my site, and downtime isn&apos;t an option. Jetpack
+							handles my site security and backups so I can focus on creation.
+						</p>
+						<p className="author">
+							<strong>Tim Ferriss</strong>
+						</p>
+						<p className="title">Author, Investor, Podcaster</p>
+					</div>
+				</div>
+			</DotPager>
+		</>
+	);
+};
+
+export default Testimonials;
