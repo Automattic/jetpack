fd0362ecd09cd8ecc04c38ba1c7920d381a96f76
Ian Ramos <5714212+IanRamosC@users.noreply.github.com>
2025-03-21 23:35:28 -0300

    Improve email regex for a more common and readable one

--- projects/packages/my-jetpack/_inc/components/onboarding-screen/connection-form/email-input.tsx
+++ projects/packages/my-jetpack/_inc/components/onboarding-screen/connection-form/email-input.tsx
@@ -18,7 +18,7 @@ const EmailInput = ( { isDisabled, onSubmit }: EmailInputProps ) => {
 	const [ shouldFetchUrl, setShouldFetchUrl ] = useState( false );
 
 	const validateEmail = ( email: string ) => {
-		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 		return emailRegex.test( email );
 	};
 
