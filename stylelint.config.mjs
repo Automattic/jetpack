0ddc9ff489e4e15d47227138499fdaca361cf787
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-03-24 14:27:37 -0600

    Set up Stylelint

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,5 @@
-import WpStylelintConfig from '@wordpress/stylelint-config/scss.js';
-// Remove the `stylelint-scss` plugin, which causes a problem and is included at a deeper level anyway.
-WpStylelintConfig.plugins = [];
-export default WpStylelintConfig;
+const config = {
+	extends: '@wordpress/stylelint-config/scss',
+};
+
+export default config;
