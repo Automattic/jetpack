cdb3d67ee6d953424331f4bede2bd1a75ae5c915
Christos <chriskmnds@gmail.com>
2025-03-24 12:11:39 +0200

    address feedback

--- projects/js-packages/number-formatters/eslint.config.mjs
+++ projects/js-packages/number-formatters/eslint.config.mjs
@@ -4,24 +4,11 @@ import makeBaseConfig from 'jetpack-js-tools/eslintrc/base.mjs';
 export default [
 	...makeBaseConfig( import.meta.url ),
 	{
-		languageOptions: {
-			parserOptions: {
-				project: './tsconfig.json',
-			},
-		},
-		files: [ '**/*.ts', '**/*.tsx' ],
-		plugins: {
-			'@typescript-eslint': true,
-			jsdoc: true,
-		},
 		rules: {
-			'jsdoc/require-jsdoc': 'off',
-			'jsdoc/require-description': 'off',
 			'jsdoc/require-returns': 'off',
 			'jsdoc/require-returns-check': 'off',
 			'jsdoc/require-param': 'off',
 			'jsdoc/require-param-description': 'off',
-			'jsdoc/require-param-type': 'off',
 		},
 	},
 ];
