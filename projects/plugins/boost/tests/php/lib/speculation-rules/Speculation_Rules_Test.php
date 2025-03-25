72396db95c00920278365b0ea87553d01b00b508
Donncha O Caoimh <5656673+donnchawp@users.noreply.github.com>
2025-03-25 11:00:17 +0000

    Add tests for the Speculation_Rules class

---
+++ projects/plugins/boost/tests/php/lib/speculation-rules/Speculation_Rules_Test.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Tests for Speculation_Rules class
+ *
+ * @package automattic/jetpack-boost
+ */
+
+namespace Automattic\Jetpack_Boost\Tests\Lib\Speculation_Rules;
+
+use Automattic\Jetpack_Boost\Lib\Speculation_Rules\Speculation_Rules;
+use Brain\Monkey;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+
+/**
+ * Class Test_Speculation_Rules
+ */
+class Speculation_Rules_Test extends MockeryTestCase {
+
+	/**
+	 * Set up test environment
+	 */
+	protected function setUp(): void {
+		parent::setUp();
+		Monkey\setUp();
+	}
+
+	/**
+	 * Tear down test environment
+	 */
+	protected function tearDown(): void {
+		Mockery::close();
+		Monkey\tearDown();
+		parent::tearDown();
+	}
+
+	/**
+	 * Test setup() when prerender is enabled
+	 */
+	public function test_setup_when_prerender_enabled() {
+		// Mock the data store get function
+		Monkey\Functions\stubs(
+			array(
+				'jetpack_boost_ds_get' => true,
+			)
+		);
+
+		// Expect add_action to be called with specific parameters
+		Monkey\Functions\expect( 'add_action' )
+			->once()
+			->with(
+				'wp_load_speculation_rules',
+				$this->callback(
+					function ( $callback ) {
+						return is_array( $callback )
+						&& $callback[0] instanceof Speculation_Rules
+						&& $callback[1] === 'add_cornerstone_rules';
+					}
+				)
+			);
+
+		$speculation_rules = new Speculation_Rules();
+		$speculation_rules->setup();
+
+		// Brain\Monkey verifies expectations automatically at the end of each test
+		// No need for explicit assertion as the test will fail if expectations aren't met
+	}
+
+	/**
+	 * Test setup() when prerender is disabled
+	 */
+	public function test_setup_when_prerender_disabled() {
+		// Mock the data store get function to return false
+		Monkey\Functions\stubs(
+			array(
+				'jetpack_boost_ds_get' => false,
+			)
+		);
+
+		// Expect add_action to never be called
+		Monkey\Functions\expect( 'add_action' )->never();
+
+		$speculation_rules = new Speculation_Rules();
+		$speculation_rules->setup();
+
+		// Brain\Monkey verifies expectations automatically at the end of each test
+		// No need for explicit assertion as the test will fail if expectations aren't met
+	}
+
+	/**
+	 * Test add_cornerstone_rules() with empty URLs
+	 */
+	public function test_add_cornerstone_rules_with_empty_urls() {
+		// Mock the data store get function to return empty array
+		Monkey\Functions\stubs(
+			array(
+				'jetpack_boost_ds_get' => array(),
+			)
+		);
+
+		// Create a proper mock for WP_Speculation_Rules
+		$wp_speculation_rules = Mockery::mock( 'WP_Speculation_Rules' );
+		$wp_speculation_rules->shouldReceive( 'add_rule' )->never();
+
+		$speculation_rules = new Speculation_Rules();
+		$speculation_rules->add_cornerstone_rules( $wp_speculation_rules );
+
+		// No need for explicit assertion - Mockery will verify expectations automatically
+	}
+
+	/**
+	 * Test add_cornerstone_rules() with valid URLs
+	 */
+	public function test_add_cornerstone_rules_with_valid_urls() {
+		$test_urls = array( 'https://example.com/page1', 'https://example.com/page2' );
+
+		// Mock the data store get function to return test URLs
+		Monkey\Functions\stubs(
+			array(
+				'jetpack_boost_ds_get' => $test_urls,
+			)
+		);
+
+		// Create a proper mock for WP_Speculation_Rules
+		$wp_speculation_rules = Mockery::mock( 'WP_Speculation_Rules' );
+		$wp_speculation_rules->expects()->add_rule(
+			'prerender',
+			'cornerstone-pages-prerender',
+			array(
+				'source'    => 'list',
+				'urls'      => $test_urls,
+				'eagerness' => 'moderate',
+			)
+		)->once();
+
+		$speculation_rules = new Speculation_Rules();
+		$speculation_rules->add_cornerstone_rules( $wp_speculation_rules );
+	}
+}
