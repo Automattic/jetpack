8f66b7650d1ebbc8956a58224af518f1d8c22919
Marcelo Vega <mavegaf@gmail.com>
2025-03-25 19:39:13 -0300

    adding translations

--- projects/packages/my-jetpack/_inc/components/testimonials/index.tsx
+++ projects/packages/my-jetpack/_inc/components/testimonials/index.tsx
@@ -1,4 +1,5 @@
 import { DotPager } from '@automattic/jetpack-components';
+import { __ } from '@wordpress/i18n';
 
 import './style.scss';
 
@@ -9,35 +10,48 @@ const Testimonials: React.FC = () => {
 				<div className="testimonial testimonial--1">
 					<div className="testimonial__content">
 						<div className="testimonial__messages">
-							<p className="testimonial__message">Grow your audience.</p>
-							<p className="testimonial__message">Speed up your site</p>
-							<p className="testimonial__message">Keep it secure.</p>
+							<p className="testimonial__message">
+								{ __( 'Grow your audience.', 'jetpack-my-jetpack' ) }
+							</p>
+							<p className="testimonial__message">
+								{ __( 'Speed up your site', 'jetpack-my-jetpack' ) }
+							</p>
+							<p className="testimonial__message">
+								{ __( 'Keep it secure.', 'jetpack-my-jetpack' ) }
+							</p>
 						</div>
 					</div>
 				</div>
 				<div className="testimonial testimonial--2">
 					<div className="testimonial__content">
 						<p className="testimonial__quote">
-							Jetpack&apos;s performance features are no-brainers for the sites I build. With
-							one-click CDN, there&apos;s no need to sacrifice performance for style. I know that it
-							just automagically works once I toggle that button.
+							{ __(
+								"Jetpack's performance features are no-brainers for the sites I build. With one-click CDN, there's no need to sacrifice performance for style. I know that it just automagically works once I toggle that button.",
+								'jetpack-my-jetpack'
+							) }
 						</p>
 						<p className="testimonial__author">
-							<strong>Sasha Endoh</strong>
+							<strong>{ __( 'Sasha Endoh', 'jetpack-my-jetpack' ) }</strong>
 						</p>
-						<p className="testimonial__title">Multidisciplinary Designed</p>
+						<p className="testimonial__title">
+							{ __( 'Multidisciplinary Designed', 'jetpack-my-jetpack' ) }
+						</p>
 					</div>
 				</div>
 				<div className="testimonial testimonial--3">
 					<div className="testimonial__content">
 						<p className="testimonial__quote">
-							Millions of people depend on my site, and downtime isn&apos;t an option. Jetpack
-							handles my site security and backups so I can focus on creation.
+							{ __(
+								"Millions of people depend on my site, and downtime isn't an option. Jetpack handles my site security and backups so I can focus on creation.",
+								'jetpack-my-jetpack'
+							) }
 						</p>
 						<p className="testimonial__author">
-							<strong>Tim Ferriss</strong>
+							<strong>{ __( 'Tim Ferriss', 'jetpack-my-jetpack' ) }</strong>
 						</p>
-						<p className="testimonial__title">Author, Investor, Podcaster</p>
+						<p className="testimonial__title">
+							{ __( 'Author, Investor, Podcaster', 'jetpack-my-jetpack' ) }
+						</p>
 					</div>
 				</div>
 			</DotPager>
