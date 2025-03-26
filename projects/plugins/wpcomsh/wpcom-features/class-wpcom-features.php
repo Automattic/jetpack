8b2499f58267a22450af1d257f5582d3a90016e9
Michalis Despotopoulos <michaeldespoto@gmail.com>
2025-03-26 00:46:25 +0000

    Keep a single instant_search feature

--- projects/plugins/wpcomsh/wpcom-features/class-wpcom-features.php
+++ projects/plugins/wpcomsh/wpcom-features/class-wpcom-features.php
@@ -380,7 +380,6 @@ class WPCOM_Features {
 	public const INSTALL_THEMES                    = 'install-themes';
 	public const INSTALL_WOO_ONBOARDING_PLUGINS    = 'install-woo-onboarding-plugins';
 	public const INSTANT_SEARCH                    = 'instant-search';
-	public const INSTANT_SEARCH_NO_BUSINESS        = 'instant-search-no-business'; // TEMP AND TO BE DELETED AFTER REBUILDING the jetpack-search index
 	public const JETPACK_DASHBOARD                 = 'jetpack-dashboard';
 	public const LEGACY_CONTACT                    = 'legacy-contact';
 	public const LIST_INSTALLED_PLUGINS            = 'list-installed-plugins';
@@ -774,16 +773,6 @@ class WPCOM_Features {
 			self::WPCOM_ECOMMERCE_TRIAL_PLANS,
 		),
 		self::INSTANT_SEARCH                    => array(
-			self::WPCOM_BUSINESS_PLANS,
-			self::WPCOM_ECOMMERCE_PLANS,
-			self::WPCOM_ECOMMERCE_TRIAL_PLANS,
-			self::WPCOM_SEARCH,
-			self::WPCOM_SEARCH_MONTHLY,
-			self::WP_P2_PLUS_MONTHLY,
-			self::JETPACK_SEARCH_PLANS,
-			self::JETPACK_COMPLETE_PLANS,
-		),
-		self::INSTANT_SEARCH_NO_BUSINESS        => array(
 			self::WPCOM_SEARCH,
 			self::WPCOM_SEARCH_MONTHLY,
 			self::WP_P2_PLUS_MONTHLY,
