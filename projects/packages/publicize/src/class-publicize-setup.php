9e03ceed4565f6c30bc4d93972c5d87e963557a4
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-03-27 15:20:04 +0530

    Do not initialize connections post field on wpcom

--- projects/packages/publicize/src/class-publicize-setup.php
+++ projects/packages/publicize/src/class-publicize-setup.php
@@ -112,9 +112,11 @@ class Publicize_Setup {
 			}
 		}
 
-		// Adding on a higher priority to make sure we're the first field registered.
-		// The priority parameter can be removed once we deprecate WPCOM_REST_API_V2_Post_Publicize_Connections_Field.
-		add_action( 'rest_api_init', array( new REST_API\Connections_Post_Field(), 'register_fields' ), 5 );
+		if ( ! $is_wpcom_simple ) {
+			// Adding on a higher priority to make sure we're the first field registered.
+			// The priority parameter can be removed once we deprecate WPCOM_REST_API_V2_Post_Publicize_Connections_Field.
+			add_action( 'rest_api_init', array( new REST_API\Connections_Post_Field(), 'register_fields' ), 5 );
+		}
 		add_action( 'rest_api_init', array( new REST_Controller(), 'register_rest_routes' ) );
 		add_action( 'current_screen', array( static::class, 'init_sharing_limits' ) );
 
