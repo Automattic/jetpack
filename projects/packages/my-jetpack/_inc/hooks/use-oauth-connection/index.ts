82a5023c2cce2c0c41a0061a4f350cdc1c4660a5
robertsreberski <robert.sreberski@automattic.com>
2025-03-28 03:20:01 -0500

    Add 'from' param to social authentication

--- projects/packages/my-jetpack/_inc/hooks/use-oauth-connection/index.ts
+++ projects/packages/my-jetpack/_inc/hooks/use-oauth-connection/index.ts
@@ -49,7 +49,7 @@ const useOauthConnection = (): UseOauthConnectionReturn => {
 			path: `${ REST_API_GET_OAUTH_AUTHORIZE_URL }/${ userEmail ? 'link' : socialService ?? '' }${
 				userEmail
 					? `?email_address=${ encodeURIComponent( userEmail ) }&from=jetpack-onboarding`
-					: ''
+					: '?from=jetpack-onboarding'
 			}`,
 		},
 		options: {
