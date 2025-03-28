5996d5c4fed94516c21a2501861c4fcc8aec4e0d
Erick Danzer <edanzer@gmail.com>
2025-03-28 12:19:41 -0600

    Restore link and adjust styles

--- projects/packages/forms/src/blocks/contact-form/components/jetpack-manage-responses-settings.js
+++ projects/packages/forms/src/blocks/contact-form/components/jetpack-manage-responses-settings.js
@@ -12,11 +12,7 @@ const JetpackManageResponsesSettings = () => {
 			<InspectorHint>
 				{ __( 'Manage and export your form responses in WPAdmin:', 'jetpack-forms' ) }
 			</InspectorHint>
-			<Button
-				variant="secondary"
-				onClick={ () => window.open( RESPONSES_PATH, '_blank' ) }
-				__next40pxDefaultSize={ true }
-			>
+			<Button variant="secondary" href={ RESPONSES_PATH } __next40pxDefaultSize={ true }>
 				{ __( 'View Form Responses', 'jetpack-forms' ) }
 				<span className="screen-reader-text">
 					{ __( '(opens in a new tab)', 'jetpack-forms' ) }
