b26741924e3af918e575fc8c19b19346c54c2f47
Kevin Zoschke <kevin.zoschke@automattic.com>
2025-04-01 16:01:18 -0400

    Fix failing tests

--- projects/plugins/jetpack/extensions/blocks/contact-info/email/test/edit.js
+++ projects/plugins/jetpack/extensions/blocks/contact-info/email/test/edit.js
@@ -2,12 +2,14 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import EmailEdit from '../edit';
 
-jest.mock( '@wordpress/block-editor', () => ( {
-	...jest.requireActual( '@wordpress/block-editor' ),
-	useBlockProps: {
-		save: () => ( {} ),
-	},
-} ) );
+jest.mock( '@wordpress/block-editor', () => {
+	const useBlockProps = () => ( {} );
+	useBlockProps.save = () => ( {} );
+	return {
+		...jest.requireActual( '@wordpress/block-editor' ),
+		useBlockProps,
+	};
+} );
 
 const setAttributes = jest.fn();
 
