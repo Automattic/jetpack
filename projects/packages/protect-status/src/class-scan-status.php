a66fe48f0b8309c9434de9c39870da44cd76c979
Nate Weller <hello@nateweller.com>
2025-04-01 14:01:30 -0600

    Protect Status: fix plan and status caching

--- projects/packages/protect-status/src/class-scan-status.php
+++ projects/packages/protect-status/src/class-scan-status.php
@@ -59,13 +59,14 @@ class Scan_Status extends Status {
 	 * @return Status_Model
 	 */
 	public static function get_status( $refresh_from_wpcom = false ) {
-		if ( self::$status !== null ) {
-			return self::$status;
-		}
-
-		if ( $refresh_from_wpcom || ! self::should_use_cache() || self::is_cache_expired() ) {
+		if ( self::$status !== null && self::$status->data_source === 'scan_api' && ! $refresh_from_wpcom ) {
+			// We already have a result from the correct data source, and we're not forcing a refresh, so return the memoized result.
+			$status = self::$status;
+		} elseif ( $refresh_from_wpcom || ! self::should_use_cache() || self::is_cache_expired() ) {
+			// We are forcing a refresh, or the cache is expired, so fetch from the API.
 			$status = self::fetch_from_api();
 		} else {
+			// We have a valid cached status, so use that.
 			$status = self::get_from_options();
 		}
 
