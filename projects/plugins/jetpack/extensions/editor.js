29a84d3c30eaa7ad244a1278b82b65d7eae314d1
Christos <chriskmnds@gmail.com>
2025-04-01 18:52:24 +0300

    getBrowserSafeLocale with fallbacks

--- projects/plugins/jetpack/extensions/editor.js
+++ projects/plugins/jetpack/extensions/editor.js
@@ -1,4 +1,3 @@
-import { initializeNumberFormatters } from '@automattic/number-formatters';
 import apiFetch from '@wordpress/api-fetch';
 import { createHigherOrderComponent } from '@wordpress/compose';
 import { addFilter } from '@wordpress/hooks';
@@ -18,8 +17,6 @@ import './store/media-source';
 import './store/membership-products';
 import extensionList from './index.json';
 import './index.scss';
-
-initializeNumberFormatters();
 
 // Register middleware for @wordpress/api-fetch to indicate the fetch is coming from the editor.
 apiFetch.use( ( options, next ) => {
