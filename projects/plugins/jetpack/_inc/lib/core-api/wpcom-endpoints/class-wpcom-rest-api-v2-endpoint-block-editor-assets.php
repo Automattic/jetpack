59486496257a3c99a6fa7a02aade2fe60d3b184e
David Calhoun <github@davidcalhoun.me>
2025-04-01 10:37:52 -0400

    style: Disable lint warning preventing schema alignment
    The `schema` key was added for alignment with existing API endpoints.

--- projects/plugins/jetpack/_inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-block-editor-assets.php
+++ projects/plugins/jetpack/_inc/lib/core-api/wpcom-endpoints/class-wpcom-rest-api-v2-endpoint-block-editor-assets.php
@@ -33,6 +33,8 @@ if ( ! class_exists( 'WPCOM_REST_API_V2_Endpoint_Block_Editor_Assets_Controller'
 				'/' . $this->rest_base,
 				array(
 					array(
+						// Disabled to allow return structure to match existing endpoints
+						// @phan-suppress-next-line PhanPluginMixedKeyNoKey
 						'methods'             => WP_REST_Server::READABLE,
 						'callback'            => array( $this, 'get_items' ),
 						'permission_callback' => array( $this, 'get_items_permissions_check' ),
