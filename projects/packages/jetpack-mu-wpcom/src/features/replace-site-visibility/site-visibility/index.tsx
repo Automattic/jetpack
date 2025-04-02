985e58c55c974731fb6766e26171cf3fcced0c34
arthur <arthur.chu@automattic.com>
2025-04-02 17:03:50 +0800

    Site Visibility: Disable search engines indexing for sites with wpcomstaging.com domain

--- projects/packages/jetpack-mu-wpcom/src/features/replace-site-visibility/site-visibility/index.tsx
+++ projects/packages/jetpack-mu-wpcom/src/features/replace-site-visibility/site-visibility/index.tsx
@@ -1,5 +1,8 @@
+import { ExternalLink } from '@wordpress/components';
+import { createInterpolateElement } from '@wordpress/element';
 import { sprintf, __ } from '@wordpress/i18n';
 import { useState } from 'react';
+import { wpcomTrackEvent } from '../../../common/tracks';
 import SitePreviewLink from '../site-preview-link';
 import type { SitePreviewLinkObject } from '../site-preview-link';
 import './style.scss';
@@ -38,6 +41,9 @@ const SiteVisibility = ( {
 
 	const { blogPublic, wpcomComingSoon, wpcomPublicComingSoon, wpcomDataSharingOptOut } = fields;
 
+	const { host } = new URL( homeUrl );
+	const isWpcomStagingDomain = host.endsWith( '.wpcomstaging.com' );
+
 	// isPrivateAndUnlaunched means it is an unlaunched coming soon v1 site
 	const isPrivateAndUnlaunched = -1 === blogPublic && isUnlaunchedSite;
 	const isAnyComingSoonEnabled =
@@ -45,7 +51,8 @@ const SiteVisibility = ( {
 	const isPublicChecked = ( blogPublic === 0 && ! wpcomPublicComingSoon ) || blogPublic === 1;
 	const showPreviewLink =
 		Number( defaultWpcomPublicComingSoon ) === 1 && isAnyComingSoonEnabled && hasSitePreviewLink;
-	const discourageSearchChecked = 0 === blogPublic && ! wpcomPublicComingSoon;
+	const discourageSearchChecked =
+		( 0 === blogPublic && ! wpcomPublicComingSoon ) || isWpcomStagingDomain;
 
 	return (
 		<>
@@ -133,6 +140,8 @@ const SiteVisibility = ( {
 										type="checkbox"
 										value="0"
 										checked={ discourageSearchChecked }
+										// See https://github.com/Automattic/wp-calypso/issues/101828.
+										disabled={ isWpcomStagingDomain }
 										onChange={ () =>
 											updateFields( {
 												blogPublic:
@@ -151,6 +160,42 @@ const SiteVisibility = ( {
 										'jetpack-mu-wpcom'
 									) }
 								</p>
+								{ isWpcomStagingDomain && (
+									<div className="notice notice-warning inline">
+										<p>
+											{ createInterpolateElement(
+												__(
+													'Your site is using <strong>wpcomstaging.com</strong> as the primary domain, which is prevented from being indexed by search engines.<br />To change this, <link1>buy a custom domain</link1>, or <link2>set a custom domain as the primary domain</link2>.',
+													'jetpack-mu-wpcom'
+												),
+												{
+													strong: <strong />,
+													br: <br />,
+													link1: (
+														<ExternalLink
+															href={ `https://wordpress.com/domains/add/${ host }?redirect_to=${ window.location.href }` }
+															target="_blank"
+															onClick={ () =>
+																wpcomTrackEvent( 'wpcom_settings_reading_add_domain_button_click' )
+															}
+														/>
+													),
+													link2: (
+														<ExternalLink
+															href={ `https://wordpress.com/domains/manage/${ host }?source=${ window.location.pathname }` }
+															target="_blank"
+															onClick={ () =>
+																wpcomTrackEvent(
+																	'wpcom_settings_reading_manage_domain_button_click'
+																)
+															}
+														/>
+													),
+												}
+											) }
+										</p>
+									</div>
+								) }
 							</li>
 							<li>
 								<label htmlFor="wpcom_site_visibility_data_sharing_opt_out">
@@ -177,7 +222,7 @@ const SiteVisibility = ( {
 									{ sprintf(
 										// translators: %s: the slug of the site
 										__( 'Prevent third-party sharing for %s', 'jetpack-mu-wpcom' ),
-										new URL( homeUrl ).host
+										host
 									) }
 								</label>
 								<p className="description">
