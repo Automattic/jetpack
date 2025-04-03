83dd7109bbe37e4404d0a54ea3157e5ead7c2ee2
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-04-03 22:01:47 +0530

    Move webpack config changes to publicize package

--- projects/packages/publicize/webpack.config.js
+++ projects/packages/publicize/webpack.config.js
@@ -44,6 +44,20 @@ const socialWebpackConfig = {
 
 			// Handle images.
 			jetpackWebpackConfig.FileRule(),
+
+			// Add textdomains (but no other optimizations) for @wordpress/dataviews.
+			jetpackWebpackConfig.TranspileRule( {
+				includeNodeModules: [ '@wordpress/dataviews/' ],
+				babelOpts: {
+					configFile: false,
+					plugins: [
+						[
+							require.resolve( '@automattic/babel-plugin-replace-textdomain' ),
+							{ textdomain: 'jetpack-publicize-pkg' },
+						],
+					],
+				},
+			} ),
 		],
 	},
 	externals: {
