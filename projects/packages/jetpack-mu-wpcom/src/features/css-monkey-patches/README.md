cb2b265dcda4a16ef11fe33c9929e0102d0857bb
Omar Alshaker <omar@omaralshaker.com>
2025-04-03 21:27:51 +0200

    Clean up

--- projects/packages/jetpack-mu-wpcom/src/features/css-monkey-patches/README.md
+++ projects/packages/jetpack-mu-wpcom/src/features/css-monkey-patches/README.md
@@ -4,6 +4,10 @@ This patches miscellaneous CSS issues that occur when plugins don't interplay we
 
 This started because of https://github.com/elementor/elementor/pull/30540.
 
+# File naming rule
+
+Please name each CSS patch with the number of the corresponding issue on GitHub. Avoid accumulating CSS in large files. These fixes are meant to be tiny and precise.
+
 # Keywords for search
 
 Custom CSS, additional CSS, CSS patch.
--- projects/packages/jetpack-mu-wpcom/src/features/css-monkey-patches/index.php
+++ projects/packages/jetpack-mu-wpcom/src/features/css-monkey-patches/index.php
@@ -11,5 +11,10 @@ add_action( 'admin_enqueue_scripts', 'jetpack_mu_wpcom_admin_css_monkey_patches'
  * Enqueue the CSS monkey patches.
  */
 function jetpack_mu_wpcom_admin_css_monkey_patches() {
-	wp_enqueue_style( 'css-monkey-patches', plugins_url( 'css-monkey-patches.css', __FILE__ ), array(), JETPACK_MU_WPCOM_VERSION );
+	/**
+	 * Only enqueue the Elementor fix on the Elementor admin page.
+	 */
+	if ( 'toplevel_page_elementor' === get_current_screen()->id ) {
+		wp_enqueue_style( 'jetpack-elementor-fix', plugins_url( 'jetpack-elementor-fix.css', __FILE__ ), array(), filemtime( __DIR__ . '/jetpack-elementor-fix.css' ) );
+	}
 }
