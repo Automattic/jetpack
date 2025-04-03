eb50fbabcc22942787df99a855b0a3d4527c82a2
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-04-03 10:43:34 +0530

    Update stylelint config to allow :global pseudo-class selector

--- tools/js-tools/stylelint.config.base.mjs
+++ tools/js-tools/stylelint.config.base.mjs
@@ -1,7 +1,18 @@
 import { fileURLToPath } from 'node:url';
 
+/**
+ * @type {import('stylelint').Config}
+ */
 const baseConfig = {
 	extends: fileURLToPath( import.meta.resolve( '@wordpress/stylelint-config/scss' ) ),
+	rules: {
+		'selector-pseudo-class-no-unknown': [
+			true,
+			{
+				ignorePseudoClasses: [ 'global' ],
+			},
+		],
+	},
 };
 
 export default baseConfig;
