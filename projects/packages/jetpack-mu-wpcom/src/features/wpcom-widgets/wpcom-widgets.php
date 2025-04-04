ba984bce32245673ddc0a835fc9677fb9e4e8fdf
Jerry Jones <jones.jeremydavid@gmail.com>
2025-04-04 11:46:45 -0500

    Move I voted widget from wpcomsh to jetpack-mu-wpcom package

---
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-widgets/wpcom-widgets.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Widgets for WordPress.com
+ *
+ * @package automattic/jetpack-mu-wpcom
+ */
+
+/**
+ * WP.com Widgets (in alphabetical order)
+ */
+require_once __DIR__ . 'class-jetpack-i-voted-widget.php';
