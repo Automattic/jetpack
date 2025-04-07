f5f95ba9293b419a259f82e4a6a24958976b645b
Daniel Richards <daniel.richards@automattic.com>
2025-04-07 15:33:33 +0800

    Remove unused import for modules page

--- tools/e2e-commons/pages/wp-admin/index.js
+++ tools/e2e-commons/pages/wp-admin/index.js
@@ -14,7 +14,6 @@ export { default as DashboardPage } from './dashboard.js';
 export { default as InPlacePlansPage } from './in-place-plans.js';
 export { default as JetpackPage } from './jetpack.js';
 export { default as JetpackDashboardPage } from './jetpack-dashboard.js';
-export { default as JetpackModulesPage } from './jetpack-modules.js';
 export { default as JetpackMyPlanPage } from './jetpack-my-plan.js';
 export { default as WPLoginPage } from './login.js';
 export { default as PluginsPage } from './plugins.js';
