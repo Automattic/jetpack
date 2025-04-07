cb568b4d83604acf1a41cf76c43ec225eb263280
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-04-07 10:11:46 -0600

    Changelog and readme.txt edits.

--- projects/packages/my-jetpack/src/class-initializer.php
+++ projects/packages/my-jetpack/src/class-initializer.php
@@ -39,7 +39,7 @@ class Initializer {
 	 *
 	 * @var string
 	 */
-	const PACKAGE_VERSION = '5.11.1';
+	const PACKAGE_VERSION = '5.11.2';
 
 	/**
 	 * HTML container ID for the IDC screen on My Jetpack page.
