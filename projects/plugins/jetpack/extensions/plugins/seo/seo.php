d3af87cc16654a68793f89e4225aa838753f4f3c
Jeremy Herve <jeremy@jeremy.hu>
2025-04-08 16:32:57 +0200

    SEO block: ensure we check for all registrations

--- projects/plugins/jetpack/extensions/plugins/seo/seo.php
+++ projects/plugins/jetpack/extensions/plugins/seo/seo.php
@@ -12,12 +12,7 @@ use Automattic\Jetpack\Modules;
 add_action(
 	'jetpack_register_gutenberg_extensions',
 	function () {
-		/*
-		* The extension is available even when the module is not active,
-		* so we can display a nudge to activate the module instead of the block.
-		* However, since non-admins cannot activate modules, we do not display the empty block for them.
-		*/
-		if ( ! ( new Modules() )->is_active( 'seo' ) && ! current_user_can( 'jetpack_activate_modules' ) ) {
+		if ( is_seo_disabled() ) {
 			return;
 		}
 
@@ -25,11 +20,21 @@ add_action(
 	}
 );
 
+/**
+ * We only want to enable the SEO extension (and display SEO settings)
+ * if the 'jetpack_disable_seo_tools' filter is not set to false.
+ *
+ * This is done on after_setup_theme to ensure that we have access to the hook
+ * that is used by SEO plugins to disable the conflicting output of SEO Tools.
+ */
 add_action(
 	'after_setup_theme',
 	function () {
-		// We only want to enable the SEO extension (and display SEO settings) if the 'jetpack_disable_seo_tools' filter is not set to false.
-		if ( ! apply_filters( 'jetpack_disable_seo_tools', false ) ) {
+		if (
+			! is_seo_disabled()
+			/** This filter is already documented in modules/seo-tools/class-jetpack-seo-utils.php */
+			&& ! apply_filters( 'jetpack_disable_seo_tools', false )
+		) {
 			\Jetpack_Gutenberg::set_extension_available( 'jetpack-seo' );
 		}
 	}
@@ -44,3 +49,14 @@ add_filter(
 		);
 	}
 );
+
+/**
+ * SEO tools should be hidden from non-admins if the module is not active.
+ *
+ * @since $$next-version$$
+ *
+ * @return bool
+ */
+function is_seo_disabled(): bool {
+	return ! ( new Modules() )->is_active( 'seo-tools' ) && ! current_user_can( 'jetpack_activate_modules' );
+}
