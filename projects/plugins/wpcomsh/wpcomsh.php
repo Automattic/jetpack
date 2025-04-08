e3588928c098d7d9c6cd9c6bb581cf7a632b6929
Jerry Jones <jones.jeremydavid@gmail.com>
2025-04-08 15:03:59 -0500

    Remove aboutme widget as it is no longer on wpcom

--- projects/plugins/wpcomsh/wpcomsh.php
+++ projects/plugins/wpcomsh/wpcomsh.php
@@ -50,7 +50,6 @@ require_once __DIR__ . '/jetpack-token-error-header/class-atomic-record-jetpack-
 /**
  * WP.com Widgets (in alphabetical order)
  */
-require_once __DIR__ . '/widgets/class-aboutme-widget.php';
 require_once __DIR__ . '/widgets/class-gravatar-widget.php';
 require_once __DIR__ . '/widgets/class-jetpack-posts-i-like-widget.php';
 require_once __DIR__ . '/widgets/class-music-player-widget.php';
