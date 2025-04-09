7575e90137c248279b7d3637ac80239656dd59a5
Ben Dwyer <ben@scruffian.com>
2025-04-09 23:17:08 +0100

    Disable the mu plugin in wpcomsh

--- projects/plugins/wpcomsh/wpcomsh.php
+++ projects/plugins/wpcomsh/wpcomsh.php
@@ -109,9 +109,11 @@ require_once __DIR__ . '/feature-plugins/wordpress-mods.php';
  *
  * JETPACK_MU_WPCOM_LOAD_VIA_BETA_PLUGIN=true will load the package via the Jetpack Beta Tester plugin, not wpcomsh.
  */
-if ( ! defined( 'JETPACK_MU_WPCOM_LOAD_VIA_BETA_PLUGIN' ) || ! JETPACK_MU_WPCOM_LOAD_VIA_BETA_PLUGIN ) {
-	if ( class_exists( 'Automattic\Jetpack\Jetpack_Mu_Wpcom' ) ) {
-		Automattic\Jetpack\Jetpack_Mu_Wpcom::init();
+if ( ! defined( 'IS_HOSTING_SITE' ) || ! IS_HOSTING_SITE ) {
+	if ( ! defined( 'JETPACK_MU_WPCOM_LOAD_VIA_BETA_PLUGIN' ) || ! JETPACK_MU_WPCOM_LOAD_VIA_BETA_PLUGIN ) {
+		if ( class_exists( 'Automattic\Jetpack\Jetpack_Mu_Wpcom' ) ) {
+			Automattic\Jetpack\Jetpack_Mu_Wpcom::init();
+		}
 	}
 }
 
