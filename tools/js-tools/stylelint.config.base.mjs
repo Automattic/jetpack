ace4f875ffb2e1447f7ac1082cff38076dbc2874
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-04-09 21:33:41 -0600

    Undisable no-invalid-position-at-import-rule

--- tools/js-tools/stylelint.config.base.mjs
+++ tools/js-tools/stylelint.config.base.mjs
@@ -37,7 +37,6 @@ const baseConfig = {
 		'length-zero-no-unit': null,
 		'no-descending-specificity': null,
 		'no-duplicate-selectors': null,
-		'no-invalid-position-at-import-rule': null,
 		'property-no-unknown': null,
 		'scss/at-extend-no-missing-placeholder': null,
 		'scss/comment-no-empty': null,
