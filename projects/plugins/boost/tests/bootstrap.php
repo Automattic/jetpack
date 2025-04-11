f943e871d85b4bd419bd61f37d327616ccc1d664
Brandon Kraft <public@brandonkraft.com>
2025-04-11 16:46:01 -0500

    Critical CSS: add php unit tests for displaying critical css

--- projects/plugins/boost/tests/bootstrap.php
+++ projects/plugins/boost/tests/bootstrap.php
@@ -10,6 +10,12 @@
  */
 require_once __DIR__ . '/../vendor/autoload.php';
 
+// Preloading the file to reconcile Brain\Monkey with Wordbless.
+require_once __DIR__ . '/../vendor/antecedent/patchwork/Patchwork.php';
+
+// Initialize WordBless test environment.
+\Automattic\Jetpack\Test_Environment::init();
+
 // Additional functions that brain/monkey doesn't currently define.
 if ( ! function_exists( 'wp_unslash' ) ) {
 	/**
