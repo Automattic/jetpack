5b7d0e0ee70c0c6519bfee3a9b438da528d4576a
Brandon Kraft <public@brandonkraft.com>
2025-04-14 09:14:29 -0500

    Separate out wordbless tests from the existing ones to prevent issues with wordbless polluting the brain/monkey mocks

--- projects/plugins/boost/tests/bootstrap.php
+++ projects/plugins/boost/tests/bootstrap.php
@@ -2,19 +2,13 @@
 /**
  * Bootstrap.
  *
- * @package automattic/
+ * @package automattic/jetpack-boost
  */
 
 /**
  * Include the composer autoloader.
  */
 require_once __DIR__ . '/../vendor/autoload.php';
-
-// Preloading the file to reconcile Brain\Monkey with Wordbless.
-require_once __DIR__ . '/../vendor/antecedent/patchwork/Patchwork.php';
-
-// Initialize WordBless test environment.
-\Automattic\Jetpack\Test_Environment::init();
 
 // Additional functions that brain/monkey doesn't currently define.
 if ( ! function_exists( 'wp_unslash' ) ) {
