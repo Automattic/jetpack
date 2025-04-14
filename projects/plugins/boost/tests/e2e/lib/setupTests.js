9084323ee66530c2adbc110a747c259a71469419
Brad Jorsch <brad.jorsch@automattic.com>
2025-04-14 13:16:37 -0400

    Boost: Run common globalSetup before custom setup

--- projects/plugins/boost/tests/e2e/lib/setupTests.js
+++ projects/plugins/boost/tests/e2e/lib/setupTests.js
@@ -1,11 +1,15 @@
 import { chromium } from '@playwright/test';
+import globalSetup from '_jetpack-e2e-commons/config/global-setup.mjs';
 import { prerequisitesBuilder } from '_jetpack-e2e-commons/env/prerequisites.js';
 import { boostPrerequisitesBuilder } from './env/prerequisites.js';
 
 /**
  * Setup tests.
+ * @param {import('@playwright/test').PlaywrightTestConfig} config - Playwright test configuration.
  */
-export default async function () {
+export default async function ( config ) {
+	await globalSetup( config );
+
 	const browser = await chromium.launch();
 	const page = await browser.newPage();
 	await prerequisitesBuilder( page ).withLoggedIn( true ).withActivePlugins( [ 'boost' ] ).build();
