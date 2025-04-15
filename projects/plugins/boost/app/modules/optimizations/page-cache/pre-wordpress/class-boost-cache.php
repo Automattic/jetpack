e6549d957d07d1f5e6232ce81f4ab173abda03d8
Donncha O Caoimh <5656673+donnchawp@users.noreply.github.com>
2025-04-15 12:21:21 +0100

    Use get_page_link() to find a page permalink.

--- projects/plugins/boost/app/modules/optimizations/page-cache/pre-wordpress/class-boost-cache.php
+++ projects/plugins/boost/app/modules/optimizations/page-cache/pre-wordpress/class-boost-cache.php
@@ -416,12 +416,16 @@ class Boost_Cache {
 		 * the post name. We need to get the post name from the post object.
 		 */
 		$permalink = get_permalink( $post->ID );
-		if ( strpos( $permalink, '?p=' ) !== false ) {
-			if ( ! function_exists( 'get_sample_permalink' ) ) {
-				require_once ABSPATH . 'wp-admin/includes/post.php';
+		if ( strpos( $permalink, '?p=' ) !== false || strpos( $permalink, '?page_id=' ) !== false ) {
+			if ( $post->post_type === 'page' ) {
+				$permalink = get_page_link( $post->ID, false, true );
+			} else {
+				if ( ! function_exists( 'get_sample_permalink' ) ) {
+					require_once ABSPATH . 'wp-admin/includes/post.php';
+				}
+				list( $permalink, $post_name ) = get_sample_permalink( $post->ID );
+				$permalink                     = str_replace( '%postname%', $post_name, $permalink );
 			}
-			list( $permalink, $post_name ) = get_sample_permalink( $post );
-			$permalink                     = str_replace( '%postname%', $post_name, $permalink );
 		}
 		return $permalink;
 	}
