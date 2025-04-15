3362211adeaa29c53a431bf085aef4134fdcdaa4
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2025-04-15 14:08:20 +1000

    Allow default checked status for standalone option fields (#43077)

--- projects/packages/forms/src/class-jetpack-forms.php
+++ projects/packages/forms/src/class-jetpack-forms.php
@@ -35,6 +35,9 @@ class Jetpack_Forms {
 
 		// Add hook to delete file attachments when a feedback post is deleted
 		add_action( 'before_delete_post', array( '\Automattic\Jetpack\Forms\ContactForm\Contact_Form', 'delete_feedback_files' ) );
+
+		// Enforces the availability of block support controls in the UI for classic themes.
+		add_filter( 'wp_theme_json_data_default', array( '\Automattic\Jetpack\Forms\ContactForm\Contact_Form', 'add_theme_json_data_for_classic_themes' ) );
 	}
 
 	/**
