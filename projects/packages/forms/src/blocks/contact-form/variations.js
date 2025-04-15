7e6b3ed1e93d553f657308489851f8f83c0c0692
Daniel Richards <daniel.richards@automattic.com>
2025-04-15 14:00:24 +0800

    Fix date picker field incorrect classname (#43059)
    * Ensure jetpack-field__input classname applies to date picker input instead of jetpack-field__date
    
    * Remove explicit date type in appointment form variation
    
    * Specify the field type for the date field in the block template
    
    * Iron out inconsistency of date type for date picker inputs
    
    * Tied up text fields array
    
    ---------
    
    Co-authored-by: Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>

--- projects/packages/forms/src/blocks/contact-form/variations.js
+++ projects/packages/forms/src/blocks/contact-form/variations.js
@@ -428,10 +428,7 @@ const variations = compact( [
 			[
 				'jetpack/field-date',
 				{ required: true },
-				[
-					[ 'jetpack/label', { label: __( 'Date', 'jetpack-forms' ) } ],
-					[ 'jetpack/input', { type: 'date' } ],
-				],
+				[ [ 'jetpack/label', { label: __( 'Date', 'jetpack-forms' ) } ], [ 'jetpack/input' ] ],
 			],
 			[
 				'jetpack/field-radio',
@@ -511,7 +508,7 @@ const variations = compact( [
 					attributes: { required: true },
 					innerBlocks: [
 						{ name: 'jetpack/label', attributes: { label: __( 'Date', 'jetpack-forms' ) } },
-						{ name: 'jetpack/input', attributes: { type: 'date' } },
+						{ name: 'jetpack/input' },
 					],
 				},
 				{
