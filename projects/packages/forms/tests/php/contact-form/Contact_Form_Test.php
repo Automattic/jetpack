695e83e0c3c755b995879696df82f39fa0045580
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2025-04-15 16:59:25 +1000

    Revert "Remove deprecated covers annotations in line with #42902"
    This reverts commit 38791a8459ac68bb654be2d724530e5473f218dd.
    
    Restoring these in case they were in place for coverage requirements.

--- projects/packages/forms/tests/php/contact-form/Contact_Form_Test.php
+++ projects/packages/forms/tests/php/contact-form/Contact_Form_Test.php
@@ -990,6 +990,8 @@ class Contact_Form_Test extends BaseTestCase {
 
 	/**
 	 * Test for checkbox field_renders
+	 *
+	 * @covers \Automattic\Jetpack\Forms\ContactForm\Contact_Form_Field::render_checkbox_field()
 	 */
 	public function test_make_sure_checkbox_field_renders_as_expected() {
 		$attributes = array(
@@ -1011,6 +1013,8 @@ class Contact_Form_Test extends BaseTestCase {
 
 	/**
 	 * Multiple fields
+	 *
+	 * @covers \Automattic\Jetpack\Forms\ContactForm\Contact_Form_Field::render_checkbox_multiple_field()
 	 */
 	public function test_make_sure_checkbox_multiple_field_renders_as_expected() {
 		$attributes          = array(
