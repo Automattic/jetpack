0c4c414048d24c2d4c3df15900ebeafc640d93eb
Daniel Richards <daniel.richards@automattic.com>
2025-04-16 13:22:40 +0800

    Fix field block examples (#43096)
    * Fix field block examples being altered by useFormWrapper
    
    useFormWrapper adds a form wrapper around any field that doesn't have one.
    
    It wasn't preserving the field's own inner blocks, which became visible in the block
    examples, where single or multiple choice fields have inner blocks but no wrapper.
    
    This PR ensures the inner blocks are preserved by calling getBlocks to get the inner
    blocks of the field and passing that as the inner blocks in the replaceBlocks call
    within useFormWrapper.
    
    * Small improvement to variable name
    
    * Add missing useEffect deps
    
    * Fix select/dropdown example
    
    * Switch e2e test close welcome guide code to use preferences

--- projects/packages/forms/src/blocks/shared/hooks/use-form-wrapper.js
+++ projects/packages/forms/src/blocks/shared/hooks/use-form-wrapper.js
@@ -6,29 +6,30 @@ import { __ } from '@wordpress/i18n';
 import { FORM_BLOCK_NAME } from '../util/constants';
 
 export default function useFormWrapper( { attributes, clientId, name } ) {
-	const BUTTON_BLOCK_NAME = 'jetpack/button';
-	const SUBMIT_BUTTON_ATTR = {
-		text: __( 'Submit', 'jetpack-forms' ),
-		element: 'button',
-		lock: { remove: true },
-	};
-
 	const { replaceBlock, __unstableMarkNextChangeAsNotPersistent } = useDispatch( blockEditorStore );
+	const { getBlocks } = useSelect( blockEditorStore );
 
-	const parents = useSelect( select => {
-		return select( blockEditorStore ).getBlockParentsByBlockName( clientId, FORM_BLOCK_NAME );
-	} );
+	const parentForms = useSelect(
+		select => {
+			return select( blockEditorStore ).getBlockParentsByBlockName( clientId, FORM_BLOCK_NAME );
+		},
+		[ clientId ]
+	);
 
 	useEffect( () => {
-		if ( ! parents?.length ) {
+		if ( ! parentForms?.length ) {
 			// As this is an automated update, ensure it doesn't end up in the undo stack
 			// by calling `__unstableMarkNextChangeAsNotPersistent`.
 			__unstableMarkNextChangeAsNotPersistent();
 			replaceBlock(
 				clientId,
 				createBlock( FORM_BLOCK_NAME, {}, [
-					createBlock( name, attributes ),
-					createBlock( BUTTON_BLOCK_NAME, SUBMIT_BUTTON_ATTR ),
+					createBlock( name, attributes, getBlocks( clientId ) ),
+					createBlock( 'jetpack/button', {
+						text: __( 'Submit', 'jetpack-forms' ),
+						element: 'button',
+						lock: { remove: true },
+					} ),
 				] )
 			);
 		}
