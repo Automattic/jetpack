77b0075ae8d97f3547f9a9a10ef3e1c3d859d16e
Enej Bajgoric <enej.bajgoric@automattic.com>
2025-04-16 14:21:57 -0700

    Revert the revert

--- projects/packages/forms/src/contact-form/class-contact-form-endpoint.php
+++ projects/packages/forms/src/contact-form/class-contact-form-endpoint.php
@@ -562,6 +562,27 @@ class Contact_Form_Endpoint extends \WP_REST_Posts_Controller {
 	}
 
 	/**
+	 * Check whether a given request has proper authorization to view feedback item.
+	 *
+	 * @param  WP_REST_Request $request Full details about the request.
+	 * @return WP_Error|boolean
+	 */
+	public function get_item_permissions_check( $request ) { //phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
+		if ( ! current_user_can( 'edit_pages' ) ) {
+			return false;
+		}
+		if ( ! is_user_member_of_blog( get_current_user_id(), get_current_blog_id() ) ) {
+			return new WP_Error(
+				'rest_cannot_view',
+				esc_html__( 'Sorry, you cannot view this resource.', 'jetpack-forms' ),
+				array( 'status' => 401 )
+			);
+		}
+
+		return true;
+	}
+
+	/**
 	 * Get status for all supported integrations.
 	 *
 	 * @return WP_REST_Response Response object.
