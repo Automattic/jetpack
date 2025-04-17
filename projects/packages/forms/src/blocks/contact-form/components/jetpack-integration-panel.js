9eb1891e22bcd71ea94fb1c8924b49eb4c16690a
Mikael Korpela <mikael@ihminen.org>
2025-04-17 11:52:38 +0300

    Consolidate toolbar and panel controls in a single file

--- projects/packages/forms/src/blocks/contact-form/components/jetpack-integration-panel.js
+++ projects/packages/forms/src/blocks/contact-form/components/jetpack-integration-panel.js
@@ -1,23 +1,57 @@
-import { Button } from '@wordpress/components';
+import { BlockControls } from '@wordpress/block-editor';
+import { ToolbarGroup, ToolbarButton, Button, PanelBody } from '@wordpress/components';
+import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import { plugins } from '@wordpress/icons';
+import IntegrationsModal from './jetpack-integrations-modal';
+import { useIntegrationsStatus } from './jetpack-integrations-modal/hooks/useIntegrationsStatus';
 
 /**
  * Integration Panel component.
  *
- * @param {object}   props                - Component props.
- * @param {Function} props.setIsModalOpen - Function to set modal open or closed
+ * @param {object}   props               - Component props.
+ * @param {object}   props.attributes    - Block attributes.
+ * @param {Function} props.setAttributes - Function to set block attributes.
  * @return {object} The IntegrationPanel component.
  */
-export default function IntegrationPanel( { setIsModalOpen } ) {
+export default function IntegrationPanel( { attributes, setAttributes } ) {
+	const [ isModalOpen, setIsModalOpen ] = useState( false );
+	const { integrations, refreshIntegrations } = useIntegrationsStatus();
+
 	return (
-		<div className="jetpack-forms-integration-panel">
-			<Button
-				variant="secondary"
-				onClick={ () => setIsModalOpen( true ) }
-				__next40pxDefaultSize={ true }
+		<>
+			<PanelBody
+				title={ __( 'Manage integrations', 'jetpack-forms' ) }
+				className="jetpack-contact-form__integrations-panel"
+				initialOpen={ false }
 			>
-				{ __( 'Manage integrations', 'jetpack-forms' ) }
-			</Button>
-		</div>
+				<div className="jetpack-forms-integration-panel">
+					<Button
+						variant="secondary"
+						onClick={ () => setIsModalOpen( true ) }
+						__next40pxDefaultSize={ true }
+					>
+						{ __( 'Manage integrations', 'jetpack-forms' ) }
+					</Button>
+				</div>
+			</PanelBody>
+
+			<BlockControls>
+				<ToolbarGroup>
+					<ToolbarButton icon={ plugins } onClick={ () => setIsModalOpen( true ) }>
+						{ __( 'Integrations', 'jetpack-forms' ) }
+					</ToolbarButton>
+				</ToolbarGroup>
+			</BlockControls>
+
+			<IntegrationsModal
+				isOpen={ isModalOpen }
+				onClose={ () => setIsModalOpen( false ) }
+				attributes={ attributes }
+				setAttributes={ setAttributes }
+				integrationsData={ integrations }
+				refreshIntegrations={ refreshIntegrations }
+			/>
+		</>
 	);
 }
