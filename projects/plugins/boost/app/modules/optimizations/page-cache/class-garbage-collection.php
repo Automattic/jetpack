a13c062e9e261549e67579a2cd9077aa21016315
Adnan Haque <adnan007.id@gmail.com>
2025-04-18 10:41:37 -0400

    Fix single garbage collection

--- projects/plugins/boost/app/modules/optimizations/page-cache/class-garbage-collection.php
+++ projects/plugins/boost/app/modules/optimizations/page-cache/class-garbage-collection.php
@@ -18,12 +18,29 @@ class Garbage_Collection {
 	}
 
 	public static function schedule_single_garbage_collection() {
-		wp_schedule_single_event( time(), self::ACTION, array( 'created_before' => time() ) );
+		$older_than = time();
+		wp_schedule_single_event( $older_than, self::ACTION, array( 'older_than' => $older_than ) );
 	}
 
-	public static function garbage_collect( $created_before = null ) {
+	/**
+	 * Garbage collect expired files.
+	 *
+	 * @param int|null $older_than The timestamp before which files should be deleted. If not provided, the files older than default cache duration will be deleted.
+	 */
+	public static function garbage_collect( $older_than = null ) {
+		$cache_ttl = JETPACK_BOOST_CACHE_DURATION;
+
+		/*
+		 * If an $older_than value is specified, use it to calculate the cache TTL.
+		 * By specifying $older_than, you can instruct garbage collection to apply on files created before a certain point in time.
+		 * This ensures garbage collection is not clearing files that were created after the request was made. Useful to avoid race conditions.
+		 */
+		if ( $older_than !== null ) {
+			$cache_ttl = time() - $older_than;
+		}
+
 		$cache = new Boost_Cache();
-		$cache->get_storage()->garbage_collect( $created_before );
+		$cache->get_storage()->garbage_collect( $cache_ttl );
 	}
 
 	/**
