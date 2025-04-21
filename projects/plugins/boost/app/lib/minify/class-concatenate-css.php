91ca8beb656c172444a29bcf63b76146cfabd7d5
Adnan Haque <adnan007.id@gmail.com>
2025-04-21 15:02:06 -0400

    Gracefully handle more overflow of max concat files

--- projects/plugins/boost/app/lib/minify/class-concatenate-css.php
+++ projects/plugins/boost/app/lib/minify/class-concatenate-css.php
@@ -195,12 +195,15 @@ class Concatenate_CSS extends WP_Styles {
 					}
 					continue;
 				} elseif ( count( $css ) > 1 ) {
-					$file_name = jetpack_boost_page_optimize_generate_concat_path( $css, $this->dependency_path_mapping );
+					$css_groups = array_chunk( $css, jetpack_boost_minify_concat_max_files() );
+					foreach ( $css_groups as $css_group ) {
+						$file_name = jetpack_boost_page_optimize_generate_concat_path( $css_group, $this->dependency_path_mapping );
 
-					if ( get_site_option( 'jetpack_boost_static_minification' ) ) {
-						$href = jetpack_boost_get_minify_url( $file_name . '.min.css' );
-					} else {
-						$href = $siteurl . jetpack_boost_get_static_prefix() . '??' . $file_name;
+						if ( get_site_option( 'jetpack_boost_static_minification' ) ) {
+							$href = jetpack_boost_get_minify_url( $file_name . '.min.css' );
+						} else {
+							$href = $siteurl . jetpack_boost_get_static_prefix() . '??' . $file_name;
+						}
 					}
 				} else {
 					$href = jetpack_boost_page_optimize_cache_bust_mtime( current( $css ), $siteurl );
