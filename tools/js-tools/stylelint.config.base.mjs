6d56ba324906629d4ef5227a77c83f69bc999ca0
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-04-23 13:11:57 -0600

    Add link to recommendation to disable no-descending-specificity if nesting is used

--- tools/js-tools/stylelint.config.base.mjs
+++ tools/js-tools/stylelint.config.base.mjs
@@ -22,6 +22,9 @@ const baseConfig = {
 		// In theory this is a good rule, but in practice it's a massive lift to resolve existing violations.
 		// Here's an example that has no good answers:
 		// https://github.com/Automattic/jetpack/blob/86e27497d4b8e0736cae61c325f017dedad16dbb/projects/js-packages/components/components/button/style.module.scss#L73-L94
+		//
+		// Even Stylelint suggests disabling this rule if nesting is used:
+		// https://github.com/stylelint/stylelint/issues/7844#issuecomment-2230857428
 		'no-descending-specificity': null,
 
 		// Disabled until a valid pattern has been decided on: https://github.com/WordPress/gutenberg/issues/28616
