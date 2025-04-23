80b9dd2f1771f99696df1fc909a2e9575b5d4542
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2025-04-23 16:40:41 +1000

    Clean up dead code in shared JetpackFieldControls

--- projects/packages/forms/src/blocks/shared/components/jetpack-field-controls.js
+++ projects/packages/forms/src/blocks/shared/components/jetpack-field-controls.js
@@ -9,73 +9,15 @@ import { __ } from '@wordpress/i18n';
 import ToolbarRequiredGroup from '../../contact-form/components/block-controls/toolbar-required-group';
 import JetpackFieldWidth from '../../contact-form/components/jetpack-field-width';
 import JetpackManageResponsesSettings from '../../contact-form/components/jetpack-manage-responses-settings';
-import useFormStyle from '../hooks/use-form-style';
-import { FORM_STYLE } from '../util/constants';
-import getBlockStyle from '../util/get-block-style';
 
 const JetpackFieldControls = ( {
 	attributes,
-	blockClassNames,
-	clientId,
 	id,
 	required,
 	setAttributes,
-	type,
 	width,
 	extraFieldSettings = [],
 } ) => {
-	const formStyle = useFormStyle( clientId );
-	const blockStyle = getBlockStyle( blockClassNames );
-	const isChoicesBlock = [ 'radio', 'checkbox' ].includes( type );
-
-	const optionColorLabel =
-		blockStyle === 'button'
-			? __( 'Button Text', 'jetpack-forms' )
-			: __( 'Option Text', 'jetpack-forms', 0 );
-
-	const inputColorLabel = isChoicesBlock
-		? optionColorLabel
-		: __( 'Field Text', 'jetpack-forms', 0 );
-
-	const backgroundColorLabel = isChoicesBlock
-		? __( 'Background', 'jetpack-forms' )
-		: __( 'Field Background', 'jetpack-forms', 0 );
-
-	const colorSettings = [
-		{
-			value: attributes.labelColor,
-			onChange: value => setAttributes( { labelColor: value } ),
-			label: __( 'Label Text', 'jetpack-forms' ),
-		},
-		{
-			value: attributes.inputColor,
-			onChange: value => setAttributes( { inputColor: value } ),
-			label: inputColorLabel,
-		},
-	];
-
-	if ( isChoicesBlock && blockStyle === 'button' ) {
-		colorSettings.push( {
-			value: attributes.buttonBackgroundColor,
-			onChange: value => setAttributes( { buttonBackgroundColor: value } ),
-			label: __( 'Button Background', 'jetpack-forms' ),
-		} );
-	}
-
-	if ( ! isChoicesBlock || formStyle === FORM_STYLE.OUTLINED ) {
-		colorSettings.push( {
-			value: attributes.fieldBackgroundColor,
-			onChange: value => setAttributes( { fieldBackgroundColor: value } ),
-			label: backgroundColorLabel,
-		} );
-
-		colorSettings.push( {
-			value: attributes.borderColor,
-			onChange: value => setAttributes( { borderColor: value } ),
-			label: __( 'Border', 'jetpack-forms' ),
-		} );
-	}
-
 	const setId = value => {
 		const newValue = value.replace( /[^a-zA-Z0-9_-]/g, '' );
 		setAttributes( { id: newValue } );
