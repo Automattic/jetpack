654703f91e39c238eb6497522c07e1becd07af00
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-04-23 14:13:46 +0530

    Move JP_CONNECTION_INITIAL_STATE  augmentation to the connection package
    The connection package should do its own job instead of other packages doing it for the package, resulting in duplication.

--- projects/packages/subscribers-dashboard/global.d.ts
+++ projects/packages/subscribers-dashboard/global.d.ts
@@ -482,58 +482,6 @@ interface Window {
 		userIsAdmin: string;
 		userIsNewToJetpack: string;
 	};
-	JP_CONNECTION_INITIAL_STATE: {
-		apiRoot: string;
-		apiNonce: string;
-		registrationNonce: string;
-		connectionStatus: {
-			isActive: boolean;
-			isStaging: boolean;
-			isRegistered: boolean;
-			isUserConnected: boolean;
-			hasConnectedOwner: boolean;
-			offlineMode: {
-				isActive: boolean;
-				constant: boolean;
-				url: boolean;
-				filter: boolean;
-				wpLocalConstant: boolean;
-			};
-			isPublic: boolean;
-		};
-		userConnectionData: {
-			currentUser: {
-				isConnected: boolean;
-				isMaster: boolean;
-				username: string;
-				id: number;
-				blogId: number;
-				wpcomUser: {
-					avatar: boolean;
-				};
-				gravatar: string;
-				permissions: {
-					admin_page?: boolean;
-					connect: boolean;
-					connect_user: boolean;
-					disconnect: boolean;
-					edit_posts?: boolean;
-					manage_modules?: boolean;
-					manage_options?: boolean;
-					manage_plugins?: boolean;
-					network_admin?: boolean;
-					network_sites_page?: boolean;
-					publish_posts?: boolean;
-					view_stats?: boolean;
-				};
-			};
-			connectionOwner: null;
-		};
-		connectedPlugins: object;
-		wpVersion: string;
-		siteSuffix: string;
-		connectionErrors: Array< string | object >;
-	};
 	myJetpackRest?: {
 		apiRoot: string;
 		apiNonce: string;
