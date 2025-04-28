95b6b0466dfd3474afeec1d5d3e123bfb5ec49cb
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2025-04-28 16:58:28 +1000

    Add icons for input, label, option, and options blocks (#43270)

--- projects/packages/forms/src/blocks/option/index.js
+++ projects/packages/forms/src/blocks/option/index.js
@@ -1,4 +1,6 @@
+import { Path } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+import renderMaterialIcon from '../contact-form/util/render-material-icon';
 import edit from './edit';
 import save from './save';
 
@@ -8,6 +10,9 @@ const settings = {
 	title: __( 'Option', 'jetpack-forms' ),
 	description: __( 'An option for a form choice field', 'jetpack-forms' ),
 	category: 'contact-form',
+	icon: {
+		src: renderMaterialIcon( <Path d="M5 11.25H19V12.75H5V11.25Z" /> ),
+	},
 	parent: [ 'jetpack/field-checkbox', 'jetpack/field-consent', 'jetpack/options' ],
 	supports: {
 		reusable: false,
