b10b920859b99d77a3fefa24beaabbd02c95386e
Erick Danzer <edanzer@gmail.com>
2025-04-29 15:32:37 -0600

    Add salesforce to forms modal

---
+++ projects/packages/forms/src/blocks/contact-form/components/jetpack-integrations-modal/salesforce-card.js
@@ -0,0 +1,93 @@
+import { TextControl, BaseControl, ExternalLink } from '@wordpress/components';
+import { useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import SalesforceIcon from '../../../../icons/salesforce';
+import HelpMessage from '../help-message';
+import IntegrationCard from './integration-card';
+
+const SalesforceCard = ( {
+	isExpanded,
+	onToggle,
+	data = {},
+	refreshStatus,
+	salesforceData = {},
+	setAttributes,
+} ) => {
+	const [ organizationIdError, setOrganizationIdError ] = useState( false );
+	const isValidOrgId = !! (
+		salesforceData.organizationId &&
+		salesforceData.organizationId.trim().match( /^[a-zA-Z0-9]{15,18}$/ )
+	);
+
+	const onHeaderToggleChange = value => {
+		setAttributes( {
+			salesforceData: {
+				...salesforceData,
+				sendToSalesforce: value,
+			},
+		} );
+	};
+
+	const setOrganizationId = value => {
+		setOrganizationIdError( false );
+		setAttributes( {
+			salesforceData: {
+				...salesforceData,
+				organizationId: value.trim(),
+			},
+		} );
+	};
+
+	const onBlurOrgIdField = e => {
+		setOrganizationIdError( ! e.target.value.trim().match( /^[a-zA-Z0-9]{15,18}$/ ) );
+	};
+
+	const cardData = {
+		...data,
+		title: __( 'Salesforce', 'jetpack-forms' ),
+		icon: <SalesforceIcon width={ 32 } height={ 22 } />,
+		showHeaderToggle: true,
+		headerToggleValue: salesforceData?.sendToSalesforce || false,
+		isHeaderToggleEnabled: true,
+		onHeaderToggleChange,
+		isConnected: isValidOrgId,
+		isLoading: typeof data.isInstalled === 'undefined',
+		refreshStatus,
+	};
+
+	return (
+		<IntegrationCard
+			title={ cardData.title }
+			icon={ cardData.icon }
+			isExpanded={ isExpanded }
+			onToggle={ onToggle }
+			cardData={ cardData }
+		>
+			<BaseControl __nextHasNoMarginBottom={ true }>
+				<TextControl
+					label={ __( 'Organization ID', 'jetpack-forms' ) }
+					value={ salesforceData.organizationId || '' }
+					placeholder={ __( 'Enter your Organization ID', 'jetpack-forms' ) }
+					onBlur={ onBlurOrgIdField }
+					onChange={ setOrganizationId }
+					help={ __( 'Enter the Salesforce organization ID to send Leads to.', 'jetpack-forms' ) }
+					__nextHasNoMarginBottom={ true }
+					__next40pxDefaultSize={ true }
+				/>
+				{ organizationIdError && (
+					<HelpMessage isError>
+						{ __(
+							'Invalid Organization ID. Should be a 15 – 18 characters long alphanumeric string.',
+							'jetpack-forms'
+						) }
+					</HelpMessage>
+				) }
+				<ExternalLink href="https://help.salesforce.com/s/articleView?id=000325251&type=1">
+					{ __( 'Where to find your Salesforce Organization ID', 'jetpack-forms' ) }
+				</ExternalLink>
+			</BaseControl>
+		</IntegrationCard>
+	);
+};
+
+export default SalesforceCard;
