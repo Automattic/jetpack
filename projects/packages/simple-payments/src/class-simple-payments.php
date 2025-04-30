f907f2270c3ea8c67bf170822191680a3facf7da
Allie Mims <allie.mims@automattic.com>
2025-04-30 14:10:39 -0400

    Generate simple-payments package

---
+++ projects/packages/simple-payments/src/class-simple-payments.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Package description here
+ *
+ * @package automattic/jetpack-simple-payments
+ */
+
+namespace Automattic\Jetpack;
+
+/**
+ * Class description.
+ */
+class Simple_Payments {
+
+	const PACKAGE_VERSION = '0.1.0-alpha';
+}
