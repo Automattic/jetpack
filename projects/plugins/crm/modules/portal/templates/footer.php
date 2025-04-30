0649d3efffdaaac92df03da7934507e72c26fef7
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-04-30 15:25:44 -0600

    Remove unused file

--- projects/plugins/crm/modules/portal/templates/footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Footer template?
- *
- * Is this actually used anywhere? If so, the reference is pretty well hidden.
- *
- * @package automattic/jetpack-crm
- * @see     https://jetpackcrm.com/kb/
- */
