fe50626849240cd2c2cf07c37582248e4138d5ca
William Viana <vianasw@gmail.com>
2025-05-08 20:01:38 +0200

    Fix LanguageDetection availability check

--- projects/js-packages/ai-client/src/chrome-ai/factory.ts
+++ projects/js-packages/ai-client/src/chrome-ai/factory.ts
@@ -86,6 +86,7 @@ export default async function ChromeAIFactory( promptArg: PromptProp ) {
 	// If the languageDetector is not available, we can't use the translation or summary features—it's safer to fall back
 	// to the default AI model than to risk an unexpected error.
 	if (
+		! ( 'ai' in self ) ||
 		! ( 'languageDetector' in self.ai ) ||
 		! self.ai.languageDetector.create ||
 		! self.ai.languageDetector.availability
