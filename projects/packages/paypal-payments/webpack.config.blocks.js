ea4dd570bd1d11a4f1f65a074bdafaa3a9ec40e7
Allie Mims <allie.mims@automattic.com>
2025-05-15 13:54:07 -0400

    Add webpack config

---
+++ projects/packages/paypal-payments/webpack.config.blocks.js
@@ -0,0 +1,95 @@
+/**
+ *WARNING: No ES6 modules here. Not transpiled! ****
+ */
+
+const path = require( 'path' );
+const jetpackWebpackConfig = require( '@automattic/jetpack-webpack-config/webpack' );
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
+
+/**
+ * Internal variables
+ */
+const sharedWebpackConfig = {
+	mode: jetpackWebpackConfig.mode,
+	devtool: jetpackWebpackConfig.devtool,
+	entry: {
+		editor: './src/block/simple-payments/editor.js',
+		view: './src/block/simple-payments/view.js',
+	},
+	output: {
+		...jetpackWebpackConfig.output,
+		path: path.join( __dirname, '../dist/block' ),
+	},
+	optimization: {
+		...jetpackWebpackConfig.optimization,
+	},
+	resolve: {
+		...jetpackWebpackConfig.resolve,
+	},
+	node: {},
+	plugins: [ ...jetpackWebpackConfig.StandardPlugins() ],
+	externals: {
+		...jetpackWebpackConfig.externals,
+		jetpackConfig: JSON.stringify( {
+			consumer_slug: 'jetpack-paypal-payments',
+		} ),
+	},
+	module: {
+		strictExportPresence: true,
+		rules: [
+			// Transpile JavaScript
+			jetpackWebpackConfig.TranspileRule( {
+				exclude: /node_modules\//,
+			} ),
+
+			// Transpile @automattic/* in node_modules too.
+			jetpackWebpackConfig.TranspileRule( {
+				includeNodeModules: [
+					'@automattic/',
+					'debug/',
+					'gridicons/',
+					'punycode/',
+					'query-string/',
+					'split-on-first/',
+					'strict-uri-encode/',
+				],
+			} ),
+
+			// Handle CSS.
+			jetpackWebpackConfig.CssRule( {
+				extensions: [ 'css', 'sass', 'scss' ],
+				extraLoaders: [
+					{
+						loader: 'postcss-loader',
+						options: {
+							// postcssOptions: { config: path.join( __dirname, 'postcss.config.js' ) },
+							postcssOptions: { plugins: [ require( 'autoprefixer' ) ] },
+						},
+					},
+					'sass-loader',
+				],
+			} ),
+
+			// Handle images.
+			jetpackWebpackConfig.FileRule(),
+		],
+	},
+};
+
+module.exports = [
+	{
+		...sharedWebpackConfig,
+		plugins: [
+			...sharedWebpackConfig.plugins,
+			new CopyWebpackPlugin( {
+				patterns: [
+					{
+						from: 'src/block/**/block.json',
+						to: '[name][ext]',
+						noErrorOnMissing: true,
+					},
+				],
+			} ),
+		],
+	},
+];
