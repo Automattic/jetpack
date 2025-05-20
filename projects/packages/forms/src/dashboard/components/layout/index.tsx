c649a04e5d1d555bca8e12f80ac951ef4eefa082
Douglas Henri <dhasilva@users.noreply.github.com>
2025-05-20 17:20:06 -0300

    Forms: Add Create Form button to dashboard header (#43529)
    * fix export button active color
    
    * make create form button green
    
    * add create form button to layout component
    
    * convert dashboard index to tsx
    
    * add lodash types to dev dependencies
    
    * changelog

--- projects/packages/forms/src/dashboard/components/layout/index.tsx
+++ projects/packages/forms/src/dashboard/components/layout/index.tsx
@@ -1,19 +1,35 @@
-import { JetpackFooter } from '@automattic/jetpack-components';
+/**
+ * External dependencies
+ */
+import { JetpackFooter, useBreakpointMatch } from '@automattic/jetpack-components';
 import { shouldUseInternalLinks } from '@automattic/jetpack-shared-extension-utils';
 import { TabPanel } from '@wordpress/components';
 import { useCallback } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import clsx from 'clsx';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+/**
+ * Internal dependencies
+ */
 import ExportResponsesButton from '../../inbox/export-responses';
 import { config } from '../../index';
+import CreateFormButton from '../create-form-button';
 import JetpackFormsLogo from '../logo';
-
 import './style.scss';
 
-const Layout = ( { className, showFooter } ) => {
+const Layout = ( {
+	className = '',
+	showFooter = false,
+}: {
+	className?: string;
+	showFooter?: boolean;
+} ) => {
 	const location = useLocation();
 	const navigate = useNavigate();
+	const [ isSm ] = useBreakpointMatch( 'sm' );
+	const createSmallLabel = __( 'Create', 'jetpack-forms' );
+	const createLargeLabel = __( 'Create form', 'jetpack-forms' );
+	const createButtonLabel = isSm ? createSmallLabel : createLargeLabel;
 
 	const enableIntegrationsTab = config( 'enableIntegrationsTab' );
 
@@ -41,7 +57,7 @@ const Layout = ( { className, showFooter } ) => {
 	};
 
 	const handleTabSelect = useCallback(
-		tabName => {
+		( tabName: string ) => {
 			if ( ! tabName ) {
 				tabName = config( 'hasFeedback' ) ? 'responses' : 'about';
 			}
@@ -59,7 +75,10 @@ const Layout = ( { className, showFooter } ) => {
 				<div className="jp-forms__logo-wrapper">
 					<JetpackFormsLogo />
 				</div>
-				{ getCurrentTab() === 'responses' && <ExportResponsesButton /> }
+				<div className="jp-forms__layout-header-actions">
+					{ getCurrentTab() === 'responses' && <ExportResponsesButton /> }
+					<CreateFormButton label={ createButtonLabel } />
+				</div>
 			</div>
 			<TabPanel
 				className="jp-forms__dashboard-tabs"
