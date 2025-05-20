d874ddbb9a15ddee0b953da86558b750b505e114
Allie Mims <allie.mims@automattic.com>
2025-05-20 15:05:23 -0400

    Update imports

--- projects/packages/paypal-payments/src/block/edit.js
+++ projects/packages/paypal-payments/src/block/edit.js
@@ -15,13 +15,12 @@ import { getWidgetIdFromBlock } from '@wordpress/widgets';
 import clsx from 'clsx';
 import emailValidator from 'email-validator';
 import { get, isEmpty, pick, trimEnd } from 'lodash';
-import { getCurrencyDefaults } from '../../shared/currencies';
-import HelpMessage from '../../shared/help-message';
 import { SIMPLE_PAYMENTS_PRODUCT_POST_TYPE, SUPPORTED_CURRENCY_LIST } from './constants';
 import { PanelControls } from './controls';
 import FeaturedMedia from './featured-media';
+import HelpMessage from './help-message';
 import ProductPlaceholder from './product-placeholder';
-import { decimalPlaces, formatPrice } from './utils';
+import { decimalPlaces, formatPrice, getCurrencyDefaults } from './utils';
 
 export const SimplePaymentsEdit = ( {
 	attributes,
