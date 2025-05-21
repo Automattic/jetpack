895859a6b6e298b3fa0189522f9b3c310ce1068d
Allie Mims <allie.mims@automattic.com>
2025-05-21 14:10:59 -0400

    Bring in block-fixtures.js and dep Update validate.js

--- projects/packages/paypal-payments/src/block/test/validate.js
+++ projects/packages/paypal-payments/src/block/test/validate.js
@@ -1,9 +1,9 @@
-import runBlockFixtureTests from '../../../shared/test/block-fixtures';
 import metadata from '../block.json';
 import deprecatedV1 from '../deprecated/v1';
 import deprecatedV2 from '../deprecated/v2';
 import edit from '../edit';
 import save from '../save';
+import runBlockFixtureTests from './block-fixtures';
 
 const intlNumberFormatSpy = jest.spyOn( Intl, 'NumberFormat' );
 beforeEach( () => {
