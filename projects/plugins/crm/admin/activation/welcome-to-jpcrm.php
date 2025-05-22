52baa66b90dc945e95c43bcceb3cfc491e065492
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-05-22 15:53:24 -0600

    Move relevant bootstrap CSS to jpcrm-welcome-wizard.scss

--- projects/plugins/crm/admin/activation/welcome-to-jpcrm.php
+++ projects/plugins/crm/admin/activation/welcome-to-jpcrm.php
@@ -28,7 +28,6 @@ if ( ! defined( 'ZEROBSCRM_PATH' ) ) {
 		wp_enqueue_script( 'zbswelcomewizard', plugins_url( '/js/welcome-to-zbs/wizard2' . wp_scripts_get_suffix() . '.js', ZBS_ROOTFILE ), array( 'jquery' ), $zbs::VERSION );
 
 		// css
-		wp_enqueue_style( 'zbswelcomebootstrap', plugins_url( '/build/lib/bootstrap/bootstrap.min.css', ZBS_ROOTFILE ), array(), $zbs::VERSION );
 		wp_enqueue_style( 'zbswelcomeloadstyles', plugins_url( '/css/welcome-to-zbs/loadstyles' . wp_scripts_get_suffix() . '.css', ZBS_ROOTFILE ), array(), $zbs::VERSION );
 		wp_enqueue_style( 'zbswelcomeopensans', plugins_url( '/css/welcome-to-zbs/opensans' . wp_scripts_get_suffix() . '.css', ZBS_ROOTFILE ), array(), $zbs::VERSION );
 		wp_enqueue_style( 'zbswelcomeadmin', plugins_url( '/css/welcome-to-zbs/admin.min.css', ZBS_ROOTFILE ), array(), $zbs::VERSION );
