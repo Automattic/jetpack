7b1a6c9e6de750727a9d5313a5f4922425b75b68
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-05-22 06:30:31 -0600

    Switch quote type
    Co-authored-by: Karen Attfield <karenlattfield@gmail.com>

--- projects/plugins/crm/modules/portal/templates/invoices.php
+++ projects/plugins/crm/modules/portal/templates/invoices.php
@@ -41,7 +41,7 @@ $invoice_endpoint = $portal->get_endpoint( ZBS_TYPE_INVOICE );
 	<nav class="zbs-portal-nav">
 	<?php $portal->render->portal_nav( $invoice_endpoint ); ?>
 	</nav>
-	<div class='zbs-portal-content zbs-portal-invoices-list'>
+	<div class="zbs-portal-content zbs-portal-invoices-list">
 		<h2><?php esc_html_e( 'Invoices', 'zero-bs-crm' ); ?></h2>
 		<div class="zbs-entry-content zbs-responsive-table" style="position:relative;">
 			<?php
