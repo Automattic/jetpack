60c7b4ff85fc2eb1e056997265eecdf0fe32cb22
Allison Levine <1689238+allilevine@users.noreply.github.com>
2025-05-23 11:10:25 -0400

    Blogging prompt: Make sure block is registered before inserting (#43580)
    * Blogging prompt: wait for registration to complete before inserting.
    
    * changelog

--- projects/plugins/jetpack/extensions/blocks/blogging-prompt/editor.js
+++ projects/plugins/jetpack/extensions/blocks/blogging-prompt/editor.js
@@ -1,4 +1,4 @@
-import { createBlock } from '@wordpress/blocks';
+import { createBlock, getBlockType } from '@wordpress/blocks';
 import { dispatch } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 import { registerJetpackBlockFromMetadata } from '../../shared/register-jetpack-block';
@@ -13,36 +13,53 @@ import save from './save';
 import './editor.scss';
 import './style.scss';
 
-registerJetpackBlockFromMetadata( metadata, {
-	edit,
-	save,
-	example: {
-		attributes: {
-			answersLink: 'https://wordpress.com/tag/dailyprompt',
-			answersLinkText: __( 'View all responses', 'jetpack' ),
-			gravatars: [ { url: avatar1 }, { url: avatar2 }, { url: avatar3 } ],
-			promptLabel: __( 'Daily writing prompt', 'jetpack' ),
-			promptText: __( "What's your favorite place to visit?", 'jetpack' ),
-			promptFetched: true,
-			promptId: 1234,
-			showResponses: true,
-			showLabel: true,
-			tagsAdded: true,
-			isBloganuary: false,
-		},
-	},
-} );
+// Register the block and return a promise that resolves when registration is complete
+const registerBlock = () =>
+	new Promise( resolve => {
+		registerJetpackBlockFromMetadata( metadata, {
+			edit,
+			save,
+			example: {
+				attributes: {
+					answersLink: 'https://wordpress.com/tag/dailyprompt',
+					answersLinkText: __( 'View all responses', 'jetpack' ),
+					gravatars: [ { url: avatar1 }, { url: avatar2 }, { url: avatar3 } ],
+					promptLabel: __( 'Daily writing prompt', 'jetpack' ),
+					promptText: __( "What's your favorite place to visit?", 'jetpack' ),
+					promptFetched: true,
+					promptId: 1234,
+					showResponses: true,
+					showLabel: true,
+					tagsAdded: true,
+					isBloganuary: false,
+				},
+			},
+		} );
+
+		// Wait for next tick to ensure registration is complete
+		setTimeout( resolve, 0 );
+	} );
 
 async function insertTemplate( promptId ) {
 	await waitForEditor();
 
-	const { insertBlocks } = dispatch( 'core/block-editor' );
-	const bloggingPromptBlocks = [
-		createBlock( 'jetpack/blogging-prompt', { promptFetched: false, promptId, tagsAdded: true } ),
-		createBlock( 'core/paragraph' ),
-	];
+	// Ensure block is registered before insertion
+	const blockType = getBlockType( 'jetpack/blogging-prompt' );
+	if ( ! blockType ) {
+		await registerBlock();
+	}
 
-	insertBlocks( bloggingPromptBlocks, 0, undefined, false );
+	const { insertBlocks } = dispatch( 'core/block-editor' );
+
+	const bloggingPromptBlock = createBlock( 'jetpack/blogging-prompt', {
+		promptFetched: false,
+		promptId,
+		tagsAdded: true,
+	} );
+
+	const paragraphBlock = createBlock( 'core/paragraph' );
+
+	insertBlocks( [ bloggingPromptBlock, paragraphBlock ], 0, undefined, false );
 }
 
 function initBloggingPrompt() {
