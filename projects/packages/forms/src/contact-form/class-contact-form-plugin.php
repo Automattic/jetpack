003e2dd2b25c268aecd6d358e86caec6a6286f88
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2025-05-23 14:56:12 +1000

    Forms: Fix custom classnames support in inner blocks migration (#43564)

--- projects/packages/forms/src/contact-form/class-contact-form-plugin.php
+++ projects/packages/forms/src/contact-form/class-contact-form-plugin.php
@@ -346,6 +346,7 @@ class Contact_Form_Plugin {
 		$color_styles      = \wp_apply_colors_support( $block_type, $attrs );
 		$typography_styles = \wp_apply_typography_support( $block_type, $attrs );
 		$border_styles     = \wp_apply_border_support( $block_type, $attrs );
+		$custom_classname  = \wp_apply_custom_classname_support( $block_type, $attrs );
 
 		// Merge all the block support classes and styles.
 		$classes = array_filter(
@@ -353,6 +354,7 @@ class Contact_Form_Plugin {
 				$color_styles['class'] ?? '',
 				$typography_styles['class'] ?? '',
 				$border_styles['class'] ?? '',
+				$custom_classname['class'] ?? '',
 			),
 			'strlen'
 		);
