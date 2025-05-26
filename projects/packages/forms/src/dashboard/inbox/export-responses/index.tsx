5d15bddb9c07c5294ab0eb850066de7f49dde54b
Douglas <douglas.henri@automattic.com>
2025-05-26 19:26:55 -0300

    extract export responses modal component

--- projects/packages/forms/src/dashboard/inbox/export-responses/index.tsx
+++ projects/packages/forms/src/dashboard/inbox/export-responses/index.tsx
@@ -1,16 +1,14 @@
 /**
  * External dependencies
  */
-// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
-import { Modal, Button, __experimentalVStack as VStack } from '@wordpress/components';
+import { Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { download } from '@wordpress/icons';
 /**
  * Internal dependencies
  */
+import ExportResponsesModal from '../../components/export-responses-modal';
 import useExportResponses from '../../hooks/use-export-responses';
-import CSVExport from './csv';
-import GoogleDriveExport from './google-drive';
 
 import './style.scss';
 
@@ -32,20 +30,13 @@ const ExportResponsesButton = () => {
 			>
 				{ __( 'Export', 'jetpack-forms' ) }
 			</Button>
+
 			{ showExportModal && (
-				<Modal
-					title={ __( 'Export your Form Responses', 'jetpack-forms' ) }
+				<ExportResponsesModal
 					onRequestClose={ closeModal }
-					size="large"
-				>
-					<VStack spacing={ 8 }>
-						<p className="jp-forms__export-modal-header-subtitle">
-							{ __( 'Choose your favorite file format or export destination:', 'jetpack-forms' ) }
-						</p>
-						<CSVExport onExport={ onExport } />
-						<GoogleDriveExport onExport={ onExport } autoConnect={ autoConnectGdrive } />
-					</VStack>
-				</Modal>
+					onExport={ onExport }
+					autoConnectGdrive={ autoConnectGdrive }
+				/>
 			) }
 		</>
 	);
