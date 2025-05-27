f97c13034b134b74ea6e4899ff8b4f684c9d2c43
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2025-05-27 10:12:49 +1000

    Try to fix tests

--- projects/packages/forms/tests/php/contact-form/Contact_Form_Plugin_Test.php
+++ projects/packages/forms/tests/php/contact-form/Contact_Form_Plugin_Test.php
@@ -439,7 +439,7 @@ class Contact_Form_Plugin_Test extends BaseTestCase {
 					'type'                     => 'checkbox-multiple',
 					'requiredText'             => 'Do it again',
 					'fieldwrapperclasses'      => 'wp-block-jetpack-field-checkbox-multiple is-style-button  is-style-button-wrap',
-					'optionsclasses'           => ' has-text-color has-background',
+					'optionsclasses'           => ' has-background',
 					'optionsstyles'            => 'color:blue-overture;background-color:green-tonight; border-top-width:2px;border-top-color:terrible-red;border-top-style:solid;',
 					'stylevariationclasses'    => ' has-background',
 					'stylevariationattributes' => '{"border":{"top":{"color":"terrible-red","width":"2px","style":"solid","radius":"10px"}},"color":{"background":"green-tonight"}}',
