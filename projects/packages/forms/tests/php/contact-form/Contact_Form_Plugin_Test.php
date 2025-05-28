7298e8a8bb19e8e8f05c4ffff482e47e0f9166d2
Daniel Richards <daniel.richards@automattic.com>
2025-05-28 09:15:51 +0800

    Forms: Remove options text color support (#43612)
    * Remove text color support for jetpack/options, since frontend does not replicate the markup of the editor and so style parity cannot be achieved
    
    * Try to fix tests
    
    * Whoops, wrong flag
    
    * One more time
    
    ---------
    
    Co-authored-by: Andrew Serong <14988353+andrewserong@users.noreply.github.com>

--- projects/packages/forms/tests/php/contact-form/Contact_Form_Plugin_Test.php
+++ projects/packages/forms/tests/php/contact-form/Contact_Form_Plugin_Test.php
@@ -439,8 +439,8 @@ class Contact_Form_Plugin_Test extends BaseTestCase {
 					'type'                     => 'checkbox-multiple',
 					'requiredText'             => 'Do it again',
 					'fieldwrapperclasses'      => 'wp-block-jetpack-field-checkbox-multiple is-style-button  is-style-button-wrap',
-					'optionsclasses'           => ' has-text-color has-background',
-					'optionsstyles'            => 'color:blue-overture;background-color:green-tonight; border-top-width:2px;border-top-color:terrible-red;border-top-style:solid;',
+					'optionsclasses'           => ' has-background',
+					'optionsstyles'            => 'background-color:green-tonight; border-top-width:2px;border-top-color:terrible-red;border-top-style:solid;',
 					'stylevariationclasses'    => ' has-background',
 					'stylevariationattributes' => '{"border":{"top":{"color":"terrible-red","width":"2px","style":"solid","radius":"10px"}},"color":{"background":"green-tonight"}}',
 					'stylevariationstyles'     => 'background-color:green-tonight; border-top-width:2px;border-top-color:terrible-red;border-top-style:solid;',
@@ -480,7 +480,6 @@ class Contact_Form_Plugin_Test extends BaseTestCase {
 								),
 								'color'  => array(
 									'background' => 'green-tonight',
-									'text'       => 'blue-overture',
 								),
 							),
 						),
