fcb87be841252d403eb09257c9d7f10de38a2c88
Enej Bajgoric <enej.bajgoric@automattic.com>
2025-05-29 13:15:06 -0700

    JWT: add package

---
+++ projects/packages/jwt/src/class-jwt.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Package description here
+ *
+ * @package automattic/jetpack-jwt
+ */
+
+namespace Automattic\Jetpack;
+
+/**
+ * Class description.
+ */
+class Jwt {
+
+	const PACKAGE_VERSION = '0.1.0-alpha';
+}
