d0966f6f85395f3f0103f8046e4df6a4590053c8
Enej Bajgoric <enej.bajgoric@automattic.com>
2025-05-29 13:38:50 -0700

    Remove templateLock

--- projects/packages/forms/src/blocks/field-file/edit.js
+++ projects/packages/forms/src/blocks/field-file/edit.js
@@ -43,7 +43,6 @@ export default function FileFieldEdit( props ) {
 				},
 			],
 		],
-		templateLock: 'insert',
 		allowedBlocks: [ 'jetpack/dropzone', 'jetpack/label' ],
 		renderAppender: false,
 	} );
