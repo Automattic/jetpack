748c7b90198136f42cc6f071f13cf0f68c6dbae1
Ramon <ramonjd@users.noreply.github.com>
2025-05-30 15:03:52 +1000

    Forms: tame select arrow positioning (#43674)
    * initial commit - adding extra markup to the select rendering and fixing positioning of labels
    
    * uncommenting
    
    * Remove default select border
    
    * Update select field rendering to include a wrapper div with the correct class structure for improved styling and consistency in tests.
    
    * Update contact form styles to inherit text color and adjust z-index for select wrapper elements
    
    * Only run `getAttribute` on select wrapper if there is one
    
    * Reinstate padding change
    
    * phan me
    
    * phanos
    
    * whitespace

--- projects/packages/forms/tests/php/contact-form/Contact_Form_Test.php
+++ projects/packages/forms/tests/php/contact-form/Contact_Form_Test.php
@@ -1484,8 +1484,11 @@ class Contact_Form_Test extends BaseTestCase {
 				'label for does not equal input name!'
 			);
 
-			$this->assertEquals( $select->getAttribute( 'class' ), 'select ' . $attributes['class'] . ' grunion-field', ' select class does not match expected' );
+			$select_wrapper = $wrapper_div->getElementsByTagName( 'div' )->item( 0 );
+			// @phan-suppress-next-line PhanUndeclaredMethod
+			$select_wrapper_class = $select_wrapper->getAttribute( 'class' ) ?? '';
 
+			$this->assertEquals( 'contact-form__select-wrapper select ' . $attributes['class'] . ' grunion-field', $select_wrapper_class, ' select class does not match expected' );
 			// Options.
 			$options = $select->getElementsByTagName( 'option' );
 			$n       = $options->length;
