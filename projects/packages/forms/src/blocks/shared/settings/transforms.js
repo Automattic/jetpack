dc52fe535734520be82771ea4d155bb0f2530a85
Ramon <ramonjd@gmail.com>
2025-06-02 14:23:53 +1000

    Enhance select field transformation by adding default placeholder for input block. Update contact form plugin to correctly reference placeholder attribute. Adjust z-index values in CSS for improved label positioning.

--- projects/packages/forms/src/blocks/shared/settings/transforms.js
+++ projects/packages/forms/src/blocks/shared/settings/transforms.js
@@ -129,6 +129,21 @@ const createTextFieldInnerBlocks = ( blockName, existingInnerBlocks = [] ) => {
 
 	const existingLabel = existingInnerBlocks.find( block => block.name === 'jetpack/label' );
 	const existingInput = existingInnerBlocks.find( block => block.name === 'jetpack/input' );
+	const existingInputAttributes = existingInput?.attributes ?? {};
+
+	if ( blockName === 'jetpack/field-select' ) {
+		/**
+		 * When transforming to a select field, add a default placeholder to the input block if none exists.
+		 * The select block's edit.js checks for the placeholder attribute on the input block,
+		 * and add the `has-placeholder` class to the field if it exists.
+		 * However if the incoming transformed block doesn't have a placeholder, the edit.js will
+		 * not add the class, BUT adds a default placeholder anyway.
+		 */
+
+		if ( ! existingInputAttributes?.placeholder ) {
+			existingInputAttributes.placeholder = __( 'Select one option', 'jetpack-forms' );
+		}
+	}
 
 	return [
 		createBlock( 'jetpack/label', {
@@ -137,7 +152,7 @@ const createTextFieldInnerBlocks = ( blockName, existingInnerBlocks = [] ) => {
 			defaultLabel: config.defaultLabel,
 		} ),
 		createBlock( 'jetpack/input', {
-			...( existingInput?.attributes || {} ),
+			...existingInputAttributes,
 			type: config.type,
 		} ),
 	];
