e854460948f0b9e5cc85185840a3e49a37dd19ec
Mikael Korpela <mikael@ihminen.org>
2025-06-04 23:09:35 +0300

    Forms: render clickable email at inbox (#43771)

--- projects/packages/forms/src/dashboard/inbox/response.js
+++ projects/packages/forms/src/dashboard/inbox/response.js
@@ -37,6 +37,13 @@ const renderFieldValue = value => {
 			</div>
 		);
 	}
+
+	// Emails
+	const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+	if ( emailRegex.test( value ) ) {
+		return <a href={ `mailto:${ value }` }>{ value }</a>;
+	}
+
 	return value;
 };
 
