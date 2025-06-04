8dbd4f4e87b20a071332520cce0de52fa9733667
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2025-06-04 17:54:30 +0800

    Add BarListChart component and related stories, types, and tests
    - Introduced the BarListChart component for rendering horizontal bar charts with customizable labels and values.
    - Created index and story files for the BarListChart, including examples for default, multi-series, and custom label/value components.
    - Added sample data for testing and demonstration purposes.
    - Implemented unit tests to ensure proper rendering and functionality of the BarListChart with various data scenarios.

---
+++ projects/js-packages/charts/src/components/bar-list-chart/bar-list-chart.tsx
@@ -0,0 +1,272 @@
+import { formatNumberCompact } from '@automattic/number-formatters';
+import { Group } from '@visx/group';
+import { createScale, scaleBand } from '@visx/scale';
+import { Text, type TextProps } from '@visx/text';
+import { useMemo } from 'react';
+import { BarChart } from '../bar-chart';
+import { withResponsive } from '../shared/with-responsive';
+import type { SeriesData } from '../..';
+import type { ScaleOptions } from '../../types';
+import type { BarChartProps } from '../bar-chart/bar-chart';
+import type { AxisRendererProps, AxisScale } from '@visx/axis';
+import type { AnyD3Scale } from '@visx/scale';
+import type { ComponentType, FC } from 'react';
+
+interface BarListChartProps
+	extends Exclude< BarChartProps, 'orientation' | 'size' | 'gridVisibility' > {
+	options?: {
+		/**
+		 * Scale for the y axis
+		 */
+		yScale: Omit< ScaleOptions, 'type' >;
+		/**
+		 * Scale for the x axis
+		 */
+		xScale: Omit< ScaleOptions, 'type' >;
+		/**
+		 * Formatter for the label
+		 */
+		labelFormatter?: ( value: string ) => string;
+		/**
+		 * Formatter for the value
+		 */
+		valueFormatter?: ( value: number ) => string;
+		/**
+		 * y offset of the label and value
+		 */
+		yOffset?: number;
+		/**
+		 * x position of the label
+		 */
+		labelPosition?: number;
+		/**
+		 * x position of the value
+		 */
+		valuePosition?: number;
+		/**
+		 * Custom render function for the label
+		 */
+		labelComponent?: ComponentType< RenderLabelProps >;
+		/**
+		 * Custom render function for the value
+		 */
+		valueComponent?: ComponentType< RenderValueProps >;
+	};
+}
+
+interface RenderLabelProps {
+	textProps: TextProps;
+	x: number;
+	y: number;
+	label: string;
+	formatter: ( value: string ) => string;
+}
+
+interface RenderValueProps {
+	textProps: TextProps;
+	x: number;
+	y: number;
+	value: number;
+	/**
+	 * Original data
+	 */
+	data: SeriesData[];
+	/**
+	 * Index of the data point
+	 */
+	index: number;
+	formatter: ( value: number ) => string;
+}
+
+/**
+ * Get the bandwidth of a scale
+ * @param scale - The scale to get the bandwidth of
+ * @return The bandwidth of the scale
+ */
+const getScaleBandwidth = < Scale extends AxisScale >( scale?: Scale ) => {
+	// Broaden type before using 'xxx' in s as typeguard.
+	const s = scale as AxisScale;
+	return s && 'bandwidth' in s ? s?.bandwidth() ?? 0 : 0;
+};
+
+const DefaultLabelComponent = ( { textProps, x, y, label, formatter } ) => {
+	return (
+		<Text { ...textProps } textAnchor="start" x={ x } y={ y }>
+			{ formatter( label ) }
+		</Text>
+	);
+};
+
+const DefaultValueComponent = ( { textProps, x, y, value, formatter }: RenderValueProps ) => {
+	return (
+		<Text { ...textProps } textAnchor="end" x={ x } y={ y } fontWeight={ 500 }>
+			{ formatter( value ) }
+		</Text>
+	);
+};
+
+const AxisRenderer = ( {
+	ticks,
+	tickLabelProps,
+	yOffset,
+	labelPosition,
+	valuePosition,
+	data,
+	labelFormatter,
+	valueFormatter,
+	LabelComponent = DefaultLabelComponent,
+	ValueComponent = DefaultValueComponent,
+}: AxisRendererProps< AnyD3Scale > & {
+	yOffset: number;
+	labelPosition: number;
+	valuePosition: number;
+	data: SeriesData[];
+	labelFormatter?: ( value: string ) => string;
+	valueFormatter?: ( value: number ) => string;
+	LabelComponent?: ComponentType< RenderLabelProps >;
+	ValueComponent?: ComponentType< RenderValueProps >;
+} ) => {
+	if ( ticks.length === 0 ) {
+		return null;
+	}
+
+	// compute the max tick label size to compute label offset
+	const allTickLabelProps = ticks.map( ( { value, index } ) =>
+		typeof tickLabelProps === 'function' ? tickLabelProps( value, index, ticks ) : {}
+	);
+
+	return ticks.map( ( { from, formattedValue }, index ) => {
+		const textProps = allTickLabelProps[ index ] ?? {};
+		// No need to pass textAnchor and dx. It will be handled by the component.
+		delete textProps.textAnchor;
+		delete textProps.dx;
+
+		const sum = data.reduce(
+			( acc, { data: seriesData } ) => acc + ( seriesData[ index ]?.value ?? 0 ),
+			0
+		);
+		const y = from.y + yOffset;
+
+		return (
+			<Group key={ index }>
+				<LabelComponent
+					textProps={ textProps }
+					x={ labelPosition }
+					y={ y }
+					label={ formattedValue }
+					formatter={ labelFormatter }
+				/>
+				<ValueComponent
+					textProps={ textProps }
+					x={ valuePosition }
+					y={ y }
+					value={ sum }
+					formatter={ valueFormatter }
+					data={ data }
+					index={ index }
+				/>
+			</Group>
+		);
+	} );
+};
+
+const BarListChart: FC< BarListChartProps > = ( {
+	data,
+	width,
+	height,
+	options = {},
+	margin = {
+		left: 0,
+		right: 20,
+		bottom: 0,
+		top: 0,
+	},
+	...rest
+} ) => {
+	const isMultiSeries = data.length > 1;
+	const chartOptions = useMemo( () => {
+		const defaultYScale = {
+			paddingInner: isMultiSeries ? 0.3 : 0.1,
+			padding: isMultiSeries ? 0.3 : 0.1,
+		};
+		const defaultXScale = {
+			zero: true,
+		};
+
+		return {
+			yScale: {
+				...defaultYScale,
+				...( options.yScale ?? {} ),
+			},
+			xScale: {
+				...defaultXScale,
+				...( options.xScale ?? {} ),
+			},
+			labelPosition: options.labelPosition ?? ( isMultiSeries ? 0 : 10 ),
+			valueFormatter: options.valueFormatter ?? ( value => formatNumberCompact( value ) ),
+			labelFormatter: options.labelFormatter ?? ( value => String( value ) ),
+			valuePosition: options.valuePosition ?? width,
+			yOffset: options.yOffset,
+		};
+	}, [ isMultiSeries, options, width ] );
+
+	// For multi series, we need to offset the bars to avoid overlapping, 6 is the gap between bars
+	const defaultYOffset = useMemo( () => {
+		const dataKeys = data.map( ( { label } ) => label );
+
+		const yScale = createScale< string >( {
+			type: 'band' as const,
+			range: [ 0, height ],
+			domain: dataKeys,
+			...chartOptions.yScale,
+		} );
+
+		const groupScale = scaleBand< string >( {
+			domain: dataKeys,
+			range: [ 0, getScaleBandwidth( yScale ) ],
+			padding: chartOptions.yScale.paddingInner,
+		} );
+
+		const barThickness = getScaleBandwidth( groupScale );
+
+		return isMultiSeries ? -( barThickness + 6 ) : 0;
+	}, [ data, chartOptions.yScale, height, isMultiSeries ] );
+
+	return (
+		<BarChart
+			orientation="horizontal"
+			gridVisibility={ 'none' }
+			data={ data }
+			width={ width }
+			height={ height }
+			margin={ margin }
+			options={ {
+				axis: {
+					y: {
+						children: ( renderProps: AxisRendererProps< AnyD3Scale > ) => (
+							<AxisRenderer
+								{ ...renderProps }
+								data={ data }
+								yOffset={ chartOptions.yOffset ?? defaultYOffset }
+								labelPosition={ chartOptions.labelPosition }
+								valuePosition={ chartOptions.valuePosition }
+								labelFormatter={ chartOptions.labelFormatter }
+								valueFormatter={ chartOptions.valueFormatter }
+								LabelComponent={ options.labelComponent }
+								ValueComponent={ options.valueComponent }
+							/>
+						),
+					},
+					x: {
+						children: () => null,
+					},
+				},
+				xScale: chartOptions.xScale,
+				yScale: chartOptions.yScale,
+			} }
+			{ ...rest }
+		/>
+	);
+};
+
+export default withResponsive< BarListChartProps >( BarListChart );
