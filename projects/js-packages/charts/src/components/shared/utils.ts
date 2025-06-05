7f96f6e1a7a7050eb037057f72db41f0eee962e9
Jasper Kang <jasper.kang@automattic.com>
2025-06-05 16:53:38 +1200

    add parseLocalDate

--- projects/js-packages/charts/src/components/shared/utils.ts
+++ projects/js-packages/charts/src/components/shared/utils.ts
@@ -23,3 +23,31 @@ export const getLongestTickWidth = < T extends AnyD3Scale >(
 
 	return getStringWidth( longestTick, labelStyle );
 };
+
+/**
+ * Parse a date string into a Date object in the timezone of browser.
+ * @param {string|number} dateString - YYYY-MM-DD format or a timestamp
+ * @return {Date} A Date object in the timezone of the browser.
+ */
+export const parseLocalDate = dateString => {
+	let validDateString = dateString;
+
+	const dateStringSplits = dateString.split( ' ' );
+	// For date strings like '2025-01-01 01:00:00'.
+	if ( dateStringSplits.length === 2 ) {
+		validDateString = `${ dateStringSplits[ 0 ] }T${ dateStringSplits[ 1 ] }Z`;
+	} else if ( dateStringSplits.length === 1 ) {
+		validDateString = `${ dateStringSplits[ 0 ] }T00:00:00Z`;
+	}
+
+	// Compatible with Date object.
+	const date = new Date( validDateString );
+	if ( isNaN( date.getTime() ) ) {
+		return date;
+	}
+
+	// Adjust the date to the timezone of the browser.
+	date.setMinutes( date.getMinutes() + date.getTimezoneOffset() );
+
+	return date;
+};
