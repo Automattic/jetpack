c21e12fc9ae705e2ea5226cf5997492fcf0cfbec
Christian Gastrell <cgastrell@gmail.com>
2025-06-09 16:47:28 -0300

    add track event for upsell nudge on file upload block

--- projects/packages/forms/src/blocks/shared/components/upsell-nudge/index.js
+++ projects/packages/forms/src/blocks/shared/components/upsell-nudge/index.js
@@ -1,9 +1,15 @@
+import jetpackAnalytics from '@automattic/jetpack-analytics';
 import { useUpgradeFlow } from '@automattic/jetpack-shared-extension-utils';
 import { Nudge } from '@automattic/jetpack-shared-extension-utils/components';
 import { __ } from '@wordpress/i18n';
 
 export const UpsellNudge = ( { requiredPlan } ) => {
-	const [ checkoutUrl, goToCheckoutPage, isRedirecting ] = useUpgradeFlow( requiredPlan );
+	const [ checkoutUrl, goToCheckoutPage, isRedirecting ] = useUpgradeFlow( requiredPlan, () => {
+		jetpackAnalytics.tracks.recordEvent( 'jetpack_forms_file_upload_upsell_click', {
+			plan: requiredPlan,
+			context: 'block-editor',
+		} );
+	} );
 	return (
 		<div className="jetpack-forms-upsell-nudge">
 			<Nudge
