18ea0114a96dedf2750e1cf900c818319c924b69
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-06-10 14:54:45 -0600

    Update projects/plugins/jetpack/modules/carousel/jetpack-carousel.php
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>

--- projects/plugins/jetpack/modules/carousel/jetpack-carousel.php
+++ projects/plugins/jetpack/modules/carousel/jetpack-carousel.php
@@ -380,7 +380,7 @@ class Jetpack_Carousel {
 
 		$this->enqueue_assets();
 
-		if ( ! isset( $post ) || ! $post instanceof WP_Post ) {
+		if ( ! $post instanceof WP_Post ) {
 			return $block_content;
 		}
 
