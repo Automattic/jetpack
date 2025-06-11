c6d0f56983472f71399c4987367403a2db597887
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-06-11 11:02:29 +0530

    Move products section components to its own directory

--- projects/packages/my-jetpack/_inc/components/my-jetpack-tab-panel/products/content.tsx
+++ projects/packages/my-jetpack/_inc/components/my-jetpack-tab-panel/products/content.tsx
@@ -1,5 +1,5 @@
 import { __ } from '@wordpress/i18n';
-import UnownedProductsCard from '../product-cards-section/unowned-products-card';
+import UnownedProductsCard from '../../product-cards-section/unowned-products-card';
 import styles from './styles.module.scss';
 
 /**
@@ -9,9 +9,9 @@ import styles from './styles.module.scss';
  */
 const ProductsContent = () => {
 	return (
-		<section className={ styles[ 'my-jetpack-tab--content' ] }>
+		<section className={ styles.content }>
 			<h2>{ __( 'Products', 'jetpack-my-jetpack' ) }</h2>
-			<p className={ styles[ 'my-jetpack-tab--content-description' ] }>
+			<p className={ styles.description }>
 				{ __(
 					'Manage and explore Jetpack products that boost growth, performance, and security.',
 					'jetpack-my-jetpack'
