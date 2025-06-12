c0cd8998c655dcbc7d979c0239040c36c985ece9
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-06-12 19:59:38 +0530

    Clean up a bit

--- projects/packages/my-jetpack/_inc/components/my-jetpack-tab-panel/products/mappings.ts
+++ projects/packages/my-jetpack/_inc/components/my-jetpack-tab-panel/products/mappings.ts
@@ -1,39 +1,8 @@
-import {
-	BackupCard,
-	AiCard,
-	AntiSpamCard,
-	BoostCard,
-	CrmCard,
-	ScanCard,
-	SearchCard,
-	SocialCard,
-	StatsCard,
-	VideopressCard,
-} from './cards';
 import { JetpackModuleSlug, JetpackProductWithCard, ProductCategory } from './types';
-
-/**
- * Mapping of Jetpack modules to their respective product cards.
- */
-export const PRODUCT_CARDS = {
-	ai: AiCard,
-	'jetpack-ai': AiCard,
-	'anti-spam': AntiSpamCard,
-	backup: BackupCard,
-	boost: BoostCard,
-	crm: CrmCard,
-	scan: ScanCard,
-	search: SearchCard,
-	social: SocialCard,
-	stats: StatsCard,
-	videopress: VideopressCard,
-} satisfies {
-	[ key in JetpackProductWithCard ]?: React.ComponentType;
-};
 
 export const CATEGORY_CARDS_AND_MODULES: {
 	[ key in ProductCategory ]: {
-		cards: Array< keyof typeof PRODUCT_CARDS >;
+		cards: Array< JetpackProductWithCard >;
 		modules: Array< JetpackModuleSlug >;
 	};
 } = {
