138bdf069bd4247dfb75535c4317b3d5b61c9deb
Enej Bajgoric <enej.bajgoric@automattic.com>
2025-06-11 22:40:57 -0700

    Add special test

--- projects/packages/forms/tests/php/contact-form/Contact_Form_Test.php
+++ projects/packages/forms/tests/php/contact-form/Contact_Form_Test.php
@@ -911,6 +911,36 @@ class Contact_Form_Test extends BaseTestCase {
 	}
 
 	/**
+	 * Tests that token in curly brackets is replaced with the value when the name has whitespace.
+	 *
+	 * @author tonykova
+	 */
+	public function test_token_can_replace_entire_subject_with_token_field_has_japanese() {
+		$plugin       = Contact_Form_Plugin::init();
+		$subject      = '{名前}';
+		$field_values = array(
+			'名前' => 'Hello',
+		);
+
+		$this->assertEquals( 'Hello', $plugin->replace_tokens_with_input( $subject, $field_values ) );
+	}
+
+	/**
+	 * Tests that token in curly brackets is replaced with the value when the name has whitespace.
+	 *
+	 * @author tonykova
+	 */
+	public function test_token_can_replace_entire_subject_with_token_field_has_emoji() {
+		$plugin       = Contact_Form_Plugin::init();
+		$subject      = '{🙈}';
+		$field_values = array(
+			'🙈' => 'Chicago',
+		);
+
+		$this->assertEquals( 'Chicago', $plugin->replace_tokens_with_input( $subject, $field_values ) );
+	}
+
+	/**
 	 * Tests that token with curly brackets is replaced with value.
 	 *
 	 * @author tonykova
