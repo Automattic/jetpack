Automattic/jetpack

commit 4c73dcfb2764d3ddb98d14d298f156b7708e7775

tools: More handling of no-mirror `packages/test-environment` (#41515)
In `release-plugin.sh`, don't wait for the package to be published to
Packagist since it won't be.

In `version-packages.sh`, remove the package from `require-dev`.

tools/version-packages.sh
@@ -83,12 +83,26 @@ for PKG in "$BASE"/projects/packages/*/composer.json; do
 	PACKAGES=$(jq -c --arg k "$(jq -r .name "$PKG")" --arg v "$(cd "${PKG%/composer.json}" && changelogger version current --default-first-version)" '.[$k] |= $v' <<<"$PACKAGES")
 done
 
+# Packages with no mirror repo to remove from require-dev.
+RMPACKAGES='{}'
+for PKG in "$BASE"/projects/packages/*/composer.json; do
+	if ! jq -e '.extra["mirror-repo"] // null' "$PKG" &>/dev/null; then
+		RMPACKAGES=$(jq -c --arg k "$(jq -r .name "$PKG")" '.[$k] |= true' <<<"$RMPACKAGES")
+	fi
+done
+
 # Update the versions in composer.json, without actually updating them yet.
 TO_UPDATE=()
 mapfile -t TO_UPDATE < <(jq -r --argjson packages "$PACKAGES" '.require // {} | to_entries[] | select( ( .value | test( "^@dev$|\\.x-dev$" ) ) and $packages[.key] ) | "\(.key)=^\($packages[.key])"' "$DIR/composer.json")
 if [[ ${#TO_UPDATE[@]} -gt 0 ]]; then
 	info "Updating packages: ${TO_UPDATE[*]}..."
 	composer require "${COMPOSER_ARGS[@]}" --no-update --working-dir="$DIR" -- "${TO_UPDATE[@]}"
+fi
+TO_UPDATE=()
+mapfile -t TO_UPDATE < <(jq -r --argjson packages "$RMPACKAGES" '.["require-dev"] // {} | to_entries[] | select( ( .value | test( "^@dev$|\\.x-dev$" ) ) and $packages[.key] ) | .key' "$DIR/composer.json")
+if [[ ${#TO_UPDATE[@]} -gt 0 ]]; then
+	info "Remove no-mirror dev packages: ${TO_UPDATE[*]}..."
+	composer remove "${COMPOSER_ARGS[@]}" --no-update --working-dir="$DIR" --dev -- "${TO_UPDATE[@]}"
 fi
 TO_UPDATE=()
 mapfile -t TO_UPDATE < <(jq -r --argjson packages "$PACKAGES" '.["require-dev"] // {} | to_entries[] | select( ( .value | test( "^@dev$|\\.x-dev$" ) ) and $packages[.key] ) | "\(.key)=^\($packages[.key])"' "$DIR/composer.json")

tools/release-plugin.sh
@@ -264,7 +264,7 @@ function do_packagist_check {
 	for PKGDIR in $(git -c core.quotepath=off diff --name-only "$GITBASE..HEAD" projects/packages/ | sed 's!^\(projects/packages/[^/]*\)/.*!\1!' | sort -u); do
 		cd "$BASE/$PKGDIR"
 		CHANGES_DIR=$(jq -r '.extra.changelogger["changes-dir"] // "changelog"' composer.json)
-		if [[ ! -d "$CHANGES_DIR" || -z "$(ls -- "$CHANGES_DIR")" ]]; then
+		if [[ ! -d "$CHANGES_DIR" || -z "$(ls -- "$CHANGES_DIR")" ]] && jq -e '.extra["mirror-repo"] // null' composer.json &>/dev/null; then
 			POLL_ARGS+=( "$( jq -r .name composer.json )=$( changelogger version current )" )
 		fi
 	done

commit 73f33259e3f383043be17ffe6abbf688fd1d54f4

tools, ci: Fix tests on plugin release branches
Plugin tests on release branches for plugins that use
packages/test-environment have been failing since #41515 because in the
release branch we now remove the non-mirrored package.

To fix this, let's record removed packages when creating the release
branch, and restore them to composer.json before trying to run the
tests.

.github/workflows/tests.yml
@@ -106,6 +106,8 @@ jobs:
         run: .github/files/select-wordpress-tag.sh
 
       - name: Composer Install
+        env:
+          CHANGED: ${{ steps.changed.outputs.projects }}
         run: |
           # If we're going to be making WorDBless use WP "nightlies", remove the relevant package from Composer's cache to get the latest version.
           if [[ "$WP_BRANCH" == 'trunk' && ( "$TEST_SCRIPT" == "test-php" || "$TEST_SCRIPT" == "test-coverage" ) ]]; then
@@ -130,6 +132,19 @@ jobs:
             fi
           fi
           echo "::endgroup::"
+
+          echo "Checking for non-mirrored require-dev packages, in case this is testing a release branch"
+          for SLUG in $( jq -r 'keys[]' <<<"$CHANGED" ); do
+            PKGS=()
+            readarray -t PKGS < <( jq -r '.extra["non-mirrored-require-dev"] // empty | .[] | . += "=@dev"' "projects/$SLUG/composer.json" )
+            if [[ ${#PKGS[@]} -gt 0 ]]; then
+              echo "Adding packages for $SLUG: ${PKGS[*]}"
+              # Make sure monorepo repositories entry is present.
+              JSON=$( jq --tab '.repositories //= [] | if any( .repositories[]; .type == "path" and ( .url | startswith( "../" ) ) and .options?.monorepo? ) then . else .repositories += [ { type: "path", url: "../../packages/*", options: { monorepo: true } } ] end' "projects/$SLUG/composer.json" )
+              echo "$JSON" > "projects/$SLUG/composer.json"
+              composer require --working-dir="projects/$SLUG/" --dev --no-install "${PKGS[@]}"
+            fi
+          done
 
       - name: Setup WordPress environment for plugin tests
         env:

tools/version-packages.sh
@@ -103,6 +103,7 @@ mapfile -t TO_UPDATE < <(jq -r --argjson packages "$RMPACKAGES" '.["require-dev"
 if [[ ${#TO_UPDATE[@]} -gt 0 ]]; then
 	info "Remove no-mirror dev packages: ${TO_UPDATE[*]}..."
 	composer remove "${COMPOSER_ARGS[@]}" --no-update --working-dir="$DIR" --dev -- "${TO_UPDATE[@]}"
+	composer config --working-dir="$DIR" 'extra.non-mirrored-require-dev' --json "$( jq -nc '$ARGS.positional' --args "${TO_UPDATE[@]}" )"
 fi
 TO_UPDATE=()
 mapfile -t TO_UPDATE < <(jq -r --argjson packages "$PACKAGES" '.["require-dev"] // {} | to_entries[] | select( ( .value | test( "^@dev$|\\.x-dev$" ) ) and $packages[.key] ) | "\(.key)=^\($packages[.key])"' "$DIR/composer.json")