ea4632d7269151cc611640075fc37578fa9e07a9
Enej Bajgoric <enej.bajgoric@automattic.com>
2025-06-16 10:28:17 -0700

    If the class is added - disable-spinner do not display the spinner!

--- projects/packages/forms/src/contact-form/class-contact-form-plugin.php
+++ projects/packages/forms/src/contact-form/class-contact-form-plugin.php
@@ -672,14 +672,19 @@ class Contact_Form_Plugin {
 		while ( $processor->next_tag() ) {
 			$id = $processor->get_attribute( 'data-id-attr' );
 			if ( 'previous-step' === $id ) {
+				$processor->add_class( 'disable-spinner' );
+				$processor->remove_attribute( 'id' );
 				$processor->set_attribute( 'data-wp-on--click', 'actions.previousStep' );
 				$processor->set_attribute( 'data-wp-class--is-hidden', 'state.isFirstStep' );
 			}
 			if ( 'next-step' === $id ) {
+				$processor->remove_attribute( 'id' );
+				$processor->add_class( 'disable-spinner' );
 				$processor->set_attribute( 'data-wp-on--click', 'actions.nextStep' );
 				$processor->set_attribute( 'data-wp-class--is-hidden', 'state.isLastStep' );
 			}
 			if ( 'submit-step' === $id ) {
+				$processor->remove_attribute( 'id' );
 				$processor->set_attribute( 'data-wp-class--is-hidden', 'state.isNotLastStep' );
 			}
 		}
