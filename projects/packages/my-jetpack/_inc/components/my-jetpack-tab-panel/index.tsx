90253149078d48ae1bc291369478744ba6cedcf8
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-06-17 13:31:28 +0530

    My Jetpack: Onboarding i4 | Always show Products tab

--- projects/packages/my-jetpack/_inc/components/my-jetpack-tab-panel/index.tsx
+++ projects/packages/my-jetpack/_inc/components/my-jetpack-tab-panel/index.tsx
@@ -2,7 +2,6 @@ import { TabPanel } from '@wordpress/components';
 import { useCallback, useEffect, useMemo, useRef } from 'react';
 import { useNavigate, useParams } from 'react-router';
 import useAnalytics from '../../hooks/use-analytics';
-import useFilteredProducts from '../../hooks/use-filtered-products';
 import useIsJetpackUserNew from '../../hooks/use-is-jetpack-user-new';
 import { MY_JETPACK_SECTION_OVERVIEW } from './constants';
 import styles from './styles.module.scss';
@@ -21,23 +20,12 @@ export function MyJetpackTabPanel() {
 	const { recordEvent } = useAnalytics();
 	const isNewUser = useIsJetpackUserNew();
 	const tabStartTimeRef = useRef< number >( Date.now() );
-	const { filteredUnownedProducts, isLoading } = useFilteredProducts();
-
-	const showProductsTab = useMemo( () => {
-		return filteredUnownedProducts.length > 0 && ! isLoading;
-	}, [ filteredUnownedProducts.length, isLoading ] );
-
-	const availableTabs = useMemo(
-		() => getMyJetpackSections( showProductsTab ),
-		[ showProductsTab ]
-	);
 
 	// If the tab is not valid, use the default one.
 	const initialTab = useMemo( () => {
-		const validTab = isValidMyJetpackSection( params.section, showProductsTab );
+		const validTab = isValidMyJetpackSection( params.section );
 		return validTab ? params.section : MY_JETPACK_SECTION_OVERVIEW;
-	}, [ params.section, showProductsTab ] );
-
+	}, [ params.section ] );
 	const onTabSelect = useCallback(
 		( tabName: string ) => {
 			if ( tabName !== params.section ) {
@@ -61,8 +49,8 @@ export function MyJetpackTabPanel() {
 		[ navigate, params.section, recordEvent, isNewUser ]
 	);
 
-	const tabRenderer = useCallback( ( tab: { name: MyJetpackSection } ) => {
-		return <TabContent name={ tab.name } />;
+	const tabRenderer = useCallback( ( tab: { name: string } ) => {
+		return <TabContent name={ tab.name as MyJetpackSection } />;
 	}, [] );
 
 	// Reset timer when component mounts or tab changes from external navigation
@@ -77,7 +65,7 @@ export function MyJetpackTabPanel() {
 			initialTabName={ initialTab }
 			onSelect={ onTabSelect }
 			children={ tabRenderer }
-			tabs={ availableTabs }
+			tabs={ getMyJetpackSections() }
 		/>
 	);
 }
