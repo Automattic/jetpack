a3dcbfe0296137b16cf5b83c4242924c18a24c99
Allie Mims <allie.mims@automattic.com>
2025-06-18 15:03:54 -0400

    Update PayPal_Payments class

--- projects/plugins/paypal-payments/src/class-paypal-payments.php
+++ projects/plugins/paypal-payments/src/class-paypal-payments.php
@@ -5,24 +5,32 @@
  * @package automattic/paypal-payments
  */
 
+use Automattic\Jetpack\PaypalPayments\Paypal_NCPS;
+
 /**
  * Class PayPal_Payments
  *
- * @package Automattic\Jetpack\PaypalPayments
+ * @package Automattic\PaypalPayments
  */
 class PayPal_Payments {
 	/**
 	 * Constructor.
 	 */
 	public function __construct() {
-		// TODO: Add initialization logic here.
+		Paypal_NCPS::register_hooks();
+		Paypal_NCPS::register_block();
+		Paypal_NCPS::load_editor_styles();
+		Paypal_NCPS::load_editor_scripts();
 	}
 
 	/**
 	 * Deactivation hook.
 	 */
 	public static function plugin_deactivation() {
-		// TODO: Add deactivation logic here.
-		// Possibly need to just unregister the block. If that.
+		if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+			error_log( // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+				__( 'PayPal Payments plugin deactivated', 'paypal-payments' )
+			);
+		}
 	}
 }
