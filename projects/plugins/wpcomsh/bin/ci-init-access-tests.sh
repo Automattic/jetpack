6cf0a9bc0e9255c0338c13d86add6983fcbfd2b4
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-06-18 09:39:03 -0600

    Update projects/plugins/wpcomsh/bin/ci-init-access-tests.sh
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>

--- projects/plugins/wpcomsh/bin/ci-init-access-tests.sh
+++ projects/plugins/wpcomsh/bin/ci-init-access-tests.sh
@@ -121,7 +121,7 @@ if [ "$1" = "private" ]; then
   docker exec $WPCLI sh /usr/local/bin/ci-init-cli.sh private
 else
   echo Setting the site to Public
-  docker exec $WPCLI /usr/bin/env sh /usr/local/bin/ci-init-cli.sh
+  docker exec $WPCLI sh /usr/local/bin/ci-init-cli.sh
 fi
 
 echo starting NGINX
