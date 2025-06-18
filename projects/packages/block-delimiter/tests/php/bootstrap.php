5613723f0bc7b9bd18f50d7d70e5680abbe9ca53
Brandon Kraft <public@brandonkraft.com>
2025-06-18 15:45:37 -0500

    Revert other changes punted to other PR

--- projects/packages/block-delimiter/tests/php/bootstrap.php
+++ projects/packages/block-delimiter/tests/php/bootstrap.php
@@ -9,6 +9,3 @@
  * Include the composer autoloader.
  */
 require_once __DIR__ . '/../../vendor/autoload.php';
-
-// Initialize WordPress test environment
-\Automattic\Jetpack\Test_Environment::init();
