29a08cd10dec27ae9e8447437f102b4ebf47948f
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-06-19 14:02:57 -0600

    packages/sync: PhanRedundantCondition

--- projects/packages/sync/src/class-actions.php
+++ projects/packages/sync/src/class-actions.php
@@ -666,7 +666,7 @@ class Actions {
 	 */
 	public static function jetpack_cron_schedule( $schedules ) {
 		if ( ! isset( $schedules[ self::DEFAULT_SYNC_CRON_INTERVAL_NAME ] ) ) {
-			$minutes = (int) ( self::DEFAULT_SYNC_CRON_INTERVAL_VALUE / 60 );
+			$minutes = ( self::DEFAULT_SYNC_CRON_INTERVAL_VALUE / 60 );
 			$display = ( 1 === $minutes ) ?
 				__( 'Every minute', 'jetpack-sync' ) :
 				/* translators: %d is an integer indicating the number of minutes. */
@@ -1184,7 +1184,7 @@ class Actions {
 		}
 
 		if ( $response_code !== 200 || false === isset( $decoded_response['processed_items'] ) ) {
-			if ( is_array( $decoded_response ) && isset( $decoded_response['code'] ) && isset( $decoded_response['message'] ) ) {
+			if ( isset( $decoded_response['code'] ) && isset( $decoded_response['message'] ) ) {
 				return new WP_Error(
 					'jetpack_sync_send_error_' . $decoded_response['code'],
 					$decoded_response['message'],
