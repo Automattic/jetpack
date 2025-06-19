39d159b8d1c4e2dd8d94014a88fa3d9c8cb7373b
Allie Mims <allie.mims@automattic.com>
2025-06-19 15:05:36 -0400

    Refactor PayPal_Payments class

--- projects/plugins/paypal-payments/src/class-paypal-payments.php
+++ projects/plugins/paypal-payments/src/class-paypal-payments.php
@@ -17,8 +17,43 @@ class PayPal_Payments {
 	 * Constructor.
 	 */
 	public function __construct() {
-		PayPal_Payment_Buttons::register_hooks();
+		$this->init();
+		$this->admin_init();
+	}
+
+	/**
+	 * Add initial hooks.
+	 */
+	public function init() {
+		add_action( 'init', array( $this, 'register_block' ) );
+		add_action( 'init', array( $this, 'register_hooks' ) );
+	}
+
+	/**
+	 * Register the block.
+	 */
+	public function register_block() {
 		PayPal_Payment_Buttons::register_block();
+	}
+
+	/**
+	 * Register the hooks.
+	 */
+	public function register_hooks() {
+		PayPal_Payment_Buttons::register_hooks();
+	}
+
+	/**
+	 * Initialize the admin resources.
+	 */
+	public function admin_init() {
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
+	}
+
+	/**
+	 * Enqueue plugin admin scripts and styles.
+	 */
+	public function enqueue_admin_scripts() {
 		PayPal_Payment_Buttons::load_editor_styles();
 		PayPal_Payment_Buttons::load_editor_scripts();
 	}
