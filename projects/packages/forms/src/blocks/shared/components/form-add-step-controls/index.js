fcfbf6875408d640966400b6ee077827c214a81c
Enej Bajgoric <enej.bajgoric@automattic.com>
2025-06-19 20:13:37 -0700

    rename file

--- projects/packages/forms/src/blocks/shared/components/form-add-step-controls/index.js
+++ projects/packages/forms/src/blocks/shared/components/form-add-step-controls/index.js
@@ -5,8 +5,8 @@ import { useDispatch, useSelect } from '@wordpress/data';
 import { useCallback } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { store as singleStepStore } from '../../../../store/form-step-preview';
-import useStepContainerClientId from '../../hooks/use-container-id';
 import useFormSteps from '../../hooks/use-form-steps';
+import useStepContainerClientId from '../../hooks/use-step-container-client-id';
 
 /**
  * Toolbar controls for managing steps within a multi-step form.
