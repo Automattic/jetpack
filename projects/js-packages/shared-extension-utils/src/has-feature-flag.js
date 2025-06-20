c994ae37e1d0e8756979de7d45894268c4d9a5ff
Enej Bajgoric <enej.bajgoric@automattic.com>
2025-06-20 11:04:41 -0700

    Make the error checking more robust hasFeatureFlag

--- projects/js-packages/shared-extension-utils/src/has-feature-flag.js
+++ projects/js-packages/shared-extension-utils/src/has-feature-flag.js
@@ -3,10 +3,19 @@ import getJetpackData from './get-jetpack-data';
 /**
  * Return the value of the Jetpack feature flag.
  *
+ * To add a new feature flag, you need use the `jetpack_block_editor_feature_flags` filter.
+ *
  * @param {string} flag - The feature flag to check.
  *
  * @return {boolean} Whether the current user is connected.
  */
 export default function hasFeatureFlag( flag ) {
-	return Boolean( getJetpackData()?.feature_flags[ flag ] );
+	const jetpackData = getJetpackData();
+	if ( ! jetpackData ) {
+		return false;
+	}
+	if ( ! jetpackData?.feature_flags ) {
+		return false;
+	}
+	return Boolean( jetpackData.feature_flags?.[ flag ] );
 }
