78936ca4f34933dab72244adb138ea7e1dd0f03f
Douglas <douglas.henri@automattic.com>
2025-06-23 19:43:49 -0300

    add getSelectedResponsesCount store selector

--- projects/packages/forms/src/dashboard/store/selectors.js
+++ projects/packages/forms/src/dashboard/store/selectors.js
@@ -2,3 +2,4 @@ export const getFilters = state => state.filters;
 export const getCurrentQuery = state => state.currentQuery;
 export const getSelectedResponsesFromCurrentDataset = state =>
 	state.selectedResponsesFromCurrentDataset;
+export const getSelectedResponsesCount = state => state.selectedResponsesFromCurrentDataset.length;
