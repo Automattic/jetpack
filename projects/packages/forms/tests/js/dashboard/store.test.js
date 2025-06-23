596fc575f78044aff37c39ab35779c577d551bc5
Douglas <douglas.henri@automattic.com>
2025-06-23 19:44:45 -0300

    fix test script

--- projects/packages/forms/tests/js/dashboard/store.test.js
+++ projects/packages/forms/tests/js/dashboard/store.test.js
@@ -2,7 +2,7 @@ import { createRegistry } from '@wordpress/data';
 /**
  * Internal dependencies
  */
-import { store } from '..';
+import { store } from '../../../src/dashboard/store';
 
 const createRegistryWithStores = () => {
 	// Create a registry and register used stores.
@@ -13,25 +13,34 @@ const createRegistryWithStores = () => {
 
 describe( 'actions', () => {
 	let registry;
+
 	beforeEach( () => {
 		registry = createRegistryWithStores();
 	} );
+
 	it( 'receiveFilters', () => {
 		const filters = { date: [ { month: 1, year: 2025 } ], source: [ { id: 29 } ] };
 		registry.dispatch( store ).receiveFilters( filters );
+
 		expect( registry.select( store ).getFilters() ).toMatchObject( filters );
 	} );
+
 	it( 'setSelectedResponses', () => {
 		expect( registry.select( store ).getSelectedResponsesFromCurrentDataset() ).toEqual( [] );
+
 		const args = [ 1, 2, 3 ];
 		registry.dispatch( store ).setSelectedResponses( args );
+
+		expect( registry.select( store ).getSelectedResponsesCount() ).toEqual( args.length );
 		expect( registry.select( store ).getSelectedResponsesFromCurrentDataset() ).toEqual(
 			expect.arrayContaining( args )
 		);
 	} );
+
 	it( 'setCurrentQuery', () => {
 		const args = { page: 1, search: 'r' };
 		registry.dispatch( store ).setCurrentQuery( args );
+
 		expect( registry.select( store ).getCurrentQuery() ).toMatchObject( args );
 	} );
 } );
