41f7ee21c942c84d76c54865ff658a61e1b41d3a
Karen Attfield <karenlattfield@gmail.com>
2025-06-24 08:15:43 +0100

    Update the other functions in the file with the same check, for consistency

--- projects/js-packages/script-data/src/utils.ts
+++ projects/js-packages/script-data/src/utils.ts
@@ -15,7 +15,7 @@ export function getScriptData() {
  * @return {import('./types').SiteData} The site data.
  */
 export function getSiteData() {
-	return getScriptData().site;
+	return getScriptData()?.site;
 }
 
 /**
@@ -26,7 +26,7 @@ export function getSiteData() {
  * @return {string} The admin URL.
  */
 export function getAdminUrl( path = '' ) {
-	return `${ getScriptData().site.admin_url }${ path }`;
+	return `${ getScriptData()?.site.admin_url }${ path }`;
 }
 
 /**
@@ -57,7 +57,7 @@ export function getMyJetpackUrl( section = '' ) {
  * @return {import('./types').SitePlan['features']['active']} The active features.
  */
 export function getActiveFeatures() {
-	return getScriptData().site.plan?.features?.active ?? [];
+	return getScriptData()?.site.plan?.features?.active ?? [];
 }
 
 /**
@@ -77,7 +77,7 @@ export function siteHasFeature( feature: string ) {
  * @return {boolean} Whether the site host is wpcom.
  */
 export function isSimpleSite() {
-	return getScriptData().site?.host === 'wpcom';
+	return getScriptData()?.site?.host === 'wpcom';
 }
 
 /**
@@ -87,7 +87,7 @@ export function isSimpleSite() {
  * @return {boolean} Whether the site is an Atomic site.
  */
 export function isAtomicSite() {
-	return getScriptData().site?.host === 'atomic';
+	return getScriptData()?.site?.host === 'atomic';
 }
 
 /**
@@ -128,5 +128,5 @@ export function isJetpackSelfHostedSite() {
  * @return Whether the current user has that capability.
  */
 export function currentUserCan( capability: keyof CurrentUserData[ 'capabilities' ] ): boolean {
-	return getScriptData().user.current_user.capabilities[ capability ];
+	return getScriptData()?.user.current_user.capabilities[ capability ];
 }
