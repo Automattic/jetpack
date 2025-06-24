05708edede7da8562ca81d51d8e17a5c302c0533
Christian Gastrell <cgastrell@gmail.com>
2025-06-24 17:30:49 -0300

    create explat client with custom fetcher

--- projects/js-packages/ai-client/src/chrome-ai/get-availability.ts
+++ projects/js-packages/ai-client/src/chrome-ai/get-availability.ts
@@ -1,8 +1,9 @@
 /**
  * External dependencies
  */
-import { initializeExPlat, loadExperimentAssignmentWithAuth } from '@automattic/jetpack-explat';
+import { initializeExPlat, createExPlatClient } from '@automattic/jetpack-explat';
 import { select } from '@wordpress/data';
+import { addQueryArgs } from '@wordpress/url';
 import debugFactory from 'debug';
 
 const debug = debugFactory( 'ai-client:chrome-ai-availability' );
@@ -32,6 +33,45 @@ function getAiAssistantFeature() {
 }
 
 /**
+ * Fetch an experiment assignment.
+ *
+ * @param {boolean} asConnectedUser - Whether the user is connected.
+ * @return {Function} A function that fetches an experiment assignment.
+ */
+const fetchExperimentAssignment =
+	( asConnectedUser = false ) =>
+	async ( {
+		experimentName,
+		anonId,
+	}: {
+		experimentName: string;
+		anonId: string | null;
+	} ): Promise< unknown > => {
+		if ( ! anonId ) {
+			debug( 'anonId is null' );
+			throw new Error( `Tracking is disabled, can't fetch experimentAssignment` );
+		}
+
+		const params = {
+			experiment_name: experimentName,
+			anon_id: anonId ?? undefined,
+			as_connected_user: asConnectedUser,
+		};
+
+		debug( 'params', params );
+
+		const assignmentsRequestUrl = addQueryArgs(
+			'https://public-api.wordpress.com/wpcom/v2/experiments/0.1.0/assignments/jetpack',
+			params
+		);
+
+		debug( 'assignmentsRequestUrl', assignmentsRequestUrl );
+
+		// using window.fetch instead of apiFetch because apiFetch only works on relative paths
+		return await window.fetch( assignmentsRequestUrl );
+	};
+
+/**
  * Check if Chrome AI can be enabled.
  *
  * @return {boolean} Whether Chrome AI can be enabled.
@@ -45,8 +85,16 @@ export async function isChromeAIAvailable() {
 		return false;
 	}
 
-	initializeExPlat();
-	debug( 'initialized explat' );
+	const anonId = await initializeExPlat();
+	debug( 'initialized explat', anonId );
+
+	const { loadExperimentAssignment: loadExperimentAssignmentWithAuth } = createExPlatClient( {
+		fetchExperimentAssignment: fetchExperimentAssignment( true ),
+		// @ts-expect-error initializeExPlat returns Promise<string | null | void> but ExPlat expects Promise<string | null>
+		getAnonId: initializeExPlat,
+		logError: debug,
+		isDevelopmentMode: false,
+	} );
 
 	const { variationName } = await loadExperimentAssignmentWithAuth(
 		'calypso_jetpack_ai_gemini_api_202503_v1'
