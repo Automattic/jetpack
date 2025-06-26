01a1e6156786ff363c4c97d32b5354aa2ea4d7fa
Christian Gastrell <cgastrell@gmail.com>
2025-06-26 16:56:32 -0300

    restrict field names matching form HTML element attribute names

--- projects/packages/forms/src/blocks/shared/components/jetpack-field-controls.js
+++ projects/packages/forms/src/blocks/shared/components/jetpack-field-controls.js
@@ -4,11 +4,25 @@ import {
 	BlockControls,
 } from '@wordpress/block-editor';
 import { PanelBody, TextControl, ToggleControl } from '@wordpress/components';
-import { isValidElement } from '@wordpress/element';
-import { __ } from '@wordpress/i18n';
+import { isValidElement, useState } from '@wordpress/element';
+import { __, sprintf } from '@wordpress/i18n';
 import JetpackFieldWidth from './jetpack-field-width';
 import JetpackManageResponsesSettings from './jetpack-manage-responses-settings';
 import ToolbarRequiredGroup from './toolbar-required-group';
+
+// List of reserved HTML form element attribute names
+const reservedAttributes = [
+	'accept',
+	'action',
+	'autocomplete',
+	'enctype',
+	'method',
+	'name',
+	'novalidate',
+	'target',
+	'type',
+	'value',
+];
 
 const JetpackFieldControls = ( {
 	attributes,
@@ -18,9 +32,39 @@ const JetpackFieldControls = ( {
 	width,
 	extraFieldSettings = [],
 } ) => {
+	const helpMessage = __(
+		"Customize the input's name/ID. Only alphanumeric, dash and underscore characters are allowed",
+		'jetpack-forms'
+	);
+
+	const [ errorState, setErrorState ] = useState( {
+		error: false,
+		message: '',
+	} );
+
 	const setId = value => {
 		const newValue = value.replace( /[^a-zA-Z0-9_-]/g, '' );
-		setAttributes( { id: newValue } );
+		const reservedWordError = word => {
+			return sprintf(
+				/* translators: %s is the reserved attribute name causing an error */
+				__( 'The name/ID "%s" is a reserved word. Please use a different name.', 'jetpack-forms' ),
+				word
+			);
+		};
+
+		// Only set ID if it's not a reserved attribute name (case insensitive)
+		if ( ! reservedAttributes.some( attr => attr.toLowerCase() === newValue.toLowerCase() ) ) {
+			setErrorState( {
+				error: false,
+				message: '',
+			} );
+			setAttributes( { id: newValue } );
+		} else {
+			setErrorState( {
+				error: true,
+				message: reservedWordError( newValue ),
+			} );
+		}
 	};
 
 	let fieldSettings = [
@@ -78,13 +122,11 @@ const JetpackFieldControls = ( {
 			</InspectorControls>
 			<InspectorAdvancedControls>
 				<TextControl
+					className={ errorState.error ? 'jetpack-forms-field-controls__input-error' : '' }
 					label={ __( 'Name/ID', 'jetpack-forms' ) }
 					value={ id || '' }
 					onChange={ setId }
-					help={ __(
-						"Customize the input's name/ID. Only alphanumeric, dash and underscore characters are allowed",
-						'jetpack-forms'
-					) }
+					help={ errorState.error ? errorState.message : helpMessage }
 					__nextHasNoMarginBottom={ true }
 					__next40pxDefaultSize={ true }
 				/>
