a2053f23c6d30381a55aa5ce63f4873339b2a12b
Brad Jorsch <brad.jorsch@automattic.com>
2025-06-26 20:29:07 -0400

    Use modern JSX transform
    Previous PRs have updated some places, but we missed a bunch.
    
    * The default for webpack-config's babel preset now sets it.
    * Jetpack's config for blocks now sets it too.
    * The default jest config now uses it too.
    
    This also exposed a bug in js-packages/charts, which is fixed too.

--- tools/js-tools/jest/babel-jest-config-factory.js
+++ tools/js-tools/jest/babel-jest-config-factory.js
@@ -4,24 +4,6 @@ module.exports = resolve => [
 		presets: [
 			[ resolve( '@automattic/jetpack-webpack-config/babel/preset' ), { modules: 'commonjs' } ],
 		],
-		plugins: [
-			[
-				resolve( '@wordpress/babel-plugin-import-jsx-pragma' ),
-				{
-					scopeVariable: 'createElement',
-					scopeVariableFrag: 'Fragment',
-					source: resolve( '@wordpress/element' ),
-					isDefault: false,
-				},
-			],
-			[
-				resolve( '@babel/plugin-transform-react-jsx' ),
-				{
-					pragma: 'createElement',
-					pragmaFrag: 'Fragment',
-				},
-			],
-		],
 		babelrc: false,
 		configFile: false,
 	},
