783dee2961169444562aee0fbc807d5a2704c1f6
Liam Sarsfield <43409125+LiamSarsfield@users.noreply.github.com>
2025-06-30 17:03:04 +0100

    Refactor E2E tests for Boost plugin
    - Updated test common.test.js structure to use `test.describe` and `test.beforeAll` for better organization and readability.
    - Enhanced the deactivation test to ensure proper cleanup of Critical CSS and dismissed recommendations.

--- projects/plugins/boost/tests/e2e/specs/base/common.test.js
+++ projects/plugins/boost/tests/e2e/specs/base/common.test.js
@@ -6,67 +6,75 @@ import playwrightConfig from '_jetpack-e2e-commons/playwright.config.mjs';
 import { boostPrerequisitesBuilder } from '../../lib/env/prerequisites.js';
 import { JetpackBoostPage } from '../../lib/pages/index.js';
 
-test.afterAll( async ( { browser } ) => {
-	const page = await browser.newPage( playwrightConfig.use );
+test.describe( 'Common tests', () => {
+	let page;
 
-	await prerequisitesBuilder( page ).withActivePlugins( [ 'boost' ] ).build();
-	await boostPrerequisitesBuilder( page ).withConnection( true ).build();
-	await page.close();
-} );
+	test.beforeAll( async ( { browser } ) => {
+		page = await browser.newPage( playwrightConfig.use );
+		await boostPrerequisitesBuilder( page ).withCleanEnv( true ).withConnection( true ).build();
+	} );
 
-test( 'Click on the plugins page should navigate to Boost settings page', async ( { page } ) => {
-	await DashboardPage.visit( page );
-	await ( await Sidebar.init( page ) ).selectInstalledPlugins();
-	await ( await PluginsPage.init( page ) ).clickOnJetpackBoostSettingsLink();
-	expect( page.url(), "URL should contain 'page=jetpack-boost" ).toContain( 'page=jetpack-boost' );
-} );
+	test.afterAll( async () => {
+		await prerequisitesBuilder( page ).withActivePlugins( [ 'boost' ] ).build();
+		await boostPrerequisitesBuilder( page ).withConnection( true ).build();
 
-test( 'Click on the sidebar Boost Jetpack submenu should navigate to Boost settings page', async ( {
-	page,
-} ) => {
-	await DashboardPage.visit( page );
-	await ( await Sidebar.init( page ) ).selectJetpackBoost();
-	expect( page.url(), "URL should contain 'page=jetpack-boost" ).toContain( 'page=jetpack-boost' );
-} );
+		await page.close();
+	} );
 
-test( 'Deactivating the plugin should clear Critical CSS and Dismissed Recommendation notice option', async ( {
-	page,
-} ) => {
-	// Generate Critical CSS to ensure that on plugin deactivation it is cleared.
-	// TODO: Also should make sure that a Critical CSS recommendation is dismissed to check that the options does not exist after deactivation of the plugin.
-	await boostPrerequisitesBuilder( page )
-		.withCleanEnv( true )
-		.withActiveModules( [ 'critical_css' ] )
-		.build();
-	const jetpackBoostPage = await JetpackBoostPage.visit( page );
+	test( 'Click on the plugins page should navigate to Boost settings page', async () => {
+		await DashboardPage.visit( page );
+		await ( await Sidebar.init( page ) ).selectInstalledPlugins();
+		await ( await PluginsPage.init( page ) ).clickOnJetpackBoostSettingsLink();
+		expect( page.url(), "URL should contain 'page=jetpack-boost" ).toContain(
+			'page=jetpack-boost'
+		);
+	} );
 
-	// Wait for generation progress UI first
-	expect(
-		await jetpackBoostPage.waitForCriticalCssGenerationProgressUIVisibility(),
-		'Critical CSS generation progress indicator should be visible'
-	).toBeTruthy();
+	test( 'Click on the sidebar Boost Jetpack submenu should navigate to Boost settings page', async () => {
+		await DashboardPage.visit( page );
+		await ( await Sidebar.init( page ) ).selectJetpackBoost();
+		expect( page.url(), "URL should contain 'page=jetpack-boost" ).toContain(
+			'page=jetpack-boost'
+		);
+	} );
 
-	// Then wait for meta info to be visible
-	expect(
-		await jetpackBoostPage.waitForCriticalCssMetaInfoVisibility(),
-		'Critical CSS meta info should be visible'
-	).toBeTruthy();
+	test( 'Deactivating the plugin should clear Critical CSS and Dismissed Recommendation notice option', async () => {
+		// Generate Critical CSS to ensure that on plugin deactivation it is cleared.
+		// TODO: Also should make sure that a Critical CSS recommendation is dismissed to check that the options does not exist after deactivation of the plugin.
+		await boostPrerequisitesBuilder( page )
+			.withCleanEnv( true )
+			.withActiveModules( [ 'critical_css' ] )
+			.build();
+		const jetpackBoostPage = await JetpackBoostPage.visit( page );
 
-	await DashboardPage.visit( page );
-	await ( await Sidebar.init( page ) ).selectInstalledPlugins();
+		// Wait for generation progress UI first
+		expect(
+			await jetpackBoostPage.waitForCriticalCssGenerationProgressUIVisibility(),
+			'Critical CSS generation progress indicator should be visible'
+		).toBeTruthy();
 
-	// Deactivate boost
-	const pluginsPage = await PluginsPage.init( page );
-	await pluginsPage.deactivatePlugin( 'jetpack-boost' );
-	await pluginsPage.click( 'text=Just Deactivate' );
+		// Then wait for meta info to be visible
+		expect(
+			await jetpackBoostPage.waitForCriticalCssMetaInfoVisibility(),
+			'Critical CSS meta info should be visible'
+		).toBeTruthy();
 
-	let result;
-	result = await execWpCommand(
-		'db query \'SELECT ID FROM wp_posts WHERE post_type LIKE "%jb_store_%"\' --skip-column-names'
-	);
-	expect( result.length, 'No DB records are found' ).toBe( 0 );
-	result = await execWpCommand(
-		'db query \'SELECT option_id FROM wp_options WHERE option_name = "jb-critical-css-dismissed-recommendations"\' --skip-column-names'
-	);
-	expect( result.length, 'No DB records are found' ).toBe( 0 );
+		await DashboardPage.visit( page );
+		await ( await Sidebar.init( page ) ).selectInstalledPlugins();
+
+		// Deactivate boost
+		const pluginsPage = await PluginsPage.init( page );
+		await pluginsPage.deactivatePlugin( 'jetpack-boost' );
+		await pluginsPage.click( 'text=Just Deactivate' );
+
+		let result;
+		result = await execWpCommand(
+			'db query \'SELECT ID FROM wp_posts WHERE post_type LIKE "%jb_store_%"\' --skip-column-names'
+		);
+		expect( result.length, 'No DB records are found' ).toBe( 0 );
+		result = await execWpCommand(
+			'db query \'SELECT option_id FROM wp_options WHERE option_name = "jb-critical-css-dismissed-recommendations"\' --skip-column-names'
+		);
+		expect( result.length, 'No DB records are found' ).toBe( 0 );
+	} );
 } );
