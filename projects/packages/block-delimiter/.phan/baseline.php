6902f3cef94f81247742d6dcedd093835109f96b
Jeremy Herve <jeremy@jeremy.hu>
2025-07-01 12:31:18 +0200

    Update Phan config

--- projects/packages/block-delimiter/.phan/baseline.php
+++ projects/packages/block-delimiter/.phan/baseline.php
@@ -9,13 +9,18 @@
  */
 return [
     // # Issue statistics:
-    // PhanPossiblyUndeclaredVariable : 10+ occurrences
-    // PhanPossiblyNullTypeMismatchProperty : 7 occurrences
-    // PhanTypeMismatchArgumentInternalProbablyReal : 1 occurrence
+    // PhanPossiblyUndeclaredVariable : 20+ occurrences
+    // PhanPossiblyNullTypeMismatchProperty : 10+ occurrences
+    // PhanRedefinedClassReference : 9 occurrences
+    // PhanTypeMismatchArgumentInternalProbablyReal : 2 occurrences
+    // PhanRedefineClass : 1 occurrence
 
     // Currently, file_suppressions and directory_suppressions are the only supported suppressions
     'file_suppressions' => [
         'src/class-block-delimiter.php' => ['PhanPossiblyNullTypeMismatchProperty', 'PhanPossiblyUndeclaredVariable', 'PhanTypeMismatchArgumentInternalProbablyReal'],
+        'src/class-block-scanner.php' => ['PhanPossiblyNullTypeMismatchProperty', 'PhanPossiblyUndeclaredVariable', 'PhanRedefinedClassReference', 'PhanTypeMismatchArgumentInternalProbablyReal'],
+        'tests/php/Block_Scanner_Test.php' => ['PhanRedefinedClassReference'],
+        'tests/stubs/class-wp-html-span.php' => ['PhanRedefineClass'],
     ],
     // 'directory_suppressions' => ['src/directory_name' => ['PhanIssueName1', 'PhanIssueName2']] can be manually added if needed.
     // (directory_suppressions will currently be ignored by subsequent calls to --save-baseline, but may be preserved in future Phan releases)
