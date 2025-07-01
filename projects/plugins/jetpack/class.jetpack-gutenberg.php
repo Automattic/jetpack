1cea23e0f2ebf20a8c912d487423e5eedae4d0c1
Brandon Kraft <public@brandonkraft.com>
2025-06-30 19:50:01 -0500

    Add doing_it_wrong as a valid place for next-version

--- projects/plugins/jetpack/class.jetpack-gutenberg.php
+++ projects/plugins/jetpack/class.jetpack-gutenberg.php
@@ -277,7 +277,7 @@ class Jetpack_Gutenberg {
 	 */
 	public static function get_preset( $deprecated = null ) {
 		if ( $deprecated ) {
-			_deprecated_argument( __METHOD__, '$$next-version', 'The $preset argument is no longer needed or used.' );
+			_deprecated_argument( __METHOD__, '1.2.3', 'The $preset argument is no longer needed or used.' );
 		}
 
 		if ( self::$preset_cache ) {
@@ -413,11 +413,7 @@ class Jetpack_Gutenberg {
 			self::$extensions = apply_filters( 'jetpack_set_available_extensions', self::get_available_extensions() );
 
 			if ( ! is_array( self::$extensions ) ) {
-				_doing_it_wrong(
-					__METHOD__,
-					esc_html__( 'The jetpack_set_available_extensions filter must return an array.', 'jetpack' ),
-					'$$next-version$$'
-				);
+				_doing_it_wrong( __METHOD__, esc_html__( 'The jetpack_set_available_extensions filter must return an array.', 'jetpack' ), '$$next-version$$' );
 				self::$extensions = array();
 			}
 		}
