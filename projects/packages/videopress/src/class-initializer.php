d73584d093ed4bad371239fd3663c446fecec83a
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2025-07-02 12:19:13 +0200

    VideoPress: Fix PHP fatals in class-initializer.php

--- projects/packages/videopress/src/class-initializer.php
+++ projects/packages/videopress/src/class-initializer.php
@@ -83,6 +83,8 @@ class Initializer {
 			require_once __DIR__ . '/utility-functions.php';
 		}
 
+		require_once __DIR__ . '/class-block-replacement.php';
+
 		// Set up package version hook.
 		add_filter( 'jetpack_package_versions', __NAMESPACE__ . '\Package_Version::send_package_version_to_tracker' );
 
@@ -168,7 +170,7 @@ class Initializer {
 	 *
 	 * @return string|false
 	 */
-	public static function video_enqueue_bridge_when_oembed_present( $cache, $url, $attr, $post_ID ) { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
+	public static function video_enqueue_bridge_when_oembed_present( $cache, $url, $attr, $post_ID = null ) { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
 		if ( Utils::is_videopress_url( $url ) ) {
 			Jwt_Token_Bridge::enqueue_jwt_token_bridge();
 		}
