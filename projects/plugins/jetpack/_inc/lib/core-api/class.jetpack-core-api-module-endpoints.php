a37e3d9e6905b1582765c822e0e617ea8a8ee67e
Tony Arcangelini <tony@arcangelini.com>
2025-07-01 21:44:57 -0500

    Fix core endpoint

--- projects/plugins/jetpack/_inc/lib/core-api/class.jetpack-core-api-module-endpoints.php
+++ projects/plugins/jetpack/_inc/lib/core-api/class.jetpack-core-api-module-endpoints.php
@@ -1050,6 +1050,44 @@ class Jetpack_Core_API_Data extends Jetpack_Core_API_XMLRPC_Consumer_Endpoint {
 					}
 					break;
 
+				case 'wpcom_newsletter_categories':
+					$sanitized_category_ids = (array) $value;
+
+					array_walk_recursive(
+						$sanitized_category_ids,
+						function ( &$value ) {
+							if ( is_int( $value ) && $value > 0 ) {
+								return;
+							}
+
+							$value = (int) $value;
+							if ( $value <= 0 ) {
+								$value = null;
+							}
+						}
+					);
+
+					$sanitized_category_ids = array_unique(
+						array_filter(
+							$sanitized_category_ids,
+							function ( $category_id ) {
+								return $category_id !== null;
+							}
+						)
+					);
+
+					$new_value = array_map(
+						function ( $category_id ) {
+							return array( 'term_id' => $category_id );
+						},
+						$sanitized_category_ids
+					);
+
+					if ( update_option( $key, $new_value ) ) {
+						$updated[ $key ] = $new_value;
+					}
+					break;
+
 				default:
 					// Boolean values are stored as 1 or 0.
 					if ( isset( $options[ $option ]['type'] ) && 'boolean' === $options[ $option ]['type'] ) {
