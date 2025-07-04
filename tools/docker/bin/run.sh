43e28815a7c3f09e9e2304c44758b7fe8f52072f
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-07-04 09:32:46 -0600

    Use plugin slugs from composer.json

--- tools/docker/bin/run.sh
+++ tools/docker/bin/run.sh
@@ -113,15 +113,13 @@ for DIR in /usr/local/src/jetpack-monorepo/projects/plugins/*; do
 	[ -d "$DIR" ] || continue # We are only interested in directories, e.g. different plugins.
 	PLUGIN="$(basename "$DIR")"
 
-	# Handle special case: symlink crm as zero-bs-crm
-	if [[ "$PLUGIN" = 'crm' ]]; then
-		PLUGIN='zero-bs-crm'
-	fi
+	# Read plugin slug from composer.json, with fallback to beta-plugin-slug
+	PLUGIN_SLUG=$(jq -r '.extra["wp-plugin-slug"] // .extra["beta-plugin-slug"]' "$DIR/composer.json")
 
 	# Symlink plugins into the wp-content dir.
-	if [ ! -e /var/www/html/wp-content/plugins/"$PLUGIN" ]; then
-		echo "Linking the $PLUGIN plugin."
-		ln -s "$DIR" /var/www/html/wp-content/plugins/"$PLUGIN"
+	if [ ! -e /var/www/html/wp-content/plugins/"$PLUGIN_SLUG" ]; then
+		echo "Linking the $PLUGIN plugin as $PLUGIN_SLUG."
+		ln -s "$DIR" /var/www/html/wp-content/plugins/"$PLUGIN_SLUG"
 	fi
 done
 
