0f1d472a508199a591538d2650166d1c284c24cf
Erick Danzer <edanzer@gmail.com>
2025-07-03 19:26:13 -0600

    Improve frontend slider styling

--- projects/packages/forms/src/modules/slider-field/view.js
+++ projects/packages/forms/src/modules/slider-field/view.js
@@ -12,7 +12,9 @@ store( 'jetpack/field-slider', {
 			const context = getContext();
 			const min = Number( context.fieldExtra?.min ?? 0 );
 			const max = Number( context.fieldExtra?.max ?? 100 );
-			const value = Number( context.fieldValue ?? min );
+			let value = Number( context.fieldValue ?? min );
+			value = value < min ? min : value;
+			value = value > max ? max : value;
 			const percent = ( ( value - min ) * 100 ) / ( max - min );
 
 			// Magic numbers: 8px base offset, 0.15px per percent
