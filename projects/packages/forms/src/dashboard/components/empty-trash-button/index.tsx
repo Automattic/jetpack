736606c7fa8e1a975004163f589fb8e86588d602
Christian Gastrell <cgastrell@gmail.com>
2025-07-11 00:49:59 -0300

    add modal confirmation before triggering the request to empty trash

--- projects/packages/forms/src/dashboard/components/empty-trash-button/index.tsx
+++ projects/packages/forms/src/dashboard/components/empty-trash-button/index.tsx
@@ -3,10 +3,10 @@
  */
 import jetpackAnalytics from '@automattic/jetpack-analytics';
 import apiFetch from '@wordpress/api-fetch';
-import { Button } from '@wordpress/components';
+import { Button, Modal } from '@wordpress/components';
 import { useEntityRecords, store as coreStore } from '@wordpress/core-data';
 import { dispatch } from '@wordpress/data';
-import { useState, useCallback } from '@wordpress/element';
+import { useState, useCallback, createInterpolateElement } from '@wordpress/element';
 import { __, _n, sprintf } from '@wordpress/i18n';
 import { trash } from '@wordpress/icons';
 import { store as noticesStore } from '@wordpress/notices';
@@ -14,6 +14,8 @@ import { store as noticesStore } from '@wordpress/notices';
  * Internal dependencies
  */
 import { store as dashboardStore } from '../../store';
+
+import './style.scss';
 
 type CoreStore = typeof coreStore & {
 	invalidateResolutionForStore: ( store: typeof dashboardStore ) => void;
@@ -25,7 +27,9 @@ type CoreStore = typeof coreStore & {
  * @return {JSX.Element} The empty trash button.
  */
 const EmptyTrashButton = (): JSX.Element => {
+	const [ isOpen, setOpen ] = useState( false );
 	const [ isLoading, setIsLoading ] = useState( false );
+
 	const { createSuccessNotice, createErrorNotice } = dispatch( noticesStore );
 	const { invalidateResolutionForStore } = dispatch( coreStore ) as unknown as CoreStore;
 
@@ -34,6 +38,9 @@ const EmptyTrashButton = (): JSX.Element => {
 	} );
 
 	const isEmpty = totalItems === 0;
+
+	const openModal = useCallback( () => setOpen( true ), [] );
+	const closeModal = useCallback( () => setOpen( false ), [] );
 
 	const onButtonClickHandler = useCallback( async () => {
 		if ( isLoading || isEmpty ) {
@@ -75,24 +82,65 @@ const EmptyTrashButton = (): JSX.Element => {
 			} )
 			.finally( () => {
 				invalidateResolutionForStore( dashboardStore );
+				closeModal();
 			} );
-	}, [ isLoading, isEmpty, invalidateResolutionForStore, createSuccessNotice, createErrorNotice ] );
+	}, [
+		isLoading,
+		isEmpty,
+		invalidateResolutionForStore,
+		createSuccessNotice,
+		createErrorNotice,
+		closeModal,
+	] );
 
 	return (
-		<Button
-			__next40pxDefaultSize
-			accessibleWhenDisabled
-			className="jp-forms__button--large-green"
-			disabled={ isEmpty || isLoading }
-			icon={ trash }
-			isBusy={ isLoading }
-			label={ isEmpty ? __( 'Trash is already empty.', 'jetpack-forms' ) : '' }
-			onClick={ onButtonClickHandler }
-			showTooltip={ isEmpty }
-			variant="primary"
-		>
-			{ __( 'Empty trash', 'jetpack-forms' ) }
-		</Button>
+		<>
+			<Button
+				__next40pxDefaultSize
+				accessibleWhenDisabled
+				className="jp-forms__button--large-green"
+				disabled={ isEmpty || isLoading }
+				icon={ trash }
+				isBusy={ isLoading }
+				label={ isEmpty ? __( 'Trash is already empty.', 'jetpack-forms' ) : '' }
+				onClick={ openModal }
+				showTooltip={ isEmpty }
+				variant="primary"
+			>
+				{ __( 'Empty trash', 'jetpack-forms' ) }
+			</Button>
+			{ isOpen && (
+				<Modal
+					title={ __( 'Confirm permanent deletion', 'jetpack-forms' ) }
+					onRequestClose={ closeModal }
+				>
+					<p>
+						{ createInterpolateElement(
+							__(
+								'Are you sure you want to empty the all responses in the trash? <strong>This action cannot be undone.</strong>',
+								'jetpack-forms'
+							),
+							{
+								strong: <strong />,
+							}
+						) }
+					</p>
+					<div className="jp-forms__empty-trash-modal-actions">
+						<Button variant="secondary" onClick={ closeModal }>
+							{ __( 'Cancel', 'jetpack-forms' ) }
+						</Button>
+						<Button
+							variant="primary"
+							isBusy={ isLoading }
+							isDestructive
+							onClick={ onButtonClickHandler }
+						>
+							{ __( 'Empty trash', 'jetpack-forms' ) }
+						</Button>
+					</div>
+				</Modal>
+			) }
+		</>
 	);
 };
 
