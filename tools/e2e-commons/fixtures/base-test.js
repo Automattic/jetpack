871a42d26d39fbf4d905d6544c449079338d5779
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-07-14 15:39:50 +0300

    Remove redundant page fixture

--- tools/e2e-commons/fixtures/base-test.js
+++ tools/e2e-commons/fixtures/base-test.js
@@ -1,43 +1,12 @@
-import { test as baseTest } from '@wordpress/e2e-test-utils-playwright';
+/**
+ * External dependencies
+ */
+import { test, expect } from '@wordpress/e2e-test-utils-playwright';
 import { allure } from 'allure-playwright';
-import config from 'config';
+/**
+ * Internal dependencies
+ */
 import { execWpCommand } from '../helpers/utils-helper.js';
-import logger from '../logger.js';
-export { expect } from '@wordpress/e2e-test-utils-playwright';
-
-const test = baseTest.extend( {
-	page: async ( { page }, use ) => {
-		// Observe console logging
-		page.on( 'console', message => {
-			const type = message.type();
-
-			// Ignore debug messages
-			if ( ! [ 'warning', 'error' ].includes( type ) ) {
-				return;
-			}
-
-			const text = message.text();
-
-			// Ignore messages
-			for ( const subString of config.consoleIgnore ) {
-				if ( text.includes( subString ) ) {
-					return;
-				}
-			}
-
-			logger.debug( `CONSOLE: ${ type.toUpperCase() }: ${ text }` );
-		} );
-
-		page.on( 'pageerror', exception => {
-			logger.debug( `Page error: "${ exception }"` );
-		} );
-
-		page.on( 'requestfailed', request => {
-			logger.debug( `Request failed: ${ request.url() }  ${ request.failure().errorText }` );
-		} );
-		await use( page );
-	},
-} );
 
 test.beforeEach( async () => {
 	await execWpCommand( 'transient delete wpcom_request_counter' );
@@ -48,4 +17,4 @@ test.afterEach( async () => {
 	allure.addParameter( 'Requests to WPCOM API', parseInt( wpcomRequestCount ) || 0 );
 } );
 
-export { test };
+export { test, expect };
