53c1325ac721ee16298d6b1818796d1db9c1ac36
Mikael Korpela <mikael@ihminen.org>
2025-07-15 16:33:07 +0300

    Forms: move number field min/max settings to the field from input

--- projects/packages/forms/src/blocks/field-number/edit.js
+++ projects/packages/forms/src/blocks/field-number/edit.js
@@ -1,25 +1,61 @@
+import { InspectorControls } from '@wordpress/block-editor';
+import { PanelBody, __experimentalNumberControl as NumberControl } from '@wordpress/components'; // eslint-disable-line @wordpress/no-unsafe-wp-apis
 import { __ } from '@wordpress/i18n';
 import JetpackField from '../shared/components/jetpack-field';
 import useFormWrapper from '../shared/hooks/use-form-wrapper';
 
 export default function NumberFieldEdit( props ) {
 	useFormWrapper( props );
+	const { attributes, clientId, insertBlocksAfter, isSelected, setAttributes } = props;
 
 	return (
-		<JetpackField
-			clientId={ props.clientId }
-			type="number"
-			label={ __( 'Number', 'jetpack-forms' ) }
-			required={ props.attributes.required }
-			requiredText={ props.attributes.requiredText }
-			setAttributes={ props.setAttributes }
-			isSelected={ props.isSelected }
-			defaultValue={ props.attributes.defaultValue }
-			placeholder={ props.attributes.placeholder }
-			id={ props.attributes.id }
-			width={ props.attributes.width }
-			attributes={ props.attributes }
-			insertBlocksAfter={ props.insertBlocksAfter }
-		/>
+		<>
+			<JetpackField
+				clientId={ clientId }
+				type="number"
+				label={ __( 'Number', 'jetpack-forms' ) }
+				required={ attributes.required }
+				requiredText={ attributes.requiredText }
+				setAttributes={ setAttributes }
+				isSelected={ isSelected }
+				defaultValue={ attributes.defaultValue }
+				placeholder={ attributes.placeholder }
+				id={ attributes.id }
+				width={ attributes.width }
+				attributes={ attributes }
+				insertBlocksAfter={ insertBlocksAfter }
+			/>
+			<InspectorControls>
+				<PanelBody title={ __( 'Settings', 'jetpack-forms' ) }>
+					<NumberControl
+						key="min"
+						label={ __( 'Minimum value', 'jetpack-forms' ) }
+						value={ attributes.min }
+						onChange={ value => setAttributes( { min: value } ) }
+						max={ attributes.max }
+						__nextHasNoMarginBottom={ true }
+						__next40pxDefaultSize={ true }
+						help={ __(
+							'The minimum value to accept in the input. Leaving empty allows any negative and positive values.',
+							'jetpack-forms'
+						) }
+					/>
+					<NumberControl
+						key="max"
+						label={ __( 'Maximum value', 'jetpack-forms' ) }
+						value={ attributes.max }
+						onChange={ value =>
+							setAttributes( {
+								max: value,
+							} )
+						}
+						min={ attributes.min }
+						__nextHasNoMarginBottom={ true }
+						__next40pxDefaultSize={ true }
+						help={ __( 'The maximum value to accept in the input.', 'jetpack-forms' ) }
+					/>
+				</PanelBody>
+			</InspectorControls>
+		</>
 	);
 }
--- projects/packages/forms/src/blocks/input/edit.js
+++ projects/packages/forms/src/blocks/input/edit.js
@@ -1,5 +1,4 @@
-import { InspectorControls, RichText, useBlockProps } from '@wordpress/block-editor';
-import { PanelBody, __experimentalNumberControl as NumberControl } from '@wordpress/components'; // eslint-disable-line @wordpress/no-unsafe-wp-apis
+import { RichText, useBlockProps } from '@wordpress/block-editor';
 import { useCallback } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { clsx } from 'clsx';
@@ -32,7 +31,7 @@ const getInputClass = type => {
 const InputEdit = ( { attributes, clientId, isSelected, name, setAttributes, context } ) => {
 	const { 'jetpack/field-share-attributes': isSynced } = context;
 	useSyncedAttributes( name, isSynced, SYNCED_ATTRIBUTE_KEYS, attributes, setAttributes );
-	const { max, min, placeholder, type } = attributes;
+	const { placeholder, type } = attributes;
 	const variationProps = useVariationStyleProperties( {
 		clientId,
 		inputBlockName: name,
@@ -79,39 +78,6 @@ const InputEdit = ( { attributes, clientId, isSelected, name, setAttributes, con
 				value={ isSelected ? placeholder : '' }
 				placeholder={ placeholder }
 			/>
-			{ type === 'number' && (
-				<InspectorControls>
-					<PanelBody title={ __( 'Settings', 'jetpack-forms' ) }>
-						<NumberControl
-							key="min"
-							label={ __( 'Minimum value', 'jetpack-forms' ) }
-							value={ min }
-							onChange={ value => setAttributes( { min: value } ) }
-							max={ max }
-							__nextHasNoMarginBottom={ true }
-							__next40pxDefaultSize={ true }
-							help={ __(
-								'The minimum value to accept in the input. Leaving empty allows any negative and positive values.',
-								'jetpack-forms'
-							) }
-						/>
-						<NumberControl
-							key="max"
-							label={ __( 'Maximum value', 'jetpack-forms' ) }
-							value={ attributes.max }
-							onChange={ value =>
-								setAttributes( {
-									max: value,
-								} )
-							}
-							min={ min }
-							__nextHasNoMarginBottom={ true }
-							__next40pxDefaultSize={ true }
-							help={ __( 'The maximum value to accept in the input.', 'jetpack-forms' ) }
-						/>
-					</PanelBody>
-				</InspectorControls>
-			) }
 		</>
 	);
 };
