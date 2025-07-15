b81ac9414662dfbef5a9f7c7fe204d4c57d26109
Jon Surrell <sirreal@users.noreply.github.com>
2025-07-15 12:53:42 +0200

    jetpack-mu-wpcom: Add advanced code editors feature (#44232)
    - Add code editors feature to jetpack-mu-wpcom.
    - Add modules build to jetpack-mu-wpcom.
    - Add typecheck command to jetpack-mu-wpcom.
    - Typecheck new code editors feature and existing verbum-comments feature.

--- projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
+++ projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
@@ -262,6 +262,7 @@ class Jetpack_Mu_Wpcom {
 		require_once __DIR__ . '/features/block-patterns/block-patterns.php';
 		require_once __DIR__ . '/features/blog-privacy/blog-privacy.php';
 		require_once __DIR__ . '/features/cloudflare-analytics/cloudflare-analytics.php';
+		require_once __DIR__ . '/features/code-editor/class-code-editor.php';
 		require_once __DIR__ . '/features/css-monkey-patches/index.php';
 		require_once __DIR__ . '/features/error-reporting/error-reporting.php';
 		require_once __DIR__ . '/features/first-posts-stream/first-posts-stream-helpers.php';
@@ -287,6 +288,7 @@ class Jetpack_Mu_Wpcom {
 
 		// Initializers, if needed.
 		\Marketplace_Products_Updater::init();
+		\Automattic\Jetpack\Code_Editor::setup();
 		\Automattic\Jetpack\Classic_Theme_Helper\Main::init();
 		\Automattic\Jetpack\Classic_Theme_Helper\Featured_Content::setup();
 
