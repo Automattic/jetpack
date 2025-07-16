0b10f2a8890f972ae4360173b66024be5ad0149a
Enej Bajgoric <enej.bajgoric@automattic.com>
2025-07-16 14:12:10 -0700

    Add JWT Token class and tests

--- projects/packages/jwt/tests/php/bootstrap.php
+++ projects/packages/jwt/tests/php/bootstrap.php
@@ -9,3 +9,7 @@
  * Include the composer autoloader.
  */
 require_once __DIR__ . '/../../vendor/autoload.php';
+
+function wp_json_encode( $value, $flags = 0, $depth = 512 ) {
+	return json_encode( $value, $flags, $depth );
+}
