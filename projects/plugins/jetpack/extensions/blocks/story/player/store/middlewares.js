28e6a666d39b587e236e0b63b9700ac2880a1edf
Brad Jorsch <anomiex@users.noreply.github.com>
2025-07-17 11:00:14 -0400

    jetpack: Remove lodash from block view.js scripts (#44319)
    The story block and the podcast-player block were loading lodash on the
    frontend. We can avoid this.
    
    * `memoize( func, keyfunc )` => `( ...args ) => { const key = keyfunc( ...args ); return ( cache[ key ] ??= func( ...args )`
    * `pick( obj, array )` => `Object.fromEntries( Object.entries( obj ).filter( ( [ k ] ) => array.includes( k ) ) )`
    * `flowRight( funcs )` => `v => funcs.reduceRight( ( a, func ) => func( a ), v )`
    * `merge` => deepmerge npm package
    * `debounce` => debounce npm package
    * `throttle( func, delay, { leading: true, trailing: false } )` =>
      `let lastRun = 0` and `v => { const now = Date.now(); if ( now - lastRun >= delay ) { func( v ); lastRun = now; } }`
    
    Note `deepmerge` merges arrays differently by default (deepmerge
    concatenates while lodash merges values at the same index), but as far
    as I can tell that makes no difference to the existing use here.

--- projects/plugins/jetpack/extensions/blocks/story/player/store/middlewares.js
+++ projects/plugins/jetpack/extensions/blocks/story/player/store/middlewares.js
@@ -1,4 +1,3 @@
-import { flowRight } from 'lodash';
 import refx from 'refx';
 import effects from './effects';
 
@@ -24,7 +23,8 @@ export default function applyMiddlewares( store ) {
 		dispatch: ( ...args ) => enhancedDispatch( ...args ),
 	};
 	chain = middlewares.map( middleware => middleware( middlewareAPI ) );
-	enhancedDispatch = flowRight( ...chain )( store.dispatch );
+
+	enhancedDispatch = value => chain.reduceRight( ( a, func ) => func( a ), value );
 
 	store.dispatch = enhancedDispatch;
 
