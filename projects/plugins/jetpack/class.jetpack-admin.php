623771a31ee5c20d23faa3b69d47c58b0e753098
Weston Ruter <westonruter@google.com>
2025-07-16 22:19:39 -0700

    Use next-version token
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>

--- projects/plugins/jetpack/class.jetpack-admin.php
+++ projects/plugins/jetpack/class.jetpack-admin.php
@@ -38,12 +38,12 @@ class Jetpack_Admin {
 	/**
 	 * Filter callback to add `no-store` to the `Cache-Control` header.
 	 *
-	 * @deprecated 14.9
+	 * @deprecated $$next-version$$
 	 * @param array $headers Headers array.
 	 * @return array Modified headers array.
 	 */
 	public static function add_no_store_header( $headers ) {
-		_deprecated_function( __METHOD__, '14.9' );
+		_deprecated_function( __METHOD__, '$$next-version$$' );
 		$headers['Cache-Control'] .= ', no-store';
 		return $headers;
 	}
