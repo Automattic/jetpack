a4d5ebcd13252456a1e32cd0d3b188c519a8835b
Mikael Korpela <mikael@ihminen.org>
2025-07-22 00:48:39 +0300

    Create new option on Enter

--- projects/packages/forms/src/blocks/dropdown-option/edit.js
+++ projects/packages/forms/src/blocks/dropdown-option/edit.js
@@ -1,22 +1,29 @@
 import { RichText, useBlockProps, store as blockEditorStore } from '@wordpress/block-editor';
 import { Button, Flex, FlexItem } from '@wordpress/components';
 import { useDispatch } from '@wordpress/data';
+import { useCallback } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { close } from '@wordpress/icons';
-import useInsertAfterOnEnterKeyDown from '../shared/hooks/use-insert-after-on-enter-key-down';
 
 const noop = () => undefined;
 
 export default function DropdownOptionEdit( props ) {
-	// eslint-disable-next-line no-console
-	console.log( props );
-	const { attributes, clientId, setAttributes, name } = props;
+	const { attributes, clientId, setAttributes } = props;
 	const { option } = attributes;
 	const className = 'jetpack-field-dropdown__option';
 	const blockProps = useBlockProps( { className } );
-	const onKeyDown = useInsertAfterOnEnterKeyDown( clientId, name );
 
-	const { removeBlocks } = useDispatch( blockEditorStore ); // insertAfterBlock
+	const { removeBlocks, insertAfterBlock } = useDispatch( blockEditorStore );
+
+	const onKeyDown = useCallback(
+		event => {
+			if ( event.key === 'Enter' && ! event.shiftKey ) {
+				event.preventDefault();
+				insertAfterBlock( clientId );
+			}
+		},
+		[ insertAfterBlock, clientId ]
+	);
 
 	const onRemove = () => {
 		return removeBlocks( clientId );
@@ -30,7 +37,6 @@ export default function DropdownOptionEdit( props ) {
 						allowedFormats={ [] }
 						onChange={ value => setAttributes( { option: value } ) }
 						onKeyDown={ onKeyDown }
-						onRemove={ onRemove }
 						onReplace={ noop }
 						placeholder={ __( 'Add option…', 'jetpack-forms' ) }
 						value={ option || '' }
