4eb9d1edfc9f79f579f7d7165a61fa62ff34432f
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-07-21 18:16:20 +0300

    Log wpcom_request_counter in Allure description

--- tools/e2e-commons/fixtures/base-test.ts
+++ tools/e2e-commons/fixtures/base-test.ts
@@ -24,7 +24,9 @@ test.beforeEach( async () => {
 
 test.afterEach( async () => {
 	const wpcomRequestCount = await execWpCommand( 'transient get wpcom_request_counter' );
-	allure.label( 'Requests to WPCOM API', String( parseInt( wpcomRequestCount ) || 0 ) );
+	allure.description(
+		`'Requests to WPCOM API: ${ String( parseInt( wpcomRequestCount ) || 0 ) }'`
+	);
 } );
 
 export { test, expect };
