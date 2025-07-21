11aa575062e009004e13af2cb71db4da6c142335
Douglas <douglas.henri@automattic.com>
2025-07-21 15:43:18 -0300

    update error message

--- projects/packages/forms/src/contact-form/class-contact-form-plugin.php
+++ projects/packages/forms/src/contact-form/class-contact-form-plugin.php
@@ -298,7 +298,7 @@ class Contact_Form_Plugin {
 				'is_required'        => __( 'This field is required.', 'jetpack-forms' ),
 				'invalid_form_empty' => __( 'The form you are trying to submit is empty.', 'jetpack-forms' ),
 				'invalid_form'       => __( 'Please fill out the form correctly.', 'jetpack-forms' ),
-				'network_error'      => __( 'There was an error submitting the form. Please try again.', 'jetpack-forms' ),
+				'network_error'      => __( 'Connection issue while submitting the form. Check that you are connected to the Internet and try again.', 'jetpack-forms' ),
 			),
 			'admin_ajax_url' => admin_url( 'admin-ajax.php' ),
 		);
