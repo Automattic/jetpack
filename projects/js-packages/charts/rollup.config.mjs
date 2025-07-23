2100c49e5c1b3f16d05d591a3da2bb77ff28f4d4
Jasper Kang <jasper.kang@automattic.com>
2025-07-24 11:58:43 +1200

    extract the input configuration to avoid duplication

--- projects/js-packages/charts/rollup.config.mjs
+++ projects/js-packages/charts/rollup.config.mjs
@@ -8,23 +8,25 @@ import dts from 'rollup-plugin-dts';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 
+const inputConfig = {
+	index: 'src/index.ts',
+	// Include component index files to support "./components/*" exports
+	'components/bar-chart/index': 'src/components/bar-chart/index.tsx',
+	'components/line-chart/index': 'src/components/line-chart/index.tsx',
+	'components/pie-chart/index': 'src/components/pie-chart/index.tsx',
+	'components/pie-semi-circle-chart/index': 'src/components/pie-semi-circle-chart/index.tsx',
+	'components/bar-list-chart/index': 'src/components/bar-list-chart/index.tsx',
+	'components/leaderboard-chart/index': 'src/components/leaderboard-chart/index.tsx',
+	'components/tooltip/index': 'src/components/tooltip/index.ts',
+	'components/legend/index': 'src/components/legend/index.ts',
+	// Include visx module index files
+	'visx/text/index': 'src/visx/text/index.ts',
+	'visx/group/index': 'src/visx/group/index.ts',
+	'visx/legend/index': 'src/visx/legend/index.ts',
+};
+
 const mainConfig = {
-	input: {
-		index: 'src/index.ts',
-		// Include component index files to support "./components/*" exports
-		'components/bar-chart/index': 'src/components/bar-chart/index.tsx',
-		'components/line-chart/index': 'src/components/line-chart/index.tsx',
-		'components/pie-chart/index': 'src/components/pie-chart/index.tsx',
-		'components/pie-semi-circle-chart/index': 'src/components/pie-semi-circle-chart/index.tsx',
-		'components/bar-list-chart/index': 'src/components/bar-list-chart/index.tsx',
-		'components/leaderboard-chart/index': 'src/components/leaderboard-chart/index.tsx',
-		'components/tooltip/index': 'src/components/tooltip/index.ts',
-		'components/legend/index': 'src/components/legend/index.ts',
-		// Include visx module index files
-		'visx/text/index': 'src/visx/text/index.ts',
-		'visx/group/index': 'src/visx/group/index.ts',
-		'visx/legend/index': 'src/visx/legend/index.ts',
-	},
+	input: inputConfig,
 	output: [
 		{
 			dir: './dist/cjs/',
@@ -79,22 +81,7 @@ const mainConfig = {
 
 // Configuration for generating TypeScript declaration files
 const dtsConfig = {
-	input: {
-		index: 'src/index.ts',
-		// Include component index files to support "./components/*" exports
-		'components/bar-chart/index': 'src/components/bar-chart/index.tsx',
-		'components/line-chart/index': 'src/components/line-chart/index.tsx',
-		'components/pie-chart/index': 'src/components/pie-chart/index.tsx',
-		'components/pie-semi-circle-chart/index': 'src/components/pie-semi-circle-chart/index.tsx',
-		'components/bar-list-chart/index': 'src/components/bar-list-chart/index.tsx',
-		'components/leaderboard-chart/index': 'src/components/leaderboard-chart/index.tsx',
-		'components/tooltip/index': 'src/components/tooltip/index.ts',
-		'components/legend/index': 'src/components/legend/index.ts',
-		// Include visx module index files
-		'visx/text/index': 'src/visx/text/index.ts',
-		'visx/group/index': 'src/visx/group/index.ts',
-		'visx/legend/index': 'src/visx/legend/index.ts',
-	},
+	input: inputConfig,
 	output: [ { dir: 'dist/types', format: 'es' } ],
 	plugins: [
 		dts( {
