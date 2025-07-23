2fc4c90f35dae69a3d56790fb2067bc8318d44be
Jasper Kang <jasper.kang@automattic.com>
2025-07-24 11:55:02 +1200

    build all components individually and remove css extraction

--- projects/js-packages/charts/rollup.config.mjs
+++ projects/js-packages/charts/rollup.config.mjs
@@ -9,7 +9,22 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 
 const mainConfig = {
-	input: 'src/index.ts',
+	input: {
+		index: 'src/index.ts',
+		// Include component index files to support "./components/*" exports
+		'components/bar-chart/index': 'src/components/bar-chart/index.tsx',
+		'components/line-chart/index': 'src/components/line-chart/index.tsx',
+		'components/pie-chart/index': 'src/components/pie-chart/index.tsx',
+		'components/pie-semi-circle-chart/index': 'src/components/pie-semi-circle-chart/index.tsx',
+		'components/bar-list-chart/index': 'src/components/bar-list-chart/index.tsx',
+		'components/leaderboard-chart/index': 'src/components/leaderboard-chart/index.tsx',
+		'components/tooltip/index': 'src/components/tooltip/index.ts',
+		'components/legend/index': 'src/components/legend/index.ts',
+		// Include visx module index files
+		'visx/text/index': 'src/visx/text/index.ts',
+		'visx/group/index': 'src/visx/group/index.ts',
+		'visx/legend/index': 'src/visx/legend/index.ts',
+	},
 	output: [
 		{
 			dir: './dist/cjs/',
@@ -37,7 +52,7 @@ const mainConfig = {
 		commonjs(),
 		json(),
 		postcss( {
-			extract: 'style.css', // Generate individual CSS files
+			extract: false, // Don't extract CSS, inject into JS instead
 			autoModules: true, // Automatically handle .module.scss as CSS modules
 			modules: true, // Enable CSS modules
 			use: [ 'sass' ], // Enable SCSS support
@@ -64,8 +79,23 @@ const mainConfig = {
 
 // Configuration for generating TypeScript declaration files
 const dtsConfig = {
-	input: 'src/index.ts',
-	output: [ { file: 'dist/index.d.ts' } ],
+	input: {
+		index: 'src/index.ts',
+		// Include component index files to support "./components/*" exports
+		'components/bar-chart/index': 'src/components/bar-chart/index.tsx',
+		'components/line-chart/index': 'src/components/line-chart/index.tsx',
+		'components/pie-chart/index': 'src/components/pie-chart/index.tsx',
+		'components/pie-semi-circle-chart/index': 'src/components/pie-semi-circle-chart/index.tsx',
+		'components/bar-list-chart/index': 'src/components/bar-list-chart/index.tsx',
+		'components/leaderboard-chart/index': 'src/components/leaderboard-chart/index.tsx',
+		'components/tooltip/index': 'src/components/tooltip/index.ts',
+		'components/legend/index': 'src/components/legend/index.ts',
+		// Include visx module index files
+		'visx/text/index': 'src/visx/text/index.ts',
+		'visx/group/index': 'src/visx/group/index.ts',
+		'visx/legend/index': 'src/visx/legend/index.ts',
+	},
+	output: [ { dir: 'dist/types', format: 'es' } ],
 	plugins: [
 		dts( {
 			respectExternal: true,
