39ce479cc3fc2d7df5f7144e387de20241e89646
Jasper Kang <jasper.kang@automattic.com>
2025-07-25 10:17:28 +1200

    re arrange exports

--- projects/js-packages/charts/rollup.config.mjs
+++ projects/js-packages/charts/rollup.config.mjs
@@ -8,9 +8,28 @@ import dts from 'rollup-plugin-dts';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 
+const inputConfig = {
+	index: 'src/index.ts',
+	// Include component index files to support "./components/*" exports
+	'components/bar-chart/index': 'src/components/bar-chart/index.tsx',
+	'components/line-chart/index': 'src/components/line-chart/index.tsx',
+	'components/pie-chart/index': 'src/components/pie-chart/index.tsx',
+	'components/pie-semi-circle-chart/index': 'src/components/pie-semi-circle-chart/index.tsx',
+	'components/bar-list-chart/index': 'src/components/bar-list-chart/index.tsx',
+	'components/leaderboard-chart/index': 'src/components/leaderboard-chart/index.tsx',
+	'components/tooltip/index': 'src/components/tooltip/index.ts',
+	'components/legend/index': 'src/components/legend/index.ts',
+	// Include visx module index files
+	'visx/text/index': 'src/visx/text/index.ts',
+	'visx/group/index': 'src/visx/group/index.ts',
+	'visx/legend/index': 'src/visx/legend/index.ts',
+	// Include provider index files
+	'providers/chart-context/index': 'src/providers/chart-context/index.ts',
+	'providers/theme/index': 'src/providers/theme/index.ts',
+};
+
 const mainConfig = {
-	input: 'src/index.ts',
-	treeshake: false,
+	input: inputConfig,
 	output: [
 		{
 			dir: './dist/cjs/',
@@ -38,7 +57,7 @@ const mainConfig = {
 		commonjs(),
 		json(),
 		postcss( {
-			extract: false, // Don't extract CSS, inject into JS instead
+			extract: 'style.css', // extract CSS to a separate file
 			autoModules: true, // Automatically handle .module.scss as CSS modules
 			modules: true, // Enable CSS modules
 			use: [ 'sass' ], // Enable SCSS support
@@ -73,7 +92,7 @@ const mainConfig = {
 
 // Configuration for generating TypeScript declaration files
 const dtsConfig = {
-	input: 'src/index.ts',
+	input: inputConfig,
 	output: [
 		{ dir: 'dist/types/', preserveModules: true, preserveModulesRoot: 'src', format: 'es' },
 	],
