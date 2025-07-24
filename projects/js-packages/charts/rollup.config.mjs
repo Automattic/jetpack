b19b87c59af124c5505791e8b6568ccd8cafd617
Jasper Kang <jasper.kang@automattic.com>
2025-07-25 09:55:20 +1200

    emit type to another folder

--- projects/js-packages/charts/rollup.config.mjs
+++ projects/js-packages/charts/rollup.config.mjs
@@ -8,28 +8,9 @@ import dts from 'rollup-plugin-dts';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 
-const inputConfig = {
-	index: 'src/index.ts',
-	// Include component index files to support "./components/*" exports
-	'components/bar-chart/index': 'src/components/bar-chart/index.tsx',
-	'components/line-chart/index': 'src/components/line-chart/index.tsx',
-	'components/pie-chart/index': 'src/components/pie-chart/index.tsx',
-	'components/pie-semi-circle-chart/index': 'src/components/pie-semi-circle-chart/index.tsx',
-	'components/bar-list-chart/index': 'src/components/bar-list-chart/index.tsx',
-	'components/leaderboard-chart/index': 'src/components/leaderboard-chart/index.tsx',
-	'components/tooltip/index': 'src/components/tooltip/index.ts',
-	'components/legend/index': 'src/components/legend/index.ts',
-	// Include visx module index files
-	'visx/text/index': 'src/visx/text/index.ts',
-	'visx/group/index': 'src/visx/group/index.ts',
-	'visx/legend/index': 'src/visx/legend/index.ts',
-	// Include provider index files
-	'providers/chart-context/index': 'src/providers/chart-context/index.ts',
-	'providers/theme/index': 'src/providers/theme/index.ts',
-};
-
 const mainConfig = {
-	input: inputConfig,
+	input: 'src/index.ts',
+	treeshake: false,
 	output: [
 		{
 			dir: './dist/cjs/',
@@ -92,8 +73,10 @@ const mainConfig = {
 
 // Configuration for generating TypeScript declaration files
 const dtsConfig = {
-	input: inputConfig,
-	output: [ { dir: 'dist/types', format: 'es' } ],
+	input: 'src/index.ts',
+	output: [
+		{ dir: 'dist/types/', preserveModules: true, preserveModulesRoot: 'src', format: 'es' },
+	],
 	plugins: [
 		dts( {
 			respectExternal: true,
