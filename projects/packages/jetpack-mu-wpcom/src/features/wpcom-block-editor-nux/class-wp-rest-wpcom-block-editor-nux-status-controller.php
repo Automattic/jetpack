0da07b0d65c273fdb59894d5a73babd471399538
Karen Attfield <karenlattfield@gmail.com>
2025-07-28 16:53:05 +0100

    Remove usage of the new is_p2_site function until the updated Status package is in use in wpcomsh

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-block-editor-nux/class-wp-rest-wpcom-block-editor-nux-status-controller.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-block-editor-nux/class-wp-rest-wpcom-block-editor-nux-status-controller.php
@@ -7,8 +7,6 @@
 
 namespace Automattic\Jetpack\Jetpack_Mu_Wpcom\NUX;
 
-use Automattic\Jetpack\Status\Host;
-
 /**
  * Class WP_REST_WPCOM_Block_Editor_NUX_Status_Controller.
  */
@@ -81,7 +79,14 @@ class WP_REST_WPCOM_Block_Editor_NUX_Status_Controller extends \WP_REST_Controll
 			$variant = 'tour';
 		}
 
-		if ( ( new Host() )->is_p2_site() ) {
+		if ( defined( 'IS_ATOMIC' ) && IS_ATOMIC ) {
+			$is_p2 = false;
+		} else {
+			$blog_id = get_current_blog_id();
+			$is_p2   = \WPForTeams\is_wpforteams_site( $blog_id );
+		}
+
+		if ( $is_p2 ) {
 			// disable welcome tour for authoring P2s.
 			// see: https://github.com/Automattic/wp-calypso/issues/62973.
 			$nux_status = 'disabled';
