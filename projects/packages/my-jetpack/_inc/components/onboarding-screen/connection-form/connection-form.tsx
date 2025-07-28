d7ffae3339e46ceeece019bbcb30a3fae9449b3c
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-07-28 09:42:49 +0530

    Remove the onboarding UI changes

--- projects/packages/my-jetpack/_inc/components/onboarding-screen/connection-form/connection-form.tsx
+++ projects/packages/my-jetpack/_inc/components/onboarding-screen/connection-form/connection-form.tsx
@@ -1,35 +1,24 @@
 import { JetpackLogo, TermsOfService, Text } from '@automattic/jetpack-components';
 import { useConnection } from '@automattic/jetpack-connection';
 import { Button, Spinner, Notice } from '@wordpress/components';
-import { __, _x } from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
 import { useCallback, useEffect } from 'react';
 import useAnalytics from '../../../hooks/use-analytics';
 import preventWidows from '../../../utils/prevent-widows';
 import styles from './styles.module.scss';
 
 const ConnectionForm = () => {
-	const {
-		userIsConnecting,
-		siteIsRegistering,
-		handleRegisterSite,
-		handleConnectUser,
-		registrationError,
-		isRegistered,
-	} = useConnection( { from: 'jetpack-onboarding' } );
+	const { userIsConnecting, siteIsRegistering, handleRegisterSite, registrationError } =
+		useConnection( { from: 'jetpack-onboarding' } );
 
 	const isConnecting = userIsConnecting || siteIsRegistering;
 
 	const { recordEvent } = useAnalytics();
 
-	const onClickConnectSite = useCallback( () => {
+	const onClickConnect = useCallback( () => {
 		recordEvent( 'jetpack_my_jetpack_onboarding_click' );
 		handleRegisterSite();
 	}, [ recordEvent, handleRegisterSite ] );
-
-	const onClickConnectUser = useCallback( () => {
-		recordEvent( 'jetpack_my_jetpack_connect_user_click' );
-		handleConnectUser();
-	}, [ recordEvent, handleConnectUser ] );
 
 	useEffect( () => {
 		if ( registrationError ) {
@@ -44,29 +33,15 @@ const ConnectionForm = () => {
 			<JetpackLogo height={ 24 } className={ styles[ 'jetpack-logo' ] } />
 
 			<Text variant="headline-medium" className={ styles.title }>
-				{ preventWidows(
-					isRegistered
-						? _x(
-								'Connect your user account to unlock powerful features',
-								'',
-								'jetpack-my-jetpack'
-						  )
-						: __( 'Start with Jetpack for free', 'jetpack-my-jetpack' )
-				) }
+				{ preventWidows( __( 'Start with Jetpack for free', 'jetpack-my-jetpack' ) ) }
 			</Text>
 
 			<Text variant="body" className={ styles.description }>
 				{ preventWidows(
-					isRegistered
-						? _x(
-								'Log in to supercharge your site with powerful security, speed, and growth tools.',
-								'',
-								'jetpack-my-jetpack'
-						  )
-						: __(
-								'Supercharge your WordPress site with powerful security, speed, and growth tools.',
-								'jetpack-my-jetpack'
-						  )
+					__(
+						'Supercharge your WordPress site with powerful security, speed, and growth tools.',
+						'jetpack-my-jetpack'
+					)
 				) }
 			</Text>
 
@@ -74,24 +49,15 @@ const ConnectionForm = () => {
 				className={ styles[ 'submit-button' ] }
 				disabled={ isConnecting }
 				aria-busy={ isConnecting }
-				onClick={ isRegistered ? onClickConnectUser : onClickConnectSite }
+				onClick={ onClickConnect }
 				// Ensure that we have the label when the button is disabled
 				aria-label={ __( 'Supercharge my site', 'jetpack-my-jetpack' ) }
 			>
-				{
-					// Use IIFE to avoid nested ternary for readability
-					( ( connecting, registered ) => {
-						if ( connecting ) {
-							return <Spinner className={ styles.spinner } />;
-						}
-
-						if ( registered ) {
-							return _x( 'Connect your account', '', 'jetpack-my-jetpack' );
-						}
-
-						return __( 'Supercharge my site', 'jetpack-my-jetpack' );
-					} )( isConnecting, isRegistered )
-				}
+				{ isConnecting ? (
+					<Spinner className={ styles.spinner } />
+				) : (
+					__( 'Supercharge my site', 'jetpack-my-jetpack' )
+				) }
 			</Button>
 
 			{ registrationError ? (
@@ -103,11 +69,7 @@ const ConnectionForm = () => {
 
 			<TermsOfService
 				className={ styles.tos }
-				agreeButtonLabel={
-					isRegistered
-						? _x( 'Connect your account', '', 'jetpack-my-jetpack' )
-						: __( 'Supercharge my site', 'jetpack-my-jetpack' )
-				}
+				agreeButtonLabel={ __( 'Supercharge my site', 'jetpack-my-jetpack' ) }
 			/>
 		</div>
 	);
