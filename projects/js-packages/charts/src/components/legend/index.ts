7c94f9bc3ca776024b7d8a8fbc4e5d32de11e5a8
annacmc <anna.mcphee@automattic.com>
2025-07-29 22:05:18 +1000

    Export ChartLegend from legend module

--- projects/js-packages/charts/src/components/legend/index.ts
+++ projects/js-packages/charts/src/components/legend/index.ts
@@ -1,5 +1,7 @@
 export { Legend } from './legend';
 export { BaseLegend } from './base-legend';
+export { ChartLegend } from './chart-legend';
 export { useChartLegendData } from './use-chart-legend-data';
 export type { LegendProps, BaseLegendProps } from './types';
+export type { ChartLegendProps } from './chart-legend';
 export type { ChartLegendOptions } from './use-chart-legend-data';
