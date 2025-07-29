7eb1c5330cc5ce8b75d15728d2454cd0dbb35f9a
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-07-29 11:32:38 +0530

    Restore components still used

--- projects/packages/my-jetpack/_inc/components/connection-screen/index.tsx
+++ projects/packages/my-jetpack/_inc/components/connection-screen/index.tsx
@@ -2,21 +2,24 @@ import { AdminPage } from '@automattic/jetpack-components';
 import { getJetpackAdminPageUrl, getMyJetpackUrl } from '@automattic/jetpack-script-data';
 import { __ } from '@wordpress/i18n';
 import { addQueryArgs } from '@wordpress/url';
-import type { FC } from 'react';
+import { useEffect, type FC } from 'react';
+import styles from './styles.module.scss';
 
 const ConnectionScreen: FC = () => {
-	const redirectUrl = addQueryArgs( getJetpackAdminPageUrl(), {
-		connect_url_redirect: 1,
-		redirect_after_auth: getMyJetpackUrl(),
-	} );
+	useEffect( () => {
+		const redirectUrl = addQueryArgs( getJetpackAdminPageUrl(), {
+			connect_url_redirect: 1,
+			redirect_after_auth: getMyJetpackUrl(),
+		} );
 
-	useEffect(() => {
 		window.location.replace( redirectUrl );
-	}, []);
+	}, [] );
 
 	return (
 		<AdminPage showBackground={ false } showFooter={ false }>
-			<div style={ { marginInline: '1.5rem' } }>{ __( 'Redirecting…', 'jetpack-my-jetpack' ) }</div>
+			<div className={ styles[ 'redirect-block' ] }>
+				{ __( 'Redirecting…', 'jetpack-my-jetpack' ) }
+			</div>
 		</AdminPage>
 	);
 };
