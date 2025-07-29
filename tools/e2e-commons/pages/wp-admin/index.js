dd13e9effcf5d88f9c49434543862e6fc38aba89
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-07-29 18:47:55 +0300

    Remove unused wp-admin pages

--- tools/e2e-commons/pages/wp-admin/index.js
+++ tools/e2e-commons/pages/wp-admin/index.js
@@ -1,12 +1,9 @@
 export { default as BlockEditorPage } from './block-editor.js';
-
 export { default as DashboardPage } from './dashboard.js';
-export { default as InPlacePlansPage } from './in-place-plans.js';
-export { default as JetpackPage } from './jetpack.js';
 export { default as JetpackDashboardPage } from './jetpack-dashboard.js';
-export { default as JetpackMyPlanPage } from './jetpack-my-plan.js';
 export { default as WPLoginPage } from './login.js';
 export { default as PluginsPage } from './plugins.js';
+export { default as ProfilePage } from './profile.js';
 export { default as ThemesPage } from './themes.js';
 export { default as RecommendationsPage } from './recommendations.js';
 export { default as Sidebar } from './sidebar.js';
