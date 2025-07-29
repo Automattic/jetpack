c023185e59e8d9024404e50a954f53cf44fdacee
annacmc <anna.mcphee@automattic.com>
2025-07-29 22:05:25 +1000

    Export ChartIdProvider from chart-context

--- projects/js-packages/charts/src/providers/chart-context/index.ts
+++ projects/js-packages/charts/src/providers/chart-context/index.ts
@@ -1,3 +1,4 @@
 export { ChartProvider, ChartContext, useChartContext } from './chart-context';
+export { ChartIdProvider, ChartIdContext, useChartIdFromContext } from './chart-id-context';
 export { useChartId, useChartRegistration } from './utils';
 export type { ChartContextValue, ChartRegistration } from './types';
