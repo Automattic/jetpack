19b618dc6bbc7543d6511094600f94ae53d99263
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-07-29 17:53:00 +0300

    Remove jetpack-connect unused helper

--- tools/e2e-commons/flows/index.js
+++ tools/e2e-commons/flows/index.js
@@ -1,4 +1,3 @@
-import { doClassicConnection, doSiteLevelConnection } from './jetpack-connect.js';
 import { loginToWpSite, loginToWpCom } from './log-in.js';
 
-export { doClassicConnection, doSiteLevelConnection, loginToWpSite, loginToWpCom };
+export { loginToWpSite, loginToWpCom };
