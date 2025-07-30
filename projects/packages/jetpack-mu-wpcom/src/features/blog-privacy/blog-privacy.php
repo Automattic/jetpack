75afb7d62b04f4c0cc2b52f43baf05613ef7641a
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-07-30 09:01:06 -0600

    Account for any type
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>

--- projects/packages/jetpack-mu-wpcom/src/features/blog-privacy/blog-privacy.php
+++ projects/packages/jetpack-mu-wpcom/src/features/blog-privacy/blog-privacy.php
@@ -22,7 +22,7 @@ namespace Automattic\Jetpack\Jetpack_Mu_Wpcom\Blog_Privacy;
  * @param int|string  $public The value of the blog_public option.
  * @return string
  */
-function robots_txt( ?string $output, $public ): string {
+function robots_txt( $output, $public ): string {
 	if ( ! is_string( $output ) ) {
 		$output = '';
 	}
