4ac0b29cdaea8e3d77c69183611851fe775e4efb
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-07-30 12:05:50 +0530

    Create getUserConnectionUrl in the connections package

---
+++ projects/js-packages/connection/helpers/get-user-connection-url.ts
@@ -0,0 +1,17 @@
+import { getJetpackAdminPageUrl, getMyJetpackUrl } from '@automattic/jetpack-script-data';
+import { addQueryArgs } from '@wordpress/url';
+
+/**
+ * Generates the user connection URL.
+ *
+ * @param redirectUrl - Optional redirect URL after connection. Defaults to the My Jetpack URL.
+ *
+ * @return The URL for user connection.
+ */
+export function getUserConnectionUrl( redirectUrl?: string | null ): string {
+	return addQueryArgs( getJetpackAdminPageUrl(), {
+		// This is handled in \Automattic\Jetpack\Connection\Webhooks::controller()
+		connect_url_redirect: 1,
+		redirect_after_auth: redirectUrl ?? getMyJetpackUrl(),
+	} );
+}
