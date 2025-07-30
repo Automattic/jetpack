bfc10d7dbcf779d6526f3a90a5e3ea7c858eb329
Allison Levine <allison.levine@automattic.com>
2025-07-30 14:10:16 -0400

    Featured image in email: Request site creation date.

--- projects/plugins/wpcomsh/feature-plugins/featured-image-in-email.php
+++ projects/plugins/wpcomsh/feature-plugins/featured-image-in-email.php
@@ -6,25 +6,75 @@
  */
 
 /**
+ * Get the site creation timestamp via API request.
+ *
+ * @return int The site creation date as a Unix timestamp or 0 for default fallback.
+ */
+function wpcomsh_get_site_creation_timestamp() {
+	$default_timestamp = 0;
+
+	// Check if Jetpack is connected
+	if ( ! class_exists( 'Jetpack' ) || ! Jetpack::is_active() ) {
+		return $default_timestamp;
+	}
+
+	$transient_key             = 'wpcomsh_featured_image_site_creation';
+	$cached_creation_timestamp = get_transient( $transient_key );
+
+	if ( false !== $cached_creation_timestamp ) {
+		return $cached_creation_timestamp;
+	}
+
+	// Get the WordPress.com site ID from Jetpack options
+	$jetpack_options = get_option( 'jetpack_options' );
+	if ( ! is_array( $jetpack_options ) || ! isset( $jetpack_options['id'] ) ) {
+		return $default_timestamp;
+	}
+
+	$site_id = (int) $jetpack_options['id'];
+
+	if ( ! $site_id ) {
+		return $default_timestamp;
+	}
+
+	// Make authenticated API request to get site creation date
+	$response = Automattic\Jetpack\Connection\Client::wpcom_json_api_request_as_blog(
+		sprintf( '/sites/%d?force=wpcom&options=created_at', $site_id ),
+		'1.1'
+	);
+
+	if ( is_wp_error( $response ) ) {
+		return $default_timestamp;
+	}
+
+	$body      = wp_remote_retrieve_body( $response );
+	$site_data = json_decode( $body );
+
+	if ( ! $site_data || ! isset( $site_data->options->created_at ) ) {
+		return $default_timestamp;
+	}
+
+	$site_creation_timestamp = strtotime( $site_data->options->created_at );
+
+	// Cache the result for 24 hours
+	set_transient( $transient_key, $site_creation_timestamp, DAY_IN_SECONDS );
+
+	return $site_creation_timestamp;
+}
+
+/**
  * Set the default value for wpcom_featured_image_in_email.
  * For Atomic sites created after May 2, 2025, default to true.
  *
  * @return bool The conditional default value.
  */
 function wpcomsh_featured_image_in_email_default() {
-	// For Atomic sites, use WordPress.com site ID as a proxy for site creation date
-	$wpcom_site_id = null;
+	$site_creation_timestamp = wpcomsh_get_site_creation_timestamp();
 
-	// Get the WordPress.com site ID from Jetpack options
-	$jetpack_options = get_option( 'jetpack_options' );
-	if ( is_array( $jetpack_options ) && isset( $jetpack_options['id'] ) ) {
-		$wpcom_site_id = (int) $jetpack_options['id'];
-	}
-
-	if ( $wpcom_site_id ) {
-		// Sites created after May 2, 2025 should have higher site IDs
-		// Using threshold of 244,134,246 to target sites created after May 2, 2025
-		if ( $wpcom_site_id > 244134246 ) {
+	if ( $site_creation_timestamp ) {
+		// Check if site was created after May 2, 2025
+		$cutoff_timestamp = strtotime( '2025-05-02' );
+		if ( $site_creation_timestamp > $cutoff_timestamp ) {
 			return true;
 		}
 	}
