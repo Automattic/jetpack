14bc9a3d374b68189ba07a4a62205a759079c710
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-07-31 09:51:48 +0530

    Use the new getUserConnectionUrl

--- projects/packages/my-jetpack/_inc/components/connection-screen/index.tsx
+++ projects/packages/my-jetpack/_inc/components/connection-screen/index.tsx
@@ -1,19 +1,13 @@
 import { AdminPage } from '@automattic/jetpack-components';
-import { getJetpackAdminPageUrl, getMyJetpackUrl } from '@automattic/jetpack-script-data';
+import { getUserConnectionUrl } from '@automattic/jetpack-connection';
 import { Flex, Spinner } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { addQueryArgs } from '@wordpress/url';
 import { useEffect, type FC } from 'react';
 import styles from './styles.module.scss';
 
 const ConnectionScreen: FC = () => {
 	useEffect( () => {
-		const redirectUrl = addQueryArgs( getJetpackAdminPageUrl(), {
-			connect_url_redirect: 1,
-			redirect_after_auth: getMyJetpackUrl(),
-		} );
-
-		window.location.replace( redirectUrl );
+		window.location.replace( getUserConnectionUrl() );
 	}, [] );
 
 	return (
