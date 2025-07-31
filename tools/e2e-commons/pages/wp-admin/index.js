f0ac3bd851f70564ba0d9072fa3dbd2b08a60124
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-07-31 18:34:20 +0300

    Removed unsued RecommendationsPage

--- tools/e2e-commons/pages/wp-admin/index.js
+++ tools/e2e-commons/pages/wp-admin/index.js
@@ -2,5 +2,4 @@ export { default as DashboardPage } from './dashboard.js';
 export { default as JetpackDashboardPage } from './jetpack-dashboard.js';
 export { default as PluginsPage } from './plugins.js';
 export { default as ThemesPage } from './themes.js';
-export { default as RecommendationsPage } from './recommendations.js';
 export { default as Sidebar } from './sidebar.js';
