3c4d4c6e3dabe0efd50e64fa585e480c69435afa
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-07-31 12:05:26 +0300

    Update social-sidebar test to use the new EditorPage

--- projects/plugins/social/tests/e2e/specs/social-sidebar.test.js
+++ projects/plugins/social/tests/e2e/specs/social-sidebar.test.js
@@ -1,6 +1,5 @@
 import { expect, test } from '_jetpack-e2e-commons/fixtures/base-test.ts';
 import logger from '_jetpack-e2e-commons/logger.js';
-import BlockEditorPage from '_jetpack-e2e-commons/pages/wp-admin/block-editor.js';
 import { connect } from '../flows/index.js';
 
 test.beforeAll( async ( { testUtils } ) => {
@@ -10,12 +9,10 @@ test.beforeAll( async ( { testUtils } ) => {
 	await testUtils.requestUtils.activatePlugin( 'jetpack-social' );
 } );
 
-test( 'Jetpack Social sidebar', async ( { page, admin } ) => {
+test( 'Jetpack Social sidebar', async ( { page, admin, editor } ) => {
 	await test.step( 'Connect wordpress.com account', async () => {
 		await connect( page );
 	} );
-
-	const blockEditor = new BlockEditorPage( page );
 
 	await test.step( 'Goto post edit page', async () => {
 		logger.action( 'Create new post' );
@@ -24,10 +21,10 @@ test( 'Jetpack Social sidebar', async ( { page, admin } ) => {
 
 	await test.step( 'Check Social sidebar', async () => {
 		logger.action( 'Open Jetpack Social sidebar' );
-		await blockEditor.openSettings( 'Jetpack Social' );
+		await editor.openSettings( 'Jetpack Social' );
 
 		logger.action( 'Checking for "Preview" button' );
-		const previewButton = blockEditor
+		const previewButton = editor
 			.getEditorSettingsSidebar()
 			.getByRole( 'button', { name: 'Open Social Previews', exact: true } );
 		await expect( previewButton ).toBeVisible();
