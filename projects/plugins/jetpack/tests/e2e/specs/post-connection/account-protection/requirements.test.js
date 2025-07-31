a82ae1e4fd90e737d6d8ee47d3d916e7ee26503c
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-07-31 13:06:39 +0300

    Update requirements test. Removed Profile page.

--- projects/plugins/jetpack/tests/e2e/specs/post-connection/account-protection/requirements.test.js
+++ projects/plugins/jetpack/tests/e2e/specs/post-connection/account-protection/requirements.test.js
@@ -1,5 +1,4 @@
 import { test, expect } from '_jetpack-e2e-commons/fixtures/base-test.ts';
-import ProfilePage from '_jetpack-e2e-commons/pages/wp-admin/profile.js';
 
 test.beforeAll( async ( { testUtils } ) => {
 	await testUtils.activateModule( 'account-protection' );
@@ -12,70 +11,64 @@ test.beforeAll( async ( { testUtils } ) => {
 
 test.describe.parallel( 'Strong password requirements', () => {
 	test( 'Enforces strong password requirements', async ( { page } ) => {
-		const profilePage = await ProfilePage.visit( page );
+		await page.goto( '/wp-admin/profile.php' );
 
-		await profilePage.page.getByRole( 'button' ).filter( { hasText: 'set new password' } ).click();
+		await page.getByRole( 'button' ).filter( { hasText: 'set new password' } ).click();
 
 		// Validate that the Jetpack password strength meter replaces the default one.
-		await expect( profilePage.page.locator( '.strength-meter' ) ).toBeVisible();
-		await expect( profilePage.page.locator( '#pass-strength-result' ) ).toBeHidden();
-		await expect( profilePage.page.getByRole( 'checkbox', { name: 'pw_weak' } ) ).toBeHidden();
+		await expect( page.locator( '.strength-meter' ) ).toBeVisible();
+		await expect( page.locator( '#pass-strength-result' ) ).toBeHidden();
+		await expect( page.getByRole( 'checkbox', { name: 'pw_weak' } ) ).toBeHidden();
 
 		// Wait for the default password to be validated.
-		await expect( profilePage.page.locator( '#pass1' ) ).not.toBeEmpty();
+		await expect( page.locator( '#pass1' ) ).not.toBeEmpty();
 		await expect(
-			profilePage.page.locator( '.strength-meter' ).filter( { hasNotText: 'Validating' } )
+			page.locator( '.strength-meter' ).filter( { hasNotText: 'Validating' } )
 		).toBeVisible();
 
 		// Enter a weak password.
-		const passwordInput = profilePage.page.locator( '#pass1' );
+		const passwordInput = page.locator( '#pass1' );
 		await passwordInput.fill( 'password' );
 		await passwordInput.evaluate( input => {
 			input.dispatchEvent( new Event( 'change', { bubbles: true } ) );
 		} );
 
 		// Validate that the Jetpack password strength meter displays "Weak".
-		await expect( profilePage.page.locator( '.strength-meter' ).getByText( 'Weak' ) ).toBeVisible();
-		await expect( profilePage.page.getByText( 'Strong password' ) ).toHaveCSS(
+		await expect( page.locator( '.strength-meter' ).getByText( 'Weak' ) ).toBeVisible();
+		await expect( page.getByText( 'Strong password' ) ).toHaveCSS( 'color', 'rgb(230, 80, 84)' );
+		await expect( page.getByText( 'Not a leaked password' ) ).toHaveCSS(
 			'color',
 			'rgb(230, 80, 84)'
 		);
-		await expect( profilePage.page.getByText( 'Not a leaked password' ) ).toHaveCSS(
-			'color',
-			'rgb(230, 80, 84)'
-		);
-		await expect( profilePage.page.getByText( 'Between 6 and 150 characters' ) ).toHaveCSS(
+		await expect( page.getByText( 'Between 6 and 150 characters' ) ).toHaveCSS(
 			'color',
 			'rgb(0, 135, 16)'
 		);
-		await expect( profilePage.page.getByText( "Doesn't match existing user data" ) ).toHaveCSS(
+		await expect( page.getByText( "Doesn't match existing user data" ) ).toHaveCSS(
 			'color',
 			'rgb(0, 135, 16)'
 		);
-		await expect( profilePage.page.getByText( 'Not used recently' ) ).toHaveCSS(
-			'color',
-			'rgb(0, 135, 16)'
-		);
+		await expect( page.getByText( 'Not used recently' ) ).toHaveCSS( 'color', 'rgb(0, 135, 16)' );
 
-		await expect( profilePage.page.getByText( 'Confirm use of weak password' ) ).toBeVisible();
-		await expect( profilePage.page.getByText( 'Update Profile', { exact: true } ) ).toBeDisabled();
+		await expect( page.getByText( 'Confirm use of weak password' ) ).toBeVisible();
+		await expect( page.getByText( 'Update Profile', { exact: true } ) ).toBeDisabled();
 
 		// check the checkbox to disable the weak password.
-		await profilePage.page.locator( '.pw-checkbox' ).check();
+		await page.locator( '.pw-checkbox' ).check();
 
-		await expect( profilePage.page.getByText( 'Update Profile', { exact: true } ) ).toBeEnabled();
+		await expect( page.getByText( 'Update Profile', { exact: true } ) ).toBeEnabled();
 
 		// update the password.
-		await profilePage.page.getByText( 'Update Profile', { exact: true } ).click();
+		await page.getByText( 'Update Profile', { exact: true } ).click();
 
 		// Wait for the navigation to complete.
-		await profilePage.page.waitForURL( '/wp-admin/profile.php' );
+		await page.waitForURL( '/wp-admin/profile.php' );
 
 		// Validate that the password was updated.
-		await expect( profilePage.page.getByText( 'Profile updated.' ) ).toBeVisible();
+		await expect( page.getByText( 'Profile updated.' ) ).toBeVisible();
 
 		// Need to save the storage state after the password update, otherwise the next test will fail.
 		// TODO: we should use a different user for this test instead of updating the password of the main user.
-		await profilePage.page.context().storageState( { path: process.env.STORAGE_STATE_PATH } );
+		await page.context().storageState( { path: process.env.STORAGE_STATE_PATH } );
 	} );
 } );
--- tools/e2e-commons/pages/wp-admin/index.js
+++ tools/e2e-commons/pages/wp-admin/index.js
@@ -2,7 +2,6 @@ export { default as DashboardPage } from './dashboard.js';
 export { default as JetpackDashboardPage } from './jetpack-dashboard.js';
 export { default as WPLoginPage } from './login.js';
 export { default as PluginsPage } from './plugins.js';
-export { default as ProfilePage } from './profile.js';
 export { default as ThemesPage } from './themes.js';
 export { default as RecommendationsPage } from './recommendations.js';
 export { default as Sidebar } from './sidebar.js';
