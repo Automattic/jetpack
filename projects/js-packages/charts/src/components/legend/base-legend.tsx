7c61e21860e244a781d549c1a4d4661a65234b01
annacmc <anna.mcphee@automattic.com>
2025-08-01 21:08:44 +1000

    Use non-breaking space for legend label-value spacing

--- projects/js-packages/charts/src/components/legend/base-legend.tsx
+++ projects/js-packages/charts/src/components/legend/base-legend.tsx
@@ -133,7 +133,7 @@ export const BaseLegend = forwardRef< HTMLDivElement, BaseLegendProps >(
 									{ label.text }
 									{ items.find( item => item.label === label.text )?.value && (
 										<span className={ styles[ 'legend-item-value' ] }>
-											{ ' ' }
+											{ '\u00A0' }
 											{ items.find( item => item.label === label.text )?.value }
 										</span>
 									) }
