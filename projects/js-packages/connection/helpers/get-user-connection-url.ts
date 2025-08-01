ffc7793d15180e19b24bd30288a5e8da05c90eae
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-08-01 10:06:20 +0530

    Connection: Add from argument to user connection url

--- projects/js-packages/connection/helpers/get-user-connection-url.ts
+++ projects/js-packages/connection/helpers/get-user-connection-url.ts
@@ -1,17 +1,25 @@
 import { getJetpackAdminPageUrl, getMyJetpackUrl } from '@automattic/jetpack-script-data';
 import { addQueryArgs } from '@wordpress/url';
 
+export type UserConnectionUrlOptions = {
+	redirectUrl?: string | null;
+	from?: string | null;
+};
+
 /**
  * Generates the user connection URL.
  *
- * @param redirectUrl - Optional redirect URL after connection. Defaults to the My Jetpack URL.
+ * @param options - Options for generating the user connection URL.
  *
  * @return The URL for user connection.
  */
-export function getUserConnectionUrl( redirectUrl?: string | null ): string {
+export function getUserConnectionUrl( options: UserConnectionUrlOptions = {} ): string {
+	const { redirectUrl, from } = options;
+
 	return addQueryArgs( getJetpackAdminPageUrl(), {
 		// 'connect_url_redirect' is handled in \Automattic\Jetpack\Connection\Webhooks::controller()
 		connect_url_redirect: 1,
 		redirect_after_auth: redirectUrl ?? getMyJetpackUrl(),
+		from: from ?? 'jetpack-onboarding',
 	} );
 }
