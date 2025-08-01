9e7b035edb9dc8ec16b782813ac7019c3fe354a2
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-08-01 14:58:12 -0600

    Charts: Provide locale during tooltip tests (#44594)
    * Use explicit locale for tests
    
    * Add changelog

--- projects/js-packages/charts/src/components/line-chart/test/line-chart.test.tsx
+++ projects/js-packages/charts/src/components/line-chart/test/line-chart.test.tsx
@@ -697,7 +697,13 @@ describe( 'LineChart', () => {
 			const user = userEvent.setup();
 			const customTooltipRenderer = jest.fn( ( { tooltipData } ) => (
 				<div role="tooltip" data-testid="custom-tooltip">
-					Custom: { tooltipData?.nearestDatum?.datum?.date?.toLocaleDateString() }
+					Custom:{ ' ' }
+					{ tooltipData?.nearestDatum?.datum?.date?.toLocaleDateString( 'ja-JP', {
+						weekday: 'long',
+						year: 'numeric',
+						month: 'long',
+						day: 'numeric',
+					} ) }
 				</div>
 			) );
 
@@ -709,7 +715,7 @@ describe( 'LineChart', () => {
 			// Click on right arrow key to focus on the first tooltip.
 			await user.keyboard( '{ArrowRight}' );
 			expect( screen.getByTestId( 'chart-tooltip-0' ) ).toHaveFocus();
-			expect( screen.getByTestId( 'chart-tooltip-0' ) ).toHaveTextContent( '1/1/2024' );
+			expect( screen.getByTestId( 'chart-tooltip-0' ) ).toHaveTextContent( '2024年1月1日月曜日' );
 
 			const customTooltip = screen.getByTestId( 'custom-tooltip' );
 			expect( customTooltip ).toBeInTheDocument();
