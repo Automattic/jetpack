9f07b68cd9e602b463f1f3f46cddcd18454eb993
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-08-04 08:50:38 +0530

    Add skip_pricing param defaulting to true

--- projects/js-packages/connection/helpers/get-user-connection-url.ts
+++ projects/js-packages/connection/helpers/get-user-connection-url.ts
@@ -4,6 +4,7 @@ import { addQueryArgs } from '@wordpress/url';
 export type UserConnectionUrlOptions = {
 	redirectUrl?: string | null;
 	from?: string | null;
+	skip_pricing?: boolean | null;
 };
 
 /**
@@ -14,12 +15,13 @@ export type UserConnectionUrlOptions = {
  * @return The URL for user connection.
  */
 export function getUserConnectionUrl( options: UserConnectionUrlOptions = {} ): string {
-	const { redirectUrl, from } = options;
+	const { redirectUrl, from, skip_pricing = true } = options;
 
 	return addQueryArgs( getJetpackAdminPageUrl(), {
 		// 'connect_url_redirect' is handled in \Automattic\Jetpack\Connection\Webhooks::controller()
 		connect_url_redirect: 1,
 		redirect_after_auth: redirectUrl ?? getMyJetpackUrl(),
 		from: from ?? 'my-jetpack',
+		skip_pricing,
 	} );
 }
