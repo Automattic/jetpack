5eb20933c404e30291baadda868dfc7ee01c6179
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-08-05 16:00:27 +0300

    Remove PluginsPage component and its exports from the admin pages

--- tools/e2e-commons/pages/wp-admin/index.js
+++ tools/e2e-commons/pages/wp-admin/index.js
@@ -1,4 +1,3 @@
 export { default as DashboardPage } from './dashboard.js';
-export { default as PluginsPage } from './plugins.js';
 export { default as ThemesPage } from './themes.js';
 export { default as Sidebar } from '../sidebar.js';
