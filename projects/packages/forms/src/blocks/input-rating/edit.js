a5fd3123412692b297cc94e04ac71ea5b76de0ff
Miguel Lezama <miguel@automattic.com>
2025-08-06 09:32:08 -0300

    Forms: Fix rating field causing unsaved post state (#44651)

--- projects/packages/forms/src/blocks/input-rating/edit.js
+++ projects/packages/forms/src/blocks/input-rating/edit.js
@@ -1,5 +1,4 @@
 import { useBlockProps } from '@wordpress/block-editor';
-import { useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { DEFAULT_GLYPHS } from './constants';
 import Symbols from './symbols';
@@ -11,21 +10,16 @@ import './editor.scss';
  * Interactive rating component that renders clickable symbols (stars, hearts, etc.)
  * and handles user input. Reads values from parent field-rating block via context.
  *
- * @param {object}   props               - Component props from WordPress block editor
- * @param {object}   props.context       - Block context values from parent block
- * @param {Function} props.setAttributes - Function to update block attributes
- * @param {string}   props.clientId      - Unique identifier for the block instance
+ * @param {object} props          - Component props from WordPress block editor
+ * @param {object} props.context  - Block context values from parent block
+ * @param {string} props.clientId - Unique identifier for the block instance
  * @return {import('react').JSX.Element} Rating input editor component
  */
-export default function RatingInputEdit( { context, setAttributes, clientId } ) {
+export default function RatingInputEdit( { context, clientId } ) {
 	const max = context?.[ 'jetpack/field-rating-max' ] || 5;
 	const defaultValue = context?.[ 'jetpack/field-rating-default' ] || 0;
 	const className = context?.[ 'jetpack/field-rating-className' ] || 'is-style-stars';
 	const onChangeDefault = context?.[ 'jetpack/field-rating-onChangeDefault' ] || ( () => {} );
-
-	useEffect( () => {
-		setAttributes( { className } );
-	}, [ className, setAttributes ] );
 
 	// Get icon component based on className
 	const icon = className.includes( 'is-style-hearts' )
