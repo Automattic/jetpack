91037b6982756b122bcd3bfdfe03d32c8093829b
Jeremy Herve <jeremy@jeremy.hu>
2025-08-06 20:51:23 +0200

    Revert "Provide a more explicit error messages for broken connections"
    This reverts commit b85fb25b4f2d0f89bf21abd8487f6f5ef04fb73b.

--- projects/packages/publicize/src/rest-api/class-base-controller.php
+++ projects/packages/publicize/src/rest-api/class-base-controller.php
@@ -90,12 +90,6 @@ abstract class Base_Controller extends WP_REST_Controller {
 
 		if ( $this->allow_requests_as_blog && self::is_authorized_blog_request() ) {
 			return true;
-		} else {
-			return new WP_Error(
-				'rest_unauthorized',
-				__( 'Please connect your user account to WordPress.com', 'jetpack-publicize-pkg' ),
-				array( 'status' => rest_authorization_required_code() )
-			);
 		}
 
 		if ( $publicize->current_user_can_access_publicize_data() ) {
