8d0055fe5522e428759d32278fcc2cbc79b82c2f
Jeremy Herve <jeremy@jeremy.hu>
2025-08-06 20:44:03 +0200

    Improve permission check readability

--- projects/packages/publicize/src/rest-api/class-base-controller.php
+++ projects/packages/publicize/src/rest-api/class-base-controller.php
@@ -88,25 +88,23 @@ abstract class Base_Controller extends WP_REST_Controller {
 			);
 		}
 
-		if ( $this->allow_requests_as_blog && self::is_authorized_blog_request() ) {
-			return true;
-		} else {
+		if ( ! $this->allow_requests_as_blog || ! self::is_authorized_blog_request() ) {
 			return new WP_Error(
 				'rest_unauthorized',
-				__( 'Please connect your user account to WordPress.com', 'jetpack-publicize-pkg' ),
+				__( 'You must be connected to WordPress.com to access this data.', 'jetpack-publicize-pkg' ),
 				array( 'status' => rest_authorization_required_code() )
 			);
 		}
 
-		if ( $publicize->current_user_can_access_publicize_data() ) {
-			return true;
+		if ( ! $publicize->current_user_can_access_publicize_data() ) {
+			return new WP_Error(
+				'invalid_user_permission_publicize',
+				__( 'Sorry, you are not allowed to access Jetpack Social data on this site.', 'jetpack-publicize-pkg' ),
+				array( 'status' => rest_authorization_required_code() )
+			);
 		}
 
-		return new WP_Error(
-			'invalid_user_permission_publicize',
-			__( 'Sorry, you are not allowed to access Jetpack Social data on this site.', 'jetpack-publicize-pkg' ),
-			array( 'status' => rest_authorization_required_code() )
-		);
+		return true;
 	}
 
 	/**
