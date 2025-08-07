75a17703b82697bd64f5debebdb1e52c742995b7
Miguel Lezama <lezama@gmail.com>
2025-08-07 15:52:45 -0300

    Refactor rating input styles and SVG attributes

--- projects/packages/forms/src/blocks/input-rating/edit.js
+++ projects/packages/forms/src/blocks/input-rating/edit.js
@@ -29,6 +29,8 @@ export default function RatingInputEdit( { attributes, context, clientId } ) {
 		<SVG className="jetpack-field-rating__icon" viewBox="0 0 24 24" aria-hidden="true">
 			<Path
 				d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.62L12 2 9.19 8.62 2 9.24l5.46 4.73L5.82 21z"
+				fill="none"
+				stroke="currentColor"
 				strokeWidth="2"
 				strokeLinejoin="round"
 			></Path>
@@ -38,6 +40,8 @@ export default function RatingInputEdit( { attributes, context, clientId } ) {
 		<SVG className="jetpack-field-rating__icon" viewBox="0 0 24 24" aria-hidden="true">
 			<Path
 				d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+				fill="none"
+				stroke="currentColor"
 				strokeWidth="2"
 				strokeLinejoin="round"
 			></Path>
@@ -65,14 +69,11 @@ export default function RatingInputEdit( { attributes, context, clientId } ) {
 					type="radio"
 					name={ `rating-${ clientId }` }
 					value={ `${ i }/${ max }` }
-					className="jetpack-field-rating__input radio grunion-field"
+					className="jetpack-field-rating__input"
 					checked={ defaultValue === i }
 					onChange={ () => handleChange( i ) }
 				/>
-				<label
-					htmlFor={ radioId }
-					className="jetpack-field-rating__label grunion-radio-label rating"
-				>
+				<label htmlFor={ radioId } className="jetpack-field-rating__label">
 					{ iconSvg }
 				</label>
 			</div>
