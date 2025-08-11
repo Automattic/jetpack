31cf5a7f38620eb375d0c70cc1328431f39a3ad6
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-08-11 14:11:27 +0300

    Fix imports

--- projects/plugins/boost/tests/e2e/specs/modules/score-auto-refresh.test.ts
+++ projects/plugins/boost/tests/e2e/specs/modules/score-auto-refresh.test.ts
@@ -1,5 +1,5 @@
-import playwrightConfig from 'playwright.config';
 import { test, expect } from '../../lib/fixtures/test';
+import playwrightConfig from '../../playwright.config';
 
 test.describe( 'Auto refresh of speed scores', () => {
 	test.beforeAll( async ( { browser, boostUtils } ) => {
--- projects/plugins/boost/tests/e2e/specs/page-cache/page-cache.test.ts
+++ projects/plugins/boost/tests/e2e/specs/page-cache/page-cache.test.ts
@@ -1,5 +1,5 @@
-import playwrightConfig from 'playwright.config';
 import { test, expect } from '../../lib/fixtures/test';
+import playwrightConfig from '../../playwright.config';
 
 test.describe( 'Cache module', () => {
 	test.beforeAll( async ( { browser, boostUtils } ) => {
