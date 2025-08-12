24a85705a4271cfff2b5835dc44dc9c9309e4540
Enej Bajgoric <enej.bajgoric@automattic.com>
2025-08-12 07:31:32 -0700

    Add a test to cover the error state

--- projects/packages/forms/tests/php/contact-form/Contact_Form_Test.php
+++ projects/packages/forms/tests/php/contact-form/Contact_Form_Test.php
@@ -3041,11 +3041,13 @@ EOT;
 		// Create a form submission
 		$_POST = Utility::get_post_request(
 			array(
-				'name'   => $name,
-				'email'  => $email,
-				'invite' => 'hello@world', // not required
-				'choose' => $choose,
-				'pick'   => $pick,
+				'name'        => $name,
+				'email'       => $email,
+				'invite'      => 'hello@world', // not required
+				'choose'      => $choose,
+				'chooseradio' => 'not-a-value',
+				'radioempty'  => '',
+				'pick'        => $pick,
 			),
 			'g' . $form_id
 		);
@@ -3055,7 +3057,7 @@ EOT;
 				'title'       => 'Test Form',
 				'description' => 'This is a test form.',
 			),
-			"[contact-field label='Name' type='name' required='1'/][contact-field label='Email' type='email' required='1'/][contact-field label='Invite' type='email' /][contact-field label='Choose' type='checkbox-multiple' options='truth,dare' required='1'/][contact-field label='Pick' type='checkbox-multiple' options='truth,dare' required='1'/]"
+			"[contact-field label='Name' type='name' required='1'/][contact-field label='Email' type='email' required='1'/][contact-field label='Invite' type='email' /][contact-field label='Choose' type='checkbox-multiple' options='truth,dare' required='1'/][contact-field label='Choose Radio' type='radio' options='truth,dare' required='1'/][contact-field label='Choose Empty' type='radio' options='truth,dare' required='1'/][contact-field label='Pick' type='checkbox-multiple' options='truth,dare' required='1'/]"
 		);
 		$form->validate();
 		unset( $_POST ); // Clean up the global $_POST variable after the test.
@@ -3068,6 +3070,8 @@ EOT;
 				'Email requires a valid email address.',
 				'Invite requires a valid email address.',
 				'Choose requires at least one selection.',
+				'Choose Radio requires at least one selection.',
+				'Choose Empty requires at least one selection.',
 				'Pick requires at least one selection.',
 			),
 			$form->get_error_messages()
