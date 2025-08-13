8d3587d7b09fe643d582fa7eaa89d7971d1f264f
Erick Danzer <edanzer@gmail.com>
2025-08-13 12:29:08 -0600

    Implement step on frontend

--- projects/packages/forms/src/contact-form/class-contact-form-plugin.php
+++ projects/packages/forms/src/contact-form/class-contact-form-plugin.php
@@ -3159,6 +3159,7 @@ class Contact_Form_Plugin {
 		$atts['min']     = isset( $parent_attrs['min'] ) ? $parent_attrs['min'] : 0;
 		$atts['max']     = isset( $parent_attrs['max'] ) ? $parent_attrs['max'] : 100;
 		$atts['default'] = isset( $parent_attrs['default'] ) ? $parent_attrs['default'] : 0;
+		$atts['step']    = isset( $parent_attrs['step'] ) ? $parent_attrs['step'] : 1;
 
 		$atts = self::block_attributes_to_shortcode_attributes( $atts, 'slider', $block );
 		return Contact_Form::parse_contact_field( $atts, $content, $block );
