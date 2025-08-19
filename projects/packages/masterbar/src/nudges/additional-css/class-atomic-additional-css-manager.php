a86698b3bbd2135ab6143bc13a26e6a0dc9b262f
Claudiu Filip <claudiu.filip@automattic.com>
2025-08-19 16:39:46 +0300

    Undo upsell plan changes and just fix the Business plan slug
    This reverts commit 5c30ee472ca438ef9a8d04da23e1bfa724de475f.

--- projects/packages/masterbar/src/nudges/additional-css/class-atomic-additional-css-manager.php
+++ projects/packages/masterbar/src/nudges/additional-css/class-atomic-additional-css-manager.php
@@ -36,11 +36,11 @@ class Atomic_Additional_CSS_Manager {
 	 * @param \WP_Customize_Manager $wp_customize_manager Core customize manager.
 	 */
 	public function register_nudge( \WP_Customize_Manager $wp_customize_manager ) {
-		$plan_name = $this->get_plan()->product_name_short;
+		$plan = $this->get_plan_name();
 
 		$nudge_url = $this->get_nudge_url();
 		/* translators: %s is the plan name. */
-		$nudge_text = sprintf( __( 'Purchase the %s plan to<br> activate CSS customization', 'jetpack-masterbar' ), $plan_name );
+		$nudge_text = sprintf( __( 'Purchase the %s plan to<br> activate CSS customization', 'jetpack-masterbar' ), $plan );
 
 		$nudge = new CSS_Customizer_Nudge(
 			$nudge_url,
@@ -54,22 +54,20 @@ class Atomic_Additional_CSS_Manager {
 	}
 
 	/**
-	 * Get the plan.
+	 * Get the plan name.
 	 *
 	 * @return mixed
 	 */
-	protected function get_plan() {
-		$plan_slug = apply_filters( 'wpcom_customize_css_plan_slug', 'value_bundle' );
-
-		return \Automattic\Jetpack\Plans::get_plan( $plan_slug );
+	protected function get_plan_name() {
+		return \Automattic\Jetpack\Plans::get_plan( 'business-bundle' )->product_name_short;
 	}
 
 	/**
-	 * Get the nudge URL.
+	 * Get the Nudge URL.
 	 *
 	 * @return string
 	 */
 	private function get_nudge_url() {
-		return '/checkout/' . $this->domain . '/' . $this->get_plan()->path_slug;
+		return '/checkout/' . $this->domain . '/business';
 	}
 }
