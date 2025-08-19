354e23db8bc552b0585fc2320fed9e6ff95a9fee
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-08-19 20:02:20 +0300

    Add polling to check for blocked request

--- projects/plugins/jetpack/tests/e2e/specs/post-connection/waf-blocking.test.ts
+++ projects/plugins/jetpack/tests/e2e/specs/post-connection/waf-blocking.test.ts
@@ -12,15 +12,17 @@ test.describe.parallel( 'WAF Blocking', () => {
 
 	test( 'Block a simple request', async ( { page } ) => {
 		await test.step( 'Block it', async () => {
-			const response = await page.goto( '/?blubb=<script>' );
-			expect( response!.status() ).toStrictEqual( 403 );
+			await expect( async () => {
+				const response = await page.goto( '/?blubb=<script>' );
+				expect( response!.status() ).toStrictEqual( 403 );
 
-			/*
-			The job of the WAF is to block certain requests, and that is what we are testing here.
-			Given that when a request is blocked, the code does die() with a specific message, we never render the page.
-			The assertion is just to ensure that we indeed do not see a page rendered in the output.
-			 */
-			expect( response!.body() ).not.toContain( '<html>' );
+				/*
+				The job of the WAF is to block certain requests, and that is what we are testing here.
+				Given that when a request is blocked, the code does die() with a specific message, we never render the page.
+				The assertion is just to ensure that we indeed do not see a page rendered in the output.
+				 */
+				expect( response!.body() ).not.toContain( '<html>' );
+			} ).toPass( { intervals: [ 1000 ], timeout: 30000 } );
 		} );
 	} );
 } );
