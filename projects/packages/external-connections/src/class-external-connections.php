c3089fd32ba3e34314e2c29f6be067871e48c5db
mmtr <1233880+mmtr@users.noreply.github.com>
2025-08-20 11:27:00 +0200

    External Connections: Add skeleton package

---
+++ projects/packages/external-connections/src/class-external-connections.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Package description here
+ *
+ * @package automattic/jetpack-external-connections
+ */
+
+namespace Automattic\Jetpack;
+
+/**
+ * Class description.
+ */
+class External_Connections {
+
+	const PACKAGE_VERSION = '0.1.0-alpha';
+}
