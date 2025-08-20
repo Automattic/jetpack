d508555a4081e017567debc962cab4faef16a6e9
mmtr <1233880+mmtr@users.noreply.github.com>
2025-08-20 15:23:29 +0200

    Fix JS build path

--- projects/packages/external-connections/webpack.config.js
+++ projects/packages/external-connections/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = [
 		output: {
 			...jetpackWebpackConfig.output,
 			filename: '[name]/[name].js',
-			path: path.resolve( __dirname, 'build' ),
+			path: path.resolve( __dirname, 'src/build' ),
 		},
 		optimization: {
 			...jetpackWebpackConfig.optimization,
