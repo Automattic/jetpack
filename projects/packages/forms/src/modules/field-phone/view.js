9e75c606b49a5741981331bfe819d2885971bfe4
Christian Gastrell <cgastrell@gmail.com>
2025-08-20 01:07:06 -0300

    adapt process to use two letter country code, derive state from it

--- projects/packages/forms/src/modules/field-phone/view.js
+++ projects/packages/forms/src/modules/field-phone/view.js
@@ -88,6 +88,8 @@ const { actions } = store( NAMESPACE, {
 	callbacks: {
 		initializeCountrySelector() {
 			const context = getContext();
+			window.parsePhoneNumber = parsePhoneNumber;
+			window.AsYouType = AsYouType;
 			if ( context.showCountrySelector ) {
 				context.countryList = countries.map( country => ( {
 					...country,
