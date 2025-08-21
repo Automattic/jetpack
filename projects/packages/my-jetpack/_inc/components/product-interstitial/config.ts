41bd869fa711cf0a6f9443af540d00e030bff522
Gergely Juhasz <gergely.juhasz@automattic.com>
2025-08-21 14:35:00 +0200

    Add config file for CRM

--- projects/packages/my-jetpack/_inc/components/product-interstitial/config.ts
+++ projects/packages/my-jetpack/_inc/components/product-interstitial/config.ts
@@ -2,6 +2,7 @@ import { JetpackProductWithCard } from '../../types';
 import { getAntiSpamConfig } from './products/anti-spam.tsx';
 import { getBackupConfig } from './products/backup';
 import { getBoostConfig } from './products/boost';
+import { getCrmConfig } from './products/crm.tsx';
 import { getJetpackAiConfig } from './products/jetpack-ai';
 import { getProtectConfig } from './products/protect';
 import { getSearchConfig } from './products/search';
@@ -17,6 +18,7 @@ export const getProductConfigs = (): ProductConfigs => ( {
 	'anti-spam': getAntiSpamConfig(),
 	backup: getBackupConfig(),
 	boost: getBoostConfig(),
+	crm: getCrmConfig(),
 	protect: getProtectConfig(),
 	social: getSocialConfig(),
 	search: getSearchConfig(),
