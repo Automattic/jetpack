8f9e82d8a5266659bb971edbc57ad52d129d9bf6
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-08-21 20:39:05 +0300

    Enhance SuiteFixerReporter: add comments to clarify suite title prefixing logic for CI

--- tools/e2e-commons/reporters/suite-fixer.ts
+++ tools/e2e-commons/reporters/suite-fixer.ts
@@ -2,6 +2,10 @@ import type { FullConfig, Reporter, Suite } from '@playwright/test/reporter';
 
 class SuiteFixerReporter implements Reporter {
 	onBegin( _config: FullConfig, suite: Suite ) {
+		// If running in CI, prefix suite titles with the project name.
+		// This helps to identify the global projects tests in the report.
+		// Example: global authentication will run multiple times, for each project that depends on it, but Allure considers it as a single test and will display it as a retry.
+		// By updating the suite title, we can ensure that such tests are identifiable in the report.
 		for ( const child of suite.suites ) {
 			child.title = `${ process.env.PROJECT_NAME ? `${ process.env.PROJECT_NAME }: ` : '' }${
 				child.title
