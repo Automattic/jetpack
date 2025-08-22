b5e189cc91f187ca0a7e08f289754e0e31b5d943
Omar Alshaker <omar@omaralshaker.com>
2025-08-22 23:20:16 +0200

    Fix LaTeX frontend script

--- projects/plugins/jetpack/extensions/blocks/latex/view.js
+++ projects/plugins/jetpack/extensions/blocks/latex/view.js
@@ -3,7 +3,7 @@ import katex from 'katex';
 import './style.scss';
 
 function renderAllLatexBlocks() {
-	const blocks = document.querySelectorAll( '.jetpack-latex' );
+	const blocks = document.querySelectorAll( '.wp-block-jetpack-latex' );
 	blocks.forEach( block => {
 		const latex = block.getAttribute( 'data-latex' ) || '';
 		const renderContainers = block.querySelectorAll( '.jetpack-latex-render' );
