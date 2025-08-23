aea9017794594128e2b145149e8904da1cb14cb0
Enej Bajgoric <enej.bajgoric@automattic.com>
2025-08-22 17:23:06 -0700

    Make beta work as expected.

--- projects/packages/forms/src/blocks/contact-form/child-blocks.js
+++ projects/packages/forms/src/blocks/contact-form/child-blocks.js
@@ -55,7 +55,6 @@ export const childBlocks = [
 	JetpackNumberField,
 	JetpackPhoneInput,
 	JetpackSingleChoiceField,
-	JetpackHiddenField,
 	JetpackTextField,
 	JetpackUrlField,
 	JetpackTelephoneField,
@@ -64,7 +63,7 @@ export const childBlocks = [
 	...( getJetpackBlocksVariation() === 'experimental'
 		? [ JetpackRatingField, JetpackRatingInput, JetpackFieldSlider, JetpackSliderInput ]
 		: [] ),
-	...( getJetpackBlocksVariation() === 'beta' ? [ JetpackTimeField ] : [] ),
+	...( getJetpackBlocksVariation() === 'beta' ? [ JetpackTimeField, JetpackHiddenField ] : [] ),
 
 	// The following are required for these blocks to be parsed correctly in block
 	// deprecations. They have been flagged with `supports.inserter: false` to
