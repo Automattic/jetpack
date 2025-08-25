70fa1c79bba32725461e1dff8263930fa47a961e
annacmc <anna.mcphee@automattic.com>
2025-08-25 16:24:45 +1000

    Update leaderboard chart stories to use centralized sample data

--- projects/js-packages/charts/src/components/leaderboard-chart/stories/index.stories.tsx
+++ projects/js-packages/charts/src/components/leaderboard-chart/stories/index.stories.tsx
@@ -1,8 +1,13 @@
 import { ThemeProvider, jetpackTheme, wooTheme } from '../../../providers/theme';
-import { leaderboardSample as sampleData } from '../../../stories/sample-data';
+import {
+	leaderboardSample as sampleData,
+	leaderboardSmall as smallDataset,
+	leaderboardLarge as largeValues,
+	leaderboardNegativeGrowth as negativeGrowth,
+	leaderboardWithImageColor as dataWithImageColor,
+} from '../../../stories/sample-data';
 import { formatMetricValue } from '../../shared/format-metric-value';
 import { LeaderboardChart } from '../leaderboard-chart';
-import { smallDataset, largeValues, negativeGrowth, dataWithImageColor } from './sample-data';
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta< typeof LeaderboardChart > = {
