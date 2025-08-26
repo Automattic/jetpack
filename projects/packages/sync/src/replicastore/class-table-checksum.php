a9640f4358d47123f4ee0d7ac63703bb2662a158
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2025-08-26 16:27:39 +0800

    Add WooCommerce products to Table_Checksum class
    - Introduced a new configuration for 'woocommerce_products' in the Table_Checksum class.
    - Added fields for table, range_field, key_fields, checksum_fields, and filter_values.
    - Implemented a callback to check if the WooCommerce products table is enabled.

--- projects/packages/sync/src/replicastore/class-table-checksum.php
+++ projects/packages/sync/src/replicastore/class-table-checksum.php
@@ -289,6 +289,21 @@ class Table_Checksum {
 			),
 			'links'                      => $wpdb->links, // TODO describe in the array format or add exceptions.
 			'options'                    => $wpdb->options, // TODO describe in the array format or add exceptions.
+			'woocommerce_products'       => array(
+				'table'                     => $wpdb->posts,
+				'range_field'               => 'ID',
+				'key_fields'                => array( 'ID' ),
+				'checksum_fields'           => array( 'post_modified_gmt' ),
+				'filter_values'             => array(
+					'post_type' => array(
+						'operator' => 'IN',
+						'values'   => array( 'product', 'product_variation' ),
+					),
+				),
+				'is_table_enabled_callback' => function () {
+					return false !== Sync\Modules::get_module( 'woocommerce_products' );
+				},
+			),
 			'woocommerce_order_items'    => array(
 				'table'                     => "{$wpdb->prefix}woocommerce_order_items",
 				'range_field'               => 'order_item_id',
