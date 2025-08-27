8dd9b947a54503fad7ed407d963864aa309206ca
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-08-27 11:21:06 -0600

    Add plugin to allow sending from localhost

---
+++ tools/docker/mu-plugins/allow_emails_from_localhost.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: Allow Emails from localhost
+ * Description: WordPress doesn't allow emails from localhost so we changes the hostname when sending.
+ * Version: 1.0
+ * Author: Automattic
+ * Author URI: https://automattic.com/
+ * Text Domain: jetpack
+ *
+ * @package automattic/jetpack
+ */
+
+namespace Jetpack\Docker\MuPlugin\AllowEmailsFromLocalhost;
+
+/**
+ * Allow sending emails from the Docker dev environment.
+ * WordPress relies on PHPMailer, which throws an exception when
+ * using localhost as the server name. This mocks a different
+ * email when needed.
+ *
+ * @param string $from_email Email address to send from.
+ *
+ * @return string Filtered email address to send from.
+ */
+function jetpack_allow_emails_from_localhost( $from_email ) {
+	if ( str_ends_with( $from_email, '@localhost' ) ) {
+		return 'wordpress@jetpack.docker';
+	}
+	return $from_email;
+}
+add_filter( 'wp_mail_from', __NAMESPACE__ . '\jetpack_allow_emails_from_localhost', 1, 3 );
