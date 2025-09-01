ac35458bdcabe7d80006bc2171bfaa6ce781b80c
Dognose <dognose24@gmail.com>
2025-09-01 11:13:10 +0800

    Remove unnecessary types exportation

--- projects/js-packages/charts/src/components/conversion-funnel-chart/index.ts
+++ projects/js-packages/charts/src/components/conversion-funnel-chart/index.ts
@@ -2,7 +2,4 @@ export {
 	ConversionFunnelChart,
 	type ConversionFunnelChartProps,
 	type FunnelStep,
-	type StepLabelRenderProps,
-	type StepRateRenderProps,
-	type MainMetricRenderProps,
 } from './conversion-funnel-chart';
