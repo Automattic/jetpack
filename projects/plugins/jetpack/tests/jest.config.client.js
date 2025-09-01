c8a700e5ed2f9800bbcbfb05147f91fad929ae42
Peter Petrov <peter.petrov89@gmail.com>
2025-09-01 16:24:18 +0300

    Fix tests

--- projects/plugins/jetpack/tests/jest.config.client.js
+++ projects/plugins/jetpack/tests/jest.config.client.js
@@ -4,6 +4,7 @@ module.exports = {
 	...baseConfig,
 	roots: [ '<rootDir>/_inc/client/state/', '<rootDir>/_inc/client/lib/', '<rootDir>/modules/' ],
 	setupFilesAfterEnv: [ ...baseConfig.setupFilesAfterEnv, '<rootDir>/tests/jest-globals.gui.js' ],
+	transformIgnorePatterns: [ '/node_modules/(?!(.pnpm|@automattic|uplot)/)' ],
 	coverageDirectory: baseConfig.coverageDirectory + '/client',
 	collectCoverageFrom: [
 		'<rootDir>/_inc/client/state/**/*.{js,mjs,cjs,jsx,ts,tsx,mts,cts}',
