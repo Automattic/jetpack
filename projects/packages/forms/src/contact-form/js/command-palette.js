16435847857f59e51f039284c3c85c21ef0986bb
Mikael Korpela <mikael@ihminen.org>
2025-09-03 16:48:12 +0300

    Use useCommandLoader

--- projects/packages/forms/src/contact-form/js/command-palette.js
+++ projects/packages/forms/src/contact-form/js/command-palette.js
@@ -1,45 +1,66 @@
 import { useAnalytics } from '@automattic/jetpack-shared-extension-utils';
-import { useCommand } from '@wordpress/commands';
-import domReady from '@wordpress/dom-ready';
+import { useCommandLoader } from '@wordpress/commands';
+import { useMemo } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { trash } from '@wordpress/icons';
 import FormsIcon from '../../blocks/shared/components/forms-icon';
 import { spam } from '../../dashboard/icons';
 
-domReady( () => {
-	// Command palette available across WP Admin from WP 6.9
-	if ( typeof useCommand === 'function' ) {
+const getDashboardUrl = status =>
+	`admin.php?page=jetpack-forms-admin#/responses?status=${ status }`;
+
+const getFormsCommands = () =>
+	function useFormsGlobalCommands() {
 		const { tracks } = useAnalytics();
 
-		useCommand( {
-			icon: FormsIcon,
-			label: __( 'View form responses', 'jetpack-forms' ),
-			name: 'jetpack/forms-inbox',
-			callback: ( { close } ) => {
-				tracks.recordEvent( 'jetpack_command_palette_forms_inbox_open' );
-				document.location.href = 'admin.php?page=jetpack-forms-admin#/responses?status=inbox';
-				close();
-			},
-		} );
-		useCommand( {
-			icon: spam,
-			label: __( 'View form spam', 'jetpack-forms' ),
-			name: 'jetpack/forms-spam',
-			callback: ( { close } ) => {
-				tracks.recordEvent( 'jetpack_command_palette_forms_spam_open' );
-				document.location.href = 'admin.php?page=jetpack-forms-admin#/responses?status=spam';
-				close();
-			},
-		} );
-		useCommand( {
-			icon: trash,
-			label: __( 'View form trash', 'jetpack-forms' ),
-			name: 'jetpack/forms-trash',
-			callback: ( { close } ) => {
-				tracks.recordEvent( 'jetpack_command_palette_forms_trash_open' );
-				document.location.href = 'admin.php?page=jetpack-forms-admin#/responses?status=trash';
-				close();
-			},
-		} );
-	}
-} );
+		const commands = useMemo( () => {
+			return [
+				{
+					icon: FormsIcon,
+					label: __( 'View form responses', 'jetpack-forms' ),
+					name: 'jetpack/forms-inbox',
+					callback: ( { close } ) => {
+						tracks.recordEvent( 'jetpack_command_palette_forms_inbox_open' );
+						document.location.href = getDashboardUrl( 'inbox' );
+						close();
+					},
+				},
+				{
+					icon: spam,
+					label: __( 'View form spam', 'jetpack-forms' ),
+					name: 'jetpack/forms-spam',
+					callback: ( { close } ) => {
+						tracks.recordEvent( 'jetpack_command_palette_forms_spam_open' );
+						document.location.href = getDashboardUrl( 'spam' );
+						close();
+					},
+				},
+				{
+					icon: trash,
+					label: __( 'View form trash', 'jetpack-forms' ),
+					name: 'jetpack/forms-trash',
+					callback: ( { close } ) => {
+						tracks.recordEvent( 'jetpack_command_palette_forms_trash_open' );
+						document.location.href = getDashboardUrl( 'trash' );
+						close();
+					},
+				},
+			];
+		}, [ tracks ] );
+
+		return {
+			commands,
+			isLoading: false,
+		};
+	};
+
+const JetpackFormsCommands = () => {
+	// Command palette available across WP Admin from Gutenberg v21.5 and (TBD) WP v6.9
+	// https://github.com/WordPress/gutenberg/pull/71030
+	useCommandLoader( {
+		name: 'jetpack/forms',
+		hook: getFormsCommands(),
+	} );
+};
+
+JetpackFormsCommands();
