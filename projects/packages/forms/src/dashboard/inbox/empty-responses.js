bb466f700d346df2b31bd995a4caf409b1c76eef
Mikael Korpela <mikael@ihminen.org>
2025-09-03 12:30:48 +0300

    More visual empty state

--- projects/packages/forms/src/dashboard/inbox/empty-responses.js
+++ projects/packages/forms/src/dashboard/inbox/empty-responses.js
@@ -1,10 +1,13 @@
+import { __experimentalText as Text } from '@wordpress/components'; // eslint-disable-line @wordpress/no-unsafe-wp-apis
 import { __ } from '@wordpress/i18n';
+import CreateFormButton from '../components/create-form-button';
 
 const EmptyResponses = () => {
 	return (
-		<p>
-			<em>{ __( 'No responses.', 'jetpack-forms' ) }</em>
-		</p>
+		<>
+			<Text>{ __( 'No responses yet.', 'jetpack-forms' ) }</Text>
+			<CreateFormButton showPatterns label={ __( 'Create a form', 'jetpack-forms' ) } />;
+		</>
 	);
 };
 
