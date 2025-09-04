2d75f06608a46d54e8f07c9d661402d2cca30061
Douglas <douglas.henri@automattic.com>
2025-09-04 20:58:06 -0300

    show actual selected options on email

--- projects/packages/forms/src/contact-form/class-feedback-field.php
+++ projects/packages/forms/src/contact-form/class-feedback-field.php
@@ -144,6 +144,8 @@ class Feedback_Field {
 				return $this->get_render_api_value();
 			case 'web': // For the post-submission page screen.
 				return $this->get_render_web_value();
+			case 'email':
+				return $this->get_render_email_value();
 			case 'ajax':
 				return $this->get_render_web_value(); // For now, we use the same value for ajax and web.
 			case 'default':
@@ -160,6 +162,26 @@ class Feedback_Field {
 	private function get_render_web_value() {
 		if ( $this->is_of_type( 'image-select' ) ) {
 			return $this->value;
+		}
+
+		return $this->get_render_default_value();
+	}
+
+	/**
+	 * Get the value of the field for rendering the email.
+	 *
+	 * @return string
+	 */
+	private function get_render_email_value() {
+		if ( $this->is_of_type( 'image-select' ) ) {
+			$choices = array();
+
+			foreach ( $this->value['choices'] as $choice ) {
+				// On the email, we want to show the actual selected value, not the perceived value, as the options can be shuffled.
+				$choices[] = $choice['selected'];
+			}
+
+			return implode( ', ', $choices );
 		}
 
 		return $this->get_render_default_value();
