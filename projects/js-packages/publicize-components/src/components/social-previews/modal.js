6acf8539a548793fa2930c3cc1cae6ba0b1e0d6d
Lance Willett <lance@automattic.com>
2025-09-04 10:24:22 -0700

    Revert "Sharing: fix typo in 'services' for sharing buttons wrapper element (#45049)" (#45075)
    This reverts commit aff8f4805e1a17a22ad1fcc9961996ff7c1aa71c.

--- projects/js-packages/publicize-components/src/components/social-previews/modal.js
+++ projects/js-packages/publicize-components/src/components/social-previews/modal.js
@@ -8,12 +8,12 @@ import { Button, Modal, TabPanel } from '@wordpress/components';
 import { decodeEntities } from '@wordpress/html-entities';
 import { __ } from '@wordpress/i18n';
 import { close } from '@wordpress/icons';
-import { useAvailableServices } from './use-available-services';
+import { useAvailableSerivces } from './use-available-services';
 import { usePostData } from './use-post-data';
 import './modal.scss';
 
 const SocialPreviewsModal = function SocialPreviewsModal( { onClose, initialTabName = null } ) {
-	const availableServices = useAvailableServices();
+	const availableServices = useAvailableSerivces();
 	const { image, media, title, description, url, excerpt } = usePostData();
 
 	return (
