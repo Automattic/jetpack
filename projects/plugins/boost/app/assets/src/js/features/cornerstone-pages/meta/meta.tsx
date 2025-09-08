b9da7665df691ec396b02f235e7828e12d9beaf4
Liam Sarsfield <43409125+LiamSarsfield@users.noreply.github.com>
2025-09-08 10:36:06 +0100

    Make logic evaulation less complex for Cornerstone Page frontend valuation

--- projects/plugins/boost/app/assets/src/js/features/cornerstone-pages/meta/meta.tsx
+++ projects/plugins/boost/app/assets/src/js/features/cornerstone-pages/meta/meta.tsx
@@ -444,19 +444,15 @@ const List: FC< ListProps > = ( {
 				);
 			}
 
-			// Fixed multisite homepage detection - exclude URLs with query parameters
+			// Only consider it homepage if it has no query parameters
 			const resolvedPath = getResolvedPath( pathname, siteUrl );
-			if ( resolvedPath === siteUrl.pathname ) {
-				// Only consider it homepage if it has no query parameters
-				const hasQueryParameters = url && url.search !== '';
-				if ( ! hasQueryParameters ) {
-					throw new Error(
-						__(
-							'The homepage does not need to be added to the list, as it is automatically included.',
-							'jetpack-boost'
-						)
-					);
-				}
+			if ( resolvedPath === siteUrl.pathname && ! url?.search ) {
+				throw new Error(
+					__(
+						'The homepage does not need to be added to the list, as it is automatically included.',
+						'jetpack-boost'
+					)
+				);
 			}
 		}
 
