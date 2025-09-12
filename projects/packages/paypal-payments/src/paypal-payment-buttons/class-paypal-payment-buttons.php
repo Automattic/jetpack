b2574525f61e98bbbc61e8e6c7b60d5a3eae91f5
Eric Binnion <ericbinnion@gmail.com>
2025-09-11 20:49:42 -0500

    PayPal Payments: Wrap single payment button in div to minimize clashes with themes

--- projects/packages/paypal-payments/src/paypal-payment-buttons/class-paypal-payment-buttons.php
+++ projects/packages/paypal-payments/src/paypal-payment-buttons/class-paypal-payment-buttons.php
@@ -114,11 +114,13 @@ class PayPal_Payment_Buttons {
 
 			$button_html = sprintf(
 				'<style>.pp-%1$s{text-align:center;border:none;border-radius:0.25rem;min-width:11.625rem;padding:0 2rem;height:2.625rem;font-weight:bold;background-color:#FFD140;color:#000000;font-family:"Helvetica Neue",Arial,sans-serif;font-size:1rem;line-height:1.25rem;cursor:pointer;}</style>
+<div>
 <form action="%2$s" method="post" target="_blank" style="display:inline-grid;justify-items:center;align-content:start;gap:0.5rem;">
   <input class="pp-%1$s" type="submit" value="%3$s" />
   <img src="https://www.paypalobjects.com/images/Debit_Credit_APM.svg" alt="cards" />
   <section style="font-size: 0.75rem;"> Powered by <img src="https://www.paypalobjects.com/paypal-ui/logos/svg/paypal-wordmark-color.svg" alt="paypal" style="height:0.875rem;vertical-align:middle;"/></section>
-</form>',
+</form>
+</div>',
 				$payment_id,
 				$action_url,
 				$button_text_escaped
