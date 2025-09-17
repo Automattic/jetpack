71022448fd6cac327c66110893ad1031acbd69f1
Miguel Lezama <miguel@automattic.com>
2025-09-17 08:47:25 -0300

    Update projects/packages/forms/src/blocks/contact-form/class-contact-form-block.php
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>

--- projects/packages/forms/src/blocks/contact-form/class-contact-form-block.php
+++ projects/packages/forms/src/blocks/contact-form/class-contact-form-block.php
@@ -115,7 +115,7 @@ class Contact_Form_Block {
 			return $content;
 		}
 
-		if ( ! class_exists( '\WP_HTML_Tag_Processor' ) ) {
+		if ( ! class_exists( \WP_HTML_Tag_Processor::class ) ) {
 			return $content;
 		}
 
