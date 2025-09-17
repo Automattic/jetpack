5141ebf0ecddde84524dc43515ab46e2d088afd6
Christian Gastrell <cgastrell@gmail.com>
2025-09-17 12:35:55 -0300

    change variation example to match the new content

--- projects/packages/forms/src/blocks/contact-form/variations.js
+++ projects/packages/forms/src/blocks/contact-form/variations.js
@@ -670,53 +670,12 @@ const variations = [
 					],
 				},
 				{
-					name: 'jetpack/field-radio',
-					attributes: { required: true },
+					name: 'jetpack/field-rating',
+					attributes: { required: true, max: 5 },
 					innerBlocks: [
 						{
 							name: 'jetpack/label',
-							attributes: { label: __( 'Please rate our website', 'jetpack-forms' ) },
-						},
-						{
-							name: 'jetpack/options',
-							attributes: { type: 'radio' },
-							innerBlocks: [
-								{
-									name: 'jetpack/option',
-									attributes: {
-										label: __( '1 - Very Bad', 'jetpack-forms' ),
-										placeholder: __( 'Add option…', 'jetpack-forms' ),
-									},
-								},
-								{
-									name: 'jetpack/option',
-									attributes: {
-										label: __( '2 - Poor', 'jetpack-forms' ),
-										placeholder: __( 'Add option…', 'jetpack-forms' ),
-									},
-								},
-								{
-									name: 'jetpack/option',
-									attributes: {
-										label: __( '3 - Average', 'jetpack-forms' ),
-										placeholder: __( 'Add option…', 'jetpack-forms' ),
-									},
-								},
-								{
-									name: 'jetpack/option',
-									attributes: {
-										label: __( '4 - Good', 'jetpack-forms' ),
-										placeholder: __( 'Add option…', 'jetpack-forms' ),
-									},
-								},
-								{
-									name: 'jetpack/option',
-									attributes: {
-										label: __( '5 - Excellent', 'jetpack-forms' ),
-										placeholder: __( 'Add option…', 'jetpack-forms' ),
-									},
-								},
-							],
+							attributes: { label: __( 'Rate your experience', 'jetpack-forms' ) },
 						},
 					],
 				},
