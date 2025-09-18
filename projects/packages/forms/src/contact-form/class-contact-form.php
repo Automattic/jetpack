a65b06204c65636f2314926ebaa2a20ea2e061db
William Viana <vianasw@gmail.com>
2025-09-18 16:14:51 +0200

    Forms: Add temp feedback scheduled deletion (#45178)
    * Add temp feedback scheduled deletion
    
    Just as we do with spam, I'm adding scheduled deletion to temp feedback
    posts. It's scheduled daily and deletes temp feedback posts older than
    one day. Since these it's meant to be temporary storage that's hidden
    from users—there's no need to keep it around for 15 days when it can
    be cleaned up after just 1 day.
    
    * changelog
    
    * Explain phpcs:ignore annotations reasons

--- projects/packages/forms/src/contact-form/class-contact-form.php
+++ projects/packages/forms/src/contact-form/class-contact-form.php
@@ -2074,6 +2074,11 @@ class Contact_Form extends Contact_Form_Shortcode {
 			wp_schedule_event( time() + 250, 'daily', 'grunion_scheduled_delete' );
 		}
 
+		// schedule deletes of old temp feedbacks
+		if ( ! wp_next_scheduled( 'grunion_scheduled_delete_temp' ) ) {
+			wp_schedule_event( time() + 250, 'daily', 'grunion_scheduled_delete_temp' );
+		}
+
 		if (
 			$is_spam !== true &&
 			/**
