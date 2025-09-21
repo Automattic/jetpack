d48d9e829e89906294e11a71a3b7975814cccfdc
Mukesh Kumar <hydtub@gmail.com>
2025-09-21 21:16:25 +0500

    Update advanced-cache.php

--- projects/plugins/super-cache/advanced-cache.php
+++ projects/plugins/super-cache/advanced-cache.php
@@ -1,29 +1,50 @@
 <?php
-// WP SUPER CACHE 1.2
+/**
+ * WP SUPER CACHE advanced-cache.php
+ *
+ * Provides early load functionality for WP Super Cache.
+ *
+ * @package WP_Super_Cache
+ * @since 1.2
+ */
+
+/**
+ * Display a message if WP Super Cache is installed but broken.
+ *
+ * @return void
+ */
 function wpcache_broken_message() {
 	global $wp_cache_config_file;
-	if ( isset( $wp_cache_config_file ) == false ) {
-		return '';
+
+	if ( empty( $wp_cache_config_file ) ) {
+		return;
 	}
 
 	$doing_ajax     = defined( 'DOING_AJAX' ) && DOING_AJAX;
 	$xmlrpc_request = defined( 'XMLRPC_REQUEST' ) && XMLRPC_REQUEST;
 	$rest_request   = defined( 'REST_REQUEST' ) && REST_REQUEST;
-	$robots_request = strpos( $_SERVER['REQUEST_URI'], 'robots.txt' ) != false;
+
+	$request_uri = isset( $_SERVER['REQUEST_URI'] )
+		? sanitize_text_field( wp_unslash( $_SERVER['REQUEST_URI'] ) )
+		: '';
+
+	$robots_request = str_contains( $request_uri, 'robots.txt' );
 
 	$skip_output = ( $doing_ajax || $xmlrpc_request || $rest_request || $robots_request );
-	if ( false == strpos( $_SERVER['REQUEST_URI'], 'wp-admin' ) && ! $skip_output ) {
+
+	if ( ! str_contains( $request_uri, 'wp-admin' ) && ! $skip_output ) {
 		echo '<!-- WP Super Cache is installed but broken. The constant WPCACHEHOME must be set in the file wp-config.php and point at the WP Super Cache plugin directory. -->';
 	}
 }
 
-if ( false == defined( 'WPCACHEHOME' ) ) {
+if ( ! defined( 'WPCACHEHOME' ) ) {
 	define( 'ADVANCEDCACHEPROBLEM', 1 );
-} elseif ( ! include_once WPCACHEHOME . 'wp-cache-phase1.php' ) {
-	if ( ! @is_file( WPCACHEHOME . 'wp-cache-phase1.php' ) ) {
+} elseif ( ! @include_once WPCACHEHOME . 'wp-cache-phase1.php' ) {
+	if ( ! is_file( WPCACHEHOME . 'wp-cache-phase1.php' ) ) {
 		define( 'ADVANCEDCACHEPROBLEM', 1 );
 	}
 }
+
 if ( defined( 'ADVANCEDCACHEPROBLEM' ) ) {
 	register_shutdown_function( 'wpcache_broken_message' );
 }
