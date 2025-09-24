d7c5b0cb3a760d3f95ba295aac3b1cc310a34041
Christian Gastrell <cgastrell@gmail.com>
2025-09-24 15:47:28 -0300

    add optional delayed execution if any of the element refs is missing

--- projects/packages/forms/src/modules/field-phone/view.js
+++ projects/packages/forms/src/modules/field-phone/view.js
@@ -4,6 +4,7 @@ import {
 	getConfig,
 	getElement,
 	withSyncEvent as originalWithSyncEvent,
+	withScope,
 } from '@wordpress/interactivity';
 import parsePhoneNumber, { AsYouType } from 'libphonenumber-js';
 import { countries } from '../../blocks/field-telephone/country-list';
@@ -36,7 +37,7 @@ const updateSelection = selectedCountry => {
 	} ) );
 };
 
-const { actions } = store( NAMESPACE, {
+const { actions, callbacks } = store( NAMESPACE, {
 	state: {
 		validators: {
 			phone: ( value, isRequired ) => {
@@ -233,6 +234,30 @@ const { actions } = store( NAMESPACE, {
 		initializePhoneFieldCustomComboBox() {
 			const context = getContext();
 			if ( ! context.showCountrySelector ) {
+				return;
+			}
+
+			if (
+				! phoneInputRefs[ context.fieldId ] ||
+				! searchInputRefs[ context.fieldId ] ||
+				! optionsListRefs[ context.fieldId ]
+			) {
+				const { ref } = getElement();
+				// delay execution with a timeout and scoping withScope and return.
+				setTimeout(
+					withScope( function () {
+						const context2 = getContext();
+						phoneInputRefs[ context2.fieldId ] = ref;
+						searchInputRefs[ context2.fieldId ] = ref.parentElement.querySelector(
+							'.jetpack-combobox-search'
+						);
+						optionsListRefs[ context2.fieldId ] = ref.parentElement.querySelector(
+							'.jetpack-combobox-options'
+						);
+						callbacks.initializePhoneFieldCustomComboBox();
+					} ),
+					100
+				);
 				return;
 			}
 			const config = getConfig( 'jetpack/field-phone' );
