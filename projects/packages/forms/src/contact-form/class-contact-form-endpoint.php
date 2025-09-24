ff69dc1e05e71edc70ecca9adc07acd017e9d3a1
Miguel Lezama <lezama@gmail.com>
2025-09-24 11:02:08 -0300

    remove unused params

--- projects/packages/forms/src/contact-form/class-contact-form-endpoint.php
+++ projects/packages/forms/src/contact-form/class-contact-form-endpoint.php
@@ -1204,11 +1204,6 @@ class Contact_Form_Endpoint extends \WP_REST_Posts_Controller {
 			array(
 				'action'       => 'feedback_export',
 				'feedback_ids' => implode( ',', $feedback_ids ),
-				'post_id'      => $post_id,
-				'search'       => $search,
-				'status'       => $status,
-				'before'       => $before,
-				'after'        => $after,
 				'nonce'        => $nonce,
 			),
 			admin_url( 'admin-post.php' )
