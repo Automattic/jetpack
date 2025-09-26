dceb0bdffede081646e9e116974484336c3c0816
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2025-09-26 15:26:22 +0200

    Only delete performance report URL if option exists

--- projects/plugins/wpcomsh/woa.php
+++ projects/plugins/wpcomsh/woa.php
@@ -219,13 +219,15 @@ function wpcomsh_woa_post_clone_clear_performance_profiler_data( $args, $assoc_a
 		return;
 	}
 
-	WP_CLI::runcommand(
-		'option delete wpcom_performance_report_url',
-		array(
-			'launch'     => false,
-			'exit_error' => false,
-		)
-	);
+	if ( get_option( 'wpcom_performance_report_url' ) ) {
+		WP_CLI::runcommand(
+			'option delete wpcom_performance_report_url',
+			array(
+				'launch'     => false,
+				'exit_error' => false,
+			)
+		);
+	}
 
 	$query   = "DELETE FROM wp_postmeta WHERE meta_key = '_wpcom_performance_report_url';";
 	$command = sprintf( 'db query "%s"', $query );
