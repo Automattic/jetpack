7699eed54bf31804746a6f05a32e70f780a4180b
Erick Danzer <edanzer@gmail.com>
2025-10-03 12:57:36 -0600

    Forms: add store for integrations

---
+++ projects/packages/forms/src/store/integrations/resolvers.ts
@@ -0,0 +1,26 @@
+import apiFetch from '@wordpress/api-fetch';
+import { addQueryArgs } from '@wordpress/url';
+import { INVALIDATE_INTEGRATIONS } from './action-types';
+import { receiveIntegrations, setIntegrationsError, setIntegrationsLoading } from './actions';
+import type { IntegrationsAction } from './types';
+import type { Integration } from '../../types';
+
+export const getIntegrations =
+	() =>
+	async ( { dispatch }: { dispatch: ( action: IntegrationsAction ) => void } ) => {
+		dispatch( setIntegrationsLoading( true ) );
+		try {
+			const path = addQueryArgs( '/wp/v2/feedback/integrations', { version: 2 } );
+			const result = await apiFetch< Integration[] >( { path } );
+			dispatch( receiveIntegrations( result ) );
+		} catch ( e ) {
+			const message = e instanceof Error ? e.message : 'Unknown error';
+			dispatch( setIntegrationsError( message ) );
+		} finally {
+			dispatch( setIntegrationsLoading( false ) );
+		}
+	};
+
+// Attach invalidation rule
+getIntegrations.shouldInvalidate = ( action: IntegrationsAction ) =>
+	action.type === INVALIDATE_INTEGRATIONS;
