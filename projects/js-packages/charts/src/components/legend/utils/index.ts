6f3d8c0242779e5907a857f9227fa4d741d2be64
annacmc <anna.mcphee@automattic.com>
2025-10-03 14:28:09 +1000

    Remove unused legend margin utilities and types

--- projects/js-packages/charts/src/components/legend/utils/index.ts
+++ projects/js-packages/charts/src/components/legend/utils/index.ts
@@ -1,4 +1,3 @@
 export { valueOrIdentity, valueOrIdentityString } from './value-or-identity';
 export { labelTransformFactory } from './label-transform-factory';
-export { processLegendMargin } from './process-legend-margin';
 export type { ValueOrIdentity } from './value-or-identity';
