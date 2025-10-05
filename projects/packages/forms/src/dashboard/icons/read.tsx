d0d75844313989d2959a72d6f27ad2ef0c6809ad
Christian Gastrell <cgastrell@gmail.com>
2025-10-06 01:24:37 +0300

    fine tune the envelope icons

--- projects/packages/forms/src/dashboard/icons/read.tsx
+++ projects/packages/forms/src/dashboard/icons/read.tsx
@@ -4,11 +4,8 @@
 import { Path, SVG } from '@wordpress/primitives';
 
 const read = (
-	<SVG width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<Path
-			d="M19.99 7C19.99 6.28 19.62 5.65 19.05 5.3L10 0L0.95 5.3C0.38 5.65 0 6.28 0 7V17C0 18.1 0.9 19 2 19H18C19.1 19 20 18.1 20 17L19.99 7ZM17.99 7V7.01L10 12L2 7L10 2.32L17.99 7ZM2 17V9.34L10 14.36L17.99 9.37L18 17H2Z"
-			fill="#323232"
-		/>
+	<SVG width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<Path d="M22 6C22 5.28 21.62 4.65 21 4.3L12 1L3 4.3C2.38 4.65 2 5.28 2 6V18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18L22 6ZM20 6V6.01L12 11L4 6L12 3.32L20 6ZM4 18V8.34L12 13.36L20 8.37L20 18H4Z" />
 	</SVG>
 );
 
--- projects/packages/forms/src/dashboard/icons/unread.tsx
+++ projects/packages/forms/src/dashboard/icons/unread.tsx
@@ -4,11 +4,8 @@
 import { Path, SVG } from '@wordpress/primitives';
 
 const unread = (
-	<SVG width="12" height="12" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<Path
-			d="M20 2C20 0.9 19.1 0 18 0H2C0.9 0 0 0.9 0 2V14C0 15.1 0.9 16 2 16H18C19.1 16 20 15.1 20 14V2ZM18 2L10 7L2 2H18ZM18 14H2V4L10 9L18 4V14Z"
-			fill="#323232"
-		/>
+	<SVG width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<Path d="M22 6C22 4.9 21.1 4 20 4H4C2.9 4 2 4.9 2 6V18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6ZM20 6L12 11L4 6H20ZM20 18H4V8L12 13L20 8V18Z" />
 	</SVG>
 );
 
