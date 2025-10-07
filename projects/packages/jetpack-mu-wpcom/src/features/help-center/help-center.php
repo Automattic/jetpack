4d9d2df3fd6d5c71ddd6b0ca73bcaa023cbada85
Omar Alshaker <omar@omaralshaker.com>
2025-10-07 22:40:30 +0200

    Fix help Center package header

--- projects/packages/jetpack-mu-wpcom/src/features/help-center/help-center.php
+++ projects/packages/jetpack-mu-wpcom/src/features/help-center/help-center.php
@@ -1,7 +1,9 @@
 <?php
 /**
- * Help center
- * This file is used to load the Help Center as a standalone plugin. It's not used by Jetpack.
+ * Plugin Name: Help Center
+ * Plugin URI: https://github.com/Automattic/jetpack/blob/trunk/projects/packages/jetpack-mu-wpcom/src/features/help-center
+ * Description: The Help Center is the main tool our customers use to reach for support.
+ * Text Domain: jetpack-mu-wpcom
  *
  * @package automattic/jetpack-mu-wpcom
  */
