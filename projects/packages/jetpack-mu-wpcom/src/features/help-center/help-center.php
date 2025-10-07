6dadf8842d059847a401c5ca34ad0014f6cc2606
Omar Alshaker <omar@omaralshaker.com>
2025-10-07 22:42:29 +0200

    Better code comment

--- projects/packages/jetpack-mu-wpcom/src/features/help-center/help-center.php
+++ projects/packages/jetpack-mu-wpcom/src/features/help-center/help-center.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: Help Center
+ * Plugin Name: Help Center. This file is used to load the Help Center as a standalone plugin. It's not used by Jetpack.
  * Plugin URI: https://github.com/Automattic/jetpack/blob/trunk/projects/packages/jetpack-mu-wpcom/src/features/help-center
  * Description: The Help Center is the main tool our customers use to reach for support.
  * Text Domain: jetpack-mu-wpcom
