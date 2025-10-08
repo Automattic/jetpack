5482254e6aa00df6c15778a2df92ecf027fc7cbb
annacmc <anna.mcphee@automattic.com>
2025-10-08 21:34:15 +1100

    Extract shared glyph renderers to line chart config

--- projects/js-packages/charts/src/components/line-chart/stories/config.tsx
+++ projects/js-packages/charts/src/components/line-chart/stories/config.tsx
@@ -27,7 +27,7 @@ export const glyphTheme = merge( jetpackTheme, {
 				key: props.key,
 				top: props.y,
 				left: props.x,
-				size: props.size * props.size,
+				size: props.size,
 				fill: props.color,
 			} ),
 		props =>
@@ -35,7 +35,7 @@ export const glyphTheme = merge( jetpackTheme, {
 				key: props.key,
 				top: props.y,
 				left: props.x,
-				size: props.size * props.size,
+				size: props.size,
 				fill: props.color,
 			} ),
 	],
@@ -45,6 +45,43 @@ export const glyphTheme = merge( jetpackTheme, {
 		},
 	},
 } );
+
+/**
+ * Shared glyph renderers for stories
+ */
+export const glyphRenderers = {
+	star: ( { color, size, x, y } ) => (
+		<GlyphStar top={ y } left={ x } size={ size } fill={ color } />
+	),
+	starOutline: ( { color, size, x, y } ) => (
+		<GlyphStar top={ y } left={ x } size={ size } fill="#fff" stroke={ color } />
+	),
+	heart: ( { color, size, x, y } ) => {
+		const hasXY = typeof x === 'number' && typeof y === 'number';
+		const groupProps = hasXY ? { transform: `translate(${ x }, ${ y })` } : {};
+		return (
+			<g { ...groupProps }>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width={ size * 2 }
+					height={ size * 2 }
+					viewBox="0 0 24 24"
+					style={ { overflow: 'visible', pointerEvents: 'none' } }
+				>
+					<path
+						d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
+						fill={ color }
+						stroke={ color }
+						strokeWidth="2"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						transform="translate(-12, -12)"
+					/>
+				</svg>
+			</g>
+		);
+	},
+};
 
 export const lineChartMetaArgs: Meta< StoryArgs > = {
 	title: 'JS Packages/Charts/Types/Line Chart',
