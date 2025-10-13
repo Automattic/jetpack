87c77236189b404754259aa60253ec2fd73a66aa
Jeremy Herve <jeremy@jeremy.hu>
2025-10-13 17:46:07 +0200

    Social Logos: add new Pocketcasts logo (#45438)
    Fixes #45402

--- projects/packages/classic-theme-helper/src/social-menu/icon-functions.php
+++ projects/packages/classic-theme-helper/src/social-menu/icon-functions.php
@@ -194,6 +194,7 @@ if ( ! function_exists( 'jetpack_social_menu_social_links_icons' ) ) {
 			'nextdoor.com'                      => 'nextdoor',
 			'patreon.com'                       => 'patreon',
 			'pinterest.'                        => 'pinterest',
+			'pocketcasts.com'                   => 'pocketcasts',
 			'podcasts.apple.com'                => 'apple-podcasts',
 			'getpocket.com'                     => 'pocket',
 			'ravelry.com'                       => 'ravelry',
