64534b514166740de3b829970a894cbb356503f6
Enej Bajgoric <enej.bajgoric@automattic.com>
2025-10-15 10:57:13 -0700

    remove comment

--- projects/packages/forms/src/dashboard/components/response-view/body.tsx
+++ projects/packages/forms/src/dashboard/components/response-view/body.tsx
@@ -30,8 +30,6 @@ import Gravatar from '../../components/gravatar';
 import { useMarkAsSpam } from '../../hooks/use-mark-as-spam';
 import { getPath, updateMenuCounter, updateMenuCounterOptimistically } from '../../inbox/utils';
 import type { FormResponse } from '../../../types';
-// Update the import path if the utils file is located elsewhere, for example:
-// Or, if the file does not exist, create 'utils.ts' in the correct folder with the required exports.
 
 const getDisplayName = response => {
 	const { author_name, author_email, author_url, ip } = response;
