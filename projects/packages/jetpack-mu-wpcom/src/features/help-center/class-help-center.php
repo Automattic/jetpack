a3c0dc83c358e02ecc77959b7bc1b8387f2f14a9
Omar Alshaker <omar@omaralshaker.com>
2025-10-21 13:03:41 +0200

    Debug

--- projects/packages/jetpack-mu-wpcom/src/features/help-center/class-help-center.php
+++ projects/packages/jetpack-mu-wpcom/src/features/help-center/class-help-center.php
@@ -64,27 +64,27 @@ class Help_Center {
 	 * @return array The preferences.
 	 */
 	public function calypso_preferences_update( $preferences ) {
-		$router_history = $preferences['help_center_router_history'];
-		$entries        = $router_history['entries'];
+		if ( isset( $preferences->help_center_router_history ) ) {
+			$router_history = &$preferences->help_center_router_history;
+			$entries        = &$router_history['entries'];
 
-		// Limit entries to 50 to prevent memory issues.
-		if ( count( $entries ) > 50 ) {
-			// Keep only the last 49 entries and add the root entry at the beginning.
-			$entries = array_slice( $entries, -49 );
-			// Keep the start at root so the back button always works.
-			array_unshift(
-				$entries,
-				array(
-					'pathname' => '/',
-					'search'   => '',
-					'hash'     => '',
-					'key'      => 'default',
-					'state'    => null,
-				)
-			);
-			// Update the entries and index in the router history.
-			$preferences['help_center_router_history']['entries'] = $entries;
-			$preferences['help_center_router_history']['index']   = 49;
+			// Limit entries to 50 to prevent spamming entries in the router history.
+			if ( count( $entries ) > 5 ) {
+				// Keep only the last 49 entries and add the root entry at the beginning.
+				$entries = array_slice( $entries, -4 );
+				// Keep the start at root so the back button always works.
+				array_unshift(
+					$entries,
+					array(
+						'pathname' => '/',
+						'search'   => '',
+						'hash'     => '',
+						'key'      => 'default',
+						'state'    => null,
+					)
+				);
+				$router_history['index'] = 4;
+			}
 		}
 		return $preferences;
 	}
