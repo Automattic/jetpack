4d240cb4f145c4489775194b5249b7fbc8c7bde2
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-10-24 14:42:40 -0600

    Skip CRM + PHP 8.5 in a different way

--- projects/plugins/crm/tests/action-skip-test-php.sh
+++ projects/plugins/crm/tests/action-skip-test-php.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Skip tests on PHP > 8.4 due to dependency constraints
+if php -r 'exit(PHP_VERSION_ID > 80499 ? 0 : 1);'; then
+	echo "Skipping tests on PHP > 8.4 (current: $(php -r 'echo PHP_VERSION;'))"
+	exit 3
+fi
+
 # Uncomment the below snippet to disable tests on WP trunk
 # if [[ "$WP_BRANCH" == 'trunk' ]]; then
 # 	echo "Codeception tests against WP trunk are temporarily disabled."
