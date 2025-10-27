52b696f9b0ad053f83a66faf0e03abe566dd274d
Dean Sas <dean.sas@automattic.com>
2025-10-27 09:39:54 +0000

    MU WPCOM: Implement a back end tracking for theme installs (#45608)
    Add tracking of wp-admin theme uploads
    
    A back-end Tracks `wpcom_theme_install` event is triggered whenever a theme zip is uploaded to a WoA site via wp-admin.
    
    These are the cases currently tracked:
    
    - Install theme via wp-admin UI (`type: 'install'`).
    - Upload a theme via wp-admin (new theme) (`type: 'upload'`).
    - Upload a theme via wp-admin (existing theme) (`type: 'update'`).
    - Update theme via update-core.php (`type: 'bulk-update'`).

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-themes/wpcom-theme-tracking.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-themes/wpcom-theme-tracking.php
@@ -63,6 +63,7 @@ function wpcom_themes_tracks_get_theme_props( $theme, $prefix = '' ) {
  * @param string   $new_theme_name New theme name.
  * @param WP_Theme $new_theme      New theme object.
  * @param WP_Theme $old_theme      Old theme object.
+ * @return void
  */
 function wpcom_themes_tracks_switch_theme( $new_theme_name, $new_theme, $old_theme ) {
 	$old_theme_props = wpcom_themes_tracks_get_theme_props( $old_theme, 'old' );
@@ -77,3 +78,68 @@ function wpcom_themes_tracks_switch_theme( $new_theme_name, $new_theme, $old_the
 	Common\wpcom_record_tracks_event( 'wpcom_theme_switch', $event_props );
 }
 add_action( 'switch_theme', 'wpcom_themes_tracks_switch_theme', 12, 3 );
+
+/**
+ * Record a theme install via wp-admin.
+ *
+ * @param Theme_Upgrader $upgrader   Upgrader instance.
+ * @param array          $hook_extra Upgrade data.
+ * @return void
+ */
+function wpcom_themes_tracks_install_theme( $upgrader, $hook_extra ) {
+	if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
+		// Simple sites can't install themes with wp-admin so should never reach here,
+		// but just in case, we exit early.
+		return;
+	}
+
+	if ( ! isset( $hook_extra['type'] ) || $hook_extra['type'] !== 'theme' || ! isset( $hook_extra['action'] ) ) {
+		return;
+	}
+
+	$theme_slug = $upgrader->result['destination_name'] ?? '';
+
+	if ( ! $theme_slug ) {
+		return;
+	}
+
+	$theme         = wp_get_theme( $theme_slug );
+	$theme_props   = wpcom_themes_tracks_get_theme_props( $theme );
+	$install_props = array(
+		'source' => '', // Intentionally left empty for debugging purposes.
+		'type'   => 'upload',
+	);
+
+	// Trying to distinguish between a manual upload vs a standard install or upgrade. It's not security sensitive.
+	// phpcs:disable WordPress.Security.NonceVerification.Recommended,WordPress.Security.ValidatedSanitizedInput.MissingUnslash,WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+	$action    = $_GET['action'] ?? '';
+	$overwrite = $_GET['overwrite'] ?? '';
+	// phpcs:enable WordPress.Security.NonceVerification.Recommended,WordPress.Security.ValidatedSanitizedInput.MissingUnslash,WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+
+	if ( $hook_extra['action'] === 'install' && ! $action ) {
+		// Install theme via wp-admin UI
+		$install_props['source'] = 'wp-admin';
+		$install_props['type']   = 'install';
+	} elseif ( $hook_extra['action'] === 'install' && $action === 'upload-theme' && ! $overwrite ) {
+		// Upload a theme via wp-admin (new theme)
+		$install_props['source'] = 'wp-admin';
+		$install_props['type']   = 'upload';
+	} elseif ( $hook_extra['action'] === 'install' && $action === 'upload-theme' && $overwrite === 'update-theme' ) {
+		// Upload a theme via wp-admin (existing theme)
+		$install_props['source'] = 'wp-admin';
+		$install_props['type']   = 'update';
+	} elseif ( $hook_extra['action'] === 'update' && $action === 'do-theme-upgrade' ) {
+		// Update theme via update-core.php
+		$install_props['source'] = 'wp-admin';
+		$install_props['type']   = 'bulk-update';
+	}
+
+	$event_props = array_merge(
+		array( 'blog_id' => get_wpcom_blog_id() ),
+		$theme_props,
+		$install_props
+	);
+
+	Common\wpcom_record_tracks_event( 'wpcom_theme_install', $event_props );
+}
+add_action( 'upgrader_process_complete', 'wpcom_themes_tracks_install_theme', 10, 2 );
