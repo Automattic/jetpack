b49816039c526f014cc8d665630abfe11cbc6131
Christian Gastrell <cgastrell@gmail.com>
2025-10-28 10:57:02 -0300

    prettify clsx call

--- projects/packages/forms/src/blocks/contact-form/edit.tsx
+++ projects/packages/forms/src/blocks/contact-form/edit.tsx
@@ -265,7 +265,10 @@ function JetpackContactFormEdit( {
 	const innerRef = useRef();
 	const blockProps = useBlockProps( {
 		ref: wrapperRef,
-		className: clsx( className, variationName === 'multistep' && 'is-multistep' ),
+		className: clsx( {
+			className,
+			'is-multistep': variationName === 'multistep',
+		} ),
 	} );
 	const formClassnames = clsx(
 		className,
