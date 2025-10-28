770787f8e4ef27bbe880bfcdf5aff2831e417f8a
Christian Gastrell <cgastrell@gmail.com>
2025-10-28 10:57:41 -0300

    fix comment for proper follow up

--- projects/packages/forms/src/contact-form/class-contact-form-field.php
+++ projects/packages/forms/src/contact-form/class-contact-form-field.php
@@ -1029,7 +1029,7 @@ class Contact_Form_Field extends Contact_Form_Shortcode {
 		// Strip 'class=' and quotes to get just the class names
 		$class_names = preg_replace( "/^class=['\"]([^'\"]*)['\"].*$/", '$1', $class );
 		// somehow we are getting the class jetpack-field__input-element on the wrong wrapper
-		// .jetpack-field__input-element is meant to style a div mocking an input element, not its wrapper.
+		// .jetpack-field__input-element is meant to be applied just to the input element, not its wrapper.
 		$class_names = preg_replace( '/jetpack-field__input-element /', '', $class_names );
 
 		$link_label_id = $id . '-number';
