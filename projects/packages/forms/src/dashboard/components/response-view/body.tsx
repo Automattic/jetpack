f6424925d4f3f3740968719cbc54b273ce1ea43f
William Viana <vianasw@gmail.com>
2025-10-29 22:24:28 +0100

    Move Browser info from div to table

--- projects/packages/forms/src/dashboard/components/response-view/body.tsx
+++ projects/packages/forms/src/dashboard/components/response-view/body.tsx
@@ -490,16 +490,14 @@ const ResponseViewBody = ( {
 									</Tooltip>
 								</td>
 							</tr>
+							{ response.browser && (
+								<tr>
+									<th>{ __( 'Browser:', 'jetpack-forms' ) }&nbsp;</th>
+									<td>{ response.browser }</td>
+								</tr>
+							) }
 						</tbody>
 					</table>
-					{ response.browser && (
-						<div className="jp-forms__inbox-response-meta-label">
-							<span className="jp-forms__inbox-response-meta-key	">
-								{ __( 'Browser:', 'jetpack-forms' ) }&nbsp;
-							</span>
-							<span className="jp-forms__inbox-response-meta-value">{ response.browser }</span>
-						</div>
-					) }
 				</div>
 
 				<div className="jp-forms__inbox-response-data">
