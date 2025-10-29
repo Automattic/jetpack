20cd664992b8bfb764e1bc732632d6620fdb7f53
Enej Bajgoric <enej.bajgoric@automattic.com>
2025-10-29 10:54:02 -0700

    Update variable name

--- projects/packages/forms/src/contact-form/class-contact-form.php
+++ projects/packages/forms/src/contact-form/class-contact-form.php
@@ -2077,11 +2077,11 @@ class Contact_Form extends Contact_Form_Shortcode {
 		);
 		$footer_ip = null;
 		if ( $comment_author_ip ) {
-			$comment_author_ip = $comment_author_ip . ' ' . $response->get_country_flag();
-			$footer_ip         = sprintf(
+			$comment_author_ip_with_flag = $comment_author_ip . ( $response->get_country_flag() ? ' ' . $response->get_country_flag() : '' );
+			$footer_ip                   = sprintf(
 				/* translators: Placeholder is the IP address of the person who submitted a form. */
 				esc_html__( 'IP Address: %1$s', 'jetpack-forms' ),
-				$comment_author_ip
+				$comment_author_ip_with_flag
 			) . '<br />';
 		}
 
