37a206a82cb254ef7ac7ad0f07def9cec249faf2
Mikael Korpela <mikael@ihminen.org>
2025-10-30 19:15:51 +0200

    Forms: fix tooltip in response IP link

--- projects/packages/forms/src/dashboard/components/response-view/body.tsx
+++ projects/packages/forms/src/dashboard/components/response-view/body.tsx
@@ -478,12 +478,12 @@ const ResponseViewBody = ( {
 							<tr>
 								<th>{ __( 'IP address:', 'jetpack-forms' ) }&nbsp;</th>
 								<td>
+									{ response.country_code && (
+										<span className="jp-forms__inbox-response-meta-country-flag response-country-flag">
+											{ getCountryFlagEmoji( response.country_code ) }
+										</span>
+									) }
 									<Tooltip text={ __( 'Lookup IP address', 'jetpack-forms' ) }>
-										{ response.country_code && (
-											<span className="jp-forms__inbox-response-meta-country-flag response-country-flag">
-												{ getCountryFlagEmoji( response.country_code ) }
-											</span>
-										) }
 										<ExternalLink href={ getRedirectUrl( 'ip-lookup', { path: response.ip } ) }>
 											{ response.ip }
 										</ExternalLink>
