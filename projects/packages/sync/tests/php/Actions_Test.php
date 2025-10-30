8b30462f135066cb6942f42ccc3dd4487f873d78
Foteini Giannaropoulou <giannaropoulou.foteini@gmail.com>
2025-10-30 08:11:57 +0200

    Update package unit tests

--- projects/packages/sync/tests/php/Actions_Test.php
+++ projects/packages/sync/tests/php/Actions_Test.php
@@ -22,6 +22,8 @@ class Actions_Test extends BaseTestCase {
 		Constants::set_constant( 'JETPACK_DISABLE_RAW_OPTIONS', true );
 		// Required for XML-RPC requests to work.
 		Constants::set_constant( 'JETPACK__API_BASE', 'https://public-api.wordpress.com' );
+		// Required for REST API requests to work.
+		Constants::set_constant( 'JETPACK__WPCOM_JSON_API_BASE', 'https://public-api.wordpress.com' );
 		// Mock Site level connection.
 		\Jetpack_Options::update_option( 'blog_token', 'blog_token.secret' );
 		\Jetpack_Options::update_option( 'id', 1 );
@@ -67,7 +69,7 @@ class Actions_Test extends BaseTestCase {
 		Settings::update_settings( array( 'dedicated_sync_enabled' => 1 ) );
 
 		add_filter( 'pre_http_request', array( $this, 'pre_http_request_set_dedicated_sync_header_off' ), 10, 3 );
-		Actions::send_data( array(), 'dummy', microtime(), 'sync', 0, 0 );
+		Actions::send_data( array( 1 ), 'dummy', microtime(), 'sync', 0, 0 );
 		remove_filter( 'pre_http_request', array( $this, 'pre_http_request_set_dedicated_sync_header_off' ) );
 
 		$this->assertFalse( Settings::is_dedicated_sync_enabled() );
@@ -78,7 +80,7 @@ class Actions_Test extends BaseTestCase {
 	 */
 	public function test_send_data_without_jetpack_dedicated_sync_enabled_response_header_off() {
 		add_filter( 'pre_http_request', array( $this, 'pre_http_request_set_dedicated_sync_header_off' ), 10, 3 );
-		Actions::send_data( array(), 'dummy', microtime(), 'sync', 0, 0 );
+		Actions::send_data( array( 1 ), 'dummy', microtime(), 'sync', 0, 0 );
 		remove_filter( 'pre_http_request', array( $this, 'pre_http_request_set_dedicated_sync_header_off' ) );
 
 		$this->assertFalse( Settings::is_dedicated_sync_enabled() );
@@ -91,7 +93,7 @@ class Actions_Test extends BaseTestCase {
 		set_transient( Dedicated_Sender::DEDICATED_SYNC_CHECK_TRANSIENT, Dedicated_Sender::DEDICATED_SYNC_VALIDATION_STRING, 100 );
 
 		add_filter( 'pre_http_request', array( $this, 'pre_http_request_set_dedicated_sync_header_on' ), 10, 3 );
-		Actions::send_data( array(), 'dummy', microtime(), 'sync', 0, 0 );
+		Actions::send_data( array( 1 ), 'dummy', microtime(), 'sync', 0, 0 );
 		remove_filter( 'pre_http_request', array( $this, 'pre_http_request_set_dedicated_sync_header_on' ) );
 
 		delete_transient( Dedicated_Sender::DEDICATED_SYNC_CHECK_TRANSIENT );
@@ -108,7 +110,7 @@ class Actions_Test extends BaseTestCase {
 		Settings::update_settings( array( 'dedicated_sync_enabled' => 1 ) );
 
 		add_filter( 'pre_http_request', array( $this, 'pre_http_request_set_dedicated_sync_header_on' ), 10, 3 );
-		Actions::send_data( array(), 'dummy', microtime(), 'sync', 0, 0 );
+		Actions::send_data( array( 1 ), 'dummy', microtime(), 'sync', 0, 0 );
 		remove_filter( 'pre_http_request', array( $this, 'pre_http_request_set_dedicated_sync_header_on' ) );
 
 		delete_transient( Dedicated_Sender::DEDICATED_SYNC_CHECK_TRANSIENT );
