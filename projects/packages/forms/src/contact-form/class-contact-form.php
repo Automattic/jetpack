ae4ab08f9198a3175c941e72173a4a5c4cc3bda0
William Viana <vianasw@gmail.com>
2025-11-04 14:01:47 +0100

    Add IP lookup link to IP email footer
    Add the lookup link the same way we have in the forms response detail
    view.

--- projects/packages/forms/src/contact-form/class-contact-form.php
+++ projects/packages/forms/src/contact-form/class-contact-form.php
@@ -2147,10 +2147,11 @@ class Contact_Form extends Contact_Form_Shortcode {
 		$footer_ip = null;
 		if ( $comment_author_ip ) {
 			$comment_author_ip_with_flag = $comment_author_ip . ( $response->get_country_flag() ? ' ' . $response->get_country_flag() : '' );
+			$ip_lookup_url               = sprintf( 'https://redirect.jetpack.com/?source=ip-lookup&path=%s', rawurlencode( $comment_author_ip ) );
 			$footer_ip                   = sprintf(
 				/* translators: Placeholder is the IP address of the person who submitted a form. */
 				esc_html__( 'IP Address: %1$s', 'jetpack-forms' ),
-				$comment_author_ip_with_flag
+				'<a href="' . esc_url( $ip_lookup_url ) . '">' . esc_html( $comment_author_ip_with_flag ) . '</a>'
 			);
 		}
 		$footer_browser = null;
