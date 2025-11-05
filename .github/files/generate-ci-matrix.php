ce0a74ccf6bd5c5120bb953adaa7f50518833c2d
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-11-05 08:58:49 -0700

    Fix indent

--- .github/files/generate-ci-matrix.php
+++ .github/files/generate-ci-matrix.php
@@ -82,11 +82,11 @@ foreach ( array( 'previous', 'trunk' ) as $wp ) {
 
 // todo: move to the main matrix once WP latest is compatible with PHP 8.5.
 $matrix[] = array(
-	'name'    => 'PHP tests: PHP 8.5 WP trunk',
-	'script'  => 'test-php',
-	'php'     => '8.5',
-	'wp'      => 'trunk',
-	'timeout' => 20,
+	'name'                => 'PHP tests: PHP 8.5 WP trunk',
+	'script'              => 'test-php',
+	'php'                 => '8.5',
+	'wp'                  => 'trunk',
+	'timeout'             => 20,
 	'force-package-tests' => true,
 );
 
