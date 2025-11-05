3763454f5a1265a9bf0e676aa4be474079bff821
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-11-05 07:57:49 -0700

    Oops, edited the wrong line

--- projects/plugins/jetpack/sal/class.json-api-links.php
+++ projects/plugins/jetpack/sal/class.json-api-links.php
@@ -329,7 +329,7 @@ class WPCOM_JSON_API_Links {
 		}
 
 		$endpoint_path_versions = $this->get_endpoint_path_versions();
-		$last_path_segment      = $this->get_last_segment_of_relative_path( $path ) ?? '';
+		$last_path_segment      = $this->get_last_segment_of_relative_path( $path );
 		$max_version_found      = null;
 
 		foreach ( $endpoint_path_versions as $endpoint_last_path_segment => $endpoints ) {
@@ -418,7 +418,7 @@ class WPCOM_JSON_API_Links {
 			list( $path, $min_version, $max_version ) = unserialize( $key );         // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.serialize_unserialize -- Legacy, see serialization at class.json-api.php.
 
 			// Grab the last component of the relative path to use as the top-level key.
-			$last_path_segment = $this->get_last_segment_of_relative_path( $path );
+			$last_path_segment = $this->get_last_segment_of_relative_path( $path ) ?? '';
 
 			$endpoint_path_versions[ $last_path_segment ][] = array(
 				'path'            => $path,
