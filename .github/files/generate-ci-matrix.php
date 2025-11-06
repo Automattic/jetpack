7951080e198762cbaa5a5e53f21e50eb2621d2a0
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-11-06 16:01:42 -0700

    CI: Update timeouts and comments (#45803)

--- .github/files/generate-ci-matrix.php
+++ .github/files/generate-ci-matrix.php
@@ -65,7 +65,7 @@ foreach ( array( '7.2', '7.3', '7.4', '8.0', '8.1', '8.2', '8.3', '8.4' ) as $ph
 		'script'  => 'test-php',
 		'php'     => $php,
 		'wp'      => 'latest',
-		'timeout' => 20, // 2024-11-12: Successful runs seem to take up to ~7 minutes.
+		'timeout' => 20, // 2025-11-06: Successful runs seem to take ~7 minutes.
 	);
 }
 
@@ -76,7 +76,7 @@ foreach ( array( 'previous', 'trunk' ) as $wp ) {
 		'script'  => 'test-php',
 		'php'     => $phpver,
 		'wp'      => $wp,
-		'timeout' => 15, // 2024-11-12: Successful runs seem to take ~7 minutes with PHP 8.2.
+		'timeout' => 15, // 2025-11-06: Successful runs seem to take ~7 minutes.
 	);
 }
 
@@ -86,7 +86,7 @@ $matrix[] = array(
 	'script'              => 'test-php',
 	'php'                 => '8.5',
 	'wp'                  => 'trunk',
-	'timeout'             => 20,
+	'timeout'             => 15, // 2025-11-06: Successful runs seem to take ~8 minutes.
 	'force-package-tests' => true,
 );
 
@@ -96,7 +96,7 @@ $matrix[] = array(
 	'script'           => 'test-php',
 	'php'              => '7.4',
 	'wp'               => 'latest',
-	'timeout'          => 20,
+	'timeout'          => 15, // 2025-11-06: Successful runs seem to take ~3 minutes.
 	'with-woocommerce' => true,
 );
 
@@ -106,7 +106,7 @@ $matrix[] = array(
 	'script'       => 'test-php',
 	'php'          => '8.1',
 	'wp'           => 'latest',
-	'timeout'      => 20,
+	'timeout'      => 15, // 2025-11-06: Successful runs seem to take ~7 minutes.
 	'with-wpcomsh' => true,
 );
 
@@ -114,7 +114,7 @@ $matrix[] = array(
 $matrix[] = array(
 	'name'    => 'JS tests',
 	'script'  => 'test-js',
-	'timeout' => 15, // 2024-11-12: Successful runs seem to take ~5 minutes.
+	'timeout' => 15, // 2025-11-06: Successful runs seem to take ~5 minutes.
 );
 
 // Add Coverage tests.
@@ -122,7 +122,7 @@ $matrix[] = array(
 	'name'    => 'Code coverage',
 	'script'  => 'test-coverage',
 	'wp'      => 'latest',
-	'timeout' => 40, // 2024-11-12: Successful runs seem to take ~14 minutes.
+	'timeout' => 40, // 2025-11-06: Successful runs seem to take ~15 minutes.
 );
 
 // END matrix definitions.
