e27b9bf1136ef901e7cb60876370f35a360af1d5
Foteini Giannaropoulou <giannaropoulou.foteini@gmail.com>
2025-11-11 12:02:36 +0200

    Sync: Increase default sync wait threshold to 15 seconds

--- projects/packages/sync/src/class-defaults.php
+++ projects/packages/sync/src/class-defaults.php
@@ -1120,7 +1120,7 @@ class Defaults {
 	 *
 	 * @var int Number of seconds.
 	 */
-	public static $default_sync_wait_threshold = 10;
+	public static $default_sync_wait_threshold = 15;
 
 	/**
 	 * Default wait between attempting to continue a full sync via requests.
