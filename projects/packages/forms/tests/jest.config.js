c6070e3387c1e6adada4b149f07c93b10f942299
Douglas <douglas.henri@automattic.com>
2025-11-17 18:09:04 -0300

    remove resolver hack

--- projects/packages/forms/tests/jest.config.js
+++ projects/packages/forms/tests/jest.config.js
@@ -9,9 +9,4 @@ export default {
 	...baseConfig,
 	rootDir: path.join( __dirname, '..' ),
 	setupFilesAfterEnv: [ ...baseConfig.setupFilesAfterEnv, '<rootDir>/tests/jest.setup.js' ],
-
-	// Hack so the ESM-only `@wordpress/interactivity` can be used with Jest in CommonJS mode.
-	// @todo Run Jest in ESM mode so this isn't needed.
-	transformIgnorePatterns: [ '/node_modules/(?!.pnpm/|@wordpress/interactivity/)' ],
-	resolver: require.resolve( './jest-resolver.js' ),
 };
