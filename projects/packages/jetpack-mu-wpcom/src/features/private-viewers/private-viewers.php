1b806f4d067844d7834ff119ed62cc6e3478e464
mmtr <1233880+mmtr@users.noreply.github.com>
2025-11-19 14:04:51 +0100

    Add menu that displays an empty page

--- projects/packages/jetpack-mu-wpcom/src/features/private-viewers/private-viewers.php
+++ projects/packages/jetpack-mu-wpcom/src/features/private-viewers/private-viewers.php
@@ -4,3 +4,29 @@
  *
  * @package automattic/jetpack-mu-wpcom
  */
+
+/**
+ * Register the Private Viewers submenu under Users.
+ */
+function wpcom_private_viewers_add_menu() {
+	add_submenu_page(
+		'users.php',
+		__( 'Private Viewers', 'jetpack-mu-wpcom' ),
+		__( 'Private Viewers', 'jetpack-mu-wpcom' ),
+		'list_users',
+		'wpcom-private-viewers',
+		'wpcom_private_viewers_display_page'
+	);
+}
+add_action( 'admin_menu', 'wpcom_private_viewers_add_menu' );
+
+/**
+ * Display the Private Viewers page.
+ */
+function wpcom_private_viewers_display_page() {
+	?>
+	<div class="wrap">
+		<h1><?php esc_html_e( 'Private Viewers', 'jetpack-mu-wpcom' ); ?></h1>
+	</div>
+	<?php
+}
