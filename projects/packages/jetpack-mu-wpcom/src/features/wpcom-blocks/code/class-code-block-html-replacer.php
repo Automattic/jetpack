Automattic/jetpack

commit e2383257bafd6924bd9d1db5ebcd07cb46069a7b

Code block: Enhance core instead of new block (#45389)
Update the code block to enhance the core/code block instead of providing an independent block.
Fix custom font-family support.

projects/packages/jetpack-mu-wpcom/src/features/wpcom-blocks/code/class-code-block-html-replacer.php
@@ -23,19 +23,53 @@ class Code_Block_HTML_Replacer extends WP_HTML_Processor {
 	 * with the tokenized HTML inserted. The HTML structure and replacement
 	 * contents are checked to ensure safety.
 	 *
-	 * @param string $html The tokenized code data.
-	 * @param array  $tokenized_code_data The tokenized code data.
+	 * @param string      $html The HTML string containing the code block.
+	 * @param array       $tokenized_code_data The tokenized code data.
+	 * @param string|null $language_name The language name, if any.
 	 * @return null|array{0: string, 1: string} Null on failure, or array with original code string
 	 *                                          and the tokenized HTML markup.
 	 */
-	public static function get_updated_html_with_replaced_content( string $html, array $tokenized_code_data ): ?array {
+	public static function get_updated_html_with_replaced_content( string $html, array $tokenized_code_data, ?string $language_name ): ?array {
 		$processor = self::create_fragment( $html );
 
-		// Find the location for insertion.
-		if ( ! $processor->next_tag( 'CODE' ) ) {
+		// Skip leading whitespace
+		while (
+			$processor->next_token()
+			&& $processor->get_token_type() === '#text'
+			&& $processor->text_node_classification === self::TEXT_IS_WHITESPACE
+		) {
+			continue;
+		}
+
+		// The serialized PRE tag has block wrapper attributes.
+		// Remove them, they'll be applied in a wrapper.
+		if ( $processor->get_tag() !== 'PRE' ) {
+			return null;
+		} else {
+			$processor->set_bookmark( 'pre_open' );
+		}
+
+		// The next token should be the CODE tag opener.
+		if ( ! $processor->next_token() || $processor->get_tag() !== 'CODE' ) {
 			return null;
 		}
-		$processor->set_bookmark( 'code_block_html_replace_start' );
+
+		if ( $language_name ) {
+			$processor->add_class(
+				\strtr(
+					\strtolower( $language_name ),
+					array(
+						' '  => '_',
+						"\t" => '_',
+						"\n" => '_',
+						"\r" => '_',
+						"\f" => '_',
+					)
+				)
+			);
+			$processor->get_updated_html();
+		}
+		$processor->set_bookmark( 'code_content_start' );
 
 		// The code should be 1 HTML CODE element containing the text.
 		// <code>### text ###</code>.
@@ -53,12 +87,13 @@ class Code_Block_HTML_Replacer extends WP_HTML_Processor {
 		) {
 			return null;
 		}
-		$processor->set_bookmark( 'code_block_html_replace_end' );
+		$processor->set_bookmark( 'code_content_end' );
 
 		// phpcs:ignore MediaWiki.Usage.ForbiddenFunctions.isset
 		if ( ! isset(
-			$processor->bookmarks['_code_block_html_replace_start'],
-			$processor->bookmarks['_code_block_html_replace_end']
+			$processor->bookmarks['_pre_open'],
+			$processor->bookmarks['_code_content_start'],
+			$processor->bookmarks['_code_content_end']
 		) ) {
 			return null;
 		}
@@ -68,10 +103,10 @@ class Code_Block_HTML_Replacer extends WP_HTML_Processor {
 			$replacement_code_html[] = '<div class="cm-line">';
 			foreach ( $line as $chunk ) {
 				if (
-					! is_array( $chunk ) ||
+					! \is_array( $chunk ) ||
 					! isset( $chunk[0] ) ||
-					! is_string( $chunk[0] ) ||
-					( isset( $chunk[1] ) && ! is_string( $chunk[1] ) )
+					! \is_string( $chunk[0] ) ||
+					( isset( $chunk[1] ) && ! \is_string( $chunk[1] ) )
 				) {
 					return null;
 				}
@@ -107,7 +142,7 @@ class Code_Block_HTML_Replacer extends WP_HTML_Processor {
 				if ( ! $class_name ) {
 					$replacement_code_html[] = $html_encoded_code;
 				} else {
-					$replacement_code_html[] = sprintf(
+					$replacement_code_html[] = \sprintf(
 						'<span class="%s">%s</span>',
 						esc_attr( $class_name ),
 						$html_encoded_code
@@ -117,18 +152,38 @@ class Code_Block_HTML_Replacer extends WP_HTML_Processor {
 			$replacement_code_html[] = '</div>';
 		}
 
-		// We'll start at the end of the CODE opener.
-		$bm_start = $processor->bookmarks['_code_block_html_replace_start'];
-		$bm_end   = $processor->bookmarks['_code_block_html_replace_end'];
-		$start    = $bm_start->start + $bm_start->length;
-		$length   = $bm_end->start - $start;
+		// Clear attributes from the PRE tag, replace everything inside the CODE block, trim the end.
+		$bm_pre_open = $processor->bookmarks['_pre_open'];
+		$bm_start    = $processor->bookmarks['_code_content_start'];
+		$bm_end      = $processor->bookmarks['_code_content_end'];
+		$start       = $bm_start->start + $bm_start->length;
+		$length      = $bm_end->start - $start;
 
+		// Remove all attributes from the PRE tag, rewrite it as a plain <pre>.
+		$processor->lexical_updates[] = new WP_HTML_Text_Replacement(
+			$bm_pre_open->start,
+			$bm_pre_open->length,
+			'<pre>'
+		);
 		$processor->lexical_updates[] = new WP_HTML_Text_Replacement(
 			$start,
 			$length,
 			implode( '', $replacement_code_html )
 		);
+		$processor->lexical_updates[] = new WP_HTML_Text_Replacement(
+			$bm_end->start + $bm_end->length,
+			// No need to calculate this precisely, just trim everything after this point.
+			\strlen( $processor->html ),
+			''
+		);
 
-		return array( $code_string, $processor->get_updated_html() );
+		// Normalize to ensure HTML that is safer to embed with other HTML.
+		// This ensures tags are correctly closed and extraneous close tags are not present.
+		$html = self::normalize( $processor->get_updated_html() );
+		if ( null === $html ) {
+			return null;
+		}
+
+		return array( $code_string, $html );
 	}
 }

pnpm-lock.yaml
@@ -2860,6 +2860,9 @@ importers:
       '@wordpress/private-apis':
         specifier: ^1.8.1
         version: 1.34.0
+      '@wordpress/rich-text':
+        specifier: 7.31.0
+        version: 7.31.0(react@18.3.1)
       '@wordpress/router':
         specifier: ^1.8.11
         version: 1.32.0(react@18.3.1)

projects/packages/jetpack-mu-wpcom/src/features/wpcom-blocks/code/block-definition/block-definition.tsx
@@ -12,9 +12,9 @@ import {
 	TextControl,
 	ToggleControl,
 } from '@wordpress/components';
+import { addFilter } from '@wordpress/hooks';
 import { __, sprintf } from '@wordpress/i18n';
 import * as React from 'react';
-import blockJson from '../common/block.json';
 import {
 	type Attributes,
 	BLOCK_NAME,
@@ -31,33 +31,12 @@ const {
 	__experimentalGetElementClassName,
 }: Window[ 'wp' ][ 'blockEditor' ] = wpBlockEditor;
 
-const { registerBlockType, registerBlockStyle }: Window[ 'wp' ][ 'blocks' ] = wpBlocks;
+const { registerBlockStyle }: Window[ 'wp' ][ 'blocks' ] = wpBlocks;
 
 const LINE_NUMBER_START_MIN = 0;
 const LINE_NUMBER_START_MAX = 10_000;
 
 type Props = EditBlockProps | SaveBlockProps;
-
-const exampleBlock = {
-	attributes: {
-		code: `// ✨ Code is poetry. ✨
-/**
- * Find the nth fibonacci number (inefficiently)
- */
-const fibonacci = ( n ) => n < 1 ? 0
-  : n > 3 ? 1
-  : fibonacci( n - 1 ) + fibonacci( n - 2 );`,
-		language: 'JavaScript',
-		languageConfidence: 'certain',
-		filename: 'example.js',
-	} satisfies Partial< Attributes >,
-};
-
-const icon = (
-	<svg width="24" height="24">
-		<path d="m8.53 7.531-4.293 4.277a.25.25 0 0 0 0 .353l4.294 4.31-1.062 1.058-4.294-4.31a1.75 1.75 0 0 1-.116-2.342l.12-.132L7.47 6.47 8.529 7.53ZM18.53 5.53l-1.292 1.292a.25.25 0 0 0 .001.354l3.582 3.57.12.131a1.75 1.75 0 0 1-.116 2.343l-4.294 4.31-1.062-1.06 4.294-4.309a.25.25 0 0 0 .031-.314l-.031-.04-3.582-3.569a1.75 1.75 0 0 1-.003-2.476L17.47 4.47l1.06 1.06Z" />
-	</svg>
-);
 
 const emptyLanguageOption = {
 	key: '',
@@ -89,130 +68,181 @@ const selectLanguageOptions: ReadonlyArray< {
 	label: name,
 } ) );
 
-registerBlockType( blockJson, {
-	icon,
-	example: exampleBlock,
-	transforms,
-	edit: withColors(
-		...( [
-			'colorComment',
-			'colorKeyword',
-			'colorBoolean',
-			'colorLiteral',
-			'colorString',
-			'colorSpecialString',
-			'colorMacroName',
-			'colorVariableDefinition',
-			'colorTypeName',
-			'colorClassName',
-			'colorInvalid',
-		] satisfies ReadonlyArray< `${ keyof Pick<
-			Attributes,
-			Extract< keyof Attributes, `color${ Capitalize< string > }` >
-		> }` > )
-	)( ( props: EditBlockProps ) => {
-		const { setAttributes, attributes } = props;
+/**
+ * Filter to enhance the core code block.
+ *
+ * @param settings - Block settings
+ * @return Modified settings.
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any -- No good interface available for this type now.
+function filterBlockRegistration( settings: any ) {
+	/*
+	 * The enhanced code block includes a "from" transform that handles things like language
+	 * name, line number settings, etc. Remove the "to" transform provided by syntaxhighlighter/code
+	 * so that simpler transform is not applied.
+	 */
+	if ( settings.name === 'syntaxhighlighter/code' ) {
+		if ( settings.transforms?.to ) {
+			settings.transforms.to = settings.transforms.to.filter(
+				( transform: { type: string; blocks?: unknown } ) =>
+					! (
+						transform.type === 'block' &&
+						Array.isArray( transform.blocks ) &&
+						transform.blocks.length === 1 &&
+						transform.blocks[ 0 ] === 'core/code'
+					)
+			);
+		}
+		return settings;
+	}
+	if ( settings.name !== 'core/code' ) {
+		return settings;
+	}
 
-		return (
-			<>
-				<InspectorControls group="color">
-					<ColorTools { ...props } />
-				</InspectorControls>
-				<InspectorControls>
-					<PanelBody title="Code Settings">
-						<SelectControl
-							label={ __( 'Language', 'jetpack-mu-wpcom' ) }
-							value={ attributes.language }
-							options={ selectLanguageOptions }
-							onChange={ ( newLanguage: string ) => {
-								setAttributes( {
-									language: newLanguage,
-									languageConfidence: 'certain',
-								} );
-							} }
-							__next40pxDefaultSize
-							__nextHasNoMarginBottom
-						/>
-						<ToggleControl
-							label={ __( 'Show Language Name', 'jetpack-mu-wpcom' ) }
-							checked={ attributes.showLanguageName }
-							onChange={ ( next: boolean ) =>
-								setAttributes( {
-									showLanguageName: next,
-								} )
+	settings.edit = blockEdit;
+	settings.save = blockSave;
+	if ( settings.example?.content ) {
+		settings.example.language = 'JavaScript';
+		settings.example.languageConfidence = 'certain';
+		settings.example.filename = 'example.js';
+	}
+
+	if ( ! settings.transforms ) {
+		settings.transforms = { from: transforms.from };
+	} else if ( ! settings.transforms.from ) {
+		settings.transforms.from = transforms.from;
+	} else {
+		settings.transforms.from.push( ...transforms.from );
+	}
+
+	return settings;
+}
+
+const blockEdit = withColors(
+	...( [
+		'colorComment',
+		'colorKeyword',
+		'colorBoolean',
+		'colorLiteral',
+		'colorString',
+		'colorSpecialString',
+		'colorMacroName',
+		'colorVariableDefinition',
+		'colorTypeName',
+		'colorClassName',
+		'colorInvalid',
+	] satisfies ReadonlyArray< `${ keyof Pick<
+		Attributes,
+		Extract< keyof Attributes, `color${ Capitalize< string > }` >
+	> }` > )
+)( ( props: EditBlockProps ) => {
+	const { setAttributes, attributes } = props;
+
+	return (
+		<>
+			<InspectorControls group="color">
+				<ColorTools { ...props } />
+			</InspectorControls>
+			<InspectorControls>
+				<PanelBody title="Code Settings">
+					<SelectControl
+						label={ __( 'Language', 'jetpack-mu-wpcom' ) }
+						value={ attributes.language }
+						options={ selectLanguageOptions }
+						onChange={ ( newLanguage: string ) => {
+							setAttributes( {
+								language: newLanguage,
+								languageConfidence: 'certain',
+							} );
+						} }
+						__next40pxDefaultSize
+						__nextHasNoMarginBottom
+					/>
+					<ToggleControl
+						label={ __( 'Show Language Name', 'jetpack-mu-wpcom' ) }
+						checked={ attributes.showLanguageName }
+						onChange={ ( next: boolean ) =>
+							setAttributes( {
+								showLanguageName: next,
+							} )
+						}
+						__nextHasNoMarginBottom
+					/>
+					<ToggleControl
+						label={ __( 'Show Copy Button', 'jetpack-mu-wpcom' ) }
+						checked={ attributes.showCopyButton }
+						onChange={ ( next: boolean ) =>
+							setAttributes( {
+								showCopyButton: next,
+							} )
+						}
+						__nextHasNoMarginBottom
+					/>
+					<ToggleControl
+						label={ __( 'Show Line Numbers', 'jetpack-mu-wpcom' ) }
+						checked={ attributes.showLineNumbers }
+						onChange={ ( next: boolean ) =>
+							setAttributes( {
+								showLineNumbers: next,
+							} )
+						}
+						__nextHasNoMarginBottom
+					/>
+					<TextControl
+						label={ __( 'Line Numbers Start At', 'jetpack-mu-wpcom' ) }
+						type="number"
+						value={ attributes.lineNumbersStartAt }
+						disabled={ ! attributes.showLineNumbers }
+						onChange={ ( _nextLineNumbersStartAt: string ) => {
+							let nextLineNumbersStartAt = Number( _nextLineNumbersStartAt );
+
+							if ( ! Number.isFinite( nextLineNumbersStartAt ) ) {
+								nextLineNumbersStartAt = 1;
 							}
-							__nextHasNoMarginBottom
-						/>
-						<ToggleControl
-							label={ __( 'Show Copy Button', 'jetpack-mu-wpcom' ) }
-							checked={ attributes.showCopyButton }
-							onChange={ ( next: boolean ) =>
-								setAttributes( {
-									showCopyButton: next,
-								} )
+							if ( ! Number.isInteger( nextLineNumbersStartAt ) ) {
+								nextLineNumbersStartAt = 1;
 							}
-							__nextHasNoMarginBottom
-						/>
-						<ToggleControl
-							label={ __( 'Show Line Numbers', 'jetpack-mu-wpcom' ) }
-							checked={ attributes.showLineNumbers }
-							onChange={ ( next: boolean ) =>
-								setAttributes( {
-									showLineNumbers: next,
-								} )
-							}
-							__nextHasNoMarginBottom
-						/>
-						<TextControl
-							label={ __( 'Line Numbers Start At', 'jetpack-mu-wpcom' ) }
-							type="number"
-							value={ attributes.lineNumbersStartAt }
-							disabled={ ! attributes.showLineNumbers }
-							onChange={ ( _nextLineNumbersStartAt: string ) => {
-								let nextLineNumbersStartAt = Number( _nextLineNumbersStartAt );
 
-								if ( ! Number.isFinite( nextLineNumbersStartAt ) ) {
-									nextLineNumbersStartAt = 1;
-								}
-								if ( ! Number.isInteger( nextLineNumbersStartAt ) ) {
-									nextLineNumbersStartAt = 1;
-								}
+							// Clamp to the allowed range
+							nextLineNumbersStartAt = Math.max(
+								LINE_NUMBER_START_MIN,
+								Math.min( LINE_NUMBER_START_MAX, nextLineNumbersStartAt )
+							);
 
-								// Clamp to the allowed range
-								nextLineNumbersStartAt = Math.max(
-									LINE_NUMBER_START_MIN,
-									Math.min( LINE_NUMBER_START_MAX, nextLineNumbersStartAt )
-								);
-
-								setAttributes( {
-									lineNumbersStartAt: nextLineNumbersStartAt,
-								} );
-							} }
-							min={ LINE_NUMBER_START_MIN }
-							max={ LINE_NUMBER_START_MAX }
-							__next40pxDefaultSize
-							__nextHasNoMarginBottom
-						/>
-					</PanelBody>
-				</InspectorControls>
-				<React.Suspense fallback={ <Loading { ...props } /> }>
-					<Chrome { ...props }>
-						<EditCodeMirror { ...props } />
-						<Notice status="warning" isDismissible={ false }>
-							<b>Caution!</b> This block is experimental and <em>will</em> change. Existing content
-							may break.
-						</Notice>
-					</Chrome>
-				</React.Suspense>
-			</>
-		);
-	} ),
-
-	save: ( props: SaveBlockProps ) => {
-		const { code } = props.attributes;
-		return <CodeWrapper { ...props }>{ htmlEncode( code ) }</CodeWrapper>;
-	},
+							setAttributes( {
+								lineNumbersStartAt: nextLineNumbersStartAt,
+							} );
+						} }
+						min={ LINE_NUMBER_START_MIN }
+						max={ LINE_NUMBER_START_MAX }
+						__next40pxDefaultSize
+						__nextHasNoMarginBottom
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<React.Suspense fallback={ <Loading { ...props } /> }>
+				<Chrome { ...props }>
+					<EditCodeMirror { ...props } />
+					<Notice status="warning" isDismissible={ false }>
+						<b>Caution!</b> This block is experimental and <em>will</em> change. Existing content
+						may break.
+					</Notice>
+				</Chrome>
+			</React.Suspense>
+		</>
+	);
 } );
+
+const blockSave = ( props: SaveBlockProps ) => {
+	const {
+		content: { text: code },
+	} = props.attributes;
+	return (
+		<CodeWrapper wrapperProps={ useBlockProps.save() } { ...props }>
+			{ htmlEncode( code ) }
+		</CodeWrapper>
+	);
+};
 
 registerBlockStyle( BLOCK_NAME, {
 	name: 'no-highlight',
@@ -370,7 +400,7 @@ const DisplayLanguage = ( props: Props ) => {
  * @return Loading state UI.
  */
 function Loading( props: EditBlockProps ): React.JSX.Element {
-	let code = props.attributes.code;
+	let code = props.attributes.content.text;
 	if ( ! code ) {
 		code = __( 'Loading…', 'jetpack-mu-wpcom' );
 	}
@@ -388,34 +418,26 @@ function Loading( props: EditBlockProps ): React.JSX.Element {
 /**
  * This function wraps the code content when it is not managed by CodeMirror.
  *
- * @param props            - Component props.
- * @param props.attributes -- Block attributes.
- * @param props.children   -- Component children, the contents of the block.
+ * @param props              - Component props.
+ * @param props.children     - Component children, the contents of the block.
+ * @param props.wrapperProps - Props to pass to the PRE container element.
  *
  * @return UI.
  */
 function CodeWrapper( {
-	attributes: { language },
 	children: code,
+	wrapperProps,
 }: {
 	children: string;
-	attributes: Pick< EditBlockProps[ 'attributes' ], 'language' >;
+	wrapperProps?: React.HTMLAttributes< HTMLPreElement >;
 } ): React.JSX.Element {
 	if ( code.endsWith( '\n' ) ) {
 		code += '\n';
 	}
 
 	return (
-		<pre className="cm-content">
-			<code
-				className={
-					language
-						? `language-${ language.toLowerCase().replaceAll( /[ \t\n\r\f]/g, '_' ) }`
-						: undefined
-				}
-			>
-				{ code }
-			</code>
+		<pre { ...wrapperProps }>
+			<code>{ code }</code>
 		</pre>
 	);
 }
@@ -520,3 +542,5 @@ const EditCodeMirror = React.lazy(
 	// eslint-disable-next-line import/no-unresolved -- The feature registers this module for import.
 	() => import( /* webpackIgnore: true */ '@a8cCodeBlock/block-edit-function' )
 );
+
+addFilter( 'blocks.registerBlockType', 'jetpack/enhance-core-code-block', filterBlockRegistration );

projects/packages/jetpack-mu-wpcom/src/features/wpcom-blocks/code/block-definition/transforms.ts
@@ -4,6 +4,7 @@ import { extensionToLang } from '@@codemirrorLanguageData@@';
 import * as wpBlocks from '@wordpress/blocks';
 import { dispatch } from '@wordpress/data';
 import { store as editorStore } from '@wordpress/editor';
+import { RichTextData } from '@wordpress/rich-text';
 import { type Attributes, BLOCK_NAME } from '../common/block.ts';
 
 const { createBlock }: Window[ 'wp' ][ 'blocks' ] = wpBlocks;
@@ -11,22 +12,8 @@ const { createBlock }: Window[ 'wp' ][ 'blocks' ] = wpBlocks;
 const CODE_FENCE_REGEXP = /^```([a-z0-9+-]*)$/i;
 
 export const transforms = {
-	to: [
-		{
-			type: 'block',
-			blocks: [ 'core/code' ],
-			transform: ( attrs: Attributes ) => {
-				const code = attrs.code;
-				const div = document.createElement( 'div' );
-				div.innerText = code;
-				const content = div.innerHTML;
-				return createBlock( 'core/code', { content } );
-			},
-		},
-	],
-
 	from: [
-		// Handle GH-like code fence openers, e.g. ```js
+		// Handle code fence openers, e.g. ```js
 		{
 			type: 'enter',
 			priority: 5,
@@ -59,16 +46,6 @@ export const transforms = {
 
 		{
 			type: 'block',
-			blocks: [ 'core/code' ],
-			transform: ( { content }: { content: { text: string } } ) => {
-				return createBlock< Attributes >( BLOCK_NAME, {
-					code: content.text,
-				} );
-			},
-		},
-
-		{
-			type: 'block',
 			blocks: [ 'kevinbatdorf/code-block-pro' ],
 			transform: ( {
 				code,
@@ -81,7 +58,7 @@ export const transforms = {
 				language?: string;
 			} ) => {
 				const blockAttributes: Partial< Attributes > = {
-					code: code,
+					content: RichTextData.fromPlainText( code ),
 				};
 				if ( attributes.lineNumbers === true ) {
 					blockAttributes.showLineNumbers = true;
@@ -113,10 +90,11 @@ export const transforms = {
 			}: {
 				content?: string;
 				language?: string;
+				lineNumbers?: boolean;
 				firstLineNumber?: string;
 			} ) => {
 				const blockAttributes: Partial< Attributes > = {
-					code: content,
+					content: RichTextData.fromPlainText( content ),
 				};
 
 				/*
@@ -135,8 +113,12 @@ export const transforms = {
 					blockAttributes.languageConfidence = 'certain';
 				}
 
-				if ( attributes.firstLineNumber ) {
-					blockAttributes.lineNumbersStartAt = Number( attributes.firstLineNumber );
+				if ( attributes.lineNumbers !== false ) {
+					blockAttributes.showLineNumbers = true;
+
+					if ( attributes.firstLineNumber ) {
+						blockAttributes.lineNumbersStartAt = Number( attributes.firstLineNumber );
+					}
 				}
 
 				return createBlock< Attributes >( BLOCK_NAME, blockAttributes );
@@ -162,22 +144,29 @@ export const transforms = {
 				const [ file ] = files;
 				const language = getLanguageFromFile( file )!;
 
-				const block = createBlock< Attributes >( BLOCK_NAME, {
+				const reader = new FileReader();
+				reader.readAsText( file );
+
+				const blockAttributes: Partial< Attributes > = {
 					language,
 					languageConfidence: 'certain',
 					filename: file.name,
-				} );
+				};
 
-				const reader = new FileReader();
-				reader.addEventListener( 'load', event => {
-					const code = event.target?.result as string;
+				if ( reader.readyState === FileReader.DONE ) {
+					if ( ! reader.error ) {
+						blockAttributes.content = RichTextData.fromPlainText( reader.result as string );
+					}
+					return createBlock< Attributes >( BLOCK_NAME, blockAttributes );
+				}
+
+				const block = createBlock< Attributes >( BLOCK_NAME, blockAttributes );
+				reader.addEventListener( 'load', () => {
 					dispatch( editorStore ).updateBlockAttributes( [ block.clientId ], {
-						code,
+						content: RichTextData.fromPlainText( reader.result as string ),
 						triggerCodeUpdate: true,
 					} );
 				} );
-				reader.readAsText( file );
-
 				return block;
 			},
 		},

projects/packages/jetpack-mu-wpcom/src/features/wpcom-blocks/code/block-edit-function/block-edit-function.tsx
@@ -13,7 +13,7 @@ import { toBase64 } from '../common/base-64.ts';
 import { LanguageData } from './codemirror-language-data.ts';
 import * as languageUtils from './language-utils';
 import { WorkerAdmin } from './worker-admin.ts';
-import type { EditBlockProps } from '../common/block.ts';
+import type { Attributes, EditBlockProps } from '../common/block.ts';
 import type { LanguageSupport } from '@codemirror/language';
 import type { Extension } from '@codemirror/state';
 
@@ -23,6 +23,7 @@ const { createBlock, getDefaultBlockName } = window.wp.blocks;
 const { useDispatch, useSelect } = window.wp.data;
 const { __ } = window.wp.i18n;
 const { isKeyboardEvent, BACKSPACE: KEY_CODE_BACKSPACE } = window.wp.keycodes;
+const { RichTextData } = window.wp.richText;
 
 /**
  * The Edit function with CodeMirror available.
@@ -83,10 +84,10 @@ function EditCodeMirror( props: EditBlockProps ) {
 			changes: {
 				from: 0,
 				to: viewRef.current.state.doc.length,
-				insert: attributes.code,
+				insert: attributes.content.text,
 			},
 		} );
-	}, [ attributes.code, attributes.triggerCodeUpdate, setAttributes ] );
+	}, [ attributes.content, attributes.triggerCodeUpdate, setAttributes ] );
 
 	/**
 	 * Attempts to infer the language inside the code block.
@@ -145,7 +146,7 @@ function EditCodeMirror( props: EditBlockProps ) {
 		( code: string ) => {
 			const tree = currentLanguageRef.current?.language.parser.parse( code ) ?? null;
 
-			let currentLine: Array< [ string, string ] | [ string ] > = [];
+			let currentLine: Array< Attributes[ 'tokenizedLines' ][ number ][ number ] > = [];
 			const lines: Array< typeof currentLine > = [];
 
 			if ( tree !== null ) {
@@ -177,7 +178,7 @@ function EditCodeMirror( props: EditBlockProps ) {
 
 			guessLanguage( code );
 			setAttributes( {
-				code,
+				content: RichTextData.fromPlainText( code ),
 				tokenizedLines: lines,
 			} );
 		},
@@ -339,7 +340,7 @@ function EditCodeMirror( props: EditBlockProps ) {
 			}
 
 			viewRef.current = new View.EditorView( {
-				doc: attributes.code,
+				doc: attributes.content?.text ?? '',
 				extensions: makeExtensions(),
 				parent: ref.current,
 			} );

commit 5d2a79c3347efb2f4c182f46b24d43242ad17653

jetpack-mu-wpcom Code Block: Restore language- class prefix (#46010)
Fix a small regression from #45389 where the language- class name prefix was unintentionally omitted.

projects/packages/jetpack-mu-wpcom/src/features/wpcom-blocks/code/class-code-block-html-replacer.php
@@ -56,6 +56,7 @@ class Code_Block_HTML_Replacer extends WP_HTML_Processor {
 
 		if ( $language_name ) {
 			$processor->add_class(
+				'language-' .
 				\strtr(
 					\strtolower( $language_name ),
 					array(