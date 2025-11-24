Automattic/jetpack

commit 2d24fc42a5a306145d6b54e9b1e08f5815908135

A4A Client: Only show disconnect dialog during plugin deactivation when connected (#37337)
* Conditionally add the disconnect on deactivation handler

* changelog

projects/plugins/automattic-for-agencies-client/src/class-automattic-for-agencies-client.php
@@ -37,7 +37,10 @@ class Automattic_For_Agencies_Client {
 		add_action( 'load-settings_page_' . AUTOMATTIC_FOR_AGENCIES_CLIENT_SLUG, array( static::class, 'load_scripts_styles' ) );
 
 		// Display a modal when trying to deactivate the plugin.
-		Deactivation_Handler::init( AUTOMATTIC_FOR_AGENCIES_CLIENT_SLUG, __DIR__ . '/admin/deactivation-dialog.php' );
+		$manager = new Connection_Manager( 'automattic-for-agencies-client' );
+		if ( $manager->is_connected() ) {
+			Deactivation_Handler::init( AUTOMATTIC_FOR_AGENCIES_CLIENT_SLUG, __DIR__ . '/admin/deactivation-dialog.php' );
+		}
 	}
 
 	/**

commit fbbb0459a354eb00da13d3369345f16b6fbc20b2

A4A Client: defer connection check to plugins_loaded hook
Move the connection check from init() to plugins_loaded hook to prevent
fatal errors when WordPress core functions (like get_user_by) are not
yet available. This fixes an issue introduced in PR #37337 where
the connection check ran too early in the plugin initialization.

Fixes a bug where the connection check would run before WordPress core
functions are loaded, causing fatal errors in environments like Atomic
where get_user_by() is used by the connection check.

projects/plugins/automattic-for-agencies-client/src/class-automattic-for-agencies-client.php
@@ -37,6 +37,19 @@ class Automattic_For_Agencies_Client {
 		add_action( 'load-settings_page_' . AUTOMATTIC_FOR_AGENCIES_CLIENT_SLUG, array( static::class, 'load_scripts_styles' ) );
 
 		// Display a modal when trying to deactivate the plugin.
+		// Hook to plugins_loaded at priority 20 to ensure connection package is initialized
+		// and WordPress core functions (like get_user_by) are available.
+		add_action( 'plugins_loaded', array( static::class, 'init_deactivation_handler' ), 20 );
+	}
+
+	/**
+	 * Initialize the deactivation handler if the site is connected.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return void
+	 */
+	public static function init_deactivation_handler() {
 		$manager = new Connection_Manager( 'automattic-for-agencies-client' );
 		if ( $manager->is_connected() ) {
 			Deactivation_Handler::init( AUTOMATTIC_FOR_AGENCIES_CLIENT_SLUG, __DIR__ . '/admin/deactivation-dialog.php' );