edb3f2bee7dee3ca361d6dfe9f1982b4b76ee0bd
Mikael Korpela <mikael@ihminen.org>
2025-11-27 12:58:28 +0200

    Handle field

--- projects/packages/forms/src/dashboard/components/response-view/field-email/index.tsx
+++ projects/packages/forms/src/dashboard/components/response-view/field-email/index.tsx
@@ -4,9 +4,19 @@ import {
 import CopyClipboardButton from '../../../components/copy-clipboard-button';
 
 const FieldEmail = ( { email } ) => {
+	const emailParts = email.split( '@' );
+
 	return (
-		<HStack alignment="center" justify="left" spacing="2">
-			<a href={ `mailto:${ email }` }>{ email }</a>
+		<HStack
+			alignment="center"
+			className="jp-forms__inbox-response-field-email"
+			justify="left"
+			spacing="2"
+		>
+			<a href={ `mailto:${ email }` }>
+				{ emailParts[ 0 ] }
+				<wbr />@{ emailParts[ 1 ] }
+			</a>
 			<CopyClipboardButton text={ email } />
 		</HStack>
 	);
