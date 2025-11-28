e24d9f111c9bc346296cab1616341658fb124a99
Enej Bajgoric <enej.bajgoric@automattic.com>
2025-11-28 13:25:46 -0800

    Forms: fix dashboard bubble overlap (#46132)
    * Forms: Add top padding to inbox actions bar
    
    Introduces padding-block-start to .jp-forms-view-actions for improved spacing in the dashboard inbox styles.
    
    * changelog
    
    * Implements a mobile first css design.

--- projects/packages/forms/src/dashboard/inbox/stage/index.js
+++ projects/packages/forms/src/dashboard/inbox/stage/index.js
@@ -524,7 +524,7 @@ export default function InboxView() {
 						style={ {
 							display: 'flex',
 							gap: '8px',
-							justifyContent: containerWidth < 600 ? 'space-between' : 'flex-end',
+							justifyContent: containerWidth < 600 ? 'unset' : 'flex-end',
 						} }
 					>
 						<DataViews.Search />
