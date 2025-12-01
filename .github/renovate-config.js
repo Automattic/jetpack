5eaee4e67cc3a7d4c38cf78063ad5a318bf3472f
Brad Jorsch <brad.jorsch@automattic.com>
2025-12-01 14:17:09 -0500

    Add minimumReleaseAge configuration

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -29,6 +29,19 @@ const versions = Object.fromEntries(
 	)
 );
 
+// We don't have a yaml parser when this is being processed, so hack it with regexes instead.
+const workspaceYaml = fs.readFileSync( path.join( monorepoBase, 'pnpm-workspace.yaml' ), 'utf8' );
+const minimumReleaseAge = workspaceYaml.match( /^minimumReleaseAge: (\d+)$/m )[ 0 ];
+const minimumReleaseAgeExcludes = [];
+for ( const exclude of
+	 // prettier-ignore
+	 workspaceYaml
+		.match( /^minimumReleaseAgeExclude:\n(\n|[ \t][^\n]+\n)+/m )[ 0 ]
+		.matchAll( /^[ \t]+- \x27?([^\x27\n]+)\x27?$/gm )
+) {
+	minimumReleaseAgeExcludes.push( exclude[ 1 ] );
+}
+
 module.exports = {
 	branchPrefix: 'renovate/',
 	allowPlugins: true,
@@ -37,6 +50,7 @@ module.exports = {
 	gitAuthor: 'Renovate Bot (self-hosted) <bot@renovateapp.com>',
 	platform: 'github',
 	repositories: [ 'Automattic/jetpack' ],
+	minimumReleaseAge: `${ minimumReleaseAge } minutes`,
 
 	// Extra code to run before creating a commit.
 	allowedCommands: [ monorepoBase + '.github/files/renovate-post-upgrade-run.sh' ],
@@ -80,6 +94,12 @@ module.exports = {
 				return monorepoPackages.sort();
 			} )(),
 			enabled: false,
+		},
+		// Minimum release age exclusions from pnpm-workspace.yaml.
+		{
+			matchDatasources: [ 'npm' ],
+			matchPackageNames: minimumReleaseAgeExcludes,
+			minimumReleaseAge: null,
 		},
 		// PHP non-dev deps need to work with the oldest PHP versions we support.
 		{
