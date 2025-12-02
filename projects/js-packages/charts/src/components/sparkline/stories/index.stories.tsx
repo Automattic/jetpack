fc2bf78c5eebc84c32e28216b32cbf58ea523d70
annacmc <anna.mcphee@automattic.com>
2025-12-02 22:13:59 +1100

    Update Storybook title to Sparkline Chart for consistency

--- projects/js-packages/charts/src/components/sparkline/stories/index.stories.tsx
+++ projects/js-packages/charts/src/components/sparkline/stories/index.stories.tsx
@@ -3,7 +3,7 @@ import type { SparklineProps } from '../types';
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta< SparklineProps > = {
-	title: 'JS Packages/Charts/Types/Sparkline',
+	title: 'JS Packages/Charts/Types/Sparkline Chart',
 	component: Sparkline,
 	parameters: {
 		layout: 'padded',
