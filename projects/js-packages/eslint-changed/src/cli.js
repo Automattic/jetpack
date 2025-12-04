675d25dfa380fb6a6214df96b61faaa2aec0abac
Brad Jorsch <brad.jorsch@automattic.com>
2025-12-04 11:00:03 -0500

    Release eslint-changed 2.1.5

--- projects/js-packages/eslint-changed/src/cli.js
+++ projects/js-packages/eslint-changed/src/cli.js
@@ -7,7 +7,7 @@ import { Command } from 'commander';
 import * as ESLintPkg from 'eslint';
 import parseDiff from 'parse-diff';
 
-const APP_VERSION = '2.1.4';
+const APP_VERSION = '2.1.5';
 
 const { ESLint } = ESLintPkg;
 const loadESLint = ESLintPkg.loadESLint ?? ( () => ESLint );
