c94d8ed2a282817523141badf1f16b785cfcdb3d
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2025-12-09 11:04:42 +1300

    Update main index to use new directory structure
    Update src/index.ts to import chart components from primitives/
    instead of components/. Legend and tooltip remain in components/.

--- projects/js-packages/charts/src/index.ts
+++ projects/js-packages/charts/src/index.ts
@@ -1,16 +1,16 @@
-// Charts
-export { BarChart, BarChartUnresponsive } from './components/bar-chart';
-export { LineChart, LineChartUnresponsive } from './components/line-chart';
-export { PieChart, PieChartUnresponsive } from './components/pie-chart';
+// Charts (Primitives)
+export { BarChart, BarChartUnresponsive } from './primitives/bar-chart';
+export { LineChart, LineChartUnresponsive } from './primitives/line-chart';
+export { PieChart, PieChartUnresponsive } from './primitives/pie-chart';
 export {
 	PieSemiCircleChart,
 	PieSemiCircleChartUnresponsive,
-} from './components/pie-semi-circle-chart';
-export { BarListChart, BarListChartUnresponsive } from './components/bar-list-chart';
-export { LeaderboardChart, LeaderboardChartUnresponsive } from './components/leaderboard-chart';
-export { ConversionFunnelChart } from './components/conversion-funnel-chart';
+} from './primitives/pie-semi-circle-chart';
+export { BarListChart, BarListChartUnresponsive } from './primitives/bar-list-chart';
+export { LeaderboardChart, LeaderboardChartUnresponsive } from './primitives/leaderboard-chart';
+export { ConversionFunnelChart } from './primitives/conversion-funnel-chart';
 
-// Chart components
+// Components
 export { BaseTooltip } from './components/tooltip';
 export { Legend, useChartLegendItems } from './components/legend';
 export type { LegendValueDisplay, BaseLegendItem } from './components/legend';
@@ -30,6 +30,6 @@ export {
 // Types
 export type * from './types';
 export type * from './visx/types';
-export type { PieChartProps } from './components/pie-chart';
+export type { PieChartProps } from './primitives/pie-chart';
 
 export type { LineStyles, GridStyles, EventHandlerParams } from '@visx/xychart';
