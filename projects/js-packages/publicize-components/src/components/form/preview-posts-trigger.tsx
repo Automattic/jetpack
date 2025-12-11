a7364bfb795a96a7dd66afae4522344001f52e3b
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-12-11 14:52:52 +0530

    Disable preview button if sharing is disabled

--- projects/js-packages/publicize-components/src/components/form/preview-posts-trigger.tsx
+++ projects/js-packages/publicize-components/src/components/form/preview-posts-trigger.tsx
@@ -20,7 +20,7 @@ export function PreviewPostsTrigger() {
 	const { recordEvent } = useAnalytics();
 	const { hasConnections } = useSocialMediaConnections();
 	const isPostPublished = useSelect( select => select( editorStore ).isCurrentPostPublished(), [] );
-	const { needsUserConnection } = usePublicizeConfig();
+	const { isPublicizeEnabled, needsUserConnection } = usePublicizeConfig();
 
 	const handleOpenModal = useCallback( () => {
 		if ( ! isModalOpen ) {
@@ -42,6 +42,7 @@ export function PreviewPostsTrigger() {
 			variant="secondary"
 			onClick={ handleOpenModal }
 			className={ styles[ 'preview-posts-trigger' ] }
+			disabled={ ! isPublicizeEnabled }
 		>
 			{ isPostPublished
 				? _x(
