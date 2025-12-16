3498c1043a429eca40bc38d076c7ab58236b4740
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-12-16 16:21:52 +0530

    Social: Fix connections management modal not opening from pre-publish panel. (#46316)

--- projects/js-packages/publicize-components/src/components/global-modals/index.tsx
+++ projects/js-packages/publicize-components/src/components/global-modals/index.tsx
@@ -1,5 +1,6 @@
 import { siteHasFeature } from '@automattic/jetpack-script-data';
 import { features } from '../../utils/constants';
+import { ThemedConnectionsModal as ManageConnectionsModal } from '../manage-connections-modal';
 import { ThemedShareStatusModal as ShareStatusModal } from '../share-status';
 import { UnifiedModal } from '../unified-modal';
 
@@ -8,6 +9,7 @@ export const GlobalModals = () => {
 		<>
 			{ siteHasFeature( features.SHARE_STATUS ) ? <ShareStatusModal /> : null }
 			{ siteHasFeature( features.UNIFIED_UI_V1 ) ? <UnifiedModal /> : null }
+			<ManageConnectionsModal />
 		</>
 	);
 };
