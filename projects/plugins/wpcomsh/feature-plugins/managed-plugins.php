5ccdd41c98bd5c889069d79409ac071069e24100
Gergely Csécsey <gcsecsey@gmail.com>
2025-12-16 17:03:00 +0000

    update both autoupdate related options

--- projects/plugins/wpcomsh/feature-plugins/managed-plugins.php
+++ projects/plugins/wpcomsh/feature-plugins/managed-plugins.php
@@ -463,12 +463,11 @@ function wpcomsh_auto_update_new_plugins_by_default( $pre_auto_update_plugins ) 
 	}
 
 	if ( is_array( $auto_update_plugins ) && ! empty( $new_unmanaged_plugins ) ) {
-		$old_auto_update_plugins = $auto_update_plugins;
-		$auto_update_plugins     = array_unique( array_merge( $auto_update_plugins, $new_unmanaged_plugins ) );
+		$auto_update_plugins = array_unique( array_merge( $auto_update_plugins, $new_unmanaged_plugins ) );
 		update_option( 'auto_update_plugins', $auto_update_plugins );
 
-		// Trigger Jetpack sync for the auto_update_plugins option change.
-		do_action( 'updated_option', 'auto_update_plugins', $old_auto_update_plugins, $auto_update_plugins );
+		// Update Jetpack's mirror option so it gets synced to wpcom.
+		update_option( 'jetpack_autoupdate_plugins', $auto_update_plugins );
 	}
 
 	if ( $baseline_plugins_list != $fresh_plugins_list ) { //phpcs:ignore
