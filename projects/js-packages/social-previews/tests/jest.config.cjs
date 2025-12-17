44bdab91874506f4826fd0576dacac37fb0ce674
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-12-17 10:00:26 +0530

    Apply suggestions from @anomiex

--- projects/js-packages/social-previews/tests/jest.config.cjs
+++ projects/js-packages/social-previews/tests/jest.config.cjs
@@ -4,8 +4,6 @@ const baseConfig = require( 'jetpack-js-tools/jest/config.base.js' );
 module.exports = {
 	...baseConfig,
 	rootDir: path.join( __dirname, '..' ),
-	testEnvironment: 'jsdom',
-	setupFilesAfterEnv: [ ...baseConfig.setupFilesAfterEnv, '@testing-library/jest-dom' ],
 	transform: {
 		...baseConfig.transform,
 		'\\.[jt]sx?$': require( 'jetpack-js-tools/jest/babel-jest-config-factory.js' )(
