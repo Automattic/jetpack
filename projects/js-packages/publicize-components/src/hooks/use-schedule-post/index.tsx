876862d5aae9a5a56c5df2eea11c249b2f662fac
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-12-17 10:47:08 +0530

    Update useSchedulePost to use the new action from the store

--- projects/js-packages/publicize-components/src/hooks/use-schedule-post/index.tsx
+++ projects/js-packages/publicize-components/src/hooks/use-schedule-post/index.tsx
@@ -1,16 +1,12 @@
 import { siteHasFeature } from '@automattic/jetpack-script-data';
-import { dispatch, useDispatch, useSelect } from '@wordpress/data';
-import { store as editorStore } from '@wordpress/editor';
+import { useDispatch } from '@wordpress/data';
 import { useCallback } from '@wordpress/element';
 import { store as socialStore } from '../../social-store';
 import { features } from '../../utils';
+import useSocialMediaConnections from '../use-social-media-connections';
 import useSocialMediaMessage from '../use-social-media-message';
 
 type SchedulePostOptions = {
-	/** The connection IDs to share to. */
-	connectionIds: number[];
-	/** The message to share. */
-	message?: string;
 	/** The timestamp to schedule the share for. */
 	timestamp: number;
 };
@@ -18,66 +14,30 @@ type SchedulePostOptions = {
 /**
  * Hook to schedule a post for sharing to social media connections.
  *
- * @return {object} Object containing schedule functionality and state.
+ * @return Object containing schedule functionality and state.
  */
 export function useSchedulePost() {
-	const { postId, isAutosaveablePost, isDirtyPost } = useSelect( select => {
-		const editorSelector = select( editorStore );
-
-		return {
-			postId: editorSelector.getCurrentPostId(),
-			isAutosaveablePost: editorSelector.isEditedPostAutosaveable(),
-			isDirtyPost: editorSelector.isEditedPostDirty(),
-		};
-	}, [] );
-
-	const { createScheduledShare } = useDispatch( socialStore );
+	const { scheduleShares } = useDispatch( socialStore );
 	const { message } = useSocialMediaMessage();
-	const hasMediaFeatures =
-		siteHasFeature( features.IMAGE_GENERATOR ) || siteHasFeature( features.ENHANCED_PUBLISHING );
+	const { enabledConnections } = useSocialMediaConnections();
 
-	const savePost = dispatch( editorStore ).savePost;
-
-	const schedulePost = useCallback(
-		async ( { connectionIds, timestamp }: SchedulePostOptions ) => {
-			if ( ! connectionIds.length || ! timestamp ) {
-				return false;
-			}
+	return useCallback(
+		async ( { timestamp }: SchedulePostOptions ) => {
+			const connectionIds = enabledConnections.map( connection =>
+				Number( connection.connection_id )
+			);
 
 			/**
 			 * The share endpoint only gets the custom message as a parameter, the attached media and
 			 * SIG is saved to the post meta and will be read on wpcom. Because of that we need to save
 			 * the post before sharing it, if it has the media features to make sure we use the latest data.
 			 */
-			if ( isDirtyPost && isAutosaveablePost && hasMediaFeatures ) {
-				await savePost();
-			}
+			const saveBeforeSharing =
+				siteHasFeature( features.IMAGE_GENERATOR ) ||
+				siteHasFeature( features.ENHANCED_PUBLISHING );
 
-			const result = await Promise.all(
-				connectionIds.map( connection_id => {
-					return createScheduledShare( {
-						post_id: Number( postId ),
-						connection_id,
-						message,
-						timestamp,
-					} );
-				} )
-			);
-
-			return result.every( Boolean );
+			return await scheduleShares( { connectionIds, message, timestamp }, { saveBeforeSharing } );
 		},
-		[
-			isDirtyPost,
-			isAutosaveablePost,
-			hasMediaFeatures,
-			savePost,
-			createScheduledShare,
-			postId,
-			message,
-		]
+		[ enabledConnections, message, scheduleShares ]
 	);
-
-	return {
-		schedulePost,
-	};
 }
