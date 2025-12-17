f6b1a07d378a22856f9dfb4e1f9e3908889fb972
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2025-12-17 14:07:05 +1300

    Add USStates story to GeoChart
    Demonstrate the region and resolution props with a US states map
    showing views data across 15 states.

--- projects/js-packages/charts/src/charts/geo-chart/stories/index.stories.tsx
+++ projects/js-packages/charts/src/charts/geo-chart/stories/index.stories.tsx
@@ -32,3 +32,29 @@ export const EmptyData: Story = {
 		data: [ [ 'Country', 'Views' ] ],
 	},
 };
+
+export const USStates: Story = {
+	args: {
+		...geoChartStoryArgs,
+		region: 'US',
+		resolution: 'provinces',
+		data: [
+			[ 'State', 'Views' ],
+			[ 'California', 2500 ],
+			[ 'Texas', 1800 ],
+			[ 'Florida', 1500 ],
+			[ 'New York', 1400 ],
+			[ 'Illinois', 900 ],
+			[ 'Pennsylvania', 850 ],
+			[ 'Ohio', 750 ],
+			[ 'Georgia', 700 ],
+			[ 'North Carolina', 650 ],
+			[ 'Michigan', 600 ],
+			[ 'Washington', 550 ],
+			[ 'Arizona', 500 ],
+			[ 'Massachusetts', 480 ],
+			[ 'Colorado', 450 ],
+			[ 'Virginia', 420 ],
+		],
+	},
+};
