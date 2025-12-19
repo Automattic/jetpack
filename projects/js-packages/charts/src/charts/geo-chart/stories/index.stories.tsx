d28bced69818f5ee318254bca0445ab2221a1d75
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2025-12-19 14:41:43 +1300

    Refactor GeoChart stories to use sample data for US states and European countries
    Updated the USStates and EuropeanCountries stories in the GeoChart component to utilize new sample data imports, enhancing maintainability and clarity. The data for both stories now references the newly defined viewsByUSState and viewsByEuropeanCountry datasets.

--- projects/js-packages/charts/src/charts/geo-chart/stories/index.stories.tsx
+++ projects/js-packages/charts/src/charts/geo-chart/stories/index.stories.tsx
@@ -1,3 +1,4 @@
+import { viewsByEuropeanCountry, viewsByUSState } from '../../../stories/sample-data';
 import GeoChart from '../geo-chart';
 import { geoChartMetaArgs, geoChartStoryArgs } from './config';
 import type { Meta, StoryObj } from '@storybook/react';
@@ -38,24 +39,7 @@ export const USStates: Story = {
 		...geoChartStoryArgs,
 		region: 'US',
 		resolution: 'provinces',
-		data: [
-			[ 'State', 'Views' ],
-			[ 'California', 2500 ],
-			[ 'Texas', 1800 ],
-			[ 'Florida', 1500 ],
-			[ 'New York', 1400 ],
-			[ 'Illinois', 900 ],
-			[ 'Pennsylvania', 850 ],
-			[ 'Ohio', 750 ],
-			[ 'Georgia', 700 ],
-			[ 'North Carolina', 650 ],
-			[ 'Michigan', 600 ],
-			[ 'Washington', 550 ],
-			[ 'Arizona', 500 ],
-			[ 'Massachusetts', 480 ],
-			[ 'Colorado', 450 ],
-			[ 'Virginia', 420 ],
-		],
+		data: viewsByUSState,
 	},
 };
 
@@ -64,17 +48,6 @@ export const EuropeanCountries: Story = {
 		...geoChartStoryArgs,
 		region: '150',
 		resolution: 'countries',
-		data: [
-			[ 'Country', 'Views' ],
-			[ 'United Kingdom', 1500 ],
-			[ 'France', 1000 ],
-			[ 'Germany', 800 ],
-			[ 'Italy', 600 ],
-			[ 'Spain', 500 ],
-			[ 'Portugal', 400 ],
-			[ 'Greece', 300 ],
-			[ 'Turkey', 200 ],
-			[ 'Russia', 100 ],
-		],
+		data: viewsByEuropeanCountry,
 	},
 };
