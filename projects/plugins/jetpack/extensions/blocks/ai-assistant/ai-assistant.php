ecc0a5e0ea68d2097d44caa271015791ac3f589a
William Viana <vianasw@gmail.com>
2025-12-29 17:50:39 +0100

    Use get_locale instead of the WPLANG option

--- projects/plugins/jetpack/extensions/blocks/ai-assistant/ai-assistant.php
+++ projects/plugins/jetpack/extensions/blocks/ai-assistant/ai-assistant.php
@@ -78,7 +78,7 @@ add_action(
 			Jetpack_Gutenberg::set_extension_available( 'ai-title-optimization-keywords-support' );
 			Jetpack_Gutenberg::set_extension_available( 'ai-assistant-image-extension' );
 
-			$site_locale = get_option( 'WPLANG' );
+			$site_locale = get_locale();
 			// Only enable Write Brief for sites with an English locale
 			if ( str_starts_with( $site_locale, 'en' ) && apply_filters( 'breve_enabled', true ) ) {
 				Jetpack_Gutenberg::set_extension_available( 'ai-proofread-breve' );
