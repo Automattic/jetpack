f9b38121d9dd41a8f3646187b29affd926890f8a
Brad Jorsch <brad.jorsch@automattic.com>
2026-01-07 11:59:01 -0500

    Get some builds working

--- projects/plugins/jetpack/tools/webpack.config.extensions.js
+++ projects/plugins/jetpack/tools/webpack.config.extensions.js
@@ -290,7 +290,7 @@ module.exports = [
 					if ( ! resource.contextInfo.issuer.includes( 'extensions/shared/i18n-to-php' ) ) {
 						resource.request = path.join(
 							path.dirname( __dirname ),
-							'./extensions/shared/i18n-to-php'
+							'./extensions/shared/i18n-to-php.js'
 						);
 					}
 				}
