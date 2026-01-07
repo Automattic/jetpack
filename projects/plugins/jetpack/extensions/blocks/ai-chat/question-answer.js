0b50d9616853f99696620194b50a69f5e54a373e
Katja Paavola <katja.paavola@automattic.com>
2026-01-07 15:25:41 +0000

    We just needed the span tag
    We just needed the span tag

--- projects/plugins/jetpack/extensions/blocks/ai-chat/question-answer.js
+++ projects/plugins/jetpack/extensions/blocks/ai-chat/question-answer.js
@@ -26,38 +26,38 @@ const waitStrings = [
 	__( '🤔 Thinking, thinking, will be back with an answer soon', 'jetpack' ),
 ];
 
-// Animates responses word-by-word using stripped text, then shows full HTML.
-// This avoids invalid HTML fragments that break React's DOM reconciliation.
+// This component displays the text word by word if show animation is true
 function ShowLittleByLittle( { html, showAnimation, onAnimationDone } ) {
-	const [ displayedContent, setDisplayedContent ] = useState( '' );
+	// This is the HTML to be displayed.
+	const [ displayedRawHTML, setDisplayedRawHTML ] = useState( '' );
 
 	useEffect(
 		() => {
+			// That will only happen once
 			if ( showAnimation && html ) {
-				// Strip HTML tags to get plain text for animation
-				const plainText = html.replace( /<[^>]+>/g, '' ).trim();
-				const tokens = plainText.split( ' ' );
-
+				// This is to animate text input. I think this will give an idea of a "better" AI.
+				// At this point this is an established pattern.
+				const tokens = html.split( ' ' );
 				for ( let i = 1; i < tokens.length; i++ ) {
 					const output = tokens.slice( 0, i ).join( ' ' );
-					setTimeout( () => setDisplayedContent( output ), 50 * i );
+					setTimeout( () => setDisplayedRawHTML( output ), 50 * i );
 				}
 				setTimeout( () => {
-					setDisplayedContent( html );
+					setDisplayedRawHTML( html );
 					onAnimationDone();
 				}, 50 * tokens.length );
 			} else {
-				setDisplayedContent( html || '' );
+				setDisplayedRawHTML( html );
 				onAnimationDone();
 			}
 		},
-		// eslint-disable-next-line react-hooks/exhaustive-deps
+		// eslint-disable-next-line
 		[]
 	);
 
 	return (
 		<div className="content">
-			<RawHTML>{ displayedContent }</RawHTML>
+			<RawHTML>{ displayedRawHTML }</RawHTML>
 		</div>
 	);
 }
