22b0fecfc446cd806a7946afe9e66253f5888d85
Brandon Kraft <public@brandonkraft.com>
2026-01-08 10:45:50 -0500

    Test PHP auto-fix

---
+++ projects/packages/changelogger/test-autofix.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Test file for auto-fix.
+ *
+ * @package automattic/changelogger
+ */
+
+/**
+ * A test function.
+ *
+ * @return int Returns 1.
+ */
+function test_function() {
+	$x = 1;
+	return $x;
+}
