2b2817e8e107b1308ec11769701fb8fe049401a3
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2026-01-09 20:06:44 +0500

    🐛 FIX: embed preview on repost

---
+++ projects/plugins/jetpack/extensions/shared/wait-for-editor.ts
@@ -0,0 +1,37 @@
+import { select, subscribe } from '@wordpress/data';
+
+/**
+ * Waits for the block editor to be initialized.
+ *
+ * @return Promise that resolves when the editor is initialized, or rejects on max attempts.
+ */
+export const waitForEditor = (): Promise< void > =>
+	new Promise( resolve => {
+		if ( isEditorReady() ) {
+			resolve();
+			return;
+		}
+
+		const unsubscribe = subscribe( (): void => {
+			if ( isEditorReady() ) {
+				unsubscribe();
+				resolve();
+			}
+		} );
+	} );
+
+/**
+ * Checks if the block editor has been initialized.
+ *
+ * @return {boolean} True if the block editor has been initialized.
+ */
+function isEditorReady(): boolean {
+	const editorStore = select( 'core/editor' );
+	const blockEditorStore = select( 'core/block-editor' );
+
+	return (
+		editorStore.isCleanNewPost() ||
+		blockEditorStore.getBlocks().length > 0 ||
+		editorStore.getCurrentPostId()
+	);
+}
