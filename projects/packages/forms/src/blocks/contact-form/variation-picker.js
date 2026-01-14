7bb04cf549f31e8dc69a5923b507c871c5394178
Enej Bajgoric <enej.bajgoric@automattic.com>
2026-01-14 13:23:44 -0800

    Enhance contact form picker with form selection
    Updated the contact form variation picker to allow selecting existing forms and improved the UI layout in the editor. Added logic to create and reference synced forms, and adjusted styles for better alignment and spacing.

--- projects/packages/forms/src/blocks/contact-form/variation-picker.js
+++ projects/packages/forms/src/blocks/contact-form/variation-picker.js
@@ -3,12 +3,17 @@ import {
 	__experimentalBlockPatternSetup as BlockPatternSetup, // eslint-disable-line @wordpress/no-unsafe-wp-apis
 	store as blockEditorStore,
 } from '@wordpress/block-editor';
-import { createBlock, store as blocksStore } from '@wordpress/blocks';
-import { Button, Modal } from '@wordpress/components';
+import { createBlock, serialize, store as blocksStore } from '@wordpress/blocks';
+import { Button, Modal, SelectControl } from '@wordpress/components';
+import { store as coreStore } from '@wordpress/core-data';
 import { useDispatch, useRegistry, useSelect } from '@wordpress/data';
+import { store as editorStore } from '@wordpress/editor';
 import { useEffect, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import { store as noticesStore } from '@wordpress/notices';
 import clsx from 'clsx';
+import { FORM_POST_TYPE } from '../shared/util/constants.js';
+
 import './util/form-styles.js';
 
 const createBlocksFromInnerBlocksTemplate = innerBlocksTemplate => {
@@ -23,18 +28,32 @@ export default function VariationPicker( { blockName, setAttributes, clientId, c
 	const registry = useRegistry();
 	const [ isPatternsModalOpen, setIsPatternsModalOpen ] = useState( false );
 	const { replaceInnerBlocks, selectBlock } = useDispatch( blockEditorStore );
-	const { blockType, defaultVariation, variations } = useSelect(
+	const { saveEntityRecord } = useDispatch( coreStore );
+	const { createSuccessNotice } = useDispatch( noticesStore );
+	const { blockType, defaultVariation, variations, currentPostType, jetpackForms } = useSelect(
 		select => {
 			const { getBlockType, getBlockVariations, getDefaultBlockVariation } = select( blocksStore );
+			const { getCurrentPostType } = select( editorStore );
+			const { getEntityRecords } = select( coreStore );
 
 			return {
 				blockType: getBlockType( blockName ),
 				defaultVariation: getDefaultBlockVariation( blockName, 'block' ),
 				variations: getBlockVariations( blockName, 'block' ),
+				currentPostType: getCurrentPostType(),
+				jetpackForms:
+					getEntityRecords( 'postType', FORM_POST_TYPE, {
+						per_page: 100,
+						status: 'publish',
+						orderBy: 'modified',
+					} ) || [],
 			};
 		},
 		[ blockName ]
 	);
+
+	// Check if we're editing a jetpack-form post directly
+	const isEditingJetpackFormPost = currentPostType === FORM_POST_TYPE;
 
 	useEffect( () => {
 		if (
@@ -46,37 +65,120 @@ export default function VariationPicker( { blockName, setAttributes, clientId, c
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [] );
 
+	const handleFormSelection = formId => {
+		if ( ! formId ) {
+			return;
+		}
+
+		registry.batch( () => {
+			setAttributes( { ref: parseInt( formId, 10 ) } );
+			selectBlock( clientId );
+		} );
+
+		createSuccessNotice( __( 'Form selected.', 'jetpack-forms' ), {
+			type: 'snackbar',
+			isDismissible: true,
+		} );
+	};
+
 	return (
 		<div className={ clsx( classNames, 'is-placeholder' ) }>
 			<BlockVariationPicker
 				icon={ blockType?.icon?.src }
 				label={ blockType?.title }
 				instructions={ __(
-					'Start by selecting one of these templates, or browse patterns.',
+					'Start by selecting one of these templates, browse patterns, or select an existing form below.',
 					'jetpack-forms'
 				) }
 				variations={ variations.filter( v => ! v.hiddenFromPicker ) }
-				onSelect={ ( nextVariation = defaultVariation ) => {
-					registry.batch( () => {
-						if ( nextVariation.attributes ) {
-							setAttributes( nextVariation.attributes );
-						}
+				onSelect={ async ( nextVariation = defaultVariation ) => {
+					// If we're editing a jetpack-form post directly, use the old behavior
+					// (just set attributes and inner blocks, don't create another ref)
+					if ( isEditingJetpackFormPost || nextVariation.name === 'regular-form' ) {
+						registry.batch( () => {
+							if ( nextVariation.attributes ) {
+								setAttributes( nextVariation.attributes );
+							}
+							if ( nextVariation.innerBlocks ) {
+								replaceInnerBlocks(
+									clientId,
+									createBlocksFromInnerBlocksTemplate( nextVariation.innerBlocks )
+								);
+							}
+							selectBlock( clientId );
+						} );
+					} else {
+						// We're editing a regular post/page - create a synced form with ref
+						try {
+							// Create inner blocks from template
+							const innerBlocks = createBlocksFromInnerBlocksTemplate( nextVariation.innerBlocks );
 
-						if ( nextVariation.innerBlocks ) {
-							replaceInnerBlocks(
-								clientId,
-								createBlocksFromInnerBlocksTemplate( nextVariation.innerBlocks )
+							// Create the full jetpack/contact-form block with attributes and inner blocks
+							const formBlock = createBlock(
+								'jetpack/contact-form',
+								nextVariation.attributes || {},
+								innerBlocks
 							);
-						}
 
-						selectBlock( clientId );
-					} );
+							// Serialize the entire form block to block markup
+							const serialized = serialize( formBlock );
+
+							// Create jetpack-form post
+							const post = await saveEntityRecord( 'postType', FORM_POST_TYPE, {
+								title: nextVariation.title || 'Form',
+								content: serialized,
+								status: 'publish',
+							} );
+
+							// Set ONLY ref attribute
+							registry.batch( () => {
+								setAttributes( { ref: post.id } );
+								selectBlock( clientId );
+							} );
+
+							// Show success notice
+							createSuccessNotice( __( 'New form created.', 'jetpack-forms' ), {
+								type: 'snackbar',
+								isDismissible: true,
+							} );
+						} catch ( error ) {
+							// eslint-disable-next-line no-console
+							console.error( 'Failed to create synced form:', error );
+							// Fallback to old behavior
+							registry.batch( () => {
+								if ( nextVariation.attributes ) {
+									setAttributes( nextVariation.attributes );
+								}
+								if ( nextVariation.innerBlocks ) {
+									replaceInnerBlocks(
+										clientId,
+										createBlocksFromInnerBlocksTemplate( nextVariation.innerBlocks )
+									);
+								}
+								selectBlock( clientId );
+							} );
+						}
+					}
 				} }
 			/>
 			<div className="form-placeholder__footer">
 				<Button variant="secondary" onClick={ () => setIsPatternsModalOpen( true ) }>
 					{ __( 'Browse form patterns', 'jetpack-forms' ) }
 				</Button>
+				{ ! isEditingJetpackFormPost && (
+					<SelectControl
+						label={ __( 'Or select an existing form', 'jetpack-forms' ) }
+						value=""
+						options={ [
+							{ label: __( 'Select a form…', 'jetpack-forms' ), value: '' },
+							...jetpackForms.map( form => ( {
+								label: form.title?.rendered || __( '(Untitled)', 'jetpack-forms' ),
+								value: form.id.toString(),
+							} ) ),
+						] }
+						onChange={ handleFormSelection }
+					/>
+				) }
 			</div>
 			{ isPatternsModalOpen && (
 				<Modal
