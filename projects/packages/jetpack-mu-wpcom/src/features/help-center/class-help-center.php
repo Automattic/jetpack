7fff0aa45db071a646119a9b8d9b913bdb7311f3
Omar Alshaker <omar@omaralshaker.com>
2026-01-15 14:25:51 +0100

    Don't fetch the avatar

--- projects/packages/jetpack-mu-wpcom/src/features/help-center/class-help-center.php
+++ projects/packages/jetpack-mu-wpcom/src/features/help-center/class-help-center.php
@@ -278,7 +278,7 @@ class Help_Center {
 			$username           = $user_data ? $user_data->user_login : null;
 			$user_email         = $user_data ? $user_data->user_email : null;
 			$display_name       = $user_data ? $user_data->display_name : null;
-			$avatar_url         = function_exists( 'wpcom_get_avatar_url' ) ? wpcom_get_avatar_url( $user_email, 64, '', true )[0] : get_avatar_url( $user_id );
+			$avatar_url         = $user_id ? ( function_exists( 'wpcom_get_avatar_url' ) ? wpcom_get_avatar_url( $user_email, 64, '', true )[0] : get_avatar_url( $user_id ) ) : null;
 			$is_commerce_garden = defined( 'IS_COMMERCE_GARDEN' );
 
 			wp_add_inline_script(
