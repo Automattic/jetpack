49b59096ce1bf814448d0b03aa29da11147c20c5
Brad Jorsch <brad.jorsch@automattic.com>
2026-01-15 08:50:09 -0500

    We can also remove setting `resolve.fullySpecified: false` for GB packages now
    Except when `@wordpress/dataviews` is involved, as that's upgraded in a
    separate PR.

--- projects/packages/my-jetpack/webpack.config.js
+++ projects/packages/my-jetpack/webpack.config.js
@@ -24,7 +24,8 @@ module.exports = [
 		module: {
 			strictExportPresence: true,
 			rules: [
-				// Gutenberg packages' ESM builds don't fully specify their imports. Sigh.
+				// Old Gutenberg packages' ESM builds don't fully specify their imports. Sigh.
+				// @todo Remove this when we upgrade @wordpress/dataviews to 11.2.0+.
 				// https://github.com/WordPress/gutenberg/issues/73362
 				{
 					test: /\/node_modules\/@wordpress\/.*\/build-module\/.*\.js$/,
