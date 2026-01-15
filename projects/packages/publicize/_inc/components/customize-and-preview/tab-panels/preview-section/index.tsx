59c5ca9d9db8afa2936efaabca9c38dcecdd53b4
Manzoor Wani <manzoorwani.jk@gmail.com>
2026-01-15 20:19:47 +0530

    Use Notice for disabled preview

--- projects/packages/publicize/_inc/components/customize-and-preview/tab-panels/preview-section/index.tsx
+++ projects/packages/publicize/_inc/components/customize-and-preview/tab-panels/preview-section/index.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@wordpress/components';
+import { Flex, Notice } from '@wordpress/components';
 import { __, _x } from '@wordpress/i18n';
 import { Connection } from '../../../../social-store/types';
 import { PostPreview } from '../../../social-post-modal/post-preview';
@@ -28,10 +28,10 @@ export function PreviewSection( { connection }: PreviewSectionProps ) {
 					<PostPreview connection={ connection } />
 				</>
 			) : (
-				<Flex className={ styles[ 'inactive-preview' ] } align="center" justify="center">
-					<p>
+				<Flex className={ styles[ 'inactive-preview' ] } justify="center">
+					<Notice isDismissible={ false } status="info">
 						{ __( 'Enable sharing to this account to see the preview.', 'jetpack-publicize-pkg' ) }
-					</p>
+					</Notice>
 				</Flex>
 			) }
 		</section>
