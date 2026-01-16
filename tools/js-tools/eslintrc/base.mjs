e4b75803204108dcd86f183eeb651eeaa0f857b0
Mikael Korpela <mikael.korpela@automattic.com>
2026-01-16 17:08:57 +0200

    Eslint: update WP eslint and add new components-no-missing-40px-size-prop rule

--- tools/js-tools/eslintrc/base.mjs
+++ tools/js-tools/eslintrc/base.mjs
@@ -177,6 +177,11 @@ export function makeBaseConfig( configurl, opts = {} ) {
 
 						// Replacement for throttle is not as straightforward as you-dont-need-lodash-underscore claims.
 						'you-dont-need-lodash-underscore/throttle': 'off',
+
+						'@wordpress/components-no-missing-40px-size-prop': [
+							'error',
+							{ checkLocalImports: true },
+						],
 					},
 				},
 				tanstackEslintPluginQuery.configs[ 'flat/recommended' ],
