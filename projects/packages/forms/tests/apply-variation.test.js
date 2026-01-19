0f628f4fcff112aac4db954a7c09d86e34bfcab1
Enej Bajgoric <enej.bajgoric@automattic.com>
2026-01-19 14:29:28 -0800

    fixes to make the tests pass.

--- projects/packages/forms/tests/apply-variation.test.js
+++ projects/packages/forms/tests/apply-variation.test.js
@@ -1,3 +1,4 @@
+import { beforeEach, describe, expect, it, jest } from '@jest/globals';
 import { applyVariationToFormBlock } from '../src/blocks/contact-form/util/apply-variation.js';
 
 describe( 'applyVariationToFormBlock', () => {
