4b1ccb1f4a81f0c7e4ece670d6085955561abe2e
Karen Attfield <karenlattfield@gmail.com>
2026-01-19 12:49:17 +0000

    Simplify post checks in wp insert functions

--- projects/packages/sync/src/modules/class-posts.php
+++ projects/packages/sync/src/modules/class-posts.php
@@ -739,12 +739,12 @@ class Posts extends Module {
 	 * @param boolean  $update  Whether this is an existing post being updated or not.
 	 */
 	public function wp_insert_post( $post_ID, $post = null, $update = null ) {
-		if ( ! is_numeric( $post_ID ) || $post === null || ! $post instanceof \WP_Post ) {
+		if ( ! is_numeric( $post_ID ) || ! $post instanceof \WP_Post ) {
 			return;
 		}
 
 		// Workaround for https://github.com/woocommerce/woocommerce/issues/18007.
-		if ( $post && 'shop_order' === $post->post_type ) {
+		if ( 'shop_order' === $post->post_type ) {
 			$post = get_post( $post_ID );
 		}
 
@@ -783,12 +783,12 @@ class Posts extends Module {
 	 * @param \WP_Post $post    Post object.
 	 **/
 	public function wp_after_insert_post( $post_ID, $post ) {
-		if ( ! is_numeric( $post_ID ) || $post === null || ! $post instanceof \WP_Post ) {
+		if ( ! is_numeric( $post_ID ) || ! $post instanceof \WP_Post ) {
 			return;
 		}
 
 		// Workaround for https://github.com/woocommerce/woocommerce/issues/18007.
-		if ( $post && 'shop_order' === $post->post_type ) {
+		if ( 'shop_order' === $post->post_type ) {
 			$post = get_post( $post_ID );
 		}
 
