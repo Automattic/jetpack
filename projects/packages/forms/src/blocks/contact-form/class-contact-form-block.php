1fb1748fa37ec282233f5620003a5fa4093aae9b
Mikael Korpela <mikael.korpela@automattic.com>
2026-01-19 19:31:40 +0200

    Add function back as deprecated

--- projects/packages/forms/src/blocks/contact-form/class-contact-form-block.php
+++ projects/packages/forms/src/blocks/contact-form/class-contact-form-block.php
@@ -828,6 +828,15 @@ class Contact_Form_Block {
 	}
 
 	/**
+	 * Load editor styles for the block.
+	 *
+	 * @deprecated $$next-version$$ This function is deprecated and will be removed in a future version.
+	 */
+	public static function load_editor_styles() {
+		_deprecated_function( __METHOD__, 'jetpack-15.5', null );
+	}
+
+	/**
 	 * Loads scripts
 	 */
 	public static function load_editor_scripts() {
