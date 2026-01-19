97aff7ad0aa4e8754455981656a2e0f8a8917302
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2026-01-19 16:13:12 +0800

    Refactor bar chart exports to streamline component usage
    Removed the export of TruncatedTickComponent from the bar chart index file, simplifying the module's interface and focusing on the essential exports for better maintainability.

--- projects/js-packages/charts/src/charts/bar-chart/private/index.ts
+++ projects/js-packages/charts/src/charts/bar-chart/private/index.ts
@@ -1,2 +1,2 @@
 export { useBarChartOptions } from './use-bar-chart-options';
-export { createTruncatedTickComponent, TruncatedTickComponent } from './truncated-tick-component';
+export { createTruncatedTickComponent } from './truncated-tick-component';
