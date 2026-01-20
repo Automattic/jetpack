3bb414f4a1d5e2ece380c76493a44291f1b0f52a
Christian Gastrell <cgastrell@gmail.com>
2026-01-20 16:17:13 -0300

    use new 'collection' format on API response for fields

--- projects/packages/forms/src/contact-form/class-contact-form-endpoint.php
+++ projects/packages/forms/src/contact-form/class-contact-form-endpoint.php
@@ -860,7 +860,7 @@ class Contact_Form_Endpoint extends \WP_REST_Posts_Controller {
 		}
 
 		if ( rest_is_field_included( 'fields', $fields ) ) {
-			$data['fields'] = $feedback_response->get_compiled_fields( 'api', 'label-value' );
+			$data['fields'] = $feedback_response->get_compiled_fields( 'api', 'collection' );
 		}
 
 		if ( rest_is_field_included( 'has_file', $fields ) ) {
