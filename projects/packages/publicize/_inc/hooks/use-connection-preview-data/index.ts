a06f7e9aff8db0ff3e22949e19518c7c658a9ccc
Manzoor Wani <manzoorwani.jk@gmail.com>
2026-01-21 15:31:30 +0530

    Add useConnectionPreviewData hook
    Introduces the useConnectionPreviewData hook to provide post data for social connection previews, including message and media selection logic based on connection settings and site features.

---
+++ projects/packages/publicize/_inc/hooks/use-connection-preview-data/index.ts
@@ -0,0 +1,86 @@
+import { siteHasFeature } from '@automattic/jetpack-script-data';
+import { useSelect } from '@wordpress/data';
+import { store as editorStore } from '@wordpress/editor';
+import { useMemo } from 'react';
+import { Connection } from '../../social-store/types';
+import { features } from '../../utils';
+import useMediaDetails from '../use-media-details';
+import { usePerNetworkCustomization } from '../use-per-network-customization';
+import useSigPreview from '../use-sig-preview';
+import useSocialMediaMessage from '../use-social-media-message';
+import { useSocialPreviewPostData } from '../use-social-preview-post-data';
+import { PostData } from '../use-social-preview-post-data/types';
+
+/**
+ * Returns the post data needed for the preview of a specific connection.
+ *
+ * @param {Connection} connection - The connection.
+ * @return The post data.
+ */
+export function useConnectionPreviewData( connection: Connection ) {
+	const { isEnabled: usingPerNetworkCustomization } = usePerNetworkCustomization();
+
+	const postData = useSocialPreviewPostData();
+	const { message: globalMessage } = useSocialMediaMessage();
+	const featuredImageId = useSelect( select =>
+		select( editorStore ).getEditedPostAttribute( 'featured_media' )
+	);
+	const [ featuredImageDetails ] = useMediaDetails( featuredImageId );
+
+	// Generate SIG preview only if site has the feature and connection is set to use SIG.
+	const generateSigPreview =
+		siteHasFeature( features.IMAGE_GENERATOR ) && connection.media_source === 'sig';
+
+	const sig = useSigPreview( generateSigPreview );
+
+	return useMemo( () => {
+		if ( ! siteHasFeature( features.ENHANCED_PUBLISHING ) || ! usingPerNetworkCustomization ) {
+			return {
+				...postData,
+				message: globalMessage.trim(),
+			};
+		}
+
+		let media: PostData[ 'media' ] = connection.attached_media || [];
+
+		switch ( connection.media_source ) {
+			case 'featured-image':
+				media = featuredImageDetails
+					? [
+							{
+								url: featuredImageDetails.mediaData?.sourceUrl,
+								type: featuredImageDetails.metaData?.mime ?? 'image/jpeg',
+							},
+					  ]
+					: [];
+				break;
+			case 'sig':
+				media = sig.url
+					? [
+							{
+								url: sig.url,
+								type: 'image/png',
+							},
+					  ]
+					: [];
+				break;
+
+			case 'none':
+				media = [];
+				break;
+		}
+
+		return {
+			...postData,
+			message: globalMessage.trim(),
+			media,
+		};
+	}, [
+		connection,
+		featuredImageDetails,
+		globalMessage,
+		postData,
+		sig.url,
+		usingPerNetworkCustomization,
+	] );
+}
