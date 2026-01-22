5b0738325cc6796e57ded68a2c95c69697f3ccd9
Katja Paavola <katja.paavola@automattic.com>
2026-01-22 11:39:40 +0000

    Feature flag to disable Tracks / TrainTracks
    Adds a feature flag to disable Tracks / TrainTracks for diagnosing if these
    are what is causing some of the privacy features for Safari to break Jetpack
    Search.

--- projects/packages/search/src/instant-search/index.jsx
+++ projects/packages/search/src/instant-search/index.jsx
@@ -21,7 +21,7 @@ const injectSearchApp = () => {
 					...window[ SERVER_OBJECT_NAME ].widgets,
 					...window[ SERVER_OBJECT_NAME ].widgetsOutsideOverlay,
 				] ) }
-				enableAnalytics
+				enableAnalytics={ ! window[ SERVER_OBJECT_NAME ].disableTracking }
 				hasOverlayWidgets={ !! window[ SERVER_OBJECT_NAME ].hasOverlayWidgets }
 				initialHref={ window.location.href }
 				// NOTE: initialIsVisible is only used in the customizer. See lib/customize.js.
