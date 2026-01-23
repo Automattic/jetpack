a17a6b5ff987ee3431ce3abee285d647ff760a8a
Enej Bajgoric <enej.bajgoric@automattic.com>
2026-01-23 09:16:59 -0800

    Remove the "Warning" that we only display to screen reader

--- projects/packages/forms/src/contact-form/class-contact-form.php
+++ projects/packages/forms/src/contact-form/class-contact-form.php
@@ -1504,7 +1504,7 @@ class Contact_Form extends Contact_Form_Shortcode {
 	private static function render_error_wrapper() {
 		$html  = '<div class="contact-form__error" data-wp-class--show-errors="state.showFormErrors">';
 		$html .= '<span class="contact-form__warning-icon" aria-hidden="true"><i></i></span>';
-		$html .= '<span class="contact-form__error-message" tabindex="-1" role="alert" data-wp-text="state.getFormErrorMessage" data-wp-watch="callbacks.focusOnValidationError"><span class="screen-reader-text">' . esc_html__( 'Warning.', 'jetpack-forms' ) . '</span></span>';
+		$html .= '<span class="contact-form__error-message" tabindex="-1" role="alert" data-wp-watch="callbacks.focusOnValidationError" data-wp-text="state.getFormErrorMessage"></span>';
 		$html .= '<ul aria-label="' . esc_attr__( 'Form errors', 'jetpack-forms' ) . '">
 				<template data-wp-each="state.getErrorList" data-wp-key="context.item.id">
 					<li><a data-wp-bind--href="context.item.anchor" data-wp-on--click="actions.scrollIntoView" data-wp-text="context.item.label"></a></li>
