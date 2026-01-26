945fadead2841f329ade5cea0c5c742c45f41839
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2026-01-26 17:13:09 +0800

    Fix baseline

--- projects/packages/woocommerce-analytics/.phan/baseline.php
+++ projects/packages/woocommerce-analytics/.phan/baseline.php
@@ -8,9 +8,10 @@
  * (can be combined with --load-baseline)
  */
 return [
-		// This baseline has no suppressions
+    // This baseline has no suppressions
     // Currently, file_suppressions and directory_suppressions are the only supported suppressions
-    'file_suppressions' => [],
+    'file_suppressions' => [
+    ],
     // 'directory_suppressions' => ['src/directory_name' => ['PhanIssueName1', 'PhanIssueName2']] can be manually added if needed.
     // (directory_suppressions will currently be ignored by subsequent calls to --save-baseline, but may be preserved in future Phan releases)
 ];
