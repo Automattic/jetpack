97e7e92380113b6ec71ecd574b93f971f7b16cd0
Douglas <douglas.henri@automattic.com>
2026-01-27 18:11:58 -0300

    fix lint issues

--- projects/packages/forms/routes/responses/stage.tsx
+++ projects/packages/forms/routes/responses/stage.tsx
@@ -334,9 +334,11 @@ function Stage() {
 							/>
 							{ styleUnreadValue(
 								<Stack direction="column" gap="2xs">
-									<Text ellipsizeMode="tail" limit={50} truncate>{ displayName }</Text>
+									<Text ellipsizeMode="tail" limit={ 50 } truncate>
+										{ displayName }
+									</Text>
 									{ showEmail && (
-										<Text variant="muted" size={12} ellipsizeMode="tail" limit={50} truncate>
+										<Text variant="muted" size={ 12 } ellipsizeMode="tail" limit={ 50 } truncate>
 											{ item.author_email }
 										</Text>
 									) }
