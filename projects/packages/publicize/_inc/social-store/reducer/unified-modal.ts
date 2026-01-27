0bed830858e45cc17fdeaf59610346c7e35a1cef
Manzoor Wani <manzoorwani.jk@gmail.com>
2026-01-27 19:30:02 +0530

    Add SET_UNIFIED_MODAL_DATA action for unified modal
    Introduces a new SET_UNIFIED_MODAL_DATA action and corresponding action creator to set data for the unified modal. Updates the reducer to handle this new action and store the provided data in the modal state.

--- projects/packages/publicize/_inc/social-store/reducer/unified-modal.ts
+++ projects/packages/publicize/_inc/social-store/reducer/unified-modal.ts
@@ -2,11 +2,13 @@ import {
 	TOGGLE_UNIFIED_MODAL,
 	SET_UNIFIED_MODAL_INITIAL_PATH,
 	SET_UNIFIED_MODAL_SCREEN_LOCK,
+	SET_UNIFIED_MODAL_DATA,
 } from '../actions/constants';
 import {
 	toggleUnifiedModal,
 	setUnifiedModalInitialPath,
 	setUnifiedModalScreenLock,
+	setUnifiedModalData,
 } from '../actions/unified-modal';
 import { SocialStoreState } from '../types';
 
@@ -15,6 +17,7 @@ type Action =
 			| typeof toggleUnifiedModal
 			| typeof setUnifiedModalInitialPath
 			| typeof setUnifiedModalScreenLock
+			| typeof setUnifiedModalData
 	  >
 	| { type: 'default' };
 
@@ -51,6 +54,12 @@ export function unifiedModal(
 			return {
 				...state,
 				isScreenLocked: action.isLocked,
+			};
+
+		case SET_UNIFIED_MODAL_DATA:
+			return {
+				...state,
+				data: action.data,
 			};
 		default:
 			return state;
