2f8887fa4318f9c3d44716508ce8e9cacb1b2454
Manzoor Wani <manzoorwani.jk@gmail.com>
2026-01-27 17:52:31 +0530

    Add "View scheduled" button to preview modal

--- projects/packages/publicize/_inc/components/unified-modal/social-post-preview/footer-content.tsx
+++ projects/packages/publicize/_inc/components/unified-modal/social-post-preview/footer-content.tsx
@@ -1,7 +1,10 @@
-import { useSelect } from '@wordpress/data';
+import { Button, useNavigator } from '@wordpress/components';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { store as editorStore } from '@wordpress/editor';
-import { _n, sprintf } from '@wordpress/i18n';
+import { useCallback } from '@wordpress/element';
+import { __, _n, sprintf } from '@wordpress/i18n';
 import useSocialMediaConnections from '../../../hooks/use-social-media-connections';
+import { store as socialStore } from '../../../social-store';
 import { ConfirmationConfig } from './confirmation-config';
 
 /**
@@ -11,29 +14,52 @@ import { ConfirmationConfig } from './confirmation-config';
  */
 export function FooterContent() {
 	const { enabledConnections } = useSocialMediaConnections();
+	const navigator = useNavigator();
+	const { setUnifiedModalData } = useDispatch( socialStore );
 
 	const isPrePublishScreen = useSelect( select => {
 		const store = select( editorStore );
 		return ! store.isCurrentPostPublished() && store.isPublishSidebarOpened();
 	}, [] );
 
+	const hasScheduledShares = useSelect( select => {
+		const postId = select( editorStore ).getCurrentPostId();
+
+		return postId && select( socialStore ).getScheduledSharesForPost( Number( postId ) ).length > 0;
+	}, [] );
+
+	const viewScheduled = useCallback( () => {
+		setUnifiedModalData( { initialTab: 'scheduled' } );
+		navigator.goTo( '/sharing-activity' );
+	}, [ navigator, setUnifiedModalData ] );
+
 	// If the pre-publish sidebar is opened, show the confirmation config.
 	if ( isPrePublishScreen ) {
 		return <ConfirmationConfig />;
 	}
 
-	return enabledConnections.length ? (
-		<span>
-			{ sprintf(
-				/* translators: %d: Number of enabled connections. */
-				_n(
-					'Ready to share to %d account.',
-					'Ready to share to %d accounts.',
-					enabledConnections.length,
-					'jetpack-publicize-pkg'
-				),
-				enabledConnections.length
-			) }
-		</span>
-	) : null;
+	return (
+		<>
+			{ enabledConnections.length ? (
+				<span>
+					{ sprintf(
+						/* translators: %d: Number of enabled connections. */
+						_n(
+							'Ready to share to %d account.',
+							'Ready to share to %d accounts.',
+							enabledConnections.length,
+							'jetpack-publicize-pkg'
+						),
+						enabledConnections.length
+					) }
+					&nbsp;
+				</span>
+			) : null }
+			{ hasScheduledShares ? (
+				<Button variant="link" onClick={ viewScheduled }>
+					{ __( 'View scheduled', 'jetpack-publicize-pkg' ) }
+				</Button>
+			) : null }
+		</>
+	);
 }
