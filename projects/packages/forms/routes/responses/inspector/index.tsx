50ee1f9fbfe9834fd8432f8a4e2c7ee963da3c7a
Mikael Korpela <mikael.korpela@automattic.com>
2026-01-27 13:34:33 +0200

    Use component in new dashboard

--- projects/packages/forms/routes/responses/inspector/index.tsx
+++ projects/packages/forms/routes/responses/inspector/index.tsx
@@ -1,27 +1,17 @@
 /**
- * External dependencies
- */
-import getRedirectUrl from '@automattic/jetpack-components/tools/jp-redirect';
-/**
  * WordPress dependencies
  */
 import { Page } from '@wordpress/admin-ui';
 import apiFetch from '@wordpress/api-fetch';
 import {
-	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
-	__experimentalHStack as HStack,
-	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
-	__experimentalVStack as VStack,
 	Button,
 	ExternalLink,
 	Modal,
 	Spinner,
 	Tip,
-	Tooltip,
 } from '@wordpress/components';
 import { store as coreStore, useEntityRecords } from '@wordpress/core-data';
 import { useDispatch, useSelect } from '@wordpress/data';
-import { dateI18n, getSettings as getDateSettings } from '@wordpress/date';
 import { useCallback, useEffect, useState } from '@wordpress/element';
 import { decodeEntities } from '@wordpress/html-entities';
 import { __, _n, sprintf } from '@wordpress/i18n';
@@ -31,8 +21,7 @@ import * as React from 'react';
  * Internal dependencies
  */
 import CopyClipboardButton from '../../../src/dashboard/components/copy-clipboard-button';
-import Flag from '../../../src/dashboard/components/flag';
-import Gravatar from '../../../src/dashboard/components/gravatar';
+import ResponseMeta from '../../../src/dashboard/components/inspector/response-meta';
 import { ResponseActions } from './actions';
 import { ResponseNavigation } from './navigation';
 import type { DispatchActions, SelectActions } from '../../../src/dashboard/inbox/stage/types.tsx';
@@ -456,12 +445,6 @@ function SingleResponseView( {
 		);
 	}
 
-	const displayName = getDisplayName( response );
-	const dateSettings = getDateSettings();
-	const gravatarEmail = response.author_email || response.ip;
-	const defaultImage = response.author_name || response.author_email ? 'initials' : 'mp';
-	const responseAuthorEmailParts = response.author_email?.split( '@' ) ?? [];
-
 	return (
 		<>
 			<div
@@ -486,118 +469,7 @@ function SingleResponseView( {
 			</div>
 
 			<div style={ { padding: '20px', overflowY: 'auto' } }>
-				<div style={ { marginBottom: '20px' } }>
-					<HStack alignment="topLeft" spacing="3">
-						<Gravatar
-							email={ gravatarEmail }
-							defaultImage={ defaultImage }
-							displayName={ displayName }
-							size={ 48 }
-						/>
-						<VStack spacing="0">
-							<h3 style={ { margin: 0, fontSize: '16px', fontWeight: 600 } }>{ displayName }</h3>
-							{ response.author_email && displayName !== response.author_email && (
-								<p style={ { margin: '4px 0 0', color: '#666', fontSize: '13px' } }>
-									<a href={ `mailto:${ response.author_email }` }>
-										{ responseAuthorEmailParts[ 0 ] }
-										<wbr />@{ responseAuthorEmailParts[ 1 ] }
-									</a>
-									<CopyClipboardButton text={ response.author_email } />
-								</p>
-							) }
-						</VStack>
-					</HStack>
-				</div>
-
-				<div style={ { marginBottom: '20px' } }>
-					<table style={ { width: '100%', borderCollapse: 'collapse', fontSize: '13px' } }>
-						<tbody>
-							<tr>
-								<th
-									style={ {
-										textAlign: 'left',
-										padding: '6px 12px 6px 0',
-										fontWeight: 'normal',
-										color: '#666',
-										width: '100px',
-									} }
-								>
-									{ __( 'Date:', 'jetpack-forms' ) }
-								</th>
-								<td style={ { padding: '6px 0' } }>
-									{ sprintf(
-										/* Translators: %1$s is the date, %2$s is the time. */
-										__( '%1$s at %2$s', 'jetpack-forms' ),
-										dateI18n( dateSettings.formats.date, response.date ),
-										dateI18n( dateSettings.formats.time, response.date )
-									) }
-								</td>
-							</tr>
-							<tr>
-								<th
-									style={ {
-										textAlign: 'left',
-										padding: '6px 12px 6px 0',
-										fontWeight: 'normal',
-										color: '#666',
-									} }
-								>
-									{ __( 'Source:', 'jetpack-forms' ) }
-								</th>
-								<td style={ { padding: '6px 0' } }>
-									{ response.entry_permalink ? (
-										<ExternalLink href={ response.entry_permalink }>
-											{ decodeEntities( response.entry_title ) || response.entry_permalink }
-										</ExternalLink>
-									) : (
-										decodeEntities( response.entry_title ) || __( 'Unknown', 'jetpack-forms' )
-									) }
-								</td>
-							</tr>
-							{ response.ip && (
-								<tr>
-									<th
-										style={ {
-											textAlign: 'left',
-											padding: '6px 12px 6px 0',
-											fontWeight: 'normal',
-											color: '#666',
-										} }
-									>
-										{ __( 'IP address:', 'jetpack-forms' ) }
-									</th>
-									<td style={ { padding: '6px 0' } }>
-										{ response.country_code && (
-											<span style={ { marginRight: '6px' } }>
-												<Flag countryCode={ response.country_code } />
-											</span>
-										) }
-										<Tooltip text={ __( 'Lookup IP address', 'jetpack-forms' ) }>
-											<ExternalLink href={ getRedirectUrl( 'ip-lookup', { path: response.ip } ) }>
-												{ response.ip }
-											</ExternalLink>
-										</Tooltip>
-									</td>
-								</tr>
-							) }
-							{ response.browser && (
-								<tr>
-									<th
-										style={ {
-											textAlign: 'left',
-											padding: '6px 12px 6px 0',
-											fontWeight: 'normal',
-											color: '#666',
-										} }
-									>
-										{ __( 'Browser:', 'jetpack-forms' ) }
-									</th>
-									<td style={ { padding: '6px 0' } }>{ response.browser }</td>
-								</tr>
-							) }
-						</tbody>
-					</table>
-				</div>
+				<ResponseMeta response={ response } />
 
 				{ response.fields && Object.keys( response.fields ).length > 0 && (
 					<div>
