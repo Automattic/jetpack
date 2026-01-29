a0ec5b9b5ff5ab5bccaaf53cbe6095e479c16d02
annacmc <anna.mcphee@automattic.com>
2026-01-29 20:40:07 +1100

    Add TimeSeriesForecastChart component with styles

---
+++ projects/js-packages/charts/src/charts/time-series-forecast-chart/index.ts
@@ -0,0 +1,12 @@
+export {
+	default as TimeSeriesForecastChart,
+	TimeSeriesForecastChartUnresponsive,
+} from './time-series-forecast-chart';
+
+export type {
+	TimeSeriesForecastChartProps,
+	TimeSeriesForecastAccessors,
+	SeriesKeys,
+	Accessor,
+	ForecastTooltipParams,
+} from './types';
