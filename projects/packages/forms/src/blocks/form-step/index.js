e4a4de4ddfa15fdec6016c522280e22006ba7db2
Christian Gastrell <cgastrell@gmail.com>
2026-01-29 18:06:34 -0300

    Forms: Rename icon.js files to icon.jsx for proper JSX extensions

--- projects/packages/forms/src/blocks/form-step/index.js
+++ projects/packages/forms/src/blocks/form-step/index.js
@@ -1,6 +1,6 @@
 import { __ } from '@wordpress/i18n';
 import edit from './edit.js';
-import StepIcon from './icon.js';
+import StepIcon from './icon.jsx';
 import save from './save.js';
 
 export const name = 'form-step';
