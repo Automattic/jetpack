0d26609d536f16bc20a8b0d8cb4ed113491fa279
Илья Салигжанов <saligzhanov.i@spectrumdata.ru>
2026-01-29 15:18:44 +0500

    fix: PHP-CS style fixes

--- projects/plugins/super-cache/wp-cache.php
+++ projects/plugins/super-cache/wp-cache.php
@@ -1379,18 +1379,18 @@ table.wpsc-settings-table {
 		<?php
 		$supercache_last_cached_option = get_option( 'supercache_last_cached' );
 		if ( is_array( $supercache_last_cached_option ) ) {
-		?>
+			?>
 		<p><?php _e( 'Newest Cached Pages:', 'wp-super-cache' ); ?><ol>
 			<?php
-				foreach ( array_reverse( $supercache_last_cached_option ) as $url ) {
-					$since = time() - strtotime( $url['date'] ?? '' );
-					echo "<li><a title='" . sprintf( esc_html__( 'Cached %s seconds ago', 'wp-super-cache' ), (int) $since ) . "' href='" . site_url( $url['url'] ) . "'>" . substr( $url['url'], 0, 20 ) . "</a></li>\n";
-				}
+			foreach ( array_reverse( $supercache_last_cached_option ) as $url ) {
+				$since = time() - strtotime( $url['date'] ?? '' );
+				echo "<li><a title='" . sprintf( esc_html__( 'Cached %s seconds ago', 'wp-super-cache' ), (int) $since ) . "' href='" . site_url( $url['url'] ) . "'>" . substr( $url['url'], 0, 20 ) . "</a></li>\n";
+			}
 			?>
 			</ol>
 			<small><?php esc_html_e( '(may not always be accurate on busy sites)', 'wp-super-cache' ); ?></small>
 		</p>
-		<?php
+			<?php
 		}
 	} elseif ( false == get_option( 'wpsupercache_start' ) ) {
 			update_option( 'wpsupercache_start', time() );
