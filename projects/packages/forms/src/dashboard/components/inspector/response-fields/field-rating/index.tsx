113a28d2e90c747c8191d4beee2cbca73e98d4bd
Christian Gastrell <cgastrell@gmail.com>
2026-01-30 17:18:05 -0300

    Forms: Add accessibility support to FieldRating component

--- projects/packages/forms/src/dashboard/components/inspector/response-fields/field-rating/index.tsx
+++ projects/packages/forms/src/dashboard/components/inspector/response-fields/field-rating/index.tsx
@@ -5,7 +5,9 @@ import {
 	__experimentalHStack as HStack, // eslint-disable-line @wordpress/no-unsafe-wp-apis
 	SVG,
 	Path,
+	VisuallyHidden,
 } from '@wordpress/components';
+import { __, sprintf } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
@@ -20,8 +22,11 @@ const FieldRating = ( { value }: FieldRatingProps ) => {
 	if ( stringValue.trim() === '' ) {
 		return <>-</>;
 	}
-	const [ rateValue ] = stringValue.split( '/' );
+	const [ rateValue, outOf ] = stringValue.split( '/' ) ?? [];
 	if ( ! rateValue || rateValue.trim() === '' ) {
+		return <>-</>;
+	}
+	if ( ! outOf || outOf.trim() === '' ) {
 		return <>-</>;
 	}
 	const iconSvg = (
@@ -35,14 +40,24 @@ const FieldRating = ( { value }: FieldRatingProps ) => {
 			/>
 		</SVG>
 	);
+	const ratingLabel = sprintf(
+		/* translators: 1: rating value, 2: maximum rating (e.g. "4" and "5" for "4 out of 5") */
+		__( 'Rating %1$s out of %2$s', 'jetpack-forms' ),
+		rateValue.trim(),
+		outOf.trim()
+	);
+
 	return (
-		<HStack spacing="1" alignment="topLeft">
-			{ Array.from( { length: parseInt( rateValue ) }, ( _, index ) => (
-				<div style={ { flex: '0 0 24px' } } key={ index }>
-					{ iconSvg }
-				</div>
-			) ) }
-		</HStack>
+		<>
+			<VisuallyHidden as="span">{ ratingLabel }</VisuallyHidden>
+			<HStack spacing="1" alignment="topLeft">
+				{ Array.from( { length: parseInt( rateValue ) }, ( _, index ) => (
+					<span style={ { flex: '0 0 24px' } } key={ index }>
+						{ iconSvg }
+					</span>
+				) ) }
+			</HStack>
+		</>
 	);
 };
 
