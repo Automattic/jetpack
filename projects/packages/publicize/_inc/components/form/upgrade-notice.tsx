19bf3e73bdba1cd0f803f011b088c88aec74adf4
Manzoor Wani <manzoorwani.jk@gmail.com>
2026-02-02 16:51:30 +0530

    Social: Convert upgrade and demo buttons to links that open in new tab

--- projects/packages/publicize/_inc/components/form/upgrade-notice.tsx
+++ projects/packages/publicize/_inc/components/form/upgrade-notice.tsx
@@ -1,12 +1,8 @@
 import { getRedirectUrl } from '@automattic/jetpack-components';
 import { isSimpleSite } from '@automattic/jetpack-script-data';
-import {
-	getSiteFragment,
-	useAutosaveAndRedirect,
-} from '@automattic/jetpack-shared-extension-utils';
-import { Notice } from '@wordpress/components';
+import { getSiteFragment } from '@automattic/jetpack-shared-extension-utils';
+import { Button, Flex, FlexItem, Notice } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import clsx from 'clsx';
 
 /**
  * A notice for upgrading to a plan that supports the Enhanced Publishing feature.
@@ -14,47 +10,52 @@ import clsx from 'clsx';
  * @return The UpgradeNotice component.
  */
 export function UpgradeNotice() {
-	const redirectUrl = getRedirectUrl( 'jetpack-social-basic-plan-block-editor', {
-		site: getSiteFragment() || '',
-		query: 'redirect_to=' + encodeURIComponent( window.location.href ),
-	} );
-
-	const { autosaveAndRedirect, isRedirecting } = useAutosaveAndRedirect( redirectUrl );
-
 	if ( isSimpleSite() ) {
 		// We don't have any upgrade options on Simple sites, yet.
 		return null;
 	}
 
+	const redirectUrl = getRedirectUrl( 'jetpack-social-basic-plan-block-editor', {
+		site: getSiteFragment() || '',
+		query: 'redirect_to=' + encodeURIComponent( window.location.href ),
+	} );
+
+	const message = __(
+		'Choose your social media image or video to share.',
+		'jetpack-publicize-pkg'
+	);
+
 	return (
-		<Notice
-			isDismissible={ false }
-			status="info"
-			actions={ [
-				{
-					variant: 'primary',
-					label: __( 'Upgrade now', 'jetpack-publicize-pkg' ),
-					/**
-					 * At the time of writing, Notice action does not support disabled or busy state,
-					 * @see https://github.com/WordPress/gutenberg/issues/74090
-					 */
-					// So we prevent onClick when redirecting.
-					onClick: isRedirecting ? undefined : autosaveAndRedirect,
-					className: clsx( 'is-compact', {
-						// Because of the above, we add a busy class for styling purposes.
-						'is-busy': isRedirecting,
-					} ),
-				},
-				{
-					variant: 'secondary',
-					label: __( 'View demo', 'jetpack-publicize-pkg' ),
-					noDefaultClasses: true,
-					className: 'is-compact',
-					url: getRedirectUrl( 'jetpack-social-landing-page' ),
-				},
-			] }
-		>
-			{ __( 'Choose your social media image or video to share.', 'jetpack-publicize-pkg' ) }
+		/**
+		 * Render actions manually instead of using Notice actions prop
+		 * because actions are not flexible enough for our use case. e.g., the actions do not accept all the button props.
+		 *
+		 * @see https://github.com/WordPress/gutenberg/issues/74090
+		 */
+		<Notice isDismissible={ false } status="info" spokenMessage={ message }>
+			<Flex direction="column">
+				<FlexItem>{ message }</FlexItem>
+				<Flex justify="flex-start" gap={ 3 }>
+					<Button
+						variant="primary"
+						className="is-compact"
+						href={ redirectUrl }
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						{ __( 'Upgrade now', 'jetpack-publicize-pkg' ) }
+					</Button>
+					<Button
+						variant="secondary"
+						href={ getRedirectUrl( 'jetpack-social-demo' ) }
+						target="_blank"
+						className="is-compact"
+						rel="noopener noreferrer"
+					>
+						{ __( 'View demo', 'jetpack-publicize-pkg' ) }
+					</Button>
+				</Flex>
+			</Flex>
 		</Notice>
 	);
 }
