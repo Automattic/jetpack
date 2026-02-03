697e3b26f66b6eab063de796b2438bc5e9052b68
Enej Bajgoric <enej.bajgoric@automattic.com>
2026-02-02 20:33:28 -0800

    Display "Publish now" as button

--- projects/packages/forms/src/blocks/contact-form/edit.tsx
+++ projects/packages/forms/src/blocks/contact-form/edit.tsx
@@ -984,7 +984,9 @@ function JetpackContactFormEdit( {
 				) }
 			{ formStatus !== 'trash' && (
 				<Button
-					variant="link"
+					variant="secondary"
+					size="small"
+					__next40pxDefaultSize={ true }
 					onClick={ handlePublishForm }
 					isBusy={ isPublishingForm }
 					disabled={ isPublishingForm }
