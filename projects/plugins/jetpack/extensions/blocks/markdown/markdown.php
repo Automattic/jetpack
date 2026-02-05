29bd6bc7890d1dbe512b4442df043e9d002f799e
Jeremy Herve <jeremy@jeremy.hu>
2026-02-05 15:46:03 +0100

    Register RSS hooks for Markdown block, use require_once for shared rss.php

--- projects/plugins/jetpack/extensions/blocks/markdown/markdown.php
+++ projects/plugins/jetpack/extensions/blocks/markdown/markdown.php
@@ -24,3 +24,10 @@ function register_block() {
 	Blocks::jetpack_register_block( __DIR__ );
 }
 add_action( 'init', __NAMESPACE__ . '\register_block' );
+
+// Add source:markdown element to RSS feeds for posts containing the Markdown block.
+require_once JETPACK__PLUGIN_DIR . '_inc/lib/markdown/rss.php';
+if ( class_exists( 'WP_Block_Processor' ) ) {
+	add_action( 'rss2_ns', 'jetpack_markdown_rss_namespace' );
+	add_action( 'rss2_item', 'jetpack_markdown_block_rss_output_source_markdown' );
+}
