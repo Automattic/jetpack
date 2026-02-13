15efa87d48185f4f1b0eb52c7d3de769aa1cf790
Douglas <douglas.henri@automattic.com>
2026-02-13 20:06:40 -0300

    move buttons to dropdown on mobile

--- projects/packages/forms/src/dashboard/wp-build/hooks/use-page-header-details.tsx
+++ projects/packages/forms/src/dashboard/wp-build/hooks/use-page-header-details.tsx
@@ -1,16 +1,19 @@
 /**
  * External dependencies
  */
+import { useBreakpointMatch } from '@automattic/jetpack-components';
 import JetpackLogo from '@automattic/jetpack-components/jetpack-logo';
 /**
  * WordPress dependencies
  */
 import { Breadcrumbs } from '@wordpress/admin-ui';
+import { DropdownMenu } from '@wordpress/components';
 import { store as coreDataStore } from '@wordpress/core-data';
 import { useSelect } from '@wordpress/data';
 import { useMemo } from '@wordpress/element';
 import { decodeEntities } from '@wordpress/html-entities';
 import { __, sprintf } from '@wordpress/i18n';
+import { moreVertical, plus, download, plugins } from '@wordpress/icons';
 import { Stack } from '@wordpress/ui';
 /**
  * Internal dependencies
@@ -20,6 +23,10 @@ import EditFormButton from '../../components/edit-form-button';
 import EmptySpamButton from '../../components/empty-spam-button';
 import EmptyTrashButton from '../../components/empty-trash-button';
 import ExportResponsesButton from '../../components/export-responses/button';
+import ExportResponsesModal from '../../components/export-responses/modal';
+import useCreateForm from '../../hooks/use-create-form';
+import useExportResponses from '../../hooks/use-export-responses';
+import useInboxData from '../../hooks/use-inbox-data';
 import ManageIntegrationsButton from '../components/manage-integrations-button';
 import type { ReactNode } from 'react';
 
@@ -61,9 +68,25 @@ export default function usePageHeaderDetails(
 		return Number.isFinite( numberValue ) && numberValue > 0 ? numberValue : null;
 	}, [ sourceId ] );
 
+	// Detect mobile viewport
+	const [ isSm ] = useBreakpointMatch( 'sm' );
+
 	// Mutually-exclusive screen flags.
 	const isFormsScreen = screen === 'forms';
 	const isSingleFormScreen = screen === 'responses' && sourceIdNumber !== null;
+
+	// Hooks for mobile dropdown menu actions
+	const { openNewForm } = useCreateForm();
+	const {
+		showExportModal,
+		openModal: openExportModal,
+		closeModal: closeExportModal,
+		onExport,
+		autoConnectGdrive,
+		exportLabel,
+	} = useExportResponses();
+	const { totalItems, isLoadingData } = useInboxData();
+	const hasResponses = ! isLoadingData && totalItems > 0;
 
 	const formRecord = useSelect(
 		select =>
@@ -122,6 +145,93 @@ export default function usePageHeaderDetails(
 	}, [ formTitle, isFormsScreen, isSingleFormScreen ] );
 
 	const actions = useMemo( () => {
+		// Mobile: show dropdown menu with actions
+		if ( isSm ) {
+			const dropdownControls = [];
+
+			if ( isFormsScreen ) {
+				// Forms screen: Manage integrations, Create a form
+				if ( isIntegrationsEnabled && showDashboardIntegrations ) {
+					dropdownControls.push( {
+						icon: plugins,
+						onClick: onOpenIntegrations,
+						title: __( 'Manage integrations', 'jetpack-forms' ),
+					} );
+				}
+
+				dropdownControls.push( {
+					icon: plus,
+					onClick: () => openNewForm( {} ),
+					title: __( 'Create a form', 'jetpack-forms' ),
+				} );
+			} else if ( isSingleFormScreen ) {
+				// Single form screen: Edit form (not in trash/spam), Export
+				if ( statusView === 'inbox' && sourceIdNumber ) {
+					dropdownControls.push( {
+						onClick: () => {
+							const fallbackEditUrl = `post.php?post=${ sourceIdNumber }&action=edit&post_type=jetpack_form`;
+							const url = new URL( fallbackEditUrl, window.location.origin );
+							window.location.href = url.toString();
+						},
+						title: __( 'Edit form', 'jetpack-forms' ),
+					} );
+				}
+				dropdownControls.push( {
+					icon: download,
+					onClick: openExportModal,
+					title: exportLabel,
+					isDisabled: ! hasResponses,
+				} );
+			} else {
+				// Responses list screen: Manage integrations (inbox only), Create a form (inbox only), Export
+				if ( statusView === 'inbox' && isIntegrationsEnabled && showDashboardIntegrations ) {
+					dropdownControls.push( {
+						icon: plugins,
+						onClick: onOpenIntegrations,
+						title: __( 'Manage integrations', 'jetpack-forms' ),
+					} );
+				}
+				if ( statusView === 'inbox' ) {
+					dropdownControls.push( {
+						icon: plus,
+						onClick: () => openNewForm( { showPatterns: false } ),
+						title: __( 'Create a form', 'jetpack-forms' ),
+					} );
+				}
+				dropdownControls.push( {
+					icon: download,
+					onClick: openExportModal,
+					title: exportLabel,
+					isDisabled: ! hasResponses,
+				} );
+			}
+
+			if ( dropdownControls.length === 0 ) {
+				return null;
+			}
+
+			return [
+				<DropdownMenu
+					key="actions-menu"
+					controls={ dropdownControls }
+					icon={ moreVertical }
+					label={ __( 'More actions', 'jetpack-forms' ) }
+				/>,
+				// Include the export modal when on mobile
+				...( showExportModal
+					? [
+							<ExportResponsesModal
+								key="export-modal"
+								onRequestClose={ closeExportModal }
+								onExport={ onExport }
+								autoConnectGdrive={ autoConnectGdrive }
+							/>,
+					  ]
+					: [] ),
+			];
+		}
+
+		// Desktop: show individual buttons
 		if ( isFormsScreen ) {
 			return [
 				...( isIntegrationsEnabled && showDashboardIntegrations
@@ -170,6 +280,7 @@ export default function usePageHeaderDetails(
 			...( statusView === 'spam' ? [ <EmptySpamButton key="empty-spam" /> ] : [] ),
 		];
 	}, [
+		isSm,
 		isIntegrationsEnabled,
 		onOpenIntegrations,
 		showDashboardIntegrations,
@@ -177,6 +288,14 @@ export default function usePageHeaderDetails(
 		isFormsScreen,
 		isSingleFormScreen,
 		statusView,
+		openNewForm,
+		openExportModal,
+		showExportModal,
+		closeExportModal,
+		onExport,
+		autoConnectGdrive,
+		hasResponses,
+		exportLabel,
 	] );
 
 	return { breadcrumbs, subtitle, actions };
