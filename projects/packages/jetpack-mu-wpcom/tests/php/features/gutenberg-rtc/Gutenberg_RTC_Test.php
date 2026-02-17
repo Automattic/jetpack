eb31a8c467dfa29e47d7a67531e9302b250821c3
heavyweight <kpapazov@gmail.com>
2026-02-17 17:35:37 +0100

    simplify test

--- projects/packages/jetpack-mu-wpcom/tests/php/features/gutenberg-rtc/Gutenberg_RTC_Test.php
+++ projects/packages/jetpack-mu-wpcom/tests/php/features/gutenberg-rtc/Gutenberg_RTC_Test.php
@@ -7,9 +7,10 @@
 
 declare( strict_types = 1 );
 
-//phpcs:ignore WordPressVIPMinimum.Files.IncludingFile.NotAbsolutePath
-require_once \Automattic\Jetpack\Jetpack_Mu_Wpcom::PKG_DIR . 'src/features/gutenberg-rtc/gutenberg-rtc.php';
+use Automattic\Jetpack\Jetpack_Mu_Wpcom;
 
+//phpcs:ignore WordPressVIPMinimum.Files.IncludingFile.NotAbsolutePath
+require_once Jetpack_Mu_Wpcom::PKG_DIR . 'src/features/gutenberg-rtc/gutenberg-rtc.php';
 use PHPUnit\Framework\Attributes\CoversFunction;
 
 /**
@@ -26,22 +27,5 @@ class Gutenberg_RTC_Test extends \WorDBless\BaseTestCase {
 	 */
 	public function test_wpcom_enqueue_gutenberg_rtc_assets_hooked() {
 		$this->assertSame( 10, has_action( 'enqueue_block_editor_assets', 'wpcom_enqueue_gutenberg_rtc_assets' ) );
-	}
-
-	/**
-	 * Tests whether the gutenberg-rtc script is enqueued correctly with proper dependencies.
-	 *
-	 * @return void
-	 */
-	public function test_wpcom_enqueue_gutenberg_rtc_assets_enqueues_script() {
-		wpcom_enqueue_gutenberg_rtc_assets();
-
-		// Check script is enqueued.
-		$this->assertTrue( wp_script_is( 'gutenberg-rtc-script', 'enqueued' ) );
-
-		// Check it has wp-hooks dependency.
-		global $wp_scripts;
-		$script = $wp_scripts->registered['gutenberg-rtc-script'];
-		$this->assertContains( 'wp-hooks', $script->deps );
 	}
 }
