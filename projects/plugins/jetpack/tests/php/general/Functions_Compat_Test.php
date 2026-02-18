885d707fc29d76f56072ae301b38456e6c90a6c9
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2026-02-18 13:13:54 +0500

    💫 UPDATE: address feedback

--- projects/plugins/jetpack/tests/php/general/Functions_Compat_Test.php
+++ projects/plugins/jetpack/tests/php/general/Functions_Compat_Test.php
@@ -156,7 +156,7 @@ class Functions_Compat_Test extends WP_UnitTestCase {
 
 	/**
 	 * @author mehmoodak
-	 * @since 14.5
+	 * @since $$next-version$$
 	 */
 	public function test_jetpack_youtube_sanitize_url_with_shorts_url() {
 		$sanitized_url = jetpack_youtube_sanitize_url( 'https://www.youtube.com/shorts/VIDEO_ID' );
@@ -194,7 +194,7 @@ class Functions_Compat_Test extends WP_UnitTestCase {
 
 	/**
 	 * @author mehmoodak
-	 * @since 14.5
+	 * @since $$next-version$$
 	 */
 	public function test_jetpack_get_youtube_id_with_shorts_url() {
 		$youtube_id = jetpack_get_youtube_id( 'https://www.youtube.com/shorts/VIDEO_ID' );
