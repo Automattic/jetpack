0ff6a8d714876cf1cec9ce85176c9f71c951a738
Mikael Korpela <mikael@ihminen.org>
2026-02-23 15:27:37 +0200

    Forms block: Add border styling support (#47274)
    * Forms block: add border support
    
    * add changelog
    
    * Fix border support opt in
    
    ---------
    
    Co-authored-by: Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>

--- projects/packages/forms/src/blocks/contact-form/class-contact-form-block.php
+++ projects/packages/forms/src/blocks/contact-form/class-contact-form-block.php
@@ -45,7 +45,7 @@ class Contact_Form_Block {
 				'render_email_callback' => array( __CLASS__, 'render_email' ),
 				'render_callback'       => array( __CLASS__, 'gutenblock_render_form' ),
 				'supports'              => array(
-					'layout' => array(
+					'layout'               => array(
 						'default'                => array(
 							'type'              => 'flex',
 							'flexWrap'          => 'wrap',
@@ -59,6 +59,18 @@ class Contact_Form_Block {
 						'allowVerticalAlignment' => true,
 						'allowJustification'     => true,
 						'allowWrap'              => false,
+					),
+					'__experimentalBorder' => array(
+						'color'                         => true,
+						'radius'                        => true,
+						'style'                         => true,
+						'width'                         => true,
+						'__experimentalDefaultControls' => array(
+							'color'  => true,
+							'radius' => true,
+							'style'  => true,
+							'width'  => true,
+						),
 					),
 				),
 				'style_handles'         => array( 'jetpack-forms-layout' ),
