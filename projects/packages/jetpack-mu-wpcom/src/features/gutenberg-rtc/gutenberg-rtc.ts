1e909adaa0af0d922d6bada011dea7a190cfc4b4
heavyweight <kpapazov@gmail.com>
2026-02-24 11:03:52 +0100

    Revert base file

--- projects/packages/jetpack-mu-wpcom/src/features/gutenberg-rtc/gutenberg-rtc.ts
+++ projects/packages/jetpack-mu-wpcom/src/features/gutenberg-rtc/gutenberg-rtc.ts
@@ -1,16 +1,4 @@
 import { addFilter } from '@wordpress/hooks';
-import { Y } from '@wordpress/sync';
-
-const win = window as unknown as { wp?: { sync?: { Y?: unknown } } };
-if ( win.wp?.sync?.Y ) {
-	// eslint-disable-next-line no-console
-	console.assert( Y === win.wp.sync.Y, 'ERROR: Two Yjs instances detected!' );
-	// eslint-disable-next-line no-console
-	console.log( 'Yjs instance check passed:', Y === win.wp.sync.Y );
-} else {
-	// eslint-disable-next-line no-console
-	console.error( 'Yjs instance not found!' );
-}
 
 // Register PingHub provider and disable Http polling by returning only this provider.
 ( function register() {
