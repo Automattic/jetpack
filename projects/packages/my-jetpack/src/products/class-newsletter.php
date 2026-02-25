0ae9a960d02f13c6fa9dff2fb4d58ae5563cce30
Dean Sas <dean.sas@automattic.com>
2026-02-25 21:50:42 +0000

    My Jetpack: Use new newsletter settings URL when filter is enabled

--- projects/packages/my-jetpack/src/products/class-newsletter.php
+++ projects/packages/my-jetpack/src/products/class-newsletter.php
@@ -172,6 +172,10 @@ class Newsletter extends Module_Product {
 	 * @return ?string
 	 */
 	public static function get_manage_url() {
+		/** This filter is documented in jetpack-newsletter/src/class-settings.php */
+		if ( apply_filters( 'jetpack_wp_admin_newsletter_settings_enabled', false ) ) {
+			return admin_url( 'admin.php?page=jetpack-newsletter' );
+		}
 		return admin_url( 'admin.php?page=jetpack#/settings?term=newsletter' );
 	}
 
