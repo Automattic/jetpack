f5cddbc771ef3fff0ce04f08cb96dd5af293ad59
Foteini Giannaropoulou <giannaropoulou.foteini@gmail.com>
2026-02-26 16:30:28 +0200

    Update 'checkout' endpoint

--- projects/packages/sync/src/class-rest-endpoints.php
+++ projects/packages/sync/src/class-rest-endpoints.php
@@ -175,6 +175,43 @@ class REST_Endpoints {
 				'methods'             => WP_REST_Server::EDITABLE,
 				'callback'            => __CLASS__ . '::checkout',
 				'permission_callback' => __CLASS__ . '::verify_default_permissions',
+				'args'                => array(
+					'queue'            => array(
+						'description' => __( 'Name of Sync queue.', 'jetpack-sync' ),
+						'type'        => 'string',
+						'required'    => false,
+					),
+					'number_of_items'  => array(
+						'description' => __( 'Number of items to checkout from the queue.', 'jetpack-sync' ),
+						'type'        => 'integer',
+						'required'    => false,
+					),
+					'pop'              => array(
+						'description'       => __( 'Pop items from the queue instead of checking out.', 'jetpack-sync' ),
+						'type'              => 'boolean',
+						'required'          => false,
+						'sanitize_callback' => 'rest_sanitize_boolean',
+					),
+					'force'            => array(
+						'description'       => __( 'Force unlock the queue before checkout.', 'jetpack-sync' ),
+						'type'              => 'boolean',
+						'required'          => false,
+						'sanitize_callback' => 'rest_sanitize_boolean',
+					),
+					'encode'           => array(
+						'description'       => __( 'Encode the items before sending.', 'jetpack-sync' ),
+						'type'              => 'boolean',
+						'required'          => false,
+						'default'           => true,
+						'sanitize_callback' => 'rest_sanitize_boolean',
+					),
+					'use_memory_limit' => array(
+						'description'       => __( 'Use memory-based checkout instead of fixed item count.', 'jetpack-sync' ),
+						'type'              => 'boolean',
+						'required'          => false,
+						'sanitize_callback' => 'rest_sanitize_boolean',
+					),
+				),
 			)
 		);
 
@@ -629,6 +666,10 @@ class REST_Endpoints {
 		}
 
 		$use_memory_limit = ! empty( $args['use_memory_limit'] );
+
+		if ( $use_memory_limit && ! empty( $args['pop'] ) ) {
+			return new WP_Error( 'invalid_args', 'pop cannot be used with use_memory_limit', 400 );
+		}
 
 		if ( ! $use_memory_limit ) {
 			$number_of_items = $args['number_of_items'];
