040c5fe106f52ce4050119cde116960fe15802de
Manzoor Wani <manzoorwani.jk@gmail.com>
2026-03-03 13:46:32 +0530

    Fix type errors in js-packages/shared-extension-utils

--- projects/js-packages/shared-extension-utils/src/get-jetpack-data.js
+++ projects/js-packages/shared-extension-utils/src/get-jetpack-data.js
@@ -3,6 +3,8 @@ export const JETPACK_DATA_PATH = 'Jetpack_Editor_Initial_State';
 /**
  * Retrieves Jetpack editor state
  *
+ * @deprecated Use the consolidated initial state using `getScriptData` from `@automattic/jetpack-script-data` instead. Feel free to extend it if needed.
+ *
  * @return {object|null}Object The Jetpack Editor State.
  */
 export default function getJetpackData() {
