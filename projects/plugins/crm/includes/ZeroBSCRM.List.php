02dfe324687b2fd82ffdcf15b6f05f993f9dd08c
Bob Matyas <45246438+bobmatyas@users.noreply.github.com>
2026-03-04 10:50:25 -0500

    Added doc link to empty state in Transactions, Quotes, and Invoices view

--- projects/plugins/crm/includes/ZeroBSCRM.List.php
+++ projects/plugins/crm/includes/ZeroBSCRM.List.php
@@ -166,7 +166,7 @@ class zeroBSCRM_list {
 			$sort = sanitize_text_field( $_GET['sort'] );
 		}
 		$sortOrder = false;
-		if ( isset( $_GET['sortdirection'] ) && ( $_GET['sortdirection'] == 'asc' || $_GET['sortdirection'] == 'desc' ) ) {
+		if ( isset( $_GET['sortdirection'] ) && ( $_GET['sortdirection'] === 'asc' || $_GET['sortdirection'] === 'desc' ) ) {
 			$sortOrder = sanitize_text_field( $_GET['sortdirection'] );
 		}
 
@@ -188,7 +188,7 @@ class zeroBSCRM_list {
 		if ( isset( $customViews ) && isset( $customViews[ $this->objType ] ) ) {
 			$currentColumns = $customViews[ $this->objType ];
 		}
-		if ( $currentColumns == false ) {
+		if ( $currentColumns === false ) {
 			$currentColumns = $defaultColumns;
 		}
 
@@ -270,7 +270,7 @@ class zeroBSCRM_list {
 										if ( ! array_key_exists( $colKey, $currentColumns ) ) {
 
 											// split em up
-											if ( isset( $col[2] ) && $col[2] == 'basefield' ) {
+											if ( isset( $col[2] ) && $col[2] === 'basefield' ) {
 												$allColumnsSorted['basefields'][ $colKey ] = $col;
 												$hasMultiColumnGroups                      = true;
 											} else {
@@ -334,7 +334,7 @@ class zeroBSCRM_list {
 									}
 
 									// if NONE output, we need to always have smt to drop to, so put empty:
-									if ( $colGroupCount == 0 ) {
+									if ( $colGroupCount === 0 ) {
 										echo '<div class="zbs-column-manager-connected">';
 										echo '</div>';
 									}
@@ -391,8 +391,15 @@ class zeroBSCRM_list {
 					echo zeroBSCRM_UI2_messageHTML( 'warning hidden', sprintf( __( 'Error retrieving %s', 'zero-bs-crm' ), $this->plural ), sprintf( __( 'There has been a problem retrieving your %s. If this issue persists, please contact support.', 'zero-bs-crm' ), $this->plural ), 'disabled warning sign', 'zbsCantLoadData' );
 					echo zeroBSCRM_UI2_messageHTML( 'warning hidden', sprintf( __( 'Error updating columns %s', 'zero-bs-crm' ), $this->plural ), __( 'There has been a problem saving your column configuration. If this issue persists, please contact support.', 'zero-bs-crm' ), 'disabled warning sign', 'zbsCantSaveCols' );
 					echo zeroBSCRM_UI2_messageHTML( 'warning hidden', sprintf( __( 'Error updating columns %s', 'zero-bs-crm' ), $this->plural ), __( 'There has been a problem saving your filter button configuration. If this issue persists, please contact support.', 'zero-bs-crm' ), 'disabled warning sign', 'zbsCantSaveButtons' ); // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
-					echo zeroBSCRM_UI2_messageHTML( 'info hidden', sprintf( __( 'No %s Found', 'zero-bs-crm' ), $this->plural ), sprintf( __( 'There are no %1$s here. Do you want to <a href="%2$s">create one</a>?', 'zero-bs-crm' ), $this->plural, jpcrm_esc_link( 'create', -1, $this->postType ) ), 'disabled warning sign', 'zbsNoResults' ); // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
-
+					$no_results_msg = sprintf( __( 'There are no %1$s here. Do you want to <a href="%2$s">create one</a>?', 'zero-bs-crm' ), $this->plural, jpcrm_esc_link( 'create', -1, $this->postType ) );
+					if ( $this->objType === 'transaction' ) { // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+						$no_results_msg .= ' ' . sprintf( __( 'Learn more about <a href="%1$s">how to create a transaction</a>.', 'zero-bs-crm' ), esc_url( 'https://kb.jetpackcrm.com/knowledge-base/how-do-i-create-a-transaction/' ) );
+					} elseif ( $this->objType === 'invoice' ) { // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+						$no_results_msg .= ' ' . sprintf( __( 'Learn more about <a href="%1$s">how to create an invoice</a>.', 'zero-bs-crm' ), esc_url( 'https://kb.jetpackcrm.com/knowledge-base/how-to-use-the-invoice-builder/' ) );
+					} elseif ( $this->objType === 'quote' ) { // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+						$no_results_msg .= ' ' . sprintf( __( 'Learn more about <a href="%1$s">how to create a quote</a>.', 'zero-bs-crm' ), esc_url( 'https://kb.jetpackcrm.com/knowledge-base/how-do-i-create-a-quote/' ) );
+					}
+					echo zeroBSCRM_UI2_messageHTML( 'info hidden', sprintf( __( 'No %s Found', 'zero-bs-crm' ), $this->plural ), $no_results_msg, 'disabled warning sign', 'zbsNoResults' ); // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
 					// any additional messages?
 					if ( isset( $this->messages ) && is_array( $this->messages ) && count( $this->messages ) > 0 ) {
 						foreach ( $this->messages as $message ) {
@@ -426,7 +433,7 @@ class zeroBSCRM_list {
 
 			<?php
 
-			$allowinlineedits = ( zeroBSCRM_getSetting( 'allowinlineedits' ) == '1' );
+			$allowinlineedits = ( zeroBSCRM_getSetting( 'allowinlineedits' ) === '1' );
 			$inlineEditStr    = array();
 			$columns          = array();
 
@@ -440,7 +447,7 @@ class zeroBSCRM_list {
 					// overrides
 
 					// Invoicing: Ref
-					if ( $this->objType == 'invoice' && $colKey == 'ref' ) {
+					if ( $this->objType === 'invoice' && $colKey === 'ref' ) {
 						$column_title = $zbs->settings->get( 'reflabel' );
 					}
 
@@ -639,12 +646,12 @@ class zeroBSCRM_list {
 				// make simplified
 				$simple_tags = array();
 				if ( is_array( $tags ) && count( $tags ) > 0 ) {
-					foreach ( $tags as $t ) {
-						$simple_tags[] = array(
-							'id'   => $t['id'],
-							'name' => $t['name'],
-							'slug' => $t['slug'],
-						);
+				foreach ( $tags as $t ) {
+					$simple_tags[] = array(
+						'id'   => $t['id'],
+						'name' => $t['name'],
+						'slug' => $t['slug'],
+					);
 					}
 				}
 
@@ -664,9 +671,9 @@ class zeroBSCRM_list {
 							// phpcs:disable WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase
 							global $zbsCustomerFields;
 							if ( is_array( $zbsCustomerFields['status'][3] ) ) {
-								echo wp_json_encode( $zbsCustomerFields['status'][3] );
+							echo wp_json_encode( $zbsCustomerFields['status'][3] );
 							} else {
-								echo '[]';
+							echo '[]';
 							}
 							// phpcs:enable WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase
 						?>
@@ -678,9 +685,9 @@ class zeroBSCRM_list {
 						// hardcoded customer perms atm
 						$possible_owners = zeroBS_getPossibleOwners( array( 'zerobs_admin', 'zerobs_customermgr' ), true );
 						if ( ! is_array( $possible_owners ) ) {
-							echo wp_json_encode( array() );
+						echo wp_json_encode( array() );
 						} else {
-							echo wp_json_encode( $possible_owners );
+						echo wp_json_encode( $possible_owners );
 						}
 
 					?>
