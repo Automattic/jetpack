0729799fc0839a32519e9f587533ed693e209b79
Addison Stavlo <Addison.Stavlo@Automattic.com>
2026-03-05 08:48:19 -0500

    fix translation comment

--- projects/plugins/jetpack/extensions/shared/memberships/subscribers-affirmation.js
+++ projects/plugins/jetpack/extensions/shared/memberships/subscribers-affirmation.js
@@ -220,8 +220,12 @@ function getAccessLevelLabel( accessLevel ) {
 	}
 
 	if ( tierName && base === 'paid_subscribers' ) {
-		// translators: %1$s: access level label (e.g. "paid subscribers"), %2$s: tier name (e.g. "Premium")
-		return sprintf( __( '%1$s (%2$s)', 'jetpack' ), label, tierName );
+		return sprintf(
+			// translators: %1$s: access level label (e.g. "paid subscribers"), %2$s: tier name (e.g. "Premium")
+			__( '%1$s (%2$s)', 'jetpack' ),
+			label,
+			tierName
+		);
 	}
 	return label;
 }
