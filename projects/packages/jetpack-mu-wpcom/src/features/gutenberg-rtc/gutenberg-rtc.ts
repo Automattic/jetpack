66c0695fb86a1be03c699889645d925ea379198d
arthur791004 <arthur.chu@automattic.com>
2026-03-06 22:02:49 +0900

    MU WPCOM: Implement PingHub provider (#47421)
    * MU WPCOM: Implement PingHub provider
    
    * Add changelog entries.
    
    * Support edit page
    
    * Update REMDME.md
    
    * Refactor to match the interface of HttpPollingProvider & PollingManager
    
    * Fix ts-expect-error
    
    * Address feedback
    
    * Fix the false disconnects issue after 30s without any changes
    
    * Fix a new peer cannot see us immediately

--- projects/packages/jetpack-mu-wpcom/src/features/gutenberg-rtc/gutenberg-rtc.ts
+++ projects/packages/jetpack-mu-wpcom/src/features/gutenberg-rtc/gutenberg-rtc.ts
@@ -1,9 +1,29 @@
 import { addFilter } from '@wordpress/hooks';
+import { createPingHubProvider } from './providers/pinghub';
 
-// Register providers (e.g. PingHub) supplied by the server, and disable HTTP polling by returning only this provider.
-( function register() {
-	if ( typeof window === 'undefined' ) {
-		return;
-	}
-	addFilter( 'sync.providers', 'wpcom/pinghub-provider', () => window.wpcomGutenbergRTC.providers );
-} )();
+/**
+ * Register providers (e.g. PingHub) supplied by the server, and disable HTTP polling by returning only this provider.
+ */
+function registerWpcomGutenbergProviders() {
+	const getProviders = () => {
+		if ( ! window.wpcomGutenbergRTC?.providers ) {
+			return [];
+		}
+
+		return window.wpcomGutenbergRTC.providers
+			.map( ( provider: string ) => {
+				switch ( provider ) {
+					case 'pinghub': {
+						return createPingHubProvider();
+					}
+					default:
+						return null;
+				}
+			} )
+			.filter( Boolean );
+	};
+
+	addFilter( 'sync.providers', 'wpcom/gutenberg-rtc-providers', () => getProviders() );
+}
+
+registerWpcomGutenbergProviders();
