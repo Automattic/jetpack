f85e3ada0d6ba81360cc0f17ccb5662777737cb4
Sorin Nunca <sorin.nunca@automattic.com>
2026-03-08 20:49:24 +0200

    Describe why we're enabling the option.

--- projects/plugins/boost/webpack.config.js
+++ projects/plugins/boost/webpack.config.js
@@ -30,6 +30,10 @@ module.exports = [
 		optimization: {
 			...jetpackWebpackConfig.optimization,
 			minimizer: [
+				/**
+				 * mck89/peast (used by wp i18n make-pot) can't correctly parse a ParenthesizedExpression generated from react-router.
+				 * Somehow, setting this causes that code from react-router to be tree-shaken out.
+				 */
 				jetpackWebpackConfig.TerserPlugin( {
 					terserOptions: {
 						enclose: true,
