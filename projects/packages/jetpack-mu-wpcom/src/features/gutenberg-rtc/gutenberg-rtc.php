5cf5e7983e312059dd867e0f295c9bb5e50c6f55
arthur791004 <arthur.chu@automattic.com>
2026-03-11 18:10:28 +0900

    MU WPCOM: Enable Gutenberg RTC for sites with the REAL_TIME_COLLABORATION feature (#47512)
    * MU WPCOM: Enable Gutenberg RTC feature for sites with the wpcom-gutenberg-rtc-enabled sticker
    
    * Add changelog entries.
    
    * Check WPCOM_Features::REAL_TIME_COLLABORATION feature
    
    * changelog
    
    * Add check for wpcom_site_has_feature
    
    ---------
    
    Co-authored-by: Changelog Bot <changelog-bot@automattic.com>

--- projects/packages/jetpack-mu-wpcom/src/features/gutenberg-rtc/gutenberg-rtc.php
+++ projects/packages/jetpack-mu-wpcom/src/features/gutenberg-rtc/gutenberg-rtc.php
@@ -16,7 +16,13 @@
  * and we are confident in proceeding with the rollout.
  */
 function wpcom_is_gutenberg_rtc_enabled() {
-	return apply_filters( 'wpcom_is_gutenberg_rtc_enabled', false );
+	$is_enabled = false;
+	if ( function_exists( 'wpcom_site_has_feature' ) && class_exists( 'WPCOM_Features' ) ) {
+		$blog_id    = get_wpcom_blog_id();
+		$is_enabled = wpcom_site_has_feature( WPCOM_Features::REAL_TIME_COLLABORATION, $blog_id );
+	}
+
+	return apply_filters( 'wpcom_is_gutenberg_rtc_enabled', $is_enabled );
 }
 
 /**
