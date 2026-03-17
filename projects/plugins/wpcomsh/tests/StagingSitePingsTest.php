bdc3f1888ee650237b3af51908b0ee6c0b161432
Volodymyr Makukha <nei.css@gmail.com>
2026-03-17 14:07:56 +0000

    test: fix

--- projects/plugins/wpcomsh/tests/StagingSitePingsTest.php
+++ projects/plugins/wpcomsh/tests/StagingSitePingsTest.php
@@ -7,6 +7,12 @@
 
 /**
  * Class StagingSitePingsTest.
+ *
+ * Each test runs in a separate process because wp_get_environment_type()
+ * caches its result in a static variable that cannot be reset between tests.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
  */
 class StagingSitePingsTest extends WP_UnitTestCase {
 	use \Automattic\Jetpack\PHPUnit\WP_UnitTestCase_Fix;
@@ -17,25 +23,6 @@ class StagingSitePingsTest extends WP_UnitTestCase {
 	public function test_outgoing_pings_disabled_in_staging() {
 		putenv( 'WP_ENVIRONMENT_TYPE=staging' );
 		$this->assertFalse( wpcomsh_disable_outgoing_pings_in_non_production_envs( true ) );
-		putenv( 'WP_ENVIRONMENT_TYPE' );
-	}
-
-	/**
-	 * Test that outgoing pings are allowed in development environment.
-	 */
-	public function test_outgoing_pings_allowed_in_development() {
-		putenv( 'WP_ENVIRONMENT_TYPE=development' );
-		$this->assertTrue( wpcomsh_disable_outgoing_pings_in_non_production_envs( true ) );
-		putenv( 'WP_ENVIRONMENT_TYPE' );
-	}
-
-	/**
-	 * Test that outgoing pings are allowed in local environment.
-	 */
-	public function test_outgoing_pings_allowed_in_local() {
-		putenv( 'WP_ENVIRONMENT_TYPE=local' );
-		$this->assertTrue( wpcomsh_disable_outgoing_pings_in_non_production_envs( true ) );
-		putenv( 'WP_ENVIRONMENT_TYPE' );
 	}
 
 	/**
@@ -44,7 +31,6 @@ class StagingSitePingsTest extends WP_UnitTestCase {
 	public function test_outgoing_pings_allowed_in_production() {
 		putenv( 'WP_ENVIRONMENT_TYPE=production' );
 		$this->assertTrue( wpcomsh_disable_outgoing_pings_in_non_production_envs( true ) );
-		putenv( 'WP_ENVIRONMENT_TYPE' );
 	}
 
 	/**
@@ -61,41 +47,6 @@ class StagingSitePingsTest extends WP_UnitTestCase {
 
 		$this->assertArrayNotHasKey( 'pingback.ping', $result );
 		$this->assertArrayHasKey( 'wp.getPosts', $result );
-		putenv( 'WP_ENVIRONMENT_TYPE' );
-	}
-
-	/**
-	 * Test that incoming pingback.ping method is kept in development environment.
-	 */
-	public function test_incoming_pings_allowed_in_development() {
-		putenv( 'WP_ENVIRONMENT_TYPE=development' );
-		$methods = array(
-			'pingback.ping' => 'some_callback',
-			'wp.getPosts'   => 'some_callback',
-		);
-
-		$result = wpcomsh_disable_incoming_pings_in_non_production_envs( $methods );
-
-		$this->assertArrayHasKey( 'pingback.ping', $result );
-		$this->assertArrayHasKey( 'wp.getPosts', $result );
-		putenv( 'WP_ENVIRONMENT_TYPE' );
-	}
-
-	/**
-	 * Test that incoming pingback.ping method is kept in local environment.
-	 */
-	public function test_incoming_pings_allowed_in_local() {
-		putenv( 'WP_ENVIRONMENT_TYPE=local' );
-		$methods = array(
-			'pingback.ping' => 'some_callback',
-			'wp.getPosts'   => 'some_callback',
-		);
-
-		$result = wpcomsh_disable_incoming_pings_in_non_production_envs( $methods );
-
-		$this->assertArrayHasKey( 'pingback.ping', $result );
-		$this->assertArrayHasKey( 'wp.getPosts', $result );
-		putenv( 'WP_ENVIRONMENT_TYPE' );
 	}
 
 	/**
@@ -112,21 +63,5 @@ class StagingSitePingsTest extends WP_UnitTestCase {
 
 		$this->assertArrayHasKey( 'pingback.ping', $result );
 		$this->assertArrayHasKey( 'wp.getPosts', $result );
-		putenv( 'WP_ENVIRONMENT_TYPE' );
-	}
-
-	/**
-	 * Test that incoming pings filter handles missing pingback.ping gracefully.
-	 */
-	public function test_incoming_pings_handles_missing_pingback_method() {
-		putenv( 'WP_ENVIRONMENT_TYPE=staging' );
-		$methods = array(
-			'wp.getPosts' => 'some_callback',
-		);
-
-		$result = wpcomsh_disable_incoming_pings_in_non_production_envs( $methods );
-
-		$this->assertArrayHasKey( 'wp.getPosts', $result );
-		putenv( 'WP_ENVIRONMENT_TYPE' );
 	}
 }
