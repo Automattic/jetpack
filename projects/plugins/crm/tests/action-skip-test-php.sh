c5159174d699e9f041c266543b80a5d43d8357b6
tbradsha <32492176+tbradsha@users.noreply.github.com>
2026-03-18 08:04:58 -0600

    CRM: Disable PHP tests on PHP 8.4.19 (#47653)

--- projects/plugins/crm/tests/action-skip-test-php.sh
+++ projects/plugins/crm/tests/action-skip-test-php.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
+# Skip tests on PHP 8.4.19 due to inconsistent Codeception timeouts on that version.
+# @todo: remove this when PHP 8.4 has a higher patch version: p1773618372849869-slack-C034JEXD1RD
+if php -r 'exit(PHP_VERSION === "8.4.19" ? 0 : 1);'; then
+	echo "Skipping Codeception tests on PHP 8.4.19 (current: $(php -r 'echo PHP_VERSION;'))"
+	exit 3
+fi
+
 # Skip tests on PHP 8.5+ due to dependency constraints (dompdf v2)
 if php -r 'exit(PHP_VERSION_ID >= 80500 ? 0 : 1);'; then
 	echo "Skipping tests on PHP 8.5+ (current: $(php -r 'echo PHP_VERSION;'))"
 	exit 3
 fi
-
-# Uncomment the below snippet to disable tests on WP trunk
-# if [[ "$WP_BRANCH" == 'trunk' ]]; then
-# 	echo "Codeception tests against WP trunk are temporarily disabled."
-# 	exit 3
-# fi
