312f4b3d0204a0ff235ad51a680169614979c976
Richard Ortiz <rcrd.ortiz@gmail.com>
2026-03-20 09:52:16 +0100

    Load RTC notices only on editor pages to avoid REST API test interference

--- projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
+++ projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
@@ -435,7 +435,6 @@ class Jetpack_Mu_Wpcom {
 		}
 
 		require_once __DIR__ . '/features/gutenberg-rtc/gutenberg-rtc.php';
-		require_once __DIR__ . '/features/gutenberg-rtc-notices/gutenberg-rtc-notices.php';
 		require_once __DIR__ . '/features/jetpack-global-styles/class-global-styles.php';
 		require_once __DIR__ . '/features/mailerlite/subscriber-popup.php';
 		require_once __DIR__ . '/features/wpcom-fse/wpcom-fse.php';
@@ -454,6 +453,7 @@ class Jetpack_Mu_Wpcom {
 			require_once __DIR__ . '/features/tags-education/tags-education.php';
 			require_once __DIR__ . '/features/wpcom-block-description-links/wpcom-block-description-links.php';
 			require_once __DIR__ . '/features/wpcom-block-editor-nux/class-wpcom-block-editor-nux.php';
+			require_once __DIR__ . '/features/gutenberg-rtc-notices/gutenberg-rtc-notices.php';
 			require_once __DIR__ . '/features/wpcom-blocks/a8c-posts-list/a8c-posts-list.php';
 			require_once __DIR__ . '/features/wpcom-blocks/event-countdown/event-countdown.php';
 			require_once __DIR__ . '/features/wpcom-blocks/timeline/timeline.php';
