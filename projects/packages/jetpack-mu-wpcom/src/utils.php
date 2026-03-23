47a98650aff39e4638a027c1ea1c7aa8b6de06bc
Dean Sas <dean.sas@automattic.com>
2026-03-23 11:44:50 +0000

    Newsletter: Enable wp-admin newsletter settings by default
    Change the jetpack_wp_admin_newsletter_settings_enabled filter default
    from false to true across all packages, and make the jetpack-mu-wpcom
    filter callback return true unconditionally (previously gated by
    newsletter-package-202603 sticker).
    
    This is Phase 1 of the rollout. Phase 2 (after next Jetpack self-hosted
    release) will remove the filter, its hook, and the old settings screen.

--- projects/packages/jetpack-mu-wpcom/src/utils.php
+++ projects/packages/jetpack-mu-wpcom/src/utils.php
@@ -215,18 +215,31 @@ function wpcom_has_blog_sticker( $blog_sticker, $blog_id ) {
 }
 
 /**
- * Enable the newsletter settings package for sites with the newsletter-package-202603 sticker.
+ * Enable the newsletter settings package on Simple sites.
  *
- * This allows opt-in testing of the newsletter settings package on wpcom infrastructure.
+ * On Simple sites, jetpack-mu-wpcom controls the full stack so we can enable
+ * immediately. On WoA/WoW sites, we explicitly return false because the
+ * autoloader may load the newer newsletter package (with default true) from
+ * jetpack-mu-wpcom before the Jetpack plugin's copy, causing the new settings
+ * to activate before the admin menu integration is ready.
+ *
+ * Phase 2 (after the Jetpack plugin ships with the new defaults) will update
+ * this callback and the wpcom-admin-menu.php filter defaults to enable the
+ * new settings on WoA/WoW sites.
  *
  * @param bool $enabled Whether the newsletter settings are enabled.
  * @return bool
  */
-function wpcom_maybe_enable_newsletter_settings( $enabled ) {
-	if ( $enabled ) {
+function wpcom_maybe_enable_newsletter_settings( $enabled ) { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
+	if ( ! class_exists( '\Automattic\Jetpack\Status\Host' ) ) {
 		return $enabled;
 	}
-
-	// Stickered sites (will always be simple, as we don't sync this sticker. WoW sites can just add their own mu-plugin/snippet)
-	return function_exists( 'has_blog_sticker' ) && has_blog_sticker( 'newsletter-package-202603' );
+	$host = new \Automattic\Jetpack\Status\Host();
+	if ( $host->is_wpcom_simple() ) {
+		return true;
+	}
+	if ( $host->is_woa_site() ) {
+		return false;
+	}
+	return true;
 }
