0c987fb3dc5489fd66664bdc1bcda7aee04933e7
Miguel Lezama <lezama@gmail.com>
2026-03-24 13:16:58 -0300

    jetpack-mu-wpcom: Initialize My Jetpack on Simple sites
    Add automattic/jetpack-my-jetpack as a dependency of the jetpack-mu-wpcom
    package and call Initializer::init() in load_wpcom_user_features(). This
    enables the My Jetpack dashboard and products page on WordPress.com Simple
    sites, where the full Jetpack plugin is not loaded.
    
    On Atomic sites, this is a no-op: wpcomsh's filter on
    jetpack_my_jetpack_should_initialize still controls visibility, and the
    did_action('my_jetpack_init') guard prevents double initialization when
    the full Jetpack plugin also calls init().

--- projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
+++ projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
@@ -420,6 +420,12 @@ class Jetpack_Mu_Wpcom {
 			\Automattic\Jetpack\Newsletter\Writing_Prompt_Widget::init();
 		}
 
+		// Initialize My Jetpack so the dashboard and products page are available
+		// on Simple sites (where the full Jetpack plugin doesn't run).
+		if ( class_exists( '\Automattic\Jetpack\My_Jetpack\Initializer' ) ) {
+			\Automattic\Jetpack\My_Jetpack\Initializer::init();
+		}
+
 		// Only load the Masterbar features on WoA sites.
 		if ( class_exists( '\Automattic\Jetpack\Status\Host' ) && ( new \Automattic\Jetpack\Status\Host() )->is_woa_site() ) {
 			// This is temporary. After we cleanup Masterbar on WPCOM we should load Masterbar for Simple sites too.
