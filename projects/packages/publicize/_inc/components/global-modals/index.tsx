964c75a1a97b48e10c3505ebaf055be5648e9215
Manzoor Wani <manzoorwani.jk@gmail.com>
2026-03-24 13:27:52 -0400

    Social: Remove unused components after UNIFIED_UI_V1 flag removal
    Remove components that are no longer imported after making the unified UI
    the default: EnhancedFeaturesNudge, SocialPostModal, MediaSection (old),
    SocialImageGeneratorPanel, ShareStatusModal, ShareList, and usePostCanUseSig.

--- projects/packages/publicize/_inc/components/global-modals/index.tsx
+++ projects/packages/publicize/_inc/components/global-modals/index.tsx
@@ -1,11 +1,9 @@
 import { ThemedConnectionsModal as ManageConnectionsModal } from '../manage-connections-modal';
-import { ThemedShareStatusModal as ShareStatusModal } from '../share-status';
 import { UnifiedModal } from '../unified-modal';
 
 export const GlobalModals = () => {
 	return (
 		<>
-			<ShareStatusModal />
 			<UnifiedModal />
 			<ManageConnectionsModal />
 		</>
