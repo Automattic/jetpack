e1aca09fb9b0fb6c8e44c2440674f66c1f6c9f5c
Bongnam <dufresnesteven@gmail.com>
2026-03-25 12:00:17 +0900

    Fix linter issues, with the help of the linter :)

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-hotfixes/wpcom-hotfixes.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-hotfixes/wpcom-hotfixes.php
@@ -8,7 +8,7 @@
 add_action(
 	'wp_default_scripts',
 	function ( $scripts ) {
-		foreach ( [ 'react', 'react-dom', 'react-jsx-runtime' ] as $handle ) {
+		foreach ( array( 'react', 'react-dom', 'react-jsx-runtime' ) as $handle ) {
 			if ( isset( $scripts->registered[ $handle ] ) ) {
 				$scripts->registered[ $handle ]->ver = '18.3.1';
 			}
