e2c1ead86a9194ec369eb71f48521e22b16ad0a7
Bongnam <dufresnesteven@gmail.com>
2026-03-25 11:04:12 +0900

    Add a hotfix.

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-hotfixes/wpcom-hotfixes.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-hotfixes/wpcom-hotfixes.php
@@ -4,3 +4,11 @@
  *
  * @package automattic/jetpack-mu-wpcom
  */
+
+add_action( 'wp_default_scripts', function( $scripts ) {
+	foreach ( [ 'react', 'react-dom', 'react-jsx-runtime' ] as $handle ) {
+		if ( isset( $scripts->registered[ $handle ] ) ) {
+			$scripts->registered[ $handle ]->ver = '18.3.1';
+		}
+	}
+}, 20 ); // priority 20 runs after Gutenberg's priority 10
