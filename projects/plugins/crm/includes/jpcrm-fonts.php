8ad2961fce04dbbc5f21cfe603fae89c9046e0b1
tbradsha <32492176+tbradsha@users.noreply.github.com>
2026-03-25 09:01:00 -0600

    Add back unused global
    This was removed as part of cleanup, but triggered tons of Phan complaints (some valid, some not so valid). Doesn't hurt to leave it here for now.

--- projects/plugins/crm/includes/jpcrm-fonts.php
+++ projects/plugins/crm/includes/jpcrm-fonts.php
@@ -9,6 +9,8 @@
 
 use FontLib\Font;
 
+global $zbs; // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable -- Removing this triggers lots of Phan complains.
+
 defined( 'ZEROBSCRM_PATH' ) || exit( 0 );
 
 /*
