4b488d52cc10436b6543d0774b80d62eafc07d3c
Gergely Juhasz <gergely.juhasz@automattic.com>
2026-04-02 11:44:24 +0800

    Create new file for service icon button

--- projects/packages/publicize/_inc/components/link-preview-panel/index.tsx
+++ projects/packages/publicize/_inc/components/link-preview-panel/index.tsx
@@ -5,47 +5,12 @@
  */
 
 import { Button, PanelBody } from '@wordpress/components';
-import { __, _x, sprintf } from '@wordpress/i18n';
+import { __, _x } from '@wordpress/i18n';
 import { useCallback, useState } from 'react';
 import { LinkPreviewModal, usePreviewTabs } from '../../exports/link-preview';
 import { LinkPreviewPlatform } from '../../exports/link-preview/types';
-import { PreviewTab } from '../../exports/link-preview/use-preview-tabs';
+import { ServiceIconButton } from './service-icon-button';
 import styles from './styles.module.scss';
-
-/**
- * A single social service icon button that opens the preview modal for that service.
- *
- * @param {object}   props         - Component props.
- * @param {object}   props.tab     - The preview tab definition.
- * @param {Function} props.onClick - Callback when the button is clicked, receives the tab name.
- * @return The service icon button component.
- */
-function ServiceIconButton( {
-	tab,
-	onClick,
-}: {
-	tab: PreviewTab;
-	onClick: ( name: LinkPreviewPlatform ) => void;
-} ) {
-	const handleClick = useCallback( () => {
-		onClick( tab.name );
-	}, [ onClick, tab.name ] );
-
-	return (
-		<Button
-			className={ styles[ 'social-icon-button' ] }
-			label={ sprintf(
-				/* translators: %s is the name of a social media service, e.g. "Facebook" */
-				__( 'Preview on %s', 'jetpack-publicize-pkg' ),
-				tab.title
-			) }
-			showTooltip
-			onClick={ handleClick }
-		>
-			{ typeof tab.icon === 'function' ? <tab.icon /> : tab.icon }
-		</Button>
-	);
-}
 
 /**
  * Display the link previews panel, showing available services and a trigger to open the preview modal.
