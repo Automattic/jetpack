bcdf1a3236eaf74bffac1e792ed13ca58b350181
Mikael Korpela <mikael.korpela@automattic.com>
2026-04-06 20:20:54 +0300

    Sidebar upsell: add "seen" event

--- projects/packages/admin-ui/src/admin-ui-upgrade-menu-tracking.js
+++ projects/packages/admin-ui/src/admin-ui-upgrade-menu-tracking.js
@@ -23,6 +23,33 @@ document.addEventListener( 'DOMContentLoaded', () => {
 		window.analytics.initialize( config.tracksUserData?.userid, config.tracksUserData?.username );
 	}
 
+	const parentTopLevelMenu = item.closest(
+		'#toplevel_page_jetpack, #toplevel_page_jetpack-network'
+	);
+
+	const isParentMenuOpen =
+		parentTopLevelMenu &&
+		parentTopLevelMenu.classList.contains( 'wp-menu-open' ) &&
+		parentTopLevelMenu.classList.contains( 'wp-has-current-submenu' );
+
+	if ( isParentMenuOpen ) {
+		// Only record the "seen" event if the parent menu is open.
+		window.analytics?.tracks?.recordEvent(
+			'jetpack_sidebar_free_upgrade_seen',
+			config.tracksEventData
+		);
+	} else {
+		// Only record the "seen" event if the parent menu was opened, and record it once only.
+		const onHover = function () {
+			window.analytics?.tracks?.recordEvent(
+				'jetpack_sidebar_free_upgrade_seen',
+				config.tracksEventData
+			);
+			parentTopLevelMenu.removeEventListener( 'hover', onHover );
+		};
+		parentTopLevelMenu.addEventListener( 'hover', onHover );
+	}
+
 	item.addEventListener( 'click', function () {
 		window.analytics?.tracks?.recordEvent(
 			'jetpack_sidebar_free_upgrade_click',
