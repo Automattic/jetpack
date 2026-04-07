27583611db31efb8d7b90cac78ee18f6748eac2d
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2026-04-07 15:37:52 +0200

    Remove unnecessary QueryClient

--- projects/packages/jetpack-mu-wpcom/src/features/launch-button/index.js
+++ projects/packages/jetpack-mu-wpcom/src/features/launch-button/index.js
@@ -20,21 +20,17 @@ async function renderLaunchButton() {
 		return;
 	}
 
-	const { QueryClient, QueryClientProvider } = await import( '@tanstack/react-query' );
 	const { createRoot } = await import( 'react-dom/client' );
 	const { LaunchButton } = await import( './launch-button' );
 
-	const queryClient = new QueryClient();
 	const root = createRoot( launchButton );
 	root.render(
-		<QueryClientProvider client={ queryClient }>
-			<LaunchButton
-				onCelebrationModalClose={ () => {
-					root.unmount();
-					launchButton.remove();
-				} }
-			/>
-		</QueryClientProvider>
+		<LaunchButton
+			onCelebrationModalClose={ () => {
+				root.unmount();
+				launchButton.remove();
+			} }
+		/>
 	);
 }
 
