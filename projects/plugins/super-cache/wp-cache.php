6c91920a991675c1a296c90b9c18a31b7d460b1f
LeTraceurSnork <le_traceur_snork@mail.ru>
2026-04-07 23:25:48 +0500

    feat: check supercache_last_cached option for array type (#46814)
    Co-authored-by: Илья Салигжанов <saligzhanov.i@spectrumdata.ru>
    Co-authored-by: Brandon Kraft <public@brandonkraft.com>

--- projects/plugins/super-cache/wp-cache.php
+++ projects/plugins/super-cache/wp-cache.php
@@ -1376,16 +1376,26 @@ table.wpsc-settings-table {
 		}
 		?>
 		<p><?php printf( __( 'Cached pages since %1$s : <strong>%2$s</strong>', 'wp-super-cache' ), date( 'M j, Y', $start_date ), number_format( get_option( 'wpsupercache_count' ) ) ); ?></p>
+		<?php
+		$supercache_last_cached_option = get_option( 'supercache_last_cached' );
+		if ( is_array( $supercache_last_cached_option ) ) {
+			?>
 		<p><?php _e( 'Newest Cached Pages:', 'wp-super-cache' ); ?><ol>
 			<?php
-			foreach ( array_reverse( (array) get_option( 'supercache_last_cached' ) ) as $url ) {
-				$since = time() - strtotime( $url['date'] );
-				echo "<li><a title='" . sprintf( esc_html__( 'Cached %s seconds ago', 'wp-super-cache' ), (int) $since ) . "' href='" . site_url( $url['url'] ) . "'>" . substr( $url['url'], 0, 20 ) . "</a></li>\n";
+			foreach ( array_reverse( $supercache_last_cached_option ) as $url ) {
+				if ( ! is_array( $url ) ) {
+					continue;
+				}
+				$since = time() - strtotime( $url['date'] ?? '' );
+				// translators: %s is the number of seconds since the page was cached.
+				echo "<li><a title='" . sprintf( esc_html__( 'Cached %s seconds ago', 'wp-super-cache' ), (int) $since ) . "' href='" . esc_url( site_url( $url['url'] ?? '' ) ) . "'>" . esc_html( substr( $url['url'] ?? '', 0, 20 ) ) . "</a></li>\n";
 			}
 			?>
 			</ol>
 			<small><?php esc_html_e( '(may not always be accurate on busy sites)', 'wp-super-cache' ); ?></small>
-		</p><?php
+		</p>
+			<?php
+		}
 	} elseif ( false == get_option( 'wpsupercache_start' ) ) {
 			update_option( 'wpsupercache_start', time() );
 			update_option( 'wpsupercache_count', 0 );
