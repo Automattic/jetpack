453f28076128bbf94a47fdffca22a9949686c24f
tbradsha <32492176+tbradsha@users.noreply.github.com>
2026-04-10 20:10:42 -0600

    Backport automattic-for-agencies-client 0.8.0 changes (#48060)

--- projects/packages/plugin-deactivation/src/class-deactivation-handler.php
+++ projects/packages/plugin-deactivation/src/class-deactivation-handler.php
@@ -21,7 +21,7 @@ class Deactivation_Handler {
 	 *
 	 * @var string
 	 */
-	const PACKAGE_VERSION = '0.3.25';
+	const PACKAGE_VERSION = '0.3.26';
 
 	/**
 	 * Slug of the plugin to intercept deactivation for.
