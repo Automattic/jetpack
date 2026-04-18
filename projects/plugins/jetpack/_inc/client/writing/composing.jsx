2ee13eb487d4a036fd2f52e8c8f837e428d21549
Darren Ethier <darren@roughsmootheng.in>
2026-04-18 15:23:19 -0400

    Componentry: Stack wrapper for Badge spacing; drop newsletter Chip
    - composing.jsx: wrap link + Badge in `@wordpress/ui` Stack with
      `gap="sm"` (8px design token) to restore the 8px margin the old Chip
      SCSS carried.
    - email-settings.jsx: drop the Chip entirely (no Badge replacement).
      This FormLegend is inside the legacy Newsletter settings tab which
      is slated for removal; no point wiring new UI into code on its way
      out.

--- projects/plugins/jetpack/_inc/client/writing/composing.jsx
+++ projects/plugins/jetpack/_inc/client/writing/composing.jsx
@@ -1,6 +1,6 @@
 import { getRedirectUrl } from '@automattic/jetpack-components';
 import { __, _x } from '@wordpress/i18n';
-import { Badge } from '@wordpress/ui';
+import { Badge, Stack } from '@wordpress/ui';
 import { Component } from 'react';
 import { connect } from 'react-redux';
 import CompactCard from 'components/card/compact';
@@ -207,11 +207,13 @@ export class Composing extends Component {
 			),
 			aiAssistantLink = (
 				<CompactCard className="jp-settings-card__configure-link">
-					<a href={ `${ this.props.siteAdminUrl }admin.php?page=my-jetpack#/jetpack-ai` }>
-						{ __( 'Learn more about all Jetpack AI features', 'jetpack' ) }
-					</a>
-					{ /* TODO: remove this Badge once it's no longer "new" */ }
-					<Badge intent="stable">{ __( 'New', 'jetpack' ) }</Badge>
+					<Stack align="center" gap="sm">
+						<a href={ `${ this.props.siteAdminUrl }admin.php?page=my-jetpack#/jetpack-ai` }>
+							{ __( 'Learn more about all Jetpack AI features', 'jetpack' ) }
+						</a>
+						{ /* TODO: remove this Badge once it's no longer "new" */ }
+						<Badge intent="stable">{ __( 'New', 'jetpack' ) }</Badge>
+					</Stack>
 				</CompactCard>
 			);
 
