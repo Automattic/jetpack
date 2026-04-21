af22d6860f01036a70b1968ca16e6cbf2ad3eb77
tbradsha <32492176+tbradsha@users.noreply.github.com>
2026-04-21 11:24:37 -0600

    Reader Link: Defer Reader link init and take 2 of loading it only when masterbar is visible (#48145)
    * Defer Reader_Link init to wp_loaded
    
    * Add changelog
    
    * Re-apply #48100: only load Reader Link when admin bar is visible
    
    Restore the changes from #48100 (reverted in #48143). The revert was
    needed because Reader_Link::init() ran too early for is_admin_bar_showing().
    This branch already defers init to wp_loaded, so the early bail-out is
    safe to reinstate here.
    
    ---------
    
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-admin-bar/wpcom-admin-bar.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-admin-bar/wpcom-admin-bar.php
@@ -10,7 +10,6 @@
 use Automattic\Jetpack\Connection\Urls;
 use Automattic\Jetpack\Current_Plan;
 use Automattic\Jetpack\Jetpack_Mu_Wpcom;
-use Automattic\Jetpack\Newsletter\Reader_Link;
 use Automattic\Jetpack\Status;
 
 // The $icon-color variable for admin color schemes.
@@ -276,7 +275,7 @@ function wpcom_add_shopping_cart( $wp_admin_bar ) {
 add_action( 'admin_bar_menu', 'wpcom_add_shopping_cart', 11 );
 
 // Add the reader icon to the admin bar before the help center icon.
-Reader_Link::init();
+add_action( 'wp_loaded', array( 'Automattic\Jetpack\Newsletter\Reader_Link', 'init' ) );
 
 /**
  * Points the "Edit Profile" and "Howdy,..." to /me if the user is not member of the blog.
