8beba54a8a2c4f154318e6439953ac220a2f0178
tbradsha <32492176+tbradsha@users.noreply.github.com>
2026-04-28 12:21:52 -0600

    Update messaging

--- projects/plugins/crm/includes/ZeroBSCRM.MetaBoxes3.Contacts.php
+++ projects/plugins/crm/includes/ZeroBSCRM.MetaBoxes3.Contacts.php
@@ -1580,7 +1580,7 @@ class zeroBS__Metabox_ContactPortal extends zeroBS__Metabox {
 				} else {
 
 					// explainer - rarely shown
-					echo '<p style="font-size: 0.9em;margin-top: 0.5em;">' . esc_html__( 'The WordPress user has a role other than CRM Contact. They will need to reset their password via the WP login page.', 'zero-bs-crm' ) . '</p>';
+					echo '<p style="font-size: 0.9em;margin-top: 0.5em;">' . esc_html__( 'You cannot edit this user. Password resets must be done via the WP login page.', 'zero-bs-crm' ) . '</p>';
 
 				}
 
