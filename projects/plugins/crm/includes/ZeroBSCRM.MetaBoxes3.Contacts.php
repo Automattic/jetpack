46411a909cd63753a0725d47f856e553f4cdd007
tbradsha <32492176+tbradsha@users.noreply.github.com>
2026-04-28 13:30:33 -0600

    Delete public files that weren't privatized successfully

--- projects/plugins/crm/includes/ZeroBSCRM.MetaBoxes3.Contacts.php
+++ projects/plugins/crm/includes/ZeroBSCRM.MetaBoxes3.Contacts.php
@@ -1078,7 +1078,12 @@ class zeroBS__Metabox_ContactCustomFiles extends zeroBS__Metabox {
 
 								} else {
 
-									// couldn't move to store, leave in uploaded for now :)
+									// Could not move the file into private CRM storage.
+									// Delete the public copy left by `wp_upload_bits()`.
+									if ( file_exists( $upload['file'] ) ) {
+										unlink( $upload['file'] );
+									}
+									wp_die( 'There was an error storing your uploaded file.' );
 
 								}
 
@@ -1425,7 +1430,12 @@ class zeroBS__Metabox_ContactFiles extends zeroBS__Metabox {
 
 					} else {
 
-						// couldn't move to store, leave in uploaded for now :)
+						// Could not move the file into private CRM storage.
+						// Delete the public copy left by `wp_upload_bits()`.
+						if ( file_exists( $upload['file'] ) ) {
+							unlink( $upload['file'] );
+						}
+						wp_die( 'There was an error storing your uploaded file.' );
 
 					}
 
