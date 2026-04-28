a53916467750528b9c97d5c8930f3ca127764570
Greg Ichneumon Brown <greg@automattic.com>
2026-04-28 17:25:07 -0600

    Search admin: expose aiAnswersEnabled in overlay initial state

--- projects/packages/search/src/class-helper.php
+++ projects/packages/search/src/class-helper.php
@@ -971,6 +971,7 @@ class Helper {
 			 * @param bool $disable_tracking Whether to disable tracking. Default false.
 			 */
 			'disableTracking'             => self::is_tracking_disabled() || apply_filters( 'jetpack_instant_search_disable_tracking', false ),
+			'aiAnswersEnabled'            => AI_Answers::is_enabled(),
 		);
 
 		// AI Answers: embed site-level hourly HMAC token for anonymous visitors.
