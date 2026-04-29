f8cf99bb442adac6e07b1900b9c46efe2ebbd34c
Tony Arcangelini <tony@arcangelini.com>
2026-04-29 09:02:52 +0200

    Paywall: fix comp subscribers blocked by tier gate

--- projects/plugins/jetpack/extensions/blocks/premium-content/_inc/subscription-service/class-abstract-token-subscription-service.php
+++ projects/plugins/jetpack/extensions/blocks/premium-content/_inc/subscription-service/class-abstract-token-subscription-service.php
@@ -427,7 +427,13 @@ abstract class Abstract_Token_Subscription_Service implements Subscription_Servi
 
 		foreach ( $user_abbreviated_subscriptions as $subscription_plan_id => $details ) {
 			$details = (array) $details;
-			$end     = is_int( $details['end_date'] ) ? $details['end_date'] : strtotime( $details['end_date'] );
+
+			// Comp subscriptions are administrative grants and bypass tier price gating.
+			if ( ! empty( $details['is_comp'] ) ) {
+				return false;
+			}
+
+			$end = is_int( $details['end_date'] ) ? $details['end_date'] : strtotime( $details['end_date'] );
 			if ( $end < time() ) {
 				// subscription not active anymore
 				continue;
@@ -669,6 +675,9 @@ abstract class Abstract_Token_Subscription_Service implements Subscription_Servi
 			) {
 				$subscriptions[ $subscription['product_id'] ]           = new \stdClass();
 				$subscriptions[ $subscription['product_id'] ]->end_date = empty( $subscription['end_date'] ) ? ( time() + 365 * 24 * 3600 ) : $subscription['end_date'];
+				if ( ! empty( $subscription['is_comp'] ) ) {
+					$subscriptions[ $subscription['product_id'] ]->is_comp = true;
+				}
 			}
 		}
 		return $subscriptions;
