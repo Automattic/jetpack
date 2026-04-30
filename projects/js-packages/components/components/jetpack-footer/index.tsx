0cb39bb43e1d3ed30acd34ca0a9a0fa7400686fc
Mikael Korpela <mikael.korpela@automattic.com>
2026-04-30 22:23:54 +0300

    Update build-optional-menu-items.test.ts

--- projects/js-packages/components/components/jetpack-footer/index.tsx
+++ projects/js-packages/components/components/jetpack-footer/index.tsx
@@ -77,7 +77,6 @@ const JetpackFooter: FC< JetpackFooterProps > = ( { className, menu, ...otherPro
 											title={ item.title || '' }
 											onClick={ item.onClick || undefined }
 											onKeyDown={ item.onKeyDown || undefined }
-											role="button"
 										/>
 									) : (
 										<Link
