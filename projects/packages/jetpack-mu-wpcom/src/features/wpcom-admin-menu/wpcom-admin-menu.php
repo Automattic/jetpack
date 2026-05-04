1514e321a9a6c79ae47eaa133ea12731abb815d5
Tony Arcangelini <tony@arcangelini.com>
2026-05-04 22:51:48 +0200

    Podcast: integrate as a hard dep of jetpack-mu-wpcom; drop fallback redirect

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-admin-menu/wpcom-admin-menu.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-admin-menu/wpcom-admin-menu.php
@@ -402,21 +402,7 @@ function wpcom_add_jetpack_submenu() {
 	}
 
 	// Jetpack > Podcast
-	// When the Podcast package is loaded (Jetpack ships with podcasting in wp-admin),
-	// register the in-admin SPA. Otherwise fall back to the legacy Calypso redirect
-	// so older Jetpack deploys keep working during the rollout.
-	if ( class_exists( '\Automattic\Jetpack\Podcast\Settings' ) ) {
-		\Automattic\Jetpack\Podcast\Settings::add_wp_admin_submenu();
-	} else {
-		add_submenu_page(
-			'jetpack',
-			__( 'Podcasting', 'jetpack-mu-wpcom' ),
-			__( 'Podcasting', 'jetpack-mu-wpcom' ),
-			'manage_options',
-			'https://wordpress.com/settings/podcasting/' . $domain,
-			null // @phan-suppress-current-line PhanTypeMismatchArgumentProbablyReal -- Core should ideally document null for no-callback arg. https://core.trac.wordpress.org/ticket/52539.
-		);
-	}
+	\Automattic\Jetpack\Podcast\Settings::add_wp_admin_submenu();
 
 	if ( $is_simple_site ) {
 		// Jetpack > Newsletter.
