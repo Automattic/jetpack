54d8750482f9e939bf66ce948d6b8f4b30aad858
Mikael Korpela <mikael.korpela@automattic.com>
2026-05-05 10:43:52 +0300

    Remove translation wrappers from Boost, Protect, and CRM product names.
    Product names (Boost, Protect, CRM) should not be translated. Remove _x()
    and __() wrappers from menu labels and page titles where the product name
    is the primary content, and add comments explaining this to future contributors.

--- projects/plugins/boost/app/admin/class-admin.php
+++ projects/plugins/boost/app/admin/class-admin.php
@@ -43,13 +43,13 @@ class Admin {
 		 * @since   1.0.0
 		 */
 		$total_problems = apply_filters( 'jetpack_boost_total_problem_count', 0 );
-		$menu_label     = _x( 'Boost', 'The Jetpack Boost product name, without the Jetpack prefix', 'jetpack-boost' );
+		$menu_label     = 'Boost'; // "Boost" is a product name, do not translate.
 		if ( $total_problems ) {
 			$menu_label .= sprintf( ' <span class="menu-counter count-%d"><span class="count">%d</span></span>', $total_problems, $total_problems );
 		}
 
 		$page_suffix = Admin_Menu::add_menu(
-			__( 'Jetpack Boost - Settings', 'jetpack-boost' ),
+			'Jetpack Boost - Settings', // "Jetpack Boost" is a product name, do not translate.
 			$menu_label,
 			'manage_options',
 			JETPACK_BOOST_SLUG,
