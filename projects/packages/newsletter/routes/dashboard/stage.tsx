465a203b1fca86613c28ae4316ead8bfbe6d9b0a
Filipe Varela <filipe@automattic.com>
2026-05-06 10:58:15 -0400

    Newsletter: wrap the Stage in a QueryClientProvider for the data layer

--- projects/packages/newsletter/routes/dashboard/stage.tsx
+++ projects/packages/newsletter/routes/dashboard/stage.tsx
@@ -1,9 +1,13 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import { Page } from '@wordpress/admin-ui';
+import { queryClient } from '../../_inc/subscribers/lib/query-client';
 
 const Stage = () => {
 	return (
-		// "Newsletter" is a product name, do not translate.
-		<Page title="Newsletter" />
+		<QueryClientProvider client={ queryClient }>
+			{ /* "Newsletter" is a product name, do not translate. */ }
+			<Page title="Newsletter" />
+		</QueryClientProvider>
 	);
 };
 
