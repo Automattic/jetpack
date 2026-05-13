905235785e0d846ecc5581327b0e42357a38faf3
Jarda Snajdr <jsnajdr@gmail.com>
2026-05-13 12:54:26 +0200

    babel-plugin-replace-textdomain: detect aliased imports from the i18n ESM module (#48355)
    * webpack-config: remove generateI18nVariants
    
    * babel-plugin-replace-textdomain: look up __ bindings in scope chain
    
    * changelog
    
    * Don't look up alias for member expressions
    
    * Make the i18nModule configurable

--- projects/js-packages/webpack-config/src/webpack/bundled-wp-pkgs-transpile-rules.js
+++ projects/js-packages/webpack-config/src/webpack/bundled-wp-pkgs-transpile-rules.js
@@ -2,25 +2,6 @@ const loadTextDomainFromComposerJson = require( './load-textdomain-from-composer
 const TranspileRule = require( './transpile-rule.js' );
 
 /**
- * Generate i18n function variants for `@automattic/babel-plugin-replace-textdomain`.
- *
- * The `@wordpress/dataviews` currently uses the i18n functions under a variety of aliases,
- * which makes it a pain to add the proper textdomain. This function generates an object
- * with the base function and 99 more variants as keys.
- *
- * @param {string} baseFn - Base function name (e.g., '__', '_x', '_n')
- * @param {number} value  - Textdomain argument position (1-based index)
- * @return {object} Object mapping function names to textdomain positions
- */
-const generateI18nVariants = ( baseFn, value ) =>
-	Object.fromEntries(
-		Array.from( { length: 100 }, ( _, i ) => [
-			`${ baseFn }${ i || '' }`, // empty suffix for 0
-			value,
-		] )
-	);
-
-/**
  * Provide transpile rules for bundled `@wordpress/*` packages.
  *
  * If bundled packages contain `@wordpress/i18n` calls, the i18n doesn't actually work due to
@@ -56,17 +37,7 @@ const BundledWpPkgsTranspileRules = ( options = {} ) => {
 		babelOpts: {
 			configFile: false,
 			plugins: [
-				[
-					require.resolve( '@automattic/babel-plugin-replace-textdomain' ),
-					{
-						textdomain,
-						functions: {
-							...generateI18nVariants( '__', 1 ),
-							...generateI18nVariants( '_x', 2 ),
-							...generateI18nVariants( '_n', 3 ),
-						},
-					},
-				],
+				[ require.resolve( '@automattic/babel-plugin-replace-textdomain' ), { textdomain } ],
 			],
 		},
 	} );
