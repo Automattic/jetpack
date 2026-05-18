ceea8ec6b6e243eeb27b6cbd718af1d017d32de0
Tony Arcangelini <tony@arcangelini.com>
2026-05-18 16:42:27 +0200

    wpcomsh: gate without depending on Podcast class autoload

--- projects/plugins/wpcomsh/wpcomsh.php
+++ projects/plugins/wpcomsh/wpcomsh.php
@@ -122,15 +122,18 @@ if ( is_readable( $jetpack_autoloader ) ) {
 
 	return;
 }
-// Load the legacy at-pressable-podcasting plugin only when the new
-// jetpack-podcast package is not taking over the feature. See
-// Automattic\Jetpack\Podcast\Podcast::is_enabled() for the gate.
-if (
-	! class_exists( Automattic\Jetpack\Podcast\Podcast::class )
-	|| ! Automattic\Jetpack\Podcast\Podcast::is_enabled()
-) {
+// Mirrors Automattic\Jetpack\Podcast\Podcast::is_enabled()'s
+// proxied-request default without depending on the Podcast class being
+// resolvable through the autoloader at this point in bootstrap.
+$wpcomsh_podcast_untangle_default = (
+	( function_exists( 'wpcom_is_proxied_request' ) && wpcom_is_proxied_request() )
+	|| ! empty( $_SERVER['A8C_PROXIED_REQUEST'] )
+	|| ( defined( 'A8C_PROXIED_REQUEST' ) && A8C_PROXIED_REQUEST )
+);
+if ( ! apply_filters( 'jetpack_podcast_untangle', $wpcomsh_podcast_untangle_default ) ) {
 	require_once __DIR__ . '/vendor/automattic/at-pressable-podcasting/podcasting.php';
 }
+unset( $wpcomsh_podcast_untangle_default );
 require_once __DIR__ . '/vendor/automattic/custom-fonts/custom-fonts.php';
 require_once __DIR__ . '/vendor/automattic/custom-fonts-typekit/custom-fonts-typekit.php';
 require_once __DIR__ . '/vendor/automattic/text-media-widget-styles/text-media-widget-styles.php';
