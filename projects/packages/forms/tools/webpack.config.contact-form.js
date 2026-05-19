0b7b80ea1c5016d92ad0f3c5ff6910cc97da0545
Christian Gastrell <cgastrell@gmail.com>
2026-05-19 12:03:56 -0300

    Revert "Forms: Remove dead legacy grunion assets (#47609)"
    This reverts commit e90729df71f669eeaa3f6d8feb772b19fb8e6205.

--- projects/packages/forms/tools/webpack.config.contact-form.js
+++ projects/packages/forms/tools/webpack.config.contact-form.js
@@ -113,7 +113,9 @@ const RenamerPlugin = {
 				},
 				assets => {
 					for ( const [ name, asset ] of Object.entries( assets ) ) {
-						const m = name.match( /^(css\/(?:grunion|editor-ui))((?:\.min)?)\.rtl\.css$/ );
+						const m = name.match(
+							/^(css\/(?:grunion|grunion-admin|editor-ui))((?:\.min)?)\.rtl\.css$/
+						);
 						if ( m ) {
 							delete assets[ name ];
 							assets[ `${ m[ 1 ] }-rtl${ m[ 2 ] }.css` ] = asset;
