0514787472e42aa40b8249155cec22d7925de200
Mikael Korpela <mikael.korpela@automattic.com>
2026-05-21 17:05:40 +0300

    Add chart background

---
+++ projects/packages/stats-admin/widgets/total-subscribers/constants.ts
@@ -0,0 +1,8 @@
+/**
+ * Placeholder subscriber trend for the decorative background chart.
+ * TODO: Replace with real time-series data when available.
+ */
+export const MOCK_SUBSCRIBER_TREND = [ 8, 10, 9, 12, 14, 16, 15, 19, 22, 24, 28, 32, 36, 40 ];
+
+/** Jetpack Green 40 */
+export const JETPACK_GREEN = '#069e08';
