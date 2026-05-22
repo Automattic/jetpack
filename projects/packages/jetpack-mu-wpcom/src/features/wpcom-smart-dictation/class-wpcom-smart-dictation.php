5753a3f0919ccc2e217eed580915a62c88e14ed2
Omar Alshaker <omar@omaralshaker.com>
2026-05-22 14:19:58 +0200

    Release Smart Dictation to all paid English-locale users (#49095)

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-smart-dictation/class-wpcom-smart-dictation.php
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-smart-dictation/class-wpcom-smart-dictation.php
@@ -16,7 +16,7 @@ class WPCOM_Smart_Dictation {
 	/**
 	 * Percentage of paid users eligible for Smart Dictation.
 	 */
-	private const PAID_USER_ROLLOUT_PERCENTAGE = 50;
+	private const PAID_USER_ROLLOUT_PERCENTAGE = 100;
 
 	/**
 	 * WPCOM_Smart_Dictation constructor.
