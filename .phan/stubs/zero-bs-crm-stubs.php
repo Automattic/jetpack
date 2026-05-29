b8252cb95f853b6885c23fcfdf1c9ea13f30351d
tbradsha <32492176+tbradsha@users.noreply.github.com>
2026-05-29 12:51:43 -0600

    Remove CRM from monorepo (#49251)
    * Remove the CRM project folder.
    * Remove files from the PHPCS exclusion list.
    * Remove Stylelint exceptions.
    * Remove from Jetpack Live Branches userscript.
    * Remove from GitHub issue templates.
    * Remove from `repo-gardening` action.
    * Remove from `jetpack docker phpunit` command.
    * Update references in various comments and documentation.
    * Create Phan stubs for use in the Jetpack plugin.
    
    ---------
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>

---
+++ .phan/stubs/zero-bs-crm-stubs.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Stubs automatically generated from Jetpack CRM 6.8.1
+ * using the definition file `tools/stubs/zero-bs-crm-stub-defs.php` in the Jetpack monorepo.
+ *
+ * Do not edit this directly! Run tools/stubs/update-stubs.sh to regenerate it.
+ */
+
+// } MS - 27th Feb 2019. This is bugged.
+// } function to detect if a specific extension is installed
+// } ONLY WORKS after plugins_loaded
+// } see https://codex.wordpress.org/Plugin_API/Action_Reference
+/**
+ * @phan-return mixed Dummy doc for stub.
+ */
+function zeroBSCRM_isExtensionInstalled($extKey = '')
+{
+}
+/**
+ * @phan-return mixed Dummy doc for stub.
+ */
+function zeroBSCRM_extension_install_jetpackforms()
+{
+}
