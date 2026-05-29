a64f95690f5a5e46be63cc9cf67388f0f8c3f428
Foteini Giannaropoulou <giannaropoulou.foteini@gmail.com>
2026-05-29 10:50:44 +0300

    Sync: Fix PHP warnings from null/undefined access in 6 modules (#49232)
    * Sync: Fix PHP warnings from null/undefined access in 6 modules

--- projects/packages/sync/src/modules/class-terms.php
+++ projects/packages/sync/src/modules/class-terms.php
@@ -301,7 +301,11 @@ class Terms extends Module {
 	 * @return array|boolean False if not whitelisted, the original hook args otherwise.
 	 */
 	public function filter_blacklisted_taxonomies( $args ) {
-		$term = $args[0];
+		$term = $args[0] ?? null;
+
+		if ( ! $term instanceof \WP_Term ) {
+			return false;
+		}
 
 		if ( in_array( $term->taxonomy, Settings::get_setting( 'taxonomies_blacklist' ), true ) ) {
 			return false;
