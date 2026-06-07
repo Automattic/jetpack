c1a8288880052cb5d675d5dacbadf7c291ca68d2
Damián Suárez <rdsuarez@gmail.com>
2026-06-07 15:15:24 +0900

    add jetpack-widget-primitives package ported from core
    Private, source-consumed copy of @wordpress/widget-primitives until core publishes it.

---
+++ projects/js-packages/widget-primitives/src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Components
+ */
+export { WidgetRender } from './components/widget-render';
+
+/**
+ * Hooks
+ */
+export { useWidgetTypes } from './hooks';
+
+/**
+ * Types
+ */
+export type { WidgetName, WidgetType, WidgetRenderProps, ResolveWidgetModule } from './types';
