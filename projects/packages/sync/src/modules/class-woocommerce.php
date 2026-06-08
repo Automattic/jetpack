a036ceea8d0b5405e6a4a04ac943080aa413c77f
Karen Attfield <karenlattfield@gmail.com>
2026-06-08 12:03:26 +0100

    Sync: Allow syncing WooCommerce Shipping settings page options (#49371)

--- projects/packages/sync/src/modules/class-woocommerce.php
+++ projects/packages/sync/src/modules/class-woocommerce.php
@@ -544,6 +544,12 @@ class WooCommerce extends Module {
 		'woocommerce_shop_page_id',
 		'woocommerce_stock_email_recipient',
 		'woocommerce_stock_format',
+		'woocommerce_pickup_location_settings',  // This and the below options relate to the WooCommerce Shipping settings page. Required for the Activity Log.
+		'pickup_location_pickup_locations',
+		'woocommerce_ship_to_destination',
+		'woocommerce_shipping_cost_requires_address',
+		'woocommerce_shipping_debug_mode',
+		'woocommerce_shipping_hide_rates_when_free',
 		'woocommerce-ppcp-data-payment', // This and the below options relate to the Pay with PayPal payments settings page. Required for the Activity Log.
 		'woocommerce-ppcp-data-settings',
 		'woocommerce_ppcp-applepay_settings',
