22075062af9994cfd13be35bb216e9dedf7438a3
tbradsha <32492176+tbradsha@users.noreply.github.com>
2026-06-09 16:46:30 -0600

    Add tests

--- projects/packages/account-protection/tests/php/Validation_Service_Test.php
+++ projects/packages/account-protection/tests/php/Validation_Service_Test.php
@@ -204,6 +204,51 @@ class Validation_Service_Test extends BaseTestCase {
 		$this->assertTrue( $validation_service->is_invalid_length( $long_password ) );
 	}
 
+	public function test_min_length_is_filterable() {
+		$password           = 'abcdef';
+		$validation_service = new Validation_Service( $this->get_connection_manager() );
+		$this->assertSame( 6, $validation_service->get_min_length() );
+
+		$callback = function () {
+			return 16;
+		};
+		add_filter( 'jetpack_account_protection_validation_min_length', $callback );
+
+		$this->assertSame( 16, $validation_service->get_min_length() );
+		$this->assertTrue( $validation_service->is_invalid_length( $password ) );
+
+		remove_filter( 'jetpack_account_protection_validation_min_length', $callback );
+	}
+
+	public function test_min_length_cannot_be_filtered_below_default() {
+		$password = 'asdf';
+
+		$validation_service = new Validation_Service( $this->get_connection_manager() );
+
+		$callback = function () {
+			return 4;
+		};
+		add_filter( 'jetpack_account_protection_validation_min_length', $callback );
+		$this->assertSame( 6, $validation_service->get_min_length() );
+		$this->assertTrue( $validation_service->is_invalid_length( $password ) );
+		remove_filter( 'jetpack_account_protection_validation_min_length', $callback );
+	}
+
+	public function test_max_length_is_filterable() {
+		$password = str_repeat( 'a', 21 );
+
+		$validation_service = new Validation_Service( $this->get_connection_manager() );
+		$this->assertSame( 150, $validation_service->get_max_length() );
+
+		$callback = function () {
+			return 20;
+		};
+		add_filter( 'jetpack_account_protection_validation_max_length', $callback );
+		$this->assertSame( 20, $validation_service->get_max_length() );
+		$this->assertTrue( $validation_service->is_invalid_length( $password ) );
+		remove_filter( 'jetpack_account_protection_validation_max_length', $callback );
+	}
+
 	public function test_returns_true_if_password_matches_user_data() {
 		$user             = new \WP_User();
 		$user->user_email = 'example@wordpress.com';
