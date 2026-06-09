520a1ea1fe9297873a36f861ffeb8566f9a2c1e0
robertsreberski <robert.sreberski@automattic.com>
2026-06-09 15:39:22 +0200

    Update changelog

--- projects/packages/forms/src/class-jetpack-forms.php
+++ projects/packages/forms/src/class-jetpack-forms.php
@@ -14,7 +14,7 @@ use Automattic\Jetpack\Forms\Dashboard\Dashboard;
  */
 class Jetpack_Forms {
 
-	const PACKAGE_VERSION = '7.22.1';
+	const PACKAGE_VERSION = '7.22.2';
 
 	/**
 	 * Load the contact form module.
