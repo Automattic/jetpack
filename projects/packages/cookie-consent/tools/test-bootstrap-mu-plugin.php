241dbb7b3c4511c40ac27d748a623ed62ab337f4
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2026-06-15 14:49:53 +0800

    test: rewrite package asset URLs for standalone mu-plugin testing

--- projects/packages/cookie-consent/tools/test-bootstrap-mu-plugin.php
+++ projects/packages/cookie-consent/tools/test-bootstrap-mu-plugin.php
@@ -14,6 +14,34 @@ namespace Automattic\Jetpack\CookieConsent\Test;
 
 use Automattic\Jetpack\CookieConsent\Cookie_Consent;
 
+/*
+ * Standalone-test only: the package is loaded straight from the monorepo (outside
+ * wp-content/plugins/), so the plugins_url() calls inside Cookie_Consent resolve
+ * against the absolute monorepo path and 404. A real consuming plugin (package in
+ * its jetpack_vendor/) does not hit this. To test the frontend here, symlink the
+ * package into the web root:
+ *
+ *   ln -s <monorepo>/projects/packages/cookie-consent \
+ *     wp-content/plugins/jetpack-cookie-consent
+ *
+ * then rewrite the package asset URLs to that web-accessible symlink.
+ */
+add_filter(
+	'plugins_url',
+	static function ( $url, $path, $plugin ) {
+		$link = WP_PLUGIN_DIR . '/jetpack-cookie-consent';
+		if ( is_string( $plugin )
+			&& false !== strpos( $plugin, '/packages/cookie-consent/' )
+			&& is_link( $link )
+		) {
+			return plugins_url( $path, $link . '/src' );
+		}
+		return $url;
+	},
+	20,
+	3
+);
+
 $jetpack_cookie_consent_autoload = __DIR__ . '/../vendor/autoload.php';
 
 if ( file_exists( $jetpack_cookie_consent_autoload ) ) {
