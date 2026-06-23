17563d0bf3e85e572279031c42e84df71e5d4ed5
Mikael Korpela <mikael@ihminen.org>
2026-06-23 16:28:01 +0300

    Components: remove spinner (#49856)

--- projects/js-packages/components/index.ts
+++ projects/js-packages/components/index.ts
@@ -25,8 +25,6 @@ export { default as AutomatticBylineLogo } from './components/automattic-byline-
 export { default as AutomatticIconLogo } from './components/automattic-icon-logo/index.tsx';
 export { default as AutomatticForAgenciesLogo } from './components/automattic-for-agencies-logo/index.tsx';
 export { default as JetpackFooter } from './components/jetpack-footer/index.tsx';
-/** @deprecated Use `Spinner` from `@wordpress/components` instead. */
-export { default as Spinner } from './components/spinner/index.tsx';
 export { default as Gravatar } from './components/gravatar/index.tsx';
 export type { GravatarProps } from './components/gravatar/index.tsx';
 export { default as IconTooltip } from './components/icon-tooltip/index.tsx';
