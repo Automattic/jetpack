d7fcb4ede4903d112db78f9612c91eacab8fb793
Jasper Kang <jasper.kang@automattic.com>
2026-06-25 10:41:47 +1200

    Premium Analytics: type Stats tags endpoint

--- projects/packages/premium-analytics/packages/data/src/hooks/use-stats-tags.ts
+++ projects/packages/premium-analytics/packages/data/src/hooks/use-stats-tags.ts
@@ -5,7 +5,15 @@ import { statsTagsQuery } from '../queries/stats-tags-query';
 import { useStatsReport } from './use-stats-report';
 import type { UseStatsOptions } from './use-stats-report';
 import type { StatsReportParams } from '../queries/stats-query';
+import type { StatsTagsResponse } from '../queries/stats-tags-query';
+
+export type { StatsTagsResponse } from '../queries/stats-tags-query';
 
 export function useStatsTags( params: StatsReportParams, options?: UseStatsOptions ) {
-	return useStatsReport( statsTagsQuery, params, 'tags', options );
+	return useStatsReport< StatsReportParams, StatsTagsResponse >(
+		statsTagsQuery,
+		params,
+		'tags',
+		options
+	);
 }
--- projects/packages/premium-analytics/packages/data/src/queries/stats-tags-query.ts
+++ projects/packages/premium-analytics/packages/data/src/queries/stats-tags-query.ts
@@ -1,7 +1,14 @@
 /**
  * Internal dependencies
  */
-import { statsReportQuery, type StatsReportParams } from './stats-query';
+import {
+	statsReportQuery,
+	type StatsReportParams,
+	type StatsReportQueryOptions,
+} from './stats-query';
+import type { StatsNormalizedReport, StatsTagsItem } from '../processing/stats';
 
-export const statsTagsQuery = ( params: StatsReportParams ) =>
+export type StatsTagsResponse = StatsNormalizedReport< StatsTagsItem >;
+
+export const statsTagsQuery = ( params: StatsReportParams ): StatsReportQueryOptions< 'tags' > =>
 	statsReportQuery( 'tags', 'stats/tags', params, 'tags' );
