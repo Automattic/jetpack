ff22cb493163646f99f3f9c23b7c8bbeb8c24aa0
Philip Jackson <p-jackson@live.com>
2026-07-08 15:43:07 +1200

    React 19: drop removed MutableRefObject type in code block edit
    Replace React.MutableRefObject<T | undefined> annotations with
    useRef<T>(undefined), inferring RefObject per React 19 types.

--- projects/packages/jetpack-mu-wpcom/src/features/wpcom-blocks/code/block-edit-function/block-edit-function.tsx
+++ projects/packages/jetpack-mu-wpcom/src/features/wpcom-blocks/code/block-edit-function/block-edit-function.tsx
@@ -35,10 +35,8 @@ function EditCodeMirror( props: EditBlockProps ) {
 	const { attributes, isSelected, setAttributes, insertBlocksAfter, onRemove } = props;
 
 	const ref: React.RefObject< HTMLDivElement > = React.useRef( null );
-	const viewRef: React.MutableRefObject< import('@codemirror/view').EditorView | undefined > =
-		React.useRef( undefined );
-	const currentLanguageRef: React.MutableRefObject< LanguageSupport | undefined > =
-		React.useRef( undefined );
+	const viewRef = React.useRef< import('@codemirror/view').EditorView >( undefined );
+	const currentLanguageRef = React.useRef< LanguageSupport >( undefined );
 	const trailingNewlineCounterRef = React.useRef( 0 );
 
 	const { getBlockOrder } = useSelect( blockEditorStore, [] );
