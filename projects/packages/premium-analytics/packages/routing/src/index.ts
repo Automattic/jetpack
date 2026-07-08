95561d75403c60821379f1d8023abaad2a9f2dae
Damián Suárez <rdsuarez@gmail.com>
2026-07-08 22:30:20 +0100

    adopt core admin-ui Breadcrumbs in post detail
    replace custom StatsBreadcrumbs; move link logic to a routing hook

--- projects/packages/premium-analytics/packages/routing/src/index.ts
+++ projects/packages/premium-analytics/packages/routing/src/index.ts
@@ -5,5 +5,14 @@ export {
 } from './search/date-range';
 
 export { deriveComparisonRange } from './search/comparison';
-export { REPORT_DATE_PARAM_KEYS, pickReportDateParams } from './search/report-params';
-export { useStagedSearch, useReportDateFilters, type ReportDateFilters } from './hooks';
+export {
+	REPORT_DATE_PARAM_KEYS,
+	pickReportDateParams,
+	buildDashboardLink,
+} from './search/report-params';
+export {
+	useStagedSearch,
+	useReportDateFilters,
+	useDashboardLink,
+	type ReportDateFilters,
+} from './hooks';
