25f7eec41b9c2ef2ff56ff4b62f3f56d4dc6b726
Damián Suárez <rdsuarez@gmail.com>
2026-07-15 13:20:56 +0100

    rename field folder name

--- projects/packages/premium-analytics/packages/fields/src/index.ts
+++ projects/packages/premium-analytics/packages/fields/src/index.ts
@@ -2,4 +2,5 @@ export {
 	ReportParamsField,
 	type ReportParamsFieldAttributes,
 } from './report-params-field/report-params-field';
-export { ArrayCheckboxField } from './array-checkbox-field';
+
+export { ArrayCheckboxField } from './field-array-checkbox';
