841e80d63617c30df061a9153b181802c33fdb73
Karen Attfield <karenlattfield@gmail.com>
2026-07-16 09:39:39 +0100

    Move StepDisconnectProps to inline types file

--- projects/js-packages/connection/components/disconnect-dialog/steps/step-disconnect.tsx
+++ projects/js-packages/connection/components/disconnect-dialog/steps/step-disconnect.tsx
@@ -2,33 +2,8 @@ import { __ } from '@wordpress/i18n';
 import { Fragment, useCallback, useEffect } from 'react';
 import ConnectedPlugins from '../../connected-plugins';
 import DisconnectActionFooter from '../../shared/disconnect-action-footer';
-import type { MouseEvent, ReactElement } from 'react';
-
-interface StepDisconnectProps {
-	/** The title to show for this section. */
-	title?: string;
-	/** Whether or not a request to disconnect is in progress. */
-	isDisconnecting?: boolean;
-	/** Callback function that is triggered by clicking the "Disconnect" button. */
-	onDisconnect: ( e?: MouseEvent< HTMLElement > ) => void;
-	/** An error message from a failed disconnect request, or false if none. */
-	disconnectError?: string | false;
-	/** A component to be rendered as part of this step. */
-	disconnectStepComponent?: ReactElement;
-	/**
-	 * Plugins that are using the Jetpack connection. Accepts either an array of
-	 * plugins (modern) or a legacy object keyed by slug (classic dashboard).
-	 */
-	connectedPlugins?: Array< { name: string; slug: string } > | Record< string, { name: string } >;
-	/** The slug of the plugin that is initiating the disconnection. */
-	disconnectingPlugin?: string;
-	/** Callback function that closes the modal. */
-	closeModal: () => void;
-	/** Where this modal is being rendered. */
-	context?: string;
-	/** Callback that tracks link/button clicks. */
-	trackModalClick: ( target: string ) => void;
-}
+import type { StepDisconnectProps } from './types';
+import type { MouseEvent } from 'react';
 
 /**
  * Disconnect step in disconnection flow.
