89ae72d136f75923f08cb09de544f0948b350401
Lae <30905719+mlaetitia@users.noreply.github.com>
2026-07-31 19:33:17 +0100

    Random Redirect: restore module to the Jetpack plugin (#50940)
    * Random Redirect: restore the module removed from the Jetpack plugin in #38310.
    
    * Random Redirect: select the random post via COUNT and a random OFFSET instead of ORDER BY RAND().
    
    * Random Redirect: add a jetpack_random_redirect_enabled filter to allow disabling the redirect.
    
    * Random Redirect: add PHPUnit coverage for the module.
    
    * Random Redirect: annotate the test redirect interceptor as never returning.

--- projects/plugins/jetpack/class.jetpack.php
+++ projects/plugins/jetpack/class.jetpack.php
@@ -122,6 +122,9 @@ class Jetpack {
 		'latex'               => array(
 			array( 'wp-latex/wp-latex.php', 'WP LaTeX' ),
 		),
+		'random-redirect'     => array(
+			array( 'random-redirect/random-redirect.php', 'Random Redirect' ),
+		),
 		'sharedaddy'          => array(
 			array( 'sharedaddy/sharedaddy.php', 'Sharedaddy' ),
 			array( 'jetpack-sharing/sharedaddy.php', 'Jetpack Sharing' ),
@@ -210,6 +213,9 @@ class Jetpack {
 			'Wordfence Security'                => 'wordfence/wordfence.php',
 			'All In One WP Security & Firewall' => 'all-in-one-wp-security-and-firewall/wp-security.php',
 			'iThemes Security'                  => 'better-wp-security/better-wp-security.php',
+		),
+		'random-redirect'    => array(
+			'Random Redirect 2' => 'random-redirect-2/random-redirect.php',
 		),
 		'related-posts'      => array(
 			'YARPP'                       => 'yet-another-related-posts-plugin/yarpp.php',
