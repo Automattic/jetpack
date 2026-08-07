2832c2d23623589592c4bd8801dd58cf74610991
Chris Jean <chris.jean@automattic.com>
2026-08-07 13:32:58 -0500

    phpcs-filter: Scope coverage bootstrap variables

--- projects/packages/phpcs-filter/tests/php/coverage-bootstrap.php
+++ projects/packages/phpcs-filter/tests/php/coverage-bootstrap.php
@@ -6,10 +6,12 @@
  * @subpackage Tests
  */
 
-$compatibility_tokens = __DIR__ . '/../../vendor/nikic/php-parser/lib/PhpParser/compatibility_tokens.php';
+( static function () {
+	$compatibility_tokens = __DIR__ . '/../../vendor/nikic/php-parser/lib/PhpParser/compatibility_tokens.php';
 
-if ( file_exists( $compatibility_tokens ) ) {
-	// PHPCS emulates future tokens with string values, while PHP-Parser requires integer token IDs.
-	// Load PHP-Parser's definitions first to prevent that conflict during coverage initialization.
-	require_once $compatibility_tokens;
-}
+	if ( is_file( $compatibility_tokens ) ) {
+		// PHPCS emulates future tokens with string values, while PHP-Parser requires integer token IDs.
+		// Load PHP-Parser's definitions first to prevent that conflict during coverage initialization.
+		require_once $compatibility_tokens;
+	}
+} )();
