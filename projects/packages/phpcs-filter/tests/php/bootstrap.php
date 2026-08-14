c654e31a66b58ddb97ad8c8d9aee98c2f1f6fe30
Brad Jorsch <brad.jorsch@automattic.com>
2026-08-14 13:34:45 -0400

    Fix PHP <7.4 testing, sigh
    nikic/php-parser v5 only supports PHP 7.4+, while we still support 7.2.
    And since v4 doesn't include the `compatibility_tokens.php` file, we
    need a `file_exists` check anyway and may as well just look for the copy
    PHPUnit (maybe) brings in.

--- projects/packages/phpcs-filter/tests/php/bootstrap.php
+++ projects/packages/phpcs-filter/tests/php/bootstrap.php
@@ -11,7 +11,9 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 // PHPCS defines new PHP token constants as strings, while nikic/php-parser
 // (used by PHPUnit's coverage code) complains if they're not ints.
 // Load nikic/php-parser's compatibility file to define them as ints before loading PHPCS's test bootstrap.
-require_once __DIR__ . '/../../vendor/nikic/php-parser/lib/PhpParser/compatibility_tokens.php';
+if ( file_exists( __DIR__ . '/../../vendor/nikic/php-parser/lib/PhpParser/compatibility_tokens.php' ) ) {
+	require_once __DIR__ . '/../../vendor/nikic/php-parser/lib/PhpParser/compatibility_tokens.php';
+}
 
 // Phpcs needs some bootstrapping of its own for tests to work.
 require_once __DIR__ . '/../../vendor/squizlabs/php_codesniffer/tests/bootstrap.php';
