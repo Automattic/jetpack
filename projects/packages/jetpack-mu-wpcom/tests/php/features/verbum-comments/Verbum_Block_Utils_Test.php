b5315ce61fac92cce794adeacd6c5ed4d2e95ff5
Jasper Kang <jasper@Jaspers-MBP16.airstation.home.kangzj.net>
2026-08-19 14:14:46 +1200

    Address review: strict has_filter check and detection breadcrumbs (comment #5336637793)

--- projects/packages/jetpack-mu-wpcom/tests/php/features/verbum-comments/Verbum_Block_Utils_Test.php
+++ projects/packages/jetpack-mu-wpcom/tests/php/features/verbum-comments/Verbum_Block_Utils_Test.php
@@ -44,8 +44,10 @@ class Verbum_Block_Utils_Test extends \WorDBless\BaseTestCase {
 		$comment_content  = '<!-- wp:paragraph --><p>test</p><!-- /wp:paragraph --><!-- wp:list --><ul><!-- wp:list-item --><li>1</li><!-- /wp:list-item --><!-- wp:list-item --><li>2</li><!-- /wp:list-item --><!-- wp:list-item --><li>3</li><!-- /wp:list-item --></ul><!-- /wp:list --><!-- wp:quote --><blockquote class="wp-block-quote"><!-- wp:paragraph --><p>something</p><!-- /wp:paragraph --><cite>someone</cite></blockquote><!-- /wp:quote -->';
 		$filtered_content = preg_replace( '/\R+/', '', Verbum_Block_Utils::render_verbum_blocks( $comment_content ) );
 
-		// WP 7.0 appends a `wp-block-paragraph` class to rendered paragraph blocks.
-		$paragraph_class  = has_filter( 'render_block_core/paragraph', 'block_core_paragraph_add_class' ) ? ' class="wp-block-paragraph"' : '';
+		// WP 7.0 appends `wp-block-paragraph` to rendered paragraph blocks; detected through core's own
+		// filter rather than a version compare, so check for a rename here if this ever mismatches.
+		// The quote's layout classes are emitted by both 6.9 and 7.0.
+		$paragraph_class  = false !== has_filter( 'render_block_core/paragraph', 'block_core_paragraph_add_class' ) ? ' class="wp-block-paragraph"' : '';
 		$expected_content = "<p{$paragraph_class}>test</p><ul><li>1</li><li>2</li><li>3</li></ul><blockquote class=\"wp-block-quote is-layout-flow wp-block-quote-is-layout-flow\"><p{$paragraph_class}>something</p><cite>someone</cite></blockquote>";
 
 		$this->assertSame( $expected_content, $filtered_content );
