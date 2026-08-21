0168808daeddc10f904e9e6cf63343a1dd83dd74
Chris Rosser <chris.rosser@automattic.com>
2026-08-21 16:15:18 +1200

    Akismet: drop aria-current from the header logo link

--- projects/plugins/jetpack/_inc/lib/admin-pages/class-akismet-admin-chrome.php
+++ projects/plugins/jetpack/_inc/lib/admin-pages/class-akismet-admin-chrome.php
@@ -323,14 +323,9 @@ class Akismet_Admin_Chrome {
 	 */
 	public function render_header() {
 		$this->print_styles();
-
-		// The header renders on every Akismet page, so on the settings page itself the link
-		// is self-referential. `$plugin_page` is core's sanitized copy of the `page` query arg.
-		global $plugin_page;
-		$aria_current = 'akismet-key-config' === $plugin_page ? 'page' : 'false';
 		?>
 		<header class="jp-akismet-header">
-			<a class="jp-akismet-header__title" href="<?php echo esc_url( admin_url( 'admin.php?page=akismet-key-config' ) ); ?>" aria-current="<?php echo esc_attr( $aria_current ); ?>">
+			<a class="jp-akismet-header__title" href="<?php echo esc_url( admin_url( 'admin.php?page=akismet-key-config' ) ); ?>">
 				<span class="jp-akismet-header__visual" aria-hidden="true"><?php echo $this->akismet_logo( 20 ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- static SVG. ?></span>
 				<h1><?php esc_html_e( 'Akismet Anti-spam', 'jetpack' ); ?></h1>
 			</a>
