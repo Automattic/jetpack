e286de3e63f43ecde8d5b31090d5ad4c30adaa28
Damián Suárez <rdsuarez@gmail.com>
2026-09-03 13:07:57 +0100

    export only the grid animation and its props type
    keyframes, tile types and the static frame stay internal to the folder; stories and tests import them by path

--- projects/packages/premium-analytics/packages/ui/src/index.ts
+++ projects/packages/premium-analytics/packages/ui/src/index.ts
@@ -24,14 +24,4 @@ export { StaleDataNotice } from './stale-data-notice';
 export { StatsBreadcrumbs, type StatsBreadcrumbsProps } from './stats-breadcrumbs';
 export { StatsPageIcon } from './stats-page-icon';
 export { usePaginatedView, type PaginatedView } from './use-paginated-view';
-export {
-	WidgetGridAnimation,
-	WIDGET_GRID_KEYFRAMES,
-	WIDGET_GRID_STATIC_FRAME,
-	type WidgetGridAnimationProps,
-	type WidgetGridKeyframe,
-	type WidgetGridRect,
-	type WidgetGridTile,
-	type WidgetGridTileContent,
-	type WidgetGridTileId,
-} from './widget-grid-animation';
+export { WidgetGridAnimation, type WidgetGridAnimationProps } from './widget-grid-animation';
--- projects/packages/premium-analytics/packages/ui/src/widget-grid-animation/index.ts
+++ projects/packages/premium-analytics/packages/ui/src/widget-grid-animation/index.ts
@@ -1,11 +1,1 @@
-export {
-	WIDGET_GRID_CANVAS,
-	WIDGET_GRID_KEYFRAMES,
-	WIDGET_GRID_STATIC_FRAME,
-	type WidgetGridKeyframe,
-	type WidgetGridRect,
-	type WidgetGridTile,
-	type WidgetGridTileContent,
-	type WidgetGridTileId,
-} from './keyframes';
 export { WidgetGridAnimation, type WidgetGridAnimationProps } from './widget-grid-animation';
