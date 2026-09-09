8e155978e98923e72a812e484d9328e6c64d39f9
Chris Jean <chris.jean@automattic.com>
2026-09-09 12:24:26 -0500

    Forms: release 8.0.2

--- projects/packages/forms/src/class-jetpack-forms.php
+++ projects/packages/forms/src/class-jetpack-forms.php
@@ -16,7 +16,7 @@ use Automattic\Jetpack\Forms\Dashboard\Dashboard;
  */
 class Jetpack_Forms {
 
-	const PACKAGE_VERSION = '8.0.1';
+	const PACKAGE_VERSION = '8.0.2';
 
 	/**
 	 * Name of the feature flag gating field conditional logic.
