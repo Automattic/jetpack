0cb1cc25de0354f060dd649aaec345be437c7fb9
Jarda Snajdr <jsnajdr@gmail.com>
2026-09-09 12:39:54 +0200

    Enable React 19 on 20% of Atomic sites (#52076)

--- projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
+++ projects/packages/jetpack-mu-wpcom/src/class-jetpack-mu-wpcom.php
@@ -31,6 +31,8 @@ class Jetpack_Mu_Wpcom {
 		'wp-table-builder/wp-table-builder.php',
 		'ultimate-blocks/ultimate-blocks.php',
 		'beehive-analytics/beehive-analytics.php',
+		'xspeed/xspeed.php',
+		'classified-listing/classified-listing.php',
 	);
 
 	/**
@@ -991,7 +993,7 @@ class Jetpack_Mu_Wpcom {
 			} elseif ( self::has_react_19_incompatible_extension() ) {
 				$is_enabled = false;
 			} else {
-				$current_segment = 10; // Segment of Atomic sites in the experiment, in %.
+				$current_segment = 20; // Segment of Atomic sites in the experiment, in %.
 				$site_segment    = $site_id % 100;
 
 				/*
