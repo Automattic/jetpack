f6008179c90c213e8d0ad2b431c4a2224f50dcbc
Liam Sarsfield <43409125+LiamSarsfield@users.noreply.github.com>
2026-09-10 16:20:50 +0100

    Restore the legacy Boost close button

--- projects/plugins/boost/app/assets/src/js/features/ui/close-button/close-button.tsx
+++ projects/plugins/boost/app/assets/src/js/features/ui/close-button/close-button.tsx
@@ -1,9 +1,8 @@
 import { __ } from '@wordpress/i18n';
-import type { MouseEvent } from 'react';
 import styles from './close-button.module.scss';
 
-const CloseButton = ( { onClick }: { onClick: () => void } ) => {
-	const handleOnClick = ( event: MouseEvent< HTMLAnchorElement > ) => {
+const CloseButton = ( { onClick } ) => {
+	const handleOnClick = event => {
 		event.preventDefault();
 		onClick();
 	};
