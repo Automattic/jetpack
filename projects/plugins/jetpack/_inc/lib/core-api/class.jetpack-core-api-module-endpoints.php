60eef0c28325ef7d2b0ba23bbd4b254ff07e0129
Adnan Haque <adnan007.id@gmail.com>
2026-09-10 20:49:51 -0400

    Revert "Jetpack: validate site verification codes (#52091)" (#52209)
    This reverts commit a1db6c69d214825e40dc73e8bd1fe51cfd9c68d2.

--- projects/plugins/jetpack/_inc/lib/core-api/class.jetpack-core-api-module-endpoints.php
+++ projects/plugins/jetpack/_inc/lib/core-api/class.jetpack-core-api-module-endpoints.php
@@ -816,13 +816,12 @@ class Jetpack_Core_API_Data extends Jetpack_Core_API_XMLRPC_Consumer_Endpoint {
 					$grouped_options_current = (array) get_option( 'verification_services_codes' );
 					$grouped_options         = $grouped_options_current;
 
-					$validated_code = jetpack_verification_validate_code( $value );
-					if ( false === $validated_code ) {
-						$error = esc_html__( 'The site verification code is invalid.', 'jetpack' );
-						break;
+					// Extracts the content attribute from the HTML meta tag if needed.
+					if ( preg_match( '#.*<meta name="(?:[^"]+)" content="([^"]+)" />.*#i', $value, $matches ) ) {
+						$grouped_options[ $option ] = $matches[1];
+					} else {
+						$grouped_options[ $option ] = $value;
 					}
-
-					$grouped_options[ $option ] = $validated_code;
 
 					// If option value was the same, consider it done.
 					$updated = $grouped_options_current !== $grouped_options
--- projects/plugins/jetpack/tests/php/modules/verification-tools/Jetpack_Verification_Tools_Utils_Test.php
+++ projects/plugins/jetpack/tests/php/modules/verification-tools/Jetpack_Verification_Tools_Utils_Test.php
@@ -1,15 +1,12 @@
 <?php
 
 use PHPUnit\Framework\Attributes\CoversFunction;
-use PHPUnit\Framework\Attributes\DataProvider;
 require __DIR__ . '/../../../../modules/verification-tools/verification-tools-utils.php';
 
 /**
  * @covers ::jetpack_verification_validate
- * @covers ::jetpack_verification_validate_code
  */
 #[CoversFunction( 'jetpack_verification_validate' )]
-#[CoversFunction( 'jetpack_verification_validate_code' )]
 class Jetpack_Verification_Tools_Utils_Test extends WP_UnitTestCase {
 	use \Automattic\Jetpack\PHPUnit\WP_UnitTestCase_Fix;
 
@@ -19,9 +16,9 @@ class Jetpack_Verification_Tools_Utils_Test extends WP_UnitTestCase {
 	 */
 	public function test_jetpack_verification_validate_google_raw_code() {
 		$this->assertEquals(
-			array( 'google' => '+nxGUDJ4QpAZ5l9Bsjdi102tLVC21AIh5d1Nl23908vVuFHs34=' ),
-			jetpack_verification_validate( array( 'google' => '+nxGUDJ4QpAZ5l9Bsjdi102tLVC21AIh5d1Nl23908vVuFHs34=' ) ),
-			'raw code should be accepted'
+			array( 'google' => 'W2gxpExLATRT5c0dgRjlJsXRnrLE7vpr_1YtYxEnDIzn9ylj7C' ),
+			jetpack_verification_validate( array( 'google' => 'W2gxpExLATRT5c0dgRjlJsXRnrLE7vpr_1YtYxEnDIzn9ylj7C' ) ),
+			'raw code should be accepeted'
 		);
 	}
 
@@ -31,8 +28,8 @@ class Jetpack_Verification_Tools_Utils_Test extends WP_UnitTestCase {
 	 */
 	public function test_jetpack_verification_validate_google_code_in_meta_double_quotes() {
 		$this->assertEquals(
-			array( 'test' => '+nxGUDJ4QpAZ5l9Bsjdi102tLVC21AIh5d1Nl23908vVuFHs34=' ),
-			jetpack_verification_validate( array( 'test' => '<meta name="google-site-verification" content="+nxGUDJ4QpAZ5l9Bsjdi102tLVC21AIh5d1Nl23908vVuFHs34=" />' ) ),
+			array( 'test' => 'bX1szG_kxD6O0CGSVgS8m4F5gKvgUPMdo96McTiJ7pZ5Ax7mQr' ),
+			jetpack_verification_validate( array( 'test' => '<meta name="google-site-verification" content="bX1szG_kxD6O0CGSVgS8m4F5gKvgUPMdo96McTiJ7pZ5Ax7mQr" />' ) ),
 			'google-style meta tag with double quotes should be accepeted'
 		);
 	}
@@ -46,87 +43,6 @@ class Jetpack_Verification_Tools_Utils_Test extends WP_UnitTestCase {
 			array( 'test' => 'jLjbTBvtuQepL3eR09id83p4q_w8JBStrB5DKCunOX7kK1XKub' ),
 			jetpack_verification_validate( array( 'test' => '<meta name="google-site-verification" content=\'jLjbTBvtuQepL3eR09id83p4q_w8JBStrB5DKCunOX7kK1XKub\' />' ) ),
 			'google-style meta tag with single quotes should be accepeted'
-		);
-	}
-
-	/**
-	 * Verification codes with valid characters are accepted for every service.
-	 *
-	 * @dataProvider valid_verification_code_provider
-	 *
-	 * @param string $service Verification service key.
-	 * @param string $code    Verification code.
-	 */
-	#[DataProvider( 'valid_verification_code_provider' )]
-	public function test_valid_code_is_accepted_for_every_service( $service, $code ) {
-		$this->assertSame(
-			array( $service => $code ),
-			jetpack_verification_validate( array( $service => $code ) )
-		);
-	}
-
-	/**
-	 * Verification codes with invalid characters are rejected for every service.
-	 *
-	 * @dataProvider invalid_verification_code_provider
-	 *
-	 * @param string $service Verification service key.
-	 * @param string $code    Verification code.
-	 */
-	#[DataProvider( 'invalid_verification_code_provider' )]
-	public function test_invalid_code_is_rejected_for_every_service( $service, $code ) {
-		$this->assertSame(
-			array( $service => '' ),
-			jetpack_verification_validate( array( $service => $code ) )
-		);
-	}
-
-	/**
-	 * The validation action receives the same canonical code that is returned for storage.
-	 */
-	public function test_validation_action_receives_canonical_code() {
-		$action_code = null;
-		$callback    = static function ( $service, $code ) use ( &$action_code ) {
-			$action_code = $code;
-		};
-		add_action( 'jetpack_site_verification_validate', $callback, 10, 2 );
-
-		$validated = jetpack_verification_validate( array( 'google' => 'verification&Code' ) );
-
-		remove_action( 'jetpack_site_verification_validate', $callback, 10 );
-		$this->assertSame( $validated['google'], $action_code );
-	}
-
-	/**
-	 * Provide valid verification codes.
-	 *
-	 * @return array<string, array{string, string}> Test cases.
-	 */
-	public static function valid_verification_code_provider() {
-		return array(
-			'google'           => array( 'google', '+nxGUDJ4QpAZ5l9Bsjdi102tLVC21AIh5d1Nl23908vVuFHs34=' ),
-			'bing'             => array( 'bing', '12C1203B5086AECE94EB3A3D9830B2E' ),
-			'pinterest'        => array( 'pinterest', 'f100679e6048d45e4a0b0b92dce1efce' ),
-			'yandex'           => array( 'yandex', '44d68e1216009f40' ),
-			'facebook'         => array( 'facebook', 'rvv8b23jxlp1lq41I9rwsvpzncy1fd' ),
-			'safe punctuation' => array( 'google', 'verification.Code_123-+/=:@~' ),
-			'ampersand'        => array( 'bing', 'verification&Code' ),
-		);
-	}
-
-	/**
-	 * Provide invalid verification codes.
-	 *
-	 * @return array<string, array{string, string}> Test cases.
-	 */
-	public static function invalid_verification_code_provider() {
-		return array(
-			'opening angle bracket'        => array( 'google', 'invalid<script' ),
-			'closing angle bracket'        => array( 'bing', 'invalid>script' ),
-			'double quote'                 => array( 'pinterest', 'invalid"code' ),
-			'single quote'                 => array( 'yandex', "invalid'code" ),
-			'control character'            => array( 'facebook', "invalid\ncode" ),
-			'unsafe character after limit' => array( 'google', '<meta name="google-site-verification" content="' . str_repeat( 'a', 100 ) . '<" />' ),
 		);
 	}
 }
