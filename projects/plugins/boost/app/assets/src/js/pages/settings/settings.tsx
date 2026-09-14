5f94bf687cdcad1dddf5d210d11b30817835cd06
Xavier Lozano Carreras <xavier.lozano.carreras@automattic.com>
2026-09-14 18:57:38 +0200

    Boost: render the grouped Settings page in the modern chassis

---
+++ projects/plugins/boost/app/assets/src/js/pages/settings/settings.tsx
@@ -0,0 +1,51 @@
+import { __ } from '@wordpress/i18n';
+import { Card, Stack } from '@wordpress/ui';
+import CornerstonePagesCard from '$features/cornerstone-pages/cornerstone-pages-card';
+import CloudCssModule from '$features/critical-css/cloud-css-module/cloud-css-module';
+import CriticalCssModule from '$features/critical-css/critical-css-module/critical-css-module';
+import ImageCdn from '$features/image-cdn/image-cdn';
+import ImageGuide from '$features/image-guide/image-guide';
+import MinifyCss from '$features/minify-css/minify-css';
+import MinifyJs from '$features/minify-js/minify-js';
+import { ModuleSurfaceProvider } from '$features/module/surface';
+import PageCacheModule from '$features/page-cache/page-cache';
+import RenderBlockingJs from '$features/render-blocking-js/render-blocking-js';
+import styles from './settings.module.scss';
+import type { ReactNode } from 'react';
+
+const Group = ( { title, children }: { title: string; children: ReactNode } ) => (
+	<Card.Root>
+		<Card.Header>
+			<Card.Title render={ <h2 /> }>{ title }</Card.Title>
+		</Card.Header>
+		<Card.Content>
+			<Stack direction="column" gap="xl">
+				{ children }
+			</Stack>
+		</Card.Content>
+	</Card.Root>
+);
+
+const Settings = () => (
+	<ModuleSurfaceProvider value="row">
+		<Stack direction="column" gap="xl" className={ styles.settings }>
+			<CornerstonePagesCard />
+			<Group title={ __( 'Code loading optimization', 'jetpack-boost' ) }>
+				<CriticalCssModule />
+				<CloudCssModule />
+				<PageCacheModule />
+				<RenderBlockingJs />
+				<MinifyJs />
+				<MinifyCss />
+			</Group>
+			<Group title={ __( 'Image CDN configuration', 'jetpack-boost' ) }>
+				<ImageCdn />
+			</Group>
+			<Group title={ __( 'Image guide', 'jetpack-boost' ) }>
+				<ImageGuide />
+			</Group>
+		</Stack>
+	</ModuleSurfaceProvider>
+);
+
+export default Settings;
