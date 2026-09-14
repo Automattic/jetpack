fcfaf457fd2989944e6e564ee2da12ab63f0d0ab
Christian Gastrell <cgastrell@gmail.com>
2026-09-14 18:58:47 -0300

    Modules: Keep admin notices out of the module list

--- projects/plugins/jetpack/_inc/lib/admin-pages/class.jetpack-settings-page.php
+++ projects/plugins/jetpack/_inc/lib/admin-pages/class.jetpack-settings-page.php
@@ -107,6 +107,9 @@ class Jetpack_Settings_Page extends Jetpack_Admin_Page {
 			$rest_api_notice
 		);
 
+		// Without this, core relocates other plugins' notices into the first module group heading below.
+		echo '<div class="wp-header-end"></div>';
+
 		if ( ! $this->is_rest_api_enabled() ) {
 			echo $rest_api_notice; //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 		}
