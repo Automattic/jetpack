d7cd535d06c8ec6818cf7b448fd4da5e6f5dee2e
Rafael Funchal <rafael.funchal@automattic.com>
2026-09-15 17:12:36 -0300

    Fix Masterbar isolated test bootstrap on PHP 7.4

--- projects/packages/masterbar/tests/php/bootstrap.php
+++ projects/packages/masterbar/tests/php/bootstrap.php
@@ -13,6 +13,11 @@ require_once __DIR__ . '/../../vendor/antecedent/patchwork/Patchwork.php';
 
 define( 'WP_DEBUG', true );
 
+// Process-isolated tests need a script filename to avoid wp_guess_url() warnings on PHP 7.4.
+if ( empty( $_SERVER['SCRIPT_FILENAME'] ) ) {
+	$_SERVER['SCRIPT_FILENAME'] = __FILE__;
+}
+
 // Initialize WordPress test environment
 \Automattic\Jetpack\Test_Environment::init();
 
