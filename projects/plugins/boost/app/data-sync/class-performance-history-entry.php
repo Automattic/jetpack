4c1f3d617470d2296832e034010c95d18246ae72
Liam Sarsfield <43409125+LiamSarsfield@users.noreply.github.com>
2026-09-15 14:32:15 +0100

    Reference the modernization filter documentation

--- projects/plugins/boost/app/data-sync/class-performance-history-entry.php
+++ projects/plugins/boost/app/data-sync/class-performance-history-entry.php
@@ -22,6 +22,7 @@ class Performance_History_Entry implements Lazy_Entry, Entry_Can_Get, Entry_Can_
 		$request = new Speed_Score_Graph_History_Request( $this->start_date, $this->end_date, array() );
 		$result  = $request->execute();
 
+		/** This filter is documented in app/admin/class-admin.php */
 		if ( is_wp_error( $result ) && $this->surface_errors && apply_filters( Admin::MODERNIZATION_FILTER, false ) ) {
 			throw new \RuntimeException( $result->get_error_message() );
 		}
