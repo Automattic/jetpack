41b6c0dd2a18c747f71a19ed64a43c8b28ef04f5
Liam Sarsfield <43409125+LiamSarsfield@users.noreply.github.com>
2026-09-15 14:32:05 +0100

    Keep the modern dashboard helper private

--- projects/plugins/boost/app/admin/class-admin.php
+++ projects/plugins/boost/app/admin/class-admin.php
@@ -89,7 +89,7 @@ class Admin {
 	 *
 	 * @return bool Whether modernization is enabled for this admin request.
 	 */
-	public static function is_modern_dashboard() {
+	private static function is_modern_dashboard() {
 		/**
 		 * Enable the modern Boost dashboard.
 		 *
