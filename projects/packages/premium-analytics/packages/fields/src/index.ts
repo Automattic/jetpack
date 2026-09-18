3200348b45dc9768e5aa780aaa024de3037d2755
Damián Suárez <rdsuarez@gmail.com>
2026-09-18 19:03:25 +0100

    rget rid of ArrayCheckboxField. Use SelectField instead.

--- projects/packages/premium-analytics/packages/fields/src/index.ts
+++ projects/packages/premium-analytics/packages/fields/src/index.ts
@@ -4,8 +4,6 @@ export {
 	type ReportParamsFieldAttributes,
 } from './report-params-field/report-params-field';
 
-export { ArrayCheckboxField } from './field-array-checkbox';
-
 export { SelectField } from './field-select';
 
 export { ToggleGroupField } from './field-toggle-group';
--- projects/packages/premium-analytics/widgets/wordads-highlights/widget.ts
+++ projects/packages/premium-analytics/widgets/wordads-highlights/widget.ts
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { ArrayCheckboxField } from '@jetpack-premium-analytics/fields';
+import { SelectField } from '@jetpack-premium-analytics/fields';
 import { __ } from '@wordpress/i18n';
 import { megaphone } from '@wordpress/icons';
 import type { WidgetAttributeField } from '@wordpress/widget-primitives';
@@ -51,7 +51,7 @@ export default {
 			label: __( 'Metrics', 'jetpack-premium-analytics-pkg' ),
 			type: 'array',
 			relevance: 'high',
-			Edit: ArrayCheckboxField,
+			Edit: SelectField,
 			elements: WORDADS_EARNINGS_METRICS.map( metric => ( {
 				value: metric.id,
 				label: metric.label,
