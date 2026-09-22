0309edd949f33836625d890076e4e9d405574d70
Liam Sarsfield <43409125+LiamSarsfield@users.noreply.github.com>
2026-09-22 13:21:17 +0100

    Boost: show a fallback upgrade link when no listener answers the Overview upgrade slot

--- projects/plugins/boost/_inc/overview/lib/upgrade-bridge.ts
+++ projects/plugins/boost/_inc/overview/lib/upgrade-bridge.ts
@@ -1,5 +1,6 @@
 export const OVERVIEW_UPGRADE_EVENT = 'jetpack-boost:mount-overview-upgrade';
 
+// A listener must set `unmount` synchronously during dispatch; an unset `unmount` means nothing answered.
 export type UpgradeSlotRequest = {
 	container: HTMLElement;
 	unmount?: () => void;
--- projects/plugins/boost/_inc/overview/upgrade-cta.tsx
+++ projects/plugins/boost/_inc/overview/upgrade-cta.tsx
@@ -1,19 +1,33 @@
-import { useEffect, useRef } from 'react';
+import { __ } from '@wordpress/i18n';
+import { Link } from '@wordpress/ui';
+import { useEffect, useRef, useState } from 'react';
 import { OVERVIEW_UPGRADE_EVENT, type UpgradeSlotRequest } from './lib/upgrade-bridge';
 import { canOfferUpgrade } from './lib/use-modules-state';
 
 export default function UpgradeCTA() {
 	const container = useRef< HTMLSpanElement >( null );
+	const [ isUnanswered, setIsUnanswered ] = useState( false );
 	useEffect( () => {
 		if ( ! container.current ) {
 			return;
 		}
 		const request: UpgradeSlotRequest = { container: container.current };
 		window.dispatchEvent( new CustomEvent( OVERVIEW_UPGRADE_EVENT, { detail: request } ) );
+		if ( ! request.unmount ) {
+			setIsUnanswered( true );
+			return;
+		}
 		return () => request.unmount?.();
 	}, [] );
 	if ( ! canOfferUpgrade() ) {
 		return null;
+	}
+	if ( isUnanswered ) {
+		return (
+			<Link href="admin.php?page=my-jetpack#/add-boost">
+				{ __( 'Upgrade now', 'jetpack-boost' ) }
+			</Link>
+		);
 	}
 	return <span ref={ container } />;
 }
