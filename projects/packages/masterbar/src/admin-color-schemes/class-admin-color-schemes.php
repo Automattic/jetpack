35e276e77083a67b1bfa446bb38a49b1c7908f2c
Ashar Fuadi <ashar.fuadi@automattic.com>
2026-09-25 15:52:41 +0700

    Admin color schemes: Deprecate the WordPress.com-specific color schemes
    Label the WordPress.com color schemes as deprecated in the profile page
    picker, list them after the core schemes, and show a warning notice to
    users who still use one.

--- projects/packages/masterbar/src/admin-color-schemes/class-admin-color-schemes.php
+++ projects/packages/masterbar/src/admin-color-schemes/class-admin-color-schemes.php
@@ -24,11 +24,20 @@ class Admin_Color_Schemes {
 	const CORE_COLOR_SCHEMES = array( 'blue', 'coffee', 'ectoplasm', 'fresh', 'light', 'midnight', 'modern', 'ocean', 'sunrise' );
 
 	/**
+	 * A list of WP.com color schemes that are now deprecated.
+	 *
+	 * @var array
+	 */
+	const WPCOM_COLOR_SCHEMES = array( 'aquatic', 'classic-blue', 'classic-bright', 'classic-dark', 'contrast', 'nightfall', 'powder-snow', 'sakura', 'sunset' );
+
+	/**
 	 * Admin_Color_Schemes constructor.
 	 */
 	public function __construct() {
 		// We want to register the admin color schemes across all environments.
 		add_action( 'admin_init', array( $this, 'register_admin_color_schemes' ) );
+		add_action( 'admin_notices', array( $this, 'show_deprecated_admin_color_scheme_notice' ) );
+		add_action( 'admin_color_scheme_picker', array( $this, 'move_admin_color_schemes_last' ), 11 );
 		// We don't want to make the admin_color available in users REST API endpoint for Simple sites.
 		if ( false === ( new Host() )->is_wpcom_simple() ) {
 			add_action( 'rest_api_init', array( $this, 'register_admin_color_meta' ) );
@@ -83,6 +92,36 @@ class Admin_Color_Schemes {
 	}
 
 	/**
+	 * Gets the translated names of the color schemes, keyed by slug.
+	 *
+	 * @return array
+	 */
+	private function get_admin_color_scheme_names() {
+		return array(
+			'aquatic'        => __( 'Aquatic', 'jetpack-masterbar' ),
+			'classic-blue'   => __( 'Classic Blue', 'jetpack-masterbar' ),
+			'classic-bright' => __( 'Classic Bright', 'jetpack-masterbar' ),
+			'classic-dark'   => __( 'Classic Dark', 'jetpack-masterbar' ),
+			'contrast'       => __( 'Contrast', 'jetpack-masterbar' ),
+			'nightfall'      => __( 'Nightfall', 'jetpack-masterbar' ),
+			'powder-snow'    => __( 'Powder Snow', 'jetpack-masterbar' ),
+			'sakura'         => __( 'Sakura', 'jetpack-masterbar' ),
+			'sunset'         => __( 'Sunset', 'jetpack-masterbar' ),
+		);
+	}
+
+	/**
+	 * Gets a color scheme's name, marked as deprecated.
+	 *
+	 * @param string $color_scheme The color scheme slug.
+	 * @return string
+	 */
+	private function get_admin_color_scheme_name( $color_scheme ) {
+		/* translators: %s: Admin color scheme name, e.g. "Aquatic". */
+		return sprintf( __( '%s (Deprecated)', 'jetpack-masterbar' ), $this->get_admin_color_scheme_names()[ $color_scheme ] );
+	}
+
+	/**
 	 * Registers new admin color schemes
 	 */
 	public function register_admin_color_schemes() {
@@ -102,7 +141,7 @@ class Admin_Color_Schemes {
 
 		wp_admin_css_color(
 			'aquatic',
-			__( 'Aquatic', 'jetpack-masterbar' ),
+			$this->get_admin_color_scheme_name( 'aquatic' ),
 			$this->get_admin_color_scheme_url( 'aquatic' ),
 			array( '#135e96', '#007e65', '#043959', '#c5d9ed' ),
 			array(
@@ -114,7 +153,7 @@ class Admin_Color_Schemes {
 
 		wp_admin_css_color(
 			'classic-blue',
-			__( 'Classic Blue', 'jetpack-masterbar' ),
+			$this->get_admin_color_scheme_name( 'classic-blue' ),
 			$this->get_admin_color_scheme_url( 'classic-blue' ),
 			array( '#135e96', '#b26200', '#dcdcde', '#646970' ),
 			array(
@@ -126,7 +165,7 @@ class Admin_Color_Schemes {
 
 		wp_admin_css_color(
 			'classic-bright',
-			__( 'Classic Bright', 'jetpack-masterbar' ),
+			$this->get_admin_color_scheme_name( 'classic-bright' ),
 			$this->get_admin_color_scheme_url( 'classic-bright' ),
 			array( '#135e96', '#c9256e', '#ffffff', '#e9eff5' ),
 			array(
@@ -138,7 +177,7 @@ class Admin_Color_Schemes {
 
 		wp_admin_css_color(
 			'classic-dark',
-			__( 'Classic Dark', 'jetpack-masterbar' ),
+			$this->get_admin_color_scheme_name( 'classic-dark' ),
 			$this->get_admin_color_scheme_url( 'classic-dark' ),
 			array( '#101517', '#c9356e', '#32373c', '#0073aa' ),
 			array(
@@ -150,7 +189,7 @@ class Admin_Color_Schemes {
 
 		wp_admin_css_color(
 			'contrast',
-			__( 'Contrast', 'jetpack-masterbar' ),
+			$this->get_admin_color_scheme_name( 'contrast' ),
 			$this->get_admin_color_scheme_url( 'contrast' ),
 			array( '#101517', '#ffffff', '#32373c', '#b4b9be' ),
 			array(
@@ -162,7 +201,7 @@ class Admin_Color_Schemes {
 
 		wp_admin_css_color(
 			'nightfall',
-			__( 'Nightfall', 'jetpack-masterbar' ),
+			$this->get_admin_color_scheme_name( 'nightfall' ),
 			$this->get_admin_color_scheme_url( 'nightfall' ),
 			array( '#00131c', '#043959', '#2271b1', '#9ec2e6' ),
 			array(
@@ -174,7 +213,7 @@ class Admin_Color_Schemes {
 
 		wp_admin_css_color(
 			'powder-snow',
-			__( 'Powder Snow', 'jetpack-masterbar' ),
+			$this->get_admin_color_scheme_name( 'powder-snow' ),
 			$this->get_admin_color_scheme_url( 'powder-snow' ),
 			array( '#101517', '#2271b1', '#dcdcde', '#646970' ),
 			array(
@@ -186,7 +225,7 @@ class Admin_Color_Schemes {
 
 		wp_admin_css_color(
 			'sakura',
-			__( 'Sakura', 'jetpack-masterbar' ),
+			$this->get_admin_color_scheme_name( 'sakura' ),
 			$this->get_admin_color_scheme_url( 'sakura' ),
 			array( '#005042', '#f2ceda', '#2271b1', '#8c1749' ),
 			array(
@@ -198,13 +237,58 @@ class Admin_Color_Schemes {
 
 		wp_admin_css_color(
 			'sunset',
-			__( 'Sunset', 'jetpack-masterbar' ),
+			$this->get_admin_color_scheme_name( 'sunset' ),
 			$this->get_admin_color_scheme_url( 'sunset' ),
 			array( '#691c1c', '#b26200', '#f0c930', '#facfd2' ),
 			array(
 				'base'    => '#facfd2',
 				'focus'   => '#fff',
 				'current' => '#4f3500',
+			)
+		);
+	}
+
+	/**
+	 * Asks users who still use a deprecated color scheme to switch to another one.
+	 */
+	public function show_deprecated_admin_color_scheme_notice() {
+		$color_scheme = get_user_option( 'admin_color' );
+		if ( ! in_array( $color_scheme, static::WPCOM_COLOR_SCHEMES, true ) ) {
+			return;
+		}
+
+		wp_admin_notice(
+			sprintf(
+				/* translators: 1: Admin color scheme name, e.g. "Aquatic". 2: URL of the user profile page. */
+				__( 'Your current <strong>%1$s</strong> admin color scheme is now deprecated and will be removed soon. <a href="%2$s">Choose a different color scheme</a>.', 'jetpack-masterbar' ),
+				esc_html( $this->get_admin_color_scheme_names()[ $color_scheme ] ),
+				esc_url( admin_url( 'profile.php#color-picker' ) )
+			),
+			array(
+				'type' => 'warning',
+			)
+		);
+	}
+
+	/**
+	 * Moves the admin color schemes to the end of the color scheme picker.
+	 */
+	public function move_admin_color_schemes_last() {
+		wp_print_inline_script_tag(
+			sprintf(
+				'document.addEventListener( "DOMContentLoaded", function () {
+					var picker = document.getElementById( "color-picker" );
+					if ( ! picker ) {
+						return;
+					}
+					%s.forEach( function ( slug ) {
+						var input = document.getElementById( "admin_color_" + slug );
+						if ( input ) {
+							picker.appendChild( input.parentNode );
+						}
+					} );
+				} );',
+				wp_json_encode( static::WPCOM_COLOR_SCHEMES, JSON_UNESCAPED_SLASHES | JSON_HEX_TAG | JSON_HEX_AMP )
 			)
 		);
 	}
