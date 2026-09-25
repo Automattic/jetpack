9eb34908adc534a22f393ba8eeb17fe1a752deec
Liam Sarsfield <43409125+LiamSarsfield@users.noreply.github.com>
2026-09-25 16:00:00 +0100

    Boost: show a fallback upgrade link when the Overview upgrade slot goes unanswered (#52620)

--- projects/plugins/boost/_inc/overview/upgrade-cta.tsx
+++ projects/plugins/boost/_inc/overview/upgrade-cta.tsx
@@ -1,19 +1,29 @@
-import { useEffect, useRef } from 'react';
-import { OVERVIEW_UPGRADE_EVENT, type UpgradeSlotRequest } from './lib/upgrade-bridge';
+import { __ } from '@wordpress/i18n';
+import { Link } from '@wordpress/ui';
+import { useEffect, useRef, useState } from 'react';
+import { OVERVIEW_UPGRADE_EVENT, type UpgradeSlotRequest, upgradeHref } from './lib/upgrade-bridge';
 import { canOfferUpgrade } from './lib/use-modules-state';
 
 export default function UpgradeCTA() {
 	const container = useRef< HTMLSpanElement >( null );
+	const [ showFallbackLink, setShowFallbackLink ] = useState( false );
 	useEffect( () => {
 		if ( ! container.current ) {
 			return;
 		}
 		const request: UpgradeSlotRequest = { container: container.current };
 		window.dispatchEvent( new CustomEvent( OVERVIEW_UPGRADE_EVENT, { detail: request } ) );
+		if ( ! request.unmount ) {
+			setShowFallbackLink( true );
+			return;
+		}
 		return () => request.unmount?.();
 	}, [] );
 	if ( ! canOfferUpgrade() ) {
 		return null;
+	}
+	if ( showFallbackLink ) {
+		return <Link href={ upgradeHref }>{ __( 'Upgrade now', 'jetpack-boost' ) }</Link>;
 	}
 	return <span ref={ container } />;
 }
--- projects/plugins/boost/app/assets/src/js/modern-overview-upgrade.tsx
+++ projects/plugins/boost/app/assets/src/js/modern-overview-upgrade.tsx
@@ -6,7 +6,7 @@ import {
 	observeLegacyModulesState,
 	observeLegacyOnboarding,
 } from '../../../../_inc/overview/lib/modules-state-bridge';
-import { OVERVIEW_UPGRADE_EVENT } from '../../../../_inc/overview/lib/upgrade-bridge';
+import { OVERVIEW_UPGRADE_EVENT, upgradeHref } from '../../../../_inc/overview/lib/upgrade-bridge';
 import { licenseKeyHref, useCanRedeemLicenseKey } from './features/upgrade-cta/license-key-link';
 import './modern-overview-upgrade.scss';
 import { recordBoostEvent, recordBoostEventAndRedirect } from './lib/utils/analytics';
@@ -24,7 +24,7 @@ async function handleUpgrade( event: MouseEvent< HTMLAnchorElement > ) {
 	}
 	event.preventDefault();
 	await recordBoostEventAndRedirect(
-		'admin.php?page=my-jetpack#/add-boost',
+		upgradeHref,
 		'performance_history_upgrade_cta_click',
 		eventProperties
 	);
@@ -55,7 +55,7 @@ window.addEventListener( OVERVIEW_UPGRADE_EVENT, ( event: Event ) => {
 		root.render(
 			<DataSyncProvider>
 				<span className="jb-modern-upgrade-actions">
-					<Link href="admin.php?page=my-jetpack#/add-boost" onClick={ handleUpgrade }>
+					<Link href={ upgradeHref } onClick={ handleUpgrade }>
 						{ __( 'Upgrade now', 'jetpack-boost' ) }
 					</Link>
 					<OverviewLicenseKeyLink />
