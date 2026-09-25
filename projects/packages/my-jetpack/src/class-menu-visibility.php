d46fa30a1861cc770d6ed9694e89a5e00b4b1ba5
Karen Attfield <karenlattfield@gmail.com>
2026-09-25 13:19:58 +0100

    Admin Menu: Avoid fatal when an older admin-ui package is loaded

--- projects/packages/my-jetpack/src/class-menu-visibility.php
+++ projects/packages/my-jetpack/src/class-menu-visibility.php
@@ -33,6 +33,10 @@ class Menu_Visibility {
 	 * @return void
 	 */
 	public static function init() {
+		if ( ! method_exists( Admin_Menu::class, 'set_visibility_resolver' ) ) {
+			return;
+		}
+
 		Admin_Menu::set_visibility_resolver( array( __CLASS__, 'resolve' ) );
 
 		// Memoized answers last one registration pass, which is as long as they can stay true.
